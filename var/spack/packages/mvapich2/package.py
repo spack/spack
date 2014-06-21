5a4881c086111a07d93e0e7d5d4f25ba09bb56cc
Luc Jaulmes <jaulmes1@llnl.gov>
2014-06-20 17:15:32 -0700

    Adding Extrae and OmpSs with some of their dependencies, hwloc and PAPI. Extrae does not compile for latest versions of any MPI implementation.

--- var/spack/packages/mvapich2/package.py
+++ var/spack/packages/mvapich2/package.py
@@ -11,8 +11,6 @@ class Mvapich2(Package):
 
     provides('mpi@:1', when='@1.9:')
 
-    patch('ad_lustre_rwcontig_open_source.patch', when='@1.9:')
-
     def install(self, spec, prefix):
         configure(
             "--prefix=" + prefix,
--- var/spack/packages/ompss/package.py
+++ var/spack/packages/ompss/package.py
@@ -1,11 +1,10 @@
 from spack import *
 import os
 import glob
-import subprocess
 
 # working config lines for ompss 14.06 :
-#./mcxx-1.99.2/config.log:	$ ./configure --prefix=/usr/gapps/exmatex/ompss --with-nanox=/usr/gapps/exmatex/ompss --enable-ompss --with-mpi=/opt/mvapich2-intel-shmem-1.7 --enable-tl-openmp-profile --enable-tl-openmp-intel
-#./nanox-0.7/config.log:	$ ./configure --prefix=/usr/gapps/exmatex/ompss --with-mcc=/usr/gapps/exmatex/ompss/ --with-hwloc=/usr
+#./nanox-0.7/config.log:    $ ./configure --prefix=/usr/gapps/exmatex/ompss --with-mcc=/usr/gapps/exmatex/ompss/ --with-hwloc=/usr
+#./mcxx-1.99.2/config.log:    $ ./configure --prefix=/usr/gapps/exmatex/ompss --with-nanox=/usr/gapps/exmatex/ompss --enable-ompss --with-mpi=/opt/mvapich2-intel-shmem-1.7 --enable-tl-openmp-profile --enable-tl-openmp-intel
 
 class Ompss(Package):
     homepage = "http://pm.bsc.es/"
@@ -15,7 +14,7 @@ class Ompss(Package):
     # all dependencies are optional, really
     depends_on("mpi")
     #depends_on("openmp")
-    #depends_on("hwloc")
+    depends_on("hwloc")
 
     def install(self, spec, prefix):
         if 'openmpi' in spec:
@@ -25,17 +24,17 @@ class Ompss(Package):
         elif 'mvapich' in spec:
             mpi = spec['mvapich']
 
+        openmp_options = ["--enable-tl-openmp-profile"]
+        if spec.satisfies('%intel'):
+            openmp_options.append( "--enable-tl-openmp-intel" )
+
         os.chdir(glob.glob('./nanox-*').pop())
-        #configure("-prefix=" + prefix + " -with-mcc=" + prefix + " -with-hwloc=" + spec['hwloc'].prefix)
-        #configure("-prefix=" + prefix, "-with-mcc=" + prefix)
-        subprocess.check_call(["configure", "-prefix=" + prefix, "-with-mcc=" + prefix])
+        configure("--prefix=%s" % prefix, "--with-mcc=%s" % prefix, "--with-hwloc=%s" % spec['hwloc'].prefix)
         make()
         make("install")
 
         os.chdir(glob.glob('../mcxx-*').pop())
-        #configure("-prefix=" + prefix + " -with-nanox=" + prefix + " -enable-ompss -with-mpi=" + mpi.prefix)
-        #configure("-prefix=" + prefix, "-with-nanox=" + prefix, "-enable-ompss", "-with-mpi=" + mpi.prefix)
-        subprocess.check_call(['configure', "-prefix=" + prefix, "-with-nanox=" + prefix, "-enable-ompss", "-with-mpi=" + mpi.prefix])
+        configure("--prefix=%s" % prefix, "--with-nanox=%s" % prefix, "--enable-ompss", "--with-mpi=%s" % mpi.prefix, *openmp_options)
         make()
         make("install")
 
