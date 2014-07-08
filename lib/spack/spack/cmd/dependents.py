10318a11dded5e69c3d9c98325613700c9b3db63
Todd Gamblin <tgamblin@llnl.gov>
2014-07-08 02:35:50 -0700

    Fix for dependent package detection.

---
+++ lib/spack/spack/cmd/dependents.py
@@ -0,0 +1,46 @@
+##############################################################################
+# Copyright (c) 2013, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Written by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://scalability-llnl.github.io/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License (as published by
+# the Free Software Foundation) version 2.1 dated February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+import argparse
+
+import llnl.util.tty as tty
+
+import spack
+import spack.cmd
+
+description = "Show dependent packages."
+
+def setup_parser(subparser):
+    subparser.add_argument(
+        'spec', nargs=argparse.REMAINDER, help="specs to list dependencies of.")
+
+
+def dependents(parser, args):
+    specs = spack.cmd.parse_specs(args.spec, concretize=True)
+    if len(specs) != 1:
+        tty.die("spack dependents takes only one spec.")
+
+    fmt = '$_$@$%@$+$=$#'
+    deps = [d.format(fmt) for d in specs[0].package.installed_dependents]
+    tty.msg("Dependents of %s" % specs[0].format(fmt), *deps)
