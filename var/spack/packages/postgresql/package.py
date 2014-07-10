0740c576a714281b0449e33f1b8e4b00e5d9d9c0
Todd Gamblin <tgamblin@llnl.gov>
2014-07-10 15:54:10 -0700

    Package for postgresql.

---
+++ var/spack/packages/postgresql/package.py
@@ -0,0 +1,24 @@
+from spack import *
+
+class Postgresql(Package):
+    """PostgreSQL is a powerful, open source object-relational
+       database system. It has more than 15 years of active
+       development and a proven architecture that has earned it a
+       strong reputation for reliability, data integrity, and
+       correctness."""
+    homepage = "http://www.postgresql.org/"
+    url      = "http://ftp.postgresql.org/pub/source/v9.3.4/postgresql-9.3.4.tar.bz2"
+
+    version('9.3.4', 'd0a41f54c377b2d2fab4a003b0dac762')
+
+    def install(self, spec, prefix):
+        # FIXME: Modify the configure line to suit your build system here.
+        configure("--prefix=%s" % prefix)
+
+        # FIXME: Add logic to build and install here
+        make()
+        make("install")
+
+
+    def dotkit(self, dk):
+        dk.setenv('PGDATA', "%s/data" %self.prefix)
