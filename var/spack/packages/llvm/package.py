8738a3a88c48fd9ed2a9c6182e71e94e6e2d5c3f
David Beckingsale <davidbeckingsale@gmail.com>
2014-07-31 15:37:35 -0700

    Added LLVM package

---
+++ var/spack/packages/llvm/package.py
@@ -0,0 +1,48 @@
+##############################################################################
+# Copyright (c) 2013, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Written by David Beckingsale, david@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://scalability-llnl.github.io/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License (as published by
+# the Free Software Foundation) version 2.1 dated February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+class Llvm(Package):
+    """The LLVM Project is a collection of modular and reusable compiler and
+       toolchain technologies. Despite its name, LLVM has little to do with
+       traditional virtual machines, though it does provide helpful libraries
+       that can be used to build them. The name "LLVM" itself is not an acronym;
+       it is the full name of the project.
+    """
+    homepage = "http://llvm.org/"
+    url      = "http://llvm.org/releases/3.4.2/llvm-3.4.2.src.tar.gz"
+
+    version('3.4.2', 'a20669f75967440de949ac3b1bad439c')
+
+    def install(self, spec, prefix):
+        configure("--prefix=%s" % prefix,
+            "--enable-optimized",
+            "--enable-debug-runtime",
+            "--enable-debug-symbols",
+            "--disable-assertions",
+            "REQUIRES_RTTI=1")
+
+        make()
+        make("install")
