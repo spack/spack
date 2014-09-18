250ffc28a3f94d039829cb96078a8242086e7dd5
Adam Moody <moody20@llnl.gov>
2014-09-17 23:40:16 -0700

    update libcircle to download tarball from github/hpc

--- var/spack/packages/libcircle/package.py
+++ var/spack/packages/libcircle/package.py
@@ -6,9 +6,9 @@ class Libcircle(Package):
        using self-stabilizing work stealing."""
 
     homepage = "https://github.com/hpc/libcircle"
-    url      = "https://github.com/adammoody/libcircle/releases/download/v0.2.1-rc.1/libcircle-0.2.1-rc.1.tar.gz"
 
-    version('0.2.1-rc.1', 'a10a14e76ac2ad7357a4b21b794e8e4e')
+    version('0.2.1-rc.1', '2b1369a5736457239f908abf88143ec2',
+             url='https://github.com/hpc/libcircle/releases/download/0.2.1-rc.1/libcircle-0.2.1-rc.1.tar.gz')
 
     depends_on('mpi')
 
