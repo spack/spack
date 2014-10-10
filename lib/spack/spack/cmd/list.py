8fd4d324087c6467e6aa68ae93bf7c83b43cec89
Todd Gamblin <tgamblin@llnl.gov>
2014-10-10 09:45:48 -0700

    Use external argparse in spack list, for 2.6 compatibility.

--- lib/spack/spack/cmd/list.py
+++ lib/spack/spack/cmd/list.py
@@ -23,8 +23,8 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 import sys
-import argparse
 import llnl.util.tty as tty
+from external import argparse
 from llnl.util.tty.colify import colify
 
 import spack
