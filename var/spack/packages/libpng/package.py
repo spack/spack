f3b6cf73f57a61618ecc1fd9f1e2c3fd3dbd0124
Alfredo Gimenez <gimenez1@llnl.gov>
2015-02-05 19:38:54 -0800

    libpng url was broken, fixed with update to 1.6.15

--- var/spack/packages/libpng/package.py
+++ var/spack/packages/libpng/package.py
@@ -3,9 +3,9 @@ from spack import *
 class Libpng(Package):
     """libpng graphics file format"""
     homepage = "http://www.libpng.org/pub/png/libpng.html"
-    url      = "http://sourceforge.net/projects/libpng/files/libpng16/1.6.14/libpng-1.6.14.tar.gz/download"
+    url      = "http://sourceforge.net/projects/libpng/files/libpng16/1.6.15/libpng-1.6.15.tar.gz/download"
 
-    version('1.6.14', '2101b3de1d5f348925990f9aa8405660')
+    version('1.6.15', '829a256f3de9307731d4f52dc071916d')
 
     def install(self, spec, prefix):
         configure("--prefix=%s" % prefix)
