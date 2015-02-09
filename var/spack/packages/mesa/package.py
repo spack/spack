9e878075aceace1439e85b1d75770a7a6f248566
Alfredo Gimenez <gimenez1@llnl.gov>
2015-02-08 16:09:13 -0800

    mesa 8.0.5 working

--- var/spack/packages/mesa/package.py
+++ var/spack/packages/mesa/package.py
@@ -5,19 +5,26 @@ class Mesa(Package):
     specification - a system for rendering interactive 3D graphics."""
 
     homepage = "http://www.mesa3d.org"
-    url      = "ftp://ftp.freedesktop.org/pub/mesa/older-versions/7.x/7.11.2/MesaLib-7.11.2.tar.gz"
+    url      = "ftp://ftp.freedesktop.org/pub/mesa/older-versions/8.x/8.0.5/MesaLib-8.0.5.tar.gz"
     # url      = "ftp://ftp.freedesktop.org/pub/mesa/10.4.4/MesaLib-10.4.4.tar.gz"
 
     # version('10.4.4', '8d863a3c209bf5116b2babfccccc68ce')
-    version('7.11.2', 'b9e84efee3931c0acbccd1bb5a860554')
+    version('8.0.5', 'cda5d101f43b8784fa60bdeaca4056f2')
 
-    depends_on("py-mako")
-    depends_on("flex")
-    depends_on("bison")
-    depends_on("libdrm")
-    depends_on("dri2proto")
-    depends_on("libxcb")
-    depends_on("libxshmfence")
+    # mesa 7.x, 8.x, 9.x 
+    depends_on("libdrm@2.4.33")
+    depends_on("llvm@3.0")
+
+    # patch("llvm-fixes.patch") # using newer llvm
+
+    # mesa 10.x
+    # depends_on("py-mako")
+    # depends_on("flex")
+    # depends_on("bison")
+    # depends_on("dri2proto")
+    # depends_on("libxcb")
+    # depends_on("libxshmfence")
+
 
     def install(self, spec, prefix):
         configure("--prefix=%s" % prefix)
