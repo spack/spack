14a79a2ddf022a09d65ba92d5450292ca6f79549
Todd Gamblin <tgamblin@llnl.gov>
2015-02-11 14:00:22 -0800

    Add OTF package.

---
+++ var/spack/packages/otf/package.py
@@ -0,0 +1,21 @@
+from spack import *
+
+class Otf(Package):
+    """To improve scalability for very large and massively parallel
+       traces the Open Trace Format (OTF) is developed at ZIH as a
+       successor format to the Vampir Trace Format (VTF3)."""
+
+    homepage = "http://tu-dresden.de/die_tu_dresden/zentrale_einrichtungen/zih/forschung/projekte/otf/index_html/document_view?set_language=en"
+    url      = "http://wwwpub.zih.tu-dresden.de/%7Emlieber/dcount/dcount.php?package=otf&get=OTF-1.12.5salmon.tar.gz"
+
+    version('1.12.5salmon', 'bf260198633277031330e3356dcb4eec')
+
+    depends_on('zlib')
+
+    def install(self, spec, prefix):
+        configure('--prefix=%s' % prefix,
+                  '--without-vtf3',
+                  '--with-zlib',
+                  '--with-zlibsymbols')
+        make()
+        make("install")
