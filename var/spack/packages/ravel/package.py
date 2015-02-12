81d518438c1de3fe4cb5ba4bed825e29cee30aee
Todd Gamblin <tgamblin@llnl.gov>
2015-02-12 09:18:54 -0800

    Point ravel download at the github release page.

--- var/spack/packages/ravel/package.py
+++ var/spack/packages/ravel/package.py
@@ -5,10 +5,11 @@ class Ravel(Package):
        orders events according to logical time."""
 
     homepage = "https://github.com/scalability-llnl/ravel"
+    url = 'https://github.com/scalability-llnl/ravel/archive/v1.0.0.tar.gz'
 
-    version('1.0.0', git="https://github.com/scalability-llnl/ravel.git",
-            branch='master')
+    version('1.0.0', 'b25fece58331c2adfcce76c5036485c2')
 
+    # TODO: make this a build dependency
     depends_on('cmake@2.8.9:')
 
     depends_on('muster@1.0.1:')
