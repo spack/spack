96db3a3144098a842c3586c7d3264370b38fcc6b
Saravan Pantham <saravan.pantham@gmail.com>
2015-04-06 14:07:04 -0700

    Fixed BLAS and Lapack installations.

--- var/spack/packages/blas/package.py
+++ var/spack/packages/blas/package.py
@@ -12,10 +12,6 @@ class Blas(Package):
 
     def install(self, spec, prefix):    
         make()
-        mv = which('mv') # Create a shell wrapper for the mv command.
-        mkdir = which('mkdir') # Create a shell wrapper for the mkdir command.
-        pwd = os.getcwd() # Retrieve the current working dir.
-        mkdir('%s' % prefix.lib) # Create the lib dir inside the install dir.
-        mv('%s/blas_LINUX.a' % pwd, '%s/libblas.a' % pwd) # Rename the generated lib file to libblas.a
-        mv('%s/libblas.a' % pwd, '%s/libblas.a' % prefix.lib) # Move the library file to the install dir.
+        mkdirp('%s' % prefix.lib) # Create the lib dir inside the install dir.
+        move('./blas_LINUX.a', '%s/libblas.a' % prefix.lib) # Rename the generated lib file to libblas.a
         
