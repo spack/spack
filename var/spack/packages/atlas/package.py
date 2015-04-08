26b9592780f09714b36a49424ee84698e1bb8ad5
Todd Gamblin <tgamblin@llnl.gov>
2015-04-07 22:32:00 -0700

    Add disablement of cpu throttle check.

--- var/spack/packages/atlas/package.py
+++ var/spack/packages/atlas/package.py
@@ -16,10 +16,22 @@ class Atlas(Package):
 
     version('3.10.2', 'a4e21f343dec8f22e7415e339f09f6da')
 
+    def patch(self):
+        # Disable thraed check.  LLNL's environment does not allow
+        # disabling of CPU throttling in a way that ATLAS actually
+        # understands.
+        filter_file(r'^\s+if \(thrchk\) exit\(1\);', 'if (0) exit(1);',
+                    'CONFIG/src/config.c')
+        # TODO: investigate a better way to add the check back in
+        # TODO: using, say, MSRs.  Or move this to a variant.
+
     def install(self, spec, prefix):
         with working_dir('ATLAS-Build', create=True):
             self.module.configure = Executable('../configure')
-            configure("--prefix=%s" % prefix)
+            configure('--prefix=%s' % prefix,
+                      '-C', 'ic', 'cc',
+                      '-C', 'if', 'f77',
+                      "--dylibs")
 
             make()
             make('check')
