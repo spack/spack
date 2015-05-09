8e87b2176ad762a75acfdab158ddd788648ee997
Todd Gamblin <tgamblin@llnl.gov>
2015-05-09 13:35:05 -0500

    Updated Sphinx RTD documentation theme supports 4 levels

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -87,7 +87,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -149,7 +149,7 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = []
+#html_theme_options = [('show_copyright', False)]
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["_themes"]
