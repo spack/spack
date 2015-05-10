6063977eaf6ba9055a5cc6497df31d4b9b78d1e3
Ben Walters <bwalter4@hawk.iit.edu>
2015-05-10 03:22:02 -0500

    Fixed wrf and added compilation of actual executable

--- var/spack/packages/wrf/package.py
+++ var/spack/packages/wrf/package.py
@@ -1,6 +1,7 @@
 from spack import *
 import subprocess
 from glob import glob
+import fileinput
 
 class Wrf(Package):
     """Description"""
@@ -13,10 +14,19 @@ class Wrf(Package):
     depends_on('ben-netcdf')
     depends_on('ben-netcdf-fortran')
     depends_on('jasper')
-    # depends_on('tcsh')
+    depends_on('tcsh')
 
     def install(self, spec, prefix):
+        first = True
+        for line in fileinput.input("compile", inplace=True):
+            if first:
+                first = False
+                print "#!%s/csh -f" % spec['tcsh'].prefix.bin,
+            else:
+                print line,
+        fileinput.close()
         process=subprocess.Popen(['./configure'], stdin=subprocess.PIPE)
         process.communicate(input='32\n')
         subprocess.call(['./compile', 'all_wrfvar'])
+        subprocess.call(['./compile', 'em_b_wave'])
         subprocess.call(['cp', '-r'] + glob('*') + [prefix])
