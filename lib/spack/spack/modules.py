d92ac2a6b2bf46fd8ad85ff8c0390fc2869f2e7c
Tom Scogland <tscogland@llnl.gov>
2015-06-11 13:51:31 -0700

    Small ruby enhancement and tmuxinator package
    It is currently less painful to pull the source from github, compile it into a
    gem, then install the gem, than it is to download a gem and install it.  This
    still lacks an activation mechanism, but `spack use tmuxinator` is functional.

--- lib/spack/spack/modules.py
+++ lib/spack/spack/modules.py
@@ -130,6 +130,13 @@ class EnvModule(object):
                 if site_packages:
                     add_path('PYTHONPATH', site_packages[0])
 
+            if self.spec.package.extends(spack.spec.Spec('ruby')):
+              add_path('GEM_PATH', self.spec.prefix)
+
+            # Allow module path extensions from packages
+            for key, val in self.spec.package.extra_environment:
+              add_path(key, val)
+
             # short description is just the package + version
             # TODO: maybe packages can optionally provide it.
             self.short_description = self.spec.format("$_ $@")
