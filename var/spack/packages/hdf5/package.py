cd8000664d9bb7b9c5674191ca291a0b85b49712
miller86 <miller86@llnl.gov>
2015-07-20 17:24:05 -0700

    Adding shared/static, debug/optimized, fortran variants, improved handling of zlib dependence. Fixed handling of mpiio support

--- var/spack/packages/hdf5/package.py
+++ var/spack/packages/hdf5/package.py
@@ -13,21 +13,60 @@ class Hdf5(Package):
 
     version('1.8.13', 'c03426e9e77d7766944654280b467289')
 
-    depends_on("mpi")
-    depends_on("zlib")
+    # global variants (eventually need to be handled across all packages)
+    variant('debug', default=False, description="Enable debugging")
+    variant('static', default=False, description="Build only static libs.")
+    variant('fortran', default=True, description="Enable Fortran interfaces.")
 
-    # TODO: currently hard-coded to use OpenMPI
+    # package specific variants
+    variant('cxx', default=True, description="Enable C++ interfaces.")
+    variant('mpiio', default=False, description="Enable MPI-IO support.")
+    # zlib should be on by default but that currently causes a key error
+#    variant('zlib', default=True, description="Enable zlib compression features.")
+    variant('zlib', default=False, description="Enable zlib compression features.")
+
+#    depends_on('mpi')
+    depends_on('zlib', when='+zlib')
+#    depends_on('zlib')
+
     def install(self, spec, prefix):
+        config_args = [
+            "CC=cc",
+            "CXX=c++",
+            "--prefix=%s" % prefix
+        ]
 
-        configure(
-            "--prefix=%s" % prefix,
-            "--with-zlib=%s" % spec['zlib'].prefix,
-            "--enable-parallel",
-            "--enable-shared",
-            "CC=%s" % spec['mpich'].prefix.bin + "/mpicc",
-            "CXX=%s" % spec['mpich'].prefix.bin + "/mpic++")
+        if '+zlib' in spec:
+            config_args += ["--with-zlib=%s" % spec['zlib'].prefix]
 
-        make()
+        if '+debug' in spec:
+            config_args += ["--enable-debug=all", "--enable-trace"]
+        else:
+            config_args += ["--enable-production"]
+
+        if '+static' in spec:
+            config_args += ["--disable-shared", "--enable-static"]
+        else:
+            config_args += ["--enable-shared", "--enable-static"]
+
+        # Note: use ~variant not -variant to test for negated variant in spec
+        if '+mpiio' in spec:
+            config_args += ["--enable-parallel"]
+        elif self.compiler.cc.startswith("mpi") and '~mpiio' not in spec:
+            config_args += ["--enable-parallel"]
+
+        # Enable fortran interface if we have a fortran compiler and
+        # fortran API isn't explicitly disabled
+        if self.compiler.fc and '~fortran' not in spec:
+            config_args += ["--enable-fortran", "--enable-fortran2003"]
+
+        # Enable C++ interface if we have a C++ compiler and
+        # C++ API isn't explicitly disabled
+        if self.compiler.cxx and '~cxx' not in spec:
+            config_args += ["--enable-cxx"]
+
+        configure(*config_args)
+
         make("install")
 
     def url_for_version(self, version):
