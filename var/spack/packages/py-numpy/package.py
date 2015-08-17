bd1cd83d7148a4856720cc7f29a1656a289fe92c
Gregory L. Lee <lee218@surface86.llnl.gov>
2015-08-17 11:54:14 -0700

    added lapack and blas deps, fixed gfortran compiler dependency

--- var/spack/packages/py-numpy/package.py
+++ var/spack/packages/py-numpy/package.py
@@ -7,9 +7,17 @@ class PyNumpy(Package):
 
     version('1.9.1', '78842b73560ec378142665e712ae4ad9')
     version('1.9.2', 'a1ed53432dbcd256398898d35bc8e645')
+    
+    patch('gnu_fcompiler.patch', when='%gcc')
 
     extends('python')
     depends_on('py-nose')
+    depends_on('netlib-blas+fpic')
+    depends_on('netlib-lapack+shared')
 
     def install(self, spec, prefix):
+        with open('site.cfg', 'w') as f:
+            f.write('[DEFAULT]\n')
+            f.write('libraries=lapack,blas\n')
+            f.write('library_dirs=%s/lib:%s/lib\n' % (spec['blas'].prefix, spec['lapack'].prefix))
         python('setup.py', 'install', '--prefix=%s' % prefix)
