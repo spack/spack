60429975259c508d651c6035ba149a04f0383f4f
Justin Too <justin@doubleotoo.com>
2015-09-07 01:32:19 +0000

    (Package) (TCL) Add v8.6.4; +threads variant

--- var/spack/packages/tcl/package.py
+++ var/spack/packages/tcl/package.py
@@ -9,14 +9,27 @@ class Tcl(Package):
        that is truly cross platform, easily deployed and highly
        extensible."""
     homepage = "http://www.tcl.tk"
+    
 
+    version('8.6.4', 'd7cbb91f1ded1919370a30edd1534304',
+            url = "http://prdownloads.sourceforge.net/tcl/tcl8.6.4-src.tar.gz")
     version('8.6.3', 'db382feca91754b7f93da16dc4cdad1f',
-            url="http://prdownloads.sourceforge.net/tcl/tcl8.6.3-src.tar.gz")
+            url = "http://prdownloads.sourceforge.net/tcl/tcl8.6.3-src.tar.gz")
+
+    variant('threads', default=False, description="enable threads")
 
     depends_on('zlib')
 
     def install(self, spec, prefix):
+        configure_args = [
+            "--prefix=%s" % prefix
+            ]
+
+        if '+threads' in spec:
+            configure_args.append('--enable-threads')
+
         with working_dir('unix'):
-            configure("--prefix=%s" % prefix)
+            configure(*configure_args)
             make()
             make("install")
+
