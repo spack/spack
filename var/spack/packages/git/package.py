214bed7407ad9c68aad3cfcc5e587548c7120d9a
Justin Too <justin@doubleotoo.com>
2015-09-07 22:48:33 +0000

    (Package) (Git) Enable Perl functionality
    Add Perl module dependencies to fix compilation failures:
    
      * Perl-ExtUtils-MakeMaker
      * Perl-ExtUtils-Install
    
    Remove --without-pcre configure option to enable Perl
    regular expressions.

--- var/spack/packages/git/package.py
+++ var/spack/packages/git/package.py
@@ -17,13 +17,11 @@ class Git(Package):
     # Use system openssl.
     # depends_on("openssl")
 
-    # Use system perl for now.
-    # depends_on("perl")
-    # depends_on("pcre")
-
     depends_on("zlib")
 
+    #-------------------------------------------------------------------------
     # Dependency - Perl::ExtUtils::MakeMaker
+    #-------------------------------------------------------------------------
     #
     # Required to address make error:
     #
@@ -31,18 +29,30 @@ class Git(Package):
     #        Can't locate ExtUtils/MakeMaker.pm in @INC ...
     #    make[1]: *** [perl.mak] Error 2
     #    make: *** [perl/perl.mak] Error 2
-    #    make: *** Waiting for unfinished jobs....
     depends_on("Perl-ExtUtils-MakeMaker")
+
+    #-------------------------------------------------------------------------
+    # Dependency - Perl::ExtUtils::Install
+    #-------------------------------------------------------------------------
+    #
+    # Required to address make error:
+    #
+    #   SUBDIR perl
+    #       Can't locate ExtUtils/Install.pm in @INC ...
+    #   make[2]: *** [pm_to_blib] Error 2
+    depends_on("Perl-ExtUtils-Install")
 
     def install(self, spec, prefix):
         # Add Perl::ExtUtils::MakeMaker to PERL5LIB
-        make_maker = spec['Perl-ExtUtils-MakeMaker']
+        # Add Perl::ExtUtils::Install to PERL5LIB
+        ExtUtils_MakeMaker = spec['Perl-ExtUtils-MakeMaker']
+        ExtUtils_Install = spec['Perl-ExtUtils-Install']
         os.environ['PERL5LIB'] = \
-            ''.join(glob(join_path(make_maker.prefix.lib, "site_perl/*"))) + \
-            ':' + os.getenv('PERL5LIB', '')
+            ''.join(glob(join_path(ExtUtils_MakeMaker.prefix.lib, "site_perl/*"))) + ':' + \
+            ''.join(glob(join_path(ExtUtils_Install.prefix.lib, "site_perl/*"))) + ':' +   \
+            os.getenv('PERL5LIB', '')
 
         configure("--prefix=%s" % prefix,
-                  "--without-pcre",
                   "--without-python")
 
         make()
