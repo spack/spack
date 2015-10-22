e6a44bd8ac40bfb93e51a99d3bfde476937207e1
Massimiliano Culpo <massimiliano.culpo@googlemail.com>
2015-10-22 15:55:43 +0200

    GCC : removed dependency on libelf. Removed isl variant

--- var/spack/packages/gcc/package.py
+++ var/spack/packages/gcc/package.py
@@ -36,6 +36,8 @@ class Gcc(Package):
     list_url = 'http://open-source-box.org/gcc/'
     list_depth = 2
 
+    DEPENDS_ON_ISL_PREDICATE = '@5.0:'
+
     version('5.2.0', 'a51bcfeb3da7dd4c623e27207ed43467')
     version('4.9.3', '6f831b4d251872736e8e9cc09746f327')
     version('4.9.2', '4df8ee253b7f3863ad0b86359cd39c43')
@@ -47,15 +49,14 @@ class Gcc(Package):
     version('4.5.4', '27e459c2566b8209ab064570e1b378f7')
     
     variant('binutils', default=False, description='Add a dependency on binutils')
-    
+
     depends_on("mpfr")
     depends_on("gmp")
     depends_on("mpc")     # when @4.5:
-    depends_on("libelf")
     depends_on("binutils~libiberty", when="+binutils")
 
     # Save these until we can do optional deps.
-    depends_on("isl", when='@5.0:')
+    depends_on("isl", when=DEPENDS_ON_ISL_PREDICATE)
     #depends_on("ppl")
     #depends_on("cloog")
 
@@ -79,11 +80,6 @@ class Gcc(Package):
                    "--with-gnu-ld",
                    "--with-gnu-as",
                    "--with-quad"]
-        # Libelf
-        if '+libelf' in spec:
-            libelf_options = ["--with-libelf=%s" % spec['libelf'].prefix]
-            options.extend(libelf_options)
-
         # Binutils
         if '+binutils' in spec:
             binutils_options = ["--with-stage1-ldflags=%s" % self.rpath_args,
@@ -93,7 +89,7 @@ class Gcc(Package):
             options.extend(binutils_options)
             
         # Isl
-        if spec.satisfies('@5.0:+isl'):
+        if spec.satisfies(Gcc.DEPENDS_ON_ISL_PREDICATE):
             isl_options = ["--with-isl=%s" % spec['isl'].prefix]
             options.extend(isl_options)
 
