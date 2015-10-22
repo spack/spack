caf93cc4080703b80d5c622370b8094c37bf014f
Kevin Huck <khuck@cs.uoregon.edu>
2015-10-22 08:03:50 -0700

    Adding package for OMPT support in the LLVM/Clang OpenMP runtime, which gives OMPT support for applications built with GCC, Intel or LLVM/Clang.

---
+++ var/spack/packages/ompt-openmp/package.py
@@ -0,0 +1,23 @@
+from spack import *
+
+class OmptOpenmp(Package):
+    """LLVM/Clang OpenMP runtime with OMPT support. This is a fork of the OpenMPToolsInterface/LLVM-openmp fork of the official LLVM OpenMP mirror.  This library provides a drop-in replacement of the OpenMP runtimes for GCC, Intel and LLVM/Clang."""
+    homepage = "https://github.com/OpenMPToolsInterface/LLVM-openmp"
+    url      = "http://github.com/khuck/LLVM-openmp/archive/v0.1-spack.tar.gz"
+
+    version('spack', '35227b2726e377faa433fc841226e036')
+
+    # depends_on("foo")
+
+    def install(self, spec, prefix):
+        with working_dir("runtime/build", create=True):
+
+            # FIXME: Modify the configure line to suit your build system here.
+            cmake('-DCMAKE_C_COMPILER=%s' % self.compiler.cc, 
+                  '-DCMAKE_CXX_COMPILER=%s' % self.compiler.cxx,
+                  '-DCMAKE_INSTALL_PREFIX=%s' % prefix,
+                  '..', *std_cmake_args)
+
+            # FIXME: Add logic to build and install here
+            make()
+            make("install")
