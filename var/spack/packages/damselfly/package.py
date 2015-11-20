4edcc59c3dc6f5c5121e0b453f39ab90ec5af3ca
Abhinav Bhatele <bhatele@llnl.gov>
2015-11-19 23:05:21 -0800

    Add damselfly package

---
+++ var/spack/packages/damselfly/package.py
@@ -0,0 +1,14 @@
+from spack import *
+
+class Damselfly(Package):
+    """Damselfly is a model-based parallel network simulator."""
+    homepage = "https://github.com/scalability-llnl/damselfly"
+    url      = "https://github.com/scalability-llnl/damselfly"
+
+    version('1.0', '05cf7e2d8ece4408c0f2abb7ab63fd74c0d62895', git='https://github.com/scalability-llnl/damselfly.git', tag='v1.0')
+
+    def install(self, spec, prefix):
+        with working_dir('spack-build', create=True):
+	    cmake('-DCMAKE_BUILD_TYPE=release', '..', *std_cmake_args)
+	    make()
+	    make('install')
