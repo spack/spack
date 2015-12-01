89fbe4fdfa01b2461a5355a8922e6083f528e409
Mario Melara <maamelara@gmail.com>
2015-12-01 11:55:38 -0800

    Changed add_target to include variables rather than hard-coded strings

--- lib/spack/spack/architectures/bgq.py
+++ lib/spack/spack/architectures/bgq.py
@@ -10,8 +10,8 @@ class Bgq(Architecture):
 
     def __init__(self):
         super(Bgq, self).__init__('cray')
-        self.add_target('power7', Target('power7'))
-        self.add_target('powerpc', Target('powerpc'))
+        self.add_target(self.front_end, Target(self.front_end))
+        self.add_target(self.back_end, Target(self.back_end))
 
     @classmethod
     def detect(self):
