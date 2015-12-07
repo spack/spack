36272c813675c98864e7b9eda50e99ea800dfe21
David Beckingsale <davidbeckingsale@gmail.com>
2015-12-07 14:58:16 -0800

    Fix MPI selection in SAMRAI and remove narrow dependency specification

--- var/spack/packages/cleverleaf/package.py
+++ var/spack/packages/cleverleaf/package.py
@@ -13,7 +13,7 @@ class Cleverleaf(Package):
 
     version('develop', git='https://github.com/UK-MAC/CleverLeaf_ref.git', branch='develop')
 
-    depends_on("SAMRAI@3.8.0")
+    depends_on("SAMRAI@3.8.0:")
     depends_on("hdf5")
     depends_on("boost")
 
