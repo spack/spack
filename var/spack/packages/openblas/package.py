9e520d73894f040fe502c6062a7c2883c073513b
Erik Schnetter <schnetter@gmail.com>
2015-12-25 13:56:15 -0500

    Add ilp64 variant of OpenBLAS
    Standard BLAS routines expect 32-bit integers, which is insufficient on some modern 64-bit systems with large amounts of memory. This variant builds a 64-bit version of OpenBLAS as well, which expects 64-bit integers (and adds a suffix "64_" to function names).
    
    This variant requires a tool "objconv" that is also packaged.

--- var/spack/packages/openblas/package.py
+++ var/spack/packages/openblas/package.py
@@ -7,13 +7,27 @@ class Openblas(Package):
 
     version('0.2.15', 'b1190f3d3471685f17cfd1ec1d252ac9')
 
+    variant('ilp64', default=False, description="Also support 64-bit indices (i.e. using int64_t or integer*8 to describe array sizes)")
+
+    depends_on('objconv', '+ilp64')
+
     # virtual dependency
     provides('blas')
     provides('lapack')
 
     def install(self, spec, prefix):
+        make('clean')
         make('libs', 'netlib', 'shared', 'CC=cc', 'FC=f77')
-        make('install', "PREFIX='%s'" % prefix)
+        make('install', "PREFIX=%s" % prefix)
+
+        if spec.satisfies("+ilp64"):
+            # Using "64_" as suffix looks a bit strange, but has been set as
+            # standard by Julia, and has been picked up by Fedora
+            make('clean')
+            make('libs', 'netlib', 'shared', 'CC=cc', 'FC=f77',
+                'INTERFACE64=1', 'SYMBOLSUFFIX=64_')
+            make('install', "PREFIX=%s" % prefix,
+                'INTERFACE64=1', 'SYMBOLSUFFIX=64_')
 
         # Blas virtual package should provide blas.a and libblas.a
         with working_dir(prefix.lib):
