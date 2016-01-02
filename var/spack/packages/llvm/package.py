356836ccbb6a187588ea6454bf1b7f7a679c5706
Tom Scogland <scogland1@llnl.gov>
2016-01-01 22:30:02 -0800

    removed extraneous comment lines

--- var/spack/packages/llvm/package.py
+++ var/spack/packages/llvm/package.py
@@ -206,8 +206,6 @@ class Llvm(Package):
                 raise SpackException('The lldb variant requires the clang variant to be selected')
 
         with working_dir('spack-build', create=True):
-            # cmake(*cmake_args)
-            # make()
             make("install")
             query_path = os.path.join('bin', 'clang-query')
             # Manually install clang-query, because llvm doesn't...
