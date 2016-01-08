94fc417363ede4f1b8deb27c353b5438df42f790
Gregory Becker <becker33.llnl.gov>
2016-01-08 11:48:59 -0800

    changing problematic package.py file permissions back to 0644

--- var/spack/packages/adios/package.py
+++ var/spack/packages/adios/package.py
@@ -18,12 +18,13 @@ class Adios(Package):
     # module load cray-netcdf/4.3.3.1
     # module load cray-hdf5/1.8.14
     # module load python/2.7.10
+    depends_on('hdf5')
     depends_on('mxml')
-    
+
     def install(self, spec, prefix):
         configure_args = ["--prefix=%s" % prefix, 
                           "--with-mxml=%s" % spec['mxml'].prefix, 
-                          "--with-hdf5=%s" % os.environ["HDF5_DIR"],
+                          "--with-hdf5=%s" % spec['hdf5'].prefix,
                           "--with-netcdf=%s" % os.environ["NETCDF_DIR"],
                           "--with-infiniband=no",
                           "MPICC=cc","MPICXX=CC","MPIFC=ftn",
