b9d268fc19811f9763f87d14a636bf8b3e43a72e
alalazo <massimiliano.culpo@googlemail.com>
2016-01-25 16:49:52 +0100

    modified netlib stack to forward variants

--- var/spack/repos/builtin/packages/netlib-lapack/package.py
+++ var/spack/repos/builtin/packages/netlib-lapack/package.py
@@ -1,4 +1,29 @@
+##############################################################################
+# Copyright (c) 2013, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Written by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License (as published by
+# the Free Software Foundation) version 2.1 dated February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
 from spack import *
+
 
 class NetlibLapack(Package):
     """
@@ -10,50 +35,54 @@ class NetlibLapack(Package):
     use in the scientific community.
     """
     homepage = "http://www.netlib.org/lapack/"
-    url      = "http://www.netlib.org/lapack/lapack-3.5.0.tgz"
+    url = "http://www.netlib.org/lapack/lapack-3.5.0.tgz"
 
+    version('3.6.0', 'f2f6c67134e851fe189bb3ca1fbb5101')
     version('3.5.0', 'b1d3e3e425b2e44a06760ff173104bdf')
     version('3.4.2', '61bf1a8a4469d4bdb7604f5897179478')
     version('3.4.1', '44c3869c38c8335c2b9c2a8bb276eb55')
     version('3.4.0', '02d5706ec03ba885fc246e5fa10d8c70')
     version('3.3.1', 'd0d533ec9a5b74933c2a1e84eedc58b4')
 
-    variant('shared', default=False, description="Build shared library version")
+    variant('debug', default=False, description='Builds the library in debug mode')
+    variant('shared', default=True, description='Builds the shared library version')
 
     # virtual dependency
     provides('lapack')
 
     # blas is a virtual dependency.
-    depends_on('blas', forward='shared')
+    depends_on('blas', forward=('debug', 'shared'))
 
     depends_on('cmake')
 
     # Doesn't always build correctly in parallel
     parallel = False
 
-    @when('^netlib-blas')
-    def get_blas_libs(self):
-        blas = self.spec['netlib-blas']
-        return [join_path(blas.prefix.lib, 'blas.a')]
-
-
-    @when('^atlas')
-    def get_blas_libs(self):
-        blas = self.spec['atlas']
-        return [join_path(blas.prefix.lib, l)
-                for l in ('libf77blas.a', 'libatlas.a')]
-
-
     def install(self, spec, prefix):
-        blas_libs = ";".join(self.get_blas_libs())
-        cmake_args = [".", '-DBLAS_LIBRARIES=' + blas_libs]
+        cmake_args = []
+        cmake_args.extend(std_cmake_args)  # Put them first, so that they can be overridden
 
+        cmake_args.append('-DUSE_OPTIMIZED_BLAS:BOOL=ON')
+        cmake_args.append('-DBLAS_LIBRARIES:PATH={blas_libraries}'.format(
+                blas_libraries=join_path(spec['blas'].prefix.lib, 'libblas.a'))
+        )
         if '+shared' in spec:
-            cmake_args.append('-DBUILD_SHARED_LIBS=ON')
+            cmake_args.extend([
+                '-DBUILD_SHARED_LIBS:BOOL=ON',
+                '-DBUILD_STATIC_LIBS:BOOL=OFF'
+            ])
+        else:
+            cmake_args.extend([
+                '-DBUILD_SHARED_LIBS:BOOL=OFF',
+                '-DBUILD_STATIC_LIBS:BOOL=ON'
+            ])
 
-        cmake_args += std_cmake_args
+        if '+debug' in spec:
+            cmake_args.append('-DCMAKE_BUILD_TYPE:STRING=Debug')
+        else:
+            cmake_args.append('-DCMAKE_BUILD_TYPE:STRING=Release')
 
-        cmake(*cmake_args)
-        make()
-        make("install")
-
+        with working_dir('spack-build', create=True):
+            cmake('..', *cmake_args)
+            make()
+            make("install")
--- var/spack/repos/builtin/packages/netlib-scalapack/package.py
+++ var/spack/repos/builtin/packages/netlib-scalapack/package.py
@@ -1,40 +1,74 @@
+##############################################################################
+# Copyright (c) 2013, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Written by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License (as published by
+# the Free Software Foundation) version 2.1 dated February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
 from spack import *
+
 
 class NetlibScalapack(Package):
     """ScaLAPACK is a library of high-performance linear algebra routines for parallel distributed memory machines"""
-    
+
     homepage = "http://www.netlib.org/scalapack/"
-    url      = "http://www.netlib.org/scalapack/scalapack-2.0.2.tgz"
+    url = "http://www.netlib.org/scalapack/scalapack-2.0.2.tgz"
 
     version('2.0.2', '2f75e600a2ba155ed9ce974a1c4b536f')
     version('2.0.1', '17b8cde589ea0423afe1ec43e7499161')
     version('2.0.0', '9e76ae7b291be27faaad47cfc256cbfe')
     # versions before 2.0.0 are not using cmake and requires blacs as
     # a separated package
-    
+
+    variant('debug', default=False, description='Builds the library in debug mode')
     variant('shared', default=True, description='Build the shared library version')
+
     variant('fpic', default=False, description="Build with -fpic compiler option")
-    
+
     provides('scalapack')
-    
+
     depends_on('mpi')
-    depends_on('lapack', forward='shared')
-    
-    def install(self, spec, prefix):       
-        options = [
-            "-DBUILD_SHARED_LIBS:BOOL=%s" % 'ON' if '+shared' in spec else 'OFF',
-            "-DBUILD_STATIC_LIBS:BOOL=%s" % 'OFF' if '+shared' in spec else 'ON',
-            "-DUSE_OPTIMIZED_LAPACK_BLAS:BOOL=ON", # forces scalapack to use find_package(LAPACK)
-            ]
+    depends_on('lapack', forward=('shared', 'debug'))
+
+    def install(self, spec, prefix):
+
+        options = []
+        options.extend(std_cmake_args)
+
+        options.extend([
+            "-DBUILD_SHARED_LIBS:BOOL=%s" % ('ON' if '+shared' in spec else 'OFF'),
+            "-DBUILD_STATIC_LIBS:BOOL=%s" % ('OFF' if '+shared' in spec else 'ON')
+        ])
+        options.append('-DUSE_OPTIMIZED_LAPACK_BLAS:BOOL=ON')  # forces scalapack to use find_package(LAPACK)
+
+        if '+debug' in spec:
+            options.append('-DCMAKE_BUILD_TYPE:STRING=Debug')
+        else:
+            options.append('-DCMAKE_BUILD_TYPE:STRING=Release')
 
         if '+fpic' in spec:
             options.extend([
                 "-DCMAKE_C_FLAGS=-fPIC",
                 "-DCMAKE_Fortran_FLAGS=-fPIC"
             ])
-           
-        options.extend(std_cmake_args)
-        
+
         with working_dir('spack-build', create=True):
             cmake('..', *options)
             make()
