23af31cb1c773cd5a01f93a4896d794ddf908bae
Tom Scogland <scogland1@llnl.gov>
2016-01-26 17:55:32 -0800

    adding the cmocka unit testing library

---
+++ var/spack/repos/builtin/packages/cmocka/package.py
@@ -0,0 +1,16 @@
+from spack import *
+
+class Cmocka(Package):
+    """Unit-testing framework in pure C"""
+    homepage = "https://cmocka.org/"
+    url      = "https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz"
+
+    version('1.0.1', 'ed861e501a21a92b2af63e466df2015e')
+    parallel = False
+
+    def install(self, spec, prefix):
+        with working_dir('spack-build', create=True):
+	    cmake('..', *std_cmake_args)
+
+	    make()
+	    make("install")
