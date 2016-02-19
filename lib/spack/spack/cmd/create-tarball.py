90fcba0bfef8aa70031f13cec897534d5e44f5c1
Brett Viren <bv@bnl.gov>
2016-02-19 16:25:02 -0500

    Add recursion and force-overwrite to create-tarball.

--- lib/spack/spack/cmd/create-tarball.py
+++ lib/spack/spack/cmd/create-tarball.py
@@ -35,20 +35,44 @@ from spack.binary_distribution import tarball_name
 description = "Create tarballs for given packages"
 
 def setup_parser(subparser):
+    subparser.add_argument('-r','--recurse',action='store_true',
+                           help="also make tarballs for dependencies.")
+    subparser.add_argument('-f','--force',action='store_true',
+                           help="overwrite tarball if it exists.")
+
     subparser.add_argument(
         'packages', nargs=argparse.REMAINDER, help="specs of packages to package")
 
 
+def flatten_to_specs(package_names, recurse = False):
+    ret = set()
+    for pkgname in package_names:
+        specs = spack.cmd.parse_specs(pkgname, concretize=True)
+        for spec in specs:
+            ret.add(spec)
+            if recurse and spec.dependencies:
+                ret.update(flatten_to_specs(spec.dependencies))
+    return ret
+
+
+def do_tar(spec, force=False):
+    tarfile = tarball_name(spec)
+    if os.path.exists(tarfile):
+        if force:
+            os.remove(tarfile)
+        else:
+            tty.die("file exists, use -f to force overwrite: %s"%tarfile)
+
+    tar = which('tar', required=True)
+    dirname = os.path.dirname(spec.prefix)
+    basename = os.path.basename(spec.prefix)
+    tar("--directory=%s" %dirname, "-cf", tarfile, basename)
+    tty.msg(tarfile)
+
 def create_tarball(parser, args):
     if not args.packages:
         tty.die("tarball creation requires at least one package argument")
-
-    specs = spack.cmd.parse_specs(args.packages, concretize=True)
+    specs = flatten_to_specs(args.packages, args.recurse)
     for spec in specs:
-        package = spack.repo.get(spec)
-        tar = which('tar', required=True)
-        tarfile = tarball_name(spec)
-        dirname = os.path.dirname(package.prefix)
-        basename = os.path.basename(package.prefix)
-        tar("--directory=%s" %dirname, "-cvf", tarfile, basename)
+        do_tar(spec, args.force)
 
