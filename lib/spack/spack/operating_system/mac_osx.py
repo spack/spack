2de81cfc62ca456b1f303d64198590eb003fdcd3
Mario Melara <maamelara@gmail.com>
2016-02-24 15:31:21 -0800

    Changed name to appropriate camelcase

--- lib/spack/spack/operating_system/mac_osx.py
+++ lib/spack/spack/operating_system/mac_osx.py
@@ -3,38 +3,30 @@
     will be represented using the major version operating system name, i.e
     el capitan, yosemite...etc.
 """
-
-import spack
-import os
 import platform as py_platform
-from spack.architecture import Platform, OperatingSystem
+import spack
+from spack.architecture import OperatingSystem
 
-class MacOSX(OperatingSystem):
+class MacOsx(OperatingSystem):
     def __init__(self):
         """ Autodetects the mac version from a dictionary. Goes back as
             far as 10.6 snowleopard. If the user has an older mac then
             the version will just be a generic mac_os.
         """
-        
-        def get_mac_release():
-            mac_releases = {'10.6': "snowleopard", 
-                            "10.7": "lion",
-                            "10.8": "mountainlion",
-                            "10.9": "mavericks",
-                            "10.10": "yosemite",
-                            "10.11": "elcapitan"}     
+        mac_releases = {'10.6': "snowleopard", 
+                        "10.7": "lion",
+                        "10.8": "mountainlion",
+                        "10.9": "mavericks",
+                        "10.10": "yosemite",
+                        "10.11": "elcapitan"}     
 
-            mac_ver = py_platform.mac_ver()[0][:-2]
-            try:
-                name = mac_releases[mac_ver]
-                return name, mac_ver
-            except KeyError:
-                name = "mac_os"
-                return name, mac_ver
+        mac_ver = py_platform.mac_ver()[0][:-2]
+        try:
+            name = mac_releases[mac_ver]
+        except KeyError:
+            name = "mac_os"
 
-        name, version = get_mac_release()
-
-        super(MacOSX, self).__init__(name, version, "PATH")
+        super(MacOsx, self).__init__(name, mac_ver, "PATH")
 
     def compiler_strategy(self):
         return self.compiler_strategy
