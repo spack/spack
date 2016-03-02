ffb9574312e8b6368c7696c2eadd17e46ec9a400
Gregory Becker <becker33@llnl.gov>
2016-03-02 15:59:23 -0800

    fixed type/bug

--- lib/spack/spack/compilers/__init__.py
+++ lib/spack/spack/compilers/__init__.py
@@ -225,7 +225,7 @@ def compiler_for_spec(compiler_spec, operating_system):
        be concrete."""
     assert(compiler_spec.concrete)
     compilers = [c for c in compilers_for_spec(compiler_spec)
-                if c.strategy == operating_system.strategy]
+                if c.strategy == operating_system.compiler_strategy]
     if len(compilers) < 1:
         raise NoCompilerForSpecError(compiler_spec, target)
     if len(compilers) > 1:
