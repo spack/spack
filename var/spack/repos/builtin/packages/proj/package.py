976d0240c467b9ff74333fafcd2ac1e896819a80
citibeth <rpf2116@columbia.edu>
2016-03-02 18:58:47 -0500

    Removed FIXMEs.

--- var/spack/repos/builtin/packages/proj/package.py
+++ var/spack/repos/builtin/packages/proj/package.py
@@ -1,19 +1,3 @@
-# FIXME:
-# This is a template package file for Spack.  We've conveniently
-# put "FIXME" labels next to all the things you'll want to change.
-#
-# Once you've edited all the FIXME's, delete this whole message,
-# save this file, and test out your package like this:
-#
-#     spack install proj
-#
-# You can always get back here to change things with:
-#
-#     spack edit proj
-#
-# See the spack documentation for more information on building
-# packages.
-#
 from spack import *
 
 class Proj(Package):
@@ -27,8 +11,7 @@ class Proj(Package):
     version('4.7.0', '927d34623b52e0209ba2bfcca18fe8cd')
     version('4.6.1', '7dbaab8431ad50c25669fd3fb28dc493')
 
-    # FIXME: Add dependencies if this package requires them.
-    # depends_on("foo")
+    # No dependencies
 
     def install(self, spec, prefix):
         configure('--prefix=%s' % prefix)
