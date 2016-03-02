a22b6634dccdf6e408f73ae01fe3b617d628d435
Elizabeth F <rpf2116@columbia.edu>
2016-03-01 23:15:27 -0500

    New CMakePackage subclass of Package.  This has two advantages, without breaking backwards compatibility:
    1. Boilerplate code in CMake projects can now be eliminated (see everytrace/package.py for details on how it can be done now).
    
    2. The CMakeProject understands a special version 'local', to enlist Spack to help configure CMake properly when developing projects.  Consider the following example for usage, in which Spack is used to set up a build and a module, but not acutally DO the build.
    
         git clone https://github.com/citibeth/everytrace.git
         cd everytrace
         spack diy --skip-patch everytrace@local
         mkdir build
         cd build
         /usr/bin/python ../spconfig.py ..     # Runs cmake with Spack-supplied configuration
         make
         make install        # Installs into spack directory
         spack load everytrace@local   # Spack even makes a module
    
    Once you're happy with your project, you can add appropriate version() commands to your package.py, and use Spack normally with it.  To make this work, your project has to cooperate with Spack, as follows:
    
      a) You need to use CMake (of course).
    
      b) Your CMakeLists.txt should use the following line, which will ensure that all TRANSITIVE dependencies are added to the include path.  If you're not running with Spack, then this line will do nothing.
           include_directories($ENV{CMAKE_TRANSITIVE_INCLUDE_PATH})

---
+++ var/spack/repos/builtin/packages/everytrace/package.py
@@ -0,0 +1,25 @@
+# Install with:
+#	   spack diy --skip-patch everytrace@devel +fortran +mpi ^openmpi
+
+from spack import *
+import llnl.util.tty as tty
+import os
+
+class Everytrace(CMakePackage):
+	"""Get stack trace EVERY time a program exits."""
+
+	homepage = "https://github.com/citibeth/everytrace"
+	url		 = "https://github.com/citibeth/everytrace/tarball/dev"
+
+	version('devel', '0123456789abcdef0123456789abcdef')
+
+	variant('mpi', default=True, description='Enables MPI parallelism')
+	variant('fortran', default=True, description='Enable use with Fortran programs')
+
+	depends_on('mpi', when='+mpi')
+	depends_on('cmake')
+
+	def config_args(self, spec, prefix):
+		return [
+			'-DUSE_MPI=%s' % 'YES' if '+mpi' in spec else 'NO',
+			'-DUSE_FORTRAN=%s' % 'YES' if '+fortran' in spec else 'NO']
