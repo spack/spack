8c32ac4059cbc7b8b8b5af5da10fc06213a2ec34
Ben Boeckel <ben.boeckel@kitware.com>
2016-03-03 13:03:14 -0500

    py-numpy: add fortran feature

--- var/spack/repos/builtin/packages/py-numpy/package.py
+++ var/spack/repos/builtin/packages/py-numpy/package.py
@@ -9,6 +9,7 @@ class PyNumpy(Package):
     version('1.9.2', 'a1ed53432dbcd256398898d35bc8e645')
 
     variant('blas', default=True)
+    variant('fortran', default=True)
 
     extends('python')
     depends_on('py-nose')
@@ -17,9 +18,14 @@ class PyNumpy(Package):
     depends_on('netlib-lapack+shared', when='+blas')
 
     def install(self, spec, prefix):
+        args = []
         if '+blas' in spec:
             with open('site.cfg', 'w') as f:
                 f.write('[DEFAULT]\n')
                 f.write('libraries=lapack,blas\n')
                 f.write('library_dirs=%s/lib:%s/lib\n' % (spec['blas'].prefix, spec['lapack'].prefix))
-        python('setup.py', 'install', '--prefix=%s' % prefix)
+        if '+fortran' not in spec:
+            # Numpy's build system complains that 'no' is not a compiler, but
+            # it is not fatal; other values are not fatal.
+            args.append('--fcompiler=no')
+        python('setup.py', 'install', '--prefix=%s' % prefix, *args)
