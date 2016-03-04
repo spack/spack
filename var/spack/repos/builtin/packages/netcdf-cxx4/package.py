951362c6f6ffc8c192dbd6f4b2cef8c01c3e3d9a
Elizabeth F <rpf2116@columbia.edu>
2016-03-04 11:33:27 -0500

    Temporary package for use with nonstandard versions of netcdf-cxx4.

--- var/spack/repos/builtin/packages/netcdf-cxx4/package.py
+++ var/spack/repos/builtin/packages/netcdf-cxx4/package.py
@@ -3,20 +3,28 @@ from spack import *
 class NetcdfCxx4(Package):
     """C++ interface for NetCDF4"""
     homepage = "http://www.unidata.ucar.edu/downloads/netcdf/netcdf-cxx/index.jsp"
-    url      = "http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx4-4.2.tar.gz"
+#    url      = "http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx4-4.2.tar.gz"
+    url      = "https://github.com/unidata/netcdf-cxx4/tarball/123"
 
-    version('4.2', 'd019853802092cf686254aaba165fc81')
+    version('ecdf914', 'afca0c0aeb5c0863c5153c08ad0af534')
+    version('4.2.1', 'd019853802092cf686254aaba165fc81')
 
 
     variant('mpi', default=True, description='Enables MPI parallelism')
+#    variant('hdf4',    default=False, description="Enable HDF4 support")
 
-    # netcdf-cxx4 doesn't really depend (directly) on MPI.  However... this
+    # NetCDF-CXX4 doesn't really depend (directly) on MPI.  However... this
     # depndency ensures taht the right version of MPI is selected (eg: ^openmpi)
     depends_on('mpi', when='+mpi')
     depends_on('netcdf')
 
-    def install(self, spec, prefix):
-        configure('--prefix=%s' % prefix)
+    # Build dependency
+    depends_on('cmake')
 
-        make()
-        make("install")
+    def install(self, spec, prefix):
+        build_directory = join_path(self.stage.path, 'spack-build')
+        source_directory = self.stage.source_path
+        with working_dir(build_directory, create=True):
+            cmake(source_directory, *std_cmake_args)
+            make()
+            make("install")
