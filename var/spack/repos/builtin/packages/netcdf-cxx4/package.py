0eb6ef2cd031d1668e1891425bdc6234df0594df
Elizabeth F <rpf2116@columbia.edu>
2016-03-04 11:09:40 -0500

    1. Removed fake MPI dependency from netcdf-cxx4 and netcdf-fortran.
    2. Removed Fortran bootstrap variant from netcdf.  Users who need NetCDF Fortran interface should install netcdf-fortran.
    
    3. Added result of ./configure --help on the netcdf-fortran.  Verified  the package has no additional options that should be exposed through Spack.

--- var/spack/repos/builtin/packages/netcdf-cxx4/package.py
+++ var/spack/repos/builtin/packages/netcdf-cxx4/package.py
@@ -7,12 +7,6 @@ class NetcdfCxx4(Package):
 
     version('4.2', 'd019853802092cf686254aaba165fc81')
 
-
-    variant('mpi', default=True, description='Enables MPI parallelism')
-
-    # netcdf-cxx4 doesn't really depend (directly) on MPI.  However... this
-    # depndency ensures taht the right version of MPI is selected (eg: ^openmpi)
-    depends_on('mpi', when='+mpi')
     depends_on('netcdf')
 
     def install(self, spec, prefix):
