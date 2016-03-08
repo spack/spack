e0e545774aaddb5386367bd900a31b85692abc51
Adam J. Stewart <ajstewart426@gmail.com>
2016-03-08 12:15:40 -0600

    Add more versions to Autotools

--- var/spack/repos/builtin/packages/automake/package.py
+++ var/spack/repos/builtin/packages/automake/package.py
@@ -5,7 +5,9 @@ class Automake(Package):
     homepage = "http://www.gnu.org/software/automake/"
     url      = "http://ftp.gnu.org/gnu/automake/automake-1.14.tar.gz"
 
+    version('1.15',   '716946a105ca228ab545fc37a70df3a3')
     version('1.14.1', 'd052a3e884631b9c7892f2efce542d75')
+    version('1.11.6', '0286dc30295b62985ca51919202ecfcc')
 
     depends_on('autoconf')
 
