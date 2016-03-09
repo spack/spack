c67b922185ebf5b7686d5b4958e2b31b8f0f1469
Elizabeth F <rpf2116@columbia.edu>
2016-03-09 00:34:08 -0500

    Added shared library capability.

--- var/spack/repos/builtin/packages/petsc/package.py
+++ var/spack/repos/builtin/packages/petsc/package.py
@@ -11,8 +11,11 @@ class Petsc(Package):
     version('3.5.3', 'd4fd2734661e89f18ac6014b5dd1ef2f')
     version('3.5.2', 'ad170802b3b058b5deb9cd1f968e7e13')
     version('3.5.1', 'a557e029711ebf425544e117ffa44d8f')
+    version('3.4.4', '7edbc68aa6d8d6a3295dd5f6c2f6979d')
 
-    depends_on("python @2.6:2.9")   # requires Python for building
+    variant('shared', default=True, description="Build shared library version")
+
+    depends_on("python @2.6:2.7")   # requires Python for building
 
     depends_on("boost")
     depends_on("blas")
@@ -33,7 +36,7 @@ class Petsc(Package):
                   "--with-metis-dir=%s"              % spec['metis'].prefix,
                   "--with-hdf5-dir=%s"               % spec['hdf5'].prefix,
                   "--with-mpi-dir=%s"                % spec['mpi'].prefix,
-                  "--with-shared-libraries=0")
+                  "--with-shared-libraries=%d"       % (1 if '+shared' in self.spec else 0))
 
         # PETSc has its own way of doing parallel make.
         make('MAKE_NP=%s' % make_jobs, parallel=False)
