f07d4c94393f6541d1a6afd9118d3f9c84bd08fc
Kelly (KT) Thompson <kgt@lanl.gov>
2016-03-16 18:55:14 -0600

    + Provide ~perl and ~shared variants.

--- var/spack/repos/builtin/packages/graphviz/package.py
+++ var/spack/repos/builtin/packages/graphviz/package.py
@@ -7,6 +7,9 @@ class Graphviz(Package):
 
     version('2.38.0', '5b6a829b2ac94efcd5fa3c223ed6d3ae')
 
+    variant('perl', default=True, description='Disable if you have problems with the optional script language bindings')
+    variant('shared', default=True, description='Building static is required on AIX')
+
     parallel = False
 
     depends_on("swig")
@@ -14,8 +17,12 @@ class Graphviz(Package):
     depends_on("ghostscript")
 
     def install(self, spec, prefix):
-        configure("--prefix=%s" %prefix)
+        options = ['--prefix=%s' % prefix]
+        if '~perl' in spec:
+            options.append('--disable-perl')
+        if '~shared' in spec:
+            options.append('--enable-shared=no')
 
+        configure(*options)
         make()
         make("install")
-
