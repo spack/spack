c003ff7e7fc9c2cf07ec933900aa57b91c342734
Erik Schnetter <schnetter@gmail.com>
2016-03-23 15:47:27 -0400

    Update Julia to 0.4.5

--- var/spack/repos/builtin/packages/julia/package.py
+++ var/spack/repos/builtin/packages/julia/package.py
@@ -8,6 +8,7 @@ class Julia(Package):
 
     version('master',
             git='https://github.com/JuliaLang/julia.git', branch='master')
+    version('0.4.5', '69141ff5aa6cee7c0ec8c85a34aa49a6')
     version('0.4.3', '8a4a59fd335b05090dd1ebefbbe5aaac')
 
     patch('gc.patch')
@@ -20,8 +21,9 @@ class Julia(Package):
     # depends_on("pkg-config")
     depends_on("python @2.7:2.999")
 
-    # I think that Julia requires the dependencies above, but it builds find (on
-    # my system) without these. We should enable them as necessary.
+    # I think that Julia requires the dependencies above, but it
+    # builds fine (on my system) without these. We should enable them
+    # as necessary.
 
     # Run-time dependencies
     # depends_on("arpack")
@@ -30,19 +32,21 @@ class Julia(Package):
     # depends_on("mpfr")
     # depends_on("pcre2")
 
-    # ARPACK: Requires BLAS and LAPACK; needs to use the same version as Julia.
+    # ARPACK: Requires BLAS and LAPACK; needs to use the same version
+    # as Julia.
 
-    # BLAS and LAPACK: Julia prefers 64-bit versions on 64-bit systems. OpenBLAS
-    # has an option for this; make it available as variant.
+    # BLAS and LAPACK: Julia prefers 64-bit versions on 64-bit
+    # systems. OpenBLAS has an option for this; make it available as
+    # variant.
 
-    # FFTW: Something doesn't work when using a pre-installed FFTW library; need
-    # to investigate.
+    # FFTW: Something doesn't work when using a pre-installed FFTW
+    # library; need to investigate.
 
-    # GMP, MPFR: Something doesn't work when using a pre-installed FFTW library;
-    # need to investigate.
+    # GMP, MPFR: Something doesn't work when using a pre-installed
+    # FFTW library; need to investigate.
 
-    # LLVM: Julia works only with specific versions, and might require patches.
-    # Thus we let Julia install its own LLVM.
+    # LLVM: Julia works only with specific versions, and might require
+    # patches. Thus we let Julia install its own LLVM.
 
     # Other possible dependencies:
     # USE_SYSTEM_OPENLIBM=0
@@ -53,10 +57,11 @@ class Julia(Package):
     # USE_SYSTEM_LIBGIT2=0
 
     def install(self, spec, prefix):
-        # Explicitly setting CC, CXX, or FC breaks building libuv, one of
-        # Julia's dependencies. This might be a Darwin-specific problem. Given
-        # how Spack sets up compilers, Julia should still use Spack's compilers,
-        # even if we don't specify them explicitly.
+        # Explicitly setting CC, CXX, or FC breaks building libuv, one
+        # of Julia's dependencies. This might be a Darwin-specific
+        # problem. Given how Spack sets up compilers, Julia should
+        # still use Spack's compilers, even if we don't specify them
+        # explicitly.
         options = [#"CC=cc",
                    #"CXX=c++",
                    #"FC=fc",
