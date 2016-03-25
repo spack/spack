c7011d21aa95145c7b3978f4220b211e39c9ecf2
Denis Davydov <davydden@gmail.com>
2016-03-25 22:15:39 +0100

    update dependencies. Builds fine on OSX

--- var/spack/repos/builtin/packages/dealii/package.py
+++ var/spack/repos/builtin/packages/dealii/package.py
@@ -12,21 +12,21 @@ class Dealii(Package):
     depends_on ("lapack")
     depends_on ("mpi")
 
-    #depends_on ("arpack")
+    depends_on ("arpack-ng")
     depends_on ("boost")
-    #depends_on ("doxygen")
-    depends_on ("hdf5+mpi")
+    depends_on ("doxygen")
+    depends_on ("hdf5+mpi~cxx") #FIXME NetCDF declares dependency with ~cxx, why?
     depends_on ("metis")
-    #depends_on "muparser"
+    depends_on ("muparser")
     depends_on ("netcdf")
-    #depends_on ("numdiff")
-    #depends_on ("oce")
+    #depends_on ("numdiff") #FIXME
+    depends_on ("oce")
     depends_on ("p4est")
     depends_on ("parmetis")
     depends_on ("petsc")
-    #depends_on ("slepc")
-    depends_on ("SuiteSparse")
-    #depends_on "tbb"
+    depends_on ("slepc")
+    depends_on ("suite-sparse")
+    depends_on ("tbb")
     depends_on ("trilinos")
 
     def install(self, spec, prefix):
@@ -42,21 +42,21 @@ class Dealii(Package):
             '-DCMAKE_BUILD_TYPE=DebugRelease',
             '-DDEAL_II_WITH_THREADS:BOOL=ON'
             '-DDEAL_II_WITH_MPI:BOOL=ON',
-            '-DCMAKE_C_COMPILER=%s' % join_path(self.spec['mpi'].prefix.bin, 'mpicc'),
+            '-DCMAKE_C_COMPILER=%s' % join_path(self.spec['mpi'].prefix.bin, 'mpicc'), # FIXME: avoid hardcoding mpi wrappers names
             '-DCMAKE_CXX_COMPILER=%s' % join_path(self.spec['mpi'].prefix.bin, 'mpic++'),
             '-DCMAKE_Fortran_COMPILER=%s' % join_path(self.spec['mpi'].prefix.bin, 'mpif90'),
-            #'-DARPACK_DIR=%s' %  ,
+            '-DARPACK_DIR=%s' % spec['arpack-ng'].prefix,
             '-DBOOST_DIR=%s' % spec['boost'].prefix,
             '-DHDF5_DIR=%s' % spec['hdf5'].prefix,
             '-DMETIS_DIR=%s' % spec['metis'].prefix,
-            #'-DMUPARSER_DIR=%s '
+            '-DMUPARSER_DIR=%s ' % spec['muparser'].prefix,
             '-DNETCDF_DIR=%s' % spec['netcdf'].prefix,
-            #'-DOPENCASCADE_DIR=
+            '-DOPENCASCADE_DIR=%s' % spec['oce'].prefix,
             '-DP4EST_DIR=%s' % spec['p4est'].prefix,
             '-DPETSC_DIR=%s' % spec['petsc'].prefix,
-            #'-DSLEPC_DIR=
-            '-DUMFPACK_DIR=%s' % spec['SuiteSparse'].prefix,
-            #'-DTBB_DIR=%s'
+            '-DSLEPC_DIR=%s' % spec['slepc'].prefix,
+            '-DUMFPACK_DIR=%s' % spec['suite-sparse'].prefix,
+            '-DTBB_DIR=%s' % spec['tbb'].prefix,
             '-DTRILINOS_DIR=%s' % spec['trilinos'].prefix
         ])
 
