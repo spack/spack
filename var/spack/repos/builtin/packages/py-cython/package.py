413c71869cbbc041fafb761d768661fdf038239a
Elizabeth F <rpf2116@columbia.edu>
2016-03-25 22:45:19 -0400

    Added new version; old versions don't work with Python3.

--- var/spack/repos/builtin/packages/py-cython/package.py
+++ var/spack/repos/builtin/packages/py-cython/package.py
@@ -3,10 +3,13 @@ from spack import *
 class PyCython(Package):
     """The Cython compiler for writing C extensions for the Python language."""
     homepage = "https://pypi.python.org/pypi/cython"
-    url      = "https://pypi.python.org/packages/source/C/Cython/cython-0.22.tar.gz"
+    url      = "https://pypi.python.org/packages/source/C/Cython/Cython-0.22.tar.gz"
 
-    version('0.21.2', 'd21adb870c75680dc857cd05d41046a4')
+    version('0.23.4', '157df1f69bcec6b56fd97e0f2e057f6e')
+
+    # These versions contain illegal Python3 code...
     version('0.22', '1ae25add4ef7b63ee9b4af697300d6b6')
+    version('0.21.2', 'd21adb870c75680dc857cd05d41046a4')
 
     extends('python')
 
