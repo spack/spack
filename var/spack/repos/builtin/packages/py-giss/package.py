058e85bec257bea8e40a16c2f9bf5e2931bf70aa
Elizabeth F <rpf2116@columbia.edu>
2016-03-27 11:42:28 -0400

    Added initial py-giss

---
+++ var/spack/repos/builtin/packages/py-giss/package.py
@@ -0,0 +1,15 @@
+from spack import *
+
+class PyNetcdf(Package):
+    """Python interface to the netCDF Library."""
+    homepage = "http://unidata.github.io/netcdf4-python"
+    url      = "https://github.com/Unidata/netcdf4-python/tarball/v1.2.3.1rel"
+
+    version('1.2.3.1', '4fc4320d4f2a77b894ebf8da1c9895af')
+
+    extends('python')
+    depends_on('py-numpy+blas+lapack')
+    depends_on('py-netcdf')
+
+    def install(self, spec, prefix):
+        python('setup.py', 'install', '--prefix=%s' % prefix)
