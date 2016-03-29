a4eff6de3faa3624a529015ea4cf5ebb1af41484
Kelly (KT) Thompson <kgt@lanl.gov>
2016-03-29 17:10:49 -0600

    + New version of cmake (3.5.1).

--- var/spack/repos/builtin/packages/cmake/package.py
+++ var/spack/repos/builtin/packages/cmake/package.py
@@ -30,6 +30,7 @@ class Cmake(Package):
     homepage  = 'https://www.cmake.org'
     url       = 'https://cmake.org/files/v3.4/cmake-3.4.3.tar.gz'
 
+    version('3.5.1',    'ca051f4a66375c89d1a524e726da0296')
     version('3.5.0',    '33c5d09d4c33d4ffcc63578a6ba8777e')
     version('3.4.3',    '4cb3ff35b2472aae70f542116d616e63')
     version('3.4.0',    'cd3034e0a44256a0917e254167217fc8')
