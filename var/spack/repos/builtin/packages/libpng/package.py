a364ab0d0f5ac226ba9d5a519f2da8ab7a09a846
Kelly (KT) Thompson <kgt@lanl.gov>
2016-03-30 17:12:32 -0600

    libpng depends on zlib.

--- var/spack/repos/builtin/packages/libpng/package.py
+++ var/spack/repos/builtin/packages/libpng/package.py
@@ -9,6 +9,8 @@ class Libpng(Package):
     version('1.6.15', '829a256f3de9307731d4f52dc071916d')
     version('1.6.14', '2101b3de1d5f348925990f9aa8405660')
 
+    depends_on('zlib')
+
     def install(self, spec, prefix):
         configure("--prefix=%s" % prefix)
         make()
