d4e6b15d981a6bad77cb6f36cdc3a41adf05525b
Elizabeth F <rpf2116@columbia.edu>
2016-04-01 15:15:23 -0400

    Fixed TCL/Tk

--- var/spack/repos/builtin/packages/tcl/package.py
+++ var/spack/repos/builtin/packages/tcl/package.py
@@ -9,9 +9,12 @@ class Tcl(Package):
        that is truly cross platform, easily deployed and highly
        extensible."""
     homepage = "http://www.tcl.tk"
+    urlpat = 'http://prdownloads.sourceforge.net/tcl/tcl%s-src.tar.gz'
 
-    version('8.6.3', 'db382feca91754b7f93da16dc4cdad1f',
-            url="http://prdownloads.sourceforge.net/tcl/tcl8.6.3-src.tar.gz")
+    version('8.6.5', '0e6426a4ca9401825fbc6ecf3d89a326', url=urlpat%'8.6.5')
+    version('8.6.4', 'd7cbb91f1ded1919370a30edd1534304', url=urlpat%'8.6.4')
+    version('8.6.3', 'db382feca91754b7f93da16dc4cdad1f', url=urlpat%'8.6.3')
+    version('8.5.19', '0e6426a4ca9401825fbc6ecf3d89a326', url=urlpat%'8.6.5')
 
     depends_on('zlib')
 
