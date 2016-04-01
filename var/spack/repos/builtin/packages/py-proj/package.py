77d6ecd31012e23c53faa8413a6c3ba881b0686f
Elizabeth F <rpf2116@columbia.edu>
2016-04-01 15:16:47 -0400

    Added bug-fix patched version to the package.

--- var/spack/repos/builtin/packages/py-proj/package.py
+++ var/spack/repos/builtin/packages/py-proj/package.py
@@ -5,7 +5,14 @@ class PyProj(Package):
     homepage = "http://jswhit.github.io/pyproj/"
     url      = "https://github.com/jswhit/pyproj/tarball/v1.9.5.1rel"
 
+
     version('1.9.5.1', 'a4b80d7170fc82aee363d7f980279835')
+
+    # We need the benefits of this PR
+    # https://github.com/jswhit/pyproj/pull/54
+    version('citibeth-latlong2',
+        git='https://github.com/citibeth/pyproj.git',
+        branch='latlong2')
 
     extends('python')
 
