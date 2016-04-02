e107c9e469af8120ce1c2d07fc8d9021e7895c17
citibeth <rpf2116@columbia.edu>
2016-04-02 12:45:41 -0400

    Use url_for_version() with tcl package.

--- var/spack/repos/builtin/packages/tcl/package.py
+++ var/spack/repos/builtin/packages/tcl/package.py
@@ -11,10 +11,14 @@ class Tcl(Package):
     homepage = "http://www.tcl.tk"
     urlpat = 'http://prdownloads.sourceforge.net/tcl/tcl%s-src.tar.gz'
 
-    version('8.6.5', '0e6426a4ca9401825fbc6ecf3d89a326', url=urlpat%'8.6.5')
-    version('8.6.4', 'd7cbb91f1ded1919370a30edd1534304', url=urlpat%'8.6.4')
-    version('8.6.3', 'db382feca91754b7f93da16dc4cdad1f', url=urlpat%'8.6.3')
-    version('8.5.19', '0e6426a4ca9401825fbc6ecf3d89a326', url=urlpat%'8.6.5')
+
+    def url_for_version(self, version):
+        return 'http://prdownloads.sourceforge.net/tcl/tcl%s-src.tar.gz' % version
+
+    version('8.6.5', '0e6426a4ca9401825fbc6ecf3d89a326')
+    version('8.6.4', 'd7cbb91f1ded1919370a30edd1534304')
+    version('8.6.3', 'db382feca91754b7f93da16dc4cdad1f')
+    version('8.5.19', '0e6426a4ca9401825fbc6ecf3d89a326')
 
     depends_on('zlib')
 
