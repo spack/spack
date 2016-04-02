2126683203f4b8bf88aac539bac8bd8643f5bee1
citibeth <rpf2116@columbia.edu>
2016-04-02 12:43:19 -0400

    Added deprecation warning to netcdf-cxx

--- var/spack/repos/builtin/packages/netcdf-cxx/package.py
+++ var/spack/repos/builtin/packages/netcdf-cxx/package.py
@@ -1,7 +1,11 @@
 from spack import *
 
 class NetcdfCxx(Package):
-    """C++ compatibility bindings for NetCDF"""
+    “””Deprecated C++ compatibility bindings for NetCDF.
+    These do NOT read or write NetCDF-4 files, and are no longer
+    maintained by Unidata.  Developers should migrate to current
+    NetCDF C++ bindings, in Spack package netcdf-cxx4.”””
+
     homepage = "http://www.unidata.ucar.edu/software/netcdf"
     url      = "http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx-4.2.tar.gz"
 
