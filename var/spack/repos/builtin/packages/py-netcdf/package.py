e260d679b8759e3d11ceed3d7b1682094bfed2fb
Elizabeth F <rpf2116@columbia.edu>
2016-04-01 23:52:46 -0400

    Fixed py-numpy build to set LD_LIBRARY_PATH.  Some of this code should be factored into / used from core Spack.

--- var/spack/repos/builtin/packages/py-netcdf/package.py
+++ var/spack/repos/builtin/packages/py-netcdf/package.py
@@ -1,4 +1,23 @@
 from spack import *
+import os
+
+
+# --------------------------------------
+def walk_dependencies(spec, deps):
+# see: spec.Spec.traverse()
+    if str(spec.version) == 'system':
+        # No Spack module for system-installed packages
+        return
+
+    for dep in spec.dependencies.values():
+        walk_dependencies(dep, deps)
+
+    deps.append(spec)
+
+
+
+# --------------------------------------
+
 
 class PyNetcdf(Package):
     """Python interface to the netCDF Library."""
@@ -13,4 +32,26 @@ class PyNetcdf(Package):
     depends_on('netcdf')
 
     def install(self, spec, prefix):
+
+        # ---- Set up LD_LIBRARY_PATH coming from py-numpy:blas
+        # (because py-numpy isn't built with RPATH)
+        # Our install process imports numpy, and that needs to work.
+        deps = list()
+        py_numpy = spec['py-numpy']
+        if 'blas' in py_numpy:
+            walk_dependencies(spec['py-numpy']['blas'], deps)
+
+        seen = set()
+        deps_unique = list()
+        for dep in deps:
+            if id(dep) not in seen:
+                deps_unique.append(dep)
+                seen.add(id(dep))
+
+        LD_LIBRARY_PATH = []
+        for dep in deps_unique:
+            LD_LIBRARY_PATH.append(join_path(dep.prefix, 'lib'))
+        # --------------------------------
+
+        os.environ['LD_LIBRARY_PATH'] = ':'.join(LD_LIBRARY_PATH)
         python('setup.py', 'install', '--prefix=%s' % prefix)
