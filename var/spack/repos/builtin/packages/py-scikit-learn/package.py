d0ca3952a34a74f0167b76bbedfa3cf8875a399c
Glenn Johnson <glenn-johnson@uiowa.edu>
2016-04-02 20:17:01 -0500

    Add version 0.17.1 of scikit-learn.

--- var/spack/repos/builtin/packages/py-scikit-learn/package.py
+++ var/spack/repos/builtin/packages/py-scikit-learn/package.py
@@ -7,6 +7,7 @@ class PyScikitLearn(Package):
 
     version('0.15.2', 'd9822ad0238e17b382a3c756ea94fe0d')
     version('0.16.1', '363ddda501e3b6b61726aa40b8dbdb7e')
+    version('0.17.1', 'a2f8b877e6d99b1ed737144f5a478dfc')
 
     extends('python')
 
