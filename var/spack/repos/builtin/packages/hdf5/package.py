ec46fed98fb14e3e9579b6ab78639af666aeff65
Erik Schnetter <schnetter@gmail.com>
2016-04-02 20:21:56 -0400

    Update HDF5 to 1.10.0

--- var/spack/repos/builtin/packages/hdf5/package.py
+++ var/spack/repos/builtin/packages/hdf5/package.py
@@ -37,6 +37,8 @@ class Hdf5(Package):
     list_url = "http://www.hdfgroup.org/ftp/HDF5/releases"
     list_depth = 3
 
+    version('1.10.0', 'bdc935337ee8282579cd6bc4270ad199',
+            url='http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0/src/hdf5-1.10.0.tar.gz')
     version('1.8.16', 'b8ed9a36ae142317f88b0c7ef4b9c618')
     version('1.8.15', '03cccb5b33dbe975fdcd8ae9dc021f24')
     version('1.8.13', 'c03426e9e77d7766944654280b467289')
@@ -80,10 +82,16 @@ class Hdf5(Package):
         # sanity check in configure, so this doesn't merit a variant.
         extra_args.append("--enable-unsupported")
 
-        if '+debug' in spec:
-            extra_args.append('--enable-debug=all')
+        if spec.satisfies('@1.10:'):
+            if '+debug' in spec:
+                extra_args.append('--enable-build-mode=debug')
+            else:
+                extra_args.append('--enable-build-mode=production')
         else:
-            extra_args.append('--enable-production')
+            if '+debug' in spec:
+                extra_args.append('--enable-debug=all')
+            else:
+                extra_args.append('--enable-production')
 
         if '+shared' in spec:
             extra_args.append('--enable-shared')
