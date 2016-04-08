dcd6b1934808e3700a3d1fc71e261087d9e71ae7
Ben Boeckel <mathstuf@gmail.com>
2016-04-08 12:03:36 -0400

    py-setuptools: sort versions

--- var/spack/repos/builtin/packages/py-setuptools/package.py
+++ var/spack/repos/builtin/packages/py-setuptools/package.py
@@ -5,11 +5,11 @@ class PySetuptools(Package):
     homepage = "https://pypi.python.org/pypi/setuptools"
     url      = "https://pypi.python.org/packages/source/s/setuptools/setuptools-11.3.tar.gz"
 
-    version('11.3.1', '01f69212e019a2420c1693fb43593930')
-    version('16.0', '0ace0b96233516fc5f7c857d086aa3ad')
-    version('18.1', 'f72e87f34fbf07f299f6cb46256a0b06')
-    version('19.2', '78353b1f80375ca5e088f4b4627ffe03')
     version('20.5', 'fadc1e1123ddbe31006e5e43e927362b')
+    version('19.2', '78353b1f80375ca5e088f4b4627ffe03')
+    version('18.1', 'f72e87f34fbf07f299f6cb46256a0b06')
+    version('16.0', '0ace0b96233516fc5f7c857d086aa3ad')
+    version('11.3.1', '01f69212e019a2420c1693fb43593930')
 
     extends('python')
 
