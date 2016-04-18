9c3d8dae574560f805f430b042b16821fabcf4ed
Adam J. Stewart <ajstewart426@gmail.com>
2016-04-18 14:54:38 -0500

    Add latest jpeg version

--- var/spack/repos/builtin/packages/jpeg/package.py
+++ var/spack/repos/builtin/packages/jpeg/package.py
@@ -1,14 +1,19 @@
 from spack import *
 
 class Jpeg(Package):
-    """jpeg library"""
-    homepage = "http://www.ijg.org"
-    url      = "http://www.ijg.org/files/jpegsrc.v9a.tar.gz"
+    """libjpeg is a widely used free library with functions for handling the
+    JPEG image data format. It implements a JPEG codec (encoding and decoding)
+    alongside various utilities for handling JPEG data."""
 
+    homepage = "http://www.ijg.org"
+    url      = "http://www.ijg.org/files/jpegsrc.v9b.tar.gz"
+
+    version('9b', '6a9996ce116ec5c52b4870dbcd6d3ddb')
     version('9a', '3353992aecaee1805ef4109aadd433e7')
 
     def install(self, spec, prefix):
         configure("--prefix=%s" % prefix)
 
         make()
+        make("test")
         make("install")
