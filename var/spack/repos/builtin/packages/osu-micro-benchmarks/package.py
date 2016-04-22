6a5aad6693dc6b8a5903b34408391d13610c8f2f
Adam J. Stewart <ajstewart426@gmail.com>
2016-04-22 10:08:54 -0500

    Add OSU Micro-Benchmarks package

---
+++ var/spack/repos/builtin/packages/osu-micro-benchmarks/package.py
@@ -0,0 +1,37 @@
+from spack import *
+
+class OsuMicroBenchmarks(Package):
+    """The Ohio MicroBenchmark suite is a collection of independent MPI
+    message passing performance microbenchmarks developed and written at
+    The Ohio State University. It includes traditional benchmarks and
+    performance measures such as latency, bandwidth and host overhead
+    and can be used for both traditional and GPU-enhanced nodes."""
+
+    homepage = "http://mvapich.cse.ohio-state.edu/benchmarks/"
+    url      = "http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-5.3.tar.gz"
+
+    version('5.3', '42e22b931d451e8bec31a7424e4adfc2')
+
+    variant('cuda', default=False, description="Enable CUDA support")
+
+    depends_on('mpi')
+    depends_on('cuda', when='+cuda')
+
+
+    def install(self, spec, prefix):
+        config_args = [
+            'CC=%s'  % spec['mpi'].prefix.bin + '/mpicc',
+            'CXX=%s' % spec['mpi'].prefix.bin + '/mpicxx',
+            '--prefix=%s' % prefix
+        ]
+
+        if '+cuda' in spec:
+            config_args.extend([
+                '--enable-cuda',
+                '--with-cuda=%s' % spec['cuda'].prefix,
+            ])
+
+        configure(*config_args)
+
+        make()
+        make('install')
