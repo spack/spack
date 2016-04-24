797af2e80f0effddeca2b964283705940fa45df8
Glenn Johnson <glennpj@gmail.com>
2016-04-24 17:39:49 -0500

    Add py-csvkit package
    This adds the py-csvkit package and needed dependencies for working with
    csv files.

---
+++ var/spack/repos/builtin/packages/py-jdcal/package.py
@@ -0,0 +1,15 @@
+from spack import *
+
+class PyJdcal(Package):
+    """
+    Julian dates from proleptic Gregorian and Julian calendars
+    """
+    homepage = 'http://github.com/phn/jdcal'
+    url      = "https://pypi.python.org/packages/source/j/jdcal/jdcal-1.2.tar.gz"
+
+    version('1.2', 'ab8d5ba300fd1eb01514f363d19b1eb9')
+
+    extends('python')
+
+    def install(self, spec, prefix):
+        python('setup.py', 'install', '--prefix=%s' % prefix)
