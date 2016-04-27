e04429910d1bfe3404b128258ed22841cc566868
Elizabeth F <rpf2116@columbia.edu>
2016-04-27 17:54:31 -0400

    glint2: Added new package for older versions of IceBin library.

---
+++ var/spack/repos/builtin/packages/glint2/package.py
@@ -0,0 +1,54 @@
+from spack import *
+
+class Glint2(CMakePackage):
+    """Regridding/Coupling library for GCM + Ice Sheet Model"""
+
+    homepage = "https://github.com/citibeth/icebin"
+    url         = "https://github.com/citibeth/icebin/tarball/v0.1.0"
+
+    version('0.1.0', '1c2769a0cb3531e4086b885dc7a6fd27')
+
+    variant('everytrace', default=False, description='Report errors through Everytrace (requires Everytrace)')
+    variant('python', default=True, description='Build Python extension (requires Python, Numpy)')
+    variant('gridgen', default=True, description='Build grid generators (requires CGAL, GMP, MPFR)')
+    variant('coupler', default=True, description='Build the GCM coupler (requires MPI)')
+    variant('pism', default=False, description='Build coupling link with PISM (requires PISM, PETSc)')
+
+    extends('python', when='+python')
+
+    depends_on('everytrace', when='+everytrace')
+
+    depends_on('python', when='+python')
+    depends_on('py-cython', when='+python')
+    depends_on('py-numpy', when='+python')
+
+    depends_on('cgal', when='+gridgen')
+    depends_on('gmp', when='+gridgen')
+    depends_on('mpfr', when='+gridgen')
+
+    depends_on('mpi', when='+coupler')
+    depends_on('pism~python', when='+coupler+pism')
+    depends_on('petsc', when='+coupler+pism')
+
+    depends_on('boost+filesystem+date_time')
+    depends_on('blitz')
+    depends_on('netcdf-cxx')
+    depends_on('proj')
+    depends_on('eigen')
+    depends_on('galahad')
+
+    # Build dependencies
+    depends_on('cmake')
+    depends_on('doxygen')
+
+    def configure_args(self):
+        spec = self.spec
+        return [
+            '-DUSE_EVERYTRACE=%s' % ('YES' if '+everytrace' in spec else 'NO'),
+            '-DUSE_PYTHON=%s' % ('YES' if '+python' in spec else 'NO'),
+            '-DUSE_PISM=%s' % ('YES' if '+pism' in spec else 'NO')]
+
+    def setup_environment(self, spack_env, env):
+        """Add <prefix>/bin to the module; this is not the default if we
+        extend python."""
+        env.prepend_path('PATH', join_path(self.prefix, 'bin'))
