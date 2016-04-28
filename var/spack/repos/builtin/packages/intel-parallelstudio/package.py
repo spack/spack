a58da9296c632b7680e6962c851dd9a9094896e6
Greg Lee <lee218@llnl.gov>
2016-04-27 21:03:05 -0700

    moved shared install code to support/intel and added intel package for compilers

--- var/spack/repos/builtin/packages/intel-parallelstudio/package.py
+++ var/spack/repos/builtin/packages/intel-parallelstudio/package.py
@@ -1,7 +1,12 @@
 from spack import *
-import os, re
+import sys, os, re
 
-class IntelParallelstudio(Package):
+#TODO we may want to fix this path hack if something is implemented for #832
+sys.path.append(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
+from support.intel import *
+sys.path.pop()
+
+class IntelParallelstudio(IntelInstaller):
     """Intel Parallel Studio.
 
     Note: You will have to add the download file to a
@@ -39,9 +44,6 @@ class IntelParallelstudio(Package):
         except OSError:
             pass # if the file does not exist
 
-        # TODO: Update to use pull request 558
-        license_path = "/usr/local/etc/license.client.intel.lic"
-
         base_components = "ALL" # when in doubt, install everything
         mpi_components = ""
         mkl_components = ""
@@ -49,10 +51,6 @@ class IntelParallelstudio(Package):
         ipp_components = ""
         tools_components = ""
 
-        def filter_pick(input_list, regex_filter):
-            return [l for l in input_list for m in (regex_filter(l),) if m]
-        def unfilter_pick(input_list, regex_filter):
-            return [l for l in input_list for m in (regex_filter(l),) if not m]
         if spec.satisfies('+all'):
             base_components = "ALL"
         else:
@@ -83,40 +81,24 @@ class IntelParallelstudio(Package):
             if spec.satisfies('+tools') and (spec.satisfies('@cluster') or spec.satisfies('@professional')):
                 components += tool_components
 
-        components_string = ';'.join(components)
-
-        silent_config_filename = 'silent.cfg'
-        with open(silent_config_filename, 'w') as f:
-            f.write("""
-ACCEPT_EULA=accept
-PSET_MODE=install
-CONTINUE_WITH_INSTALLDIR_OVERWRITE=yes
-PSET_INSTALL_DIR=%s
-ACTIVATION_LICENSE_FILE=%s
-ACTIVATION_TYPE=license_file
-PHONEHOME_SEND_USAGE_DATA=no
-COMPONENTS=%s
-""" %(prefix, license_path, components_string))
-
-        install_script = which("install.sh")
-
-        install_script('--silent', silent_config_filename)
+        self.intel_components = ';'.join(components)
+        IntelInstaller.install(self, spec, prefix)
 
         absbindir = os.path.split(os.path.realpath(os.path.join(self.prefix.bin, "icc")))[0]
         abslibdir = os.path.split(os.path.realpath(os.path.join(self.prefix.lib, "intel64", "libimf.a")))[0]
 
         # symlink or copy?
-        os.symlink(license_path, os.path.join(absbindir, "license.lic"))
+        os.symlink(self.license_path, os.path.join(absbindir, "license.lic"))
         if spec.satisfies('+tools') and (spec.satisfies('@cluster') or spec.satisfies('@professional')):
             os.mkdir(os.path.join(self.prefix, "inspector_xe/licenses"))
-            os.symlink(license_path, os.path.join(self.prefix, "inspector_xe/licenses", "license.lic"))
+            os.symlink(self.license_path, os.path.join(self.prefix, "inspector_xe/licenses", "license.lic"))
             os.mkdir(os.path.join(self.prefix, "advisor_xe/licenses"))
-            os.symlink(license_path, os.path.join(self.prefix, "advisor_xe/licenses", "license.lic"))
+            os.symlink(self.license_path, os.path.join(self.prefix, "advisor_xe/licenses", "license.lic"))
             os.mkdir(os.path.join(self.prefix, "vtune_amplifier_xe/licenses"))
-            os.symlink(license_path, os.path.join(self.prefix, "vtune_amplifier_xe/licenses", "license.lic"))
+            os.symlink(self.license_path, os.path.join(self.prefix, "vtune_amplifier_xe/licenses", "license.lic"))
 
         if (spec.satisfies('+all') or spec.satisfies('+mpi')) and spec.satisfies('@cluster'):
-            os.symlink(license_path, os.path.join(self.prefix, "itac_latest", "license.lic"))
+            os.symlink(self.license_path, os.path.join(self.prefix, "itac_latest", "license.lic"))
 
         if spec.satisfies('+rpath'):
             for compiler_command in ["icc", "icpc", "ifort"]:
