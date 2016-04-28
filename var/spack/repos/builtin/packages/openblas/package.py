6b9dbbe3fbfe11e2ce78b203d9b8d52fc6d678a1
Elizabeth F <rpf2116@columbia.edu>
2016-04-27 20:35:29 -0400

    Add '+fpic' variant.

--- var/spack/repos/builtin/packages/openblas/package.py
+++ var/spack/repos/builtin/packages/openblas/package.py
@@ -14,6 +14,7 @@ class Openblas(Package):
 
     variant('shared', default=True, description="Build shared libraries as well as static libs.")
     variant('openmp', default=True, description="Enable OpenMP support.")
+    variant('fpic', default=True, description="Build position independent code")
 
     # virtual dependency
     provides('blas')
@@ -33,6 +34,8 @@ class Openblas(Package):
         if '+shared' in spec:
             make_targets += ['shared']
         else:
+            if '+fpic' in spec:
+                make_defs.extend(['CFLAGS=-fPIC', 'FFLAGS=-fPIC'])
             make_defs += ['NO_SHARED=1']
 
         # fix missing _dggsvd_ and _sggsvd_
