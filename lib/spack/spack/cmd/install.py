acace23ead872cbbfda966cd93297d3e512c3d1e
alalazo <massimiliano.culpo@googlemail.com>
2016-04-29 13:28:34 +0200

    install : finer graned locking for install command

--- lib/spack/spack/cmd/install.py
+++ lib/spack/spack/cmd/install.py
@@ -79,14 +79,13 @@ def install(parser, args):
     specs = spack.cmd.parse_specs(args.packages, concretize=True)
     for spec in specs:
         package = spack.repo.get(spec)
-        with spack.installed_db.write_transaction():
-            package.do_install(
-                keep_prefix=args.keep_prefix,
-                keep_stage=args.keep_stage,
-                ignore_deps=args.ignore_deps,
-                make_jobs=args.jobs,
-                run_tests=args.run_tests,
-                verbose=args.verbose,
-                fake=args.fake,
-                dirty=args.dirty,
-                explicit=True)
+        package.do_install(
+            keep_prefix=args.keep_prefix,
+            keep_stage=args.keep_stage,
+            ignore_deps=args.ignore_deps,
+            make_jobs=args.jobs,
+            run_tests=args.run_tests,
+            verbose=args.verbose,
+            fake=args.fake,
+            dirty=args.dirty,
+            explicit=True)
