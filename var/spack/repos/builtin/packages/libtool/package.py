ee2820180ae290d1d86f7d8425ba1acf4c6d7a1f
Vishal Boddu <vishal.boddu@fau.de>
2016-05-03 11:23:24 +0200

    bugfix: changed libtool version to be downloaded from 2.4.2 to 2.4.6

--- var/spack/repos/builtin/packages/libtool/package.py
+++ var/spack/repos/builtin/packages/libtool/package.py
@@ -3,7 +3,7 @@ from spack import *
 class Libtool(Package):
     """libtool -- library building part of autotools"""
     homepage = "https://www.gnu.org/software/libtool/"
-    url      = "http://ftpmirror.gnu.org/libtool/libtool-2.4.2.tar.gz"
+    url      = "http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz"
 
     version('2.4.6' , 'addf44b646ddb4e3919805aa88fa7c5e')
     version('2.4.2' , 'd2f3b7d4627e69e13514a40e72a24d50')
