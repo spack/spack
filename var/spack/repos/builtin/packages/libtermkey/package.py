22afc6dadd81db12262571bddad6256c2900db64
Tom Scogland <scogland1@llnl.gov>
2016-05-06 15:33:26 -0700

    pile of dependencies for neovim, including luajit and libuv

---
+++ var/spack/repos/builtin/packages/libtermkey/package.py
@@ -0,0 +1,17 @@
+from spack import *
+
+class Libtermkey(Package):
+    """Easy keyboard entry processing for terminal programs"""
+    homepage = "http://www.leonerd.org.uk/code/libtermkey/"
+    url      = "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.18.tar.gz"
+
+    version('0.18' , '3be2e3e5a851a49cc5e8567ac108b520')
+    version('0.17' , '20edb99e0d95ec1690fe90e6a555ae6d')
+    version('0.16' , '7a24b675aaeb142d30db28e7554987d4')
+    version('0.15b', '27689756e6c86c56ae454f2ac259bc3d')
+    version('0.14' , 'e08ce30f440f9715c459060e0e048978')
+
+
+    def install(self, spec, prefix):
+        make()
+        make("install", "PREFIX=" + prefix)
