2e0ee5404d839a69c626407436c6cb059a51d9fd
Todd Gamblin <tgamblin@llnl.gov>
2016-05-09 17:14:25 -0700

    clean up Kripke package and dependencies.

--- var/spack/repos/builtin/packages/kripke/package.py
+++ var/spack/repos/builtin/packages/kripke/package.py
@@ -1,18 +1,28 @@
 from spack import *
 
 class Kripke(Package):
-    """Kripke is a simple, scalable, 3D Sn deterministic particle transport proxy/mini app."""
+    """Kripke is a simple, scalable, 3D Sn deterministic particle
+       transport proxy/mini app.
+    """
     homepage = "https://codesign.llnl.gov/kripke.php"
     url      = "https://codesign.llnl.gov/downloads/kripke-openmp-1.1.tar.gz"
 
     version('1.1', '7fe6f2b26ed983a6ce5495ab701f85bf')
 
-    #depends_on("mvapich2@1.9:")
+    variant('mpi', default=True, description='Enable MPI support')
+
+    depends_on('mpi', when="+mpi")
 
     def install(self, spec, prefix):
         with working_dir('build', create=True):
-         cmake('-DCMAKE_INSTALL_PREFIX:PATH=.', '-DENABLE_OPENMP=1', '-DENABLE_MPI=1', '..', *std_cmake_args)
-         make()
-	 #Kripke does not provide an install, so creating one here.
-	 mkdirp(prefix.bin)
-         install('kripke', prefix.bin)
+            cmake('-DCMAKE_INSTALL_PREFIX:PATH=.',
+                  '-DENABLE_OPENMP=1',
+                  '-DENABLE_MPI=1',
+                  '..',
+                  *std_cmake_args)
+            make()
+
+            # Kripke does not provide install target, so we have to copy
+            # things into place.
+            mkdirp(prefix.bin)
+            install('kripke', prefix.bin)
