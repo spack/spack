5d477bc956e0033bd2816d48df1f4096856bc5fe
Benedikt Hegner <hegner@cern.ch>
2016-05-10 23:19:30 +0200

    fix setting config for list parameters

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -545,7 +545,10 @@ def update_config(section, update_data, scope=None):
     # read in the config to ensure we've got current data
     configuration = get_config(section)
 
-    configuration.update(update_data)
+    if isinstance(update_data, list):
+        configuration = update_data
+    else:
+        configuration.extend(update_data)
 
     # read only the requested section's data.
     scope.sections[section] = {section: configuration}
