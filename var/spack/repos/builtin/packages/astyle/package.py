c8d88607425866413c0d3c931e760c89c01a00fd
Denis Davydov <davydden@gmail.com>
2016-05-10 17:46:11 +0200

    astyle: fix formatting

--- var/spack/repos/builtin/packages/astyle/package.py
+++ var/spack/repos/builtin/packages/astyle/package.py
@@ -1,8 +1,11 @@
 from spack import *
-import os
+
 
 class Astyle(Package):
-    """A Free, Fast, and Small Automatic Formatter for C, C++, C++/CLI, Objective-C, C#, and Java Source Code."""
+    """
+    A Free, Fast, and Small Automatic Formatter for C, C++, C++/CLI,
+    Objective-C, C#, and Java Source Code.
+    """
     homepage = "http://astyle.sourceforge.net/"
     url      = "http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz"
 
@@ -11,13 +14,15 @@ class Astyle(Package):
     def install(self, spec, prefix):
 
         with working_dir('src'):
-            # unfortunately we need to edit the makefile in place to set compiler:
-            make_file = join_path(self.stage.source_path,'build','gcc','Makefile')
-            filter_file(r'^CXX\s*=.*', 'CXX=%s' % spack_cxx, make_file)
+            # we need to edit the makefile in place to set compiler:
+            make_file = join_path(self.stage.source_path,
+                                  'build', 'gcc', 'Makefile')
+            filter_file(r'^CXX\s*=.*', 'CXX=%s'.format(spack_cxx), make_file)
 
             make('-f',
-                make_file,
-                parallel=False)
+                 make_file,
+                 parallel=False)
 
             mkdirp(self.prefix.bin)
-            install(join_path(self.stage.source_path, 'src','bin','astyle'), self.prefix.bin)
+            install(join_path(self.stage.source_path, 'src', 'bin', 'astyle'),
+                    self.prefix.bin)
