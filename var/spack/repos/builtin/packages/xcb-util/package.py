7597fdf689b2b8dad0be60c7abf4885a7fb19894
Dhanannjay 'Djay' Deo <dhanannjay.deo@kitware.com>
2016-05-11 16:49:43 -0400

    Fix: xcb-util formatting and fixmes

--- var/spack/repos/builtin/packages/xcb-util/package.py
+++ var/spack/repos/builtin/packages/xcb-util/package.py
@@ -1,37 +1,24 @@
-# FIXME:
-# This is a template package file for Spack.  We've conveniently
-# put "FIXME" labels next to all the things you'll want to change.
-#
-# Once you've edited all the FIXME's, delete this whole message,
-# save this file, and test out your package like this:
-#
-#     spack install xcb-util
-#
-# You can always get back here to change things with:
-#
-#     spack edit xcb-util
-#
-# See the spack documentation for more information on building
-# packages.
-#
 from spack import *
 
+
 class XcbUtil(Package):
-    """FIXME: put a proper description of your package here."""
-    # FIXME: add a proper url for your package's homepage here.
-    homepage = "http://www.example.com"
+    """The xcb-util module provides a number of libraries which sit on top of
+    libxcb, the core X protocol library, and some of the extension libraries.
+    These experimental libraries provide convenience functions and interfaces
+    which make the raw X protocol more usable. Some of the libraries also
+    provide client-side code which is not strictly part of the X protocol but
+    which have traditionally been provided by Xlib."""
+
+    homepage = "https://xcb.freedesktop.org/XcbUtil"
     url      = "https://xcb.freedesktop.org/dist/xcb-util-0.4.0.tar.gz"
 
     version('0.4.0', '157d82738aa89934b6adaf3ca508a0f5')
     version('0.3.9', 'ca04b25d913239a3ef94b688f7ac38cd')
 
-    # FIXME: Add dependencies if this package requires them.
     depends_on("libxcb")
 
     def install(self, spec, prefix):
-        # FIXME: Modify the configure line to suit your build system here.
         configure('--prefix=%s' % prefix)
 
-        # FIXME: Add logic to build and install here
         make()
         make("install")
