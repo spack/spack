e763eeacdde4bd63d5beb1fee0ca6eb8593a0884
George Zagaris <george.zagaris@gmail.com>
2016-05-12 21:30:11 -0700

    fix format in doxygen/package.py

--- var/spack/repos/builtin/packages/doxygen/package.py
+++ var/spack/repos/builtin/packages/doxygen/package.py
@@ -22,20 +22,21 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#------------------------------------------------------------------------------
+
 # Author: Justin Too <justin@doubleotoo.com>
 # Date: September 11, 2015
-#------------------------------------------------------------------------------
 
 from spack import *
 import sys
+
 
 class Doxygen(Package):
     """Doxygen is the de facto standard tool for generating documentation
     from annotated C++ sources, but it also supports other popular programming
     languages such as C, Objective-C, C#, PHP, Java, Python, IDL (Corba,
-    Microsoft, and UNO/OpenOffice flavors), Fortran, VHDL, Tcl, and to some extent D..
-    """
+    Microsoft, and UNO/OpenOffice flavors), Fortran, VHDL, Tcl, and to some
+    extent D.."""
+
     homepage = "http://www.stack.nl/~dimitri/doxygen/"
     url      = "http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.10.src.tar.gz"
 
@@ -46,7 +47,6 @@ class Doxygen(Package):
     # flex does not build on OSX, but it's provided there anyway
     depends_on("flex", sys.platform != 'darwin')
     depends_on("bison", sys.platform != 'darwin')
-
 
     def install(self, spec, prefix):
         cmake('.', *std_cmake_args)
