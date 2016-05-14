4bf9b168a57dab285983485394f7d22d6ec7ea1f
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-05-14 16:19:10 -0400

    netcdf-cxx4: Update to latest released version 4.3.0

--- var/spack/repos/builtin/packages/netcdf-cxx4/package.py
+++ var/spack/repos/builtin/packages/netcdf-cxx4/package.py
@@ -27,13 +27,16 @@ from spack import *
 class NetcdfCxx4(Package):
     """C++ interface for NetCDF4"""
     homepage = "http://www.unidata.ucar.edu/software/netcdf"
-    url      = "http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx4-4.2.tar.gz"
+    url      = "https://www.github.com/unidata/netcdf-cxx4/tarball/v4.3.0"
 
-    version('4.2', 'd019853802092cf686254aaba165fc81')
+    version('4.3.0', '0dde8b9763eecdafbd69d076e687337e')
+    version('4.2.1', 'd019853802092cf686254aaba165fc81')
 
     depends_on('netcdf')
+    depends_on("autoconf")
 
     def install(self, spec, prefix):
+        which('autoreconf')('-ivf')    # Rebuild to prevent problems of inconsistency in git repo
         configure('--prefix=%s' % prefix)
         make()
         make("install")
