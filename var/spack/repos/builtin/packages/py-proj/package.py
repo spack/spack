eab87aa37a41a652401320c1340e08fd1cc3cc09
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-05-14 16:09:01 -0400

    py-proj: Removed versions from forked repos, now that necessary bug fixes have been merged into the main repo.

--- var/spack/repos/builtin/packages/py-proj/package.py
+++ var/spack/repos/builtin/packages/py-proj/package.py
@@ -5,14 +5,12 @@ class PyProj(Package):
     homepage = "http://jswhit.github.io/pyproj/"
     url      = "https://github.com/jswhit/pyproj/tarball/v1.9.5.1rel"
 
+    # This is not a tagged release of pyproj.
+    # The changes in this "version" fix some bugs, especially with Python3 use.
+    version('1.9.5.1.1', 'd035e4bc704d136db79b43ab371b27d2',
+        url='https://www.github.com/jswhit/pyproj/tarball/0be612cc9f972e38b50a90c946a9b353e2ab140f')
 
     version('1.9.5.1', 'a4b80d7170fc82aee363d7f980279835')
-
-    # We need the benefits of this PR
-    # https://github.com/jswhit/pyproj/pull/54
-    version('citibeth-latlong2',
-        git='https://github.com/citibeth/pyproj.git',
-        branch='latlong2')
 
     extends('python')
 
