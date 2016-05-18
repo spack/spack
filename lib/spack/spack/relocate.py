987c549fec10ab18c5ef202016c035824679397e
Patrick Gartung <gartung@fnal.gov>
2016-05-18 18:30:04 -0500

    using @rpath was a bust; switched replacing olddir with newdir; add option to Darwin ld to pad rpath entries as much as possible for long relocation paths:

--- lib/spack/spack/relocate.py
+++ lib/spack/spack/relocate.py
@@ -1,4 +1,5 @@
 #  "Benedikt Hegner (CERN)"
+#  "Patrick Gartung (FNAL)"
 
 import os
 import stat
@@ -10,32 +11,11 @@ from spack.util.executable import which
 import llnl.util.tty as tty
 
 
-def get_existing_rpath(path_name, patchelf_executable):
+def get_existing_elf_rpaths(path_name, old_dir, new_dir):
     """
-    Return the RPATHS in given file as a list of strings.
+    Return the RPATHS in given elf file as a list of strings.
     """
-    if platform.system() == 'Darwin':
-        command = which('otool')
-        output  = command("-l", path_name, output=str, err=str)
-        if command.returncode != 0:
-            tty.warn('failed reading rpath for %s.' % path_name)
-            return False
-        last_cmd = None
-        path = set()
-        for line in output.split('\n'):
-            match = re.search('( *[a-zA-Z]+ )(.*)', line)
-            if match:
-                lhs = match.group(1).lstrip().rstrip()
-                rhs = match.group(2)
-                match2 = re.search('(.*) \(.*\)', rhs)
-                if match2:
-                    rhs = match2.group(1)
-                if lhs == 'cmd':
-                    last_cmd = rhs
-                if lhs == 'path' and last_cmd == 'LC_RPATH':
-                    path.add(rhs)
-        return path
-    elif platform.system() == 'Linux':
+    if platform.system() == 'Linux':
         command = '%s --print-rpath %s ' % (patchelf_executable, path_name)
         status, output = getstatusoutput(command)
         if status != 0:
@@ -46,9 +26,33 @@ def get_existing_rpath(path_name, patchelf_executable):
         tty.die('relocation not supported for this platform')
     return retval
 
-def change_dylib(path_name, rpaths):
+def modify_macho_object(path_name, old_dir, new_dir):
     """
-    Return the RPATHS in given file as a list of strings.
+    Modify MachO binaries by changing rpaths,and id and dependency lib paths.
+
+    Examines the output of otool -l for these three patterns
+
+          cmd LC_ID_DYLIB
+      cmdsize 160
+         name /Users/gartung/spack-macdev/opt/spack/darwin-x86_64/clang-7.0.2-apple/tcl-8.6.5-xfeydlhaojmei6iws2rnxndvriym242k/lib/libtcl8.6.dylib (offset 24)
+
+          cmd LC_LOAD_DYLIB
+      cmdsize 160
+         name /Users/gartung/spack-macdev/opt/spack/darwin-x86_64/clang-7.0.2-apple/zlib-1.2.8-cyvcqvrzlgurne424y55hxvfucvz2354/lib/libz.1.dylib (offset 24)
+
+          cmd LC_RPATH
+      cmdsize 128
+         path /Users/gartung/spack-macdev/opt/spack/darwin-x86_64/clang-7.0.2-apple/xz-5.2.2-d4ecxpuzf2g3ycz3cnj3xmdj7zdnuqwb/lib (offset 12)
+  
+    the old install dir in LC_LOAD_DYLIB is replace with the new install dir  using 
+        install_name_tool -id newid binary
+
+    the old install dir in LC_LOAD_DYLIB is replaced with the new install dir  using 
+        install_name_tool -change old new binary
+
+    the old install dir in LC_RPATH is replaced with the new install dir using
+        install_name_tool  -rpath old new binary
+   
     """
     command = which('otool')
     output  = command("-l", path_name, output=str, err=str)
@@ -56,7 +60,8 @@ def change_dylib(path_name, rpaths):
         tty.warn('failed reading rpath for %s.' % path_name)
         return False
     last_cmd = None
-    path = ''
+    idpath = ''
+    rpaths = []
     deps = []
     for line in output.split('\n'):
         match = re.search('( *[a-zA-Z]+ )(.*)', line)
@@ -68,19 +73,21 @@ def change_dylib(path_name, rpaths):
                 rhs = match2.group(1)
             if lhs == 'cmd':
                 last_cmd = rhs
+            if lhs == 'path' and last_cmd == 'LC_RPATH':
+                rpaths.append(rhs)
             if lhs == 'name' and last_cmd == 'LC_ID_DYLIB':
-                path=rhs
+                idpath=rhs
             if lhs == 'name' and last_cmd == 'LC_LOAD_DYLIB':
                 deps.append(rhs)
-    id = path
+    id = idpath.replace(old_dir,new_dir)
     ndeps = []
+    nrpaths = []
     for rpath in rpaths:
-        if re.match(rpath,path):
-            id = '@rpath'+re.split(rpath,path)[1]
+        nrpath = rpath.replace(old_dir,new_dir)
+        nrpaths.append(nrpath)
         for dep in deps:
-            if re.match(rpath,dep):
-                ndep = '@rpath'+re.split(rpath,dep)[1]
-                ndeps.append(ndep)
+            ndep = dep.replace(old_dir,new_dir)
+            ndeps.append(ndep)
 
     st = os.stat(path_name)
     wmode = os.access(path_name, os.W_OK)
@@ -93,6 +100,7 @@ def change_dylib(path_name, rpaths):
         if status != 0:
             tty.warn('failed writing id for %s.' % path_name)
             tty.warn(output)
+
     for orig, new in zip(deps, ndeps):
         command = ("install_name_tool -change  %s %s %s" % 
                          (orig, new, path_name))
@@ -100,6 +108,15 @@ def change_dylib(path_name, rpaths):
         if status != 0:
             tty.warn('failed writing dep for %s.' % path_name)
             tty.warn(output)
+
+    for orig, new in zip(rpaths, nrpaths):
+        command= ("install_name_tool -rpath %s %s %s" % 
+                         (orig,new,path_name) )
+        status, output = getstatusoutput(command)
+        if status != 0:
+            tty.warn('failed writing id for %s.' % path_name)
+            tty.warn(output)
+
     os.chmod(path_name, st.st_mode)
     return
 
@@ -117,25 +134,15 @@ def get_filetype(path_name):
     return output.strip()
 
 
-def modify_rpath(path_name, orig_rpath, new_rpath, patchelf_executable):
+def modify_elf_object(path_name, orig_rpath, new_rpath, patchelf_executable):
     """
-    Replace RPATH in given binary
+    Replace RPATH's in given elf object
     """
     st = os.stat(path_name)
     wmode = os.access(path_name, os.W_OK)
     if not wmode:
         os.chmod(path_name, st.st_mode | stat.S_IWUSR)
-    if platform.system() == 'Darwin':
-        for orig, new in zip(orig_rpath, new_rpath):
-            command = which("install_name_tool")
-            output = command("-rpath", "%s" % orig,
-                             "%s" % new,
-                             "%s" % path_name,
-                             output=str,
-                             err=str)
-            if command.returncode != 0:
-                tty.warn('failed writing rpath for %s.' % path_name)
-    elif platform.system() == 'Linux':
+    if platform.system() == 'Linux':
         new_joined = ':'.join(new_rpath)
         command = "%s --force-rpath --set-rpath '%s' '%s'" % \
             (patchelf_executable, new_joined, path_name)
@@ -172,13 +179,16 @@ def needs_text_relocation(filetype):
 
 def relocate_binary(path_name, old_dir, new_dir, patchelf_executable):
     """
-    Change RPATHs in given file
+    Change RPATHs in given elf or mach-o file
     """
-    orig_rpath = get_existing_rpath(path_name, patchelf_executable)
-    new_rpath  = substitute_rpath(orig_rpath, old_dir, new_dir)
-    modify_rpath(path_name, orig_rpath, new_rpath, patchelf_executable)
     if platform.system() == 'Darwin':
-       change_dylib(path_name, orig_rpath)
+        modify_macho_object(path_name, old_dir, new_dir)
+    elif platform.system() == 'Linux':
+        orig_rpaths = get_existing_elf_rpaths(path_name, patchelf_executable)
+        new_rpaths  = substitute_rpath(orig_rpath, old_dir, new_dir)
+        modify_elf_object(path_name, orig_rpaths, new_rpaths, patchelf_executable)
+    else:
+        tty.die("Relocation not implemented for %s" % platform.system())
 
 
 def relocate_text(path_name, old_dir, new_dir):
