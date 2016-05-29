fa73ce152f7929795e812225823f9df065a48969
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-05-29 14:31:49 -0400

    Added new version

--- var/spack/repos/builtin/packages/py-giss/package.py
+++ var/spack/repos/builtin/packages/py-giss/package.py
@@ -6,12 +6,14 @@ class PyGiss(Package):
     homepage = "https://github.com/citibeth/pygiss"
     url      = "https://github.com/citibeth/pygiss/tarball/v0.1.0"
 
+    version('0.1.2', '1c1c745c2818a6930c29c6ec7f835943')
     version('0.1.1', '172d468690a8b8f474884d7a60064bc7')
     version('glint2', git='https://github.com/citibeth/pygiss.git', branch='glint2')
 
     # Requires python@3:
     extends('python')
 
+    depends_on('python@3:')
     depends_on('py-numpy+blas+lapack')
     depends_on('py-netcdf')
     depends_on('py-matplotlib')
