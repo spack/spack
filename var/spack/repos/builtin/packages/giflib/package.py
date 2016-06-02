8b6035ab8268de726933b657837d5c1bd8d13d4e
Nicolas Richart <nicolas.richart@epfl.ch>
2016-06-03 00:21:41 +0200

    Making emacs dependent on internal packages

--- var/spack/repos/builtin/packages/giflib/package.py
+++ var/spack/repos/builtin/packages/giflib/package.py
@@ -23,44 +23,19 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import llnl.util.tty as tty
 
 
-class Emacs(Package):
-    """The Emacs programmable text editor."""
+class Giflib(Package):
+    """The GIFLIB project maintains the giflib service library, which has
+    been pulling images out of GIFs since 1989."""
 
-    homepage = "https://www.gnu.org/software/emacs"
-    url      = "http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz"
+    homepage = "http://giflib.sourceforge.net/"
+    url      = "https://downloads.sourceforge.net/project/giflib/giflib-5.1.4.tar.bz2"
 
-    version('24.5', 'd74b597503a68105e61b5b9f6d065b44')
-
-    variant('X', default=True, description="Enable a X toolkit (GTK+)")
-    variant('gtkplus', default=False, description="Enable a GTK+ as X toolkit (this variant is ignored if ~X)")
-
-    depends_on('ncurses')
-    depends_on('libtiff', when='+X')
-    depends_on('libpng', when='+X')
-    depends_on('libxpm', when='+X')
-    depends_on('giflib', when='+X')
-    depends_on('gtkplus', when='+X+gtkplus')
+    version('5.1.4', '2c171ced93c0e83bb09e6ccad8e3ba2b')
 
     def install(self, spec, prefix):
-        args = []
-        if '+X' in spec:
-            if '+gtkplus' in spec:
-                toolkit = 'gtk{0}'.format(spec['gtkplus'].version.up_to(1))
-            else:
-                toolkit = 'no'
-            args = [
-                '--with-x',
-                '--with-x-toolkit={0}'.format(toolkit)
-            ]
-        else:
-            args = ['--without-x']
-            if '+gtkplus' in spec:
-                tty.warn('The variant +gtkplus is ignored if ~X is selected.')
-
-        configure('--prefix={0}'.format(prefix), *args)
+        configure('--prefix=%s' % prefix)
 
         make()
         make("install")
