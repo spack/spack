4dcb15cf6d7ae2f6874972f1f9c339c056875500
Glenn Johnson <glenn-johnson@uiowa.edu>
2016-06-12 22:00:49 -0500

    Ran through autopep8 and cleaned up.

--- var/spack/repos/builtin/packages/postgresql/package.py
+++ var/spack/repos/builtin/packages/postgresql/package.py
@@ -24,12 +24,13 @@
 ##############################################################################
 from spack import *
 
+
 class Postgresql(Package):
-    """PostgreSQL is a powerful, open source object-relational
-       database system. It has more than 15 years of active
-       development and a proven architecture that has earned it a
-       strong reputation for reliability, data integrity, and
-       correctness."""
+    """PostgreSQL is a powerful, open source object-relational database system.
+    It has more than 15 years of active development and a proven architecture
+    that has earned it a strong reputation for reliability, data integrity, and
+    correctness."""
+
     homepage = "http://www.postgresql.org/"
     url      = "http://ftp.postgresql.org/pub/source/v9.3.4/postgresql-9.3.4.tar.bz2"
 
