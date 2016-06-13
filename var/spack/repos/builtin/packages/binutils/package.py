8ff7cbb9cef11181df46c626c298299321ba43c9
Glenn Johnson <glenn-johnson@uiowa.edu>
2016-06-12 21:57:03 -0500

    Run binutils/package.py through autopep8.

--- var/spack/repos/builtin/packages/binutils/package.py
+++ var/spack/repos/builtin/packages/binutils/package.py
@@ -24,11 +24,12 @@
 ##############################################################################
 from spack import *
 
+
 class Binutils(Package):
     """GNU binutils, which contain the linker, assembler, objdump and others"""
-    homepage = "http://www.gnu.org/software/binutils/"
 
-    url="https://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.bz2"
+    homepage = "http://www.gnu.org/software/binutils/"
+    url      = "https://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.bz2"
 
     version('2.26', '64146a0faa3b411ba774f47d41de239f')
     version('2.25', 'd9f3303f802a5b6b0bb73a335ab89d66')
@@ -40,8 +41,10 @@ class Binutils(Package):
     depends_on('flex')
     depends_on('bison')
 
-    # Add a patch that creates binutils libiberty_pic.a which is preferred by OpenSpeedShop and cbtf-krell
-    variant('krellpatch', default=False, description="build with openspeedshop based patch.")
+    # Add a patch that creates binutils libiberty_pic.a which is preferred by
+    # OpenSpeedShop and cbtf-krell
+    variant('krellpatch', default=False,
+            description="build with openspeedshop based patch.")
     variant('gold', default=True, description="build the gold linker")
     patch('binutilskrell-2.24.patch', when='@2.24+krellpatch')
 
