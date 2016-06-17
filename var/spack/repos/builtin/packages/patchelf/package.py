2526f6165d8f5df4517da69fc70cdfd62b33fb22
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-06-17 12:32:07 -0400

    patchelf: Updated to version 0.9

--- var/spack/repos/builtin/packages/patchelf/package.py
+++ var/spack/repos/builtin/packages/patchelf/package.py
@@ -1,13 +1,18 @@
 from spack import *
 
+
 class Patchelf(Package):
     """PatchELF is a small utility to modify the dynamic linker and RPATH of ELF executables."""
 
     homepage = "https://nixos.org/patchelf.html"
-    url      = "http://nixos.org/releases/patchelf/patchelf-0.8/patchelf-0.8.tar.gz"
+    def url_for_version(self, version):
+        return "http://nixos.org/releases/patchelf/" \
+            + "patchelf-%s/patchelf-%s.tar.gz" % (version, version)
+
     list_url = "http://nixos.org/releases/patchelf/"
     list_depth = 2
 
+    version('0.9', '3c265508526760f233620f35d79c79fc')
     version('0.8', '407b229e6a681ffb0e2cdd5915cb2d01')
 
     def install(self, spec, prefix):
