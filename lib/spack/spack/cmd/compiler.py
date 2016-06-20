fe301135021a9f64069a9bf2e3a074c8d4b712ef
Mario Melara <maamelara@gmail.com>
2016-06-20 10:57:10 -0700

    Adding init_config back. Don't initalize config rather let compiler_find do all the work. spack compiler list and spack compilers can do the work

--- lib/spack/spack/cmd/compiler.py
+++ lib/spack/spack/cmd/compiler.py
@@ -76,8 +76,13 @@ def compiler_find(args):
     if not paths:
         paths = get_path('PATH')
 
-    compilers = [c for c in spack.compilers.find_compilers(*args.add_paths)
-                 if c.spec not in spack.compilers.all_compilers(scope=args.scope)]
+    # Don't initialize compilers config via compilers.get_compiler_config. 
+    # Just let compiler_find do the 
+    # entire process and return an empty config from all_compilers
+    # Default for any other process is init_config=True
+    compilers = [c for c in spack.compilers.find_compilers(*paths)
+                 if c.spec not in spack.compilers.all_compilers(
+                     scope=args.scope, init_config=False)] 
     if compilers:
         spack.compilers.add_compilers_to_config(compilers, scope=args.scope)
         n = len(compilers)
