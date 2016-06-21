66089c89aa85369f1e19ae1636ed307e82fdb708
Adam J. Stewart <ajstewart426@gmail.com>
2016-06-21 14:55:49 -0500

    Depend on pil

--- var/spack/repos/builtin/packages/openjpeg/package.py
+++ var/spack/repos/builtin/packages/openjpeg/package.py
@@ -37,9 +37,9 @@ class Openjpeg(Package):
     homepage = "https://github.com/uclouvain/openjpeg"
     url      = "https://github.com/uclouvain/openjpeg/archive/version.2.1.tar.gz"
 
-    version('2.1'  , '3e1c451c087f8462955426da38aa3b3d')
+    version('2.1',   '3e1c451c087f8462955426da38aa3b3d')
     version('2.0.1', '105876ed43ff7dbb2f90b41b5a43cfa5')
-    version('2.0'  , 'cdf266530fee8af87454f15feb619609')
+    version('2.0',   'cdf266530fee8af87454f15feb619609')
     version('1.5.2', '545f98923430369a6b046ef3632ef95c')
     version('1.5.1', 'd774e4b5a0db5f0f171c4fc0aabfa14e')
 
