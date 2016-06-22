67886eaeb95913e9d0f4ab99dd01d8cc97be8a7f
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-06-22 17:12:00 -0400

    graphviz: Added all language binding variants; disabled enough in the default configuration to avoid dependencies.

--- var/spack/repos/builtin/packages/graphviz/package.py
+++ var/spack/repos/builtin/packages/graphviz/package.py
@@ -33,26 +33,56 @@ class Graphviz(Package):
 
     version('2.38.0', '5b6a829b2ac94efcd5fa3c223ed6d3ae', url="http://pkgs.fedoraproject.org/repo/pkgs/graphviz/graphviz-2.38.0.tar.gz/5b6a829b2ac94efcd5fa3c223ed6d3ae/graphviz-2.38.0.tar.gz")
 
-    # By default disable optional Perl language support to prevent build issues
-    # related to missing Perl packages. If spack begins support for Perl in the
-    # future, this package can be updated to depend_on('perl') and the
-    # ncecessary devel packages.
-    variant('perl', default=False, description='Enable if you need the optional Perl language bindings.')  # NOQA: ignore=E501
+    # We try to leave language bindings enabled if they don't cause build issues
+    # or add dependencies.
+    variant('swig', default=False,
+        description='Enable for optional swig language bindings.')
+    variant('sharp', default=True,
+        description='Enable for optional sharp language bindings.')
+    variant('go', default=False,
+        description='Enable for optional go language bindings.')
+    variant('guile', default=True,
+        description='Enable for optional guile language bindings.')
+    variant('io', default=False,
+        description='Enable for optional io language bindings.')
+    variant('java', default=False,  # Spack has no Java support
+        description='Enable for optional java language bindings.')
+    variant('lua', default=True,
+        description='Enable for optional lua language bindings.')
+    variant('ocaml', default=True,
+        description='Enable for optional ocaml language bindings.')
+    variant('perl', default=False,    # Spack has no Perl support
+        description='Enable for optional perl language bindings.')
+    variant('php', default=True,
+        description='Enable for optional php language bindings.')
+    variant('python', default=False,    # Build issues with Python 2/3
+        description='Enable for optional python language bindings.')
+    variant('r', default=True,
+        description='Enable for optional r language bindings.')
+    variant('ruby', default=True,
+        description='Enable for optional ruby language bindings.')
+    variant('tcl', default=True,
+        description='Enable for optional tcl language bindings.')
 
     parallel = False
 
-    depends_on("swig")
-    depends_on("python")
+    depends_on("swig", when='+swig')
+    # Graphviz does not work with Python3
+    depends_on("python@2:2.8", when='+python')
     depends_on("ghostscript")
     depends_on("pkg-config")
 
     def install(self, spec, prefix):
         options = ['--prefix=%s' % prefix]
-        # We don't depend on java, so we can't guarantee that Java is available.
-        options.append('--disable-java')
 
-        if '+perl' not in spec:
-            options.append('--disable-perl')
+
+        vars = ('+swig', '+sharp', '+go', '+guile', '+io',
+            '+java', '+lua', '+ocaml', '+perl', '+php',
+            '+python', '+r', '+ruby', '+tcl')
+
+        for var in vars:
+            enable = 'enable' if (var in spec) else 'disable'
+            options.append('--%s-%s' % (enable, var[1:]))
 
         # On OSX fix the compiler error:
         # In file included from tkStubLib.c:15:
