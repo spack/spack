450c750214267db6b5f4178273a1f46f7d3d207d
Denis Davydov <davydden@gmail.com>
2016-06-23 08:45:44 +0200

    kdiff3: initial version of the package

---
+++ var/spack/repos/builtin/packages/kdiff3/package.py
@@ -0,0 +1,20 @@
+from spack import *
+
+
+class Kdiff3(Package):
+    """Compare and merge 2 or 3 files or directories."""
+    homepage = "http://kdiff3.sourceforge.net/"
+    url      = "https://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/kdiff3-0.9.98.tar.gz"
+
+    version('0.9.98', 'b52f99f2cf2ea75ed5719315cbf77446')
+
+    depends_on("qt@5.2.0:")
+
+    def install(self, spec, prefix):
+        # make is done inside
+        configure('qt4')
+
+        # there is no make install, bummer...
+        mkdirp(self.prefix.bin)
+        install(join_path(self.stage.source_path, 'releaseQt', 'kdiff3'),
+                self.prefix.bin)
