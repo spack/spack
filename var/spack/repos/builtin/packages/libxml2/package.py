81daaddfe91a1684e9d80c9fd42ca6ba2586ee33
Glenn Johnson <glenn-johnson@uiowa.edu>
2016-07-11 20:08:30 -0500

    Use the site_packages_dir from the Python class
    It is not necessary to set site_packages_dir in this package as it is
    already provided by the Python package class.

--- var/spack/repos/builtin/packages/libxml2/package.py
+++ var/spack/repos/builtin/packages/libxml2/package.py
@@ -43,9 +43,6 @@ class Libxml2(Package):
 
     def install(self, spec, prefix):
         if '+python' in spec:
-            site_packages_dir = os.path.join(prefix,
-                                             'lib/python%s/site-packages' %
-                                             (spec['python'].version.up_to(2)))
             python_args = ["--with-python=%s" % spec['python'].prefix,
                            "--with-python-install-dir=%s" % site_packages_dir]
         else:
