955d02049cfdab6ecbe092924a1feb10c743cd8f
Glenn Johnson <glenn-johnson@uiowa.edu>
2016-07-12 12:27:54 -0500

    New package - r-ggvis
    Bring in ggvis and dependencies.
    
    GGvis is an implementaion of the 'Grammar of Graphics', combined with
    shiny.

---
+++ var/spack/repos/builtin/packages/r-httpuv/package.py
@@ -0,0 +1,49 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class RHttpuv(Package):
+    """Provides low-level socket and protocol support for handling HTTP and
+    WebSocket requests directly from within R. It is primarily intended as a
+    building block for other packages, rather than making it particularly easy
+    to create complete web applications using httpuv alone. httpuv is built on
+    top of the libuv and http-parser C libraries, both of which were developed
+    by Joyent, Inc. (See LICENSE file for libuv and http-parser license
+    information.)"""
+
+    homepage = "https://github.com/rstudio/httpuv"
+    url      = "https://cran.r-project.org/src/contrib/httpuv_1.3.3.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/httpuv"
+
+    version('1.3.3', 'c78ae068cf59e949b9791be987bb4489')
+
+    extends('R')
+
+    depends_on('r-rcpp')
+
+    def install(self, spec, prefix):
+        R('CMD', 'INSTALL', '--library={0}'.format(self.module.r_lib_dir),
+          self.stage.source_path)
---
+++ var/spack/repos/builtin/packages/r-shiny/package.py
@@ -0,0 +1,52 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class RShiny(Package):
+    """Makes it incredibly easy to build interactive web applications with R.
+    Automatic "reactive" binding between inputs and outputs and extensive
+    pre-built widgets make it possible to build beautiful, responsive, and
+    powerful applications with minimal effort."""
+
+    homepage = "http://shiny.rstudio.com/"
+    url      = "https://cran.r-project.org/src/contrib/shiny_0.13.2.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/shiny"
+
+    version('0.13.2', 'cb5bff7a28ad59ec2883cd0912ca9611')
+
+    extends('R')
+
+    depends_on('r-httpuv')
+    depends_on('r-mime')
+    depends_on('r-jsonlite')
+    depends_on('r-xtable')
+    depends_on('r-digest')
+    depends_on('r-htmltools')
+    depends_on('r-R6')
+
+    def install(self, spec, prefix):
+        R('CMD', 'INSTALL', '--library={0}'.format(self.module.r_lib_dir),
+          self.stage.source_path)
