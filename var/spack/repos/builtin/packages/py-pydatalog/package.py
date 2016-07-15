f3e95884572ee379b38b6b77516dc4fe6ad3d3fc
Todd Gamblin <tgamblin@llnl.gov>
2016-07-14 23:15:54 -0700

    Fix package name bug.

--- var/spack/repos/builtin/packages/py-pydatalog/package.py
+++ var/spack/repos/builtin/packages/py-pydatalog/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class PyDatalog(Package):
+class PyPydatalog(Package):
     """pyDatalog adds logic programming to Python."""
     homepage = 'https://pypi.python.org/pypi/pyDatalog/'
     url      = 'https://pypi.python.org/packages/09/0b/2670eb9c0027aacfb5b5024ca75e5fee2f1261180ab8797108ffc941158a/pyDatalog-0.17.1.zip'
