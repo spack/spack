a46138dea94cdcddd91f83aaedb7388a0e53f7e4
Paul Hopkins <paul.hopkins@ligo.org>
2016-07-18 13:59:58 +0100

    More flake8 fixes for package-list

--- lib/spack/spack/cmd/package-list.py
+++ lib/spack/spack/cmd/package-list.py
@@ -22,10 +22,8 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-import re
 import cgi
 from StringIO import StringIO
-import llnl.util.tty as tty
 from llnl.util.tty.colify import *
 import spack
 
