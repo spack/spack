9523e50732ff651e1ae073791708c312217bedb1
George Hartzell <hartzell@alerce.com>
2016-07-21 13:58:16 -0400

    Fix shebang line in tcl scripts
    Lmod's configure script goes to the trouble of finding
    tclsh.  This change uses that info to rewrite the #! lines
    in the tcl scripts so that they call the tclsh that the
    configure script discovered.
    
    It needs to massage the existing shebang lines into something
    that the sed statement in the makefile can manipulate and
    it needs to add the path_to_tclsh info into the set of sed
    statements.
    
    Checked with versions 6.4.1 and 6.3.7 (the checksum for 6.0.1 is
    incorrect, a fix for another time).

--- var/spack/repos/builtin/packages/lmod/package.py
+++ var/spack/repos/builtin/packages/lmod/package.py
@@ -23,7 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-
+from glob import glob
 
 class Lmod(Package):
     """
@@ -53,6 +53,15 @@ class Lmod(Package):
             self.stage.path, 'Lmod-{version}', 'src', '?.lua')
         spack_env.append_path('LUA_PATH', stage_lua_path.format(
             version=self.version), separator=';')
+
+    patch('fix_tclsh_paths.patch')
+    def patch(self):
+        """The tcl scripts should use the tclsh that was discovered
+           by the configure script.  Touch up their #! lines so that the
+           sed in the Makefile's install step has something to work on.
+           Requires the change in the associated patch file.fg"""
+        for tclscript in glob('src/*.tcl'):
+            filter_file(r'^#!.*tclsh', '#!@path_to_tclsh@', tclscript)
 
     def install(self, spec, prefix):
         configure('--prefix=%s' % prefix)
