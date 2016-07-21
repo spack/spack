812e1bf5b3675cfac36970d53b2fc9ccd2071466
Michael Kuhn <suraia@ikkoku.de>
2016-07-21 13:47:48 +0200

    Improve distribution detection.
    - Lowercase distribution name for consistency.
    - Support Ubuntu's YY.MM version scheme.

--- lib/spack/spack/operating_systems/linux_distro.py
+++ lib/spack/spack/operating_systems/linux_distro.py
@@ -2,6 +2,7 @@ import re
 import platform as py_platform
 from spack.architecture import OperatingSystem
 
+
 class LinuxDistro(OperatingSystem):
     """ This class will represent the autodetected operating system
         for a Linux System. Since there are many different flavors of
@@ -13,10 +14,17 @@ class LinuxDistro(OperatingSystem):
         distname, version, _ = py_platform.linux_distribution(
             full_distribution_name=False)
 
+        distname = distname.lower()
+
         # Grabs major version from tuple on redhat; on other platforms
         # grab the first legal identifier in the version field.  On
         # debian you get things like 'wheezy/sid'; sid means unstable.
         # We just record 'wheezy' and don't get quite so detailed.
-        version = re.split(r'[^\w-]', version)[0]
+        version = re.split(r'[^\w-]', version)
+
+        if 'ubuntu' in distname:
+            version = '.'.join(version[0:2])
+        else:
+            version = version[0]
 
         super(LinuxDistro, self).__init__(distname, version)
