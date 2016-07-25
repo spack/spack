bfcec696308ee8bfd226a54c17a7e15d49e2aed7
George Hartzell <hartzell@alerce.com>
2016-07-25 14:17:44 -0400

    Add standard header, use spack helpers
    Added the standard header (stolen from R).
    
    Touched up the install to use set_executable rather than doing it
    myself.

--- var/spack/repos/builtin/packages/nextflow/package.py
+++ var/spack/repos/builtin/packages/nextflow/package.py
@@ -1,6 +1,29 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
 from spack import *
-from glob import glob
-import os
+
 
 class Nextflow(Package):
     """Data-driven computational pipelines"""
@@ -17,8 +40,6 @@ class Nextflow(Package):
         pass
 
     def install(self, spec, prefix):
-        chmod = which('chmod')
-
         mkdirp(prefix.bin)
         install("nextflow", join_path(prefix.bin, "nextflow"))
-        chmod('+x', join_path(prefix.bin, "nextflow"))
+        set_executable( join_path(prefix.bin, "nextflow"))
