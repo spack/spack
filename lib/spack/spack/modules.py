9c7b98dcc80d3bb3b5a09d06ec81da12b8761fb2
alalazo <massimiliano.culpo@googlemail.com>
2016-07-26 09:03:17 +0200

    modules : handle dashes in environment variables

--- lib/spack/spack/modules.py
+++ lib/spack/spack/modules.py
@@ -287,7 +287,7 @@ class EnvModule(object):
     def upper_tokens(self):
         """Tokens that can be substituted in environment variable names"""
         upper_tokens = {
-            'name': self.spec.name.upper()
+            'name': self.spec.name.replace('-', '_').upper()
         }
         return upper_tokens
 
