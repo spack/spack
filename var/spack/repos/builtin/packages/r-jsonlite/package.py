12d126398066379471f395529a3eac0476c9fdb9
George Hartzell <hartzell@alerce.com>
2016-07-26 12:57:33 -0400

    Update several R packages (r-shiny prereqs)
    r-jsonlite 0.0.21 -> 1.0
    r-mime     0.4    -> 0.5
    rcpp       0.12.5 -> 0.12.6
    
    CRAN is funny.  The older versions of these packages are still available
    in package specific directories but the current version is not there, so
    I don't see any way to make the older versions work.

--- var/spack/repos/builtin/packages/r-jsonlite/package.py
+++ var/spack/repos/builtin/packages/r-jsonlite/package.py
@@ -38,10 +38,10 @@ class RJsonlite(Package):
     use with dynamic data in systems and applications."""
 
     homepage = "https://github.com/jeroenooms/jsonlite"
-    url      = "https://cran.r-project.org/src/contrib/jsonlite_0.9.21.tar.gz"
+    url      = "https://cran.r-project.org/src/contrib/jsonlite_1.0.tar.gz"
     list_url = "https://cran.r-project.org/src/contrib/Archive/jsonlite"
 
-    version('0.9.21', '4fc382747f88a79ff0718a0d06bed45d')
+    version('1.0', 'c8524e086de22ab39b8ac8000220cc87')
 
     extends('R')
 
