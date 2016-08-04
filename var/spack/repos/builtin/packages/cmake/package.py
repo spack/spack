b47357d73c66c3ae9f2a5fa6bcba011b14f1d162
Adam J. Stewart <ajstewart426@gmail.com>
2016-08-04 15:07:43 -0500

    Add patch for Intel compilers

--- var/spack/repos/builtin/packages/cmake/package.py
+++ var/spack/repos/builtin/packages/cmake/package.py
@@ -67,6 +67,10 @@ class Cmake(Package):
     depends_on('openssl',        when='+openssl')
     depends_on('ncurses',        when='+ncurses')
 
+    # Cannot build with Intel, should be fixed in 3.6.2
+    # https://gitlab.kitware.com/cmake/cmake/issues/16226
+    patch('intel-c-gnu11.patch', when='@3.6.0:3.6.1')
+
     def url_for_version(self, version):
         """Handle CMake's version-based custom URLs."""
         return 'https://cmake.org/files/v%s/cmake-%s.tar.gz' % (
