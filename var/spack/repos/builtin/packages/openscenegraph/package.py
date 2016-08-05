065e9daa33d96a15382ca6c7249b1aeb242d64d8
Joseph Ciurej <ciurej1@llnl.gov>
2016-08-05 14:38:25 -0700

    Fixed up a few remaining style issues in the 'openscenegraph' package.

--- var/spack/repos/builtin/packages/openscenegraph/package.py
+++ var/spack/repos/builtin/packages/openscenegraph/package.py
@@ -22,23 +22,6 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-################################################################################
-# Copyright (c) 2015-2016 Krell Institute. All Rights Reserved.
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-# Place, Suite 330, Boston, MA  02111-1307  USA
-################################################################################
 
 from spack import *
 
@@ -47,9 +30,8 @@ class Openscenegraph(Package):
     """TODO(JRC)"""
 
     homepage = "http://www.openscenegraph.org"
-    url      = "http://trac.openscenegraph.org/downloads/developer_releases/OpenSceneGraph-3.4.0.zip"
+    url      = "http://trac.openscenegraph.org/downloads/developer_releases/OpenSceneGraph-3.2.3.zip"
 
-    version('3.4.0', 'a5e762c64373a46932e444f6f7332496')
     version('3.2.3', '02ffdad7744c747d8fad0d7babb58427')
     version('3.1.5', '1c90b851b109849c985006486ef59822')
 
@@ -61,11 +43,14 @@ class Openscenegraph(Package):
     depends_on('zlib')
 
     def install(self, spec, prefix):
+        build_type = 'Debug' if '+debug' in spec else 'Release'
+        shared_status = 'ON' if '+shared' in spec else 'OFF'
+
         cmake_args = std_cmake_args[:]
         cmake_args.extend([
-            '-DCMAKE_BUILD_TYPE=%s' % ('Release' if '+debug' in spec else 'Debug'),
-            '-DDYNAMIC_OPENSCENEGRAPH=%s' % ('ON' if '+shared' in spec else 'OFF'),
-            '-DDYNAMIC_OPENTHREADS=%s' % ('ON' if '+shared' in spec else 'OFF'),
+            '-DCMAKE_BUILD_TYPE={0}'.format(build_type),
+            '-DDYNAMIC_OPENSCENEGRAPH={0}'.format(shared_status),
+            '-DDYNAMIC_OPENTHREADS={0}'.format(shared_status),
         ])
 
         source_directory = self.stage.source_path
@@ -73,18 +58,17 @@ class Openscenegraph(Package):
         with working_dir(build_directory, create=True):
             cmake(
                 source_directory,
-                '-DCMAKE_INSTALL_PREFIX=%s' % prefix,
-                '-DCMAKE_C_COMPILER=%s' % self.compiler.cc,
-                '-DCMAKE_CXX_COMPILER=%s' % self.compiler.cxx,
-                '-DZLIB_INCLUDE_DIR=%s' % spec['zlib'].prefix.include,
-                '-DZLIB_LIBRARY=%s/libz.so' % spec['zlib'].prefix.lib,
+                '-DCMAKE_INSTALL_PREFIX={0}'.format(prefix),
+                '-DCMAKE_C_COMPILER={0}'.format(self.compiler.cc),
+                '-DCMAKE_CXX_COMPILER={0}'.format(self.compiler.cxx),
+                '-DZLIB_INCLUDE_DIR={0}'.format(spec['zlib'].prefix.include),
+                '-DZLIB_LIBRARY={0}/libz.so'.format(spec['zlib'].prefix.lib),
                 '-DBUILD_OSG_APPLICATIONS=OFF',
                 '-DOSG_NOTIFY_DISABLED=ON',
                 '-DLIB_POSTFIX=',
                 '-DFFMPEG_LIBAVCODEC_INCLUDE_DIRS=',
                 '-DFFMPEG_LIBAVFORMAT_INCLUDE_DIRS=',
                 '-DFFMPEG_LIBAVUTIL_INCLUDE_DIRS=',
-                '-DCMAKE_VERBOSE_MAKEFILE:BOOL=TRUE', # TODO(JRC): Remove
                 *cmake_args
             )
             make()
