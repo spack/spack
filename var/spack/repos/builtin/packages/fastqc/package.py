60d2d26caca68e79707cee67558197d25382d04c
George Hartzell <hartzell@alerce.com>
2016-08-09 18:24:23 -0400

    Add package for fastqc
    This tool is a java mess.  Their Way To Do It is to just copy the entire
    tree into it's final resting place, make the perl script at the top
    level executable and take it from there.
    
    Yuck.
    
    This package assumes that `set_executable` actually sets all the user
    bits.  If that change doesn't go in, then something equivalent needs to
    be done.

---
+++ var/spack/repos/builtin/packages/fastqc/package.py
@@ -0,0 +1,51 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+from distutils.dir_util import copy_tree
+
+# FIXME: make sure set_executable fix goes in first.
+class Fastqc(Package):
+    """A quality control tool for high throughput sequence data."""
+
+    homepage = "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/"
+    url      = "http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.zip"
+
+    version('0.11.5', '3524f101c0ab0bae77c7595983170a76')
+
+    depends_on('jdk')
+    depends_on('perl')          # for fastqc "script", any perl will do
+
+    def install(self, spec, prefix):
+        # ick...
+        copy_tree('.', self.prefix)
+        # this assumes "set_executable" == "chmod +x..."
+        set_executable(join_path(prefix, 'fastqc'))
+
+    def setup_environment(self, spack_env, env):
+        """Add <prefix> to the path; the package has a script at the
+           top level."""
+
+        env.prepend_path('PATH', self.prefix)
+        env.prepend_path('PATH', join_path(self.spec['jdk'].prefix, 'bin'))
