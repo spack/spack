f147d645272bc96e57c707f39a80b64d1e637fb1
Adam J. Stewart <ajstewart426@gmail.com>
2016-08-11 11:01:57 -0500

    Rebase messed up flake8

--- var/spack/repos/builtin/packages/py-matplotlib/package.py
+++ var/spack/repos/builtin/packages/py-matplotlib/package.py
@@ -26,7 +26,6 @@ from spack import *
 import os
 
 
-
 class PyMatplotlib(Package):
     """matplotlib is a python 2D plotting library which produces publication
     quality figures in a variety of hardcopy formats and interactive
