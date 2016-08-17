4d12c5455a18e14b77badca5e27743af47339947
Adam J. Stewart <ajstewart426@gmail.com>
2016-08-17 13:03:56 -0500

    Fix NetCDF/HDF5 dependency resolution problems

--- var/spack/repos/builtin/packages/netcdf/package.py
+++ var/spack/repos/builtin/packages/netcdf/package.py
@@ -50,10 +50,15 @@ class Netcdf(Package):
 
     # Required for NetCDF-4 support
     depends_on("zlib")
-    depends_on('hdf5@:1.8+mpi', when='@:4.4.0+mpi')
-    depends_on('hdf5+mpi', when='@4.4.1:+mpi')
-    depends_on('hdf5@:1.8~mpi', when='@:4.4.0~mpi')
-    depends_on('hdf5~mpi', when='@4.4.1:~mpi')
+    depends_on('hdf5')
+
+    # Variant forwarding
+    depends_on('hdf5+mpi',  when='+mpi')
+    depends_on('hdf5~mpi',  when='~mpi')
+
+    # NetCDF 4.4.0 and prior have compatibility issues with HDF5 1.10 and later
+    # https://github.com/Unidata/netcdf-c/issues/250
+    depends_on('hdf5@:1.8', when='@:4.4.0')
 
     def install(self, spec, prefix):
         # Environment variables
