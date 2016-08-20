57b2d0a7acf31746dff84f486e4c7e434c57e30a
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-08-19 23:20:04 -0400

    openssl: Removed url_for_version(), paralleling fix in main Spack repo.

--- var/spack/repos/builtin/packages/openssl/package.py
+++ var/spack/repos/builtin/packages/openssl/package.py
@@ -48,12 +48,6 @@ class Openssl(Package):
     depends_on("zlib")
     parallel = False
 
-    def url_for_version(self, version):
-        if '@system' in self.spec:
-            return '@system (reserved version for system openssl)'
-        else:
-            return super(Openssl, self).url_for_version(self.version)
-
     def handle_fetch_error(self, error):
         tty.warn("Fetching OpenSSL failed. This may indicate that OpenSSL has "
                  "been updated, and the version in your instance of Spack is "
