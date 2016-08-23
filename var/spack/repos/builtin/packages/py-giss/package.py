1d2165648cc0b62e1126898e5b886455c43c23c6
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-08-23 16:24:21 -0400

    py-giss: Use setup_py()

--- var/spack/repos/builtin/packages/py-giss/package.py
+++ var/spack/repos/builtin/packages/py-giss/package.py
@@ -24,4 +24,4 @@ class PyGiss(Package):
     depends_on('py-six')
 
     def install(self, spec, prefix):
-        python('setup.py', 'install', '--prefix=%s' % prefix)
+        setup_py('install', '--prefix=%s' % prefix)
