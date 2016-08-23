29af5ce16f0fde4b42dc1c1a298d9e9cd42b4cdb
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-08-23 18:08:24 -0400

    modele-control: Added new package

---
+++ var/spack/repos/builtin/packages/modele-control/package.py
@@ -0,0 +1,18 @@
+from spack import *
+
+class ModeleControl(Package):
+    """Misc. Python Stuff."""
+
+    homepage = "https://github.com/citibeth/modele-control"
+    url      = "https://github.com/citibeth/modele-control/tarball/v0.1.0"
+
+    version('develop', git='https://github.com/citibeth/modele-control.git', branch='develop')
+
+    extends('python')
+    depends_on('python@:2.8')
+
+    # depends_on('binutils', type='run')    # ldd; assume already installed on system
+    depends_on('netcdf', type='run')    # ncdump
+
+    def install(self, spec, prefix):
+        setup_py('install', '--prefix=%s' % prefix)
