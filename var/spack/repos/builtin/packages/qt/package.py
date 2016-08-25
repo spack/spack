aa9faceb0332e63c2dd83d90887e03ad6c2eb837
Ben Boeckel <ben.boeckel@kitware.com>
2016-08-25 10:42:40 -0400

    qt: disable phonon support

--- var/spack/repos/builtin/packages/qt/package.py
+++ var/spack/repos/builtin/packages/qt/package.py
@@ -156,6 +156,7 @@ class Qt(Package):
             '-shared',
             '-confirm-license',
             '-openssl-linked',
+            '-no-phonon',
             '-optimized-qmake',
             '-no-openvg',
             '-no-pch',
