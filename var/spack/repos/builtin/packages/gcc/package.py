f784173aa799a24c3de98e466c69b055805da4d9
Patrick Gartung <gartung@fnal.gov>
2016-08-30 12:51:14 -0500

    spack specifies full path to lib64

--- var/spack/repos/builtin/packages/gcc/package.py
+++ var/spack/repos/builtin/packages/gcc/package.py
@@ -64,7 +64,7 @@ class Gcc(Package):
                     string=True)
 
         # Make libgcc_s relocatable
-        filter_file(r"@shlib_slibdir@", "@rpath/lib64", 
+        filter_file(r"@shlib_slibdir@", "@rpath/", 
             'libgcc/config/t-slibgcc-darwin',string=True)
 
         enabled_languages = set(('c', 'c++', 'fortran', 'java', 'objc'))
