497bbb3ae319f0bad23e7e6122560d541a32f1f3
Massimiliano Culpo <massimiliano.culpo@googlemail.com>
2016-09-03 17:13:47 +0200

    added package : pexsi (#1550)
    * pexsi : added package
    * cp2k : added pexsi (works for gcc+openmpi with and without plumed)

--- var/spack/repos/builtin/packages/mvapich2/package.py
+++ var/spack/repos/builtin/packages/mvapich2/package.py
@@ -227,6 +227,10 @@ class Mvapich2(Package):
         self.spec.mpicxx = join_path(self.prefix.bin, 'mpicxx')
         self.spec.mpifc  = join_path(self.prefix.bin, 'mpif90')
         self.spec.mpif77 = join_path(self.prefix.bin, 'mpif77')
+        self.spec.mpicxx_shared_libs = [
+            join_path(self.prefix.lib, 'libmpicxx.{0}'.format(dso_suffix)),
+            join_path(self.prefix.lib, 'libmpi.{0}'.format(dso_suffix))
+        ]
 
     def install(self, spec, prefix):
         # we'll set different configure flags depending on our
