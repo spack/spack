deb1cb4805a9f02330a2380ccd68b72a48c84f13
Adam J. Stewart <ajstewart426@gmail.com>
2016-09-29 14:35:10 -0500

    Add libint package (#1264)
    * Add libint package
    
    * Add Intel optimization flags recommended by CP2K
    
    * Add new version and Intel compiler optimization flags for libxc
    
    * Add older version of libint
    
    * Libint depends on GMP C++ library

--- var/spack/repos/builtin/packages/libxc/package.py
+++ var/spack/repos/builtin/packages/libxc/package.py
@@ -32,11 +32,27 @@ class Libxc(Package):
     homepage = "http://www.tddft.org/programs/octopus/wiki/index.php/Libxc"
     url      = "http://www.tddft.org/programs/octopus/down.php?file=libxc/libxc-2.2.2.tar.gz"
 
+    version('3.0.0', '8227fa3053f8fc215bd9d7b0d36de03c')
     version('2.2.2', 'd9f90a0d6e36df6c1312b6422280f2ec')
 
     def install(self, spec, prefix):
-        configure('--prefix=%s' % prefix,
+        # Optimizations for the Intel compiler, suggested by CP2K
+        optflags = '-O2'
+        if self.compiler.name == 'intel':
+            optflags += ' -xAVX -axCORE-AVX2 -ipo'
+            if which('xiar'):
+                env['AR'] = 'xiar'
+
+        env['CFLAGS']  = optflags
+        env['FCFLAGS'] = optflags
+
+        configure('--prefix={0}'.format(prefix),
                   '--enable-shared')
 
         make()
-        make("install")
+
+        # libxc provides a testsuite, but many tests fail
+        # http://www.tddft.org/pipermail/libxc/2013-February/000032.html
+        # make('check')
+
+        make('install')
