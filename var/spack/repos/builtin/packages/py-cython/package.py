31738cfd7b20d1b11a4615e3a4760248cadc8b52
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-10-05 16:59:12 -0400

    Add binutils dependencies for packages that really do not like old/buggy binutils.

--- var/spack/repos/builtin/packages/py-cython/package.py
+++ var/spack/repos/builtin/packages/py-cython/package.py
@@ -38,6 +38,7 @@ class PyCython(Package):
     version('0.21.2', 'd21adb870c75680dc857cd05d41046a4')
 
     extends('python')
+    depends_on('binutils', type='build')
 
     def install(self, spec, prefix):
         python('setup.py', 'install', '--prefix=%s' % prefix)
