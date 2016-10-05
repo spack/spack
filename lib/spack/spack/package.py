015e29efe105ddd039e8b395e12cf78a3787ebb3
Elizabeth Fischer <rpf2116@columbia.edu>
2016-10-05 16:00:27 -0400

    Documentation Improvements for SC16 (#1676)
    * Transferred pending changes from efischer/develop
    
    * 1. Rewrite of "Getting Started": everything you need to set up Spack, even on old/ornery systems.  This is not a reference manual section; items covered here are covered more systematically elsewhere in the manual.  Some sections were moved here from elsewhere.
    
    2. Beginning to write three methods of application developer support.  Two methods were moved from elsewhere.
    
    * Edits...
    
    * Moved sections in preparation for additional text to be added from old efischer/docs branch.
    
    * Moved 2 more sections.
    
    * Avoid accid
    
    * Applied proofreading edits from @adamjstewart
    
    * Fixed non-standard section characters.
    
    * Moved section on profiling to the developer's guide.
    
    * Still working on Spack workflows...
    
    * Finished draft of packaging_guide.rst
    
    * Renamed sample projects.
    
    * Updates to docstrings
    
    * Added documentation to resolve #638 (content taken from #846)
    
    * Added section on resolving inconsistent run dependencies.  Addresses #645
    
    * Showed how to build Python extensions only compatible with certain versions of Python.
    
    * Added examples of getting the right behavior from depends_on().  See #1035
    
    * Added section on Intel compilers and their GCC masquerading feature.  Addresses #638, #1687.
    
    * Fixed formatting
    
    * Added fixes to filesystem views.  Added a caveats section to ``spack setup``.
    
    * Updated section on Intel compiler configuration because compiler flags currently do not work (see #1687)
    
    * Defined trusted downloads, and updated text based on them. (See #1696)
    
    * Added workflow to deal with buggy upstream software.  See #1683
    
    * Added proper separation between Spack Docs vs. Reference Manual
    
    * Renamed spack_workflows to workflows.  Resolves a conflict with the .gitignore file.
    
    * Removed repeated section.
    
    * Created new "Vendor Specific Compiler Configuration" section and organized existing Intel section into it.  Added new PGI and NAG sections; but they need to be expanded  / rewritten based on the existing text plus research through Spack issues on GitHub.
    
    * Fixed text on `spack load --dependencies` to conform to reality.  See #1662
    
    * Added patching as option for upstream bugfixes.
    
    * Added section on using licensed compilers.
    
    * Added section on non-downloadable tarballs.
    
    * Wrote sections on NAG and PGI.  Arranged compilers in alphabetical order.
    
    * Fix indent.
    
    * Fixed typos.
    
    * Clarified dependency types.
    
    * Applied edits from Adam J. Stewart.  Spellchecked workflows and getting_started.
    
    * Removed spurious header
    
    * Fixed Sphinx errors
    
    * Fixed erroneous symbol in docstring.
    
    * Fix many typos and formatting problems.
    
    * Spacing changes
    
    * Added section on fixing Git problems.  See #1779
    
    * Fixed signature of install() method.
    
    * Addressed system packages in greater detail.  See #1794 #1795
    
    * Fixed typos
    
    * Fixed quotes
    
    * Duplicate section on Spack profiling removed from configuration.rst.  It had earlier been moved to developer_guide.rst, where it fits better.
    
    * Minor edits
    
    - Tweak supported platform language.
    - Various small changes to the new getting started guide.
    
    * Fixed bug with quotes.

--- lib/spack/spack/package.py
+++ lib/spack/spack/package.py
@@ -482,8 +482,13 @@ class Package(object):
 
     # TODO: move this out of here and into some URL extrapolation module?
     def url_for_version(self, version):
-        """
-        Returns a URL that you can download a new version of this package from.
+        """Returns a URL from which the specified version of this package
+        may be downloaded.
+
+        version: class Version
+            The version for which a URL is sought.
+
+        See Class Version (version.py)
         """
         if not isinstance(version, Version):
             version = Version(version)
