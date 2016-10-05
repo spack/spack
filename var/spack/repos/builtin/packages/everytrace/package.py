5d038fd0579e8f0474f2fe1e2443de6857cafec2
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-10-05 12:21:34 -0400

    flake8

--- var/spack/repos/builtin/packages/everytrace/package.py
+++ var/spack/repos/builtin/packages/everytrace/package.py
@@ -23,8 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import llnl.util.tty as tty
-import os
 
 
 class Everytrace(CMakePackage):
