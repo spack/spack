2e5a96592a5d9d778e81134bef9cbaf53eeff137
Adam J. Stewart <ajstewart426@gmail.com>
2016-10-05 10:37:51 -0500

    Add latest versions, resolve variant resolution bug

--- var/spack/repos/builtin/packages/gsl/package.py
+++ var/spack/repos/builtin/packages/gsl/package.py
@@ -38,6 +38,7 @@ class Gsl(Package):
     homepage = "http://www.gnu.org/software/gsl"
     url      = "http://mirror.switch.ch/ftp/mirror/gnu/gsl/gsl-2.1.tar.gz"
 
+    version('2.2.1', '3d90650b7cfe0a6f4b29c2d7b0f86458')
     version('2.1', 'd8f70abafd3e9f0bae03c52d1f4e8de5')
     version('2.0', 'ae44cdfed78ece40e73411b63a78c375')
     version('1.16', 'e49a664db13d81c968415cd53f62bc8b')
