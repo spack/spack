42c69b3809c09a3d86e85c237180872ad95d9036
Elizabeth Fischer <rpf2116@columbia.edu>
2016-10-05 13:03:05 -0400

    everytrace: New package (#1543)
    * everytrace: New package
    
    Everytrace ensures that stack trace is obtained every time a program exits, for whatever reason.
    
    * everytrace: Change CMake to build dependency
    
    * Renamed to everytrace-example, flake8 and copyright issues.
    
    * flake8
    
    * Missing type=build

---
+++ var/spack/repos/builtin/packages/everytrace-example/package.py
@@ -0,0 +1,46 @@
+##############################################################################
+# Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class EverytraceExample(CMakePackage):
+    """Get stack trace EVERY time a program exits."""
+
+    homepage = "https://github.com/citibeth/everytrace-example"
+    version('develop',
+            git='https://github.com/citibeth/everytrace-example.git',
+            branch='develop')
+
+    depends_on('cmake', type='build')
+    depends_on('everytrace+mpi+fortran')
+
+    # Currently the only MPI this everytrace works with.
+    depends_on('openmpi')
+
+    def configure_args(self):
+        return []
+
+    def setup_environment(self, spack_env, env):
+        env.prepend_path('PATH', join_path(self.prefix, 'bin'))
