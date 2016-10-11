802e9433651228b90c6badf5c689b4af5dca9057
Luigi Calori <luigi.calori@gmail.com>
2016-10-11 20:15:54 +0200

    adding newer version of turbovnc annd libjpeg-turbo

--- var/spack/repos/builtin/packages/turbovnc/package.py
+++ var/spack/repos/builtin/packages/turbovnc/package.py
@@ -1,3 +1,27 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
 from spack import *
 
 class Turbovnc(Package):
@@ -9,13 +33,12 @@ class Turbovnc(Package):
     homepage = "http://www.turbovnc.org/"
     url      = "http://downloads.sourceforge.net/project/turbovnc/2.0.1/turbovnc-2.0.1.tar.gz"
 
-    version('2.0.1', 'a279fdb9ac86a1ebe82f85ab68353dcc',
-            url      = "http://downloads.sourceforge.net/project/turbovnc/2.0.1/turbovnc-2.0.1.tar.gz"
-    )
+
+    version('2.0.1', 'a279fdb9ac86a1ebe82f85ab68353dcc')
     version('2.0.91', '1e203cc533103cf7bd1186c7fd620186',
-            url      = "http://downloads.sourceforge.net/project/turbovnc/2.0.91%20%282.1beta2%29/turbovnc-2.0.91.tar.gz"
-            
+            url      = "http://downloads.sourceforge.net/project/turbovnc/2.0.91%20%282.1beta2%29/turbovnc-2.0.91.tar.gz"            
     )
+    version('2.1', '6748bb13647d318f0c932394f8298d10')
 
     # FIXME: Add dependencies if this package requires them.
     variant('java', default=False, description='Enable Java build')
@@ -23,7 +46,14 @@ class Turbovnc(Package):
    
     depends_on('cmake', type='build')
     depends_on("libjpeg-turbo")
+    #depends_on("libjpeg-turbo@1.5.1", when='@2.1')
     depends_on("openssl")
+
+    def url_for_version(self, version):
+        """Handle TurboVNC's version-based custom URLs."""
+        return '"http://downloads.sourceforge.net/project/turbovnc/%s/turbovnc-%s.tar.gz' % (
+            version, version)
+
 
     def install(self, spec, prefix):
         def feature_to_bool(feature, on='ON', off='OFF'):
