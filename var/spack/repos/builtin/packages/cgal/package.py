de973222599a6c5d920cf2b22601bd79a4eed3e0
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-10-12 13:59:48 -0400

    cgal: Reverted to not require Qt5

--- var/spack/repos/builtin/packages/cgal/package.py
+++ var/spack/repos/builtin/packages/cgal/package.py
@@ -22,57 +22,44 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+
 from spack import *
 
 
 class Cgal(Package):
-    """The Computational Geometry Algorithms Library (CGAL) is a C++ library
-    that aims to provide easy access to efficient and reliable algorithms in
-    computational geometry. CGAL is used in various areas needing geometric
-    computation, such as geographic information systems, computer aided design,
-    molecular biology, medical imaging, computer graphics, and robotics.
+    """CGAL is a software project that provides easy access to efficient and
+       reliable geometric algorithms in the form of a C++ library. CGAL
+       is used in various areas needing geometric computation, such as
+       geographic information systems, computer aided design, molecular
+       biology, medical imaging, computer graphics, and robotics.
+
     """
     homepage = 'http://www.cgal.org/'
     url = 'https://github.com/CGAL/cgal/archive/releases/CGAL-4.7.tar.gz'
 
-    version('4.9', '7b628db3e5614347f776c046b7666089')
     version('4.7', '4826714810f3b4c65cac96b90fb03b67')
     version('4.6.3', 'e8ee2ecc8d2b09b94a121c09257b576d')
 
+    # Installation instructions :
+    # http://doc.cgal.org/latest/Manual/installation.html
     variant('shared', default=True,
             description='Enables the build of shared libraries')
     variant('debug', default=False,
             description='Builds a debug version of the libraries')
 
-    # Essential Third Party Libraries
     depends_on('boost')
-    depends_on('gmp')
     depends_on('mpfr')
+    depends_on('gmp')
     depends_on('zlib')
-    # depends_on('opengl')
-    depends_on('qt@5:')
-
-    # Optional Third Party Libraries
-    # depends_on('leda')
-    # depends_on('mpfi')
-    # depends_on('rs')
-    # depends_on('rs3')
-    # depends_on('ntl')
-    # depends_on('eigen')
-    # depends_on('libqglviewer')
-    # depends_on('esbtl')
-    # depends_on('intel-tbb')
-
-    # Build dependencies
     depends_on('cmake', type='build')
 
+    # FIXME : Qt5 dependency missing (needs Qt5 and OpenGL)
+    # FIXME : Optional third party libraries missing
+
     def install(self, spec, prefix):
-        # Installation instructions:
-        # http://doc.cgal.org/latest/Manual/installation.html
 
         options = []
         options.extend(std_cmake_args)
-
         # CGAL supports only Release and Debug build type. Any other build type
         # will raise an error at configure time
         if '+debug' in spec:
@@ -85,7 +72,9 @@ class Cgal(Package):
         else:
             options.append('-DBUILD_SHARED_LIBS:BOOL=OFF')
 
-        with working_dir('spack-build', create=True):
-            cmake('..', *options)
+        build_directory = join_path(self.stage.path, 'spack-build')
+        source_directory = self.stage.source_path
+        with working_dir(build_directory, create=True):
+            cmake(source_directory, *options)
             make()
-            make('install')
+            make("install")
