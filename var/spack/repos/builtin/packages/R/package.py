9194fcd4b5be924295b03040e1721991d2ad38d2
George Hartzell <george.hartzell.gh1@contractors.roche.com>
2016-10-21 22:20:16 +0000

    Conditionalize X stuff on variant
    Another step towards hooking up the X stuff.
    
    There's still discussion happening on the issue, but this is better
    than the 'everything must build with X' state that it was in.

--- var/spack/repos/builtin/packages/R/package.py
+++ var/spack/repos/builtin/packages/R/package.py
@@ -50,6 +50,8 @@ class R(Package):
 
     variant('external-lapack', default=False,
             description='Links to externally installed BLAS/LAPACK')
+    variant('X', default=True,
+            description='Enable X11 support (call configure --with-x)')
 
     # Virtual dependencies
     depends_on('blas', when='+external-lapack')
@@ -65,12 +67,16 @@ class R(Package):
     depends_on('libtiff')
     depends_on('jpeg')
     depends_on('cairo')
+    depends_on('cairo+X', when='+X')
+    depends_on('cairo~X', when='~X')
     depends_on('pango')
     depends_on('freetype')
     depends_on('tcl')
     depends_on('tk')
-    depends_on('libx11')
-    depends_on('libxt')
+    depends_on('tk+X', when='+X')
+    depends_on('tk~X', when='~X')
+    depends_on('libx11', when='+X')
+    depends_on('libxt', when='+X')
     depends_on('curl')
     depends_on('pcre')
     depends_on('jdk')
