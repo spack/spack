0a59a697797b75e72f79a61e42ac678e836dd746
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2016-10-23 10:50:39 -0400

    flake8 + code review fixes.

--- var/spack/repos/builtin/packages/issm/package.py
+++ var/spack/repos/builtin/packages/issm/package.py
@@ -1,3 +1,27 @@
+##############################################################################
+# Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
 from spack import *
 
 # TODO:
@@ -25,14 +49,14 @@ class Issm(Package):
     depends_on("mumps")
     depends_on("metis")
 
-#!!    depends_on("m1qn3")
-#!!    depends_on("triangle")
+    #!! depends_on("m1qn3")
+    #!! depends_on("triangle")
 
     depends_on("python@2:", when="+python")
     depends_on("py-nose", when="+python")
     depends_on("py-numpy", when="+python")
     depends_on("py-scipy", when="+python")
-#!!    depends_on("py-netcdf", when="+python")
+    #!! depends_on("py-netcdf", when="+python")
 
     depends_on("matlab", when="+matlab")
 
@@ -53,13 +77,16 @@ class Issm(Package):
             "--with-m1qn3-dir=%s" % spec['m1qn3'].prefix]
 
         if '+python' in spec:
-            config_args.append("--with-python-dir=%s" % spec['python'].prefix)
-            config_args.append("--with-python-numpy-dir=%s" % spec['py-numpy'].prefix)
+            config_args.append(
+                "--with-python-dir=%s" % spec['python'].prefix)
+            config_args.append(
+                "--with-python-numpy-dir=%s" % spec['py-numpy'].prefix)
 
         if '+matlab' in spec:
-            configure_args.append("--with-matlab-dir=%s" % spec['matlab'].prefix)
+            configure_args.append(
+                "--with-matlab-dir=%s" % spec['matlab'].prefix)
 
-        which('autoreconf')('-iv')
+        autoreconf('-iv')
         configure(*configure_args)
         make()
         make("install")
