9a9b54134e230ca6f8e1df70396ab55843c57851
Alicia Klinvex <amklinv@sandia.gov>
2016-10-28 13:15:07 -0600

    Added support for xSDKTrilinos package

---
+++ var/spack/repos/builtin/packages/xsdktrilinos/package.py
@@ -0,0 +1,121 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+import os
+import sys
+
+# Trilinos is complicated to build, as an inspiration a couple of links to
+# other repositories which build it:
+# https://github.com/hpcugent/easybuild-easyblocks/blob/master/easybuild/easyblocks/t/trilinos.py#L111
+# https://github.com/koecher/candi/blob/master/deal.II-toolchain/packages/trilinos.package
+# https://gitlab.com/configurations/cluster-config/blob/master/trilinos.sh
+# https://github.com/Homebrew/homebrew-science/blob/master/trilinos.rb and some
+# relevant documentation/examples:
+# https://github.com/trilinos/Trilinos/issues/175
+
+
+class Xsdktrilinos(Package):
+    """The Trilinos Project is an effort to develop algorithms and enabling
+    technologies within an object-oriented software framework for the solution
+    of large-scale, complex multi-physics engineering and scientific problems.
+    A unique design feature of Trilinos is its focus on packages.
+    """
+    homepage = "https://trilinos.org/"
+    base_url = "https://github.com/trilinos/xSDKTrilinos/archive"
+
+    version('develop', git='https://github.com/trilinos/xSDKTrilinos.git', tag='master')
+    version('12.8.1', '9cc338ded17d1e10ea6c0dc18b22dcd4')
+    version('12.6.4', '44c4c54ccbac73bb8939f68797b9454a')
+
+    def url_for_version(self, version):
+        return '%s/trilinos-release-%s.tar.gz' % \
+            (Xsdktrilinos.base_url, version.dashed)
+
+    variant('hypre',        default=True,
+            description='Compile with Hypre preconditioner')
+    variant('petsc',        default=True,
+            description='Compile with PETSc solvers')
+    variant('shared',       default=True,
+            description='Enables the build of shared libraries')
+    variant('debug',        default=False,
+            description='Builds a debug version of the libraries')
+
+    depends_on('cmake', type='build')
+
+    # MPI related dependencies
+    depends_on('mpi')
+    depends_on('hypre', when='+hypre')
+    depends_on('petsc', when='+petsc')
+    depends_on('trilinos')
+
+    def install(self, spec, prefix):
+        options = []
+        options.extend(std_cmake_args)
+
+        mpi_bin = spec['mpi'].prefix.bin
+        # Note: -DXYZ_LIBRARY_NAMES= needs semicolon separated list of names
+        options.extend([
+            '-DxSDKTrilinos_VERBOSE_CONFIGURE:BOOL=OFF',
+            '-DxSDKTrilinos_ENABLE_TESTS:BOOL=ON',
+            '-DxSDKTrilinos_ENABLE_EXAMPLES:BOOL=ON',
+            '-DTrilinos_INSTALL_DIR=%s' % spec['trilinos'].prefix,
+            '-DCMAKE_BUILD_TYPE:STRING=%s' % (
+                'DEBUG' if '+debug' in spec else 'RELEASE'),
+            '-DBUILD_SHARED_LIBS:BOOL=%s' % (
+                'ON' if '+shared' in spec else 'OFF'),
+            '-DTPL_ENABLE_MPI:BOOL=ON',
+            '-DMPI_BASE_DIR:PATH=%s' % spec['mpi'].prefix,
+            '-DxSDKTrilinos_ENABLE_CXX11:BOOL=ON',
+            '-DTPL_ENABLE_HYPRE:BOOL=%s' % (
+                'ON' if '+hypre' in spec else 'OFF'),
+            '-DTPL_ENABLE_PETSC:BOOL=%s' % (
+                'ON' if '+petsc' in spec else 'OFF')
+        ])
+
+        # Fortran lib
+        if spec.satisfies('%gcc') or spec.satisfies('%clang'):
+            libgfortran = os.path.dirname(os.popen(
+                '%s --print-file-name libgfortran.a' %
+                join_path(mpi_bin, 'mpif90')).read())
+            options.extend([
+                '-DxSDKTrilinos_EXTRA_LINK_FLAGS:STRING=-L%s/ -lgfortran' % (
+                    libgfortran),
+                '-DxSDKTrilinos_ENABLE_Fortran=ON'
+            ])
+
+        # for build-debug only:
+        # options.extend([
+        #    '-DCMAKE_VERBOSE_MAKEFILE:BOOL=TRUE'
+        # ])
+
+        with working_dir('spack-build', create=True):
+            cmake('..', *options)
+            make()
+            make('install')
+
+            # The shared libraries are not installed correctly on Darwin;
+            # correct this
+            if (sys.platform == 'darwin') and ('+shared' in spec):
+                fix_darwin_install_name(prefix.lib)
