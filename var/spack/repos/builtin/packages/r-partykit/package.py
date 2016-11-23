3258a0a2457c45d3da40e7e81683a4469ef56f54
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2016-11-23 11:38:00 +0100

    Splitting lines to pass flake8 travis test

--- var/spack/repos/builtin/packages/r-partykit/package.py
+++ var/spack/repos/builtin/packages/r-partykit/package.py
@@ -27,13 +27,14 @@ from spack import *
 
 
 class RPartykit(Package):
-    """A toolkit with infrastructure for representing, summarizing, and visualizing
-    tree-structured regression and classification models. This unified infrastructure
-    can be used for reading/coercing tree models from different sources ('rpart', 'RWeka', 'PMML')
-    yielding objects that share functionality for print()/plot()/predict() methods.
-    Furthermore, new and improved reimplementations of conditional inference trees
-    (ctree()) and model-based recursive partitioning (mob()) from the 'party'
-    package are provided based on the new infrastructure."""
+    """A toolkit with infrastructure for representing, summarizing, and
+    visualizing tree-structured regression and classification models. This
+    unified infrastructure can be used for reading/coercing tree models from
+    different sources ('rpart', 'RWeka', 'PMML') yielding objects that share
+    functionality for print()/plot()/predict() methods. Furthermore, new and
+    improved reimplementations of conditional inference trees (ctree()) and
+    model-based recursive partitioning (mob()) from the 'party' package are
+    provided based on the new infrastructure."""
 
     homepage = "http://partykit.r-forge.r-project.org/partykit"
     url      = "https://cran.r-project.org/src/contrib/partykit_1.1-1.tar.gz"
