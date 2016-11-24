3fb7fb2a80eccc90d42569030f6fb834d0f7e36a
Javier <javiercvilla@gmail.com>
2016-11-24 21:18:47 +0100

    New package - r-formatr (#2405)
    * New package - r-formatr
    
    * Block comment should start with '# '

---
+++ var/spack/repos/builtin/packages/r-formatr/package.py
@@ -0,0 +1,51 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+
+from spack import *
+
+
+class RFormatr(Package):
+    """Provides a function tidy_source() to format R source code. Spaces and
+    indent will be added to the code automatically, and comments will be
+    preserved under certain conditions, so that R code will be more
+    human-readable and tidy. There is also a Shiny app as a user interface in
+    this package."""
+
+    homepage = "http://yihui.name/formatR"
+    url      = "https://cran.r-project.org/src/contrib/formatR_1.4.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/formatR"
+
+    version('1.4', '98b9b64b2785b35f9df403e1aab6c73c')
+
+    extends('R')
+
+    depends_on('r-codetools', type=nolink)
+    depends_on('r-shiny', type=nolink)
+    depends_on('r-testit', type=nolink)
+    # depends_on('r-knitr', type=nolink) - mutual dependency
+
+    def install(self, spec, prefix):
+        R('CMD', 'INSTALL', '--library={0}'.format(self.module.r_lib_dir),
+          self.stage.source_path)
