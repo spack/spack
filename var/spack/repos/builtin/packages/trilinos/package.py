bc4237c93ed754c6d5a41a065bf3a2d4ab85161b
Bruno Turcksin <bruno.turcksin@gmail.com>
2016-12-01 12:52:05 -0500

    Update trilinos version to 12.10.1 (#2454)

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -45,6 +45,7 @@ class Trilinos(CMakePackage):
     homepage = "https://trilinos.org/"
     base_url = "https://github.com/trilinos/Trilinos/archive"
 
+    version('12.10.1', '40f28628b63310f9bd17c26d9ebe32b1')
     version('12.8.1', '01c0026f1e2050842857db941060ecd5')
     version('12.6.4', 'c2ea7b5aa0d10bcabdb9b9a6e3bac3ea')
     version('12.6.3', '8de5cc00981a0ca0defea6199b2fe4c1')
@@ -113,7 +114,7 @@ class Trilinos(CMakePackage):
     depends_on('py-numpy', when='+python')
     depends_on('swig', when='+python')
 
-    patch('umfpack_from_suitesparse.patch')
+    patch('umfpack_from_suitesparse.patch', when='@:12.8.1')
 
     # check that the combination of variants makes sense
     def variants_check(self):
