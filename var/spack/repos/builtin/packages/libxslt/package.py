943c007fb5ff42e7aaa053a17b43d02f3d047ec8
Tom Scogland <tom.scogland@gmail.com>
2016-12-09 17:19:53 -0800

    update flux dependencies and package (#2541)
    * update flux dependencies and package
    
    * refinements from @adamjstewart
    
    * fix flux document generation
    
    The docbook-xsl package has been added, and correctly configures catalog
    files to generate documentation correctly with asciidoc.

--- var/spack/repos/builtin/packages/libxslt/package.py
+++ var/spack/repos/builtin/packages/libxslt/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Libxslt(Package):
+class Libxslt(AutotoolsPackage):
     """Libxslt is the XSLT C library developed for the GNOME
        project. XSLT itself is a an XML language to define
        transformation for XML. Libxslt is based on libxml2 the XML C
@@ -37,13 +37,9 @@ class Libxslt(Package):
     url      = "http://xmlsoft.org/sources/libxslt-1.1.28.tar.gz"
 
     version('1.1.28', '9667bf6f9310b957254fdcf6596600b7')
+    version('1.1.29', 'a129d3c44c022de3b9dcf6d6f288d72e')
 
     depends_on("libxml2")
     depends_on("xz")
     depends_on("zlib")
     depends_on("libgcrypt")
-
-    def install(self, spec, prefix):
-        configure("--prefix=%s" % prefix)
-        make()
-        make("install")
