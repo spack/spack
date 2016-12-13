3f36ca3c303189fbd3b7e7cb29e50bad5258f746
Kim Serradell <kim.serradell@bsc.es>
2016-12-13 11:51:51 +0100

    Add python cdo support

---
+++ var/spack/repos/builtin/packages/py-cdo/package.py
@@ -0,0 +1,42 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+class PyCdo(Package):
+    """The cdo package provides an interface to the Climate Data Operators from Python."""
+
+    homepage = "https://pypi.python.org/pypi/cdo"
+    url      = "https://pypi.python.org/packages/source/c/cdo/cdo-1.3.2.tar.gz"
+
+    version('1.3.2', '4b3686ec1b9b891f166c1c466c6db745')
+
+    extends('python')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-scipy', type=nolink)
+    depends_on('py-netcdf', type=nolink)   
+
+    def install(self, spec, prefix):
+        setup_py('install', '--prefix={0}'.format(prefix))
