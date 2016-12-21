f7dc6b768d5f4752560af6523529b94af717a7dd
Adam J. Stewart <ajstewart426@gmail.com>
2016-12-21 17:59:30 -0500

    Ensure that every package has a license
    Also fixes URLs with http://http:// doubled.
    
    This is a continuation of #2656.

--- var/spack/repos/builtin/packages/cscope/package.py
+++ var/spack/repos/builtin/packages/cscope/package.py
@@ -25,18 +25,13 @@
 from spack import *
 
 
-class Cscope(Package):
+class Cscope(AutotoolsPackage):
     """Cscope is a developer's tool for browsing source code."""
-    homepage = "http://http://cscope.sourceforge.net/"
+
+    homepage = "http://cscope.sourceforge.net/"
     url      = "http://downloads.sourceforge.net/project/cscope/cscope/15.8b/cscope-15.8b.tar.gz"
 
     version('15.8b', '8f9409a238ee313a96f9f87fe0f3b176')
 
     # Can be configured to use flex (not necessary)
     # ./configure --with-flex
-
-    def install(self, spec, prefix):
-        configure('--prefix=%s' % prefix)
-
-        make()
-        make("install")
