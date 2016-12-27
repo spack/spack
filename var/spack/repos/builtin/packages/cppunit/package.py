81c1404b9cd071d1433852bd0b05fb168a53f981
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-12-27 08:32:29 +0000

    Adds catch, cppunit, spdlog, tinyxml(1 and 2), google benchmark (#2627)
    * Adds catch, cppunit, spdlog, tinyxml(1 and 2), google benchmark
    
    Tinyxml comes in two flavors, 1 and 2. Each comes in several
    versions... So they cannot be easily united into a single package.
    
    * Use CMakePackage and friends, add copyright
    
    Also eleminate debug/release variants, since it no longuer fits in the
    CMakePackage format.
    
    * Remove unnecessary url
    
    * spdlog now has tagged releases
    
    * Remove unnecessary url argument
    
    * Fewer quotes in cmake args, because magic
    
    * Incorrect base class for tinyxml

---
+++ var/spack/repos/builtin/packages/cppunit/package.py
@@ -0,0 +1,34 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Cppunit(AutotoolsPackage):
+    """Obsolete Unit testing framework for C++"""
+
+    homepage = "https://wiki.freedesktop.org/www/Software/cppunit/"
+    url = "http://dev-www.libreoffice.org/src/cppunit-1.13.2.tar.gz"
+
+    version('1.13.2', '0eaf8bb1dcf4d16b12bec30d0732370390d35e6f')
