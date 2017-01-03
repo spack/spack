190bd6ca65ae724abd409d644a18689bec433063
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-01-03 15:23:14 -0600

    Create rockstar package
    rockstar is a halo finding algorithm

---
+++ var/spack/repos/builtin/packages/rockstar/package.py
@@ -0,0 +1,37 @@
+from spack import *
+
+class Rockstar(Package):
+    """Description"""
+
+    homepage = "https://bitbucket.org/gfcstanford/rockstar"
+    url      = "https://bitbucket.org/gfcstanford/rockstar"
+
+    version('develop', git='https://bitbucket.org/gfcstanford/rockstar.git')
+    version('yt', hg='https://bitbucket.org/MatthewTurk/rockstar')
+
+    variant('hdf5', description='Build rockstar with HDF5 support')
+
+    patch('adjust_buildscript.patch')
+
+    depends_on('hdf5', when='+hdf5')  
+
+    def install(self, spec, prefix):
+        # Set environment appropriately for HDF5
+    	if '+hdf5' in spec:
+	    os.environ['HDF5_INC_DIR'] = os.environ['HDF5_DIR']+"/include"
+	    os.environ['HDF5_LIB_DIR'] = os.environ['HDF5_DIR']+"/lib"
+	
+	# Build depending on whether hdf5 is to be used
+	if '+hdf5' in spec:
+            make('with_hdf5')
+	else:
+            make()
+
+	# Build rockstar library
+	make('lib')
+
+	mkdir(join_path(prefix.bin))
+	mkdir(join_path(prefix.lib))
+
+	install('rockstar', join_path(prefix.bin, 'rockstar'))
+	install('librockstar.so', join_path(prefix.lib, 'librockstar.so'))
