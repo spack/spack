269536d59228ba65ea9c7cae5b6b48519d1573ff
Adam J. Stewart <ajstewart426@gmail.com>
2017-01-09 11:31:39 -0600

    Add TAO package

--- var/spack/repos/builtin/packages/petsc/package.py
+++ var/spack/repos/builtin/packages/petsc/package.py
@@ -48,6 +48,7 @@ class Petsc(Package):
     version('3.5.3', 'd4fd2734661e89f18ac6014b5dd1ef2f')
     version('3.5.2', 'ad170802b3b058b5deb9cd1f968e7e13')
     version('3.5.1', 'a557e029711ebf425544e117ffa44d8f')
+    version('3.4.5', '7681cda4de9dcac02a585aa9dfb0ffac')
     version('3.4.4', '7edbc68aa6d8d6a3295dd5f6c2f6979d')
 
     variant('shared',  default=True,
