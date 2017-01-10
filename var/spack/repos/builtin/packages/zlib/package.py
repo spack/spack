70eb1d538d744b12dad56f1ab02bf9dc3f1d26af
Eun Kyung Lee <eunkyung.lee@us.ibm.com>
2017-01-10 17:40:06 -0500

    Added latest version zlib

--- var/spack/repos/builtin/packages/zlib/package.py
+++ var/spack/repos/builtin/packages/zlib/package.py
@@ -31,8 +31,9 @@ class Zlib(AutotoolsPackage):
        data-compression library."""
 
     homepage = "http://zlib.net"
-    url = "http://zlib.net/zlib-1.2.8.tar.gz"
+    url = "http://zlib.net/zlib-1.2.10.tar.gz"
 
+    version('1.2.10', '8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017')
     version('1.2.8', '44d667c142d7cda120332623eab69f40')
 
     variant('pic', default=True,
