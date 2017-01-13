de1c8fb556a475dbc07a5f310d2508ad9f227425
sven <sjm324@cornell.edu>
2017-01-13 13:22:50 -0500

    Removing deprecated python support, adding missing dependencies.

--- var/spack/repos/builtin/packages/py-ipdb/package.py
+++ var/spack/repos/builtin/packages/py-ipdb/package.py
@@ -33,43 +33,22 @@ class PyIpdb(Package):
     url      = "https://pypi.io/packages/source/i/ipdb/ipdb-0.10.1.tar.gz"
 
     version('0.10.1', '4aeab65f633ddc98ebdb5eebf08dc713')
-    # Old versions of python have conflicts, search for 'Warning' on
-    # the pypi page: https://pypi.python.org/pypi/ipdb
-    version('0.8',    '96dca0712efa01aa5eaf6b22071dd3ed') # python 2.5
-    version('0.6',    '4a4b32e64c043522368c7871d8f50b6d') # python 2.4 or less
-
-    def url_for_version(self, version):
-        base_url = "https://pypi.io/packages/source/i/ipdb/ipdb-"
-        # v0.8 was only packaged as a .zip, the others usually are .tar.gz
-        # See https://pypi.python.org/simple/ipdb/
-        if version == Version("0.8"):
-            return "{}{}.zip".format(base_url, version)
-        else:
-            return "{}{}.tar.gz".format(base_url, version)
 
     # :TODO:
     # There might be potential to add variants here, but at the time of writing
     # this the original packager does not know what they are. See the 3rd party
     # section on ipdb's GitHub:
     #     https://github.com/gotcha/ipdb#third-party-support
-
-    # The python dependencies below are enumerating the latest known stable for
-    # earlier versions of python. The author understands these dependencies as:
-    #     - Any python >= 2.5 works with 0.8 and later
-    #     - Python v2.4 and earlier need 0.6 or earlier
-    #     - Python 2.x should be able to compile 0.6,
-    #       but 3.x can only compile 0.8+
-    # Thanks @adamjstewart for help with the dependencies :)
-    # Future editors should not add _earlier_ versions or the following logic
-    # may not be valid anymore!
     extends('python')
-    depends_on('python@2.6:2.7,3.2:', when='@0.9.1:0.10.1')
-    depends_on('python@2.5:2.7,3.2.0:3.2.999', when='@0.7:0.9.0')
-    depends_on('python@2.4:2.7', when='@:0.6.1')
 
     # Dependencies gathered from: https://github.com/gotcha/ipdb/blob/master/setup.py
     depends_on('py-setuptools', type='build')
-    depends_on('py-ipython',    type=('build', 'link'))# ipdb needs ipython to run
+    # ipdb needs many packages available at runtime
+    depends_on('py-ipython',        type=('build', 'link'))
+    depends_on('py-traitlets',      type=('build', 'link'))
+    depends_on('py-six',            type=('build', 'link'))
+    depends_on('py-pexpect',        type=('build', 'link'))
+    depends_on('py-prompt-toolkit', type=('build', 'link'))
 
     def install(self, spec, prefix):
         # Installation is uncomplicated, this should suffice.
