018afc8d9039333eb3085c3e56899bdfc1e97cd2
Jianwen WEI <weijianwen@gmail.com>
2017-01-31 00:04:40 +0800

    Add CUDA and lua-jit.

--- BUILD.sh
+++ BUILD.sh
@@ -11,25 +11,41 @@
 
 # Tools: git, emacs
 
-spack install git
+spack install git %gcc@4.8.5
 
 # JDK
 
 spack install jdk
 
+# Perl
+
+spack install perl@5.24.0 %gcc@5.4.0
+
+# LuaJIT
+
+spack install lua-jit %gcc@5.4.0
+spack install lua-jit %intel@16.0.3
+
 # Python
 
-spack install python@2.7.13 %gcc@5.4.0
-spack install python@3.6.0 %gcc@5.4.0
+#spack install python@2.7.13 	%gcc@5.4.0
+#spack install python@2.7.13 	%intel@16.0.3
+#spack install python@3.6.0 	%gcc@5.4.0
 
 # MPI 
 
-spack install opnmpi
-spack install mvapich2
-spack install mpich
+spack install openmpi	%gcc@5.4.0
+spack install mvapich2	%gcc@5.4.0
+spack install mpich	%gcc@5.4.0
+spack install openmpi	%intel@16.0.3
 
 # Bazel
-spack install bazel
+spack install bazel	%gcc@5.4.0
+
+# CUDA
+spack install cuda@8.0.44 %gcc@5.4.0
+spack install cuda@7.5.18 %gcc@5.4.0
+spack install cuda@6.5.14 %gcc@4.8.5
 
 # R
 # spack install r@3.3.2 %gcc@5.4.0
