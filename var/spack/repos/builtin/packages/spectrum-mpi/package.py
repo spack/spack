3d119d8c5c17a121eccb1f7c2f2ae680fff97d94
serbanmaerean <serban@us.ibm.com>
2017-02-02 15:13:56 -0500

    spectrum-mpi: Added license files and removed the versions
    No need for versions as the package is external.

--- var/spack/repos/builtin/packages/spectrum-mpi/package.py
+++ var/spack/repos/builtin/packages/spectrum-mpi/package.py
@@ -1,35 +1,60 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at International Business Machines Corporation
+#
+# This file is part of Spack.
+# Created by Serban Maerean, serban@us.ibm.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
 from spack import *
 
-class Spectrum(Package):
-    """IBM MPI implementation from Spectrum MPI."""
 
-    homepage = "http://www.example.com"
-    url      = "http://www.example.com/ibm-mpi-10.1.0.tar.gz"
+class SpectrumMpi(Package):
+    """
+    IBM MPI implementation from Spectrum MPI.
 
-    version('10.1.0.2', '0123456789abcdef0123456789abcdef')
-    version('10.1.0', '0123456789abcdef0123456789abcdef')
+    """
 
-    provides('mpi')
+    homepage = "http://www.example.com"
+    url      = "http://www.example.com/ibm-mpi-10.1.0.2.tar.gz"
 
-    def install(self, spec, prefix):
-        raise InstallError('IBM MPI is not installable; it is vendor supplied')
+    provides('mpi')
+
+    def install(self, spec, prefix):
+        raise InstallError('IBM MPI is not installable; it is vendor supplied')
 
     def setup_dependent_package(self, module, dspec):
-        # get library name and directory
-        self.spec.mpi_base_dir = self.prefix
-        self.spec.mpi_library = join_path(self.prefix.lib, 'libmpi_ibm.so')
+        # get library name and directory
+        self.spec.mpi_base_dir = self.prefix
+        self.spec.mpi_library = join_path(self.prefix.lib, 'libmpi_ibm.so')
         self.spec.mpi_include_path = self.prefix.include
         self.spec.mpi_library_path = self.prefix.lib
         self.spec.mpi_exec = join_path(self.prefix.bin, 'mpirun')
         self.spec.mpi_np_flag = '-np'
         if '%xl' in dspec or '%xl_r' in dspec:
-            self.spec.mpicc = join_path(self.prefix.bin, 'mpixlc')
+            self.spec.mpicc = join_path(self.prefix.bin, 'mpixlc')
             self.spec.mpicxx = join_path(self.prefix.bin, 'mpixlC')
             self.spec.mpif77 = join_path(self.prefix.bin, 'mpixlf')
             self.spec.mpif90 = join_path(self.prefix.bin, 'mpixlf')
             self.spec.mpifc = join_path(self.prefix.bin, 'mpixlf')
         else:
-            self.spec.mpicc = join_path(self.prefix.bin, 'mpicc')
+            self.spec.mpicc = join_path(self.prefix.bin, 'mpicc')
             self.spec.mpicxx = join_path(self.prefix.bin, 'mpicxx')
             self.spec.mpif77 = join_path(self.prefix.bin, 'mpif77')
             self.spec.mpif90 = join_path(self.prefix.bin, 'mpif90')
@@ -37,14 +62,14 @@ class Spectrum(Package):
 
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        if '%xl' in dependent_spec or '%xl_r' in dependent_spec:
-            spack_env.set('MPICC',  join_path(self.prefix.bin, 'mpixlc'))
+        if '%xl' in dependent_spec or '%xl_r' in dependent_spec:
+            spack_env.set('MPICC', join_path(self.prefix.bin, 'mpixlc'))
             spack_env.set('MPICXX', join_path(self.prefix.bin, 'mpixlC'))
             spack_env.set('MPIF77', join_path(self.prefix.bin, 'mpixlf'))
             spack_env.set('MPIF90', join_path(self.prefix.bin, 'mpixlf'))
         else:
-            spack_env.set('MPICC',  join_path(self.prefix.bin, 'mpicc'))
-            spack_env.set('MPICXX', join_path(self.prefix.bin, 'mpic++'))
+            spack_env.set('MPICC', join_path(self.prefix.bin, 'mpicc'))
+            spack_env.set('MPICXX', join_path(self.prefix.bin, 'mpic++'))
             spack_env.set('MPIF77', join_path(self.prefix.bin, 'mpif77'))
             spack_env.set('MPIF90', join_path(self.prefix.bin, 'mpif90'))
 
