564fa67a4c4feb251ceb4bcc480aa182c5687e51
Jianwen WEI <weijianwen@gmail.com>
2017-02-04 12:32:57 +0800

    Remove R and Python from build_rpm.

--- build_rpm.sh
+++ build_rpm.sh
@@ -7,10 +7,10 @@ compilers=(
 
 # Python
 
-spack install python@2.7.13 	%gcc@5.4.0
-spack install python@2.7.13 	%intel@16.0.3
-spack install python@3.6.0 	%gcc@5.4.0
-spack install python@3.6.0 	%intel@16.0.3
-
-# R
-spack install r@3.3.2 %gcc@5.4.0
+for compiler in "${compilers[@]}"
+do
+spack install python@2.7.13 $mpiler
+spack install python@3.6.0  $compiler
+spack install r@3.3.2 	    $compiler
+spack install boost 	    $compiler
+done
