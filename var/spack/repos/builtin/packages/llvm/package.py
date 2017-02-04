97fbb1cc6b45cc7401860e831ccb8d71d4bb62bf
Gregory Lee <lee218@llnl.gov>
2017-02-04 15:42:22 -0800

    added archer OpenMP race detector and its deps (#3030)

--- var/spack/repos/builtin/packages/llvm/package.py
+++ var/spack/repos/builtin/packages/llvm/package.py
@@ -77,6 +77,7 @@ class Llvm(Package):
 
     # Universal dependency
     depends_on('python@2.7:2.8')  # Seems not to support python 3.X.Y
+    depends_on('py-lit')
 
     # lldb dependencies
     depends_on('ncurses', when='+lldb')
