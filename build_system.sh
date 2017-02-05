dabf75356448c4c1a0e2458a4ce5fb53f114ceb5
Jianwen WEI <weijianwen@gmail.com>
2017-02-05 17:31:06 +0800

    Move JDK and lua-jit to gcc-4.8.5/

--- build_system.sh
+++ build_system.sh
@@ -1,40 +1,25 @@
 #!/bin/sh
 
 # Compilers
-
 compilers=(
     %gcc@5.4.0
     %intel@16.0.3
 )
 
-# GCC (install GCC only once)
-
+# Compilers
 spack install gcc@4.9.4 %gcc@4.8.5
 spack install gcc@5.4.0 %gcc@4.8.5
 spack install gcc@6.3.0 %gcc@4.8.5
 spack install llvm 	%gcc@4.8.5
+spack install jdk 	%gcc@4.8.5
+spack install bazel	%gcc@4.8.5
+spack install lua-jit	%gcc@4.8.5
 
 # Tools: git, emacs
-
 spack install git 	%gcc@4.8.5
 spack install emacs 	%gcc@4.8.5
 
-# JDK
-
-for compiler in "${compilers[@]}"
-do
-	spack install jdk $compiler
-done
-
-# LuaJIT
-
-for compiler in "${compilers[@]}"
-do
-	spack install lua-jit $compiler
-done
-
 # MPI 
-
 for compiler in "${compilers[@]}"
 do
 	spack install openmpi@2.0.1 	$compiler
@@ -43,9 +28,6 @@ do
 	spack install mvapich2		$compiler
 	spack install mpich		$compiler
 done
-
-# Bazel
-spack install bazel	%gcc@5.4.0
 
 # CUDA
 spack install cuda@8.0.44 %gcc@5
