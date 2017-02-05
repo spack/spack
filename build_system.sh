192bd0f567ea8b5b831a85f5791cc16a15f9daa5
Jianwen WEI <weijianwen@gmail.com>
2017-02-05 21:42:02 +0800

    Remove openmpi and cuda from build_system.sh.

--- build_system.sh
+++ build_system.sh
@@ -1,37 +1,17 @@
 #!/bin/sh
 
-# Compilers
-compilers=(
-    %gcc@5.4.0
-    %intel@16.0.3
-)
+cc=%gcc@4.8.5
 
 # Compilers
-spack install gcc@4.9.4 %gcc@4.8.5
-spack install gcc@5.4.0 %gcc@4.8.5
-spack install gcc@6.3.0 %gcc@4.8.5
-spack install llvm 	%gcc@4.8.5
-spack install jdk 	%gcc@4.8.5
-spack install bazel	%gcc@4.8.5
-spack install maven	%gcc@4.8.5
-spack install lua-jit	%gcc@4.8.5
+spack install gcc@4.9.4 $cc
+spack install gcc@5.4.0 $cc
+spack install gcc@6.3.0 $cc
+spack install llvm 	$cc
+spack install jdk 	$cc
+spack install bazel	$cc
+spack install maven	$cc
+spack install lua-jit	$cc
 
 # Tools: git, emacs
-spack install git 	%gcc@4.8.5
-spack install emacs 	%gcc@4.8.5
-
-# MPI 
-for compiler in "${compilers[@]}"
-do
-	spack install openmpi@2.0.1 	$compiler
-	spack install openmpi@1.10.3 	$compiler
-	spack install openmpi@1.6.5 	$compiler
-	spack install mvapich2		$compiler
-	spack install mpich		$compiler
-done
-
-# CUDA
-spack install cuda@8.0.44 %gcc@5
-spack install cuda@8.0.44 %intel@16
-spack install cuda@7.5.18 %gcc@4.8.5
-spack install cuda@7.5.18 %intel@15
+spack install git 	$cc	
+spack install emacs 	$cc	
