d8a0a3a9d4ed247a1b27cefd45b2df75f3ef3c43
Brian C. Van Essen <vanessen1@llnl.gov>
2017-02-07 00:00:32 -0800

    Recipe for building LBANN toolkit.  Contains limited feature set and is optimized for building with GNU gcc and OpenBLAS.

--- var/spack/repos/builtin/packages/lbann/package.py
+++ var/spack/repos/builtin/packages/lbann/package.py
@@ -49,16 +49,24 @@ class Lbann(CMakePackage):
             description='Elemental: use 64bit integers for BLAS')
 
     depends_on('cmake', type='build')
+
+    # Currently required to allow lbann to specify a specific BLAS library
+    depends_on('blas')
+    depends_on('lapack')
+    depends_on('scalapack')
+
     depends_on('elemental +openmp_blas +scalapack')
+    depends_on('elemental +openmp_blas +scalapack +int64', when='+int64')
     depends_on('elemental +openmp_blas +scalapack +debug', when='+debug')
+    depends_on('elemental +openmp_blas +scalapack +int64 +debug', when='+debug +int64')
     depends_on('cuda', when='+gpu')
     depends_on('mpi')
-    depends_on('opencv', when='+opencv')
+    depends_on('opencv@2.4.13', when='+opencv')
     depends_on('protobuf@3.0.2')
 
     def build_type(self):
-        """Returns the correct value for the ``CMAKE_BUILD_TYPE`` variable                                                                                                                                
-        :return: value for ``CMAKE_BUILD_TYPE``                                                                                                                                                           
+        """Returns the correct value for the ``CMAKE_BUILD_TYPE`` variable
+        :return: value for ``CMAKE_BUILD_TYPE``
         """
         if '+debug' in self.spec:
             return 'Debug'
@@ -67,27 +75,18 @@ class Lbann(CMakePackage):
 
     def cmake_args(self):
         spec = self.spec
-
-#        include_flags = ["-I%s" % join_path(self.stage.source_path, "spack-build/include")]
-#        include_flags = ["-I%s" % join_path(spec.prefix, "include")]
-#        link_flags = self.lapack_libs.ld_flags.split()
-
-#/g/g19/vanessen/spack.git/opt/spack/linux-rhel7-x86_64/gcc-4.9.3/lbann-develop-ioboloaiznrunp4da4xjgodobsxdihe6/include
-        args = ['-DCMAKE_INSTALL_MESSAGE=LAZY',
-#                '-D GFORTRAN_LIB=/usr/tce/packages/gcc/gcc-4.9.3/lib64/libgfortran.so',
-                '-DCMAKE_CXX_FLAGS=-DLBANN_SET_EL_RNG -I{0}/spack-build/include'.format((
-                    self.stage.source_path)),
-                '-DWITH_CUDA:BOOL={0}'.format((
-                    'ON' if '+gpu' in self.spec else 'OFF')),
-                '-DWITH_CUDNN:BOOL={0}'.format((
-                    'ON' if '+gpu' in self.spec else 'OFF')),
-                '-DWITH_TBINF=OFF',
-                '-DWITH_VTUNE=OFF',
-                '-DElemental_DIR={0}'.format(self.spec['elemental'].prefix),
-                '-DELEMENTAL_MATH_LIBS={0}'.format(self.spec['elemental'].elemental_libs),
-                '-DVERBOSE=0',
-                '-DLBANN_HOME=.',
-                '-DLBANN_VERSION=spack']
+        args = [
+            '-DCMAKE_INSTALL_MESSAGE=LAZY',
+            '-DCMAKE_CXX_FLAGS=-DLBANN_SET_EL_RNG',
+            '-DWITH_CUDA:BOOL=%s' % ('+gpu' in spec),
+            '-DWITH_CUDNN:BOOL=%s' % ('+gpu' in spec),
+            '-DWITH_TBINF=OFF',
+            '-DWITH_VTUNE=OFF',
+            '-DElemental_DIR={0}'.format(self.spec['elemental'].prefix),
+            '-DELEMENTAL_MATH_LIBS={0}'.format(self.spec['elemental'].elemental_libs),
+            '-DVERBOSE=0',
+            '-DLBANN_HOME=.',
+            '-DLBANN_VER=spack']
 
         if '+opencv' in self.spec:
             args.extend(['-DOpenCV_DIR:STRING={0}'.format(
