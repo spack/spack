b969b2a3b99d5737c87f32c4dadbf189e05bae18
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-02-08 14:41:51 -0600

    Fix package.py as requested by @adamjstewart

--- var/spack/repos/builtin/packages/exmcutils/package.py
+++ var/spack/repos/builtin/packages/exmcutils/package.py
@@ -22,21 +22,7 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install exmcutils
-#
-# You can edit this file again by typing:
-#
-#     spack edit exmcutils
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
+
 from spack import *
 
 
@@ -48,11 +34,4 @@ class Exmcutils(AutotoolsPackage):
 
     version('0.5.3', '0e3ed6cc2991c684cd8f08db45c99a39')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
-
-    def configure_args(self):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    # This package has no dependencies.
