79c1f7885a7d249eb03b259f1c885a6de90222ce
Massimiliano Culpo <massimiliano.culpo@googlemail.com>
2017-02-08 03:28:01 +0100

    db and concretization of modified packages: fixes #2911 (#2920)

--- lib/spack/spack/database.py
+++ lib/spack/spack/database.py
@@ -340,6 +340,7 @@ class Database(object):
         # cached prematurely.
         for hash_key, rec in data.items():
             rec.spec._mark_concrete()
+            rec.spec.package.spec._mark_concrete()
 
         self._data = data
 
