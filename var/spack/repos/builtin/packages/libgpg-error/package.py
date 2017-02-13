94f7fde8641f0d3df2bb88ae6bdb9fc3eb5c30e9
healther <andreas.baumbach@kip.uni-heidelberg.de>
2017-02-13 15:07:05 +0000

    add 1.26 and fix url

--- var/spack/repos/builtin/packages/libgpg-error/package.py
+++ var/spack/repos/builtin/packages/libgpg-error/package.py
@@ -32,9 +32,10 @@ class LibgpgError(AutotoolsPackage):
        SmartCard Daemon and possibly more in the future. """
 
     homepage = "https://www.gnupg.org/related_software/libgpg-error"
-    url      = "https://ftp.heanet.ie/disk1/gnupg/libgpg-error/libgpg-error-1.18.tar.bz2"
-    url_list = "http://gd.tuwien.ac.at/pub/gnupg/libgpg-error/libgpg-error-1.18.tar.bz2"
+    url      = "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.26.tar.bz2"
+    list_url = "http://gd.tuwien.ac.at/pub/gnupg/libgpg-error/libgpg-error-1.18.tar.bz2"
     list_depth = 2
 
+    version('1.26', '97456709dbbcbb69354317ffe3e347cd')
     version('1.21', 'ab0b5aba6d0a185b41d07bda804fd8b2')
     version('1.18', '12312802d2065774b787cbfc22cc04e9')
