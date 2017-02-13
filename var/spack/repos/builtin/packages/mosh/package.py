9a5a8d0d1a1057117042389b9c386d303141c939
Eric <muffgaga@gmx.de>
2017-02-13 12:37:16 +0100

    Update package.py

--- var/spack/repos/builtin/packages/mosh/package.py
+++ var/spack/repos/builtin/packages/mosh/package.py
@@ -22,28 +22,15 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install mosh
-#
-# You can edit this file again by typing:
-#
-#     spack edit mosh
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
 class Mosh(AutotoolsPackage):
     """Remote terminal application that allows roaming, supports intermittent
     connectivity, and provides intelligent local echo and line editing of user
-    keystrokes."""
+    keystrokes. Mosh is a replacement for SSH. It's more robust and responsive,
+    especially over Wi-Fi, cellular, and long-distance links.
+    """
 
     homepage = "https://mosh.org/"
     url      = "https://mosh.org/mosh-1.2.6.tar.gz"
