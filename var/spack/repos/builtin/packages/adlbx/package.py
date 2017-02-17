3a7435820ac298058a53f84dcb2bc9fae8e8c669
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-02-17 13:35:45 -0600

    Swift/T: all packages (#3130)
    * New package.py for ExM C-Utils: An initial package dependency for Swift/T
    * New package.py for ADLB/X: A 2nd package dependency for Swift/T
    * New package.py for Turbine
    * New package.py for STC

--- var/spack/repos/builtin/packages/adlbx/package.py
+++ var/spack/repos/builtin/packages/adlbx/package.py
@@ -29,13 +29,13 @@ from spack import *
 class Adlbx(AutotoolsPackage):
     """ADLB/X: Master-worker library + work stealing and data dependencies"""
 
-    homepage = "http://swift-lang.org/Swift-T"
-    url      = "http://swift-lang.github.io/swift-t-downloads/adlbx-0.8.0.tar.gz"
+    homepage = 'http://swift-lang.org/Swift-T'
+    url      = 'http://swift-lang.github.io/swift-t-downloads/adlbx-0.8.0.tar.gz'
     version('0.8.0', '34ade59ce3be5bc296955231d47a27dd')
 
     depends_on('exmcutils')
     depends_on('mpi')
 
     def configure_args(self):
-        args = ["--with-c-utils=" + self.spec['exmcutils'].prefix]
+        args = ['--with-c-utils=' + self.spec['exmcutils'].prefix]
         return args
--- var/spack/repos/builtin/packages/turbine/package.py
+++ var/spack/repos/builtin/packages/turbine/package.py
@@ -27,19 +27,20 @@ from spack import *
 
 
 class Turbine(AutotoolsPackage):
+    """Turbine: The Swift/T runtime"""
 
-    homepage = "http://swift-lang.org/Swift-T"
-    url      = "http://swift-lang.github.io/swift-t-downloads/turbine-1.0.0.tar.gz"
+    homepage = 'http://swift-lang.org/Swift-T'
+    url      = 'http://swift-lang.github.io/swift-t-downloads/turbine-1.0.0.tar.gz'
 
     version('1.0.0', '7ed56d65d6db0bfe15a439d818b4259e')
 
     depends_on('adlbx')
     depends_on('tcl')
-    depends_on('zsh')
+    depends_on('zsh', type='run')
 
     def configure_args(self):
-        args = ["--with-c-utils=" + self.spec['exmcutils'].prefix,
-                "--with-adlb="    + self.spec['adlbx'].prefix,
-                "--with-tcl="     + self.spec['tcl'].prefix,
-                "--with-mpi="     + self.spec['mpi'].prefix]
+        args = ['--with-c-utils=' + self.spec['exmcutils'].prefix,
+                '--with-adlb='    + self.spec['adlbx'].prefix,
+                '--with-tcl='     + self.spec['tcl'].prefix,
+                '--with-mpi='     + self.spec['mpi'].prefix]
         return args
