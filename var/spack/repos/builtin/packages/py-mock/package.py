6334341a0dd6a656d0af7dab6bafb29e3783fc9e
Eric Müller <mueller@kip.uni-heidelberg.de>
2017-02-24 11:57:35 +0100

    Add py-mock version 2.0.0

--- var/spack/repos/builtin/packages/py-mock/package.py
+++ var/spack/repos/builtin/packages/py-mock/package.py
@@ -33,6 +33,7 @@ class PyMock(PythonPackage):
     homepage = "https://github.com/testing-cabal/mock"
     url      = "https://pypi.python.org/packages/source/m/mock/mock-1.3.0.tar.gz"
 
+    version('2.0.0', '0febfafd14330c9dcaa40de2d82d40ad')
     version('1.3.0', '73ee8a4afb3ff4da1b4afa287f39fdeb')
 
     depends_on('py-pbr', type=('build', 'run'))
