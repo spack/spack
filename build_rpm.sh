ed5ffc5d489416b317418e35b54f3383ee3225d0
Jianwen WEI <weijianwen@gmail.com>
2017-02-25 23:51:27 +0800

    Add spack_install for build_rpm and build_user.

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+function s {
+	spack find $@ | grep 'No package'
+	if [ $? -eq 0 ]
+	then
+		spack install $@
+	else
+		echo "$@ has been installed."
+	fi
+}
+
 compilers=(
     %gcc@6.3.0
     %intel@17.0.1
@@ -14,22 +24,22 @@ mpis=(
 )
 
 # CUDA
-spack install cuda@8.0.44 %gcc@5
-spack install cuda@8.0.44 %intel@16
-spack install cuda@7.5.18 %gcc@4.8.5
-spack install cuda@7.5.18 %intel@15
+s cuda@8.0.44 %gcc@5
+s cuda@8.0.44 %intel@16
+s cuda@7.5.18 %gcc@4.8.5
+s cuda@7.5.18 %intel@15
 
 # Perl
-spack install perl@5.24.1 %gcc@6
-spack install perl@5.24.1 %intel@17 cflags="-fPIC"
+s perl@5.24.1 %gcc@6
+s perl@5.24.1 %intel@17 cflags="-fPIC"
 
 # Python, R, Boost
 for compiler in "${compilers[@]}"
 do
-spack install python@2.7.13 $compiler
-spack install python@3.6.0  $compiler
-spack install r@3.3.2 	    $compiler
-spack install boost 	    $compiler
+s python@2.7.13 $compiler
+s python@3.6.0  $compiler
+s r@3.3.2 	    $compiler
+s boost 	    $compiler
 done
 
 # MPI and MPI-dependent Libraries
@@ -37,9 +47,6 @@ for compiler in "${compilers[@]}"
 do
 	for mpi in "${mpis[@]}"
 	do
-		spack install $mpi $compiler
+		s $mpi $compiler
 	done
 done
-
-
-
