bba1dc60c8ec9c41e06200996a5f0f8f41ca707f
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-02-24 23:49:11 -0600

    Add and clean up some debug statements

--- lib/spack/spack/util/executable.py
+++ lib/spack/spack/util/executable.py
@@ -166,7 +166,7 @@ class Executable(object):
         cmd_line = "'%s'" % "' '".join(
             map(lambda arg: arg.replace("'", "'\"'\"'"), cmd))
 
-        tty.debug(cmd_line)
+        tty.debug("Executing command: (" + cmd_line + ")")
 
         try:
             proc = subprocess.Popen(
