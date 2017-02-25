807e21285da0cbfbe006a5bf385df13d65f73f43
Aniruddha Pispati <pispati2@linux-a1.ews.illinois.edu>
2017-02-25 17:22:23 -0600

    new branch

---
+++ var/spack/repos/builtin/packages/tensorflow/package.py
@@ -0,0 +1,61 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install tensorflow
+#
+# You can edit this file again by typing:
+#
+#     spack edit tensorflow
+#
+# See the Spack documentation for more information on packaging.
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+from spack import *
+
+
+class Tensorflow(Package):
+    """
+    TensorFlow is an open source software library for numerical computation using data flow graphs
+    """
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://github.com/tensorflow"
+    # url      = "http://www.example.com/example-1.2.3.tar.gz"
+
+    # FIXME: Add proper versions and checksums here.
+    version('e697cf7', git='https://github.com/tensorflow.git', commit='e697cf787b09193a6921af6d2b7db2d6c4d2a5dd')
+
+    # FIXME: Add dependencies if required.
+    # depends_on('foo')
+
+    def install(self, spec, prefix):
+        # FIXME: Unknown build system
+        make()
+        make('install')
