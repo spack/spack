9d9f4207f8b52cf8adf91c35428164f3423db168
muakasan <muakasan@gmail.com>
2017-02-25 17:34:45 -0600

    added dependencies

--- var/spack/repos/builtin/packages/tensorflow/package.py
+++ var/spack/repos/builtin/packages/tensorflow/package.py
@@ -22,21 +22,6 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install tensorflow
-#
-# You can edit this file again by typing:
-#
-#     spack edit tensorflow
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
@@ -45,16 +30,20 @@ class Tensorflow(Package):
     TensorFlow is an open source software library for numerical computation using data flow graphs
     """
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/tensorflow"
-    # url      = "http://www.example.com/example-1.2.3.tar.gz"
 
-    # FIXME: Add proper versions and checksums here.
     version('e697cf7', git='https://github.com/tensorflow.git', commit='e697cf787b09193a6921af6d2b7db2d6c4d2a5dd')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
-
+    depends_on('zlib')
+    depends_on('giflib')
+    depends_on('png')
+    depends_on('jpeg')
+    depends_on('eigen')
+    depends_on('gemmlowp')
+    depends_on('jsoncpp')
+    depends_on('farmhash')
+    depends_on('highwayhash')
+    depends_on('protobuf')
     def install(self, spec, prefix):
         # FIXME: Unknown build system
         make()
