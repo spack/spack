f9cf2f3edbf66ac19543a8eb00e55151a12f9d04
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-02-28 17:46:53 +0100

    add setuptools dependency

--- var/spack/repos/builtin/packages/py-xmlrunner/package.py
+++ var/spack/repos/builtin/packages/py-xmlrunner/package.py
@@ -33,4 +33,5 @@ class PyXmlrunner(PythonPackage):
 
     version('1.7.7', '7b0b152ed2d278516aedbc0cac22dfb3')
 
+    depends_on('py-setuptools', type='build')
     depends_on('py-unittest2', type=('build', 'run'), when='^python@:2.7')
