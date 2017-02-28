ffa37c8f68d129512e1cac088232b7335a7c1e82
healther <healther@users.noreply.github.com>
2017-02-28 17:21:06 +0100

    add py-ranger package (#3258)

---
+++ var/spack/repos/builtin/packages/py-ranger/package.py
@@ -0,0 +1,37 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+#
+from spack import *
+
+
+class PyRanger(PythonPackage):
+    """A VIM-inspired filemanager for the console"""
+
+    homepage = "http://ranger.nongnu.org/"
+    url      = "https://github.com/ranger/ranger/archive/v1.7.2.tar.gz"
+
+    version('1.7.2', '27805c3ab7ec4b129e1b93249506d925')
+
+    depends_on('python@2.6:')
