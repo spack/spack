2195598ceb0ba5f88edb96f5a20fdae44f105e79
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-02-28 11:56:58 +0100

    add various variants to vim

--- var/spack/repos/builtin/packages/vim/package.py
+++ var/spack/repos/builtin/packages/vim/package.py
@@ -50,8 +50,17 @@ class Vim(Package):
     variant('ruby', default=False, description="build with Ruby")
     depends_on('ruby', when='+ruby')
 
+    variant('lua', default=False, description="build with Lua")
+    depends_on('lua', when='+lua')
+
+    variant('perl', default=False, description="build with Perl")
+    depends_on('perl', when='+perl')
+
     variant('cscope', default=False, description="build with cscope support")
     depends_on('cscope', when='+cscope', type='run')
+
+    variant('failmissing', default=True,
+        description="Fail if dependencies on additional features are missing.")
 
     variant('gui', default=False, description="build with gui (gvim)")
     # virtual dependency?
@@ -78,6 +87,10 @@ class Vim(Package):
             feature_set = 'normal'
 
         configure_args = []
+
+        if '+failmissing' in spec:
+            configure_args.append("--enable-fail-if-missing")
+
         configure_args.append("--with-features=" + feature_set)
 
         if '+python' in spec:
@@ -89,6 +102,17 @@ class Vim(Package):
             configure_args.append("--enable-rubyinterp=yes")
         else:
             configure_args.append("--enable-rubyinterp=dynamic")
+
+        if '+lua' in spec:
+            configure_args.append("--enable-luainterp=yes")
+            configure_args.append("--with-lua-prefix=%s" % spec['lua'].prefix)
+        else:
+            configure_args.append("--enable-luainterp=dynamic")
+
+        if '+perl' in spec:
+            configure_args.append("--enable-perlinterp=yes")
+        else:
+            configure_args.append("--enable-perlinterp=dynamic")
 
         if '+gui' in spec:
             configure_args.append("--enable-gui=auto")
