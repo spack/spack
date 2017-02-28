b98b8abcd7884b39c3d93b6a7b3084758f23e647
healther <healther@users.noreply.github.com>
2017-02-28 19:56:36 +0100

    add package py-saga-python and dependencies (#3267)
    * add package py-saga-python and dependencies
    
    * fixed url and dependencies

---
+++ var/spack/repos/builtin/packages/py-colorama/package.py
@@ -0,0 +1,36 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class PyColorama(PythonPackage):
+    """Cross-platform colored terminal text."""
+
+    homepage = "https://github.com/tartley/colorama"
+    url      = "https://pypi.io/packages/source/c/colorama/colorama-0.3.7.tar.gz"
+
+    version('0.3.7', '349d2b02618d3d39e5c6aede36fe3c1a')
+
+    depends_on('py-setuptools', type='build')
