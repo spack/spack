b9e7a69d871afc0bf006836d420ccfa7c71014d7
healther <healther@users.noreply.github.com>
2017-03-01 14:51:17 +0100

    add package units (#3257)
    * add package units
    
    * removed unnecessary dependency
    
    * readded python dependency

---
+++ var/spack/repos/builtin/packages/units/package.py
@@ -0,0 +1,36 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Units(AutotoolsPackage):
+    """GNU units converts between different systems of units"""
+
+    homepage = "https://www.gnu.org/software/units/"
+    url      = "https://ftp.gnu.org/gnu/units/units-2.13.tar.gz"
+
+    version('2.13', '5cbf2a6af76e94ba0ac55fc8d99d5a3e')
+
+    depends_on('python', type=('build', 'run'))
