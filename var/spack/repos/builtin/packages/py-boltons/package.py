d50c37a3f24cf4d83c5f4009cf933b453750dc91
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-03-01 19:05:51 +0100

    added description

--- var/spack/repos/builtin/packages/py-boltons/package.py
+++ var/spack/repos/builtin/packages/py-boltons/package.py
@@ -26,7 +26,13 @@ from spack import *
 
 
 class PyBoltons(PythonPackage):
-    """ """
+    """When they're not builtins, they're boltons.
+
+    Functionality that should be in the standard library. Like builtins,
+    but Boltons.
+
+    Otherwise known as, “everyone’s util.py,” but cleaned up and tested.
+    """
     homepage = "https://boltons.readthedocs.io/"
     url = "https://pypi.io/packages/source/b/boltons/boltons-16.5.1.tar.gz"
 
