ee0f3a2ee374fe0494e4d43836f6c538ccccd295
Ondřej Čertík <ondrej.certik@gmail.com>
2017-03-01 23:40:38 -0700

    Move the url to version()

--- var/spack/repos/builtin/packages/mesa/package.py
+++ var/spack/repos/builtin/packages/mesa/package.py
@@ -30,11 +30,11 @@ class Mesa(AutotoolsPackage):
     specification - a system for rendering interactive 3D graphics."""
 
     homepage = "http://www.mesa3d.org"
-    #url      = "http://ftp.iij.ad.jp/pub/X11/x.org/pub/mesa/12.0.3/mesa-12.0.3.tar.gz"
-    url      = "ftp://ftp.freedesktop.org/pub/mesa/older-versions/10.x/10.2.4/MesaLib-10.2.4.tar.bz2"
+    url      = "http://ftp.iij.ad.jp/pub/X11/x.org/pub/mesa/12.0.3/mesa-12.0.3.tar.gz"
 
     version('12.0.3', '60c5f9897ddc38b46f8144c7366e84ad')
-    version('10.2.4', '11d3542da1b703618634be840a87b0b2')
+    version('10.2.4', '11d3542da1b703618634be840a87b0b2',
+        url="ftp://ftp.freedesktop.org/pub/mesa/older-versions/10.x/10.2.4/MesaLib-10.2.4.tar.bz2")
 
     # General dependencies
     depends_on('autoconf', type='build')
