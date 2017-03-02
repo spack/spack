2f0c3d81aac5abd09b50136ec62efbf896ea4687
healther <healther@users.noreply.github.com>
2017-03-02 19:43:13 +0100

    add version 2.0.0 for py-matplotlib (#3289)

--- var/spack/repos/builtin/packages/py-matplotlib/package.py
+++ var/spack/repos/builtin/packages/py-matplotlib/package.py
@@ -34,6 +34,7 @@ class PyMatplotlib(PythonPackage):
     homepage = "https://pypi.python.org/pypi/matplotlib"
     url      = "https://pypi.io/packages/source/m/matplotlib/matplotlib-1.4.2.tar.gz"
 
+    version('2.0.0', '7aa54b06327f0e1c4f3877fc2f7d6b17')
     version('1.5.3', 'ba993b06113040fee6628d74b80af0fd')
     version('1.5.1', 'f51847d8692cb63df64cd0bd0304fd20')
     version('1.4.3', '86af2e3e3c61849ac7576a6f5ca44267')
