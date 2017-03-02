080bbcb987d842f60b8463711611ddf06bf62320
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-03-02 21:20:56 +0100

    add variants for docs and pandas

--- var/spack/repos/builtin/packages/py-elephant/package.py
+++ var/spack/repos/builtin/packages/py-elephant/package.py
@@ -34,12 +34,15 @@ class PyElephant(PythonPackage):
 
     version('0.3.0', '84e69e6628fd617af469780c30d2da6c')
 
+    variant('docs', default=False, description='Build the documentation')
+    variant('pandas', default=True, description='Build with pandas')
+
     depends_on('py-setuptools',         type='build')
     depends_on('py-neo@0.3.4:',         type=('build', 'run'))  # > 0.3.3 ?
     depends_on('py-numpy@1.8.2:',       type=('build', 'run'))
     depends_on('py-quantities@0.10.1:', type=('build', 'run'))
     depends_on('py-scipy@0.14.0:',      type=('build', 'run'))
-    depends_on('py-pandas@0.14.1:',     type=('build', 'run'))
-    # depends_on('py-numpydoc@0.5:',      type=('build', 'run')) # docs
-    # depends_on('py-sphinx@1.2.2:',      type=('build', 'run')) # docs
+    depends_on('py-pandas@0.14.1:',     type=('build', 'run'), when='+pandas')
+    depends_on('py-numpydoc@0.5:',      type=('build', 'run'), when='+docs')
+    depends_on('py-sphinx@1.2.2:',      type=('build', 'run'), when='+docs')
     # depends_on('py-nose@1.3.3:',        type=('build', 'run')) # tests
