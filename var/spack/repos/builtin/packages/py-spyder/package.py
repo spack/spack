f168e6afeba3c59db9b76a7958d70c7e46605ee7
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-03-02 19:42:47 +0100

    fixed dependencies

--- var/spack/repos/builtin/packages/py-spyder/package.py
+++ var/spack/repos/builtin/packages/py-spyder/package.py
@@ -34,6 +34,7 @@ class PySpyder(PythonPackage):
     version('3.1.3', '4b9b7c8c3e6dc00001e6e98473473c36')
     version('2.3.9', 'dd01e07a77123c128ff79ba57b97c1d7')
 
+    depends_on('py-setuptools',       type='build')
     depends_on('py-rope@0.9.4:',      type=('build', 'run'), when='^python@:3')
     # depends_on('py-rope_py3k',        type=('build', 'run'), when='^python@3:')
     depends_on('py-jedi@0.9.0',       type=('build', 'run'))
@@ -50,4 +51,5 @@ class PySpyder(PythonPackage):
     depends_on('py-qtpy@1.1.0:',      type=('build', 'run'))
     depends_on('py-pyzmq',            type=('build', 'run'))
     depends_on('py-chardet@2:',       type=('build', 'run'))
+    depends_on('py-pickleshare',      type=('build', 'run'))
     depends_on('py-numpydoc',         type=('build', 'run'))
