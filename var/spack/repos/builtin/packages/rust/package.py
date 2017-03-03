54be777df232b4a58ac0205d5a8eaacc08daff26
Adam J. Stewart <ajstewart426@gmail.com>
2017-03-03 15:32:02 -0600

    Consistent docs and usage of env mod methods

--- var/spack/repos/builtin/packages/rust/package.py
+++ var/spack/repos/builtin/packages/rust/package.py
@@ -71,7 +71,7 @@ class Rust(Package):
             make()
             make("install")
 
-    def setup_dependent_package(self, module, ext_spec):
+    def setup_dependent_package(self, module, dependent_spec):
         """
         Called before python modules' install() methods.
 
