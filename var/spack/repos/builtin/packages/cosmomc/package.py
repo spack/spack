258e3e47219eb0afdcc240e05705fe225011964f
Erik Schnetter <schnetter@gmail.com>
2017-03-04 09:03:18 -0500

    cosmomc: Correct flake8 error

--- var/spack/repos/builtin/packages/cosmomc/package.py
+++ var/spack/repos/builtin/packages/cosmomc/package.py
@@ -122,33 +122,35 @@ class Cosmomc(Package):
         install('cosmomc', prefix.bin)
         root = join_path(prefix.share, 'cosmomc')
         mkdirp(root)
-        for entry in ['batch1',
-                      'batch2',
-                      'batch3',
-                      'camb',
-                      'chains',
-                      'clik_latex.paramnames',
-                      'clik_units.paramnames',
-                      'cosmomc.cbp',
-                      'data',
-                      'distgeneric.ini',
-                      'distparams.ini',
-                      'disttest.ini',
-                      'docs',
-                      'job_script',
-                      'job_script_MOAB',
-                      'job_script_SLURM',
-                      'paramnames',
-                      'params_generic.ini',
-                      'planck_covmats',
-                      'python',
-                      'scripts',
-                      # don't copy 'source'
-                      'test.ini',
-                      'test_pico.ini',
-                      'test_planck.ini',
-                      'tests',
-        ]:
+        entries = [
+            'batch1',
+            'batch2',
+            'batch3',
+            'camb',
+            'chains',
+            'clik_latex.paramnames',
+            'clik_units.paramnames',
+            'cosmomc.cbp',
+            'data',
+            'distgeneric.ini',
+            'distparams.ini',
+            'disttest.ini',
+            'docs',
+            'job_script',
+            'job_script_MOAB',
+            'job_script_SLURM',
+            'paramnames',
+            'params_generic.ini',
+            'planck_covmats',
+            'python',
+            'scripts',
+            # don't copy 'source'
+            'test.ini',
+            'test_pico.ini',
+            'test_planck.ini',
+            'tests',
+        ]
+        for entry in entries:
             if os.path.isfile(entry):
                 install(entry, root)
             else:
