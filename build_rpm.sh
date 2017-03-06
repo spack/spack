4b64ba239d991511570e692acfc3a75ed8e860fe
Jianwen WEI <weijianwen@gmail.com>
2017-03-06 19:58:40 +0800

    Remove -fPIC for perl from the command line.

--- build_rpm.sh
+++ build_rpm.sh
@@ -35,17 +35,14 @@ s cuda@8.0.44 %intel@16
 s cuda@7.5.18 %gcc@4.8.5
 s cuda@7.5.18 %intel@15
 
-# Perl
-s perl@5.24.1 %gcc@6
-s perl@5.24.1 %intel@17 cflags="-fPIC"
-
-# Python, R, Boost
+# Perl, Python, R, Boost
 for compiler in "${compilers[@]}"
 do
 s python@2.7.13 $compiler
 s python@3.6.0  $compiler
 s r@3.3.2	$compiler
 s boost		$compiler
+s perl		$compiler
 done
 
 # MPI and MPI-dependent Libraries
