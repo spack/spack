4b64154beefe10054212442f22ebb047d5e73fb2
Hans Pabst <hans.pabst@intel.com>
2017-03-06 08:31:03 +0100

    Fixed flake8 complaints.

--- var/spack/repos/builtin/packages/libxsmm/package.py
+++ var/spack/repos/builtin/packages/libxsmm/package.py
@@ -37,25 +37,22 @@ class Libxsmm(Package):
     version('develop', git='https://github.com/hfp/libxsmm.git')
 
     version('1.7.1', 'a938335b1c2c90616dc72c2c1a5824ab')
-    #version('1.7',   'cb3aff6d123ba70bd3d4daf575767d14')
+#   version('1.7',   'cb3aff6d123ba70bd3d4daf575767d14')
     version('1.6.6', '8b45ae022f36b8c212f579a0952b5034')
-    #version('1.6.5', 'fa21fe88d092477fa796a346ac7674ff')
-    #version('1.6.4', '23e484f7b9f980c2a3819db6e6c68710')
-    #version('1.6.3', '7969c56e6faed66f9e07d86f063ecf0b')
-    #version('1.6.2', '1920e58fdf00d2635d24cf5c07007bfc')
-    #version('1.6.1', 'e3493c77c57d42cfa58e0e55a69ee22c')
-    #version('1.6',   '31c130aa176db23944de420b59e1c74d')
+#   version('1.6.5', 'fa21fe88d092477fa796a346ac7674ff')
+#   version('1.6.4', '23e484f7b9f980c2a3819db6e6c68710')
+#   version('1.6.3', '7969c56e6faed66f9e07d86f063ecf0b')
+#   version('1.6.2', '1920e58fdf00d2635d24cf5c07007bfc')
+#   version('1.6.1', 'e3493c77c57d42cfa58e0e55a69ee22c')
+#   version('1.6',   '31c130aa176db23944de420b59e1c74d')
     version('1.5.2', 'ecda62ec3e5c60897d0d7780c524bc19')
-    #version('1.5.1', '612244e92024e11ec672bafb6e85c01b')
-    #version('1.5',   '7b550702b55dc8e73a42a2986a1e1b36')
+#   version('1.5.1', '612244e92024e11ec672bafb6e85c01b')
+#   version('1.5',   '7b550702b55dc8e73a42a2986a1e1b36')
     version('1.4.4', '78beefa57da02126cf4556f0eef3f8f0')
-    #version('1.4.3', '9839bf0fb8be7badf1e97ce4c817149b')
-    #version('1.4.2', 'ea025761437f3b5c936821b9ca21ec31')
-    #version('1.4.1', '71648500ea4510529845d329091917df')
-    #version('1.4',   'b42f91bf5285e7ad0463446e55ebdc2b')
-
-    #variant('shared', default=False,
-            #description='Build a shared library rather than an archive.')
+#   version('1.4.3', '9839bf0fb8be7badf1e97ce4c817149b')
+#   version('1.4.2', 'ea025761437f3b5c936821b9ca21ec31')
+#   version('1.4.1', '71648500ea4510529845d329091917df')
+#   version('1.4',   'b42f91bf5285e7ad0463446e55ebdc2b')
 
     variant('debug', default=False,
             description='Unoptimized with call-trace (LIBXSMM_TRACE).')
@@ -78,8 +75,10 @@ class Libxsmm(Package):
         install_tree('lib', prefix.lib)
         doc_path = prefix.share + '/libxsmm/doc'
         mkdirp(doc_path)
-        for doc_file in glob('documentation/*.md'):  install(doc_file, doc_path)
-        for doc_file in glob('documentation/*.pdf'): install(doc_file, doc_path)
+        for doc_file in glob('documentation/*.md'):
+            install(doc_file, doc_path)
+        for doc_file in glob('documentation/*.pdf'):
+            install(doc_file, doc_path)
         install('README.md', doc_path)
         install('LICENSE', doc_path)
 
@@ -88,10 +87,11 @@ class Libxsmm(Package):
         make_args = ['SYM=1']
 
         # JIT (AVX and later) makes MNK, M, N, or K spec. superfluous
-        #make_args += ['MNK=1 4 5 6 8 9 13 16 17 22 23 24 26 32']
+#       make_args += ['MNK=1 4 5 6 8 9 13 16 17 22 23 24 26 32']
 
         # include call trace as the build is already de-optimized
-        if '+debug' in spec: make_args += ['DBG=1 TRACE=1']
+        if '+debug' in spec:
+            make_args += ['DBG=1 TRACE=1']
 
         make(*make_args)
         self.manual_install(prefix)
