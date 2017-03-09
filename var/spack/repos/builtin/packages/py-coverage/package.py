c44fe2e1210ec51c915f55efe86ea4d75a30877f
Javier <javiercvilla@gmail.com>
2017-03-09 15:10:45 +0100

    Fixing checkum of py-coverage v4.3.4 (#3386)

--- var/spack/repos/builtin/packages/py-coverage/package.py
+++ var/spack/repos/builtin/packages/py-coverage/package.py
@@ -31,7 +31,7 @@ class PyCoverage(PythonPackage):
     homepage = "http://nedbatchelder.com/code/coverage/"
     url      = "https://pypi.io/packages/source/c/coverage/coverage-4.3.4.tar.gz"
 
-    version('4.3.4', 'd347766b06bbb4fd0bc822014b7cfb0a')
+    version('4.3.4', '89759813309185efcf4af8b9f7762630')
     version('4.0a6', '1bb4058062646148965bef0796b61efc')
 
     depends_on('py-setuptools', type='build')
