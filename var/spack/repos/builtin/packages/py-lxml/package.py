bfbd800327c1494213edeae4f864025fa72ff15c
Javier <javiercvilla@gmail.com>
2017-03-10 20:18:17 +0100

    Adding new package: py-lxml (#3403)
    * Adding new package: py-lxml
    
    * Adding missing version

---
+++ var/spack/repos/builtin/packages/py-lxml/package.py
@@ -0,0 +1,37 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class PyLxml(PythonPackage):
+    """lxml is the most feature-rich and easy-to-use library for processing
+    XML and HTML in the Python language."""
+
+    homepage = "http://lxml.de/"
+    url      = "https://pypi.io/packages/source/l/lxml/lxml-2.3.tar.gz"
+
+    version('2.3', 'a245a015fd59b63e220005f263e1682a')
+
+    depends_on('py-setuptools@0.6c5:', type='build')
