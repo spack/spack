7b484d727ee62374d399c99b7f2a45b00029bde3
Erik Schnetter <schnetter@gmail.com>
2017-03-11 13:39:33 -0500

    py-yt: New version 3.3.4 (#3420)

--- var/spack/repos/builtin/packages/py-yt/package.py
+++ var/spack/repos/builtin/packages/py-yt/package.py
@@ -37,6 +37,8 @@ class PyYt(PythonPackage):
     homepage = "http://yt-project.org"
     url = "https://bitbucket.org/yt_analysis/yt"
 
+    version("3.3.4", "3a84e56dfd82f9dd923f3fb8490e679c",
+            url="https://bitbucket.org/yt_analysis/yt/get/yt-3.3.4.tar.gz")
     version("3.3.1", hg="https://bitbucket.org/yt_analysis/yt",
             tag="yt-3.3.1", commit="9bc3d0e9b750c923d44d73c447df64fc431f5838")
     version("3.2.3", hg="https://bitbucket.org/yt_analysis/yt",
