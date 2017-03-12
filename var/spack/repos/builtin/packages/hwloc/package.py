82bce8e4fb1f9edf97ffcaba8be6e4005f79eddc
Erik Schnetter <schnetter@gmail.com>
2017-03-11 21:44:58 -0500

    hwloc: Convert `install` to `configure_args`

--- var/spack/repos/builtin/packages/hwloc/package.py
+++ var/spack/repos/builtin/packages/hwloc/package.py
@@ -54,11 +54,8 @@ class Hwloc(AutotoolsPackage):
     def url_for_version(self, version):
         return "http://www.open-mpi.org/software/hwloc/v%s/downloads/hwloc-%s.tar.gz" % (version.up_to(2), version)
 
-    def install(self, spec, prefix):
+    def configure_args(self):
         # Disable OpenCL, since hwloc might pick up an OpenCL library
         # at build time that is then not found at run time
-        configure("--prefix=%s" % prefix,
-                  "--disable-opencl")
-
-        make()
-        make("install")
+        # (Alternatively, we could require OpenCL as dependency.)
+        return ["--disable-opencl"]
