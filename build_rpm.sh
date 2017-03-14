b5d3cdda1fe75f3ead38943427d5916db0953367
Jianwen WEI <weijianwen@gmail.com>
2017-03-14 15:04:07 +0800

    Install cuda,gradle,jdk in /lustre/spack/tools.

--- build_rpm.sh
+++ build_rpm.sh
@@ -30,12 +30,6 @@ mpipkgs=(
 
 icc='%intel@17'
 
-# CUDA
-s cuda@8.0.44 %gcc@5
-s cuda@8.0.44 %intel@16
-s cuda@7.5.18 %gcc@4.8.5
-s cuda@7.5.18 %intel@15
-
 # Perl, Python, R, Boost
 for compiler in "${compilers[@]}"
 do
@@ -44,9 +38,6 @@ s python@3.6.0  $compiler
 s r@3.3.2	$compiler
 s boost		$compiler
 s perl		$compiler
-s jdk		$compiler
-s bazel		$compiler
-s gradle	$compiler
 done
 
 # MPI and MPI-dependent Libraries
--- build_system.sh
+++ build_system.sh
@@ -18,12 +18,18 @@ s gcc@5.4.0 $cc
 s gcc@6.3.0 $cc
 s llvm 	$cc
 s jdk 	$cc
+s gradle $cc
 s bazel	$cc
 s maven	$cc
 s lua-jit	$cc
 rm -f /home/rpm/spack/etc/spack/licenses/intel/license.lic; s intel-parallel-studio@cluster.2016.4$cc +openmp+mpi+mkl+ipp
 rm -f /home/rpm/spack/etc/spack/licenses/intel/license.lic; s intel-parallel-studio@cluster.2015.6$cc +openmp+mpi+mkl+ipp
 rm -f /home/rpm/spack/etc/spack/licenses/intel/license.lic; s intel-parallel-studio@cluster.2017.1$cc +openmp+mpi+mkl+ipp 
+
+# CUDA
+s cuda@8
+s cuda@7
+s cuda@6
 
 # Tools
 # s git-lfs 	$cc	
