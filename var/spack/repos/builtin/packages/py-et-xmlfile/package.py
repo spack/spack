5430d45db18f064741b86e6f61d0b03f17baf718
Adam J. Stewart <ajstewart426@gmail.com>
2017-03-14 09:44:46 -0500

    Add latest version of openpyxl and missing dependencies (#3431)

--- var/spack/repos/builtin/packages/py-et-xmlfile/package.py
+++ var/spack/repos/builtin/packages/py-et-xmlfile/package.py
@@ -25,17 +25,12 @@
 from spack import *
 
 
-class PyOpenpyxl(PythonPackage):
-    """A Python library to read/write Excel 2010 xlsx/xlsm files"""
+class PyEtXmlfile(PythonPackage):
+    """An implementation of lxml.xmlfile for the standard library."""
 
-    homepage = "http://openpyxl.readthedocs.org/"
-    url      = "https://pypi.io/packages/source/o/openpyxl/openpyxl-2.4.5.tar.gz"
+    homepage = "https://bitbucket.org/openpyxl/et_xmlfile"
+    url      = "https://pypi.io/packages/source/e/et_xmlfile/et_xmlfile-1.0.1.tar.gz"
 
-    version('2.4.5', '3de13dc9b731e1a9dd61b873d9b35a8a')
-
-    depends_on('python@2.6:2.8,3.0:3.1,3.3:')
+    version('1.0.1', 'f47940fd9d556375420b2e276476cfaf')
 
     depends_on('py-setuptools', type='build')
-
-    depends_on('py-jdcal',      type=('build', 'run'))
-    depends_on('py-et-xmlfile', type=('build', 'run'))
