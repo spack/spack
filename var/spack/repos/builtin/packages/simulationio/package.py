ba91c7706c8ac5b5357426070f2c3b114a65b33b
Erik Schnetter <schnetter@gmail.com>
2017-03-16 11:17:27 -0400

    simulationio: Remove unused imports

--- var/spack/repos/builtin/packages/simulationio/package.py
+++ var/spack/repos/builtin/packages/simulationio/package.py
@@ -23,10 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 
-
 from spack import *
-import glob
-import os
 
 
 class Simulationio(CMakePackage):
