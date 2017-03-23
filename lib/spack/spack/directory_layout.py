4780f4520b214f85b7d05bc6be958ffc42848ffd
Peter Josef Scheibel <scheibel1@llnl.gov>
2017-03-22 19:55:29 -0700

    Package install remove prior unfinished installs
    Depending on how spack is terminated in the middle of building a
    package it may leave a partially installed package in the install
    prefix. Originally Spack treated the package as being installed if
    the prefix was present, in which case the user would have to
    manually remove the installation prefix before restarting an
    install. This commit adds a more thorough check to ensure that a
    package is actually installed. If the installation prefix is present
    but Spack determines that the install did not complete, it removes
    the installation prefix and starts a new install; if the user has
    enabled --keep-prefix, then Spack reverts to its old behavior.

--- lib/spack/spack/directory_layout.py
+++ lib/spack/spack/directory_layout.py
@@ -239,6 +239,17 @@ class YamlDirectoryLayout(DirectoryLayout):
         return join_path(self.path_for_spec(spec), self.metadata_dir,
                          self.packages_dir)
 
+    def _completion_marker_file(self, spec):
+        return join_path(self.path_for_spec(spec), self.metadata_dir,
+                         'complete')
+
+    def mark_complete(self, spec):
+        with open(self._completion_marker_file(spec), 'w'):
+            pass
+
+    def completed_install(self, spec):
+        return os.path.exists(self._completion_marker_file(spec))
+
     def create_install_directory(self, spec):
         _check_concrete(spec)
 
@@ -252,26 +263,34 @@ class YamlDirectoryLayout(DirectoryLayout):
     def check_installed(self, spec):
         _check_concrete(spec)
         path = self.path_for_spec(spec)
-        spec_file_path = self.spec_file_path(spec)
 
         if not os.path.isdir(path):
             return None
+        elif not self.completed_install(spec):
+            raise InconsistentInstallDirectoryError(
+                'The prefix %s contains a partial install' % path)
+
+        self.check_metadata_consistency(spec)
+        return path
+
+    def check_metadata_consistency(self, spec):
+        spec_file_path = self.spec_file_path(spec)
 
         if not os.path.isfile(spec_file_path):
             raise InconsistentInstallDirectoryError(
                 'Install prefix exists but contains no spec.yaml:',
-                "  " + path)
+                "  " + spec.prefix)
 
         installed_spec = self.read_spec(spec_file_path)
         if installed_spec == spec:
-            return path
+            return
 
         # DAG hashes currently do not include build dependencies.
         #
         # TODO: remove this when we do better concretization and don't
         # ignore build-only deps in hashes.
         elif installed_spec == spec.copy(deps=('link', 'run')):
-            return path
+            return
 
         if spec.dag_hash() == installed_spec.dag_hash():
             raise SpecHashCollisionError(spec, installed_spec)
