8194b39028b90a865b1d5cf755c44e8c48dabc31
Alicia Klinvex <amklinv@sandia.gov>
2017-03-30 12:40:58 -0600

    Removed patch from alquimia
    It has been incorporated into alquimia, both the develop and
    rc2 tagged versions.  The 0.2.0 version of alquimia has been updated
    to tag rc2 rather than rc1.

--- var/spack/repos/builtin/packages/alquimia/package.py
+++ var/spack/repos/builtin/packages/alquimia/package.py
@@ -31,7 +31,7 @@ class Alquimia(CMakePackage):
 
     homepage = "https://github.com/LBL-EESA/alquimia-dev"
 
-    version('xsdk-0.2.0', git='https://github.com/LBL-EESA/alquimia-dev.git', tag='xsdk-0.2.0-rc1')
+    version('xsdk-0.2.0', git='https://github.com/LBL-EESA/alquimia-dev.git', tag='xsdk-0.2.0-rc2')
     version('develop', git='https://github.com/LBL-EESA/alquimia-dev.git')    
 
     variant('shared', default=True,
@@ -45,8 +45,6 @@ class Alquimia(CMakePackage):
     depends_on('pflotran@develop', when='@develop')    
     depends_on('petsc@xsdk-0.2.0', when='@xsdk-0.2.0')
     depends_on('petsc@develop', when='@develop')    
-
-    patch('petsc_arch.patch', when='@xsdk-0.2.0')
 
     def cmake_args(self):
         spec = self.spec
