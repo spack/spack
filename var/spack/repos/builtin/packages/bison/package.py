bf7da1f20b2a247acc89bcded8f852231aa5d8cb
mark <mark@opencfd>
2017-03-31 12:46:36 +0200

    CONFIG: add older bison version 2.7
    - for programs that don't yet work with bison 3.x

--- var/spack/repos/builtin/packages/bison/package.py
+++ var/spack/repos/builtin/packages/bison/package.py
@@ -34,6 +34,7 @@ class Bison(AutotoolsPackage):
     url      = "http://ftp.gnu.org/gnu/bison/bison-3.0.tar.gz"
 
     version('3.0.4', 'a586e11cd4aff49c3ff6d3b6a4c9ccf8')
+    version('2.7',   'ded660799e76fb1667d594de1f7a0da9')
 
     depends_on('m4', type=('build', 'run'))
 
