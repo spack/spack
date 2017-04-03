b1f2d3eaddf18a6d88055aaf4a51c69a6b17a2fc
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-03 16:17:19 -0500

    Flake8 fix, remove unused import

--- lib/spack/spack/test/web.py
+++ lib/spack/spack/test/web.py
@@ -23,7 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 """Tests for web.py."""
-import pytest
 import os
 
 import spack
