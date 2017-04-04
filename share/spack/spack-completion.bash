d942dde5975c09ae1b0f2fe2663ab87581e0d787
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-04-04 15:26:19 -0500

    Add -F and --show-full-compiler to bash completion

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -363,9 +363,9 @@ function _spack_find {
     if $list_options
     then
         compgen -W "-h --help -s --short -p --paths -d --deps -l --long
-                    -L --very-long -f --show-flags -e --explicit
-                    -E --implicit -u --unknown -m --missing -v --variants
-                    -M --only-missing -N --namespace" -- "$cur"
+                    -L --very-long -f --show-flags -F --show-full-compiler
+                    -e --explicit -E --implicit -u --unknown -m --missing
+                    -v --variants -M --only-missing -N --namespace" -- "$cur"
     else
         compgen -W "$(_installed_packages)" -- "$cur"
     fi
