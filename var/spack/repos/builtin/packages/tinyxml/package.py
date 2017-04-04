e3a3bb0843fdd0b360fadd1968d7a87a5797cde5
Mayeul d'Avezac <mdavezac@gmail.com>
2017-04-04 09:16:53 +0100

    URL version-parsing should work as of #2972

--- var/spack/repos/builtin/packages/tinyxml/package.py
+++ var/spack/repos/builtin/packages/tinyxml/package.py
@@ -33,7 +33,7 @@ class Tinyxml(CMakePackage):
     homepage = "http://grinninglizard.com/tinyxml/"
     url = "https://downloads.sourceforge.net/project/tinyxml/tinyxml/2.6.2/tinyxml_2_6_2.tar.gz"
 
-    version('2.6.2', 'cba3f50dd657cb1434674a03b21394df9913d764', url=url)
+    version('2.6.2', 'cba3f50dd657cb1434674a03b21394df9913d764')
 
     def patch(self):
         copyfile(join_path(os.path.dirname(__file__),
