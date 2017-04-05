048b6fc4548bc6711e51a29bfbaa42e21e56c000
Josh Asplund <joshuata@users.noreply.github.com>
2017-04-05 14:26:22 -0700

    Fixes patch paths for earlier boost versions (#3712)
    * Fixes patch paths for earlier boost versions
    
    The directory structure of boost changed at version 1.56.0, so the patch
    being used for python support did not work on earlier versions. This
    adds another patch that matches earlier versions.
    
    * Removes commented out code

--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -131,7 +131,8 @@ class Boost(Package):
     patch('boost_11856.patch', when='@1.60.0%gcc@4.4.7')
 
     # Patch fix from https://svn.boost.org/trac/boost/ticket/11120
-    patch('python_jam.patch', when='^python@3:')
+    patch('python_jam.patch', when='@1.56.0: ^python@3:')
+    patch('python_jam_pre156.patch', when='@:1.55.0 ^python@3:')
 
     # Patch fix from https://svn.boost.org/trac/boost/ticket/10125
     patch('boost_10125.patch', when='@1.55.0%gcc@5.0:5.9')
