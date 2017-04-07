5ff0f828f9639a8909acef2ea1eaf59d5d8cb8ec
miller86 <miller86@llnl.gov>
2017-04-06 23:05:33 -0700

    adding 0.5.1 and variant for uint8 bit stream word type

--- var/spack/repos/builtin/packages/zfp/package.py
+++ var/spack/repos/builtin/packages/zfp/package.py
@@ -31,23 +31,44 @@ class Zfp(Package):
        target error bounds or bit rates.  Although bit-for-bit lossless
        compression is not always possible, zfp is usually accurate to
        within machine epsilon in near-lossless mode, and is often orders
-       of magnitude more accurate than other lossy compressors.
-
+       of magnitude more accurate than other lossy compressors. Versions
+       of zfp 0.5.1 or newer also support compression of integer data.
     """
 
     homepage = "http://computation.llnl.gov/projects/floating-point-compression"
-    url      = "http://computation.llnl.gov/projects/floating-point-compression/download/zfp-0.5.0.tar.gz"
+    url      = "http://computation.llnl.gov/projects/floating-point-compression/download/zfp-0.5.1.tar.gz"
+    list_url = "http://computation.llnl.gov/projects/floating-point-compression/download"
+    list_depth = 1
 
+    version('0.5.1', '0ed7059a9b480635e0dd33745e213d17')
     version('0.5.0', '2ab29a852e65ad85aae38925c5003654')
+
+    variant('bswtuint8', default=False,
+        description='Build with bit stream word type of uint8')
+
+    def edit(self, spec, prefix):
+        config_file = FileFilter('Config')
+
+        if '+bswtunit8' in self.spec:
+            config_file.filter('#DEFS += -DBIT_STREAM_WORD_TYPE=uint8)','DEFS += -DBIT_STREAM_WORD_TYPE=uint8')
 
     def install(self, spec, prefix):
         make("shared")
+
+        zfp_incdir = 'inc'
+        if spec.satisfies('@0.5.1:'):
+            zfp_incdir = 'include'
 
         # No install provided
         mkdirp(prefix.lib)
         mkdirp(prefix.include)
         install('lib/libzfp.so', prefix.lib)
-        install('inc/zfp.h', prefix.include)
-        install('inc/types.h', prefix.include)
-        install('inc/bitstream.h', prefix.include)
-        install('inc/system.h', prefix.include)
+        install('%s/zfp.h'%zfp_incdir, prefix.include)
+        install('%s/bitstream.h'%zfp_incdir, prefix.include)
+        if spec.satisfies('@0.5.1:'):
+            mkdirp('%s/zfp'%prefix.include)
+            install('%s/zfp/system.h'%zfp_incdir, '%s/zfp'%prefix.include)
+            install('%s/zfp/types.h'%zfp_incdir, '%s/zfp'%prefix.include)
+        else:
+            install('%s/types.h'%zfp_incdir, prefix.include)
+            install('%s/system.h'%zfp_incdir, prefix.include)
