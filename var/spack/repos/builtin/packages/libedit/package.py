1e5fb03bd05662093688f2d5f13d8041b9298e21
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-07 11:03:19 -0500

    Tell libedit to link to -lncursesw (#3752)

--- var/spack/repos/builtin/packages/libedit/package.py
+++ var/spack/repos/builtin/packages/libedit/package.py
@@ -28,8 +28,9 @@ from spack import *
 class Libedit(AutotoolsPackage):
     """An autotools compatible port of the NetBSD editline library"""
     homepage = "http://thrysoee.dk/editline/"
-    url      = "http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz"
+    url      = "http://thrysoee.dk/editline/libedit-20170329-3.1.tar.gz"
 
+    version('3.1-20170329', 'c57a0690e62ef523c083598730272cfd')
     version('3.1-20160903', '0467d27684c453a351fbcefebbcb16a3')
     version('3.1-20150325', '43cdb5df3061d78b5e9d59109871b4f6')
 
@@ -38,3 +39,6 @@ class Libedit(AutotoolsPackage):
     def url_for_version(self, version):
         url = "http://thrysoee.dk/editline/libedit-{0}-{1}.tar.gz"
         return url.format(version[-1], version.up_to(-1))
+
+    def configure_args(self):
+        return ['LIBS=-lncursesw']
