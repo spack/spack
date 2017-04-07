71c82dfd4ef6f998f49bcfb08bc9944222ef8895
Milton Woods <miltonjwoods@gmail.com>
2017-04-08 04:03:57 +1000

    subversion: spec.satisfies('^swig') does not work; replace spec.satisfies by "in spec" (#3662)

--- var/spack/repos/builtin/packages/subversion/package.py
+++ var/spack/repos/builtin/packages/subversion/package.py
@@ -69,9 +69,9 @@ class Subversion(Package):
         options.append('--with-sqlite=%s' % spec['sqlite'].prefix)
         options.append('--with-serf=%s' % spec['serf'].prefix)
 
-        if spec.satisfies('^swig'):
+        if 'swig' in spec:
             options.append('--with-swig=%s' % spec['swig'].prefix)
-        if spec.satisfies('+perl'):
+        if 'perl' in spec:
             options.append(
                 'PERL=%s' % join_path(spec['perl'].prefix.bin, 'perl'))
 
