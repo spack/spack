a7f32b0e12f070de1d73db4011048f7bfb8c6cf5
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-10 15:09:59 -0500

    Rename libson-c to json-c, add new version (#3776)

--- var/spack/repos/builtin/packages/json-c/package.py
+++ var/spack/repos/builtin/packages/json-c/package.py
@@ -25,11 +25,12 @@
 from spack import *
 
 
-class LibjsonC(AutotoolsPackage):
-    """ A JSON implementation in C """
+class JsonC(AutotoolsPackage):
+    """A JSON implementation in C."""
     homepage = "https://github.com/json-c/json-c/wiki"
-    url      = "https://s3.amazonaws.com/json-c_releases/releases/json-c-0.11.tar.gz"
+    url      = "https://s3.amazonaws.com/json-c_releases/releases/json-c-0.12.1.tar.gz"
+
+    version('0.12.1', '55f7853f7d8cf664554ce3fa71bf1c7d')
+    version('0.11',   'aa02367d2f7a830bf1e3376f77881e98')
 
     parallel = False
-
-    version('0.11', 'aa02367d2f7a830bf1e3376f77881e98')
