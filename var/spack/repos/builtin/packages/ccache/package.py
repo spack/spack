4187269c1d26f6184a981c85e513c84ca656c012
JavierCvilla <javiercvilla@gmail.com>
2017-04-11 15:56:51 +0200

    Flake8 fix

--- var/spack/repos/builtin/packages/ccache/package.py
+++ var/spack/repos/builtin/packages/ccache/package.py
@@ -37,7 +37,7 @@ class Ccache(AutotoolsPackage):
     version('3.3.3', 'ea1f95303749b9ac136c617d1b333eef')
     version('3.3.2', 'b966d56603e1fad2bac22930e5f01830')
     version('3.3.1', '7102ef024cff09d353b3f4c48379b40b')
-    version('3.3'  , 'b7ac8fdd556f93831618483325fbb1ef')
+    version('3.3', 'b7ac8fdd556f93831618483325fbb1ef')
     version('3.2.9', '8f3f6e15e75a0e6020166927d41bd0b3')
 
     depends_on('gperf')
