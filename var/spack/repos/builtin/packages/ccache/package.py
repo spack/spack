a80a6849e627f78ce178cf76e9b133ea27541b3d
Javier <javiercvilla@gmail.com>
2017-04-11 16:32:31 +0200

    Update ccache version to 3.3.4 (#3794)
    * Update ccache version to 3.3.4
    
    * Flake8 fix

--- var/spack/repos/builtin/packages/ccache/package.py
+++ var/spack/repos/builtin/packages/ccache/package.py
@@ -31,12 +31,13 @@ class Ccache(AutotoolsPackage):
     again."""
 
     homepage = "https://ccache.samba.org/"
-    url      = "https://www.samba.org/ftp/ccache/ccache-3.3.3.tar.gz"
+    url      = "https://www.samba.org/ftp/ccache/ccache-3.3.4.tar.gz"
 
+    version('3.3.4', '61326f1edac7cd211a7018458dfe2d86')
     version('3.3.3', 'ea1f95303749b9ac136c617d1b333eef')
     version('3.3.2', 'b966d56603e1fad2bac22930e5f01830')
     version('3.3.1', '7102ef024cff09d353b3f4c48379b40b')
-    version('3.3'  , 'b7ac8fdd556f93831618483325fbb1ef')
+    version('3.3', 'b7ac8fdd556f93831618483325fbb1ef')
     version('3.2.9', '8f3f6e15e75a0e6020166927d41bd0b3')
 
     depends_on('gperf')
