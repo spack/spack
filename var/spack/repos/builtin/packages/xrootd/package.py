67287b1235127c94144fdae181cf899a13f43cab
JavierCvilla <javiercvilla@gmail.com>
2017-04-12 10:41:21 +0200

    Removing unnecessary url_for_version method

--- var/spack/repos/builtin/packages/xrootd/package.py
+++ var/spack/repos/builtin/packages/xrootd/package.py
@@ -31,7 +31,6 @@ class Xrootd(Package):
        tolerant access to data repositories of many kinds."""
     homepage = "http://xrootd.org"
     url      = "http://xrootd.org/download/v4.6.0/xrootd-4.6.0.tar.gz"
-    list_url = "http://xrootd.org/download"
 
     version('4.6.0', '5d60aade2d995b68fe0c46896bc4a5d1')
     version('4.5.0', 'd485df3d4a991e1c35efa4bf9ef663d7')
@@ -55,6 +54,3 @@ class Xrootd(Package):
             cmake(source_directory, *options)
             make()
             make("install")
-
-    def url_for_version(self, version):
-        return ("{0}/v{1}/xrootd-{1}.tar.gz".format(self.list_url, version))
