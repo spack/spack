6689629ec8a989294317f13cf7df0016384312b4
Christoph Junghans <christoph.junghans@gmail.com>
2017-04-14 16:53:35 -0600

    New package: boost-mpl-cartesian_product (#3841)

---
+++ var/spack/repos/builtin/packages/boostmplcartesianproduct/package.py
@@ -0,0 +1,40 @@
+##############################################################################
+# Copyright (c) 2017, Los Alamos National Security, LLC
+# Produced at the Los Alamos National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+
+from spack import *
+
+
+class Boostmplcartesianproduct(Package):
+    """Cartesian_product is an extension to the Boost.MPL library and as such
+       requires a version of the Boost libraries on your system.
+    """
+
+    homepage = "http://www.organicvectory.com/index.php?option=com_content&view=article&id=75:boostmplcartesianproduct&catid=42:boost&Itemid=78"
+    url      = "https://github.com/quinoacomputing/BoostMPLCartesianProduct/tarball/20161205"
+
+    version('20161205', 'b0c8534ee807484ffd161723cbc8fc04')
+
+    def install(self, spec, prefix):
+        install_tree('boost/mpl', join_path(prefix.include, 'boost', 'mpl'))
