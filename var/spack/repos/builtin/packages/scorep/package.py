a3ed5e786fb1d30512f2318f12b2181c6d229fa1
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-14 10:38:34 -0500

    Prevent users from building Score-P on macOS (#3825)
    * Prevent users from building Score-P on macOS
    
    * Add link to original reporting issue

--- var/spack/repos/builtin/packages/scorep/package.py
+++ var/spack/repos/builtin/packages/scorep/package.py
@@ -65,6 +65,11 @@ class Scorep(AutotoolsPackage):
 
     variant('shmem', default=False, description='Enable shmem tracing')
 
+    # Score-P requires a case-sensitive file system, and therefore
+    # does not work on macOS
+    # https://github.com/LLNL/spack/issues/1609
+    conflicts('platform=darwin')
+
     def configure_args(self):
         spec = self.spec
 
