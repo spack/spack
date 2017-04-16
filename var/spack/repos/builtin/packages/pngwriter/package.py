cc747d6b6f30238ed6f4ecec291ca0d223710057
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-16 12:23:26 -0500

    Remove redundant cmake dependencies (#3860)

--- var/spack/repos/builtin/packages/pngwriter/package.py
+++ var/spack/repos/builtin/packages/pngwriter/package.py
@@ -45,7 +45,6 @@ class Pngwriter(CMakePackage):
     version('0.6.0', '0a19bc55c5f6379fea7343752fd3ffae')
     version('0.5.6', 'c13bd1fdc0e331a246e6127b5f262136')
 
-    depends_on('cmake', type='build')
     depends_on('libpng')
     depends_on('zlib')
     depends_on('freetype')
