7e6ad2b516cfd28c649e5e3fe13c20f62d1543a9
kiel friedt <friedt2@llnl.gov>
2017-04-18 13:49:51 -0700

    missing sys

--- lib/spack/spack/cmd/test_suite.py
+++ lib/spack/spack/cmd/test_suite.py
@@ -23,6 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 import os
+import sys
 import traceback
 import spack.package
 import glob
