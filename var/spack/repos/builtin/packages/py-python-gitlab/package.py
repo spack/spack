8c0920d3dc99e813f861c7bb29ee1349715fe931
Javier <javiercvilla@gmail.com>
2017-04-21 18:47:52 +0200

    Adding new python package: py-python-gitlab (#3936)

---
+++ var/spack/repos/builtin/packages/py-python-gitlab/package.py
@@ -0,0 +1,41 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class PyPythonGitlab(PythonPackage):
+    """Python wrapper for the GitLab API"""
+
+    homepage = "https://github.com/gpocentek/python-gitlab"
+    url      = "https://pypi.io/packages/source/p/python-gitlab/python-gitlab-0.19.tar.gz"
+
+    version('0.19', '6564d7204c2b7e65c54b3fa89ec91df6')
+    version('0.18', 'c31dae1d0bab3966cb830f2308a96308')
+    version('0.17', '8a69c602e07dd4731856531d79bb58eb')
+    version('0.16', 'e0421d930718021e7d796d74d2ad7194')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-six', type=('build', 'run'))
+    depends_on('py-requests@1.0:', type=('build', 'run'))
