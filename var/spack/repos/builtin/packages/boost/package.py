096089d1f6739e5b9c26b0cec90e099e0adb9325
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-25 15:45:52 -0500

    Mark 1.64.0 unstable until +mpi+python fixed. (#3984)

--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -42,8 +42,15 @@ class Boost(Package):
     list_url = "http://sourceforge.net/projects/boost/files/boost/"
     list_depth = 1
 
+    # NOTE: 1.64.0 seems fine for *most* applications, but if you need
+    #       +python and +mpi, there seem to be errors with out-of-date
+    #       API calls from mpi/python.
+    #       See: https://github.com/LLNL/spack/issues/3963
     version('1.64.0', '93eecce2abed9d2442c9676914709349')
-    version('1.63.0', '1c837ecd990bb022d07e7aab32b09847')
+
+    # Set previous release to preferred for now, can be removed
+    # once boost+python+mpi is fixed.
+    version('1.63.0', '1c837ecd990bb022d07e7aab32b09847', preferred=True)
     version('1.62.0', '5fb94629535c19e48703bdb2b2e9490f')
     version('1.61.0', '6095876341956f65f9d35939ccea1a9f')
     version('1.60.0', '65a840e1a0b13a558ff19eeb2c4f0cbe')
