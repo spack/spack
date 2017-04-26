73f420be79521f9ed5f698094401cc59cb722170
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-26 17:34:27 -0500

    Add latest version of libxrender (#4013)

--- var/spack/repos/builtin/packages/libxrender/package.py
+++ var/spack/repos/builtin/packages/libxrender/package.py
@@ -29,9 +29,10 @@ class Libxrender(AutotoolsPackage):
     """libXrender - library for the Render Extension to the X11 protocol."""
 
     homepage = "http://cgit.freedesktop.org/xorg/lib/libXrender"
-    url      = "https://www.x.org/archive/individual/lib/libXrender-0.9.9.tar.gz"
+    url      = "https://www.x.org/archive/individual/lib/libXrender-0.9.10.tar.gz"
 
-    version('0.9.9', '0c797c4f2a7b782896bc223e6dac4333')
+    version('0.9.10', '98a14fc11aee08b4a1769426ab4b23a3')
+    version('0.9.9',  '0c797c4f2a7b782896bc223e6dac4333')
 
     depends_on('libx11@1.6:')
 
