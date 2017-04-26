dd4478487bb8e0ccc324cb6fb3c456a6f07ccb8d
Adam J. Stewart <ajstewart426@gmail.com>
2017-04-26 10:49:27 -0500

    Add list_url for bzip2 (#3988)

--- var/spack/repos/builtin/packages/bzip2/package.py
+++ var/spack/repos/builtin/packages/bzip2/package.py
@@ -34,6 +34,7 @@ class Bzip2(Package):
 
     homepage = "http://www.bzip.org"
     url      = "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
+    list_url = "http://www.bzip.org/downloads.html"
 
     version('1.0.6', '00b516f4704d4a7cb50a1d97e6e8e15b')
 
