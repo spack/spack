0cc116e6b2846a3c4d9e7e9946b0bd154af784f1
George Hartzell <hartzell@alerce.com>
2017-04-28 12:47:43 -0700

    Depend on readline, remove hardcoded -ltermcap (#4042)
    * depend on readline, remove hardcoded -ltermcap
    
    Bowtie should use Spack's readline and not explicitly depend on the
    system termcap (which, on CentOS, leads to linking against the
    system's tinfo library).
    
    * Add depends_on('zlib')
    
    * Add conflict with gcc@6:
    
    Build seems to have trouble with 6's migration to -std=gnu++14.

--- var/spack/repos/builtin/packages/bowtie2/package.py
+++ var/spack/repos/builtin/packages/bowtie2/package.py
@@ -37,9 +37,14 @@ class Bowtie2(Package):
     version('2.2.5', '51fa97a862d248d7ee660efc1147c75f')
 
     depends_on('tbb', when='@2.3.1')
+    depends_on('readline')
+    depends_on('zlib')
 
     patch('bowtie2-2.2.5.patch', when='@2.2.5', level=0)
     patch('bowtie2-2.3.1.patch', when='@2.3.1', level=0)
+
+    # seems to have trouble with 6's -std=gnu++14
+    conflicts('%gcc@6:')
 
     def install(self, spec, prefix):
         make()
