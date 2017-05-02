2de92ac0a4739e147205f92a2296acc13d5daf2f
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2017-05-02 12:13:32 -0500

    Change development to develop

--- var/spack/repos/builtin/packages/py-webkit-server/package.py
+++ var/spack/repos/builtin/packages/py-webkit-server/package.py
@@ -31,5 +31,5 @@ class PyWebkitServer(PythonPackage):
     homepage = "https://github.com/niklasb/webkit-server"
     url      = "https://pypi.io/packages/source/w/webkit-server/webkit-server-1.0.tar.gz"
 
-    version('development', git="https://github.com/niklasb/webkit-server", branch="master")
+    version('develop', git="https://github.com/niklasb/webkit-server", branch="master")
     version('1.0', '8463245c2b4f0264d934c0ae20bd4654')
