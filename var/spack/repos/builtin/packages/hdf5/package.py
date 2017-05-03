d3971bb69acec37565fe613156707896b903aad7
Massimiliano Culpo <massimiliano.culpo@googlemail.com>
2017-05-03 15:06:04 +0200

    hdf5: updated version (#4108)

--- var/spack/repos/builtin/packages/hdf5/package.py
+++ var/spack/repos/builtin/packages/hdf5/package.py
@@ -37,6 +37,7 @@ class Hdf5(AutotoolsPackage):
     list_url = "http://www.hdfgroup.org/ftp/HDF5/releases"
     list_depth = 3
 
+    version('1.10.1', '43a2f9466702fb1db31df98ae6677f15')
     version('1.10.0-patch1', '9180ff0ef8dc2ef3f61bd37a7404f295')
     version('1.10.0', 'bdc935337ee8282579cd6bc4270ad199')
     version('1.8.18', 'dd2148b740713ca0295442ec683d7b1c',
