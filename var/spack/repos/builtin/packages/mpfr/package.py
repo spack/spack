bd1896bc4e45192969faafde67d800f55e9368db
Adam J. Stewart <ajstewart426@gmail.com>
2017-05-06 12:21:38 -0500

    http -> https

--- var/spack/repos/builtin/packages/mpfr/package.py
+++ var/spack/repos/builtin/packages/mpfr/package.py
@@ -30,7 +30,7 @@ class Mpfr(AutotoolsPackage):
        floating-point computations with correct rounding."""
 
     homepage = "http://www.mpfr.org"
-    url      = "http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.5.tar.bz2"
+    url      = "https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.5.tar.bz2"
 
     version('3.1.5', 'b1d23a55588e3b2a13e3be66bc69fd8d')
     version('3.1.4', 'b8a2f6b0e68bef46e53da2ac439e1cf4')
