spack/spack

commit ce3ab503ded78d19aae45a938084d1f8670cc625

Python command, libraries, and headers (#3367)
## Motivation

Python installations are both important and unfortunately inconsistent. Depending on the Python version, OS, and the strength of the Earth's magnetic field when it was installed, the name of the Python executable, directory containing its libraries, library names, and the directory containing its headers can vary drastically. 

I originally got into this mess with #3274, where I discovered that Boost could not be built with Python 3 because the executable is called `python3` and we were telling it to use `python`. I got deeper into this mess when I started hacking on #3140, where I discovered just how difficult it is to find the location and name of the Python libraries and headers.

Currently, half of the packages that depend on Python and need to know this information jump through hoops to determine the correct information. The other half are hard-coded to use `python`, `spec['python'].prefix.lib`, and `spec['python'].prefix.include`. Obviously, none of these packages would work for Python 3, and there's no reason to duplicate the effort. The Python package itself should contain all of the information necessary to use it properly. This is in line with the recent work by @alalazo and @davydden with respect to `spec['blas'].libs` and friends.

## Prefix

lib/spack/spack/test/file_list.py
@@ -25,7 +25,7 @@
 
 import unittest
 
-from llnl.util.filesystem import LibraryList
+from llnl.util.filesystem import LibraryList, HeaderList
 
 
 class LibraryListTest(unittest.TestCase):
@@ -63,15 +63,29 @@ class LibraryListTest(unittest.TestCase):
         self.assertTrue('-L/dir2' in search_flags)
         self.assertTrue('-L/dir3' in search_flags)
         self.assertTrue(isinstance(search_flags, str))
+        self.assertEqual(
+            search_flags,
+            '-L/dir1 -L/dir2 -L/dir3'
+        )
 
         link_flags = self.liblist.link_flags
+        self.assertTrue('-llapack' in link_flags)
+        self.assertTrue('-lfoo'    in link_flags)
+        self.assertTrue('-lblas'   in link_flags)
+        self.assertTrue('-lbar'    in link_flags)
+        self.assertTrue('-lbaz'    in link_flags)
+        self.assertTrue(isinstance(link_flags, str))
         self.assertEqual(
             link_flags,
             '-llapack -lfoo -lblas -lbar -lbaz'
         )
 
         ld_flags = self.liblist.ld_flags
-        self.assertEqual(ld_flags, search_flags + ' ' + link_flags)
+        self.assertTrue(isinstance(ld_flags, str))
+        self.assertEqual(
+            ld_flags,
+            search_flags + ' ' + link_flags
+        )
 
     def test_paths_manipulation(self):
         names = self.liblist.names
@@ -108,4 +122,104 @@ class LibraryListTest(unittest.TestCase):
         self.assertEqual(
             type(pylist + self.liblist),
             type(self.liblist)
+        )
+
+
+class HeaderListTest(unittest.TestCase):
+    def setUp(self):
+        h = [
+            '/dir1/Python.h',
+            '/dir2/datetime.h',
+            '/dir1/pyconfig.h',
+            '/dir3/core.h',
+            'pymem.h'
+        ]
+        headlist = HeaderList(h)
+        headlist.add_macro('-DBOOST_LIB_NAME=boost_regex')
+        headlist.add_macro('-DBOOST_DYN_LINK')
+        self.headlist = headlist
+
+    def test_repr(self):
+        x = eval(repr(self.headlist))
+        self.assertEqual(self.headlist, x)
+
+    def test_joined_and_str(self):
+        s1 = self.headlist.joined()
+        self.assertEqual(
+            s1,
+            '/dir1/Python.h /dir2/datetime.h /dir1/pyconfig.h /dir3/core.h pymem.h'  # NOQA: ignore=E501
+        )
+        s2 = str(self.headlist)
+        self.assertEqual(s1, s2)
+        s3 = self.headlist.joined(';')
+        self.assertEqual(
+            s3,
+            '/dir1/Python.h;/dir2/datetime.h;/dir1/pyconfig.h;/dir3/core.h;pymem.h'  # NOQA: ignore=E501
+        )
+
+    def test_flags(self):
+        include_flags = self.headlist.include_flags
+        self.assertTrue('-I/dir1' in include_flags)
+        self.assertTrue('-I/dir2' in include_flags)
+        self.assertTrue('-I/dir3' in include_flags)
+        self.assertTrue(isinstance(include_flags, str))
+        self.assertEqual(
+            include_flags,
+            '-I/dir1 -I/dir2 -I/dir3'
+        )
+
+        macros = self.headlist.macro_definitions
+        self.assertTrue('-DBOOST_LIB_NAME=boost_regex' in macros)
+        self.assertTrue('-DBOOST_DYN_LINK' in macros)
+        self.assertTrue(isinstance(macros, str))
+        self.assertEqual(
+            macros,
+            '-DBOOST_LIB_NAME=boost_regex -DBOOST_DYN_LINK'
+        )
+
+        cpp_flags = self.headlist.cpp_flags
+        self.assertTrue(isinstance(cpp_flags, str))
+        self.assertEqual(
+            cpp_flags,
+            include_flags + ' ' + macros
+        )
+
+    def test_paths_manipulation(self):
+        names = self.headlist.names
+        self.assertEqual(
+            names,
+            ['Python', 'datetime', 'pyconfig', 'core', 'pymem']
+        )
+
+        directories = self.headlist.directories
+        self.assertEqual(directories, ['/dir1', '/dir2', '/dir3'])
+
+    def test_get_item(self):
+        a = self.headlist[0]
+        self.assertEqual(a, '/dir1/Python.h')
+
+        b = self.headlist[:]
+        self.assertEqual(type(b), type(self.headlist))
+        self.assertEqual(self.headlist, b)
+        self.assertTrue(self.headlist is not b)
+
+    def test_add(self):
+        pylist = [
+            '/dir1/Python.h',  # removed from the final list
+            '/dir2/pyconfig.h',
+            '/dir4/datetime.h'
+        ]
+        another = HeaderList(pylist)
+        h = self.headlist + another
+        self.assertEqual(len(h), 7)
+        # Invariant : l == l + l
+        self.assertEqual(h, h + h)
+        # Always produce an instance of HeaderList
+        self.assertEqual(
+            type(self.headlist),
+            type(self.headlist + pylist)
+        )
+        self.assertEqual(
+            type(pylist + self.headlist),
+            type(self.headlist)
         )

lib/spack/spack/package.py
@@ -64,6 +64,7 @@ from llnl.util.lang import *
 from llnl.util.link_tree import LinkTree
 from llnl.util.tty.log import log_output
 from spack import directory_layout
+from spack.util.executable import which
 from spack.stage import Stage, ResourceStage, StageComposite
 from spack.util.environment import dump_environment
 from spack.version import *
@@ -1025,17 +1026,27 @@ class PackageBase(with_metaclass(PackageMeta, object)):
         return namespace
 
     def do_fake_install(self):
-        """Make a fake install directory containing a 'fake' file in bin."""
-        # FIXME : Make this part of the 'install' behavior ?
-        mkdirp(self.prefix.bin)
-        touch(join_path(self.prefix.bin, 'fake'))
-        mkdirp(self.prefix.include)
-        mkdirp(self.prefix.lib)
+        """Make a fake install directory containing fake executables,
+        headers, and libraries."""
+
+        name = self.name
         library_name = 'lib' + self.name
-        dso_suffix = 'dylib' if sys.platform == 'darwin' else 'so'
+        dso_suffix = '.dylib' if sys.platform == 'darwin' else '.so'
+        chmod = which('chmod')
+
+        mkdirp(self.prefix.bin)
+        touch(join_path(self.prefix.bin, name))
+        chmod('+x', join_path(self.prefix.bin, name))
+
+        mkdirp(self.prefix.include)
+        touch(join_path(self.prefix.include, name + '.h'))
+
+        mkdirp(self.prefix.lib)
         touch(join_path(self.prefix.lib, library_name + dso_suffix))
         touch(join_path(self.prefix.lib, library_name + '.a'))
+
         mkdirp(self.prefix.man1)
+
         packages_dir = spack.store.layout.build_packages_path(self.spec)
         dump_packages(self.spec, packages_dir)
 

lib/spack/spack/spec.py
@@ -101,6 +101,8 @@ import collections
 import ctypes
 import hashlib
 import itertools
+import os
+
 from operator import attrgetter
 from six import StringIO
 from six import string_types
@@ -115,12 +117,13 @@ import spack.store
 import spack.util.spack_json as sjson
 import spack.util.spack_yaml as syaml
 
-from llnl.util.filesystem import find_libraries
+from llnl.util.filesystem import find_headers, find_libraries, is_exe
 from llnl.util.lang import *
 from llnl.util.tty.color import *
 from spack.build_environment import get_path_from_module, load_module
 from spack.provider_index import ProviderIndex
 from spack.util.crypto import prefix_bits
+from spack.util.executable import Executable
 from spack.util.prefix import Prefix
 from spack.util.spack_yaml import syaml_dict
 from spack.util.string import *
@@ -745,9 +748,9 @@ class FlagMap(HashableMap):
 
 
 class DependencyMap(HashableMap):
-
     """Each spec has a DependencyMap containing specs for its dependencies.
        The DependencyMap is keyed by name. """
+
     @property
     def concrete(self):
         return all((d.spec.concrete and d.deptypes)
@@ -757,38 +760,104 @@ class DependencyMap(HashableMap):
         return "{deps: %s}" % ', '.join(str(d) for d in sorted(self.values()))
 
 
-def _libs_default_handler(descriptor, spec, cls):
-    """Default handler when for ``libs`` attribute in Spec interface.
+def _command_default_handler(descriptor, spec, cls):
+    """Default handler when looking for the 'command' attribute.
 
-    Tries to search for ``lib{spec.name}`` recursively starting from
-    `spec.prefix`.
+    Tries to search for ``spec.name`` in the ``spec.prefix.bin`` directory.
 
-    Args:
-        descriptor (ForwardQueryToPackage): descriptor that triggered
-            the call
+    Parameters:
+        descriptor (ForwardQueryToPackage): descriptor that triggered the call
         spec (Spec): spec that is being queried
-        cls (type(spec)): type of spec, to match the signature of the
-            descriptor `__get__` method
-    """
-    name = 'lib' + spec.name
-    shared = '+shared' in spec
-    return find_libraries(name, root=spec.prefix, shared=shared, recurse=True)
-
-
-def _cppflags_default_handler(descriptor, spec, cls):
-    """Default handler for the ``cppflags`` attribute in the Spec interface.
-
-    The default just returns ``-I{spec.prefix.include}``.
-
-    Args:
-        descriptor (ForwardQueryToPackage): descriptor that triggered
-            the call
-        spec (Spec): spec that is being queried
-
         cls (type(spec)): type of spec, to match the signature of the
             descriptor ``__get__`` method
+
+    Returns:
+        Executable: An executable of the command
+
+    Raises:
+        RuntimeError: If the command is not found
     """
-    return '-I' + spec.prefix.include
+    path = os.path.join(spec.prefix.bin, spec.name)
+
+    if is_exe(path):
+        return Executable(path)
+    else:
+        msg = 'Unable to locate {0} command in {1}'
+        raise RuntimeError(msg.format(spec.name, spec.prefix.bin))
+
+
+def _headers_default_handler(descriptor, spec, cls):
+    """Default handler when looking for the 'headers' attribute.
+
+    Tries to search for ``*.h`` files recursively starting from
+    ``spec.prefix.include``.
+
+    Parameters:
+        descriptor (ForwardQueryToPackage): descriptor that triggered the call
+        spec (Spec): spec that is being queried
+        cls (type(spec)): type of spec, to match the signature of the
+            descriptor ``__get__`` method
+
+    Returns:
+        HeaderList: The headers in ``prefix.include``
+
+    Raises:
+        RuntimeError: If no headers are found
+    """
+    headers = find_headers('*', root=spec.prefix.include, recurse=True)
+
+    if headers:
+        return headers
+    else:
+        msg = 'Unable to locate {0} headers in {1}'
+        raise RuntimeError(msg.format(spec.name, spec.prefix.include))
+
+
+def _libs_default_handler(descriptor, spec, cls):
+    """Default handler when looking for the 'libs' attribute.
+
+    Tries to search for ``lib{spec.name}`` recursively starting from
+    ``spec.prefix``.
+
+    Parameters:
+        descriptor (ForwardQueryToPackage): descriptor that triggered the call
+        spec (Spec): spec that is being queried
+        cls (type(spec)): type of spec, to match the signature of the
+            descriptor ``__get__`` method
+
+    Returns:
+        LibraryList: The libraries found
+
+    Raises:
+        RuntimeError: If no libraries are found
+    """
+    name = 'lib' + spec.name
+
+    if '+shared' in spec:
+        libs = find_libraries(
+            name, root=spec.prefix, shared=True, recurse=True
+        )
+    elif '~shared' in spec:
+        libs = find_libraries(
+            name, root=spec.prefix, shared=False, recurse=True
+        )
+    else:
+        # Prefer shared
+        libs = find_libraries(
+            name, root=spec.prefix, shared=True, recurse=True
+        )
+        if libs:
+            return libs
+
+        libs = find_libraries(
+            name, root=spec.prefix, shared=False, recurse=True
+        )
+
+    if libs:
+        return libs
+    else:
+        msg = 'Unable to recursively locate {0} libraries in {1}'
+        raise RuntimeError(msg.format(spec.name, spec.prefix))
 
 
 class ForwardQueryToPackage(object):
@@ -797,7 +866,7 @@ class ForwardQueryToPackage(object):
     def __init__(self, attribute_name, default_handler=None):
         """Create a new descriptor.
 
-        Args:
+        Parameters:
             attribute_name (str): name of the attribute to be
                 searched for in the Package instance
             default_handler (callable, optional): default function to be
@@ -815,7 +884,7 @@ class ForwardQueryToPackage(object):
         """Retrieves the property from Package using a well defined chain
         of responsibility.
 
-        The order of call is :
+        The order of call is:
 
         1. if the query was through the name of a virtual package try to
             search for the attribute `{virtual_name}_{attribute_name}`
@@ -885,15 +954,19 @@ class ForwardQueryToPackage(object):
 
 
 class SpecBuildInterface(ObjectWrapper):
+    command = ForwardQueryToPackage(
+        'command',
+        default_handler=_command_default_handler
+    )
+
+    headers = ForwardQueryToPackage(
+        'headers',
+        default_handler=_headers_default_handler
+    )
 
     libs = ForwardQueryToPackage(
         'libs',
         default_handler=_libs_default_handler
-    )
-
-    cppflags = ForwardQueryToPackage(
-        'cppflags',
-        default_handler=_cppflags_default_handler
     )
 
     def __init__(self, spec, name, query_parameters):

lib/spack/spack/test/database.py
@@ -32,6 +32,7 @@ import os.path
 import pytest
 import spack
 import spack.store
+from spack.util.executable import Executable
 from llnl.util.tty.colify import colify
 
 
@@ -114,11 +115,17 @@ def test_default_queries(database):
     rec = install_db.get_record('zmpi')
 
     spec = rec.spec
+
     libraries = spec['zmpi'].libs
     assert len(libraries) == 1
 
-    cppflags_expected = '-I' + spec.prefix.include
-    assert spec['zmpi'].cppflags == cppflags_expected
+    headers = spec['zmpi'].headers
+    assert len(headers) == 1
+
+    command = spec['zmpi'].command
+    assert isinstance(command, Executable)
+    assert command.name == 'zmpi'
+    assert os.path.exists(command.path)
 
 
 def test_005_db_exists(database):

lib/spack/spack/util/executable.py
@@ -53,7 +53,30 @@ class Executable(object):
 
     @property
     def command(self):
+        """The command-line string.
+
+        Returns:
+            str: The executable and default arguments
+        """
         return ' '.join(self.exe)
+
+    @property
+    def name(self):
+        """The executable name.
+
+        Returns:
+            str: The basename of the executable
+        """
+        return os.path.basename(self.path)
+
+    @property
+    def path(self):
+        """The path to the executable.
+
+        Returns:
+            str: The path to the executable
+        """
+        return self.exe[0]
 
     def __call__(self, *args, **kwargs):
         """Run this executable in a subprocess.

var/spack/repos/builtin/packages/abinit/package.py
@@ -33,9 +33,9 @@ class Abinit(AutotoolsPackage):
     energy, charge density and electronic structure of systems made of
     electrons and nuclei (molecules and periodic solids) within
     Density Functional Theory (DFT), using pseudopotentials and a planewave
-    or wavelet basis. 
-    
-    ABINIT also includes options to optimize the geometry according to the 
+    or wavelet basis.
+
+    ABINIT also includes options to optimize the geometry according to the
     DFT forces and stresses, or to perform molecular dynamics
     simulations using these forces, or to generate dynamical matrices,
     Born effective charges, and dielectric tensors, based on Density-Functional
@@ -149,7 +149,7 @@ class Abinit(AutotoolsPackage):
         # LibXC library
         libxc = spec['libxc:fortran']
         options.extend([
-            'with_libxc_incs={0}'.format(libxc.cppflags),
+            'with_libxc_incs={0}'.format(libxc.headers.cpp_flags),
             'with_libxc_libs={0}'.format(libxc.libs.ld_flags + ' -lm')
         ])
 
@@ -161,7 +161,7 @@ class Abinit(AutotoolsPackage):
             hdf5 = spec['hdf5:hl']
             netcdff = spec['netcdf-fortran:shared']
             options.extend([
-                '--with-netcdf-incs={0}'.format(netcdff.cppflags),
+                '--with-netcdf-incs={0}'.format(netcdff.headers.cpp_flags),
                 '--with-netcdf-libs={0}'.format(
                     netcdff.libs.ld_flags + ' ' + hdf5.libs.ld_flags
                 ),
@@ -175,7 +175,7 @@ class Abinit(AutotoolsPackage):
 
     def check(self):
         """This method is called after the build phase if tests have been
-        explicitly activated by user. 
+        explicitly activated by user.
         """
         make('check')
         make('tests_in')

var/spack/repos/builtin/packages/ack/package.py
@@ -45,7 +45,7 @@ class Ack(Package):
         ack = 'ack-{0}-single-file'.format(self.version)
 
         # rewrite the script's #! line to call the perl dependency
-        shbang = '#!' + join_path(spec['perl'].prefix.bin, 'perl')
+        shbang = '#!' + spec['perl'].command.path
         filter_file(r'^#!/usr/bin/env perl', shbang, ack)
 
         install(ack, join_path(prefix.bin, "ack"))

commit 6fae4429aec7f5bdc4e98228872fa0eabbb4be74

filesystem.py: fixed bug introduced in #3367 (scrambled order in output)
PR #3367 inadvertently changed the semantics of _find_recursive and
_find_non_recursive so that the returned list are not ordered as the
input search list. This commit restores the original semantic, and adds
tests to verify it.

lib/spack/llnl/util/filesystem.py
@@ -551,26 +551,41 @@ def find(root, files, recurse=True):
 
 
 def _find_recursive(root, search_files):
-    found_files = []
+
+    # The variable here is **on purpose** a defaultdict. The idea is that
+    # we want to poke the filesystem as little as possible, but still maintain
+    # stability in the order of the answer. Thus we are recording each library
+    # found in a key, and reconstructing the stable order later.
+    found_files = collections.defaultdict(list)
 
     for path, _, list_files in os.walk(root):
         for search_file in search_files:
             for list_file in list_files:
                 if fnmatch.fnmatch(list_file, search_file):
-                    found_files.append(join_path(path, list_file))
+                    found_files[search_file].append(join_path(path, list_file))
 
-    return found_files
+    answer = []
+    for search_file in search_files:
+        answer.extend(found_files[search_file])
+
+    return answer
 
 
 def _find_non_recursive(root, search_files):
-    found_files = []
+    # The variable here is **on purpose** a defaultdict as os.list_dir
+    # can return files in any order (does not preserve stability)
+    found_files = collections.defaultdict(list)
 
     for list_file in os.listdir(root):
         for search_file in search_files:
             if fnmatch.fnmatch(list_file, search_file):
-                found_files.append(join_path(root, list_file))
+                found_files[search_file].append(join_path(root, list_file))
 
-    return found_files
+    answer = []
+    for search_file in search_files:
+        answer.extend(found_files[search_file])
+
+    return answer
 
 
 # Utilities for libraries and headers

lib/spack/spack/test/file_list.py
@@ -23,9 +23,14 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 
-import pytest
+import fnmatch
+import os
 
+import pytest
+import six
+import spack
 from llnl.util.filesystem import LibraryList, HeaderList
+from llnl.util.filesystem import find_libraries, find_headers
 
 
 @pytest.fixture()
@@ -201,3 +206,70 @@ class TestHeaderList(object):
         # Always produce an instance of HeaderList
         assert type(header_list + pylist) == type(header_list)
         assert type(pylist + header_list) == type(header_list)
+
+
+#: Directory where the data for the test below is stored
+search_dir = os.path.join(spack.test_path, 'data', 'directory_search')
+
+
+@pytest.mark.parametrize('search_fn,search_list,root,kwargs', [
+    (find_libraries, 'liba', search_dir, {'recurse': True}),
+    (find_libraries, ['liba'], search_dir, {'recurse': True}),
+    (find_libraries, 'libb', search_dir, {'recurse': True}),
+    (find_libraries, ['libc'], search_dir, {'recurse': True}),
+    (find_libraries, ['libc', 'liba'], search_dir, {'recurse': True}),
+    (find_libraries, ['liba', 'libc'], search_dir, {'recurse': True}),
+    (find_libraries, ['libc', 'libb', 'liba'], search_dir, {'recurse': True}),
+    (find_libraries, ['liba', 'libc'], search_dir, {'recurse': True}),
+    (find_libraries,
+     ['libc', 'libb', 'liba'],
+     search_dir,
+     {'recurse': True, 'shared': False}
+     ),
+    (find_headers, 'a', search_dir, {'recurse': True}),
+    (find_headers, ['a'], search_dir, {'recurse': True}),
+    (find_headers, 'b', search_dir, {'recurse': True}),
+    (find_headers, ['c'], search_dir, {'recurse': True}),
+    (find_headers, ['c', 'a'], search_dir, {'recurse': True}),
+    (find_headers, ['a', 'c'], search_dir, {'recurse': True}),
+    (find_headers, ['c', 'b', 'a'], search_dir, {'recurse': True}),
+    (find_headers, ['a', 'c'], search_dir, {'recurse': True}),
+    (find_libraries,
+     ['liba', 'libd'],
+     os.path.join(search_dir, 'b'),
+     {'recurse': False}
+     ),
+    (find_headers,
+     ['b', 'd'],
+     os.path.join(search_dir, 'b'),
+     {'recurse': False}
+     ),
+])
+def test_searching_order(search_fn, search_list, root, kwargs):
+
+    # Test search
+    result = search_fn(search_list, root, **kwargs)
+
+    # The tests are set-up so that something is always found
+    assert len(result) != 0
+
+    # Now reverse the result and start discarding things
+    # as soon as you have matches. In the end the list should
+    # be emptied.
+    l = list(reversed(result))
+
+    # At this point make sure the search list is a sequence
+    if isinstance(search_list, six.string_types):
+        search_list = [search_list]
+
+    # Discard entries in the order they appear in search list
+    for x in search_list:
+        try:
+            while fnmatch.fnmatch(l[-1], x) or x in l[-1]:
+                l.pop()
+        except IndexError:
+            # List is empty
+            pass
+
+    # List should be empty here
+    assert len(l) == 0