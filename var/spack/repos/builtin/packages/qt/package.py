9344a29d817fd72eb6f1db62bde85fe09c6537b9
Denis Davydov <davydden@gmail.com>
2017-05-08 21:29:08 +0200

    qt: add 5.8.0 which builds on Sierra with clang 8.1.0 (#4109)
    * qt: add 5.8.0 which builds on Sierra with clang 8.1.0
    
    * enable parallel build, add freetype dependency
    
    * minor
    
    * minor

--- var/spack/repos/builtin/packages/qt/package.py
+++ var/spack/repos/builtin/packages/qt/package.py
@@ -35,6 +35,7 @@ class Qt(Package):
     list_url = 'http://download.qt.io/archive/qt/'
     list_depth = 3
 
+    version('5.8.0',  'a9f2494f75f966e2f22358ec367d8f41')
     version('5.7.1',  '031fb3fd0c3cc0f1082644492683f18d')
     version('5.7.0',  '9a46cce61fc64c20c3ac0a0e0fa41b42')
     version('5.5.1',  '59f0216819152b77536cf660b015d784')
@@ -64,6 +65,11 @@ class Qt(Package):
 
     patch('qt3krell.patch', when='@3.3.8b+krellpatch')
 
+    # see https://bugreports.qt.io/browse/QTBUG-57656
+    patch('QTBUG-57656.patch', when='@5.8.0')
+    # see https://bugreports.qt.io/browse/QTBUG-58038
+    patch('QTBUG-58038.patch', when='@5.8.0')
+
     # https://github.com/xboxdrv/xboxdrv/issues/188
     patch('btn_trigger_happy.patch', when='@5.7.0:')
 
@@ -84,6 +90,10 @@ class Qt(Package):
     depends_on("libmng")
     depends_on("jpeg")
     depends_on("icu4c")
+    # FIXME:
+    # depends_on("freetype", when='@5.8:') and '-system-freetype'
+    # -system-harfbuzz
+    # -system-pcre
 
     # QtQml
     depends_on("python", when='@5.7.0:', type='build')
@@ -185,9 +195,18 @@ class Qt(Package):
             '-confirm-license',
             '-openssl-linked',
             '-optimized-qmake',
-            '-no-openvg',
-            '-no-pch',
+            '-no-pch'
         ]
+
+        if '@:5.7.1' in self.spec:
+            config_args.append('-no-openvg')
+        else:
+            # FIXME: those could work for other versions
+            config_args.extend([
+                '-system-libpng',
+                '-system-libjpeg',
+                '-system-zlib'
+            ])
 
         if '@:5.7.0' in self.spec:
             config_args.extend([
