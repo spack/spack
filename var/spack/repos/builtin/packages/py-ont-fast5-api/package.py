ff54f912e8625e717e17b4665ca82ebbd39f231f
JusticeForMikeBrown <quackmaster@protonmail.com>
2017-05-12 11:58:29 -0400

    removed bad characters

--- var/spack/repos/builtin/packages/py-ont-fast5-api/package.py
+++ var/spack/repos/builtin/packages/py-ont-fast5-api/package.py
@@ -26,12 +26,12 @@ from spack import *
 
 
 class PyOntFast5Api(PythonPackage):
-    """This project provides classes and utility functions for working with$
-    read fast5 files. It provides an abstraction layer between the underlying$
-    h5py library and the various concepts central to read fast5 files, such as$
+    """This project provides classes and utility functions for working with
+    read fast5 files. It provides an abstraction layer between the underlying
+    h5py library and the various concepts central to read fast5 files, such as
     "reads", "analyses", "analysis summaries", and "analysis datasets".
-    Ideally$ all interaction with a read fast5 file should be possible via this
-    API,$ without having to directly invoke the h5py library."""
+    Ideally all interaction with a read fast5 file should be possible via this
+    API, without having to directly invoke the h5py library."""
 
     homepage = "https://github.com/nanoporetech/ont_fast5_api"
     url      = "https://pypi.io/packages/source/o/ont-fast5-api/ont-fast5-api-0.3.2.tar.gz"
