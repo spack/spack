68deb61356aeff4f8728088613f0dd5ab5b557ee
Adam J. Stewart <ajstewart426@gmail.com>
2017-05-11 19:04:38 -0500

    Fix metis list_url (#4222)

--- var/spack/repos/builtin/packages/metis/package.py
+++ var/spack/repos/builtin/packages/metis/package.py
@@ -38,7 +38,7 @@ class Metis(Package):
 
     homepage = "http://glaros.dtc.umn.edu/gkhome/metis/metis/overview"
     url      = "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz"
-    list_url = "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD"
+    list_url = "http://glaros.dtc.umn.edu/gkhome/fsroot/sw/metis/OLD"
 
     version('5.1.0', '5465e67079419a69e0116de24fce58fe')
     version('5.0.2', 'acb521a4e8c2e6dd559a7f9abd0468c5')
