726de48edd97fe5640954e5cf977c11e0180304b
Erik Schnetter <schnetter@gmail.com>
2017-05-19 18:31:53 -0400

    hpx5: new version (#4273)

--- var/spack/repos/builtin/packages/hpx5/package.py
+++ var/spack/repos/builtin/packages/hpx5/package.py
@@ -39,6 +39,7 @@ class Hpx5(AutotoolsPackage):
     homepage = "http://hpx.crest.iu.edu"
     url      = "http://hpx.crest.iu.edu/release/hpx-3.1.0.tar.gz"
 
+    version('4.1.0', '43cb78758506f77416b95276a472f84f')
     version('4.0.0', 'b40dc03449ae1039cbb48ee149952b22')
     version('3.1.0', '9e90b8ac46788c009079632828c77628')
     version('2.0.0', '3d2ff3aab6c46481f9ec65c5b2bfe7a6')
