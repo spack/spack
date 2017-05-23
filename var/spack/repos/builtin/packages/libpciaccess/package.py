99926312a9d19744fc560d2c277e81fb12a15f10
Adam J. Stewart <ajstewart426@gmail.com>
2017-05-23 11:51:49 -0500

    Add latest version of libpciaccess

--- var/spack/repos/builtin/packages/libpciaccess/package.py
+++ var/spack/repos/builtin/packages/libpciaccess/package.py
@@ -29,8 +29,9 @@ class Libpciaccess(AutotoolsPackage):
     """Generic PCI access library."""
 
     homepage = "http://cgit.freedesktop.org/xorg/lib/libpciaccess/"
-    url      = "http://xorg.freedesktop.org/archive/individual/lib/libpciaccess-0.13.4.tar.gz"
+    url      = "http://xorg.freedesktop.org/archive/individual/lib/libpciaccess-0.13.5.tar.gz"
 
+    version('0.13.5', '81468664fde96d1df2c3216fdf3c4a20')
     version('0.13.4', 'cc1fad87da60682af1d5fa43a5da45a4')
 
     depends_on('libtool', type='build')
