bcc96635e2db8fcf6020529a17b94ce2c5bd79a2
JusticeForMikeBrown <quackmaster@protonmail.com>
2017-05-23 16:55:32 -0400

    mesa: added logic to only do '--enable-sysfs' on centos / rhel6 servers which have old libudev

--- var/spack/repos/builtin/packages/mesa/package.py
+++ var/spack/repos/builtin/packages/mesa/package.py
@@ -23,6 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import distro
 
 
 class Mesa(AutotoolsPackage):
@@ -33,6 +34,10 @@ class Mesa(AutotoolsPackage):
     url      = "http://ftp.iij.ad.jp/pub/X11/x.org/pub/mesa/12.0.3/mesa-12.0.3.tar.gz"
 
     version('12.0.3', '60c5f9897ddc38b46f8144c7366e84ad')
+
+    variant('dri', default=True, description='Enable DRI and EGL support')
+    variant('osmesa', default=False, description='Enable OSMesa support')
+    conflicts('+dri', '+osmesa')
 
     # General dependencies
     depends_on('python@2.6.4:')
@@ -52,10 +57,49 @@ class Mesa(AutotoolsPackage):
     depends_on('libxfixes')
 
     depends_on('glproto@1.4.14:', type='build')
-    depends_on('dri2proto@2.6:', type='build')
-    depends_on('dri3proto@1.0:', type='build')
+    depends_on('dri2proto@2.6:', when='+dri', type='build')
+    depends_on('dri3proto@1.0:', when='+dri', type='build')
     depends_on('presentproto@1.0:', type='build')
     depends_on('pkg-config@0.9.0:', type='build')
+    depends_on('llvm', when='+osmesa', type='build')
 
+    # adamjstewart
     # TODO: Add package for systemd, provides libudev
     # Using the system package manager to install systemd didn't work for me
+    # justiceformikebrown
+    # Added:
+    # ~dri will disable dri on older Centos /RHEL6 servers
+    # which lack newer udev
+    # You must also yum install libsysfs-devel
+    # https://www.mesa3d.org/osmesa.html
+    # https://www.mesa3d.org/autoconf.html
+    def configure_args(self):
+        spec = self.spec
+        version = distro.linux_distribution()[1]
+        # centos / rhel 6
+        if version.find('6.0:6.9') == -1:
+            args = ['--enable-sysfs']
+        else:
+            args = []
+
+        if '~dri' in spec:
+            args.extend([
+                '--disable-dri',
+                '--disable-egl',
+                '--disable-driglx-direct',
+                '--with-gallium-drivers=swrast'
+            ])
+        else:
+            args.extend([
+                '--enable-dri',
+                '--enable-egl'
+            ])
+
+        if '+osmesa' in spec:
+            args.extend([
+                '--enable-osmesa',
+                '--disable-driglx-direct',
+                '--disable-dri',
+                '--with-gallium-drivers=llvmpipe'
+            ])
+        return args
