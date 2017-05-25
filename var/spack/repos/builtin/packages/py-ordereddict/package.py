2050b89704f2b6ffa7c616ef03ddb4fac7b5b1d5
Adam J. Stewart <ajstewart426@gmail.com>
2017-05-25 09:49:00 -0500

    Add missing dependency to py-entrypoints (#4330)

--- var/spack/repos/builtin/packages/py-ordereddict/package.py
+++ var/spack/repos/builtin/packages/py-ordereddict/package.py
@@ -30,6 +30,8 @@ class PyOrdereddict(PythonPackage):
     OrderedDict that works in Python 2.4-2.6."""
 
     homepage = "https://pypi.python.org/pypi/ordereddict"
-    url      = "https://pypi.python.org/packages/source/o/ordereddict/ordereddict-1.1.tar.gz"
+    url      = "https://pypi.io/packages/source/o/ordereddict/ordereddict-1.1.tar.gz"
+
+    import_modules = ['ordereddict']
 
     version('1.1', 'a0ed854ee442051b249bfad0f638bbec')
