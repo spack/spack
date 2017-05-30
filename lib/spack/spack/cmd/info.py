5fb4239cc81815dbd1314185f4499d4751457b54
Adam J. Stewart <ajstewart426@gmail.com>
2017-05-30 13:37:56 -0500

    Fix spack info bug for Python 3 (#4391)

--- lib/spack/spack/cmd/info.py
+++ lib/spack/spack/cmd/info.py
@@ -25,7 +25,7 @@
 from __future__ import print_function
 
 import textwrap
-import itertools
+from six.moves import zip_longest
 from llnl.util.tty.colify import *
 import spack
 import spack.fetch_strategy as fs
@@ -119,7 +119,7 @@ class VariantFormatter(object):
                     v.description,
                     width=self.column_widths[2]
                 )
-                for t in itertools.izip_longest(
+                for t in zip_longest(
                         name, allowed, description, fillvalue=''
                 ):
                     yield "    " + self.fmt % t
