69bd119d64a70e04b207769fea5517f68626683d
Massimiliano Culpo <massimiliano.culpo@googlemail.com>
2017-06-07 21:42:15 +0200

    folly: added new package. Also modified double-conversion to use 'pic'. (#4449)

--- var/spack/repos/builtin/packages/folly/package.py
+++ var/spack/repos/builtin/packages/folly/package.py
@@ -22,29 +22,39 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+
 from spack import *
 
 
-class DoubleConversion(CMakePackage):
-    """This project (double-conversion) provides binary-decimal and decimal-binary
-    routines for IEEE doubles.
+class Folly(AutotoolsPackage):
+    """Folly (acronymed loosely after Facebook Open Source Library) is a
+    library of C++11 components designed with practicality and efficiency
+    in mind.
 
-    The library consists of efficient conversion routines that have been
-    extracted from the V8 JavaScript engine. The code has been refactored
-    and improved so that it can be used more easily in other projects.
-
-    There is extensive documentation in src/double-conversion.h. Other examples
-    can be found in test/cctest/test-conversions.cc.
+    Folly contains a variety of core library components used extensively at
+    Facebook. In particular, it's often a dependency of Facebook's other open
+    source C++ efforts and place where those projects can share code.
     """
 
-    homepage = "https://github.com/google/double-conversion"
-    url      = "https://github.com/google/double-conversion/archive/v2.0.1.zip"
+    homepage = "https://github.com/facebook/folly"
+    url = "https://github.com/facebook/folly/archive/v2017.06.05.00.tar.gz"
 
-    version('2.0.1', '5be77f780841af528e92986d46620b1e')
-    version('2.0.0', '045f7927246c368b57dcdb844ec61211')
-    version('1.1.5', 'ddf782373e2630c07b2691c31cee0b24')
-    version('1.1.4', '5df72704406d93cd54c73d73f02e2744')
-    version('1.1.3', 'b312152c8c66c80449d5e0325b94502e')
+    version('2017.06.05.00', 'a25e8d646702c3e0c1400f591e485a33')
 
-    def cmake_args(self):
-        return ['-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true']
+    depends_on('m4', type='build')
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool', type='build')
+    depends_on('pkg-config', type='build')
+
+    # TODO: folly requires gcc 4.9+ and a version of boost compiled with
+    # TODO: C++14 support (but there's no neat way to check that these
+    # TODO: constraints are met right now)
+    depends_on('boost')
+
+    depends_on('gflags')
+    depends_on('glog')
+    depends_on('double-conversion')
+    depends_on('libevent')
+
+    configure_directory = 'folly'
