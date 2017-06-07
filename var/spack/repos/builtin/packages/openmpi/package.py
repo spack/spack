4e928119394d5a4efb07c2f22df967f84dd052c4
Howard Pritchard <hppritcha@gmail.com>
2017-06-07 13:42:49 -0600

    Open MPI: add 2.0.3 (#4450)
    Add Open MPI bug fix release 2.0.3
    
    Signed-off-by: Howard Pritchard <howardp@lanl.gov>

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -72,6 +72,7 @@ class Openmpi(AutotoolsPackage):
     version('2.1.0', '4838a5973115c44e14442c01d3f21d52')  # libmpi.so.20.10.0
 
     # Still supported
+    version('2.0.3', '6c09e56ac2230c4f9abd8ba029f03edd')  # libmpi.so.20.0.3
     version('2.0.2', 'ecd99aa436a1ca69ce936a96d6a3fa48')  # libmpi.so.20.0.2
     version('2.0.1', '6f78155bd7203039d2448390f3b51c96')  # libmpi.so.20.0.1
     version('2.0.0', 'cdacc800cb4ce690c1f1273cb6366674')  # libmpi.so.20.0.0
