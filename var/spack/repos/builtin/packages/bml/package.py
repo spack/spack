8c491c1914d1d4b50ba0fdadbe287442817bb0b4
Robert Pavel <rspavel@gmail.com>
2017-06-11 16:21:51 -0600

    New Package: bml (#4213)
    * New Package: lanl-bml
    
    Added LANL's BML library as Spack Package as dependency for future
    packages
    
    * Adjusted lanl-bml versions
    
    Specified current master as develop and added v1.1.0 tagged version
    
    * Renamed lanlbml package to bml

---
+++ var/spack/repos/builtin/packages/bml/package.py
@@ -0,0 +1,49 @@
+##############################################################################
+# Copyright (c) 2017, Los Alamos National Security, LLC
+# Produced at the Los Alamos National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Bml(CMakePackage):
+    """The basic matrix library (bml) is a collection of various matrix data
+    formats (in dense and sparse) and their associated algorithms for basic
+    matrix operations."""
+
+    homepage = "https://github.com/qmmd/bml"
+    url      = "https://github.com/qmmd/bml"
+
+    version('develop', git='https://github.com/qmmd/bml', branch='master')
+    version('1.1.0', git='https://github.com/qmmd/bml', tag='v1.1.0')
+
+    variant('debug', default=False, description='Build debug version')
+
+    depends_on("blas")
+    depends_on("lapack")
+
+    def build_type(self):
+        spec = self.spec
+        if '+debug' in spec:
+            return 'Debug'
+        else:
+            return 'Release'
