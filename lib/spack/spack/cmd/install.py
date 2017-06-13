36b39c16ccecb13920622a7a655850e8a9c258de
scheibelp <scheibel1@llnl.gov>
2017-06-13 09:15:51 -0700

    Override partial installs by default - part three (#4331)
    * During install, remove prior unfinished installs
    
    If a user performs an installation which fails, in some cases the
    install prefix is still present, and the stage path may also be
    present. With this commit, unless the user specifies
    '--keep-prefix', installs are guaranteed to begin with a clean
    slate. The database is used to decide whether an install finished,
    since a database record is not added until the end of the install
    process.
    
    * test updates
    
    * repair_partial uses keep_prefix and keep_stage
    
    * use of mock stage object to ensure that stage is destroyed when it should be destroyed (and otherwise not)
    
    * add --restage option to 'install' command; when this option is not set, the default is to reuse a stage if it is found.

--- lib/spack/spack/cmd/install.py
+++ lib/spack/spack/cmd/install.py
@@ -66,6 +66,9 @@ the dependencies"""
         '--keep-stage', action='store_true', dest='keep_stage',
         help="don't remove the build stage if installation succeeds")
     subparser.add_argument(
+        '--restage', action='store_true', dest='restage',
+        help="if a partial install is detected, delete prior state")
+    subparser.add_argument(
         '-n', '--no-checksum', action='store_true', dest='no_checksum',
         help="do not check packages against checksum")
     subparser.add_argument(
@@ -307,6 +310,7 @@ def install(parser, args, **kwargs):
     kwargs.update({
         'keep_prefix': args.keep_prefix,
         'keep_stage': args.keep_stage,
+        'restage': args.restage,
         'install_deps': 'dependencies' in args.things_to_install,
         'make_jobs': args.jobs,
         'run_tests': args.run_tests,
