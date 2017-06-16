59761f47e23290856280eb912faaaa6860ba5430
David Boehme <boehme3@llnl.gov>
2017-06-16 09:31:28 -0700

    caliper: Update caliper package

--- var/spack/repos/builtin/packages/caliper/package.py
+++ var/spack/repos/builtin/packages/caliper/package.py
@@ -24,11 +24,12 @@
 ##############################################################################
 from spack import *
 
-
-class Caliper(Package):
-    """Caliper is a generic context annotation system. It gives programmers the
-    ability to provide arbitrary program context information to (performance)
-    tools at runtime.
+class Caliper(CMakePackage):
+    """Caliper is a program instrumentation and performance measurement
+    framework. It provides data collection mechanisms and a source-code
+    annotation API for a variety of performance engineering use cases,
+    e.g., performance profiling, tracing, monitoring, and
+    auto-tuning.
     """
 
     homepage = "https://github.com/LLNL/Caliper"
@@ -36,15 +37,48 @@ class Caliper(Package):
 
     version('master', git='https://github.com/LLNL/Caliper.git')
 
-    variant('mpi', default=True, description='Enable MPI function wrappers.')
+    variant('mpi', default=True, 
+            description='Enable MPI wrappers')
+    variant('dyninst', default=False, 
+            description='Enable symbol translation support with dyninst')
+    variant('callpath', default=True,
+            description='Enable callpath service (requires libunwind)')
+    variant('papi', default=True, description='Enable PAPI service')
+    variant('gotcha', default=True, description='Enable GOTCHA support')
 
-    depends_on('libunwind')
-    depends_on('papi')
+    depends_on('dyninst', when='+dyninst')
+    depends_on('papi', when='+papi')
     depends_on('mpi', when='+mpi')
-    depends_on('cmake', type='build')
+    depends_on('libunwind', when='+callpath')
 
-    def install(self, spec, prefix):
-        with working_dir('build', create=True):
-            cmake('..', *std_cmake_args)
-            make()
-            make("install")
+    depends_on('cmake', type='build')
+    depends_on('python', type='build')
+
+    def cmake_args(self):
+        spec = self.spec
+
+        args = [
+            '-DBUILD_TESTING=Off',
+            '-DWITH_DOCS=Off',
+            '-DWITH_TEST_APPS=Off'
+        ]
+
+        if '+dyninst' not in spec:
+            args.append('-DWITH_DYNINST=Off')
+        if '+callpath' not in spec:
+            args.append('-DWITH_CALLPATH=Off')
+        if '+gotcha' not in spec:
+            args.append('-DWITH_GOTCHA=Off')
+
+        if '+papi' in spec:
+            args.append('-DPAPI_PREFIX=%s' % spec['papi'].prefix)
+        else:
+            args.append('-DWITH_PAPI=Off')
+
+        if '+mpi' in spec:
+            args.append('-DMPI_C_COMPILER=%s' % spec['mpi'].mpicc)
+            args.append('-DMPI_CXX_COMPILER=%s' % spec['mpi'].mpicxx)
+        else:
+            args.append('-DWITH_MPI=Off')
+
+        return args
