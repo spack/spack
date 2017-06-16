5360528024071c246fcd943b7a84e421b8edc511
Iulian Grindeanu <iulian@mcs.anl.gov>
2017-06-16 14:41:22 -0500

    modify moab package
    add shared, debug options to cgm, too
    build some tools by default (mbsize, mbconvert)
    add hdf5, netcdf dependency
    add pnetcdf variant (dependency), metis, parmetis, zoltan
    add shared, debug, fortran options to moab

--- var/spack/repos/builtin/packages/moab/package.py
+++ var/spack/repos/builtin/packages/moab/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Moab(Package):
+class Moab(AutotoolsPackage):
     """MOAB is a component for representing and evaluating mesh
     data. MOAB can store structured and unstructured mesh, consisting
     of elements in the finite element 'zoo.' The functional interface
@@ -34,23 +34,39 @@ class Moab(Package):
     optimized for efficiency in space and time, based on access to
     mesh in chunks rather than through individual entities, while also
     versatile enough to support individual entity access."""
-    homepage = "https://bitbucket.org/fathomteam/moab"
-    url      = "http://ftp.mcs.anl.gov/pub/fathom/moab-4.9.1.tar.gz"
 
+    homepage = "https://bitbucket.org/fathomteam/moab"
+    url      = "http://ftp.mcs.anl.gov/pub/fathom/moab-5.0.0.tar.gz"
+
+    version('5.0.0', '1840ca02366f4d3237d44af63e239e3b')
+    version('4.9.2-RC1', '012c2c29e2c504c5c39131a0a10adc50')
+    version('4.9.2-RC0', '8581acec855308b34144c66e1163ad8e')
+    version('4.9.2', '540931a604c180bbd3c1bb3ee8c51dd0')
     version('4.9.1', '19cc2189fa266181ad9109b18d0b2ab8')
     version('4.9.0', '40695d0a159040683cfa05586ad4a7c2')
     version('4.8.2', '1dddd10f162fce3cfffaedc48f6f467d')
 
+    variant('mpi', default=True, description='enable mpi support')
+    variant('hdf5', default=True,
+            description='Required to enable the hdf5 (default I/O) format')
     variant('netcdf', default=False,
             description='Required to enable the ExodusII reader/writer.')
-    variant('shared', default=True,
-            description='Enables the build of shared libraries')
-    variant('fortran', default=True, description='Enable Fortran support')
-    variant('pnetcdf', default=False, description='Enable pnetcdf support')
+    variant('pnetcdf', default=False, 
+            description='Enable pnetcdf (AKA parallel-netcdf) support')
+    variant('netcdf', default=False,
+            description='Required to enable the ExodusII reader/writer.')
     variant('zoltan', default=False, description='Enable zoltan support')
     variant('cgm', default=False, description='Enable common geometric module')
     variant('metis', default=True, description='Enable metis link')
     variant('parmetis', default=True, description='Enable parmetis link')
+    variant('irel', default=False, description='Enable irel interface') 
+    variant('fbigeom', default=False, description='Enable fbigeom interface')
+    variant('coupler', default=True, description='Enable mbcoupler tool')
+
+    variant("debug", default=False, description='enable debug symbols')
+    variant('shared', default=False,
+            description='Enables the build of shared libraries')
+    variant('fortran', default=True, description='Enable Fortran support')
 
     # There are many possible variants for MOAB. Here are examples for
     # two of them:
@@ -60,51 +76,61 @@ class Moab(Package):
     # depends_on('cgns', when='+cgns')
     # depends_on('vtk', when='+vtk')
 
-    depends_on('mpi')
-    depends_on('hdf5+mpi')
+    depends_on('mpi', when='+mpi')
+    depends_on('hdf5', when='+hdf5')
     depends_on('netcdf', when='+netcdf')
-    depends_on('metis')
-    depends_on('parmetis')
+    depends_on('parallel-netcdf', when='+pnetcdf')
+    depends_on('cgm', when='+cgm')
+    depends_on('metis', when='+metis')
+    depends_on('parmetis', when='+parmetis')
     depends_on('zoltan', when='+zoltan')
 
-    def install(self, spec, prefix):
+    def configure_args(self):
+        spec = self.spec
 
         options = [
-            '--prefix=%s' % prefix,
             '--enable-optimize',
-            '--disable-hexmodops',
             '--disable-vtkMOABReader',
-            '--disable-mbskin',
             '--disable-mbtagprop',
             '--disable-mbmem',
             '--disable-spheredecomp',
             '--disable-mbsurfplot',
             '--disable-gsets',
-            '--disable-mbmerge',
-            '--disable-mbdepth',
-            '--disable-mbcoupler',
             '--disable-mcnpmit',
             '--disable-refiner',
             '--disable-h5mtools',
             '--disable-mbcslam',
-            '--with-pic',
-            '--with-mpi=%s' % spec['mpi'].prefix,
-            '--with-hdf5=%s' % spec['hdf5'].prefix,
-            '--with-parmetis=%s' % spec['parmetis'].prefix,
-            '--without-vtk',
-            'CXX=%s' % spec['mpi'].mpicxx,
-            'CC=%s' % spec['mpi'].mpicc,
-            'FC=%s' % spec['mpi'].mpifc]
-
-        if '~fortran' in spec:
-            options.append('--disable-fortran')
-        if '+shared' in spec:
-            options.append('--enable-shared')
+            '--with-pic', 
+            '--without-vtk'
+            ]
+        if '+mpi' in spec:
+            options.extend ([ 
+                '--with-mpi=%s' % spec['mpi'].prefix,
+                'CXX=%s' % spec['mpi'].mpicxx,
+                'CC=%s' % spec['mpi'].mpicc,
+                'FC=%s' % spec['mpi'].mpifc
+                ])
+            if '+parmetis' in spec:
+                 options.append('--with-parmetis=%s' % spec['parmetis'].prefix)
+                
+        if '+hdf5' in spec:
+            options.append('--with-hdf5=%s' % spec['hdf5'].prefix)
         if '+netcdf' in spec:
             options.append('--with-netcdf=%s' % spec['netcdf'].prefix)
+        if '+pnetcdf' in spec:
+            options.append('--with-pnetcdf=%s' % spec['parallel-netcdf'].prefix)
+        if '+cgm' in spec:
+            options.append('--with-cgm=%s' % spec['cgm'].prefix)
+        if '+metis' in spec:
+            options.append('--with-metis=%s' % spec['metis'].prefix)
+        if '+parmetis' in spec:
+            options.append('--with-parmetis=%s' % spec['parmetis'].prefix)
         if '+zoltan' in spec:
             options.append('--with-zoltan=%s' % spec['zoltan'].prefix)
-
-        configure(*options)
-        make()
-        make('install')
+        if '+debug' in spec:
+            options.append('--enable-debug')
+        if '+shared' in spec:
+            options.append('--enable-shared')
+        if '~fortran' in spec:
+            options.append('--disable-fortran')
+        return options
