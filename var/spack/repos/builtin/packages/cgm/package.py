171b07e7ed58dce49e40d7b84ece7360ef374ae6
iulian787 <iulian787@users.noreply.github.com>
2017-06-16 09:29:59 -0400

    oce changes for cgm (#4514)
    * oce changes for cgm
    
    cgm can be configured with oce, but oce needs
    to have X11 enabled, because some libraries
    (like TKCAF in occ/oce) gets built only of X11 is NOT disabled
    so introduce a variant +X11 for oce, which is needed when
    configuring cgm with oce(+X11)
    
    * follow up review
    
    simplify by removing the patch and different url paths
    use append instead of extend, when necessary
    make mpi true by default
    
    X11 dependency is still to be addressed; it pulls in some opengl
    library (on laptop nvidia seem to work, on virtual linux machine
    mesa was good enough)

--- var/spack/repos/builtin/packages/cgm/package.py
+++ var/spack/repos/builtin/packages/cgm/package.py
@@ -25,30 +25,40 @@
 from spack import *
 
 
-class Cgm(Package):
+class Cgm(AutotoolsPackage):
     """The Common Geometry Module, Argonne (CGMA) is a code library
        which provides geometry functionality used for mesh generation and
        other applications."""
-    homepage = "http://trac.mcs.anl.gov/projects/ITAPS/wiki/CGM"
-    url      = "http://ftp.mcs.anl.gov/pub/fathom/cgm13.1.1.tar.gz"
+    homepage = "http://sigma.mcs.anl.gov/cgm-library"         
+    url = "http://ftp.mcs.anl.gov/pub/fathom/cgm-16.0.tar.gz"
 
+    version('16.0', 'a68aa5954d82502ff75d5eb91a29a01c')
     version('13.1.1', '4e8dbc4ba8f65767b29f985f7a23b01f')
     version('13.1.0', 'a6c7b22660f164ce893fb974f9cb2028')
     version('13.1', '95f724bda04919fc76818a5b7bc0b4ed')
 
-    depends_on("mpi")
+    variant("mpi", default=True, description='enable mpi support')
+    variant("oce", default=False, description='enable oce geometry kernel')
 
-    def patch(self):
-        filter_file('^(#include "CGMParallelConventions.h")',
-                    '//\1',
-                    'geom/parallel/CGMReadParallel.cpp')
+    depends_on('mpi', when='+mpi')
+    depends_on('oce+X11', when='+oce')
 
-    def install(self, spec, prefix):
-        configure("--with-mpi",
-                  "--prefix=%s" % prefix,
-                  "CFLAGS=-static",
-                  "CXXFLAGS=-static",
-                  "FCFLAGS=-static")
+    def configure_args(self):
+        spec = self.spec
+        args = []
 
-        make()
-        make("install")
+        if '+mpi' in spec:
+            args.extend([
+                "--with-mpi",
+                "CC={0}".format(spec['mpi'].mpicc),
+                "CXX={0}".format(spec['mpi'].mpicxx)
+            ])
+        else:
+            args.append("--without-mpi")
+
+        if '+oce' in spec:
+            args.append("--with-occ={0}".format(spec['oce'].prefix))
+        else:
+            args.append("--without-occ")
+
+        return args
