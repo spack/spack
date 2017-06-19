56001e69c68ae71c9b99eadaba2c7cd2b7ae8258
Adam J. Stewart <ajstewart426@gmail.com>
2017-06-19 15:13:18 -0500

    Various updates to wget and dependencies (#4116)
    * Various updates to wget and dependencies
    
    * Add newer version of gnutls

--- var/spack/repos/builtin/packages/gnutls/package.py
+++ var/spack/repos/builtin/packages/gnutls/package.py
@@ -26,25 +26,30 @@ from spack import *
 
 
 class Gnutls(AutotoolsPackage):
-    """GnuTLS is a secure communications library implementing the SSL,
-       TLS and DTLS protocols and technologies around them. It
-       provides a simple C language application programming interface
-       (API) to access the secure communications protocols as well as
-       APIs to parse and write X.509, PKCS #12, OpenPGP and other
-       required structures. It is aimed to be portable and efficient
-       with focus on security and interoperability."""
+    """GnuTLS is a secure communications library implementing the SSL, TLS
+    and DTLS protocols and technologies around them. It provides a simple C
+    language application programming interface (API) to access the secure
+    communications protocols as well as APIs to parse and write X.509, PKCS
+    #12, OpenPGP and other required structures. It is aimed to be portable
+    and efficient with focus on security and interoperability."""
 
     homepage = "http://www.gnutls.org"
-    url      = "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.5/gnutls-3.5.10.tar.xz"
+    url      = "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.5/gnutls-3.5.13.tar.xz"
 
+    version('3.5.13', '4fd41ad86572933c2379b4cc321a0959')
     version('3.5.10', '336c03a71ba90184ffd0388075dde504')
     version('3.5.9',  '0ab25eb6a1509345dd085bc21a387951')
     version('3.3.9',  'ff61b77e39d09f1140ab5a9cf52c58b6')
 
+    variant('zlib', default=True, description='Enable zlib compression support')
+
     # Note that version 3.3.9 of gnutls doesn't support nettle 3.0.
-    depends_on("nettle@:2.9", when='@3.3.9')
-    depends_on("nettle", when='@3.5:')
-    depends_on("zlib", when='@3.5:')
+    depends_on('nettle@:2.9', when='@3.3.9')
+    depends_on('nettle', when='@3.5:')
+    depends_on('zlib', when='+zlib')
+    depends_on('gettext')
+
+    depends_on('pkg-config@0.9.0:', type='build')
 
     build_directory = 'spack-build'
 
@@ -53,10 +58,33 @@ class Gnutls(AutotoolsPackage):
         return url.format(version.up_to(2), version)
 
     def configure_args(self):
-        args = []
-        if self.spec.satisfies('@3.5:'):
+        spec = self.spec
+        args = [
+            '--enable-static',
+        ]
+
+        if spec.satisfies('@3.5:'):
             # use shipped libraries, might be turned into variants
             args.append('--with-included-libtasn1')
             args.append('--with-included-unistring')
             args.append('--without-p11-kit')  # p11-kit@0.23.1: ...
+
+        if '+zlib' in spec:
+            args.append('--with-zlib')
+        else:
+            args.append('--without-zlib')
+
+        if self.run_tests:
+            args.extend([
+                '--enable-tests',
+                '--enable-valgrind-tests',
+                '--enable-full-test-suite',
+            ])
+        else:
+            args.extend([
+                '--disable-tests',
+                '--disable-valgrind-tests',
+                '--disable-full-test-suite',
+            ])
+
         return args
