fd25dafbf0be211d875624955fd3e60e1de59390
Stanislav Sergienko <ssergien@anl.gov>
2017-06-21 13:38:50 -0500

    fixed flake8 errors

--- var/spack/repos/builtin/packages/py-pybtex/package.py
+++ var/spack/repos/builtin/packages/py-pybtex/package.py
@@ -33,12 +33,17 @@ class PyPybtex(PythonPackage):
     homepage = "https://pybtex.org"
     url      = "https://pypi.io/packages/source/P/Pybtex/pybtex-0.21.tar.gz"
 
-    import_modules = ['custom_fixers', 'pybtex', 'pybtex.style', 'pybtex.tests',
-    'pybtex.database', 'pybtex.backends', 'pybtex.bibtex', 'pybtex.charwidths',
-    'pybtex.markup', 'pybtex.plugin', 'pybtex.style.sorting', 'pybtex.style.names',
-    'pybtex.style.labels', 'pybtex.style.formatting', 'pybtex.tests.database_test',
-    'pybtex.tests.bst_parser_test', 'pybtex.tests.data', 'pybtex.database.output',
-    'pybtex.database.input', 'pybtex.database.format', 'pybtex.database.convert']
+    import_modules = [
+        'custom_fixers', 'pybtex', 'pybtex.style', 'pybtex.tests',
+        'pybtex.database', 'pybtex.backends', 'pybtex.bibtex',
+        'pybtex.charwidths', 'pybtex.markup', 'pybtex.plugin',
+        'pybtex.style.sorting', 'pybtex.style.names',
+        'pybtex.style.labels', 'pybtex.style.formatting',
+        'pybtex.tests.database_test', 'pybtex.tests.bst_parser_test',
+        'pybtex.tests.data', 'pybtex.database.output',
+        'pybtex.database.input', 'pybtex.database.format',
+        'pybtex.database.convert'
+    ]
 
     version('0.21', 'e7b320b2bcb34c664c4385533a2ea831')
 
