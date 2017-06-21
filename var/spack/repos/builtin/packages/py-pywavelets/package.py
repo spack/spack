61008ca26e05b4e43fb2707e826dada4dfbd7850
Stanislav Sergienko <ssergien@anl.gov>
2017-06-21 08:07:22 -0500

    added py-nose dependency

--- var/spack/repos/builtin/packages/py-pywavelets/package.py
+++ var/spack/repos/builtin/packages/py-pywavelets/package.py
@@ -40,3 +40,4 @@ class PyPywavelets(PythonPackage):
     depends_on('py-setuptools', type='build')
     depends_on('py-cython', type='build')
     depends_on('py-numpy@1.9.1:',  type=('build', 'run'))
+    depends_on('py-nose',  type=('build', 'run'))
