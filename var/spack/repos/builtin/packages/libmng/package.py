5cc43a17760f9488f5d1c3b991896f57ec467e4c
Adam J. Stewart <ajstewart426@gmail.com>
2017-06-22 12:52:02 -0500

    Add latest version of libmng (#4575)

--- var/spack/repos/builtin/packages/libmng/package.py
+++ var/spack/repos/builtin/packages/libmng/package.py
@@ -30,8 +30,9 @@ class Libmng(AutotoolsPackage):
        and examining Multiple-Image Network Graphics.  MNG is the animation
        extension to the popular PNG image-format."""
     homepage = "http://sourceforge.net/projects/libmng/"
-    url      = "http://downloads.sourceforge.net/project/libmng/libmng-devel/2.0.2/libmng-2.0.2.tar.gz"
+    url      = "http://downloads.sourceforge.net/project/libmng/libmng-devel/2.0.3/libmng-2.0.3.tar.gz"
 
+    version('2.0.3', '7e9a12ba2a99dff7e736902ea07383d4')
     version('2.0.2', '1ffefaed4aac98475ee6267422cbca55')
 
     depends_on("jpeg")
@@ -39,10 +40,15 @@ class Libmng(AutotoolsPackage):
     depends_on("lcms")
 
     def patch(self):
-        # jpeg requires stdio to beincluded before its headrs.
+        # jpeg requires stdio to be included before its headers.
         filter_file(r'^(\#include \<jpeglib\.h\>)',
                     '#include<stdio.h>\n\\1', 'libmng_types.h')
 
     @run_before('configure')
     def clean_configure_directory(self):
+        """Without this, configure crashes with:
+
+            configure: error: source directory already configured;
+            run "make distclean" there first
+        """
         make('distclean')
