ae53b6bea8af389ddc06c2c6f4b3563de9661456
Milton Woods <milton.woods@bom.gov.au>
2017-06-22 14:19:16 +1000

    perl: convert tabs added by editor to spaces for flake8

--- var/spack/repos/builtin/packages/perl/package.py
+++ var/spack/repos/builtin/packages/perl/package.py
@@ -93,20 +93,20 @@ class Perl(Package):  # Perl doesn't use Autotools, it should subclass Package
             '-Dloclibpth=' + self.spec['gdbm'].prefix.lib,
         ]
 
-	# Extensions are installed into their private tree via
-	# `INSTALL_BASE`/`--install_base` (see [1]) which results in a
-	# "predictable" installation tree that sadly does not match the
-	# Perl core's @INC structure.  This means that when activation
-	# merges the extension into the extendee[2], the directory tree
-	# containing the extensions is not on @INC and the extensions can
-	# not be found.
-	#
-	# This bit prepends @INC with the directory that is used when
-	# extensions are activated [3].
-	#
-	# [1] https://metacpan.org/pod/ExtUtils::MakeMaker#INSTALL_BASE
-	# [2] via the activate method in the PackageBase class
-	# [3] https://metacpan.org/pod/distribution/perl/INSTALL#APPLLIB_EXP
+        # Extensions are installed into their private tree via
+        # `INSTALL_BASE`/`--install_base` (see [1]) which results in a
+        # "predictable" installation tree that sadly does not match the
+        # Perl core's @INC structure.  This means that when activation
+        # merges the extension into the extendee[2], the directory tree
+        # containing the extensions is not on @INC and the extensions can
+        # not be found.
+        #
+        # This bit prepends @INC with the directory that is used when
+        # extensions are activated [3].
+        #
+        # [1] https://metacpan.org/pod/ExtUtils::MakeMaker#INSTALL_BASE
+        # [2] via the activate method in the PackageBase class
+        # [3] https://metacpan.org/pod/distribution/perl/INSTALL#APPLLIB_EXP
         config_args.append('-Accflags=-DAPPLLIB_EXP=\\"' + join_path(
                            self.prefix.lib, 'perl5') + '\\"')
 
