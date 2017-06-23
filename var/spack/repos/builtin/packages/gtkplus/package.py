18b9439dc2f5a35dee28e16521da057396c19f46
Milton Woods <miltonjwoods@gmail.com>
2017-06-24 00:01:09 +1000

    Gtkplus icons (#4511)
    * gtkplus: set XDG_DATA_DIRS
    
    * shared-mime-info: database of common MIME types
    
    * gtkplus: needs shared-mime-info for correct handling of stock icons
    
    * shared-mime-info: builds with newer intltool than provided by some platforms
    
    * atk: set XDG_DATA_DIRS at run-time
    
    * gdk-pixbuf: set XDG_DATA_DIRS at run-time
    
    * gtkplus: set XDG_DATA_DIRS at run-time
    
    * pango: set XDG_DATA_DIRS at run-time
    
    * shared-mime-info: set XDG_DATA_DIRS at run-time

--- var/spack/repos/builtin/packages/gtkplus/package.py
+++ var/spack/repos/builtin/packages/gtkplus/package.py
@@ -45,6 +45,7 @@ class Gtkplus(AutotoolsPackage):
     depends_on("pango~X", when='~X')
     depends_on("pango+X", when='+X')
     depends_on('gobject-introspection', when='+X')
+    depends_on('shared-mime-info')
 
     patch('no-demos.patch')
 
@@ -52,3 +53,9 @@ class Gtkplus(AutotoolsPackage):
         # remove disable deprecated flag.
         filter_file(r'CFLAGS="-DGDK_PIXBUF_DISABLE_DEPRECATED $CFLAGS"',
                     '', 'configure', string=True)
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        spack_env.prepend_path("XDG_DATA_DIRS",
+                               self.prefix.share)
+        run_env.prepend_path("XDG_DATA_DIRS",
+                             self.prefix.share)
