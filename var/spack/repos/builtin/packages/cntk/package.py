7932738ba14c2bd312ebe7c0bc8de05f1100e567
Jimmy Tang <jcftang@gmail.com>
2017-06-23 15:18:50 +0100

    Minor fixes to CNTK package (#4429)
    * Remove unneeded patch lines which fail for the most recent CNTK version on the head of the git repo
    
    * Really check for NVML/GDK
    
    * Fix some violations

--- var/spack/repos/builtin/packages/cntk/package.py
+++ var/spack/repos/builtin/packages/cntk/package.py
@@ -31,10 +31,10 @@ class Cntk(Package):
     via a directed graph."""
 
     homepage = "https://www.microsoft.com/en-us/research/product/cognitive-toolkit"
-    url      = "https://github.com/Microsoft/CNTK/archive/v2.0.beta15.0.tar.gz"
+    url      = "https://github.com/Microsoft/CNTK/archive/v2.0.tar.gz"
 
     version('master', git='https://github.com/Microsoft/CNTK.git', branch='master')
-    version('2.0.rc1', url='https://github.com/Microsoft/CNTK/archive/v2.0.rc1.tar.gz')
+    version('2.0', '8038780f1169ceea578e5ef4d69e4c6f')
 
     variant('opencv', default=False, description="Enable OpenCV support.")
     variant('kaldi', default=False, description="Enable Kaldi support.")
@@ -106,6 +106,10 @@ class Cntk(Package):
             args.append('--with-cudnn={0}'
                         .format(spec['cudnn'].prefix))
             args.append('--with-nccl={0}'.format(spec['nccl'].prefix))
+            args.append('--with-gdk-include={0}'
+                        .format(spec['cuda'].prefix.include))
+            args.append('--with-gdk-nvml-lib={0}/stubs'
+                        .format(spec['cuda'].prefix.lib64))
 
         configure(*args)
 
