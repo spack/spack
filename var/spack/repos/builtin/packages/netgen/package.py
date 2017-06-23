23abdd2103996aeaec83095be12d4213711cae57
iulian787 <iulian787@users.noreply.github.com>
2017-06-23 09:53:43 -0400

    Moab 5.0 and meshkit 1.5 (#4557)
    * enable some of the tools
    
    do not make zoltan default
    
    * modify moab package
    
    add shared, debug options to cgm, too
    build some tools by default (mbsize, mbconvert)
    add hdf5, netcdf dependency
    add pnetcdf variant (dependency), metis, parmetis, zoltan
    add shared, debug, fortran options to moab
    
    * few kinks for moab
    
    zoltan should be built without fortran api, otherwise moab
    complains (need to fix)
    also, notice that when built with cgm, shared can't find
    cgm libraries for the new tools/geometry execs
    
    install in serial for the time being (because of example makefile
    duplicate?)
    
    for example, these builds are successful:
    spack install moab+mpi+hdf5+zoltan ^mpich@3.2
    spack install moab+mpi+hdf5+shared ^mpich@3.2
    
    * force hdf5+mpi if both are specified
    
    also, something like this works:
    spack install  moab+mpi+hdf5+cgm ^mpich@3.2 ^cgm+oce+mpi ^oce@0.17.2
    
    use mpich32, and cgm built with oce 0.17.2
    
    * forgot about irel, fbigeom, mbcoupler
    
    * add meshkit package
    
    simple build so far, moab and cgm deps only
    
    also, an example of complex build for cgm
    spack install  moab+mpi+hdf5+cgm+irel+fbigeom ^mpich@3.2 ^cgm+oce+mpi ^oce@0.17.2
    
    * for meshkit, moab needs to have irel, fbigeom
    
    * forgot to self.spec
    
    after building moab with cgm with oce 0.17.2, we can build
    meshkit with something like this
    spack install meshkit ^moab/tsb75zk
    
    cgm depencency is found out from moab
    
    moab has to be built with irel and fbigeom
    
    * add netgen package and review
    
    do not support yet older versions of meshkit
    (which depend on lasso, etc)
    add netgen package; tested with meshkit (netgen has to be built without
    occ, for meshkit)
    We are not enforcing that yet, we may have to test
    
    * use conflicts where needed, suggested by review
    
    remove release candidates
    
    * flake8 alignment errors
    
    * flake8
    
    * reviews
    
    flake8 alignment
    explicit options, even for default variants
    variant for netgen should be "gui", with the default ~gui (False)
    FIXME: with-occ does not work right for netgen ; maybe it should be disabled?
           also, with +gui, it should depend on a lot more, like tk, tcl?
    
    * flake8 issues
    
    whitespaces and a comment in netgen
    
    * add more explicit options
    
    --without-mpi needs fixing for cgm, moab and meshkit
    add variable url for netgen (although we don''t know if other
    versions will appear)
    
    * flake8, trailing whitespace

--- var/spack/repos/builtin/packages/netgen/package.py
+++ var/spack/repos/builtin/packages/netgen/package.py
@@ -25,33 +25,37 @@
 from spack import *
 
 
-class Cgm(AutotoolsPackage):
-    """The Common Geometry Module, Argonne (CGMA) is a code library
-       which provides geometry functionality used for mesh generation and
-       other applications."""
-    homepage = "http://sigma.mcs.anl.gov/cgm-library"         
-    url = "http://ftp.mcs.anl.gov/pub/fathom/cgm-16.0.tar.gz"
+class Netgen(AutotoolsPackage):
+    """NETGEN is an automatic 3d tetrahedral mesh generator. It accepts
+       input from constructive solid geometry (CSG) or boundary
+       representation (BRep) from STL file format. The connection to
+       a geometry kernel allows the handling of IGES and STEP files.
+       NETGEN contains modules for mesh optimization and hierarchical
+       mesh refinement. """
 
-    version('16.0', 'a68aa5954d82502ff75d5eb91a29a01c')
-    version('13.1.1', '4e8dbc4ba8f65767b29f985f7a23b01f')
-    version('13.1.0', 'a6c7b22660f164ce893fb974f9cb2028')
-    version('13.1', '95f724bda04919fc76818a5b7bc0b4ed')
+    homepage = "https://ngsolve.org/"
+    url = "https://gigenet.dl.sourceforge.net/project/netgen-mesher/netgen-mesher/5.3/netgen-5.3.1.tar.gz"
+
+    version('5.3.1', 'afd5a9b0b1296c242a9c554f06af6510')
 
     variant("mpi", default=True, description='enable mpi support')
     variant("oce", default=False, description='enable oce geometry kernel')
-    variant("debug", default=False, description='enable debug symbols')
-    variant("shared", default=False, description='enable shared builds')
+    variant("gui", default=False, description='enable gui')
+    variant("metis", default=False, description='use metis for partitioning')
 
     depends_on('mpi', when='+mpi')
     depends_on('oce+X11', when='+oce')
+    depends_on('metis', when='+metis')
+
+    def url_for_version(self, version):
+        url = "https://gigenet.dl.sourceforge.net/project/netgen-mesher/netgen-mesher/{0}/netgen-{1}.tar.gz"
+        return url.format(version.up_to(2), version)
 
     def configure_args(self):
         spec = self.spec
         args = []
-
         if '+mpi' in spec:
             args.extend([
-                "--with-mpi",
                 "CC={0}".format(spec['mpi'].mpicc),
                 "CXX={0}".format(spec['mpi'].mpicxx)
             ])
@@ -60,13 +64,20 @@ class Cgm(AutotoolsPackage):
 
         if '+oce' in spec:
             args.append("--with-occ={0}".format(spec['oce'].prefix))
+        #  FIXME
+        # due to a bug in netgen config, when --without-occ is specified
+        #   or --with-occ=no, OCC flags is turned true, and build fails
+        #   later; so do not specify anything like that
+        # else:
+        #    args.append("--without-occ")
+
+        if '~gui' in spec:
+            args.append("--disable-gui")
         else:
-            args.append("--without-occ")
-
-        if '+debug' in spec:
-            args.append("--enable-debug")
-
-        if '+shared' in spec:
-            args.append("--enable-shared")
+            args.append("--enable-gui")
+        if '+metis' in spec:
+            args.append('--with-metis=%s' % spec['metis'].prefix)
+        else:
+            args.append("--without-metis")
 
         return args
