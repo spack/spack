cab3b8ce51c0b13118d68a1f9545121ab537dfdf
miller86 <miller86@llnl.gov>
2017-06-23 10:18:35 -0700

    fixing flake8 issue

--- lib/spack/spack/cmd/info.py
+++ lib/spack/spack/cmd/info.py
@@ -106,7 +106,8 @@ class VariantFormatter(object):
         else:
             yield "    " + self.fmt % self.headers
             yield '\n'
-            for k, v in sorted(self.variants.items(), key=lambda t: t[1].iorder):
+            for k, v in sorted(self.variants.items(),
+                               key=lambda t: t[1].iorder):
                 name = textwrap.wrap(
                     '{0} [{1}]'.format(k, self.default(v)),
                     width=self.column_widths[0]
