cac4362f6491bf48dd287f31f3de9169464713ea
Todd Gamblin <tgamblin@llnl.gov>
2017-06-24 22:22:55 -0700

    Make LICENSE recognizable by GitHub. (#4598)

--- var/spack/repos/builtin/packages/foam-extend/package.py
+++ var/spack/repos/builtin/packages/foam-extend/package.py
@@ -7,7 +7,7 @@
 # LLNL-CODE-647188
 #
 # For details, see https://github.com/llnl/spack
-# Please also see the LICENSE file for our notice and the LGPL.
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
 #
 # License
 # -------
