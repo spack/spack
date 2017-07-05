e6938840378add76eaa4a049f32f0accf61ae727
Mehta, Kshitij V <kshitij-v-mehta@users.noreply.github.com>
2017-07-05 09:24:57 -0400

    codar-cheetah: conforming to flake8

--- var/spack/repos/builtin/packages/codar-cheetah/package.py
+++ var/spack/repos/builtin/packages/codar-cheetah/package.py
@@ -25,19 +25,20 @@
 from spack import *
 from distutils.dir_util import copy_tree
 
+
 class CodarCheetah(Package):
     """CODAR Cheetah:
     The CODAR Experiment Harness for Exascale science applications.
     """
 
     homepage = "https://github.com/CODARcode/cheetah"
-    url      = "https://github.com/CODARcode/cheetah/releases/tag/v0.1.tar.gz"
+    url = "https://github.com/CODARcode/cheetah/releases/tag/v0.1.tar.gz"
 
     version('0.1', '6918021f74fa7a2f1de26c0bb31a63ef')
     version('develop', git='https://github.com/CODARcode/cheetah.git',
             branch='master')
 
-    depends_on('python@3:')
+    depends_on('python@3:', type=('build', 'run'))
     depends_on('savanna')
 
     def install(self, spec, prefix):
