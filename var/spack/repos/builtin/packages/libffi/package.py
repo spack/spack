d1ba46c8aff2a58a465bb783639ff4f414518ed0
Mario Melara <maamelara@gmail.com>
2017-07-06 10:58:08 -0700

    Add shared variant

--- var/spack/repos/builtin/packages/libffi/package.py
+++ var/spack/repos/builtin/packages/libffi/package.py
@@ -34,6 +34,19 @@ class Libffi(AutotoolsPackage):
 
     version('3.2.1', '83b89587607e3eb65c70d361f13bab43',
             url="https://www.mirrorservice.org/sites/sourceware.org/pub/libffi/libffi-3.2.1.tar.gz")
+    variant("shared", default=True, description="Enable shared libs")
+
+    def configure_args(self):
+        args = []
+        if "+shared" in self.spec:
+            args.append("--enable-shared")
+        else:
+            args.append("--disable-shared")
+        
+        if self.spec.satisfies("%intel") and self.spec.satisfies("os=cnl5"):
+            args.append("--host=x86_64")
+
+        return args
     # version('3.1', 'f5898b29bbfd70502831a212d9249d10',url =
     # "ftp://sourceware.org/pub/libffi/libffi-3.1.tar.gz") # Has a bug
     # $(lib64) instead of ${lib64} in libffi.pc
