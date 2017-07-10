a283f07f09f7b0cacd4d792fcedcda74022a85fc
pkondamudi <pkondamudi@anl.gov>
2017-07-10 16:11:48 -0500

    minor changes

--- var/spack/repos/builtin/packages/nekbone/package.py
+++ var/spack/repos/builtin/packages/nekbone/package.py
@@ -22,21 +22,6 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install nekbone
-#
-# You can edit this file again by typing:
-#
-#     spack edit nekbone
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 import os
 
@@ -47,16 +32,12 @@ class Nekbone(Package):
        Nek5000 is a high order, incompressible Navier-Stokes solver based on
        the spectral element method."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/ANL-CESAR/"
     url = "https://github.com/ANL-CESAR/nekbone.git"
 
     tags = ['proxy-app']
 
-    # FIXME: Add proper versions and checksums here.
     version('master', git='https://github.com/ANL-CESAR/nekbone.git')
-
-    # FIXME: Add dependencies if required.
 
     def install(self, spec, prefix):
 
