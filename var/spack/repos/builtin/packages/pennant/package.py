ed093a9fd652a5200905c348a9c16f8050665e68
Anna Kim <jiseung@github.com>
2017-07-10 21:16:10 +0000

    made adjustments based on comments

--- var/spack/repos/builtin/packages/pennant/package.py
+++ var/spack/repos/builtin/packages/pennant/package.py
@@ -22,21 +22,6 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install pennant
-#
-# You can edit this file again by typing:
-#
-#     spack edit pennant
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
@@ -70,9 +55,9 @@ class Pennant(MakefilePackage):
         debug = '-g'
         opt = '-O3'
 
-        if self.compiler.name == 'icpc':
+        if self.compiler.name == 'intel':
             opt += ' -fast -fno-alias'
-        if self.compiler.name == 'pgCC':
+        if self.compiler.name == 'pgi':
             opt += ' -fastsse'
 
         makefile.filter('CXX .*', 'CXX := c++')
@@ -92,9 +77,7 @@ class Pennant(MakefilePackage):
                 'CXX := {0}'.format(spec['mpi'].mpicxx))
 
         if '+mpi' not in spec:
-            makefile.filter('CXX := mpicxx', '#')
             makefile.filter('-DUSE_MPI', '#')
-            makefile.filter('CXX .*', 'CXX := c++')
 
         if '+openmp' not in spec:
             makefile.filter('.*CXXFLAGS_OPENMP.*', '#')
