8ce164b384104ec5db6e2dc8bfa4cd4a13438ba3
Patrick Gartung <gartung@fnal.gov>
2017-07-13 14:21:22 -0500

    autopep8:

--- var/spack/repos/builtin/packages/gcc/package.py
+++ var/spack/repos/builtin/packages/gcc/package.py
@@ -36,7 +36,7 @@ class Gcc(AutotoolsPackage):
     Fortran, Ada, and Go, as well as libraries for these languages."""
 
     homepage = 'https://gcc.gnu.org'
-    url      = 'http://ftp.gnu.org/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.bz2'
+    url = 'http://ftp.gnu.org/gnu/gcc/gcc-7.1.0/gcc-7.1.0.tar.bz2'
     list_url = 'http://ftp.gnu.org/gnu/gcc/'
     list_depth = 1
 
@@ -80,9 +80,9 @@ class Gcc(AutotoolsPackage):
     variant('rpathlibgcc',
             default=False,
             description='Apply patch to set libgcc_s.dylib Mach-O ID to' +
-                    ' @rpath/libgcc_s.dylib for relocatability ' + 
-                    'Note: using this variant requires setting [extra_rpaths] '+
-                    'in compilers.yaml to the compiler library path' )
+            ' @rpath/libgcc_s.dylib for relocatability ' +
+            'Note: using this variant requires setting [extra_rpaths] ' +
+            'in compilers.yaml to the compiler library path')
 
     # https://gcc.gnu.org/install/prerequisites.html
     depends_on('gmp@4.3.2:')
@@ -201,9 +201,8 @@ class Gcc(AutotoolsPackage):
                         'gcc/Makefile.in')
         if spec.satisfies('+rpathlibgcc'):
             # Make libgcc_s relocatable
-            filter_file(r"@shlib_slibdir@", "@rpath", 
+            filter_file(r"@shlib_slibdir@", "@rpath",
                         'libgcc/config/t-slibgcc-darwin', string=True)
-
 
     def configure_args(self):
         spec = self.spec
