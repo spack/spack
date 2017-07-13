f1887e6455f161c204c9457f4e25ded6227c5bd1
Brian Homerding <homerdin@gmail.com>
2017-07-13 14:35:12 +0000

    Changed to make Makefile edit more specific

--- var/spack/repos/builtin/packages/pathfinder/package.py
+++ var/spack/repos/builtin/packages/pathfinder/package.py
@@ -40,8 +40,7 @@ class Pathfinder(MakefilePackage):
 
     def edit(self, spec, prefix):
         makefile = FileFilter('PathFinder_ref/Makefile')
-        makefile.filter('CFLAGS += .*',
-                        'CFLAGS += {0}'.format(self.compiler.openmp_flag))
+        makefile.filter('-fopenmp','{0}'.format(self.compiler.openmp_flag))
 
     def install(self, spec, prefix):
         # Manual installation
