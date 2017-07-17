4c5cad50e3b4935594552864e616b4355bc650f2
alalazo <massimiliano.culpo@googlemail.com>
2017-07-17 12:22:49 +0200

    spack info: added basic test

--- lib/spack/spack/test/cmd/info.py
+++ lib/spack/spack/test/cmd/info.py
@@ -22,11 +22,37 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+import argparse
+
 import pytest
+import spack.cmd.info
+import spack.cmd.info as info
 
 from spack.main import SpackCommand
 
 info = SpackCommand('info')
+
+@pytest.fixture(scope='module')
+def parser():
+    """Returns the parser for the module command"""
+    prs = argparse.ArgumentParser()
+    info.setup_parser(prs)
+    return prs
+
+
+@pytest.fixture()
+def info_lines():
+    lines = []
+    return lines
+
+
+@pytest.fixture()
+def mock_print(monkeypatch, info_lines):
+
+    def _print(*args):
+        info_lines.extend(args)
+
+    monkeypatch.setattr(spack.cmd.info, 'print', _print, raising=False)
 
 
 @pytest.mark.parametrize('pkg', [
@@ -38,3 +64,29 @@ info = SpackCommand('info')
 ])
 def test_it_just_runs(pkg):
     info(pkg)
+
+
+@pytest.mark.parametrize('pkg_query', [
+    'hdf5',
+    'cloverleaf3d',
+    'trilinos'
+])
+@pytest.mark.usefixtures('mock_print')
+def test_info_fields(pkg_query, parser, info_lines):
+
+    expected_fields = (
+        'Description:',
+        'Homepage:',
+        'Safe versions:',
+        'Variants:',
+        'Installation Phases:',
+        'Virtual Packages:',
+        'Tags:'
+    )
+
+    args = parser.parse_args([pkg_query])
+    spack.cmd.info.info(parser, args)
+
+    for text in expected_fields:
+        match = [x for x in info_lines if text in x]
+        assert match
