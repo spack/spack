46c70bb8a70b6807329f399245a1ffbfcd233780
Adam Fidel <refuse@gmail.com>
2017-07-17 14:54:10 -0500

    Removing template.

--- var/spack/repos/builtin/packages/tig/package.py
+++ var/spack/repos/builtin/packages/tig/package.py
@@ -22,23 +22,7 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install tig
-#
-# You can edit this file again by typing:
-#
-#     spack edit tig
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
-
 
 class Tig(AutotoolsPackage):
     """Text-mode interface for git"""
