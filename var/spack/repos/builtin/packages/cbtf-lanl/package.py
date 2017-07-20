a3dbe88079735522c735549ba0b3a9fed00af51e
Jim Galarowicz <jeg@krellinst.org>
2017-07-20 13:25:57 -0500

    Spack updates to three Krell packages (#4765)
    * Update the krell institute products to use the latest features of spack for building on cluster platforms.
    
    * Address travis error messages and resubmit the pull request.
    
    * Update the contents of openspeedshop package.py so it passes the flake8 tests.
    
    * Fix flake8 error-whitespack issue in mrnet package.py file.
    
    * Add updates based on spack reviewer feedback.
    
    * More fixes based on comments from reviewers.  Switch using extend to using append, remove additional setting of PATH and LD_LIBRARY_PATH that should not be required due to RPATH.
    
    * More review related changes.  Update MPIOption.append lines and take out xercesc references.
    
    * Create a base options function for common openspeedshop base cmake options to reduce redundencies.
    
    * Add libxml2+python depends on to get around issues with the libxml2 package file.
    
    * Using boost over 1.60.0 causes compile errors.  This is a known boost bug. Also, dyninst-9.2.0 is set to be the vesrion of dyninst to use with OSS, as of now. The newer version fails to build.
    
    * Fix bad syntax in specifying the boost version range.
    
    * Update the version numbers for the krell institute components and tools: cbtf and openspeedshop.
    
    * Do not build glib for qt3, it is not needed and causes build problems at this time anyway.
    
    * A fix was added for setting LD_LIBRARY_PATH in the qt3 build, but if LD_LIBRARY_PATH is not set the qt build fails. So so check and set LD_LIBRARY_PATH if not set, update if it is set.
    
    * Update the fix for qt3 build by setting LD_LIBRARY_PATH instead of checking for whether it is set or not per Adams comment that spack clears LD_LIBRARY_PATH.
    
    * A fix was added for setting LD_LIBRARY_PATH in the qt3 build, but if LD_LIBRARY_PATH is not set the qt build fails. So so check and set LD_LIBRARY_PATH if not set, update if it is set.
    
    * Trim comments to fit more concisely.
    
    * Fix tabs versus spaces and swap if and else clause check from a negative to a positive check.
    
    * Fix issues with the cbtf-argonavis build, update to use dyninst-9.3.2, fixes to openspeedshop package build.
    
    * Fix issues with the cbtf-argonavis package.py files related to comments.
    
    * Add changes for changing the krell packages from Package to CMakePackage.
    
    * Add better changes for changing the krell packages from Package to CMakePackage.
    
    * Add more modifications for changing the krell packages from Package to CMakePackage.
    
    * Add additional modifications for changing the krell packages from Package to CMakePackage and fixing Travis erros
    
    * Fix new travis errors.
    
    * Fix new travis errors.
    
    * Add more changes for PR 4765.
    
    * Add more refinements to the conversion from Package to CMakePackage.
    
    * Fix new travis errors.
    
    * Add dependencies for MPI to be passed to cbtf-krell, so it can build the MPI collectors requested by the builder of openspeedshop.
    
    * Remove extra unnecessary routine to adjust build arguments. Fix if-else clause issue.
    
    * Fix more flake issues caused by last changes.

--- var/spack/repos/builtin/packages/cbtf-lanl/package.py
+++ var/spack/repos/builtin/packages/cbtf-lanl/package.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory.
 #
 # This file is part of Spack.
@@ -23,7 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 ##########################################################################
-# Copyright (c) 2015-2016 Krell Institute. All Rights Reserved.
+# Copyright (c) 2015-2017 Krell Institute. All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -43,14 +43,10 @@
 from spack import *
 
 
-class CbtfLanl(Package):
+class CbtfLanl(CMakePackage):
     """CBTF LANL project contains a memory tool and data center type system
        command monitoring tool."""
     homepage = "http://sourceforge.net/p/cbtf/wiki/Home/"
-
-    # Mirror access template example
-    # url      = "file:/g/g24/jeg/cbtf-lanl-1.6.tar.gz"
-    # version('1.6', 'c3f78f967b0a42c6734ce4be0e602426')
 
     version('1.8', branch='master',
             git='http://git.code.sf.net/p/cbtf-lanl/cbtf-lanl')
@@ -64,61 +60,24 @@ class CbtfLanl(Package):
 
     parallel = False
 
-    def adjustBuildTypeParams_cmakeOptions(self, spec, cmakeOptions):
-        # Sets build type parameters into cmakeOptions the options that will
-        # enable the cbtf-krell built type settings
+    build_directory = 'build_cbtf_lanl'
 
+    def build_type(self):
+        return 'None'
+
+    def cmake_args(self):
+
+        spec = self.spec
         compile_flags = "-O2 -g"
-        BuildTypeOptions = []
-        # Set CMAKE_BUILD_TYPE to what cbtf-krell wants it to be, not the
-        # stdcmakeargs
-        for word in cmakeOptions[:]:
-            if word.startswith('-DCMAKE_BUILD_TYPE'):
-                cmakeOptions.remove(word)
-            if word.startswith('-DCMAKE_CXX_FLAGS'):
-                cmakeOptions.remove(word)
-            if word.startswith('-DCMAKE_C_FLAGS'):
-                cmakeOptions.remove(word)
-            if word.startswith('-DCMAKE_VERBOSE_MAKEFILE'):
-                cmakeOptions.remove(word)
-        BuildTypeOptions.extend([
-            '-DCMAKE_VERBOSE_MAKEFILE=ON',
-            '-DCMAKE_BUILD_TYPE=None',
-            '-DCMAKE_CXX_FLAGS=%s'         % compile_flags,
-            '-DCMAKE_C_FLAGS=%s'           % compile_flags
-        ])
 
-        cmakeOptions.extend(BuildTypeOptions)
+        cmake_args = [
+            '-DCMAKE_CXX_FLAGS=%s'        % compile_flags,
+            '-DCMAKE_C_FLAGS=%s'          % compile_flags,
+            '-DCBTF_DIR=%s'               % spec['cbtf'].prefix,
+            '-DCBTF_KRELL_DIR=%s'         % spec['cbtf-krell'].prefix,
+            '-DMRNET_DIR=%s'              % spec['mrnet'].prefix,
+            '-DXERCESC_DIR=%s'            % spec['xerces-c'].prefix,
+            '-DCMAKE_MODULE_PATH=%s'      % join_path(
+                prefix.share, 'KrellInstitute', 'cmake')]
 
-    def install(self, spec, prefix):
-
-        # Add in paths for finding package config files that tell us where to
-        # find these packages
-        cmake_prefix_path = join_path(
-            spec['cbtf'].prefix) + ':' + join_path(spec['cbtf-krell'].prefix)
-
-        with working_dir('build', create=True):
-            cmakeOptions = []
-            cmakeOptions.extend(
-                ['-DCMAKE_INSTALL_PREFIX=%s'   % prefix,
-                 '-DCBTF_DIR=%s'               % spec['cbtf'].prefix,
-                 '-DCBTF_KRELL_DIR=%s'         % spec['cbtf-krell'].prefix,
-                 '-DMRNET_DIR=%s'              % spec['mrnet'].prefix,
-                 '-DXERCESC_DIR=%s'            % spec['xerces-c'].prefix,
-                 '-DCMAKE_PREFIX_PATH=%s'      % cmake_prefix_path,
-                 '-DCMAKE_MODULE_PATH=%s'      % join_path(
-                     prefix.share, 'KrellInstitute', 'cmake')])
-
-            # Add in the standard cmake arguments
-            cmakeOptions.extend(std_cmake_args)
-
-            # Adjust the standard cmake arguments to what we want the build
-            # type, etc to be
-            self.adjustBuildTypeParams_cmakeOptions(spec, cmakeOptions)
-
-            # Invoke cmake
-            cmake('..', *cmakeOptions)
-
-            make("clean")
-            make()
-            make("install")
+        return cmake_args
