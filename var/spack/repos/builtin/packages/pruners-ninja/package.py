b183aab43d45b85666de0a0e95f2a7f72cbdba7c
Gregory Lee <lee218@llnl.gov>
2017-07-21 15:57:12 -0700

    added new pruners-ninja version (#4859)

--- var/spack/repos/builtin/packages/pruners-ninja/package.py
+++ var/spack/repos/builtin/packages/pruners-ninja/package.py
@@ -31,6 +31,7 @@ class PrunersNinja(AutotoolsPackage):
     homepage = "https://github.com/PRUNERS/NINJA"
     url      = "https://github.com/PRUNERS/NINJA/releases/download/v1.0.0/NINJA-1.0.0.tar.gz"
 
+    version("1.0.1", "f0728cad61d8f1f970dffb7bb430addb")
     version("1.0.0", "fee53c4712ac521ebec3cd8692e5185a")
 
     depends_on("mpi")
