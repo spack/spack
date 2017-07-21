cba633ff98897f58e1fefc47dd9b40fe289e81d4
las_thoma15 <las_thoma15@iastate.edu>
2017-07-21 12:32:43 -0500

    changing name and adding stable release

--- var/spack/repos/builtin/packages/snap-berkeley/package.py
+++ var/spack/repos/builtin/packages/snap-berkeley/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Snap(MakefilePackage):
+class SnapBerkeley(MakefilePackage):
     """SNAP is a fast and accurate aligner for short DNA reads. It is
        optimized for modern read lengths of 100 bases or higher, and takes
        advantage of these reads to align data quickly through a hash-based
@@ -35,6 +35,7 @@ class Snap(MakefilePackage):
     url      = "https://github.com/amplab/snap/archive/v1.0beta.18.tar.gz"
 
     version('1.0beta.18', '41e595fffa482e9eda1c3f69fb5dedeb')
+    version('0.15',       'a7d87cc822f052665a347ab0aa84d4de', preferred=True)
 
     depends_on('zlib')
 
@@ -49,5 +50,8 @@ class Snap(MakefilePackage):
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        install('snap-aligner', prefix.bin)
-        install('SNAPCommand', prefix.bin)
+        if self.spec.satisfies('@1.0beta.18:'):
+            install('snap-aligner', prefix.bin)
+            install('SNAPCommand', prefix.bin)
+        else:
+            install('snap', prefix.bin)
