3a03689d3607008fa4bea3842832c349f194887f
snehring <snehring@iastate.edu>
2017-07-26 12:00:52 -0500

    chlorop: cleaning up, removing CHLOROTMP, advising user to set it.

--- var/spack/repos/builtin/packages/chlorop/package.py
+++ var/spack/repos/builtin/packages/chlorop/package.py
@@ -34,25 +34,21 @@ class Chlorop(Package):
     sites. You will need to obtain the tarball by visiting the
     URL and completing the form. You can then either run spack
     install with the tarball in the directory, or add it to a
-    mirror."""
+    mirror. You will need to set the CHLOROTMP environment variable
+    to the full path of the directory you want chlorop to use as
+    a temporary directory."""
 
     homepage = "http://www.cbs.dtu.dk/services/ChloroP/"
+    url      = "file://{0}/chlorop-1.1.Linux.tar.gz".format(os.getcwd())
 
-    version('1.1',
-            'eb0ba6b28dfa735163ad5fc70e30139e46e33f6ae27f87666a7167a4ac5f71d9')
+    version('1.1', 'eb0ba6b28dfa735163ad5fc70e30139e46e33f6ae27f87666a7167a4ac5f71d9')
 
-    depends_on('gawk', type='run')
+    depends_on('awk', type='run')
     patch('chlorop.patch')
-
-    # get tarball from current directory
-    def url_for_version(self, version):
-        return "file://{0}/chlorop-{1}.Linux.tar.gz".format(os.getcwd(),
-                                                            version)
 
     def install(self, spec, prefix):
         os.rename('chlorop', 'bin/chlorop')
         distutils.dir_util.copy_tree(".", prefix)
 
     def setup_environment(self, spack_env, run_env):
-        run_env.set('CHLOROP', self.spec.prefix)
-        run_env.set('CHLOROTMP', tempfile.gettempdir() + '/chlorop')
+        run_env.set('CHLOROP', self.prefix)
