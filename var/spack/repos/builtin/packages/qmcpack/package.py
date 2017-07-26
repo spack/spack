1cdbb09d7d1ed364684d56b6de9888ccd08e68aa
William S <wscullin@alcf.anl.gov>
2017-07-26 11:36:41 -0500

    Removing remaining boilerplate

--- var/spack/repos/builtin/packages/qmcpack/package.py
+++ var/spack/repos/builtin/packages/qmcpack/package.py
@@ -22,21 +22,6 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install qmcpack
-#
-# You can edit this file again by typing:
-#
-#     spack edit qmcpack
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
@@ -44,7 +29,7 @@ class Qmcpack(CMakePackage):
     """QMCPACK, is a modern high-performance open-source Quantum Monte 
        Carlo (QMC) simulation code."""
 
-    # FIXME: Add a proper url for your package's homepage here.
+    # Package information
     homepage = "http://www.qmcpack.org/"
     url      = "https://github.com/QMCPACK/qmcpack/archive/v3.1.0.tar.gz"
 
