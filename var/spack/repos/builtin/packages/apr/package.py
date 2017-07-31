b36d8e4a56d51518d9129cc9f4b0939592024f88
Adam J. Stewart <ajstewart426@gmail.com>
2017-07-31 12:35:48 -0500

    Add latest version of apr (#4931)

--- var/spack/repos/builtin/packages/apr/package.py
+++ var/spack/repos/builtin/packages/apr/package.py
@@ -25,15 +25,11 @@
 from spack import *
 
 
-class Apr(Package):
+class Apr(AutotoolsPackage):
     """Apache portable runtime."""
+
     homepage  = 'https://apr.apache.org/'
-    url       = 'http://archive.apache.org/dist/apr/apr-1.5.2.tar.gz'
+    url       = 'http://archive.apache.org/dist/apr/apr-1.6.2.tar.gz'
 
-    version('1.5.2',    '98492e965963f852ab29f9e61b2ad700')
-
-    def install(self, spec, prefix):
-        options = ['--prefix=%s' % prefix]
-        configure(*options)
-        make()
-        make('install')
+    version('1.6.2', '8672e78514e3fcef2643127c524bf0f9')
+    version('1.5.2', '98492e965963f852ab29f9e61b2ad700')
