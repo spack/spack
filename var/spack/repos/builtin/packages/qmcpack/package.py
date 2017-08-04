e0771ceb1fe62b9391078ef93ee9405fd1adecac
William S <wscullin@gmail.com>
2017-08-04 02:46:19 -0500

    Flake8 fixes

--- var/spack/repos/builtin/packages/qmcpack/package.py
+++ var/spack/repos/builtin/packages/qmcpack/package.py
@@ -26,7 +26,7 @@ from spack import *
 
 
 class Qmcpack(CMakePackage):
-    """QMCPACK, is a modern high-performance open-source Quantum Monte 
+    """QMCPACK, is a modern high-performance open-source Quantum Monte
        Carlo (QMC) simulation code."""
 
     # Package information
@@ -36,16 +36,23 @@ class Qmcpack(CMakePackage):
     version('3.1.0', 'bdf3acd090557acdb6cab5ddbf7c7960')
     version('3.0.0', '75f9cf70e6cc6d8b7ff11a86340da43d')
 
-    # This download method is untrusted, and is not recommend by the Spack manual.
+    # This download method is untrusted, and is not recommended
+    # by the Spack manual.
     version('develop', git='https://github.com/QMCPACK/qmcpack.git')
 
     # These defaults match those in the QMCPACK manual
     variant('debug', default=False, description='Build debug version')
     variant('mpi', default=True, description='Build with MPI support')
-    variant('cuda', default=False, description='Enable CUDA and GPU acceleration.')
-    variant('complex', default=True, description='Build the complex (general twist/k-point) version')
-    variant('mixed', default=False, description='Build the mixed precision (mixture of single and double precision) version for gpu and cpu (experimental)')
-    variant('gui', default=False, description='Install with Py-Matplotlib (long installation time)') 
+    variant('cuda', default=False,
+            description='Enable CUDA and GPU acceleration.')
+    variant('complex', default=True,
+            description='Build the complex (general twist/k-point) version')
+    variant('mixed', default=False,
+            description='Build the mixed precision (mixture of single and '
+                        'double precision) version for gpu and cpu '
+                        '(experimental)')
+    variant('gui', default=False,
+            description='Install with Matplotlib (long installation time)')
 
     depends_on('cmake@3.4.3:', type='build')
     depends_on('mpi', when='+mpi')
@@ -58,7 +65,7 @@ class Qmcpack(CMakePackage):
     depends_on('fftw+mpi', when='+mpi')
     depends_on('fftw~mpi', when='~mpi')
     depends_on('cuda', when='+cuda')
-   
+
     # qmcpack needs these for data analysis
     depends_on('python', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
@@ -66,7 +73,8 @@ class Qmcpack(CMakePackage):
     # GUI is optional and leads to a long complex DAG for dependencies
     depends_on('py-matplotlib', type=('build', 'run'), when='+gui')
 
-    # B-spline basis calculation require a patched version of Quantum Espresso 5.3.0 (see QMCPACK manual)
+    # B-spline basis calculation require a patched version of
+    # Quantum Espresso 5.3.0 (see QMCPACK manual)
     depends_on('espresso@5.3.0+qmcpackconv~elpa', when='+mpi')
     depends_on('espresso@5.3.0+qmcpackconv~elpa~scalapack~mpi', when='~mpi')
 
@@ -76,7 +84,6 @@ class Qmcpack(CMakePackage):
         filter_file(r'$ENV{LIBXML2_HOME}/lib',
                     '${LIBXML2_HOME}/lib $ENV{LIBXML2_HOME}/lib',
                     'CMake/FindLibxml2QMC.cmake')
-  
 
         if 'cxxflags' in self.compiler.flags:
             cxx_flags = ' '.join(self.compiler.flags['cxxflags'])
@@ -87,30 +94,38 @@ class Qmcpack(CMakePackage):
             args.append('-DCMAKE_C_FLAGS={0}'.format(c_flags))
 
         if '+mpi' in self.spec:
-            args.append('-DCMAKE_C_COMPILER={0}'.format(self.spec['mpi'].mpicc))
-            args.append('-DCMAKE_CXX_COMPILER={0}'.format(self.spec['mpi'].mpicxx))
-            args.append('-DMPI_BASE_DIR:PATH={0}'.format(self.spec['mpi'].prefix))
+            mpi = self.spec['mpi']
+            args.append('-DCMAKE_C_COMPILER={0}'.format(mpi.mpicc))
+            args.append('-DCMAKE_CXX_COMPILER={0}'.format(mpi.mpicxx))
+            args.append('-DMPI_BASE_DIR:PATH={0}'.format(mpi.prefix))
 
-        # Currently FFTW_HOME and LIBXML2_HOME are used. CMake Warning is benign.
-        args.append('-DLIBXML2_HOME={0}'.format(self.spec['libxml2'].prefix))
-        args.append('-DLibxml2_INCLUDE_DIRS={0}'.format(self.spec['libxml2'].prefix.include))
-        args.append('-DLibxml2_LIBRARY_DIRS={0}'.format(self.spec['libxml2'].prefix.lib))
-        args.append('-DFFTW_HOME={0}'.format(self.spec['fftw'].prefix))
+        # Currently FFTW_HOME and LIBXML2_HOME are used by CMake.
+        # Any CMake warnings about other variables are benign.
+        xml2_prefix = self.spec['libxml2'].prefix
+        args.append('-DLIBXML2_HOME={0}'.format(xml2_prefix))
+        args.append('-DLibxml2_INCLUDE_DIRS={0}'.format(xml2_prefix.include))
+        args.append('-DLibxml2_LIBRARY_DIRS={0}'.format(xml2_prefix.lib))
+
+        fftw_prefix = self.spec['fftw'].prefix
+        args.append('-DFFTW_HOME={0}'.format(fftw_prefix))
+        args.append('-DFFTW_INCLUDE_DIRS={0}'.format(fftw_prefix.include))
+        args.append('-DFFTW_LIBRARY_DIRS={0}'.format(fftw_prefix.lib))
+
         args.append('-DBOOST_ROOT={0}'.format(self.spec['boost'].prefix))
         args.append('-DHDF5_ROOT={0}'.format(self.spec['hdf5'].prefix))
-        args.append('-DFFTW_INCLUDE_DIRS={0}'.format(self.spec['fftw'].prefix.include))
-        args.append('-DFFTW_LIBRARY_DIRS={0}'.format(self.spec['fftw'].prefix.lib))
-        
+
         # Default is MPI, serial version is convenient for cases, e.g. laptops
         if '~mpi' in self.spec:
             args.append('-DQMC_MPI=0')
 
-        # when '-DQMC_CUDA=1', CMake automatically sets '-DQMC_MIXED_PRECISION=1'
-        # there is a double-precision CUDA path, but it is deprecated
+        # When '-DQMC_CUDA=1', CMake automatically sets:
+        # '-DQMC_MIXED_PRECISION=1'
+        #
+        # Note: there is a double-precision CUDA path, but it is deprecated
         if '+cuda' in self.spec:
             args.append('-DQMC_CUDA=1')
 
-        # this is for the experimental mixed-prescision CPU code 
+        # this is for the experimental mixed-prescision CPU code
         if '+mixed' in self.spec:
             args.append('-DQMC_MIXED_PRECISION=1')
 
@@ -120,7 +135,7 @@ class Qmcpack(CMakePackage):
         """Make the install targets"""
 
         # QMCPACK 'make install' does nothing, which causes
-        # Spack to throw an error.  
+        # Spack to throw an error.
         #
         # This install method creates the top level directory
         # and copies the bin subdirectory into the appropriate
@@ -130,16 +145,16 @@ class Qmcpack(CMakePackage):
         mkdirp(prefix)
 
         # We assume cwd is self.stage.source_path
-        
-        # install manual 
+
+        # install manual
         install_tree('manual', prefix.manual)
 
         # install nexus
         install_tree('nexus', prefix.nexus)
-        
+
         with working_dir(self.build_directory):
             # QMCPACK 'make install' does nothing, which causes
-            # Spack to throw an error.  
+            # Spack to throw an error.
             #
             # This install method creates the top level directory
             # and copies the bin subdirectory into the appropriate
@@ -153,7 +168,9 @@ class Qmcpack(CMakePackage):
     @run_after('build')
     @on_package_attributes(run_tests=True)
     def check_build(self):
-        """Run ctest after building binary. Use 'spack install --run-tests qmcpack'.
-        It can take 24 hours to run all the regression tests and unit tests. """
+        """Run ctest after building binary.
+        Use 'spack install --run-tests qmcpack'.
+        It can take 24 hours to run all the regression tests
+        and unit tests. """
         self.ctest
         pass
