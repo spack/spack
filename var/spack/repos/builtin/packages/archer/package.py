957378c247475758c37c692f0e5a5314ab4bdf89
Adam J. Stewart <ajstewart426@gmail.com>
2017-08-06 15:18:03 -0500

    Add better generator support to CMakePackage

--- var/spack/repos/builtin/packages/archer/package.py
+++ var/spack/repos/builtin/packages/archer/package.py
@@ -36,22 +36,14 @@ class Archer(CMakePackage):
 
     depends_on('cmake@3.4.3:', type='build')
     depends_on('llvm')
-    depends_on('ninja', type='build')
+    depends_on('ninja@1.5:', type='build')
     depends_on('llvm-openmp-ompt')
+
+    generator = 'Ninja'
 
     def cmake_args(self):
         return [
-            '-G', 'Ninja',
             '-DCMAKE_C_COMPILER=clang',
             '-DCMAKE_CXX_COMPILER=clang++',
             '-DOMP_PREFIX:PATH=%s' % spec['llvm-openmp-ompt'].prefix,
         ]
-
-    # TODO: Add better ninja support to CMakePackage
-    def build(self, spec, prefix):
-        with working_dir(self.build_directory):
-            ninja()
-
-    def install(self, spec, prefix):
-        with working_dir(self.build_directory):
-            ninja('install')
