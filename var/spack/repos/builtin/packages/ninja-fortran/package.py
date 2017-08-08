dc4b6f9d0ad31971e7ac4db91b7f8a32c0bf5520
Adam J. Stewart <ajstewart426@gmail.com>
2017-08-08 13:33:24 -0500

    Add latest version of Ninja (#4987)
    * Add latest version of Ninja
    
    * Re-add symlinks, add latest version of ninja-fortran

--- var/spack/repos/builtin/packages/ninja-fortran/package.py
+++ var/spack/repos/builtin/packages/ninja-fortran/package.py
@@ -23,27 +23,45 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import os
 
 
 class NinjaFortran(Package):
-    """ A Fortran capable fork of ninja """
+    """A Fortran capable fork of ninja."""
+
     homepage = "https://github.com/Kitware/ninja"
-    url      = "https://github.com/Kitware/ninja/archive/v1.7.2.gcc0ea.kitware.dyndep-1.tar.gz"
+    url      = "https://github.com/Kitware/ninja/archive/v1.7.2.gaad58.kitware.dyndep-1.tar.gz"
 
-    version('1.7.2', '3982f508c415c0abaca34cb5e92e711a')
+    # Each version is a fork off of a specific commit of ninja
+    # Hashes don't sort properly, so manually set newest version
+    version('1.7.2.gaad58', 'eb51b042b9dbaf8ecd79a6fb24de1320', preferred=True)
+    version('1.7.2.gcc0ea', '3982f508c415c0abaca34cb5e92e711a')
+    version('1.7.1.g7ca7f', '187a8d15c1e20e5e9b00c5c3f227ca8a')
 
-    extends('python')
+    depends_on('python', type=('build', 'run'))
+
+    phases = ['configure', 'install']
 
     def url_for_version(self, version):
-        url = 'https://github.com/Kitware/ninja/archive/v{0}.gcc0ea.kitware.dyndep-1.tar.gz'
+        url = 'https://github.com/Kitware/ninja/archive/v{0}.kitware.dyndep-1.tar.gz'
         return url.format(version)
 
-    def install(self, spec, prefix):
+    def configure(self, spec, prefix):
         python('configure.py', '--bootstrap')
 
+    @run_after('configure')
+    @on_package_attributes(run_tests=True)
+    def test(self):
+        ninja = Executable('./ninja')
+        ninja('-j{0}'.format(make_jobs), 'ninja_test')
+        ninja_test = Executable('./ninja_test')
+        ninja_test()
+
+    def install(self, spec, prefix):
         mkdir(prefix.bin)
         install('ninja', prefix.bin)
-        install_tree('misc', join_path(prefix, 'misc'))
+        install_tree('misc', prefix.misc)
+
+        # Some distros like Fedora install a 'ninja-build' executable
+        # instead of 'ninja'. Install both for uniformity.
         with working_dir(prefix.bin):
-            os.symlink('ninja', 'ninja-build')
+            symlink('ninja', 'ninja-build')
