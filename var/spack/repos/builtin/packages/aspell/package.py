d843b05ebfeaf0455c7e012ba145932e6faecd46
hartzelg <george.hartzell.gh1@contractors.roche.com>
2017-08-15 13:37:58 -0700

    Use a helper method to generate info for variants
    I figured out my issues with static methods.  I *think* that it this
    is pythonic.

--- var/spack/repos/builtin/packages/aspell/package.py
+++ var/spack/repos/builtin/packages/aspell/package.py
@@ -28,7 +28,7 @@ from six.moves.urllib.parse import urlparse
 
 
 class Aspell(AutotoolsPackage):
-    """GNU Aspell is a Free and Open Source spell checker designed to 
+    """GNU Aspell is a Free and Open Source spell checker designed to
     eventually replace Ispell."""
 
     homepage = "http://aspell.net/"
@@ -36,32 +36,30 @@ class Aspell(AutotoolsPackage):
 
     version('0.60.6.1', 'e66a9c9af6a60dc46134fdacf6ce97d7')
 
-    # additional dictionaries here: ftp://ftp.gnu.org/gnu/aspell/dict/0index.html
-    dicts = [
-        {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/en/aspell6-en-2017.01.22-0.tar.bz2',
-         'md5': "a6e002076574de9dc4915967032a1dab",
-        },
-        {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/es/aspell6-es-1.11-2.tar.bz2',
-         'md5': '8406336a89c64e47e96f4153d0af70c4',
-        },
-        {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/de/aspell6-de-20030222-1.tar.bz2',
-         'md5': '5950c5c8a36fc93d4d7616591bace6a6',
-        },
-    ]
-    for d in dicts:
-        o = urlparse(d['url'])
-        p = o.path.split('/')
-        d['name'] = p[4]
-        d['package'] = re.sub(r'\.tar.bz2', '', p[5])
+    @staticmethod
+    def list_resources():
+        # additional dictionaries here: ftp://ftp.gnu.org/gnu/aspell/dict/0index.html
+        dicts = [
+            {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/en/aspell6-en-2017.01.22-0.tar.bz2',
+             'md5': "a6e002076574de9dc4915967032a1dab",
+            },
+            {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/es/aspell6-es-1.11-2.tar.bz2',
+             'md5': '8406336a89c64e47e96f4153d0af70c4',
+            },
+            {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/de/aspell6-de-20030222-1.tar.bz2',
+             'md5': '5950c5c8a36fc93d4d7616591bace6a6',
+            },
+        ]
+        for d in dicts:
+            p = urlparse(d['url']).path.split('/')
+            name = p[4]
+            package = re.sub('\.tar.bz2', '', p[5])
+            yield d['url'], d['md5'], name, package
 
-        variant(d['name'], default=False, description=d['package'])
-        resource(
-            name=d['name'],
-            url=d['url'],
-            md5=d['md5'],
-            placement=d['package'],
-            when='+{0}'.format(d['name']),
-        )
+    for dict_url,md5,name,package in list_resources.__func__():
+        variant(name, default=False, description=package)
+        resource(name=name, url=dict_url, md5=md5, placement=package,
+                 when='+{0}'.format(name))
 
     # When installing dictionaries, prezip needs to be able to find
     # prezip-bin.
@@ -71,9 +69,9 @@ class Aspell(AutotoolsPackage):
     @run_after('install')
     def install_dictionaries(self):
         prefix = self.prefix
-        for d in self.dicts:
-            if '+{0}'.format(d['name']) in self.spec:
-                with working_dir(d['package']):
+        for dict_url, md5, name, package in self.list_resources():
+            if '+{0}'.format(name) in self.spec:
+                with working_dir(package):
                     sh = which('sh')
                     aspell = join_path(prefix.bin, "aspell")
                     prezip = join_path(prefix.bin, "prezip")
