5c0a12d1057d79277b055ae6301c6f42235cf108
hartzelg <george.hartzell.gh1@contractors.roche.com>
2017-08-16 11:39:38 -0700

    Convert aspell to an extendable package
    Convert aspell to be extendable and rework the dictionaries to be
    extensions.
    
    As it stands, there's a great deal of cut and paste in the
    dictionaries, I'll abstract that out next.
    
    The {de,}activate methods copy a great deal of code out of
    package.py.  Perhaps there's a better way....

--- var/spack/repos/builtin/packages/aspell/package.py
+++ var/spack/repos/builtin/packages/aspell/package.py
@@ -23,8 +23,9 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import re
-from six.moves.urllib.parse import urlparse
+from llnl.util.link_tree import LinkTree
+import spack.store
+from spack.package import ExtensionConflictError
 
 
 class Aspell(AutotoolsPackage):
@@ -34,47 +35,38 @@ class Aspell(AutotoolsPackage):
     homepage = "http://aspell.net/"
     url      = "https://ftpmirror.gnu.org/aspell/aspell-0.60.6.1.tar.gz"
 
+    extendable = True           # support activating dictionaries
+
     version('0.60.6.1', 'e66a9c9af6a60dc46134fdacf6ce97d7')
 
-    @staticmethod
-    def list_resources():
-        # additional dictionaries here: ftp://ftp.gnu.org/gnu/aspell/dict/0index.html
-        dicts = [
-            {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/en/aspell6-en-2017.01.22-0.tar.bz2',
-             'md5': "a6e002076574de9dc4915967032a1dab",
-            },
-            {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/es/aspell6-es-1.11-2.tar.bz2',
-             'md5': '8406336a89c64e47e96f4153d0af70c4',
-            },
-            {'url': 'ftp://ftp.gnu.org/gnu/aspell/dict/de/aspell6-de-20030222-1.tar.bz2',
-             'md5': '5950c5c8a36fc93d4d7616591bace6a6',
-            },
-        ]
-        for d in dicts:
-            p = urlparse(d['url']).path.split('/')
-            name = p[4]
-            package = re.sub('\.tar.bz2', '', p[5])
-            yield d['url'], d['md5'], name, package
+    # The dictionaries install all their bits into their prefix.lib dir,
+    # we want to link them into aspell's dict-dir.
+    # These are identical to what's in spack/package.py except
+    # for using:
+    #   - extension.prefix.lib instead of extension.prefix in LinkTree()
+    #   - dest_dir instead of self.prefix in tree.(find_conflict|merge)()
+    def activate(self, extension, **kwargs):
+        aspell = which(self.prefix.bin.aspell)
+        dest_dir = aspell('dump', 'config', 'dict-dir', output=str).strip()
+        tree = LinkTree(extension.prefix.lib)
 
-    for dict_url,md5,name,package in list_resources.__func__():
-        variant(name, default=False, description=package)
-        resource(name=name, url=dict_url, md5=md5, placement=package,
-                 when='+{0}'.format(name))
+        def ignore(filename):
+            return (filename in spack.store.layout.hidden_file_paths or
+                    kwargs.get('ignore', lambda f: False)(filename))
 
-    # When installing dictionaries, prezip needs to be able to find
-    # prezip-bin.
-    def setup_environment(self, spack_env, run_env):
-        spack_env.prepend_path('PATH', self.prefix.bin)
+        conflict = tree.find_conflict(dest_dir, ignore=ignore)
+        if conflict:
+            raise ExtensionConflictError(conflict)
 
-    @run_after('install')
-    def install_dictionaries(self):
-        prefix = self.prefix
-        for dict_url, md5, name, package in self.list_resources():
-            if '+{0}'.format(name) in self.spec:
-                with working_dir(package):
-                    sh = which('sh')
-                    aspell = join_path(prefix.bin, "aspell")
-                    prezip = join_path(prefix.bin, "prezip")
-                    sh('./configure', '--vars', "ASPELL={0}".format(aspell),
-                        "PREZIP={0}".format(prezip))
-                    make('install')
+        tree.merge(dest_dir, ignore=ignore)
+
+    def deactivate(self, extension, **kwargs):
+        aspell = which(self.prefix.bin.aspell)
+        dest_dir = aspell('dump', 'config', 'dict-dir', output=str).strip()
+
+        def ignore(filename):
+            return (filename in spack.store.layout.hidden_file_paths or
+                    kwargs.get('ignore', lambda f: False)(filename))
+
+        tree = LinkTree(extension.prefix.lib)
+        tree.unmerge(dest_dir, ignore=ignore)
---
+++ var/spack/repos/builtin/packages/aspell6-es/package.py
@@ -0,0 +1,52 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+# This isn't really an Autotools package, but it's close enough
+# that this works if we override configure().
+class Aspell6Es(AutotoolsPackage):
+    """Spanish (es) dictionary for aspell."""
+
+    extends('aspell')
+
+    homepage = "http://aspell.net/"
+    url      = "ftp://ftp.gnu.org/gnu/aspell/dict/es/aspell6-es-1.11-2.tar.bz2"
+
+    version('1.11-2', '8406336a89c64e47e96f4153d0af70c4')
+
+    def patch(self):
+        filter_file(r'^dictdir=.*$', 'dictdir=/lib', 'configure')
+        filter_file(r'^datadir=.*$', 'datadir=/lib', 'configure')
+
+    def configure(self, spec, prefix):
+        aspell = spec['aspell'].prefix.bin.aspell
+        prezip = spec['aspell'].prefix.bin.prezip
+        destdir = prefix
+
+        sh = which('sh')
+        sh('./configure', '--vars', "ASPELL={0}".format(aspell),
+           "PREZIP={0}".format(prezip),
+           "DESTDIR={0}".format(destdir))
