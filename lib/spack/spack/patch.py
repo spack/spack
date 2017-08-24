5801a9a21fc137e42bc05c75890aa38c92bee07c
Nichols A. Romero <naromero@anl.gov>
2017-08-24 17:40:17 -0500

    Revert "patching from url now only calls expand_archive for actual .tar files. plain text patch files are no longer expanded. In support of PR#4907."
    This reverts commit ee63e3638308f652a39023393045d1c838a21716.

--- lib/spack/spack/patch.py
+++ lib/spack/spack/patch.py
@@ -131,8 +131,7 @@ class UrlPatch(Patch):
             patch_stage.fetch()
             patch_stage.check()
             patch_stage.cache_local()
-            if self.url.lower().endswith('.tar'):
-                patch_stage.expand_archive()
+            patch_stage.expand_archive()
             self.path = os.path.abspath(
                 os.listdir(patch_stage.path).pop()
             )
