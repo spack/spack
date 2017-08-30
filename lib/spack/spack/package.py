8a9ba273eb36547cf94e1c5e65d902ec44537f23
Denis Davydov <davydden@gmail.com>
2017-08-30 10:17:23 +0200

    add :

--- lib/spack/spack/package.py
+++ lib/spack/spack/package.py
@@ -545,7 +545,7 @@ class PackageBase(with_metaclass(PackageMeta, object)):
     # Verbosity level, preserved across installs.
     _verbose = None
 
-    # List of strings which contains GitHub usernames of package maintainers.
+    #: List of strings which contains GitHub usernames of package maintainers.
     #: Do not include @ here in order not to unnecessarily ping the users.
     maintainers = []
 
