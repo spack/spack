e754b1aaa077076835edef05dd53682736f199bb
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-09-08 16:27:09 +0200

    readded old version with custom url

--- var/spack/repos/builtin/packages/py-quantities/package.py
+++ var/spack/repos/builtin/packages/py-quantities/package.py
@@ -32,6 +32,8 @@ class PyQuantities(PythonPackage):
     url      = "https://pypi.io/packages/source/q/quantities/quantities-0.12.1.tar.gz"
 
     version('0.12.1', '9c9ecda15e905cccfc420e5341199512')
+    version('0.11.1', 'f4c6287bfd2e93322b25a7c1311a0243',
+            url="https://pypi.io/packages/source/q/quantities/quantities-0.11.1.zip")
 
     conflicts('py-numpy@1.13:', when='@:0.11.99')
 
