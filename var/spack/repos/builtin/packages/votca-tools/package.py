e6f2d2f2f44df8245e7cfda14cf3c8be7a03ed2c
Christoph Junghans <christoph.junghans@gmail.com>
2017-09-11 14:28:53 -0600

    added votca-1.4.1 modules (#5276)

--- var/spack/repos/builtin/packages/votca-tools/package.py
+++ var/spack/repos/builtin/packages/votca-tools/package.py
@@ -38,6 +38,7 @@ class VotcaTools(CMakePackage):
 
     version('develop', git='https://github.com/votca/tools', branch='master')
     version('1.4', 'cd47868e9f28e2c7b9d01f95aa0185ca')
+    version('1.4.1', '3176b72f8a41ec053cc740a5398e7dc4')
 
     depends_on("cmake@2.8:", type='build')
     depends_on("expat")
