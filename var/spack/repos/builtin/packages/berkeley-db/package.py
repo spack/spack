c6dd3b9fe45d750102b8eb8b07daf074b8f03c39
Christoph Junghans <junghans@lanl.gov>
2017-09-11 09:50:08 -0600

    berkeley-db: added more versions

--- var/spack/repos/builtin/packages/berkeley-db/package.py
+++ var/spack/repos/builtin/packages/berkeley-db/package.py
@@ -32,12 +32,16 @@ class BerkeleyDb(AutotoolsPackage):
     url      = "http://download.oracle.com/berkeley-db/db-5.3.28.tar.gz"
 
     version('5.3.28', 'b99454564d5b4479750567031d66fe24')
+    version('6.0.35', 'c65a4d3e930a116abaaf69edfc697f25')
+    version('6.1.29', '7f4d47302dfec698fe088e5285c9098e')
+    version('6.2.32', '33491b4756cb44b91c3318b727e71023')
 
     configure_directory = 'dist'
     build_directory = 'spack-build'
 
     def url_for_version(self, version):
-        return 'http://download.oracle.com/berkeley-db/db-{0}.tar.gz'.format(version)
+        # newer version need oracle login, so get them from gentoo mirror
+        return 'http://distfiles.gentoo.org/distfiles/db-{0}.tar.gz'.format(version)
 
     def configure_args(self):
         return ['--disable-static', '--enable-cxx', '--enable-stl']
