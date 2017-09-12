ada89185bcce2c27d34d4c3e0acc35cd7e87ff67
Louis Vernon <vernon@lanl.gov>
2017-09-12 15:53:21 -0600

    Add type=build

--- var/spack/repos/builtin/packages/nauty/package.py
+++ var/spack/repos/builtin/packages/nauty/package.py
@@ -68,7 +68,7 @@ class Nauty(AutotoolsPackage):
     depends_on('libtool',  type='build', when='@2.6r7')
     depends_on('pkg-config',  type='build')
     depends_on('zlib')
-    depends_on('help2man')
+    depends_on('help2man', type='build')
 
     @property
     def force_autoreconf(self):
