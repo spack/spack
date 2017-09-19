23d06c0f1d17a7d2ccfa06c65b8fd59dbd3f075b
healther <healther@users.noreply.github.com>
2017-09-19 22:10:13 +0200

    pr-quantities: add version & py-numpy conflict (#5307)

--- var/spack/repos/builtin/packages/py-quantities/package.py
+++ var/spack/repos/builtin/packages/py-quantities/package.py
@@ -29,9 +29,13 @@ class PyQuantities(PythonPackage):
     """Support for physical quantities with units, based on numpy"""
 
     homepage = "http://python-quantities.readthedocs.org"
-    url      = "https://pypi.io/packages/source/q/quantities/quantities-0.11.1.zip"
+    url      = "https://pypi.io/packages/source/q/quantities/quantities-0.12.1.tar.gz"
 
-    version('0.11.1', 'f4c6287bfd2e93322b25a7c1311a0243')
+    version('0.12.1', '9c9ecda15e905cccfc420e5341199512')
+    version('0.11.1', 'f4c6287bfd2e93322b25a7c1311a0243',
+            url="https://pypi.io/packages/source/q/quantities/quantities-0.11.1.zip")
+
+    conflicts('py-numpy@1.13:', when='@:0.11.99')
 
     depends_on('python@2.6.0:')
     depends_on('py-numpy@1.4.0:', type=('build', 'run'))
