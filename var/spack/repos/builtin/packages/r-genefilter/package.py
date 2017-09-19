16d014196c331aba6b170d6e2d0b1125a921eb16
las_lynn94 <las_lynn94@build.las.iastate.edu>
2017-09-19 14:17:41 -0500

    r-genefilter: Create New Package.

---
+++ var/spack/repos/builtin/packages/r-genefilter/package.py
@@ -0,0 +1,41 @@
+##############################################################################
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+#
+#
+from spack import *
+
+
+class RGenefilter(RPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://www.bioconductor.org/packages/release/bioc/html/genefilter.html"
+    url      = "https://www.bioconductor.org/packages/release/bioc/src/contrib/genefilter_1.58.1.tar.gz"
+    list_url = homepage
+    version('1.58.1', 'bc1a90bdf93d8db994220545cd80f438')
+    depends_on('r-s4vectors', type = ('build', 'run'))
+    depends_on('r-annotationdbi', type = ('build', 'run'))
+    depends_on('r-annotate', type = ('build', 'run'))
+    depends_on('r-biobase', type = ('build', 'run'))
+
