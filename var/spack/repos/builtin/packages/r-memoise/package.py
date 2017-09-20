d71876ae743c8d0f81d4569214a5cfc192f4309b
lingnanyuan <1297162327@qq.com>
2017-09-20 14:55:36 -0500

    r-memoise: Update version.

--- var/spack/repos/builtin/packages/r-memoise/package.py
+++ var/spack/repos/builtin/packages/r-memoise/package.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory.
 #
 # This file is part of Spack.
@@ -29,9 +29,10 @@ class RMemoise(RPackage):
     """Cache the results of a function so that when you call it again with the
     same arguments it returns the pre-computed value."""
 
-    homepage = "https://github.com/hadley/memoise"
-    url      = "https://cran.r-project.org/src/contrib/memoise_1.0.0.tar.gz"
-
+    homepage = "https://cran.r-project.org/web/packages/memoise/index.html"
+    url      = "https://cran.r-project.org/src/contrib/Archive/memoise/memoise_1.0.0.tar.gz"
+    list_url = homepage
+    version('1.1.0', '493209ee04673f0fcab473c3dd80fb8c')
     version('1.0.0', 'd31145292e2a88ae9a504cab1602e4ac')
 
     depends_on('r-digest', type=('build', 'run'))
