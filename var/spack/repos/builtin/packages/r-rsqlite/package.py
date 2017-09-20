a72664f67bddec319a3a83f92de7ed1fde0b86ea
lingnanyuan <1297162327@qq.com>
2017-09-20 15:22:25 -0500

    r-rsqlite: Create new package.

--- var/spack/repos/builtin/packages/r-rsqlite/package.py
+++ var/spack/repos/builtin/packages/r-rsqlite/package.py
@@ -34,8 +34,6 @@ class RRsqlite(RPackage):
     url      = "https://cran.r-project.org/src/contrib/RSQLite_1.0.0.tar.gz"
     list_url = homepage
     version('2.0', '63842410e78ccdfc52d4ee97992521d5')
-
-
     depends_on('r-dbi', type=('build', 'run'))
     depends_on('r-bit64', type=('build', 'run'))
     depends_on('r-blob', type=('build', 'run'))
