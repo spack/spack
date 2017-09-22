211d033fe824314b3ce8738b90f7ab2fbee4522c
Steve Leak <sleak@lbl.gov>
2017-09-22 11:32:05 -0700

    Update abinit url and version

--- var/spack/repos/builtin/packages/abinit/package.py
+++ var/spack/repos/builtin/packages/abinit/package.py
@@ -47,8 +47,10 @@ class Abinit(AutotoolsPackage):
     """
 
     homepage = 'http://www.abinit.org'
-    url      = 'http://ftp.abinit.org/abinit-8.0.8b.tar.gz'
+    #url      = 'http://ftp.abinit.org/abinit-8.0.8b.tar.gz'
+    url       = 'https://www.abinit.org/sites/default/files/packages/abinit-8.0.8b.tar.gz'
 
+    version('8.4.4', 'e26b46c0f98dbf2b5baf2c422672fcd1')
     version('8.2.2', '5f25250e06fdc0815c224ffd29858860')
     # Versions before 8.0.8b are not supported.
     version('8.0.8b', 'abc9e303bfa7f9f43f95598f87d84d5d')
