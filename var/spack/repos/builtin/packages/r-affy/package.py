46e865cc86cf6752f264a5c4f1137f57ba9e1885
Yifan Zhu <ifanzhu@foxmail.com>
2017-09-25 17:57:56 -0500

    r-affy: created new package (#5471)

---
+++ var/spack/repos/builtin/packages/r-affy/package.py
@@ -0,0 +1,44 @@
+##############################################################################
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class RAffy(RPackage):
+    """The package contains functions for exploratory oligonucleotide array
+       analysis. The dependence on tkWidgets only concerns few convenience
+       functions. 'affy' is fully functional without it."""
+
+    homepage = "https://bioconductor.org/packages/affy/"
+    url      = "https://bioconductor.org/packages/3.5/bioc/src/contrib/affy_1.54.0.tar.gz"
+    list_url = homepage
+
+    version('1.54.0', '84270de80ca61bb14b6127a6f5390100')
+
+    depends_on('r-biocgenerics', type=('build', 'run'))
+    depends_on('r-biobase', type=('build', 'run'))
+    depends_on('r-affyio', type=('build', 'run'))
+    depends_on('r-biocinstaller', type=('build', 'run'))
+    depends_on('r-preprocesscore', type=('build', 'run'))
+    depends_on('r-zlibbioc', type=('build', 'run'))
