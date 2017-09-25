1cb9f9ad4a8f66f03af4122b176996e3845590ae
lingnanyuan <1297162327@qq.com>
2017-09-25 14:29:45 -0500

    r-bit64: Correct format.

--- var/spack/repos/builtin/packages/r-bit64/package.py
+++ var/spack/repos/builtin/packages/r-bit64/package.py
@@ -27,17 +27,17 @@ from spack import *
 
 class RBit64(RPackage):
     """Package 'bit64' provides serializable S3 atomic 64bit (signed)
- integers. These are useful for handling database keys and exact
- counting in +-2^63. WARNING: do not use them as replacement for 32bit
- integers, integer64 are not supported for subscripting by R-core and
- they have different semantics when combined with double, e.g.
- integer64 + double => integer64. Class integer64 can be used in vectors,
- matrices, arrays and data.frames. Methods are available for coercion
- from and to logicals, integers, doubles, characters and factors
- as well as many elementwise and summary functions. Many fast
- algorithmic operations such as 'match' and 'order' support
- inter- active data exploration
- and manipulation and optionally leverage caching."""
+    integers. These are useful for handling database keys and exact
+    counting in +-2^63. WARNING: do not use them as replacement for 32bit
+    integers, integer64 are not supported for subscripting by R-core and
+    they have different semantics when combined with double, e.g.
+    integer64 + double => integer64. Class integer64 can be used in vectors,
+    matrices, arrays and data.frames. Methods are available for coercion
+    from and to logicals, integers, doubles, characters and factors
+    as well as many elementwise and summary functions. Many fast
+    algorithmic operations such as 'match' and 'order' support
+    inter- active data exploration
+    and manipulation and optionally leverage caching."""
 
     homepage = "https://cran.rstudio.com/web/packages/bit64/index.html"
     url      = "https://cran.rstudio.com/src/contrib/bit64_0.9-7.tar.gz"
