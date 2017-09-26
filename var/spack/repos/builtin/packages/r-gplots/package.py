ffc86c22dfebd8fb059d3b4fcf22d088032f3540
lingnanyuan <1297162327@qq.com>
2017-09-26 15:57:30 -0500

    r-gplots: Correct format.

--- var/spack/repos/builtin/packages/r-gplots/package.py
+++ var/spack/repos/builtin/packages/r-gplots/package.py
@@ -34,10 +34,7 @@ class RGplots(RPackage):
     url      = "https://cran.rstudio.com/src/contrib/gplots_3.0.1.tar.gz"
     list_url = homepage
     version('3.0.1', '6116822401d55add044beb120ca93d14')
-    depends_on('r-gtools',type = ('build','run'))
-    depends_on('r-gdata',type = ('build','run'))
-    depends_on('r-catools',type = ('build','run'))
-    depends_on('r-kernsmooth',type = ('build','run'))
-
-
-
+    depends_on('r-gtools', type=('build', 'run'))
+    depends_on('r-gdata', type=('build', 'run'))
+    depends_on('r-catools', type=('build', 'run'))
+    depends_on('r-kernsmooth', type=('build', 'run'))
