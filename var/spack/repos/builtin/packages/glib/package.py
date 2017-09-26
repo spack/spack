9857413b7488df43767754c20453c79b9806bb7b
Michael Kuhn <suraia@ikkoku.de>
2017-09-26 15:27:28 +0200

    Missing perl dependencies (#5204)
    * glib: Add missing perl dependency
    
    * shared-mime-info: Add missing perl dependency
    
    * libx11: Add missing perl dependency
    
    * likwid: Add missing perl dependency

--- var/spack/repos/builtin/packages/glib/package.py
+++ var/spack/repos/builtin/packages/glib/package.py
@@ -46,6 +46,7 @@ class Glib(AutotoolsPackage):
     depends_on('libffi')
     depends_on('zlib')
     depends_on('gettext')
+    depends_on('perl', type=('build', 'run'))
     depends_on('pcre+utf', when='@2.48:')
     depends_on('util-linux', when='+libmount')
 
@@ -70,3 +71,11 @@ class Glib(AutotoolsPackage):
             args.append('--disable-libmount')
 
         return args
+
+    @run_before('install')
+    def filter_sbang(self):
+        # Filter sbang before install so Spack's sbang hook can fix it up
+        perl = join_path(self.spec['perl'].prefix.bin, 'perl')
+        files = ['gobject/glib-mkenums']
+
+        filter_file('^#! /usr/bin/perl', '#!{0}'.format(perl), *files)
