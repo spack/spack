43ecce154aa89d3020e84a2417ee532b5b20ad01
Robert Pavel <rspavel@lanl.gov>
2017-09-26 11:59:32 -0600

    Added CoEVP Spackage
    Added spackage for baseline (mpi, no other dependencies) CoEVP

---
+++ var/spack/repos/builtin/packages/coevp/package.py
@@ -0,0 +1,69 @@
+##############################################################################
+# Copyright (c) 2017, Los Alamos National Security, LLC
+# Produced at the Los Alamos National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+
+from spack import *
+
+
+class Coevp(MakefilePackage):
+    """CoEVP is a scale-bridging proxy application for embedded viscoplasticity
+    applications. It is created and maintained by The Exascale Co-Design Center
+    for Materials in Extreme Environments (ExMatEx). The code is intended to
+    serve as a vehicle for co-design by allowing others to extend and/or
+    reimplement it as needed to test performance of new architectures,
+    programming models, etc.
+    Due to the size and complexity of the studied models, as well as
+    restrictions on distribution, the currently available LULESH proxy
+    application provides the coarse-scale model implementation and the ASPA
+    proxy application provides the adaptive sampling support."""
+
+    homepage = 'https://github.com/exmatex/CoEVP'
+
+    version('develop', git='https://github.com/exmatex/CoEVP.git',
+            branch='master')
+
+    variant('mpi', default=True, description='Build with MPI Support')
+
+    depends_on('mpi', when='+mpi')
+
+    @property
+    def build_targets(self):
+        targets = []
+        if '+mpi' in self.spec:
+            targets.append('COEVP_MPI=yes')
+        else:
+            targets.append('COEVP_MPI=no')
+        targets.append('FLANN=no')
+        targets.append('REDIS=no')
+        targets.append('SILO=no')
+        targets.append('TWEMPROXY=no')
+        return targets
+
+    def install(self, spec, prefix):
+        mkdir(prefix.bin)
+        mkdir(prefix.doc)
+        install('LULESH/lulesh', prefix.bin)
+        install('COPYRIGHT', prefix.doc)
+        install('README.md', prefix.doc)
+        install('CoEVP.pdf', prefix.doc)
