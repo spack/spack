66fbf0a5d47c49740eb0f33a2c4547ac41b952d1
lingnanyuan <1297162327@qq.com>
2017-09-26 13:09:10 -0500

    r-kegg-db: Correct format.

--- var/spack/repos/builtin/packages/r-kegg-db/package.py
+++ var/spack/repos/builtin/packages/r-kegg-db/package.py
@@ -29,8 +29,7 @@ class RKeggDb(RPackage):
     """A set of annotation maps for KEGG assembled using data from KEGG."""
 
     homepage = "https://www.bioconductor.org/packages/KEGG.db/"
-    url = "https://www.bioconductor.org/packages/release/data"
-    "/annotation/src/contrib/KEGG.db_3.2.3.tar.gz"
+    url = "https://www.bioconductor.org/packages/release/data/annotation/src/contrib/KEGG.db_3.2.3.tar.gz"
 
     version('3.2.3', '023ac22f57063627c2e62d1ae5e011b0')
     depends_on('r-annotationdbi', type=('build', 'run'))
