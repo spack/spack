365c26a485fc3823172f4c11842cd4786117d2eb
Sheng Di <sdi1@anl.gov>
2017-09-27 16:24:20 -0500

    add a new version (1.4.11) to the Spack pacakge for sz.

--- var/spack/repos/builtin/packages/sz/package.py
+++ var/spack/repos/builtin/packages/sz/package.py
@@ -35,6 +35,8 @@ class Sz(AutotoolsPackage):
     version('develop', git='https://github.com/disheng222/SZ.git',
             branch='master')
     version('1.4.11.0', '10dee28b3503821579ce35a50e352cc6')
+    version('1.4.10.0', '82e23dc5a51bcce1f70ba7e3b68a5965')
+    version('1.4.9.2',  '028ce90165b7a4c4051d4c0189f193c0')
 
     variant('fortran', default=False,
             description='Enable fortran compilation')
