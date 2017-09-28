cbb385e3b87f1767017f10d24c3ea98b66d7c23e
fanne-stat <ifanzhu@foxmail.com>
2017-09-28 15:44:55 -0500

    r-dbi: modified according to flake8

--- var/spack/repos/builtin/packages/r-dbi/package.py
+++ var/spack/repos/builtin/packages/r-dbi/package.py
@@ -33,4 +33,4 @@ class RDbi(RPackage):
     homepage = "http://rstats-db.github.io/DBI"
     url      = "https://cran.rstudio.com/src/contrib/DBI_0.7.tar.gz"
     list_url = homepage
-    version('0.7', '66065dd687d758b72d638adb6a8cab2e')    
+    version('0.7', '66065dd687d758b72d638adb6a8cab2e')
