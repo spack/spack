c3f5e96941b7bac987405664c92f962ffcffb311
Yifan Zhu <ifanzhu@foxmail.com>
2017-09-28 15:53:48 -0500

    Update package.py

--- var/spack/repos/builtin/packages/r-dbi/package.py
+++ var/spack/repos/builtin/packages/r-dbi/package.py
@@ -33,4 +33,6 @@ class RDbi(RPackage):
     homepage = "http://rstats-db.github.io/DBI"
     url      = "https://cran.rstudio.com/src/contrib/DBI_0.7.tar.gz"
     list_url = homepage
+ 
+    version('0.4-1', 'c7ee8f1c5037c2284e99c62698d0f087')
     version('0.7', '66065dd687d758b72d638adb6a8cab2e')
