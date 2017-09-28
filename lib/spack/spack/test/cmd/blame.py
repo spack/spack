41a2652ef256c673dd457eebe8f8787ba151aeab
Todd Gamblin <tgamblin@llnl.gov>
2017-09-28 10:47:08 -0700

    Add 'spack blame' command: shows contributors to packages
    `spack blame` prints out the contributors to a package.
    
    By modification time:
    
    ```
    $ spack blame --time llvm
    LAST_COMMIT    LINES  %      AUTHOR               EMAIL
    3 days ago     2      0.6    Andrey Prokopenko    <andrey.prok@gmail.com>
    3 weeks ago    125    34.7   Massimiliano Culpo   <massimiliano.culpo@epfl.ch>
    3 weeks ago    3      0.8    Peter Scheibel       <scheibel1@llnl.gov>
    2 months ago   21     5.8    Adam J. Stewart      <ajstewart426@gmail.com>
    2 months ago   1      0.3    Gregory Becker       <becker33@llnl.gov>
    3 months ago   116    32.2   Todd Gamblin         <tgamblin@llnl.gov>
    5 months ago   2      0.6    Jimmy Tang           <jcftang@gmail.com>
    5 months ago   6      1.7    Jean-Paul Pelteret   <jppelteret@gmail.com>
    7 months ago   65     18.1   Tom Scogland         <tscogland@llnl.gov>
    11 months ago  13     3.6    Kelly (KT) Thompson  <kgt@lanl.gov>
    a year ago     1      0.3    Scott Pakin          <pakin@lanl.gov>
    a year ago     3      0.8    Erik Schnetter       <schnetter@gmail.com>
    3 years ago    2      0.6    David Beckingsale    <davidbeckingsale@gmail.com>
    
    3 days ago     360    100.0
    ```
    
    Or by percent contribution:
    
    ```
    $ spack blame --percent llvm
    LAST_COMMIT    LINES  %      AUTHOR               EMAIL
    3 weeks ago    125    34.7   Massimiliano Culpo   <massimiliano.culpo@epfl.ch>
    3 months ago   116    32.2   Todd Gamblin         <tgamblin@llnl.gov>
    7 months ago   65     18.1   Tom Scogland         <tscogland@llnl.gov>
    2 months ago   21     5.8    Adam J. Stewart      <ajstewart426@gmail.com>
    11 months ago  13     3.6    Kelly (KT) Thompson  <kgt@lanl.gov>
    5 months ago   6      1.7    Jean-Paul Pelteret   <jppelteret@gmail.com>
    3 weeks ago    3      0.8    Peter Scheibel       <scheibel1@llnl.gov>
    a year ago     3      0.8    Erik Schnetter       <schnetter@gmail.com>
    3 years ago    2      0.6    David Beckingsale    <davidbeckingsale@gmail.com>
    3 days ago     2      0.6    Andrey Prokopenko    <andrey.prok@gmail.com>
    5 months ago   2      0.6    Jimmy Tang           <jcftang@gmail.com>
    2 months ago   1      0.3    Gregory Becker       <becker33@llnl.gov>
    a year ago     1      0.3    Scott Pakin          <pakin@lanl.gov>
    
    3 days ago     360    100.0
    ```

---
+++ lib/spack/spack/test/cmd/blame.py
@@ -0,0 +1,59 @@
+##############################################################################
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+import pytest
+
+import spack.cmd
+from spack.main import SpackCommand
+from spack.util.executable import which
+
+pytestmark = pytest.mark.skipif(
+    not which('git') or not spack.cmd.spack_is_git_repo(),
+    reason="needs git")
+
+blame = SpackCommand('blame')
+
+
+def test_blame_by_modtime(builtin_mock):
+    """Sanity check the blame command to make sure it works."""
+    out = blame('--time', 'mpich')
+    assert 'LAST_COMMIT' in out
+    assert 'AUTHOR' in out
+    assert 'EMAIL' in out
+
+
+def test_blame_by_percent(builtin_mock):
+    """Sanity check the blame command to make sure it works."""
+    out = blame('--percent', 'mpich')
+    assert 'LAST_COMMIT' in out
+    assert 'AUTHOR' in out
+    assert 'EMAIL' in out
+
+
+def test_blame_by_git(builtin_mock, capfd):
+    """Sanity check the blame command to make sure it works."""
+    with capfd.disabled():
+        out = blame('--git', 'mpich')
+    assert 'Mpich' in out
+    assert 'mock_packages' in out
