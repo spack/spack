5c22ba0c81bff73879524176b11f5e5c8bab9738
peetsv <vernon.peets@contractors.roche.com>
2017-10-05 10:33:24 -0700

    Updating ag to the latest version

--- var/spack/repos/builtin/packages/the-silver-searcher/package.py
+++ var/spack/repos/builtin/packages/the-silver-searcher/package.py
@@ -28,8 +28,9 @@ from spack import *
 class TheSilverSearcher(AutotoolsPackage):
     """Fast recursive grep alternative"""
     homepage = "http://geoff.greer.fm/ag/"
-    url      = "http://geoff.greer.fm/ag/releases/the_silver_searcher-0.32.0.tar.gz"
+    url      = "https://geoff.greer.fm/ag/releases/the_silver_searcher-2.1.0.tar.gz"
 
+    version('2.1.0', '3e7207b060424174323236932bf76ec2')
     version('0.32.0', '3fdfd5836924246073d5344257a06823')
     version('0.30.0', '95e2e7859fab1156c835aff7413481db')
 
