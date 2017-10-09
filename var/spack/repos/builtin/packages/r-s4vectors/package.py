affcd4ad634774cb52d44faf0c5bbd832db88191
fanne-stat <ifanzhu@foxmail.com>
2017-10-09 15:50:57 -0500

    r-s4vectors: added version 0.14.7

--- var/spack/repos/builtin/packages/r-s4vectors/package.py
+++ var/spack/repos/builtin/packages/r-s4vectors/package.py
@@ -37,8 +37,10 @@ class RS4vectors(RPackage):
 
     homepage = "https://bioconductor.org/packages/S4Vectors/"
     url      = "https://bioconductor.org/packages/3.5/bioc/src/contrib/S4Vectors_0.14.4.tar.gz"
+    list_url = homepage
 
+    version('0.14.7', '3d7cc843d8fd56ec983d511f27a4ed57')
     version('0.14.4', '08ccc46e6d39f3aa9091f868ecec1f70')
 
     depends_on('r-biocgenerics', type=('build', 'run'))
-    depends_on('r@3.4.0:3.4.9', when='@0.14.4')
+    depends_on('r@3.4.0:3.4.9', when='@0.14.4:0.14.7')
