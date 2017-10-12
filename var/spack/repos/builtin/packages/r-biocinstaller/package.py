4816db95ead66c201aee3f9ad276265dc219fc8d
Yifan Zhu <ifanzhu@foxmail.com>
2017-10-11 20:49:08 -0500

    r-biocinstaller: added versioned dependency on r (#5663)

--- var/spack/repos/builtin/packages/r-biocinstaller/package.py
+++ var/spack/repos/builtin/packages/r-biocinstaller/package.py
@@ -36,4 +36,4 @@ class RBiocinstaller(RPackage):
     version('1.26.1', 'ec0f8866639475bcd91179dcc840b5f4')
     version('1.25.3', '6214770455a5122dca5544861f52c91d')
 
-    depends_on('r@3.4.0:')
+    depends_on('r@3.4.0:3.4.9', when='@1.26.1')
