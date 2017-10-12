a903a62377411fd8540263cd5d2a3230d8f775fb
Satish Balay <balay@mcs.anl.gov>
2017-10-12 15:35:28 -0500

    mfem: fix merge

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -46,7 +46,7 @@ class Xsdk(Package):
     depends_on('hypre@xsdk-0.2.0~internal-superlu', when='@xsdk-0.2.0')
     depends_on('hypre@develop~internal-superlu', when='@develop')
 
-    depends_on('mfem@3.3.2-rc2', when='@xsdk-0.3.0')
+    depends_on('mfem@3.3.2-rc2', when='@0.3.0-rc1')
     depends_on('mfem@3.3.2-rc2', when='@develop')
 
     depends_on('superlu-dist@xsdk-0.2.0', when='@xsdk-0.2.0')
