da4323df78981530c7661a83f594afda5600f43a
Satish Balay <balay@mcs.anl.gov>
2017-10-13 16:16:58 -0500

    flake8: fix warnings (#5759)

--- var/spack/repos/builtin/packages/mfem/package.py
+++ var/spack/repos/builtin/packages/mfem/package.py
@@ -22,8 +22,8 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-from spack import *
 import re
+from spack import *
 
 
 class Mfem(Package):
