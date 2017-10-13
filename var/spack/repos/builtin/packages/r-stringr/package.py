3479c101cc0e678762d36da5ee2d96f79a6a1bb0
Yifan Zhu <ifanzhu@foxmail.com>
2017-10-13 08:46:29 -0500

    r-stringr: added list_url and version 1.2.0 (#5738)

--- var/spack/repos/builtin/packages/r-stringr/package.py
+++ var/spack/repos/builtin/packages/r-stringr/package.py
@@ -34,7 +34,9 @@ class RStringr(RPackage):
 
     homepage = "https://cran.r-project.org/web/packages/stringr/index.html"
     url      = "https://cran.r-project.org/src/contrib/stringr_1.1.0.tar.gz"
+    list_url = "https://cran.r-project.org/web/packages/stringr/index.html"
 
+    version('1.2.0', '9054b1de91c578cc5cf454d656e9c697')
     version('1.1.0', '47973a33944c6d5db9524b1e835b8a5d')
     version('1.0.0', '5ca977c90351f78b1b888b379114a7b4')
 
