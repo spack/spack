31115af128b9ef0fb83935589b8e86e8bbccc94c
Christoph Junghans <christoph.junghans@gmail.com>
2017-10-14 12:53:25 -0600

    boost: call_once_variadic.patch is already part of 1.56.0 (#5341)

--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -162,7 +162,7 @@ class Boost(Package):
     patch('xl_1_62_0_le.patch', when='@1.62.0%xl_r')
     patch('xl_1_62_0_le.patch', when='@1.62.0%xl')
 
-    patch('call_once_variadic.patch', when='@:1.56.0%gcc@5:')
+    patch('call_once_variadic.patch', when='@:1.55.9999%gcc@5:')
 
     # Patch fix for PGI compiler
     patch('boost_1.63.0_pgi.patch', when='@1.63.0%pgi')
