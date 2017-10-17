aa120eefbce96cae33930538bc91e567289f4148
shudson <shudson@anl.gov>
2017-10-17 13:26:26 -0500

    py-petsc4py: Update version to 3.8.0 and add dependencies
    - Added explicit dependency on python and numpy

--- var/spack/repos/builtin/packages/py-petsc4py/package.py
+++ var/spack/repos/builtin/packages/py-petsc4py/package.py
@@ -28,11 +28,14 @@ from spack import *
 class PyPetsc4py(PythonPackage):
     """This package provides Python bindings for the PETSc package.
     """
+
     homepage = "https://pypi.python.org/pypi/petsc4py"
-    url      = "https://pypi.io/packages/source/p/petsc4py/petsc4py-3.7.0.tar.gz"
+    url      = "https://pypi.io/packages/source/p/petsc4py/petsc4py-3.8.0.tar.gz"
 
-    version('3.7.0', '816a20040a6a477bd637f397c9fb5b6d')
+    version('3.8.0', '02029be4bdec904854f0e0692005fb06')
 
+    depends_on('python@2.7:2.8,3.3:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
+    depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-mpi4py', type=('build', 'run'))
     depends_on('petsc+mpi')
