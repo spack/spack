c59b91eda90559535f63fa0397ff10d43a8861c1
Mark Grondona <mark.grondona@gmail.com>
2017-10-23 04:32:51 -0700

    jansson: enable shared library (#5857)
    Jansson builds only a static library by default, which is probably
    not what most users want. Add Cmake args required to build a shared
    library and enable those via a default 'shared' variant of the
    package.

--- var/spack/repos/builtin/packages/jansson/package.py
+++ var/spack/repos/builtin/packages/jansson/package.py
@@ -33,3 +33,12 @@ class Jansson(CMakePackage):
     url      = "https://github.com/akheron/jansson/archive/v2.9.tar.gz"
 
     version('2.9', 'd2db25c437b359fc5a065ed938962237')
+
+    variant('shared', default=True,
+            description='Enables the build of shared libraries')
+
+    def cmake_args(self):
+        return [
+            '-DJANSSON_BUILD_SHARED_LIBS:BOOL=%s' % (
+                'ON' if '+shared' in self.spec else 'OFF'),
+        ]
