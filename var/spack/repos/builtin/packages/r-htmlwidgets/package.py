ccc8fabd4282ec01108563de5f6a42e670cdbcab
lingnanyuan <1297162327@qq.com>
2017-10-24 04:25:42 -0500

    r-htmlwidgets: Update verion to 0.9 (#5881)

--- var/spack/repos/builtin/packages/r-htmlwidgets/package.py
+++ var/spack/repos/builtin/packages/r-htmlwidgets/package.py
@@ -30,10 +30,11 @@ class RHtmlwidgets(RPackage):
     including the R console, 'R Markdown' documents, and 'Shiny' web
     applications."""
 
-    homepage = "https://github.com/ramnathv/htmlwidgets"
-    url      = "https://cran.r-project.org/src/contrib/htmlwidgets_0.6.tar.gz"
-    list_url = homepage
+    homepage = "https://cran.r-project.org/package=htmlTable"
+    url      = "https://cran.rstudio.com/src/contrib/htmlwidgets_0.9.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/htmlwidgets"
 
+    version('0.9', 'b42730691eca8fc9a28903c272d11605')
     version('0.8', '06b0404a00e25736946607a36ee5351d')
     version('0.6', '7fa522d2eda97593978021bda9670c0e')
 
