eb249a29fc779a2b43091c35a1bd6fab7407295e
Michael Wolf <mmwolf@sandia.gov>
2017-10-24 17:02:52 -0600

    adding miniTri miniapp

---
+++ var/spack/repos/builtin/packages/minitri/package.py
@@ -0,0 +1,90 @@
+##############################################################################
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install minitri
+#
+# You can edit this file again by typing:
+#
+#     spack edit minitri
+#
+# See the Spack documentation for more information on packaging.
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+from spack import *
+
+
+class Minitri(MakefilePackage):
+    """A simple, triangle-based data analytics proxy application."""
+
+    homepage = "https://github.com/Mantevo/miniTri"
+    url      = "https://github.com/Mantevo/miniTri/archive/v1.0.tar.gz"
+
+    version('1.0', '947e296ca408275232f47724267a85ce')
+
+    # FIXME: Add dependencies if required.
+    # depends_on('foo')
+
+    variant('mpi', default=False, description='Build with MPI support')
+
+    depends_on('mpi', when="+mpi")
+
+
+# Need to deal with compilers!! -- especially for serial
+
+    @property
+    def build_targets(self):
+        targets = []
+        if '+mpi' in self.spec:
+            targets.append('CC={0}'.format(self.spec['mpi'].mpicc))
+            targets.append('--file=Makefile')
+            targets.append('--directory=miniTri/linearAlgebra/MPI')
+        else:
+            targets.append('--file=Makefile')
+            targets.append('--directory=miniTri/linearAlgebra/serial')
+
+        return targets
+
+    def install(self, spec, prefix):
+        # Manual installation
+        mkdir(prefix.bin)
+        mkdir(prefix.doc)
+
+        if '+mpi' in spec:
+            install('miniTri/linearAlgebra/MPI/miniTri.exe', prefix.bin)
+        else:
+            install('miniTri/linearAlgebra/serial/miniTri.exe', prefix.bin)
+
+
+
+
+    #def install(self, spec, prefix):
+        # FIXME: Unknown build system
+    #    make()
+    #    make('install')
