8d238306638ede46260e24229fa527b0e50fdb45
lingnanyuan <1297162327@qq.com>
2017-10-24 04:22:23 -0500

    r-testit: Update version to 0.7 (#5852)

--- var/spack/repos/builtin/packages/r-testit/package.py
+++ var/spack/repos/builtin/packages/r-testit/package.py
@@ -30,7 +30,9 @@ class RTestit(RPackage):
     """Provides two convenience functions assert() and test_pkg() to facilitate
     testing R packages."""
 
-    homepage = "https://github.com/yihui/testit"
+    homepage = "https://cran.r-project.org/package=testit"
     url      = "https://cran.r-project.org/src/contrib/testit_0.5.tar.gz"
+    list_url = homepage
 
+    version('0.7', 'cfc5f5c66aa644fbf53efc4b29d18e8c')
     version('0.5', 'f206d3cbdc5174e353d2d05ba6a12e59')
