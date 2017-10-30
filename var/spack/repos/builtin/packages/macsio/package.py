336ec1d01275008f6ce9fe003fbe245df5c737d7
Abhinav Bhatele <bhatele@llnl.gov>
2017-10-30 07:19:10 -0700

    macsio: make requested changes

--- var/spack/repos/builtin/packages/macsio/package.py
+++ var/spack/repos/builtin/packages/macsio/package.py
@@ -26,7 +26,9 @@ from spack import *
 
 
 class Macsio(CMakePackage):
-    """A Multi-purpose, Application-Centric, Scalable I/O Proxy Application"""
+    """A Multi-purpose, Application-Centric, Scalable I/O Proxy Application
+    """
+    tags = ['proxy-app', 'ecp-proxy-app']
 
     homepage = "http://llnl.github.io/MACSio"
 
@@ -34,31 +36,26 @@ class Macsio(CMakePackage):
     # url = "https://github.com/LLNL/MACSio/releases/download/v0.9/macsio-0.9.tar.gz"
     # version('0.9', '6d4bf863c90975a3df54795c5abb9eb8f23d5aaf')
 
-    version('master', git='https://github.com/LLNL/MACSio.git', branch='cmake2')
-    depends_on('json-cwx')
+    version('develop', git='https://github.com/LLNL/MACSio.git',
+            branch='cmake2')
 
-    ## TODO: Test these and add these to spack!
-
-    variant('mpi', default=False, description="Build MPI plugin")
-    depends_on('mpi', when="+mpi")
-
+    variant('mpi', default=True, description="Build MPI plugin")
     variant('silo', default=False, description="Build with SILO plugin")
-    depends_on('silo', when="+silo")
-
     variant('hdf5', default=False, description="Build HDF5 plugin")
-    depends_on('hdf5', when="+hdf5")
-
     variant('pdb', default=False, description="Build PDB plugin")
-    depends_on('pdb', when="+pdb")
-
     variant('exodus', default=False, description="Build EXODUS plugin")
-    depends_on('exodus', when="+exodus")
-
     variant('typhonio', default=False, description="Build TYPHONIO plugin")
-    depends_on('typhonio', when="+typhonio")
-
     variant('scr', default=False, description="Build with SCR support")
+
+    depends_on('mpi')
+    depends_on('json-cwx')
+    depends_on('silo', when="+silo")
+    depends_on('hdf5', when="+hdf5")
     depends_on('scr', when="+scr")
+    ## TODO: Test these and add these to spack!
+    depends_on('pdb', when="+pdb")
+    depends_on('exodus', when="+exodus")
+    depends_on('typhonio', when="+typhonio")
 
 
     def get_abs_path_rel_prefix(self, path):
@@ -70,9 +67,11 @@ class Macsio(CMakePackage):
 
     def cmake_args(self):
         spec = self.spec
-        args = []
+        cmake_args = []
 
         if "+silo" in spec:
-            args.apprend("-DWITH_SILO_PREFIX={0}".format(spec['silo'].prefix))
+            cmake_args.apprend("-DWITH_SILO_PREFIX={0}".format(spec['silo'].prefix))
 
         # TODO: add proper args
+
+        return cmake_args
