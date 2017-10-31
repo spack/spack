bf9fe2d150eafcc65a08d505b9a9ffac4dfd644f
Milton Woods <milton.woods@bom.gov.au>
2017-11-01 09:29:36 +1100

    blitz: change URL for release tarballs

--- var/spack/repos/builtin/packages/blitz/package.py
+++ var/spack/repos/builtin/packages/blitz/package.py
@@ -28,7 +28,7 @@ from spack import *
 class Blitz(AutotoolsPackage):
     """N-dimensional arrays for C++"""
     homepage = "http://github.com/blitzpp/blitz"
-    url = "https://github.com/blitzpp/blitz/tarball/1.0.0"
+    url = "https://github.com/blitzpp/blitz/archive/1.0.1.tar.gz"
 
     version('1.0.1', 'fe43e2cf6c9258bc8b369264dd008971')
     version('1.0.0', '9f040b9827fe22228a892603671a77af')
