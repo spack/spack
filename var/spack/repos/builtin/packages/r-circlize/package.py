17ee00591360d3678215227bbca78e0dc2031253
fanne-stat <ifanzhu@foxmail.com>
2017-11-01 14:49:41 -0500

    r-circlize: modified description

--- var/spack/repos/builtin/packages/r-circlize/package.py
+++ var/spack/repos/builtin/packages/r-circlize/package.py
@@ -27,7 +27,15 @@ from spack import *
 
 class RCirclize(RPackage):
     """Circular layout is an efficient way for the visualization of huge
-       amounts of information. Here this package provides an implementation of circular layout generation in R as well as an enhancement of available software. The flexibility of the package is based on the usage of low-level graphics functions such that self-defined high-level graphics can be easily implemented by users for specific purposes. Together with the seamless connection between the powerful computational and visual environment in R, it gives users more convenience and freedom to design figures for better understanding complex patterns behind multiple dimensional data."""
+       amounts of information. Here this package provides an implementation
+       of circular layout generation in R as well as an enhancement of
+       available software. The flexibility of the package is based on the
+       usage of low-level graphics functions such that self-defined
+       high-level graphics can be easily implemented by users for specific
+       purposes. Together with the seamless connection between the powerful
+       computational and visual environment in R, it gives users more
+       convenience and freedom to design figures for better understanding
+       complex patterns behind multiple dimensional data."""
 
     homepage = "https://cran.r-project.org/package=circlize"
     url      = "https://cran.r-project.org/src/contrib/circlize_0.4.1.tar.gz"
