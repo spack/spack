92c08d2c39961fe38c0f3e3c88e8a7c7a68f5edd
Satish Balay <balay@mcs.anl.gov>
2017-11-05 08:24:16 -0600

    xsdk: update petsc to 3.8.1 to get MKL build fix

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -64,7 +64,7 @@ class Xsdk(Package):
     depends_on('trilinos@develop+xsdkflags+hypre+superlu-dist+metis+hdf5~mumps+boost~suite-sparse~tpetra~ifpack2~zoltan2~amesos2~exodus',
                when='@develop')
 
-    depends_on('petsc@3.8.0+trilinos+mpi+hypre+superlu-dist+metis+hdf5~mumps~boost+double~int64',
+    depends_on('petsc@3.8.1+trilinos+mpi+hypre+superlu-dist+metis+hdf5~mumps~boost+double~int64',
                when='@0.3.0-rc1')
     depends_on('petsc@xsdk-0.2.0+trilinos+mpi+hypre+superlu-dist+metis+hdf5~mumps~boost+double~int64',
                when='@xsdk-0.2.0')
