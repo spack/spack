2a8c97d440c24fcf40af7775ef6b7bd304f0f540
Adam Moody <moody20@llnl.gov>
2017-11-05 09:25:03 -0800

    dtcmp: add v1.1.0 (#6146)

--- var/spack/repos/builtin/packages/dtcmp/package.py
+++ var/spack/repos/builtin/packages/dtcmp/package.py
@@ -32,6 +32,7 @@ class Dtcmp(Package):
     homepage = "https://github.com/hpc/dtcmp"
     url      = "https://github.com/hpc/dtcmp/releases/download/v1.0.3/dtcmp-1.0.3.tar.gz"
 
+    version('1.1.0', 'af5c73f7d3a9afd90a22d0df85471d2f')
     version('1.0.3', 'cdd8ccf71e8ff67de2558594a7fcd317')
 
     depends_on('mpi')
