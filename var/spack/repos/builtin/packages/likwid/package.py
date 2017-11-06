619984ecc7995ec6c914b433c016656c1ed6f753
Michael Kuhn <suraia@ikkoku.de>
2017-11-06 19:14:34 +0100

    likwid: Use external lua if possible (#6009)

--- var/spack/repos/builtin/packages/likwid/package.py
+++ var/spack/repos/builtin/packages/likwid/package.py
@@ -45,8 +45,7 @@ class Likwid(Package):
     # The reason is that the internal hwloc is patched to contain extra
     # functionality and functions are prefixed with "likwid_".
 
-    # TODO: how to specify those?
-    # depends_on('lua')
+    depends_on('lua', when='@4.2.0:')
 
     # TODO: check
     # depends_on('gnuplot', type='run')
@@ -87,6 +86,23 @@ class Likwid(Package):
         filter_file('^INSTALL_CHOWN.*',
                     'INSTALL_CHOWN = -o $(USER)',
                     'config.mk')
+
+        if spec.satisfies('^lua'):
+            filter_file('^#LUA_INCLUDE_DIR.*',
+                        'LUA_INCLUDE_DIR = {0}'.format(
+                            spec['lua'].prefix.include),
+                        'config.mk')
+            filter_file('^#LUA_LIB_DIR.*',
+                        'LUA_LIB_DIR = {0}'.format(
+                            spec['lua'].prefix.lib),
+                        'config.mk')
+            filter_file('^#LUA_LIB_NAME.*',
+                        'LUA_LIB_NAME = lua',
+                        'config.mk')
+            filter_file('^#LUA_BIN.*',
+                        'LUA_BIN = {0}'.format(
+                            spec['lua'].prefix.bin),
+                        'config.mk')
 
         make()
         make('install')
