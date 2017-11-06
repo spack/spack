555267364c6d3f061ac8adb238df179d9b21a7d8
Todd Gamblin <tgamblin@llnl.gov>
2017-11-06 13:53:21 -0800

    add artisanal handcrafted SVG Spack logo.
    - This isn't one of those autogenerated SVGs from a drawing program!
    
    - This is a completely re-traced, minimalist SVG file with clearly
      delineated pieces so that your favorite renderer can draw a Spack logo
      at whatever resolution you want.
    
    - Included versions with text, as well.

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -249,7 +249,7 @@ html_theme_path = ["_themes"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../../../share/spack/logo/spack-logo-white-text-48.png'
+html_logo = '../../../share/spack/logo/spack-logo-white-text.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
