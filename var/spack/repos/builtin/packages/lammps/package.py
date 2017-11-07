32fad0f483816f761d62c40cf32d07d743568e8f
Vishal Boddu <vishal.boddu@fau.de>
2017-11-07 23:20:46 +0100

    removing trailing whitespaces

--- var/spack/repos/builtin/packages/lammps/package.py
+++ var/spack/repos/builtin/packages/lammps/package.py
@@ -47,10 +47,10 @@ class Lammps(CMakePackage):
 
     supported_packages = ['asphere', 'body', 'class2', 'colloid', 'compress',
                           'coreshell', 'dipole', 'granular', 'kspace', 'latte',
-                          'manybody', 'mc', 'meam', 'misc', 'molecule', 
+                          'manybody', 'mc', 'meam', 'misc', 'molecule',
                           'mpiio', 'peri', 'poems', 'python', 'qeq', 'reax',
-                          'replica', 'rigid', 'shock', 'snap', 'srd', 
-                          'user-atc', 'user-h5md', 'user-lb', 'user-misc', 
+                          'replica', 'rigid', 'shock', 'snap', 'srd',
+                          'user-atc', 'user-h5md', 'user-lb', 'user-misc',
                           'user-netcdf', 'user-omp', 'voronoi']
 
     for pkg in supported_packages:
