3bea9de46c8984ecad83b9c434dc895a426d0779
lingnanyuan <1297162327@qq.com>
2017-11-08 20:51:13 -0600

    r-quantmod: Update version to 0.4-11 (#6212)

--- var/spack/repos/builtin/packages/r-quantmod/package.py
+++ var/spack/repos/builtin/packages/r-quantmod/package.py
@@ -33,8 +33,10 @@ class RQuantmod(RPackage):
     url      = "https://cran.r-project.org/src/contrib/quantmod_0.4-5.tar.gz"
     list_url = "https://cran.r-project.org/src/contrib/Archive/quantmod"
 
+    version('0.4-10', 'e4119c673567801eee16dcbbd0265de8')
     version('0.4-5', 'cab3c409e4de3df98a20f1ded60f3631')
 
     depends_on('r-xts', type=('build', 'run'))
     depends_on('r-zoo', type=('build', 'run'))
     depends_on('r-ttr', type=('build', 'run'))
+    depends_on('r-curl', type=('build', 'run'))
