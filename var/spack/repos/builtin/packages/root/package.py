266de67c24c40ef79cde490418bb78c4eda59206
Benedikt Riedel <benedikt.riedel@gmail.com>
2017-11-09 08:49:27 -0600

    ROOT needs to have a consistent version of binutils, as it is very sensitive to the version of libopcodes. You can't move the code from one system (with the same OS) as another. (#6226)

--- var/spack/repos/builtin/packages/root/package.py
+++ var/spack/repos/builtin/packages/root/package.py
@@ -55,6 +55,7 @@ class Root(CMakePackage):
     depends_on('cmake@3.4.3:', type='build')
     depends_on('pkg-config',   type='build')
 
+    depends_on('binutils')
     depends_on('zlib')
     # depends_on('unuran')
     depends_on('freetype')
