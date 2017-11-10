2db0238b3f677eeadab86d6f9997b16feb8e3184
shudson <shudson@anl.gov>
2017-11-10 17:53:43 -0600

    Update to 3.8.1

--- var/spack/repos/builtin/packages/py-petsc4py/package.py
+++ var/spack/repos/builtin/packages/py-petsc4py/package.py
@@ -30,8 +30,9 @@ class PyPetsc4py(PythonPackage):
     """
 
     homepage = "https://pypi.python.org/pypi/petsc4py"
-    url      = "https://pypi.io/packages/source/p/petsc4py/petsc4py-3.8.0.tar.gz"
+    url      = "https://pypi.io/packages/source/p/petsc4py/petsc4py-3.8.1.tar.gz"
 
+    version('3.8.1', '5157220c2b81765c581d2b17c03259f8')
     version('3.8.0', '02029be4bdec904854f0e0692005fb06')
     version('3.7.0', '816a20040a6a477bd637f397c9fb5b6d')
 
