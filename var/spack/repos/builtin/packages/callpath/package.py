53d60ed51adbdad6d5ee79cb1a6ca3ca7b55d9c2
becker33 <becker33@llnl.gov>
2017-11-10 15:52:48 -0800

    Add version 1.04 (#6259)

--- var/spack/repos/builtin/packages/callpath/package.py
+++ var/spack/repos/builtin/packages/callpath/package.py
@@ -32,6 +32,7 @@ class Callpath(CMakePackage):
     homepage = "https://github.com/llnl/callpath"
     url      = "https://github.com/llnl/callpath/archive/v1.0.1.tar.gz"
 
+    version('1.0.4', '39d2e06bfa316dec1085b874092e4b08')
     version('1.0.2', 'b1994d5ee7c7db9d27586fc2dcf8f373')
     version('1.0.1', '0047983d2a52c5c335f8ba7f5bab2325')
 
