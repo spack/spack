34bbc731f1e1011edbe76e6e776b012eddb85d6d
Greg Becker <becker33@llnl.gov>
2017-11-12 23:01:46 +0000

    Update packaging tutorial

--- lib/spack/docs/tutorial/examples/1.package.py
+++ lib/spack/docs/tutorial/examples/1.package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Mpileaks(AutotoolsPackage):
+class Mpileaks(Package):
     """Tool to detect and report MPI objects like MPI_Requests and
     MPI_Datatypes."""
 
@@ -35,14 +35,9 @@ class Mpileaks(AutotoolsPackage):
     version('1.0', '8838c574b39202a57d7c2d68692718aa')
 
     # FIXME: Add dependencies if required.
-    # depends_on('m4', type='build')
-    # depends_on('autoconf', type='build')
-    # depends_on('automake', type='build')
-    # depends_on('libtool', type='build')
     # depends_on('foo')
 
-    def configure_args(self):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete the function
-        args = []
-        return args
+    def install(self, spec, prefix):
+        # FIXME: Unknown build system
+        make()
+        make('install')
--- lib/spack/docs/tutorial/examples/5.package.py
+++ lib/spack/docs/tutorial/examples/5.package.py
@@ -22,37 +22,32 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install mpileaks
-#
-# You can edit this file again by typing:
-#
-#     spack edit mpileaks
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
 class Mpileaks(Package):
-    """FIXME: Put a proper description of your package here."""
+    """Tool to detect and report MPI objects like MPI_Requests and
+    MPI_Datatypes."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "http://www.example.com"
+    homepage = "https://github.com/hpc/mpileaks"
     url      = "https://github.com/hpc/mpileaks/releases/download/v1.0/mpileaks-1.0.tar.gz"
 
     version('1.0', '8838c574b39202a57d7c2d68692718aa')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
+    variant('stackstart', values=int, default=0, description='Specify the number of stack frames to truncate.')
+
+    depends_on('mpi')
+    depends_on('adept-utils')
+    depends_on('callpath')
 
     def install(self, spec, prefix):
-        # FIXME: Unknown build system
+        stackstart = int(self.spec.variants['stackstart'].value)
+        confargs = ['--with-adept-utils=%s' % self.spec['adept-utils'].prefix,
+                    '--with-callpath=%s' % self.spec['callpath'].prefix,
+                    '--prefix=%s' % self.spec.prefix]
+        if stackstart:
+            confargs.extend(['--with-stack-start-c=%s' % stackstart,
+                             '--with-stack-start-fortran=%s' % stackstart])
+        configure(*confargs)
         make()
         make('install')
