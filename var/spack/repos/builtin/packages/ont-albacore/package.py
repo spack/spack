7728bdccaa887cc9b6c0801c472e8dc9b32c469e
Eric <erimar77@users.noreply.github.com>
2017-11-13 09:23:14 -0600

    ont-albacore: add version 2.1.2

--- var/spack/repos/builtin/packages/ont-albacore/package.py
+++ var/spack/repos/builtin/packages/ont-albacore/package.py
@@ -35,6 +35,7 @@ class OntAlbacore(Package):
     homepage = "https://nanoporetech.com"
     url = "https://mirror.oxfordnanoportal.com/software/analysis/ont_albacore-1.2.4-cp35-cp35m-manylinux1_x86_64.whl"
 
+    version('2.1.2', '1e60cfb95628829f2a61a85247f1b6af', expand=False)
     version('1.2.4', '559640bec4693af12e4d923e8d77adf6', expand=False)
     version('1.1.0', 'fab4502ea1bad99d813aa2629e03e83d', expand=False)
     extends('python')
