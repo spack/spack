46c94b9471752eadb8bd987c2905c9ab7d85de12
Eric Martin <ericmartin@wustl.edu>
2017-11-17 20:39:53 -0600

    r-som: fix blank line

--- var/spack/repos/builtin/packages/r-som/package.py
+++ var/spack/repos/builtin/packages/r-som/package.py
@@ -24,6 +24,7 @@
 ##############################################################################
 from spack import *
 
+
 class RSom(RPackage):
     """Self-Organizing Map (with application in gene clustering)."""
 
