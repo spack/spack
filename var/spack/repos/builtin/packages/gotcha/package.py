d0f6afec294e86675f7158632f6e6dc35f50ec3a
David Poliakoff <david.poliakoff@gmail.com>
2017-11-20 15:05:30 -0800

    Commit with no boilerplate

--- var/spack/repos/builtin/packages/gotcha/package.py
+++ var/spack/repos/builtin/packages/gotcha/package.py
@@ -22,21 +22,6 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install gotcha
-#
-# You can edit this file again by typing:
-#
-#     spack edit gotcha
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
 
 
@@ -49,7 +34,6 @@ class Gotcha(CMakePackage):
 
     variant('test', default=False, description='Build tests for Gotcha')
 
-    # FIXME: Add proper versions and checksums here.
     version('develop', git='https://github.com/LLNL/gotcha.git', branch="develop")
     version('master', git='https://github.com/LLNL/gotcha.git', branch="master")
     version('0.0.2', git='https://github.com/LLNL/gotcha.git', tag="0.0.2")
