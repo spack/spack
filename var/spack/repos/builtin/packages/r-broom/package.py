5250c8eb0af734571ec6e93ec589c02e628e895b
Yifan Zhu <ifanzhu@foxmail.com>
2017-11-21 03:49:09 -0600

    r-broom: new package (#6282)

--- var/spack/repos/builtin/packages/r-broom/package.py
+++ var/spack/repos/builtin/packages/r-broom/package.py
@@ -25,26 +25,25 @@
 from spack import *
 
 
-class RDplyr(RPackage):
-    """A fast, consistent tool for working with data frame like objects, both
-    in memory and out of memory."""
+class RBroom(RPackage):
+    """Convert statistical analysis objects from R into tidy data frames, so
+       that they can more easily be combined, reshaped and otherwise processed
+       with tools like 'dplyr', 'tidyr' and 'ggplot2'. The package provides
+       three S3 generics: tidy, which summarizes a model's statistical
+       findings such as coefficients of a regression; augment, which adds
+       columns to the original data such as predictions, residuals and cluster
+       assignments; and glance, which provides a one-row summary of
+       model-level statistics."""
 
-    homepage = "https://github.com/hadley/dplyr"
-    url      = "https://cran.rstudio.com/src/contrib/dplyr_0.7.3.tar.gz"
-    list_url = "https://cran.r-project.org/src/contrib/Archive/dplyr"
+    homepage = "http://github.com/tidyverse/broom"
+    url      = "https://cran.r-project.org/src/contrib/broom_0.4.2.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/broom"
+    version('0.4.2', '6eabab1f2eaec10f93cf9aa56d6a61de')
 
-    version('0.7.3', 'f9760b796917747e9dcd927ebb531c7d')
-    version('0.5.0', '1fcafcacca70806eea2e6d465cdb94ef')
-
-    depends_on('r-assertthat', type=('build', 'run'))
-    depends_on('r-r6', type=('build', 'run'))
-    depends_on('r-rcpp', type=('build', 'run'))
-    depends_on('r-tibble', type=('build', 'run'))
-    depends_on('r-magrittr', type=('build', 'run'))
-    depends_on('r-lazyeval', type=('build', 'run'))
-    depends_on('r-dbi', type=('build', 'run'))
-    depends_on('r-bindrcpp', type=('build', 'run'))
-    depends_on('r-glue', type=('build', 'run'))
-    depends_on('r-pkgconfig', type=('build', 'run'))
-    depends_on('r-plogr', type=('build', 'run'))
-    depends_on('r-bh', type=('build', 'run'))
+    depends_on('r-plyr', type=('build', 'run'))
+    depends_on('r-dplyr', type=('build', 'run'))
+    depends_on('r-tidyr', type=('build', 'run'))
+    depends_on('r-psych', type=('build', 'run'))
+    depends_on('r-stringr', type=('build', 'run'))
+    depends_on('r-reshape2', type=('build', 'run'))
+    depends_on('r-nlme', type=('build', 'run'))
