05f08262d566a45c7f96fcbc21d403957001eca0
Johann Klähn <johann.klaehn@kip.uni-heidelberg.de>
2017-12-04 11:26:39 +0100

    Update py-backports-functools-lru-cache to 1.4

--- var/spack/repos/builtin/packages/py-backports-functools-lru-cache/package.py
+++ var/spack/repos/builtin/packages/py-backports-functools-lru-cache/package.py
@@ -28,9 +28,10 @@ from spack import *
 class PyBackportsFunctoolsLruCache(PythonPackage):
     """Backport of functools.lru_cache from Python 3.3"""
 
-    homepage = "https://bitbucket.org/jaraco/backports.functools_lru_cache"
-    url      = "https://pypi.io/packages/source/b/backports.functools_lru_cache/backports.functools_lru_cache-1.0.1.zip"
+    homepage = "https://github.com/jaraco/backports.functools_lru_cache"
+    url = "https://pypi.io/packages/source/b/backports.functools_lru_cache/backports.functools_lru_cache-1.4.tar.gz"
 
+    version('1.4', 'b954e7d5e2ca0f0f66ad2ed12ba800e5')
     version('1.0.1', 'c789ef439d189330b99872746a6d9e85')
 
     depends_on('py-setuptools', type='build')
