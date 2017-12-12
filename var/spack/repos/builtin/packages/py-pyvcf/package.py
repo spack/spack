545bb7265e7eb9b790523f941842501899a30205
peetsv <vernon.peets@contractors.roche.com>
2017-12-12 13:09:28 -0800

    a little cleanup

--- var/spack/repos/builtin/packages/py-pyvcf/package.py
+++ var/spack/repos/builtin/packages/py-pyvcf/package.py
@@ -22,32 +22,12 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-pyvcf
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-pyvcf
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
-
 
 class PyPyvcf(PythonPackage):
     """PyVCF - A Variant Call Format Parser for Python"""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/jamescasbon/PyVCF"
     url      = "https://github.com/jamescasbon/PyVCF/archive/v0.6.0.tar.gz"
 
     version('0.6.0', '5e38a610b1bf837e5e74a748e281cec7')
-
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
