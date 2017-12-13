88225bd9124a21fcd62aecf47272116be0e06606
Jianwen WEI <weijianwen@gmail.com>
2017-12-13 18:19:11 +0800

    Register external packages.

--- build_user.sh
+++ build_user.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env python3
 
-function s {
-	spack find $@ | grep 'No package'
-	if [ $? -eq 0 ]
-	then
-		spack install -y $@
-	else
-		echo "$@ has been installed."
-	fi
-}  
+from spack_install import install
+from spack_install import check_pass
+import os
 
-compilers=(
-    %gcc@6.3.0
-    %intel@17
-)
-
-s zlib 	%gcc@6.3.0
+# Register exernal packages
+for pkg in ["jdk@8u141-b15%gcc@4.8.5",
+            "bazel@0.4.5%gcc@4.8.5",
+            "gcc@6.4.0%gcc@4.8.5",
+            "gcc@5.4.0%gcc@4.8.5",
+            "gcc@4.9.4%gcc@4.8.5",
+            "maven@3.3.9%gcc@4.8.",
+            "gradle@3.4%gcc@4.8.5",
+            "ant@1.9.9%gcc@4.8.5",
+            "sbt@0.13.12%gcc@4.8.5",
+            "cmake@3.9.4%gcc@4.8.5",
+            "environment-modules@3.2.10",
+            "intel-parallel-studio@cluster.2017.5+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune  %intel@17.0.5 threads=openmp"]:
+           os.system("spack install {}".format(pkg))
