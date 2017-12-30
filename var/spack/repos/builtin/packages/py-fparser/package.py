b6c76ccaa0523b18cefda5edad34d9839b87f9da
Milton Woods <miltonjwoods@gmail.com>
2017-12-30 15:46:09 +1100

    py-fparser: add latest release 0.0.6

--- var/spack/repos/builtin/packages/py-fparser/package.py
+++ var/spack/repos/builtin/packages/py-fparser/package.py
@@ -33,6 +33,8 @@ class PyFparser(PythonPackage):
     url      = "https://github.com/stfc/fparser/archive/0.0.5.tar.gz"
     giturl   = "https://github.com/stfc/fparser.git"
 
+    version('0.0.6', git=giturl,
+            commit='638c51ec57cf17624505b70321c3784e356b8910')
     version('0.0.5', git=giturl,
             commit='a3ff86b635f7bd7bd281ee94cbd3d9455b288fd9')
     version('develop', git=giturl, branch='master')
