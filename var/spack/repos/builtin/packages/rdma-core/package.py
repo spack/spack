1033fddb45db6186feaba40055a55517082cae47
Levi Baber <baber@iastate.edu>
2018-01-11 12:16:46 -0600

    libfabric: adding rdma-core to provide libibverbs for libfabric

---
+++ var/spack/repos/builtin/packages/rdma-core/package.py
@@ -0,0 +1,44 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+
+from spack import *
+import sys
+
+
+class RdmaCore(CMakePackage):
+    """RDMA core userspace libraries and daemons"""
+
+    homepage = "https://github.com/linux-rdma/rdma-core"
+    url      = "https://github.com/linux-rdma/rdma-core/releases/download/v13/rdma-core-13.tar.gz"
+
+    version('13', '6b072b4307d1cfe45eba4373f68e2927')
+
+    depends_on('libnl')
+
+    @run_before('cmake')
+    def check_platform(self):
+        if not (sys.platform.startswith('freebsd') or
+                sys.platform.startswith('linux')):
+            raise InstallError("rdma-core requires FreeBSD or Linux")
