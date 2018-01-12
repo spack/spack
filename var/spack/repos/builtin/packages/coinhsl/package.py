ada4b71ef2fef9c1902157863f565f0542f6af1a
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2018-01-11 22:35:43 -0800

    coinhsl: make 2014.01.10 preferred version
    Make Harwell Subroutines Library for IPOPT version 2014.01.10 the
    preferred version because it is the latest stable version released.

--- var/spack/repos/builtin/packages/coinhsl/package.py
+++ var/spack/repos/builtin/packages/coinhsl/package.py
@@ -45,7 +45,8 @@ class Coinhsl(AutotoolsPackage):
     url = "file://{0}/coinhsl-archive-2014.01.17.tar.gz".format(os.getcwd())
 
     version('2014.01.17', '9eb3dd40ed034814ed8dfee75b281180c1d9d2ae')
-    version('2014.01.10', '7c2be60a3913b406904c66ee83acdbd0709f229b652c4e39ee5d0876f6b2e907')
+    version('2014.01.10', '7c2be60a3913b406904c66ee83acdbd0709f229b652c4e39ee5d0876f6b2e907',
+            preferred=True)
 
     # CoinHSL fails to build in parallel
     parallel = False
