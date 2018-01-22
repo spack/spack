70ae9f35f8fb6a52dd5b7c5a97c0589c6a09b999
Levi Baber <baberlevi@gmail.com>
2018-01-22 14:06:49 -0600

    libfabric: add fabtests package

---
+++ var/spack/repos/builtin/packages/fabtests/package.py
@@ -0,0 +1,42 @@
+##############################################################################
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Fabtests(AutotoolsPackage):
+    """Fabtests provides a set of examples that uses libfabric"""
+
+    homepage = "http://libfabric.org"
+    url      = "https://github.com/ofiwg/fabtests/archive/v1.5.3.tar.gz"
+
+    version('1.5.3',    'a80b4741f5885a97983259d045f9865c')
+    version('1.4.2',    '50e96cd9559a6e5be2e4fe6e5476224e')
+
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool',  type='build')
+    depends_on('m4',       type='build')
+    depends_on('libfabric')
+
