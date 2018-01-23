b3abf09c3ba3a929922ec1a1a8c160057a613fd0
Christoph Junghans <junghans@lanl.gov>
2018-01-23 14:44:54 -0700

    gromacs: add v2018

--- var/spack/repos/builtin/packages/gromacs/package.py
+++ var/spack/repos/builtin/packages/gromacs/package.py
@@ -41,6 +41,7 @@ class Gromacs(CMakePackage):
     homepage = 'http://www.gromacs.org'
     url = 'http://ftp.gromacs.org/gromacs/gromacs-5.1.2.tar.gz'
 
+    version('2018', '6467ffb1575b8271548a13abfba6374c')
     version('2016.4', '19c8b5c85f3ec62df79d2249a3c272f8')
     version('2016.3', 'e9e3a41bd123b52fbcc6b32d09f8202b')
     version('5.1.4', 'ba2e34d59b3982603b4935d650c08040')
