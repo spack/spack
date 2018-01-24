82b25928de27243e54bcae0ddac59fec742091c4
sknigh <sknigh@sandia.gov>
2018-01-23 19:46:22 -0800

    Added v1.4.2 to libfabric package

--- var/spack/repos/builtin/packages/libfabric/package.py
+++ var/spack/repos/builtin/packages/libfabric/package.py
@@ -33,6 +33,7 @@ class Libfabric(AutotoolsPackage):
     url      = "https://github.com/ofiwg/libfabric/releases/download/v1.5.0/libfabric-1.5.0.tar.gz"
 
     version('1.5.0', 'fda3e9b31ebe184f5157288d059672d6')
+    version('1.4.2', '2009c8e0817060fb99606ddbf6c5ccf8')
 
     fabrics = ('psm',
                'psm2',
