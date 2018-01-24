a62e1a0451d81241e730d0e93dcb113d31041ebf
Brian Van Essen <vanessen1@llnl.gov>
2018-01-23 18:05:54 -0700

    Lbann update (#6987)
    * Added new link line flag
    
    * Refactored some of the common components out of the different
    version's build commands.  Also corrected the when commands to
    properly capture non-numeric names such as local.
    
    * Added support to the Elemental package for the Hydrogen branch.

--- var/spack/repos/builtin/packages/elemental/package.py
+++ var/spack/repos/builtin/packages/elemental/package.py
@@ -34,6 +34,8 @@ class Elemental(CMakePackage):
     homepage = "http://libelemental.org"
     url      = "https://github.com/elemental/Elemental/archive/v0.87.6.tar.gz"
 
+    version('hydrogen-develop', git='https://github.com/LLNL/Elemental.git', branch='hydrogen')
+
     version('develop', git='https://github.com/elemental/Elemental.git', branch='master')
     version('0.87.7', '6c1e7442021c59a36049e37ea69b8075')
     version('0.87.6', '9fd29783d45b0a0e27c0df85f548abe9')
@@ -105,6 +107,7 @@ class Elemental(CMakePackage):
             'libEl', root=self.prefix, shared=shared, recurse=True
         )
 
+    @when('@0.87.6:')
     def cmake_args(self):
         spec = self.spec
 
@@ -168,5 +171,39 @@ class Elemental(CMakePackage):
         if '+python' in spec:
             args.extend([
                 '-DPYTHON_SITE_PACKAGES:STRING={0}'.format(site_packages_dir)])
+
+        return args
+
+    @when('@:0.87.6')
+    def cmake_args(self):
+        spec = self.spec
+
+        if '@:0.87.7' in spec and '%intel@:17.0.2' in spec:
+            raise UnsupportedCompilerError(
+                "Elemental {0} has a known bug with compiler: {1} {2}".format(
+                    spec.version, spec.compiler.name, spec.compiler.version))
+
+        args = [
+            '-DCMAKE_INSTALL_MESSAGE:STRING=LAZY',
+            '-DCMAKE_C_COMPILER=%s' % spec['mpi'].mpicc,
+            '-DCMAKE_CXX_COMPILER=%s' % spec['mpi'].mpicxx,
+            '-DCMAKE_Fortran_COMPILER=%s' % spec['mpi'].mpifc,
+            '-DBUILD_SHARED_LIBS:BOOL=%s'      % ('+shared' in spec),
+            '-DHydrogen_ENABLE_OPENMP:BOOL=%s'       % ('+hybrid' in spec),
+            '-DHydrogen_ENABLE_QUADMATH:BOOL=%s'     % ('+quad' in spec),
+            '-DHydrogen_USE_64BIT_INTS:BOOL=%s'      % ('+int64' in spec),
+            '-DHydrogen_USE_64BIT_BLAS_INTS:BOOL=%s' % ('+int64_blas' in spec),
+            '-DHydrogen_ENABLE_MPC:BOOL=%s'        % ('+mpfr' in spec),
+            '-DHydrogen_GENERAL_LAPACK_FALLBACK=ON',
+        ]
+
+        if 'blas=openblas' in spec:
+            args.extend([
+                '-DHydrogen_USE_OpenBLAS:BOOL=%s' % ('blas=openblas' in spec),
+                '-DOpenBLAS_DIR:STRING={0}'.format(
+                    spec['elemental'].prefix)])
+        elif 'blas=mkl' in spec:
+            args.extend([
+                '-DHydrogen_USE_MKL:BOOL=%s' % ('blas=mkl' in spec)])
 
         return args
