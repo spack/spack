940005513318426bbe9ed7391cb56b7cfcba820a
Brian C. Van Essen <vanessen1@llnl.gov>
2018-01-25 13:41:28 -0800

    Fixed flake8

--- var/spack/repos/builtin/packages/elemental/package.py
+++ var/spack/repos/builtin/packages/elemental/package.py
@@ -156,7 +156,8 @@ class Elemental(CMakePackage):
             xl_root = os.path.dirname(xl_bin)
             libfortran = LibraryList('{0}/lib/libxlf90_r.{1}.1'
                                      .format(xl_root, dso_suffix))
-        else: libfortran = None
+        else:
+            libfortran = None
 
         if 'libfortran' in locals():
             args.append('-DGFORTRAN_LIB=%s' % libfortran.libraries[0])
