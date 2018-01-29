60cfca6acba7974ccb60c62979491cce27c4c789
Mark W. Krentel <krentel@rice.edu>
2018-01-29 15:13:13 -0600

    intel-xed: style fixes for flake8.

--- var/spack/repos/builtin/packages/intel-xed/package.py
+++ var/spack/repos/builtin/packages/intel-xed/package.py
@@ -35,13 +35,13 @@ class IntelXed(Package):
     url = "https://github.com/intelxed/xed"
 
     version('2018.01.12',
-            git = 'https://github.com/intelxed/xed',
-            commit = '5c538047876feecf080d9441110f81d0e67b5de8')
+            git='https://github.com/intelxed/xed',
+            commit='5c538047876feecf080d9441110f81d0e67b5de8')
 
-    resource(name = 'mbuild',
-             git = 'https://github.com/intelxed/mbuild',
-             commit = 'bb9123152a330c7fa1ff1a502950dc199c83e177',
-             destination = '')
+    resource(name='mbuild',
+             git='https://github.com/intelxed/mbuild',
+             commit='bb9123152a330c7fa1ff1a502950dc199c83e177',
+             destination='')
 
     variant('debug', default=False, description='enable debug symbols')
 
@@ -51,26 +51,34 @@ class IntelXed(Package):
 
     # Save CFLAGS for use in install.
     def flag_handler(self, name, flags):
-        if name == 'cflags': self.mycflags = flags
+        if name == 'cflags':
+            self.mycflags = flags
         return (flags, None, None)
 
     def install(self, spec, prefix):
         mfile = Executable('./mfile.py')
 
         # Translate CFLAGS '-O2' to mbuild syntax.
-        if '-O0' in self.mycflags: opt = '0'
-        elif '-O' in self.mycflags: opt = '1'
-        elif '-O1' in self.mycflags: opt = '1'
-        elif '-O2' in self.mycflags: opt = '2'
-        elif '-O3' in self.mycflags: opt = '3'
-        else: opt = '2'
+        if '-O0' in self.mycflags:
+            opt = '0'
+        elif '-O' in self.mycflags:
+            opt = '1'
+        elif '-O1' in self.mycflags:
+            opt = '1'
+        elif '-O2' in self.mycflags:
+            opt = '2'
+        elif '-O3' in self.mycflags:
+            opt = '3'
+        else:
+            opt = '2'
 
         args = ['-j', str(make_jobs),
                 '--cc=%s' % spack_cc,
                 '--opt=%s' % opt,
                 '--no-werror']
 
-        if '+debug' in spec: args.append('--debug')
+        if '+debug' in spec:
+            args.append('--debug')
 
         # Build and install static libxed.a.
         mfile('--clean')
