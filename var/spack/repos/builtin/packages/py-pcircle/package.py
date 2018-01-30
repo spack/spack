a1fded6fef821346a2d437bda4786ce47a94832c
Feiyi Wang <fwang2@gmail.com>
2018-01-30 14:20:45 -0500

    Add pcircle - a suite of parallel file system tools

---
+++ var/spack/repos/builtin/packages/py-pcircle/package.py
@@ -0,0 +1,45 @@
+##############################################################################
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class PyPcircle(PythonPackage):
+    """ A suite of parallel file system tools."""
+    homepage = "http://github.com/olcf/pcircle"
+    version('develop', git='http://github.com/olcf/pcircle.git',
+            branch='dev')
+
+    import_modules = ['openmpi', 'python']
+
+    phases = ['install']
+
+    depends_on('python@2.7.14:')
+    depends_on('py-mpi4py')
+    depends_on('py-scandir')
+    depends_on('py-xattr')
+    depends_on('py-numpy')
+    depends_on('py-bitarray')
+    depends_on('py-cffi', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
