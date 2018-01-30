554e8f3647954164260a454f99226caf059877a0
Mario Melara <maamelara@gmail.com>
2018-01-30 15:24:26 -0800

    Add shared variant (#31)

--- var/spack/repos/builtin/packages/libpciaccess/package.py
+++ var/spack/repos/builtin/packages/libpciaccess/package.py
@@ -31,9 +31,15 @@ class Libpciaccess(AutotoolsPackage):
     homepage = "http://cgit.freedesktop.org/xorg/lib/libpciaccess/"
     url      = "http://xorg.freedesktop.org/archive/individual/lib/libpciaccess-0.13.5.tar.gz"
 
+    variant("shared", default=True, description="Build shared libraries")
     version('0.13.5', '81468664fde96d1df2c3216fdf3c4a20')
     version('0.13.4', 'cc1fad87da60682af1d5fa43a5da45a4')
 
     depends_on('libtool', type='build')
     depends_on('pkgconfig', type='build')
     depends_on('util-macros', type='build')
+
+    def configure_args(self):
+        if "~shared" in self.spec:
+            return ["--disable-shared"]
+        return []
