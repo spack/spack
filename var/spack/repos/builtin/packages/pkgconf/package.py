986ae39852776c93f57c807413c9d3facbdcdea5
Mario <maamelara@gmail.com>
2018-02-02 12:11:43 -0800

    Add PKG_CONFIG_PATH to pkgconf
    Was breaking on Cray machines because it did not include the cray
    pkgconfig paths. This adds the same lines from pkg-config to pkgconf.

--- var/spack/repos/builtin/packages/pkgconf/package.py
+++ var/spack/repos/builtin/packages/pkgconf/package.py
@@ -40,6 +40,15 @@ class Pkgconf(AutotoolsPackage):
 
     provides('pkgconfig')
 
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        #  Is this required for other systems?
+        #  spack_env.append_path("ACLOCAL_PATH",
+                              #  join_path(self.prefix.share, "aclocal"))
+        if 'platform=cray' in self.spec:
+            spack_env.append_path('PKG_CONFIG_PATH', '/usr/lib64/pkgconfig')
+            spack_env.append_path('PKG_CONFIG_PATH',
+                                  '/usr/local/lib64/pkgconfig')
+
     @run_after('install')
     def link_pkg_config(self):
         symlink('pkgconf', '{0}/pkg-config'.format(self.prefix.bin))
