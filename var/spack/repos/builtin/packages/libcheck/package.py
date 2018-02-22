c57f6180e56643295502ee0b91465e1fcdd72855
Brian C. Van Essen <vanessen1@llnl.gov>
2018-02-22 08:47:33 -0800

    Added the libcheck package.  Fixed flake8 errors in py-pygpu recipe and removed spurious dependency on cudnn for libgpuarray.

--- var/spack/repos/builtin/packages/libcheck/package.py
+++ var/spack/repos/builtin/packages/libcheck/package.py
@@ -25,28 +25,12 @@
 from spack import *
 
 
-class PyPygpu(PythonPackage):
-    """Python packge for the libgpuarray C library."""
+class Libcheck(CMakePackage):
+    """A unit testing framework for C."""
 
-    homepage = "http://deeplearning.net/software/libgpuarray/"
-    url      = "https://github.com/Theano/libgpuarray/archive/v0.6.1.tar.gz"
+    homepage = "https://libcheck.github.io/check/index.html"
+    url      = "https://github.com/libcheck/check/releases/download/0.12.0/check-0.12.0.tar.gz"
 
-    version('0.7.5', '2534011464555c3e99d14231db965c20')
-    version('0.7.4', '19f57cd381175162048c8154f5251546')
-    version('0.7.3', 'cb44aeb8482330974abdb36b0a477e5d')
-    version('0.7.2', '0f9d7748501bc5c71bf04aae2285ac4e')
-    version('0.7.1', '7eb5bb6689ddbc386a9d498f5c0027fb')
-    version('0.7.0', 'f71b066f21ef7666f3a851e96c26f52e')
-    version('0.6.9', '7f75c39f1436c920ed9c5ffde5631fc0')
-    version('0.6.2', '7f163bd5f48f399cd6e608ee3d528ee4')
-    version('0.6.1', 'cfcd1b54447f9d55b05514df62c70ae2')
-    version('0.6.0', '98a4ec1b4c8f225f0b89c18b899a000b')
-
-    depends_on('libgpuarray')
-    # not just build-time, requires pkg_resources
-    depends_on('py-setuptools', type=('build', 'run'))
-    depends_on('py-cython@0.25:', type=('build', 'run'))
-    depends_on('py-nose', type=('build', 'run'))
-    depends_on('py-numpy', type=('build', 'run'))
-    depends_on('py-mako', type=('build', 'run'))
-    depends_on('libcheck')
+    version('0.12.0', '31b17c6075820a434119592941186f70')
+    version('0.11.0', '9b90522b31f5628c2e0f55dda348e558')
+    version('0.10.0', '53c5e5c77d090e103a17f3ed7fd7d8b8')
--- var/spack/repos/builtin/packages/libgpuarray/package.py
+++ var/spack/repos/builtin/packages/libgpuarray/package.py
@@ -45,5 +45,5 @@ class Libgpuarray(CMakePackage):
     version('0.6.0', '98a4ec1b4c8f225f0b89c18b899a000b')
 
     depends_on('cuda')
-    depends_on('cudnn')
     depends_on('cmake@3:', type='build')
+    depends_on('libcheck')
