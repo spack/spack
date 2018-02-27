4a1df072e9bc82e6ca577a418d8003d1a9d28cab
fanne-stat <ifanzhu@foxmail.com>
2018-02-27 12:22:55 -0600

    trnascan-se: new package

--- var/spack/repos/builtin/packages/trnascan-se/package.py
+++ var/spack/repos/builtin/packages/trnascan-se/package.py
@@ -1,12 +1,12 @@
 ##############################################################################
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory.
 #
 # This file is part of Spack.
 # Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
 # LLNL-CODE-647188
 #
-# For details, see https://github.com/llnl/spack
+# For details, see https://github.com/spack/spack
 # Please also see the NOTICE and LICENSE files for our notice and the LGPL.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -25,29 +25,10 @@
 from spack import *
 
 
-class TrnascanSe(MakefilePackage):
-    """A program for improved detection of transfer RNA genes in genomic
-       sequence."""
+class TrnascanSe(AutotoolsPackage):
+    """Seaching for tRNA genes in genomic sequence"""
 
     homepage = "http://lowelab.ucsc.edu/tRNAscan-SE/"
-    url      = "http://lowelab.ucsc.edu/software/tRNAscan-SE-1.3.1.tar.gz"
+    url      = "http://trna.ucsc.edu/software/trnascan-se-2.0.0.tar.gz"
 
-    version('1.3.1', '714689121a1cf11c5d9c59561769c4d7')
-
-    depends_on('perl', type=('build', 'run'))
-
-    # Had to get kind of hacky to get around the $(HOME) variable in the
-    # paths below in the Makefile
-    def edit(self, spec, prefix):
-        makefile = FileFilter('Makefile')
-        makefile.filter('PERLDIR = /usr/bin', 'PERLDIR = %s'
-                        % self.spec['perl'].prefix.bin)
-        makefile.filter('BINDIR  = ', 'BINDIR = %s \n#'
-                        % prefix.bin)
-        makefile.filter('LIBDIR  = ', 'LIBDIR = %s \n#'
-                        % prefix.lib)
-        makefile.filter('MANDIR  = ', 'MANDIR = %s \n#'
-                        % prefix.share.man)
-        
-    def setup_environment(self, spack_env, run_env):
-        run_env.prepend_path('PERL5LIB', prefix.bin)
+    version('2.0.0',    '36d2b53bcb281efbae09e0305346e544')
