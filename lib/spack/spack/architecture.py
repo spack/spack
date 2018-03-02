39491786573ea96909822eb041c60134b49dccaf
Peter Josef Scheibel <scheibel1@llnl.gov>
2018-03-02 12:32:00 -0800

    replace '-' with '_' in the version as well

--- lib/spack/spack/architecture.py
+++ lib/spack/spack/architecture.py
@@ -241,7 +241,7 @@ class OperatingSystem(object):
 
     def __init__(self, name, version):
         self.name = name.replace('-', '_')
-        self.version = version
+        self.version = version.replace('-', '_')
 
     def __str__(self):
         return "%s%s" % (self.name, self.version)
