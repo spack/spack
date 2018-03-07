ef87c2fa9aeac53046e9158823a9b0bc837d933c
Simo Tuomisto <simo.tuomisto@aalto.fi>
2018-03-07 09:46:30 +0200

    Fixed last of Flake8 errors.

--- var/spack/repos/builtin/packages/py-gpaw/package.py
+++ var/spack/repos/builtin/packages/py-gpaw/package.py
@@ -26,7 +26,7 @@ from spack import *
 
 
 class PyGpaw(PythonPackage):
-    """GPAW is a density-functional theory (DFT) Python code based on the 
+    """GPAW is a density-functional theory (DFT) Python code based on the
     projector-augmented wave (PAW) method and the atomic simulation environment
     (ASE)."""
 
@@ -40,7 +40,7 @@ class PyGpaw(PythonPackage):
             description='Build with ScaLAPACK support')
     variant('fftw', default=True, description='Build with FFTW support')
 
-    depends_on('mpi',when='+mpi', type=('build','link','run'))
+    depends_on('mpi', when='+mpi', type=('build', 'link', 'run'))
     depends_on('python@2.6:')
     depends_on('py-ase@3.13.0:', type=('build', 'run'))
     depends_on('py-numpy +blas +lapack', type=('build', 'run'))
@@ -62,10 +62,10 @@ class PyGpaw(PythonPackage):
 
         libs = blas.libs + lapack.libs + libxc.libs
         include_dirs = [
-                        blas.prefix.include,
-                        lapack.prefix.include,
-                        libxc.prefix.include
-                        ]
+            blas.prefix.include,
+            lapack.prefix.include,
+            libxc.prefix.include
+        ]
         if '+mpi' in spec:
             libs += spec['mpi'].libs
             mpi_include_dirs = repr([spec['mpi'].prefix.include])
@@ -74,21 +74,25 @@ class PyGpaw(PythonPackage):
         if '+scalapack' in spec:
             libs += spec['scalapack'].libs
             include_dirs.append(spec['scalapack'].prefix.include)
-            scalapack_macros=[('GPAW_NO_UNDERSCORE_CBLACS','1'),
-                             ('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
+            scalapack_macros = repr([
+                ('GPAW_NO_UNDERSCORE_CBLACS', '1'),
+                ('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')
+            ])
         if '+fftw' in spec:
             libs += spec['fftw'].libs
             include_dirs.append(spec['fftw'].prefix.include)
 
         lib_dirs = list(libs.directories)
         libs = list(libs.names)
-        rpath_str=':'.join(self.rpath)
+        rpath_str = ':'.join(self.rpath)
 
         with open('customize.py', 'w') as f:
             f.write("libraries = {0}\n".format(repr(libs)))
             f.write("include_dirs = {0}\n".format(repr(include_dirs)))
             f.write("library_dirs = {0}\n".format(repr(lib_dirs)))
-            f.write("extra_link_args += ['-Wl,-rpath={0}']\n".format(rpath_str))
+            f.write(
+                "extra_link_args += ['-Wl,-rpath={0}']\n".format(rpath_str)
+            )
             if '+mpi' in spec:
                 f.write("define_macros += [('PARALLEL', '1')]\n")
                 f.write("compiler='{0}'\n".format(spec['mpi'].mpicc))
@@ -100,4 +104,4 @@ class PyGpaw(PythonPackage):
                 f.write("mpicompiler = None\n")
             if '+scalapack' in spec:
                 f.write("scalapack = True\n")
-                f.write("define_macros += {0}\n".format(repr(scalapack_macros)))
+                f.write("define_macros += {0}\n".format(scalapack_macros))
