6e950e23163f7f1e771dae435664dc1e225e4870
Brian C. Van Essen <vanessen1@llnl.gov>
2018-03-09 19:14:39 -0800

    Changed all of the OpenCV variants to be off by default.  Note that I spoke with Todd about this and he was okay with it.  Minimized LBANN requirements.

--- var/spack/repos/builtin/packages/lbann/package.py
+++ var/spack/repos/builtin/packages/lbann/package.py
@@ -61,10 +61,7 @@ class Lbann(CMakePackage):
     depends_on('hwloc ~pci ~libxml2')
     # LBANN wraps OpenCV calls in OpenMP parallel loops, build without OpenMP
     depends_on('opencv@3.2.0: +core +highgui +imgproc +jpeg +png +tiff +zlib '
-               '+fast-math ~lapack ~calib3d ~dnn ~features2d ~flann ~java ~ml '
-               '~python ~stitching ~superres ~ts ~video ~videostab ~videoio '
-               '~eigen ~pthreads_pf ~opencl ~opencl_svm ~openclamdfft '
-               '~openclamdblas', when='+opencv')
+               '+fast-math +powerpc +vsx', when='+opencv')
     depends_on('protobuf@3.0.2:')
     depends_on('cnpy')
     depends_on('nccl', when='+gpu +nccl')
