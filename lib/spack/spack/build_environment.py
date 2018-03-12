58c2c082435a0c3978fe62b2922f515046dba34d
Mario Melara <maamelara@gmail.com>
2018-03-12 16:04:01 -0700

    Unload darshan module (#14)

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -344,6 +344,7 @@ def set_build_environment_variables(pkg, env, dirty):
         # unload the cray-libsci module to prevently silently linking
         # with module.
         unload_module("cray-libsci")
+        unload_module("darshan")
     return env
 
 
