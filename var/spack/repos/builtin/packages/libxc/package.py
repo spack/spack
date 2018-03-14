2807f6744c5699794212da58ff4ad551d891551d
Michael Sternberg <sternberg@anl.gov>
2018-03-14 12:37:17 -0500

    Promote to Autotools for simplicity; broaden Intel CPU opt targeting from SSE4.2 to AVX2

--- var/spack/repos/builtin/packages/libxc/package.py
+++ var/spack/repos/builtin/packages/libxc/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Libxc(Package):
+class Libxc(AutotoolsPackage):
     """Libxc is a library of exchange-correlation functionals for
     density-functional theory."""
 
@@ -35,6 +35,10 @@ class Libxc(Package):
     version('3.0.0', '8227fa3053f8fc215bd9d7b0d36de03c')
     version('2.2.2', 'd9f90a0d6e36df6c1312b6422280f2ec')
     version('2.2.1', '38dc3a067524baf4f8521d5bb1cd0b8f')
+
+    # libxc provides a testsuite, but many tests fail
+    # http://www.tddft.org/pipermail/libxc/2013-February/000032.html
+    phases = ['configure', 'build', 'install']
 
     @property
     def libs(self):
@@ -63,31 +67,38 @@ class Libxc(Package):
             libraries, root=self.prefix, shared=shared, recursive=True
         )
 
-    def install(self, spec, prefix):
-        # Optimizations for the Intel compiler, suggested by CP2K
+    def setup_environment(self, spack_env, run_env):
         optflags = '-O2'
         if self.compiler.name == 'intel':
-            optflags += ' -xAVX -axCORE-AVX2 -ipo'
+            # Optimizations for the Intel compiler, suggested by CP2K
+            #
+            # Note that not every lowly login node has advanced CPUs:
+            # 
+            #   $ icc  -xAVX -axCORE-AVX2 -ipo hello.c 
+            #   $ ./a.out 
+            #   Please verify that both the operating system and the \ 
+            #   processor support Intel(R) AVX instructions. 
+            #
+            # NB: The same flags are applied in:
+            #   - ../libint/package.py
+            #
+            # Related:
+            #   - ../fftw/package.py        variants: simd, fma
+            #   - ../c-blosc/package.py     variant:  avx2
+            #   - ../r-rcppblaze/package.py AVX* in "info" but not in code?
+            #   - ../openblas/package.py    variants: cpu_target!?!
+            #   - ../cp2k/package.py
+            #
+            # Documentation at:
+            # https://software.intel.com/en-us/cpp-compiler-18.0-developer-guide-and-reference-ax-qax
+            #
+            optflags += ' -xSSE4.2 -axAVX,CORE-AVX2 -ipo'
             if which('xiar'):
-                env['AR'] = 'xiar'
+                spack_env.set('AR', 'xiar')
 
-        if 'CFLAGS' in env and env['CFLAGS']:
-            env['CFLAGS'] += ' ' + optflags
-        else:
-            env['CFLAGS'] = optflags
+        spack_env.append_flags('CFLAGS',  optflags)
+        spack_env.append_flags('FCFLAGS', optflags)
 
-        if 'FCFLAGS' in env and env['FCFLAGS']:
-            env['FCFLAGS'] += ' ' + optflags
-        else:
-            env['FCFLAGS'] = optflags
-
-        configure('--prefix={0}'.format(prefix),
-                  '--enable-shared')
-
-        make()
-
-        # libxc provides a testsuite, but many tests fail
-        # http://www.tddft.org/pipermail/libxc/2013-February/000032.html
-        # make('check')
-
-        make('install')
+    def configure_args(self):
+        args = ['--enable-shared']
+        return args
