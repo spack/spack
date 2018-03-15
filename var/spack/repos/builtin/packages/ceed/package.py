3ebb3edef0751d68c35499bd870d01d2f9efa912
Veselin Dobrev <dobrev@llnl.gov>
2018-03-14 20:56:00 -0700

    [CEED] Add variants for OCCA and PUMI. Replace the hdf5 variant with a variant called quickbuild that disables variants in packages to speedup the build - currently hdf5, boost, and superlu-dist in PETSc.

--- var/spack/repos/builtin/packages/ceed/package.py
+++ var/spack/repos/builtin/packages/ceed/package.py
@@ -39,39 +39,73 @@ class Ceed(Package):
 
     version('1.0.0', sha1, expand=False)
 
-    variant('cuda', default=False, description='Enable CUDA dependent packages')
-    variant('mfem', default=True, description='Enable MFEM dependent packages')
-    variant('nek', default=True, description='Enable Nek dependent packages')
-    variant('petsc', default=True, description='Enable PETSc dependent packages')
-    variant('hdf5', default=False, description='Enable HDF5 dependent packages')
+    variant('cuda', default=False,
+            description='Build MAGMA; enable CUDA support in libCEED and OCCA')
+    variant('mfem', default=True, description='Build MFEM and Laghos')
+    variant('nek', default=True,
+            description='Build Nek5000, GSLIB, Nekbone, and NekCEM')
+    variant('occa', default=True,
+            description='Build OCCA; enable OCCA support in libCEED')
+    variant('petsc', default=True,
+            description='Build PETSc and HPGMG')
+    variant('pumi', default=True,
+            description='Build PUMI')
+    variant('quickbuild', default=True,
+            description='Speed-up the build by disabling variants in packages')
     # TODO: Add 'int64' variant?
 
-    depends_on('gslib@1.0.1', when='@1.0.0+nek')
-    depends_on('hpgmg@a0a5510+fe', when='@1.0.0+petsc')
-    depends_on('laghos@1.0', when='@1.0.0+mfem')
+    # LibCEED
     # FIXME: Make a v0.2 release of libceed from the current master?
-    depends_on('libceed@0.1+occa', when='@1.0.0+mfem+nek')
+    depends_on('libceed@0.1~cuda', when='@1.0.0~cuda')
+    depends_on('libceed@0.1+cuda', when='@1.0.0+cuda')
+    depends_on('libceed@0.1+occa', when='@1.0.0+occa')
+    depends_on('libceed@0.1~occa', when='@1.0.0~occa')
+    # TODO:
+    # depends_on('libceed@0.1+examples-mfem', when='@1.0.0+mfem')
+    # depends_on('libceed@0.1+examples-nek', when='@1.0.0+nek')
+    # depends_on('libceed@0.1+examples-petsc', when='@1.0.0+petsc')
+
+    # OCCA
+    # FIXME: pick a fixed occa version:
+    depends_on('occa@develop~cuda', when='@1.0.0+occa~cuda')
+    depends_on('occa@develop+cuda', when='@1.0.0+occa+cuda')
+
+    # Nek5000, GSLIB, Nekbone, and NekCEM
+    depends_on('nek5000@17.0', when='@1.0.0+nek')
+    depends_on('gslib@1.0.1', when='@1.0.0+nek')
+    depends_on('nekbone@17.0', when='@1.0.0+nek')
+    depends_on('nekcem@0b8bedd', when='@1.0.0+nek')
+
+    # PETSc, HPGMG
+    # For a +quickbuild we disable boost, hdf5, and superlu-dist in PETSc.
+    # Ideally, these can be turned into recommendations to Spack for
+    # concretizing the PETSc spec, if Spack ever supports recommendations.
+    depends_on('petsc@3.8.3~boost~hdf5~superlu-dist',
+               when='@1.0.0+petsc+quickbuild')
+    depends_on('petsc@3.8.3+mpi+double~int64', when='@1.0.0+petsc~mfem')
+    # The mfem petsc examples need the petsc variants +hypre, +suite-sparse,
+    # and +mumps:
+    depends_on('petsc@3.8.3+mpi+hypre+suite-sparse+mumps+double~int64',
+               when='@1.0.0+petsc+mfem')
+    depends_on('hpgmg@a0a5510+fe', when='@1.0.0+petsc')
+
+    # MAGMA
     depends_on('magma@2.2.0', when='@1.0.0+cuda')
+
+    # PUMI
+    depends_on('pumi@2.1.0', when='@1.0.0+pumi')
+
+    # MFEM, Laghos
+    depends_on('mfem@3.3.2+mpi+examples+miniapps', when='@1.0.0+mfem~petsc')
+    depends_on('mfem@3.3.2+mpi+petsc+examples+miniapps',
+               when='@1.0.0+mfem+petsc')
+    depends_on('laghos@1.0', when='@1.0.0+mfem')
     # The next line seems to be necessary because the concretizer somehow
     # decides that mfem requires 'hypre+internal-superlu' even though the mfem
     # package lists simply 'hypre' as dependency. This is only an issue because
     # petsc explicitly requires 'hypre~internal-superlu' which for the
     # concretizer is a conflict.
-    depends_on('hypre~internal-superlu')
-    depends_on('mfem@3.3.2+mpi+petsc+examples+miniapps', when='@1.0.0+mfem+petsc')
-    depends_on('mfem@3.3.2+mpi+examples+miniapps', when='@1.0.0+mfem~petsc')
-    depends_on('nek5000@17.0', when='@1.0.0+nek')
-    depends_on('nekbone@17.0', when='@1.0.0+nek')
-    depends_on('nekcem@0b8bedd', when='@1.0.0+nek')
-    # The mfem petsc examples need the petsc variants '+suite-sparse+mumps':
-    depends_on('petsc@3.8.3+mpi+hypre+suite-sparse+metis+hdf5+mumps~boost'
-               '+double~int64', when='@1.0.0+petsc+hdf5')
-    depends_on('petsc@3.8.3+mpi+hypre+suite-sparse+metis~hdf5+mumps~boost'
-               '+double~int64', when='@1.0.0+petsc~hdf5')
-    depends_on('pumi@2.1.0', when='@1.0.0')
-    # FIXME: pick a fixed occa version:
-    depends_on('occa@develop+cuda', when='@1.0.0+cuda')
-    depends_on('occa@develop~cuda', when='@1.0.0~cuda')
+    depends_on('hypre~internal-superlu', when='@1.0.0+mfem')
 
     # Dummy install
     def install(self, spec, prefix):
