4a89760a623b157102dda90b571434ec64410002
Denis Davydov <davydden@gmail.com>
2018-03-16 18:09:11 +0100

    minor

--- var/spack/repos/builtin/packages/cquery/package.py
+++ var/spack/repos/builtin/packages/cquery/package.py
@@ -31,13 +31,55 @@ class Cquery(WafPackage):
 
     homepage = "https://github.com/cquery-project/cquery"
     url      = "https://github.com/cquery-project/cquery/archive/v20180302.tar.gz"
+    base_url = 'https://github.com/cquery-project/cquery/archive/v{0}.tar.gz'
 
-    version('20180302', '23eabb9640cfb4de614873314aafb427')
+    releases = [
+        {
+            'version': '20180302',
+            'md5': '23eabb9640cfb4de614873314aafb427',
+            'resources': {
+                'doctest': 'b40b7e799deabac916d631d181a7f19f3060acc5',
+                'loguru': '2c35b5e7251ab5d364b1b3164eccef7b5d2293c5',
+                'msgpack-c': '208595b2620cf6260ce3d6d4cf8543f13b206449',
+                'rapidjson': 'daabb88e001f562e1f7df5f44d7fed32a0c107c2',
+                'sparsepp': '1ca7189fe81ee8c59bf08196852f70843a68a63a'
+            }
+        },
+    ]
+
+    resources = {
+        'doctest': {
+            'url': 'https://github.com/onqtam/doctest.git',
+        },
+        'loguru': {
+            'url': 'https://github.com/emilk/loguru.git',
+        },
+        'msgpack-c': {
+            'url': 'https://github.com/msgpack/msgpack-c.git',
+        },
+        'rapidjson': {
+            'url': 'https://github.com/miloyip/rapidjson.git',
+        },
+        'sparsepp': {
+            'url': 'https://github.com/greg7mdp/sparsepp.git',
+        },
+    }
+
+    for release in releases:
+        version(release['version'], release['md5'],
+                url=base_url.format(release['version']))
+
+        for name, commit in release['resources'].items():
+            resource(name=name,
+                     git=resources[name]['url'],
+                     commit=commit,
+                     placement='third_party/{0}'.format(name))
+
+    depends_on('llvm@3.3:+clang')
 
     def configure_args(self):
         return ['--llvm-config=llvm-config']
 
-    depends_on('llvm@3.3:+clang')
 
     # FIXME: Override configure_args(), build_args(),
     # or install_args() if necessary.
