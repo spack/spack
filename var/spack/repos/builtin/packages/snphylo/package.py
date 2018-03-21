55f4bbb1116cadb7ee5c05d9932d4365b8f919c4
Yifan Zhu <ifanzhu@foxmail.com>
2018-03-21 16:21:55 -0500

    snphylo: new package (#7045)
    * pushing partially done recipe for snphylo
    
    * r-snphylo: try
    
    * phylip: added version 3.697
    
    * snphylo: updated package.py
    
    * snphylo: flake8 error fixed

--- var/spack/repos/builtin/packages/snphylo/package.py
+++ var/spack/repos/builtin/packages/snphylo/package.py
@@ -23,24 +23,35 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import distutils.dir_util
 
 
-class Phylip(Package):
-    """PHYLIP (the PHYLogeny Inference Package) is a package of programs for
-       inferring phylogenies (evolutionary trees)."""
+class Snphylo(Package):
+    """A pipeline to generate a phylogenetic tree from huge SNP data"""
 
-    homepage = "http://evolution.genetics.washington.edu/phylip/"
-    url      = "http://evolution.gs.washington.edu/phylip/download/phylip-3.697.tar.gz"
+    homepage = "http://chibba.pgml.uga.edu/snphylo/"
+    url      = "http://chibba.pgml.uga.edu/snphylo/snphylo.tar.gz"
 
-    version('3.697', '0e83d17fb4d668213603c86bc21d9012')
-    version('3.696', 'dbe5abc26f6089ead3dba41c2db526ee')
+    version('2016-02-04', '467660814965bc9bed6c020c05c0d3a6')
+
+    depends_on('python', type=('build', 'run'))
+    depends_on('r', type=('build', 'run'))
+    depends_on('r-phangorn', type=('build', 'run'))
+    depends_on('r-gdsfmt', type=('build', 'run'))
+    depends_on('r-snprelate', type=('build', 'run'))
+    depends_on('r-getopt', type=('build', 'run'))
+    depends_on('muscle')
+    depends_on('phylip')
 
     def install(self, spec, prefix):
-        with working_dir('src'):
-            if self.spec.satisfies('platform=darwin'):
-                make('all', '-f', 'Makefile.osx')
-                make('put', '-f', 'Makefile.osx')
-            else:
-                make('all', '-f', 'Makefile.unx')
-                make('put', '-f', 'Makefile.unx')
-        install_tree('exe', prefix.bin)
+        install_answer = ['y', 'y', 'y', 'y']
+        install_answer_input = 'spack-config.in'
+        with open(install_answer_input, 'w') as f:
+            f.writelines(install_answer)
+        with open(install_answer_input, 'r') as f:
+            bash = which('bash')
+            bash('./setup.sh', input=f)
+            distutils.dir_util.copy_tree(".", prefix)
+
+    def setup_environment(self, spack_env, run_env):
+        run_env.prepend_path('PATH', self.spec.prefix)
