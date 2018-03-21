011d4071326c677736980b055bef0e9af761d4db
lingnanyuan <1297162327@qq.com>
2018-03-21 13:53:26 -0500

    Xorg-server: Add dependencies and fix installation. (#7220)
    * xorg-server: Add several dependencies.
    
    * libepoxy: Add dependendies mesa and meson.
    
    * mesa: Add dependencies for hwrender.
    
    * mesa: undo some changes.

--- var/spack/repos/builtin/packages/libepoxy/package.py
+++ var/spack/repos/builtin/packages/libepoxy/package.py
@@ -29,6 +29,11 @@ class Libepoxy(AutotoolsPackage):
     """Epoxy is a library for handling OpenGL function pointer management for
     you."""
     homepage = "https://github.com/anholt/libepoxy"
-    url      = "https://github.com/anholt/libepoxy/releases/download/v1.3.1/libepoxy-1.3.1.tar.bz2"
+    url      = "https://github.com/anholt/libepoxy/releases/download/1.4.3/libepoxy-1.4.3.tar.xz"
+    list_url = "https://github.com/anholt/libepoxy/releases"
 
+    version('1.4.3', 'af4c3ce0fb1143bdc4e43f85695a9bed')
     version('1.3.1', '96f6620a9b005a503e7b44b0b528287d')
+
+    depends_on('meson')
+    depends_on('mesa')
