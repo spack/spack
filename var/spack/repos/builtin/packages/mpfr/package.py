2abeacb5a36af6f6e534567e8ee320d3b13e8551
Adam J. Stewart <ajstewart426@gmail.com>
2018-03-21 12:20:12 -0500

    Add latest version of MPFR and patches for older versions

--- var/spack/repos/builtin/packages/mpfr/package.py
+++ var/spack/repos/builtin/packages/mpfr/package.py
@@ -30,18 +30,28 @@ class Mpfr(AutotoolsPackage):
        floating-point computations with correct rounding."""
 
     homepage = "http://www.mpfr.org"
-    url      = "https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.5.tar.bz2"
+    url      = "https://ftp.gnu.org/gnu/mpfr/mpfr-4.0.1.tar.bz2"
 
+    version('4.0.1', '8c21d8ac7460493b2b9f3ef3cc610454')
+    version('4.0.0', 'ef619f3bb68039e35c4a219e06be72d0')
+    version('3.1.6', '320c28198def956aeacdb240b46b8969')
     version('3.1.5', 'b1d23a55588e3b2a13e3be66bc69fd8d')
     version('3.1.4', 'b8a2f6b0e68bef46e53da2ac439e1cf4')
     version('3.1.3', '5fdfa3cfa5c86514ee4a241a1affa138')
     version('3.1.2', 'ee2c3ac63bf0c2359bf08fc3ee094c19')
 
     # mpir is a drop-in replacement for gmp
-    depends_on('gmp@4.1.0:')  # 4.2.3 or higher is recommended
+    depends_on('gmp@4.1:')  # 4.2.3 or higher is recommended
+    depends_on('gmp@5.0:', when='@4.0.0:')  # http://www.mpfr.org/mpfr-4.0.0/
 
-    patch('vasprintf.patch', when='@3.1.5')
-    patch('strtofr.patch',   when='@3.1.5')
+    # Note: patches are frequently released and updated for old releases.
+    # Check the Bugs section of old release pages for patches.
+    # http://www.mpfr.org/mpfr-X.Y.Z
+    patch('cumulative-3.1.6.patch', when='@3.1.6')
+    patch('cumulative-3.1.5.patch', when='@3.1.5')
+    patch('cumulative-3.1.4.patch', when='@3.1.4')
+    patch('cumulative-3.1.3.patch', when='@3.1.3')
+    patch('cumulative-3.1.2.patch', when='@3.1.2')
 
     def configure_args(self):
         args = [
