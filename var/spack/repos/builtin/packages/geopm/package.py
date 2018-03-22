1070a957b2d2a34361609d560da4f0c2cbf250da
Matthias Maiterth <matthias.maiterth@intel.com>
2018-03-22 15:19:52 +0100

    geopm: Updated variants for hwloc and mpi defaults

--- var/spack/repos/builtin/packages/geopm/package.py
+++ var/spack/repos/builtin/packages/geopm/package.py
@@ -80,13 +80,14 @@ class Geopm(Package):
     version('master', git='https://github.com/geopm/geopm.git', branch='master')
     version('dev', git='https://github.com/geopm/geopm.git', branch='dev')
 
-    # FIXME: Add dependencies if required.
+    # Added dependencies.
     depends_on('m4', type='build')
     depends_on('automake', type='build')
     depends_on('autoconf', type='build')
     depends_on('libtool', type='build')
     depends_on('numactl', type='build')
-    depends_on('mpi', type='build')
+    depends_on('mpi', when='~disable-mpi', type=('build', 'run'))
+    depends_on('hwloc', type=('build', 'run'))
 
     # python dependcies for running:
     depends_on('py-pandas', type='run', when='+python')
@@ -94,7 +95,18 @@ class Geopm(Package):
     depends_on('py-natsort', type='run', when='+python')
     depends_on('py-matplotlib', type='run', when='+python')
 
-    variant('debug', default=False, description='Enable debug.')
+    # Variants reflecting most ./configure --help options
+    variant('enable-debug', default=False, description='Enable debug.')
+    variant('enable-coverage', default=False, description='Build with test coverage support and enable debug.')
+    variant('disable-procfs', default=False, description='Disable use of procfs for use on operating systems.')
+    variant('disable-mpi', default=False, description='Do not build components that require MPI.')
+    variant('disable-fortran', default=False, description='Do not build fortran interface.')
+    variant('disable-doc', default=False, description='Do not create man pages with ronn.')
+    variant('enable-dependency-tracking', default=False, description='Enable depenency-tracking and do not reject slow dependency extractors. Else speeds up one-time build.')
+    variant('disable-openmp', default=False, description='Disable usage of OpenMP.')
+    variant('enable-ompt', default=False, description='Use OpenMP Tool interface.')
+    variant('hwloc', default=True, description='Build with hwloc. (By Spack, default=True)')
+    variant('gnu-ld', default=False, description='Assume C uses gnu-ld. (Default=False)')
     variant('python', default=False, description='Using this option python dependecies are build using spack. These are required for running. It is assumed they are installed on the system. Use this only if this is not the case. (Long build time).')
 
     parallel = False
@@ -105,13 +117,13 @@ class Geopm(Package):
 
         args.append('--prefix=%s' % prefix)
 
-        if '+debug' in spec:
+        if '+enable-debug' in spec:
             args.append('--enable-debug')
 
-        if '+coverage' in spec:
+        if '+enable-coverage' in spec:
             args.append('--enable-coverage')
 
-        if '+overhead' in spec:
+        if '+enable-overhead' in spec:
             args.append('--enable-overhead')
 
         if '+disable-procfs' in spec:
@@ -119,6 +131,14 @@ class Geopm(Package):
 
         if '+disable-mpi' in spec:
             args.append('--disable-mpi')
+        else:
+            args.extend([
+                '--with-mpi-bin={0}'.format(spec['mpi'].prefix.bin),
+                '--with-mpicc={0}'.format(spec['mpi'].mpicc),
+                '--with-mpicxx={0}'.format(spec['mpi'].mpicxx),
+                '--with-mpifc={0}'.format(spec['mpi'].mpifc),
+                '--with-mpif77={0}'.format(spec['mpi'].mpif77),
+            ])
 
         if '+disable-fortran' in spec:
             args.append('--disable-fortran')
@@ -126,19 +146,24 @@ class Geopm(Package):
         if '+disable-doc' in spec:
             args.append('--disable-doc')
 
-        if '+dependency-tracking' in spec:
+        if '+enable-dependency-tracking' in spec:
             args.append('--enable-dependency-tracking')
-        # else:
-        #    args.append('--disable-dependency-tracking')
+        else:
+            args.append('--disable-dependency-tracking')
 
         if '+disable-openmp' in spec:
             args.append('--disable-openmp')
 
-        if '+ompt' in spec:
+        if '+enable-ompt' in spec:
             args.append('--enable-ompt')
 
         if '+gnu-ld' in spec:
             args.append('--with-gnu-ld')
+
+        if '+hwloc' in spec:
+            args.append('--with-hwloc={0}'.format(spec['hwloc'].prefix))
+        # else:
+        #     args.append('--without-hwloc')
 
         bash = which('bash')
         bash('./autogen.sh')
