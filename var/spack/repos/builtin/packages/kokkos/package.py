d0b75951804638bccbc85f45d76a5a4b4cf742fd
Robert Pavel <rspavel@lanl.gov>
2018-04-02 13:08:54 -0600

    Explicitly commenting fix for posterity
    Explicitly commented version number of hwloc dependency for kokkos for
    posterity purposes and to pass code coverage for speedy merging

--- var/spack/repos/builtin/packages/kokkos/package.py
+++ var/spack/repos/builtin/packages/kokkos/package.py
@@ -37,6 +37,7 @@ class Kokkos(Package):
     variant('qthreads', default=False, description="enable Qthreads backend")
     variant('cuda', default=False, description="enable Cuda backend")
 
+    #Specify that v1.11.9 is required as v2.x has API changes
     depends_on('hwloc@1.11.9')
     depends_on('qthreads', when='+qthreads')
     depends_on('cuda', when='+cuda')
