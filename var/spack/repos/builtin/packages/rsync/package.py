ceab7e5f54bba4f23b80ec44d9bf80b682d320ab
peetsv <32349688+peetsv@users.noreply.github.com>
2018-04-04 16:13:50 -0700

    rsync: adding version 3.1.3 (#7669)

--- var/spack/repos/builtin/packages/rsync/package.py
+++ var/spack/repos/builtin/packages/rsync/package.py
@@ -30,5 +30,6 @@ class Rsync(AutotoolsPackage):
     homepage = "https://rsync.samba.org"
     url      = "https://download.samba.org/pub/rsync/src/rsync-3.1.2.tar.gz"
 
+    version('3.1.3', '1581a588fde9d89f6bc6201e8129afaf')
     version('3.1.2', '0f758d7e000c0f7f7d3792610fad70cb')
     version('3.1.1', '43bd6676f0b404326eee2d63be3cdcfe')
