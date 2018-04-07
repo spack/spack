bbad4ff99647e518a75424fda97774d46bbc9b58
Sinan81 <Sinan81@github>
2018-04-07 10:54:22 -0700

    get rid of make() in install phase, remove spaces

--- var/spack/repos/builtin/packages/dsrc/package.py
+++ var/spack/repos/builtin/packages/dsrc/package.py
@@ -25,7 +25,6 @@
 
 from spack import *
 
-
 class Dsrc(MakefilePackage):
     """DNA Sequence Reads Compression is an application designed for
     compression of data files containing reads from DNA sequencing in
@@ -34,12 +33,11 @@ class Dsrc(MakefilePackage):
     homepage = "http://sun.aei.polsl.pl/dsrc"
     url      = "https://github.com/refresh-bio/DSRC/archive/v2.0.2.tar.gz"
 
-    version('2.0.2',     '0a75deb6db948f9179df0756c259b870')
+    version('2.0.2', '0a75deb6db948f9179df0756c259b870')
 
     parallel = False
 
     def install(self, spec, prefix):
-        make()
         install_tree('bin', prefix.bin)
         install_tree('lib', prefix.lib)
         install_tree('include', prefix.include)
