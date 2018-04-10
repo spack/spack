34412d98bca48322a20bdd09b39e4c0e7e4583f4
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2018-04-10 15:21:49 +0200

    fix alignment

--- var/spack/repos/builtin/packages/py-flask-socketio/package.py
+++ var/spack/repos/builtin/packages/py-flask-socketio/package.py
@@ -26,13 +26,18 @@ from spack import *
 
 
 class PyFlaskSocketio(PythonPackage):
-    """Flask-SocketIO gives Flask applications access to low latency bi-directional communications between the clients and the server. The client-side application can use any of the SocketIO official clients libraries in Javascript, C++, Java and Swift, or any compatible client to establish a permanent connection to the server."""
+    """Flask-SocketIO gives Flask applications access to low latency
+    bi-directional communications between the clients and the server.
+    The client-side application can use any of the SocketIO official clients
+    libraries in Javascript, C++, Java and Swift, or any compatible client to
+    establish a permanent connection to the server.
+    """
 
     homepage = "https://flask-socketio.readthedocs.io"
     url      = "https://pypi.io/packages/source/F/Flask-SocketIO/Flask-SocketIO-2.9.6.tar.gz"
 
     version('2.9.6', 'bca83faf38355bd91911f2f140f9b50f')
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-flask@0.9:',        type=('build', 'run'))
-    depends_on('py-python-socketio@1.6.1:',        type=('build', 'run'))
+    depends_on('py-setuptools',             type='build')
+    depends_on('py-flask@0.9:',             type=('build', 'run'))
+    depends_on('py-python-socketio@1.6.1:', type=('build', 'run'))
