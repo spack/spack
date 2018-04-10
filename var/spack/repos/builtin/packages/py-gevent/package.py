4f07f3eb72dd7d60878816f4d1fb15244e117f84
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2018-04-10 15:19:20 +0200

    fix alignment

--- var/spack/repos/builtin/packages/py-gevent/package.py
+++ var/spack/repos/builtin/packages/py-gevent/package.py
@@ -33,7 +33,7 @@ class PyGevent(PythonPackage):
 
     version('1.3a2', '8d73a7b0ceb0ca791b22e6f7b7061e9e')
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-cython@0.27:', type='build')
-    depends_on('py-greenlet@0.4.13:',        type=('build', 'run'))
-    depends_on('python@2.7:2.8,3.4:',        type=('build', 'run'))
+    depends_on('py-setuptools',         type='build')
+    depends_on('py-cython@0.27:',       type='build')
+    depends_on('py-greenlet@0.4.13:',   type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.4:',   type=('build', 'run'))
