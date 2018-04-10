d65a7dc83763e4ef5b7059f5a2bbeaa87a41a4ce
Daniel Bremer <5bremer@informatik.uni-hamburg.de>
2018-04-10 15:36:17 +0200

    Small fix

--- var/spack/repos/builtin/packages/c-lime/package.py
+++ var/spack/repos/builtin/packages/c-lime/package.py
@@ -1,3 +1,4 @@
+##############################################################################
 # Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory.
 #
