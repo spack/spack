48667500696ba43b79d90e10c02d611559d62ba1
Sinan81 <Sinan81@github>
2018-04-10 21:56:07 -0700

    new package: py-unicycler (bioinformatics)

---
+++ var/spack/repos/builtin/packages/py-unicycler/package.py
@@ -0,0 +1,63 @@
+##############################################################################
+# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install py-unicycler
+#
+# You can edit this file again by typing:
+#
+#     spack edit py-unicycler
+#
+# See the Spack documentation for more information on packaging.
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+from spack import *
+
+
+class PyUnicycler(PythonPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "http://www.example.com"
+    url      = "https://github.com/rrwick/Unicycler/archive/v0.4.5.tar.gz"
+
+    version('0.4.5', 'f7b4f6b712fee6a4fa86a046a6781768')
+
+    depends_on('python@3.4:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('spades')
+    depends_on('pilon')
+    depends_on('jdk')
+    depends_on('bowtie2')
+    depends_on('samtools@1.0:')
+    depends_on('racon')
+    depends_on('blast-plus')
+
+    conflicts('%gcc@:4.9.0')
+    conflicts('%clang@:3.4.2')
