f2af58bc5dd217eb991c0c43f4bcab5f0fa52d47
Raghu Raja <craghun@amazon.com>
2018-04-11 15:46:06 +0000

    libfabric: Updating to the latest stable release v1.6.0
    Signed-off-by: Raghu Raja <craghun@amazon.com>

--- var/spack/repos/builtin/packages/libfabric/package.py
+++ var/spack/repos/builtin/packages/libfabric/package.py
@@ -30,8 +30,9 @@ class Libfabric(AutotoolsPackage):
        fabric communication services to applications."""
 
     homepage = "https://libfabric.org/"
-    url      = "https://github.com/ofiwg/libfabric/releases/download/v1.5.0/libfabric-1.5.0.tar.gz"
+    url      = "https://github.com/ofiwg/libfabric/releases/download/v1.6.0/libfabric-1.6.0.tar.gz"
 
+    version('1.6.0', '91d63ab3c0b9724a4db660019f928cab')
     version('1.5.0', 'fda3e9b31ebe184f5157288d059672d6')
     version('1.4.2', '2009c8e0817060fb99606ddbf6c5ccf8')
 
