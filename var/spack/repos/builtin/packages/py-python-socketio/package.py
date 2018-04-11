0100d50fb93b4cbbbc3f18fbfb4b11438aec9d66
healther <healther@users.noreply.github.com>
2018-04-11 19:34:41 +0200

    Update package.py

--- var/spack/repos/builtin/packages/py-python-socketio/package.py
+++ var/spack/repos/builtin/packages/py-python-socketio/package.py
@@ -39,5 +39,5 @@ class PyPythonSocketio(PythonPackage):
 
     depends_on('py-setuptools',                 type='build')
     depends_on('py-six@1.9.0:',                 type=("build", "run"))
-    depends_on('py-python-engineio@:1.2.1:',    type=("build", "run"))
+    depends_on('py-python-engineio@1.2.1:',    type=("build", "run"))
     depends_on('py-eventlet', when='+eventlet', type=("build", "run"))
