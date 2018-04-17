a07ae67f4b77e2cb2013229eb73ec9a5c2d11728
Eric Martin <ericmartin@wustl.edu>
2018-04-17 12:46:20 -0500

    perl-statistics-pca: fix dependency types

--- var/spack/repos/builtin/packages/perl-statistics-pca/package.py
+++ var/spack/repos/builtin/packages/perl-statistics-pca/package.py
@@ -34,6 +34,6 @@ class PerlStatisticsPca(PerlPackage):
     version('0.0.1', '6e0e05fe13f6becea525b973a0c29001')
 
     depends_on('perl-module-build', type='build')
-    depends_on('perl-contextual-return')
-    depends_on('perl-text-simpletable')
-    depends_on('perl-math-matrixreal')
+    depends_on('perl-contextual-return', type=('build', 'run'))
+    depends_on('perl-text-simpletable', type=('build', 'run'))
+    depends_on('perl-math-matrixreal', type=('build', 'run'))
