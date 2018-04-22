ab6f391eeefcdbe7faf876cf6e2afe843f3fec0b
Sinan81 <Sinan81@github>
2018-04-21 23:05:06 -0700

    add cmake_arg method

--- var/spack/repos/builtin/packages/racon/package.py
+++ var/spack/repos/builtin/packages/racon/package.py
@@ -33,6 +33,7 @@ class Racon(CMakePackage):
     homepage = "https://github.com/isovic/racon"
     url      = "https://github.com/isovic/racon/releases/download/1.2.1/racon-v1.2.1.tar.gz"
 
+    version('1.3.0', 'e00d61f391bce2af20ebd2a3aee1e05a')
     version('1.2.1', '7bf273b965a5bd0f41342a9ffe5c7639')
 
     depends_on('cmake@3.2:', type='build')
@@ -41,8 +42,10 @@ class Racon(CMakePackage):
     conflicts('%gcc@:4.7')
     conflicts('%clang@:3.1')
 
+    def cmake_args(self):
+        args = ['-Dracon_build_wrapper=ON']
+        return args
+
     def install(self, spec, prefix):
-        cmake('-Dracon_build_wrapper=ON')
         install_tree('spack-build/bin', prefix.bin)
         install_tree('spack-build/lib', prefix.lib)
-        install('bin/racon_wrapper', prefix.bin)
