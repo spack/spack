0060ae3e1ef1c98daf3b1ab79206a0efde598fa4
Emre Ates <ates@bu.edu>
2018-04-24 15:00:57 -0400

    Lint fixes

--- lib/spack/spack/util/module_cmd.py
+++ lib/spack/spack/util/module_cmd.py
@@ -80,13 +80,15 @@ def get_module_cmd_from_bash(bashopts=''):
 
     # Find the portion of the module function that is evaluated
     try:
-        find_exec = re.search(r'.*`(.*(:? bash | sh | zsh ).*)`.*', module_func)
+        find_exec = re.search(r'.*`(.*(:? bash | sh | zsh ).*)`.*',
+                              module_func)
         exec_line = find_exec.group(1)
     except BaseException:
         try:
             # This will fail with nested parentheses. TODO: expand regex.
-            find_exec = re.search(r'.*\(([^()]*(:? bash | sh | zsh )[^()]*)\).*',
-                                  module_func)
+            find_exec = re.search(
+                r'.*\(([^()]*(:? bash | sh | zsh )[^()]*)\).*',
+                module_func)
             exec_line = find_exec.group(1)
         except BaseException:
             raise ModuleError('get_module_cmd cannot '
