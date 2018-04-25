097ba97e10620f548d41de345e3534ed8e64e846
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2018-04-24 23:05:07 -0400

    Re-did reading/writing of Spack Environments

--- lib/spack/spack/cmd/env.py
+++ lib/spack/spack/cmd/env.py
@@ -22,6 +22,7 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+import stat
 import llnl.util.tty as tty
 import spack
 import llnl.util.filesystem as fs
@@ -53,6 +54,22 @@ level = "long"
 
 _db_dirname = fs.join_path(spack.var_path, 'environments')
 
+def get_env_root(name):
+    """Given an environment name, determines its root directory"""
+    return fs.join_path(_db_dirname, name)
+
+def get_dotenv_dir(env_root):
+    """@return Directory in an environment that is owned by Spack"""
+    return fs.join_path(env_root, '.env')
+
+def get_write_paths(env_root):
+    """Determines the names of temporary and permanent directories to
+    write machine-generated environment info."""
+    tmp_new = fs.join_path(env_root, '.env.new')
+    dest = get_dotenv_dir(env_root)
+    tmp_old = fs.join_path(env_root, '.env.old')
+    return tmp_new, dest, tmp_old
+
 
 class Environment(object):
     def __init__(self, name):
@@ -67,7 +84,12 @@ class Environment(object):
         # any root of run deps
         self.common_bins = dict()  # name -> hash
 
+    @property
+    def path(self):
+        return get_env_root(self.name)
+
     def add(self, user_spec):
+        """Add a single user_spec (non-concretized) to the Environment"""
         query_spec = Spec(user_spec)
         existing = set(x for x in self.user_specs
                        if Spec(x).name == query_spec.name)
@@ -77,6 +99,7 @@ class Environment(object):
         self.user_specs.append(user_spec)
 
     def remove(self, query_spec):
+        """Remove specs from an environment that match a query_spec"""
         query_spec = Spec(query_spec)
         match_index = -1
         for i, spec in enumerate(self.user_specs):
@@ -94,7 +117,16 @@ class Environment(object):
             del self.specs_by_hash[spec_hash]
 
     def concretize(self, force=False):
+        """Concretize user_specs in an Environment, creating (fully
+        concretized) specs.
+
+        force: bool
+           If set, re-concretize ALL specs, even those that were
+           already concretized.
+        """
+
         if force:
+            # Clear previously concretized specs
             self.specs_by_hash = dict()
             self.concretized_order = list()
 
@@ -102,12 +134,15 @@ class Environment(object):
         new_specs = list()
         for user_spec in self.user_specs[num_concretized:]:
             tty.msg('Concretizing %s' % user_spec)
+
             spec = Spec(user_spec)
             spec.concretize()
             new_specs.append(spec)
             dag_hash = spec.dag_hash()
             self.specs_by_hash[dag_hash] = spec
             self.concretized_order.append(spec.dag_hash())
+
+            # Display concretized spec to the user
             sys.stdout.write(spec.tree(
                 recurse_dependencies=True, install_status=True,
                 hashlen=7, hashes=True))
@@ -115,6 +150,17 @@ class Environment(object):
         return new_specs
 
     def install(self, args):
+        """Do a `spack install` on all the (concretized)
+        specs in an Environment."""
+
+        # Make sure log directory exists
+        logs = fs.join_path(self.path, 'logs')
+        try: 
+            os.makedirs(logs)
+        except OSError:
+            if not os.path.isdir(logs):
+                raise
+
         for concretized_hash in self.concretized_order:
             spec = self.specs_by_hash[concretized_hash]
 
@@ -122,19 +168,23 @@ class Environment(object):
             # that will be passed to Package.do_install API
             kwargs = dict()
             spack.cmd.install.update_kwargs_from_args(args, kwargs)
-            with pushd(self, args.output):
+            with pushd(self.path):
                 spec.package.do_install(**kwargs)
 
+                # Link the resulting log file into logs dir
+                logname = '%s-%s.log' % (spec.name, spec.dag_hash(7))
+                os.symlink(
+                    spec.package.build_log_path,
+                    fs.join_path(logs, logname))
+
     def uninstall(self, args):
-        environment = read(args.environment)
-
-        specs = environment._get_environment_specs(
-            recurse_dependencies=True)
-
+        """Uninstall all the specs in an Environment."""
+        specs = self._get_environment_specs(recurse_dependencies=True)
         args.all = False
         spack.cmd.uninstall.uninstall_specs(args, specs)
 
     def list(self, stream, **kwargs):
+        """List the specs in an environment."""
         for user_spec, concretized_hash in zip_longest(
                 self.user_specs, self.concretized_order):
 
@@ -195,7 +245,8 @@ class Environment(object):
 
     def _get_environment_specs(self, recurse_dependencies):
         """Returns the specs of all the packages in an environment.
-        At most one instance of any package gets added to the environment"""
+        If these specs appear under different user_specs, only one copy
+        is added to the list returned."""
         package_to_spec = {}
         spec_list = list()
 
@@ -216,6 +267,7 @@ class Environment(object):
         return spec_list
 
     def to_dict(self):
+        """Used in serializing to JSON"""
         concretized_order = list(self.concretized_order)
         concrete_specs = dict()
         for spec in self.specs_by_hash.values():
@@ -232,36 +284,29 @@ class Environment(object):
 
     @staticmethod
     def from_dict(name, d):
-        c = Environment(name)
-        c.user_specs = list(d['user_specs'])
-        c.concretized_order = list(d['concretized_order'])
+        """Used in deserializing from JSON"""
+        env = Environment(name)
+        env.user_specs = list(d['user_specs'])
+        env.concretized_order = list(d['concretized_order'])
         specs_dict = d['concrete_specs']
-        c.specs_by_hash = reconstitute(specs_dict, set(c.concretized_order))
-        return c
 
-    def path(self):
-        return fs.join_path(_db_dirname, self.name)
+        hash_to_node_dict = specs_dict
+        root_hashes = set(env.concretized_order)
 
-    def config_path(self):
-        return fs.join_path(self.path(), 'config')
+        specs_by_hash = {}
+        for dag_hash, node_dict in hash_to_node_dict.items():
+            specs_by_hash[dag_hash] = Spec.from_node_dict(node_dict)
 
-    def repo_path(self):
-        return fs.join_path(self.path(), 'repo')
+        for dag_hash, node_dict in hash_to_node_dict.items():
+            for dep_name, dep_hash, deptypes in (
+                    Spec.dependencies_from_node_dict(node_dict)):
+                specs_by_hash[dag_hash]._add_dependency(
+                    specs_by_hash[dep_hash], deptypes)
 
+        env.specs_by_hash = dict(
+            (x, y) for x, y in specs_by_hash.items() if x in root_hashes)
 
-def reconstitute(hash_to_node_dict, root_hashes):
-    specs_by_hash = {}
-    for dag_hash, node_dict in hash_to_node_dict.items():
-        specs_by_hash[dag_hash] = Spec.from_node_dict(node_dict)
-
-    for dag_hash, node_dict in hash_to_node_dict.items():
-        for dep_name, dep_hash, deptypes in (
-                Spec.dependencies_from_node_dict(node_dict)):
-            specs_by_hash[dag_hash]._add_dependency(
-                specs_by_hash[dep_hash], deptypes)
-
-    return dict((x, y) for x, y in specs_by_hash.items() if x in root_hashes)
-
+        return env
 
 def reset_os_and_compiler(spec, compiler=None):
     spec = spec.copy()
@@ -287,38 +332,24 @@ def upgrade_dependency_version(spec, dep_name):
     return spec
 
 
-def write(environment, new_repo=None, config_files=None):
-    """
-    config_files will overwrite any existing config files in the environment.
-    """
-    tmp_new, dest, tmp_old = write_paths(environment)
-
+def check_consistent_env(env_root):
+    tmp_new,dest,tmp_old = get_write_paths(env_root)
     if os.path.exists(tmp_new) or os.path.exists(tmp_old):
         tty.die("Partial write state, run 'spack env repair'")
 
+def write(environment):
+    """Writes an in-memory environment back to its location on disk,
+    in an atomic manner."""
+
+    tmp_new, dest, tmp_old = get_write_paths(get_env_root(environment.name))
+
+    # Write the machine-generated stuff
     fs.mkdirp(tmp_new)
     # create one file for the environment object
     with open(fs.join_path(tmp_new, 'environment.json'), 'w') as F:
         sjson.dump(environment.to_dict(), stream=F)
 
-    dest_repo_dir = fs.join_path(tmp_new, 'repo')
-    if new_repo:
-        shutil.copytree(new_repo.root, dest_repo_dir)
-    elif os.path.exists(environment.repo_path()):
-        shutil.copytree(environment.repo_path(), dest_repo_dir)
-
-    new_config_dir = fs.join_path(tmp_new, 'config')
-    if os.path.exists(environment.config_path()):
-        shutil.copytree(environment.config_path(), new_config_dir)
-    else:
-        fs.mkdirp(new_config_dir)
-
-    if config_files:
-        for cfg_path in config_files:
-            dst_fname = os.path.basename(cfg_path)
-            dst = fs.join_path(new_config_dir, dst_fname)
-            shutil.copyfile(cfg_path, dst)
-
+    # Swap in new directory atomically
     if os.path.exists(dest):
         shutil.move(dest, tmp_old)
     shutil.move(tmp_new, dest)
@@ -326,22 +357,14 @@ def write(environment, new_repo=None, config_files=None):
         shutil.rmtree(tmp_old)
 
 
-def write_paths(environment):
-    tmp_new = fs.join_path(_db_dirname, "_" + environment.name)
-    dest = environment.path()
-    tmp_old = fs.join_path(_db_dirname, "." + environment.name)
-    return tmp_new, dest, tmp_old
-
-
 def repair(environment_name):
-    """
+    """Recovers from crash during critical section of write().
     Possibilities:
         tmp_new, dest
         tmp_new, tmp_old
         tmp_old, dest
     """
-    environment = Environment(environment_name)
-    tmp_new, dest, tmp_old = write_paths(environment)
+    tmp_new, dest, tmp_old = get_write_paths(get_env_root(environment_name))
     if os.path.exists(tmp_old):
         if not os.path.exists(dest):
             shutil.move(tmp_new, dest)
@@ -358,13 +381,11 @@ def repair(environment_name):
 
 
 def read(environment_name):
-    tmp_new, environment_dir, tmp_old = write_paths(
-        Environment(environment_name))
+    # Check that env is in a consistent state on disk
+    env_root = get_env_root(environment_name)
 
-    if os.path.exists(tmp_new) or os.path.exists(tmp_old):
-        tty.die("Partial write state, run 'spack env repair'")
-
-    with open(fs.join_path(environment_dir, 'environment.json'), 'r') as F:
+    dotenv_dir = get_dotenv_dir(env_root)
+    with open(fs.join_path(dotenv_dir, 'environment.json'), 'r') as F:
         environment_dict = sjson.load(F)
     environment = Environment.from_dict(environment_name, environment_dict)
 
@@ -374,56 +395,19 @@ def read(environment_name):
 # =============== Modifies Environment
 
 def environment_create(args):
-    environment = Environment(args.environment)
-    if os.path.exists(environment.path()):
+    if os.path.exists(get_env_root(args.environment)):
         raise tty.die("Environment already exists: " + args.environment)
 
-    init_config = None
-    if args.init_file:
-        with open(args.init_file) as F:
-            init_config = syaml.load(F)
-
-    _environment_create(args.environment, init_config)
+    _environment_create(args.environment)
 
 
-def _environment_create(name, init_config=None):
+def _environment_create(name):
     environment = Environment(name)
-
-    config_paths = None
-    if init_config:
-        user_specs = list()
-        config_sections = {}
-        for key, val in init_config.items():
-            if key == 'user_specs':
-                user_specs.extend(val)
-            else:
-                config_sections[key] = val
-
-        for user_spec in user_specs:
-            environment.add(user_spec)
-        if config_sections:
-            import tempfile
-            tmp_cfg_dir = tempfile.mkdtemp()
-            config_paths = []
-        for key, val in config_sections.items():
-            yaml_section = syaml.dump({key: val}, default_flow_style=False)
-            yaml_file = '{0}.yaml'.format(key)
-            yaml_path = fs.join_path(tmp_cfg_dir, yaml_file)
-            config_paths.append(yaml_path)
-            with open(yaml_path, 'w') as F:
-                F.write(yaml_section)
-
-    write(environment, config_files=config_paths)
-    if config_paths:
-        shutil.rmtree(tmp_cfg_dir)
-
-
-def environment_update_config(args):
-    environment = Environment(args.environment)
-    write(environment, config_files=args.config_files)
+    write(environment)
 
 
 def environment_add(args):
+    check_consistent_env(get_env_root(args.environment))
     environment = read(args.environment)
     for spec in spack.cmd.parse_specs(args.package):
         environment.add(spec.format())
@@ -431,6 +415,7 @@ def environment_add(args):
 
 
 def environment_remove(args):
+    check_consistent_env(get_env_root(args.environment))
     environment = read(args.environment)
     for spec in spack.cmd.parse_specs(args.package):
         environment.remove(spec.format())
@@ -438,33 +423,43 @@ def environment_remove(args):
 
 
 def environment_concretize(args):
+    check_consistent_env(get_env_root(args.environment))
     environment = read(args.environment)
     _environment_concretize(environment, force=args.force)
 
 
 def _environment_concretize(environment, force=False):
     """Function body separated out to aid in testing."""
+
+    # Change global search paths
     repo = prepare_repository(environment)
     prepare_config_scope(environment)
 
-    # new_specs =
     environment.concretize(force=force)
+
     # Temporarily disable.
     # See https://github.com/spack/spack/issues/7878
     # for spec in new_specs:
     #    for dep in spec.traverse():
+    #        # Store in <env>/.env.new
     #        dump_to_environment_repo(dep, repo)
-    write(environment, repo)
+    # NOTE: This section of code should write the repo
+    #       already to <env>/.env.new
+
+    # Moves <env>/.env.new to <env>/.env
+    write(environment)
 
 # =============== Does not Modify Environment
 
 def environment_install(args):
+    check_consistent_env(get_env_root(args.environment))
     environment = read(args.environment)
     prepare_repository(environment)
     environment.install(args)
 
 
 def environment_uninstall(args):
+    check_consistent_env(get_env_root(args.environment))
     environment = read(args.environment)
     prepare_repository(environment)
     environment.uninstall(args)
@@ -478,12 +473,12 @@ def dump_to_environment_repo(spec, repo):
 
 
 def prepare_repository(environment, remove=None):
+    """Adds environment's repository to the global search path of repos"""
     # Temporarily disable.
     # See https://github.com/spack/spack/issues/7878
     return
     import tempfile
-    repo_stage = tempfile.mkdtemp()
-    new_repo_dir = fs.join_path(repo_stage, 'repo')
+    new_repo_dir = get_dotenv_dir(env_root(environment.name))
     if os.path.exists(environment.repo_path()):
         shutil.copytree(environment.repo_path(), new_repo_dir)
     else:
@@ -501,10 +496,12 @@ def prepare_repository(environment, remove=None):
 
 
 def prepare_config_scope(environment):
-    tty.debug("Check for config scope at " + environment.config_path())
-    if os.path.exists(environment.config_path()):
-        tty.msg("Using config scope at " + environment.config_path())
-        ConfigScope(environment.name, environment.config_path())
+    """Adds environment's scope to the global search path of configuration scopes"""
+    dotenv = get_dotenv_dir(environment.path)
+    tty.debug("Check for config scope at " + dotenv)
+    if os.path.exists(dotenv):
+        tty.msg("Using config scope at " + dotenv)
+        ConfigScope(environment.name, dotenv)
 
 
 def environment_relocate(args):
@@ -535,29 +532,24 @@ def environment_stage(args):
 
 def environment_location(args):
     environment = read(args.environment)
-    print(environment.path())
+    print(environment.path)
 
 
 @contextmanager
-def redirect_stdout(environment, ofname, defname):
+def redirect_stdout(ofname):
     """Redirects STDOUT to (by default) a file within the environment;
     or else a user-specified filename."""
-    if ofname == '-':
+    with open(ofname, 'w') as f:
+        original = sys.stdout
+        sys.stdout = f
         yield
-    else:
-        path = os.path.join(environment.path(), defname) \
-            if ofname is None else ofname
-        with open(path, 'w') as f:
-            original = sys.stdout
-            sys.stdout = f
-            yield
-            sys.stdout = original
+        sys.stdout = original
 
 
 @contextmanager
-def pushd(environment, odname):
+def pushd(dir):
     original = os.getcwd()
-    os.chdir(environment.path() if odname is None else odname)
+    os.chdir(dir)
     yield
     os.chdir(original)
 
@@ -574,11 +566,14 @@ def environment_loads(args):
     environment = read(args.environment)
     recurse_dependencies = args.recurse_dependencies
     args.recurse_dependencies = False
-    with redirect_stdout(environment, args.output, 'loads.sh'):
+    ofname = fs.join_path(environment.path, 'loads')
+    with redirect_stdout(ofname):
         specs = environment._get_environment_specs(
             recurse_dependencies=recurse_dependencies)
         spack.cmd.module.loads(module_types, specs, args)
 
+    print('To load this environment, type:')
+    print('   source %s' % ofname)
 
 def environment_upgrade_dependency(args):
     environment = read(args.environment)
@@ -643,10 +638,6 @@ def setup_parser(subparser):
         help='List modules for an installed environment '
         '(see spack module loads)')
     spack.cmd.module.add_loads_arguments(loads_parser)
-    loads_parser.add_argument(
-        '-o', '--output', dest='output', default=None,
-        help="Output file (or - for stdout); "
-        "otherwise places in <env-location>/loads.sh")
 
     loads_parser = sp.add_parser(
         'location',
@@ -680,10 +671,6 @@ version''')
         'install',
         help='Install all concretized specs in an environment')
     spack.cmd.install.add_common_arguments(install_parser)
-    install_parser.add_argument(
-        '-o', '--output', dest='output', default=None,
-        help="Write install logs and setup files here "
-        "(defaults to environment path)")
 
     uninstall_parser = sp.add_parser(
         'uninstall',
@@ -704,7 +691,6 @@ def env(parser, args, **kwargs):
         'upgrade': environment_upgrade_dependency,
         'stage': environment_stage,
         'install': environment_install,
-        'uninstall': environment_uninstall,
-        'update-config': environment_update_config,
+        'uninstall': environment_uninstall
     }
     action[args.environment_command](args)
