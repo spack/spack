52d82216ff34211a401c56394af9494ecac0a0b3
Geoffrey M. Oxberry <goxberry@gmail.com>
2018-05-01 14:21:58 -0700

    zlib: add version 1.2.7 for RHEL 7

--- var/spack/repos/builtin/packages/zlib/package.py
+++ var/spack/repos/builtin/packages/zlib/package.py
@@ -39,6 +39,7 @@ class Zlib(Package):
     # Due to the bug fixes, any installations of 1.2.9 or 1.2.10 should be
     # immediately replaced with 1.2.11.
     version('1.2.8', '44d667c142d7cda120332623eab69f40')
+    version('1.2.7', 'c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1')
     version('1.2.3', 'debc62758716a169df9f62e6ab2bc634')
 
     variant('pic', default=True,
