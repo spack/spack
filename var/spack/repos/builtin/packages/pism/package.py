1dac9d13f7ff72f49161167f67acd070ef42ebf8
Elizabeth Fischer <rpf2116@columbia.edu>
2018-05-03 22:39:13 -0400

    Fixed missing trailing quote

--- var/spack/repos/builtin/packages/pism/package.py
+++ var/spack/repos/builtin/packages/pism/package.py
@@ -29,7 +29,7 @@ class Pism(CMakePackage):
     """Parallel Ice Sheet Model"""
 
     homepage = "http://pism-docs.org/wiki/doku.php:="
-    url      = "https://github.com/pism/pism/archive/v0.7.3.tar.gz
+    url      = "https://github.com/pism/pism/archive/v0.7.3.tar.gz"
 
     maintainers = ['citibeth']
 
