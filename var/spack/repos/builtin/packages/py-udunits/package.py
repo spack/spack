96b7870d8bacdcd85827f5692dc2a1af710e8532
Elizabeth Fischer <rpf2116@columbia.edu>
2018-05-03 22:33:30 -0400

    Remove unnecssary import?

--- var/spack/repos/builtin/packages/py-udunits/package.py
+++ var/spack/repos/builtin/packages/py-udunits/package.py
@@ -23,8 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import llnl.util.filesystem
-
 import os
 
 
