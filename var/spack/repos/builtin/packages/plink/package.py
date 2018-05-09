04b1703c90ad0440cdb46a651a36c9fc75779d79
fanne-stat <ifanzhu@foxmail.com>
2018-05-09 15:30:24 -0500

    plink: new source version 1.9-beta5

--- var/spack/repos/builtin/packages/plink/package.py
+++ var/spack/repos/builtin/packages/plink/package.py
@@ -32,10 +32,21 @@ class Plink(Package):
 
     homepage = "https://www.cog-genomics.org/plink/1.9/"
 
+    version('1.9-beta5', '737545504ae19348a44a05fa69b75c28',
+            url='https://github.com/chrchang/plink-ng/archive/b15c19f.tar.gz')
     version('1.9', 'a2325881594856c0f1b7523290d1e04f',
             url='https://www.cog-genomics.org/static/bin/plink170815/plink_linux_x86_64.zip')
 
+    depends_on('atlas', when='@1.9-beta5')
+    depends_on('netlib-lapack', when='@1.9-beta5')
+
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        install('plink', prefix.bin)
-        install('prettify', prefix.bin)
+        if spec.version == Version('1.9'):
+            install('plink', prefix.bin)
+            install('prettify', prefix.bin)
+        if spec.version == Version('1.9-beta5'):
+            with working_dir('1.9'):
+                first_compile = Executable('./plink_first_compile')
+                first_compile()
+                install('plink', prefix.bin)
