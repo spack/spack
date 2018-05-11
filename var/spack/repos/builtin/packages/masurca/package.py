114ed7b9513a626b8dc30d2917a3fb056edf715f
Levi Baber <baber@iastate.edu>
2018-05-11 12:42:35 -0500

    masurca: new version

--- var/spack/repos/builtin/packages/masurca/package.py
+++ var/spack/repos/builtin/packages/masurca/package.py
@@ -35,6 +35,7 @@ class Masurca(Package):
     url      = "ftp://ftp.genome.umd.edu/pub/MaSuRCA/latest/MaSuRCA-3.2.3.tar.gz"
 
     version('3.2.3', 'd9b4419adfe6b64e42ce986253a50ff5')
+    version('3.2.6', 'f068f91e33fd7381de406a7a954bfe01')
 
     depends_on('perl', type=('build', 'run'))
     depends_on('boost')
