b3c5c4dd2c229284507d38bd0d586053e93f90e7
Sajid Ali <30510036+s-sajid-ali@users.noreply.github.com>
2018-05-13 22:05:29 -0500

    as requested

--- var/spack/repos/builtin/packages/py-numexpr3/package.py
+++ var/spack/repos/builtin/packages/py-numexpr3/package.py
@@ -37,12 +37,12 @@ class PyNumexpr3(PythonPackage):
     SSE2 or AVX2, if your processor supports them. Use of a newer version of
     gcc such as 5.4 is strongly recommended."""
     homepage = "https://github.com/pydata/numexpr/tree/numexpr-3.0"
-    url = "https://files.pythonhosted.org/packages/f2/fe/52b6aca524622e57fb1fa9681c6e12bf3dd7df42c93edc2900ae844d42ce/numexpr3-3.0.1a1.tar.gz"
+    url = "https://pypi.io/packages/source/n/numexpr3/numexpr3-3.0.1a1.tar.gz"
 
     version('3.0.1.a1', '9fa8dc59b149aa1956fc755f982a78ad')
     # TODO: Add CMake build system for better control of passing flags related
     # to CPU ISA.
 
-    depends_on('python@2.6:')
+    depends_on('python@2.6:2.8,3.3:', type=('build', 'run'))
     depends_on('py-numpy@1.7:', type=('build', 'run'))
     depends_on('py-setuptools@18.2:', type='build')
