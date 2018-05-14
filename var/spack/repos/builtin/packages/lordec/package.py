8b2efd6c337b4195c4f1ff2674cfe9f052aa8dc4
lingnanyuan <1297162327@qq.com>
2018-05-14 13:21:18 -0500

    lordec: Add homepage, change to MakefilePackage.

--- var/spack/repos/builtin/packages/lordec/package.py
+++ var/spack/repos/builtin/packages/lordec/package.py
@@ -25,12 +25,12 @@
 from spack import *
 
 
-class Lordec(Package):
+class Lordec(MakefilePackage):
     """LoRDEC is a program to correct sequencing errors in long reads from
     3rd generation sequencing with high error rate, and is especially
     intended for PacBio reads."""
 
-    homepage = "http://www.example.com"
+    homepage = "http://www.atgc-montpellier.fr/lordec/"
     url      = "https://gite.lirmm.fr/lordec/lordec-releases/uploads/e3116a5f251e46e47f7a3b7ddb2bd7f6/lordec-src_0.8.tar.gz"
 
     version('0.8', 'd5a57db681d1cd31c516aa780ce53f9d')
@@ -38,6 +38,7 @@ class Lordec(Package):
     depends_on('boost')
     depends_on('cmake@3.1.0:')
 
+    build_targets = ['clean', 'all']
+
     def install(self, spec, prefix):
-        make('clean', 'all', '-j8')
         install_tree('.', prefix.bin)
