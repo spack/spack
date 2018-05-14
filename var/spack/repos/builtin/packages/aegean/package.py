b5d578a5e183f88ba48b73d2271c95ea49a6567a
Levi Baber <baberlevi@gmail.com>
2018-05-14 11:10:19 -0500

    Aegean: new package (#8113)
    * aegean: new package
    
    * genometools: set dependent env for aegean

--- var/spack/repos/builtin/packages/aegean/package.py
+++ var/spack/repos/builtin/packages/aegean/package.py
@@ -21,28 +21,23 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#############################################################################
+##############################################################################
 from spack import *
 
 
-class Genometools(MakefilePackage):
-    """genometools is a free collection of bioinformatics tools (in the realm
-       of genome informatics) combined into a single binary named gt."""
+class Aegean(MakefilePackage):
+    """The AEGeAn Toolkit is designed for the Analysis and Evaluation of
+       Genome Annotations. The toolkit includes a variety of analysis programs
+       as well as a C library whose API provides access to AEGeAn's core
+       functions and data structures."""
 
-    homepage = "http://genometools.org/"
-    url      = "http://genometools.org/pub/genometools-1.5.9.tar.gz"
+    homepage = "http://brendelgroup.github.io/AEGeAn/"
+    url      = "https://github.com/BrendelGroup/AEGeAn/archive/v0.15.2.tar.gz"
 
-    version('1.5.9', 'e400d69092f9f13db09b33f9dea39d2e')
+    version('0.15.2', 'd7d73f5f132ff52340975b636564e949')
 
-    depends_on('perl', type=('build', 'run'))
-    depends_on('cairo')
-    depends_on('pango')
+    depends_on('genometools')
 
-    # build fails with gcc 7"
-    conflicts('%gcc@7.1.0:')
-
-    def install(self, spec, prefix):
-        make('install', 'prefix=%s' % prefix)
-
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        spack_env.set('CPATH', self.prefix.include.genometools)
+    def edit(self, spec, prefix):
+        makefile = FileFilter('Makefile')
+        makefile.filter('/usr/local', prefix)
