52aafd3d1cd32f722d9147785048c3ce10281caf
Denis Davydov <davydden@gmail.com>
2018-05-17 20:14:29 +0200

    flex: add link to issue 6492, remove os from the conflict

--- var/spack/repos/builtin/packages/flex/package.py
+++ var/spack/repos/builtin/packages/flex/package.py
@@ -34,7 +34,8 @@ class Flex(AutotoolsPackage):
 
     version('2.6.4', '2882e3179748cc9f9c23ec593d6adc8d')
     # 2.6.4 fails to compile with GCC on Ubuntu 18.04LTS
-    # see https://github.com/spack/spack/issues/8152
+    # see https://github.com/spack/spack/issues/8152 and
+    # https://github.com/spack/spack/issues/6942
     version('2.6.3', 'a5f65570cd9107ec8a8ec88f17b31bb1', preferred=True)
     # Avoid flex '2.6.2' (major bug)
     # See issue #2554; https://github.com/westes/flex/issues/113
@@ -57,7 +58,7 @@ class Flex(AutotoolsPackage):
 
     # Build issue for v2.6.4 when gcc 7.2.0 is used on Ubuntu 17.10.
     # See issue #219; https://github.com/westes/flex/issues/219
-    conflicts('%gcc@7.2.0: os=ubuntu17.10', when='@2.6.4')
+    conflicts('%gcc@7.2.0:', when='@2.6.4')
 
     def url_for_version(self, version):
         url = "https://github.com/westes/flex"
