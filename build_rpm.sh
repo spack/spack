cef0e84ea6a1f50c903e72192b5291aee44130fa
Jianwen WEI <weijianwen@gmail.com>
2018-05-19 22:00:41 +0800

    Update RPM packages.

--- build_rpm.sh
+++ build_rpm.sh
@@ -12,8 +12,8 @@ else:
 COMPILERS = ["gcc@5.4.0", "intel@18.0.1"]
 
 # Register exernal packages
-for pkg in ["jdk@8u141-b15%gcc@4.8.5",
-            # "bazel@0.4.5%gcc@4.8.",
+for pkg in ["jdk@8u172-b11%gcc@4.8.5",
+            "bazel@0.11.1%gcc@4.8.",
             "maven@3.3.9%gcc@4.8.",
             "gradle@3.4%gcc@4.8.5",
             "ant@1.9.9%gcc@4.8.5",
@@ -24,37 +24,7 @@ for pkg in ["jdk@8u141-b15%gcc@4.8.5",
           os.system("spack install {}".format(pkg))
 
 # Build non-MPI packages
-nonmpipkgs = {"miniconda2@4.3.30": [""],
-              "miniconda3@4.3.30": [""],
-              "openblas@0.2.20 threads=openmp": [""],
-              "perl@5.24.1": [""],
-              "sga@0.10.15": [""],
-              "boost@1.64.0": [""],
-              "cuda@9.1.85": [""],
-              "cuda@8.0.61": [""],
-              "cuda@7.5.18": [""],
-              "cuda@6.5.14": [""],
-              "cudnn@7.0": ["^cuda@9.1.85", "^cuda@8.0.61"],
-              "cudnn@6.0": ["^cuda@8.0.61"],
-              "cudnn@5.1": ["^cuda@8.0.61"],
-              "samtools@1.6": [""],
-              "glib@2.53.1": [""],
-              "bwa@0.7.17": [""],
-              "r@3.4.3+external-lapack": ["^openblas threads=openmp", "^intel-parallel-studio+mkl"],
-              "octave+qt~readline@4.2.1": ["^openblas threads=openmp"],
-              "fftw@3.3.7~mpi+openmp": [""],
-              # "fftw@2.1.5~mpi+openmp": [""],
-              "eigen~fftw~metis~scotch~suitesparse@3.3.3": [""],
-              "opencv+core+eigen+imgproc+openmp+jpeg+png+tiff@3.3.0": ["^eigen~fftw~metis~scotch~suitesparse@3.3.3"],
-              "gsl@1.16": [""],
-              "gsl@2.4": [""],
-              "glib@2.53.1": [""],
-              "gmp@6.1.2": [""],
-              "ffmpeg@3.2.4": [""],
-              "bamtools@2.4.1": [""],
-              "bcftools@1.6": [""],
-              "hdf5@1.10.1+cxx+fortran+threadsafe~mpi": [""]
-}
+nonmpipkgs = {}
 for pkg,specs in nonmpipkgs.items():
     for spec in specs:
         for compiler in COMPILERS:
@@ -73,8 +43,8 @@ else:
     OMPIFAB = ""
 
 # Build MPI libraries
-MPIS = {"openmpi@2.0.4+pmi~vt~cuda fabrics={} ~java schedulers=slurm".format(OMPIFAB): "",
-        "mvapich2@2.2~cuda fabrics={} process_managers=slurm".format(MVFAB): "",
+MPIS = {"openmpi@3.1.0+pmi~vt~cuda fabrics={} ~java schedulers=slurm".format(OMPIFAB): "",
+        "mvapich2@2.3rc2~cuda fabrics={} process_managers=slurm".format(MVFAB): "",
         # "mvapich2@2.2+cuda fabrics={} process_managers=slurm".format(MVFAB): "^cuda@9.1.85",
         # "mvapich2@2.2+cuda fabrics={} process_managers=slurm".format(MVFAB): "^cuda@8.0.61",
        "intel-parallel-studio@cluster.2018.1+mpi": ""
@@ -86,14 +56,7 @@ for pkg,spec in MPIS.items():
                           install("{} %{} {}".format(pkg, compiler, spec))
 
 # Build MPI packages
-mpipkgs = {"fftw@3.3.7+mpi+openmp": [""],
-           "hdf5@1.10.1+cxx+fortran+threadsafe+mpi": [""],
-           # "fftw@2.1.5+mpi+openmp": [""],
-           "gromacs+mpi~cuda@2016.4": ["^fftw+mpi+openmp@3.3.6-pl2"],
-           "gromacs+mpi+cuda@2016.4": ["^fftw+mpi+openmp@3.3.6-pl2 ^cuda@9.1.85", "^fftw+mpi+openmp@3.3.6-pl2 ^cuda@8.0.61"],
-           "hpl@2.2": ["^openblas threads=openmp", ""]
-           # "scotch+mpi@6.0.4": [""]
-}
+mpipkgs = {}
 for pkg,specs in mpipkgs.items():
     for spec in specs:
         for compiler in COMPILERS:
--- build_user.sh
+++ build_user.sh
@@ -5,8 +5,8 @@ from spack_install import check_pass
 import os
 
 # Register exernal packages
-for pkg in ["jdk@8u141-b15%gcc@4.8.5",
-            "bazel@0.4.5%gcc@4.8.5",
+for pkg in ["jdk@8u172-b11%gcc@4.8.5",
+            "bazel@0.11.1%gcc@4.8.5",
             "gcc@6.4.0%gcc@4.8.5",
             "gcc@5.4.0%gcc@4.8.5",
             "gcc@4.9.4%gcc@4.8.5",
@@ -14,7 +14,6 @@ for pkg in ["jdk@8u141-b15%gcc@4.8.5",
             "gradle@3.4%gcc@4.8.5",
             "ant@1.9.9%gcc@4.8.5",
             "sbt@0.13.12%gcc@4.8.5",
-            "cmake@3.11.1%gcc@4.8.5",
             "environment-modules@3.2.10",
-            "intel-parallel-studio@cluster.2017.5+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune  %intel@17.0.5 threads=openmp"]:
+            "intel-parallel-studio@cluster.2018.1+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune  %intel@18.0.1 threads=openmp"]:
            os.system("spack install {}".format(pkg))
