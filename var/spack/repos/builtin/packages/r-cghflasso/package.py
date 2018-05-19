d62f80e9be27c362b0747b973306bb38c4b630c1
peetsv <vernon.peets@contractors.roche.com>
2018-05-19 09:53:28 -0700

    adding list_url

--- var/spack/repos/builtin/packages/r-cghflasso/package.py
+++ var/spack/repos/builtin/packages/r-cghflasso/package.py
@@ -31,5 +31,6 @@ class RCghflasso(RPackage):
 
     homepage = "https://cran.r-project.org/package=cghFLasso"
     url      = "https://cran.r-project.org/src/contrib/cghFLasso_0.2-1.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/cghFLasso"
 
     version('0.2-1', '9a714a6dda3de9e905d23dadfa5d37f1')
