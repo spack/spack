a3f9eaaea82bbc62378c8215af974416a9406b95
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2018-05-21 11:20:54 -0400

    pdftk: code review

--- var/spack/repos/builtin/packages/pdftk/package.py
+++ var/spack/repos/builtin/packages/pdftk/package.py
@@ -28,7 +28,9 @@ import llnl.util.filesystem
 
 
 class Pdftk(MakefilePackage):
-    """Parallel Ice Sheet Model"""
+    """PDFtk Server is a command-line tool for working with PDFs. It is
+    commonly used for client-side scripting or server-side processing
+    of PDFs."""
 
     homepage = "https://www.pdflabs.com/tools/pdftk-server"
     url      = "https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk-2.02-src.zip"
@@ -37,12 +39,18 @@ class Pdftk(MakefilePackage):
 
     version('2.02', '6534365fd6727724f288a556ede33faa')
 
+    # Only takes effect in phases not overridden here
     build_directory = 'pdftk'
 
     # https://www.pdflabs.com/docs/install-pdftk-on-redhat-or-centos/
     def edit(self, spec, prefix):
-        compiler = self.compiler
 
+        # ------ Fix install directory in main Makefile
+        makefile = FileFilter(join_path('pdftk', 'Makefile.Base'))
+        makefile.filter('/usr/local/bin', spec.prefix.bin)
+
+        # ------ Create new config file
+        compiler = self.compiler
         gcc_base = os.path.split(os.path.split(compiler.cxx)[0])[0]
         gcc_version = compiler.version
 
@@ -80,31 +88,11 @@ class Pdftk(MakefilePackage):
     def build(self, spec, prefix):
         compiler = self.compiler
         gcc_base = os.path.split(os.path.split(compiler.cxx)[0])[0]
-        env = dict(os.environ)
         env['PATH'] = join_path(gcc_base, 'bin') + ':' + env['PATH']
-        os.chdir(self.build_directory)
-        print('build', os.getcwd())
-
-        # Parallel build is known to fail.  But let it get most
-        # of the way there.
-        # (Not sure if this actually saves any time)
-#        try:
-#            make('-f', 'Makefile.Spack')
-#        except:
-#            pass
-
-        # Finish the build in serial
-        make('-f', 'Makefile.Spack', '-j1')
+        with working_dir(self.build_directory):
+            make('-f', 'Makefile.Spack', parallel=False)
 
     def install(self, spec, prefix):
-        # "make install" is hardcoded to /usr...
-        llnl.util.filesystem.mkdirp(self.spec.prefix.bin)
-        llnl.util.filesystem.install(
-            join_path(self.stage.source_path, 'pdftk', 'pdftk'),
-            self.spec.prefix.bin)
-
-#    def setup_environment(self, spack_env, run_env):
-#        # This is only needed to solve the ej1 stuff see #8165
-#        compiler = self.compiler
-#        gcc_base = os.path.split(os.path.split(compiler.cxx)[0])[0]
-#        spack_env.prepend_path('PATH', join_path(gcc_base, 'bin'))
+        mkdirp(self.spec.prefix.bin)
+        with working_dir(self.build_directory):
+            make('-f', 'Makefile.Spack', 'install', parallel=False)
