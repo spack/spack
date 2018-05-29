4ae29445b7a7c608b4abbf32a8de56988327876c
serban <serban@us.ibm.com>
2018-05-29 17:15:32 -0400

    Limit boost_pgi.patch to @1.67.0 only.

--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -172,7 +172,7 @@ class Boost(Package):
     patch('call_once_variadic.patch', when='@1.54.0:1.55.9999%gcc@5.0:5.9')
 
     # Patch fix for PGI compiler
-    patch('boost_pgi.patch', when='%pgi')
+    patch('boost_pgi.patch', when='@1.67.0%pgi')
     patch('boost_1.63.0_pgi.patch', when='@1.63.0%pgi')
     patch('boost_1.63.0_pgi_17.4_workaround.patch', when='@1.63.0%pgi@17.4')
 
