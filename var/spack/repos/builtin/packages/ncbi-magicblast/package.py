77364205bf72cb5b3ecf2e22131a3ea96f0c8ae5
fanne-stat <ifanzhu@foxmail.com>
2018-05-31 15:30:14 -0500

    updated package.py

--- var/spack/repos/builtin/packages/ncbi-magicblast/package.py
+++ var/spack/repos/builtin/packages/ncbi-magicblast/package.py
@@ -26,17 +26,16 @@ from spack import *
 
 
 class NcbiMagicblast(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Magic-BLAST is a tool for mapping large next-generation RNA or DNA
+    sequencing runs against a whole genome or transcriptome. """
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "http://www.example.com"
+    homepage = "https://ncbi.github.io/magicblast/"
     url      = "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/1.3.0/ncbi-magicblast-1.3.0-src.tar.gz"
 
     version('1.3.0', '2615b919c1fe1bf7dc3d816392ab4420')
 
-    # depends_on('foo')
-
+    depends_on('ncbi-vdb')
     configure_directory = 'c++'
 
-
-
+    def configure_args(self):
+        return ['--without-internal']
