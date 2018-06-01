spack/spack

commit db657d938d38775e4364a6917cc78cb9cdb0b133

Refactor IntelInstaller into IntelPackage base class (#4300)
* Refactor IntelInstaller into IntelPackage base class

* Move license attributes from __init__ to class-level

* Flake8 fixes: remove unused imports

* Fix logic that writes the silent.cfg file

* More specific version numbers for Intel MPI

* Rework logic that selects components to install

* Final changes necessary to get intel package working

* Various updates to intel-parallel-studio

* Add latest version of every Intel package

* Add environment variables for Intel packages

* Update env vars for intel package

* Finalize components for intel-parallel-studio package

Adds a +tbb variant to intel-parallel-studio.
The tbb package was renamed to intel-tbb.
Now both intel-tbb and intel-parallel-studio+tbb
provide tbb.

* Overhaul environment variables set by intel-parallel-studio

* Point dependent packages to the correct MPI wrappers

* Never default to intel-parallel-studio

* Gather env vars by sourcing setup scripts

* Use mpiicc instead of mpicc when using Intel compiler

* Undo change to ARCH

* Add changes from intel-mpi to intel-parallel-studio

* Add comment explaining mpicc vs mpiicc

* Prepend env vars containing 'PATH' or separators

* Flake8 fix

* Fix bugs in from_sourcing_file

* Indentation fix

* Prepend, not set if contains separator

* Fix license symlinking broken by changes to intel-parallel-studio

* Use comments instead of docstrings to document attributes

* Flake8 fixes

* Use a set instead of a list to prevent duplicate components

* Fix MKL and MPI library linking directories

* Remove +all variant from intel-parallel-studio

* It is not possible to build with MKL, GCC, and OpenMP at this time

* Found a workaround for locating GCC libraries

* Typos and variable names

* Fix initialization of empty LibraryList

var/spack/repos/builtin/packages/intel-mpi/package.py
@@ -22,30 +22,41 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-from spack import *
 import os
 
-from spack.pkg.builtin.intel import IntelInstaller
+from spack import *
+from spack.environment import EnvironmentModifications
 
 
-class IntelMpi(IntelInstaller):
+class IntelMpi(IntelPackage):
     """Intel MPI"""
 
     homepage = "https://software.intel.com/en-us/intel-mpi-library"
 
-    version('2017.3', '721ecd5f6afa385e038777e5b5361dfb',
+    version('2017.3.196', '721ecd5f6afa385e038777e5b5361dfb',
             url='http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/11595/l_mpi_2017.3.196.tgz')
-    version('2017.2', 'b6c2e62c3fb9b1558ede72ccf72cf1d6',
+    version('2017.2.174', 'b6c2e62c3fb9b1558ede72ccf72cf1d6',
             url='http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/11334/l_mpi_2017.2.174.tgz')
-    version('2017.1', 'd5e941ac2bcf7c5576f85f6bcfee4c18',
+    version('2017.1.132', 'd5e941ac2bcf7c5576f85f6bcfee4c18',
             url='http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/11014/l_mpi_2017.1.132.tgz')
-    version('5.1.3', '4316e78533a932081b1a86368e890800',
+    version('5.1.3.223',  '4316e78533a932081b1a86368e890800',
             url='http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/9278/l_mpi_p_5.1.3.223.tgz')
 
     provides('mpi')
 
     @property
+    def license_required(self):
+        # The Intel libraries are provided without requiring a license as of
+        # version 2017.2. Trying to specify the license will fail. See:
+        # https://software.intel.com/en-us/articles/free-ipsxe-tools-and-libraries
+        if self.version >= Version('2017.2'):
+            return False
+        else:
+            return True
+
+    @property
     def mpi_libs(self):
+        mpi_root = self.prefix.compilers_and_libraries.linux.mpi.lib64
         query_parameters = self.spec.last_query.extra_parameters
         libraries = ['libmpifort', 'libmpi']
 
@@ -53,19 +64,15 @@ class IntelMpi(IntelInstaller):
             libraries = ['libmpicxx'] + libraries
 
         return find_libraries(
-            libraries, root=self.prefix.lib64, shared=True, recurse=True
+            libraries, root=mpi_root, shared=True, recurse=True
         )
 
     @property
     def mpi_headers(self):
         # recurse from self.prefix will find too many things for all the
         # supported sub-architectures like 'mic'
-        return find_headers(
-            'mpi', root=self.prefix.include64, recurse=False)
-
-    def install(self, spec, prefix):
-        self.intel_prefix = prefix
-        IntelInstaller.install(self, spec, prefix)
+        mpi_root = self.prefix.compilers_and_libraries.linux.mpi.include64
+        return find_headers('mpi', root=mpi_root, recurse=False)
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         spack_env.set('I_MPI_CC', spack_cc)
@@ -75,15 +82,52 @@ class IntelMpi(IntelInstaller):
         spack_env.set('I_MPI_FC', spack_fc)
 
     def setup_dependent_package(self, module, dep_spec):
-        # Check for presence of bin64 or bin directory
-        if os.path.isdir(self.prefix.bin):
-            bindir = self.prefix.bin
-        elif os.path.isdir(self.prefix.bin64):
-            bindir = self.prefix.bin64
-        else:
-            raise RuntimeError('No suitable bindir found')
+        # Intel comes with 2 different flavors of MPI wrappers:
+        #
+        # * mpiicc, mpiicpc, and mpifort are hardcoded to wrap around
+        #   the Intel compilers.
+        # * mpicc, mpicxx, mpif90, and mpif77 allow you to set which
+        #   compilers to wrap using I_MPI_CC and friends. By default,
+        #   wraps around the GCC compilers.
+        #
+        # In theory, these should be equivalent as long as I_MPI_CC
+        # and friends are set to point to the Intel compilers, but in
+        # practice, mpicc fails to compile some applications while
+        # mpiicc works.
+        bindir = self.prefix.compilers_and_libraries.linux.mpi.intel64.bin
 
-        self.spec.mpicc = join_path(bindir, 'mpicc')
-        self.spec.mpicxx = join_path(bindir, 'mpicxx')
-        self.spec.mpifc = join_path(bindir, 'mpif90')
-        self.spec.mpif77 = join_path(bindir, 'mpif77')
+        if self.compiler.name == 'intel':
+            self.spec.mpicc  = bindir.mpiicc
+            self.spec.mpicxx = bindir.mpiicpc
+            self.spec.mpifc  = bindir.mpiifort
+            self.spec.mpif77 = bindir.mpiifort
+        else:
+            self.spec.mpicc  = bindir.mpicc
+            self.spec.mpicxx = bindir.mpicxx
+            self.spec.mpifc  = bindir.mpif90
+            self.spec.mpif77 = bindir.mpif77
+
+    def setup_environment(self, spack_env, run_env):
+        """Adds environment variables to the generated module file.
+
+        These environment variables come from running:
+
+        .. code-block:: console
+
+           $ source compilers_and_libraries/linux/mpi/intel64/bin/mpivars.sh
+        """
+        # NOTE: Spack runs setup_environment twice, once pre-build to set up
+        # the build environment, and once post-installation to determine
+        # the environment variables needed at run-time to add to the module
+        # file. The script we need to source is only present post-installation,
+        # so check for its existence before sourcing.
+        # TODO: At some point we should split setup_environment into
+        # setup_build_environment and setup_run_environment to get around
+        # this problem.
+        mpivars = os.path.join(
+            self.prefix.compilers_and_libraries.linux.mpi.intel64.bin,
+            'mpivars.sh')
+
+        if os.path.isfile(mpivars):
+            run_env.extend(EnvironmentModifications.from_sourcing_file(
+                mpivars))

lib/spack/spack/package.py
@@ -484,37 +484,64 @@ class PackageBase(with_metaclass(PackageMeta, object)):
     #
     # These are default values for instance variables.
     #
-    """By default we build in parallel.  Subclasses can override this."""
+
+    #: By default we build in parallel.  Subclasses can override this.
     parallel = True
 
-    """# jobs to use for parallel make. If set, overrides default of ncpus."""
+    #: # jobs to use for parallel make. If set, overrides default of ncpus.
     make_jobs = spack.build_jobs
 
-    """By default do not run tests within package's install()"""
+    #: By default do not run tests within package's install()
     run_tests = False
 
     # FIXME: this is a bad object-oriented design, should be moved to Clang.
-    """By default do not setup mockup XCode on macOS with Clang"""
+    #: By default do not setup mockup XCode on macOS with Clang
     use_xcode = False
 
-    """Most packages are NOT extendable. Set to True if you want extensions."""
+    #: Most packages are NOT extendable. Set to True if you want extensions.
     extendable = False
 
-    """When True, add RPATHs for the entire DAG. When False, add RPATHs only
-       for immediate dependencies."""
+    #: When True, add RPATHs for the entire DAG. When False, add RPATHs only
+    #: for immediate dependencies.
     transitive_rpaths = True
 
-    """List of prefix-relative file paths (or a single path). If these do
-       not exist after install, or if they exist but are not files,
-       sanity checks fail.
-    """
+    #: List of prefix-relative file paths (or a single path). If these do
+    #: not exist after install, or if they exist but are not files,
+    #: sanity checks fail.
     sanity_check_is_file = []
 
-    """List of prefix-relative directory paths (or a single path). If
-       these do not exist after install, or if they exist but are not
-       directories, sanity checks will fail.
-    """
+    #: List of prefix-relative directory paths (or a single path). If
+    #: these do not exist after install, or if they exist but are not
+    #: directories, sanity checks will fail.
     sanity_check_is_dir = []
+
+    #
+    # Set default licensing information
+    #
+
+    #: Boolean. If set to ``True``, this software requires a license.
+    #: If set to ``False``, all of the ``license_*`` attributes will
+    #: be ignored. Defaults to ``False``.
+    license_required = False
+
+    #: String. Contains the symbol used by the license manager to denote
+    #: a comment. Defaults to ``#``.
+    license_comment = '#'
+
+    #: List of strings. These are files that the software searches for when
+    #: looking for a license. All file paths must be relative to the
+    #: installation directory. More complex packages like Intel may require
+    #: multiple licenses for individual components. Defaults to the empty list.
+    license_files = []
+
+    #: List of strings. Environment variables that can be set to tell the
+    #: software where to look for a license if it is not in the usual location.
+    #: Defaults to the empty list.
+    license_vars = []
+
+    #: String. A URL pointing to license setup instructions for the software.
+    #: Defaults to the empty string.
+    license_url = ''
 
     def __init__(self, spec):
         # this determines how the package should be built.
@@ -568,22 +595,6 @@ class PackageBase(with_metaclass(PackageMeta, object)):
 
         if not hasattr(self, 'list_depth'):
             self.list_depth = 0
-
-        # Set default licensing information
-        if not hasattr(self, 'license_required'):
-            self.license_required = False
-
-        if not hasattr(self, 'license_comment'):
-            self.license_comment = '#'
-
-        if not hasattr(self, 'license_files'):
-            self.license_files = []
-
-        if not hasattr(self, 'license_vars'):
-            self.license_vars = []
-
-        if not hasattr(self, 'license_url'):
-            self.license_url = None
 
         # Set up some internal variables for timing.
         self._fetch_time = 0.0

var/spack/repos/builtin/packages/intel-daal/package.py
@@ -22,18 +22,14 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-from spack import *
 import os
 
-from spack.pkg.builtin.intel import IntelInstaller
+from spack import *
+from spack.environment import EnvironmentModifications
 
 
-class IntelDaal(IntelInstaller):
-    """Intel Data Analytics Acceleration Library.
-
-    Note: You will have to add the download file to a
-    mirror so that Spack can find it. For instructions on how to set up a
-    mirror, see http://spack.readthedocs.io/en/latest/mirrors.html"""
+class IntelDaal(IntelPackage):
+    """Intel Data Analytics Acceleration Library."""
 
     homepage = "https://software.intel.com/en-us/daal"
 
@@ -52,11 +48,35 @@ class IntelDaal(IntelInstaller):
 
     provides('daal')
 
-    def install(self, spec, prefix):
+    @property
+    def license_required(self):
+        # The Intel libraries are provided without requiring a license as of
+        # version 2017.2. Trying to specify the license will fail. See:
+        # https://software.intel.com/en-us/articles/free-ipsxe-tools-and-libraries
+        if self.version >= Version('2017.2'):
+            return False
+        else:
+            return True
 
-        self.intel_prefix = os.path.join(prefix, "pkg")
-        IntelInstaller.install(self, spec, prefix)
+    def setup_environment(self, spack_env, run_env):
+        """Adds environment variables to the generated module file.
 
-        daal_dir = os.path.join(self.intel_prefix, "daal")
-        for f in os.listdir(daal_dir):
-            os.symlink(os.path.join(daal_dir, f), os.path.join(self.prefix, f))
+        These environment variables come from running:
+
+        .. code-block:: console
+
+           $ source daal/bin/daalvars.sh intel64
+        """
+        # NOTE: Spack runs setup_environment twice, once pre-build to set up
+        # the build environment, and once post-installation to determine
+        # the environment variables needed at run-time to add to the module
+        # file. The script we need to source is only present post-installation,
+        # so check for its existence before sourcing.
+        # TODO: At some point we should split setup_environment into
+        # setup_build_environment and setup_run_environment to get around
+        # this problem.
+        daalvars = os.path.join(self.prefix.daal.bin, 'daalvars.sh')
+
+        if os.path.isfile(daalvars):
+            run_env.extend(EnvironmentModifications.from_sourcing_file(
+                daalvars, 'intel64'))

var/spack/repos/builtin/packages/intel-ipp/package.py
@@ -22,18 +22,14 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-from spack import *
 import os
 
-from spack.pkg.builtin.intel import IntelInstaller
+from spack import *
+from spack.environment import EnvironmentModifications
 
 
-class IntelIpp(IntelInstaller):
-    """Intel Integrated Performance Primitives.
-
-    Note: You will have to add the download file to a
-    mirror so that Spack can find it. For instructions on how to set up a
-    mirror, see http://spack.readthedocs.io/en/latest/mirrors.html"""
+class IntelIpp(IntelPackage):
+    """Intel Integrated Performance Primitives."""
 
     homepage = "https://software.intel.com/en-us/intel-ipp"
 
@@ -50,11 +46,35 @@ class IntelIpp(IntelInstaller):
 
     provides('ipp')
 
-    def install(self, spec, prefix):
+    @property
+    def license_required(self):
+        # The Intel libraries are provided without requiring a license as of
+        # version 2017.2. Trying to specify the license will fail. See:
+        # https://software.intel.com/en-us/articles/free-ipsxe-tools-and-libraries
+        if self.version >= Version('2017.2'):
+            return False
+        else:
+            return True
 
-        self.intel_prefix = os.path.join(prefix, "pkg")
-        IntelInstaller.install(self, spec, prefix)
+    def setup_environment(self, spack_env, run_env):
+        """Adds environment variables to the generated module file.
 
-        ipp_dir = os.path.join(self.intel_prefix, "ipp")
-        for f in os.listdir(ipp_dir):
-            os.symlink(os.path.join(ipp_dir, f), os.path.join(self.prefix, f))
+        These environment variables come from running:
+
+        .. code-block:: console
+
+           $ source ipp/bin/ippvars.sh intel64
+        """
+        # NOTE: Spack runs setup_environment twice, once pre-build to set up
+        # the build environment, and once post-installation to determine
+        # the environment variables needed at run-time to add to the module
+        # file. The script we need to source is only present post-installation,
+        # so check for its existence before sourcing.
+        # TODO: At some point we should split setup_environment into
+        # setup_build_environment and setup_run_environment to get around
+        # this problem.
+        ippvars = os.path.join(self.prefix.ipp.bin, 'ippvars.sh')
+
+        if os.path.isfile(ippvars):
+            run_env.extend(EnvironmentModifications.from_sourcing_file(
+                ippvars, 'intel64'))

var/spack/repos/builtin/packages/intel-mkl/package.py
@@ -22,13 +22,13 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-from spack import *
 import os
 
-from spack.pkg.builtin.intel import IntelInstaller
+from spack import *
+from spack.environment import EnvironmentModifications
 
 
-class IntelMkl(IntelInstaller):
+class IntelMkl(IntelPackage):
     """Intel Math Kernel Library."""
 
     homepage = "https://software.intel.com/en-us/intel-mkl"
@@ -50,11 +50,20 @@ class IntelMkl(IntelInstaller):
     variant('ilp64', default=False, description='64 bit integers')
     variant('openmp', default=False, description='OpenMP multithreading layer')
 
-    # virtual dependency
     provides('blas')
     provides('lapack')
     provides('scalapack')
     provides('mkl')
+
+    @property
+    def license_required(self):
+        # The Intel libraries are provided without requiring a license as of
+        # version 2017.2. Trying to specify the license will fail. See:
+        # https://software.intel.com/en-us/articles/free-ipsxe-tools-and-libraries
+        if self.version >= Version('2017.2'):
+            return False
+        else:
+            return True
 
     @property
     def blas_libs(self):
@@ -69,15 +78,27 @@ class IntelMkl(IntelInstaller):
 
         mkl_threading = ['libmkl_sequential']
 
+        omp_libs = LibraryList([])
+
         if '+openmp' in spec:
             if '%intel' in spec:
-                mkl_threading = ['libmkl_intel_thread', 'libiomp5']
-            else:
+                mkl_threading = ['libmkl_intel_thread']
+                omp_threading = ['libiomp5']
+
+                omp_root = prefix.compilers_and_libraries.linux.lib.intel64
+                omp_libs = find_libraries(
+                    omp_threading, root=omp_root, shared=shared)
+            elif '%gcc' in spec:
                 mkl_threading = ['libmkl_gnu_thread']
+
+                gcc = Executable(self.compiler.cc)
+                libgomp = gcc('--print-file-name', 'libgomp.{0}'.format(
+                    dso_suffix), output=str)
+                omp_libs = LibraryList(libgomp)
 
         # TODO: TBB threading: ['libmkl_tbb_thread', 'libtbb', 'libstdc++']
 
-        mkl_root = join_path(prefix.lib, 'intel64')
+        mkl_root = prefix.compilers_and_libraries.linux.mkl.lib.intel64
 
         mkl_libs = find_libraries(
             mkl_integer + ['libmkl_core'] + mkl_threading,
@@ -91,7 +112,7 @@ class IntelMkl(IntelInstaller):
             shared=shared
         )
 
-        return mkl_libs + system_libs
+        return mkl_libs + omp_libs + system_libs
 
     @property
     def lapack_libs(self):
@@ -120,30 +141,46 @@ class IntelMkl(IntelInstaller):
         elif '^intel-mpi' in root:
             libnames.append('libmkl_blacs_intelmpi')
         else:
-            raise InstallError("No MPI found for scalapack")
+            raise InstallError('No MPI found for scalapack')
 
-        shared = True if '+shared' in self.spec else False
         integer = 'ilp64' if '+ilp64' in self.spec else 'lp64'
+        mkl_root = self.prefix.compilers_and_libraries.linux.mkl.lib.intel64
+        shared = True if '+shared' in self.spec else False
+
         libs = find_libraries(
             ['{0}_{1}'.format(l, integer) for l in libnames],
-            root=join_path(self.prefix.lib, 'intel64'),
+            root=mkl_root,
             shared=shared
         )
+
         return libs
-
-    def install(self, spec, prefix):
-        self.intel_prefix = os.path.join(prefix, "pkg")
-        IntelInstaller.install(self, spec, prefix)
-
-        mkl_dir = os.path.join(self.intel_prefix, "mkl")
-        for f in os.listdir(mkl_dir):
-            os.symlink(os.path.join(mkl_dir, f), os.path.join(self.prefix, f))
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         # set up MKLROOT for everyone using MKL package
+        mkl_root = self.prefix.compilers_and_libraries.linux.mkl.lib.intel64
+
         spack_env.set('MKLROOT', self.prefix)
-        spack_env.append_path('SPACK_COMPILER_EXTRA_RPATHS',
-                              join_path(self.prefix.lib, 'intel64'))
+        spack_env.append_path('SPACK_COMPILER_EXTRA_RPATHS', mkl_root)
 
     def setup_environment(self, spack_env, run_env):
-        run_env.set('MKLROOT', self.prefix)
+        """Adds environment variables to the generated module file.
+
+        These environment variables come from running:
+
+        .. code-block:: console
+
+           $ source mkl/bin/mklvars.sh intel64
+        """
+        # NOTE: Spack runs setup_environment twice, once pre-build to set up
+        # the build environment, and once post-installation to determine
+        # the environment variables needed at run-time to add to the module
+        # file. The script we need to source is only present post-installation,
+        # so check for its existence before sourcing.
+        # TODO: At some point we should split setup_environment into
+        # setup_build_environment and setup_run_environment to get around
+        # this problem.
+        mklvars = os.path.join(self.prefix.mkl.bin, 'mklvars.sh')
+
+        if os.path.isfile(mklvars):
+            run_env.extend(EnvironmentModifications.from_sourcing_file(
+                mklvars, 'intel64'))

var/spack/repos/builtin/packages/intel-tbb/package.py
@@ -26,7 +26,7 @@ from spack import *
 import glob
 
 
-class Tbb(Package):
+class IntelTbb(Package):
     """Widely used C++ template library for task parallelism.
     Intel Threading Building Blocks (Intel TBB) lets you easily write parallel
     C++ programs that take full advantage of multicore performance, that are
@@ -46,6 +46,8 @@ class Tbb(Package):
             url='https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb44_20160413oss_src.tgz')
     version('4.4.3', '80707e277f69d9b20eeebdd7a5f5331137868ce1',
             url='https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb44_20160128oss_src_0.tgz')
+
+    provides('tbb')
 
     def coerce_to_spack(self, tbb_build_subdir):
         for compiler in ["icc", "gcc", "clang"]:

commit 4b12d79c30afe0ee64a515735d3c749c914b4d09

fix intel-mpi package
the main reason Intel-MPI was added in
https://github.com/spack/spack/pull/3905 was to support
a stand-alone installation, i.e. not a part of parallel studio or alike.
The intended behaviour got broken during refactoring in
https://github.com/spack/spack/pull/4300
This commit fixes the issue by supporting different
directories.

var/spack/repos/builtin/packages/intel-mpi/package.py
@@ -29,7 +29,12 @@ from spack.environment import EnvironmentModifications
 
 
 class IntelMpi(IntelPackage):
-    """Intel MPI"""
+    """Intel MPI
+
+    One of the purposes of this package is to support a stand-alone
+    installation, that is when prefix does not contain
+    compilers_and_libraries folder and alike.
+    """
 
     homepage = "https://software.intel.com/en-us/intel-mpi-library"
 
@@ -64,7 +69,16 @@ class IntelMpi(IntelPackage):
 
     @property
     def mpi_libs(self):
-        mpi_root = self.prefix.compilers_and_libraries.linux.mpi.lib64
+        dirs = [
+            self.prefix.compilers_and_libraries.linux.mpi.lib64,
+            self.prefix.lib64
+        ]
+
+        for d in dirs:
+            if os.path.isdir(d):
+                mpi_root = d
+                break
+
         query_parameters = self.spec.last_query.extra_parameters
         libraries = ['libmpifort', 'libmpi']
 
@@ -79,7 +93,16 @@ class IntelMpi(IntelPackage):
     def mpi_headers(self):
         # recurse from self.prefix will find too many things for all the
         # supported sub-architectures like 'mic'
-        mpi_root = self.prefix.compilers_and_libraries.linux.mpi.include64
+        dirs = [
+            self.prefix.compilers_and_libraries.linux.mpi.include64,
+            self.prefix.include64
+        ]
+
+        for d in dirs:
+            if os.path.isdir(d):
+                mpi_root = d
+                break
+
         return find_headers('mpi', root=mpi_root, recursive=False)
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
@@ -102,7 +125,16 @@ class IntelMpi(IntelPackage):
         # and friends are set to point to the Intel compilers, but in
         # practice, mpicc fails to compile some applications while
         # mpiicc works.
-        bindir = self.prefix.compilers_and_libraries.linux.mpi.intel64.bin
+        dirs = [
+            self.prefix.compilers_and_libraries.linux.mpi.intel64.bin,
+            self.prefix.bin64,
+            self.prefix.bin
+        ]
+
+        for d in dirs:
+            if os.path.isdir(d):
+                bindir = d
+                break
 
         if self.compiler.name == 'intel':
             self.spec.mpicc  = bindir.mpiicc