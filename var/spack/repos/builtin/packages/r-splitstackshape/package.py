81da0fa6fc076d119fb03d4294e8dcb0ff2e012f
lingnanyuan <1297162327@qq.com>
2018-06-04 15:42:22 -0500

    r-splitstackshape: Add version constraints.

--- var/spack/repos/builtin/packages/r-splitstackshape/package.py
+++ var/spack/repos/builtin/packages/r-splitstackshape/package.py
@@ -26,17 +26,21 @@ from spack import *
 
 
 class RSplitstackshape(RPackage):
-    """Online data collection tools like Google Forms often export multiple-
-    response questions with data concatenated in cells. The concat.split
-    (cSplit) family of functions splits such data into separate cells. The
-    package also includes functions to stack groups of columns and to reshape
-    wide data, even when the data are "unbalanced"—something which reshape
-    (from base R) does not handle, and which melt and dcast from reshape2
-    do not easily handle."""
+    """Stack and Reshape Datasets After Splitting Concatenated Values.
+
+    Online data collection tools like Google Forms often export
+    multiple-response questions with data concatenated in cells. The
+    concat.split (cSplit) family of functions splits such data into
+    separate cells. The package also includes functions to stack groups
+    of columns and to reshape wide data, even when the data are
+    "unbalanced" something which reshape (from base R) does not handle,
+    and which melt and dcast from reshape2 do not easily handle.
+    """
 
     homepage = "http://github.com/mrdwab/splitstackshape"
     url      = "https://cran.r-project.org/src/contrib/splitstackshape_1.4.4.tar.gz"
 
     version('1.4.4', '54d2554fe92dfc4670a000b45baacc28')
 
-    depends_on('r-data-table', type=('build', 'run'))
+    depends_on('r-data-table@1.9.4:', type=('build', 'run'))
+    depends_on('r@2.10:')
