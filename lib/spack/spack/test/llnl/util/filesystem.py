89353db3a19962a94033d2772540182d379195fb
Adam J. Stewart <ajstewart426@gmail.com>
2018-06-06 20:23:59 -0500

    Add tests for copy and install methods

--- lib/spack/spack/test/llnl/util/filesystem.py
+++ lib/spack/spack/test/llnl/util/filesystem.py
@@ -22,8 +22,93 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+"""Tests for ``llnl/util/filesystem.py``"""
+
+from spack.stage import Stage
 
 import llnl.util.filesystem as fs
+import os
+import pytest
+
+
+@pytest.fixture()
+def stage():
+    """Creates a stage with the directory structure for the tests."""
+
+    s = Stage('copy-install-test')
+    s.create()
+
+    with fs.working_dir(s.path):
+        # Create source file hierarchy
+        fs.touchp('source/1')
+        fs.touchp('source/a/b/2')
+        fs.touchp('source/a/b/3')
+        fs.touchp('source/c/4')
+        fs.touchp('source/c/d/5')
+        fs.touchp('source/c/d/6')
+        fs.touchp('source/c/d/e/7')
+
+        # Create symlink
+        os.symlink('source/1', 'source/2')
+
+        # Create destination directory
+        fs.mkdirp('dest')
+
+    yield s
+
+    s.destroy()
+
+
+class TestCopy:
+    """Tests for ``filesystem.copy``"""
+
+    def test_file_dest(self, stage):
+        """Test using a filename as the destination."""
+
+        with fs.working_dir(stage.path):
+            fs.copy('source/1', 'dest/1')
+
+            assert os.path.exists('dest/1')
+            assert os.stat('source/1').st_mode == os.stat('dest/1').st_mode
+
+    def test_dir_dest(self, stage):
+        """Test using a directory as the destination."""
+
+        with fs.working_dir(stage.path):
+            fs.copy('source/1', 'dest')
+
+            assert os.path.exists('dest/1')
+            assert os.stat('source/1').st_mode == os.stat('dest/1').st_mode
+
+
+class TestInstall:
+    """Tests for ``filesystem.install``"""
+
+    def test_file_dest(self, stage):
+        """Test using a filename as the destination."""
+
+        with fs.working_dir(stage.path):
+            fs.install('source/1', 'dest/1')
+
+            assert os.path.exists('dest/1')
+            assert os.stat('source/1').st_mode == os.stat('dest/1').st_mode
+
+    def test_dir_dest(self, stage):
+        """Test using a directory as the destination."""
+
+        with fs.working_dir(stage.path):
+            fs.install('source/1', 'dest')
+
+            assert os.path.exists('dest/1')
+            assert os.stat('source/1').st_mode == os.stat('dest/1').st_mode
+
+
+class TestCopyTree:
+    pass
+
+
+class TestInstallTree:
+    pass
 
 
 def test_move_transaction_commit(tmpdir):
