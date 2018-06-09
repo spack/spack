1f1ed292cc36ae6a8d580df08fc87bf0c770e291
Christopher Zachow <christopher.zachow@dlr.de>
2018-06-09 12:08:04 +0200

    py-projectq:removed dependency and changed type of py-pytest dependency

--- var/spack/repos/builtin/packages/py-projectq/package.py
+++ var/spack/repos/builtin/packages/py-projectq/package.py
@@ -50,12 +50,12 @@ class PyProjectq(PythonPackage):
             git='https://github.com/projectq-framework/projectq.git')
 
     # Dependencies
-    depends_on('python', type=('build', 'run'))
+    extends('python')
     depends_on('py-setuptools', type=('build'))
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-scipy', type=('build', 'run'))
     depends_on('py-future', type=('build', 'run'))
-    depends_on('py-pytest@3.1.0:', type=('build', 'run'))
+    depends_on('py-pytest@3.1.0:', type=('test'))
     depends_on('py-requests', type=('build', 'run'))
     # conflict with pybind11@2.2.0 -> see requirements.txt
     depends_on('py-pybind11@1.7:2.1,2.2.1:', type=('build', 'run'))
