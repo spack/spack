335c4e59cd31487a59cf935b01ae6326e10ec352
Jon Rood <jon.rood@nrel.gov>
2018-06-15 16:01:08 -0600

    Trilinos needs parallel-netcdf as an explicit dependency to avoid concretization errors when netcdf is an external.

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -280,6 +280,7 @@ class Trilinos(CMakePackage):
     depends_on('mpi')
     depends_on('netcdf+mpi', when="~pnetcdf")
     depends_on('netcdf+mpi+parallel-netcdf', when="+pnetcdf@master,12.12.1:")
+    depends_on('parallel-netcdf', when="+pnetcdf@master,12.12.1:")
     depends_on('parmetis', when='+metis')
     depends_on('cgns', when='+cgns')
     # Trilinos' Tribits config system is limited which makes it very tricky to
