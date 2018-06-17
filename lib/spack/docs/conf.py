3b56493cfc550f11a5b29bf1a0eb273c48097b3d
Todd Gamblin <tgamblin@llnl.gov>
2018-06-17 00:41:10 -0700

    docs: remove vendored sphinx_rtd_theme

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -222,7 +222,7 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = { 'logo_only' : True }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes"]
+# html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
