87fbebef3712819a04900584330240bd785a5bb4
Peter Josef Scheibel <scheibel1@llnl.gov>
2018-06-18 17:57:53 -0700

    update activate/deactivate command tests with assertions; add test for deactivate --all

--- lib/spack/spack/test/cmd/activate.py
+++ lib/spack/spack/test/cmd/activate.py
@@ -27,6 +27,7 @@ from spack.main import SpackCommand
 activate = SpackCommand('activate')
 deactivate = SpackCommand('deactivate')
 install = SpackCommand('install')
+extensions = SpackCommand('extensions')
 
 
 def test_activate(
@@ -34,6 +35,8 @@ def test_activate(
         install_mockery):
     install('extension1')
     activate('extension1')
+    output = extensions('--show', 'activated', 'extendee')
+    assert 'extension1' in output
 
 
 def test_deactivate(
@@ -42,3 +45,17 @@ def test_deactivate(
     install('extension1')
     activate('extension1')
     deactivate('extension1')
+    output = extensions('--show', 'activated', 'extendee')
+    assert 'extension1' not in output
+
+
+def test_deactivate_all(
+        mock_packages, mock_archive, mock_fetch, config,
+        install_mockery):
+    install('extension1')
+    install('extension2')
+    activate('extension1')
+    activate('extension2')
+    deactivate('--all', 'extendee')
+    output = extensions('--show', 'activated', 'extendee')
+    assert 'extension1' not in output
