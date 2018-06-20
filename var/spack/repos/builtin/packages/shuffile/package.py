edbf845f488c740cb8ab9b101b62aaab9b5f88bb
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2018-06-20 16:12:49 -0700

    shuffile package

---
+++ var/spack/repos/builtin/packages/shuffile/package.py
@@ -0,0 +1,45 @@
+##############################################################################
+# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Shuffile(CMakePackage):
+    """Shuffle files between MPI ranks"""
+
+    homepage = "https://github.com/ECP-VeloC/shuffile"
+    url      = "https://github.com/ECP-VeloC/shuffile/archive/v0.0.1.zip"
+
+    version('0.0.1', '5b8737cce52e67b1a094376d13ca6826')
+
+    depends_on('mpi')
+    depends_on('kvtree')
+
+    def cmake_args(self):
+        args = []
+        args.append("-DMPI_C_COMPILER=%s" % spec['mpi'].mpicc)
+        if self.spec.satisfies('platform=cray'):
+            args.append("-DSHUFFILE_LINK_STATIC=ON")
+        args.append("-DWITH_KVTREE_PREFIX=%s" % spec['kvtree'].prefix)
+        return args
