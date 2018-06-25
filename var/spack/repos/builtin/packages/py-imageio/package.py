edd8026f34667820138478909e201cc40e85c238
Sajid Ali <30510036+s-sajid-ali@users.noreply.github.com>
2018-06-25 11:58:42 -0500

    Update package.py

--- var/spack/repos/builtin/packages/py-imageio/package.py
+++ var/spack/repos/builtin/packages/py-imageio/package.py
@@ -26,9 +26,9 @@ from spack import *
 
 
 class PyImageio(PythonPackage):
-    """ Imageio is a Python library that provides an easy interface 
-    to read and write a wide range of image data, including animated 
-    images, video, volumetric data, and scientific formats. It is 
+    """ Imageio is a Python library that provides an easy interface
+    to read and write a wide range of image data, including animated
+    images, video, volumetric data, and scientific formats. It is
     cross-platform, runs on Python 2.7 and 3.4+, and is easy to install."""
 
     homepage = "http://imageio.github.io/"
@@ -36,14 +36,11 @@ class PyImageio(PythonPackage):
 
     version('2.3.0', '4722c4e1c366748abcb18729881cffb8')
 
-    #TODO: Add variants for plugins, and optional dependencies
+    # TODO: Add variants for plugins, and optional dependencies
 
     # Fix for python 2 if needed.
-    depends_on('py-numpy',  type=('build','run'))
-    depends_on('py-pillow', type=('build', 'run'))
-    depends_on('ffmpeg',    type='run')
-
-
-    def build_args(self, spec, prefix):
-        args = []
-        return args
+    depends_on('py-numpy',            type=('build', 'run'))
+    depends_on('py-pillow',           type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
+    depends_on('py-setuptools',       type='build')
+    depends_on('ffmpeg',              type='run')
