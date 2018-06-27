7f9b7b29dbf1369f0f463a9b2e71eb8c94efc68d
Jon Rood <jon.rood@nrel.gov>
2018-06-26 22:35:18 -0600

    First draft of amrvis package file.

---
+++ var/spack/repos/builtin/packages/amrvis/package.py
@@ -0,0 +1,160 @@
+##############################################################################
+# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.  #
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Amrvis(MakefilePackage):
+    """Amrvis is a visualization package specifically designed to
+       read and display output and profiling data from codes built
+       on the AMReX framework.
+    """
+
+    homepage = "https://github.com/AMReX-Codes/Amrvis"
+    url      = "https://github.com/AMReX-Codes/Amrvis.git"
+
+    version('master', git='https://github.com/AMReX-Codes/Amrvis.git', tag='master')
+
+    variant('dims',
+        default='3',
+        values=('1', '2', '3'),
+        multi=False,
+        description='Number of spatial dimensions')
+
+    variant('prec',
+        default='DOUBLE',
+        values=('FLOAT', 'DOUBLE'),
+        multi=False,
+        description='Floating point precision')
+
+    variant('mpi', default=False, description='Enable MPI parallel support')
+    variant('debug', default=False, description='Enable debugging features')
+
+    depends_on('mpi', when='+mpi')
+    depends_on('libsm')
+    depends_on('libice')
+    depends_on('libxpm')
+    depends_on('libx11')
+    depends_on('libxt')
+    depends_on('libxext')
+    depends_on('motif')
+
+    conflicts('%cce', msg='Amrvis is only set to build with gcc currently')
+    conflicts('%clang', msg='Amrvis is only set to build with gcc currently')
+    conflicts('%intel', msg='Amrvis is only set to build with gcc currently')
+    conflicts('%nag', msg='Amrvis is only set to build with gcc currently')
+    conflicts('%pgi', msg='Amrvis is only set to build with gcc currently')
+    conflicts('%xl', msg='Amrvis is only set to build with gcc currently')
+    conflicts('%xl_r', msg='Amrvis is only set to build with gcc currently')
+    conflicts('%intel', msg='Amrvis is only set to build with gcc currently')
+
+    resource(name='amrex',
+             git='https://github.com/AMReX-Codes/amrex.git',
+             tag='master',
+             placement='amrex')
+
+    def edit(self, spec, prefix):
+        filter_file(r'^AMREX_HOME\s*=.*',
+                    'AMREX_HOME = {0}'.format('./amrex'),
+                    'GNUmakefile')
+        filter_file(r'^PRECISION\s*=.*',
+                    'PRECISION = {0}'.format(spec.variants['prec'].value),
+                    'GNUmakefile')
+        filter_file(r'^DIM\s*=.*',
+                    'DIM = {0}'.format(spec.variants['dims'].value),
+                    'GNUmakefile')
+        filter_file(r'^PROFILE\s*=.*',
+                    'PROFILE = FALSE',
+                    'GNUmakefile')
+        filter_file(r'^TRACE_PROFILE\s*=.*',
+                    'TRACE_PROFILE = FALSE',
+                    'GNUmakefile')
+        filter_file(r'^COMM_PROFILE\s*=.*',
+                    'COMM_PROFILE = FALSE',
+                    'GNUmakefile')
+        # Only doing gcc at the moment
+        filter_file(r'^COMP\s*=.*',
+                    'COMP = gnu',
+                    'GNUmakefile')
+        filter_file(r'^DEBUG\s*=.*',
+                    'DEBUG = {0}'.format(spec.variants['debug'].value).upper(),
+                    'GNUmakefile')
+        filter_file(r'^USE_ARRAYVIEW\s*=.*',
+                    'USE_ARRAY_VIEW = FALSE',
+                    'GNUmakefile')
+        filter_file(r'^USE_MPI\s*=.*',
+                    'USE_MPI = {0}'.format(spec.variants['mpi'].value).upper(),
+                    'GNUmakefile')
+        filter_file(r'^USE_CXX11\s*=.*',
+                    'USE_CXX11 = TRUE',
+                    'GNUmakefile')
+        filter_file(r'^USE_VOLRENDER\s*=.*',
+                    'USE_VOLRENDER = FALSE',
+                    'GNUmakefile')
+        filter_file(r'^USE_PARALLELVOLRENDER\s*=.*',
+                    'USE_PARALLELVOLRENDER = FALSE',
+                    'GNUmakefile')
+        filter_file(r'^USE_PROFPARSER\s*=.*',
+                    'USE_PROFPARSER = FALSE',
+                    'GNUmakefile')
+
+        # Delete all /usr and /opt X library default search paths in makefile
+        filter_file(r'^.*\b(usr|opt)\b.*$',
+                    '##### Spack removed all INCLUDE_LOCATIONS and LIBRARY_LOCATIONS',
+                    'GNUmakefile')
+
+        # Read GNUmakefile into array
+        with open('GNUmakefile', 'r') as file:
+            contents = file.readlines()
+        
+        # Edit GNUmakefile INCLUDES and LIBRARIES to use Spack dependencies
+        line_offset = 63
+        contents.insert(line_offset+1, 'LIBRARY_LOCATIONS += {0}\n'.format(spec['libsm'].prefix.lib))
+        contents.insert(line_offset+2, 'INCLUDE_LOCATIONS += {0}\n'.format(spec['libsm'].prefix.include))
+        contents.insert(line_offset+3, 'LIBRARY_LOCATIONS += {0}\n'.format(spec['libice'].prefix.lib))
+        contents.insert(line_offset+4, 'INCLUDE_LOCATIONS += {0}\n'.format(spec['libice'].prefix.include))
+        contents.insert(line_offset+5, 'LIBRARY_LOCATIONS += {0}\n'.format(spec['libxpm'].prefix.lib))
+        contents.insert(line_offset+6, 'INCLUDE_LOCATIONS += {0}\n'.format(spec['libxpm'].prefix.include))
+        contents.insert(line_offset+7, 'LIBRARY_LOCATIONS += {0}\n'.format(spec['libx11'].prefix.lib))
+        contents.insert(line_offset+8, 'INCLUDE_LOCATIONS += {0}\n'.format(spec['libx11'].prefix.include))
+        contents.insert(line_offset+9, 'LIBRARY_LOCATIONS += {0}\n'.format(spec['libxt'].prefix.lib))
+        contents.insert(line_offset+10, 'INCLUDE_LOCATIONS += {0}\n'.format(spec['libxt'].prefix.include))
+        contents.insert(line_offset+11, 'LIBRARY_LOCATIONS += {0}\n'.format(spec['libxext'].prefix.lib))
+        contents.insert(line_offset+12, 'INCLUDE_LOCATIONS += {0}\n'.format(spec['libxext'].prefix.include))
+        contents.insert(line_offset+13, 'LIBRARY_LOCATIONS += {0}\n'.format(spec['motif'].prefix.lib))
+        contents.insert(line_offset+14, 'INCLUDE_LOCATIONS += {0}\n'.format(spec['motif'].prefix.include))
+        
+        # Write GNUmakefile
+        with open('GNUmakefile', 'w') as file:
+            file.writelines(contents)
+
+    def setup_environment(self, build_env, run_env):
+        if '+mpi' in self.spec:
+           # The location of the system MPI tree
+           build_env.set('MPI_HOME', self.spec['mpi'].prefix)
+           build_env.set('MPIHOME', self.spec['mpi'].prefix)
+
+    def install(self, spec, prefix):
+        exe = 'amrvis{0}d.gnu.ex'.format(spec.variants['dims'].value)
+        mkdirp(prefix.bin)
+        install(exe, prefix.bin)
