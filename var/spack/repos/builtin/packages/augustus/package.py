5a2be7f84f83a54b7437183109b8b05aa87417a7
Roberto Villegas-Diaz <Roberto.VillegasDiaz@sdstate.edu>
2018-06-27 15:40:06 -0500

    Augustus - Version-specific URLs replaced by list_url

--- var/spack/repos/builtin/packages/augustus/package.py
+++ var/spack/repos/builtin/packages/augustus/package.py
@@ -31,13 +31,7 @@ class Augustus(MakefilePackage):
 
     homepage = "http://bioinf.uni-greifswald.de/augustus/"
     url      = "http://bioinf.uni-greifswald.de/augustus/binaries/augustus-3.3.1.tar.gz"
-
-    version('3.3.1', '8363ece221c799eb169f47e545efb951',
-            url='http://bioinf.uni-greifswald.de/augustus/binaries/augustus-3.3.1.tar.gz')
-    version('3.3', '93691d9aafc7d3d0e1adf31ec308507f',
-            url='http://bioinf.uni-greifswald.de/augustus/binaries/old/augustus-3.3.tar.gz')
-    version('3.2.3', 'b8c47ea8d0c45aa7bb9a82626c8ff830',
-            url='http://bioinf.uni-greifswald.de/augustus/binaries/old/augustus-3.2.3.tar.gz')
+    list_url = "http://bioinf.uni-greifswald.de/augustus/binaries/old"
 
     depends_on('bamtools')
     depends_on('gsl')
