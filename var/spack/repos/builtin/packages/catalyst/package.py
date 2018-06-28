a1248447953e1fedc14b3860ca2536ebd2c4a387
Mark Olesen <Mark.Olesen@esi-group.com>
2018-06-28 08:57:34 +0200

    - Handle updated library paths for catalyst.
      Versions 5.4 and old places libraries under a paraview subdirectory.
      Eg, "lib/paraview-5.4", they are now placed directly under "lib"
    
    - Minor code style cleanup

--- var/spack/repos/builtin/packages/catalyst/package.py
+++ var/spack/repos/builtin/packages/catalyst/package.py
@@ -54,10 +54,10 @@ class Catalyst(CMakePackage):
     variant('python', default=False, description='Enable Python support')
     variant('essentials', default=False, description='Enable Essentials support')
     variant('extras', default=False, description='Enable Extras support')
-    variant('rendering', default=False, description='Enable Vtk Rendering support')
-    variant('expat', default=True, description="Use external EXPAT")
+    variant('rendering', default=False, description='Enable VTK Rendering support')
+    variant('expat', default=True, description="Use external expat")
 
-    depends_on('git')
+    depends_on('git', type='build')
     depends_on('mpi')
     depends_on('expat', when='+expat')
     depends_on('python@2:2.8', when='+python')
@@ -76,6 +76,30 @@ class Catalyst(CMakePackage):
         else:
             return self._urlfmt.format(version.up_to(2), version, '')
 
+    @property
+    def paraview_subdir(self):
+        """The paraview subdirectory name as paraview-major.minor"""
+        return 'paraview-{0}'.format(self.spec.version.up_to(2))
+
+    @property
+    def editions(self):
+        """Transcribe spack variants into names of Catalyst Editions"""
+        selected = ['Base']  # Always required
+
+        if '+python' in self.spec:
+            selected.append('Enable-Python')
+
+        if '+essentials' in self.spec:
+            selected.append('Essentials')
+
+        if '+extras' in self.spec:
+            selected.append('Extras')
+
+        if '+rendering' in self.spec:
+            selected.append('Rendering-Base')
+
+        return selected
+
     def do_stage(self, mirror_only=False):
         """Unpacks and expands the fetched tarball.
         Then, generate the catalyst source files."""
@@ -85,38 +109,15 @@ class Catalyst(CMakePackage):
         paraview_dir = os.path.join(self.stage.path,
                                     'ParaView-v' + str(self.version))
         catalyst_script = os.path.join(paraview_dir, 'Catalyst', 'catalyze.py')
+        editions_dir = os.path.join(paraview_dir, 'Catalyst', 'Editions')
         catalyst_source_dir = os.path.abspath(self.root_cmakelists_dir)
 
         command = ['python', catalyst_script,
-                   '-r', paraview_dir]
+                   '-r', paraview_dir,
+                   '-o', catalyst_source_dir]
 
-        catalyst_edition = os.path.join(paraview_dir, 'Catalyst',
-                                        'Editions', 'Base')
-        command.append('-i')
-        command.append(catalyst_edition)
-        if '+python' in self.spec:
-            catalyst_edition = os.path.join(paraview_dir, 'Catalyst',
-                                            'Editions', 'Enable-Python')
-            command.append('-i')
-            command.append(catalyst_edition)
-        if '+essentials' in self.spec:
-            catalyst_edition = os.path.join(paraview_dir, 'Catalyst',
-                                            'Editions', 'Essentials')
-            command.append('-i')
-            command.append(catalyst_edition)
-        if '+extras' in self.spec:
-            catalyst_edition = os.path.join(paraview_dir, 'Catalyst',
-                                            'Editions', 'Extras')
-            command.append('-i')
-            command.append(catalyst_edition)
-        if '+rendering' in self.spec:
-            catalyst_edition = os.path.join(paraview_dir, 'Catalyst',
-                                            'Editions', 'Rendering-Base')
-            command.append('-i')
-            command.append(catalyst_edition)
-
-        command.append('-o')
-        command.append(catalyst_source_dir)
+        for edition in self.editions:
+            command.extend(['-i', os.path.join(editions_dir, edition)])
 
         if not os.path.isdir(catalyst_source_dir):
             os.mkdir(catalyst_source_dir)
@@ -126,17 +127,41 @@ class Catalyst(CMakePackage):
             tty.msg("Already generated %s in %s" % (self.name,
                                                     self.stage.path))
 
+    @when('@:5.4.1')
     def setup_environment(self, spack_env, run_env):
+        # paraview 5.4 and earlier
+        # - libs  under lib/paraview-5.4
         if os.path.isdir(self.prefix.lib64):
             lib_dir = self.prefix.lib64
         else:
             lib_dir = self.prefix.lib
-        paraview_version = 'paraview-%s' % self.spec.version.up_to(2)
-        run_env.prepend_path('ParaView_DIR', self.prefix)
-        run_env.prepend_path('LIBRARY_PATH', join_path(lib_dir,
-                             paraview_version))
-        run_env.prepend_path('LD_LIBRARY_PATH', join_path(lib_dir,
-                             paraview_version))
+
+        run_env.set('ParaView_DIR', self.prefix)
+
+        # Everything else under lib/paraview-5.4
+        lib_dir = join_path(lib_dir, paraview_subdir)
+        run_env.prepend_path('LIBRARY_PATH', lib_dir)
+        run_env.prepend_path('LD_LIBRARY_PATH', lib_dir)
+
+    def setup_environment(self, spack_env, run_env):
+        # paraview 5.5 and later
+        # - cmake under lib/cmake/paraview-5.5
+        # - libs  under lib
+        # - python bits under lib/python2.8/site-packages
+        if os.path.isdir(self.prefix.lib64):
+            lib_dir = self.prefix.lib64
+        else:
+            lib_dir = self.prefix.lib
+
+        run_env.set('ParaView_DIR', self.prefix)
+        run_env.prepend_path('LIBRARY_PATH', lib_dir)
+        run_env.prepend_path('LD_LIBRARY_PATH', lib_dir)
+
+        if '+python' in self.spec:
+            python_version = self.spec['python'].version.up_to(2)
+            run_env.prepend_path('PYTHONPATH', join_path(lib_dir,
+                                 'python{0}'.format(python_version),
+                                 'site-packages'))
 
     @property
     def root_cmakelists_dir(self):
@@ -170,7 +195,7 @@ class Catalyst(CMakePackage):
 
         def nvariant_bool(feature):
             """Negated ternary for spec variant to OFF/ON string"""
-            return variant_bool(feature, on='OFF', off='ON')        
+            return variant_bool(feature, on='OFF', off='ON')
 
         cmake_args = [
             '-DPARAVIEW_GIT_DESCRIBE=v%s' % str(self.version),
