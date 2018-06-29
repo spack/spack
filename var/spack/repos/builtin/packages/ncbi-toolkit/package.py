153dd51d3c000bbc7546866ba999c670131504a1
lingnanyuan <1297162327@qq.com>
2018-06-29 14:14:21 -0500

    Add version constraints and change format.

--- var/spack/repos/builtin/packages/ncbi-toolkit/package.py
+++ var/spack/repos/builtin/packages/ncbi-toolkit/package.py
@@ -23,7 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import re
 
 
 class NcbiToolkit(AutotoolsPackage):
@@ -34,31 +33,32 @@ class NcbiToolkit(AutotoolsPackage):
 
     version('21_0_0', '14e021e08b1a78ac9cde98d0cab92098')
 
-    depends_on('boost@1.57.0')
+    depends_on('boost@1.35.0:')
     depends_on('bzip2')
     depends_on('libjpeg')
     depends_on('libpng')
     depends_on('libtiff')
     depends_on('libxml2')
-    depends_on('libxslt')
+    depends_on('libxslt@1.1.14:')
     depends_on('lzo')
     depends_on('pcre')
     depends_on('giflib')
-    depends_on('sqlite')
+    depends_on('sqlite@3.6.6')
     depends_on('zlib')
     depends_on('samtools')
     depends_on('bamtools')
 
     def configure_args(self):
-        return['--without-sybase', '--without-fastcgi']
+        return ['--without-sybase', '--without-fastcgi']
+
+    def patch(self):
+        with working_dir(join_path('src', 'util', 'image')):
+            filter_file(r'jpeg_start_compress(&cinfo, true)',
+                        'jpeg_start_compress(&cinfo, TRUE)',
+                        'image_io_jpeg.cpp', string=True)
 
     def build(self, spec, prefix):
-        with working_dir(join_path('src', 'util', 'image')):
-            filter_file(r'jpeg_start_compress\(\&cinfo, true\)',
-                        'jpeg_start_compress(&cinfo, TRUE)',
-                        'image_io_jpeg.cpp')
-
-        CompilerVersion = re.sub(r'\.', "", self.compiler.version.string)
+        CompilerVersion = self.compiler.version.joined
 
         with working_dir(join_path(
                 'GCC{0}-DebugMT64'.format(CompilerVersion), 'build')):
