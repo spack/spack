74383f6f66dec7ee8d098bcc03238553bf3d75fa
Jianwen WEI <weijianwen@gmail.com>
2018-06-30 14:15:54 +0800

    Update log4cplus versions.

--- var/spack/repos/builtin/packages/log4cplus/package.py
+++ var/spack/repos/builtin/packages/log4cplus/package.py
@@ -31,7 +31,7 @@ class Log4cplus(CMakePackage):
     granular control over log management and configuration."""
 
     homepage = "https://sourceforge.net/projects/log4cplus/"
-    url      = "https://download.sourceforge.net/project/log4cplus/log4cplus-stable/1.0.3/log4cplus-1.0.3.tar.bz2"
+    url      = "https://download.sourceforge.net/project/log4cplus/log4cplus-stable/2.0.1/log4cplus-2.0.1.tar.bz2"
 
     version('2.0.1', 'ec01c03241ebd31127a44d1880830d8f')
     version('1.2.0', '7f98418ab25273a5b44cb377b54cbbfb')
