6f7e012cab40caf2c89006da10790c4c9bf6ee19
Matthias Diener <matthias.diener@gmail.com>
2018-07-02 15:17:39 -0500

    byobu: update package (#8628)

--- var/spack/repos/builtin/packages/byobu/package.py
+++ var/spack/repos/builtin/packages/byobu/package.py
@@ -32,6 +32,7 @@ class Byobu(AutotoolsPackage):
     homepage = "http://www.byobu.co"
     url      = "https://launchpad.net/byobu/trunk/5.123/+download/byobu_5.123.orig.tar.gz"
 
+    version('5.125', 'f90f15771325e8be9360f301b52182d2')
     version('5.123', '961e0072c01c78c9ce4c20d1aa1b0dc4')
 
     depends_on('tmux', type=('build', 'run'))
