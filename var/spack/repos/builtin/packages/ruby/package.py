4b6335ad9be41cfae86f314efbbd34cadf9b0240
Adam J. Stewart <ajstewart426@gmail.com>
2018-07-05 21:38:53 -0500

    Remove Ruby 2.5.1, keep url_for_version and list_url

--- var/spack/repos/builtin/packages/ruby/package.py
+++ var/spack/repos/builtin/packages/ruby/package.py
@@ -30,11 +30,10 @@ class Ruby(AutotoolsPackage):
     simplicity and productivity."""
 
     homepage = "https://www.ruby-lang.org/"
-    url      = "http://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.1.tar.gz"
+    url      = "http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz"
     list_url = "http://cache.ruby-lang.org/pub/ruby/"
     list_depth = 1
 
-    version('2.5.1', '23867bc8c16c55e43b14dfe0614bcfa8')
     version('2.2.0', 'cd03b28fd0b555970f5c4fd481700852')
 
     variant('openssl', default=True, description="Enable OpenSSL support")
