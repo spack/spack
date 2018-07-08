9bd2c68496a73a181910df890ec5a684c3189295
Mehta, Kshitij V <kshitij-v-mehta@users.noreply.github.com>
2018-07-08 18:58:25 -0400

    gtkorvo-atl: Updated to version 2.2

--- var/spack/repos/builtin/packages/gtkorvo-atl/package.py
+++ var/spack/repos/builtin/packages/gtkorvo-atl/package.py
@@ -35,10 +35,14 @@ class GtkorvoAtl(CMakePackage):
 
     version('develop', git='https://github.com/GTkorvo/atl.git',
             branch='master')
+    version('2.2', 'f0e3581e4b4c6943bf4b203685630564')
     version('2.1', 'b2324ff041bccba127330a0e1b241978')
 
     depends_on('gtkorvo-cercs-env')
 
     def cmake_args(self):
-        args = ["-DENABLE_TESTING=0", "-DENABLE_BUILD_STATIC=STATIC"]
+        if self.spec.satisfies('@2.2:'):
+            args = ["-DENABLE_TESTING=0", "-DENABLE_BUILD_LIBS=OFF"]
+        else:
+            args = ["-DENABLE_TESTING=0", "-DENABLE_BUILD_STATIC=STATIC"]
         return args
