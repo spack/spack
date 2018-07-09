f15c59d16c8434688488b7353c591008473ce587
Levi Baber <baber@iastate.edu>
2018-07-09 10:41:24 -0500

    express: use libs property

--- var/spack/repos/builtin/packages/express/package.py
+++ var/spack/repos/builtin/packages/express/package.py
@@ -53,14 +53,8 @@ class Express(CMakePackage):
                                 'bamtools'].prefix.include.bamtools.api)
             edit = FileFilter('CMakeLists.txt')
             # really ugly way to escape the $ in CMakeLists.txt
-            edit.filter('{CMAKE_CURRENT_SOURCE_DIR}/../bamtools/lib/'
-                        'libbamtools.a', '$')
-            if 'bamtools@:2.4' in self.spec:
-                edit.filter('\\$\\$', '%s' % join_path(self.spec[
-                            'bamtools'].prefix.lib.bamtools, 'libbamtools.a'))
-            if 'bamtools@2.5:' in self.spec:
-                edit.filter('\\$\\$', '%s' % join_path(self.spec[
-                            'bamtools'].prefix.lib64, 'libbamtools.a'))
+            edit.filter('\${CMAKE_CURRENT_SOURCE_DIR}/../bamtools/lib/'
+                        'libbamtools.a', '%s' % self.spec['bamtools'].libs)
 
     def setup_environment(self, spack_env, run_env):
         spack_env.prepend_path('CPATH', self.spec[
