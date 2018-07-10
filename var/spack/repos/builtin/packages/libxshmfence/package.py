00c3331de92ca33e0783b0b80786832de0ecdc66
Michael Kuhn <suraia@ikkoku.de>
2018-07-10 14:58:44 +0200

    libxshmfence: Update to 1.3 (#8667)
    libxshmfence@1.2 does not compile with recent versions of glibc.

--- var/spack/repos/builtin/packages/libxshmfence/package.py
+++ var/spack/repos/builtin/packages/libxshmfence/package.py
@@ -33,9 +33,10 @@ class Libxshmfence(AutotoolsPackage):
     using file descriptor passing."""
 
     homepage = "https://cgit.freedesktop.org/xorg/lib/libxshmfence/"
-    url      = "http://xorg.freedesktop.org/archive/individual/lib/libxshmfence-1.2.tar.gz"
+    url      = "https://www.x.org/archive/individual/lib/libxshmfence-1.3.tar.bz2"
 
-    version('1.2', 'f0b30c0fc568b22ec524859ee28556f1')
+    version('1.3', '42dda8016943dc12aff2c03a036e0937')
+    version('1.2', '66662e76899112c0f99e22f2fc775a7e')
 
     depends_on('xproto', type='build')
     depends_on('pkgconfig', type='build')
