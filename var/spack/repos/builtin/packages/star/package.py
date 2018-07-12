ecff47538e7064c7c5529abf0213a1c9378c6010
Adam J. Stewart <ajstewart426@gmail.com>
2018-07-12 00:28:11 -0500

    Remove unnecessary version-specific URL overrides (#8693)

--- var/spack/repos/builtin/packages/star/package.py
+++ var/spack/repos/builtin/packages/star/package.py
@@ -31,8 +31,7 @@ class Star(Package):
     homepage = "https://github.com/alexdobin/STAR"
     url      = "https://github.com/alexdobin/STAR/archive/2.5.3a.tar.gz"
 
-    version('2.5.3a', 'baf8d1b62a50482cfa13acb7652dc391',
-            url='https://github.com/alexdobin/STAR/archive/2.5.3a.tar.gz')
+    version('2.5.3a', 'baf8d1b62a50482cfa13acb7652dc391')
     version('2.4.2a', '8b9345f2685a5ec30731e0868e86d506',
             url='https://github.com/alexdobin/STAR/archive/STAR_2.4.2a.tar.gz')
 
