1d8644202c3880685d800e7ff0d4dfbb482b5ece
Levi Baber <baber@iastate.edu>
2018-07-17 12:46:18 -0500

    py-biom-format: needs py-pyqi

--- var/spack/repos/builtin/packages/py-biom-format/package.py
+++ var/spack/repos/builtin/packages/py-biom-format/package.py
@@ -46,3 +46,4 @@ class PyBiomFormat(PythonPackage):
     depends_on('py-scipy@0.13.0:', type=('build', 'run'))
     depends_on('py-pandas@0.19.2:', type=('build', 'run'))
     depends_on('py-six@1.10.0:', type=('build', 'run'))
+    depends_on('py-pyqi', type=('build', 'run'))
