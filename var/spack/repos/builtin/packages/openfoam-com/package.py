c6f277d6a32d689a82780a862ecd7a96c68f0a0c
Simone Bna <simone.bna@cineca.it>
2018-07-19 10:22:33 +0200

    openfoam-com: added missing env variables to module generation

--- var/spack/repos/builtin/packages/openfoam-com/package.py
+++ var/spack/repos/builtin/packages/openfoam-com/package.py
@@ -421,16 +421,16 @@ class OpenfoamCom(Package):
 
                         # Unneeded bits
                         # -------------
-                        'FOAM_SETTINGS',  # Do not use with modules
-                        'FOAM_INST_DIR',  # Old
-                        'FOAM_(APP|ETC|SRC|SOLVERS|UTILITIES)',
+                        #'FOAM_SETTINGS',  # Do not use with modules
+                        #'FOAM_INST_DIR',  # Old
+                        #'FOAM_(APP|ETC|SRC|SOLVERS|UTILITIES)',
                         # 'FOAM_TUTORIALS',  # can be useful
-                        'WM_OSTYPE',      # Purely optional value
+                        #'WM_OSTYPE',      # Purely optional value
 
                         # Third-party cruft - only used for orig compilation
                         # -----------------
                         '[A-Z].*_ARCH_PATH',
-                        '(KAHIP|METIS|SCOTCH)_VERSION',
+                        #'(KAHIP|METIS|SCOTCH)_VERSION',
 
                         # User-specific
                         # -------------
