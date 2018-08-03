b726530ba8a2e5d0154d6f8a4c652d0d188261b8
Jianwen WEI <weijianwen@gmail.com>
2018-08-03 10:02:28 +0800

    Replace copy_tree with install_tree.

--- var/spack/repos/builtin/packages/dmd/package.py
+++ var/spack/repos/builtin/packages/dmd/package.py
@@ -23,7 +23,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-from distutils.dir_util import copy_tree
 
 
 class Dmd(MakefilePackage):
@@ -49,4 +48,4 @@ class Dmd(MakefilePackage):
 
     def install(self, spec, prefix):
         make('-f', 'posix.mak', 'install', 'AUTO_BOOTSTRAP=1')
-        copy_tree('src', prefix.src)
+        install_tree('src', prefix.src)
