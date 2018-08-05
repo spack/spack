5f7d9c9082ea832748439fc30591e94b4e5e5d38
Jianwen WEI <weijianwen@gmail.com>
2018-08-06 01:15:08 +0800

    Update dmd.

--- var/spack/repos/builtin/packages/dmd/package.py
+++ var/spack/repos/builtin/packages/dmd/package.py
@@ -23,6 +23,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import shutil
+import glob
 
 
 class Dmd(MakefilePackage):
@@ -36,16 +38,51 @@ class Dmd(MakefilePackage):
     depends_on('openssl')
     depends_on('curl')
 
+    # https://wiki.dlang.org/Building_under_Posix
+    resource(name='druntime',
+             url='https://github.com/dlang/druntime/archive/v2.081.1.tar.gz',
+             md5='49c8ba48fcb1e53d553a52d8ed7f9164',
+             placement='druntime')
+    resource(name='phobos',
+             url='https://github.com/dlang/phobos/archive/v2.081.1.tar.gz',
+             md5='ccf4787275b490eb2ddfc6713f9e9882',
+             placement='phobos')
+    resource(name='tools',
+             url='https://github.com/dlang/tools/archive/v2.081.1.tar.gz',
+             md5='a3bc7ed3d60b39712ef011bf19b3d427',
+             placement='tools')
+
     def setup_environment(self, spack_env, run_env):
         run_env.prepend_path('PATH', self.prefix.linux.bin64)
 
+    def do_stage(self, mirror_only=False):
+        # wrap (decorate) the standard expand_archive step with a
+        # helper, then call the real do_stage().
+        self.stage.expand_archive = self.unpack_dmd(self.stage.expand_archive)
+        super(Dmd, self).do_stage(mirror_only)
+
+    def unpack_dmd(self, f):
+        def wrap():
+            f() # call the original expand_archive()
+            with working_dir(self.stage.path):
+                dir_dmd = glob.glob(join_path('dmd*'))[0]
+                # mkdir = which('mkdir')
+                # mkdir('dmd')
+                # mv =  which('mv')
+                # mv(join_path(dir_dmd, '*'), 'dmd')
+                # shutil.move(join_path(dir_dmd, '*'), 'dmd')
+        return wrap
+
     def edit(self, spec, prefix):
-        makefile = FileFilter('posix.mak')
+        makefile = FileFilter('dmd/posix.mak')
         makefile.filter('$(PWD)/../install', prefix, string=True)
 
     def build(self, spec, prefix):
-        make('-f', 'posix.mak', 'AUTO_BOOTSTRAP=1')
+        with working_dir('dmd'):
+            make('-f', 'posix.mak', 'AUTO_BOOTSTRAP=1')
+        with working_dir('phobos'):
+            make('-f', 'posix.mak')
 
     def install(self, spec, prefix):
-        make('-f', 'posix.mak', 'install', 'AUTO_BOOTSTRAP=1')
-        install_tree('src', prefix.src)
+        with working_dir('dmd'):
+            make('-f', 'posix.mak', 'install', 'AUTO_BOOTSTRAP=1')
