9a4da2894edc23759fd74d3c2578155e9f6c30cb
hartzell <hartzell@alerce.com>
2018-08-10 16:24:21 -0700

    Switch to MakefilePackage and clean up a bit
    - Clean up the description
    - Switch to MakefilePackage
      - generalize the path to the chosen Makefile
      - implement separate `edit`, `build`, and `install` phases.

--- var/spack/repos/builtin/packages/fasta/package.py
+++ var/spack/repos/builtin/packages/fasta/package.py
@@ -25,14 +25,15 @@
 from spack import *
 
 
-class Fasta(Package):
+class Fasta(MakefilePackage):
     """The FASTA programs find regions of local or global similarity
-between Protein or DNA sequences, either by searching Protein or DNA
-databases, or by identifying local duplications within a
-sequence. Other programs provide information on the statistical
-significance of an alignment. Like BLAST, FASTA can be used to infer
-functional and evolutionary relationships between sequences as well as
-help identify members of gene families."""
+    between Protein or DNA sequences, either by searching Protein or
+    DNA databases, or by identifying local duplications within a
+    sequence. Other programs provide information on the statistical
+    significance of an alignment. Like BLAST, FASTA can be used to
+    infer functional and evolutionary relationships between sequences
+    as well as help identify members of gene families.
+    """
 
     homepage = "https://fasta.bioch.virginia.edu/fasta_www2/fasta_list2.shtml"
     url      = "https://github.com/wrpearson/fasta36/archive/fasta-v36.3.8g.tar.gz"
@@ -58,13 +59,19 @@ Linux x86_64
 ''')
         return name
 
-    def install(self, spec, prefix):
-        with working_dir('make'):
-            makefile = FileFilter(self.makefile_name)
-            makefile.filter('XDIR = .*', 'XDIR = {}'.format(prefix.bin))
+    @property
+    def makefile_path(self):
+        return join_path(self.stage.source_path, 'make', self.makefile_name)
 
+    def edit(self, spec, prefix):
+        makefile = FileFilter(self.makefile_path)
+        makefile.filter('XDIR = .*', 'XDIR = {}'.format(prefix.bin))
+
+    def build(self, spec, prefix):
         with working_dir('src'):
-            makefile_path = join_path('..', 'make', self.makefile_name)
-            make('-f', makefile_path)
+            make('-f', self.makefile_path)
+
+    def install(self, spec, prefix):
+        with working_dir('src'):
             mkdir(prefix.bin)
-            make('-f', makefile_path, 'install')
+            make('-f', self.makefile_path, 'install')
