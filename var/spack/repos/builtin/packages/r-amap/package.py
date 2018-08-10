9af11b3cd05f03d85ed10750a06f4d6707e489d0
Levi Baber <baber@iastate.edu>
2018-08-10 13:34:36 -0500

    r-amap: add list_url

--- var/spack/repos/builtin/packages/r-amap/package.py
+++ var/spack/repos/builtin/packages/r-amap/package.py
@@ -31,6 +31,7 @@ class RAmap(RPackage):
 
     homepage = "http://mulcyber.toulouse.inra.fr/projects/amap/"
     url      = "https://cran.rstudio.com/src/contrib/amap_0.8-16.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/amap/"
 
     version('0.8-16', sha256='d3775ad7f660581f7d2f070e426be95ae0d6743622943e6f5491988e5217d4e2')
 
