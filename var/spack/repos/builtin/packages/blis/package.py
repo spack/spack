f4a60cd7013b0eaed09d6c687819881717fbcd9c
s-sajid-ali <sajidsyed2021@u.northwestern.edu>
2018-08-14 09:19:56 -0500

    modified:   var/spack/repos/builtin/packages/blis/package.py

--- var/spack/repos/builtin/packages/blis/package.py
+++ var/spack/repos/builtin/packages/blis/package.py
@@ -116,7 +116,7 @@ class Blis(Package):
         else:
             config_args.append("--disable-static")
 
-        config_args.append("CC=" + env['CC'])
+        #config_args.append("CC=intel")
 
         # FIXME: add cpu isa variants.
         config_args.append("auto")
