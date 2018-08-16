d69ec9b4828bbbf87708d113c3f5b05e7e108491
Jonas Thies <Jonas.Thies@DLR.de>
2018-08-16 13:17:20 -0700

    packages/xsdk: add a comment reg. updating phist dependency

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -96,7 +96,8 @@ class Xsdk(Package):
     
     # the Fortran 2003 bindings of phist require python@3:, but this
     # creates a conflict with other packages like petsc@develop. Actually
-    # these are type='build' dependencies, but spack reports a conflict anyway. 
+    # these are type='build' dependencies, but spack reports a conflict anyway.
+    # This will be fixed once the new concretizer becomes available (says @adamjsteward)
     depends_on('phist@develop ~fortran', when='@develop')
 
     # xSDKTrilinos depends on the version of Trilinos built with
