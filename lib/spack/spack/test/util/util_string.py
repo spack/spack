c0855d9bd54be07162435cec605a1ad5efe1e4d1
Todd Gamblin <tgamblin@llnl.gov>
2018-08-21 14:48:19 -0700

    tests: add tests for warnings in build output
    - add tests for build warnings and error messages
    - simplify some code in build environment with plural() function

--- lib/spack/spack/test/util/util_string.py
+++ lib/spack/spack/test/util/util_string.py
@@ -22,54 +22,12 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+from spack.util.string import plural
 
 
-def comma_list(sequence, article=''):
-    if type(sequence) != list:
-        sequence = list(sequence)
-
-    if not sequence:
-        return
-    elif len(sequence) == 1:
-        return sequence[0]
-    else:
-        out = ', '.join(str(s) for s in sequence[:-1])
-        if len(sequence) != 2:
-            out += ','   # oxford comma
-        out += ' '
-        if article:
-            out += article + ' '
-        out += str(sequence[-1])
-        return out
-
-
-def comma_or(sequence):
-    return comma_list(sequence, 'or')
-
-
-def comma_and(sequence):
-    return comma_list(sequence, 'and')
-
-
-def quote(sequence, q="'"):
-    return ['%s%s%s' % (q, e, q) for e in sequence]
-
-
-def plural(n, singular, plural=None):
-    """Pluralize <singular> word by adding an s if n != 1.
-
-    Arguments:
-        n (int): number of things there are
-        singular (str): singular form of word
-        plural (str, optional): optional plural form, for when it's not just
-            singular + 's'
-
-    Returns:
-        (str): "1 thing" if n == 1 or "n things" if n != 1
-    """
-    if n == 1:
-        return "%d %s" % (n, singular)
-    elif plural is not None:
-        return "%d %s" % (n, plural)
-    else:
-        return "%d %ss" % (n, singular)
+def test_plural():
+    assert plural(0, 'thing') == '0 things'
+    assert plural(1, 'thing') == '1 thing'
+    assert plural(2, 'thing') == '2 things'
+    assert plural(1, 'thing', 'wombats') == '1 thing'
+    assert plural(2, 'thing', 'wombats') == '2 wombats'
