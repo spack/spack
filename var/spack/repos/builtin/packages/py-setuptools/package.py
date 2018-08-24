5d2c0ac855b8cf94d58fa680d179debbbd0c0361
Adam J. Stewart <ajstewart426@gmail.com>
2018-08-23 23:28:56 -0500

    Update to latest version of pytest and dependencies

--- var/spack/repos/builtin/packages/py-setuptools/package.py
+++ var/spack/repos/builtin/packages/py-setuptools/package.py
@@ -29,11 +29,18 @@ class PySetuptools(PythonPackage):
     """A Python utility that aids in the process of downloading, building,
        upgrading, installing, and uninstalling Python packages."""
 
-    homepage = "https://pypi.python.org/pypi/setuptools"
-    url      = "https://pypi.io/packages/source/s/setuptools/setuptools-39.0.1.zip"
+    homepage = "https://github.com/pypa/setuptools"
+    url      = "https://pypi.io/packages/source/s/setuptools/setuptools-40.2.0.zip"
 
-    import_modules = ['pkg_resources', 'setuptools', 'setuptools.command']
+    import_modules = [
+        'setuptools', 'pkg_resources', 'setuptools._vendor',
+        'setuptools.command', 'setuptools.extern',
+        'setuptools._vendor.packaging', 'pkg_resources._vendor',
+        'pkg_resources.extern', 'pkg_resources._vendor.packaging',
+        'easy_install'
+    ]
 
+    version('40.2.0', '592efabea3a65d8e97a025ed52f69b12')
     version('39.2.0', 'dd4e3fa83a21bf7bf9c51026dc8a4e59')
     version('39.0.1', '75310b72ca0ab4e673bf7679f69d7a62')
     version('35.0.2', 'c368b4970d3ad3eab5afe4ef4dbe2437')
@@ -48,7 +55,7 @@ class PySetuptools(PythonPackage):
     version('16.0',   '0ace0b96233516fc5f7c857d086aa3ad')
     version('11.3.1', '01f69212e019a2420c1693fb43593930')
 
-    depends_on('python@2.6:2.8,3.3:')
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
 
     # Previously, setuptools vendored all of its dependencies to allow
     # easy bootstrapping. As of version 34.0.0, this is no longer done
@@ -70,3 +77,7 @@ class PySetuptools(PythonPackage):
             url += '.tar.gz'
 
         return url
+
+    def test(self):
+        # Unit tests require pytest, creating a circular dependency
+        pass
