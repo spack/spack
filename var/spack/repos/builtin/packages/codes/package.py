ae6362626965387e89aec5820105565e71aebdda
Abhinav Bhatele <bhatele@llnl.gov>
2018-08-31 13:06:49 -0700

    CODES: remove -fi from extra autoreconf flags

--- var/spack/repos/builtin/packages/codes/package.py
+++ var/spack/repos/builtin/packages/codes/package.py
@@ -50,9 +50,8 @@ class Codes(AutotoolsPackage):
     depends_on('ross')
     depends_on('sst-dumpi', when="+dumpi")
 
-    # CODES documentation says to use these flags (--force, --install, and
     # add the local m4 directory to the search path
-    autoreconf_extra_args = ["-fi", "-Im4"]
+    autoreconf_extra_args = ["-Im4"]
     # Testing if srcdir is '.' in configure.ac does not work with spack
     patch('codes-1.0.0.patch')
 
