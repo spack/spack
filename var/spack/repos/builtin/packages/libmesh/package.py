fd807edd0741dd56e5408819438cc6310fcd564f
Josh Deaton <jdeaton@users.noreply.github.com>
2018-09-04 15:26:48 -0400

    libmesh: Added new versions.

--- var/spack/repos/builtin/packages/libmesh/package.py
+++ var/spack/repos/builtin/packages/libmesh/package.py
@@ -32,7 +32,10 @@ class Libmesh(Package):
 
     homepage = "http://libmesh.github.io/"
     url      = "https://github.com/libMesh/libmesh/releases/download/v1.0.0/libmesh-1.0.0.tar.bz2"
+    git      = "https://github.com/libMesh/libmesh.git"
 
+    version('1.3.0', sha256='a8cc2cd44f42b960989dba10fa438b04af5798c46db0b4ec3ed29591b8359786')
+    version('1.2.1', sha256='11c22c7d96874a17de6b8c74caa45d6745d40bf3610e88b2bd28fd3381f5ba70')
     version('1.0.0', 'cb464fc63ea0b71b1e69fa3f5d4f93a4')
 
     variant('mpi', default=True, description='Enables MPI parallelism')
