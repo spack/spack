20be9eca8800056464b60f34bf5076dab40c5f9b
snehring <snehring@iastate.edu>
2018-09-13 09:51:40 -0500

    ncbi-rmblastn: cleaning up version dependent stuff

--- var/spack/repos/builtin/packages/ncbi-rmblastn/package.py
+++ var/spack/repos/builtin/packages/ncbi-rmblastn/package.py
@@ -12,97 +12,96 @@ class NcbiRmblastn(AutotoolsPackage):
     homepage = "https://www.ncbi.nlm.nih.gov/"
     url      = "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-2.2.28-src.tar.gz"
 
-    version('2.2.28', 'fb5f4e2e02ffcb1b17af2e9f206c5c22')
     version('2.6.0', 'c8ce8055b10c4d774d995f88c7cc6225',
             url='ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.6.0/ncbi-blast-2.6.0+-src.tar.gz')
+    version('2.2.28', 'fb5f4e2e02ffcb1b17af2e9f206c5c22')
 
+    # This patch contains the changes by the repeatmasker authors
+    # to make ncbi-blast into rmblastn and is how they've decided
+    # to distribute version 2.6.0
     patch('isb-2.6.0+-changes-vers2.patch', when='@2.6.0', level=1)
 
     configure_directory = 'c++'
     build_directory = 'c++'
 
+    @when('@2.6.0')
     def configure_args(self):
-        if self.spec.version < Version('2.6.0'):
-            options = []
-        elif self.spec.version == Version('2.6.0'):
-            options = [
-                '--without-internal',
-                '--with-mt',
-                '--without-debug',
-                '--without-krb5',
-                '--without-check',
-                '--without-bz2',
-                '--without-lzo',
-                '--with-strip',
-                '--with-ncbi-public',
-                '--without-ncbi-c',
-                '--without-sss',
-                '--without-sssdb',
-                '--without-vdb',
-                '--without-pcre',
-                '--without-gcrypt',
-                '--without-nettle',
-                '--without-gnutls',
-                '--without-openssl',
-                '--without-sybase',
-                '--without-ftds',
-                '--without-mysql',
-                '--without-opengl',
-                '--without-mesa',
-                '--without-glut',
-                '--without-glew',
-                '--without-wxwidgets',
-                '--without-freetype',
-                '--without-ftgl',
-                '--without-fastcgi',
-                '--without-bdb',
-                '--without-sp',
-                '--without-orbacus',
-                '--without-sqlite3',
-                '--without-icu',
-                '--without-expat',
-                '--without-sablot',
-                '--without-libxml',
-                '--without-libxslt',
-                '--without-libexslt',
-                '--without-xerces',
-                '--without-xalan',
-                '--without-zorba',
-                '--without-oechem',
-                '--without-sge',
-                '--without-muparser',
-                '--without-hdf5',
-                '--without-gif',
-                '--without-png',
-                '--without-tiff',
-                '--without-xpm',
-                '--without-magic',
-                '--without-curl',
-                '--without-mimetic',
-                '--without-gsoap',
-                '--without-avro',
-                '--without-cereal',
-                '--without-sasl2',
-                '--without-mongodb',
-                '--without-gmock',
-                '--without-lapack',
-                '--without-lmdb',
-                '--without-3psw',
-                '--without-local-lbsm',
-                '--without-ncbi-crypt',
-                '--without-connext',
-                '--without-dbapi',
-                '--without-ctools',
-                '--without-gui',
-                '--without-gbench'
-            ]
+        options = [
+            '--without-internal',
+            '--with-mt',
+            '--without-debug',
+            '--without-krb5',
+            '--without-check',
+            '--without-bz2',
+            '--without-lzo',
+            '--with-strip',
+            '--with-ncbi-public',
+            '--without-ncbi-c',
+            '--without-sss',
+            '--without-sssdb',
+            '--without-vdb',
+            '--without-pcre',
+            '--without-gcrypt',
+            '--without-nettle',
+            '--without-gnutls',
+            '--without-openssl',
+            '--without-sybase',
+            '--without-ftds',
+            '--without-mysql',
+            '--without-opengl',
+            '--without-mesa',
+            '--without-glut',
+            '--without-glew',
+            '--without-wxwidgets',
+            '--without-freetype',
+            '--without-ftgl',
+            '--without-fastcgi',
+            '--without-bdb',
+            '--without-sp',
+            '--without-orbacus',
+            '--without-sqlite3',
+            '--without-icu',
+            '--without-expat',
+            '--without-sablot',
+            '--without-libxml',
+            '--without-libxslt',
+            '--without-libexslt',
+            '--without-xerces',
+            '--without-xalan',
+            '--without-zorba',
+            '--without-oechem',
+            '--without-sge',
+            '--without-muparser',
+            '--without-hdf5',
+            '--without-gif',
+            '--without-png',
+            '--without-tiff',
+            '--without-xpm',
+            '--without-magic',
+            '--without-curl',
+            '--without-mimetic',
+            '--without-gsoap',
+            '--without-avro',
+            '--without-cereal',
+            '--without-sasl2',
+            '--without-mongodb',
+            '--without-gmock',
+            '--without-lapack',
+            '--without-lmdb',
+            '--without-3psw',
+            '--without-local-lbsm',
+            '--without-ncbi-crypt',
+            '--without-connext',
+            '--without-dbapi',
+            '--without-ctools',
+            '--without-gui',
+            '--without-gbench'
+        ]
         return options
 
+    @when('@2.6.0')
     def install(self, spec, prefix):
-        # make install fails with this version, override
-        if spec.version == Version('2.6.0'):
-            with working_dir('c++'):
-                mkdirp(prefix.bin)
-                install_tree('ReleaseMT/bin', prefix.bin)
-        else:
-            super(NcbiRmblastn, self).install(spec, prefix)
+        # make install fails on 2.6.0, override
+        with working_dir('c++'):
+            mkdirp(prefix.bin)
+            install_tree('ReleaseMT/bin', prefix.bin)
