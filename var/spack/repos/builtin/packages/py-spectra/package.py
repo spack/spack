a83068b20a58b5bb025bc0cd51cba1964647c073
Levi Baber <baberlevi@gmail.com>
2018-09-14 17:35:21 -0500

    py-spectra: add sha256 indicator

--- var/spack/repos/builtin/packages/py-spectra/package.py
+++ var/spack/repos/builtin/packages/py-spectra/package.py
@@ -31,7 +31,7 @@ class PySpectra(PythonPackage):
     homepage = "https://pypi.python.org/pypi/spectra/0.0.8"
     url      = "https://pypi.io/packages/source/s/spectra/spectra-0.0.8.tar.gz"
 
-    version('0.0.11', '8eb362a5187cb63cee13cd01186799c0c791a3ad3bec57b279132e12521762b8')
+    version('0.0.11', sha256='8eb362a5187cb63cee13cd01186799c0c791a3ad3bec57b279132e12521762b8')
     version('0.0.8', '83020b29e584389f24c7720f38f0136c')
 
     depends_on('py-setuptools', type='build')
