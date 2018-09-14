3c33b5efef1456f5882e16509f3a2e60654da9dd
Levi Baber <baberlevi@gmail.com>
2018-09-14 17:52:02 -0500

    py-networkx: new version (#9241)

--- var/spack/repos/builtin/packages/py-networkx/package.py
+++ var/spack/repos/builtin/packages/py-networkx/package.py
@@ -31,8 +31,11 @@ class PyNetworkx(PythonPackage):
     homepage = "http://networkx.github.io/"
     url      = "https://pypi.io/packages/source/n/networkx/networkx-1.11.tar.gz"
 
-    version('1.11', '6ef584a879e9163013e9a762e1cf7cd1')
-    version('1.10', 'eb7a065e37250a4cc009919dacfe7a9d')
+    version('2.1', sha256='64272ca418972b70a196cb15d9c85a5a6041f09a2f32e0d30c0255f25d458bb1',
+            url='https://pypi.io/packages/source/n/networkx/networkx-2.1.zip')
+    version('1.11', md5='6ef584a879e9163013e9a762e1cf7cd1')
+    version('1.10', md5='eb7a065e37250a4cc009919dacfe7a9d')
 
     depends_on('py-decorator', type=('build', 'run'))
+    depends_on('py-decorator@4.1.0:', type=('build', 'run'), when='@2.1:')
     depends_on('py-setuptools', type='build')
