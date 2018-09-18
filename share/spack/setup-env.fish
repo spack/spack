9731110ddf46c224c3b682aca6f9803aa8cbadd6
Johannes Blaschke <jpblaschke@lbl.gov>
2018-09-18 15:13:23 -0700

    working on documentation

--- share/spack/setup-env.fish
+++ share/spack/setup-env.fish
@@ -41,7 +41,22 @@
 
 
 
-function stream_args
+#
+# STREAM_ARGS and SHIFT_ARGS: helper functions manipulating the `argv` array:
+#   -> STREAM_ARGS: echos the `argv` array element-by-element
+#   -> SHIFT_ARGS:  echos the `argv` array element-by-element starting with the
+#                   second element. If # `argv` has only one element, echo the
+#                   empty string `""`.
+# NOTE: while `stream_args` is not strictly necessary, it adds a nice symmetry
+#       to `shift_args`
+#
+
+
+function stream_args -d "echos args as a stream"
+    # return the elements of `$argv` as an array
+    #  -> since we want to be able to call it as part of `set x (shift_args
+    #     $x)`, we return these one-at-a-time using echo... this means that the
+    #     sub-command stream will correctly concatenate the output into an array
     for elt in $argv
         echo $elt
     end
@@ -52,8 +67,8 @@ end
 function shift_args -d "simulates bash shift"
     #
     # Returns argv[2..-1] (as an array)
-    #  -> if argv has only 1 element, then returns the empty string
-    # simulates the behavior of bash `shift`
+    #  -> if argv has only 1 element, then returns the empty string simulates
+    #     the behavior of bash `shift`
     #
 
     if test -z "$argv[2]"
@@ -68,7 +83,6 @@ function shift_args -d "simulates bash shift"
         for elt in $argv[2..-1]
             echo $elt
         end
-
     end
 
 end
@@ -76,10 +90,17 @@ end
 
 
 
+#
+# GET_SP_FLAGS, GET_MOD_ARGS, and CHECK_SP_FLAGS: support functions for
+# extracting and checking arguments and flags. Note bash's `shift` operation is
+# simulated by tracking the `remaining_args` array.
+#
+
 
 function get_sp_flags -d "return leading flags"
     #
-    # accumulate initial flags for main spack command
+    # Accumulate initial flags for main spack command. NOTE: Sets the external
+    # array: `remaining_args` containing all unprocessed arguments.
     #
 
     # initialize argument counter
@@ -116,6 +137,11 @@ end
 
 
 function get_mod_args -d "return submodule flags"
+    #
+    # Accumulate subcommand and submodule arguments. These are accumulated into
+    # the external arrays `sp_subcommand_args`, and `sp_module_args`. NOTE: Sets
+    # the external array: `remaining_args` containing all unprocessed arguments.
+    #
 
     set sp_subcommand_args ""
     set sp_module_args ""
@@ -155,8 +181,6 @@ end
 
 
 
-
-
 function check_sp_flags -d "check spack flags for h/V flags"
 
     # check if inputs contain h or V flags.
@@ -180,15 +204,12 @@ end
 
 
 
+#
+# SPACK wrapper function, preprocessing arguments and flags.
+#
+
+
 function spack -d "wrapper for the `spack` command"
-
-    #
-    # save raw arguments into an array before butchering them
-    #
-
-    set -l args $argv
-
-
 
     #
     # accumulate initial flags for main spack command
@@ -227,7 +248,8 @@ function spack -d "wrapper for the `spack` command"
 
 
     #
-    # Filter out use and unuse. For any other commands, just run the command.
+    # Filter out cd, and use and unuse (or similarly module's load and unload).
+    # For any other commands, just run the command.
     #
 
     switch $sp_subcommand
@@ -271,9 +293,10 @@ function spack -d "wrapper for the `spack` command"
             return
 
 
-            # CASE: spack subcommand is either `use`, `unuse`, `load`, or `unload`.
-            # These statements deal with the technical details of actually using
-            # modules.
+        # CASE: spack subcommand is either `use`, `unuse`, `load`, or `unload`.
+        # These statements deal with the technical details of actually using
+        # modules. Especially to deal with the substituting latest version
+        # numbers to the module command.
 
         case "use" or "unuse" or "load" or "unload"
 
