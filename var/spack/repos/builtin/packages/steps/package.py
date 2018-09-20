9f067250299dccbfb24e6cda6a947170f23a90b7
Tristan Carel <tristan.carel@gmail.com>
2018-09-20 16:00:24 +0200

    steps: new "coverage" variant to perform code coverage of tests suite

--- var/spack/repos/builtin/packages/steps/package.py
+++ var/spack/repos/builtin/packages/steps/package.py
@@ -20,13 +20,17 @@ class Steps(CMakePackage):
     variant("lapack", default=False, description="Use new BDSystem/Lapack code for E-Field solver")
     variant("petsc", default=False, description="Use PETSc library for parallel E-Field solver")
     variant("mpi", default=True, description="Use MPI for parallel solvers")
+    variant("coverage", default=False, description="Enable code coverage")
 
     depends_on("blas")
     depends_on("lapack", when="+lapack")
+    depends_on("lcov", when="+coverage", type="build")
     depends_on("mpi", when="+mpi")
-    depends_on("petsc~debug+int64", when="+petsc")
-    depends_on("python")
+    depends_on("petsc~debug+int64+mpi", when="+petsc+mpi")
+    depends_on("petsc~debug+int64~mpi", when="+petsc~mpi")
     depends_on("py-cython")
+    depends_on("py-gcovr", when="+coverage", type="build")
+    depends_on("python")
 
     def cmake_args(self):
         args = []
@@ -52,8 +56,29 @@ class Steps(CMakePackage):
         else:
             args.append("-DUSE_MPI:BOOL=False")
 
+        if "+coverage" in spec:
+            args.append("-DENABLE_CODECOVERAGE:BOOL=True")
+
         args.append('-DBLAS_LIBRARIES=' + spec['blas'].libs.joined(";"))
         return args
+
+    @property
+    def build_targets(self):
+        targets = []
+        if "+coverage" in self.spec:
+            if self.compiler.name != "gcc":
+                raise ValueError(
+                    "Package " + self.name +
+                    " build with coverage enabled requires GCC to build"
+                )
+            targets = [
+                "CTEST_OUTPUT_ON_FAILURE=1",
+                "all",  # build
+                "coverage_init",  # initialize coverage counters
+                "test",  # run tests suite
+                "coverage"  #  collect coverage counters and build reports
+            ]
+        return targets
 
     def setup_environment(self, spack_env, run_env):
         # This recipe exposes a Python package from a C++ CMake project.
