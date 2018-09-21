27568fe761ea725beddba5348bf8b2d8a0c8fd27
trecakov <trecakov@nmsu.edu>
2018-09-21 14:08:44 -0600

    new versions of r-dplyr,r-plogr,r-rlang

--- var/spack/repos/builtin/packages/r-dplyr/package.py
+++ var/spack/repos/builtin/packages/r-dplyr/package.py
@@ -29,15 +29,18 @@ class RDplyr(RPackage):
     """A fast, consistent tool for working with data frame like objects, both
     in memory and out of memory."""
 
-    homepage = "https://github.com/hadley/dplyr"
-    url      = "https://cran.r-project.org/src/contrib/dplyr_0.7.4.tar.gz"
+    homepage = "https://cran.r-project.org/package=dplyr"
+    url      = "https://cran.r-project.org/src/contrib/dplyr_0.7.6.tar.gz"
     list_url = "https://cran.r-project.org/src/contrib/Archive/dplyr"
 
+    version('0.7.5', sha256='2fbd8f316a59670076d43a0fe854654621941ee5f621ea5f0185a3f5daafda50')
     version('0.7.4', '9edee9b2db9831c2438054d0d2c1647d')
     version('0.7.3', 'f9760b796917747e9dcd927ebb531c7d')
     version('0.5.0', '1fcafcacca70806eea2e6d465cdb94ef')
 
+    depends_on('r-tidyselect@0.2.3', type=('build', 'run'))
     depends_on('r-assertthat', type=('build', 'run'))
+    depends_on('r-bindr@0.1.1', type=('build', 'run'))
     depends_on('r-r6', type=('build', 'run'))
     depends_on('r-rcpp', type=('build', 'run'))
     depends_on('r-tibble', type=('build', 'run'))
