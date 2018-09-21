96614c66c95d9ce7dbebea7755ffe9e8e5b50b46
Srinath Vadlamani <srinath.vadlamani@arm.com>
2018-09-20 22:58:49 -0600

    added arm.py

--- lib/spack/spack/compilers/arm.py
+++ lib/spack/spack/compilers/arm.py
@@ -1,9 +1,10 @@
 ##############################################################################
-# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC., et al.
 # Produced at the Lawrence Livermore National Laboratory.
 #
 # This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# Created by Todd Gamblin, tgamblin@llnl.gov, 
+# srinath.vadlamani@arm.com, Nick.Forrington@arm.com All rights reserved.
 # LLNL-CODE-647188
 #
 # For details, see https://github.com/spack/spack
