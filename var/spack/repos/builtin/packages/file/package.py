0d94552cd760b3f290aa005f99db02fcf13da40d
Tristan Carel <tristan.carel@gmail.com>
2018-09-21 15:16:49 +0200

    file: new package starting at 5.34

---
+++ var/spack/repos/builtin/packages/file/package.py
@@ -0,0 +1,34 @@
+##############################################################################
+# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class File(AutotoolsPackage):
+    """a file type guesser"""
+
+    homepage = "https://www.darwinsys.com/file"
+    url      = "ftp://ftp.astron.com/pub/file/file-5.34.tar.gz"
+
+    version('5.34', sha256='f15a50dbbfa83fec0bd1161e8e191b092ec832720e30cd14536e044ac623b20a')
