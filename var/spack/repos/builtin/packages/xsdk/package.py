b5cadfd124e22aafa00659ed7e72fd8917540f62
Satish Balay <balay@mcs.anl.gov>
2018-09-27 14:40:43 -0500

    xsdk-0.4.0: add/update package versions:   hypre@2.15.0   mfem@develop   superlu-dist@develop   trilinos@12.14.0-rc1   petsc@3.10.2   dealii@9.0.1   pflotran@develop   alquimia@develop   sundials@3.2.0   plasma@17.2   magma@2.4.0   amrex@18.10   slepc@3.10.0   omega-h@9.19.0   strumpack@master   pumi@develop   tasmanian@6.0

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -40,25 +40,31 @@ class Xsdk(Package):
     url      = 'http://ftp.mcs.anl.gov/pub/petsc/externalpackages/xsdk.tar.gz'
 
     version('develop', 'a52dc710c744afa0b71429b8ec9425bc')
-    version('0.3.0', 'a52dc710c744afa0b71429b8ec9425bc', preferred=True)
+    version('0.4.0', 'a52dc710c744afa0b71429b8ec9425bc', preferred=True)
+    version('0.3.0', 'a52dc710c744afa0b71429b8ec9425bc')
     version('xsdk-0.2.0', 'a52dc710c744afa0b71429b8ec9425bc')
 
     variant('debug', default=False, description='Compile in debug mode')
     variant('cuda', default=False, description='Enable CUDA dependent packages')
 
     depends_on('hypre@develop~internal-superlu', when='@develop')
+    depends_on('hypre@2.15.0~internal-superlu', when='@0.4.0')
     depends_on('hypre@2.12.1~internal-superlu', when='@0.3.0')
     depends_on('hypre@xsdk-0.2.0~internal-superlu', when='@xsdk-0.2.0')
 
     depends_on('mfem@develop+mpi+hypre+superlu-dist+petsc+sundials+examples+miniapps', when='@develop')
+    depends_on('mfem@develop+mpi+hypre+superlu-dist+petsc+sundials+examples+miniapps', when='@0.4.0')
     depends_on('mfem@3.3.2+mpi+hypre+superlu-dist+petsc+sundials+examples+miniapps', when='@0.3.0')
 
     depends_on('superlu-dist@develop', when='@develop')
+    depends_on('superlu-dist@develop', when='@0.4.0')
     depends_on('superlu-dist@5.2.2', when='@0.3.0')
     depends_on('superlu-dist@xsdk-0.2.0', when='@xsdk-0.2.0')
 
-    depends_on('trilinos@develop+hypre+superlu-dist+metis+hdf5~mumps+boost~suite-sparse+tpetra+nox+ifpack2+zoltan2+amesos2~exodus',
+    depends_on('trilinos@master+hypre+superlu-dist+metis+hdf5~mumps+boost~suite-sparse+tpetra+nox+ifpack2+zoltan2+amesos2~exodus',
                when='@develop')
+    depends_on('trilinos@12.14.0-rc1+hypre+superlu-dist+metis+hdf5~mumps+boost~suite-sparse+tpetra+nox+ifpack2+zoltan2+amesos2~exodus',
+               when='@0.4.0')
     depends_on('trilinos@12.12.1+hypre+superlu-dist+metis+hdf5~mumps+boost~suite-sparse~tpetra~ifpack2~zoltan2~amesos2~exodus',
                when='@0.3.0')
     depends_on('trilinos@xsdk-0.2.0+hypre+superlu-dist+metis+hdf5~mumps+boost~suite-sparse~tpetra~ifpack2~zoltan2~amesos2~exodus',
@@ -66,33 +72,56 @@ class Xsdk(Package):
 
     depends_on('petsc@develop+trilinos+mpi+hypre+superlu-dist+metis+hdf5~mumps+double~int64',
                when='@develop')
+    depends_on('petsc@3.10.2+trilinos+mpi+hypre+superlu-dist+metis+hdf5~mumps+double~int64',
+               when='@0.4.0')
     depends_on('petsc@3.8.2+trilinos+mpi+hypre+superlu-dist+metis+hdf5~mumps+double~int64',
                when='@0.3.0')
     depends_on('petsc@xsdk-0.2.0+trilinos+mpi+hypre+superlu-dist+metis+hdf5~mumps+double~int64',
                when='@xsdk-0.2.0')
 
     depends_on('dealii@develop~assimp~python~doc~slepc~gmsh+petsc+mpi+trilinos~int64+hdf5~netcdf+metis', when='@develop')
+    depends_on('dealii@9.0.1~assimp~python~doc~slepc~gmsh+petsc+mpi+trilinos~int64+hdf5~netcdf+metis', when='@0.4.0')
 
     depends_on('pflotran@develop', when='@develop')
+    depends_on('pflotran@develop', when='@0.4.0')
     depends_on('pflotran@xsdk-0.3.0', when='@0.3.0')
     depends_on('pflotran@xsdk-0.2.0', when='@xsdk-0.2.0')
 
     depends_on('alquimia@develop', when='@develop')
+    depends_on('alquimia@develop', when='@0.4.0')
     depends_on('alquimia@xsdk-0.3.0', when='@0.3.0')
     depends_on('alquimia@xsdk-0.2.0', when='@xsdk-0.2.0')
 
-    depends_on('sundials@3.1.0~int64+hypre', when='@develop')
+    depends_on('sundials@3.2.0~int64+hypre', when='@develop')
+    depends_on('sundials@3.2.0~int64+hypre', when='@0.4.0')
     depends_on('sundials@3.1.0~int64+hypre', when='@0.3.0')
 
     depends_on('plasma@17.2:', when='@develop %gcc@6.0:')
+    depends_on('plasma@17.2:', when='@0.4.0 %gcc@6.0:')
 
-    depends_on('magma@2.2.0', when='@develop +cuda')
+    depends_on('magma@2.4.0', when='@develop +cuda')
+    depends_on('magma@2.4.0', when='@0.4.0 +cuda')
     depends_on('magma@2.2.0', when='@0.3.0 +cuda')
 
     depends_on('amrex@develop', when='@develop %intel')
     depends_on('amrex@develop', when='@develop %gcc')
+    depends_on('amrex@18.10', when='@0.4.0 %intel')
+    depends_on('amrex@18.10', when='@0.4.0 %gcc')
 
     depends_on('slepc@develop', when='@develop')
+    depends_on('slepc@3.10.0', when='@0.4.0')
+
+    depends_on('omega-h@develop', when='@develop')
+    depends_on('omega-h@9.19.0', when='@0.4.0')
+
+    depends_on('strumpack@master', when='@develop')
+    depends_on('strumpack@master', when='@0.4.0')
+
+    depends_on('pumi@develop', when='@develop')
+    depends_on('pumi@develop', when='@0.4.0')
+
+    depends_on('tasmanian@develop+xsdkflags~openmp', when='@develop')
+    depends_on('tasmanian@6.0+xsdkflags~openmp', when='@0.4.0')
 
     # xSDKTrilinos depends on the version of Trilinos built with
     # +tpetra which is turned off for faster xSDK
