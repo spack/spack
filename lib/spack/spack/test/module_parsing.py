spack/spack

commit 22fbb3dba70283d9fdfe8e4e285815e4fafed7d8

Bug fix: module file path parsing (#9100)
Improve Spack's parsing of module show to eliminate some false
positives (e.g. accepting MODULEPATH when it is in fact looking for
PATH). This makes the following changes:

* Updates the pattern searching for several paths to avoid the case
  where they are prefixes of unwanted paths
* Adds a warning message when an extracted path doesn't exist (which
  may help catch future module parsing bugs faster)
* Adds a test with the content mentioned in #9083

lib/spack/spack/util/module_cmd.py
@@ -148,7 +148,7 @@ def load_module(mod):
     exec(compile(load, '<string>', 'exec'))
 
 
-def get_argument_from_module_line(line):
+def get_path_arg_from_module_line(line):
     if '(' in line and ')' in line:
         # Determine which lua quote symbol is being used for the argument
         comma_index = line.index(',')
@@ -160,9 +160,15 @@ def get_argument_from_module_line(line):
             # Change error text to describe what is going on.
             raise ValueError("No lua quote symbol found in lmod module line.")
         words_and_symbols = line.split(lua_quote)
-        return words_and_symbols[-2]
+        path_arg = words_and_symbols[-2]
     else:
-        return line.split()[2]
+        path_arg = line.split()[2]
+
+    if not os.path.exists(path_arg):
+        tty.warn("Extracted path from module does not exist:"
+                 "\n\tExtracted path: " + path_arg +
+                 "\n\tFull line: " + line)
+    return path_arg
 
 
 def get_path_from_module(mod):
@@ -175,16 +181,22 @@ def get_path_from_module(mod):
     # Read the module
     text = modulecmd('show', mod, output=str, error=str).split('\n')
 
+    return get_path_from_module_contents(text, mod)
+
+
+def get_path_from_module_contents(text, module_name):
     # If it sets the LD_LIBRARY_PATH or CRAY_LD_LIBRARY_PATH, use that
     for line in text:
-        if line.find('LD_LIBRARY_PATH') >= 0:
-            path = get_argument_from_module_line(line)
+        pattern = r'\WLD_LIBRARY_PATH'
+        if re.search(pattern, line):
+            path = get_path_arg_from_module_line(line)
             return path[:path.find('/lib')]
 
     # If it lists its package directory, return that
     for line in text:
-        if line.find(mod.upper() + '_DIR') >= 0:
-            return get_argument_from_module_line(line)
+        pattern = r'\W{0}_DIR'.format(module_name.upper())
+        if re.search(pattern, line):
+            return get_path_arg_from_module_line(line)
 
     # If it lists a -rpath instruction, use that
     for line in text:
@@ -200,8 +212,9 @@ def get_path_from_module(mod):
 
     # If it sets the PATH, use it
     for line in text:
-        if line.find('PATH') >= 0:
-            path = get_argument_from_module_line(line)
+        pattern = r'\WPATH'
+        if re.search(pattern, line):
+            path = get_path_arg_from_module_line(line)
             return path[:path.find('/bin')]
 
     # Unable to find module path

lib/spack/spack/test/module_parsing.py
@@ -25,10 +25,13 @@
 import pytest
 import subprocess
 import os
-from spack.util.module_cmd import get_path_from_module
-from spack.util.module_cmd import get_argument_from_module_line
-from spack.util.module_cmd import get_module_cmd_from_bash
-from spack.util.module_cmd import get_module_cmd, ModuleError
+from spack.util.module_cmd import (
+    get_path_from_module,
+    get_path_from_module_contents,
+    get_path_arg_from_module_line,
+    get_module_cmd_from_bash,
+    get_module_cmd,
+    ModuleError)
 
 
 typeset_func = subprocess.Popen('module avail',
@@ -73,6 +76,26 @@ def test_get_path_from_module(save_env):
     assert path is None
 
 
+def test_get_path_from_module_contents():
+    module_show_output = """
+os.environ["MODULEPATH"] = "/path/to/modules1:/path/to/modules2";
+----------------------------------------------------------------------------
+   /root/cmake/3.9.2.lua:
+----------------------------------------------------------------------------
+help([[CMake Version 3.9.2
+]])
+whatis("Name: CMake")
+whatis("Version: 3.9.2")
+whatis("Category: Tools")
+whatis("URL: https://cmake.org/")
+prepend_path("PATH","/path/to/cmake-3.9.2/bin")
+prepend_path("MANPATH","/path/to/cmake/cmake-3.9.2/share/man")
+"""
+    module_show_lines = module_show_output.split('\n')
+    assert (get_path_from_module_contents(module_show_lines, 'cmake-3.9.2') ==
+            '/path/to/cmake-3.9.2')
+
+
 def test_get_argument_from_module_line():
     lines = ['prepend-path LD_LIBRARY_PATH /lib/path',
              'prepend-path  LD_LIBRARY_PATH  /lib/path',
@@ -83,10 +106,10 @@ def test_get_argument_from_module_line():
     bad_lines = ['prepend_path(PATH,/lib/path)',
                  'prepend-path (LD_LIBRARY_PATH) /lib/path']
 
-    assert all(get_argument_from_module_line(l) == '/lib/path' for l in lines)
+    assert all(get_path_arg_from_module_line(l) == '/lib/path' for l in lines)
     for bl in bad_lines:
         with pytest.raises(ValueError):
-            get_argument_from_module_line(bl)
+            get_path_arg_from_module_line(bl)
 
 
 @pytest.mark.skipif(MODULE_NOT_DEFINED, reason='Depends on defined module fn')

commit 28f6a4a095fb5b0057b5b480157b2e2abaad9f1c

Module path parsing: CRAY_LD_LIBRARY_PATH and PACKAGE_DIR (#9374)
Fix two bugs with module file parsing:

* Detection of the CRAY_LD_LIBRARY_PATH variable was broken by #9100.
  This fixes it and adds a test for it.
* For module names like "foo-bar/1.0", the associated PACKAGE_DIR
  environment variable name would be "FOO_BAR_DIR", but Spack was not
  parsing the components and not converting "-" to "_"

lib/spack/spack/util/module_cmd.py
@@ -185,16 +185,25 @@ def get_path_from_module(mod):
 
 
 def get_path_from_module_contents(text, module_name):
+    tty.debug("Module name: " + module_name)
+    pkg_var_prefix = module_name.replace('-', '_').upper()
+    components = pkg_var_prefix.split('/')
+    # For modules with multiple components like foo/1.0.1, retrieve the package
+    # name "foo" from the module name
+    if len(components) > 1:
+        pkg_var_prefix = components[-2]
+    tty.debug("Package directory variable prefix: " + pkg_var_prefix)
+
     # If it sets the LD_LIBRARY_PATH or CRAY_LD_LIBRARY_PATH, use that
     for line in text:
-        pattern = r'\WLD_LIBRARY_PATH'
+        pattern = r'\W(CRAY_)?LD_LIBRARY_PATH'
         if re.search(pattern, line):
             path = get_path_arg_from_module_line(line)
             return path[:path.find('/lib')]
 
     # If it lists its package directory, return that
     for line in text:
-        pattern = r'\W{0}_DIR'.format(module_name.upper())
+        pattern = r'\W{0}_DIR'.format(pkg_var_prefix)
         if re.search(pattern, line):
             return get_path_arg_from_module_line(line)
 

lib/spack/spack/test/module_parsing.py
@@ -58,6 +58,7 @@ def save_env():
 
 def test_get_path_from_module(save_env):
     lines = ['prepend-path LD_LIBRARY_PATH /path/to/lib',
+             'prepend-path CRAY_LD_LIBRARY_PATH /path/to/lib',
              'setenv MOD_DIR /path/to',
              'setenv LDFLAGS -Wl,-rpath/path/to/lib',
              'setenv LDFLAGS -L/path/to/lib',
@@ -67,7 +68,6 @@ def test_get_path_from_module(save_env):
         module_func = '() { eval `echo ' + line + ' bash filler`\n}'
         os.environ['BASH_FUNC_module()'] = module_func
         path = get_path_from_module('mod')
-
         assert path == '/path/to'
 
     os.environ['BASH_FUNC_module()'] = '() { eval $(echo fill bash $*)\n}'
@@ -77,6 +77,8 @@ def test_get_path_from_module(save_env):
 
 
 def test_get_path_from_module_contents():
+    # A line with "MODULEPATH" appears early on, and the test confirms that it
+    # is not extracted as the package's path
     module_show_output = """
 os.environ["MODULEPATH"] = "/path/to/modules1:/path/to/modules2";
 ----------------------------------------------------------------------------
@@ -94,6 +96,16 @@ prepend_path("MANPATH","/path/to/cmake/cmake-3.9.2/share/man")
     module_show_lines = module_show_output.split('\n')
     assert (get_path_from_module_contents(module_show_lines, 'cmake-3.9.2') ==
             '/path/to/cmake-3.9.2')
+
+
+def test_pkg_dir_from_module_name():
+    module_show_lines = ['setenv FOO_BAR_DIR /path/to/foo-bar']
+
+    assert (get_path_from_module_contents(module_show_lines, 'foo-bar') ==
+            '/path/to/foo-bar')
+
+    assert (get_path_from_module_contents(module_show_lines, 'foo-bar/1.0') ==
+            '/path/to/foo-bar')
 
 
 def test_get_argument_from_module_line():