07c689e8c6483d337dabc33eb9933ae56b252072
Pieter Ghysels <pghysels@lbl.gov>
2018-10-04 11:25:15 -0700

    Update to STRUMPACK version 3.0.3. This adds a check for OpenMP task priority support (since OpenMP 4.5)

--- var/spack/repos/builtin/packages/strumpack/package.py
+++ var/spack/repos/builtin/packages/strumpack/package.py
@@ -38,10 +38,11 @@ class Strumpack(CMakePackage):
     iterative solvers."""
 
     homepage = "http://portal.nersc.gov/project/sparse/strumpack"
-    url      = "https://github.com/pghysels/STRUMPACK/archive/v3.0.2.tar.gz"
+    url      = "https://github.com/pghysels/STRUMPACK/archive/v3.0.3.tar.gz"
     git      = "https://github.com/pghysels/STRUMPACK.git"
 
     version('master', branch='master')
+    version('3.0.3', sha256='2bd2a40d9585b769ae4ba461de02c6e36433bf2b21827f824a50f2fdf73389f7')
     version('3.0.2', sha256='828e5ec59019b2c74e008745b04ceebbb7ef1313fb4e3ac01fa8ff350799df38')
     version('3.0.1', sha256='b4a4d870c589937e22e77a6c4b52a96fd808f0b564e363f826ae5ffc94b9d000')
     version('3.0.0', sha256='7acd9b4653b8b11380de733c80b164348ca00f9226904f5dc166a8e3db88cd20')
