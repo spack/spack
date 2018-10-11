b100d1fc4045a9bf743f7f7cbf85c93d3e9b1898
Piotr Luszczek <luszczek@icl.utk.edu>
2018-10-10 22:14:31 -0400

    Magma: add version 2.4.0 (#9478)

--- var/spack/repos/builtin/packages/magma/package.py
+++ var/spack/repos/builtin/packages/magma/package.py
@@ -35,8 +35,9 @@ class Magma(CMakePackage):
     homepage = "http://icl.cs.utk.edu/magma/"
     url = "http://icl.cs.utk.edu/projectsfiles/magma/downloads/magma-2.2.0.tar.gz"
 
-    version('2.3.0', '9aaf85a338d3a17303e0c69f86f0ec52')
-    version('2.2.0', '6c1ebf4cdf63eb302ff6258ff8c49217')
+    version('2.4.0', sha256='4eb839b1295405fd29c8a6f5b4ed578476010bf976af46573f80d1169f1f9a4f')
+    version('2.3.0', sha256='010a4a057d7aa1e57b9426bffc0958f3d06913c9151463737e289e67dd9ea608')
+    version('2.2.0', sha256='df5d4ace417e5bf52694eae0d91490c6bde4cde1b0da98e8d400c5c3a70d83a2')
 
     variant('fortran', default=True,
             description='Enable Fortran bindings support')
