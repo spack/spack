094afbc7a1b03d776adf36f8d2e69f428438e5ac
Piotr Luszczek <luszczek@icl.utk.edu>
2018-10-11 00:04:03 -0400

    PLASMA: add version 18.9.0 (w/CMake) and remove 17.1 (no config. system)

--- var/spack/repos/builtin/packages/plasma/package.py
+++ var/spack/repos/builtin/packages/plasma/package.py
@@ -11,7 +11,7 @@
 from spack import *
 
 
-class Plasma(MakefilePackage):
+class Plasma(CMakePackage):
     """Parallel Linear Algebra Software for Multicore Architectures, PLASMA is
     a software package for solving problems in dense linear algebra using
     multicore processors and Xeon Phi coprocessors. PLASMA provides
@@ -21,17 +21,17 @@ class Plasma(MakefilePackage):
     problems, and singular value problems."""
 
     homepage = "https://bitbucket.org/icl/plasma/"
-    url      = "https://bitbucket.org/icl/plasma/downloads/plasma-17.1.tar.gz"
-    hg       = "https://luszczek@bitbucket.org/icl/plasma"
+    url = "https://bitbucket.org/icl/plasma/downloads/plasma18.9.0.tar.gz"
+    hg = "https://luszczek@bitbucket.org/icl/plasma"
 
     version("develop", hg=hg)
-    version("17.1", "64b410b76023a41b3f07a5f0dca554e1")
+    version("18.9.0", sha256="753eae28ea48986a2cc7b8204d6eef646584541e59d42c3c94fa9879116b0774")
 
-    variant('shared', default=True, description="Build shared library (disables static library)")
+    variant("shared", default=True,
+            description="Build shared library (disables static library)")
 
     depends_on("blas")
     depends_on("lapack")
-    depends_on("readline", when='@17.2:')
 
     conflicts("atlas")  # does not have LAPACKE interface
 
@@ -57,65 +57,19 @@ class Plasma(MakefilePackage):
     conflicts("%xl")
     conflicts("%xl_r")
 
-    patch("remove_absolute_mkl_include.patch", when="@17.1")
-    patch("add_netlib_lapacke_detection.patch", when="@17.1")
+    def cmake_args(self):
+        options = list()
 
-    def getblaslapacklibs(self):
-        if '^netlib-lapack' in self.spec:
-            bl_attr = ':c,fortran'
-        else:
-            bl_attr = ''
-        return self.spec['lapack' + bl_attr].libs + \
-            self.spec['blas' + bl_attr].libs + \
-            find_system_libraries(['libm'])
+        options.extend([
+            "-DCMAKE_INSTALL_PREFIX=%s" % prefix,
+            "-DCMAKE_INSTALL_NAME_DIR:PATH=%s/lib" % prefix,
+            "-DBLAS_LIBRARIES=%s" % self.spec["blas"].libs.joined(";"),
+            "-DLAPACK_LIBRARIES=%s" % self.spec["lapack"].libs.joined(";")
+        ])
 
-    def edit(self, spec, prefix):
-        # copy "make.inc.mkl-gcc" provided by default into "make.inc"
-        open("make.inc", "w").write(open("make.inc.mkl-gcc").read())
+        options += [
+            "-DBUILD_SHARED_LIBS=%s" %
+            ('ON' if ('+shared' in self.spec) else 'OFF')
+        ]
 
-        make_inc = FileFilter("make.inc")
-
-        if '~shared' in self.spec:
-            make_inc.filter("-fPIC", "")  # not using fPIC
-
-        if "^mkl" not in spec:
-            make_inc.filter("-DPLASMA_WITH_MKL", "")  # not using MKL
-            make_inc.filter("-DHAVE_MKL", "")         # not using MKL
-
-        header_flags = ""
-        # accumulate CPP flags for headers: <cblas.h> and <lapacke.h>
-        for dep in ("blas", "lapack"):
-            try:  # in case the dependency does not provide header flags
-                header_flags += " " + spec[dep].headers.cpp_flags
-            except Exception:
-                pass
-
-        make_inc.filter("CFLAGS +[+]=", "CFLAGS += " + header_flags + " ")
-
-        # pass prefix variable from "make.inc" to "Makefile"
-        make_inc.filter("# --*", "prefix={0}".format(self.prefix))
-
-        # make sure CC variable comes from build environment
-        make_inc.filter("CC *[?]*= * .*cc", "")
-
-        libs = self.getblaslapacklibs().ld_flags
-        if 'readline' in self.spec:
-            libs += ' ' + self.spec['readline'].libs.ld_flags
-            libs += ' ' + find_system_libraries(['libdl']).ld_flags
-        make_inc.filter("LIBS *[?]*= * .*", "LIBS = " + libs)
-
-    @property
-    def build_targets(self):
-        targets = list()
-
-        # use $CC set by Spack
-        targets.append("CC = {0}".format(self.compiler.cc))
-
-        if "^mkl" in self.spec:
-            targets.append("MKLROOT = {0}".format(env["MKLROOT"]))
-
-        targets.append("LIBS = {0} {1} {2}".format(
-                       self.getblaslapacklibs().ld_flags,
-                       self.spec['readline'].libs.ld_flags,
-                       find_system_libraries(['libdl']).ld_flags))
-        return targets
+        return options
