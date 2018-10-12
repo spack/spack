d9a1bf106813d931328629e60a45a7ee9f9e1991
Satish Balay <balay@mcs.anl.gov>
2018-10-12 15:34:28 -0500

    gmodel: switch from .zip to .tar.gz to avoid unzip dependency

--- var/spack/repos/builtin/packages/gmodel/package.py
+++ var/spack/repos/builtin/packages/gmodel/package.py
@@ -34,6 +34,6 @@ class Gmodel(CMakePackage):
       for Gmsh.
     """
     homepage = "https://github.com/ibaned/gmodel"
-    url      = "https://github.com/ibaned/gmodel/archive/v2.1.0.zip"
+    url      = "https://github.com/ibaned/gmodel/archive/v2.1.0.tar.gz"
 
-    version('2.1.0', '9130a90c93c40edde378f6d4086c5fe0')
+    version('2.1.0', '80df0c6dc413a9ffa0f0e7b65118b05b643ba3e1bfcac28fb91d2d3ad017fda0')
