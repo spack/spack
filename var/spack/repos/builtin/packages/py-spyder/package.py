35150b610f2d9a6f2b7e416c3dadad572d890ce1
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2018-10-12 09:53:11 +0200

    mention github issue
    Change-Id: I119199942423ec4019b8c655e0e2d67a3af1353b

--- var/spack/repos/builtin/packages/py-spyder/package.py
+++ var/spack/repos/builtin/packages/py-spyder/package.py
@@ -56,7 +56,7 @@ class PySpyder(PythonPackage):
     depends_on('py-qtawesome@0.4.1:', type=('build', 'run'))
     depends_on('py-qtpy@1.1.0:',      type=('build', 'run'))
     # technically this is a transitive dependency in order for py-pyqt
-    # to pick up webkit, but this is the easier solution
+    # to pick up webkit, but this is the easier solution (see #9207)
     depends_on('qt+webkit',           type=('build', 'run'))
     depends_on('py-pickleshare',      type=('build', 'run'))
     depends_on('py-zmq',              type=('build', 'run'))
