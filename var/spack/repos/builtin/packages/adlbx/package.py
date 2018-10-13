ec81ec5f54494527340c902a8b8d9f382fd32aa9
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-10-12 19:32:27 -0500

    Swift/T Release 1.3 (#4641)
    New release of Swift/T along with turbine, adlbx, and exmcutils
    dependencies. Update URLs for archive downloads.

--- var/spack/repos/builtin/packages/adlbx/package.py
+++ var/spack/repos/builtin/packages/adlbx/package.py
@@ -30,11 +30,20 @@ class Adlbx(AutotoolsPackage):
     """ADLB/X: Master-worker library + work stealing and data dependencies"""
 
     homepage = 'http://swift-lang.org/Swift-T'
-    url      = 'http://swift-lang.github.io/swift-t-downloads/adlbx-0.8.0.tar.gz'
+    url      = 'http://swift-lang.github.io/swift-t-downloads/spack/adlbx-0.0.0.tar.gz'
+
+    version('0.9.1', '07151ddef5fb83d8f4b40700013d9daf')
     version('0.8.0', '34ade59ce3be5bc296955231d47a27dd')
 
-    depends_on('exmcutils')
+    depends_on('exmcutils@:0.5.3', when='@:0.8.0')
+    depends_on('exmcutils', when='@0.9.1:')
     depends_on('mpi')
+
+    def setup_environment(self, spack_env, run_env):
+        spec = self.spec
+        spack_env.set('CC', spec['mpi'].mpicc)
+        spack_env.set('CXX', spec['mpi'].mpicxx)
+        spack_env.set('CXXLD', spec['mpi'].mpicxx)
 
     def configure_args(self):
         args = ['--with-c-utils=' + self.spec['exmcutils'].prefix]
