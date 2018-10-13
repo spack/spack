8fb4f39dd32905c606d3f301e81618cc8d85b484
Lukasz Kaczmarczyk <likask@wp.pl>
2018-10-13 09:42:06 +0100

    mofem: add new version

--- var/spack/repos/builtin/packages/mofem-cephas/package.py
+++ var/spack/repos/builtin/packages/mofem-cephas/package.py
@@ -31,6 +31,8 @@ class MofemCephas(CMakePackage):
 
     maintainers = ['likask']
 
+    version('0.8.14', git='https://bitbucket.org/likask/mofem-cephas.git',
+        tag='v0.8.14', submodules=True)
     version('0.8.13', git='https://bitbucket.org/likask/mofem-cephas.git',
         tag='v0.8.13', submodules=True)
     version('0.8.12', git='https://bitbucket.org/likask/mofem-cephas.git',
