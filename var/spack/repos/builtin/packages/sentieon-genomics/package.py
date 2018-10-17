7ab856b98c9472e8ad6b16b5300b33936913fd21
Miles Perry <mpperry@iastate.edu>
2018-10-17 12:20:44 -0500

    added package sentieon

---
+++ var/spack/repos/builtin/packages/sentieon-genomics/package.py
@@ -0,0 +1,45 @@
+##############################################################################
+# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class SentieonGenomics(Package):
+    """Sentieon provides complete solutions for secondary DNA analysis.
+    Our software improves upon BWA, GATK, Mutect, and Mutect2 based pipelines.
+    The Sentieon tools are deployable
+    on any generic-CPU-based computing system."""
+
+    homepage = "https://www.sentieon.com/"
+    url      = "https://s3.amazonaws.com/sentieon-release/software/sentieon-genomics-201808.01.tar.gz"
+
+    version('201808.01', sha256='6d77bcd5a35539549b28eccae07b19a3b353d027720536e68f46dcf4b980d5f7')
+
+    def install(self, spec, prefix):
+        install_tree('bin', prefix.bin)
+        install_tree('doc', prefix.doc)
+        install_tree('etc', prefix.etc)
+        install_tree('lib', prefix.lib)
+        install_tree('libexec', prefix.libexec)
+        install_tree('share', prefix.share)
