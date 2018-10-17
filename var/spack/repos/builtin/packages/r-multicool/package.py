66d1a3d83b17be7d76fa55020ce18831471006b1
Dan Han <las_danhan@iastate.edu>
2018-10-17 14:10:33 -0500

    r-multicool: newer version, list_url

--- var/spack/repos/builtin/packages/r-multicool/package.py
+++ var/spack/repos/builtin/packages/r-multicool/package.py
@@ -30,7 +30,9 @@ class RMulticool(RPackage):
 
     homepage = "https://cran.r-project.org/package=multicool"
     url      = "https://cran.r-project.org/src/contrib/Archive/multicool/multicool_0.1-9.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/multicool/"
 
+    version('0.1-10', sha256='5bb0cb0d9eb64420c862877247a79bb0afadacfe23262ec8c3fa26e5e34d6ff9')
     version('0.1-9', sha256='bdf92571cef1b649952d155395a92b8683099ee13114f73a9d41fc5d7d49d329')
 
     depends_on('r-rcpp@0.11.2:', type=('build', 'run'))
