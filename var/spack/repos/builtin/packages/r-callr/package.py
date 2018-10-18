57075dbf336dcb4e090337fb3ecaac9f0452f8fd
Dan Han <danhan@iastate.edu>
2018-10-18 15:40:38 -0500

    r-matr and r-callr: add new versions, list_urls (#9562)

--- var/spack/repos/builtin/packages/r-callr/package.py
+++ var/spack/repos/builtin/packages/r-callr/package.py
@@ -13,5 +13,7 @@ class RCallr(RPackage):
 
     homepage = "https://github.com/MangoTheCat/callr"
     url      = "https://cran.r-project.org/src/contrib/callr_1.0.0.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/callr/"
 
+    version('3.0.0', sha256='e36361086c65660a6ecbbc09b5ecfcddee6b59caf75e983e48b21d3b8defabe7')
     version('1.0.0', 'd9af99bb95696310fa1e5d1cb7166c91')
