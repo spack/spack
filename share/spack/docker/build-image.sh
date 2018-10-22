f23a598b20ab5c4b96ef5bdac862cd2552d59908
Omar Padron <omar.padron@kitware.com>
2018-10-22 11:24:27 -0400

    update files with copyright info

--- share/spack/docker/build-image.sh
+++ share/spack/docker/build-image.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
+#
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 script="$( basename "$0" )"
 cd "$( dirname "$0" )"
--- share/spack/docker/entrypoint.bash
+++ share/spack/docker/entrypoint.bash
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash -e
+#
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 if [ "$1" '=' 'docker-shell' ] ; then
     if [ -t 0 ] ; then
