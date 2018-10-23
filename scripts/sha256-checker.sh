61f6f3f854b057968282317c6f68999c95b72d1d
Daniel Topa <dantopa@lanl.gov>
2018-10-22 20:58:37 -0600

    trinitite-log.in-nid00012 Mon Oct 22 20:58:37 MDT 2018

--- scripts/sha256-checker.sh
+++ scripts/sha256-checker.sh
@@ -4,18 +4,21 @@ printf '%s\n' "**  **  **  $(date) ${BASH_SOURCE[0]}"
 export here="$(pwd)"
 
 cd "/usr/projects/draco/vendors/spack.mirror/openmpi"
+
 for filename in `ls -r *.tar.bz2`; do
-# for filename in *; do
-    #
-    version=$(cut -d "-" -f 2 <<< ${filename}) # chop of leading v
-    version=$(cut -d "t" -f 1 <<< ${version}) # chop off tar.gz
-    version=${version%.*} # chop off trailing .
+# for filename in *; doi
+        #
+    # ${filename} = openmpi-3.1.2.tar.bz2
+    version=$(cut -d "-" -f 2 <<< ${filename}) # chop of leading name
+    version=$(cut -d "t" -f 1 <<< ${version})  # chop off tar.bz2
+    version=${version%.*}                      # chop off trailing .
+        #
     # echo "version = ${version}"
-    #
-    sha="$(cut -d "=" -f 2 <<< "$(sha256sum ${filename})")";
+    # sha256sum openmpi-3.1.2.tar.bz2
+    # c654ed847f34a278c52a15c98add40402b4a90f0c540779f1ae6c489af8a76c5  openmpi-3.1.2.tar.bz2
+    sha="$(sha256sum ${filename})";
     sha=${sha% *} # everything right of the blank
-    sha=${sha%?} # remove last character (a blank)
-    sha="${sha:1}"
+    sha=${sha%?}  # remove last character (a blank)
     sha="SHA256='${sha}"
     # echo "${sha}"
     #
