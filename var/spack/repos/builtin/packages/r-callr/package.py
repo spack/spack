5b04a7add9ef643c3c0803273d214308f050c327
molecuul <jtst@iastate.edu>
2018-10-24 15:49:55 -0500

    r-callr: add list_url

--- var/spack/repos/builtin/packages/r-callr/package.py
+++ var/spack/repos/builtin/packages/r-callr/package.py
@@ -13,5 +13,6 @@ class RCallr(RPackage):
 
     homepage = "https://github.com/MangoTheCat/callr"
     url      = "https://cran.r-project.org/src/contrib/callr_1.0.0.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/callr"
 
     version('1.0.0', 'd9af99bb95696310fa1e5d1cb7166c91')
