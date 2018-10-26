3374a6c8cf701c49124922370632cd3116c28784
Matthieu Dorier <matthieu.dorier@irisa.fr>
2018-10-26 16:27:52 +0200

    damaris: making flake8 happy

--- var/spack/repos/builtin/packages/damaris/package.py
+++ var/spack/repos/builtin/packages/damaris/package.py
@@ -55,7 +55,8 @@ class Damaris(CMakePackage):
 
         if (self.spec.variants['catalyst'].value):
             args.extend(['-DENABLE_CATALYST:BOOL=ON'])
-            args.extend(['-DParaView_ROOT:PATH=%s' % self.spec['catalyst'].prefix])
+            args.extend(['-DParaView_ROOT:PATH=%s' 
+                         % self.spec['catalyst'].prefix])
 
         if (self.spec.variants['visit'].value):
             args.extend(['-DENABLE_VISIT:BOOL=ON'])
