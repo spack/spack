63a2eb91702f9cef53c7f3991bcdb23a59646451
Srinath Vadlamani <srinath.vadlamani@arm.com>
2018-10-31 17:54:38 -0500

    Changed licence to Arm suggested header

--- lib/spack/spack/compilers/arm.py
+++ lib/spack/spack/compilers/arm.py
@@ -1,28 +1,26 @@
 ##############################################################################
-# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC., et al.
-# Produced at the Lawrence Livermore National Laboratory.
+# Copyright (c) 2018 Arm Limited and affiliates. All rights reserved.
 #
 # This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, 
-# srinath.vadlamani@arm.com, Nick.Forrington@arm.com All rights reserved.
-# LLNL-CODE-647188
 #
-# For details, see https://github.com/spack/spack
-# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+# For details, see https://github.com/spack/spack # Please also see the NOTICE
+# and LICENSE files for the LLNL notice and LGPL.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
+# License (SPDX-License-Identifier:LGPL-2.1-only)
+# ----------------------------------------------- This program is free
+# software; you can redistribute it and/or modify # it under the terms of the
+# GNU Lesser General Public License (as # published by the Free Software
+# Foundation) version 2.1, February 1999.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
+# This program is distributed in the hope that it will be useful, but # WITHOUT
+# ANY WARRANTY; without even the IMPLIED WARRANTY OF # MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the terms and # conditions of the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
+# You should have received a copy of the GNU Lesser General Public # License
+# along with this program; if not, write to the Free Software # Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# ##############################################################################
 import re
 
 from spack.compiler import Compiler, _version_cache
