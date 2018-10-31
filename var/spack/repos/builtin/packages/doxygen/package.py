a3ff21eb2ba1b7a3565d736c0da72f0c9a135407
Kelly (KT) Thompson <KineticTheory@users.noreply.github.com>
2018-10-31 11:03:13 -0600

    Provide a newer doxygen version. (#9681)
    * Provide a newer doxygen version.
    
    + Also provide a patch that allows collaboration diagrams to work properly with
      C++ std::shared_ptr<T>. Ref:
      https://github.com/Sleepyowl/doxygen/commit/6c380ba91ae41c6d5c409a5163119318932ae2a3?diff=unified

--- var/spack/repos/builtin/packages/doxygen/package.py
+++ var/spack/repos/builtin/packages/doxygen/package.py
@@ -16,6 +16,7 @@ class Doxygen(CMakePackage):
     homepage = "http://www.stack.nl/~dimitri/doxygen/"
     url      = "http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.10.src.tar.gz"
 
+    version('1.8.14', '41d8821133e8d8104280030553e2b42b')
     version('1.8.12', '08e0f7850c4d22cb5188da226b209a96')
     version('1.8.11', 'f4697a444feaed739cfa2f0644abc19b')
     version('1.8.10', '79767ccd986f12a0f949015efb5f058f')
@@ -30,3 +31,8 @@ class Doxygen(CMakePackage):
 
     # optional dependencies
     depends_on("graphviz", when="+graphviz", type='run')
+
+    # Support C++14's std::shared_ptr. For details about this patch, see
+    # https://github.com/Sleepyowl/doxygen/commit/6c380ba91ae41c6d5c409a5163119318932ae2a3?diff=unified
+    # Also - https://github.com/doxygen/doxygen/pull/6588
+    patch('shared_ptr.patch', when='@1.8.14')
