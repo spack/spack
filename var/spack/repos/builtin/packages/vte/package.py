7915f865f3b09cdabc1783cdfef3cdf42bf5ade4
Luigi Calori <luigi.calori@gmail.com>
2018-10-31 01:00:25 +0100

    fix config guess to work in powerpc machines, hack similar to one used for libiconv/package.py

--- var/spack/repos/builtin/packages/vte/package.py
+++ var/spack/repos/builtin/packages/vte/package.py
@@ -1,28 +1,8 @@
-##############################################################################
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/llnl/spack
-# Please also see the LICENSE file for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
-#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 from spack import *
 
 
@@ -32,25 +12,30 @@ class Vte(AutotoolsPackage):
 
     homepage = "http://www.linuxfromscratch.org/blfs/view/svn/gnome/vte.html"
     url      = "http://ftp.gnome.org/pub/gnome/sources/vte/0.28/vte-0.28.2.tar.xz"
+    _urlfmt  = "http://ftp.gnome.org/pub/gnome/sources/vte/{0}/vte-{1}.tar.xz"
+    version('0.54.2', '054a8a46b9de9078f81931311cf27a68')
+    version('0.36.5', '96f102ef9e178b6238edcfdb1fa9dbcc')
+    version('0.36.2', '9c13f35f47b31abed4cdca2684d3d2ff')
+    version('0.29.1', '91cd70995099ca7e534654e0679ab5a7')
     version('0.28.2', '497f26e457308649e6ece32b3bb142ff')
 
-    # depends_on('m4', type='build')
-    # depends_on('autoconf', type='build')
-    # depends_on('automake', type='build')
     depends_on('binutils', type='build')
     depends_on('libtool', type='build')
     depends_on('intltool', type='build')
     depends_on('perl-xml-parser', type='build')
     depends_on('perl', type='build')
-    depends_on('pkg-config', type='build')
+    depends_on('pkgconfig', type='build')
     depends_on('gtkplus')
+    depends_on('gtkplus@3.1:', when='@0.29:')
     depends_on('glib')
-    # depends_on('pcre2')
-    # depends_on('libxml2')
-    # depends_on('libx11')
-    # depends_on('libice')
-    # depends_on('libxext')
-    # depends_on('libxfixes')
-    # depends_on('libxdamage')
-    # depends_on('libxrender')
-    # depends_on('libsm')
+
+    def url_for_version(self, version):
+        """Handle vte version-based custom URLs."""
+        return self._urlfmt.format(version.up_to(2), version)
+
+    def configure_args(self):
+        # A hack to patch config.guess and config.sub
+        # in the gnome-pty-helper sub directory
+        copy('./config.guess', 'gnome-pty-helper/config.guess')
+        copy('./config.sub', 'gnome-pty-helper/config.sub')
+        return []
