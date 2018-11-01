11aa9336c2e96a0084e75cd1775508009a93595e
Sinan81 <Sinan81@github>
2018-11-01 02:23:51 -0700

    clean up, add conflicts

--- var/spack/repos/builtin/packages/py-python-htmlgen/package.py
+++ var/spack/repos/builtin/packages/py-python-htmlgen/package.py
@@ -3,22 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-python-htmlgen
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-python-htmlgen
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
@@ -31,7 +15,7 @@ class PyPythonHtmlgen(PythonPackage):
     url      = "https://github.com/srittau/python-htmlgen/archive/v1.2.2.tar.gz"
 
     version('1.2.2', sha256='9dc60e10511f0fd13014659514c6c333498c21779173deb585cd4964ea667770')
-
+    conflicts('python@3.0:3.3.99')
     depends_on('py-setuptools', type='build')
     # dependencies for tests
     depends_on('py-typing', type='build')
