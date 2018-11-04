381bbb01ab9e0b129f61e4f975a5671369a59e20
Denis Davydov <davydden@gmail.com>
2018-11-04 16:29:16 +0100

    muparser: minor fixes to 2.2.5 and add 2.2.6 with CMake

--- var/spack/repos/builtin/packages/muparser/package.py
+++ var/spack/repos/builtin/packages/muparser/package.py
@@ -11,6 +11,7 @@ class Muparser(Package):
     homepage = "http://muparser.beltoforion.de/"
     url      = "https://github.com/beltoforion/muparser/archive/v2.2.5.tar.gz"
 
+    version('2.2.6', 'f197b2815ca0422b2091788a78f2dc8a')
     version('2.2.5', '02dae671aa5ad955fdcbcd3fee313fb7')
 
     # Replace std::auto_ptr by std::unique_ptr
@@ -18,10 +19,29 @@ class Muparser(Package):
     patch('auto_ptr.patch',
           when='@2.2.5')
 
+    depends_on('cmake@3.1.0:', when='@2.2.6:', type='build')
+
+    # Cmake build
+    def install(self, spec, prefix):
+        cmake_args = [
+            '-DENABLE_SAMPLES=OFF',
+            '-DENABLE_OPENMP=OFF',
+            '-DBUILD_SHARED_LIBS=ON'
+        ]
+
+        cmake_args.extend(std_cmake_args)
+
+        with working_dir('spack-build', create=True):
+            cmake('..', *cmake_args)
+            make()
+            make('install')
+
+    @when('@2.2.5')
     def install(self, spec, prefix):
         options = ['--disable-debug',
+                   '--disable-samples',
                    '--disable-dependency-tracking',
-                   'CXXFLAGS=-std=c++11',
+                   'CXXFLAGS={0}'.format(self.compiler.cxx11_flag),
                    '--prefix=%s' % prefix]
 
         configure(*options)
