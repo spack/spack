e656e29782cee224e2928514a7f9ac9d51114850
Phil Tooley <phil.tooley@sheffield.ac.uk>
2018-11-06 09:52:19 +0000

    fix numpy dependency

--- var/spack/repos/builtin/packages/openimageio/package.py
+++ var/spack/repos/builtin/packages/openimageio/package.py
@@ -31,8 +31,8 @@ class Openimageio(CMakePackage):
 
     variant('python', default=False, description="Build python bindings")
     extends('python', when='+python')
-    depends_on('numpy', when='+python')
-    depends_on('py-pybind11', when='+python')
+    depends_on('py-numpy', when='+python', type=('build', 'run'))
+    depends_on('py-pybind11', when='+python', type=('build', 'run'))
 
     variant('qt', default=False, description="Build qt viewer")
     depends_on('qt@5.6.0:+opengl', when='+qt')
