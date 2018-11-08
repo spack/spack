aaa2b49a5a84a856ae040c77d2e0bf4a75d67982
molecuul <jtst@iastate.edu>
2018-11-08 09:18:13 -0600

    added several new perl modules

---
+++ var/spack/repos/builtin/packages/perl-params-validate/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PerlParamsValidate(PerlPackage):
+    """Params::Validate - Validate method/function parameters"""
+
+    homepage = "https://metacpan.org/pod/Params::Validate"
+    url      = "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/Params-Validate-1.29.tar.gz"
+
+    version('1.29', sha256='49a68dfb430bea028042479111d19068e08095e5a467e320b7ab7bde3d729733')
+
+    depends_on('perl-module-build', type='build')
+    depends_on('perl-module-implementation', type=('build', 'run'))
---
+++ var/spack/repos/builtin/packages/perl-statistics-basic/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PerlStatisticsBasic(PerlPackage):
+    """Statistics::Basic - A collection of very basic statistics modules"""
+
+    homepage = "https://metacpan.org/pod/distribution/Statistics-Basic/lib/Statistics/Basic.pod"
+    url      = "https://cpan.metacpan.org/authors/id/J/JE/JETTERO/Statistics-Basic-1.6611.tar.gz"
+
+    version('1.6611', sha256='6855ce5615fd3e1af4cfc451a9bf44ff29a3140b4e7130034f1f0af2511a94fb')
+
+    depends_on('perl-number-format', type=('build', 'run'))
+    depends_on('perl-scalar-list-utils', type=('build', 'run'))
