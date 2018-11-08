d02d7e56ab41f39014aed3d66dbb7d79843a5cde
Ross Miller <rgmiller@ornl.gov>
2018-11-08 10:57:10 -0500

    py-numba: Add upper bounds on dependency versions

--- var/spack/repos/builtin/packages/py-numba/package.py
+++ var/spack/repos/builtin/packages/py-numba/package.py
@@ -17,11 +17,17 @@ class PyNumba(PythonPackage):
 
     depends_on('python@3.3:3.7.9999', type=('build', 'run'), when='@0.40.1:')
     depends_on('python@3.3:3.6.9999', type=('build', 'run'), when='@:0.35.0')
-    depends_on('py-numpy@1.10:',    type=('build', 'run'))
-    depends_on('py-llvmlite@0.25:', type=('build', 'run'), when='@0.40.1:')
-    depends_on('py-llvmlite@0.20:', type=('build', 'run'), when='@0.35.1')
-    depends_on('py-argparse',       type=('build', 'run'))
-    depends_on('py-funcsigs',       type=('build', 'run'), when='^python@:3.3.99')
+    depends_on('py-numpy@1.10:1.99', type=('build', 'run'))
+
+    # Note: As of 1 Nov 2018, 0.25 was the latest version of py-llvmlite.
+    # That's why it was chosen as an upper bound in the following depends_on
+    # calls.  If newer versions maintain backwards compatibility, the calls
+    # can be updated accordingly.
+    depends_on('py-llvmlite@0.25', type=('build', 'run'), when='@0.40.1:')
+    depends_on('py-llvmlite@0.20:0.25', type=('build', 'run'), when='@0.35.1')
+
+    depends_on('py-argparse', type=('build', 'run'))
+    depends_on('py-funcsigs', type=('build', 'run'), when='^python@:3.3.99')
     depends_on('py-singledispatch', type=('build', 'run'), when='^python@:3.3.99')
 
     # Version 6.0.0 of llvm had a hidden symbol which breaks numba at runtime.
