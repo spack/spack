b3eaa2ad8f8a7fa25bad0549ba34bfc4e714f9e4
molecuul <jtst@iastate.edu>
2018-11-12 15:38:40 -0600

    r-manipulatewidget: flake8 fixes

--- var/spack/repos/builtin/packages/r-manipulatewidget/package.py
+++ var/spack/repos/builtin/packages/r-manipulatewidget/package.py
@@ -11,12 +11,12 @@ class RManipulatewidget(RPackage):
        to easily add controls like sliders, pickers, checkboxes, etc. that can
        be used to modify the input data or the parameters of an interactive
        chart created with package 'htmlwidgets'."""
-    
+
     homepage = "https://github.com/rte-antares-rpackage/manipulateWidget"
     url      = "https://cran.r-project.org/src/contrib/manipulateWidget_0.10.0.tar.gz"
     list_url = "https://cran.r-project.org/src/contrib/Archive/manipulateWidget/"
 
-    version('0.10.0', sha256='3d61a3d0cedf5c8a850a3e62ed6af38c600dc3f25b44c4ff07a5093bf9ca4ffd');
+    version('0.10.0', sha256='3d61a3d0cedf5c8a850a3e62ed6af38c600dc3f25b44c4ff07a5093bf9ca4ffd')
 
     depends_on('r-base64enc', type=('build', 'run'))
     depends_on('r-codetools', type=('build', 'run'))
