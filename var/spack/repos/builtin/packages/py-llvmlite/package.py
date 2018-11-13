37a8486fc62fc175641b080d8b74e59729980d33
Ross Miller <rgmiller@ornl.gov>
2018-11-13 00:07:40 -0500

    py-numba: update dependencies and versions (#9706)
    The latest version of py-numba (0.40.1) wants a newer version of
    py-llvmlite.  Had to add that version to py-llvmlite/package.py.

--- var/spack/repos/builtin/packages/py-llvmlite/package.py
+++ var/spack/repos/builtin/packages/py-llvmlite/package.py
@@ -12,6 +12,7 @@ class PyLlvmlite(PythonPackage):
     homepage = "http://llvmlite.readthedocs.io/en/latest/index.html"
     url = "https://pypi.io/packages/source/l/llvmlite/llvmlite-0.23.0.tar.gz"
 
+    version('0.25.0', sha256='fd64def9a51dd7dc61913a7a08eeba5b9785522740bec5a7c5995b2a90525025')
     version('0.23.0', '6fc856576a11dbeef71de862f7c419de')
     version('0.20.0', 'f2aa60d0981842b7930ba001b03679ab')
 
--- var/spack/repos/builtin/packages/py-numba/package.py
+++ var/spack/repos/builtin/packages/py-numba/package.py
@@ -12,12 +12,22 @@ class PyNumba(PythonPackage):
     homepage = "https://numba.pydata.org/"
     url      = "https://pypi.io/packages/source/n/numba/numba-0.35.0.tar.gz"
 
+    version('0.40.1', sha256='52d046c13bcf0de79dbfb936874b7228f141b9b8e3447cc35855e9ad3e12aa33')
     version('0.35.0', '4f447383406f54aaf18ffaba3a0e79e8')
 
-    depends_on('py-numpy@1.10:',    type=('build', 'run'))
-    depends_on('py-llvmlite@0.20:', type=('build', 'run'))
-    depends_on('py-argparse',       type=('build', 'run'))
-    depends_on('py-funcsigs',       type=('build', 'run'), when='^python@:3.3.99')
+    depends_on('python@3.3:3.7.9999', type=('build', 'run'), when='@0.40.1:')
+    depends_on('python@3.3:3.6.9999', type=('build', 'run'), when='@:0.35.0')
+    depends_on('py-numpy@1.10:1.99', type=('build', 'run'))
+
+    # Note: As of 1 Nov 2018, 0.25 was the latest version of py-llvmlite.
+    # That's why it was chosen as an upper bound in the following depends_on
+    # calls.  If newer versions maintain backwards compatibility, the calls
+    # can be updated accordingly.
+    depends_on('py-llvmlite@0.25', type=('build', 'run'), when='@0.40.1:')
+    depends_on('py-llvmlite@0.20:0.25', type=('build', 'run'), when='@0.35.1')
+
+    depends_on('py-argparse', type=('build', 'run'))
+    depends_on('py-funcsigs', type=('build', 'run'), when='^python@:3.3.99')
     depends_on('py-singledispatch', type=('build', 'run'), when='^python@:3.3.99')
 
     # Version 6.0.0 of llvm had a hidden symbol which breaks numba at runtime.
