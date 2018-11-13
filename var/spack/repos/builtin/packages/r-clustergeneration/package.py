a95ddcec0b9afb1adf495ae5c9110b68309f194c
Justin Stanley <molecuul@users.noreply.github.com>
2018-11-12 18:34:03 -0600

    r-clustergeneration: new package at 1.3.4 (#9765)

---
+++ var/spack/repos/builtin/packages/r-clustergeneration/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RClustergeneration(RPackage):
+    """Random Cluster Generation (with Specified Degree of Separation)"""
+
+    homepage = "https://cran.r-project.org/package=clusterGeneration"
+    url      = "https://cran.r-project.org/src/contrib/clusterGeneration_1.3.4.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/clusterGeneration/"
+
+    version('1.3.4', sha256='7c591ad95a8a9d7fb0e4d5d80dfd78f7d6a63cf7d11eb53dd3c98fdfb5b868aa')
+
+    depends_on('r-mass', type=('build', 'run'))
