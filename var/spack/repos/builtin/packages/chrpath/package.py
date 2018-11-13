e42f8ea6b70186b22e56082e345109f4c0bd7b68
Matthias Diener <matthias.diener@gmail.com>
2018-11-12 23:21:56 -0600

    chrpath: add new package (#9708)

---
+++ var/spack/repos/builtin/packages/chrpath/package.py
@@ -0,0 +1,16 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Chrpath(AutotoolsPackage):
+    """chrpath: Modifies the dynamic library load path (rpath and runpath)
+       of compiled programs and libraries."""
+
+    homepage = "https://directory.fsf.org/wiki/Chrpath"
+    url      = "https://cfhcable.dl.sourceforge.net/project/pisilinux/source/chrpath-0.16.tar.gz"
+
+    version('0.16', sha256='bb0d4c54bac2990e1bdf8132f2c9477ae752859d523e141e72b3b11a12c26e7b')
