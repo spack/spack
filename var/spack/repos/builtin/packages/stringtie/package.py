7ebf176fddd3bbf9a4e79de88f497f5e57376236
Eric <erimar77@users.noreply.github.com>
2018-11-13 10:20:30 -0600

    stringtie: add version 1.3.4d (#9791)

--- var/spack/repos/builtin/packages/stringtie/package.py
+++ var/spack/repos/builtin/packages/stringtie/package.py
@@ -13,6 +13,7 @@ class Stringtie(MakefilePackage):
     homepage = "https://ccb.jhu.edu/software/stringtie"
     url      = "https://github.com/gpertea/stringtie/archive/v1.3.3b.tar.gz"
 
+    version('1.3.4d', '0134c0adc264efd31a1df4301b33bfcf3b3fe96bd3990ce3df90819bad9af968')
     version('1.3.4a', '2a191ef6512242a3a5778cf7718bb6af')
     version('1.3.3b', '11a43260b18e4272182380e922445d88')
 
