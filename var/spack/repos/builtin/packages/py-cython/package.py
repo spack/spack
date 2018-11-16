cbebd2685f7230f92f7efb5e69e887e0d994f405
Sinan <sbulutw@gmail.com>
2018-11-16 13:20:34 -0800

    py-cython: add new versions (#9846)

--- var/spack/repos/builtin/packages/py-cython/package.py
+++ var/spack/repos/builtin/packages/py-cython/package.py
@@ -11,6 +11,8 @@ class PyCython(PythonPackage):
     homepage = "https://pypi.python.org/pypi/cython"
     url      = "https://pypi.io/packages/source/c/cython/Cython-0.25.2.tar.gz"
 
+    version('0.29', sha256='94916d1ede67682638d3cc0feb10648ff14dc51fb7a7f147f4fedce78eaaea97')
+    version('0.28.6', '3c3fb47806a4476f8e9429943439cc60')
     version('0.28.3', '586f0eb70ba1fcc34334e9e10c5e68c0')
     version('0.28.1', 'c549effadb52d90bdcb1affc1e5dbb97')
     version('0.25.2', '642c81285e1bb833b14ab3f439964086')
