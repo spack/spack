f5bbd4fe99640913fda842e1a2fc626a0a4e8ded
Andrea Negri <andrea.negri@gmail.com>
2018-11-19 20:19:19 +0100

    new package: mongo-cxx-driver (#9880)
    * new package: mongo-cxx-driver
    
    * fixed url

---
+++ var/spack/repos/builtin/packages/mongo-cxx-driver/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class MongoCxxDriver(CMakePackage):
+    """C++ Driver for MongoDB"""
+
+    homepage = "http://www.mongocxx.org"
+    url      = "https://github.com/mongodb/mongo-cxx-driver/archive/r3.2.0.tar.gz"
+
+    version('3.2.0',     sha256='e26edd44cf20bd6be91907403b6d63a065ce95df4c61565770147a46716aad8c')
+
+    depends_on('libmongoc@1.9.2:')
