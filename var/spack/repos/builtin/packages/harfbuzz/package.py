4fad3589dc9382d84490f5204a7f5311f8bcd3b5
Sinan <sbulutw@gmail.com>
2018-11-19 18:42:08 -0800

    package/harfbuzz: disable building of gtk-docs, add new version (#9891)

--- var/spack/repos/builtin/packages/harfbuzz/package.py
+++ var/spack/repos/builtin/packages/harfbuzz/package.py
@@ -11,6 +11,7 @@ class Harfbuzz(AutotoolsPackage):
     homepage = "http://www.freedesktop.org/wiki/Software/HarfBuzz/"
     url      = "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.37.tar.bz2"
 
+    version('2.1.3', sha256='613264460bb6814c3894e3953225c5357402915853a652d40b4230ce5faf0bee')
     version('1.4.6', '21a78b81cd20cbffdb04b59ac7edfb410e42141869f637ae1d6778e74928d293')
     version('0.9.37', 'bfe733250e34629a188d82e3b971bc1e')
 
@@ -20,6 +21,17 @@ class Harfbuzz(AutotoolsPackage):
     depends_on("freetype")
     depends_on("cairo")
     depends_on("zlib")
+
+    def configure_args(self):
+        args = []
+        # disable building of gtk-doc files following #9771
+        args.append('--disable-gtk-doc-html')
+        true = which('true')
+        args.append('GTKDOC_CHECK={0}'.format(true))
+        args.append('GTKDOC_CHECK_PATH={0}'.format(true))
+        args.append('GTKDOC_MKPDF={0}'.format(true))
+        args.append('GTKDOC_REBASE={0}'.format(true))
+        return args
 
     def patch(self):
         change_sed_delimiter('@', ';', 'src/Makefile.in')
