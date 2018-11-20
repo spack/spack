f04e36705ca0e4fb32381356aa75547c0ae416ec
Justin Stanley <molecuul@users.noreply.github.com>
2018-11-19 20:44:44 -0600

    added several new perl modules (#9760)

---
+++ var/spack/repos/builtin/packages/perl-extutils-config/package.py
@@ -0,0 +1,15 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PerlExtutilsConfig(PerlPackage):
+    """ExtUtils::Config - A wrapper for perl's configuration"""
+
+    homepage = "https://metacpan.org/pod/ExtUtils::Config"
+    url      = "https://cpan.metacpan.org/authors/id/L/LE/LEONT/ExtUtils-Config-0.008.tar.gz"
+
+    version('0.008', sha256='ae5104f634650dce8a79b7ed13fb59d67a39c213a6776cfdaa3ee749e62f1a8c')
---
+++ var/spack/repos/builtin/packages/perl-extutils-installpaths/package.py
@@ -0,0 +1,15 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PerlExtutilsInstallpaths(PerlPackage):
+    """ExtUtils::InstallPaths - Build.PL install path logic made easy"""
+
+    homepage = "https://metacpan.org/pod/ExtUtils::InstallPaths"
+    url      = "https://cpan.metacpan.org/authors/id/L/LE/LEONT/ExtUtils-InstallPaths-0.012.tar.gz"
+
+    version('0.012', sha256='84735e3037bab1fdffa3c2508567ad412a785c91599db3c12593a50a1dd434ed')
