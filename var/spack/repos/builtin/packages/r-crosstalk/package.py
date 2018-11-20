00b93b8e0417b4b8b794e7958217aa21f6c0badc
Levi Baber <baberlevi@gmail.com>
2018-11-19 20:41:41 -0600

    r-crosstalk: remove unnecessary constraint (#9892)

--- var/spack/repos/builtin/packages/r-crosstalk/package.py
+++ var/spack/repos/builtin/packages/r-crosstalk/package.py
@@ -15,7 +15,6 @@ class RCrosstalk(RPackage):
 
     version('1.0.0', 'c13c21b81af2154be3f08870fd3a7077')
 
-    depends_on('r@3.4.0:3.4.9')
     depends_on('r-htmltools', type=('build', 'run'))
     depends_on('r-jsonlite', type=('build', 'run'))
     depends_on('r-lazyeval', type=('build', 'run'))
