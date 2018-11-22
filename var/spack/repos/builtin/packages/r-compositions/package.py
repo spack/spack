4c01894dc0f7ae4c4bd8b9bdfd632d28a2255220
Neil Flood <Neil.Flood@des.qld.gov.au>
2018-11-22 15:24:59 +1000

    r-compositions: new package, including its dependencies

---
+++ var/spack/repos/builtin/packages/r-compositions/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RCompositions(RPackage):
+    """Compositional Data Analysis"""
+
+    homepage = "https://cran.r-project.org/web/packages/compositions/index.html"
+    url      = "https://cran.r-project.org/src/contrib/compositions_1.40-2.tar.gz"
+
+    version('1.40-2', 'ad87efe2fb303d95472e73c8ca8d9a01')
+
+    depends_on('r-tensora', type=('build', 'run'))
+    depends_on('r-robustbase', type=('build', 'run'))
+    depends_on('r-energy', type=('build', 'run'))
+    depends_on('r-bayesm', type=('build', 'run'))
