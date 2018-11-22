0114a88ce9bf8ccabb046e678289871f058340a1
Michael Kuhn <suraia@ikkoku.de>
2018-11-22 19:39:51 +0100

    multitime: new package (#9929)

---
+++ var/spack/repos/builtin/packages/multitime/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Multitime(AutotoolsPackage):
+    """multitime is, in essence, a simple extension to time which runs a
+    command multiple times and prints the timing means, standard deviations,
+    mins, medians, and maxes having done so. This can give a much better
+    understanding of the command's performance."""
+
+    homepage = "https://tratt.net/laurie/src/multitime/"
+    url      = "https://tratt.net/laurie/src/multitime/releases/multitime-1.4.tar.gz"
+
+    version('1.4', sha256='dd85c431c022d0b992f3a8816a1a3dfb414454a229c0ec22514761bf72d3ce47')
