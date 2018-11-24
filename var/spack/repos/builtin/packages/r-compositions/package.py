face26515d2c0eae8afb6ffdce1205d141a810d4
Neil Flood <neilflood@fastmail.fm>
2018-11-24 10:06:57 +1000

    r-compositions: new package, including its dependencies (#9921)
    * r-compositions: new package, including its dependencies
    
    * Include the list_url for the R package archive dir
    
    * Added explicit version ranges on dependencies, as per the relevant CRAN pages

---
+++ var/spack/repos/builtin/packages/r-compositions/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RCompositions(RPackage):
+    """Compositional Data Analysis"""
+
+    homepage = "https://cran.r-project.org/web/packages/compositions/index.html"
+    url      = "https://cran.r-project.org/src/contrib/compositions_1.40-2.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/compositions"
+
+    version('1.40-2', 'ad87efe2fb303d95472e73c8ca8d9a01')
+
+    depends_on('r@2.2.0:', type=('build', 'run'))
+    depends_on('r-tensora', type=('build', 'run'))
+    depends_on('r-robustbase', type=('build', 'run'))
+    depends_on('r-energy', type=('build', 'run'))
+    depends_on('r-bayesm', type=('build', 'run'))
