de72dd63eb0860985e997e7b4bb4f5dd8d15cfc5
Greg Sjaardema <gsjaardema@gmail.com>
2018-11-28 15:06:56 -0700

    SEACAS: Shorten long lines; fixup indentation

--- var/spack/repos/builtin/packages/seacas/package.py
+++ var/spack/repos/builtin/packages/seacas/package.py
@@ -14,9 +14,11 @@ from spack.operating_systems.mac_os import macos_version
 #  Kokkos enable/disable
 #  Data Warehouse (FAODEL) enable/disable
 
+
 class Seacas(CMakePackage):
-    """The SEACAS Project contains the Exodus and IOSS libraries and a collection 
-    of applications which create, query, modify, or translate exodus databases.
+    """The SEACAS Project contains the Exodus and IOSS libraries and a
+     collection of applications which create, query, modify, or
+     translate exodus databases.
     """
     homepage = "http://gsjaardema.github.io/seacas/"
     git      = "https://github.com/gsjaardema/seacas.git"
@@ -134,7 +136,6 @@ class Seacas(CMakePackage):
 
     # Everything should be compiled position independent (-fpic)
 
-
     depends_on('netcdf@4.6.2+mpi+parallel-netcdf', when='+mpi')
     depends_on('netcdf@4.6.2~mpi', when='~mpi')
     depends_on('cgns@develop+mpi+scoping', when='+cgns +mpi')
@@ -181,49 +182,92 @@ class Seacas(CMakePackage):
             ])
         else:
             options.extend([
-                '-DSEACASProj_ENABLE_SEACASAlgebra:BOOL=%s' % ('ON' if '+algebra' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASAprepro:BOOL=%s' % ('ON' if '+aprepro' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASAprepro_lib:BOOL=%s' % ('ON' if '+aprepro_lib' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASBlot:BOOL=%s' % ('ON' if '+blot' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASChaco:BOOL=%s' % ('ON' if '+chaco' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASConjoin:BOOL=%s' % ('ON' if '+conjoin' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASEjoin:BOOL=%s' % ('ON' if '+ejoin' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASEpu:BOOL=%s' % ('ON' if '+epu' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASEx1ex2v2:BOOL=%s' % ('ON' if '+ex1ex2v2' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASEx2ex1v2:BOOL=%s' % ('ON' if '+ex2ex1v2' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExo2mat:BOOL=%s' % ('ON' if '+exo2mat' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExo_format:BOOL=%s' % ('ON' if '+exo_format' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExodiff:BOOL=%s' % ('ON' if '+exodiff' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExodus:BOOL=%s' % ('ON' if '+exodus' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExodus_for:BOOL=%s' % ('ON' if '+exodus' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExoIIv2for32:BOOL=%s' % ('ON' if '+exodus' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExomatlab:BOOL=%s' % ('ON' if '+exomatlab' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExotec2:BOOL=%s' % ('ON' if '+exotec2' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExotxt:BOOL=%s' % ('ON' if '+exotxt' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASExplore:BOOL=%s' % ('ON' if '+explore' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASFastq:BOOL=%s' % ('ON' if '+fastq' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASGen3D:BOOL=%s' % ('ON' if '+gen3d' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASGenshell:BOOL=%s' % ('ON' if '+genshell' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASGjoin:BOOL=%s' % ('ON' if '+gjoin' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASGrepos:BOOL=%s' % ('ON' if '+grepos' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASIoss:BOOL=%s' % ('ON' if '+ioss' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASMapvar:BOOL=%s' % ('ON' if '+mapvar' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASMapvar-kd:BOOL=%s' % ('ON' if '+mapvar-kd' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASMapvarlib:BOOL=%s' % ('ON' if '+mapvar' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASMapvarlib:BOOL=%s' % ('ON' if '+mapvar-kd' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASMat2exo:BOOL=%s' % ('ON' if '+mat2exo' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASNemesis:BOOL=%s' % ('ON' if '+nemesis' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASNemslice:BOOL=%s' % ('ON' if '+nemslice' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASNemspread:BOOL=%s' % ('ON' if '+nemspread' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASNumbers:BOOL=%s' % ('ON' if '+numbers' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASPLT:BOOL=%s' % ('ON' if '+plt' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASSVDI:BOOL=%s' % ('ON' if '+svdi' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASSlice:BOOL=%s' % ('ON' if '+slice' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASSupes:BOOL=%s' % ('ON' if '+supes' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASSuplib:BOOL=%s' % ('ON' if '+suplib' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASSuplibC:BOOL=%s' % ('ON' if '+suplib' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASSuplibCpp:BOOL=%s' % ('ON' if '+suplib' in spec else 'OFF'),
-                '-DSEACASProj_ENABLE_SEACASTxtexo:BOOL=%s' % ('ON' if '+txtexo' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASAlgebra:BOOL=%s' % (
+                    'ON' if '+algebra' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASAprepro:BOOL=%s' % (
+                    'ON' if '+aprepro' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASAprepro_lib:BOOL=%s' % (
+                    'ON' if '+aprepro_lib' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASBlot:BOOL=%s' % (
+                    'ON' if '+blot' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASChaco:BOOL=%s' % (
+                    'ON' if '+chaco' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASConjoin:BOOL=%s' % (
+                    'ON' if '+conjoin' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASEjoin:BOOL=%s' % (
+                    'ON' if '+ejoin' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASEpu:BOOL=%s' % (
+                    'ON' if '+epu' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASEx1ex2v2:BOOL=%s' % (
+                    'ON' if '+ex1ex2v2' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASEx2ex1v2:BOOL=%s' % (
+                    'ON' if '+ex2ex1v2' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExo2mat:BOOL=%s' % (
+                    'ON' if '+exo2mat' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExo_format:BOOL=%s' % (
+                    'ON' if '+exo_format' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExodiff:BOOL=%s' % (
+                    'ON' if '+exodiff' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExodus:BOOL=%s' % (
+                    'ON' if '+exodus' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExodus_for:BOOL=%s' % (
+                    'ON' if '+exodus' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExoIIv2for32:BOOL=%s' % (
+                    'ON' if '+exodus' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExomatlab:BOOL=%s' % (
+                    'ON' if '+exomatlab' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExotec2:BOOL=%s' % (
+                    'ON' if '+exotec2' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExotxt:BOOL=%s' % (
+                    'ON' if '+exotxt' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASExplore:BOOL=%s' % (
+                    'ON' if '+explore' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASFastq:BOOL=%s' % (
+                    'ON' if '+fastq' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASGen3D:BOOL=%s' % (
+                    'ON' if '+gen3d' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASGenshell:BOOL=%s' % (
+                    'ON' if '+genshell' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASGjoin:BOOL=%s' % (
+                    'ON' if '+gjoin' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASGrepos:BOOL=%s' % (
+                    'ON' if '+grepos' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASIoss:BOOL=%s' % (
+                    'ON' if '+ioss' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASMapvar:BOOL=%s' % (
+                    'ON' if '+mapvar' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASMapvar-kd:BOOL=%s' % (
+                    'ON' if '+mapvar-kd' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASMapvarlib:BOOL=%s' % (
+                    'ON' if '+mapvar' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASMapvarlib:BOOL=%s' % (
+                    'ON' if '+mapvar-kd' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASMat2exo:BOOL=%s' % (
+                    'ON' if '+mat2exo' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASNemesis:BOOL=%s' % (
+                    'ON' if '+nemesis' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASNemslice:BOOL=%s' % (
+                    'ON' if '+nemslice' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASNemspread:BOOL=%s' % (
+                    'ON' if '+nemspread' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASNumbers:BOOL=%s' % (
+                    'ON' if '+numbers' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASPLT:BOOL=%s' % (
+                    'ON' if '+plt' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASSVDI:BOOL=%s' % (
+                    'ON' if '+svdi' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASSlice:BOOL=%s' % (
+                    'ON' if '+slice' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASSupes:BOOL=%s' % (
+                    'ON' if '+supes' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASSuplib:BOOL=%s' % (
+                    'ON' if '+suplib' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASSuplibC:BOOL=%s' % (
+                    'ON' if '+suplib' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASSuplibCpp:BOOL=%s' % (
+                    'ON' if '+suplib' in spec else 'OFF'),
+                '-DSEACASProj_ENABLE_SEACASTxtexo:BOOL=%s' % (
+                    'ON' if '+txtexo' in spec else 'OFF'),
             ])
 
         if '+kokkos' in spec:
@@ -239,18 +283,18 @@ class Seacas(CMakePackage):
                     '-DCUDA_TOOLKIT_ROOT_DIR:PATH=${CUDA_PATH}',
                     '-DKOKKOS_ENABLE_DEPRECATED_CODE:BOOL=OFF',
                     '-DKokkos_ENABLE_Pthread:BOOL=OFF',
-                    ])
+                ])
             else:
                 options.extend([
                     '-DSEACASProj_ENABLE_Kokkos:BOOL=ON',
                     '-DSEACASProj_ENABLE_OpenMP:Bool=ON',
                     '-DKOKKOS_ENABLE_DEPRECATED_CODE:BOOL=OFF',
                     '-DKokkos_ENABLE_Pthread:BOOL=OFF',
-                    ])
+                ])
         else:
             options.extend([
                 '-DSEACASProj_ENABLE_Kokkos:BOOL=OFF'
-                ])
+            ])
 
         if '+thread_safe' in spec:
             options.extend([
@@ -365,5 +409,3 @@ class Seacas(CMakePackage):
         ])
 
         return options
-
-    
