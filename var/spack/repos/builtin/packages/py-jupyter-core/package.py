96a947ab7cafaf59274bcc04430c1882a0dde1bd
jwallior <jwallior@users.noreply.github.com>
2018-11-28 22:44:15 -0500

    New versions for a few python modules (#9969)
    * py-jupyter-console: Add version 5.2.0
    
    * py-jupyter-core: add version 4.4.0
    
    * py-lxml: add version 4.2.5
    
    * py-zmq: add version 17.1.2
    
    * py-terminado: add version 0.8.1
    
    * py-pexpect: add version 4.6.0
    
    * py-pytables: add version 3.4.4

--- var/spack/repos/builtin/packages/py-jupyter-core/package.py
+++ var/spack/repos/builtin/packages/py-jupyter-core/package.py
@@ -12,6 +12,7 @@ class PyJupyterCore(PythonPackage):
     homepage = "http://jupyter-core.readthedocs.io/"
     url      = "https://github.com/jupyter/jupyter_core/archive/4.2.0.tar.gz"
 
+    version('4.4.0', 'f2bdb8be2959d3043b77508dd6498687')
     version('4.2.0', '25c1fc68b1b73c0a2e616c76f02bf061')
     version('4.1.1', '2fce5ff60291bc01b39b1f00b3cbb784')
     version('4.1.0', 'b7e928f965f68aef13fea1bf9d6384aa')
