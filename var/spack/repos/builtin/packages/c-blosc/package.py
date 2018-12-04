e9b9a8fd604f5036c6e24f6045411735d336cf32
Axel Huebl <axel.huebl@plasma.ninja>
2018-12-04 16:10:08 +0100

    c-blosc: 1.15.0 (#10009)
    Add latest version of c-blosc.

--- var/spack/repos/builtin/packages/c-blosc/package.py
+++ var/spack/repos/builtin/packages/c-blosc/package.py
@@ -14,13 +14,14 @@ class CBlosc(CMakePackage):
     homepage = "http://www.blosc.org"
     url      = "https://github.com/Blosc/c-blosc/archive/v1.11.1.tar.gz"
 
-    version('1.12.1', '6fa4ecb7ef70803a190dd386bf4a2e93')
-    version('1.11.1', 'e236550640afa50155f3881f2d300206')
-    version('1.9.2',  'dd2d83069d74b36b8093f1c6b49defc5')
-    version('1.9.1',  '7d708d3daadfacf984a87b71b1734ce2')
-    version('1.9.0',  'e4c1dc8e2c468e5cfa2bf05eeee5357a')
-    version('1.8.1',  'd73d5be01359cf271e9386c90dcf5b05')
-    version('1.8.0',  '5b92ecb287695ba20cc33d30bf221c4f')
+    version('1.15.0', sha256='dbbb01f9fedcdf2c2ff73296353a9253f44ce9de89c081cbd8146170dce2ba8f')
+    version('1.12.1', sha256='e04535e816bb942bedc9a0ba209944d1eb34e26e2d9cca37f114e8ee292cb3c8')
+    version('1.11.1', sha256='d15937961d37b0780b8fb0641483eb9f6d4c379f88ac7ee84ff5dd06c2b72360')
+    version('1.9.2',  sha256='6349ab927705a451439b2e23ec5c3473f6b7e444e6d4aafaff76b789713e9fee')
+    version('1.9.1',  sha256='e4433fb0708517607cf4377837c4589807b9a8c112b94f7978cc8aaffb719bf0')
+    version('1.9.0',  sha256='0cb5b5f7a25f71227e3dced7a6035e8ffd94736f7ae9fae546efa3b7c6e7a852')
+    version('1.8.1',  sha256='1abf048634c37aeca53eeb6a9248ea235074077028d12b3560eccf1dff7143b8')
+    version('1.8.0',  sha256='e0f8b9e12e86776a1b037385826c55006da6e2ae4973dac5b5ad3cfcf01e9043')
 
     variant('avx2', default=True, description='Enable AVX2 support')
 
