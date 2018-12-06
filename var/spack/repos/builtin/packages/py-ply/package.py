d8e714b7dfa2e8e427a35c9011e0d8308fc4fcc0
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2018-12-06 22:31:36 +0100

    fix checksum for py-ply

--- var/spack/repos/builtin/packages/py-ply/package.py
+++ var/spack/repos/builtin/packages/py-ply/package.py
@@ -11,5 +11,5 @@ class PyPly(PythonPackage):
     homepage = "http://www.dabeaz.com/ply"
     url      = "http://www.dabeaz.com/ply/ply-3.11.tar.gz"
 
-    version('3.11', sha256='928c5642612f4710b168d3c49c25f6ece2913a5e8d1c5e37fde5d6162fec3fd2')
+    version('3.11', sha256='00c7c1aaa88358b9c765b6d3000c6eec0ba42abca5351b095321aef446081da3')
     version('3.8', '94726411496c52c87c2b9429b12d5c50', url='http://www.dabeaz.com/ply/ply-3.8.tar.gz')
