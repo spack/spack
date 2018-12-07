efd7c4c6694a288dc3bd332d916b386c66487854
Mario <maamelara@gmail.com>
2018-12-07 08:06:09 -0800

    Remove git and add new version hash

--- var/spack/repos/builtin/packages/spades/package.py
+++ var/spack/repos/builtin/packages/spades/package.py
@@ -13,7 +13,7 @@ class Spades(CMakePackage):
     homepage = "http://cab.spbu.ru/software/spades/"
     url      = "http://cab.spbu.ru/files/release3.10.1/SPAdes-3.10.1.tar.gz"
 
-    version('3.13.0', git='https://github.com/ablab/spades.git', tag='v3.13.0')
+    version('3.13.0', 'c63442248c4c712603979fa70503c2bff82354f005acda2abc42dd5598427040')
     version('3.12.0', '15b48a3bcbbe6a8ad58fd04ba5d3f1015990fbfd9bdf4913042803b171853ac7')
     version('3.11.1', '478677b560e2f98db025e8efd5245cdf')
     version('3.10.1', 'dcab7d145af81b59cc867562f27536c3')
@@ -27,9 +27,4 @@ class Spades(CMakePackage):
 
     conflicts('%gcc@7.1.0:', when='@:3.10.1')
 
-    @property
-    def root_cmakelists_dir(self):
-        if self.spec.satisfies("@:3.12.0"):
-            return "src"
-        else:
-            return "assembler/src"
+    root_cmakelists_dir = "src"
