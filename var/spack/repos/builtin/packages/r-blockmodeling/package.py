76bef6d16167daa3a1ff67bc6092e18331437968
Justin Stanley <molecuul@users.noreply.github.com>
2018-12-07 11:24:43 -0600

    r-blockmodeling: new package at 0.3.1 (#10044)

---
+++ var/spack/repos/builtin/packages/r-blockmodeling/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RBlockmodeling(RPackage):
+    """blockmodeling: Generalized and Classical
+       Blockmodeling of Valued Networks"""
+
+    homepage = "https://cran.r-project.org/package=blockmodeling"
+    url      = "https://cran.rstudio.com/src/contrib/blockmodeling_0.3.1.tar.gz"
+    list_url = "https://cran.rstudio.com/src/contrib/Archive/blockmodeling/"
+
+    version('0.3.1', sha256='39e8360400cec6baa920d5589d4e779568bdf2954f7331be0e3cadf22a217d31')
+
+    depends_on('r-matrix', type=('build', 'run'))
+    depends_on('r-doparallel', type=('build', 'run'))
+    depends_on('r-dorng', type=('build', 'run'))
+    depends_on('r-foreach', type=('build', 'run'))
