dee5cc45da40800db7a2459194417ab2d362d735
Miles Perry <mpperry@iastate.edu>
2018-12-07 09:25:21 -0600

    flake8 edits

--- var/spack/repos/builtin/packages/spm/package.py
+++ var/spack/repos/builtin/packages/spm/package.py
@@ -24,18 +24,17 @@ from spack import *
 
 
 class Spm(Package):
-    """FIXME: Put a proper description of your package here."""
+    """Statistical Parametric Mapping refers to the construction and assessment
+       of spatially extended statistical processes used to test hypotheses
+       about functional imaging data. These ideas have been instantiated in
+       software that is called SPM."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "http://www.example.com"
+    homepage = "https://www.fil.ion.ucl.ac.uk/spm/"
     url      = "https://www.fil.ion.ucl.ac.uk/spm/download/restricted/utopia/spm12_r7219.zip"
 
     version('12_r7219', sha256='b46fe8ce5ab537caeea7634c650f3a12fe2716f6a2e8ac15aa0d62b3652fe764')
 
     depends_on('zip', type='build')
-
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
 
     def install(self, spec, prefix):
         unzip = which('unzip')
