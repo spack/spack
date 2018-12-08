bf38fcbbeb1e19d7d31038e71fe2a8c05486ed42
Justin Stanley <molecuul@users.noreply.github.com>
2018-12-07 19:39:07 -0600

    scallop: new package at 0.10.3 (#10041)

---
+++ var/spack/repos/builtin/packages/scallop/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Scallop(AutotoolsPackage):
+    """Scallop is a reference-based transcriptome assembler for RNA-seq"""
+
+    homepage = "https://github.com/Kingsford-Group/scallop"
+    url      = "https://github.com/Kingsford-Group/scallop/releases/download/v0.10.3/scallop-0.10.3.tar.gz"
+
+    version('0.10.3', sha256='04eb3ab27ed8c7ae38e1780d6b2af16b6a2c01807ffafd59e819d33bfeff58a0')
+
+    depends_on('clp')
+    depends_on('boost')
+    depends_on('htslib@1.5:')
+
+    def configure_args(self):
+        return [
+            '--with-clp={0}'.format(self.spec['clp'].prefix),
+            '--with-htslib={0}'.format(self.spec['htslib'].prefix),
+            '--with-boost={0}'.format(self.spec['boost'].prefix),
+        ]
