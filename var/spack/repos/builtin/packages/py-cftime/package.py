9c7a21cb7567f1cb63d780e39573f45b5e854624
Neil Flood <Neil.Flood@des.qld.gov.au>
2018-12-10 08:47:52 +1000

    py-cftime: new package

---
+++ var/spack/repos/builtin/packages/py-cftime/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyCftime(PythonPackage):
+    """Python library for decoding time units and variable values in a
+       netCDF file conforming to the Climate and Forecasting (CF)
+       netCDF conventions"""
+
+    homepage = "https://unidata.github.io/cftime/"
+    url      = "https://github.com/Unidata/cftime/archive/v1.0.3.4rel.tar.gz"
+
+    version('1.0.3.4', sha256='f261ff8c65ceef4799784cd999b256d608c177d4c90b083553aceec3b6c23fd3')
+
+    depends_on('py-cython', type='build')
+    depends_on('py-numpy', type=('build', 'run'))
