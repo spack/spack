969170c846fa45a955b853be19826dc034484bd5
Adam J. Stewart <ajstewart426@gmail.com>
2018-12-13 22:05:24 -0600

    Add new packages for ELSD and ELSDc (#10072)

---
+++ var/spack/repos/builtin/packages/elsd/package.py
@@ -0,0 +1,28 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Elsd(MakefilePackage):
+    """ELSD: Ellipse and Line Segment Detector"""
+
+    homepage = "http://ubee.enseeiht.fr/vision/ELSD/"
+    git      = "https://github.com/viorik/ELSD.git"
+
+    version('master', branch='master')
+
+    depends_on('blas')
+    depends_on('lapack')
+
+    def edit(self, spec, prefix):
+        lapack_blas = spec['lapack'].libs + spec['blas'].libs
+
+        makefile = FileFilter('makefile')
+        makefile.filter('-llapack -lblas', lapack_blas.link_flags)
+
+    def install(self, spec, prefix):
+        mkdir(prefix.bin)
+        install('elsd', prefix.bin)
