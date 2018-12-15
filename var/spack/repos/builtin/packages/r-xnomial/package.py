693b27a024e90c3acc1fb63ba10fbffe63ebebca
Justin Stanley <molecuul@users.noreply.github.com>
2018-12-15 08:43:08 -0600

    r-xnomial: new package at 1.0.4 (#10111)
    * r-xnomial: new package at 1.0.4
    
    * r-xnomial: add list_url

---
+++ var/spack/repos/builtin/packages/r-xnomial/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+# See the Spack documentation for more information on packaging.
+
+from spack import *
+
+
+class RXnomial(RPackage):
+    """XNomial: Exact Goodness-of-Fit Test for Multinomial Data with Fixed
+       Probabilities"""
+
+    homepage = "https://cran.r-project.org/package=XNomial"
+    url      = "https://cran.r-project.org/src/contrib/XNomial_1.0.4.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/XNomial/"
+
+    version('1.0.4', sha256='e6237f79d96f02bb30af1cf055ae9f70541abba34ce045a9d4359b5304189dd7')
+
+    depends_on('r@2.14:', type=('build', 'run'))
+    depends_on('r-knitr', type=('build', 'run'))
