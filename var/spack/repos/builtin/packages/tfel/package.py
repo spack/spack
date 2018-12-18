606035daae7948384ba8aea790fbd10f07c173bc
Helfer Thomas <thomas.helfer@cea.fr>
2018-12-18 15:18:38 +0100

    Small fix

--- var/spack/repos/builtin/packages/tfel/package.py
+++ var/spack/repos/builtin/packages/tfel/package.py
@@ -10,7 +10,7 @@ class Tfel(CMakePackage):
     """
     The TFEL project is a collaborative development of CEA
     (French Alternative Energies and Atomic Energy Commission) and
-    EDF (Électricite de France).
+    EDF (Electricite de France).
 
     It mostly contains the MFront code generator which translates
     a set of closely related domain specific languages into plain C++
