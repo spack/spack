3bc33a2172924eb8858cdd1e4ae1eeb5be4cab7f
Matthias Wolf <matthias.wolf@epfl.ch>
2018-12-18 11:12:34 +0100

    flatbuffers: add 1.10.0
    Also unpin the version from the arrow package.

--- var/spack/repos/builtin/packages/flatbuffers/package.py
+++ var/spack/repos/builtin/packages/flatbuffers/package.py
@@ -13,5 +13,6 @@ class Flatbuffers(CMakePackage):
     homepage = "http://google.github.io/flatbuffers/"
     url      = "https://github.com/google/flatbuffers/archive/v1.9.0.tar.gz"
 
+    version('1.10.0', '3714e3db8c51e43028e10ad7adffb9a36fc4aa5b1a363c2d0c4303dd1be59a7c')
     version('1.9.0', '8be7513bf960034f6873326d09521a4b')
     version('1.8.0', '276cab8303c4189cbe3b8a70e0515d65')
