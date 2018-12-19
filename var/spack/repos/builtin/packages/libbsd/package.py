c4c1aaab5a7040666808839fedcfb875e6d1e271
Adam J. Stewart <ajstewart426@gmail.com>
2018-12-19 11:37:11 -0600

    Restrict patch to oldest version of libbsd (#10145)

--- var/spack/repos/builtin/packages/libbsd/package.py
+++ var/spack/repos/builtin/packages/libbsd/package.py
@@ -17,6 +17,8 @@ class Libbsd(AutotoolsPackage):
     url      = "https://libbsd.freedesktop.org/releases/libbsd-0.9.1.tar.xz"
 
     version('0.9.1', sha256='56d835742327d69faccd16955a60b6dcf30684a8da518c4eca0ac713b9e0a7a4')
-    version('0.8.6', '4ab7bec639af17d0aacb50222b479110')
+    version('0.9.0', sha256='8a469afd1bab340992cf99e1e6b7ae4f4c54882d663d8a2c5ea52250617afb01')
+    version('0.8.7', sha256='f548f10e5af5a08b1e22889ce84315b1ebe41505b015c9596bad03fd13a12b31')
+    version('0.8.6', sha256='467fbf9df1f49af11f7f686691057c8c0a7613ae5a870577bef9155de39f9687')
 
-    patch('cdefs.h.patch', when='%gcc@:4')
+    patch('cdefs.h.patch', when='@0.8.6 %gcc@:4')
