be068d09cb53f99bc088b62e53a9b81e0dd40a4b
Andrew Gaspar <andrew.gaspar@outlook.com>
2018-12-26 18:19:22 -0700

    Remove extra whitespace from string-view-lite package

--- var/spack/repos/builtin/packages/string-view-lite/package.py
+++ var/spack/repos/builtin/packages/string-view-lite/package.py
@@ -15,7 +15,7 @@ class StringViewLite(Package):
 
     homepage = "https://github.com/martinmoene/string-view-lite"
     url      = "https://github.com/martinmoene/string-view-lite/archive/v1.0.0.tar.gz"
-    
+
     version('1.1.0', sha256='88fb33ad7a345a25aca4ddf3244afd81b8d54787e5fb316a7ed60f702bc646cd')
     version('1.0.0', sha256='44e30dedd6f4777e646da26528f9d2d5cc96fd0fa79e2e5c0adc14817d048d63')
     version('0.2.0', sha256='c8ae699dfd2ccd15c5835e9b1d246834135bbb91b82f7fc4211b8ac366bffd34')
