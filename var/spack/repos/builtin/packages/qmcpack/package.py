d9c2222825f8b418037eae842fdd312ee908b55f
naromero77 <naromero@anl.gov>
2018-12-31 22:19:24 -0600

    QMCPACK compiler C++14 conflicts were off by one version.

--- var/spack/repos/builtin/packages/qmcpack/package.py
+++ var/spack/repos/builtin/packages/qmcpack/package.py
@@ -93,11 +93,11 @@ class Qmcpack(CMakePackage):
     conflicts('%intel@19:', when='+cuda', msg=cuda_warning)
 
     # QMCPACK 3.6.0 or later requires support for C++14
-    compiler_warning = 'QMCPACK 3.6.0 or later requires a \
-    compiler with support for C++14'
-    conflicts('%gcc@:5', when='@3.6.0:', msg=compiler_warning)
-    conflicts('%intel@:18', when='@3.6.0:', msg=compiler_warning)
-    conflicts('%pgi@:18', when='@3.6.0:', msg=compiler_warning)
+    compiler_warning = 'QMCPACK 3.6.0 or later requires a ' \
+                       'compiler with support for C++14'
+    conflicts('%gcc@:4', when='@3.6.0:', msg=compiler_warning)
+    conflicts('%intel@:17', when='@3.6.0:', msg=compiler_warning)
+    conflicts('%pgi@:17', when='@3.6.0:', msg=compiler_warning)
     conflicts('%llvm@:3.4', when='@3.6.0:', msg=compiler_warning)
 
     # Dependencies match those in the QMCPACK manual.
