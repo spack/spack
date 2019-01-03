505b090f3b897fb1294f270a2136decb1320003b
Jon Rood <jon.rood@nrel.gov>
2019-01-03 16:17:50 -0700

    Fixing mistake in py-numpy site.cfg generation.

--- var/spack/repos/builtin/packages/py-numpy/package.py
+++ var/spack/repos/builtin/packages/py-numpy/package.py
@@ -125,10 +125,11 @@ class PyNumpy(PythonPackage):
                     if spec.satisfies('+blas'):
                         f.write('[blas]\n')
                         f.write('blas_libs=%s\n' % (names))
+                        write_library_dirs(f, dirs)
                     if spec.satisfies('+lapack'):
                         f.write('[lapack]\n')
                         f.write('lapack_libs=%s\n' % (names))
-                    write_library_dirs(f, dirs)
+                        write_library_dirs(f, dirs)
                 else:
                     # The section title for the defaults changed in @1.10, see
                     # https://github.com/numpy/numpy/blob/master/site.cfg.example
