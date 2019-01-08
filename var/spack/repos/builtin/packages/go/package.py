bfb9a9882be8c2727ef6484e502e15c172c45172
Michael Kuhn <suraia@ikkoku.de>
2019-01-08 13:57:25 +0100

    go: add 1.11.4 (#10272)

--- var/spack/repos/builtin/packages/go/package.py
+++ var/spack/repos/builtin/packages/go/package.py
@@ -35,6 +35,7 @@ class Go(Package):
 
     extendable = True
 
+    version('1.11.4', '4cfd42720a6b1e79a8024895fa6607b69972e8e32446df76d6ce79801bbadb15')
     version('1.11.2', '042fba357210816160341f1002440550e952eb12678f7c9e7e9d389437942550')
     version('1.11.1', '558f8c169ae215e25b81421596e8de7572bd3ba824b79add22fba6e284db1117')
     version('1.11',   'afc1e12f5fe49a471e3aae7d906c73e9d5b1fdd36d52d72652dde8f6250152fb')
