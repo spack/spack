3fcfca89b0df2dcbb39bd0d239f2728c8391cfb8
Eric Martin <ericmartin@wustl.edu>
2019-01-08 12:56:32 -0600

    prokka: fixes and switch to latest version available

--- var/spack/repos/builtin/packages/prokka/package.py
+++ var/spack/repos/builtin/packages/prokka/package.py
@@ -1,27 +1,8 @@
-##############################################################################
-# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/spack/spack
-# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 from spack import *
 
 
@@ -30,10 +11,9 @@ class Prokka(Package):
     genomes quickly and produce standards-compliant output files."""
 
     homepage = "https://github.com/tseemann/prokka"
-    url      = "https://github.com/tseemann/prokka/archive/v1.12.tar.gz"
+    url      = "https://github.com/tseemann/prokka/archive/v1.13.tar.gz"
 
-    version('1.13', '168193a4c61263759784564581523640')
-    version('1.12', '658c4c203ddded3623e68a36f94cabec')
+    version('1.13.4', sha256='19a699effe3fd38b3b50007473634161a1749eb7af00d1a67c42afa20446e5e3')
 
     depends_on('aragorn', type='run')
     depends_on('blast-plus', type='run')
@@ -46,12 +26,13 @@ class Prokka(Package):
     depends_on('perl-html-parser', type='run')
     depends_on('perl-swissknife', type='run')
     depends_on('perl-text-unidecode', type='run')
-    depends_on('perl-time-piece', type='run')
     depends_on('perl-xml-simple', type='run')
     depends_on('prodigal', type='run')
     depends_on('signalp', type='run')
-    depends_on('tbl2asn', type='run')
 
     def install(self, spec, prefix):
         install_tree('bin', prefix.bin)
         install_tree('db', prefix.db)
+
+        #Use bundled tbl2asn binary until PR#6875 is merged
+        install('binaries/linux/tbl2asn', prefix.bin)
