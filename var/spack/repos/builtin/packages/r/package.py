0b2314bbc4c4cfb6f18e3e07513e2750a24344ed
Simon Flood <simonflood@users.noreply.github.com>
2019-01-09 18:19:58 +0000

    R: add version 3.5.2 (#10292)

--- var/spack/repos/builtin/packages/r/package.py
+++ var/spack/repos/builtin/packages/r/package.py
@@ -20,6 +20,7 @@ class R(AutotoolsPackage):
 
     extendable = True
 
+    version('3.5.2', sha256='e53d8c3cf20f2b8d7a9c1631b6f6a22874506fb392034758b3bb341c586c5b62')
     version('3.5.1', sha256='0463bff5eea0f3d93fa071f79c18d0993878fd4f2e18ae6cf22c1639d11457ed')
     version('3.5.0', 'c0455dbfa76ca807e4dfa93d49dcc817')
     version('3.4.4', '9d6f73be072531e95884c7965ff80cd8')
