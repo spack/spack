e00443f3e470d24f0c7435738a904ed884002241
Matthias Wolf <m+git@sushinara.net>
2019-01-14 14:14:13 +0100

    py-pytouchreader: add 1.2.0, create module. (#182)

--- var/spack/repos/builtin/packages/py-pytouchreader/package.py
+++ var/spack/repos/builtin/packages/py-pytouchreader/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
+
 from spack import *
 
 
@@ -13,10 +15,21 @@ class PyPytouchreader(PythonPackage):
     url      = "ssh://bbpcode.epfl.ch/hpc/PyModules"
 
     version('develop', git=url, clean=False)
+    version('1.2.0', tag='v1.2.0', git=url, clean=False, preferred=True)
 
     depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('py-setuptools-scm', type=('build', 'run'))
     depends_on('py-future', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-lazy-property', type=('build', 'run'))
 
     build_directory = 'PyTouchReader'
+
+    @run_before('build')
+    def link_git(self):
+        """Link the git directory into the local directory
+
+        Needed for `setuptools_scm` to work.
+        """
+        with working_dir(self.build_directory):
+            os.symlink('../.git', '.git')
