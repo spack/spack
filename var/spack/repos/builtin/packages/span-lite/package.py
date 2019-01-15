04d4b0763601f360981a900986cb8d0482f499e6
Andrew Gaspar <andrew.gaspar@outlook.com>
2019-01-14 17:22:29 -0700

    Add span-lite version (#10347)

--- var/spack/repos/builtin/packages/span-lite/package.py
+++ var/spack/repos/builtin/packages/span-lite/package.py
@@ -16,6 +16,7 @@ class SpanLite(Package):
     homepage = "https://github.com/martinmoene/span-lite"
     url      = "https://github.com/martinmoene/span-lite/archive/v0.3.0.tar.gz"
 
+    version('0.4.0', sha256='973858839cc881f9457a874b9c39e16c3d4a798b1204258bb0ca997cd13d1a87')
     version('0.3.0', sha256='e083f368167fe632f866956edaa2c7a7d57a33ffb0d8def9b9f1a9daa47834bb')
     version('0.2.0', sha256='6e3305fe868442410a00962a39fc59ed494cecc4f99fe2aff187e33932f06e46')
     version('0.1.0', sha256='0a84b9369f86beba326e2160b683fd0922f416ce136437751a9ed70afcc67a1c')
