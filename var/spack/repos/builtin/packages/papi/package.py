4b37a356f19570d517dcf85465b23399a97683d1
Gerald Ragghianti <gragghia@methane.icl.utk.edu>
2019-01-25 10:28:18 -0500

    Added multiple variants to support PAPI optional components

--- var/spack/repos/builtin/packages/papi/package.py
+++ var/spack/repos/builtin/packages/papi/package.py
@@ -22,6 +22,7 @@ class Papi(Package):
     homepage = "http://icl.cs.utk.edu/papi/index.html"
 
     url      = "http://icl.cs.utk.edu/projects/papi/downloads/papi-5.4.1.tar.gz"
+    version('develop', git="http://bitbucket.org/icl/papi")
     version('5.6.0', 'fdd075860b2bc4b8de8b8b5c3abf594a')
     version('5.5.1', '86a8a6f3d0f34cd83251da3514aae15d')
     version('5.5.0', '5e1244a04ca031d4cc29b46ce3dd05b5')
@@ -29,11 +30,48 @@ class Papi(Package):
     version('5.4.1', '9134a99219c79767a11463a76b0b01a2')
     version('5.3.0', '367961dd0ab426e5ae367c2713924ffb')
 
+    variant('example', default=True, description='')
+    variant('cuda', default=False, description='')
+    variant('nvml', default=False, description='')
+    variant('infiniband', default=False, description='')
+    variant('infiniband_umad', default=False, description='')
+    variant('powercap', default=False, description='')
+    variant('rapl', default=False, description='')
+    variant('lmsensors', default=False, description='')
+
+    conflicts('+cuda', when='@:5.6.0')
+    conflicts('+nvml', when='@:5.6.0')
+
+    depends_on('cuda', when='+cuda')
+    depends_on('cuda', when='+nvml')
+    depends_on('lm-sensors', when='+lmsensors')
+
     # Does not build with newer versions of gcc, see
     # https://bitbucket.org/icl/papi/issues/46/cannot-compile-on-arch-linux
     patch('https://bitbucket.org/icl/papi/commits/53de184a162b8a7edff48fed01a15980664e15b1/raw', sha256='64c57b3ad4026255238cc495df6abfacc41de391a0af497c27d0ac819444a1f8', when='@5.4.0:5.6.99%gcc@8')
 
+    def setup_environment(self, spack_env, run_env):
+        if '+cuda' in self.spec or 'nvml' in self.spec:
+            spack_env.set('CUDA_DIR', self.spec['cuda'].prefix)
+            run_env.prepend_path('LD_LIBRARY_PATH', join_path(self.spec['cuda'].prefix, "extras/CUPTI/lib64"))
+
     def install(self, spec, prefix):
+        with working_dir("src/components/nvml"):
+            if '+nvml' in spec:
+                configure_args = [
+                    "--with-nvml-incdir=%s/include" % spec['cuda'].prefix,
+                    "--with-nvml-libdir=%s/lib64/stubs" % spec['cuda'].prefix,
+                    "--with-cuda-dir=%s" % spec['cuda'].prefix]
+                configure(*configure_args)
+        with working_dir("src/components/infiniband_umad"):
+            if '+infiniband_umad' in spec:
+                configure()
+        with working_dir("src/components/lmsensors"):
+            if '+lmsensors' in spec:
+                configure_args = [
+                    "--with-sensors_incdir=%s/include/sensors" % spec['lm-sensors'].prefix,
+                    "--with-sensors_libdir=%s/lib64" % spec['lm-sensors'].prefix]
+                configure(*configure_args)
         with working_dir("src"):
 
             configure_args = ["--prefix=%s" % prefix]
@@ -42,6 +80,8 @@ class Papi(Package):
             # an MPI package, we ensure that all attempts to use MPI
             # fail, so that PAPI does not get confused
             configure_args.append('MPICC=:')
+
+            configure_args.append('--with-components={0}'.format(' '.join(filter(lambda x: spec.variants[x].value, spec.variants))))
 
             configure(*configure_args)
 
@@ -58,3 +98,4 @@ class Papi(Package):
                 os.rename(join_path(prefix.lib, 'libpapi.so'),
                           join_path(prefix.lib, 'libpapi.dylib'))
                 fix_darwin_install_name(prefix.lib)
+
