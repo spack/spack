9bfbdb1ba5812ce1e60d8f3a92d8e0a703654fc6
Robert Blake <blake14@llnl.gov>
2019-01-25 17:10:40 -0800

    Changing package to pass flake8

--- var/spack/repos/builtin/packages/cardioid/package.py
+++ var/spack/repos/builtin/packages/cardioid/package.py
@@ -24,6 +24,7 @@ from spack import *
 import spack.environment as ev
 import os
 
+
 class Cardioid(CMakePackage):
     """Cardiac simulation suite."""
 
@@ -38,7 +39,7 @@ class Cardioid(CMakePackage):
             description='Build with cuda support')
     variant('mfem', default=False,
             description='Build with mfem support')
-    
+
     depends_on('blas')
     depends_on('lapack')
     depends_on('mpi')
@@ -59,30 +60,30 @@ class Cardioid(CMakePackage):
             basename = str(self.spec.arch)
         else:
             basename = env.name
-            
+
         return os.path.join(self.stage.source_path, "build", basename)
-    
+
     def cmake_args(self):
         spec = self.spec
         args = [
-            "-DLAPACK_LIB:PATH="+";".join(spec['lapack'].libs.libraries),
-            "-DBLAS_LIB:PATH="+";".join(spec['blas'].libs.libraries),
+            "-DLAPACK_LIB:PATH=" + ";".join(spec['lapack'].libs.libraries),
+            "-DBLAS_LIB:PATH=" + ";".join(spec['blas'].libs.libraries),
             "-DENABLE_OPENMP:BOOL=ON",
-        
+
             "-DENABLE_MPI:BOOL=ON",
             "-DENABLE_FIND_MPI:BOOL=OFF",
-            "-DMPI_C_COMPILER:STRING="+spec['mpi'].mpicc,
-            "-DMPI_CXX_COMPILER:STRING="+spec['mpi'].mpicxx,
-            "-DCMAKE_C_COMPILER:STRING="+spec['mpi'].mpicc,
-            "-DCMAKE_CXX_COMPILER:STRING="+spec['mpi'].mpicxx,
+            "-DMPI_C_COMPILER:STRING=" + spec['mpi'].mpicc,
+            "-DMPI_CXX_COMPILER:STRING=" + spec['mpi'].mpicxx,
+            "-DCMAKE_C_COMPILER:STRING=" + spec['mpi'].mpicc,
+            "-DCMAKE_CXX_COMPILER:STRING=" + spec['mpi'].mpicxx,
         ]
-        
+
         if "+cuda" in self.spec:
             args.append("-DENABLE_CUDA:BOOL=ON")
-            args.append("-DCUDA_TOOLKIT_ROOT:PATH="+spec['cuda'].prefix)
+            args.append("-DCUDA_TOOLKIT_ROOT:PATH=" + spec['cuda'].prefix)
         else:
             args.append("-DENABLE_CUDA:BOOL=OFF")
-        
+
         if "+mfem" in self.spec:
-            args.append("-DMFEM_DIR:PATH="+spec['mfem'].prefix)
+            args.append("-DMFEM_DIR:PATH=" + spec['mfem'].prefix)
         return args
