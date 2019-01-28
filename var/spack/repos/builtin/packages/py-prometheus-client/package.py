f550efa8e849a3d889ddd084b45ebac3e9efaf00
Wyatt Spear <wspear@cs.uoregon.edu>
2019-01-28 13:23:57 -0800

    Switched to pypi url

--- var/spack/repos/builtin/packages/py-prometheus-client/package.py
+++ var/spack/repos/builtin/packages/py-prometheus-client/package.py
@@ -9,8 +9,8 @@ from spack import *
 class PyPrometheusClient(PythonPackage):
     """Prometheus instrumentation library for Python applications."""
 
-    homepage = "https://github.com/prometheus/client_python"
-    url      = "https://github.com/prometheus/client_python/archive/v0.5.0.tar.gz"
+    homepage = "https://pypi.org/project/prometheus_client/"
+    url      = "https://pypi.io/packages/source/p/prometheus_client/prometheus_client-0.5.0.tar.gz"
 
     version('0.5.0', sha256='2d7f7af343dec0a96ee849b1bba18aad9f767bf16d5eb5f0c11cae837bf22731')
 
