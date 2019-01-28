b83b42eb3f4b98823a8822c0ac0f7aed2cf4d25e
Wyatt Spear <wspear@cs.uoregon.edu>
2019-01-28 12:18:33 -0800

    Switched to pypi url

--- var/spack/repos/builtin/packages/py-async-generator/package.py
+++ var/spack/repos/builtin/packages/py-async-generator/package.py
@@ -10,7 +10,7 @@ class PyAsyncGenerator(PythonPackage):
     """Provides async generator functionality to python 3.5."""
 
     homepage = "https://pypi.org/project/async_generator/"
-    url      = "https://files.pythonhosted.org/packages/ce/b6/6fa6b3b598a03cba5e80f829e0dadbb49d7645f523d209b2fb7ea0bbb02a/async_generator-1.10.tar.gz"
+    url      = "https://pypi.io/packages/source/a/async_generator/async_generator-1.10.tar.gz"
 
     version('1.10', sha256='6ebb3d106c12920aaae42ccb6f787ef5eefdcdd166ea3d628fa8476abe712144')
 
