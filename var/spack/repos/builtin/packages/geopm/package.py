60ac9cc6026b0c89bf33d47e786083850c46fd70
Matthias Maiterth <matthias.maiterth@gmail.com>
2019-01-30 19:50:38 +0100

    geopm package: new versions, dependency updates (#10480)
    Added versions 1.0.0-rc1, 0.6.1, 0.6.0, 0.5.1
    
    hwloc dependency was removed after 0.5.1, so the related configure
    option is not specified after 0.5.1
    
    Require py-matplotlib dependency version 2.2.3 (version was not
    specified before)

--- var/spack/repos/builtin/packages/geopm/package.py
+++ var/spack/repos/builtin/packages/geopm/package.py
@@ -24,6 +24,11 @@ class Geopm(AutotoolsPackage):
     # Add additional proper versions and checksums here. "spack checksum geopm"
     version('develop', branch='dev')
     version('master', branch='master')
+    version('1.0.0-rc1', sha256='f8a2e5c384a15e9663f409de478b6372cd63e63a28d4701a33ac043fc27905e0', 
+            url='https://github.com/geopm/geopm/releases/download/v1.0.0-rc1/geopm-1.0.0+rc1.tar.gz')
+    version('0.6.1', sha256='0ca42853f90885bf213df190c3462b8675c143cc843aee0d8b8a0e30802b55a9')
+    version('0.6.0', sha256='95ccf256c2b7cb35838978152479569d154347c3065af1639ed17be1399182d3')
+    version('0.5.1', sha256='db247af55f7000b6e4628af099956349b68a637500b9d4fe8d8fb13687124d53')
     version('0.5.0', '61b454bc74d4606fe84818aef16c1be4')
     version('0.4.0', 'd4cc8fffe521296dab379857d7e2064d')
     version('0.3.0', '568fd37234396fff134f8d57b60f2b83')
@@ -38,7 +43,7 @@ class Geopm(AutotoolsPackage):
     variant('doc', default=True, description='Create man pages with ruby-ronn.')
     variant('openmp', default=True, description='Build with OpenMP.')
     variant('ompt', default=False, description='Use OpenMP Tools Interface.')
-    variant('hwloc', default=True, description='Build with hwloc.')
+    variant('hwloc', default=False, description='Build with hwloc, deprecated and ignored after v0.5.1.')
     variant('gnu-ld', default=False, description='Assume C compiler uses gnu-ld.')
 
     # Added dependencies.
@@ -50,13 +55,12 @@ class Geopm(AutotoolsPackage):
     depends_on('doxygen', type='build', when='+doc')
     depends_on('numactl')
     depends_on('mpi', when='+mpi')
-    # TODO: check if hwloc@specific-version still required with future openmpi
-    depends_on('hwloc@1.11.9', when='+hwloc')
+    depends_on('hwloc@1.11.9', when='@:0.5.1+hwloc')
     depends_on('json-c')
     depends_on('py-pandas', type='run')
     depends_on('py-numpy', type='run')
     depends_on('py-natsort', type='run')
-    depends_on('py-matplotlib', type='run')
+    depends_on('py-matplotlib@2.2.3', type='run')
 
     parallel = False
 
@@ -71,7 +75,10 @@ class Geopm(AutotoolsPackage):
         args.extend(self.enable_or_disable('doc'))
         args.extend(self.enable_or_disable('openmp'))
         args.extend(self.enable_or_disable('ompt'))
-        args.extend(self.with_or_without('hwloc', activation_value='prefix'))
+        if self.version <= Version('0.5.1'):
+            args.extend(self.with_or_without(
+                'hwloc',
+                activation_value='prefix'))
         args.extend(self.with_or_without('gnu-ld'))
 
         return args
