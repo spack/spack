0c15ca35d65bd8a067729f0df64cac2149c47e20
Sajid Ali <30510036+s-sajid-ali@users.noreply.github.com>
2019-01-30 12:45:19 -0600

    py-numexpr package: add version 2.6.9 (#10477)

--- var/spack/repos/builtin/packages/py-numexpr/package.py
+++ var/spack/repos/builtin/packages/py-numexpr/package.py
@@ -9,8 +9,9 @@ from spack import *
 class PyNumexpr(PythonPackage):
     """Fast numerical expression evaluator for NumPy"""
     homepage = "https://pypi.python.org/pypi/numexpr"
-    url      = "https://pypi.io/packages/source/n/numexpr/numexpr-2.6.5.tar.gz"
+    url      = "https://pypi.io/packages/source/n/numexpr/numexpr-2.6.9.tar.gz"
 
+    version('2.6.9', sha256='fc218b777cdbb14fa8cff8f28175ee631bacabbdd41ca34e061325b6c44a6fa6')
     version('2.6.5', 'c9b5859c11bd6da092f6c8a84a472e77')
     version('2.6.1', '6365245705b446426df9543ad218dd8e')
     version('2.5',   '84f66cced45ba3e30dcf77a937763aaa')
