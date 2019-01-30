b59b7f801e2e387d4d12e315ccaed3f26dc0f756
Jimmy Tang <jcftang@gmail.com>
2019-01-30 19:44:24 +0000

    cudnn package: add version 7.2 (#10481)

--- var/spack/repos/builtin/packages/cudnn/package.py
+++ var/spack/repos/builtin/packages/cudnn/package.py
@@ -14,6 +14,8 @@ class Cudnn(Package):
 
     version('7.3', '72666d3532850752612706601258a0b2',
             url='http://developer.download.nvidia.com/compute/redist/cudnn/v7.3.0/cudnn-9.0-linux-x64-v7.3.0.29.tgz')
+    version('7.2', '17e010153a4a95bf9c2df2a3c3ceea63',
+            url='http://developer.download.nvidia.com/compute/redist/cudnn/v7.2.1/cudnn-9.0-linux-x64-v7.2.1.38.tgz')
     version('6.0', 'a08ca487f88774e39eb6b0ef6507451d',
             url='http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0.tgz')
     version('5.1', '406f4ac7f7ee8aa9e41304c143461a69',
