2294dd1614aa24f922624f427071d50d39afc45e
zschulte <zschulte@broadinstitute.org>
2019-01-31 11:26:44 -0500

    Replaced individual urls with list_url. Removed preferred from latest version.

--- var/spack/repos/builtin/packages/gatk/package.py
+++ var/spack/repos/builtin/packages/gatk/package.py
@@ -15,33 +15,28 @@ class Gatk(Package):
     """
 
     homepage = "https://software.broadinstitute.org/gatk/"
-    url = "https://github.com/broadinstitute/gatk/releases/download/4.0.4.0/gatk-4.0.4.0.zip"
+    url = "https://github.com/broadinstitute/gatk/releases/download/4.1.0.0/gatk-4.1.0.0.zip"
+    list_url = "https://github.com/broadinstitute/gatk/releases"
 
     version(
         "4.1.0.0",
         sha256="148aa061328d922a570d0120d88f27e61e5da877f542206f4d77f2d788b7d21d",
-        url="https://github.com/broadinstitute/gatk/releases/download/4.1.0.0/gatk-4.1.0.0.zip",
-        preferred=True,
     )
     version(
         "4.0.12.0",
         sha256="733134303f4961dec589247ff006612b7a94171fab8913c5d44c836aa086865f",
-        url="https://github.com/broadinstitute/gatk/releases/download/4.0.12.0/gatk-4.0.12.0.zip",
     )
     version(
         "4.0.11.0",
         sha256="5ee23159be7c65051335ac155444c6a49c4d8e3515d4227646c0686819934536",
-        url="https://github.com/broadinstitute/gatk/releases/download/4.0.11.0/gatk-4.0.11.0.zip",
     )
     version(
         "4.0.8.1",
         sha256="e4bb082d8c8826d4f8bc8c2f83811d0e81e5088b99099d3396d284f82fbf28c9",
-        url="https://github.com/broadinstitute/gatk/releases/download/4.0.8.1/gatk-4.0.8.1.zip",
     )
     version(
         "4.0.4.0",
-        "083d655883fb251e837eb2458141fc2b",
-        url="https://github.com/broadinstitute/gatk/releases/download/4.0.4.0/gatk-4.0.4.0.zip",
+        sha256="801bbb181c275cfabc96dc0cb21f3f901634cec11efde9ba9c8b91e2834feef4",
     )
     version(
         "3.8-1",
