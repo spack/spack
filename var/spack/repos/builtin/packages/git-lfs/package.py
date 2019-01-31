1467c8074cda064dd29f4886cabf827748ca5eee
Matthias Diener <matthias.diener@gmail.com>
2019-01-30 21:21:08 -0600

    git-lfs: add version 2.6.1, update build system (#10405)
    Build system was changed to use a Makefile. Therefore older versions
    (which don't support this) are removed. The new build system also
    supports testing by default.

--- var/spack/repos/builtin/packages/git-lfs/package.py
+++ var/spack/repos/builtin/packages/git-lfs/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class GitLfs(Package):
+class GitLfs(MakefilePackage):
     """Git LFS is a system for managing and versioning large files in
        association with a Git repository.  Instead of storing the large files
        within the Git repository as blobs, Git LFS stores special "pointer
@@ -14,23 +14,16 @@ class GitLfs(Package):
        Git LFS server."""
 
     homepage = "https://git-lfs.github.com"
-    git      = "https://github.com/github/git-lfs.git"
+    url      = "https://github.com/git-lfs/git-lfs/archive/v2.6.1.tar.gz"
 
-    version('2.3.0', tag='v2.3.0')
-    version('2.2.1', tag='v2.2.1')
-    version('2.0.2', tag='v2.0.2')
-    version('1.4.1', tag='v1.4.1')
-    version('1.3.1', tag='v1.3.1')
-
-    # TODO: Add tests by following the instructions at this location:
-    # https://github.com/github/git-lfs/blob/master/CONTRIBUTING.md#building
+    version('2.6.1', sha256='e17cd9d4e66d1116be32f7ddc7e660c7f8fabbf510bc01b01ec15a22dd934ead')
 
     depends_on('go@1.5:', type='build')
     depends_on('git@1.8.2:', type='run')
 
-    def install(self, spec, prefix):
-        bootstrap_script = Executable(join_path('script', 'bootstrap'))
-        bootstrap_script()
+    parallel = False
 
+    # Git-lfs does not provide an 'install' target in the Makefile
+    def install(self, spec, prefix):
         mkdirp(prefix.bin)
         install(join_path('bin', 'git-lfs'), prefix.bin)
