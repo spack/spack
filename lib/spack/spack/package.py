b1374e99a36e28a02e7f36e39dbbfce48f5f0672
Adam J. Stewart <ajstewart426@gmail.com>
2019-02-01 13:20:52 -0600

    Fix 'make test' detection when LANG is not in English

--- lib/spack/spack/package.py
+++ lib/spack/spack/package.py
@@ -1139,7 +1139,10 @@ class PackageBase(with_metaclass(PackageMeta, PackageViewMixin, object)):
         Returns:
             bool: True if 'target' is found, else False
         """
-        make = inspect.getmodule(self).make
+        make = copy.deepcopy(inspect.getmodule(self).make)
+
+        # Use English locale for missing target message comparison
+        make.add_default_env('LC_ALL', 'C')
 
         # Check if we have a Makefile
         for makefile in ['GNUmakefile', 'Makefile', 'makefile']:
