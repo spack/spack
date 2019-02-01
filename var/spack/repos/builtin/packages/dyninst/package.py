639b42b0df6ed60af9be9616d7c795778c3648f1
Mark W. Krentel <krentel@rice.edu>
2019-02-01 16:18:16 -0600

    dyninst: add patch to work with new boost (#10454)
    Add tribool.patch to add explicit casts from tribool to bool in two
    places.  Boost, starting with version 1.69, turned on explicit
    conversions for tribool, which breaks the build for dyninst.
    
    This is merged into the current dyninst develop and is expected to be
    part of the next release, so the version range uses the current
    release as an upper bound.

--- var/spack/repos/builtin/packages/dyninst/package.py
+++ var/spack/repos/builtin/packages/dyninst/package.py
@@ -54,6 +54,7 @@ class Dyninst(CMakePackage):
     patch('stat_dysect.patch', when='+stat_dysect')
     patch('stackanalysis_h.patch', when='@9.2.0')
     patch('v9.3.2-auto.patch', when='@9.3.2 %gcc@:4.7.99')
+    patch('tribool.patch', when='@9.3.0:10.0.0 ^boost@1.69:')
 
     # Versions 9.3.x used cotire, but have no knob to turn it off.
     # Cotire has no real use for one-time builds and can break
