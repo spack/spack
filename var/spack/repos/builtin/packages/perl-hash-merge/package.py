9245699f518cb1c0d30c0c1e457b436635cb2f9b
Levi Baber <baber@iastate.edu>
2019-02-04 15:44:16 -0600

    perl-hash-merge: missed deps

--- var/spack/repos/builtin/packages/perl-hash-merge/package.py
+++ var/spack/repos/builtin/packages/perl-hash-merge/package.py
@@ -13,3 +13,6 @@ class PerlHashMerge(PerlPackage):
     url      = "https://cpan.metacpan.org/authors/id/R/RE/REHSACK/Hash-Merge-0.300.tar.gz"
 
     version('0.300', sha256='402fd52191d51415bb7163b7673fb4a108e3156493d7df931b8db4b2af757c40')
+
+    depends_on('perl-scalar-list-utils', type=('build', 'run'))
+    depends_on('perl-clone-choose', type=('build', 'run'))
