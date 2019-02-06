8b1b62b5d2f4acc16615fa58f3217f6b0067ac18
Ben Olson <macslayer@shepherd>
2019-02-05 21:33:42 -0500

    Fixed authoritarian Travis style checker

--- var/spack/repos/builtin/packages/sicm-low/package.py
+++ var/spack/repos/builtin/packages/sicm-low/package.py
@@ -5,8 +5,10 @@
 
 from spack import *
 
+
 class SicmLow(CMakePackage):
-    """SICM: Simplified Interface to Complex Memory. Includes only the low-level interface and arena allocator."""
+    """SICM: Simplified Interface to Complex Memory. 
+    Includes only the low-level interface and arena allocator."""
 
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/lanl/SICM/"
