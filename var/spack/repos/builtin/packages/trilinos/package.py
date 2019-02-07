570af0c31325d8f999e137750076d4d34298dd9a
mathsen <mathias.anselmann@gmail.com>
2019-02-07 19:58:45 +0100

    Trilinos package: update superlu-dist dependency  (#10511)
    Trilinos master branch gets the latest superlu-dist (currently 6.1.0)

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -300,7 +300,7 @@ class Trilinos(CMakePackage):
     depends_on('mumps@5.0:+mpi+shared', when='+mumps')
     depends_on('scalapack', when='+mumps')
     depends_on('superlu-dist', when='+superlu-dist')
-    depends_on('superlu-dist@:4.3', when='@:12.6.1+superlu-dist')
+    depends_on('superlu-dist@:4.3', when='@11.14.1:12.6.1+superlu-dist')
     depends_on('superlu-dist@4.4:5.3', when='@12.6.2:12.12.1+superlu-dist')
     depends_on('superlu-dist@develop', when='@develop+superlu-dist')
     depends_on('superlu-dist@xsdk-0.2.0', when='@xsdk-0.2.0+superlu-dist')
