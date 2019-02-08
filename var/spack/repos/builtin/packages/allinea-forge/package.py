38fe51819ea87507958e7bbb08b5789e4b2294d2
Jorge Blanco Alonso <41900536+jorblancoa@users.noreply.github.com>
2019-02-08 14:46:16 +0100

    Allinea-forge: Add support for Ubuntu distro (#219)
    - Added different Allinea-forge versions on redhat and ubuntu
    - Used platform.linux_distribution() to get the distro name
    - Add conflict for OS X

--- var/spack/repos/builtin/packages/allinea-forge/package.py
+++ var/spack/repos/builtin/packages/allinea-forge/package.py
@@ -4,7 +4,16 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
+import platform
 
+def getDistroName():
+    distro = platform.linux_distribution()
+    name = distro[0]
+    if name.startswith('Red'):
+        name = 'Redhat'
+    elif name.startswith('Ubuntu'):
+        name = 'Ubuntu'
+    return name
 
 class AllineaForge(Package):
     """Allinea Forge is the complete toolsuite for software development - with
@@ -14,8 +23,11 @@ class AllineaForge(Package):
 
     homepage = "http://www.allinea.com/products/develop-allinea-forge"
 
-    version('6.0', 'c85fec6d01680b5b46fea80111186244')
-    version('7.0', '4c2f5b2ff83d494854df74e6df4be7be')
+    # Default versions: Redhat
+    version('latest', '585490ed224c71896f89b78b4d1629a6', preferred=True)
+    version('19.0.1', 'ab1158398e72de75ea32d62abaf17649')
+    version('19.0', '4c2f5b2ff83d494854df74e6df4be7be')
+    version('18.3', '0188fb8a0b35b49c342927ce0ba9d552')
 
     # Licensing
     license_required = True
@@ -24,10 +36,23 @@ class AllineaForge(Package):
     license_vars = ['ALLINEA_LICENCE_FILE', 'ALLINEA_LICENSE_FILE']
     license_url = 'http://www.allinea.com/user-guide/forge/Installation.html'
 
+    conflicts('platform=darwin')
+
     def url_for_version(self, version):
-        # TODO: add support for other architectures/distributions
-        url = "http://content.allinea.com/downloads/"
-        return url + "arm-forge-latest-Redhat-%s-x86_64.tar" % version
+        url = "http://content.allinea.com/downloads/arm-forge-{0}-{1}-x86_64.tar"
+        # TODO: add support for other architectures/distributions/distroversions
+        distro = getDistroName()
+
+        if distro == 'Ubuntu':
+            checksums = {
+                Version('latest'): 'd8396b046e9b7f4241d69466f6155790',
+                Version('19.0.1'): '67f6f05352ee9991acb844e032282b09',
+                Version('19.0'): 'bf032f88c9294d839790886f83e9ce20',
+                Version('18.3'): 'a1a3b1c6409881e1051bb5de66eaa9f2'
+            }
+            self.versions[version] = {'checksum': checksums[version]}
+            return url.format(version, 'Ubuntu-16.04')
+        return url.format(version, 'Redhat-7.0')
 
     def install(self, spec, prefix):
         bash = which("bash")
