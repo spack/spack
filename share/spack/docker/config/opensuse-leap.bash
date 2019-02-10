35deb2574320a5cdba5eae9be835da1cbd61374a
Omar Padron <omar.padron@kitware.com>
2019-02-09 19:09:23 -0500

    Add more docker base image configurations.
     - Renamed configs
    
       - "centos" -> "centos-7"
       - "fedora" -> "fedora-24"
       - "opensuse" -> "opensuse-tumbleweed"
       - "scilinux" -> "scilinux-7"
       - "ubuntu" -> "ubuntu-1804"
    
     - Added configs
    
       - "centos-6"
       - "fedora-29"
       - "opensuse-leap"
       - "scilinux-6"
       - "ubuntu-1604"
    
    With these changes, our docker hub image
    collection will consist of the following:
    
     - "spack/archlinux"
       - "latest"
     - "spack/centos"
       - "latest", "centos7", "7"
       - "centos6", "6"
     - "spack/fedora"
       - "latest", "29"
       - "24"
     - "spack/opensuse"
       - "latest", "tumbleweed"
       - "leap"
     - "spack/scilinux"
       - "latest", "7"
       - "6"
     - "spack/ubuntu"
       - "latest", "bionic", "18.04"
       - "xenial", "16.04"

--- share/spack/docker/config/opensuse-leap.bash
+++ share/spack/docker/config/opensuse-leap.bash
@@ -4,12 +4,12 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 unset DISTRO
+unset DISTRO_VERSION
 unset BASE_IMAGE
 unset BASE_NAME
 unset BASE_TAG
 unset TAG
 unset EXTRA_TAGS
 
-export BASE_IMAGE=ubuntu
-export BASE_TAG="bionic"
-export EXTRA_TAGS="latest"
+export BASE_IMAGE=opensuse
+export BASE_TAG="leap"
