629382eb061f1941b79ed7b979d1d26e1dd712d0
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-02-12 11:13:28 +0000

    icu4c: cxxstd variant cannot be multi

--- var/spack/repos/builtin/packages/icu4c/package.py
+++ var/spack/repos/builtin/packages/icu4c/package.py
@@ -23,6 +23,7 @@ class Icu4c(AutotoolsPackage):
     variant('cxxstd',
             default='11',
             values=('11', '14', '17'),
+            multi=False,
             description='Use the specified C++ standard when building')
 
     configure_directory = 'source'
