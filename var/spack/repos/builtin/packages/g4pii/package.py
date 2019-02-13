fe5701e60af66c65d6dcfaa1e95255afd67c5b50
Patrick Gartung <gartung@fnal.gov>
2019-02-12 23:32:04 -0600

    flake8

--- var/spack/repos/builtin/packages/g4pii/package.py
+++ var/spack/repos/builtin/packages/g4pii/package.py
@@ -18,7 +18,6 @@ class G4pii(Package):
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         run_env.set('G4PIIDATA', join_path(prefix.share, 'data'))
 
-
     def url_for_version(self, version):
         """Handle version string."""
         return ("http://geant4-data.web.cern.ch/geant4-data/datasets/G4PII.1.3.tar.gz" % version)
