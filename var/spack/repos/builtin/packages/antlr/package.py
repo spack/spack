f0b287cdeb85c6d583d36a9dfb93870ab99e0950
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-02-13 18:41:49 -0500

    flake8

--- var/spack/repos/builtin/packages/antlr/package.py
+++ var/spack/repos/builtin/packages/antlr/package.py
@@ -32,9 +32,9 @@ class Antlr(AutotoolsPackage):
         spec = self.spec
 
         return [
-            '--disable-csharp',
-            '--{0}-cxx'.format('enable' if '+cxx' in spec else 'disable'),
-            '--{0}-java'.format('enable' if '+java' in spec else 'disable'),
-            '--{0}-python'.format('enable' if '+python' in spec else 'disable'),
-            '--disable-csharp'
+          '--disable-csharp',
+          '--{0}-cxx'.format('enable' if '+cxx' in spec else 'disable'),
+          '--{0}-java'.format('enable' if '+java' in spec else 'disable'),
+          '--{0}-python'.format('enable' if '+python' in spec else 'disable'),
+          '--disable-csharp'
         ]
