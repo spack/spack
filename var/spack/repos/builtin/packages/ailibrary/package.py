2cdb4c233f626b557a0b373cb8c90623915a815a
Egor Starobinskii <st.eb@ailurus.ru>
2019-02-16 20:44:24 +0300

    Added line breaks to the description, removed link check

--- var/spack/repos/builtin/packages/ailibrary/package.py
+++ var/spack/repos/builtin/packages/ailibrary/package.py
@@ -7,7 +7,10 @@ from spack import *
 
 
 class Ailibrary(Package):
-    """AiLibrary is a single-header C++ Library from Ailurus Studio that brings you extra time to admire life instead of coding the same functions again and again."""
+    """AiLibrary is a single-header C++ Library from Ailurus Studio
+    that brings you extra time to admire life instead of coding the
+    same functions again and again
+    """
 
     homepage = "https://github.com/starobinskii/AiLibrary"
     url      = "https://github.com/starobinskii/AiLibrary/archive/v1.3.0.tar.gz"
@@ -15,7 +18,6 @@ class Ailibrary(Package):
     version('1.3.0', '56f59bbacfc242f9b8b38d9f2b8133ec')
 
     sanity_check_is_file = ['include/ai.hh']
-    sanity_check_is_file = ['include/ai']
 
     def install(self, spec, prefix):
         make()
