72eb012231fa2f69d698ff1e3a09c9d9c157c7c1
Matthias Wolf <m+git@sushinara.net>
2019-02-18 11:44:45 +0100

    spykfunc: add 0.12.2 (#239)

--- var/spack/repos/builtin/packages/spykfunc/package.py
+++ var/spack/repos/builtin/packages/spykfunc/package.py
@@ -37,7 +37,8 @@ class Spykfunc(PythonPackage):
     version('develop', git=url, submodules=True, clean=False)
     version('0.11.0', git=url, tag='v0.11.0', submodules=True, clean=False)
     version('0.12.0', git=url, tag='v0.12.0', submodules=True, clean=False)
-    version('0.12.1', git=url, tag='v0.12.1', submodules=True, preferred=True, clean=False)
+    version('0.12.1', git=url, tag='v0.12.1', submodules=True, clean=False)
+    version('0.12.2', git=url, tag='v0.12.2', submodules=True, preferred=True, clean=False)
 
     depends_on('hdf5~mpi')
     depends_on('highfive~mpi', type='build')
