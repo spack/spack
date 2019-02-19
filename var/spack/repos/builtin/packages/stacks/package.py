9ae553a810a88117a89ddd00b896000b204f5f25
Levi Baber <baber@iastate.edu>
2019-02-19 16:36:12 -0600

    stacks: make sha256 explicit

--- var/spack/repos/builtin/packages/stacks/package.py
+++ var/spack/repos/builtin/packages/stacks/package.py
@@ -13,7 +13,7 @@ class Stacks(AutotoolsPackage):
     homepage = "http://catchenlab.life.illinois.edu/stacks/"
     url      = "http://catchenlab.life.illinois.edu/stacks/source/stacks-1.46.tar.gz"
 
-    version('2.3b', 'a46786d8811a730ebcdc17891e89f50d4f4ae196734439dac86091f45c92ac72')
+    version('2.3b', sha256='a46786d8811a730ebcdc17891e89f50d4f4ae196734439dac86091f45c92ac72')
     version('1.46', '18b0568a4bba44fb4e5be0eb7ee2c08d')
 
     variant('sparsehash', default=True, description='Improve Stacks memory usage with SparseHash')
