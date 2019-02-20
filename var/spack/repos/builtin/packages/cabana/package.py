72c20759eea0c94bf03897d549df47115189deb2
Christoph Junghans <junghans@lanl.gov>
2019-02-20 14:04:34 -0700

    cabana: cuda backend

--- var/spack/repos/builtin/packages/cabana/package.py
+++ var/spack/repos/builtin/packages/cabana/package.py
@@ -23,6 +23,7 @@ class Cabana(CMakePackage):
     depends_on("cmake@3.9:", type='build')
     depends_on("kokkos+serial", when="+serial")
     depends_on("kokkos+openmp", when="+openmp")
+    depends_on("kokkos+cuda", when="+cuda")
 
     def cmake_args(self):
         options = [
@@ -30,7 +31,9 @@ class Cabana(CMakePackage):
             '-DCabana_ENABLE_Serial=%s'  % (
                 'On' if '+serial'  in self.spec else 'Off'),
             '-DCabana_ENABLE_OpenMP=%s'  % (
-                'On' if '+openmp'  in self.spec else 'Off')
+                'On' if '+openmp'  in self.spec else 'Off'),
+            '-DCabana_ENABLE_Cuda=%s'  % (
+                'On' if '+cuda'  in self.spec else 'Off')
         ]
 
         return options
