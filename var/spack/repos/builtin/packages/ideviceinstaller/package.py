57695fe4730dcc92d5b7850b62737b327d78187e
Isaac Whitfield <iw@whitfin.io>
2019-02-19 23:17:35 -0800

    Add older versions for ideviceinstaller

--- var/spack/repos/builtin/packages/ideviceinstaller/package.py
+++ var/spack/repos/builtin/packages/ideviceinstaller/package.py
@@ -15,6 +15,8 @@ class Ideviceinstaller(Package):
 
     version('master', branch='master')
     version('1.1.0',  sha256='0821b8d3ca6153d9bf82ceba2706f7bd0e3f07b90a138d79c2448e42362e2f53')
+    version('1.0.1',  sha256='e2e5dc41c08cce7cec9edaf4596322f424d5195c255d3c1b957b81b45529b4f5')
+    version('1.0.0',  sha256='6d781621e0823275b22cfbcfb13f2707d4630da040744acca77ed987d0928d86')
 
     depends_on('autoconf', when='@master')
     depends_on('automake', when='@master')
