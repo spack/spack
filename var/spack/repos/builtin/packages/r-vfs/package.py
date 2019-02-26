80c958b29108b67b0e65ea7dd93dc49d88d271cc
Justin Stanley <molecuul@users.noreply.github.com>
2019-02-26 12:21:12 -0600

    r-vfs: new package at 1.0.2 (#10697)

---
+++ var/spack/repos/builtin/packages/r-vfs/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RVfs(RPackage):
+    """VFS: Vegetated Filter Strip and Erosion Model"""
+
+    homepage = "https://cran.r-project.org/package=VFS"
+    url      = "https://cran.r-project.org/src/contrib/VFS_1.0.2.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/VFS"
+
+    version('1.0.2', sha256='8ff7e7e13919ff21f10c7c693ef596a2c7b57c7ca37d79278e443ed122a21aad')
+
+    depends_on('r@3.4.0:', type=('build', 'run'))
+    depends_on('r-e1071', type=('build', 'run'))
+    depends_on('r-nleqslv@3.3.0:', type=('build', 'run'))
+    depends_on('r-knitr', type=('build', 'run'))
+    depends_on('r-rmarkdown', type=('build', 'run'))
+    depends_on('r-testthat', type=('build', 'run'))
