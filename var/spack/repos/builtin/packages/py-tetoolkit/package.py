a811629ee4a0f6515b01b6bc2a0328cc2a2a34cb
Eric Martin <erimar77@users.noreply.github.com>
2019-02-28 05:59:30 -0600

    py-tetoolkit: Add version 2.0.3 (#10749)

--- var/spack/repos/builtin/packages/py-tetoolkit/package.py
+++ var/spack/repos/builtin/packages/py-tetoolkit/package.py
@@ -15,9 +15,11 @@ class PyTetoolkit(PythonPackage):
     homepage = "http://hammelllab.labsites.cshl.edu/software"
     url      = "https://pypi.io/packages/source/T/TEToolkit/TEToolkit-1.5.1.tar.gz"
 
+    version('2.0.3', sha256='1d0f5928b30c6cd9dbef8e092ae0c11e9e707faf92a19af8eed3e360da7d4e46')
     version('1.5.1', '05745b2d5109911e95593e423446a831')
 
     depends_on('py-setuptools')
     depends_on('python@2.7:', type=('build', 'run'))
     depends_on('py-pysam', type=('build', 'run'))
-    depends_on('r-deseq', type=('build', 'run'))
+    depends_on('r-deseq', when='@:1.5.1', type=('build', 'run'))
+    depends_on('r-deseq2', when='@2.0.0:', type=('build', 'run'))
