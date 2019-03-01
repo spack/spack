2003cb6488bdc5e63d192bc2f5bf03d94898ba72
Justin Stanley <molecuul@users.noreply.github.com>
2019-03-01 09:52:07 -0600

    r-rgdal: add 1.3-9 (#10732)

--- var/spack/repos/builtin/packages/r-rgdal/package.py
+++ var/spack/repos/builtin/packages/r-rgdal/package.py
@@ -18,9 +18,10 @@ class RRgdal(RPackage):
     'Expat') are provided on 'CRAN'."""
 
     homepage = "https://cran.r-project.org/package=rgdal"
-    url      = "https://cran.rstudio.com/src/contrib/rgdal_1.2-16.tar.gz"
+    url      = "https://cran.rstudio.com/src/contrib/rgdal_1.3-9.tar.gz"
     list_url = "https://cran.rstudio.com/src/contrib/Archive/rgdal"
 
+    version('1.3-9', sha256='3e44f88d09894be4c0abd8874d00b40a4a5f4542b75250d098ffbb3ba41e2654')
     version('1.2-16', 'de83bf08519a53de68a7632ecb7f2dc9')
 
     depends_on('r-sp', type=('build', 'run'))
