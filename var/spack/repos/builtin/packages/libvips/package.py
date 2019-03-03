3327ec582d46c9ab8ba2513d6d17327556b618d5
s-sajid-ali <sajidsyed2021@u.northwestern.edu>
2019-03-03 14:51:04 -0600

    modified:   var/spack/repos/builtin/packages/libvips/package.py

--- var/spack/repos/builtin/packages/libvips/package.py
+++ var/spack/repos/builtin/packages/libvips/package.py
@@ -10,11 +10,17 @@ class Libvips(AutotoolsPackage):
     """A fast image processing library with low memory needs."""
 
     homepage = "https://libvips.github.io/libvips/"
+    url      = "https://github.com/libvips/libvips/releases/download/v8.7.4/vips-8.7.4.tar.gz"
     git      = "https://github.com/libvips/libvips.git"
 
     version('8.7.4', tag='v8.7.4')
 
-    depends_on('swig')
-    depends_on('gobject-introspection')
-    #depends_on('gtk-doc')
-
+    depends_on('glib')
+    depends_on('libjpeg')
+    depends_on('giflib')
+    depends_on('libtiff')
+    depends_on('poppler')
+    depends_on('libtiff')
+    depends_on('fftw')
+    depends_on('lcms')
+    depends_on('libpng')
