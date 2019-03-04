143990ed96bab6322164d8dc007c17a836aac31e
Ryan S. Elliott <relliott@umn.edu>
2019-03-04 13:07:30 -0600

    Fixup flake8 items

--- var/spack/repos/builtin/packages/openkim-models-v2/package.py
+++ var/spack/repos/builtin/packages/openkim-models-v2/package.py
@@ -25,7 +25,9 @@ class OpenkimModelsV2(CMakePackage):
     version('2019-02-21', sha256='3bd30b0cf2bab314755a66eed621a77c72d3f990818d08366874149be39f208e', extension='txz', url='https://s3.openkim.org/archives/collection/OpenKIM-Models-v2-2019-02-21.txz')
 
     def cmake_args(self):
-       args = []
-       args.append('-DKIM_API_MODEL_INSTALL_PREFIX={0}/lib/kim-api-v2/models'.format(prefix))
-       args.append('-DKIM_API_MODEL_DRIVER_INSTALL_PREFIX={0}/lib/kim-api-v2/model-drivers'.format(prefix))
-       return args
+        args = []
+        args.append('-DKIM_API_MODEL_INSTALL_PREFIX={0}/lib/kim-api-v2/models'
+        .format(prefix))
+        args.append(('-DKIM_API_MODEL_DRIVER_INSTALL_PREFIX={0}/lib/kim-api-v2/'
+        + 'model-drivers').format(prefix))
+        return args
