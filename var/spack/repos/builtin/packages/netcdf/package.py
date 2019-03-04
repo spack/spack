08a97af2daf2461b03cfe50746b34f477ad267e9
Greg Sjaardema <gsjaardema@gmail.com>
2019-03-04 11:51:57 -0700

    Fix flake8 error
    Added blank line as required by flake8

--- var/spack/repos/builtin/packages/netcdf/package.py
+++ var/spack/repos/builtin/packages/netcdf/package.py
@@ -24,6 +24,7 @@ class Netcdf(AutotoolsPackage):
     homepage = "http://www.unidata.ucar.edu/software/netcdf"
     git      = "https://github.com/Unidata/netcdf-c"
     url      = "http://www.gfd-dennou.org/arch/netcdf/unidata-mirror/netcdf-c-4.6.3.tar.gz"
+
     def url_for_version(self, version):
         if version >= Version('4.6.2'):
             url = "https://www.gfd-dennou.org/arch/netcdf/unidata-mirror/netcdf-c-{0}.tar.gz"
