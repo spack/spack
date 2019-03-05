cec3521f73839f2f63dd76870dd02d739b0d883d
Patrick Gartung <gartung@fnal.gov>
2019-03-05 13:52:37 -0600

    flake8

--- lib/spack/setup.py
+++ lib/spack/setup.py
@@ -3,14 +3,16 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import glob
 
-extensions=[]
+extensions = []
 
-excludes = ['spack/main.py','spack/paths.py']
+excludes = ['spack/main.py', 'spack/paths.py']
 
 for f in glob.glob('spack/*.py'):
     if f in excludes:
         continue
-    extensions.append(Extension(f.replace('spack/','spack.').replace('.py',''),[f]))
+    extensions.append(Extension(f.replace('spack/', 'spack.').replace('.py',
+                                                                      ''),
+                                [f]))
 
 extensions.append(Extension('llnl.util.lang', ['llnl/util/lang.py']))
-setup(name="spack", ext_modules = cythonize(extensions))
+setup(name="spack", ext_modules=cythonize(extensions))
