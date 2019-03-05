ffddf38b47258abb6325e86023145532ad0c9485
Patrick Gartung <gartung@fnal.gov>
2019-03-05 13:27:10 -0600

    Add setup.py to cythonize parts of spack for speed up. Use cd spack/lib/spack python setup.py build_ext --inplace to build the corresponing *.so files. Require system or virtual env with cython installed.

---
+++ lib/spack/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import glob
+
+extensions=[]
+
+excludes = ['spack/main.py','spack/paths.py']
+
+for f in glob.glob('spack/*.py'):
+    if f in excludes:
+        continue
+    extensions.append(Extension(f.replace('spack/','spack.').replace('.py',''),[f]))
+
+extensions.append(Extension('llnl.util.lang', ['llnl/util/lang.py']))
+setup(name="spack", ext_modules = cythonize(extensions))
