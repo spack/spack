04bc7571bd2881f4f814e3d2be285175a45167b5
Fernando Pereira <ferdonline@gmail.com>
2019-03-06 15:11:10 +0100

    Numpy recipe works even if python is external (#267)
    * Numpy recipe works even if python/numpy are external

--- var/spack/repos/builtin/packages/py-numpy/package.py
+++ var/spack/repos/builtin/packages/py-numpy/package.py
@@ -140,6 +140,9 @@ class PyNumpy(PythonPackage):
         return args
 
     def setup_environment(self, spack_env, run_env):
+        # Quick fix for the cases when python/numpy are external packages
+        if not 'python' in self.spec:
+            return
         python_version = self.spec['python'].version.up_to(2)
 
         include_path = join_path(
