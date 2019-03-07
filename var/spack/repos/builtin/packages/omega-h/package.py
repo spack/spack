48b286406944384b38559dfac7334ab656366a52
Satish Balay <balay@mcs.anl.gov>
2019-03-06 20:10:47 -0600

    omega-h: add version 9.22.2 and update gcc 8 conflict (#10776)
    * Versions before 9.22.2 compiler with gcc 8.3 or later
    * Added version 9.22.2, which compiles with all gcc 8.x versions
      (including those before 8.3)

--- var/spack/repos/builtin/packages/omega-h/package.py
+++ var/spack/repos/builtin/packages/omega-h/package.py
@@ -16,6 +16,7 @@ class OmegaH(CMakePackage):
     git      = "https://github.com/SNLComputation/omega_h.git"
 
     version('develop', branch='master')
+    version('9.22.2', sha256='ab5636be9dc171a514a7015df472bd85ab86fa257806b41696170842eabea37d')
     version('9.19.1', sha256='60ef65c2957ce03ef9d1b995d842fb65c32c5659d064de002c071effe66b1b1f')
     version('9.19.0', sha256='4a1606c4e7287a1b67359cf6ef1c2d7e24b7dc379065566a1d2e0b0330c0abbd')
     version('9.15.0', sha256='342a506a0ff22f6cac759862efdcf34e360110f7901eb9b4c5de8afe38741522')
@@ -39,7 +40,7 @@ class OmegaH(CMakePackage):
     depends_on('zlib', when='+zlib')
 
     # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=86610
-    conflicts('%gcc@8:')
+    conflicts('%gcc@8:8.2.99', when='@:9.22.1')
 
     def _bob_options(self):
         cmake_var_prefix = 'Omega_h_CXX_'
