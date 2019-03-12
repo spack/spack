ab16f62d3dbbde0a51c85e8b2b8f4a97a00ba15f
Eric Martin <erimar77@users.noreply.github.com>
2019-03-11 19:44:54 -0500

    STAR: Add versions 2.7.0d and 2.7.0e (#10831)

--- var/spack/repos/builtin/packages/star/package.py
+++ var/spack/repos/builtin/packages/star/package.py
@@ -12,6 +12,8 @@ class Star(Package):
     homepage = "https://github.com/alexdobin/STAR"
     url      = "https://github.com/alexdobin/STAR/archive/2.6.1b.tar.gz"
 
+    version('2.7.0e', sha256='2fc9d9103bd02811904d41e3a3d50e47c7de17cb55c3b4880ea5f39300a9ba0d')
+    version('2.7.0d', sha256='7a757478868dc73fe7619bf6ea302dd642bd30e1c8c1fb4acdbe7fa151cf9fd1')
     version('2.6.1b', sha256='1bba5b26c1e6e9a7aca8473a99dbf37bad1dbdd0a589402448e278553bb6b3da')
     version('2.6.1a', sha256='dc87357211432c05123ce49966aae712dec590cbe27c1fd0193c3aeb8d4abe4b')
     version('2.6.0c', sha256='bebba6cc72da302429c44c20f3b07bdde6b0ddf33e538a99e297f1d342070387')
