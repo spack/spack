c8a4a1879bf6f879e55d40b15b29a095671c16dc
Veselin Dobrev <dobrev@llnl.gov>
2019-03-14 18:40:10 -0700

    Initial commit for v2.0 of the CEED software suite.

--- var/spack/repos/builtin/packages/ceed/package.py
+++ var/spack/repos/builtin/packages/ceed/package.py
@@ -20,6 +20,7 @@ class Ceed(Package):
     url  = 'file://' + os.path.dirname(__file__) + '/README.md'
     sha1 = 'b2eefd95c09ba573f663a761b84811a2d7e39788'
 
+    version('2.0.0', sha1, expand=False)
     version('1.0.0', sha1, expand=False)
 
     variant('cuda', default=False,
@@ -38,22 +39,55 @@ class Ceed(Package):
     # TODO: Add 'int64' variant?
 
     # LibCEED
+    # ceed-2.0
+    # TODO: update the libceed version
+    depends_on('libceed@develop~cuda', when='@2.0.0~cuda')
+    depends_on('libceed@develop+cuda', when='@2.0.0+cuda')
+    depends_on('libceed@develop+occa', when='@2.0.0+occa')
+    depends_on('libceed@develop~occa', when='@2.0.0~occa')
+    # ceed-1.0
     depends_on('libceed@0.2~cuda', when='@1.0.0~cuda')
     depends_on('libceed@0.2+cuda', when='@1.0.0+cuda')
     depends_on('libceed@0.2+occa', when='@1.0.0+occa')
     depends_on('libceed@0.2~occa', when='@1.0.0~occa')
 
     # OCCA
+    # ceed-2.0
+    # TODO: update the occa version
+    depends_on('occa@develop~cuda', when='@2.0.0+occa~cuda')
+    depends_on('occa@develop+cuda', when='@2.0.0+occa+cuda')
+    # ceed-1.0
     depends_on('occa@v1.0.0-alpha.5~cuda', when='@1.0.0+occa~cuda')
     depends_on('occa@v1.0.0-alpha.5+cuda', when='@1.0.0+occa+cuda')
 
     # Nek5000, GSLIB, Nekbone, and NekCEM
+    # ceed-2.0
+    # TODO: upate the versions of nek5000, gslib, nekbone, and nekcem
+    depends_on('nek5000@develop', when='@2.0.0+nek')
+    depends_on('gslib@1.0.2', when='@2.0.0+nek')
+    depends_on('nekbone@develop', when='@2.0.0+nek')
+    depends_on('nekcem@develop', when='@2.0.0+nek')
+    # ceed-1.0
     depends_on('nek5000@17.0', when='@1.0.0+nek')
     depends_on('gslib@1.0.2', when='@1.0.0+nek')
     depends_on('nekbone@17.0', when='@1.0.0+nek')
     depends_on('nekcem@0b8bedd', when='@1.0.0+nek')
 
     # PETSc, HPGMG
+    # ceed-2.0
+    # TODO: upate the versions of petsc and hpgmg
+    # For a +quickbuild we disable hdf5, and superlu-dist in PETSc.
+    # Ideally, these can be turned into recommendations to Spack for
+    # concretizing the PETSc spec, if Spack ever supports recommendations.
+    depends_on('petsc@develop~hdf5~superlu-dist',
+               when='@2.0.0+petsc+quickbuild')
+    depends_on('petsc@develop+mpi+double~int64', when='@2.0.0+petsc~mfem')
+    # The mfem petsc examples need the petsc variants +hypre, +suite-sparse,
+    # and +mumps:
+    depends_on('petsc@develop+mpi+hypre+suite-sparse+mumps+double~int64',
+               when='@2.0.0+petsc+mfem')
+    depends_on('hpgmg@develop+fe', when='@2.0.0+petsc')
+    # ceed-1.0
     # For a +quickbuild we disable hdf5, and superlu-dist in PETSc.
     # Ideally, these can be turned into recommendations to Spack for
     # concretizing the PETSc spec, if Spack ever supports recommendations.
@@ -67,12 +101,36 @@ class Ceed(Package):
     depends_on('hpgmg@a0a5510df23b+fe', when='@1.0.0+petsc')
 
     # MAGMA
+    # ceed-2.0
+    # TODO: upate the version of magma
+    depends_on('magma@2.5.0', when='@2.0.0+cuda')
+    # ceed-1.0
     depends_on('magma@2.3.0', when='@1.0.0+cuda')
 
     # PUMI
+    # ceed-2.0
+    # TODO: upate the version of pumi
+    depends_on('pumi@2.2.0', when='@2.0.0+pumi')
+    # ceed-1.0
     depends_on('pumi@2.1.0', when='@1.0.0+pumi')
 
     # MFEM, Laghos
+    # ceed-2.0
+    depends_on('mfem@3.4.0+mpi+examples+miniapps', when='@2.0.0+mfem~petsc')
+    depends_on('mfem@3.4.0+mpi+petsc+examples+miniapps',
+               when='@2.0.0+mfem+petsc')
+    depends_on('laghos@2.0', when='@2.0.0+mfem')
+    # The next line seems to be necessary because the concretizer somehow
+    # decides that mfem requires 'hypre+internal-superlu' even though the mfem
+    # package lists simply 'hypre' as dependency. This is only an issue because
+    # petsc explicitly requires 'hypre~internal-superlu' which for the
+    # concretizer is a conflict.
+    depends_on('hypre~internal-superlu', when='@2.0.0+mfem')
+
+    # If using gcc version <= 4.8 build suite-sparse version <= 5.1.0
+    depends_on('suite-sparse@:5.1.0', when='@2.0.0%gcc@:4.8+mfem+petsc')
+
+    # ceed-1.0
     depends_on('mfem@3.3.2+mpi+examples+miniapps', when='@1.0.0+mfem~petsc')
     depends_on('mfem@3.3.2+mpi+petsc+examples+miniapps',
                when='@1.0.0+mfem+petsc')
