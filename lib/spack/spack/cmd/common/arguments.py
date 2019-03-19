fabf8a0a78625227b061c5519dcbef182625c545
Chris Green <greenc@fnal.gov>
2019-03-19 15:02:06 -0500

    Fix help text for --no-checksum. (#10932)

--- lib/spack/spack/cmd/common/arguments.py
+++ lib/spack/spack/cmd/common/arguments.py
@@ -131,4 +131,4 @@ _arguments['install_status'] = Args(
 
 _arguments['no_checksum'] = Args(
     '-n', '--no-checksum', action='store_true', default=False,
-    help="do not use checksums to verify downloadeded files (unsafe)")
+    help="do not use checksums to verify downloaded files (unsafe)")
