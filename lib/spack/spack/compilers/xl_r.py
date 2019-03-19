26f2a94a45f222803236b1d14d2a5d2414625e48
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2019-03-19 14:02:18 +0100

    Removed redundant code from xl_r compiler class

--- lib/spack/spack/compilers/xl_r.py
+++ lib/spack/spack/compilers/xl_r.py
@@ -3,11 +3,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.compiler import Compiler, UnsupportedCompilerFlag
-from spack.version import ver
+import spack.compilers.xl
 
 
-class XlR(Compiler):
+class XlR(spack.compilers.xl.Xl):
     # Subclasses use possible names of C compiler
     cc_names = ['xlc_r']
 
@@ -25,60 +24,3 @@ class XlR(Compiler):
                   'cxx': 'xl_r/xlc++_r',
                   'f77': 'xl_r/xlf_r',
                   'fc': 'xl_r/xlf90_r'}
-
-    version_argument = '-qversion'
-    version_regex = r'([0-9]?[0-9]\.[0-9])'
-
-    @property
-    def openmp_flag(self):
-        return "-qsmp=omp"
-
-    @property
-    def cxx11_flag(self):
-        if self.version < ver('13.1'):
-            raise UnsupportedCompilerFlag(self,
-                                          "the C++11 standard",
-                                          "cxx11_flag",
-                                          "< 13.1")
-        else:
-            return "-qlanglvl=extended0x"
-
-    @property
-    def pic_flag(self):
-        return("-qpic")
-
-    @property
-    def fflags(self):
-        # The -qzerosize flag is effective only for the Fortran 77
-        # compilers and allows the use of zero size objects.
-        # For Fortran 90 and beyond, it is set by default and has not impact.
-        # Its use has no negative side effects.
-        return "-qzerosize"
-
-    @classmethod
-    def fc_version(cls, fc):
-        # The fortran and C/C++ versions of the XL compiler are always
-        # two units apart.  By this we mean that the fortran release that
-        # goes with XL C/C++ 11.1 is 13.1.  Having such a difference in
-        # version number is confusing spack quite a lot.  Most notably
-        # if you keep the versions as is the default xl compiler will
-        # only have fortran and no C/C++.  So we associate the Fortran
-        # compiler with the version associated to the C/C++ compiler.
-        # One last stumble. Version numbers over 10 have at least a .1
-        # those under 10 a .0. There is no xlf 9.x or under currently
-        # available. BG/P and BG/L can such a compiler mix and possibly
-        # older version of AIX and linux on power.
-        fortran_version = cls.default_version(fc)
-        if fortran_version >= 16:
-            # Starting with version 16.1, the XL C and Fortran compilers
-            # have the same version.  So no need to downgrade the Fortran
-            # compiler version to match that of the C compiler version.
-            return str(fortran_version)
-        c_version = float(fortran_version) - 2
-        if c_version < 10:
-            c_version = c_version - 0.1
-        return str(c_version)
-
-    @classmethod
-    def f77_version(cls, f77):
-        return cls.fc_version(f77)
