121df9663888052eda773ba211eda727f83723b6
Eric Martin <erimar77@users.noreply.github.com>
2019-03-22 05:18:45 -0500

    trimgalore: Add versions 0.6.0 and 0.6.1 (#10981)

--- var/spack/repos/builtin/packages/trimgalore/package.py
+++ var/spack/repos/builtin/packages/trimgalore/package.py
@@ -14,6 +14,8 @@ class Trimgalore(Package):
     homepage = "https://github.com/FelixKrueger/TrimGalore"
     url      = "https://github.com/FelixKrueger/TrimGalore/archive/0.4.4.tar.gz"
 
+    version('0.6.1', sha256='658578c29d007fe66f9ab49608442be703a6fcf535db06eb82659c7edccb62b0')
+    version('0.6.0', sha256='f374dfa4c94e2ad50c63276dda0f341fd95b29cb1d5a0e2ad56e8b0168b758ec')
     version('0.4.5', 'c71756042b2a65c34d483533a29dc206')
     version('0.4.4', 'aae1b807b48e38bae7074470203997bb')
 
