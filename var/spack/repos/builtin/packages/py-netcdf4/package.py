41c78e2113a196a360351d3b775ed7fffbf7168e
Dustin Lagoy <dustin.lagoy@gmail.com>
2019-03-25 18:03:02 -0700

    py-netcdf4: avoid using system installs of dependencies (#10885)
    Use environment variables to point setup.py to Spack installations
    of hdf5/netcdf

--- var/spack/repos/builtin/packages/py-netcdf4/package.py
+++ var/spack/repos/builtin/packages/py-netcdf4/package.py
@@ -23,3 +23,13 @@ class PyNetcdf4(PythonPackage):
 
     depends_on('netcdf')
     depends_on('hdf5@1.8.0:')
+
+    def setup_environment(self, spack_env, run_env):
+        """Ensure installed netcdf and hdf5 libraries are used"""
+        # Explicitly set these variables so setup.py won't erroneously pick up
+        # system versions
+        spack_env.set('USE_SETUPCFG', '0')
+        spack_env.set('HDF5_INCDIR', self.spec['hdf5'].prefix.include)
+        spack_env.set('HDF5_LIBDIR', self.spec['hdf5'].prefix.lib)
+        spack_env.set('NETCDF4_INCDIR', self.spec['netcdf'].prefix.include)
+        spack_env.set('NETCDF4_LIBDIR', self.spec['netcdf'].prefix.lib)
