7d1b6d543ea5c0f8518aee59b3f13f05a42ce368
ajw1980 <ajw1980@gmail.com>
2019-03-26 14:42:31 -0500

    py-dateutil package: add version 2.7.5 (#10961)

--- var/spack/repos/builtin/packages/py-dateutil/package.py
+++ var/spack/repos/builtin/packages/py-dateutil/package.py
@@ -11,10 +11,11 @@ class PyDateutil(PythonPackage):
     homepage = "https://pypi.python.org/pypi/dateutil"
     url      = "https://pypi.io/packages/source/p/python-dateutil/python-dateutil-2.4.0.tar.gz"
 
-    version('2.2',   'c1f654d0ff7e33999380a8ba9783fd5c')
-    version('2.4.0', '75714163bb96bedd07685cdb2071b8bc')
-    version('2.4.2', '4ef68e1c485b09e9f034e10473e5add2')
+    version('2.7.5', sha256='88f9287c0174266bb0d8cedd395cfba9c58e87e5ad86b2ce58859bc11be3cf02')
     version('2.5.2', 'eafe168e8f404bf384514f5116eedbb6')
+    version('2.4.2', '4ef68e1c485b09e9f034e10473e5add2')
+    version('2.4.0', '75714163bb96bedd07685cdb2071b8bc')
+    version('2.2',   'c1f654d0ff7e33999380a8ba9783fd5c')
 
     depends_on('py-setuptools', type='build')
     depends_on('py-six', type=('build', 'run'))
