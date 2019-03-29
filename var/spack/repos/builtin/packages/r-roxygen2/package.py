8541efcaa697e4b4320e40b0ecb3e979565779f6
molecuul <jtst@iastate.edu>
2019-03-28 21:34:34 -0500

    r-roxygen2: added 6.1.1, more dependencies

--- var/spack/repos/builtin/packages/r-roxygen2/package.py
+++ var/spack/repos/builtin/packages/r-roxygen2/package.py
@@ -14,10 +14,17 @@ class RRoxygen2(RPackage):
     url      = "https://cran.r-project.org/src/contrib/roxygen2_5.0.1.tar.gz"
     list_url = "https://cran.r-project.org/src/contrib/Archive/roxygen2"
 
+    version('6.1.1', sha256='ed46b7e062e0dfd8de671c7a5f6d120fb2b720982e918dbeb01e6985694c0273')
     version('5.0.1', 'df5bdbc12fda372e427710ef1cd92ed7')
 
-    depends_on('r-stringr', type=('build', 'run'))
-    depends_on('r-stringi', type=('build', 'run'))
     depends_on('r-brew', type=('build', 'run'))
+    depends_on('r-commonmark', type=('build', 'run'))
+    depends_on('r-desc@1.2.0:', type=('build', 'run'))
     depends_on('r-digest', type=('build', 'run'))
-    depends_on('r-rcpp', type=('build', 'run'))
+    depends_on('r-pkgload@1.0.2:', type=('build', 'run'))
+    depends_on('r-purrr', type=('build', 'run'))
+    depends_on('r-r6@2.1.2:', type=('build', 'run'))
+    depends_on('r-rcpp@0.11.0:', type=('build', 'run'))
+    depends_on('r-stringi', type=('build', 'run'))
+    depends_on('r-stringr@1.0.0:', type=('build', 'run'))
+    depends_on('r-xml2', type=('build', 'run'))
