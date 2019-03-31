2827e16098feb99783721195f29d7f688dea4691
Satish Balay <balay@mcs.anl.gov>
2019-03-31 12:19:43 -0500

    superlu-dist: add version 6.1.1 (#11066)

--- var/spack/repos/builtin/packages/superlu-dist/package.py
+++ var/spack/repos/builtin/packages/superlu-dist/package.py
@@ -16,6 +16,7 @@ class SuperluDist(CMakePackage):
 
     version('develop', branch='master')
     version('xsdk-0.2.0', tag='xsdk-0.2.0')
+    version('6.1.1', '35d25cff592c724439870444ed45e1d1d15ca2c65f02ccd4b83a6d3c9d220bd1')
     version('6.1.0', '92c6d1424dd830ee2d1e7396a418a5f6645160aea8472e558c4e4bfe006593c4')
     version('6.0.0', 'ff6cdfa0263d595708bbb6d11fb780915d8cfddab438db651e246ea292f37ee4')
     version('5.4.0', '3ac238fe082106a2c4dbaf0c22af1ff1247308ffa8f053de9d78c3ec7dd0d801')
