7a1d9c51ad441e86dd20a5f2f2f3221b38b55233
Vanessa Sochat <vsochat@stanford.edu>
2019-04-02 16:56:52 -0400

    updating for singularity install with golang, 3.0 and up

--- var/spack/repos/builtin/packages/singularity/package.py
+++ var/spack/repos/builtin/packages/singularity/package.py
@@ -4,30 +4,80 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
+import os
+import shutil
+import tempfile
 
 
-class Singularity(AutotoolsPackage):
-    """Singularity is a container platform focused on supporting 'Mobility of
-       Compute'"""
+class Singularity(Package):
+    '''Singularity is a container technology focused on building portable
+       encapsulated environments to support "Mobility of Compute" For older
+       versions of Singularity (pre 3.0) you should use singularity-legacy,
+       which has a different install base (Autotools).
+    '''
 
     homepage = "https://www.sylabs.io/singularity/"
-    url      = "https://github.com/singularityware/singularity/releases/download/2.5.2/singularity-2.5.2.tar.gz"
+    url      = "https://github.com/sylabs/singularity/archive/v3.1.0.tar.gz"
     git      = "https://github.com/singularityware/singularity.git"
 
-    # Versions before 2.5.2 suffer from a serious security problem.
-    # https://nvd.nist.gov/vuln/detail/CVE-2018-12021
+    # ##########################################################################
+    # 3.1.0 Release Branch (GoLang)
+
     version('develop', branch='master')
-    version('2.6.1', sha256='f38d46a225e8368eb4693137806d2dc96e925a50bdf7f6983662848831041df2')
-    version('2.6.0', sha256='7c425211a099f6fa6f74037e6e17be58fb5923b0bd11aea745e48ef83c488b49')
-    version('2.5.2', '2edc1a8ac9a4d7d26fba6244f1c5fd95')
+    version('3.1.0', 'f3f1388f7e904dbcc1f7934740b847a771965019')
 
-    depends_on('libarchive', when='@2.5.2:')
-    # these are only needed if we're grabbing the unreleased tree
-    depends_on('m4',       type='build', when='@develop')
-    depends_on('autoconf', type='build', when='@develop')
-    depends_on('automake', type='build', when='@develop')
-    depends_on('libtool',  type='build', when='@develop')
+    depends_on('go', when='@develop')
+    depends_on('libuuid', when='@develop')
+    depends_on('libgpg-error', when='@develop')
+    depends_on('squashfs', when='@develop')
 
-    # When installing as root, the copy has to run before chmod runs
+    phases = ['configure', 'build', 'install']
+
+    def configure(self, spec, prefix):
+
+        # $GOPATH/src/github.com/sylabs/singularity
+        tmpgo = prepare_gopath()
+
+        # Remove old install
+        if os.path.exists(tmpgo):
+            shutil.rmtree(tmpgo)
+
+        shutil.move(os.getcwd(), tmpgo) 
+
+        with working_dir(tmpgo):
+            configure = Executable('./mconfig --prefix=%s' % prefix)
+            configure()
+
+    def build(self, spec, prefix):
+
+        # $GOPATH/src/github.com/sylabs/singularity
+        tmpgo = prepare_gopath()
+
+        # The package needs to be in GOPATH in order for it to be found
+        with working_dir(tmpgo):
+            make('-C', 'builddir', parallel=False) 
+
     def install(self, spec, prefix):
-        make('install', parallel=False)
+        with working_dir(prepare_gopath()):
+            make('install', '-C', 'builddir', parallel=False)    
+
+
+def prepare_gopath():
+    '''The repository needed to be cloned into 
+          $GOPATH/src/github.com/sylabs/singularity to begin with. To
+       mimic this, we create the structure in a temporary directory and
+       work from there. If we don't do that, when we cd into the builddir
+       to make, the entire set of source files aren't found on GOPATH. But
+       we cannot add the default cloned source to the GOPATH because it
+       will append the default src/github.com. If we lose working from the
+       present working directory, we also risk losing the vendor folder.
+    '''
+    gopath = os.path.join(tempfile.gettempdir(), 'go')
+    tmpgo = os.path.join(gopath, 'src', 'github.com' ,'sylabs', 'singularity')
+    os.environ['GOPATH'] = gopath + ':' + os.environ['GOPATH']
+
+    # Create gopath
+    if not os.path.exists(gopath):
+        os.makedirs(gopath)
+
+    return tmpgo
