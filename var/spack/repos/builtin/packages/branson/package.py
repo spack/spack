d3c6ee4505022be55d57305307b63ecbc341a84d
Christoph Junghans <christoph.junghans@gmail.com>
2019-04-02 16:44:46 -0600

    branson: fix install (#11093)

--- var/spack/repos/builtin/packages/branson/package.py
+++ var/spack/repos/builtin/packages/branson/package.py
@@ -45,6 +45,6 @@ class Branson(CMakePackage):
     def install(self, spec, prefix):
         mkdir(prefix.bin)
         mkdir(prefix.doc)
-        install('spack-build/BRANSON', prefix.bin)
+        install('../spack-build/BRANSON', prefix.bin)
         install('LICENSE.md', prefix.doc)
         install('README.md', prefix.doc)
