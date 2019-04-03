eb8e50d0db9c3ba9595e358bfe362cd9e7b82b92
Elsa Gonsiorowski, PhD <gonsie@me.com>
2019-04-02 17:50:41 -0700

    ross package: add version 7.0.1 (#11078)
    * add version 7.0.1
    * replace md5 with sha256 for various versions.
    * v7.0.1 fixes some environment hacking to properly find MPI and
      system architecture

--- var/spack/repos/builtin/packages/ross/package.py
+++ var/spack/repos/builtin/packages/ross/package.py
@@ -11,20 +11,23 @@ class Ross(CMakePackage):
 
     homepage = "http://ross-org.github.io"
     git      = "https://github.com/ROSS-org/ROSS.git"
+    url      = "https://github.com/ROSS-org/ROSS/archive/v7.0.0.tar.gz"
 
     version('develop', branch='master')
-    version('7.0.0', tag='v7.0.0')
+    version('7.0.1', sha256='40780dada4ab501d2b8ea229f70b9fea920404431d7a60081ba84dd4a50b2517')
+    version('7.0.0', sha256='fd16be2c86d9d71ae64eef67c02933471ab758c8a5b01b04fe358d9228fc581e')
+    version('6.0.0', sha256='07ff70518a58503e116bb7386f490e901212798afdd471da1bcd34f78a7e6030')
 
     depends_on('mpi')
+    depends_on('cmake@3.5:', when="@7.0.1:", type='build')
 
+    @when("@:7.0.0")
     def cmake_args(self):
-        if 'x86_64' not in self.spec.architecture:
-            raise InstallError(
-                'This package currently only builds on x86_64 architectures')
+        args = []
 
-        args = ["-DBUILD_SHARED_LIBS=ON",
-                "-DARCH=x86_64",
-                "-DCMAKE_C_COMPILER=%s" % self.spec['mpi'].mpicc,
-                "-DCMAKE_CXX_COMPILER=%s" % self.spec['mpi'].mpicxx]
+        args.append("-DBUILD_SHARED_LIBS=ON")
+        args.append("-DARCH=%s" % self.spec.architecture.target)
+        args.append("-DCMAKE_C_COMPILER=%s" % self.spec['mpi'].mpicc)
+        args.append("-DCMAKE_CXX_COMPILER=%s" % self.spec['mpi'].mpicxx)
 
         return args
