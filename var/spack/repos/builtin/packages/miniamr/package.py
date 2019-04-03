8da6593e43485d9d82c24c6971d49567639839be
Abhinav Bhatele <bhatele@gmail.com>
2019-04-03 15:17:04 -0700

    miniamr: add develop version and update release (#11101)
    * miniamr: add develop version and update release
    
    * change from url to git
    
    * add url back

--- var/spack/repos/builtin/packages/miniamr/package.py
+++ var/spack/repos/builtin/packages/miniamr/package.py
@@ -13,10 +13,14 @@ class Miniamr(MakefilePackage):
     """
 
     homepage = "https://mantevo.org"
+    git      = "https://github.com/Mantevo/miniAMR.git"
     url      = "https://github.com/Mantevo/miniAMR/archive/v1.4.0.tar.gz"
 
     tags = ['proxy-app', 'ecp-proxy-app']
 
+    version('develop', branch='master')
+    version('1.4.3', sha256='4c3fbc1662ae3e139669fb3844134486a7488a0b6e085c3b24bebcc8d12d3ac6')
+    version('1.4.2', sha256='d2347e0e22a8e79aa0dc3316b67dd7c40dded39d82f6e068e6fb8c9f0766566b')
     version('1.4.1', sha256='dd8e8d9fd0768cb4f2c5d7fe6989dfa6bb95a8461f04deaccdbb50b0dd51e97a')
     version('1.4.0', '3aab0247047a94e343709cf2e51cc46e')
 
