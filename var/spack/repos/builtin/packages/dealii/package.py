dbd9c9a5dc98b19dde83c86b9c5d2fe412ea75a0
Denis Davydov <davydden@gmail.com>
2019-04-09 10:48:31 +0200

    dealii: help to pick up TBB from intel-parallel-studio

--- var/spack/repos/builtin/packages/dealii/package.py
+++ var/spack/repos/builtin/packages/dealii/package.py
@@ -224,10 +224,20 @@ class Dealii(CMakePackage, CudaPackage):
                 lapack_blas_headers.directories),
             '-DLAPACK_LIBRARIES=%s' % lapack_blas_libs.joined(';'),
             '-DUMFPACK_DIR=%s' % spec['suite-sparse'].prefix,
-            '-DTBB_DIR=%s' % spec['tbb'].prefix,
             '-DZLIB_DIR=%s' % spec['zlib'].prefix,
             '-DDEAL_II_ALLOW_BUNDLED=OFF'
         ])
+
+        if (spec.satisfies('^intel-parallel-studio+tbb')):
+            # deal.II will have hard timepicking up TBB from Intel
+            options.extend([
+                '-DTBB_FOUND=true',
+                '-DTBB_INCLUDE_DIRS=%s' % ';'.join(
+                    spec['tbb'].headers.directories),
+                '-DTBB_LIBRARIES=%s' % spec['tbb'].libs.joined(';')
+            ])
+        else:
+            options.append('-DTBB_DIR=%s' % spec['tbb'].prefix)
 
         if (spec.satisfies('^openblas+ilp64') or
             spec.satisfies('^intel-mkl+ilp64') or
