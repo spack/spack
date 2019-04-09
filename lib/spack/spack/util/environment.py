713486c6eb383bec07915dcafb9ecc5887642d60
Peter Josef Scheibel <scheibel1@llnl.gov>
2019-04-08 19:27:52 -0700

    python 2.6 string format compatability

--- lib/spack/spack/util/environment.py
+++ lib/spack/spack/util/environment.py
@@ -26,14 +26,14 @@ system_dirs = [os.path.join(p, s) for s in suffixes for p in system_paths] + \
 
 
 _shell_set_strings = {
-    'sh': 'export {}={};\n',
-    'csh': 'setenv {} {};\n',
+    'sh': 'export {0}={1};\n',
+    'csh': 'setenv {0} {1};\n',
 }
 
 
 _shell_unset_strings = {
-    'sh': 'unset {};\n',
-    'csh': 'unsetenv {};\n',
+    'sh': 'unset {0};\n',
+    'csh': 'unsetenv {0};\n',
 }
 
 
