326cf4caac53556fcb3a6c838f41153158fe73de
Frédéric Simonis <simonisfrederic@gmail.com>
2019-04-16 21:38:08 +0200

    precice package: add versions and dependencies (#11176)
    * Add versions 1.4.0, 1.3.0, and 1.2.0
    * Add dependencies and update constraints (e.g. version 1.4.0 of
      precice requires a later version of boost)
    * Add maintainers

--- var/spack/repos/builtin/packages/precice/package.py
+++ var/spack/repos/builtin/packages/precice/package.py
@@ -15,9 +15,14 @@ class Precice(CMakePackage):
 
     homepage = 'https://www.precice.org'
     git      = 'https://github.com/precice/precice.git'
+    url      = 'https://github.com/precice/precice/archive/v1.2.0.tar.gz'
+    maintainers = ['fsimonis', 'MakisH']
 
-    # Skip version 1.1.1 entirely, the cmake was lacking install.
     version('develop', branch='develop')
+    version('1.4.0', sha256='3499bfc0941fb9f004d5e32eb63d64f93e17b4057fab3ada1cde40c8311bd466')
+    version('1.3.0', sha256='610322ba1b03df8e8f7d060d57a6a5afeabd5db4e8c4a638d04ba4060a3aec96')
+    version('1.2.0', sha256='0784ecd002092949835151b90393beb6e9e7a3e9bd78ffd40d18302d6da4b05b')
+    # Skip version 1.1.1 entirely, the cmake was lacking install.
 
     variant('mpi', default=True, description='Enable MPI support')
     variant('petsc', default=False, description='Enable PETSc support')
@@ -33,11 +38,13 @@ class Precice(CMakePackage):
 #        default=False, description='Use 64-bit integers for indices')
 
     depends_on('cmake@3.5:', type='build')
+    depends_on('cmake@3.9.6:', type='build', when='@1.4:')
     depends_on('boost@1.60.0:')
+    depends_on('boost@1.65.1:', when='@1.4:')
     depends_on('eigen@3.2:')
-    # Implicit via eigen, don't over-constrain: depends_on('libxml2')
+    depends_on('libxml2')
     depends_on('mpi', when='+mpi')
-    depends_on('petsc', when='+petsc')
+    depends_on('petsc@3.6:', when='+petsc')
     depends_on('python@2.7', when='+python', type=('build', 'run'))
 
     def cmake_args(self):
