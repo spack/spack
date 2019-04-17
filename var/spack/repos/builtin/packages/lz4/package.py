91b00edd87f124af3f76e09c6bf44d31df668da9
Michael Kuhn <michael@ikkoku.de>
2019-04-17 16:30:23 +0200

    lz4: Add 1.9.0 (#11207)

--- var/spack/repos/builtin/packages/lz4/package.py
+++ var/spack/repos/builtin/packages/lz4/package.py
@@ -16,6 +16,7 @@ class Lz4(Package):
     homepage = "http://lz4.github.io/lz4/"
     url      = "https://github.com/lz4/lz4/archive/v1.7.5.tar.gz"
 
+    version('1.9.0',   'f8b6d5662fa534bd61227d313535721ae41a68c9d84058b7b7d86e143572dcfb')
     version('1.8.3',   '33af5936ac06536805f9745e0b6d61da606a1f8b4cc5c04dd3cbaca3b9b4fc43')
     version('1.8.1.2', '343538e69ba752a386c669b1a28111e2')
     version('1.7.5',   'c9610c5ce97eb431dddddf0073d919b9')
