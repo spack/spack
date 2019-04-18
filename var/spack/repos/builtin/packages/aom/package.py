f4048da27acbb6ea7a6622ef7a1c901b19547ccf
Sean McGrath <smcgrat@tchpc.tcd.ie>
2019-04-18 10:01:27 +0100

    Fix for Flake8 sytle mistake

--- var/spack/repos/builtin/packages/aom/package.py
+++ var/spack/repos/builtin/packages/aom/package.py
@@ -1,5 +1,5 @@
-# Copyright 2013-2019 Trinity College Dublin  and other Spack Project Developers.
-# See the top-level COPYRIGHT file for details.
+# Copyright 2013-2019 Trinity College Dublin and other Spack Project
+# Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
