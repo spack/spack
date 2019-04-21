a7c06cf9c8568e95b7fb6f215027ec2a1feba594
Todd Gamblin <tgamblin@llnl.gov>
2019-04-20 20:51:45 -0700

    spack edit: use execv instead of Executable (#11245)
    - `spack edit` previously used `spack.util.executable` `Executable` objects,
      and didn't `exec` the editor like you'd expect it to
    
    - This meant that Spack was still running while your editor was, and
     stdout/stdin were being set up in weird ways
    
    - e.g. on macOS, if you call `spack edit` with `EDITOR` set to the
      builtin `emacs` command, then type `Ctrl-g`, the whole thing dies with
      a `==> Error: Keyboard interrupt`
    
    - Fix all this by changing spack.util.editor to use `os.execv` instead of
      Spack's `Executable` object

--- lib/spack/spack/util/editor.py
+++ lib/spack/spack/util/editor.py
@@ -12,30 +12,120 @@ specified editor fails (e.g. no DISPLAY for a graphical editor). If
 neither variable is set, we fall back to one of several common editors,
 raising an EnvironmentError if we are unable to find one.
 """
-import copy
 import os
+import shlex
 
-from spack.util.executable import Executable, which
+import llnl.util.tty as tty
 
-_visual_exe\
-    = Executable(os.environ['VISUAL']) if 'VISUAL' in os.environ else None
-_editor_exe\
-    = Executable(os.environ['EDITOR']) \
-    if 'EDITOR' in os.environ else which('vim', 'vi', 'emacs', 'nano')
+import spack.config
+from spack.util.executable import which_string
 
 
-# Invoke the user's editor.
+#: editors to try if VISUAL and EDITOR are not set
+_default_editors = ['vim', 'vi', 'emacs', 'nano']
+
+
+def _find_exe_from_env_var(var):
+    """Find an executable from an environment variable.
+
+    Args:
+        var (str): environment variable name
+
+    Returns:
+        (str or None, list): executable string (or None if not found) and
+            arguments parsed from the env var
+    """
+    # try to get the environment variable
+    exe = os.environ.get(var)
+    if not exe:
+        return None, []
+
+    # split env var into executable and args if needed
+    args = shlex.split(exe)
+    if not args:
+        return None, []
+
+    exe = which_string(args[0])
+    args = [exe] + args[1:]
+    return exe, args
+
+
 def editor(*args, **kwargs):
-    if _visual_exe:
-        visual_kwargs = copy.copy(kwargs)
-        visual_kwargs['fail_on_error'] = False
-        _visual_exe(*args, **visual_kwargs)
-        if _visual_exe.returncode == 0:
-            return  # Otherwise, fall back to EDITOR.
+    """Invoke the user's editor.
 
-    if _editor_exe:
-        _editor_exe(*args, **kwargs)
-    else:
-        raise EnvironmentError(
-            'No text editor found! Please set the VISUAL and/or EDITOR '
-            'environment variable(s) to your preferred text editor.')
+    This will try to execute the following, in order:
+
+      1. $VISUAL <args>    # the "visual" editor (per POSIX)
+      2. $EDITOR <args>    # the regular editor (per POSIX)
+      3. some default editor (see ``_default_editors``) with <args>
+
+    If an environment variable isn't defined, it is skipped.  If it
+    points to something that can't be executed, we'll print a
+    warning. And if we can't find anything that can be executed after
+    searching the full list above, we'll raise an error.
+
+    Arguments:
+        args (list of str): args to pass to editor
+
+    Optional Arguments:
+        _exec_func (function): invoke this function instead of ``os.execv()``
+
+    """
+    # allow this to be customized for testing
+    _exec_func = kwargs.get('_exec_func', os.execv)
+
+    def try_exec(exe, args, var=None):
+        """Try to execute an editor with execv, and warn if it fails.
+
+        Returns: (bool) False if the editor failed, ideally does not
+            return if ``execv`` succeeds, and ``True`` if the
+            ``_exec_func`` does return successfully.
+        """
+        try:
+            _exec_func(exe, args)
+            return True
+
+        except OSError as e:
+            if spack.config.get('config:debug'):
+                raise
+
+            # Show variable we were trying to use, if it's from one
+            if var:
+                exe = '$%s (%s)' % (var, exe)
+            tty.warn('Could not execute %s due to error:' % exe, str(e))
+            return False
+
+    def try_env_var(var):
+        """Find an editor from an environment variable and try to exec it.
+
+        This will warn if the variable points to something is not
+        executable, or if there is an error when trying to exec it.
+        """
+        if var not in os.environ:
+            return False
+
+        exe, editor_args = _find_exe_from_env_var(var)
+        if not exe:
+            tty.warn('$%s is not an executable:' % var, os.environ[var])
+            return False
+
+        full_args = editor_args + list(args)
+        return try_exec(exe, full_args, var)
+
+    # try standard environment variables
+    if try_env_var('VISUAL'):
+        return
+    if try_env_var('EDITOR'):
+        return
+
+    # nothing worked -- try the first default we can find don't bother
+    # trying them all -- if we get here and one fails, something is
+    # probably much more deeply wrong with the environment.
+    exe = which_string(*_default_editors)
+    if try_exec(exe, [exe] + list(args)):
+        return
+
+    # Fail if nothing could be found
+    raise EnvironmentError(
+        'No text editor found! Please set the VISUAL and/or EDITOR '
+        'environment variable(s) to your preferred text editor.')
