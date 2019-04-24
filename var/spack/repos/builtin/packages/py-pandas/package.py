da798422fe8b8edc33e641d21121eece378847ee
Eric Martin <erimar77@users.noreply.github.com>
2019-04-23 19:01:56 -0500

    py-pandas: Add version 0.20.0 (#11264)

--- var/spack/repos/builtin/packages/py-pandas/package.py
+++ var/spack/repos/builtin/packages/py-pandas/package.py
@@ -22,6 +22,7 @@ class PyPandas(PythonPackage):
     version('0.24.1', sha256='435821cb2501eabbcee7e83614bd710940dc0cf28b5afbc4bdb816c31cec71af')
     version('0.23.4', sha256='5b24ca47acf69222e82530e89111dd9d14f9b970ab2cd3a1c2c78f0c4fbba4f4')
     version('0.21.1', '42ae7f81b81a86c3f91f663b66c525f7')
+    version('0.20.0', sha256='54f7a2bb2a7832c0446ad51d779806f07ec4ea2bb7c9aea4b83669fa97e778c4')
     version('0.19.2', '26df3ef7cd5686fa284321f4f48b38cd')
     version('0.19.0', 'bc9bb7188e510b5d44fbdd249698a2c3')
     version('0.18.0', 'f143762cd7a59815e348adf4308d2cf6')
