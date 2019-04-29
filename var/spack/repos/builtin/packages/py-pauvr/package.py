9ff988ba1856f8dea279b75f7f2e7c5808ce8c7a
xfzhao <xfzhao@iastate.edu>
2019-04-29 13:15:50 -0500

    py-pauvr: using url and Releases

--- var/spack/repos/builtin/packages/py-pauvr/package.py
+++ var/spack/repos/builtin/packages/py-pauvr/package.py
@@ -3,6 +3,23 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install py-pauvr
+#
+# You can edit this file again by typing:
+#
+#     spack edit py-pauvr
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
 from spack import *
 
 
@@ -10,14 +27,13 @@ class PyPauvr(PythonPackage):
     """pauvr: a plotting package designed for nanopore and PacBio long reads"""
 
     homepage = "https://github.com/conchoecia/pauvre"
-    git      = "https://github.com/conchoecia/pauvre.git"
+    url      = "https://github.com/conchoecia/pauvre/archive/0.1.86.tar.gz"
 
-    version('develop', branch='develop')
-    version('master',  branch='master')
-    version('0.1.86',  tag='0.1.86')
+    version('0.1.86', sha256='aa0b3653e7c12fb50a0907ce088d85b8e1b52c97f40e4d2e6e6b7525a681aa1a')
 
     depends_on('python@3:',        type=('build', 'run'))
     depends_on('py-matplotlib',    type=('build', 'run'))
     depends_on('py-biopython',     type=('build', 'run'))
     depends_on('py-pandas',        type=('build', 'run'))
     depends_on('py-pillow',        type=('build', 'run'))
+
