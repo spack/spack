a605fc7e06dd63a38151560e4ffdc06ce0213913
Sean McGrath <smcgrat@tcd.ie>
2019-05-01 19:33:03 +0100

    ffmpeg package: add aom support (#11210)
    * Add Aom package
    * Add aom variant to ffmpeg package which controls whether to build
      with the Aom dependency

--- var/spack/repos/builtin/packages/ffmpeg/package.py
+++ var/spack/repos/builtin/packages/ffmpeg/package.py
@@ -11,15 +11,20 @@ class Ffmpeg(AutotoolsPackage):
     convert and stream audio and video."""
 
     homepage = "https://ffmpeg.org"
-    url      = "http://ffmpeg.org/releases/ffmpeg-3.2.4.tar.bz2"
+    url      = "http://ffmpeg.org/releases/ffmpeg-4.1.1.tar.bz2"
 
+    version('4.1.1',   '4a64e3cb3915a3bf71b8b60795904800')
     version('4.1',   'b684fb43244a5c4caae652af9022ed5d85ce15210835bce054a33fb26033a1a5')
     version('3.2.4', 'd3ebaacfa36c6e8145373785824265b4')
 
     variant('shared', default=True,
             description='build shared libraries')
 
+    variant('aom', default=False,
+            description='build Alliance for Open Media libraries')
+
     depends_on('yasm@1.2.0:')
+    depends_on('aom', when='+aom')
 
     def configure_args(self):
         spec = self.spec
@@ -27,5 +32,10 @@ class Ffmpeg(AutotoolsPackage):
 
         if '+shared' in spec:
             config_args.append('--enable-shared')
+
+        if '+aom' in spec:
+            config_args.append('--enable-libaom')
+        else:
+            config_args.append('--disable-libaom')
 
         return config_args
