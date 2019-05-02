d9a1bf1ca9d84fd7acedffe74f5191a763ab8517
Levi Baber <baber@iastate.edu>
2019-05-02 11:04:21 -0500

    fix error when adding package to env in container (#11321)

--- lib/spack/spack/environment.py
+++ lib/spack/spack/environment.py
@@ -125,7 +125,7 @@ def activate(
             cmds += 'setenv SPACK_OLD_PROMPT "${prompt}";\n'
             cmds += 'set prompt="%s ${prompt}";\n' % prompt
     else:
-        if 'color' in os.environ['TERM'] and prompt:
+        if os.getenv('TERM') and 'color' in os.getenv('TERM') and prompt:
             prompt = colorize('@G{%s} ' % prompt, color=True)
 
         cmds += 'export SPACK_ENV=%s;\n' % env.path
