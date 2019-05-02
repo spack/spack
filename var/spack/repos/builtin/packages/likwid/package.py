0a35341c20cf934166008905461d39ab05be2dbe
Thomas Roehl <thomas.roehl@fau.de>
2019-05-02 10:07:35 +0200

    Rebase due to conflict

--- var/spack/repos/builtin/packages/likwid/package.py
+++ var/spack/repos/builtin/packages/likwid/package.py
@@ -19,6 +19,10 @@ class Likwid(Package):
     homepage = "https://github.com/RRZE-HPC/likwid"
     url      = "https://github.com/RRZE-HPC/likwid/archive/4.1.2.tar.gz"
 
+    maintainers = ['davydden']
+
+    version('4.3.4', '37bdcd20630ef3694435065a6c8a14fa')
+    version('4.3.3', '70ff0d1238989010842644db792f4910')
     version('4.3.2', '2cf00e220dfe22c8d9b6e44f7534e11d')
     version('4.3.1', 'ff28250f622185688bf5e2e0975368ea')
     version('4.3.0', '7f8f6981d7d341fce2621554323f8c8b')
