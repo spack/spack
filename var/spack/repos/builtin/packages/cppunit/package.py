1ffa5d343809f247643f9a1dd2ed6af8c8ff9485
Chris Green <greenc@fnal.gov>
2019-05-03 02:12:22 -0500

    cppunit: New version 1.14.0. (#11359)

--- var/spack/repos/builtin/packages/cppunit/package.py
+++ var/spack/repos/builtin/packages/cppunit/package.py
@@ -12,6 +12,7 @@ class Cppunit(AutotoolsPackage):
     homepage = "https://wiki.freedesktop.org/www/Software/cppunit/"
     url = "http://dev-www.libreoffice.org/src/cppunit-1.13.2.tar.gz"
 
+    version('1.14.0', sha256='3d569869d27b48860210c758c4f313082103a5e58219a7669b52bfd29d674780')
     version('1.13.2', '0eaf8bb1dcf4d16b12bec30d0732370390d35e6f')
 
     variant('cxxstd',
