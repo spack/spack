4e2508c404df05f090aba28711373d8f76e6d0e1
Phil Carns <carns@mcs.anl.gov>
2019-05-03 02:11:31 -0500

    add BMI package (#11310)
    - also enable optional dependency in Mercury

---
+++ var/spack/repos/builtin/packages/bmi/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Bmi(AutotoolsPackage):
+    """a communications framework and network abstraction layer"""
+
+    homepage = 'http://git.mcs.anl.gov/bmi.git/'
+    git = 'git://git.mcs.anl.gov/bmi'
+
+    version('develop', branch='master')
+
+    depends_on('autoconf', type='build')
+
+    # need to override 'autoreconf' so we can run BMI's 'prepare' script
+    def autoreconf(self, spec, prefix):
+        Executable('./prepare')()
+
+    def configure_args(self):
+        args = ["--enable-shared", "--enable-bmi-only"]
+        return args
