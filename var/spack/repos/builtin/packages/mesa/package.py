03546ba81f19377868b3155da2582b4f325c6230
George Hartzell <ghartzell@audentestx.com>
2019-05-03 09:03:36 -0700

    Constrain glproto prereq to when +glx

--- var/spack/repos/builtin/packages/mesa/package.py
+++ var/spack/repos/builtin/packages/mesa/package.py
@@ -33,7 +33,7 @@ class Mesa(MesonPackage):
     depends_on('libxml2')
     depends_on('zlib')
     depends_on('expat')
-    depends_on('glproto')
+    depends_on('glproto', when='+glx')
 
     # Internal options
     variant('llvm', default=True, description="Enable LLVM.")
