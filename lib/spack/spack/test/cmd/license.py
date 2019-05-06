dbf9a708cdab9bf5d2690066ef5ed4516ade5996
Peter Josef Scheibel <scheibel1@llnl.gov>
2019-05-06 16:35:09 -0700

    make license check more-strict
    The license text is now expected to match almost exactly (not
    accounting for formatting in different file types (e.g. rst vs.
    bash script vs. python)

--- lib/spack/spack/test/cmd/license.py
+++ lib/spack/spack/test/cmd/license.py
@@ -61,7 +61,7 @@ def test_verify(tmpdir):
     assert str(old_lgpl_header) in out
     assert str(correct_header) not in out
     assert '3 improperly licensed files' in out
-    assert re.search(r'files with no SPDX-License-Identifier:\s*1', out)
+    assert re.search(r'files not containing expected license:\s*1', out)
     assert re.search(r'files with wrong SPDX-License-Identifier:\s*1', out)
     assert re.search(r'files with old license header:\s*1', out)
 
