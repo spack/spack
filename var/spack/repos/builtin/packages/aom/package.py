96bb717767de18268b0df7829dbdb67564a9b02f
Peter Josef Scheibel <scheibel1@llnl.gov>
2019-05-09 10:53:09 -0700

    update aom package license

--- var/spack/repos/builtin/packages/aom/package.py
+++ var/spack/repos/builtin/packages/aom/package.py
@@ -1,8 +1,16 @@
-# Copyright 2013-2019 Trinity College Dublin and other Spack Project
-# Developers. See the top-level COPYRIGHT file for details.
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+#
+# Author: Darach Golden <dgolden@tchpc.tcd.ie>,
+# Institution: Trinity Centre for High Performance Computing, https://www.tchpc.tcd.ie/  # noqa: E501
+# Date: May 09, 2019
+# Author: Sean McGrath <smcgrat@tchpc.tcd.ie>
+# Institution: Trinity Centre for High Performance Computing, https://www.tchpc.tcd.ie/  # noqa: E501
+# Date: May 09, 2019
+#
 from spack import *
 
 
