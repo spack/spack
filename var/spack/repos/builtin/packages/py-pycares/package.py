765ec30d210b5d47a5d47264faa87c261e8bc96d
Shane R. Spencer <shane@bogomip.com>
2019-05-10 09:43:01 -0800

    Update package.py

--- var/spack/repos/builtin/packages/py-pycares/package.py
+++ var/spack/repos/builtin/packages/py-pycares/package.py
@@ -1,5 +1,5 @@
-# Copyright 2019 Shane R. Spencer and other Spack Project Developers. See the
-# top-level COPYRIGHT file for details.
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
