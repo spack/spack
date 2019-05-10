c6ab0604e6374f296e90a6e9030f1ae2b29377a0
William Dawson <william.dawson@riken.jp>
2019-05-10 18:33:24 +0900

    Basic build works.

--- var/spack/repos/builtin/packages/elsi/package.py
+++ var/spack/repos/builtin/packages/elsi/package.py
@@ -18,20 +18,42 @@ class Elsi(CMakePackage):
 
     version('2.2.1', sha256='5b4b2e8fa4b3b68131fe02cc1803a884039b89a1b1138af474af66453bec0b4d')
 
-    depends_on('cmake')
-    depends_on('mpi')
-    depends_on('slepc')
-    depends_on('petsc')
+    variant(
+      'enable_pexsi', default=False, description='Enable PEXSI support'
+    )
+    variant(
+      'enable_sips', default=False, description='Enable SLEPc-SIPs support'
+    )
+
     depends_on('blas')
-    depends_on('lapack')
-    depends_on('scalapack')
+    depends_on('cmake')
     depends_on('elpa')
+    depends_on('lapack')
+    depends_on('mpi')
     depends_on('ntpoly')
-    depends_on('superlu')
+    depends_on('slepc', when='+enable_sips')
+    depends_on('petsc', when='+enable_sips')
+    depends_on('scalapack')
+    depends_on('superlu', when='+enable_pexsi')
 
     def cmake_args(self):
         args = []
-        args += "-DUSE_EXTERNAL_ELPA=ON"
-        args += "-DUSE_EXTERNAL_NTPOLY=ON"
-        args += "-DUSE_EXTERNAL_SUPERLU=ON"
+
+        # Compiler Information
+        # (ELSI wants these explicitly set)
+        args += ["-DCMAKE_Fortran_COMPILER="+self.spec["mpi"].mpifc]
+        args += ["-DCMAKE_C_COMPILER="+self.spec["mpi"].mpicc]
+        args += ["-DCMAKE_CXX_COMPILER="+self.spec["mpi"].mpicxx]
+
+        # External Arguments
+        #args += ["-DUSE_EXTERNAL_ELPA=ON"]
+        args += ["-DUSE_EXTERNAL_NTPOLY=ON"]
+
+        # Optional parameters
+        if '+enable_pexsi' in self.spec:
+            args += ["-DENABLE_PEXSI=ON"]
+            args += ["-DUSE_EXTERNAL_SUPERLU=ON"]
+        if '+enable_sips' in self.spec:
+            args += ["-DENABLE_SIPS=ON"]
+
         return args
