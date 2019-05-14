172f877044e8c0212bd8b842d7d959b8111b6862
Peter Josef Scheibel <scheibel1@llnl.gov>
2019-05-14 10:44:49 -0700

    update 'libs' for the CUDA package to exclude the compatibility libraries

--- var/spack/repos/builtin/packages/cuda/package.py
+++ var/spack/repos/builtin/packages/cuda/package.py
@@ -5,6 +5,8 @@
 
 from spack import *
 from glob import glob
+from llnl.util.filesystem import LibraryList
+import os
 
 
 class Cuda(Package):
@@ -58,3 +60,16 @@ class Cuda(Package):
             '--toolkit',        # install CUDA Toolkit
             '--toolkitpath=%s' % prefix
         )
+
+    @property
+    def libs(self):
+        libs = find_libraries(
+            'libcuda', root=self.prefix, shared=True, recursive=True
+        )
+        filtered_libs = []
+        # CUDA 10.0 provides Compatability libraries for running newer versions
+        # of CUDA with older drivers. These do not work with newer drivers.
+        for lib in libs:
+            if 'compat' not in lib.split(os.sep):
+                filtered_libs.append(lib)
+        return LibraryList(filtered_libs)
