1cfa7425b27f5fcf9a3cba3307b966dc94ffcaa6
Justin S <3630356+codeandkey@users.noreply.github.com>
2019-05-14 15:57:54 -0500

    r-rstantools: new package at 1.5.1 (#11451)

---
+++ var/spack/repos/builtin/packages/r-rstantools/package.py
@@ -0,0 +1,16 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RRstantools(RPackage):
+    """rstantools: Tools for Developing R Packages Interfacing with 'Stan'"""
+
+    homepage = "http://discourse.mc-stan.org/"
+    url      = "https://cran.r-project.org/src/contrib/rstantools_1.5.1.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/rstantools"
+
+    version('1.5.1', sha256='5cab16c132c12e84bd08e18cd6ef25ba39d67a04ce61015fc4490659c7cfb485')
