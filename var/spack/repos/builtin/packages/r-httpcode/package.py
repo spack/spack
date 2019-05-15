a6e17910480dcc1e95e1451eab4079543281f13f
Justin S <3630356+codeandkey@users.noreply.github.com>
2019-05-15 15:50:32 -0500

    r-httpcode: new package at 0.2.0 (#11454)

---
+++ var/spack/repos/builtin/packages/r-httpcode/package.py
@@ -0,0 +1,16 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RHttpcode(RPackage):
+    """httpcode: 'HTTP' Status Code Helper"""
+
+    homepage = "https://github.com/sckott/httpcode"
+    url      = "https://cran.r-project.org/src/contrib/httpcode_0.2.0.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/httpcode"
+
+    version('0.2.0', sha256='fbc1853db742a2cc1df11285cf27ce2ea43bc0ba5f7d393ee96c7e0ee328681a')
