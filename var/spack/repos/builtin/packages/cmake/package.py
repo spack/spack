e0bccfb8587894772cc571efb16c1d044b1688c0
Leonhard Reichenbach <Zehvogel@users.noreply.github.com>
2019-05-15 19:07:31 +0200

    CMake package: add dependencies (#11257)
    Add dependencies to CMake and GnuTLS which would otherwise be
    picked up from the system
    
    * Add gnutls as explicit dependency of CMake
    * Add new package libidn2 as explicit dependency for gnutls

--- var/spack/repos/builtin/packages/cmake/package.py
+++ var/spack/repos/builtin/packages/cmake/package.py
@@ -82,6 +82,7 @@ class Cmake(Package):
     depends_on('openssl', when='+openssl')
     depends_on('openssl@:1.0.99', when='@:3.6.9+openssl')
     depends_on('ncurses',        when='+ncurses')
+    depends_on('gnutls')
 
     # Cannot build with Intel, should be fixed in 3.6.2
     # https://gitlab.kitware.com/cmake/cmake/issues/16226
