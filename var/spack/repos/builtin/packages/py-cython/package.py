af0e53bc5e60eaffd93ca20829aa53205622c6f4
Adam J. Stewart <ajstewart426@gmail.com>
2019-05-16 17:30:52 -0500

    Add latest version of py-cython

--- var/spack/repos/builtin/packages/py-cython/package.py
+++ var/spack/repos/builtin/packages/py-cython/package.py
@@ -8,9 +8,17 @@ from spack import *
 
 class PyCython(PythonPackage):
     """The Cython compiler for writing C extensions for the Python language."""
-    homepage = "https://pypi.python.org/pypi/cython"
-    url      = "https://pypi.io/packages/source/c/cython/Cython-0.25.2.tar.gz"
 
+    homepage = "https://pypi.python.org/pypi/cython"
+    url      = "https://pypi.io/packages/source/c/cython/Cython-0.29.7.tar.gz"
+
+    import_modules = [
+        'Cython', 'pyximport', 'Cython.Distutils', 'Cython.Runtime',
+        'Cython.Tempita', 'Cython.Debugger', 'Cython.Plex', 'Cython.Build',
+        'Cython.Compiler', 'Cython.Utility',
+    ]
+
+    version('0.29.7', sha256='55d081162191b7c11c7bfcb7c68e913827dfd5de6ecdbab1b99dab190586c1e8')
     version('0.29.5', sha256='9d5290d749099a8e446422adfb0aa2142c711284800fb1eb70f595101e32cbf1')
     version('0.29', sha256='94916d1ede67682638d3cc0feb10648ff14dc51fb7a7f147f4fedce78eaaea97')
     version('0.28.6', '3c3fb47806a4476f8e9429943439cc60')
@@ -24,7 +32,15 @@ class PyCython(PythonPackage):
     version('0.22', '1ae25add4ef7b63ee9b4af697300d6b6')
     version('0.21.2', 'd21adb870c75680dc857cd05d41046a4')
 
+    depends_on('py-setuptools', type='build')
+
     @property
     def command(self):
         """Returns the Cython command"""
         return Executable(self.prefix.bin.cython)
+
+    def test(self):
+        # Seems to be a bug in the testing code:
+        # https://github.com/cython/cython/issues/2957
+        # TypeError: __init__() missing 1 required positional argument: 'dist'
+        pass
