4a1ce3789732aa143cbd693fcd082eff9a385663
Adam J. Stewart <ajstewart426@gmail.com>
2019-05-20 15:39:25 -0500

    Make flake8 check spack script as well as .py files (#11513)
    * Make flake8 check bin/spack

--- lib/spack/spack/cmd/flake8.py
+++ lib/spack/spack/cmd/flake8.py
@@ -127,7 +127,7 @@ def changed_files(args):
 
         for f in files:
             # Ignore non-Python files
-            if not f.endswith('.py'):
+            if not (f.endswith('.py') or f == 'bin/spack'):
                 continue
 
             # Ignore files in the exclude locations
