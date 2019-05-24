d638051ce37594427d2710a2a59cd4f44252c55e
Levi Baber <baber@iastate.edu>
2019-05-24 13:20:29 -0500

    r-geomorph: new version

--- var/spack/repos/builtin/packages/r-geomorph/package.py
+++ var/spack/repos/builtin/packages/r-geomorph/package.py
@@ -16,7 +16,8 @@ class RGeomorph(RPackage):
     url      = "https://cran.r-project.org/src/contrib/geomorph_3.0.5.tar.gz"
     list_url = "https://cran.r-project.org/src/contrib/Archive/geomorph"
 
-    version('3.0.7', '68f8942e1a5bc3f0298c9f5b8f69c4702c8e12ecb2275d740fda4d04d66d38e1')
+    version('3.1.1', sha256='8eb222011433100860b308beef6f02ade7c421785f575ab4461ee25e38dfa1bd')
+    version('3.0.7', sha256='68f8942e1a5bc3f0298c9f5b8f69c4702c8e12ecb2275d740fda4d04d66d38e1')
     version('3.0.5', '240e69fe260ca3ef4d84b4281d61396c')
 
     depends_on('r@3.1.0:', type=('build', 'run'))
