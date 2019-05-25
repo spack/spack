76979f4712b4ead2ffb37c47da751598eda6f7e9
Olaf Mersmann <olafm@p-value.net>
2019-05-25 11:46:48 +0200

    libassuan: Update to v2.5.3

--- var/spack/repos/builtin/packages/libassuan/package.py
+++ var/spack/repos/builtin/packages/libassuan/package.py
@@ -13,6 +13,7 @@ class Libassuan(AutotoolsPackage):
     homepage = "https://gnupg.org/software/libassuan/index.html"
     url = "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.5.tar.bz2"
 
+    version('2.5.3', sha256='91bcb0403866b4e7c4bc1cc52ed4c364a9b5414b3994f718c70303f7f765e702')
     version('2.4.5', '4f22bdb70d424cfb41b64fd73b7e1e45')
     version('2.4.3', '8e01a7c72d3e5d154481230668e6eb5a')
 
