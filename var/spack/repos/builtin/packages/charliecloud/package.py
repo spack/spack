5f3e818a3a7d6036d5fe6f35b05badbd410c9da1
Jordan Ogas <jogas@lanl.gov>
2019-05-30 16:27:31 -0600

    remove url declaration

--- var/spack/repos/builtin/packages/charliecloud/package.py
+++ var/spack/repos/builtin/packages/charliecloud/package.py
@@ -10,7 +10,6 @@ class Charliecloud(MakefilePackage):
     """Lightweight user-defined software stacks for HPC."""
 
     homepage = "https://hpc.github.io/charliecloud"
-    url = "https://github.com/hpc/charliecloud/releases/download/v0.9.10/charliecloud-0.9.10.tar.gz"
 
     version('0.9.10', sha256='44e821b62f9c447749d3ed0d2b2e44d374153058814704a5543e83f42db2a45a')
     version('0.9.9', sha256='2624c5a0b19a01c9bca0acf873ceeaec401b9185a23e9108fadbcee0b9d74736')
