f5b7a86f0f1dd5649cb434fabdd70dea0a2cecea
Patrick Gartung <gartung@fnal.gov>
2019-05-31 12:37:00 -0500

    Pass needed flags to make

--- var/spack/repos/builtin/packages/git/package.py
+++ var/spack/repos/builtin/packages/git/package.py
@@ -238,6 +238,16 @@ class Git(AutotoolsPackage):
             # Don't link with -lrt; the system has no (and needs no) librt
             filter_file(r' -lrt$', '', 'Makefile')
 
+    @run_after('configure')
+    def make_opts(self):
+        self.build_targets=['NO_INSTALL_HARDLINKS=1',
+                            'ETC_GITATTRIBUTES=config',
+                            'ETC_GITCONFIG=config']
+        self.install_targets=['NO_INSTALL_HARDLINKS=1',
+                              'ETC_GITATTRIBUTES=config',
+                              'ETC_GITCONFIG=config',
+                              'install']
+
     def check(self):
         make('test')
 
