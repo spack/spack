0990f12dd9f22597991f6a08594060deff0bb755
Greg Becker <becker33@llnl.gov>
2019-06-05 08:15:47 +0900

    modules: set permissions based on package configuration (#11337)
    Previously, module files were not set with the same permissions as the package installation.  For world-readable packages, this would not cause a problem.  For group readable packages, it does:
    
    ```
    packages:
      mypackage:
        permissions:
          group: mygroup
          read: group
          write: group
    ```
    
    In this case, the modulefile is unreadable by members of the group other than the one who installed it.  Add logic to the modulefile writers to set the permissions based on the configuration in `packages.yaml`

--- lib/spack/spack/test/modules/common.py
+++ lib/spack/spack/test/modules/common.py
@@ -3,8 +3,12 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
+import pytest
+import os
+import stat
+import spack.spec
 import spack.modules.common
+import spack.modules.tcl
 
 
 def test_update_dictionary_extending_list():
@@ -32,3 +36,37 @@ def test_update_dictionary_extending_list():
     assert len(target['foo']) == 4
     assert len(target['bar']) == 4
     assert target['baz'] == 'foobaz'
+
+
+@pytest.fixture()
+def mock_module_filename(monkeypatch, tmpdir):
+    filename = str(tmpdir.join('module'))
+    monkeypatch.setattr(spack.modules.common.BaseFileLayout,
+                        'filename',
+                        filename)
+
+    yield filename
+
+
+@pytest.fixture()
+def mock_package_perms(monkeypatch):
+    perms = stat.S_IRGRP | stat.S_IWGRP
+    monkeypatch.setattr(spack.package_prefs,
+                        'get_package_permissions',
+                        lambda spec: perms)
+
+    yield perms
+
+
+def test_modules_written_with_proper_permissions(mock_module_filename,
+                                                 mock_package_perms,
+                                                 mock_packages, config):
+    spec = spack.spec.Spec('mpileaks').concretized()
+
+    # The code tested is common to all module types, but has to be tested from
+    # one. TCL picked at random
+    generator = spack.modules.tcl.TclModulefileWriter(spec)
+    generator.write()
+
+    assert mock_package_perms & os.stat(
+        mock_module_filename).st_mode == mock_package_perms
