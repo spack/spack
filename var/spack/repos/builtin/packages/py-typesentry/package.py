eabc8a69514855104f3cd4502a75902e79c2a606
Sinan81 <Sinan81@github>
2019-06-07 04:07:26 -0700

    new python package/py-typesentry

---
+++ var/spack/repos/builtin/packages/py-typesentry/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class PyTypesentry(PythonPackage):
+    """Python library for run-time type checking for type-annotated functions.
+    """
+
+    homepage = "https://github.com/h2oai/typesentry"
+    git = "https://github.com/h2oai/typesentry.git"
+
+    version('2018-10-12', commit='0ca8ed0e62d15ffe430545e7648c9a9b2547b49c')
+
+    depends_on('py-colorama@0.3.0:', type=('build', 'run'))
