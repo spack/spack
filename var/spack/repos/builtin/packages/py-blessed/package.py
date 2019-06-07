86a794ad88b9866030720bb34bc93774bd4d67a5
Sinan81 <Sinan81@github>
2019-06-07 03:35:54 -0700

    make flake8 happy

--- var/spack/repos/builtin/packages/py-blessed/package.py
+++ var/spack/repos/builtin/packages/py-blessed/package.py
@@ -8,14 +8,13 @@ from spack import *
 
 
 class PyBlessed(PythonPackage):
-    """Blessed is a thin, practical wrapper around terminal capabilities in Python."""
+    """Blessed is a thin, practical wrapper around terminal capabilities in
+Python."""
 
     homepage = "https://github.com/jquast/blessed"
     url      = "https://pypi.io/packages/source/b/blessed/blessed-1.15.0.tar.gz"
 
     version('1.15.0', sha256='777b0b6b5ce51f3832e498c22bc6a093b6b5f99148c7cbf866d26e2dec51ef21')
 
-    # depends_on('py-setuptools', type='build')
     depends_on('py-wcwidth', type=('build', 'run'))
     depends_on('py-six', type=('build', 'run'))
-
