2be37e1f4958548dc49aba7da787a8a53cdc7254
Tamara Dahlgren <dahlgren1@llnl.gov>
2019-06-11 12:59:25 -0700

    Fixed location command test environment issues.

--- lib/spack/spack/test/cmd/location.py
+++ lib/spack/spack/test/cmd/location.py
@@ -8,11 +8,15 @@ import shutil
 import pytest
 
 from llnl.util.filesystem import mkdirp
+import spack.environment as ev
 from spack.cmd.location import BadEnvironmentError, MissingBuildDirectoryError
 from spack.cmd.location import SpecificationError
 from spack.main import SpackCommand
 import spack.paths
 
+
+# everything here uses the mock_env_path
+pytestmark = pytest.mark.usefixtures('config')
 
 # location prints out "locations of packages and spack directories"
 location = SpackCommand('location')
@@ -22,6 +26,15 @@ _pkg_dir = os.path.join(_repo_dir, 'packages')
 
 _mock_spec_name = 'externaltest'
 _test_env_name = 'test'
+
+
+@pytest.fixture
+def mock_test_env(mutable_mock_env_path):
+    env_dir = ev.root(_test_env_name)
+    mkdirp(env_dir)
+    yield
+
+    shutil.rmtree(env_dir)
 
 
 @pytest.fixture
@@ -38,10 +51,41 @@ def mock_spec():
     shutil.rmtree(pkg.stage.path)
 
 
+def test_location_bad_env():
+    """Tests spack location with an unknown environment."""
+    with pytest.raises(BadEnvironmentError, match="No such environment"):
+        location('--env', 'test-unknown-env')
+
+
+@pytest.mark.parametrize('spec1,spec2,msg', [
+    (None, None, "You must supply a spec"),
+    ('spec1', 'spec2', "Only one spec, not 2")])
+def test_location_bad_spec_options(spec1, spec2, msg):
+    """Tests spack location with bad spec options."""
+    args = [spec1, spec2]
+    if not all(args):
+        args = []
+
+    with pytest.raises(SpecificationError, match=msg):
+        location(*args)
+
+
+@pytest.mark.db
+@pytest.mark.usefixtures('database', 'mock_spec')
+def test_location_build_dir():
+    """Tests spack location --build-dir"""
+    out = location('--build-dir', _mock_spec_name)
+    assert _mock_spec_name in out
+
+
+@pytest.mark.usefixtures('mock_test_env')
+def test_location_env():
+    out = location('--env', _test_env_name)
+    assert 'mock-env-path' in out and out[:-1].endswith(_test_env_name)
+
+
 @pytest.mark.db
 @pytest.mark.parametrize('cmd,option,result,endswith', [
-    ('--env', _test_env_name, os.path.join('environments', _test_env_name),
-     True),
     ('--install-dir', 'libelf', 'libelf-', False),
     ('--module-dir', None, spack.paths.module_path, True),
     ('--package-dir', 'mpileaks', os.path.join(_pkg_dir, 'mpileaks'), True),
@@ -64,35 +108,9 @@ def test_location_options(cmd, option, result, endswith):
 
 
 @pytest.mark.db
-@pytest.mark.usefixtures('database', 'mock_spec')
-def test_location_build_dir():
-    """Tests spack location --build-dir"""
-    out = location('--build-dir', _mock_spec_name)
-    assert _mock_spec_name in out
-
-
-def test_location_bad_env():
-    """Tests spack location with an unknown environment."""
-    with pytest.raises(BadEnvironmentError, match="No such environment"):
-        location('--env', 'test-unknown-env')
-
-
 @pytest.mark.usefixtures('database')
 def test_location_missing_build_dir():
     """Tests spack location with a missing build directory."""
     with pytest.raises(MissingBuildDirectoryError,
                        match="Build directory does not exist yet"):
         location('--build-dir', 'mpileaks')
-
-
-@pytest.mark.parametrize('spec1,spec2,msg', [
-    (None, None, "You must supply a spec"),
-    ('spec1', 'spec2', "Only one spec, not 2")])
-def test_location_bad_spec_options(spec1, spec2, msg):
-    """Tests spack location with bad spec options."""
-    args = [spec1, spec2]
-    if not all(args):
-        args = []
-
-    with pytest.raises(SpecificationError, match=msg):
-        location(*args)
