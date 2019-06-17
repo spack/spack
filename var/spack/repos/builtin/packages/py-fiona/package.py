4237f3f87ca8dd98e7e8347a5ca2b6ded3b538c9
Adam J. Stewart <ajstewart426@gmail.com>
2019-06-17 13:12:15 -0500

    Python 3 will be required in the next release

--- var/spack/repos/builtin/packages/py-fiona/package.py
+++ var/spack/repos/builtin/packages/py-fiona/package.py
@@ -18,6 +18,7 @@ class PyFiona(PythonPackage):
     version('1.8.6',  sha256='fa31dfe8855b9cd0b128b47a4df558f1b8eda90d2181bff1dd9854e5556efb3e')
     version('1.7.12', sha256='8b54eb8422d7c502bb7776b184018186bede1a489cf438a7a47f992ade6a0e51')
 
+    depends_on('python@3:', type=('build', 'run'), when='@1.9:')
     depends_on('python@2.6:', type=('build', 'run'))
     depends_on('gdal@1.8:', type=('build', 'run'))
 
