7b78d7ccce90c73ee11bb2f50c8d50852efd00eb
Sinan <sbulutw@gmail.com>
2019-06-16 21:31:44 -0700

    Update var/spack/repos/builtin/packages/py-blessed/package.py
    specify dependency version.
    
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-blessed/package.py
+++ var/spack/repos/builtin/packages/py-blessed/package.py
@@ -17,4 +17,4 @@ class PyBlessed(PythonPackage):
     version('1.15.0', sha256='777b0b6b5ce51f3832e498c22bc6a093b6b5f99148c7cbf866d26e2dec51ef21')
 
     depends_on('py-wcwidth', type=('build', 'run'))
-    depends_on('py-six', type=('build', 'run'))
+    depends_on('py-six@1.9.0:', type=('build', 'run'))
