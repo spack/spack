60c8f5598c65ad8d1d33e1f3b622bf6c5afda65b
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-06-20 00:38:04 +0200

    py-matplotlib: Fix dependencies in [2.1.0:3.0.0) (#11787)
    Starting with 2.1.0, backports.functools_lru_cache replaces functools32
    when using python 2.7.x until python2 support is dropped in 3.0.0.
    
    Change-Id: I8db61eac975da0042f6f5babe0f5f0c92807200e

--- var/spack/repos/builtin/packages/py-matplotlib/package.py
+++ var/spack/repos/builtin/packages/py-matplotlib/package.py
@@ -61,8 +61,10 @@ class PyMatplotlib(PythonPackage):
     depends_on('py-pytz', type=('build', 'run'))
     depends_on('py-cycler@0.9:', type=('build', 'run'))
     depends_on('py-subprocess32', type=('build', 'run'), when='^python@:2.7')
-    depends_on('py-functools32', type=('build', 'run'), when='^python@2.7')
+    depends_on('py-functools32', type=('build', 'run'), when='@:2.0.999 ^python@2.7')
     depends_on('py-kiwisolver', type=('build', 'run'), when='@2.2.0:')
+    depends_on('py-backports-functools-lru-cache', type=('build', 'run'),
+               when='@2.1.0:2.999.999')
 
     # ------ Optional GUI frameworks
     depends_on('tk@8.3:', when='+tk')  # not 8.6.0 or 8.6.1
