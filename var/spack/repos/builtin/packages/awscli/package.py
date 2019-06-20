e1af8c6e66a2750c2427dac62eff5116b1cf5f17
Neil Flood <Neil.Flood@des.qld.gov.au>
2019-06-21 06:48:21 +1000

    More precise py-pyyaml version dep, as per setup.py

--- var/spack/repos/builtin/packages/awscli/package.py
+++ var/spack/repos/builtin/packages/awscli/package.py
@@ -20,7 +20,10 @@ class Awscli(PythonPackage):
     depends_on('py-docutils@0.10:', type=('build', 'run'))
     depends_on('py-colorama@0.2.5:0.3.9', type=('build', 'run'))
     depends_on('py-rsa@3.1.2:3.5.0', type=('build', 'run'))
-    depends_on('py-pyyaml@3.10:3.13', type=('build', 'run'))
+    depends_on('py-pyyaml@3.10:3.13', type=('build', 'run'),
+               when='^python@2.6:2.6.99')
+    depends_on('py-pyyaml@3.10:5.1', type=('build', 'run'),
+               when='^python@2.7:')
     depends_on('py-argparse@1.1:', when='^python@2.6:2.6.99',
                type=('build', 'run'))
     depends_on('py-s3transfer@0.2.0:0.2.999', type=('build', 'run'))
