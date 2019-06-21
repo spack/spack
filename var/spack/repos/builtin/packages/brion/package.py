c57ce872d78d8bbdb3bcdb8f8ed7fcf6bad6c64c
ppodhajski <podhajski@gmail.com>
2019-06-21 13:51:00 +0200

    adding brion to serial libraries (#411)
    adding brion to serial libraries
    
    Co-Authored-By: Matthias Wolf <m+git@sushinara.net>

--- var/spack/repos/builtin/packages/brion/package.py
+++ var/spack/repos/builtin/packages/brion/package.py
@@ -24,7 +24,7 @@ class Brion(CMakePackage):
     depends_on('py-numpy', type=('build', 'run'), when='+python')
     depends_on('boost +python', when='+python')
 
-    depends_on('boost')
+    depends_on('boost@1.65.0')
     depends_on('lunchbox')
     depends_on('vmmlib')
     depends_on('highfive@2.1: +boost ~mpi')
