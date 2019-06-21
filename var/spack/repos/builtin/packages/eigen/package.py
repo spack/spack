c94634c6e9660b7e9057d34215fdc05e0ec36f5b
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2019-06-21 04:35:24 -0400

    Update eigen package with upsteam and add new versions (#270)

--- var/spack/repos/builtin/packages/eigen/package.py
+++ var/spack/repos/builtin/packages/eigen/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -14,22 +14,29 @@ class Eigen(CMakePackage):
     homepage = 'http://eigen.tuxfamily.org/'
     url      = 'https://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2'
 
-    version('3.3.5', 'e83549a79d1b721da0f8899ab34edf95')
-    version('3.3.4', 'a7aab9f758249b86c93221ad417fbe18')
-    version('3.3.3', 'b2ddade41040d9cf73b39b4b51e8775b')
-    version('3.3.1', 'edb6799ef413b0868aace20d2403864c')
+    version('3.3.7', sha256='9f13cf90dedbe3e52a19f43000d71fdf72e986beb9a5436dddcd61ff9d77a3ce')
+    version('3.3.6', sha256='6019aff3c9c32bc72f1a82590e66d0efdcea0eb669b487e66e772c80f12d674d')
+    version('3.3.5', sha256='7352bff3ea299e4c7d7fbe31c504f8eb9149d7e685dec5a12fbaa26379f603e2')
+    version('3.3.4', sha256='dd254beb0bafc695d0f62ae1a222ff85b52dbaa3a16f76e781dce22d0d20a4a6')
+    version('3.3.3', sha256='a4143fc45e4454b4b98fcea3516b3a79b8cdb3bc7fadf996d088c6a0d805fea1')
+    version('3.3.2', sha256='3e1fa6e8c45635938193f84fee6c35a87fac26ee7c39c68c230e5080c4a8fe98')
+    version('3.3.1', sha256='a0b4cebaabd8f371d1b364f9723585fbcc7c9640ca60273b99835e6cf115f056')
+    version('3.3.0', sha256='e3cf8f9289de20540a79c9c5653bbe623cadd6202bfe9692e95c420b5adbb7e7')
     version('3.2.10', 'a85bb68c82988648c3d53ba9768d7dcbcfe105f8')
     version('3.2.9', '59ab81212f8eb2534b1545a9b42c38bf618a0d71')
     version('3.2.8', '64f4aef8012a424c7e079eaf0be71793ab9bc6e0')
     version('3.2.7', 'cc1bacbad97558b97da6b77c9644f184')
 
-    variant('metis', default=True, description='Enables metis backend')
-    variant('scotch', default=True, description='Enables scotch backend')
-    variant('fftw', default=True, description='Enables FFTW backend')
-    variant('suitesparse', default=True,
-            description='Enables SuiteSparse support')
-    variant('mpfr', default=True,
-            description='Enables support for multi-precisions FP via mpfr')
+    variant('metis', default=False,
+            description='Enables metis permutations in sparse algebra')
+    variant('scotch', default=False,
+            description='Enables scotch/pastix sparse factorization methods')
+    variant('fftw', default=False,
+            description='Enables FFTW backend for the FFT plugin')
+    variant('suitesparse', default=False,
+            description='Enables SuiteSparse sparse factorization methods')
+    variant('mpfr', default=False,
+            description='Enables the multi-precisions floating-point plugin')
     variant('build_type', default='RelWithDebInfo',
             description='The build type to build',
             values=('Debug', 'Release', 'RelWithDebInfo'))
@@ -43,3 +50,13 @@ class Eigen(CMakePackage):
     depends_on('gmp', when='+mpfr')
 
     patch('find-ptscotch.patch', when='@3.3.4')
+
+    def setup_environment(self, spack_env, run_env):
+        run_env.prepend_path('CPATH',
+                             join_path(self.prefix, 'include', 'eigen3'))
+
+    @property
+    def headers(self):
+        headers = find_all_headers(self.prefix.include)
+        headers.directories = [self.prefix.include.eigen3]
+        return headers
