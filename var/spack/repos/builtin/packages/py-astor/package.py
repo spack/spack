0e792278a14eba178e5afc7cbc05a80faaf535e0
Andreas Baumbach <healther@users.noreply.github.com>
2019-06-23 20:53:02 +0200

    Update package.py

--- var/spack/repos/builtin/packages/py-astor/package.py
+++ var/spack/repos/builtin/packages/py-astor/package.py
@@ -13,8 +13,8 @@ class PyAstor(PythonPackage):
     homepage = "https://pypi.python.org/pypi/astor"
     url      = "https://pypi.io/packages/source/a/astor/astor-0.8.0.tar.gz"
 
-    version('0.8.0', '37a6eed8b371f1228db08234ed7f6cfdc7817a3ed3824797e20cbb11dc2a7862')
-    version('0.6', '175ec395cde36aa0178c5a3120d03954c65d1ef4bb19ec4aa30e9d7a7cc426c4')
+    version('0.8.0', sha256='37a6eed8b371f1228db08234ed7f6cfdc7817a3ed3824797e20cbb11dc2a7862')
+    version('0.6', sha256='175ec395cde36aa0178c5a3120d03954c65d1ef4bb19ec4aa30e9d7a7cc426c4')
 
     depends_on('python@2.7:2.8,3.4:')
     depends_on('py-setuptools', type='build')
