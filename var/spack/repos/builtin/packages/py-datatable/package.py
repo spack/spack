88fcafde238daf0842b008b9568f9b3a8258a8fa
Sinan81 <Sinan81@github>
2019-06-24 16:46:19 -0700

    add date for commit of interest, restore url

--- var/spack/repos/builtin/packages/py-datatable/package.py
+++ var/spack/repos/builtin/packages/py-datatable/package.py
@@ -16,12 +16,13 @@ class PyDatatable(PythonPackage):
     """
 
     homepage = "https://github.com/h2oai/datatable"
-    url      = "https://github.com/h2oai/datatable/"
+    url      = "https://github.com/h2oai/datatable/archive/v0.8.0.tar.gz"
     git      = "https://github.com/h2oai/datatable.git"
 
     # Use the version of the package as of the following git commit
-    # as v0.8.0 yields a run time error. See the following ticket:
+    # since v0.8.0 yields a run time error. See the following ticket:
     # https://github.com/h2oai/datatable/issues/1875#issuecomment-504695182
+    # the below commit was pushed on 2019 June 21
     version('master', git=git, commit='b533b487e26519e586a0231641d82576f322a683')
 
     depends_on('python@3.5.0:')
@@ -33,3 +34,9 @@ class PyDatatable(PythonPackage):
     depends_on('py-xlrd',       type=('build', 'run'))
     depends_on('py-blessed',    type=('build', 'run'))
     depends_on('py-typesentry', type=('build', 'run'))
+
+    def setup_py(self, *args, **kwargs):
+        setup = self.setup_file()
+
+        with working_dir(self.build_directory):
+            self.python('-s', setup, *args, **kwargs)
