acc40ab7745e8137f81964ce7969de8c3c1f34f9
Carson Woods <carsonwoods@users.noreply.github.com>
2019-06-24 19:20:23 -0600

    Singularity package: add version 3.2.1 (#11833)

--- var/spack/repos/builtin/packages/singularity/package.py
+++ var/spack/repos/builtin/packages/singularity/package.py
@@ -25,6 +25,7 @@ class Singularity(MakefilePackage):
     git      = "https://github.com/sylabs/singularity.git"
 
     version('develop', branch='master')
+    version('3.2.1', sha256='d4388fb5f7e0083f0c344354c9ad3b5b823e2f3f27980e56efa7785140c9b616')
     version('3.1.1', '158f58a79db5337e1d655ee0159b641e42ea7435')
 
     depends_on('go')
