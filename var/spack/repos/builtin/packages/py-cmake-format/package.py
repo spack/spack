25a1e94cd4856a3644d7142dcb48556a28374dad
Matthias Wolf <matthias.wolf@epfl.ch>
2019-06-25 15:42:15 +0200

    cmake-format: fix dependencies. (#417)

--- var/spack/repos/builtin/packages/py-cmake-format/package.py
+++ var/spack/repos/builtin/packages/py-cmake-format/package.py
@@ -14,4 +14,5 @@ class PyCmakeFormat(PythonPackage):
 
     version('0.4.5', sha256='16602408c774cd989ecfa25883de4c2dbac937e3890b735be4aab76f9647875a')
 
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('py-pyyaml', type='run')
