924e14b56792b6769c6c0edfcc00177e920a083f
Carson Woods <cwoods289@gmail.com>
2019-06-25 15:05:43 -0600

    Fix flake8 formatting

--- var/spack/repos/builtin/packages/powerapi/package.py
+++ var/spack/repos/builtin/packages/powerapi/package.py
@@ -2,24 +2,6 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install pwrapi-ref-git
-#
-# You can edit this file again by typing:
-#
-#     spack edit pwrapi-ref-git
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -30,8 +12,6 @@ class Powerapi(AutotoolsPackage):
     git      = "https://github.com/pwrapi/pwrapi-ref/tree/v1.1.1.git"
 
     version('1.1.1', commit='93f66dfa29f014067823f2b790a1862e5841a11c', expand=False)
-        
-
 
     variant('hwloc', default=False, description='Build hwloc support')
     variant('debug', default=False, description='Enable debug support')
@@ -47,7 +27,6 @@ class Powerapi(AutotoolsPackage):
     depends_on('hwloc', when='+hwloc')
     depends_on('mpi', when='+mpi')
 
-
     def autoreconf(self, spec, prefix):
         bash = which('bash')
         bash('./autogen.sh')
@@ -62,10 +41,12 @@ class Powerapi(AutotoolsPackage):
             config_args.append('--with-gnu-ld')
 
         if '+hwloc' in self.spec:
-            config_args.append('--with-hwloc={0}'.format(self.spec['hwloc'].prefix))
+            config_args.append('--with-hwloc={0}'
+                               .format(self.spec['hwloc'].prefix))
 
         if '+mpi' in self.spec:
-            config_args.append('--with-mpi={0}'.format(self.spec['mpi'].prefix))
+            config_args.append('--with-mpi={0}'
+                               .format(self.spec['mpi'].prefix))
 
         if '+debug' in self.spec:
             config_args.append('--enable-debug')
