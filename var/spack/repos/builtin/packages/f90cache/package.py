070158ba6685f37db7d7888def7f4ae505be6176
Hironori-Yamaji <52182908+Hironori-Yamaji@users.noreply.github.com>
2019-06-28 01:55:48 +0900

    f90cache: change the download url (#11838)
    * f90cache: change the download url
    
    * f90cache: added the download url for ver 0.99

--- var/spack/repos/builtin/packages/f90cache/package.py
+++ var/spack/repos/builtin/packages/f90cache/package.py
@@ -14,6 +14,8 @@ class F90cache(AutotoolsPackage):
        great speedup in common compilations.
     """
     homepage = "https://perso.univ-rennes1.fr/edouard.canot/f90cache/"
-    url      = "https://perso.univ-rennes1.fr/edouard.canot/f90cache/f90cache-0.99.tar.bz2"
+    url      = "https://perso.univ-rennes1.fr/edouard.canot/f90cache/f90cache-0.99c.tar.gz"
 
-    version('0.99', 'e4767ae876203ed4f3e118e22204cdec')
+    version('0.99c', sha256='13f8297ecba73671d43376b71ef0e453bd9d6677a901d1c95f01f16cc33776e1')
+    version('0.99', 'e4767ae876203ed4f3e118e22204cdec',
+            url='http://distfiles.exherbo.org/distfiles/f90cache-0.99.tar.bz2')
