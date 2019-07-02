332f7ec9628bb9c8ab10f27860167f1027df6048
Milton Woods <milton.woods@bom.gov.au>
2019-07-02 16:06:00 +1000

    py-pyke: expand the package docstring

--- var/spack/repos/builtin/packages/py-pyke/package.py
+++ var/spack/repos/builtin/packages/py-pyke/package.py
@@ -7,7 +7,10 @@ from spack import *
 
 
 class PyPyke(PythonPackage):
-    """Python Knowledge Engine and Automatic Python Program Generator."""
+    """Pyke introduces a form of Logic Programming (inspired by Prolog)
+    to the Python community by providing a knowledge-based inference
+    engine (expert system) written in 100% Python.
+    """
 
     homepage = "http://sourceforge.net/projects/pyke"
     url      = "https://downloads.sourceforge.net/pyke/pyke3-1.1.1.zip"
