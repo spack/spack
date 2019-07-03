41bd72697ec2a1d09628865886746620b541ebb0
Milton Woods <milton.woods@bom.gov.au>
2019-07-03 10:43:56 +1000

    py-cf-units: remove extension keyword from version

--- var/spack/repos/builtin/packages/py-cf-units/package.py
+++ var/spack/repos/builtin/packages/py-cf-units/package.py
@@ -16,7 +16,7 @@ class PyCfUnits(PythonPackage):
     git      = "https://github.com/SciTools/cf-units.git"
 
     version('master', branch='master')
-    version('2.1.1', extension='tar.gz', sha256='fa0ef8efd84546e61088aa23e76ebbaf7043167dc3a7f35f34549c234b543530')
+    version('2.1.1', sha256='fa0ef8efd84546e61088aa23e76ebbaf7043167dc3a7f35f34549c234b543530')
 
     depends_on('python@3:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
