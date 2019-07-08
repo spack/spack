242bdef6e8dcea1dd99d18a90a4ad008a9a75772
Justin S <3630356+codeandkey@users.noreply.github.com>
2019-07-08 13:59:54 -0500

    r-biocgenerics: add 0.30.0 (#11935)

--- var/spack/repos/builtin/packages/r-biocgenerics/package.py
+++ var/spack/repos/builtin/packages/r-biocgenerics/package.py
@@ -12,9 +12,11 @@ class RBiocgenerics(RPackage):
     homepage = "https://www.bioconductor.org/packages/BiocGenerics/"
     git      = "https://git.bioconductor.org/packages/BiocGenerics.git"
 
+    version('0.30.0', commit='fc7c3af4a5635a30988a062ed09332c13ca1d1a8')
     version('0.26.0', commit='5b2a6df639e48c3cd53789e0b174aec9dda6b67d')
     version('0.24.0', commit='3db111e8c1f876267da89f4f0c5406a9d5c31cd1')
     version('0.22.1', commit='9c90bb8926885289d596a81ff318ee3745cbb6ad')
 
     depends_on('r@3.4.0:3.4.9', when='@0.22.1', type=('build', 'run'))
     depends_on('r@3.5.0:3.5.9', when='@0.26.0', type=('build', 'run'))
+    depends_on('r@3.6.0:', when='@0.30.0', type=('build', 'run'))
