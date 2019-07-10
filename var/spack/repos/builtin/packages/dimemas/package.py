61492d6d9b296bf604ce863755c4a37ca66a302b
Phil Tooley <phil.tooley@protonmail.ch>
2019-07-10 17:18:38 +0100

    add dimemas recipe

---
+++ var/spack/repos/builtin/packages/dimemas/package.py
@@ -0,0 +1,34 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Dimemas(AutotoolsPackage):
+    """High-abstracted network simulator for message-passing programs."""
+
+    homepage = "https://tools.bsc.es/dimemas"
+    url      = "https://github.com/bsc-performance-tools/dimemas/archive/5.4.1.tar.gz"
+
+    version('5.4.1', sha256='10ddca3745a56ebab5c1ba180f6f4bce5832c4deac50c1b1dc08271db5c7cafa')
+
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool',  type='build')
+    depends_on('m4',       type='build')
+
+    depends_on('bison', type=('build', 'link', 'run'))
+    depends_on('flex', type=('build', 'link', 'run'))
+    depends_on('boost@1.65.0+program_options cxxstd=11', type=('build', 'link'))
+
+    def autoreconf(self, spec, prefix):
+        autoreconf('--install', '--verbose', '--force')
+
+    def configure_args(self):
+        args = ["--with-boost=%s" % self.spec['boost'].prefix,
+                "--with-boost-libdir=%s" % self.spec['boost'].prefix.lib,
+                "LEXLIB=-l:libfl.a"]
+
+        return args
