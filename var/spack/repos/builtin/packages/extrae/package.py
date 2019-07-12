6ca3f51e83f9c0cdac2fb55718685b9c38bfbc11
Phil Tooley <phil.tooley@nag.co.uk>
2019-07-12 14:18:57 +0100

    convert to autotools, tidy up

--- var/spack/repos/builtin/packages/extrae/package.py
+++ var/spack/repos/builtin/packages/extrae/package.py
@@ -24,7 +24,7 @@ from spack import *
 # LDFLAGS=-pthread
 
 
-class Extrae(Package):
+class Extrae(AutotoolsPackage):
     """Extrae is the package devoted to generate tracefiles which can
        be analyzed later by Paraver. Extrae is a tool that uses
        different interposition mechanisms to inject probes into the
@@ -35,9 +35,10 @@ class Extrae(Package):
        OpenMP, CUDA, OpenCL, pthread, OmpSs"""
     homepage = "https://tools.bsc.es/extrae"
     url      = "https://ftp.tools.bsc.es/extrae/extrae-3.4.1-src.tar.bz2"
-    version('3.7.1', sha256='95810b057f95e91bfc89813eb8bd320dfe40614fc8e98c63d95c5101c56dd213', preferred=True)
+    version('3.7.1', sha256='95810b057f95e91bfc89813eb8bd320dfe40614fc8e98c63d95c5101c56dd213')
     version('3.4.1', '69001f5cfac46e445d61eeb567bc8844')
 
+    depends_on("autoconf")
     depends_on("mpi")
     depends_on("libunwind")
     depends_on("boost")
@@ -45,55 +46,47 @@ class Extrae(Package):
     depends_on("papi")
     depends_on("elf", type="link")
     depends_on("libxml2")
-
-    variant('dyninst', default=False, description="Use dyninst for dynamic code installation")
-    depends_on('dyninst@:9.9.99', type=('build', 'link'), when='+dyninst')
-
-    variant('papi', default=True, description="Use PAPI to collect performance counters")
-    depends_on('papi', type=('build', 'link'), when='+papi')
-
+    depends_on("numactl")
+    depends_on("binutils+libiberty")
+    depends_on("gettext")
     # gettext dependency added to find -lintl
     # https://www.gnu.org/software/gettext/FAQ.html#integrating_undefined
-    depends_on("gettext")
-    depends_on("binutils+libiberty")
 
-    def install(self, spec, prefix):
-        if 'openmpi' in spec:
-            mpi = spec['openmpi']
-        elif 'mpich' in spec:
-            mpi = spec['mpich']
-        elif 'mvapich2' in spec:
-            mpi = spec['mvapich2']
+    build_directory = 'spack-build'
 
-        config_args = ["--prefix=%s" % prefix,
-                       "--with-mpi=%s" % mpi.prefix,
-                       "--with-unwind=%s" % spec['libunwind'].prefix,
-                       "--with-boost=%s" % spec['boost'].prefix,
-                       "--with-dwarf=%s" % spec['libdwarf'].prefix,
-                       "--with-elf=%s" % spec['elf'].prefix,
-                       "--with-xml-prefix=%s" % spec['libxml2'].prefix,
-                       "--with-binutils=%s" % spec['binutils'].prefix]
 
-        config_args += (["--with-papi=%s" % spec['papi'].prefix]
-                        if '+papi' in self.spec else
-                        ["--without-papi"])
+    variant('dyninst', default=False, description="Use dyninst for dynamic code installation")
+    depends_on('dyninst@:9', when='+dyninst')
 
-        config_args += (["--with-dyninst=%s" % spec['dyninst'].prefix,
-                         "--with-dyninst-headers=%s" %
-                         spec['dyninst'].prefix.include,
-                         "--with-dyninst-libs=%s" % spec['dyninst'].prefix.lib]
-                        if '+dyninst' in self.spec else
-                        ["--without-dyninst"])
+    variant('papi', default=True, description="Use PAPI to collect performance counters")
+    depends_on('papi', when='+papi')
+
+    def configure_args(self):
+        spec=self.spec
+        prefix=self.prefix
+
+        args = ["--with-mpi=%s" % spec['mpi'].prefix,
+                "--with-unwind=%s" % spec['libunwind'].prefix,
+                "--with-boost=%s" % spec['boost'].prefix,
+                "--with-dwarf=%s" % spec['libdwarf'].prefix,
+                "--with-elf=%s" % spec['elf'].prefix,
+                "--with-xml-prefix=%s" % spec['libxml2'].prefix,
+                "--with-binutils=%s" % spec['binutils'].prefix]
+
+        args += (["--with-papi=%s" % spec['papi'].prefix]
+                  if '+papi' in self.spec else
+                  ["--without-papi"])
+
+        args += (["--with-dyninst=%s" % spec['dyninst'].prefix]
+                 if '+dyninst' in self.spec else
+                  ["--without-dyninst"])
 
         if spec.satisfies("^dyninst@9.3.0:"):
             make.add_default_arg('CXXFLAGS=-std=c++11')
-            config_args.append('CXXFLAGS=-std=c++11')
+            args.append('CXXFLAGS=-std=c++11')
 
         # This was added due to configure failure
         # https://www.gnu.org/software/gettext/FAQ.html#integrating_undefined
-        config_args.append('LDFLAGS=-lintl')
+        args.append('LDFLAGS=-lintl')
 
-        configure(*config_args)
-
-        make()
-        make("install", parallel=False)
+        return(args)
