1123cd5a75629c773ba6444a503f0c68d29af099
Peter Josef Scheibel <scheibel1@llnl.gov>
2019-07-12 19:06:17 -0700

    hashes: consolidate and make hashing logic more consistent
    Spack has evolved to have three types of hash functions, and it's
    becoming hard to tell when each one is called. Whlie we aren't yet ready
    to get rid of them, we can refactor them so that the code is clearer and
    easier to track.
    
    - Add a `hash_types` module with concise descriptors for hashes.
    
    - Consolidate hashing logic in a private `Spec._spec_hash()` function.
    
    - `dag_hash()`, `build_hash()`, and `full_hash()` all call `_spec_hash()`
    
    - `to_node_dict()`, `to_dict()`, `to_yaml()` and `to_json()` now take a
      `hash` parameter consistent with the one that `_spec_hash()` requires.
    
    Co-authored-by: Todd Gamblin <tgamblin@llnl.gov>

---
+++ lib/spack/spack/hash_types.py
@@ -0,0 +1,36 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+"""Definitions that control how Spack creates Spec hashes."""
+
+import spack.dependency as dp
+
+
+class SpecHashDescriptor(object):
+    """This class defines how hashes are generated on Spec objects.
+
+    Spec hashes in Spack are generated from a serialized (e.g., with
+    YAML) representation of the Spec graph.  The representation may only
+    include certain dependency types, and it may optionally include a
+    canonicalized hash of the package.py for each node in the graph.
+
+    We currently use different hashes for different use cases.
+    """
+    def __init__(self, deptype=('link', 'run'), package_hash=False):
+        self.deptype = dp.canonical_deptype(deptype)
+        self.package_hash = package_hash
+
+
+#: Default Hash descriptor, used by Spec.dag_hash() and stored in the DB.
+dag_hash = SpecHashDescriptor(deptype=('link', 'run'), package_hash=False)
+
+
+#: Hash descriptor that includes build dependencies.
+build_hash = SpecHashDescriptor(
+    deptype=('build', 'link', 'run'), package_hash=False)
+
+
+#: Full hash used in build pipelines to determine when to rebuild packages.
+full_hash = SpecHashDescriptor(deptype=('link', 'run'), package_hash=True)
