50440a91c0fa84a41c3ba7e933a14d2d90ae6bfd
codeandkey <jtst@iastate.edu>
2019-07-15 14:06:10 -0500

    r-pfam-db: add 3.8.2

--- var/spack/repos/builtin/packages/r-pfam-db/package.py
+++ var/spack/repos/builtin/packages/r-pfam-db/package.py
@@ -13,7 +13,12 @@ class RPfamDb(RPackage):
     homepage = "https://www.bioconductor.org/packages/PFAM.db/"
     url      = "https://www.bioconductor.org/packages/3.5/data/annotation/src/contrib/PFAM.db_3.4.1.tar.gz"
 
+    version('3.8.2', sha256='3a951930f6caddfef4b61cb633b4c60c3c40ec268479c2bc1f3e5af67f56c166',
+            url='https://www.bioconductor.org/packages/3.9/data/annotation/src/contrib/PFAM.db_3.8.2.tar.gz')
     version('3.4.1', '65ed35887ecc44f5ac9f9c8563e03f44')
 
     depends_on('r@3.4.0:3.4.9', when='@3.4.1')
+    depends_on('r@3.6.0:3.6.0', when='@3.8.2')
+
     depends_on('r-annotationdbi', type=('build', 'run'))
+    depends_on('r-annotationdbi@1.43.1:', when='@3.8.2:', type=('build', 'run'))
