ef50b160a4dfc91dd792aac853b761ec78b7ab2f
Marc-Andre Hermanns <m.a.hermanns@fz-juelich.de>
2019-07-15 13:48:37 +0200

    Fixing flake8 errors

--- var/spack/repos/builtin/packages/jube/package.py
+++ var/spack/repos/builtin/packages/jube/package.py
@@ -26,7 +26,8 @@ class Jube(PythonPackage):
     version('2.0.0', sha256='ecfe8717bc61f35f333bc24d27b39e78e67c596e23512bdd97c9b4f28491f0b3', extension="tar.gz")
 
     variant(
-        'resource_manager', default='slurm', description='Select resource manager templates',
+        'resource_manager', default='slurm',
+        description='Select resource manager templates',
         values=('loadleveler', 'lsf', 'moab', 'pbs', 'slurm'), multi=False
     )
 
@@ -34,6 +35,7 @@ class Jube(PythonPackage):
 
     def setup_environment(self, spack_env, run_env):
         run_env.prepend_path(
-            'JUBE_INCLUDE_PATH', prefix + "/platform/" + self.spec.variants['resource_manager'].value
+            'JUBE_INCLUDE_PATH',
+            prefix + "/platform/" +
+            self.spec.variants['resource_manager'].value
         )
-
