64071c60688f83da4ffb2fa4f0005f1ae7c312a2
codeandkey <jtst@iastate.edu>
2019-07-15 14:39:49 -0500

    r-org-hs-eg-db: add 3.8.2

--- var/spack/repos/builtin/packages/r-org-hs-eg-db/package.py
+++ var/spack/repos/builtin/packages/r-org-hs-eg-db/package.py
@@ -13,7 +13,13 @@ class ROrgHsEgDb(RPackage):
     homepage = "https://bioconductor.org/packages/org.Hs.eg.db/"
     url      = "https://www.bioconductor.org/packages/3.5/data/annotation/src/contrib/org.Hs.eg.db_3.4.1.tar.gz"
 
+    version('3.8.2', sha256='a0a16b7428f9e3d6ba54ebf4e05cd97a7bd298510ec4cf46ed2bed3e8f80db02',
+            url='https://www.bioconductor.org/packages/3.9/data/annotation/src/contrib/org.Hs.eg.db_3.8.2.tar.gz')
+
     version('3.4.1', '0a987ef7d6167df70e91e6f48145e41c')
 
+    depends_on('r@3.6.0:3.6.9', when='@3.8.2')
     depends_on('r@3.4.0:3.4.9', when='@3.4.1')
+
     depends_on('r-annotationdbi', type=('build', 'run'))
+    depends_on('r-annotationdbi@1.43.1:', when='@3.8.2', type=('build', 'run'))
