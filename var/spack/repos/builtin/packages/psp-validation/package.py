279560551461516f1ec73461fdda96c0d2428636
MikeG <michael.gevaert@epfl.ch>
2019-07-15 09:10:46 +0200

    update bglibpy dependency for psp-validation (#436)

--- var/spack/repos/builtin/packages/psp-validation/package.py
+++ var/spack/repos/builtin/packages/psp-validation/package.py
@@ -24,5 +24,5 @@ class PspValidation(PythonPackage):
     depends_on('py-numpy@1.10:', type='run')
     depends_on('py-tqdm@4.0:', type='run')
 
-    depends_on('py-bglibpy@4.0.13:4.999', type='run')
+    depends_on('py-bglibpy@4.0.17', type='run')
     depends_on('py-bluepy@0.13.3:', type='run')
