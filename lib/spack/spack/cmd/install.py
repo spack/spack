27e1140df7e42df4845bbb984bffda5fd32948b8
Carson Woods <cwoods289@gmail.com>
2019-07-17 15:01:58 -0600

    Reset active directory after each global install

--- lib/spack/spack/cmd/install.py
+++ lib/spack/spack/cmd/install.py
@@ -200,6 +200,8 @@ def install_spec(cli_args, kwargs, abstract_spec, spec):
             env.write()
         else:
             spec.package.do_install(**kwargs)
+            spack.config.set('config:active_tree', '~/.spack/opt/spack',
+                             scope='user')
 
     try:
         if cli_args.things_to_install == 'dependencies':
