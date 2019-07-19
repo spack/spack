eb9ac74129e7d83eece1cd2991fada19109ef068
Matt Larsen <mclarsen@users.noreply.github.com>
2019-07-19 13:36:15 -0700

    Update package.py

--- var/spack/repos/builtin/packages/ascent/package.py
+++ var/spack/repos/builtin/packages/ascent/package.py
@@ -72,7 +72,7 @@ class Ascent(Package):
     # package dependencies
     ###########################################################################
 
-    depends_on("cmake@3.9.2:3.9.999", type='build')
+    depends_on("cmake@3.14.5", type='build')
     depends_on("conduit~python", when="~python")
     depends_on("conduit+python", when="+python+shared")
     depends_on("conduit~shared~python", when="~shared")
