0fb26ba9f7dcb468118c605b384fcb5437d821a4
naromero77 <naromero@anl.gov>
2019-07-23 14:36:09 -0500

    Add support for QE 6.4.1

--- var/spack/repos/builtin/packages/quantum-espresso/package.py
+++ var/spack/repos/builtin/packages/quantum-espresso/package.py
@@ -17,9 +17,10 @@ class QuantumEspresso(Package):
     """
 
     homepage = 'http://quantum-espresso.org'
-    url = 'https://gitlab.com/QEF/q-e/-/archive/qe-6.3/q-e-qe-6.3.tar.gz'
+    url = 'https://gitlab.com/QEF/q-e/-/archive/qe-6.4.1/q-e-qe-6.4.1.tar.gz'
     git = 'https://gitlab.com/QEF/q-e.git'
 
+    version('6.4.1', sha256='b0d7e9f617b848753ad923d8c6ca5490d5d82495f82b032b71a0ff2f2e9cfa08')
     version('6.4', sha256='781366d03da75516fdcf9100a1caadb26ccdd1dedd942a6f8595ff0edca74bfe')
     version('6.3',   '1b67687d90d1d16781d566d44d14634c')
     version('6.2.1', '769cc973382156bffd35254c3dbaf453')
