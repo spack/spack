61930da5c610a69af30f8c1fdb8d10b01d257bad
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-07-29 23:36:33 +0200

    zeromq package: choose URL based on version; add versions (#12149)
    * Add versions 4.3.0, 4.3.1, and 4.3.2
    * The URL format changed after 4.1.4, so this adds a url_for_version
      function to choose the URL format based on the version

--- var/spack/repos/builtin/packages/zeromq/package.py
+++ var/spack/repos/builtin/packages/zeromq/package.py
@@ -10,12 +10,14 @@ class Zeromq(AutotoolsPackage):
     """The ZMQ networking/concurrency library and core API"""
 
     homepage = "http://zguide.zeromq.org/"
-    url      = "http://download.zeromq.org/zeromq-4.1.2.tar.gz"
+    url      = "https://github.com/zeromq/libzmq/releases/download/v4.3.2/zeromq-4.3.2.tar.gz"
     git      = "https://github.com/zeromq/libzmq.git"
 
     version('develop', branch='master')
-    version('4.2.5', 'a1c95b34384257e986842f4d006957b8',
-            url='https://github.com/zeromq/libzmq/releases/download/v4.2.5/zeromq-4.2.5.tar.gz')
+    version('4.3.2', sha256='ebd7b5c830d6428956b67a0454a7f8cbed1de74b3b01e5c33c5378e22740f763')
+    version('4.3.1', sha256='bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb')
+    version('4.3.0', sha256='8e9c3af6dc5a8540b356697081303be392ade3f014615028b3c896d0148397fd')
+    version('4.2.5', 'a1c95b34384257e986842f4d006957b8')
     version('4.2.2', '52499909b29604c1e47a86f1cb6a9115')
     version('4.1.4', 'a611ecc93fffeb6d058c0e6edf4ad4fb')
     version('4.1.2', '159c0c56a895472f02668e692d122685')
@@ -24,7 +26,8 @@ class Zeromq(AutotoolsPackage):
     version('4.0.6', 'd47dd09ed7ae6e7fd6f9a816d7f5fdf6')
     version('4.0.5', '73c39f5eb01b9d7eaf74a5d899f1d03d')
 
-    variant("libsodium", default=True, description="Build with libsodium support")
+    variant("libsodium", default=True,
+            description="Build with message encryption support via libsodium")
 
     depends_on("libsodium", when='+libsodium')
     depends_on("libsodium@:1.0.3", when='+libsodium@:4.1.2')
@@ -35,6 +38,13 @@ class Zeromq(AutotoolsPackage):
     depends_on('pkgconfig', type='build')
 
     conflicts('%gcc@8:', when='@:4.2.2')
+
+    def url_for_version(self, version):
+        if version <= Version('4.1.4'):
+            url = "http://download.zeromq.org/zeromq-{0}.tar.gz"
+        else:
+            url = "https://github.com/zeromq/libzmq/releases/download/v{0}/zeromq-{0}.tar.gz"
+        return url.format(version)
 
     @when('@develop')
     def autoreconf(self, spec, prefix):
