0ea73206c8d6fed14d48ec0f1e4f888350cafdf5
Omar Padron <omar.padron@kitware.com>
2019-07-30 10:59:37 -0400

    libglvnd: fix style/remove unecessary variants

--- var/spack/repos/builtin/packages/libglvnd/package.py
+++ var/spack/repos/builtin/packages/libglvnd/package.py
@@ -18,20 +18,13 @@ class Libglvnd(AutotoolsPackage):
     ES."""
 
     homepage = "https://github.com/NVIDIA/libglvnd"
-    url      = ("https://github.com/NVIDIA/libglvnd/releases/download"
-                "/v1.1.1/libglvnd-1.1.1.tar.gz")
+    url      = "https://github.com/NVIDIA/libglvnd/releases/download/v1.1.1/libglvnd-1.1.1.tar.gz"
     git      = "https://github.com/NVIDIA/libglvnd.git"
 
     version('master', branch='master')
 
-    version('1.1.1', sha256='71918ed1261e4eece18c0b74b50dc62c'
-                            '0237b8d526e83277ef078554544720b9')
-    version('1.1.0', sha256='f5a74598e769d55d652c464cb6507437'
-                            'dac5c2d513f16c6ddf3a1bec655a1824')
-
-    variant('egl', default=True, description="Enable OpenGL EGL support.")
-    variant('glx', default=True, description="Enable GLX support.")
-    variant('gles', default=True, description="Enable OpenGL ES support.")
+    version('1.1.1', sha256='71918ed1261e4eece18c0b74b50dc62c0237b8d526e83277ef078554544720b9')
+    version('1.1.0', sha256='f5a74598e769d55d652c464cb6507437dac5c2d513f16c6ddf3a1bec655a1824')
 
     depends_on('libxext')
     depends_on('libx11')
@@ -43,19 +36,4 @@ class Libglvnd(AutotoolsPackage):
         autogen()
 
     def configure_args(self):
-        args = []
-        if '+egl' in self.spec:
-            args.append('--enable-egl')
-        if '+glx' in self.spec:
-            args.append('--enable-glx')
-        if '+gles' in self.spec:
-            args.append('--enable-gles')
-
-        if '~egl' in self.spec:
-            args.append('--disable-egl')
-        if '~glx' in self.spec:
-            args.append('--disable-glx')
-        if '~gles' in self.spec:
-            args.append('--disable-gles')
-
-        return args
+        return []
