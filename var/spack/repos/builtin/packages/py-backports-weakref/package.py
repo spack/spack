4ac8b4d34231366fa37d8cc824552398eae1a314
Sinan81 <Sinan81@github>
2019-08-03 12:22:52 -0700

    use the version at pypi.org

--- var/spack/repos/builtin/packages/py-backports-weakref/package.py
+++ var/spack/repos/builtin/packages/py-backports-weakref/package.py
@@ -10,11 +10,8 @@ class PyBackportsWeakref(PythonPackage):
     """Backports of new features in Python's weakref module"""
 
     homepage = "https://github.com/PiDelport/backports.weakref"
-    # Can't use pypi link as it doesn't have source files for the versions
-    # indicated below.
-    url      = "https://github.com/PiDelport/backports.weakref/archive/v1.0.tar.gz"
+    url      = "https://pypi.org/packages/source/b/backports.weakref/backports.weakref-1.0.post1.tar.gz"
 
-    version('1.0rc1', sha256='7826e249a0ccb382f1200e5f03c70a624537d4619f050e1c024ff18315550eed')
-    version('1.0',    sha256='7e7457e61879db43b12b20d05acc3ef5bc6748adf37c6a4d9514316ba990a141')
+    version('1.0.post1', sha256='bc4170a29915f8b22c9e7c4939701859650f2eb84184aee80da329ac0b9825c2')
 
     depends_on('py-setuptools', type='build')
