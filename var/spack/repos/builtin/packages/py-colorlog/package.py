cac2b037a6dd8f1de870e98429fa37f171d7c167
Sinan <sbulutw@gmail.com>
2019-08-03 12:06:11 -0700

    improve styling.

--- var/spack/repos/builtin/packages/py-colorlog/package.py
+++ var/spack/repos/builtin/packages/py-colorlog/package.py
@@ -11,8 +11,8 @@ class PyColorlog(PythonPackage):
 
     homepage = "https://github.com/borntyping/python-colorlog"
     url      = "https://pypi.org/packages/source/c/colorlog/colorlog-4.0.2.tar.gz"
-
-    depends_on('py-setuptools', type='build')
-
+  
     version('4.0.2', sha256='3cf31b25cbc8f86ec01fef582ef3b840950dea414084ed19ab922c8b493f9b42')
     version('3.1.4', sha256='418db638c9577f37f0fae4914074f395847a728158a011be2a193ac491b9779d')
+
+    depends_on('py-setuptools', type='build')
