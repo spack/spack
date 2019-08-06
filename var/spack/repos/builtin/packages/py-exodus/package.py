9b1d1149f7bb93dd382e9ad2f7d3d923733d4312
Daryl W. Grunau <dwg@lanl.gov>
2019-08-06 15:26:04 -0600

    address becker33's comments

--- var/spack/repos/builtin/packages/py-exodus/package.py
+++ var/spack/repos/builtin/packages/py-exodus/package.py
@@ -15,8 +15,6 @@ class PyExodus(PythonPackage):
 
     version('2.0.2', sha256='4e896a2034b94cf7b4fb33d86a68e29a7d3b08e57541e444db34dddc6ac1ef68')
 
-    extends('python')
-
     depends_on('musl', type='run', when='%clang')
     depends_on('musl', type='run', when='%gcc')
     depends_on('py-setuptools', type=('build', 'run'))
