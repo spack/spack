c4eff576534bb118a7295bdf08d6984477dd1426
Sinan81 <Sinan81@github>
2019-08-05 17:19:39 -0700

    now python bindings compile successfully

--- var/spack/repos/builtin/packages/qscintilla/package.py
+++ var/spack/repos/builtin/packages/qscintilla/package.py
@@ -16,23 +16,33 @@ class Qscintilla(QMakePackage):
     # didn't have much luck with newer versions of Qscintilla and QT@4.8.6, so prefer the following version
     version('2.10.2', sha256='14b31d20717eed95ea9bea4cd16e5e1b72cee7ebac647cba878e0f6db6a65ed0', preferred=True)
 
-    ## TODO build python pindings
     # TODO add qt-designer as variant
-    depends_on('qt~phonon~dbus') #phonon and dbus are causing compilation problems
-    depends_on('py-pyqt4', type='build') #when='qt@4' 
-    depends_on('py-pyqt5', type='build', when='qt@5') #when='qt@4' 
+    # TODO add python bindings as variant
+
+    # QScintilla so far tested to compile with Qt@4.8.6
+    depends_on('qt') #+phonon +dbus variants are causing compilation problems
+
+    # Beyond py-pyqt@4.12.1, pyqt4 needs its own sip module (not implemented yet)
+    # Without private sip moduele, python bindings will not compile
+    # Ref: https://www.riverbankcomputing.com/static/Docs/PyQt4/installation.html   
+    # TODO implement private sip module for py-pyqt4
+    depends_on('py-pyqt4@:4.12.1', type='build', when='@qt4') 
+    depends_on('py-pyqt5', type='build', when='qt@5')
     depends_on('python', type=('build', 'run'))
 
-    conflicts('qt@4', when='@2.10.3:') # with qt@4.8.6, didn't have much luck with newer versions
+    # with qt@4.8.6, didn't have much luck in compiling newer versions
+    conflicts('qt@4', when='@2.10.3:') 
 
     def chdir(self):
         os.chdir(str(self.stage.source_path)+'/Qt4Qt5')
+
 
     def qmake_args(self):
         # below, DEFINES ... gets rid of ...regex...errors during build
         # although, there shouldn't be such errors since we use '-std=c++11'
         args = ['CONFIG+=-std=c++11', 'DEFINES+=NO_CXX11_REGEX=1']
         return args
+
 
     # the following installs files under "/qscintilla_prefix/qmake_prefix"
     # it gets rid of 'Nothing installed error'
@@ -41,7 +51,6 @@ class Qscintilla(QMakePackage):
         spack_env.set('INSTALL_ROOT', prefix)
 
 
-    #python bindings aren't compiling yet: can't find pyqt4.core etc
     def make_python_bindings(self):
         os.chdir(str(self.stage.source_path)+'/Python')
         python = which('python')
@@ -50,7 +59,15 @@ class Qscintilla(QMakePackage):
         else:
             python('configure.py -pyqt=PyQt5')
 
+    def make_designer_qt(self):
+        pass # not implemented yet
+
+
+    def make_variants(self):
+        make_python_bindings(self)
+        make_designer_qt(self)
+
 
     run_before('qmake')(chdir)
 
-    run_after('install')(make_python_bindings)
+    run_after('install')(make_variants)
