7989d545e83ced706c8e225dacfdfe96805cf302
Fernando Pereira <ferdonline@gmail.com>
2019-08-07 13:37:43 +0200

    Adding spatial index (#468)
    * Adding spatial index
    * Using patched tag. Add to deployment

---
+++ var/spack/repos/builtin/packages/spatial-index/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack import *
+
+
+class SpatialIndex(PythonPackage):
+    """Spatial indexer for geometries and morphologies"""
+
+    homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/hpc/SpatialIndex"
+    git      = "ssh://bbpcode.epfl.ch/hpc/SpatialIndex"
+    url      = "ssh://bbpcode.epfl.ch/hpc/SpatialIndex"
+
+    depends_on("py-setuptools")
+    depends_on("cmake")
+    depends_on("boost")
+
+    version('0.0.1', tag='0.0.1-2', submodules=True)
+
+
