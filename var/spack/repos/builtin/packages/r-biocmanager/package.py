d5c339caa4666e4ef0bc7569aa746bc2dc818cef
codeandkey <jtst@iastate.edu>
2019-08-09 16:43:26 -0500

    r-biocmanager: new package at 1.30.4

---
+++ var/spack/repos/builtin/packages/r-biocmanager/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RBiocmanager(RPackage):
+    """BiocManager: Access the Bioconductor Project Package Repository"""
+
+    homepage = "https://cran.r-project.org/package=BiocManager"
+    url      = "https://cran.r-project.org/src/contrib/BiocManager_1.30.4.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/BiocManager"
+
+    version('1.30.4', sha256='50093f5c8ed8fba6e68bc715784b713887bdad3538fbb92f152dcc1eaf39ba4f')
+
+    depends_on('r@3.5.0:', type=('build', 'run'))
