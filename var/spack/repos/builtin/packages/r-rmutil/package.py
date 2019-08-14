3c48cfcf470dadf9b9f372827337a8d6547b1839
codeandkey <jtst@iastate.edu>
2019-08-14 11:18:11 -0500

    r-rmutil: use cloud.r-project.org

--- var/spack/repos/builtin/packages/r-rmutil/package.py
+++ var/spack/repos/builtin/packages/r-rmutil/package.py
@@ -11,8 +11,8 @@ class RRmutil(RPackage):
        MeasurementsModels"""
 
     homepage = "http://www.commanster.eu/rcode.html"
-    url      = "https://cran.r-project.org/src/contrib/rmutil_1.1.3.tar.gz"
-    list_url = "https://cran.r-project.org/src/contrib/Archive/rmutil"
+    url      = "https://cloud.r-project.org/src/contrib/rmutil_1.1.3.tar.gz"
+    list_url = "https://cloud.r-project.org/src/contrib/Archive/rmutil"
 
     version('1.1.3', sha256='7abaf41e99d1c4a0e4082c4594964ac1421c53b4268116c82fa55aa8bc0582da')
 
