26d82479f00266528c30c7694fd3c8dc95a66e20
Axel Huebl <axel.huebl@plasma.ninja>
2019-08-14 18:02:49 -0700

    ADIOS2: Variant Defaults Change
    * Python: False by default
    * ADIOS1: only used prior to 2.3.0

--- var/spack/repos/builtin/packages/adios2/package.py
+++ var/spack/repos/builtin/packages/adios2/package.py
@@ -25,8 +25,8 @@ class Adios2(CMakePackage):
     variant('shared', default=True,
             description='Also build shared libraries')
     variant('pic', default=True,
-            description="Enable position independent code "
-                        "(for usage of static in shared downstream deps)")
+            description='Enable position independent code '
+                        '(for usage of static in shared downstream deps)')
     variant('mpi', default=True,
             description='Enable MPI')
 
@@ -52,9 +52,10 @@ class Adios2(CMakePackage):
     variant('hdf5', default=False,
             description='Enable the HDF5 engine')
     variant('adios1', default=False,
-            description='Enable the ADIOS 1.x engine')
+            description='Enable the ADIOS 1.x engine '
+                        '(in 2.3.0+ integrated in BPFile engine)')
     # language bindings
-    variant('python', default=True,
+    variant('python', default=False,
             description='Enable the Python >= 2.7 bindings')
     variant('fortran', default=True,
             description='Enable the Fortran bindings')
@@ -70,6 +71,9 @@ class Adios2(CMakePackage):
     # DataMan needs dlopen
     conflicts('+dataman', when='~shared')
 
+    # BPFile engine was emulated via a ADIOS1 engine prior to v2.3.0
+    conflicts('+adios1', when='@2.3.0:')
+
     depends_on('cmake@3.6.0:', type='build')
     depends_on('pkgconfig', type='build', when='@2.2.0:')
     # The included ffs requires bison and flex but using them makes
@@ -82,8 +86,8 @@ class Adios2(CMakePackage):
 
     depends_on('hdf5', when='+hdf5')
     depends_on('hdf5+mpi', when='+hdf5+mpi')
-    depends_on('adios', when='+adios1')
-    depends_on('adios+mpi', when='+adios1+mpi')
+    depends_on('adios', when='@:2.2.99 +adios1')
+    depends_on('adios+mpi', when='@:2.2.99 +adios1+mpi')
 
     depends_on('c-blosc', when='@2.4.0: +blosc')
     depends_on('bzip2', when='+bzip2')
@@ -123,13 +127,16 @@ class Adios2(CMakePackage):
                 'ON' if '+dataman' in spec else 'OFF'),
             '-DADIOS2_USE_HDF5={0}'.format(
                 'ON' if '+hdf5' in spec else 'OFF'),
-            '-DADIOS2_USE_ADIOS1={0}'.format(
-                'ON' if '+adios1' in spec else 'OFF'),
             '-DADIOS2_USE_Python={0}'.format(
                 'ON' if '+python' in spec else 'OFF'),
             '-DADIOS2_USE_Fortran={0}'.format(
                 'ON' if '+fortran' in spec else 'OFF')
         ]
+
+        # option removed and integrated in internal BPFile engine
+        if self.spec.version < Version('2.3.0'):
+            args.append('-DADIOS2_USE_ADIOS1={0}'.format(
+                'ON' if '+adios1' in spec else 'OFF'))
 
         if self.spec.version >= Version('2.4.0'):
             args.append('-DADIOS2_USE_Blosc={0}'.format(
