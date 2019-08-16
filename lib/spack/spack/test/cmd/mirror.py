0307b5a3dc57338456fd558f5603a28b7d90f1e6
Greg Becker <becker33@llnl.gov>
2019-08-15 18:45:17 -0700

    mirrors: make `spack mirror` environment aware (#12345)
    Allow "spack mirror" with no spec arguments to create a mirror for the
    active environment (download all concretized specs in the environment)

--- lib/spack/spack/test/cmd/mirror.py
+++ lib/spack/spack/test/cmd/mirror.py
@@ -4,10 +4,16 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import pytest
+import os
 
 from spack.main import SpackCommand
+import spack.environment as ev
+import spack.config
 
 mirror = SpackCommand('mirror')
+env = SpackCommand('env')
+add = SpackCommand('add')
+concretize = SpackCommand('concretize')
 
 
 @pytest.mark.disable_clean_stage_check
@@ -17,3 +23,25 @@ def test_regression_8083(tmpdir, capfd, mock_packages, mock_fetch, config):
         output = mirror('create', '-d', str(tmpdir), 'externaltool')
     assert 'Skipping' in output
     assert 'as it is an external spec' in output
+
+
+@pytest.mark.regression('12345')
+def test_mirror_from_env(tmpdir, mock_packages, mock_fetch, config,
+                         mutable_mock_env_path):
+    mirror_dir = str(tmpdir)
+    env_name = 'test'
+
+    env('create', env_name)
+    with ev.read(env_name):
+        add('trivial-install-test-package')
+        add('git-test')
+        concretize()
+        with spack.config.override('config:checksum', False):
+            mirror('create', '-d', mirror_dir)
+
+    e = ev.read(env_name)
+    assert set(os.listdir(mirror_dir)) == set([s.name for s in e.user_specs])
+    for spec in e.specs_by_hash.values():
+        mirror_res = os.listdir(os.path.join(mirror_dir, spec.name))
+        expected = ['%s.tar.gz' % spec.format('{name}-{version}')]
+        assert mirror_res == expected
