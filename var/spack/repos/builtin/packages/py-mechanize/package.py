16a33c6d91ea604146e79f51f8bd2a0cb5277d30
Sinan81 <Sinan81@github>
2019-08-18 16:02:20 -0700

    add version for dependency

--- var/spack/repos/builtin/packages/py-mechanize/package.py
+++ var/spack/repos/builtin/packages/py-mechanize/package.py
@@ -15,5 +15,5 @@ class PyMechanize(PythonPackage):
     version('0.4.3', sha256='d7d7068be5e1b3069575c98c870aaa96dd26603fe8c8697b470e2f65259fddbf')
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-html5lib', type=('build', 'run'))
+    depends_on('py-html5lib@0.9999999999:', type=('build', 'run'))
     depends_on('python@2.7:', type=('build', 'run'))
