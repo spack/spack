080155366efaa3ddc5a7f77303a3d70720dca899
kjappelbaum <kevin.jablonka@epfl.ch>
2019-08-18 14:13:15 +0200

    Version

--- var/spack/repos/builtin/packages/libint/package.py
+++ var/spack/repos/builtin/packages/libint/package.py
@@ -14,13 +14,27 @@ class Libint(AutotoolsPackage):
     homepage = "https://github.com/evaleev/libint"
     url = "https://github.com/evaleev/libint/archive/v2.1.0.tar.gz"
 
-    version('2.4.2', sha256='86dff38065e69a3a51d15cfdc638f766044cb87e5c6682d960c14f9847e2eac3')
-    version('2.4.1', sha256='0513be124563fdbbc7cd3c7043e221df1bda236a037027ba9343429a27db8ce4')
-    version('2.4.0', sha256='52eb16f065406099dcfaceb12f9a7f7e329c9cfcf6ed9bfacb0cff7431dd6019')
-    version('2.2.0', sha256='f737d485f33ac819d7f28c6ce303b1f3a2296bfd2c14f7c1323f8c5d370bb0e3')
-    version('2.1.0', sha256='43c453a1663aa1c55294df89ff9ece3aefc8d1bbba5ea31dbfe71b2d812e24c8')
-    version('1.1.6', sha256='f201b0c621df678cfe8bdf3990796b8976ff194aba357ae398f2f29b0e2985a6')
-    version('1.1.5', sha256='ec8cd4a4ba1e1a98230165210c293632372f0e573acd878ed62e5ec6f8b6174b')
+    version('2.4.2',
+            sha256=
+            '86dff38065e69a3a51d15cfdc638f766044cb87e5c6682d960c14f9847e2eac3')
+    version('2.4.1',
+            sha256=
+            '0513be124563fdbbc7cd3c7043e221df1bda236a037027ba9343429a27db8ce4')
+    version('2.4.0',
+            sha256=
+            '52eb16f065406099dcfaceb12f9a7f7e329c9cfcf6ed9bfacb0cff7431dd6019')
+    version('2.2.0',
+            sha256=
+            'f737d485f33ac819d7f28c6ce303b1f3a2296bfd2c14f7c1323f8c5d370bb0e3')
+    version('2.1.0',
+            sha256=
+            '43c453a1663aa1c55294df89ff9ece3aefc8d1bbba5ea31dbfe71b2d812e24c8')
+    version('1.1.6',
+            sha256=
+            'f201b0c621df678cfe8bdf3990796b8976ff194aba357ae398f2f29b0e2985a6')
+    version('1.1.5',
+            sha256=
+            'ec8cd4a4ba1e1a98230165210c293632372f0e573acd878ed62e5ec6f8b6174b')
 
     # Build dependencies
     depends_on('autoconf@2.52:', type='build')
@@ -72,7 +86,7 @@ class Libint(AutotoolsPackage):
         optflags = self.optflags
 
         # Optimization flag names have changed in libint 2
-        if self.version < Version('2.0.0'):
+        if self.version < version('2.0.0'):
             config_args.extend([
                 '--with-cc-optflags={0}'.format(optflags),
                 '--with-cxx-optflags={0}'.format(optflags)
@@ -84,9 +98,7 @@ class Libint(AutotoolsPackage):
             ])
 
         # Options required by CP2K, removed in libint 2
-        if self.version < Version('2.0.0'):
-            config_args.extend([
-                '--with-libint-max-am=5',
-                '--with-libderiv-max-am1=4'
-            ])
+        if self.version < version('2.0.0'):
+            config_args.extend(
+                ['--with-libint-max-am=5', '--with-libderiv-max-am1=4'])
         return config_args
