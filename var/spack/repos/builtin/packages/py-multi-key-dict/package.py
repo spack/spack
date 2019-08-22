9a19937508e9c3dfe31872f7d0318277b526b4ad
Sinan81 <Sinan81@github>
2019-08-22 02:10:34 -0700

    get rid of comments

--- var/spack/repos/builtin/packages/py-multi-key-dict/package.py
+++ var/spack/repos/builtin/packages/py-multi-key-dict/package.py
@@ -13,7 +13,3 @@ class PyMultiKeyDict(PythonPackage):
     url      = "https://pypi.io/packages/source/m/multi_key_dict/multi_key_dict-2.0.3.tar.gz"
 
     version('2.0.3', sha256='deebdec17aa30a1c432cb3f437e81f8621e1c0542a0c0617a74f71e232e9939e')
-
-    # FIXME: Add dependencies if required.
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
