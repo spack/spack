fc40f4fdc0eeaa62670917cd5216b5f8391ec3aa
Sinan81 <Sinan81@github>
2019-08-22 21:46:33 -0700

    add python dependency version info

--- var/spack/repos/builtin/packages/py-simplekml/package.py
+++ var/spack/repos/builtin/packages/py-simplekml/package.py
@@ -15,3 +15,5 @@ class PySimplekml(PythonPackage):
     url      = "https://pypi.io/packages/source/s/simplekml/simplekml-1.3.1.tar.gz"
 
     version('1.3.1', sha256='30c121368ce1d73405721730bf766721e580cae6fbb7424884c734c89ec62ad7')
+
+    depends_on('python@2.6:')
