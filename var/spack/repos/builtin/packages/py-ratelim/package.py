d0cd76ace5e18245f98b4de3dcb1d0193442b92a
Sinan81 <Sinan81@github>
2019-08-23 11:48:22 -0700

    new package: py-ratelim

---
+++ var/spack/repos/builtin/packages/py-ratelim/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyRatelim(PythonPackage):
+    """Makes it easy to respect rate limits."""
+
+    homepage = "https://github.com/themiurgo/ratelim"
+    url      = "https://pypi.io/packages/source/r/ratelim/ratelim-0.1.6.tar.gz"
+
+    version('0.1.6', sha256='826d32177e11f9a12831901c9fda6679fd5bbea3605910820167088f5acbb11d')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-decorator', type=('build', 'run'))
