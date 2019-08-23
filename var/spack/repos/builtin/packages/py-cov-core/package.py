8bee9e1c8830bdfd1f72d0e6e4f75fe8cd639272
Sinan81 <Sinan81@github>
2019-08-23 10:14:56 -0700

    add missing dependency

--- var/spack/repos/builtin/packages/py-cov-core/package.py
+++ var/spack/repos/builtin/packages/py-cov-core/package.py
@@ -14,4 +14,5 @@ class PyCovCore(PythonPackage):
 
     version('1.15.0', sha256='4a14c67d520fda9d42b0da6134638578caae1d374b9bb462d8de00587dba764c')
 
+    depends_on('py-setuptools', type='build')
     depends_on('py-coverage@3.6:', type=('build', 'run'))
