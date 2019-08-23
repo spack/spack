c9a3cfee58e6f32c04f44ddb4623b78c22fb47c0
Sinan81 <Sinan81@github>
2019-08-23 12:30:57 -0700

    new package: py-repoze-lru

---
+++ var/spack/repos/builtin/packages/py-repoze-lru/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyRepozeLru(PythonPackage):
+    """A tiny LRU cache implementation and decorator"""
+
+    homepage = "https://pypi.org/project/repoze.lru/"
+    url      = "https://pypi.io/packages/source/r/repoze.lru/repoze.lru-0.7.tar.gz"
+
+    version('0.7', sha256='0429a75e19380e4ed50c0694e26ac8819b4ea7851ee1fc7583c8572db80aff77')
+
+    variant('docs', default=False, description='Build docs')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
+    depends_on('py-sphinx', type='build', when='+docs')
