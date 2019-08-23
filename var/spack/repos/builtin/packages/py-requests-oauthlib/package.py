f141d91b98448550717c172685216363324f4f57
Sinan81 <Sinan81@github>
2019-08-23 10:37:52 -0700

    add a version

--- var/spack/repos/builtin/packages/py-requests-oauthlib/package.py
+++ var/spack/repos/builtin/packages/py-requests-oauthlib/package.py
@@ -14,6 +14,7 @@ class PyRequestsOauthlib(PythonPackage):
     url      = "https://pypi.io/packages/source/r/requests-oauthlib/requests-oauthlib-1.2.0.tar.gz"
 
     version('1.2.0', sha256='bd6533330e8748e94bf0b214775fed487d309b8b8fe823dc45641ebcd9a32f57')
+    version('0.3.3', sha256='37557b4de3eef50d2a4c65dc9382148b8331f04b1c637c414b3355feb0f007e9')
 
     depends_on('py-setuptools', type='build')
     depends_on('py-oauthlib@3.0.0', type=('build', 'run'))
