5fa60d9fa35a33a27741bc0981b2636ea1ab55d9
Sinan81 <Sinan81@github>
2019-08-23 11:31:39 -0700

    add a version

--- var/spack/repos/builtin/packages/py-pytools/package.py
+++ var/spack/repos/builtin/packages/py-pytools/package.py
@@ -13,6 +13,7 @@ class PyPytools(PythonPackage):
     url      = "https://pypi.io/packages/source/p/pytools/pytools-2019.1.1.tar.gz"
 
     version('2019.1.1', sha256='ce2d702ae4ef10a70197b00b93141461140d00578f2a862fa946ca1446a300db')
+    version('2016.2.6', sha256='6dd49932b8f81a8b622685cff3dd515e351a9290aef0fd5d020e4df00c06aa95')
 
     depends_on('py-setuptools', type='build')
     depends_on('py-decorator@3.2.0:', type=('build', 'run'))
