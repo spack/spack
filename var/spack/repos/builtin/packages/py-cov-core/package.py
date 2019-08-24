cdcc05db0ac8451bdb5bf627f9da40a7b962ed0d
Sinan <sbulutw@gmail.com>
2019-08-23 17:21:08 -0700

    New package: py-cov-core (#12526)

---
+++ var/spack/repos/builtin/packages/py-cov-core/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyCovCore(PythonPackage):
+    """plugin core for use by pytest-cov, nose-cov and nose2-cov"""
+
+    homepage = "https://github.com/schlamar/cov-core"
+    url      = "https://pypi.io/packages/source/c/cov-core/cov-core-1.15.0.tar.gz"
+
+    version('1.15.0', sha256='4a14c67d520fda9d42b0da6134638578caae1d374b9bb462d8de00587dba764c')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-coverage@3.6:', type=('build', 'run'))
