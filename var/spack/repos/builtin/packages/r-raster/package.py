66a59fb354cc4182731a94482e19e42558af250b
Glenn P Johnson <glenn-johnson@uiowa.edu>
2019-08-23 23:45:38 -0500

    Set R version constraint for r-raster

--- var/spack/repos/builtin/packages/r-raster/package.py
+++ var/spack/repos/builtin/packages/r-raster/package.py
@@ -19,5 +19,6 @@ class RRaster(RPackage):
     version('2.9-22', sha256='8107d95f1aa85cea801c8101c6aa391becfef4b5b915d9bc7a323531fee26128')
     version('2.5-8', '2a7db931c74d50516e82d04687c0a577')
 
+    depends_on('r@3.0.0:', type=('build', 'run'))
     depends_on('r-sp@1.2-0:', type=('build', 'run'))
     depends_on('r-rcpp', type=('build', 'run'))
