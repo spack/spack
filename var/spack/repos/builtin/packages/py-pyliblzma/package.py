565903ce9a1ebb0c885cf92adf330439594d402b
Sinan81 <Sinan81@github>
2019-08-23 21:24:40 -0700

    add dependency

--- var/spack/repos/builtin/packages/py-pyliblzma/package.py
+++ var/spack/repos/builtin/packages/py-pyliblzma/package.py
@@ -15,3 +15,4 @@ class PyPyliblzma(PythonPackage):
     version('0.5.3', sha256='08d762f36d5e59fb9bb0e22e000c300b21f97e35b713321ee504cfb442667957')
 
     depends_on('py-setuptools', type='build')
+    depends_on('lzma', type=('build', 'run'))
