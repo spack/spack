01783789a01546622e5534238ced3f6924fc7ff8
Geoffrey Oxberry <goxberry@gmail.com>
2019-08-24 09:44:20 -0700

    gobject-introspection: add GIR file search path (#12564)

--- var/spack/repos/builtin/packages/gobject-introspection/package.py
+++ var/spack/repos/builtin/packages/gobject-introspection/package.py
@@ -57,6 +57,10 @@ class GobjectIntrospection(Package):
         url = 'http://ftp.gnome.org/pub/gnome/sources/gobject-introspection/{0}/gobject-introspection-{1}.tar.xz'
         return url.format(version.up_to(2), version)
 
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        spack_env.prepend_path("XDG_DATA_DIRS", self.prefix.share)
+        run_env.prepend_path("XDG_DATA_DIRS", self.prefix.share)
+
     def install(self, spec, prefix):
         configure("--prefix=%s" % prefix)
         # we need to filter this file to avoid an overly long hashbang line
