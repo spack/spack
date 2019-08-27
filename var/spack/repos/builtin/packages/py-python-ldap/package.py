f4443b34fe0b998a6c038143a533ed1342d2e423
Sinan81 <sbulut@3vgeomatics.com>
2019-08-27 01:07:37 -0700

    fix dependency types

--- var/spack/repos/builtin/packages/py-python-ldap/package.py
+++ var/spack/repos/builtin/packages/py-python-ldap/package.py
@@ -17,6 +17,6 @@ class PyPythonLdap(PythonPackage):
     version('3.2.0', sha256='7d1c4b15375a533564aad3d3deade789221e450052b21ebb9720fb822eccdb8e')
 
     depends_on('py-setuptools', type='build')
-    depends_on('python@2.7:2.8,3.4:',        type=('build', 'run'))
-    depends_on('py-pyasn1',        type=('build', 'run'))
-    depends_on('py-pyasn1-modules',        type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
+    depends_on('py-pyasn1', type='run')
+    depends_on('py-pyasn1-modules', type='build')
