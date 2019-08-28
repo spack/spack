efc522c91c33e1f40cea9a5cb902cbf97d805a7a
Sinan81 <sbulut@3vgeomatics.com>
2019-08-27 20:41:37 -0700

    new package: liblas

---
+++ var/spack/repos/builtin/packages/liblas/package.py
@@ -0,0 +1,42 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Liblas(CMakePackage):
+    """libLAS is a C/C++ library for reading and writing the very common
+    LAS LiDAR format.
+    """
+
+    homepage = "https://liblas.org/"
+    url      = "http://download.osgeo.org/liblas/libLAS-1.8.1.tar.bz2"
+
+    version('1.8.1', sha256='9adb4a98c63b461ed2bc82e214ae522cbd809cff578f28511122efe6c7ea4e76')
+
+    # libLAS linkage of GDAL and libgeotiff enhances spatial coordinate system
+    # description and provides data reprojection support.
+    # Ref.: https://liblas.org/compilation.html#optional-libraries
+    variant('endian',  default=False, description='Build with "Endian-aware" option')
+    variant('gdal',    default=False, description='Build with GDAL for enhanced performance')
+    variant('geotiff', default=True,  description='Build with GeoTIFF for enhanced performance')
+    variant('laszip',  default=False, description='Build with LasZip')
+
+    depends_on('libgeotiff')
+    depends_on('boost@:1.69.0')
+    depends_on('laszip', when='+laszip')
+    depends_on('gdal', when='+gdal')
+
+    def cmake_args(self):
+        args = []
+        if '+endian' in self.spec:
+           args.append('-DWITH_ENDIANAWARE=ON')
+        if '+gdal' in self.spec:
+           args.append('-DWITH_GDAL=ON')
+        if '+geotiff' in self.spec:
+           args.append('-DWITH_GEOTIFF=ON')
+        if '+laszip' in self.spec:
+           args.append('-DWITH_LASZIP=ON')
+        return args
