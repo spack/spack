3aa589c0cac319cb8dd37100aa711331d29c074b
Sinan81 <sbulut@3vgeomatics.com>
2019-08-27 17:30:33 -0700

    improve meta info, add version

--- var/spack/repos/builtin/packages/py-mysqlclient/package.py
+++ var/spack/repos/builtin/packages/py-mysqlclient/package.py
@@ -7,18 +7,17 @@ from spack import *
 
 
 class PyMysqlclient(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Python interface to MySQL"""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "http://www.example.com"
+    # Note: _mysql module can be imported via "from MySQLdb import _mysql"
+    # The documentation is misleading about this.
+
+    homepage = "https://github.com/PyMySQL/mysqlclient-python"
     url      = "https://pypi.io/packages/source/m/mysqlclient/mysqlclient-1.4.4.tar.gz"
 
     version('1.4.4', sha256='9c737cc55a5dc8dd3583a942d5a9b21be58d16f00f5fefca4e575e7d9682e98c')
+    version('1.3.13', sha256='ff8ee1be84215e6c30a746b728c41eb0701a46ca76e343af445b35ce6250644f')
 
     depends_on('py-setuptools', type='build')
+    # Below: cxxstd=17 also works
     depends_on('mysql cxxstd=14')
-#
-#    def install(self, spec, prefix):
-#        # FIXME: Unknown build system
-#        make()
-#        make('install')
