39e42e942d7cfe432676b24ead9202a2255e755d
Sinan <sbulutw@gmail.com>
2019-09-03 11:35:38 -0700

    Update var/spack/repos/builtin/packages/protobuf-c/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/protobuf-c/package.py
+++ var/spack/repos/builtin/packages/protobuf-c/package.py
@@ -17,4 +17,4 @@ class ProtobufC(AutotoolsPackage):
     version('1.3.2', sha256='53f251f14c597bdb087aecf0b63630f434d73f5a10fc1ac545073597535b9e74')
 
     depends_on('protobuf')
-    depends_on('pkg-config')
+    depends_on('pkgconfig', type='build')
