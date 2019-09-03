198c3f8ab735cafba07e76f30566185efdd5458f
Sinan <sbulutw@gmail.com>
2019-09-03 11:36:35 -0700

    Update var/spack/repos/builtin/packages/py-configobj/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-configobj/package.py
+++ var/spack/repos/builtin/packages/py-configobj/package.py
@@ -18,7 +18,7 @@ class PyConfigobj(PythonPackage):
 
     # The version on PyPi seems to be outdated (2014) although confusingly
     # enough version number is the same latest release on github.
-    depends_on('py-six', type='run')
+    depends_on('py-six', type=('build', 'run'))
     depends_on('python@2.7:2.8,3.4:')
 
     def url_for_version(self, version):
