5e59fdfa626d082dd7d0d5f2e8c3c427c90c885e
ppodhajski <podhajski@gmail.com>
2019-09-04 14:20:04 +0200

    Add x264 library package (#484)

--- var/spack/repos/builtin/packages/ffmpeg/package.py
+++ var/spack/repos/builtin/packages/ffmpeg/package.py
@@ -13,12 +13,17 @@ class Ffmpeg(AutotoolsPackage):
     homepage = "https://ffmpeg.org"
     url      = "http://ffmpeg.org/releases/ffmpeg-3.2.4.tar.bz2"
 
-    version('3.2.4',   'd3ebaacfa36c6e8145373785824265b4')
+    version('3.2.4', 'd3ebaacfa36c6e8145373785824265b4')
+    version('4.2', '41b4ade83439fafe635001127f1056d4')
 
     variant('shared', default=True,
             description='build shared libraries')
+    variant('x264', default=True,
+            description='enable x264 encoder')
 
     depends_on('yasm@1.2.0:')
+    depends_on('x264', when='@4.2: +x264')
+
 
     def configure_args(self):
         spec = self.spec
@@ -27,4 +32,7 @@ class Ffmpeg(AutotoolsPackage):
         if '+shared' in spec:
             config_args.append('--enable-shared')
 
+        if '+x264' in spec and self.version >= Version('4.2'):
+            config_args.append('--enable-libx264')
+            config_args.append('--enable-gpl')
         return config_args
