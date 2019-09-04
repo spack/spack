4f5a4ff50745b6b884c8af00547158d303f73068
Sinan81 <sbulut@3vgeomatics.com>
2019-09-04 11:57:49 -0700

    improve style

--- var/spack/repos/builtin/packages/py-configobj/package.py
+++ var/spack/repos/builtin/packages/py-configobj/package.py
@@ -23,8 +23,8 @@ class PyConfigobj(PythonPackage):
 
     def url_for_version(self, version):
         if self.version <= Version('5.0.0'):
-            url = "https://pypi.io/packages/source/c/configobj/configobj-{0}.tar.gz".format(self.version)
+            url = "https://pypi.io/packages/source/c/configobj/configobj-{0}.tar.gz"
         else:
-            url = "https://github.com/DiffSK/configobj/archive/v{0}.tar.gz".format(self.version)
+            url = "https://github.com/DiffSK/configobj/archive/v{0}.tar.gz"
 
-        return url
+        return url.format(version)
