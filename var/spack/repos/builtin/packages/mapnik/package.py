b2904f16c1a319325481a5f5f2e80a53ef5a8f2f
Sinan81 <sbulut@3vgeomatics.com>
2019-09-04 13:09:55 -0700

    further improvements

--- var/spack/repos/builtin/packages/mapnik/package.py
+++ var/spack/repos/builtin/packages/mapnik/package.py
@@ -7,19 +7,18 @@ from spack import *
 
 
 class Mapnik(AutotoolsPackage):
-    """
-    Mapnik combines pixel-perfect image output with lightning-fast cartographic
-    algorithms, and exposes interfaces in C++, Python, and Node.
-    """
+    """FIXME: Put a proper description of your package here."""
 
-    homepage = "https://mapnik.org/"
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "http://www.example.com"
     url      = "https://github.com/mapnik/mapnik/releases/download/v3.0.22/mapnik-v3.0.22.tar.bz2"
 
     version('3.0.22', sha256='930612ad9e604b6a29b9cea1bc1de85cf7cf2b2b8211f57ec8b6b94463128ab9')
 
+    # FIXME: Add dependencies if required.
     depends_on('python', type=('build', 'run'))
     # Build fails with boost@1.70
-    depends_on('boost@:1.69.0+filesystem+system+icu+program_options cxxstd=11')
+    depends_on('boost@:1.69.0+regex+filesystem+system+icu+program_options+python cxxstd=11')
     depends_on('icu4c')
     depends_on('zlib')
     depends_on('freetype')
@@ -30,17 +29,26 @@ class Mapnik(AutotoolsPackage):
     depends_on('libtiff')
     depends_on('proj')
     depends_on('cairo')
-    depends_on('postgresql')
-    depends_on('gdal')
-    depends_on('sqlite+rtree')
+    depends_on('postgresql', type=('build', 'link', 'run'))
+    depends_on('gdal', type=('build', 'link', 'run'))
+    depends_on('sqlite+rtree', type=('build', 'link', 'run'))
     depends_on('libwebp')
+    depends_on('python', type='build')
 
-    # Build fails for gcc@9
     conflicts('%gcc@9.0.0:')
+
+    def setup_environment(self, spack_env, run_env):
+        spec = self.spec
+        spack_env.set('GDAL_DATA', spec['gdal'].prefix+'/share/gdal')
 
     def configure_args(self):
         args = []
         args.append('CXXFLAGS="-std=c++11"')
-        # config is expecting PREFIX not "prefix"
-        args.append('PREFIX=' + str(self.prefix))
+        args.append('PREFIX=' + self.prefix)
+        args.append('BOOST_INCLUDES=' + self.spec['boost'].prefix.include )
+        args.append('BOOST_LIBS=' + self.spec['boost'].prefix.lib )
+        args.append('PROJ_INCLUDES=' + self.spec['proj'].prefix.include )
+        args.append('PROJ_LIBS=' + self.spec['proj'].prefix.lib )
+        args.append('CAIRO_INCLUDES=' + self.spec['cairo'].prefix.include )
+        args.append('CAIRO_LIBS=' + self.spec['cairo'].prefix.lib )
         return args
