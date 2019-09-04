3121dde0b88b4d2ea4561f0e928cd554e33b4bc5
asmaahassan90 <31959389+asmaahassan90@users.noreply.github.com>
2019-09-04 14:25:06 +0200

    Break down lines to pass lint

--- var/spack/repos/builtin/packages/gams/package.py
+++ var/spack/repos/builtin/packages/gams/package.py
@@ -8,9 +8,9 @@ import os
 
 
 class Gams(Package):
-    """The General Algebraic Modeling System is a high-level modeling system for
-    mathematical optimization. GAMS is designed for modeling and solving linear,
-    nonlinear, and mixed-integer optimization problems."""
+    """The General Algebraic Modeling System is a high-level modeling system
+    for mathematical optimization. GAMS is designed for modeling and solving
+    linear, nonlinear, and mixed-integer optimization problems."""
 
     homepage = "https://www.gams.com/"
     version('27.2', '4f3f3484a4389661e0522a4cfe0289fd', expand=False)
@@ -19,12 +19,19 @@ class Gams(Package):
         return "file://{0}/linux_x64_64_sfx.exe".format(os.getcwd())
 
     def setup_environment(self, spack_env, run_env):
-        run_env.prepend_path("PATH", join_path(self.prefix, 'gams{0}_linux_x64_64_sfx'.format(self.version)))
+        run_env.prepend_path("PATH", join_path(self.prefix,
+                                               'gams{0}_linux_x64_64_sfx'
+                                               .format(self.version)))
 
     def install(self, spec, prefix):
-        os.chmod(join_path(self.stage.source_path, "linux_x64_64_sfx.exe"), 0o755)
+        os.chmod(join_path(self.stage.source_path,
+                           "linux_x64_64_sfx.exe"), 0o755)
         os.system("./linux_x64_64_sfx.exe")
-        install_tree(join_path(self.stage.source_path, 'gams{0}_linux_x64_64_sfx'.format(self.version)),
-                     join_path(self.prefix, 'gams{0}_linux_x64_64_sfx'.format(self.version)))
+        install_tree(join_path(self.stage.source_path,
+                               'gams{0}_linux_x64_64_sfx'
+                               .format(self.version)),
+                     join_path(self.prefix, 'gams{0}_linux_x64_64_sfx'
+                               .format(self.version)))
         install('{0}/gamslice.txt'.format(os.getcwd()),
-                join_path(self.prefix, 'gams{0}_linux_x64_64_sfx'.format(self.version), 'gamslice.txt'))
+                join_path(self.prefix, 'gams{0}_linux_x64_64_sfx'
+                          .format(self.version), 'gamslice.txt'))
