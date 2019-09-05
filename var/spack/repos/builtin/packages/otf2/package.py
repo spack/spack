40dc4e70fb6afec94697b7e5b724aa8fe0aa64c3
t-karatsu <fj6085ge@aa.jp.fujitsu.com>
2019-09-05 11:24:36 +0900

    otf2: add configure args to specify compiler commands.

--- var/spack/repos/builtin/packages/otf2/package.py
+++ var/spack/repos/builtin/packages/otf2/package.py
@@ -27,6 +27,10 @@ class Otf2(AutotoolsPackage):
     def configure_args(self):
         return [
             '--enable-shared',
+            'CC={0}'.format(self.compiler.cc),
+            'CXX={0}'.format(self.compiler.cxx),
+            'F77={0}'.format(self.compiler.f77),
+            'FC={0}'.format(self.compiler.fc),
             'CFLAGS={0}'.format(self.compiler.pic_flag),
             'CXXFLAGS={0}'.format(self.compiler.pic_flag)
         ]
