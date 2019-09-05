30756a643b212a04776887bc8904acbee7004174
Janne Blomqvist <blomqvist.janne@gmail.com>
2019-09-05 17:09:37 +0300

    py-ase: New version 3.18 (#12697)
    * py-ase: New version 3.18
    
    Add ASE version 3.18. ASE 3.18 has dropped support for python 2.x,
    hence add a dependency on python@3.5: for it.
    
    * Add more ASE dependencies
    
    Address review comments, add dependencies on py-scipy, py-matplotlib,
    py-flask, and also add type=('build', 'run') dependency specifiers.

--- var/spack/repos/builtin/packages/py-ase/package.py
+++ var/spack/repos/builtin/packages/py-ase/package.py
@@ -14,8 +14,14 @@ class PyAse(PythonPackage):
     homepage = "https://wiki.fysik.dtu.dk/ase/"
     url      = "https://pypi.io/packages/source/a/ase/ase-3.13.0.tar.gz"
 
+    version('3.18.0',
+            sha256='39d45f12def2669605bffc82926acfb13a0d0610e6d82740fa316aafa70f97f9')
     version('3.15.0', '65a0143753517c2df157e53bd29a18e3')
     version('3.13.0', 'e946a0addc5b61e5e2e75857e0f99b89')
 
-    depends_on('python@2.6:')
+    depends_on('python@2.6:', type=('build', 'run'), when='@:3.15.0')
+    depends_on('python@3.5:', type=('build', 'run'), when='@3.18.0:')
     depends_on('py-numpy', type=('build', 'run'))
+    depends_on('py-matplotlib', type=('build', 'run'))
+    depends_on('py-scipy', type=('build', 'run'))
+    depends_on('py-flask', type=('build', 'run'))
