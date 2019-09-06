7400f44caa6e98419eb4bc3107704b91e8a2018b
Owen Solberg <owen.solberg@gmail.com>
2019-09-06 11:18:54 -0700

    update py-xopen (add 0.8.2 checksum)

--- var/spack/repos/builtin/packages/py-xopen/package.py
+++ var/spack/repos/builtin/packages/py-xopen/package.py
@@ -15,6 +15,7 @@ class PyXopen(PythonPackage):
     homepage = "https://github.com/marcelm/xopen"
     url      = "https://pypi.io/packages/source/x/xopen/xopen-0.1.1.tar.gz"
 
+    version('0.8.2', sha256='003749e09af74103a29e9c64c468c03e084aa6dfe6feff4fe22366679a6534f7')
     version('0.5.0', sha256='b097cd25e8afec42b6e1780c1f6315016171b5b6936100cdf307d121e2cbab9f')
     version('0.1.1', '4e0e955546ee6bee4ea736b54623a671')
 
