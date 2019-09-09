f6357de8ea5bfa0f7d0aa8095d6969480409a4ac
Sinan81 <sbulut@3vgeomatics.com>
2019-09-09 00:24:10 -0700

    fix a dynamic linking error for Qsci python module

--- var/spack/repos/builtin/packages/qscintilla/package.py
+++ var/spack/repos/builtin/packages/qscintilla/package.py
@@ -87,9 +87,11 @@ class Qscintilla(QMakePackage):
                 # Add config options to avoid build errors
                 # "QAbstractScrollArea: No such file or directory"
                 # "qprinter.h: No such file or directory"
+                # ".../Qsci.so: undefined symbol: _ZTI10Qsci...."
                 qscipro=FileFilter('Qsci/Qsci.pro')
+                link_qscilibs = 'LIBS += -L'+self.prefix.lib+' -lqscintilla2_qt5'
                 qscipro.filter('TEMPLATE = lib',
-                               'TEMPLATE = lib\nQT += widgets\nQT += printsupport')
+                               'TEMPLATE = lib\nQT += widgets\nQT += printsupport\n'+link_qscilibs)
 
                 make()
 
