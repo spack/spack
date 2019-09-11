fe9c8c7839298b7df32c56990e83f30c8f1a9268
Sinan81 <sbulut@3vgeomatics.com>
2019-09-11 01:43:50 -0700

    build Qscintilla python bindings here

--- var/spack/repos/builtin/packages/py-pyqt4/package.py
+++ var/spack/repos/builtin/packages/py-pyqt4/package.py
@@ -28,8 +28,11 @@ class PyPyqt4(SIPPackage):
     version('4.11.3', '997c3e443165a89a559e0d96b061bf70',
             url='http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt-x11-gpl-4.11.3.tar.gz')
 
+    variant('qsci', default=False, description='Build with QScintilla python bindings')
+
     # Supposedly can also be built with Qt 5 compatibility layer
     depends_on('qt@:4')
+    depends_on('qscintilla', when='+qsci')
 
     # https://www.riverbankcomputing.com/static/Docs/PyQt4/installation.html
     def configure_file(self):
@@ -40,4 +43,41 @@ class PyPyqt4(SIPPackage):
             '--pyuic4-interpreter', self.spec['python'].command.path,
             '--sipdir', self.prefix.share.sip.PyQt4,
             '--stubsdir', join_path(site_packages_dir, 'PyQt4'),
+            '--qsci-api-destdir', self.prefix.share+'/qsci'
         ]
+
+    @run_after('install')
+    def make_qsci(self):
+        if '+qsci' in self.spec:
+            with working_dir(str(self.spec['qscintilla'].prefix)+'/share/qscintilla/src/Python'):
+                pydir = join_path(site_packages_dir, 'PyQt4')
+                python = which('python')
+                python('configure.py',
+                       '--sip=' + self.prefix.bin + '/sip',
+                       '--qsci-incdir=' + self.spec['qscintilla'].prefix.include,
+                       '--qsci-libdir=' + self.spec['qscintilla'].prefix.lib,
+                       '--qsci-sipdir=' + self.prefix.share.sip.PyQt4,
+                       '--apidir=' + self.prefix + '/share/qsci',
+                       '--destdir=' + pydir,
+                       '--pyqt-sipdir=' + self.prefix.share.sip.PyQt4,
+                       '--sip-incdir=' + self.prefix + '/include/python'+str(self.spec['python'].version.up_to(2)),
+                       '--stubsdir='+pydir)
+
+                # Fix build errors
+                # "QAbstractScrollArea: No such file or directory"
+                # "qprinter.h: No such file or directory"
+                # ".../Qsci.so: undefined symbol: _ZTI10Qsci...."
+                qscipro = FileFilter('Qsci/Qsci.pro')
+                link_qscilibs = 'LIBS += -L'+self.prefix.lib+' -lqscintilla2_qt4'
+                qscipro.filter('TEMPLATE = lib',
+                               'TEMPLATE = lib\nQT += widgets\nQT += printsupport\n'+link_qscilibs)
+
+                make()
+
+                # Fix installation prefixes
+                makefile = FileFilter('Makefile')
+                makefile.filter(r'\$\(INSTALL_ROOT\)','')
+                makefile = FileFilter('Qsci/Makefile')
+                makefile.filter(r'\$\(INSTALL_ROOT\)','')
+
+                make('install')
