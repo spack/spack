b8481d791395b5ecd5fb23f0e022d6db7fc33120
Adam J. Stewart <ajstewart426@gmail.com>
2019-09-16 13:04:50 -0500

    Add py-sortedcontainers package (#12826)

---
+++ var/spack/repos/builtin/packages/py-sortedcontainers/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PySortedcontainers(PythonPackage):
+    """Sorted Containers is an Apache2 licensed sorted collections library,
+    written in pure-Python, and fast as C-extensions."""
+
+    homepage = "http://www.grantjenks.com/docs/sortedcontainers/"
+    url      = "https://pypi.io/packages/source/s/sortedcontainers/sortedcontainers-2.1.0.tar.gz"
+
+    version('2.1.0', sha256='974e9a32f56b17c1bac2aebd9dcf197f3eb9cd30553c5852a3187ad162e1a03a')
+
+    depends_on('py-setuptools', type='build')
