2465a33ad92ed0c87227a307cc575ce5add9e22c
t-karatsu <fj6085ge@aa.jp.fujitsu.com>
2019-09-17 10:29:00 +0900

    * Modified the MPI commands to a simple form. * Unnecessary processing has been deleted.

--- var/spack/repos/builtin/packages/fujitsu-mpi/package.py
+++ var/spack/repos/builtin/packages/fujitsu-mpi/package.py
@@ -21,21 +21,14 @@ class FujitsuMpi(Package):
 
     def setup_dependent_package(self, module, dependent_spec):
         if '%fj' in dependent_spec:
-            self.spec.mpicc = join_path(self.prefix.bin, 'mpifcc')
-            self.spec.mpicxx = join_path(self.prefix.bin, 'mpiFCC')
-            self.spec.mpif77 = join_path(self.prefix.bin, 'mpifrt')
-            self.spec.mpifc = join_path(self.prefix.bin, 'mpifrt')
+            self.spec.mpicc = self.prefix.bin.mpifcc
+            self.spec.mpicxx = self.prefix.bin.mpiFCC
+            self.spec.mpif77 = self.prefix.bin.mpifrt
+            self.spec.mpifc = self.prefix.bin.mpifrt
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         if '%fj' in dependent_spec:
-            spack_env.set('MPICC', join_path(self.prefix.bin, 'mpifcc'))
-            spack_env.set('MPICXX', join_path(self.prefix.bin, 'mpiFCC'))
-            spack_env.set('MPIF77', join_path(self.prefix.bin, 'mpifrt'))
-            spack_env.set('MPIF90', join_path(self.prefix.bin, 'mpifrt'))
-
-        spack_env.set('OMPI_CC', spack_cc)
-        spack_env.set('OMPI_CXX', spack_cxx)
-        spack_env.set('OMPI_FC', spack_fc)
-        spack_env.set('OMPI_F77', spack_f77)
-
-        spack_env.prepend_path('LD_LIBRARY_PATH', self.prefix.lib)
+            spack_env.set('MPICC', self.prefix.bin.mpifcc)
+            spack_env.set('MPICXX', self.prefix.bin.mpiFCC)
+            spack_env.set('MPIF77', self.prefix.bin.mpifrt)
+            spack_env.set('MPIF90', self.prefix.bin.mpifrt)
