bc85dfe61a4dd33376ae1781e2b42a5b24b98ac3
Julien Loiseau <contact@julien-loiseau.fr>
2019-09-18 14:03:11 -0700

    formatting

--- var/spack/repos/builtin/packages/mstk/package.py
+++ var/spack/repos/builtin/packages/mstk/package.py
@@ -7,18 +7,18 @@ from spack import *
 
 
 class Mstk(CMakePackage):
-    """MSTK is a mesh framework that allows users to represent, 
-    manipulate and query unstructured 3D arbitrary topology meshes 
-    in a general manner without the need to code their own data 
-    structures. MSTK is a flexible framework in that it allows a 
-    variety of underlying representations for the mesh while 
-    maintaining a common interface. It allows users to choose from 
-    different mesh representations either at initialization (implemented) 
-    or during the program execution (not yet implemented) so that the 
-    optimal data structures are used for the particular algorithm. 
-    The interaction of users and applications with MSTK is through a 
-    functional interface that acts as though the mesh always contains 
-    vertices, edges, faces and regions and maintains connectivity between 
+    """MSTK is a mesh framework that allows users to represent,
+    manipulate and query unstructured 3D arbitrary topology meshes
+    in a general manner without the need to code their own data
+    structures. MSTK is a flexible framework in that it allows a
+    variety of underlying representations for the mesh while
+    maintaining a common interface. It allows users to choose from
+    different mesh representations either at initialization (implemented)
+    or during the program execution (not yet implemented) so that the
+    optimal data structures are used for the particular algorithm.
+    The interaction of users and applications with MSTK is through a
+    functional interface that acts as though the mesh always contains
+    vertices, edges, faces and regions and maintains connectivity between
     all these entities."""
 
     homepage = "https://github.com/MeshToolkit/MSTK"
@@ -27,7 +27,11 @@ class Mstk(CMakePackage):
     version("develop", branch='master', submodules=False, preferred=True)
 
     variant('parallel', default='none', description='Enable Parallel Support',
-            values=('none','metis','zoltan','zoltan_parmetis','zoltan_ptscotch'), multi=False)
+            values=(
+                'none',
+                'metis',
+                'zoltan', 'zoltan_parmetis', 'zoltan_ptscotch'),
+            multi=False)
     variant('exodusii', default=False,
             description='Enable ExodusII')
     variant('tests', default=True, description="Enable testing")
@@ -45,8 +49,8 @@ class Mstk(CMakePackage):
     depends_on("zoltan -fortran", when='parallel="zoltan_ptscotch"')
     depends_on("scotch +mpi", when='parallel="zoltan_ptscotch"')
 
-    # Exodusii variant 
-    # The default exodusii build with mpi support 
+    # Exodusii variant
+    # The default exodusii build with mpi support
     # It includes netcdf which includes hdf5
     depends_on("exodusii", when='+exodusii')
 
@@ -57,7 +61,7 @@ class Mstk(CMakePackage):
 
         # Parallel variant 
         if self.spec.variants['parallel'].value != "none":
-            # Use mpi for compilation 
+            # Use mpi for compilation
             options.append('-DCMAKE_CXX_COMPILER=mpicxx')
             options.append('-DCMAKE_C_COMPILER=mpicc')
             options.append('-DCMAKE_FORTRAN_COMPILER=mpifort')
@@ -73,8 +77,8 @@ class Mstk(CMakePackage):
             options.append('-DENABLE_ZOLTAN=ON')
             options.append('-DZOLTAN_NEEDS_PTScotch=ON')
 
-        # ExodusII variant 
-        if self.spec.variants['exodusii'].value == True: 
+        # ExodusII variant
+        if self.spec.variants['exodusii'].value == True:
             options.append('-DENABLE_ExodusII=ON')
 
         return options
