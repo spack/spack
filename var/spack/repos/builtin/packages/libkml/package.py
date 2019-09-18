8790bc4bf8129e41f7985680b6c2a359bed71b11
Adam J. Stewart <ajstewart426@gmail.com>
2019-09-18 16:38:07 -0500

    libkml: fix darwin install name

--- var/spack/repos/builtin/packages/libkml/package.py
+++ var/spack/repos/builtin/packages/libkml/package.py
@@ -57,3 +57,9 @@ class Libkml(CMakePackage):
             args.append('-DBUILD_TESTING:BOOL=OFF')
 
         return args
+
+    @run_after('install')
+    def darwin_fix(self):
+        # The shared library is not installed correctly on Darwin; fix this
+        if self.spec.satisfies('platform=darwin'):
+            fix_darwin_install_name(self.prefix.lib)
