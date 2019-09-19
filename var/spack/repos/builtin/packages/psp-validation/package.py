d474767c96f1aa9268c3937fcd351660f860c437
Benoit Coste <benoit.coste@protonmail.com>
2019-09-19 13:43:25 +0200

    Bump psp-validation (#513)
    Co-Authored-By: Matthias Wolf <m+git@sushinara.net>

--- var/spack/repos/builtin/packages/psp-validation/package.py
+++ var/spack/repos/builtin/packages/psp-validation/package.py
@@ -13,16 +13,20 @@ class PspValidation(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/nse/psp-validation"
 
     version('develop', branch='master')
-    version('0.1.12', tag='psp-validation-v0.1.12', preferred=True)
+    version('0.1.14', tag='psp-validation-v0.1.14')
+    version('0.1.12', tag='psp-validation-v0.1.12')
 
     depends_on('py-setuptools', type=('build', 'run'))
 
+    depends_on('py-attrs', type='run')
     depends_on('py-click@7.0:7.999', type='run')
     depends_on('py-future@0.16:', type='run')
     depends_on('py-h5py~mpi@2.7:', type='run')
     depends_on('py-joblib@0.13:', type='run')
     depends_on('py-numpy@1.10:', type='run')
     depends_on('py-tqdm@4.0:', type='run')
-
     depends_on('py-bglibpy@4.0:', type='run')
     depends_on('py-bluepy@0.13.3:', type='run')
+    depends_on('py-efel@3.0.39:', type='run')
+
+    depends_on('py-mock@3.0.5', type='build')
