cb33f2e8cf9b372d50ca5e73eaff0e83abde72df
Sinan81 <sbulut@3vgeomatics.com>
2019-09-23 03:22:20 -0700

    flake8

--- var/spack/repos/builtin/packages/openldap/package.py
+++ var/spack/repos/builtin/packages/openldap/package.py
@@ -8,19 +8,18 @@ from spack import *
 
 class Openldap(AutotoolsPackage):
     """
-    OpenLDAP Software is an open source implementation of the Lightweight Directory Access Protocol.
-    The suite includes:
+    OpenLDAP Software is an open source implementation of the Lightweight
+    Directory Access Protocol. The suite includes:
 
     slapd - stand-alone LDAP daemon (server)
     libraries implementing the LDAP protocol, and
-    utilities, tools, and sample clients. 
+    utilities, tools, and sample clients.
     """
 
     homepage = "https://www.openldap.org/"
     url      = "ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.9.tgz"
 
     version('2.4.48', sha256='d9523ffcab5cd14b709fcf3cb4d04e8bc76bb8970113255f372bc74954c6074d')
-
 
     variant('client_only', default=True, description='Client only installation')
     variant('icu', default=False, description='Build with unicode support')
@@ -31,44 +30,44 @@ class Openldap(AutotoolsPackage):
     depends_on('gnutls', when='~client_only~openssl')
     depends_on('unixodbc', when='~client_only')
     depends_on('postgresql', when='~client_only')
-    depends_on('berkeley-db', when='~client_only') # for slapd
+    depends_on('berkeley-db', when='~client_only')  # for slapd
     # Recommended dependencies by Linux From Scratch
     # depends_on('cyrus-sasl', when='~client_only') # not avail. in spack yet
     # depends_on('openslp', when='~client_only') # not avail. in spack yet
     # depends_on('Pth', when='~client_only') # not avail. in spack yet
-    depends_on('perl', when='+perl_backend') # for slapd
+    depends_on('perl', when='+perl_backend')  # for slapd
 
     # Ref: http://www.linuxfromscratch.org/blfs/view/svn/server/openldap.html
     @when('+client_only')
     def configure_args(self):
         return ['CPPFLAGS=-D_GNU_SOURCE',
-            '--disable-static',
-            '--enable-dynamic',
-            '--disable-debug',
-            '--disable-slapd',
-        ]
+                '--disable-static',
+                '--enable-dynamic',
+                '--disable-debug',
+                '--disable-slapd',
+                ]
 
     @when('~client_only')
     def configure_args(self):
         # Ref: https://www.openldap.org/lists/openldap-technical/201009/msg00304.html
-        args = ['CPPFLAGS=-D_GNU_SOURCE', # fixes a build error
-            '--disable-static',
-            '--disable-debug',
-            '--with-cyrus-sasl',
-            '--enable-dynamic',
-            '--enable-crypt',
-            '--enable-spasswd',
-            '--enable-slapd',
-            '--enable-modules',
-            '--enable-rlookups',
-            '--enable-backends=mod',
-            '--disable-ndb',
-            '--disable-sql',
-            '--disable-shell',
-            '--disable-bdb',
-            '--disable-hdb',
-            '--enable-overlays=mod',
-            ]
+        args = ['CPPFLAGS=-D_GNU_SOURCE',  # fixes a build error
+                '--disable-static',
+                '--disable-debug',
+                '--with-cyrus-sasl',
+                '--enable-dynamic',
+                '--enable-crypt',
+                '--enable-spasswd',
+                '--enable-slapd',
+                '--enable-modules',
+                '--enable-rlookups',
+                '--enable-backends=mod',
+                '--disable-ndb',
+                '--disable-sql',
+                '--disable-shell',
+                '--disable-bdb',
+                '--disable-hdb',
+                '--enable-overlays=mod',
+                ]
 
         if when('~openssl'):
             args.append('--with-tls=gnutls')
@@ -81,7 +80,6 @@ class Openldap(AutotoolsPackage):
             args.append('--disable-perl')
 
         return args
-
 
     def install(self, spec, prefix):
         make('depend')
