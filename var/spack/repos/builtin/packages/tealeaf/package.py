31e5318c240e12d59b01972cbaf92680ef4be6bd
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2019-09-26 10:02:08 +0200

    Removed references to '(-march|-mtune|-mcpu)=native' (#12948)
    Now that Spack injects microarchitecture specific optimizations for
    the selected target, packages should avoid adding flags that could
    step over those.

--- var/spack/repos/builtin/packages/tealeaf/package.py
+++ var/spack/repos/builtin/packages/tealeaf/package.py
@@ -25,10 +25,7 @@ class Tealeaf(MakefilePackage):
     depends_on('mpi')
 
     def edit(self, spec, prefix):
-        if spec.target.family == 'aarch64' and spec.satisfies('%gcc@:5.9'):
-            filter_file(
-                '-march=native', '', join_path('TeaLeaf_ref', 'Makefile')
-            )
+        filter_file('-march=native', '', join_path('TeaLeaf_ref', 'Makefile'))
 
     @property
     def build_targets(self):
