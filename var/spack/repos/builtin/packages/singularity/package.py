87715bcee3ba0182f028be901e25e5395597af9a
ArangoGutierrez <eduardo@sylabs.io>
2019-10-01 09:16:58 -0500

    add singularity 3.4.1
    Signed-off-by: ArangoGutierrez <eduardo@sylabs.io>

--- var/spack/repos/builtin/packages/singularity/package.py
+++ var/spack/repos/builtin/packages/singularity/package.py
@@ -21,11 +21,12 @@ class Singularity(MakefilePackage):
     '''
 
     homepage = "https://www.sylabs.io/singularity/"
-    url      = "https://github.com/sylabs/singularity/releases/download/v3.1.1/singularity-3.1.1.tar.gz"
+    url      = "https://github.com/sylabs/singularity/releases/download/v3.4.1/singularity-3.4.1.tar.gz"
     git      = "https://github.com/sylabs/singularity.git"
 
     version('develop', branch='master')
 
+    version('3.4.1', sha256='638fd7cc5ab2a20e779b8768f73baf21909148339d6c4edf6ff61349c53a70c2')
     version('3.4.0', sha256='eafb27f1ffbed427922ebe2b5b95d1c9c09bfeb897518867444fe230e3e35e41')
     version('3.3.0', sha256='070530a472e7e78492f1f142c8d4b77c64de4626c4973b0589f0d18e1fcf5b4f')
     version('3.2.1', sha256='d4388fb5f7e0083f0c344354c9ad3b5b823e2f3f27980e56efa7785140c9b616')
@@ -39,7 +40,7 @@ class Singularity(MakefilePackage):
     depends_on('shadow', type='run', when='@3.3:')
     depends_on('cryptsetup', type=('build', 'run'), when='@3.4:')
 
-    patch('singularity_v3.4.0_remove_root_check.patch', level=0, when='@3.4.0')
+    patch('singularity_v3.4.1_remove_root_check.patch', level=0, when='@3.4.1')
 
     # Go has novel ideas about how projects should be organized.
     # We'll point GOPATH at the stage dir, and move the unpacked src
