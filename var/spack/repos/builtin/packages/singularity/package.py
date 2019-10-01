7b01f3801085364ce523d7a116f14fde636e09c9
Eduardo Arango <eduardo@sylabs.io>
2019-10-01 14:09:31 -0500

    add maintainers list to pkg singularity
    Add @alalazo and me to the Singularity maintainers list

--- var/spack/repos/builtin/packages/singularity/package.py
+++ var/spack/repos/builtin/packages/singularity/package.py
@@ -24,6 +24,8 @@ class Singularity(MakefilePackage):
     url      = "https://github.com/sylabs/singularity/releases/download/v3.1.1/singularity-3.1.1.tar.gz"
     git      = "https://github.com/sylabs/singularity.git"
 
+    maintainers = ['ArangoGutierrez','alalazo']
+    
     version('develop', branch='master')
 
     version('3.4.1', sha256='638fd7cc5ab2a20e779b8768f73baf21909148339d6c4edf6ff61349c53a70c2')
