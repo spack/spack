e0b21aae141f4ace61d0f11d3d64a164506bef0e
Matthias Wolf <matthias.wolf@epfl.ch>
2019-10-01 14:01:13 +0200

    touchdetector: add 5.3.1 (#531)

--- var/spack/repos/builtin/packages/touchdetector/package.py
+++ var/spack/repos/builtin/packages/touchdetector/package.py
@@ -33,6 +33,7 @@ class Touchdetector(CMakePackage):
     git      = "ssh://bbpcode.epfl.ch/building/TouchDetector"
 
     version('develop', submodules=True)
+    version('5.3.1', tag='5.3.1', submodules=True)
     version('5.3.0', tag='5.3.0', submodules=True)
     version('5.2.0', tag='5.2.0', submodules=True)
     version('5.1.0', tag='5.1.0', submodules=True)
