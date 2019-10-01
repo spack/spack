aaf157e0636bb0b01eba3f70f41a3368d87d9ab3
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2019-10-01 17:02:14 +0200

    libaio: simplified package by adding an explicit conflict for darwin

--- var/spack/repos/builtin/packages/libaio/package.py
+++ var/spack/repos/builtin/packages/libaio/package.py
@@ -3,22 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
-
 
 class Libaio(Package):
     """This is the linux native Asynchronous I/O interface library."""
 
     homepage = "http://lse.sourceforge.net/io/aio.html"
-    url      = "https://debian.inf.tu-dresden.de/debian/pool/main/liba/libaio/libaio_0.3.110.orig.tar.gz"
+    url = "https://debian.inf.tu-dresden.de/debian/pool/main/liba/libaio/libaio_0.3.110.orig.tar.gz"
 
     version('0.3.110', '2a35602e43778383e2f4907a4ca39ab8')
 
-    def install(self, spec, prefix):
-        # libaio is not supported on OS X
-        if spec.platform == 'darwin' and spec.target.family == 'x86_64':
-            # create a dummy directory
-            mkdir(prefix.lib)
-            return
+    conflicts('platform=darwin', msg="libaio is a linux specific library")
 
+    def install(self, spec, prefix):
         make('prefix={0}'.format(prefix), 'install')
