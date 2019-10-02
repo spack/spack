705ade8dde49b74250cdb03d0be176fb7c64955b
ppodhajski <podhajski@gmail.com>
2019-10-02 18:17:00 +0200

    add emsim spackage (#524)

---
+++ var/spack/repos/builtin/packages/emsim/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Emsim(CMakePackage):
+    """ EMSim is a tool for computing VSD and LFP """ 
+
+    git = "ssh://bbpcode.epfl.ch/viz/EMSim"
+
+    generator = 'Ninja'
+
+    version('1.0.0', tag='v1.0.0', submodules=True)
+
+    depends_on('cmake@3.1:', type='build')
+    depends_on('ninja', type='build')
+    depends_on('brion')
