7c0b0b7862aafee0f36780100d817e061a4e8e68
Matthias Wolf <matthias.wolf@epfl.ch>
2019-10-02 09:10:40 +0200

    spykfunc: bump to 0.15.1 (#534)

--- var/spack/repos/builtin/packages/spykfunc/package.py
+++ var/spack/repos/builtin/packages/spykfunc/package.py
@@ -47,6 +47,7 @@ class Spykfunc(PythonPackage):
     version('0.14.2', tag='v0.14.2', submodules=True, clean=False)
     version('0.14.3', tag='v0.14.3', submodules=True, clean=False)
     version('0.15.0', tag='v0.15.0', submodules=True, clean=False)
+    version('0.15.1', tag='v0.15.1', submodules=True, clean=False)
 
     depends_on('hdf5~mpi')
     depends_on('highfive~mpi', type='build')
