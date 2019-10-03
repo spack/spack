073d449b89417f8fce150028f9901009d50ac863
Toyohisa Kameyama <kameyama@riken.jp>
2019-10-03 17:01:52 +0900

    hapcut2: add zlib dependency.

--- var/spack/repos/builtin/packages/hapcut2/package.py
+++ var/spack/repos/builtin/packages/hapcut2/package.py
@@ -17,6 +17,8 @@ class Hapcut2(MakefilePackage):
     version('2017-07-10', commit='2966b94c2c2f97813b757d4999b7a6471df1160e',
             submodules=True)
 
+    depends_on('zlib', type='link')
+
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
         with working_dir('build'):
