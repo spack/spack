4775b907a93fc400f7eeb1f5e8070f56270e377b
Andreas Baumbach <healther.astro@gmail.com>
2019-10-06 10:04:11 +0200

    update py-line-profiler to work with current python
    * unfortunately there is no current release so
      we take a current commit

--- var/spack/repos/builtin/packages/py-line-profiler/package.py
+++ var/spack/repos/builtin/packages/py-line-profiler/package.py
@@ -12,6 +12,9 @@ class PyLineProfiler(PythonPackage):
     homepage = "https://github.com/rkern/line_profiler"
     url      = "https://pypi.io/packages/source/l/line_profiler/line_profiler-2.0.tar.gz"
 
+    # unfortunately no new release is available and 2.0 does not
+    # yet work with python?3.5?: so we use a current commit
+    version('2.new', git='git@github.com:rkern/line_profiler.git', commit='3cdc2fab6793802d31130b1af2705bddebf6f7fd')
     version('2.0', 'fc93c6bcfac3b7cb1912cb28836d7ee6')
 
     depends_on('python@2.5:')
