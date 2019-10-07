729ac41ba24a5f8d323b4e4be3a803bc2e65860e
Francesco Di Natale <dinatale3@llnl.gov>
2019-10-07 12:19:24 -0700

    Addition of 1.1.3 and 1.1.4

--- var/spack/repos/builtin/packages/py-maestrowf/package.py
+++ var/spack/repos/builtin/packages/py-maestrowf/package.py
@@ -13,6 +13,8 @@ class PyMaestrowf(PythonPackage):
     homepage = "https://github.com/LLNL/maestrowf/"
     url      = "https://github.com/LLNL/maestrowf/archive/v1.1.2.tar.gz"
 
+    version('1.1.4', 'e2da101fb1cad9a164d375eccb1f07ce')
+    version('1.1.3', '0299e4ae3ec8b8c0296df0efaa8b517f')
     version('1.1.2', 'a9e05d82910cd2dd077321fb9b0c8dcd')
     version('1.1.1', 'd38bbf634de4f29fd01d1864ba2f70e0')
     version('1.1.0', '3c20bf36fbb85d14c3bfdb865944a409')
