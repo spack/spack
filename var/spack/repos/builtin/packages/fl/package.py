36976c5e92c7def951349fc89a7da4dc37a61e23
Jianwen WEI <weijianwen@gmail.com>
2019-10-08 15:49:22 +0800

    Fix flake8 issues.

--- var/spack/repos/builtin/packages/fl/package.py
+++ var/spack/repos/builtin/packages/fl/package.py
@@ -7,19 +7,18 @@ from spack import *
 
 
 class Fl(Package):
-    """Fawlty Language is an IDL8 (Interactive Data Language) compatible compiler."""
+    """Fawlty Language is an IDL8
+    (Interactive Data Language) compatible compiler."""
 
     homepage = "https://www.flxpert.hu/fl/"
     url      = "https://www.flxpert.hu/fl/fl_0.79.47-amd64-linux.tar.gz"
 
     version('0.79.47', sha256='b8a4a74118c1a024313bf912261fbc016a53f2d15adb1226217f2a10a9f7ca9a')
 
-    # TODO: Add runtime dependency since Fawlty is binary distributed.
-
     def install(self, spec, prefix):
-        with working_dir('fl_{0}'.format(spec.version)):
-            if 'x86_64' in spack.architecture.sys_type():
+        if (self.spec.satisfies('platform=linux') and
+           self.spec.satisfies('target=x86_64')):
+            with working_dir('fl_{0}'.format(spec.version)):
                 install_tree('.', prefix)
-            else:
-                raise InstallError('Cannot install Fawlty on {0} architecture.'
-                                   .format(spack.architecture.sys_type()))
+        else:
+            raise InstallError('fl is built for Linux x86_64 platform only.')
