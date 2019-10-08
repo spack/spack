620a7b636dc7b310a48f10c3bd4ae93c82dd7cea
Wyatt Spear <wspear@cs.uoregon.edu>
2019-10-08 10:26:04 -0700

    Fixed hash of version .0.5 and added latest version (.0.7.1)

--- var/spack/repos/builtin/packages/py-prometheus-client/package.py
+++ var/spack/repos/builtin/packages/py-prometheus-client/package.py
@@ -10,8 +10,9 @@ class PyPrometheusClient(PythonPackage):
     """Prometheus instrumentation library for Python applications."""
 
     homepage = "https://pypi.org/project/prometheus_client/"
-    url      = "https://pypi.io/packages/source/p/prometheus_client/prometheus_client-0.5.0.tar.gz"
+    url      = "https://pypi.io/packages/source/p/prometheus_client/prometheus_client-0.7.1.tar.gz"
 
-    version('0.5.0', sha256='2d7f7af343dec0a96ee849b1bba18aad9f767bf16d5eb5f0c11cae837bf22731')
+    version('0.7.1', sha256='71cd24a2b3eb335cb800c7159f423df1bd4dcd5171b234be15e3f31ec9f622da')
+    version('0.5.0', sha256='e8c11ff5ca53de6c3d91e1510500611cafd1d247a937ec6c588a0a7cc3bef93c')
 
     depends_on('py-setuptools', type='build')
