abd183f759f2aa2b6784cfe95844a8de1c6ab444
Wyatt Spear <wspear@cs.uoregon.edu>
2019-10-10 14:44:52 -0700

    Fixed line lengths

--- var/spack/repos/builtin/packages/py-python-memcached/package.py
+++ var/spack/repos/builtin/packages/py-python-memcached/package.py
@@ -7,9 +7,10 @@ from spack import *
 
 
 class PyPythonMemcached(PythonPackage):
-    """This software is a 100% Python interface to the memcached memory cache daemon.
-    It is the client side software which allows storing values in one or more,
-    possibly remote, memcached servers. Search google for memcached for more information."""
+    """This software is a 100% Python interface to the memcached memory cache
+    daemon. It is the client side software which allows storing values in one
+    or more, possibly remote, memcached servers. Search google for memcached
+    for more information."""
 
     homepage = "https://pypi.org/project/python-memcached/"
     url      = "https://pypi.io/packages/source/p/python-memcached/python-memcached-1.59.tar.gz"
