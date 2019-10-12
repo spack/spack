ebb5ce4b69683c7ff87e100ee651e82ded66cb3d
Nichols A. Romero <naromero77@users.noreply.github.com>
2019-10-12 12:30:43 -0500

    F18 Oct2019 Minor Update (#13192)
    * Minor clean-up of package information.
    
    * Add variants, fix dependencies, and add conflicts for compilers that do not support C++17.
    
    * Correct dependency type for CMake.

--- var/spack/repos/builtin/packages/f18/package.py
+++ var/spack/repos/builtin/packages/f18/package.py
@@ -10,10 +10,24 @@ class F18(CMakePackage):
     """F18 is a front-end for Fortran intended to replace the existing front-end
     in the Flang compiler"""
 
+    # Package information
     homepage = "https://github.com/flang-compiler/f18"
-
-    git      = "https://github.com/flang-compiler/f18"
+    git      = "https://github.com/flang-compiler/f18.git"
 
     version('master', branch='master')
 
-    depends_on('llvm@6.0.0+clang', when='@master')
+    # Variants
+    variant('build_type', default='Release',
+            description='The build type to build',
+            values=('Debug', 'Release', 'RelWithDebInfo'))
+
+    # Dependencies
+    depends_on('cmake@3.9.0:', type='build')
+    depends_on('llvm+clang@7:')
+
+    # Conflicts
+    compiler_warning = 'F18 requires a compiler with support for C++17'
+    conflicts('%clang@:6', msg=compiler_warning)
+    conflicts('%gcc@:7.1', msg=compiler_warning)
+    conflicts('%intel', msg=compiler_warning)
+    conflicts('%pgi', msg=compiler_warning)
