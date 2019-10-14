117e8a4b7f2002661e9bfa1ac6cfbb4302f595d0
Satish Balay <balay@mcs.anl.gov>
2019-10-13 20:54:40 -0500

    amrex: update sundials dependency as sundials@5.0 is also compatible

--- var/spack/repos/builtin/packages/amrex/package.py
+++ var/spack/repos/builtin/packages/amrex/package.py
@@ -53,7 +53,7 @@ class Amrex(CMakePackage):
 
     # Build dependencies
     depends_on('mpi', when='+mpi')
-    depends_on('sundials@4.0.0:4.1.0 +ARKODE +CVODE', when='@19.08: +sundials')
+    depends_on('sundials@4.0.0: +ARKODE +CVODE', when='@19.08: +sundials')
     depends_on('python@2.7:', type='build')
     depends_on('cmake@3.5:',  type='build', when='@:18.10.99')
     depends_on('cmake@3.13:',  type='build', when='@18.11:')
