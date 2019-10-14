cede1df05bb215b0447ac8aee594e1f6fb6b60aa
codeandkey <jtst@iastate.edu>
2019-10-14 14:20:32 -0500

    py-gensim: add 3.8.1

--- var/spack/repos/builtin/packages/py-gensim/package.py
+++ var/spack/repos/builtin/packages/py-gensim/package.py
@@ -14,6 +14,8 @@ class PyGensim(PythonPackage):
     homepage = "https://pypi.org/project/gensim/"
     url      = "https://files.pythonhosted.org/packages/3a/bc/1415be59292a23ff123298b4b46ec4be80b3bfe72c8d188b58ab2653dee4/gensim-3.8.0.tar.gz"
 
+    version('3.8.1', sha256='33277fc0a8d7b0c7ce70fcc74bb82ad39f944c009b334856c6e86bf552b1dfdc',
+            url='https://files.pythonhosted.org/packages/73/f2/e9af000df6419bf1a63ffed3e6033a1b1d8fcf2f971fcdac15296619aff8/gensim-3.8.1.tar.gz')
     version('3.8.0', sha256='ec5de7ff2bfa8692fa96a846bb5aae52f267fc322fbbe303c1f042d258af5766')
 
     depends_on('python@3.5:', type=('build', 'run'))
