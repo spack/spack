40094a74f3622708e32d91bd43e6d5b81805f6fd
Nicolas Cornu <nicolac76@yahoo.fr>
2019-10-14 16:08:15 +0200

    touchdetector: add a depends to highfive (#546)
    * touchdetector: add a depends to highfive
    
    * depends of previous stable version
    
    * touchdetector: fix depends on highfive with +mpi

--- var/spack/repos/builtin/packages/touchdetector/package.py
+++ var/spack/repos/builtin/packages/touchdetector/package.py
@@ -58,6 +58,7 @@ class Touchdetector(CMakePackage):
     depends_on('mpi')
     depends_on('pugixml', when='@4.5:')
     depends_on('range-v3', when='@5.0.2:')
+    depends_on('highfive+mpi', when='@5.3.0:')
 
     # Old dependencies
     depends_on('hpctools~openmp', when='~openmp@:4.4')
