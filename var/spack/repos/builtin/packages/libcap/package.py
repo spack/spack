0c105b99ef6f59c822542fc6d2a5dc1adb505c85
Cedric Chevalier <cedric.chevalier@cea.fr>
2019-10-18 16:23:03 +0200

    Update libcap.

--- var/spack/repos/builtin/packages/libcap/package.py
+++ var/spack/repos/builtin/packages/libcap/package.py
@@ -13,11 +13,13 @@ class Libcap(MakefilePackage):
     distinct privileges."""
 
     homepage = "https://sites.google.com/site/fullycapable/"
-    url      = "https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.25.tar.gz"
+    url      = "https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.25.tar.xz"
 
-    version('2.25', sha256='4ca80dc6f9f23d14747e4b619fd9784434c570e24a7346f326c692784ed83a86')
+    version('2.27', sha256='dac1792d0118bee6aae6ba7fb93ff1602c6a9bda812fd63916eee1435b9c486a')
+    version('2.26', sha256='b630b7c484271b3ba867680d6a14b10a86cfa67247a14631b14c06731d5a458b')
+    version('2.25', sha256='693c8ac51e983ee678205571ef272439d83afe62dd8e424ea14ad9790bc35162')
 
-    patch('libcap-fix-the-libcap-native-building-failure-on-CentOS-6.7.patch')
+    patch('libcap-fix-the-libcap-native-building-failure-on-CentOS-6.7.patch', when="@2.25")
 
     def install(self, spec, prefix):
         make_args = [
