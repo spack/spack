290a8f889f7c44da24a826ff75703058bba9936b
Satish Balay <balay@mcs.anl.gov>
2019-10-22 13:44:05 -0500

    ginkgo: add version 1.1.0

--- var/spack/repos/builtin/packages/ginkgo/package.py
+++ var/spack/repos/builtin/packages/ginkgo/package.py
@@ -18,7 +18,8 @@ class Ginkgo(CMakePackage, CudaPackage):
 
     version('develop', branch='develop')
     version('master', branch='master')
-    version('1.0.0', commit='4524464')  # v1.0.0
+    version('1.1.0', commit='b9bec8225442b3eb2a85a870efa112ab767a17fb')  # v1.1.0
+    version('1.0.0', commit='45244641e0c2b19ba33aecd25153c0bddbcbe1a0')  # v1.0.0
 
     variant('shared', default=True, description='Build shared libraries')
     variant('full_optimizations', default=False, description='Compile with all optimizations')
