8112377d3121cf709924e00b23f95b2560a8d6cf
Marc Mengel <mengel@fnal.gov>
2019-10-25 16:56:25 -0500

    package py-jaraco-functools

---
+++ var/spack/repos/builtin/packages/py-jaraco-functools/package.py
@@ -0,0 +1,12 @@
+from spack import *
+
+
+class PyJaracoFunctools(PythonPackage):
+    """Functools like those found in stdlib"""
+
+    homepage = "https://github.com/jaraco/jaraco.functools"
+    url      = "https://files.pythonhosted.org/packages/a9/1e/44f6a5cffef147a3ffd37a748b8f4c2ded9b07ca20a15f17cd9874158f24/jaraco.functools-2.0.tar.gz"
+
+    version('2.0', sha256='35ba944f52b1a7beee8843a5aa6752d1d5b79893eeb7770ea98be6b637bf9345')
+
+    depends_on('py-setuptools', type='build')
