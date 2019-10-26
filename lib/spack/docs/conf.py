847703c13d0fd69be0b06d355308c47f4a060e66
Todd Gamblin <tgamblin@llnl.gov>
2019-10-25 21:49:27 -0700

    tutorial: move tutorial to standalone site (#13450)
    * docs: add a spack environment for building the docs
    
    * docs: remove tutorial and link to spack-tutorial.readthedocs.io
    
    The tutorial now has its own standalone website, versioned by instances
    of the tutorial.  Link to that instead of versioning it directly with Spack.

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -159,7 +159,7 @@ gettext_uuid = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '_spack_root']
+exclude_patterns = ['_build', '_spack_root', '.spack-env']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
