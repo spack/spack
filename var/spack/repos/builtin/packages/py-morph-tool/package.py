1fe36fc9b5c7f20bfd99e01b651ad5007c6048a2
genric <genrich.ivaska@epfl.ch>
2019-10-28 12:55:28 +0100

    add morph-tool 0.1.14 (#580)

--- var/spack/repos/builtin/packages/py-morph-tool/package.py
+++ var/spack/repos/builtin/packages/py-morph-tool/package.py
@@ -13,7 +13,8 @@ class PyMorphTool(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/nse/morph-tool"
 
     version('develop', branch='master')
-    version('0.1.3', tag='morph-tool-v0.1.3', preferred=True)
+    version('0.1.14', tag='morph-tool-v0.1.14')
+    version('0.1.3', tag='morph-tool-v0.1.3')
 
     variant('neuron', default=False, description='Neuron-based functionality')
 
