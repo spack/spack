a67ddf70def56016bb03a7b513eda004d6d0f403
Greg Lee <lee218@llnl.gov>
2019-10-28 08:18:46 -0700

    flake8 fix

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -433,7 +433,8 @@ class Openmpi(AutotoolsPackage):
         # The wrappers fail to automatically link libfabric. This will cause
         # undefined references unless we add the appropriate flags.
         if 'fabrics=libfabric' in spec:
-            config_args.append('--with-wrapper-ldflags=-L{0} -Wl,-rpath={0}'.format(spec['libfabric'].prefix.lib))
+            config_args.append('--with-wrapper-ldflags=-L{0} -Wl,-rpath={0}'
+                .format(spec['libfabric'].prefix.lib))
             config_args.append('--with-wrapper-libs=-lfabric')
 
         # Schedulers
