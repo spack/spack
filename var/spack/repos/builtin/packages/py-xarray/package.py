2fdaa83a69f2ea14de0ad9af6bcbb57313066e9f
Jannek Squar <squar@informatik.uni-hamburg.de>
2019-10-29 13:17:27 +0100

    Adjusted xarray package to use pypi-source

--- var/spack/repos/builtin/packages/py-xarray/package.py
+++ var/spack/repos/builtin/packages/py-xarray/package.py
@@ -11,12 +11,11 @@ class PyXarray(PythonPackage):
 
     homepage = "https://github.com/pydata/xarray"
     url      = "https://pypi.io/packages/source/x/xarray/xarray-0.9.1.tar.gz"
-    git      = "https://github.com/pydata/xarray.git"
 
-    version('0.14.0', tag='v0.14.0')
-    version('0.13.0', tag='v0.13.0')
-    version('0.12.0', tag='v0.12.0')
-    version('0.11.0', tag='v0.11.0')
+    version('0.14.0', sha256='a8b93e1b0af27fa7de199a2d36933f1f5acc9854783646b0f1b37fed9b4da091')
+    version('0.13.0', sha256='80e5746ffdebb96b997dba0430ff02d98028ef3828e6db6106cbbd6d62e32825')
+    version('0.12.0', sha256='856fd062c55208a248ac3784cac8d3524b355585387043efc92a4188eede57f3')
+    version('0.11.0', sha256='636964baccfca0e5d69220ac4ecb948d561addc76f47704064dcbe399e03a818')
     version('0.9.1', sha256='89772ed0e23f0e71c3fb8323746374999ecbe79c113e3fadc7ae6374e6dc0525')
 
     depends_on('py-setuptools',      type='build')
