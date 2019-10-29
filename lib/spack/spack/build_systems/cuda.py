d88358b544034d752ff6fa344792e2324a81e9e9
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2019-10-29 10:15:16 +0100

    Relaxed conflicts for version > 10.1

--- lib/spack/spack/build_systems/cuda.py
+++ lib/spack/spack/build_systems/cuda.py
@@ -60,7 +60,8 @@ class CudaPackage(PackageBase):
     conflicts('%gcc@5:', when='+cuda ^cuda@:7.5' + arch_platform)
     conflicts('%gcc@6:', when='+cuda ^cuda@:8' + arch_platform)
     conflicts('%gcc@7:', when='+cuda ^cuda@:9.1' + arch_platform)
-    conflicts('%gcc@8:', when='+cuda ^cuda@:10.2' + arch_platform)
+    conflicts('%gcc@8:', when='+cuda ^cuda@:10.0' + arch_platform)
+    conflicts('%gcc@9:', when='+cuda ^cuda@:10.2' + arch_platform)
     conflicts('%pgi@:14.8', when='+cuda ^cuda@:7.0.27' + arch_platform)
     conflicts('%pgi@:15.3,15.5:', when='+cuda ^cuda@7.5' + arch_platform)
     conflicts('%pgi@:16.2,16.0:16.3', when='+cuda ^cuda@8' + arch_platform)
@@ -85,7 +86,8 @@ class CudaPackage(PackageBase):
     arch_platform = ' target=ppc64le: platform=linux'
     # information prior to CUDA 9 difficult to find
     conflicts('%gcc@6:', when='+cuda ^cuda@:9' + arch_platform)
-    conflicts('%gcc@8:', when='+cuda ^cuda@:10.2' + arch_platform)
+    conflicts('%gcc@8:', when='+cuda ^cuda@:10.0' + arch_platform)
+    conflicts('%gcc@9:', when='+cuda ^cuda@:10.2' + arch_platform)
     conflicts('%pgi', when='+cuda ^cuda@:8' + arch_platform)
     conflicts('%pgi@:16', when='+cuda ^cuda@:9.1.185' + arch_platform)
     conflicts('%pgi@:17', when='+cuda ^cuda@:10' + arch_platform)
