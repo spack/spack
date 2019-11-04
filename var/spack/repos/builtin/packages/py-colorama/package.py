74f31d27cbbc9e57a3567f6006c932391081ff1a
Andreas Baumbach <healther.astro@gmail.com>
2019-11-04 20:24:17 +0100

    add python dependency

--- var/spack/repos/builtin/packages/py-colorama/package.py
+++ var/spack/repos/builtin/packages/py-colorama/package.py
@@ -15,4 +15,5 @@ class PyColorama(PythonPackage):
     version('0.4.1', sha256='05eed71e2e327246ad6b38c540c4a3117230b19679b875190486ddd2d721422d')
     version('0.3.7', sha256='e043c8d32527607223652021ff648fbb394d5e19cba9f1a698670b338c9d782b')
 
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
