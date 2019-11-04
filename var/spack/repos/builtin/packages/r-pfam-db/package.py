9a93d72c4a2203e5c2e7d8e17d990340c8163a6f
codeandkey <jtst@iastate.edu>
2019-11-04 12:00:09 -0600

    r-pfam-db: update dependencies, add 3.10.0

--- var/spack/repos/builtin/packages/r-pfam-db/package.py
+++ var/spack/repos/builtin/packages/r-pfam-db/package.py
@@ -13,12 +13,10 @@ class RPfamDb(RPackage):
     homepage = "https://www.bioconductor.org/packages/PFAM.db/"
     url      = "https://www.bioconductor.org/packages/3.5/data/annotation/src/contrib/PFAM.db_3.4.1.tar.gz"
 
-    version('3.8.2', sha256='3a951930f6caddfef4b61cb633b4c60c3c40ec268479c2bc1f3e5af67f56c166',
-            url='https://www.bioconductor.org/packages/3.9/data/annotation/src/contrib/PFAM.db_3.8.2.tar.gz')
+    version('3.10.0', sha256='038888f95ce69230ac0e0b08aa3bcb09965682415520d437a7fb0a031eefe158')
     version('3.4.1', sha256='fc45a0d53139daf85873f67bd3f1b68f2d883617f4447caddbd2d7dcc58a393f')
 
-    depends_on('r@3.4.0:3.4.9', when='@3.4.1')
-    depends_on('r@3.6.0:3.6.0', when='@3.8.2')
+    depends_on('r@2.7.0:', when='@3.4.1:', type=('build', 'run'))
+    depends_on('r-annotationdbi@1.37.4:', when='@3.4.1:', type=('build', 'run'))
 
-    depends_on('r-annotationdbi', type=('build', 'run'))
-    depends_on('r-annotationdbi@1.43.1:', when='@3.8.2:', type=('build', 'run'))
+    depends_on('r-annotationdbi@1.47.1:', when='@3.10.0:', type=('build', 'run'))
