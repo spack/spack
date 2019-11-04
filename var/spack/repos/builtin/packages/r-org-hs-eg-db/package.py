49de035ae31471860eb0b7efcc2accd9b58818e3
codeandkey <jtst@iastate.edu>
2019-11-04 12:03:36 -0600

    update r-org-hs-eg-db

--- var/spack/repos/builtin/packages/r-org-hs-eg-db/package.py
+++ var/spack/repos/builtin/packages/r-org-hs-eg-db/package.py
@@ -8,7 +8,7 @@ from spack import *
 
 class ROrgHsEgDb(RPackage):
     """Genome wide annotation for Human, primarily based on mapping
-    using Entrez Gene identifiers."""
+       using Entrez Gene identifiers."""
 
     homepage = "https://bioconductor.org/packages/org.Hs.eg.db/"
     url      = "https://www.bioconductor.org/packages/3.5/data/annotation/src/contrib/org.Hs.eg.db_3.4.1.tar.gz"
@@ -17,8 +17,7 @@ class ROrgHsEgDb(RPackage):
             url='https://www.bioconductor.org/packages/3.9/data/annotation/src/contrib/org.Hs.eg.db_3.8.2.tar.gz')
     version('3.4.1', sha256='0f87b3f1925a1d7007e5ad9200bdf511788bd1d7cb76f1121feeb109889c2b00')
 
-    depends_on('r@3.6.0:3.6.9', when='@3.8.2')
-    depends_on('r@3.4.0:3.4.9', when='@3.4.1')
+    depends_on('r@2.7.0:', when='@3.4.1:', type=('build', 'run'))
 
-    depends_on('r-annotationdbi', type=('build', 'run'))
-    depends_on('r-annotationdbi@1.43.1:', when='@3.8.2', type=('build', 'run'))
+    depends_on('r-annotationdbi@1.37.4:', when='@3.4.1:', type=('build', 'run'))
+    depends_on('r-annotationdbi@1.43.1:', when='@3.8.2:', type=('build', 'run'))
