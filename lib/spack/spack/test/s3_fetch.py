2f4f2c06f8171d2e6d7905403fc1c55599c4c502
Omar Padron <omar.padron@kitware.com>
2019-11-04 12:07:59 -0500

    update s3_fetch tests

--- lib/spack/spack/test/s3_fetch.py
+++ lib/spack/spack/test/s3_fetch.py
@@ -5,14 +5,14 @@
 
 import pytest
 
-from spack.fetch_strategy import S3FetchStrategy
-from spack.stage import Stage
+import spack.fetch_strategy as spack_fs
+import spack.stage as spack_stage
 
 
 def test_s3fetchstrategy_sans_url():
     """Ensure constructor with no URL fails."""
     with pytest.raises(ValueError):
-        with S3FetchStrategy(None):
+        with spack_fs.S3FetchStrategy(None):
             pass
 
 
@@ -20,11 +20,11 @@ def test_s3fetchstrategy_bad_url(tmpdir):
     """Ensure fetch with bad URL fails as expected."""
     testpath = str(tmpdir)
 
-    with pytest.raises(ValueError):
-        fetcher = S3FetchStrategy(url='file:///does-not-exist')
+    with pytest.raises(spack_fs.FetchError):
+        fetcher = spack_fs.S3FetchStrategy(url='file:///does-not-exist')
         assert fetcher is not None
 
-        with Stage(fetcher, path=testpath) as stage:
+        with spack_stage.Stage(fetcher, path=testpath) as stage:
             assert stage is not None
             assert fetcher.archive_file is None
             fetcher.fetch()
