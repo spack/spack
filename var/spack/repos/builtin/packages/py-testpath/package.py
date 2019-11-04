e575286d2ec9d2add43f5173e1cd94f4754fe629
Andreas Baumbach <healther.astro@gmail.com>
2019-11-04 20:33:25 +0100

    Remove the unnecessary pip dependency

--- var/spack/repos/builtin/packages/py-testpath/package.py
+++ var/spack/repos/builtin/packages/py-testpath/package.py
@@ -14,9 +14,3 @@ class PyTestpath(PythonPackage):
     url      = "https://pypi.io/packages/source/t/testpath/testpath-0.4.2.tar.gz"
 
     version('0.4.2', sha256='b694b3d9288dbd81685c5d2e7140b81365d46c29f5db4bc659de5aa6b98780f8')
-
-    depends_on('py-pip', type='build')
-
-    def install(self, spec, prefix):
-        pip = which('pip')
-        pip('install', '.', '--prefix={}'.format(prefix))
