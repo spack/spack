382599ebdcdc97fc6bfcc0533471fc5626a1bde1
Marc Mengel <marc.mengel@gmail.com>
2019-11-05 16:46:53 -0600

    Update var/spack/repos/builtin/packages/py-cherrypy/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-cherrypy/package.py
+++ var/spack/repos/builtin/packages/py-cherrypy/package.py
@@ -5,7 +5,7 @@ class PyCherrypy(PythonPackage):
     """CherryPy is a pythonic, object-oriented HTTP framework."""
 
     homepage = "https://cherrypy.org/"
-    url      = "https://files.pythonhosted.org/packages/1f/de/3327bd7168be762180924085fecef2e127d128f1d6157f88cd87fdac2971/CherryPy-18.1.1.tar.gz"
+    url      = "https://pypi.io/packages/source/C/CherryPy/CherryPy-18.1.1.tar.gz"
 
     version('18.1.1', '76e5d3c6b7be845345f871c604cfdf58')
 
