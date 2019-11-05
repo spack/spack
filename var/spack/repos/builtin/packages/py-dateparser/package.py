69ae5f198552c4a8fedff5bd121cadbadc7e2530
Andreas Baumbach <healther.astro@gmail.com>
2019-11-05 08:50:15 +0100

    take setup.py dependencies into account

--- var/spack/repos/builtin/packages/py-dateparser/package.py
+++ var/spack/repos/builtin/packages/py-dateparser/package.py
@@ -14,11 +14,20 @@ class PyDateparser(PythonPackage):
 
     version('0.7.2', sha256='e1eac8ef28de69a554d5fcdb60b172d526d61924b1a40afbbb08df459a36006b')
 
+    variant('calendars', default=True, help='Add calendar libraries')
+
+    depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
-    depends_on('py-convertdate@2.1.3:', type=('build', 'run'))
-    depends_on('py-jdatetime@3.1.0:', type=('build', 'run'))
+    depends_on('py-convertdate@2.1.3:', type=('build', 'run'), when='+calendars')
+    depends_on('py-jdatetime@3.1.0:', type=('build', 'run'), when='+calendars')
     depends_on('py-python-dateutil@2.7.5:', type=('build', 'run'))
     depends_on('py-pytz@2018.9:', type=('build', 'run'))
     depends_on('py-regex@2019.01.24:', type=('build', 'run'))
     depends_on('py-tzlocal@1.5.1:', type=('build', 'run'))
-    depends_on('py-umalqurra@0.2:', type=('build', 'run'))
+    depends_on('py-umalqurra@0.2:', type=('build', 'run'), when='+calendars')
+    depends_on('py-ruamel-yaml', type=('build', 'run'), when='+calendars')
+    depends_on('py-mock', type='test')
+    depends_on('py-nose', type='test')
+    depends_on('py-parameterized', type='test')
+    depends_on('py-six', type='test')
+    depends_on('py-coverage', type='test')
