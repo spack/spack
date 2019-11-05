spack/spack

commit cc4094bb9b6b228ef9c3821e9e76361df50b7f94

bugfix: env.write() should stringify Spec lists.
- Setting specs from lockfiles was not correctly stringifying concretized
  user specs.

- Fix `_set_user_specs_from_lockfile`

- Add some validation code to `SpecList` constructor

lib/spack/spack/environment.py
@@ -600,8 +600,8 @@ class Environment(object):
         """Copy user_specs from a read-in lockfile."""
         self.spec_lists = {
             user_speclist_name: SpecList(
-                user_speclist_name, [Spec(s)
-                                     for s in self.concretized_user_specs]
+                user_speclist_name,
+                [str(s) for s in self.concretized_user_specs]
             )
         }
 

lib/spack/spack/spec_list.py
@@ -28,6 +28,12 @@ class SpecList(object):
         self.name = name
         self._reference = reference  # TODO: Do we need defensive copy here?
 
+        # Validate yaml_list before assigning
+        if not all(isinstance(s, string_types) or isinstance(s, (list, dict))
+                   for s in yaml_list):
+            raise ValueError(
+                "yaml_list can contain only valid YAML types!  Found:\n  %s"
+                % [type(s) for s in yaml_list])
         self.yaml_list = yaml_list[:]
 
         # Expansions can be expensive to compute and difficult to keep updated

commit 2c504e8cdbe12be35cea3c345c17d55b002ad324

bugfix: uninstall should find concrete specs by DAG hash
This fixes a regression introduced in #10792.  `spack uninstall` in an
environment would not match concrete query specs properly after the index
hash of enviroments changed.

- [x] Search by DAG hash for specs to remove instead of by build hash

lib/spack/spack/environment.py
@@ -833,8 +833,16 @@ class Environment(object):
 
         if not matches:
             # concrete specs match against concrete specs in the env
+            # by *dag hash*, not build hash.
+            dag_hashes_in_order = [
+                self.specs_by_hash[build_hash].dag_hash()
+                for build_hash in self.concretized_order
+            ]
+
             specs_hashes = zip(
-                self.concretized_user_specs, self.concretized_order)
+                self.concretized_user_specs, dag_hashes_in_order
+            )
+
             matches = [
                 s for s, h in specs_hashes
                 if query_spec.dag_hash() == h