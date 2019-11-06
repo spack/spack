81352799d5eaebb30463e15fd1308f2f5d049ce8
Marc Mengel <marc.mengel@gmail.com>
2019-11-06 14:07:10 -0600

    Update var/spack/repos/builtin/packages/py-zc-buildout/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-zc-buildout/package.py
+++ var/spack/repos/builtin/packages/py-zc-buildout/package.py
@@ -5,7 +5,7 @@ class PyZcBuildout(PythonPackage):
     """System for managing development buildouts"""
 
     homepage = "http://buildout.org/"
-    url      = "https://files.pythonhosted.org/packages/3f/92/bf6ddecce944e3dcebfd8af4efef414a7131276b1433afb4f842012ed5ca/zc.buildout-2.13.1.tar.gz"
+    url      = "https://pypi.io/packages/source/z/zc.buildout/zc.buildout-2.13.1.tar.gz"
 
     version('2.13.1', sha256='3d14d07226963a517295dfad5879d2799e2e3b65b2c61c71b53cb80f5ab11484')
 
