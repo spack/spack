60a28788acb2fd6e3919347078783faa3eeedad8
Marc Mengel <marc.mengel@gmail.com>
2019-11-06 14:56:34 -0600

    Update var/spack/repos/builtin/packages/py-tempora/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-tempora/package.py
+++ var/spack/repos/builtin/packages/py-tempora/package.py
@@ -13,5 +13,5 @@ class PyTempora(PythonPackage):
     depends_on('py-setuptools-scm', type='build')
     depends_on('py-six', type=('build', 'run'))
     depends_on('py-pytz', type=('build', 'run'))
-    depends_on('py-jaraco-functools@1.20', type=('build', 'run'))
+    depends_on('py-jaraco-functools@1.20:', type=('build', 'run'))
     depends_on('python@2.7', type=('build', 'run'))
