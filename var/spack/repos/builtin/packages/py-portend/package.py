f4c1ca54008973090db127b457daddef6d3035d4
Marc Mengel <mengel@fnal.gov>
2019-11-06 13:38:14 -0600

    dependency in wrong package

--- var/spack/repos/builtin/packages/py-portend/package.py
+++ var/spack/repos/builtin/packages/py-portend/package.py
@@ -11,3 +11,4 @@ class PyPortend(PythonPackage):
         '2.5', sha256='19dc27bfb3c72471bd30a235a4d5fbefef8a7e31cab367744b5d87a205e7bfd9')
 
     depends_on('py-setuptools', type='build')
+    depends_on('portend', type=('run','build'))
