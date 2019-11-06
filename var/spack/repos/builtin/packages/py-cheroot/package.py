0fef5cba9cc8ef21d2fa42549666cf53309e26dd
Marc Mengel <mengel@fnal.gov>
2019-11-06 14:16:20 -0600

    flake8

--- var/spack/repos/builtin/packages/py-cheroot/package.py
+++ var/spack/repos/builtin/packages/py-cheroot/package.py
@@ -14,7 +14,6 @@ class PyCheroot(PythonPackage):
     depends_on('py-setuptools', type='build')
     depends_on('py-setuptools-scm@1.15.0', type='build')
     depends_on('py-setuptools-git@1.0', type='build')
-    depends_on('py-more-itertools@2.6', type=('build','run'))
-    depends_on('py-six@1.11.0', type=('build','run'))
-    depends_on('py-backports-functools-lru-cache', type=('build','run'))
-
+    depends_on('py-more-itertools@2.6', type=('build', 'run'))
+    depends_on('py-six@1.11.0', type=('build', 'run'))
+    depends_on('py-backports-functools-lru-cache', type=('build', 'run'))
