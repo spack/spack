cb9db0a770a4a856f21a3b802502055e150b392f
Andreas Baumbach <healther.astro@gmail.com>
2019-11-06 18:43:17 +0100

    drop kanji, add variant, and restrict to future releases

--- var/spack/repos/builtin/packages/py-tatsu/package.py
+++ var/spack/repos/builtin/packages/py-tatsu/package.py
@@ -7,7 +7,7 @@ from spack import *
 
 
 class PyTatsu(PythonPackage):
-    """竜 TatSu (the successor to Grako) is a tool that takes grammars in
+    """TatSu (the successor to Grako) is a tool that takes grammars in
     a variation of EBNF as input, and outputs memoizing (Packrat) PEG
     parsers in Python."""
 
@@ -16,10 +16,12 @@ class PyTatsu(PythonPackage):
 
     version('4.4.0', sha256='80713413473a009f2081148d0f494884cabaf9d6866b71f2a68a92b6442f343d')
 
-    depends_on('python@3.6:', type=('build', 'run'))
+    variant('future_regex', default=True, description='Use regex implementation')
+
+    depends_on('python@3.6:', type=('build', 'run'), when='@4.5:')
     depends_on('py-setuptools', type='build')
-    # part of the standard lib in python@3.7.0
-    depends_on('py-dataclasses@0.6:', type=('build', 'run'), when='^python@3.6.0:3.6.99')
-    depends_on('py-mypy@0.641:', type='test')
+    # part of the standard lib in python@3.7.0, required in the current HEAD
+    depends_on('py-dataclasses@0.6:', type=('build', 'run'), when='@4.5:^python@:3.6.99')
+    depends_on('py-pytest-mypy', type='test')
     # optional dependency, otherwise falls back to standard implementation
-    depends_on('py-regex@2018.8:', type=('build', 'run'))
+    depends_on('py-regex@2018.8:', type=('build', 'run'), when='+future_regex')
