3e81fc713940d2b7fb22193bc1f5ab3590de711e
Marc Mengel <mengel@fnal.gov>
2019-11-06 14:04:34 -0600

    setuptools_scm bits

--- var/spack/repos/builtin/packages/py-cherrypy/package.py
+++ var/spack/repos/builtin/packages/py-cherrypy/package.py
@@ -9,9 +9,11 @@ class PyCherrypy(PythonPackage):
 
     version('18.1.1', '76e5d3c6b7be845345f871c604cfdf58')
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-more-itertools',    type=('build', 'run'))
-    depends_on('py-zc-buildout',       type=('build', 'run'))
-    depends_on('py-zc-lockfile',       type=('build', 'run'))
+    depends_on('py-setuptools',     type='build')
+    depends_on('py-setuptools-scm', type='build')
+    depends_on('py-more-itertools', type=('build', 'run'))
+    depends_on('py-zc-buildout',    type=('build', 'run'))
+    depends_on('py-zc-lockfile',    type=('build', 'run'))
     depends_on('py-cheroot@6.2.4:', type=('build', 'run'))
     depends_on('py-portend@2.1.1:', type=('build', 'run'))
+    depends_on('python@3.5:',       when='@18.0.0:',  type=('build', 'run'))
