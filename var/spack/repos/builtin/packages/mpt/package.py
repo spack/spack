a20919c803b54dc18f7510323048dfb8a35e0551
Adrian Jackson <adrianj@epcc.ed.ac.uk>
2019-11-06 19:39:04 +0000

    liniting

--- var/spack/repos/builtin/packages/mpt/package.py
+++ var/spack/repos/builtin/packages/mpt/package.py
@@ -7,22 +7,23 @@ from spack import *
 
 
 class Mpt(Package):
-    """HPE MPI is HPE's implementation of 
+    """HPE MPI is HPE's implementation of
     the Message Passing Interface (MPI) standard.
- 
+
     Note: HPE MPI is proprietry software. Spack will search your
     current directory for the download file. Alternatively, add this file to a
     mirror so that Spack can find it. For instructions on how to set up a
     mirror, see http://spack.readthedocs.io/en/latest/mirrors.html"""
 
-    homepage = ""
+    homepage = "https://buy.hpe.com/us/en/software/high-performance-computing-software/hpe-message-passing-interface-mpi/p/1010144155"
 
     provides('mpi')
     provides('mpi@:3.1', when='@3:')
     provides('mpi@:1.3', when='@1:')
 
     filter_compiler_wrappers(
-        'mpicc', 'mpicxx', 'mpif77', 'mpif90', 'mpif77', 'mpif08', relative_root='bin'
+        'mpicc','mpicxx','mpif77','mpif90','mpif77','mpif08',
+        relative_root='bin'
     )
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
@@ -52,7 +53,7 @@ class Mpt(Package):
         msg = """This package is a placeholder for HPE MPI, a
         system-provided, proprietary MPI implementation.
 
-        Add to your packages.yaml (changing the /opt/ path to match 
+        Add to your packages.yaml (changing the /opt/ path to match
         where HPE MPI is actually installed):
 
         packages:
