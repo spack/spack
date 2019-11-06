0b22a94e927a771dc42e95ad62c44289a4dc920f
Andreas Baumbach <healther@users.noreply.github.com>
2019-11-06 20:26:25 +0100

    Update var/spack/repos/builtin/packages/py-pyarrow/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-pyarrow/package.py
+++ var/spack/repos/builtin/packages/py-pyarrow/package.py
@@ -31,7 +31,7 @@ class PyPyarrow(PythonPackage):
     depends_on('py-pytest', type='test', when='@0.15.0:')
     depends_on('py-pandas', type='test', when='@0.15.0:')
     depends_on('py-hypothesis', type='test', when='@0.15.0:')
-    depends_on('py-pathlib2', type='test', when='@0.15.0^python@:3.3.99')
+    depends_on('py-pathlib2', type='test', when='@0.15.0: ^python@:3.3.99')
     depends_on('py-numpy@1.14:', type=('build', 'run'), when='@0.15.0:')
     depends_on('py-six@1.0.0:', type=('build', 'run'), when='@0.15.0:')
     depends_on('py-futures', type=('build', 'run'), when='@0.15.0:^python@:3.1.99')
