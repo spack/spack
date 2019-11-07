718e94f702456ce1803b32d9def047656614a1bf
Cyrus Harrison <cyrush@llnl.gov>
2019-11-06 20:49:52 -0800

    fix metis src dl url

--- var/spack/repos/builtin/packages/metis/package.py
+++ var/spack/repos/builtin/packages/metis/package.py
@@ -18,7 +18,7 @@ class Metis(Package):
        partitioning schemes."""
 
     homepage = "http://glaros.dtc.umn.edu/gkhome/metis/metis/overview"
-    url      = "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz"
+    url      = "https://github.com/scivision/METIS/raw/master/metis-5.1.0.tar.gz"
     list_url = "http://glaros.dtc.umn.edu/gkhome/fsroot/sw/metis/OLD"
 
     version('5.1.0', sha256='76faebe03f6c963127dbb73c13eab58c9a3faeae48779f049066a21c087c5db2')
