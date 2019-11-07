31be0078848c072e17a62c34d11ed52f9cf27a28
Marc Mengel <mengel@fnal.gov>
2019-11-07 16:49:02 -0600

    license bits

--- var/spack/repos/builtin/packages/py-portend/package.py
+++ var/spack/repos/builtin/packages/py-portend/package.py
@@ -1,3 +1,8 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 from spack import *
 
 
@@ -11,4 +16,4 @@ class PyPortend(PythonPackage):
         '2.5', sha256='19dc27bfb3c72471bd30a235a4d5fbefef8a7e31cab367744b5d87a205e7bfd9')
 
     depends_on('py-setuptools', type='build')
-    depends_on('portend', type=('run','build'))
+    depends_on('portend', type=('run', 'build'))
