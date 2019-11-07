ee0aabe9d451074ec4b3512c8e84d93f311a2c92
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-11-06 19:24:49 -0800

    Update package.py
    - cleanup
    - python deps specify type
    - python 2.6 compatibility
    - double quotes to single quotes

--- var/spack/repos/builtin/packages/timemory/package.py
+++ var/spack/repos/builtin/packages/timemory/package.py
@@ -4,72 +4,54 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 #
 # ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install timemory
-#
-# You can edit this file again by typing:
-#
-#     spack edit timemory
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
-import os
 
 
 class Timemory(CMakePackage):
     """Timing + Memory + Hardware Counter Utilities for C/C++/CUDA/Python"""
 
-    homepage = "https://timemory.readthedocs.io/en/latest/"
-    git = "https://github.com/NERSC/timemory.git"
+    homepage = 'https://timemory.readthedocs.io/en/latest/'
+    git = 'https://github.com/NERSC/timemory.git'
 
     version('master', branch='master', submodules=True)
 
-    variant('python', default=True, description="Enable Python support")
-    variant('mpi', default=False, description="Enable MPI support")
-    variant('papi', default=True, description="Enable PAPI support")
-    variant('cuda', default=True, description="Enable CUDA support")
-    variant('caliper', default=True, description="Enable Caliper support")
-    variant('gperftools', default=True, description="Enable gperftools support")
-    variant('cupti', default=True, description="Enable CUPTI support")
+    variant('python', default=True, description='Enable Python support')
+    variant('mpi', default=False, description='Enable MPI support')
+    variant('papi', default=True, description='Enable PAPI support')
+    variant('cuda', default=True, description='Enable CUDA support')
+    variant('cupti', default=True, description='Enable CUPTI support')
+    variant('caliper', default=True, description='Enable Caliper support')
+    variant('gperftools', default=True, description='Enable gperftools support')
 
-    extends('python', when="+python")
-    depends_on('python@3:', when="+python", type=('build', 'run'))
-    depends_on('py-pip', when='+python')
+    extends('python', when='+python')
+    depends_on('python@3:', when='+python', type=('build', 'run'))
+    depends_on('py-numpy', when='+python', type=('run'))
+    depends_on('py-pillow', when='+python', type=('run'))
+    depends_on('py-matplotlib', when='+python', type=('run'))
     depends_on('mpi', when='+mpi')
     depends_on('papi', when='+papi')
     depends_on('cuda', when='+cuda')
-    depends_on('gperftools', when='+gperftools')
     depends_on('caliper', when='+caliper')
     depends_on('gperftools', when='+gperftools')
-    depends_on('py-numpy', when='+python')
-    depends_on('py-pillow', when='+python')
-    depends_on('py-matplotlib', when='+python')
 
     def cmake_args(self):
         spec = self.spec
 
         # Use spack install of Caliper instead of internal build
         args = [
-            "-DTIMEMORY_BUILD_CALIPER=OFF",
-            "-DTIMEMORY_BUILD_TOOLS=ON",
-            "-DTIMEMORY_BUILD_EXTRA_OPTIMIZATIONS=ON",
-            "-DTIMEMORY_BUILD_GTEST=OFF",
-            "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON",
+            '-DTIMEMORY_BUILD_CALIPER=OFF',
+            '-DTIMEMORY_BUILD_TOOLS=ON',
+            '-DTIMEMORY_BUILD_EXTRA_OPTIMIZATIONS=ON',
+            '-DTIMEMORY_BUILD_GTEST=OFF',
+            '-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON',
         ]
 
         if '+python' in spec:
             args.append('-DPYTHON_EXECUTABLE={0}'.format(
                 spec['python'].command.path))
             args.append('-DTIMEMORY_BUILD_PYTHON=ON')
-            args.append("-DTIMEMORY_TLS_MODEL='global-dynamic'")
+            args.append('-DTIMEMORY_TLS_MODEL=global-dynamic')
         else:
             args.append('-DTIMEMORY_BUILD_PYTHON=OFF')
 
@@ -85,8 +67,8 @@ class Timemory(CMakePackage):
             args.append('-DTIMEMORY_USE_PAPI=OFF')
 
         if '+mpi' in spec:
-            args.append('-DMPI_C_COMPILER={}'.format(spec['mpi'].mpicc))
-            args.append('-DMPI_CXX_COMPILER={}'.format(spec['mpi'].mpicxx))
+            args.append('-DMPI_C_COMPILER={0}'.format(spec['mpi'].mpicc))
+            args.append('-DMPI_CXX_COMPILER={0}'.format(spec['mpi'].mpicxx))
         else:
             args.append('-DTIMEMORY_USE_MPI=OFF')
 
