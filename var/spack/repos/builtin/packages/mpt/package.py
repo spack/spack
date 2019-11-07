07e1751aacf56ab0411f18dcc3a52661c8980762
Adrian Jackson <adrianj@epcc.ed.ac.uk>
2019-11-07 11:06:12 +0000

    Investigating api errors

--- var/spack/repos/builtin/packages/mpt/package.py
+++ var/spack/repos/builtin/packages/mpt/package.py
@@ -21,25 +21,25 @@ class Mpt(Package):
     provides('mpi@:3.1', when='@3:')
     provides('mpi@:1.3', when='@1:')
 
-    filter_compiler_wrappers(
-        'mpicc', 'mpicxx', 'mpif77', 'mpif90', 'mpif77', 'mpif08',
-        relative_root='bin'
-    )
+#    filter_compiler_wrappers(
+#        'mpicc', 'mpicxx', 'mpif77', 'mpif90', 'mpif77', 'mpif08',
+#        relative_root='bin'
+#    )
 
-    @property
-    def libs(self):
-        query_parameters = self.spec.last_query.extra_parameters
-        libraries = ['libmpi']
+#    @property
+#    def libs(self):
+#        query_parameters = self.spec.last_query.extra_parameters
+#        libraries = ['libmpi']
+#
+#        if 'cxx' in query_parameters:
+#            libraries = ['libmpicxx'] + libraries
+#
+#        return find_libraries(
+#            libraries, root=self.prefix, shared=True, recursive=True
+#        )
 
-        if 'cxx' in query_parameters:
-            libraries = ['libmpicxx'] + libraries
-
-        return find_libraries(
-            libraries, root=self.prefix, shared=True, recursive=True
-        )
-
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        print("testing")
+#    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+#        print("testing")
 #        spack_env.set('MPICC',  self.prefix.bin.mpicc)
 #        spack_env.set('MPICXX', self.prefix.bin.mpicxx)
 #        spack_env.set('MPIF77', self.prefix.bin.mpif77)
@@ -48,31 +48,31 @@ class Mpt(Package):
 #        spack_env.set('MPICXX_CXX', spack_cxx)
 #        spack_env.set('MPIF90_F90', spack_fc)
 
-    def setup_dependent_package(self, module, dependent_spec):
-        if 'platform=cray' in self.spec:
-            self.spec.mpicc = spack_cc
-            self.spec.mpicxx = spack_cxx
-            self.spec.mpifc = spack_fc
-            self.spec.mpif77 = spack_f77
-        else:
-            self.spec.mpicc = self.prefix.bin.mpicc
-            self.spec.mpicxx = self.prefix.bin.mpicxx
-            self.spec.mpifc = self.prefix.bin.mpifc
-            self.spec.mpif77 = self.prefix.bin.mpif77
+#    def setup_dependent_package(self, module, dependent_spec):
+#        if 'platform=cray' in self.spec:
+#            self.spec.mpicc = spack_cc
+#            self.spec.mpicxx = spack_cxx
+#            self.spec.mpifc = spack_fc
+#            self.spec.mpif77 = spack_f77
+#        else:
+#            self.spec.mpicc = self.prefix.bin.mpicc
+#            self.spec.mpicxx = self.prefix.bin.mpicxx
+#            self.spec.mpifc = self.prefix.bin.mpifc
+#            self.spec.mpif77 = self.prefix.bin.mpif77
 
-    @property
-    def fetcher(self):
-        msg = """This package is a placeholder for HPE MPI, a
-        system-provided, proprietary MPI implementation.
-
-        Add to your packages.yaml (changing the /opt/ path to match
-        where HPE MPI is actually installed):
-
-        packages:
-          mpt:
-            paths:
-              mpt@2.20: /opt/
-            buildable: False
-
-        """
-        raise InstallError(msg)
+#    @property
+#    def fetcher(self):
+#        msg = """This package is a placeholder for HPE MPI, a
+#        system-provided, proprietary MPI implementation.
+#
+#        Add to your packages.yaml (changing the /opt/ path to match
+#        where HPE MPI is actually installed):
+#
+#        packages:
+#          mpt:
+#            paths:
+#              mpt@2.20: /opt/
+#            buildable: False
+#
+#        """
+#        raise InstallError(msg)
