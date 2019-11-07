bfea36f98d354467555d073ec4b888a6bfd18584
Adrian Jackson <adrianj@epcc.ed.ac.uk>
2019-11-07 16:22:57 +0000

    Investigating api errors: adding back in functions to see when the build fails

--- var/spack/repos/builtin/packages/mpt/package.py
+++ var/spack/repos/builtin/packages/mpt/package.py
@@ -21,10 +21,10 @@ class Mpt(Package):
     provides('mpi@:3.1', when='@3:')
     provides('mpi@:1.3', when='@1:')
 
-#    filter_compiler_wrappers(
-#        'mpicc', 'mpicxx', 'mpif77', 'mpif90', 'mpif77', 'mpif08',
-#        relative_root='bin'
-#    )
+    filter_compiler_wrappers(
+        'mpicc', 'mpicxx', 'mpif77', 'mpif90', 'mpif77', 'mpif08',
+        relative_root='bin'
+    )
 
     @property
     def libs(self):
@@ -38,15 +38,14 @@ class Mpt(Package):
             libraries, root=self.prefix, shared=True, recursive=True
         )
 
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        print("testing")
-#        spack_env.set('MPICC',  self.prefix.bin.mpicc)
-#        spack_env.set('MPICXX', self.prefix.bin.mpicxx)
-#        spack_env.set('MPIF77', self.prefix.bin.mpif77)
-#        spack_env.set('MPIF90', self.prefix.bin.mpifc)
-#        spack_env.set('MPICC_CC', spack_cc)
-#        spack_env.set('MPICXX_CXX', spack_cxx)
-#        spack_env.set('MPIF90_F90', spack_fc)
+    def setup_dependent_build_environment(self, spack_env, run_env, dependent_spec):
+        spack_env.set('MPICC',  self.prefix.bin.mpicc)
+        spack_env.set('MPICXX', self.prefix.bin.mpicxx)
+        spack_env.set('MPIF77', self.prefix.bin.mpif77)
+        spack_env.set('MPIF90', self.prefix.bin.mpifc)
+        spack_env.set('MPICC_CC', spack_cc)
+        spack_env.set('MPICXX_CXX', spack_cxx)
+        spack_env.set('MPIF90_F90', spack_fc)
 
     def setup_dependent_package(self, module, dependent_spec):
         if 'platform=cray' in self.spec:
