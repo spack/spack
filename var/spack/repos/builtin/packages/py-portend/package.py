a17576725e3f5e7fbb5882ec0451a778af895e71
Marc Mengel <mengel@fnal.gov>
2019-11-08 12:49:52 -0600

    dependencies again

--- var/spack/repos/builtin/packages/py-portend/package.py
+++ var/spack/repos/builtin/packages/py-portend/package.py
@@ -16,4 +16,6 @@ class PyPortend(PythonPackage):
         '2.5', sha256='19dc27bfb3c72471bd30a235a4d5fbefef8a7e31cab367744b5d87a205e7bfd9')
 
     depends_on('py-setuptools', type='build')
-    depends_on('portend', type=('run', 'build'))
+    depends_on('py-setuptools-scm@1.15.0:', type='build')
+    depends_on('py-tempora@1.8:', type=('run', 'build'))
+    depends_on('python@2.7:', type=('run', 'build'))
