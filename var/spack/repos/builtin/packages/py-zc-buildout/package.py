4c6e744a1885a541cd03bbcbc705c825a76c7af6
Marc Mengel <marc.mengel@gmail.com>
2019-11-08 12:55:09 -0600

    Update var/spack/repos/builtin/packages/py-zc-buildout/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-zc-buildout/package.py
+++ var/spack/repos/builtin/packages/py-zc-buildout/package.py
@@ -14,4 +14,4 @@ class PyZcBuildout(PythonPackage):
 
     version('2.13.1', sha256='3d14d07226963a517295dfad5879d2799e2e3b65b2c61c71b53cb80f5ab11484')
 
-    depends_on('py-setuptools@8.0:', type=('build', 'install'))
+    depends_on('py-setuptools@8.0:', type=('build', 'run'))
