80fe65ac4928ced375c94f7023338098411c9614
Roman Briskine <roman.briskine@gmail.com>
2019-11-11 15:55:10 +0100

    Add licensing; replace url with url_for_version; create .spack dir during installation; symlink wolframscript; generate spec.yaml if missing

--- var/spack/repos/builtin/packages/mathematica/package.py
+++ var/spack/repos/builtin/packages/mathematica/package.py
@@ -18,10 +18,18 @@ class Mathematica(Package):
        http://spack.readthedocs.io/en/latest/mirrors.html"""
 
     homepage = "https://www.wolfram.com/mathematica/"
-    url = 'file://{0}/Mathematica_12.0.0_LINUX.sh'.format(os.getcwd())
 
     version('12.0.0', sha256='b9fb71e1afcc1d72c200196ffa434512d208fa2920e207878433f504e58ae9d7',
             expand=False)
+
+    # Licensing
+    license_required = True
+    license_comment  = '#'
+    license_files    = ['Configuration/Licensing/mathpass']
+    license_url      = 'https://reference.wolfram.com/language/tutorial/RegistrationAndPasswords.html#857035062'
+
+    def url_for_version(self, version):
+        return "file://{0}/Mathematica_{1}_LINUX.sh".format(os.getcwd(), version)
 
     def install(self, spec, prefix):
         sh = which('sh')
@@ -29,3 +37,19 @@ class Mathematica(Package):
            '-targetdir={0}'.format(prefix),
            '-execdir={0}'.format(prefix.bin),
            '-selinux=y')
+        # .spack directory in the install directory may not exist and needs to be created
+        install_spack_dir = os.path.dirname(self.install_log_path)
+        os.makedirs(install_spack_dir, exist_ok=True)
+        # This is what most people would use on a cluster but the installer does not symlink it
+        ws_link_path = os.path.join(prefix.bin, 'wolframscript')
+        if not os.path.exists(ws_link_path):
+            ln = which('ln')
+            ws_path = os.path.join(prefix, 'Executables', 'wolframscript')
+            ln('-s', ws_path, ws_link_path)
+        # spec.yaml may not be generated automatically but required for successful registration of
+        # the package
+        spec_path = os.path.join(install_spack_dir, 'spec.yaml')
+        if not os.path.exists(spec_path):
+            with open(spec_path, 'w') as f:
+                spec.to_yaml(f)
+
