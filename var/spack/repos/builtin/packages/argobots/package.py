991eed7c73a1329ef47800e36a425076979e54bf
Shintaro Iwasaki <siwasaki@anl.gov>
2019-11-21 00:13:36 +0000

    argobots: use "master" for the master branch

--- var/spack/repos/builtin/packages/argobots/package.py
+++ var/spack/repos/builtin/packages/argobots/package.py
@@ -20,7 +20,7 @@ class Argobots(AutotoolsPackage):
     git      = "https://github.com/pmodels/argobots.git"
     maintainers = ['shintaro-iwasaki']
 
-    version("develop", branch="master")
+    version("master", branch="master")
     version("1.0rc2", sha256="7496b8bd39930a548b01aa3b1fe8f8b582c272600ef6a05ddc4398cf21dc12a2")
     version("1.0rc1", sha256="2dc4487556dce602655a6535f501136f0edc3575708029c80b1af6dccd069ce7")
     version("1.0b1", sha256="480b85b0e8db288400088a57c2dc5639f556843b06b0492841920c38348a2a3e")
