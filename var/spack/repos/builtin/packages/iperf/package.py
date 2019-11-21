ff5e4a652a274eef72d71ae805b3a79ad62de310
darmac <darmac@163.com>
2019-11-21 11:41:57 +0800

    Update package.py
    * remove 'FIXME' messages
    * remove unused configure_args()

--- var/spack/repos/builtin/packages/iperf/package.py
+++ var/spack/repos/builtin/packages/iperf/package.py
@@ -3,25 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install iperf
-#
-# You can edit this file again by typing:
-#
-#     spack edit iperf
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
-
 
 class Iperf(AutotoolsPackage):
     """
@@ -39,7 +21,3 @@ class Iperf(AutotoolsPackage):
     url      = "https://sourceforge.net/projects/iperf2/files/iperf-2.0.13.tar.gz/download"
 
     version('2.0.13', sha256='c88adec966096a81136dda91b4bd19c27aae06df4d45a7f547a8e50d723778ad')
-
-    def configure_args(self):
-        args = []
-        return args
