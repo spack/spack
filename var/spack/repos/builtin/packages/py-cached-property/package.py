9605918daad7bbb084560896e05a3bdb0eaabd6e
Joseph M. Koning <koning1@llnl.gov>
2019-11-22 12:46:25 -0800

    Change md5 hash to sha256.

--- var/spack/repos/builtin/packages/py-cached-property/package.py
+++ var/spack/repos/builtin/packages/py-cached-property/package.py
@@ -12,6 +12,6 @@ class PyCachedProperty(PythonPackage):
     homepage = "https://pypi.org/project/cached-property/"
     url      = "https://pypi.io/packages/source/c/cached-property/cached-property-1.5.1.tar.gz"
 
-    version('1.5.1', '4b6f3cd429da5f487f4ebf3242bb991f')
+    version('1.5.1', sha256="9217a59f14a5682da7c4b8829deadbfc194ac22e9908ccf7c8820234e80a1504")
 
     depends_on('py-setuptools', type='build')
