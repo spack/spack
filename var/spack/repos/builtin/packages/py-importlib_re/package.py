668915eefb5aad8dcd5b5d1f61d0e3f8b565d1ec
Joseph M. Koning <koning1@llnl.gov>
2019-11-22 09:23:20 -0800

    Add the py-importlib_re package

---
+++ var/spack/repos/builtin/packages/py-importlib_re/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyImportlibRe(PythonPackage):
+    """Read resources from Python packages"""
+
+    homepage = "https://pypi.org/project/importlib_resources/"
+    url      = "https://pypi.io/packages/source/i/importlib_resources/importlib_resources-1.0.2.tar.gz"
+
+    version('1.0.2', sha256='d3279fd0f6f847cced9f7acc19bd3e5df54d34f93a2e7bb5f238f81545787078')
+
+    depends_on('py-setuptools', type='build')
