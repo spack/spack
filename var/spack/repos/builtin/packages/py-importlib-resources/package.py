d10b01da5c915677ea6bff0f437c4cbf5f2923ec
Joseph Koning <joking@pobox.com>
2019-11-24 09:45:34 -0800

    Rename package to py-importlib-resources Add python depend modules for previous python versions and depends_on python.

--- var/spack/repos/builtin/packages/py-importlib-resources/package.py
+++ var/spack/repos/builtin/packages/py-importlib-resources/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class PyImportlibRe(PythonPackage):
+class PyImportlibResources(PythonPackage):
     """Read resources from Python packages"""
 
     homepage = "https://pypi.org/project/importlib_resources/"
@@ -14,4 +14,9 @@ class PyImportlibRe(PythonPackage):
 
     version('1.0.2', sha256='d3279fd0f6f847cced9f7acc19bd3e5df54d34f93a2e7bb5f238f81545787078')
 
+    depends_on('python@2.7:2.8,3.4:', type=('build','run'))
+
     depends_on('py-setuptools', type='build')
+    depends_on('py-wheel', type='build')
+    depends_on('py-pathlib2', when='^python@:2', type=('build','run'))
+    depends_on('py-typing', when='^python@:3.4', type=('build','run'))
