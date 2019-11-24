5784215586e94c41be271c38caec014fdfb68e51
Joseph Koning <joking@pobox.com>
2019-11-24 10:14:56 -0800

    Add whitespaces.

--- var/spack/repos/builtin/packages/py-importlib-resources/package.py
+++ var/spack/repos/builtin/packages/py-importlib-resources/package.py
@@ -14,9 +14,9 @@ class PyImportlibResources(PythonPackage):
 
     version('1.0.2', sha256='d3279fd0f6f847cced9f7acc19bd3e5df54d34f93a2e7bb5f238f81545787078')
 
-    depends_on('python@2.7:2.8,3.4:', type=('build','run'))
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
 
     depends_on('py-setuptools', type='build')
     depends_on('py-wheel', type='build')
-    depends_on('py-pathlib2', when='^python@:2', type=('build','run'))
-    depends_on('py-typing', when='^python@:3.4', type=('build','run'))
+    depends_on('py-pathlib2', when='^python@:2', type=('build', 'run'))
+    depends_on('py-typing', when='^python@:3.4', type=('build', 'run'))
