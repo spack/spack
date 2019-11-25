ffe2061878020cda74fd15ed9645c0522de4efbe
Joseph M. Koning <koning1@llnl.gov>
2019-11-25 10:49:20 -0800

    Change source url to use pypi.

--- var/spack/repos/builtin/packages/py-coloredlogs/package.py
+++ var/spack/repos/builtin/packages/py-coloredlogs/package.py
@@ -10,7 +10,7 @@ class PyColoredlogs(PythonPackage):
     """Colored terminal output for Python's logging module"""
 
     homepage = "https://pypi.org/project/coloredlogs/"
-    url      = "https://files.pythonhosted.org/packages/63/09/1da37a51b232eaf9707919123b2413662e95edd50bace5353a548910eb9d/coloredlogs-10.0.tar.gz"
+    url      = "https://pypi.io/packages/source/c/coloredlogs/coloredlogs-10.0.tar.gz"
 
     version('10.0', sha256='b869a2dda3fa88154b9dd850e27828d8755bfab5a838a1c97fbc850c6e377c36')
 
