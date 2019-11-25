6f33756ed1fce2442e1b74ac186027c71800a62e
darmac <darmac@163.com>
2019-11-25 14:31:32 +0800

    add new package : httperf@master

---
+++ var/spack/repos/builtin/packages/httperf/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Httperf(AutotoolsPackage):
+    """Httperf is a tool for measuring web server performance. It provides
+    a flexible facility for generating various HTTP workloads and for
+    measuring server performance."""
+
+    homepage = "https://github.com/httperf"
+    url      = "https://github.com/httperf/httperf/archive/master.zip"
+
+    version('master', '8a14602560914710a4caac56607d74ed82081ba0f666e3da903514eaac28c455')
+
+    depends_on('autoconf')
+    depends_on('automake')
+    depends_on('libtool')
+    depends_on('m4')
+
+    @run_before('autoreconf')
+    def e_autogen(self):
+        mkdirp('m4')
