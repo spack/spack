8fec47d925b35314814e9d2ab39c318712c67517
Joseph M. Koning <koning1@llnl.gov>
2019-11-25 08:10:55 -0800

    Add version for humanfriendly dep and build to type.

--- var/spack/repos/builtin/packages/py-coloredlogs/package.py
+++ var/spack/repos/builtin/packages/py-coloredlogs/package.py
@@ -15,4 +15,4 @@ class PyColoredlogs(PythonPackage):
     version('10.0', sha256='b869a2dda3fa88154b9dd850e27828d8755bfab5a838a1c97fbc850c6e377c36')
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-humanfriendly', type='run')
+    depends_on('py-humanfriendly@4.7:', type=('build', 'run'))
