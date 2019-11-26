718998d420657841082b9dd297740b688fe6fcfe
Rao Garimella <rao@abyzou.lanl.gov>
2019-11-25 18:45:24 -0700

    updates to avoid enable_tests variant; correct versioning

--- var/spack/repos/builtin/packages/mstk/package.py
+++ var/spack/repos/builtin/packages/mstk/package.py
@@ -24,28 +24,23 @@ class Mstk(CMakePackage):
     homepage = "https://github.com/MeshToolkit/MSTK"
     git      = "https://github.com/MeshToolkit/MSTK"
 
-    maintainers = ['raovgarimella','julienloiseau']
+    maintainers = ['raovgarimella', 'julienloiseau']
 
-    def url_for_version(self, version):
-        url = 'https://github.com/MeshToolkit/MSTK/archive/{0}.tar.gz'
-        return url.format(version)
-    
     version('master', branch='master')
-    version('v3_2_1', sha256='430bdf06826d47bd025dffe70165113505d2c6fbc6b3fd0d019a39c1a4e6ee5a')
-    version('v3_2_0', sha256='76a3bcdff132e586fddb39c60855c9201eaa587a6736b8b8bc762f66a4d0cb82')
-    version('v3_1_3', sha256='ce0a13e1c32ddf50787afbcab79b581e1f858d4fb6013846a4b3434ba613759c')
-    version('v3_1_2', sha256='f068e6d535122b5820a8a4563a2aab73d6a25c8d2db1a788cd1f3bb510de3574')
-    version('v3_1_1', sha256='1c161d2e1bfef4ac2ceb95d0e82265f0a32b654c9fd5ae117e2ce3aee0252393')
-    version('v3_1_0', sha256='975f9b8218675c80e7fe0691efe342cd0480cd90e9d3e8c78fe2e17a7ea992b9')
-    version('v3_0_4', sha256='ce9a5ccd60833ad2b74791a41e18a3bf200bfd4604baa3b284384a363873db8f')
-    version('v3_0_3', sha256='9cf5398e4beac587457c8a7d1fe146d2c126793fac9b46d76da5994dd10163b1')
-    version('v3_0_2', sha256='1cdae8cad0cb65991854bb7667f5a08e6c9ea23f41e2dca7f3484c4c0b8f9a90')
-    version('v3_0_1', sha256='ac63063c861e605ef9060f726568600f9ad367d6b7e2a5664bc97e766d188f4d')
-    version('v3_0_0', sha256='7f1b3ad68b397d5487de87122c642b48fb76f656ac68a733bf8a0195fddbaeac')
+    version('3.2.1', sha256='09bc6684abb576e34c324322db6f71f8987c6ec436a54256b85ef4db40185470')
+    version('3.2.0', sha256='57e58c5a7a456dbc608ce9f834a06c212a1aa64ac3ebc880ea0b8a19b000dab0')
+    version('3.1.3', sha256='03f6901cd6d563e1705a173c1a2fbbef10ab9a43f533f6ba758e357c03bdfa16')
+    version('3.1.2', sha256='1eb44c29554f45695f875fc60ab81554a2c46a041ef58a7224f3767d30e2846e')
+    version('3.1.1', sha256='5a03873c69fb38acd7ce27f2a5fa0643dd1cfcc3e214cff4aa26ee717651cc0b')
+    version('3.1.0', sha256='70e68d8a90cd2367015e0ab3459ed230a7995ad2297671742d9be6e28bc03dcf')
+    version('3.0.4', sha256='99e4c996bf22fd325335ed1391dfe735b0a338e8e4d733204d56dde7ef4eedf3')
+    version('3.0.3', sha256='618e371a48077f2b4ccfafd4d174c05f007b2ea7a51e0399df67442639409518')
+    version('3.0.2', sha256='b0172cd68e5137b8585d82c37b8a4af4b7e884f04d1b7d006a399d39447fe89e')
+    version('3.0.1', sha256='d44e4bf01b118b1d19710aa839b3f5f0c1a8391264a435f641ba4bd23bcf45ec')
+    version('3.0.0', sha256='d993ff5fc6c431067eb97e4089835c7790397d9c1ad88a56523c0591d451df19')
 
     variant('exodusii', default=False, description='Enable ExodusII')
     variant('use_markers', default=True, description='Enable use of markers')
-    variant('enable_tests', default=False, description='Enable unit testing')
     variant('parallel', default=False, description='Enable Parallel Support')
     variant('partitioner', default='none',
             values=('none', 'metis', 'zoltan','all'),
@@ -74,7 +69,7 @@ class Mstk(CMakePackage):
     depends_on('exodusii', when='+exodusii')
 
     # Unit testing variant
-    depends_on('unittest-cpp', when='+enable_tests')
+    depends_on('unittest-cpp', type='test')
 
     def cmake_args(self):
         options = []
@@ -110,7 +105,7 @@ class Mstk(CMakePackage):
             options.append('-DENABLE_ExodusII=OFF')
 
         # Unit test variant
-        if '+enable_tests' in self.spec:
+        if self.run_tests:
             options.append('-DENABLE_Tests=ON')
         else:
             options.append('-DENABLE_Tests=OFF')
