75a591f7075791c77501c16dff53f99d131c22fc
darmac <darmac@163.com>
2019-11-26 10:28:02 +0800

    1. add some other version for busybox 2. change Busybox class to MakefilePackage 3. move make('defconfig') and make() to build() function 4. change install_tree('', prefix) to install_tree('.', prefix)

--- var/spack/repos/builtin/packages/busybox/package.py
+++ var/spack/repos/builtin/packages/busybox/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class Busybox(Package):
+class Busybox(MakefilePackage):
     """BusyBox combines tiny versions of many common UNIX utilities into
     a single small executable. It provides replacements for most of
     the utilities you usually find in GNU fileutils, shellutils, etc"""
@@ -15,9 +15,14 @@ class Busybox(Package):
     url      = "https://busybox.net/downloads/busybox-1.31.0.tar.bz2"
 
     version('1.31.1', sha256='d0f940a72f648943c1f2211e0e3117387c31d765137d92bd8284a3fb9752a998')
+    version('1.31.0', sha256='0e4925392fd9f3743cc517e031b68b012b24a63b0cf6c1ff03cce7bb3846cc99')
+    version('1.30.1', sha256='3d1d04a4dbd34048f4794815a5c48ebb9eb53c5277e09ffffc060323b95dfbdc')
+    version('1.30.0', sha256='9553da068c0a30b1b8b72479908c1ba58672e2be7b535363a88de5e0f7bc04ce')
 
-    def install(self, spec, prefix):
+    def build(self, spec, prefix):
         make('defconfig')
         make()
+
+    def install(self, spec, prefix):
         make('install')
-        install_tree('', prefix)
+        install_tree('.', prefix)
