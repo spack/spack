b89296cbb2f309bff4aba7d686cd40db4bb78c98
Hector Martinez-Seara <hseara@gmail.com>
2019-11-26 12:30:19 +0100

    py-pymol: removed unnecesary dependency mesa-glu

--- var/spack/repos/builtin/packages/py-pymol/package.py
+++ var/spack/repos/builtin/packages/py-pymol/package.py
@@ -26,7 +26,6 @@ class PyPymol(PythonPackage):
     depends_on('tcsh', type=('build', 'run'))
     depends_on('py-pyqt5', type=('build', 'run'))
     depends_on('py-pmw', type=('build', 'run'))
-    depends_on('mesa-glu', type=('build', 'run'))
 
     depends_on('libmmtf-cpp', type=('build'))
     depends_on('msgpack-c', type=('build'))
