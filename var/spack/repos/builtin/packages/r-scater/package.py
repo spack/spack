7104937ef0e4ce021b476f39393c23b88f85e340
codeandkey <jtst@iastate.edu>
2019-11-27 15:53:03 -0600

    r-scater: tighten R version constraints

--- var/spack/repos/builtin/packages/r-scater/package.py
+++ var/spack/repos/builtin/packages/r-scater/package.py
@@ -22,7 +22,7 @@ class RScater(RPackage):
     version('1.6.3', commit='964effb4e883102d7c8cae627dbac4ba5d216a75')
     version('1.4.0', commit='90a2eab66ff82ba6dd7fbb33e41cd0ded20fa218')
 
-    depends_on('r@3.3:', type=('build', 'run'))
+    depends_on('r@3.3:', when='@1.4.0', type=('build', 'run'))
     depends_on('r-biobase', when='@1.4.0:1.8.4', type=('build', 'run'))
     depends_on('r-ggplot2', type=('build', 'run'))
     depends_on('r-biomart', when='@1.4.0:1.6.3', type=('build', 'run'))
@@ -43,7 +43,7 @@ class RScater(RPackage):
     depends_on('r-tximport', when='@1.4.0:1.8.4', type=('build', 'run'))
     depends_on('r-viridis', type=('build', 'run'))
 
-    depends_on('r@3.4:', when='@1.6.3:', type=('build', 'run'))
+    depends_on('r@3.4:', when='@1.6.3', type=('build', 'run'))
     depends_on('r-singlecellexperiment', when='@1.6.3:', type=('build', 'run'))
     depends_on('r-summarizedexperiment', when='@1.6.3:', type=('build', 'run'))
     depends_on('r-s4vectors', when='@1.6.3:', type=('build', 'run'))
@@ -51,13 +51,13 @@ class RScater(RPackage):
     depends_on('r-rhdf5lib', when='@1.6.3:1.10.1', type=('build', 'run'))
     depends_on('r-beachmat', when='@1.6.3:', type=('build', 'run'))
 
-    depends_on('r@3.5:', when='@1.8.4:', type=('build', 'run'))
+    depends_on('r@3.5:', when='@1.8.4', type=('build', 'run'))
     depends_on('r-delayedmatrixstats', when='@1.8.4:', type=('build', 'run'))
     depends_on('r-rcpp@0.12.14:', when='@1.8.4:', type=('build', 'run'))
     depends_on('r-delayedarray', when='@1.8.4:', type=('build', 'run'))
 
     depends_on('r-biocparallel', when='@1.10.1:', type=('build', 'run'))
 
-    depends_on('r@3.6:', when='@1.12.2:', type=('build', 'run'))
+    depends_on('r@3.6:', when='@1.12.2', type=('build', 'run'))
     depends_on('r-biocneighbors', when='@1.12.2:', type=('build', 'run'))
     depends_on('r-biocsingular', when='@1.12.2:', type=('build', 'run'))
