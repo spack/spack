09772b1ef52484723e4245731e4a13afc69928bf
codeandkey <jtst@iastate.edu>
2019-11-27 16:37:25 -0600

    r-biostrings: fix r-iranges version reqs

--- var/spack/repos/builtin/packages/r-biostrings/package.py
+++ var/spack/repos/builtin/packages/r-biostrings/package.py
@@ -25,15 +25,16 @@ class RBiostrings(RPackage):
     depends_on('r@2.8.0:', type=('build', 'run'))
     depends_on('r-biocgenerics@0.15.6:', type=('build', 'run'))
     depends_on('r-s4vectors@0.13.13:', type=('build', 'run'))
-    depends_on('r-iranges@2.9.18:', type=('build', 'run'))
+    depends_on('r-iranges@2.9.18:' when='@2.44.2:2.46.0', type=('build', 'run'))
     depends_on('r-xvector@0.11.6:', type=('build', 'run'))
 
     depends_on('r-s4vectors@0.17.25:', when='@2.48.0:', type=('build', 'run'))
-    depends_on('r-iranges@2.13.24:', when='@2.48.0:', type=('build', 'run'))
+    depends_on('r-iranges@2.13.24:', when='@2.48.0', type=('build', 'run'))
     depends_on('r-xvector@0.19.8:', when='@2.48.0:', type=('build', 'run'))
 
     depends_on('r@3.5.0:', when='@2.50.2:', type=('build', 'run'))
     depends_on('r-xvector@0.21.4:', when='@2.50.2:', type=('build', 'run'))
+    depends_on('r-iranges', when='@2.50.2:', type=('build', 'run'))
 
     depends_on('r-s4vectors@0.21.13:', when='@2.52.0:', type=('build', 'run'))
     depends_on('r-xvector@0.23.2:', when='@2.52.0:', type=('build', 'run'))
