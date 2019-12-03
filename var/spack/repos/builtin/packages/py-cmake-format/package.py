81ab52e51228dd92bf46e536224eee095406b1a1
Tristan Carel <tristan.carel@gmail.com>
2019-12-03 09:10:59 +0100

    py-cmake-format: add 0.6.0 / py-pyyaml: add 5.1.2 (#616)
    * py-cmake-format: add 0.6.0
    * py-pyyaml: add 5.1.2

--- var/spack/repos/builtin/packages/py-cmake-format/package.py
+++ var/spack/repos/builtin/packages/py-cmake-format/package.py
@@ -10,8 +10,9 @@ class PyCmakeFormat(PythonPackage):
     """Source code formatter for cmake listfiles."""
 
     homepage = "https://github.com/cheshirekow/cmake_format"
-    url      = "https://files.pythonhosted.org/packages/d8/3f/14e1a63402eeb664b35392b33450f27f0a7f8d00d8b0c26e8e108d9cef24/cmake_format-0.4.5.tar.gz"
+    url      = "https://pypi.io/packages/source/c/cmake_format/cmake_format-0.4.5.tar.gz"
 
+    version('0.6.0', sha256='fc9795907c508b4a1f851eba311bd7478b374a4ba4430cdda976ebbec440376a')
     version('0.4.5', sha256='16602408c774cd989ecfa25883de4c2dbac937e3890b735be4aab76f9647875a')
 
     depends_on('py-setuptools', type=('build', 'run'))
--- var/spack/repos/builtin/packages/py-pyyaml/package.py
+++ var/spack/repos/builtin/packages/py-pyyaml/package.py
@@ -11,6 +11,7 @@ class PyPyyaml(PythonPackage):
     homepage = "http://pyyaml.org/wiki/PyYAML"
     url      = "http://pyyaml.org/download/pyyaml/PyYAML-3.11.tar.gz"
 
+    version('5.1.2', sha256='01adf0b6c6f61bd11af6e10ca52b7d4057dd0be0343eb9283c878cf3af56aee4')
     version('3.13', sha256='3ef3092145e9b70e3ddd2c7ad59bdd0252a94dfe3949721633e41344de00a6bf')
     version('3.12', '4c129761b661d181ebf7ff4eb2d79950')
     version('3.11', 'f50e08ef0fe55178479d3a618efe21db')
