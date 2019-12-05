48f9a62087847aba05c35834833b7768542c5d94
Gregory Becker <becker33@llnl.gov>
2019-12-05 12:18:49 -0800

    improve handling of find -L to match find -l

--- lib/spack/spack/test/cmd/find.py
+++ lib/spack/spack/test/cmd/find.py
@@ -309,11 +309,12 @@ def test_find_command_basic_usage(database):
 
 @pytest.mark.regression('9875')
 def test_find_prefix_in_env(mutable_mock_env_path, install_mockery, mock_fetch,
-                            mock_packages, mock_archive):
+                            mock_packages, mock_archive, config):
     """Test `find` formats requiring concrete specs work in environments."""
     env('create', 'test')
     with ev.read('test'):
         install('mpileaks')
         find('-p')
+        find('-l')
         find('-L')
         # Would throw error on regression
