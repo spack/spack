9046fd76ba6503b8f0a5c4eec69c29b90fc519be
Seth R Johnson <johnsonsr@ornl.gov>
2019-12-06 06:36:11 -0500

    Fix requirements and update version

--- var/spack/repos/builtin/packages/py-h5sh/package.py
+++ var/spack/repos/builtin/packages/py-h5sh/package.py
@@ -9,16 +9,17 @@ from spack import *
 class PyH5sh(PythonPackage):
     """Shell-like environment for HDF5."""
 
-    homepage = "https://h5sh.readthedocs.io/en/"
-    url      = "https://github.com/sethrj/h5sh/archive/v0.1.0.tar.gz"
+    homepage = "https://pypi.python.org/pypi/h5sh"
+    url      = "https://github.com/sethrj/h5sh/archive/v0.1.1.tar.gz"
 
     maintainers = ['sethrj']
 
-    version('0.1.0', sha256='52d672e3140323e51084a1bfa2d84012e23c640469242381090a3c7af3e00e4f')
+    version('0.1.1', sha256='111989d8200d1da8e150aee637a907e524ca0f98d5005a55587cba0d94d9c4a0')
 
-    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
-    depends_on('py-h5py@2.7.1:', type=('build', 'run'))
-    depends_on('py-numpy@1.15:', type=('build', 'run'))
+    depends_on('py-h5py', type=('build', 'run'))
+    depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-prompt-toolkit@2:', type=('build', 'run'))
     depends_on('py-pygments', type=('build', 'run'))
+    depends_on('py-six', type=('build', 'run'))
+    depends_on('py-pytest', type='test')
