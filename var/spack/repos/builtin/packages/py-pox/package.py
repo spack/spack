c6051765a16ff478f695378ade23654bf1aaef14
Ivan Razumov <ivan.razumov@cern.ch>
2019-12-10 15:59:00 +0100

    Fix py-pox build recipe

--- var/spack/repos/builtin/packages/py-pox/package.py
+++ var/spack/repos/builtin/packages/py-pox/package.py
@@ -11,6 +11,8 @@ class PyPox(PythonPackage):
 
     homepage = "https://github.com/uqfoundation/pox"
     url      = "https://pypi.io/packages/source/p/pox/pox-0.2.3.zip"
+    list_url = "https://pypi.io/packages/source/p/pox/"
+    
 
     version('0.2.3', sha256='d3e8167a1ebe08ae56262a0b9359118d90bc4648cd284b5d10ae240343100a75')
     version('0.2.2', sha256='c0b88e59ef0e4f2fa4839e11bf90d2c32d6ceb5abaf01f0c8138f7558e6f87c1')
@@ -19,3 +21,14 @@ class PyPox(PythonPackage):
     depends_on('python@2.5:2.8,3.1:')
 
     depends_on('py-setuptools@0.6:', type='build')
+
+    def url_for_version(self, version):
+        url = self.list_url
+        if version >= Version('0.2.5'):
+            url += 'pox-{0}.tar.gz'
+        else:
+            url += 'pox-{0}.zip'
+       
+        url = url.format(str(version))
+        return url
+
