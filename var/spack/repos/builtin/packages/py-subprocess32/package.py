4bba02bcb2cd560088920b5006ed40675289124b
Ivan Razumov <ivan.razumov@cern.ch>
2019-12-10 16:03:32 +0100

    Add missing dependency on setuptools to py-subprocess32

--- var/spack/repos/builtin/packages/py-subprocess32/package.py
+++ var/spack/repos/builtin/packages/py-subprocess32/package.py
@@ -13,3 +13,4 @@ class PySubprocess32(PythonPackage):
     url      = "https://pypi.io/packages/source/s/subprocess32/subprocess32-3.2.7.tar.gz"
 
     version('3.2.7', sha256='1e450a4a4c53bf197ad6402c564b9f7a53539385918ef8f12bdf430a61036590')
+    depends_on('py-setuptools', type='build')
