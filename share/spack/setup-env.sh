c07f5a057920ab9db2dae52f3893a885d95a7ff2
Gregory Becker <becker33@llnl.gov>
2019-12-10 15:39:23 -0800

    make changes to setupenv.sh work with zsh

--- share/spack/setup-env.sh
+++ share/spack/setup-env.sh
@@ -43,10 +43,10 @@ spack() {
     # Store LD_LIBRARY_PATH variables from spack shell function
     # This is necessary because MacOS System Integrity Protection clears
     # (DY?)LD_LIBRARY_PATH variables on process start.
-    if [ "${LD_LIBRARY_PATH+x}" == x ]; then
+    if [ -n "${LD_LIBRARY_PATH-}" ]; then
         export SPACK_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
     fi
-    if [ "${DYLD_LIBRARY_PATH+x}" == x ]; then
+    if [ -n "${DYLD_LIBRARY_PATH-}" ]; then
         export SPACK_DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
     fi
 
