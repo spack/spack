5a7b7a195f586c64cd17eb8186063b25cd204041
Nicolas Cornu <nicolas.cornu@epfl.ch>
2019-12-11 13:25:24 +0100

    zee: add depends on fmt (#626)

--- var/spack/repos/builtin/packages/zee/package.py
+++ var/spack/repos/builtin/packages/zee/package.py
@@ -43,6 +43,7 @@ class Zee(CMakePackage):
     depends_on('gmsh@4: +metis~mpi+oce+openmp+shared')
     depends_on('mpi')
     depends_on('omega-h')
+    depends_on('fmt')
 
     depends_on('metis+int64')
     depends_on('petsc +int64', when='+petsc')
