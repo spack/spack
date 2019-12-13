0167ceb10437311e6c8ae5541492e0034130214f
Dan Lipsa <dan.lipsa@kitware.com>
2019-12-13 16:19:31 -0500

    Add shared option to ParaView. (#14131)
    * Add shared option to ParaView.
    
    * Use append for one attribute

--- var/spack/repos/builtin/packages/paraview/package.py
+++ var/spack/repos/builtin/packages/paraview/package.py
@@ -44,10 +44,13 @@ class Paraview(CMakePackage, CudaPackage):
     variant('opengl2', default=True, description='Enable OpenGL2 backend')
     variant('examples', default=False, description="Build examples")
     variant('hdf5', default=False, description="Use external HDF5")
+    variant('shared', default=True,
+            description='Builds a shared version of the library')
 
     conflicts('+python', when='+python3')
     conflicts('+python', when='@5.6:')
     conflicts('+python3', when='@:5.5')
+    conflicts('+shared', when='+cuda')
 
     # Workaround for
     # adding the following to your packages.yaml
@@ -234,6 +237,15 @@ class Paraview(CMakePackage, CudaPackage):
                 '-DMPI_C_COMPILER:PATH=%s' % spec['mpi'].mpicc,
                 '-DMPI_Fortran_COMPILER:PATH=%s' % spec['mpi'].mpifc
             ])
+
+        if '+shared' in spec:
+            cmake_args.append(
+                '-DPARAVIEW_BUILD_SHARED_LIBS:BOOL=ON'
+            )
+        else:
+            cmake_args.append(
+                '-DPARAVIEW_BUILD_SHARED_LIBS:BOOL=OFF'
+            )
 
         if '+cuda' in spec:
             cmake_args.extend([
