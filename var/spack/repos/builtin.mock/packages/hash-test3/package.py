60580f587179f57e182f9ff957dc96c3ac079c0a
Peter Scheibel <scheibel1@llnl.gov>
2019-12-14 14:31:39 -0800

    package hash: gracefully handle @when with non-string args (#14153)
    * when constructing package hash, default to including a method in the content hash if we can't determine whether it would be included by examining the AST
    * add a test for updated content-hash calculations
    * refactor content hash tests to eliminate repeated lines

--- var/spack/repos/builtin.mock/packages/hash-test3/package.py
+++ var/spack/repos/builtin.mock/packages/hash-test3/package.py
@@ -8,23 +8,18 @@ from spack import *
 import os
 
 
-class HashTest1(Package):
+class HashTest3(Package):
     """Used to test package hashing
     """
 
-    homepage = "http://www.hashtest1.org"
-    url = "http://www.hashtest1.org/downloads/hashtest1-1.1.tar.bz2"
+    homepage = "http://www.hashtest3.org"
+    url = "http://www.hashtest1.org/downloads/hashtest3-1.1.tar.bz2"
 
-    version('1.1', 'a' * 32)
     version('1.2', 'b' * 32)
     version('1.3', 'c' * 32)
-    version('1.4', 'd' * 32)
     version('1.5', 'd' * 32)
     version('1.6', 'e' * 32)
     version('1.7', 'f' * 32)
-
-    patch('patch1.patch', when="@1.1")
-    patch('patch2.patch', when="@1.4")
 
     variant('variantx', default=False, description='Test variant X')
     variant('varianty', default=False, description='Test variant Y')
@@ -41,6 +36,7 @@ class HashTest1(Package):
     def install(self, spec, prefix):
         os.listdir(os.getcwd())
 
-    @when('@1.5,1.6')
-    def extra_phase(self, spec, prefix):
-        pass
+    for _version_constraint in ['@1.5', '@1.6']:
+        @when(_version_constraint)
+        def extra_phase(self, spec, prefix):
+            pass
