ba9de2ffd5f9afe1307b3e3be23414efe779b6d9
darmac <xiaojun2@hisilicon.com>
2019-12-16 11:45:45 +0800

    refine hbase version and depends

--- var/spack/repos/builtin/packages/hbase/package.py
+++ var/spack/repos/builtin/packages/hbase/package.py
@@ -16,14 +16,12 @@ class Hbase(Package):
     """
 
     homepage = "https://github.com/apache/hbase"
-    url      = "https://github.com/apache/hbase/archive/rel/1.4.12.tar.gz"
+    url      = "https://github.com/apache/hbase/archive/rel/2.2.2.tar.gz"
 
-    version('2.2.2RC0', sha256='fa78e642225ee13c5b5f9e53f7eb3ec67c0188a776f5525d4d72362a40fcf653')
-    version('2.2.2',    sha256='e9a58946e9adff1cac23a0b261ecf32da32f8d2ced0706af1d04e8a67d582926', preferred=True)
-    version('2.1.8RC0', sha256='e2de965f2dcea83cbd96159d40c2ba9e4a9d0f459d70bc133408886954df627c')
+    version('2.2.2',    sha256='e9a58946e9adff1cac23a0b261ecf32da32f8d2ced0706af1d04e8a67d582926')
     version('2.1.8',    sha256='121cea4c554879c8401f676c8eb49e39bd35d41c358e919379ad4a318844c8de')
 
-    depends_on('jdk@1.7:', type='run')
+    depends_on('java@7:', type='run')
 
     def install(self, spec, prefix):
         install_tree('.', prefix)
