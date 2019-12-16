deca899816b4916565daf8930e0a62b2bd3a31b6
darmac <xiaojun2@hisilicon.com>
2019-12-16 11:04:20 +0800

    remove alpha version and duplicated autoheader cmd

--- var/spack/repos/builtin/packages/filebench/package.py
+++ var/spack/repos/builtin/packages/filebench/package.py
@@ -21,10 +21,7 @@ class Filebench(AutotoolsPackage):
     homepage = "https://github.com/filebench/filebench"
     url      = "https://github.com/filebench/filebench/archive/1.5-alpha3.tar.gz"
 
-    version('1.5-alpha3', sha256='2dedfc46458f5bb13e5f5a4a9f4db6263152f6186690d57653c96138610db35a')
-    version('1.5-alpha2', sha256='02aeae7bd7e6c3e1d32862a4d1c5e14e59a1ffbb8fadbf09dc84ebe82bc6029e')
-    version('1.5-alpha1', sha256='8266c25274a20025c325050ed4050d2783d239f77558fc332381d05de9917dfc')
-    version('1.4.9.1',    sha256='77ae91b83c828ded1219550aec74fbbd6975dce02cb5ab13c3b99ac2154e5c2e', preferred=True)
+    version('1.4.9.1',    sha256='77ae91b83c828ded1219550aec74fbbd6975dce02cb5ab13c3b99ac2154e5c2e')
     version('1.4.9',      sha256='61b8a838c1450b51a4ce61481a19a1bf0d6e3993180c524ff4051f7c18bd9c6a')
 
     depends_on('autoconf', type='build')
@@ -38,7 +35,6 @@ class Filebench(AutotoolsPackage):
         sh = which('sh')
         sh('libtoolize')
         sh('aclocal')
-        sh('autoheader')
         sh('autoheader')
         sh_automake = Executable('automake --add-missing')
         sh_automake()
