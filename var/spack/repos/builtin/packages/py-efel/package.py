e8acbf5f0f7b0232f49398c029f2526f1a749381
Benoit Coste <benoit.coste@protonmail.com>
2019-12-16 18:07:06 +0100

    Bump psp-validation to 0.2.0 and BluePy to 0.14.3 (#630)

--- var/spack/repos/builtin/packages/py-efel/package.py
+++ var/spack/repos/builtin/packages/py-efel/package.py
@@ -26,19 +26,20 @@ from spack import *
 
 
 class PyEfel(PythonPackage):
-    """The Electrophys Feature Extract Library (eFEL) allows 
-    neuroscientists to automatically extract features from time series data 
+    """The Electrophys Feature Extract Library (eFEL) allows
+    neuroscientists to automatically extract features from time series data
     recorded from neurons (both in vitro and in silico).
-    Examples are the action potential width and amplitude in 
+    Examples are the action potential width and amplitude in
     voltage traces recorded during whole-cell patch clamp experiments.
-    The user of the library provides a set of traces and selects the 
-    features to be calculated. The library will then extract the requested 
+    The user of the library provides a set of traces and selects the
+    features to be calculated. The library will then extract the requested
     features and return the values to the user."""
     homepage = "https://github.com/BlueBrain/eFEL"
-    url = "https://pypi.io/packages/source/e/efel/efel-3.0.22.tar.gz"
+    url = "https://pypi.io/packages/source/e/efel/efel-3.0.70.tar.gz"
 
+    version('3.0.70', sha256='3f3368012cdec5ca7d5551cea35b30a53befd0c0c740fc535209f840616c07b1')
     version('3.0.22', sha256='97b2c1a0425b12cd419e8539bb1e936ce64c4e93f5d0dd7f81f38554490064a2')
-    
+
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy', type='run')
     depends_on('py-six', type='run')
