23069b0155816ea3f97490e746ca06251fbc6299
Adam J. Stewart <ajstewart426@gmail.com>
2019-12-18 11:17:53 -0600

    Remove py-zc-buildout dependency

--- var/spack/repos/builtin/packages/py-zc-lockfile/package.py
+++ var/spack/repos/builtin/packages/py-zc-lockfile/package.py
@@ -7,13 +7,11 @@ from spack import *
 
 
 class PyZcLockfile(PythonPackage):
-    """Basic inter-process locks """
+    """Basic inter-process locks"""
 
-    homepage = "http://www.python.org/pypi/zc.lockfile"
+    homepage = "https://www.python.org/pypi/zc.lockfile"
     url      = "https://pypi.io/packages/source/z/zc.lockfile/zc.lockfile-1.4.tar.gz"
 
-    version(
-        '1.4', sha256='95a8e3846937ab2991b61703d6e0251d5abb9604e18412e2714e1b90db173253')
+    version('1.4', sha256='95a8e3846937ab2991b61703d6e0251d5abb9604e18412e2714e1b90db173253')
 
     depends_on('py-setuptools', type=('build', 'run'))
-    depends_on('py-zc-buildout', type=('build', 'run'))
