b3ccc88965b9a875e58e6db55fda5073098283ed
Greg Becker <becker33@llnl.gov>
2019-12-18 11:56:27 -0800

    update py-hvac url
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-hvac/package.py
+++ var/spack/repos/builtin/packages/py-hvac/package.py
@@ -10,7 +10,7 @@ class PyHvac(PythonPackage):
     """HashiCorp Vault API client"""
 
     homepage = "https://github.com/hvac/hvac/"
-    url      = "https://github.com/ianunruh/hvac/archive/v0.2.17.tar.gz"
+    url      = "https://github.com/hvac/hvac/archive/v0.2.17.tar.gz"
 
     version('0.9.6', sha256='ff60972edc512c73ac4811e91bcffccdb99f0d8975c0b41e44a4c79d73c711af')
     version('0.9.5', sha256='3e4af2d84833b8f61c9c3351d4a9799bf8e8e4344d0ad4e162609a7379961a16')
