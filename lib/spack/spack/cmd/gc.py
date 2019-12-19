f5070b662f775e5a5cba87accc20fa2825bc3a61
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2019-12-19 14:46:42 +0100

    Preserve root specs in env contexts

--- lib/spack/spack/cmd/gc.py
+++ lib/spack/spack/cmd/gc.py
@@ -5,9 +5,10 @@
 
 import llnl.util.tty as tty
 
-import spack.store
 import spack.cmd.common.arguments
 import spack.cmd.uninstall
+import spack.environment
+import spack.store
 
 description = "remove specs that are now no longer needed"
 section = "build"
@@ -20,6 +21,18 @@ def setup_parser(subparser):
 
 def gc(parser, args):
     specs = spack.store.unused_specs()
+
+    # If we are in an env context be sure to preserve root specs
+    env = spack.environment.get_env(args=None, cmd_name='gc')
+    if env:
+        env.concretize()
+        hashes = set([s.dag_hash() for x in env.roots()
+                      for s in x.traverse(deptype=('link', 'run'))])
+        if hashes:
+            msg = 'Filtering out specs needed by the "{0}" environment'
+            tty.msg(msg.format(env.name))
+            specs = [s for s in specs if s.dag_hash() not in hashes]
+
     if not specs:
         msg = "There are no unused specs. Spack's store is clean."
         tty.msg(msg)
