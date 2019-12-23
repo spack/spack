cd6bb188093aa334fbba78b6df9608686bc66e44
codeandkey <jtst@iastate.edu>
2019-12-23 10:51:32 -0600

    py-intervaltree: fix checksum

--- var/spack/repos/builtin/packages/py-intervaltree/package.py
+++ var/spack/repos/builtin/packages/py-intervaltree/package.py
@@ -12,7 +12,7 @@ class PyIntervaltree(PythonPackage):
     homepage = "https://github.com/chaimleib/intervaltree"
     url      = "https://github.com/chaimleib/intervaltree/archive/3.0.2.tar.gz"
 
-    version('3.0.2', sha256='cb4f61c81dcb4fea6c09903f3599015a83c9bdad1f0bbd232495e6681e19e273')
+    version('3.0.2', sha256='e8ab75b66077f2e5fb85ac56cb6df834689edb048d38601d53d8867cce3b77d1')
 
     depends_on('py-sortedcontainers@2:3', type=('build', 'run'))
     depends_on('py-pytest', type=('build', 'run'))
