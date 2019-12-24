81eb0bc637f86eafd208044fa9db04d626d75d82
asmaahassan90 <31959389+asmaahassan90@users.noreply.github.com>
2019-12-24 15:20:24 +0200

    fix home url and linting

--- var/spack/repos/builtin/packages/avizo/package.py
+++ var/spack/repos/builtin/packages/avizo/package.py
@@ -13,16 +13,17 @@ class Avizo(Package):
     Wherever three-dimensional imaging data sets need to be processed, in
     materials science, geosciences or engineering applications, Avizo offers
     abundant state-of-the-art image data processing, exploration and analysis
-    features within an intuitive workflow and easy-to-use graphical user interface."""
+    features within an intuitive workflow and easy-to-use graphical user
+    interface."""
 
-    homepage = "https://www.thermofisher.com/sa/en/home/industrial/electron-microscopy/\
-               electron-microscopy-instruments-workflow-solutions/3d-visualization-\
-               analysis-software.html"
+    homepage = "https://www.thermofisher.com/sa/en/home/industrial/electron-microscopy/electron-microscopy-instruments-workflow-solutions/3d-visualization-analysis-software.html"
     version('9.7.0', 'ed3947e61a1d17839005c824df975030')
 
-    ## You have to create the tarball for the source code to be processed correctly.
+    # You have to create the tarball for the source code to be 
+    # processed correctly.
     def url_for_version(self, version):
-        return "file://{0}/avizo_{1}_linux64_gcc44.tar.gz".format(os.getcwd(), version)
+        return "file://{0}/avizo_{1}_linux64_gcc44.tar.gz"
+                .format(os.getcwd(), version)
 
     def install(self, spec, prefix):
         ver = self.version.joined
@@ -30,8 +31,9 @@ class Avizo(Package):
         sh('Avizo-{0}-Linux64-gcc44.bin'.format(ver), '--noexec', '--keep')
 
         with working_dir('Avizo'):
-            avizo_tar = tarfile.open(name='Avizo-{0}-Linux64-gcc44.tar.bz2'.format(self.version))
+            avizo_tar = tarfile.open(name='Avizo-{0}-Linux64-gcc44.tar.bz2'
+                                     .format(self.version))
             avizo_tar.extractall()
-            install_tree(join_path(self.stage.source_path, 'Avizo/Avizo-{0}'.format(self.version)), prefix)
+            install_tree(join_path(self.stage.source_path, 'Avizo/Avizo-{0}'
+                                   .format(self.version)), prefix)
             intsall('password.dat', prefix.share.license)
-                
