07003fe317419eb9ea67b4503c9dfc2f7c945050
Sinan81 <sbulut@3vgeomatics.com>
2019-12-30 14:52:46 -0800

    create package py-zarr

---
+++ var/spack/repos/builtin/packages/py-zarr/package.py
@@ -0,0 +1,29 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyZarr(PythonPackage):
+    """Zarr is a Python package providing an implementation of chunked,
+    compressed, N-dimensional arrays."""
+
+    homepage = "https://zarr.readthedocs.io"
+    url      = "https://pypi.io/packages/source/z/zarr/zarr-2.3.2.tar.gz"
+
+    version('2.3.2', sha256='c62d0158fb287151c978904935a177b3d2d318dea3057cfbeac8541915dfa105')
+
+    depends_on('python@3.5:',           type=('build', 'run'))
+    depends_on('py-asciitree',          type=('build', 'run'))
+    depends_on('py-fasteners',          type=('build', 'run'))
+    depends_on('py-setuptools',         type='build')
+    depends_on('py-numcodecs@0.6.4:',   type=('build', 'run'))
+    depends_on('py-numpy@1.7:',         type=('build', 'run'))
+
+    def build_args(self, spec, prefix):
+        # FIXME: Add arguments other than --prefix
+        # FIXME: If not needed delete this function
+        args = []
+        return args
