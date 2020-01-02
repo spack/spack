9585751fbd5771291c62a1c69f184b883a44441b
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2020-01-02 12:46:01 -0800

    Updated version to include v3.0.1
    - this patch has a rather critical MT fix

--- var/spack/repos/builtin/packages/timemory/package.py
+++ var/spack/repos/builtin/packages/timemory/package.py
@@ -19,6 +19,8 @@ class Timemory(CMakePackage):
     version('develop', branch='develop', submodules=True)
     version('3.0.0', commit='b36b1673b2c6b7ff3126d8261bef0f8f176c7beb',
             submodules=True)
+    version('3.0.1', commit='ef638e1cde90275ce7c0e12fc4902c27bcbdeefd',
+            submodules=True)
 
     variant('python', default=True, description='Enable Python support')
     variant('mpi', default=False, description='Enable MPI support')
