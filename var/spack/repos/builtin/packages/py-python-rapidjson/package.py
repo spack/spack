d9474014030a45aa31fa78c05524143a09285e82
codeandkey <jtst@iastate.edu>
2020-01-03 15:03:43 -0600

    py-rapidjson: rename to py-python-rapidjson, use PyPI mirror

--- var/spack/repos/builtin/packages/py-python-rapidjson/package.py
+++ var/spack/repos/builtin/packages/py-python-rapidjson/package.py
@@ -5,32 +5,17 @@
 
 from spack import *
 
-from os import symlink
-from shutil import rmtree
 
-
-class PyRapidjson(PythonPackage):
+class PyPythonRapidjson(PythonPackage):
     """Python wrapper around rapidjson."""
 
     homepage = "https://github.com/python-rapidjson/python-rapidjson"
-    url      = "https://github.com/python-rapidjson/python-rapidjson/archive/v0.9.1.tar.gz"
+    url      = "https://files.pythonhosted.org/packages/41/3e/a150b4ed792b7962c8dad466856e2eda36275e9f547f67f9ae17e1b0a4af/python-rapidjson-0.9.1.tar.gz"
 
-    version('0.9.1', sha256='5820f6dd3ca2a247269cce7bd3cae42edb078e925fff130e519bff9946c5cbcf')
-
-    resource(
-        name='rapidjson',
-        git='https://github.com/Tencent/rapidjson.git',
-        commit='7484e06c589873e1ed80382d262087e4fa80fb63',
-        destination='rapidjsonsrc',
-    )
+    version('0.9.1', sha256='ad80bd7e4bb15d9705227630037a433e2e2a7982b54b51de2ebabdd1611394a1')
 
     depends_on('py-readme-renderer@24.0', type=('build', 'run'))
     depends_on('py-twine@1.13.0', type=('build', 'run'))
     depends_on('py-simplejson@3.16.0', type=('build', 'run'))
     depends_on('py-ujson@1.35', type=('build', 'run'))
     depends_on('py-yajl@0.3.5', type=('build', 'run'))
-
-    @run_before('build')
-    def prep_rapidjson(self):
-        rmtree('rapidjson')
-        symlink('rapidjsonsrc/rapidjson', 'rapidjson')
