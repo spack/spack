afae3e4d61f8f7e66117fc816535a12da5dfe84d
asmaahassan90 <31959389+asmaahassan90@users.noreply.github.com>
2020-01-04 15:17:15 +0300

    Converting recipe to a PythonPackage base class

--- var/spack/repos/builtin/packages/thirdorder/package.py
+++ var/spack/repos/builtin/packages/thirdorder/package.py
@@ -4,51 +4,24 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-from subprocess import call
 
 
-class PyThirdorder(Package):
-    """It helps ShengBTE users create FORCE_CONSTANTS_3RD files effciently"""
+class PyThirdorder(PythonPackage):
+    """It helps ShengBTE users create FORCE_CONSTANTS_3RD files efficiently"""
 
     homepage = "http://www.shengbte.org"
     url      = "http://www.shengbte.org/downloads/thirdorder-v1.1.1-8526f47.tar.bz2"
 
     version('1.1.1-8526f47', 'c39ff34056a0e57884a6ff262581dbbe')
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-numpy',        type=('build', 'run'))
-    depends_on('py-scipy',        type=('build', 'run'))
-    depends_on('spglib',        type=('build', 'run'))
+    depends_on('python', type=('build', 'run'))
+    depends_on('py-numpy', type=('build', 'run'))
+    depends_on('py-scipy', type=('build', 'run'))
+    depends_on('spglib', type=('build', 'run'))
 
-    def setup_environment(self, spack_env, run_env):
-        python_version = self.spec['python'].version.up_to(2)
+    phases = ['edit', 'build', 'install', "post_install"]
 
-        run_env.prepend_path('PYTHONPATH', join_path(
-            self.spec['python'].prefix.lib,
-            'python{0}'.format(python_version), 'site-packages'))
-        run_env.prepend_path('LIBRARY_PATH', self.spec['python'].prefix.lib)
-        run_env.prepend_path('LD_LIBRARY_PATH', self.spec['python'].prefix.lib)
-
-        run_env.prepend_path('PYTHONPATH', join_path(
-            prefix.lib, 'python{0}'.format(python_version), 'site-packages'))
-        run_env.prepend_path('LIBRARY_PATH', prefix.lib)
-        run_env.prepend_path('LD_LIBRARY_PATH', prefix.lib)
-
-        run_env.prepend_path('PYTHONPATH', join_path(
-            self.spec['py-numpy'].prefix.lib,
-            'python{0}'.format(python_version), 'site-packages'))
-        run_env.prepend_path('LIBRARY_PATH', self.spec['py-numpy'].prefix.lib)
-        run_env.prepend_path(
-            'LD_LIBRARY_PATH', self.spec['py-numpy'].prefix.lib)
-
-        run_env.prepend_path('PYTHONPATH', join_path(
-            self.spec['py-scipy'].prefix.lib,
-            'python{0}'.format(python_version), 'site-packages'))
-        run_env.prepend_path('LIBRARY_PATH', self.spec['py-scipy'].prefix.lib)
-        run_env.prepend_path(
-            'LD_LIBRARY_PATH', self.spec['py-scipy'].prefix.lib)
-
-    def patch(self):
+    def edit(self, spec, prefix):
         setupfile = FileFilter('setup.py')
         setupfile.filter('LIBRARY_DIRS = .*', 'LIBRARY_DIRS = ["%s"]'
                          % self.spec['spglib'].prefix.lib)
@@ -58,10 +31,8 @@ class PyThirdorder(Package):
         sourcefile = FileFilter('thirdorder_core.c')
         sourcefile.filter('#include "spglib.*"', '#include "spglib.h"')
 
-    def install(self, spec, prefix):
-        call(['python', 'setup.py', 'build'])
-        call(['python', 'setup.py', 'install', '--prefix=%s' % prefix])
-        mkdirp(prefix.bin)
+    def post_install(self, spec, prefix):
+        mkdirp(prefix.bin)        
         install('thirdorder_espresso.py', prefix.bin)
         install('thirdorder_vasp.py', prefix.bin)
         install('thirdorder_castep.py', prefix.bin)
