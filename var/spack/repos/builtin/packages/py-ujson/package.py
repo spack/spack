f28991654a6229fee24d44302bcfdecc3c4b4fbe
codeandkey <jtst@iastate.edu>
2020-01-06 13:09:34 -0600

    py-ujson: use short PyPI url

--- var/spack/repos/builtin/packages/py-ujson/package.py
+++ var/spack/repos/builtin/packages/py-ujson/package.py
@@ -11,7 +11,7 @@ class PyUjson(PythonPackage):
        bindings."""
 
     homepage = "https://github.com/esnme/ultrajson"
-    url      = "https://files.pythonhosted.org/packages/16/c4/79f3409bc710559015464e5f49b9879430d8f87498ecdc335899732e5377/ujson-1.35.tar.gz"
+    url      = "https://pypi.io/packages/source/u/ujson/ujson-1.35.tar.gz"
 
     version('1.35', sha256='f66073e5506e91d204ab0c614a148d5aa938bdbf104751be66f8ad7a222f5f86')
 
