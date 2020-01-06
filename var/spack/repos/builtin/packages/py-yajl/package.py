62a772b7f0da8a1e65d9c02110935766bdd4ba5f
codeandkey <jtst@iastate.edu>
2020-01-06 13:06:17 -0600

    py-yajl: use short PyPI url

--- var/spack/repos/builtin/packages/py-yajl/package.py
+++ var/spack/repos/builtin/packages/py-yajl/package.py
@@ -12,7 +12,7 @@ class PyYajl(PythonPackage):
     """Python bindings for the Yajl JSON encoder/decoder library."""
 
     homepage = "https://github.com/rtyler/py-yajl"
-    url      = "https://files.pythonhosted.org/packages/70/5a/57ed8b430a55e2bfc9c4ea0a263f41f4d2fe5d962ff073c8af117da78be1/yajl-0.3.5.tar.gz"
+    url      = "https://pypi.io/packages/source/y/yajl/yajl-0.3.5.tar.gz"
 
     version('0.3.5', sha256='432321ea613692a4782a2368a300f57b59c64a3c8508c7465af3fdc045e7bcc2')
 
