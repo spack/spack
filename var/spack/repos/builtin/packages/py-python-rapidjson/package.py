f050dc6ac93faf4cfe45d821fc2157e7a3b35a00
codeandkey <jtst@iastate.edu>
2020-01-06 13:04:32 -0600

    python-rapidjson: use short PyPI url

--- var/spack/repos/builtin/packages/py-python-rapidjson/package.py
+++ var/spack/repos/builtin/packages/py-python-rapidjson/package.py
@@ -10,7 +10,7 @@ class PyPythonRapidjson(PythonPackage):
     """Python wrapper around rapidjson."""
 
     homepage = "https://github.com/python-rapidjson/python-rapidjson"
-    url      = "https://files.pythonhosted.org/packages/41/3e/a150b4ed792b7962c8dad466856e2eda36275e9f547f67f9ae17e1b0a4af/python-rapidjson-0.9.1.tar.gz"
+    url      = "https://pypi.io/packages/source/p/python-rapidjson/python-rapidjson-0.9.1.tar.gz"
 
     version('0.9.1', sha256='ad80bd7e4bb15d9705227630037a433e2e2a7982b54b51de2ebabdd1611394a1')
 
