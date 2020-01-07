97e37605cd24913421edc9852de0b4074c372881
Corey adams <coreyjadams@gmail.com>
2020-01-07 15:44:06 -0600

    Update package.py
    Trying to address flake8 corrections

--- var/spack/repos/builtin/packages/py-scikit-build/package.py
+++ var/spack/repos/builtin/packages/py-scikit-build/package.py
@@ -7,21 +7,21 @@ from spack import *
 
 
 class PyScikitBuild(PythonPackage):
-    """scikit-build is an improved build system generator for CPython C/C++/Fortran/Cython extensions. It provides better support for additional compilers, build systems, cross compilation, and locating dependencies and their associated build requirements.
+    """scikit-build is an improved build system generator for CPython 
+       C/C++/Fortran/Cython extensions. It provides better support for 
+       additional compilers, build systems, cross compilation, and 
+       locating dependencies and their associated build requirements.
 
-The scikit-build package is fundamentally just glue between the setuptools Python module and CMake."""
+       The scikit-build package is fundamentally just glue between
+       the setuptools Python module and CMake."""
 
     homepage = "https://scikit-build.readthedocs.io/en/latest/"
     url      = "https://github.com/scikit-build/scikit-build/archive/0.10.0.tar.gz"
 
-    
     maintainers = ['coreyjadams']
 
     version('0.10.0', sha256='2beec252813b20327072c15e9d997f15972aedcc6a130d0154979ff0fdb1b010')
 
-
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-packaging',  type=('build', 'run'))
     depends_on('py-wheel',      type=('build', 'run'))
-
-
