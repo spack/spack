5b7cdc8832015c852df38292fb6eb3959c1d9a8f
asmaahassan90 <31959389+asmaahassan90@users.noreply.github.com>
2020-01-08 11:20:09 +0300

    Fix typo and lint

--- var/spack/repos/builtin/packages/adf/package.py
+++ var/spack/repos/builtin/packages/adf/package.py
@@ -8,14 +8,15 @@ from spack import *
 
 class Adf(Package):
     """Amsterdam Density Functional (ADF) is a program for first-principles
-    electronic structure calculations that makes use of density functional theory."""
+    electronic structure calculations that makes use of density functional
+    theory."""
 
     homepage = "https://www.scm.com/product/adf/"
     version('2017.113', '666ef15d253b74c707dd14da35e7cf283ca20e21e24ed43cb953fb9d1f2f1e15')
 
     def url_for_version(self, version):
-        return "file://{0}/adf/adf{1}.pc64_linux.openmpi.bin.tgz".format(os.getcwd(),
-                                                                         version)
+        return "file://{0}/adf/adf{1}.pc64_linux.openmpi.bin.tgz"
+                .format(os.getcwd(), version)
 
     # Licensing
     license_required = True
@@ -28,12 +29,13 @@ class Adf(Package):
         run_env.set('ADFHOME', self.prefix)
         run_env.set('ADFBIN', join_path(self.prefix, 'bin'))
         run_env.set('ADFRESOURCES', join_path(self.prefix, 'atomicdata'))
-        run_env.set('SCMLICENSE', join_path(self.prefix, 'license.txt')
+        run_env.set('SCMLICENSE', join_path(self.prefix, 'license.txt'))
         run_env.set('SCM_TMPDIR', '/tmp')
 
     def install(self, spec, prefix):
         for filename in os.listdir(self.stage.source_path):
             if os.path.isdir(filename):
                 shutil.copytree(filename, join_path(self.prefix, filename))
-            elif not fnmatch.fnmatch(filename, "spack-build.*") or fnmatch.fnmatch(filename, "adf*"):
+            elif not fnmatch.fnmatch(filename, "spack-build.*")
+                 or fnmatch.fnmatch(filename, "adf*"):
                 shutil.copy(filename, self.prefix)
