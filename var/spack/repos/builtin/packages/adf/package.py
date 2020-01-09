6781b671241541e64b79feb93c3aaf36f6ff3557
asmaahassan90 <31959389+asmaahassan90@users.noreply.github.com>
2020-01-09 08:53:04 +0300

    changes as per reviewer's request
    fix setup environment function, enhance recipe

--- var/spack/repos/builtin/packages/adf/package.py
+++ var/spack/repos/builtin/packages/adf/package.py
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-import os
-import fnmatch
-import shutil
 
 
 class Adf(Package):
@@ -26,19 +23,12 @@ class Adf(Package):
     license_files = ['license.txt']
     license_vars = ['SCMLICENSE']
 
-    def setup_environment(self, spack_env, run_env):
-        run_env.prepend_path('PATH', join_path(self.prefix, 'bin'))
-
+    def setup_run_environment(self, env):
         run_env.set('ADFHOME', self.prefix)
-        run_env.set('ADFBIN', join_path(self.prefix, 'bin'))
-        run_env.set('ADFRESOURCES', join_path(self.prefix, 'atomicdata'))
+        run_env.set('ADFBIN', self.prefix.bin)
+        run_env.set('ADFRESOURCES', self.prefix.atomicdata)
         run_env.set('SCMLICENSE', join_path(self.prefix, 'license.txt'))
         run_env.set('SCM_TMPDIR', '/tmp')
 
     def install(self, spec, prefix):
-        for filename in os.listdir(self.stage.source_path):
-            if os.path.isdir(filename):
-                shutil.copytree(filename, join_path(self.prefix, filename))
-            elif not fnmatch.fnmatch(filename, "spack-build.*") or \
-                    fnmatch.fnmatch(filename, "adf*"):
-                shutil.copy(filename, self.prefix)
+        install_tree('.', prefix)
