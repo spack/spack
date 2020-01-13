408a0de10183e4f78c42b9be0d07f51d88e5e0ab
Mehta, Kshitij V <kshitij-v-mehta@users.noreply.github.com>
2020-01-13 17:43:08 -0500

    codar-cheetah: Added version 0.5

--- var/spack/repos/builtin/packages/codar-cheetah/package.py
+++ var/spack/repos/builtin/packages/codar-cheetah/package.py
@@ -6,20 +6,19 @@
 from spack import *
 
 
-class CodarCheetah(Package):
+class CodarCheetah(PythonPackage):
     """CODAR Cheetah:
     The CODAR Experiment Harness for Exascale science applications.
     """
+
+    maintainers = ['kshitij-v-mehta']
 
     homepage = "https://github.com/CODARcode/cheetah"
     url      = "https://github.com/CODARcode/cheetah/archive/v0.1.tar.gz"
     git      = "https://github.com/CODARcode/cheetah.git"
 
-    version('develop', branch='master')
+    version('develop', branch='dev')
+    version('0.5', sha256='f37a554741eff4bb8407a68f799dd042dfc4df525e84896cad70fccbd6aca6ee')
     version('0.1', sha256='281564f8ae57a70ce28457616fde26247ea4efb29e55c7bf89a782a259a1a028')
 
-    depends_on('python@3:', type=('build', 'run'))
-    depends_on('savanna')
-
-    def install(self, spec, prefix):
-        install_tree('.', prefix)
+    depends_on('python@3.5:', type=('build', 'run'))
