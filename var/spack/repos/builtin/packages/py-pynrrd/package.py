03c9fb9dd5ca5e804b609b37ee281600e3ce4a05
MikeG <michael.gevaert@epfl.ch>
2020-01-14 08:54:42 +0100

    Update py-voxcell to have correct pynrrd dependency (#656)
    * add newer version of pynrrd (0.4.0)

--- var/spack/repos/builtin/packages/py-pynrrd/package.py
+++ var/spack/repos/builtin/packages/py-pynrrd/package.py
@@ -10,9 +10,10 @@ class PyPynrrd(PythonPackage):
     """Python library for reading and writing NRRD files into and from numpy arrays"""
 
     homepage = "https://github.com/mhe/pynrrd"
-    url = "https://pypi.io/packages/source/p/pynrrd/pynrrd-0.3.5.tar.gz"
+    url = "https://pypi.io/packages/source/p/pynrrd/pynrrd-0.4.0.tar.gz"
 
-    version('0.3.5', sha256='190bc76e26eebd8cfda5acfdbcecedd062f28e911aebe9399e370396e32c7e8e', preferred=True)
+    version('0.4.0', sha256='5c7d8b04e0e22aa2916829b91375076f89ecc5317a4c08b364cfd2f556bf018c', preferred=True)
+    version('0.3.5', sha256='190bc76e26eebd8cfda5acfdbcecedd062f28e911aebe9399e370396e32c7e8e')
     version('0.2.5', sha256='d5e50fd6300ca1f09d091fa2552953192767e322b7b10c3e7aa82b19be9b115b')
 
     depends_on('py-numpy', type='run')
