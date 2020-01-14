14914f54d8fc5ddc5f1221ef52dc116fc50baff2
Glenn P Johnson <glenn-johnson@uiowa.edu>
2020-01-14 15:14:14 -0600

    Consolidate the urls
    Consolidate the URLs in the `version` directives by using an if test in
    `url_for_version`.

--- var/spack/repos/builtin/packages/icu4c/package.py
+++ var/spack/repos/builtin/packages/icu4c/package.py
@@ -12,22 +12,17 @@ class Icu4c(AutotoolsPackage):
     C/C++ interface."""
 
     homepage = "http://site.icu-project.org/"
-    url      = "https://github.com/unicode-org/icu/releases/download/release-65-1/icu4c-65_1-src.tgz"
 
     version('65.1',
             sha256='53e37466b3d6d6d01ead029e3567d873a43a5d1c668ed2278e253b683136d948')
     version('64.1',
-            sha256='92f1b7b9d51b396679c17f35a2112423361b8da3c1b9de00aa94fd768ae296e6',
-            url='http://download.icu-project.org/files/icu4c/64.1/icu4c-64_1-src.tgz'),
+            sha256='92f1b7b9d51b396679c17f35a2112423361b8da3c1b9de00aa94fd768ae296e6')
     version('60.1',
-            sha256='f8f5a6c8fbf32c015a467972bdb1477dc5f5d5dfea908b6ed218715eeb5ee225',
-            url='http://download.icu-project.org/files/icu4c/60.1/icu4c-60_1-src.tgz'),
+            sha256='f8f5a6c8fbf32c015a467972bdb1477dc5f5d5dfea908b6ed218715eeb5ee225')
     version('58.2',
-            sha256='2b0a4410153a9b20de0e20c7d8b66049a72aef244b53683d0d7521371683da0c',
-            url='http://download.icu-project.org/files/icu4c/58.2/icu4c-58_2-src.tgz')
+            sha256='2b0a4410153a9b20de0e20c7d8b66049a72aef244b53683d0d7521371683da0c')
     version('57.1',
-            sha256='ff8c67cb65949b1e7808f2359f2b80f722697048e90e7cfc382ec1fe229e9581',
-            url='http://download.icu-project.org/files/icu4c/57.1/icu4c-57_1-src.tgz')
+            sha256='ff8c67cb65949b1e7808f2359f2b80f722697048e90e7cfc382ec1fe229e9581')
 
     variant('cxxstd',
             default='11',
@@ -43,8 +38,12 @@ class Icu4c(AutotoolsPackage):
     configure_directory = 'source'
 
     def url_for_version(self, version):
-        url = "https://github.com/unicode-org/icu/releases/download/release-{0}/icu4c-{1}-src.tgz"
-        return url.format(version.dashed, version.underscored)
+        if version >= Version('65'):
+            url = "https://github.com/unicode-org/icu/releases/download/release-{0}/icu4c-{1}-src.tgz"
+            return url.format(version.dashed, version.underscored)
+        else:
+            url = "http://download.icu-project.org/files/icu4c/{0}/icu4c-{1}-src.tgz"
+            return url.format(version.dotted, version.underscored)
 
     def flag_handler(self, name, flags):
         if name == 'cxxflags':
