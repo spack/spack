1c6458a722002cd9e42ac860f8e9bdb0bd9035b8
Hadrien Grasland <grasland@lal.in2p3.fr>
2020-01-15 11:33:51 +0100

    Comment it out instead of removing it

--- var/spack/repos/builtin/packages/ftgl/package.py
+++ var/spack/repos/builtin/packages/ftgl/package.py
@@ -16,11 +16,12 @@ class Ftgl(CMakePackage):
     version('2.4.0', commit='483639219095ad080538e07ceb5996de901d4e74')
     version('2.3.1', commit='3c0fdf367824b6381f29df3d8b4590240db62ab7')
 
-    variant('doc', default=False, description='Build the documentation')
+    # FIXME: Doc generation is broken in upstream build system
+    # variant('doc', default=False, description='Build the documentation')
     variant('shared', default=True, description='Build as a shared library')
 
     depends_on('cmake@2.8:', type='build')
-    depends_on('doxygen', type='build', when='+doc')
+    # depends_on('doxygen', type='build', when='+doc')  -- FIXME, see above
     depends_on('pkgconfig', type='build')
     depends_on('gl')
     depends_on('glu')
@@ -34,14 +35,15 @@ class Ftgl(CMakePackage):
         args = ['-DBUILD_SHARED_LIBS={0}'.format(spec.satisfies('+shared'))]
         return args
 
-    @run_after('build')
-    def build_docs(self):
-        if '+doc' in self.spec:
-            cmake = self.spec['cmake'].command
-            cmake('--build', '../spack-build', '--target', 'doc')
-
-    @run_after('install')
-    def install_docs(self):
-        if '+doc' in self.spec:
-            cmake = self.spec['cmake'].command
-            cmake('--install', '../spack-build', '--target', 'doc')
+    # FIXME: See doc variant comment
+    # @run_after('build')
+    # def build_docs(self):
+    #     if '+doc' in self.spec:
+    #         cmake = self.spec['cmake'].command
+    #         cmake('--build', '../spack-build', '--target', 'doc')
+    #
+    # @run_after('install')
+    # def install_docs(self):
+    #     if '+doc' in self.spec:
+    #         cmake = self.spec['cmake'].command
+    #         cmake('--install', '../spack-build', '--target', 'doc')
