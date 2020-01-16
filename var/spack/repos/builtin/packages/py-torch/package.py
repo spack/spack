38cd15d0e09a19a6e824fa8b1390ac813e7f48d2
Adam J. Stewart <ajstewart426@gmail.com>
2020-01-16 15:46:41 -0600

    Add py-torch 1.4.0 (#14543)

--- var/spack/repos/builtin/packages/py-torch/package.py
+++ var/spack/repos/builtin/packages/py-torch/package.py
@@ -51,6 +51,7 @@ class PyTorch(PythonPackage, CudaPackage):
     ]
 
     version('master', branch='master', submodules=True)
+    version('1.4.0', tag='v1.4.0', submodules=True)
     version('1.3.1', tag='v1.3.1', submodules=True)
     version('1.3.0', tag='v1.3.0', submodules=True)
     version('1.2.0', tag='v1.2.0', submodules=True)
