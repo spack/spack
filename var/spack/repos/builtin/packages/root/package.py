spack/spack

commit aac3799d2c2ed87373f89372942cc42a6aa6a6e9

Unset SPACK_INCLUDE_DIRS so the compiler wrapper does not prepend any -I flags. The assumption is that cmake will set the correct include flags. (#11129)

var/spack/repos/builtin/packages/root/package.py
@@ -463,6 +463,7 @@ class Root(CMakePackage):
 
     def setup_environment(self, spack_env, run_env):
         run_env.prepend_path('PYTHONPATH', self.prefix.lib)
+        spack_env.set('SPACK_INCLUDE_DIRS', '')
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         spack_env.set('ROOTSYS', self.prefix)

commit 9749a5406abc8df63956ae926c8e947cb594c6a3

Fix root+x breakage from #11129

var/spack/repos/builtin/packages/root/package.py
@@ -428,6 +428,17 @@ class Root(CMakePackage):
             env.append_path('CMAKE_PREFIX_PATH',
                             self.spec['lz4'].prefix)
         env.set('SPACK_INCLUDE_DIRS', '', force=True)
+        if '+x' in self.spec:
+            env.append_path('SPACK_INCLUDE_DIRS',
+                            self.spec['fontconfig'].prefix.include)
+            env.append_path('SPACK_INCLUDE_DIRS',
+                            self.spec['glew'].prefix.include)
+            env.append_path('SPACK_INCLUDE_DIRS',
+                            self.spec['libx11'].prefix.include)
+            env.append_path('SPACK_INCLUDE_DIRS',
+                            self.spec['mesa-glu'].prefix.include)
+            env.append_path('SPACK_INCLUDE_DIRS',
+                            self.spec['xproto'].prefix.include)
 
     def setup_run_environment(self, env):
         env.set('ROOTSYS', self.prefix)