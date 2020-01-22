spack/spack

commit aac3799d2c2ed87373f89372942cc42a6aa6a6e9

Unset SPACK_INCLUDE_DIRS so the compiler wrapper does not prepend any -I flags. The assumption is that cmake will set the correct include flags. (#11129)

var/spack/repos/builtin/packages/root/package.py
@@ -463,6 +463,7 @@ class Root(CMakePackage):
 
     def setup_environment(self, spack_env, run_env):
         run_env.prepend_path('PYTHONPATH', self.prefix.lib)
+        spack_env.set('SPACK_INCLUDE_DIRS', '')
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         spack_env.set('ROOTSYS', self.prefix)

commit 15ec1cd41fe7e542868878bd46ae045ea9507a07

Not strictly X11-related, but more breakage from  #11129

var/spack/repos/builtin/packages/root/package.py
@@ -428,6 +428,9 @@ class Root(CMakePackage):
             env.append_path('CMAKE_PREFIX_PATH',
                             self.spec['lz4'].prefix)
         env.set('SPACK_INCLUDE_DIRS', '', force=True)
+        if self.spec.satisfies('@:6.12.99'):
+            env.append_path('SPACK_INCLUDE_DIRS',
+                            self.spec['zlib'].prefix.include)
         if '+x' in self.spec:
             env.append_path('SPACK_INCLUDE_DIRS',
                             self.spec['fontconfig'].prefix.include)