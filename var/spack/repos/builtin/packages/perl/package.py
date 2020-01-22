6da00536bd767688570b274832244e031c53efce
dahlgren <dahlgren1@llnl.gov>
2020-01-21 17:33:44 -0800

    Added some standard use statements

--- var/spack/repos/builtin/packages/perl/package.py
+++ var/spack/repos/builtin/packages/perl/package.py
@@ -305,7 +305,7 @@ class Perl(Package):  # Perl doesn't use Autotools, it should subclass Package
         assert '(v{0})'.format(self.spec.version) in output
 
         tty.msg('test: Ensuring perl runs')
-        message = 'Hello, World!\n'
-        output = perl('-e', 'print("{0}");'.format(message),
-                      output=str.split, error=str.split)
+        message = 'Hello, World!'
+        output = perl('-e', 'use warnings; use strict;\nprint("{0}");'.
+		      format(message), output=str.split, error=str.split)
         assert output == message
