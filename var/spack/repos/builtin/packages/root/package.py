spack/spack

commit aac3799d2c2ed87373f89372942cc42a6aa6a6e9

Unset SPACK_INCLUDE_DIRS so the compiler wrapper does not prepend any -I flags. The assumption is that cmake will set the correct include flags. (#11129)

var/spack/repos/builtin/packages/root/package.py
@@ -463,6 +463,7 @@ class Root(CMakePackage):
 
     def setup_environment(self, spack_env, run_env):
         run_env.prepend_path('PYTHONPATH', self.prefix.lib)
+        spack_env.set('SPACK_INCLUDE_DIRS', '')
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         spack_env.set('ROOTSYS', self.prefix)

commit 1a385a5178bb8f78f93c1eb215ad497337729128

root: Fix root+x breakage from #11129 (#14224)
* Fix root+x breakage from #11129

* Separate out +opengl breakage

* Not strictly X11-related, but more breakage from  #11129

* Another X11 breakage found while building 6.08.x

* Don't put system headers in SPACK_INCLUDE_DIRS + deduplicate

* xextproto is only a dependency in +x builds

var/spack/repos/builtin/packages/root/package.py
@@ -5,6 +5,7 @@
 
 
 from spack import *
+from spack.util.environment import is_system_path
 import sys
 
 
@@ -427,7 +428,35 @@ class Root(CMakePackage):
         if 'lz4' in self.spec:
             env.append_path('CMAKE_PREFIX_PATH',
                             self.spec['lz4'].prefix)
+
+        # This hack is made necessary by a header name collision between
+        # asimage's "import.h" and Python's "import.h" headers...
         env.set('SPACK_INCLUDE_DIRS', '', force=True)
+
+        # ...but it breaks header search for any ROOT dependency which does not
+        # use CMake. To resolve this, we must bring back those dependencies's
+        # include paths into SPACK_INCLUDE_DIRS.
+        #
+        # But in doing so, we must be careful not to inject system header paths
+        # into SPACK_INCLUDE_DIRS, even in a deprioritized form, because some
+        # system/compiler combinations don't like having -I/usr/include around.
+        def add_include_path(dep_name):
+            include_path = self.spec[dep_name].prefix.include
+            if not is_system_path(include_path):
+                env.append_path('SPACK_INCLUDE_DIRS', include_path)
+
+        # With that done, let's go fixing those deps
+        if self.spec.satisfies('+x @:6.08.99'):
+            add_include_path('xextproto')
+        if self.spec.satisfies('@:6.12.99'):
+            add_include_path('zlib')
+        if '+x' in self.spec:
+            add_include_path('fontconfig')
+            add_include_path('libx11')
+            add_include_path('xproto')
+        if '+opengl' in self.spec:
+            add_include_path('glew')
+            add_include_path('mesa-glu')
 
     def setup_run_environment(self, env):
         env.set('ROOTSYS', self.prefix)