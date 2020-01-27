3dde075452d70c2f524f1b69a139277ca46e5573
Joseph M. Koning <koning1@llnl.gov>
2020-01-27 12:08:09 -0800

    Remove mysql variant until new mysql interface module is enabled. The mysql code may be removed.

--- var/spack/repos/builtin/packages/py-merlinwf/package.py
+++ var/spack/repos/builtin/packages/py-merlinwf/package.py
@@ -10,16 +10,14 @@ class PyMerlinwf(PythonPackage):
     """Merlin Workflow for HPC."""
 
     homepage = "https://github.com/LLNL/merlin"
-    url      = "https://pypi.io/packages/source/m/merlinwf/merlinwf-1.2.2.tar.gz"
+    url      = "https://pypi.io/packages/source/m/merlinwf/merlinwf-1.2.3.tar.gz"
     git      = "https://github.com/LLNL/merlin.git"
 
-    version('1.2.2', sha256='37b92b1781e6409ba27a24ca04a41e10c9e6fdb1e8e40a6951ca0d214fbb42e3')
+    version('1.2.3', sha256='6b13a315f3e8e2894ea05d9cc072639f02eaf71ae0fdbd2bafebd1c20c8470ab')
     version('1.1.1', sha256='306055a987e42a79ce348a3f9d71293ed8a9b7f5909c26b6fd233d6a176fff6d')
     version('1.0.5', sha256='d66f50eac84ff9d7aa484f2d9655dc60f0352196d333284d81b6623a6f0aa180')
     version('master', branch='master')
     version('develop', branch='develop')
-
-    variant('mysql', default=False, description="Support for mysql results backend")
 
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
@@ -38,7 +36,3 @@ class PyMerlinwf(PythonPackage):
     depends_on('py-psutil@5.1.0:', type=('build', 'run'))
     depends_on('py-pyyaml@5.1.2:', type=('build', 'run'))
     depends_on('py-tabulate', type=('build', 'run'))
-
-    # Optional packages
-    depends_on('py-mysql-connector-python-rf', when='+mysql', type=('build', 'run'))
-    depends_on('py-pymysql', when='+mysql', type=('build', 'run'))
