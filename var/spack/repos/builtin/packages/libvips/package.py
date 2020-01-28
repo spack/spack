fe944bf8431f521cb879a2b0c78a9257a2721a3a
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2020-01-28 00:48:31 -0600

    update

--- var/spack/repos/builtin/packages/libvips/package.py
+++ var/spack/repos/builtin/packages/libvips/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -7,27 +7,31 @@ from spack import *
 
 
 class Libvips(AutotoolsPackage):
-    """A fast image processing library with low memory needs."""
+    """libvips is a demand-driven, horizontally threaded image processing
+    library. Compared to similar libraries, libvips runs quickly and uses
+    little memory."""
 
     homepage = "https://libvips.github.io/libvips/"
-    url      = "https://github.com/libvips/libvips/releases/download/v8.7.4/vips-8.7.4.tar.gz"
+    url      = "https://github.com/libvips/libvips/releases/download/v8.9.0/vips-8.9.0.tar.gz"
     git      = "https://github.com/libvips/libvips.git"
 
     version('develop', branch='master')
-    version('8.7.4',sha256 = 'ce7518a8f31b1d29a09b3d7c88e9852a5a2dcb3ee1501524ab477e433383f205')
+    version('8.9.0', sha256="97334a5e70aff343d2587f23cb8068fc846a58cd937c89a446142ccf00ea0349")
+    version('8.7.4', sha256="ce7518a8f31b1d29a09b3d7c88e9852a5a2dcb3ee1501524ab477e433383f205")
 
     depends_on('glib')
     depends_on('gobject-introspection')
     depends_on('expat')
     depends_on('swig',when='@develop')
     depends_on('libjpeg')
-    #depends_on('giflib')
     depends_on('libtiff')
     depends_on('poppler')
     depends_on('libtiff')
     depends_on('fftw')
     depends_on('lcms')
     depends_on('libpng')
+
+    # TODO : Add more dependencies/config options!
 
     def configure_args(self):
         args = []
