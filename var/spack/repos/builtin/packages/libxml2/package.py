bd60e0f13772b4769cbf6831bcd3bc6497fb121b
Greg Becker <becker33@llnl.gov>
2020-01-29 12:39:55 -0800

    fix cycle dependency in libxml+python (#13847)
    * fix cycle dependency in libxml+python
    * comment why we need these dependencies

--- var/spack/repos/builtin/packages/libxml2/package.py
+++ var/spack/repos/builtin/packages/libxml2/package.py
@@ -27,7 +27,8 @@ class Libxml2(AutotoolsPackage):
     depends_on('zlib')
     depends_on('xz')
 
-    depends_on('python+shared', when='+python')
+    # avoid cycle dependency for concretizer
+    depends_on('python+shared~libxml2', when='+python')
     extends('python', when='+python',
             ignore=r'(bin.*$)|(include.*$)|(share.*$)|(lib/libxml2.*$)|'
             '(lib/xml2.*$)|(lib/cmake.*$)')
