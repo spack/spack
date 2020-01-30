54c6e3d2143a909271338820dd6df1b67c215794
Patrick Gartung <gartung@fnal.gov>
2020-01-30 14:49:55 -0600

    Add binary_distribution::get_spec which takes concretized spec Add binary_distribution::try_download_specs for downloading of spec.yaml files to cache get_spec is used by package::try_install_from_binary_cache to download only the spec.yaml for the concretized spec if it exists.

--- lib/spack/spack/binary_distribution.py
+++ lib/spack/spack/binary_distribution.py
@@ -660,15 +660,46 @@ def extract_tarball(spec, filename, allow_root=False, unsigned=False,
         shutil.rmtree(tmpdir)
 
 
-#: Internal cache for get_specs
-_cached_specs = None
 
+def get_spec(spec=None, force=False):
+    """
+    Check if spec.yaml exists on mirrors and return it if it does
+    """
+    urls = set()
+    if spec is None:
+        return {}
+    specfile_name = tarball_name(spec, '.spec.yaml')
+    if _cached_specs:
+        tty.debug("Using previously-retrieved specs")
+        return _cached_specs
+
+    if not spack.mirror.MirrorCollection():
+        tty.debug("No Spack mirrors are currently configured")
+        return {}
+
+    for mirror in spack.mirror.MirrorCollection().values():
+        fetch_url_build_cache = url_util.join(
+            mirror.fetch_url, _build_cache_relative_path)
+
+        mirror_dir = url_util.local_file_path(fetch_url_build_cache)
+        if mirror_dir:
+            tty.msg("Finding buildcaches in %s" % mirror_dir)
+            mirror_spec_path=os.path.join(mirror_dir, specfile_name)
+            link = url_util.join(fetch_url_build_cache, specfile_name)
+            urls.add(link)
+
+        else:
+            tty.msg("Finding buildcaches at %s" %
+                    url_util.format(fetch_url_build_cache))
+            link = url_util.join(fetch_url_build_cache, specfile_name)
+            urls.add(link)
+
+    return try_download_specs(urls=urls, force=force)
 
 def get_specs(force=False, use_arch=False, names=[]):
     """
     Get spec.yaml's for build caches available on mirror
     """
-    global _cached_specs
 
     arch = architecture.Arch(architecture.platform(),
                              'default_os', 'default_target')
@@ -714,7 +745,20 @@ def get_specs(force=False, use_arch=False, names=[]):
                 m = name_re.search(link)
                 if m:
                     urls.add(link)
+
+    return try_download_specs(urls=urls, force=force)
+
+#: Internal cache for try_download_specs
+_cached_specs = None
+
+def try_download_specs(urls=None, force=False):
+    '''
+    Try to download the urls and cache them
+    '''
+    global _cached_specs
     _cached_specs = []
+    if urls is None:
+        return {}
     for link in urls:
         with Stage(link, name="build_cache", keep=True) as stage:
             if force and os.path.exists(stage.save_filename):
