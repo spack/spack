8ecdccf0c92deebfa5213a26e0e7c220a256c34b
Ivan Razumov <ivan.razumov@cern.ch>
2020-02-03 11:42:22 +0100

    I think I've finally got it

--- var/spack/repos/builtin/packages/py-tables/package.py
+++ var/spack/repos/builtin/packages/py-tables/package.py
@@ -29,7 +29,8 @@ class PyTables(PythonPackage):
     variant('lzo', default=False, description='Support for lzo compression')
 
     # requirements.txt
-    depends_on('python@2.7:2.7.999,3.0:', when='@3.2.2:', type=('build', 'run'))
+    depends_on('python@2.6:2.8,3.5:', when='@3.4:', type=('build', 'run'))
+    depends_on('python@2.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-cython@0.21:', type='build')
     depends_on('py-numpy@1.9.3:', type=('build', 'run'))
