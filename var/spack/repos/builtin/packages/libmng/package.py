eaf282f2df0079fdbd1b987dab80620563b06af3
Seth R Johnson <johnsonsr@ornl.gov>
2020-02-04 15:32:18 -0600

    Simplify cmake logic and remove unused variant

--- var/spack/repos/builtin/packages/libmng/package.py
+++ var/spack/repos/builtin/packages/libmng/package.py
@@ -16,8 +16,6 @@ class Libmng(CMakePackage):
     version('2.0.3', sha256='cf112a1fb02f5b1c0fce5cab11ea8243852c139e669c44014125874b14b7dfaa')
     version('2.0.2', sha256='4908797bb3541fb5cd8fffbe0b1513ed163509f2a4d57a78b26a96f8d1dd05a2')
 
-    variant('shared', default=True, description="Build shared library version")
-
     depends_on("gzip")
     depends_on("jpeg")
     depends_on("zlib")
@@ -29,8 +27,5 @@ class Libmng(CMakePackage):
                     '#include<stdio.h>\n\\1', 'libmng_types.h')
 
     def cmake_args(self):
-        args = [('WITH_LCMS2', True),
-                ('WITH_LCMS1', False)]
-
-        return ['-D{0}:BOOL={1}'.format(key, 'ON' if val else 'OFF')
-                for (key, val) in args]
+        return ['-DWITH_LCMS2:BOOL=ON',
+                '-DWITH_LCMS1:BOOL=OFF']
