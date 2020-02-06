7198cb4f20bcd82ec4920831c9e728592ac20b81
Ivan Razumov <ivan.razumov@cern.ch>
2020-02-06 08:41:23 +0100

    Add dependency on geant4-data

--- var/spack/repos/builtin/packages/geant4/package.py
+++ var/spack/repos/builtin/packages/geant4/package.py
@@ -71,6 +71,7 @@ class Geant4(CMakePackage):
 
     depends_on('geant4-data@10.03.p03', when='@10.03.p03 ~data')
     depends_on('geant4-data@10.04', when='@10.04 ~data')
+    depends_on('geant4-data@10.05.p01', when='@10.05.p01 ~data')
 
     # As released, 10.03.03 has issues with respect to using external
     # CLHEP.
