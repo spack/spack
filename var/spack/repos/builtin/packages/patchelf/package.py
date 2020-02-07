2af58a154a5a59895e9188e53ed198b7c720e702
darmac <xiaojun2@hisilicon.com>
2020-02-07 16:13:07 +0800

    patchelf : change tar.gz url to tar.bz2 url

--- var/spack/repos/builtin/packages/patchelf/package.py
+++ var/spack/repos/builtin/packages/patchelf/package.py
@@ -11,10 +11,10 @@ class Patchelf(AutotoolsPackage):
        ELF executables."""
 
     homepage = "https://nixos.org/patchelf.html"
-    url      = "https://nixos.org/releases/patchelf/patchelf-0.10/patchelf-0.10.tar.gz"
+    url      = "https://nixos.org/releases/patchelf/patchelf-0.10/patchelf-0.10.tar.bz2"
     list_url = "https://nixos.org/releases/patchelf/"
     list_depth = 1
 
-    version('0.10', sha256='b2deabce05c34ce98558c0efb965f209de592197b2c88e930298d740ead09019')
-    version('0.9',  sha256='f2aa40a6148cb3b0ca807a1bf836b081793e55ec9e5540a5356d800132be7e0a')
-    version('0.8',  sha256='14af06a2da688d577d64ff8dac065bb8903bbffbe01d30c62df7af9bf4ce72fe')
+    version('0.10', sha256='f670cd462ac7161588c28f45349bc20fb9bd842805e3f71387a320e7a9ddfcf3')
+    version('0.9',  sha256='a0f65c1ba148890e9f2f7823f4bedf7ecad5417772f64f994004f59a39014f83')
+    version('0.8',  sha256='c99f84d124347340c36707089ec8f70530abd56e7827c54d506eb4cc097a17e7')
