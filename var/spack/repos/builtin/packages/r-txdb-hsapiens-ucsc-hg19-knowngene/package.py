46df26d6ea61ffa4c1494cf008c530347210d122
Glenn P Johnson <glenn-johnson@uiowa.edu>
2020-02-08 14:05:11 -0600

    New package: r-watermelon
    This PR creates the r-watermelon package, along with dependencies.
    
    - new package: r-fdb-infiniummethylation-hg19
    - new package: r-illuminahumanmethylation450kanno-ilmn12-hg19
    - new package: r-lumi
    - new package: r-methylumi
    - new package: r-roc
    - new package: r-txdb-hsapiens-ucsc-hg19-knowngene
    - updated package: r-matrixstats, new version needed as a dependency

---
+++ var/spack/repos/builtin/packages/r-txdb-hsapiens-ucsc-hg19-knowngene/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RTxdbHsapiensUcscHg19Knowngene(RPackage):
+    """Exposes an annotation databases generated from UCSC by exposing these as
+    TxDb objects."""
+
+    # This is a bioconductor package but there is no available git repo.
+    homepage = "https://bioconductor.org/packages/release/data/annotation/html/TxDb.Hsapiens.UCSC.hg19.knownGene.html"
+    url      = "https://bioconductor.org/packages/release/data/annotation/src/contrib/TxDb.Hsapiens.UCSC.hg19.knownGene_3.2.2.tar.gz"
+
+    version('3.2.2', sha256='063de2b1174782a0b2b8ab7f04a0bdf3c43252cb67c685a9f8ef2b8e318352e9')
+
+    depends_on('r-genomicfeatures@1.21.30:', type=('build', 'run'))
+    depends_on('r-annotationdbi', type=('build', 'run'))
