920357883c2b9c6467198f50fe4dca4c5f702bf0
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2020-02-13 10:24:42 -0800

    axl: update for versions 0.3 and 0.2

--- var/spack/repos/builtin/packages/axl/package.py
+++ var/spack/repos/builtin/packages/axl/package.py
@@ -26,6 +26,8 @@ class Axl(CMakePackage):
     tags = ['ecp']
 
     version('master', branch='master')
+    version('0.3.0', sha256='3f5efff87be700a5792a0ee9a7aeae45c640e2936623b024e8bc1056f7952a46')
+    version('0.2.0', sha256='a0babe3576da30919f89df2f83c76bd01d06345919f2e54d4dddcd6f73faedcc')
     version('0.1.1', sha256='ebbf231bb542a6c91efb79fce05d4c8a346d5506d88ae1899fb670be52e81933')
 
     variant('async_api', default='daemon',
