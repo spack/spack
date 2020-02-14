aa8fa286272a3cd0621c0421518953b9b4ab2263
Ivan Razumov <ivan.razumov@cern.ch>
2020-02-14 15:32:52 +0100

    Add extra version of py-jsonschema

--- var/spack/repos/builtin/packages/py-jsonschema/package.py
+++ var/spack/repos/builtin/packages/py-jsonschema/package.py
@@ -12,6 +12,7 @@ class PyJsonschema(PythonPackage):
     homepage = "http://github.com/Julian/jsonschema"
     url      = "https://pypi.io/packages/source/j/jsonschema/jsonschema-2.6.0.tar.gz"
 
+    version('3.0.1', sha256='0c0a81564f181de3212efa2d17de1910f8732fa1b71c42266d983cd74304e20d')
     version('2.6.0', sha256='6ff5f3180870836cae40f06fa10419f557208175f13ad7bc26caa77beb1f6e02')
     version('2.5.1', sha256='36673ac378feed3daa5956276a829699056523d7961027911f064b52255ead41')
 
