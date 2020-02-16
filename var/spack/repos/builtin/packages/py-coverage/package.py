6b7f678d33bd1eefe9c5723b822cc3f210718aae
iarspider <iarspider@gmail.com>
2020-02-16 05:48:10 +0100

    Add extra version of py-coverage (#14976)

--- var/spack/repos/builtin/packages/py-coverage/package.py
+++ var/spack/repos/builtin/packages/py-coverage/package.py
@@ -13,6 +13,7 @@ class PyCoverage(PythonPackage):
     url      = "https://pypi.io/packages/source/c/coverage/coverage-4.5.4.tar.gz"
 
     version('4.5.4', sha256='e07d9f1a23e9e93ab5c62902833bf3e4b1f65502927379148b6622686223125c')
+    version('4.5.3', sha256='9de60893fb447d1e797f6bf08fdf0dbcda0c1e34c1b06c92bd3a363c0ea8c609')
     version('4.3.4', sha256='eaaefe0f6aa33de5a65f48dd0040d7fe08cac9ac6c35a56d0a7db109c3e733df')
     version('4.0a6', sha256='85c7f3efceb3724ab066a3fcccc05b9b89afcaefa5b669a7e2222d31eac4728d')
 
