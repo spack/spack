bcccf0d4e7964f5c98761c5d3c621ab1cb23dc0b
iarspider <iarspider@gmail.com>
2020-02-16 05:35:47 +0100

    Add extra version of py-importlib-metadata (#14983)

--- var/spack/repos/builtin/packages/py-importlib-metadata/package.py
+++ var/spack/repos/builtin/packages/py-importlib-metadata/package.py
@@ -15,6 +15,7 @@ class PyImportlibMetadata(PythonPackage):
     version('1.2.0', sha256='41e688146d000891f32b1669e8573c57e39e5060e7f5f647aa617cd9a9568278')
     version('0.23',  sha256='aa18d7378b00b40847790e7c27e11673d7fed219354109d0e7b9e5b25dc3ad26')
     version('0.19',  sha256='23d3d873e008a513952355379d93cbcab874c58f4f034ff657c7a87422fa64e8')
+    version('0.18',  sha256='cb6ee23b46173539939964df59d3d72c3e0c1b5d54b84f1d8a7e912fe43612db')
 
     depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
