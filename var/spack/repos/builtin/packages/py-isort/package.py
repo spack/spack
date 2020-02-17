38d9d5d3ce6352fe79764b19b5278a0f5d50a58b
iarspider <iarspider@gmail.com>
2020-02-17 14:14:28 +0100

    Update package.py

--- var/spack/repos/builtin/packages/py-isort/package.py
+++ var/spack/repos/builtin/packages/py-isort/package.py
@@ -15,4 +15,10 @@ class PyIsort(PythonPackage):
     version('4.3.20', sha256='c40744b6bc5162bbb39c1257fe298b7a393861d50978b565f3ccd9cb9de0182a')
     version('4.2.15', sha256='79f46172d3a4e2e53e7016e663cc7a8b538bec525c36675fcfd2767df30b3983')
 
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build', 'install'))
+    depends_on('py-pytest', type='test')
+    depends_on('py-mock', type='test')
+    depends_on('python@2.6:2.7.999,3.3:', when='@4.2.15:')
+    depends_on('python@2.7:2.7.999,3.4:', when='@4.3.0:')
+    depends_on('py-futures', when='@4.3.0: ^python@2.7:2.7.999,:3.1')
+    depends_on('py-backports-functools-lru-cache', when='@4.3.10: ^python@:3.2')
