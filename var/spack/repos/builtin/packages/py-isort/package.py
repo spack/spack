82e58822539bc08a7a768df1242ebe033260bf75
iarspider <iarspider@gmail.com>
2020-02-17 19:38:52 +0100

    Update var/spack/repos/builtin/packages/py-isort/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-isort/package.py
+++ var/spack/repos/builtin/packages/py-isort/package.py
@@ -18,7 +18,7 @@ class PyIsort(PythonPackage):
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-pytest', type='test')
     depends_on('py-mock', type='test')
-    depends_on('python@2.6:2.7.999,3.3:', type=('build', 'run'), when='@4.2.15:')
+    depends_on('python@2.6:2.8,3.3:', type=('build', 'run'))
     depends_on('python@2.7:2.7.999,3.4:', type=('build', 'run'), when='@4.3.0:')
     depends_on('py-futures', type=('build', 'run'), when='@4.3.0: ^python@2.7:2.7.999,:3.1')
     depends_on('py-backports-functools-lru-cache', type=('build', 'run'), when='@4.3.10: ^python@:3.2')
