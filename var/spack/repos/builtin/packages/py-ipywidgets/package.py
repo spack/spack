251ad5741779eb4b1054cd1ef341d1f37867ac31
iarspider <iarspider@gmail.com>
2020-02-17 14:26:22 +0100

    Update var/spack/repos/builtin/packages/py-ipywidgets/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-ipywidgets/package.py
+++ var/spack/repos/builtin/packages/py-ipywidgets/package.py
@@ -15,7 +15,7 @@ class PyIpywidgets(PythonPackage):
     version('7.4.2', sha256='f156165e8a855ed862fdf48e72700bdcd6956d089a2018c5b36d358255d45b2b')
     version('5.2.2', sha256='d61ab8bb12b90981a3a6010429816d70eaa041e622043207bcb74239b664d4f3')
 
-    depends_on('python@2.7:2.8,3.3:')
+    depends_on('python@2.7:2.8,3.3:', type=('build', 'run'))
     depends_on('py-ipython@4.0.0:', type=('build', 'run'))
     depends_on('py-ipykernel@4.2.2:', type=('build', 'run'))
     depends_on('py-traitlets@4.2.1:', type=('build', 'run'))
