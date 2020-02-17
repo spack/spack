f40345aeba9b9eb199810a84dd8baad92a8c5305
Ivan Razumov <ivan.razumov@cern.ch>
2020-02-17 17:38:14 +0100

    Add extra dependencies for py-dask+distributed

---
+++ var/spack/repos/builtin/packages/py-distributed/package.py
@@ -0,0 +1,32 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyDistributed(PythonPackage):
+    """Distributed scheduler for Dask"""
+
+    homepage = "https://distributed.dask.org/"
+    url      = "py-distributed"
+
+    version('2.10.0', '300e5495d0306f472c1aa34a522273e3066b9f50d3eb25cee17cd3b6177eea54')
+
+    depends_on('python@2.7:2.7.999,3.5:')
+    depends_on('py-click@6.6:')
+    depends_on('py-cloudpickle@0.2.2:')
+    depends_on('py-msgpack')
+    depends_on('py-psutil@5.0:')
+    depends_on('py-six')
+    depends_on('py-sortedcontainers')
+    conflicts('py-sortedcontainers@2.0.0,2.0.1')
+    depends_on('py-tblib')
+    depends_on('py-toolz@0.7.4:')
+    depends_on('py-tornado@5:')
+    depends_on('py-zict@0.1.3:')
+    depends_on('py-pyyaml')
+    depends_on('py-futures', when='@:2.9.999')
+    depends_on('py-singledispatch', when='@:3.3.999')
+
---
+++ var/spack/repos/builtin/packages/py-heapdict/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyHeapdict(PythonPackage):
+    """A heap with decrease-key and increase-key operations"""
+
+    homepage = "http://stutzbachenterprises.com/"
+    url      = "https://files.pythonhosted.org/packages/source/h/heapdict/HeapDict-1.0.1.tar.gz"
+
+    version('1.0.1', sha256='8495f57b3e03d8e46d5f1b2cc62ca881aca392fd5cc048dc0aa2e1a6d23ecdb6')
+
+    depends_on('python@2.6:2.7.999,3.0:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+
