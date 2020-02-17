17f19ad4078a709ab6280c6332407af6014a3b06
iarspider <iarspider@gmail.com>
2020-02-18 00:38:53 +0100

    Add extra version of py-dill (#14971)
    * Add extra version of py-dill
    
    * Update package.py

--- var/spack/repos/builtin/packages/py-dill/package.py
+++ var/spack/repos/builtin/packages/py-dill/package.py
@@ -12,6 +12,7 @@ class PyDill(PythonPackage):
     homepage = "https://github.com/uqfoundation/dill"
     url      = "https://pypi.io/packages/source/d/dill/dill-0.2.7.tar.gz"
 
+    version('0.2.9', sha256='f6d6046f9f9195206063dd0415dff185ad593d6ee8b0e67f12597c0f4df4986f')
     version('0.2.7', sha256='ddda0107e68e4eb1772a9f434f62a513c080c7171bd0dd6fb65d992788509812')
     version('0.2.6', sha256='6c1ccca68be483fa8c66e85a89ffc850206c26373aa77a97b83d8d0994e7f1fd')
     version('0.2.5', sha256='e82b3db7b9d962911c9c2d5cf2bb4a04f43933f505a624fb7dc5f68b949f0a5c')
@@ -21,7 +22,7 @@ class PyDill(PythonPackage):
     version('0.2.1', sha256='a54401bdfae419cfe1c9e0b48e9b290afccaa413d2319d9bb0fdb85c130a7923')
     version('0.2', sha256='aba8d4c81c4136310e6ce333bd6f4f3ea2d53bd367e2f69c864428f260c0308c')
 
-    depends_on('python@2.5:2.8,3.1:')
+    depends_on('python@2.5:2.8,3.1:', type=('build', 'run'))
 
     depends_on('py-setuptools@0.6:', type='build')
 
