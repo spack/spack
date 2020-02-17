5b398eb74cb06091172dc08496fb29dc42442ae3
iarspider <iarspider@gmail.com>
2020-02-17 11:22:29 +0100

    Update var/spack/repos/builtin/packages/py-lxml/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-lxml/package.py
+++ var/spack/repos/builtin/packages/py-lxml/package.py
@@ -29,4 +29,4 @@ class PyLxml(PythonPackage):
     depends_on('libxslt', type=('build', 'run'))
     depends_on('py-html5lib', when='+html5', type=('build', 'run'))
     depends_on('py-beautifulsoup4', when='+htmlsoup', type=('build', 'run'))
-    depends_on('py-cssselect', when='+css', type=('build', 'run'))
+    depends_on('py-cssselect@0.7:', when='+cssselect', type=('build', 'run'))
