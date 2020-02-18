0c229f69ce5731911c0dd98a80815e520c74515f
iarspider <iarspider@gmail.com>
2020-02-18 19:29:57 +0100

    Flake-8

--- var/spack/repos/builtin/packages/py-cryptography/package.py
+++ var/spack/repos/builtin/packages/py-cryptography/package.py
@@ -17,19 +17,19 @@ class PyCryptography(PythonPackage):
     version('2.7',   sha256='e6347742ac8f35ded4a46ff835c60e68c22a536a8ae5c4422966d06946b6d4c6')
     version('2.3.1', sha256='8d10113ca826a4c29d5b85b2c4e045ffa8bad74fb525ee0eceb1d38d4c70dfd6')
     version('1.8.1', sha256='323524312bb467565ebca7e50c8ae5e9674e544951d28a2904a50012a8828190')
-    
+
     variant('idna', default=False, description='Deprecated U-label support')
     conflicts('+idna', when='@:2.4')
 
     # dependencies taken from https://github.com/pyca/cryptography/blob/master/setup.py
     depends_on('python@2.6:2.8,3.4:',                 type=('build', 'run'))
     depends_on('python@2.7:2.8,3.4:', when='@2.3.1:', type=('build', 'run'))
-    
+
     depends_on('py-setuptools@20.5:',   type='build')
-    
+
     depends_on('py-cffi@1.4.1:',             type=('build', 'run'))
     depends_on('py-cffi@1.8:1.11.2,1.11.4:', type=('build', 'run'), when='@2.7:')
-    
+
     depends_on('py-asn1crypto@0.21.0:', type=('build', 'run'))
     depends_on('py-six@1.4.1:',         type=('build', 'run'))
     depends_on('py-idna@2.1:',          type=('build', 'run'), when='@:2.4')  # deprecated
