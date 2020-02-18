34fae870a10a16a80c900a0e69406957043091cb
iarspider <iarspider@gmail.com>
2020-02-18 20:49:42 +0100

    Update var/spack/repos/builtin/packages/py-distributed/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-distributed/package.py
+++ var/spack/repos/builtin/packages/py-distributed/package.py
@@ -17,7 +17,7 @@ class PyDistributed(PythonPackage):
 
     depends_on('python@2.7:2.8,3.5:', when='@:1', type=('build', 'run'))
     depends_on('python@3.6:', when='@2:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
 
     depends_on('py-click@6.6:', type=('build', 'run'))
     depends_on('py-cloudpickle@0.2.2:', type=('build', 'run'))
