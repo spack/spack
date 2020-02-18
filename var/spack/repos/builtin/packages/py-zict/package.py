93c17f98dabb7232465de6a516c6c5e15abee99e
iarspider <iarspider@gmail.com>
2020-02-18 14:53:40 +0100

    Update package.py

--- var/spack/repos/builtin/packages/py-zict/package.py
+++ var/spack/repos/builtin/packages/py-zict/package.py
@@ -14,7 +14,5 @@ class PyZict(PythonPackage):
 
     version('1.0.0', sha256='e34dd25ea97def518fb4c77f2c27078f3a7d6c965b0a3ac8fe5bdb0a8011a310')
 
-    depends_on('python', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-heapdict', type=('build', 'run'))
-
