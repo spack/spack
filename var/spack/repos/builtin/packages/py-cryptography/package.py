b3aa708dbed57ae64b463058b565dfa8336052f8
iarspider <iarspider@gmail.com>
2020-02-18 09:02:17 +0100

    Update var/spack/repos/builtin/packages/py-cryptography/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-cryptography/package.py
+++ var/spack/repos/builtin/packages/py-cryptography/package.py
@@ -22,7 +22,7 @@ class PyCryptography(PythonPackage):
     conflicts('+idna', when='@:2.4')
 
     # dependencies taken from https://github.com/pyca/cryptography/blob/master/setup.py
-    depends_on('python@2.6:2.7.999,3.4:')
+    depends_on('python@2.6:2.8,3.4:', type=('build', 'run'))
     depends_on('python@2.7:2.7.999,3.4:', when='@2.3.1:')
     depends_on('py-setuptools@20.5:',   type='build')
     
