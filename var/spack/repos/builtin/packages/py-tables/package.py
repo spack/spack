57f98b6473c1f91173f19fd15623a97ddc2cd228
iarspider <iarspider@gmail.com>
2020-02-18 16:42:08 +0100

    Update package.py

--- var/spack/repos/builtin/packages/py-tables/package.py
+++ var/spack/repos/builtin/packages/py-tables/package.py
@@ -32,7 +32,7 @@ class PyTables(PythonPackage):
     depends_on('python@3.5:', when='@3.6.1:', type=('build', 'run'))
     depends_on('python@3.4:', when='@3.6.0:', type=('build', 'run'))
     depends_on('python@2.6:', type=('build', 'run'))
-    
+
     depends_on('py-setuptools', type='build')
     depends_on('py-cython@0.21:', type='build')
     depends_on('py-numpy@1.9.3:', type=('build', 'run'))
