1479eda9525a11103b7be54aca131340b92d643f
Jen Herting <jen@herting.cc>
2020-02-18 12:06:37 -0500

    [py-astropy-helpers] - removed boilerplate - added homepage - added depends_on('py-astropy')

--- var/spack/repos/builtin/packages/py-astropy-helpers/package.py
+++ var/spack/repos/builtin/packages/py-astropy-helpers/package.py
@@ -3,36 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-astropy-helpers
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-astropy-helpers
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyAstropyHelpers(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """The astropy-helpers package includes many build,
+    installation, and documentation-related tools used by the
+    Astropy project, but packaged separately for use by other
+    projects that wish to leverage this work."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/astropy/astropy-helpers"
     url      = "https://github.com/astropy/astropy-helpers/archive/v4.0.1.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('4.0.1',  sha256='88602971c3b63d6aaa6074d013f995d1e234acb3d517d70d7fcebd30cdaf5c89')
     version('4.0rc1', sha256='322aeda0ff0c8459efc6306b4d26eb115d09e96946f48f1dfe6e91f41ea6af1a')
@@ -45,13 +26,8 @@ class PyAstropyHelpers(PythonPackage):
     version('2.0.11', sha256='0cfca63fe2d9920ac1138ffd0a977572b41439e02f6f500f6f62981ef138f0ca')
     version('2.0.10', sha256='528421568531719447677df73f547c25305fbd116c6692faa0f268eac4a3e6d6')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
+    depends_on('py-astropy')
 
     def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
         args = []
         return args
