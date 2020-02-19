00475f0888ed4267b516b7bf1f8d68cce9e8bcd9
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2020-02-19 18:30:25 +0100

    Fixed PEP8

--- var/spack/repos/builtin/packages/py-resultsfile/package.py
+++ var/spack/repos/builtin/packages/py-resultsfile/package.py
@@ -16,9 +16,10 @@ class PyResultsfile(PythonPackage):
 
     version('1.0', sha256='e029054b2727131da9684fa2ec9fb8b6a3225dc7f648216a9390267b2d5d60c3')
 
-    depends_on('python@2.7:2.8.999', type=('build','run'))
+    depends_on('python@2.7:2.8.999', type=('build', 'run'))
 
-    # It depends on py-setuptools only at build time, but I get the following error:
+    # It depends on py-setuptools only at build time, but I get the following
+    # error:
     #   error: option --single-version-externally-managed not recognized
     # So I fixed it by setting the type to 'run' here, which is a bit dirty.
     depends_on('py-setuptools', type='run')
