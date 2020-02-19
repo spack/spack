e9862671c97c37c217d7e68d106c10fb75b0e797
iarspider <iarspider@gmail.com>
2020-02-19 19:56:57 +0100

    Add extra version of py-pyzmq (#15080)

--- var/spack/repos/builtin/packages/py-pyzmq/package.py
+++ var/spack/repos/builtin/packages/py-pyzmq/package.py
@@ -12,6 +12,7 @@ class PyPyzmq(PythonPackage):
     url      = "https://github.com/zeromq/pyzmq/archive/v14.7.0.tar.gz"
 
     version('18.1.0', sha256='32f7618b8104021bc96cbd60be4330bdf37b929e8061dbce362c9f3478a08e21')
+    version('18.0.1', sha256='7b0107992d8cc4c43d9af1c2e13d573ea761c7feb23d7e0e7da9dc963811e68f')
     version('17.1.2', sha256='77a32350440e321466b1748e6063b34a8a73768b62cb674e7d799fbc654b7c45')
     version('16.0.2', sha256='717dd902c3cf432b1c68e7b299ad028b0de0d0a823858e440b81d5f1baa2b1c1')
     version('14.7.0', sha256='809a5fcc720d286c840f7f64696e60322b5b2544795a73db626f09b344d16a15')
