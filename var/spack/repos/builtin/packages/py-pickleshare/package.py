40550036d7bb472afc6dc2613addfe2a9d8da5f5
Ivan Razumov <ivan.razumov@cern.ch>
2020-02-19 13:00:59 +0100

    Add extra version of py-pickleshare

--- var/spack/repos/builtin/packages/py-pickleshare/package.py
+++ var/spack/repos/builtin/packages/py-pickleshare/package.py
@@ -12,6 +12,7 @@ class PyPickleshare(PythonPackage):
     homepage = "https://pypi.python.org/pypi/pickleshare"
     url      = "https://pypi.io/packages/source/p/pickleshare/pickleshare-0.7.4.tar.gz"
 
+    version('0.7.5', sha256='87683d47965c1da65cdacaf31c8441d12b8044cdec9aca500cd78fc2c683afca')
     version('0.7.4', sha256='84a9257227dfdd6fe1b4be1319096c20eb85ff1e82c7932f36efccfe1b09737b')
 
     depends_on('py-setuptools', type='build')
