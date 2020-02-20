2552b8efaed10dd869976826e96be4c25c15f373
Matt Larsen <larsen30@llnl.gov>
2020-02-20 11:03:32 -0800

    update vtk-m package

--- var/spack/repos/builtin/packages/vtk-m/package.py
+++ var/spack/repos/builtin/packages/vtk-m/package.py
@@ -22,6 +22,7 @@ class VtkM(CMakePackage, CudaPackage):
     git      = "https://gitlab.kitware.com/vtk/vtk-m.git"
 
     version('master', branch='master')
+    version('1.5.1', commit="124fb23c50c14b171ae91b27abca77c435968fa5")
     version('1.5.0', sha256="b1b13715c7fcc8d17f5c7166ff5b3e9025f6865dc33eb9b06a63471c21349aa8")
     version('1.4.0', sha256="8d83cca7cd5e204d10da151ce4f1846c1f7414c7c1e579173d15c5ea0631555a")
     version('1.3.0', sha256="f88c1b0a1980f695240eeed9bcccfa420cc089e631dc2917c9728a2eb906df2e")
