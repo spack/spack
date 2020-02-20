8f9d5d7ca20fb73c9e02fce511da307ed39b3d2f
iarspider <iarspider@gmail.com>
2020-02-20 09:57:46 +0100

    Update package.py

--- var/spack/repos/builtin/packages/py-prometheus-client/package.py
+++ var/spack/repos/builtin/packages/py-prometheus-client/package.py
@@ -16,7 +16,7 @@ class PyPrometheusClient(PythonPackage):
     version('0.7.0', sha256='ee0c90350595e4a9f36591f291e6f9933246ea67d7cd7d1d6139a9781b14eaae')
     version('0.5.0', sha256='e8c11ff5ca53de6c3d91e1510500611cafd1d247a937ec6c588a0a7cc3bef93c')
 
-    variant('twisted', default=True, description='Expose metrics as a twisted resource')
+    variant('twisted', default=False, description='Expose metrics as a twisted resource')
 
     depends_on('py-setuptools', type='build')
     depends_on('py-twisted', type=('build', 'run'), when='+twisted')
