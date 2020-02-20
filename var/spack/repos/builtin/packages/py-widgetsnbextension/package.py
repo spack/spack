5c0275f7d3a1ab10fedd6d4542a5496a5181b05e
Ivan Razumov <ivan.razumov@cern.ch>
2020-02-20 09:38:54 +0100

    Update dependency version

--- var/spack/repos/builtin/packages/py-widgetsnbextension/package.py
+++ var/spack/repos/builtin/packages/py-widgetsnbextension/package.py
@@ -18,3 +18,4 @@ class PyWidgetsnbextension(PythonPackage):
     depends_on('py-setuptools', type='build')
     depends_on('python@2.7:2.8,3.3:')
     depends_on('py-notebook@4.2.0:', type=('build', 'run'))
+    depends_on('py-notebook@4.4.1:', type=('build', 'run'), when='@3.3.0:')
