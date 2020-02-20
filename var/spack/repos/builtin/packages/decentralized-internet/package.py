eaab4df4a6aac913b9e789a42c95f5453d6af55d
Andrew Kamal <kamalandrew55@gmail.com>
2020-02-20 15:37:19 -0500

    Update package.py

--- var/spack/repos/builtin/packages/decentralized-internet/package.py
+++ var/spack/repos/builtin/packages/decentralized-internet/package.py
@@ -10,9 +10,9 @@ from spack import *
 class DecentralizedInternet(Package):
     """A library for building decentralized and grid computing projects"""
     homepage = "https://lonero.readthedocs.io"
-    url      = "https://github.com/Lonero-Team/Decentralized-Internet/releases/download/4.2.3/Decentralized.Internet.tar.bz2"
+    url      = "    url      = "https://github.com/Lonero-Team/Decentralized-Internet/releases/download/4.2.3/Decentralized.Internet.tar.gz"2"
     maintainers = ['Lonero-Team', 'Mentors4edu']
-    version('4.2.3', sha256='fb78c56fbebed12867e97c81e1ba61d5d5f85da4eb9925a8e740af89ff104ff3')
+    version('4.2.3', sha256='2922b9128b411ece2f04d07942a453f1e772548aa27b3936c9f9bcfbc0737058')
 
     def install(self, spec, prefix):
         make()
