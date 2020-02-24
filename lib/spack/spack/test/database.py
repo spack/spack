spack/spack

commit 6b619daef36b2d780bb6b3e32ec3d89a4f011faf

specs: better lookup by hash; allow references to missing dependency hashes
- previously spec parsing didn't allow you to look up missing (but still
  known) specs by hash

- This allows you to reference and potentially reinstall
  force-uninstalled dependencies

- add testing for force uninstall and for reference by spec

- cmd/install tests now use mutable_database

lib/spack/spack/database.py
@@ -937,6 +937,73 @@ class Database(object):
                 continue
             # TODO: conditional way to do this instead of catching exceptions
 
+    def get_by_hash_local(self, dag_hash, default=None, installed=any):
+        """Look up a spec in *this DB* by DAG hash, or by a DAG hash prefix.
+
+        Arguments:
+            dag_hash (str): hash (or hash prefix) to look up
+            default (object, optional): default value to return if dag_hash is
+                not in the DB (default: None)
+            installed (bool or any, optional): if ``True``, includes only
+                installed specs in the search; if ``False`` only missing specs,
+                and if ``any``, either installed or missing (default: any)
+
+        ``installed`` defaults to ``any`` so that we can refer to any
+        known hash.  Note that ``query()`` and ``query_one()`` differ in
+        that they only return installed specs by default.
+
+        Returns:
+            (list): a list of specs matching the hash or hash prefix
+
+        """
+        with self.read_transaction():
+            # hash is a full hash and is in the data somewhere
+            if dag_hash in self._data:
+                rec = self._data[dag_hash]
+                if installed is any or rec.installed == installed:
+                    return [rec.spec]
+                else:
+                    return default
+
+            # check if hash is a prefix of some installed (or previously
+            # installed) spec.
+            matches = [record.spec for h, record in self._data.items()
+                       if h.startswith(dag_hash) and
+                       (installed is any or installed == record.installed)]
+            if matches:
+                return matches
+
+            # nothing found
+            return default
+
+    def get_by_hash(self, dag_hash, default=None, installed=any):
+        """Look up a spec by DAG hash, or by a DAG hash prefix.
+
+        Arguments:
+            dag_hash (str): hash (or hash prefix) to look up
+            default (object, optional): default value to return if dag_hash is
+                not in the DB (default: None)
+            installed (bool or any, optional): if ``True``, includes only
+                installed specs in the search; if ``False`` only missing specs,
+                and if ``any``, either installed or missing (default: any)
+
+        ``installed`` defaults to ``any`` so that we can refer to any
+        known hash.  Note that ``query()`` and ``query_one()`` differ in
+        that they only return installed specs by default.
+
+        Returns:
+            (list): a list of specs matching the hash or hash prefix
+
+        """
+        search_path = [self] + self.upstream_dbs
+        for db in search_path:
+            spec = db.get_by_hash_local(
+                dag_hash, default=default, installed=installed)
+            if spec is not None:
+                return spec
+
+        return default
+
     def _query(
             self,
             query_spec=any,

lib/spack/spack/spec.py
@@ -3951,17 +3951,15 @@ class SpecParser(spack.parse.Parser):
     def spec_by_hash(self):
         self.expect(ID)
 
-        specs = spack.store.db.query()
-        matches = [spec for spec in specs if
-                   spec.dag_hash()[:len(self.token.value)] == self.token.value]
-
+        dag_hash = self.token.value
+        matches = spack.store.db.get_by_hash(dag_hash)
         if not matches:
-            raise NoSuchHashError(self.token.value)
+            raise NoSuchHashError(dag_hash)
 
         if len(matches) != 1:
             raise AmbiguousHashError(
                 "Multiple packages specify hash beginning '%s'."
-                % self.token.value, *matches)
+                % dag_hash, *matches)
 
         return matches[0]
 

lib/spack/spack/test/cmd/uninstall.py
@@ -8,6 +8,7 @@ import spack.store
 from spack.main import SpackCommand, SpackCommandError
 
 uninstall = SpackCommand('uninstall')
+install = SpackCommand('install')
 
 
 class MockArgs(object):
@@ -21,24 +22,21 @@ class MockArgs(object):
 
 
 @pytest.mark.db
-@pytest.mark.usefixtures('database')
-def test_multiple_matches():
+def test_multiple_matches(mutable_database):
     """Test unable to uninstall when multiple matches."""
     with pytest.raises(SpackCommandError):
         uninstall('-y', 'mpileaks')
 
 
 @pytest.mark.db
-@pytest.mark.usefixtures('database')
-def test_installed_dependents():
+def test_installed_dependents(mutable_database):
     """Test can't uninstall when ther are installed dependents."""
     with pytest.raises(SpackCommandError):
         uninstall('-y', 'libelf')
 
 
 @pytest.mark.db
-@pytest.mark.usefixtures('mutable_database')
-def test_recursive_uninstall():
+def test_recursive_uninstall(mutable_database):
     """Test recursive uninstall."""
     uninstall('-y', '-a', '--dependents', 'callpath')
 
@@ -56,12 +54,11 @@ def test_recursive_uninstall():
 
 @pytest.mark.db
 @pytest.mark.regression('3690')
-@pytest.mark.usefixtures('mutable_database')
 @pytest.mark.parametrize('constraint,expected_number_of_specs', [
     ('dyninst', 7), ('libelf', 5)
 ])
 def test_uninstall_spec_with_multiple_roots(
-        constraint, expected_number_of_specs
+        constraint, expected_number_of_specs, mutable_database
 ):
     uninstall('-y', '-a', '--dependents', constraint)
 
@@ -70,14 +67,91 @@ def test_uninstall_spec_with_multiple_roots(
 
 
 @pytest.mark.db
-@pytest.mark.usefixtures('mutable_database')
 @pytest.mark.parametrize('constraint,expected_number_of_specs', [
     ('dyninst', 13), ('libelf', 13)
 ])
 def test_force_uninstall_spec_with_ref_count_not_zero(
-        constraint, expected_number_of_specs
+        constraint, expected_number_of_specs, mutable_database
 ):
     uninstall('-f', '-y', constraint)
 
     all_specs = spack.store.layout.all_specs()
     assert len(all_specs) == expected_number_of_specs
+
+
+@pytest.mark.db
+def test_force_uninstall_and_reinstall_by_hash(mutable_database):
+    """Test forced uninstall and reinstall of old specs."""
+    # this is the spec to be removed
+    callpath_spec = spack.store.db.query_one('callpath ^mpich')
+    dag_hash = callpath_spec.dag_hash()
+
+    # ensure can look up by hash and that it's a dependent of mpileaks
+    def validate_callpath_spec(installed):
+        assert installed is True or installed is False
+
+        specs = spack.store.db.get_by_hash(dag_hash, installed=installed)
+        assert len(specs) == 1 and specs[0] == callpath_spec
+
+        specs = spack.store.db.get_by_hash(dag_hash[:7], installed=installed)
+        assert len(specs) == 1 and specs[0] == callpath_spec
+
+        specs = spack.store.db.get_by_hash(dag_hash, installed=any)
+        assert len(specs) == 1 and specs[0] == callpath_spec
+
+        specs = spack.store.db.get_by_hash(dag_hash[:7], installed=any)
+        assert len(specs) == 1 and specs[0] == callpath_spec
+
+        specs = spack.store.db.get_by_hash(dag_hash, installed=not installed)
+        assert specs is None
+
+        specs = spack.store.db.get_by_hash(dag_hash[:7],
+                                           installed=not installed)
+        assert specs is None
+
+        mpileaks_spec = spack.store.db.query_one('mpileaks ^mpich')
+        assert callpath_spec in mpileaks_spec
+
+        spec = spack.store.db.query_one('callpath ^mpich', installed=installed)
+        assert spec == callpath_spec
+
+        spec = spack.store.db.query_one('callpath ^mpich', installed=any)
+        assert spec == callpath_spec
+
+        spec = spack.store.db.query_one('callpath ^mpich',
+                                        installed=not installed)
+        assert spec is None
+
+    validate_callpath_spec(True)
+
+    uninstall('-y', '-f', 'callpath ^mpich')
+
+    # ensure that you can still look up by hash and see deps, EVEN though
+    # the callpath spec is missing.
+    validate_callpath_spec(False)
+
+    # BUT, make sure that the removed callpath spec is not in queries
+    def db_specs():
+        all_specs = spack.store.layout.all_specs()
+        return (
+            all_specs,
+            [s for s in all_specs if s.satisfies('mpileaks')],
+            [s for s in all_specs if s.satisfies('callpath')],
+            [s for s in all_specs if s.satisfies('mpi')]
+        )
+    all_specs, mpileaks_specs, callpath_specs, mpi_specs = db_specs()
+    assert len(all_specs) == 13
+    assert len(mpileaks_specs) == 3
+    assert len(callpath_specs) == 2
+    assert len(mpi_specs) == 3
+
+    # Now, REINSTALL the spec and make sure everything still holds
+    install('--fake', '/%s' % dag_hash[:7])
+
+    validate_callpath_spec(True)
+
+    all_specs, mpileaks_specs, callpath_specs, mpi_specs = db_specs()
+    assert len(all_specs) == 14      # back to 14
+    assert len(mpileaks_specs) == 3
+    assert len(callpath_specs) == 3  # back to 3
+    assert len(mpi_specs) == 3

commit e8a28ab7dc6a762d1d6a90940ae2eb92aeff46f8

core: fix database and module issues (#712)
* modules: delete old code, handled in other functions

* Fix: hash-based references to upstream specs (#14629)

Spack commands referring to upstream-installed specs by hash have
been broken since 6b619da (merged September 2019), which added a new
Database function specifically for parsing hashes from command-line
specs; this function was inappropriately attempting to acquire locks
on upstream databases.

This PR updates the offending function to avoid locking upstream
databases and also updates associated tests to catch regression
errors: the upstream database created for these tests was not
explicitly set as an upstream (i.e. initialized with upstream=True)
so it was not guarding against inappropriate accesses.

Co-authored-by: Andrew W Elble <aweits@rit.edu>

lib/spack/spack/cmd/modules/__init__.py
@@ -203,31 +203,13 @@ def loads(module_type, specs, args, out=sys.stdout):
 
 
 def find(module_type, specs, args):
-    """Returns the module file "use" name if there's a single match. Raises
-    error messages otherwise.
-    """
-    if args.latest:
-        def install_date(s):
-            _, record = spack.store.db.query_by_spec_hash(s.dag_hash())
-            return record.installation_time
-        specs = sorted(specs, key=install_date, reverse=True)[:1]
+    """Retrieve paths or use names of module files"""
 
-    spec = one_spec_or_raise(specs)
-
-    if spec.package.installed_upstream:
-        module = spack.modules.common.upstream_module(spec, module_type)
-        if module:
-            print(module.path)
-        return
-
-    # Check if the module file is present
-    def module_exists(spec):
-        writer = spack.modules.module_types[module_type](spec)
-        return os.path.isfile(writer.layout.filename)
+    single_spec = one_spec_or_raise(specs)
 
     if args.recurse_dependencies:
         dependency_specs_to_retrieve = list(
-            spec.traverse(root=False, order='post', cover='nodes',
+            single_spec.traverse(root=False, order='post', cover='nodes',
                                  deptype=('link', 'run')))
     else:
         dependency_specs_to_retrieve = []
@@ -240,7 +222,7 @@ def find(module_type, specs, args):
 
         modules.append(
             spack.modules.common.get_module(
-                module_type, spec, args.full_path, required=True))
+                module_type, single_spec, args.full_path, required=True))
     except spack.modules.common.ModuleNotFoundError as e:
         tty.die(e.message)
 

lib/spack/spack/database.py
@@ -1119,7 +1119,27 @@ class Database(object):
                 continue
             # TODO: conditional way to do this instead of catching exceptions
 
-    def get_by_hash_local(self, dag_hash, default=None, installed=any):
+    def _get_by_hash_local(self, dag_hash, default=None, installed=any):
+        # hash is a full hash and is in the data somewhere
+        if dag_hash in self._data:
+            rec = self._data[dag_hash]
+            if rec.install_type_matches(installed):
+                return [rec.spec]
+            else:
+                return default
+
+        # check if hash is a prefix of some installed (or previously
+        # installed) spec.
+        matches = [record.spec for h, record in self._data.items()
+                   if h.startswith(dag_hash) and
+                   record.install_type_matches(installed)]
+        if matches:
+            return matches
+
+        # nothing found
+        return default
+
+    def get_by_hash_local(self, *args, **kwargs):
         """Look up a spec in *this DB* by DAG hash, or by a DAG hash prefix.
 
         Arguments:
@@ -1143,24 +1163,7 @@ class Database(object):
 
         """
         with self.read_transaction():
-            # hash is a full hash and is in the data somewhere
-            if dag_hash in self._data:
-                rec = self._data[dag_hash]
-                if rec.install_type_matches(installed):
-                    return [rec.spec]
-                else:
-                    return default
-
-            # check if hash is a prefix of some installed (or previously
-            # installed) spec.
-            matches = [record.spec for h, record in self._data.items()
-                       if h.startswith(dag_hash) and
-                       record.install_type_matches(installed)]
-            if matches:
-                return matches
-
-            # nothing found
-            return default
+            return self._get_by_hash_local(*args, **kwargs)
 
     def get_by_hash(self, dag_hash, default=None, installed=any):
         """Look up a spec by DAG hash, or by a DAG hash prefix.
@@ -1185,9 +1188,14 @@ class Database(object):
             (list): a list of specs matching the hash or hash prefix
 
         """
-        search_path = [self] + self.upstream_dbs
-        for db in search_path:
-            spec = db.get_by_hash_local(
+
+        spec = self.get_by_hash_local(
+            dag_hash, default=default, installed=installed)
+        if spec is not None:
+            return spec
+
+        for upstream_db in self.upstream_dbs:
+            spec = upstream_db._get_by_hash_local(
                 dag_hash, default=default, installed=installed)
             if spec is not None:
                 return spec

lib/spack/spack/test/database.py
@@ -41,10 +41,11 @@ def test_store(tmpdir):
 @pytest.fixture()
 def upstream_and_downstream_db(tmpdir_factory, gen_mock_layout):
     mock_db_root = str(tmpdir_factory.mktemp('mock_db_root'))
-    upstream_db = spack.database.Database(mock_db_root)
+    upstream_write_db = spack.database.Database(mock_db_root)
+    upstream_db = spack.database.Database(mock_db_root, is_upstream=True)
     # Generate initial DB file to avoid reindex
-    with open(upstream_db._index_path, 'w') as db_file:
-        upstream_db._write_to_file(db_file)
+    with open(upstream_write_db._index_path, 'w') as db_file:
+        upstream_write_db._write_to_file(db_file)
     upstream_layout = gen_mock_layout('/a/')
 
     downstream_db_root = str(
@@ -55,13 +56,14 @@ def upstream_and_downstream_db(tmpdir_factory, gen_mock_layout):
         downstream_db._write_to_file(db_file)
     downstream_layout = gen_mock_layout('/b/')
 
-    yield upstream_db, upstream_layout, downstream_db, downstream_layout
+    yield upstream_write_db, upstream_db, upstream_layout,\
+        downstream_db, downstream_layout
 
 
 @pytest.mark.usefixtures('config')
 def test_installed_upstream(upstream_and_downstream_db):
-    upstream_db, upstream_layout, downstream_db, downstream_layout = (
-        upstream_and_downstream_db)
+    upstream_write_db, upstream_db, upstream_layout,\
+        downstream_db, downstream_layout = (upstream_and_downstream_db)
 
     default = ('build', 'link')
     x = MockPackage('x', [], [])
@@ -75,7 +77,14 @@ def test_installed_upstream(upstream_and_downstream_db):
         spec.concretize()
 
         for dep in spec.traverse(root=False):
-            upstream_db.add(dep, upstream_layout)
+            upstream_write_db.add(dep, upstream_layout)
+        upstream_db._read()
+
+        for dep in spec.traverse(root=False):
+            record = downstream_db.get_by_hash(dep.dag_hash())
+            assert record is not None
+            with pytest.raises(spack.database.ForbiddenLockError):
+                record = upstream_db.get_by_hash(dep.dag_hash())
 
         new_spec = spack.spec.Spec('w')
         new_spec.concretize()
@@ -96,8 +105,8 @@ def test_installed_upstream(upstream_and_downstream_db):
 
 @pytest.mark.usefixtures('config')
 def test_removed_upstream_dep(upstream_and_downstream_db):
-    upstream_db, upstream_layout, downstream_db, downstream_layout = (
-        upstream_and_downstream_db)
+    upstream_write_db, upstream_db, upstream_layout,\
+        downstream_db, downstream_layout = (upstream_and_downstream_db)
 
     default = ('build', 'link')
     z = MockPackage('z', [], [])
@@ -108,13 +117,15 @@ def test_removed_upstream_dep(upstream_and_downstream_db):
         spec = spack.spec.Spec('y')
         spec.concretize()
 
-        upstream_db.add(spec['z'], upstream_layout)
+        upstream_write_db.add(spec['z'], upstream_layout)
+        upstream_db._read()
 
         new_spec = spack.spec.Spec('y')
         new_spec.concretize()
         downstream_db.add(new_spec, downstream_layout)
 
-        upstream_db.remove(new_spec['z'])
+        upstream_write_db.remove(new_spec['z'])
+        upstream_db._read()
 
         new_downstream = spack.database.Database(
             downstream_db.root, upstream_dbs=[upstream_db])
@@ -129,8 +140,8 @@ def test_add_to_upstream_after_downstream(upstream_and_downstream_db):
     DB. When a package is recorded as installed in both, the results should
     refer to the downstream DB.
     """
-    upstream_db, upstream_layout, downstream_db, downstream_layout = (
-        upstream_and_downstream_db)
+    upstream_write_db, upstream_db, upstream_layout,\
+        downstream_db, downstream_layout = (upstream_and_downstream_db)
 
     x = MockPackage('x', [], [])
     mock_repo = MockPackageMultiRepo([x])
@@ -141,7 +152,8 @@ def test_add_to_upstream_after_downstream(upstream_and_downstream_db):
 
         downstream_db.add(spec, downstream_layout)
 
-        upstream_db.add(spec, upstream_layout)
+        upstream_write_db.add(spec, upstream_layout)
+        upstream_db._read()
 
         upstream, record = downstream_db.query_by_spec_hash(spec.dag_hash())
         # Even though the package is recorded as installed in the upstream DB,