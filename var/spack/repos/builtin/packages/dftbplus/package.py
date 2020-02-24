2fbe323fce182abcb72e6b86de0c201f82bbcb09
Fabricio Cannini <fcannini@gmail.com>
2020-02-24 18:04:13 -0300

    dtfbplus: New package.

---
+++ var/spack/repos/builtin/packages/dftbplus/package.py
@@ -0,0 +1,140 @@
+from spack import *
+
+import os
+
+
+class Dftbplus(MakefilePackage):
+    """DFTB+ is an implementation of the
+    Density Functional based Tight Binding (DFTB) method,
+    containing many extensions to the original method."""
+
+    homepage = "https://www.dftbplus.org"
+    url      = "https://www.dftbplus.org/fileadmin/DFTBPLUS/public/dftbplus/19.1/dftbplus-19.1.tar.xz"
+
+    version('19.1', sha256='78f45ef0571c78cf732a5493d32830455a832fa05ebcad43098895e46ad8d220')
+
+    variant('mpi', default=True,
+            description="Build an MPI-paralelised version of the code.")
+
+    variant('gpu', default=False,
+            description="""Use the MAGMA library
+            for GPU accelerated computation""")
+
+    variant('elsi', default=False,
+            description="""Use the ELSI library for large scale systems.
+            Only has any effect if you build with '+mpi'""")
+
+    variant('sockets', default=False,
+            description="""Whether the socket library
+            (external control) should be linked""")
+
+    variant('arpack', default=False,
+            description="Use ARPACK for excited state DFTB functionality")
+
+    variant('transport', default=False,
+            description="""Whether transport via libNEGF should be included.
+            Only affects parallel build.
+            (serial version is built without libNEGF/transport)""")
+
+    variant('dftd3', default=False,
+            description="""Use DftD3 dispersion library
+            (if you need this dispersion model)""")
+
+    depends_on('lapack')
+    depends_on('blas')
+    depends_on('scalapack', when="+mpi")
+    depends_on('mpi', when="+mpi")
+    depends_on('elsi', when="+elsi")
+    depends_on('magma', when="+gpu")
+    depends_on('arpack-ng', when="+arpack")
+    depends_on('dftd3-lib@0.9.2', when="+dftd3")
+
+    def edit(self, spec, prefix):
+        """
+        First, change the ROOT variable, because, for some reason,
+        the Makefile and the spack install script run in different directories
+
+        Then, if using GCC, rename the file 'sys/make.x86_64-linux-gnu'
+        to make.arch.
+
+        After that, edit the make.arch to point to the dependencies
+
+        And the last thing we do here is to set the installdir
+        """
+        dircwd = os.getcwd()
+        makefile = FileFilter("makefile")
+        makefile.filter("ROOT := .*", "ROOT := {}".format(dircwd))
+
+        archmake = join_path(".", "sys", "make.x86_64-linux-gnu")
+        copy(archmake, join_path(dircwd, "make.arch"))
+
+        march = FileFilter(join_path(dircwd, 'make.arch'))
+
+        mconfig = FileFilter(join_path(dircwd, 'make.config'))
+
+        mconfig.filter('INSTALLDIR := .*', 'INSTALLDIR := {}'.format(prefix))
+
+        if '+gpu' in self.spec:
+            march.filter('MAGMADIR = .*',
+                         'MAGMADIR = {}'.format(spec['magma'].prefix))
+
+            mconfig.filter('WITH_GPU := .*', 'WITH_GPU := 1')
+
+        if '+mpi' in self.spec:
+            march.filter('SCALAPACKDIR = .*',
+                         'SCALAPACKDIR = {}'.format(spec['scalapack'].prefix))
+
+            march.filter('LIB_LAPACK = -l.*',
+                         'LIB_LAPACK = {}'.format(spec['blas'].libs.ld_flags))
+
+            march.filter('mpifort', '{}'.format(spec['mpi'].mpifc))
+
+            mconfig.filter('WITH_MPI := .*', 'WITH_MPI := 1')
+
+            if '+elsi' in self.spec:
+                mconfig.filter('WITH_ELSI := .*', 'WITH_ELSI := 1')
+
+                has_pexsi = 'enable_pexsi' in spec['elsi']
+
+                mconfig.filter('WITH_PEXSI := .*', 'WITH_PEXSI := {}'.format(
+                    '1' if has_pexsi is True else '0'
+                ))
+
+                march.filter("ELSIINCDIR .*", "ELSIINCDIR = {}".format(
+                    spec['elsi'].prefix.include
+                ))
+
+                march.filter("ELSIDIR .*",
+                             "ELSIDIR = {}".format(spec['elsi'].prefix))
+
+        else:
+            march.filter('LIB_LAPACK += -l.*',
+                         'LIB_LAPACK += {}'.format(spec['blas'].libs.ld_flags))
+
+        if '+sockets' in self.spec:
+            mconfig.filter('WITH_SOCKETS := .*', 'WITH_SOCKETS := 1')
+
+        if '+transport' in self.spec:
+            mconfig.filter('WITH_TRANSPORT := .*', 'WITH_TRANSPORT := 1')
+
+        if '+arpack' in self.spec:
+            march.filter('ARPACK_LIBS = .*', 'ARPACK_LIBS = {}'.format(
+                spec['arpack-ng'].libs.ld_flags
+            ))
+
+            mconfig.filter('WITH_ARPACK := .*', 'WITH_ARPACK := 1')
+
+        if '+dftd3' in self.spec:
+            march.filter('COMPILE_DFTD3 = .*', 'COMPILE_DFTD3 = 0')
+            march.filter('DFTD3_INCS = .*', 'DFTD3_INCS = -I{}'.format(
+                spec['dftd3-lib'].prefix.include
+            ))
+
+            march.filter('DFTD3_LIBS = .*', 'DFTD3_LIBS = -L{} -ldftd3'.format(
+                spec['dftd3-lib'].prefix
+            ))
+
+            mconfig.filter('WITH_DFTD3 := .*', 'WITH_DFTD3 := 1')
+
+        # Download some data needed for the tests
+        Executable('./utils/get_opt_externals')
