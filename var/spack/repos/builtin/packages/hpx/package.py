6b19d4fa964f4a1102123d08baffc0d9cdda7cc0
R. Tohid <tohid.rod@gmail.com>
2020-02-25 05:15:02 +0000

    HPX: Make std::tuple adaptation an option.

--- var/spack/repos/builtin/packages/hpx/package.py
+++ var/spack/repos/builtin/packages/hpx/package.py
@@ -50,6 +50,7 @@ class Hpx(CMakePackage, CudaPackage):
 
     variant('tools', default=False, description='Build HPX tools')
     variant('examples', default=False, description='Build examples')
+    variant('std_tuple', default='ON', description="Enable `std::tuple' adaption")
 
     depends_on('hwloc')
     depends_on('python', type=('build', 'test', 'run'))
@@ -155,12 +156,16 @@ class Hpx(CMakePackage, CudaPackage):
             'ON' if '+examples' in spec else 'OFF'
         ))
 
+        # adapt std::tuple
+        args.append('-DHPX_DATASTRUCTURES_WITH_ADAPT_STD_TUPLE={0}'.format(
+            'ON' if 'std_tuple' in spec else 'OFF'
+        ))
+
         args.extend([
             '-DBOOST_ROOT={0}'.format(spec['boost'].prefix),
             '-DHWLOC_ROOT={0}'.format(spec['hwloc'].prefix),
             '-DHPX_WITH_BOOST_ALL_DYNAMIC_LINK=ON',
             '-DBUILD_SHARED_LIBS=ON',
-            '-DHPX_DATASTRUCTURES_WITH_ADAPT_STD_TUPLE=OFF'
         ])
 
         return args
