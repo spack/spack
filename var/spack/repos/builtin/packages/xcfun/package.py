97710e6f8aa40cfee593897863da7bf5ca67c515
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-02-25 13:07:28 +0100

    Fix style

--- var/spack/repos/builtin/packages/xcfun/package.py
+++ var/spack/repos/builtin/packages/xcfun/package.py
@@ -10,11 +10,13 @@ class Xcfun(CMakePackage):
     """Exchange-Correlation functionals with arbitrary order derivatives."""
 
     homepage = "https://github.com/dftlibs/xcfun"
-    url      = "https://github.com/dftlibs/xcfun/archive/v2.0.0a4.tar.gz"
+    url = "https://github.com/dftlibs/xcfun/archive/v2.0.0a4.tar.gz"
 
     maintainers = ['robertodr', 'bast']
 
-    version('2.0.0a6', sha256='a51086490890393439f98c5e3e4e1622908fe934bbc5063b1d4363cc4c15496d')
+    version('2.0.0a6',
+            sha256=
+            'a51086490890393439f98c5e3e4e1622908fe934bbc5063b1d4363cc4c15496d')
 
     extends('python')
     depends_on('cmake@3.11:', type='build')
@@ -27,7 +29,8 @@ class Xcfun(CMakePackage):
 
         args = [
             "-DCMAKE_INSTALL_LIBDIR=lib",
-            "-DPYMOD_INSTALL_LIBDIR=/python{0}/site-packages".format(spec["python"].version[:-1]),
+            "-DPYMOD_INSTALL_LIBDIR=/python{0}/site-packages".format(
+                spec["python"].version[:-1]),
             "-DXCFUN_MAX_ORDER=8",
             "-DXCFUN_PYTHON_INTERFACE=ON",
             "-DPYTHON_EXECUTABLE={0}".format(spec['python'].command),
