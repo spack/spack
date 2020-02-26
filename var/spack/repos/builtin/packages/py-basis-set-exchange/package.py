39d27203e84f1d00277b5cf7b27699adbf508482
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2020-02-26 09:08:25 +0100

    Removed py-versioneer, according to @adamjstewart

--- var/spack/repos/builtin/packages/py-basis-set-exchange/package.py
+++ var/spack/repos/builtin/packages/py-basis-set-exchange/package.py
@@ -27,8 +27,7 @@ class PyBasisSetExchange(PythonPackage):
 
     depends_on('py-argcomplete', type=('build', 'run'))
     depends_on('py-jsonschema', type=('build', 'run'))
-    depends_on('py-setuptools', type=('build','run'))
-    depends_on('python@2.7:', type=('build', 'run'))
-    depends_on('py-versioneer', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('python@3:', type=('build', 'run'))
     depends_on('py-pytest@4.0:', type='test')
     depends_on('py-pytest-cov', type='test')
