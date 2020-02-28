5329308b834ce2cf62a4e09d6f8c7a003bc388a1
Christoph Junghans <junghans@lanl.gov>
2020-02-28 13:58:31 -0700

    gromacs: add v2019.6 (#15267)
    * gromacs: add v2019.6
    
    * Update var/spack/repos/builtin/packages/gromacs/package.py
    
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/gromacs/package.py
+++ var/spack/repos/builtin/packages/gromacs/package.py
@@ -24,8 +24,9 @@ class Gromacs(CMakePackage):
     git      = 'https://github.com/gromacs/gromacs.git'
     maintainers = ['junghans', 'marvinbernhardt']
 
-    version('develop', branch='master')
+    version('master', branch='master')
     version('2020', sha256='477e56142b3dcd9cb61b8f67b24a55760b04d1655e8684f979a75a5eec40ba01')
+    version('2019.6', sha256='bebe396dc0db11a9d4cc205abc13b50d88225617642508168a2195324f06a358')
     version('2019.5', sha256='438061a4a2d45bbb5cf5c3aadd6c6df32d2d77ce8c715f1c8ffe56156994083a')
     version('2019.4', sha256='ba4366eedfc8a1dbf6bddcef190be8cd75de53691133f305a7f9c296e5ca1867')
     version('2019.3', sha256='4211a598bf3b7aca2b14ad991448947da9032566f13239b1a05a2d4824357573')
