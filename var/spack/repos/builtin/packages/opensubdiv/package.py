0c10dd40e3c9e9afba28036dc4caff0e44507a2a
Jen Herting <jen@herting.cc>
2020-02-28 12:19:13 -0500

    [opensubdiv] removed boilerplate

--- var/spack/repos/builtin/packages/opensubdiv/package.py
+++ var/spack/repos/builtin/packages/opensubdiv/package.py
@@ -3,22 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install opensubdiv
-#
-# You can edit this file again by typing:
-#
-#     spack edit opensubdiv
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
@@ -32,18 +16,11 @@ class Opensubdiv(CMakePackage):
     homepage = "http://graphics.pixar.com/opensubdiv/docs/intro.html"
     url      = "https://github.com/PixarAnimationStudios/OpenSubdiv/archive/v3_4_0.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('3_4_0',     sha256='d932b292f83371c7518960b2135c7a5b931efb43cdd8720e0b27268a698973e4')
 
     # FIXME: Add dependencies if required.
     # depends_on('foo')
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = []
         return args
