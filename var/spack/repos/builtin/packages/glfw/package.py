2ad05d1d1e55b4fd05975fbc4bcc302d94d8595f
Jen Herting <jen@herting.cc>
2020-03-02 11:08:57 -0500

    added description and homepage

--- var/spack/repos/builtin/packages/glfw/package.py
+++ var/spack/repos/builtin/packages/glfw/package.py
@@ -24,10 +24,12 @@ from spack import *
 
 
 class Glfw(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """GLFW is an Open Source, multi-platform library for
+    OpenGL, OpenGL ES and Vulkan development on the desktop. It
+    provides a simple API for creating windows, contexts and
+    surfaces, receiving input and events."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://www.glfw.org/"
     url      = "https://github.com/glfw/glfw/archive/3.3.2.tar.gz"
 
     # FIXME: Add a list of GitHub accounts to
