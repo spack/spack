b045b5e369a32eb2b41b530e0f285ad3fdf8e518
Jen Herting <jen@herting.cc>
2020-03-02 11:31:29 -0500

    removed extra boilerplae

--- var/spack/repos/builtin/packages/glfw/package.py
+++ var/spack/repos/builtin/packages/glfw/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install glfw
-#
-# You can edit this file again by typing:
-#
-#     spack edit glfw
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -31,10 +14,6 @@ class Glfw(CMakePackage):
 
     homepage = "https://www.glfw.org/"
     url      = "https://github.com/glfw/glfw/archive/3.3.2.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('3.3.2', sha256='98768e12e615fbe9f3386f5bbfeb91b5a3b45a8c4c77159cef06b1f6ff749537')
     version('3.3.1', sha256='6bca16e69361798817a4b62a5239a77253c29577fcd5d52ae8b85096e514177f')
@@ -58,10 +37,3 @@ class Glfw(CMakePackage):
     depends_on('fontconfig')
     depends_on('doxygen', type='build')
     depends_on('pkg-config', type='build')
-
-    def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
-        args = []
-        return args
