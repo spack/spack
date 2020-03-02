7fa0c1350964cd26eeca6f2b4b424a5ebf685521
Jen Herting <jen@herting.cc>
2020-03-02 15:25:02 -0500

    [blender] opensubdiv needs to be compiled with openmp

--- var/spack/repos/builtin/packages/blender/package.py
+++ var/spack/repos/builtin/packages/blender/package.py
@@ -59,7 +59,7 @@ class Blender(CMakePackage):
     depends_on('libuuid')
     depends_on('jemalloc', when='+jemalloc')
     depends_on('ilmbase')
-    depends_on('opensubdiv', when='+opensubdiv')
+    depends_on('opensubdiv+openmp', when='+opensubdiv')
 
     def cmake_args(self):
         spec = self.spec
