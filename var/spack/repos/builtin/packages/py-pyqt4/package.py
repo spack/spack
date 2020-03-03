c0ba4108e941c55d766d975bcc9ce8057ae8aba6
Sinan81 <sbulut@3vgeomatics.com>
2020-03-03 03:04:38 -0800

    set sip-incdir correctly for the new setup

--- var/spack/repos/builtin/packages/py-pyqt4/package.py
+++ var/spack/repos/builtin/packages/py-pyqt4/package.py
@@ -77,7 +77,8 @@ class PyPyqt4(SIPPackage):
                        '--apidir=' + self.prefix.share.qsci,
                        '--destdir=' + pydir,
                        '--pyqt-sipdir=' + self.prefix.share.sip.PyQt4,
-                       '--sip-incdir=' + python_include_dir,
+                       '--sip-incdir=' + join_path(self.spec['py-sip'].prefix.include,
+                                                   'python' + str(self.spec['python'].version.up_to(2))),
                        '--stubsdir=' + pydir)
 
                 # Fix build errors
