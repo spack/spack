cc8d9eee8ec7fe60235b00d52170f717c703dc0b
Adam J. Stewart <ajstewart426@gmail.com>
2020-03-04 13:16:42 -0600

    suite-sparse: fix installation for v5.X (#15326)
    fixes #15184
    
    GraphBLAS depends on m4 according to CMake error message
    Do not use INSTALL= when compiling the library

--- var/spack/repos/builtin/packages/suite-sparse/package.py
+++ var/spack/repos/builtin/packages/suite-sparse/package.py
@@ -33,6 +33,7 @@ class SuiteSparse(Package):
 
     depends_on('blas')
     depends_on('lapack')
+    depends_on('m4', type='build', when='@5.0.0:')
     depends_on('cmake', when='@5.2.0:', type='build')
 
     depends_on('metis@5.1.0', when='@4.5.1:')
@@ -63,7 +64,6 @@ class SuiteSparse(Package):
         pic_flag  = self.compiler.pic_flag if '+pic' in spec else ''
 
         make_args = [
-            'INSTALL=%s' % prefix,
             # By default, the Makefile uses the Intel compilers if
             # they are found. The AUTOCC flag disables this behavior,
             # forcing it to use Spack's compiler wrappers.
@@ -134,6 +134,7 @@ class SuiteSparse(Package):
             self.spec.version <= Version('5.6.0')):
             make('default', *make_args)
 
+        make_args.append('INSTALL=%s' % prefix)
         make('install', *make_args)
 
     @property
