6da9ee72c5c94caa12229c1a1892a1796f47ea71
Mathias Anselmann <mathias.anselmann@posteo.de>
2020-03-05 16:34:14 +0100

    fixed indentation

--- var/spack/repos/builtin/packages/cscope/package.py
+++ var/spack/repos/builtin/packages/cscope/package.py
@@ -18,7 +18,6 @@ class Cscope(AutotoolsPackage):
             url = "https://sourceforge.net/projects/cscope/files/cscope/v15.9/cscope-15.9.tar.gz"
         return url
 
-
     version('15.9', sha256='c5505ae075a871a9cd8d9801859b0ff1c09782075df281c72c23e72115d9f159')
     version('15.8b', sha256='4889d091f05aa0845384b1e4965aa31d2b20911fb2c001b2cdcffbcb7212d3af')
 
