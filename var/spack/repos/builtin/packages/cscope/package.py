ffe5d40d9a9c3cff2466b875d43dff72b92643b4
Mathias Anselmann <mathias.anselmann@posteo.de>
2020-03-05 16:25:00 +0100

    Update to v15.9

--- var/spack/repos/builtin/packages/cscope/package.py
+++ var/spack/repos/builtin/packages/cscope/package.py
@@ -10,8 +10,16 @@ class Cscope(AutotoolsPackage):
     """Cscope is a developer's tool for browsing source code."""
 
     homepage = "http://cscope.sourceforge.net/"
-    url      = "http://downloads.sourceforge.net/project/cscope/cscope/15.8b/cscope-15.8b.tar.gz"
 
+    def url_for_version(self, version):
+        if '@:15.8b' in self.spec:
+            url = "https://sourceforge.net/projects/cscope/files/cscope/v15.9/cscope-15.9.tar.gz"
+        else:
+            url = "https://sourceforge.net/projects/cscope/files/cscope/v15.9/cscope-15.9.tar.gz"
+        return url
+
+
+    version('15.9', sha256='c5505ae075a871a9cd8d9801859b0ff1c09782075df281c72c23e72115d9f159')
     version('15.8b', sha256='4889d091f05aa0845384b1e4965aa31d2b20911fb2c001b2cdcffbcb7212d3af')
 
     depends_on('ncurses')
