8f0bafd71fa0122c412b143c08ff08f27d4adfb2
plamborn <plamborn@lanl.gov>
2020-03-05 14:58:32 -0700

    Update package.py

--- var/spack/repos/builtin/packages/libhio/package.py
+++ var/spack/repos/builtin/packages/libhio/package.py
@@ -16,6 +16,7 @@ class Libhio(AutotoolsPackage):
 
     homepage = "https://github.com/hpc/libhio"
     url      = "https://github.com/hpc/libhio/releases/download/hio.1.4.1.0/libhio-1.4.1.0.tar.bz2"
+    git      = "https://github.com/hpc/libhio.git"
     maintainers = ['plamborn']
 
     #
