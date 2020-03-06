f93e77650f095c43f9f515b8c20f94fdb1cc4fee
Patrick Broderick <Patrick.Broderick@gmail.com>
2020-03-06 11:33:08 -0500

    contrib-spiral: update hcol package to install in <prefix>/namespaces/packages/hcol

--- var/spack/repos/builtin/packages/hcol/package.py
+++ var/spack/repos/builtin/packages/hcol/package.py
@@ -20,14 +20,8 @@ class Hcol(Package):
     version('1.0.0', sha256='9a95574e2b061d03d264c32dbf733e893017d1644b6486c7a8a55a3b24783f58')
 
     # HCOL package is an extension for Spiral.  Install the files in their own
-    # prefix.  This folder needs to be linked to spiral so that spiral can
-    # import/use the package
+    # prefix, in "namespaces/packages/hcol".  This allows 'spack activate' to
+    # symlink hcol at the right location for spiral packages.
     def install(self, spec, prefix):
         source = self.stage.source_path
-        install_tree(source, prefix)
-        link = spec['spiral'].prefix
-        with working_dir(join_path(link, 'namespaces/packages')):
-            if os.path.isfile('hcol'):
-                os.remove('hcol')
-
-            os.symlink(prefix, 'hcol')
+        install_tree(source, join_path(prefix, 'namespaces/packages/hcol'))
