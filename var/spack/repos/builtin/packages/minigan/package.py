9f3546990568c3bb75897d3c03db41cd703c16f6
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2020-03-06 13:48:16 -0700

    Spackages for horovod and minigan

--- var/spack/repos/builtin/packages/minigan/package.py
+++ var/spack/repos/builtin/packages/minigan/package.py
@@ -3,38 +3,33 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install minigan
-#
-# You can edit this file again by typing:
-#
-#     spack edit minigan
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
+import tarfile
+import fnmatch
+import os
 
-
-class Minigan(Package):
-    """miniGAN is a generative adversarial network code developed as part of the Exascale Computing Project's (ECP) ExaLearn project at Sandia National Laboratories."""
+class Minigan(PythonPackage):
+    """miniGAN is a generative adversarial network code developed as part of the
+    Exascale Computing Project's (ECP) ExaLearn project at
+    Sandia National Laboratories."""
 
     homepage = "https://github.com/SandiaMLMiniApps/miniGAN"
     url      = "https://github.com/SandiaMLMiniApps/miniGAN/archive/1.0.0.tar.gz"
 
     version('1.0.0', sha256='ef6d5def9c7040af520acc64b7a8b6c8ec4b7901721b11b0cb25a583ea0c8ae3')
 
-    depends_on('python@3.5.2')
-    depends_on('py-torch@1.3.1')
+    depends_on('python', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-torch')
     depends_on('py-numpy')
-    depends_on('py-horovod@0.18.0')
+    depends_on('py-horovod@master')
     depends_on('py-torchvision')
-    depends_on('matplotlib@3.0.0')
+    depends_on('py-matplotlib@3.0.0')
 
+    phases = ['install']
+
+    def install(self, spec, prefix):
+        for file in os.listdir(prefix):
+            if fnmatch.fnmatch(file, '*.tar.gz'):
+                tf = tarfile.extract(filename)
+                tf.extractall()
--- var/spack/repos/builtin/packages/py-horovod/package.py
+++ var/spack/repos/builtin/packages/py-horovod/package.py
@@ -3,34 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-horovod
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-horovod
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
-
+import fnmatch
+import os
 
 class PyHorovod(PythonPackage):
-    """Horovod is a distributed deep learning training framework for TensorFlow, Keras, PyTorch, and Apache MXNet."""
+    """Horovod is a distributed deep learning training framework for
+    TensorFlow, Keras, PyTorch, and Apache MXNet."""
 
     homepage = "https://www.example.com"
-    url      = "https://github.com/horovod/horovod/archive/v0.18.0.tar.gz"
     git      = "https://github.com/horovod/horovod.git"
+    url      = "https://github.com/horovod/horovod/archive/v0.19.0.tar.gz"
 
-    version('master',       branch='master', submodules='True')
+    version('master', branch='master', submodules='True')
     version('0.19.0',       sha256='0e9fec11cd7f5f39a09f0785d1097cb51c44537ae14c9b4b2578b5cdd21efb9b')
     version('0.18.2',       sha256='a073e08cec65474afdb2d011486b4cb6c7ac8fcb1eca3e02b169e1e7b4a66da6')
     version('0.18.1',       sha256='26e236d1f60955e9dd12b9f0a836f0691296a010fcd1ac72295970a780f4e4fb')
@@ -42,7 +27,17 @@ class PyHorovod(PythonPackage):
     version('0.16.3',       sha256='1857cf1b335723366cc71e4bcd0583f2dde0c821212cda0e1b6bddfe4ba1ea0d')
     version('0.16.2',       sha256='baa9754e59ab0ee72d3b5769cf77e06a2c7b0a2d9626e0e14ca2ab131934ce74')
 
+    depends_on('python', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('openmpi', type=('build', 'run'))
     depends_on('nccl')
-    depends_on('py-torch@1.0.0', type=('build', 'run'))
+    depends_on('py-torch', type=('build', 'run'))
+    depends_on('py-pip', type=('build'))
+
+    phases = ['clean', 'sdist', 'install']    
+
+    def install(self, spec, prefix):
+        pip = which('pip')
+        for file in os.listdir(prefix):
+            if fnmatch.fnmatch(file, 'horovod-*.tar.gz'):
+                pip('install', file, '--prefix={0}'.format(prefix))
