8a57b9fb546d2e758c81288424e97784d9eb2c35
Gregory Becker <becker33@llnl.gov>
2020-03-10 13:23:04 -0700

    rename private method for clarity

--- lib/spack/llnl/util/tty/log.py
+++ lib/spack/llnl/util/tty/log.py
@@ -39,7 +39,8 @@ def background_safe():
     signal.signal(signal.SIGTTOU, signal.SIG_DFL)
 
 
-def is_background():
+def _is_background_tty():
+    """Return True iff this process is backgrounded and stdout is a tty"""
     if sys.stdout.isatty():
         return os.getpgrp() != os.tcgetpgrp(sys.stdout.fileno())
     return False  # not writing to tty, not background
@@ -97,7 +98,7 @@ class keyboard_input(object):
 
         try:
             # If this fails, self.old_cfg will remain None
-            if not is_background():
+            if not _is_background_tty():
                 import termios
 
                 # save old termios settings
@@ -462,7 +463,7 @@ class log_output(object):
                     tostop = conf[3] & termios.TOSTOP
                 except Exception:
                     tostop = True
-                if not (tostop and is_background()):
+                if not (tostop and _is_background_tty()):
                     sys.stdout.write(line)
                     sys.stdout.flush()
 
@@ -478,7 +479,7 @@ class log_output(object):
 
         try:
             while True:
-                if not is_background():
+                if not _is_background_tty():
                     with keyboard_input(stdin):
                         # No need to set any timeout for select.select
                         # Wait until a key press or an event on in_pipe.
@@ -487,7 +488,7 @@ class log_output(object):
                         # Currently ignores other chars.
                         if stdin in rlist:
                             # Double check we're still in the foreground
-                            if not is_background():
+                            if not _is_background_tty():
                                 if stdin.read(1) == 'v':
                                     echo = not echo
 
