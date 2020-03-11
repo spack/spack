45f213f274175208248ef89a79d46ff721be69ea
Brian C. Van Essen <vanessen1@llnl.gov>
2020-03-10 17:53:16 -0700

    Updated recipe to provide cuda architecture constaints.

--- var/spack/repos/builtin/packages/dihydrogen/package.py
+++ var/spack/repos/builtin/packages/dihydrogen/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class Dihydrogen(CMakePackage):
+class Dihydrogen(CMakePackage, CudaPackage):
     """DiHydrogen is the second version of the Hydrogen fork of the
        well-known distributed linear algebra library,
        Elemental. DiHydrogen aims to be a basic distributed
@@ -23,8 +23,6 @@ class Dihydrogen(CMakePackage):
 
     variant('al', default=True,
             description='Builds with Aluminum communication library')
-    variant('cuda', default=False,
-            description='Builds with support for GPUs via CUDA')
     variant('developer', default=False,
             description='Enable extra warnings and force tests to be enabled.')
     variant('half', default=False,
@@ -42,21 +40,43 @@ class Dihydrogen(CMakePackage):
     variant('test', default=False,
             description='Builds test suite')
 
+    # Override the default set of CUDA architectures with the relevant
+    # subset from lib/spack/spack/build_systems/cuda.py
+    cuda_arch_values = [
+        '60', '61', '62',
+        '70', '72', '75',
+    ]
+    variant('cuda_arch',
+            description='CUDA architecture',
+            values=spack.variant.auto_or_any_combination_of(*cuda_arch_values))
+
     depends_on('mpi')
     depends_on('catch2', type='test')
 
     depends_on('aluminum', when='+al ~cuda')
     depends_on('aluminum +gpu +nccl +mpi_cuda', when='+al +cuda')
 
-    depends_on('cuda', when='+cuda')
-    depends_on('cudnn', when='+cuda')
-    depends_on('cub', when='+cuda')
+    depends_on('cuda', when=('+cuda' or '+legacy'))
+    depends_on('cudnn', when=('+cuda' or '+legacy'))
+    depends_on('cub', when=('+cuda' or '+legacy'))
+
+    # Legacy builds require cuda
+    conflicts('~cuda', when='+legacy')
 
     depends_on('half', when='+half')
 
     generator = 'Ninja'
     depends_on('ninja', type='build')
     depends_on('cmake@3.14.0:', type='build')
+
+    illegal_cuda_arch_values = [
+        '10', '11', '12', '13',
+        '20', '21',
+        '30', '32', '35', '37',
+        '50', '52', '53',
+    ]
+    for value in illegal_cuda_arch_values:
+        conflicts('cuda_arch=' + value)
 
     @property
     def libs(self):
@@ -70,7 +90,6 @@ class Dihydrogen(CMakePackage):
 
         args = [
             '-DCMAKE_INSTALL_MESSAGE:STRING=LAZY',
-            '-DCMAKE_CXX_COMPILER=%s' % spec['mpi'].mpicxx,
             '-DBUILD_SHARED_LIBS:BOOL=%s'      % ('+shared' in spec),
             '-DH2_ENABLE_CUDA=%s' % ('+cuda' in spec),
             '-DH2_ENABLE_DISTCONV_LEGACY=%s' % ('+legacy' in spec),
@@ -78,7 +97,17 @@ class Dihydrogen(CMakePackage):
             '-DH2_ENABLE_FP16=%s' % ('+half' in spec),
             '-DH2_ENABLE_HIP_ROCM=%s' % ('+rocm' in spec),
             '-DH2_DEVELOPER_BUILD=%s' % ('+developer' in spec),
-            '-DCMAKE_CUDA_FLAGS=-arch=sm_60',
         ]
+
+        if '+cuda' in spec:
+            cuda_arch = spec.variants['cuda_arch'].value
+            if len(cuda_arch) == 1 and cuda_arch[0] == 'auto':
+                args.append('-DCMAKE_CUDA_FLAGS=-arch=sm_60')
+            else:
+                cuda_arch = [x for x in spec.variants['cuda_arch'].value if x != 'auto']
+                if cuda_arch:
+                    args.append('-DCMAKE_CUDA_FLAGS={0}'.format(
+                        ' '.join(self.cuda_flags(cuda_arch))
+                    ))
 
         return args
