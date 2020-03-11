bb1928f953810ef3be7fa1db1ccd78b292307bdc
Amjad Kotobi <amjadkotbi@gmail.com>
2020-03-11 14:50:19 +0100

    ebasing

--- var/spack/repos/builtin/packages/jblob/package.py
+++ var/spack/repos/builtin/packages/jblob/package.py
@@ -4,34 +4,32 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-import fileinput, os, shutil
+
 
 class Jblob(Package):
-    """The German Climate Computing Center (DKRZ: Deutsches Klimarechenzentrum GmbH) 
-       dsaprovides a Long Term Archiving Service for large research data sets which are relevant for climate or Earth system research.
+    """
+       The German Climate Computing Center
+       (DKRZ: Deutsches Klimarechenzentrum GmbH)
+       provides a Long Term Archiving Service for large research
+       data sets which are relevant for climate or Earth system research.
     """
 
     homepage = "https://cera-www.dkrz.de/WDCC/ui/cerasearch"
-    url      = "http://cera-www.dkrz.de/jblob/jblob-3.0.zip"
+    url      = "https://cera-www.dkrz.de/jblob/jblob-3.0.zip"
 
     maintainers = ['ajkotobi']
 
-    version('3.0', sha256='576b5956358386a8832c6d1d13c410705e54888354a10cfd4f094513458067e4', expand=True)
+    version('3.0', sha256='576b5956358386a8832c6d1d13c410705e54888354a10cfd4f094513458067e4')
 
-    depends_on('openjdk@1.8.0_202-b08:', type=('build', 'link', 'run'))
+    depends_on('java@8:', type='run')
 
     def setup_environment(self, spack_env, run_env):
         run_env.set('JAVA_HOME', self.spec['java'].prefix)
 
     def install(self, spec, prefix):
-
-        with open('jblob') as f:
-            replaced = f.read().replace('/opt/jblob-3.0', self.prefix)
-
-        with open('jblob', "w") as f:
-            f.write(replaced)
+        filter_file('/opt/jblob-' + self.version, prefix, 'jblob')
 
         mkdir(prefix.bin)
-        copy('jblob', prefix.bin)
+        install('jblob', prefix.bin)
         install_tree('lib', prefix.lib)
         install_tree('docs', prefix.docs)
