9b2573a5dc7d00d19aa4a70f9cb8e5f771f51efc
he-b <10837193+he-b@users.noreply.github.com>
2020-03-12 16:24:14 -0500

    Allows the package to download from GNU mirrors

--- var/spack/repos/builtin/packages/source-highlight/package.py
+++ var/spack/repos/builtin/packages/source-highlight/package.py
@@ -7,13 +7,13 @@
 from spack import *
 
 
-class SourceHighlight(AutotoolsPackage):
+class SourceHighlight(AutotoolsPackage, GNUMirrorPackage):
     """This program, given a source file, produces a document with syntax
     highlighting. It also provides a C++ highlight library
     (since version 3.0). """
 
     homepage = "https://www.gnu.org/software/src-highlite/"
-    url      = "ftp://ftp.gnu.org/gnu/src-highlite/source-highlight-3.1.8.tar.gz"
+    gnu_mirror_path = "src-highlite/source-highlight-3.1.8.tar.gz"
 
     version('3.1.9', sha256='3a7fd28378cb5416f8de2c9e77196ec915145d44e30ff4e0ee8beb3fe6211c91')
     version('3.1.8', sha256='01336a7ea1d1ccc374201f7b81ffa94d0aecb33afc7d6903ebf9fbf33a55ada3')
