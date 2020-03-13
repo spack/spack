e41991329c1b279a68649b3e89246bab7cfc151e
Hans Pabst <hans.pabst@intel.com>
2020-03-13 19:17:34 +0100

    LIBXSMM: renamed development version according to the related branch on GitHub.

--- var/spack/repos/builtin/packages/libxsmm/package.py
+++ var/spack/repos/builtin/packages/libxsmm/package.py
@@ -17,7 +17,7 @@ class Libxsmm(MakefilePackage):
     url      = 'https://github.com/hfp/libxsmm/archive/1.15.tar.gz'
     git      = 'https://github.com/hfp/libxsmm.git'
 
-    version('develop', branch='master')
+    version('master', branch='master')
     version('1.15',   sha256='499e5adfbf90cd3673309243c2b56b237d54f86db2437e1ac06c8746b55ab91c')
     version('1.14',   sha256='9c0af4509ea341d1ee2c6c19fc6f19289318c3bd4b17844efeb9e7f9691abf76')
     version('1.13',   sha256='47c034e169820a9633770eece0e0fdd8d4a744e09b81da2af8c2608a4625811e')
