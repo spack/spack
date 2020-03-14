a59dec230b0d3652e510299e6a98f7a36b05e549
Neil Flood <neilflood@fastmail.fm>
2020-03-15 07:05:58 +1000

    py-tuiview: Versions up to 1.1.99 are qt4 only

--- var/spack/repos/builtin/packages/py-tuiview/package.py
+++ var/spack/repos/builtin/packages/py-tuiview/package.py
@@ -16,6 +16,6 @@ class PyTuiview(PythonPackage):
 
     version('1.1.7', sha256='fbf0bf29cc775357dad4f8a2f0c2ffa98bbf69d603a96353e75b321adef67573')
 
-    depends_on("py-pyqt4", type=('build', 'run'))
+    depends_on("py-pyqt4", type=('build', 'run'), when='@:1.1.99')
     depends_on("py-numpy", type=('build', 'run'))
     depends_on("gdal+python")
