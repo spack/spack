138ade06bf5c35bd7ede95aad2d579b794063424
Sinan81 <Sinan81@github>
2020-03-19 14:46:04 -0700

    tidy up

--- var/spack/repos/builtin/packages/arrayfire/package.py
+++ var/spack/repos/builtin/packages/arrayfire/package.py
@@ -3,44 +3,23 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install arrayfire
-#
-# You can edit this file again by typing:
-#
-#     spack edit arrayfire
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
-class Arrayfire(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+class Arrayfire(CMakePackage, CudaPackage):
+    """ArrayFire is a high performance software library for parallel computing
+    with an easy-to-use API. Its array based function set makes parallel
+    programming more accessible."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
-#    url      = "https://github.com/arrayfire/arrayfire/archive/v3.7.0.tar.gz"
+    homepage = "http://arrayfire.org/docs/index.htm"
     git      = "https://github.com/arrayfire/arrayfire.git"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('master', submodules=True)
+    version('3.7.0', submodules=True, tag='v3.7.0')
 
-    variant('cuda', default=False)
-    variant('opencl', default=False)
+    variant('cuda',   default=False, description='Enable Cuda backend')
+    variant('opencl', default=False, description='Enable OpenCL backend')
 
-    # FIXME: Add dependencies if required.
     depends_on('boost')
     depends_on('fftw')
     depends_on('openblas')
@@ -48,8 +27,11 @@ class Arrayfire(CMakePackage):
     depends_on('opencl', when='+opencl')
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = []
+        args.extend([
+            '-DAF_BUILD_CUDA={0}'.format(
+                'ON' if '+cuda' in self.spec else 'OFF'),
+            '-DAF_BUILD_OPENCL={0}'.format(
+                'ON' if '+opencl' in self.spec else 'OFF'),
+        ])
         return args
