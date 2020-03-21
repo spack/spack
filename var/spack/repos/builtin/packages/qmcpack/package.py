10ae5cf8692f33093221a1049f5744efd8fe933b
Nichols A. Romero <naromero77@users.noreply.github.com>
2020-03-20 23:52:45 -0500

    Update var/spack/repos/builtin/packages/qmcpack/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/qmcpack/package.py
+++ var/spack/repos/builtin/packages/qmcpack/package.py
@@ -384,7 +384,7 @@ class Qmcpack(CMakePackage, CudaPackage):
         Set PATH and PYTHONPATH for basic analysis scripts for Nexus."""
 
         env.prepend_path('PATH', self.prefix.nexus.bin)
-        env.prepend_path('PYTHONPATH', self.prefix.nexus + '/lib')
+        env.prepend_path('PYTHONPATH', self.prefix.nexus.lib)
 
     @run_after('build')
     @on_package_attributes(run_tests=True)
