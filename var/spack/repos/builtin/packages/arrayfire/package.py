dcf7da7f89a3a680039d8aa56240f35ba3fbfb65
Sinan81 <sbulut@3vgeomatics.com>
2020-03-22 12:21:18 -0700

    improve

--- var/spack/repos/builtin/packages/arrayfire/package.py
+++ var/spack/repos/builtin/packages/arrayfire/package.py
@@ -18,19 +18,25 @@ class Arrayfire(CMakePackage, CudaPackage):
     version('3.7.0', submodules=True, tag='v3.7.0')
 
     variant('cuda',   default=False, description='Enable Cuda backend')
+    variant('forge',   default=False, description='Enable graphics library')
     variant('opencl', default=False, description='Enable OpenCL backend')
 
-    depends_on('boost')
+    depends_on('boost@1.65:')
     depends_on('fftw')
-    depends_on('openblas')
-    depends_on('cuda', when='+cuda')
+    depends_on('blas')
+    depends_on('cuda@7.5:', when='+cuda')
     depends_on('opencl', when='+opencl')
+
+    depends_on('fontconfig', when='+forge')
+    depends_on('glfw@3.1.4:', when='+forge')
 
     def cmake_args(self):
         args = []
         args.extend([
             '-DAF_BUILD_CUDA={0}'.format(
                 'ON' if '+cuda' in self.spec else 'OFF'),
+            '-DAF_BUILD_FORGE={0}'.format(
+                'ON' if '+forge' in self.spec else 'OFF'),
             '-DAF_BUILD_OPENCL={0}'.format(
                 'ON' if '+opencl' in self.spec else 'OFF'),
         ])
