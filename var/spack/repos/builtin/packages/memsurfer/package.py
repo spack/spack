96afb4ef07209a037768e9acc020ba205ff2f409
Harsh Bhatia <bhatia4@llnl.gov>
2020-03-23 00:46:05 -0700

    flake8 compliance

--- var/spack/repos/builtin/packages/memsurfer/package.py
+++ var/spack/repos/builtin/packages/memsurfer/package.py
@@ -12,32 +12,31 @@ class Memsurfer(PythonPackage):
        wide variety of large-scale molecular simulations."""
 
     homepage = "https://github.com/LLNL/MemSurfer"
-    git      = "git@github.com:LLNL/MemSurfer.git"
+    git = "git@github.com:LLNL/MemSurfer.git"
 
-    version('1.0', tag='v1.0', submodules=True)
-    version('master', branch='master', submodules=True)
+    version('1.0',     tag='v1.0',       submodules=True)
+    version('master',  branch='master',  submodules=True)
     version('develop', branch='develop', submodules=True)
 
-    variant('osmesa', default=False, description='Enable OSMesa support (for VTK)')
+    variant('osmesa', default=False,
+            description='Enable OSMesa support (for VTK)')
 
     extends('python@3.7:')
-    
     depends_on('cmake@3.14:', type='build')
     depends_on('swig@3.0.12', type='build')
 
     depends_on('py-cython')
     depends_on('py-numpy')
-    #depends_on('py-pip')
 
     depends_on('eigen@3.3.7')
     depends_on('cgal@4.13 build_type=Release +shared~core~demos~imageio')
 
     # vtk needs to know whether to build with mesa or opengl
-    depends_on('vtk@8.1.2 build_type=Release ~ffmpeg~mpi+opengl2~qt~xdmf +python ~osmesa', when='~osmesa')
-    depends_on('vtk@8.1.2 build_type=Release ~ffmpeg~mpi+opengl2~qt~xdmf +python +osmesa', when='+osmesa')
-    
-    # this is needed only to resolve the conflict between
-    # the default and netcdf's spec
+    vtk_conf = 'build_type=Release ~ffmpeg~mpi+opengl2~qt~xdmf+python'
+    depends_on('vtk@8.1.2 ' + vtk_conf + ' ~osmesa', when='~osmesa')
+    depends_on('vtk@8.1.2 ' + vtk_conf + ' +osmesa', when='+osmesa')
+
+    # needed only to resolve the conflict between the default and netcdf's spec
     depends_on('hdf5 +hl')
 
     # memsurfer's setup needs path to these deps to build extension modules
