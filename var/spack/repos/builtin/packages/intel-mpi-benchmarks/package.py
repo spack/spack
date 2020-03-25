54eb0e1c02373b9aa68c55a9d54d6396dcb623f5
Carson Woods <cwoods289@gmail.com>
2020-03-24 21:51:41 -0500

    Add initial attempt at intel-mpi-benchmarks package

---
+++ var/spack/repos/builtin/packages/intel-mpi-benchmarks/package.py
@@ -0,0 +1,32 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class IntelMpiBenchmarks(MakefilePackage):
+    """Intel(R) MPI Benchmarks provides a set of elementary benchmarks that conform to MPI-1, MPI-2, and MPI-3 standard. 
+       You can run all of the supported benchmarks, or a subset specified in the command line using one executable file. 
+       Use command-line parameters to specify various settings, such as time measurement, message lengths, and selection of communicators. """
+
+    homepage = "https://software.intel.com/en-us/articles/intel-mpi-benchmarks"
+    url      = "https://github.com/intel/mpi-benchmarks/archive/IMB-v2019.5.tar.gz"
+
+    maintainers = ['carsonwoods']
+
+    version('2019.5', sha256='61f8e872a3c3076af53007a68e4da3a8d66be2ba7a051dc21e626a4e2d26e651')
+
+    depends_on('gmake', type='build')
+    depends_on('mpi')
+
+    def install(self, spec, prefix):
+        env['CC'] = spec['mpi'].mpicc
+        env['CXX'] = spec['mpi'].mpicxx
+        env['F77'] = spec['mpi'].mpif77
+        env['FC'] = spec['mpi'].mpifci
+
+        install('all')
+
+
