e7f2c3be35cf42c14b7fc7e621b14eccd254f6fa
Stephanie Brink <brink2@llnl.gov>
2020-03-24 23:05:26 -0700

    xz: add version 5.2.5 (#15656)

--- var/spack/repos/builtin/packages/xz/package.py
+++ var/spack/repos/builtin/packages/xz/package.py
@@ -13,9 +13,10 @@ class Xz(AutotoolsPackage):
     to LZMA Utils."""
 
     homepage = "http://tukaani.org/xz/"
-    url      = "http://tukaani.org/xz/xz-5.2.4.tar.bz2"
+    url      = "http://tukaani.org/xz/xz-5.2.5.tar.bz2"
     list_url = "http://tukaani.org/xz/old.html"
 
+    version('5.2.5', sha256='5117f930900b341493827d63aa910ff5e011e0b994197c3b71c08a20228a42df')
     version('5.2.4', sha256='3313fd2a95f43d88e44264e6b015e7d03053e681860b0d5d3f9baca79c57b7bf')
     version('5.2.3', sha256='fd9ca16de1052aac899ad3495ad20dfa906c27b4a5070102a2ec35ca3a4740c1')
     version('5.2.2', sha256='6ff5f57a4b9167155e35e6da8b529de69270efb2b4cf3fbabf41a4ee793840b5')
