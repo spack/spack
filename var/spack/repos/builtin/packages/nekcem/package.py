a95d36bd827821eba42a0ed52466ed1e7d64d891
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-03-26 02:10:09 -0500

    [nek] Add Nek5000-v19 & polishing Nek packages

--- var/spack/repos/builtin/packages/nekcem/package.py
+++ var/spack/repos/builtin/packages/nekcem/package.py
@@ -20,9 +20,7 @@ class Nekcem(Package):
     variant('mpi', default=True, description='Build with MPI')
 
     # We only have a development version
-    version('development')
-    version('0b8bedd', commit='0b8beddfdcca646bfcc866dfda1c5f893338399b')
-    version('7332619', commit='7332619b73d03868a256614b61794dce2d95b360')
+    version('develop', branch='development')
 
     # dependencies
     depends_on('mpi', when='+mpi')
