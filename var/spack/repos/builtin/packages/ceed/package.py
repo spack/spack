864f204c30933363e0654fd50ff465332abe8355
Veselin Dobrev <dobrev@llnl.gov>
2020-03-25 17:20:18 -0700

    [ceed] Add a draft for v3.0. Not tested. Just made sure that        concretization works for 'ceed' and 'ceed+cuda'.

--- var/spack/repos/builtin/packages/ceed/package.py
+++ var/spack/repos/builtin/packages/ceed/package.py
@@ -16,15 +16,17 @@ class Ceed(BundlePackage):
 
     homepage = "https://ceed.exascaleproject.org"
 
-    version('2.0')
+    version('3.0.0')
+    version('2.0.0')
+    version('1.0.0')
 
     variant('cuda', default=False,
-            description='Build MAGMA; enable CUDA support in libCEED and OCCA')
+            description='Enable CUDA support')
     variant('mfem', default=True, description='Build MFEM and Laghos')
     variant('nek', default=True,
             description='Build Nek5000, GSLIB, Nekbone, and NekCEM')
     variant('occa', default=True,
-            description='Build OCCA; enable OCCA support in libCEED')
+            description='Enable OCCA support')
     variant('petsc', default=True,
             description='Build PETSc and HPGMG')
     variant('pumi', default=True,
@@ -34,6 +36,13 @@ class Ceed(BundlePackage):
     # TODO: Add 'int64' variant?
 
     # LibCEED
+    # ceed-3.0
+    # TODO: set release version
+    depends_on('libceed@develop~cuda', when='@3.0.0~cuda')
+    # TODO: add magma variant in libceed and use +magma here:
+    depends_on('libceed@develop+cuda', when='@3.0.0+cuda')
+    depends_on('libceed@develop+occa', when='@3.0.0+occa')
+    depends_on('libceed@develop~occa', when='@3.0.0~occa')
     # ceed-2.0
     depends_on('libceed@0.4~cuda', when='@2.0.0~cuda')
     depends_on('libceed@0.4+cuda', when='@2.0.0+cuda')
@@ -46,6 +55,10 @@ class Ceed(BundlePackage):
     depends_on('libceed@0.2~occa', when='@1.0.0~occa')
 
     # OCCA
+    # ceed-3.0
+    # TODO: set release version
+    depends_on('occa@develop~cuda', when='@3.0.0+occa~cuda')
+    depends_on('occa@develop+cuda', when='@3.0.0+occa+cuda')
     # ceed-2.0
     depends_on('occa@1.0.8~cuda', when='@2.0.0+occa~cuda')
     depends_on('occa@1.0.8+cuda', when='@2.0.0+occa+cuda')
@@ -54,6 +67,13 @@ class Ceed(BundlePackage):
     depends_on('occa@1.0.0-alpha.5+cuda', when='@1.0.0+occa+cuda')
 
     # Nek5000, GSLIB, Nekbone, and NekCEM
+    # ceed-3.0
+    # TODO: set release versions for all nek packages
+    depends_on('nek5000@develop', when='@3.0.0+nek')
+    depends_on('nektools@develop%gcc', when='@3.0.0+nek')
+    depends_on('gslib@develop', when='@3.0.0+nek')
+    depends_on('nekbone@develop', when='@3.0.0+nek')
+    depends_on('nekcem@develop', when='@3.0.0+nek')
     # ceed-2.0
     depends_on('nek5000@17.0', when='@2.0.0+nek')
     depends_on('nektools@17.0%gcc', when='@2.0.0+nek')
@@ -68,6 +88,20 @@ class Ceed(BundlePackage):
     depends_on('nekcem@0b8bedd', when='@1.0.0+nek')
 
     # PETSc, HPGMG
+    # ceed-3.0
+    # For a +quickbuild we disable hdf5, and superlu-dist in PETSc.
+    # TODO: set release versions for petsc and hpgmg
+    depends_on('petsc@develop~hdf5~superlu-dist',
+               when='@3.0.0+petsc+quickbuild')
+    depends_on('petsc@develop+mpi+double~int64', when='@3.0.0+petsc~mfem')
+    # The mfem petsc examples need the petsc variants +hypre, +suite-sparse,
+    # and +mumps:
+    depends_on('petsc@develop+mpi+hypre+suite-sparse+mumps+double~int64',
+               when='@3.0.0+petsc+mfem')
+    # TODO: petsc@develop requires hypre@develop; remove next line after the
+    #       release version is set:
+    depends_on('hypre@develop', when='@3.0.0+petsc')
+    depends_on('hpgmg@develop+fe', when='@3.0.0+petsc')
     # ceed-2.0
     # For a +quickbuild we disable hdf5, and superlu-dist in PETSc.
     # Ideally, these can be turned into recommendations to Spack for
@@ -94,18 +128,44 @@ class Ceed(BundlePackage):
     depends_on('hpgmg@a0a5510df23b+fe', when='@1.0.0+petsc')
 
     # MAGMA
+    # ceed-3.0
+    # TODO: make sure this is the real latest version
+    depends_on('magma@2.5.2', when='@3.0.0+cuda')
     # ceed-2.0
     depends_on('magma@2.5.0', when='@2.0.0+cuda')
     # ceed-1.0
     depends_on('magma@2.3.0', when='@1.0.0+cuda')
 
     # PUMI
+    # ceed-3.0
+    depends_on('pumi@2.2.2', when='@3.0.0+pumi')
     # ceed-2.0
     depends_on('pumi@2.2.0', when='@2.0.0+pumi')
     # ceed-1.0
     depends_on('pumi@2.1.0', when='@1.0.0+pumi')
 
     # MFEM, Laghos
+    # ceed-3.0
+    depends_on('mfem@4.1.0+mpi+examples+miniapps', when='@3.0.0+mfem~petsc')
+    depends_on('mfem@4.1.0+mpi+petsc+examples+miniapps',
+               when='@3.0.0+mfem+petsc')
+    depends_on('mfem@4.1.0+pumi', when='@3.0.0+mfem+pumi')
+    depends_on('mfem@4.1.0+gslib', when='@3.0.0+mfem+nek')
+    depends_on('mfem@4.1.0+libceed', when='@3.0.0+mfem')
+    depends_on('mfem@4.1.0+cuda', when='@3.0.0+mfem+cuda')
+    depends_on('mfem@4.1.0+occa', when='@3.0.0+mfem+occa')
+    # TODO: set new release version (we do not use laghos@develop for testing
+    #       since it asks for mfem@develop)
+    depends_on('laghos@2.0', when='@3.0.0+mfem')
+    # Help the spack concretizer find a suitable version of hypre:
+    # TODO: is this still necessary with the latest spack?
+    depends_on('hypre~internal-superlu', when='@3.0.0+mfem')
+    depends_on('hypre~internal-superlu~superlu-dist',
+               when='@3.0.0+mfem+quickbuild')
+
+    # If using gcc version <= 4.8 build suite-sparse version <= 5.1.0
+    depends_on('suite-sparse@:5.1.0', when='@3.0.0%gcc@:4.8+mfem+petsc')
+
     # ceed-2.0
     depends_on('mfem@3.4.0+mpi+examples+miniapps', when='@2.0.0+mfem~petsc')
     depends_on('mfem@3.4.0+mpi+petsc+examples+miniapps',
