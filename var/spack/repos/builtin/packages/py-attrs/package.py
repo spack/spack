f87284128d51eb51496f6ec7966f16fc6fad93f6
iarspider <iarspider@gmail.com>
2020-03-27 13:03:46 +0100

    py-attrs: add version 19.3.0

--- var/spack/repos/builtin/packages/py-attrs/package.py
+++ var/spack/repos/builtin/packages/py-attrs/package.py
@@ -14,6 +14,7 @@ class PyAttrs(PythonPackage):
 
     import_modules = ['attr']
 
+    version('19.3.0', sha256='f7b7ce16570fe9965acd6d30101a28f62fb4a7f9e926b3bbc9b61f8b04247e72')
     version('19.2.0', sha256='f913492e1663d3c36f502e5e9ba6cd13cf19d7fab50aa13239e420fef95e1396')
     version('19.1.0', sha256='f0b870f674851ecbfbbbd364d6b5cbdff9dcedbc7f3f5e18a6891057f21fe399')
     version('18.1.0', sha256='e0d0eb91441a3b53dab4d9b743eafc1ac44476296a2053b6ca3af0b139faf87b')
@@ -26,5 +27,6 @@ class PyAttrs(PythonPackage):
     depends_on('py-hypothesis', type='test')
     depends_on('py-pympler', type='test')
     depends_on('py-pytest', type='test')
+    depends_on('py-pytest@4.3.0:', type='test', when='@19.3.0:')
     depends_on('py-six', type='test')
     depends_on('py-zope-interface', type='test')
