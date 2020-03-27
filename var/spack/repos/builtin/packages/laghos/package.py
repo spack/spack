a623faa43b4a09aa5e619c4a35748df9de2148d3
Veselin Dobrev <dobrev@llnl.gov>
2020-03-27 13:31:00 -0700

    [laghos] Keep the injection of CXX in the makefile for laghos          versions <= 2.0.

--- var/spack/repos/builtin/packages/laghos/package.py
+++ var/spack/repos/builtin/packages/laghos/package.py
@@ -46,6 +46,8 @@ class Laghos(MakefilePackage):
         targets.append('MFEM_DIR=%s' % spec['mfem'].prefix)
         targets.append('CONFIG_MK=%s' % spec['mfem'].package.config_mk)
         targets.append('TEST_MK=%s' % spec['mfem'].package.test_mk)
+        if spec.satisfies('@:2.0'):
+            targets.append('CXX=%s' % spec['mpi'].mpicxx)
 
         return targets
 
