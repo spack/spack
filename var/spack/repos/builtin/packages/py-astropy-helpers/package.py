4764fd8d0c1d6f55d492ee6de5b47e3c63971254
Jen Herting <jen@herting.cc>
2020-03-27 09:30:29 -0400

    [py-astropy-helpers] cleaned up unnecessary older versions

--- var/spack/repos/builtin/packages/py-astropy-helpers/package.py
+++ var/spack/repos/builtin/packages/py-astropy-helpers/package.py
@@ -16,15 +16,6 @@ class PyAstropyHelpers(PythonPackage):
     url      = "https://github.com/astropy/astropy-helpers/archive/v4.0.1.tar.gz"
 
     version('4.0.1',  sha256='88602971c3b63d6aaa6074d013f995d1e234acb3d517d70d7fcebd30cdaf5c89')
-    version('4.0rc1', sha256='322aeda0ff0c8459efc6306b4d26eb115d09e96946f48f1dfe6e91f41ea6af1a')
-    version('4.0',    sha256='ca50fdcbe432fb101bbedda6db3cd256ab8b52a9b402ac4fc1c488cc79d11dba')
-    version('3.2.2',  sha256='101b657b6d5785ab26c11082125340655578698ca6c38c2ddb33d73c1084a8f4')
-    version('3.2.1',  sha256='2638157c65bbf92530b7ecc86bc62c16ec4bc5ccc533f0d96d014f605a8386a3')
-    version('3.2rc1', sha256='f9a5ae63a46f78af032a656c14790ff3ed63773c574adf0fb7ddf3a916d54ddb')
-    version('3.2',    sha256='0671f70eb67f82d32c23d6b30542c7a1985335ed2fb5e95eb1804bf333258c5b')
-    version('3.1.1',  sha256='8ba7e693f1cc9a1c12be3b4ba9e65b67246bff1741c726b79a6dc7b233ba4cd4')
-    version('2.0.11', sha256='0cfca63fe2d9920ac1138ffd0a977572b41439e02f6f500f6f62981ef138f0ca')
-    version('2.0.10', sha256='528421568531719447677df73f547c25305fbd116c6692faa0f268eac4a3e6d6')
 
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools@30.3:', type='build')
