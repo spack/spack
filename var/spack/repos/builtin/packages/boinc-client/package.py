1762d002af3c7c22b08aece3e7d60fccb08cd400
Jen Herting <jen@herting.cc>
2020-03-27 17:26:38 -0400

    [boinc-client] added dependency version specificity

--- var/spack/repos/builtin/packages/boinc-client/package.py
+++ var/spack/repos/builtin/packages/boinc-client/package.py
@@ -43,10 +43,17 @@ class BoincClient(AutotoolsPackage):
     variant('manager', default=False, description='Builds the client manager')
     variant('graphics', default=False, description='Graphic apps support')
 
-    depends_on('autoconf', type='build')
-    depends_on('automake', type='build')
-    depends_on('libtool',  type='build')
-    depends_on('m4',       type='build')
+    # Dependency documentation:
+    # https://boinc.berkeley.edu/trac/wiki/SoftwarePrereqsUnix
+    conflicts('%gcc@:3.0.4')
+
+    depends_on('autoconf@2.58:', type='build')
+    depends_on('automake@1.8:',  type='build')
+    depends_on('libtool@1.5:',   type='build')
+    depends_on('m4@1.4:',        type='build')
+
+    depends_on('curl@7.17.1:')
+    depends_on('openssl@0.9.8:')
 
     depends_on('freeglut@3:', when='+graphics')
     depends_on('libsm', when='+graphics')
@@ -56,7 +63,7 @@ class BoincClient(AutotoolsPackage):
     depends_on('libx11', when='+graphics')
     depends_on('libjpeg', when='+graphics')
     
-    depends_on('wxwidgets', when='+manager')
+    depends_on('wxwidgets@3.0.0:', when='+manager')
     depends_on('libnotify', when='+manager')
 
     def autoreconf(self, spec, prefix):
