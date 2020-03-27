a82b652817b03507bb457f21f09f7ad834507252
iarspider <iarspider@gmail.com>
2020-03-27 17:17:16 +0100

    Update package.py

--- var/spack/repos/builtin/packages/py-multiprocess/package.py
+++ var/spack/repos/builtin/packages/py-multiprocess/package.py
@@ -17,7 +17,7 @@ class PyMultiprocess(PythonPackage):
     version('0.70.5', sha256='c4c196f3c4561dc1d78139c3e73709906a222d2fc166ef3eef895d8623df7267')
     version('0.70.4', sha256='a692c6dc8392c25b29391abb58a9fbdc1ac38bca73c6f27d787774201e68e12c')
 
-    depends_on('python@2.6:2.8,3.1:')
+    depends_on('python@2.5:2.8,3.1:', type=('build', 'run'))
 
     depends_on('py-setuptools@0.6:', type='build')
     depends_on('py-dill@0.2.6:', type=('build', 'run'))
