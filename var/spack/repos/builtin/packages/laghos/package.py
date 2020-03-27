382b04a03c83f3cf0fa5adae981cbeae2529a962
Veselin Dobrev <dobrev@llnl.gov>
2020-03-26 20:39:54 -0700

    [laghos] Add version 3.0, pointing to dev branch for now.          Do not set CXX at the make command line.          Simplify the dependecy directives a little.
    [ceed] Use laghos v3.0 for ceed v3.0.0.

--- var/spack/repos/builtin/packages/laghos/package.py
+++ var/spack/repos/builtin/packages/laghos/package.py
@@ -19,22 +19,24 @@ class Laghos(MakefilePackage):
     git      = "https://github.com/CEED/Laghos.git"
 
     version('develop', branch='master')
+    # TODO: replace the branch 'okina' with the new sha256 sum when the 3.0
+    #       release is done.
+    version('3.0', branch='okina')
     version('2.0', sha256='dd3632d5558889beec2cd3c49eb60f633f99e6d886ac868731610dd006c44c14')
     version('1.1', sha256='53b9bfe2af263c63eb4544ca1731dd26f40b73a0d2775a9883db51821bf23b7f')
     version('1.0', sha256='af50a126355a41c758fcda335a43fdb0a3cd97e608ba51c485afda3dd84a5b34')
 
     variant('metis', default=True, description='Enable/disable METIS support')
 
-    depends_on('mfem@develop+mpi+metis', when='@develop+metis')
-    depends_on('mfem@develop+mpi~metis', when='@develop~metis')
+    depends_on('mfem+mpi+metis', when='+metis')
+    depends_on('mfem+mpi~metis', when='~metis')
 
+    depends_on('mfem@develop', when='@develop')
+    depends_on('mfem@4.1.0:', when='@3.0')
     # Recommended mfem version for laghos v2.0 is: ^mfem@3.4.1-laghos-v2.0
-    depends_on('mfem@3.4.0:+mpi+metis', when='@2.0+metis')
-    depends_on('mfem@3.4.0:+mpi~metis', when='@2.0~metis')
-
+    depends_on('mfem@3.4.0:', when='@2.0')
     # Recommended mfem version for laghos v1.x is: ^mfem@3.3.1-laghos-v1.0
-    depends_on('mfem@3.3.1-laghos-v1.0:+mpi+metis', when='@1.0,1.1+metis')
-    depends_on('mfem@3.3.1-laghos-v1.0:+mpi~metis', when='@1.0,1.1~metis')
+    depends_on('mfem@3.3.1-laghos-v1.0:', when='@1.0,1.1')
 
     @property
     def build_targets(self):
@@ -44,7 +46,6 @@ class Laghos(MakefilePackage):
         targets.append('MFEM_DIR=%s' % spec['mfem'].prefix)
         targets.append('CONFIG_MK=%s' % spec['mfem'].package.config_mk)
         targets.append('TEST_MK=%s' % spec['mfem'].package.test_mk)
-        targets.append('CXX=%s' % spec['mpi'].mpicxx)
 
         return targets
 
