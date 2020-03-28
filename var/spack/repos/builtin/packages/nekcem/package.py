60cc1e3aa83169980593f34b45756a13a253f1dd
Veselin Dobrev <dobrev@llnl.gov>
2020-03-27 17:26:47 -0700

    [nekcem] Recovert hash-versions used by older versions of the          'ceed' package.

--- var/spack/repos/builtin/packages/nekcem/package.py
+++ var/spack/repos/builtin/packages/nekcem/package.py
@@ -21,6 +21,9 @@ class Nekcem(Package):
 
     # We only have a development version
     version('develop', branch='development')
+    # The following hash-versions are used by the 'ceed' package
+    version('0b8bedd', commit='0b8beddfdcca646bfcc866dfda1c5f893338399b')
+    version('7332619', commit='7332619b73d03868a256614b61794dce2d95b360')
 
     # dependencies
     depends_on('mpi', when='+mpi')
