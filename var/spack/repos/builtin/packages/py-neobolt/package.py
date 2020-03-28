f82cf97c8926942a82dc542cfb847f3a1c5282bb
iarspider <iarspider@gmail.com>
2020-03-28 10:10:04 +0100

    Apply suggestions from code review
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-neobolt/package.py
+++ var/spack/repos/builtin/packages/py-neobolt/package.py
@@ -15,4 +15,4 @@ class PyNeobolt(PythonPackage):
     version('1.7.16', sha256='ca4e87679fe3ed39aec23638658e02dbdc6bbc3289a04e826f332e05ab32275d')
 
     depends_on('py-setuptools', type='build')
-    depends_on('python@2.7,3.4:', type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
