49fd59ecf3ccec41a554414845623f033fff7234
Jen Herting <jen@herting.cc>
2020-03-30 18:16:01 -0400

    [libnotify] cleaned up leftover fixme

--- var/spack/repos/builtin/packages/libnotify/package.py
+++ var/spack/repos/builtin/packages/libnotify/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install libnotify
-#
-# You can edit this file again by typing:
-#
-#     spack edit libnotify
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -28,10 +11,6 @@ class Libnotify(MesonPackage):
 
     homepage = "https://github.com/GNOME/libnotify"
     url      = "https://github.com/GNOME/libnotify/archive/0.7.9.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.7.9', sha256='9bd4f5fa911d27567e7cc2d2d09d69356c16703c4e8d22c0b49a5c45651f3af0')
 
