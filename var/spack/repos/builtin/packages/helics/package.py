94608b54ddb20f356cd64a84cc1482e375460c89
Ryan Mast <mast9@llnl.gov>
2020-03-30 15:00:59 -0700

    Remove FIXME boilerplate

--- var/spack/repos/builtin/packages/helics/package.py
+++ var/spack/repos/builtin/packages/helics/package.py
@@ -3,25 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install helics
-#
-# You can edit this file again by typing:
-#
-#     spack edit helics
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
-
 
 class Helics(CMakePackage):
     """HELICS is a general-purpose, modular, highly-scalable co-simulation
