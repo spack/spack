654e06e8328eec9c449cf876b36bd2502e7a4b1f
Jen Herting <jen@herting.cc>
2020-03-30 18:17:50 -0400

    [gtk-doc] flake8

--- var/spack/repos/builtin/packages/gtk-doc/package.py
+++ var/spack/repos/builtin/packages/gtk-doc/package.py
@@ -17,9 +17,9 @@ class GtkDoc(AutotoolsPackage):
     homepage = "https://wiki.gnome.org/DocumentationProject/GtkDoc"
 
     version('1.32', sha256='0890c1f00d4817279be51602e67c4805daf264092adc58f9c04338566e8225ba')
-    
+
     # Commented out until package dblatex has been created
-    #variant('pdf', default=False, description='Adds PDF support')
+    # variant('pdf', default=False, description='Adds PDF support')
 
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
@@ -32,10 +32,9 @@ class GtkDoc(AutotoolsPackage):
     depends_on('libxslt')
     depends_on('libxml2')
     depends_on('docbook-xsl')
-    #depends_on('dblatex', when='+pdf')
+    # depends_on('dblatex', when='+pdf')
 
     def url_for_version(self, version):
         """Handle gnome's version-based custom URLs."""
         url = 'https://gitlab.gnome.org/GNOME/gtk-doc/-/archive/GTK_DOC_{0}/gtk-doc-GTK_DOC_{0}.tar.gz'
         return url.format(version.underscored)
-
