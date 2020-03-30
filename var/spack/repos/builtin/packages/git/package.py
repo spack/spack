7d02be1b45ee40d0387e9fe632943aadd7de65ec
Amjad Kotobi <amjadkotbi@gmail.com>
2020-03-30 15:08:02 +0200

    Fix1

--- var/spack/repos/builtin/packages/git/package.py
+++ var/spack/repos/builtin/packages/git/package.py
@@ -24,7 +24,8 @@ class Git(AutotoolsPackage):
     # You can find the source here: https://mirrors.edge.kernel.org/pub/software/scm/git/sha256sums.asc
 
     releases = [
-        {   'version': '2.26.0',
+        {
+            'version': '2.26.0',
             'sha256': 'aa168c2318e7187cd295a645f7370cc6d71a324aafc932f80f00c780b6a26bed',
             'sha256_manpages': 'c1ffaf0b4cd1e80a0eb0d4039e208c9d411ef94d5da44e38363804e1a7961218'
         },
