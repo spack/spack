6e7a8bb7a7477fb1aef348109f0af24329323b18
Adam J. Stewart <ajstewart426@gmail.com>
2020-03-30 20:14:40 -0500

    Fix bad rebase

--- var/spack/repos/builtin/packages/otf2/package.py
+++ var/spack/repos/builtin/packages/otf2/package.py
@@ -32,6 +32,6 @@ class Otf2(AutotoolsPackage):
             'F77={0}'.format(spack_f77),
             'FC={0}'.format(spack_fc),
             'CFLAGS={0}'.format(self.compiler.cc_pic_flag),
-            'CXXFLAGS={0}'.format(self.compiler.cxx_pic_flag)
-            'PYTHON_FOR_GENERATOR=:'
+            'CXXFLAGS={0}'.format(self.compiler.cxx_pic_flag),
+            'PYTHON_FOR_GENERATOR=:',
         ]
