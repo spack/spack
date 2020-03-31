77e13849026b9b177c27fcaefd134dd32e4329e4
Ben Morgan <drbenmorgan@users.noreply.github.com>
2020-03-31 09:54:31 +0100

    geant4: new version 10.6 plus simplifications (#15447)
    * geant4: new version 10.6 plus simplifications
    
    Add new 10.6.0 release, migrating download of source to use Geant4's
    public release repo on CERN GitLab. Change versioning scheme to use
    clearer and standard semantic scheme.
    
    Update geant4-data and g4XXX data packages with new versions. Migrate
    geant4-data as a BundlePackage of the g4XXX packages, installing links
    to each under a single directory under share for geant4-data. Ensure
    each g4XXX package exports the environment variable pointing to its
    location expected by Geant4.
    
    Remove "data" variant from Geant4 package and always use geant4-data.
    
    Simplify cxxstd variant transport to dependencies.
    
    * g4<DATA>: Use self to resolve correct prefix
    
    * geant4, data: Fix flake8 errors
    
    * g4photonevaporation: flake8 fix
    
    * geant4: vecgeom version depends_on
    
    Geant4 major.minor versions have specific dependencies on vecgeom
    versions. Add missing vecgeom version for geant4 10.5, and match
    version requirements for vecgeom in geant4 depends_on.
    
    * geant4: c++17 patch specific for 10.4.3
    
    * geant4: simplify geant4-data setup
    
    * geant4: Use new define_from_variant function
    
    * geant4: fix flake8 errors

--- var/spack/repos/builtin/packages/geant4-data/package.py
+++ var/spack/repos/builtin/packages/geant4-data/package.py
@@ -8,90 +8,89 @@ import os
 import glob
 
 
-class Geant4Data(Package):
-    """An umbrella package to hold Geant4 data packages"""
+class Geant4Data(BundlePackage):
+    """A bundle package to hold Geant4 data packages"""
 
     homepage = "http://geant4.cern.ch"
-    url      = "http://geant4-data.web.cern.ch/geant4-data/ReleaseNotes/ReleaseNotes4.10.3.html"
 
     maintainers = ['drbenmorgan']
 
-    version('10.03.p03', sha256='3e0d4d4e6854c8667d930fd5beaec09b7e6ec41f4847935e5d6a2720d0094b30', expand=False)
-    version('10.04', sha256='f67fb899b99348a1a7e471a05f249f972e7e303c78238fc5f693b99968642255', expand=False)
+    version('10.6.0')
+    version('10.5.1')
+    version('10.4.3')
+    version('10.4.0')
+    version('10.3.3')
 
-    # geant4@10.03.p03
-    depends_on("g4abla@3.0", when='@10.03.p03 ')
-    depends_on("g4emlow@6.50", when='@10.03.p03 ')
-    depends_on("g4ndl@4.5", when='@10.03.p03 ')
-    depends_on("g4neutronxs@1.4", when='@10.03.p03 ')
-    depends_on("g4saiddata@1.1", when='@10.03.p03 ')
-    depends_on("g4ensdfstate@2.1", when='@10.03.p03 ')
-    depends_on("g4photonevaporation@4.3.2", when='@10.03.p03 ')
-    depends_on("g4pii@1.3", when='@10.03.p03 ')
-    depends_on("g4radioactivedecay@5.1.1", when='@10.03.p03 ')
-    depends_on("g4realsurface@1.0", when='@10.03.p03 ')
-    depends_on("g4tendl@1.3", when='@10.03.p03 ')
-    # geant4@10.04
-    depends_on("g4abla@3.1", when='@10.04 ')
-    depends_on("g4emlow@7.3", when='@10.04 ')
-    depends_on("g4ndl@4.5", when='@10.04 ')
-    depends_on("g4neutronxs@1.4", when='@10.04 ')
-    depends_on("g4saiddata@1.1", when='@10.04 ')
-    depends_on("g4ensdfstate@2.2", when='@10.04 ')
-    depends_on("g4photonevaporation@5.2", when='@10.04 ')
-    depends_on("g4pii@1.3", when='@10.04 ')
-    depends_on("g4radioactivedecay@5.2", when='@10.04 ')
-    depends_on("g4realsurface@2.1", when='@10.04 ')
-    depends_on("g4tendl@1.3.2", when='@10.04 ')
+    # Add install phase so we can create the data "view"
+    phases = ['install']
+
+    # For clarity, declare deps on a Major-Minor version basis as
+    # they generally don't change on the patch level
+    # Can move to declaring on a dataset basis if needed
+    # geant4@10.6.X
+    depends_on("g4ndl@4.6", when='@10.6.0')
+    depends_on("g4emlow@7.9", when='@10.6.0')
+    depends_on("g4photonevaporation@5.5", when='@10.6.0')
+    depends_on("g4radioactivedecay@5.4", when='@10.6.0')
+    depends_on("g4particlexs@2.1", when='@10.6.0')
+    depends_on("g4pii@1.3", when='@10.6.0')
+    depends_on("g4realsurface@2.1.1", when='@10.6.0')
+    depends_on("g4saiddata@2.0", when='@10.6.0')
+    depends_on("g4abla@3.1", when='@10.6.0')
+    depends_on("g4incl@1.0", when='@10.6.0')
+    depends_on("g4ensdfstate@2.2", when='@10.6.0')
+
+    # geant4@10.5.X
+    depends_on("g4ndl@4.5", when='@10.5.0:10.5.9999')
+    depends_on("g4emlow@7.7", when='@10.5.0:10.5.9999')
+    depends_on("g4photonevaporation@5.3", when='@10.5.0:10.5.9999')
+    depends_on("g4radioactivedecay@5.3", when='@10.5.0:10.5.9999')
+    depends_on("g4particlexs@1.1", when='@10.5.0:10.5.9999')
+    depends_on("g4pii@1.3", when='@10.5.0:10.5.9999')
+    depends_on("g4realsurface@2.1.1", when='@10.5.0:10.5.9999')
+    depends_on("g4saiddata@2.0", when='@10.5.0:10.5.9999')
+    depends_on("g4abla@3.1", when='@10.5.0:10.5.9999')
+    depends_on("g4incl@1.0", when='@10.5.0:10.5.9999')
+    depends_on("g4ensdfstate@2.2", when='@10.5.0:10.5.9999')
+
+    # geant4@10.4.X
+    depends_on("g4ndl@4.5", when='@10.4.0:10.4.9999')
+    depends_on("g4emlow@7.3", when='@10.4.0:10.4.9999')
+    depends_on("g4photonevaporation@5.2", when='@10.4.0:10.4.9999')
+    depends_on("g4radioactivedecay@5.2", when='@10.4.0:10.4.9999')
+    depends_on("g4neutronxs@1.4", when='@10.4.0:10.4.9999')
+    depends_on("g4pii@1.3", when='@10.4.0:10.4.9999')
+
+    depends_on("g4realsurface@2.1.1", when='@10.4.2:10.4.9999')
+    depends_on("g4realsurface@2.1", when='@10.4.0:10.4.1')
+
+    depends_on("g4saiddata@1.1", when='@10.4.0:10.4.9999')
+    depends_on("g4abla@3.1", when='@10.4.0:10.4.9999')
+    depends_on("g4ensdfstate@2.2", when='@10.4.0:10.4.9999')
+
+    # geant4@10.3.X
+    depends_on("g4ndl@4.5", when='@10.3.0:10.3.9999')
+    depends_on("g4emlow@6.50", when='@10.3.0:10.3.9999')
+
+    depends_on("g4photonevaporation@4.3.2", when='@10.3.1:10.3.9999')
+    depends_on("g4photonevaporation@4.3", when='@10.3.0')
+
+    depends_on("g4radioactivedecay@5.1.1", when='@10.3.1:10.3.9999')
+    depends_on("g4radioactivedecay@5.1", when='@10.3.0')
+
+    depends_on("g4neutronxs@1.4", when='@10.3.0:10.3.9999')
+    depends_on("g4pii@1.3", when='@10.3.0:10.3.9999')
+    depends_on("g4realsurface@1.0", when='@10.3.0:10.3.9999')
+    depends_on("g4saiddata@1.1", when='@10.3.0:10.3.9999')
+    depends_on("g4abla@3.0", when='@10.3.0:10.3.9999')
+    depends_on("g4ensdfstate@2.1", when='@10.3.0:10.3.9999')
 
     def install(self, spec, prefix):
         spec = self.spec
-        version = self.version
-        major = version[0]
-        minor = version[1]
-        if len(version) > 2:
-            patch = version[-1]
-        else:
-            patch = 0
-        data = 'Geant4-%s.%s.%s/data' % (major, minor, patch)
+        data = '{0}-{1}'.format(self.name, self.version.dotted)
         datadir = join_path(spec.prefix.share, data)
-        with working_dir(datadir, create=True):
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4abla'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4emlow'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4ndl'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4saiddata'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4neutronxs'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4ensdfstate'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4photonevaporation'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4pii'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4radioactivedecay'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4realsurface'].prefix):
-                os.symlink(d, os.path.basename(d))
-            for d in glob.glob('%s/share/data/*' %
-                               spec['g4tendl'].prefix):
-                os.symlink(d, os.path.basename(d))
 
-    def url_for_version(self, version):
-        """Handle version string."""
-        url = 'http://geant4-data.web.cern.ch/geant4-data/ReleaseNotes/'
-        url = url + 'ReleaseNotes4.{0}.{1}.html'.format(version[0], version[1])
-        return url
+        with working_dir(datadir, create=True):
+            for s in spec.dependencies():
+                for d in glob.glob('{0}/data/*'.format(s.prefix.share)):
+                    os.symlink(d, os.path.basename(d))
