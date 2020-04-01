62a15f2980eceef8e7eeb889e1c8bff899931858
Michael Bentley <mikebentley15@gmail.com>
2020-04-01 14:45:49 -0600

    Update flit package to v2.1.0 and add dependencies

--- var/spack/repos/builtin/packages/flit/package.py
+++ var/spack/repos/builtin/packages/flit/package.py
@@ -3,9 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
 from spack import *
-
 
 class Flit(MakefilePackage):
     """Floating-point Litmus Tests (FLiT) is a C++ test infrastructure for
@@ -14,16 +12,23 @@ class Flit(MakefilePackage):
 
     homepage = "https://pruners.github.io/flit"
     url      = "https://github.com/PRUNERS/FLiT"
-    url      = "https://github.com/PRUNERS/FLiT/archive/v2.0-alpha.1.tar.gz"
+    url      = "https://github.com/PRUNERS/FLiT/archive/v2.1.0.tar.gz"
 
+    version('2.1.0', sha256='b31ffa02fda1ab0f5555acdc6edc353d93d53ae8ef85e099f83bcf1c83e70885')
     version('2.0-alpha.1', sha256='8de2bd400acf0f513d69f3dbf588e8984dfb18b8ccaaf684391811a0582f694b')
 
-    # Add dependencies
-    depends_on('python@3:', type='run')
-    depends_on('py-numpy', type='run')
-    depends_on('py-matplotlib tk=False', type='run')
-    depends_on('py-toml', type='run')
+    maintainers = ['mikebentley15']
 
-    @property
-    def install_targets(self):
-        return ['install', 'PREFIX=%s' % self.prefix]
+    # Add dependencies
+    depends_on('python@3:',      type='run')
+    depends_on('py-toml',        type='run')
+    depends_on('py-pyelftools',  type='run')
+
+    depends_on('bash',           type='run')
+    depends_on('binutils@2.26:', type='run')
+    depends_on('coreutils',      type='run')
+    depends_on('gmake',          type=('run', 'build'))
+    depends_on('sqlite@3:',      type='run')
+
+    def edit(self, spec, prefix):
+        env['PREFIX'] = prefix
