318a4807d92c54635cec435871518661e4a08da4
Sinan81 <sbulut@3vgeomatics.com>
2020-03-31 23:07:41 -0700

    add new versions

--- var/spack/repos/builtin/packages/postgis/package.py
+++ var/spack/repos/builtin/packages/postgis/package.py
@@ -16,8 +16,9 @@ class Postgis(AutotoolsPackage):
     homepage = "https://postgis.net/"
     url      = "https://download.osgeo.org/postgis/source/postgis-2.5.3.tar.gz"
 
-    version('3.0.0alpha4', sha256='f20b38fe002a5d5f1a9f8d9ee16a55cb284ad3b4606cacefa0e43e1c2ef2e4dd')
-    version('2.5.3', preferred=True, sha256='72e8269d40f981e22fb2b78d3ff292338e69a4f5166e481a77b015e1d34e559a')
+    version('3.0.1', sha256='5a5432f95150d9bae9215c6d1c7bb354e060482a7c379daa9b8384e1d03e6353')
+    version('3.0.0', sha256='c06fd2cd5cea0119106ffe17a7235d893c2bbe6f4b63c8617c767630973ba594')
+    version('2.5.3', sha256='72e8269d40f981e22fb2b78d3ff292338e69a4f5166e481a77b015e1d34e559a')
 
     variant('gui', default=False, description='Build with GUI support, creating shp2pgsql-gui graphical interface to shp2pgsql')
 
