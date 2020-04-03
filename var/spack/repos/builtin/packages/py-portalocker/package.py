3ce3bca37ec0805ab5502790899d0feb192dbaae
Jen Herting <jen@herting.cc>
2020-04-03 12:18:03 -0400

    [py-portalocker] cleaned up fixmes

--- var/spack/repos/builtin/packages/py-portalocker/package.py
+++ var/spack/repos/builtin/packages/py-portalocker/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-portalocker
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-portalocker
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -30,17 +13,7 @@ class PyPortalocker(PythonPackage):
     homepage = "https://github.com/WoLpH/portalocker"
     url      = "https://github.com/WoLpH/portalocker/archive/v1.6.0.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('1.6.0', sha256='084ff315ccb9fb38a7c06155d409da5df29647da7c6d2bc2b24637f9f79001ff')
 
     depends_on('python', type=('build', 'run'))
     depends_on('py-setuptools@38.3.0:', type='build')
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
