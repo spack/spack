4173c2786832b2eec57c4098668586d0e8985db4
Matthieu Dorier <mdorier@anl.gov>
2020-04-03 21:48:29 +0100

    unqlite: restricted patch to version 1.1.9 (#15857)
    * restricted unqlite patch to version 1.1.9
    
    * putting back the URL of the PR corresponding to the patch

--- var/spack/repos/builtin/packages/unqlite/package.py
+++ var/spack/repos/builtin/packages/unqlite/package.py
@@ -18,8 +18,7 @@ class Unqlite(CMakePackage):
     version('1.1.9', sha256='33d5b5e7b2ca223942e77d31112d2e20512bc507808414451c8a98a7be5e15c0')
 
     # This patch corresponds to https://github.com/symisc/unqlite/pull/99
-    # It should restricted to @1.1.9 once the PR lands.
-    patch('0001-Removed-the-STATIC-key-word-to-enable-building-a-sha.patch')
+    patch('0001-Removed-the-STATIC-key-word-to-enable-building-a-sha.patch', when='@1.1.9')
 
     def cmake_args(self):
         args = ["-DBUILD_SHARED_LIBS:BOOL=ON"]
