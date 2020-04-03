f111812e7a585cf0501aa3c0289f71fc1c747e41
Jen Herting <jen@herting.cc>
2020-04-03 12:19:22 -0400

    [py-gluon-cv] cleaned up fixmes

--- var/spack/repos/builtin/packages/py-gluon-cv/package.py
+++ var/spack/repos/builtin/packages/py-gluon-cv/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-gluon-cv
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-gluon-cv
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -32,10 +15,6 @@ class PyGluonCv(PythonPackage):
 
     homepage = "https://gluon-cv.mxnet.io/"
     url      = "https://github.com/dmlc/gluon-cv/archive/v0.6.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.6.0', sha256='5ac89d73f34d02b2e60595a5cc35f46d0a69376567fae3a9518005dd89161305')
 
@@ -50,9 +29,3 @@ class PyGluonCv(PythonPackage):
     depends_on('py-scipy',       type=('build', 'run'))
 
     patch('no-unicode-readme.patch')
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
