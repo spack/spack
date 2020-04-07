8e42f1312eaa1637a2b8306ccfefbb006172949b
naromero77 <naromero@anl.gov>
2020-04-07 18:27:48 -0500

    Add Cmake flags that toggle Fortran Intermediate Representation on/off. Exclude Flang tests for now.

--- var/spack/repos/builtin/packages/f18/package.py
+++ var/spack/repos/builtin/packages/f18/package.py
@@ -34,3 +34,20 @@ class F18(CMakePackage):
     conflicts('%gcc@:7.1', msg=compiler_warning)
     conflicts('%intel', msg=compiler_warning)
     conflicts('%pgi', msg=compiler_warning)
+
+    def cmake_args(self):
+        spec = self.spec
+        args = ['-DLLVM_DIR=%s' % self.spec['llvm'].prefix.lib.cmake.llvm]
+        # Tests have linking errors with older compilers (before GCC 8.x).
+        # Don't build tests for now.
+        # https://bugs.llvm.org/show_bug.cgi?id=45463
+        args.append('-DFLANG_INCLUDE_TESTS:BOOL=OFF')
+
+        if '+fir' in spec:
+            args.append('-DLINK_WITH_FIR:BOOL=ON')
+            args.append(
+                '-DMLIR_DIR=%s' % self.spec['llvm'].prefix.lib.cmake.mlir)
+        else:
+            args.append('-DLINK_WITH_FIR:BOOL=OFF')
+
+        return args
