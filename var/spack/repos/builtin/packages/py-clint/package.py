40d3798f5d0b38ffc67c470645aa02351289b817
codeandkey <jtst@iastate.edu>
2020-04-07 17:09:35 -0500

    py-clint: update py-setuptools dep type

--- var/spack/repos/builtin/packages/py-clint/package.py
+++ var/spack/repos/builtin/packages/py-clint/package.py
@@ -14,5 +14,5 @@ class PyClint(PythonPackage):
 
     version('0.5.1', sha256='05224c32b1075563d0b16d0015faaf9da43aa214e4a2140e51f08789e7a4c5aa')
 
-    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
     depends_on('py-args', type=('build', 'run'))
