d24c59c9b637ec1b78ca22439f2a298fd3296cee
Hadrien G <knights_of_ni@gmx.com>
2020-04-08 00:00:01 +0200

    [acts-core] Add version 0.21 (#15922)
    * Add ACTS v0.21
    
    * Update var/spack/repos/builtin/packages/acts-core/package.py
    
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/acts-core/package.py
+++ var/spack/repos/builtin/packages/acts-core/package.py
@@ -33,7 +33,8 @@ class ActsCore(CMakePackage):
     git      = "https://github.com/acts-project/acts.git"
     maintainers = ['HadrienG2']
 
-    version('develop', branch='master')
+    version('master', branch='master')
+    version('0.21.0', commit='10b719e68ddaca15b28ac25b3daddce8c0d3368d')
     version('0.20.0', commit='1d37a849a9c318e8ca4fa541ef8433c1f004637b')
     version('0.19.0', commit='408335636486c421c6222a64372250ef12544df6')
     version('0.18.0', commit='d58a68cf75b52a5e0f563bc237f09250aa9da80c')
