cd1d786d30f499044b686d1053fe3c2b441fd1ba
Gerald Ragghianti <ragghianti@icl.utk.edu>
2020-04-08 16:50:41 -0400

    Added explicit conflict for browser version requirement

--- var/spack/repos/builtin/packages/papi/package.py
+++ var/spack/repos/builtin/packages/papi/package.py
@@ -42,6 +42,8 @@ class Papi(AutotoolsPackage):
 
     depends_on('lm-sensors', when='+lmsensors')
 
+    conflicts('%gcc@8:', when='@:5.6.99', msg='Requires GCC version less than 8.0')
+
     # Does not build with newer versions of gcc, see
     # https://bitbucket.org/icl/papi/issues/46/cannot-compile-on-arch-linux
     patch('https://bitbucket.org/icl/papi/commits/53de184a162b8a7edff48fed01a15980664e15b1/raw', sha256='64c57b3ad4026255238cc495df6abfacc41de391a0af497c27d0ac819444a1f8', when='@5.4.0:5.6.99%gcc@8:')
