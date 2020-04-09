9e8d17e5cbc4d3ce70c771daa571cace55530d1d
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2020-04-09 19:42:50 +0200

    meson: Make ninja a hard dependency

--- var/spack/repos/builtin/packages/meson/package.py
+++ var/spack/repos/builtin/packages/meson/package.py
@@ -22,11 +22,9 @@ class Meson(PythonPackage):
     version('0.41.2', sha256='2daf448d3f2479d60e30617451f09bf02d26304dd1bd12ee1de936a53e42c7a4')
     version('0.41.1', sha256='a48901f02ffeb9ff5cf5361d71b1fca202f9cd72998043ad011fc5de0294cf8b')
 
-    variant('ninjabuild', default=True)
-
-    depends_on('python@3:', type=('build', 'run'))
+    depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
-    depends_on('ninja', when='+ninjabuild', type=('build', 'run'))
+    depends_on('ninja', type='run')
 
     # By default, Meson strips the rpath on installation. This patch disables
     # rpath modification completely to make sure that Spack's rpath changes
