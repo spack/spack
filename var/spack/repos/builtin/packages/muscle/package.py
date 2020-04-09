63e74f51132279812590665de1b192e01e0027e9
codeandkey <jtst@iastate.edu>
2020-04-09 13:40:06 -0500

    muscle: add 3.8.31

--- var/spack/repos/builtin/packages/muscle/package.py
+++ var/spack/repos/builtin/packages/muscle/package.py
@@ -14,13 +14,25 @@ class Muscle(MakefilePackage):
     homepage = "http://drive5.com/muscle/"
     url      = "http://www.drive5.com/muscle/muscle_src_3.8.1551.tar.gz"
 
+    version('3.8.31', sha256='43c5966a82133bd7da5921e8142f2f592c2b5f53d802f0527a2801783af809ad',
+            url='https://drive5.com/muscle/downloads3.8.31/muscle3.8.31_src.tar.gz')
     version('3.8.1551', sha256='c70c552231cd3289f1bad51c9bd174804c18bb3adcf47f501afec7a68f9c482e')
 
+    build_directory = 'src'
+
+    @run_before('edit')
+    def set_builddir(self):
+        # build_directory changed in newer versions, but the attribute
+        # is immutable so symlinks must be used to redirect the build.
+        if not self.spec.satisfies('@3.8.31:'):
+            symlink(self.stage.source_path, self.build_directory)
+
     def edit(self, spec, prefix):
-        makefile = FileFilter('Makefile')
+        target_name = 'mk' if self.spec.satisfies('@3.8.31:') else 'Makefile'
+        makefile = FileFilter(join_path(self.build_directory, target_name))
         makefile.filter('-static', '')
         makefile.filter('-funroll-loops', '')
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        install('muscle', prefix.bin)
+        install('{0}/muscle'.format(self.build_directory), prefix.bin)
