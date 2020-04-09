e630082bd249593157445d787a8256608962af51
Glenn P Johnson <glenn-johnson@uiowa.edu>
2020-04-09 15:39:13 -0500

    new package: py-youtube-dl + fixes for dependencies
    This PR adds the py-youtube-dl program. In addition, there are a couple
    of dependency packages that needed to be updated.
    
    * ffmpeg
    This is needed by py-youtube-dl. However, the spack ffmpeg recipe does
    not include a lot of options, specifically, a dependency on openssl for
    working with the https protocol.
    
    - Added updated version.
    - Added variants for the different licensing options.
    - Added "meta" variants for X and drawtext. These turn on/off several
      options.
    - Set variants and dependencies for many options. The defaults are based
      on the configuration settings in ffmpeg.
    - Set dependencies that were missing or that will likely get pulled in
      from the system.
    
    * libxml2
    The ffmpeg+libxml2 variant initially failed to build. The issue is that
    libxml2 sets the headers property to
    
    include_dir = self.spec.prefix.include.libxml2
    
    The ffmpeg configure looks for prefix.include and fills in the rest.
    This could probably be patched in ffmpeg but the headers property in the
    libxml2 recipe is not consistent with the environment module or the
    pkgconfig file, both of which set the headers path to prefix.include.
    This PR sets the libxml2 headers property to
    
    include_dir = self.spec.prefix.include
    
    A spot check of a few libxml2 dependents did not rreveal any problems
    with this change.

--- var/spack/repos/builtin/packages/libxml2/package.py
+++ var/spack/repos/builtin/packages/libxml2/package.py
@@ -40,7 +40,7 @@ class Libxml2(AutotoolsPackage):
 
     @property
     def headers(self):
-        include_dir = self.spec.prefix.include.libxml2
+        include_dir = self.spec.prefix.include
         hl = find_all_headers(include_dir)
         hl.directories = include_dir
         return hl
