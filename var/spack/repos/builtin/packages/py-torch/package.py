ee0cbd7a179417bbf9ff4c693bd3fd89c65a36f1
Andrew W Elble <aweits@rit.edu>
2020-04-09 04:50:08 -0400

    py-torch: Fix v1.4.0 by adding v1.4.1 (#15761)
    * py-torch: Fix v1.4.0 by adding v1.4.1
    
    version/tag hack so that 1.4.0 is still workable.
    see pytorch/pytorch#35149
    
    * delete/disable fbgemm support in 1.4.0
    
    * moved conflict

--- var/spack/repos/builtin/packages/py-torch/package.py
+++ var/spack/repos/builtin/packages/py-torch/package.py
@@ -51,7 +51,10 @@ class PyTorch(PythonPackage, CudaPackage):
     ]
 
     version('master', branch='master', submodules=True)
-    version('1.4.0', tag='v1.4.0', submodules=True)
+    version('1.4.1', tag='v1.4.1', submodules=True)
+    # see https://github.com/pytorch/pytorch/issues/35149
+    version('1.4.0', tag='v1.4.0', submodules=True,
+            submodules_delete=['third_party/fbgemm'])
     version('1.3.1', tag='v1.3.1', submodules=True)
     version('1.3.0', tag='v1.3.0', submodules=True)
     version('1.2.0', tag='v1.2.0', submodules=True)
@@ -101,6 +104,8 @@ class PyTorch(PythonPackage, CudaPackage):
     conflicts('+redis', when='@:1.0')
     conflicts('+zstd', when='@:1.0')
     conflicts('+tbb', when='@:1.1')
+    # see https://github.com/pytorch/pytorch/issues/35149
+    conflicts('+fbgemm', when='@1.4.0')
 
     cuda_arch_conflict = ('This version of Torch/Caffe2 only supports compute '
                           'capabilities ')
