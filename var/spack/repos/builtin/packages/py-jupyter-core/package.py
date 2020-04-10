c1822226993dfa41bc96824f261f724845130694
Jen Herting <jen@herting.cc>
2020-04-10 11:09:10 -0400

    [py-jupyter-core] added version 4.6.3

--- var/spack/repos/builtin/packages/py-jupyter-core/package.py
+++ var/spack/repos/builtin/packages/py-jupyter-core/package.py
@@ -12,6 +12,7 @@ class PyJupyterCore(PythonPackage):
     homepage = "http://jupyter-core.readthedocs.io/"
     url      = "https://pypi.io/packages/source/j/jupyter-core/jupyter_core-4.6.0.tar.gz"
 
+    version('4.6.3', sha256='394fd5dd787e7c8861741880bdf8a00ce39f95de5d18e579c74b882522219e7e')
     version('4.6.0', sha256='85103cee6548992780912c1a0a9ec2583a4a18f1ef79a248ec0db4446500bce3')
     version('4.4.0', sha256='ba70754aa680300306c699790128f6fbd8c306ee5927976cbe48adacf240c0b7')
     version('4.2.0', sha256='44ec837a53bebf4e937112d3f9ccf31fee4f8db3e406dd0dd4f0378a354bed9c')
