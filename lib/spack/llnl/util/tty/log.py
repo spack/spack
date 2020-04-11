0623fb7fb4e7ebbd5f52ef948d27115a83e23fd6
Todd Gamblin <tgamblin@llnl.gov>
2020-04-10 23:13:38 -0700

    refactor: move `_writer_daemon` function out of `log_output` class

--- lib/spack/llnl/util/tty/log.py
+++ lib/spack/llnl/util/tty/log.py
@@ -416,11 +416,11 @@ class log_output(object):
         self._saved_debug = tty._debug
 
         # OS-level pipe for redirecting output to logger
-        self.read_fd, self.write_fd = os.pipe()
+        read_fd, write_fd = os.pipe()
 
         # Multiprocessing pipe for communication back from the daemon
         # Currently only used to save echo value between uses
-        self.parent, self.child = multiprocessing.Pipe()
+        self.parent_pipe, child_pipe = multiprocessing.Pipe()
 
         # Sets a daemon that writes to file what it reads from a pipe
         try:
@@ -431,10 +431,15 @@ class log_output(object):
                 input_stream = None  # just don't forward input if this fails
 
             self.process = multiprocessing.Process(
-                target=self._writer_daemon, args=(input_stream,))
+                target=_writer_daemon,
+                args=(
+                    input_stream, read_fd, write_fd, self.echo, self.log_file,
+                    child_pipe
+                )
+            )
             self.process.daemon = True  # must set before start()
             self.process.start()
-            os.close(self.read_fd)  # close in the parent process
+            os.close(read_fd)  # close in the parent process
 
         finally:
             if input_stream:
@@ -456,9 +461,9 @@ class log_output(object):
             self._saved_stderr = os.dup(sys.stderr.fileno())
 
             # redirect to the pipe we created above
-            os.dup2(self.write_fd, sys.stdout.fileno())
-            os.dup2(self.write_fd, sys.stderr.fileno())
-            os.close(self.write_fd)
+            os.dup2(write_fd, sys.stdout.fileno())
+            os.dup2(write_fd, sys.stderr.fileno())
+            os.close(write_fd)
 
         else:
             # Handle I/O the Python way. This won't redirect lower-level
@@ -471,7 +476,7 @@ class log_output(object):
             self._saved_stderr = sys.stderr
 
             # create a file object for the pipe; redirect to it.
-            pipe_fd_out = os.fdopen(self.write_fd, 'w')
+            pipe_fd_out = os.fdopen(write_fd, 'w')
             sys.stdout = pipe_fd_out
             sys.stderr = pipe_fd_out
 
@@ -510,14 +515,14 @@ class log_output(object):
 
         # print log contents in parent if needed.
         if self.write_log_in_parent:
-            string = self.parent.recv()
+            string = self.parent_pipe.recv()
             self.file_like.write(string)
 
         if self.close_log_in_parent:
             self.log_file.close()
 
         # recover and store echo settings from the child before it dies
-        self.echo = self.parent.recv()
+        self.echo = self.parent_pipe.recv()
 
         # join the daemon process. The daemon will quit automatically
         # when the write pipe is closed; we just wait for it here.
@@ -548,85 +553,128 @@ class log_output(object):
             sys.stdout.write(xoff)
             sys.stdout.flush()
 
-    def _writer_daemon(self, stdin):
-        """Daemon that writes output to the log file and stdout."""
-        # Use line buffering (3rd param = 1) since Python 3 has a bug
-        # that prevents unbuffered text I/O.
-        in_pipe = os.fdopen(self.read_fd, 'r', 1)
-        os.close(self.write_fd)
 
-        echo = self.echo        # initial echo setting, user-controllable
-        force_echo = False      # parent can force echo for certain output
+def _writer_daemon(stdin, read_fd, write_fd, echo, log_file, control_pipe):
+    """Daemon used by ``log_output`` to write to a log file and to ``stdout``.
 
-        # list of streams to select from
-        istreams = [in_pipe, stdin] if stdin else [in_pipe]
+    The daemon receives output from the parent process and writes it both
+    to a log and, optionally, to ``stdout``.  The relationship looks like
+    this::
 
-        log_file = self.log_file
+        Terminal
+           |
+           |          +-------------------------+
+           |          | Parent Process          |
+           +--------> |   with log_output():    |
+           | stdin    |     ...                 |
+           |          +-------------------------+
+           |            ^             | write_fd (parent's redirected stdout)
+           |            | control     |
+           |            | pipe        |
+           |            |             v read_fd
+           |          +-------------------------+   stdout
+           |          | Writer daemon           |------------>
+           +--------> |   read from read_fd     |   log_file
+             stdin    |   write to out and log  |------------>
+                      +-------------------------+
 
-        try:
-            with keyboard_input(stdin) as kb:
-                while True:
-                    # fix the terminal settings if we recently came to
-                    # the foreground
-                    kb.check_fg_bg()
+    Within the ``log_output`` handler, the parent's output is redirected
+    to a pipe from which the daemon reads.  The daemon writes each line
+    from the pipe to a log file and (optionally) to ``stdout``.  The user
+    can hit ``v`` to toggle output on ``stdout``.
 
-                    # wait for input from any stream. use a coarse timeout to
-                    # allow other checks while we wait for input
-                    rlist, _, _ = _retry(select.select)(istreams, [], [], 1e-1)
+    In addition to the input and output file descriptors, the daemon
+    interacts with the parent via ``control_pipe``.  It reports whether
+    ``stdout`` was enabled or disabled when it finished and, if the
+    ``log_file`` is a ``StringIO`` object, then the daemon also sends the
+    logged output back to the parent as a string, to be written to the
+    ``StringIO`` in the parent. This is mainly for testing.
 
-                    # Allow user to toggle echo with 'v' key.
-                    # Currently ignores other chars.
-                    # only read stdin if we're in the foreground
-                    if stdin in rlist and not _is_background_tty(stdin):
-                        # it's possible to be backgrounded between the above
-                        # check and the read, so we ignore SIGTTIN here.
-                        with ignore_signal(signal.SIGTTIN):
-                            try:
-                                if stdin.read(1) == 'v':
-                                    echo = not echo
-                            except IOError as e:
-                                # If SIGTTIN is ignored, the system gives EIO
-                                # to let the caller know the read failed b/c it
-                                # was in the bg. Ignore that too.
-                                if e.errno != errno.EIO:
-                                    raise
+    Arguments:
+        stdin (stream): input from the terminal
+        read_fd (int): pipe for reading from parent's redirected stdout
+        write_fd (int): parent's end of the pipe will write to (will be
+            immediately closed by the writer daemon)
+        echo (bool): initial echo setting -- controlled by user and
+            preserved across multiple writer daemons
+        log_file (file-like): file to log all output
+        control_pipe (Pipe): multiprocessing pipe on which to send control
+            information to the parent
 
-                    if in_pipe in rlist:
-                        # Handle output from the calling process.
-                        line = _retry(in_pipe.readline)()
-                        if not line:
-                            break
+    """
+    # Use line buffering (3rd param = 1) since Python 3 has a bug
+    # that prevents unbuffered text I/O.
+    in_pipe = os.fdopen(read_fd, 'r', 1)
+    os.close(write_fd)
 
-                        # find control characters and strip them.
-                        controls = control.findall(line)
-                        line = control.sub('', line)
+    # list of streams to select from
+    istreams = [in_pipe, stdin] if stdin else [in_pipe]
+    force_echo = False      # parent can force echo for certain output
 
-                        # Echo to stdout if requested or forced.
-                        if echo or force_echo:
-                            sys.stdout.write(line)
-                            sys.stdout.flush()
+    try:
+        with keyboard_input(stdin) as kb:
+            while True:
+                # fix the terminal settings if we recently came to
+                # the foreground
+                kb.check_fg_bg()
 
-                        # Stripped output to log file.
-                        log_file.write(_strip(line))
-                        log_file.flush()
+                # wait for input from any stream. use a coarse timeout to
+                # allow other checks while we wait for input
+                rlist, _, _ = _retry(select.select)(istreams, [], [], 1e-1)
 
-                        if xon in controls:
-                            force_echo = True
-                        if xoff in controls:
-                            force_echo = False
+                # Allow user to toggle echo with 'v' key.
+                # Currently ignores other chars.
+                # only read stdin if we're in the foreground
+                if stdin in rlist and not _is_background_tty(stdin):
+                    # it's possible to be backgrounded between the above
+                    # check and the read, so we ignore SIGTTIN here.
+                    with ignore_signal(signal.SIGTTIN):
+                        try:
+                            if stdin.read(1) == 'v':
+                                echo = not echo
+                        except IOError as e:
+                            # If SIGTTIN is ignored, the system gives EIO
+                            # to let the caller know the read failed b/c it
+                            # was in the bg. Ignore that too.
+                            if e.errno != errno.EIO:
+                                raise
 
-        except BaseException:
-            tty.error("Exception occurred in writer daemon!")
-            traceback.print_exc()
+                if in_pipe in rlist:
+                    # Handle output from the calling process.
+                    line = _retry(in_pipe.readline)()
+                    if not line:
+                        break
 
-        finally:
-            # send written data back to parent if we used a StringIO
-            if self.write_log_in_parent:
-                self.child.send(log_file.getvalue())
-            log_file.close()
+                    # find control characters and strip them.
+                    controls = control.findall(line)
+                    line = control.sub('', line)
 
-        # send echo value back to the parent so it can be preserved.
-        self.child.send(echo)
+                    # Echo to stdout if requested or forced.
+                    if echo or force_echo:
+                        sys.stdout.write(line)
+                        sys.stdout.flush()
+
+                    # Stripped output to log file.
+                    log_file.write(_strip(line))
+                    log_file.flush()
+
+                    if xon in controls:
+                        force_echo = True
+                    if xoff in controls:
+                        force_echo = False
+
+    except BaseException:
+        tty.error("Exception occurred in writer daemon!")
+        traceback.print_exc()
+
+    finally:
+        # send written data back to parent if we used a StringIO
+        if isinstance(log_file, StringIO):
+            control_pipe.send(log_file.getvalue())
+        log_file.close()
+
+    # send echo value back to the parent so it can be preserved.
+    control_pipe.send(echo)
 
 
 def _retry(function):
