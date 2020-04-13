0c7097e9e4953098b2b6d263b158becc646d3d26
Antonio Arena <antonio.arena@kaust.edu.sa>
2020-04-13 16:10:43 +0300

    Create VMD recipe
    This is a new recipe to install VMD on Spack-managed hosts.

---
+++ vmd/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+import os
+
+
+class Vmd(AutotoolsPackage):
+    """VMD provides user-editable materials which can be applied to molecular geometry.
+    These material properties control the details of how VMD shades the molecular geometry,
+    and how transparent or opaque the displayed molecular geometry is. With this feature,
+    one can easily create nice looking transparent surfaces which allow inner structural
+    details to be seen within a large molecular structure. The material controls can be
+    particularly helpful when rendering molecular scenes using external ray tracers, each of
+    which typically differ slightly.
+    """
+
+    homepage = "https://www.ks.uiuc.edu/Research/vmd/"
+    version('1.9.3', sha256='145b4d0cc10b56cadeb71e16c54ab8be713e268f11491714cd617422758ec643',
+            url='file://{0}/vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz'.format(os.getcwd()))
+
+    build_directory = 'src'
+    build_targets = ['install']
+
+    def configure(self, spec, prefix):
+        os.environ['VMDINSTALLBINDIR'] = prefix.bin
+        os.environ['VMDINSTALLLIBRARYDIR'] = prefix.lib64
+
+        configure = Executable('./configure')
+        configure('LINUXAMD64')
+
+    def setup_run_environment(self, env):
+        env.set('PLUGINDIR', self.spec.prefix.lib64.plugins)
