fa265e50dc14b16152636e7c9e14dfa09f84556c
Harmen Stoppels <harmenstoppels@gmail.com>
2020-04-14 15:40:18 +0200

    Fix linter issues

--- var/spack/repos/builtin/packages/libtree/package.py
+++ var/spack/repos/builtin/packages/libtree/package.py
@@ -5,8 +5,10 @@
 
 from spack import *
 
+
 class Libtree(CMakePackage):
-    """ldd as a tree with an option to bundle dependencies into a single folder"""
+    """ldd as a tree with an option to bundle dependencies into a
+       single folder"""
 
     homepage = "https://github.com/haampie/libtree"
     url      = "https://github.com/haampie/libtree/releases/download/v1.0.3/sources.tar.gz"
@@ -14,4 +16,3 @@ class Libtree(CMakePackage):
     maintainers = ['haampie']
 
     version('1.0.3', sha256='67ce886c191d50959a5727246cdb04af38872cd811c9ed4e3822f77a8f40b20b')
-
