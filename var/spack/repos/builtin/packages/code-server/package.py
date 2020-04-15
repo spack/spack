784c267564d93fce21647ad790055c105a2e3881
Jen Herting <jen@herting.cc>
2020-04-15 13:50:42 -0400

    [code-server] removed fixmes

--- var/spack/repos/builtin/packages/code-server/package.py
+++ var/spack/repos/builtin/packages/code-server/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install code-server
-#
-# You can edit this file again by typing:
-#
-#     spack edit code-server
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -29,10 +12,6 @@ class CodeServer(Package):
 
     homepage = "https://github.com/cdr/code-server"
     url      = "https://github.com/cdr/code-server/releases/download/3.1.0/code-server-3.1.0-linux-x86_64.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('3.1.0', sha256='5ef85c8f280ce781a176a8b77386b333efe892755a5c325a1782e4eac6016e59')
 
