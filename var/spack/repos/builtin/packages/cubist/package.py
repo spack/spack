13a993db44dae65908d6cf90119359214ade4779
asmaahassan90 <31959389+asmaahassan90@users.noreply.github.com>
2020-04-15 14:49:12 +0200

    enhance recipe

--- var/spack/repos/builtin/packages/cubist/package.py
+++ var/spack/repos/builtin/packages/cubist/package.py
@@ -11,7 +11,9 @@ import os
 class Cubist(Package):
     """Cubist is a powerful tool for generating rule-based models that
     balance the need for accurate prediction against the requirements of
-    intelligibility. Cubist models generally give better results than those
+    intelligibility.
+    
+    Cubist models generally give better results than those
     produced by simple techniques such as multivariate linear regression,
     while also being easier to understand than neural networks."""
 
@@ -21,7 +23,7 @@ class Cubist(Package):
     version('2.07', 'f2b20807cd3275e775c42263a4efd3f50df6e495a8b6dc8989ea2d41b973ac1a')
 
     def install(self, spec, prefix):
-        os.system("make")
+        make()
         mkdirp(self.prefix.bin)
         install('cubist', prefix.bin)
         install('summary', prefix.bin)
