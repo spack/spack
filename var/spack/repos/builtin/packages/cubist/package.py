a6888d34fa515a71cf4ca8d35a6b5d15a60ec056
asmaahassan90 <31959389+asmaahassan90@users.noreply.github.com>
2020-04-15 13:49:20 +0200

    Add Cubist

---
+++ var/spack/repos/builtin/packages/cubist/package.py
@@ -0,0 +1,28 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+import os
+
+
+class Cubist(Package):
+    """Cubist is a powerful tool for generating rule-based models that
+    balance the need for accurate prediction against the requirements of
+    intelligibility. Cubist models generally give better results than those
+    produced by simple techniques such as multivariate linear regression,
+    while also being easier to understand than neural networks."""
+
+    homepage = "https://www.rulequest.com"
+    url      = "https://www.rulequest.com/GPL/Cubist.tgz"
+
+    version('2.07', 'f2b20807cd3275e775c42263a4efd3f50df6e495a8b6dc8989ea2d41b973ac1a')
+
+    def install(self, spec, prefix):
+        os.system("make")
+        mkdirp(self.prefix.bin)
+        install('cubist', prefix.bin)
+        install('summary', prefix.bin)
+        install('xval', prefix.bin)
