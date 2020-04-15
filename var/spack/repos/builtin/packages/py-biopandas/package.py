35bfa813efc15b2631324b5433ed505fdafe6385
ilbiondo <61497728+ilbiondo@users.noreply.github.com>
2020-04-15 18:55:53 +0200

    Update var/spack/repos/builtin/packages/py-biopandas/package.py
    Co-Authored-By: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-biopandas/package.py
+++ var/spack/repos/builtin/packages/py-biopandas/package.py
@@ -21,5 +21,5 @@ class PyBiopandas(PythonPackage):
 
     depends_on('python@3.5:',       type=('build', 'run'))
     depends_on('py-pandas@0.24.2:', type=('build', 'run'))
-    depends_on('py-numpy@1.11.2:',  type=('build', 'run'))
+    depends_on('py-numpy@1.16.2:',  type=('build', 'run'))
     depends_on('py-scipy@0.18.1:',  type=('build', 'run'))
