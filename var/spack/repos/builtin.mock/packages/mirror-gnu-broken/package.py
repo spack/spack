03c18dbdc4b38625979c84e8468eb2a2832b1d7d
Axel Huebl <axel.huebl@plasma.ninja>
2020-04-15 10:18:17 -0700

    Tests: Mirror Mixin Classes

---
+++ var/spack/repos/builtin.mock/packages/mirror-gnu-broken/package.py
@@ -0,0 +1,15 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class MirrorGnuBroken(AutotoolsPackage, GNUMirrorPackage):
+    """Simple GNU package"""
+
+    homepage = "https://www.gnu.org/software/make/"
+    url      = "https://ftpmirror.gnu.org/make/make-4.2.1.tar.gz"
+
+    version('4.2.1', sha256='e40b8f018c1da64edd1cc9a6fce5fa63b2e707e404e20cad91fbae337c98a5b7')
