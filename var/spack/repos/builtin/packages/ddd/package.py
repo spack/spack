8f5d028eb282414ae054f6a00963a4a46f88ed88
Daryl W. Grunau <dwg@lanl.gov>
2020-04-16 13:01:13 -0600

    use the upstream patch

--- var/spack/repos/builtin/packages/ddd/package.py
+++ var/spack/repos/builtin/packages/ddd/package.py
@@ -24,8 +24,9 @@ class Ddd(AutotoolsPackage, GNUMirrorPackage):
     depends_on('lesstif@0.89:')
 
     # Patch to fix hangs due to injection of bogus GDB init settings:
-    #     https://stackoverflow.com/questions/2914003/ddd-hangs-on-start
-    patch('extended-prompt.patch')
+    #     https://savannah.gnu.org/bugs/index.php?58191
+    patch('https://savannah.gnu.org/bugs/download.php?file_id=48852',
+          sha256='8faf986f1dbe529c377f8683deaa8dc2b3b08fe7c297214c9e03860d5c4a1aab')
 
     # Needed for OSX 10.9 DP6 build failure:
     #     https://savannah.gnu.org/patch/?8178
