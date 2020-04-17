81a154de4ac56478d5db4c83fca15f5b8db63e49
Harmen Stoppels <harmenstoppels@gmail.com>
2020-04-17 10:30:05 +0200

    libtree: added v1.1.2 (#16119)

--- var/spack/repos/builtin/packages/libtree/package.py
+++ var/spack/repos/builtin/packages/libtree/package.py
@@ -15,6 +15,7 @@ class Libtree(CMakePackage):
 
     maintainers = ['haampie']
 
+    version('1.1.2', sha256='31641c6bf6c2980ffa7b4c57392460434f97ba66fe51fe6346867430b33a0374')
     version('1.1.1', sha256='3e8543145a40a94e9e2ce9fed003d2bf68294e1fce9607028a286bc132e17dc4')
     version('1.1.0', sha256='6cf36fb9a4c8c3af01855527d4931110732bb2d1c19be9334c689f1fd1c78536')
     version('1.0.4', sha256='b15a54b6f388b8bd8636e288fcb581029f1e65353660387b0096a554ad8e9e45')
