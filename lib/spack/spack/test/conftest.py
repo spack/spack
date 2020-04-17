d31be689b09a284669ac131dddabd3bee14ad4e6
Peter Josef Scheibel <scheibel1@llnl.gov>
2020-04-17 15:51:10 -0700

    remove clear_caches since 'config' fixture would need to be refactored to initialize scopes every time it is used if I do that

--- lib/spack/spack/test/conftest.py
+++ lib/spack/spack/test/conftest.py
@@ -442,7 +442,11 @@ def mutable_config(tmpdir_factory, configuration_dir):
     with use_configuration(cfg):
         yield cfg
 
-    spack.config.config.clear_caches()
+    # TODO: this should be done, but it would actually undo the one-time setup
+    # that occurs for the configuration_dir fixture and interferes with tests
+    # using the 'config' fixture. To allow this, the 'config' fixture needs
+    # to be refactored to reassemble the scopes into a config every time.
+    # spack.config.config.clear_caches()
 
 
 @pytest.fixture()
