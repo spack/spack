463819f6c787c0d69a410f255dd059d396936595
darmac <xiaojun2@hisilicon.com>
2020-04-19 03:29:56 +0800

    refine byacc info

--- var/spack/repos/builtin/packages/byacc/package.py
+++ var/spack/repos/builtin/packages/byacc/package.py
@@ -7,11 +7,11 @@ from spack import *
 
 
 class Byacc(AutotoolsPackage):
-    """Berkeley Yacc is an LALR(1) parser generator.  Berkeley Yacc has been made
-    as compatible as possible with AT&T Yacc.  Berkeley Yacc can accept any input
-    specification that conforms to the AT&T Yacc documentation.  Specifications
-    that take advantage of undocumented features of AT&T Yacc will probably be
-    rejected."""
+    """Berkeley Yacc is an LALR(1) parser generator.  Berkeley Yacc has
+    been made as compatible as possible with AT&T Yacc.  Berkeley Yacc
+    can accept any input specification that conforms to the AT&T Yacc
+    documentation. Specifications that take advantage of undocumented
+    features of AT&T Yacc will probably be rejected."""
 
     homepage = "https://github.com/grandseiken/byacc"
     git      = "https://github.com/grandseiken/byacc.git"
