e8a7e4f5ecd0c1a6dcd9f0769f16f5ad2569dff5
darmac <xiaojun2@hisilicon.com>
2020-04-21 05:23:47 +0800

    Add new package: bgpdump (#16122)

---
+++ var/spack/repos/builtin/packages/bgpdump/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Bgpdump(AutotoolsPackage):
+    """Utility and C Library for parsing MRT files"""
+
+    homepage = "https://github.com/RIPE-NCC/bgpdump/wiki"
+    git      = "https://github.com/RIPE-NCC/bgpdump.git"
+
+    version('master', branch='master')
+
+    depends_on('m4',       type='build')
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool',  type='build')
+    depends_on('bzip2')
