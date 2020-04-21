f7c602a3bd23e1c7bf19e02cf13d5149b0cbdba3
Joseph Ciurej <ciurej1@llnl.gov>
2020-04-21 16:26:32 -0700

    openscenegraph: add @3.4.0/3.4.1, update versioning to use git, improve dependency handling (esp. for ffmpeg, opengl)

--- var/spack/repos/builtin/packages/openscenegraph/package.py
+++ var/spack/repos/builtin/packages/openscenegraph/package.py
@@ -12,30 +12,48 @@ class Openscenegraph(CMakePackage):
        that's used in a variety of visual simulation applications."""
 
     homepage = "http://www.openscenegraph.org"
-    url      = "https://github.com/openscenegraph/OpenSceneGraph/archive/OpenSceneGraph-3.6.4.tar.gz"
+    git_url  = "https://github.com/openscenegraph/OpenSceneGraph.git"
 
-    version('3.6.5', sha256='aea196550f02974d6d09291c5d83b51ca6a03b3767e234a8c0e21322927d1e12')
-    version('3.6.4', sha256='81394d1b484c631028b85d21c5535280c21bbd911cb058e8746c87e93e7b9d33')
-    version('3.2.3', sha256='a1ecc6524197024834e1277916922b32f30246cb583e27ed19bf3bf889534362')
-    version('3.1.5', sha256='dddecf2b33302076712100af59b880e7647bc595a9a7cc99186e98d6e0eaeb5c')
+    version('3.6.5', git=git_url, tag='OpenSceneGraph-3.6.5')
+    version('3.6.4', git=git_url, tag='OpenSceneGraph-3.6.4')
+    version('3.4.1', git=git_url, tag='OpenSceneGraph-3.4.1')
+    version('3.4.0', git=git_url, tag='OpenSceneGraph-3.4.0')
+    version('3.2.3', git=git_url, tag='OpenSceneGraph-3.2.3')
+    version('3.1.5', git=git_url, tag='OpenSceneGraph-3.1.5')
 
     variant('shared', default=True, description='Builds a shared version of the library')
 
     depends_on('cmake@2.8.7:', type='build')
+    depends_on('qt+opengl')
     depends_on('qt@4:', when='@3.2:')
     depends_on('qt@:4', when='@:3.1')
+    # TODO: Since 'ffmpeg' constantly changes its API and OSG must change in response,
+    # the version of the dependency here matters greatly on the version and patch of
+    # the release of OSG that's being used.
+    depends_on('ffmpeg@1:2+avresample')
     depends_on('zlib')
     depends_on('libxinerama')
     depends_on('libxrandr')
+
+    patch('glibc-jasper.patch', when='@3.4.0:3.4.999%gcc')
 
     def cmake_args(self):
         spec = self.spec
 
         shared_status = 'ON' if '+shared' in spec else 'OFF'
+        opengl_profile = 'GL{0}'.format(spec['gl'].version.up_to(1))
 
         args = [
+            # Library Options #
+            '-DZLIB_INCLUDE_DIR={0}'.format(spec['zlib'].prefix.include),
+            '-DZLIB_LIBRARY={0}/libz.{1}'.format(spec['zlib'].prefix.lib,
+                                                 dso_suffix),
+            '-DFFMPEG_ROOT={0}'.format(spec['ffmpeg'].prefix),
+            # Variant Options #
             '-DDYNAMIC_OPENSCENEGRAPH={0}'.format(shared_status),
             '-DDYNAMIC_OPENTHREADS={0}'.format(shared_status),
+            '-DOPENGL_PROFILE={0}'.format(opengl_profile),
+            # General Options #
             '-DBUILD_OSG_APPLICATIONS=OFF',
             '-DOSG_NOTIFY_DISABLED=ON',
             '-DLIB_POSTFIX=',
@@ -48,6 +66,14 @@ class Openscenegraph(CMakePackage):
                                                      dso_suffix),
             ])
 
+        # NOTE: Specifying only the root seems to be sufficient, and specifying
+        # the locations explicity causes OSG to be buggy.
+        for ffmpeg_lib in ['libavcodec', 'libavformat', 'libavutil']:
+            args.extend([
+                '-DFFMPEG_{0}_INCLUDE_DIRS='.format(ffmpeg_lib.upper()),
+                '-DFFMPEG_{0}_LIBRARIES='.format(ffmpeg_lib.upper()),
+            ])
+
         # NOTE: This is necessary in order to allow OpenSceneGraph to compile
         # despite containing a number of implicit bool to int conversions.
         if spec.satisfies('%gcc'):
@@ -55,5 +81,8 @@ class Openscenegraph(CMakePackage):
                 '-DCMAKE_C_FLAGS=-fpermissive',
                 '-DCMAKE_CXX_FLAGS=-fpermissive',
             ])
+
+        if spec.satisfies('@3.4:'):
+            args.extend(['-DBUILD_OSG_FRAMEWORKS=OFF'])
 
         return args
