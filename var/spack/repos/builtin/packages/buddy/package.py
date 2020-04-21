d1db50ac0ebf56cdc4df55067447a44c54499ac3
darmac <xiaojun2@hisilicon.com>
2020-04-22 01:28:04 +0800

    refile list_url and remove autotools depends

--- var/spack/repos/builtin/packages/buddy/package.py
+++ var/spack/repos/builtin/packages/buddy/package.py
@@ -12,15 +12,10 @@ class Buddy(AutotoolsPackage):
 
     homepage = "https://sourceforge.net/projects/buddy/"
     url      = "https://sourceforge.net/projects/buddy/files/buddy/BuDDy%202.4/buddy-2.4.tar.gz"
-    list_url = url
+    list_url = "https://sourceforge.net/projects/buddy/files/buddy"
     list_depth = 1
 
     version('2.4', sha256='d3df80a6a669d9ae408cb46012ff17bd33d855529d20f3a7e563d0d913358836')
-
-    depends_on('m4',       type='build')
-    depends_on('autoconf', type='build')
-    depends_on('automake', type='build')
-    depends_on('libtool',  type='build')
 
     def configure_args(self):
         if platform.machine() == 'aarch64':
