dbc9212332dd31b01c72f0bfd88a7f33b46b5196
Greg Becker <becker33@llnl.gov>
2020-04-21 11:31:45 -0700

    Features/spack test subcommands (#16054)
    * spack test: subcommands for asynchronous tests
    
    * commands are `run`, `list`, `status`, `results`, `remove`.

--- lib/spack/spack/cmd/test.py
+++ lib/spack/spack/cmd/test.py
@@ -8,8 +8,12 @@ import os
 import argparse
 import textwrap
 import datetime
+import fnmatch
+import re
+import shutil
 
 import llnl.util.tty as tty
+import llnl.util.filesystem as fs
 
 import spack.environment as ev
 import spack.cmd
@@ -23,27 +27,40 @@ level = "long"
 
 
 def setup_parser(subparser):
-    subparser.add_argument('--keep-stage', action='store_true',
-                           help='Keep testing directory for debugging')
-    subparser.add_argument(
+    sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='test_command')
+
+    # Run
+    run_parser = sp.add_parser('run', help=test_run.__doc__)
+
+    name_help_msg = "Name the test for subsequent access."
+    name_help_msg += " Default is the timestamp of the run formatted"
+    name_help_msg += " 'YYYY-MM-DD_HH:MM:SS'"
+    run_parser.add_argument('-n', '--name', help=name_help_msg)
+
+    run_parser.add_argument(
+        '--keep-stage',
+        action='store_true',
+        help='Keep testing directory for debugging'
+    )
+    run_parser.add_argument(
         '--log-format',
         default=None,
         choices=spack.report.valid_formats,
         help="format to be used for log files"
     )
-    subparser.add_argument(
+    run_parser.add_argument(
         '--log-file',
         default=None,
         help="filename for the log file. if not passed a default will be used"
     )
-    arguments.add_cdash_args(subparser, False)
-    subparser.add_argument(
+    arguments.add_cdash_args(run_parser, False)
+    run_parser.add_argument(
         '--help-cdash',
         action='store_true',
         help="Show usage instructions for CDash reporting"
     )
 
-    length_group = subparser.add_mutually_exclusive_group()
+    length_group = run_parser.add_mutually_exclusive_group()
     length_group.add_argument(
         '--smoke', action='store_true', dest='smoke_test', default=True,
         help='run smoke tests (default)')
@@ -51,17 +68,37 @@ def setup_parser(subparser):
         '--capability', action='store_false', dest='smoke_test', default=True,
         help='run full capability tests using pavilion')
 
-    cd_group = subparser.add_mutually_exclusive_group()
+    cd_group = run_parser.add_mutually_exclusive_group()
     arguments.add_common_arguments(cd_group, ['clean', 'dirty'])
 
-    arguments.add_common_arguments(subparser, ['installed_specs'])
+    arguments.add_common_arguments(run_parser, ['installed_specs'])
 
+    # List
+    list_parser = sp.add_parser('list', help=test_list.__doc__)
+    list_parser.add_argument(
+        'filter', nargs=argparse.REMAINDER,
+        help='optional case-insensitive glob patterns to filter results.')
 
-def test(parser, args):
-    # record test time
-    now = datetime.datetime.now()
-    tty.msg("Spack test at %s" % now.strftime('%Y-%m-%d_%H:%M:%S'))
+    # Status
+    status_parser = sp.add_parser('status', help=test_status.__doc__)
+    status_parser.add_argument('name', help="Test for which to provide status")
 
+    # Results
+    results_parser = sp.add_parser('results', help=test_results.__doc__)
+    results_parser.add_argument('name', help="Test for which to print results")
+
+    # Remove
+    remove_parser = sp.add_parser('remove', help=test_remove.__doc__)
+    remove_parser.add_argument(
+        'name', nargs='?',
+        help="Test to remove from test stage")
+
+def test_run(args):
+    """Run tests for the specified installed packages
+
+    If no specs are listed, run tests for all packages in the current
+    environment or all installed packages if there is no active environment.
+    """
     # cdash help option
     if args.help_cdash:
         parser = argparse.ArgumentParser(
@@ -74,6 +111,11 @@ environment variables:
         arguments.add_cdash_args(parser, True)
         parser.print_help()
         return
+
+    # record test time; this will be default test name
+    now = datetime.datetime.now()
+    test_name = args.name or now.strftime('%Y-%m-%d_%H:%M:%S')
+    tty.msg("Spack test %s" % test_name)
 
     # Get specs to test
     env = ev.get_env(args, 'test')
@@ -101,26 +143,127 @@ environment variables:
         else:
             log_file = os.path.join(
                 os.getcwd(),
-                'test-%s' % now.strftime('%Y-%m-%d_%H:%M:%S'))
+                'test-%s' % test_name)
         reporter.filename = log_file
     reporter.specs = specs_to_test
 
     # test_stage_dir
-    test_name = now.strftime('%Y-%m-%d_%H:%M:%S')
-    stage = os.path.join(
-        spack.util.path.canonicalize_path(
-            spack.config.get('config:test_stage', os.getcwd())),
-        test_name)
+    stage = _get_stage(test_name)
+    fs.mkdirp(stage)
 
     with reporter('test', stage):
         if args.smoke_test:
             for spec in specs_to_test:
                 try:
                     spec.package.do_test(
-                        time=now,
+                        name=test_name,
                         remove_directory=not args.keep_stage,
                         dirty=args.dirty)
+                    with open(_get_results_file(test_name), 'a') as f:
+                        f.write("%s PASSED\n" % spec.format("{name}-{hash:7}"))
                 except BaseException:
-                    pass  # Test is logged, go on to other tests
+                    with open(_get_results_file(test_name), 'a') as f:
+                        f.write("%s FAILED\n" % spec.format("{name}-{hash:7}"))
         else:
             raise NotImplementedError
+
+
+def test_list(args):
+    """List tests that are running or have available results."""
+    stage_dir = _get_stage()
+    tests = os.listdir(stage_dir)
+
+    # Filter tests by filter argument
+    if args.filter:
+        def create_filter(f):
+            raw = fnmatch.translate('f' if '*' in f or '?' in f
+                                    else '*' + f + '*')
+            return re.compile(raw, flags=re.IGNORECASE)
+        filters = [create_filter(f) for f in args.filter]
+
+        def match(t, f):
+            return f.match(t)
+        tests = [t for t in tests if any(match(t, f) for f in filters)]
+
+    if tests:
+        # TODO: Make these specify results vs active
+        msg = "Spack test results available for the following tests:\n"
+        msg += "        %s\n" % ' '.join(tests)
+        msg += "    Run `spack test remove` to remove all tests"
+        tty.msg(msg)
+    else:
+        msg = "No test results match the query\n"
+        msg += "        Tests may have been removed using `spack test remove`"
+        tty.msg(msg)
+
+
+def test_status(args):
+    """Get the current status for a particular Spack test."""
+    name = args.name
+    stage = _get_stage(name)
+
+    if os.path.exists(stage):
+        # TODO: Make this handle capability tests too
+        tty.msg("Test %s completed" % name)
+    else:
+        tty.msg("Test %s no longer available" % name)
+
+
+def test_results(args):
+    """Get the results for a particular Spack test."""
+    name = args.name
+    stage = _get_stage(name)
+
+    # TODO: Make this handle capability tests too
+    # The results file may turn out to be a placeholder for future work
+    if os.path.exists(stage):
+        results_file = _get_results_file(name)
+        if os.path.exists(results_file):
+            msg = "Results for test %s: \n" % name
+            with open(results_file, 'r') as f:
+                lines = f.readlines()
+            for line in lines:
+                msg += "        %s" % line
+            tty.msg(msg)
+        else:
+            msg = "Test %s has no results.\n" % name
+            msg += "        Check if it is active with "
+            msg += "`spack test status %s`" % name
+            tty.msg(msg)
+    else:
+        tty.msg("No test %s found in test stage" % name)
+
+
+def test_remove(args):
+    """Remove results for a test from the test stage.
+
+    If no test is listed, remove all tests from the test stage.
+
+    Removed tests can no longer be accessed for results or status, and will not
+    appear in `spack test list` results."""
+    stage_dir = _get_stage(args.name)
+    if args.name:
+        shutil.rmtree(stage_dir)
+    else:
+        fs.remove_directory_contents(stage_dir)
+
+
+def test(parser, args):
+    globals()['test_%s' % args.test_command](args)
+
+
+def _get_stage(name=None):
+    """
+    Return the test stage for the named test or the overall test stage.
+    """
+    stage_dir = spack.util.path.canonicalize_path(
+        spack.config.get('config:test_stage', '~/.spack/test'))
+    return os.path.join(stage_dir, name) if name else stage_dir
+
+
+def _get_results_file(name):
+    """
+    Return the results file for the named test.
+    """
+    stage = _get_stage(name)
+    return os.path.join(stage, 'results.txt')
