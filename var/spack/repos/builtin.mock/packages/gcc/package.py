4d361c8f2e20ebd6d9999567432e7b5ffc9d1a95
Gregory Becker <becker33@llnl.gov>
2020-04-22 12:00:38 -0700

    add regression test

---
+++ var/spack/repos/builtin.mock/packages/gcc/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Gcc(Package):
+    """Simple compiler package."""
+
+    homepage = "http://www.example.com"
+    url      = "http://www.example.com/gcc-1.0.tar.gz"
+
+    version('1.0', '0123456789abcdef0123456789abcdef')
+    version('2.0', '2.0_a_hash')
+
+    def install(self, spec, prefix):
+        # Create the minimal compiler that will fool `spack compiler find`
+        mkdirp(prefix.bin)
+        with open(prefix.bin.gcc, 'w') as f:
+            f.write('#!/bin/bash\necho "%s"' % str(spec.version))
+        set_executable(prefix.bin.gcc)
