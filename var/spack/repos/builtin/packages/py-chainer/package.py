5af34918fb633e9be2fd4f4218dd6c06fbf456ce
Toyohisa Kameyama <kameyama@riken.jp>
2020-04-23 13:40:28 +0900

    1. Add maintainers. 2. fix dependency. 3. Add typing-extention@3.6.6 and fix version dependency.

--- var/spack/repos/builtin/packages/py-chainer/package.py
+++ var/spack/repos/builtin/packages/py-chainer/package.py
@@ -22,14 +22,21 @@ class PyChainer(PythonPackage):
     homepage = "https://chainer.org/"
     url      = "https://github.com/chainer/chainer/archive/v7.2.0.tar.gz"
 
+    maintainers = ['adamjstewart']
+
     version('7.2.0', sha256='6e2fba648cc5b8a5421e494385b76fe5ec154f1028a1c5908557f5d16c04f0b3')
     version('6.7.0', sha256='87cb3378a35e7c5c695028ec91d58dc062356bc91412384ea939d71374610389')
 
-    depends_on('python@3.5:', when='@7:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
-    depends_on('py-numpy@1.9:', type='run')
-    depends_on('py-six', type='run')
-    depends_on('py-typing-extensions', type='run')
-    depends_on('py-filelock', type='run')
-    # for pkg_resouces
-    depends_on('py-pip', type='run')
+    depends_on('python@3.5.1:', when='@7:', type=('build', 'run'))
+    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('py-numpy@1.9:', type=('build', 'run'))
+    depends_on('py-six@1.9.0:', type=('build', 'run'))
+    depends_on('py-typing-extensions', type=('build', 'run'))
+    depends_on(
+        'py-typing-extensions@:3.6.6', when='@:6', type=('build', 'run')
+    )
+    depends_on('py-filelock', type=('build', 'run'))
+    depends_on('py-protobuf@3:', type=('build', 'run'))
+    depends_on(
+        'py-typing@:3.6.6', when='@:6 ^python@:3.6', type=('build', 'run')
+    )
