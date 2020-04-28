cf31472e5a34724ee3d20799b1705af4750d1eaa
darmac <xiaojun2@hisilicon.com>
2020-04-28 19:04:31 +0800

    cassandra: added v3.11.6 and dependency on Java (#16341)

--- var/spack/repos/builtin/packages/cassandra/package.py
+++ var/spack/repos/builtin/packages/cassandra/package.py
@@ -17,7 +17,10 @@ class Cassandra(Package):
 
     version('4.0-alpha2', sha256='6a8e99d8bc51efd500981c85c6aa547387b2fdbedecd692308f4632dbc1de3ba')
     version('4.0-alpha1', sha256='2fdf5e3d6c03a29d24a09cd52bb17575e5faccdc4c75a07edd63a9bf4f740105')
-    version('3.11.5',     sha256='0ee3da12a2be86d7e03203fcc56c3589ddb38347b9cd031495a2b7fcf639fea6', preferred=True)
+    version('3.11.6',     sha256='ce34edebd1b6bb35216ae97bd06d3efc338c05b273b78267556a99f85d30e45b', preferred=True)
+    version('3.11.5',     sha256='0ee3da12a2be86d7e03203fcc56c3589ddb38347b9cd031495a2b7fcf639fea6')
+
+    depends_on('java', type=('build', 'run'))
 
     def install(self, spec, prefix):
         install_tree('.', prefix)
