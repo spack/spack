74272854907045b69388848f9edaf0be67fe7e23
Andrew Gaspar <agaspar@lanl.gov>
2020-04-27 23:08:57 -0600

    Only require llvm in bat for the newest version

--- var/spack/repos/builtin/packages/bat/package.py
+++ var/spack/repos/builtin/packages/bat/package.py
@@ -13,9 +13,11 @@ class Bat(CargoPackage):
     crates_io = "bat"
     git = "https://github.com/sharkdp/bat.git"
 
+    maintainers = ['AndrewGaspar']
+
     # onig-sys needs llvm for libclang in order to generate Rust code for onig
     # headers.
-    depends_on('llvm', type='build')
+    depends_on('llvm@6.0:', type='build', when='@0.13.0:')
     depends_on('oniguruma')
     depends_on('libgit2')
 
@@ -28,9 +30,10 @@ class Bat(CargoPackage):
     def setup_build_environment(self, env):
         # onig-sys environment
         env.append_flags('RUSTONIG_DYNAMIC_LIBONIG', '1')
-        env.append_flags(
-            'LLVM_CONFIG_PATH',
-            join_path(self.spec['llvm'].prefix.bin, 'llvm-config'))
+        if '^llvm' in self.spec:
+            env.append_flags(
+                'LLVM_CONFIG_PATH',
+                join_path(self.spec['llvm'].prefix.bin, 'llvm-config'))
 
         # git-sys environment
         env.append_flags('LIBGIT2_SYS_USE_PKG_CONFIG', '1')
