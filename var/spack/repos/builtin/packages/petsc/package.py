2667567798cf74129f8914a325a171a05c37e0d9
Jeffrey Salmond <js947@cam.ac.uk>
2020-04-28 15:58:53 +0100

    flake8 fixes

--- var/spack/repos/builtin/packages/petsc/package.py
+++ var/spack/repos/builtin/packages/petsc/package.py
@@ -300,9 +300,10 @@ class Petsc(Package):
                         'mumps', 'trilinos', 'fftw', 'valgrind'):
             options.append(
                 '--with-{library}={value}'.format(
-                    library=library, value=('1' if '+'+library in spec else '0'))
+                    library=library,
+                    value=('1' if '+' + library in spec else '0'))
             )
-            if '+'+library in spec:
+            if '+' + library in spec:
                 options.append(
                     '--with-{library}-dir={path}'.format(
                         library=library, path=spec[library].prefix)
