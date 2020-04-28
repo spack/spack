76e9171d25fcbdcf9be11d768340dd83f44f6a9b
darmac <xiaojun2@hisilicon.com>
2020-04-28 16:59:52 +0800

    colordiff: modified url to fix 404 error (#16349)

--- var/spack/repos/builtin/packages/colordiff/package.py
+++ var/spack/repos/builtin/packages/colordiff/package.py
@@ -10,7 +10,7 @@ class Colordiff(Package):
     """Colorful diff utility."""
 
     homepage = "https://www.colordiff.org"
-    url      = "https://www.colordiff.org/colordiff-1.0.18.tar.gz"
+    url      = "https://www.colordiff.org/archive/colordiff-1.0.18.tar.gz"
 
     version('1.0.18', sha256='29cfecd8854d6e19c96182ee13706b84622d7b256077df19fbd6a5452c30d6e0')
 
