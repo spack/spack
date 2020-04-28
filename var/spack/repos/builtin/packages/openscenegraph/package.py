06625cf7c3af96c1c9fccf66f58fc8221f8d26bc
Joseph Ciurej <ciurej1@llnl.gov>
2020-04-28 10:04:42 -0700

    openscenegraph: add '+ffmpeg' variant, add 'jasper' dependency, clean up cmake config variables

--- var/spack/repos/builtin/packages/openscenegraph/package.py
+++ var/spack/repos/builtin/packages/openscenegraph/package.py
@@ -3,7 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
 from spack import *
 
 
@@ -22,20 +21,22 @@ class Openscenegraph(CMakePackage):
     version('3.1.5', git=git_url, tag='OpenSceneGraph-3.1.5')
 
     variant('shared', default=True, description='Builds a shared version of the library')
+    variant('ffmpeg', default=False, description='Builds ffmpeg plugin for audio encoding/decoding')
 
     depends_on('cmake@2.8.7:', type='build')
     depends_on('qt+opengl')
     depends_on('qt@4:', when='@3.2:')
     depends_on('qt@:4', when='@:3.1')
-    # TODO: Since 'ffmpeg' constantly changes its API and OSG must change in response,
-    # the version of the dependency here matters greatly on the version and patch of
-    # the release of OSG that's being used.
-    depends_on('ffmpeg@1:2+avresample')
-    depends_on('zlib')
     depends_on('libxinerama')
     depends_on('libxrandr')
+    depends_on('jasper')
+    depends_on('zlib')
 
-    patch('glibc-jasper.patch', when='@3.4.0:3.4.999%gcc')
+    depends_on('ffmpeg+avresample', when='+ffmpeg')
+    # https://github.com/openscenegraph/OpenSceneGraph/issues/167
+    depends_on('ffmpeg@:2', when='@:3.4.0+ffmpeg')
+
+    patch('glibc-jasper.patch', when='@3.4%gcc')
 
     def cmake_args(self):
         spec = self.spec
@@ -44,35 +45,25 @@ class Openscenegraph(CMakePackage):
         opengl_profile = 'GL{0}'.format(spec['gl'].version.up_to(1))
 
         args = [
-            # Library Options #
-            '-DZLIB_INCLUDE_DIR={0}'.format(spec['zlib'].prefix.include),
-            '-DZLIB_LIBRARY={0}/libz.{1}'.format(spec['zlib'].prefix.lib,
-                                                 dso_suffix),
-            '-DFFMPEG_ROOT={0}'.format(spec['ffmpeg'].prefix),
             # Variant Options #
             '-DDYNAMIC_OPENSCENEGRAPH={0}'.format(shared_status),
             '-DDYNAMIC_OPENTHREADS={0}'.format(shared_status),
             '-DOPENGL_PROFILE={0}'.format(opengl_profile),
+
             # General Options #
             '-DBUILD_OSG_APPLICATIONS=OFF',
             '-DOSG_NOTIFY_DISABLED=ON',
             '-DLIB_POSTFIX=',
             '-DCMAKE_RELWITHDEBINFO_POSTFIX=',
+            '-DCMAKE_MINSIZEREL_POSTFIX='
         ]
-        if spec.satisfies('@:3.2'):
-            args.extend([
-                '-DZLIB_INCLUDE_DIR={0}'.format(spec['zlib'].prefix.include),
-                '-DZLIB_LIBRARY={0}/libz.{1}'.format(spec['zlib'].prefix.lib,
-                                                     dso_suffix),
-            ])
 
-        # NOTE: Specifying only the root seems to be sufficient, and specifying
-        # the locations explicity causes OSG to be buggy.
-        for ffmpeg_lib in ['libavcodec', 'libavformat', 'libavutil']:
-            args.extend([
-                '-DFFMPEG_{0}_INCLUDE_DIRS='.format(ffmpeg_lib.upper()),
-                '-DFFMPEG_{0}_LIBRARIES='.format(ffmpeg_lib.upper()),
-            ])
+        if spec.satisfies('~ffmpeg'):
+            for ffmpeg_lib in ['libavcodec', 'libavformat', 'libavutil']:
+                args.extend([
+                    '-DFFMPEG_{0}_INCLUDE_DIRS='.format(ffmpeg_lib.upper()),
+                    '-DFFMPEG_{0}_LIBRARIES='.format(ffmpeg_lib.upper()),
+                ])
 
         # NOTE: This is necessary in order to allow OpenSceneGraph to compile
         # despite containing a number of implicit bool to int conversions.
@@ -81,8 +72,5 @@ class Openscenegraph(CMakePackage):
                 '-DCMAKE_C_FLAGS=-fpermissive',
                 '-DCMAKE_CXX_FLAGS=-fpermissive',
             ])
-
-        if spec.satisfies('@3.4:'):
-            args.extend(['-DBUILD_OSG_FRAMEWORKS=OFF'])
 
         return args
