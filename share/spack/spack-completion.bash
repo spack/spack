d3c2fc355b6d433db2feabf6c1232b580526f416
Peter Josef Scheibel <scheibel1@llnl.gov>
2020-04-29 15:40:21 -0700

    update shell autocompletion

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -829,7 +829,7 @@ _spack_external() {
 _spack_external_find() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help"
+        SPACK_COMPREPLY="-h --help --not-buildable"
     else
         _all_packages
     fi
