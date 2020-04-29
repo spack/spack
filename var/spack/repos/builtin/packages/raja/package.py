01756c96b282945d8ddec5d0d6ee90c098e4e757
Matt Larsen <mclarsen@users.noreply.github.com>
2020-04-29 15:27:17 -0700

    Make RAJA aware of the cuda architecture

--- var/spack/repos/builtin/packages/raja/package.py
+++ var/spack/repos/builtin/packages/raja/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class Raja(CMakePackage):
+class Raja(CMakePackage,CudaPackage):
     """RAJA Parallel Framework."""
 
     homepage = "http://software.llnl.gov/RAJA/"
@@ -46,5 +46,9 @@ class Raja(CMakePackage):
             options.extend([
                 '-DENABLE_CUDA=On',
                 '-DCUDA_TOOLKIT_ROOT_DIR=%s' % (spec['cuda'].prefix)])
+            if 'cuda_arch' in spec.variants:
+                cuda_value = spec.variants['cuda_arch'].value
+                cuda_arch = cuda_value[0]
+                options.append('-DCUDA_ARCH=sm_{0}'.format(cuda_arch))
 
         return options
