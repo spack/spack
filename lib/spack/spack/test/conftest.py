d1e6e078ad4986b9a54fbe207811a2906a5adb16
Peter Josef Scheibel <scheibel1@llnl.gov>
2020-05-01 17:41:17 -0700

    Revert "Revert "Testing: fix unintended interactions between tests (#16003)" (#16420)"
    This reverts commit 40df44e021e2908b8ae7d05d7c07159444c8b8d5.

--- lib/spack/spack/test/conftest.py
+++ lib/spack/spack/test/conftest.py
@@ -423,13 +423,13 @@ def config(mock_configuration):
 
 
 @pytest.fixture(scope='function')
-def mutable_config(tmpdir_factory, configuration_dir, monkeypatch):
+def mutable_config(tmpdir_factory, configuration_dir):
     """Like config, but tests can modify the configuration."""
     mutable_dir = tmpdir_factory.mktemp('mutable_config').join('tmp')
     configuration_dir.copy(mutable_dir)
 
     cfg = spack.config.Configuration(
-        *[spack.config.ConfigScope(name, str(mutable_dir))
+        *[spack.config.ConfigScope(name, str(mutable_dir.join(name)))
           for name in ['site', 'system', 'user']])
 
     with use_configuration(cfg):
