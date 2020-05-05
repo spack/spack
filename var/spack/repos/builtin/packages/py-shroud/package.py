02946c5accb475ceb4d7c998e939ad5767ebbd4a
Chris White <white238@llnl.gov>
2020-05-04 18:39:39 -0700

    Fix flake8 and add copyright

--- var/spack/repos/builtin/packages/py-shroud/package.py
+++ var/spack/repos/builtin/packages/py-shroud/package.py
@@ -1,4 +1,10 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 from spack import *
+
 
 class PyShroud(PythonPackage):
     """Create Fortran wrappers for a C++ library."""
