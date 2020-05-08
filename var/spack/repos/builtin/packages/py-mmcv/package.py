2f396b0aff89aff6914f7b49f2a8079957e6322c
Jen Herting <jen@herting.cc>
2020-05-08 12:25:51 -0400

    [py-mmcv] cleaned up fixmes

--- var/spack/repos/builtin/packages/py-mmcv/package.py
+++ var/spack/repos/builtin/packages/py-mmcv/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-mmcv
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-mmcv
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -31,10 +14,6 @@ class PyMmcv(PythonPackage):
     homepage = "https://mmcv.readthedocs.io/en/latest/"
     url      = "https://github.com/open-mmlab/mmcv/archive/v0.5.1.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('0.5.1', sha256='7c5ad30d9b61e44019e81ef46c406aa85dd08b5d0ba12ddd5cdc9c445835a55e')
 
     depends_on('python@3.6:', type=('build', 'run'))
@@ -43,9 +22,3 @@ class PyMmcv(PythonPackage):
     depends_on('py-pyyaml', type=('build', 'run'))
     depends_on('opencv+python', type=('build', 'run'))
     depends_on('py-cython', type='build')
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
