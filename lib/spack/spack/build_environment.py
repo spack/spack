81d57af7a119d241ae0211740998f7b8326bd0ee
Adam J. Stewart <ajstewart426@gmail.com>
2020-05-11 21:33:49 -0500

    Flake8 fix

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -725,7 +725,8 @@ def setup_package(pkg, dirty):
 
     implicit_rpaths = pkg.compiler.implicit_rpaths()
     if implicit_rpaths:
-        build_env.set('SPACK_COMPILER_IMPLICIT_RPATHS', ':'.join(implicit_rpaths))
+        build_env.set('SPACK_COMPILER_IMPLICIT_RPATHS',
+                      ':'.join(implicit_rpaths))
 
     # Make sure nothing's strange about the Spack environment.
     validate(build_env, tty.warn)
