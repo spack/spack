2e21cb69167e7a6934e8d2df001ffc3819f56e66
jjwilke <jjwilke@sandia.gov>
2020-05-12 23:06:36 -0700

    add SST packages and dependencies

---
+++ var/spack/repos/builtin/packages/goblin-hmc-sim/package.py
@@ -0,0 +1,28 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class GoblinHmcSim(MakefilePackage):
+    """
+    The Goblin HMC-Sim is a Hybrid Memory Cube
+    Functional Simulation Environment
+    """
+
+    homepage = "https://github.com/tactcomplabs/gc64-hmcsim"
+    git = "https://github.com/tactcomplabs/gc64-hmcsim"
+    # The version numbers track the SST they were released with
+    url = "https://github.com/tactcomplabs/gc64-hmcsim/archive/sst-8.0.0-release.tar.gz"
+    # This works with parallel builds outside Spack
+    # For some reason .o files get thrashed inside Spack
+    parallel = False
+
+    maintainers = ['jjwilke']
+
+    version('8.0.0', sha256="8a5e6b701865a581f15965d3ddd8c7d301b15f4b63543c444058e9c3688fd2c8")
+
+    def install(self, spec, prefix):
+        install_tree(".", prefix)
