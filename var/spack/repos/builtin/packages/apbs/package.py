79395dd1bf07fa42771eef53265eb6db85868584
Jen Herting <jen@herting.cc>
2020-05-13 14:00:20 -0400

    [apbs] removed fixmes

--- var/spack/repos/builtin/packages/apbs/package.py
+++ var/spack/repos/builtin/packages/apbs/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install apbs
-#
-# You can edit this file again by typing:
-#
-#     spack edit apbs
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
@@ -32,10 +15,6 @@ class Apbs(AutotoolsPackage):
 
     # Working around race condition
     parallel = False
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     # Version 1.4.2.1 uses cmake. Commented out pending resolution of
     # https://github.com/spack/spack/pull/12941
