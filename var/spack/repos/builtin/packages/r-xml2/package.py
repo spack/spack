dcf8acf147a92d55340bb7038ff66e95bf007401
Amjad Kotobi <amjadkotbi@gmail.com>
2020-05-13 14:51:41 +0200

    r-xml2: updated dependencies

--- var/spack/repos/builtin/packages/r-xml2/package.py
+++ var/spack/repos/builtin/packages/r-xml2/package.py
@@ -22,4 +22,4 @@ class RXml2(RPackage):
     depends_on('r@3.1.0:', type=('build', 'run'))
     depends_on('r-rcpp@0.12.12:', when='@:1.2', type=('build', 'run'))
     depends_on('r-bh', when='@:1.1.1', type=('build', 'run'))
-    depends_on('libxml2', when='@:1.2')
+    depends_on('libxml2')
