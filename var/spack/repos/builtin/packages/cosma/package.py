d6801c79143bb4188e825cba38a712a0658f7d54
Marko Kabic <mkmarkokabic@gmail.com>
2020-05-16 01:05:13 +0200

    Updated versions of COSMA.

--- var/spack/repos/builtin/packages/cosma/package.py
+++ var/spack/repos/builtin/packages/cosma/package.py
@@ -6,38 +6,35 @@
 
 from spack import *
 
-
 class Cosma(CMakePackage):
     """
     Distributed Communication-Optimal Matrix-Matrix Multiplication Library
     """
 
-    maintainers = ['teonnik', 'kabicm']
+    maintainers = ['haampie', 'kabicm', 'teonnik']
     homepage = 'https://github.com/eth-cscs/COSMA'
-    url = 'https://github.com/eth-cscs/COSMA/releases/download/v2.0.2/cosma.tar.gz'
+    url = 'https://github.com/eth-cscs/COSMA/releases/download/v2.2.0/cosma.tar.gz'
     git = 'https://github.com/eth-cscs/COSMA.git'
 
     # note: The default archives produced with github do not have the archives
     #       of the submodules.
     version('master', branch='master', submodules=True)
-    version('2.0.2', sha256='4f3354828bc718f3eef2f0098c3bdca3499297497a220da32db1acd57920c68d')
-    # note: this version fails to build at the moment
-    # version('1.0.0',
-    #         url='https://github.com/eth-cscs/COSMA/releases/download/1.0/cosma.tar.gz',
-    #         sha256='c142104258dcca4c17fa7faffc2990a08d2777235c7980006e93c5dca51061f6')
+    version('2.2.0', '8f3c6b9235c83092777a958bda37bfc2fa2b0da4')
+    version('2.0.7', '70d0d9ab45f2af3d06c5c0e856e28b2adb246ff9')
+    version('2.0.2', 'fcedd19be56ca75d122782f29872923d9bead847')
 
-    variant('cuda', default=False,
-            description='Build with the CUBLAS back end.')
+    variant('gpu', default=False,
+            description='Build with the GPU back end.')
     variant('scalapack', default=False,
-            description='Build with ScaLAPACK support.')
+            description='Build with ScaLAPACK API.')
 
     depends_on('cmake@3.12:', type='build')
     depends_on('mpi@3:')
-    depends_on('blas', when='~cuda')
+    depends_on('blas', when='~gpu')
     depends_on('scalapack', when='+scalapack')
-    # COSMA is written entirely in C++, it may use cublasXt but a CUDA capable
+    # COSMA is written entirely in C/C++, it may use cublas but a CUDA capable
     # compiler is not needed. There is no need for CudaPackage in this recipe.
-    depends_on('cuda', when='+cuda')
+    depends_on('cuda', when='+gpu')
 
     def setup_build_environment(self, env):
         if '+cuda' in self.spec:
@@ -52,17 +49,21 @@ class Cosma(CMakePackage):
 
         if '^mkl' in spec:
             args += ['-DCOSMA_BLAS=MKL']
+        elif '^cray-libsci' in spec:
+            args += ['-DCOSMA_BLAS=CRAY_LIBSCI']
         elif '^netlib-lapack' in spec:
             args += ['-DCOSMA_BLAS=CUSTOM']
         elif '^openblas' in spec:
             args += ['-DCOSMA_BLAS=OPENBLAS']
-        elif '+cuda' in spec:
+        elif '+gpu' in spec:
             args += ['-DCOSMA_BLAS=CUDA']
         else:  # TODO '^rocm' in spec:
             args += ['-DCOSMA_BLAS=ROCM']
 
         if '+scalapack' and '^mkl' in spec:
             args += ['-DCOSMA_SCALAPACK=MKL']
+        elif '+scalapack' and '^cray-libsci' in spec:
+            args += ['-DCOSMA_SCALAPACK=CRAY_LIBSCI']
         elif '+scalapack' and '^netlib-scalapack' in spec:
             args += ['-DCOSMA_SCALAPACK=CUSTOM']
 
