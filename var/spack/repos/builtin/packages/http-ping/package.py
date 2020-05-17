07048ba67a1390b6f580e596f78ab504c7e6c3ce
darmac <xiaojun2@hisilicon.com>
2020-05-17 09:41:21 +0800

    Add new package: http-ping

---
+++ var/spack/repos/builtin/packages/http-ping/package.py
@@ -0,0 +1,28 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class HttpPing(MakefilePackage):
+    """http_ping is like the regular ping command, except that it sends
+    HTTP requests instead of ICMP echo requests."""
+
+    homepage = "http://www.acme.com/software/http_ping/"
+    url      = "http://www.acme.com/software/http_ping/http_ping_09Mar2016.tar.gz"
+
+    version('09Mar2016', sha256='6bdc570c776a760d2c08f7d18e00b0edd74cf603400929c66c512801b6bb5871')
+
+    def edit(self, spec, prefix):
+        makefile = FileFilter("Makefile")
+        makefile.filter("BINDIR =\t/usr/local/bin",
+                        "BINDIR =    {0}/bin".format(self.prefix))
+        makefile.filter("MANDIR =\t/usr/local/man/man1",
+                        "MANDIR={0}/man/man1".format(self.prefix))
+
+    def install(self, spec, prefix):
+        mkdirp(prefix.bin)
+        mkdirp(prefix.man.man1)
+        make('install')
