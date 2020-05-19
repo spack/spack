972afd5dc94adb41d6c1ad0d7eab5369135e6947
Gregory Becker <becker33@llnl.gov>
2020-05-18 17:51:45 -0700

    flake

--- lib/spack/spack/compilers/clang.py
+++ lib/spack/spack/compilers/clang.py
@@ -57,8 +57,8 @@ class Clang(Compiler):
 
     @property
     def opt_flags(self):
-        return ['-O0', '-O1', '-O2', '-O3', '-Ofast','-Os', '-Oz', '-Og', '-O',
-                '-O4']
+        return ['-O0', '-O1', '-O2', '-O3', '-Ofast', '-Os', '-Oz', '-Og',
+                '-O', '-O4']
 
     # Clang has support for using different fortran compilers with the
     # clang executable.
