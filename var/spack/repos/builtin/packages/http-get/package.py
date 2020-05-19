b773fa88dab340737842cbf14a85e4a2b5efe5ad
darmac <xiaojun2@hisilicon.com>
2020-05-20 01:30:19 +0800

    refine version name

--- var/spack/repos/builtin/packages/http-get/package.py
+++ var/spack/repos/builtin/packages/http-get/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
+import datetime
 
 
 class HttpGet(MakefilePackage):
@@ -14,7 +15,12 @@ class HttpGet(MakefilePackage):
     homepage = "http://www.acme.com/software/http_get/"
     url      = "http://www.acme.com/software/http_get/http_get_23May2018.tar.gz"
 
-    version('23May2018', sha256='7d46ce25e53b6d3e27a99c1853c3054a046cc97d5e30a713a7ec986cfe7c4fe0')
+    version('2018-05-23', sha256='7d46ce25e53b6d3e27a99c1853c3054a046cc97d5e30a713a7ec986cfe7c4fe0')
+
+    def url_for_version(self, version):
+        ver = datetime.datetime.strptime(str(version), '%Y-%m-%d').date()
+        verstr = datetime.datetime.strftime(ver, '%d%b%Y')
+        return "http://www.acme.com/software/http_get/http_get_{0}.tar.gz".format(verstr)
 
     def edit(self, spec, prefix):
         makefile = FileFilter("Makefile")
