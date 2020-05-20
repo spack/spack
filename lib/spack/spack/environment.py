892e5d6efea0b39b839bbe1e72c3b4c7b5b43fa3
Peter Josef Scheibel <scheibel1@llnl.gov>
2020-05-20 11:18:29 -0700

    another flake edit

--- lib/spack/spack/environment.py
+++ lib/spack/spack/environment.py
@@ -1437,9 +1437,9 @@ class Environment(object):
                 # The primary list is handled differently
                 continue
 
-            active_yaml_lists = [L for L in yaml_dict.get('definitions', [])
-                                 if name in L and
-                                 _eval_conditional(L.get('when', 'True'))]
+            active_yaml_lists = [x for x in yaml_dict.get('definitions', [])
+                                 if name in x and
+                                 _eval_conditional(x.get('when', 'True'))]
 
             # Remove any specs in yaml that are not in internal representation
             for ayl in active_yaml_lists:
