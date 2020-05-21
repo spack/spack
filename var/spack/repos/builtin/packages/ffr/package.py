3e22c18c85ddc4f1ed1655e052934804c875913f
noguchi-k <fj6618as@aa.jp.fujitsu.com>
2020-05-21 13:56:20 +0900

    ffr: changed to elif.

--- var/spack/repos/builtin/packages/ffr/package.py
+++ var/spack/repos/builtin/packages/ffr/package.py
@@ -39,7 +39,7 @@ class Ffr(MakefilePackage):
         fflags = flags[:]
         if spec.satisfies('%gcc'):
             fflags.append('-ffixed-line-length-none')
-        if spec.satisfies('%fj'):
+        elif spec.satisfies('%fj'):
             fflags.append('-Fwide')
         d = find('.', 'src_main', recursive=True)
         root_dir  = os.path.dirname(d[0])
