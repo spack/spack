8839c3af1161e17fa0559517b8f531fb2fb6a177
t-karatsu <fj6085ge@fujitsu.com>
2020-05-25 16:36:24 +0900

    netlib-scalapack: Add MPI definition when using 'fujitsu-mpi'.

--- var/spack/repos/builtin/packages/netlib-scalapack/package.py
+++ var/spack/repos/builtin/packages/netlib-scalapack/package.py
@@ -82,6 +82,14 @@ class NetlibScalapack(CMakePackage):
                 "-DCMAKE_Fortran_FLAGS=%s" % self.compiler.fc_pic_flag
             ])
 
+        if '^fujitsu-mpi' in spec:
+            options.extend([
+                '-DMPI_C_COMPILER=%s'       % spec['mpi'].mpicc,
+                '-DMPI_CXX_COMPILER=%s'     % spec['mpi'].mpicxx,
+                '-DMPI_Fortran_COMPILER=%s' % spec['mpi'].mpifc,
+                '-DMPI_BASE_DIR=%s' % spec['mpi'].prefix
+            ])
+
         return options
 
     @run_after('install')
