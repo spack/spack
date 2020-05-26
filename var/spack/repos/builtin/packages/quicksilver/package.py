8c217f560f8ac46f74e80887ffcf1f7bc84a468d
Christoph Junghans <junghans@lanl.gov>
2020-05-26 13:41:23 -0600

    quicksilver: add mpi and openmpi variant

--- var/spack/repos/builtin/packages/quicksilver/package.py
+++ var/spack/repos/builtin/packages/quicksilver/package.py
@@ -19,14 +19,39 @@ class Quicksilver(MakefilePackage):
 
     maintainers = ['richards12']
 
-    version('develop', branch='master')
+    version('master', branch='master')
+
+    variant('openmp', default=True, description='Build with OpenMP support')
+    variant('mpi', default=True, description='Build with MPI support')
+
+    depends_on('mpi', when="+mpi")
 
     build_directory = 'src'
 
     @property
     def build_targets(self):
         targets = []
-        targets.append('CXX={0}'.format(spack_cxx))
+        spec = self.spec
+
+        targets.append('CXXFLAGS={0}'.format(self.compiler.cxx11_flag))
+
+        if '+mpi' in spec:
+            targets.append('CXX={0}'.format(spec['mpi'].mpicxx))
+        else:
+            targets.append('CXX={0}'.format(spack_cxx))
+
+        if '+openmp+mpi' in spec:
+            targets.append('CPPFLAGS=-DHAVE_MPI -DHAVE_OPENMP {0}'.format(
+                self.compiler.openmp_flag))
+        elif '+openmp' in spec:
+            targets.append('CPPFLAGS=-DHAVE_OPENMP {0}'.format(
+                self.compiler.openmp_flag))
+        elif '+mpi' in spec:
+            targets.append('CPPFLAGS=-DHAVE_MPI')
+
+        if '+openmp' in self.spec:
+            targets.append('LDFLAGS={0}'.format(self.compiler.openmp_flag))
+
         return targets
 
     def install(self, spec, prefix):
@@ -35,3 +60,4 @@ class Quicksilver(MakefilePackage):
         install("src/qs", prefix.bin)
         install('LICENSE.md', prefix.doc)
         install('README.md', prefix.doc)
+        install_tree('Examples', prefix.Examples)
