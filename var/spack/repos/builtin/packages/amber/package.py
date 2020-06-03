f7e1d230aa332cb3763167df8c6c0c929579bd03
dmorone <dmorone@users.noreply.github.com>
2020-06-03 09:31:26 +0000

    Updated to cuda 10.2.89
    Fix message 'amber requires cuda version :10.1.243, but spec asked for 10.2.89'

--- var/spack/repos/builtin/packages/amber/package.py
+++ var/spack/repos/builtin/packages/amber/package.py
@@ -96,7 +96,7 @@ class Amber(Package, CudaPackage):
     depends_on('mpi', when='+mpi')
 
     # Cuda dependencies
-    depends_on('cuda@:10.1.243', when='@18:+cuda')
+    depends_on('cuda@:10.2.89', when='@18:+cuda')
     depends_on('cuda@7.5.18', when='@:16+cuda')
 
     # conflicts
