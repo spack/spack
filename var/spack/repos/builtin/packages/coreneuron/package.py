4227d5ae2c95c5330bcb6dd81e0ff8d1e62e9647
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-06-05 10:32:23 +0200

    Fixes issue with installing nmodl@develop with coreneuron@0.17: (#828)
    * Fixes issue with installing nmodl@develop with coreneuron@0.17:

--- var/spack/repos/builtin/packages/coreneuron/package.py
+++ var/spack/repos/builtin/packages/coreneuron/package.py
@@ -56,7 +56,7 @@ class Coreneuron(CMakePackage):
     depends_on('tau', when='+profile')
 
     # nmodl specific dependency
-    depends_on('nmodl@0.3b', when='@0.17:+nmodl')
+    depends_on('nmodl@0.3b:', when='@0.17:+nmodl')
     depends_on('nmodl@0.3a', when='@0:0.16+nmodl')
     depends_on('eigen@3.3.4:~metis~scotch~fftw~suitesparse~mpfr', when='+nmodl')
     depends_on('ispc', when='+ispc')
