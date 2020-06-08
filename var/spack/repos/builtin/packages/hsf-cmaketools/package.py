3dd1032dc3aec105ed48d5ff361f60cb4383a8fb
vvolkl <valentin.volkl@cern.ch>
2020-06-08 09:15:29 +0200

    Apply suggestions from code review
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/hsf-cmaketools/package.py
+++ var/spack/repos/builtin/packages/hsf-cmaketools/package.py
@@ -19,9 +19,9 @@ class HsfCmaketools(Package):
     version('1.8', sha256='91af30f5701dadf80a5d7e0d808c224c934f0784a3aff2d3b69aff24f7e1db41')
 
     def install(self, spec, prefix):
-        mkdir(join_path(prefix, "modules"))
-        copy_tree('modules', join_path(prefix, 'modules'))
-        copy("CMakeToolsConfig.cmake", prefix)
+        mkdir(prefix.modules)
+        install_tree('modules', prefix.modules)
+        install("CMakeToolsConfig.cmake", prefix)
 
     def setup_dependent_build_environment(self, env, dependent_spec):
         env.set('CMAKE_PREFIX_PATH', self.prefix)
