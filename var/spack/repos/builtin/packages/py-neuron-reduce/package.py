ac2515ed3d7f7f8d11b771952870b8add22721f8
asanin-epfl <53935643+asanin-epfl@users.noreply.github.com>
2020-06-08 12:17:16 +0200

    py-sonata-network-reduction v0.0.8 (#834)
    py-sonata-network-reduction v0.0.8

--- var/spack/repos/builtin/packages/py-neuron-reduce/package.py
+++ var/spack/repos/builtin/packages/py-neuron-reduce/package.py
@@ -14,8 +14,9 @@ class PyNeuronReduce(PythonPackage):
     git = "git@github.com:orena1/neuron_reduce.git"
 
     version('develop', branch='master')
-    version('0.0.6', sha256='e75ce138ae049f550d72ec86c5cae4afcada577a2805d9dc50cbe80eff1f2256')
+    version('0.0.8', commit='3aada2ad3606723a6ebbaf39581153e38de24733')
     version('0.0.7', commit='1bad597f2faa5ff6aa8c94b6f326f86a02e656d7')
+    version('0.0.6', sha256='e75ce138ae049f550d72ec86c5cae4afcada577a2805d9dc50cbe80eff1f2256')
 
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-numpy@1.14:', type='run')
--- var/spack/repos/builtin/packages/py-sonata-network-reduction/package.py
+++ var/spack/repos/builtin/packages/py-sonata-network-reduction/package.py
@@ -13,6 +13,7 @@ class PySonataNetworkReduction(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/nse/sonata-network-reduction"
 
     version('develop', branch='master')
+    version('0.0.8', tag='sonata-network-reduction-v0.0.8')
     version('0.0.6', tag='sonata-network-reduction-v0.0.6')
     version('0.0.5', tag='sonata-network-reduction-v0.0.5')
     version('0.0.4', tag='sonata-network-reduction-v0.0.4')
@@ -20,7 +21,7 @@ class PySonataNetworkReduction(PythonPackage):
     depends_on('py-setuptools', type=('build', 'run'))
 
     depends_on('py-numpy@1.14:', type='run')
-    depends_on('py-h5py@2.8:', type='run')
+    depends_on('py-h5py@2.10:', type='run')
     depends_on('py-pandas@0.25:0.30', type='run')
     depends_on('py-lxml@4.3.4:', type='run')
     depends_on('py-tqdm@4.34:', type='run')
@@ -28,11 +29,11 @@ class PySonataNetworkReduction(PythonPackage):
     depends_on('py-joblib@0.14:', type='run')
     depends_on('py-bluepyopt@1.8.68:', type='run')
     depends_on('py-bglibpy@4.2:', type='run')
-    depends_on('py-morphio@2.3.4:', type='run')
+    depends_on('py-morphio@2.3.9:', type='run')
     depends_on('neuron+python', type='run')
     depends_on('py-aibs-circuit-converter@0.0.3:', type='run')
-    depends_on('py-bluepysnap@0.1.2:', type='run')
-    depends_on('py-neuron-reduce@0.0.7:', type='run')
+    depends_on('py-bluepysnap@0.4.1:', type='run')
+    depends_on('py-neuron-reduce@0.0.8:', type='run')
 
     def setup_run_environment(self, env):
         env.set('NEURON_INIT_MPI', "0")
