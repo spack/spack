5bd49a1572ea76b871d2b676d09eec24d85a863f
Gregory Becker <becker33@llnl.gov>
2020-06-08 16:55:59 -0700

    typo

--- var/spack/repos/builtin/packages/fujitsu-mpi/package.py
+++ var/spack/repos/builtin/packages/fujitsu-mpi/package.py
@@ -34,7 +34,7 @@ class FujitsuMpi(Package):
         self.spec.mpifc = self.prefix.bin.mpifrt
 
     def setup_dependent_build_environment(self, env, dependent_spec):
-        self.setup_run_environment(env):
+        self.setup_run_environment(env)
 
     def setup_run_environment(self, env):
         # Because MPI are both compilers and runtimes, we set up the compilers
