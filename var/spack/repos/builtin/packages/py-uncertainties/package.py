7f74741bee395a74b8c0fe65a20437b4a2c29ab5
Sinan81 <Sinan81@github>
2020-06-07 23:02:52 -0700

    future is only required for python@2 I think

--- var/spack/repos/builtin/packages/py-uncertainties/package.py
+++ var/spack/repos/builtin/packages/py-uncertainties/package.py
@@ -19,6 +19,6 @@ class PyUncertainties(PythonPackage):
 
     depends_on('python@2.7:',   type=('build', 'run'))
     depends_on('py-setuptools', type='build')
-    depends_on('py-future',     type=('build', 'run'))
+    depends_on('py-future',     type=('build', 'run'), when='python@:2')
     depends_on('py-numpy',      type=('build', 'run'), when='+numpy')
     depends_on('py-sphinx',     type=('build', 'run'), when='+docs')
