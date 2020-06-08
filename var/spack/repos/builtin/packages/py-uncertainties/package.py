728e56b1b88b233befe3af0c99b4adb2e9ded627
Sinan81 <Sinan81@github>
2020-06-07 23:04:51 -0700

    fix typo, flake8

--- var/spack/repos/builtin/packages/py-uncertainties/package.py
+++ var/spack/repos/builtin/packages/py-uncertainties/package.py
@@ -7,15 +7,17 @@ from spack import *
 
 
 class PyUncertainties(PythonPackage):
-    """Transparent calculations with uncertainties on the quantities involved (aka error propagation); fast calculation of derivatives"""
+    """Transparent calculations with uncertainties on the quantities involved (aka error propagation);
+    fast calculation of derivatives
+    """
 
     homepage = "https://uncertainties-python-package.readthedocs.io/en/latest/"
     url      = "https://pypi.io/packages/source/u/uncertainties/uncertainties-3.1.4.tar.gz"
 
     version('3.1.4', sha256='63548a94899f2a51eeb89b640f6ac311f481a8016b37dce157186e44619bc968')
 
-    varaint('numpy', default=False, description='Enable extra features requiring numpy')
-    varaint('docs',  default=False, description='Build with documentation')
+    variant('numpy', default=False, description='Enable extra features requiring numpy')
+    variant('docs',  default=False, description='Build with documentation')
 
     depends_on('python@2.7:',   type=('build', 'run'))
     depends_on('py-setuptools', type='build')
