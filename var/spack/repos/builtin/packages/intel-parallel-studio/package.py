5024bd29383ab68b6ef7865052b1f0c4003a9f5f
Gregory Becker <becker33@llnl.gov>
2020-06-08 18:22:35 -0700

    ditto

--- var/spack/repos/builtin/packages/intel-parallel-studio/package.py
+++ var/spack/repos/builtin/packages/intel-parallel-studio/package.py
@@ -230,4 +230,4 @@ class IntelParallelStudio(IntelPackage):
         super(self, IntelParallelStudio).setup_run_environment(*args)
         # We can fake the dependent spec because it isn't used
         # This allows us to setup mpi compilers in run env
-        self.setup_dependent_build_environment(*args, None)
+        self.setup_dependent_build_environment(*args, dependent_spec=None)
