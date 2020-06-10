7558bc1f937c3ddb8b7ff5d83fffaec60644653c
Michio Ogawa <michioga@bekkoame.ne.jp>
2020-06-10 13:07:20 +0900

    removed comment from package.py

--- var/spack/repos/builtin/packages/frontistr/package.py
+++ var/spack/repos/builtin/packages/frontistr/package.py
@@ -3,22 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install frontistr
-#
-# You can edit this file again by typing:
-#
-#     spack edit frontistr
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
@@ -26,20 +10,12 @@ from spack import *
 class Frontistr(CMakePackage):
     """Large-Scale Parallel FEM Program for Nonlinear Structural Analysis : FrontISTR"""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://www.frontistr.com"
     url      = "https://gitlab.com/FrontISTR-Commons/FrontISTR/-/archive/v5.0/FrontISTR-v5.0.tar.gz"
-    #url      = "https://gitlab.com/FrontISTR-Commons/FrontISTR/-/archive/master/FrontISTR-master.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('5.0', sha256='b60e77146da0b46d0b094416bab01298a44c33bbcf705763473be294a78c8993')
-    #version('master', git='https://gitlab.com/FrontISTR-Commons/FrontISTR.git', commit='73aed597fc0894bde7ae8d19365447acfde424d5')
     version('master', git='https://gitlab.com/FrontISTR-Commons/FrontISTR.git')
 
-    # FIXME: Add dependencies if required.
     depends_on('mpi', type=('build', 'link', 'run'))
     depends_on('lapack', type=('build', 'link'))
     depends_on('blas', type=('build', 'link'))
@@ -49,8 +25,5 @@ class Frontistr(CMakePackage):
     depends_on('cmake', type='build')
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = []
         return args
