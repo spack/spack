519f1f6a0428ac68c173cba75566d5262cbdd46b
Omar Padron <omar.padron@kitware.com>
2020-06-10 13:47:21 -0400

    update command completion

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -474,7 +474,7 @@ _spack_ci() {
 }
 
 _spack_ci_generate() {
-    SPACK_COMPREPLY="-h --help --output-file --copy-to --spack-repo --spack-ref"
+    SPACK_COMPREPLY="-h --help --output-file --copy-to --spack-repo --spack-ref --optimize"
 }
 
 _spack_ci_rebuild() {
