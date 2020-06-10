3a7655c97c1bb1ace816e371661df6203da8c9e6
Omar Padron <omar.padron@kitware.com>
2020-06-10 11:49:17 -0400

    make optimization opt-in

--- lib/spack/spack/ci.py
+++ lib/spack/spack/ci.py
@@ -449,7 +449,8 @@ def format_job_needs(phase_name, strip_compilers, dep_jobs,
 
 
 def generate_gitlab_ci_yaml(env, print_summary, output_file,
-                            custom_spack_repo=None, custom_spack_ref=None):
+                            custom_spack_repo=None, custom_spack_ref=None,
+                            run_optimizer=False):
     # FIXME: What's the difference between one that opens with 'spack'
     # and one that opens with 'env'?  This will only handle the former.
     with spack.concretize.disable_compiler_existence_check():
@@ -789,8 +790,9 @@ def generate_gitlab_ci_yaml(env, print_summary, output_file,
         sorted_output[output_key] = output_value
 
     # TODO(opadron): remove this or refactor
-    import spack.ci_optimization.optimizer as optimizer
-    sorted_output = optimizer(sorted_output)
+    if run_optimizer:
+        import spack.ci_optimization.optimizer as optimizer
+        sorted_output = optimizer(sorted_output)
 
     with open(output_file, 'w') as outf:
         outf.write(syaml.dump_config(sorted_output, default_flow_style=True))
