f094ed01ee271df8f7725d6a93c18c2b094a6a54
Sinan81 <sbulut@3vgeomatics.com>
2020-06-10 17:34:23 -0700

    improve docstring

--- var/spack/repos/builtin/packages/opencl-icd-loader/package.py
+++ var/spack/repos/builtin/packages/opencl-icd-loader/package.py
@@ -8,25 +8,12 @@ from spack import *
 
 class OpenclIcdLoader(CMakePackage):
     """
-    OpenCL defines an Installable Client Driver (ICD) mechanism to allow developers to build applications against an Installable Client Driver loader (ICD loader) rather than linking their applications against a specific OpenCL implementation. The ICD Loader is responsible for:
-
-    Exporting OpenCL API entry points
-    Enumerating OpenCL implementations
-    Forwarding OpenCL API calls to the correct implementation
-
+    OpenCL defines an Installable Client Driver (ICD) mechanism to allow developers to build applications against an Installable Client Driver loader (ICD loader) rather than linking their applications against a specific OpenCL implementation. The ICD Loader is responsible for: (i) Exporting OpenCL API entry points, (ii) Enumerating OpenCL implementations, and (iii) Forwarding OpenCL API calls to the correct implementation
     """
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://www.khronos.org/news/permalink/opencl-installable-client-driver-icd-loader"
     url      = "https://github.com/KhronosGroup/OpenCL-ICD-Loader/archive/v2020.03.13.tar.gz"
 
     version('2020.03.13', sha256='cafcfa9e48d523d0534e6879af2badd7006b3c646eab5f05314de72a2c542816')
 
     depends_on('opencl')
-
-    def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
-        args = []
-        return args
