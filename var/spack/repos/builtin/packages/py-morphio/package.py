97bc5e2979d136c712b7679790b2b3ab5d8cccd0
Benoit Coste <benoit.coste@protonmail.com>
2020-06-11 00:35:46 -0700

    Fix NeuroM: only require MorphIO when version==mut_morphio (#837)
    Co-authored-by: Matthias Wolf <m+git@sushinara.net>

--- var/spack/repos/builtin/packages/py-morphio/package.py
+++ var/spack/repos/builtin/packages/py-morphio/package.py
@@ -14,6 +14,7 @@ class PyMorphio(PythonPackage):
 
     version('develop', branch='master', submodules=True, get_full_repo=True)
     version('unifurcation', branch='unifurcation', submodules=True, get_full_repo=True)
+    version('2.3.10', tag='v2.3.10', submodules=True, get_full_repo=True)
     version('2.3.4', tag='v2.3.4', submodules=True, get_full_repo=True)
     version('2.2.1', tag='v2.2.1', submodules=True, get_full_repo=True)
     version('2.1.2', tag='v2.1.2', submodules=True, get_full_repo=True)
