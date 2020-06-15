97ca2e93cdbb28b64c1c6421f83dcd81cb369303
Itaru Kitayama <itaru.kitayama@riken.jp>
2020-06-15 13:03:54 +0900

    Add 4.5 support.

--- var/spack/repos/builtin/packages/cubew/package.py
+++ var/spack/repos/builtin/packages/cubew/package.py
@@ -12,6 +12,7 @@ class Cubew(AutotoolsPackage):
     homepage = "http://www.scalasca.org/software/cube-4.x/download.html"
     url = "http://apps.fz-juelich.de/scalasca/releases/cube/4.4/dist/cubew-4.4.tar.gz"
 
+    version('4.5', sha256='16bd8fd864197a74ca65f7325761ad75d73d555072326e95e1338cff39f28a5c')
     version('4.4.3', sha256='93fff6cc1e8b0780f0171ef5302a2e1a257f99b6383fbfc1b9b82f925ceff501')
     version('4.4.2', sha256='31a71e9a05e6523de2b86b4026821bbb75fb411eb5b18ae38b27c1f44158014a')
     version('4.4.1', sha256='c09e3f5a3533ebedee2cc7dfaacd7bac4680c14c3fa540669466583a23f04b67')
