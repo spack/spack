aae1a567784a626f6ef0e8289c43708723c9d73e
Gregory Becker <becker33@llnl.gov>
2020-06-15 20:57:02 -0500

    improved regex and remove redundant error checking

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -3945,7 +3945,8 @@ class SpecLexer(spack.parse.Lexer):
 
             # Filenames match before identifiers, so no initial filename
             # component is parsed as a spec (e.g., in subdir/spec.yaml)
-            (r'[/\w.-]+\.yaml$', lambda scanner, v: self.token(FILE, v)),
+            (r'[/\w.-]*/[/\w/-]+\.yaml$',
+             lambda scanner, v: self.token(FILE, v)),
 
             # Hash match after filename. No valid filename can be a hash
             # (files end w/.yaml), but a hash can match a filename prefix.
@@ -4096,23 +4097,6 @@ class SpecParser(spack.parse.Parser):
         """
         path = self.token.value
 
-        # don't treat builtin.yaml, builtin.yaml-cpp, etc. as filenames
-        if re.match(spec_id_re, path):
-            self.push_tokens([spack.parse.Token(ID, self.token.value)])
-            return None
-
-        # Special case where someone omits a space after a filename. Consider:
-        #
-        #     libdwarf^/some/path/to/libelf.yamllibdwarf ^../../libelf.yaml
-        #
-        # The error is clearly an omitted space. To handle this, the FILE
-        # regex admits text *beyond* .yaml, and we raise a nice error for
-        # file names that don't end in .yaml.
-        if not path.endswith(".yaml"):
-            raise SpecFilenameError(
-                "Spec filename must end in .yaml: '{0}'".format(path))
-
-        # if we get here, we're *finally* interpreting path as a filename
         if not os.path.exists(path):
             raise NoSuchSpecFileError("No such spec file: '{0}'".format(path))
 
@@ -4458,11 +4442,7 @@ class NoSuchHashError(spack.error.SpecError):
             % hash)
 
 
-class SpecFilenameError(spack.error.SpecError):
-    """Raised when a spec file name is invalid."""
-
-
-class NoSuchSpecFileError(SpecFilenameError):
+class NoSuchSpecFileError(spack.error.SpecError):
     """Raised when a spec file doesn't exist."""
 
 
