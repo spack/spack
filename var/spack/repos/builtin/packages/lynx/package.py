ef18e8476e6c6c9e42667334a5d5caee7f117c8b
darmac <xiaojun2@hisilicon.com>
2020-06-17 06:43:29 +0800

    refine lynx version and remove version 2.8.8&2.8.7

--- var/spack/repos/builtin/packages/lynx/package.py
+++ var/spack/repos/builtin/packages/lynx/package.py
@@ -12,8 +12,18 @@ class Lynx(AutotoolsPackage):
     homepage = "https://lynx.invisible-island.net/"
     url      = "https://invisible-mirror.net/archives/lynx/tarballs/lynx2.8.9rel.1.tar.gz"
 
-    version('2.8.9rel.1', sha256='a46e4167b8f02c066d2fe2eafcc5603367be0e3fe2e59e9fc4eb016f306afc8e')
-    version('2.8.8rel.1', sha256='28cdb16136eea8ca097d2994138519bfbbdfe63b7d10fee6a4908580e5f55fc5')
-    version('2.8.7rel.1', sha256='6570e3088c0ae22fbd48a528f4841a1f2b83f588c7d31c059c3bbbcd5c7e7298')
+    version('2.8.9.1', sha256='a46e4167b8f02c066d2fe2eafcc5603367be0e3fe2e59e9fc4eb016f306afc8e')
 
     depends_on('ncurses')
+
+    def url_for_version(self, version):
+        version_str = version.string
+        index = version_str.rfind('.')
+        tmp = list(version_str)
+        if index >= 0:
+            tmp.insert(index, 'rel')
+            version_str = ''.join(tmp)
+        else:
+            version_str = version
+        url = "https://invisible-mirror.net/archives/lynx/tarballs/lynx{0}.tar.gz".format(version_str)
+        return url
