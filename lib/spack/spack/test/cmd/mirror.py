414532ad683aa3196536ea52b9d20c50637aa4e9
Gregory <becker33@llnl.gov>
2020-06-16 12:47:30 -0700

    fix mirror tests that depend on config to use config fixture

--- lib/spack/spack/test/cmd/mirror.py
+++ lib/spack/spack/test/cmd/mirror.py
@@ -102,7 +102,7 @@ class MockMirrorArgs(object):
         self.exclude_specs = exclude_specs
 
 
-def test_exclude_specs(mock_packages):
+def test_exclude_specs(mock_packages, config):
     args = MockMirrorArgs(
         specs=['mpich'],
         versions_per_spec='all',
@@ -117,7 +117,7 @@ def test_exclude_specs(mock_packages):
     assert (not expected_exclude & set(mirror_specs))
 
 
-def test_exclude_file(mock_packages, tmpdir):
+def test_exclude_file(mock_packages, tmpdir, config):
     exclude_path = os.path.join(str(tmpdir), 'test-exclude.txt')
     with open(exclude_path, 'w') as exclude_file:
         exclude_file.write("""\
