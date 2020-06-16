ef2a697a8b9cedd1dfe704c653dbd6b40ca04058
Chris White <white238@llnl.gov>
2020-06-16 13:32:00 -0700

    Honor debug variant when using global compiler flags

--- var/spack/repos/builtin/packages/mfem/package.py
+++ var/spack/repos/builtin/packages/mfem/package.py
@@ -366,6 +366,19 @@ class Mfem(Package):
         cxxflags = spec.compiler_flags['cxxflags']
 
         if cxxflags:
+            # Add opt/debug flags if they are not present in global cxx flags
+            opt_flag_found = any(f in self.compiler.opt_flags for f in cxxflags)
+            debug_flag_found = any(f in self.compiler.debug_flags for f in cxxflags)
+
+            if '+debug' in spec:
+                if debug_flag_found:
+                    cxxflags.append('-g')
+                if opt_flag_found:
+                    cxxflags.append('-O0')
+            else:
+                if opt_flag_found:
+                    cxxflags.append('-O2')
+
             cxxflags = [(xcompiler + flag) for flag in cxxflags]
             if '+cuda' in spec:
                 cxxflags += [
