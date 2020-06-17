8e6f0c353cc40970051326ea9540beabb388baca
Glenn Johnson <glenn-johnson@uiowa.edu>
2020-06-16 22:35:03 -0500

    Add patch for older versions of py-tensorflow (#17122)
    Versions of py-tensorflow between versions 1.1 and 1.14 need a patch to
    avoid an import error on the cloud package even if built without support
    for the cloud package.

--- var/spack/repos/builtin/packages/py-tensorflow/package.py
+++ var/spack/repos/builtin/packages/py-tensorflow/package.py
@@ -255,6 +255,14 @@ class PyTensorflow(Package, CudaPackage):
     # Allows 2.0.* releases to build with '--config=nogcp'
     patch('0001-Remove-contrib-cloud-bigtable-and-storage-ops-kernel.patch',
           when='@2.0.0:2.0.1')
+    # do not import contrib.cloud if not available
+    patch('https://github.com/tensorflow/tensorflow/commit/ed62ac8203999513dfae03498e871ea35eb60cc4.patch',
+          sha256='c37d14622a86b164e2411ea45a04f756ac61b2044d251f19ab17733c508e5305', when='@1.14.0')
+    # import_contrib_cloud patch for older versions
+    patch('contrib_cloud_1.10.patch', when='@1.10:1.13')
+    patch('contrib_cloud_1.9.patch', when='@1.9')
+    patch('contrib_cloud_1.4.patch', when='@1.4:1.8')
+    patch('contrib_cloud_1.1.patch', when='@1.1:1.3')
 
     phases = ['configure', 'build', 'install']
 
