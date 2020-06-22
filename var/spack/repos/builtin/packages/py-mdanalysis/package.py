ae09b8fdf46e1ab99f9505438ec22edd32e4e121
Jen Herting <jen@herting.cc>
2020-06-22 12:51:24 -0400

    [py-mdanalysis] only requires mmtf-python after version 0.16.0 and requires version 1.0.0

--- var/spack/repos/builtin/packages/py-mdanalysis/package.py
+++ var/spack/repos/builtin/packages/py-mdanalysis/package.py
@@ -51,7 +51,7 @@ class PyMdanalysis(PythonPackage):
     depends_on('py-griddataformats@0.4:',   when='@0.17.0:', type=('build', 'run'))
     
     depends_on('py-gsd@1.4.0', when='@1.17.0:', type=('build', 'run'))
-    depends_on('py-mmtf-python', type=('build', 'run'))
+    depends_on('py-mmtf-python@1.0.0', when='@0.16.0:', type=('build', 'run'))
     depends_on('py-scikit-learn', type=('build', 'run'))
 
     depends_on('py-matplotlib', when='+analysis', type=('build', 'run'))
