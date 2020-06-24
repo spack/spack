9f1b25ecea4c5aa97f884e102608205d993b11f1
Doug Heckman <dahdco@rit.edu>
2020-06-24 13:20:54 -0400

    [py-crossmap] updated url and versions

--- var/spack/repos/builtin/packages/py-crossmap/package.py
+++ var/spack/repos/builtin/packages/py-crossmap/package.py
@@ -11,9 +11,10 @@ class PyCrossmap(PythonPackage, SourceforgePackage):
        (or annotation files) between different assemblies"""
 
     homepage = "http://crossmap.sourceforge.net/"
-    sourceforge_mirror_path = "crossmap/CrossMap-0.3.3.tar.gz"
+    url = "https://pypi.io/packages/source/c/crossmap/CrossMap-0.4.2.tar.gz"
 
-    version('0.3.3', sha256='56d99fd606e13e399b83438953d0d89fc281df1c1e8e47eed7d773e7ec9c88f8')
+    version('0.4.2', sha256='35e918bd6ff210d5156ec136b31df440e1783cfb96f6d2d4596e9803d64c3c14')
+    version('0.3.9', sha256='e20a4653e9fc313ac0f5a6cfc37b42e83c3cf2b42f9483706cfb9ec9ff72c74c')
     version('0.2.9', sha256='57243ee5051352c93088874c797ceac0426f249704ba897360fb628b3365d0af')
 
     depends_on('python@3:', type=('build', 'run'), when='@0.3.0:')
