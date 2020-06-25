5a6f8cf6713d5d43bed7c46277ab0e25c3d22490
Peter Scheibel <scheibel1@llnl.gov>
2020-06-25 06:27:27 -0700

    Fix global activation check for upstream extendees (#17231)
    * short-circuit is_activated check when the extendee is installed upstream
    
    * add test for checking activation status of packages with an extendee installed upstream

--- lib/spack/spack/package.py
+++ lib/spack/spack/package.py
@@ -1019,6 +1019,11 @@ class PackageBase(with_metaclass(PackageMeta, PackageViewMixin, object)):
         if not self.is_extension:
             raise ValueError(
                 "is_activated called on package that is not an extension.")
+        if self.extendee_spec.package.installed_upstream:
+            # If this extends an upstream package, it cannot be activated for
+            # it. This bypasses construction of the extension map, which can
+            # can fail when run in the context of a downstream Spack instance
+            return False
         extensions_layout = view.extensions_layout
         exts = extensions_layout.extension_map(self.extendee_spec)
         return (self.name in exts) and (exts[self.name] == self.spec)
