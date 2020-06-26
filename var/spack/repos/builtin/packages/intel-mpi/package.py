c8e6a9e5358f0b57dc88478e9b61aa1bb959b6ec
Adam J. Stewart <ajstewart426@gmail.com>
2020-06-26 14:04:35 -0500

    Fix super invocation in Intel packages (#17248)

--- var/spack/repos/builtin/packages/intel-mpi/package.py
+++ var/spack/repos/builtin/packages/intel-mpi/package.py
@@ -65,7 +65,7 @@ class IntelMpi(IntelPackage):
         })
 
     def setup_run_environment(self, env):
-        super(self, IntelMpi).setup_run_environment(env)
+        super(IntelMPI, self).setup_run_environment(env)
 
         for name, value in self.mpi_compiler.wrappers.items():
             env.set(name, value)
