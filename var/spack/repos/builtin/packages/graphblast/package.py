6092c5444dcc0c9e415af7788a58eb6a59084258
Sinan81 <Sinan81@github>
2020-06-29 20:29:13 -0700

    add variant description

--- var/spack/repos/builtin/packages/graphblast/package.py
+++ var/spack/repos/builtin/packages/graphblast/package.py
@@ -14,7 +14,7 @@ class Graphblast(MakefilePackage, CudaPackage):
 
     version('master', submodules=True)
 
-    variant('cuda', default=True)
+    variant('cuda', default=True, description="Build with Cuda support")
 
     depends_on('boost +program_options')
 
