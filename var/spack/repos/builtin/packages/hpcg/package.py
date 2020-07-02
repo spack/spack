bbc4480e7da95c09f11aedd54fae964de1ba6fae
yellowhat <yellowhat46@gmail.com>
2020-07-02 21:35:52 +0100

    Update var/spack/repos/builtin/packages/hpcg/package.py
    Co-authored-by: Michael Kuhn <michael@ikkoku.de>

--- var/spack/repos/builtin/packages/hpcg/package.py
+++ var/spack/repos/builtin/packages/hpcg/package.py
@@ -30,7 +30,8 @@ class Hpcg(AutotoolsPackage):
     build_targets = ['arch={0}'.format(arch)]
 
     def configure(self, spec, prefix):
-        CXXFLAGS = '-O3 -ffast-math -ftree-vectorize -ftree-vectorizer-verbose=0 '
+        CXXFLAGS = '-O3 -ffast-math '
+        CXXFLAGS += '-ftree-vectorize -ftree-vectorizer-verbose=0 '
         if '+openmp' in self.spec:
             CXXFLAGS += self.compiler.openmp_flag
         config = [
