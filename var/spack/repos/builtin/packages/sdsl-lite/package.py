2dabd63007c4761b582f8b0370707e2c3d625bba
Kenta Tsubouchi <kenta.tsubouchi@hac-inc.co.jp>
2020-07-02 14:02:09 +0900

    sasl-lite: use self.spec.satisfies

--- var/spack/repos/builtin/packages/sdsl-lite/package.py
+++ var/spack/repos/builtin/packages/sdsl-lite/package.py
@@ -32,7 +32,7 @@ class SdslLite(Package):
         tar('-xvf', self.stage.archive_file)
 
         with working_dir('sdsl-lite-{0}'.format(spec.version.dotted)):
-            if '%fj' in spec:
+            if self.spec.satisfies('%fj'):
                 filter_file(
                     'stdlib=libc',
                     'stdlib=libstdc',
