a48549b4596518968a3c17b6b8a662871fc176e7
Matthias Wolf <m+git@sushinara.net>
2020-07-08 12:56:00 +0200

    spykfunc: one more mention of py-bb5 to fix. (#859)

--- var/spack/repos/builtin/packages/spykfunc/package.py
+++ var/spack/repos/builtin/packages/spykfunc/package.py
@@ -70,8 +70,9 @@ class Spykfunc(PythonPackage):
         env.set('SPARK_HOME', self.spec['spark'].prefix)
         env.set('HADOOP_HOME', self.spec['hadoop'].prefix)
 
-        env.prepend_path('PATH',
-                         os.path.join(self.spec['py-bb5'].prefix, 'bin'))
+        if self.spec.satisfies('@:0.15.6'):
+            env.prepend_path('PATH',
+                             os.path.join(self.spec['py-bb5'].prefix, 'bin'))
         env.prepend_path('PATH',
                          os.path.join(self.spec['py-sparkmanager'].prefix,
                                       'bin'))
