a3dc2ca2632d2a3e59d5dc998c289e99cf24f602
Julius Plehn <juplehn@me.com>
2020-07-09 00:33:07 +0200

    changa cleanup

--- var/spack/repos/builtin/packages/changa/package.py
+++ var/spack/repos/builtin/packages/changa/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install changa
-#
-# You can edit this file again by typing:
-#
-#     spack edit changa
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 import os
 
@@ -35,10 +18,6 @@ class Changa(AutotoolsPackage):
     url      = "https://github.com/N-BodyShop/changa/archive/v3.4.tar.gz"
     git      = "https://github.com/N-BodyShop/changa.git"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('3.4', sha256='c2bceb6ac00025dfd704bb6960bc17c6df7c746872185845d1e75f47e6ce2a94')
     patch("fix_configure_path.patch")
 
@@ -50,13 +29,6 @@ class Changa(AutotoolsPackage):
             )
 
     depends_on("charmpp build-target=ChaNGa")
-    #depends_on("libtirpc")
-    """
-    @run_before('configure')
-    def cleanup(self):
-        if os.path.isfile('Makefile'):
-            make('dist-clean')
-    """
 
     def configure_args(self):
         args = []
