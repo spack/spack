d10ad73f1871a3ecd6abb739c8058725b9b5b453
Matthias Wolf <m+git@sushinara.net>
2020-07-08 09:07:17 +0200

    py-dask: updates all around, add py-dask-mpi, too. (#856)

---
+++ var/spack/repos/builtin/packages/py-dask-mpi/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyDaskMpi(PythonPackage):
+    """Deploying Dask using MPI4Py."""
+
+    homepage = "https://github.com/dask/dask-mpi"
+    url      = "https://pypi.io/packages/source/d/dask-mpi/dask-mpi-2.0.0.tar.gz"
+
+    import_modules = ['dask_mpi']
+
+    version('2.0.0', sha256='774cd2d69e5f7154e1fa133c22498062edd31507ffa2ea19f4ab4d8975c27bc3')
+
+    depends_on('py-dask@2.2:', type=('build', 'run'))
+    depends_on('py-mpi4py', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
