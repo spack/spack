4a14011e7d60ca9881e39d877692288d9971a247
Hadrien Grasland <grasland@lal.in2p3.fr>
2020-07-10 14:13:40 +0200

    Hello xext my old friend...

--- var/spack/repos/builtin/packages/root/package.py
+++ var/spack/repos/builtin/packages/root/package.py
@@ -449,9 +449,10 @@ class Root(CMakePackage):
         return options
 
     def setup_build_environment(self, env):
-        if 'lz4' in self.spec:
-            env.append_path('CMAKE_PREFIX_PATH',
-                            self.spec['lz4'].prefix)
+        spec = self.spec
+
+        if 'lz4' in spec:
+            env.append_path('CMAKE_PREFIX_PATH', spec['lz4'].prefix)
 
         # This hack is made necessary by a header name collision between
         # asimage's "import.h" and Python's "import.h" headers...
@@ -465,20 +466,20 @@ class Root(CMakePackage):
         # into SPACK_INCLUDE_DIRS, even in a deprioritized form, because some
         # system/compiler combinations don't like having -I/usr/include around.
         def add_include_path(dep_name):
-            include_path = self.spec[dep_name].prefix.include
+            include_path = spec[dep_name].prefix.include
             if not is_system_path(include_path):
                 env.append_path('SPACK_INCLUDE_DIRS', include_path)
 
         # With that done, let's go fixing those deps
-        if self.spec.satisfies('+x @:6.08.99'):
-            add_include_path('xextproto')
-        if self.spec.satisfies('@:6.12.99'):
+        if spec.satisfies('@:6.12.99'):
             add_include_path('zlib')
-        if '+x' in self.spec:
+        if '+x' in spec:
+            if spec.satisfies('@:6.08.99') or spec.satisfies('@6.22:'):
+                add_include_path('xextproto')
             add_include_path('fontconfig')
             add_include_path('libx11')
             add_include_path('xproto')
-        if '+opengl' in self.spec:
+        if '+opengl' in spec:
             add_include_path('glew')
             add_include_path('mesa-glu')
 
