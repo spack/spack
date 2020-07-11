6f153ce2c4b6fa6dbf55f14b2c7ee9fb9c14ed90
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-07-10 17:40:46 -0700

    Updates to chainmap hashes.

--- var/spack/repos/builtin/packages/py-chainmap/package.py
+++ var/spack/repos/builtin/packages/py-chainmap/package.py
@@ -14,7 +14,7 @@ class PyChainmap(PythonPackage):
     homepage = "https://bitbucket.org/jeunice/chainmap/src/default/"
     url      = "https://pypi.io/packages/source/c/chainmap/chainmap-1.0.3.tar.gz"
 
-    version('1.0.3', sha256='22b6ccf38698c0356e65e5dfa49c98fb16e3408928b95d90e2332e2fb787988e')
+    version('1.0.3', sha256='e42aaa4b3e2f66102a11bfd563069704bfbfd84fdcb517b564effd736bf53cd9')
     version('1.0.2', sha256='405da3bce9913bfb33e6e497803b447b60d12ab44031ca357626143e087e0526')
     version('1.0.1', sha256='c1cd76c679dd7af982ec5a45788ef4a0dfc20e77f27ccdeca289a5141862ff64')
     version('1.0.0', sha256='2e24b2efa3494b16772282812efcd712b1d6b80e1b761a56b1b7cbc8fe0313c4')
