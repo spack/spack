0fb024eab5aed246883b0a278f9a283c1c32a88f
Julius Plehn <juplehn@me.com>
2020-07-12 15:15:20 +0200

    grads & shapelib package

--- var/spack/repos/builtin/packages/grads/package.py
+++ var/spack/repos/builtin/packages/grads/package.py
@@ -24,18 +24,27 @@ from spack import *
 
 
 class Grads(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+    """The Grid Analysis and Display System (GrADS) is an interactive
+    desktop tool that is used for easy access, manipulation, and visualization
+    of earth science data. GrADS has two data models for handling gridded and
+    station data. GrADS supports many data file formats, including
+    binary (stream or sequential), GRIB (version 1 and 2), NetCDF,
+    HDF (version 4 and 5), and BUFR (for station data)."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "http://cola.gmu.edu/grads/grads.php"
     url      = "ftp://cola.gmu.edu/grads/2.2/grads-2.2.1-src.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('2.2.1', sha256='695e2066d7d131720d598bac0beb61ac3ae5578240a5437401dc0ffbbe516206')
 
+    variant('hdf5', default=False, description="Enable HDF5 support")
+    variant('netcdf', default=False, description="Enable NetCDF support")
+    variant('geotiff', default=False, description="Enable GeoTIFF support")
+    variant('shapefile', default=False, description="Enable Shapefile support")
+
+    depends_on('hdf5', when='+hdf5')
+    depends_on('netcdf-c', when='+netcdf')
+    depends_on('libgeotiff', when='+geotiff')
+    depends_on('shapelib', when='+shapefile')
     depends_on('libgd')
     depends_on('libxmu')
     depends_on('cairo +X +pdf +fc +ft')
@@ -45,21 +54,15 @@ class Grads(AutotoolsPackage):
     def setup_environment(self, spack_env, run_env):
         spack_env.set('SUPPLIBS', '/')
         run_env.set('GADDIR', join_path(self.prefix, 'data'))
-    
+
     @run_after('install')
     def copy_data(self):
         with working_dir(self.build_directory):
             install_tree('data', join_path(self.prefix, 'data'))
         with working_dir(self.package_dir):
             copy('udpt', join_path(self.prefix, 'data'))
-            filter_file(r'({lib})', join_path(self.prefix, 'lib'), join_path(self.prefix, 'data/udpt'))
-
-    """
-    def configure_args(self):
-        args = []
-        args.append('--exec-prefix={0}'.format(self.prefix))
-        args.append('--enable-dyn-supplibs=yes')
-        args.append('--with-gnu-ld')
-        args.append('--with-sysroot=/')
-        return args
-    """
+            filter_file(
+                r'({lib})',
+                join_path(self.prefix, 'lib'),
+                join_path(self.prefix, 'data/udpt')
+            )
