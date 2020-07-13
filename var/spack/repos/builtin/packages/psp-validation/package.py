205596262505a824d451b0c9072c8a6e38ca6944
asanin-epfl <53935643+asanin-epfl@users.noreply.github.com>
2020-07-13 15:59:49 +0200

    psp-validation@0.3.0 (#869)

--- var/spack/repos/builtin/packages/psp-validation/package.py
+++ var/spack/repos/builtin/packages/psp-validation/package.py
@@ -13,6 +13,7 @@ class PspValidation(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/nse/psp-validation"
 
     version('develop', branch='master')
+    version('0.3.0', tag='psp-validation-v0.3.0')
     version('0.2.1', tag='psp-validation-v0.2.1')
     version('0.2.0', tag='psp-validation-v0.2.0')
     version('0.1.19', tag='psp-validation-v0.1.19')
@@ -28,7 +29,7 @@ class PspValidation(PythonPackage):
     depends_on('py-joblib@0.13:', type='run')
     depends_on('py-numpy@1.10:', type='run')
     depends_on('py-tqdm@4.0:', type='run')
-    depends_on('py-bglibpy@4.0:', type='run')
+    depends_on('py-bglibpy@4.2:', type='run')
     depends_on('py-bluepy@0.14.3:', type='run')
     depends_on('py-efel@3.0.39:', type='run')
 
