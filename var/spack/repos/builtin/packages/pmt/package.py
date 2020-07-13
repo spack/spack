99b74e8280c21d6f08198b25c5cf6157ab68208d
Cyrus Harrison <cyrush@llnl.gov>
2020-07-13 09:57:29 -0700

    add pmt package

---
+++ var/spack/repos/builtin/packages/pmt/package.py
@@ -0,0 +1,43 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Pmt(CMakePackage):
+    """A multi-runtime implementation of a distributed merge tree
+    segmentation algorithm. The implementation relies on the framework
+    BabelFlow, which allows to execute the algorithm on different runtime
+    systems."""
+
+    homepage = "https://bitbucket.org/cedmav/parallelmergetree"
+    url      = "https://bitbucket.org/cedmav/parallelmergetree/get/ascent.zip"
+
+    maintainers = ['spetruzza']
+
+    version('develop',
+            git='https://bitbucket.org/cedmav/parallelmergetree.git',
+            branch='ascent',
+            commit='6774ed74fd13b9747ac792978a676ce6e8b05cab',
+            submodules=True,
+            preferred=True)
+
+    depends_on('babelflow@develop')
+
+    variant("shared", default=True, description="Build ParallelMergeTree as shared libs")
+
+    def cmake_args(self):
+        args = []
+        args.append('-DLIBRARY_ONLY=ON')
+        return args
+
+    def cmake_install(self, spec, prefix):
+        if "+shared" in spec:
+            cmake_args.append('-DBUILD_SHARED_LIBS=ON')
+        else:
+            cmake_args.append('-DBUILD_SHARED_LIBS=OFF')
+
+        make()
+        make('install')
