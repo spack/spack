82475cc37b809bee2681fa659a01f37def1447a4
Adam J. Stewart <ajstewart426@gmail.com>
2020-07-18 14:38:31 -0500

    py-azure-cli: add new package

--- var/spack/repos/builtin/packages/py-argcomplete/package.py
+++ var/spack/repos/builtin/packages/py-argcomplete/package.py
@@ -9,9 +9,11 @@ from spack import *
 class PyArgcomplete(PythonPackage):
     """Bash tab completion for argparse."""
 
-    homepage = "https://pypi.python.org/pypi/argcomplete"
-    url      = "https://pypi.io/packages/source/a/argcomplete/argcomplete-1.1.1.tar.gz"
+    homepage = "https://github.com/kislyuk/argcomplete"
+    url      = "https://pypi.io/packages/source/a/argcomplete/argcomplete-1.12.0.tar.gz"
 
-    version('1.1.1', sha256='cca45b5fe07000994f4f06a0b95bd71f7b51b04f81c3be0b4ea7b666e4f1f084')
+    version('1.12.0', sha256='2fbe5ed09fd2c1d727d4199feca96569a5b50d44c71b16da9c742201f7cc295c')
+    version('1.1.1',  sha256='cca45b5fe07000994f4f06a0b95bd71f7b51b04f81c3be0b4ea7b666e4f1f084')
 
     depends_on('py-setuptools', type='build')
+    depends_on('py-importlib-metadata@0.23:1.999', when='@1.12: ^python@:3.7', type=('build', 'run'))
--- var/spack/repos/builtin/packages/py-azure-mgmt-containerregistry/package.py
+++ var/spack/repos/builtin/packages/py-azure-mgmt-containerregistry/package.py
@@ -10,7 +10,9 @@ class PyAzureMgmtContainerregistry(PythonPackage):
     homepage = "https://github.com/Azure/azure-sdk-for-python"
     url      = "https://pypi.io/packages/source/a/azure-mgmt-containerregistry/azure-mgmt-containerregistry-2.8.0.zip"
 
-    version('2.8.0', sha256='b24be1050d54f3158e8be7f6ad677f0c8888dddefd09fb8391ebfc73d40173a4')
+    # Release candidate needed for py-azure-cli
+    version('3.0.0rc14', sha256='d23ce93ec5903d00f79f0ac995e16bf47197130239f7f182509add3277b73071')
+    version('2.8.0', sha256='b24be1050d54f3158e8be7f6ad677f0c8888dddefd09fb8391ebfc73d40173a4', preferred=True)
 
     depends_on('py-setuptools', type='build')
     depends_on('py-msrest@0.5.0:', type=('build', 'run'))
