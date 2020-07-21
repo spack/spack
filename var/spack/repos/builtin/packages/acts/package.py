b81339cf809ea8f94931e91362a5ad83770bd8e7
Hadrien G <grasland@lal.in2p3.fr>
2020-07-21 19:56:18 +0200

    [acts] Add 0.27.x series (#17621)
    Add acts v0.27 and v0.27.1

--- var/spack/repos/builtin/packages/acts/package.py
+++ var/spack/repos/builtin/packages/acts/package.py
@@ -35,6 +35,8 @@ class Acts(CMakePackage, CudaPackage):
 
     # Supported Acts versions
     version('master', branch='master')
+    version('0.27.1', commit='8ba3010a532137bc0ab6cf83a38b483cef646a01')
+    version('0.27.0', commit='f7b1a1c27d5a95d08bb67236ad0e117fcd1c679f')
     version('0.26.0', commit='cf542b108b31fcc349fc18fb0466f889e4e42aa6')
     version('0.25.2', commit='76bf1f3e4be51d4d27126b473a2caa8d8a72b320')
     version('0.25.1', commit='6e8a1ea6d2c7385a78e3e190efb2a8a0c1fa957f')
