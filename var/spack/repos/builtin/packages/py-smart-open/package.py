6ff34e75065a62b205743d2bf9ba6e951f47ebdc
Jen Herting <jen@herting.cc>
2020-07-21 14:28:27 -0400

    [py-smart-open] version 1.8.4 requires py-boto

--- var/spack/repos/builtin/packages/py-smart-open/package.py
+++ var/spack/repos/builtin/packages/py-smart-open/package.py
@@ -21,5 +21,6 @@ class PySmartOpen(PythonPackage):
     depends_on('py-setuptools', type='build')
     depends_on('py-requests', type=('build', 'run'))
     depends_on('py-boto3', type=('build', 'run'))
+    depends_on('py-boto', when='@1.8.4', type=('build', 'run'))
     depends_on('py-google-cloud-storage', type=('build', 'run'))
     depends_on('py-bz2file', when='^python@:2', type=('build', 'run'))
