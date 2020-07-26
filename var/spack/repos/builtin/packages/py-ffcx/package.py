d6c84b5df7a975eba0f78325284e69cf13c20e2e
Jeffrey Salmond <jeffrey@cambridgesupercomputing.com>
2020-07-26 10:46:20 +0100

    add py-ffcx package from fenics

---
+++ var/spack/repos/builtin/packages/py-ffcx/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyFfcx(PythonPackage):
+    """Next generation FEniCS Form Compiler"""
+
+    homepage = "https://github.com/FEniCS/ffcx"
+    git = "https://github.com/FEniCS/ffcx.git"
+
+    version("master", branch="master")
+
+    extends("python")
+    depends_on("py-setuptools")
+    depends_on("py-cffi")
+    depends_on("py-ufl")
+    depends_on("py-fiat")
+    depends_on("py-numpy")
