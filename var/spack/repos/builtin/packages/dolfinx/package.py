ca6f960b72de7b117884f3af94c9cffb7592526e
Jeffrey Salmond <jeffrey@cambridgesupercomputing.com>
2020-07-28 15:50:00 +0100

    setup variants complex and int64 of dolfinx

--- var/spack/repos/builtin/packages/dolfinx/package.py
+++ var/spack/repos/builtin/packages/dolfinx/package.py
@@ -20,31 +20,45 @@ class Dolfinx(CMakePackage):
     variant("parmetis", default=False, description="parmetis support")
     variant("slepc", default=False, description="slepc support")
 
+    variant("int64", default=False, description="use 64 bit integers for indexes")
+    variant("complex", default=False, description="use complex numbers")
+
     depends_on("cmake@3.9:", type="build")
     depends_on("pkgconfig")
     depends_on("mpi")
     depends_on("hdf5+hl+fortran")
     depends_on("boost")
     depends_on("eigen")
-    depends_on("petsc+mpi+shared+hypre+metis")
+    depends_on("petsc+mpi+shared+hypre+metis ~int64~complex", when="~int64~complex")
+    depends_on("petsc+mpi+shared+hypre+metis +int64~complex", when="+int64~complex")
+    depends_on("petsc+mpi+shared+hypre+metis ~int64+complex", when="~int64+complex")
+    depends_on("petsc+mpi+shared+hypre+metis +int64+complex", when="+int64+complex")
     depends_on("scotch+mpi")
 
     depends_on("kahip", when="+kahip")
     depends_on("parmetis", when="+parmetis")
     depends_on("slepc", when="+slepc")
 
-    depends_on("python@3.5:")
-    depends_on("py-ufl")
     depends_on("py-ffcx")
-    depends_on("py-fiat")
     depends_on("py-sphinx@1.0.1:", when="+doc", type="build")
 
     root_cmakelists_dir = "cpp"
 
     def cmake_args(self):
-        return [
+        args = [
             "-DDOLFINX_SKIP_BUILD_TESTS=True",
             "-DDOLFINX_ENABLE_KAHIP=%s"%('ON' if "+kahip" in self.spec else 'OFF'),
             "-DDOLFINX_ENABLE_PARMETIS=%s"%('ON' if "+parmetis" in self.spec else 'OFF'),
             "-DDOLFINX_ENABLE_SLEPC=%s"%('ON' if "+slepc" in self.spec else 'OFF'),
         ]
+
+        if "platform=darwin" in self.spec:
+            args += [
+                "-DCMAKE_SKIP_BUILD_RPATH=FALSE",
+                "-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE",
+                "-DCMAKE_INSTALL_RPATH=",
+                "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE",
+            ]
+
+        return args
+
