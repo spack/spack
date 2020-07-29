e4473588b96330384b7bdb7ee824c8225f180098
Jen Herting <jen@herting.cc>
2020-07-29 16:41:02 -0400

    [py-google-drive-downloader] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-google-drive-downloader/package.py
+++ var/spack/repos/builtin/packages/py-google-drive-downloader/package.py
@@ -3,48 +3,14 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-google-drive-downloader
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-google-drive-downloader
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyGoogleDriveDownloader(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Minimal class to download shared files from Google
+    Drive."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/ndrplz/google-drive-downloader"
     url      = "https://github.com/ndrplz/google-drive-downloader/archive/0.2.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('0.2', sha256='093d37242b632aa6204a4ffb7f41d44108c2b0687895b717003440385493d3e6')
-
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
