a5578eaf66e192a1d7beb91d980c052ea777884c
Wouter Deconinck <wdconinc@gmail.com>
2020-07-29 20:56:28 -0500

    [dire] Remove older versions (same physics and more included in 2.004)
    Older versions do not compile correctly. New users should use 2.004,
    not any of the older versions.

--- var/spack/repos/builtin/packages/dire/package.py
+++ var/spack/repos/builtin/packages/dire/package.py
@@ -19,18 +19,12 @@ class Dire(Package):
     maintainer = ['mdiefent']
 
     version('2.004', sha256='8cc1213b58fec744fdaa50834560a14b141de99efb2c3e3d3d47f3d6d84b179f')
-    version('2.003', sha256='98ee082718504c0f514a5c377310c4a2a34bb9625c999d3690d92342e52b532a')
-    version('2.002', sha256='7fba480bee785ddacd76446190df766d74e61a3c5969f362b8deace7d3fed8c1')
-    version('2.001', sha256='d9d5f8ff6829c51fefc008a78f4fa0ac3f4be99cd2a03ef01ca9fb84f4319836')
-    version('2.000', sha256='ce30477474709496d3c9a31806c12872fb003cdeec412ec4027245da6aa8b40b')
-    version('1.500', sha256='3b6e711a8b161e60f84168a3560bf71b3cf89f566f3b536631208e475fdc512f')
 
     depends_on('zlib')
     depends_on('boost')
     depends_on('lhapdf')
     depends_on('hepmc')
-    depends_on('pythia8@8212:', when='@:2.000')
-    depends_on('pythia8@8226:', when='@2.001:')
+    depends_on('pythia8@8226:', when='@2.004:')
 
     def install(self, spec, prefix):
         configure_args = ['--prefix={0}'.format(prefix)]
