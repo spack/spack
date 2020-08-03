40acec8f2e028ec2bd7ccc5c43ad53a525d74bfb
Claire Guilbaud <34576189+Bidibulke@users.noreply.github.com>
2020-08-03 11:16:15 +0200

    Update var/spack/repos/builtin/packages/py-colorspacious/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-colorspacious/package.py
+++ var/spack/repos/builtin/packages/py-colorspacious/package.py
@@ -16,4 +16,5 @@ class PyColorspacious(PythonPackage):
 
     version('1.1.2', sha256='5e9072e8cdca889dac445c35c9362a22ccf758e97b00b79ff0d5a7ba3e11b618')
 
-    depends_on('py-numpy')
+    depends_on('py-setuptools', type='build')
+    depends_on('py-numpy', type=('build', 'run'))
