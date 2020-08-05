440417bb1d58221a5ce37fc21a6588493f88d491
Matthias Wolf <matthias.wolf@epfl.ch>
2020-08-05 15:57:00 +0200

    py-datrie: bump to version that can handle a real Python

--- var/spack/repos/builtin/packages/py-datrie/package.py
+++ var/spack/repos/builtin/packages/py-datrie/package.py
@@ -13,6 +13,7 @@ class PyDatrie(PythonPackage):
     homepage = "https://github.com/kmike/datrie"
     url      = "https://pypi.io/packages/source/d/datrie/datrie-3.11.2.tar.gz"
 
+    version('0.8.2', '525b08f638d5cf6115df6ccd818e5a01298cd230b2dac91c8ff2e6499d18765d')
     version('0.8', 'bdd5da6ba6a97e7cd96eef2e7441c8d2ef890d04ba42694a41c7dffa3aca680c')
 
     depends_on('py-setuptools', type=('build', 'run'))
