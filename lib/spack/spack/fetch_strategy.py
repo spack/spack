1dfaeac62cf05bd4e985001e6c4cc6b0c6deae45
Peter Josef Scheibel <scheibel1@llnl.gov>
2020-08-07 16:40:45 -0700

    most code changes to allow pickling Spec/Package can be undone now that the spec is passed as a dictionary rather than an object

--- lib/spack/spack/fetch_strategy.py
+++ lib/spack/spack/fetch_strategy.py
@@ -214,16 +214,13 @@ class BundleFetchStrategy(FetchStrategy):
         """BundlePackages don't have a mirror id."""
 
 
-class FetchStrategyComposite(pattern.Composite):
+@pattern.composite(interface=FetchStrategy)
+class FetchStrategyComposite(object):
     """Composite for a FetchStrategy object.
+
+    Implements the GoF composite pattern.
     """
     matches = FetchStrategy.matches
-
-    def __init__(self):
-        super(FetchStrategyComposite, self).__init__([
-            'fetch', 'check', 'expand', 'reset', 'archive', 'cachable',
-            'mirror_id'
-        ])
 
     def source_id(self):
         component_ids = tuple(i.source_id() for i in self)
