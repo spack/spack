1db5239d65a6cb841a39d46875abe872f9e5b883
Sinan81 <sbulut@3vgeomatics.com>
2020-08-07 15:32:12 -0700

    flake8, improve dependency specifics

--- var/spack/repos/builtin/packages/py-cmake/package.py
+++ var/spack/repos/builtin/packages/py-cmake/package.py
@@ -7,12 +7,14 @@ from spack import *
 
 
 class PyCmake(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """CMake is an open-source, cross-platform family of tools designed to
+    build, test and package software
+    """
 
     homepage = "https://www.example.com"
     url      = "https://files.pythonhosted.org/packages/bf/b6/bc83a846ac9ffbcb7f9490af135c42002e12f3adc8253c9e55c07de49cf3/cmake-3.18.0.tar.gz"
 
     version('3.18.0', sha256='52b98c5ee70b5fa30a8623e96482227e065292f78794eb085fdf0fecb204b79b')
 
-    depends_on('cmake@3.18.0', when='@3.18.0')
-    depends_on('py-scikit-build')
+    depends_on('cmake@3.18.0', type=('build', 'link', 'run'), when='@3.18.0')
+    depends_on('py-scikit-build', type='build')
