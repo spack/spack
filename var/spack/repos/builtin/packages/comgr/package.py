5a5009ffae1e4cfe44f006449fe066f983d65b31
Harmen Stoppels <harmenstoppels@gmail.com>
2020-08-07 13:48:51 +0200

    Fix flake8 errors

--- var/spack/repos/builtin/packages/comgr/package.py
+++ var/spack/repos/builtin/packages/comgr/package.py
@@ -21,10 +21,12 @@ class Comgr(CMakePackage):
     variant('build_type', default='Release', values=("Release", "Debug"), description='CMake build type')
 
     depends_on('cmake@3:', type='build')
+
     for ver in ['3.5.0']:
         depends_on('llvm-amdgpu@' + ver, type='build', when='@' + ver)
         depends_on('rocm-device-libs@' + ver, type='build', when='@' + ver)
-        depends_on('rocm-cmake@' + ver , type='build', when='@' + ver)
+        depends_on('rocm-cmake@' + ver, type='build', when='@' + ver)
+
     depends_on('zlib', type='link')
     depends_on('z3', type='link')
     depends_on('ncurses', type='link')
