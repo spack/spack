2ead3e2aea074c2a3a19273567e9872e52f82455
darmac <xiaojun2@hisilicon.com>
2020-08-08 18:34:38 +0800

    bmake: fix compile error and update version

--- var/spack/repos/builtin/packages/bmake/package.py
+++ var/spack/repos/builtin/packages/bmake/package.py
@@ -6,29 +6,42 @@
 from spack import *
 
 
-class Bmake(Package):
+class Bmake(AutotoolsPackage):
     """Portable version of NetBSD make(1)."""
 
     homepage = "http://www.crufty.net/help/sjg/bmake.htm"
     url      = "http://www.crufty.net/ftp/pub/sjg/bmake-20180512.tar.gz"
 
+    version('20200710', sha256='6538fc4319ef79d178dca76d3b869f7aa93a9bb7b510df08a7d872c01a56b76c')
     version('20180512', sha256='ac3cd262065fcc20c1dec7c95f06306c8138b3e17025b949343a06a8980a5508')
     version('20171207', sha256='1703667e53a0498c0903b20612ebcbb41b886a94b238624cfeadd91a4111d39a')
 
-    phases = ['configure', 'build', 'install']
-
-    def patch(self):
-        # Do not pre-roff cat pages
-        filter_file('MANTARGET?', 'MANTARGET', 'mk/man.mk', string=True)
-
-    def configure(self, spec, prefix):
-        sh = which('sh')
-        sh('boot-strap', 'op=configure')
+    depends_on('m4',       type='build')
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool',  type='build')
 
     def build(self, spec, prefix):
-        sh = which('sh')
-        sh('boot-strap', 'op=build')
+        bash = which('bash')
+        bash('./make-bootstrap.sh')
 
     def install(self, spec, prefix):
-        sh = which('sh')
-        sh('boot-strap', '--prefix={0}'.format(prefix), 'op=install')
+        mkdirp(prefix.bin)
+        install('bmake', prefix.bin)
+#    phases = ['configure', 'build', 'install']
+
+#    def patch(self):
+        # Do not pre-roff cat pages
+#        filter_file('MANTARGET?', 'MANTARGET', 'mk/man.mk', string=True)
+
+#    def configure(self, spec, prefix):
+#        sh = which('sh')
+#        sh('boot-strap', 'op=configure')
+
+#    def build(self, spec, prefix):
+#        sh = which('sh')
+#        sh('boot-strap', 'op=build')
+
+#    def install(self, spec, prefix):
+#        sh = which('sh')
+#        sh('boot-strap', '--prefix={0}'.format(prefix), 'op=install')
