8039dd415df9778ea10db9e52d72bff9bd24bf5d
Jen Herting <jen@herting.cc>
2020-08-11 15:56:05 -0400

    [py-torch-cluster] depends on scipy

--- var/spack/repos/builtin/packages/py-torch-cluster/package.py
+++ var/spack/repos/builtin/packages/py-torch-cluster/package.py
@@ -37,6 +37,7 @@ class PyTorchCluster(PythonPackage):
     version('1.5.7', sha256='71701d2f7f3e458ebe5904c982951349fdb60e6f1654e19c7e102a226e2de72e')
 
     depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-scipy', type=('build', 'run'))
     extends('py-torch')
 
     def setup_build_environment(self, env):
