3faa3f1853c2664dd6eb4327d49dc9de0271783e
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2020-08-11 09:36:58 +0200

    Fix loading of compiler modules on CRAY
    The modifications in 193e8333fa23a2e9b44d44a80e153d9a27033860
    introduced a bug in the loading of compiler modules, since a
    function that was expecting a list of string was just getting
    a string.
    
    This commit fixes the bug and adds an assertion to verify the
    prerequisite of the function.

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -642,7 +642,7 @@ def get_rpaths(pkg):
     # Second module is our compiler mod name. We use that to get rpaths from
     # module show output.
     if pkg.compiler.modules and len(pkg.compiler.modules) > 1:
-        rpaths.append(path_from_modules(pkg.compiler.modules[1]))
+        rpaths.append(path_from_modules([pkg.compiler.modules[1]]))
     return list(dedupe(filter_system_paths(rpaths)))
 
 
