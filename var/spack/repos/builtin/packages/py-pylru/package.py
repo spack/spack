a90d4fa2b73ecc7975dc111764606e4fba606daa
asanin-epfl <53935643+asanin-epfl@users.noreply.github.com>
2020-08-12 12:50:17 +0200

    add bluepy 0.14.12 version (#891)

--- var/spack/repos/builtin/packages/py-pylru/package.py
+++ var/spack/repos/builtin/packages/py-pylru/package.py
@@ -12,4 +12,7 @@ class PyPylru(PythonPackage):
     homepage = "https://github.com/jlhutch/pylru"
     url = "https://pypi.io/packages/source/p/pylru/pylru-1.1.0.tar.gz"
 
+    depends_on('py-setuptools', type=('build', 'run'))
+
+    version('1.2.0', sha256='492f934bb98dc6c8b2370c02c95c65516ddc08c8f64d27f70087eb038621d297')
     version('1.1.0', sha256='e03a3d354eb8fdfa11638698e8a1f06cd3b3a214ebc0a120c603a79290d9ebec')
