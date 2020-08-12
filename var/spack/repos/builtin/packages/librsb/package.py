d1fae90f848bedb810cafb4f54235aa968526746
Michele Martone <michelemartone@users.noreply.github.com>
2020-08-12 14:09:49 +0200

    librsb: added v1.2.0.9 (#18015)

--- var/spack/repos/builtin/packages/librsb/package.py
+++ var/spack/repos/builtin/packages/librsb/package.py
@@ -11,9 +11,10 @@ class Librsb(AutotoolsPackage):
     library for the Recursive Sparse Blocks format"""
 
     homepage = "http://librsb.sourceforge.net/"
-    url      = "http://download.sourceforge.net/librsb/librsb-1.2.0.8.tar.gz"
+    url      = "http://download.sourceforge.net/librsb/librsb-1.2.0.9.tar.gz"
     list_url = "https://sourceforge.net/projects/librsb/files/"
 
+    version('1.2.0.9',   'f421f5d572461601120933e3c1cfee2ca69e6ecc92cbb11baa4e86bdedd3d9fa')
     version('1.2.0.8',   '8bebd19a1866d80ade13eabfdd0f07ae7e8a485c0b975b5d15f531ac204d80cb')
 
     depends_on('zlib')
