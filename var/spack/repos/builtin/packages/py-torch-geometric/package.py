b6d42b638d39a431e91bf939c3807ebdd840a132
Jen Herting <jen@herting.cc>
2020-08-12 11:46:55 -0400

    [py-torch-geometric] setting TORCH_CUDA_ARCH_LIST

--- var/spack/repos/builtin/packages/py-torch-geometric/package.py
+++ var/spack/repos/builtin/packages/py-torch-geometric/package.py
@@ -38,3 +38,9 @@ class PyTorchGeometric(PythonPackage):
     depends_on('py-ase', type=('build', 'run'))
     depends_on('py-jinja2', type=('build', 'run'))
     depends_on('py-torch-sparse', type=('build', 'run'))
+    
+    def setup_build_environment(self, env):
+        cuda_arches = list(self.spec['py-torch'].variants['cuda_arch'].value)
+        for i, x in enumerate(cuda_arches):
+            cuda_arches[i] = '{0}.{1}'.format(x[0:-1], x[-1])
+        env.set('TORCH_CUDA_ARCH_LIST', str.join(' ', cuda_arches))
