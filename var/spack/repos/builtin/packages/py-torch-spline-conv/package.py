8b0b3b0dc3ac329ee566056c74dc527c741c5c13
Jen Herting <jen@herting.cc>
2020-08-13 14:12:43 -0400

    [py-torch-spline-conv] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-torch-spline-conv/package.py
+++ var/spack/repos/builtin/packages/py-torch-spline-conv/package.py
@@ -3,36 +3,15 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-torch-spline-conv
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-torch-spline-conv
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyTorchSplineConv(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This is a PyTorch implementation of the spline-based
+    convolution operator of SplineCNN."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rusty1s/pytorch_spline_conv"
     url      = "https://github.com/rusty1s/pytorch_spline_conv/archive/1.2.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.2.0', sha256='ab8da41357c8a4785662366655bb6dc5e84fd0e938008194955409aefe535009')
 
@@ -51,9 +30,3 @@ class PyTorchSplineConv(PythonPackage):
             env.set('CUDA_HOME', self.spec['cuda'].prefix)
         else:
             env.set('FORCE_CUDA', '0')
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
