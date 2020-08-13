4726a615300412e1daa6cf39d175038b5c35f37f
Adam J. Stewart <ajstewart426@gmail.com>
2020-08-12 19:50:59 -0500

    Redirect stderr too

--- var/spack/repos/builtin/packages/gmake/package.py
+++ var/spack/repos/builtin/packages/gmake/package.py
@@ -33,7 +33,7 @@ class Gmake(AutotoolsPackage, GNUMirrorPackage):
 
     @classmethod
     def determine_version(cls, exe):
-        output = Executable(exe)('--version', output=str)
+        output = Executable(exe)('--version', output=str, error=str)
         match = re.search(r'GNU Make (\S+)', output)
         return match.group(1) if match else None
 
