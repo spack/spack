2e647f511db8dc000a1024246399c14f1ec2bca7
iarspider <iarspider@gmail.com>
2020-08-13 15:43:51 +0200

    Add conflict statement (reported to devs)

--- var/spack/repos/builtin/packages/madgraph5amc/package.py
+++ var/spack/repos/builtin/packages/madgraph5amc/package.py
@@ -39,6 +39,8 @@ class Madgraph5amc(Package):
     depends_on('py-six', when='@2.7.3.py3')
 
     depends_on('python@:2.7.999', when='@2.7.3.py2')
+    conflicts('%gcc@10:', when='@2.7.3')
+
     depends_on('python@3:', when='@2.7.3.py3')
 
     patch('madgraph5amc-2.7.2.patch', level=0, when='@2.7.2~atlas')
