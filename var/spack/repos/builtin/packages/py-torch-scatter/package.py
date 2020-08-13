27014174af0ec75ae360956e1a23d60294021ee2
Jen Herting <jen@herting.cc>
2020-08-13 14:14:29 -0400

    [py-torch-scatter] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-torch-scatter/package.py
+++ var/spack/repos/builtin/packages/py-torch-scatter/package.py
@@ -3,36 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-torch-scatter
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-torch-scatter
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyTorchScatter(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This package consists of a small extension library of
+    highly optimized sparse update (scatter and segment)
+    operations for the use in PyTorch, which are missing in the
+    main package."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rusty1s/pytorch_scatter"
     url      = "https://github.com/rusty1s/pytorch_scatter/archive/2.0.5.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('2.0.5', sha256='e29b364beaa9c84a99e0e236be89ed19d4452d89010ff736184ddcce488b47f6')
 
@@ -53,9 +34,3 @@ class PyTorchScatter(PythonPackage):
             env.set('CUDA_HOME', self.spec['cuda'].prefix)
         else:
             env.set('FORCE_CUDA', '0')
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
