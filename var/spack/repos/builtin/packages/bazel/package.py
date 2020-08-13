78d76b76eeaac856f55a904c0d6b78d5c9efda24
Adam J. Stewart <ajstewart426@gmail.com>
2020-08-12 19:49:52 -0500

    Redirect stderr too

--- var/spack/repos/builtin/packages/bazel/package.py
+++ var/spack/repos/builtin/packages/bazel/package.py
@@ -141,7 +141,7 @@ class Bazel(Package):
 
     @classmethod
     def determine_version(cls, exe):
-        output = Executable(exe)('version', output=str)
+        output = Executable(exe)('version', output=str, error=str)
         match = re.search(r'Build label: ([\d.]+)', output)
         return match.group(1) if match else None
 
