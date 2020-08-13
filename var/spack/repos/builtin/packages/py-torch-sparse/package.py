33ede698c76237f494dffab56dd346fe27ff4a81
Jen Herting <jen@herting.cc>
2020-08-13 14:16:46 -0400

    [py-torch-sparse] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-torch-sparse/package.py
+++ var/spack/repos/builtin/packages/py-torch-sparse/package.py
@@ -3,36 +3,15 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-torch-sparse
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-torch-sparse
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyTorchSparse(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This package consists of a small extension library of
+    optimized sparse matrix operations with autograd support."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rusty1s/pytorch_sparse/"
     url      = "https://github.com/rusty1s/pytorch_sparse/archive/0.6.7.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.6.7', sha256='0d038a1502548692972a085cd0496460b5d2050bb7328427add990f081d6c44d')
 
@@ -54,9 +33,3 @@ class PyTorchSparse(PythonPackage):
             env.set('CUDA_HOME', self.spec['cuda'].prefix)
         else:
             env.set('FORCE_CUDA', '0')
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
