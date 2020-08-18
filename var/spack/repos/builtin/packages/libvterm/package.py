347690acf8fba9d3fb611acd19e3682549b27e78
Toyohisa Kameyama <kameyama@riken.jp>
2020-08-18 14:21:46 +0900

    libvterm: renumber version and add 1.0.3 neovim: build on aarrch64

--- var/spack/repos/builtin/packages/libvterm/package.py
+++ var/spack/repos/builtin/packages/libvterm/package.py
@@ -9,11 +9,22 @@ from spack import *
 class Libvterm(Package):
     """An abstract library implementation of a terminal emulator"""
     homepage = "http://www.leonerd.org.uk/code/libvterm/"
-    url      = "http://www.leonerd.org.uk/code/libvterm/libvterm-0+bzr681.tar.gz"
+    url      = "http://www.leonerd.org.uk/code/libvterm/libvterm-0.1.3.tar.gz"
 
-    version('681', sha256='abea46d1b0b831dec2af5d582319635cece63d260f8298d9ccce7c1c2e62a6e8')
+    version('0.1.3', preferred=True, sha256='e41724466a4658e0f095e8fc5aeae26026c0726dce98ee71d6920d06f7d78e2b')
+    version('0.0.726', sha256='6344eca01c02e2270348b79e033c1e0957028dbcd76bc784e8106bea9ec3029d')
+    version('0.0.681', sha256='abea46d1b0b831dec2af5d582319635cece63d260f8298d9ccce7c1c2e62a6e8')
 
     depends_on('libtool', type='build')
+
+    def url_for_version(self, version):
+        urlbase = "http://www.leonerd.org.uk/code/libvterm/"
+        if version < Version('0.1'):
+            snapshot = version.string.replace('0.0.', '')
+            url = urlbase + "libvterm-0+bzr{0}.tar.gz".format(snapshot)
+        else:
+            url = urlbase + "libvterm-{0}.tar.gz".format(version)
+        return url
 
     def install(self, spec, prefix):
         make()
