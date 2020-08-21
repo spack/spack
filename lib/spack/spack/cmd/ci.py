3169d90f66684cf800a8c4228531b95308f605a3
Peter Josef Scheibel <scheibel1@llnl.gov>
2020-08-21 11:47:11 -0700

    Revert "trigger rebuilds for all packages"
    This reverts commit afe050e477b3f214fdaf0144bcf8b9dde858eecb.

--- lib/spack/spack/cmd/ci.py
+++ lib/spack/spack/cmd/ci.py
@@ -11,6 +11,7 @@ from six.moves.urllib.parse import urlencode
 
 import llnl.util.tty as tty
 
+import spack.binary_distribution as bindist
 import spack.ci as spack_ci
 import spack.cmd.buildcache as buildcache
 import spack.environment as ev
@@ -281,7 +282,7 @@ def ci_rebuild(args):
         with open(root_spec_yaml_path, 'w') as fd:
             fd.write(spec_map['root'].to_yaml(hash=ht.build_hash))
 
-        if 1:
+        if bindist.needs_rebuild(job_spec, remote_mirror_url, True):
             # Binary on remote mirror is not up to date, we need to rebuild
             # it.
             #
