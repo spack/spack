512aa4015149689cffa50337c667cee1defdf73b
Jen Herting <jen@herting.cc>
2020-08-21 13:45:57 -0400

    [py-numba] added version 0.50.1 (#17880)
    * [py-numba] fixing upper limit for conditional dependency
    
    * [py-numba] added version 0.50.1
    
    * [py-numba] Specifying llvmlite version for new version

--- var/spack/repos/builtin/packages/py-numba/package.py
+++ var/spack/repos/builtin/packages/py-numba/package.py
@@ -12,12 +12,13 @@ class PyNumba(PythonPackage):
     homepage = "https://numba.pydata.org/"
     url      = "https://pypi.io/packages/source/n/numba/numba-0.35.0.tar.gz"
 
+    version('0.50.1', sha256='89e81b51b880f9b18c82b7095beaccc6856fcf84ba29c4f0ced42e4e5748a3a7')
     version('0.48.0', sha256='9d21bc77e67006b5723052840c88cc59248e079a907cc68f1a1a264e1eaba017')
     version('0.40.1', sha256='52d046c13bcf0de79dbfb936874b7228f141b9b8e3447cc35855e9ad3e12aa33')
     version('0.35.0', sha256='11564937757605bee590c5758c73cfe9fd6d569726b56d970316a6228971ecc3')
 
     depends_on('python@3.6:', type=('build', 'run'), when='@0.48.0:')
-    depends_on('python@3.3:3.7.9999', type=('build', 'run'), when='@0.40.1:')
+    depends_on('python@3.3:3.7.9999', type=('build', 'run'), when='@0.40.1:0.47.999')
     depends_on('python@3.3:3.6.9999', type=('build', 'run'), when='@:0.35.0')
     depends_on('py-numpy@1.15:', type=('build', 'run'), when='@0.48.0:')
     depends_on('py-numpy@1.10:1.99', type=('build', 'run'))
@@ -27,6 +28,7 @@ class PyNumba(PythonPackage):
     # That's why it was chosen as an upper bound in the following depends_on
     # calls.  If newer versions maintain backwards compatibility, the calls
     # can be updated accordingly.
+    depends_on('py-llvmlite@0.33:0.34', type=('build', 'run'), when='@0.50.1')
     depends_on('py-llvmlite@0.31.0:0.32.0', type=('build', 'run'), when='@0.48.0')
     depends_on('py-llvmlite@0.25:', type=('build', 'run'), when='@0.40.1:')
     depends_on('py-llvmlite@0.20:0.25', type=('build', 'run'), when='@0.35.1')
