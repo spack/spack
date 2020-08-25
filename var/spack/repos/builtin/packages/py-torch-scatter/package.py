4fd95ba8174551aeee1b5dfa0e85cb1994b8141f
Jen Herting <jen@herting.cc>
2020-08-25 14:34:27 -0400

    [py-torch-scatter] flake8

--- var/spack/repos/builtin/packages/py-torch-scatter/package.py
+++ var/spack/repos/builtin/packages/py-torch-scatter/package.py
@@ -25,7 +25,8 @@ class PyTorchScatter(PythonPackage):
 
     def setup_build_environment(self, env):
         if '+cuda' in self.spec:
-            cuda_arches = list(self.spec['py-torch'].variants['cuda_arch'].value)
+            cuda_arches = list(
+                self.spec['py-torch'].variants['cuda_arch'].value)
             for i, x in enumerate(cuda_arches):
                 cuda_arches[i] = '{0}.{1}'.format(x[0:-1], x[-1])
             env.set('TORCH_CUDA_ARCH_LIST', str.join(' ', cuda_arches))
