2cb1939daa65ad093c84a91833657482e7abe563
Adam J. Stewart <ajstewart426@gmail.com>
2020-08-25 08:53:26 -0500

    Update var/spack/repos/builtin/packages/py-tensorflow/package.py
    Co-authored-by: Andrew W Elble <aweits@rit.edu>

--- var/spack/repos/builtin/packages/py-tensorflow/package.py
+++ var/spack/repos/builtin/packages/py-tensorflow/package.py
@@ -532,7 +532,7 @@ class PyTensorflow(Package, CudaPackage):
         env.set('INCLUDEDIR', spec['protobuf'].prefix.include)
 
     def patch(self):
-        if spec.satisfies('@2.3.0'):
+        if self.spec.satisfies('@2.3.0'):
             filter_file('deps = protodeps + well_known_proto_libs(),',
                         'deps = protodeps,',
                         'tensorflow/core/platform/default/build_config.bzl',
