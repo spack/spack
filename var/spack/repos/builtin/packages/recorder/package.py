865a77fcf0fc212afb48b82920eac7cab473d9f8
Chen Wang <wangvsa@gmail.com>
2020-08-27 11:11:20 -0500

    New version: Recorder 2.1.5

--- var/spack/repos/builtin/packages/recorder/package.py
+++ var/spack/repos/builtin/packages/recorder/package.py
@@ -15,11 +15,12 @@ class Recorder(AutotoolsPackage):
 
     maintainers = ['wangvsa']
 
+    version('2.1.5', sha256='6d2f8b942f61da498e25327e79c1a25b2244f4f78a9cf5482fb4aaa32d7332a1')
     version('2.1.4', sha256='f66756595a7f310929c247ae03fd08a18d9843f578fffa1e3072f557bf5a158e')
 
-    variant('posix', default=True, description="Enable POSIX level tracing")
-    variant('mpi', default=True, description="Enable MPI level tracing. MPI is required even with this option turn off")
-    variant('hdf5', default=True, description="Enable HDF5 level tracing. HDF5 is required even with this option turn off.")
+    variant('posix', default=True, description="Enable POSIX level tracing.")
+    variant('mpi', default=True, description="Enable MPI level tracing. MPI is required even with this option disabled.")
+    variant('hdf5', default=True, description="Enable HDF5 level tracing. HDF5 is required even with this option disabled.")
 
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
