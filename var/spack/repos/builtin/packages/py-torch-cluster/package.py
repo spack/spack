acf5b74d9a403c260b865f738127e9d0eec4b5d0
Jen Herting <jen@herting.cc>
2020-08-27 15:58:45 -0400

    [py-torch-cluster] added py-setuptools and py-pytest-runner as dependencies

--- var/spack/repos/builtin/packages/py-torch-cluster/package.py
+++ var/spack/repos/builtin/packages/py-torch-cluster/package.py
@@ -19,6 +19,8 @@ class PyTorchCluster(PythonPackage):
     variant('cuda', default=False, description='Enables CUDA support')
 
     depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-pytest-runner', type='build')
     depends_on('py-scipy', type=('build', 'run'))
     depends_on('py-torch+cuda', when='+cuda')
     depends_on('py-torch~cuda', when='~cuda')
