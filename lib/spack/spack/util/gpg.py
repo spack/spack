97b54f3f54097fe10f0d6486b7313dd376d744ff
Omar Padron <omar.padron@kitware.com>
2020-08-28 14:04:53 -0400

    try even harder to work around gnupg issues

--- lib/spack/spack/util/gpg.py
+++ lib/spack/spack/util/gpg.py
@@ -145,6 +145,13 @@ class GpgConstants(object):
         if spack.version.Version(match.group(2)) < self.target_version:
             raise SpackGPGError(no_gpgconf_msg)
 
+        # ensure that the gpgconf we found can run "gpgconf --create-socketdir"
+        try:
+            exe('--dry-run', '--create-socketdir')
+        except spack.util.executable.ProcessError:
+            # no dice
+            exe_str = None
+
         return exe_str
 
     @cached_property
@@ -175,34 +182,50 @@ class GpgConstants(object):
 
     @cached_property
     def user_run_dir(self):
-        # Try to ensure that /run/user/$(id -u) exists so that
+        # Try to ensure that (/var)/run/user/$(id -u) exists so that
         #  `gpgconf --create-socketdir` can be run later.
         #
         # NOTE(opadron): This action helps prevent a large class of
         #                "file-name-too-long" errors in gpg.
-        var_run_user = '/run/user'
-        result = os.path.join(var_run_user, str(os.getuid()))
-        try:
-            mkdir = (os.path.isdir(var_run_user) and
-                     not os.path.exists(result))
-            if mkdir:
-                os.mkdir(result)
-                os.chmod(result, 0o700)
 
-        # If the above operation fails due to lack of permissions, then just
-        # carry on without running gpgconf and hope for the best.
-        #
-        # NOTE(opadron): Without a dir in which to create a socket for IPC,
-        #                gnupg may fail if GNUPGHOME is set to a path that is
-        #                too long, where "too long" in this context is actually
-        #                quite short; somewhere in the neighborhood of more
-        #                than 100 characters.
-        #
-        # TODO(opadron): Maybe a warning should be printed in this case?
-        except OSError as exc:
-            if exc.errno not in (errno.EPERM, errno.EACCES):
-                raise
-            result = None
+        try:
+            has_suitable_gpgconf = bool(GpgConstants.gpgconf_string)
+        except SpackGPGError:
+            has_suitable_gpgconf = False
+
+        # If there is no suitable gpgconf, don't even bother trying to
+        # precreate a user run dir.
+        if not has_suitable_gpgconf:
+            return None
+
+        result = None
+        for var_run_user in ('/run/user', '/var/run/user'):
+            user_dir = os.path.join(var_run_user, str(os.getuid()))
+            try:
+                mkdir = (os.path.isdir(var_run_user) and
+                         not os.path.exists(user_dir))
+                if mkdir:
+                    os.mkdir(user_dir)
+                    os.chmod(user_dir, 0o700)
+
+            # If the above operation fails due to lack of permissions, then
+            # just carry on without running gpgconf and hope for the best.
+            #
+            # NOTE(opadron): Without a dir in which to create a socket for IPC,
+            #                gnupg may fail if GNUPGHOME is set to a path that
+            #                is too long, where "too long" in this context is
+            #                actually quite short; somewhere in the
+            #                neighborhood of more than 100 characters.
+            #
+            # TODO(opadron): Maybe a warning should be printed in this case?
+            except OSError as exc:
+                if exc.errno not in (errno.EPERM, errno.EACCES):
+                    raise
+                user_dir = None
+
+            # return the last iteration that provides a usable user run dir
+            if user_dir is not None:
+                result = user_dir
 
         return result
 
