a94d3b79ecc07a70f8d307a15991f0778b59a183
Matthias Wolf <matthias.wolf@epfl.ch>
2020-08-28 15:35:45 +0200

    style: line length.

--- var/spack/repos/builtin/packages/brayns/package.py
+++ var/spack/repos/builtin/packages/brayns/package.py
@@ -57,7 +57,11 @@ class Brayns(CMakePackage):
     def patch(self):
         for cmake_filename in find(self.stage.source_path, "CMakeLists.txt"):
             filter_file(r'\$\{GLEW_LIBRARIES\}', 'GLEW', cmake_filename)
-            filter_file(r'-Werror', '-Werror -Wno-error=deprecated-copy', cmake_filename)
+            filter_file(
+                r'-Werror',
+                '-Werror -Wno-error=deprecated-copy',
+                cmake_filename
+            )
         if self.spec.satisfies('@1.0:'):
             filter_file(r'cast<const uint8_t \*const',
                         'cast<const uint8_t *',
