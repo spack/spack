f68f7dc89cc66092ba4c851f1da678cd60367e93
Matthias Wolf <matthias.wolf@epfl.ch>
2020-08-28 16:32:49 +0200

    py-tess: needs re-cythonizing

--- var/spack/repos/builtin/packages/py-tess/package.py
+++ var/spack/repos/builtin/packages/py-tess/package.py
@@ -15,4 +15,5 @@ class PyTess(PythonPackage):
     version('0.3.1', sha256='e48734ebe2bb096461d6ddafdda2792650400af1d7a90c98cef53bd6364916d2')
     version('0.3.0', sha256='68105859bf10c04bf2df73450f1f800fa842d1323be65a81ca447f2528fa5ad6')
 
+    depends_on('py-cython', type='build')
     depends_on('py-setuptools', type='build')
