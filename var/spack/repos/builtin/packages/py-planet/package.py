4f2076909bb01f8982ca6c59955731690118d07b
sbulut <sbulut@3vgeomatics.com>
2020-08-28 22:09:54 -0700

    specify checksum type

--- var/spack/repos/builtin/packages/py-planet/package.py
+++ var/spack/repos/builtin/packages/py-planet/package.py
@@ -11,7 +11,7 @@ class PyPlanet(PythonPackage):
 
     homepage = "https://github.com/planetlabs/planet-client-python"
     url      = "https://pypi.io/packages/source/p/planet/planet-1.4.6.tar.gz"
-    version('1.4.6', '43ff6a765f465302f500aaf65b81a46ac6aad7bb42899e4a7543bdc293d4ca0d')
+    version('1.4.6', sha256='43ff6a765f465302f500aaf65b81a46ac6aad7bb42899e4a7543bdc293d4ca0d')
 
     depends_on('py-setuptools', type='build')
     depends_on('py-click')
