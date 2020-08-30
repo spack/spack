b1a18d8c5c553cc4ccd8cbf4457486a0c8708370
Sinan <sbulutw@gmail.com>
2020-08-29 18:44:16 -0700

    Update var/spack/repos/builtin/packages/py-rios/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-rios/package.py
+++ var/spack/repos/builtin/packages/py-rios/package.py
@@ -20,4 +20,4 @@ class PyRios(PythonPackage):
     version('1.4.10', sha256='7f11b54eb1f2ec551d7fc01c039b60bf2c67f0c2fc5b2946f8d986d6a9bc7063')
 
     depends_on('py-numpy', type=('build', 'run'))
-    depends_on('gdal', type=('build', 'run'))
+    depends_on('gdal+python', type=('build', 'run'))
