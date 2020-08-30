8beda1f638dee5952bec87fac41f082439d4dd21
Sinan81 <Sinan81@earth>
2020-08-29 18:29:59 -0700

    add test deps

--- var/spack/repos/builtin/packages/py-update-checker/package.py
+++ var/spack/repos/builtin/packages/py-update-checker/package.py
@@ -15,6 +15,10 @@ class PyUpdateChecker(PythonPackage):
     version('0.18.0', sha256='6a2d45bb4ac585884a6b03f9eade9161cedd9e8111545141e9aa9058932acb13')
     version('0.17',   sha256='2def8db7f63bd45c7d19df5df570f3f3dfeb1a1f050869d7036529295db10e62')
 
-    depends_on('python@3.6:', type=('build', 'run'), when='@0.18.0:')
-    depends_on('python@2.7:2.8,3.3:', type=('build', 'run'), when='@0.17.0')
     depends_on('py-setuptools', type='build')
+    depends_on('py-pytest',     type='test')
+    depends_on('py-black',      type='test')
+    depends_on('py-flake8',     type='test')
+    depends_on('python@3.6:',           type=('build', 'run'), when='@0.18.0:')
+    depends_on('python@2.7:2.8,3.3:',   type=('build', 'run'), when='@0.17.0')
+    depends_on('py-requests@2.3.0',     type=('build', 'run'))
