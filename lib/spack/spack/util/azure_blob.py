43051beb8a9d9fa456965419f958ef3342f94f2f
Cormac Garvey <cogarvey@microsoft.com>
2020-09-01 14:34:05 -0500

    Corrected formatting errors

--- lib/spack/spack/util/azure_blob.py
+++ lib/spack/spack/util/azure_blob.py
@@ -3,7 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-"""This file contains the definition of the Azure Blob storage Class used to integrate Azure Blob storage with spack buildcache.
+"""This file contains the definition of the Azure Blob storage Class used to 
+   integrate Azure Blob storage with spack buildcache.
 """
 
 import os
@@ -13,23 +14,27 @@ from pathlib import Path
 
 import llnl.util.tty as tty
 
+
 class AzureBlob:
     def __init__(self, url):
         from azure.storage.blob import BlobServiceClient
         self.url = url
         (self.container_name, self.blob_path) = self.get_container_blob_path()
         if url.scheme != 'azure':
-           raise ValueError(
+            raise ValueError(
             'Can not create Azure blob connection from URL with scheme: {SCHEME}'.format(
                 SCHEME=url.scheme))
         if "AZURE_STORAGE_CONNECTION_STRING" in os.environ:
-           self.connect_str = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
-           self.blob_service_client = BlobServiceClient.from_connection_string(self.connect_str)
-           if not self.azure_container_exists():
-              tty.warn("The container {} does not exist, it will be created".format(self.container_name))
-              self.blob_service_client.create_container(self.container_name)
+            self.connect_str = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
+            self.blob_service_client = BlobServiceClient.from_connection_string(self.connect_str)
+            if not self.azure_container_exists():
+                tty.warn("The container {} does not exist, it will be created".format(self.container_name))
+                self.blob_service_client.create_container(self.container_name)
         else:
-           tty.error("Error: Environmental variable AZURE_STORAGE_CONNECTION_STRING is not defined, it is required if you want to use Azure Blob storage as an spack buildcache")
+            tty.error("Error: Environmental variable 
+                      AZURE_STORAGE_CONNECTION_STRING is not defined, it is 
+                      required if you want to use Azure Blob storage as an 
+                      spack buildcache")
            sys.exit(1)
 
     def get_container_blob_path(self):
@@ -43,16 +48,16 @@ class AzureBlob:
 
     def is_https(self):
         if 'https' in self.connect_str:
-           return True
+            return True
         else:
-           return False
+            return False
 
 
     def azure_container_exists(self):
         try:
-           ttt = self.blob_service_client.get_container_client(self.container_name).list_blobs()
-           for blob in ttt:
-               pass
+            ttt = self.blob_service_client.get_container_client(self.container_name).list_blobs()
+            for blob in ttt:
+                pass
         except Exception as ex:
             return False
         return True
@@ -95,14 +100,18 @@ class AzureBlob:
 
 
     def azure_url_sas(self):
-        from azure.storage.blob import ResourceTypes, AccountSasPermissions, generate_account_sas
+        from azure.storage.blob import ResourceTypes, AccountSasPermissions, 
+             generate_account_sas
         try:
-           sas_token = generate_account_sas(self.blob_service_client.account_name, 
-                       account_key=self.blob_service_client.credential.account_key, 
-                       resource_types=ResourceTypes(object=True), permission=AccountSasPermissions(read=True), 
-                       expiry=datetime.datetime.utcnow() + datetime.timedelta(minutes=5))
+            sas_token = generate_account_sas(
+                        self.blob_service_client.account_name, account_key=
+                        self.blob_service_client.credential.account_key, 
+                        resource_types=ResourceTypes(object=True), 
+                        permission=AccountSasPermissions(read=True), 
+                        expiry=datetime.datetime.utcnow() + 
+                        datetime.timedelta(minutes=5))
         except Exception as ex:
-           tty.error("{}, Could not generate a sas token for Azure blob storage".format(ex))
+            tty.error("{}, Could not generate a sas token for Azure blob storage".format(ex))
         url_str = self.url.geturl()
         url_str = url_str.replace('azure', 'https', 1)
         url_sas_str = url_str + '?' + sas_token 
