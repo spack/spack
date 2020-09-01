7e107101c388dd9003d478932519fad80c74545f
Matthias Wolf <m+git@sushinara.net>
2020-09-01 10:15:59 +0200

    parquet-converters: bump. (#903)

--- var/spack/repos/builtin/packages/parquet-converters/package.py
+++ var/spack/repos/builtin/packages/parquet-converters/package.py
@@ -14,6 +14,8 @@ class ParquetConverters(CMakePackage):
     git      = "ssh://bbpcode.epfl.ch/building/ParquetConverters"
 
     version('develop', submodules=True)
+    version('0.5.7', tag='v0.5.7', submodules=True)
+    version('0.5.6', tag='v0.5.6', submodules=True)
     version('0.5.5', tag='v0.5.5', submodules=True)
     version('0.5.4', tag='v0.5.4', submodules=True)
     version('0.5.3', tag='v0.5.3', submodules=True)
