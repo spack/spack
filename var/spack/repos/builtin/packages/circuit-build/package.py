2c8bdeeff148c28fb280adfd9bf7ecc518153012
asanin-epfl <53935643+asanin-epfl@users.noreply.github.com>
2020-09-03 16:36:04 +0200

    create circuit-build package (#909)

---
+++ var/spack/repos/builtin/packages/circuit-build/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class CircuitBuild(PythonPackage):
+    """Command Line API for building circuits"""
+
+    homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/common/circuit-build"
+    git      = "ssh://bbpcode.epfl.ch/common/circuit-build"
+
+    version('develop', branch='master')
+    version('3.1.1', tag='circuit-build-v3.1.1')
+
+    depends_on('py-setuptools', type=('build', 'run'))
+
+    depends_on('py-click@7.0:7.99', type='run')
+    depends_on('snakemake@5.6:5.99', type='run')
+    depends_on('py-pyyaml@5.0:', type='run')
