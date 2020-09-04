424dc60d601f79ecb90c304b4625eb3ea986d510
sreenivasa murthy kolam <sreenivasamurthy.kolam@amd.com>
2020-09-04 04:51:51 +0000

    changes to rocrand for 3.7.0

--- var/spack/repos/builtin/packages/rocrand/package.py
+++ var/spack/repos/builtin/packages/rocrand/package.py
@@ -16,11 +16,12 @@ class Rocrand(CMakePackage):
 
     maintainers = ['srekolam', 'arjun-raj-kuppala']
 
+    version('3.7.0', sha256='5e43fe07afe2c7327a692b3b580875bae6e6ee790e044c053fffafbfcbc14860')
     version('3.5.0', sha256='592865a45e7ef55ad9d7eddc8082df69eacfd2c1f3e9c57810eb336b15cd5732')
 
     depends_on('cmake@3.5.1:', type='build')
-
-    for ver in ['3.5.0']:
+    depends_on('numactl', when='@3.7.0')
+    for ver in ['3.5.0', '3.7.0']:
         depends_on('hip@' + ver, type='build', when='@' + ver)
         depends_on('comgr@' + ver, type='build', when='@' + ver)
         depends_on('rocm-device-libs@' + ver, type='build', when='@' + ver)
