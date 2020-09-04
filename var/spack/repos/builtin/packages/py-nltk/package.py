401fa1cac87b8db83f45912e042528cf9fa65420
Andrew Elble <aweits@rit.edu>
2020-09-04 13:52:19 -0400

    new package: py-textblob
    add variant to py-nltk to allow for data download/installation
    add dependencies to py-nltk so that bin/nltk works

--- var/spack/repos/builtin/packages/py-nltk/package.py
+++ var/spack/repos/builtin/packages/py-nltk/package.py
@@ -13,11 +13,28 @@ class PyNltk(PythonPackage):
 
     version('3.5', sha256='845365449cd8c5f9731f7cb9f8bd6fd0767553b9d53af9eb1b3abf7700936b35')
 
+    variant('data', default=False, description='Download the NLTK data')
+
     depends_on('python@3.5:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-click', type=('build', 'run'))
     depends_on('py-regex', type=('build', 'run'))
     depends_on('py-tqdm', type=('build', 'run'))
+    depends_on('py-importlib-metadata', type=('build', 'run'))
+
+    def setup_run_environment(self, run_env):
+        if '+data' in self.spec:
+            run_env.prepend_path("NLTK_DATA", self.prefix.data)
+
+    @run_after('install')
+    def install_data(self):
+        if '+data' in self.spec:
+            mkdirp(prefix.data)
+            python('-m',
+                   'nltk.downloader',
+                   '-d',
+                   prefix.data,
+                   'all')
 
     # May require additional third-party software:
     # https://github.com/nltk/nltk/wiki/Installing-Third-Party-Software
