869ca7b542575da4e2ef4d243434da37067c3573
Arjun Raj Kuppala <ArjunRaj.Kuppala@amd.com>
2020-09-03 17:53:25 -0700

    remove binutils dependency and update to devicelibs tar path

--- var/spack/repos/builtin/packages/aomp/package.py
+++ var/spack/repos/builtin/packages/aomp/package.py
@@ -27,7 +27,6 @@ class Aomp(Package):
     version('3.5.0', sha256=aomp_sha)
 
     depends_on('cmake@3.5.2:3.13.4', type='build')
-    depends_on('binutils', type='build')
     depends_on('rocm-device-libs@3.5:', type='build')
     depends_on('hsakmt-roct@3.5:', type='build')
     depends_on('hsa-rocr-dev@3.5:', type='build')
@@ -42,7 +41,7 @@ class Aomp(Package):
 
     resource(
         name='rocm-device-libs',
-        url=compute_url + '/ROCm-Device-Libs/archive/roc-3.5.0.tar.gz',
+        url=compute_url + '/ROCm-Device-Libs/archive/rocm-3.5.0.tar.gz',
         sha256=devlib_sha,
         expand=True,
         destination='aomp-dir',
@@ -269,8 +268,7 @@ class Aomp(Package):
             '-DCMAKE_CXX_COMPILER={0}/bin/clang++'.format(aomp_prefix),
             '-DCMAKE_C_COMPILER={0}/bin/clang'.format(aomp_prefix),
             '-DCMAKE_Fortran_COMPILER={0}/bin/flang'.format(aomp_prefix),
-            '-DLLVM_TARGETS_TO_BUILD=AMDGPU;x86',
-            '-DCMAKE_AR={0}/bin/ar'.format(self.spec['binutils'].prefix)
+            '-DLLVM_TARGETS_TO_BUILD=AMDGPU;x86'
         ]
 
         components['flang'] = [
