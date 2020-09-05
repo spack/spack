8b5d393466469cdba51199c8511731a9b800eded
Gregory Becker <becker33@llnl.gov>
2020-09-04 17:20:39 -0700

    flake

--- lib/spack/spack/test/cmd/config.py
+++ lib/spack/spack/test/cmd/config.py
@@ -399,7 +399,7 @@ def test_remove_list(mutable_empty_config):
 
 def test_config_add_to_env_preserve_comments(mutable_empty_config,
                                              mutable_mock_env_path):
-    env = ev.create('test')
+    ev.create('test')
     with ev.read('test'):
         config('add', 'config:dirty:true')
         output = config('get')
