7e80f76717ce3108d76c30129635e05298c7ac61
Toyohisa Kameyama <kameyama@riken.jp>
2020-09-09 10:08:19 +0900

    gettext: Add ACLOCAL_PATH to build dependent packages.

--- var/spack/repos/builtin/packages/gettext/package.py
+++ var/spack/repos/builtin/packages/gettext/package.py
@@ -98,3 +98,7 @@ class Gettext(AutotoolsPackage, GNUMirrorPackage):
                 "libintl"],
             root=self.prefix, recursive=True
         )
+
+    def setup_dependent_build_environment(self, env, dependent_spec):
+        """Adds the ACLOCAL path for autotools."""
+        env.append_path('ACLOCAL_PATH', self.prefix.share.aclocal)
