b50bcc89bc89c229ce01b047dcf9db550d1b8633
Robert QIAO <rob.qiao@flinders.edu.au>
2020-09-10 14:43:12 +0930

    sys var build_directory sub

--- var/spack/repos/builtin/packages/pbbam/package.py
+++ var/spack/repos/builtin/packages/pbbam/package.py
@@ -34,9 +34,9 @@ class Pbbam(CMakePackage):
         return options
 
     def install(self, spec, prefix):
-        install_tree('../spack-build/bin', prefix.bin)
-        install_tree('../spack-build/lib', prefix.lib)
-        install_tree('include/pbbam', prefix.include.pbbam)
+        install_tree('%(build_directory)s/bin', prefix.bin)
+        install_tree('%(build_directory)s/lib', prefix.lib)
+        install_tree('%(build_directory)s/pbbam', prefix.include.pbbam)
 
     def setup_dependent_build_environment(self, env, dependent_spec):
         env.set('PacBioBAM_LIBRARIES', self.prefix.lib)
