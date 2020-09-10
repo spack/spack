9d27f7d13619dd67fa1a65641f54c1f75801aa01
Richarda Butler <39577672+RikkiButler20@users.noreply.github.com>
2020-09-10 11:52:50 -0700

    Update lib/spack/spack/environment.py
    Removing unused variables
    
    Co-authored-by: Greg Becker <becker33@llnl.gov>

--- lib/spack/spack/environment.py
+++ lib/spack/spack/environment.py
@@ -829,7 +829,6 @@ class Environment(object):
                 scope = spack.config.ConfigScope(config_name, config_path)
             elif os.path.exists(config_path):
                 # files are assumed to be SingleFileScopes
-                base, ext = os.path.splitext(os.path.basename(config_path))
                 config_name = 'env:%s:%s' % (self.name, config_path)
                 scope = spack.config.SingleFileScope(
                     config_name, config_path, spack.schema.merged.schema)
