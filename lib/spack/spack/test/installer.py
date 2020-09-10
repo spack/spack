e170e3792edca747caa40c41b3c2c03046ecdee7
Scott Wittenburg <scott.wittenburg@kitware.com>
2020-09-10 12:19:10 -0600

    Provide --require-full-hash-match option to spack install command
    This option defaults to false, preserving the previous behavior, which
    was to install the first binary package found that matched the DAG hash
    of the target spec.  If this option is provided, spack will install the
    spec from source if it cannot find a binary spec on a remote mirror with
    a full_hash that matches that of the local target spec.

--- lib/spack/spack/test/installer.py
+++ lib/spack/spack/test/installer.py
@@ -177,7 +177,7 @@ def test_process_binary_cache_tarball_none(install_mockery, monkeypatch,
 
 def test_process_binary_cache_tarball_tar(install_mockery, monkeypatch, capfd):
     """Tests of _process_binary_cache_tarball with a tar file."""
-    def _spec(spec):
+    def _spec(spec, url=None):
         return spec
 
     # Skip binary distribution functionality since assume tested elsewhere
@@ -196,9 +196,12 @@ def test_process_binary_cache_tarball_tar(install_mockery, monkeypatch, capfd):
 def test_try_install_from_binary_cache(install_mockery, mock_packages,
                                        monkeypatch, capsys):
     """Tests SystemExit path for_try_install_from_binary_cache."""
-    def _spec(spec, force):
+    def _spec(spec, force, full_hash_match=False):
         spec = spack.spec.Spec('mpi').concretized()
-        return {spec: None}
+        return [{
+            'mirror_url': 'notused',
+            'spec': spec,
+        }]
 
     spec = spack.spec.Spec('mpich')
     spec.concretize()
