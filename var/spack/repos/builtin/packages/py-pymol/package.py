c5d1697e164ad9e7ba674db6a13cb182edd1c54f
Miles Perry <perrymil@ohsu.edu>
2020-09-14 12:18:10 -0700

    white space in empty line removal

--- var/spack/repos/builtin/packages/py-pymol/package.py
+++ var/spack/repos/builtin/packages/py-pymol/package.py
@@ -15,7 +15,7 @@ class PyPymol(PythonPackage):
     homepage = "https://pymol.org"
     url      = "https://github.com/schrodinger/pymol-open-source/archive/v2.3.0.tar.gz"
     url      = "https://github.com/schrodinger/pymol-open-source/archive/v2.4.0.tar.gz"
-    
+
     version('2.3.0', sha256='62aa21fafd1db805c876f89466e47513809f8198395e1f00a5f5cc40d6f40ed0')
     version('2.4.0', sha256='5ede4ce2e8f53713c5ee64f5905b2d29bf01e4391da7e536ce8909d6b9116581')
 
