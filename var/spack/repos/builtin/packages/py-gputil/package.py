03980b1d074d960bcf0c8dc85c39118e3a094ae4
Robert Pavel <rspavel@lanl.gov>
2020-09-14 11:52:57 -0600

    Fixed GPUtil Issues
    Thanks to Vinay Ramakrishnaiah for overwriting install

--- var/spack/repos/builtin/packages/py-gputil/package.py
+++ var/spack/repos/builtin/packages/py-gputil/package.py
@@ -10,14 +10,15 @@ class PyGputil(PythonPackage):
     """GPUtil is a Python module for getting the GPU status from NVIDA GPUs
     using nvidia-smi."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/anderskm/gputil"
-    url      = "https://github.com/anderskm/gputil/archive/v1.4.0.zip"
+    url      = "https://pypi.io/packages/source/G/GPUtil/GPUtil-1.4.0.tar.gz"
 
-    version('1.4.0', sha256='6c157fba2ab6bfbf9ce7e8eedb4c38685ef01a1b484043320a2861822e905a37')
-    version('1.3.0', sha256='098be020168d677c27f9c9c31585e0f5081584514915b6bb5861843c51acc48c')
-    version('1.2.3', sha256='93db313dcbcd9c989874ad9191848e63295f3b159cf90a5d5abcd86b965a326e')
-    version('1.2.2', sha256='43badf6018bbdc4d849233b4c9fa8bd1c518a549eea4af2db29eeea0aee645aa')
+    version('1.4.0', sha256='099e52c65e512cdfa8c8763fca67f5a5c2afb63469602d5dcb4d296b3661efb9')
 
     depends_on('py-setuptools', type='build')
     depends_on('py-wheel', type='build')
+
+    def install(self, spec, prefix):
+        # Override install to avoid
+        #   error: option --single-version-externally-managed not recognized
+        setup_py('install', '--root=/', '--prefix={0}'.format(prefix))
