2595b58503f390dab2b4a53791afcfef36e214b2
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2020-09-14 19:23:47 +0200

    test_noversion_pkg: generalized the error to be caught
    The new concretizer and the old concretizer solve constraints
    in a different way. Here we ensure that a SpackError is raised,
    instead of a specific error that made sense in the old concretizer
    but probably not in the new.

--- lib/spack/spack/test/concretize.py
+++ lib/spack/spack/test/concretize.py
@@ -13,7 +13,7 @@ import spack.concretize
 import spack.repo
 
 from spack.concretize import find_spec, NoValidVersionError
-from spack.error import SpecError
+from spack.error import SpecError, SpackError
 from spack.spec import Spec, CompilerSpec, ConflictsInSpecError
 from spack.version import ver
 from spack.util.mock_package import MockPackageMultiRepo
@@ -608,7 +608,7 @@ class TestConcretize(object):
     @pytest.mark.parametrize('spec', ['noversion', 'noversion-bundle'])
     def test_noversion_pkg(self, spec):
         """Test concretization failures for no-version packages."""
-        with pytest.raises(NoValidVersionError, match="no valid versions"):
+        with pytest.raises(SpackError):
             Spec(spec).concretized()
 
     @pytest.mark.parametrize('spec, best_achievable', [
