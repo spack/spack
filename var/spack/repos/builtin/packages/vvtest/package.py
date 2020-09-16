52a34b59506ea362c0827b9d3fb07c12bdc4b0c4
Miranda Mundt <mmundt@sandia.gov>
2020-09-16 15:10:06 -0600

    Fixing flake8 failures

--- var/spack/repos/builtin/packages/vvtest/package.py
+++ var/spack/repos/builtin/packages/vvtest/package.py
@@ -6,11 +6,12 @@
 from os import system
 from spack import *
 
+
 class Vvtest(Package):
-    """Vvtest is a test harness originally authored by Richard Drake for the 
+    """Vvtest is a test harness originally authored by Richard Drake for the
     Alegra-Emphasis project, written in pure Python, and designed for running
-    verification, validation, and regression type tests. Being a stand-alone 
-    product, it can also be used by analysts to help manage sets of 
+    verification, validation, and regression type tests. Being a stand-alone
+    product, it can also be used by analysts to help manage sets of
     simulations, especially useful on batch platforms."""
 
     homepage    = "https://github.com/rrdrake/vvtest"
@@ -18,10 +19,12 @@ class Vvtest(Package):
     git         = "https://github.com/rrdrake/vvtest.git"
     maintainers = ['mrmundt', 'rrdrake']
 
-    version('1.2.0', sha256='d6b2432a2e6c43fb0d87ffc0eaa34a74d2268a732f7709ebdcf1344fbcaee154', preferred=True)
-    version('1.1.0', sha256='674585f12d393ab9745a5ab26f59cb0f0e213f9c597b37467125979b5955ca79')
-    version('1.0.0', sha256='acd04e8e6635ed1b1725b793e8287a58831d6380759a81159142a6ff3397a8dd')
-
+    version('1.2.0',
+            sha256='d6b2432a2e6c43fb0d87ffc0eaa34a74d2268a732f7709ebdcf1344fbcaee154', preferred=True)
+    version('1.1.0',
+            sha256='674585f12d393ab9745a5ab26f59cb0f0e213f9c597b37467125979b5955ca79')
+    version('1.0.0',
+            sha256='acd04e8e6635ed1b1725b793e8287a58831d6380759a81159142a6ff3397a8dd')
 
     def install(self, spec, prefix):
         system('ls -la')
