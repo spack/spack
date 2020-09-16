ae63a7968bd69e9cf413a2480e036122d89ad5fc
Robert Blake <blake14@llnl.gov>
2020-09-16 12:32:30 -0700

    Adding explicit dependence on libuuid

--- var/spack/repos/builtin/packages/util-linux/package.py
+++ var/spack/repos/builtin/packages/util-linux/package.py
@@ -31,6 +31,7 @@ class UtilLinux(AutotoolsPackage):
     variant('bash', default=False, description='Install bash completion scripts')
 
     depends_on('bash', when="+bash")
+    depends_on('libuuid', when="+libuuid")
 
     def url_for_version(self, version):
         url = "https://www.kernel.org/pub/linux/utils/util-linux/v{0}/util-linux-{1}.tar.gz"
