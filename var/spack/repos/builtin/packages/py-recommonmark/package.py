cca32f6c38d3c0a786f27c48bfd899b8e233e794
Axel Huebl <axel.huebl@plasma.ninja>
2020-09-17 11:46:00 -0700

    py-recommonmark: fix docutils version
    Fix a version constraint that leads to build errors.

--- var/spack/repos/builtin/packages/py-recommonmark/package.py
+++ var/spack/repos/builtin/packages/py-recommonmark/package.py
@@ -20,5 +20,5 @@ class PyRecommonmark(PythonPackage):
     version('0.6.0', sha256='29cd4faeb6c5268c633634f2d69aef9431e0f4d347f90659fd0aab20e541efeb')
 
     depends_on('py-commonmark@0.8.1:')
-    depends_on('py-docutils@011:')
+    depends_on('py-docutils@0.11:')
     depends_on('py-sphinx@1.3.1:')
