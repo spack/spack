0f332c73a6835a2fe978e35d63ae13cc5b5212ff
Ganesh Kumar <46193450+gkathirv@users.noreply.github.com>
2020-09-17 20:30:45 +0530

    Rocm 3.7 miopen, miopengemm, rocalution and rocm-opencl (#18690)
    * ROCm 3.5 miopen recipe
    
    * fixing flake8 issues
    
    * cmake variant fix
    
    * min support fix
    
    * variant possible values
    
    * rocm3.7 change for miopen, rocalution and rocm-opencl
    
    * review comments

--- var/spack/repos/builtin/packages/rocm-opencl/package.py
+++ var/spack/repos/builtin/packages/rocm-opencl/package.py
@@ -10,15 +10,25 @@ class RocmOpencl(CMakePackage):
     """OpenCL: Open Computing Language on ROCclr"""
 
     homepage = "https://github.com/RadeonOpenCompute/ROCm-OpenCL-Runtime"
-    url      = "https://github.com/RadeonOpenCompute/ROCm-OpenCL-Runtime/archive/roc-3.5.0.tar.gz"
 
     maintainers = ['srekolam', 'arjun-raj-kuppala']
 
+    def url_for_version(self, version):
+        if version == Version('3.5.0'):
+            return "https://github.com/RadeonOpenCompute/ROCm-OpenCL-Runtime/archive/roc-3.5.0.tar.gz"
+
+        url = "https://github.com/RadeonOpenCompute/ROCm-OpenCL-Runtime/archive/rocm-{0}.tar.gz"
+        return url.format(version)
+
+    version('3.7.0', sha256='283e1dfe4c3d2e8af4d677ed3c20e975393cdb0856e3ccd77b9c7ed2a151650b')
     version('3.5.0', sha256='511b617d5192f2d4893603c1a02402b2ac9556e9806ff09dd2a91d398abf39a0')
+
     depends_on('cmake@3:', type='build')
-    depends_on('rocclr@3.5.0', type='build', when='@3.5.0')
-    depends_on('comgr@3.5.0', type='build', when='@3.5.0')
     depends_on('mesa~llvm@18.3:', type='link')
+    for ver in ['3.5.0', '3.7.0']:
+        depends_on('rocclr@' + ver, type='build', when='@' + ver)
+        depends_on('comgr@' + ver, type='build', when='@' + ver)
+        depends_on('hsa-rocr-dev@' + ver, type='build', when='@' + ver)
 
     def flag_handler(self, name, flags):
         # The includes are messed up in ROCm 3.5.0:
@@ -27,7 +37,7 @@ class RocmOpencl(CMakePackage):
         # commented out. So instead we provide
         # all the includes...
 
-        if self.spec.satisfies('@3.5.0') and name in ('cflags', 'cxxflags'):
+        if name in ('cflags', 'cxxflags'):
             rocclr = self.spec['rocclr'].prefix.include
             extra_includes = [
                 'include',
