28262821001556a4e6ba032ab3421876834189ff
Axel Huebl <axel.huebl@plasma.ninja>
2020-09-18 09:36:52 -0700

    py-(re)commonmark: dependency types
    fix the python dependency types for python packages.

--- var/spack/repos/builtin/packages/py-commonmark/package.py
+++ var/spack/repos/builtin/packages/py-commonmark/package.py
@@ -16,4 +16,4 @@ class PyCommonmark(PythonPackage):
 
     version('0.9.0', sha256='867fc5db078ede373ab811e16b6789e9d033b15ccd7296f370ca52d1ee792ce0')
 
-    depends_on('py-future')
+    depends_on('py-future', type=('build', 'run'))
