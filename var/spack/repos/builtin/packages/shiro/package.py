840f8d7e08ac1499d0b44a1b8ceae2a15bb08259
darmac <xiaojun2@hisilicon.com>
2020-09-18 14:32:02 +0800

    refine description and dependencies

--- var/spack/repos/builtin/packages/shiro/package.py
+++ var/spack/repos/builtin/packages/shiro/package.py
@@ -9,7 +9,7 @@ from spack import *
 class Shiro(MavenPackage):
     """Apache Shiro is a powerful and easy-to-use Java security framework
     that performs authentication, authorization, cryptography, and session
-    management. With Shiro’s easy-to-understand API, you can quickly and
+    management. With Shiro's easy-to-understand API, you can quickly and
     easily secure any application - from the smallest mobile applications
     to the largest web and enterprise applications."""
 
@@ -18,3 +18,6 @@ class Shiro(MavenPackage):
 
     version('1.6.0', sha256='50338badfd3261076060fbe70330089512d38071bc51aa3f84ad23e707d2b7c9')
     version('1.5.3', sha256='25c5d99eddf790969e0f80bd9769f773465c9c9e1e8ec1f549f476bdae8c6983')
+
+    depends_on('java@8:', type=('build', 'run'))
+    depends_on('maven@3.5:3.999', type='build')
