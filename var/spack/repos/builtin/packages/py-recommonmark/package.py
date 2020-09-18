7d75becabc41c8729a0077526d5c6e940a50b129
Axel Huebl <axel.huebl@plasma.ninja>
2020-09-17 22:57:49 -0700

    py-recommonmark: fix source url

--- var/spack/repos/builtin/packages/py-recommonmark/package.py
+++ var/spack/repos/builtin/packages/py-recommonmark/package.py
@@ -15,7 +15,7 @@ class PyRecommonmark(PythonPackage):
     http://recommonmark.readthedocs.org"""
 
     homepage = "https://github.com/readthedocs/recommonmark"
-    url      = "https://files.pythonhosted.org/packages/f5/71/046160d730f664662d65b3f8b399b519ad378ffa4369ff3b6060cf1318d7/recommonmark-0.6.0.tar.gz"
+    url      = "https://pypi.io/packages/source/r/recommonmark/recommonmark-0.6.0.tar.gz"
 
     version('0.6.0', sha256='29cd4faeb6c5268c633634f2d69aef9431e0f4d347f90659fd0aab20e541efeb')
 
