b7d508d3b41c439a2eab35a8b0acc5d81b27ceee
Adam J. Stewart <ajstewart426@gmail.com>
2020-09-19 11:53:04 -0500

    libksba: add new version

--- var/spack/repos/builtin/packages/libksba/package.py
+++ var/spack/repos/builtin/packages/libksba/package.py
@@ -13,6 +13,7 @@ class Libksba(AutotoolsPackage):
     homepage = "https://gnupg.org/software/libksba/index.html"
     url = "https://gnupg.org/ftp/gcrypt/libksba/libksba-1.3.5.tar.bz2"
 
+    version('1.4.0', sha256='bfe6a8e91ff0f54d8a329514db406667000cb207238eded49b599761bfca41b6')
     version('1.3.5', sha256='41444fd7a6ff73a79ad9728f985e71c9ba8cd3e5e53358e70d5f066d35c1a340')
 
     depends_on('libgpg-error@1.8:')
