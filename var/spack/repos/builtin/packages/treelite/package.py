a77d1a273c7553def02e454a01985be676b37ccb
Jen Herting <jen@herting.cc>
2020-09-21 14:49:36 -0400

    [treelite] cleaned up fixmes and added description and homepage

--- var/spack/repos/builtin/packages/treelite/package.py
+++ var/spack/repos/builtin/packages/treelite/package.py
@@ -3,45 +3,14 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install treelite
-#
-# You can edit this file again by typing:
-#
-#     spack edit treelite
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Treelite(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """Treelite is a model compiler for efficient deployment of
+    decision tree ensembles."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/dmlc/treelite"
     url      = "https://github.com/dmlc/treelite/archive/0.93.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('0.93',    sha256='7d347372f7fdc069904afe93e69ed0bf696ba42d271fe2f8bf6835d2ab2f45d5')
-
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
-
-    def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
-        args = []
-        return args
