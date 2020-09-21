6ddffecc13300603cea48f464cb3a8db7d0c1511
Chris White <white238@llnl.gov>
2020-09-21 16:16:34 -0700

    Fix version

--- var/spack/repos/builtin/packages/umpire/package.py
+++ var/spack/repos/builtin/packages/umpire/package.py
@@ -16,7 +16,7 @@ class Umpire(CMakePackage, CudaPackage):
 
     version('develop', branch='develop', submodules='True')
     version('main', branch='main', submodules='True')
-    version('4.0.1', tag='v4.0.0', submodules='True')
+    version('4.0.1', tag='v4.0.1', submodules='True')
     version('4.0.0', tag='v4.0.0', submodules='True')
     version('3.0.0', tag='v3.0.0', submodules='True')
     version('2.1.0', tag='v2.1.0', submodules='True')
