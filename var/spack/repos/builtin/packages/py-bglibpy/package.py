6a23733517cd17f7dd0e4e506bc041a82c54816c
anilbey <anil.tuncel@epfl.ch>
2020-09-22 08:59:23 +0200

    update bglibpy to version 4.4 (#920)
    * Update bglibpy version
    
    * Update bbp-packages with new bglibpy version
    
    * update bglibpy to 4.4
    
    * keep the previous version as well
    
    Co-authored-by: Werner Van Geit <werner.vangeit@epfl.ch>

--- var/spack/repos/builtin/packages/py-bglibpy/package.py
+++ var/spack/repos/builtin/packages/py-bglibpy/package.py
@@ -14,6 +14,7 @@ class PyBglibpy(PythonPackage):
 
     version('develop', branch='master')
 
+    version('4.4', commit='4597bf81374f4041f689a4e73e4319bf5c13947b')
     version('4.3.19', commit='bce00a1ddedb605a5ed5225989192eb5f7e133ae')
     version('4.3.15', commit='dccd717a2570d32776de824d864fba9dfdbf56f6')
     version('4.3.12', commit='eb4accd2dc4ebcb01e65d6429bb8221af5aa14bf')
