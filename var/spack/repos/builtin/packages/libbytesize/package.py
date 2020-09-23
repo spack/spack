788a488387f2b43400d39167499ecb070565225d
darmac <xiaojun2@hisilicon.com>
2020-09-23 10:36:06 +0800

    libbytesize: refine url

--- var/spack/repos/builtin/packages/libbytesize/package.py
+++ var/spack/repos/builtin/packages/libbytesize/package.py
@@ -11,22 +11,13 @@ class Libbytesize(AutotoolsPackage):
     facilitate the common operations with sizes in bytes."""
 
     homepage = "https://github.com/storaged-project/libbytesize"
-    url      = "https://github.com/storaged-project/libbytesize/archive/2.4.tar.gz"
+    url      = "https://github.com/storaged-project/libbytesize/releases/download/2.4/libbytesize-2.4.tar.gz"
 
-    version('2.4', sha256='aab45b8fc4f5d9a949750bd863bd268e50a899777a4576a33bef2fd1d827e225')
+    version('2.4', sha256='25ccb5762bb8c860b63ed1d40e0c5564e3e0084693fbe6554467a8ca1c1d8c7f')
     version('2.3', sha256='3c74113fc8cd1a2fbd8870fa0ed7cef2ef24d60ef91e7145fbc041f9aa144479')
     version('2.2', sha256='b93c54b502880c095c9f5767a42464853e2687db2e5e3084908a615bafe73baa')
 
-    depends_on('gettext',  type='build')
-    depends_on('autoconf', type='build')
-    depends_on('automake', type='build')
-    depends_on('libtool',  type='build')
-    depends_on('m4',       type='build')
     extends('python')
     depends_on('pcre2')
     depends_on('gmp')
     depends_on('mpfr')
-
-    def autoreconf(self, spec, prefix):
-        sh = which('sh')
-        sh('./autogen.sh')
