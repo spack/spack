6d85808786baf1ad137b99464b491e71e26d70a6
takanori-ihara <ihara.takanori@jp.fujitsu.com>
2020-09-24 18:35:23 +0900

    fujitsu-ssl2: Add thread-parallel support and  SVE support and scalapack support

--- var/spack/repos/builtin/packages/fujitsu-ssl2/package.py
+++ var/spack/repos/builtin/packages/fujitsu-ssl2/package.py
@@ -16,6 +16,8 @@ class FujitsuSsl2(Package):
     homepage = "https://www.fujitsu.com/us/"
     has_code = False
 
+    variant("parallel", default=True, description="Build with thread-parallel versions")
+
     conflicts("%arm")
     conflicts("%cce")
     conflicts("%apple-clang")
@@ -34,33 +36,92 @@ class FujitsuSsl2(Package):
     def install(self, spec, prefix):
         raise InstallError(
             "Fujitsu SSL2 is not installable; it is vendor supplied \
-             You need to specify it as an external package in packages.yaml")
+             You need to specify it as an external package in packages.yaml"
+        )
 
     @property
-    def libs(self):
+    def blas_libs(self):
+        spec = self.spec
+        sharedlibslist = ["libfj90i", "libfj90f", "libfjsrcinfo", "libfj90rt"]
+        staticlibslist = []
+
+        if spec.target == "a64fx":  # Build with SVE support
+            sharedlibslist += ["libfjlapacksve"]
+
+            if "+parallel" in spec:  # parallel
+                staticlibslist = ["libssl2mtexsve"]
+
+            staticlibslist += ["libssl2mtsve", "libfj90rt2", "libfj90fmt_sve"]
+
+        else:  # Build with NEON support
+            sharedlibslist += ["libfjlapack"]
+
+            if "+parallel" in spec:  # parallel
+                staticlibslist = ["libssl2mtex"]
+
+            staticlibslist += [
+                "libssl2mt",
+                "libfj90rt2",
+                "libfj90fmt"
+            ]
+
         sharedlibs = find_libraries(
-            [
-                "libfjscalapack",
-                "libfjlapack",
-                "libfj90i",
-                "libfj90f",
-                "libfjsrcinfo",
-                "libfj90rt",
-            ],
-            self.prefix.lib64,
-            shared=True,
-            recursive=False,
+            sharedlibslist, self.prefix.lib64, shared=True, recursive=False
         )
         staticlibs = find_libraries(
-            [
-                "libssl2mt",
-                "libssl2mtex",
+            staticlibslist, self.prefix.lib64, shared=False, recursive=False
+        )
+        libs = sharedlibs + staticlibs
+        return libs
+
+    @property
+    def lapack_libs(self):
+        return self.blas_libs
+
+    @property
+    def scalapack_libs(self):
+        spec = self.spec
+        sharedlibslist = [
+            "libmpi_usempi_ignore_tkr",
+            "libmpi_mpifh",
+            "libfj90i",
+            "libfj90f",
+            "libfjsrcinfo",
+            "libfj90rt",
+        ]
+        staticlibslist = []
+
+        if spec.target == "a64fx":  # Build with SVE support
+            sharedlibslist += ["libfjscalapacksve", "libfjlapacksve"]
+
+            if "+parallel" in spec:  # parallel
+                staticlibslist = ["libssl2mtexsve"]
+
+            staticlibslist += [
+                "libscalapacksve",
+                "libssl2mtsve",
                 "libfj90rt2",
-                "libfj90fmt",
-            ],
-            self.prefix.lib64,
-            shared=False,
-            recursive=False,
+                "libfj90fmt_sve",
+            ]
+
+        else:  # Build with NEON support
+            sharedlibslist += ["libfjscalapack", "libfjlapack"]
+
+            if "+parallel" in spec:  # parallel
+                staticlibslist = ["libssl2mtex"]
+
+            staticlibslist += [
+                "libscalapack",
+                "libssl2mt",
+                "libfj90rt2",
+                "libfj90fmt"
+            ]
+
+        sharedlibs = find_libraries(
+            sharedlibslist, self.prefix.lib64, shared=True, recursive=False
+        )
+        staticlibs = find_libraries(
+            staticlibslist, self.prefix.lib64, shared=False, recursive=False
         )
         libs = sharedlibs + staticlibs
         return libs
