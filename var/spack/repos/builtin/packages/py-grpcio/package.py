836f32a86c073b38cbe631c08df9971b53bffb1a
Vinay Bharadwaj Ramakrishnaiah <vinayr@lanl.gov>
2020-09-30 16:22:33 -0600

    some deps

--- var/spack/repos/builtin/packages/py-grpcio/package.py
+++ var/spack/repos/builtin/packages/py-grpcio/package.py
@@ -10,8 +10,9 @@ class PyGrpcio(PythonPackage):
     """HTTP/2-based RPC framework."""
 
     homepage = "https://grpc.io/"
-    url      = "https://pypi.io/packages/source/g/grpcio/grpcio-1.29.0.tar.gz"
+    url      = "https://pypi.io/packages/source/g/grpcio/grpcio-1.30.0.tar.gz"
 
+    version('1.30.0', sha256='e8f2f5d16e0164c415f1b31a8d9a81f2e4645a43d1b261375d6bab7b0adf511f')
     version('1.29.0', sha256='a97ea91e31863c9a3879684b5fb3c6ab4b17c5431787548fc9f52b9483ea9c25')
     version('1.27.2', sha256='5ae532b93cf9ce5a2a549b74a2c35e3b690b171ece9358519b3039c7b84c887e')
     version('1.25.0', sha256='c948c034d8997526011960db54f512756fb0b4be1b81140a15b4ef094c6594a4')
