b81a89612e7c401fcba47f81b6c883d8f2431f98
Joseph M. Koning <koning1@llnl.gov>
2020-09-30 15:28:41 -0700

    Remove extra line.

--- var/spack/repos/builtin/packages/py-click-didyoumean/package.py
+++ var/spack/repos/builtin/packages/py-click-didyoumean/package.py
@@ -11,7 +11,6 @@ class PyClickDidyoumean(PythonPackage):
     homepage = "https://github.com/click-contrib/click-didyoumean"
     url      = "https://files.pythonhosted.org/packages/9f/79/d265d783dd022541b744d002745d9e55d84c04a41930e35d8795934f6526/click-didyoumean-0.0.3.tar.gz"
 
-
     version('0.0.3', sha256='112229485c9704ff51362fe34b2d4f0b12fc71cc20f6d2b3afabed4b8bfa6aeb')
 
     depends_on('python@3.0:', type=('build', 'run'))
