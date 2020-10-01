5f18d998a9c113fffa450f34e600f0a02190ea37
Jen Herting <jen@herting.cc>
2020-10-01 11:54:28 -0400

    [py-rmm] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-rmm/package.py
+++ var/spack/repos/builtin/packages/py-rmm/package.py
@@ -3,45 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-rmm
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-rmm
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyRmm(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """RMM: RAPIDS Memory Manager. Achieving optimal
+    performance in GPU-centric workflows frequently requires
+    customizing how host and device memory are allocated."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rapidsai/rmm"
     url      = "https://github.com/rapidsai/rmm/archive/v0.15.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.15.0',  sha256='599f97b95d169a90d11296814763f7e151a8a1e060ba10bc6c8f4684a5cd7972')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
     depends_on('python@3.6:3.7', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-cython', type='build')
@@ -53,9 +27,3 @@ class PyRmm(PythonPackage):
     depends_on('spdlog')
 
     build_directory = 'python'
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
