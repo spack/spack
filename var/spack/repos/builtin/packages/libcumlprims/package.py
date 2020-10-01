1be7a5fcd157aad0d54e2c44e14972712944a6cd
Jen Herting <jen@herting.cc>
2020-10-01 10:45:19 -0400

    [libcumlprims] trying to fix cumlprims includedir

--- var/spack/repos/builtin/packages/libcumlprims/package.py
+++ var/spack/repos/builtin/packages/libcumlprims/package.py
@@ -45,6 +45,9 @@ class Libcumlprims(Package):
     depends_on('cuda@10.2.0:10.2.999', when='@0.15.0-cuda10.2_gdbd0d39_0')
     depends_on('cuda@10.1.0:10.1.999', when='@0.15.0-cuda10.1_gdbd0d39_0')
 
+    def setup_dependent_build_environment(self, env, dependent_spec):
+        env.prepend_path('INCLUDE_DIR', self.prefix.include.cumlprims)
+
     def url_for_version(self, version):
         url = "https://anaconda.org/nvidia/libcumlprims/{0}/download/linux-64/libcumlprims-{1}.tar.bz2"
         return url.format(version.up_to(3), version)
