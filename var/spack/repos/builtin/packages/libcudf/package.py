be239f7460facbbc72f41db05f1ae62f3eadc5d3
Jen Herting <jen@herting.cc>
2020-10-01 12:04:30 -0400

    [libcudf] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/libcudf/package.py
+++ var/spack/repos/builtin/packages/libcudf/package.py
@@ -3,41 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install libcudf
-#
-# You can edit this file again by typing:
-#
-#     spack edit libcudf
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Libcudf(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """Built based on the Apache Arrow columnar memory format,
+    cuDF is a GPU DataFrame library for loading, joining,
+    aggregating, filtering, and otherwise manipulating data."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://rapids.ai"
     url      = "https://github.com/rapidsai/cudf/archive/v0.15.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.15.0',  sha256='57b8036134368daa8a967eae971ee121cdc58c8678a03eba1358ed2b927dc16e')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('cmake@3.14:', type='build')
     depends_on('cuda@10.0:')
     depends_on('boost')
@@ -48,9 +26,6 @@ class Libcudf(CMakePackage):
     root_cmakelists_dir = 'cpp'
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = []
 
         #args.append('-DGPU_ARCHES')
