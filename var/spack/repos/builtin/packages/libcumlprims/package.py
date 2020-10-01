c83f4776be45890279e339f83b10be27f9ee98ac
Jen Herting <jen@herting.cc>
2020-10-01 10:55:13 -0400

    [libcumlprims] typoo

--- var/spack/repos/builtin/packages/libcumlprims/package.py
+++ var/spack/repos/builtin/packages/libcumlprims/package.py
@@ -46,7 +46,7 @@ class Libcumlprims(Package):
     depends_on('cuda@10.1.0:10.1.999', when='@0.15.0-cuda10.1_gdbd0d39_0')
 
     def setup_dependent_build_environment(self, env, dependent_spec):
-        env.prepend_path('SPACK_INCLUDE_DIR', self.prefix.include.cumlprims)
+        env.prepend_path('SPACK_INCLUDE_DIRS', self.prefix.include.cumlprims)
 
     def url_for_version(self, version):
         url = "https://anaconda.org/nvidia/libcumlprims/{0}/download/linux-64/libcumlprims-{1}.tar.bz2"
