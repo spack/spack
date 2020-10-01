2c45e178649d0f70e66e8b8536203c96824a12e5
Jen Herting <jen@herting.cc>
2020-10-01 11:50:15 -0400

    [librmm] adds homepage and description. removes fixmes

--- var/spack/repos/builtin/packages/librmm/package.py
+++ var/spack/repos/builtin/packages/librmm/package.py
@@ -3,46 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install librmm
-#
-# You can edit this file again by typing:
-#
-#     spack edit librmm
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Librmm(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """RMM: RAPIDS Memory Manager. Achieving optimal
+    performance in GPU-centric workflows frequently requires
+    customizing how host and device memory are allocated."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rapidsai/rmm"
     url      = "https://github.com/rapidsai/rmm/archive/v0.15.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.15.0',  sha256='599f97b95d169a90d11296814763f7e151a8a1e060ba10bc6c8f4684a5cd7972')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('cuda@9.0:')
-
-    def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
-        args = []
-        return args
