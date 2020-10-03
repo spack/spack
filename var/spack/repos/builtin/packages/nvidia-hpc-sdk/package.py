62684f9436cb18fb2193de04cc8089ed2c80e832
Nischay Ram Mamidi <NischayPro@gmail.com>
2020-10-03 19:38:45 +0530

    nvidia hpc sdk: use sha256 instead of md5

--- var/spack/repos/builtin/packages/nvidia-hpc-sdk/package.py
+++ var/spack/repos/builtin/packages/nvidia-hpc-sdk/package.py
@@ -10,9 +10,9 @@ import platform
 
 _versions = {
     '20.7': {
-        'Linux_aarch64': ('71ef81910ae00d2749a23bc1ca6e4a66', 'https://developer.download.nvidia.com/hpc-sdk/nvhpc_2020_207_Linux_aarch64_cuda_11.0.tar.gz'),
-        'Linux_ppc64le': ('13210525ae495e86a0a8ecdf1a23251b', 'https://developer.download.nvidia.com/hpc-sdk/nvhpc_2020_207_Linux_ppc64le_cuda_multi.tar.gz'),
-        'Linux_x86_64': ('71ef81910ae00d2749a23bc1ca6e4a66', 'https://developer.download.nvidia.com/hpc-sdk/nvhpc_2020_207_Linux_x86_64_cuda_multi.tar.gz')
+        'Linux_aarch64': ('5b83ca1919199ac0aa609309b31c345c5a6453dd3131fddeef9e3ee9059a0e9b', 'https://developer.download.nvidia.com/hpc-sdk/nvhpc_2020_207_Linux_aarch64_cuda_11.0.tar.gz'),
+        'Linux_ppc64le': ('800ead240bdf61611910b2f6df24ee1d7359377ff3767c923738dd81fcea9312', 'https://developer.download.nvidia.com/hpc-sdk/nvhpc_2020_207_Linux_ppc64le_cuda_multi.tar.gz'),
+        'Linux_x86_64':  ('a5c5c8726d2210f2310a852c6d6e03c9ef8c75e3643e9c94e24909f5e9c2ea7a', 'https://developer.download.nvidia.com/hpc-sdk/nvhpc_2020_207_Linux_x86_64_cuda_multi.tar.gz')
     }
 }
 
@@ -21,7 +21,7 @@ class NvidiaHpcSdk(Package):
     """The NVIDIA HPC SDK is a comprehensive suite of compilers and libraries
     enabling HPC developers to program the entire HPC platform from the GPU
     foundation to the CPU and through the interconnect. It is the only
-    comprehensive, integrated SDK for programming accelerated computing 
+    comprehensive, integrated SDK for programming accelerated computing
     systems."""
 
     homepage = "https://developer.nvidia.com/hpc-sdk"
@@ -32,7 +32,7 @@ class NvidiaHpcSdk(Package):
         key = "{0}_{1}".format(platform.system(), platform.machine())
         pkg = packages.get(key)
         if pkg:
-            version(ver, md5=pkg[0], url=pkg[1], expand=True)
+            version(ver, sha256=pkg[0], url=pkg[1], expand=True)
 
     variant('network', default=True, description="Perform a network install")
 
@@ -43,7 +43,7 @@ class NvidiaHpcSdk(Package):
     else:
         variant('cuda_version', default='11.0', description='Choose CUDA version', values=('11.0'), multi=False)
 
-    variant('mpi', default=False, description="Enable installation of Open MPI")
+    variant('mpi', default=False, description="Enable installation of OpenMPI")
 
     depends_on('cuda')
 
@@ -58,7 +58,7 @@ class NvidiaHpcSdk(Package):
 
         if '+network' in spec and '~single' in spec:
             os.environ['NVHPC_INSTALL_TYPE'] = "network"
-            os.environ['NVHPC_INSTALL_LOCAL_DIR'] = "{}/{}/shared_objects"\
+            os.environ['NVHPC_INSTALL_LOCAL_DIR'] = "{0}/{1}/shared_objects"\
                 .format(prefix, self.version)
         elif '+single' in spec and '~network' in spec:
             os.environ['NVHPC_INSTALL_TYPE'] = "single"
@@ -77,8 +77,8 @@ class NvidiaHpcSdk(Package):
             platform.system(), platform.machine()), self.version)
         )
 
-        env.set('target', '{0}_{1}'.format(platform.system(),
-            platform.machine())
+        env.set('target', '{0}_{1}'.format(
+            platform.system(), platform.machine())
         )
         env.set('version', repr(self.version))
         env.set('NVHPC', prefix)
