a0cc5b78c8fa2c29d9d57b8c77cfdb2648cf4150
Adam J. Stewart <ajstewart426@gmail.com>
2020-10-04 13:48:35 -0500

    py-grpcio: added v1.32.0 (#18995)

--- var/spack/repos/builtin/packages/py-grpcio/package.py
+++ var/spack/repos/builtin/packages/py-grpcio/package.py
@@ -3,18 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
-
 
 class PyGrpcio(PythonPackage):
     """HTTP/2-based RPC framework."""
 
     homepage = "https://grpc.io/"
-    url      = "https://pypi.io/packages/source/g/grpcio/grpcio-1.27.2.tar.gz"
+    url      = "https://pypi.io/packages/source/g/grpcio/grpcio-1.32.0.tar.gz"
 
+    version('1.32.0', sha256='01d3046fe980be25796d368f8fc5ff34b7cf5e1444f3789a017a7fe794465639')
     version('1.27.2', sha256='5ae532b93cf9ce5a2a549b74a2c35e3b690b171ece9358519b3039c7b84c887e')
     version('1.25.0', sha256='c948c034d8997526011960db54f512756fb0b4be1b81140a15b4ef094c6594a4')
 
+    depends_on('python@3.5:', when='@1.30:', type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-six@1.5.2:', type=('build', 'run'))
     depends_on('py-futures@2.2.0:', when='^python@:3.1', type=('build', 'run'))
@@ -29,6 +30,11 @@ class PyGrpcio(PythonPackage):
         env.set('GRPC_PYTHON_BUILD_SYSTEM_OPENSSL', True)
         env.set('GRPC_PYTHON_BUILD_SYSTEM_ZLIB', True)
         env.set('GRPC_PYTHON_BUILD_SYSTEM_CARES', True)
+
+        for dep in self.spec.dependencies(deptype='link'):
+            query = self.spec[dep.name]
+            env.prepend_path('LIBRARY_PATH', query.libs.directories[0])
+            env.prepend_path('CPATH', query.headers.directories[0])
 
     def patch(self):
         if self.spec.satisfies('%fj'):
