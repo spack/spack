52a23e8e3fd97d1cc656a0d49b1b296ffca040ce
Scott Wittenburg <scott.wittenburg@kitware.com>
2020-10-05 15:53:01 -0600

    Revert "Merge pull request #18359 from scottwittenburg/add-binary-distribution-cache-manager"
    This reverts commit 075c3e0d9247e9c1716ac44e7052366ac59ff110.

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -1552,8 +1552,6 @@ class Spec(object):
 
         if not self._concrete:
             d['concrete'] = False
-        else:
-            d['full_hash'] = self._full_hash
 
         if 'patches' in self.variants:
             variant = self.variants['patches']
@@ -1736,7 +1734,6 @@ class Spec(object):
 
         # specs read in are concrete unless marked abstract
         spec._concrete = node.get('concrete', True)
-        spec._full_hash = node.get('full_hash', None)
 
         if 'patches' in node:
             patches = node['patches']
