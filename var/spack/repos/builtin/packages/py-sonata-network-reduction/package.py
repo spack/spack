53ac12ee118499f2883f2b72fe5dacd315dbe0ba
asanin-epfl <53935643+asanin-epfl@users.noreply.github.com>
2020-10-05 17:55:08 +0200

    py-sonata-network-reduction@0.1.0 (#928)

--- var/spack/repos/builtin/packages/py-sonata-network-reduction/package.py
+++ var/spack/repos/builtin/packages/py-sonata-network-reduction/package.py
@@ -13,6 +13,7 @@ class PySonataNetworkReduction(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/nse/sonata-network-reduction"
 
     version('develop', branch='master')
+    version('0.1.0', tag='sonata-network-reduction-v0.1.0')
     version('0.0.10', tag='sonata-network-reduction-v0.0.10')
     version('0.0.9', tag='sonata-network-reduction-v0.0.9')
     version('0.0.8', tag='sonata-network-reduction-v0.0.8')
