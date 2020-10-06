bb4bb4a072782685b06afb7c28a367c6040eed53
Scott Wittenburg <scott.wittenburg@kitware.com>
2020-10-06 14:45:22 -0600

    Pipelines: Support running gitlab pipelines on PRs from forks
    This change makes improvements to the `spack ci rebuild` command
    which supports running gitlab pipelines on PRs from forks.  Much
    of this has to do with making sure we can run without the secrets
    previously required for running gitlab pipelines (e.g signing key,
    aws credentials, etc).  Specific improvements in this PR:
    
    Check if spack has precisely one signing key, and use that information
    as an additional constraint on whether or not we should attempt to sign
    the binary package we create.
    
    Also, if spack does not have at least one public key, add the install
    option "--no-check-signature"
    
    If we are running a pipeline without any profile or environment
    variables allowing us to push to S3, the pipeline could still
    successfully create a buildcache in the artifacts and move on.  So
    just print a message and move on if pushing either the buildcache
    entry or cdash id file to the remote mirror fails.
    
    When we attempt to generate a pacakge or gpg key index on an S3
    mirror, and there is nothing to index, just print a warning and
    exit gracefully rather than throw an exception.
    
    Support the use of PR-specific mirrors for temporary binary pkg
    storage.  This will allow quality-of-life improvement for developers,
    providing a place to store binaries over the lifetime of a PR, so
    that they must only wait for packages to rebuild from source when
    they push a new commit that causes it to be necessary.
    
    Replace two-pass install with a single pass and the new option:
     --require-full-hash-match.  Doing this also removes the need to
    save a copy of the spack.yaml to be copied over the one spack
    rewrites in between the two spack install passes.
    
    Work around a mirror configuration issue caused by using
    spack.util.executable to do the package installation.

--- lib/spack/spack/ci.py
+++ lib/spack/spack/ci.py
@@ -33,11 +33,15 @@ import spack.repo
 from spack.spec import Spec
 import spack.util.spack_yaml as syaml
 import spack.util.web as web_util
+import spack.util.gpg as gpg_util
+import spack.util.url as url_util
 
 
 JOB_RETRY_CONDITIONS = [
     'always',
 ]
+
+SPACK_PR_MIRRORS_ROOT_URL = 's3://spack-pr-mirrors'
 
 spack_gpg = spack.main.SpackCommand('gpg')
 spack_compiler = spack.main.SpackCommand('compiler')
@@ -529,6 +533,12 @@ def generate_gitlab_ci_yaml(env, print_summary, output_file,
         os.environ.get('SPACK_IS_PR_PIPELINE', '').lower() == 'true'
     )
 
+    spack_pr_branch = os.environ.get('SPACK_PR_BRANCH', None)
+    pr_mirror_url = None
+    if spack_pr_branch:
+        pr_mirror_url = url_util.join(SPACK_PR_MIRRORS_ROOT_URL,
+                                      spack_pr_branch)
+
     ci_mirrors = yaml_root['mirrors']
     mirror_urls = [url for url in ci_mirrors.values()]
 
@@ -858,6 +868,9 @@ def generate_gitlab_ci_yaml(env, print_summary, output_file,
         'SPACK_CHECKOUT_VERSION': version_to_clone,
     }
 
+    if pr_mirror_url:
+        output_object['variables']['SPACK_PR_MIRROR_URL'] = pr_mirror_url
+
     sorted_output = {}
     for output_key, output_value in sorted(output_object.items()):
         sorted_output[output_key] = output_value
@@ -918,6 +931,14 @@ def import_signing_key(base64_signing_key):
     tty.debug(trusted_keys_output)
     tty.debug('spack gpg list --signing')
     tty.debug(signing_keys_output)
+
+
+def can_sign_binaries():
+    return len(gpg_util.signing_keys()) == 1
+
+
+def can_verify_binaries():
+    return len(gpg_util.public_keys()) >= 1
 
 
 def configure_compilers(compiler_action, scope=None):
@@ -1095,12 +1116,15 @@ def read_cdashid_from_mirror(spec, mirror_url):
     return int(contents)
 
 
-def push_mirror_contents(env, spec, yaml_path, mirror_url, build_id):
+def push_mirror_contents(env, spec, yaml_path, mirror_url, build_id,
+                         sign_binaries):
     if mirror_url:
-        tty.debug('Creating buildcache')
+        unsigned = not sign_binaries
+        tty.debug('Creating buildcache ({0})'.format(
+            'unsigned' if unsigned else 'signed'))
         buildcache._createtarball(env, spec_yaml=yaml_path, add_deps=False,
                                   output_location=mirror_url, force=True,
-                                  allow_root=True)
+                                  allow_root=True, unsigned=unsigned)
         if build_id:
             tty.debug('Writing cdashid ({0}) to remote mirror: {1}'.format(
                 build_id, mirror_url))
