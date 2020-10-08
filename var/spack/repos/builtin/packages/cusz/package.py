d2ddeb2fefd19c8b7acafbbc87e199ce2adc8a8a
Dingwen Tao <tao@cs.ua.edu>
2020-10-07 20:42:14 -0700

    add cuSZ description

--- var/spack/repos/builtin/packages/cusz/package.py
+++ var/spack/repos/builtin/packages/cusz/package.py
@@ -24,26 +24,15 @@ from spack import *
 
 
 class Cusz(MakefilePackage):
-    """FIXME: Put a proper description of your package here."""
+    """cuSZ is a CUDA-based error-bounded lossy compressor for scientific data (floating point and integers)."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://szcompressor.org"
     url      = "https://github.com/szcompressor/cuSZ/releases/download/v0.1.2/cuSZ-0.1.2.tar.gz"
     git      = "https://github.com/szcompressor/cuSZ"
     maintainers = ['dingwentao', 'jtian0']
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('master', branch='master')
     version('0.1.2', sha256='c6e89a26b295724edefc8052f62653c5a315c78eaf6d5273299a8e11a5cf7363')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
-
     def edit(self, spec, prefix):
-        # FIXME: Edit the Makefile if necessary
-        # FIXME: If not needed delete this function
         makefile = FileFilter('Makefile')
-        # makefile.filter('CC = .*', 'CC = cc')
