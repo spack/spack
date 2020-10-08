ffc1c15387885f61ac39c3cc762aa27ba9fb91e5
Amjad Kotobi <amjadkotbi@gmail.com>
2020-10-08 19:55:14 +0200

    nvidia-hpc-sdk: Added missing environment variables and fix

---
+++ var/spack/repos/builtin/packages/nvidia-hpc-sdk/package.py
@@ -0,0 +1,84 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+from spack.util.prefix import Prefix
+import platform
+
+
+class NvidiaHpcSdk(Package, CudaPackage):
+    """The NVIDIA HPC Software Development Kit (SDK) includes the proven compilers,
+       libraries and software tools essential to maximizing developer productivity
+       and the performance and portability of HPC applications."""
+
+    homepage = "https://developer.nvidia.com/hpc-sdk"
+
+    maintainers = ['ajkotobi']
+
+    version('2020_207', sha256='a5c5c8726d2210f2310a852c6d6e03c9ef8c75e3643e9c94e24909f5e9c2ea7a')
+    #Probably better way to deal with this
+    if platform.machine() == "aarch64":
+	variant(
+          'cuda', default='11.0', description='List of CUDA that are enabled',
+          values=('11.0'), multi=False)
+    else:
+        variant(
+          'cuda', default='11.0', description='List of CUDA that are enabled',
+          values=('11.0', '10.2', '10.1'), multi=False)
+
+    variant(
+        'network', default='network', description='Network installation',
+        values=('network', 'single'), multi=False
+    )
+
+    depends_on('cuda@11.0.2:',   when='cuda=11.0')
+    depends_on('cuda@10.2.89:',  when='cuda=10.2')
+    depends_on('cuda@10.1.243:', when='cuda=10.1')
+
+    def url_for_version(self, version):
+	if platform.machine() == "aarch64":
+           url = "https://developer.download.nvidia.com/hpc-sdk/nvhpc_{0}_Linux_{1}_cuda_11.0.tar.gz"
+	else:
+           url = "https://developer.download.nvidia.com/hpc-sdk/nvhpc_{0}_Linux_{1}_cuda_multi.tar.gz"
+	return url.format(version, platform.machine())
+
+    def install(self, spec, prefix):
+
+        os.environ['NVHPC_SILENT'] = "true"
+        os.environ['NVHPC_INSTALL_DIR'] = self.prefix
+        os.environ['NVHPC_DEFAULT_CUDA'] = self.spec.variants['cuda'].value
+        os.environ['NVHPC_INSTALL_TYPE'] = self.spec.variants['network'].value
+        os.environ['NVHPC_INSTALL_LOCAL_DIR'] = self.prefix
+
+        os.system("./install")
+
+    def setup_run_environment(self, env):
+	#TO-DO: Cleaner way to handle path building
+	ver_build = self.version.split("_", 1)[1]
+	target_version = ver_build[:2]+'.'+ver_build[:-1]
+	prefix_new = Prefix(join_path(self.prefix,
+		platform.system()+'_'+platform.machine(), target_version))
+
+	env.set('target', platform.system()+'_'+platform.machine())
+	env.set('version', target_version)
+	env.set('NVHPC', prefix_new)
+
+	env.set('PATH', prefix_new.compilers.bin)
+	env.set('LD_LIBRARY_PATH', prefix_new.compilers.lib)
+	env.set('MANPATH', prefix_new.compilers.man)
+
+        env.set('F77', join_path(prefix_new.compilers.bin, 'nvfortran'))
+        env.set('F90', join_path(prefix_new.compilers.bin, 'nvfortran'))
+        env.set('FC',  join_path(prefix_new.compilers.bin, 'nvfortran'))
+        env.set('CC',  join_path(prefix_new.compilers.bin, 'nvc'))
+        env.set('CXX', join_path(prefix_new.compilers.bin, 'nvc++'))
+        env.set('CPP',  'cpp')
+
+        env.prepend_path('CPATH', prefix_new.math_libs.include)
+        env.prepend_path('CPATH', prefix_new.compilers.include)
+        env.prepend_path('CPATH', prefix_new.cuda.include)
+
+        env.prepend_path('LD_LIBRARY_PATH', prefix_new.cuda.lib64)
+        env.prepend_path('LD_LIBRARY_PATH', prefix_new.math_libs.lib64)
