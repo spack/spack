5da6c93813c89ec286a29160e910ecfb3dda3c33
Jen Herting <jen@herting.cc>
2020-10-12 12:08:53 -0400

    [py-cuml] depends on py-scipy

--- var/spack/repos/builtin/packages/py-cuml/package.py
+++ var/spack/repos/builtin/packages/py-cuml/package.py
@@ -47,6 +47,7 @@ class PyCuml(PythonPackage):
     depends_on('py-cudf', type=('build', 'run'))
     depends_on('cuda')
     depends_on('py-cupy', type=('build', 'run'))
+    depends_on('py-scipy', type=('build', 'run'))
 
     for v in ('11.0', '10.2', '10.1'):
         depends_on(
