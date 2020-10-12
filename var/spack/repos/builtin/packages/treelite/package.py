3b7bb97aee19a8560eaea2be6c83ec6a91659641
Jen Herting <jen@herting.cc>
2020-10-12 14:57:24 -0400

    [treelite] added protobuf variant

--- var/spack/repos/builtin/packages/treelite/package.py
+++ var/spack/repos/builtin/packages/treelite/package.py
@@ -14,3 +14,18 @@ class Treelite(CMakePackage):
     url      = "https://github.com/dmlc/treelite/archive/0.93.tar.gz"
 
     version('0.93',    sha256='7d347372f7fdc069904afe93e69ed0bf696ba42d271fe2f8bf6835d2ab2f45d5')
+
+    variant('protobuf', default=True, description='Build with protobuf')
+
+    depends_on('protobuf', when='+protobuf')
+
+    def cmake_args(self):
+        args = []
+
+        if '+protobuf' in self.spec:
+            args.append('-DENABLE_PROTOBUF:BOOL=ON')
+            args.append('-DProtobuf_LIBRARY={0}'.format(self.spec['protobuf'].prefix))
+        else:
+            args.append('-DENABLE_PROTOBUF:BOOL=OFF')
+
+        return args
