f5fc601d686e3be8e0216de29b8d9cf69e809ce9
Amjad Kotobi <amjadkotbi@gmail.com>
2020-10-13 19:04:12 +0200

    nvidia-hpc-sdk: fix flake8 fix2

--- var/spack/repos/builtin/packages/nvidia-hpc-sdk/package.py
+++ var/spack/repos/builtin/packages/nvidia-hpc-sdk/package.py
@@ -22,36 +22,36 @@ class NvidiaHpcSdk(Package, CudaPackage):
     version('2020_207', sha256='a5c5c8726d2210f2310a852c6d6e03c9ef8c75e3643e9c94e24909f5e9c2ea7a')
 
     variant('network', default='network', description='Network installation',
-      values=('network', 'single'), multi=False)
+            values=('network', 'single'), multi=False)
 
     depends_on('cuda@10:')
     conflicts('^cuda@:10', when='platform=aarch64')
 
     def url_for_version(self, version):
-       if platform.machine() == "aarch64":
-           url = "https://developer.download.nvidia.com/hpc-sdk/nvhpc_{0}_Linux_{1}_cuda_11.0.tar.gz"
-       else:
-           url = "https://developer.download.nvidia.com/hpc-sdk/nvhpc_{0}_Linux_{1}_cuda_multi.tar.gz"
-       return url.format(version, platform.machine())
+        if platform.machine() == "aarch64":
+            url = "https://developer.download.nvidia.com/hpc-sdk/nvhpc_{0}_Linux_{1}_cuda_11.0.tar.gz"
+        else:
+            url = "https://developer.download.nvidia.com/hpc-sdk/nvhpc_{0}_Linux_{1}_cuda_multi.tar.gz"
+        return url.format(version, platform.machine())
 
     def install(self, spec, prefix):
 
         os.environ['NVHPC_SILENT'] = "true"
         os.environ['NVHPC_INSTALL_DIR'] = self.prefix
         os.environ['NVHPC_DEFAULT_CUDA'] =
-           str(self.spec['cuda'].version.up_to(2))
+        str(self.spec['cuda'].version.up_to(2))
         os.environ['NVHPC_INSTALL_TYPE'] = self.spec.variants['network'].value
         os.environ['NVHPC_INSTALL_LOCAL_DIR'] = self.prefix
 
         os.system("./install")
 
     def setup_run_environment(self, env):
-        ver_build=self.version.split("_", 1)[1]
-        target_version = ver_build[:2]+'.'+ver_build[:-1]
-        prefix_new=Prefix(join_path(self.prefix,
-          platform.system()+'_'+platform.machine(), target_version))
+        ver_build = self.version.split("_", 1)[1]
+        target_version = ver_build[:2] + '.' + ver_build[:-1]
+        prefix_new = Prefix(join_path(self.prefix,
+                                      platform.system() + '_' + platform.machine(), target_version))
 
-        env.set('target', platform.system()+'_'+platform.machine())
+        env.set('target', platform.system() + '_' + platform.machine())
         env.set('version', target_version)
         env.set('NVHPC', prefix_new)
 
@@ -61,10 +61,10 @@ class NvidiaHpcSdk(Package, CudaPackage):
 
         env.set('F77', join_path(prefix_new.compilers.bin, 'nvfortran'))
         env.set('F90', join_path(prefix_new.compilers.bin, 'nvfortran'))
-        env.set('FC',  join_path(prefix_new.compilers.bin, 'nvfortran'))
-        env.set('CC',  join_path(prefix_new.compilers.bin, 'nvc'))
+        env.set('FC', join_path(prefix_new.compilers.bin, 'nvfortran'))
+        env.set('CC', join_path(prefix_new.compilers.bin, 'nvc'))
         env.set('CXX', join_path(prefix_new.compilers.bin, 'nvc++'))
-        env.set('CPP',  'cpp')
+        env.set('CPP', 'cpp')
 
         env.prepend_path('CPATH', prefix_new.math_libs.include)
         env.prepend_path('CPATH', prefix_new.compilers.include)
