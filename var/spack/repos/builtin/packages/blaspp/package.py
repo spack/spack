05b3619795bcc3d29cc06aaf60e356e879cd36c7
Gerald Ragghianti <ragghianti@icl.utk.edu>
2020-10-14 16:46:52 -0400

    Changed version name for development branch

--- var/spack/repos/builtin/packages/blaspp/package.py
+++ var/spack/repos/builtin/packages/blaspp/package.py
@@ -13,10 +13,11 @@ class Blaspp(CMakePackage, CudaPackage):
        Knoxville."""
 
     homepage = 'https://bitbucket.org/icl/blaspp'
+    git = homepage
     url = 'https://bitbucket.org/icl/blaspp/downloads/blaspp-2020.09.00.tar.gz'
     maintainers = ['teonnik', 'Sely85', 'G-Ragghianti', 'mgates3']
 
-    version('develop', git=homepage)
+    version('master', branch='master')
     version('2020.10.00', sha256='ce148cfe397428d507c72d7d9eba5e9d3f55ad4cd842e6e873c670183dcb7795')
     version('2020.09.00', sha256='ee5d29171bbed515734007dd121ce2e733e2f83920c4d5ede046e657f4a513ef')
 
@@ -44,6 +45,7 @@ class Blaspp(CMakePackage, CudaPackage):
         ]
 
     def check(self):
+        # If the tester fails to build, ensure that the check() fails.
         if os.path.isfile(join_path(self.build_directory, 'test', 'tester')):
             make('check')
         else:
