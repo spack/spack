317162345996ac985d7803b43918a136f9a7b770
darmac <xiaojun2@hisilicon.com>
2020-10-16 10:46:31 +0800

    Update var/spack/repos/builtin/packages/parquet-format/package.py
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/parquet-format/package.py
+++ var/spack/repos/builtin/packages/parquet-format/package.py
@@ -15,5 +15,5 @@ class ParquetFormat(MavenPackage):
     version('2.8.0', sha256='345c044cea73997162e0c38ae830509ee424faf49c90974e4f244079a3df01b0')
     version('2.7.0', sha256='e821ffc67f61b49afce017ce2d1d402b4df352ca49dbeae167b06c4d3264b6ba')
 
-    depends_on('thrift@0.12.0', when='@2.7.0:' )
+    depends_on('thrift@0.12.0', when='@2.7.0:')
     depends_on('java@8', type=('build', 'run'))
