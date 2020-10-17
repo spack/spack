6c2af34baa0b229c46c41c1ab60eafd2a5168c98
Stephen Hudson <shudson@anl.gov>
2020-10-17 18:19:12 -0500

    Update var/spack/repos/builtin/packages/py-libensemble/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-libensemble/package.py
+++ var/spack/repos/builtin/packages/py-libensemble/package.py
@@ -34,7 +34,7 @@ class PyLibensemble(PythonPackage):
 
     # depends_on('python@2.7:2.8,3.3:', when='@:0.4.1')
     # depends_on('python@3.5:', when='@0.5.0:')
-    depends_on('python@3.5:')
+    depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-psutil', type=('build', 'run'), when='@0.7.1:')
