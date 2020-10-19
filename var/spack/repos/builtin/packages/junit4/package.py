21d69cab424935c5c2efc628e70df4a130bb9f34
darmac <xiaojun2@hisilicon.com>
2020-10-19 10:13:30 +0800

    Update var/spack/repos/builtin/packages/junit4/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/junit4/package.py
+++ var/spack/repos/builtin/packages/junit4/package.py
@@ -15,4 +15,5 @@ class Junit4(MavenPackage):
     version('4.13', sha256='c4e8f5681ad387a386a5aebe05ed4b73ffbfff963e154fbc4d77090f230777c7')
     version('4.12', sha256='9a5b458258c6537df0d2df7122a06895a26b9c7c8061e5991a0be81d76b10d24')
 
-    depends_on('maven@3.0.4')
+    depends_on('java@5:', type=('build', 'run'))
+    depends_on('maven@3.0.4:', type='build')
