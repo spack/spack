63d9c679539adb9a4d665be51eb24704d2f14cb8
Andrew Elble <aweits@rit.edu>
2020-10-21 12:51:51 -0400

    cython is buildtime dep

--- var/spack/repos/builtin/packages/py-fastdtw/package.py
+++ var/spack/repos/builtin/packages/py-fastdtw/package.py
@@ -18,4 +18,4 @@ class PyFastdtw(PythonPackage):
 
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy', type=('build', 'run'))
-    depends_on('py-cython', type=('build', 'run'))
+    depends_on('py-cython', type='build')
