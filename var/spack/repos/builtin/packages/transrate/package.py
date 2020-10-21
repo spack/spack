efae5667bce75e755f39717bf760acdd004405fb
codeandkey <jtst@iastate.edu>
2020-10-21 15:35:40 -0500

    transrate: add macOS binary distribution

--- var/spack/repos/builtin/packages/transrate/package.py
+++ var/spack/repos/builtin/packages/transrate/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
+import sys
 
 
 class Transrate(Package):
@@ -11,9 +12,14 @@ class Transrate(Package):
        analysis."""
 
     homepage = "http://hibberdlab.com/transrate/"
-    url      = "https://bintray.com/artifact/download/blahah/generic/transrate-1.0.3-linux-x86_64.tar.gz"
 
     version('1.0.3', sha256='68d034ecd7012f1d3d505a2edd820c1155cd8b64d2acbf2ac833f30d3800141b')
+
+    def url_for_version(self, ver):
+        osstr = 'osx' if sys.platform == 'darwin' else 'linux-x86_64'
+        base = "https://bintray.com/artifact/download/blahah/generic/transrate-{0}-{1}.tar.gz"
+
+        return base.format(ver.dotted, osstr)
 
     def install(self, spec, prefix):
         install_tree('.', prefix.bin)
