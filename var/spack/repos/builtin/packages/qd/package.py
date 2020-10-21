d3fed3ab290a7cdd9f751df9b3809240c6aaa368
iarspider <iarspider@gmail.com>
2020-10-21 07:26:19 +0200

    Update package.py
    Changes from review

--- var/spack/repos/builtin/packages/qd/package.py
+++ var/spack/repos/builtin/packages/qd/package.py
@@ -21,9 +21,6 @@ class Qd(AutotoolsPackage):
     depends_on('libtool',  type='build')
     depends_on('m4',       type='build')
 
-    def autoreconf(self, spec, prefix):
-        autoreconf('--install', '--verbose', '--force')
-
     def configure_args(self):
         args = ['--enable-shared']
         return args
