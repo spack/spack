3eb7975160701d57b13300e878fbc77497d47f45
Jen Herting <jen@herting.cc>
2020-10-21 10:12:05 -0400

    [orc] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/orc/package.py
+++ var/spack/repos/builtin/packages/orc/package.py
@@ -3,41 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install orc
-#
-# You can edit this file again by typing:
-#
-#     spack edit orc
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Orc(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """the smallest, fastest columnar storage for Hadoop
+    workloads."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://orc.apache.org/"
     url      = "https://github.com/apache/orc/archive/rel/release-1.6.5.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.6.5',  sha256='df5885db8fa2e4435db8d486c6c7fc4e2c565d6197eee27729cf9cbdf36353c0')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('maven')
     depends_on('openssl')
     depends_on('zlib@1.2.11:')
@@ -51,9 +28,6 @@ class Orc(CMakePackage):
     patch('thirdparty.patch')
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = []
         args.append('-DCMAKE_CXX_FLAGS=-fPIC')
         args.append('-DCMAKE_C_FLAGS=-fPIC')
