2860acd0526d9e9c14ed35cc7a11c8baf641fc2c
Jen Herting <jen@herting.cc>
2020-10-21 10:27:26 -0400

    [py-cuml] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-cuml/package.py
+++ var/spack/repos/builtin/packages/py-cuml/package.py
@@ -3,42 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-cuml
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-cuml
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyCuml(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """cuML is a suite of libraries that implement machine
+    learning algorithms and mathematical primitives functions
+    that share compatible APIs with other RAPIDS projects."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rapidsai/cuml"
     url      = "https://github.com/rapidsai/cuml/archive/v0.15.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.15.0',  sha256='b6b37c0f370cd4e881fc24083166ee86a934f1b823159ad36fac6457412c79cd')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@3.7:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-cython', type='build')
@@ -63,9 +40,3 @@ class PyCuml(PythonPackage):
     phases = ['build_ext', 'install']
 
     build_directory = 'python'
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
