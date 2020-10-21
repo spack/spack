f4622073260c1e9398b91c457b08a5768ccd75b3
iarspider <iarspider@gmail.com>
2020-10-21 18:14:45 +0200

    Changes from MR

--- var/spack/repos/builtin/packages/py-gosam/package.py
+++ var/spack/repos/builtin/packages/py-gosam/package.py
@@ -16,7 +16,11 @@ class PyGosam(PythonPackage):
 
     version('2.0.4', sha256='faf621c70f66d9dffc16ac5cce66258067f39f686d722a4867eeb759fcde4f44')
 
-    depends_on('form', type=('build', 'run'))
-    depends_on('qgraf', type=('build', 'run'))
-    depends_on('gosam-contrib', type=('build', 'run'))
+    depends_on('form', type='run')
+    depends_on('qgraf', type='run')
+    depends_on('gosam-contrib', type='link')
     depends_on('python@2.7:2.7.99', type=('build', 'run'))
+
+    def setup_run_environment(self, env):
+        gosam_contrib_lib_dir = self.spec['gosam-contrib'].prefix.lib
+        env.prepend_path('LD_LIBRARY_PATH', gosam_contrib_lib_dir)
