1a87b7ceb3f178a2e61b843f8f2ea5acf2b68d74
Robert QIAO <rob.qiao@flinders.edu.au>
2020-10-22 16:39:32 +1030

    py-cyordereddict URL+dep fix

--- var/spack/repos/builtin/packages/py-cyordereddict/package.py
+++ var/spack/repos/builtin/packages/py-cyordereddict/package.py
@@ -11,8 +11,5 @@ class PyCyordereddict(PythonPackage):
     A drop-in replacement that is 2-6x faster."""
 
     homepage = "https://github.com/shoyer/cyordereddict"
-    url      = "https://files.pythonhosted.org/packages/d1/1a/364cbfd927be1b743c7f0a985a7f1f7e8a51469619f9fefe4ee9240ba210/cyordereddict-1.0.0.tar.gz"
-
+    url      = "https://http://pypi.io/packages/source/c/cyordereddict/cyordereddict-1.0.0.tar.gz"
     version('1.0.0', sha256='d9b2c31796999770801a9a49403b8cb49510ecb64e5d1e9d4763ed44f2d5a76e')
-
-    depends_on('python@2.7:2.8', type=('build', 'run'))
