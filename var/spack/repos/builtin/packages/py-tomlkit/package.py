b55476b66a732679c66e6974b8ec941621e79a58
codeandkey <jtst@iastate.edu>
2020-10-22 12:34:20 -0500

    py-tomlkit: add dependencies

--- var/spack/repos/builtin/packages/py-tomlkit/package.py
+++ var/spack/repos/builtin/packages/py-tomlkit/package.py
@@ -16,3 +16,7 @@ class PyTomlkit(PythonPackage):
 
     depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
+
+    depends_on('py-enum34@1.1:1.999', when='^python@:2', type=('build', 'run'))
+    depends_on('py-functools32@3.2.3:3.999', when='^python@:2', type=('build', 'run'))
+    depends_on('py-typing@3.6:3.999', when='^python@:3.4', type=('build', 'run'))
