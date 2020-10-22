177aca68171c1a0ce4b2dc8d5897f47325300f51
Jen Herting <jen@herting.cc>
2020-10-22 12:20:02 -0400

    [py-cupy] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-cupy/package.py
+++ var/spack/repos/builtin/packages/py-cupy/package.py
@@ -3,42 +3,21 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-cupy
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-cupy
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyCupy(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """CuPy is an open-source array library accelerated with
+    NVIDIA CUDA. CuPy provides GPU accelerated computing with
+    Python. CuPy uses CUDA-related libraries including cuBLAS,
+    cuDNN, cuRand, cuSolver, cuSPARSE, cuFFT and NCCL to make
+    full use of the GPU architecture."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://cupy.dev/"
     git      = "https://github.com/cupy/cupy"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('8.0.0', tag='v8.0.0', submodules=True)
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-fastrlock@0.3:', type=('build', 'run'))
@@ -46,9 +25,3 @@ class PyCupy(PythonPackage):
     depends_on('cuda')
     depends_on('nccl')
     depends_on('cudnn')
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
