c59d50d990003f5a985e0bfcb7372a19f9828416
Tom Payerle <payerle@umd.edu>
2020-10-22 09:40:37 -0400

    py-qsymm: Convert to using tarballs from PyPi instead of git checkouts

--- var/spack/repos/builtin/packages/py-qsymm/package.py
+++ var/spack/repos/builtin/packages/py-qsymm/package.py
@@ -12,16 +12,14 @@ class PyQsymm(PythonPackage):
     constraints and finds the full symmetry group of your Hamiltonian."""
 
     homepage = "https://gitlab.kwant-project.org/qt/qsymm"
-    #url = "https://downloads.kwant-project.org/tinyarray/tinyarray-1.2.3.tar.gz"
+    url = "https://files.pythonhosted.org/packages/ec/a1/e8f8cfdf69b64fefb509258fba4ccac59a9e16bba5e7d838db0ef57ff327/qsymm-1.2.7.tar.gz"
     git = "https://gitlab.kwant-project.org/qt/qsymm.git"
 
     # Add a list of GitHub accounts to notify when the
     # package is updated
     maintainers = ['payerle']
 
-    version('1.2.7', commit='b0362829887dee187029f62192d42e666dedb11a')
-    version('1.2.6', commit='6256f3db38800a4106ec968c7bac8ca9e65bc2e6')
-    version('1.2.5', commit='4d983163d472e1c2735193cd4d0bb016e1338025')
+    version('1.2.7', sha256='9af92a30c3f72883c744d4717e4ec30c8b48121e208c10553e8e0e428fe43bbc')
 
     depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
