a1902b5230bd768d02fc60655d05cc73c1e93466
Jen Herting <jen@herting.cc>
2020-10-22 16:18:42 -0400

    [py-torchfile] depends on py-numpy

--- var/spack/repos/builtin/packages/py-torchfile/package.py
+++ var/spack/repos/builtin/packages/py-torchfile/package.py
@@ -40,6 +40,7 @@ class PyTorchfile(PythonPackage):
     # if you need specific versions. A generic python dependency is
     # added implicity by the PythonPackage class.
     depends_on('python@2.7:2.999,3.4:', type=('build', 'run'))
+    depends_on('py-numpy', type=('build', 'run'))
     # depends_on('py-setuptools', type='build')
     # depends_on('py-foo',        type=('build', 'run'))
 
