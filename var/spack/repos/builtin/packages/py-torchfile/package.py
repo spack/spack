888303e901ae12785134d5cd51b28312ac3d1eb3
Jen Herting <jen@herting.cc>
2020-10-22 16:29:43 -0400

    [py-torchfile] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/py-torchfile/package.py
+++ var/spack/repos/builtin/packages/py-torchfile/package.py
@@ -3,49 +3,20 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-torchfile
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-torchfile
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyTorchfile(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Mostly direct port of the torch7 Lua and C serialization
+    implementation to Python, depending only on numpy (and the
+    standard library: array and struct). Sharing of objects
+    including torch.Tensors is preserved."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/bshillingford/python-torchfile"
     url      = "https://files.pythonhosted.org/packages/source/t/torchfile/torchfile-0.1.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
+    # license = "BSD-3-Clause"
 
     version('0.1.0', sha256='a53dfe134b737845a9f2cb24fe0585317874f965932cebdb0439d13c8da4136e')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@2.7:2.999,3.4:', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
