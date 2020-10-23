f7e7383f92a6888105de062fbf26328a1145ad64
lorddavidiii <maximillian.bensch@aei.mpg.de>
2020-10-23 17:47:38 +0200

    Update asciidoc-py3 and fix build problem (#19491)
    * asciidoc-py3: add new version 9.0.3
    
    * asciidoc-py3: fix build
    
    - add libxml2 and libxslt as dependency

--- var/spack/repos/builtin/packages/asciidoc-py3/package.py
+++ var/spack/repos/builtin/packages/asciidoc-py3/package.py
@@ -17,6 +17,9 @@ class AsciidocPy3(AutotoolsPackage):
     homepage = "https://github.com/asciidoc/asciidoc-py3"
     url      = "https://github.com/asciidoc/asciidoc-py3/releases/download/9.0.2/asciidoc-9.0.2.tar.gz"
 
+    version('9.0.3', sha256='d99c8be8e8a9232742253c2d87c547b2efd4bbd3f0c1e23ef14898ad0fff77c4')
     version('9.0.2', sha256='185fd68e47034c4dd892e1d4ae64c81152bc049e9bdc7d1ad63f927d35810a3b')
 
     depends_on('python@3.5:', type=('build', 'run'))
+    depends_on('libxml2',     type=('build', 'run'))
+    depends_on('libxslt',     type=('build', 'run'))
