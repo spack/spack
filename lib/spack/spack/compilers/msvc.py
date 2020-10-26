e2898dfe51a29f1a508354694bcc03073b7fa968
Ben Cowan <benc@txcorp.com>
2020-10-26 11:52:02 -0600

    Snapshot of some MSVC infrastructure added during experiments a while ago.  Rebasing from spack/develop.

---
+++ lib/spack/spack/compilers/msvc.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.compiler import Compiler, UnsupportedCompilerFlag
+from spack.version import ver
+
+
+class Msvc(Compiler):
+    # Subclasses use possible names of C compiler
+    cc_names = ['cl.exe']
+
+    # Subclasses use possible names of C++ compiler
+    cxx_names = ['cl.exe']
+
+    # Subclasses use possible names of Fortran 77 compiler
+    f77_names = []
+
+    # Subclasses use possible names of Fortran 90 compiler
+    fc_names = []
+
+    # Named wrapper links within build_env_path
+    link_paths = {'cc': 'msvc/cl.exe',
+                  'cxx': 'msvc/cl.exe',
+                  'f77': '',
+                  'fc': ''}
+
+    @classmethod
+    def verbose_flag(cls):
+        return ""
+
+    @property
+    def pic_flag(self):
+        return ""
