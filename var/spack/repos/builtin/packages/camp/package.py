21aced48100abc6bafb176dd7b91c707282a2f52
Chuck Atkins <chuck.atkins@kitware.com>
2020-10-27 09:25:35 -0400

    camp: Add missing checksum

--- var/spack/repos/builtin/packages/camp/package.py
+++ var/spack/repos/builtin/packages/camp/package.py
@@ -14,9 +14,10 @@ class Camp(CMakePackage, CudaPackage):
 
     homepage = "https://github.com/LLNL/camp"
     git      = "https://github.com/LLNL/camp.git"
+    url      = "https://github.com/LLNL/camp/archive/v0.1.0.tar.gz"
 
     version('master', branch='master', submodules='True')
-    version('0.1.0', url='https://github.com/LLNL/camp/archive/v0.1.0.tar.gz')
+    version('0.1.0', sha256='fd4f0f2a60b82a12a1d9f943f8893dc6fe770db493f8fae5ef6f7d0c439bebcc')
 
     depends_on('cmake@3.8:', type='build')
     depends_on('cmake@3.9:', type='build', when="+cuda")
