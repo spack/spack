1f3465c090f471f00755c98f0e0d41bd03c8d004
Geoffrey M Gunter <geoffrey.m.gunter@gmail.com>
2020-10-26 17:03:40 -0700

    date: simplify defining cmake cli args

--- var/spack/repos/builtin/packages/date/package.py
+++ var/spack/repos/builtin/packages/date/package.py
@@ -10,7 +10,7 @@ class Date(CMakePackage):
     """A date and time library based on the C++11/14/17 <chrono> header"""
 
     homepage = "https://github.com/HowardHinnant/date"
-    url = "https://github.com/HowardHinnant/date/archive/v3.0.0.zip"
+    url      = "https://github.com/HowardHinnant/date/archive/v3.0.0.zip"
 
     version('3.0.0', sha256='ddbec664607bb6ec7dd4c7be1f9eefc3d8ce88293ffc9391486ce6ce887ec9b2')
 
@@ -33,26 +33,17 @@ class Date(CMakePackage):
 
     def cmake_args(self):
         spec = self.spec
-        args = []
-
-        args.append('-DCMAKE_CXX_STANDARD={0}'.format(
-            spec.variants['cxxstd'].value))
-        # Require standard at configure time to guarantee the
-        # compiler supports the selected standard.
-        args.append('-DCMAKE_CXX_STANDARD_REQUIRED=ON')
-
-        args.append('-DBUILD_SHARED_LIBS={0}'.format(
-            'ON' if spec.variants['shared'].value else 'OFF'))
-
-        args.append('-DBUILD_TZ_LIB={0}'.format(
-            'ON' if spec.variants['tz'].value else 'OFF'))
+        args = [
+            self.define('CMAKE_CXX_STANDARD', spec.variants['cxxstd'].value),
+            self.define('CMAKE_CXX_STANDARD_REQUIRED', True),
+            self.define('BUILD_SHARED_LIBS', '+shared' in spec),
+            self.define('BUILD_TZ_LIB', '+tz' in spec),
+        ]
 
         tzdb = spec.variants['tzdb'].value
-        if tzdb == 'system':
-            args.append('-DUSE_SYSTEM_TZ_DB=ON')
-        elif tzdb == 'manual':
-            args.append('-DMANUAL_TZ_DB=ON')
-        else:
-            args.extend(['-DUSE_SYSTEM_TZ_DB=OFF', '-DMANUAL_TZ_DB=OFF'])
+        args.extend([
+            self.define('USE_SYSTEM_TZ_DB', tzdb == 'system'),
+            self.define('MANUAL_TZ_DB', tzdb == 'manual'),
+        ])
 
         return args
