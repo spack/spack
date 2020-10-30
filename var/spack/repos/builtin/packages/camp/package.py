9fb2d5521de3f24f970ebf9cb5384ba43f471564
Danny Taller <66029857+dtaller@users.noreply.github.com>
2020-10-30 13:33:54 -0700

    changes to support hip build of camp (#19502)
    * camp: changes to support hip build
    
    * hip: add fallback path for external hip to detect other rocm components
    
    Co-authored-by: Greg Becker <becker33@llnl.gov>

--- var/spack/repos/builtin/packages/camp/package.py
+++ var/spack/repos/builtin/packages/camp/package.py
@@ -22,6 +22,27 @@ class Camp(CMakePackage, CudaPackage):
     depends_on('cmake@3.8:', type='build')
     depends_on('cmake@3.9:', type='build', when="+cuda")
 
+    variant('hip', default=False, description='Enable HIP support')
+
+    # possible amd gpu targets for hip builds
+    # TODO: we should add a hip build system description equivalent to
+    # lib/spack/spack/build_systems/cuda.py, where possible hip amd gpu
+    # architectures are defined in a similar way as for cuda gpu
+    # architectures. In the meantime, require users to define
+    # amd gpu type for hip builds with a variant here.
+    amdgpu_targets = (
+        'gfx701', 'gfx801', 'gfx802', 'gfx803',
+        'gfx900', 'gfx906', 'gfx908', 'gfx1010',
+        'gfx1011', 'gfx1012', 'none'
+    )
+    variant('amdgpu_target', default='none', values=amdgpu_targets)
+
+    depends_on('llvm-amdgpu', when='+hip')
+    depends_on('hip', when='+hip')
+
+    # need amd gpu type for hip builds
+    conflicts('amdgpu_target=none', when='+hip')
+
     def cmake_args(self):
         spec = self.spec
 
@@ -39,6 +60,15 @@ class Camp(CMakePackage, CudaPackage):
                 options.append('-DCMAKE_CUDA_FLAGS:STRING={0}'.format(flag))
         else:
             options.append('-DENABLE_CUDA=OFF')
+
+        if '+hip' in spec:
+            arch = self.spec.variants['amdgpu_target'].value
+            options.extend([
+                '-DENABLE_HIP=ON',
+                '-DHIP_ROOT_DIR={0}'.format(spec['hip'].prefix),
+                '-DHIP_HCC_FLAGS=--amdgpu-target={0}'.format(arch)])
+        else:
+            options.append('-DENABLE_HIP=OFF')
 
         options.append('-DENABLE_TESTS={0}'.format(
             "On" if self.run_tests else "Off"))
