91661ef70df980428d95ed784ce09c9036758c49
Quincy Wofford <quincy.wofford@gmail.com>
2020-10-30 11:08:28 -0700

    XL compiler fix for Paraview v5.6.2
    Build errors related to the eigen library for XL compilers

--- var/spack/repos/builtin/packages/paraview/package.py
+++ var/spack/repos/builtin/packages/paraview/package.py
@@ -144,6 +144,10 @@ class Paraview(CMakePackage, CudaPackage):
     # Broken H5Part with external parallel HDF5
     patch('h5part-parallel.patch', when='@5.7:5.7.999')
 
+    # Broken XL compile at Paraview v5.6.2
+    patch('xl_compiler_pv562.patch', when='@5.6.2%xl')
+    patch('xl_compiler_pv562.patch', when='@5.6.2%xl_r')
+
     def url_for_version(self, version):
         _urlfmt  = 'http://www.paraview.org/files/v{0}/ParaView-v{1}{2}.tar.{3}'
         """Handle ParaView version-based custom URLs."""
