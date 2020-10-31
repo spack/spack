12f32ac14b99c142b998c1c194f69dde1589f8cd
Geoffrey Gunter <12984092+gmgunter@users.noreply.github.com>
2020-10-31 12:04:29 -0700

    range-v3: rename 'develop' version to 'master'
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/range-v3/package.py
+++ var/spack/repos/builtin/packages/range-v3/package.py
@@ -21,7 +21,7 @@ class RangeV3(CMakePackage):
     git      = "https://github.com/ericniebler/range-v3.git"
     maintainers = ['chissg']
 
-    version('develop', branch='master')
+    version('master', branch='master')
     version('0.11.0', sha256='376376615dbba43d3bef75aa590931431ecb49eb36d07bb726a19f680c75e20c')
     version('0.10.0', sha256='5a1cd44e7315d0e8dcb1eee4df6802221456a9d1dbeac53da02ac7bd4ea150cd')
     version('0.5.0', sha256='32e30b3be042246030f31d40394115b751431d9d2b4e0f6d58834b2fd5594280')
