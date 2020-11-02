spack/spack

commit 31f57e56bb3c71f03d2ea2d265d0fa4713f0baa6

Binary caching: use full hashes (#19209)
* "spack install" now has a "--require-full-hash-match" option, which
  forces Spack to skip an available binary package when the full hash
  doesn't match. Normally only a DAG-hash match is required, which
  ensures equivalent Specs, but does not account for changing logic
  inside the associated package.
* Add a local binary cache index which tracks specs that have a binary
  install available in a remote binary cache. It is updated with
  "spack buildcache list" or for a given spec when a binary package
  is retrieved for that Spec.

lib/spack/spack/spec.py
@@ -1640,13 +1640,21 @@ class Spec(object):
         """
         node_list = []
         for s in self.traverse(order='pre', deptype=hash.deptype):
-            node = s.to_node_dict(hash)
-            node[s.name]['hash'] = s.dag_hash()
-            if 'build' in hash.deptype:
-                node[s.name]['build_hash'] = s.build_hash()
-            node_list.append(node)
+            node_list.append(s.node_dict_with_hashes(hash))
 
         return syaml.syaml_dict([('spec', node_list)])
+
+    def node_dict_with_hashes(self, hash=ht.dag_hash):
+        """ Returns a node_dict of this spec with the dag hash added.  If this
+        spec is concrete, the full hash is added as well.  If 'build' is in
+        the hash_type, the build hash is also added. """
+        node = self.to_node_dict(hash)
+        node[self.name]['hash'] = self.dag_hash()
+        if self.concrete:
+            node[self.name]['full_hash'] = self.full_hash()
+        if 'build' in hash.deptype:
+            node[self.name]['build_hash'] = self.build_hash()
+        return node
 
     def to_record_dict(self):
         """Return a "flat" dictionary with name and hash as top-level keys.

lib/spack/spack/schema/spec.py
@@ -81,6 +81,7 @@ properties = {
         ],
         'properties': {
             'hash': {'type': 'string'},
+            'full_hash': {'type': 'string'},
             'version': {
                 'oneOf': [
                     {'type': 'string'},

lib/spack/spack/cmd/buildcache.py
@@ -295,7 +295,7 @@ def match_downloaded_specs(pkgs, allow_multiple_matches=False, force=False,
     specs_from_cli = []
     has_errors = False
 
-    specs = bindist.get_specs()
+    specs = bindist.update_cache_and_get_specs()
     if not other_arch:
         arch = spack.architecture.default_arch().to_spec()
         specs = [s for s in specs if s.satisfies(arch)]
@@ -509,7 +509,7 @@ def install_tarball(spec, args):
 
 def listspecs(args):
     """list binary packages available from mirrors"""
-    specs = bindist.get_specs()
+    specs = bindist.update_cache_and_get_specs()
     if not args.allarch:
         arch = spack.architecture.default_arch().to_spec()
         specs = [s for s in specs if s.satisfies(arch)]

lib/spack/spack/cmd/install.py
@@ -45,6 +45,7 @@ def update_kwargs_from_args(args, kwargs):
         'explicit': True,  # Always true for install command
         'stop_at': args.until,
         'unsigned': args.unsigned,
+        'full_hash_match': args.full_hash_match,
     })
 
     kwargs.update({
@@ -107,6 +108,11 @@ the dependencies"""
         '--no-check-signature', action='store_true',
         dest='unsigned', default=False,
         help="do not check signatures of binary packages")
+    subparser.add_argument(
+        '--require-full-hash-match', action='store_true',
+        dest='full_hash_match', default=False, help="""when installing from
+binary mirrors, do not install binary package unless the full hash of the
+remote spec matches that of the local spec""")
     subparser.add_argument(
         '--show-log-on-error', action='store_true',
         help="print full build log to stderr if build fails")

lib/spack/spack/database.py
@@ -209,7 +209,7 @@ class InstallRecord(object):
 
         for field_name in include_fields:
             if field_name == 'spec':
-                rec_dict.update({'spec': self.spec.to_node_dict()})
+                rec_dict.update({'spec': self.spec.node_dict_with_hashes()})
             elif field_name == 'deprecated_for' and self.deprecated_for:
                 rec_dict.update({'deprecated_for': self.deprecated_for})
             else:
@@ -1112,6 +1112,7 @@ class Database(object):
             # the original hash of concrete specs.
             new_spec._mark_concrete()
             new_spec._hash = key
+            new_spec._full_hash = spec._full_hash
 
         else:
             # If it is already there, mark it as installed and update

lib/spack/spack/installer.py
@@ -213,7 +213,8 @@ def _hms(seconds):
     return ' '.join(parts)
 
 
-def _install_from_cache(pkg, cache_only, explicit, unsigned=False):
+def _install_from_cache(pkg, cache_only, explicit, unsigned=False,
+                        full_hash_match=False):
     """
     Extract the package from binary cache
 
@@ -229,8 +230,8 @@ def _install_from_cache(pkg, cache_only, explicit, unsigned=False):
         (bool) ``True`` if the package was extract from binary cache,
             ``False`` otherwise
     """
-    installed_from_cache = _try_install_from_binary_cache(pkg, explicit,
-                                                          unsigned)
+    installed_from_cache = _try_install_from_binary_cache(
+        pkg, explicit, unsigned=unsigned, full_hash_match=full_hash_match)
     pkg_id = package_id(pkg)
     if not installed_from_cache:
         pre = 'No binary for {0} found'.format(pkg_id)
@@ -302,7 +303,8 @@ def _process_external_package(pkg, explicit):
         spack.store.db.add(spec, None, explicit=explicit)
 
 
-def _process_binary_cache_tarball(pkg, binary_spec, explicit, unsigned):
+def _process_binary_cache_tarball(pkg, binary_spec, explicit, unsigned,
+                                  preferred_mirrors=None):
     """
     Process the binary cache tarball.
 
@@ -312,12 +314,15 @@ def _process_binary_cache_tarball(pkg, binary_spec, explicit, unsigned):
         explicit (bool): the package was explicitly requested by the user
         unsigned (bool): ``True`` if binary package signatures to be checked,
             otherwise, ``False``
+        preferred_mirrors (list): Optional list of urls to prefer when
+            attempting to download the tarball
 
     Return:
         (bool) ``True`` if the package was extracted from binary cache,
             else ``False``
     """
-    tarball = binary_distribution.download_tarball(binary_spec)
+    tarball = binary_distribution.download_tarball(
+        binary_spec, preferred_mirrors=preferred_mirrors)
     # see #10063 : install from source if tarball doesn't exist
     if tarball is None:
         tty.msg('{0} exists in binary cache but with different hash'
@@ -333,7 +338,8 @@ def _process_binary_cache_tarball(pkg, binary_spec, explicit, unsigned):
     return True
 
 
-def _try_install_from_binary_cache(pkg, explicit, unsigned=False):
+def _try_install_from_binary_cache(pkg, explicit, unsigned=False,
+                                   full_hash_match=False):
     """
     Try to extract the package from binary cache.
 
@@ -345,13 +351,18 @@ def _try_install_from_binary_cache(pkg, explicit, unsigned=False):
     """
     pkg_id = package_id(pkg)
     tty.debug('Searching for binary cache of {0}'.format(pkg_id))
-    specs = binary_distribution.get_spec(pkg.spec, force=False)
-    binary_spec = spack.spec.Spec.from_dict(pkg.spec.to_dict())
-    binary_spec._mark_concrete()
-    if binary_spec not in specs:
+    matches = binary_distribution.get_mirrors_for_spec(
+        pkg.spec, force=False, full_hash_match=full_hash_match)
+
+    if not matches:
         return False
 
-    return _process_binary_cache_tarball(pkg, binary_spec, explicit, unsigned)
+    # In the absence of guidance from user or some other reason to prefer one
+    # mirror over another, any match will suffice, so just pick the first one.
+    preferred_mirrors = [match['mirror_url'] for match in matches]
+    binary_spec = matches[0]['spec']
+    return _process_binary_cache_tarball(pkg, binary_spec, explicit, unsigned,
+                                         preferred_mirrors=preferred_mirrors)
 
 
 def _update_explicit_entry_in_db(pkg, rec, explicit):
@@ -1054,6 +1065,7 @@ class PackageInstaller(object):
         unsigned = kwargs.get('unsigned', False)
         use_cache = kwargs.get('use_cache', True)
         verbose = kwargs.get('verbose', False)
+        full_hash_match = kwargs.get('full_hash_match', False)
 
         pkg = task.pkg
         pkg_id = package_id(pkg)
@@ -1065,7 +1077,8 @@ class PackageInstaller(object):
 
         # Use the binary cache if requested
         if use_cache and \
-                _install_from_cache(pkg, cache_only, explicit, unsigned):
+                _install_from_cache(pkg, cache_only, explicit, unsigned,
+                                    full_hash_match):
             self._update_installed(task)
             if task.compiler:
                 spack.compilers.add_compilers_to_config(

lib/spack/spack/schema/buildcache_spec.py
@@ -26,7 +26,6 @@ schema = {
                 'relative_rpaths': {'type': 'boolean'},
             },
         },
-        'full_hash': {'type': 'string'},
         'spec': {
             'type': 'array',
             'items': spack.schema.spec.properties,

commit 3309f19cb80ed5cbbf12df9401af28a644e679b0

bugfix: don't recompute full_hash or build_hash
There was an error introduced in #19209 where `full_hash()` and
`build_hash()` are called on older specs that we've read in from the DB.

Since we evaluate hashes lazily, `node_dict_with_hashes()` has no way of
knowing whether the spec just never had a `full_hash` or a `build_hash`
or whether they need to be lazily computed. When we just had `dag_hash`,
this wasn't an issue since it was always computed when concrete specs
were stored -- it would always be there for specs read in from files.

To get around this ambiguity and to fix the issue, this makes two
changes:

- [x] Always compute `dag_hash`, `build_hash`, and `full_hash` after
      concretization, so we know if one is present, then all should be,
      if they are avaialble.

- [x] Only store `full_hash` and `build_hash` if they are present. If
      not, don't bother recomputing.

This *may* make concretization slightly slower. If it does we could look
at some other way to store whether full_hash and build_hash can be
computed (e.g. we coudl store a `bool` on the `Spec` at concretize time),
or we could look at other options like using JSON for these hashes to
speed them up.

lib/spack/spack/spec.py
@@ -1649,11 +1649,24 @@ class Spec(object):
         spec is concrete, the full hash is added as well.  If 'build' is in
         the hash_type, the build hash is also added. """
         node = self.to_node_dict(hash)
-        node[self.name]['hash'] = self.dag_hash()
+
         if self.concrete:
-            node[self.name]['full_hash'] = self.full_hash()
-        if 'build' in hash.deptype:
-            node[self.name]['build_hash'] = self.build_hash()
+            # if the spec is concrete, all hashes have already been
+            # computed, DAG hash is required, so always add it
+            node[self.name]['hash'] = self.dag_hash()
+
+            # full_hash and build_hash may not have been computed by
+            # older spack versions, so they may not be on a concrete spec
+            # read in from somewhere. We want to avoid computing
+            # full_hash or build_hash on old specs, as we likely don't
+            # have the information needed to do it right (packages,
+            # patches, etc. have likely changed). So we do not attempt to
+            # recompute full_hash or build_hash for old specs, and we
+            # only write these if they're present.
+            if self._full_hash:
+                node[self.name]['full_hash'] = self._full_hash
+            if self._build_hash and 'build' in hash.deptype:
+                node[self.name]['build_hash'] = self._build_hash
         return node
 
     def to_record_dict(self):
@@ -2332,6 +2345,12 @@ class Spec(object):
 
         # Mark everything in the spec as concrete, as well.
         self._mark_concrete()
+
+        # compute all hashes at concretization time, so that we're
+        # guaranteed that if one is there, they're all there.
+        self.dag_hash()
+        self.full_hash()
+        self.build_hash()
 
         # If any spec in the DAG is deprecated, throw an error
         deprecated = []