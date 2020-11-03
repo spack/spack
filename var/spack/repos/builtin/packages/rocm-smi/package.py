e93ac1a8bdcbb4b0155e6463508923d52615ba8b
Sreenivasa Murthy Kolam <67086238+srekolam@users.noreply.github.com>
2020-11-04 00:09:40 +0530

    bump up hsakmt-roct ,hsa-rocr-dev,llvm-amdgpu version for 3.9.0 release (#19668)
    * bump up version for 3.9.0 release
    
    * update version of rocminfo for rocm-3.9.0
    
    * bump up rocm-cmake version for rocm-3.9.0
    
    * bump up rocm-smi and rocmdevice-libs for 3.9.0
    
    * bumpup comgr version for rocm_ 3.9.0
    
    * bump rocm-clang-ocl for rocm-3.9.0
    
    * bump hipify-clang for rocm-3.9.0

--- var/spack/repos/builtin/packages/rocm-smi/package.py
+++ var/spack/repos/builtin/packages/rocm-smi/package.py
@@ -13,10 +13,11 @@ class RocmSmi(MakefilePackage):
        management of your ROCm enabled system"""
 
     homepage = "https://github.com/RadeonOpenCompute/ROC-smi"
-    url      = "https://github.com/RadeonOpenCompute/ROC-smi/archive/rocm-3.8.0.tar.gz"
+    url      = "https://github.com/RadeonOpenCompute/ROC-smi/archive/rocm-3.9.0.tar.gz"
 
     maintainers = ['srekolam', 'arjun-raj-kuppala']
 
+    version('3.9.0', sha256='af3cc6d5e2296f47b1873339faad2d27cf2f24725771bf34c7f644d20cc6ef3b')
     version('3.8.0', sha256='248d9bddc3353c74defd57f203df0648278a4613f2af7fb838d92a4bc8de575d')
     version('3.7.0', sha256='4e34b3b4e409bb89677882f47d9988d56bc2d9bb9893f0712c22a4b73789e06a')
     version('3.5.0', sha256='4f46e947c415a4ac12b9f6989f15a42afe32551706b4f48476fba3abf92e8e7c')
