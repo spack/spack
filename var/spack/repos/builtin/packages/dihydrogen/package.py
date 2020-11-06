e25933a94805e49e141604b3264216506e125aca
Brian C. Van Essen <vanessen1@llnl.gov>
2020-11-06 10:02:40 -0800

    Added code to help DiHydrogen find cuDNN and CUB

--- var/spack/repos/builtin/packages/dihydrogen/package.py
+++ var/spack/repos/builtin/packages/dihydrogen/package.py
@@ -150,4 +150,12 @@ class Dihydrogen(CMakePackage, CudaPackage):
                         ' '.join(self.cuda_flags(cuda_arch))
                     ))
 
+        if '+cudnn' in spec:
+            args.extend(['-DcuDNN_DIR={0}'.format(
+                spec['cudnn'].prefix)])
+
+        if '+cub' in spec:
+            args.extend(['-DCUB_DIR={0}'.format(
+                spec['cub'].prefix)])
+
         return args
