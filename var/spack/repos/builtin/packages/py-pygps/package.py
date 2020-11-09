a6caa1f40a4dfe2838be20c8720daa8527c69594
Toyohisa Kameyama <kameyama@riken.jp>
2020-11-09 09:58:46 +0900

    fixed url.

--- var/spack/repos/builtin/packages/py-pygps/package.py
+++ var/spack/repos/builtin/packages/py-pygps/package.py
@@ -11,7 +11,7 @@ class PyPygps(PythonPackage):
        Regression and Classification."""
 
     homepage = "https://github.com/marionmari/pyGPs"
-    url      = "https://files.pythonhosted.org/packages/05/68/90138cccbdd5aa95b69761d4b3ef7d90594094c574c0696a09af64a6e3c1/pyGPs-1.3.5.tar.gz"
+    url      = "https://pypi.io/packages/source/p/pygps/pyGPs-1.3.5.tar.gz"
 
     version('1.3.5', sha256='5af668415a7bf1666c7c6da3bb09d29e48c395862c6feb23964b476972a015d4')
 
