625d41001128c45f9748cb47b4cb7124e2d5c0b1
Kai Torben Ohlhus <k.ohlhus@gmail.com>
2020-11-09 17:11:14 +0900

    openblas: Add versions 0.3.11 and 0.3.12
    [OpenBLAS 0.3.11](https://github.com/xianyi/OpenBLAS/releases/tag/v0.3.11) and [OpenBLAS 0.3.12](https://github.com/xianyi/OpenBLAS/releases/tag/v0.3.12) were released.

--- var/spack/repos/builtin/packages/openblas/package.py
+++ var/spack/repos/builtin/packages/openblas/package.py
@@ -18,6 +18,8 @@ class Openblas(MakefilePackage):
     git      = 'https://github.com/xianyi/OpenBLAS.git'
 
     version('develop', branch='develop')
+    version('0.3.12', sha256='65a7d3a4010a4e3bd5c0baa41a234797cd3a1735449a4a5902129152601dc57b')
+    version('0.3.11', sha256='bc4617971179e037ae4e8ebcd837e46db88422f7b365325bd7aba31d1921a673')
     version('0.3.10', sha256='0484d275f87e9b8641ff2eecaa9df2830cbe276ac79ad80494822721de6e1693')
     version('0.3.9', sha256='17d4677264dfbc4433e97076220adc79b050e4f8a083ea3f853a53af253bc380')
     version('0.3.8', sha256='8f86ade36f0dbed9ac90eb62575137388359d97d8f93093b38abe166ad7ef3a8')
