37ea99a57b2650d6099b0269258504912b0bace2
Todd Gamblin <tgamblin@llnl.gov>
2020-11-09 03:47:08 -0800

    commands: add `spack tutorial` command (#19808)
    Added a command to set up Spack for our tutorial at
    https://spack-tutorial.readthedocs.io.
    
    The command does some common operations we need first-time users to do.
    Specifically:
    
    - checks out a particular branch of Spack
    - deletes spurious configuration in `~/.spack` that might be
      left over from prior parts of the tutorial
    - adds a mirror and trusts its public key

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -320,7 +320,7 @@ _spack() {
     then
         SPACK_COMPREPLY="-h --help -H --all-help --color -C --config-scope -d --debug --timestamp --pdb -e --env -D --env-dir -E --no-env --use-env-repo -k --insecure -l --enable-locks -L --disable-locks -m --mock -p --profile --sorted-profile --lines -v --verbose --stacktrace -V --version --print-shell-vars"
     else
-        SPACK_COMPREPLY="activate add arch blame build-env buildcache cd checksum ci clean clone commands compiler compilers concretize config containerize create deactivate debug dependencies dependents deprecate dev-build docs edit env extensions external fetch find flake8 gc gpg graph help info install license list load location log-parse maintainers mirror module patch pkg providers pydoc python reindex remove rm repo resource restage setup spec stage test uninstall unload url verify versions view"
+        SPACK_COMPREPLY="activate add arch blame build-env buildcache cd checksum ci clean clone commands compiler compilers concretize config containerize create deactivate debug dependencies dependents deprecate dev-build docs edit env extensions external fetch find flake8 gc gpg graph help info install license list load location log-parse maintainers mirror module patch pkg providers pydoc python reindex remove rm repo resource restage setup spec stage test tutorial uninstall unload url verify versions view"
     fi
 }
 
@@ -1431,6 +1431,10 @@ _spack_test() {
     else
         _tests
     fi
+}
+
+_spack_tutorial() {
+    SPACK_COMPREPLY="-h --help -y --yes-to-all"
 }
 
 _spack_uninstall() {
