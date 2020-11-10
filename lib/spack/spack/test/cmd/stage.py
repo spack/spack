05a935efbd0cbcebf02c9c362e3d8221a7fcd952
TomohiroAkashi <fj3375ce@fujitsu.com>
2020-11-10 10:24:47 +0900

    add test cmd

---
+++ lib/spack/spack/test/cmd/stage.py
@@ -0,0 +1,40 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+import pytest
+
+from llnl.util.filesystem import working_dir
+from spack.util.executable import Executable
+from spack.main import SpackCommand
+from spack.spec import Spec
+
+stage = SpackCommand('stage')
+
+
+@pytest.mark.disable_clean_stage_check
+def test_stage_cmd(mock_packages, mock_fetch, config,
+                   install_mockery):
+
+    spec = Spec('patchcmd')
+    spec.concretize()
+    stage('patchcmd')
+
+    path = spec.package.stage.source_path
+
+    assert os.path.exists(os.path.join(path, 'configure'))
+
+
+@pytest.mark.disable_clean_stage_check
+def test_stage_path(tmpdir, mock_packages, mock_fetch, config,
+                    install_mockery):
+
+    test = os.path.join(tmpdir,'test')
+    spec = Spec('patchcmd')
+    spec.concretize()
+
+    stage('-p', str(test), 'patchcmd')
+
+    assert os.path.exists(os.path.join(test, 'spack-src', 'configure'))
