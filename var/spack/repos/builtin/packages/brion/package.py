adc6f9d46936183e1ffead43961ec133ba7072c1
ppodhajski <podhajski@gmail.com>
2020-11-11 10:02:21 +0100

    check for brain module in specific version of brion only (#956)

--- var/spack/repos/builtin/packages/brion/package.py
+++ var/spack/repos/builtin/packages/brion/package.py
@@ -81,7 +81,8 @@ class Brion(CMakePackage):
             pathname = os.path.join(target, *site_dir)
             if os.path.isdir(pathname):
                 with working_dir(pathname):
-                    if self.spec.version <= Version('3.2.0'):
+                    if self.spec.version >= Version('3.1.0') and \
+                       self.spec.version <= Version('3.2.0'):
                         python('-c', 'import brain; print(brain)')
                     else:
                         python('-c', 'import brion; print(brion)')
