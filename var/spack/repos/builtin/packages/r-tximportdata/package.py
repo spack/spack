24e1deb47df42abcfbac0c10ce51a51b0966e3a2
ajxander12 <ajb12@iastate.edu>
2020-11-12 15:08:48 -0600

    Fixed file format / flake8 errors

--- var/spack/repos/builtin/packages/r-tximportdata/package.py
+++ var/spack/repos/builtin/packages/r-tximportdata/package.py
@@ -3,30 +3,13 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install r-tximportdata
-#
-# You can edit this file again by typing:
-#
-#     spack edit r-tximportdata
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class RTximportdata(RPackage):
-    """This packages provides the output of running various transcript abundance quantifiers on a set of 6 RNA-seq samples from the GEUVADIS project"""
+    """This packages provides the output of running various transcript abundance
+    quantifiers on a set of 6 RNA-seq samples from the GEUVADIS project"""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "http://bioconductor.org/packages/release/data/experiment/html/tximportData.html"
     url      = "http://bioconductor.org/packages/release/data/experiment/src/contrib/tximportData_1.18.0.tar.gz"
 
