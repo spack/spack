69a5518dffb42627f43e3b2bbbf2a0df86f5d9cf
Robert Blake <blake14@llnl.gov>
2020-11-12 10:06:16 -0800

    Moving the nested functions.

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -339,21 +339,6 @@ class Openmpi(AutotoolsPackage):
 
     @classmethod
     def determine_variants(cls, exes, version):
-        def get_spack_compiler_spec(path):
-            spack_compilers = spack.compilers.find_compilers([path])
-            actual_compiler = None
-            # check if the compiler actually matches the one we want
-            for spack_compiler in spack_compilers:
-                if os.path.dirname(spack_compiler.cc) == path:
-                    actual_compiler = spack_compiler
-                    break
-            return actual_compiler.spec if actual_compiler else None
-
-        def is_enabled(text):
-            if text in set(['t', 'true', 'enabled', 'yes', '1']):
-                return True
-            return False
-
         results = []
         for exe in exes:
             variants = ''
@@ -837,3 +822,20 @@ class Openmpi(AutotoolsPackage):
                     tty.debug("File not present: " + exe)
                 else:
                     copy(script_stub, exe)
+
+
+def get_spack_compiler_spec(path):
+    spack_compilers = spack.compilers.find_compilers([path])
+    actual_compiler = None
+    # check if the compiler actually matches the one we want
+    for spack_compiler in spack_compilers:
+        if os.path.dirname(spack_compiler.cc) == path:
+            actual_compiler = spack_compiler
+            break
+    return actual_compiler.spec if actual_compiler else None
+
+
+def is_enabled(text):
+    if text in set(['t', 'true', 'enabled', 'yes', '1']):
+        return True
+    return False
