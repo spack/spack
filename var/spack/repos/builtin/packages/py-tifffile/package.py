22c88464f0c3fcd934685e7ab8100c313a02c7c3
Andreas Baumbach <healther.astro@gmail.com>
2020-11-15 18:51:17 +0100

    add new version py-tifffile@2020.10.1

--- var/spack/repos/builtin/packages/py-tifffile/package.py
+++ var/spack/repos/builtin/packages/py-tifffile/package.py
@@ -14,6 +14,7 @@ class PyTifffile(PythonPackage):
 
     import_modules = ['tifffile']
 
+    version('2020.10.1', sha256='799feeccc91965b69e1288c51a1d1118faec7f40b2eb89ad2979591b85324830')
     version('0.12.1', sha256='802367effe86b0d1e64cb5c2ed886771f677fa63260b945e51a27acccdc08fa1')
 
     depends_on('py-setuptools', type='build')
