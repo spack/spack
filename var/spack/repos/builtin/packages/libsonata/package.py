51ddecb137a0d6b0a711f11113ff3b36bfea39ac
tomdele <42337286+tomdele@users.noreply.github.com>
2020-11-16 14:49:51 +0100

    Add libsonata 0.1.6 (#964)
    * Add libsonata 0.1.6
    
    * Add py-libsonata

--- var/spack/repos/builtin/packages/libsonata/package.py
+++ var/spack/repos/builtin/packages/libsonata/package.py
@@ -17,6 +17,7 @@ class Libsonata(CMakePackage):
     git = "https://github.com/BlueBrain/libsonata.git"
 
     version('develop', branch='master', submodules=False, get_full_repo=True)
+    version('0.1.6', tag='v0.1.6', submodules=False, get_full_repo=True)
     version('0.1.5', tag='v0.1.5', submodules=False, get_full_repo=True)
     version('0.1.4', tag='v0.1.4', submodules=False, get_full_repo=True)
     version('0.1.3', tag='v0.1.3', submodules=False, get_full_repo=True)
