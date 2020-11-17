53b390fddcfe9e546f0271a8417229d5c3370bd9
Andreas Baumbach <healther.astro@gmail.com>
2020-11-17 17:09:04 +0100

    add epilog, also black reformatting

--- lib/spack/spack/cmd/fetch.py
+++ lib/spack/spack/cmd/fetch.py
@@ -19,12 +19,23 @@ level = "long"
 def setup_parser(subparser):
     arguments.add_common_arguments(subparser, ['no_checksum', 'deprecated'])
     subparser.add_argument(
-        '-m', '--missing', action='store_true',
-        help="fetch only missing (not yet installed) dependencies")
+        "-m",
+        "--missing",
+        action="store_true",
+        help="fetch only missing (not yet installed) dependencies",
+    )
     subparser.add_argument(
-        '-D', '--dependencies', action='store_true',
-        help="also fetch all dependencies")
-    arguments.add_common_arguments(subparser, ['specs'])
+        "-D",
+        "--dependencies",
+        action="store_true",
+        help="also fetch all dependencies",
+    )
+    arguments.add_common_arguments(subparser, ["specs"])
+    subparser.epilog = (
+        "With an active environment, the specs "
+        "parameter can be omitted. In this case all (uninstalled"
+        ", in case of --missing) specs from the environment are fetched"
+    )
 
 
 def fetch(parser, args):
@@ -36,20 +47,22 @@ def fetch(parser, args):
         # fetch all uninstalled specs from it otherwise fetch all.
         # If we are also not in an environment, complain to the
         # user that we don't know what to do.
-        env = ev.get_env(args, 'fetch')
+        env = ev.get_env(args, "fetch")
         if env:
             if args.missing:
                 specs = env.uninstalled_specs()
             else:
                 specs = env.all_specs()
             if specs == []:
-                tty.die("No uninstalled specs in environment. Did you "
-                        "run `spack concretize` yet?")
+                tty.die(
+                    "No uninstalled specs in environment. Did you "
+                    "run `spack concretize` yet?"
+                )
         else:
             tty.die("fetch requires at least one spec argument")
 
     if args.no_checksum:
-        spack.config.set('config:checksum', False, scope='command_line')
+        spack.config.set("config:checksum", False, scope="command_line")
 
     if args.deprecated:
         spack.config.set('config:deprecated', True, scope='command_line')
