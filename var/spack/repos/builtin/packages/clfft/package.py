c4a8252f99efbe1a73af555db45cdf6e7632bd35
t-nojiri <tomoyasu.nojiri@riken.jp>
2020-11-17 09:21:09 +0900

    clfft: Changed Patch file

--- var/spack/repos/builtin/packages/clfft/package.py
+++ var/spack/repos/builtin/packages/clfft/package.py
@@ -20,9 +20,8 @@ class Clfft(CMakePackage):
     depends_on('opencl@1.2:')
     depends_on('boost@1.33.0:', when='+client')
 
-    # This patch is required until the following PRs are merged.
-    # https://github.com/clMathLibraries/clFFT/pull/229
-    patch('for_aarch64.patch', when='@2.12.2 target=aarch64:')
+    patch('https://github.com/clMathLibraries/clFFT/commit/eea7dbc888367b8dbea602ba539eb1a9cbc118d9.patch',
+          sha256='3148d5937077def301b30b913bc2437df869204fca1de4385ccd46e3b98b13aa', when='@2.12.2')
 
     root_cmakelists_dir = 'src'
 
