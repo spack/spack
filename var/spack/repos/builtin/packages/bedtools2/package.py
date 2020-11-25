fe046ab019f28d12580b0e391c351e76dedd7354
Robert QIAO <rob.qiao@flinders.edu.au>
2020-11-26 10:21:33 +1030

    bedtool2 revert back to old versioning

--- var/spack/repos/builtin/packages/bedtools2/package.py
+++ var/spack/repos/builtin/packages/bedtools2/package.py
@@ -17,6 +17,10 @@ class Bedtools2(Package):
 
     version('2.29.2', sha256='bc2f36b5d4fc9890c69f607d54da873032628462e88c545dd633d2c787a544a5')
     version('2.27.1', sha256='edcac089d84e63a51f85c3c189469daa7d42180272130b046856faad3cf79112')
+    version('2.27.0', sha256='e91390b567e577d337c15ca301e264b0355441f5ab90fa4f971622e3043e0ca0')
+    version('2.26.0', sha256='15db784f60a11b104ccbc9f440282e5780e0522b8d55d359a8318a6b61897977')
+    version('2.25.0', sha256='159122afb9978015f7ec85d7b17739b01415a5738086b20a48147eeefcf08cfb')
+    version('2.23.0', sha256='9dacaa561d11ce9835d1d51e5aeb092bcbe117b7119663ec9a671abac6a36056')
 
     depends_on('zlib')
     depends_on('python', type='build')
