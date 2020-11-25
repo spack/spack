59aab965e2fa4e8a52bc7b4412fa30dd96f3af97
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2020-11-25 14:37:50 +0000

    Update the packages needed for Isocortex (#968)
    Update the packages needed for Isocortex
    
    placement-algorithm==2.1.1 (update)
    py-diameter-synthesis==0.1.7 (new)
    py-region-grower==0.1.10 (update)
    py-tmd==2.0.9 (update)
    py-tns==2.2.7(update)

--- var/spack/repos/builtin/packages/py-region-grower/package.py
+++ var/spack/repos/builtin/packages/py-region-grower/package.py
@@ -13,12 +13,16 @@ class PyRegionGrower(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/molecularsystems/region-grower"
 
     version('develop', branch='master')
+    version('0.1.10', tag='region-grower-v0.1.10')
     version('0.1.5', tag='region-grower-v0.1.5')
     version('0.1.2', tag='region-grower-v0.1.2')
 
     depends_on('py-setuptools', type='build')
 
     depends_on('py-click@7.0:', type='run')
-    depends_on('py-tqdm@4.0:', type='run')
-    depends_on('py-tns@2.0.4:', type='run')
-    depends_on('py-voxcell@2.5:', type='run')
+    depends_on('py-tqdm@4.28.1:', type='run')
+    depends_on('py-tns@2.2.5:', type='run')
+    depends_on('py-voxcell@2.6.3:', type='run')
+    depends_on('py-diameter-synthesis@0.1.7:', type='run')
+    depends_on('py-morphio@2.3.4:', type='run')
+    depends_on('py-neuroc@0.2.3:', type='run')
