d539952bd697707520fdd1e3117d172c47af7689
Michael Kuhn <michael.kuhn@ovgu.de>
2020-11-28 02:45:23 +0100

    librsvg: Add 2.50.2

--- var/spack/repos/builtin/packages/librsvg/package.py
+++ var/spack/repos/builtin/packages/librsvg/package.py
@@ -12,6 +12,7 @@ class Librsvg(AutotoolsPackage):
     homepage = "https://wiki.gnome.org/Projects/LibRsvg"
     url      = "https://download.gnome.org/sources/librsvg/2.44/librsvg-2.44.14.tar.xz"
 
+    version('2.50.2', sha256='6211f271ce4cd44a7318190d36712e9cea384a933d3e3570004edeb210a056d3')
     version('2.50.0', sha256='b3fadba240f09b9c9898ab20cb7311467243e607cf8f928b7c5f842474ee3df4')
     version('2.44.14', sha256='6a85a7868639cdd4aa064245cc8e9d864dad8b8e9a4a8031bb09a4796bc4e303')
 
