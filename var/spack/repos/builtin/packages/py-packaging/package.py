3d43473041aefbd400c978af040976ca0349e594
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-11-29 19:15:56 -0800

    add tons of changes to support python 2.6 invocations
    create a reasonable variant of python to build from scratch
    
    add todo

--- var/spack/repos/builtin/packages/py-packaging/package.py
+++ var/spack/repos/builtin/packages/py-packaging/package.py
@@ -19,7 +19,7 @@ class PyPackaging(PythonPackage):
     version('16.8', sha256='5d50835fdf0a7edf0b55e311b7c887786504efea1177abd7e69329a8e5ea619e')
 
     depends_on('py-setuptools', type='build')
-    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
+    depends_on('python@2.6:2.8,3.4:', type=('build', 'run'))
     depends_on('py-attrs', when='@19.1', type=('build', 'run'))
     depends_on('py-pyparsing@2.0.2:', type=('build', 'run'))
     depends_on('py-six', type=('build', 'run'))
