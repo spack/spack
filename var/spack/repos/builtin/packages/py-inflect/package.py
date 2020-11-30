56dd18095aa6ffa9cbd888c7713c12dedc0dcf3d
Alex C Leute <aclrc@sporcsubmit.rc.rit.edu>
2020-11-30 16:11:00 -0500

    python package py-inflect added

---
+++ var/spack/repos/builtin/packages/py-inflect/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyInflect(PythonPackage):
+    """inflect.py - Correctly generate plurals, singular nouns, ordinals,
+    indefinite articles; convert numbers to words."""
+
+    homepage = "https://github.com/jaraco/inflect"
+    url      = "https://pypi.io/packages/source/i/inflect/inflect-5.0.2.tar.gz"
+
+    version('5.0.2', sha256='d284c905414fe37c050734c8600fe170adfb98ba40f72fc66fed393f5b8d5ea0')
+
+    depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
