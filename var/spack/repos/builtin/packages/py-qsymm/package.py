6e16bb9fe7c9087c5a67bbde1e03ee47cb09cf09
Tom Payerle <payerle@umd.edu>
2020-12-01 09:34:50 -0500

    py-qsymm: Fix url to use pypi hidden download interface

--- var/spack/repos/builtin/packages/py-qsymm/package.py
+++ var/spack/repos/builtin/packages/py-qsymm/package.py
@@ -12,7 +12,7 @@ class PyQsymm(PythonPackage):
     constraints and finds the full symmetry group of your Hamiltonian."""
 
     homepage = "https://gitlab.kwant-project.org/qt/qsymm"
-    url = "https://files.pythonhosted.org/packages/ec/a1/e8f8cfdf69b64fefb509258fba4ccac59a9e16bba5e7d838db0ef57ff327/qsymm-1.2.7.tar.gz"
+    url = "https://pypi.io/packages/source/q/qsymm/qsymm-1.2.7.tar.gz"
     git = "https://gitlab.kwant-project.org/qt/qsymm.git"
 
     # Add a list of GitHub accounts to notify when the
