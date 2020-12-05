250f88b283d4c9ea76b089750f2e0665492f8c10
dorton21 <odesmond21@gmail.com>
2020-12-04 19:34:20 -0600

    fixes to versions update

--- var/spack/repos/builtin/packages/py-biom-format/package.py
+++ var/spack/repos/builtin/packages/py-biom-format/package.py
@@ -21,15 +21,14 @@ class PyBiomFormat(PythonPackage):
     version('2.1.6', sha256='8eefc275a85cc937f6d6f408d91b7b45eae854cd5d1cbda411a3af51f5b49b0d')
 
     variant('h5py', default=True, description='For use with BIOM 2.0+ files')
-    variant('pyqi', default=False, description='For use with BIOM 2.1.6')
 
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-cython', type='build')
     depends_on('py-h5py', type=('build', 'run'), when='+h5py')
-    depends_on('py-click', type=('build', 'run'))
+    depends_on('py-click', type=('build', 'run'), when='@2.1.5:')
     depends_on('py-numpy@1.3.0:', type=('build', 'run'))
     depends_on('py-future@0.16.0:', type=('build', 'run'))
     depends_on('py-scipy@0.13.0:', type=('build', 'run'))
     depends_on('py-pandas@0.19.2:', type=('build', 'run'))
     depends_on('py-six@1.10.0:', type=('build', 'run'))
-    depends_on('py-pyqi', type=('build', 'run'), when='+pyqi')
+    depends_on('py-pyqi', type=('build', 'run'), when='@:2.1.4')
