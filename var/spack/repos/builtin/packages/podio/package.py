bb368934e14274eaff7506f634135b3c97bf5eed
Valentin Volkl <valentin.volkl@cern.ch>
2020-12-07 10:42:58 +0100

    [podio, edm4hep] update url_for_version

--- var/spack/repos/builtin/packages/podio/package.py
+++ var/spack/repos/builtin/packages/podio/package.py
@@ -52,30 +52,36 @@ class Podio(CMakePackage):
         ]
         return args
 
-    def url_for_version(self, version):
-        # podio releases are dashes and padded with a leading zero
-        # the patch version is omitted when 0
-        # so for example v01-12-01, v01-12 ...
-        base_url = self.url.rsplit('/', 1)[0]
-        major = str(version[0]).zfill(2)
-        minor = str(version[1]).zfill(2)
-        # handle the different cases for the patch version:
-        # first case, no patch version is given in spack, i.e 0.1
-        if len(version) == 2:
-            url = base_url + "/v%s-%s.tar.gz" % (major, minor)
-        # a patch version is specified in spack, i.e. 0.1.x ...
-        elif len(version) == 3:
-            patch = str(version[2]).zfill(2)
-            # ... but it is zero, and not part of the ilc release url
-            if version[2] == 0:
-                url = base_url + "/v%s-%s.tar.gz" % (major, minor)
-            # ... if it is non-zero, it is part  of the release url
-            else:
-                url = base_url + "/v%s-%s-%s.tar.gz" % (major, minor, patch)
-        else:
-            print('Error - Wrong version format provided')
-            return
-        return url
-
     def setup_run_environment(self, env):
         env.prepend_path('PYTHONPATH', self.prefix.python)
+
+		def url_for_version(self, version):
+        """Translate version numbers to ilcsoft conventions.
+        in spack, the convention is: 0.1 (or 0.1.0) 0.1.1, 0.2, 0.2.1 ...
+        in ilcsoft, releases are dashed and padded with a leading zero
+        the patch version is omitted when 0
+        so for example v01-12-01, v01-12 ...
+        :param self: spack package class that has a url
+        :type self: class: `spack.PackageBase`
+        :param version: version
+        :type param: str
+        """
+        base_url = self.url.rsplit('/', 1)[0]
+
+        if len(version) == 1:
+            major = version[0]
+            minor, patch = 0, 0
+        elif len(version) == 2:
+            major, minor = version
+            patch = 0
+        else:
+            major, minor, patch = version
+
+        # By now the data is normalized enough to handle it easily depending
+        # on the value of the patch version
+        if patch == 0:
+            version_str = 'v%02d-%02d.tar.gz' % (major, minor)
+        else:
+            version_str = 'v%02d-%02d-%02d.tar.gz' % (major, minor, patch)
+
+        return base_url + '/' + version_str
