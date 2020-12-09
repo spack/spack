fadad017740ae32420d4941c45e7e5f3c47cbefc
Dave Love <dave.love@manchester.ac.uk>
2020-12-09 21:26:30 +0000

    procenv:  Patch for gcc 10

--- var/spack/repos/builtin/packages/procenv/package.py
+++ var/spack/repos/builtin/packages/procenv/package.py
@@ -5,6 +5,7 @@
 
 from spack import *
 
+
 class Procenv(AutotoolsPackage):
     """A command-line tool that displays as much detail about itself and
 its environment as possible.  It can be used as a test tool, to
@@ -15,6 +16,9 @@ comparing system environments."""
     url      = "https://github.com/jamesodhunt/procenv/archive/0.51.tar.gz"
 
     version('0.51', sha256='b831c14729e06a285cc13eba095817ce3b6d0ddf484b1264951b03ee4fe25bc9')
+
+    # https://github.com/jamesodhunt/procenv/pull/16
+    patch("7cafed1316ddb16fe0689d54ba10c05dd2edd347.patch", when="@:0.51")
 
     depends_on('expat')
     depends_on('libpcap')
