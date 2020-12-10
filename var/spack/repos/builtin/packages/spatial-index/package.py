6011ac51d8d037f24ff6ab8a3afe5600d248b0e7
Antonio Bellotta <antonio.bellotta@epfl.ch>
2020-12-10 22:46:38 +0100

    Update dependencies for Circuit Indexer in SpatialIndex (#984)
    * Update dependencies for Circuit Indexer in SpatialIndex
    
    Co-authored-by: Pramod Kumbhar <pramod.s.kumbhar@gmail.com>

--- var/spack/repos/builtin/packages/spatial-index/package.py
+++ var/spack/repos/builtin/packages/spatial-index/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 from spack import *
 
+
 class SpatialIndex(PythonPackage):
     """Spatial indexer for geometries and morphologies"""
 
@@ -14,6 +15,9 @@ class SpatialIndex(PythonPackage):
     depends_on("py-setuptools")
     depends_on("cmake")
     depends_on("boost")
+    depends_on("py-morphio")
+    depends_on("py-mvdtool~mpi")
+    depends_on("py-morpho-kit")
 
     version('0.2.1', tag='0.2.1', submodules=True)
     version('0.1.0', tag='0.1.0', submodules=True)
