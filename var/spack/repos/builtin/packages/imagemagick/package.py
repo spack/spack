b35ce198d5bb2edd83e75f7ced6e6710477bff4f
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2020-12-14 11:07:32 +0000

    Add missing dependency on pkg-config to imagemagick
    Change-Id: Ie8baebb74ed495d8381dd977ea6b02b37258f4b4

--- var/spack/repos/builtin/packages/imagemagick/package.py
+++ var/spack/repos/builtin/packages/imagemagick/package.py
@@ -28,6 +28,7 @@ class Imagemagick(AutotoolsPackage):
     depends_on('ghostscript')
     depends_on('ghostscript-fonts')
     depends_on('libsm')
+    depends_on('pkg-config', type='build')
 
     def configure_args(self):
         spec = self.spec
