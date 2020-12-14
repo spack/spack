e3fa8d9f52a773eefc1384b86e7048fa8e1a660d
Vicente Bolea <vicente.bolea@gmail.com>
2020-12-14 16:23:50 -0500

    ParaView: add new ParaView-5.9.0-RC2 release (#20222)
    * ParaView: add new ParaView-5.9.0-RC2 release
    
    Signed-off-by: Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
    
    * Update var/spack/repos/builtin/packages/paraview/package.py
    
    Indeed, I misunderstood the previous review. This looks good to me too.
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/paraview/package.py
+++ var/spack/repos/builtin/packages/paraview/package.py
@@ -19,8 +19,9 @@ class Paraview(CMakePackage, CudaPackage):
 
     maintainers = ['chuckatkins', 'danlipsa']
 
-    version('develop', branch='master', submodules=True)
-    version('5.8.1', sha256='7653950392a0d7c0287c26f1d3a25cdbaa11baa7524b0af0e6a1a0d7d487d034')
+    version('master', branch='master', submodules=True)
+    version('5.9.0-RC2', sha256='a3e6f7db7804f11351df48781e8d67ff3e9c18a691bb9ef9a5485aa6520e70bc')
+    version('5.8.1', sha256='7653950392a0d7c0287c26f1d3a25cdbaa11baa7524b0af0e6a1a0d7d487d034', preferred=True)
     version('5.8.0', sha256='219e4107abf40317ce054408e9c3b22fb935d464238c1c00c0161f1c8697a3f9')
     version('5.7.0', sha256='e41e597e1be462974a03031380d9e5ba9a7efcdb22e4ca2f3fec50361f310874')
     version('5.6.2', sha256='1f3710b77c58a46891808dbe23dc59a1259d9c6b7bb123aaaeaa6ddf2be882ea')
