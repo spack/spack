46ab2ac66af2aa3830e4fca6823908e986c4324f
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2020-12-15 16:34:37 +0100

    dealii,piranha: remove directives mentioning non-existing variants

--- var/spack/repos/builtin/packages/piranha/package.py
+++ var/spack/repos/builtin/packages/piranha/package.py
@@ -23,8 +23,8 @@ class Piranha(CMakePackage):
 
     # Build dependencies
     depends_on('cmake@3.2.0:', type='build')
-    extends('python',         when='+pyranha')
-    depends_on('python@2.6:', type='build', when='+pyranha')
+    extends('python',         when='+python')
+    depends_on('python@2.6:', type='build', when='+python')
 
     # Other dependencies
     depends_on('boost+iostreams+regex+serialization',
