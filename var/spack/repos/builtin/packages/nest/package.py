0cf047f6d036dd8ceca0f8a462ce73cc14b8f2cd
Itaru Kitayama <itaru.kitayama@riken.jp>
2020-12-15 11:18:10 +0900

    Change the NEST official releases arvhices url to the current one.

--- var/spack/repos/builtin/packages/nest/package.py
+++ var/spack/repos/builtin/packages/nest/package.py
@@ -13,7 +13,7 @@ class Nest(CMakePackage):
     than on the exact morphology of individual neurons."""
 
     homepage = "http://www.nest-simulator.org"
-    url      = "https://github.com/nest/nest-simulator/releases/download/v2.12.0/nest-2.12.0.tar.gz"
+    url      = "https://github.com/nest/nest-simulator/archive/v2.12.0.tar.gz"
 
     version('2.20.0', sha256='40e33187c22d6e843d80095b221fa7fd5ebe4dbc0116765a91fc5c425dd0eca4')
     version('2.14.0', sha256='d6316d6c9153100a3220488abfa738958c4b65bf2622bd15540e4aa81e79f17f')
