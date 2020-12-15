7e47992e4a023f25e993bb9960ecc73efbe18a0c
Ben Cowan <benc@txcorp.com>
2020-12-15 14:21:38 -0700

    Formatting.

--- var/spack/repos/builtin/packages/py-pyamg/package.py
+++ var/spack/repos/builtin/packages/py-pyamg/package.py
@@ -24,25 +24,19 @@ from spack import *
 
 
 class PyPyamg(PythonPackage):
-  """PyAMG is a library of Algebraic Multigrid (AMG) solvers with
-  a convenient Python interface."""
+    """PyAMG is a library of Algebraic Multigrid (AMG) solvers with
+    a convenient Python interface."""
 
-  homepage = "https://github.com/pyamg/pyamg"
-  url      = "https://github.com/pyamg/pyamg/archive/v4.0.0.zip"
+    homepage = "https://github.com/pyamg/pyamg"
+    url      = "https://github.com/pyamg/pyamg/archive/v4.0.0.zip"
 
-# A list of GitHub accounts to notify when the package is updated.
-  maintainers = []
+    # A list of GitHub accounts to notify when the package is updated.
+    maintainers = []
 
-  version('4.0.0', sha256 = "015d5e706e6e54d3de82e05fdb173c30d8b27cb8a117ab584cd62ad41d9ea042")
+    version('4.0.0', sha256="015d5e706e6e54d3de82e05fdb173c30d8b27cb8a117ab584cd62ad41d9ea042")
 
-# Dependencies. A generic python dependency is added implicity by the
-# PythonPackage class.
-  depends_on("py-setuptools", type = "build")
-  depends_on("py-numpy", type = ("build", "run"))
-  depends_on("py-scipy", type = ("build", "run"))
-
-#   def build_args(self, spec, prefix):
-# # Add arguments other than --prefix.  If not needed delete this
-# # function
-#     args = []
-#     return args
+    # Dependencies. A generic python dependency is added implicity by the
+    # PythonPackage class.
+    depends_on("py-setuptools", type="build")
+    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-scipy", type=("build", "run"))
