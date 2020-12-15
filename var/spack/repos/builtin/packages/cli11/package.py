8a33f36e91ce381003b53647f302f75d8934435c
Ryan Mast <mast9@llnl.gov>
2020-12-14 20:29:14 -0800

    cli11: add new package

---
+++ var/spack/repos/builtin/packages/cli11/package.py
@@ -0,0 +1,45 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install cli11
+#
+# You can edit this file again by typing:
+#
+#     spack edit cli11
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class Cli11(CMakePackage):
+    """CLI11 is a command line parser for C++11 and beyond that provides a rich feature set with a simple and intuitive interface."""
+
+    homepage = "https://cliutils.github.io/CLI11/book/"
+    url      = "https://github.com/CLIUtils/CLI11/archive/v1.9.1.tar.gz"
+    maintainers = ['nightlark']
+
+    version('1.9.1', sha256='c780cf8cf3ba5ec2648a7eeb20a47e274493258f38a9b417628e0576f473a50b')
+
+    depends_on('cmake@3.4:', type='build')
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = [
+            '-DCLI11_BUILD_DOCS=OFF',
+            '-DCLI11_BUILD_TESTS=OFF',
+        ]
+        return args
