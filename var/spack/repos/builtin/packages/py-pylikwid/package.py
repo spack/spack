b63e266c4d0332a6e5c4b982ea03420337108963
Thomas Roehl <Thomas.Roehl@fau.de>
2020-12-15 18:38:11 +0100

    Flake8 over-indent

--- var/spack/repos/builtin/packages/py-pylikwid/package.py
+++ var/spack/repos/builtin/packages/py-pylikwid/package.py
@@ -20,8 +20,9 @@ class PyPylikwid(PythonPackage):
 
     version('0.4.0', sha256='f7894a6d7ebcea7da133ef639599a314f850f55cd6c5ffdd630bb879bd2aa0b8')
 
-    variant('cuda', default=False,
-                    description='with Nvidia GPU profiling support')
+    variant('cuda',
+            default=False,
+            description='with Nvidia GPU profiling support')
 
     depends_on('likwid', when='~cuda')
     depends_on('likwid+cuda', when='+cuda')
