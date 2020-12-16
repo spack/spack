d733069cce26be2cfdd4015968d8a47d7256299c
Ben Cowan <benc@txcorp.com>
2020-12-16 11:19:37 -0700

    Updated Windows platform file to conform to new archspec use.

--- lib/spack/spack/platforms/windows.py
+++ lib/spack/spack/platforms/windows.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import platform
-import llnl.util.cpu as cpu
+import archspec.cpu
 from spack.architecture import Platform, Target
 from spack.operating_systems.windows_os import WindowsOs
 
@@ -16,10 +16,10 @@ class Windows(Platform):
     def __init__(self):
         super(Windows, self).__init__('windows')
 
-        for name in cpu.targets:
+        for name in archspec.cpu.TARGETS:
             self.add_target(name, Target(name))
 
-        self.default = cpu.host().name
+        self.default = archspec.cpu.host().name
         self.front_end = self.default
         self.back_end = self.default
 
