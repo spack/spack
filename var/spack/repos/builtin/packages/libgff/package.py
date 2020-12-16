a33ee291f1d0228b4607ccca88b819cc88425ab1
ajxander12 <ajb12@iastate.edu>
2020-12-16 12:33:45 -0600

    Fixed errors

--- var/spack/repos/builtin/packages/libgff/package.py
+++ var/spack/repos/builtin/packages/libgff/package.py
@@ -10,13 +10,10 @@ class Libgff(CMakePackage):
     """Lightweight GTF/GFF Parser, exposes a C++ interface without drawing in
     a heavyweight dependency."""
 
-    homepage = "https://github.com/COMBINE-lab/libgff/tree/v2.0.0"
+    homepage = "https://github.com/COMBINE-lab/libgff/"
     url      = "https://github.com/COMBINE-lab/libgff/archive/v2.0.0.tar.gz"
 
     # notify when the package is updated.
     maintainers = ['ajxander12']
 
     version('2.0.0', sha256='7656b19459a7ca7d2fd0fcec4f2e0fd0deec1b4f39c703a114e8f4c22d82a99c')
-
-    # depends_on('foo')
-    depends_on('cmake@3.12:', type='build')
