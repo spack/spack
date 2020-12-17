7ac0cca39a0e92e5a26d7b2a7f08f4b33a28fdf9
Jan Ciesko <jciesko@sandia.gov>
2020-12-16 18:36:58 -0600

    Add spack test support for Qthreads

--- var/spack/repos/builtin/packages/qthreads/package.py
+++ var/spack/repos/builtin/packages/qthreads/package.py
@@ -85,3 +85,58 @@ class Qthreads(AutotoolsPackage):
                     % self.spec.variants['scheduler'].value)
 
         return args
+
+
+
+    test_base_path = 'test/basics/'
+    test_list = ['hello_world_multi', 'hello_world']
+
+    @run_after('install')
+    def setup_build_tests(self):
+        """Copy the build test files after the package is installed to an
+        install test subdirectory for use during `spack test run`."""
+        tests = self.test_list
+        relative_test_dir = self.test_base_path
+
+        tests_to_cpy = []
+        header = 'test/argparsing.h'
+        for test in tests:
+            test_path = join_path(relative_test_dir, test + '.c')
+            tests_to_cpy.append(test_path)
+        
+        self.cache_extra_test_sources(tests_to_cpy)
+        self.cache_extra_test_sources(header) 
+
+    def build_tests(self):
+        """Build and run the added smoke (install) test."""
+        tests = self.test_list
+        relative_test_dir = self.test_base_path
+
+        for test in tests:
+            options = [
+               '-I{0}'.format(self.prefix.include),
+               '-I{0}'.format(self.install_test_root + '/test'),
+               join_path(self.install_test_root, relative_test_dir, test + '.c'),
+               '-o',
+               test,
+               '-L{0}'.format(self.prefix.lib),
+               '-lqthread',
+               '{0}{1}'.format(self.compiler.cc_rpath_arg, self.prefix.lib)]
+            reason = 'test:{0}: Checking ability to link to the library.'.format(test)
+            self.run_test('cc', options, [], installed=False, purpose=reason)
+
+    def run_tests(self):
+        tests = self.test_list
+           # Now run the program 
+        for test in tests:
+           reason = 'test:{0}: Checking ability to execute.'.format(test)
+           self.run_test(test, [], purpose=reason)
+
+
+    def test(self):
+        # Build
+        self.build_tests()
+
+        # Run test programs pulled from the build
+        self.run_tests()
+
