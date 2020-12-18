b5a9d588bffd765bfa19a4ba3bd97139f860f008
ajxander12 <ajb12@iastate.edu>
2020-12-18 11:11:40 -0600

    removed extra lines

--- var/spack/repos/builtin/packages/r-circstats/package.py
+++ var/spack/repos/builtin/packages/r-circstats/package.py
@@ -12,7 +12,6 @@ class RCircstats(RPackage):
     homepage = "https://cran.r-project.org/package=CircStats"
     url      = "https://cran.r-project.org/src/contrib/CircStats_0.2-6.tar.gz"
 
-
     version('0.2-6', sha256='8efed93b75b314577341effea214e3dd6e0a515cfe1212eb051047a1f3276f1d')
 
     depends_on('r-boot')
