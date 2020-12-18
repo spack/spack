b59470003aa12aaf84a57c62bb58f3990a0d53c2
ajxander12 <ajb12@iastate.edu>
2020-12-18 11:09:34 -0600

    Added new package r-circstats at version 0.2-6

---
+++ var/spack/repos/builtin/packages/r-circstats/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RCircstats(RPackage):
+    """Circular Statistics, from Topics in Circular Statistics"""
+
+    homepage = "https://cran.r-project.org/package=CircStats"
+    url      = "https://cran.r-project.org/src/contrib/CircStats_0.2-6.tar.gz"
+
+
+    version('0.2-6', sha256='8efed93b75b314577341effea214e3dd6e0a515cfe1212eb051047a1f3276f1d')
+
+    depends_on('r-boot')
+    depends_on('r-mass')
