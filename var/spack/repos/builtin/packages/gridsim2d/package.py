09c8f14b08981ee14e6b9c9dbfa77a2eb94cafb7
Harsh Bhatia <bhatia4@llnl.gov>
2020-12-22 12:06:53 -0800

    new macro version

--- var/spack/repos/builtin/packages/gridsim2d/package.py
+++ var/spack/repos/builtin/packages/gridsim2d/package.py
@@ -19,9 +19,11 @@ class Gridsim2d(MakefilePackage):
     version('v2020-10-09', tag='v2020-10-09')
     version('v2020-10-09.2', tag='v2020-10-09.2')
     version('v2020-10-16-summit', tag='v2020-10-16-summit')
-    version('v2020-10-23-scaling', tag='v2020-10-23-scaling')  ## Added by tomaso, 2020-10-23
-    version('v2020-11-13', tag='v2020-11-13')  ## Added by tomaso, 2020-11-13
+    version('v2020-10-23-scaling', tag='v2020-10-23-scaling')   ## Added by tomaso, 2020-10-23
+    version('v2020-11-13', tag='v2020-11-13')                   ## Added by tomaso, 2020-11-13
     version('v2020-12-04', tag='v2020-12-04')
+    version('v2020-12-21-c3-final', tag='v2020-12-21-c3-final') ## Added by tomaso, 2020-12-21
+    version('v2020-12-22-c3-final', tag='v2020-12-22-c3-final')
 
     depends_on('mpi')
     depends_on('fftw')
