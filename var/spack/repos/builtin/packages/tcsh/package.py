be66aa5432e287546d338f78d78353f217d80cff
Tom Scogland <scogland1@llnl.gov>
2020-12-23 15:33:36 -0800

    make incorrectly instance method static

--- var/spack/repos/builtin/packages/tcsh/package.py
+++ var/spack/repos/builtin/packages/tcsh/package.py
@@ -22,7 +22,10 @@ class Tcsh(AutotoolsPackage):
     version('6.21.00', sha256='c438325448371f59b12a4c93bfd3f6982e6f79f8c5aef4bc83aac8f62766e972')
     version('6.20.00', sha256='b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d')
 
-    def fedora_patch(commit, file, **kwargs):  # noqa
+    @staticmethod
+    def fedora_patch(commit,  # type: str
+                     file,  # type: str
+                     **kwargs):  # noqa
         prefix = 'https://src.fedoraproject.org/rpms/tcsh/raw/{0}/f/'.format(commit)
         patch('{0}{1}'.format(prefix, file), **kwargs)
 
