cac8887cea5ba8c4ab2b92fa3b1787706426512c
Danny McClanahan <mcclanahan7@llnl.gov>
2020-12-22 16:17:54 -0800

    flake flake flake flake flake flake flake flake

--- lib/spack/spack/util/provides.py
+++ lib/spack/spack/util/provides.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 def setup_libtirpc_build_environment(spec, env):
-    """The `libtirpc` package places its headers differently than some of its consumers expect.
+    """The `libtirpc` package places its headers in a weird directory.
 
     The `libtirpc` package provides the virtual dependency `rpc`.
     """
