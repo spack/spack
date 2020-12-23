4adb7c7e8fbe5a94303458c8f908753299735c87
Robert Cohn <rscohn2@gmail.com>
2020-12-23 18:39:40 -0500

    Add Intel oneAPI packages (#20411)
    This creates a set of packages which all use the same script to install
    components of Intel oneAPI. This includes:
    
    * An inheritable IntelOneApiPackage which knows how to invoke the
      installation script based on which components are requested
    * For components which include headers/libraries, an inheritable
      IntelOneApiLibraryPackage is provided to locate them
    * Individual packages for DAL, DNN, TBB, etc.
    * A package for the Intel oneAPI compilers (icx/ifx). This also includes
      icc/ifortran but these are not currently detected in this PR

--- lib/spack/spack/pkgkit.py
+++ lib/spack/spack/pkgkit.py
@@ -20,6 +20,8 @@ from spack.build_systems.aspell_dict import AspellDictPackage
 from spack.build_systems.autotools import AutotoolsPackage
 from spack.build_systems.cmake import CMakePackage
 from spack.build_systems.cuda import CudaPackage
+from spack.build_systems.oneapi import IntelOneApiPackage
+from spack.build_systems.oneapi import IntelOneApiLibraryPackage
 from spack.build_systems.rocm import ROCmPackage
 from spack.build_systems.qmake import QMakePackage
 from spack.build_systems.maven import MavenPackage
