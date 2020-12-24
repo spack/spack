298e461e59943ecdb5c8b382a2b9b9347c2bde08
a-saitoh-fj <a-saitoh@fujitsu.com>
2020-12-24 11:35:46 +0900

    Update to the latest version (1.6.0)

--- var/spack/repos/builtin/packages/py-cloudpickle/package.py
+++ var/spack/repos/builtin/packages/py-cloudpickle/package.py
@@ -12,7 +12,9 @@ class PyCloudpickle(PythonPackage):
     homepage = "https://github.com/cloudpipe/cloudpickle"
     url      = "https://pypi.io/packages/source/c/cloudpickle/cloudpickle-0.5.2.tar.gz"
 
+    version('1.6.0', sha256='9bc994f9e9447593bd0a45371f0e7ac7333710fcf64a4eb9834bf149f4ef2f32')
     version('1.2.1', sha256='603244e0f552b72a267d47a7d9b347b27a3430f58a0536037a290e7e0e212ecf')
     version('0.5.2', sha256='b0e63dd89ed5285171a570186751bc9b84493675e99e12789e9a5dc5490ef554')
 
+    depends_on('python@3.5:',   type=('build', 'run'), when='@1.6.0:')
     depends_on('py-setuptools', type='build')
