763fc989c576cf13cd4f2f42d2604bf8d2c4fecd
Paul R. C. Kent <kentpr@ornl.gov>
2020-12-24 17:46:28 -0500

    New versions: py-sphinxcontrib-bibtex 2.2.0 and py-pybtex-docutils 0.2.2 (#20438)
    * Add v2.0.0
    
    * Add 0.2.2
    
    * Improve packages
    
    * No trailing

--- var/spack/repos/builtin/packages/py-pybtex-docutils/package.py
+++ var/spack/repos/builtin/packages/py-pybtex-docutils/package.py
@@ -12,8 +12,10 @@ class PyPybtexDocutils(PythonPackage):
     homepage = "https://pypi.python.org/pypi/pybtex-docutils/"
     url      = "https://pypi.io/packages/source/p/pybtex-docutils/pybtex-docutils-0.2.1.tar.gz"
 
+    version('0.2.2', sha256='ea90935da188a0f4de2fe6b32930e185c33a0e306154322ccc12e519ebb5fa7d')
     version('0.2.1', sha256='e4b075641c1d68a3e98a6d73ad3d029293fcf9e0773512315ef9c8482f251337')
 
+    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-docutils@0.8:', type=('build', 'run'))
     depends_on('py-pybtex@0.16:', type=('build', 'run'))
--- var/spack/repos/builtin/packages/py-sphinxcontrib-bibtex/package.py
+++ var/spack/repos/builtin/packages/py-sphinxcontrib-bibtex/package.py
@@ -12,14 +12,21 @@ class PySphinxcontribBibtex(PythonPackage):
     homepage = "https://pypi.python.org/pypi/sphinxcontrib-bibtex"
     url      = "https://pypi.io/packages/source/s/sphinxcontrib-bibtex/sphinxcontrib-bibtex-0.3.5.tar.gz"
 
+    version('2.0.0', sha256='98e18eb0b088d3f556199f3fbb91d3d48ebb7596fe86b6c37cc4c4dc5419b7a1')
     version('1.0.0', sha256='629612b001f86784669d65e662377a482052decfd9a0a17c46860878eef7b9e0')
     version('0.3.5', sha256='c93e2b4a0d14f0ab726f95f0a33c1675965e9df3ed04839635577b8f978206cd')
 
+    depends_on('python@2.7:2.8,3.3:', type=('build', 'run'))
+    depends_on('python@3.5:', type=('build', 'run'), when='@1:')
+    depends_on('python@3.6:', type=('build', 'run'), when='@2:')
     depends_on('py-setuptools', type='build')
     depends_on('py-latexcodec@0.3.0:', type=('build', 'run'))
-    depends_on('py-pybtex@0.17:', type=('build', 'run'))
-    depends_on('py-pybtex-docutils@0.2.0:', type=('build', 'run'))
-    depends_on('py-six@1.4.1:', type=('build', 'run'))
-    depends_on('py-sphinx@1.0:', type=('build', 'run'))
+    depends_on('py-pybtex@0.17:', type=('build', 'run'), when='@:1.9.9')
+    depends_on('py-pybtex@0.20:', type=('build', 'run'), when='@2.0.0:')
+    depends_on('py-pybtex-docutils@0.2.0:', type=('build', 'run'), when='@:1.9.9')
+    depends_on('py-pybtex-docutils@0.2.2:', type=('build', 'run'), when='@2.0.0:')
+    depends_on('py-six@1.4.1:', type=('build', 'run'), when='@0.3.5')
+    depends_on('py-sphinx@1.0:', type=('build', 'run'), when='@:1.9.9')
+    depends_on('py-sphinx@2.0:', type=('build', 'run'), when='@2.0.0:')
     depends_on('py-oset@0.1.3:', type=('build', 'run'))
     depends_on('py-ordereddict@1.1:', when='^python@:2.6', type=('build', 'run'))
