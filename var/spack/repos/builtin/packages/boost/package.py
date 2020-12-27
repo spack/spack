0a241b241429f77b2f3ad9e3735d2cb4050686b9
ketsubouchi <kenta.tsubouchi@hac-inc.co.jp>
2020-12-27 21:38:54 +0900

    boost: backport python3 import fix (#20301)

--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -256,6 +256,10 @@ class Boost(Package):
     # See https://github.com/boostorg/build/pull/154
     patch('boost_154.patch', when='@:1.63.99')
 
+    # Backport Python3 import problem
+    # See https://github.com/boostorg/python/pull/218
+    patch('boost_218.patch', when='@:1.67.99')
+
     def patch(self):
         # Disable SSSE3 and AVX2 when using the NVIDIA compiler
         if self.spec.satisfies('%nvhpc'):
