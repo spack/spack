39a5c75a83efcdcff169041e2e2f5be30dfbc7b4
QuellynSnead <quellyn@lanl.gov>
2020-12-28 04:57:03 -0700

    libquo: add pic flags for PGI (#20528)
    Enforce PIC for PGI ((https://github.com/lanl/libquo/issues/41))

--- var/spack/repos/builtin/packages/libquo/package.py
+++ var/spack/repos/builtin/packages/libquo/package.py
@@ -34,7 +34,11 @@ class Libquo(AutotoolsPackage):
         bash('./autogen')
 
     def configure_args(self):
-        return [
+        config_args = [
             'CC={0}'.format(self.spec['mpi'].mpicc),
             'FC={0}'.format(self.spec['mpi'].mpifc)
         ]
+        if '%pgi' in self.spec:
+            config_args.append('CFLAGS={0}'.format(self.compiler.cc_pic_flag))
+            config_args.append('FCFLAGS={0}'.format(self.compiler.fc_pic_flag))
+        return config_args
