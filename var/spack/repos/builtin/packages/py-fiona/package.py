59334323e966a7fe09a4acb4df55e9e85ad40c50
Adam J. Stewart <ajstewart426@gmail.com>
2020-12-28 17:17:48 -0600

    Fix deptypes for some geospatial Python packages

--- var/spack/repos/builtin/packages/py-fiona/package.py
+++ var/spack/repos/builtin/packages/py-fiona/package.py
@@ -20,12 +20,12 @@ class PyFiona(PythonPackage):
     version('1.8.6',  sha256='fa31dfe8855b9cd0b128b47a4df558f1b8eda90d2181bff1dd9854e5556efb3e')
     version('1.7.12', sha256='8b54eb8422d7c502bb7776b184018186bede1a489cf438a7a47f992ade6a0e51')
 
-    depends_on('python@3.6:', type=('build', 'run'), when='@1.9:')
-    depends_on('python@2.6:', type=('build', 'run'))
-    depends_on('gdal@1.11:', type=('build', 'run'), when='@1.9:')
-    depends_on('gdal@1.8:', type=('build', 'run'))
+    depends_on('python@3.6:', type=('build', 'link', 'run'), when='@1.9:')
+    depends_on('python@2.6:', type=('build', 'link', 'run'))
+    depends_on('gdal@1.11:', type=('build', 'link', 'run'), when='@1.9:')
+    depends_on('gdal@1.8:', type=('build', 'link', 'run'))
 
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-cython', type='build', when='@master')
     depends_on('py-attrs@17:', type=('build', 'run'))
     depends_on('py-certifi', type=('build', 'run'), when='@1.8.18')
