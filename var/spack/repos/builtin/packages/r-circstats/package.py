a2225771f1bd1ed3d4f091d276203edeedb1a46a
ajxander12 <ajb12@iastate.edu>
2020-12-29 09:36:06 -0600

    added build args and list url

--- var/spack/repos/builtin/packages/r-circstats/package.py
+++ var/spack/repos/builtin/packages/r-circstats/package.py
@@ -11,8 +11,9 @@ class RCircstats(RPackage):
 
     homepage = "https://cran.r-project.org/package=CircStats"
     url      = "https://cran.r-project.org/src/contrib/CircStats_0.2-6.tar.gz"
+    list_url = "https://cloud.r-project.org/src/contrib/Archive/CircStats/"
 
     version('0.2-6', sha256='8efed93b75b314577341effea214e3dd6e0a515cfe1212eb051047a1f3276f1d')
 
-    depends_on('r-boot')
-    depends_on('r-mass')
+    depends_on('r-boot', type=('build', 'run'))
+    depends_on('r-mass', type=('build', 'run'))
