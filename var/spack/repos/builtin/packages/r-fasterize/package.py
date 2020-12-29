dfd38e92d57aed6dd9161e9b3154f47c66f53cbf
ajxander12 <ajb12@iastate.edu>
2020-12-29 09:15:14 -0600

    added listurl, added build types

--- var/spack/repos/builtin/packages/r-fasterize/package.py
+++ var/spack/repos/builtin/packages/r-fasterize/package.py
@@ -3,7 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
 from spack import *
 
 
@@ -12,11 +11,12 @@ class RFasterize(RPackage):
 
     homepage = "https://cran.r-project.org/package=fasterize"
     url      = "https://cran.r-project.org/src/contrib/fasterize_1.0.3.tar.gz"
+    list_url = "https://cloud.r-project.org/src/contrib/Archive/fasterize/"
 
     version('1.0.3', sha256='62b459625e9bdb00251ec5f6cb873e0c59713f3e86dc1e2c8332adc0cea17f81')
 
-    depends_on('r@3.3.0:')
-    depends_on('r-rcpp')
-    depends_on('r-raster')
-    depends_on('r-sp')
-    depends_on('r-rcpparmadillo')
+    depends_on('r@3.3.0:', type=('build', 'run'))
+    depends_on('r-rcpp', type=('build', 'run'))
+    depends_on('r-raster', type=('build', 'run'))
+    depends_on('r-sp', type=('build', 'run'))
+    depends_on('r-rcpparmadillo', type=('build', 'run'))
