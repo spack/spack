09a5795f134f529bf03a8a4ab804b554e2ed1007
sreenivasa murthy kolam <sreenivasamurthy.kolam@amd.com>
2021-01-01 03:15:31 +0000

    bumpup version for rocm-4.0.0 release

--- var/spack/repos/builtin/packages/roctracer-dev/package.py
+++ var/spack/repos/builtin/packages/roctracer-dev/package.py
@@ -13,10 +13,11 @@ class RoctracerDev(CMakePackage):
        specific runtime profiler to trace API and asyncronous activity."""
 
     homepage = "https://github.com/ROCm-Developer-Tools/roctracer"
-    url      = "https://github.com/ROCm-Developer-Tools/roctracer/archive/rocm-3.10.0.tar.gz"
+    url      = "https://github.com/ROCm-Developer-Tools/roctracer/archive/rocm-4.0.0.tar.gz"
 
     maintainers = ['srekolam', 'arjun-raj-kuppala']
 
+    version('4.0.0', sha256='f47859a46173228b597c463eda850b870e810534af5efd5f2a746067ef04edee')
     version('3.10.0', sha256='ac4a1d059fc34377e906071fd0e56f5434a7e0e4ded9db8faf9217a115239dec')
     version('3.9.0', sha256='0678f9faf45058b16923948c66d77ba2c072283c975d167899caef969169b292')
     version('3.8.0', sha256='5154a84ce7568cd5dba756e9508c34ae9fc62f4b0b5731f93c2ad68b21537ed1')
@@ -28,7 +29,7 @@ class RoctracerDev(CMakePackage):
     depends_on('python@:2', type='build')
     depends_on('py-cppheaderparser', type='build')
 
-    for ver in ['3.5.0', '3.7.0', '3.8.0', '3.9.0', '3.10.0']:
+    for ver in ['3.5.0', '3.7.0', '3.8.0', '3.9.0', '3.10.0', '4.0.0']:
         depends_on('hsakmt-roct@' + ver, type='build', when='@' + ver)
         depends_on('hsa-rocr-dev@' + ver, type='build', when='@' + ver)
         depends_on('rocminfo@' + ver, type='build', when='@' + ver)
