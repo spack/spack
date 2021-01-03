3257ca591e9c4dba3b0691cb12a2511499479290
darmac <xiaojun2@hisilicon.com>
2021-01-03 15:18:16 +0800

    fix missing dependencies

--- var/spack/repos/builtin/packages/dovecot/package.py
+++ var/spack/repos/builtin/packages/dovecot/package.py
@@ -13,3 +13,15 @@ class Dovecot(AutotoolsPackage):
     url      = "https://dovecot.org/releases/2.3/dovecot-2.3.11.3.tar.gz"
 
     version('2.3.11.3', sha256='d3d9ea9010277f57eb5b9f4166a5d2ba539b172bd6d5a2b2529a6db524baafdc')
+
+    depends_on('pkg-config', type='build')
+    depends_on('gettext',    type='build')
+    depends_on('pandoc',     type='build', when='+pandoc')
+
+    variant('pandoc', default=False, description="Build with pandoc")
+
+    def setup_build_environment(self, env):
+        if '+pandoc' in self.spec:
+            env.prepend_path('PANDOC', 'True')
+        else:
+            env.prepend_path('PANDOC', 'False')
