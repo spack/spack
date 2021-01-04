4f25a81d01e7f96543f9e45575e3d5b86bad87ec
Alex C Leute <aclrc@rit.edu>
2021-01-04 14:43:13 -0500

    added package r-reams

---
+++ var/spack/repos/builtin/packages/r-reams/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RReams(RPackage):
+    """reams: Resampling-Based Adaptive Model Selection"""
+
+    homepage = "https://www.example.com"
+    url      = "https://cloud.r-project.org/src/contrib/reams_0.1.tar.gz"
+
+    version('0.1', sha256='ac24ea875b24bd18152afd87538b1f807f442cf2bd1c6ac1a365cf543c88181e')
+
+    extends('r')
+    depends_on('r@2.9.0:', type=('build', 'run'))
+    depends_on('r-leaps', type=('build', 'run'))
+    depends_on('r-mgcv', type=('build', 'run'))
