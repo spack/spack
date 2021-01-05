a1e4465c596d150bd6b831b5206aa1e6d92f4e8f
Robert Cohn <rscohn2@gmail.com>
2021-01-05 12:37:46 -0500

    compiler version format changed (#20671)

--- var/spack/repos/builtin/packages/intel-oneapi-compilers/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-compilers/package.py
@@ -10,7 +10,7 @@ from os import path
 from spack import *
 
 
-releases = {'2021.1':
+releases = {'2021.1.0':
             {'irc_id': '17427', 'build': '2684'}}
 
 
@@ -25,7 +25,7 @@ class IntelOneapiCompilers(IntelOneApiPackage):
 
     homepage = 'https://software.intel.com/content/www/us/en/develop/tools/oneapi/components/dpc-compiler.html'
 
-    version('2021.1', sha256='666b1002de3eab4b6f3770c42bcf708743ac74efeba4c05b0834095ef27a11b9', expand=False)
+    version('2021.1.0', sha256='666b1002de3eab4b6f3770c42bcf708743ac74efeba4c05b0834095ef27a11b9', expand=False)
 
     depends_on('patchelf', type='build')
 
