bb693f68a459564fd93dd4c8f7d46fd5311c6d3f
Andrew Elble <aweits@rit.edu>
2021-01-06 15:51:14 -0500

    py-absl-py: 0.10.0

--- var/spack/repos/builtin/packages/py-absl-py/package.py
+++ var/spack/repos/builtin/packages/py-absl-py/package.py
@@ -16,6 +16,7 @@ class PyAbslPy(PythonPackage):
 
     pypi = "absl-py/absl-py-0.7.0.tar.gz"
 
+    version('0.10.0', sha256='b20f504a7871a580be5268a18fbad48af4203df5d33dbc9272426cb806245a45')
     version('0.7.1', sha256='b943d1c567743ed0455878fcd60bc28ac9fae38d129d1ccfad58079da00b8951')
     version('0.7.0', sha256='8718189e4bd6013bf79910b9d1cb0a76aecad8ce664f78e1144980fabdd2cd23')
     version('0.1.6', sha256='02c577d618a8bc0a2a5d1a51f160d3649745d7a2516d87025322f46ac1391a22')
