23c0e86cefa168d39675d75846504b531a6d6417
Jen Herting <jen@herting.cc>
2021-01-07 16:41:00 -0500

    [py-imageio-ffmpeg] added dependencies

--- var/spack/repos/builtin/packages/py-imageio-ffmpeg/package.py
+++ var/spack/repos/builtin/packages/py-imageio-ffmpeg/package.py
@@ -39,8 +39,8 @@ class PyImageioFfmpeg(PythonPackage):
     # FIXME: Add dependencies if required. Only add the python dependency
     # if you need specific versions. A generic python dependency is
     # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
+    depends_on('python@3.4:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
     # depends_on('py-foo',        type=('build', 'run'))
 
     def build_args(self, spec, prefix):
