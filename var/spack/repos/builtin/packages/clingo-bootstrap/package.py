6d01ea2ca3a7bd2cbc1e4a56b6af26b26ff23011
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-01-07 14:12:25 +0100

    clingo-bootstrap: added more rules for Darwin

--- var/spack/repos/builtin/packages/clingo-bootstrap/package.py
+++ var/spack/repos/builtin/packages/clingo-bootstrap/package.py
@@ -4,12 +4,43 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 from spack.pkg.builtin.clingo import Clingo
 
+import spack.compilers
+
 
 class ClingoBootstrap(Clingo):
     """Clingo with some options used for bootstrapping"""
     variant('build_type', default='Release', values=('Release',),
             description='CMake build type')
 
+    # On Linux we bootstrap with GCC
+    for compiler_spec in [
+        c for c in spack.compilers.supported_compilers()
+        if c != 'gcc'
+    ]:
+        conflicts('%{0}'.format(compiler_spec), when='platform=linux',
+                  msg='GCC is required to bootstrap clingo on Linux')
+    conflicts('%gcc@:5.99.99', when='platform=linux',
+              msg='C++14 support is required to bootstrap clingo on Linux')
+
+    # On Darwin we bootstrap with Apple Clang
+    for compiler_spec in [
+        c for c in spack.compilers.supported_compilers()
+        if c != 'apple-clang'
+    ]:
+        conflicts('%{0}'.format(compiler_spec), when='platform=darwin',
+                  msg='Apple-clang is required to bootstrap clingo on MacOS')
+
+    # Clingo needs the Python module to be usable by Spack
+    conflicts('~python', msg='Python support is required to bootstrap Spack')
+
     def setup_build_environment(self, env):
-        env.set('CXXFLAGS', '-static-libstdc++ -static-libgcc -Wl,--exclude-libs,ALL')
-        env.set('LDFLAGS', '-static-libstdc++ -static-libgcc -Wl,--exclude-libs,ALL')
+        if '%apple-clang platform=darwin' in self.spec:
+            opts = '-mmacosx-version-min=10.13'
+        elif '%gcc platform=linux' in self.spec:
+            opts = '-static-libstdc++ -static-libgcc -Wl,--exclude-libs,ALL'
+        else:
+            msg = 'unexpected compiler for spec "{0}"'.format(self.spec)
+            raise RuntimeError(msg)
+
+        env.set('CXXFLAGS', opts)
+        env.set('LDFLAGS', opts)
