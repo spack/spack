114f6cef6b44cdb8b4b088f4fc9a2969c937244a
Jen Herting <jen@herting.cc>
2021-01-07 15:19:59 -0500

    [py-intel-openmp] created template

---
+++ var/spack/repos/builtin/packages/py-intel-openmp/package.py
@@ -0,0 +1,50 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install py-intel-openmp
+#
+# You can edit this file again by typing:
+#
+#     spack edit py-intel-openmp
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class PyIntelOpenmp(PythonPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+    url      = "https://files.pythonhosted.org/packages/e5/15/3a478d0075660e201cc69801b7f6681dff9c9070278c8de4aa388dea24a8/intel_openmp-2021.1.2-py2.py3-none-macosx_10_15_x86_64.whl"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('2021.1.2', sha256='2af893738b4b06cb0183746f2992169111031340b59c84a0fd4dec1ed66b80f2')
+
+    # FIXME: Add dependencies if required. Only add the python dependency
+    # if you need specific versions. A generic python dependency is
+    # added implicity by the PythonPackage class.
+    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
+    # depends_on('py-setuptools', type='build')
+    # depends_on('py-foo',        type=('build', 'run'))
+
+    def build_args(self, spec, prefix):
+        # FIXME: Add arguments other than --prefix
+        # FIXME: If not needed delete this function
+        args = []
+        return args
