fa7a62dd22d40a7c1cd59020921c721afda85ebe
Jen Herting <jen@herting.cc>
2021-01-07 16:37:30 -0500

    [py-moviepy] added dependencies

--- var/spack/repos/builtin/packages/py-moviepy/package.py
+++ var/spack/repos/builtin/packages/py-moviepy/package.py
@@ -42,6 +42,14 @@ class PyMoviepy(PythonPackage):
     # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
     # depends_on('py-setuptools', type='build')
     # depends_on('py-foo',        type=('build', 'run'))
+    depends_on('py-decorator@4.0.2:4.9999', type=('build', 'run'))
+    depends_on('py-imageio@2.5:2.9999', when='^python@3.4:', type=('build', 'run'))
+    depends_on('py-imageio@2.0:2.4.9999', when='^python@:3.3.9999', type=('build', 'run'))
+    depends_on('py-imageio-ffmpeg@0.2.0:', when='^python@3.4:', type=('build', 'run'))
+    depends_on('py-tqdm@4.11.2:4.9999', type=('build', 'run'))
+    depends_on('py-numpy@1.17.3:', type=('build', 'run'))
+    depends_on('py-requests@2.8.1:2.99999', type=('build', 'run'))
+    depends_on('py-proglog@:1', type=('build', 'run'))
 
     def build_args(self, spec, prefix):
         # FIXME: Add arguments other than --prefix
