9cf5fbc6a9087c8f47df6574738b552e7dc9d07c
Jen Herting <jen@herting.cc>
2021-01-08 11:28:08 -0500

    [py-proglog] removed fixmes, added homepage and description

--- var/spack/repos/builtin/packages/py-proglog/package.py
+++ var/spack/repos/builtin/packages/py-proglog/package.py
@@ -3,49 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-proglog
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-proglog
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyProglog(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Proglog is a progress logging system for Python. It
+    allows to build complex libraries while giving the user
+    control on the management of logs, callbacks and progress
+    bars."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://pypi.org/project/proglog/"
     url      = "https://pypi.io/packages/source/p/proglog/proglog-0.1.9.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.1.9', sha256='d8c4ccbf2138e0c5e3f3fc0d80dc51d7e69dcfe8bfde4cacb566725092a5b18d')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
     depends_on('py-tqdm', type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
