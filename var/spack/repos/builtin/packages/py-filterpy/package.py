b0aab5acb7fb9be08544f1272dac495ad659b0dd
Jen Herting <jen@herting.cc>
2021-01-08 11:00:15 -0500

    [py-filterpy] removed fixmes, added homepage and description

--- var/spack/repos/builtin/packages/py-filterpy/package.py
+++ var/spack/repos/builtin/packages/py-filterpy/package.py
@@ -3,50 +3,20 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-filterpy
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-filterpy
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyFilterpy(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This library provides Kalman filtering and various
+    related optimal and non-optimal filtering software written
+    in Python."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/rlabbe/filterpy/"
     url      = "https://github.com/rlabbe/filterpy/archive/1.4.5.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.4.5', sha256='fc371ad800ca5a5ff8b8352894a09c353b794ccc8b813c03d5187df451ccef3a')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-scipy', type=('build', 'run'))
     depends_on('py-matplotlib', type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
