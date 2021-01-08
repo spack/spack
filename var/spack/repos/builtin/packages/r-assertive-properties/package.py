74c26edd4aeaeea67dc55086d184794472e97108
Alex C Leute <aclrc@rit.edu>
2021-01-08 13:35:25 -0500

    added package r-assertive-properties

---
+++ var/spack/repos/builtin/packages/r-assertive-properties/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RAssertiveProperties(RPackage):
+    """assertive.properties: Assertions to Check Properties of Variables"""
+
+    homepage = "https://www.example.com"
+    url      = "https://cloud.r-project.org/src/contrib/assertive.properties_0.0-4.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/assertive.properties"
+
+    version('0.0-4', sha256='5c0663fecb4b7c30f2e1d65da8644534fcfe97fb3d8b51f74c1327cd14291a6b')
+
+    extends('r')
+    depends_on('r-assertive-base@0.0-7:', type=('build', 'run'))
