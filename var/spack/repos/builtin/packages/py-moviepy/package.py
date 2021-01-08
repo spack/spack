de9688194ed1a4ba3d5811d49fb27eb0fcac15da
Jen Herting <jen@herting.cc>
2021-01-08 11:52:37 -0500

    [py-moviepy] removed fixmes, added homepage and description

--- var/spack/repos/builtin/packages/py-moviepy/package.py
+++ var/spack/repos/builtin/packages/py-moviepy/package.py
@@ -3,45 +3,22 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-moviepy
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-moviepy
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyMoviepy(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """MoviePy is a Python module for video editing, which can
+    be used for basic operations (like cuts, concatenations,
+    title insertions), video compositing (a.k.a. non-linear
+    editing), video processing, or to create advanced effects.
+    It can read and write the most common video formats,
+    including GIF."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://zulko.github.io/moviepy/"
     url      = "https://github.com/Zulko/moviepy/archive/v1.0.3.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.0.3',      sha256='2bc5f749b614d7d4475096ca8cb59681ad2cf332eeb6fd129e6f8a6e6da5836e')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
     depends_on('py-decorator@4.0.2:4.9999', type=('build', 'run'))
     depends_on('py-imageio@2.5:2.9999', when='^python@3.4:', type=('build', 'run'))
     depends_on('py-imageio@2.0:2.4.9999', when='^python@:3.3.9999', type=('build', 'run'))
@@ -50,9 +27,3 @@ class PyMoviepy(PythonPackage):
     depends_on('py-numpy@1.17.3:', type=('build', 'run'))
     depends_on('py-requests@2.8.1:2.99999', type=('build', 'run'))
     depends_on('py-proglog@:1', type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
