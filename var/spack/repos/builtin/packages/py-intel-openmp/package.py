b204d72802835643d79c1e00392a83f918e1deeb
Jen Herting <jen@herting.cc>
2021-01-08 11:06:04 -0500

    [py-intel-openmp] removed fixmes, added homepage and description

--- var/spack/repos/builtin/packages/py-intel-openmp/package.py
+++ var/spack/repos/builtin/packages/py-intel-openmp/package.py
@@ -3,49 +3,25 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-intel-openmp
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-intel-openmp
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyIntelOpenmp(Package):
-    """FIXME: Put a proper description of your package here."""
+    """Intel OpenMP* Runtime Library x86_64 dynamic libraries
+    for macOS*. Intel OpenMP* Runtime Library provides OpenMP
+    API specification support in Intel C Compiler, Intel C++
+    Compiler and Intel Fortran Compiler. It helps to improve
+    performance by creating multithreaded software using shared
+    memory and running on multi-core processor systems."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://pypi.org/project/intel-openmp/"
     url      = "https://files.pythonhosted.org/packages/e5/15/3a478d0075660e201cc69801b7f6681dff9c9070278c8de4aa388dea24a8/intel_openmp-2021.1.2-py2.py3-none-macosx_10_15_x86_64.whl"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
-    #version('2021.1.2', sha256='2af893738b4b06cb0183746f2992169111031340b59c84a0fd4dec1ed66b80f2', expand=False)
     version('2021.1.2-py2.py3-none-manylinux1_x86_64',
             url='https://files.pythonhosted.org/packages/27/92/68c00e053c0e38fc5e7b0eb1a47a048ce499e12829aede84b400a4c38a96/intel_openmp-2021.1.2-py2.py3-none-manylinux1_x86_64.whl',
             sha256='8796797ecae99f39b27065e4a7f1f435e2ca08afba654ca57a77a2717f864dca',
             expand=False)
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
     depends_on('py-pip', type='build')
 
     def install(self, spec, prefix):
