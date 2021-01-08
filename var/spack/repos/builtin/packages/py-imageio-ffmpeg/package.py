ff948bc9d1a90f8e27f2a6bbb2e524647aa9513a
Jen Herting <jen@herting.cc>
2021-01-08 11:15:34 -0500

    [py-imageio-ffmpeg] removed fixmes, added homepage and description

--- var/spack/repos/builtin/packages/py-imageio-ffmpeg/package.py
+++ var/spack/repos/builtin/packages/py-imageio-ffmpeg/package.py
@@ -3,49 +3,23 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-imageio-ffmpeg
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-imageio-ffmpeg
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyImageioFfmpeg(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """The purpose of this project is to provide a simple and
+    reliable ffmpeg wrapper for working with video files. It
+    implements two simple generator functions for reading and
+    writing data from/to ffmpeg, which reliably terminate the
+    ffmpeg process when done. It also takes care of publishing
+    platform-specific wheels that include the binary ffmpeg
+    executables."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/imageio/imageio-ffmpeg"
     url      = "https://github.com/imageio/imageio-ffmpeg/archive/v0.4.3.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.4.3', sha256='906680ae784d79972671c6a0adf5ce1604703506def9096d346f758a6a52bf6f')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@3.4:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('ffmpeg', type='run')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
