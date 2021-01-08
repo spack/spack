e5b7fb44fdc1284f1c194dc91f0d715161577273
Alex C Leute <aclrc@rit.edu>
2021-01-08 13:52:56 -0500

    fixed url issues

--- var/spack/repos/builtin/packages/r-assertive-properties/package.py
+++ var/spack/repos/builtin/packages/r-assertive-properties/package.py
@@ -9,9 +9,9 @@ from spack import *
 class RAssertiveProperties(RPackage):
     """assertive.properties: Assertions to Check Properties of Variables"""
 
-    homepage = "https://www.example.com"
+    homepage = "https://cloud.r-project.org/package=assertive-properties"
     url      = "https://cloud.r-project.org/src/contrib/assertive.properties_0.0-4.tar.gz"
-    list_url = "https://cran.r-project.org/src/contrib/Archive/assertive.properties"
+    list_url = "https://cloud.r-project.org/src/contrib/Archive/assertive.properties"
 
     version('0.0-4', sha256='5c0663fecb4b7c30f2e1d65da8644534fcfe97fb3d8b51f74c1327cd14291a6b')
 
