22234ab59ce916aa08b3d83801cba265c9cb0946
amd-toolchain-support <toolchainsupport@amd.com>
2021-01-11 14:48:51 +0530

    fixing style issues

--- var/spack/repos/builtin/packages/wrf/package.py
+++ var/spack/repos/builtin/packages/wrf/package.py
@@ -261,8 +261,9 @@ class Wrf(Package):
                         ofh.write(line)
 
         if self.spec.satisfies("@4.2 %aocc"):
-            # In version 4.2 the file to be patched is called configure.defaults, while 
-            # in earlier versions it's configure_new.defaults
+            # In version 4.2 the file to be patched is called 
+            # configure.defaults, while in earlier versions 
+            # it's configure_new.defaults
             rename(
                 "./arch/configure.defaults",
                 "./arch/configure.defaults.bak",
