64f30c7baa6e52564c53dd2b234f1192ba8718fd
Doug Heckman <dahdco@rit.edu>
2021-01-11 14:41:28 -0500

    [py-scikit-fmm] Removed redundant depends_on

--- var/spack/repos/builtin/packages/py-scikit-fmm/package.py
+++ var/spack/repos/builtin/packages/py-scikit-fmm/package.py
@@ -18,6 +18,5 @@ class PyScikitFmm(PythonPackage):
     version('master', branch='master')
     version('2019.1.30', sha256='eb64b6d8e30b8df8f8636d5fc4fd7ca6a9b05938ccd62518c80c1d9e823069dd')
 
-    depends_on('python', type=('build', 'run'))
     depends_on('py-numpy@1.0.2:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
