17455add4000d5440ecccec6d56df116449c4e53
Jen Herting <jen@herting.cc>
2021-01-12 10:29:03 -0500

    [py-bayesian-optimization] added dependency on setuptools

--- var/spack/repos/builtin/packages/py-bayesian-optimization/package.py
+++ var/spack/repos/builtin/packages/py-bayesian-optimization/package.py
@@ -15,6 +15,7 @@ class PyBayesianOptimization(PythonPackage):
 
     version('1.2.0', sha256='34e2444ec0649fd3391738fd388a6bed82a8c0a3702d53d18e8eb3a57b415b38')
 
+    depends_on('py-setuptools', type='build')
     depends_on("py-numpy@1.9.0:", type=('build', 'run'))
     depends_on("py-scipy@0.14.0:", type=('build', 'run'))
     depends_on("py-scikit-learn@0.18.0:", type=('build', 'run'))
