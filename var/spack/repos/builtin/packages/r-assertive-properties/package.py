18d08b505e772538cf770a0c52829f86f0927e43
Alex Leute <acl2809@rit.edu>
2021-01-13 16:02:16 -0500

    fixing some mistakes in dependencies and in urls

--- var/spack/repos/builtin/packages/r-assertive-properties/package.py
+++ var/spack/repos/builtin/packages/r-assertive-properties/package.py
@@ -9,11 +9,12 @@ from spack import *
 class RAssertiveProperties(RPackage):
     """assertive.properties: Assertions to Check Properties of Variables"""
 
-    homepage = "https://cloud.r-project.org/package=assertive-properties"
+    homepage = "https://cloud.r-project.org/package=assertive.properties"
     url      = "https://cloud.r-project.org/src/contrib/assertive.properties_0.0-4.tar.gz"
     list_url = "https://cloud.r-project.org/src/contrib/Archive/assertive.properties"
 
     version('0.0-4', sha256='5c0663fecb4b7c30f2e1d65da8644534fcfe97fb3d8b51f74c1327cd14291a6b')
 
     extends('r')
+    depends_on('r@3.0.0:', type=('build', 'run'))
     depends_on('r-assertive-base@0.0-7:', type=('build', 'run'))
