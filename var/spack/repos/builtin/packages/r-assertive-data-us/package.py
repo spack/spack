77b6faba659415b3ff91a5f62396893f66649244
Alex Leute <acl2809@rit.edu>
2021-01-14 15:39:18 -0500

    added package r-assertive-data-us

---
+++ var/spack/repos/builtin/packages/r-assertive-data-us/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class RAssertiveDataUs(RPackage):
+    """assertive.data.us: Assertions to Check Properties of Strings"""
+
+    homepage = "https://cran.r-project.org/package=assertive.data.us"
+    url      = "https://cloud.r-project.org/src/contrib/assertive.data.us_0.0-2.tar.gz"
+    list_url = "https://cran.r-project.org/src/contrib/Archive/assertive.data.us"
+
+    version('0.0-2', sha256='180e64dfe6339d25dd27d7fe9e77619ef697ef6e5bb6a3cf4fb732a681bdfaad')
+
+    extends('r')
+    depends_on('r@3.0.0:', type=('build', 'run'))
+    depends_on('r-asseritve-base@0.0-2:', type=('build', 'run'))
+    depends_on('r-assertive-strings', type=('build', 'run'))
