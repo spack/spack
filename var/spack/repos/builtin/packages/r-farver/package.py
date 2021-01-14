6c01fe31e10c5e0e307cca4bf8d55cf00eb6d51d
t-nojiri <tomoyasu.nojiri@riken.jp>
2021-01-14 15:36:27 +0900

    r-farver: Fix checksum for 2.0.1, Add version 2.0.3

--- var/spack/repos/builtin/packages/r-farver/package.py
+++ var/spack/repos/builtin/packages/r-farver/package.py
@@ -13,4 +13,6 @@ class RFarver(RPackage):
     url      = "https://cloud.r-project.org/src/contrib/farver_2.0.1.tar.gz"
     list_url = "https://cloud.r-project.org/src/contrib/Archive/farver"
 
-    version('2.0.1', sha256='1642ca1519ef80616ab044ae7f6eaf464118356f2a7875e9d0e3df60ca84012b')
+    version('2.0.3', sha256='0e1590df79ec6078f10426411b96216b70568a4eaf3ffd84ca723add0ed8e5cc')
+    version('2.0.1', sha256='71473e21727357084c6aec4bb9bb258a6797a0f676b4b27504a03f16aa2f4e54',
+            url="https://cloud.r-project.org/src/contrib/Archive/farver/farver_2.0.1.tar.gz")
