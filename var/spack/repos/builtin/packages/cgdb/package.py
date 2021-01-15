a1c33e73de1c94a67a34a923e16642f28febca9a
t-nojiri <tomoyasu.nojiri@riken.jp>
2021-01-15 10:52:28 +0900

    cgdb: Add texinfo depend

--- var/spack/repos/builtin/packages/cgdb/package.py
+++ var/spack/repos/builtin/packages/cgdb/package.py
@@ -17,6 +17,7 @@ class Cgdb(AutotoolsPackage):
     # Required dependency
     depends_on('ncurses')
     depends_on('readline')
+    depends_on('texinfo', type='build')
 
     def configure_args(self):
         spec = self.spec
