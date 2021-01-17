ebf168338c579a5b3cf91719b9864c62be15f69f
Glenn P Johnson <glenn-johnson@uiowa.edu>
2021-01-17 14:57:59 -0600

    Improve formatting of description
    In case the list gets formatted as a non-list:
    - added semicolons to end of list items
    - replaced dashes with [#]

--- var/spack/repos/builtin/packages/r-gtools/package.py
+++ var/spack/repos/builtin/packages/r-gtools/package.py
@@ -11,33 +11,34 @@ class RGtools(RPackage):
 
     Functions to assist in R programming.
     Including:
-    - assist in developing, updating, and maintaining R and R packages
-      ('ask', 'checkRVersion', 'getDependencies', 'keywords', 'scat')
-    - calculate the logit and inverse logit transformations
-      ('logit', 'inv.logit')
-    - test if a value is missing, empty or contains only NA and NULL values
-      ('invalid')
-    - manipulate R's .Last function ('addLast')
-    - define macros ('defmacro')
-    - detect odd and even integers ('odd', 'even')
-    - convert strings containing non-ASCII characters (like single
-      quotes) to plain ASCII ('ASCIIfy')
-    - perform a binary search ('binsearch')
-    - sort strings containing both numeric and character components
-      ('mixedsort')
-    - create a factor variable from the quantiles of a continuous variable
-      ('quantcut')
-    - enumerate permutations and combinations ('combinations', 'permutation')
-    - calculate and convert between fold-change and log-ratio
-      ('foldchange', 'logratio2foldchange', 'foldchange2logratio')
-    - calculate probabilities and generate random numbers from Dirichlet
-      distributions ('rdirichlet', 'ddirichlet')
-    - apply a function over adjacent subsets of a vector ('running')
-    - modify the TCP_NODELAY ('de-Nagle') flag for socket objects
-    - efficient 'rbind' of data frames, even if the column names don't
-      match ('smartbind')
-    - generate significance stars from p-values ('stars.pval')
-    - convert characters to/from ASCII codes.
+    [1] assist in developing, updating, and maintaining R and R packages
+    ('ask', 'checkRVersion', 'getDependencies', 'keywords', 'scat');
+    [2] calculate the logit and inverse logit transformations ('logit',
+    'inv.logit');
+    [3] test if a value is missing, empty or contains only NA and NULL values
+    ('invalid');
+    [4] manipulate R's .Last function ('addLast');
+    [5] define macros ('defmacro');
+    [6] detect odd and even integers ('odd', 'even');
+    [7] convert strings containing non-ASCII characters (like single
+     quotes) to plain ASCII ('ASCIIfy');
+    [8] perform a binary search ('binsearch');
+    [9] sort strings containing both numeric and character components
+    ('mixedsort');
+    [10] create a factor variable from the quantiles of a continuous variable
+    ('quantcut');
+    [11] enumerate permutations and combinations ('combinations',
+    'permutation');
+    [12] calculate and convert between fold-change and log-ratio ('foldchange',
+    'logratio2foldchange', 'foldchange2logratio');
+    [13] calculate probabilities and generate random numbers from Dirichlet
+    distributions ('rdirichlet', 'ddirichlet');
+    [14] apply a function over adjacent subsets of a vector ('running');
+    [15] modify the TCP_NODELAY ('de-Nagle') flag for socket objects;
+    [16] efficient 'rbind' of data frames, even if the column names don't match
+    ('smartbind');
+    [17] generate significance stars from p-values ('stars.pval');
+    [18] convert characters to/from ASCII codes;
     """
 
     homepage = "https://cloud.r-project.org/package=gtools"
