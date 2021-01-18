56280eca0130d2ad1af3022ed2ad66369adbf76e
Andrew Elble <aweits@rit.edu>
2021-01-18 14:01:43 -0500

    py-tensorflow-estimator: update for 2.4.0

--- var/spack/repos/builtin/packages/py-tensorflow-estimator/package.py
+++ var/spack/repos/builtin/packages/py-tensorflow-estimator/package.py
@@ -15,6 +15,7 @@ class PyTensorflowEstimator(Package):
 
     maintainers = ['aweits']
 
+    version('2.4.0', sha256='e6ea12014c3d8c89a81ace95f8f8b7c39ffcd3e4e4626709e4aee0010eefd962')
     version('2.3.0', sha256='75403e7de7e8ec30ec0781ede56ed84cbe5e90daad64a9c242cd489c8fe63a17')
     version('2.2.0', sha256='2d68cb6e6442e7dcbfa2e092aa25bdcb0eda420536a829b85d732854a4c85d46')
     version('2.1', sha256='1d74c8181b981748976fa33ad97d3434c3cf2b7e29a0b00861365fe8329dbc4e')
@@ -23,16 +24,12 @@ class PyTensorflowEstimator(Package):
 
     extends('python')
 
-    depends_on('py-tensorflow@2.3.0:',  when='@2.3.0')
-    depends_on('py-tensorflow@2.2.0',  when='@2.2.0')
+    depends_on('py-tensorflow@2.4.0:2.4.999',  when='@2.4.0')
+    depends_on('py-tensorflow@2.3.0:2.3.999',  when='@2.3.0')
+    depends_on('py-tensorflow@2.2.0:2.2.999',  when='@2.2.0')
     depends_on('py-tensorflow@2.1.0:2.1.999',  when='@2.1')
     depends_on('py-tensorflow@2.0.0:2.0.999',  when='@2.0.0')
     depends_on('py-tensorflow@1.13.1', when='@1.13.0')
-
-    depends_on('py-absl-py@0.7.0:', type=('build', 'run'), when='@1.12.1,1.14:')
-    depends_on('py-numpy@1.16.0:1.18',  type=('build', 'run'), when='@1.13.2,1.15:')
-    depends_on('py-six@1.12.0:', type=('build', 'run'), when='@2.1:')
-    depends_on('py-keras-preprocessing@1.1.1:1.999', type=('build', 'run'), when='@2.3:')
 
     depends_on('bazel@0.19.0:', type='build')
     depends_on('py-funcsigs@1.0.2:', type=('build', 'run'))
@@ -59,6 +56,7 @@ class PyTensorflowEstimator(Package):
             '--verbose_failures',
             # Show (formatted) subcommands being executed
             '--subcommands=pretty_print',
+            '--spawn_strategy=local',
             # Ask bazel to explain what it's up to
             # Needs a filename as argument
             '--explain=explainlogfile.txt',
