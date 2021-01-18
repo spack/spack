91fcd49484db06f8a534a002ca86f412f35527b2
Glenn Johnson <glenn-johnson@uiowa.edu>
2021-01-18 12:55:18 -0600

    add version 1.0-6 to r-libcoin (#21075)

--- var/spack/repos/builtin/packages/r-libcoin/package.py
+++ var/spack/repos/builtin/packages/r-libcoin/package.py
@@ -7,7 +7,9 @@ from spack import *
 
 
 class RLibcoin(RPackage):
-    """Basic infrastructure for linear test statistics and permutation
+    """Linear Test Statistics for Permutation Inference
+
+    Basic infrastructure for linear test statistics and permutation
     inference in the framework of Strasser and Weber (1999)
     <http://epub.wu.ac.at/102/>. This package must not be used by end-users.
     CRAN package 'coin' implements all user interfaces and is ready to be used
@@ -17,6 +19,7 @@ class RLibcoin(RPackage):
     url      = "https://cloud.r-project.org/src/contrib/libcoin_1.0-4.tar.gz"
     list_url = "https://cloud.r-project.org/src/contrib/Archive/libcoin"
 
+    version('1.0-6', sha256='48afc1415fc89b29e4f2c8b6f6db3cffef1531580e5c806ad7cacf4afe6a4e5a')
     version('1.0-4', sha256='91dcbaa0ab8c2109aa54c3eda29ad0acd67c870efcda208e27acce9d641c09c5')
 
     depends_on('r@3.4.0:', type=('build', 'run'))
