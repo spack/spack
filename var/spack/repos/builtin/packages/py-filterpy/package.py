c3646413790c2d07d609a88020c75697e5b40418
Jen Herting <jen@herting.cc>
2021-01-21 17:58:15 -0500

    [py-filterpy] url -> pypi

--- var/spack/repos/builtin/packages/py-filterpy/package.py
+++ var/spack/repos/builtin/packages/py-filterpy/package.py
@@ -12,7 +12,7 @@ class PyFilterpy(PythonPackage):
     in Python."""
 
     homepage = "https://github.com/rlabbe/filterpy/"
-    url = "https://pypi.io/packages/source/f/filterpy/filterpy-1.4.5.zip"
+    pypi = "filterpy/filterpy-1.4.5.zip"
 
     version('1.4.5', sha256='4f2a4d39e4ea601b9ab42b2db08b5918a9538c168cff1c6895ae26646f3d73b1')
 
