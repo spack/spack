6c2ca1a9b01fa4e7189d7ea1475fda427ab7c1ec
iarspider <iarspider@gmail.com>
2021-01-23 10:37:14 +0100

    New package: py-bigfloat

---
+++ var/spack/repos/builtin/packages/py-bigfloat/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyBigfloat(PythonPackage):
+    """Arbitrary-precision correctly-rounded floating-point arithmetic, via MPFR."""
+
+    homepage = "http://github.com/mdickinson/bigfloat"
+    pypi     = "bigfloat/bigfloat-0.4.0.tar.gz"
+
+    version('0.4.0', sha256='58b96bde872aca5989d13d82eba3acf2aa1b94e22117dd72a16ba5911b0c0cb8')
+
+    depends_on('py-cython', type='build')
+    depends_on('py-six', type=('build', 'run'))
+    depends_on('gmp', type=('build', 'run'))
+    depends_on('mpfr', type=('build', 'link'))
