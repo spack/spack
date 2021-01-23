019e590083880bffbc22c1c5701bf9c61bcc82ca
iarspider <iarspider@gmail.com>
2021-01-23 20:19:16 +0100

    Changes from review

--- var/spack/repos/builtin/packages/py-pythonsollya/package.py
+++ var/spack/repos/builtin/packages/py-pythonsollya/package.py
@@ -14,11 +14,11 @@ class PyPythonsollya(PythonPackage):
 
     version('0.4.0-alpha0', sha256='faac899744c92b1d20980cadef309cd5610d79722322e97940ff142c207c41b5')
     version('0.3.0', url='https://gitlab.com/metalibm-dev/pythonsollya/-/archive/0.3/pythonsollya-0.3.tar.gz',
-            sha256='cdccd0c5549247ad7498546095544d8d01e78bcb4a3e55c32d6daea6b845b6b9')
+            sha256='cdccd0c5549247ad7498546095544d8d01e78bcb4a3e55c32d6daea6b845b6b9', preferred=True)
 
     depends_on('py-cython', type='build')
+    depends_on('py-six', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
-    depends_on('py-pip', type='build')
     depends_on('libffi', type='build')
     depends_on('sollya', type=('build', 'link'))
     depends_on('py-bigfloat', type=('build', 'run'))
