df8d6d80074ba39fd219345ecee37995ef8ab3f4
iarspider <iarspider@gmail.com>
2021-01-24 22:03:49 +0100

    Changes from review

--- var/spack/repos/builtin/packages/mpfi/package.py
+++ var/spack/repos/builtin/packages/mpfi/package.py
@@ -12,10 +12,10 @@ class Mpfi(AutotoolsPackage):
     # Notice: no simple way to deduct URL from version
     homepage = "https://perso.ens-lyon.fr/nathalie.revol/software.html"
 
-    version('1.5.3', url='https://gforge.inria.fr/frs/download.php/file/37331/mpfi-1.5.3.tar.bz2',
-            sha256='2383d457b208c6cd3cf2e66b69c4ce47477b2a0db31fbec0cd4b1ebaa247192f')
     version('1.5.4', url='https://gforge.inria.fr/frs/download.php/file/38111/mpfi-1.5.4.tgz',
             sha256='3b3938595d720af17973deaf727cfc0dd41c8b16c20adc103a970f4a43ae3a56')
+    version('1.5.3', url='https://gforge.inria.fr/frs/download.php/file/37331/mpfi-1.5.3.tar.bz2',
+            sha256='2383d457b208c6cd3cf2e66b69c4ce47477b2a0db31fbec0cd4b1ebaa247192f')
 
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
