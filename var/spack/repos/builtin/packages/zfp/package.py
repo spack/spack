4a145d6dd7830fba8640b7541dd74d00613692a1
lindstro <pl@llnl.gov>
2021-01-26 17:09:46 -0800

    zfp: address suggestions by Spack team
    - use conflicts() instead of raising exceptions
    - use define() and define_from_variant() where applicable

--- var/spack/repos/builtin/packages/zfp/package.py
+++ var/spack/repos/builtin/packages/zfp/package.py
@@ -7,11 +7,13 @@ from spack import *
 
 
 class Zfp(CMakePackage, CudaPackage):
-    """zfp is a compressed format for representing multidimensional
-       floating-point and integer arrays. zfp provides compressed-array
-       classes that support high throughput read and write random access
-       to individual array elements. zfp also supports serial and parallel
-       (OpenMP and CUDA) compression of whole arrays.
+    """zfp is a compressed number format for multidimensional floating-point
+       and integer arrays.
+
+       zfp provides compressed-array classes that support high throughput
+       read and write random access to individual array elements. zfp also
+       supports serial and parallel (OpenMP and CUDA) compression of whole
+       arrays.
     """
 
     # Package info
@@ -21,17 +23,15 @@ class Zfp(CMakePackage, CudaPackage):
     maintainers = ['lindstro', 'GarrettDMorrison']
 
     # Versions
+    version('develop', branch='develop')
     version('0.5.5', sha256='fdf7b948bab1f4e5dccfe2c2048fd98c24e417ad8fb8a51ed3463d04147393c5')
     version('0.5.4', sha256='746e17aaa401c67dcffd273d6e6f95c76adfbbd5cf523dcad56d09e9d3b71196')
     version('0.5.3', sha256='a5d2f8e5b47a7c92e2a5775b82cbfb3a76c87d0ac83d25abb4ac10ea75a2856e')
     version('0.5.2', sha256='9c738ec525cc76b4bb80b2b3f7c9f07507eeda3a341470e5942cda97efbe9a4f', url='https://github.com/LLNL/zfp/archive/0.5.2/zfp-0.5.2.tar.gz')
     version('0.5.1', sha256='f255dd1708c9ae4dc6a56dd2614e8b47a10d833c87fd349cbd47545a19c2b779', url='https://github.com/LLNL/zfp/archive/0.5.1/zfp-0.5.1.tar.gz')
-    version('develop', branch='develop')
 
     # Build targets
-    variant('utilities', default=True,  description='Build utilities')
-    variant('examples',  default=False, description='Build examples')
-    variant('tests',     default=True,  description='Build tests')
+    # variant('utilities', default=True,  description='Build utilities')
     variant('shared',    default=True,  description='Build shared libraries')
 
     # Language bindings
@@ -45,8 +45,9 @@ class Zfp(CMakePackage, CudaPackage):
 
     # Advanced options
     variant('bsws', default='64', values=('8', '16', '32', '64'), multi=False,
-            description='Bit stream word size: use smaller for finer '
-            'rate granularity. Use 8 for H5Z-ZFP filter.')
+            description='Bit stream word size: '
+            'use smaller for finer rate granularity. '
+            'Use 8 for H5Z-ZFP filter.')
     variant('strided', default=False,
             description='Enable strided access for progressive zfp streams')
     variant('aligned', default=False,
@@ -58,6 +59,22 @@ class Zfp(CMakePackage, CudaPackage):
     variant('profile', default=False,
             description='Count cache misses')
 
+    # Conflicts
+    conflicts('+c',        when='@:0.5.3',
+              msg='+c requires zfp 0.5.4 or later')
+    conflicts('+python',   when='@:0.5.4',
+              msg='+python requires zfp 0.5.5 or later')
+    conflicts('+fortran',  when='@:0.5.4',
+              msg='+fortran requires zfp 0.5.5 or later')
+    conflicts('+openmp',   when='@:0.5.2',
+              msg='+openmp requires zfp 0.5.3 or later')
+    conflicts('+cuda',     when='@:0.5.3',
+              msg='+cuda requires zfp 0.5.4 or later')
+    conflicts('+fasthash', when='@:0.5.1',
+              msg='+fasthash requires zfp 0.5.2 or later')
+    conflicts('+profile',  when='@:0.5.1',
+              msg='+profile requires zfp 0.5.2 or later')
+
     # Dependencies
     depends_on('cmake@3.4.0:', type='build')
     depends_on('cuda@7:',      type=('build', 'test', 'run'), when='+cuda')
@@ -67,82 +84,21 @@ class Zfp(CMakePackage, CudaPackage):
     def cmake_args(self):
         spec = self.spec
 
-        # Common CMake options for zfp 0.5.1 and later
+        # CMake options
         args = [
-            '-DBUILD_UTILITIES:BOOL={0}'.format(
-                'ON' if '+utilities' in spec else 'OFF'),
-            '-DBUILD_EXAMPLES:BOOL={0}'.format(
-                'ON' if '+examples' in spec else 'OFF'),
-            '-DBUILD_TESTING:BOOL={0}'.format(
-                'ON' if self.run_tests else 'OFF'),
-            '-DBUILD_SHARED_LIBS:BOOL={0}'.format(
-                'ON' if '+shared' in spec else 'OFF'),
-            '-DZFP_BIT_STREAM_WORD_SIZE:STRING={0}'.format(
-                spec.variants['bsws'].value),
-            '-DZFP_WITH_BIT_STREAM_STRIDED:BOOL={0}'.format(
-                'ON' if '+strided' in spec else 'OFF'),
-            '-DZFP_WITH_ALIGNED_ALLOC:BOOL={0}'.format(
-                'ON' if '+aligned' in spec else 'OFF'),
-            '-DZFP_WITH_CACHE_TWOWAY:BOOL={0}'.format(
-                'ON' if '+twoway' in spec else 'OFF'),
-            '-DZFP_WITH_CACHE_TWOWAY:BOOL={0}'.format(
-                'ON' if '+twoway' in spec else 'OFF')
+            # self.define_from_variant('BUILD_UTILITIES', 'utilities'),
+            self.define('BUILD_TESTING', self.run_tests),
+            self.define_from_variant('BUILD_SHARED_LIBS', 'shared'),
+            self.define_from_variant('BUILD_CFP', 'c'),
+            self.define_from_variant('BUILD_ZFPY', 'python'),
+            self.define_from_variant('BUILD_ZFORP', 'fortran'),
+            self.define('ZFP_BIT_STREAM_WORD_SIZE',
+                        spec.variants['bsws'].value),
+            self.define_from_variant('ZFP_WITH_BIT_STREAM_STRIDED', 'strided'),
+            self.define_from_variant('ZFP_WITH_ALIGNED_ALLOC', 'aligned'),
+            self.define_from_variant('ZFP_WITH_CACHE_TWOWAY', 'twoway'),
+            self.define_from_variant('ZFP_WITH_CACHE_FAST_HASH', 'fasthash'),
+            self.define_from_variant('ZFP_WITH_CACHE_PROFILE', 'profile')
         ]
-
-        # cfp compressed-array C bindings
-        if '+c' in spec:
-            if not spec.satisfies('@0.5.4:'):
-                raise InstallError('+c requires zfp 0.5.4 or later')
-            args.append('-DBUILD_CFP:BOOL=ON')
-        else:
-            args.append('-DBUILD_CFP:BOOL=OFF')
-
-        # zfPy Python bindings to libzfp
-        if '+python' in spec:
-            if not spec.satisfies('@0.5.5:'):
-                raise InstallError('+python requires zfp 0.5.5 or later')
-            args.append('-DBUILD_ZFPY:BOOL=ON')
-        else:
-            args.append('-DBUILD_ZFPY:BOOL=OFF')
-
-        # zFORp Fortran bindings to libzfp
-        if '+fortran' in spec:
-            if not spec.satisfies('@0.5.5:'):
-                raise InstallError('+fortran requires zfp 0.5.5 or later')
-            args.append('-DBUILD_ZFORP:BOOL=ON')
-        else:
-            args.append('-DBUILD_ZFORP:BOOL=OFF')
-
-        # OpenMP support
-        if '+openmp' in spec:
-            if not spec.satisfies('@0.5.3:'):
-                raise InstallError('+openmp requires zfp 0.5.3 or later')
-            args.append('-DZFP_WITH_OPENMP:BOOL=ON')
-        else:
-            args.append('-DZFP_WITH_OPENMP:BOOL=OFF')
-
-        # CUDA support
-        if '+cuda' in spec:
-            if not spec.satisfies('@0.5.4:'):
-                raise InstallError('+cuda requires zfp 0.5.4 or later')
-            args.append('-DZFP_WITH_CUDA:BOOL=ON')
-        else:
-            args.append('-DZFP_WITH_CUDA:BOOL=OFF')
-
-        # Advanced option: cache fast hash function
-        if '+fasthash' in spec:
-            if not spec.satisfies('@0.5.2:'):
-                raise InstallError('+fasthash requires zfp 0.5.2 or later')
-            args.append('-DZFP_WITH_CACHE_FAST_HASH:BOOL=ON')
-        else:
-            args.append('-DZFP_WITH_CACHE_FAST_HASH:BOOL=OFF')
-
-        # Advanced option: cache profiling
-        if '+profile' in spec:
-            if not spec.satisfies('@0.5.2:'):
-                raise InstallError('+profile requires zfp 0.5.2 or later')
-            args.append('-DZFP_WITH_CACHE_PROFILE:BOOL=ON')
-        else:
-            args.append('-DZFP_WITH_CACHE_PROFILE:BOOL=OFF')
 
         return args
