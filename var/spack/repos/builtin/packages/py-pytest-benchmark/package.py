c78aefd4638e2c9d3896cf90ecf69f716237f3cd
bryanherman <63422190+bryanherman@users.noreply.github.com>
2021-01-27 16:01:37 -0500

    Update var/spack/repos/builtin/packages/py-pytest-benchmark/package.py
    Added Python2 dependence.
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-pytest-benchmark/package.py
+++ var/spack/repos/builtin/packages/py-pytest-benchmark/package.py
@@ -15,7 +15,7 @@ class PyPytestBenchmark(PythonPackage):
 
     version('3.2.3', sha256='ad4314d093a3089701b24c80a05121994c7765ce373478c8f4ba8d23c9ba9528')
 
-    depends_on('python@3.5:', type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-pytest@3.8:', type=('build', 'run'))
     depends_on('py-py-cpuinfo', type=('build', 'run'))
