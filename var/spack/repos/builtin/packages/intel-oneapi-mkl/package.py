24c5778ec5ceecd6b63ba2f86cf7277ab062843e
Frank Willmore <frankwillmore@gmail.com>
2021-01-28 16:19:47 +0000

    can now build some sample applications using generated modules

--- var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
@@ -31,3 +31,30 @@ class IntelOneapiMkl(IntelOneApiLibraryPackage):
                             releases=releases,
                             url_name='onemkl')
         super(IntelOneapiMkl, self).__init__(spec)
+    #FTW: Need to work in paths for  /soft/spack/opt/spack/linux-opensuse_leap15-cascadelake/oneapi-2021.1/intel-oneapi-mkl-2021.1.1-n2zoxk4l5w62slfcyp3sdirrrxp77bko/mkl/latest/../2021.1.1/lib/intel64 (all the mkl stuff) plus tbb: /soft/spack/opt/spack/linux-opensuse_leap15-cascadelake/gcc-10.1.0/intel-oneapi-compilers-2021.1.0-wgzjfktx5hsx75gghtd6k2qxad5vnot2/tbb/2021.1.1/lib/intel64/gcc4.8
+    def _join_prefix(self, path):
+        return join_path(self.prefix, 'mkl', 'latest', path)
+
+    def _ld_library_path(self):
+        dirs = ['lib/intel64']
+        for dir in dirs:
+            yield self._join_prefix(dir)
+
+    def _library_path(self):
+        dirs = ['lib/intel64']
+        for dir in dirs:
+            yield self._join_prefix(dir)
+
+    def install(self, spec, prefix):
+        super(IntelOneapiMkl, self).install(spec, prefix)
+
+    def setup_run_environment(self, env):
+        env.prepend_path('PATH', self._join_prefix('bin/intel64'))
+        env.prepend_path('CPATH', self._join_prefix('include'))
+        for dir in self._library_path():
+            env.prepend_path('LIBRARY_PATH', dir)
+            # need to prepend path for tbb or is that picked up by having compiler loaded? 
+        for dir in self._ld_library_path():
+            env.prepend_path('LD_LIBRARY_PATH', dir)
+            # need to prepend path for tbb or is that picked up by having compiler loaded? 
+        env.set('MKLROOT', join_path(self.prefix, 'mkl', 'latest'))
