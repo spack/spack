f3c87c4dbb7344327c4ad1c1fca365f9fba07d6d
t-nojiri <tomoyasu.nojiri@riken.jp>
2021-01-29 13:31:55 +0900

    py-cheetah: Add python2 depend and version 2.4.4

--- var/spack/repos/builtin/packages/py-cheetah/package.py
+++ var/spack/repos/builtin/packages/py-cheetah/package.py
@@ -9,10 +9,11 @@ from spack import *
 class PyCheetah(PythonPackage):
     """Cheetah is a template engine and code generation tool."""
 
-    pypi = "Cheetah3/Cheetah3-3.2.6.tar.gz"
+    pypi = "Cheetah/Cheetah-2.3.0.tar.gz"
 
-    version('3.2.6', sha256='f1c2b693cdcac2ded2823d363f8459ae785261e61c128d68464c8781dba0466b')
+    version('2.4.4', sha256='be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550')
     version('2.3.0', sha256='2a32d7f7f70be98c2d57aa581f979bc799d4bf17d09fc0e7d77280501edf3e53')
 
+    depends_on('python@2:2.99.999', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-markdown@2.0.1:', type=('build', 'run'))
