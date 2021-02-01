ecc4d8a14c7a5a2f439df4d7947866ab9a3dbf16
Kenta Tsubouchi <kenta.tsubouchi@hac-inc.co.jp>
2021-02-01 20:12:13 +0900

    homepage

--- var/spack/repos/builtin/packages/cbflib/package.py
+++ var/spack/repos/builtin/packages/cbflib/package.py
@@ -12,7 +12,7 @@ class Cbflib(MakefilePackage):
     for accessing Crystallographic Binary Files (CBF files) and
     Image-supporting CIF (imgCIF) files."""
 
-    homepage = "https://sourceforge.net/projects/cbflib/"
+    homepage = "http://www.bernstein-plus-sons.com/software/CBF/"
     url      = "https://downloads.sourceforge.net/project/cbflib/cbflib/CBFlib_0.9.2/CBFlib-0.9.2.tar.gz"
 
     version('0.9.2', sha256='367e37e1908a65d5472e921150291332823a751206804866e752b793bca17afc')
