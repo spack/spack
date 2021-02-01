aa06e3bdb45d118f8cae2745c873c45f0045f07e
Andrew Dunning-NNL <Andrew.Dunning@unnpp.gov>
2021-02-01 13:27:44 +0000

    py-ftfy: depend on python>3.5

--- var/spack/repos/builtin/packages/py-ftfy/package.py
+++ var/spack/repos/builtin/packages/py-ftfy/package.py
@@ -15,6 +15,6 @@ class PyFtfy(PythonPackage):
 
     version('5.8', sha256='51c7767f8c4b47d291fcef30b9625fb5341c06a31e6a3b627039c706c42f3720')
 
-    depends_on('python@3:', type=('build', 'run'))
+    depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-wcwidth', type=('build', 'run'))
