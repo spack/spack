f867a6eea38eb352ca15344b10368d1a0a2b01e4
Frank Willmore <frankwillmore@gmail.com>
2021-02-03 18:21:54 -0600

    intel-oneapi-compilers/mpi: add module support (#20808)
    Facilitate running intel-oneapi-mpi outside of Spack (set PATH,
    LD_LIBRARY_PATH, etc. appropriately).
    
    Co-authored-by: Robert Cohn <rscohn2@gmail.com>

--- var/spack/repos/builtin/packages/intel-oneapi-mpi/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-mpi/package.py
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
+import subprocess
+
 from spack import *
 
 releases = {
@@ -20,6 +22,8 @@ class IntelOneapiMpi(IntelOneApiLibraryPackage):
     version('2021.1.1', sha256='8b7693a156c6fc6269637bef586a8fd3ea6610cac2aae4e7f48c1fbb601625fe', expand=False)
 
     provides('mpi@:3')
+
+    depends_on('patchelf', type='build')
 
     def __init__(self, spec):
         self.component_info(dir_name='mpi',
@@ -50,3 +54,42 @@ class IntelOneapiMpi(IntelOneApiLibraryPackage):
             ldir = find_libraries('*', root=lib_path, shared=True, recursive=False)
             libs += ldir
         return libs
+
+    def _join_prefix(self, path):
+        return join_path(self.prefix, 'mpi', 'latest', path)
+
+    def _ld_library_path(self):
+        dirs = ['lib',
+                'lib/release',
+                'libfabric/lib']
+        for dir in dirs:
+            yield self._join_prefix(dir)
+
+    def _library_path(self):
+        dirs = ['lib',
+                'lib/release',
+                'libfabric/lib']
+        for dir in dirs:
+            yield self._join_prefix(dir)
+
+    def install(self, spec, prefix):
+        super(IntelOneapiMpi, self).install(spec, prefix)
+
+        # need to patch libmpi.so so it can always find libfabric
+        libfabric_rpath = self._join_prefix('libfabric/lib')
+        for lib_version in ['debug', 'release', 'release_mt', 'debug_mt']:
+            file = self._join_prefix('lib/' + lib_version + '/libmpi.so')
+            subprocess.call(['patchelf', '--set-rpath', libfabric_rpath, file])
+
+    def setup_run_environment(self, env):
+        env.prepend_path('PATH', self._join_prefix('bin'))
+        env.prepend_path('CPATH', self._join_prefix('include'))
+        for dir in self._library_path():
+            env.prepend_path('LIBRARY_PATH', dir)
+        for dir in self._ld_library_path():
+            env.prepend_path('LD_LIBRARY_PATH', dir)
+        # so wrappers know where MPI lives
+        mpi_root = join_path(prefix, 'mpi', 'latest')
+        env.set('I_MPI_ROOT', mpi_root)
+        # set this so that wrappers can find libfabric providers
+        env.set('FI_PROVIDER_PATH', self._join_prefix('libfabric/lib/prov'))
