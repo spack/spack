36c83ab8f1b42b3ea4916d2144b8c329af04644f
Benoit Coste <benoit.coste@protonmail.com>
2021-02-04 11:31:01 +0100

    Bump morphology workflow v2.0.3 (#1038)

--- var/spack/repos/builtin/packages/py-morphology-repair-workflow/package.py
+++ var/spack/repos/builtin/packages/py-morphology-repair-workflow/package.py
@@ -13,8 +13,8 @@ class PyMorphologyRepairWorkflow(PythonPackage):
     homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/nse/morphology-repair-workflow"
     git      = "ssh://bbpcode.epfl.ch/nse/morphology-repair-workflow"
     version('develop', branch='master')
+    version('2.0.3', tag='morphology-repair-workflow-v2.0.3')
     version('2.0.2', tag='morphology-repair-workflow-v2.0.2')
-    version('2.0.1', tag='morphology-repair-workflow-v2.0.1')
     version('1.0.4', tag='morphology-repair-workflow-v1.0.4')
 
     depends_on('py-setuptools', type=('build', 'run'))
