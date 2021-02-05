d938a05e0e769d681b473db335c7270b7260aab2
iarspider <iarspider@gmail.com>
2021-02-05 11:10:38 +0100

    Flake-8

--- var/spack/repos/builtin/packages/cmaketools/package.py
+++ var/spack/repos/builtin/packages/cmaketools/package.py
@@ -9,7 +9,8 @@ from spack import *
 class Cmaketools(MakefilePackage):
     """This is a small package containing some commonly used CMake "find" modules.
 
-       To use them, you need to have the top directory in the CMAKE_PREFIX_PATH, then you can enable the modules in your CMakeLists.txt with something like:
+       To use them, you need to have the top directory in the CMAKE_PREFIX_PATH,
+       then you can enable the modules in your CMakeLists.txt with something like:
 
        find_package(CMakeTools)
        UseCMakeTools()
