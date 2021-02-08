c0c21a42c5be0651ca0ff4cfb37cbf337363ce0e
Satish Balay <balay@mcs.anl.gov>
2021-02-08 11:45:42 -0600

    petsc: update to version 3.14.4, and petsc4py to 3.14.1

--- var/spack/repos/builtin/packages/py-petsc4py/package.py
+++ var/spack/repos/builtin/packages/py-petsc4py/package.py
@@ -17,6 +17,7 @@ class PyPetsc4py(PythonPackage):
     maintainers = ['dalcinl', 'balay']
 
     version('develop', branch='master')
+    version('3.14.1', sha256='f5f8daf3a4cd1dfc945876b0d83a05b25f3c54e08046312eaa3e3036b24139c0')
     version('3.14.0', sha256='33ac9fb55a541e4c1deabd6e2144da96d5ae70e70c830a55de558000cf3f0ec5')
     version('3.13.0', sha256='0e11679353c0c2938336a3c8d1a439b853e20d3bccd7d614ad1dbea3ec5cb31f')
     version('3.12.0', sha256='4c94a1dbbf244b249436b266ac5fa4e67080d205420805deab5ec162b979df8d')
@@ -42,7 +43,8 @@ class PyPetsc4py(PythonPackage):
     depends_on('petsc+mpi', when='+mpi')
     depends_on('petsc~mpi', when='~mpi')
     depends_on('petsc@develop', when='@develop')
-    depends_on('petsc@3.14:3.14.99', when='@3.14:3.14.99')
+    depends_on('petsc@3.14.2:3.14.99', when='@3.14.1:3.14.99')
+    depends_on('petsc@3.14.0:3.14.1', when='@3.14.0')
     depends_on('petsc@3.13:3.13.99', when='@3.13:3.13.99')
     depends_on('petsc@3.12:3.12.99', when='@3.12:3.12.99')
     depends_on('petsc@3.11:3.11.99', when='@3.11:3.11.99')
