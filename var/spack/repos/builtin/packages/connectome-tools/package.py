46f17f5478f72f44cea8441b696e87cf29e64ed7
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2021-02-08 21:28:24 +0100

    Add connectome-tools 0.3.4 and 0.4.0 (#1041)
    * 0.4.0 depends on bluepy >= 2.0
    * 0.3.4 depends on bluepy < 2.0, and it can be removed later
      if there are no issues with 0.4.0

--- var/spack/repos/builtin/packages/connectome-tools/package.py
+++ var/spack/repos/builtin/packages/connectome-tools/package.py
@@ -13,7 +13,8 @@ class ConnectomeTools(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/nse/connectome-tools"
 
     version('develop', branch='master')
-    version('0.3.3', tag='connectome-tools-v0.3.3', preferred=True)
+    version('0.4.0', tag='connectome-tools-v0.4.0', preferred=True)
+    version('0.3.4', tag='connectome-tools-v0.3.4')
 
     depends_on('py-setuptools', type=('build', 'run'))
 
@@ -22,8 +23,12 @@ class ConnectomeTools(PythonPackage):
     depends_on('py-joblib@0.16.0:', type='run')
     depends_on('py-lxml@3.3:', type='run')
     depends_on('py-numpy@1.9:', type='run')
-    depends_on('py-pandas@0.17:', type='run')
-    depends_on('py-six@1.0:', type='run')
+    depends_on('py-pandas@1.0.0:', type='run')
+    depends_on('py-psutil@5.7.2:', type='run')
+    depends_on('py-pyyaml@5.3.1:', type='run')
 
-    depends_on('py-bluepy@0.13.3:', type='run')
-    depends_on('py-voxcell@2.5.6:', type='run')
+    depends_on('py-bluepy@2.0:2.999', when='@0.4.0:', type='run')
+    depends_on('py-voxcell@3.0:3.999', when='@0.4.0:', type='run')
+
+    depends_on('py-bluepy@0.13.3:1.999', when='@:0.3.4', type='run')
+    depends_on('py-voxcell@2.5.6:2.999', when='@:0.3.4', type='run')
