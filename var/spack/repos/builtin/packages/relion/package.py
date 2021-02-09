c4a2ebdd84288954d1ed02ab1a3e05dd9b21138c
Evan Bollig <ebbollig@amazon.com>
2021-02-08 20:14:15 -0600

    Added patch for ubuntu builds (reorders MKL links)

--- var/spack/repos/builtin/packages/relion/package.py
+++ var/spack/repos/builtin/packages/relion/package.py
@@ -53,6 +53,8 @@ class Relion(CMakePackage, CudaPackage):
     depends_on('tbb', when='~cuda')
     depends_on('mkl', when='~cuda +mklfft')
 
+    patch('0002-Simple-patch-to-fix-intel-mkl-linking.patch', when='os=ubuntu18.04')
+
     def cmake_args(self):
 
         carch = self.spec.variants['cuda_arch'].value[0]
