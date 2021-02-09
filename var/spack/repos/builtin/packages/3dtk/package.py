a533ece98ab10cdfef811c843893faa7fff786bd
Adam J. Stewart <ajstewart426@gmail.com>
2021-02-09 09:58:11 -0600

    Add missing opencv variants
    Co-authored-by: Michael Kuhn <michael@ikkoku.de>

--- var/spack/repos/builtin/packages/3dtk/package.py
+++ var/spack/repos/builtin/packages/3dtk/package.py
@@ -44,7 +44,7 @@ class _3dtk(CMakePackage):
     depends_on('gl', when='+opengl')
     depends_on('glew', when='+opengl')
     depends_on('freeglut', when='+opengl')
-    depends_on('opencv+calib3d+core+features2d+highgui+imgproc+ml', when='+opencv')
+    depends_on('opencv+calib3d+core+features2d+highgui+imgcodecs+imgproc+ml+videoio', when='+opencv')
     # Because concretizer is broken
     depends_on('opencv+flann', when='+opencv')
     depends_on('cuda', when='+cuda')
