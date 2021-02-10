294304098794d864f7a679a7e761f6f89c687933
Olivier Cessenat <cessenat@gmail.com>
2021-02-10 08:06:37 +0100

    ocamlbuild: correcting package from first review

--- var/spack/repos/builtin/packages/ocamlbuild/package.py
+++ var/spack/repos/builtin/packages/ocamlbuild/package.py
@@ -12,36 +12,31 @@ class Ocamlbuild(MakefilePackage):
 
     # Add a proper url for your package's homepage here.
     homepage = "https://ocaml.org/learn/tutorials/ocamlbuild/"
-    url      = "https://github.com/ocaml/ocamlbuild/archive/master.zip"
-    git      = "https://github.com/ocaml/ocamlbuild"
-    # list_url = "https://github.com/ocaml/ocamlbuild/releases/tag"
-    # list_url = "https://github.com/ocaml/ocamlbuild/archive/"
+    url      = "https://github.com/ocaml/ocamlbuild/archive/0.14.0.tar.gz"
+    git      = "https://github.com/ocaml/ocamlbuild/ocamlbuild.git"
 
     # Add a list of GitHub accounts to
     # notify when the package is updated.
-    maintainers = ['gasche']
+    maintainers = ['scemama', 'cessenat']
 
     # Add proper versions here.
     # spack -d install --no-checksum ocamlbuild
-    version('develop', branch='master')
-    version('0.14.0', sha256='87b29ce96958096c0a1a8eeafeb6268077b2d11e1bf2b3de0f5ebc9cf8d42e78', url='https://github.com/ocaml/ocamlbuild/archive/0.14.0.tar.gz')
-    version('0.13.1', sha256='79839544bcaebc8f9f0d73d029e2b67e2c898bba046c559ea53de81ea763408c', url='https://github.com/ocaml/ocamlbuild/archive/0.13.1.tar.gz')
+    version('master', sha256='bee0f96a265544818a4eb1a9cda0abd402ad160330f01a95229ba2d1011f4df3')
+    version('0.14.0', sha256='87b29ce96958096c0a1a8eeafeb6268077b2d11e1bf2b3de0f5ebc9cf8d42e78')
+    version('0.13.1', sha256='79839544bcaebc8f9f0d73d029e2b67e2c898bba046c559ea53de81ea763408c')
 
     # Add dependencies if required.
     depends_on('ocaml')
 
-    # def setup_run_environment(self, env):
-    #    env.set('OCAML_PREFIX', join(self.spec['ocaml'].prefix, 'toto'))
-    #    env.set('DESTDIR', join(self.spec['ocaml'].prefix, 'toto'))
-
     # Installation : https://github.com/ocaml/ocamlbuild/
     def edit(self, spec, prefix):
-        makefile_inc = []
+        makefile_inc = [
+            'BINDIR       = bin',
+            'LIBDIR       = lib',
+            'MANDIR       = man',
+        ]
         makefile_inc.append('OCAML_PREFIX       = %s' % self.spec['ocaml'].prefix)
         makefile_inc.append('DESTDIR       = %s/' % self.spec.prefix)
-        makefile_inc.append('BINDIR       = bin')
-        makefile_inc.append('LIBDIR       = lib')
-        makefile_inc.append('MANDIR       = man')
         with working_dir('.'):
             with open('Makefile.config', 'a') as fh:
                 fh.write('\n'.join(makefile_inc))
@@ -49,6 +44,3 @@ class Ocamlbuild(MakefilePackage):
 
     def build(self, spec, prefix):
         make()
-
-    def install(self, spec, prefix):
-        make('install', parallel=False)
