173c62e18dc23a5fa061dab46b3bcb0601c1b1bd
Jen Herting <jen@herting.cc>
2021-02-11 11:18:26 -0500

    [py-hyperopt] added vairants spark, mongo, and atpe

--- var/spack/repos/builtin/packages/py-hyperopt/package.py
+++ var/spack/repos/builtin/packages/py-hyperopt/package.py
@@ -16,6 +16,10 @@ class PyHyperopt(PythonPackage):
 
     version('0.2.5', sha256='bc6047d50f956ae64eebcb34b1fd40f186a93e214957f20e87af2f10195295cc')
 
+    variant('spark', default=False, description="SparkTrials")
+    variant('mongo', default=False, description="MongoTrials")
+    variant('atpe',  default=False, description="ATPE")
+
     depends_on('python@2.7:',       type=('build', 'run'))
     depends_on('py-setuptools',     type='build')
     depends_on('py-numpy',          type=('build', 'run'))
@@ -25,6 +29,6 @@ class PyHyperopt(PythonPackage):
     depends_on('py-future',         type=('build', 'run'))
     depends_on('py-tqdm',           type=('build', 'run'))
     depends_on('py-cloudpickle',    type=('build', 'run'))
-    depends_on('py-pyspark',        type=('build', 'run'))
-    depends_on('py-pymongo',        type=('build', 'run'))
-    depends_on('py-scikit-learn',   type=('build', 'run'))
+    depends_on('py-pyspark',        when="+spark",  type=('build', 'run'))
+    depends_on('py-pymongo',        when="+mongo",  type=('build', 'run'))
+    depends_on('py-scikit-learn',   when="+atpe",   type=('build', 'run'))
