872657d960f8eb1f1cea4b009705ac883f4cbd7a
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:21:54 +0000

    Add new versions of py-imageio

--- var/spack/repos/builtin/packages/py-imageio/package.py
+++ var/spack/repos/builtin/packages/py-imageio/package.py
@@ -15,6 +15,10 @@ class PyImageio(PythonPackage):
     homepage = "http://imageio.github.io/"
     pypi = "imageio/imageio-2.3.0.tar.gz"
 
+    version('2.9.0', sha256='52ddbaeca2dccf53ba2d6dec5676ca7bc3b2403ef8b37f7da78b7654bb3e10f0')
+    version('2.8.0', sha256='fb5fd6d3d17126bbaac9af29fe340e2c97a196eb9416d4f28c0e543744a152cf')
+    version('2.6.1', sha256='f44eb231b9df485874f2ffd22dfd0c3c711e7de076516b9374edea5c65bc67ae')
+    version('2.6.0', sha256='cfafaedf42bc8cdc7d45b0886b0e332932729492a6d04d46123a8f32f95c3583')
     version('2.5.0', sha256='42e65aadfc3d57a1043615c92bdf6319b67589e49a0aae2b985b82144aceacad')
     version('2.4.1', sha256='16b8077bc8a5fa7a58b3e744f7ecbb156d8c088132df31e0f4f546c98de3514a')
     version('2.3.0', sha256='c4fd5183c342d47fdc2e98552d14e3f24386021bbc3efedd1e3b579d7d249c07')
