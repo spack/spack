2a6e70fea4ca48d43e80627f95dfb457043f4f8d
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:54:57 +0000

    Add new versions of skilion-onedrive

--- var/spack/repos/builtin/packages/skilion-onedrive/package.py
+++ var/spack/repos/builtin/packages/skilion-onedrive/package.py
@@ -13,6 +13,9 @@ class SkilionOnedrive(MakefilePackage):
     homepage = "https://github.com/skilion/onedrive"
     url      = "https://github.com/skilion/onedrive/archive/v1.1.1.tar.gz"
 
+    version('1.1.4', sha256='c6ef18c5798ce70c32843f2bed73600af5ad342fd20239c973887e9e751a35b6')
+    version('1.1.3', sha256='fb12235a73919b3374b8f27785b834a690fba1c6e70c6e6f1f5da3e51eb471a0')
+    version('1.1.2', sha256='d68588bf8bc3700a06243db675cc672f94530cbfd6571a0a51da473fb5449309')
     version('1.1.1', sha256='fb51c81ec95c28f3fe3b29e3b7f915e30161bd5f4b14bb53ae5c2233cc1e92e9')
 
     depends_on('dmd')
