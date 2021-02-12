2af2e6f9a3ca4d9074a6f2ec6fa377c3268444eb
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:06:05 +0000

    Add new versions of py-backports-ssl-match-hostname

--- var/spack/repos/builtin/packages/py-backports-ssl-match-hostname/package.py
+++ var/spack/repos/builtin/packages/py-backports-ssl-match-hostname/package.py
@@ -13,4 +13,5 @@ class PyBackportsSslMatchHostname(PythonPackage):
 
     py_namespace = 'backports'
 
+    version('3.7.0.1', sha256='bb82e60f9fbf4c080eabd957c39f0641f0fc247d9a16e31e26d594d8f42b9fd2')
     version('3.5.0.1', sha256='502ad98707319f4a51fa2ca1c677bd659008d27ded9f6380c79e8932e38dcdf2')
