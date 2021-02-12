8754a0bc8ed585e98a1bdf320550129ecab70818
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:39:55 +0000

    Add new versions of postgis

--- var/spack/repos/builtin/packages/postgis/package.py
+++ var/spack/repos/builtin/packages/postgis/package.py
@@ -16,6 +16,10 @@ class Postgis(AutotoolsPackage):
     homepage = "https://postgis.net/"
     url      = "https://download.osgeo.org/postgis/source/postgis-2.5.3.tar.gz"
 
+    version('3.1.1', sha256='0e96afef586db6939d48fb22fbfbc9d0de5e6bc1722d6d553d63bb41441a2a7d')
+    version('3.1.0', sha256='da06e2456876cd0c6a9bee0636ff700982e900c597d1e4cb80a99a2623400201')
+    version('3.0.3', sha256='07fedbd046cbed55d24bb22474890a4651d0223b6e12d2dac1517b9a43674817')
+    version('3.0.2', sha256='a3a1641dfd73c83924088a185bdb8b35567b3d1dc8d0887f9e4b492e228ab2ca')
     version('3.0.1', sha256='5a5432f95150d9bae9215c6d1c7bb354e060482a7c379daa9b8384e1d03e6353')
     version('3.0.0', sha256='c06fd2cd5cea0119106ffe17a7235d893c2bbe6f4b63c8617c767630973ba594')
     version('2.5.3', sha256='72e8269d40f981e22fb2b78d3ff292338e69a4f5166e481a77b015e1d34e559a')
