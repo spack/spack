b7b350347cf4a7a2538ba9c46fcfeea5b75a8a4a
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:00:34 +0000

    Add new versions of py-qtpy

--- var/spack/repos/builtin/packages/py-qtpy/package.py
+++ var/spack/repos/builtin/packages/py-qtpy/package.py
@@ -12,6 +12,8 @@ class PyQtpy(PythonPackage):
     homepage = "https://github.com/spyder-ide/qtpy"
     pypi = "QtPy/QtPy-1.2.1.tar.gz"
 
+    version('1.9.0', sha256='2db72c44b55d0fe1407be8fba35c838ad0d6d3bb81f23007886dc1fc0f459c8d')
+    version('1.8.0', sha256='eb66998a9049d276c75f26fc31554666504a7c1732cc3edd42a434c1fed01c92')
     version('1.7.1', sha256='e97275750934b3a1f4d8e263f5b889ae817ed36f26867ab0ce52be731ab1ed9e')
     version('1.2.1', sha256='5803ce31f50b24295e8e600b76cc91d7f2a3140a5a0d526d40226f9ec5e9097d')
 
