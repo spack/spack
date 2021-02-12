8755ca025e7e479f950f5ef12b45af55279b610c
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:57:06 +0000

    Add new versions of py-clustershell

--- var/spack/repos/builtin/packages/py-clustershell/package.py
+++ var/spack/repos/builtin/packages/py-clustershell/package.py
@@ -14,6 +14,9 @@ class PyClustershell(PythonPackage):
     homepage = "http://cea-hpc.github.io/clustershell/"
     url      = "https://github.com/cea-hpc/clustershell/archive/v1.8.tar.gz"
 
+    version('1.8.3', sha256='5659adf458de56a4926ed3da0522936bff38ddcbabaa177292fceb310e7cace1')
+    version('1.8.2', sha256='e5703ce3f6d2138c8456cbb653974277f800f8280787715db065cccb0b7ae2bf')
+    version('1.8.1', sha256='0c3da87108de8b735f40b5905b8dcd8084a234849aee2a8b8d2e20b99b57100c')
     version('1.8', sha256='ad5a13e2d107b4095229810c35365e22ea94dfd2baf4fdcfcc68ce58ee37cee3')
 
     depends_on('py-setuptools', type='build')
