f8021274240c07252626774cfc2aa1789da9d2ca
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:03:49 +0000

    Add new versions of py-typing

--- var/spack/repos/builtin/packages/py-typing/package.py
+++ var/spack/repos/builtin/packages/py-typing/package.py
@@ -13,6 +13,8 @@ class PyTyping(PythonPackage):
     homepage = "https://docs.python.org/3/library/typing.html"
     pypi = "typing/typing-3.7.4.1.tar.gz"
 
+    version('3.7.4.3', sha256='1187fb9c82fd670d10aa07bbb6cfcfe4bdda42d6fab8d5134f04e8c4d0b71cc9')
+    version('3.7.4.2', sha256='6f6f3553709d2234e412092e5daa93aaaaa42ea1854505442280b39f9311707f')
     version('3.7.4.1', sha256='91dfe6f3f706ee8cc32d38edbbf304e9b7583fb37108fef38229617f8b3eba23')
     version('3.6.6', sha256='4027c5f6127a6267a435201981ba156de91ad0d1d98e9ddc2aa173453453492d')
     version('3.6.4', sha256='d400a9344254803a2368533e4533a4200d21eb7b6b729c173bc38201a74db3f2')
