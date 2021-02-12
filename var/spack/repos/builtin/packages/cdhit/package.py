3f26a6353f479d730db13c48a3d707d1da738b3f
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:43:36 +0000

    Add new versions of cdhit

--- var/spack/repos/builtin/packages/cdhit/package.py
+++ var/spack/repos/builtin/packages/cdhit/package.py
@@ -13,6 +13,7 @@ class Cdhit(MakefilePackage):
     homepage = "http://cd-hit.org/"
     url      = "https://github.com/weizhongli/cdhit/archive/V4.6.8.tar.gz"
 
+    version('4.8.1-2019-0228', sha256='26172dba3040d1ae5c73ff0ac6c3be8c8e60cc49fc7379e434cdf9cb1e7415de')
     version('4.8.1', sha256='f8bc3cdd7aebb432fcd35eed0093e7a6413f1e36bbd2a837ebc06e57cdb20b70')
     version('4.6.8', sha256='37d685e4aa849314401805fe4d4db707e1d06070368475e313d6f3cb8fb65949')
 
