086a9830c4565a2d8b338bce10c62e77d28531ae
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:15:31 +0000

    Add new versions of py-azure-batch

--- var/spack/repos/builtin/packages/py-azure-batch/package.py
+++ var/spack/repos/builtin/packages/py-azure-batch/package.py
@@ -10,6 +10,7 @@ class PyAzureBatch(PythonPackage):
     homepage = "https://github.com/Azure/azure-sdk-for-python"
     pypi = "azure-batch/azure-batch-9.0.0.zip"
 
+    version('10.0.0', sha256='83d7a2b0be42ca456ac2b56fa3dc6ce704c130e888d37d924072c1d3718f32d0')
     version('9.0.0', sha256='47ca6f50a640915e1cdc5ce3c1307abe5fa3a636236e561119cf62d9df384d84')
 
     depends_on('py-setuptools', type='build')
