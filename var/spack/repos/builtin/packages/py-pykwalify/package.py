c4411121a53a5e8d449855d8103e78227494f671
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:56:58 +0000

    Add new versions of py-pykwalify

--- var/spack/repos/builtin/packages/py-pykwalify/package.py
+++ var/spack/repos/builtin/packages/py-pykwalify/package.py
@@ -14,6 +14,7 @@ class PyPykwalify(PythonPackage):
     homepage = "https://github.com/grokzen/pykwalify"
     pypi = "pykwalify/pykwalify-1.7.0.tar.gz"
 
+    version('1.8.0', sha256='796b2ad3ed4cb99b88308b533fb2f559c30fa6efb4fa9fda11347f483d245884')
     version('1.7.0', sha256='7e8b39c5a3a10bc176682b3bd9a7422c39ca247482df198b402e8015defcceb2')
     version('1.6.1', sha256='191fd3f457f23c0aa8538c3a5c0249f70eeb1046e88d0eaaef928e09c44dff8d')
 
