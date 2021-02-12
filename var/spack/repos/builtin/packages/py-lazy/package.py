a2221a5d959c3881a250532502e9eb5bebad298c
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:28:07 +0000

    Add new versions of py-lazy

--- var/spack/repos/builtin/packages/py-lazy/package.py
+++ var/spack/repos/builtin/packages/py-lazy/package.py
@@ -11,6 +11,8 @@ class PyLazy(PythonPackage):
 
     pypi = "lazy/lazy-1.2.zip"
 
+    version('1.4', sha256='2c6d27a5ab130fb85435320651a47403adcb37ecbcc501b0c6606391f65f5b43')
+    version('1.3', sha256='c80a77bf7106ba7b27378759900cfefef38271088dc63b014bcfe610c8e68e3d')
     version('1.2', sha256='127ea610418057b953f0d102bed83f2c367be13b59f8d0ddf3b8a86c7d31b970')
 
     depends_on('py-setuptools', type='build')
