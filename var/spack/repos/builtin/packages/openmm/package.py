19a9db75f78e8ea0d55070e8068dde1ccd821af8
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:13:23 +0000

    Add new versions of openmm

--- var/spack/repos/builtin/packages/openmm/package.py
+++ var/spack/repos/builtin/packages/openmm/package.py
@@ -17,6 +17,8 @@ class Openmm(CMakePackage, CudaPackage):
     homepage = "http://openmm.org/"
     url      = "https://github.com/openmm/openmm/archive/7.4.1.tar.gz"
 
+    version('7.5.0', sha256='516748b4f1ae936c4d70cc6401174fc9384244c65cd3aef27bc2c53eac6d6de5')
+    version('7.4.2', sha256='2e121ad5cfcc4840861032bb1b11d4d0aea2d3ca3c59baaed73b8b0b4fd069cc')
     version('7.4.1', sha256='e8102b68133e6dcf7fcf29bc76a11ea54f30af71d8a7705aec0aee957ebe3a6d')
 
     install_targets = ['install', 'PythonInstall']
