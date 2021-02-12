b57a25905e3ea010fece6b6d30ac61e754fc32d6
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:26:41 +0000

    Add new versions of py-azure-mgmt-search

--- var/spack/repos/builtin/packages/py-azure-mgmt-search/package.py
+++ var/spack/repos/builtin/packages/py-azure-mgmt-search/package.py
@@ -10,6 +10,8 @@ class PyAzureMgmtSearch(PythonPackage):
     homepage = "https://github.com/Azure/azure-sdk-for-python"
     pypi = "azure-mgmt-search/azure-mgmt-search-2.1.0.zip"
 
+    version('8.0.0', sha256='a96d50c88507233a293e757202deead980c67808f432b8e897c4df1ca088da7e')
+    version('3.0.0', sha256='d4c78b14b48edd2e27e2068c9a448acfc84a18595be77fe483afe7bb447e1eb6')
     version('2.1.0', sha256='92a40a1a7a9e3a82b6fa302042799e8d5a67d3996c20835af72afc14f1610501')
 
     depends_on('py-setuptools', type='build')
