b75f646a260e3ba84035e2f38a9cad2ccde2e5b6
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:52:28 +0000

    Add new versions of py-pygdal

--- var/spack/repos/builtin/packages/py-pygdal/package.py
+++ var/spack/repos/builtin/packages/py-pygdal/package.py
@@ -20,6 +20,13 @@ class PyPygdal(PythonPackage):
     homepage = "https://github.com/nextgis/pygdal"
     pypi = "pygdal/pygdal-3.0.1.5.tar.gz"
 
+    version('3.2.1.6', sha256='c99cb36b1f22b78e3bef0a8dc78be1d20ff9f01b444ffa25eee4b1cd1ff192f2')
+    version('3.2.0.6', sha256='2659066eadd3dc5da3f54ac86a1e292b2b13961bf1c2dec0de53d8308ead94a4')
+    version('3.1.4.6', sha256='0b07f6ebf1c9a98dbd2da0c0239c81e7714cd58085619aa4c500be7e69ad37a9')
+    version('3.1.3.6', sha256='e104da30ec6d68bf0f3e2f740d89ff70a1f0c337422dfe2d76d1ba36be2adfe6')
+    version('3.1.2.6', sha256='d6a1df815259ab5f0c3988c6e2fa318881576509d38b870ce4a95d58aa08f375')
+    version('3.1.1.6', sha256='dd37ebf53705e91051a95da958214eb5d3e14a4800af8c4066feaa246b9ad18b')
+    version('3.1.0.6', sha256='d1c4677a471dfa0dbd282155d61388c2edb87f9b5b6f7d6628270c38ce4bd833')
     version('3.0.4.6', sha256='8e39b58cd9465bb5f41786a7cf6a62df93334c104db05a5bfb8181a0be276b86')
     version('3.0.1.5', sha256='1222f69fe5e6b632d0d2a42d3acb8fac80fb4577c05e01969d8cd5548192ccaa')
     version('2.4.2.5', sha256='73386683c0b10ab43b6d64257fca2ba812f53ec61b268de8811565fd9ae9bacd')
