28884923048fc4b230a66e70bbe67580dc45c26e
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:36:29 +0000

    Add new versions of perl-parallel-forkmanager

--- var/spack/repos/builtin/packages/perl-parallel-forkmanager/package.py
+++ var/spack/repos/builtin/packages/perl-parallel-forkmanager/package.py
@@ -12,4 +12,6 @@ class PerlParallelForkmanager(PerlPackage):
     homepage = "http://search.cpan.org/~yanick/Parallel-ForkManager/lib/Parallel/ForkManager.pm"
     url      = "http://search.cpan.org/CPAN/authors/id/Y/YA/YANICK/Parallel-ForkManager-1.19.tar.gz"
 
+    version('2.02', sha256='c1b2970a8bb666c3de7caac4a8f4dbcc043ab819bbc337692ec7bf27adae4404')
+    version('1.20', sha256='7cc4c1c3b0e676b61ffa90f82f4128e8057327449ca86a9beb2f39217023f289')
     version('1.19', sha256='f1de2e9875eeb77d65f80338905dedd522f3913822502982f805aa71cde5a472')
