9756f521a2df5cd0b9a69d0a0e7b55e2888bd6c0
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:38:58 +0000

    Add new versions of py-ligo-segments

--- var/spack/repos/builtin/packages/py-ligo-segments/package.py
+++ var/spack/repos/builtin/packages/py-ligo-segments/package.py
@@ -11,6 +11,7 @@ class PyLigoSegments(PythonPackage):
 
     pypi = "ligo-segments/ligo-segments-1.2.0.tar.gz"
 
+    version('1.3.0', sha256='13e0e278f9a14b247793884b368d6fae3b806cd794284e7afba158a2ccc016b3')
     version('1.2.0', sha256='5edbcb88cae007c4e154a61cb2c9d0a6d6d4016c1ecaf0a59a667a267bd20e7a')
 
     depends_on('py-setuptools', type='build')
