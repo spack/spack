25386232034032c71a6df132fe4b6ebcebd73b9f
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:40:51 +0000

    Add new versions of py-ranger-fm

--- var/spack/repos/builtin/packages/py-ranger-fm/package.py
+++ var/spack/repos/builtin/packages/py-ranger-fm/package.py
@@ -13,4 +13,5 @@ class PyRangerFm(PythonPackage):
     pypi = "ranger-fm/ranger-fm-1.9.2.tar.gz"
     git      = "https://github.com/ranger/ranger.git"
 
+    version('1.9.3', sha256='9476ed1971c641f4ba3dde1b8b80387f0216fcde3507426d06871f9d7189ac5e')
     version('1.9.2', sha256='0ec62031185ad1f40b9faebd5a2d517c8597019c2eee919e3f1c60ce466d8625')
