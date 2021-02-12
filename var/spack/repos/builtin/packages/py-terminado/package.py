edf992482e280f87f7156a7ff8a8001f91bcfd96
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:00:23 +0000

    Add new versions of py-terminado

--- var/spack/repos/builtin/packages/py-terminado/package.py
+++ var/spack/repos/builtin/packages/py-terminado/package.py
@@ -9,6 +9,9 @@ class PyTerminado(PythonPackage):
 
     pypi = "terminado/terminado-0.8.3.tar.gz"
 
+    version('0.9.2', sha256='89e6d94b19e4bc9dce0ffd908dfaf55cc78a9bf735934e915a4a96f65ac9704c')
+    version('0.9.1', sha256='3da72a155b807b01c9e8a5babd214e052a0a45a975751da3521a1c3381ce6d76')
+    version('0.9.0', sha256='7ceea4c1644b0e95588d1d2ba09c5bf365b318c26d0a13ef6a5f2751a2f6efa3')
     version('0.8.3', sha256='4804a774f802306a7d9af7322193c5390f1da0abb429e082a10ef1d46e6fb2c2')
     version('0.8.2', sha256='de08e141f83c3a0798b050ecb097ab6259c3f0331b2f7b7750c9075ced2c20c2')
     version('0.8.1', sha256='55abf9ade563b8f9be1f34e4233c7b7bde726059947a593322e8a553cc4c067a')
