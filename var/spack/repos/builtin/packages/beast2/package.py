38d212f8f18bfa49162a6f0c899a60dfa4e70c78
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:35:33 +0000

    Add new versions of beast2

--- var/spack/repos/builtin/packages/beast2/package.py
+++ var/spack/repos/builtin/packages/beast2/package.py
@@ -17,6 +17,9 @@ class Beast2(Package):
     homepage = "http://beast2.org/"
     url      = "https://github.com/CompEvol/beast2/releases/download/v2.4.6/BEAST.v2.4.6.Linux.tgz"
 
+    version('2.6.3', sha256='8899277b0d7124ab04dc512444d45f0f1a13505f3ce641e1f117098be3e2e20d')
+    version('2.6.2', sha256='9b6882e7f825e16a4dd91548e43b71b10931a8b0c128a1c6ae90a185c7c6dca3')
+    version('2.6.0', sha256='b7ae682904cec3a0c33f7d8b2f9d840afe5c00a3f0db6bd7aade7cfd7629dd76')
     version('2.5.2', sha256='2feb2281b4f7cf8f7de1a62de50f52a8678ed0767fc72f2322e77dde9b8cd45f')
     version('2.4.6', sha256='84029c5680cc22f95bef644824130090f5f12d3d7f48d45cb4efc8e1d6b75e93')
 
