c5db717c2bb642354ddcc0f40544f5d779d81785
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:54:15 +0000

    Add new versions of py-uvw

--- var/spack/repos/builtin/packages/py-uvw/package.py
+++ var/spack/repos/builtin/packages/py-uvw/package.py
@@ -17,6 +17,7 @@ class PyUvw(PythonPackage):
 
     maintainers = ['prs513rosewood']
 
+    version('0.3.2', sha256='24f0d0f116e55cd80bf8f29fb45eb515a659849623017587c654230eeee3c4d9')
     version('0.3.1', sha256='31e3347ec342bd5381091f3c782ea1a1bfa4709d1de41cd700509e0b813f2265')
     version('0.0.7', sha256='4bcb77cf9655f0dcd5f38f024210ac5ad7ebc6fcfb45f898468d29a927bcb7a5')
 
