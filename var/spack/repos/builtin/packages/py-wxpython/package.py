b3dc72d969ef26afcb9555735ac87e24329f63e2
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:20:19 +0000

    Add new versions of py-wxpython

--- var/spack/repos/builtin/packages/py-wxpython/package.py
+++ var/spack/repos/builtin/packages/py-wxpython/package.py
@@ -12,6 +12,9 @@ class PyWxpython(PythonPackage):
     homepage = "https://www.wxpython.org/"
     pypi = "wxPython/wxPython-4.0.6.tar.gz"
 
+    version('4.1.1', sha256='00e5e3180ac7f2852f342ad341d57c44e7e4326de0b550b9a5c4a8361b6c3528')
+    version('4.1.0', sha256='2e2475cb755ac8d93d2f9335c39c060b4d17ecb5d4e0e86626d1e2834b64a48b')
+    version('4.0.7', sha256='3be608bfdede3063678cc703453850ab0a018b82bafd5ee057302250b18f0233')
     version('4.0.6', sha256='35cc8ae9dd5246e2c9861bb796026bbcb9fb083e4d49650f776622171ecdab37')
 
     depends_on('wxwidgets')
