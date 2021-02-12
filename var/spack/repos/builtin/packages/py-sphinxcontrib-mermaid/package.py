02af6a8e077905a782cb75fd89b9ec309bdab134
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:28:22 +0000

    Add new versions of py-sphinxcontrib-mermaid

--- var/spack/repos/builtin/packages/py-sphinxcontrib-mermaid/package.py
+++ var/spack/repos/builtin/packages/py-sphinxcontrib-mermaid/package.py
@@ -14,6 +14,8 @@ class PySphinxcontribMermaid(PythonPackage):
     homepage = "https://github.com/mgaitan/sphinxcontrib-mermaid"
     pypi = "sphinxcontrib-mermaid/sphinxcontrib-mermaid-0.4.0.tar.gz"
 
+    version('0.6.1', sha256='75ba8ab66b5157864360f5654a335af4a78384d5e9659c8d8e9ec9601d0a81b6')
+    version('0.6.0', sha256='e23d7bf90603814cf099434576994558f1e083ae21c2d7d136412957dfd9ddb6')
     version('0.4.0', sha256='0ee45ba45b9575505eacdd6212e4e545213f4f93dfa32c7eeca32720dbc3b468')
 
     depends_on('py-sphinx@1.7:')
