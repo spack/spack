69ba2d595005df3e944179aea79255d87ca68842
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:11:31 +0000

    Add new versions of lftp

--- var/spack/repos/builtin/packages/lftp/package.py
+++ var/spack/repos/builtin/packages/lftp/package.py
@@ -13,6 +13,12 @@ class Lftp(AutotoolsPackage):
     homepage = "http://lftp.yar.ru/"
     url      = "http://lftp.yar.ru/ftp/lftp-4.7.7.tar.gz"
 
+    version('4.9.2', sha256='a37589c61914073f53c5da0e68bd233b41802509d758a022000e1ae2076da733')
+    version('4.9.1', sha256='02336d4ffc617b453e7cfd335f3a424758408ff31a10720d59add86e3737966a')
+    version('4.9.0', sha256='2f799849f54e1d7f9013a751b69732782ea13a51b32b119940637cd469111ca7')
+    version('4.8.4', sha256='19f3a4236558fbdb88eec01bc9d693c51b122d23487b6bedad4cc67ae6825fc2')
+    version('4.8.3', sha256='cfbbbd067c25ff9d629828a010cc700214859b02e33b2405dfe7ed045d080f0f')
+    version('4.8.2', sha256='9b21261faaa05d4fd235589cc21e2e45fc14c9314d7b4bb1f1aec44ea39eb3ee')
     version('4.8.1', sha256='6117866215cd889dab30ff73292cd1d35fe0e12a9af5cd76d093500d07ab65a3')
     version('4.7.7', sha256='7bce216050094a1146ed05bed8fe5b3518224764ffe98884a848d44dc76fff8f')
     version('4.6.4', sha256='791e783779d3d6b519d0c23155430b9785f2854023eb834c716f5ba78873b15a')
