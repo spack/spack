b5b54e5b8a30512a04276e90d0897104b920e215
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:25:34 +0000

    Add new versions of py-dlcpar

--- var/spack/repos/builtin/packages/py-dlcpar/package.py
+++ var/spack/repos/builtin/packages/py-dlcpar/package.py
@@ -13,6 +13,9 @@ class PyDlcpar(PythonPackage):
     homepage = "https://www.cs.hmc.edu/~yjw/software/dlcpar/"
     url      = "https://www.cs.hmc.edu/~yjw/software/dlcpar/pub/sw/dlcpar-1.0.tar.gz"
 
+    version('2.0.1', sha256='ff30f4e2ade540be1afbf5d6ae75ff64febc9051da734c0f4f3b6abd94a32502')
+    version('2.0',   sha256='76ce40d6e6d0c06a7dd0d3064a6d2afdd0b25d848708efb9b0f9309223e5acc0')
+    version('1.1',   sha256='61aaf65197d7501bb59659cdc3ac9b13c80aea33c417429a8aff41fa2cd87319')
     version('1.0', sha256='774319caba0f10d1230b8f85b8a147eda5871f9a316d7b3381b91c1bde97aa0a')
 
     depends_on('py-numpy', type=('build', 'run'))
