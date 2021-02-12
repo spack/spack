f6177403d0f0745e084fdb13c10ec4a9ef62bdf8
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:03:02 +0000

    Add new versions of lmdb

--- var/spack/repos/builtin/packages/lmdb/package.py
+++ var/spack/repos/builtin/packages/lmdb/package.py
@@ -15,6 +15,10 @@ class Lmdb(MakefilePackage):
     homepage = "https://lmdb.tech/"
     url      = "https://github.com/LMDB/lmdb/archive/LMDB_0.9.21.tar.gz"
 
+    version('0.9.28', sha256='47457d3d3ae2c489b52078a07e9f55ec6e094b48c2204029c7754e2972fe1882')
+    version('0.9.27', sha256='114a87aa1f03ead60c7b50beaadde689fe50c646012974d0a17f32763b88a11c')
+    version('0.9.26', sha256='dffdbbc314242a039cfc04f5a058ad741a070e9131cf2cb73caf2c9eafbd1654')
+    version('0.9.25', sha256='5320a24f27e40cd4a00a2e5eaf37d80186835bd4d63a9adefcf90987dfa71ee4')
     version('0.9.24', sha256='44602436c52c29d4f301f55f6fd8115f945469b868348e3cddaf91ab2473ea26')
     version('0.9.22', sha256='f3927859882eb608868c8c31586bb7eb84562a40a6bf5cc3e13b6b564641ea28')
     version('0.9.21', sha256='1187b635a4cc415bb6972bba346121f81edd996e99b8f0816151d4090f90b559')
