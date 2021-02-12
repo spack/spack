7ab23be2a65289a3f273ae4f0f208b7f6c23c18e
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:25:54 +0000

    Add new versions of py-pytest-runner

--- var/spack/repos/builtin/packages/py-pytest-runner/package.py
+++ var/spack/repos/builtin/packages/py-pytest-runner/package.py
@@ -12,6 +12,8 @@ class PyPytestRunner(PythonPackage):
     homepage = "https://github.com/pytest-dev/pytest-runner"
     pypi = "pytest-runner/pytest-runner-5.1.tar.gz"
 
+    version('5.3.0', sha256='ca3f58ff4957e8be6c54c55d575b235725cbbcf4dc0d5091c29c6444cfc8a5fe')
+    version('5.2',   sha256='96c7e73ead7b93e388c5d614770d2bae6526efd997757d3543fe17b557a0942b')
     version('5.1',    sha256='25a013c8d84f0ca60bb01bd11913a3bcab420f601f0f236de4423074af656e7a')
     version('2.11.1', sha256='983a31eab45e375240e250161a556163bc8d250edaba97960909338c273a89b3')
 
