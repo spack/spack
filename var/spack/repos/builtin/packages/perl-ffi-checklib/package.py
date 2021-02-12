8fbaf6baaa84d58ef6d91c0d7ae318d068ec4b0e
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:06:38 +0000

    Add new versions of perl-ffi-checklib

--- var/spack/repos/builtin/packages/perl-ffi-checklib/package.py
+++ var/spack/repos/builtin/packages/perl-ffi-checklib/package.py
@@ -17,4 +17,6 @@ class PerlFfiChecklib(PerlPackage):
     homepage = "https://metacpan.org/pod/FFI::CheckLib"
     url      = "https://cpan.metacpan.org/authors/id/P/PL/PLICEASE/FFI-CheckLib-0.25.tar.gz"
 
+    version('0.27', sha256='8d442747c24fcd8560107ed9deb98265838f17bc850cb7237f8b6d4821992d74')
+    version('0.26', sha256='06e33bf0807f841626248197e8f39475ac50d1364a0336836c9b581d19a97f65')
     version('0.25', sha256='eb36b9a7cff1764a65b1b77e01e92c26207c558a3f986d0d17d2b110fa366ba4')
