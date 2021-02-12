cae39e843899f4a2be56287e91804d920b7d1e01
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:56:51 +0000

    Add new versions of py-restview

--- var/spack/repos/builtin/packages/py-restview/package.py
+++ var/spack/repos/builtin/packages/py-restview/package.py
@@ -12,6 +12,12 @@ class PyRestview(PythonPackage):
     homepage = "https://mg.pov.lt/restview/"
     pypi = "restview/restview-2.6.1.tar.gz"
 
+    version('2.9.2', sha256='790097eb587c0465126dde73ca06c7a22c5007ce1be4a1de449a13c0767b32dc')
+    version('2.9.1', sha256='de87c84f19526bd4a76505f6d40b51b7bb03ca43b6067c93f82f1c7237ac9e84')
+    version('2.9.0', sha256='d33748440af00ba4434c13c095ccfbde6e3237c3bd661198cf74066c0c027e7a')
+    version('2.8.1', sha256='45320b4e52945d23b3f1aeacc7ff97a3b798204fe625f8b81ed5322326d5bcd1')
+    version('2.8.0', sha256='5f6f1523228eab3269f59dd03ac560f7d370cd81df6fdbcb4914b5e6bd896a11')
+    version('2.7.0', sha256='e7842100f3de179c68cfe7c2cf56c61509cd6068bc6dd58ab42c0ade5d5f97ec')
     version('2.6.1', sha256='14d261ee0edf30e0ebc1eb320428ef4898e97422b00337863556966b851fb5af')
 
     depends_on('python@2.7:2.8,3.3:3.5')
