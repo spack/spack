442052efdf044b68aba83b12f2ae8e4c917fc40c
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:59:34 +0000

    Add new versions of double-conversion

--- var/spack/repos/builtin/packages/double-conversion/package.py
+++ var/spack/repos/builtin/packages/double-conversion/package.py
@@ -21,6 +21,11 @@ class DoubleConversion(CMakePackage):
     homepage = "https://github.com/google/double-conversion"
     url      = "https://github.com/google/double-conversion/archive/v2.0.1.zip"
 
+    version('3.1.5', sha256='72c0e3925a1214095afc6f1c214faecbec20e8526cf6b8a541cf72195a11887f')
+    version('3.1.4', sha256='b867193066a4cd87dc9f11e6ba6ba72281439d2b5f0ad6f351c8ccc429652c0c')
+    version('3.1.3', sha256='598b66a329dd70ba3778cdb9766c008d2a94e46367e30ec59764402cf563eefc')
+    version('3.1.2', sha256='428114167438ccb263ee05bb764dbad1d825bc553a69839970f6459ab362726e')
+    version('3.0.3', sha256='2502a6fc1dab7fd5d023b20c2f1127dd2d8c3b8778532b2841ebdfb5406e6586')
     version('2.0.1', sha256='476aefbdc2051bbcca0d5919ebc293c90a7ad2c0cb6c4ad877d6e665f469146b')
     version('2.0.0', sha256='437df89059bfa6c1c0f8703693c2584a57f75289ed7020d801c9befb23f46a26')
     version('1.1.5', sha256='496fd3354fa0ff17562907632f5560c1d444ea98b6069f1436fa573949b94fb0')
