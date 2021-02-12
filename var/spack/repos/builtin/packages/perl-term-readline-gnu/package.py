5b899aa54401e4680cfcaa5b1fc1c18bc1dfdf45
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:10:34 +0000

    Add new versions of perl-term-readline-gnu

--- var/spack/repos/builtin/packages/perl-term-readline-gnu/package.py
+++ var/spack/repos/builtin/packages/perl-term-readline-gnu/package.py
@@ -11,6 +11,7 @@ class PerlTermReadlineGnu(PerlPackage):
     homepage = "https://metacpan.org/pod/Term::ReadLine::Gnu"
     url      = "https://cpan.metacpan.org/authors/id/H/HA/HAYASHI/Term-ReadLine-Gnu-1.36.tar.gz"
 
+    version('1.37', sha256='3bd31a998a9c14748ee553aed3e6b888ec47ff57c07fc5beafb04a38a72f0078')
     version('1.36', sha256='9a08f7a4013c9b865541c10dbba1210779eb9128b961250b746d26702bab6925')
 
     depends_on('readline')
