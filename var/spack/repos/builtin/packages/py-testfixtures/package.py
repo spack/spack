d436bcde4c95acaff3a239c9d56a457b1f75d402
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:54:31 +0000

    Add new versions of py-testfixtures

--- var/spack/repos/builtin/packages/py-testfixtures/package.py
+++ var/spack/repos/builtin/packages/py-testfixtures/package.py
@@ -13,6 +13,8 @@ class PyTestfixtures(PythonPackage):
     homepage = "https://github.com/Simplistix/testfixtures"
     url      = "https://github.com/Simplistix/testfixtures/archive/6.16.0.zip"
 
+    version('6.17.1', sha256='8c65b000ae8c9bac881b19f57edfc27750165a7743f319238611dbe1b9b3ec00')
+    version('6.17.0', sha256='2bf208c54b619a0664f7735636f99ddaa660f9a4bd9ee6e1869dc5f2db2f86b1')
     version('6.16.0', sha256='6b5bbca4f7d5692ca4566c60c0383b121d9f1cfecbc3de8442c499a63c264eb8')
 
     depends_on('py-setuptools', type='build')
