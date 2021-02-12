c640f41af64db7ceb5fe50703e5b02d7d0301f3d
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:44:30 +0000

    Add new versions of soci

--- var/spack/repos/builtin/packages/soci/package.py
+++ var/spack/repos/builtin/packages/soci/package.py
@@ -12,6 +12,7 @@ class Soci(CMakePackage):
     homepage = "https://github.com/SOCI/soci"
     url      = "https://github.com/SOCI/soci/archive/4.0.0.tar.gz"
 
+    version('4.0.1', sha256='fa69347b1a1ef74450c0382b665a67bd6777cc7005bbe09726479625bcf1e29c')
     version('4.0.0', sha256='359b988d8cbe81357835317821919f7e270c0705e41951a92ac1627cb9fe8faf')
     version('3.2.3', sha256='1166664d5d7c4552c4c2abf173f98fa4427fbb454930fd04de3a39782553199e')
     version('3.2.2', sha256='cf1a6130ebdf0b84d246da948874ab1312c317e2ec659ede732b688667c355f4')
