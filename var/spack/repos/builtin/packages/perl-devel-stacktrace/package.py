44ab75deda3b84e3367d050905366b17a8705bbf
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:47:57 +0000

    Add new versions of perl-devel-stacktrace

--- var/spack/repos/builtin/packages/perl-devel-stacktrace/package.py
+++ var/spack/repos/builtin/packages/perl-devel-stacktrace/package.py
@@ -12,4 +12,6 @@ class PerlDevelStacktrace(PerlPackage):
     homepage = "http://search.cpan.org/~drolsky/Devel-StackTrace-2.02/lib/Devel/StackTrace.pm"
     url      = "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/Devel-StackTrace-2.02.tar.gz"
 
+    version('2.04', sha256='cd3c03ed547d3d42c61fa5814c98296139392e7971c092e09a431f2c9f5d6855')
+    version('2.03', sha256='7618cd4ebe24e254c17085f4b418784ab503cb4cb3baf8f48a7be894e59ba848')
     version('2.02', sha256='cbbd96db0ecf194ed140198090eaea0e327d9a378a4aa15f9a34b3138a91931f')
