ba2a88c6726454d5c5eee98ee3d923b8e144613e
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:26:06 +0000

    Add new versions of tpm2-tss

--- var/spack/repos/builtin/packages/tpm2-tss/package.py
+++ var/spack/repos/builtin/packages/tpm2-tss/package.py
@@ -12,6 +12,9 @@ class Tpm2Tss(AutotoolsPackage):
     homepage = "https://tpm2-software.github.io/"
     url      = "https://github.com/tpm2-software/tpm2-tss/archive/3.0.0.tar.gz"
 
+    version('3.0.3', sha256='78392be7309baf47f51b122f566ac915fd4d1760ea78571cba2e1484f9b5be17')
+    version('3.0.2', sha256='8f3b7ac8b7404a361013d1adb38be33f69384f2f9fbb44dabe94597f63ab0e00')
+    version('3.0.1', sha256='2432533a1cac0f0d15d0337d2f0a23591a50b36aad68ab72412ce694818b7e76')
     version('3.0.0', sha256='e88e91aeee2e01ccc45596fb8afcc3b521a660dcebe5a6e1b14ea5e9e5c15cf5')
     version('2.4.2', sha256='1cec5e834a6a750b138cabcd100b3fcd12b16cd21fa4f9103739914743511f75')
     version('2.4.1', sha256='cc6f0691307f3c65d5a1375e2cd22508cc72850dbc70eb820b892f0b3d0cbea2')
