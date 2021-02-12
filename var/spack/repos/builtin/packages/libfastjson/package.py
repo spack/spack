3bba6d7be4ef0733a29b631fd0e6fa8b9f81ffc6
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:20:54 +0000

    Add new versions of libfastjson

--- var/spack/repos/builtin/packages/libfastjson/package.py
+++ var/spack/repos/builtin/packages/libfastjson/package.py
@@ -12,6 +12,7 @@ class Libfastjson(AutotoolsPackage):
     homepage = "https://github.com/rsyslog/libfastjson"
     url      = "https://github.com/rsyslog/libfastjson/archive/v0.99.8.tar.gz"
 
+    version('0.99.9', sha256='881f954633aa76931e4c756ece0bda6fd8a673c6e66955a3db3b2bb9d6bbff72')
     version('0.99.8', sha256='7e49057b26a5a9e3c6623e024f95f9fd9a14b571b9150aeb89d6d475fc3633e3')
     version('0.99.7', sha256='a142a6e5fa5c9c4ac32615c42fc663a1a14bff305c922e55192b6abf7d1ce1d8')
     version('0.99.6', sha256='617373e5205c84b5f674354df6ee9cba53ef8a227f0d1aa928666ed8a16d5547')
