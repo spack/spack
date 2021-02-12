5bb6482d8062cfa8ffa5ef67f76c3bcec3a7cdf0
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:43:37 +0000

    Add new versions of py-wasabi

--- var/spack/repos/builtin/packages/py-wasabi/package.py
+++ var/spack/repos/builtin/packages/py-wasabi/package.py
@@ -10,6 +10,11 @@ class PyWasabi(PythonPackage):
     homepage = "https://ines.io/"
     pypi = "wasabi/wasabi-0.6.0.tar.gz"
 
+    version('0.8.2', sha256='b4a36aaa9ca3a151f0c558f269d442afbb3526f0160fd541acd8a0d5e5712054')
+    version('0.8.1', sha256='6e5228a51f5550844ef5080e74759e7ecb6e344241989d018686ba968f0b4f5a')
+    version('0.8.0', sha256='75fec6db6193c8615d7f398ae4aa2c4ad294e6e3e81c6a6dbbbd3864ee2223c3')
+    version('0.7.1', sha256='ee3809f4ce00e1e7f424b1572c753cff0dcaca2ca684e67e31f985033a9f070b')
+    version('0.7.0', sha256='e875f11d7126a2796999ff7f092195f24005edbd90b32b2df16dde5d392ecc8c')
     version('0.6.0', sha256='b8dd3e963cd693fde1eb6bfbecf51790171aa3534fa299faf35cf269f2fd6063')
 
     depends_on('py-setuptools', type='build')
