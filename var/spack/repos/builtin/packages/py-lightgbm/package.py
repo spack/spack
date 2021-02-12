0545b03cbb91bb14a9dfc0951b912a7a39d11669
Jen Herting <jen@herting.cc>
2021-02-12 11:57:01 -0500

    [py-lightgbm] removed fixmes

--- var/spack/repos/builtin/packages/py-lightgbm/package.py
+++ var/spack/repos/builtin/packages/py-lightgbm/package.py
@@ -34,12 +34,6 @@ class PyLightgbm(PythonPackage):
 
     variant('mpi', default=False, description="Build with mpi support")
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-wheel', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
