bc55d7b18cceb923c4dd4bb9e617dd40c7351c43
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:14:38 +0000

    Add new versions of ripgrep

--- var/spack/repos/builtin/packages/ripgrep/package.py
+++ var/spack/repos/builtin/packages/ripgrep/package.py
@@ -15,6 +15,8 @@ class Ripgrep(Package):
     homepage = "https://github.com/BurntSushi/ripgrep"
     url      = "https://github.com/BurntSushi/ripgrep/archive/11.0.2.tar.gz"
 
+    version('12.1.1', sha256='88d3b735e43f6f16a0181a8fec48847693fae80168d5f889fdbdeb962f1fc804')
+    version('12.1.0', sha256='c6bba6d643b1a1f18994683e26d4d2b998b41a7a7360e63cb8ec9db8ffbf793c')
     version('11.0.2', sha256='0983861279936ada8bc7a6d5d663d590ad34eb44a44c75c2d6ccd0ab33490055')
 
     depends_on('rust')
