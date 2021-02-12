96c4848d90b78a15fe7c963bdbb7b9b003e4f02f
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:14:48 +0000

    Add new versions of rdptools

--- var/spack/repos/builtin/packages/rdptools/package.py
+++ var/spack/repos/builtin/packages/rdptools/package.py
@@ -12,6 +12,7 @@ class Rdptools(MakefilePackage):
     homepage = "https://github.com/rdpstaff/RDPTools"
     url      = "https://github.com/rdpstaff/RDPTools/archive/2.0.2.tar.gz"
 
+    version('2.0.3', sha256='2e1a32d0d2fdf775aae6e5a33035a6065775c781c1dd28148d588970cf3a6e2b')
     version('2.0.2', sha256='fc3d7f8129b45e602fc2c23e5e037a7f48c14d5a6b05c64f8c1d48e9767ac01d')
 
     # https://github.com/bioconda/bioconda-recipes/blob/master/recipes/rdptools/meta.yaml
