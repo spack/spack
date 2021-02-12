a4e7a908c050c20febfbb8d308a85f775327c5e9
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:18:10 +0000

    Add new versions of ruby-thor

--- var/spack/repos/builtin/packages/ruby-thor/package.py
+++ var/spack/repos/builtin/packages/ruby-thor/package.py
@@ -10,6 +10,7 @@ class RubyThor(RubyPackage):
     homepage = "http://whatisthor.com/"
     url      = "https://github.com/erikhuda/thor/archive/v1.0.1.tar.gz"
 
+    version('1.1.0', sha256='ef285b4fedcb7ed48283198678bcc6d633dc26ac77ba01d7609ba81c2f10e130')
     version('1.0.1', sha256='e6b902764e237ce296cf9e339c93f8ca83bec5b59be0bf8bacd7ffddc6684d07')
 
     depends_on('ruby@2.0.0:', type=('build', 'run'))
