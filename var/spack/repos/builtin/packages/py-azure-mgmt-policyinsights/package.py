044fdd1816054bb30a5de0b9c5151ef29946b6e2
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:42:21 +0000

    Add new versions of py-azure-mgmt-policyinsights

--- var/spack/repos/builtin/packages/py-azure-mgmt-policyinsights/package.py
+++ var/spack/repos/builtin/packages/py-azure-mgmt-policyinsights/package.py
@@ -10,6 +10,8 @@ class PyAzureMgmtPolicyinsights(PythonPackage):
     homepage = "https://github.com/Azure/azure-sdk-for-python"
     pypi = "azure-mgmt-policyinsights/azure-mgmt-policyinsights-0.5.0.zip"
 
+    version('1.0.0', sha256='75103fb4541aeae30bb687dee1fedd9ca65530e6b97b2d9ea87f74816905202a')
+    version('0.6.0', sha256='2c64533f6eab08dc16450fc5d7c7651557fc0edc8ef1278dda336333d648a7c4')
     version('0.5.0', sha256='ed229e3845c477e88dde413825d4fba0d38b3a5ffab4e694c7d0da995f3db0f3')
 
     depends_on('py-setuptools', type='build')
