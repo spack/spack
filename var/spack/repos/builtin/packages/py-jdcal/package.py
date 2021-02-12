cb6c16c3c025a360e09d961c07c0ba8e4105a1fe
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:09:34 +0000

    Add new versions of py-jdcal

--- var/spack/repos/builtin/packages/py-jdcal/package.py
+++ var/spack/repos/builtin/packages/py-jdcal/package.py
@@ -12,6 +12,8 @@ class PyJdcal(PythonPackage):
     homepage = "http://github.com/phn/jdcal"
     pypi = "jdcal/jdcal-1.3.tar.gz"
 
+    version('1.4.1', sha256='472872e096eb8df219c23f2689fc336668bdb43d194094b5cc1707e1640acfc8')
+    version('1.4',   sha256='ea0a5067c5f0f50ad4c7bdc80abad3d976604f6fb026b0b3a17a9d84bb9046c9')
     version('1.3', sha256='b760160f8dc8cc51d17875c6b663fafe64be699e10ce34b6a95184b5aa0fdc9e')
     version('1.2', sha256='5ebedb58b95ebabd30f56abef65139c6f69ec1687cf1d2f3a7c503f9a2cdfa4d')
 
