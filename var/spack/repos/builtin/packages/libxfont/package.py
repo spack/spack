fe2a004cfb71f1241d393dc839c9bdc87c26aba2
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:24:15 +0000

    Add new versions of libxfont

--- var/spack/repos/builtin/packages/libxfont/package.py
+++ var/spack/repos/builtin/packages/libxfont/package.py
@@ -17,6 +17,8 @@ class Libxfont(AutotoolsPackage, XorgPackage):
     homepage = "http://cgit.freedesktop.org/xorg/lib/libXfont"
     xorg_mirror_path = "lib/libXfont-1.5.2.tar.gz"
 
+    version('1.5.4', sha256='59be6eab53f7b0feb6b7933c11d67d076ae2c0fd8921229c703fc7a4e9a80d6e')
+    version('1.5.3', sha256='e8e3538dacf0a3c0317f0b92a092a3bd9d70fd23de10a6d895435ac78de54b7c')
     version('1.5.2', sha256='a7350c75171d03d06ae0d623e42240356d6d3e1ac7dfe606639bf20f0d653c93')
 
     depends_on('libfontenc')
