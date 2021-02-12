ef44895464adcf1fc87a5907e98adbd8c469731f
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:13:56 +0000

    Add new versions of libzip

--- var/spack/repos/builtin/packages/libzip/package.py
+++ var/spack/repos/builtin/packages/libzip/package.py
@@ -13,6 +13,7 @@ class Libzip(AutotoolsPackage):
     homepage = "https://nih.at/libzip/index.html"
     url      = "https://nih.at/libzip/libzip-1.2.0.tar.gz"
 
+    version('1.7.3', sha256='0e2276c550c5a310d4ebf3a2c3dfc43fb3b4602a072ff625842ad4f3238cb9cc')
     version('1.2.0', sha256='6cf9840e427db96ebf3936665430bab204c9ebbd0120c326459077ed9c907d9f')
 
     depends_on('zlib@1.1.2:')
