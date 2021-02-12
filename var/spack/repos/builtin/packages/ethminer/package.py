9b4b31c03803a6cf0f9bd7a282247c38d3c4aaff
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:33:19 +0000

    Add new versions of ethminer

--- var/spack/repos/builtin/packages/ethminer/package.py
+++ var/spack/repos/builtin/packages/ethminer/package.py
@@ -12,6 +12,11 @@ class Ethminer(CMakePackage):
     homepage = "https://github.com/ethereum-mining/ethminer"
     url = "https://github.com/ethereum-mining/ethminer/archive/v0.12.0.tar.gz"
 
+    version('0.19.0', sha256='8cedd087542dee74568c062960aec10f0c95ab3355c553f68e9b7023e01a4582')
+    version('0.18.0', sha256='f666cb171cd05ab397b138b99cd205fdfe449b629f892f0dd7eac5bb50f4bed4')
+    version('0.17.1', sha256='823908ab8a22b6c319eda4922a4708ae2600061f09683325f72b330119769a1d')
+    version('0.17.0', sha256='2cb8d1c3f37757e1cc440656962780585699ce8d7db7b3d47dafa503d7c672c7')
+    version('0.16.2', sha256='c66ed82c830fec094d5e53e565bc7d91a42b4961f48b91b89202f79a8958dfe8')
     version('0.12.0', sha256='71122c8aa1be2c29e46d7f07961fa760b1eb390e4d9a2a21cf900f6482a8755a')
 
     variant('opencl', default=True, description='Enable OpenCL mining.')
