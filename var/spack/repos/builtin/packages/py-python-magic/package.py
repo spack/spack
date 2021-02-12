a21caf202ebd5dc5a28cf63018a73dfd491fbb1e
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:04:15 +0000

    Add new versions of py-python-magic

--- var/spack/repos/builtin/packages/py-python-magic/package.py
+++ var/spack/repos/builtin/packages/py-python-magic/package.py
@@ -13,6 +13,10 @@ class PyPythonMagic(PythonPackage):
     homepage = "https://github.com/ahupp/python-magic"
     pypi = "python-magic/python-magic-0.4.15.tar.gz"
 
+    version('0.4.20', sha256='0cc52ccad086c377b9194014e3dbf98d94b194344630172510a6a3e716b47801')
+    version('0.4.19', sha256='c11721314cad741bde0cb57e978c3789b8cdab11d5456ef5842b7e7529bd43f1')
+    version('0.4.18', sha256='b757db2a5289ea3f1ced9e60f072965243ea43a2221430048fd8cacab17be0ce')
+    version('0.4.17', sha256='adf74ea289dbc1960547abab0fd5a519c21e9dd9bd96d3c280c3ec5e842a1b09')
     version('0.4.15', sha256='f3765c0f582d2dfc72c15f3b5a82aecfae9498bd29ca840d72f37d7bd38bfcd5')
 
     depends_on('py-setuptools', type='build')
