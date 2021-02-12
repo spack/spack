c03d2a78d82061fb184a5a8655bafc0053a93de1
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:56:25 +0000

    Add new versions of tcsh

--- var/spack/repos/builtin/packages/tcsh/package.py
+++ var/spack/repos/builtin/packages/tcsh/package.py
@@ -18,6 +18,7 @@ class Tcsh(AutotoolsPackage):
     url      = "http://ftp.funet.fi/pub/mirrors/ftp.astron.com/pub/tcsh/tcsh-6.20.00.tar.gz"
     list_url = "http://ftp.funet.fi/pub/mirrors/ftp.astron.com/pub/tcsh/old/"
 
+    version('6.22.03', sha256='be2cfd653d2a0c7f506d2dd14c12324ba749bd484037be6df44a3973f52262b7')
     version('6.22.02', sha256='ed287158ca1b00ba477e8ea57bac53609838ebcfd05fcb05ca95021b7ebe885b')
     version('6.21.00', sha256='c438325448371f59b12a4c93bfd3f6982e6f79f8c5aef4bc83aac8f62766e972')
     version('6.20.00', sha256='b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d')
