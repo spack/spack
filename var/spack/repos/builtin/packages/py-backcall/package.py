9eb216e0cd0f849c1f9f782aab9504decd9c31b7
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:12:42 +0000

    Add new versions of py-backcall

--- var/spack/repos/builtin/packages/py-backcall/package.py
+++ var/spack/repos/builtin/packages/py-backcall/package.py
@@ -12,4 +12,5 @@ class PyBackcall(PythonPackage):
     homepage = "https://github.com/takluyver/backcall"
     pypi = "backcall/backcall-0.1.0.tar.gz"
 
+    version('0.2.0', sha256='5cbdbf27be5e7cfadb448baf0aa95508f91f2bbc6c6437cd9cd06e2a4c215e1e')
     version('0.1.0', sha256='38ecd85be2c1e78f77fd91700c76e14667dc21e2713b63876c0eb901196e01e4')
