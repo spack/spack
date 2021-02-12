8defa8707b853b8abf7f8583b850eb2b48b7e4e4
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:49:14 +0000

    Add new versions of py-docker

--- var/spack/repos/builtin/packages/py-docker/package.py
+++ var/spack/repos/builtin/packages/py-docker/package.py
@@ -10,6 +10,11 @@ class PyDocker(PythonPackage):
     homepage = "https://github.com/docker/docker-py"
     pypi = "docker/docker-4.2.1.tar.gz"
 
+    version('4.4.1', sha256='0604a74719d5d2de438753934b755bfcda6f62f49b8e4b30969a4b0a2a8a1220')
+    version('4.4.0', sha256='cffc771d4ea1389fc66bc95cb72d304aa41d1a1563482a9a000fba3a84ed5071')
+    version('4.3.1', sha256='bad94b8dd001a8a4af19ce4becc17f41b09f228173ffe6a4e0355389eef142f2')
+    version('4.3.0', sha256='431a268f2caf85aa30613f9642da274c62f6ee8bae7d70d968e01529f7d6af93')
+    version('4.2.2', sha256='26eebadce7e298f55b76a88c4f8802476c5eaddbdbe38dbc6cce8781c47c9b54')
     version('4.2.1', sha256='380a20d38fbfaa872e96ee4d0d23ad9beb0f9ed57ff1c30653cbeb0c9c0964f2')
 
     depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
