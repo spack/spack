9c02e8357fb87a5508fb71b4d721cbd4a841fac4
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:52:51 +0000

    Add new versions of py-blessings

--- var/spack/repos/builtin/packages/py-blessings/package.py
+++ var/spack/repos/builtin/packages/py-blessings/package.py
@@ -11,6 +11,8 @@ class PyBlessings(PythonPackage):
     homepage = "https://github.com/erikrose/blessings"
     pypi = "blessings/blessings-1.6.tar.gz"
 
+    version('1.7',   sha256='98e5854d805f50a5b58ac2333411b0482516a8210f23f43308baeb58d77c157d')
+    version('1.6.1', sha256='74919575885552e14bc24a68f8b539690bd1b5629180faa830b1a25b8c7fb6ea')
     version('1.6', sha256='edc5713061f10966048bf6b40d9a514b381e0ba849c64e034c4ef6c1847d3007')
 
     depends_on('py-setuptools', type='build')
