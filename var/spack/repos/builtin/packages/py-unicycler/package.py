a88def3667145e090392527b60aee9bc1b4e2412
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:34:03 +0000

    Add new versions of py-unicycler

--- var/spack/repos/builtin/packages/py-unicycler/package.py
+++ var/spack/repos/builtin/packages/py-unicycler/package.py
@@ -17,6 +17,7 @@ class PyUnicycler(PythonPackage):
     homepage = "https://github.com/rrwick/Unicycler"
     url      = "https://github.com/rrwick/Unicycler/archive/v0.4.5.tar.gz"
 
+    version('0.4.8', sha256='e948871e4de9db5964c9ca6f8f877c3cbe6a46f62052dfab52ffe0f45bbbd203')
     version('0.4.7', sha256='a8cf65e46dc2694b0fbd4e9190c73a1f300921457aadfab27a1792b785620d63')
     version('0.4.6', sha256='56f6f358a5d1f8dd0fcd1df04504079fc42cec8453a36ee59ff89295535d03f5')
     version('0.4.5', sha256='67043656b31a4809f8fa8f73368580ba7658c8440b9f6d042c7f70b5eb6b19ae')
