f4674265189a9acd847169851887af057319e427
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:33:00 +0000

    Add new versions of listres

--- var/spack/repos/builtin/packages/listres/package.py
+++ var/spack/repos/builtin/packages/listres/package.py
@@ -13,6 +13,7 @@ class Listres(AutotoolsPackage, XorgPackage):
     homepage = "http://cgit.freedesktop.org/xorg/app/listres"
     xorg_mirror_path = "app/listres-1.0.3.tar.gz"
 
+    version('1.0.4', sha256='949b38c754285c4840af9eb5335f32ef7c5ab26d75574b2a036d2cc0f74bac38')
     version('1.0.3', sha256='87d5698b8aa4d841e45e6556932c9914210cbd8b10003d664b31185b087981be')
 
     depends_on('libxaw')
