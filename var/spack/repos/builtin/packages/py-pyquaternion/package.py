f52c3f08420a554341b817d141978eb7aabd0583
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:54:07 +0000

    Add new versions of py-pyquaternion

--- var/spack/repos/builtin/packages/py-pyquaternion/package.py
+++ var/spack/repos/builtin/packages/py-pyquaternion/package.py
@@ -13,6 +13,8 @@ class PyPyquaternion(PythonPackage):
     homepage = "https://kieranwynn.github.io/pyquaternion/"
     pypi = "pyquaternion/pyquaternion-0.9.5.tar.gz"
 
+    version('0.9.9', sha256='b1f61af219cb2fe966b5fb79a192124f2e63a3f7a777ac3cadf2957b1a81bea8')
+    version('0.9.8', sha256='17b389149adbe2273162d97f91d923ddc0d7e8fc387682e1564d6c0022be582f')
     version('0.9.5', sha256='2d89d19259d62a8fbd25219eee7dacc1f6bb570becb70e1e883f622597c7d81d')
 
     depends_on('py-setuptools', type=('build', 'run'))
