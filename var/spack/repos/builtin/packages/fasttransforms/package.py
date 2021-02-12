db563c67d935246d19b0b94d044ec03cb3885b2b
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:08:05 +0000

    Add new versions of fasttransforms

--- var/spack/repos/builtin/packages/fasttransforms/package.py
+++ var/spack/repos/builtin/packages/fasttransforms/package.py
@@ -16,6 +16,9 @@ class Fasttransforms(MakefilePackage):
     homepage = "https://github.com/MikaelSlevinsky/FastTransforms"
     url      = "https://github.com/MikaelSlevinsky/FastTransforms/archive/v0.3.4.tar.gz"
 
+    version('0.5.0', sha256='9556d0037bd5348a33f15ad6100e32053b6e22cab16a97c504f30d6c52fd0efd')
+    version('0.4.1', sha256='5d30b3b9cb8e1260072b81831e4beeb73e5a956fa8952df8281e9dab399bbbb4')
+    version('0.4.0', sha256='0a7c85a0f10acc0d57f9498925f941b79eff68b942eb89067f973ba08fb7a1bc')
     version('0.3.4', sha256='a5c8b5aedbdb40218521d061a7df65ef32ce153d4e19d232957db7e3e63c7e9b')
 
     variant('quadmath', default=False, description="Support 128-bit floats")
