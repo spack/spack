df274bd9dd0ddbebda7ec8d2a5bdde93aca4a4c4
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:35:07 +0000

    Add new versions of py-stdlib-list

--- var/spack/repos/builtin/packages/py-stdlib-list/package.py
+++ var/spack/repos/builtin/packages/py-stdlib-list/package.py
@@ -11,6 +11,8 @@ class PyStdlibList(PythonPackage):
 
     pypi = "stdlib-list/stdlib-list-0.6.0.tar.gz"
 
+    version('0.8.0', sha256='a1e503719720d71e2ed70ed809b385c60cd3fb555ba7ec046b96360d30b16d9f')
+    version('0.7.0', sha256='66c1c1724a12667cdb35be9f43181c3e6646c194e631efaaa93c1f2c2c7a1f7f')
     version('0.6.0', sha256='133cc99104f5a4e1604dc88ebb393529bd4c2b99ae7e10d46c0b596f3c67c3f0')
 
     depends_on('py-functools32', when="^python@:3.1", type=('build', 'run'))
