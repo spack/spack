0b0870bcd0be8fab57e97ce32f123378e71bbc81
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:13:06 +0000

    Add new versions of py-audioread

--- var/spack/repos/builtin/packages/py-audioread/package.py
+++ var/spack/repos/builtin/packages/py-audioread/package.py
@@ -13,6 +13,7 @@ class PyAudioread(PythonPackage):
     homepage = "https://github.com/beetbox/audioread"
     pypi = "audioread/audioread-2.1.8.tar.gz"
 
+    version('2.1.9', sha256='a3480e42056c8e80a8192a54f6729a280ef66d27782ee11cbd63e9d4d1523089')
     version('2.1.8', sha256='073904fabc842881e07bd3e4a5776623535562f70b1655b635d22886168dd168')
 
     depends_on('py-setuptools', type='build')
