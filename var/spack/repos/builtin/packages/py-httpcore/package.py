d0f970167da11f94488e8cd5a59fb901b800d073
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:06:04 +0000

    Add new versions of py-httpcore

--- var/spack/repos/builtin/packages/py-httpcore/package.py
+++ var/spack/repos/builtin/packages/py-httpcore/package.py
@@ -13,6 +13,11 @@ class PyHttpcore(PythonPackage):
     homepage = "https://github.com/encode/httpcore"
     pypi = "httpcore/httpcore-0.11.0.tar.gz"
 
+    version('0.12.3', sha256='37ae835fb370049b2030c3290e12ed298bf1473c41bb72ca4aa78681eba9b7c9')
+    version('0.12.2', sha256='dd1d762d4f7c2702149d06be2597c35fb154c5eff9789a8c5823fbcf4d2978d6')
+    version('0.12.1', sha256='3c5fcd97c52c3f6a1e4d939d776458e6177b5c238b825ed51d72840e582573b5')
+    version('0.12.0', sha256='2526a38f31ac5967d38b7f593b5d8c4bd3fa82c21400402f866ba3312946acbf')
+    version('0.11.1', sha256='a35dddd1f4cc34ff37788337ef507c0ad0276241ece6daf663ac9e77c0b87232')
     version('0.11.0', sha256='35ffc735d746b83f8fc6d36f82600e56117b9e8adc65d0c0423264b6ebfef7bf')
 
     depends_on('py-setuptools', type='build')
