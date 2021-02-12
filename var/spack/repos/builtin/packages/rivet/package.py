695890351ec8afbc45bc72c885438a7d19e9f746
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:49:05 +0000

    Add new versions of rivet

--- var/spack/repos/builtin/packages/rivet/package.py
+++ var/spack/repos/builtin/packages/rivet/package.py
@@ -15,6 +15,7 @@ class Rivet(AutotoolsPackage):
 
     tags = ['hep']
 
+    version('3.1.3', sha256='53ddce41705b9c22b2eaa90603f6659aa9bf46c466d8772ca9dbe4430972e021')
     version('3.1.2',  sha256='c041d09644f4eae7c212d82237033267fbc1583dfbb4e3e67377f86cece9577a')
     version('3.1.1',  sha256='7c98b26af5f859bc65200499d15765e4b056b4cf233b34176f27a7e6bc4cf9b1')
     version('3.1.0',  sha256='4e156daee5eb10bd1573ef32d4a6a6df74788cd9180fc977db93ef4cb281000c')
