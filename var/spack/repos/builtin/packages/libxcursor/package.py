6d9b81e969895b3a70a24f6ff3db14e8b7eae7fd
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:13:25 +0000

    Add new versions of libxcursor

--- var/spack/repos/builtin/packages/libxcursor/package.py
+++ var/spack/repos/builtin/packages/libxcursor/package.py
@@ -12,6 +12,8 @@ class Libxcursor(AutotoolsPackage, XorgPackage):
     homepage = "http://cgit.freedesktop.org/xorg/lib/libXcursor"
     xorg_mirror_path = "lib/libXcursor-1.1.14.tar.gz"
 
+    version('1.2.0',  sha256='ad5b2574fccaa4c3fa67b9874fbed863d29ad230c784e9a08b20692418f6a1f8')
+    version('1.1.15', sha256='449befea2b11dde58ba3323b2c1ec30550013bd84d80501eb56d0048e62251a1')
     version('1.1.14', sha256='be0954faf274969ffa6d95b9606b9c0cfee28c13b6fc014f15606a0c8b05c17b')
 
     depends_on('libxrender@0.8.2:')
