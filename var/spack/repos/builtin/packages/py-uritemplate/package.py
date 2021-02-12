f71a26fea605d51dc93a0861069f448f0b40cb8f
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:29:56 +0000

    Add new versions of py-uritemplate

--- var/spack/repos/builtin/packages/py-uritemplate/package.py
+++ var/spack/repos/builtin/packages/py-uritemplate/package.py
@@ -12,6 +12,7 @@ class PyUritemplate(PythonPackage):
     homepage = "https://uritemplate.readthedocs.org/"
     pypi = "uritemplate/uritemplate-3.0.0.tar.gz"
 
+    version('3.0.1', sha256='5af8ad10cec94f215e3f48112de2022e1d5a37ed427fbd88652fa908f2ab7cae')
     version('3.0.0', sha256='c02643cebe23fc8adb5e6becffe201185bf06c40bda5c0b4028a93f1527d011d')
 
     depends_on('py-setuptools', type='build')
