8b013a17d92d06616378c27c6fc41f91762cf09c
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:11:01 +0000

    Add new versions of perl-log-log4perl

--- var/spack/repos/builtin/packages/perl-log-log4perl/package.py
+++ var/spack/repos/builtin/packages/perl-log-log4perl/package.py
@@ -12,4 +12,7 @@ class PerlLogLog4perl(PerlPackage):
     homepage = "http://search.cpan.org/~mschilli/Log-Log4perl-1.44/lib/Log/Log4perl.pm"
     url      = "https://cpan.metacpan.org/authors/id/M/MS/MSCHILLI/Log-Log4perl-1.46.tar.gz"
 
+    version('1.49', sha256='b739187f519146cb6bebcfc427c64b1f4138b35c5f4c96f46a21ed4a43872e16')
+    version('1.48', sha256='cf6e9fc1f9183fabbe540d84f603c6541458034092b7c53e41008093db62dc98')
+    version('1.47', sha256='9001dded011226538b9a50c7856815bb0dba72a1e6218fdcaba56f651356b96f')
     version('1.46', sha256='31011a17c04e78016e73eaa4865d0481d2ffc3dc22813c61065d90ad73c64e6f')
