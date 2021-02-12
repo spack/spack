e4ad6452867a5ddf8786d41fe0c41437a687d838
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:38:43 +0000

    Add new versions of py-colorama

--- var/spack/repos/builtin/packages/py-colorama/package.py
+++ var/spack/repos/builtin/packages/py-colorama/package.py
@@ -12,6 +12,9 @@ class PyColorama(PythonPackage):
     homepage = "https://github.com/tartley/colorama"
     pypi = "colorama/colorama-0.3.7.tar.gz"
 
+    version('0.4.4', sha256='5941b2b48a20143d2267e95b1c2a7603ce057ee39fd88e7329b0c292aa16869b')
+    version('0.4.3', sha256='e96da0d330793e2cb9485e9ddfd918d456036c7149416295932478192f4436a1')
+    version('0.4.2', sha256='3e0d36a97e86c019317b4896075b38a72438df68abdde0518a6ca12c88448a4b')
     version('0.4.1', sha256='05eed71e2e327246ad6b38c540c4a3117230b19679b875190486ddd2d721422d')
     version('0.3.7', sha256='e043c8d32527607223652021ff648fbb394d5e19cba9f1a698670b338c9d782b')
 
