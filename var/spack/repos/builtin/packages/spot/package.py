f3ef0d3fc6fe9a1ff6adba2a4df253dad4746215
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:19:48 +0000

    Add new versions of spot

--- var/spack/repos/builtin/packages/spot/package.py
+++ var/spack/repos/builtin/packages/spot/package.py
@@ -12,6 +12,8 @@ class Spot(AutotoolsPackage):
     homepage = "https://spot.lrde.epita.fr/"
     url      = "http://www.lrde.epita.fr/dload/spot/spot-1.99.3.tar.gz"
 
+    version('2.9.6', sha256='3cc6f69f17f0d1566d68be7040099df70203748b66121354d8ab84d8d13dd3a8')
+    version('2.9.5', sha256='0a863adc177b606cce8a90f345b316a31382c841e102ec39d74cd89a4ffba05f')
     version('2.9.4', sha256='e11208323baabe9b5f98098d4b9bb39803fb102a68abbbaf900f1fcd578f0f85')
     version('1.99.3', sha256='86964af559994af4451a8dca663a9e1db6e869ed60e747ab60ce72dddc31b61b')
     version('1.2.6', sha256='360678c75f6741f697e8e56cdbc9937f104eb723a839c3629f0dc5dc6de11bfc')
