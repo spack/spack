ebd821b05179e60e7f61dc41412a39c789cafa92
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:28:59 +0000

    Add new versions of exiv2

--- var/spack/repos/builtin/packages/exiv2/package.py
+++ var/spack/repos/builtin/packages/exiv2/package.py
@@ -14,6 +14,7 @@ class Exiv2(CMakePackage):
     homepage = "https://www.exiv2.org/"
     url      = "https://github.com/Exiv2/exiv2/archive/v0.27.2.tar.gz"
 
+    version('0.27.3', sha256='6398bc743c32b85b2cb2a604273b8c90aa4eb0fd7c1700bf66cbb2712b4f00c1')
     version('0.27.2', sha256='3dbcaf01fbc5b98d42f091d1ff0d4b6cd9750dc724de3d9c0d113948570b2934')
 
     depends_on('zlib', type='link')
