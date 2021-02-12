17d4c6950a7f1be6d7774ab78a5ae3950f35fce7
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:12:39 +0000

    Add new versions of tixi

--- var/spack/repos/builtin/packages/tixi/package.py
+++ var/spack/repos/builtin/packages/tixi/package.py
@@ -14,6 +14,8 @@ class Tixi(CMakePackage):
     url      = "https://github.com/DLR-SC/tixi/archive/v3.0.3.tar.gz"
     git      = "https://github.com/DLR-SC/tixi.git"
 
+    version('3.1.1', sha256='b40693bbf1b6ee3e6feeca08b5153e4d7b26dfaf992fb3349beb38e24064f8be')
+    version('3.1.0', sha256='4547133e452f3455b5a39045a8528955dce55faf059afe652a350ecf37d709ba')
     version('3.0.3', sha256='3584e0cec6ab811d74fb311a9af0663736b1d7f11b81015fcb378efaf5ad3589')
     version('2.2.4', sha256='9080d2a617b7c411b9b4086de23998ce86e261b88075f38c73d3ce25da94b21c')
 
