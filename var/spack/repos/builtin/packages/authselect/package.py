1f297abdc7768c33dfb049b01745ae4afee9b855
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:56:29 +0000

    Add new versions of authselect

--- var/spack/repos/builtin/packages/authselect/package.py
+++ var/spack/repos/builtin/packages/authselect/package.py
@@ -12,6 +12,7 @@ class Authselect(AutotoolsPackage):
     homepage = "https://github.com/authselect/authselect"
     url      = "https://github.com/authselect/authselect/archive/1.2.1.tar.gz"
 
+    version('1.2.2', sha256='1b0d7b1c2a83734b4fefc06355d7a6965d5fefa62d99944cdff1e835d7b91105')
     version('1.2.1', sha256='6f58c36d8b405da836dc9d1f44c1a22660c60f9e7ece327138d1b2492cb57749')
     version('1.2',   sha256='c354c87a0115612cb51b09b5157f151569e16384cdd69f32b8515209036531b4')
     version('1.1',   sha256='39b888575980c1ecac7022dfe5a5452eef59cef850b8544ed5f928e2e8a335dd')
