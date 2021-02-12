c6a0c83a65a7fa7c7445329c94edaa3b4fd97d5f
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:30:25 +0000

    Add new versions of rsbench

--- var/spack/repos/builtin/packages/rsbench/package.py
+++ var/spack/repos/builtin/packages/rsbench/package.py
@@ -14,6 +14,9 @@ class Rsbench(MakefilePackage):
     homepage = "https://github.com/ANL-CESAR/RSBench"
     url = "https://github.com/ANL-CESAR/RSBench/archive/v2.tar.gz"
 
+    version('12', sha256='2e437dbdaf7bf12bb9ade429d46a9e74fd519fc4686777a452770790d0546499')
+    version('11', sha256='8e3b806626fe803ef6284c1e20a05063fc89be153c81e4bd629b6db82eaed3da')
+    version('10', sha256='180ceec6ac7a0b8107b897d428d640abc22a18c6c65101a348e409e5db3505cc')
     version('2', sha256='1e97a38a863836e98cedc5cc669f8fdcaed905fafdc921d2bce32319b3e157ff')
     version('0', sha256='95c06cf4cb6f396f9964d5e4b58a477bf9d7131cd39804480f1cb74e9310b271')
 
