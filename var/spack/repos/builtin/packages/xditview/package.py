6c8ba22f945fd9f02671003a60e1328b13c800e2
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:13:05 +0000

    Add new versions of xditview

--- var/spack/repos/builtin/packages/xditview/package.py
+++ var/spack/repos/builtin/packages/xditview/package.py
@@ -12,6 +12,7 @@ class Xditview(AutotoolsPackage, XorgPackage):
     homepage = "http://cgit.freedesktop.org/xorg/app/xditview"
     xorg_mirror_path = "app/xditview-1.0.4.tar.gz"
 
+    version('1.0.5', sha256='67c4522a24dd7e8762ae458fe216c5bddc12101af295e78c19ff7313fa8cbfad')
     version('1.0.4', sha256='73ad88cfc879edcc6ede65999c11d670da27575388126795d71f3ad60286d379')
 
     depends_on('libxaw')
