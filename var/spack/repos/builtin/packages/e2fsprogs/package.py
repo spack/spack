9718a0bb0c362c7655ceeba6a8cd315053a3b5e4
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:07:07 +0000

    Add new versions of e2fsprogs

--- var/spack/repos/builtin/packages/e2fsprogs/package.py
+++ var/spack/repos/builtin/packages/e2fsprogs/package.py
@@ -13,6 +13,9 @@ class E2fsprogs(AutotoolsPackage):
     homepage = "https://github.com/tytso/e2fsprogs"
     url      = "https://github.com/tytso/e2fsprogs/archive/v1.45.6.tar.gz"
 
+    version('1.46.1', sha256='6e37ac2bf0db9a30b986cae71224526bce930730bd52fad46af86a1ab39b61ca')
+    version('1.46.0', sha256='9c17885fad5193395e8e49ff6ccf01b03f711d06ffc11733f60d396cc6b63ed9')
+    version('1.45.7', sha256='1014b7400cb510a75ad7dcb504f3e28d11a3808dafb53e0177c4ce7035b1b682')
     version('1.45.6', sha256='d785164a2977cd88758cb0cac5c29add3fe491562a60040cfb193abcd0f9609b')
     version('1.45.5', sha256='0fd76e55c1196c1d97a2c01f2e84f463b8e99484541b43ff4197f5a695159fd3')
 
