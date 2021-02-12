c3ab9a8819ee4e7d35cef18924637d1ff582c730
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:13:06 +0000

    Add new versions of py-uvloop

--- var/spack/repos/builtin/packages/py-uvloop/package.py
+++ var/spack/repos/builtin/packages/py-uvloop/package.py
@@ -12,6 +12,7 @@ class PyUvloop(PythonPackage):
     homepage = "http://github.com/MagicStack/uvloop"
     pypi = "uvloop/uvloop-0.14.0.tar.gz"
 
+    version('0.15.0', sha256='1a503d5b49da6e3dd5607d6e533a5315b1caedbf629901807c65a23a09cad065')
     version('0.14.0', sha256='123ac9c0c7dd71464f58f1b4ee0bbd81285d96cdda8bc3519281b8973e3a461e')
 
     depends_on('py-setuptools', type='build')
