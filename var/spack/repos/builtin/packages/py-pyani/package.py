ddd1125ff0f1f551659b6e681c17d4b4961c0ada
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:56:13 +0000

    Add new versions of py-pyani

--- var/spack/repos/builtin/packages/py-pyani/package.py
+++ var/spack/repos/builtin/packages/py-pyani/package.py
@@ -16,6 +16,9 @@ class PyPyani(PythonPackage):
     homepage = "http://widdowquinn.github.io/pyani"
     pypi = "pyani/pyani-0.2.7.tar.gz"
 
+    version('0.2.10', sha256='154e421c52125d3bf309c96cfcfd0defba4b990a14cc7fdac89a8b31e5b22b59')
+    version('0.2.9',  sha256='0b87870a03cf5ccd8fbab7572778903212a051990f00cf8e4ef5887b36b9ec91')
+    version('0.2.8',  sha256='8059f3ede59225d0cb81ddc4d7193c1744b3732a548fa05b09c5992c27837345')
     version('0.2.7', sha256='dbc6c71c46fbbfeced3f8237b84474221268b51170caf044bec8559987a7deb9')
     version('0.2.6', sha256='e9d899bccfefaabe7bfa17d48eef9c713d321d2d15465f7328c8984807c3dd8d')
 
