77e7294644cd3bffcd882e487f4013e931c5d8a0
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:53:27 +0000

    Add new versions of ufo-filters

--- var/spack/repos/builtin/packages/ufo-filters/package.py
+++ var/spack/repos/builtin/packages/ufo-filters/package.py
@@ -14,6 +14,9 @@ class UfoFilters(CMakePackage):
     homepage = "https://ufo.kit.edu"
     url      = "https://github.com/ufo-kit/ufo-filters/archive/v0.14.1.tar.gz"
 
+    version('0.16.0', sha256='80d6444d92f999787e4470b2c1c4dae5a81db1058e72b292ebfe662ee56471cd')
+    version('0.15.1', sha256='9ae303b43f8771693342d41099d1884c572580fa4a022c7618b4adaf420bf7b4')
+    version('0.15.0', sha256='f682ac91fcff5610d4d92669881bd5baf3d7b71ab8c5713f3f1fadbdf336a9d4')
     version('0.14.1', sha256='084d7cdef59205e1a048e5c142be1ffeaacedc42965824b642e8302ef30ebb13')
 
     depends_on('ufo-core')
