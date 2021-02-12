2ebbeff6413fad545aec293187142c37fc5cc824
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 20:52:37 +0000

    Add new versions of perl-test-needs

--- var/spack/repos/builtin/packages/perl-test-needs/package.py
+++ var/spack/repos/builtin/packages/perl-test-needs/package.py
@@ -12,4 +12,5 @@ class PerlTestNeeds(PerlPackage):
     homepage = "http://search.cpan.org/~haarg/Test-Needs-0.002005/lib/Test/Needs.pm"
     url      = "http://search.cpan.org/CPAN/authors/id/H/HA/HAARG/Test-Needs-0.002005.tar.gz"
 
+    version('0.002006', sha256='77f9fff0c96c5e09f34d0416b3533c3319f7cd0bb1f7fe8f8072ad59f433f0e5')
     version('0.002005', sha256='5a4f33983586edacdbe00a3b429a9834190140190dab28d0f873c394eb7df399')
