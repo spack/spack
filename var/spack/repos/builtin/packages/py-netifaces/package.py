69b6eb294a3748f69fbbe6f54f365f16ffe20023
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:50:21 +0000

    Add new versions of py-netifaces

--- var/spack/repos/builtin/packages/py-netifaces/package.py
+++ var/spack/repos/builtin/packages/py-netifaces/package.py
@@ -12,6 +12,10 @@ class PyNetifaces(PythonPackage):
     homepage = "https://bitbucket.org/al45tair/netifaces"
     pypi = "netifaces/netifaces-0.10.5.tar.gz"
 
+    version('0.10.9', sha256='2dee9ffdd16292878336a58d04a20f0ffe95555465fee7c9bd23b3490ef2abf3')
+    version('0.10.8', sha256='befc9800751991c005fcc24e75be90c5752e5c1907ed4fa4efa17adfcc09d490')
+    version('0.10.7', sha256='bd590fcb75421537d4149825e1e63cca225fd47dad861710c46bd1cb329d8cbd')
+    version('0.10.6', sha256='0c4da523f36d36f1ef92ee183f2512f3ceb9a9d2a45f7d19cda5a42c6689ebe0')
     version('0.10.5', sha256='59d8ad52dd3116fcb6635e175751b250dc783fb011adba539558bd764e5d628b')
 
     depends_on('py-setuptools', type='build')
