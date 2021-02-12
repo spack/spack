1279559dee04aed9a3624fd2f728a20afe7ea0a1
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:11:15 +0000

    Add new versions of py-azure-mgmt-web

--- var/spack/repos/builtin/packages/py-azure-mgmt-web/package.py
+++ var/spack/repos/builtin/packages/py-azure-mgmt-web/package.py
@@ -10,6 +10,8 @@ class PyAzureMgmtWeb(PythonPackage):
     homepage = "https://github.com/Azure/azure-sdk-for-python"
     pypi = "azure-mgmt-web/azure-mgmt-web-0.47.0.zip"
 
+    version('1.0.0',  sha256='c4b218a5d1353cd7c55b39c9b2bd1b13bfbe3b8a71bc735122b171eab81670d1')
+    version('0.48.0', sha256='da0f9e3b57528c72a7bc92e3515413a4a4fdbc9626c26ac04b7551a7739a81ec')
     version('0.47.0', sha256='789a328e2a60df48a82452ca6fbc1a7b4adf3c38d4701d278efe4e81cf21cce8')
 
     depends_on('py-setuptools', type='build')
