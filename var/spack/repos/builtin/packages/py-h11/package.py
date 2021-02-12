1ca947ffc024ac8f665f5917c202d50c5ae16a86
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:02:02 +0000

    Add new versions of py-h11

--- var/spack/repos/builtin/packages/py-h11/package.py
+++ var/spack/repos/builtin/packages/py-h11/package.py
@@ -12,6 +12,8 @@ class PyH11(PythonPackage):
     homepage = "https://github.com/python-hyper/h11"
     pypi = "h11/h11-0.10.0.tar.gz"
 
+    version('0.12.0', sha256='47222cb6067e4a307d535814917cd98fd0a57b6788ce715755fa2b6c28b56042')
+    version('0.11.0', sha256='3c6c61d69c6f13d41f1b80ab0322f1872702a3ba26e12aa864c928f6a43fbaab')
     version('0.10.0', sha256='311dc5478c2568cc07262e0381cdfc5b9c6ba19775905736c87e81ae6662b9fd')
     version('0.9.0', sha256='33d4bca7be0fa039f4e84d50ab00531047e53d6ee8ffbc83501ea602c169cae1')
 
