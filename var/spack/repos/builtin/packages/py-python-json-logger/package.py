5bf84a314076104f54c223e3d7b14c4fd65433fd
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:14:53 +0000

    Add new versions of py-python-json-logger

--- var/spack/repos/builtin/packages/py-python-json-logger/package.py
+++ var/spack/repos/builtin/packages/py-python-json-logger/package.py
@@ -12,6 +12,8 @@ class PyPythonJsonLogger(PythonPackage):
     homepage = "https://github.com/madzak/python-json-logger"
     pypi = "python-json-logger/python-json-logger-0.1.11.tar.gz"
 
+    version('2.0.1', sha256='f26eea7898db40609563bed0a7ca11af12e2a79858632706d835a0f961b7d398')
+    version('2.0.0', sha256='6c15023a8571200228472d4c9de7cb891cd45f670061f7729b8209bf643d5bbf')
     version('0.1.11', sha256='b7a31162f2a01965a5efb94453ce69230ed208468b0bbc7fdfc56e6d8df2e281')
 
     depends_on('python@2.7:', type=('build', 'run'))
