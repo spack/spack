2332875de35c5d8ea8169575b920c4f80e047e73
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:27:50 +0000

    Add new versions of zziplib

--- var/spack/repos/builtin/packages/zziplib/package.py
+++ var/spack/repos/builtin/packages/zziplib/package.py
@@ -15,6 +15,9 @@ class Zziplib(AutotoolsPackage):
     homepage = "https://github.com/gdraheim/zziplib"
     url      = "https://github.com/gdraheim/zziplib/archive/v0.13.69.tar.gz"
 
+    version('0.13.72', sha256='93ef44bf1f1ea24fc66080426a469df82fa631d13ca3b2e4abaeab89538518dc')
+    version('0.13.71', sha256='2ee1e0fbbb78ec7cc46bde5b62857bc51f8d665dd265577cf93584344b8b9de2')
+    version('0.13.70', sha256='a1457262d7a237dc50ce1f98ca57242bc714055ff81146f419ee53cdea1bf029')
     version('0.13.69', sha256='846246d7cdeee405d8d21e2922c6e97f55f24ecbe3b6dcf5778073a88f120544')
 
     patch('python2to3.patch')
