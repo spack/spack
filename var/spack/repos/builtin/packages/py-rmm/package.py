5d118e462defec321eac8fabbe4592f2320c25bf
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 21:16:26 +0000

    Add new versions of py-rmm

--- var/spack/repos/builtin/packages/py-rmm/package.py
+++ var/spack/repos/builtin/packages/py-rmm/package.py
@@ -14,6 +14,8 @@ class PyRmm(PythonPackage):
     homepage = "https://github.com/rapidsai/rmm"
     url      = "https://github.com/rapidsai/rmm/archive/v0.15.0.tar.gz"
 
+    version('0.17.0', sha256='84ba6fbbdbc550b70ca1a6c63dc24dd9a5084f7701e319b9a982b9ce5a0889f6')
+    version('0.16.0', sha256='972fcdd3b5ab5af9a8e89671f0dbd47cfe1559ff8c4031c515ab89a487201ae2')
     version('0.15.0',  sha256='599f97b95d169a90d11296814763f7e151a8a1e060ba10bc6c8f4684a5cd7972')
 
     depends_on('python@3.6:', type=('build', 'run'))
