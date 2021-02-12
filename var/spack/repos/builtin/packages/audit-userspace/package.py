e7de032df9dcd92d4fd23f7d446402fef5dda4a5
Harmen Stoppels <me@harmenstoppels.nl>
2021-02-12 22:02:14 +0000

    Add new versions of audit-userspace

--- var/spack/repos/builtin/packages/audit-userspace/package.py
+++ var/spack/repos/builtin/packages/audit-userspace/package.py
@@ -12,6 +12,8 @@ class AuditUserspace(AutotoolsPackage):
     homepage = "https://github.com/linux-audit/audit-userspace"
     url      = "https://github.com/linux-audit/audit-userspace/archive/v2.8.5.tar.gz"
 
+    version('3.0.1', sha256='82e5d684c56c172977de6adb8ed0fb54218cc92f1f05e8303e13868e065886df')
+    version('3.0',   sha256='fc57db8cf3e7ebb9f871d523832694b6667a990bcbf41d854248f391e40c0a91')
     version('2.8.5', sha256='835ffdd65056ba0c26509dbf48882713b00dbe70e1d8cf25d538501136c2e3e9')
     version('2.8.4', sha256='089dfdceb38edf056202a6de4892fd0c9aaa964c08bd7806c5d0c7c33f09e18d')
     version('2.8.3', sha256='c239e3813b84bc264aaf2f796c131c1fe02960244f789ec2bd8d88aad4561b29')
