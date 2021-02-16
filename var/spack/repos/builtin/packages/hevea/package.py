033e290755672bb8de9b9547ed15e9bfed9f8297
Olivier Cessenat <cessenat@gmail.com>
2021-02-16 11:45:41 +0100

    hevea: Removing comments that read as instructions

--- var/spack/repos/builtin/packages/hevea/package.py
+++ var/spack/repos/builtin/packages/hevea/package.py
@@ -9,16 +9,12 @@ from spack import *
 class Hevea(MakefilePackage):
     """Hevea a fast Latex to HTML translator"""
 
-    # Add a proper url for your package's homepage here.
     homepage = "http://hevea.inria.fr/"
     url      = "https://github.com/maranget/hevea/archive/v2.35.tar.gz"
     git      = "https://github.com/maranget/hevea.git"
 
-    # Add a list of GitHub accounts to
-    # notify when the package is updated.
     maintainers = ['scemama', 'cessenat']
 
-    # Add proper versions here.
     version('develop', branch='master')
     version('2.35', sha256='f189bada5d3e5b35855dfdfdb5b270c994fc7a2366b01250d761359ad66c9ecb')
     version('2.34', sha256='f505a2a5bafdc2ea389ec521876844e6fdcb5c1b656396b7e8421c1631469ea2')
