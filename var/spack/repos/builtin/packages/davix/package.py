7012f46c86056230efdd5ac74895a954245fb4b4
iarspider <iarspider@gmail.com>
2021-02-18 22:16:57 +0100

    Fix checksum

--- var/spack/repos/builtin/packages/davix/package.py
+++ var/spack/repos/builtin/packages/davix/package.py
@@ -12,7 +12,7 @@ class Davix(CMakePackage):
     homepage = "https://davix.web.cern.ch/davix/docs/devel/index.html"
     url      = "https://github.com/cern-fts/davix/releases/download/R_0_7_6/davix-0.7.6.tar.gz"
 
-    version('0.7.6', sha256='984d01d06a22addf6d6a1dfe72e279203ee29cb7cda9394e6fa3105357837138')
+    version('0.7.6', sha256='a2e7fdff29f7ba247a3bcdb08ab1db6d6ed745de2d3971b46526986caf360673')
     version('0.7.5', sha256='d920ca976846875d83af4dc50c99280bb3741fcf8351d5733453e70fa5fe6fc8')
     version('0.7.3', sha256='cd46276e72c6a0da1e2ad30eb66ec509a4c023687767c62a66713fa8c23d328a')
     version('0.6.9', sha256='fbd97eb5fdf82ca48770d06bf8e2805b35f23255478aa381a9d25a49eb98e348')
