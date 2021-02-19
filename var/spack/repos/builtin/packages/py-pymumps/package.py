57131b27cc7c540b39feeed0a8df2793dcea8bc7
Tom Payerle <payerle@umd.edu>
2021-02-19 10:40:33 -0500

    py-pymumps: fix dependency types
    Following suggestion of @adamjstewart

--- var/spack/repos/builtin/packages/py-pymumps/package.py
+++ var/spack/repos/builtin/packages/py-pymumps/package.py
@@ -19,10 +19,10 @@ class PyPymumps(PythonPackage):
 
     version('0.3.2', sha256='f290ec4850098f108fb91cb9e7fa07302ebf5076e4329f8e6ea4924de8ba35df')
 
-    depends_on('py-cython', type='build')
-    depends_on('py-setuptools', type='build')
-    depends_on('mumps', type=('build', 'run'))
-    depends_on('py-mpi4py', type=('build', 'run'))
+    depends_on('py-cython', type='link')
+    depends_on('py-setuptools', type='link')
+    depends_on('mumps', type='link')
+    depends_on('py-mpi4py', type='link')
 
     # Patch to add libmumps_common.so to library dependencies
     # See https://github.com/PyMumps/pymumps/issues/13
