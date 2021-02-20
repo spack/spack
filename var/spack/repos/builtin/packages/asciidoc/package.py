fa19a646581f01cee93d6ca58bd6b1d30f09f51d
Olivier Cessenat <cessenat@gmail.com>
2021-02-20 10:29:30 +0100

    asciidoc: current sourceforge a2x needs python2, new github release python3

--- var/spack/repos/builtin/packages/asciidoc/package.py
+++ var/spack/repos/builtin/packages/asciidoc/package.py
@@ -18,13 +18,17 @@ class Asciidoc(AutotoolsPackage):
 
     version('master', branch='master')
     version('9.1.0', sha256='fd499fcf51317b1aaf27336fb5e919c44c1f867f1ae6681ee197365d3065238b')
+    version('9.0.5', sha256='edc8328c3682a8568172656f6fc309b189f65219a49517966c7ea144cb25f8b2')
+    version('9.0.3', sha256='d99c8be8e8a9232742253c2d87c547b2efd4bbd3f0c1e23ef14898ad0fff77c4')
+    version('9.0.2', sha256='185fd68e47034c4dd892e1d4ae64c81152bc049e9bdc7d1ad63f927d35810a3b')
     version('8.6.9', sha256='78db9d0567c8ab6570a6eff7ffdf84eadd91f2dfc0a92a2d0105d323cab4e1f0')
 
-    depends_on('libxml2')
-    depends_on('libxslt')
-    depends_on('docbook-xml')
-    depends_on('docbook-xsl')
+    depends_on('libxml2',     type=('build', 'run'))
+    depends_on('libxslt',     type=('build', 'run'))
+    depends_on('docbook-xml', type=('build', 'run'))
+    depends_on('docbook-xsl', type=('build', 'run'))
     depends_on('python@2.7.0:2.7.99', when='@:8.6.9')
+    depends_on('python@3.5:',         when='@9.0.2:')
 
     def url_for_version(self, version):
         if version > Version('8.6.9'):
