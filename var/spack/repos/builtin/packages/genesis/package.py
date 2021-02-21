5b17c53eb2165c25ec7a65b6c7ca028bf0358495
Takanori Ihara <ihara.takanori@jp.fujitsu.com>
2021-02-21 13:37:37 +0900

    genesis: Fix for comments and add test method

--- var/spack/repos/builtin/packages/genesis/package.py
+++ var/spack/repos/builtin/packages/genesis/package.py
@@ -17,36 +17,50 @@ class Genesis(AutotoolsPackage, CudaPackage):
     url = "https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2020/09/genesis-1.5.1.tar.bz2"
     git = "https://github.com/genesis-release-r-ccs/genesis-2.0.git"
 
-    version('2.0b', branch='master')
-    version('1.5.1', sha256='62a453a573c36779484b4ffed2dfa56ea03dfe1308d631b33ef03f733259b3ac',
-            url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2020/09/genesis-1.5.1.tar.bz2')
+    version("2.0b", branch="master")
+    version(
+        "1.5.1",
+        sha256="62a453a573c36779484b4ffed2dfa56ea03dfe1308d631b33ef03f733259b3ac",
+        url="https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2020/09/genesis-1.5.1.tar.bz2",
+    )
 
-    resource(when='@1.5.1',
-             name='user_guide',
-             url='https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2019/10/GENESIS-1.4.0.pdf',
-             sha256='da2c3f8bfa1e93adb992d3cfce09fb45d8d447a94f9a4f884ac834ea7279b9c7',
-             expand=False,
-             placement='doc')
+    resource(
+        when="@1.5.1",
+        name="user_guide",
+        url="https://www.r-ccs.riken.jp/labs/cbrt/wp-content/uploads/2019/10/GENESIS-1.4.0.pdf",
+        sha256="da2c3f8bfa1e93adb992d3cfce09fb45d8d447a94f9a4f884ac834ea7279b9c7",
+        expand=False,
+        placement="doc",
+    )
 
     variant("openmp", default=True, description="Enable OpenMP.")
     variant("single", default=False, description="Enable single precision.")
     variant("hmdisk", default=False, description="Enable huge molecule on hard disk.")
-    variant("fugaku", default=False, description="Enable Fugaku option")
 
-    conflicts("%clang", when="+openmp")
-    conflicts('+fugaku', when="@:1.9.9", msg='Fugaku option is not available.')
+    conflicts("%apple-clang", when="+openmp")
 
-    depends_on("autoconf", type="build")
-    depends_on("automake", type="build")
-    depends_on("libtool", type="build")
-    depends_on("m4", type="build")
+    depends_on("autoconf", type="build", when="@1.5.1 %fj")
+    depends_on("autoconf", type="build", when="@2.0b")
+    depends_on("automake", type="build", when="@1.5.1 %fj")
+    depends_on("automake", type="build", when="@2.0b")
+    depends_on("libtool", type="build", when="@1.5.1 %fj")
+    depends_on("libtool", type="build", when="@2.0b")
+    depends_on("m4", type="build", when="@1.5.1 %fj")
+    depends_on("m4", type="build", when="@2.0b")
 
     depends_on("mpi", type=("build", "run"))
     depends_on("lapack")
+    depends_on("python@2.7.18", type=("build", "run"), when="@2.0b")
 
-    patch("fj_compiler.patch", when="%fj")
+    patch("fj_compiler.patch", when="@2.0b %fj")
+    patch("fj_compiler_1.5.1.patch", when="@1.5.1 %fj")
 
     parallel = False
+
+    @property
+    def force_autoreconf(self):
+        # Run autoreconf due to build system patch
+        return self.spec.satisfies("@1.5.1 %fj")
 
     def configure_args(self):
         spec = self.spec
@@ -55,11 +69,11 @@ class Genesis(AutotoolsPackage, CudaPackage):
         options.extend(self.enable_or_disable("single"))
         options.extend(self.enable_or_disable("hmdisk"))
         if "+cuda" in spec:
-            options.append('--enable-gpu')
+            options.append("--enable-gpu")
             options.append("--with-cuda=%s" % spec["cuda"].prefix)
         else:
-            options.append('--disable-gpu')
-        if "+fugaku" in spec:
+            options.append("--disable-gpu")
+        if spec.target == "a64fx" and self.spec.satisfies("@2.0b %fj"):
             options.append("--host=Fugaku")
         return options
 
@@ -70,10 +84,29 @@ class Genesis(AutotoolsPackage, CudaPackage):
         env.set("CXX", self.spec["mpi"].mpicxx, force=True)
         env.set("LAPACK_LIBS", "{0}".format(self.spec["lapack"].libs.ld_flags))
         if "+cuda" in self.spec:
-            cuda_arch = self.spec.variants['cuda_arch'].value
-            cuda_gencode = ' '.join(self.cuda_flags(cuda_arch))
+            cuda_arch = self.spec.variants["cuda_arch"].value
+            cuda_gencode = " ".join(self.cuda_flags(cuda_arch))
             env.set("NVCCFLAGS", cuda_gencode)
 
     def install(self, spec, prefix):
         make("install")
-        install_tree('doc', prefix.share.doc)
+        install_tree("doc", prefix.share.doc)
+
+    @run_after("install")
+    def cache_test_sources(self):
+        if self.spec.satisfies("@2.0b"):
+            self.cache_extra_test_sources(["tests"])
+
+    def test(self):
+        if self.spec.satisfies("@2.0b"):
+            exe_name = self.spec["python"].command.path
+            test_name = join_path(
+                self.install_test_root, "tests", "regression_test", "test.py"
+            )
+            bin_name = join_path(self.prefix.bin, "spdyn")
+            opts = [
+                test_name,
+                self.spec["mpi"].prefix.bin.mpirun + " -np 8 " + bin_name,
+            ]
+            env["OMP_NUM_THREADS"] = "1"
+            self.run_test(exe_name, options=opts, expected="Passed  53 / 53")
