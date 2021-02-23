3da7f7eea6d3b6ee41b88bcb212de55f82fc9e23
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-02-23 21:33:52 +0000

    amr-wind: simplify cuda_arch mapping

--- var/spack/repos/builtin/packages/amr-wind/package.py
+++ var/spack/repos/builtin/packages/amr-wind/package.py
@@ -1,6 +1,11 @@
-import os
-import itertools
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 from spack import *
+import itertools
+
 
 def process_amrex_constraints():
     """Map constraints when building with external AMReX"""
@@ -10,12 +15,14 @@ def process_amrex_constraints():
     for k in itertools.product(*a3):
         if '+cuda' in k:
             for arch in CudaPackage.cuda_arch_values:
-                yield ''.join(k) + " cuda_arch=%s"%arch
+                yield ''.join(k) + " cuda_arch=%s" % arch
         else:
             yield ''.join(k)
 
+
 class AmrWind(CMakePackage, CudaPackage):
-    """AMR-Wind is a massively parallel, block-structured adaptive-mesh, incompressible flow sover for wind turbine and wind farm simulations. """
+    """AMR-Wind is a massively parallel, block-structured adaptive-mesh,
+    incompressible flow sover for wind turbine and wind farm simulations. """
 
     homepage = "https://github.com/Exawind/amr-wind"
     git      = "https://github.com/exawind/amr-wind.git"
@@ -52,9 +59,12 @@ class AmrWind(CMakePackage, CudaPackage):
     conflicts('+openmp', when='+cuda')
 
     depends_on('mpi', when='+mpi')
-    
+
     for opt in process_amrex_constraints():
-        depends_on('amrex'+opt+'@20.12:', when='~internal-amrex'+opt)
+        dopt = '+particles' + opt
+        if '+hypre' in dopt:
+            dopt = "+fortran" + dopt
+        depends_on('amrex@develop' + dopt, when='~internal-amrex' + opt)
 
     depends_on('hypre+mpi+int64~cuda@2.20.0:', when='+mpi~cuda+hypre')
     depends_on('hypre~mpi+int64~cuda@2.20.0:', when='~mpi~cuda+hypre')
@@ -67,30 +77,11 @@ class AmrWind(CMakePackage, CudaPackage):
     depends_on('masa', when='+masa')
     depends_on('openfast+cxx', when='+openfast')
 
-    def process_cuda_args(self):
-        """Process CUDA arch spec and convert it to AMReX format"""
-        amrex_arch_map = {'20': '2.0', '21': '2.1', '30': '3.0', '32': '3.2',
-                          '35': '3.5', '37': '3.7', '50': '5.0', '52': '5.2',
-                          '53': '5.3', '60': '6.0', '61': '6.1', '62': '6.2',
-                          '70': '7.0', '72': '7.2', '75': '7.5', '80': '8.0',
-                          '86': '8.6'}
-
-        cuda_arch = self.spec.variants['cuda_arch'].value
-        try:
-            amrex_arch = []
-            for vv in cuda_arch:
-                if vv in amrex_arch_map:
-                    amrex_arch.append(amrex_arch_map[vv])
-            return amrex_arch
-        except:
-            return []
-
     def cmake_args(self):
         define = CMakePackage.define
-        spec = self.spec
 
-        vs = ["mpi", "cuda", "openmp", "netcdf", "hypre", "masa", 
-                "openfast", "tests", "fortran"]
+        vs = ["mpi", "cuda", "openmp", "netcdf", "hypre", "masa",
+              "openfast", "tests", "fortran"]
         args = [
             self.define_from_variant("AMR_WIND_ENABLE_%s" % v.upper(), v)
             for v in vs
@@ -104,9 +95,10 @@ class AmrWind(CMakePackage, CudaPackage):
         ]
 
         if '+cuda' in self.spec:
-            amrex_arch = self.process_cuda_args()
+            amrex_arch = ';'.join('{0:.1f}'.format(float(i) / 10.0)
+                                  for i in self.spec.variants['cuda_arch'].value)
             if amrex_arch:
-                args.append(define('AMReX_CUDA_ARCH', ';'.join(amrex_arch)))
+                args.append(define('AMReX_CUDA_ARCH', amrex_arch))
 
         if '+internal-amrex' in self.spec:
             args.append(self.define('AMR_WIND_USE_INTERNAL_AMREX', True))
