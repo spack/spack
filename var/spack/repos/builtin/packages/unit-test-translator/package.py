9a8bc1b1f328375fd7f906e0a9df526a3a374353
Matthias Wolf <m+git@sushinara.net>
2021-02-24 16:07:56 +0100

    unit-test-translator: add package and module (#1061)

---
+++ var/spack/repos/builtin/packages/unit-test-translator/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class UnitTestTranslator(PythonPackage):
+    """Transforms the XML unit test output from CMake to proper JUnit XML.
+    """
+
+    homepage = "https://bbpgitlab.epfl.ch/hpc/unit-test-translator"
+    git      = "git@bbpgitlab.epfl.ch:hpc/unit-test-translator.git"
+
+    version('develop')
+    version('0.0.1', tag='v0.0.1')
+
+    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('py-setuptools-scm', type=('build', 'run'))
+    depends_on('py-lxml', type=('build', 'run'))
