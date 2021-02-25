18e7ff7ce8aaeda263c462efc5b30b9d7a7a261a
dorton21 <odesmond21@gmail.com>
2021-02-25 12:35:54 -0600

    Almost forgot list_url

--- var/spack/repos/builtin/packages/r-circstats/package.py
+++ var/spack/repos/builtin/packages/r-circstats/package.py
@@ -14,8 +14,9 @@ class RCircstats(RPackage):
 
     homepage = "https://www.example.com"
     url      = "https://cloud.r-project.org/src/contrib/CircStats_0.2-6.tar.gz"
+    list_url = "https://cloud.r-project.org/src/contrib/Archive/CircStats"
 
-    maintainers = ['github_user1', 'github_user2']
+    maintainers = ['dorton21']
 
     version('0.2-6', sha256='8efed93b75b314577341effea214e3dd6e0a515cfe1212eb051047a1f3276f1d')
 
