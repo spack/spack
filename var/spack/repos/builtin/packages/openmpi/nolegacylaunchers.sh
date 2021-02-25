f1a58c2fc6eb2006f5c608aa14d71e9771f0e367
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2021-02-25 15:32:32 +0100

    openmpi: more straightforward handling for '~legacylaunchers'

--- var/spack/repos/builtin/packages/openmpi/nolegacylaunchers.sh
+++ var/spack/repos/builtin/packages/openmpi/nolegacylaunchers.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-echo "This version of Spack (openmpi ~legacylaunchers schedulers=slurm) "
-echo "is installed without the mpiexec/mpirun commands to prevent "
-echo "unintended performance issues. See https://github.com/spack/spack/pull/10340 "
-echo "for more details."
-echo "If you understand the potential consequences of a misconfigured mpirun, you can"
-echo "use spack to install 'openmpi+legacylaunchers' to restore the executables."
-echo "Otherwise, use srun to launch your MPI executables."
+cat <<_EOF >&2
+The users are strongly discouraged from using
+'$0'.
+
+Please, refer to your site's user guide for the recommended way to launch MPI
+applications.
+
+_EOF
 exit 2
