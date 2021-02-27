ede100bf168c834db4a4caee4a3adbcee6d7ddd3
Adam J. Stewart <ajstewart426@gmail.com>
2021-02-26 19:25:59 -0600

    kcov: elfutils not needed on macOS (#21993)

--- var/spack/repos/builtin/packages/kcov/package.py
+++ var/spack/repos/builtin/packages/kcov/package.py
@@ -19,8 +19,8 @@ class Kcov(CMakePackage):
     depends_on('cmake@2.8.4:', type='build')
     depends_on('zlib')
     depends_on('curl')
-    depends_on('elfutils')
-    depends_on('binutils +libiberty', type='link')
+    depends_on('elfutils', when='platform=linux')
+    depends_on('binutils +libiberty', when='platform=linux', type='link')
 
     def cmake_args(self):
         # Necessary at least on macOS, fixes linking error to LLDB
