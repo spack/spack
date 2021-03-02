e6efa1fbc6bd4443ba3d2573c79dd8fdacb62c11
Haruki Denpo <haruki.denpo@riken.jp>
2021-03-02 10:49:05 +0900

    py-azureml-dataprep-rslex: install only x86_64

--- var/spack/repos/builtin/packages/py-azureml-dataprep-rslex/package.py
+++ var/spack/repos/builtin/packages/py-azureml-dataprep-rslex/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import sys
+import archspec
 
 from spack import *
 
@@ -13,6 +14,10 @@ class PyAzuremlDataprepRslex(Package):
     capabilities to load, transform, and write data for machine learning workflows."""
 
     homepage = "http://aka.ms/data-prep-sdk"
+
+    for t in set([str(x.family) for x in archspec.cpu.TARGETS.values()
+                 if str(x.family) != 'x86_64']):
+        conflicts('target={0}:'.format(t), msg='py-azureml-dataprep-rslex is available x86_64 only')
 
     if sys.platform == 'darwin':
         version('1.9.0-py3.9', sha256='9bdaa31d129dac19ee20d5a3aad1726397e90d8d741b4f6de4554040800fefe8', expand=False,
