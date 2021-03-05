e9be420a8d515ae1dfbddd37681285dc1264184c
Valentin Volkl <valentin.volkl@cern.ch>
2021-03-05 10:37:33 +0100

    [py-awkward1] add dlpack dependency

--- var/spack/repos/builtin/packages/py-awkward1/package.py
+++ var/spack/repos/builtin/packages/py-awkward1/package.py
@@ -25,5 +25,6 @@ class PyAwkward1(PythonPackage):
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-numpy@1.13.1:')
     depends_on('py-pybind11')
+    depends_on('dlpack', when="@1.0.0:")
     depends_on('rapidjson')
     depends_on('cmake', type='build')
