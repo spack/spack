ea11f9241db2203aef24a6050ddeb1ed106c3520
vsoch <vsoch@users.noreply.github.com>
2021-03-06 16:27:58 -0700

    updating command completion
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -333,7 +333,7 @@ _spack() {
     then
         SPACK_COMPREPLY="-h --help -H --all-help --color -C --config-scope -d --debug --timestamp --pdb -e --env -D --env-dir -E --no-env --use-env-repo -k --insecure -l --enable-locks -L --disable-locks -m --mock -p --profile --sorted-profile --lines -v --verbose --stacktrace -V --version --print-shell-vars"
     else
-        SPACK_COMPREPLY="activate add arch blame build-env buildcache cd checksum ci clean clone commands compiler compilers concretize config containerize create deactivate debug dependencies dependents deprecate dev-build develop docs edit env extensions external fetch find flake8 gc gpg graph help info install license list load location log-parse maintainers mark mirror module patch pkg providers pydoc python reindex remove rm repo resource restage solve spec stage style test test-env tutorial undevelop uninstall unit-test unload url verify versions view"
+        SPACK_COMPREPLY="activate add analyze arch blame build-env buildcache cd checksum ci clean clone commands compiler compilers concretize config containerize create deactivate debug dependencies dependents deprecate dev-build develop docs edit env extensions external fetch find flake8 gc gpg graph help info install license list load location log-parse maintainers mark mirror module patch pkg providers pydoc python reindex remove rm repo resource restage solve spec stage style test test-env tutorial undevelop uninstall unit-test unload url verify versions view"
     fi
 }
 
@@ -350,6 +350,15 @@ _spack_add() {
     if $list_options
     then
         SPACK_COMPREPLY="-h --help -l --list-name"
+    else
+        _all_packages
+    fi
+}
+
+_spack_analyze() {
+    if $list_options
+    then
+        SPACK_COMPREPLY="-h --help --overwrite --monitor --monitor-no-auth --monitor-keep-going --monitor-host --monitor-prefix -o --outdir --list-analyzers -a --analyzers"
     else
         _all_packages
     fi
