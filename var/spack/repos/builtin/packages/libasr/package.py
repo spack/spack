ba7e4eff06f9a57ed5e240b2fa368587c1011242
darmac <xiaojun2@hisilicon.com>
2021-03-06 16:18:31 +0800

    libasr: fix package style error

--- var/spack/repos/builtin/packages/libasr/package.py
+++ var/spack/repos/builtin/packages/libasr/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack import *
+
 
 class Libasr(AutotoolsPackage):
     """libasr is a free, simple and portable asynchronous resolver library."""
