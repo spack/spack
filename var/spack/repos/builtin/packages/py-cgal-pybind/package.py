a2f5af9383c632b8857e5a43bcc9aa63672cd365
lguyot <luc.guyot@epfl.ch>
2021-03-08 11:31:24 +0100

    A package for bbpcode.epfl.ch/common/cgal-pybind [NSETM-1035]
    This change adds var/spack/repos/builtin/packages/py-cgal-pybind.
    
    The file py-cgal-pybind/package.py has been extracted from the branch
    https://github.com/BlueBrain/spack/tree/cgal-pybind.
    
    This change is to be merged into spack/tree/develop so that
    cgal-pybind can be used directly (without branch switch) by
    atlas-building-tools.

--- var/spack/repos/builtin/packages/py-cgal-pybind/package.py
+++ var/spack/repos/builtin/packages/py-cgal-pybind/package.py
@@ -14,21 +14,5 @@ class PyCgalPybind(PythonPackage):
     depends_on('cgal')
     depends_on('eigen')
     depends_on('py-pybind11')
-
     depends_on('py-numpy@1.12:', type=('build', 'run'))
 
-    #def patch(self):
-    #    """Prevent `-isystem /usr/include` from appearing, since this confuses gcc.
-    #    """
-    #    if self.spec.satisfies('@gap-junctions'):
-    #        return
-    #    filter_file(r'(include_directories\()SYSTEM ',
-    #                r'\1',
-    #                'functionalizer/CMakeLists.txt')
-
-    #def cmake_args(self):
-    #    args = [
-    #        '-DCMAKE_C_COMPILER={}'.format(self.spec['mpi'].mpicc),
-    #        '-DCMAKE_CXX_COMPILER={}'.format(self.spec['mpi'].mpicxx)
-    #    ]
-    #    return args
