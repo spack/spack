643bfbda29df794d9df10db0c51d061bb973a744
Jen Herting <jen@herting.cc>
2021-03-08 16:59:57 -0500

    [blender] working on 2.92.0

--- var/spack/repos/builtin/packages/blender/package.py
+++ var/spack/repos/builtin/packages/blender/package.py
@@ -15,6 +15,9 @@ class Blender(CMakePackage):
     homepage = "https://www.blender.org/"
     url      = "http://download.blender.org/source/blender-2.79b.tar.gz"
 
+    version('2.92.0',
+            url='https://download.blender.org/source/blender-2.92.0.tar.xz',
+            sha256='e791cfc403292383577c3c8ce2cd34e5aa2cd8da0a7483041049a1609ddb4595')
     version('2.80', sha256='cd9d7e505c1f6e63a4f72366ed04d446859977eeb34cde21283aaea6a304a5c0')
     version('2.79b', sha256='4c944c304a49e68ac687ea06f5758204def049b66dc211e1cffa1857716393bc')
     version('2.79a', sha256='8ccf5f624ef020c903e0a302bfc6cd367eccb778e36e112c74c0a5e9762d84d7')
@@ -39,31 +42,88 @@ class Blender(CMakePackage):
 
     depends_on('python@3.5:', when="@:2.79b")
     depends_on('python@3.7:', when="@2.80:")
+    depends_on('python@3.7.7:', when="@2.92.0:")
+
     depends_on('py-numpy', when="@2.80:")
+    depends_on('py-numpy@1.17.5:', when='@2.92.0:')
+    
     depends_on('glew')
+    depends_on('glew@1.13.0:', when='@2.92.0:')
+
     depends_on('opengl')
     # depends_on('openglu')
+    
     depends_on('libpng')
+    depends_on('libpng@1.6.37:', when='@2.92.0:')
+
     depends_on('libjpeg')
+    depends_on('libjpeg@2.0.4:', when='@2.92.0:')
+
     depends_on('openjpeg')
-    depends_on('boost@1.49:1.69')
+    depends_on('openjpeg@2.3.1:', when='@2.92.0:')
+
+#     depends_on('boost@1.49:1.69')
+    depends_on('boost@1.70.0:', when='@2.92.0:')
+
     depends_on('openimageio', when='+cycles')
+    depends_on('openimageio@2.1.15.0:', when='@2.92.0: +cycles')
+
     # Upper bound per: https://developer.blender.org/T54779
     depends_on('ffmpeg@3.2.1:3.999', when='@:2.79b+ffmpeg')
     depends_on('ffmpeg@3.2.1:', when='@2.80:+ffmpeg')
+    depends_on('ffmpeg@4.2.3:', when='@2.92.0:+ffmpeg')
+
 #    depends_on('opencolorio@1.0:', when='+ocio')
+
     depends_on('llvm@3.0:', when='+llvm')
+    depends_on('llvm@9.0.1:', when='@2.92.0:+llvm')
     # depends_on('openshadinglanguage')
     # depends_on('openvdb@3.1:')
     depends_on('freetype')
+    depends_on('freetype@2.10.2:', when='@2.92.0:')
+
     depends_on('libuuid')
     depends_on('jemalloc', when='+jemalloc')
     depends_on('ilmbase')
+
     depends_on('opensubdiv+openmp', when='+opensubdiv')
+    depends_on('opensubdiv@3.4.3:', when='@2.92.0:+opensubdiv')
+    
     depends_on('cuda@10.1.0:10.1.999', when='+cycles', type=('link','run'))
     # FIXME: The version of GCC should probably be the version of GCC that is actually
     # compiling blender, not hardcoding the version that the package creater is using. 
-    depends_on('gcc@7.4.0', when='+cycles', type=('run'))
+#     depends_on('gcc@7.4.0', when='+cycles', type=('run'))
+
+
+    # Dependencies for 2.92.0
+#     depends_on('zlib@1.2.11::', when='@2.92.0:')
+#     depends_on('openal@1.20.1:', when='@2.92.0:')
+#     depends_on('blosc@1.5.0:', when='@2.92.0:')
+#     depends_on('pthreads@3.0.0:', when='@2.92.0:')
+#     depends_on('openexr@2.4.0:', when='@2.92.0:')
+#     depends_on('freeglut@3.0.0:', when='@2.92.0:')
+#     depends_on('alembic@1.7.12:', when='@2.92.0:')
+#     depends_on('glfw@3.1.2:', when='@2.92.0:')
+#     depends_on('sdl@2.0.12:', when='@2.92.0:')
+#     depends_on('opencollada@1.6.68:', when='@2.92.0:')
+#     depends_on('opencolorio@1.1.1:', when='@2.92.0:')
+#     depends_on('tiff@4.1.0:', when='@2.92.0:')
+#     depends_on('openshadinglanguage@1.10.10:', when='@2.92.0:')
+#     depends_on('tbb@2019_u9:', when='@2.92.0:')
+#     depends_on('openvdb@7.0.0:', when='@2.92.0:')
+#     depends_on('idna@2.9:', when='@2.92.0:')
+#     depends_on('lame@3.100:', when='@2.92.0:')
+#     depends_on('ogg@1.3.4:', when='@2.92.0:')
+#     depends_on('vorbis@1.3.6:', when='@2.92.0:')
+#     depends_on('theora@1.1.1:', when='@2.92.0:')
+#     depends_on('flac@1.3.3:', when='@2.92.0:')
+#     depends_on('vpx@1.8.2:', when='@2.92.0:')
+#     depends_on('opus@1.3.1:', when='@2.92.0:')
+#     depends_on('xvidcore@1.3.7:', when='@2.92.0:')
+#     depends_on('fftw@3.3.8:', when='@2.92.0:')
+#     depends_on('iconv@1.16:', when='@2.92.0:')
+#     depends_on('sndfile@1.0.28:', when='@2.92.0:')
+    # sndfile
 
     def cmake_args(self):
         spec = self.spec
