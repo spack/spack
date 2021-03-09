6547a00d7cebdbc51ce68527df331e7e3b54001d
Jen Herting <jen@herting.cc>
2021-03-09 16:48:29 -0500

    [alembic] added homepage and description. removed some fixmes

--- var/spack/repos/builtin/packages/alembic/package.py
+++ var/spack/repos/builtin/packages/alembic/package.py
@@ -3,36 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install alembic
-#
-# You can edit this file again by typing:
-#
-#     spack edit alembic
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Alembic(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """Alembic is an open computer graphics interchange
+    framework. Alembic distills complex, animated scenes into a
+    non-procedural, application-independent set of baked
+    geometric results."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://www.alembic.io"
     url      = "https://github.com/alembic/alembic/archive/1.7.16.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.7.16', sha256='2529586c89459af34d27a36ab114ad1d43dafd44061e65cfcfc73b7457379e7c')
 
