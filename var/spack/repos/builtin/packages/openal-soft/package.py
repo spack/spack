650ed4ed9346b2103b88cb5c582fc16f563511f8
Jen Herting <jen@herting.cc>
2021-03-12 12:17:00 -0500

    [openal-soft] added homepage and description. removed fixmes

--- var/spack/repos/builtin/packages/openal-soft/package.py
+++ var/spack/repos/builtin/packages/openal-soft/package.py
@@ -3,49 +3,25 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install openal-soft
-#
-# You can edit this file again by typing:
-#
-#     spack edit openal-soft
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class OpenalSoft(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+    """OpenAL provides capabilities for playing audio in a
+    virtual 3D environment. Distance attenuation, doppler
+    shift, and directional sound emitters are among the
+    features handled by the API."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://openal-soft.org"
     url      = "https://openal-soft.org/openal-releases/openal-soft-1.21.1.tar.bz2"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.21.1', sha256='c8ad767e9a3230df66756a21cc8ebf218a9d47288f2514014832204e666af5d8')
 
     variant('alsa', default=False, description="ALSA support")
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('alsa-lib', when="+alsa")
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = []
 
         if self.spec.satisfies('+alsa'):
