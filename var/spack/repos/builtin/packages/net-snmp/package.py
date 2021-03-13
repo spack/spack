23d8b63a55b811b2f9c47cf483c3a6abc4f6256d
darmac <xiaojun2@hisilicon.com>
2021-03-13 09:42:44 +0800

    net-snmp: refine url, homepage and description

--- var/spack/repos/builtin/packages/net-snmp/package.py
+++ var/spack/repos/builtin/packages/net-snmp/package.py
@@ -7,10 +7,10 @@ from spack import *
 
 
 class NetSnmp(AutotoolsPackage):
-    """SNMP Daemon."""
+    """A SNMP application library, tools and daemon."""
 
-    homepage = "http://net-snmp.sourceforge.net"
-    url      = "https://udomain.dl.sourceforge.net/project/net-snmp/net-snmp/5.9/net-snmp-5.9.tar.gz"
+    homepage = "http://www.net-snmp.org/"
+    url      = "https://sourceforge.net/projects/net-snmp/files/net-snmp/5.9/net-snmp-5.9.tar.gz"
 
     version('5.9', sha256='04303a66f85d6d8b16d3cc53bde50428877c82ab524e17591dfceaeb94df6071')
 
