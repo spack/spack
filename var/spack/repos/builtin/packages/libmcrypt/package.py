1636ca6c5358d157fe44b2c5ba038c022d8a7ae3
darmac <xiaojun2@hisilicon.com>
2021-03-13 10:14:21 +0800

    libmcrypt: fix CI error

--- var/spack/repos/builtin/packages/libmcrypt/package.py
+++ var/spack/repos/builtin/packages/libmcrypt/package.py
@@ -2,10 +2,6 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
 
