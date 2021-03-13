2c9da0fed7573bf4ca44467c711201f3a920f619
darmac <xiaojun2@hisilicon.com>
2021-03-13 09:27:25 +0800

    Update var/spack/repos/builtin/packages/indent/package.py
    Co-authored-by: Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>

--- var/spack/repos/builtin/packages/indent/package.py
+++ var/spack/repos/builtin/packages/indent/package.py
@@ -9,7 +9,7 @@ from spack import *
 class Indent(AutotoolsPackage):
     """A tool to make code easier to read."""
 
-    homepage = "http://www.gnu.org/software/indent"
+    homepage = "https://www.gnu.org/software/indent"
     url      = "https://ftp.gnu.org/gnu/indent/indent-2.2.11.tar.gz"
 
     version('2.2.12', sha256='e77d68c0211515459b8812118d606812e300097cfac0b4e9fb3472664263bb8b')
