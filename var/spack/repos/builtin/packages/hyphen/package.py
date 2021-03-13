b6b054a9d7324d68a141671798e4e177c4a07b29
darmac <xiaojun2@hisilicon.com>
2021-03-13 09:32:26 +0800

    hyphen: refine url

--- var/spack/repos/builtin/packages/hyphen/package.py
+++ var/spack/repos/builtin/packages/hyphen/package.py
@@ -9,7 +9,7 @@ from spack import *
 class Hyphen(AutotoolsPackage):
     """A library of text hyphenation."""
 
-    homepage = "http://hunspell.github.io"
-    url      = "http://downloads.sourceforge.net/hunspell/hyphen-2.8.8.tar.gz"
+    homepage = "https://hunspell.github.io"
+    url      = "https://downloads.sourceforge.net/hunspell/hyphen-2.8.8.tar.gz"
 
     version('2.8.8', sha256='304636d4eccd81a14b6914d07b84c79ebb815288c76fe027b9ebff6ff24d5705')
