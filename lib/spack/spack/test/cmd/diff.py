077a1ed408cde65d7d7489e7a3eb775f7092f8d4
vsoch <vsoch@users.noreply.github.com>
2021-03-14 11:23:14 -0600

    adding tests for diff that use command directly to test functions for json, first, bold
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- lib/spack/spack/test/cmd/diff.py
+++ lib/spack/spack/test/cmd/diff.py
@@ -3,9 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import pytest
+
 import spack.config
 import spack.store
 import spack.cmd.diff
+import spack.main
+import spack.util.spack_json as sjson
+
+install = spack.main.SpackCommand('install')
+diff = spack.main.SpackCommand('diff')
 
 
 def test_diff(install_mockery, mock_fetch, mock_archive, mock_packages):
@@ -25,3 +32,41 @@ def test_diff(install_mockery, mock_fetch, mock_archive, mock_packages):
     assert len(c['b_not_a']) == 1
     assert c['a_not_b'][0] == ['variant_set', 'mpileaks debug bool(False)']
     assert c['b_not_a'][0] == ['variant_set', 'mpileaks debug bool(True)']
+
+
+def test_load_first(install_mockery, mock_fetch, mock_archive, mock_packages):
+    """Test with and without the --first option"""
+    install('mpileaks')
+
+    # Only one version of mpileaks will work
+    diff('mpileaks', 'mpileaks')
+
+    # Ensure they are the same
+    assert "No differences" in diff('mpileaks', 'mpileaks')
+    output = diff('--json', 'mpileaks', 'mpileaks')
+    result = sjson.load(output)
+
+    assert len(result['a_not_b']) == 0
+    assert len(result['b_not_a']) == 0
+    assert result['a_name'] == 'mpileaks'
+    assert result['b_name'] == 'mpileaks'
+    assert "intersect" in result and len(result['intersect']) > 50
+
+    # After we install another version, it should ask us to disambiguate
+    install('mpileaks+debug')
+
+    # There are two versions of mpileaks
+    with pytest.raises(spack.main.SpackCommandError):
+        diff('mpileaks', 'mpileaks+debug')
+
+    # But if we tell it to use the first, it won't try to disambiguate
+    assert "VARIANT_SET" in diff('--first', 'mpileaks', 'mpileaks+debug')
+
+    # This matches them exactly
+    output = diff("--json", "mpileaks@2.3/ysubb76", "mpileaks@2.3/ft5qff3")
+    result = sjson.load(output)
+
+    assert len(result['a_not_b']) == 1
+    assert len(result['b_not_a']) == 1
+    assert result['a_not_b'][0] == ['variant_set', 'mpileaks debug bool(False)']
+    assert result['b_not_a'][0] == ['variant_set', 'mpileaks debug bool(True)']
