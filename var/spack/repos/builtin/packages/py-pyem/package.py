e7e4b8578da7fbc2045f626fce104ff9ee97f29f
dorton21 <odesmond21@gmail.com>
2021-03-15 13:58:27 -0500

    Changed deps

--- var/spack/repos/builtin/packages/py-pyem/package.py
+++ var/spack/repos/builtin/packages/py-pyem/package.py
@@ -20,14 +20,8 @@ class PyPyem(PythonPackage):
     version('2.1.0', sha256='5234a20427ab2813a8a0bf1e9112d4d854b1b0502b3e63d17c1b1a3c4be9340e')
 
     depends_on('python@3.7:', type=('build', 'run'))
-    depends_on('py-virtualenv', type='build')
     depends_on('py-pip', type='build')
-    depends_on('py-pytest', type=('build', 'run'))
-    depends_on('py-mypy', type=('build', 'run'))
-    depends_on('py-black', type=('build', 'run'))
-    depends_on('flit', type=('build', 'run'))
 
     def install(self, spec, prefix):
         pip = which('pip')
         pip('install', self.stage.archive_file, '--prefix={0}'.format(prefix))
-
