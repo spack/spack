b7780101b85a3e7314efc223ee50a8d7339a0cac
vsoch <vsoch@users.noreply.github.com>
2021-03-16 13:55:13 -0600

    start of work to add rpm package.
    The original goal was to generate the debugedit tool, but further
    investigation reveals that the tool is being removed from this package
    (and does not even have required symbols in the newest releases to build)
    so we will not do it here. For rpm to build with lua, we additionally
    need to add a lua.pc file (added as a patch) that the configure script
    can find.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- var/spack/repos/builtin/packages/lua/package.py
+++ var/spack/repos/builtin/packages/lua/package.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from llnl.util.filesystem import join_path
 from spack import *
 import os
 
@@ -27,6 +28,7 @@ class Lua(Package):
     version('5.1.4', sha256='b038e225eaf2a5b57c9bcc35cd13aa8c6c8288ef493d52970c9545074098af3a')
     version('5.1.3', sha256='6b5df2edaa5e02bf1a2d85e1442b2e329493b30b0c0780f77199d24f087d296d')
 
+    variant("pc-file", default=False, description="Add patch for lua.pc generation")
     variant('shared', default=True,
             description='Builds a shared version of the library')
 
@@ -36,6 +38,9 @@ class Lua(Package):
     depends_on('readline')
     # luarocks needs unzip for some packages (e.g. lua-luaposix)
     depends_on('unzip', type='run')
+
+    # By default, we are missing lua.pc
+    patch('add-lua-pc-file.patch', when="+pc-file")
 
     resource(
         name="luarocks",
@@ -165,6 +170,12 @@ class Lua(Package):
                 'LUA_CPATH',
                 os.path.join(self.spec.prefix, self.lua_lib_dir, '?.so'),
                 separator=';')
+
+    @run_after('install')
+    def link_csh(self):
+        if "+pc-file" in self.spec:
+            symlink(join_path(self.prefix.lib, 'pkgconfig', 'lua-5.3.pc'),
+                    join_path(self.prefix.lib, 'pkgconfig', 'lua.pc'))
 
     @property
     def lua_lib_dir(self):
