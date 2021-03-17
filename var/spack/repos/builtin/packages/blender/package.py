84664a6b04da5f5fd9e67a455f64557f251e4d9c
Jen Herting <jen@herting.cc>
2021-03-17 11:07:54 -0400

    [blender] added comment for location of dependency versions

--- var/spack/repos/builtin/packages/blender/package.py
+++ var/spack/repos/builtin/packages/blender/package.py
@@ -41,6 +41,7 @@ class Blender(CMakePackage):
     variant('opensubdiv', default=False, description='Build with opensubdiv support')
     variant('jemalloc', default=True)
 
+    # https://developer.blender.org/diffusion/B/browse/blender-v2.92-release/build_files/build_environment/cmake/versions.cmake
     depends_on('python@3.5:', when="@:2.79b")
     depends_on('python@3.7:', when="@2.80:")
     depends_on('python@3.7.7:', when="@2.92.0:")
