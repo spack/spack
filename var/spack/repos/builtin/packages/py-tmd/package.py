a530207802be7b5f0b6aaf17e94a36959515db81
arnaudon <alexis.arnaudon@epfl.ch>
2021-03-17 11:57:41 +0100

    tns/tms/diameter-synthesis

--- var/spack/repos/builtin/packages/py-tmd/package.py
+++ var/spack/repos/builtin/packages/py-tmd/package.py
@@ -12,6 +12,7 @@ class PyTmd(PythonPackage):
     homepage = "https://github.com/BlueBrain/TMD"
     url = "https://pypi.io/packages/source/t/tmd/tmd-2.0.9.tar.gz"
 
+    version('2.0.10', sha256='256992d9a47587876ae76d29f1f9350e837c70ec71e62336d67f9e8e4cf96aab')
     version('2.0.9', sha256='9bfb4b014e2c735c9db5f3ee61e4ccdbac29248f608ba49a43d862e4835b88a5')
     version('2.0.8', sha256='56373aa32f2cc201b083760cf5110749f7d6fd488603a5809e6af8c312c1a77e')
     version('2.0.4', sha256='b1709f36964d11dd555a35f99dfa26187ce2ef98a56f07eaa4805da944e86652')
