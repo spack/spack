ed53d86ecb47672413a1b958460da0415d2d4a6c
Jen Herting <jen@herting.cc>
2021-03-18 16:50:35 -0400

    [ghostscript] applying patch for newer versions of freetype
    Source: https://git.ghostscript.com/?p=ghostpdl.git;a=commitdiff;h=41ef9a0bc36b

--- var/spack/repos/builtin/packages/ghostscript/package.py
+++ var/spack/repos/builtin/packages/ghostscript/package.py
@@ -36,6 +36,7 @@ class Ghostscript(AutotoolsPackage):
     depends_on('gtkplus')
 
     patch('nogoto.patch', when='%fj@:4.1.0')
+    patch('freetype-2.10.4.patch', when='^freetype@2.10.4:')
 
     def url_for_version(self, version):
         baseurl = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs{0}/ghostscript-{1}.tar.gz"
