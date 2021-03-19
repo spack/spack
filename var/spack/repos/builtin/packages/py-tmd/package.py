5d8cde664a69b0651474cba30553c31ee6da519d
Alexis Arnaudon <alexis.arnaudon@gmail.com>
2021-03-19 07:29:03 +0100

    tns/tmd/diameter-synthesis update (#1094)

--- var/spack/repos/builtin/packages/py-tmd/package.py
+++ var/spack/repos/builtin/packages/py-tmd/package.py
@@ -12,7 +12,7 @@ class PyTmd(PythonPackage):
     homepage = "https://github.com/BlueBrain/TMD"
     url = "https://pypi.io/packages/source/t/tmd/tmd-2.0.9.tar.gz"
 
-    version('2.0.10', sha256='256992d9a47587876ae76d29f1f9350e837c70ec71e62336d67f9e8e4cf96aab')
+    version('2.0.11', sha256='ba19167d19eb2132af766b760a6f09f423abbdcf871c23c2bfdf8c9064ba888e')
     version('2.0.9', sha256='9bfb4b014e2c735c9db5f3ee61e4ccdbac29248f608ba49a43d862e4835b88a5')
     version('2.0.8', sha256='56373aa32f2cc201b083760cf5110749f7d6fd488603a5809e6af8c312c1a77e')
     version('2.0.4', sha256='b1709f36964d11dd555a35f99dfa26187ce2ef98a56f07eaa4805da944e86652')
@@ -20,6 +20,7 @@ class PyTmd(PythonPackage):
 
     depends_on('py-setuptools', type='build')
 
+    depends_on('py-morphio@2.7.1:', type='run')
     depends_on('py-enum34@1.0:', type='run', when='^python@:3.3.99')
     depends_on('py-h5py@2.8:', type='run')
     depends_on('py-munkres@1.0.12:', type='run')
