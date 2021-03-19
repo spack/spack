8856e5732ec62138b72d0a94b2ba6f4d5b3cdc51
lou.lawrence@kitware.com <lou.lawrence@kitware.com>
2021-03-19 11:06:37 -0400

    spack_cmd.bat: look for external python first

--- lib/spack/spack/cmd/installer/spack_cmd.bat
+++ lib/spack/spack/cmd/installer/spack_cmd.bat
@@ -1,11 +1,21 @@
 @ECHO OFF
+setlocal EnableDelayedExpansion
 :: (c) 2021 Lawrence Livermore National Laboratory
 pushd %~dp0
 set spackinstdir=%CD%
 popd
 
-for /f "tokens=*" %%g in ('dir /b /a:d "%spackinstdir%\Python*"') do (set python_pf_ver=%%g)
-set "py_path=%spackinstdir%\%python_pf_ver%"
+(for /f "delims=" %%F in ('where python.exe') do (set python_pf_ver=%%F) ) 2> NUL
+if not defined python_pf_ver (
+	for /f "tokens=*" %%g in ('dir /b /a:d "!spackinstdir!\Python*"') do (set python_ver=%%g)
+	set "py_path=!spackinstdir!\!python_ver!"
+	if not defined python_ver (
+		echo "Python was not found on your system."
+		echo "Please install Python or add Python to your PATH."
+		)
+	) else (
+		set "py_path=!python_pf_ver!:python.exe="
+	)
 for /f "tokens=*" %%g in ('dir /b /a:d "%spackinstdir%\spack*"') do (set spack_ver=%%g)
 set "SPACK_ROOT=%spackinstdir%\%spack_ver%"
 
