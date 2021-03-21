7a0d88404984ad44b50669783b3fe0607e0aa8f1
Thomas Zeiser <tz-rrze@users.noreply.github.com>
2021-03-21 12:04:30 +0100

    lammps: add most recent stable and patch release
    + lammps Patch release 10 March 2021
    + lammps Stable release 29 October 2020

--- var/spack/repos/builtin/packages/lammps/package.py
+++ var/spack/repos/builtin/packages/lammps/package.py
@@ -21,6 +21,8 @@ class Lammps(CMakePackage, CudaPackage):
     tags = ['ecp', 'ecp-apps']
 
     version('master', branch='master')
+    version('20210310', sha256='25708378dbeccf794bc5045aceb84380bf4a3ca03fc8e5d150a26ca88d371474')
+    version('20201029', sha256='759705e16c1fedd6aa6e07d028cc0c78d73c76b76736668420946a74050c3726')
     version('20200721', sha256='845bfeddb7b667799a1a5dbc166b397d714c3d2720316604a979d3465b4190a9')
     version('20200630', sha256='413cbfabcc1541a339c7a4ab5693fbeb768f46bb1250640ba94686c6e90922fc')
     version('20200505', sha256='c49d77fd602d28ebd8cf10f7359b9fc4d14668c72039028ed7792453d416de73')
