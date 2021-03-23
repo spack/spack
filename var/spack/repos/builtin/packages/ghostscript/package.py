d97ac71e3ad5fd92e4bff5317b9895990b556e4d
Jen Herting <jen@herting.cc>
2021-03-23 11:15:23 -0400

    [ghostscript] reworking the patch for url

--- var/spack/repos/builtin/packages/ghostscript/package.py
+++ var/spack/repos/builtin/packages/ghostscript/package.py
@@ -36,7 +36,9 @@ class Ghostscript(AutotoolsPackage):
     depends_on('gtkplus')
 
     patch('nogoto.patch', when='%fj@:4.1.0')
-    patch('freetype-2.10.4.patch', when='^freetype@2.10.4:')
+
+    # Patch source: https://git.ghostscript.com/?p=ghostpdl.git;a=commitdiff;h=41ef9a0bc36b
+    patch("https://git.ghostscript.com/?p=ghostpdl.git;a=commitdiff_plain;h=41ef9a0bc36b", when='^freetype@2.10.4:', sha256="254532a8561afddb5b39c1c30526676559ed9f0b87c1e9985eb797a59c98a3dd")
 
     def url_for_version(self, version):
         baseurl = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs{0}/ghostscript-{1}.tar.gz"
