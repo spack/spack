b15a9da4196bd5894cd1ab94750a28bc9d69d817
Peter Josef Scheibel <scheibel1@llnl.gov>
2021-03-22 18:30:42 -0700

    some mirror tests need 'mock_packages' (but not 'mutable_mock_repo')

--- lib/spack/spack/test/mirror.py
+++ lib/spack/spack/test/mirror.py
@@ -16,7 +16,7 @@ from spack.util.executable import which
 
 from llnl.util.filesystem import resolve_link_target_relative_to_the_link
 
-pytestmark = pytest.mark.usefixtures('mutable_config')
+pytestmark = pytest.mark.usefixtures('mutable_config', 'mock_packages')
 
 # paths in repos that shouldn't be in the mirror tarballs.
 exclude = ['.hg', '.git', '.svn']
