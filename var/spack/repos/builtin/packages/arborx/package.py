2484a2746fbb8e59f1f6261e6cbe242b29cdfe0e
Daniel Arndt <arndtd@ornl.gov>
2021-03-23 15:12:15 -0400

    Use sha256

--- var/spack/repos/builtin/packages/arborx/package.py
+++ var/spack/repos/builtin/packages/arborx/package.py
@@ -16,7 +16,7 @@ class Arborx(CMakePackage):
     maintainers = ['aprokop']
 
     version('master',   branch='master')
-    version('1.0',      commit='41dff19c319bcc23e3917fa9ed22cec3e67e5c93')
+    version('1.0',      sha256='9b5f45c8180622c907ef0b7cc27cb18ba272ac6558725d9e460c3f3e764f1075')
     version('0.9-beta', sha256='b349b5708d1aa00e8c20c209ac75dc2d164ff9bf1b85adb5437346d194ba6c0d')
 
     # ArborX relies on Kokkos to provide devices, providing one-to-one matching
