e1e0b398c6ee698f5d89dadd1e83bc9f1c6f8732
vsoch <vsoch@users.noreply.github.com>
2021-03-24 16:56:37 -0600

    dont change pyelftools too much, just update version and add homepage
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- var/spack/repos/builtin/packages/py-pyelftools/package.py
+++ var/spack/repos/builtin/packages/py-pyelftools/package.py
@@ -7,12 +7,13 @@ from spack import *
 
 
 class PyPyelftools(PythonPackage):
-    """Library for analyzing ELF files and DWARF debugging information"""
-
-    homepage = "https://github.com/eliben/pyelftools"
+    """A pure-Python library for parsing and analyzing ELF files and DWARF
+       debugging information"""
     pypi = "pyelftools/pyelftools-0.27.tar.gz"
+    homepage = "https://github.com/eliben/pyelftools"
 
     version('0.27', sha256='cde854e662774c5457d688ca41615f6594187ba7067af101232df889a6b7a66b')
+    version('0.26', sha256='86ac6cee19f6c945e8dedf78c6ee74f1112bd14da5a658d8c9d4103aed5756a2')
+    version('0.23', sha256='fc57aadd096e8f9b9b03f1a9578f673ee645e1513a5ff0192ef439e77eab21de')
 
-    depends_on('python@2.7:2.8,3.4:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', when='@0.25:', type='build')
