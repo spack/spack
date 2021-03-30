25d05a928e5547334285117fbce6ef3e0d6cd0d8
dorton21 <odesmond21@gmail.com>
2021-03-30 12:48:10 -0500

    Added the proper deps

--- var/spack/repos/builtin/packages/py-iocapture/package.py
+++ var/spack/repos/builtin/packages/py-iocapture/package.py
@@ -16,7 +16,5 @@ class PyIocapture(PythonPackage):
 
     version('0.1.2', sha256='86670e1808bcdcd4f70112f43da72ae766f04cd8311d1071ce6e0e0a72e37ee8')
 
-    depends_on('py-six', type=('build', 'run'))
-    depends_on('py-flexmock', type=('build', 'run'))
-    depends_on('py-pytest', type=('build', 'run'))
-    depends_on('py-pytest-cov', type=('build', 'run'))
+    depends_on('python@2.4:', type=('build', 'run'))
+    depends_on('py-setuptools', type=('build', 'run'))
