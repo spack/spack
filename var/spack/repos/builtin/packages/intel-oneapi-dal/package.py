69953c7b3d19f935576c3a58f6e0d788392343d1
Danilin, Vasily <vasily.danilin@intel.com>
2021-03-31 13:58:32 +0300

    Integrate oneAPI 2021.2.0 release

--- var/spack/repos/builtin/packages/intel-oneapi-dal/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-dal/package.py
@@ -12,14 +12,18 @@ from spack import *
 class IntelOneapiDal(IntelOneApiLibraryPackage):
     """Intel oneAPI DAL."""
 
-    maintainers = ['rscohn2']
+    maintainers = ['rscohn2', 'danvev']
 
     homepage = 'https://software.intel.com/content/www/us/en/develop/tools/oneapi/components/onedal.html'
 
     if platform == 'linux':
+        version('2021.2.0',
+                url='https://registrationcenter-download.intel.com/akdlm/irc_nas/17747/l_daal_oneapi_p_2021.2.0.358_offline.sh',
+                sha256='cbf4e64dbd21c10179f2d1d7e8b8b0f12eeffe6921602df33276cd0ebd1f8e34',
+                expand=False)
         version('2021.1.1',
-                sha256='6e0e24bba462e80f0fba5a46e95cf0cca6cf17948a7753f8e396ddedd637544e',
                 url='https://registrationcenter-download.intel.com/akdlm/irc_nas/17443/l_daal_oneapi_p_2021.1.1.79_offline.sh',
+                sha256='6e0e24bba462e80f0fba5a46e95cf0cca6cf17948a7753f8e396ddedd637544e',
                 expand=False)
 
     depends_on('intel-oneapi-tbb')
