68e6f220e8adfc2a8dccf6e2bba4563a0adc2bfb
Cameron Rutherford <robert.rutherford@pnnl.gov>
2021-03-31 15:51:28 -0700

    Remove kron dependency for hiop within exago.
    modified:   package.py
                Removed dependency of kron with hiop by default.
                Added separate logic for hiop+raja.

--- var/spack/repos/builtin/packages/exago/package.py
+++ var/spack/repos/builtin/packages/exago/package.py
@@ -47,7 +47,8 @@ class Exago(CMakePackage, CudaPackage):
     depends_on('cmake@3.15:', type='build')
 
     # HiOp dependency logic
-    depends_on('hiop+shared+kron+raja', when='+hiop')
+    depends_on('hiop+shared', when='+hiop')
+    depends_on('hiop+raja', when='+hiop+raja')
     depends_on('hiop@0.3.99:', when='@0.99:+hiop')
     depends_on('hiop+cuda', when='+hiop+cuda')
     depends_on('hiop~mpi', when='+hiop~mpi')
