3f50da277809ecdbcad4ec2b4914b5b8450046a3
Veselin Dobrev <dobrev@llnl.gov>
2021-04-01 14:17:58 -0700

    mfem: apply 'mfem-4.2-slepc.patch' just to v4.2.

--- var/spack/repos/builtin/packages/mfem/package.py
+++ var/spack/repos/builtin/packages/mfem/package.py
@@ -273,9 +273,7 @@ class Mfem(Package, ROCmPackage):
     patch('mfem-3.3-3.4-petsc-3.9.patch',
           when='@3.3.0:3.4.0 +petsc ^petsc@3.9.0:')
     patch('mfem-4.2-umpire.patch', when='@4.2.0+umpire')
-    # FIXME: restrict to only version 4.2.0 when this issue is resolved, see
-    #        https://github.com/mfem/mfem/issues/1962.
-    patch('mfem-4.2-slepc.patch', when='@4.2.0:+slepc')
+    patch('mfem-4.2-slepc.patch', when='@4.2.0+slepc')
 
     # Patch to fix MFEM makefile syntax error. See
     # https://github.com/mfem/mfem/issues/1042 for the bug report and
