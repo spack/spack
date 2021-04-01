93435d3fb21e53f1c01f8917afde2145d2eae18c
Jen Herting <jen@herting.cc>
2021-04-01 15:55:55 -0400

    [ghostscript] added comment pointing to related bug report

--- var/spack/repos/builtin/packages/ghostscript/package.py
+++ var/spack/repos/builtin/packages/ghostscript/package.py
@@ -37,8 +37,9 @@ class Ghostscript(AutotoolsPackage):
 
     patch('nogoto.patch', when='%fj@:4.1.0')
 
+    # Related bug report: https://bugs.ghostscript.com/show_bug.cgi?id=702985
     # Patch source: https://git.ghostscript.com/?p=ghostpdl.git;a=commitdiff;h=41ef9a0bc36b
-    patch("https://git.ghostscript.com/?p=ghostpdl.git;a=commitdiff_plain;h=41ef9a0bc36b", when='^freetype@2.10.4:', sha256="254532a8561afddb5b39c1c30526676559ed9f0b87c1e9985eb797a59c98a3dd")
+    patch("https://git.ghostscript.com/?p=ghostpdl.git;a=commitdiff_plain;h=41ef9a0bc36b", when='^freetype@2.10.3:', sha256="254532a8561afddb5b39c1c30526676559ed9f0b87c1e9985eb797a59c98a3dd")
 
     def url_for_version(self, version):
         baseurl = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs{0}/ghostscript-{1}.tar.gz"
