c2c9bb69cce2daa997ff788c4aae049621201666
Axel Huebl <axel.huebl@plasma.ninja>
2021-04-01 19:34:49 -0700

    WarpX: 21.04
    Update WarpX: add first tagged version.

--- var/spack/repos/builtin/packages/warpx/package.py
+++ var/spack/repos/builtin/packages/warpx/package.py
@@ -17,11 +17,13 @@ class Warpx(CMakePackage):
     """
 
     homepage = "https://ecp-warpx.github.io"
+    url      = "https://github.com/ECP-WarpX/WarpX/archive/refs/tags/21.04.tar.gz"
     git      = "https://github.com/ECP-WarpX/WarpX.git"
 
     maintainers = ['ax3l', 'dpgrote', 'MaxThevenet', 'RemiLehe']
 
     version('develop', branch='development')
+    version('21.04', sha256='51d2d8b4542eada96216e8b128c0545c4b7527addc2038efebe586c32c4020a0')
 
     variant('app', default=True,
             description='Build the WarpX executable application')
