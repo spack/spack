fdadf59832b2888b9295316f05307c262eac6c66
Jen Herting <jen@herting.cc>
2021-04-05 14:45:42 -0400

    [py-ilmbase] added homepage and description removed fixmes

--- var/spack/repos/builtin/packages/py-ilmbase/package.py
+++ var/spack/repos/builtin/packages/py-ilmbase/package.py
@@ -3,36 +3,14 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-ilmbase
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-ilmbase
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyIlmbase(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+    """The PyIlmBase libraries provides python bindings for the IlmBase libraries."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/AcademySoftwareFoundation/openexr/tree/v2.3.0/PyIlmBase"
     url      = "https://github.com/AcademySoftwareFoundation/openexr/releases/download/v2.3.0/pyilmbase-2.3.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('2.3.0', sha256='9c898bb16e7bc916c82bebdf32c343c0f2878fc3eacbafa49937e78f2079a425')
 
