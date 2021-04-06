12ef2300d60c8f9d6f08f877169ed43e1d9e4251
iarspider <iarspider@gmail.com>
2021-04-06 10:08:05 +0200

    Start fleshing out recipe

--- var/spack/repos/builtin/packages/oracle-instant-client/package.py
+++ var/spack/repos/builtin/packages/oracle-instant-client/package.py
@@ -5,25 +5,47 @@
 
 from spack import *
 
+def oracleclient_releases():
+    releases = [
+    {
+        'version': '21.1.0.0.0',
+        'components': {
+           'basic': ['https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-basic-linux.x64-21.1.0.0.0.zip', '9b63e264c01ac54a0f0e61bd638576aed6f04a36b305bcd17847755e7b9855ce'],
+           'sqlplus': ['https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-sqlplus-linux.x64-21.1.0.0.0.zip', '3220f486940e82f1a7825e8f0875729d63abd57cc708f1908e2d5f2163b93937'],
+           'tools': ['https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-tools-linux.x64-21.1.0.0.0.zip', 'ff652d5bbfeaaa2403cbc13c5667f52e1d648aa2a5c59a50f4c9f84e6d2bba74'],
+           'sdk': ['https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-sdk-linux.x64-21.1.0.0.0.zip', '80a465530a565ed327ab9ae0d9fc067ed42338536c7e8721cf2c26e474f4f75f'],
+           'jdbc': ['https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-jdbc-linux.x64-21.1.0.0.0.zip', '76c866272712f2b432cc4be675605b22deca02f7a88a292b5ed8d29212d79dc7'],
+           'odbc': ['https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-odbc-linux.x64-21.1.0.0.0.zip', 'ec7722b522684f0a3f63481573d0eb3537764224eabed6223f33699dd940bf20']
+        }
+    }]
+    
+    return releases
+
 
 class OracleInstantClient(Package):
     """Oracle instant client"""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://www.oracle.com/database/technologies/instant-client.html"
     url      = "https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-basic-linux.x64-21.1.0.0.0.zip"
+    
+    releases = oracleclient_releases()
+    for release in releases:
+        # define the version using the mscore tarball
+        oracle_version = release['version']
+        main_pkg = release['components']['basic']
+        url, sha256 = *main_pkg
+        version(oracle_version, sha256=sha256, url=url)
+        # define resources for the other tarballs
+        for name, atts in release['components'].items():
+            # skip the tarball used for the version(...) call
+            if name == main_pkg:
+                continue
+            url, sha256 = *attrs
+            condition = "@{0}".format(oracle_version)
+            resource(name=name, url=url, sha256=sha256, when=condition, placement=name)
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
-    # FIXME: Add proper versions here.
-    # version('1.2.4')
-
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
-
+    def patch(self):
+        pass
+    
     def install(self, spec, prefix):
-        # FIXME: Unknown build system
-        make()
-        make('install')
+        pass
