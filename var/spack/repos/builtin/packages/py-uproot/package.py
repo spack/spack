1126beb4ab687d815c82aebdce71ee10b89d9f46
Glenn Johnson <glenn-johnson@uiowa.edu>
2021-04-07 11:04:09 -0500

    iterate over versions
    - changed from git to pypi
    - revert checksum to pypi version
    - only keep latest version in py-uproot
    - add back python version dependency that got dropped

--- var/spack/repos/builtin/packages/py-uproot/package.py
+++ var/spack/repos/builtin/packages/py-uproot/package.py
@@ -16,19 +16,14 @@ class PyUproot(PythonPackage):
     on C++ ROOT. Instead, it uses Numpy to cast blocks of data from the ROOT
     file as Numpy arrays."""
 
-    homepage = "https://uproot4.readthedocs.io"
-    git      = "https://github.com/scikit-hep/uproot4"
-    url      = "https://github.com/scikit-hep/uproot4/archive/0.0.27.tar.gz"
+    homepage = "homepage = https://github.com/scikit-hep/uproot4"
+    pypi     = "uproot/uproot-4.0.6.tar.gz"
 
     maintainers = ['vvolkl']
 
     tags = ['hep']
 
-    version('master', branch='master')
-    version('4.0.6', sha256='359eba307dfbf40b26a2da67ec829bd82cce67aad8c22120e02df3401be2870d')
-    version('4.0.2', sha256='8145af29788cbe6bf0ee279a7f176159f3eee801641ead4ad6e627f8c4dff0a9')
-    version('0.1.2', sha256='b32dbffadc87bc5707ee0093964d2ce4a5ccfd521b17bbf10732afc25b820d82')
-    version('0.0.27', sha256='de87555937332998b476f3e310392962bc983bddc008ed2b3c07a25c0379c4c9')
+    version('4.0.6', sha256='1bea2ccc899c6959fb2af69d7e5d1e2df210caab30d3510e26f3fc07c143c37e')
 
     variant('xrootd', default=True,
             description='Build with xrootd support ')
@@ -40,6 +35,7 @@ class PyUproot(PythonPackage):
             description='Build with support for reading '
                         'zstd-compressed rootfiles ')
 
+    depends_on('python@2.6:2.999,3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy', type=('build', 'run'))
 
