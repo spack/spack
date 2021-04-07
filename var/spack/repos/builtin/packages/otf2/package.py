b7d72e26bb5dae2517d1b91030269144833a6e94
Severin Strobl <severin.strobl@dlr.de>
2021-04-07 11:55:24 +0200

    otf2: patch for uninitialized variable (#22719)
    As pointed out in https://github.com/STEllAR-GROUP/hpx/issues/5239,
    there is an issues in OTF2 <=2.2 where a variable is not properly
    initialized. As currently no release of OTF2 is available fixing this,
    the patch should be applied.

--- var/spack/repos/builtin/packages/otf2/package.py
+++ var/spack/repos/builtin/packages/otf2/package.py
@@ -24,6 +24,10 @@ class Otf2(AutotoolsPackage):
     version('1.3.1', sha256='c4605ace845d89fb1a19223137b92cc503b01e3db5eda8c9e0715d0cfcf2e4b9')
     version('1.2.1', sha256='1db9fb0789de4a9c3c96042495e4212a22cb581f734a1593813adaf84f2288e4')
 
+    # Fix missing initialization of variable resulting in issues when used by
+    # APEX/HPX: https://github.com/STEllAR-GROUP/hpx/issues/5239
+    patch('collective_callbacks.patch', when='@2.1:2.2')
+
     def configure_args(self):
         return [
             '--enable-shared',
