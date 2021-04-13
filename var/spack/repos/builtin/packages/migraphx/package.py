cf711d92db151e1712e944bcd421a85de7eda7aa
arjun-raj-kuppala <60718144+arjun-raj-kuppala@users.noreply.github.com>
2021-04-13 10:46:57 -0700

    Update package.py

--- var/spack/repos/builtin/packages/migraphx/package.py
+++ var/spack/repos/builtin/packages/migraphx/package.py
@@ -41,6 +41,9 @@ class Migraphx(CMakePackage):
     depends_on('nlohmann-json', type='link')
     depends_on('msgpack-c', type='link')
     depends_on('half@1.12.0', type='link')
+    depends_on('py-pybind11', type='build')
+    depends_on('py-pybind11@2.6:', type='build', when='@4.1.0')
+    depends_on('nlohmann-json', when='@3.9.0:')
 
     for ver in ['3.5.0', '3.7.0', '3.8.0', '3.9.0', '3.10.0', '4.0.0', '4.1.0']:
         depends_on('hip@' + ver, type='build', when='@' + ver)
@@ -48,10 +51,6 @@ class Migraphx(CMakePackage):
         depends_on('llvm-amdgpu@' + ver, type='build', when='@' + ver)
         depends_on('rocblas@' + ver, type='link', when='@' + ver)
         depends_on('miopen-hip@' + ver, type='link', when='@' + ver)
-        depends_on('py-pybind11', type='build')
-        depends_on('py-pybind11@2.6:', type='build', when='@4.1.0')
-
-    depends_on('nlohmann-json', when='@3.9.0:')
 
     def cmake_args(self):
         args = [
