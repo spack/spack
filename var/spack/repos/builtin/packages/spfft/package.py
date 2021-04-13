c11af21da24bcf4ee36814738064987ed2b07917
Simon Frasch <simon.frasch@cscs.ch>
2021-04-13 13:35:37 +0200

    spfft: add version 1.0.2 (#22958)

--- var/spack/repos/builtin/packages/spfft/package.py
+++ var/spack/repos/builtin/packages/spfft/package.py
@@ -17,6 +17,7 @@ class Spfft(CMakePackage, CudaPackage):
     version('develop', branch='develop')
     version('master', branch='master')
 
+    version('1.0.2', sha256='9b1296bda0b9ec3d37c74fd64354a01ebc6e2da7cb026c1f821882160b03c692')
     version('1.0.1', sha256='f8ab706309776cfbd2bfd8e29a6a9ffb5c8f3cd62399bf82db1e416ae5c490c8')
     version('1.0.0', sha256='bd98897aa6734563ec63cd84168e731ef2e2bbc01a574c6dc59b74475742b6ee')
     version('0.9.13', sha256='5ccc93c9362bec14cfb6e31dd0e7ae7e48db0453ab49ebc9722041b69db759ef')
