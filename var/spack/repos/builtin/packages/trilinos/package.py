1d9b6a6271b389ae80e480b0d722080d0958055b
Andreas Fink <finkandreas@web.de>
2021-04-13 18:55:41 +0000

    fix building trilinos with cuda-11 and cxxstd=14 variant

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -428,6 +428,7 @@ class Trilinos(CMakePackage, CudaPackage):
     patch('fix_clang_errors_12_18_1.patch', when='@12.18.1%clang')
     patch('cray_secas_12_12_1.patch', when='@12.12.1%cce')
     patch('cray_secas.patch', when='@12.14.1:12.18.1%cce')
+    patch('fix_cxx14_cuda11.patch', when='@13.0.0:13.0.1 cxxstd=14 ^cuda@11:')
 
     def url_for_version(self, version):
         url = "https://github.com/trilinos/Trilinos/archive/trilinos-release-{0}.tar.gz"
