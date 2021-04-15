64fd2d68730f14cc54ed64da1e3aa5f6ca18be81
loulawrence <72574166+loulawrence@users.noreply.github.com>
2021-04-15 15:30:58 -0400

    spec parsing fix (#23010)

--- lib/spack/spack/modules/lmod.py
+++ lib/spack/spack/modules/lmod.py
@@ -8,6 +8,7 @@ import itertools
 import os.path
 from typing import Any, Dict  # novm
 
+import posixpath
 import llnl.util.lang as lang
 
 import spack.compilers
@@ -99,7 +100,7 @@ def guess_core_compilers(name, store=False):
 
 class LmodConfiguration(BaseConfiguration):
     """Configuration class for lmod module files."""
-    default_projections = {'all': os.path.join('{name}', '{version}')}
+    default_projections = {'all': posixpath.join('{name}', '{version}')}
 
     @property
     def core_compilers(self):
