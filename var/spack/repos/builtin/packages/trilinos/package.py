c0473445d0272edf09f57451c707427edd8e08f8
Andreas Fink <finkandreas@web.de>
2021-04-15 09:37:11 +0000

    add comments about patch provenance

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -428,6 +428,9 @@ class Trilinos(CMakePackage, CudaPackage):
     patch('fix_clang_errors_12_18_1.patch', when='@12.18.1%clang')
     patch('cray_secas_12_12_1.patch', when='@12.12.1%cce')
     patch('cray_secas.patch', when='@12.14.1:12.18.1%cce')
+
+    # workaround an NVCC bug with c++14 (https://github.com/trilinos/Trilinos/issues/6954)
+    # avoid calling deprecated functions with CUDA-11
     patch('fix_cxx14_cuda11.patch', when='@13.0.0:13.0.1 cxxstd=14 ^cuda@11:')
 
     def url_for_version(self, version):
