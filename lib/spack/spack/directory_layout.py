c98fe26e214f2c6684fc968c57508d7a4c43896f
loulawrence <72574166+loulawrence@users.noreply.github.com>
2021-04-15 15:30:58 -0400

    spec parsing fix (#23010)

--- lib/spack/spack/directory_layout.py
+++ lib/spack/spack/directory_layout.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import os
+import posixpath
 import shutil
 import glob
 import tempfile
@@ -21,7 +22,7 @@ import spack.util.spack_json as sjson
 from spack.error import SpackError
 
 
-default_projections = {'all': os.path.join(
+default_projections = {'all': posixpath.join(
     '{architecture}', '{compiler.name}-{compiler.version}',
     '{name}-{version}-{hash}')}
 
