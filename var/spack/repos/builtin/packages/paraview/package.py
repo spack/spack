0ae35937042ac2768f6031ff840cf30532c0f643
psakiev <psakiev@sandia.gov>
2021-04-14 20:45:42 -0600

    hack for paraview clingo issue

--- var/spack/repos/builtin/packages/paraview/package.py
+++ var/spack/repos/builtin/packages/paraview/package.py
@@ -51,9 +51,12 @@ class Paraview(CMakePackage, CudaPackage):
             description='Builds a shared version of the library')
     variant('kits', default=True,
             description='Use module kits')
+
+    legacy_cuda_values=['native', 'fermi', 'kepler', 'maxwell',
+                        'pascal', 'volta', 'turing', 'all', 'none']
+    cuda_values = legacy_cuda_values.extend(CudaPackage.cuda_arch_values)
     variant('cuda_arch', default='native', multi=False,
-            values=('native', 'fermi', 'kepler', 'maxwell',
-                    'pascal', 'volta', 'turing', 'all', 'none'),
+            values=cuda_values,
             description='CUDA architecture')
 
     conflicts('+python', when='+python3')
