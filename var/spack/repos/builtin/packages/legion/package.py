204f58f405a3040fbda4880b74a773be8bbb74eb
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-04-20 13:01:01 -0600

    Little bits of cleanup and finalizing some details.

--- var/spack/repos/builtin/packages/legion/package.py
+++ var/spack/repos/builtin/packages/legion/package.py
@@ -20,13 +20,13 @@ class Legion(CMakePackage):
        the memory hierarchy and assignment of tasks to processors in a way
        that is orthogonal to correctness, thereby enabling easy porting and
        tuning of Legion applications to new architectures."""
-       
+
     homepage = "http://legion.stanford.edu/"
-    url = "https://github.com/StanfordLegion/legion/tarball/legion-20.03.0"
     git = "https://github.com/StanfordLegion/legion.git"
 
     maintainers = ['pmccormick', 'streichler']
 
+    version('21.03.0', tag='legion-21.03.0')
     version('stable', branch='stable')
     version('master', branch='master')
     version('cr', branch='control_replication')
