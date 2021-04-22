2de608767b1ea8c8d8f4cf81576a64ca5cfc2ecc
vsoch <vsoch@users.noreply.github.com>
2021-04-22 12:52:52 -0600

    testing fix for lock on github runners
    The issue is reported to be resolved, e.g., see
    https://github.com/actions/virtual-environments/issues/3185 and
    I want to test if it indeed is.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- lib/spack/llnl/util/lock.py
+++ lib/spack/llnl/util/lock.py
@@ -264,7 +264,7 @@ class Lock(object):
         self.old_host = self.host
 
         self.pid = os.getpid()
-        self.host = socket.gethostname()
+        self.host = socket.getfqdn()
 
         # write pid, host to disk to sync over FS
         self._file.seek(0)
