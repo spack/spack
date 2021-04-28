d75611cce1277896e68c3e7e4f6affcaf6417ced
Sid Pendelberry <sid@rit.edu>
2021-04-28 13:14:14 -0400

    fixed style errors in lightning

--- var/spack/repos/builtin/packages/py-pytorch-lightning/package.py
+++ var/spack/repos/builtin/packages/py-pytorch-lightning/package.py
@@ -15,20 +15,20 @@ class PyPytorchLightning(PythonPackage):
     version('1.3.0rc1', sha256='a9a7603ff50e3024f0adb80b620899af4bf67cb47a1f81e16b144c26115e368a')
     version('1.2.10', sha256='2d8365e30ded0c20e73ce6e5b6028478ae460b8fd33727df2275666df005a301')
 
-    conflicts('py-pyyaml@5.4') # OmegaConf requirement >=5.1
-    conflicts('py-tensorboard@2.5.0') # 2.5.0 GPU CI error: Couldn't build proto file into descriptor pool!
-    
+    conflicts('py-pyyaml@5.4')          # OmegaConf requirement >=5.1
+    conflicts('py-tensorboard@2.5.0')   # 2.5.0 GPU CI error: Couldn't build proto file into descriptor pool!
+
     # requirements from setup.py
-    depends_on('py-setuptools',         type='build')
-    depends_on('python@3.6:',           type=('build', 'run'))
-    depends_on('py-torch@1.4:',         type=('build', 'run'))
-    depends_on('py-torchvision',        type=('build', 'run'))
-    depends_on('py-numpy@1.17.2:',      type=('build', 'run'))
-    depends_on('py-future@0.17.1:',     type=('build', 'run'))
-    depends_on('py-pyyaml@3.13:',       type=('build', 'run'))
-    depends_on('py-tqdm@4.41.0:',       type=('build', 'run'))
-    depends_on('py-fsspec@2021.4.0:',   type=('build', 'run'))
-    depends_on('py-tensorboard@2.2.0:', type=('build', 'run'))
-    depends_on('py-torchmetrics@0.2.0:',type=('build', 'run'))
-    depends_on('py-pydeprecate@0.3.0',  type=('build', 'run'))
-    depends_on('py-packaging',          type=('build', 'run'))
+    depends_on('py-setuptools',             type='build')
+    depends_on('python@3.6:',               type=('build', 'run'))
+    depends_on('py-torch@1.4:',             type=('build', 'run'))
+    depends_on('py-torchvision',            type=('build', 'run'))
+    depends_on('py-numpy@1.17.2:',          type=('build', 'run'))
+    depends_on('py-future@0.17.1:',         type=('build', 'run'))
+    depends_on('py-pyyaml@3.13:',           type=('build', 'run'))
+    depends_on('py-tqdm@4.41.0:',           type=('build', 'run'))
+    depends_on('py-fsspec@2021.4.0:',       type=('build', 'run'))
+    depends_on('py-tensorboard@2.2.0:',     type=('build', 'run'))
+    depends_on('py-torchmetrics@0.2.0:',    type=('build', 'run'))
+    depends_on('py-pydeprecate@0.3.0',      type=('build', 'run'))
+    depends_on('py-packaging',              type=('build', 'run'))
