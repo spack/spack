22b1d68dfadbeef72bdf25bf21a8076eacd042ba
Gerald Ragghianti <ragghianti@icl.utk.edu>
2021-04-28 09:59:36 -0400

    Change "make check" to correctly run from the build directory.

--- var/spack/repos/builtin/packages/blaspp/package.py
+++ var/spack/repos/builtin/packages/blaspp/package.py
@@ -64,6 +64,7 @@ class Blaspp(CMakePackage, CudaPackage, ROCmPackage):
     def check(self):
         # If the tester fails to build, ensure that the check() fails.
         if os.path.isfile(join_path(self.build_directory, 'test', 'tester')):
-            make('check')
+            with working_dir(self.build_directory):
+                make('check')
         else:
             raise Exception('The tester was not built!')
