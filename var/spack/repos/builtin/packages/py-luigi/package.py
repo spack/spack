5f4451c8f5d2c4c6ba5f3309d549b3a14c6aa75d
genrich <genrich.ivaska@epfl.ch>
2021-04-29 17:46:58 +0200

    more review comments

--- var/spack/repos/builtin/packages/py-luigi/package.py
+++ var/spack/repos/builtin/packages/py-luigi/package.py
@@ -17,8 +17,9 @@ class PyLuigi(PythonPackage):
     version('3.0.1', sha256='f158f4e093638bf734e2f4f08261bdba414bac7187ab69f1d6f8c95b1c408409')
     version('2.8.3', sha256='8b5c84a3c3f4df07309056d3b98348b93c054f1931b7ee22fc29e7989f645c9e')
 
-    depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
-    depends_on('python@3.5:',         type=('build', 'run'), when='@3:')
+    depends_on('python@2.7:2.8,3.3:', type=('build', 'run'))
+    depends_on('python@3.3:',         type=('build', 'run'), when='@3.0.0:')
+    depends_on('python@3.5:',         type=('build', 'run'), when='@3.0.2:')
 
     depends_on('py-setuptools', type='build')
 
@@ -33,5 +34,3 @@ class PyLuigi(PythonPackage):
     depends_on('py-python-daemon', type=('build', 'run'))
 
     depends_on('py-python-dateutil@2.7.5:2.999', when='@2.8.3:', type=('build', 'run'))
-
-    depends_on('py-pytest@3.3.0:', type='test')
