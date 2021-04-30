46bce56cef6aa5cbea591adc47b2bb5dd2344939
Keita Iwabuchi <kiwabuchi@llnl.gov>
2021-04-30 14:51:07 -0700

    Metall package: add v0.11 and v0.12 (#23373)
    * Metall: add version 0.2
    
    * Add Metall v0.3
    
    * Update Metall package to v0.4 and v0.5.
    
    * Metall package: add v0.6
    
    * Metall package: add v0.7
    
    * Metall package: add v0.8 and v0.9
    
    * Add Metall package v0.10
    
    * Metall package: set run_environment METALL_ROOT
    
    * Metall package: removed blanks
    
    * Metall package: add v0.11 and v0.12
    
    * Metall package: change required cmake version

--- var/spack/repos/builtin/packages/metall/package.py
+++ var/spack/repos/builtin/packages/metall/package.py
@@ -16,9 +16,12 @@ class Metall(CMakePackage):
     version('master', branch='master')
     version('develop', branch='develop')
 
+    version('0.12', sha256='b757b354b355e866bd6d42da53b0160442f3b7f663a19ba113da1ffc1a76176e')
+    version('0.11', sha256='7cfa6a7eaaeb7fd11ecfbe43a172a36c8cde200601d6cd3b309d7a0acf752f3c')
     version('0.10', sha256='58b4b5507d4db5baca315b1bed2b728981755d755b91ef63bd0b6dfaf320f46b')
     version('0.9', sha256='2d7bd9ea2f1e04136050f210884445a9e3dcb96c992cf42ff9ea4b392f85f927')
 
+    depends_on('cmake@3.10:', type='build')
     depends_on('boost@1.64:', type=('build', 'link'))
 
     def cmake_args(self):
