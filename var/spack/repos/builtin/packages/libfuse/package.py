f1a51ad53ae3438e0c490869ef4c32aa5af0dd20
Harmen Stoppels <harmenstoppels@gmail.com>
2021-05-03 14:30:02 +0200

    Fix url

--- var/spack/repos/builtin/packages/libfuse/package.py
+++ var/spack/repos/builtin/packages/libfuse/package.py
@@ -12,7 +12,7 @@ class Libfuse(MesonPackage):
     Userspace) interface"""
 
     homepage = "https://github.com/libfuse/libfuse"
-    url      = "https://github.com/libfuse/libfuse/archive/fuse-3.9.3.tar.gz"
+    url      = "https://github.com/libfuse/libfuse/releases/download/fuse-2.9.9/fuse-2.9.9.tar.gz"
 
     version('3.10.2', sha256='a16f93cc083264afd0d2958a0dc88f24c6c5d40a9f3842c645b1909e13edb75f')
     version('3.10.1', sha256='d8954e7b4c022c651aa80db3bb4a161437dd285cd5f1a23d0e25f055dcebe00d')
@@ -21,6 +21,11 @@ class Libfuse(MesonPackage):
     version('3.9.3',  sha256='0f8f7ad9cc6667c6751efa425dd0a665dcc9d75f0b7fc0cb5b85141a514110e9')
     version('3.9.2',  sha256='b4409255cbda6f6975ca330f5b04cb335b823a95ddd8c812c3d224ec53478fc0')
     version('2.9.9',  sha256='d0e69d5d608cc22ff4843791ad097f554dd32540ddc9bed7638cc6fea7c1b4b5')
+
+    def url_for_version(self, version):
+        if version < Version("3.0.0"):
+            return "https://github.com/libfuse/libfuse/releases/download/fuse-{0}/fuse-{1}.tar.gz".format(version, version)
+        return "https://github.com/libfuse/libfuse/archive/refs/tags/fuse-{0}.tar.gz".format(version)
 
     variant('useroot', default=False, description="Use root privileges to make fusermount a setuid binary after installation")
     variant('system_install', default=False, description=(
