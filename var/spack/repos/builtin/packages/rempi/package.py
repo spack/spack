b26b30879515b690fe1b33950b20b03f4e0e4a4e
Luke Roskop <luke.roskop@hpe.com>
2021-05-07 14:58:17 -0500

    remove unused spec assignment

--- var/spack/repos/builtin/packages/rempi/package.py
+++ var/spack/repos/builtin/packages/rempi/package.py
@@ -22,8 +22,6 @@ class Rempi(AutotoolsPackage):
     depends_on("libpciaccess", type='link')
 
     def setup_environment(self, spack_env, run_env):
-        spec = self.spec
-
         if self.spec.satisfies('%cce'):
             spack_env.set('MPICC', 'mpicc')
             spack_env.set('MPICXX', 'mpicxx')
