45f84ee548842bef71038bb7d599f6f2ea3685df
dsjense <79338261+dsjense@users.noreply.github.com>
2021-05-07 17:56:28 -0600

    Bugfix/matplotlib clang lto (#23519)
    * Disable link-time optimization for matplotlib when using Clang.
    
    * Remove duplicated line.
    
    * Disable LTO only for versions >= 3.3.0.

--- var/spack/repos/builtin/packages/py-matplotlib/package.py
+++ var/spack/repos/builtin/packages/py-matplotlib/package.py
@@ -189,6 +189,8 @@ class PyMatplotlib(PythonPackage):
                 setup.write('[libs]\n')
                 setup.write('system_freetype = True\n')
                 setup.write('system_qhull = True\n')
+                if self.spec.satisfies('%clang'):
+                    setup.write('enable_lto = False\n')
 
     @run_after('build')
     @on_package_attributes(run_tests=True)
