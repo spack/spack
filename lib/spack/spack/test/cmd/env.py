b483985b674d885fd156b3115d5208e5ee93e734
Gregory Becker <becker33@llnl.gov>
2021-05-07 18:38:01 -0700

    make environments available in install subprocess

--- lib/spack/spack/test/cmd/env.py
+++ lib/spack/spack/test/cmd/env.py
@@ -2501,7 +2501,7 @@ spack:
     _env_create('test', StringIO(spack_yaml))
 
     with ev.read('test') as e:
-        install('-v')
+        install()
 
         spec = e.specs_by_hash[e.concretized_order[0]]
         view_prefix = e.default_view.view().get_projection_for_spec(spec)
@@ -2536,7 +2536,7 @@ spack:
     _env_create('test', StringIO(spack_yaml))
 
     with ev.read('test') as e:
-        print(install())
+        install()
 
         spec = e.specs_by_hash[e.concretized_order[0]]
         view_prefix = e.default_view.view().get_projection_for_spec(spec)
