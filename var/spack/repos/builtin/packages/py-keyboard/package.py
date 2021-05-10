2832c7c1ce2484ee5356c8fc526e273c082603f5
Jen Herting <jen@herting.cc>
2021-05-10 14:21:21 -0400

    [py-keyboard]
    - updated homepage
    - added dependency for OSX
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-keyboard/package.py
+++ var/spack/repos/builtin/packages/py-keyboard/package.py
@@ -3,48 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-keyboard
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-keyboard
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyKeyboard(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Take full control of your keyboard with this small
+    Python library. Hook global events, register hotkeys,
+    simulate key presses and much more."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/boppreh/keyboard"
     pypi     = "keyboard/keyboard-0.13.5.zip"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.13.5', sha256='63ed83305955939ca5c9a73755e5cc43e8242263f5ad5fd3bb7e0b032f3d308b')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('py-setuptools', type='build')
+    depends_on('py-pyobjc', when='platform=darwin', type=('build', 'run'))
