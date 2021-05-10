16dea9f7dd039a58676a619f26e29a897912569b
Jen Herting <jen@herting.cc>
2021-05-10 15:05:25 -0400

    [py-ibm-watson] added dependencies

--- var/spack/repos/builtin/packages/py-ibm-watson/package.py
+++ var/spack/repos/builtin/packages/py-ibm-watson/package.py
@@ -40,8 +40,11 @@ class PyIbmWatson(PythonPackage):
     # if you need specific versions. A generic python dependency is
     # added implicity by the PythonPackage class.
     # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-requests@2.0:2.999', type=('build', 'run'))
+    depends_on('py-python-dateutil@2.5.3:', type=('build', 'run'))
+    depends_on('py-websocket-client@0.48.0', type=('build', 'run'))
+    depends_on('py-ibm-cloud-sdk-core@3.3.6:', type=('build', 'run'))
 
     def build_args(self, spec, prefix):
         # FIXME: Add arguments other than --prefix
