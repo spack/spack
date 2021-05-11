297f0e489af0f1daf962c628321d04ec8ea8d869
lukebroskop <luke.roskop@hpe.com>
2021-05-11 18:08:51 -0500

    craype fix for rempi (#23507)
    When comping with cce, use the mpi wrappers

--- var/spack/repos/builtin/packages/rempi/package.py
+++ var/spack/repos/builtin/packages/rempi/package.py
@@ -20,3 +20,9 @@ class Rempi(AutotoolsPackage):
     depends_on("automake", type='build')
     depends_on("libtool", type='build')
     depends_on("libpciaccess", type='link')
+
+    def setup_build_environment(self, env):
+        if self.spec.satisfies('%cce'):
+            spack_env.set('MPICC', 'mpicc')
+            spack_env.set('MPICXX', 'mpicxx')
+            spack_env.set('MPICH_CC', 'cc')
