d4e8daf3d7e008188d7a7bcaa1e053dbef7f8f54
Kayla Richarda Butler <butler59@quartz188.llnl.gov>
2021-05-12 13:52:49 -0700

    Remove conflict

--- var/spack/repos/builtin/packages/slepc/package.py
+++ var/spack/repos/builtin/packages/slepc/package.py
@@ -21,7 +21,6 @@ class Slepc(Package):
 
     version('main', branch='main')
     version('3.15.0', sha256='e53783ae13acadce274ea65c67186b5ab12332cf17125a694e21d598aa6b5f00')
->>>>>>> d19d81a3f4451af5911ad46d24754268ba5137d6
     version('3.14.2', sha256='3e54578dda1f4c54d35ac27d02f70a43f6837906cb7604dbcec0e033cfb264c8')
     version('3.14.1', sha256='cc78a15e34d26b3e6dde003d4a30064e595225f6185c1975bbd460cb5edd99c7')
     version('3.14.0', sha256='37f8bb270169d1d3f5d43756ac8929d56204e596bd7a78a7daff707513472e46')
