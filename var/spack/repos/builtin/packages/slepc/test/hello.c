8e653db3cebf0eac4825278ddc5bb48238a44710
Kayla Richarda Butler <butler59@quartz188.llnl.gov>
2021-05-12 13:11:47 -0700

    Edit hello world example

--- var/spack/repos/builtin/packages/slepc/test/hello.c
+++ var/spack/repos/builtin/packages/slepc/test/hello.c
@@ -1,17 +1,14 @@
-//Slepc tutorial: https://slepc.upv.es/handson/handson0.html.
-static char help[] = "Simple Hello World example program in SLEPc\n";
+static char help[] = "Hello World example program\n";
 
 #include "slepcsys.h"
 
-#undef __FUNCT__
-#define __FUNCT__ "main"
 int main( int argc, char **argv )
 {
   int ierr;
 
   SlepcInitialize(&argc,&argv,(char*)0,help);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"Hello world\n");CHKERRQ(ierr);
-  ierr = SlepcFinalize();
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Hello world\n");
+
   return ierr;
 }
 
