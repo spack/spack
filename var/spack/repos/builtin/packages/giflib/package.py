66ca442f67582521dcb9263097da10ebc919d900
Ivan Razumov <ivan.razumov@cern.ch>
2021-05-12 11:17:27 +0200

    giflib 5.20+ no longer uses autotools

--- var/spack/repos/builtin/packages/giflib/package.py
+++ var/spack/repos/builtin/packages/giflib/package.py
@@ -17,7 +17,28 @@ class Giflib(AutotoolsPackage, SourceforgePackage):
     version('5.2.0', sha256='dc7257487c767137602d86c17098ec97065a718ff568a61cfcf1a9466f197b1f')
     version('5.1.4', sha256='df27ec3ff24671f80b29e6ab1c4971059c14ac3db95406884fc26574631ba8d5', extension='tar.bz2')
 
+    depends_on('automake', type='build')
+    depends_on('autoconf', type='build')
+    depends_on('m4', type='build')
+    depends_on('libtool', type='build')
+
     patch('bsd-head.patch')
 
     def check(self):
         make('check', parallel=False)
+
+    @when('@5.2.0:')
+    def configure(self, spec, prefix):
+        return
+
+    @when('@5.2.0:')
+    def autoreconf(self, spec, prefix):
+        return
+
+    @when('@5.2.0:')
+    def install(self, spec, prefix):
+        make('install', 'PREFIX={0}'.format(prefix))
+
+    def patch(self):
+        if self.spec.satisfies('@5.2.0:'):
+            touch(join_path(self.stage.source_path, 'configure'))
