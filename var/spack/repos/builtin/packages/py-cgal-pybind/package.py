ab65f42501dd659d7b442795ee67ad566f9f672b
Luc Guyot <luc.guyot@epfl.ch>
2021-05-12 17:57:50 +0200

    Add version 0.1.0 to py-cgal-pybind/package.py [NSETM-1454] (#1159)
    * Add version 0.1.0 to py-cgal-pybind/package.py [NSETM-1454]
    
    This new version has no install dependency anymore on trimesh
    but only an extra dependency named "tests".
    
    This allows consumers of the cgal-pybind API like atlas-building-tools
    to lower the required version of trimesh so that it matches the versions
    available in BBP's py-trimesh/package.py (currently trimesh==2.38.10).
    
    See https://github.com/BlueBrain/spack/pull/1152 for full context.
    
    * Fixes flake8 errors
    
    * Puts latest version at the top of the version list (package.py)
    
    * Refers to the gitlab repository only
    
    * Removes older version 0.0.2: it is actually unused

--- var/spack/repos/builtin/packages/py-cgal-pybind/package.py
+++ var/spack/repos/builtin/packages/py-cgal-pybind/package.py
@@ -9,16 +9,16 @@ from spack import *
 class PyCgalPybind(PythonPackage):
     """Internal Python bindings for CGAL"""
 
-    homepage = "https://bbpcode.epfl.ch/browse/code/common/cgal-pybind/tree/"
-    git      = "ssh://bbpcode.epfl.ch/common/cgal-pybind"
+    homepage = "https://bbpgitlab.epfl.ch/nse/cgal-pybind"
+    git = "git@bbpgitlab.epfl.ch:nse/cgal-pybind.git"
 
-    version('develop', submodules=True)
-    version('0.0.2', commit='7aa1382d1628ccd51f692750a2b145b1df0694d9', submodules=True)
+    version("develop", submodules=True)
+    version("0.1.0", tag="cgal_pybind-v0.1.0", submodules=True)
 
-    depends_on('py-setuptools', type='build')
-    depends_on('boost@1.50:')
-    depends_on('cmake', type='build')
-    depends_on('cgal')
-    depends_on('eigen')
-    depends_on('py-pybind11')
-    depends_on('py-numpy@1.12:', type=('build', 'run'))
+    depends_on("py-setuptools", type="build")
+    depends_on("boost@1.50:")
+    depends_on("cmake", type="build")
+    depends_on("cgal")
+    depends_on("eigen")
+    depends_on("py-pybind11")
+    depends_on("py-numpy@1.12:", type=("build", "run"))
