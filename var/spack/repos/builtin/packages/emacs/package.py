34c62c58c6814914a8a97f9c2058ae511dfb500a
miheer vaidya <v.miheer@gmail.com>
2021-05-13 17:34:19 -0600

    Mark native dependent on master

--- var/spack/repos/builtin/packages/emacs/package.py
+++ var/spack/repos/builtin/packages/emacs/package.py
@@ -57,6 +57,7 @@ class Emacs(AutotoolsPackage, GNUMirrorPackage):
     depends_on('gcc@11: +strip languages=jit', when="+native")
 
     conflicts('@:26.3', when='platform=darwin os=catalina')
+    conflicts('+native', when='@:27.99.99', msg="native compilation require @master")
 
     @when('platform=darwin')
     def setup_build_environment(self, env):
