a8076d0ea91a55b23ee380b74a780b2d64d3f86b
Jen Herting <jen@herting.cc>
2021-05-14 12:47:28 -0400

    [py-pygetwindow] added dependencies

--- var/spack/repos/builtin/packages/py-pygetwindow/package.py
+++ var/spack/repos/builtin/packages/py-pygetwindow/package.py
@@ -40,7 +40,8 @@ class PyPygetwindow(PythonPackage):
     # if you need specific versions. A generic python dependency is
     # added implicity by the PythonPackage class.
     # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type='build')
+    depends_on('py-pyrect', type=('build', 'run'))
     # depends_on('py-foo',        type=('build', 'run'))
 
     def build_args(self, spec, prefix):
