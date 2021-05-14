a66bca591c47edf9c96b4a988fc97126634fc356
Michael Kuhn <michael.kuhn@ovgu.de>
2021-05-15 00:42:09 +0200

    zstd: add 1.5.0 (#23646)

--- var/spack/repos/builtin/packages/zstd/package.py
+++ var/spack/repos/builtin/packages/zstd/package.py
@@ -20,6 +20,7 @@ class Zstd(CMakePackage):
     maintainers = ['haampie']
 
     version('develop', branch='dev')
+    version('1.5.0', sha256='0d9ade222c64e912d6957b11c923e214e2e010a18f39bec102f572e693ba2867')
     version('1.4.9', sha256='acf714d98e3db7b876e5b540cbf6dee298f60eb3c0723104f6d3f065cd60d6a8')
     version('1.4.8', sha256='f176f0626cb797022fbf257c3c644d71c1c747bb74c32201f9203654da35e9fa')
     version('1.4.7', sha256='085500c8d0b9c83afbc1dc0d8b4889336ad019eba930c5d6a9c6c86c20c769c8')
