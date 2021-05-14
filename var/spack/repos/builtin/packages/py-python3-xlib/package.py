1208263ca5f79f6422d32fba81db35150586c38d
Jen Herting <jen@herting.cc>
2021-05-14 10:55:32 -0400

    [py-python3-xlib] requires python3

--- var/spack/repos/builtin/packages/py-python3-xlib/package.py
+++ var/spack/repos/builtin/packages/py-python3-xlib/package.py
@@ -39,7 +39,7 @@ class PyPython3Xlib(PythonPackage):
     # FIXME: Add dependencies if required. Only add the python dependency
     # if you need specific versions. A generic python dependency is
     # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
+    depends_on('python@3:', type=('build', 'run'))
     # depends_on('py-setuptools', type='build')
     # depends_on('py-foo',        type=('build', 'run'))
 
