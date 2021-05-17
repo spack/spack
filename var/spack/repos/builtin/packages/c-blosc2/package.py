f0d48439c3396b75066adbb88b40cd50e2aaf448
Robert Mijakovic <robert.mijakovic@gmail.com>
2021-05-17 12:12:27 +0200

    c-blosc2: new version, v2.0.0rc1 (#23560)

--- var/spack/repos/builtin/packages/c-blosc2/package.py
+++ var/spack/repos/builtin/packages/c-blosc2/package.py
@@ -11,11 +11,13 @@ class CBlosc2(CMakePackage):
        other bells and whistles"""
 
     homepage = "http://www.blosc.org"
+    url      = "https://github.com/Blosc/c-blosc2/archive/refs/tags/v2.0.0.rc1.tar.gz"
     git      = "https://github.com/Blosc/c-blosc2.git"
 
     maintainers = ['ax3l']
 
     version('develop', branch='master')
+    version('2.0.0.rc1', sha256='c30b72af5446f052bad1791000e5a44d156c96b0e39b4bc8e9f97a013c7d1b69')
 
     variant('avx2', default=True, description='Enable AVX2 support')
 
