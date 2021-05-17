c7bf227af8d7acabac0ba9e8ce1c233d8813521d
Andreas Baumbach <healther.astro@gmail.com>
2021-05-17 19:38:41 +0200

    fixup
    Change-Id: I551888c25ca7bc0b738c40d63d12974510b59ecf

--- var/spack/repos/builtin/packages/py-alembic/package.py
+++ var/spack/repos/builtin/packages/py-alembic/package.py
@@ -14,6 +14,7 @@ class PyAlembic(PythonPackage):
     version('1.5.5', sha256='df0028c19275a2cff137e39617a39cdcdbd1173733b87b6bfa257b7c0860213b')
     version('1.0.7', sha256='16505782b229007ae905ef9e0ae6e880fddafa406f086ac7d442c1aaf712f8c2')
 
+    depends_on('python@2.7:2.8,3.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-sqlalchemy@1.3.0:', type=('build', 'run'), when='@1.5:')
     depends_on('py-sqlalchemy@1.1.0:', type=('build', 'run'), when='@:1.4.999')
