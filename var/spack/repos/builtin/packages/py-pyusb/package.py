7645ff9fd4464f5433917fbf1fda4bf4cbf7812f
Andreas Baumbach <healther.astro@gmail.com>
2021-05-18 10:48:29 +0200

    New package: py-pyusb
    Change-Id: I606127858b961b5841c60befc5a8353df0f9f38c

---
+++ var/spack/repos/builtin/packages/py-pyusb/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyPyusb(PythonPackage):
+    """Easy USB access from Python."""
+
+    homepage = "https://github.com/pyusb/pyusb"
+    pypi     = "pyusb/pyusb-1.0.2.tar.gz"
+
+    version('1.0.2', sha256='4e9b72cc4a4205ca64fbf1f3fff39a335512166c151ad103e55c8223ac147362')
+
+    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('libusb', type=('build', 'run'))
