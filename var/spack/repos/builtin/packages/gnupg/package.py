361e7f99144e4920796875ff391b2692f00188d7
Harmen Stoppels <harmenstoppels@gmail.com>
2021-05-19 23:39:14 +0200

    You can't use `@when` and `@run_after` together

--- var/spack/repos/builtin/packages/gnupg/package.py
+++ var/spack/repos/builtin/packages/gnupg/package.py
@@ -44,9 +44,9 @@ class Gnupg(AutotoolsPackage):
     depends_on('zlib')
 
     @run_after('install')
-    @when('@2.0:2.999')
     def add_gpg2_symlink(self):
-        symlink('gpg', join_path(self.prefix, 'bin', 'gpg2'))
+        if self.spec.satisfies("@2.0:2.999"):
+            symlink('gpg', self.prefix.bin.gpg2)
 
     def configure_args(self):
         args = [
