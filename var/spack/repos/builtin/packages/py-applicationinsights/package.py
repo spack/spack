e4494015df68dfc61540f3a323f036780a810abd
Adam J. Stewart <ajstewart426@gmail.com>
2021-05-25 17:27:43 -0500

    py-applicationinsights: fix import tests

--- var/spack/repos/builtin/packages/py-applicationinsights/package.py
+++ var/spack/repos/builtin/packages/py-applicationinsights/package.py
@@ -11,6 +11,14 @@ class PyApplicationinsights(PythonPackage):
     homepage = "https://github.com/Microsoft/ApplicationInsights-Python"
     pypi = "applicationinsights/applicationinsights-0.11.9.tar.gz"
 
+    # 'applicationinsights.django' requires 'django'
+    import_modules = [
+        'applicationinsights', 'applicationinsights.flask',
+        'applicationinsights.exceptions', 'applicationinsights.requests',
+        'applicationinsights.channel', 'applicationinsights.channel.contracts',
+        'applicationinsights.logging'
+    ]
+
     version('0.11.9', sha256='30a11aafacea34f8b160fbdc35254c9029c7e325267874e3c68f6bdbcd6ed2c3')
 
     depends_on('py-setuptools', type='build')
