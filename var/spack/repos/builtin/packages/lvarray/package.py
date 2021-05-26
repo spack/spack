e54b8d29a9f8cb259cd7ac0b3564c7851c1b7c1e
Benjamin Curtice Corbett <corbett5@llnl.gov>
2021-05-26 14:37:56 -0700

    Changed lvarray python dependency types.

--- var/spack/repos/builtin/packages/lvarray/package.py
+++ var/spack/repos/builtin/packages/lvarray/package.py
@@ -74,10 +74,9 @@ class Lvarray(CMakePackage, CudaPackage):
 
     depends_on('caliper', when='+caliper')
 
-    depends_on('python +shared +pic', when='+pylvarray')
-    depends_on('py-numpy@1.19:', when='+pylvarray')
-    depends_on('py-scipy@1.5.2:', when='+pylvarray')
-    depends_on('py-pip', when='+pylvarray')
+    depends_on('python +shared +pic', type=('build', 'link', 'run'), when='+pylvarray')
+    depends_on('py-numpy@1.19:', type=('build', 'link', 'run'), when='+pylvarray')
+    depends_on('py-scipy@1.5.2:', type=('build', 'run'), when='+pylvarray')
 
     depends_on('doxygen@1.8.13:', when='+docs', type='build')
     depends_on('py-sphinx@1.6.3:', when='+docs', type='build')
