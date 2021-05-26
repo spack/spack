d33d14b689c60f676dd957a1b4a523e63a044209
dorton21 <odesmond21@gmail.com>
2021-05-26 12:27:27 -0500

    Added version and deps for @0.4.0

--- var/spack/repos/builtin/packages/py-gql/package.py
+++ var/spack/repos/builtin/packages/py-gql/package.py
@@ -14,10 +14,17 @@ class PyGql(PythonPackage):
     url      = "https://github.com/graphql-python/gql/archive/v3.0.0a1.tar.gz"
 
     version('3.0.0a1', sha256='3254a6010464932e3700a8d225cf6e40a6983aaf5f279615504c8196a374daf9')
+    version('0.4.0', sha256='bd27c44b15b529fecdd80b07291dfd40264a2af937a75b41ff2be49a730e41d8',
+            deprecated=True)
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-aiohttp@3.6.2', type=('build', 'run'))
-    depends_on('py-graphql-core@3.1.0:3.1.999', type=('build', 'run'))
-    depends_on('py-requests@2.23:2.999', type=('build', 'run'))
-    depends_on('py-websockets@8.1:8.999', type=('build', 'run'))
-    depends_on('py-yarl@1.4:1.999', type=('build', 'run'))
+    depends_on('py-aiohttp@3.6.2', type=('build', 'run'), when='@3.0:')
+    depends_on('py-graphql-core@3.1.0:3.1.999', type=('build', 'run'), when='@3.0:')
+    depends_on('py-requests@2.23:2.999', type=('build', 'run'), when='@3.0:')
+    depends_on('py-websockets@8.1:8.999', type=('build', 'run'), when='@3.0:')
+    depends_on('py-yarl@1.4:1.999', type=('build', 'run'), when='@3.0:')
+
+    depends_on('py-graphql-core@2.0:2.9', type=('build', 'run'), when='@0.4.0')
+    depends_on('py-six@1.10.0:', type=('build', 'run'), when='@0.4.0')
+    depends_on('py-promise@2.0:3', type=('build', 'run'), when='@0.4.0')
+    depends_on('py-requests@2.12:2.999', type=('build', 'run'), when='@0.4.0')
