b120b516059cf201ab07ed6a50cb3a6617361ecc
Desmond Orton <odesmond21@gmail.com>
2021-05-26 18:13:31 -0600

    New package py-rx@3.2.0 (#23929)
    * New package py-rx@3.2.0
    
    * Removal of deps and added python constraint

---
+++ var/spack/repos/builtin/packages/py-rx/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyRx(PythonPackage):
+    """Reactive Extensions (Rx) for Python"""
+
+    homepage = "http://reactivex.io/"
+    pypi     = "Rx/Rx-3.2.0.tar.gz"
+
+    maintainers = ['dorton21']
+
+    version('3.2.0', sha256='b657ca2b45aa485da2f7dcfd09fac2e554f7ac51ff3c2f8f2ff962ecd963d91c')
+    version('1.6.1', sha256='13a1d8d9e252625c173dc795471e614eadfe1cf40ffc684e08b8fff0d9748c23')
+
+    depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
