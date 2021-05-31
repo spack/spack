1a4128ab8c68f2d868d4daa096530a51819feeed
Manuela Kuhn <manuela.kuhn@ovgu.de>
2021-05-31 21:13:50 +0200

    py-simplejson: add 3.17.2
    Also fix python version dependency.

--- var/spack/repos/builtin/packages/py-simplejson/package.py
+++ var/spack/repos/builtin/packages/py-simplejson/package.py
@@ -13,6 +13,7 @@ class PySimplejson(PythonPackage):
     homepage = "https://github.com/simplejson/simplejson"
     pypi = "simplejson/simplejson-3.10.0.tar.gz"
 
+    version('3.17.2', sha256='75ecc79f26d99222a084fbdd1ce5aad3ac3a8bd535cd9059528452da38b68841')
     version('3.16.0', sha256='b1f329139ba647a9548aa05fb95d046b4a677643070dc2afc05fa2e975d09ca5')
     version('3.10.0', sha256='953be622e88323c6f43fad61ffd05bebe73b9fd9863a46d68b052d2aa7d71ce2')
     version('3.9.0',  sha256='e9abeee37424f4bfcd27d001d943582fb8c729ffc0b74b72bd0e9b626ed0d1b6')
@@ -20,4 +21,5 @@ class PySimplejson(PythonPackage):
     version('3.8.1',  sha256='428ac8f3219c78fb04ce05895d5dff9bd813c05a9a7922c53dc879cd32a12493')
     version('3.8.0',  sha256='217e4797da3a9a4a9fbe6722e0db98070b8443a88212d7acdbd241a7668141d9')
 
+    depends_on('python@2.5:2.8,3.3:', type=('build', 'run'), when='@3.16.0:')
     depends_on('py-setuptools', type='build')
