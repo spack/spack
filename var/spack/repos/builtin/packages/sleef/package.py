2f5afec98786da9f11983c85ba6ca22e960d8412
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2021-05-31 12:15:01 +0200

    Update SLEEF with new version (#1179)

--- var/spack/repos/builtin/packages/sleef/package.py
+++ var/spack/repos/builtin/packages/sleef/package.py
@@ -13,4 +13,6 @@ class Sleef(CMakePackage):
     homepage = "http://sleef.org"
     url      = "https://github.com/shibatch/sleef/archive/3.2.tar.gz"
 
+    version('3.5.1', sha256='415ee9b1bcc5816989d3d4d92afd0cd3f9ee89cbd5a33eb008e69751e40438ab')
+    version('3.5.0', sha256='6b952560cec091477affcb18baf06bf50cef9f932ff6aba491a744ee8e77ffea')
     version('3.2', sha256='3130c5966e204e6d6a3ace81e543d12b5b21f60897f1c185bfa587c1bd77bee2')
