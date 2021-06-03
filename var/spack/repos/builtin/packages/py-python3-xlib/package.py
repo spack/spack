f28fdf059bcba6833e0c9cc22dad1542fa32a911
Jen Herting <jen@herting.cc>
2021-06-03 16:29:23 -0400

    [py-python3-xlib] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-python3-xlib/package.py
+++ var/spack/repos/builtin/packages/py-python3-xlib/package.py
@@ -3,48 +3,15 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-python3-xlib
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-python3-xlib
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyPython3Xlib(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """python3-xlib is python3 version of python-xlib."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://www.example.com"
     pypi     = "python3-xlib/python3-xlib-0.15.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('0.15', sha256='dc4245f3ae4aa5949c1d112ee4723901ade37a96721ba9645f2bfa56e5b383f8')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@3:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
