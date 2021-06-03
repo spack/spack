7e168b8535e0b44aeee32f3cb8c76d6c189c26a3
Desmond Orton <odesmond21@gmail.com>
2021-06-03 04:40:45 -0500

    py-gsutil: new package (#23970)

---
+++ var/spack/repos/builtin/packages/py-gsutil/package.py
@@ -0,0 +1,32 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyGsutil(PythonPackage):
+    """A command line tool for interacting with cloud storage services."""
+
+    homepage = "https://cloud.google.com/storage/docs/gsutil"
+    pypi     = "gsutil/gsutil-4.59.tar.gz"
+
+    maintainers = ['dorton21']
+
+    version('4.59', sha256='349e0e0b48c281659acec205917530ae57e2eb23db7220375f5add44688d3ddf')
+
+    depends_on('python@2.7:2.8,3.5:3.999', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-argcomplete@1.9.4:', type=('build', 'run'))
+    depends_on('py-crcmod@1.7:', type=('build', 'run'))
+    depends_on('py-fasteners@0.14.1:', type=('build', 'run'))
+    depends_on('py-gcs-oauth2-boto-plugin@2.7:', type=('build', 'run'))
+    depends_on('py-google-apitools@0.5.32:', type=('build', 'run'))
+    depends_on('py-httplib2@0.18:', type=('build', 'run'))
+    depends_on('py-google-auth@0.1.0:', type=('build', 'run'))
+    depends_on('py-mock@2.0.0', type=('build', 'run'))
+    depends_on('py-monotonic@1.4:', type=('build', 'run'))
+    depends_on('py-pyopenssl@0.13:', type=('build', 'run'))
+    depends_on('py-retry-decorator@1.0.0:', type=('build', 'run'))
+    depends_on('py-six@1.12.0:', type=('build', 'run'))
