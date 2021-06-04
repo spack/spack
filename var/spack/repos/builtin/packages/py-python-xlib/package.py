f081883eceb20961c01e6f9e62b9f3fa3c7aead7
Jen Herting <jen@herting.cc>
2021-06-04 17:21:13 -0400

    [py-python-xlib] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-python-xlib/package.py
+++ var/spack/repos/builtin/packages/py-python-xlib/package.py
@@ -3,51 +3,22 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-python-xlib
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-python-xlib
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyPythonXlib(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """The Python X Library is intended to be a fully
+    functional X client library for Python programs. It is
+    written entirely in Python, in contrast to earlier X
+    libraries for Python (the ancient X extension and the newer
+    plxlib) which were interfaces to the C Xlib."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/python-xlib/python-xlib"
     pypi     = "python-xlib/python-xlib-0.30.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.30', sha256='74131418faf9e7b83178c71d9d80297fbbd678abe99ae9258f5a20cd027acb5f')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
     depends_on('python@2.7,3.3:3.6', type=('build', 'run'))
     depends_on('py-setuptools@30.3.0:', type='build')
     depends_on('py-setuptools-scm', type='build')
     depends_on('py-six@1.10.0:', type=('build', 'run'))
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
