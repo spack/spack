8227d5722cfcd26101bcb2b4fd0a56b94e499d96
Jen Herting <jen@herting.cc>
2021-06-10 15:58:43 -0400

    [py-pyscreeze] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-pyscreeze/package.py
+++ var/spack/repos/builtin/packages/py-pyscreeze/package.py
@@ -3,50 +3,21 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-pyscreeze
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-pyscreeze
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyPyscreeze(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """PyScreeze can take screenshots, save them to files, and
+    locate images within the screen. This is useful if you have
+    a small image of, say, a button that needs to be clicked
+    and want to locate it on the screen."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/asweigart/pyscreeze"
     pypi     = "PyScreeze/PyScreeze-0.1.27.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.1.27', sha256='cba2f264fe4b6c70510061cb2ba6e1da0e3bfecfdbe8a3b2cd6305a2afda9e6b')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@2.7:2.999,3.2:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-pillow', type=('build', 'run'))
     depends_on('scrot', type='run')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
