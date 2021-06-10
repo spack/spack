43023749982d3a16c0544bbd41053dcc8a747d5d
Jen Herting <jen@herting.cc>
2021-06-10 15:53:12 -0400

    [scrot] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/scrot/package.py
+++ var/spack/repos/builtin/packages/scrot/package.py
@@ -3,41 +3,20 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install scrot
-#
-# You can edit this file again by typing:
-#
-#     spack edit scrot
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Scrot(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+    """scrot (SCReenshOT) is a simple command line screen
+    capture utility that uses imlib2 to grab and save images.
+    Multiple image formats are supported through imlib2's
+    dynamic saver modules."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/resurrecting-open-source-projects/scrot"
     url      = "https://github.com/resurrecting-open-source-projects/scrot/archive/refs/tags/1.5.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.5', sha256='87afba3998aac097f13231f3b0452c21188bf4b5cc6ac0747693a1da1a0ae40f')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('autoconf-archive', type='build')
     depends_on('automake', type='build')
     depends_on('giblib', when=':1.5')  # @master already has this dependency removed
@@ -45,9 +24,3 @@ class Scrot(AutotoolsPackage):
     depends_on('libtool')
     depends_on('libxcomposite')
     depends_on('libxfixes')
-
-    def configure_args(self):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
