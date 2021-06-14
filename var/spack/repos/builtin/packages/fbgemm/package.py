0f472920f307d02361faf0d0198665f38a2df26c
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-14 23:26:30 +0000

    FBGEMM: add new package

---
+++ var/spack/repos/builtin/packages/fbgemm/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Fbgemm(CMakePackage):
+    """FBGEMM (Facebook GEneral Matrix Multiplication) is a low-precision,
+    high-performance matrix-matrix multiplications and convolution library
+    for server-side inference."""
+
+    homepage = "https://github.com/pytorch/FBGEMM"
+    git      = "https://github.com/pytorch/FBGEMM.git"
+
+    version('master', branch='master', submodules=True)
+
+    depends_on('cmake@3.5:', type='build')
+    depends_on('python', type='build')
+    depends_on('llvm-openmp', when='%apple-clang')
