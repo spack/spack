e0f8abc877f957c36e0b40ed758268d12483c3be
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-06-15 13:04:37 +0200

    Test the "packages" subcommand at a finer level

--- lib/spack/spack/test/cmd/audit.py
+++ lib/spack/spack/test/cmd/audit.py
@@ -2,19 +2,29 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
+import pytest
+
 from spack.main import SpackCommand
 
 
 audit = SpackCommand('audit')
 
 
-def test_audit_packages(mutable_config, mock_packages):
+@pytest.mark.parametrize('pkgs,expected_returncode', [
+    # A single package with issues, should exit 1
+    (['wrong-variant-in-conflicts'], 1),
+    # A "sane" package should exit 0
+    (['mpileaks'], 0),
+    # A package with issues and a package without should exit 1
+    (['wrong-variant-in-conflicts', 'mpileaks'], 1),
+    (['mpileaks', 'wrong-variant-in-conflicts'], 1),
+])
+def test_audit_packages(
+        pkgs, expected_returncode, mutable_config, mock_packages
+):
     """Sanity check ``spack audit packages`` to make sure it works."""
-    audit('packages', fail_on_error=False)
-    # Some mock packages have issues by design, to check what would
-    # be Spack behavior in those cases. Ensure that the command returns
-    # non-zero exit code
-    assert audit.returncode == 1
+    audit('packages', *pkgs, fail_on_error=False)
+    assert audit.returncode == expected_returncode
 
 
 def test_audit_configs(mutable_config, mock_packages):
