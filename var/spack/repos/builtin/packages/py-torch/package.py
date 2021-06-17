bb8154426e841a3484ac8df0dfe2702c5f2d6bea
Adam J. Stewart <ajstewart426@gmail.com>
2021-06-17 19:07:55 +0000

    System libs don't work for many submodules

--- var/spack/repos/builtin/packages/py-torch/package.py
+++ var/spack/repos/builtin/packages/py-torch/package.py
@@ -32,7 +32,7 @@ class PyTorch(PythonPackage, CudaPackage):
     version('1.5.1', tag='v1.5.1', submodules=True)
     version('1.5.0', tag='v1.5.0', submodules=True)
     version('1.4.1', tag='v1.4.1', submodules=True)
-    version('1.4.0', tag='v1.4.0', submodules=True,
+    version('1.4.0', tag='v1.4.0', submodules=True, deprecated=True,
             submodules_delete=['third_party/fbgemm'])
     version('1.3.1', tag='v1.3.1', submodules=True)
     version('1.3.0', tag='v1.3.0', submodules=True)
@@ -40,10 +40,10 @@ class PyTorch(PythonPackage, CudaPackage):
     version('1.1.0', tag='v1.1.0', submodules=True)
     version('1.0.1', tag='v1.0.1', submodules=True)
     version('1.0.0', tag='v1.0.0', submodules=True)
-    version('0.4.1', tag='v0.4.1', submodules=True,
+    version('0.4.1', tag='v0.4.1', submodules=True, deprecated=True,
             submodules_delete=['third_party/nervanagpu'])
-    version('0.4.0', tag='v0.4.0', submodules=True)
-    version('0.3.1', tag='v0.3.1', submodules=True)
+    version('0.4.0', tag='v0.4.0', submodules=True, deprecated=True)
+    version('0.3.1', tag='v0.3.1', submodules=True, deprecated=True)
 
     is_darwin = sys.platform == 'darwin'
 
@@ -117,6 +117,7 @@ class PyTorch(PythonPackage, CudaPackage):
     depends_on('py-tqdm', type='run')
     depends_on('blas')
     depends_on('lapack')
+    depends_on('sleef', when='@1.0:')
     depends_on('protobuf', when='@0.4:')
     depends_on('py-protobuf', when='@0.4:', type=('build', 'run'))
     depends_on('eigen', when='@0.4:')
@@ -126,46 +127,20 @@ class PyTorch(PythonPackage, CudaPackage):
     depends_on('cuda@9:', when='@1.1:+cuda', type=('build', 'link', 'run'))
     depends_on('cudnn@6:', when='+cudnn')
     depends_on('cudnn@7:', when='@1.1:+cudnn')
-    depends_on('fbgemm@master', when='@master+fbgemm')
-    depends_on('fbgemm@2021-05-10', when='@1.9.0:1.9.999+fbgemm')
-    depends_on('fbgemm@2020-11-13', when='@1.8.0:1.8.999+fbgemm')
-    depends_on('fbgemm@2020-09-14', when='@1.7.0:1.7.999+fbgemm')
-    depends_on('fbgemm@2020-05-31', when='@1.6.0:1.6.999+fbgemm')
-    depends_on('fbgemm@2020-05-21', when='@1.5.1+fbgemm')
-    depends_on('fbgemm@2020-03-22', when='@1.5.0+fbgemm')
-    depends_on('fbgemm@2019-11-20', when='@1.4.0:1.4.999+fbgemm')
-    depends_on('fbgemm@2019-09-26', when='@1.3.0:1.3.999+fbgemm')
-    depends_on('fbgemm@2019-07-22', when='@1.2.0:1.2.999+fbgemm')
-    depends_on('fbgemm@2019-04-18', when='@1.1.0:1.1.999+fbgemm')
-    depends_on('fbgemm@2019-01-23', when='@1.0.1+fbgemm')
-    depends_on('fbgemm@2018-12-04', when='@1.0.0+fbgemm')
-    depends_on('kineto@master', when='@master+kineto')
-    depends_on('kineto@2021-05-12', when='@1.9.0+kineto')
-    depends_on('kineto@2021-03-16', when='@1.8.1+kineto')
-    depends_on('kineto@2021-02-04', when='@1.8.0+kineto')
     depends_on('magma', when='+magma')
     depends_on('nccl', when='+nccl')
-    depends_on('nnpack@master', when='@master+nnpack')
-    depends_on('nnpack@2020-12-21', when='@1.8.0:1.9.999+nnpack')
-    depends_on('nnpack@2019-10-07', when='@1.4.0:1.7.999+nnpack')
-    depends_on('nnpack@2019-03-23', when='@1.1.0:1.3.999+nnpack')
-    depends_on('nnpack@2018-09-03', when='@1.0.0:1.0.999+nnpack')
-    depends_on('nnpack@2018-05-21', when='@0.4.1+nnpack')
-    depends_on('nnpack@2018-04-05', when='@:0.4.0+nnpack')
     depends_on('numactl', when='+numa')
     depends_on('py-numpy', when='+numpy', type=('build', 'run'))
     depends_on('llvm-openmp', when='%apple-clang +openmp')
-    depends_on('qnnpack@master', when='@master+qnnpack')
-    depends_on('qnnpack@2019-08-28', when='@1.3.0:1.9.999+qnnpack')
-    depends_on('qnnpack@2018-12-27', when='@1.2.0:1.2.999+qnnpack')
-    depends_on('qnnpack@2018-12-04', when='@1.0.0:1.1.999+qnnpack')
     depends_on('valgrind', when='+valgrind')
+    depends_on('xnnpack', when='+xnnpack')
     depends_on('xnnpack@master', when='@master+xnnpack')
     depends_on('xnnpack@2021-02-22', when='@1.8.0:1.9.999+xnnpack')
     depends_on('xnnpack@2020-03-23', when='@1.6.0:1.7.999+xnnpack')
     depends_on('xnnpack@2020-02-24', when='@1.5.0:1.5.999+xnnpack')
-    depends_on('onednn', when='+mkldnn')
     depends_on('mpi', when='+mpi')
+    depends_on('gloo', when='+gloo')
+    depends_on('gloo+cuda', when='+gloo+cuda')
     depends_on('gloo@master', when='@master+gloo')
     depends_on('gloo@2021-05-04', when='@1.9.0:1.9.999+gloo')
     depends_on('gloo@2020-09-18', when='@1.7.0:1.8.999+gloo')
@@ -177,12 +152,6 @@ class PyTorch(PythonPackage, CudaPackage):
     depends_on('gloo@2018-11-20', when='@1.0.0+gloo')
     depends_on('gloo@2018-05-29', when='@0.4.1+gloo')
     depends_on('gloo@2018-04-06', when='@:0.4.0+gloo')
-    depends_on('tensorpipe@master', when='@master+tensorpipe')
-    depends_on('tensorpipe@2021-05-13', when='@1.9.0:1.9.999+tensorpipe')
-    depends_on('tensorpipe@2021-03-04', when='@1.8.1+tensorpipe')
-    depends_on('tensorpipe@2021-02-09', when='@1.8.0+tensorpipe')
-    depends_on('tensorpipe@2020-09-28', when='@1.7.0:1.7.999+tensorpipe')
-    depends_on('tensorpipe@2020-06-26', when='@1.6.0:1.6.999+tensorpipe')
     depends_on('onnx', when='+onnx_ml')
 
     # Test dependencies
@@ -251,7 +220,7 @@ class PyTorch(PythonPackage, CudaPackage):
         most flags defined in ``CMakeLists.txt`` can be specified as
         environment variables.
         """
-        def enable_or_disable(variant, keyword='USE', newer=False):
+        def enable_or_disable(variant, keyword='USE', var=None, newer=False):
             """Set environment variable to enable or disable support for a
             particular variant.
 
@@ -260,7 +229,8 @@ class PyTorch(PythonPackage, CudaPackage):
                 keyword (str): the prefix to use for enabling/disabling
                 newer (bool): newer variants that never used NO_*
             """
-            var = variant.upper()
+            if var is None:
+                var = variant.upper()
 
             # Version 1.1.0 switched from NO_* to USE_* or BUILD_*
             # But some newer variants have always used USE_* or BUILD_*
@@ -287,7 +257,6 @@ class PyTorch(PythonPackage, CudaPackage):
         if '+cuda' in self.spec:
             # cmake/public/cuda.cmake
             # cmake/Modules_CUDA_fix/upstream/FindCUDA.cmake
-            env.set('CUDA_HOME', self.spec['cuda'].prefix)
             torch_cuda_arch = ';'.join('{0:.1f}'.format(float(i) / 10.0) for i
                                        in
                                        self.spec.variants['cuda_arch'].value)
@@ -298,7 +267,6 @@ class PyTorch(PythonPackage, CudaPackage):
         enable_or_disable('cudnn')
         if '+cudnn' in self.spec:
             # cmake/Modules_CUDA_fix/FindCUDNN.cmake
-            env.set('CUDNN_ROOT', self.spec['cudnn'].prefix)
             env.set('CUDNN_INCLUDE_DIR', self.spec['cudnn'].prefix.include)
             env.set('CUDNN_LIBRARY', self.spec['cudnn'].libs[0])
 
@@ -309,7 +277,6 @@ class PyTorch(PythonPackage, CudaPackage):
 
         enable_or_disable('nccl')
         if '+nccl' in self.spec:
-            env.set('NCCL_ROOT', self.spec['nccl'].prefix)
             env.set('NCCL_LIB_DIR', self.spec['nccl'].libs.directories[0])
             env.set('NCCL_INCLUDE_DIR', self.spec['nccl'].prefix.include)
 
@@ -326,21 +293,20 @@ class PyTorch(PythonPackage, CudaPackage):
         # cmake/Modules/FindOpenMP.cmake
         enable_or_disable('openmp', newer=True)
         enable_or_disable('qnnpack')
+        enable_or_disable('qnnpack', var='PYTORCH_QNNPACK')
         enable_or_disable('valgrind')
         enable_or_disable('xnnpack')
-
         enable_or_disable('mkldnn')
-        if '+mkldnn' in self.spec:
-            # cmake/public/mkldnn.cmake
-            # cmake/Modules/FindMKLDNN.cmake
-            env.set('MKLDNN_HOME', self.spec['onednn'].prefix)
-
         enable_or_disable('distributed')
         enable_or_disable('mpi')
         # cmake/Modules/FindGloo.cmake
         enable_or_disable('gloo', newer=True)
         enable_or_disable('tensorpipe')
-        enable_or_disable('onnx_ml')
+
+        if '+onnx_ml' in self.spec:
+            env.set('ONNX_ML', 'ON')
+        else:
+            env.set('ONNX_ML', 'OFF')
 
         if not self.spec.satisfies('@master'):
             env.set('PYTORCH_BUILD_VERSION', self.version)
@@ -362,7 +328,6 @@ class PyTorch(PythonPackage, CudaPackage):
 
         # Don't use vendored third-party libraries
         env.set('BUILD_CUSTOM_PROTOBUF', 'OFF')
-        env.set('USE_PYTORCH_QNNPACK', 'OFF')
         env.set('USE_SYSTEM_NCCL', 'ON')
         env.set('USE_SYSTEM_EIGEN_INSTALL', 'ON')
         env.set('USE_SYSTEM_LIBS', 'ON')
