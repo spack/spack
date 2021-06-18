b31e2533f5f7e10d5a43d6ed9e3687367b3f18dd
Alec Scott <alecbcs@gmail.com>
2021-06-18 11:43:15 -0700

    Update package.py

--- var/spack/repos/builtin/packages/abyss/package.py
+++ var/spack/repos/builtin/packages/abyss/package.py
@@ -1,7 +1,7 @@
 # Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)  
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import numbers
 from spack import *
