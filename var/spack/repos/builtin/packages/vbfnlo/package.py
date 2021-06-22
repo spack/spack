c54b8d18a628590d0aad6ca5df57f310b4f0de3d
Thomas Madlener <thomas.madlener@desy.de>
2021-06-22 11:45:28 +0200

    [vbfnlo] Add doc variant to toggle building of docs

--- var/spack/repos/builtin/packages/vbfnlo/package.py
+++ var/spack/repos/builtin/packages/vbfnlo/package.py
@@ -26,6 +26,12 @@ class Vbfnlo(AutotoolsPackage):
     version('2.7.1',      sha256='13e33d73d8a8ef64094621f87e6f94e01712e76cc19a86298d0b52cfcb9decca', preferred=True)
     # version('2.7.0',      sha256='0e96c0912599e3000fffec5305700b947b604a7b06c7975851503f445311e4ef')
 
+    # Documentation is broken on some systems:
+    # See https://github.com/vbfnlo/vbfnlo/issues/2
+    variant('doc', default=False,
+            description='Build documentation')
+    patch('vbfnlo_no_docs.patch', when='~doc')
+
     depends_on('hepmc')
     depends_on('gsl')
     depends_on('lhapdf')
