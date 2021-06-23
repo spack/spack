dc903366be75c2ee84f9c8262c9c31e5e78afeea
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-06-23 09:54:17 +0200

    Make python-dateutil a soft dependency for unit tests
    Before #23212 people could clone spack and run
    ```
    spack unit-tests
    ```
    while now this is not possible, since python-dateutil is
    a required but not vendored dependency. This change makes
    it not a hard requirement, i.e. it will be used if found
    in the current interpreter.

--- lib/spack/spack/test/conftest.py
+++ lib/spack/spack/test/conftest.py
@@ -12,20 +12,19 @@ import os
 import os.path
 import re
 import shutil
-import sys
 import tempfile
 import xml.etree.ElementTree
 
-if sys.version_info >= (2, 7):
+try:
     # CVS outputs dates in different formats on different systems. We are using
     # the dateutil package to parse these dates. This package does not exist
     # for Python <2.7. That means that we cannot test checkouts "by date" for
     # CVS respositories. (We can still use CVS repos with all features, only
     # our tests break.)
     from dateutil.parser import parse as parse_date
-else:
+except ImportError:
     def parse_date(string):
-        pytest.skip("dateutil package not available for Python 2.6")
+        pytest.skip("dateutil package not available")
 
 import py
 import pytest
