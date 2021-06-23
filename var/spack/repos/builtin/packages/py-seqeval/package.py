bd60a693832fd36c29dccc90302fc82cf6814578
Jen Herting <jen@herting.cc>
2021-06-23 11:30:19 -0400

    [py-seqeval] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-seqeval/package.py
+++ var/spack/repos/builtin/packages/py-seqeval/package.py
@@ -3,36 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-seqeval
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-seqeval
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PySeqeval(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """seqeval is a Python framework for sequence labeling
+    evaluation. seqeval can evaluate the performance of
+    chunking tasks such as named-entity recognition,
+    part-of-speech tagging, semantic role labeling and so on."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/chakki-works/seqeval"
     pypi     = "seqeval/seqeval-1.2.2.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.2.2', sha256='f28e97c3ab96d6fcd32b648f6438ff2e09cfba87f05939da9b3970713ec56e6f')
 
@@ -40,9 +21,3 @@ class PySeqeval(PythonPackage):
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy@1.14:', type=('build', 'run'))
     depends_on('py-scikit-learn@0.21.3:', type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
