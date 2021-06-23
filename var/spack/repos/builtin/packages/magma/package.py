5e4c1a37354ecbc95962c8c42ef7a06f7e02145e
G-Ragghianti <gerald@ragghianti.com>
2021-06-23 17:19:28 +0000

    Changed rocblas to hipblas

--- var/spack/repos/builtin/packages/magma/package.py
+++ var/spack/repos/builtin/packages/magma/package.py
@@ -41,7 +41,7 @@ class Magma(CMakePackage, CudaPackage, ROCmPackage):
     depends_on('blas')
     depends_on('lapack')
     depends_on('cuda@8:', when='@2.5.1:')  # See PR #14471
-    depends_on('rocblas', when='+rocm')
+    depends_on('hipblas', when='+rocm')
 
     conflicts('~cuda', when='~rocm', msg='Either CUDA or HIP support must be enabled')
     conflicts('+rocm', when='+cuda', msg='CUDA must be disabled to support HIP (ROCm)')
