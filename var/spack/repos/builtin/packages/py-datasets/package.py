f3c9a5f36854d90c6c9be4d71a1e20bf40fed019
Jen Herting <jen@herting.cc>
2021-06-23 12:59:04 -0400

    [py-datasets] added dependencies

--- var/spack/repos/builtin/packages/py-datasets/package.py
+++ var/spack/repos/builtin/packages/py-datasets/package.py
@@ -36,12 +36,23 @@ class PyDatasets(PythonPackage):
 
     version('1.8.0', sha256='d57c32bb29e453ee7f3eb0bbca3660ab4dd2d0e4648efcfa987432624cab29d3')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
+    depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-numpy@1.17:', type=('build', 'run'))
+    depends_on('py-pyarrow@1.0.0:3.999', type=('build', 'run'))
+    depends_on('py-dill', type=('build', 'run'))
+    depends_on('py-pandas', type=('build', 'run'))
+    depends_on('py-requests@2.19:', type=('build', 'run'))
+    depends_on('py-tqdm@4.27:4.49.999', type=('build', 'run'))
+    depends_on('py-dataclasses', when='^python@:3.6.999', type=('build', 'run'))
+    depends_on('py-xxhash', type=('build', 'run'))
+    depends_on('py-multiprocess', type=('build', 'run'))
+    depends_on('py-importlib-metadata', when='^python@:3.7.999', type=('build', 'run'))
+    depends_on('py-huggingface-hub@:0.0.999', type=('build', 'run'))
+    depends_on('py-packaging', type=('build', 'run'))
+
+    depends_on('py-fsspec@:0.8.0', when='^python@:3.7.999', type=('build', 'run'))
+    depends_on('py-fsspec', when='^python@3.8:', type=('build', 'run'))
 
     def build_args(self, spec, prefix):
         # FIXME: Add arguments other than --prefix
