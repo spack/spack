e0c4df3cbfc7ba9655acb5d427a50f294a632c07
Kayla Richarda Butler <butler59@quartz2498.llnl.gov>
2021-06-24 14:45:33 -0700

    Remove unneeded variables

--- var/spack/repos/builtin/packages/aml/package.py
+++ var/spack/repos/builtin/packages/aml/package.py
@@ -23,3 +23,41 @@ class Aml(AutotoolsPackage):
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
     depends_on('libtool', type='build')
+
+    @run_after('install')
+    def cache_test_sources(self):
+        """Copy the example source files after the package is installed to an
+        install test subdirectory for use during `spack test run`."""
+        self.cache_extra_test_sources(['tests', 'include/config.h'])
+
+    def run_test(self):
+        """Run stand alone test: test_area"""
+
+        test_dir = join_path(self.prefix, '.spack/test/tests/area')
+
+        if not os.path.exists(test_dir):
+            print('Skipping aml test')
+            return
+
+        exe = 'test_area'
+
+        self.run_test('gcc',
+                      options=['-o', exe, join_path(test_dir, 'test_area.c'),
+                               '-I{0}'.format(join_path(self.prefix,
+                                                        '.spack/test/include')),
+                               '-I{0}'.format(self.prefix.include),
+                               '-I{0}'.format(self.spec['numactl'].prefix.include),
+                               '-L{0}'.format(self.prefix.lib),
+                               '-laml', '-lexcit', '-lpthread'],
+                      purpose='test: compile {0} example'.format(exe),
+                      work_dir=test_dir)
+
+        self.run_test(exe,
+                      options=[],
+                      expected=[0],
+                      purpose='test: run {0} example'.format(exe),
+                      work_dir=test_dir)
+        print(other)
+
+    def test(self):
+        self.run_test()
