691840c71bd6b66f1c4376fbca7549c9e2f3b3f5
snehring <snehring@iastate.edu>
2021-06-25 13:51:02 -0500

    bwa: adding string=True to filter_file

--- var/spack/repos/builtin/packages/bwa/package.py
+++ var/spack/repos/builtin/packages/bwa/package.py
@@ -40,8 +40,9 @@ class Bwa(Package):
         filter_file('^CC=.*', 'CC={0}'.format(spack_cc), 'Makefile')
         # fix gcc 10+ errors
         if self.spec.satisfies('%gcc@10:'):
-            filter_file(r'const uint8_t rle_auxtab\[8\]',
-                        'extern const uint8_t rle_auxtab[8]', 'rle.h')
+            filter_file('const uint8_t rle_auxtab[8]',
+                        'extern const uint8_t rle_auxtab[8]',
+                        'rle.h', string=True)
         make()
 
         mkdirp(prefix.bin)
