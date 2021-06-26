4c149ade7f60fa01f1ebf98d79ac8b763c0eb903
Alec Scott <alec@bcs.sh>
2021-06-26 04:07:13 -0700

    accumulo: add v2.0.1 (#24409)

--- var/spack/repos/builtin/packages/accumulo/package.py
+++ var/spack/repos/builtin/packages/accumulo/package.py
@@ -9,8 +9,9 @@ class Accumulo(MavenPackage):
     provides robust, scalable data storage and retrieval."""
 
     homepage = "https://accumulo.apache.org/"
-    url      = "https://github.com/apache/accumulo/archive/rel/2.0.0.tar.gz"
+    url      = "https://github.com/apache/accumulo/archive/rel/2.0.1.tar.gz"
 
+    version('2.0.1', sha256='2756ac14e850b30ad9bd1043418d621b93307d083f84904cd8fac5c8beec751b')
     version('2.0.0', sha256='2564056dc24398aa464763c21bae10ef09356fe3261600d27744071cf965c265')
     version('1.9.3', sha256='d9548d5b9cf9f494f027f0fe59d5d6d45d09064359d7761cade62991ce2a5d0c')
     version('1.9.2', sha256='11ab028143ad6313cd5fc701b36b4c35e46a4a3fa2ce663869860b9f6bf5ee4d')
