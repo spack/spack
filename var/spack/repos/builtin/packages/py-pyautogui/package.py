0846f901b4e6b05b441b538b9760513e018b6e14
Jen Herting <jen@herting.cc>
2021-06-28 16:06:26 -0400

    [py-pyautogui] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-pyautogui/package.py
+++ var/spack/repos/builtin/packages/py-pyautogui/package.py
@@ -3,53 +3,22 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-pyautogui
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-pyautogui
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyPyautogui(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """PyAutoGUI lets your Python scripts control the mouse and
+    keyboard to automate interactions with other
+    applications."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://pyautogui.readthedocs.io/en/latest/"
     pypi     = "PyAutoGUI/PyAutoGUI-0.9.52.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.9.52', sha256='a486cb6b818bcbcdf98b48d010c7cee964134fa394b756e8ce6e50d43b58ecc8')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-pymsgbox', type=('build', 'run'))
     depends_on('py-pytweening@1.0.1:', type=('build', 'run'))
     depends_on('py-pyscreeze@0.1.21:', type=('build', 'run'))
     depends_on('py-pygetwindow@0.0.5:', type=('build', 'run'))
     depends_on('py-mouseinfo', type=('build', 'run'))
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
