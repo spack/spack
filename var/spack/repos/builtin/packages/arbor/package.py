82b21a174291b93f832b9d829ed3001f93f773fe
Brent Huisman <brent@huisman.pl>
2021-06-28 16:23:51 +0200

    flake8 passing

--- var/spack/repos/builtin/packages/arbor/package.py
+++ var/spack/repos/builtin/packages/arbor/package.py
@@ -73,8 +73,9 @@ class Arbor(CMakePackage, CudaPackage):
 
         # query spack for the architecture-specific compiler flags set by its wrapper
         args.append('-DARB_ARCH=none')
-        opt_flags = self.spec.target.optimization_flags(self.spec.compiler.name,
-                    self.spec.compiler.version)
+        opt_flags = self.spec.target.optimization_flags(
+            self.spec.compiler.name,
+            self.spec.compiler.version)
         args.append('-DARB_CXX_FLAGS_TARGET=' + opt_flags)
 
         return args
