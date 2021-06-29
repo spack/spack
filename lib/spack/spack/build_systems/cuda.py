bb4ddbed8d29a7ad6ad767998de07e197ec9c517
David Beckingsale <davidbeckingsale@gmail.com>
2021-06-29 14:16:02 -0700

    Move variant to CUDA package

--- lib/spack/spack/build_systems/cuda.py
+++ lib/spack/spack/build_systems/cuda.py
@@ -38,9 +38,6 @@ class CudaPackage(PackageBase):
             description='CUDA architecture',
             values=spack.variant.any_combination_of(*cuda_arch_values))
 
-    variant('allow-unsupported-compilers', default=False,
-            description='Allow unsupported host compiler and CUDA version combinations')
-
     # https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html#nvcc-examples
     # https://llvm.org/docs/CompileCudaWithLLVM.html#compiling-cuda-code
     @staticmethod
@@ -91,37 +88,37 @@ class CudaPackage(PackageBase):
 
     # Linux x86_64 compiler conflicts from here:
     # https://gist.github.com/ax3l/9489132
-    conflicts('%gcc@5:', when='+cuda ~allow-unsupported-compilers ^cuda@:7.5 target=x86_64:')
-    conflicts('%gcc@6:', when='+cuda ~allow-unsupported-compilers ^cuda@:8 target=x86_64:')
-    conflicts('%gcc@7:', when='+cuda ~allow-unsupported-compilers ^cuda@:9.1 target=x86_64:')
-    conflicts('%gcc@8:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.0.130 target=x86_64:')
-    conflicts('%gcc@9:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.2.89 target=x86_64:')
-    conflicts('%gcc@:4', when='+cuda ~allow-unsupported-compilers ^cuda@11.0.2: target=x86_64:')
-    conflicts('%gcc@10:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.0.2 target=x86_64:')
-    conflicts('%gcc@11:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.1.0 target=x86_64:')
-    conflicts('%pgi@:14.8', when='+cuda ~allow-unsupported-compilers ^cuda@:7.0.27 target=x86_64:')
-    conflicts('%pgi@:15.3,15.5:', when='+cuda ~allow-unsupported-compilers ^cuda@7.5 target=x86_64:')
-    conflicts('%pgi@:16.2,16.0:16.3', when='+cuda ~allow-unsupported-compilers ^cuda@8 target=x86_64:')
-    conflicts('%pgi@:15,18:', when='+cuda ~allow-unsupported-compilers ^cuda@9.0:9.1 target=x86_64:')
-    conflicts('%pgi@:16,19:', when='+cuda ~allow-unsupported-compilers ^cuda@9.2.88:10 target=x86_64:')
+    conflicts('%gcc@5:', when='+cuda ^cuda@:7.5 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%gcc@6:', when='+cuda ^cuda@:8 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%gcc@7:', when='+cuda ^cuda@:9.1 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%gcc@8:', when='+cuda ^cuda@:10.0.130 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%gcc@9:', when='+cuda ^cuda@:10.2.89 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%gcc@:4', when='+cuda ^cuda@11.0.2: target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%gcc@10:', when='+cuda ^cuda@:11.0.2 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%gcc@11:', when='+cuda ^cuda@:11.1.0 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%pgi@:14.8', when='+cuda ^cuda@:7.0.27 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%pgi@:15.3,15.5:', when='+cuda ^cuda@7.5 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%pgi@:16.2,16.0:16.3', when='+cuda ^cuda@8 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%pgi@:15,18:', when='+cuda ^cuda@9.0:9.1 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%pgi@:16,19:', when='+cuda ^cuda@9.2.88:10 target=x86_64: ~allow-unsupported-compilers')
     conflicts('%pgi@:17,20:',
-              when='+cuda ~allow-unsupported-compilers ^cuda@10.1.105:10.2.89 target=x86_64:')
+              when='+cuda ^cuda@10.1.105:10.2.89 target=x86_64: ~allow-unsupported-compilers')
     conflicts('%pgi@:17,21:',
-              when='+cuda ~allow-unsupported-compilers ^cuda@11.0.2:11.1.0 target=x86_64:')
-    conflicts('%clang@:3.4', when='+cuda ~allow-unsupported-compilers ^cuda@:7.5 target=x86_64:')
+              when='+cuda ^cuda@11.0.2:11.1.0 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@:3.4', when='+cuda ^cuda@:7.5 target=x86_64: ~allow-unsupported-compilers')
     conflicts('%clang@:3.7,4:',
-              when='+cuda ~allow-unsupported-compilers ^cuda@8.0:9.0 target=x86_64:')
+              when='+cuda ^cuda@8.0:9.0 target=x86_64: ~allow-unsupported-compilers')
     conflicts('%clang@:3.7,4.1:',
-              when='+cuda ~allow-unsupported-compilers ^cuda@9.1 target=x86_64:')
-    conflicts('%clang@:3.7,5.1:', when='+cuda ~allow-unsupported-compilers ^cuda@9.2 target=x86_64:')
-    conflicts('%clang@:3.7,6.1:', when='+cuda ~allow-unsupported-compilers ^cuda@10.0.130 target=x86_64:')
-    conflicts('%clang@:3.7,7.1:', when='+cuda ~allow-unsupported-compilers ^cuda@10.1.105 target=x86_64:')
+              when='+cuda ^cuda@9.1 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@:3.7,5.1:', when='+cuda ^cuda@9.2 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@:3.7,6.1:', when='+cuda ^cuda@10.0.130 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@:3.7,7.1:', when='+cuda ^cuda@10.1.105 target=x86_64: ~allow-unsupported-compilers')
     conflicts('%clang@:3.7,8.1:',
-              when='+cuda ~allow-unsupported-compilers ^cuda@10.1.105:10.1.243 target=x86_64:')
-    conflicts('%clang@:3.2,9:', when='+cuda ~allow-unsupported-compilers ^cuda@10.2.89 target=x86_64:')
-    conflicts('%clang@:5', when='+cuda ~allow-unsupported-compilers ^cuda@11.0.2: target=x86_64:')
-    conflicts('%clang@10:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.0.2 target=x86_64:')
-    conflicts('%clang@11:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.1.0 target=x86_64:')
+              when='+cuda ^cuda@10.1.105:10.1.243 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@:3.2,9:', when='+cuda ^cuda@10.2.89 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@:5', when='+cuda ^cuda@11.0.2: target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@10:', when='+cuda ^cuda@:11.0.2 target=x86_64: ~allow-unsupported-compilers')
+    conflicts('%clang@11:', when='+cuda ^cuda@:11.1.0 target=x86_64: ~allow-unsupported-compilers')
 
     # x86_64 vs. ppc64le differ according to NVidia docs
     # Linux ppc64le compiler conflicts from Table from the docs below:
@@ -132,49 +129,49 @@ class CudaPackage(PackageBase):
     # https://docs.nvidia.com/cuda/archive/8.0/cuda-installation-guide-linux/index.html
 
     # information prior to CUDA 9 difficult to find
-    conflicts('%gcc@6:', when='+cuda ~allow-unsupported-compilers ^cuda@:9 target=ppc64le:')
-    conflicts('%gcc@8:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.0.130 target=ppc64le:')
-    conflicts('%gcc@9:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.1.243 target=ppc64le:')
+    conflicts('%gcc@6:', when='+cuda ^cuda@:9 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%gcc@8:', when='+cuda ^cuda@:10.0.130 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%gcc@9:', when='+cuda ^cuda@:10.1.243 target=ppc64le: ~allow-unsupported-compilers')
     # officially, CUDA 11.0.2 only supports the system GCC 8.3 on ppc64le
-    conflicts('%gcc@:4', when='+cuda ~allow-unsupported-compilers ^cuda@11.0.2: target=ppc64le:')
-    conflicts('%gcc@10:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.0.2 target=ppc64le:')
-    conflicts('%gcc@11:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.1.0 target=ppc64le:')
-    conflicts('%pgi', when='+cuda ~allow-unsupported-compilers ^cuda@:8 target=ppc64le:')
-    conflicts('%pgi@:16', when='+cuda ~allow-unsupported-compilers ^cuda@:9.1.185 target=ppc64le:')
-    conflicts('%pgi@:17', when='+cuda ~allow-unsupported-compilers ^cuda@:10 target=ppc64le:')
-    conflicts('%clang@4:', when='+cuda ~allow-unsupported-compilers ^cuda@:9.0.176 target=ppc64le:')
-    conflicts('%clang@5:', when='+cuda ~allow-unsupported-compilers ^cuda@:9.1 target=ppc64le:')
-    conflicts('%clang@6:', when='+cuda ~allow-unsupported-compilers ^cuda@:9.2 target=ppc64le:')
-    conflicts('%clang@7:', when='+cuda ~allow-unsupported-compilers ^cuda@10.0.130 target=ppc64le:')
-    conflicts('%clang@7.1:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.1.105 target=ppc64le:')
-    conflicts('%clang@8.1:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.2.89 target=ppc64le:')
-    conflicts('%clang@:5', when='+cuda ~allow-unsupported-compilers ^cuda@11.0.2: target=ppc64le:')
-    conflicts('%clang@10:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.0.2 target=ppc64le:')
-    conflicts('%clang@11:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.1.0 target=ppc64le:')
+    conflicts('%gcc@:4', when='+cuda ^cuda@11.0.2: target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%gcc@10:', when='+cuda ^cuda@:11.0.2 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%gcc@11:', when='+cuda ^cuda@:11.1.0 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%pgi', when='+cuda ^cuda@:8 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%pgi@:16', when='+cuda ^cuda@:9.1.185 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%pgi@:17', when='+cuda ^cuda@:10 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@4:', when='+cuda ^cuda@:9.0.176 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@5:', when='+cuda ^cuda@:9.1 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@6:', when='+cuda ^cuda@:9.2 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@7:', when='+cuda ^cuda@10.0.130 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@7.1:', when='+cuda ^cuda@:10.1.105 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@8.1:', when='+cuda ^cuda@:10.2.89 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@:5', when='+cuda ^cuda@11.0.2: target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@10:', when='+cuda ^cuda@:11.0.2 target=ppc64le: ~allow-unsupported-compilers')
+    conflicts('%clang@11:', when='+cuda ^cuda@:11.1.0 target=ppc64le: ~allow-unsupported-compilers')
 
     # Intel is mostly relevant for x86_64 Linux, even though it also
     # exists for Mac OS X. No information prior to CUDA 3.2 or Intel 11.1
-    conflicts('%intel@:11.0', when='+cuda ~allow-unsupported-compilers ^cuda@:3.1')
-    conflicts('%intel@:12.0', when='+cuda ~allow-unsupported-compilers ^cuda@5.5:')
-    conflicts('%intel@:13.0', when='+cuda ~allow-unsupported-compilers ^cuda@6.0:')
-    conflicts('%intel@:13.2', when='+cuda ~allow-unsupported-compilers ^cuda@6.5:')
-    conflicts('%intel@:14.9', when='+cuda ~allow-unsupported-compilers ^cuda@7:')
+    conflicts('%intel@:11.0', when='+cuda ^cuda@:3.1 ~allow-unsupported-compilers')
+    conflicts('%intel@:12.0', when='+cuda ^cuda@5.5: ~allow-unsupported-compilers')
+    conflicts('%intel@:13.0', when='+cuda ^cuda@6.0: ~allow-unsupported-compilers')
+    conflicts('%intel@:13.2', when='+cuda ^cuda@6.5: ~allow-unsupported-compilers')
+    conflicts('%intel@:14.9', when='+cuda ^cuda@7: ~allow-unsupported-compilers')
     # Intel 15.x is compatible with CUDA 7 thru current CUDA
-    conflicts('%intel@16.0:', when='+cuda ~allow-unsupported-compilers ^cuda@:8.0.43')
-    conflicts('%intel@17.0:', when='+cuda ~allow-unsupported-compilers ^cuda@:8.0.60')
-    conflicts('%intel@18.0:', when='+cuda ~allow-unsupported-compilers ^cuda@:9.9')
-    conflicts('%intel@19.0:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.0')
-    conflicts('%intel@19.1:', when='+cuda ~allow-unsupported-compilers ^cuda@:10.1')
-    conflicts('%intel@19.2:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.1.0')
+    conflicts('%intel@16.0:', when='+cuda ^cuda@:8.0.43 ~allow-unsupported-compilers')
+    conflicts('%intel@17.0:', when='+cuda ^cuda@:8.0.60 ~allow-unsupported-compilers')
+    conflicts('%intel@18.0:', when='+cuda ^cuda@:9.9 ~allow-unsupported-compilers')
+    conflicts('%intel@19.0:', when='+cuda ^cuda@:10.0 ~allow-unsupported-compilers')
+    conflicts('%intel@19.1:', when='+cuda ^cuda@:10.1 ~allow-unsupported-compilers')
+    conflicts('%intel@19.2:', when='+cuda ^cuda@:11.1.0 ~allow-unsupported-compilers')
 
     # XL is mostly relevant for ppc64le Linux
-    conflicts('%xl@:12,14:', when='+cuda ~allow-unsupported-compilers ^cuda@:9.1')
-    conflicts('%xl@:12,14:15,17:', when='+cuda ~allow-unsupported-compilers ^cuda@9.2')
-    conflicts('%xl@:12,17:', when='+cuda ~allow-unsupported-compilers ^cuda@:11.1.0')
+    conflicts('%xl@:12,14:', when='+cuda ^cuda@:9.1 ~allow-unsupported-compilers')
+    conflicts('%xl@:12,14:15,17:', when='+cuda ^cuda@9.2 ~allow-unsupported-compilers')
+    conflicts('%xl@:12,17:', when='+cuda ^cuda@:11.1.0 ~allow-unsupported-compilers')
 
     # Darwin.
     # TODO: add missing conflicts for %apple-clang cuda@:10
-    conflicts('platform=darwin', when='+cuda ~allow-unsupported-compilers ^cuda@11.0.2:')
+    conflicts('platform=darwin', when='+cuda ^cuda@11.0.2: ~allow-unsupported-compilers')
 
     # Make sure cuda_arch can not be used without +cuda
     for value in cuda_arch_values:
