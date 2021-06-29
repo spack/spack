e265b663fbc2fd565efb53a8539fc7b172769680
Jen Herting <jen@herting.cc>
2021-06-29 15:23:28 -0400

    [py-datasets] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-datasets/package.py
+++ var/spack/repos/builtin/packages/py-datasets/package.py
@@ -3,36 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-datasets
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-datasets
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyDatasets(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Datasets is a lightweight library providing two main
+    features: one-line dataloaders for many public datasets and
+    efficient data pre-processing."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/huggingface/datasets"
     pypi     = "datasets/datasets-1.8.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.8.0', sha256='d57c32bb29e453ee7f3eb0bbca3660ab4dd2d0e4648efcfa987432624cab29d3')
 
@@ -53,9 +33,3 @@ class PyDatasets(PythonPackage):
 
     depends_on('py-fsspec@:0.8.0', when='^python@:3.7.999', type=('build', 'run'))
     depends_on('py-fsspec', when='^python@3.8:', type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
