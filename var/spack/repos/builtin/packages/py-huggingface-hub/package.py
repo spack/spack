5b9df36b52d304afe6c8f1d5258d423e453c363e
Jen Herting <jen@herting.cc>
2021-06-29 10:19:04 -0400

    [py-huggingface-hub] Final cleanup
    - added description
    - added homepage
    - removed fixmes

--- var/spack/repos/builtin/packages/py-huggingface-hub/package.py
+++ var/spack/repos/builtin/packages/py-huggingface-hub/package.py
@@ -3,43 +3,20 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-huggingface-hub
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-huggingface-hub
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyHuggingfaceHub(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This library allows anyone to work with the Hub
+    repositories: you can clone them, create them and upload
+    your models to them."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/huggingface/huggingface_hub"
     pypi     = "huggingface_hub/huggingface_hub-0.0.10.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.0.10', sha256='556765e4c7edd2d2c4c733809bae1069dca20e10ff043870ec40d53e498efae2')
     version('0.0.8', sha256='be5b9a7ed36437bb10a780d500154d426798ec16803ff3406f7a61107e4ebfc2')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-filelock', type=('build', 'run'))
@@ -47,9 +24,3 @@ class PyHuggingfaceHub(PythonPackage):
     depends_on('py-tqdm', type=('build', 'run'))
     depends_on('py-typing-extensions', when='@0.0.10:', type=('build', 'run'))
     depends_on('py-importlib-metadata', when='^python@:3.7.999', type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
