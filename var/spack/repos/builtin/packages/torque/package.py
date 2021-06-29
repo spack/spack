a173f8dcfc13835feec4cbd99eff5b43a09122a3
Seth R Johnson <johnsonsr@ornl.gov>
2021-06-29 18:48:04 -0400

    torque: change to uninstallable package

--- var/spack/repos/builtin/packages/torque/package.py
+++ var/spack/repos/builtin/packages/torque/package.py
@@ -6,12 +6,30 @@
 from spack import *
 
 
-class Torque(BundlePackage):
-    """Placeholder package for external TORUQE/PBS installation."""
+class Torque(Package):
+    """TORQUE (Terascale Open-source Resource and QUEue Manager) is an open
+    source project based on the original PBS resource manager developed by NASA,
+    LLNL, and MRJ."""
 
     homepage = "https://github.com/abarbu/torque"
+    has_code = False
 
     version('3.0.4')
     version('3.0.2')
 
     provides('pbs')
+
+    # TORQUE needs to be added as an external package to SPACK. For this, the
+    # config file packages.yaml needs to be adjusted:
+    #
+    # packages:
+    #   torque:
+    #     buildable: False
+    #     externals:
+    #     - spec: torque@3.0.2
+    #       prefix: /opt/torque (path to your TORQUE installation)
+
+    def install(self, spec, prefix):
+        raise InstallError(
+            self.spec.format('{name} is not installable, you need to specify '
+                             'it as an external package in packages.yaml'))
