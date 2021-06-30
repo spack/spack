c8868f1922c91fb7d8635ed88a06159857a7cae6
Zack Galbreath <zack.galbreath@kitware.com>
2021-06-30 14:16:15 -0400

    ci: only write to broken-specs list on SpackError (#24618)
    ci: only write to broken-specs list on SpackError
    
    Only write to the broken-specs list when `spack install` raises a SpackError,
    instead of writing to this list unnecessarily when infrastructure-related problems
    prevent a develop job from completing successfully.

--- lib/spack/spack/cmd/ci.py
+++ lib/spack/spack/cmd/ci.py
@@ -14,11 +14,10 @@ import tempfile
 from six.moves.urllib.parse import urlencode
 
 import llnl.util.tty as tty
-
 import spack.binary_distribution as bindist
 import spack.ci as spack_ci
-import spack.config as cfg
 import spack.cmd.buildcache as buildcache
+import spack.config as cfg
 import spack.environment as ev
 import spack.hash_types as ht
 import spack.mirror
@@ -492,7 +491,7 @@ def ci_rebuild(args):
     # If a spec fails to build in a spack develop pipeline, we add it to a
     # list of known broken full hashes.  This allows spack PR pipelines to
     # avoid wasting compute cycles attempting to build those hashes.
-    if install_exit_code != 0 and spack_is_develop_pipeline:
+    if install_exit_code == 1 and spack_is_develop_pipeline:
         tty.debug('Install failed on develop')
         if 'broken-specs-url' in gitlab_ci:
             broken_specs_url = gitlab_ci['broken-specs-url']
