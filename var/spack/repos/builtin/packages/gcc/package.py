78c6211d894bc8d8b7c07a5347d5d1d11aa3c331
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-07-01 14:20:49 +0200

    Fixed import order in gcc

--- var/spack/repos/builtin/packages/gcc/package.py
+++ var/spack/repos/builtin/packages/gcc/package.py
@@ -11,8 +11,7 @@ import sys
 import llnl.util.tty as tty
 import spack.architecture
 import spack.util.executable
-
-from spack.operating_systems.mac_os import macos_version, macos_sdk_path
+from spack.operating_systems.mac_os import macos_sdk_path, macos_version
 
 
 class Gcc(AutotoolsPackage, GNUMirrorPackage):
