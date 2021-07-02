7901354fd49c90b422f97976d2f71b536368fcb6
Dylan Simon <dylan@dylex.net>
2021-07-01 20:40:20 -0400

    pthreadpool: enable shared libraries

--- var/spack/repos/builtin/packages/pthreadpool/package.py
+++ var/spack/repos/builtin/packages/pthreadpool/package.py
@@ -50,6 +50,7 @@ class Pthreadpool(CMakePackage):
 
     def cmake_args(self):
         return [
+            '-DBUILD_SHARED_LIBS=ON',
             self.define('FXDIV_SOURCE_DIR',
                         join_path(self.stage.source_path, 'deps', 'fxdiv')),
             self.define('GOOGLETEST_SOURCE_DIR',
