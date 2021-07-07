75e7c1861dc6aec9891eafc4be269ba2a5723ca5
John Parent <john.parent@kitware.com>
2021-07-07 15:21:39 -0400

    Refactor for new cmd batch location

--- share/spack/qa/windows_test_setup.ps1
+++ share/spack/qa/windows_test_setup.ps1
@@ -1,11 +1,8 @@
 Set-Location ../
-if (!(test-path scripts)) {
-    mkdir scripts
-}
-Copy-Item "spack\lib\spack\spack\cmd\installer\scripts\*" "scripts"
-Copy-Item "spack\lib\spack\spack\cmd\installer\spack_cmd.bat" "."
+
 $env:python_pf_ver="C:\hostedtoolcache\windows\Python\3.9.5\x64\python.exe"
-cmd /c "`".\spack_cmd.bat`" print " |
+
+cmd /c "`"spack\bin\spack_cmd.bat`" print " |
 foreach {
     if ($_ -match "=") {
         $v = $_.split("=")
