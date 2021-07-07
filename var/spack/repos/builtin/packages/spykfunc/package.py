ec9807b83e62d2e32ee067a97da68a92dfc91109
Matthias Wolf <matthias.wolf@epfl.ch>
2021-07-07 13:57:24 +0200

    spykfunc: add future dependencies (#1224)

--- var/spack/repos/builtin/packages/spykfunc/package.py
+++ var/spack/repos/builtin/packages/spykfunc/package.py
@@ -30,8 +30,9 @@ class Spykfunc(PythonPackage):
     version('0.12.2', tag='v0.12.2', submodules=True, get_full_repo=True)
 
     depends_on('cmake', type='build', when='@0.15.4:')
-    depends_on('boost', type=('build', 'link'), when='@0.15.4:')
+    depends_on('boost', type=('build', 'link'), when='@0.15.4:0.16.999')
     depends_on('morpho-kit', type=('build', 'link'), when='@0.15.4:')
+    depends_on('random123', when='@0.17.0:')
 
     depends_on('py-mvdtool~mpi', type=('build', 'run'), when='@0.14.4:0.16.1')
 
