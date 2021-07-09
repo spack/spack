de8d4e9d9a9c8338389781de640457353308b787
Michele Mesiti <mmesiti@users.noreply.github.com>
2021-07-09 13:02:26 +0200

    sombrero: add v2021-07-08, deprecate v1.0 (#24782)

--- var/spack/repos/builtin/packages/sombrero/package.py
+++ var/spack/repos/builtin/packages/sombrero/package.py
@@ -12,15 +12,25 @@ class Sombrero(MakefilePackage):
     "A next-generation conjugate gradient benchmark from computational particle physics"
 
     homepage = "https://github.com/sa2c/sombrero"
-    url      = "https://github.com/sa2c/sombrero/archive/refs/tags/1.0.tar.gz"
+    url = "https://github.com/sa2c/sombrero/archive/refs/tags/1.0.tar.gz"
 
-    version('1.0', sha256='423a631c86f0e5f14dea186228871099ca0374dc07bf1bb24b6be17f79784682')
+    version('2021-07-08',
+            sha256='816b0f0a684a421fa620f11c21649ac162e85d1febd6a7e10cfd07604760c0d6')
+
+    # Version 1 is incompatible with spack
+    # as CFLAGS and the like are hardcoded in the makefile.
+    version('1.0',
+            sha256='423a631c86f0e5f14dea186228871099ca0374dc07bf1bb24b6be17f79784682',
+            deprecated=True)
 
     depends_on('mpi')
 
+    maintainers = ['mmesiti', 'edbennett']
+
     def edit(self, spec, prefix):
         # Make the `sombrero.sh` driver relocatable
-        sombrero_sh = FileFilter(join_path(self.stage.source_path, 'sombrero.sh'))
+        sombrero_sh = FileFilter(
+            join_path(self.stage.source_path, 'sombrero.sh'))
         sombrero_dir = join_path(prefix.bin, 'sombrero')
         sombrero_sh.filter('sombrero/', '{0}/'.format(sombrero_dir))
 
