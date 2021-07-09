2be2fb4bfb9e1b808e925c517694e1282dd52c0b
John W. Parent <45471568+johnwparent@users.noreply.github.com>
2021-07-09 16:39:57 -0400

    Add Github Actions for Windows (#24504)
    Setup Installer CI (#25184), (#25191)
    
    Co-authored-by: Zack Galbreath <zack.galbreath@kitware.com>
    Co-authored-by: lou.lawrence@kitware.com <lou.lawrence@kitware.com>
    Co-authored-by: Betsy McPhail <betsy.mcphail@kitware.com>

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -915,14 +915,14 @@ _spack_env() {
 _spack_env_activate() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help --sh --csh --fish -v --with-view -V --without-view -p --prompt --temp -d --dir"
+        SPACK_COMPREPLY="-h --help --sh --csh --fish --bat -v --with-view -V --without-view -p --prompt --temp -d --dir"
     else
         _environments
     fi
 }
 
 _spack_env_deactivate() {
-    SPACK_COMPREPLY="-h --help --sh --csh --fish"
+    SPACK_COMPREPLY="-h --help --sh --csh --fish --bat"
 }
 
 _spack_env_create() {
@@ -1201,7 +1201,7 @@ _spack_list() {
 _spack_load() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help --sh --csh --fish --first --only --list"
+        SPACK_COMPREPLY="-h --help --sh --csh --fish --bat --first --only --list"
     else
         _installed_packages
     fi
@@ -1237,7 +1237,7 @@ _spack_maintainers() {
 _spack_make_installer() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help -v --spack_version -s --spack_source"
+        SPACK_COMPREPLY="-h --help -v --spack_version -s --spack_source -g --git-installer-verbosity"
     else
         SPACK_COMPREPLY=""
     fi
@@ -1787,7 +1787,7 @@ _spack_unit_test() {
 _spack_unload() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help --sh --csh --fish -a --all"
+        SPACK_COMPREPLY="-h --help --sh --csh --fish --bat -a --all"
     else
         _installed_packages
     fi
