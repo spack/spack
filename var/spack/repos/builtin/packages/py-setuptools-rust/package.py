8dd151731726564d2133fe179fd4c8a0a2e6eec6
Manuela Kuhn <manuela.kuhn@ovgu.de>
2021-07-13 22:27:16 +0200

    mark 0.10.6 as deprecated and fix style

--- var/spack/repos/builtin/packages/py-setuptools-rust/package.py
+++ var/spack/repos/builtin/packages/py-setuptools-rust/package.py
@@ -14,10 +14,11 @@ class PySetuptoolsRust(PythonPackage):
 
     version('0.12.1', sha256='647009e924f0ae439c7f3e0141a184a69ad247ecb9044c511dabde232d3d570e')
     # Version 0.10.6 is not available on pypi and can only be found on github
-    version('0.10.6', sha256='1446d3985e4aaf4cc679fda8a48a73ac1390b627c8ae1bebe7d9e08bb3b33769')
+    version('0.10.6', sha256='1446d3985e4aaf4cc679fda8a48a73ac1390b627c8ae1bebe7d9e08bb3b33769',
             # version specific url is not used here because spack checksum would
             # use it instead of pypi (see #24668)
-#            url="https://github.com/PyO3/setuptools-rust/archive/v0.10.6.tar.gz")
+            # url="https://github.com/PyO3/setuptools-rust/archive/v0.10.6.tar.gz",
+            deprecated=True)
 
     depends_on('python@3.6:', when='@0.12:', type=('build', 'run'))
     depends_on('py-setuptools@46.1:', when='@0.11.6:', type='build')
