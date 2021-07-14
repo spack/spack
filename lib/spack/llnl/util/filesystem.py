4b6558a8a4ba5ff1925117ac7c2ed95b43c25db4
Betsy McPhail <betsy.mcphail@kitware.com>
2021-07-14 12:13:25 -0400

    Various filepath updates found when testing on Windows

--- lib/spack/llnl/util/filesystem.py
+++ lib/spack/llnl/util/filesystem.py
@@ -824,7 +824,7 @@ def ancestor(dir, n=1):
     parent = os.path.abspath(dir)
     for i in range(n):
         parent = os.path.dirname(parent)
-    return parent
+    return parent.replace("\\", "/")
 
 
 def get_single_file(directory):
@@ -1166,11 +1166,16 @@ def _find_recursive(root, search_files):
 
     # Make the path absolute to have os.walk also return an absolute path
     root = os.path.abspath(root)
-
+    if sys.platform == "win32":
+        root = root.replace("\\", "/")
     for path, _, list_files in os.walk(root):
+        if sys.platform == "win32":
+            path = path.replace("\\", "/")
         for search_file in search_files:
             matches = glob.glob(os.path.join(path, search_file))
             matches = [os.path.join(path, x) for x in matches]
+            if sys.platform == "win32":
+                matches = [x.replace("\\", "/") for x in matches]
             found_files[search_file].extend(matches)
 
     answer = []
@@ -1187,10 +1192,14 @@ def _find_non_recursive(root, search_files):
 
     # Make the path absolute to have absolute path returned
     root = os.path.abspath(root)
+    if sys.platform == "win32":
+        root = root.replace("\\", "/")
 
     for search_file in search_files:
         matches = glob.glob(os.path.join(root, search_file))
         matches = [os.path.join(root, x) for x in matches]
+        if sys.platform == "win32":
+            matches = [x.replace("\\", "/") for x in matches]
         found_files[search_file].extend(matches)
 
     answer = []
