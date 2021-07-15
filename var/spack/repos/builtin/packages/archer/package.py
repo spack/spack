0fd29f54d200309a96ee1b398a061dc1776d9d8f
Kayla Butler <butler59@llnl.gov>
2021-07-15 09:33:37 -0700

    Use test stage copy

--- var/spack/repos/builtin/packages/archer/package.py
+++ var/spack/repos/builtin/packages/archer/package.py
@@ -47,7 +47,7 @@ class Archer(CMakePackage):
     def run_parallel_example_test(self):
         """Run stand alone test: parallel-simple"""
 
-        test_dir = join_path(self.install_test_root, 'test', 'parallel')
+        test_dir = join_path(self.test_suite.current_test_cache_dir, 'test', 'parallel')
 
         if not os.path.exists(test_dir):
             print('Skipping archer test')
