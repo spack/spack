61f43499bf033a348940b94145fbf1c39a044689
Kayla Butler <butler59@llnl.gov>
2021-07-16 15:06:32 -0700

    Add test requires compiler

--- var/spack/repos/builtin/packages/archer/package.py
+++ var/spack/repos/builtin/packages/archer/package.py
@@ -15,6 +15,8 @@ class Archer(CMakePackage):
     homepage = "https://github.com/PRUNERS/ARCHER"
     url      = "https://github.com/PRUNERS/archer/archive/v1.0.0.tar.gz"
 
+    test_requires_compiler = True
+
     version('2.0.0', sha256='3241cadb0078403368b69166b27f815e12c350486d4ceb3fb33147895b9ebde8')
     version('1.0.0', sha256='df814a475606b83c659932caa30a68bed1c62e713386b375c1b78eb8d60e0d15')
 
