c56f2a935db03e7041bd88783149caffd0366adc
Adam J. Stewart <ajstewart426@gmail.com>
2021-07-15 23:09:39 -0500

    Sphinx 3.4+ required for correct reference target linking

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -97,7 +97,7 @@ def setup(sphinx):
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.8'
+needs_sphinx = '3.4'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
