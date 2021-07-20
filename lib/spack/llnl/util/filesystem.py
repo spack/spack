5d71b44896d5d4efd629beadd6923e464422d0c5
Zack Galbreath <zack.galbreath@kitware.com>
2021-07-20 11:32:06 -0400

    os.sep is not a function

--- lib/spack/llnl/util/filesystem.py
+++ lib/spack/llnl/util/filesystem.py
@@ -1842,7 +1842,7 @@ def prefixes(path):
     if sys.platform == "win32":
         sep = "/"
     else:
-        sep = os.sep()
+        sep = os.sep
 
     if sys.platform == "win32":
         path = path.replace("\\", "/")
