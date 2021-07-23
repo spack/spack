e72db4ec1ee9cd757e6d5c5da0b4b3ecf0e41668
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2021-07-23 14:09:26 +0200

    Bump connectome-tools 0.5.1 (#1228)

--- var/spack/repos/builtin/packages/connectome-tools/package.py
+++ var/spack/repos/builtin/packages/connectome-tools/package.py
@@ -13,11 +13,11 @@ class ConnectomeTools(PythonPackage):
     git = "git@bbpgitlab.epfl.ch:nse/connectome-tools.git"
 
     version('develop', branch='main')
-    version('0.5.0', tag='connectome-tools-v0.5.0')
+    version('0.5.1', tag='connectome-tools-v0.5.1')
 
     depends_on('py-setuptools', type=('build', 'run'))
 
-    depends_on('py-click@7.0:7.999', type='run')
+    depends_on('py-click@7.0:8.999', type='run')
     depends_on('py-equation@1.2:', type='run')
     depends_on('py-joblib@0.16.0:', type='run')
     depends_on('py-lxml@3.3:', type='run')
@@ -26,9 +26,6 @@ class ConnectomeTools(PythonPackage):
     depends_on('py-psutil@5.7.2:', type='run')
     depends_on('py-pyyaml@5.3.1:', type='run')
 
+    depends_on('py-bluepy@2.3.0:2.999', type='run')
+    depends_on('py-morphio@3.0.1:3.999', type='run')
     depends_on('py-voxcell@3.0:3.999', type='run')
-
-    depends_on('py-bluepy@2.2.0', type='run', when='@0.5.0')
-    depends_on('py-neurom@1.8.0', type='run', when='@0.5.0')
-    depends_on('py-morph-tool@2.4.1', type='run', when='@0.5.0')
-    depends_on('py-bluepysnap@0.11.0', type='run', when='@0.5.0')
