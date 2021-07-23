33504069fa0328db42c84b0d30db654f520594fe
Caleb Robinson <calebrob6@gmail.com>
2021-07-23 02:58:19 +0000

    Adding package for omegaconf

---
+++ var/spack/repos/builtin/packages/py-omegaconf/package.py
@@ -0,0 +1,25 @@
+
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyOmegaconf(PythonPackage):
+    """A hierarchical configuration system, with support for merging configurations from
+    multiple sources (YAML config files, dataclasses/objects and CLI arguments)
+    providing a consistent API regardless of how the configuration was created.
+    """
+
+    homepage = "https://github.com/omry/omegaconf"
+    pypi     = "omegaconf/omegaconf-2.1.0.tar.gz"
+
+    maintainers = ['calebrob6']
+
+    version('2.1.0', sha256='a08aec03a63c66449b550b85d70238f4dee9c6c4a0541d6a98845dcfeb12439d')
+
+    depends_on('python', type=('build', 'run'))
+    depends_on('py-antlr4-python3-runtime@4.8', type=('build', 'run'))
+    depends_on('py-pyyaml@5.1.0:', type=('build', 'run'))
