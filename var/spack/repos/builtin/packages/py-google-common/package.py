a0fd231423e21b83da7e44e17ca684f52473eb58
iarspider <iarspider@gmail.com>
2021-07-26 11:14:01 +0200

    New package: py-google-common

---
+++ var/spack/repos/builtin/packages/py-google-common/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyGoogleCommon(PythonPackage):
+    """Google namespace package"""
+
+    homepage = "http://pypi.python.org/pypi/google-common"
+    pypi     = "google-common/google-common-0.0.1.tar.gz"
+
+
+    version('0.0.1', sha256='add3cf494034fa11080e77cef2cc1f55a436a3353f5f5f95cc4f2b9788a678d1')
+
+    depends_on('py-setuptools', type='build')
