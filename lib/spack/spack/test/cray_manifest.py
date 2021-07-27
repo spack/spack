2fc88f0b8693481ab2e12305dcf539bec3948dde
Peter Josef Scheibel <scheibel1@llnl.gov>
2021-07-27 14:04:53 -0700

    add more checks to test

--- lib/spack/spack/test/cray_manifest.py
+++ lib/spack/spack/test/cray_manifest.py
@@ -221,13 +221,20 @@ def _example_db():
     }
 
 
-def test_read_external_db(
+def test_read_cray_manifest(
         tmpdir, config, mock_packages, mutable_database):
+    """Check that (a) we can read the cray manifest and add it to the Spack
+       Database and (b) we can concretize specs based on that.
+    """
 
     with tmpdir.as_cwd():
         test_db_fname = 'external-db.json'
         with open(test_db_fname, 'w') as db_file:
             json.dump(_example_db(), db_file)
         cray_manifest.read(test_db_fname, True)
-        specs = spack.store.db.query('openmpi')
-        assert any(x.dag_hash() == 'openmpifakehasha' for x in specs)
+        query_specs = spack.store.db.query('openmpi')
+        assert any(x.dag_hash() == 'openmpifakehasha' for x in query_specs)
+
+        concretized_specs = spack.cmd.parse_specs(
+            'openmpi ^/openmpifakehasha'.split(), concretize=True)
+        assert concretized_specs[0]['hwloc'].dag_hash() == 'hwlocfakehashaaa'
