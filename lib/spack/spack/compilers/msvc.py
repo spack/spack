11e462a2e8c2df49ba249becf45a48653ff4f7c2
Jared Popelar <jdpopelar@gmail.com>
2021-07-27 11:10:52 -0600

    Packaging of netlib-lapack for windows (#24993)
    * Loading Intel's ifx Fortran compiler into MSVC; if there are multiple
    versions of MSVC installed and detected, ifx will only be placed into
    the first block written in compilers.yaml. The version number of ifx can
    be detected using MSVC's version flag (instead of /QV) by using
    ignore_version_errors. This commit also provides support for detection
    of Intel compilers in their own compiler block by adding ifx.exe to the
    fc/f77_name blocks inside intel.py
    
    * Giving CMake a Fortran compiler argument
    
    * Adding patch file for removing duplicated mangling header for versions 3.9.1 and older; static and shared now successfully building on Windows
    
    * Have netlib-lapack depend  on ninja@1.10
    
    Co-authored-by: John R. Cary <cary@txcorp.com>
    Co-authored-by: Jared Popelar <jpopelar@txcorp.com>

--- lib/spack/spack/compilers/msvc.py
+++ lib/spack/spack/compilers/msvc.py
@@ -8,6 +8,8 @@ import subprocess
 import sys
 from typing import List  # novm
 
+import spack.operating_systems.windows_os
+import spack.util.executable
 from spack.compiler import Compiler
 
 
@@ -19,19 +21,23 @@ class Msvc(Compiler):
     cxx_names = ['cl.exe']
 
     # Subclasses use possible names of Fortran 77 compiler
-    f77_names = []  # type: List[str]
+    f77_names = ['ifx.exe']  # type: List[str]
 
     # Subclasses use possible names of Fortran 90 compiler
-    fc_names = []  # type: List[str]
+    fc_names = ['ifx.exe']  # type: List[str]
 
     # Named wrapper links within build_env_path
-    link_paths = {'cc': 'msvc/cl.exe',
-                  'cxx': 'msvc/cl.exe',
+    link_paths = {'cc': '',
+                  'cxx': '',
                   'f77': '',
                   'fc': ''}
 
     #: Compiler argument that produces version information
     version_argument = ''
+
+    # For getting ifx's version, call it with version_argument
+    # and ignore the error code
+    ignore_version_errors = [1]
 
     #: Regex used to extract version from compiler's output
     version_regex = r'([1-9][0-9]*\.[0-9]*\.[0-9]*)'
@@ -41,10 +47,15 @@ class Msvc(Compiler):
 
     def __init__(self, *args, **kwargs):
         super(Msvc, self).__init__(*args, **kwargs)
-        self.vcvarsallfile = os.path.abspath(
-            os.path.join(self.cc, '../../../../../../..'))
-        self.vcvarsallfile = os.path.join(
-            self.vcvarsallfile, 'Auxiliary', 'Build', 'vcvarsall.bat')
+        if os.getenv("ONEAPI_ROOT"):
+            # If this found, it sets all the vars
+            self.setvarsfile = os.path.join(
+                os.getenv("ONEAPI_ROOT"), "setvars.bat")
+        else:
+            self.setvarsfile = os.path.abspath(
+                os.path.join(self.cc, '../../../../../../..'))
+            self.setvarsfile = os.path.join(
+                self.setvarsfile, 'Auxiliary', 'Build', 'vcvars64.bat')
 
     @property
     def verbose_flag(self):
@@ -55,28 +66,60 @@ class Msvc(Compiler):
         return ""
 
     def setup_custom_environment(self, pkg, env):
-        """Set environment variables for MSVC using the Microsoft-provided
-        script."""
+        """Set environment variables for MSVC using the
+        Microsoft-provided script."""
         if sys.version_info[:2] > (2, 6):
-            # Capture output from batch script and DOS environment dump
+            # Set the build environment variables for spack. Just using
+            # subprocess.call() doesn't work since that operates in its own
+            # environment which is destroyed (along with the adjusted variables)
+            # once the process terminates. So go the long way around: examine
+            # output, sort into dictionary, use that to make the build
+            # environment.
             out = subprocess.check_output(  # novermin
-                'cmd /u /c "{0}" {1} && set'.format(self.vcvarsallfile, 'amd64'),
+                'cmd /u /c "{}" {} && set'.format(self.setvarsfile, 'amd64'),
                 stderr=subprocess.STDOUT)
             if sys.version_info[0] >= 3:
-                out = out.decode('utf-16le', errors='replace')
+                out = out.decode('utf-16le', errors='replace')  # novermin
+
+            int_env = {  # novermin
+                key.lower(): value
+                for key, _, value in
+                (line.partition('=') for line in out.splitlines())
+                if key and value
+            }
+
+            if 'path' in int_env:
+                env.set_path('PATH', int_env['path'].split(';'))
+            env.set_path('INCLUDE', int_env.get('include', '').split(';'))
+            env.set_path('LIB', int_env.get('lib', '').split(';'))
         else:
+            # Should not this be an exception?
             print("Cannot pull msvc compiler information in Python 2.6 or below")
 
-        # Process in to nice Python dictionary
-        vc_env = {  # novermin
-            key.lower(): value
-            for key, _, value in
-            (line.partition('=') for line in out.splitlines())
-            if key and value
-        }
+    # fc_version only loads the ifx compiler into the first MSVC stanza;
+    # if there are other versions of Microsoft VS installed and detected, they
+    # will only have cl.exe as the C/C++ compiler
 
-        # Request setting environment variables
-        if 'path' in vc_env:
-            env.set_path('PATH', vc_env['path'].split(';'))
-        env.set_path('INCLUDE', vc_env.get('include', '').split(';'))
-        env.set_path('LIB', vc_env.get('lib', '').split(';'))
+    @classmethod
+    def fc_version(cls, fc):
+        # We're using intel for the Fortran compilers, which exist if
+        # ONEAPI_ROOT is a meaningful variable
+        if os.getenv("ONEAPI_ROOT"):
+            try:
+                sps = spack.operating_systems.windows_os.WindowsOs.compiler_search_paths
+            except Exception:
+                print("sps not found.")
+                raise
+            try:
+                clp = spack.util.executable.which_string("cl", path=sps)
+            except Exception:
+                print("cl not found.")
+                raise
+            ver = cls.default_version(clp)
+            return ver
+        else:
+            return cls.default_version(fc)
+
+    @classmethod
+    def f77_version(cls, f77):
+        return cls.fc_version(f77)
