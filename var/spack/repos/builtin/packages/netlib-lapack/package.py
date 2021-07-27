662da68f669fa48c072ba37c2514bee04c9c87cf
Jared Popelar <jdpopelar@gmail.com>
2021-07-27 11:10:52 -0600

    Packaging of netlib-lapack for windows (#24993)
    * Loading Intel's ifx Fortran compiler into MSVC; if there are multiple
    versions of MSVC installed and detected, ifx will only be placed into
    the first block written in compilers.yaml. The version number of ifx can
    be detected using MSVC's version flag (instead of /QV) by using
    ignore_version_errors. This commit also provides support for detection
    of Intel compilers in their own compiler block by adding ifx.exe to the
    fc/f77_name blocks inside intel.py
    
    * Giving CMake a Fortran compiler argument
    
    * Adding patch file for removing duplicated mangling header for versions 3.9.1 and older; static and shared now successfully building on Windows
    
    * Have netlib-lapack depend  on ninja@1.10
    
    Co-authored-by: John R. Cary <cary@txcorp.com>
    Co-authored-by: Jared Popelar <jpopelar@txcorp.com>

--- var/spack/repos/builtin/packages/netlib-lapack/package.py
+++ var/spack/repos/builtin/packages/netlib-lapack/package.py
@@ -62,6 +62,7 @@ class NetlibLapack(CMakePackage):
     depends_on('blas', when='+external-blas')
     depends_on('netlib-xblas+fortran+plain_blas', when='+xblas')
     depends_on('python@2.7:', type='test')
+    depends_on('ninja@1.10.0:', when='platform=windows')
 
     # We need to run every phase twice in order to get static and shared
     # versions of the libraries. When ~shared, we run the default
@@ -82,6 +83,11 @@ class NetlibLapack(CMakePackage):
                 '${CMAKE_CURRENT_SOURCE_DIR}/CMAKE/',
                 '${CMAKE_CURRENT_SOURCE_DIR}/cmake/',
                 'CBLAS/CMakeLists.txt', string=True)
+
+        # Remove duplicate header file that gets generated during CMake shared
+        # builds: https://github.com/Reference-LAPACK/lapack/issues/583
+        if self.spec.satisfies('platform=windows @0:3.9.1'):
+            force_remove('LAPACKE/include/lapacke_mangling.h')
 
     @property
     def blas_libs(self):
