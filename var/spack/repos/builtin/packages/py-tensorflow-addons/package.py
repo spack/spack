65d5284ec9bb9199c3de2be6df64722e573dffd3
Matthias Wolf <matthias.wolf@epfl.ch>
2021-07-28 14:37:18 +0200

    py-tensorflow-addons: bump version to trigger rebuild. (#1238)

--- var/spack/repos/builtin/packages/py-tensorflow-addons/package.py
+++ var/spack/repos/builtin/packages/py-tensorflow-addons/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class PyTensorflowAddons(PythonPackage):
+class PyTensorflowAddons(Package):
     """
     TensorFlow Addons is a repository of contributions that conform to
     well-established API patterns, but implement new functionality not
@@ -21,6 +21,8 @@ class PyTensorflowAddons(PythonPackage):
     homepage = "https://pypi.org/project/tensorflow-addons/"
     url = "https://files.pythonhosted.org/packages/bf/57/1f478ae21b8cfefc1a55c764f8fbd6e016528b830b279a908002b9f9b9f5/tensorflow_addons-0.13.0-cp38-cp38-manylinux2010_x86_64.whl"
 
+    # The version below just serves to trigger a rebuild!
+    version('0.13.0.20210728', url=url, sha256='ea76eab2abf2e1de3037acca27953b30c010087aabb27d80d860f92e1c66cef8', expand=False)
     version('0.13.0', sha256='eaa258923bbf48fcd3688177a9e1055f674854437c93ae461b1a166d08e06286', expand=False)
 
     maintainers = ['pramodk', 'matz-e']
