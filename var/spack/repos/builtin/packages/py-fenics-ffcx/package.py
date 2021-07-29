a62b0436c717a556cb65225bbdc9870552d9626c
Garth N. Wells <gnw20@cam.ac.uk>
2021-07-29 15:25:42 +0100

    Fix dependency types

--- var/spack/repos/builtin/packages/py-fenics-ffcx/package.py
+++ var/spack/repos/builtin/packages/py-fenics-ffcx/package.py
@@ -19,12 +19,12 @@ class PyFenicsFfcx(PythonPackage):
 
     depends_on('python@3.7:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
-    depends_on('py-cffi', type=('build', 'run'))
+    depends_on('py-cffi', type=('run'))
 
-    depends_on('py-fenics-ufl@main', type=('build', 'run'), when='@main')
-    depends_on('py-fenics-ufl@2021.1.0', type=('build', 'run'), when='@0.1.0')
+    depends_on('py-fenics-ufl@main', type=('run'), when='@main')
+    depends_on('py-fenics-ufl@2021.1.0', type=('run'), when='@0.1.0')
 
-    depends_on('py-fenics-basix@main', type=('build', 'run'), when='@main')
-    depends_on('py-fenics-basix@0.1.0', type=('build', 'run'), when='@0.1.0')
+    depends_on('py-fenics-basix@main', type=('run'), when='@main')
+    depends_on('py-fenics-basix@0.1.0', type=('run'), when='@0.1.0')
 
-    depends_on('py-numpy', type=('build', 'run'))
+    depends_on('py-numpy', type=('run'))
