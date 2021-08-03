0b3b5a3d4026574dd59d8655eafa45ccc93e4560
Harmen Stoppels <harmenstoppels@gmail.com>
2021-08-03 16:02:50 +0200

    Make dependency on keystone actually optional
    Co-authored-by: Amjad Kotobi <amjadkotbi@gmail.com>

--- var/spack/repos/builtin/packages/py-python-swiftclient/package.py
+++ var/spack/repos/builtin/packages/py-python-swiftclient/package.py
@@ -32,4 +32,4 @@ class PyPythonSwiftclient(PythonPackage):
     depends_on('py-requests@1.1.0:', type=('build', 'run'))
     depends_on('py-six@1.9:', type=('build', 'run'))
 
-    depends_on('py-python-keystoneclient@0.7.0:', type=('build', 'run'))
+    depends_on('py-python-keystoneclient@0.7.0:', when='+keystone', type=('build', 'run'))
