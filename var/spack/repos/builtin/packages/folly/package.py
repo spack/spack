e5f0c1fbd793e9854aa52c1d6b279cf8b4cb698c
Ryan O'Malley <rd.omalley@comcast.net>
2021-08-05 09:07:14 -0500

    Added composable boost dependencies for folly

--- var/spack/repos/builtin/packages/folly/package.py
+++ var/spack/repos/builtin/packages/folly/package.py
@@ -25,14 +25,13 @@ class Folly(CMakePackage):
     # CMakePackage Dependency
     depends_on('pkgconfig', type='build')
 
-    # folly requires gcc 4.9+ and a version of boost compiled with >= C++14
+    # folly requires gcc 5+ and a version of boost compiled with >= C++14
     variant('cxxstd', default='14', values=('14', '17'), multi=False, description='Use the specified C++ standard when building.')
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
+    # Boost library dependencies:
+    # CMake threw errors when program_options and thread were not included.
     # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants)
-    depends_on('boost+context+container cxxstd=14', when='cxxstd=14')
-    depends_on('boost+context+container cxxstd=17', when='cxxstd=17')
+    depends_on('boost+container+context+exception+filesystem+program_options+regex+serialization+system+thread cxxstd=14', when='cxxstd=14')
+    depends_on('boost+container+context+exception+filesystem+program_options+regex+serialization+system+thread cxxstd=17', when='cxxstd=17')
 
     # required dependencies
     depends_on('gflags')
