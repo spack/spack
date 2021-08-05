7c615308d996c8d11189827ac2d130df728dfe14
Chris Richardson <chris@bpi.cam.ac.uk>
2021-08-05 16:44:13 +0100

    Updates for 0.2.0

--- var/spack/repos/builtin/packages/py-fenics-ffcx/package.py
+++ var/spack/repos/builtin/packages/py-fenics-ffcx/package.py
@@ -10,21 +10,23 @@ class PyFenicsFfcx(PythonPackage):
     """Next generation FEniCS Form Compiler"""
 
     homepage = "https://github.com/FEniCS/ffcx"
-    url = "https://github.com/FEniCS/ffcx/archive/0.1.0.tar.gz"
+    url = "https://github.com/FEniCS/ffcx/archive/v0.1.0.tar.gz"
     git = "https://github.com/FEniCS/ffcx.git"
     maintainers = ["js947", "chrisrichardson", "garth-wells"]
 
     version('main', branch='main')
     version('0.1.0', sha256='98a47906146ac892fb4a358e04cbfd04066f12d0a4cdb505a6b08ff0b1a17e89')
+    version('0.2.0', sha256='0457168cdba0e86ffbfc5b1d44f7b1e1b91fd8a73f54f1eff5998d4b61e6bd10')
 
     depends_on('python@3.7:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-cffi', type='run')
 
     depends_on('py-fenics-ufl@main', type='run', when='@main')
-    depends_on('py-fenics-ufl@2021.1.0', type='run', when='@0.1.0')
+    depends_on('py-fenics-ufl@2021.1.0', type='run', when='@0.1:')
 
     depends_on('py-fenics-basix@main', type='run', when='@main')
     depends_on('py-fenics-basix@0.1.0', type='run', when='@0.1.0')
+    depends_on('py-fenics-basix@0.2.0', type='run', when='@0.2.0')
 
     depends_on('py-numpy', type='run')
