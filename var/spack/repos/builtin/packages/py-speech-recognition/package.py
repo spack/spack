c8cc50f0f675ad284822abca5963b1ad83ab1edc
Jen Herting <jen@herting.cc>
2021-08-05 12:14:56 -0400

    [py-speech-recognition] Final cleanup
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-speech-recognition/package.py
+++ var/spack/repos/builtin/packages/py-speech-recognition/package.py
@@ -3,48 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-speech-recognition
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-speech-recognition
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PySpeechRecognition(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Library for performing speech recognition, with support
+    for several engines and APIs, online and offline."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/Uberi/speech_recognition"
     url      = "https://github.com/Uberi/speech_recognition/archive/refs/tags/3.8.1.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('3.8.1', sha256='82d3313db383409ddaf3e42625fb0c3518231a1feb5e2ed5473b10b3d5ece7bd')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
     depends_on('python@2.6:2.999,3.3:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
