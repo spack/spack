06da762e834792deca341c4541b62acb65e6808b
Betsy McPhail <betsy.mcphail@kitware.com>
2021-08-06 15:51:37 -0400

    Allow 'spack external find' to find executables on the system path (#22091)
    Allow 'spack external find' to find executables on the system path
    
    # Conflicts:
    #	lib/spack/spack/installer.py

--- lib/spack/spack/main.py
+++ lib/spack/spack/main.py
@@ -29,7 +29,7 @@ import llnl.util.filesystem as fs
 import llnl.util.tty as tty
 import llnl.util.tty.colify
 import llnl.util.tty.color as color
-from llnl.util.tty.log import log_output
+from llnl.util.tty.log import log_output, winlog
 
 import spack
 import spack.architecture
@@ -580,9 +580,14 @@ class SpackCommand(object):
 
         out = StringIO()
         try:
-            with log_output(out):
-                self.returncode = _invoke_command(
-                    self.command, self.parser, args, unknown)
+            if sys.platform == 'win32':
+                with winlog(out):
+                    self.returncode = _invoke_command(
+                        self.command, self.parser, args, unknown)
+            else:
+                with log_output(out):
+                    self.returncode = _invoke_command(
+                        self.command, self.parser, args, unknown)
 
         except SystemExit as e:
             self.returncode = e.code
