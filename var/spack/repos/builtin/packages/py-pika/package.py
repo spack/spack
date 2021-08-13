358aa522cd3400484f457510239fd8bba8067102
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-08-13 17:45:35 -0400

    pika: removed maintainer

--- var/spack/repos/builtin/packages/py-pika/package.py
+++ var/spack/repos/builtin/packages/py-pika/package.py
@@ -13,8 +13,6 @@ class PyPika(PythonPackage):
     git      = 'https://github.com/pika/pika.git'
     pypi     = 'pika/pika-1.2.0.tar.gz'
 
-    maintainers = ['gmr']
-
     version('1.2.0',  sha256='f023d6ac581086b124190cb3dc81dd581a149d216fa4540ac34f9be1e3970b89')
     version('1.1.0',  sha256='9fa76ba4b65034b878b2b8de90ff8660a59d925b087c5bb88f8fdbb4b64a1dbf')
     version('1.0.0',  sha256='fba41293b35c845bd96cfdd29981f0eeff91f705ac0c3ba361a771c4bfbc3485')
