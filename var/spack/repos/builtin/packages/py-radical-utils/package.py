cb419eeca14990f2266d39b5240dd0a4bdae6ba5
Mikhail Titov <mikhail.a.titov@gmail.com>
2021-08-13 12:52:21 -0400

    radical-utils: updated dependencies and set old versions deprecated

--- var/spack/repos/builtin/packages/py-radical-utils/package.py
+++ var/spack/repos/builtin/packages/py-radical-utils/package.py
@@ -16,19 +16,21 @@ class PyRadicalUtils(PythonPackage):
 
     maintainers = ['andre-merzky']
 
-    version('devel', branch='devel')
-    version('1.6.7', sha256='552f6c282f960ccd9d2401d686b0b3bfab35dfa94a26baeb2d3b4e45211f05a9')
+    version('develop', branch='devel')
+    version('1.6.7',   sha256='552f6c282f960ccd9d2401d686b0b3bfab35dfa94a26baeb2d3b4e45211f05a9')
 
-    depends_on('py-radical-gtod',       type=('build', 'run'))
-    depends_on('py-radical-gtod@devel', type=('build', 'run'), when='@devel')
+    version('0.45',    sha256='1333cff1a69532e51d4484fbac3fad6b172d415d2055a3141117c7cf8bdee6c5', deprecated=True)
+    version('0.41.1',  sha256='582900e0434f49b69885a89bc65dc787362756e1014d52a4afac0bb61bcaa3ce', deprecated=True)
 
-    depends_on('python@3.6:',           type=('build', 'run'))
-    depends_on('py-colorama',           type=('build', 'run'))
-    depends_on('py-msgpack',            type=('build', 'run'))
-    depends_on('py-netifaces',          type=('build', 'run'))
-    depends_on('py-ntplib',             type=('build', 'run'))
-    depends_on('py-pymongo',            type=('build', 'run'))
-    depends_on('py-pyzmq',              type=('build', 'run'))
-    depends_on('py-regex',              type=('build', 'run'))
-    depends_on('py-setproctitle',       type=('build', 'run'))
-    depends_on('py-setuptools',         type='build')
+    depends_on('py-radical-gtod', type=('build', 'run'))
+
+    depends_on('python@3.6:',     type=('build', 'run'))
+    depends_on('py-colorama',     type=('build', 'run'))
+    depends_on('py-msgpack',      type=('build', 'run'))
+    depends_on('py-netifaces',    type=('build', 'run'))
+    depends_on('py-ntplib',       type=('build', 'run'))
+    depends_on('py-pymongo',      type=('build', 'run'))
+    depends_on('py-pyzmq',        type=('build', 'run'))
+    depends_on('py-regex',        type=('build', 'run'))
+    depends_on('py-setproctitle', type=('build', 'run'))
+    depends_on('py-setuptools',   type='build')
