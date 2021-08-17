e545c6106a79441f8e37b52b897fbc86b1a521e9
Miheer Vaidya <m.vaidya@utah.edu>
2021-08-16 20:45:51 -0400

    Foster p7zip package
    This package should still go away and be replaced by
    upstream 7zip package see: https://www.phoronix.com/scan.php?page=news_item&px=7-Zip-21.01-Initial-Linux

--- var/spack/repos/builtin/packages/p7zip/package.py
+++ var/spack/repos/builtin/packages/p7zip/package.py
@@ -7,6 +7,7 @@
 class P7zip(MakefilePackage):
     """A Unix port of the 7z file archiver"""
 
+    maintainers = ['vmiheer']
     homepage = "http://p7zip.sourceforge.net"
     url      = "https://downloads.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2"
 
