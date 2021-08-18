9a72abb52b0d2ff9eea52f04cb7e48251e2f8f4e
Alberto Invernizzi <invernizzi@cscs.ch>
2021-08-18 18:11:03 +0200

    patch for cmake targets from blt

--- var/spack/repos/builtin/packages/umpire/package.py
+++ var/spack/repos/builtin/packages/umpire/package.py
@@ -50,6 +50,12 @@ class Umpire(CMakePackage, CudaPackage, ROCmPackage):
     patch('cmake_version_check.patch', when='@4.1')
     patch('missing_header_for_numeric_limits.patch', when='@4.1:5.0.1')
 
+    # https://llnl-blt.readthedocs.io/en/develop/tutorial/exporting_targets.html
+    # https://github.com/LLNL/Umpire/pull/541
+    patch('https://github.com/LLNL/Umpire/commit/5773ce9af88952c8d23f9bcdcb2e503ceda40763.patch',
+          sha256='f5c691752e4833a936bce224bbe0fe884d3afa84c5e5a4a481f59a12840159c9',
+          when='@:5.0.1 ^blt@0.4:')
+
     variant('fortran', default=False, description='Build C/Fortran API')
     variant('c', default=True, description='Build C API')
     variant('numa', default=False, description='Enable NUMA support')
