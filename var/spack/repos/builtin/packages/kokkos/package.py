b9990d980d0de6431f47ff6c0546d86c57c3a71d
Mikael Simberg <simberg@cscs.ch>
2021-08-20 12:48:46 +0200

    Require same C++ standard in HPX when used as a dependency in Kokkos

--- var/spack/repos/builtin/packages/kokkos/package.py
+++ var/spack/repos/builtin/packages/kokkos/package.py
@@ -195,12 +195,17 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("kokkos-nvcc-wrapper@master", when="@master+wrapper")
     conflicts("+wrapper", when="~cuda")
 
-    variant("std", default="14", values=["11", "14", "17", "20"], multi=False)
+    stds = ["11", "14", "17", "20"]
+    variant("std", default="14", values=stds, multi=False)
     variant("pic", default=False, description="Build position independent code")
 
     # nvcc does not currently work with C++17 or C++20
     conflicts("+cuda", when="std=17 ^cuda@:10.99.99")
     conflicts("+cuda", when="std=20")
+
+    # HPX should use the same C++ standard
+    for std in stds:
+        depends_on('hpx cxxstd={0}'.format(std), when='+hpx std={0}'.format(std))
 
     variant('shared', default=True, description='Build shared libraries')
 
