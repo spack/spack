28ea480852de6663fea7fc5c062a495d42230903
Terry Cojean <terry.cojean@kit.edu>
2021-08-25 18:46:24 +0200

    Change versions supporting smoke tests

--- var/spack/repos/builtin/packages/ginkgo/package.py
+++ var/spack/repos/builtin/packages/ginkgo/package.py
@@ -124,8 +124,8 @@ class Ginkgo(CMakePackage, CudaPackage, ROCmPackage):
     @run_after('install')
     def setup_build_tests(self):
         """Build and install the smoke tests."""
-        # For now only develop and next releases support this scheme.
-        if not self.spec.satisfies('@develop') and not self.spec.satisfies('@1.4.0:'):
+        # For now only 1.4.0 and later releases support this scheme.
+        if self.spec.satisfies('@:1.3.0'):
             return
         with working_dir(self.build_directory):
             make("test_install")
@@ -135,8 +135,8 @@ class Ginkgo(CMakePackage, CudaPackage, ROCmPackage):
 
     def test(self):
         """Run the smoke tests."""
-        # For now only develop and next releases support this scheme.
-        if not self.spec.satisfies('@develop') and not self.spec.satisfies('@1.4.0:'):
+        # For now only 1.4.0 and later releases support this scheme.
+        if self.spec.satisfies('@:1.3.0'):
             print("SKIPPED: smoke tests not supported with this Ginkgo version.")
             return
         files = [('test_install', [r'REFERENCE',
