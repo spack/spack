389644496ee458fe01ceffae4b95c6b80553d479
Jen Herting <jen@herting.cc>
2021-08-26 16:30:56 -0400

    [py-plotly] flake8

--- var/spack/repos/builtin/packages/py-plotly/package.py
+++ var/spack/repos/builtin/packages/py-plotly/package.py
@@ -16,11 +16,11 @@ class PyPlotly(PythonPackage):
     version('2.2.0', sha256='ca668911ffb4d11fed6d7fbb12236f8ecc6a7209db192326bcb64bdb41451a58')
 
     depends_on('python@3.6:', when='@5.2.2:', type=('build', 'run'))
-    
+
     depends_on('py-setuptools', type='build')
     depends_on('py-requests',   type=('build', 'run'))
     depends_on('py-six',        type=('build', 'run'))
-    
+
     depends_on('py-pytz',             when='@:2.2.0', type=('build', 'run'))
     depends_on('py-decorator@4.0.6:', when='@:2.2.0', type=('build', 'run'))
     depends_on('py-nbformat@4.2.0:',  when='@:2.2.0', type=('build', 'run'))
