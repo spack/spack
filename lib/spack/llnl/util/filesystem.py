b7bb88c5c7eeaaa965421da79656358cf530db82
Jared Popelar <jpopelar@txcorp.com>
2021-08-30 13:39:44 -0600

    Fixing library extension on Windows

--- lib/spack/llnl/util/filesystem.py
+++ lib/spack/llnl/util/filesystem.py
@@ -1696,7 +1696,9 @@ def find_libraries(libraries, root, shared=True, recursive=False):
         raise TypeError(message)
 
     # Construct the right suffix for the library
-    if shared is True:
+    if sys.platform == 'win32':
+        suffix = 'lib'
+    elif shared is True:
         suffix = 'dylib' if sys.platform == 'darwin' else 'so'
     else:
         suffix = 'a'
