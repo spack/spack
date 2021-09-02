9c7f3214ea828ac4a192d0d4666459246f8a2193
Ignacio Laguna <ilaguna@llnl.gov>
2021-09-02 14:18:03 -0700

    FPChecker: adding new package and conflicts

--- var/spack/repos/builtin/packages/fpchecker/package.py
+++ var/spack/repos/builtin/packages/fpchecker/package.py
@@ -13,15 +13,22 @@ class Fpchecker(CMakePackage):
     """
 
     homepage = "https://fpchecker.org/"
-    url      = "https://github.com/LLNL/FPChecker/archive/refs/tags/v0.3.1.tar.gz"
+    url      = "https://github.com/LLNL/FPChecker/archive/refs/tags/v0.3.2.tar.gz"
 
     maintainers = ['ilagunap']
 
-    version('0.3.1', sha256='5ff81f5743d453161c35402ea6f2b0edba062bb8c45f19153cb58cbae8d979ec')
+    version('0.3.2', sha256='aa591962734a3027c97ec050e126166912aaecfc2743b2d739eff844ea7418db')
 
     depends_on('llvm@12.0.1', type=('build', 'link'))
     depends_on('cmake@3.4:', type='build')
     depends_on('python@3:', type='run')
+
+    conflicts('%intel', 
+          msg='FPChecker cannot be built with Intel ICC. Please use LLVM.')
+    conflicts('%gcc', 
+          msg='FPChecker cannot be built with GCC. Please use LLVM.')
+    conflicts('%pgi', 
+          msg='FPChecker cannot be built with PGI. Please use LLVM.')
 
     def cmake_args(self):
         args = ['-DCMAKE_C_COMPILER=clang', '-DCMAKE_CXX_COMPILER=clang++']
