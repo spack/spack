510f869c3ae8ac2721debd29e98076212ee75852
Ignacio Laguna <ilaguna@llnl.gov>
2021-09-03 15:04:22 -0700

    FPChecker: adding new package version 0.3.4

--- var/spack/repos/builtin/packages/fpchecker/package.py
+++ var/spack/repos/builtin/packages/fpchecker/package.py
@@ -13,22 +13,15 @@ class Fpchecker(CMakePackage):
     """
 
     homepage = "https://fpchecker.org/"
-    url      = "https://github.com/LLNL/FPChecker/archive/refs/tags/v0.3.2.tar.gz"
+    url      = "https://github.com/LLNL/FPChecker/archive/refs/tags/v0.3.4.tar.gz"
 
     maintainers = ['ilagunap']
 
-    version('0.3.2', sha256='aa591962734a3027c97ec050e126166912aaecfc2743b2d739eff844ea7418db')
+    version('0.3.4', sha256='ecea778dcddc8347da86b02069e12d574a3ba27a4f7c6224bf492fbff6cd162a')
 
     depends_on('llvm@12.0.1')
     depends_on('cmake@3.4:', type='build')
     depends_on('python@3:', type='run')
-
-    conflicts('%intel', 
-          msg='FPChecker cannot be built with Intel ICC. Please use LLVM.')
-    conflicts('%gcc', 
-          msg='FPChecker cannot be built with GCC. Please use LLVM.')
-    conflicts('%pgi', 
-          msg='FPChecker cannot be built with PGI. Please use LLVM.')
 
     def cmake_args(self):
         args = ['-DCMAKE_C_COMPILER=clang', '-DCMAKE_CXX_COMPILER=clang++']
