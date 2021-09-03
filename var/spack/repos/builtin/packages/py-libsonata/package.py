e495d2894ff0faba4ecbc1c28a7a2e27ad6f9466
Sergio <sergio.rivasgomez@epfl.ch>
2021-09-03 10:00:58 +0200

    Adds missing versions in py-libsonata (#1274)

--- var/spack/repos/builtin/packages/py-libsonata/package.py
+++ var/spack/repos/builtin/packages/py-libsonata/package.py
@@ -12,6 +12,13 @@ class PyLibsonata(PythonPackage):
     homepage = "https://github.com/BlueBrain/libsonata"
     git = "https://github.com/BlueBrain/libsonata.git"
 
+    version('develop', branch='master', submodules=True, get_full_repo=True)
+    # Important: v0.1.9 contains an external dependency that cannot be easily
+    # resolved by Spack. Hence, the temp. versions v0.1.9.YYYYMMDD add fixes,
+    # which will be included in future official releases (v0.1.10 and above).
+    version('0.1.9.20210901', commit='653d104', submodules=True, get_full_repo=True)
+    version('0.1.9.20210721', commit='59b298e', submodules=True, get_full_repo=True)
+    # version('0.1.9', tag='v0.1.9', submodules=True, get_full_repo=True)
     version('0.1.8', tag='v0.1.8', submodules=True, get_full_repo=True)
     version('0.1.6', tag='v0.1.6', submodules=True, get_full_repo=True)
     version('0.1.5', tag='v0.1.5', submodules=True, get_full_repo=True)
