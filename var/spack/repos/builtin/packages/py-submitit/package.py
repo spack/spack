b389df0c4c72d73d5ab66cd3a0973188446a22f8
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2021-09-06 12:14:40 +0200

    Bump submitit (#1279)

--- var/spack/repos/builtin/packages/py-submitit/package.py
+++ var/spack/repos/builtin/packages/py-submitit/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -7,14 +7,17 @@ from spack import *
 
 
 class PySubmitit(PythonPackage):
-    """Submit Python functions for computation within a Slurm cluster."""
+    """Python toolbox for submitting jobs to Slurm."""
 
     homepage = "https://github.com/facebookincubator/submitit"
+    git      = "https://github.com/facebookincubator/submitit.git"
     url      = "https://pypi.io/packages/source/s/submitit/submitit-1.3.3.tar.gz"
 
-    version("1.3.3", sha256="efaa77b2df9ea9ee02545478cbfc377853ddf8016bff59df6988bebcf51ffa7e")
+    version('1.3.3.20210820', commit='6f9e1f67178b08b050576fe6bc02e4555568128a')
+    version('1.3.3', sha256='efaa77b2df9ea9ee02545478cbfc377853ddf8016bff59df6988bebcf51ffa7e')
 
-    depends_on("py-setuptools", type=("build", "run"))
+    depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type=('build', 'run'))
 
-    depends_on("py-cloudpickle@1.2.1:", type="run")
-    depends_on("py-typing-extensions@3.7.4.2:", type="run")
+    depends_on('py-cloudpickle@1.2.1:', type=('build', 'run'))
+    depends_on('py-typing-extensions@3.7.4.2:', type=('build', 'run'))
