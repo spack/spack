9e414345f3e857f8cbc8e89bcf659c2291a55528
Olivier Cessenat <cessenat@gmail.com>
2021-09-07 14:40:35 +0200

    visit: forgot to set executables

--- var/spack/repos/builtin/packages/visit/package.py
+++ var/spack/repos/builtin/packages/visit/package.py
@@ -62,6 +62,8 @@ class Visit(CMakePackage):
 
     extendable = True
 
+    executables = ['^xml2cmake$']
+
     version('develop', branch='develop')
     version('3.1.1', sha256='0b60ac52fd00aff3cf212a310e36e32e13ae3ca0ddd1ea3f54f75e4d9b6c6cf0')
     version('3.0.1', sha256='a506d4d83b8973829e68787d8d721199523ce7ec73e7594e93333c214c2c12bd')
