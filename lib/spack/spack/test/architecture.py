367ec82154369d1937afa22dc4ac020927d998a8
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-09-08 20:34:45 +0200

    Parametrized a few tests in test/architecture.py
    Using this parametrization it's easier to read the
    test and distinguish it from setup and teardown.

--- lib/spack/spack/test/architecture.py
+++ lib/spack/spack/test/architecture.py
@@ -2,10 +2,6 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-""" Test checks if the architecture class is created correctly and also that
-    the functions are looking for the correct architecture name
-"""
 import itertools
 import os
 import platform
@@ -18,101 +14,81 @@ import spack.platforms
 import spack.spec
 
 
-def test_dict_functions_for_architecture():
+@pytest.fixture
+def sample_arch():
+    """Sample test architecture"""
     arch = spack.architecture.Arch()
     arch.platform = spack.architecture.platform()
     arch.os = arch.platform.operating_system('default_os')
     arch.target = arch.platform.target('default_target')
-
-    new_arch = spack.architecture.Arch.from_dict(arch.to_dict())
-
-    assert arch == new_arch
-    assert isinstance(arch, spack.architecture.Arch)
-    assert isinstance(arch.platform, spack.architecture.Platform)
-    assert isinstance(arch.os, spack.architecture.OperatingSystem)
-    assert isinstance(arch.target, spack.architecture.Target)
-    assert isinstance(new_arch, spack.architecture.Arch)
-    assert isinstance(new_arch.platform, spack.architecture.Platform)
-    assert isinstance(new_arch.os, spack.architecture.OperatingSystem)
-    assert isinstance(new_arch.target, spack.architecture.Target)
+    return arch
 
 
-def test_platform():
-    output_platform_class = spack.architecture.real_platform()
+@pytest.fixture
+def current_host_platform():
+    """Return the platform of the current host as detected by the
+    'platform' stdlib package.
+    """
     if os.path.exists('/opt/cray/pe'):
-        my_platform_class = spack.platforms.Cray()
+        current_platform = spack.platforms.Cray()
     elif 'Linux' in platform.system():
-        my_platform_class = spack.platforms.Linux()
+        current_platform = spack.platforms.Linux()
     elif 'Darwin' in platform.system():
-        my_platform_class = spack.platforms.Darwin()
+        current_platform = spack.platforms.Darwin()
+    return current_platform
 
-    assert str(output_platform_class) == str(my_platform_class)
+
+def test_dict_round_trip(sample_arch):
+    """Check that a round trip through dict return an equivalent architecture"""
+    sample_arch_copy = spack.architecture.Arch.from_dict(sample_arch.to_dict())
+
+    assert sample_arch == sample_arch_copy
+    for current_arch in (sample_arch, sample_arch_copy):
+        assert isinstance(current_arch, spack.architecture.Arch)
+        assert isinstance(current_arch.platform, spack.architecture.Platform)
+        assert isinstance(current_arch.os, spack.architecture.OperatingSystem)
+        assert isinstance(current_arch.target, spack.architecture.Target)
 
 
-def test_boolness():
+def test_platform(current_host_platform):
+    """Check that current host detection return the correct platform"""
+    detected_platform = spack.architecture.real_platform()
+    assert str(detected_platform) == str(current_host_platform)
+
+
+@pytest.mark.parametrize('attribute_name,expected', [
     # Make sure architecture reports that it's False when nothing's set.
+    (None, False),
+    # ... and it reports True otherwise
+    ('platform', True),
+    ('os', True),
+    ('target', True)
+])
+def test_boolness(sample_arch, attribute_name, expected):
+    """Boolean checks on an architecture object"""
     arch = spack.architecture.Arch()
-    assert not arch
+    if attribute_name:
+        setattr(arch, attribute_name, getattr(sample_arch, attribute_name))
 
-    # Dummy architecture parts
-    plat = spack.architecture.platform()
-    plat_os = plat.operating_system('default_os')
-    plat_target = plat.target('default_target')
-
-    # Make sure architecture reports that it's True when anything is set.
-    arch = spack.architecture.Arch()
-    arch.platform = plat
-    assert arch
-
-    arch = spack.architecture.Arch()
-    arch.os = plat_os
-    assert arch
-
-    arch = spack.architecture.Arch()
-    arch.target = plat_target
-    assert arch
+    assert bool(arch) is expected
 
 
-def test_user_front_end_input(config):
-    """Test when user inputs just frontend that both the frontend target
-    and frontend operating system match
+@pytest.mark.parametrize('os_str,target_str,spec_str', [
+    ('frontend', 'frontend', 'libelf os=frontend target=frontend'),
+    ('backend', 'backend', 'libelf os=backend target=backend'),
+    ('default_os', 'default_target', 'libelf')
+])
+def test_frontend_and_backend_in_specs(config, os_str, target_str, spec_str):
+    """Test when user inputs 'frontend' or 'backend' or use the defaults that
+    both the target and the operating system match.
     """
-    platform = spack.architecture.platform()
-    frontend_os = str(platform.operating_system('frontend'))
-    frontend_target = platform.target('frontend')
+    current_platform = spack.architecture.platform()
+    expected_os = str(current_platform.operating_system(os_str))
+    expected_target = current_platform.target(target_str)
+    concrete_spec = spack.spec.Spec(spec_str).concretized()
 
-    frontend_spec = spack.spec.Spec('libelf os=frontend target=frontend')
-    frontend_spec.concretize()
-
-    assert frontend_os == frontend_spec.architecture.os
-    assert frontend_target == frontend_spec.architecture.target
-
-
-def test_user_back_end_input(config):
-    """Test when user inputs backend that both the backend target and
-    backend operating system match
-    """
-    platform = spack.architecture.platform()
-    backend_os = str(platform.operating_system("backend"))
-    backend_target = platform.target("backend")
-
-    backend_spec = spack.spec.Spec("libelf os=backend target=backend")
-    backend_spec.concretize()
-
-    assert backend_os == backend_spec.architecture.os
-    assert backend_target == backend_spec.architecture.target
-
-
-def test_user_defaults(config):
-    platform = spack.architecture.platform()
-    default_os = str(platform.operating_system("default_os"))
-    default_target = platform.target("default_target")
-
-    default_spec = spack.spec.Spec("libelf")  # default is no args
-    default_spec.concretize()
-
-    assert default_os == default_spec.architecture.os
-    assert default_target == default_spec.architecture.target
+    assert expected_os == concrete_spec.architecture.os
+    assert expected_target == concrete_spec.architecture.target
 
 
 def test_user_input_combination(config):
@@ -232,16 +208,15 @@ def test_satisfy_strict_constraint_when_not_concrete(
 
 
 @pytest.mark.parametrize('root_target_range,dep_target_range,result', [
-    (('x86_64:nocona', 'x86_64:core2', 'nocona')),  # pref not in intersection
-    (('x86_64:core2', 'x86_64:nocona', 'nocona')),
-    (('x86_64:haswell', 'x86_64:mic_knl', 'core2')),  # pref in intersection
-    (('ivybridge', 'nocona:skylake', 'ivybridge')),  # one side concrete
-    (('haswell:icelake', 'broadwell', 'broadwell')),
+    ('x86_64:nocona', 'x86_64:core2', 'nocona'),  # pref not in intersection
+    ('x86_64:core2', 'x86_64:nocona', 'nocona'),
+    ('x86_64:haswell', 'x86_64:mic_knl', 'core2'),  # pref in intersection
+    ('ivybridge', 'nocona:skylake', 'ivybridge'),  # one side concrete
+    ('haswell:icelake', 'broadwell', 'broadwell'),
     # multiple ranges in lists with multiple overlaps
-    (('x86_64:nocona,haswell:broadwell', 'nocona:haswell,skylake:',
-      'nocona')),
+    ('x86_64:nocona,haswell:broadwell', 'nocona:haswell,skylake:', 'nocona'),
     # lists with concrete targets, lists compared to ranges
-    (('x86_64,haswell', 'core2:broadwell', 'haswell'))
+    ('x86_64,haswell', 'core2:broadwell', 'haswell')
 ])
 @pytest.mark.usefixtures('mock_packages', 'config')
 def test_concretize_target_ranges(
