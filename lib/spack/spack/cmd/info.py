a8e7d447f7654532c698059f5498e6b8594c87ab
Tamara Dahlgren <dahlgren1@llnl.gov>
2021-09-07 18:51:05 -0700

    Feature: Add --preferred and --latest to

--- lib/spack/spack/cmd/info.py
+++ lib/spack/spack/cmd/info.py
@@ -37,6 +37,16 @@ def padder(str_list, extra=0):
     return pad
 
 
+def preferred_version(pkg):
+    # Here we sort first on the fact that a version is marked
+    # as preferred in the package, then on the fact that the
+    # version is not develop, then lexicographically
+    key_fn = lambda v: (pkg.versions[v].get('preferred', False),
+                        not v.isdevelop(),
+                        v)
+    return sorted(pkg.versions, key=key_fn).pop()
+
+
 def setup_parser(subparser):
     arguments.add_common_arguments(subparser, ['package'])
 
@@ -197,13 +207,7 @@ def print_text_info(pkg):
     else:
         pad = padder(pkg.versions, 4)
 
-        # Here we sort first on the fact that a version is marked
-        # as preferred in the package, then on the fact that the
-        # version is not develop, then lexicographically
-        key_fn = lambda v: (pkg.versions[v].get('preferred', False),
-                            not v.isdevelop(),
-                            v)
-        preferred = sorted(pkg.versions, key=key_fn).pop()
+        preferred = preferred_version(pkg)
         url = ''
         if pkg.has_code:
             url = fs.for_package_version(pkg, preferred)
