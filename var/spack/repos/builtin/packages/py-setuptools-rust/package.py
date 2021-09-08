09cb570a4c311ad2dd5a8e8ed41c391a679abe43
Tamara Dahlgren <dahlgren1@llnl.gov>
2021-09-08 09:44:23 -0700

    Remove checksum url workaround (issue 24668)

--- var/spack/repos/builtin/packages/py-setuptools-rust/package.py
+++ var/spack/repos/builtin/packages/py-setuptools-rust/package.py
@@ -13,11 +13,10 @@ class PySetuptoolsRust(PythonPackage):
     pypi = "setuptools-rust/setuptools-rust-0.12.1.tar.gz"
 
     version('0.12.1', sha256='647009e924f0ae439c7f3e0141a184a69ad247ecb9044c511dabde232d3d570e')
+
     # Version 0.10.6 is not available on pypi and can only be found on github
     version('0.10.6', sha256='1446d3985e4aaf4cc679fda8a48a73ac1390b627c8ae1bebe7d9e08bb3b33769',
-            # version specific url is not used here because spack checksum would
-            # use it instead of pypi (see #24668)
-            # url="https://github.com/PyO3/setuptools-rust/archive/v0.10.6.tar.gz",
+            url="https://github.com/PyO3/setuptools-rust/archive/v0.10.6.tar.gz",
             deprecated=True)
 
     depends_on('python@3.6:', when='@0.12:', type=('build', 'run'))
@@ -27,11 +26,3 @@ class PySetuptoolsRust(PythonPackage):
     depends_on('py-semantic-version@2.6.0:', type=('build', 'run'))
     depends_on('py-toml@0.9.0:', type=('build', 'run'))
     depends_on('rust', type='run')
-
-    # when #24668 is fixed remove url_for_version and use url= (see above) for
-    # version 0.10.6
-    def url_for_version(self, version):
-        if version >= Version('0.12.0'):
-            return 'https://files.pythonhosted.org/packages/source/s/setuptools-rust/setuptools-rust-{0}.tar.gz'.format(version)
-        else:
-            return 'https://github.com/PyO3/setuptools-rust/archive/v{0}.tar.gz'.format(version)
