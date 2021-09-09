f7e2fa7c77136975367c146898ebe155ff0fa35c
Nic McDonald <n.mcdonald83@gmail.com>
2021-09-09 12:20:45 -0600

    add nicmcd as maintainer of log4cxx

--- var/spack/repos/builtin/packages/log4cxx/package.py
+++ var/spack/repos/builtin/packages/log4cxx/package.py
@@ -12,6 +12,8 @@ class Log4cxx(CMakePackage):
     homepage = "https://logging.apache.org/log4cxx/latest_stable/"
     url      = "https://dlcdn.apache.org/logging/log4cxx/0.12.0/apache-log4cxx-0.12.0.tar.gz"
 
+    maintainers = ['nicmcd']
+
     version('0.12.0', sha256='bd5b5009ca914c8fa7944b92ea6b4ca6fb7d146f65d526f21bf8b3c6a0520e44')
 
     depends_on('cmake@3.13:', type='build')
