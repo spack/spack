4d009d6f014b61752d4b1c0f9130da547caaf832
Jen Herting <jen@herting.cc>
2021-09-09 15:39:10 -0400

    [py-tf-slim]
    - added homepage
    - added description
    - removed fixmes
    - added dependencies

--- var/spack/repos/builtin/packages/py-tf-slim/package.py
+++ var/spack/repos/builtin/packages/py-tf-slim/package.py
@@ -3,48 +3,20 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-tf-slim
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-tf-slim
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyTfSlim(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """TF-Slim is a lightweight library for defining, training
+    and evaluating complex models in TensorFlow. Components of
+    tf-slim can be freely mixed with native tensorflow, as well
+    as other frameworks."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/google-research/tf-slim"
     url      = "https://github.com/google-research/tf-slim/archive/refs/tags/v1.1.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.1.0', sha256='964cde4b7728a408dcd5c841ab6b93d95137ab4b60db28b10400f86286bfeb8b')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('py-setuptools', type='build')
+    depends_on('py-absl-py@0.2.2:', type='build')
+    depends_on('py-tensorflow', type='run')
