ceb94bd6ae7b63bc4abe111f8cf1da4a81c7f6d9
Nic McDonald <nicmcd@users.noreply.github.com>
2021-09-13 05:17:08 -0600

    Log4cxx add v0.12.0, convert to CMakePackage (#25875)

--- var/spack/repos/builtin/packages/log4cxx/package.py
+++ var/spack/repos/builtin/packages/log4cxx/package.py
@@ -6,25 +6,22 @@
 from spack import *
 
 
-class Log4cxx(AutotoolsPackage):
+class Log4cxx(CMakePackage):
     """A C++ port of Log4j"""
 
     homepage = "https://logging.apache.org/log4cxx/latest_stable/"
-    url      = "http://mirror.netcologne.de/apache.org/logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.tar.gz"
+    url      = "https://dlcdn.apache.org/logging/log4cxx/0.12.0/apache-log4cxx-0.12.0.tar.gz"
 
-    version('0.10.0', sha256='0de0396220a9566a580166e66b39674cb40efd2176f52ad2c65486c99c920c8c')
+    maintainers = ['nicmcd']
+
+    version('0.12.0', sha256='bd5b5009ca914c8fa7944b92ea6b4ca6fb7d146f65d526f21bf8b3c6a0520e44')
+
+    depends_on('cmake@3.13:', type='build')
 
     depends_on('apr-util')
     depends_on('apr')
+    depends_on('zlib')
     depends_on('zip')
 
-    build_directory = 'spack-build'
-
-    # patches from https://aur.archlinux.org/packages/log4cxx/
-    patch('log4cxx-0.10.0-missing_includes.patch')
-    patch('log4cxx-0.10.0-narrowing-fixes-from-upstream.patch')
-
-    def configure_args(self):
-        args = ['--disable-static']
-
-        return args
+    def cmake_args(self):
+        return [self.define('BUILD_TESTING', 'off')]
