a7d2e86eb94f040f3644f705540df1b616b1da99
Weston Ortiz <weston@wortiz.com>
2021-09-13 08:03:38 -0600

    isort fix

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -7,9 +7,10 @@ import os
 import sys
 
 from spack import *
+from spack.build_environment import dso_suffix
 from spack.operating_systems.mac_os import macos_version
 from spack.pkg.builtin.kokkos import Kokkos
-from spack.build_environment import dso_suffix
+
 
 # Trilinos is complicated to build, as an inspiration a couple of links to
 # other repositories which build it:
