7ab5a821e0781a349582d9c731dd9746c5e2860c
Harmen Stoppels <harmenstoppels@gmail.com>
2021-09-14 17:33:02 +0200

    Make `spack -e [env] spec` show environment root specs

--- lib/spack/spack/test/cmd/spec.py
+++ lib/spack/spack/test/cmd/spec.py
@@ -7,8 +7,9 @@ import re
 
 import pytest
 
+import spack.environment as ev
 import spack.spec
-from spack.main import SpackCommand
+from spack.main import SpackCommand, SpackCommandError
 
 pytestmark = pytest.mark.usefixtures('config', 'mutable_mock_repo')
 
@@ -85,6 +86,20 @@ def test_spec_deptypes_edges():
 
 
 def test_spec_returncode():
-    with pytest.raises(spack.main.SpackCommandError):
+    with pytest.raises(SpackCommandError):
         spec()
     assert spec.returncode == 1
+
+
+def test_env_aware_spec(mutable_mock_env_path):
+    env = ev.create('test')
+    env.add('mpileaks')
+
+    with env:
+        output = spec()
+        assert 'mpileaks@2.3' in output
+        assert 'callpath@1.0' in output
+        assert 'dyninst@8.2' in output
+        assert 'libdwarf@20130729' in output
+        assert 'libelf@0.8.1' in output
+        assert 'mpich@3.0.4' in output
