8bb0e291013ced47a35fc21103d7a7c21c578855
Joe Schoonover <joe@fluidnumerics.com>
2021-09-13 23:28:38 -0600

    Add v1.1.0 for feq-parse

--- var/spack/repos/builtin/packages/feq-parse/package.py
+++ var/spack/repos/builtin/packages/feq-parse/package.py
@@ -13,9 +13,15 @@ class FeqParse(CMakePackage):
 
     homepage = "https://github.com/FluidNumerics/feq-parse"
     url      = "https://github.com/FluidNumerics/feq-parse/archive/v1.0.0.tar.gz"
+    baseurl  = "https://github.com/FluidNumerics/feq-parse"
 
     maintainers = ['schoonovernumerics']
 
-    version('1.0.2', sha256='1cd1db7562908ea16fc65dc5268b654405d0b3d9dcfe11f409949c431b48a3e8')
+    version('1.1.0', 
+            sha256='1cd1db7562908ea16fc65dc5268b654405d0b3d9dcfe11f409949c431b48a3e8',
+            url = baseurl+'/archive/refs/tags/v1.1.0.tar.gz')
+    version('1.0.2',
+            sha256='1cd1db7562908ea16fc65dc5268b654405d0b3d9dcfe11f409949c431b48a3e8',
+            url = baseurl+'/archive/refs/tags/v1.0.2.tar.gz')
 
     depends_on('cmake@3.0.2:', type='build')
