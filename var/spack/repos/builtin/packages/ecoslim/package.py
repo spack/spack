0353ad130f7397f1f26fc65bbc496ec6e9f925fa
Steven Smith <smith84@llnl.gov>
2021-09-14 17:34:14 -0700

    Ecoslim package

---
+++ var/spack/repos/builtin/packages/ecoslim/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+class Ecoslim(CMakePackage):
+    """EcoSLIM is a Lagrangian, particle-tracking code that simulates
+    advective and diffusive movement of water parcels. This code can
+    be used to simulate age, diagnose travel times, source water
+    composition and flowpaths. It integrates seamlessly with
+    ParFlow-CLM."""
+
+    homepage = "https://github.com/reedmaxwell/EcoSLIM"
+    url      = "https://github.com/reedmaxwell/EcoSLIM/archive/refs/tags/1.3.tar.gz"
+    git      = "git@github.com:reedmaxwell/EcoSLIM.git"
+
+    maintainers = ['smithsg84']
+
+    #    version('develop', branch='master')
+    version('develop', branch='develop')
+    version('1.3.0', sha256='b532e570b4767e4fa84123d8773732150679e8e3d7fecd5c6e99fb1d4dc57b84')
+
+    variant('build_type', default='Release', description='CMake build type',
+            values=('Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel'))
+
+    def cmake_args(self):
+        """Populate cmake arguments for EcoSLIM."""
+        spec = self.spec
+
+        cmake_args = []
+
+        return cmake_args
+
