249cd11e46dcfa536b116153b917894e5228cd68
Steven Smith <smith84@llnl.gov>
2021-09-15 16:32:28 -0700

    Update var/spack/repos/builtin/packages/ecoslim/package.py
    Co-authored-by: Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>

--- var/spack/repos/builtin/packages/ecoslim/package.py
+++ var/spack/repos/builtin/packages/ecoslim/package.py
@@ -17,7 +17,7 @@ class Ecoslim(CMakePackage):
     maintainers = ['reedmaxwell', 'lecondon', 'smithsg84']
 
     version('1.3', sha256='b532e570b4767e4fa84123d8773732150679e8e3d7fecd5c6e99fb1d4dc57b84')
-    version('develop', branch='develop')
+    version('master', branch='master')
 
     def cmake_args(self):
         """Populate cmake arguments for EcoSLIM."""
