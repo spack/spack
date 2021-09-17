d4df8c846a017ab79f02274937598d4a01d89f49
sreenivasa murthy kolam <sreenivasamurthy.kolam@amd.com>
2021-09-17 22:39:30 +0000

    fix review comments -remove the git download of yamp-cpp

--- var/spack/repos/builtin/packages/rocm-validation-suite/package.py
+++ var/spack/repos/builtin/packages/rocm-validation-suite/package.py
@@ -35,6 +35,7 @@ class RocmValidationSuite(CMakePackage):
     patch('001-fixes-for-rocblas-rocm-smi-install-prefix-path.patch')
     patch('002-remove-force-setting-hip-inc-path.patch', when='@4.1.0:')
     patch('003-cmake-change-to-remove-installs-and-sudo.patch', when='@4.1.0:')
+    patch('004-remove-git-download-yaml-cpp-use-yaml-cpp-recipe.patch',when='@4.3.0:')
 
     depends_on('cmake@3.5:', type='build')
     depends_on('zlib', type='link')
@@ -56,5 +57,7 @@ class RocmValidationSuite(CMakePackage):
         return [
             self.define('HIP_INC_DIR', self.spec['hip'].prefix),
             self.define('ROCM_SMI_DIR', self.spec['rocm-smi-lib'].prefix),
-            self.define('ROCBLAS_DIR', self.spec['rocblas'].prefix)
+            self.define('ROCBLAS_DIR', self.spec['rocblas'].prefix),
+            self.define('YAML_INC_DIR', self.spec['yaml-cpp'].prefix.include),
+            self.define('YAML_LIB_DIR', self.spec['yaml-cpp'].prefix.lib64)
         ]
