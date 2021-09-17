af52a098dfffc1f831e2337d9e37785d1e4ddc76
Ivan Razumov <iarspider@gmail.com>
2021-09-17 13:17:51 +0200

    Add shared and static variants to gosamcontrib

--- var/spack/repos/builtin/packages/gosam-contrib/package.py
+++ var/spack/repos/builtin/packages/gosam-contrib/package.py
@@ -17,6 +17,25 @@ class GosamContrib(AutotoolsPackage):
     version('2.0', sha256='c05beceea74324eb51c1049773095e2cb0c09c8c909093ee913d8b0da659048d')
     version('1.0', sha256='a29d4232d9190710246abc2ed97fdcd8790ce83580f56a360f3456b0377c40ec')
 
+    variant('shared', default=False, description='Build shared libraries')
+    variant('static', default=True, description='Build static libraries')
+    variant('pic', default=False, description='Build position-independent code')
+
+    conflicts('~shared', when='~static', msg='Please enable at least one of shared or static')
+    conflicts('~static', when='~shared', msg='Please enable at least one of shared or static')
+
     def configure_args(self):
-        args = ["FFLAGS=-std=legacy"]
+        args = []
+        args += self.enable_or_disable('shared')
+        args += self.enable_or_disable('static')
+
+        if '+pic' in spec:
+            args.extend([
+                'CFLAGS={0}'.format(self.compiler.cc_pic_flag),
+                'CXXFLAGS={0}'.format(self.compiler.cxx_pic_flag),
+                'FFLAGS={0} -std=legacy'.format(self.compiler.f77_pic_flag)
+            ])
+        else:
+            args += "FFLAGS=-std=legacy"
+
         return args
