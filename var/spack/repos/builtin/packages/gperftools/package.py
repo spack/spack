0282e55e1def98d0cd6dcc269ce9f47393bffc13
iarspider <iarspider@gmail.com>
2021-09-20 14:15:17 +0200

    Flake-8

--- var/spack/repos/builtin/packages/gperftools/package.py
+++ var/spack/repos/builtin/packages/gperftools/package.py
@@ -32,7 +32,8 @@ class Gperftools(AutotoolsPackage):
     def configure_args(self):
         args = []
         args += self.enable_or_disable("sized-delete", variant_name='sized_delete')
-        args += self.enable_or_disable("dynamic-sized-delete-support", variant_name='dynamic_sized_delete_support')
+        args += self.enable_or_disable("dynamic-sized-delete-support", 
+                                       variant_name='dynamic_sized_delete_support')
         args += self.enable_or_disable("debugalloc")
         args += self.enable_or_disable("libunwind")
 
