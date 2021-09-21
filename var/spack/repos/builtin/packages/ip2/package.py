c1b71e45343ca7eee57e659132662c67a6c20110
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2021-09-21 14:49:13 -0600

    ip2: add NOAA software maintainers and deprecation note (#26107)
    * added NOAA software maintainers to maintainer list, added comment about library being deprecated
    
    * deleted trailing whitespace

--- var/spack/repos/builtin/packages/ip2/package.py
+++ var/spack/repos/builtin/packages/ip2/package.py
@@ -7,14 +7,17 @@ from spack import *
 
 
 class Ip2(CMakePackage):
-    """The NCEP general interpolation library 2 (ip2lib) contains Fortran 90
-    subprograms to be used for interpolating between nearly all grids used at
-    NCEP."""
+    """The NCEP general interpolation library 2 (ip2lib) contains Fortran
+    90 subprograms to be used for interpolating between nearly all
+    grids used at NCEP. This library is deprecated; all functionality
+    has been moved to the ip library.
+
+    This is part of the NCEPLIBS project."""
 
     homepage = "https://github.com/NOAA-EMC/NCEPLIBS-ip2"
     url      = "https://github.com/NOAA-EMC/NCEPLIBS-ip2/archive/refs/tags/v1.1.2.tar.gz"
 
-    maintainers = ['t-brown']
+    maintainers = ['t-brown', 'kgerheiser', 'Hang-Lei-NOAA', 'edwardhartnett']
 
     version('1.1.2', sha256='73c6beec8fd463ec7ccba3633d8c5d53d385c43d507367efde918c2db0af42ab')
 
