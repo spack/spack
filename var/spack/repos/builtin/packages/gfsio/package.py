5097a41675cb565b89e28741cd145f828dce1e88
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2021-09-21 19:20:47 -0600

    gfsio: add NOAA software maintainers  (#26106)
    * added NOAA software maintainers to maintainer list
    
    * added comment about NCEPLIBS

--- var/spack/repos/builtin/packages/gfsio/package.py
+++ var/spack/repos/builtin/packages/gfsio/package.py
@@ -8,11 +8,13 @@ from spack import *
 
 class Gfsio(CMakePackage):
     """The GFSIO library provides an API to convert GFS Gaussian output into
-    grib output."""
+    grib output.
+
+    This is part of the NOAA NCEPLIBS project."""
 
     homepage = "https://github.com/NOAA-EMC/NCEPLIBS-gfsio"
     url      = "https://github.com/NOAA-EMC/NCEPLIBS-gfsio/archive/refs/tags/v1.4.1.tar.gz"
 
-    maintainers = ['t-brown']
+    maintainers = ['t-brown', 'kgerheiser', 'Hang-Lei-NOAA', 'edwardhartnett']
 
     version('1.4.1', sha256='eab106302f520600decc4f9665d7c6a55e7b4901fab6d9ef40f29702b89b69b1')
