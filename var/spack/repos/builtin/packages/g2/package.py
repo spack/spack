54ea7caf3fbabf57eb484dca3a696ab2b0452949
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2021-09-24 00:17:07 -0600

    g2: add maintainers and version 3.4.5 (#26105)
    * added new release, added noaa software maintainers to maintainer list
    
    * updated comment
    
    * removed trailing whitespace

--- var/spack/repos/builtin/packages/g2/package.py
+++ var/spack/repos/builtin/packages/g2/package.py
@@ -9,13 +9,16 @@ from spack import *
 class G2(CMakePackage):
     """Utilities for coding/decoding GRIB2 messages. This library contains
     Fortran 90 decoder/encoder routines for GRIB edition 2, as well as
-    indexing/searching utility routines."""
+    indexing/searching utility routines.
+
+    This is part of the NCEPLIBS project."""
 
     homepage = "https://noaa-emc.github.io/NCEPLIBS-g2"
     url      = "https://github.com/NOAA-EMC/NCEPLIBS-g2/archive/refs/tags/v3.4.3.tar.gz"
 
-    maintainers = ['t-brown']
+    maintainers = ['t-brown', 'kgerheiser', 'Hang-Lei-NOAA', 'edwardhartnett']
 
+    version('3.4.5', sha256='c18e991c56964953d778632e2d74da13c4e78da35e8d04cb742a2ca4f52737b6')
     version('3.4.3', sha256='679ea99b225f08b168cbf10f4b29f529b5b011232f298a5442ce037ea84de17c')
 
     depends_on('jasper')
