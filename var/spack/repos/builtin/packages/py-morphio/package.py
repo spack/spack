11154ef9a6cb5504efd89e86ab4f097b2ea931cc
Sergio <sergio.rivasgomez@epfl.ch>
2021-09-24 14:36:28 +0200

    Add missing dependency to Ninja in py-morphio (#1308)

--- var/spack/repos/builtin/packages/py-morphio/package.py
+++ var/spack/repos/builtin/packages/py-morphio/package.py
@@ -22,6 +22,7 @@ class PyMorphio(PythonPackage):
     depends_on('py-setuptools', type='build')
     depends_on('py-setuptools-scm', type='build')
 
+    depends_on('ninja', type='build')
     depends_on('cmake@3.2:', type='build')
     depends_on('py-numpy@1.14.1:', type='run')
     depends_on('hdf5', type=('build', 'run'))
