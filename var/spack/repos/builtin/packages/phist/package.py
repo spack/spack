a7d2832dcd31ddbfc021bd4100bed97a15ea4e92
Jonas Thies <16190001+jthies@users.noreply.github.com>
2021-09-25 12:46:34 +0200

    add a comment ot the patch

--- var/spack/repos/builtin/packages/phist/package.py
+++ var/spack/repos/builtin/packages/phist/package.py
@@ -103,6 +103,8 @@ class Phist(CMakePackage):
 
     # ###################### Patches ##########################
 
+    # resolve #22758: while SSE instructions are handled correctly, but a compile-time
+    # error will occur unless -DNO_WARN_X86_INTRINSICS is defined.
     patch('ppc64_sse.patch', when='@1.7.4:1.9.4')
     patch('update_tpetra_gotypes.patch', when='@:1.8')
 >>>>>>> add a patch for the case +host arch=ppc64le (fixes #22758)
