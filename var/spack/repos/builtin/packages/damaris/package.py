f9a520ddea04447cb0d726cdf3a8e4aa78404535
Josh Bowden <joshua-charles.bowden@inria.fr>
2021-09-27 11:20:04 +0200

    added maintainer filed and removed extra blank lines

--- var/spack/repos/builtin/packages/damaris/package.py
+++ var/spack/repos/builtin/packages/damaris/package.py
@@ -3,18 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
 from spack import *
 
 
 class Damaris(CMakePackage):
-    """Damaris is a middleware for I/O and in situ analytics
+    """Damaris is a middleware for asynchronous I/O and in situ analytics
     targeting large-scale, MPI-based HPC simulations."""
 
     homepage = "https://project.inria.fr/damaris/"
     git      = "https://gitlab.inria.fr/Damaris/damaris.git"
+    maintainers = ['jcbowden']
 
-    # version('master', branch='master')
     version('1.5.0',  tag='v1.5.0')
     version('1.3.3',  tag='v1.3.3')
     version('1.3.2',  tag='v1.3.2')
@@ -37,11 +36,9 @@ class Damaris(CMakePackage):
     depends_on('visit+mpi', when='+visit')
 
     def cmake_args(self):
-
         args = []
         if(not self.spec.variants['static'].value):
             args.extend(['-DBUILD_SHARED_LIBS=ON'])
-
         args.extend(['-DCMAKE_CXX_COMPILER=%s' % self.spec['mpi'].mpicxx])
         args.extend(['-DCMAKE_C_COMPILER=%s' % self.spec['mpi'].mpicc])
         args.extend(['-DBOOST_ROOT=%s' % self.spec['boost'].prefix])
@@ -61,10 +58,9 @@ class Damaris(CMakePackage):
             args.extend(['-DENABLE_CATALYST:BOOL=ON'])
             args.extend(['-DParaView_ROOT:PATH=%s'
                          % self.spec['catalyst'].prefix])
-        
+
         if (self.spec.variants['examples'].value):
             args.extend(['-DENABLE_EXAMPLES:BOOL=ON'])
-
 
         if (self.spec.variants['visit'].value):
             args.extend(['-DENABLE_VISIT:BOOL=ON'])
