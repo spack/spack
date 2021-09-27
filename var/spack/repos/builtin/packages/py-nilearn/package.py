55fddbb1cb68e9d308a0aeffafa5f5e2aa88810b
Manuela Kuhn <manuela.kuhn@ovgu.de>
2021-09-27 21:33:16 +0200

    py-nilearn: add 0.8.1

--- var/spack/repos/builtin/packages/py-nilearn/package.py
+++ var/spack/repos/builtin/packages/py-nilearn/package.py
@@ -12,6 +12,7 @@ class PyNilearn(PythonPackage):
     homepage = "https://nilearn.github.io/"
     pypi     = "nilearn/nilearn-0.7.1.tar.gz"
 
+    version('0.8.1', sha256='a0489940855130f35bbc4cac0750479a6f82025215ea7b1d778faca064219298')
     version('0.8.0', sha256='f2d3dc81005f829f3a183efa6c90d698ea6818c06264d2e3f03e805c4340febb')
     version('0.7.1', sha256='8b1409a5e1f0f6d1a1f02555c2f11115a2364f45f1e57bcb5fb3c9ea11f346fa')
 
