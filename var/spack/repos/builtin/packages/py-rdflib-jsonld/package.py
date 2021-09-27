b932c94442f8b5e285ca22d733edb3e25c1ef0e8
Nabil-AL <63203348+Nabil-AL@users.noreply.github.com>
2021-09-27 09:18:11 +0200

    Add rdflib-jsonld version 0.5.0 to be used by py-nexusforge (#1309)

--- var/spack/repos/builtin/packages/py-rdflib-jsonld/package.py
+++ var/spack/repos/builtin/packages/py-rdflib-jsonld/package.py
@@ -10,10 +10,10 @@ class PyRdflibJsonld(PythonPackage):
     """RDFLib plugin providing JSON-LD parsing and serialization."""
 
     homepage = "https://github.com/RDFLib/rdflib-jsonld"
-    url      = "https://pypi.io/packages/source/r/rdflib-jsonld/rdflib-jsonld-0.4.0.tar.gz"
+    url      = "https://pypi.io/packages/source/r/rdflib-jsonld/rdflib-jsonld-0.5.0.tar.gz"
 
+    version('0.5.0', sha256='4f7d55326405071c7bce9acf5484643bcb984eadb84a6503053367da207105ed')
     version('0.4.0', sha256='144774786a2fc7de09b24a9309cbcccc78bc48b152536d2ea1c1df2ad715bc2d')
 
     depends_on('py-setuptools', type='build')
-
     depends_on('py-rdflib', type='run')
