f88ac8ec36ef470279e9e43be733466ad7fab01a
Robert Romero <1013414+NessieCanCode@users.noreply.github.com>
2021-09-28 15:25:40 -0700

    Update package.py

--- var/spack/repos/builtin/packages/cddlib/package.py
+++ var/spack/repos/builtin/packages/cddlib/package.py
@@ -16,14 +16,12 @@ class Cddlib(AutotoolsPackage):
     homepage = "https://people.inf.ethz.ch/fukudak/cdd_home"
     url      = "https://github.com/cddlib/cddlib/archive/refs/tags/0.94h.tar.gz"
     maintainers = ['NessieCanCode']
-    
     version('0.94h', sha256='7382782c3834214b022c8b2898ed775a7bf915f2cb2acb73fa045d6fd9a3de33')
 
     # Note: It should be possible to build cddlib also without gmp
 
     depends_on("gmp")
     depends_on("libtool", type="build")
-    
     def url_for_version(self, version):
         url = "https://github.com/cddlib/cddlib/archive/refs/tags/{0}.tar.gz"
         return url.format(version.dotted)
