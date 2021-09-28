3c5a4157b019d014a390db30c9a1c600ba6db4c1
bernhardkaindl <43588962+bernhardkaindl@users.noreply.github.com>
2021-09-28 17:37:09 +0200

    py-anuga: add git main version to support build with python@3.5: (#26248)
    * py-anuga: add git main version to support build with python@3.5:
    
    py-anuga's main branch has been converted to Python-3 recently.
    
    * py-triangle: use pypi, py-anuga: Fixed depends and test suite works now

---
+++ var/spack/repos/builtin/packages/py-triangle/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyTriangle(PythonPackage):
+    """Python bindings to the triangle library"""
+
+    homepage = 'https://github.com/drufat/triangle'
+    pypi     = 'triangle/triangle-20200424.tar.gz'
+
+    version('20200424', sha256='fc207641f8f39986f7d2bee1b91688a588cd235d2e67777422f94e61fece27e9')
+
+    depends_on('triangle',  type=('build', 'run'))
+    depends_on('py-numpy',  type=('build', 'run'))
+    depends_on('py-cython', type=('build'))
