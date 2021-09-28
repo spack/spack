4f4984e3138ab9b2a8204ef580ec385bf71da14f
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2021-09-28 09:55:09 -0600

    wrf-io: added NOAA software maintainers, added openmp variant (#26113)

--- var/spack/repos/builtin/packages/wrf-io/package.py
+++ var/spack/repos/builtin/packages/wrf-io/package.py
@@ -9,14 +9,22 @@ from spack import *
 class WrfIo(CMakePackage):
     """The WRFIO package is a lightweight WRF-IO API library for Unified
     Post Processor (UPP). It reads wrf forecasts (WRF state plus
-    diagnostics)."""
+    diagnostics).
+
+    This is part of the NCEPLIBS project."""
 
     homepage = "https://noaa-emc.github.io/NCEPLIBS-wrf_io"
     url      = "https://github.com/NOAA-EMC/NCEPLIBS-wrf_io/archive/refs/tags/v1.2.0.tar.gz"
 
-    maintainers = ['t-brown']
+    maintainers = ['t-brown', 'kgerheiser', 'Hang-Lei-NOAA', 'edwardhartnett']
 
     version('1.2.0', sha256='000cf5294a2c68460085258186e1f36c86d3d0d9c433aa969a0f92736b745617')
 
+    variant('openmp', default=False, description='Enable multithreading with OpenMP')
+
     depends_on('netcdf-c')
     depends_on('netcdf-fortran')
+
+    def cmake_args(self):
+        args = [self.define_from_variant('OPENMP', 'openmp')]
+        return args
