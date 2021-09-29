44cbfedbcbc60a47a403aadf0732539cafaaac1d
Harmen Stoppels <harmenstoppels@gmail.com>
2021-09-29 11:14:21 +0200

    Un-unset TERM and DISPLAY
    For interactive `spack build-env`'s this is undesired behavior

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -173,10 +173,6 @@ def clean_environment():
 
     # Affects GNU make, can e.g. indirectly inhibit enabling parallel build
     env.unset('MAKEFLAGS')
-    # Could make testsuites attempt to color their output
-    env.unset('TERM')
-    # Tests of GUI widget libraries might try to connect to an X server
-    env.unset('DISPLAY')
 
     # Avoid that libraries of build dependencies get hijacked.
     env.unset('LD_PRELOAD')
