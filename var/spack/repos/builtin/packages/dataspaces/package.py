03185925e014c052820a8c24e2b0788b65f46ea3
Philip Davis <philip.e.davis@rutgers.edu>
2021-09-29 04:57:10 +0000

    Fixed license

--- var/spack/repos/builtin/packages/dataspaces/package.py
+++ var/spack/repos/builtin/packages/dataspaces/package.py
@@ -1,7 +1,7 @@
 # Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
 
