59c8cd0bcb51be9cfe1f04719a274c53e56b83b3
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2021-09-29 21:34:18 +0200

    py-jupyterlab: add 3.0.14, 3.0.18 and 3.1.14 (#26339)

--- var/spack/repos/builtin/packages/py-jupyterlab/package.py
+++ var/spack/repos/builtin/packages/py-jupyterlab/package.py
@@ -14,7 +14,10 @@ class PyJupyterlab(PythonPackage):
     # Skip 'jupyterlab.tests' packages
     import_modules = ['jupyterlab', 'jupyterlab.handlers']
 
+    version('3.1.14', sha256='13174cb6076dd5da6f1b85725ccfcc9518d8f98e86b8b644fc89b1dfaeda63a9')
+    version('3.0.18', sha256='0e4bb4b89014607a16658b54f13df2f0af14f3c286109a0e14d5a46cbbe28caf')
     version('3.0.16', sha256='7ad4fbe1f6d38255869410fd151a8b15692a663ca97c0a8146b3f5c40e275c23')
+    version('3.0.14', sha256='713a84991dfcca8c0bc260911f1bd54ac25a386a86285713b9555a60f795059b')
     version('2.2.7', sha256='a72ffd0d919cba03a5ef8422bc92c3332a957ff97b0490494209c83ad93826da')
     version('2.1.0', sha256='8c239aababf5baa0b3d36e375fddeb9fd96f3a9a24a8cda098d6a414f5bbdc81')
 
@@ -22,7 +25,7 @@ class PyJupyterlab(PythonPackage):
     depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-jupyter-packaging@0.9:1.999', when='@3.0.15:', type='build')
-    depends_on('py-jupyter-packaging@0.7.3:0.7.999', when='@3.0.0:3.0.14', type='build')
+    depends_on('py-jupyter-packaging@0.7.3:0.7.999', when='@3.0.0:3.0.14', type=('build', 'run'))
     # dependency on py-jinja2@2.1 seems to be a migration issue from the switch
     # to setup.cfg in 3.0.15, leave it a 2.10
     depends_on('py-jinja2@2.10:', type=('build', 'run'))
