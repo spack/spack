ca8523751b91c45918004953c2b2c7d3cac5e638
hategan <mhategan@gmail.com>
2021-09-30 17:02:30 -0700

    Added random-order plugin for py-pytest.

---
+++ var/spack/repos/builtin/packages/py-pytest-random-order/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+class PyPytestRandomOrder(PythonPackage):
+    """
+    Randomise the order in which pytest tests are run with some control over the randomness
+    """
+
+    homepage = "https://github.com/jbasko/pytest-random-order"
+    pypi     = "pytest-random-order/pytest-random-order-1.0.4.tar.gz"
+
+
+    version('1.0.4', sha256='6b2159342a4c8c10855bc4fc6d65ee890fc614cb2b4ff688979b008a82a0ff52')
+
+    depends_on('python@3.4:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+
