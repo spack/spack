0ac53ef42fb447537431bbd60480fb1f28227e47
Mihael Hategan <hategan@mcs.anl.gov>
2021-10-01 14:50:55 -0700

    Added py-sqlalchemy-stubs package. (#26414)
    Co-authored-by: Bernhard Kaindl <bernhardkaindl7@gmail.com>

---
+++ var/spack/repos/builtin/packages/py-sqlalchemy-stubs/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+class PySqlalchemyStubs(PythonPackage):
+    """
+    Mypy plugin and stubs for SQLAlchemy
+    """
+
+    homepage = "https://github.com/dropbox/sqlalchemy-stubs"
+    pypi     = "sqlalchemy-stubs/sqlalchemy-stubs-0.4.tar.gz"
+
+    version('0.4', sha256='c665d6dd4482ef642f01027fa06c3d5e91befabb219dc71fc2a09e7d7695f7ae')
+
+    depends_on('python@3.5:',                 type=('build', 'run'))
+    depends_on('py-mypy@0.790:',              type=('test', 'run'))
+    depends_on('py-typing-extensions@3.7.4:', type=('test', 'run'))
+    depends_on('py-setuptools',               type=('build'))
