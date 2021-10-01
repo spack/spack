5403faae59fc32d9829f0077178c238fc8d4ac3a
hategan <mhategan@gmail.com>
2021-10-01 10:27:31 -0700

    style fixes

--- var/spack/repos/builtin/packages/py-pytest-random-order/package.py
+++ var/spack/repos/builtin/packages/py-pytest-random-order/package.py
@@ -12,7 +12,6 @@ class PyPytestRandomOrder(PythonPackage):
     homepage = "https://github.com/jbasko/pytest-random-order"
     pypi     = "pytest-random-order/pytest-random-order-1.0.4.tar.gz"
 
-
     version('1.0.4', sha256='6b2159342a4c8c10855bc4fc6d65ee890fc614cb2b4ff688979b008a82a0ff52')
 
     depends_on('python@3.4:', type=('build', 'run'))
