547a2a8e5dddca399f4c83b8091bca9e5f4451b1
Bernhard Kaindl <bernhardkaindl7@gmail.com>
2021-10-02 19:28:13 +0000

    gmp@0.94h only, rm libtool dep, not all vers needed
    I've ran into cddlib myself and had PR #26088 for it, closing mine.
    
    Merging from my PR, with one further fix/improvement:
    
    libgmp is only needed for 0.94
    libtool not at all (also removed from my host, otherwise it uses that)
    
    I also removed the span of versions between the original version
    and the latest. Usually only the latest version is added as checksum,
    but thanks to your url_from_version, all versions can be built even
    automatically without question when using spack install -n to suppress
    it asking if one really wants to install a version without a checksum.
    
    In case you'd need a version in that span, you can submit a pull
    request but as you wrote that 0.94h was enough for you, I'm assuming
    that you it's ok for you to just have the latest version an 0.94h.

--- var/spack/repos/builtin/packages/cddlib/package.py
+++ var/spack/repos/builtin/packages/cddlib/package.py
@@ -14,19 +14,12 @@ class Cddlib(AutotoolsPackage):
     of linear inequalities"""
 
     homepage = "https://people.inf.ethz.ch/fukudak/cdd_home"
-    url      = "https://github.com/cddlib/cddlib/archive/refs/tags/0.94h.tar.gz"
+    url      = "https://github.com/cddlib/cddlib/archive/refs/tags/0.94m.tar.gz"
     maintainers = ['NessieCanCode']
     version('0.94m', sha256='70dffdb3369b8704dc75428a1b3c42ab9047b81ce039f12f427e2eb2b1b0dee2')
-    version('0.94l', sha256='1ef6b1ee44509a26d480cda699ca1a9a38ecc9a2aba5092dbd7390ca285769e0')
-    version('0.94k', sha256='de7397d7fe32758a6b53453a889ec7619b6c68a15d84eb132421f3d7d457be44')
-    version('0.94j', sha256='27d7fcac2710755a01ef5381010140fc57c95f959c3c5705c58539d8c4d17bfb')
-    version('0.94i', sha256='c60dac8697357740c593f8f255d49ac8a5069623561d68720fd9089367c90f4a')
     version('0.94h', sha256='7382782c3834214b022c8b2898ed775a7bf915f2cb2acb73fa045d6fd9a3de33')
 
-    # Note: It should be possible to build cddlib also without gmp
-
-    depends_on("gmp")
-    depends_on("libtool", type="build")
+    depends_on("gmp", when='@0.94h')
 
     def url_for_version(self, version):
         if self.spec.satisfies('@:0.94i'):
