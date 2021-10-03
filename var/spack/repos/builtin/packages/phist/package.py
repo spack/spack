8b05131cc36ae5f962f90211fb9081251d7f0197
Jonas Thies <16190001+jthies@users.noreply.github.com>
2021-10-03 10:12:46 +0200

    packages/phist source code formatting

--- var/spack/repos/builtin/packages/phist/package.py
+++ var/spack/repos/builtin/packages/phist/package.py
@@ -178,15 +178,15 @@ class Phist(CMakePackage):
                 % ('64' if '+int64' in spec else '32'),
                 self.define_from_variant('PHIST_HOST_OPTIMIZE', 'host'),
                 ]
+        # Force phist to use the MPI wrappers instead of raw compilers
+        # (see issue #26002 and the comment in the trilinos package.py)
         if '+mpi' in spec:
-                # Force phist to use the MPI wrappers instead of raw compilers
-                # (see issue #26002 and the comment in the trilinos package.py)
-                args.extend([
-                        define('CMAKE_C_COMPILER', spec['mpi'].mpicc),
-                        define('CMAKE_CXX_COMPILER', spec['mpi'].mpicxx),
-                        define('CMAKE_Fortran_COMPILER', spec['mpi'].mpifc),
-                        define('MPI_BASE_DIR', spec['mpi'].prefix),
-                ])
+            args.extend(
+                [define('CMAKE_C_COMPILER', spec['mpi'].mpicc),
+                 define('CMAKE_CXX_COMPILER', spec['mpi'].mpicxx),
+                 define('CMAKE_Fortran_COMPILER', spec['mpi'].mpifc),
+                 define('MPI_BASE_DIR', spec['mpi'].prefix),
+                 ])
 
         return args
 
