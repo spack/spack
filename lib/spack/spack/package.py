bd4f4b623e6ccf31e28ef6707d57c6f24c845b61
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2021-10-04 10:01:01 -0700

    Update lib/spack/spack/package.py
    Co-authored-by: Seth R. Johnson <johnsonsr@ornl.gov>

--- lib/spack/spack/package.py
+++ lib/spack/spack/package.py
@@ -2642,7 +2642,7 @@ def test_process(pkg, kwargs):
 
                     # Skip any test methods consisting solely of 'pass'
                     # since they do not contribute to package testing.
-                    source = (inspect.getsource(test_fn)).split('\n')[1:]
+                    source = (inspect.getsource(test_fn)).splitlines()[1:]
                     lines = (ln.strip() for ln in source)
                     statements = [ln for ln in lines if not ln.startswith('#')]
                     if len(statements) > 0 and statements[0] == 'pass':
