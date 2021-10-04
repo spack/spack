34e4291df04ac4a5df886a0e54e2066001427998
haralmha <haralmha@stud.ntnu.no>
2021-10-04 20:34:50 +0200

    Add 4.2.0

--- var/spack/repos/builtin/packages/libtiff/package.py
+++ var/spack/repos/builtin/packages/libtiff/package.py
@@ -12,6 +12,7 @@ class Libtiff(AutotoolsPackage):
     homepage = "http://www.simplesystems.org/libtiff/"
     url      = "https://download.osgeo.org/libtiff/tiff-4.1.0.tar.gz"
 
+    version('4.2.0',  sha256='eb0484e568ead8fa23b513e9b0041df7e327f4ee2d22db5a533929dfc19633cb')
     version('4.1.0',  sha256='5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634')
     version('4.0.10', sha256='2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4')
     version('4.0.9',  sha256='6e7bdeec2c310734e734d19aae3a71ebe37a4d842e0e23dbb1b8921c0026cfcd')
