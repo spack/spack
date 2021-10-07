35c882ec049cf2b67ceac594f1832a44119d3df7
Satish Balay <balay@mcs.anl.gov>
2021-10-07 13:42:05 -0500

    superlu-dist add version 7.1.0

--- var/spack/repos/builtin/packages/superlu-dist/package.py
+++ var/spack/repos/builtin/packages/superlu-dist/package.py
@@ -19,6 +19,7 @@ class SuperluDist(CMakePackage, CudaPackage):
     maintainers = ['xiaoye', 'gchavez2', 'balay', 'pghysels']
 
     version('develop', branch='master')
+    version('7.1.0', sha256='edbea877562be95fb22c7de1ff484f18685bec4baa8e4f703c414d3c035d4a66')
     version('6.4.0', sha256='cb9c0b2ba4c28e5ed5817718ba19ae1dd63ccd30bc44c8b8252b54f5f04a44cc')
     version('6.3.1', sha256='3787c2755acd6aadbb4d9029138c293a7570a2ed228806676edcc7e1d3f5a1d3')
     version('6.3.0', sha256='daf3264706caccae2b8fd5a572e40275f1e128fa235cb7c21ee2f8051c11af95')
