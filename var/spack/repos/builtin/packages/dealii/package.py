488a18dd04e2435239eb92aa4cc3d6d09c036956
Satish Balay <balay@mcs.anl.gov>
2021-10-11 17:28:08 -0500

    dealii: use branch dealii-9.3

--- var/spack/repos/builtin/packages/dealii/package.py
+++ var/spack/repos/builtin/packages/dealii/package.py
@@ -25,6 +25,7 @@ class Dealii(CMakePackage, CudaPackage):
     generator = 'Ninja'
 
     version('master', branch='master')
+    version('9.3.2', branch='dealii-9.3')
     version('9.3.1', sha256='a62f4676ab2dc029892251d141427fb75cbb83cddd606019f615d0dde9c61ab8')
     version('9.3.0', sha256='aef8c7a87510ce827dfae3bdd4ed7bff82004dc09f96fa7a65b2554f2839b931')
     version('9.2.0', sha256='d05a82fb40f1f1e24407451814b5a6004e39366a44c81208b1ae9d65f3efa43a')
