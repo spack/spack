b6bf945a7811c80e93737e352db4a61cd851e5b7
Joseph C Wang <joequant@gmail.com>
2021-10-11 17:48:30 +0800

    fix thepeg depends

--- var/spack/repos/builtin/packages/herwig3/package.py
+++ var/spack/repos/builtin/packages/herwig3/package.py
@@ -24,7 +24,7 @@ class Herwig3(AutotoolsPackage):
     depends_on('libtool',  type='build')
     depends_on('m4',       type='build')
     depends_on('lhapdf')
-    depends_on('thepeg@2.2.1', when='@7.2.1', type='link')
+    depends_on('thepeg@2.2.1', when='@7.2.1:', type='link')
     depends_on('boost', type='link')
     depends_on('python', type=('build', 'run'))
     depends_on('gsl', type='link')
