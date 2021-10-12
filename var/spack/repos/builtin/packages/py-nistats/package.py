890047ecb7feff3a483dc56f81b5b1fdcc2acd26
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2021-10-12 16:37:33 +0200

    Update var/spack/repos/builtin/packages/py-nistats/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-nistats/package.py
+++ var/spack/repos/builtin/packages/py-nistats/package.py
@@ -23,7 +23,7 @@ class PyNistats(PythonPackage):
     depends_on('py-nibabel@2.0.2:', type=('build', 'run'))
     # needs +plotting to avoid ModuleNotFoundError:
     # 'nilearn.plotting.js_plotting_utils' when importing nistats.reporting
-    depends_on('py-nilearn+plotting@0.4:', type=('build', 'run'))
+    depends_on('py-nilearn+plotting@0.4:0.6', type=('build', 'run'))
     depends_on('py-pandas@0.18:', type=('build', 'run'))
 
     conflicts('py-nilearn@0.7:',
