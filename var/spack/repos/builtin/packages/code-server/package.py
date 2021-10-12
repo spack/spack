a5612be4dc6d247bbf6c36232f179228b1544460
Nick Booher <njbooher@iastate.edu>
2021-10-12 12:03:51 -0500

    code-server: add more versions

--- var/spack/repos/builtin/packages/code-server/package.py
+++ var/spack/repos/builtin/packages/code-server/package.py
@@ -11,11 +11,13 @@ class CodeServer(Package):
     accessible through the browser."""
 
     homepage = "https://github.com/cdr/code-server"
-    url      = "https://github.com/cdr/code-server/releases/download/3.1.0/code-server-3.1.0-linux-x86_64.tar.gz"
-
-    version('3.4.1', sha256='957cc7bafb399b4d8c82fcfc9dc0b4e6ce50d095f2636a31320a628ada75d6a8')
-    version('3.1.1', sha256='5dd922d28b2e351c146081849d987fb1e439ee7d53b941434b2eecb2a194da71')
-    version('3.1.0', sha256='5ef85c8f280ce781a176a8b77386b333efe892755a5c325a1782e4eac6016e59')
+    url      = "https://github.com/cdr/code-server/releases/download/v3.12.0/code-server-3.12.0-linux-amd64.tar.gz"
+    
+    version('3.12.0', sha256='d3ca41a55e36d73d80300702af2687e25d440cff6b613bb58a2c88d9b8a0a38f')
+    version('3.9.3', sha256='eba42eaf868c2144795b1ac54929e3b252ae35403bf8553b3412a5ac4f365a41')
+    version('3.4.1', sha256='957cc7bafb399b4d8c82fcfc9dc0b4e6ce50d095f2636a31320a628ada75d6a8', url='https://github.com/cdr/code-server/releases/download/3.4.1/code-server-3.4.1-linux-x86_64.tar.gz')
+    version('3.1.1', sha256='5dd922d28b2e351c146081849d987fb1e439ee7d53b941434b2eecb2a194da71', url='https://github.com/cdr/code-server/releases/download/3.1.1/code-server-3.1.1-linux-x86_64.tar.gz')
+    version('3.1.0', sha256='5ef85c8f280ce781a176a8b77386b333efe892755a5c325a1782e4eac6016e59', url='https://github.com/cdr/code-server/releases/download/3.1.0/code-server-3.1.0-linux-x86_64.tar.gz')
 
     depends_on('git@2:')
 
