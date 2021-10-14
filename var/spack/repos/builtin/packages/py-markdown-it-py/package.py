a1abe359660d030bbaa19baadeb2e985886f01dc
Valentin Volkl <valentin.volkl@cern.ch>
2021-10-14 08:46:54 +0000

    py-jupytext: add new package

---
+++ var/spack/repos/builtin/packages/py-markdown-it-py/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class PyMarkdownItPy(PythonPackage):
+    """Markdown parser, done right.
+       100% CommonMark support, extensions, syntax plugins & high speed """
+
+    homepage = "https://github.com/executablebooks/markdown-it-py"
+    git      = "https://github.com/executablebooks/markdown-it-py"
+    pypi     = "markdown-it-py/markdown-it-py-1.1.0.tar.gz"
+
+    version('1.1.0', sha256='36be6bb3ad987bfdb839f5ba78ddf094552ca38ccbd784ae4f74a4e1419fc6e3')
+
+    depends_on('py-setuptools', type='build')
