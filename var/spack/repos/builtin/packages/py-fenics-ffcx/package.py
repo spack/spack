38c9d1ba5c7e4ec3bbc8a808ec254c2e6c37b2ed
Garth N. Wells <gnw20@cam.ac.uk>
2021-10-16 17:25:45 +0100

    Remove stray colon

--- var/spack/repos/builtin/packages/py-fenics-ffcx/package.py
+++ var/spack/repos/builtin/packages/py-fenics-ffcx/package.py
@@ -22,7 +22,7 @@ class PyFenicsFfcx(PythonPackage):
     depends_on('python@3.7:', type=('build', 'run'))
     depends_on('py-setuptools@58:', type='build', when="@0.4:")
     depends_on('py-setuptools', type='build')
-    depends_on('py-wheel:', type='build', when="@0.4:")
+    depends_on('py-wheel', type='build', when="@0.4:")
 
     depends_on('py-cffi', type='run')
 
