0f4ee459686c3ebaecfc0ce52e1e3c9e1e4ca9cc
Daniele Cesarini <d.cesarini@cineca.it>
2021-10-18 18:39:57 +0200

    Fixed libpaw bug with patch file

--- var/spack/repos/builtin/packages/bigdft-libabinit/package.py
+++ var/spack/repos/builtin/packages/bigdft-libabinit/package.py
@@ -17,8 +17,8 @@ class BigdftLibabinit(AutotoolsPackage):
     version('1.9.1',   sha256='3c334da26d2a201b572579fc1a7f8caad1cbf971e848a3e10d83bc4dc8c82e41')
     version('1.9.0',   sha256='4500e505f5a29d213f678a91d00a10fef9dc00860ea4b3edf9280f33ed0d1ac8')
     version('1.8.3',   sha256='f112bb08833da4d11dd0f14f7ab10d740b62bc924806d77c985eb04ae0629909')
-    #version('1.8.2',   sha256='042e5a3b478b1a4c050c450a9b1be7bcf8e13eacbce4759b7f2d79268b298d61')
-    #version('1.8.1',   sha256='e09ff0ba381f6ffbe6a3c0cb71db5b73117874beb41f22a982a7e5ba32d018b3')
+    version('1.8.2',   sha256='042e5a3b478b1a4c050c450a9b1be7bcf8e13eacbce4759b7f2d79268b298d61')
+    version('1.8.1',   sha256='e09ff0ba381f6ffbe6a3c0cb71db5b73117874beb41f22a982a7e5ba32d018b3')
 
     depends_on('python@:2.8', type='build', when="@:1.9.0")
     depends_on('python@3.0:', type='build', when="@1.9.1:")
@@ -27,13 +27,18 @@ class BigdftLibabinit(AutotoolsPackage):
     depends_on('libxc@:2.2.2')
     depends_on('bigdft-futile')
 
+    patch('m_libpaw_mpi.F90.patch', when='@:1.8.2')
+
     phases = ['autoreconf', 'configure', 'build', 'install']
 
     def autoreconf(self, spec, prefix):
         autoreconf = which('autoreconf')
 
         with working_dir('libABINIT'):
-            autoreconf('-fi')
+            if spec.satisfies('@:1.8.2'):
+                autoreconf('-i')
+            else:
+                autoreconf('-fi')
 
     def configure(self, spec, prefix):
         fcflags = []
