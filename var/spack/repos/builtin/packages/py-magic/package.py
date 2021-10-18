45a8eaf1ad28bb369c9e15440424f7741f69ba51
Seth R Johnson <johnsonsr@ornl.gov>
2021-10-18 13:32:47 -0400

    Update python version requirements

--- var/spack/repos/builtin/packages/py-magic/package.py
+++ var/spack/repos/builtin/packages/py-magic/package.py
@@ -18,5 +18,6 @@ class PyMagic(PythonPackage):
 
     version('0.4.15', sha256='6d730389249ab1e34ffb0a3c5beaa44e116687ffa081e0176dab6c59ff271593', deprecated=True)
 
+    depends_on('python@2.7.0:2.7,3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('file', type='run')
