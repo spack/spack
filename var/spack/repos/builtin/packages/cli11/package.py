65d344b2f8d97bd581d8621627beab1cd6c59c6b
Ryan Mast <3969255+nightlark@users.noreply.github.com>
2021-10-19 07:53:45 -0700

    cli11: disable building the examples (#26781)

--- var/spack/repos/builtin/packages/cli11/package.py
+++ var/spack/repos/builtin/packages/cli11/package.py
@@ -20,6 +20,7 @@ class Cli11(CMakePackage):
 
     def cmake_args(self):
         args = [
+            '-DCLI11_BUILD_EXAMPLES=OFF',
             '-DCLI11_BUILD_DOCS=OFF',
             '-DCLI11_BUILD_TESTS=OFF',
         ]
