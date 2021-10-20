a1a3e952eb25402ac7a209421d037b5048c51ceb
James Prewett <download@carc.unm.edu>
2021-10-20 14:43:20 -0600

    oops blank line is in the right place now

--- var/spack/repos/builtin/packages/tramonto/package.py
+++ var/spack/repos/builtin/packages/tramonto/package.py
@@ -3,8 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
 from spack import *
+
 
 class Tramonto(CMakePackage):
     """Tramonto: Software for Nanostructured Fluids in Materials and Biology"""
