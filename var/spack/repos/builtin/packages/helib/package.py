e6f694d787ec7cc3b8ec9f784087c33e66f9fae3
John Wohlbier <wohlbier@users.noreply.github.com>
2021-10-21 11:05:47 -0400

    Update var/spack/repos/builtin/packages/helib/package.py
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/helib/package.py
+++ var/spack/repos/builtin/packages/helib/package.py
@@ -35,8 +35,7 @@ class Helib(CMakePackage):
     version('1.1.0',        sha256='77a912ed3c86f8bde31b7d476321d0c2d810570c04a60fa95c4bd32a1955b5cf')
     version('1.0.2',        sha256='b907eaa8381af3d001d7fb8383273f4c652415b3320c11d5be2ad8f19757c998')
 
-    variant('shared', values=bool, default=False,
-            description='Build shared library.')
+    variant('shared', default=False, description='Build shared library.')
     depends_on('gmp@6.2.1:')
     depends_on('ntl@11.5.1:')
     depends_on('ntl+shared', when='+shared')
