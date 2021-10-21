626e723ef952adbd21615fcbe5173e4749710d38
John Wohlbier <wohlbier@users.noreply.github.com>
2021-10-21 11:09:16 -0400

    Update var/spack/repos/builtin/packages/ntl/package.py
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/ntl/package.py
+++ var/spack/repos/builtin/packages/ntl/package.py
@@ -17,12 +17,6 @@ class Ntl(Package):
     Documentation is available in the file doc/tour.html, which can be viewed
     with a web browser.
 
-    For a detailed guide to installation, please see the appropriate
-    documentation:
-    * doc/tour-unix.html for unix systems
-    * doc/tour-win.html for Windows systems
-
-    The latest version of NTL is available at http://www.shoup.net.
     """
 
     homepage = "https://libntl.org"
