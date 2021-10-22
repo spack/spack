63c4125c31f116b6ed3ac6a4553c803cca2909e9
Betsy McPhail <betsy.mcphail@kitware.com>
2021-10-22 16:24:48 -0400

    MSVC Compiler Find and vcvars env integration (#22856)
    Made the vcvars batch script location a member variable of the msvc compiler subclass, initialized from the compiler executable path.  Added a setup_custom_environment() method to the msvc subclass that sources the vcvars script, dumps the environment, and copies the relevant environment variables to the Spack environment.  Added class variables to the Windows OS and MSVC compiler subclasses to enable finding the compiler executables and determining their versions.

--- lib/spack/spack/operating_systems/windows_os.py
+++ lib/spack/spack/operating_systems/windows_os.py
@@ -3,6 +3,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import sys
+import os
+import subprocess
+import glob
 from spack.architecture import OperatingSystem
 from spack.version import Version
 
@@ -21,6 +25,35 @@ class WindowsOs(OperatingSystem):
     python setup that runs natively.  The Windows platform will be represented
     using the major version operating system number, e.g. 10.
     """
+
+    # Find MSVC directories using vswhere
+    compSearchPaths = []
+    root = os.environ.get('ProgramFiles(x86)') or os.environ.get('ProgramFiles')
+    if root:
+        try:
+            extra_args = {}
+            if sys.version_info[:3] >= (3, 6, 0):
+                extra_args = {'encoding': 'mbcs', 'errors': 'strict'}
+            paths = subprocess.check_output([
+                os.path.join(root, "Microsoft Visual Studio", "Installer",
+                             "vswhere.exe"),
+                "-prerelease",
+                "-requires", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
+                "-property", "installationPath",
+                "-products", "*",
+            ], **extra_args).strip()
+            if (3, 0) <= sys.version_info[:2] <= (3, 5):
+                paths = paths.decode()
+            msvcPaths = paths.split('\n')
+            msvcPaths = [os.path.join(path, "VC", "Tools", "MSVC")
+                         for path in msvcPaths]
+            for p in msvcPaths:
+                compSearchPaths.extend(
+                    glob.glob(os.path.join(p, '*', 'bin', 'Hostx64', 'x64')))
+        except (subprocess.CalledProcessError, OSError, UnicodeDecodeError):
+            pass
+    if compSearchPaths:
+        compiler_search_paths = compSearchPaths
 
     def __init__(self):
         super(WindowsOs, self).__init__('Windows10', '10')
