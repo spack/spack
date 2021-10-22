f8ec72236ddecc22e290d9c88669965222ebc0df
Michael Kuhn <michael.kuhn@ovgu.de>
2021-10-23 00:03:54 +0200

    glib: add 2.70.0

--- var/spack/repos/builtin/packages/glib/package.py
+++ var/spack/repos/builtin/packages/glib/package.py
@@ -23,6 +23,7 @@ class Glib(Package):
 
     maintainers = ['michaelkuhn']
 
+    version('2.70.0', sha256='200d7df811c5ba634afbf109f14bb40ba7fde670e89389885da14e27c0840742')
     version('2.68.4', sha256='62fd061d08a75492617e625a73e2c05e259f831acbb8e1f8b9c81f23f7993a3b')
     version('2.68.3', sha256='e7e1a3c20c026109c45c9ec4a31d8dcebc22e86c69486993e565817d64be3138')
     version('2.68.2', sha256='ecc7798a9cc034eabdfd7f246e6dd461cdbf1175fcc2e9867cc7da7b7309e0fb')
@@ -85,7 +86,8 @@ class Glib(Package):
     # glib prefers the libc version of gettext, which breaks the build if the
     # external version is also found.
     patch('meson-gettext.patch', when='@2.58:2.64')
-    patch('meson-gettext-2.66.patch', when='@2.66:')
+    patch('meson-gettext-2.66.patch', when='@2.66:2.68')
+    patch('meson-gettext-2.70.patch', when='@2.70:')
 
     def url_for_version(self, version):
         """Handle glib's version-based custom URLs."""
