27d19d5793c7469e9882ae9d6b7c2b19e73d92af
Betsy McPhail <betsy.mcphail@kitware.com>
2021-10-22 12:16:11 -0400

    Windows: Symlink support
    To provide Windows-compatible functionality, spack code should use
    llnl.util.symlink instead of os.symlink. On non-Windows platforms
    and on Windows where supported, os.symlink will still be used.
    
    Use junctions when symlinks aren't supported on Windows (#22583)
    
    Support islink for junctions (#24182)
    
    Windows: Update llnl/util/filesystem
    
    * Use '/' as path separator on Windows.
    * Recognizing that Windows paths start with '<Letter>:/' instead of '/'
    
    Co-authored-by: lou.lawrence@kitware.com <lou.lawrence@kitware.com>
    Co-authored-by: John Parent <john.parent@kitware.com>

--- lib/spack/spack/filesystem_view.py
+++ lib/spack/spack/filesystem_view.py
@@ -14,6 +14,7 @@ from llnl.util import tty
 from llnl.util.filesystem import mkdirp, remove_dead_links, remove_empty_directories
 from llnl.util.lang import index_by, match_predicate
 from llnl.util.link_tree import LinkTree, MergeConflictError
+from llnl.util.symlink import symlink
 from llnl.util.tty.color import colorize
 
 import spack.config
@@ -44,7 +45,7 @@ _projections_path = '.spack/projections.yaml'
 def view_symlink(src, dst, **kwargs):
     # keyword arguments are irrelevant
     # here to fit required call signature
-    os.symlink(src, dst)
+    symlink(src, dst)
 
 
 def view_hardlink(src, dst, **kwargs):
@@ -146,7 +147,7 @@ class FilesystemView(object):
             Initialize a filesystem view under the given `root` directory with
             corresponding directory `layout`.
 
-            Files are linked by method `link` (os.symlink by default).
+            Files are linked by method `link` (llnl.util.symlink by default).
         """
         self._root = root
         self.layout = layout
