af8670d1beb20b0ef7aa6cd5e5e57df72378eea8
Nathan Hanford <hanford1@llnl.gov>
2021-10-26 22:39:20 -0700

    cleanup and resolve issue

--- lib/spack/spack/test/rewiring.py
+++ lib/spack/spack/test/rewiring.py
@@ -44,8 +44,7 @@ def test_rewire(mock_fetch, install_mockery):
                                           spack.store.layout.metadata_dir,
                                           spack.store.layout.manifest_file_name)
         assert os.path.exists(manifest_file_path)
-        # monkeypatch the modulefile_generation hook to an accumulator and make
-        # sure that it’s called with all of the arguments you would expect
+        # monkeypatch the modulefile_generation hook to an accumulator
 
 
 def test_rewire_bin(mock_fetch, install_mockery):
@@ -68,18 +67,13 @@ def test_rewire_bin(mock_fetch, install_mockery):
     assert installed_in_db
 
     # check the file in the prefix has the correct paths
+    bin_names = {'garply': 'garplinator',
+                 'corge': 'corgegator',
+                 'quux': 'quuxifier'}
     for node in spliced_spec.traverse(root=True):
-        if 'garply' in node.name:
-            bin_name = 'garplinator'
-        elif 'corge' in node.name:
-            bin_name = 'corgegator'
-        elif 'quux' in node.name:
-            bin_name = 'quuxifier'
-        bin_file_path = os.path.join(node.prefix.bin, bin_name)
-        for fname in glob(bin_file_path):
-            with open(fname, 'rb') as f:
-                f.seek(0)
-            assert text_in_bin(node.prefix, fname)
+        for dep in node.traverse(root=True):
+            bin_file_path = os.path.join(dep.prefix.bin, bin_names[dep.name])
+            assert text_in_bin(dep.prefix, bin_file_path)
         # test install manifest
         assert spack.store.layout.check_installed(node)
         manifest_file_path = os.path.join(node.prefix,
