336d1f40ddea40773c5ca2ddcfb33378cb389c8a
Manuela Kuhn <manuela.kuhn@ovgu.de>
2021-10-27 04:18:09 +0200

    r-vctrs: add 0.3.8

--- var/spack/repos/builtin/packages/r-vctrs/package.py
+++ var/spack/repos/builtin/packages/r-vctrs/package.py
@@ -15,9 +15,9 @@ class RVctrs(RPackage):
     function interfaces."""
 
     homepage = "https://github.com/r-lib/vctrs"
-    url      = "https://github.com/r-lib/vctrs/archive/v0.3.5.tar.gz"
-    list_url = "https://cloud.r-project.org/src/contrib/Archive/vctrs"
+    cran = "vctrs"
 
+    version('0.3.8', sha256='87d8e49edd56315f25b7165e9175255c8819fbd813e508ddbaff0f11adbec6df')
     version('0.3.6', sha256='5869a2cc2cb62ccb6f7ff7e8f88c02790d5ea256cccaf51aee56fb28e7ee48ce')
     version('0.3.5', sha256='798dd19809ab99267456ebf488e7aa4e3c03f7f307f5e0abde01dc7ba1cf53ce')
     version('0.2.0', sha256='5bce8f228182ecaa51230d00ad8a018de9cf2579703e82244e0931fe31f20016')
@@ -25,8 +25,9 @@ class RVctrs(RPackage):
     depends_on('r@3.2:', type=('build', 'run'))
     depends_on('r@3.3:', when='@0.3.5:', type=('build', 'run'))
     depends_on('r-ellipsis@0.2.0:', type=('build', 'run'))
-    depends_on('r-digest', type=('build', 'run'))
     depends_on('r-glue', type=('build', 'run'))
     depends_on('r-rlang@0.4.0:', type=('build', 'run'))
     depends_on('r-rlang@0.4.7:', when='@0.3.5:', type=('build', 'run'))
+    depends_on('r-rlang@0.4.10:', when='@0.3.7:', type=('build', 'run'))
+    depends_on('r-digest', when='@:0.3.6', type=('build', 'run'))
     depends_on('r-zeallot', when='@:0.2.0', type=('build', 'run'))
