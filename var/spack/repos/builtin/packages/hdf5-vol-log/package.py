c991330434f1fd1cef0a205e0b83bd2d92336db5
H. Joe Lee <hyoklee@hdfgroup.org>
2021-10-27 10:28:05 -0500

    OESS-205: Apply @alalazo reviews.

--- var/spack/repos/builtin/packages/hdf5-vol-log/package.py
+++ var/spack/repos/builtin/packages/hdf5-vol-log/package.py
@@ -15,10 +15,9 @@ class Hdf5VolLog(AutotoolsPackage):
     git = 'https://github.com/DataLib-ECP/vol-log-based.git'
     maintainers = ['hyoklee']
 
-    version('master', branch='master', git=git,
-            commit='b13778efd9e0c79135a9d7352104985408078d45')
+    version('master', commit='b13778efd9e0c79135a9d7352104985408078d45')
 
-    depends_on('hdf5@1.12.1')
+    depends_on('hdf5@1.12.1:')
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
     depends_on('libtool',  type='build')
