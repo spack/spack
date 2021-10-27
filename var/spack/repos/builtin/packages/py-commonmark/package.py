fadfc30a3e4eaee2a04f73b9705ec46aadb79278
Ivan Razumov <iarspider@gmail.com>
2021-10-27 17:03:52 +0200

    New version: py-commonmark 0.9.1

--- var/spack/repos/builtin/packages/py-commonmark/package.py
+++ var/spack/repos/builtin/packages/py-commonmark/package.py
@@ -14,7 +14,9 @@ class PyCommonmark(PythonPackage):
     homepage = "https://github.com/readthedocs/commonmark.py"
     pypi = "commonmark/commonmark-0.9.0.tar.gz"
 
+    version('0.9.1', sha256='452f9dc859be7f06631ddcb328b6919c67984aca654e5fefb3914d54691aed60')
     version('0.9.0', sha256='867fc5db078ede373ab811e16b6789e9d033b15ccd7296f370ca52d1ee792ce0')
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-future', type=('build', 'run'))
+    depends_on('py-future', type=('build', 'run'), when='@0.9.0')
+    depends_on('py-future@0.14.0:', type=('build', 'run'), when='@0.9.1: ^python@:2.8')
