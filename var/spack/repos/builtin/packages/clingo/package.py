983b3744244bf4d30afc932831c3a8a4a2eacf28
John Parent <john.parent@kitware.com>
2021-10-28 12:28:51 -0400

    First pass clingo windows support

--- var/spack/repos/builtin/packages/clingo/package.py
+++ var/spack/repos/builtin/packages/clingo/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
+
 from spack import *
 from spack.compiler import UnsupportedCompilerFlag
 
@@ -22,9 +24,9 @@ class Clingo(CMakePackage):
     git      = 'https://github.com/potassco/clingo.git'
 
     maintainers = ["tgamblin", "alalazo"]
-
-    version('master', branch='master', submodules=True)
-    version('spack', commit='2a025667090d71b2c9dce60fe924feb6bde8f667', submodules=True)
+    if os.name != 'nt':
+        version('master', branch='master', submodules=True)
+        version('spack', commit='2a025667090d71b2c9dce60fe924feb6bde8f667', submodules=True)
 
     version('5.5.1', sha256='b9cf2ba2001f8241b8b1d369b6f353e628582e2a00f13566e51c03c4dd61f67e')
     version('5.5.0', sha256='c9d7004a0caec61b636ad1c1960fbf339ef8fdee9719321fc1b6b210613a8499')
@@ -39,8 +41,7 @@ class Clingo(CMakePackage):
     depends_on('cmake@3.1:', type='build')
 
     depends_on('doxygen', type="build", when="+docs")
-    depends_on('re2c@0.13:', type="build")
-    depends_on('bison@2.5:', type="build")
+
 
     depends_on('python', type=("build", "link", "run"), when="+python")
     extends('python', when='+python')
@@ -50,7 +51,11 @@ class Clingo(CMakePackage):
         'python@3.6.0:',
         type=('build', 'link', 'run'), when='@5.5.0: +python'
     )
-    depends_on('py-cffi', type=('build', 'run'), when='@5.5.0: +python')
+
+    if os.name != 'nt':
+        depends_on('re2c@0.13:', type="build")
+        depends_on('bison@2.5:', type="build")
+        depends_on('py-cffi', type=('build', 'run'), when='@5.5.0: +python')
 
     patch('python38.patch', when="@5.3:5.4")
 
