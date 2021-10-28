9cd3b5de6c552cdb5ca941c84761ba9eb1c0873d
Eric Brugger <brugger1@llnl.gov>
2021-10-28 14:23:43 -0700

    Update for building with 3.2.1.

--- var/spack/repos/builtin/packages/visit/package.py
+++ var/spack/repos/builtin/packages/visit/package.py
@@ -54,9 +54,9 @@ class Visit(CMakePackage):
     # (dyld: Symbol not found: _GENERAL_NAME_free - which comes from OpenSSL)
     #
     ############################
-    homepage = "https://wci.llnl.gov/simulation/computer-codes/visit/"
+    homepage = "https://visit-dav.github.io/visit-website/"
     git      = "https://github.com/visit-dav/visit.git"
-    url = "https://github.com/visit-dav/visit/releases/download/v3.1.1/visit3.1.1.tar.gz"
+    url = "https://github.com/visit-dav/visit/releases/download/v3.2.1/visit3.2.1.tar.gz"
 
     tags = ['radiuss']
 
@@ -67,22 +67,14 @@ class Visit(CMakePackage):
     executables = ['^visit$']
 
     version('develop', branch='develop')
-    version('3.1.1', sha256='0b60ac52fd00aff3cf212a310e36e32e13ae3ca0ddd1ea3f54f75e4d9b6c6cf0')
-    version('3.0.1', sha256='a506d4d83b8973829e68787d8d721199523ce7ec73e7594e93333c214c2c12bd')
-    version('2.13.3', sha256='cf0b3d2e39e1cd102dd886d3ef6da892733445e362fc28f24d9682012cccf2e5')
-    version('2.13.0', sha256='716644b8e78a00ff82691619d4d1e7a914965b6535884890b667b97ba08d6a0f')
-    version('2.12.3', sha256='2dd351a291ee3e79926bc00391ca89b202cfa4751331b0fdee1b960c7922161f')
-    version('2.12.2', sha256='55897d656ac2ea4eb87a30118b2e3963d6c8a391dda0790268426a73e4b06943')
-    version('2.10.3', sha256='05018215c4727eb42d47bb5cc4ff937b2a2ccaca90d141bc7fa426a0843a5dbc')
-    version('2.10.2', sha256='89ecdfaf197ef431685e31b75628774deb6cd75d3e332ef26505774403e8beff')
-    version('2.10.1', sha256='6b53dea89a241fd03300a7a3a50c0f773e2fb8458cd3ad06816e9bd2f0337cd8')
+    version('3.2.1', sha256='779d59564c63f31fcbfeff24b14ddd6ac941b3bb7d671d31765a770d193f02e8')
 
     variant('gui',    default=True, description='Enable VisIt\'s GUI')
     variant('adios2', default=False, description='Enable ADIOS2 file format')
     variant('hdf5',   default=True, description='Enable HDF5 file format')
     variant('silo',   default=True, description='Enable Silo file format')
     variant('python', default=True, description='Enable Python support')
-    variant('mpi',    default=True, description='Enable parallel engine')
+    variant('mpi',    default=False, description='Enable parallel engine')
 
     patch('spack-changes-3.1.patch', when="@3.1.0:,develop")
     patch('spack-changes-3.0.1.patch', when="@3.0.1")
@@ -177,19 +169,31 @@ class Visit(CMakePackage):
     #
     # =====================================
 
-    depends_on('cmake@3.0:', type='build')
+    depends_on('cmake@3.14.7', type='build')
     # https://github.com/visit-dav/visit/issues/3498
-    depends_on('vtk@8.1.0:8.1+opengl2~python', when='~python @3.0:3,develop')
-    depends_on('vtk@8.1.0:8.1+opengl2+python', when='+python @3.0:3,develop')
+    depends_on('vtk@8.1.0+opengl2+osmesa~python',
+               patches=[patch('vtk_compiler_visibility.patch'),
+                        patch('vtk_rendering_opengl2_x11.patch'),
+                        patch('vtk_wrapping_python_x11.patch'),
+                       ],
+               when='~python @3.2:,develop')
+    depends_on('vtk@8.1.0+opengl2+osmesa+python',
+               patches=[patch('vtk_compiler_visibility.patch'),
+                        patch('vtk_rendering_opengl2_x11.patch'),
+                        patch('vtk_wrapping_python_x11.patch'),
+                       ],
+               when='+python @3.2:,develop')
     depends_on('glu', when='platform=linux')
-    depends_on('vtk@6.1.0~opengl2', when='@:2')
-    depends_on('vtk+python', when='+python @3.0:,develop')
+    depends_on('vtk+python', when='+python @3.2:,develop')
     depends_on('vtk~mpi', when='~mpi')
     depends_on('vtk+qt', when='+gui')
-    depends_on('qt+gui@4.8.6:4', when='+gui @:2')
-    depends_on('qt+gui@5.10:', when='+gui @3.0:,develop')
-    depends_on('qwt', when='+gui')
-    depends_on('python@2.6:2.8', when='+python')
+    depends_on('qt+gui@5.14.2:', when='+gui @3.2:,develop')
+    depends_on('qwt@6.1.6', when='+gui')
+    depends_on('python@3.7.7', when='+python')
+    depends_on('llvm@6.0.1', when='^mesa')
+    depends_on('mesa+glx@20.2.1', when='^mesa')
+    depends_on('mesa-glu@9.0.1', when='^mesa')
+    depends_on('hdf5@1.8.14', when='+hdf5')
     # VisIt uses Silo's 'ghost zone' data structures, which are only available
     # in v4.10+ releases: https://wci.llnl.gov/simulation/computer-codes/silo/releases/release-notes-4.10
     depends_on('silo@4.10:+shared', when='+silo')
@@ -200,13 +204,9 @@ class Visit(CMakePackage):
     depends_on('mpi', when='+mpi')
     depends_on('adios2', when='+adios2')
 
-    conflicts('+adios2', when='@:2')
-    conflicts('+hdf5', when='~gui @:2')
-    conflicts('+silo', when='~gui @:2')
-
     root_cmakelists_dir = 'src'
 
-    @when('@3.0.0:3,develop')
+    @when('@3.0.0:,develop')
     def patch(self):
         # Some of VTK's targets don't create explicit libraries, so there is no
         # 'vtktiff'. Instead, replace with the library variable defined from
@@ -235,6 +235,7 @@ class Visit(CMakePackage):
             '-DVISIT_USE_GLEW=OFF',
             '-DCMAKE_CXX_FLAGS=' + ' '.join(cxx_flags),
             '-DCMAKE_C_FLAGS=' + ' '.join(cc_flags),
+            '-DVISIT_CONFIG_SITE=NONE',
         ]
 
         # Provide the plugin compilation environment so as to extend VisIt
@@ -265,6 +266,12 @@ class Visit(CMakePackage):
         else:
             args.append('-DVISIT_SERVER_COMPONENTS_ONLY=ON')
             args.append('-DVISIT_ENGINE_ONLY=ON')
+
+        if '^mesa' in spec:
+            args.append(
+                '-DVISIT_LLVM_DIR:PATH={0}'.format(spec['llvm'].prefix))
+            args.append(
+                '-DVISIT_MESAGL_DIR:PATH={0}'.format(spec['mesa'].prefix))
 
         if '+hdf5' in spec:
             args.append(
