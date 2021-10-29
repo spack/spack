3fdeb6091c6b414c3a403d032dc7e9fe5d089b1b
iarspider <iarspider@gmail.com>
2021-10-29 20:49:48 +0200

    Update var/spack/repos/builtin/packages/py-html5lib/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-html5lib/package.py
+++ var/spack/repos/builtin/packages/py-html5lib/package.py
@@ -13,7 +13,7 @@ class PyHtml5lib(PythonPackage):
     pypi = "html5lib/html5lib-1.1.tar.gz"
 
     version('1.1', sha256='b2e5b40261e20f354d198eae92afc10d750afb487ed5e50f9c4eaf07c184146f',
-           preferred=True)
+            preferred=True)
     version('1.0.1', sha256='66cb0dcfdbbc4f9c3ba1a63fdb511ffdbd4f513b2b6d81b80cd26ce6b3fb3736')
     version('0.99', sha256='e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
     version('099', sha256='2612a191a8d5842bfa057e41ba50bbb9dcb722419d2408c78cff4758d0754868',
