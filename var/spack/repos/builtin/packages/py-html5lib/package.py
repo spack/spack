aa68cd7c68f1d2d81af2bd277005bc6d3093ef80
Ivan Razumov <iarspider@gmail.com>
2021-10-29 15:45:02 +0200

    New verion: py-html5lib 1.1

--- var/spack/repos/builtin/packages/py-html5lib/package.py
+++ var/spack/repos/builtin/packages/py-html5lib/package.py
@@ -12,12 +12,19 @@ class PyHtml5lib(PythonPackage):
     homepage = "https://github.com/html5lib/html5lib-python"
     pypi = "html5lib/html5lib-099.tar.gz"
 
+    version('1.1', sha256='b2e5b40261e20f354d198eae92afc10d750afb487ed5e50f9c4eaf07c184146f')
     version('1.0.1', sha256='66cb0dcfdbbc4f9c3ba1a63fdb511ffdbd4f513b2b6d81b80cd26ce6b3fb3736')
-    version('099', sha256='2612a191a8d5842bfa057e41ba50bbb9dcb722419d2408c78cff4758d0754868')
+    version('0.99', sha256='2612a191a8d5842bfa057e41ba50bbb9dcb722419d2408c78cff4758d0754868',
+            url='https://files.pythonhosted.org/packages/source/h/html5lib/html5lib-099.tar.gz')
 
     depends_on('python@2.6:2.8,3.2:', when='@099', type=('build', 'run'))
     depends_on('python@2.6:2.8,3.3:', when='@1.0.1:', type=('build', 'run'))
+    depends_on('python@2.6:2.8,3.5:', when='@1.1:', type=('build', 'run'))
     depends_on('py-six', type=('build', 'run'))
     depends_on('py-six@1.9:', type=('build', 'run'), when='@1.0.1:')
     depends_on('py-setuptools', type='build', when='@1.0.1:')
     depends_on('py-webencodings', type=('build', 'run'), when='@1.0.1:')
+
+    def url_for_version(self, version):
+        root = 'https://files.pythonhosted.org/packages/source/h/html5lib/html5lib-'
+        return root + str(version) + '.tar.gz'
