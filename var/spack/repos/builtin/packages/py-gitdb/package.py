653fe2560b356b15eb993807785e87cab52f223e
iarspider <iarspider@gmail.com>
2021-10-29 21:30:26 +0200

    Update var/spack/repos/builtin/packages/py-gitdb/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-gitdb/package.py
+++ var/spack/repos/builtin/packages/py-gitdb/package.py
@@ -22,6 +22,6 @@ class PyGitdb(PythonPackage):
     depends_on('python@3.4:', type=('build', 'run'))
     depends_on('python@3.6:', type=('build', 'run'), when='@4.0.8:')
     depends_on('py-setuptools', type='build')
-    depends_on('py-smmap@3.0.1:3', type=('build', 'run'))
-    depends_on('py-smmap@3.0.1:4', type=('build', 'run'), when='@4.0.6:')
+    depends_on('py-smmap@3.0.1:3', type=('build', 'run'), when='@:4.0.5)
+    depends_on('py-smmap@3.0.1:4', type=('build', 'run'), when='@4.0.6:4.0.7')
     depends_on('py-smmap@3.0.1:5', type=('build', 'run'), when='@4.0.8:')
