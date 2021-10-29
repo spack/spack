2f1129f7f16000e0de29c7a3568bff68e622d2e6
iarspider <iarspider@gmail.com>
2021-10-29 18:58:35 +0200

    Update package.py

--- var/spack/repos/builtin/packages/py-html5lib/package.py
+++ var/spack/repos/builtin/packages/py-html5lib/package.py
@@ -15,8 +15,7 @@ class PyHtml5lib(PythonPackage):
     version('1.1', sha256='b2e5b40261e20f354d198eae92afc10d750afb487ed5e50f9c4eaf07c184146f',
            preferred=True)
     version('1.0.1', sha256='66cb0dcfdbbc4f9c3ba1a63fdb511ffdbd4f513b2b6d81b80cd26ce6b3fb3736')
-    version('0.99', sha256='e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
-            url='https://files.pythonhosted.org/packages/source/h/html5lib/html5lib-0.99.tar.gz')
+    version('0.99', sha256='e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
     version('099', sha256='2612a191a8d5842bfa057e41ba50bbb9dcb722419d2408c78cff4758d0754868',
            deprecated=True)
 
