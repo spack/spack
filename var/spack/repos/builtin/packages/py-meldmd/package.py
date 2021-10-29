cc615863d87d6e650cdd998c07affbbd3a1002ca
Jen Herting <jen@herting.cc>
2021-10-29 15:49:09 -0400

    [py-meldmd]
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-meldmd/package.py
+++ var/spack/repos/builtin/packages/py-meldmd/package.py
@@ -3,36 +3,15 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-meldmd
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-meldmd
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyMeldmd(PythonPackage, CudaPackage):
-    """FIXME: Put a proper description of your package here."""
+    """MELD is a tool for inferring the structure of
+    biomolecules from sparse, ambiguous, or noisy data."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "http://meldmd.org/"
     url      = "https://github.com/maccallumlab/meld/archive/refs/tags/0.4.20.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.4.20', sha256='8c8d2b713f8dc0ecc137d19945b3957e12063c8dda569696e47c8820eeac6c92')
 
@@ -43,16 +22,7 @@ class PyMeldmd(PythonPackage, CudaPackage):
     depends_on('py-netcdf4', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-scipy', type=('build', 'run'))
-    depends_on('py-scikit-learn' , type=('build', 'run'))
+    depends_on('py-scikit-learn', type=('build', 'run'))
     depends_on('py-parmed', type=('build', 'run'))
     depends_on('py-tqdm', type=('build', 'run'))
     depends_on('py-mpi4py', type=('build', 'run'))
-
-
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
