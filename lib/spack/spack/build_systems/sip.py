5cfc0abb3fcc8896245743a9a51f8fd649572b36
iarspider <iarspider@gmail.com>
2021-11-02 15:53:03 +0100

    SIP tests: allow importing weirdly-named modules

--- lib/spack/spack/build_systems/sip.py
+++ lib/spack/spack/build_systems/sip.py
@@ -147,7 +147,7 @@ class SIPPackage(PackageBase):
         # not the ones in the source directory
         for module in self.import_modules:
             self.run_test(inspect.getmodule(self).python.path,
-                          ['-c', 'import {0}'.format(module)],
+                          ['-c', '__import__("{0}")'.format(module)],
                           purpose='checking import of {0}'.format(module),
                           work_dir='spack-test')
 
