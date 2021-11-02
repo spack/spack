c9f8dd93f33b41ce9249c9f6b6a72a49ea1bb4ed
Satish Balay <balay@mcs.anl.gov>
2021-11-02 14:07:42 -0500

    trilinos: new version 13.2.0 (#27106)
    * trilinos: add @13.2.0, and switch default to cxxstd=14
    
    * trilinos: fix python dependency when using +ifpack or +ifpack2
    
    * trilinos: add conflict for ~epetra +ml when @13.2.0:
    
    * trilinos: keep 13.0.1 as the preferred version
    
    * Update var/spack/repos/builtin/packages/trilinos/package.py
    
    Co-authored-by: Seth R. Johnson <johnsonsr@ornl.gov>
    
    * update
    
    Co-authored-by: Seth R. Johnson <johnsonsr@ornl.gov>

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -39,7 +39,8 @@ class Trilinos(CMakePackage, CudaPackage):
 
     version('master', branch='master')
     version('develop', branch='develop')
-    version('13.0.1', commit='4796b92fb0644ba8c531dd9953e7a4878b05c62d')  # tag trilinos-release-13-0-1
+    version('13.2.0', commit='4a5f7906a6420ee2f9450367e9cc95b28c00d744')  # tag trilinos-release-13-2-0
+    version('13.0.1', commit='4796b92fb0644ba8c531dd9953e7a4878b05c62d', preferred=True)  # tag trilinos-release-13-0-1
     version('13.0.0', commit='9fec35276d846a667bc668ff4cbdfd8be0dfea08')  # tag trilinos-release-13-0-0
     version('12.18.1', commit='55a75997332636a28afc9db1aee4ae46fe8d93e7')  # tag trilinos-release-12-8-1
     version('12.14.1', sha256='52a4406cca2241f5eea8e166c2950471dd9478ad6741cbb2a7fc8225814616f0')
@@ -62,7 +63,7 @@ class Trilinos(CMakePackage, CudaPackage):
     # Build options
     variant('complex', default=False, description='Enable complex numbers in Trilinos')
     variant('cuda_rdc', default=False, description='turn on RDC for CUDA build')
-    variant('cxxstd', default='11', values=['11', '14', '17'], multi=False)
+    variant('cxxstd', default='14', values=['11', '14', '17'], multi=False)
     variant('debug', default=False, description='Enable runtime safety and debug checks')
     variant('explicit_template_instantiation', default=True, description='Enable explicit template instantiation (ETI)')
     variant('float', default=False, description='Enable single precision (float) numbers in Trilinos')
@@ -179,6 +180,7 @@ class Trilinos(CMakePackage, CudaPackage):
         conflicts('+epetraext')
         conflicts('+ifpack')
         conflicts('+isorropia')
+        conflicts('+ml', when='@13.2:')
     with when('~epetraext'):
         conflicts('+isorropia')
         conflicts('+teko')
@@ -301,6 +303,8 @@ class Trilinos(CMakePackage, CudaPackage):
     depends_on('py-mpi4py', when='+mpi+python', type=('build', 'run'))
     depends_on('py-numpy', when='+python', type=('build', 'run'))
     depends_on('python', when='+python')
+    depends_on('python', when='@13.2: +ifpack +hypre', type='build')
+    depends_on('python', when='@13.2: +ifpack2 +hypre', type='build')
     depends_on('scalapack', when='+mumps')
     depends_on('scalapack', when='+strumpack+mpi')
     depends_on('strumpack+shared', when='+strumpack')
