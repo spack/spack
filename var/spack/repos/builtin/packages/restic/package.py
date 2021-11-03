c2b415003bfdb955a6afe65d43f38f0829b66c1d
Alec Scott <hi@alecbcs.com>
2021-11-03 16:10:43 -0700

    Update homepage to proper location

--- var/spack/repos/builtin/packages/restic/package.py
+++ var/spack/repos/builtin/packages/restic/package.py
@@ -9,16 +9,16 @@ from spack import *
 class Restic(Package):
     """Fast, secure, efficient backup program."""
 
-    homepage = "https://rclone.org"                                   
+    homepage = "https://restic.net"
     url      = "https://github.com/restic/restic/releases/download/v0.12.1/restic-0.12.1.tar.gz"
 
     maintainers = ['alecbcs']
 
-    version('0.12.1', sha256='a9c88d5288ce04a6cc78afcda7590d3124966dab3daa9908de9b3e492e2925fb') 
-    
+    version('0.12.1', sha256='a9c88d5288ce04a6cc78afcda7590d3124966dab3daa9908de9b3e492e2925fb')
+
     depends_on("go", type='build')
 
-    def setup_build_environment(self, env):  
+    def setup_build_environment(self, env):
         # Point GOPATH at the top of the staging dir for the build step.
         env.prepend_path('GOPATH', self.stage.path)
 
