e03afdcb5c42b9b1f4c535b809d4cc83ac83bf5b
Joe Schoonover <joe@fluidnumerics.com>
2021-11-03 09:31:49 -0600

    Add new versions for HOHQMesh and switch to tar-ball releases

--- var/spack/repos/builtin/packages/hohqmesh/package.py
+++ var/spack/repos/builtin/packages/hohqmesh/package.py
@@ -10,13 +10,15 @@ class Hohqmesh(CMakePackage):
     """High Order mesh generator for Hexahedral and Quadrilateral meshes."""
 
     homepage = "https://github.com/trixi-framework/HOHQMesh"
-    url      = "https://github.com/trixi-framework/HOHQMesh"
     git      = "https://github.com/trixi-framework/HOHQMesh.git"
+    url      = "https://github.com/trixi-framework/HOHQMesh/archive/v1.0.0.tar.gz"
 
-    maintainers = ['schoonovernumerics']
+    maintainers = ['fluidnumerics-joe']
 
-    version('main', branch='main')
-    version('v1.0.1', tag='v1.0.1')
+    version('1.1.0', sha256='977109d57dbb6b74c2d2c52b9bc3ac5ad4ecbfaa0dbe3ef2291f68eea05f1b78')
+    version('1.0.1', sha256='83c2c48aec9cd6b1ade0d9b04a0d68782ea7e0a5d9fb28ac7510d4ad401e64ee')
+    version('1.0.0', sha256='b5e983fa4a34311042e29792bf0e233fafc63b8f98873c041b8f7e4f1e26a19f')
+
 
     depends_on('ftobjectlibrary')
 
