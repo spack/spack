83978b973571461817d87ada0d6f4c36b9f2b999
Sid Pendelberry <sid@rit.edu>
2021-11-04 13:59:03 -0400

    updated to 3.4.0

--- var/spack/repos/builtin/packages/py-pyscipopt/package.py
+++ var/spack/repos/builtin/packages/py-pyscipopt/package.py
@@ -10,9 +10,9 @@ class PyPyscipopt(PythonPackage):
     """Python interface for the SCIP Optimization Suite"""
 
     homepage = "https://github.com/scipopt/PySCIPOpt"
-    pyip     = "pyscipopt/pysciopt-3.3.0.tar.gz"
+    pypi     = "PySCIPOpt/PySCIPOpt-3.4.0.tar.gz"
 
-    version('3.3.0', sha256='bae5e19014583f0049018bf6079905a86076da2b06093e989e16838cac88c071')
+    version('3.4.0', sha256='8da4db57b21010e0d5a863292dd455c88dd71ecec12a8439171c213a8092f88a')
 
     depends_on('python',        type=('build', 'run'))
     depends_on('py-setuptools', type='build')
