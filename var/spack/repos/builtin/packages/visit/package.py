b2f89b8d5c5006a849a277de87acb82a63c4bc49
Eric Brugger <brugger1@llnl.gov>
2021-11-04 11:27:46 -0700

    Improve visit package to address review comments.

--- var/spack/repos/builtin/packages/visit/package.py
+++ var/spack/repos/builtin/packages/visit/package.py
@@ -54,7 +54,7 @@ class Visit(CMakePackage):
     # (dyld: Symbol not found: _GENERAL_NAME_free - which comes from OpenSSL)
     #
     ############################
-    homepage = "https://visit-dav.github.io/visit-website/"
+    homepage = "https://wci.llnl.gov/simulation/computer-codes/visit/"
     git      = "https://github.com/visit-dav/visit.git"
     url = "https://github.com/visit-dav/visit/releases/download/v3.2.1/visit3.2.1.tar.gz"
 
@@ -68,6 +68,15 @@ class Visit(CMakePackage):
 
     version('develop', branch='develop')
     version('3.2.1', sha256='779d59564c63f31fcbfeff24b14ddd6ac941b3bb7d671d31765a770d193f02e8')
+    version('3.1.1', sha256='0b60ac52fd00aff3cf212a310e36e32e13ae3ca0ddd1ea3f54f75e4d9b6c6cf0')
+    version('3.0.1', sha256='a506d4d83b8973829e68787d8d721199523ce7ec73e7594e93333c214c2c12bd')
+    version('2.13.3', sha256='cf0b3d2e39e1cd102dd886d3ef6da892733445e362fc28f24d9682012cccf2e5')
+    version('2.13.0', sha256='716644b8e78a00ff82691619d4d1e7a914965b6535884890b667b97ba08d6a0f')
+    version('2.12.3', sha256='2dd351a291ee3e79926bc00391ca89b202cfa4751331b0fdee1b960c7922161f')
+    version('2.12.2', sha256='55897d656ac2ea4eb87a30118b2e3963d6c8a391dda0790268426a73e4b06943')
+    version('2.10.3', sha256='05018215c4727eb42d47bb5cc4ff937b2a2ccaca90d141bc7fa426a0843a5dbc')
+    version('2.10.2', sha256='89ecdfaf197ef431685e31b75628774deb6cd75d3e332ef26505774403e8beff')
+    version('2.10.1', sha256='6b53dea89a241fd03300a7a3a50c0f773e2fb8458cd3ad06816e9bd2f0337cd8')
 
     variant('gui',    default=True, description='Enable VisIt\'s GUI')
     variant('adios2', default=False, description='Enable ADIOS2 file format')
@@ -171,6 +180,15 @@ class Visit(CMakePackage):
 
     depends_on('cmake@3.14.7', type='build')
     # https://github.com/visit-dav/visit/issues/3498
+    # The vtk_compiler_visibility patch fixes a bug where
+    # VTKGenerateExportHeader.cmake fails to recognize gcc versions 10.0
+    # or greater.
+    # The vtk_rendering_opengl2_x11 patch adds include directories to
+    # Rendering/OpenGL2/CMakeLists.txt for systems that don't have the
+    # system X libraries and include files installed.
+    # The vtk_wrapping_python_x11 patch adds include directories to
+    # Wrapping/Python/CMakelists.txt for systems that don't have the
+    # system X libraries and include files installed.
     depends_on('vtk@8.1.0+opengl2+osmesa~python',
                patches=[patch('vtk_compiler_visibility.patch'),
                         patch('vtk_rendering_opengl2_x11.patch'),
@@ -187,13 +205,19 @@ class Visit(CMakePackage):
     depends_on('vtk+python', when='+python @3.2:,develop')
     depends_on('vtk~mpi', when='~mpi')
     depends_on('vtk+qt', when='+gui')
+    # VisIt doesn't work with later versions of qt.
     depends_on('qt+gui@5.14.2:', when='+gui @3.2:,develop')
-    depends_on('qwt@6.1.6', when='+gui')
-    depends_on('python@3.7.7', when='+python')
-    depends_on('llvm@6.0.1', when='^mesa')
-    depends_on('mesa+glx@20.2.1', when='^mesa')
-    depends_on('mesa-glu@9.0.1', when='^mesa')
-    depends_on('hdf5@1.8.14', when='+hdf5')
+    depends_on('qwt', when='+gui')
+    # python@3.8 doesn't work with VisIt.
+    depends_on('python@3.7', when='+python')
+    # llvm@12.0.1, @11.1.0, @10.0.1 fail in build phase with gcc 6.1.0.
+    # llvm@9.0.1 fails in cmake phase with gcc 6.1.0.
+    depends_on('llvm@6:8', when='^mesa')
+    depends_on('mesa+glx', when='^mesa')
+    depends_on('mesa-glu', when='^mesa')
+    # VisIt doesn't build with hdf5@1.12 and hdf5@1.10 produces files that
+    # are incompatible with hdf5@1.8
+    depends_on('hdf5@1.8', when='+hdf5')
     # VisIt uses Silo's 'ghost zone' data structures, which are only available
     # in v4.10+ releases: https://wci.llnl.gov/simulation/computer-codes/silo/releases/release-notes-4.10
     depends_on('silo@4.10:+shared', when='+silo')
