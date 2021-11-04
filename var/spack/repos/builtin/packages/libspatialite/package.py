411590e0c10daa901b62540f158f5f2816a65aaf
sbulut <sbulut@3vgeomatics.com>
2021-11-04 00:26:30 -0700

    undo preferred version

--- var/spack/repos/builtin/packages/libspatialite/package.py
+++ var/spack/repos/builtin/packages/libspatialite/package.py
@@ -17,7 +17,7 @@ class Libspatialite(AutotoolsPackage):
     manual_download = True
 
     version('5.0.1', sha256='eecbc94311c78012d059ebc0fae86ea5ef6eecb13303e6e82b3753c1b3409e98')
-    version('5.0.0', preferred=True, sha256='7b7fd70243f5a0b175696d87c46dde0ace030eacc27f39241c24bac5dfac6dac')
+    version('5.0.0', sha256='7b7fd70243f5a0b175696d87c46dde0ace030eacc27f39241c24bac5dfac6dac')
     # Must download manually from:
     #     https://www.gaia-gis.it/fossil/libspatialite/info/c7f67038bf06d98d
     # For instructions on the file:// below..
