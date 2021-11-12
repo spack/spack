eb99bd86a5773481faab9d32ea50dea7ee1e2cc6
Stephen Crowell <stephen.crowell@kitware.com>
2021-11-12 20:34:06 +0000

    Upadted cmake args for vtk

--- var/spack/repos/builtin/packages/vtk/package.py
+++ var/spack/repos/builtin/packages/vtk/package.py
@@ -61,9 +61,6 @@ class Vtk(CMakePackage):
     # VTK 8.1, that should change
     conflicts('+osmesa', when='+qt')
 
-    # Do not build osmesa and egl together
-    conflicts('+osmesa', when='+egl')
-
     extends('python', when='+python')
 
     # Acceptable python versions depend on vtk version
