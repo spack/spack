4e19d144c8a3742360cd3eb22f238767d618eb41
Jen Herting <jen@herting.cc>
2021-11-12 11:04:46 -0500

    [py-pyscipopt] removed redundant python dependency and fixed dependency types

--- var/spack/repos/builtin/packages/py-pyscipopt/package.py
+++ var/spack/repos/builtin/packages/py-pyscipopt/package.py
@@ -14,8 +14,7 @@ class PyPyscipopt(PythonPackage):
 
     version('3.4.0', sha256='8da4db57b21010e0d5a863292dd455c88dd71ecec12a8439171c213a8092f88a')
 
-    depends_on('python',        type=('build', 'run'))
     depends_on('py-setuptools', type='build')
-    depends_on('py-cython',     type=('build', 'run'))
-    depends_on('py-wheel',      type=('build', 'run'))
+    depends_on('py-cython',     type='build')
+    depends_on('py-wheel',      type='build')
     depends_on('scipoptsuite')
