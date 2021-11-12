80e956602edc4ef88f0eefebb96062547f7d1bf2
jacorvar <jacorvar@users.noreply.github.com>
2021-11-12 15:36:28 +0100

    Update spp version
    The following changes resolve the compiler-related conflict, allowing to use gcc > 6.

--- var/spack/repos/builtin/packages/r-phantompeakqualtools/package.py
+++ var/spack/repos/builtin/packages/r-phantompeakqualtools/package.py
@@ -13,18 +13,16 @@ class RPhantompeakqualtools(RPackage):
        package."""
 
     homepage = "https://github.com/kundajelab/phantompeakqualtools"
-    url      = "https://github.com/kundajelab/phantompeakqualtools/raw/master/spp_1.14.tar.gz"
-
-    version('1.14', sha256='d03be6163e82aed72298e54a92c181570f9975a395f57a69b21ac02b1001520b')
-
+    url      = "https://github.com/hms-dbmi/spp/archive/refs/tags/1.15.2.tar.gz"
+    
+    version('1.15', sha256='172516b0f1f2a8132f22ab6fbfe41fb5943d675c2be02c733fcd5aa9651c6d22')
+    
     depends_on('boost@1.41.0:')
     depends_on('r-catools', type=('build', 'run'))
     depends_on('r-snow', type=('build', 'run'))
     depends_on('r-snowfall', type=('build', 'run'))
     depends_on('r-bitops', type=('build', 'run'))
     depends_on('r-rsamtools', type=('build', 'run'))
-
-    conflicts('%gcc@6:')
 
     def setup_build_environment(self, env):
         env.set('BOOST_ROOT', self.spec['boost'].prefix)
