325774e5726f928b68ab227f43d14d4729f0e08a
iarspider <iarspider@gmail.com>
2021-11-12 11:41:37 +0100

    New version: py-pkgconfig 1.5.5

--- var/spack/repos/builtin/packages/py-pkgconfig/package.py
+++ var/spack/repos/builtin/packages/py-pkgconfig/package.py
@@ -18,5 +18,5 @@ class PyPkgconfig(PythonPackage):
 
     depends_on('python@2.6:2.8,3.3:', type=('build', 'run'))
     depends_on('python@2.7:2.8,3.3:', when='@1.5:', type=('build', 'run'))
-    depends_on('py-setuptools', when='@:1.4', type=('build', 'run'))
+    depends_on('py-setuptools', when='@:1.4,1.5.1:', type=('build', 'run'))
     depends_on('pkgconfig', type=('build', 'run'))
