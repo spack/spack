19a533aa4e18c7c6dc894db97c22361ce9015f6a
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2021-11-17 19:29:19 +0100

    Bump connectome-tools to 0.6.2 (#1365)

--- var/spack/repos/builtin/packages/connectome-tools/package.py
+++ var/spack/repos/builtin/packages/connectome-tools/package.py
@@ -13,7 +13,7 @@ class ConnectomeTools(PythonPackage):
     git = "git@bbpgitlab.epfl.ch:nse/connectome-tools.git"
 
     version('develop', branch='main')
-    version('0.6.1', tag='connectome-tools-v0.6.1')
+    version('0.6.2', tag='connectome-tools-v0.6.2')
 
     depends_on('python@3.8:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
@@ -21,13 +21,13 @@ class ConnectomeTools(PythonPackage):
     depends_on('py-click@7.0:8.999', type=('build', 'run'))
     depends_on('py-equation@1.2:', type=('build', 'run'))
     depends_on('py-joblib@1.0.1:', type=('build', 'run'))
-    depends_on('py-jsonschema@3.2.0:3.999', type=('build', 'run'))
+    depends_on('py-jsonschema@3.2.0:4.999', type=('build', 'run'))
     depends_on('py-lxml@3.3:', type=('build', 'run'))
     depends_on('py-numpy@1.9:', type=('build', 'run'))
     depends_on('py-pandas@1.0.0:', type=('build', 'run'))
     depends_on('py-psutil@5.7.2:', type=('build', 'run'))
     depends_on('py-pyyaml@5.3.1:', type=('build', 'run'))
-    depends_on('py-submitit@1.3.3.20210820:', type=('build', 'run'))
+    depends_on('py-submitit@1.4:1.999', type=('build', 'run'))
 
     depends_on('py-bluepy@2.3.0:2.999', type=('build', 'run'))
     depends_on('py-morphio@3.0.1:3.999', type=('build', 'run'))
