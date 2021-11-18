64f7c01edee322a545b98ea0620786fb5c806c00
Larry Knox <lrknox@hdfgroup.org>
2021-11-18 14:25:32 -0600

    Add source url and checksum.  Update HDF5 minimum version.

--- var/spack/repos/builtin/packages/hdf5-vol-external-passthrough/package.py
+++ var/spack/repos/builtin/packages/hdf5-vol-external-passthrough/package.py
@@ -10,11 +10,13 @@ class Hdf5VolExternalPassthrough(CMakePackage):
     """Package for HDF5 external pass-through VOL."""
 
     homepage = "https://sdm.lbl.gov/"
+    url      = "https://github.com/hpc-io/vol-external-passthrough/archive/refs/tags/v1.0.tar.gz"
     git      = "https://github.com/hpc-io/vol-external-passthrough.git"
     maintainers = ['hyoklee']
 
-    version('v1.0')
-    depends_on('hdf5@develop-1.13')
+    version('develop', branch='develop')
+    version('1.0', sha256='99a06d1c31451f8f0c8c10fec112410cda1f951f0eda1bd0ca999d6b35cf7740')
+    depends_on('hdf5@1.13.0:')
 
     def cmake_args(self):
         args = [
