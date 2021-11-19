fd0f0e564d3114811b41b64c77cf95ad9c483aed
Adam J. Stewart <ajstewart426@gmail.com>
2021-11-19 14:35:31 -0600

    py-ruamel-yaml-clib: link to python (#27557)

--- var/spack/repos/builtin/packages/py-ruamel-yaml-clib/package.py
+++ var/spack/repos/builtin/packages/py-ruamel-yaml-clib/package.py
@@ -13,5 +13,5 @@ class PyRuamelYamlClib(PythonPackage):
 
     version('0.2.0', sha256='b66832ea8077d9b3f6e311c4a53d06273db5dc2db6e8a908550f3c14d67e718c')
 
-    depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
+    depends_on('python@2.7:2.8,3.5:', type=('build', 'link', 'run'))
     depends_on('py-setuptools@28.7.0:', type='build')
