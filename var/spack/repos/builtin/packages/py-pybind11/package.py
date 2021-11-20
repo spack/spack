fdd67c6c26f8c884ee7bd21e7b29c48d416313e2
Thomas Green <tomgreen66@hotmail.com>
2021-11-20 07:35:18 +0000

    Update package.py
    Due to python setup.py using the newer `-B` and `-S` cmake options we need to use a more recent cmake.

--- var/spack/repos/builtin/packages/py-pybind11/package.py
+++ var/spack/repos/builtin/packages/py-pybind11/package.py
@@ -45,6 +45,7 @@ class PyPybind11(CMakePackage, PythonPackage):
     depends_on('py-setuptools', type='build')
     depends_on('py-pytest', type='test')
     depends_on('python@2.7:2.8,3.5:', type=('build', 'run'))
+    depends_on('cmake@3.13:', type='build')
 
     # compiler support
     conflicts('%gcc@:4.7')
