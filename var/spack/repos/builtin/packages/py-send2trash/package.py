5e8e20feeb564aa2d9dc641b8d161692024c4d86
iarspider <iarspider@gmail.com>
2021-11-22 14:38:48 +0100

    New version: py-send2trash 1.8.0

--- var/spack/repos/builtin/packages/py-send2trash/package.py
+++ var/spack/repos/builtin/packages/py-send2trash/package.py
@@ -12,6 +12,7 @@ class PySend2trash(PythonPackage):
     homepage = "https://github.com/hsoft/send2trash"
     url      = "https://github.com/hsoft/send2trash/archive/1.5.0.tar.gz"
 
+    version('1.8.0',   sha256='937b038abd9f1e7b8c5d7a116be5dc4663beb71df74dcccffe56cacf992c7a9c')
     version('1.5.0', sha256='7cebc0ffc8b6d6e553bce9c6bb915614610ba2dec17c2f0643b1b97251da2a41')
 
     depends_on('py-setuptools', type='build')
