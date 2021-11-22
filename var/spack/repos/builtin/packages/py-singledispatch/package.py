a870cd94c840ab7b1325021750460d6d2fe04b05
iarspider <iarspider@gmail.com>
2021-11-22 21:48:19 +0100

    Apply suggestions from code review
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-singledispatch/package.py
+++ var/spack/repos/builtin/packages/py-singledispatch/package.py
@@ -15,7 +15,7 @@ class PySingledispatch(PythonPackage):
     version('3.4.0.3', sha256='5b06af87df13818d14f08a028e42f566640aef80805c3b50c5056b086e3c2b9c')
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-setuptools@42:', type='build', when='@3.7.0')
-    depends_on('py-setuptools-scm@3.4.1: +toml', type='build', when='@3.7.0')
+    depends_on('py-setuptools@42:', type='build', when='@3.7:')
+    depends_on('py-setuptools-scm@3.4.1: +toml', type='build', when='@3.7:')
     depends_on('py-six', type=('build', 'run'), when='@:3.6')
     depends_on('py-ordereddict', when="^python@:2.6", type=('build', 'run'))
