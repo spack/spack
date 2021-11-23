cd4df430dbbac7dbecdc2fb05fae2438588d3e45
Glenn Johnson <glenn-johnson@uiowa.edu>
2021-11-23 11:45:36 -0600

    rewrite of opencv
    - all prebuilt apps are now variants and can be installed
    - core is no longer a variant. It was always built anyway so it was not
      really a variant.
    - contrib is no longer a variant. All of the contrib modules are now
      available as variants.
    - components that can not be built with Spack are no longer variants.
      They are set to 'off' to prevent pulling from system.
    - handle the case where a module and a component have the same name
    - use `with when` framework
    - adjust dependencies and conflicts
    - new package: libraw1394
    - have libdc1394 depend on libraw1394
    - patch to find clp
    - patch to find onnx
    - patch for cvv to find Qt
    - format with black

--- var/spack/repos/builtin/packages/libdc1394/package.py
+++ var/spack/repos/builtin/packages/libdc1394/package.py
@@ -10,26 +10,30 @@ class Libdc1394(AutotoolsPackage):
     """Library providing an API for IEEE 1394 cameras."""
 
     homepage = "https://damien.douxchamps.net/ieee1394/libdc1394/"
-    url      = "https://downloads.sourceforge.net/project/libdc1394/libdc1394-2/2.2.6/libdc1394-2.2.6.tar.gz"
+    url = "https://downloads.sourceforge.net/project/libdc1394/libdc1394-2/2.2.6/libdc1394-2.2.6.tar.gz"
 
-    maintainers = ['traversaro']
+    maintainers = ["traversaro"]
 
-    version('2.2.6', sha256='2b905fc9aa4eec6bdcf6a2ae5f5ba021232739f5be047dec8fe8dd6049c10fed')
+    version(
+        "2.2.6",
+        sha256="2b905fc9aa4eec6bdcf6a2ae5f5ba021232739f5be047dec8fe8dd6049c10fed",
+    )
 
-    depends_on('pkgconfig', type='build')
-    depends_on('libusb')
-    depends_on('sdl2')
-    depends_on('libraw1394')
+    depends_on("pkgconfig", type="build")
+    depends_on("libusb")
+    depends_on("sdl2")
+    depends_on("libraw1394")
 
     def configure_args(self):
         args = []
-        args.extend([
-            '--disable-dependency-tracking',
-            '--disable-examples',
-            '--disable-sdltest',
-        ])
+        args.extend(
+            [
+                "--disable-dependency-tracking",
+                "--disable-examples",
+                "--disable-sdltest",
+            ]
+        )
         return args
 
     def setup_build_environment(self, env):
-        env.set('SDL_CONFIG', join_path(self.spec['sdl2'].prefix.bin,
-                                        'sdl2-config'))
+        env.set("SDL_CONFIG", join_path(self.spec["sdl2"].prefix.bin, "sdl2-config"))
--- var/spack/repos/builtin/packages/libraw1394/package.py
+++ var/spack/repos/builtin/packages/libraw1394/package.py
@@ -11,6 +11,9 @@ class Libraw1394(AutotoolsPackage):
     Linux 1394 subsystem's raw1394 user space interface."""
 
     homepage = "https://sourceforge.net/projects/libraw1394/"
-    url      = "https://sourceforge.net/projects/libraw1394/files/libraw1394/1.2.0/libraw1394-1.2.0.tar.gz"
+    url = "https://sourceforge.net/projects/libraw1394/files/libraw1394/1.2.0/libraw1394-1.2.0.tar.gz"
 
-    version('1.2.0', sha256='1fdcfa4c5a0938705b925d06f17da9be6ec3f8f065040bb7f33082ef3fc63fad')
+    version(
+        "1.2.0",
+        sha256="1fdcfa4c5a0938705b925d06f17da9be6ec3f8f065040bb7f33082ef3fc63fad",
+    )
