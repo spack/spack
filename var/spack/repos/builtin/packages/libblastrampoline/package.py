ec9c0bd9a268f37ad34899353604b95b116faaec
Harmen Stoppels <harmenstoppels@gmail.com>
2021-11-23 18:06:16 +0100

    Add myself as maintainer

--- var/spack/repos/builtin/packages/libblastrampoline/package.py
+++ var/spack/repos/builtin/packages/libblastrampoline/package.py
@@ -13,6 +13,8 @@ class Libblastrampoline(MakefilePackage):
     git      = "https://github.com/JuliaLinearAlgebra/libblastrampoline.git"
     url      = "https://github.com/JuliaLinearAlgebra/libblastrampoline/archive/refs/tags/v3.1.0.tar.gz"
 
+    maintainers = ['haampie']
+
     version('3.1.0', sha256='f6136cc2b5d090ceca67cffa55b4c8af4bcee874333d49297c867abdb0749b5f')
     version('3.0.4', sha256='3c8a54a3bd8a2737b7f74ebeb56df8e2a48083c9094dbbff80b225c228e31793')
     version('3.0.3', sha256='a9c553ee6f20fa2f92098edcb3fc4a331c653250e559f72b9317b4ee84500cd7')
