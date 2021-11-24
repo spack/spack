29a56c451c94c6ebbba81872aa6752a978968d71
iarspider <iarspider@gmail.com>
2021-11-24 18:07:41 +0100

    New version: py-yarl 1.7.2

--- var/spack/repos/builtin/packages/py-yarl/package.py
+++ var/spack/repos/builtin/packages/py-yarl/package.py
@@ -10,19 +10,27 @@ class PyYarl(PythonPackage):
     homepage = "https://github.com/aio-libs/yarl"
     url      = "https://github.com/aio-libs/yarl/archive/v1.4.2.tar.gz"
 
+    version('1.7.2', sha256='19b94c68e8eda5731f87d79e3c34967a11e69695965113c4724d2491f76ad461')
     version('1.4.2', sha256='a400eb3f54f7596eeaba8100a8fa3d72135195423c52808dc54a43c6b100b192')
 
     depends_on('python@3.5:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools@40:', type='build', when='@1.7.2:')
     depends_on('py-cython', type='build')
     depends_on('py-multidict@4.0:', type=('build', 'run'))
     depends_on('py-idna@2.0:', type=('build', 'run'))
+    depends_on('py-typing-extensions@3.7.4:', type=('build', 'run'), when='@1.7.2:')
 
     @run_before('build')
     def fix_cython(self):
+        if self.spec.satisfies('@1.7.2:'):
+            pyxfile = 'yarl/_quoting_c'
+        else:
+            pyxfile = 'yarl/_quoting'
+
         cython = self.spec['py-cython'].command
         cython('-3',
                '-o',
-               'yarl/_quoting.c',
-               'yarl/_quoting.pyx',
+               pyxfile + '.c',
+               pyxfile + '.pyx',
                '-Iyarl')
