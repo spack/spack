84be289cfd3989d5d6b5fb6e03561ffddcc46db3
Jen Herting <jen@herting.cc>
2021-11-29 14:48:37 -0500

    [py-gin-config]
    - depends on setup tools
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-gin-config/package.py
+++ var/spack/repos/builtin/packages/py-gin-config/package.py
@@ -3,48 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-gin-config
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-gin-config
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyGinConfig(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Gin provides a lightweight configuration framework for
+    Python, based on dependency injection."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/google/gin-config"
     pypi     = "gin-config/gin-config-0.5.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.5.0', sha256='0c6ea5026ded927c8c93c990b01c695257c1df446e45e549a158cfbc79e19ed6')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('py-setuptools', type='build')
