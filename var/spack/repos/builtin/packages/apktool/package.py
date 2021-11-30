d614ce4ebb678194e1244ec4125da4fe86251033
Jen Herting <jen@herting.cc>
2021-11-30 16:00:33 -0500

    [apktool]
    - added homepage
    - added description
    - removed fixmes
    - removed debug code

--- var/spack/repos/builtin/packages/apktool/package.py
+++ var/spack/repos/builtin/packages/apktool/package.py
@@ -3,41 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install apktool
-#
-# You can edit this file again by typing:
-#
-#     spack edit apktool
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Apktool(Package):
-    """FIXME: Put a proper description of your package here."""
+    """A tool for reverse engineering 3rd party, closed, binary
+    Android apps."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://ibotpeaches.github.io/Apktool/"
     url      = "https://github.com/iBotPeaches/Apktool/archive/refs/tags/v2.6.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('2.6.0', sha256='74739cdb1434ca35ec34e51ca7272ad3f378ae3ed0a2d5805d9a2fab5016037f')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('java@8:', type=('build', 'run'))
 
     phases = ['build', 'install']
@@ -46,8 +23,6 @@ class Apktool(Package):
         env.set('LC_ALL', 'en_US.UTF-8')
 
     def build(self, spec, prefix):
-        stty = which('stty')
-        stty()
         gradlew = Executable('./gradlew')
         gradlew('--info', '--debug', 'build', 'shadowJar')
 
