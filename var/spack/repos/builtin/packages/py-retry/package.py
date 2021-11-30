87772db0e21f996f2638a64d50e0148aca6481d6
Jen Herting <jen@herting.cc>
2021-11-30 17:17:20 -0500

    [py-retry]
    - added homepage
    - removed unnecessary depends_on

--- var/spack/repos/builtin/packages/py-retry/package.py
+++ var/spack/repos/builtin/packages/py-retry/package.py
@@ -9,11 +9,9 @@ from spack import *
 class PyRetry(PythonPackage):
     """Easy to use retry decorator."""
 
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/invl/retry"
     pypi     = "retry/retry-0.9.2.tar.gz"
 
     version('0.9.2', sha256='f8bfa8b99b69c4506d6f5bd3b0aabf77f98cdb17f3c9fc3f5ca820033336fba4')
 
-    depends_on('python',                type=('build', 'run'))
     depends_on('py-setuptools',         type='build')
-    depends_on('py-decorator@3.4.2:',   type=('build', 'run'))
