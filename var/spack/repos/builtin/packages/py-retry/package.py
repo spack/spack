5cfb2c1205cd7663fef6bcb03febb7a5a8b2bd10
Jen Herting <jen@herting.cc>
2021-12-01 15:26:19 -0500

    [py-retry] added dependencies back

--- var/spack/repos/builtin/packages/py-retry/package.py
+++ var/spack/repos/builtin/packages/py-retry/package.py
@@ -14,4 +14,8 @@ class PyRetry(PythonPackage):
 
     version('0.9.2', sha256='f8bfa8b99b69c4506d6f5bd3b0aabf77f98cdb17f3c9fc3f5ca820033336fba4')
 
-    depends_on('py-setuptools',         type='build')
+    depends_on('python@2.6:',         type=('build', 'run'))
+    depends_on('py-setuptools',       type='build')
+    depends_on('py-pbr',              type='build')
+    depends_on('py-decorator@3.4.2:', type=('build', 'run'))
+    depends_on('py-py@1.4.26:1.999',  type=('build', 'run'))
