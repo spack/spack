48b8bfa4626c175510b55a343cbff0d69b8ab901
Adam J. Stewart <ajstewart426@gmail.com>
2021-12-03 14:19:18 -0600

    Use pip to bootstrap pip

--- var/spack/repos/builtin/packages/py-pip/package.py
+++ var/spack/repos/builtin/packages/py-pip/package.py
@@ -6,11 +6,12 @@
 from spack import *
 
 
-class PyPip(PythonPackage):
+class PyPip(Package):
     """The PyPA recommended tool for installing Python packages."""
 
     homepage = "https://pip.pypa.io/"
-    pypi = "pip/pip-20.2.tar.gz"
+    url = "https://files.pythonhosted.org/packages/source/p/pip/pip-20.2.tar.gz"
+    list_url = "https://pypi.org/simple/pip/"
 
     version('21.1.2', sha256='eb5df6b9ab0af50fe1098a52fd439b04730b6e066887ff7497357b9ebd19f79b')
     version('20.2',   sha256='912935eb20ea6a3b5ed5810dde9754fde5563f5ca9be44a8a6e5da806ade970b')
@@ -21,12 +22,24 @@ class PyPip(PythonPackage):
     version('10.0.1', sha256='f2bd08e0cd1b06e10218feaf6fef299f473ba706582eb3bd9d52203fdbd7ee68')
     version('9.0.1',  sha256='09f243e1a7b461f654c26a725fa373211bb7ff17a9300058b205c61658ca940d')
 
+    extends('python')
     depends_on('python@3.6:', when='@21:', type=('build', 'run'))
     depends_on('python@2.7:2.8,3.5:', when='@19.2:', type=('build', 'run'))
     depends_on('python@2.7:2.8,3.4:', when='@18:', type=('build', 'run'))
     depends_on('python@2.7:2.8,3.3:', when='@10:', type=('build', 'run'))
     depends_on('python@2.6:2.8,3.3:', type=('build', 'run'))
 
-    # Most Python packages only require setuptools as a build dependency.
-    # However, pip requires setuptools during runtime as well.
-    depends_on('py-setuptools', type=('build', 'run'))
+    # This bootstrapping procedure is similar to the one used by Nix:
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/python-modules/bootstrapped-pip/default.nix
+    # Also see further discussion in:
+    # https://discuss.python.org/t/bootstrapping-a-specific-version-of-pip/12306
+
+    def setup_build_environment(self, env):
+        # Use pip to bootstrap itself
+        env.prepend_path('PYTHONPATH', 'src')
+
+    def install(self, spec, prefix):
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/python-modules/bootstrapped-pip/default.nix
+        python('-m', 'pip', 'install', '--no-deps', '--prefix', prefix,
+               '--ignore-installed', '--no-build-isolation', '--no-index',
+               '--no-warn-script-location', '.')
