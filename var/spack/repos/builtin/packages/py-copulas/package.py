afd5769f15f98d821375850e80383370e8c320cc
Jen Herting <jen@herting.cc>
2021-12-03 15:24:22 -0500

    [py-copulas] flake8 and removed extra spaces

--- var/spack/repos/builtin/packages/py-copulas/package.py
+++ var/spack/repos/builtin/packages/py-copulas/package.py
@@ -7,17 +7,22 @@ from spack import *
 
 
 class PyCopulas(PythonPackage):
-    """Copulas is a Python library for modeling multivariate distributions and sampling from them using copula functions. Given a table containing numerical data, we can use Copulas to learn the distribution and later on generate new synthetic rows following the same statistical properties."""
+    """Copulas is a Python library for modeling multivariate
+    distributions and sampling from them using copula
+    functions. Given a table containing numerical data, we can
+    use Copulas to learn the distribution and later on generate
+    new synthetic rows following the same statistical
+    properties."""
 
     homepage = "https://github.com/sdv-dev/Copulas"
     pypi     = "copulas/copulas-0.6.0.tar.gz"
 
     version('0.6.0', sha256='9de6cc738769db19794fc18e2f506a4b5ee17e6902519c0842a4698c0efb6749')
 
-    depends_on('python@3.6:',                       type=('build', 'run'))
-    depends_on('py-setuptools',                     type='build')
-    depends_on('py-matplotlib@3.2:4.0',             type=('build', 'run'))
-    depends_on('py-numpy@1.18:1.20',                type=('build', 'run'), when= '+ python@3.6')
-    depends_on('py-numpy@1.20:',                    type=('build', 'run'), when= '+ python@3.7:')
-    depends_on('py-pandas@1.1.3:2.0.0',             type=('build', 'run'))
-    depends_on('py-scipy@1.5.4:2.0.0',              type=('build', 'run'))
+    depends_on('python@3.6:',           type=('build', 'run'))
+    depends_on('py-setuptools',         type='build')
+    depends_on('py-matplotlib@3.2:4.0', type=('build', 'run'))
+    depends_on('py-numpy@1.18:1.20',    type=('build', 'run'), when='+ python@3.6')
+    depends_on('py-numpy@1.20:',        type=('build', 'run'), when='+ python@3.7:')
+    depends_on('py-pandas@1.1.3:2.0.0', type=('build', 'run'))
+    depends_on('py-scipy@1.5.4:2.0.0',  type=('build', 'run'))
