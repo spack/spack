4c922ec572d19c8b4d7a181241937f7d30a1022d
Ethan Stam <33101855+EthanS94@users.noreply.github.com>
2021-12-02 18:18:23 -0700

    ParaView: add dependencies needed in paraview@master (#27745)
    * The proj and nlohmann-json packages are needed in paraview@master
    * ParaView: bump master subdir version to 5.10

--- var/spack/repos/builtin/packages/paraview/package.py
+++ var/spack/repos/builtin/packages/paraview/package.py
@@ -173,6 +173,14 @@ class Paraview(CMakePackage, CudaPackage):
     # https://gitlab.kitware.com/paraview/paraview/-/merge_requests/4951
     depends_on('cli11@1.9.1', when='@5.10:')
 
+    # ParaView depends on nlohmann-json due to changes in MR
+    # https://gitlab.kitware.com/vtk/vtk/-/merge_requests/8550
+    depends_on('nlohmann-json', when='@master')
+
+    # ParaView depends on proj@8.1.0 due to changes in MR
+    # https://gitlab.kitware.com/vtk/vtk/-/merge_requests/8474
+    depends_on('proj@8.1.0', when='@master')
+
     patch('stl-reader-pv440.patch', when='@4.4.0')
 
     # Broken gcc-detection - improved in 5.1.0, redundant later
@@ -207,7 +215,7 @@ class Paraview(CMakePackage, CudaPackage):
     def paraview_subdir(self):
         """The paraview subdirectory name as paraview-major.minor"""
         if self.spec.version == Version('master'):
-            return 'paraview-5.9'
+            return 'paraview-5.10'
         else:
             return 'paraview-{0}'.format(self.spec.version.up_to(2))
 
