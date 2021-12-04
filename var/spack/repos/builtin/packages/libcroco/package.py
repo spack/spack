0309c5f0eb7b4fd7b2c4a92eaf659e06f83e0898
Wouter Deconinck <wdconinc@gmail.com>
2021-12-03 18:58:02 -0600

    [libcroco] variant doc, depends_on gtk-doc when +doc

--- var/spack/repos/builtin/packages/libcroco/package.py
+++ var/spack/repos/builtin/packages/libcroco/package.py
@@ -15,12 +15,19 @@ class Libcroco(AutotoolsPackage):
     version('0.6.13', sha256='767ec234ae7aa684695b3a735548224888132e063f92db585759b422570621d4')
     version('0.6.12', sha256='ddc4b5546c9fb4280a5017e2707fbd4839034ed1aba5b7d4372212f34f84f860')
 
+    variant('doc', default=False, description='Build documentation with gtk-doc')
+
     depends_on('glib')
     depends_on('libxml2')
-    depends_on('gtk-doc', type='build')
+    depends_on('gtk-doc', type='build', when='+doc')
     depends_on('pkgconfig', type='build')
 
     def configure_args(self):
-        # macOS ld does not support this flag
-        # https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/libcroco.rb
-        return ['--disable-Bsymbolic']
+        args = [
+            '--enable-gtk-doc=' + 'yes' if spec.variants['doc'].value else 'no'
+        ]
+        if sys.platform == 'darwin':
+            # macOS ld does not support this flag
+            # https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/libcroco.rb
+            args.append('--disable-Bsymbolic')
+        return args
