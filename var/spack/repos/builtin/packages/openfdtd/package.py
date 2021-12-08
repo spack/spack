880fc0e7cbd42e6fba773802178ff6b1e482e84a
Toyohisa Kameyama <fj5358cc@fujitsu.com>
2021-12-08 16:42:26 +0900

    openfdtd: Add 2.6.3 and support fujitsu compiler.

--- var/spack/repos/builtin/packages/openfdtd/package.py
+++ var/spack/repos/builtin/packages/openfdtd/package.py
@@ -5,7 +5,6 @@
 
 from spack import *
 
-
 class Openfdtd(MakefilePackage):
     """OpenFDTD is general purpose FDTD simulator applicable to a wide range
        of applications. The FDTD method (Finite Difference Time Domain method)
@@ -14,20 +13,30 @@ class Openfdtd(MakefilePackage):
        by the difference method."""
 
     homepage = "http://www.e-em.co.jp/OpenFDTD/"
-    url      = "http://www.e-em.co.jp/OpenFDTD/OpenFDTD.zip"
+    url      = "http://www.e-em.co.jp/OpenFDTD/old/OpenFDTD_230.zip"
 
+    version('2.6.3', sha256='1551cce7f96c1c53ad5d5e676bce2b26fd1593dd5f492a801e976a8a65a42a00',
+        url='http://www.e-em.co.jp/OpenFDTD/OpenFDTD.zip')
     version('2.6.0', sha256='92f7b92dc55ff6d8fc8c31eda77ca10fe25a5f54b002f2523a3d67f485d77e9f')
-    version('2.3.0', sha256='10ac70f2ed7160da87dd9222a5a17ca7b72365ee886235359afc48c4fb7b4be4',
-            url='http://www.e-em.co.jp/OpenFDTD/old/OpenFDTD_230.zip')
+    version('2.3.0', sha256='10ac70f2ed7160da87dd9222a5a17ca7b72365ee886235359afc48c4fb7b4be4')
 
     variant('mpi', default=False, description='Build with MPI Support')
 
     depends_on('mpi', when='+mpi')
 
     def edit(self, spec, prefix):
-        filter_file('gcc', spack_cc, './src/Makefile_gcc')
-        if '+mpi' in self.spec:
-            filter_file('mpicc', spec['mpi'].mpicc, './mpi/Makefile_gcc')
+        makefiles = [join_path('src', 'Makefile_gcc')]
+        if spec.satisfies('+mpi'):
+            makefiles.append(join_path('mpi', 'Makefile_gcc'))
+        for makefile in makefiles:
+            m = FileFilter(makefile)
+            m.filter('gcc', spack_cc)
+            m.filter('-fopenmp', self.compiler.openmp_flag)
+            if spec.satisfies('+mpi'):
+                m.filter('mpicc', spec['mpi'].mpicc)
+            if spec.satisfies('%fj'):
+                m.filter('-Ofast', '-Kfast,ocl -D_VECTOR')
+                m.filter('-O2', '-Kfast,ocl -D_VECTOR')
 
     # Openfdtd has "Makefile" and "Makefile_gcc".
     # "Makefile" is used only in Windows development environment.
