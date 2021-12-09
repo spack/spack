618eff18f752a850c79cf174a13240158daadaad
iarspider <iarspider@gmail.com>
2021-12-09 22:11:18 +0100

    Update package.py

--- var/spack/repos/builtin/packages/py-climate/package.py
+++ var/spack/repos/builtin/packages/py-climate/package.py
@@ -10,9 +10,9 @@ class PyClimate(PythonPackage):
     """Command line arguments parsing"""
 
     homepage = "https://pypi.org/project/climate/"
+    url = 'https://pypi.io/packages/py3/c/climate/climate-0.1.0-py3-none-any.whl'
 
-    version('0.1.0', sha256='01026c764b34d8204b8f527a730ef667fa5827fca765993ff1ed3e9dab2c11ae', expand=False,
-            url='https://files.pythonhosted.org/packages/1c/d6/ed62a152d3728f33e38528ba8d314e350c03d3699cba1fc63641ed05af58/climate-0.1.0-py3-none-any.whl')
+    version('0.1.0', sha256='01026c764b34d8204b8f527a730ef667fa5827fca765993ff1ed3e9dab2c11ae', expand=False)
 
     depends_on('python@3.7:3', type=('build', 'run'))
     depends_on('py-wheel',     type='build')
