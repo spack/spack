201a8b8f029b7433cdc0a98b6a029a831ec8a18f
Toyohisa Kameyama <fj5358cc@fujitsu.com>
2021-12-10 17:44:41 +0900

    Add frontistr@5.3 Add fujitsu-frontistr static variant style fix.

--- var/spack/repos/builtin/packages/frontistr/package.py
+++ var/spack/repos/builtin/packages/frontistr/package.py
@@ -30,12 +30,13 @@ class FrontistrBase(CMakePackage):
         cmake_args = [
             define('WITH_ML', True),
             define('REFINER_INCLUDE_PATH',
-                self.spec['revocap-refiner'].prefix.include),
+                   self.spec['revocap-refiner'].prefix.include),
             define('REFINER_LIBRARIES',
-                join_path(self.spec['revocap-refiner'].prefix.lib,
-                    'libRcapRefiner.a'))
+                   join_path(self.spec['revocap-refiner'].prefix.lib,
+                             'libRcapRefiner.a'))
         ]
         return cmake_args
+
 
 class Frontistr(FrontistrBase):
     """Open-Source Large-Scale Parallel FEM Program for
@@ -45,6 +46,7 @@ class Frontistr(FrontistrBase):
     git      = "https://gitlab.com/FrontISTR-Commons/FrontISTR.git"
     maintainers = ['hiroshi.okuda', 'kgoto', 'morita', 'inagaki', 'michioga']
 
+    version('5.3', tag='v5.3')
     version('5.2', tag='v5.2')
     version('5.1.1', tag='v5.1.1')
     version('5.1', tag='v5.1')
--- var/spack/repos/builtin/packages/fujitsu-frontistr/package.py
+++ var/spack/repos/builtin/packages/fujitsu-frontistr/package.py
@@ -21,6 +21,11 @@ class FujitsuFrontistr(FrontistrBase):
     version('5.2', sha256='ebf73a96c33ae7c9e616c99f9ce07ec90d802764dbf6abf627b0083c3bbd2b2e')
     version('5.0', sha256='7a3a2dd0f834048fb71cc254c9da6c2637fb23110e79b5efaf208d6f69a5b30a')
 
+    variant('static', default=True, description='Build with static linkage')
+    depends_on('metis ~shared', when='+static')
+    depends_on('mumps ~shared', when='+static')
+    depends_on('trilinos ~shared', when='+static')
+
     def url_for_version(self, version):
         url = "https://github.com/fujitsu/FrontISTR/archive/refs/tags/v{0}_tuned.tar.gz"
         return url.format(version)
@@ -30,12 +35,15 @@ class FujitsuFrontistr(FrontistrBase):
         args = super(FujitsuFrontistr, self).cmake_args()
         if self.spec.satisfies('%fj'):
             args.extend([
-                define('CMAKE_C_FLAGS', '-Kcmodel=large -Nlst=t -Kocl -Kfast -Kzfill -Koptmsg=2'),
-                define('CMAKE_CXX_FLAGS', '-Kcmodel=large -Nlst=t -Kocl -Kfast -Kzfill -Koptmsg=2'),
-                define('CMAKE_Fortran_FLAGS', '-Kcmodel=large -Nlst=t -Kocl -Kfast -Kzfill -Koptmsg=2'),
+                define('CMAKE_C_FLAGS',
+                       '-Kcmodel=large -Nlst=t -Kocl -Kfast -Kzfill -Koptmsg=2'),
+                define('CMAKE_CXX_FLAGS',
+                       '-Kcmodel=large -Nlst=t -Kocl -Kfast -Kzfill -Koptmsg=2'),
+                define('CMAKE_Fortran_FLAGS',
+                       '-Kcmodel=large -Nlst=t -Kocl -Kfast -Kzfill -Koptmsg=2'),
                 define('CMAKE_Fortran_MODDIR_FLAG', 'M'),
-                define('OpenMP_C_FLAGS', '-Kopenmp -Kparallel'),
-                define('OpenMP_CXX_FLAGS', '-Kopenmp -Kparallel'),
-                define('OpenMP_Fortran_FLAGS', '-Kopenmp -Kparallel')
+                define('OpenMP_C_FLAGS', '-Kopenmp'),
+                define('OpenMP_CXX_FLAGS', '-Kopenmp'),
+                define('OpenMP_Fortran_FLAGS', '-Kopenmp')
             ])
         return args
