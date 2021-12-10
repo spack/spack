bcd33571194eedf80f98761831b4a2d76d7e6132
Ivan Razumov <iarspider@gmail.com>
2021-12-10 12:43:19 +0100

    Build py-flawfinder from source

--- var/spack/repos/builtin/packages/py-flawfinder/package.py
+++ var/spack/repos/builtin/packages/py-flawfinder/package.py
@@ -6,23 +6,13 @@
 from spack import *
 
 
-class PyFlawfinder(PythonPackage):
+class PyFlawfinder(PythonPackage, SourceforgePackage):
     """a program that examines source code looking for security weaknesses"""
 
     homepage = "http://dwheeler.com/flawfinder/"
+    sourceforge_mirror_path = "project/flawfinder/flawfinder-2.0.19.tar.gz"
 
-    version('2.0.19', sha256='f18f0021d9a2f3ffc1c1e32aa7bb1bb5ca1bad91b82b32157f44e5d0a1a23ad0',
-            url='https://files.pythonhosted.org/packages/98/64/32f24ec56ea8603ccfc471549de2fb9957613fba75315e121108a9b80b51/flawfinder-2.0.19-py2.py3-none-any.whl',
-            expand=False)
+    version('2.0.19', sha256='fe550981d370abfa0a29671346cc0b038229a9bd90b239eab0f01f12212df618')
 
     depends_on('python@2.7:', type=('build', 'run'))
-    depends_on('py-pip',      type='build')
-    depends_on('py-wheel',    type='build')
-
-    phases = ['install']
-
-    # copied from py-azureml-core
-    def install(self, spec, prefix):
-        pip = which('pip')
-        pip('install', '--no-deps', self.stage.archive_file,
-            '--prefix={0}'.format(prefix))
+    depends_on('py-setuptools', type='build')
