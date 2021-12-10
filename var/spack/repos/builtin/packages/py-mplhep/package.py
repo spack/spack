98c740f80ed396b306c7d398b35435c93b9b01ca
iarspider <iarspider@gmail.com>
2021-12-10 18:25:02 +0100

    Update var/spack/repos/builtin/packages/py-mplhep/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-mplhep/package.py
+++ var/spack/repos/builtin/packages/py-mplhep/package.py
@@ -15,7 +15,7 @@ class PyMplhep(PythonPackage):
     version('0.3.15', sha256='595f796ea65930094e86a805214e0d44537ead267a7487ae16eda02d1670653e')
 
     depends_on('python@3.7:',        type=('build', 'run'))
-    depends_on('py-setuptools',      type='build')
+    depends_on('py-setuptools@39.2:',      type='build')
     depends_on('py-mplhep-data',     type=('build', 'run'))
     depends_on('py-matplotlib@3.4:', type=('build', 'run'))
     depends_on('py-numpy@1.16.0:',   type=('build', 'run'))
