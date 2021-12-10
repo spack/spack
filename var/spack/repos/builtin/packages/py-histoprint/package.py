fb02e5136f35d16881f25a787dfc4ad8b6eff475
iarspider <iarspider@gmail.com>
2021-12-10 18:24:57 +0100

    Update var/spack/repos/builtin/packages/py-histoprint/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-histoprint/package.py
+++ var/spack/repos/builtin/packages/py-histoprint/package.py
@@ -15,7 +15,8 @@ class PyHistoprint(PythonPackage):
     version('2.2.0', sha256='ef8b65f7926aaa989f076857b76291175245dd974804b408483091d1e28b00f6')
 
     depends_on('python@3.6:',     type=('build', 'run'))
-    depends_on('py-setuptools',   type='build')
+    depends_on('py-setuptools@42:',   type='build')
+    depends_on('py-setuptools-scm@3.4:+toml', type='build')
     depends_on('py-click@7.0.0:', type=('build', 'run'))
     depends_on('py-numpy',        type=('build', 'run'))
     depends_on('py-uhi@0.2.1:',   type=('build', 'run'))
