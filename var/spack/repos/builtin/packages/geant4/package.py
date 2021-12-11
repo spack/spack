0281e90d0544c6695d2fa240aa2b6b47d4af1e16
Wouter Deconinck <wdconinc@gmail.com>
2021-12-11 11:07:29 -0600

    [geant4] new version 11.0.0

--- var/spack/repos/builtin/packages/geant4/package.py
+++ var/spack/repos/builtin/packages/geant4/package.py
@@ -19,6 +19,7 @@ class Geant4(CMakePackage):
 
     maintainers = ['drbenmorgan']
 
+    version('11.0.0', sha256='dbfc6b5030a36936b46f56a0bede4b647d0160c178a5629d39ce392124e47936', deprecated=True)
     version('10.7.3', sha256='8615d93bd4178d34f31e19d67bc81720af67cdab1c8425af8523858dcddcf65b')
     version('10.7.2', sha256='593fc85883a361487b17548ba00553501f66a811b0a79039276bb75ad59528cf')
     version('10.7.1', sha256='2aa7cb4b231081e0a35d84c707be8f35e4edc4e97aad2b233943515476955293')
