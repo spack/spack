de655ebef69d9aa3169ec0679cc95489abff844a
Teodor Nikolov <teodor.nikolov22@gmail.com>
2021-12-13 10:53:26 +0100

    Issue a warning on post-installation steps for Sarus
    - Use a patch that fixes the rpath issue for tini-static instead of
      disabling it.
    - Format `sarus/package.py`

--- var/spack/repos/builtin/packages/sarus/package.py
+++ var/spack/repos/builtin/packages/sarus/package.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import llnl.util.tty as tty
 
 from spack import *
 
@@ -11,86 +12,75 @@ class Sarus(CMakePackage):
     """Sarus is an OCI-compliant container engine for HPC systems."""
 
     homepage = "https://github.com/eth-cscs/sarus"
-    url      = "https://github.com/eth-cscs/sarus/archive/1.3.3.tar.gz"
-    git      = "https://github.com/eth-cscs/sarus.git"
+    url = "https://github.com/eth-cscs/sarus/archive/1.3.3.tar.gz"
+    git = "https://github.com/eth-cscs/sarus.git"
     maintainers = ["Madeeks", "teonnik"]
 
-    version('develop', branch='develop')
-    version('master',  branch='master')
-    version('1.4.0',   tag='1.4.0')
-    version('1.3.3',   tag='1.3.3')
-    version('1.3.2',   tag='1.3.2')
-    version('1.3.1',   tag='1.3.1')
-    version('1.3.0',   tag='1.3.0')
-    version('1.2.0',   tag='1.2.0')
-    version('1.1.0',   tag='1.1.0')
-    version('1.0.1',   tag='1.0.1')
-    version('1.0.0',   tag='1.0.0')
+    version("develop", branch="develop")
+    version("master", branch="master")
+    version("1.4.0", tag="1.4.0")
+    version("1.3.3", tag="1.3.3")
+    version("1.3.2", tag="1.3.2")
+    version("1.3.1", tag="1.3.1")
+    version("1.3.0", tag="1.3.0")
+    version("1.2.0", tag="1.2.0")
+    version("1.1.0", tag="1.1.0")
+    version("1.0.1", tag="1.0.1")
+    version("1.0.0", tag="1.0.0")
 
-    variant('ssh', default=True,
-            description='Build and install the SSH hook and custom SSH software '
-                        'to enable connections inside containers')
-    variant('configure_installation', default=True,
-            description='Run the script to setup a starting Sarus configuration as '
-                        'part of the installation phase. Running the script requires '
-                        'super-user privileges.')
+    variant(
+        "ssh",
+        default=True,
+        description="Build and install the SSH hook and custom SSH software "
+        "to enable connections inside containers",
+    )
 
-    depends_on('wget', type='build')
-    depends_on('expat', type='build')
-    depends_on('squashfs', type=('build', 'run'))
-    depends_on('boost@1.65.0: cxxstd=11')
-    depends_on('cpprestsdk@2.10.0:')
-    depends_on('libarchive@3.4.1:')
-    depends_on('rapidjson@00dbcf2', type='build')
-    #depends_on('rapidjson', type='build')
-    depends_on('runc')
-    depends_on('tini')
+    depends_on("wget", type="build")
+    depends_on("expat", type="build")
+    depends_on("squashfs", type=("build", "run"))
+    depends_on("boost@1.65.0: cxxstd=11")
+    depends_on("cpprestsdk@2.10.0:")
+    depends_on("libarchive@3.4.1:")
+    depends_on("rapidjson@00dbcf2", type="build")
+    depends_on("runc")
+    depends_on("tini")
 
     # autoconf is required to build Dropbear for the SSH hook
-    depends_on('autoconf', type='build')
+    depends_on("autoconf", type="build")
 
     # Python 3 is used to run integration tests
-    depends_on('python@3:', type='run', when='@develop')
+    depends_on("python@3:", type="run", when="@develop")
 
     def cmake_args(self):
         spec = self.spec
-        args = ['-DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain_files/gcc.cmake',
-                '-DENABLE_SSH=%s' % ('+ssh' in spec)]
+        args = [
+            "-DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain_files/gcc.cmake",
+            "-DENABLE_SSH=%s" % ("+ssh" in spec),
+        ]
         return args
 
     def install(self, spec, prefix):
         with working_dir(self.build_directory):
             make(*self.install_targets)
             mkdirp(prefix.var.OCIBundleDir)
-            #self.install_runc(spec, prefix)
-            #self.install_tini(spec, prefix)
-            #if '+configure_installation' in spec:
-            #    self.configure_installation(spec, prefix)
 
-    @run_after('install')
+    @run_after("install")
     def build_perms_script(self):
-        #import subprocess
-        #self.configure_installation(spec, prefix)
-        #script_path = prefix + '/configure_installation.sh'
-        #subprocess.check_call(script_path)
-        sudo = which('sudo')
-        sudo(join_path(self.spec.prefix.bin, 'configure_installation.sh'))
+        script_sh = join_path(self.spec.prefix, "configure_installation.sh")
+        tty.warn(
+            """
+                To complete Sarus's configuration do:
 
-    #def install_runc(self, spec, prefix):
-    #    wget = which('wget')
-    #    runc_url = 'https://github.com/opencontainers/runc/releases/download/v1.0.2/runc.amd64'
-    #    runc_install_path = prefix.bin + '/runc.amd64'
-    #    wget('-O', runc_install_path, runc_url)
-    #    set_executable(runc_install_path)
+                1. Make sure `tini` and `tini-static` binaries are in PATH, for example do `spack load tini`.
+                2. Execute the script {} with root privileges.
 
-    #def install_tini(self, spec, prefix):
-    #    wget = which('wget')
-    #    tini_url = 'https://github.com/krallin/tini/releases/download/v0.18.0/tini-static-amd64'
-    #    tini_install_path = prefix.bin + '/tini-static-amd64'
-    #    wget('-O', tini_install_path, tini_url)
-    #    set_executable(tini_install_path)
+                The script generates a basic working configuration. For more details:
 
-    #def configure_installation(selfself, spec, prefix):
-    #    import subprocess
-    #    script_path = prefix + '/configure_installation.sh'
-    #    subprocess.check_call(script_path)
+                https://sarus.readthedocs.io/en/stable/config/basic_configuration.html
+            """.format(
+                script_sh
+            )
+        )
+
+        # sudo = which('sudo')
+        # sudo(script_sh)
