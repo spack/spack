247e07aef95a579f200389885933b4605cfed4a1
iarspider <iarspider@gmail.com>
2021-12-14 22:36:17 +0100

    Update package.py

--- var/spack/repos/builtin/packages/py-neurolab/package.py
+++ var/spack/repos/builtin/packages/py-neurolab/package.py
@@ -15,3 +15,4 @@ class PyNeurolab(PythonPackage):
     version('0.3.5', sha256='96ec311988383c63664f3325668f27c30561cf4349e3bc5420665c042a3b9191')
 
     depends_on('py-numpy', type=('build', 'run'))
+    # depends_on('py-setuptools', type='build')
