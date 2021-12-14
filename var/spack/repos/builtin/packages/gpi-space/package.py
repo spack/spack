49c594de718527d67ac1e804bfbd45ba1f75e85a
Max Zeyen <max.zeyen@itwm.fraunhofer.de>
2021-12-14 12:45:59 +0000

    gpi-space: fixing style errors

--- var/spack/repos/builtin/packages/gpi-space/package.py
+++ var/spack/repos/builtin/packages/gpi-space/package.py
@@ -3,8 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-import os
-
 from spack import *
 
 
@@ -30,54 +28,41 @@ class GpiSpace(CMakePackage):
     version('21.12',  sha256='51794e2b593b8d1dc7d6310e17744842919bf44205b2cb7a79de2f2bbac3352a')
     version('21.09',  sha256='7f3861c2bfec15a4da46378ea38b304e1462ed315cd315b81ab2c2a8ba50dd3e')
 
-    variant(
-        "network",
-        default="ethernet",
-        values=("infiniband", "ethernet"),
-        description="GPI-2 fabric to enable"
-    )
-    variant(
-        "monitor",
-        default=True,
-        description="Enables the gspc-monitor application for execution monitoring."
-    )
-    variant(
-        "build_type",
-        default="Release",
-        values=("Debug", "Release", "RelWithDebInfo", "MinSizeRel"),
-        description="CMake build type"
-    )
+    variant("network",
+            default="ethernet",
+            values=("infiniband", "ethernet"),
+            description="GPI-2 fabric to enable")
+    variant("monitor",
+            default=True,
+            description="""
+                        Enables the gspc-monitor application for execution monitoring.
+                        """)
+    variant("build_type",
+            default="Release",
+            values=("Debug", "Release", "RelWithDebInfo", "MinSizeRel"),
+            description="CMake build type")
 
     depends_on("cmake@3.15.0:",
-        type=("build", "run")
-    )
+               type=("build", "run"))
     depends_on("chrpath@0.13:",
-        type=("build", "run")
-    )
+               type=("build", "run"))
     depends_on("pkgconf",
-        type="build"
-    )
+               type="build")
     depends_on("boost@1.62.0:1.63.0 +coroutine +context cxxstd=14")
     depends_on("hwloc@1.10: +libudev ~shared ~libxml2")
     depends_on("libssh2@1.7: build_type=Release")
     depends_on("openssl@0.9:")
     depends_on("gpi-2@1.3.2:1.3.3 fabrics=infiniband",
-        when="network=infiniband"
-    )
+               when="network=infiniband")
     depends_on("gpi-2@1.3.2:1.3.3 fabrics=ethernet",
-        when="network=ethernet"
-    )
+               when="network=ethernet")
     depends_on("qt@5.9:5.15",
-        when="+monitor"
-    )
+               when="+monitor")
 
     def cmake_args(self):
-        spec = self.spec
-        args = [
-            self.define("FHG_ASSERT_MODE", False),
-            self.define("INSTALL_DO_NOT_BUNDLE", True),
-            self.define("BUILD_TESTING", False),
-            self.define_from_variant("GSPC_WITH_MONITOR_APP", "monitor")
-        ]
+        args = [self.define("FHG_ASSERT_MODE", False),
+                self.define("INSTALL_DO_NOT_BUNDLE", True),
+                self.define("BUILD_TESTING", False),
+                self.define_from_variant("GSPC_WITH_MONITOR_APP", "monitor")]
 
         return args
