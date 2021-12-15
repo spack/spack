cfffc555c501ab5fcdb9efbcaac7ad031ff72388
mohan babu <mohbabul@amd.com>
2021-12-15 22:33:23 +0530

    addressing the review comments

--- var/spack/repos/builtin/packages/openfoam/package.py
+++ var/spack/repos/builtin/packages/openfoam/package.py
@@ -559,9 +559,13 @@ class Openfoam(Package):
             filter_file(r'trapFpe\s+\d+\s*;', 'trapFpe 0;',
                         controlDict, backup=False)
 
-    @when('%aocc@3.2.0')
+    @when('@:2106 %aocc@3.2.0')
     @run_before('configure')
     def make_amd_rules(self):
+        """Due to the change in the linker behavior in AOCC v3.2, it is now
+        issuing diagnostic messages for the unreferenced symbols in the
+        shared objects as it may lead to run time failures.
+        """
         general_rules = 'wmake/rules/General'
         src = join_path(general_rules, 'Clang')
         filter_file('clang++', spack_cxx + ' -pthread', join_path(src, 'c++'),
