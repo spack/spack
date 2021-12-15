60b4cae2ef9798c278a420f00fe3202024d1c361
egreen <45573926+egreen77@users.noreply.github.com>
2021-12-15 14:42:54 -0800

    Add umap smoke test

--- var/spack/repos/builtin/packages/umap/package.py
+++ var/spack/repos/builtin/packages/umap/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.error import UnsupportedPlatformError
+import os
 
 class Umap(CMakePackage):
     """Umap is a library that provides an mmap()-like interface to a
@@ -18,10 +19,15 @@ class Umap(CMakePackage):
 
     maintainers = ['egreen77', 'ibpeng', 'mayagokhale']
 
+    test_requires_compiler = True
+
     version('develop', branch='develop')
     version('2.1.0', sha256='dfdc5b717aecdbfbb0da22e8567b9f2ffbc3607000a31122bf7c5ab3b85cecd9')
     version('2.0.0', sha256='85c4bc68e8790393847a84eb54eaf6fc321acade382a399a2679d541b0e34150')
     version('1.0.0', sha256='c746de3fae5bfc5bbf36234d5e888ea45eeba374c26cd8b5a817d0c08e454ed5')
+
+    # C++11 support is required for umap
+    depends_on('gcc@4.8.5:', type=('test'))
 
     variant('logging', default=False, description='Build with logging enabled.')
     variant('tests', default=False, description='Build examples/tests.')
@@ -39,3 +45,18 @@ class Umap(CMakePackage):
             self.define_from_variant('ENABLE_DISPLAY_STATS', 'stats')
         ]
         return args
+
+    def test(self):
+        test_path = self.test_suite.current_test_data_dir
+        include_path = self.prefix.join("include/")
+        lib_path = self.prefix.join("lib/")
+        test_src = test_path.join("umaptest.cpp")
+        test_exe = test_path.join("umaptest.out")
+        test_out = [r"R[OW] test passed."]
+
+        cxx = os.environ['CXX']
+        compile_opts = ['-std=c++11', '-L', lib_path, '-I', include_path, '-lumap', '-o', test_exe, test_src]
+
+        compiled = self.run_test(cxx, options=compile_opts, expected="", purpose="Compiling test")
+        if compiled:
+            self.run_test(test_exe, expected=test_out)
