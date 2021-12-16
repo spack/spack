38391345053199586bbc26a39c33977b8b907f22
Jen Herting <jen@herting.cc>
2021-12-16 12:01:12 -0500

    [py-pynisher]
    - Added homepage
    - Added description
    - Removed fixmes
    - added dependencies

--- var/spack/repos/builtin/packages/py-pynisher/package.py
+++ var/spack/repos/builtin/packages/py-pynisher/package.py
@@ -3,48 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-pynisher
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-pynisher
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyPynisher(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """A small Python library to limit the resources used by a
+    function by executing it inside a subprocess."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/automl/pynisher"
     pypi     = "pynisher/pynisher-0.6.4.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.6.4', sha256='111d91aad471375c0509a912415ff90053ef909100facf412511383af107c124')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('python@3.6:',   type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-psutil',     type=('build', 'run'))
