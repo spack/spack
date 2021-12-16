25d67c42fe017e4881b92581f0905fada15d86fa
Jen Herting <jen@herting.cc>
2021-12-16 11:29:54 -0500

    [py-liac-arff]
    - added dependencies
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-liac-arff/package.py
+++ var/spack/repos/builtin/packages/py-liac-arff/package.py
@@ -3,48 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-liac-arff
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-liac-arff
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyLiacArff(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """The liac-arff module implements functions to read and
+    write ARFF files in Python."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/renatopp/liac-arff"
     pypi     = "liac-arff/liac-arff-2.5.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('2.5.0', sha256='3220d0af6487c5aa71b47579be7ad1d94f3849ff1e224af3bf05ad49a0b5c4da')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('python@2.7:2.999,3.5:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-cython', type='build')
