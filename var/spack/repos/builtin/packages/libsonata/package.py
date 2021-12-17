518a486c1faa4e073c5e112257038010a1dfdd7f
MikeG <michael.gevaert@epfl.ch>
2021-12-17 16:09:53 +0100

    update libsonata to release v0.1.10 (#1397)

--- var/spack/repos/builtin/packages/libsonata/package.py
+++ var/spack/repos/builtin/packages/libsonata/package.py
@@ -17,6 +17,7 @@ class Libsonata(CMakePackage):
     git = "https://github.com/BlueBrain/libsonata.git"
 
     version('develop', branch='master', submodules=False, get_full_repo=True)
+    version('0.1.10', submodules=False, get_full_repo=True)
     # Important: v0.1.9 contains an external dependency that cannot be easily
     # resolved by Spack. Hence, the temp. versions v0.1.9.YYYYMMDD add fixes,
     # which will be included in future official releases (v0.1.10 and above).
