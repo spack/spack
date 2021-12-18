733ff0e9c394e1c75cb46c48f484933bd53ae1d4
Adam J. Stewart <ajstewart426@gmail.com>
2021-12-17 18:32:17 -0600

    Use wheels for pip/wheel

--- var/spack/repos/builtin/packages/py-pip/package.py
+++ var/spack/repos/builtin/packages/py-pip/package.py
@@ -12,18 +12,18 @@ class PyPip(Package):
     """The PyPA recommended tool for installing Python packages."""
 
     homepage = "https://pip.pypa.io/"
-    url = "https://files.pythonhosted.org/packages/source/p/pip/pip-20.2.tar.gz"
+    url = "https://files.pythonhosted.org/packages/py3/p/pip/pip-20.2-py3-none-any.whl"
     list_url = "https://pypi.org/simple/pip/"
 
-    version('21.3.1', sha256='fd11ba3d0fdb4c07fbc5ecbba0b1b719809420f25038f8ee3cd913d3faa3033a')
-    version('21.1.2', sha256='eb5df6b9ab0af50fe1098a52fd439b04730b6e066887ff7497357b9ebd19f79b')
-    version('20.2',   sha256='912935eb20ea6a3b5ed5810dde9754fde5563f5ca9be44a8a6e5da806ade970b')
-    version('19.3',   sha256='324d234b8f6124846b4e390df255cacbe09ce22791c3b714aa1ea6e44a4f2861')
-    version('19.1.1', sha256='44d3d7d3d30a1eb65c7e5ff1173cdf8f7467850605ac7cc3707b6064bddd0958')
-    version('19.0.3', sha256='6e6f197a1abfb45118dbb878b5c859a0edbdd33fd250100bc015b67fded4b9f2')
-    version('18.1',   sha256='c0a292bd977ef590379a3f05d7b7f65135487b67470f6281289a94e015650ea1')
-    version('10.0.1', sha256='f2bd08e0cd1b06e10218feaf6fef299f473ba706582eb3bd9d52203fdbd7ee68')
-    version('9.0.1',  sha256='09f243e1a7b461f654c26a725fa373211bb7ff17a9300058b205c61658ca940d')
+    version('21.3.1', sha256='deaf32dcd9ab821e359cd8330786bcd077604b5c5730c0b096eda46f95c24a2d', expand=False)
+    version('21.1.2', sha256='f8ea1baa693b61c8ad1c1d8715e59ab2b93cd3c4769bacab84afcc4279e7a70e', expand=False)
+    version('20.2',   sha256='d75f1fc98262dabf74656245c509213a5d0f52137e40e8f8ed5cc256ddd02923', expand=False)
+    version('19.3',   sha256='e100a7eccf085f0720b4478d3bb838e1c179b1e128ec01c0403f84e86e0e2dfb', expand=False)
+    version('19.1.1', sha256='993134f0475471b91452ca029d4390dc8f298ac63a712814f101cd1b6db46676', expand=False)
+    version('19.0.3', sha256='bd812612bbd8ba84159d9ddc0266b7fbce712fc9bc98c82dee5750546ec8ec64', expand=False)
+    version('18.1',   sha256='7909d0a0932e88ea53a7014dfd14522ffef91a464daaaf5c573343852ef98550', expand=False)
+    version('10.0.1', sha256='717cdffb2833be8409433a93746744b59505f42146e8d37de6c62b430e25d6d7', expand=False)
+    version('9.0.1',  sha256='690b762c0a8460c303c089d5d0be034fb15a5ea2b75bdf565f40421f542fefb0', expand=False)
 
     extends('python')
     depends_on('python@3.6:', when='@21:', type=('build', 'run'))
@@ -32,79 +32,21 @@ class PyPip(Package):
     depends_on('python@2.7:2.8,3.3:', when='@10:', type=('build', 'run'))
     depends_on('python@2.6:2.8,3.3:', type=('build', 'run'))
 
-    # This bootstrapping procedure is similar to the one used by Nix:
-    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/python-modules/bootstrapped-pip/default.nix
-    # Also see further discussion in:
-    # https://discuss.python.org/t/bootstrapping-a-specific-version-of-pip/12306
-
-    # Latest version, should be updated from time to time
-    resource(
-        name='setuptools',
-        url='https://pypi.io/packages/source/s/setuptools/setuptools-59.4.0.tar.gz',
-        sha256='b4c634615a0cf5b02cf83c7bedffc8da0ca439f00e79452699454da6fbd4153d',
-        placement='setuptools',
-        when='^python@3.6:',
-    )
-    # Latest version that supports Python 3.5
-    resource(
-        name='setuptools',
-        url='https://pypi.io/packages/source/s/setuptools/setuptools-50.3.2.zip',
-        sha256='ed0519d27a243843b05d82a5e9d01b0b083d9934eaa3d02779a23da18077bd3c',
-        placement='setuptools',
-        when='^python@3.5',
-    )
-    # Latest version that supports Python 2.7
-    resource(
-        name='setuptools',
-        url='https://pypi.io/packages/source/s/setuptools/setuptools-44.1.1.zip',
-        sha256='c67aa55db532a0dadc4d2e20ba9961cbd3ccc84d544e9029699822542b5a476b',
-        placement='setuptools',
-        when='^python@2.7',
-    )
-    # Latest version that supports Python 3.4
-    resource(
-        name='setuptools',
-        url='https://pypi.io/packages/source/s/setuptools/setuptools-43.0.0.zip',
-        sha256='db45ebb4a4b3b95ff0aca3ce5fe1e820ce17be393caf8902c78aa36240e8c378',
-        placement='setuptools',
-        when='^python@3.4',
-    )
-
-    # Latest version, should be updated from time to time
-    resource(
-        name='wheel',
-        url='https://files.pythonhosted.org/packages/source/w/wheel/wheel-0.37.0.tar.gz',
-        sha256='e2ef7239991699e3355d54f8e968a21bb940a1dbf34a4d226741e64462516fad',
-        placement='wheel',
-        when='^python@2.7,3.5:',
-    )
-    # Latest version that supports Python 3.4
-    resource(
-        name='wheel',
-        url='https://files.pythonhosted.org/packages/source/w/wheel/wheel-0.33.6.tar.gz',
-        sha256='10c9da68765315ed98850f8e048347c3eb06dd81822dc2ab1d4fde9dc9702646',
-        placement='wheel',
-        when='^python@3.4',
-    )
-
-    conflicts(
-        '^python@:2.6,3.0:3.3',
-        msg='An older version of setuptools/wheel is required to bootstrap '
-        'this package, use a newer Python or contact @adamjstewart to fix this'
-    )
-
-    def setup_build_environment(self, env):
-        # Use pip to bootstrap itself
-        env.prepend_path('PYTHONPATH', 'src')
-
-        # Setuptools and wheel are required to build pip from source
-        env.prepend_path('PYTHONPATH', join_path(self.stage.source_path, 'setuptools'))
-        env.prepend_path(
-            'PYTHONPATH', join_path(self.stage.source_path, 'wheel', 'src')
-        )
+    def url_for_version(self, version):
+        url = "https://files.pythonhosted.org/packages/{0}/p/pip/pip-{1}-{0}-none-any.whl"
+        if version >= Version('21'):
+            python_tag = 'py3'
+        else:
+            python_tag = 'py2.py3'
+        return url.format(python_tag, version)
 
     def install(self, spec, prefix):
-        args = ['-m', 'pip'] + std_pip_args + ['--prefix=' + prefix, '.']
+        # To build and install pip from source, you need setuptools, wheel, and pip
+        # already installed. We get around this by using a pre-built wheel to install
+        # itself, see:
+        # https://discuss.python.org/t/bootstrapping-a-specific-version-of-pip/12306
+        whl = self.stage.archive_file
+        args = [os.path.join(whl, 'pip')] + std_pip_args + ['--prefix=' + prefix, whl]
         python(*args)
 
     @property
