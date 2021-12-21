4ba52c7d7dd33c605cbc3889e3bbd424ef98fb9b
Todd Gamblin <tgamblin@llnl.gov>
2021-12-21 15:53:38 -0800

    unparse: Make unparsing consistent for 2.7 and 3.5-3.10
    Previously, there were differences in the unparsed code for Python 2.7 and for 3.5-3.10.
    This makes unparsed code the same across these Python versions by:
    
        1. Ensuring there are no spaces between unary operators and
           their operands.
        2. Ensuring that *args and **kwargs are always the last arguments,
           regardless of the python version.
        3. Always unparsing print as a function.
        4. Not putting an extra comma after Python 2 class definitions.
    
    Without these changes, the same source can generate different code for different
    Python versions, depending on subtle AST differences.
    
    One place where single source will generate an inconsistent AST is with
    multi-argument print statements, e.g.:
    
    ```
        print("foo", "bar", "baz")
    ```
    
    In Python 2, this prints a tuple; in Python 3, it is the print function with
    multiple arguments.  Use `from __future__ import print_function` to avoid
    this inconsistency.

--- lib/spack/external/spack_astunparse/__init__.py
+++ lib/spack/external/spack_astunparse/__init__.py
@@ -7,7 +7,7 @@ from .unparser import Unparser
 __version__ = '1.6.3'
 
 
-def unparse(tree):
+def unparse(tree, py_ver_consistent=False):
     v = cStringIO()
-    Unparser(tree, file=v)
+    Unparser(tree, file=v, py_ver_consistent=py_ver_consistent)
     return v.getvalue()
