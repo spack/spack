34873f5fe72ca244df63307b4a19efea42f2dac3
Joseph Snyder <joe.snyder@kitware.com>
2021-12-22 10:15:49 -0500

    Use consistent method of checking for presence of info in connection (#27694)
    Fixes #27652
    
    Ensure that mirror's to_dict function returns a syaml_dict object for all code
    paths.
    
    Switch to using the .get function for accessing the potential information from
    the S3 mirror objects.  If the key is not there, it will gracefully return
    None instead of failing with a KeyError
    
    Additionally, check that the connection object is a dictionary before trying
    to "get" from it.
    
    Add a test for the capturing of the new S3 information.

--- lib/spack/spack/test/web.py
+++ lib/spack/spack/test/web.py
@@ -246,6 +246,29 @@ class MockS3Client(object):
         raise self.ClientError
 
 
+def test_gather_s3_information(monkeypatch, capfd):
+    mock_connection_data = {"access_token": "AAAAAAA",
+                            "profile": "SPacKDeV",
+                            "access_pair": ("SPA", "CK"),
+                            "endpoint_url": "https://127.0.0.1:8888"}
+
+    session_args, client_args = spack.util.s3.get_mirror_s3_connection_info(mock_connection_data)  # noqa: E501
+
+    # Session args are used to create the S3 Session object
+    assert "aws_session_token" in session_args
+    assert session_args.get("aws_session_token") == "AAAAAAA"
+    assert "aws_access_key_id" in session_args
+    assert session_args.get("aws_access_key_id") == "SPA"
+    assert "aws_secret_access_key" in session_args
+    assert session_args.get("aws_secret_access_key") == "CK"
+    assert "profile_name" in session_args
+    assert session_args.get("profile_name") == "SPacKDeV"
+
+    # In addition to the session object, use the client_args to create the s3
+    # Client object
+    assert "endpoint_url" in client_args
+
+
 def test_remove_s3_url(monkeypatch, capfd):
     fake_s3_url = 's3://my-bucket/subdirectory/mirror'
 
