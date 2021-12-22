af1351abf209ca2b682057a3efe465f34cf3c1cb
Matthieu Dorier <mdorier@anl.gov>
2021-12-22 14:13:35 +0000

    [py-aiohttp] added new version

--- var/spack/repos/builtin/packages/py-aiohttp/package.py
+++ var/spack/repos/builtin/packages/py-aiohttp/package.py
@@ -13,7 +13,13 @@ class PyAiohttp(PythonPackage):
     homepage = "https://github.com/aio-libs/aiohttp"
     url      = "https://github.com/aio-libs/aiohttp/archive/v3.6.2.tar.gz"
 
+    version('3.8.1', sha256='45210606552ed4f8992e17ffa3273e9ad2815c8c3ca00832a57b3a62735b1fc7')
+    version('3.8.0', sha256='0587288bf063cc0433542ab17674af11cb3639f4cb5ddf89420501ca16565e75')
     version('3.7.4', sha256='d90ace66b55747e49531b13bf0a9c7ae9d1e14f315a56ea6b54c0dc1b6facd6e')
+    version('3.7.3', sha256='2caac02447ff5d5b2db30a828fe676e90bb01c384e7751392784b68b7c7fcc18')
+    version('3.7.2', sha256='22ed7395d53c03f0f4872e5cdd051330bbce33f3d81b0af9aefd48a7077db040')
+    version('3.7.1', sha256='6075f4fb46cfb73f9282709272ec4755967668c633b6276433f4bad5b6db1b2b')
+    version('3.7.0', sha256='4549b9f1857dc2f8ba4028d8ba697eb7d7e5db7803fc7eee232f07027551ea26')
     version('3.6.2', sha256='4ad2d8393843ea0c7b50a93bf04fb1cf20b93c0a5e958de128efcd2e5f8adf80')
 
     depends_on('py-setuptools', type='build')
