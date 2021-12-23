1b4f14129ccc58cb06901b67c1daa76237462f48
Jen Herting <jen@herting.cc>
2021-12-23 10:55:13 -0500

    [py-fastcore] depends on setuptools

--- var/spack/repos/builtin/packages/py-fastcore/package.py
+++ var/spack/repos/builtin/packages/py-fastcore/package.py
@@ -24,5 +24,6 @@ class PyFastcore(PythonPackage):
     version('1.3.27', sha256='0161f538d5b913932869a46bb90e98193eee79b8798b566272a394f7ef957243')
 
     depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
     depends_on('py-pip', type='build')
     depends_on('py-packaging', type='build')
