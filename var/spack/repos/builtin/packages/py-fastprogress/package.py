88dc637e769a897e0b9bdff5dbc05651d37121dc
Jen Herting <jen@herting.cc>
2021-12-23 11:01:52 -0500

    [py-fastprogress]
    - added dependencies
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-fastprogress/package.py
+++ var/spack/repos/builtin/packages/py-fastprogress/package.py
@@ -3,48 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-fastprogress
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-fastprogress
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyFastprogress(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """A fast and simple progress bar for Jupyter Notebook and
+    console. Created by Sylvain Gugger for fast.ai."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/fastai/fastprogress"
     pypi     = "fastprogress/fastprogress-1.0.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.0.0', sha256='89e28ac1d2a5412aab18ee3f3dfd1ee8b5c1f2f7a44d0add0d0d4f69f0191bfe')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-numpy', type=('build', 'run'))
