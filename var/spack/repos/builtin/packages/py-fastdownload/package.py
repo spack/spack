a8adde43de3f3f59840f5ff4e8997f5b2873114a
Jen Herting <jen@herting.cc>
2021-12-23 10:52:55 -0500

    [py-fastdownload]
    - added homepage
    - added description
    - added dependencies
    - removed fixmes

--- var/spack/repos/builtin/packages/py-fastdownload/package.py
+++ var/spack/repos/builtin/packages/py-fastdownload/package.py
@@ -3,48 +3,21 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-fastdownload
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-fastdownload
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyFastdownload(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """If you have datasets or other archives that you want to
+    make available to your users, and ensure they always have
+    the latest versions and that they are downloaded correctly,
+    fastdownload can help."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/fastai/fastdownload/tree/master/"
     pypi     = "fastdownload/fastdownload-0.0.5.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.0.5', sha256='64e67af30690fa98ae1c8a1b52495769842f723565239a5430208ad05585af18')
 
-    # FIXME: Add dependencies if required. Only add the python dependency
-    # if you need specific versions. A generic python dependency is
-    # added implicity by the PythonPackage class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-foo',        type=('build', 'run'))
-
-    def build_args(self, spec, prefix):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-fastprogress', type=('build', 'run'))
+    depends_on('py-fastcore@1.3.26:', type=('build', 'run'))
