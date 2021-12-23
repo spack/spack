1c3881b48ab4c38e29cf166ae897a753713128fe
Axel Huebl <axel.huebl@plasma.ninja>
2021-12-23 12:11:50 -0800

    HDF5 package: change 1.10.8/1.13.0 CMake Path (#28103)
    These versions change the install location of CMake files used
    by dependents, but most dependents don't seem to look in this
    new location.
    
    Co-authored-by: Harmen Stoppels <harmenstoppels@gmail.com>
    Co-authored-by: Chris White <white238@llnl.gov>

--- var/spack/repos/builtin/packages/hdf5/package.py
+++ var/spack/repos/builtin/packages/hdf5/package.py
@@ -381,6 +381,11 @@ class Hdf5(CMakePackage):
                 args.append(self.define('CMAKE_Fortran_COMPILER',
                                         spec['mpi'].mpifc))
 
+        # work-around for https://github.com/HDFGroup/hdf5/issues/1320
+        if spec.satisfies('@1.10.8,1.13.0'):
+            args.append(self.define('HDF5_INSTALL_CMAKE_DIR',
+                                    'share/cmake/hdf5'))
+
         return args
 
     @run_after('install')
