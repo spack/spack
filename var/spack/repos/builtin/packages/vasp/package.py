60934ccd9029cc097dc23c000866c113317f9ace
mohan babu <mohbabul@amd.com>
2021-12-23 11:03:02 +0530

    adding period (.) in regex line no:138

--- var/spack/repos/builtin/packages/vasp/package.py
+++ var/spack/repos/builtin/packages/vasp/package.py
@@ -135,7 +135,7 @@ class Vasp(MakefilePackage):
         filter_file('^FFTW[ ]{0,}?=.*$', 'FFTW ?=', 'makefile.include')
         filter_file('^MPI_INC[ ]{0,}=.*$', 'MPI_INC ?=', 'makefile.include')
         filter_file('-DscaLAPACK.*$\n', '', 'makefile.include')
-        filter_file('^SCALAPACK*$', '', 'makefile.include')
+        filter_file('^SCALAPACK.*$', '', 'makefile.include')
 
         if '+cuda' in spec:
             filter_file('^OBJECTS_GPU[ ]{0,}=.*$',
