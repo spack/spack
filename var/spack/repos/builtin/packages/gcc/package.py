f097c3f5f1a56640e3a0780be4f6565656a45521
Glenn Johnson <glenn-johnson@uiowa.edu>
2021-12-24 23:34:35 -0600

    fix typo in comment

--- var/spack/repos/builtin/packages/gcc/package.py
+++ var/spack/repos/builtin/packages/gcc/package.py
@@ -292,7 +292,7 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage):
     patch('sys_ustat.h.patch', when='@5.0:6.4,7.0:7.3,8.1')
     patch('sys_ustat-4.9.patch', when='@4.9')
 
-    # this patch removes cylades support from gcc-5 and allows gg-5 to be built
+    # this patch removes cylades support from gcc-5 and allows gcc-5 to be built
     # with newer glibc versions.
     patch('glibc-2.31-libsanitizer-3-gcc-5.patch', when='@5.3.0:5.5.0')
 
