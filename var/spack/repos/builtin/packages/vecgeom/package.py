21441cd2356b39ebd7f1ca09f11eee45985f6021
Seth R Johnson <johnsonsr@ornl.gov>
2021-12-26 17:17:51 -0600

    vecgeom: require exact version of veccore
    Fixes configure error from downstream package:
    ```
    CMake Error at /rnsdhpc/code/spack/opt/spack/apple-clang/cmake/7zgbrwt/share/cmake-3.22/Modules/CMakeFindDependencyMacro.cmake:47 (find_package):
      Could not find a configuration file for package "VecCore" that is
      compatible with requested version "0.8.0".
    
      The following configuration files were considered but not accepted:
    
        /rnsdhpc/code/spack/var/spack/environments/celeritas/.spack-env/view/lib/cmake/VecCore/VecCoreConfig.cmake, version: 0.6.0
    ```

--- var/spack/repos/builtin/packages/vecgeom/package.py
+++ var/spack/repos/builtin/packages/vecgeom/package.py
@@ -50,6 +50,7 @@ class Vecgeom(CMakePackage, CudaPackage):
     variant('shared', default=True,
             description='Build shared libraries')
 
+    depends_on('veccore@0.8.0', type=('build', 'link'), when='@1.1.18')
     depends_on('veccore@0.5.2:', type=('build', 'link'), when='@1.1.0:')
     depends_on('veccore@0.4.2', type=('build', 'link'), when='@:1.0')
     depends_on('veccore+cuda', type=('build', 'link'), when='+cuda')
