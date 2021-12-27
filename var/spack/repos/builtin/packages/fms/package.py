26644482274331fd82624429a2a6b076b48be2a6
Dom Heinzeller <dom.heinzeller@icloud.com>
2021-12-27 08:08:03 -0700

    For FMS, need to be able to determine gfortran version when clang/apple-clang is used. Also need to be able to turn off quad precision on Apple's M1 chips.

--- var/spack/repos/builtin/packages/fms/package.py
+++ var/spack/repos/builtin/packages/fms/package.py
@@ -20,6 +20,7 @@ class Fms(CMakePackage):
     variant('64bit', default=True, description='64 bit?')
     variant('gfs_phys', default=True, description='Use GFS Physics?')
     variant('openmp', default=True, description='Use OpenMP?')
+    variant('enable_quad_precision', default=True, description='Enable quad precision?')
 
     version('2021.03.01', sha256='1f70e2a57f0d01e80fceb9ca9ce9661f5c1565d0437ab67618c2c4dfea0da6e9')
 
@@ -30,11 +31,17 @@ class Fms(CMakePackage):
         args = [
             self.define_from_variant('64BIT'),
             self.define_from_variant('GFS_PHYS'),
-            self.define_from_variant('OPENMP')
+            self.define_from_variant('OPENMP'),
+            self.define_from_variant('ENABLE_QUAD_PRECISION')
         ]
 
         args.append(self.define('CMAKE_C_COMPILER', self.spec['mpi'].mpicc))
         args.append(self.define('CMAKE_CXX_COMPILER', self.spec['mpi'].mpicxx))
         args.append(self.define('CMAKE_Fortran_COMPILER', self.spec['mpi'].mpifc))
+
+        if self.compiler.name in ['gcc', 'clang', 'apple-clang']:
+            gfortran_major_version = int(spack.compiler.get_compiler_version_output(self.compiler.fc, '-dumpversion').split('.')[0])
+            if gfortran_major_version>=10:
+                args.append(self.define('CMAKE_Fortran_FLAGS', '-fallow-argument-mismatch'))
 
         return args
