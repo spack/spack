ac8299962dd80656bba95245a5c870d55c3b9119
Matthieu Dorier <mdorier@anl.gov>
2021-12-30 09:34:24 +0000

    [py-gpustat] added dependency on python 3.4+

--- var/spack/repos/builtin/packages/py-gpustat/package.py
+++ var/spack/repos/builtin/packages/py-gpustat/package.py
@@ -14,6 +14,7 @@ class PyGpustat(PythonPackage):
     version('0.6.0', sha256='f69135080b2668b662822633312c2180002c10111597af9631bb02e042755b6c',
             preferred=True)
 
+    depends_on('python@3.4:', when='@1.0.0:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-pytest-runner', type='build')
     depends_on('py-six@1.7:', type=('build', 'run'))
