baeb1a14d5cfd68188ff0f0e0397eaa3d042bb57
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-01-01 21:57:01 -0600

    Update var/spack/repos/builtin/packages/py-tensorflow/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-tensorflow/package.py
+++ var/spack/repos/builtin/packages/py-tensorflow/package.py
@@ -160,7 +160,7 @@ class PyTensorflow(Package, CudaPackage):
     depends_on('py-clang@5.0.0:5.0', type=('build', 'run'), when='@2.6')
     depends_on('py-enum34@1.1.6:', type=('build', 'run'), when='@1.5: ^python@:3.3')
     depends_on('py-enum34@1.1.6:', type=('build', 'run'), when='@1.4.0:1.4.1')
-    depends_on('py-gast@0.2.1:0.5.0', type=('build', 'run'), when='@2.7:')
+    depends_on('py-gast@0.2.1:0.4', type=('build', 'run'), when='@2.7:')
     depends_on('py-gast@0.4.0', type=('build', 'run'), when='@2.5:2.6')
     depends_on('py-gast@0.3.3', type=('build', 'run'), when='@2.2:2.4')
     depends_on('py-gast@0.2.2', type=('build', 'run'), when='@1.15:2.1')
