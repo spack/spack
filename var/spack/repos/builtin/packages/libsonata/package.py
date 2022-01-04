0cd1c704c8d18619c4f136f7a6423a7aded0b5d4
Sergio <sergio.rivasgomez@epfl.ch>
2022-01-04 14:48:41 +0100

    Fix for missing tag in libsonata v0.1.10 (#1414)

--- var/spack/repos/builtin/packages/libsonata/package.py
+++ var/spack/repos/builtin/packages/libsonata/package.py
@@ -17,7 +17,7 @@ class Libsonata(CMakePackage):
     git = "https://github.com/BlueBrain/libsonata.git"
 
     version('develop', branch='master', submodules=False, get_full_repo=True)
-    version('0.1.10', submodules=False, get_full_repo=True)
+    version('0.1.10', tag='v0.1.10', submodules=False, get_full_repo=True)
     # Important: v0.1.9 is not Spack-compatible (use v0.1.10 instead)
     # version('0.1.9', tag='v0.1.9', submodules=False, get_full_repo=True)
     version('0.1.8', tag='v0.1.8', submodules=False, get_full_repo=True)
