45f362ea023c7f4fc09fbae4a0d3a2bcb8466b70
Sid Pendelberry <sid@rit.edu>
2022-01-05 16:42:12 -0500

    finished package with git reference

--- var/spack/repos/builtin/packages/py-deepsig/package.py
+++ var/spack/repos/builtin/packages/py-deepsig/package.py
@@ -7,19 +7,22 @@ from spack import *
 
 
 class PyDeepsig(PythonPackage):
-    """"""
+    """deep-significance: Easy and Better Significance Testing for Deep Neural
+    Networks"""
 
     homepage = "https://github.com/Kaleidophon/deep-significance"
-    url      = "https://github.com/Kaleidophon/deep-significance/archive/refs/tags/v1.2.0.tar.gz"
+    pypi     = "deepsig/deepsig-1.2.0.tar.gz"
     git      = "https://github.com/Kaleidophon/deep-significance.git"
 
-    version('1.2.0', sha256='e9abfb33aad03dbf6b5351fa35f813f7a041edc0ef64c16ce537a65911903306')
+    # The tarball for pypi is missing files using github
+    version('1.2.0', url='https://github.com/Kaleidophon/deep-significance/archive/refs/tags/v1.2.0.zip', 
+            sha256='4fc5d572d1bec91849daacdbba5722bd216e8398c1a8dc362d7671ff5d16548f')
 
-    depends_on('python@3.5.3:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
-    depends_on('py-numpy@1.19.5', type=('build', 'run'))
-    depends_on('py-scipy@1.6.0', type=('build', 'run'))
-    depends_on('py-tqdm@4.59.0', type=('build', 'run'))
-    depends_on('py-joblib@1.0.1', type=('build', 'run'))
-    depends_on('py-pandas@1.3.3', type=('build', 'run'))
-    depends_on('py-dill@0.3.4', type=('build', 'run'))
+    depends_on('python@3.5.3:',         type=('build', 'run'))
+    depends_on('py-setuptools',         type='build')
+    depends_on('py-numpy@1.19.5',       type=('build', 'run'))
+    depends_on('py-scipy@1.6.0',        type=('build', 'run'))
+    depends_on('py-tqdm@4.59.0',        type=('build', 'run'))
+    depends_on('py-joblib@1.0.1',       type=('build', 'run'))
+    depends_on('py-pandas@1.3.3',       type=('build', 'run'))
+    depends_on('py-dill@0.3.4',         type=('build', 'run'))
