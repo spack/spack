cef3358eac204d8e698909c872a3b5cd1e37b29c
Adam J. Stewart <ajstewart426@gmail.com>
2022-01-07 12:37:37 -0600

    Use wheels for py-tomli to avoid circular dep on py-flit-core

--- var/spack/repos/builtin/packages/py-tomli/package.py
+++ var/spack/repos/builtin/packages/py-tomli/package.py
@@ -12,21 +12,10 @@ class PyTomli(PythonPackage):
     Tomli is fully compatible with TOML v1.0.0."""
 
     homepage = "https://github.com/hukkin/tomli"
-    pypi = "tomli/tomli-1.2.1.tar.gz"
+    url = "https://files.pythonhosted.org/packages/py3/t/tomli/tomli-1.2.2-py3-none-any.whl"
+    list_url = "https://pypi.org/simple/tomli/"
 
-    version('1.2.2', sha256='c6ce0015eb38820eaf32b5db832dbc26deb3dd427bd5f6556cf0acac2c214fee')
-    version('1.2.1', sha256='a5b75cb6f3968abb47af1b40c1819dc519ea82bcc065776a866e8d74c5ca9442')
+    version('1.2.2', sha256='f04066f68f5554911363063a30b108d2b5a5b1a010aa8b6132af78489fe3aade', expand=False)
+    version('1.2.1', sha256='8dd0e9524d6f386271a36b41dbf6c57d8e32fd96fd22b6584679dc569d20899f', expand=False)
 
     depends_on('python@3.6:', type=('build', 'run'))
-
-    resource(
-        name='flit-core',
-        url='https://files.pythonhosted.org/packages/source/f/flit-core/flit_core-3.5.1.tar.gz',
-        sha256='3083720351a6cb00e0634a1ec0e26eae7b273174c3c6c03d5b597a14203b282e',
-        placement='flit-core',
-    )
-
-    def setup_build_environment(self, env):
-        # tomli is built using flit-core, but flit-core has a run-time dep on tomli
-        env.prepend_path('PYTHONPATH', '.')
-        env.prepend_path('PYTHONPATH', join_path(self.stage.source_path, 'flit-core'))
