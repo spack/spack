b32c827b52b9e2d559bc96333b82596aa4fe692d
Brian C. Van Essen <vanessen1@llnl.gov>
2022-01-11 09:33:02 -0800

    Fixed Flake8

--- var/spack/repos/builtin/packages/hwloc/package.py
+++ var/spack/repos/builtin/packages/hwloc/package.py
@@ -2,8 +2,8 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-import sys
 import re
+import sys
 
 
 class Hwloc(AutotoolsPackage):
