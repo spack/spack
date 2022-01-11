a45d8deb9297e510931837fe3402f7eda3e80553
Teodor Nikolov <teodor.nikolov22@gmail.com>
2022-01-11 10:56:44 +0100

    Use release tarballs: checksums + better caching

--- var/spack/repos/builtin/packages/sarus/package.py
+++ var/spack/repos/builtin/packages/sarus/package.py
@@ -18,15 +18,15 @@ class Sarus(CMakePackage):
 
     version("develop", branch="develop")
     version("master", branch="master")
-    version("1.4.0", tag="1.4.0")
-    version("1.3.3", tag="1.3.3")
-    version("1.3.2", tag="1.3.2")
-    version("1.3.1", tag="1.3.1")
-    version("1.3.0", tag="1.3.0")
-    version("1.2.0", tag="1.2.0")
-    version("1.1.0", tag="1.1.0")
-    version("1.0.1", tag="1.0.1")
-    version("1.0.0", tag="1.0.0")
+    version("1.4.0", sha256="59cdb957bcf34b5f85999165986c0b45e9694facfb851f6504ec1fa1c7d77221")
+    version("1.3.3", sha256="792741601ef4b8aa60f55a698f7e08fd510786f636d42285ddcbe659a914fe66")
+    version("1.3.2", sha256="94d571c3eab7df195c17387818c6f652a6b2301a4e56081aaea74d4d19a4bd07")
+    version("1.3.1", sha256="7809f4ecf6d65cd5791b74cd296da407d68a2e6fbc319e1af9c0d33c2830d979")
+    version("1.3.0", sha256="3f11b3bd58c44d165762bff5e53b081e729486df98f4fe6fecde104e21eab131")
+    version("1.2.0", sha256="c5eca12e4cf96ab586401546d932afb33952d60b10048ff7382526a7ac8fe591")
+    version("1.1.0", sha256="79f3d3e0eef94c42a0356a012291afb42d79cc09f5d92dc241d9a8c743cfae83")
+    version("1.0.1", sha256="6422805a496a738f43f137291761ca21c1e46303b2eea0777f36d3dbcbc622fa")
+    version("1.0.0", sha256="b08f26b8249a064d6a45d935b96efc04e25070a03ac8002d0e427da0598d51fc")
 
     variant(
         "ssh",
