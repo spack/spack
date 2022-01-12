7d3a696d27abae89ff6d4e712bb4f4133be7039f
Adam J. Stewart <ajstewart426@gmail.com>
2022-01-12 13:47:21 -0600

    fenics: specify python exe (#28323)

--- var/spack/repos/builtin/packages/fenics/package.py
+++ var/spack/repos/builtin/packages/fenics/package.py
@@ -122,7 +122,7 @@ class Fenics(CMakePackage):
     depends_on('py-sphinx@1.0.1:', when='+doc', type='build')
 
     def cmake_args(self):
-        return [
+        args = [
             self.define_from_variant('BUILD_SHARED_LIBS', 'shared'),
             self.define('DOLFIN_SKIP_BUILD_TESTS', True),
             self.define_from_variant('DOLFIN_ENABLE_OPENMP', 'openmp'),
@@ -145,6 +145,12 @@ class Fenics(CMakePackage):
             self.define_from_variant('DOLFIN_ENABLE_VTK', 'vtk'),
             self.define_from_variant('DOLFIN_ENABLE_ZLIB', 'zlib'),
         ]
+
+        if '+python' in self.spec:
+            args.append(self.define(
+                'PYTHON_EXECUTABLE', self.spec['python'].command.path))
+
+        return args
 
     # set environment for bulding python interface
     def setup_build_environment(self, env):
