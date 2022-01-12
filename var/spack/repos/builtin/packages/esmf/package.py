2bf42221ae4cca250f5448bdbf6b2b89b54bf0a1
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-01-12 16:50:38 -0700

    Bug fixes in var/spack/repos/builtin/packages/esmf/package.py to build ESMF on macOS with clang+gfortran and on cray

--- var/spack/repos/builtin/packages/esmf/package.py
+++ var/spack/repos/builtin/packages/esmf/package.py
@@ -129,8 +129,9 @@ class Esmf(MakefilePackage):
             gfortran_major_version = int(spack.compiler.get_compiler_version_output(self.compiler.fc, '-dumpversion').split('.')[0])
         elif self.compiler.name == 'intel':
             os.environ['ESMF_COMPILER'] = 'intel'
-        elif self.compiler.name == 'clang':
+        elif self.compiler.name in ['clang', 'apple-clang']:
             os.environ['ESMF_COMPILER'] = 'gfortranclang'
+            gfortran_major_version = int(spack.compiler.get_compiler_version_output(self.compiler.fc, '-dumpversion').split('.')[0])
         elif self.compiler.name == 'nag':
             os.environ['ESMF_COMPILER'] = 'nag'
         elif self.compiler.name == 'pgi':
@@ -155,8 +156,16 @@ class Esmf(MakefilePackage):
             # Build an optimized version of the library.
             os.environ['ESMF_BOPT'] = 'O'
 
-        if self.spec.satisfies('%gcc@10:'):
+        if self.compiler.name in ['gcc', 'clang', 'apple-clang'] and gfortran_major_version>=10:
             os.environ['ESMF_F90COMPILEOPTS'] = '-fallow-argument-mismatch'
+
+        #######
+        # OS  #
+        #######
+
+        # ESMF_OS must be set for Cray systems
+        if 'platform=cray' in self.spec:
+            os.environ['ESMF_OS']='Unicos'
 
         #######
         # MPI #
@@ -277,3 +286,9 @@ class Esmf(MakefilePackage):
 
     def check(self):
         make('check', parallel=False)
+
+    def setup_dependent_build_environment(self, env, dependent_spec):
+        env.set('ESMFMKFILE', os.path.join(self.prefix.lib, 'esmf.mk'))
+
+    def setup_run_environment(self, env):
+        env.set('ESMFMKFILE', os.path.join(self.prefix.lib, 'esmf.mk'))
