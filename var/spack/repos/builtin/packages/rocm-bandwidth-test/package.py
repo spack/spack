819955288be705d4d8f06f5c282788c28cd545a1
Renjith Ravindran <Renjith.RavindranKannath@amd.com>
2022-01-11 21:05:59 -0800

    Update recipe for the version for 4.5.2 release

--- var/spack/repos/builtin/packages/rocm-bandwidth-test/package.py
+++ var/spack/repos/builtin/packages/rocm-bandwidth-test/package.py
@@ -17,6 +17,7 @@ class RocmBandwidthTest(CMakePackage):
     maintainers = ['srekolam', 'arjun-raj-kuppala']
 
     version('master', branch='master')
+    version('4.5.2', sha256='559ca7ef582d81047c5dd5a908f3989cb2694e89577f7f556214e324ba65e75e')
     version('4.5.0', sha256='4d20a6017ca6975df98f3ca61ba95fa0c7c62fbf63cd6abae3396c30a423933f')
     version('4.3.1', sha256='a4804c28586457c231594b4e7689872eaf91972119d892325468f3fe8fdbe5ef')
     version('4.3.0', sha256='c6eb406cd2836af61dd5987f6b761340a1be20f66a9325f480423d10b9d3ec1b')
@@ -34,7 +35,7 @@ class RocmBandwidthTest(CMakePackage):
     depends_on('cmake@3:', type='build')
 
     for ver in ['3.5.0', '3.7.0', '3.8.0', '3.9.0', '3.10.0', '4.0.0', '4.1.0',
-                '4.2.0', '4.3.0', '4.3.1', '4.5.0', 'master']:
+                '4.2.0', '4.3.0', '4.3.1', '4.5.0', '4.5.2', 'master']:
         depends_on('hsa-rocr-dev@' + ver, when='@' + ver)
         depends_on('hsakmt-roct@' + ver, when='@' + ver)
 
