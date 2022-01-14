e4cc816139f53e6add3f11f5d999d283210533f1
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2022-01-14 10:10:27 +0100

    Update voxcell and brainbuilder (#1429)

--- bluebrain/repo-bluebrain/packages/brainbuilder/package.py
+++ bluebrain/repo-bluebrain/packages/brainbuilder/package.py
@@ -13,20 +13,21 @@ class Brainbuilder(PythonPackage):
     git      = "git@bbpgitlab.epfl.ch:nse/brainbuilder.git"
 
     version('develop')
-    version('0.16.2', tag='brainbuilder-v0.16.2')
+    version('0.17.0', tag='brainbuilder-v0.17.0')
 
     depends_on('py-setuptools', type=('build', 'run'))
 
-    depends_on('py-click@7.0:7.999', type=('build', 'run'))
+    depends_on('py-click@7.0:8.999', type=('build', 'run'))
     depends_on('py-h5py@3.1.0:', type=('build', 'run'))
+    depends_on('py-jsonschema@3.2.0:', type=('build', 'run'))
     depends_on('py-lxml@3.3:', type=('build', 'run'))
     depends_on('py-numpy@1.9:', type=('build', 'run'))
     depends_on('py-pandas@1.0.0:', type=('build', 'run'))
     depends_on('py-scipy@0.13:', type=('build', 'run'))
     depends_on('py-tqdm@4.0:', type=('build', 'run'))
-    depends_on('py-bluepy@2.1:2.999', type=('build', 'run'))
+    depends_on('py-bluepy@2.1:', type=('build', 'run'))
     depends_on('py-libsonata@0.1.6:', type=('build', 'run'))
-    depends_on('py-voxcell@3.0.0:', type=('build', 'run'))
+    depends_on('py-voxcell@3.1.0:', type=('build', 'run'))
     depends_on('py-morphio@3.0.0:3.999', type=('build', 'run'))
 
     depends_on('py-attrs@:19.999', type=('build', 'run'))
