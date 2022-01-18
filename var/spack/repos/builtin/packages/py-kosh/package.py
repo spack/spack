d48cb476a69459b6ce809103b4c0df27c6ebabf9
Charles Doutriaux <doutriaux1@llnl.gov>
2022-01-18 15:41:33 -0800

    adressing @adamjstewart comments

--- var/spack/repos/builtin/packages/py-kosh/package.py
+++ var/spack/repos/builtin/packages/py-kosh/package.py
@@ -8,16 +8,17 @@ from spack import *
 
 
 class PyKosh(PythonPackage):
-    """Kosh allows codes to store, query, share data via an easy-to-use Python API.
-Kosh lies on top of Sina and as a result can use any database backend supported by Sina.
-In adition Kosh aims to make data access and sharing as simple as possible.
-"""
+    """i
+    Kosh allows codes to store, query, share data via an easy-to-use Python API.
+    Kosh lies on top of Sina and can use any database backend supported by Sina.
+    In adition Kosh aims to make data access and sharing as simple as possible.
+    """
 
     homepage = "https://github.com/LLNL/kosh"
     url = "https://github.com/LLNL/kosh/archive/refs/tags/v2.0.tar.gz"
 
     # notify when the package is updated.
-    maintainers = ['doutriaux1', ]
+    maintainers = ['doutriaux1']
 
     version('2.0', sha256='059e431e3d3219b53956cb464d9e10933ca141dc89662f55d9c633e35c8b3a1e')
 
