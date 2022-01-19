4d62c0fd337a1c998c9b3831b85e90f493ec34d4
Peter Brady <ptb@lanl.gov>
2022-01-19 09:08:06 -0700

    Move git to class level and remove redundant depends

--- var/spack/repos/builtin/packages/glm/package.py
+++ var/spack/repos/builtin/packages/glm/package.py
@@ -13,14 +13,14 @@ class Glm(CMakePackage):
 
     homepage = "https://github.com/g-truc/glm"
     url = "https://github.com/g-truc/glm/archive/0.9.9.8.tar.gz"
+    git = "https://github.com/g-truc/glm.git"
 
-    version('develop', git="https://github.com/g-truc/glm.git")
+    version('develop', branch="master")
     version('0.9.9.8', sha256='7d508ab72cb5d43227a3711420f06ff99b0a0cb63ee2f93631b162bfe1fe9592')
     version('0.9.7.1', sha256='285a0dc8f762b4e523c8710fbd97accaace0c61f45bc8be2bdb0deed07b0e6f3')
 
     depends_on('cmake@2.6:', type='build')
     depends_on('cmake@3.2:', type='build', when='@0.9.9.0:')
-    depends_on('cmake@3.2:', type='build', when='@develop')
 
     # CMake install target was removed in version 0.9.9.6
     @when('@0.9.9.6:0.9.9.8')
