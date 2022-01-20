aba2a9324e9f0414c2f0f3079872a13ca72e047e
Kayla Butler <butler59@llnl.gov>
2022-01-19 16:51:53 -0800

    Update package Copy files from share/Tasmanian to install_test_root Use run_test to generate, build, and run test Fix import style

--- var/spack/repos/builtin/packages/tasmanian/package.py
+++ var/spack/repos/builtin/packages/tasmanian/package.py
@@ -3,11 +3,11 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
+
 from llnl.util import tty
 
 from spack import *
-
-import os
 
 
 class Tasmanian(CMakePackage, CudaPackage, ROCmPackage):
@@ -163,32 +163,39 @@ class Tasmanian(CMakePackage, CudaPackage, ROCmPackage):
     def cmake_bin(self, set=True):
         """(Hack) Set/get cmake dependency path."""
         filepath = join_path(self.install_test_root, 'cmake_bin_path.txt')
-        if os.path.isfile(filepath):
-            if set:
-                mkdirp(self.install_test_root)
-                with open(filepath, 'w') as out_file:
-                    cmake_bin = join_path(self.spec['cmake'].prefix.bin, 'cmake')
-                    out_file.write('{0}\n'.format(cmake_bin))
-            else:
-                with open(filepath, 'r') as in_file:
-                    return in_file.read().strip()
-        else:
-            tty.msg('Skipping tasmanian test: cmake_bin_path.txt not found')
+        if set:
+            with open(filepath, 'w') as out_file:
+                cmake_bin = join_path(self.spec['cmake'].prefix.bin, 'cmake')
+                out_file.write('{0}\n'.format(cmake_bin))
+        elif os.path.isfile(filepath):
+            with open(filepath, 'r') as in_file:
+                return in_file.read().strip()
 
     @run_after('install')
     def setup_smoke_test(self):
-        if self.spec['cmake'].satisfies('@3.22:'):
-            self.cmake_bin(set=True)
+        if not self.spec['cmake'].satisfies('@3.22:'):
+            return
+
+        install_tree(self.prefix.share.Tasmanian.examples,
+                     join_path(self.install_test_root, 'examples'))
+        install_tree(self.prefix.share.Tasmanian.testing,
+                     join_path(self.install_test_root, 'testing'))
+        self.cmake_bin(set=True)
 
     def test(self):
         cmake_bin = self.cmake_bin(set=False)
 
-        self.run_test(cmake_bin,
-                      purpose='Build with same CMake version as install')
+        if not cmake_bin:
+            tty.msg('Skipping tasmanian test: cmake_bin_path.txt not found')
+            return
 
         # using the tests installed in <prefix>/share/Tasmanian/testing
-        cmake_dir = join_path(self.prefix, 'share', 'Tasmanian', 'testing')
-        with working_dir(self.test_suite.current_test_cache_dir, create=True):
-            cmake(cmake_dir)
-            make()
-            make('test')
+        cmake_dir = self.test_suite.current_test_cache_dir.testing
+        self.run_test(cmake_bin,
+                      options=[cmake_dir],
+                      purpose='Generate the Makefile')
+        self.run_test('make',
+                      purpose='Build test software')
+        self.run_test('make',
+                      options=['test'],
+                      purpose='Run test')
