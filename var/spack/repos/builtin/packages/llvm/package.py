ce902aaa40f640a0709b91f89f943c00bcd68b85
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-01-21 14:27:08 -0600

    llvm: patch for gcc-11
    - added llvm-gcc11.patch
    - adjusted version constraints on existing missing-includes patch.

--- var/spack/repos/builtin/packages/llvm/package.py
+++ var/spack/repos/builtin/packages/llvm/package.py
@@ -310,7 +310,7 @@ class Llvm(CMakePackage, CudaPackage):
     patch('sanitizer-ipc_perm_mode.patch', when="@5:9+compiler-rt%gcc@9:")
 
     # github.com/spack/spack/issues/24270: MicrosoftDemangle for %gcc@10: and %clang@13:
-    patch('missing-includes.patch', when='@8:9')
+    patch('missing-includes.patch', when='@8')
 
     # Backport from llvm master + additional fix
     # see  https://bugs.llvm.org/show_bug.cgi?id=39696
@@ -341,6 +341,9 @@ class Llvm(CMakePackage, CudaPackage):
     # https://reviews.llvm.org/D102059
     patch('no_cyclades.patch', when='@10:12.0.0')
     patch('no_cyclades9.patch', when='@6:9')
+
+    # patch to enable llvm to compile with gcc-11
+    patch('llvm-gcc11.patch', when='@9:11%gcc@11:')
 
     # The functions and attributes below implement external package
     # detection for LLVM. See:
