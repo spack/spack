34346a2f92adef533f2a7441b62ba80efd59bd94
Tracy-Pantleo <91685320+Tracy-Pantleo@users.noreply.github.com>
2022-01-24 11:45:34 -0700

    Update package.py

--- var/spack/repos/builtin/packages/jali/package.py
+++ var/spack/repos/builtin/packages/jali/package.py
@@ -18,20 +18,22 @@ class Jali(CMakePackage):
     maintainers = ['raovgarimella']
 
     version('master', branch='master')
+    version('1.1.6', sha256='a2f4e4f238c60ea78486e0c9ea5b3e2cdd9d91c2ae5ea006a1d33a12e9eafa3a')
     version('1.1.5', sha256='4f18f3e8b50f20a89918e99596a7226c215944d84df642bc1fb2d6c31464b95b')
-    version('1.1.4', sha256='135ab02be1487fcdfb039613cbed630bce336d581a66468c66209db0a9d8a104')
     version('1.1.1', sha256='c96c000b3893ea7f15bbc886524476dd466ae145e77deedc27e412fcc3541207')
     version('1.1.0', sha256='783dfcd6a9284af83bb380ed257fa8b0757dc2f7f9196d935eb974fb6523c644')
     version('1.0.5', sha256='979170615d33a7bf20c96bd4d0285e05a2bbd901164e377a8bccbd9af9463801')
 
     variant('mstk', default=True, description='Enable MSTK')
-
+   
     # dependencies
     depends_on('cmake@3.13:', type='build')
 
     depends_on('mpi')
 
-    depends_on('boost')
+    depends_on('boost', when='@:1.1.5')
+       
+
 
     depends_on('mstk@3.3.5: +exodusii+parallel~use_markers partitioner=all', when='+mstk')
 
