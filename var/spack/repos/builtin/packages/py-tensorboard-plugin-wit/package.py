fbf370d94185e2c0e0e8d6004cc484fbe31868bc
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-01-24 17:24:37 -0600

    py-tensorboard-plugin-wit
    - switch to PythonPackage
    - add version 1.8.1
    - remove unneeded code

--- var/spack/repos/builtin/packages/py-tensorboard-plugin-wit/package.py
+++ var/spack/repos/builtin/packages/py-tensorboard-plugin-wit/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class PyTensorboardPluginWit(Package):
+class PyTensorboardPluginWit(PythonPackage):
     """The What-If Tool makes it easy to efficiently and
        intuitively explore up to two models' performance
        on a dataset. Investigate model performances for
@@ -20,6 +20,9 @@ class PyTensorboardPluginWit(Package):
 
     maintainers = ['aweits']
 
+    version('1.8.1',
+            sha256='ff26bdd583d155aa951ee3b152b3d0cffae8005dc697f72b44a8e8c2a77a8cbe',
+            expand=False)
     version('1.8.0',
             sha256='2a80d1c551d741e99b2f197bb915d8a133e24adb8da1732b840041860f91183a',
             expand=False)
@@ -27,11 +30,4 @@ class PyTensorboardPluginWit(Package):
             sha256='ee775f04821185c90d9a0e9c56970ee43d7c41403beb6629385b39517129685b',
             expand=False)
 
-    extends('python')
-
     depends_on('python@3.2:', type=('build', 'run'))
-    depends_on('py-pip', type='build')
-
-    def install(self, spec, prefix):
-        pip = which('pip')
-        pip('install', '--prefix={0}'.format(prefix), self.stage.archive_file)
