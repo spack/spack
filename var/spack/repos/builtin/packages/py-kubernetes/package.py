b32ca866f260071052686de36dea4c04e3998134
Valentin Volkl <valentin.volkl@cern.ch>
2022-01-31 19:40:52 +0100

    py-kubernetes: add new versions up to v21.7.0

--- var/spack/repos/builtin/packages/py-kubernetes/package.py
+++ var/spack/repos/builtin/packages/py-kubernetes/package.py
@@ -16,6 +16,10 @@ class PyKubernetes(PythonPackage):
 
     maintainers = ['vvolkl']
 
+    version('21.7.0',    sha256='c9849afc2eafdce60efa210049ee7a94e7ef6cf3a7afa14a69b3bf0447825977')
+    version('20.13.0',   sha256='ce5e881c13dc56f21a243804f90bc3c507af93c380f505c00e392c823968e4de')
+    version('19.15.0',   sha256='08c93f300a9837104282ecc81458b903a56444c5c1ec3d990d237557312af47f')
+    version('18.20.0',   sha256='0c72d00e7883375bd39ae99758425f5e6cb86388417cf7cc84305c211b2192cf')
     version('17.17.0',   sha256='c69b318696ba797dcf63eb928a8d4370c52319f4140023c502d7dfdf2080eb79')
     version('12.0.1',    sha256='ec52ea01d52e2ec3da255992f7e859f3a76f2bdb51cf65ba8cd71dfc309d8daa')
     version('12.0.0',    sha256='72f095a1cd593401ff26b3b8d71749340394ca6d8413770ea28ce18efd5bcf4c')
@@ -28,7 +32,8 @@ class PyKubernetes(PythonPackage):
     depends_on('py-six@1.9.0:', type=('build', 'run'))
     depends_on('py-python-dateutil@2.5.3:', type=('build', 'run'))
     depends_on('py-setuptools@21.0.0:', type=('build'))
-    depends_on('py-pyyaml@3.12:', type=('build', 'run'))
+    depends_on('py-pyyaml@3.12:', when='@:18.19', type=('build', 'run'))
+    depends_on('py-pyyaml@5.4.1:', when='@18.20.0:', type=('build', 'run'))
     depends_on('py-google-auth@1.0.1:', type=('build', 'run'))
     depends_on('py-ipaddress@1.0.17:', when='^python@:2.8', type=('build', 'run'))
     depends_on('py-websocket-client@0.32:0.39,0.43:', type=('build', 'run'))
