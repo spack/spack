3b64ca30c779d5a8d333a1c71a61e5b28fd79f4b
liuyangzhuan <liuyangzhuan@gmail.com>
2022-01-31 11:21:06 -0800

    changing site_packages_dir to python_platlib for gptune (#28669)
    * added package gptune with all its dependencies: adding py-autotune, pygmo, py-pyaml, py-autotune, py-gpy, py-lhsmdu, py-hpbandster, pagmo2, py-opentuner; modifying superlu-dist, py-scikit-optimize
    
    * adding gptune package
    
    * minor fix for macos spack test
    
    * update patch for py-scikit-optimize; update test files for gptune
    
    * fixing gptune package style error
    
    * fixing unit tests
    
    * a few changes reviewed in the PR
    
    * improved gptune package.py with a few newly added/improved dependencies
    
    * fixed a few style errors
    
    * minor fix on package name py-pyro4
    
    * fixing more style errors
    
    * Update var/spack/repos/builtin/packages/py-scikit-optimize/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * resolved a few issues in the PR
    
    * fixing file permissions
    
    * a few minor changes
    
    * style correction
    
    * minor correction to jq package file
    
    * Update var/spack/repos/builtin/packages/py-pyro4/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * fixing a few issues in the PR
    
    * adding py-selectors34 required by py-pyro4
    
    * improved the superlu-dist package
    
    * improved the superlu-dist package
    
    * moree changes to gptune and py-selectors34 based on the PR
    
    * Update var/spack/repos/builtin/packages/py-selectors34/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * improved gptune package: 1. addressing comments of tldahlgren in PR 26936; 2. adding variant openmpi
    
    * fixing style issue of gptune
    
    * changing file mode
    
    * improved gptune package: add variant mpispawn which depends on openmpi; add variant superlu and hypre for installing the drivers; modified hypre package file to add a gptune variant
    
    * fixing style error
    
    * corrected pddrive_spawn path in gptune test; enforcing gcc>7
    
    * fixing style error
    
    * setting environment variables when loading gptune
    
    * removing debug print in hypre/package.py
    
    * adding superlu-dist v7.2.0; fixing an issue with CMAKE_INSTALL_LIBDIR
    
    * changing site_packages_dir to python_platlib
    
    * not using python3.9 for py-gpy, which causes due to dropped support of tp_print
    
    * more replacement of site_packages_dir
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/gptune/package.py
+++ var/spack/repos/builtin/packages/gptune/package.py
@@ -66,7 +66,7 @@ class Gptune(CMakePackage):
             fc_flags.append('-fallow-argument-mismatch')
 
         args = [
-            '-DGPTUNE_INSTALL_PATH=%s' % site_packages_dir,
+            '-DGPTUNE_INSTALL_PATH=%s' % python_platlib,
             '-DTPL_BLAS_LIBRARIES=%s' % spec['blas'].libs.joined(";"),
             '-DTPL_LAPACK_LIBRARIES=%s' % spec['lapack'].libs.joined(";"),
             '-DTPL_SCALAPACK_LIBRARIES=%s' % spec['scalapack'].
@@ -92,7 +92,7 @@ class Gptune(CMakePackage):
         self.cache_extra_test_sources([self.examples_src_dir])
 
     def setup_run_environment(self, env):
-        env.set('GPTUNE_INSTALL_PATH', site_packages_dir)
+        env.set('GPTUNE_INSTALL_PATH', python_platlib)
 
     def test(self):
         spec = self.spec
@@ -142,7 +142,7 @@ class Gptune(CMakePackage):
             envfile.write('export MPIRUN={0}\n'.format
                           (which(spec['mpi'].prefix.bin + '/mpirun')))
             envfile.write('export PYTHONPATH={0}:$PYTHONPATH\n'.format
-                          (site_packages_dir + '/gptune'))
+                          (python_platlib + '/gptune'))
             envfile.write('export proc=$(spack arch)\n')
             envfile.write('export mpi={0}\n'.format(spec['mpi'].name))
             envfile.write('export compiler={0}\n'.format(comp_name))
@@ -183,7 +183,7 @@ class Gptune(CMakePackage):
                           '{\\\"nodes\\\":$nodes,\\\"cores\\\":$cores}}}") \n')
 
         # copy the environment configuration files to non-cache directories
-        op = ['run_env.sh', site_packages_dir + '/gptune/.']
+        op = ['run_env.sh', python_platlib + '/gptune/.']
         self.run_test('cp', options=op, work_dir=wd)
         op = ['run_env.sh', self.install_test_root + '/.']
         self.run_test('cp', options=op, work_dir=wd)
