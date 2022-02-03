f692eaa9e679c9eea5845dd5139598d7bf1b80dd
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2022-02-03 10:18:51 +0100

    add NEURON 8.0.2 (#1466)

--- bluebrain/repo-patches/packages/neuron/package.py
+++ bluebrain/repo-patches/packages/neuron/package.py
@@ -35,6 +35,7 @@ class Neuron(CMakePackage):
     patch("patch-v800-cmake-nvhpc.patch", when="@8.0.0%nvhpc^cmake@3.20:")
 
     version("develop", branch="master")
+    version("8.0.2", tag="8.0.2")
     version("8.0.1", tag="8.0.1")
     version("8.0.0", tag="8.0.0")
     version("7.9.0b",  commit="94147e5")
