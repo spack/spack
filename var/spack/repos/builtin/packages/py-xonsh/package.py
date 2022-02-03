f94e63c568554d5fb97bc54faa6f0f69c463cdf4
Matthieu Dorier <matthieu.dorier@gmail.com>
2022-02-03 17:59:19 +0000

    [py-xonsh] change dependency to python 3.6
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-xonsh/package.py
+++ var/spack/repos/builtin/packages/py-xonsh/package.py
@@ -17,5 +17,5 @@ class PyXonsh(PythonPackage):
 
     version('0.11.0', sha256='0d9c3d9a4e8b8199ae697fbc9d1e0ae55085cdbdd4306d04813350996f9c15dc')
 
-    depends_on('python@3.7:', type=('build', 'run'))
+    depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
