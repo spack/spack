8b913d55f8244fc6e1a6626f63c698dd7c3708a5
Axel Huebl <axel.huebl@plasma.ninja>
2022-02-03 10:04:05 -0800

    Update: py-cmake
    Add additional dependencies as declared by the `py-cmake` repository.
    
    Note: for either from-source or from-binary builds, this downloads
    additional software via the network. We might want to propose upstream
    patches to make this work on nodes without internet connection.

--- var/spack/repos/builtin/packages/py-cmake/package.py
+++ var/spack/repos/builtin/packages/py-cmake/package.py
@@ -9,16 +9,20 @@ from spack import *
 class PyCmake(PythonPackage):
     """CMake is an open-source, cross-platform family of tools designed to
     build, test and package software
+
+    Repository: https://github.com/scikit-build/cmake-python-distributions
     """
 
-    homepage = "https://cmake.org/"
-    pypi = "cmake/cmake-3.18.0.tar.gz"
+    homepage = "https://cmake.org"
+    pypi = "cmake/cmake-3.21.4.tar.gz"
 
     version('3.21.4', sha256='30fa5ed8a5ad66dcd263adb87f3ce3dc2d0ec0ac3958f5becff577e4b62cd065')
     version('3.18.0', sha256='52b98c5ee70b5fa30a8623e96482227e065292f78794eb085fdf0fecb204b79b')
 
     depends_on('py-scikit-build@0.12:', type='build')
     depends_on('py-setuptools@42:', type='build')
+    depends_on('py-wheel', type='build')
+    depends_on('git', type='build')
     depends_on('cmake@3.21.4', type=('build', 'link', 'run'), when='@3.21.4')
     depends_on('cmake@3.18.0', type=('build', 'link', 'run'), when='@3.18.0')
 
