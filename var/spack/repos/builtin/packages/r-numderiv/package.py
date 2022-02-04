5d5ebfa3a17afda9ca10a0d965cabc92e649d331
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-02-04 14:11:09 -0600

    Transliterate Unicode characters to ASCII

--- var/spack/repos/builtin/packages/r-numderiv/package.py
+++ var/spack/repos/builtin/packages/r-numderiv/package.py
@@ -10,10 +10,10 @@ class RNumderiv(RPackage):
     """Accurate Numerical Derivatives.
 
     Methods for calculating (usually) accurate numerical first and second order
-    derivatives. Accurate calculations are done using 'Richardson”s'
+    derivatives. Accurate calculations are done using 'Richardson"s'
     extrapolation or, when applicable, a complex step derivative is available.
     A simple difference method is also provided. Simple difference is (usually)
-    less accurate but is much quicker than 'Richardson”s' extrapolation and
+    less accurate but is much quicker than 'Richardson"s' extrapolation and
     provides a useful cross-check. Methods are provided for real scalar and
     vector valued functions."""
 
