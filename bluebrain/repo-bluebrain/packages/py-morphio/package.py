69f230858af931d5c156f637278cc19396baac6a
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2022-02-08 10:44:57 +0100

    Update morphio to support >1.2 h5v1 morph spec version (#1474)
    Update spack with the latest morphio version will result in touchdetector using the updated
    version check that includes 1.3 in the morphio reader. See NSETM-1759.

--- bluebrain/repo-bluebrain/packages/py-morphio/package.py
+++ bluebrain/repo-bluebrain/packages/py-morphio/package.py
@@ -11,11 +11,12 @@ class PyMorphio(PythonPackage):
 
     homepage = "https://github.com/BlueBrain/MorphIO"
     git      = "https://github.com/BlueBrain/MorphIO.git"
-    url      = "https://pypi.io/packages/source/m/morphio/MorphIO-3.1.1.tar.gz"
+    url      = "https://pypi.io/packages/source/m/morphio/MorphIO-3.3.2.tar.gz"
 
     version('develop', branch='master', submodules=True, get_full_repo=True)
     version('unifurcation', branch='unifurcation', submodules=True, get_full_repo=True)
 
+    version('3.3.2', sha256="fc961defbfbfb3f11360954fb3ec51373eaff25b154fa31d6b31decca6937780")
     version('3.1.1', sha256="ad9f0e363f09f03c6eda54f5f3b006d204236677d2f2c9675421e0441033a503")
     version('2.7.1', sha256="3f3e2229da85e874527775fce080f712b6dc287edc44b90b6de35d17b34badff")
 
