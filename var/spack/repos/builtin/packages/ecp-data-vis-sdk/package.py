bd60fd7b2fcc54ddb3a9bd644efc38b455f25210
Ryan Krattiger <ryan.krattiger@kitware.com>
2022-02-10 15:50:39 -0600

    ECP-SDK: Propagate cuda to ascent

--- var/spack/repos/builtin/packages/ecp-data-vis-sdk/package.py
+++ var/spack/repos/builtin/packages/ecp-data-vis-sdk/package.py
@@ -121,11 +121,17 @@ class EcpDataVisSdk(BundlePackage, CudaPackage):
     dav_sdk_depends_on('sensei@develop +vtkio +python ~miniapps', when='+sensei',
                        propagate=dict(propagate_to_sensei))
 
+    dav_sdk_depends_on('ascent+mpi+fortran+openmp+python+shared+vtkh+dray',
+                       when='+ascent',
+                       propagate=['adios2'] + cuda_arch_variants)
     # Need to explicitly turn off conduit hdf5_compat in order to build
     # hdf5@1.12 which is required for SDK
-    dav_sdk_depends_on('ascent+shared+mpi+fortran+openmp+python+vtkh+dray',
-                       when='+ascent')
     depends_on('ascent ^conduit ~hdf5_compat', when='+ascent +hdf5')
+    depends_on('ascent~cuda', when='+ascent~cuda')
+    depends_on('ascent+cuda', when='+ascent+cuda ^vtk-m@1.7:')
+    # Disable configuring with @develop. This should be removed after ascent
+    # releases 0.8 and ascent can build with conduit@0.8: and vtk-m@1.7:
+    conflicts('ascent@develop')
 
     depends_on('py-cinemasci', when='+cinema')
 
