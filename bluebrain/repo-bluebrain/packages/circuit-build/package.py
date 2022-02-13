442f3beb1264f6b0062339850f21c759c089c4e8
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2022-02-13 19:22:15 +0100

    Bump circuit-build to 4.0.0 (#1481)

--- bluebrain/repo-bluebrain/packages/circuit-build/package.py
+++ bluebrain/repo-bluebrain/packages/circuit-build/package.py
@@ -12,15 +12,16 @@ class CircuitBuild(PythonPackage):
     homepage = "https://bbpgitlab.epfl.ch/nse/circuit-build"
     git      = "git@bbpgitlab.epfl.ch:nse/circuit-build.git"
 
-    version('develop', branch='main')
-    version('3.1.1', tag='circuit-build-v3.1.1')
-    version('3.1.3', tag='circuit-build-v3.1.3')
+    version('develop')
+    version('4.0.0', tag='circuit-build-v4.0.0')
 
     depends_on('py-setuptools', type=('build', 'run'))
 
-    depends_on('py-click@7.0:7.99', type='run')
-    depends_on('snakemake@5.6:5.99', type='run')
-    depends_on('py-pyyaml@5.0:', type='run')
+    depends_on('py-click@7.0:7.99', type=('build', 'run'))
+    depends_on('py-pyyaml@5.0:', type=('build', 'run'))
+    depends_on('snakemake@5.10:6.99', type=('build', 'run'))
+    depends_on('py-jsonschema@3.2.0:3.99', type=('build', 'run'))
+    depends_on('py-jinja2@2.10.0:3.99', type=('build', 'run'))
 
     def setup_run_environment(self, env):
         env.prepend_path('PATH', self.spec['snakemake'].prefix.bin)
