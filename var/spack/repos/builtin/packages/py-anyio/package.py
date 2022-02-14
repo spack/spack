1bce80cd46f8f4f64eb9e0699149563dcdef4ffb
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-02-14 18:04:38 +0100

    Add wheel dependency

--- var/spack/repos/builtin/packages/py-anyio/package.py
+++ var/spack/repos/builtin/packages/py-anyio/package.py
@@ -20,6 +20,7 @@ class PyAnyio(PythonPackage):
     depends_on('python@3.6.2:', type=('build', 'run'))
     depends_on('py-setuptools@42:', type='build')
     depends_on('py-setuptools-scm+toml@3.4:', type='build')
+    depends_on('py-wheel@0.29:', type='build')
     depends_on('py-contextvars', when='@3.4: ^python@:3.6', type=('build', 'run'))
     depends_on('py-dataclasses', when='^python@:3.6', type=('build', 'run'))
     depends_on('py-idna@2.8:', type=('build', 'run'))
