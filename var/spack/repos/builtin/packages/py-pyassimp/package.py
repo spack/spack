510538c08aabea1cdad961a250e96336ca5213ed
Viv Eric Hafener <vehrc@sporcbuild.rc.rit.edu>
2022-02-15 08:48:10 -0500

    Cleaned up dependencies

--- var/spack/repos/builtin/packages/py-pyassimp/package.py
+++ var/spack/repos/builtin/packages/py-pyassimp/package.py
@@ -13,10 +13,3 @@ class PyPyassimp(PythonPackage):
     pypi     = "pyassimp/pyassimp-4.1.4.tar.gz"
 
     version('4.1.4', sha256='266bd4be170d46065b8c2ad0f5396dad10938a6bbf9a566c4e4d56456e33aa6a')
-
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-pip@X.Y:', type='build')
-    # depends_on('py-wheel@X.Y:', type='build')
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-flit-core', type='build')
-    # depends_on('py-poetry-core', type='build')
