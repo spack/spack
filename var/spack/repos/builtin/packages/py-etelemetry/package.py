95966c07fd676413d19006b63827d421d6c6e96d
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2022-02-16 10:32:16 +0100

    Update var/spack/repos/builtin/packages/py-etelemetry/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-etelemetry/package.py
+++ var/spack/repos/builtin/packages/py-etelemetry/package.py
@@ -15,7 +15,7 @@ class PyEtelemetry(PythonPackage):
     version('0.3.0', sha256='5f710fdb17ec02f93be29d234b33c1c62ad641320d1b4047e61044679018de03')
     version('0.2.2', sha256='bfec416552d248ad0f50b90ba5ff015e825ad70e4a87f7a06cc7da6d19152897')
 
-    depends_on('python@3.7:', when='@3:', type=('build', 'run'))
+    depends_on('python@3.7:', when='@0.3:', type=('build', 'run'))
     depends_on('python@3.6:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
 
