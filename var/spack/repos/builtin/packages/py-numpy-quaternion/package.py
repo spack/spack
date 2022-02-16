501bda669c2c438313e1043ed685773a1220eafc
Jen Herting <jen@herting.cc>
2022-02-16 11:32:12 -0500

    [py-numpy-quaternion] added scipy and numba variants

--- var/spack/repos/builtin/packages/py-numpy-quaternion/package.py
+++ var/spack/repos/builtin/packages/py-numpy-quaternion/package.py
@@ -21,5 +21,12 @@ class PyNumpyQuaternion(PythonPackage):
 
     version('2021.11.4.15.26.3', sha256='b0dc670b2adc8ff2fb8d6105a48769873f68d6ccbe20af6a19e899b1e8d48aaf')
 
-    depends_on('py-setuptools',         type='build')
-    depends_on('py-numpy@1.13:',        type=('build', 'run'))
+    variant('scipy', default=True, description="Build with scipy support")
+    variant('numba', default=True, description="Build with numba support")
+
+    depends_on('py-setuptools',     type='build')
+    depends_on('py-numpy@1.13:',    type=('build', 'run'))
+    depends_on('py-scipy',          type=('build', 'run'), when='+scipy')
+    depends_on('py-numba',          type=('build', 'run'), when='+numba')
+    depends_on('py-numba@:0.48',    type=('build', 'run'), when='+numba^python@:3.5')
+    depends_on('py-llvmlite@:0.31', type=('build', 'run'), when='+numba^python@:3.5')
