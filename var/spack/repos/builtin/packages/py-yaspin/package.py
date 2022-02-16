7d1d4832b8421e8fc59e37588751955883024aa5
Jen Herting <jen@herting.cc>
2022-02-16 18:05:47 -0500

    [py-yaspin]
    - added homepage
    - added description
    - added dependencies
    - removed fixmes

--- var/spack/repos/builtin/packages/py-yaspin/package.py
+++ var/spack/repos/builtin/packages/py-yaspin/package.py
@@ -3,63 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-yaspin
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-yaspin
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyYaspin(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Yet Another Terminal Spinner"""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/pavdmyt/yaspin"
     pypi     = "yaspin/yaspin-2.1.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('2.1.0', sha256='c8d34eca9fda3f4dfbe59f57f3cf0f3641af3eefbf1544fbeb9b3bacf82c580a')
 
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-pip@X.Y:', type='build')
-    # depends_on('py-wheel@X.Y:', type='build')
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-flit-core', type='build')
-    # depends_on('py-poetry-core', type='build')
-
-    # FIXME: Add additional dependencies if required.
-    # depends_on('py-foo', type=('build', 'run'))
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
+    depends_on('python@3.6.2:3',       type=('build', 'run'))
+    depends_on('py-setuptools',        type='build')
+    depends_on('py-poetry-core@1:',    type='build')
+    depends_on('py-termcolor@1.1.0:1', type=('build', 'run'))
