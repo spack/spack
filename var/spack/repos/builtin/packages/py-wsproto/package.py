8958af6ba9b9838b81d21649971579c140b267f3
Jen Herting <jen@herting.cc>
2022-02-17 09:50:25 -0500

    [py-wsproto]
    - added homepage
    - added description
    - added dependencies
    - removed fixmes

--- var/spack/repos/builtin/packages/py-wsproto/package.py
+++ var/spack/repos/builtin/packages/py-wsproto/package.py
@@ -3,63 +3,23 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-wsproto
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-wsproto
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyWsproto(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This repository contains a pure-Python implementation of
+    a WebSocket protocol stack. It’s written from the ground up
+    to be embeddable in whatever program you choose to use,
+    ensuring that you can communicate via WebSockets, as
+    defined in RFC6455, regardless of your programming
+    paradigm."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/python-hyper/wsproto/"
     pypi     = "wsproto/wsproto-1.0.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('1.0.0', sha256='868776f8456997ad0d9720f7322b746bbe9193751b5b290b7f924659377c8c38')
 
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-pip@X.Y:', type='build')
-    # depends_on('py-wheel@X.Y:', type='build')
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-flit-core', type='build')
-    # depends_on('py-poetry-core', type='build')
-
-    # FIXME: Add additional dependencies if required.
-    # depends_on('py-foo', type=('build', 'run'))
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
+    depends_on('python@3.6.1:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-dataclasses', type=('build', 'run'), when='^python@:3.6')
+    depends_on('py-h11@0.9.0:0', type=('build', 'run'))
