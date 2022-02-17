c23b791ec8eb4e7b496fa465d8a95a764072d84c
Jen Herting <jen@herting.cc>
2022-02-16 19:49:29 -0500

    [py-hypercorn]
    - added homepage
    - added description
    - added dependencies
    - removed fixmes

--- var/spack/repos/builtin/packages/py-hypercorn/package.py
+++ var/spack/repos/builtin/packages/py-hypercorn/package.py
@@ -3,63 +3,24 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-hypercorn
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-hypercorn
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyHypercorn(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """A ASGI Server based on Hyper libraries and inspired by
+    Gunicorn."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://gitlab.com/pgjones/hypercorn/"
     pypi     = "Hypercorn/Hypercorn-0.13.2.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('0.13.2', sha256='6307be5cbdf6ba411967d4661202dc4f79bd511b5d318bc4eed88b09418427f8')
 
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-pip@X.Y:', type='build')
-    # depends_on('py-wheel@X.Y:', type='build')
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-flit-core', type='build')
-    # depends_on('py-poetry-core', type='build')
-
-    # FIXME: Add additional dependencies if required.
-    # depends_on('py-foo', type=('build', 'run'))
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
+    depends_on('python@3.7:',          type=('build', 'run'))
+    depends_on('py-setuptools',        type='build')
+    depends_on('py-poetry-core@1:',    type='build')
+    depends_on('py-h11',               type=('build', 'run'))
+    depends_on('py-h2@3.1.0:',         type=('build', 'run'))
+    depends_on('py-priority',          type=('build', 'run'))
+    depends_on('py-toml',              type=('build', 'run'))
+    depends_on('py-wsproto@0.14.0:',   type=('build', 'run'))
+    depends_on('py-typing-extensions', type=('build', 'run'), when='^python@:3.7')
