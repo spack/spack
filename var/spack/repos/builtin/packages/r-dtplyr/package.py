4de79a687689d3597439d71b441e5381ccfde435
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-02-18 11:22:06 +0100

    r-dtplyr: add 1.2.1

--- var/spack/repos/builtin/packages/r-dtplyr/package.py
+++ var/spack/repos/builtin/packages/r-dtplyr/package.py
@@ -16,10 +16,12 @@ class RDtplyr(RPackage):
     homepage = "https://github.com/tidyverse/dtplyr"
     cran     = "dtplyr"
 
+    version('1.2.1', sha256='2640e9cde4eaa06f02cff29e3f2b99fdd08488df07ea2e6629b2ed6a8285d0f3')
     version('1.1.0', sha256='99681b7285d7d5086e5595ca6bbeebf7f4e2ee358a32b694cd9d35916cdfc732')
 
     depends_on('r@3.3:', type=('build', 'run'))
     depends_on('r-crayon', type=('build', 'run'))
+    depends_on('r-data-table@1.13.0:', when='@1.2:', type=('build', 'run'))
     depends_on('r-data-table@1.12.4:', type=('build', 'run'))
     depends_on('r-dplyr@1.0.3:', type=('build', 'run'))
     depends_on('r-ellipsis', type=('build', 'run'))
