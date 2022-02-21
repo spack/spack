fef58db7927be41fd41a44dbb7a233db5497e9a4
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2022-02-21 20:57:16 +0100

    r-brobdingnag: add 1.2-7 (#28997)

--- var/spack/repos/builtin/packages/r-brobdingnag/package.py
+++ var/spack/repos/builtin/packages/r-brobdingnag/package.py
@@ -15,6 +15,7 @@ class RBrobdingnag(RPackage):
 
     cran = "Brobdingnag"
 
+    version('1.2-7', sha256='73a734342736da5b29c2827d91f662101873503af7ad9cdf9e9e697bb32dd742')
     version('1.2-6', sha256='19eccaed830ce9d93b70642f6f126ac66722a98bbd48586899cc613dd9966ad4')
 
     depends_on('r@2.13.0:', type=('build', 'run'))
