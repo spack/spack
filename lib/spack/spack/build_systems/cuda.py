a7a4f11d3c3bc58b03ef55ab0a982ed69b02cfbd
Alberto Invernizzi <9337627+albestro@users.noreply.github.com>
2022-02-21 13:50:16 +0100

    Fix clang conflict
    Co-authored-by: Harmen Stoppels <harmenstoppels@gmail.com>

--- lib/spack/spack/build_systems/cuda.py
+++ lib/spack/spack/build_systems/cuda.py
@@ -108,7 +108,7 @@ class CudaPackage(PackageBase):
         conflicts('%gcc@10:', when='+cuda ^cuda@:11.0')
         conflicts('%gcc@11:', when='+cuda ^cuda@:11.4.0')
         conflicts('%gcc@12:', when='+cuda ^cuda@:11.6')
-        conflicts('%clang@12:', when='+cuda ^cuda@:11.4')
+        conflicts('%clang@12:', when='+cuda ^cuda@:11.4.0')
         conflicts('%clang@13:', when='+cuda ^cuda@:11.5')
         conflicts('%clang@14:', when='+cuda ^cuda@:11.6')
 
