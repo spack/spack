cd28c7175649bf081e3f362cd920556537520cea
Valentin Volkl <valentin.volkl@cern.ch>
2022-02-23 08:40:25 +0000

    starlight: add new package

---
+++ var/spack/repos/builtin/packages/starlight/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class Starlight(CMakePackage):
+    """STARlight is a Monte Carlo that simulates two-photon
+        and photon-Pomeron interactions between relativistic nuclei and protons."""
+
+    homepage = "https://starlight.hepforge.org"
+    url      = "https://starlight.hepforge.org/downloads?f=starlight_r313.tar"
+
+    tags = ['hep']
+
+    maintainers = ['vvolkl']
+
+    version('313', sha256='afff1ac01bc312cb443931eb425808a85ac7e349ed3df228405a4ba87d780f23',
+            url='https://starlight.hepforge.org/downloads?f=starlight_r313.tar')
+    version('300', sha256='e754212f173fb42a96611b3519cb386c772429e690f3e088396bd103c2cf9348',
+            url='https://starlight.hepforge.org/downloads?f=starlight_r300.tar')
+
+    patch('starlight-r3xx.patch', when='@300:')
