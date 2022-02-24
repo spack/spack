0f8d9fd5277c9c1a64657b0faa37e33d41bfabe7
Hadrien Grasland <grasland@lal.in2p3.fr>
2022-02-24 10:27:43 +0100

    Add version 17.1

--- var/spack/repos/builtin/packages/acts/package.py
+++ var/spack/repos/builtin/packages/acts/package.py
@@ -39,6 +39,7 @@ class Acts(CMakePackage, CudaPackage):
     # Supported Acts versions
     version('main', branch='main')
     version('master', branch='main', deprecated=True)  # For compatibility
+    version('17.1.0', commit='0d9c3a6da022da48d6401e10c273896a1f775a9e', submodules=True)
     version('17.0.0', commit='ccbf4c7d4ec3698bac4db9687fab2455a3f9c203', submodules=True)
     version('16.0.0', commit='9bd86921155e708189417b5a8019add10fd5b273', submodules=True)
     version('15.1.0', commit='a96e6db7de6075e85b6d5346bc89845eeb89b324', submodules=True)
