a9323be9d083aa942bfe4a85bd6a02e3f729eb63
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-02-26 00:00:21 +0100

    fix python dependency

--- var/spack/repos/builtin/packages/py-mako/package.py
+++ var/spack/repos/builtin/packages/py-mako/package.py
@@ -20,6 +20,6 @@ class PyMako(PythonPackage):
     version('1.0.4', sha256='fed99dbe4d0ddb27a33ee4910d8708aca9ef1fe854e668387a9ab9a90cbf9059')
     version('1.0.1', sha256='45f0869febea59dab7efd256fb451c377cbb7947bef386ff0bb44627c31a8d1c')
 
+    depends_on('python@2.7:2,3.4:', when='@1.1.0:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
     depends_on('py-markupsafe@0.9.2:', type=('build', 'run'))
-    depends_on('python@2.7:2,3.4:', when='@1.1.0:')
