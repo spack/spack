d29253bd7af4c0c491492bc2ba59c97a8559857d
iarspider <iarspider@gmail.com>
2022-02-27 20:34:09 +0100

    Add 'master' version for dmtcp (#29211)
    * Add 'develop' version for dmtcp
    
    * Update var/spack/repos/builtin/packages/dmtcp/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Update package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/dmtcp/package.py
+++ var/spack/repos/builtin/packages/dmtcp/package.py
@@ -13,7 +13,9 @@ class Dmtcp(AutotoolsPackage):
 
     homepage = "http://dmtcp.sourceforge.net/"
     url      = "https://sourceforge.net/projects/dmtcp/files/2.6.0/dmtcp-2.6.0.tar.gz/download"
+    git      = "https://github.com/dmtcp/dmtcp.git"
 
+    version('master', branch='master')
     version('2.6.0', sha256='3ed62a86dd0cb9c828b93ee8c7c852d6f9c96a0efa48bcfe867521adf7bced68')
     version('2.5.2', sha256='0e3e5e15bd401b7b6937f2b678cd7d6a252eab0a143d5740b89cc3bebb4282be')
     patch('for_aarch64.patch', when='@2.6.0 target=aarch64:')
