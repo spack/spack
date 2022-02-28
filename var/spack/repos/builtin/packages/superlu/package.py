f15036af8fb794daf731d1e923247038a27e1727
Kayla Butler <butler59@llnl.gov>
2022-02-28 13:37:49 -0800

    Check each step of test

--- var/spack/repos/builtin/packages/superlu/package.py
+++ var/spack/repos/builtin/packages/superlu/package.py
@@ -172,14 +172,17 @@ class Superlu(CMakePackage):
         return config_args
 
     def run_superlu_test(self, test_dir, exe, args):
-        self.run_test('make',
-                      options=args,
-                      purpose='test: compile {0} example'.format(exe),
-                      work_dir=test_dir)
+        if not self.run_test('make',
+                             options=args,
+                             purpose='test: compile {0} example'.format(exe),
+                             work_dir=test_dir):
+            tty.warn('Skipping test: failed to compile example')
+            return
 
-        self.run_test(exe,
-                      purpose='test: run {0} example'.format(exe),
-                      work_dir=test_dir)
+        if not self.run_test(exe,
+                             purpose='test: run {0} example'.format(exe),
+                             work_dir=test_dir):
+            tty.warn('Skipping test: failed to run example')
 
     def test(self):
         config_args = self._generate_make_hdr_for_test()
