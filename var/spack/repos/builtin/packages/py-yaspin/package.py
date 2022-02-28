5ddfc027b684318c13f654e0ec61aaccb2c864c2
Jen Herting <jen@herting.cc>
2022-02-28 12:04:05 -0500

    [py-yaspin]
    - removed setuptools
    - added dependency on py-dataclasses

--- var/spack/repos/builtin/packages/py-yaspin/package.py
+++ var/spack/repos/builtin/packages/py-yaspin/package.py
@@ -15,6 +15,6 @@ class PyYaspin(PythonPackage):
     version('2.1.0', sha256='c8d34eca9fda3f4dfbe59f57f3cf0f3641af3eefbf1544fbeb9b3bacf82c580a')
 
     depends_on('python@3.6.2:3',       type=('build', 'run'))
-    depends_on('py-setuptools',        type='build')
     depends_on('py-poetry-core@1:',    type='build')
     depends_on('py-termcolor@1.1.0:1', type=('build', 'run'))
+    depends_on('py-dataclasses@0.8:0', type=('build', 'run'), when='^python@3.6')
