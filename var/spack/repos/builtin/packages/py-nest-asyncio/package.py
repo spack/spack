20ff7b092426e0802e9d82a1c44b0c756f782b62
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-03-01 13:06:19 +0100

    py-nest-asyncio: add 1.5.4

--- var/spack/repos/builtin/packages/py-nest-asyncio/package.py
+++ var/spack/repos/builtin/packages/py-nest-asyncio/package.py
@@ -10,6 +10,7 @@ class PyNestAsyncio(PythonPackage):
     homepage = "https://github.com/erdewit/nest_asyncio"
     pypi = "nest-asyncio/nest_asyncio-1.4.0.tar.gz"
 
+    version('1.5.4', sha256='f969f6013a16fadb4adcf09d11a68a4f617c6049d7af7ac2c676110169a63abd')
     version('1.5.1', sha256='afc5a1c515210a23c461932765691ad39e8eba6551c055ac8d5546e69250d0aa')
     version('1.4.0', sha256='5773054bbc14579b000236f85bc01ecced7ffd045ec8ca4a9809371ec65a59c8')
 
