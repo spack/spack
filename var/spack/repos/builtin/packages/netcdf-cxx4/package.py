79fbe8093a3fbc5eebd8609a4d507fe3f603d28e
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2022-03-02 13:49:42 +0100

    netcdf-cxx4: inject PIC flags for C++ when '+pic'

--- var/spack/repos/builtin/packages/netcdf-cxx4/package.py
+++ var/spack/repos/builtin/packages/netcdf-cxx4/package.py
@@ -39,6 +39,8 @@ class NetcdfCxx4(AutotoolsPackage):
     def flag_handler(self, name, flags):
         if name == 'cflags' and '+pic' in self.spec:
             flags.append(self.compiler.cc_pic_flag)
+        if name == 'cxxflags' and '+pic' in self.spec:
+            flags.append(self.compiler.cxx_pic_flag)
         elif name == 'cppflags':
             flags.append('-I' + self.spec['netcdf-c'].prefix.include)
         elif name == 'ldflags':
