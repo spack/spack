fc1ce9aee4c33f1c3c39309b208d57ccf7379191
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-03-02 14:19:18 +0100

    remove dependencies

--- var/spack/repos/builtin/packages/py-msgpack/package.py
+++ var/spack/repos/builtin/packages/py-msgpack/package.py
@@ -17,6 +17,4 @@ class PyMsgpack(PythonPackage):
     version('1.0.0', sha256='9534d5cc480d4aff720233411a1f765be90885750b07df772380b34c10ecb5c0')
     version('0.6.2', sha256='ea3c2f859346fcd55fc46e96885301d9c2f7a36d453f5d8f2967840efa1e1830')
 
-    depends_on('py-setuptools@35.0.2:', when='@1.0.3:', type='build')
     depends_on('py-setuptools', type='build')
-    depends_on('py-cython@0.29.13:0.29', type='build')
