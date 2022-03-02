9c5320ebcef8665b2c7a9a910508d1c1f5d197e0
Marcus Boden <mboden@gwdg.de>
2022-03-02 13:34:29 +0100

    Added py-yte package

---
+++ var/spack/repos/builtin/packages/py-yte/package.py
@@ -0,0 +1,27 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyYte(PythonPackage):
+    """YTE is a template engine for YAML format that utilizes the YAML
+    structure in combination with Python expressions for enabling to
+    dynamically build YAML documents."""
+
+    homepage = "https://github.com/koesterlab/yte"
+    pypi = "yte/yte-1.2.0.tar.gz"
+    maintainers = ['marcusboden']
+    version('1.2.0', '0368f220bb96fb3290bbd5a90e3d218af483d0d3e5abf9b08b24e29b150f151e')
+
+    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
+    # or need to change the dependency type. Generic python/pip/wheel dependencies are
+    # added implicity by the PythonPackage base class.
+    depends_on('python@3.7:', type=('build', 'run'))
+#    depends_on('py-setuptools', type='build')
+    depends_on('py-poetry', type='build')
+
+    depends_on('py-plac@1.3.4:1', type=('build', 'run'))
+    depends_on('py-pyyaml@6.0:6', type=('build', 'run'))
