781dec01557892aec52693518160dd5ed61da67b
Paul Kuberry <pakuber@sandia.gov>
2022-03-03 07:55:37 -0700

    py-pycompadre: add debug variant

--- var/spack/repos/builtin/packages/py-pycompadre/package.py
+++ var/spack/repos/builtin/packages/py-pycompadre/package.py
@@ -23,6 +23,8 @@ class PyPycompadre(PythonPackage):
     version('master',  branch='master', preferred=True)
 
     variant('trilinos', default=False, description='Use Kokkos from Trilinos')
+    variant('debug', default='0', values=['0', '1', '2'], multi=False,
+            description='Debugging level 0) release 1) debug 2) extreme debugging')
 
     depends_on('cmake@3.10.0:', type='build')
     depends_on('python@3.4:', type=('build', 'link', 'run'))
@@ -32,8 +34,18 @@ class PyPycompadre(PythonPackage):
     depends_on('trilinos@13.2:', when='+trilinos')
 
     @run_before('install')
-    def set_kokkos(self):
+    def set_cmake_from_variants(self):
         spec = self.spec
         with open('cmake_opts.txt', 'w') as f:
             if '+trilinos' in spec:
                 f.write('Trilinos_PREFIX:PATH=%s\n' % spec['trilinos'].prefix)
+            if spec.variants['debug'].value == '0':
+                f.write('CMAKE_CXX_FLAGS:STRING=%s\n' %
+                        "' -Ofast -funroll-loops -march=native -mtune=native '")
+                f.write('Compadre_DEBUG:BOOL=OFF\n')
+            else:
+                f.write('CMAKE_CXX_FLAGS:STRING=%s\n' % "'-g -O0'")
+                f.write('CMAKE_BUILD_TYPE:STRING=%s\n' % "DEBUG")
+                f.write('Compadre_DEBUG:BOOL=ON\n')
+                if spec.variants['debug'].value == '2':
+                    f.write('Compadre_EXTREME_DEBUG:BOOL=ON\n')
