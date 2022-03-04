557fab5564c86ad28b69c3e784c4f2635278b068
Matthias Wolf <m+git@sushinara.net>
2022-03-04 11:24:06 +0100

    deploy: dedicated source cache directory, bump morphio to test (#1510)

--- bluebrain/repo-bluebrain/packages/morphio/package.py
+++ bluebrain/repo-bluebrain/packages/morphio/package.py
@@ -15,6 +15,7 @@ class Morphio(CMakePackage):
 
     version('develop', submodules=True)
 
+    version('3.3.3', sha256='f6d91970cfd734b2e5fb8f9239a0bfa00519fe082dd8e403e4cc204dbdf0a9fa')
     version('3.3.2', sha256="fc961defbfbfb3f11360954fb3ec51373eaff25b154fa31d6b31decca6937780")
     version('3.1.1', sha256="ad9f0e363f09f03c6eda54f5f3b006d204236677d2f2c9675421e0441033a503")
     version('2.7.1', sha256="3f3e2229da85e874527775fce080f712b6dc287edc44b90b6de35d17b34badff")
@@ -32,9 +33,12 @@ class Morphio(CMakePackage):
             '-DBUILD_BINDINGS:BOOL=OFF',
             '-DEXTERNAL_HIGHFIVE:BOOL=ON',
         ]
-        if self.spec.satisfies("+mpi"):
+        if self.spec.satisfies('+mpi'):
             args += [
                 '-DCMAKE_C_COMPILER={0}'.format(self.spec['mpi'].mpicc),
                 '-DCMAKE_CXX_COMPILER={0}'.format(self.spec['mpi'].mpicxx)
             ]
+        # PyPI tarballs don't include the unit tests
+        if self.spec.satisfies('@3.3.3'):
+            args.append('-DMORPHIO_TESTS:BOOL=OFF')
         return args
--- bluebrain/repo-bluebrain/packages/py-morphio/package.py
+++ bluebrain/repo-bluebrain/packages/py-morphio/package.py
@@ -16,6 +16,7 @@ class PyMorphio(PythonPackage):
     version('develop', branch='master', submodules=True, get_full_repo=True)
     version('unifurcation', branch='unifurcation', submodules=True, get_full_repo=True)
 
+    version('3.3.3', sha256='f6d91970cfd734b2e5fb8f9239a0bfa00519fe082dd8e403e4cc204dbdf0a9fa')
     version('3.3.2', sha256="fc961defbfbfb3f11360954fb3ec51373eaff25b154fa31d6b31decca6937780")
     version('3.1.1', sha256="ad9f0e363f09f03c6eda54f5f3b006d204236677d2f2c9675421e0441033a503")
     version('2.7.1', sha256="3f3e2229da85e874527775fce080f712b6dc287edc44b90b6de35d17b34badff")
