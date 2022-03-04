1605700677bf1d3c0c6339cc7d4ec850f909773f
Marcus Boden <mboden@gwdg.de>
2022-03-04 06:56:20 +0100

    Update var/spack/repos/builtin/packages/py-yte/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-yte/package.py
+++ var/spack/repos/builtin/packages/py-yte/package.py
@@ -17,7 +17,7 @@ class PyYte(PythonPackage):
     version('1.2.0', '0368f220bb96fb3290bbd5a90e3d218af483d0d3e5abf9b08b24e29b150f151e')
 
     depends_on('python@3.7:', type=('build', 'run'))
-    depends_on('py-poetry', type='build')
+    depends_on('py-poetry-core@1:', type='build')
 
     depends_on('py-plac@1.3.4:1', type=('build', 'run'))
     depends_on('py-pyyaml@6.0:6', type=('build', 'run'))
