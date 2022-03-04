f8e95c4cd426c90b5e5699e109661ad707e5ec1c
Harmen Stoppels <harmenstoppels@gmail.com>
2022-03-04 12:30:44 +0100

    environment.py: allow link:run
    Some users want minimal views, excluding run-type dependencies, since
    those type of dependencies are covered by rpaths and the symlinked
    libraries in the view aren't used anyways.
    
    With this change, an environment like this:
    
    ```
    spack:
      specs: ['py-flake8']
      view:
        default:
          root: view
          link: run
    ```
    
    includes python packages and python, but no link type deps of python.

--- lib/spack/spack/environment/environment.py
+++ lib/spack/spack/environment/environment.py
@@ -471,80 +471,89 @@ class ViewDescriptor(object):
 
         return True
 
-    def specs_for_view(self, all_specs, roots):
-        specs_for_view = []
-        specs = all_specs if self.link == 'all' else roots
+    def specs_for_view(self, concretized_specs):
+        """
+        From the list of concretized user specs in the environment, flatten
+        the dags, and filter installed specs, remove duplicates on dag hash.
+        """
+        specs = []
 
-        for spec in specs:
-            # The view does not store build deps, so if we want it to
-            # recognize environment specs (which do store build deps),
-            # then they need to be stripped.
-            if spec.concrete:  # Do not link unconcretized roots
-                # We preserve _hash _normal to avoid recomputing DAG
-                # hashes (DAG hashes don't consider build deps)
-                spec_copy = spec.copy(deps=('link', 'run'))
-                spec_copy._hash = spec._hash
-                spec_copy._normal = spec._normal
-                specs_for_view.append(spec_copy)
-        return specs_for_view
+        for (_, s) in concretized_specs:
+            if self.link == 'all':
+                specs.extend(s.traverse(deptype=('link', 'run')))
+            elif self.link == 'run':
+                specs.extend(s.traverse(deptype=('run')))
+            else:
+                specs.append(s)
 
-    def regenerate(self, all_specs, roots):
-        specs_for_view = self.specs_for_view(all_specs, roots)
+        # De-dupe by dag hash
+        hashes = set()
+        specs_unique = []
+        for s in specs:
+            hash = s.dag_hash()
+            if hash in hashes:
+                continue
+            hashes.add(hash)
+            specs_unique.append(s)
 
-        # regeneration queries the database quite a bit; this read
-        # transaction ensures that we don't repeatedly lock/unlock.
+        specs = specs_unique
+
+        # Filter installed specs
         with spack.store.db.read_transaction():
-            installed_specs_for_view = set(
-                s for s in specs_for_view if s in self and s.package.installed)
+            specs = [s for s in specs if s.package.installed]
 
-            # To ensure there are no conflicts with packages being installed
-            # that cannot be resolved or have repos that have been removed
-            # we always regenerate the view from scratch.
-            # We will do this by hashing the view contents and putting the view
-            # in a directory by hash, and then having a symlink to the real
-            # view in the root. The real root for a view at /dirname/basename
-            # will be /dirname/._basename_<hash>.
-            # This allows for atomic swaps when we update the view
+        return specs
 
-            # cache the roots because the way we determine which is which does
-            # not work while we are updating
-            new_root = self._next_root(installed_specs_for_view)
-            old_root = self._current_root
+    def regenerate(self, concretized_specs):
+        specs = self.specs_for_view(concretized_specs)
 
-            if new_root == old_root:
-                tty.debug("View at %s does not need regeneration." % self.root)
-                return
+        # To ensure there are no conflicts with packages being installed
+        # that cannot be resolved or have repos that have been removed
+        # we always regenerate the view from scratch.
+        # We will do this by hashing the view contents and putting the view
+        # in a directory by hash, and then having a symlink to the real
+        # view in the root. The real root for a view at /dirname/basename
+        # will be /dirname/._basename_<hash>.
+        # This allows for atomic swaps when we update the view
 
-            # construct view at new_root
-            tty.msg("Updating view at {0}".format(self.root))
+        # cache the roots because the way we determine which is which does
+        # not work while we are updating
+        new_root = self._next_root(specs)
+        old_root = self._current_root
 
-            view = self.view(new=new_root)
-            fs.mkdirp(new_root)
-            view.add_specs(*installed_specs_for_view,
-                           with_dependencies=False)
+        if new_root == old_root:
+            tty.debug("View at %s does not need regeneration." % self.root)
+            return
 
-            # create symlink from tmpname to new_root
-            root_dirname = os.path.dirname(self.root)
-            tmp_symlink_name = os.path.join(root_dirname, '._view_link')
-            if os.path.exists(tmp_symlink_name):
-                os.unlink(tmp_symlink_name)
-            os.symlink(new_root, tmp_symlink_name)
+        # construct view at new_root
+        tty.msg("Updating view at {0}".format(self.root))
 
-            # mv symlink atomically over root symlink to old_root
-            if os.path.exists(self.root) and not os.path.islink(self.root):
-                msg = "Cannot create view: "
-                msg += "file already exists and is not a link: %s" % self.root
-                raise SpackEnvironmentViewError(msg)
-            os.rename(tmp_symlink_name, self.root)
+        view = self.view(new=new_root)
+        fs.mkdirp(new_root)
+        view.add_specs(*specs, with_dependencies=False)
 
-            # remove old_root
-            if old_root and os.path.exists(old_root):
-                try:
-                    shutil.rmtree(old_root)
-                except (IOError, OSError) as e:
-                    msg = "Failed to remove old view at %s\n" % old_root
-                    msg += str(e)
-                    tty.warn(msg)
+        # create symlink from tmpname to new_root
+        root_dirname = os.path.dirname(self.root)
+        tmp_symlink_name = os.path.join(root_dirname, '._view_link')
+        if os.path.exists(tmp_symlink_name):
+            os.unlink(tmp_symlink_name)
+        os.symlink(new_root, tmp_symlink_name)
+
+        # mv symlink atomically over root symlink to old_root
+        if os.path.exists(self.root) and not os.path.islink(self.root):
+            msg = "Cannot create view: "
+            msg += "file already exists and is not a link: %s" % self.root
+            raise SpackEnvironmentViewError(msg)
+        os.rename(tmp_symlink_name, self.root)
+
+        # remove old_root
+        if old_root and os.path.exists(old_root):
+            try:
+                shutil.rmtree(old_root)
+            except (IOError, OSError) as e:
+                msg = "Failed to remove old view at %s\n" % old_root
+                msg += str(e)
+                tty.warn(msg)
 
 
 def _create_environment(*args, **kwargs):
@@ -1303,9 +1312,8 @@ class Environment(object):
                       " maintain a view")
             return
 
-        specs = self._get_environment_specs()
         for view in self.views.values():
-            view.regenerate(specs, self.roots())
+            view.regenerate(self.concretized_specs())
 
     def check_views(self):
         """Checks if the environments default view can be activated."""
