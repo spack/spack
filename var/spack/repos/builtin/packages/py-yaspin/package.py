90b00914b8c0e9b08142bc2829cf11c70457c8f9
Jen Herting <jen@herting.cc>
2022-03-07 14:35:03 -0500

    [py-yaspin] removed range for py-dataclasses version

--- var/spack/repos/builtin/packages/py-yaspin/package.py
+++ var/spack/repos/builtin/packages/py-yaspin/package.py
@@ -17,4 +17,4 @@ class PyYaspin(PythonPackage):
     depends_on('python@3.6.2:3',       type=('build', 'run'))
     depends_on('py-poetry-core@1:',    type='build')
     depends_on('py-termcolor@1.1.0:1', type=('build', 'run'))
-    depends_on('py-dataclasses@0.8:0', type=('build', 'run'), when='^python@3.6')
+    depends_on('py-dataclasses@0.8',   type=('build', 'run'), when='^python@3.6')
