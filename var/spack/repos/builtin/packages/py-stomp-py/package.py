0ddbf746fd17c2343382be49d6aac3614035db1d
haralmha <haralmha@stud.ntnu.no>
2022-03-07 18:11:31 +0100

    py-stomp-py: Add package

---
+++ var/spack/repos/builtin/packages/py-stomp-py/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyStompPy(PythonPackage):
+    """Python client library for accessing messaging servers
+    (such as ActiveMQ, Artemis or RabbitMQ) using the STOMP
+    protocol (STOMP v1.0, STOMP v1.1 and STOMP v1.2)"""
+
+    homepage = "https://github.com/jasonrbriggs/stomp.py"
+    pypi     = "stomp.py/stomp.py-8.0.0.tar.gz"
+
+    maintainers = ['haralmha']
+
+    version('8.0.0', sha256='7085935293bfcc4a112a9830513275b2e0f3b040c5aad5ff8907e78f285b8b57')
+
+    depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-poetry@0.12:', type='build')
+    depends_on('py-docopt@0.6.2:', type=('build', 'run'))
