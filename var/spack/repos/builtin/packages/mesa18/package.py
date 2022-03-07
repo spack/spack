e3ab326228e51945a760b944115970e6d26b2b81
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-07 15:59:20 +0100

    mesa, mesa18: default to ~llvm (#29360)

--- var/spack/repos/builtin/packages/mesa18/package.py
+++ var/spack/repos/builtin/packages/mesa18/package.py
@@ -41,7 +41,7 @@ class Mesa18(AutotoolsPackage):
     depends_on('ncurses+termlib')
 
     # Internal options
-    variant('llvm', default=True, description="Enable LLVM.")
+    variant('llvm', default=False, description="Enable LLVM.")
     _SWR_ENABLED_VALUES = ('avx', 'avx2', 'knl', 'skx')
     variant('swr', values=any_combination_of(*_SWR_ENABLED_VALUES),
             description="Enable the SWR driver.")
