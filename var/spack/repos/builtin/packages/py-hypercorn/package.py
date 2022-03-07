b28f79404122980f4b52f3e7019f164e9b5e5b44
Jen Herting <jen@herting.cc>
2022-03-07 15:02:54 -0500

    [py-hypercorn] removed setuptools dependency and added version restriction for py-typing-extensions

--- var/spack/repos/builtin/packages/py-hypercorn/package.py
+++ var/spack/repos/builtin/packages/py-hypercorn/package.py
@@ -16,11 +16,10 @@ class PyHypercorn(PythonPackage):
     version('0.13.2', sha256='6307be5cbdf6ba411967d4661202dc4f79bd511b5d318bc4eed88b09418427f8')
 
     depends_on('python@3.7:',          type=('build', 'run'))
-    depends_on('py-setuptools',        type='build')
     depends_on('py-poetry-core@1:',    type='build')
     depends_on('py-h11',               type=('build', 'run'))
     depends_on('py-h2@3.1.0:',         type=('build', 'run'))
     depends_on('py-priority',          type=('build', 'run'))
     depends_on('py-toml',              type=('build', 'run'))
     depends_on('py-wsproto@0.14.0:',   type=('build', 'run'))
-    depends_on('py-typing-extensions', type=('build', 'run'), when='^python@:3.7')
+    depends_on('py-typing-extensions@3.7.4:', type=('build', 'run'), when='^python@:3.7')
