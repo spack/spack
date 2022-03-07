a0da0c56a6d688668d32c7460f845d3910fc8fa8
Axel Huebl <axel.huebl@plasma.ninja>
2022-03-07 13:29:03 -0800

    Add: Ninja
    Preferred generator according to outputs and upstream repo logic

--- var/spack/repos/builtin/packages/py-cmake/package.py
+++ var/spack/repos/builtin/packages/py-cmake/package.py
@@ -19,6 +19,7 @@ class PyCmake(PythonPackage):
     version('3.21.4', sha256='30fa5ed8a5ad66dcd263adb87f3ce3dc2d0ec0ac3958f5becff577e4b62cd065')
     version('3.18.0', sha256='52b98c5ee70b5fa30a8623e96482227e065292f78794eb085fdf0fecb204b79b')
 
+    depends_on('ninja', type='build')
     depends_on('py-scikit-build@0.12:', type='build')
     depends_on('py-setuptools@42:', type='build')
     depends_on('git', type='build')
