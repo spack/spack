fd347a2b67330bdc86e565180b5daf809b3c2c9f
haralmha <haralmha@stud.ntnu.no>
2022-03-07 19:36:00 +0100

    py-control: Add new package

---
+++ var/spack/repos/builtin/packages/py-control/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyControl(PythonPackage):
+    """The Python Control Systems Library is a Python module that implements
+    basic operations for analysis and design of feedback control systems."""
+
+    homepage = "http://python-control.org/"
+    pypi     = "control/control-0.9.1.tar.gz"
+
+    maintainers = ['haralmha']
+
+    version('0.9.1', sha256='8c9084bf386eafcf5d74008f780fae6dec68d243d18a380c866ac10a3549f8d3')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-numpy', type=('build', 'run'))
+    depends_on('py-scipy', type=('build', 'run'))
+    depends_on('py-matplotlib', type=('build', 'run'))
