abe1a15bad433610a302b7b0b867a191fa5f3132
Jen Herting <jen@herting.cc>
2022-03-09 13:39:24 -0500

    [py-distance]
    - added homepage
    - added description
    - removed fixmes

--- var/spack/repos/builtin/packages/py-distance/package.py
+++ var/spack/repos/builtin/packages/py-distance/package.py
@@ -3,63 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-distance
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-distance
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class PyDistance(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This package provides helpers for computing similarities
+    between arbitrary sequences. Included metrics are
+    Levenshtein, Hamming, Jaccard, and Sorensen distance, plus
+    some bonuses."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/doukremt/distance"
     pypi     = "Distance/Distance-0.1.3.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('0.1.3', sha256='60807584f5b6003f5c521aa73f39f51f631de3be5cccc5a1d67166fcbf0d4551')
-
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-pip@X.Y:', type='build')
-    # depends_on('py-wheel@X.Y:', type='build')
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-flit-core', type='build')
-    # depends_on('py-poetry-core', type='build')
-
-    # FIXME: Add additional dependencies if required.
-    # depends_on('py-foo', type=('build', 'run'))
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
