98d59093de824524379255b4aa6af74ef04e6675
Jen Herting <jen@herting.cc>
2022-03-09 16:12:11 -0500

    [py-pypinyin] created template

---
+++ var/spack/repos/builtin/packages/py-pypinyin/package.py
@@ -0,0 +1,65 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install py-pypinyin
+#
+# You can edit this file again by typing:
+#
+#     spack edit py-pypinyin
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class PyPypinyin(PythonPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+    pypi     = "pypinyin/pypinyin-0.46.0.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('0.46.0', sha256='0d2e41e95dbc20a232c0f5d3850654eebbfcba303d96358d2c46592725bb989c')
+
+    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
+    # or need to change the dependency type. Generic python/pip/wheel dependencies are
+    # added implicity by the PythonPackage base class.
+    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
+    # depends_on('py-pip@X.Y:', type='build')
+    # depends_on('py-wheel@X.Y:', type='build')
+
+    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
+    # exists, use setuptools.
+    # depends_on('py-setuptools', type='build')
+    # depends_on('py-flit-core', type='build')
+    # depends_on('py-poetry-core', type='build')
+
+    # FIXME: Add additional dependencies if required.
+    # depends_on('py-foo', type=('build', 'run'))
+
+    def global_options(self, spec, prefix):
+        # FIXME: Add options to pass to setup.py
+        # FIXME: If not needed, delete this function
+        options = []
+        return options
+
+    def install_options(self, spec, prefix):
+        # FIXME: Add options to pass to setup.py install
+        # FIXME: If not needed, delete this function
+        options = []
+        return options
