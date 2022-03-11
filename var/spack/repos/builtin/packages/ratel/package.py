5cf25675eebdc5a3d8df22b3e6a3ea42d6cd2900
Jeremy L Thompson <jeremy@jeremylt.org>
2022-03-11 14:46:51 -0700

    ratel - initial package info for develop and v0.1.0

---
+++ var/spack/repos/builtin/packages/ratel/package.py
@@ -0,0 +1,62 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Ratel(MakefilePackage, CudaPackage, ROCmPackage):
+    """Extensible, performance-portable solid mechanics with libCEED and PETSc"""
+
+    homepage = "https://ratel.micromorph.org"
+    git = "https://gitlab.com/micromorph/ratel.git"
+
+    maintainers = ['jedbrown', 'jeremylt']
+
+    # development version
+    version('develop', branch='main')
+    depends_on('libceed@develop', when='@develop')
+    depends_on('petsc@main', when='@develop')
+
+    # version 0.1.0
+    version('0.1.0', tag='v0.1.0')
+    depends_on('libceed@0.10.1', when='@0.1.0')
+    depends_on('petsc@3.17.0', when='@0.1.0')
+
+    # Note: '+cuda' and 'cuda_arch' variants are added by the CudaPackage
+    #       '+rocm' and 'amdgpu_target' variants are added by the ROCmPackage
+    # But we need to sync cuda/rocm with libCEED and PETSc
+    for sm_ in CudaPackage.cuda_arch_values:
+        depends_on('libceed+cuda cuda_arch={0}'.format(sm_),
+                   when='+cuda cuda_arch={0}'.format(sm_))
+        depends_on('petsc+cuda cuda_arch={0}'.format(sm_),
+                   when='+cuda cuda_arch={0}'.format(sm_))
+    for gfx in ROCmPackage.amdgpu_targets:
+        depends_on('libceed+rocm amdgpu_target={0}'.format(gfx),
+                   when='+rocm amdgpu_target={0}'.format(gfx))
+        depends_on('petsc+rocm amdgpu_target={0}'.format(gfx),
+                   when='+rocm amdgpu_target={0}'.format(gfx))
+    # Kokkos required for AMD GPUs
+    depends_on('petsc+kokkos', when='+rocm')
+
+    def configure(self, spec, prefix):
+        options = [
+            'CEED_DIR=%s' % spec['libceed'].prefix,
+            'PETSC_DIR=%s' % spec['petsc'].prefix,
+        ]
+        make('configure', *options)
+
+    def edit(self, spec, prefix):
+        make('info', *sef.common_make_opts)
+
+    @property
+    def build_targets(self, spec, prefix):
+        return self.common_make_opts
+
+    @property
+    def install_targets(self, spec, prefix):
+        return ['prefix={0}'.format(self.prefix)] + self.common_make_opts
+
+    def check(self):
+        make('prove', *self.common_make_opts, parallel=False)
