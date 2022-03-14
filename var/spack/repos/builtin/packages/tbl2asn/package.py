22685caea3b89ed606d23d966705f72c1e126ef2
Robert Romero <1013414+NessieCanCode@users.noreply.github.com>
2022-03-14 12:20:29 -0700

    Update package.py
    checksum was also off

--- var/spack/repos/builtin/packages/tbl2asn/package.py
+++ var/spack/repos/builtin/packages/tbl2asn/package.py
@@ -14,7 +14,7 @@ class Tbl2asn(Package):
 
     homepage = "https://www.ncbi.nlm.nih.gov/genbank/tbl2asn2/"
 
-    version('2020-03-01', sha256='7cc1119d3cfcbbffdbd4ecf33cef8bbdd44fc5625c72976bee08b1157625377e')
+    version('2020-03-01', sha256='b7edf378c971e6d7c48bf4d391d43a6d79f0e26dfa57156b4f87456af83e1fee')
 
     def url_for_version(self, ver):
         return "https://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/linux64.tbl2asn.gz"
