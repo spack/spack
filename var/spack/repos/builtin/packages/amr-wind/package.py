c0a763445a62bb6e316aa737472d58851f698f6e
Jon Rood <jon.rood@nrel.gov>
2022-03-14 15:53:54 -0600

    amr-wind: Fix hypre dependency.

--- var/spack/repos/builtin/packages/amr-wind/package.py
+++ var/spack/repos/builtin/packages/amr-wind/package.py
@@ -37,7 +37,7 @@ class AmrWind(CMakePackage, CudaPackage, ROCmPackage):
             description='Activate regression tests')
 
     depends_on('hypre~int64+shared@2.20.0:', when='+hypre')
-    depends_on('hypre+mpi', when='+mpi')
+    depends_on('hypre+mpi', when='+hypre+mpi')
     for arch in CudaPackage.cuda_arch_values:
         depends_on('hypre+unified-memory+cuda cuda_arch=%s' % arch,
                    when='+cuda+hypre cuda_arch=%s' % arch)
