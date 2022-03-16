6aefa36bdf8b0e6e19ae012aa6f149f0370b5d0e
Wouter Deconinck <wdconinc@gmail.com>
2022-03-16 14:19:06 -0500

    [assimp] depends_on pkgconfig and disable hunter, build zlib/minizip

--- var/spack/repos/builtin/packages/assimp/package.py
+++ var/spack/repos/builtin/packages/assimp/package.py
@@ -31,11 +31,13 @@ class Assimp(CMakePackage):
     variant('shared',  default=True,
             description='Enables the build of shared libraries')
 
+    depends_on('pkgconfig')
     depends_on('zlib')
     depends_on('boost')
 
     def cmake_args(self):
         args = [
+            '-DASSIMP_HUNTER_ENABLED=OFF',
             '-DASSIMP_BUILD_TESTS=OFF',
             self.define_from_variant('BUILD_SHARED_LIBS', 'shared'),
         ]
