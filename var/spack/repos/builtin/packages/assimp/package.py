a93fdaf7bac5b67a77d6088232882a444bd46ccf
Wouter Deconinck <wdconinc@gmail.com>
2022-03-19 21:19:47 -0500

    assimp: new version 5.2.3
    This version indeed includes the fix for which the PR 4203 patch range ends at 5.2.2.

--- var/spack/repos/builtin/packages/assimp/package.py
+++ var/spack/repos/builtin/packages/assimp/package.py
@@ -18,6 +18,7 @@ class Assimp(CMakePackage):
     maintainers = ['wdconinc']
 
     version('master', branch='master')
+    version('5.2.3', sha256='b20fc41af171f6d8f1f45d4621f18e6934ab7264e71c37cd72fd9832509af2a8')
     version('5.2.2', sha256='ad76c5d86c380af65a9d9f64e8fc57af692ffd80a90f613dfc6bd945d0b80bb4')
     version('5.2.1', sha256='c9cbbc8589639cd8c13f65e94a90422a70454e8fa150cf899b6038ba86e9ecff')
     version('5.1.4', sha256='bd32cdc27e1f8b7ac09d914ab92dd81d799c97e9e47315c1f40dcb7c6f7938c6')
