066183a8c9d293afeebdcfefab155921d24670bd
Josh Bowden <joshua-charles.bowden@inria.fr>
2022-03-21 16:54:44 +0100

    Damaris package updated to use master and tag 1.6.0. Support for building docs via doxygen added as cmake option and preliminary python support cmake option also added

--- var/spack/repos/builtin/packages/damaris/package.py
+++ var/spack/repos/builtin/packages/damaris/package.py
@@ -3,18 +3,20 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+
 from spack import *
-from spack.pkg.builtin.boost import Boost
 
 
 class Damaris(CMakePackage):
-    """Damaris is a middleware for asynchronous I/O and in situ analytics
+    """Damaris is a middleware for I/O and in situ analytics
     targeting large-scale, MPI-based HPC simulations."""
 
     homepage = "https://project.inria.fr/damaris/"
     git      = "https://gitlab.inria.fr/Damaris/damaris.git"
     maintainers = ['jcbowden']
 
+    version('master', branch='master')
+    version('1.6.0',  tag='v1.6.0')
     version('1.5.0',  tag='v1.5.0')
     version('1.3.3',  tag='v1.3.3')
     version('1.3.2',  tag='v1.3.2')
@@ -26,25 +28,25 @@ class Damaris(CMakePackage):
     variant('catalyst', default=False, description='Enables the Catalyst visualization plugin')
     variant('visit',    default=False, description='Enables the VisIt visualization plugin')
     variant('examples', default=False, description='Enables compilation and installation of the examples code')
+    variant('docs',     default=False, description='Enables the building of dOxygen documentation')
+    variant('python',   default=False, description='Enables building of Python enabled Damaris library - boost::python boost::numpy needed')
 
     depends_on('mpi')
     depends_on('cmake@3.18.0:', type=('build'))
-    depends_on('boost +thread+log+filesystem+date_time @1.67:')
-
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
-    # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants)
+    depends_on('boost+thread+log+filesystem+date_time @1.67:')
     depends_on('xsd')
     depends_on('xerces-c')
     depends_on('hdf5@1.8.20:', when='+hdf5')
     depends_on('paraview+python3', when='+catalyst')
     depends_on('visit+mpi', when='+visit')
+    depends_on('boost+thread+log+filesystem+date_time+python+numpy @1.67:', when='+python')
 
     def cmake_args(self):
+
         args = []
         if(not self.spec.variants['static'].value):
             args.extend(['-DBUILD_SHARED_LIBS=ON'])
+
         args.extend(['-DCMAKE_CXX_COMPILER=%s' % self.spec['mpi'].mpicxx])
         args.extend(['-DCMAKE_C_COMPILER=%s' % self.spec['mpi'].mpicc])
         args.extend(['-DBOOST_ROOT=%s' % self.spec['boost'].prefix])
@@ -67,6 +69,12 @@ class Damaris(CMakePackage):
 
         if (self.spec.variants['examples'].value):
             args.extend(['-DENABLE_EXAMPLES:BOOL=ON'])
+
+        if (self.spec.variants['docs'].value):
+            args.extend(['-DENABLE_DOCS:BOOL=ON'])
+
+        if (self.spec.variants['python'].value):
+            args.extend(['-DENABLE_PYTHON:BOOL=ON'])
 
         if (self.spec.variants['visit'].value):
             args.extend(['-DENABLE_VISIT:BOOL=ON'])
