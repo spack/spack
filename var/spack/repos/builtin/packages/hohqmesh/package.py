8c9c507d1f453ba9b4961fa106b0f7f3aade2f22
Joe Schoonover <joe@fluidnumerics.com>
2022-03-21 09:54:44 -0600

    Skip v1.2.0 of HOHQMesh

--- var/spack/repos/builtin/packages/hohqmesh/package.py
+++ var/spack/repos/builtin/packages/hohqmesh/package.py
@@ -15,7 +15,7 @@ class Hohqmesh(CMakePackage):
 
     maintainers = ['fluidnumerics-joe']
 
-    version('1.2.0', sha256='01aa2319bd67350fae095e0cc64917dffdffaca5f52c57e0a81f90a4a4ed4f7a')
+    version('1.2.1', sha256='b1b13a680c3ef6b8d6a8d05406f68c1ff641c26f69c468ccf2d7bed8d556dd7e')
     version('1.1.0', sha256='5fdb75157d9dc29bba55e6ae9dc2be71294754204f4f0912795532ae66aada10')
     version('1.0.1', sha256='8435f13c96d714a287f3c24392330047e2131d53fafe251a77eba365bd2b3141')
     version('1.0.0', sha256='3800e63975d0a61945508f13fb76d5e2145c0260440484252b6b81aa0bfe076d')
