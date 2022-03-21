e4f4d5d5671e84d993c64d87e2753e1c3c833d30
Greg Becker <becker33@llnl.gov>
2022-03-21 08:55:02 -0700

    config add: fix parsing of validator error to infer type from oneOf (#29475)

--- lib/spack/spack/test/cmd/config.py
+++ lib/spack/spack/test/cmd/config.py
@@ -239,6 +239,12 @@ def test_config_add_ordered_dict(mutable_empty_config):
 """
 
 
+def test_config_add_interpret_oneof(mutable_empty_config):
+    # Regression test for a bug that would raise a validation error
+    config('add', 'packages:all:target:[x86_64]')
+    config('add', 'packages:all:variants:~shared')
+
+
 def test_config_add_invalid_fails(mutable_empty_config):
     config('add', 'packages:all:variants:+debug')
     with pytest.raises(
