f1cef6ff836e83f1680097a3e9f43cef250e6674
Tom Scogland <scogland1@llnl.gov>
2022-03-22 13:29:58 -0700

    gcc: add gawk dependency

--- var/spack/repos/builtin/packages/gcc/package.py
+++ var/spack/repos/builtin/packages/gcc/package.py
@@ -113,6 +113,7 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage):
 
     # https://gcc.gnu.org/install/prerequisites.html
     depends_on('gmp@4.3.2:')
+    depends_on('gawk@3.1.5:') # mawk is not sufficient for go support
     # GCC 7.3 does not compile with newer releases on some platforms, see
     #   https://github.com/spack/spack/issues/6902#issuecomment-433030376
     depends_on('mpfr@2.4.2:3.1.6', when='@:9.9')
