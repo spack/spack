1250b37150cfb6a06237b718c3a91c2d1ec951f3
Tom Scogland <scogland1@llnl.gov>
2022-03-24 11:00:12 -0700

    fix style

--- var/spack/repos/builtin/packages/wdiff/package.py
+++ var/spack/repos/builtin/packages/wdiff/package.py
@@ -7,12 +7,15 @@
 from spack import *
 
 
-class Wdiff(AutotoolsPackage,GNUMirrorPackage):
+class Wdiff(AutotoolsPackage, GNUMirrorPackage):
     """FIXME: Put a proper description of your package here."""
 
     homepage = "https://www.gnu.org/software/wdiff/"
-    url      = "http://ftp.gnu.org/gnu/wdiff/wdiff-1.2.2.tar.gz"
+    url = "http://ftp.gnu.org/gnu/wdiff/wdiff-1.2.2.tar.gz"
 
-    version('1.2.2', sha256='34ff698c870c87e6e47a838eeaaae729fa73349139fc8db12211d2a22b78af6b')
+    version(
+        "1.2.2",
+        sha256="34ff698c870c87e6e47a838eeaaae729fa73349139fc8db12211d2a22b78af6b",
+    )
 
-    depends_on('ncurses')
+    depends_on("ncurses")
