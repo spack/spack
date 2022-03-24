1a2222197fea7c03219c688719c19990f2536623
Tom Scogland <scogland1@llnl.gov>
2022-03-24 10:35:10 -0700

    first-cut stack for development tools
    More will be needed here, and some of these are probably preference, but
    this covers a lot of the things that are usually missing from clusters
    for doing development work.  Added packages for wiggle and wdiff as well
    since wiggle especially is almost always missing and spack lacked both.

---
+++ var/spack/repos/builtin/packages/wdiff/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class Wdiff(AutotoolsPackage,GNUMirrorPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://www.gnu.org/software/wdiff/"
+    url      = "http://ftp.gnu.org/gnu/wdiff/wdiff-1.2.2.tar.gz"
+
+    version('1.2.2', sha256='34ff698c870c87e6e47a838eeaaae729fa73349139fc8db12211d2a22b78af6b')
+
+    depends_on('ncurses')
---
+++ var/spack/repos/builtin/packages/wiggle/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Wiggle(MakefilePackage):
+    """Word-wise heuristic diff and merge tool to apply rejected patches and perform
+    word-wise diffs
+    """
+
+    homepage = "http://neil.brown.name/wiggle"
+    url      = "https://github.com/neilbrown/wiggle/archive/refs/tags/v1.3.tar.gz"
+
+    maintainers = ['trws']
+
+    version('1.3', sha256='ff92cf0133c1f4dce33563e263cb30e7ddb6f4abdf86d427b1ec1490bec25afa')
+
+    def edit(self, spec, prefix):
+        makefile = FileFilter('Makefile')
+        makefile.filter('^\s*PREFIX\s*=.*', 'PREFIX = "{}"'.format(spec.prefix))
