7e63a9dc9c4859b0880fc0aaa303096170c8dcaa
Thomas Madlener <thomas.madlener@desy.de>
2022-03-25 13:13:48 +0100

    py-awkward: Add new versions

--- var/spack/repos/builtin/packages/py-awkward/package.py
+++ var/spack/repos/builtin/packages/py-awkward/package.py
@@ -15,7 +15,11 @@ class PyAwkward(PythonPackage):
 
     maintainers = ['vvolkl']
 
+    version('master', branch='master')
+    version('1.8.0', sha256='6655fa22d1b1d1dcb9ccee0d502350ab90c53467a10b540b7624422b594d2e72')
+    version('1.7.0', sha256='e4e642dfe496d2acb245c90e37dc18028e25d5e936421e7371ea6ba0fde6435a')
     version('1.5.1', sha256='c0357c62223fefcfc7a7565389dbd4db900623bf10eccf2bc8e87586ec59b38d')
+    version('1.5.0', sha256='3cb1b0e28f420232d894d89665d5c0c8241b99e56d806171f4faf5cdfec08ae1')
     version('1.4.0', sha256='25ae6114d5962c717cb87e3bc30a2f6eaa232b252cf8c51ba805b8f04664ae0d')
     version('1.3.0', sha256='b6021694adec9813842bad1987b837e439dabaf5b0dff9041201d238fca71fb4')
     version('1.2.3', sha256='7d727542927a926f488fa62d04e2c5728c72660f17f822e627f349285f295063')
