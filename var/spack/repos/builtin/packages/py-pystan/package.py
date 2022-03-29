2f0d2acaa92c9d7bdde7200c6c6f1cc7bd6e3a95
haralmha <haralmha@stud.ntnu.no>
2022-03-29 16:28:04 +0200

    py-pystan: Add new package

---
+++ var/spack/repos/builtin/packages/py-pystan/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyPystan(PythonPackage):
+    """PyStan is a Python interface to Stan, a package for Bayesian inference."""
+
+    homepage = "https://mc-stan.org/"
+    pypi     = "pystan/pystan-3.4.0.tar.gz"
+
+    maintainers = ['haralmha']
+
+    version('3.4.0', sha256='325e2fb0ab804555c05a603e0c9152ab11fcc3af01f3e9a9ff9fe9954b93184f')
+
+    depends_on('python@3.8:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
+    depends_on('py-poetry-core@1.0.0:', type='build')
+    depends_on('py-aiohttp@3.6:', type=('build', 'run'))
+    depends_on('py-httpstan@:4.6,4.8:', type=('build', 'run'))
+    depends_on('py-pysimdjson@3.2:', type=('build', 'run'))
+    depends_on('py-numpy@1.19:', type=('build', 'run'))
+    depends_on('py-clikit@0.6:', type=('build', 'run'))
