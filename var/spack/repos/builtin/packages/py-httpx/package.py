c2954a8894425e7f9cf4f5e01e4021ca7b72b383
Jen Herting <jen@herting.cc>
2022-03-30 14:05:40 -0400

    [py-httpx] fixed older version dependencies

--- var/spack/repos/builtin/packages/py-httpx/package.py
+++ var/spack/repos/builtin/packages/py-httpx/package.py
@@ -16,8 +16,25 @@ class PyHttpx(PythonPackage):
     version('0.15.2', sha256='713a2deaf96d85bbd4a1fbdf0edb27d6b4ee2c9aaeda8433042367e4b9e1628d')
     version('0.11.1', sha256='7d2bfb726eeed717953d15dddb22da9c2fcf48a4d70ba1456aa0a7faeda33cf7')
 
+    variant('http2', default=False, when='@0.15.2:', description='Enable http2 support')
+
+    depends_on('python@3.6:',               type=('build', 'run'))
     depends_on('py-setuptools',             type='build')
     depends_on('py-certifi',                type=('build', 'run'))
-    depends_on('py-sniffio',                type=('build', 'run'))
-    depends_on('py-httpcore@0.11:',         type=('build', 'run'))
-    depends_on('py-rfc3986+idna2008@1.3:1', type=('build', 'run'))
+    depends_on('py-httpcore@0.11.0:0.11',   type=('build', 'run'), when='@0.15.2:')
+
+    depends_on('py-sniffio@1.0:1',          type=('build', 'run'), when='@0.11.1')
+    depends_on('py-sniffio',                type=('build', 'run'), when='@0.15.2:')
+
+    depends_on('py-rfc3986@1.3:1',          type=('build', 'run'), when='@0.11.1')
+    depends_on('py-rfc3986+idna2008@1.3:1', type=('build', 'run'), when='@0.15.2:')
+
+    depends_on('py-h2@3.0:3',               type=('build', 'run'), when='@0.11.1')
+    depends_on('py-h2@3.0:3',               type=('build', 'run'), when='@0.15.2+http2')
+
+    # Version 0.11.1 only dependencies
+    depends_on('py-hstspreload',            type=('build', 'run'), when='@0.11.1')
+    depends_on('py-chardet@3.0:3',          type=('build', 'run'), when='@0.11.1')
+    depends_on('py-h11@0.8:0.9',            type=('build', 'run'), when='@0.11.1')
+    depends_on('py-idna@2.0:2',             type=('build', 'run'), when='@0.11.1')
+    depends_on('py-urllib3@1.0:1',          type=('build', 'run'), when='@0.11.1')
