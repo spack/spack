a6b7caaf69e7d2a7573b3da7ead51dd346a97994
Jen Herting <jen@herting.cc>
2022-03-30 13:49:21 -0400

    [py-hpack] py-wheel is implied by PythonPackage

--- var/spack/repos/builtin/packages/py-hpack/package.py
+++ var/spack/repos/builtin/packages/py-hpack/package.py
@@ -15,4 +15,3 @@ class PyHpack(PythonPackage):
     version('4.0.0', sha256='fc41de0c63e687ebffde81187a948221294896f6bdc0ae2312708df339430095')
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-wheel', type='build')
