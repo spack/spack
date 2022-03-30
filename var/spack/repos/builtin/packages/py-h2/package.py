e0b84ece60b816c64494297383c8ece8545e0bb2
Jen Herting <jen@herting.cc>
2022-03-30 13:58:15 -0400

    [py-h2] added version 3.2.0

--- var/spack/repos/builtin/packages/py-h2/package.py
+++ var/spack/repos/builtin/packages/py-h2/package.py
@@ -13,8 +13,11 @@ class PyH2(PythonPackage):
     pypi = "h2/h2-4.0.0.tar.gz"
 
     version('4.0.0', sha256='bb7ac7099dd67a857ed52c815a6192b6b1f5ba6b516237fc24a085341340593d')
+    version('3.2.0', sha256='875f41ebd6f2c44781259005b157faed1a5031df3ae5aa7bcb4628a6c0782f14')
 
-    depends_on('python@3.6.1:',       type=('build', 'run'))
+    depends_on('python@3.6.1:',       type=('build', 'run'), when='@4.0.0:')
     depends_on('py-setuptools',       type='build')
-    depends_on('py-hyperframe@6.0:6', type=('build', 'run'))
-    depends_on('py-hpack@4.0:4',      type=('build', 'run'))
+    depends_on('py-hyperframe@5.2:5', type=('build', 'run'), when='@3.2.0')
+    depends_on('py-hyperframe@6.0:6', type=('build', 'run'), when='@4.0.0')
+    depends_on('py-hpack@3.0:3',      type=('build', 'run'), when='@3.2.0')
+    depends_on('py-hpack@4.0:4',      type=('build', 'run'), when='@4.0.0')
