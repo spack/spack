b4fd6ddfc5807d0ca2031b016b746150a6ef14b4
Erik Schnetter <schnetter@gmail.com>
2022-03-30 08:27:54 -0400

    Update to 1.2.12
    Update to version 1.2.12.
    Mark older versions as deprecated because they have security bugs.

--- var/spack/repos/builtin/packages/zlib/package.py
+++ var/spack/repos/builtin/packages/zlib/package.py
@@ -19,11 +19,10 @@ class Zlib(Package):
     # URL must remain http:// so Spack can bootstrap curl
     url = "https://zlib.net/fossils/zlib-1.2.11.tar.gz"
 
-    version('1.2.11', sha256='c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1')
-    # Due to the bug fixes, any installations of 1.2.9 or 1.2.10 should be
-    # immediately replaced with 1.2.11.
-    version('1.2.8', sha256='36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d')
-    version('1.2.3', sha256='1795c7d067a43174113fdf03447532f373e1c6c57c08d61d9e4e9be5e244b05e')
+    version('1.2.12', sha256='91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9')
+    version('1.2.11', sha256='c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1', deprecated=True)
+    version('1.2.8', sha256='36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d', deprecated=True)
+    version('1.2.3', sha256='1795c7d067a43174113fdf03447532f373e1c6c57c08d61d9e4e9be5e244b05e', deprecated=True)
 
     variant('pic', default=True,
             description='Produce position-independent code (for shared libs)')
