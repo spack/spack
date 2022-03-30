391481b003a5b38fea9f20fc7dd5b7858d07a817
Jen Herting <jen@herting.cc>
2022-03-30 13:45:51 -0400

    [py-hyperframe] added version 5.2.0

--- var/spack/repos/builtin/packages/py-hyperframe/package.py
+++ var/spack/repos/builtin/packages/py-hyperframe/package.py
@@ -13,6 +13,7 @@ class PyHyperframe(PythonPackage):
     pypi = "hyperframe/hyperframe-6.0.0.tar.gz"
 
     version('6.0.0', sha256='742d2a4bc3152a340a49d59f32e33ec420aa8e7054c1444ef5c7efff255842f1')
+    version('5.2.0', sha256='a9f5c17f2cc3c719b917c4f33ed1c61bd1f8dfac4b1bd23b7c80b3400971b41f')
 
-    depends_on('python@3.6.1:', type=('build', 'run'))
+    depends_on('python@3.6.1:', type=('build', 'run'), when="@6:")
     depends_on('py-setuptools', type='build')
