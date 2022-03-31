4ed32a74685e7781c34430a839d84f03abb6c183
haralmha <47558670+haralmha@users.noreply.github.com>
2022-03-31 08:45:26 +0200

    Add run dependency to py-setuptools
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-pystan/package.py
+++ var/spack/repos/builtin/packages/py-pystan/package.py
@@ -17,7 +17,7 @@ class PyPystan(PythonPackage):
     version('3.4.0', sha256='325e2fb0ab804555c05a603e0c9152ab11fcc3af01f3e9a9ff9fe9954b93184f')
 
     depends_on('python@3.8:3', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-poetry-core@1.0.0:', type='build')
     depends_on('py-aiohttp@3.6:3', type=('build', 'run'))
     depends_on('py-httpstan@4.7', type=('build', 'run'))
