718f0a64740e40c145fb8c64a3e3169512e03528
Harmen Stoppels <harmenstoppels@gmail.com>
2022-04-02 17:49:48 +0200

    libbsd: can't compile with %nvhpc

--- var/spack/repos/builtin/packages/libbsd/package.py
+++ var/spack/repos/builtin/packages/libbsd/package.py
@@ -32,19 +32,10 @@ class Libbsd(AutotoolsPackage):
 
     patch('cdefs.h.patch', when='@0.8.6 %gcc@:4')
     patch('local-elf.h.patch', when='@:0.10 %intel')
-    patch('nvhpc.patch', when='%nvhpc')
+
+    conflicts('%nvhpc')
 
     # https://gitlab.freedesktop.org/libbsd/libbsd/issues/1
     conflicts('platform=darwin')
 
     depends_on('libmd', when='@0.11:')
-
-    def patch(self):
-        # Remove flags not recognized by the NVIDIA compiler
-        if self.spec.satisfies('%pgi') or self.spec.satisfies('%nvhpc'):
-            filter_file('-isystem', '-I', 'src/Makefile.in')
-            # This is not a 1 for 1 replacement, requiring nvhpc.patch
-            # to include config.h where needed
-            filter_file('-include ', '-I ', 'src/Makefile.in')
-            filter_file('-Wall -Wextra -Wno-unused-variable '
-                        '-Wno-unused-parameter', '-Wall', 'configure')
