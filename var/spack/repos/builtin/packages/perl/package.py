8ef299ed1696755e21fd90871d98d83badb81a01
Harmen Stoppels <harmenstoppels@gmail.com>
2022-04-04 08:33:09 +0200

    perl: conflict with unpatched releases for %nvhpc (#29854)

--- var/spack/repos/builtin/packages/perl/package.py
+++ var/spack/repos/builtin/packages/perl/package.py
@@ -109,6 +109,7 @@ class Perl(Package):  # Perl doesn't use Autotools, it should subclass Package
     # the mini Perl environment to bootstrap installation.
     patch('nvhpc-5.30.patch', when='@5.30.0:5.30 %nvhpc')
     patch('nvhpc-5.32.patch', when='@5.32.0:5.32 %nvhpc')
+    conflicts('@5.34.0:', when='%nvhpc')  # todo, add patches...
     conflicts('@5.32.0:', when='%nvhpc@:20.11',
               msg='The NVIDIA compilers are incompatible with version 5.32 and later')
 
