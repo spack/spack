1b4aa25173d9718b98e2b6af3a35afb56700f9ae
H. Joe Lee <hyoklee@hdfgroup.org>
2022-04-04 00:31:57 -0500

    Correct the PR error.
    I accidentally committed the wrong package.

--- var/spack/repos/builtin/packages/hermes/package.py
+++ var/spack/repos/builtin/packages/hermes/package.py
@@ -3,34 +3,59 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack import *
 
-import os
 
+class Hermes(CMakePackage):
+    """
+    Hermes is a heterogeneous-aware, multi-tiered, dynamic, and distributed
+    I/O buffering system that aims to significantly accelerate I/O performance.
+    """
 
-class Scipoptsuite(CMakePackage):
-    """The SCIP Optimization Suite is a toolbox for generating and
-    solving mixed integer nonlinear programs, in particular mixed
-    integer linear programs, and constraint integer programs
+    homepage = "http://www.cs.iit.edu/~scs/assets/projects/Hermes/Hermes.html"
+    git = "https://github.com/HDFGroup/hermes.git"
 
-    Note: A manual download is required for SCIP.  Spack will search
-    your current directory for the download file.  Alternatively,
-    add this file to a mirror so that Spack can find it.  For
-    instructions on how to set up a mirror, see
-    https://spack.readthedocs.io/en/latest/mirrors.html"""
+    maintainers = ['hyoklee']
 
-    homepage = "https://scipopt.org"
-    url = "https://github.com/scipopt/scip/archive/v703.tar.gz"
-    # url      = "file://{0}/scipoptsuite-8.0.0.tgz".format(os.getcwd())
-    # manual_download = True
+    version('master', branch='master')
+    version('0.4.0-beta', url="https://github.com/HDFGroup/hermes/archive/v0.4.0-be\
+ta.tar.gz", sha256='e8bd0b701149844f7f113a55a107e40b3579945d70ca2cb8ce9db5b5a131efd\
+b')
 
-    # version('8.0.0', sha256='971962f2d896b0c8b8fa554c18afd2b5037092685735d9494a05dc16d56ad422')
-    version('703', 'da4470adb2b95639c234f15a1febb5033c03ba7bfa6924f279eb4ec689730bd9')
-    depends_on('gmp')
-    depends_on('zlib')
-    depends_on('readline')
-    depends_on('ncurses')
+    variant('vfd', default=False, description='Enable HDF5 VFD')
+
+    depends_on('mochi-thallium~cereal@0.8.3')
+    depends_on('catch2@2.13.3')
+    depends_on('or-tools')
+    depends_on('mpich@3.3.2:')
+    depends_on('hdf5@1.13.0:', when='+vfd')
 
     def cmake_args(self):
-        cmake_args = []
-        cmake_args.append('-DPAPILO=OFF')
-        return cmake_args
+        args = ['-DCMAKE_INSTALL_PREFIX={}'.format(self.prefix),
+                '-DHERMES_RPC_THALLIUM=ON',
+                '-DHERMES_INSTALL_TESTS=ON',
+                '-DBUILD_TESTING=ON']
+        if '+vfd' in self.spec:
+        if '+vfd' in self.spec:
+            args.append(self.define('HERMES_ENABLE_VFD', 'ON'))
+        return args
+
+    def set_include(self, env, path):
+        env.append_flags('CFLAGS', '-I{}'.format(path))
+        env.append_flags('CXXFLAGS', '-I{}'.format(path))
+
+    def set_lib(self, env, path):
+        env.prepend_path('LD_LIBRARY_PATH', path)
+        env.append_flags('LDFLAGS', '-L{}'.format(path))
+
+    def set_flags(self, env):
+        self.set_include(env, '{}/include'.format(self.prefix))
+        self.set_include(env, '{}/include'.format(self.prefix))
+        self.set_lib(env, '{}/lib'.format(self.prefix))
+        self.set_lib(env, '{}/lib64'.format(self.prefix))
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        self.set_flags(spack_env)
+
+    def setup_run_environment(self, env):
+        self.set_flags(env)
