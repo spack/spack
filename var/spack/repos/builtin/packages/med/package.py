9c7d41a35bc3a8f23e9fcfc5f7e71cc79e5a0824
Mathieu Courtois <mathieu.courtois@edf.fr>
2022-04-05 14:37:44 +0200

    med: merge with 'salome-med', autotools is recommended and required to build with int64

--- var/spack/repos/builtin/packages/med/package.py
+++ var/spack/repos/builtin/packages/med/package.py
@@ -6,71 +6,85 @@
 from spack import *
 
 
-class Med(CMakePackage):
-    """The MED file format is a specialization of the HDF5 standard."""
+class Med(AutotoolsPackage):
+    """This package is the MED file format that is the SALOME platform standard
+    file for meshes and fields and med is based on HDF5 library."""
+
+    # This is a merge of the original 'med' and 'salome-med' packages.
+    # NOTE: README.cmake from sources recommends to use autotools build system.
 
     homepage = "https://docs.salome-platform.org/latest/dev/MEDCoupling/med-file.html"
     url = "https://files.salome-platform.org/Salome/other/med-3.2.0.tar.gz"
 
-    maintainers = ['likask']
+    maintainers = ["likask", "franciskloss", "mthcrts"]
 
-    # 4.1.0 does not compile in static mode
-    version('4.1.0', sha256='847db5d6fbc9ce6924cb4aea86362812c9a5ef6b9684377e4dd6879627651fce')
-    version('4.0.0', sha256='a474e90b5882ce69c5e9f66f6359c53b8b73eb448c5f631fa96e8cd2c14df004', preferred=True)
-    version('3.2.0', sha256='d52e9a1bdd10f31aa154c34a5799b48d4266dc6b4a5ee05a9ceda525f2c6c138')
+    version("4.1.0", sha256="847db5d6fbc9ce6924cb4aea86362812c9a5ef6b9684377e4dd6879627651fce")
+    version("4.0.0", sha256="a474e90b5882ce69c5e9f66f6359c53b8b73eb448c5f631fa96e8cd2c14df004")
+    version("3.3.1", sha256="856e9c4bb75eb0cceac3d5a5c65b1ce52fb3c46b9182920e1c9f34ae69bd2d5f")
+    version("3.2.0", sha256="d52e9a1bdd10f31aa154c34a5799b48d4266dc6b4a5ee05a9ceda525f2c6c138")
 
-    variant('api23', default=True, description='Enable API2.3')
-    variant('mpi', default=True, description='Enable MPI')
-    variant('shared', default=False,
-            description='Builds a shared version of the library')
-    variant('fortran', default=False, description='Enable Fortran support')
+    variant("mpi", default=False, description="Enable MPI")
+    variant("shared", default=True, description="Build MED-file shared libraries")
+    variant("fortran", default=False, description="Enable Fortran")
+    variant("int64", default=False, description="Set the default Fortran INTEGER size to 8")
+    variant("python", default=False, description="Enables python bindings")
+    variant("mesgerr", default=True, description="Enable display of library error messages")
+    variant("api23", default=True, description="Enable a complete MED 2.3.6 API")
 
-    depends_on('mpi', when='+mpi')
-    depends_on('hdf5@:1.8.22+mpi', when='@3.2.0+mpi')
-    depends_on('hdf5@1.10.2:1.10.7+mpi', when='@4.0.0:+mpi')
-    depends_on('hdf5@:1.8.22~mpi', when='@3.2.0~mpi')
-    depends_on('hdf5@1.10.2:1.10.7~mpi', when='@4.0.0:~mpi')
-    # the "TARGET hdf5" patch below only works with HDF5 shared library builds
-    depends_on('hdf5+shared', when='@4.0.0:4.1.99')
+    depends_on("mpi", when="+mpi")
+    depends_on("python", when="+python")
+    depends_on("swig", when="+python")
 
-    conflicts("@4.1.0", when="~shared", msg="Link error when static")
+    depends_on("hdf5~mpi", when="~mpi")
+    depends_on("hdf5+mpi", when="+mpi")
+
+    depends_on("hdf5@1.10.3", when="@4:")
+    depends_on("hdf5@1.8.14", when="@:3.4")
 
     # C++11 requires a space between literal and identifier
-    patch('add_space.patch', when='@3.2.0')
-    # fix problem where CMake "could not find TARGET hdf5"
-    patch('med-4.1.0-hdf5-target.patch', when='@4.0.0:4.1.99')
+    patch("add_space.patch", when="@3.2.0")
+    patch("MAJ_400_410_champs.patch", when="@4.1.0~shared", working_dir="./tools/medimport/4.0.0")
 
-    def cmake_args(self):
+    def check(self):
+        with working_dir(self.build_directory):
+            make("test", parallel=False)
+
+    def setup_build_environment(self, env):
         spec = self.spec
+        env.set("CC", spec["mpi"].mpicc)
+        env.set("CXX", spec["mpi"].mpicxx)
+        env.set("FC", spec["mpi"].mpifc)
+        env.set("F77", spec["mpi"].mpifc)
+        env.set("HDF5_ROOT_DIR", spec["hdf5"].prefix)
+        if "+fortran" in spec:
+            if "+int64" in spec:
+                env.append_flags("FFLAGS", "-fdefault-integer-8")
+                env.append_flags("FCFLAGS", "-fdefault-integer-8")
 
-        options = [
-            self.define('HDF5_ROOT_DIR', spec['hdf5'].prefix),
-            self.define('MEDFILE_BUILD_TESTS', self.run_tests),
-            self.define('MEDFILE_BUILD_PYTHON', False),
-            self.define('MEDFILE_INSTALL_DOC', False),
-        ]
-        if '~fortran' in spec:
-            options.append('-DCMAKE_Fortran_COMPILER=')
+    def configure_args(self):
+        spec = self.spec
+        args = ["--with-hdf5={0}".format(spec["hdf5"].prefix)]
 
-        if '+api23' in spec:
-            options.extend([
-                '-DCMAKE_CXX_FLAGS:STRING=-DMED_API_23=1',
-                '-DCMAKE_C_FLAGS:STRING=-DMED_API_23=1',
-                '-DMED_API_23=1'])
+        args += self.with_or_without("mpi")
+        args += self.enable_or_disable("shared")
+        args += self.enable_or_disable("python")
+        args += self.with_or_without("swig", variant="python")
+        args += self.enable_or_disable("mesgerr")
+        args += self.enable_or_disable("fortran")
+        args += self.enable_or_disable("api23")
 
-        if '+shared' in spec:
-            options.extend([
-                '-DMEDFILE_BUILD_SHARED_LIBS=ON',
-                '-DMEDFILE_BUILD_STATIC_LIBS=OFF',
-            ])
-        else:
-            options.extend([
-                '-DMEDFILE_BUILD_SHARED_LIBS=OFF',
-                '-DMEDFILE_BUILD_STATIC_LIBS=ON',
-            ])
+        if self.run_tests:
+            args.extend(["--enable-installtest"])
 
-        if '+mpi' in spec:
-            options.extend(['-DMEDFILE_USE_MPI=YES',
-                            '-DMPI_ROOT_DIR=%s' % spec['mpi'].prefix])
+        return args
 
-        return options
+    def setup_run_environment(self, env):
+        env.prepend_path("PATH", self.prefix.bin)
+        env.prepend_path(
+            "PYTHONPATH",
+            join_path(
+                self.prefix.lib,
+                "python{0}".format(self.spec["python"].version.up_to(2)),
+                "site-packages",
+            ),
+        )
