b46072ae72affc62da0a7536765236708863045c
Mathieu Courtois <mathieu.courtois@edf.fr>
2022-04-05 14:49:58 +0200

    salome-medcoupling: now depends on 'med', variant 'int64', use 'ptscocth' with '+mpi'

--- var/spack/repos/builtin/packages/salome-medcoupling/package.py
+++ var/spack/repos/builtin/packages/salome-medcoupling/package.py
@@ -3,142 +3,142 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 #
-# Important feature: a version of salome-medcoupling depand on
-# a specific version of salome-med package
+# Important feature: a version of salome-medcoupling depends on
+# a specific version of med package
 
 from spack import *
 
 
 class SalomeMedcoupling(CMakePackage):
     """salome-medcoupling is a part of SALOME platform to manipulate meshes and
-    fields in memory, and use salome-med format for files."""
+    fields in memory, and use med format for files."""
 
-    maintainers = ['franciskloss']
+    maintainers = ["franciskloss", "mthcrts"]
 
     homepage = "https://docs.salome-platform.org/latest/dev/MEDCoupling/developer/index.html"
-    git      = "https://git.salome-platform.org/gitpub/tools/medcoupling.git"
+    git = "https://git.salome-platform.org/gitpub/tools/medcoupling.git"
 
-    version('9.7.0', tag='V9_7_0')
-    version('9.6.0', tag='V9_6_0')
-    version('9.5.0', tag='V9_5_0')
-    version('9.4.0', tag='V9_4_0')
-    version('9.3.0', tag='V9_3_0')
+    version("9.7.0", tag="V9_7_0")
+    version("9.6.0", tag="V9_6_0")
+    version("9.5.0", tag="V9_5_0")
+    version("9.4.0", tag="V9_4_0")
+    version("9.3.0", tag="V9_3_0")
 
-    variant('static',      default=False, description='Enable static library build')
-    variant('mpi',         default=False, description='Enable MPI')
-    variant('in64',        default=False, description='Enable 64 bits indexes')
-    variant('partitioner', default=False, description='Enable partitioner')
-    variant('metis',       default=False, description='Enable Metis')
-    variant('scotch',      default=False, description='Enable Scotch')
+    variant("static", default=False, description="Enable static library build")
+    variant("mpi", default=False, description="Enable MPI")
+    variant("int64", default=False, description="Enable 64 bits indexes")
+    variant("metis", default=False, description="Enable Metis")
+    variant("scotch", default=False, description="Enable Scotch")
 
-    depends_on('libxml2@2.9.1:')
-    depends_on('libtirpc')
-    depends_on('cppunit')
-    depends_on('python@3.6.5:')
-    depends_on('py-scipy@0.19.1:', type=('build', 'run'))
-    depends_on('py-numpy@1.15.1:', type=('build', 'run'))
-    depends_on('boost+python+numpy@1.58.0:')
-    depends_on('swig@3.0.12:', type='build')
+    depends_on("libxml2@2.9.1:")
+    depends_on("libtirpc")
+    depends_on("cppunit")
+    depends_on("python@3.6.5:")
+    depends_on("py-scipy@0.19.1:", type=("build", "run"))
+    depends_on("py-numpy@1.15.1:", type=("build", "run"))
+    depends_on("boost+python+numpy@1.58.0:")
+    depends_on("swig@3.0.12:", type="build")
 
-    depends_on('metis@5.1.0:',  when='+metis')
-    depends_on('scotch@6.0.4:', when='+scotch')
-    depends_on('mpi',           when='+mpi')
+    depends_on("metis@5.1.0:", when="+metis")
+    depends_on("scotch@6.0.4:", when="+scotch")
+    depends_on("mpi", when="+mpi")
 
-    depends_on('salome-configuration@9.7.0',  when='@9.7.0')
-    depends_on('salome-med@4.1.0+mpi+static', when='@9.7.0+mpi+static')
-    depends_on('salome-med@4.1.0+mpi',        when='@9.7.0+mpi')
-    depends_on('salome-med@4.1.0+static',     when='@9.7.0~mpi+static')
-    depends_on('salome-med@4.1.0',            when='@9.7.0~mpi')
+    depends_on("salome-configuration@9.7.0", when="@9.7.0")
+    depends_on("med@4.1.0+mpi~shared", when="@9.7.0+mpi+static")
+    depends_on("med@4.1.0+mpi", when="@9.7.0+mpi")
+    depends_on("med@4.1.0~shared", when="@9.7.0~mpi+static")
+    depends_on("med@4.1.0", when="@9.7.0~mpi")
 
-    depends_on('salome-configuration@9.6.0',  when='@9.6.0')
-    depends_on('salome-med@4.1.0+mpi+static', when='@9.6.0+mpi+static')
-    depends_on('salome-med@4.1.0+mpi',        when='@9.6.0+mpi')
-    depends_on('salome-med@4.1.0+static',     when='@9.6.0~mpi+static')
-    depends_on('salome-med@4.1.0',            when='@9.6.0~mpi')
+    depends_on("salome-configuration@9.6.0", when="@9.6.0")
+    depends_on("med@4.1.0+mpi~shared", when="@9.6.0+mpi+static")
+    depends_on("med@4.1.0+mpi", when="@9.6.0+mpi")
+    depends_on("med@4.1.0~shared", when="@9.6.0~mpi+static")
+    depends_on("med@4.1.0", when="@9.6.0~mpi")
 
-    depends_on('salome-configuration@9.5.0',  when='@9.5.0')
-    depends_on('salome-med@4.1.0+mpi+static', when='@9.5.0+mpi+static')
-    depends_on('salome-med@4.1.0+mpi',        when='@9.5.0+mpi')
-    depends_on('salome-med@4.1.0+static',     when='@9.5.0~mpi+static')
-    depends_on('salome-med@4.1.0',            when='@9.5.0~mpi')
+    depends_on("salome-configuration@9.5.0", when="@9.5.0")
+    depends_on("med@4.1.0+mpi~shared", when="@9.5.0+mpi+static")
+    depends_on("med@4.1.0+mpi", when="@9.5.0+mpi")
+    depends_on("med@4.1.0~shared", when="@9.5.0~mpi+static")
+    depends_on("med@4.1.0", when="@9.5.0~mpi")
 
-    depends_on('salome-configuration@9.4.0',  when='@9.4.0')
-    depends_on('salome-med@4.0.0+mpi+static', when='@9.4.0+mpi+static')
-    depends_on('salome-med@4.0.0+mpi',        when='@9.4.0+mpi')
-    depends_on('salome-med@4.0.0+static',     when='@9.4.0~mpi+static')
-    depends_on('salome-med@4.0.0',            when='@9.4.0~mpi')
+    depends_on("salome-configuration@9.4.0", when="@9.4.0")
+    depends_on("med@4.0.0+mpi~shared", when="@9.4.0+mpi+static")
+    depends_on("med@4.0.0+mpi", when="@9.4.0+mpi")
+    depends_on("med@4.0.0~shared", when="@9.4.0~mpi+static")
+    depends_on("med@4.0.0", when="@9.4.0~mpi")
 
-    depends_on('salome-configuration@9.3.0',  when='@9.3.0')
-    depends_on('salome-med@4.0.0+mpi+static', when='@9.3.0+mpi+static')
-    depends_on('salome-med@4.0.0+mpi',        when='@9.3.0+mpi')
-    depends_on('salome-med@4.0.0+static',     when='@9.3.0~mpi+static')
-    depends_on('salome-med@4.0.0',            when='@9.3.0~mpi')
+    depends_on("salome-configuration@9.3.0", when="@9.3.0")
+    depends_on("med@4.0.0+mpi~shared", when="@9.3.0+mpi+static")
+    depends_on("med@4.0.0+mpi", when="@9.3.0+mpi")
+    depends_on("med@4.0.0~shared", when="@9.3.0~mpi+static")
+    depends_on("med@4.0.0", when="@9.3.0~mpi")
 
     def check(self):
         pass
 
     def setup_build_environment(self, env):
-        if '+metis' in self.spec:
-            env.set('METIS_ROOT_DIR', self.spec['metis'].prefix)
+        spec = self.spec
+        if "+metis" in spec:
+            env.set("METIS_ROOT_DIR", spec["metis"].prefix)
 
-        if '+scotch' in self.spec:
-            env.set('SCOTCH_ROOT_DIR', self.spec['scotch'].prefix)
+        if "+scotch" in spec:
+            name = "PTSCOTCH" if "+mpi" in spec else "SCOTCH"
+            env.set(name + "_ROOT_DIR", spec["scotch"].prefix)
+            env.set(name + "_INCLUDES_DIR", join_path(spec["scotch"].prefix, "include"))
 
     def setup_run_environment(self, env):
         env.prepend_path(
-            'PYTHONPATH',
+            "PYTHONPATH",
             join_path(
                 self.prefix.lib,
-                'python{0}'.format(self.spec['python'].version.up_to(2)),
-                'site-packages'))
+                "python{0}".format(self.spec["python"].version.up_to(2)),
+                "site-packages",
+            ),
+        )
 
     def cmake_args(self):
-        spec    = self.spec
+        spec = self.spec
         options = []
 
-        if '+static' in spec:
-            options.extend(['-DMEDCOUPLING_BUILD_STATIC=ON'])
+        if "+static" in spec:
+            options.extend(["-DMEDCOUPLING_BUILD_STATIC=ON"])
         else:
-            options.extend(['-DMEDCOUPLING_BUILD_STATIC=OFF'])
+            options.extend(["-DMEDCOUPLING_BUILD_STATIC=OFF"])
 
-        if '+mpi' in spec:
-            options.extend([
-                '-DMEDCOUPLING_USE_MPI=ON',
-                '-DSALOME_USE_MPI=ON'])
+        if "+mpi" in spec:
+            options.extend(["-DMEDCOUPLING_USE_MPI=ON", "-DSALOME_USE_MPI=ON"])
         else:
-            options.extend([
-                '-DMEDCOUPLING_USE_MPI=OFF',
-                '-DSALOME_USE_MPI=OFF'])
+            options.extend(["-DMEDCOUPLING_USE_MPI=OFF", "-DSALOME_USE_MPI=OFF"])
 
-        if '+in64' in spec:
-            options.extend(['-DMEDCOUPLING_USE_64BIT_IDS=ON'])
+        if "+int64" in spec:
+            options.extend(["-DMEDCOUPLING_USE_64BIT_IDS=ON"])
         else:
-            options.extend(['-DMEDCOUPLING_USE_64BIT_IDS=OFF'])
+            options.extend(["-DMEDCOUPLING_USE_64BIT_IDS=OFF"])
 
-        if '+partitioner' in spec:
-            options.extend(['-DMEDCOUPLING_ENABLE_PARTITIONER=ON'])
+        if "+metis" in spec:
+            options.extend(["-DMEDCOUPLING_PARTITIONER_METIS=ON"])
         else:
-            options.extend(['-DMEDCOUPLING_ENABLE_PARTITIONER=OFF'])
+            options.extend(["-DMEDCOUPLING_PARTITIONER_METIS=OFF"])
 
-        if '+metis' in spec:
-            options.extend(['-DMEDCOUPLING_ENABLE_PARTITIONER=ON'])
-            options.extend(['-DMEDCOUPLING_PARTITIONER_METIS=ON'])
+        name = "PTSCOTCH" if "+mpi" in spec else "SCOTCH"
+        other = "SCOTCH" if name == "PTSCOTCH" else "PTSCOTCH"
+        if "+scotch" in spec:
+            options.extend(["-DMEDCOUPLING_PARTITIONER_{0}=ON".format(name)])
         else:
-            options.extend(['-DMEDCOUPLING_PARTITIONER_METIS=OFF'])
+            options.extend(["-DMEDCOUPLING_PARTITIONER_{0}=OFF".format(name)])
+        options.extend(["-DMEDCOUPLING_PARTITIONER_{0}=OFF".format(other)])
 
-        if '+scotch' in spec:
-            options.extend(['-DMEDCOUPLING_ENABLE_PARTITIONER=ON'])
-            options.extend(['-DMEDCOUPLING_PARTITIONER_SCOTCH=ON'])
-        else:
-            options.extend(['-DMEDCOUPLING_PARTITIONER_SCOTCH=OFF'])
-
-        options.extend([
-            '-DMEDCOUPLING_BUILD_DOC=OFF',
-            '-DMEDCOUPLING_ENABLE_PYTHON=ON',
-            '-DMEDCOUPLING_ENABLE_RENUMBER=OFF',
-            '-DMEDCOUPLING_PARTITIONER_PARMETIS=OFF',
-            '-DMEDCOUPLING_PARTITIONER_PTSCOTCH=OFF',
-            '-DMEDCOUPLING_MICROMED=OFF',
-            '-DMEDCOUPLING_BUILD_TESTS=OFF'])
+        options.extend(
+            [
+                "-DSALOME_CMAKE_DEBUG=OFF",
+                "-DMEDCOUPLING_BUILD_DOC=OFF",
+                "-DMEDCOUPLING_ENABLE_PYTHON=ON",
+                "-DMEDCOUPLING_ENABLE_RENUMBER=OFF",
+                "-DMEDCOUPLING_PARTITIONER_PARMETIS=OFF",
+                "-DMEDCOUPLING_MICROMED=OFF",
+                "-DMEDCOUPLING_BUILD_TESTS=OFF",
+                "-DMEDCOUPLING_MEDLOADER_USE_XDR=OFF",
+            ]
+        )
 
         return options
