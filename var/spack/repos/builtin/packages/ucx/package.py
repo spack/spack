37857ed354dffbe008c1065fe36f20a29b98b36a
iarspider <iarspider@gmail.com>
2022-04-07 18:07:51 +0200

    Apply compiler-opt flag

--- var/spack/repos/builtin/packages/ucx/package.py
+++ var/spack/repos/builtin/packages/ucx/package.py
@@ -159,6 +159,8 @@ class Ucx(AutotoolsPackage, CudaPackage):
             if 'rdma-core' in self.spec else None
 
         config_args.extend(self.enable_or_disable('optimizations'))
+        config_args.append('--enable-compiler-opt=' + \
+                           self.spec.variant['opt'].value)
         config_args.extend(self.enable_or_disable('assertions'))
         config_args.extend(self.enable_or_disable('logging'))
 
