44c4e4b21ecd2f6049afcfd973c1df63f0c8ceca
Joseph C Wang <joequant@gmail.com>
2022-04-10 14:31:10 +0800

    update with 0.8.1

--- var/spack/repos/builtin/packages/davix/package.py
+++ var/spack/repos/builtin/packages/davix/package.py
@@ -10,7 +10,7 @@ class Davix(CMakePackage):
     """High-performance file management over WebDAV/HTTP."""
 
     homepage = "https://davix.web.cern.ch/davix/docs/devel/index.html"
-    url      = "https://github.com/cern-fts/davix/releases/download/R_0_7_6/davix-0.7.6.tar.gz"
+    url      = "https://github.com/cern-fts/davix/releases/download/R_0_8_1/davix-0.8.1.tar.gz"
 
     version('0.8.1', sha256='3f42f4eadaf560ab80984535ffa096d3e88287d631960b2193e84cf29a5fe3a6')
     version('0.8.0', sha256='2f108da0408a83fb5b9f0c68150d360ba733e4b3a0fe298d45b0d32d28ab7124')
