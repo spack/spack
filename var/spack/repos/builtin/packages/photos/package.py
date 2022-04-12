fc9470da84195511f99fb4acd3a26290ba820777
Valentin Volkl <valentin.volkl@cern.ch>
2022-04-12 13:35:28 +0200

    photos: switch default from hepmc2 to hepmc3

--- var/spack/repos/builtin/packages/photos/package.py
+++ var/spack/repos/builtin/packages/photos/package.py
@@ -18,8 +18,8 @@ class Photos(AutotoolsPackage):
     version('3.64', sha256='cb4096b4804289fc4d54a992caa566cbbd33f41f65f8906deb01200dc5163027')
     version('3.61', sha256='acd3bcb769ba2a3e263de399e9b89fd6296405c9cbc5045b83baba3e60db4b26')
 
-    variant('hepmc', default=True, description='Build with HepMC2 support')
-    variant('hepmc3', default=False, description='Build with HepMC3 support')
+    variant('hepmc', default=False, description='Build with HepMC2 support')
+    variant('hepmc3', default=True, description='Build with HepMC3 support')
 
     maintainers = ['vvolkl']
 
