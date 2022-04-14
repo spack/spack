727555a8ac883e7fdbb841296bf240d4954dc39c
marcost2 <marcostolcachir@gmail.com>
2022-04-14 17:29:58 -0300

    Also set FCFLAGS for gcc@10
    * Otherwise the error will still trigger when buiding

--- var/spack/repos/builtin/packages/mpich/package.py
+++ var/spack/repos/builtin/packages/mpich/package.py
@@ -330,6 +330,7 @@ with '-Wl,-commons,use_dylibs' and without
         # https://bugzilla.redhat.com/show_bug.cgi?id=1795817
         if self.spec.satisfies('%gcc@10:'):
             env.set('FFLAGS', '-fallow-argument-mismatch')
+            env.set('FCFLAGS', '-fallow-argument-mismatch')
         # Same fix but for macOS - avoids issue #17934
         if self.spec.satisfies('%apple-clang@11:'):
             env.set('FFLAGS', '-fallow-argument-mismatch')
