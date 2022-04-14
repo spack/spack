4a0789423b0493cf7d484266c366e103655db271
Veselin Dobrev <dobrev@llnl.gov>
2022-04-13 19:38:53 -0700

    LIBCEED: add missing 'install' target in 'install_targets'

--- var/spack/repos/builtin/packages/libceed/package.py
+++ var/spack/repos/builtin/packages/libceed/package.py
@@ -138,7 +138,8 @@ class Libceed(MakefilePackage, CudaPackage, ROCmPackage):
 
     @property
     def install_targets(self):
-        return ['prefix={0}'.format(self.prefix)] + self.common_make_opts
+        return ['install', 'prefix={0}'.format(self.prefix)] + \
+            self.common_make_opts
 
     def check(self):
         make('prove', *self.common_make_opts, parallel=False)
