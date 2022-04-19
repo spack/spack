c227d408632f64e74f8e16bc21d106845bfa5b18
H. Joe Lee <hyoklee@hdfgroup.org>
2022-04-19 10:15:56 -0500

    Restore the changes that @sethrj recommended.

--- var/spack/repos/builtin/packages/hermes/package.py
+++ var/spack/repos/builtin/packages/hermes/package.py
@@ -14,47 +14,25 @@ class Hermes(CMakePackage):
 
     homepage = "http://www.cs.iit.edu/~scs/assets/projects/Hermes/Hermes.html"
     git = "https://github.com/HDFGroup/hermes.git"
-
     maintainers = ['hyoklee']
 
     version('master', branch='master')
-    version('0.4.0-beta', url="https://github.com/HDFGroup/hermes/archive/v0.4.0-be\
-ta.tar.gz", sha256='e8bd0b701149844f7f113a55a107e40b3579945d70ca2cb8ce9db5b5a131efd\
-b')
+    version('0.4.0-beta', url="https://github.com/HDFGroup/hermes/archive/v0.4.0-beta.tar.gz", sha256='e8bd0b701149844f7f113a55a107e40b3579945d70ca2cb8ce9db5b5a131efdb')
 
     variant('vfd', default=False, description='Enable HDF5 VFD')
 
-    depends_on('mochi-thallium~cereal@0.8.3')
-    depends_on('catch2@2.13.3')
+    depends_on('mochi-thallium~cereal@0.8:')
+    depends_on('catch2@2.13:')
     depends_on('or-tools')
-    depends_on('mpich@3.3.2:')
+    depends_on('mpi')
     depends_on('hdf5@1.13.0:', when='+vfd')
 
     def cmake_args(self):
-        args = ['-DCMAKE_INSTALL_PREFIX={}'.format(self.prefix),
-                '-DHERMES_RPC_THALLIUM=ON',
-                '-DHERMES_INSTALL_TESTS=ON',
-                '-DBUILD_TESTING=ON']
-        if '+vfd' in self.spec:
-            args.append(self.define('HERMES_ENABLE_VFD', 'ON'))
+        args = [
+            self.define('CMAKE_INSTALL_PREFIX', self.prefix),
+            self.define('HERMES_RPC_THALLIUM', True),
+            self.define('HERMES_INSTALL_TESTS', True),
+            self.define('BUILD_TESTING', True),
+            self.define_from_variant('HERMES_ENABLE_VFD', 'vfd'),
+        ]
         return args
-
-    def set_include(self, env, path):
-        env.append_flags('CFLAGS', '-I{}'.format(path))
-        env.append_flags('CXXFLAGS', '-I{}'.format(path))
-
-    def set_lib(self, env, path):
-        env.prepend_path('LD_LIBRARY_PATH', path)
-        env.append_flags('LDFLAGS', '-L{}'.format(path))
-
-    def set_flags(self, env):
-        self.set_include(env, '{}/include'.format(self.prefix))
-        self.set_include(env, '{}/include'.format(self.prefix))
-        self.set_lib(env, '{}/lib'.format(self.prefix))
-        self.set_lib(env, '{}/lib64'.format(self.prefix))
-
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        self.set_flags(spack_env)
-
-    def setup_run_environment(self, env):
-        self.set_flags(env)
