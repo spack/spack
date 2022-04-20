97fd7750c5b97958a69b6b97f820abac28872baa
EthanS94 <ethan.stam@gmail.com>
2022-04-20 08:49:27 -0600

    py-cinemasci: new version 1.7.0

--- var/spack/repos/builtin/packages/py-cinemasci/package.py
+++ var/spack/repos/builtin/packages/py-cinemasci/package.py
@@ -11,12 +11,13 @@ class PyCinemasci(PythonPackage):
     databases"""
 
     homepage = "https://github.com/cinemascience"
-    pypi = "cinemasci/cinemasci-1.3.tar.gz"
+    pypi = "cinemasci/cinemasci-1.7.0.tar.gz"
 
     tags = ['e4s']
 
     maintainers = ['EthanS94']
 
+    version('1.7.0', sha256='70e1fa494bcbefdbd9e8859cdf1b01163a94ecffcdfa3da1011e4ef2fcee6169', preferred=True)
     version('1.3', sha256='c024ca9791de9d78e5dad3fd11e8f87d8bc1afa5830f2697d7ec4116a5d23c20')
 
     variant('mpi', default=False, description='Enable MPI')
@@ -29,5 +30,8 @@ class PyCinemasci(PythonPackage):
     depends_on('py-h5py+mpi', when='+mpi', type=('build', 'run'))
     depends_on('py-ipywidgets', type=('build', 'run'))
     depends_on('py-jupyterlab', type=('build', 'run'))
+    depends_on('py-matplotlib', type=('build', 'run'), when='@1.7.0:')
     depends_on('py-pandas', type=('build', 'run'))
+    depends_on('py-scikit-image', type=('build', 'run'), when='@1.7.0:')
+    depends_on('py-scipy', type=('build', 'run'), when='@1.7.0:')
     depends_on('py-setuptools', type=('build'))
