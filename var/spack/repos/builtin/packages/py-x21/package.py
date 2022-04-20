0555d2e536b8739bbb8170ad76cc1803902a6265
Jen Herting <jen@herting.cc>
2022-04-20 15:17:53 -0400

    [py-x21]
    - added homepage
    - added placeholder description
    - added comment about checksums

--- var/spack/repos/builtin/packages/py-x21/package.py
+++ var/spack/repos/builtin/packages/py-x21/package.py
@@ -7,15 +7,18 @@ from spack import *
 
 
 class PyX21(PythonPackage):
+    """FIXME: I think this is for unpacking this authors
+    obfuscaded python libraries."""
 
+    homepage = "https://pypi.org/project/x21/"
     list_url = "https://pypi.org/simple/x21/"
 
     def url_for_version(self, version):
         url = "https://pypi.io/packages/cp{1}/x/x21/x21-{0}-cp{1}-cp{1}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
         return url.format(version, self.spec['python'].version.up_to(2).joined)
 
-    # version('0.2.6', sha256='7c5c58ff6dc81caac6815578f78cf545e719beb0bf4017f77120d38025d2bc7d', expand=False)
-    # version('0.2.4', sha256='83a7f3bb904a2564ff24da3d8b24bc3871fadc0db53e88aa1ee0a63a52af3c38', expand=False)
+    # NOTE: Due to the this package having different packages for each
+    # version of python, checksums have not been provided. 
     version('0.2.6', expand=False)
 
     depends_on('python@3.7:', type=('build', 'run'))
