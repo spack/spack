b20c8f016f157c532219131058bbb7480a11bf4c
Jen Herting <jen@herting.cc>
2022-04-20 16:05:33 -0400

    [py-x21] added darwin support and fixed issue with python 3.7 wheel name

--- var/spack/repos/builtin/packages/py-x21/package.py
+++ var/spack/repos/builtin/packages/py-x21/package.py
@@ -14,11 +14,20 @@ class PyX21(PythonPackage):
     list_url = "https://pypi.org/simple/x21/"
 
     def url_for_version(self, version):
-        url = "https://pypi.io/packages/cp{1}/x/x21/x21-{0}-cp{1}-cp{1}-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
-        return url.format(version, self.spec['python'].version.up_to(2).joined)
+        url = "https://pypi.io/packages/cp{1}/x/x21/x21-{0}-cp{1}-cp{1}{2}-{3}.whl"
+
+        platform_string = {
+            'linux': "manylinux_2_17_x86_64.manylinux2014_x86_64",
+            'darwin': "macosx_10_9_x86_64"
+        }
+
+        return url.format(version,
+                          self.spec['python'].version.up_to(2).joined,
+                          'm' if self.spec.satisfies("^python@3.7.0:3.7") else '',
+                          platform_string[self.spec.platform])
 
     # NOTE: Due to the this package having different packages for each
-    # version of python, checksums have not been provided. 
+    # version of python and platform, checksums have not been provided.
     version('0.2.6', expand=False)
 
     depends_on('python@3.7:', type=('build', 'run'))
