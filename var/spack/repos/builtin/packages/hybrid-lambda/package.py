f8fb24a85d03d5de64428e0ac6443b1c9bed7267
QianJianhua1 <qianjh@fujitsu.com>
2022-04-21 16:24:43 +0800

    hybrid-lambda: fix test

--- var/spack/repos/builtin/packages/hybrid-lambda/package.py
+++ var/spack/repos/builtin/packages/hybrid-lambda/package.py
@@ -33,9 +33,7 @@ class HybridLambda(AutotoolsPackage):
     # for instance depends_on('boost +filesystem')
     # See https://github.com/spack/spack/pull/22303 for reference
     depends_on(Boost.with_default_variants)
-    depends_on('cppunit', type='test')
-
-    build_directory = 'src'
+    depends_on('cppunit', type=('test', 'link'))
 
     @run_after('configure')
     def change_install_option_in_makefile(self):
