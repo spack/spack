8bee787ea0247f162d23877bbf978c8719a2029e
Lhxone <lhxone@outlook.com>
2022-04-21 20:52:11 +0800

    Update package.py

--- var/spack/repos/builtin/packages/cans/package.py
+++ var/spack/repos/builtin/packages/cans/package.py
@@ -25,8 +25,8 @@ class Cans(MakefilePackage):
     version('1.1.2', sha256='31c8d6c1f619fb60b7919922c7a3a64dd614a1a2f89f38560184f75ed0526171')
     version('1.1.0', sha256='e3fd84902e18715c6476fe780e2395ca04db9e6b0c830b55a7aa9204b1fd0886')
 
-    depends_on('openmpi', type=('build', 'run'))
-    depends_on('fftw', type=('build', 'run'))
+    depends_on('mpi')
+    depends_on('fftw')
 
     def edit(self, spec, prefix):
         with working_dir('src'):
