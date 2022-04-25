6767c80c580b935918cbb67c1faa4188ab4633d0
snehring <snehring@iastate.edu>
2022-04-25 16:48:05 -0500

    gcta: new recipe and version update

--- var/spack/repos/builtin/packages/gcta/package.py
+++ var/spack/repos/builtin/packages/gcta/package.py
@@ -3,27 +3,89 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install gcta
+#
+# You can edit this file again by typing:
+#
+#     spack edit gcta
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
 from spack import *
 
 
-class Gcta(Package):
-
+class Gcta(CMakePackage):
     """GCTA (Genome-wide Complex Trait Analysis) was originally designed to
     estimate the proportion of phenotypic variance explained by all genome-wide
     SNPs for complex traits (the GREML method), and has subsequently extended
     for many other analyses to better understand the genetic architecture of
     complex traits. GCTA currently supports the following analyses."""
 
-    homepage = "https://cnsgenomics.com/software/gcta/#Overview"
-    url      = "http://cnsgenomics.com/software/gcta/gcta_1.91.2beta.zip"
+    homepage = "https://github.com/jianyangqt/gcta"
+    url      = "https://github.com/jianyangqt/gcta/archive/refs/tags/v1.91.2.tar.gz"
+    git      = "https://github.com/jianyangqt/gcta.git"
+    maintainers = ['snehring']
 
-    version('1.91.2beta_mac', 'ce0882ad35dd9474ffe40911da369274700af1ecb9916c0a355b7bad14850234')
-    version('1.91.2beta', '192efb767be1c7ca9c2dac5d2c2317a97c7a9db1f801168d19ad2a51b98d9b10', preferred=True)
+    version('1.94.0beta',  commit='746e3975ddb463fc7bd15b03c6cc64b023eca497', submodules=True)
+    version('1.91.2',      sha256='0609d0fba856599a2acc66adefe87725304117acc226360ec2aabf8a0ac64e85')
 
-    conflicts('@1.91.2beta', when='platform=darwin')
-    conflicts('@1.91.2beta_mac', when='platform=linux')
+    conflicts('target=aarch64:', when='@:1.91.2', msg='aarch64 support added in 1.94.0')
+
+    depends_on('cmake@3.1:', type='build')
+    depends_on('intel-mkl@2017:', type=('build', 'link'), when='target=x86_64:')
+    depends_on('openblas', type=('build', 'link'), when='target=aarch64:')
+    depends_on('eigen@3.3.1', type=('build', 'link'), when='@1.91.2')
+    depends_on('eigen@3.3.7:', type=('build', 'link'), when='@1.94.0beta:')
+    depends_on('boost@1.4:', type=('build', 'link'), when='@1.94.0beta:')
+    depends_on('zlib', type=('build', 'link'))
+    depends_on('sqlite@3.3.1:', type=('build', 'link'), when='@1.94.0beta:')
+    depends_on('zstd@1.4.4:', type=('build', 'link'), when='@1.94.0beta:')
+    depends_on('spectra', type=('build', 'link'), when='@1.94.0beta:')
+    depends_on('gsl', type=('build', 'link'), when='@1.94.0beta:')
+
+    def patch(self):
+        # allow us to specify the locations with cmake_args
+        strings = [
+            'SET(EIGEN3_INCLUDE_DIR "$ENV{EIGEN3_INCLUDE_DIR}")',
+            'SET(SPECTRA_LIB "$ENV{SPECTRA_LIB}")',
+            'SET(BOOST_LIB "$ENV{BOOST_LIB}")',
+            'SET(OPENBLAS "$ENV{OPENBLAS}")',
+            'SET(MKLROOT "$ENV{MKLROOT}")'
+        ]
+        for s in strings:
+            filter_file(s, '', 'CMakeLists.txt', string=True)
+
+    def cmake_args(self):
+        eigen = self.spec['eigen'].prefix.include
+        args = [self.define('EIGEN3_INCLUDE_DIR', eigen)]
+        if self.spec.satisfies('@1.94.0beta:'):
+            spectra = self.spec['spectra'].prefix.include
+            boost = self.spec['boost'].prefix.include
+            deps = [
+                self.define('SPECTRA_LIB', spectra),
+                self.define('BOOST_LIB', boost),
+            ]
+            args.extend(deps)
+
+        if self.spec.satisfies('target=x86_64:'):
+            mkl = self.spec['intel-mkl'].prefix
+            args.append(self.define('MKLROOT', mkl))
+        elif self.spec.satisfies('target=aarch64:'):
+            openblas = self.spec['openblas'].prefix
+            args.append(self.define('OPENBLAS', openblas))
+
+        return args
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        install('gcta64', join_path(prefix.bin, 'gcta64'))
-        set_executable(join_path(prefix.bin, 'gcta64'))
+        with working_dir(self.build_directory):
+            install('gcta64', join_path(prefix.bin, 'gcta64'))
