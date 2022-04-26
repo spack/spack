8b10060b762fce1c357310f38d95032c5d77efde
Cody J. Balos <balos1@llnl.gov>
2022-04-26 11:21:34 -0700

    make xsdk-examples a cuda package

--- var/spack/repos/builtin/packages/xsdk-examples/package.py
+++ var/spack/repos/builtin/packages/xsdk-examples/package.py
@@ -7,7 +7,7 @@
 from spack import *
 
 
-class XsdkExamples(CMakePackage):
+class XsdkExamples(CMakePackage, CudaPackage):
     """xSDK Examples show usage of libraries in the xSDK package."""
 
     homepage = 'http://xsdk.info'
@@ -21,50 +21,61 @@ class XsdkExamples(CMakePackage):
     version('0.2.0', sha256='cf26e3a16a83eba6fb297fb106b0934046f17cf978f96243b44d9d17ad186db6')
     version('0.1.0', sha256='d24cab1db7c0872b6474d69e598df9c8e25d254d09c425fb0a6a8d6469b8018f')
 
-    variant('cuda', default=False, description='Compile CUDA examples')
-
     depends_on('xsdk+cuda', when='+cuda')
     depends_on('xsdk@0.7.0', when='@0.3.0')
     depends_on('xsdk@0.6.0', when='@0.2.0')
     depends_on('xsdk@0.5.0', when='@0.1.0')
+    depends_on('mpi')
 
     def cmake_args(self):
+        #define = CMakePackage.define
+        #from_variant = self.define_from_variant
         spec = self.spec
+
         args = [
             '-DCMAKE_C_COMPILER=%s' % spec['mpi'].mpicc,
-            '-DMETIS_INCLUDE_DIRS=%s' % spec['metis'].prefix.include,
-            '-DMETIS_LIBRARY=%s' % spec['metis'].libs,
-            '-DMPI_DIR=%s' % spec['mpi'].prefix,
-            '-DSUNDIALS_DIR=%s'      % spec['sundials'].prefix,
-            '-DHYPRE_DIR=%s'         % spec['hypre'].prefix,
-            '-DHYPRE_INCLUDE_DIR=%s' % spec['hypre'].prefix.include,
-            '-DPETSC_DIR=%s'         % spec['petsc'].prefix,
-            '-DPETSC_INCLUDE_DIR=%s' % spec['petsc'].prefix.include,
-            '-DPETSC_LIBRARY_DIR=%s' % spec['petsc'].prefix.lib,
-            '-DSUPERLUDIST_DIR=%s' % spec['superlu-dist'].prefix,
-            '-DSUPERLUDIST_INCLUDE_DIR=%s' %
-            spec['superlu-dist'].prefix.include,
-            '-DSUPERLUDIST_LIBRARY_DIR=%s' % spec['superlu-dist'].prefix.lib,
-            '-DSUPERLUDIST_LIBRARY=%s' % spec['superlu-dist'].libs,
+            '-DCMAKE_CXX_COMPILER=%s' % spec['mpi'].mpicxx,
+            '-DENABLE_HYPRE=ON',
+            '-DHYPRE_DIR=%s' % spec['hypre'].prefix,
+            '-DENABLE_MFEM=ON',
+            '-DMETIS_DIR=%s' % spec['metis'].prefix,
             '-DMFEM_DIR=%s' % spec['mfem'].prefix,
-            '-DMFEM_INCLUDE_DIR=%s' % spec['mfem'].prefix.include,
-            '-DMFEM_LIBRARY_DIR=%s' % spec['mfem'].prefix.include.lib,
-            '-DGINKGO_DIR=%s' % spec['ginkgo'].prefix,
-            '-DGINKGO_INCLUDE_DIR=%s' % spec['ginkgo'].prefix.include,
-            '-DGINKGO_LIBRARY_DIR=%s' % spec['ginkgo'].prefix.include.lib,
-            # allow use of default `find_package(Ginkgo)`
-            '-DCMAKE_PREFIX_PATH=%s/cmake' % spec['ginkgo'].prefix.include.lib
+            '-DENABLE_PETSC=ON',
+            '-DPETSc_DIR=%s' % spec['petsc'].prefix,
+            '-DENABLE_SUNDIALS=ON',
+            '-DSUNDIALS_DIR=%s' % spec['sundials'].prefix,
+            '-DENABLE_SUPERLUDIST=ON',
+            '-DSUPERLUDIST_DIR=%s' % spec['superlu-dist'].prefix
         ]
-        if '+cuda' in spec:
+
+        if '+cuda' in spec: # if cuda variant was activated for xsdk
+                # '-DCMAKE_CUDA_ARCHITECTURES=%s' % spec.variants['cuda_arch'].value
             args.extend([
-                '-DENABLE_CUDA=ON'
-            ])
-        if 'trilinos' in spec:  # if trilinos variant was activated for xsdk
+                '-DENABLE_CUDA=ON',
+                '-DCMAKE_CUDA_ARCHITECTURES=70'
+                ])
+        if '+ginkgo' in spec: # if ginkgo variant was activated for xsdk
             args.extend([
-                '-DTRILINOS_DIR_PATH=%s' % spec['trilinos'].prefix,
+                '-DENABLE_GINKGO=ON',
+                '-DGINKGO_DIR=%s' % spec['ginkgo'].prefix
             ])
+        if '+magma' in spec: # if magma variant was activated for xsdk
+            args.extend([
+                '-DENABLE_MAGMA=ON',
+                '-DMAGMA_DIR=%s' % spec['magma'].prefix
+            ])
+        if '+strumpack' in spec: # if magma variant was activated for xsdk
+            args.extend([
+                '-DENABLE_STRUMPACK=ON',
+                '-DSTRUMPACK_DIR=%s' % spec['strumpack'].prefix
+            ])
+        # if 'trilinos' in spec: # if trilinos variant was activated for xsdk
+        # if 'trilinos' in spec: # if trilinos variant was activated for xsdk
+        #     args.extend([
+        #         'ENABLE_TRILINOS=ON',
+        #         '-DTRILINOS_DIR_PATH=%s' % spec['trilinos'].prefix
+        #     ])
         if 'zlib' in spec:  # if zlib variant was activated for MFEM
-            args.extend([
-                '-DZLIB_LIBRARY_DIR=%s' % spec['zlib'].prefix.lib,
-            ])
+            args.append('-DZLIB_LIBRARY_DIR=%s' % spec['zlib'].prefix.lib)
+
         return args
