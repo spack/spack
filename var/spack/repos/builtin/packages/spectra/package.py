df3eba7a89d596422509330431cdd6b3bfad12de
snehring <snehring@iastate.edu>
2022-04-25 20:24:08 -0500

    fix boilerplate

--- var/spack/repos/builtin/packages/spectra/package.py
+++ var/spack/repos/builtin/packages/spectra/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install spectra
-#
-# You can edit this file again by typing:
-#
-#     spack edit spectra
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
