89ea2fd7bfad20c249d1ef0720b31c020a80e61e
snehring <snehring@iastate.edu>
2022-04-26 16:34:56 -0500

    maq: adding maintainer

--- var/spack/repos/builtin/packages/maq/package.py
+++ var/spack/repos/builtin/packages/maq/package.py
@@ -13,6 +13,7 @@ class Maq(AutotoolsPackage):
     homepage = "http://maq.sourceforge.net/"
     url      = "https://downloads.sourceforge.net/project/maq/maq/0.7.1/maq-0.7.1.tar.bz2"
     list_url = "https://sourceforge.net/projects/maq/files/maq/"
+    maintainers = ['snehring']
 
     version('0.7.1', sha256='e1671e0408b0895f5ab943839ee8f28747cf5f55dc64032c7469b133202b6de2')
     version('0.5.0', sha256='c292c19baf291b2415b460d687d43a71ece00a7d178cc5984bc8fc30cfce2dfb')
