31669510e362aaddd3445eec9477beae41a006aa
snehring <snehring@iastate.edu>
2022-04-26 16:15:23 -0500

    maq: adding perl dep, addressing gcc errors

--- var/spack/repos/builtin/packages/maq/package.py
+++ var/spack/repos/builtin/packages/maq/package.py
@@ -17,4 +17,15 @@ class Maq(AutotoolsPackage):
     version('0.7.1', sha256='e1671e0408b0895f5ab943839ee8f28747cf5f55dc64032c7469b133202b6de2')
     version('0.5.0', sha256='c292c19baf291b2415b460d687d43a71ece00a7d178cc5984bc8fc30cfce2dfb')
 
-    conflicts('%gcc@4.7.0:', when='@0.7.1')
+    depends_on('perl', type='run')
+
+    def patch(self):
+        with working_dir('scripts'):
+            scripts = ['farm-run.pl', 'maq_eval.pl', 'maq.pl', 'maq_plot.pl']
+            for s in scripts:
+                filter_file('/usr/bin/perl', self.spec['perl'].prefix.bin.perl, s)
+
+    def flag_handler(self, name, flags):
+        if name.lower() == 'cxxflags':
+            flags.append('-fpermissive')
+        return (flags, None, None)
