444afecce6a8c1802f0c520cb6c41257d7a2a0b0
Luc Berger-Vergiat <lberge@sandia.gov>
2022-04-26 18:00:47 -0600

    Kokkos: update package for release 3.6.00

--- var/spack/repos/builtin/packages/kokkos/package.py
+++ var/spack/repos/builtin/packages/kokkos/package.py
@@ -13,7 +13,7 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
 
     homepage = "https://github.com/kokkos/kokkos"
     git      = "https://github.com/kokkos/kokkos.git"
-    url      = "https://github.com/kokkos/kokkos/archive/3.5.00.tar.gz"
+    url      = "https://github.com/kokkos/kokkos/archive/3.6.00.tar.gz"
 
     tags = ['e4s']
 
@@ -23,6 +23,7 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
 
     version('master',  branch='master')
     version('develop', branch='develop')
+    version('3.6.00', sha256='53b11fffb53c5d48da5418893ac7bc814ca2fde9c86074bdfeaa967598c918f4')
     version('3.5.00', sha256='748f06aed63b1e77e3653cd2f896ef0d2c64cb2e2d896d9e5a57fec3ff0244ff')
     version('3.4.01', sha256='146d5e233228e75ef59ca497e8f5872d9b272cb93e8e9cdfe05ad34a23f483d1')
     version('3.4.00', sha256='2e4438f9e4767442d8a55e65d000cc9cde92277d415ab4913a96cd3ad901d317')
@@ -140,7 +141,8 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
     amdgpu_arch_map = {
         'gfx900': 'vega900',
         'gfx906': 'vega906',
-        'gfx908': 'vega908'
+        'gfx908': 'vega908',
+        'gfx90A': 'vega90A'
     }
     amd_support_conflict_msg = (
         '{0} is not supported; '
