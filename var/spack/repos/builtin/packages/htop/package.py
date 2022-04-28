b5176cf598e56f089d3e21e2e8c0d49a900a5e43
Jennifer Kathleen Green <jgreen@lanl.gov>
2022-04-28 10:55:27 -0600

    including variants: hwloc, static debug, adjust homepage to product specific repo

--- var/spack/repos/builtin/packages/htop/package.py
+++ var/spack/repos/builtin/packages/htop/package.py
@@ -9,16 +9,39 @@ from spack import *
 class Htop(AutotoolsPackage):
     """htop is an interactive text-mode process viewer for Unix systems."""
 
-    homepage = "https://github.com/hishamhm/htop"
-    url      = "https://hisham.hm/htop/releases/2.0.2/htop-2.0.2.tar.gz"
-    list_url = "https://hisham.hm/htop/releases"
+    homepage = "https://github.com/htop-dev/htop"
+    url      = "https://github.com/htop-dev/htop/archive/refs/tags/3.0.5.tar.gz"
+    list_url = "https://github.com/htop-dev/htop/releases"
     list_depth = 1
 
-    version('2.2.0', sha256='d9d6826f10ce3887950d709b53ee1d8c1849a70fa38e91d5896ad8cbc6ba3c57')
-    version('2.0.2', sha256='179be9dccb80cee0c5e1a1f58c8f72ce7b2328ede30fb71dcdf336539be2f487')
+    version('3.0.5', sha256='4c2629bd50895bd24082ba2f81f8c972348aa2298cc6edc6a21a7fa18b73990c')
 
-    depends_on('ncurses')
+    variant('unicode', default=True,
+            description='enable Unicode support dependency')
+    variant('hwloc', default=False,
+            description='Enable hwloc support for CPU affinity')
+    variant('static', default=False, description='Build a static htop binary')
+    variant('debug', default=False,
+            description='Enable asserts and internal sanity checks')
+
+    depends_on('m4')
+    depends_on('autoconf')
+    depends_on('automake')
+    depends_on('libtool')
+    depends_on('hwloc', when='+hwloc')
+    depends_on('ncurses', when='+unicode')
     depends_on('python+pythoncmd', type='build')
 
+    conflicts('+static', when='+hwloc')
+
     def configure_args(self):
-        return ['--enable-shared']
+        args = ['--enable-shared']
+
+        if self.spec.satisfies('+hwloc'):
+            args.append('--enable-hwloc')
+        if self.spec.satisfies('+static'):
+            args.append('--enable-static')
+        if self.spec.satisfies('+debug'):
+            args.append('--enable-debug')
+
+        return args
