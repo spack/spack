6bc774404d905af86b29b8f5198febf8d5801b30
Harsh Bhatia <bhatia4@llnl.gov>
2022-05-03 12:43:47 -0700

    Update package.py

--- var/spack/repos/builtin/packages/py-pytaridx/package.py
+++ var/spack/repos/builtin/packages/py-pytaridx/package.py
@@ -19,6 +19,6 @@ class PyPytaridx(PythonPackage):
 
     version('1.0.2', sha26='702c42ade13ae8688a56a8edfcd7e0e7512a489a22796c6cfdbcef677010ee47')
     version('master', branch='master')
-    
+
     depends_on('python@3:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
