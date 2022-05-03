54ade5ab50fbd02809d403d4d8100bbf9d09ac49
Richard Berger <rberger@lanl.gov>
2022-05-03 11:05:57 -0600

    Update var/spack/repos/builtin/packages/flcl/package.py

--- var/spack/repos/builtin/packages/flcl/package.py
+++ var/spack/repos/builtin/packages/flcl/package.py
@@ -16,13 +16,14 @@ class Flcl(CMakePackage):
     maintainers = ['womeld', 'agaspar']
 
     version('develop', branch='develop')
-    version('0.3', sha256='fc18c8fa3ae33db61203b647ad9025d894612b0faaf7fe07426aaa8bbfa9e703')
-    version('0.4.0', sha256='0fe327906a991262866b126a7d58098eb48297148f117fd59a2dbcc14e76f394')
-    version('0.5.0', sha256='bfd9b9092904eab1135d3bb4c458a50653b3325c176a722af56f158da0a16f19')
     version('0.99.0', sha256='edb8310154e5e5cf315dad63cd59f13b2537e0ba698869ce9757b04e38047464')
+    version('0.5.0', sha256='bfd9b9092904eab1135d3bb4c458a50653b3325c176a722af56f158da0a16f19')
+    version('0.4.0', sha256='0fe327906a991262866b126a7d58098eb48297148f117fd59a2dbcc14e76f394')
+    version('0.3', sha256='fc18c8fa3ae33db61203b647ad9025d894612b0faaf7fe07426aaa8bbfa9e703')
 
     depends_on('kokkos')
-    depends_on('cmake@3.19:', type='build')
+    depends_on('cmake@3.17:', type='build', when='@:0.4.0')
+    depends_on('cmake@3.19:', type='build', when='@0.5.0:')
 
     conflicts('kokkos@3.3.00:', when='@:0.4.99', msg='Requires FLCL >= 0.5.0 to use Kokkos >= 3.3')
 
