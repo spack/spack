2daece79030839485d2bfbe049a3eeb9b153dea6
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-05-03 21:53:28 +0200

    nasm: rework to use multiple build-systems

--- var/spack/repos/builtin/packages/nasm/package.py
+++ var/spack/repos/builtin/packages/nasm/package.py
@@ -8,14 +8,20 @@ import os
 from spack import *
 
 
-class Nasm(Package):
+class Nasm(AutotoolsPackage):
     """NASM (Netwide Assembler) is an 80x86 assembler designed for
     portability and modularity. It includes a disassembler as well."""
 
     homepage = "https://www.nasm.us"
-    url      = "https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.gz"
+    url = "https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.gz"
     list_url = "https://www.nasm.us/pub/nasm/releasebuilds"
     list_depth = 1
+
+    buildsystem(
+        'autotools',
+        conditional('generic', when='platform=windows'),
+        default='autotools'
+    )
 
     version('2.15.05', sha256='9182a118244b058651c576baa9d0366ee05983c4d4ae1d9ddd3236a9f2304997')
     version('2.14.02', sha256='b34bae344a3f2ed93b2ca7bf25f1ed3fb12da89eeda6096e3551fd66adeae9fc')
@@ -26,12 +32,11 @@ class Nasm(Package):
     # https://bugzilla.nasm.us/show_bug.cgi?id=3392461
     patch('https://src.fedoraproject.org/rpms/nasm/raw/0cc3eb244bd971df81a7f02bc12c5ec259e1a5d6/f/0001-Remove-invalid-pure_func-qualifiers.patch', level=1, sha256='ac9f315d204afa6b99ceefa1fe46d4eed2b8a23c7315d32d33c0f378d930e950', when='@2.13.03 %gcc@8:')
 
-    patch('msvc.mak.patch', when='@2.15.05 platform=windows')
+    with when('platform=windows'):
+        depends_on('perl')
+        patch('msvc.mak.patch', when='@2.15.05')
 
-    conflicts('%intel@:14', when='@2.14:',
-              msg="Intel 14 has immature C11 support")
-
-    depends_on('perl', when='platform=windows')
+    conflicts('%intel@:14', when='@2.14:', msg="Intel <= 14 lacks support for C11")
 
     def patch(self):
         # Remove flags not recognized by the NVIDIA compiler
@@ -41,13 +46,7 @@ class Nasm(Package):
             filter_file(r'CFLAGS="\$pa_add_flags__old_flags -Werror=.*"',
                         'CFLAGS="$pa_add_flags__old_flags"', 'configure')
 
-    def install(self, spec, prefix):
-        with working_dir(self.stage.source_path, create=True):
-            configure(*['--prefix={0}'.format(self.prefix)])
-            make('V=1')
-            make('install')
-
-    @when('platform=windows')
+    @generic
     def install(self, spec, prefix):
         with working_dir(self.stage.source_path, create=True):
             # build NASM with nmake
