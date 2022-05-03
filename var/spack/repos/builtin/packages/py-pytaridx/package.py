06000dc5ec15e500f6d9561d09c0e2e215ddccfb
Harsh Bhatia <bhatia4@llnl.gov>
2022-05-03 12:09:24 -0700

    added pypi link

--- var/spack/repos/builtin/packages/py-pytaridx/package.py
+++ var/spack/repos/builtin/packages/py-pytaridx/package.py
@@ -13,12 +13,17 @@ class PyPytaridx(PythonPackage):
 
     homepage = "https://github.com/LLNL/pytaridx"
     git      = "https://github.com/LLNL/pytaridx.git"
-    url      = "https://github.com/LLNL/pytaridx/archive/refs/tags/v1.0.2.tar.gz"
+    pypi = "pytaridx/pytaridx-1.0.2.tar.gz"
+    #url      = "https://github.com/LLNL/pytaridx/archive/refs/tags/v1.0.2.tar.gz"
 
     maintainers = ['bhatiaharsh']
 
-    version('1.0.2', sha256='b1de4d5224b61f9c5aa1edf24b4ba0bfe1c5d2eb358c7a9b7914734ffb4efe8b')
-    version('master',  branch='master')
+    # use pypi
+    version('1.0.2', sha26='702c42ade13ae8688a56a8edfcd7e0e7512a489a22796c6cfdbcef677010ee47')
 
+    # use git
+    #version('1.0.2', sha256='b1de4d5224b61f9c5aa1edf24b4ba0bfe1c5d2eb358c7a9b7914734ffb4efe8b')
+    #version('master',  branch='master')
+    
     depends_on('python@3:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
