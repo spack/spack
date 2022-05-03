cbf0c3a8c4df1c7757a99ba58817a18980fa49c0
Harsh Bhatia <bhatia4@llnl.gov>
2022-05-02 19:17:54 -0700

    update version of py-mock, py-msgpack, py-gridformats, py-pyrsistent (#30445)
    * add v3.0.3 for py-mock
    
    * added v0.6.0 and 0.6.1 for py-msgpack
    
    * added v0.4.0 for py-gridformats
    
    * added v0.16.0 for py-pyrsistent

--- var/spack/repos/builtin/packages/py-mock/package.py
+++ var/spack/repos/builtin/packages/py-mock/package.py
@@ -16,6 +16,7 @@ class PyMock(PythonPackage):
 
     version('4.0.3', sha256='7d3fbbde18228f4ff2f1f119a45cdffa458b4c0dee32eb4d2bb2f82554bac7bc')
     version('3.0.5', sha256='83657d894c90d5681d62155c82bda9c1187827525880eda8ff5df4ec813437c3')
+    version('3.0.3', sha256='5eda46efb363128828d6fd2bf8d16f6ebb66f5b543b9f7f8f4eb224c5cb503fe')
     version('2.0.0', sha256='b158b6df76edd239b8208d481dc46b6afd45a846b7812ff0ce58971cf5bc8bba')
     version('1.3.0', sha256='1e247dbecc6ce057299eb7ee019ad68314bb93152e81d9a6110d35f4d5eca0f6')
 
