f81b5a88c318deebcd2d8173dcca07c5d16c3224
Ken Raffenetti <raffenet@users.noreply.github.com>
2022-05-04 09:04:39 -0500

    Update var/spack/repos/builtin/packages/level-zero/package.py
    Co-authored-by: Robert Cohn <rscohn2@gmail.com>

--- var/spack/repos/builtin/packages/level-zero/package.py
+++ var/spack/repos/builtin/packages/level-zero/package.py
@@ -12,7 +12,7 @@ class LevelZero(CMakePackage):
     homepage = "https://spec.oneapi.io/versions/latest/elements/l0/source/index.html"
     url      = "https://github.com/oneapi-src/level-zero/archive/refs/tags/v1.7.15.tar.gz"
 
-    # maintainers = ['github_user1', 'github_user2']
+    maintainers = ['rscohn2']
 
     version('1.7.15', sha256='c39bb05a8e5898aa6c444e1704105b93d3f1888b9c333f8e7e73825ffbfb2617')
     version('1.7.9',  sha256='b430a7f833a689c899b32172a31c3bca1d16adcad8ff866f240a3a8968433de7')
