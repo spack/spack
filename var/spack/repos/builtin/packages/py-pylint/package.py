94f5d958f0feb383423620777aa2b69bb1ae855a
aandvalenzuela <aand.valenzuela@gmail.com>
2022-05-04 13:46:39 +0200

    Remove mccabe and setuptools-scm dependencies

--- var/spack/repos/builtin/packages/py-pylint/package.py
+++ var/spack/repos/builtin/packages/py-pylint/package.py
@@ -49,10 +49,9 @@ class PyPylint(PythonPackage):
     depends_on('py-isort@4.2.5:5', when='@2.3.1:', type=('build', 'run'))
     depends_on('py-mccabe', type=('build', 'run'))
     depends_on('py-mccabe@0.6.0:0.6', when='@2.3.1:2.11', type=('build', 'run'))
-    depends_on('py-mccabe@0.6.0:0.7', when='@2.13.5:', type=('build', 'run'))
     depends_on('py-pip', type=('build'))  # see https://github.com/spack/spack/issues/27075
     # depends_on('py-setuptools-scm@1.15.0:', type='build')
-    depends_on('py-setuptools-scm', type='build', when='@2.8.2:')
+    depends_on('py-setuptools-scm', type='build', when='@2.8.2')
     depends_on('py-setuptools@17.1:', type='build')
     depends_on('py-singledispatch', when='^python@:3.3', type=('build', 'run'))
     depends_on('py-six', type=('build', 'run'), when='@1:2.3.1')
