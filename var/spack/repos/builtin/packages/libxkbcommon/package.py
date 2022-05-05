2addf6d0371a43009efd5f70aaa4a5d3edfd7eb9
Wouter Deconinck <wdconinc@gmail.com>
2022-05-05 10:20:56 -0500

    libxkbcommon: fix style

--- var/spack/repos/builtin/packages/libxkbcommon/package.py
+++ var/spack/repos/builtin/packages/libxkbcommon/package.py
@@ -73,4 +73,3 @@ class Libxkbcommon(MesonPackage):
         """Run the AutotoolsPackage install phase in source_path"""
         with working_dir(self.stage.source_path):
             inspect.getmodule(self).make(*self.install_targets)
-
