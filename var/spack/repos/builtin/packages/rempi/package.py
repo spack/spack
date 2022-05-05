73a7a997fe186dd11094450a51a0ff339134617e
Tim Haines <thaines.astro@gmail.com>
2022-05-05 18:47:40 -0500

    ReMPI: add version for github branch

--- var/spack/repos/builtin/packages/rempi/package.py
+++ var/spack/repos/builtin/packages/rempi/package.py
@@ -10,8 +10,10 @@ class Rempi(AutotoolsPackage):
     """ReMPI is a record-and-replay tool for MPI applications."""
     homepage = "https://github.com/PRUNERS/ReMPI"
     url      = "https://github.com/PRUNERS/ReMPI/releases/download/v1.0.0/ReMPI-1.0.0.tar.gz"
+    git      = "https://github.com/PRUNERS/ReMPI.git"
     tags     = ['e4s']
 
+    version('master', branch='master')
     version("1.1.0", sha256="4fd94fca52311fd19dc04a32547841e6c1c1656b7999b2f76f537d6ec24efccc")
     version("1.0.0", sha256="1cb21f457cf8a04632150156a2ba699dd0c3f81d47e8881a9b943b9bf575fa01")
 
