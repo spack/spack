bd9e19739bfa83e8b2351c121c2879751510504b
Stepan Vanecek <stepan.vanecek@tum.de>
2022-05-09 19:50:40 +0200

    sys-sage: remove FIXMEs from the package

--- var/spack/repos/builtin/packages/sys-sage/package.py
+++ var/spack/repos/builtin/packages/sys-sage/package.py
@@ -3,22 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install sys-sage
-#
-# You can edit this file again by typing:
-#
-#     spack edit sys-sage
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
@@ -30,20 +14,14 @@ class SysSage(CMakePackage):
     url      = "https://github.com/stepanvanecek/sys-sage/archive/refs/tags/v0.1.1-alpha.2.tar.gz"
     git      = "https://github.com/stepanvanecek/sys-sage.git"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
     maintainers = ['stepanvanecek']
 
     version('master',  branch='master')
     version('0.1.1-alpha.2', sha256='991a77cf37b061a911c8566fd4486f914de4f4c8cdf39112ec8a32903450c178')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
     depends_on('cmake@3.21:', type='build')
     depends_on('libxml2@2.9.13')
-    #def cmake_args(self):
-    #    # FIXME: Add arguments other than
-    #    # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-    #    # FIXME: If not needed delete this function
+   
+   #def cmake_args(self):
     #    args = []
     #    return args
