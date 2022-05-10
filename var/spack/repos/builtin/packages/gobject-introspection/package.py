27e1546e2d9dd7d437f59634a1e99f646dcb154e
Gregory L. Lee <lee218@llnl.gov>
2022-05-10 11:55:29 -0700

    added libffi as dependence for gobject-introspection

--- var/spack/repos/builtin/packages/gobject-introspection/package.py
+++ var/spack/repos/builtin/packages/gobject-introspection/package.py
@@ -27,6 +27,9 @@ class GobjectIntrospection(Package):
     depends_on("bison", type="build")
     depends_on("flex", type="build")
     depends_on("pkgconfig", type="build")
+    depends_on('libffi')
+    depends_on('libffi@:3.3', when='@:1.70') # libffi 3.4 was causing seg faults
+    # https://gitlab.gnome.org/GNOME/gobject-introspection/-/merge_requests/283
 
     # GobjectIntrospection does not build with sed from darwin:
     depends_on('sed', when='platform=darwin', type='build')
