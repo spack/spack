00e978013676e0f1a1c5aecfccb40f12fa29f7b4
Sreenivasa Murthy Kolam <67086238+srekolam@users.noreply.github.com>
2022-05-10 08:42:22 -0700

    Update ROCm recipes for rocm-5.1.0 release (#30027)
    * Bump up rocm release version to rocm-5.1.0
    * update rocm-opencl for rocm-5.1.0 release
    * update the migraphx,miopen(hip,opencl),mivisionx,rocm-tensile
    * update the mlirmiopen checksum version

--- var/spack/repos/builtin/packages/hipify-clang/package.py
+++ var/spack/repos/builtin/packages/hipify-clang/package.py
@@ -17,6 +17,7 @@ class HipifyClang(CMakePackage):
     maintainers = ['srekolam', 'arjun-raj-kuppala']
 
     version('master', branch='master')
+    version('5.1.0', sha256='ba792294cbdcc880e0f02e38ee352dff8d4a2c183430e13d1c5ed176bd46cfc5')
     version('5.0.2', sha256='812bccfeb044483a1c7df89f45843afcb28d8146f348c792f082b693cbff3984')
     version('5.0.0', sha256='06fbb3259b6d014bc24fb3c05f71026bc39ae564559d40f2ca37236044c7ba17')
     version('4.5.2', sha256='f0d401e634642a1d6659b9163a38661ee38da1e1aceabb1f16f78f8fce048a4e')
@@ -37,5 +38,5 @@ class HipifyClang(CMakePackage):
     depends_on('cmake@3.5:', type='build')
     for ver in ['3.5.0', '3.7.0', '3.8.0', '3.9.0', '3.10.0', '4.0.0', '4.1.0',
                 '4.2.0', '4.3.0', '4.3.1', '4.5.0', '4.5.2', '5.0.0', '5.0.2',
-                'master']:
+                '5.1.0', 'master']:
         depends_on('llvm-amdgpu@' + ver, when='@' + ver)
