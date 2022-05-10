23b7071bb0031bdbb54bac7dda1dddfe387c3d72
mcuma <m.cuma@utah.edu>
2022-05-09 23:42:21 -0600

    wi4mpi: fix a missed conversion to string (#30541)

--- var/spack/repos/builtin/packages/wi4mpi/package.py
+++ var/spack/repos/builtin/packages/wi4mpi/package.py
@@ -50,7 +50,7 @@ class Wi4mpi(CMakePackage):
 
     def setup_run_environment(self, env):
         env.set('WI4MPI_ROOT', self.prefix)
-        env.set('WI4MPI_VERSION', self.version)
+        env.set('WI4MPI_VERSION', str(self.version))
         env.set('WI4MPI_CC', self.compiler.cc)
         env.set('WI4MPI_CXX', self.compiler.cxx)
         env.set('WI4MPI_FC', self.compiler.fc)
