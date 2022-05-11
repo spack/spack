1dc36cc3e68d26f154adc2a1c220ec4208ad9a63
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-05-11 11:53:43 +0200

    initial draft for py-neo, added versions 0.9.0, 0.10.0, 0.10.1 and 0.10.2

--- var/spack/repos/builtin/packages/py-neo/package.py
+++ var/spack/repos/builtin/packages/py-neo/package.py
@@ -14,12 +14,22 @@ class PyNeo(PythonPackage):
     homepage = "https://neuralensemble.org/neo"
     pypi = "neo/neo-0.4.1.tar.gz"
 
+    # list of GitHub accounts to notify when the package is updated.
+    # maintainers = ['acc1', 'acc2']
+
+    version('0.10.2', sha256='2d4218b0826daeea880e155227060029ec38a00238ceb5f097138d9467c6399b')
+    version('0.10.1', sha256='444ff15278ddcf9e3577820e878cf6b9f4220ce145bf1013c8f30536cf60939b')
+    version('0.10.0', sha256='e591a53e18cfa4478603a0e133f3fa0e07bc016b2a279d21d72cf8196eca8353')
+    version('0.9.0', sha256='6e31c88d7c52174fa2512df589b2b5003e9471fde27fca9f315f4770ba3bd3cb')
     version('0.8.0', sha256='3382a37b24a384006238b72981f1e9259de9bfa71886f8ed564d35d254ace458')
     version('0.5.2', sha256='1de436b7d5e72a5b4f1baa68bae5b790624a9ac44b2673811cb0b6ef554d3f8b')
     version('0.4.1', sha256='a5a4f3aa31654d52789f679717c9fb622ad4f59b56d227dca490357b9de0a1ce')
     version('0.3.3', sha256='6b80eb5bdc9eb4eca829f7464f861c5f1a3a6289559de037930d529bb3dddefb')
 
-    depends_on('py-setuptools',        type='build')
+    depends_on('py-setuptools', type='build')
     depends_on('py-numpy@1.7.1:', when='@0.4:', type=('build', 'run'))
-    depends_on('py-numpy@1.3.0:', type=('build', 'run'))
-    depends_on('py-quantities@0.9.0:', type=('build', 'run'))
+    depends_on('py-numpy@1.3.0:', when='@0.3.3', type=('build', 'run'))
+    depends_on('py-numpy@1.13.0:', when='@0.9.0', type=('build', 'run'))
+    depends_on('py-numpy@1.16.1:', when='@0.10.0:', type=('build', 'run'))
+    depends_on('py-quantities@0.9.0:', when='@0.3.3:', type=('build', 'run'))
+    depends_on('py-quantities@0.12.1:', when='@0.9.0:', type=('build', 'run'))
