e12218ef018de03d76c4a081f11a5023d8925dff
snehring <snehring@iastate.edu>
2022-05-11 12:52:54 -0500

    bamutil: updating to 1.0.15

--- var/spack/repos/builtin/packages/bamutil/package.py
+++ var/spack/repos/builtin/packages/bamutil/package.py
@@ -14,22 +14,31 @@ class Bamutil(MakefilePackage):
 
     homepage = "https://genome.sph.umich.edu/wiki/BamUtil"
     url      = "https://genome.sph.umich.edu/w/images/7/70/BamUtilLibStatGen.1.0.13.tgz"
+    git      = "https://github.com/statgen/bamUtil.git"
+    maintainers = ['snehring']
 
+    version('1.0.15', commit='3ad3980a3a3a3fc35eca3636b7206676c8303ce6')
     version('1.0.13', sha256='16c1d01c37d1f98b98c144f3dd0fda6068c1902f06bd0989f36ce425eb0c592b')
 
-    depends_on('zlib', type=('build', 'link'))
+    depends_on('zlib')
+    depends_on('git', type='build', when='@1.0.15:')
 
-    # Looks like this will be fixed in 1.0.14.
-    # https://github.com/statgen/libStatGen/issues/9
-    patch('libstatgen-issue-9.patch', when='@1.0.13:')
-    # These are fixed in the standalone libStatGen,
-    # but bamutil@1.0.13 embeds its own copy, so fix 'em here.
+    patch('libstatgen-issue-9.patch', when='@1.0.13')
     patch('libstatgen-issue-19.patch', when='@1.0.13')
     patch('libstatgen-issue-17.patch', when='@1.0.13')
     patch('libstatgen-issue-7.patch', when='@1.0.13')
     patch('verifybamid-issue-8.patch', when='@1.0.13')
 
     parallel = False
+
+    @when('@1.0.15')
+    def edit(self, spec, prefix):
+        filter_file('git://', 'https://', 'Makefile.inc', String=True)
+
+    @when('@1.0.15:')
+    def build(self, spec, prefix):
+        make('cloneLib')
+        make()
 
     @property
     def install_targets(self):
