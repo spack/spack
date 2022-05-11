a0c45debc2ba392b64eb50f3dcb0ff61469b66c1
Harmen Stoppels <harmenstoppels@gmail.com>
2022-05-11 11:36:24 +0200

    jasper: add 3.0.x

--- var/spack/repos/builtin/packages/jasper/package.py
+++ var/spack/repos/builtin/packages/jasper/package.py
@@ -12,6 +12,7 @@ class Jasper(Package):
     homepage = "https://www.ece.uvic.ca/~frodo/jasper/"
     url      = "https://github.com/mdadams/jasper/archive/version-2.0.32.tar.gz"
 
+    version('3.0.3', sha256='1b324f7746681f6d24d06fcf163cf3b8ae7ac320adc776c3d611b2b62c31b65f')
     version('2.0.32', sha256='a3583a06698a6d6106f2fc413aa42d65d86bedf9a988d60e5cfa38bf72bc64b9')
     version('2.0.31', sha256='d419baa2f8a6ffda18472487f6314f0f08b673204723bf11c3a1f5b3f1b8e768')
     version('2.0.16', sha256='f1d8b90f231184d99968f361884e2054a1714fdbbd9944ba1ae4ebdcc9bbfdb1')
@@ -26,6 +27,9 @@ class Jasper(Package):
     depends_on('cmake@2.8.11:', type='build', when='@2:')
     depends_on('jpeg', when='+jpeg')
     depends_on('gl', when='+opengl')
+
+    # invalid compilers flags
+    conflicts('@2.0.0:2', when='%nvhpc')
 
     # Fixes a bug where an assertion fails when certain JPEG-2000
     # files with an alpha channel are processed.
