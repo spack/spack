3e8f31a0687c2213e6cc31054c9ffc0bdc20cc3b
Simon Pintarelli <1237199+simonpintarelli@users.noreply.github.com>
2022-05-12 20:09:30 +0200

    atompaw: add version 4.2.0.0 (#30650)

--- var/spack/repos/builtin/packages/atompaw/package.py
+++ var/spack/repos/builtin/packages/atompaw/package.py
@@ -18,6 +18,7 @@ class Atompaw(AutotoolsPackage):
     homepage = "https://users.wfu.edu/natalie/papers/pwpaw/man.html"
     url = "https://users.wfu.edu/natalie/papers/pwpaw/atompaw-4.0.0.13.tar.gz"
 
+    version('4.2.0.0', sha256='9ab4f4ab78a720fbcd95bbbc1403e8ff348d15570e7c694932a56be15985e93d')
     version('4.1.1.0', sha256='b1ee2b53720066655d98523ef337e54850cb1e68b3a2da04ff5a1576d3893891')
     version('4.0.0.13', sha256='cbd73f11f3e9cc3ff2e5f3ec87498aeaf439555903d0b95a72f3b0a021902020')
     version('3.1.0.3', sha256='15fe9a0369bdcc366370a0ecaa67e803ae54534b479ad63c4c7494a04fa3ea78')
