fbbb2f7edd208831fccddb2daf237193b2be1998
snehring <snehring@iastate.edu>
2022-05-13 14:58:05 -0500

    bpp-popgen: new package

---
+++ var/spack/repos/builtin/packages/bpp-popgen/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class BppPopgen(CMakePackage):
+    """The Bio++ Population Genetics Library"""
+
+    homepage = "https://https://github.com/BioPP/bpp-popgen"
+    url      = "https://github.com/BioPP/bpp-popgen/archive/refs/tags/v2.4.1.tar.gz"
+
+    maintainers = ['snehring']
+
+    version('2.4.1', sha256='03b57d71a63c8fa7f11c085e531d0d691fc1d40d4ea541070dabde0ab3baf413')
+
+    depends_on('bpp-seq')
+
+    def cmake_args(self):
+        args = []
+        return args
