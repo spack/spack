4d4715042840a40d5a67c2f43927869556b61e25
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-05-15 20:06:44 -0500

    Get older versions to build
    - deprecate versions 0.16 and lower as they do not build
    - versions older than 0.22 depend on java@8
    - adjust when patches are applied
    - adjust gcc conflicts directive

--- var/spack/repos/builtin/packages/bazel/package.py
+++ var/spack/repos/builtin/packages/bazel/package.py
@@ -180,18 +180,18 @@ class Bazel(Package):
     # https://blog.bazel.build/2021/05/21/bazel-4-1.html
     conflicts("platform=darwin target=aarch64:", when="@:4.0")
 
-    # patches for compiling bazel-4.1: with gcc-11
+    # patches for compiling bazel-4.1:4 with gcc-11
     # these are derived from
     # https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/29084/
-    patch('gcc11_1.patch', when='@4.1:%gcc@11:')
-    patch('gcc11_2.patch', when='@4.1:%gcc@11:')
-    patch('gcc11_3.patch', when='@4.1:%gcc@11:')
-    patch('gcc11_4.patch', when='@4.1:%gcc@11:')
+    patch('gcc11_1.patch', when='@0.3:4%gcc@11:')
+    patch('gcc11_2.patch', when='@0.3:4%gcc@11:')
+    patch('gcc11_3.patch', when='@0.3:4%gcc@11:')
+    patch('gcc11_4.patch', when='@4.1:4%gcc@11:')
 
     # bazel-4.0.0 does not compile with gcc-11
     # newer versions of grpc and abseil dependencies are needed but are not in
     # bazel-4.0.0
-    conflicts('@4.0.0', when='%gcc@11:')
+    conflicts('@:0.2,4.0.0', when='%gcc@11:')
 
     executables = ['^bazel$']
 
