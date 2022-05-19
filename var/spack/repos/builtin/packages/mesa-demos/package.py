49cc7735a227741d3ca6fb6b1728ada2d20a9eb9
Chuck Atkins <chuck.atkins@kitware.com>
2022-05-19 14:20:21 -0400

    mesa-glu and mesa-demos: Fix conflicts with glu and osmesa

--- var/spack/repos/builtin/packages/mesa-demos/package.py
+++ var/spack/repos/builtin/packages/mesa-demos/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import sys
+
 from spack import *
 
 
@@ -16,13 +18,43 @@ class MesaDemos(AutotoolsPackage):
     version('8.2.0', sha256='5a9f71b815d968d0c3b77edfcc3782d0211f8520b00da9e554ccfed80c8889f6')
     version('8.1.0', sha256='cc5826105355830208c90047fc38c5b09fa3ab0045366e7e859104935b00b76d')
 
+    variant('osmesa', default=False, description="Enable the OSMesa frontend.")
+
+    is_linux = sys.platform.startswith('linux')
+    variant('glx', default=is_linux, description="Enable the GLX frontend.")
+
+    conflicts('+osmesa', when='+glx')
+
     depends_on('autoconf',  type='build')
     depends_on('automake',  type='build')
     depends_on('libtool',   type='build')
     depends_on('m4',        type='build')
     depends_on('pkgconfig', type='build')
-    depends_on('mesa')
-    depends_on('mesa-glu')
-    depends_on('freetype')
+
+    depends_on('gl')
+    depends_on('glx', when='+glx')
+    depends_on('libx11',  when='+glx')
+    depends_on('libxext', when='+glx')
+    depends_on('osmesa', when='+osmesa')
+
+    depends_on('glu', when='~osmesa')
+    depends_on('mesa-glu+osmesa', when='+osmesa')
     depends_on('freeglut')
     depends_on('glew@1.5.4:')
+
+    def configure_args(self):
+        args = [
+            '--disable-egl',
+            '--disable-gles1',
+            '--disable-gles2',
+            '--disable-vg',
+            '--disable-libdrm',
+            '--disable-wayland',
+            '--disable-gbm',
+            '--disable-freetype2',
+            '--disable-rbug',
+            self.enable_or_disable('osmesa'),
+            self.enable_or_disable('x11', variant='glx'),
+        ]
+
+        return args
--- var/spack/repos/builtin/packages/mesa-glu/package.py
+++ var/spack/repos/builtin/packages/mesa-glu/package.py
@@ -12,17 +12,23 @@ class MesaGlu(AutotoolsPackage):
     homepage = "https://www.mesa3d.org"
     url      = "https://www.mesa3d.org/archive/glu/glu-9.0.0.tar.gz"
 
+    version('9.0.2', sha256='24effdfb952453cc00e275e1c82ca9787506aba0282145fff054498e60e19a65')
     version('9.0.1', sha256='f6f484cfcd51e489afe88031afdea1e173aa652697e4c19ddbcb8260579a10f7')
     version('9.0.0', sha256='4387476a1933f36fec1531178ea204057bbeb04cc2d8396c9ea32720a1f7e264')
 
-    depends_on('gl@3:')
+    variant('osmesa', default=False, description='Enable OSMesa instead of libGL')
 
-    provides('glu@1.3')
+    depends_on('gl@3:')
+    depends_on('osmesa', when='+osmesa')
+
+    # Since pacakges like mesa provide both gl and osmesa this will prevent
+    # consuming packages from getting a glu tied to a differnt gl library
+    provides('glu@1.3', when='~osmesa')
 
     def configure_args(self):
         args = []
-        if self.spec['gl'].satisfies('mesa'):
-            args.append('--enable-osmesa')
+        args.extend(self.enable_or_disable('osmesa'))
+
         return args
 
     @property
