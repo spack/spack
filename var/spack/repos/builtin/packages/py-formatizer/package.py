74bef2105a75871b9513d4473e50f6fa7b352e98
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2022-05-20 19:37:55 +0200

    py-formatizer: add new package (#30755)

---
+++ var/spack/repos/builtin/packages/py-formatizer/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyFormatizer(PythonPackage):
+    """Literal string formatting for Python versions older than 3.6."""
+
+    homepage = "https://github.com/fgimian/formatizer"
+    pypi     = "formatizer/formatizer-0.1.1.tar.gz"
+
+    version('0.1.1', sha256='3061ced1daa08f1836b79f4a3de16a33a54179331273e0b9c757d27ab339c29f')
+
+    depends_on('py-setuptools', type='build')
