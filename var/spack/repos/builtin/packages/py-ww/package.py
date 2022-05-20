6fa70a7460e3b6add6447c14c308ad34383dc9fc
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-05-20 19:56:48 +0200

    py-ww: add new package

---
+++ var/spack/repos/builtin/packages/py-ww/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyWw(PythonPackage):
+    """Wrappers for Python builtins with higher-level APIs."""
+
+    homepage = "https://github.com/tygs/ww/"
+    pypi     = "ww/ww-0.2.1.tar.gz"
+
+    version('0.2.1', sha256='3664f1f91bf927fe597ab153e8df73c8954927258b3737220efd1cb9912ebd7e')
+
+    depends_on('py-setuptools', type='build')
+
+    depends_on('py-chardet', type=('build', 'run'))
+    depends_on('py-formatizer', type=('build', 'run'))
+    depends_on('py-future', type=('build', 'run'))
+    depends_on('py-six', type=('build', 'run'))
