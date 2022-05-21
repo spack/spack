2542d16a2e5e745754b4b338433992166809bfa4
Abhik Sarkar <62109745+asarkar-parsys@users.noreply.github.com>
2022-05-21 15:57:59 -0700

    Feature/composed boost pkg deps p3 (#28961)
    * This commit removes the Boost.with_default_variants to variants that packages are precisely dependant upon. This is the third batch of 16 packages with modified boost dependencies.
    
    * style fix
    
    * Update var/spack/repos/builtin/packages/sympol/package.py
    
    Co-authored-by: Tim Haines <thaines.astro@gmail.com>
    
    * fix style
    
    * Apply suggestions from code review
    
    Co-authored-by: Tim Haines <thaines.astro@gmail.com>
    
    * Fix Trilinos boost deps
    
    * Fix style
    
    Co-authored-by: Tim Haines <thaines.astro@gmail.com>
    Co-authored-by: Tom Scogland <tom.scogland@gmail.com>

--- var/spack/repos/builtin/packages/simgrid/package.py
+++ var/spack/repos/builtin/packages/simgrid/package.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-from spack.pkg.builtin.boost import Boost
 
 
 class Simgrid(CMakePackage):
@@ -74,12 +73,8 @@ class Simgrid(CMakePackage):
     variant('mc', default=False, description='Model checker')
 
     # does not build correctly with some old compilers -> rely on packages
-
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
-    # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants, when='@:3.21')
     depends_on('boost@:1.69.0', when='@:3.21')
+    depends_on('boost+exception')
 
     conflicts('%gcc@10:', when='@:3.23',
               msg='simgrid <= v3.23 cannot be built with gcc >= 10,'
