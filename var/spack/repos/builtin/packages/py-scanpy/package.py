3b9c1775069ec04c213f6074367434e14318f0ae
James A Zilberman <jazrc@rit.edu>
2022-05-24 13:28:35 -0400

    [py-scanpy] Added types

--- var/spack/repos/builtin/packages/py-scanpy/package.py
+++ var/spack/repos/builtin/packages/py-scanpy/package.py
@@ -15,8 +15,8 @@ class PyScanpy(PythonPackage):
     version('1.9.1', sha256='00c9a83b649da7e0171c91e9a08cff632102faa760614fd05cd4d1dbba4eb541')
 
     depends_on('python@3.7:', type='build')
-    depends_on('py-setuptools-scm')
-    depends_on('py-flit-core@3.4:3')
-    depends_on('py-importlib-metadata@0.7:')
-    depends_on('py-tomli')
-    depends_on('py-packaging')
+    depends_on('py-setuptools-scm', type=('build', 'run'))
+    depends_on('py-flit-core@3.4:3', type=('build', 'run'))
+    depends_on('py-importlib-metadata@0.7:', type=('build', 'run'))
+    depends_on('py-tomli', type=('build', 'run'))
+    depends_on('py-packaging', type=('build', 'run'))
