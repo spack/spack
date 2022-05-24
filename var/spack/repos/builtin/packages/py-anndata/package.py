5ae97f274db03d5003f9b3c4b00da4e1e6da1e7f
James A Zilberman <jazrc@rit.edu>
2022-05-24 13:28:14 -0400

    [py-anndata] Added dependencies

--- var/spack/repos/builtin/packages/py-anndata/package.py
+++ var/spack/repos/builtin/packages/py-anndata/package.py
@@ -7,42 +7,15 @@ from spack import *
 
 
 class PyAnndata(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """anndata is a Python package for handling annotated data matrices 
+    in memory and on disk, positioned between pandas and xarray."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://www.example.com"
     pypi     = "anndata/anndata-0.8.0.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
-
     version('0.8.0', sha256='94d2cc6f76c0317c0ac28564e3092b313b7ad19c737d66701961f3e620b9066e')
 
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-    # depends_on('py-pip@X.Y:', type='build')
-    # depends_on('py-wheel@X.Y:', type='build')
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on('py-setuptools', type='build')
-    # depends_on('py-flit-core', type='build')
-    # depends_on('py-poetry-core', type='build')
-
-    # FIXME: Add additional dependencies if required.
-    # depends_on('py-foo', type=('build', 'run'))
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
+    depends_on('python@3.7:', type='build')
+    depends_on('py-setuptools-scm')
+    depends_on('py-flit-core@3.4:3')
+    depends_on('py-importlib-metadata@0.7:')
