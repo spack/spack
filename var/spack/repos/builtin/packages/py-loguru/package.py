c7c146342886df1dde213910f49afa178cfd8e77
James A Zilberman <jazrc@rit.edu>
2022-05-24 13:28:27 -0400

    [py-loguru] Removed commented out line

--- var/spack/repos/builtin/packages/py-loguru/package.py
+++ var/spack/repos/builtin/packages/py-loguru/package.py
@@ -15,7 +15,6 @@ class PyLoguru(PythonPackage):
     version('0.6.0', sha256='066bd06758d0a513e9836fd9c6b5a75bfb3fd36841f4b996bc60b547a309d41c')
 
     depends_on('python@3.5:', type='build')
-    # depends_on('py-setuptools', type='build')
     depends_on('py-aiocontextvars@0.2.0:', when='^python@3.6:')
     depends_on('py-win32-setctime@1.0.0:', when='platform=win32')
     depends_on('py-colorama@0.3.4:')
