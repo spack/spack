d2482be6f4b9d442b193d4df00a60f3b32346368
genric <genrich.ivaska@epfl.ch>
2022-05-25 13:26:06 +0200

    restore visual-cortex-analysis after gitlab move (#1603)

---
+++ bluebrain/repo-bluebrain/packages/py-visual-cortex-analysis/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyVisualCortexAnalysis(PythonPackage):
+    """Visual cortex analyses"""
+
+    homepage = 'https://bbpgitlab.epfl.ch/circuits/proj120/visual-cortex-analysis'
+    git      = 'git@bbpgitlab.epfl.ch:circuits/proj120/visual-cortex-analysis.git'
+
+    version('0.0.0-2022-05-25', commit='e6bffd239a7a5722e7a6f9f5b12bf8d0dc485082')
+
+    depends_on('py-setuptools', type='build')
+
+    depends_on('py-numpy',      type=('run'))
+    depends_on('py-pandas',     type=('run'))
+    depends_on('py-lazy',       type=('run'))
+    depends_on('py-tqdm',       type=('run'))
+    depends_on('py-pyyaml',     type=('run'))
+    depends_on('py-seaborn',    type=('run'))
+    depends_on('py-frozendict', type=('run'))
+    depends_on('py-bmtk',       type=('run'))
--- bluebrain/repo-patches/packages/py-bmtk/package.py
+++ bluebrain/repo-patches/packages/py-bmtk/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -10,11 +10,9 @@ class PyBmtk(PythonPackage):
     """The Brain Modeling Toolkit"""
 
     homepage = "https://github.com/AllenInstitute/bmtk"
-    url      = "https://pypi.io/packages/source/b/bmtk/bmtk-0.0.8.tar.gz"
+    url      = "https://pypi.io/packages/source/b/bmtk/bmtk-1.0.5.tar.gz"
 
-    version('0.0.8', sha256='00ab884a94e4f19f26d64476021c036178332396a10bc5d02164dfd633f169f4')
-
-    depends_on('python@3.6:', type=('build', 'run'))
+    version('1.0.5', sha256='e0cb47b334467a6d124cfb99bbc67cc88f39f0291f4c39929f50d153130642a4')
 
     depends_on('py-setuptools', type='build')
 
