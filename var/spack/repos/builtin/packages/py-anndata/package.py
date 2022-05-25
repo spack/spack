e5c275e4d1f7131dddb27971221a525728c7002b
Jen Herting <jen@herting.cc>
2022-05-25 15:33:01 -0400

    [py-anndata] reworking dependencies and flake8

--- var/spack/repos/builtin/packages/py-anndata/package.py
+++ var/spack/repos/builtin/packages/py-anndata/package.py
@@ -7,7 +7,7 @@ from spack import *
 
 
 class PyAnndata(PythonPackage):
-    """anndata is a Python package for handling annotated data matrices 
+    """anndata is a Python package for handling annotated data matrices
     in memory and on disk, positioned between pandas and xarray."""
 
     homepage = "https://github.com/theislab/anndata"
@@ -15,7 +15,14 @@ class PyAnndata(PythonPackage):
 
     version('0.8.0', sha256='94d2cc6f76c0317c0ac28564e3092b313b7ad19c737d66701961f3e620b9066e')
 
-    depends_on('python@3.7:', type='build')
-    depends_on('py-setuptools-scm', type=('build', 'run'))
-    depends_on('py-flit-core@3.4:3', type=('build', 'run'))
-    depends_on('py-importlib-metadata@0.7:', type=('build', 'run'))
+    depends_on('python@3.7:', type=('build', 'run'))
+    depends_on('py-setuptools-scm', type='build')
+    depends_on('py-flit-core@3.4:3', type='build')
+    depends_on('py-importlib-metadata@0.7:', type=('build', 'run'), when='^python@:3.7')
+    depends_on('py-pandas@1.1.1:', type=('build', 'run'))
+    depends_on('py-numpy@1.16.5:', type=('build', 'run'))
+    depends_on('py-scipy@1.4.1:', type=('build', 'run'))
+    depends_on('py-h5py@3:', type=('build', 'run'))
+    depends_on('py-natsort', type=('build', 'run'))
+    depends_on('py-packaging@20:', type=('build', 'run'))
+    depends_on('py-typing-extensions', when='^python@:3.7', type=('build', 'run'))
