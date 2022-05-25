d818843f092d77f618e7de5ad065a03420950b52
Jen Herting <jen@herting.cc>
2022-05-25 16:28:40 -0400

    [py-pathml] added spack import and fixed setuptools type

--- var/spack/repos/builtin/packages/py-pathml/package.py
+++ var/spack/repos/builtin/packages/py-pathml/package.py
@@ -3,6 +3,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack import *
+
+
 class PyPathml(PythonPackage):
     """An open-source toolkit for computational pathology and machine learning."""
 
@@ -11,7 +14,7 @@ class PyPathml(PythonPackage):
 
     version('2.1.0', sha256='462bb2f16452dddad310c30f62678a1336ce492263355fd6722c07ee4840ea6a')
 
-    depends_on('py-setuptools@42:', type=('build', 'run'))
+    depends_on('py-setuptools@42:', type='build')
     depends_on('py-numpy@1.16.4:', type=('build', 'run'))
     depends_on('py-pandas', type=('build', 'run'))
     depends_on('py-scipy', type=('build', 'run'))
