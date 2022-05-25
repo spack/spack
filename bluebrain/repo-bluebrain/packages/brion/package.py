10a735bc717dc877edfbf1958490b361ade808b0
Matthias Wolf <m+git@sushinara.net>
2022-05-25 11:13:16 +0200

    cleanup: remove unused packages. (#1602)

--- bluebrain/repo-bluebrain/packages/brion/package.py
+++ bluebrain/repo-bluebrain/packages/brion/package.py
@@ -16,8 +16,6 @@ class Brion(CMakePackage):
     generator = 'Ninja'
 
     version('develop', submodules=True)
-    version('3.1.0', tag='3.1.0', submodules=True)
-    version('3.2.0', tag='3.2.0', submodules=True)
     version('3.3.0', tag='3.3.0', submodules=True)
     version('3.3.1', tag='3.3.1', submodules=True)
     version('3.3.2', tag='3.3.2', submodules=True)
@@ -41,19 +39,15 @@ class Brion(CMakePackage):
     depends_on('boost +shared', when='~python')
     depends_on('boost +shared +python', when='+python')
 
-    depends_on('libsonata@0.1.2', when='@:3.1.0')
-    depends_on('libsonata', when='@3.2.0:')
+    depends_on('libsonata')
     depends_on('morphio', when='@3.3.5:')
 
     # TODO: bzip2 is a dependency of boost. Needed here because of linking
     # issue (libboost_iostreams.so.1.68.0 not finding libbz2.so)
     depends_on('bzip2')
-    depends_on('lunchbox', when='@3.1.0')
-    depends_on('vmmlib', when='@3.1.0')
     depends_on('highfive@2.3.1 +boost', when='@3.3.8:')
     depends_on('highfive@2.2.2 +boost', when='@3.3.2:3.3.7')
-    depends_on('highfive@2.2.1 +boost', when='@3.2.0:3.3.1')
-    depends_on('highfive@2.1.1 +boost', when='@3.1.0')
+    depends_on('highfive@2.2.1 +boost', when='@:3.3.1')
     depends_on('mvdtool')
     depends_on('glm@:0.9.9.5')
 
@@ -62,27 +56,18 @@ class Brion(CMakePackage):
           when='@3.3.4 ^python@3.9:')
 
     def patch(self):
-        if self.spec.version == Version('3.1.0'):
-            filter_file(r'-py36', r'36 -py36',
-                        'CMake/common/ChoosePython.cmake')
-        if self.spec.satisfies('@3.2.0'):
-            filter_file(r'-Werror', r'# -Werror',
-                        'CMake/CompileOptions.cmake')
-        elif self.spec.satisfies('@3.3.0:'):
-            filter_file(
-                r'-Werror',
-                '-Werror -Wno-error=deprecated-copy -Wno-error=range-loop-construct '
-                '-Wno-error=unused-function',
-                'CMake/CompileOptions.cmake'
-            )
+        filter_file(
+            r'-Werror',
+            '-Werror -Wno-error=deprecated-copy -Wno-error=range-loop-construct '
+            '-Wno-error=unused-function',
+            'CMake/CompileOptions.cmake'
+        )
 
     def cmake_args(self):
         args = ['-DBRION_SKIP_LIBSONATA_SUBMODULE=ON',
                 '-DDISABLE_SUBPROJECTS=0N',
                 '-DBRION_REQUIRE_PYTHON=%s' % ("ON" if "+python" in self.spec
                                                else "OFF")]
-        if self.spec.satisfies('@3.1.0'):
-            args.append('-DCOMMON_DISABLE_WERROR:BOOL=ON')
         return args
 
     def setup_run_environment(self, env):
@@ -107,11 +92,7 @@ class Brion(CMakePackage):
             pathname = os.path.join(target, *site_dir)
             if os.path.isdir(pathname):
                 with working_dir(pathname):
-                    if self.spec.version >= Version('3.1.0') and \
-                       self.spec.version <= Version('3.2.0'):
-                        python('-c', 'import brain; print(brain)')
-                    else:
-                        python('-c', 'import brion; print(brion)')
+                    python('-c', 'import brion; print(brion)')
 
     def _get_site_dir(self):
         return (self.spec['python']
