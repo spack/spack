3bdc8a67b178b4a07e2a8f3fbceb005e8b3f36c3
Sergio <sergio.rivasgomez@epfl.ch>
2022-05-27 11:26:48 +0200

    Bumps arm-forge to v22.0.1 (#1594)
    * Bumps arm-forge to v22.0.1
    * Fix for licensing and SLURM issues on BB5

--- bluebrain/repo-patches/packages/arm-forge/package.py
+++ bluebrain/repo-patches/packages/arm-forge/package.py
@@ -6,13 +6,15 @@ class ArmForge(BuiltinArmForge):
     __doc__ = BuiltinArmForge.__doc__
 
     version(
-        "21.1.2-Linux-x86_64",
-        sha256="ebc99fa3461d2cd968e4d304c11b70cc8d9c5a2acd68681cec2067c128255cd5",
-        url="https://content.allinea.com/downloads/arm-forge-21.1.2-linux-x86_64.tar",
+        "22.0.1-Linux-x86_64",
+        sha256="8f8a61c159665d3de3bc5334ed97bdb4966bfbdb91b65d32d162d489eb2219ac",
+        url="https://content.allinea.com/downloads/arm-forge-22.0.1-linux-x86_64.tar",
     )
 
-    version(
-        "20.2.0-Redhat-7.0-x86_64",
-        sha256="26592a77c42f970f724f15b70cc5ce6af1078fd0ef9243a37c3215916cfa7cf4",
-        url="https://content.allinea.com/downloads/arm-forge-20.2-Redhat-7.0-x86_64.tar",
-    )
+    def setup_run_environment(self, env):
+        # ENV variables required to avoid licensing and SLURM issues on BB5
+        env.unset('HTTP_PROXY')
+        env.unset('HTTPS_PROXY')
+        env.unset('http_proxy')
+        env.unset('https_proxy')
+        env.set('ALLINEA_USE_SSH_STARTUP', "1")
