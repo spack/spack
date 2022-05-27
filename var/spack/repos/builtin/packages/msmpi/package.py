ce4621294567a3ed7e777afd6e022fb051e0a008
Jared Popelar <jpopelar@txcorp.com>
2022-05-26 19:59:42 -0600

    Styling

--- var/spack/repos/builtin/packages/msmpi/package.py
+++ var/spack/repos/builtin/packages/msmpi/package.py
@@ -3,11 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-import os
-import re
-import sys
-
-import spack.build_environment
 from spack import *
 
 
@@ -21,15 +16,14 @@ class Msmpi(Package):
     version('10.0', sha256='7dae13797627726f67fab9c1d251aec2df9ecd25939984645ec05748bdffd396', extension='exe')
 
     provides('mpi')
-    
+
     conflicts('platform=linux')
     conflicts('platform=darwin')
     conflicts('platform=cray')
-    
+
     def url_for_version(self, version):
-      return 'https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/msmpisetup.exe'
-      
+        return 'https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/msmpisetup.exe'
+
     def install(self):
-      installer = Executable('msmpisetup.exe')
-      installer()
-      
+        installer = Executable('msmpisetup.exe')
+        installer()
