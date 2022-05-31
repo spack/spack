ee8c630e8accbf7a8de99fc5203c19ac65bf1b44
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-05-31 14:39:13 -0500

    add version 2.5.1 to r-loo

--- var/spack/repos/builtin/packages/r-bayesplot/package.py
+++ var/spack/repos/builtin/packages/r-bayesplot/package.py
@@ -36,4 +36,4 @@ class RBayesplot(RPackage):
     depends_on('r-tibble@2.0.0:', type=('build', 'run'), when='@1.9.0:')
     depends_on('r-tidyselect', type=('build', 'run'))
     depends_on('pandoc@1.12.3', when='@1.9.0:')
-    depends_on('citeproc', when='@1.9.0:')
+    depends_on('pandoc-citeproc', when='@1.9.0:')
