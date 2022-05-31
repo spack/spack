feb2cdc71dbacb24c8ffc17442709dcabe9a77f7
Derek Ryan Strong <drkrynstrng@pm.me>
2022-05-31 13:57:57 -0700

    Update rclone git repo path

--- var/spack/repos/builtin/packages/rclone/package.py
+++ var/spack/repos/builtin/packages/rclone/package.py
@@ -11,7 +11,7 @@ class Rclone(Package):
        to and from various cloud storage providers"""
 
     homepage = "https://rclone.org"
-    url      = "https://github.com/ncw/rclone/releases/download/v1.57.0/rclone-v1.57.0.tar.gz"
+    url      = "https://github.com/rclone/rclone/releases/download/v1.57.0/rclone-v1.57.0.tar.gz"
 
     maintainers = ['alecbcs']
 
