spack/spack

commit 18c2f1a57a69224748e091e912851317d8a00eea

refactor: packages import `spack.package` explicitly (#30404)
Explicitly import package utilities in all packages, and corresponding fallout.

This includes:

* rename `spack.package` to `spack.package_base`
* rename `spack.pkgkit` to `spack.package`
* update all packages in builtin, builtin_mock and tutorials to include `from spack.package import *`
* update spack style
  * ensure packages include the import
  * automatically add the new import and remove any/all imports of `spack` and `spack.pkgkit`
    from packages when using `--fix`
  * add support for type-checking packages with mypy when SPACK_MYPY_CHECK_PACKAGES
    is set in the environment
* fix all type checking errors in packages in spack upstream
* update spack create to include the new imports
* update spack repo to inject the new import, injection persists to allow for a deprecation period

Original message below:
 
As requested @adamjstewart, update all packages to use pkgkit.  I ended up using isort to do this,
so repro is easy:

```console
$ isort -a 'from spack.pkgkit import *' --rm 'spack' ./var/spack/repos/builtin/packages/*/package.py
$ spack style --fix
```

There were several line spacing fixups caused either by space manipulation in isort or by packages
that haven't been touched since we added requirements, but there are no functional changes in here.

* [x] add config to isort to make sure this is maintained going forward

var/spack/repos/builtin/atompaw/package.py
@@ -1,0 +1,41 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Atompaw(AutotoolsPackage):
+    """atompaw generates projector augmented wave (PAW)
+    potentials for electronics structure calculations."""
+
+    homepage = "https://users.wfu.edu/natalie/papers/pwpaw/"
+    url      = "https://users.wfu.edu/natalie/papers/pwpaw/atompaw-4.2.0.0.tar.gz"
+
+    version('4.2.0.0', sha256='9ab4f4ab78a720fbcd95bbbc1403e8ff348d15570e7c694932a56be15985e93d')
+    version('4.1.1.0', sha256='b1ee2b53720066655d98523ef337e54850cb1e68b3a2da04ff5a1576d3893891')
+    version('4.1.0.6', sha256='42a46c0569367c0b971fbc3dcaf5eaec7020bdff111022b6f320de9f11c41c2c')
+
+    variant('libxc', default=False, description='Compile with libxc')
+    variant('shared', default=True)
+
+    depends_on('libxc', when='+libxc')
+    depends_on('blas')
+
+    def configure_args(self):
+        spec = self.spec
+
+        args = ['--with-linalg-prefix=' + spec['blas'].prefix]
+        args += ['--with-linalg-libs=' + ' '.join(spec['blas'].libs)]
+
+        if '+libxc' in spec:
+            args += ['--enable-libxc']
+            args += ['--with-libxc-prefix=' + spec['libxc'].prefix]
+
+        if '+shared' in spec:
+            args += ['--enable-shared']
+        else:
+            args += ['--enable-static']
+
+        return args

var/spack/repos/builtin.mock/packages/a/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class A(AutotoolsPackage):

var/spack/repos/builtin.mock/packages/archive-files/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class ArchiveFiles(AutotoolsPackage):

var/spack/repos/builtin.mock/packages/autotools-conditional-variants-test/package.py
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
+from spack.package import *
+
+
 class AutotoolsConditionalVariantsTest(AutotoolsPackage):
     homepage = "https://www.example.com"
     has_code = False

var/spack/repos/builtin.mock/packages/autotools-config-replacement/package.py
@@ -5,7 +5,7 @@
 
 import os
 
-from spack import *
+from spack.package import *
 
 
 class AutotoolsConfigReplacement(AutotoolsPackage):

var/spack/repos/builtin.mock/packages/b/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class B(Package):

var/spack/repos/builtin.mock/packages/boost/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class Boost(Package):

commit dc4245f65a163e8b85b18291e5c0aca8a869a954

atompow: remove again misplaced duplicate package
Regression re-introduced in #30404

var/spack/repos/builtin/atompaw/package.py
@@ -1,41 +1,0 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-from spack.package import *
-
-
-class Atompaw(AutotoolsPackage):
-    """atompaw generates projector augmented wave (PAW)
-    potentials for electronics structure calculations."""
-
-    homepage = "https://users.wfu.edu/natalie/papers/pwpaw/"
-    url      = "https://users.wfu.edu/natalie/papers/pwpaw/atompaw-4.2.0.0.tar.gz"
-
-    version('4.2.0.0', sha256='9ab4f4ab78a720fbcd95bbbc1403e8ff348d15570e7c694932a56be15985e93d')
-    version('4.1.1.0', sha256='b1ee2b53720066655d98523ef337e54850cb1e68b3a2da04ff5a1576d3893891')
-    version('4.1.0.6', sha256='42a46c0569367c0b971fbc3dcaf5eaec7020bdff111022b6f320de9f11c41c2c')
-
-    variant('libxc', default=False, description='Compile with libxc')
-    variant('shared', default=True)
-
-    depends_on('libxc', when='+libxc')
-    depends_on('blas')
-
-    def configure_args(self):
-        spec = self.spec
-
-        args = ['--with-linalg-prefix=' + spec['blas'].prefix]
-        args += ['--with-linalg-libs=' + ' '.join(spec['blas'].libs)]
-
-        if '+libxc' in spec:
-            args += ['--enable-libxc']
-            args += ['--with-libxc-prefix=' + spec['libxc'].prefix]
-
-        if '+shared' in spec:
-            args += ['--enable-shared']
-        else:
-            args += ['--enable-static']
-
-        return args