5b751488e1860e643516e4a80d2abee50545431a
Brice Videau <bvideau@anl.gov>
2022-06-06 20:13:00 +0000

    Fix libunwind dependency.

--- var/spack/repos/builtin/packages/py-py-spy/package.py
+++ var/spack/repos/builtin/packages/py-py-spy/package.py
@@ -19,7 +19,7 @@ class PyPySpy(Package):
     # Need to figure out how to manage these with Spack once we have a
     # CargoPackage base class.
     depends_on('rust', type='build')
-    depends_on('unwind')
+    depends_on('libunwind components=ptrace')
 
     def install(self, spec, prefix):
         cargo = which('cargo')
