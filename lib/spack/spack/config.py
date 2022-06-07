ca926b8c8284e6f1800e3efadfe8fae8b97e75d8
Marc Mengel <mengel@fnal.gov>
2022-06-07 15:49:22 -0500

    make flake8 happy

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -749,13 +749,6 @@ def _add_platform_scope(cfg, scope_type, name, path):
     plat_path = os.path.join(path, platform)
     cfg.push_scope(scope_type(plat_name, plat_path))
 
-def _add_os_scope(cfg, scope_type, name, path):
-    """Add an os-specific subdirectory for the current platform."""
-    host_platform = spack.platforms.host()
-    oss = host_platform.operating_system('frontend')
-    os_name = '%s/%s' % (name, oss)
-    os_path = '%s/%s' % (path, oss)
-    cfg.push_scope(scope_type(os_name, os_path))
 
 def _add_os_scope(cfg, scope_type, name, path):
     """Add an os-specific subdirectory for the current platform."""
