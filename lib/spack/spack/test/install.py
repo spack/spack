spack/spack

commit 1aa7758dbb572aa0f92efe1e1f61b2df37aae2e5

match .spack literal, not as a regex (#26374)

lib/spack/spack/package.py
@@ -1952,7 +1952,7 @@ class PackageBase(six.with_metaclass(PackageMeta, PackageViewMixin, object)):
 
         installed = set(os.listdir(self.prefix))
         installed.difference_update(
-            spack.store.layout.hidden_file_paths)
+            spack.store.layout.hidden_file_regexes)
         if not installed:
             raise InstallError(
                 "Install failed for %s.  Nothing was installed!" % self.name)

lib/spack/spack/directory_layout.py
@@ -6,6 +6,7 @@
 import errno
 import glob
 import os
+import re
 import shutil
 import tempfile
 from contextlib import contextmanager
@@ -88,8 +89,8 @@ class DirectoryLayout(object):
         self.manifest_file_name  = 'install_manifest.json'
 
     @property
-    def hidden_file_paths(self):
-        return (self.metadata_dir,)
+    def hidden_file_regexes(self):
+        return (re.escape(self.metadata_dir),)
 
     def relative_path_for_spec(self, spec):
         _check_concrete(spec)

lib/spack/spack/filesystem_view.py
@@ -406,7 +406,7 @@ class YamlFilesystemView(FilesystemView):
 
         ignore = ignore or (lambda f: False)
         ignore_file = match_predicate(
-            self.layout.hidden_file_paths, ignore)
+            self.layout.hidden_file_regexes, ignore)
 
         # check for dir conflicts
         conflicts = tree.find_dir_conflicts(view_dst, ignore_file)
@@ -432,7 +432,7 @@ class YamlFilesystemView(FilesystemView):
 
         ignore = ignore or (lambda f: False)
         ignore_file = match_predicate(
-            self.layout.hidden_file_paths, ignore)
+            self.layout.hidden_file_regexes, ignore)
 
         merge_map = tree.get_file_map(view_dst, ignore_file)
         pkg.remove_files_from_view(self, merge_map)

commit 3495926a93f4715b208c500f1551b609e2364c59

Fix empty install prefix post-install sanity check
Regression since #26374.

lib/spack/spack/package_base.py
@@ -33,7 +33,7 @@ import six
 
 import llnl.util.filesystem as fsys
 import llnl.util.tty as tty
-from llnl.util.lang import memoized, nullcontext
+from llnl.util.lang import match_predicate, memoized, nullcontext
 from llnl.util.link_tree import LinkTree
 
 import spack.compilers
@@ -2178,10 +2178,8 @@ class PackageBase(six.with_metaclass(PackageMeta, PackageViewMixin, object)):
         check_paths(self.sanity_check_is_file, 'file', os.path.isfile)
         check_paths(self.sanity_check_is_dir, 'directory', os.path.isdir)
 
-        installed = set(os.listdir(self.prefix))
-        installed.difference_update(
-            spack.store.layout.hidden_file_regexes)
-        if not installed:
+        ignore_file = match_predicate(spack.store.layout.hidden_file_regexes)
+        if all(map(ignore_file, os.listdir(self.prefix))):
             raise InstallError(
                 "Install failed for %s.  Nothing was installed!" % self.name)
 

var/spack/repos/builtin.mock/packages/failing-empty-install/package.py
@@ -1,0 +1,16 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack.package import *
+
+
+class FailingEmptyInstall(Package):
+    """This package installs nothing, install should fail."""
+    homepage = "http://www.example.com/trivial_install"
+    url      = "http://www.unit-test-should-replace-this-url/trivial_install-1.0.tar.gz"
+
+    version('1.0', '0123456789abcdef0123456789abcdef')
+
+    def install(self, spec, prefix):
+        pass

lib/spack/spack/test/install.py
@@ -612,3 +612,16 @@ def test_install_error():
         assert exc.__class__.__name__ == 'InstallError'
         assert exc.message == msg
         assert exc.long_message == long_msg
+
+
+@pytest.mark.disable_clean_stage_check
+def test_empty_install_sanity_check_prefix(
+        monkeypatch, install_mockery, mock_fetch, mock_packages
+):
+    """Test empty install triggers sanity_check_prefix."""
+    spec = Spec('failing-empty-install').concretized()
+    with pytest.raises(
+        spack.build_environment.ChildError,
+        match='Nothing was installed'
+    ):
+        spec.package.do_install()