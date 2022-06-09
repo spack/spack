a491e005cdd405eb0daaac4ff3a85c3c1b783ba2
Gerald Ragghianti <ragghianti@icl.utk.edu>
2022-06-08 21:09:04 -0400

    Fixed dependency on mesa so that it will be compatible with hip build

--- var/spack/repos/builtin/packages/hip/package.py
+++ var/spack/repos/builtin/packages/hip/package.py
@@ -44,6 +44,7 @@ class Hip(CMakePackage):
     depends_on('cmake@3.4.3:', type='build')
     depends_on('perl@5.10:', type=('build', 'run'))
     depends_on('gl@4.5:')
+    depends_on('mesa~llvm', when="^mesa")
 
     for ver in ['3.5.0', '3.7.0', '3.8.0', '3.9.0', '3.10.0', '4.0.0', '4.1.0',
                 '4.2.0', '4.3.0', '4.3.1']:
