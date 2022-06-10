spack/spack

commit 0c2de252f1e33d293bb8832e2053d1b4a96eed16

introduce `llnl.util.compat` to remove sys.version_info checks (#21720)
- also split typing.py into typing_extensions and add py2 shims

lib/spack/spack/util/spack_json.py
@@ -5,13 +5,13 @@
 
 """Simple wrapper around JSON to guarantee consistent use of load/dump. """
 import json
-import sys
+from typing import Any, Dict, Optional  # novm
 
-from six import iteritems, string_types
+from six import PY3, iteritems, string_types
 
 import spack.error
 
-__all__ = ['load', 'dump', 'SpackJSONError']
+__all__ = ['load', 'dump', 'SpackJSONError', 'encode_json_dict', 'decode_json_dict']
 
 _json_dump_args = {
     'indent': 2,
@@ -20,27 +20,45 @@ _json_dump_args = {
 
 
 def load(stream):
+    # type: (Any) -> Dict
     """Spack JSON needs to be ordered to support specs."""
     if isinstance(stream, string_types):
-        load = json.loads
+        load = json.loads       # type: ignore[assignment]
     else:
-        load = json.load
+        load = json.load        # type: ignore[assignment]
 
     return _strify(load(stream, object_hook=_strify), ignore_dicts=True)
 
 
+def encode_json_dict(data):
+    # type: (Dict) -> Dict
+    """Converts python 2 unicodes to str in JSON data."""
+    return _strify(data)
+
+
 def dump(data, stream=None):
+    # type: (Dict, Optional[Any]) -> Optional[str]
     """Dump JSON with a reasonable amount of indentation and separation."""
+    data = _strify(data)
     if stream is None:
-        return json.dumps(data, **_json_dump_args)
-    else:
-        return json.dump(data, stream, **_json_dump_args)
+        return json.dumps(data, **_json_dump_args)     # type: ignore[arg-type]
+    json.dump(data, stream, **_json_dump_args)         # type: ignore[arg-type]
+    return None
+
+
+def decode_json_dict(data):
+    # type: (Dict) -> Dict
+    """Converts str to python 2 unicodes in JSON data."""
+    return _strify(data)
 
 
 def _strify(data, ignore_dicts=False):
-    """Converts python 2 unicodes to str in JSON data."""
+    # type: (Dict, bool) -> Dict
+    """Helper method for ``encode_json_dict()`` and ``decode_json_dict()``.
+
+    Converts python 2 unicodes to str in JSON data, or the other way around."""
     # this is a no-op in python 3
-    if sys.version_info[0] >= 3:
+    if PY3:
         return data
 
     # if this is a unicode string in python 2, return its string representation
@@ -66,4 +84,5 @@ class SpackJSONError(spack.error.SpackError):
     """Raised when there are issues with JSON parsing."""
 
     def __init__(self, msg, json_error):
+        # type: (str, BaseException) -> None
         super(SpackJSONError, self).__init__(msg, str(json_error))

lib/spack/spack/test/spec_yaml.py
@@ -11,9 +11,10 @@ YAML format preserves DAG information in the spec.
 import ast
 import inspect
 import os
-import sys
 
 import pytest
+
+from llnl.util.compat import Iterable, Mapping
 
 import spack.hash_types as ht
 import spack.spec
@@ -24,11 +25,6 @@ from spack import repo
 from spack.spec import Spec, save_dependency_specfiles
 from spack.util.mock_package import MockPackageMultiRepo
 from spack.util.spack_yaml import SpackYAMLError, syaml_dict
-
-if sys.version_info >= (3, 3):
-    from collections.abc import Iterable, Mapping  # novm
-else:
-    from collections import Iterable, Mapping
 
 
 def check_yaml_round_trip(spec):

lib/spack/external/py2/typing.py
@@ -6,79 +6,77 @@
 This is a fake set of symbols to allow spack to import typing in python
 versions where we do not support type checking (<3)
 """
-Annotated = None
-Any = None
-Callable = None
-ForwardRef = None
-Generic = None
-Literal = None
-Optional = None
-Tuple = None
-TypeVar = None
-Union = None
-AbstractSet = None
-ByteString = None
-Container = None
-Hashable = None
-ItemsView = None
-Iterable = None
-Iterator = None
-KeysView = None
-Mapping = None
-MappingView = None
-MutableMapping = None
-MutableSequence = None
-MutableSet = None
-Sequence = None
-Sized = None
-ValuesView = None
-Awaitable = None
-AsyncIterator = None
-AsyncIterable = None
-Coroutine = None
-Collection = None
-AsyncGenerator = None
-AsyncContextManager = None
-Reversible = None
-SupportsAbs = None
-SupportsBytes = None
-SupportsComplex = None
-SupportsFloat = None
-SupportsInt = None
-SupportsRound = None
-ChainMap = None
-Dict = None
-List = None
-OrderedDict = None
-Set = None
-FrozenSet = None
-NamedTuple = None
-Generator = None
-AnyStr = None
-cast = None
+from collections import defaultdict
+
+# (1) Unparameterized types.
+Annotated = object
+Any = object
+AnyStr = object
+ByteString = object
+Counter = object
+Final = object
+Hashable = object
+NoReturn = object
+Sized = object
+SupportsAbs = object
+SupportsBytes = object
+SupportsComplex = object
+SupportsFloat = object
+SupportsIndex = object
+SupportsInt = object
+SupportsRound = object
+
+# (2) Parameterized types.
+AbstractSet = defaultdict(lambda: object)
+AsyncContextManager = defaultdict(lambda: object)
+AsyncGenerator = defaultdict(lambda: object)
+AsyncIterable = defaultdict(lambda: object)
+AsyncIterator = defaultdict(lambda: object)
+Awaitable = defaultdict(lambda: object)
+Callable = defaultdict(lambda: object)
+ChainMap = defaultdict(lambda: object)
+ClassVar = defaultdict(lambda: object)
+Collection = defaultdict(lambda: object)
+Container = defaultdict(lambda: object)
+ContextManager = defaultdict(lambda: object)
+Coroutine = defaultdict(lambda: object)
+DefaultDict = defaultdict(lambda: object)
+Deque = defaultdict(lambda: object)
+Dict = defaultdict(lambda: object)
+ForwardRef = defaultdict(lambda: object)
+FrozenSet = defaultdict(lambda: object)
+Generator = defaultdict(lambda: object)
+Generic = defaultdict(lambda: object)
+ItemsView = defaultdict(lambda: object)
+Iterable = defaultdict(lambda: object)
+Iterator = defaultdict(lambda: object)
+KeysView = defaultdict(lambda: object)
+List = defaultdict(lambda: object)
+Literal = defaultdict(lambda: object)
+Mapping = defaultdict(lambda: object)
+MappingView = defaultdict(lambda: object)
+MutableMapping = defaultdict(lambda: object)
+MutableSequence = defaultdict(lambda: object)
+MutableSet = defaultdict(lambda: object)
+NamedTuple = defaultdict(lambda: object)
+Optional = defaultdict(lambda: object)
+OrderedDict = defaultdict(lambda: object)
+Reversible = defaultdict(lambda: object)
+Sequence = defaultdict(lambda: object)
+Set = defaultdict(lambda: object)
+Tuple = defaultdict(lambda: object)
+Type = defaultdict(lambda: object)
+TypedDict = defaultdict(lambda: object)
+Union = defaultdict(lambda: object)
+ValuesView = defaultdict(lambda: object)
+
+# (3) Type variable declarations.
+TypeVar = lambda *args, **kwargs: None
+
+# (4) Functions.
+cast = lambda _type, x: x
 get_args = None
 get_origin = None
 get_type_hints = None
 no_type_check = None
 no_type_check_decorator = None
-NoReturn = None
-
-# these are the typing extension symbols
-ClassVar = None
-Final = None
-Protocol = None
-Type = None
-TypedDict = None
-ContextManager = None
-Counter = None
-Deque = None
-DefaultDict = None
-SupportsIndex = None
-final = None
-IntVar = None
-Literal = None
-NewType = None
-overload = None
-runtime_checkable = None
-Text = None
-TYPE_CHECKING = None

lib/spack/external/py2/typing_extensions.py
@@ -1,0 +1,26 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+"""
+This is a fake set of symbols to allow spack to import typing in python
+versions where we do not support type checking (<3)
+"""
+from collections import defaultdict
+
+# (1) Unparameterized types.
+IntVar = object
+Literal = object
+NewType = object
+Text = object
+
+# (2) Parameterized types.
+Protocol = defaultdict(lambda: object)
+
+# (3) Macro for avoiding evaluation except during type checking.
+TYPE_CHECKING = False
+
+# (4) Decorators.
+final = lambda x: x
+overload = lambda x: x
+runtime_checkable = lambda x: x

lib/spack/llnl/util/compat.py
@@ -1,0 +1,39 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+# isort: off
+
+import sys
+
+if sys.version_info < (3,):
+    from itertools import ifilter as filter
+    from itertools import imap as map
+    from itertools import izip as zip
+    from itertools import izip_longest as zip_longest  # novm
+    from urllib import urlencode as urlencode
+    from urllib import urlopen as urlopen
+else:
+    filter = filter
+    map = map
+    zip = zip
+    from itertools import zip_longest as zip_longest  # novm # noqa: F401
+    from urllib.parse import urlencode as urlencode   # novm # noqa: F401
+    from urllib.request import urlopen as urlopen     # novm # noqa: F401
+
+if sys.version_info >= (3, 3):
+    from collections.abc import Hashable as Hashable                # novm
+    from collections.abc import Iterable as Iterable                # novm
+    from collections.abc import Mapping as Mapping                  # novm
+    from collections.abc import MutableMapping as MutableMapping    # novm
+    from collections.abc import MutableSequence as MutableSequence  # novm
+    from collections.abc import MutableSet as MutableSet            # novm
+    from collections.abc import Sequence as Sequence                # novm
+else:
+    from collections import Hashable as Hashable                # noqa: F401
+    from collections import Iterable as Iterable                # noqa: F401
+    from collections import Mapping as Mapping                  # noqa: F401
+    from collections import MutableMapping as MutableMapping    # noqa: F401
+    from collections import MutableSequence as MutableSequence  # noqa: F401
+    from collections import MutableSet as MutableSet            # noqa: F401
+    from collections import Sequence as Sequence                # noqa: F401

lib/spack/llnl/util/filesystem.py
@@ -21,14 +21,10 @@ from contextlib import contextmanager
 import six
 
 from llnl.util import tty
+from llnl.util.compat import Sequence
 from llnl.util.lang import dedupe, memoized
 
 from spack.util.executable import Executable
-
-if sys.version_info >= (3, 3):
-    from collections.abc import Sequence  # novm
-else:
-    from collections import Sequence
 
 __all__ = [
     'FileFilter',

lib/spack/llnl/util/lang.py
@@ -14,17 +14,7 @@ from datetime import datetime, timedelta
 
 from six import string_types
 
-if sys.version_info < (3, 0):
-    from itertools import izip_longest  # novm
-    zip_longest = izip_longest
-else:
-    from itertools import zip_longest  # novm
-
-if sys.version_info >= (3, 3):
-    from collections.abc import Hashable, MutableMapping  # novm
-else:
-    from collections import Hashable, MutableMapping
-
+from llnl.util.compat import Hashable, MutableMapping, zip_longest
 
 # Ignore emacs backups when listing modules
 ignore_modules = [r'^\.#', '~$']

lib/spack/spack/ci_needs_workaround.py
@@ -3,18 +3,11 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-import collections
-
-try:
-    # dynamically import to keep vermin from complaining
-    collections_abc = __import__('collections.abc')
-except ImportError:
-    collections_abc = collections
-
+from llnl.util.compat import Mapping
 
 get_job_name = lambda needs_entry: (
     needs_entry.get('job') if (
-        isinstance(needs_entry, collections_abc.Mapping) and
+        isinstance(needs_entry, Mapping) and
         needs_entry.get('artifacts', True))
 
     else
@@ -25,7 +18,7 @@ get_job_name = lambda needs_entry: (
 
 
 def convert_job(job_entry):
-    if not isinstance(job_entry, collections_abc.Mapping):
+    if not isinstance(job_entry, Mapping):
         return job_entry
 
     needs = job_entry.get('needs')

commit 4def246bd450c64f25c179a0eed693cf63946d3d

bugfix: preserve dict order for `Spec.dag_hash()` in Python 2
Fix a bug introduced in #21720. `spack_json.dump()` calls `_strify()` on dictionaries to
convert `unicode` to `str`, but it constructs `dict` objects instead of
`collections.OrderedDict` objects, so in Python 2 (or earlier versions of 3) it can
scramble dictionary order.

This can cause hashes to differ between Python 2 and Python 3, or between Python 3.7
and earlier Python 3's.

- [x] use `OrderedDict` in `_strify`
- [x] add a regression test

lib/spack/spack/util/spack_json.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 """Simple wrapper around JSON to guarantee consistent use of load/dump. """
+import collections
 import json
 from typing import Any, Dict, Optional  # novm
 
@@ -72,9 +73,10 @@ def _strify(data, ignore_dicts=False):
     # if this is a dictionary, return dictionary of byteified keys and values
     # but only if we haven't already byteified it
     if isinstance(data, dict) and not ignore_dicts:
-        return dict((_strify(key, ignore_dicts=True),
-                     _strify(value, ignore_dicts=True)) for key, value in
-                    iteritems(data))
+        return collections.OrderedDict(
+            (_strify(key, ignore_dicts=True), _strify(value, ignore_dicts=True))
+            for key, value in iteritems(data)
+        )
 
     # if it's anything else, return it in its original form
     return data

lib/spack/spack/test/spec_yaml.py
@@ -8,7 +8,10 @@
 The YAML and JSON formats preserve DAG information in the spec.
 
 """
+from __future__ import print_function
+
 import ast
+import collections
 import inspect
 import os
 
@@ -433,3 +436,75 @@ spec:
     spec = Spec.from_yaml(yaml)
     concrete_spec = spec.concretized()
     assert concrete_spec.eq_dag(spec)
+
+
+#: A well ordered Spec dictionary, using ``OrderdDict``.
+#: Any operation that transforms Spec dictionaries should
+#: preserve this order.
+ordered_spec = collections.OrderedDict([
+    ("arch", collections.OrderedDict([
+        ("platform", "darwin"),
+        ("platform_os", "bigsur"),
+        ("target", collections.OrderedDict([
+            ("features", [
+                "adx",
+                "aes",
+                "avx",
+                "avx2",
+                "bmi1",
+                "bmi2",
+                "clflushopt",
+                "f16c",
+                "fma",
+                "mmx",
+                "movbe",
+                "pclmulqdq",
+                "popcnt",
+                "rdrand",
+                "rdseed",
+                "sse",
+                "sse2",
+                "sse4_1",
+                "sse4_2",
+                "ssse3",
+                "xsavec",
+                "xsaveopt"
+            ]),
+            ("generation", 0),
+            ("name", "skylake"),
+            ("parents", ["broadwell"]),
+            ("vendor", "GenuineIntel"),
+        ])),
+    ])),
+    ("compiler", collections.OrderedDict([
+        ("name", "apple-clang"),
+        ("version", "13.0.0"),
+    ])),
+    ("name", "zlib"),
+    ("namespace", "builtin"),
+    ("parameters", collections.OrderedDict([
+        ("cflags", []),
+        ("cppflags", []),
+        ("cxxflags", []),
+        ("fflags", []),
+        ("ldflags", []),
+        ("ldlibs", []),
+        ("optimize", True),
+        ("pic", True),
+        ("shared", True),
+    ])),
+    ("version", "1.2.11"),
+])
+
+
+@pytest.mark.regression("31092")
+def test_strify_preserves_order():
+    """Ensure that ``spack_json._strify()`` dumps dictionaries in the right order.
+
+    ``_strify()`` is used in ``spack_json.dump()``, which is used in
+    ``Spec.dag_hash()``, so if this goes wrong, ``Spec`` hashes can vary between python
+    versions.
+
+    """
+    strified = sjson._strify(ordered_spec)
+    assert list(ordered_spec.items()) == list(strified.items())