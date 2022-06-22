spack/spack

commit bc03b5caafe05439e5a33968ef821d7d14901fd9

meson: fix to recognize intel oneapi compiler (#30605)
Signed-off-by: Howard Pritchard <howardp@lanl.gov>

var/spack/repos/builtin/packages/meson/package.py
@@ -64,6 +64,10 @@ class Meson(PythonPackage):
     patch('rpath-0.54.patch', when='@0.54:0.55')
     patch('rpath-0.56.patch', when='@0.56:0.57')
     patch('rpath-0.58.patch', when='@0.58:')
+    # Help meson recognize Intel OneAPI compilers
+    patch('https://patch-diff.githubusercontent.com/raw/mesonbuild/meson/pull/9850.patch',
+          sha256='72c77637f4dd5f8af5788dacf01c71d921cd4073af0aa0641a4b5111c50f59cb',
+          when='@0.60.0:')
 
     executables = ['^meson$']
 

commit f8eb5e27990de70380071b99e44217b24892b507

Fix patch scheme
Issues introduced with #30605 and not detected in CI
until it landed in develop.

var/spack/repos/builtin/packages/meson/package.py
@@ -65,8 +65,8 @@ class Meson(PythonPackage):
     patch('rpath-0.56.patch', when='@0.56:0.57')
     patch('rpath-0.58.patch', when='@0.58:')
     # Help meson recognize Intel OneAPI compilers
-    patch('https://patch-diff.githubusercontent.com/raw/mesonbuild/meson/pull/9850.patch',
-          sha256='72c77637f4dd5f8af5788dacf01c71d921cd4073af0aa0641a4b5111c50f59cb',
+    patch('https://patch-diff.githubusercontent.com/raw/mesonbuild/meson/pull/9850.patch?full_index=1',
+          sha256='9c874726ce0a06922580d3e3d6adbe74e5144b3a661ef1059f32c9c1bc478b65',
           when='@0.60.0:')
 
     executables = ['^meson$']