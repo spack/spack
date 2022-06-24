ee80246674dbc798df41f4a4bac00319301056ab
Luke Diorio-Toth <led2386@gmail.com>
2022-06-24 09:14:56 -0500

    added setuptools dependency

--- var/spack/repos/builtin/packages/py-gtdbtk/package.py
+++ var/spack/repos/builtin/packages/py-gtdbtk/package.py
@@ -18,6 +18,7 @@ class PyGtdbtk(PythonPackage):
     version('2.1.0', sha256='980885141f13502afdf05e720871427e3de4fe27f4f3f97e74af6fed87eb50a7')
 
     depends_on('python@3.6:', type=('build', 'run'))
+    depends_on('py-setuptools', type=('build'))
     depends_on('py-dendropy@4.1.0:', type=('build', 'run'))
     depends_on('py-numpy@1.9.0:',        type=('build', 'run'))
     depends_on('py-tqdm@4.35.0:',        type=('build', 'run'))
