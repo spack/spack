633a61e507df6a63abf2e3018b1118dcd68d4155
Valentin Volkl <valentin.volkl@cern.ch>
2022-06-27 23:41:23 +0200

    ampt: add atlas patch

--- var/spack/repos/builtin/packages/ampt/package.py
+++ var/spack/repos/builtin/packages/ampt/package.py
@@ -18,7 +18,12 @@ class Ampt(MakefilePackage):
 
     tags = ['hep']
 
-    version('v2.26t9', sha256='d90e2e5a88f93baec69bc56d67ed8e35dc243fd1e5104e551e8d7e69fd09716a')
+    patch('https://gitlab.cern.ch/sft/lcgcmake/-/raw/master/generators/patches/ampt-2.26t9b_atlas.patch',
+          sha256='7a9a4f175f84dc3021301dae5d48adab1fc714fccf44ec17128a3ba1608bff4c',
+          when='@2.26t9b_atlas')
+
+    version('2.26t9b_atlas', sha256='d90e2e5a88f93baec69bc56d67ed8e35dc243fd1e5104e551e8d7e69fd09716a')
+    version('2.26t9', sha256='d90e2e5a88f93baec69bc56d67ed8e35dc243fd1e5104e551e8d7e69fd09716a')
 
     def install(self, spec, prefix):
         mkdir(prefix.bin)
