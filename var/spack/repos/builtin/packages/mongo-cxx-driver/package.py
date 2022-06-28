2f987b724e97e2af76ace86ca64065c4650deb33
Adam J. Stewart <ajstewart426@gmail.com>
2022-06-28 15:03:15 -0700

    Remove unrelated changes

--- var/spack/repos/builtin/packages/mongo-cxx-driver/package.py
+++ var/spack/repos/builtin/packages/mongo-cxx-driver/package.py
@@ -13,7 +13,6 @@ class MongoCxxDriver(CMakePackage):
     homepage = "http://www.mongocxx.org"
     url      = "https://github.com/mongodb/mongo-cxx-driver/archive/r3.2.0.tar.gz"
 
-    version('3.6.7', sha256='a9244d3117d4029a2f039dece242eef10e34502e4600e2afa968ab53589e6de7')
-    version('3.2.0', sha256='e26edd44cf20bd6be91907403b6d63a065ce95df4c61565770147a46716aad8c')
+    version('3.2.0',     sha256='e26edd44cf20bd6be91907403b6d63a065ce95df4c61565770147a46716aad8c')
 
     depends_on('mongo-c-driver@1.9.2:')
