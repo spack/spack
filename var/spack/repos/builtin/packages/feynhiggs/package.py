f948ec07ccaad9515df50cf7a838602738915b90
Valentin Volkl <valentin.volkl@cern.ch>
2022-06-28 11:05:29 +0200

    feynhiggs: add hep tag (#31325)
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/feynhiggs/package.py
+++ var/spack/repos/builtin/packages/feynhiggs/package.py
@@ -16,6 +16,7 @@ class Feynhiggs(AutotoolsPackage):
     url      = "https://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/FeynHiggs-2.18.1.tar.gz"
 
     maintainers = ['vvolkl']
+    tags = ['hep']
 
     version('2.18.1', sha256='a9cdc4e2759f96fb9bd981b7be1ba8df070fb20c46d5b95e0c9700fccafe5ee6')
 
