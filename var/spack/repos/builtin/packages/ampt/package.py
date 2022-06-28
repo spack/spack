e7a9d82ef6c5ce729dd4906513d76e6d893e079a
Valentin Volkl <valentin.volkl@cern.ch>
2022-06-28 09:50:15 +0200

    Update var/spack/repos/builtin/packages/ampt/package.py
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/ampt/package.py
+++ var/spack/repos/builtin/packages/ampt/package.py
@@ -20,7 +20,7 @@ class Ampt(MakefilePackage):
 
     patch('https://gitlab.cern.ch/sft/lcgcmake/-/raw/master/generators/patches/ampt-2.26t9b_atlas.patch', level=0,
           sha256='7a9a4f175f84dc3021301dae5d48adab1fc714fccf44ec17128a3ba1608bff4c',
-          when='@2.26t9b_atlas')
+          when='@2.26-t9b_atlas')
 
     version('2.26-t9b_atlas', sha256='9441b5f77c2ab91a57b291abd4afd12de7968f9cbe9f3cc8dbe60fbf5293ed55',
             url='http://myweb.ecu.edu/linz/ampt/ampt-v1.26t9b-v2.26t9b.zip')
