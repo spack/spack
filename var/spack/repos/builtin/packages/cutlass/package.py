675adfa54af20031f54cf282cade362b857f3eb9
WuK <i@wu-kan.cn>
2022-06-30 06:04:21 +0800

    cutlass: adding a message for cuda_arch
    Co-authored-by: Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>

--- var/spack/repos/builtin/packages/cutlass/package.py
+++ var/spack/repos/builtin/packages/cutlass/package.py
@@ -19,7 +19,10 @@ class Cutlass(CMakePackage, CudaPackage):
     version(
         '2.9.0', sha256="ccca4685739a3185e3e518682845314b07a5d4e16d898b10a3c3a490fd742fb4")
     variant('cuda', default=True, description='Build with CUDA')
-    conflicts('~cuda')
+    conflicts('~cuda', msg='Cutlass requires CUDA')
+    conflicts('cuda_arch=none',
+                    msg='Must specify CUDA compute capabilities of your GPU, see '
+                    'https://developer.nvidia.com/cuda-gpus')
 
     def setup_build_environment(self, env):
         env.set('CUDACXX', self.spec["cuda"].prefix.bin.nvcc)
