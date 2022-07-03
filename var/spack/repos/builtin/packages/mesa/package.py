11e6573eccfd31c9b8d0de4da90f0b1ad4097f20
Cordell Bloor <Cordell.Bloor@amd.com>
2022-07-03 07:15:54 -0600

    Update patch name

--- var/spack/repos/builtin/packages/mesa/package.py
+++ var/spack/repos/builtin/packages/mesa/package.py
@@ -125,7 +125,7 @@ class Mesa(MesonPackage):
     # requires native to be added to llvm_modules when using gallium swrast
     patch('https://cgit.freedesktop.org/mesa/mesa/patch/meson.build?id=054dd668a69acc70d47c73abe4646e96a1f23577', sha256='36096a178070e40217945e12d542dfe80016cb897284a01114d616656c577d73', when='@21.0.0:21.0.3')
 
-    patch('mesa_check_llvm_version_suffix.patch', when='@21.2.3:')
+    patch('handle_missing_set_override_stack_alignment.patch', when='@21.2.3:')
 
     # 'auto' needed when shared llvm is built
     @when('^llvm~shared_libs')
