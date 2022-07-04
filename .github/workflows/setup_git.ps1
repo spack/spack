1092ad26b02258bccad411790f39e25006b80dfa
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-07-04 16:58:05 +0200

    Try fixing the CI issue in Windows

--- .github/workflows/setup_git.ps1
+++ .github/workflows/setup_git.ps1
@@ -9,4 +9,5 @@ git config --global core.longpaths true
 if ($(git branch --show-current) -ne "develop")
 {
     git branch develop origin/develop
+    git branch releases/v0.18 origin/releases/v0.18
 }
