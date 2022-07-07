7cad826ce40834b995cdaf703faf06deeaeff891
Erin Newcomer <erinpnewcomer@gmail.com>
2022-07-07 12:08:51 -0500

    py-phylophlan: fixed dependencies

--- var/spack/repos/builtin/packages/py-phylophlan/package.py
+++ var/spack/repos/builtin/packages/py-phylophlan/package.py
@@ -11,14 +11,17 @@ class PyPhylophlan(PythonPackage):
     phylogenetic profiling of genomes and metagenomes."""
 
     homepage = "https://github.com/biobakery/phylophlan"
-    url      = "https://github.com/biobakery/phylophlan/archive/refs/tags/3.0.2.tar.gz"
+    url = "https://github.com/biobakery/phylophlan/archive/refs/tags/3.0.2.tar.gz"
 
     version('3.0.2', sha256='c342116662bbfbb49f0665291fc7c0be5a0d04a02a7be2da81de0322eb2256b4')
 
-    depends_on('python@3:')
-    depends_on('py-numpy@1.12.1:')
-    depends_on('py-biopython@1.70:')
-    depends_on('py-dendropy@4.2.0:')
-    depends_on('muscle')
-    depends_on('blast-plus')
-    depends_on('diamond')
+    depends_on('python@3:', type=('build', 'run'))
+    depends_on('py-numpy@1.12.1:', type=('build', 'run'))
+    depends_on('py-biopython@1.70:', type=('build', 'run'))
+    depends_on('py-dendropy@4.2.0:', type=('build', 'run'))
+    depends_on('muscle', type=('build', 'run'))
+    depends_on('blast-plus', type=('build', 'run'))
+    depends_on('diamond', type=('build', 'run'))
+    depends_on('py-matplotlib', type=('build','run'))
+    depends_on('py-pandas', type=('build','run'))
+    depends_on('py-seaborn', type=('build','run'))
