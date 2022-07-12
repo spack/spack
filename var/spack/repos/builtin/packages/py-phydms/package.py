1d4bdfae6433cad35966836a4904f2bfbed8ef42
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-07-12 18:16:53 -0500

    added dependencies and new packages

--- var/spack/repos/builtin/packages/py-phydms/package.py
+++ var/spack/repos/builtin/packages/py-phydms/package.py
@@ -20,3 +20,17 @@ class PyPhydms(PythonPackage):
 
     depends_on('python@3.5:',   type=('build', 'run'))
     depends_on('py-setuptools', type='build')
+
+    depends_on('py-biopython@1.67:',   type=('build', 'run'))
+    depends_on('py-cython@0.28:',      type=('build', 'run'))
+    depends_on('py-numpy@1.16.5:',     type=('build', 'run'))
+    depends_on('py-scipy@0.18:',       type=('build', 'run'))
+    depends_on('py-matplotlib@2.0.2:', type=('build', 'run'))
+    depends_on('py-natsort@5.0.1:',    type=('build', 'run'))
+    depends_on('py-sympy@1.0:',        type=('build', 'run'))
+    depends_on('py-six@1.10:',         type=('build', 'run'))
+    depends_on('py-pandas@0.20.2:',    type=('build', 'run'))
+    depends_on('py-pyvolve@1.0.3:',    type=('build', 'run'))
+    depends_on('py-statsmodels@0.8:',  type=('build', 'run'))
+    depends_on('py-weblogo@3.4:3.5',   type=('build', 'run'))
+    depends_on('py-pypdf2@1.26:',      type=('build', 'run'))
