6fbb01fe3181dd2b81f85abe52f8ed6feaa8a000
Elizabeth Sherrock <lizzyd710@gmail.com>
2022-07-13 03:19:23 -0400

    py-ford: add v6.1.13 (#31483)

--- var/spack/repos/builtin/packages/py-ford/package.py
+++ var/spack/repos/builtin/packages/py-ford/package.py
@@ -14,6 +14,7 @@ class PyFord(PythonPackage):
 
     maintainers = ['wscullin']
 
+    version('6.1.13', sha256='95b743ea25c5a9c6a9e13db3633e04f91e11d1debb69f48ca3ef7fefc51f0559')
     version('6.1.12', sha256='101191e1aa33cfe780ea5b2d66d02c7281b9b314e82bb138d76809a49c08506a')
     version('6.1.11', sha256='feb9a88040e717e84c632e4b023904ab36a463fc9a8ff80c8c7f86454e5d8043')
 
