76536636091c826096a1afe9bf65e386634f9506
Sid Pendelberry <sid@rit.edu>
2022-07-13 10:07:54 -0400

    first build of keras-tuner with dataset kt-legacy

---
+++ var/spack/repos/builtin/packages/py-kt-legacy/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyKtLegacy(PythonPackage):
+    """This repository is to support the deprecated import name of Keras Tuner.
+    With this repo, you can import Keras Tuner as kerastuner. In the main Keras
+    Tuner repository the import name has been changed to keras_tuner."""
+
+    homepage = "https://www.example.com"
+    pypi     = "kt-legacy/kt-legacy-1.0.4.tar.gz"
+
+    version('1.0.4', sha256='a94112e42a50e7cc3aad31f3287aa384c23555ea1432c55b5823852e09e706cf')
