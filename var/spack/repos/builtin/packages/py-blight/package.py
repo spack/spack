ac972130bdcc7ed92a7e7aeccb3c82b9d8f1540c
lizzyd710 <lizzyd710@gmail.com>
2022-07-13 13:42:16 -0400

    Finished adding py-blight

--- var/spack/repos/builtin/packages/py-blight/package.py
+++ var/spack/repos/builtin/packages/py-blight/package.py
@@ -4,21 +4,6 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 # ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-blight
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-blight
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
@@ -35,26 +20,30 @@ class PyBlight(PythonPackage):
 
     version('0.0.47', sha256='eb4a881adb98e03a0a855b95bfcddb0f4b3ca568b00cb45b571f047ae75c5667')
 
-    # variant('dev')
+    variant('dev', default=False, description='Enable dev stuff idk')
 
     depends_on('python@3.7:', type=('build', 'run'))
 
     # In process of changing build backend after 0.0.47 release.
     depends_on('py-setuptools', type='build')
+  
+    depends_on('py-click@7.1:8', type=('build', 'run'), when='@0.0.31:0.0.33,0.0.38:')
+    depends_on('py-click@8.0:8', type=('build', 'run'), when='@0.0.34:0.0.37')
+    depends_on('py-click@7.1:7', type=('build', 'run'), when='@:0.0.30')
+    depends_on('py-typing-extensions', type=('build', 'run'), when='@0.0.9:')
+    depends_on('py-pydantic@1.7:1', type=('build', 'run'), when='@0.0.26:')
 
-    # FIXME: Add additional dependencies if required.
-    depends_on('py-click', type=('build', 'run'))
-    depends_on('py-typing-extensions', type=('build', 'run'))
-    depends_on('py-pydantic', type=('build', 'run'))
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
+    depends_on('py-flake8', type=('build', 'run'), when='+dev') 
+    depends_on('py-black', type=('build', 'run'), when='+dev')
+    # dev variant of blight needed the pyproject variant of isort until isort 5.0.0
+    # moved that functionality to the program by default, removing the pyproject variant.
+    # blight adjusted for this in 0.0.15. spack has isort versions before 5.0.0, but no
+    # pyproject variant, so I'm arbitrarily placing a version requirement of >= 5.0.0 to
+    # avoid potential issues, even though blight doesn't specify isort versions.
+    depends_on('py-isort@5.0.0:', type=('build', 'run'), when='+dev')
+    depends_on('py-pytest', type=('build', 'run'), when='+dev')
+    depends_on('py-pytest-cov', type=('build', 'run'), when='+dev')
+    depends_on('py-coverage+toml', type=('build', 'run'), when='+dev')
+    depends_on('py-twine', type=('build', 'run'), when='+dev')
+    depends_on('py-pdoc3', type=('build', 'run'), when='+dev'
+    depends_on('py-mypy', type=('build', 'run'), when='@0.0.5:+dev')
