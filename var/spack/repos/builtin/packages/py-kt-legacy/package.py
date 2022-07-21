ec135655c60eb37e477b8f0db85a8be78881ce39
Jen Herting <jen@herting.cc>
2022-07-21 09:58:15 -0400

    [py-kt-legacy] fixed homepage

--- var/spack/repos/builtin/packages/py-kt-legacy/package.py
+++ var/spack/repos/builtin/packages/py-kt-legacy/package.py
@@ -11,7 +11,7 @@ class PyKtLegacy(PythonPackage):
     With this repo, you can import Keras Tuner as kerastuner. In the main Keras
     Tuner repository the import name has been changed to keras_tuner."""
 
-    homepage = "https://www.example.com"
+    homepage = "https://github.com/haifeng-jin/kt-legacy"
     pypi     = "kt-legacy/kt-legacy-1.0.4.tar.gz"
 
     version('1.0.4', sha256='a94112e42a50e7cc3aad31f3287aa384c23555ea1432c55b5823852e09e706cf')
