8eaa210c530b2c601d67049779c54ad60a81eda1
Alan Sill <alan.sill@ttu.edu>
2022-07-22 15:48:36 -0500

    Fix ***EXTREMELY*** small style complaint
    It's complaining about trailing whitespace ***in a comment***!
    Way way too picky

--- var/spack/repos/builtin/packages/vasp/package.py
+++ var/spack/repos/builtin/packages/vasp/package.py
@@ -116,7 +116,7 @@ class Vasp(MakefilePackage):
                                          'makefile.include.linux_' +
                                          spec.compiler.name)
 
-        # Recent versions of vasp have renamed the makefile.include files 
+        # Recent versions of vasp have renamed the makefile.include files
         # to leave out the linux_ string
         if os.path.exists(make_include):
             os.rename(make_include, 'makefile.include')
