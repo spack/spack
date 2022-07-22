2000b6af4398c68455f591c9ad999311f2840dab
Peter Josef Scheibel <scheibel1@llnl.gov>
2022-07-21 22:43:34 -0700

    add comments to tests; rename some tests; add a test with multiple one_of groups for one package

--- lib/spack/spack/test/config_requirements.py
+++ lib/spack/spack/test/config_requirements.py
@@ -41,6 +41,7 @@ _pkgx = ('x', """\
 class X(Package):
     version('1.1')
     version('1.0')
+    version('0.9')
 
     variant('shared', default=True,
             description='Build shared libraries')
@@ -81,7 +82,10 @@ repo:
         yield mock_repo_path
 
 
-def test_requirements_arent_optional(concretize_scope, test_repo):
+def test_requirement_isnt_optional(concretize_scope, test_repo):
+    """If a user spec requests something that directly conflicts
+       with a requirement, make sure we get an error.
+    """
     if spack.config.get('config:concretizer') == 'original':
         pytest.skip("Original concretizer does not support configuration"
                     " requirements")
@@ -96,7 +100,10 @@ packages:
         Spec('x@1.1').concretize()
 
 
-def test_basic_requirements_are_respected(concretize_scope, test_repo):
+def test_requirement_is_successfully_applied(concretize_scope, test_repo):
+    """If a simple requirement can be satisfied, make sure the
+       concretization succeeds and the requirement spec is applied.
+    """
     if spack.config.get('config:concretizer') == 'original':
         pytest.skip("Original concretizer does not support configuration"
                     " requirements")
@@ -116,8 +123,11 @@ packages:
     assert s2.satisfies('@1.0')
 
 
-def test_multiple_package_requirements_are_respected(
+def test_multiple_packages_requirements_are_respected(
         concretize_scope, test_repo):
+    """Apply requirements to two packages; make sure the concretization
+       succeeds and both requirements are respected.
+    """
     if spack.config.get('config:concretizer') == 'original':
         pytest.skip("Original concretizer does not support configuration"
                     " requirements")
@@ -135,7 +145,10 @@ packages:
     assert spec['y'].satisfies('@2.4')
 
 
-def test_one_package_one_of(concretize_scope, test_repo):
+def test_one_of(concretize_scope, test_repo):
+    """'one_of' allows forcing the concretizer to satisfy one of
+       the specs in the group (but not all have to be satisfied).
+    """
     if spack.config.get('config:concretizer') == 'original':
         pytest.skip("Original concretizer does not support configuration"
                     " requirements")
@@ -151,3 +164,24 @@ packages:
     # The concretizer only has to satisfy one of @2.4/~shared, and @2.4
     # comes first so it is prioritized
     assert spec['y'].satisfies('@2.4+shared')
+
+
+def test_one_package_multiple_one_of_groups(concretize_scope, test_repo):
+    if spack.config.get('config:concretizer') == 'original':
+        pytest.skip("Original concretizer does not support configuration"
+                    " requirements")
+
+    conf_str = """\
+packages:
+  y:
+    require:
+    - one_of: ["@2.4%gcc", "@2.5%clang"]
+    - one_of: ["@2.5~shared", "@2.4+shared"]
+"""
+    update_packages_config(conf_str)
+
+    s1 = Spec('y@2.5').concretized()
+    assert s1.satisfies('%clang~shared')
+
+    s2 = Spec('y@2.4').concretized()
+    assert s2.satisfies('%gcc+shared')
