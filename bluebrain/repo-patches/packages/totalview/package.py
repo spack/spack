858a1d5337bc94cd0fb743538f2ce3680b2d827a
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2022-07-26 13:07:56 +0200

    Add totalview v2022.2.13 to the deployment (#1628)

--- bluebrain/repo-patches/packages/totalview/package.py
+++ bluebrain/repo-patches/packages/totalview/package.py
@@ -10,11 +10,21 @@ class Totalview(BuiltinTotalview):
     # new totalview docs suggest this license path
     license_files = ['toolworks/FNP_license/license.dat']
 
+    version('2022.2.13',
+            sha256='4bf625c760454e532fe66666f2f5479d38f36f569f104bbe3341c0f48cbc8766')
+
     version('2021.1.16',
             sha256='4c51c7b6ab6b6afa7635ba2e9fc3b0ef833806f775a0ad0da26b13d6320625dd')
 
     version('2021.4.10',
             sha256='c476288ebe1964e0803c7316975c71a957e52f45187b135bc1dc3b65491bb61d')
+
+    resource(
+        name='x86_64',
+        url='file://{0}/totalview_2022.2.13_linux_x86-64.tar'.format(os.getcwd()),
+        destination='.',
+        sha256='aebd11b837ce18b8200859ea762caa56e2cea346daa114f2841aa0f05a422309',
+        when='@2022.2.13 target=x86_64:')
 
     resource(
         name='x86_64',
