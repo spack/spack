b1493ae5c8a82a1c1993d33c14cdb1a6101fe8a4
Harmen Stoppels <harmenstoppels@gmail.com>
2022-07-26 22:43:53 +0200

    gnupg: bump and deprecate over cve (#31730)

--- var/spack/repos/builtin/packages/gnupg/package.py
+++ var/spack/repos/builtin/packages/gnupg/package.py
@@ -14,24 +14,26 @@ class Gnupg(AutotoolsPackage):
 
     maintainers = ['alalazo']
 
-    version('2.3.4', sha256='f3468ecafb1d7f9ad7b51fd1db7aebf17ceb89d2efa8a05cf2f39b4d405402ae')
-    version('2.3.3', sha256='5789b86da6a1a6752efb38598f16a77af51170a8494039c3842b085032e8e937')
-    version('2.3.2', sha256='e1d953e0e296072fca284215103ef168885eaac596c4660c5039a36a83e3041b')
-    version('2.3.1', sha256='c498db346a9b9a4b399e514c8f56dfc0a888ce8f327f10376ff984452cd154ec')
-    version('2.2.27', sha256='34e60009014ea16402069136e0a5f63d9b65f90096244975db5cea74b3d02399')
-    version('2.2.25', sha256='c55307b247af4b6f44d2916a25ffd1fb64ce2e509c3c3d028dbe7fbf309dc30a')
-    version('2.2.24', sha256='9090b400faae34f08469d78000cfec1cee5b9c553ce11347cc96ef16eab98c46')
-    version('2.2.23', sha256='10b55e49d78b3e49f1edb58d7541ecbdad92ddaeeb885b6f486ed23d1cd1da5c')
-    version('2.2.22', sha256='7c1370565e1910b9d8c4e0fb57b9de34aa062ec7bb91abad5803d791f38d855b')
-    version('2.2.21', sha256='61e83278fb5fa7336658a8b73ab26f379d41275bb1c7c6e694dd9f9a6e8e76ec')
-    version('2.2.20', sha256='04a7c9d48b74c399168ee8270e548588ddbe52218c337703d7f06373d326ca30')
-    version('2.2.19', sha256='242554c0e06f3a83c420b052f750b65ead711cc3fddddb5e7274fcdbb4e9dec0')
-    version('2.2.17', sha256='afa262868e39b651a2db4c071fba90415154243e83a830ca00516f9a807fd514')
-    version('2.2.15', sha256='cb8ce298d7b36558ffc48aec961b14c830ff1783eef7a623411188b5e0f5d454')
-    version('2.2.3',  sha256='cbd37105d139f7aa74f92b6f65d136658682094b0e308666b820ae4b984084b4')
-    version('2.1.21', sha256='7aead8a8ba75b69866f583b6c747d91414d523bfdfbe9a8e0fe026b16ba427dd')
+    version('2.3.7', sha256='ee163a5fb9ec99ffc1b18e65faef8d086800c5713d15a672ab57d3799da83669')
+    # Deprecated over CVE-2022-34903
+    version('2.3.4', sha256='f3468ecafb1d7f9ad7b51fd1db7aebf17ceb89d2efa8a05cf2f39b4d405402ae', deprecated=True)
+    version('2.3.3', sha256='5789b86da6a1a6752efb38598f16a77af51170a8494039c3842b085032e8e937', deprecated=True)
+    version('2.3.2', sha256='e1d953e0e296072fca284215103ef168885eaac596c4660c5039a36a83e3041b', deprecated=True)
+    version('2.3.1', sha256='c498db346a9b9a4b399e514c8f56dfc0a888ce8f327f10376ff984452cd154ec', deprecated=True)
+    version('2.2.27', sha256='34e60009014ea16402069136e0a5f63d9b65f90096244975db5cea74b3d02399', deprecated=True)
+    version('2.2.25', sha256='c55307b247af4b6f44d2916a25ffd1fb64ce2e509c3c3d028dbe7fbf309dc30a', deprecated=True)
+    version('2.2.24', sha256='9090b400faae34f08469d78000cfec1cee5b9c553ce11347cc96ef16eab98c46', deprecated=True)
+    version('2.2.23', sha256='10b55e49d78b3e49f1edb58d7541ecbdad92ddaeeb885b6f486ed23d1cd1da5c', deprecated=True)
+    version('2.2.22', sha256='7c1370565e1910b9d8c4e0fb57b9de34aa062ec7bb91abad5803d791f38d855b', deprecated=True)
+    version('2.2.21', sha256='61e83278fb5fa7336658a8b73ab26f379d41275bb1c7c6e694dd9f9a6e8e76ec', deprecated=True)
+    version('2.2.20', sha256='04a7c9d48b74c399168ee8270e548588ddbe52218c337703d7f06373d326ca30', deprecated=True)
+    version('2.2.19', sha256='242554c0e06f3a83c420b052f750b65ead711cc3fddddb5e7274fcdbb4e9dec0', deprecated=True)
+    version('2.2.17', sha256='afa262868e39b651a2db4c071fba90415154243e83a830ca00516f9a807fd514', deprecated=True)
+    version('2.2.15', sha256='cb8ce298d7b36558ffc48aec961b14c830ff1783eef7a623411188b5e0f5d454', deprecated=True)
+    version('2.2.3',  sha256='cbd37105d139f7aa74f92b6f65d136658682094b0e308666b820ae4b984084b4', deprecated=True)
+    version('2.1.21', sha256='7aead8a8ba75b69866f583b6c747d91414d523bfdfbe9a8e0fe026b16ba427dd', deprecated=True)
 
-    version('1.4.23', sha256='c9462f17e651b6507848c08c430c791287cd75491f8b5a8b50c6ed46b12678ba')
+    version('1.4.23', sha256='c9462f17e651b6507848c08c430c791287cd75491f8b5a8b50c6ed46b12678ba', deprecated=True)
 
     depends_on('npth@1.2:', when='@2:')
 
