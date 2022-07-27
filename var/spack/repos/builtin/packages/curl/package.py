ade7bcd514c5b90910368ef6ac0df6ee4a9951b0
Harmen Stoppels <harmenstoppels@gmail.com>
2022-07-27 04:37:49 +0200

    curl: 7.84.0 (#31725)

--- var/spack/repos/builtin/packages/curl/package.py
+++ var/spack/repos/builtin/packages/curl/package.py
@@ -19,6 +19,7 @@ class Curl(AutotoolsPackage):
 
     executables = ['^curl$']
 
+    version('7.84.0', sha256='702fb26e73190a3bd77071aa146f507b9817cc4dfce218d2ab87f00cd3bc059d')
     version('7.83.0', sha256='247c7ec7521c4258e65634e529270d214fe32969971cccb72845e7aa46831f96')
     version('7.82.0', sha256='46d9a0400a33408fd992770b04a44a7434b3036f2e8089ac28b57573d59d371f')
     version('7.81.0', sha256='1e7a38d7018ec060f1f16df839854f0889e94e122c4cfa5d3a37c2dc56f1e258')
