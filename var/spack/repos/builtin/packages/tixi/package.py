c1a1ef1baaecef3c42a2df4b605fcecd25d53ea1
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-07-27 18:51:21 +0200

    tixi: new versions (#31761)

--- var/spack/repos/builtin/packages/tixi/package.py
+++ var/spack/repos/builtin/packages/tixi/package.py
@@ -14,6 +14,9 @@ class Tixi(CMakePackage):
     url      = "https://github.com/DLR-SC/tixi/archive/v3.0.3.tar.gz"
     git      = "https://github.com/DLR-SC/tixi.git"
 
+    version('3.3.0', sha256='988d79ccd53c815d382cff0c244c0bb8e393986377dfb45385792766adf6f6a9')
+    version('3.2.0', sha256='8df65c4d252d56e98c5ef2657c7aff6086a07b5686716e786891609adaca9d2d')
+    version('3.1.0', sha256='4547133e452f3455b5a39045a8528955dce55faf059afe652a350ecf37d709ba')
     version('3.0.3', sha256='3584e0cec6ab811d74fb311a9af0663736b1d7f11b81015fcb378efaf5ad3589')
     version('2.2.4', sha256='9080d2a617b7c411b9b4086de23998ce86e261b88075f38c73d3ce25da94b21c')
 
