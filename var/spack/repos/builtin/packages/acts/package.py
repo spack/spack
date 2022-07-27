3e6891a864e86264c48139f35189ef1c11202811
Wouter Deconinck <wdconinc@gmail.com>
2022-07-27 10:39:32 -0500

    acts: new versions 19.4.0 and 19.5.0

--- var/spack/repos/builtin/packages/acts/package.py
+++ var/spack/repos/builtin/packages/acts/package.py
@@ -39,6 +39,8 @@ class Acts(CMakePackage, CudaPackage):
     # Supported Acts versions
     version('main', branch='main')
     version('master', branch='main', deprecated=True)  # For compatibility
+    version('19.5.0', commit='bf9f0270eadd8e78d283557b7c9070b80dece4a7', submodules=True)
+    version('19.4.0', commit='498af243755219486c26d32fb125b7ebf2557166', submodules=True)
     version('19.3.0', commit='747053f60254c5ad3aa1fe7b18ae89c19029f4a6', submodules=True)
     version('19.2.0', commit='adf079e0f7e278837093bf53988da73730804e22', submodules=True)
     version('19.1.0', commit='82f42a2cc80d4259db251275c09b84ee97a7bd22', submodules=True)
