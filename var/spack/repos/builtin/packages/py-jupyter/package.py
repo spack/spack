d55c6c7f6c85dd56c10263a91245c4239322d685
Davide Del Vento <davide.del.vento@gmail>
2022-07-29 16:37:37 -0600

    making qtconsole optional

--- var/spack/repos/builtin/packages/py-jupyter/package.py
+++ var/spack/repos/builtin/packages/py-jupyter/package.py
@@ -14,11 +14,13 @@ class PyJupyter(PythonPackage):
 
     version('1.0.0', sha256='d9dc4b3318f310e34c82951ea5d6683f67bed7def4b259fafbfe4f1beb1d8e5f')
 
+    variant('qtconsole', default=False, description='Enable Qt console')
+
     # pip silently replaces distutils with setuptools
     depends_on('py-setuptools', type='build')
     depends_on('py-notebook', type=('build', 'run'))
-    depends_on('py-qtconsole', type=('build', 'run'))
-    depends_on('py-jupyter-console', type=('build', 'run'))
+    depends_on('py-qtconsole', type=('build', 'run'), when='+qtconsole')
+    depends_on('py-jupyter-console', type=('build', 'run'), when='+qtconsole')
     depends_on('py-nbconvert', type=('build', 'run'))
     depends_on('py-ipykernel', type=('build', 'run'))
     depends_on('py-ipywidgets', type=('build', 'run'))
