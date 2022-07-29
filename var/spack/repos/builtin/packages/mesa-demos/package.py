57af891cb12cdba7c07fcac6c539adc292284755
Chuck Atkins <chuck.atkins@kitware.com>
2022-07-29 13:04:42 -0500

    OpenGL restructure (#31748)
    * OpenGL: Restructures the OpenGL packages
    
    This provides concrete glx and osmesa packages which delegate to
    virtual libglx and libosmesa packages provided by mesa. This was
    necessary because GLX and OSMesa both provide gl implementations but
    with mesa providing the girtual gl package there was no way to properly
    distinguish which of the two opengl implementations was beiing requested
    when querying the spec['gl'] dependency.  This additional level of
    indirection allows for that.
    
    * OpenGL: Adjust downstream dependents of OpenGL for the restructure
    
    This implements the necessary fixes in the packages that depend on
    OpenGL to work with the restructuring.  This also attempts to create a
    consistent variant for specifying glx or osmesa.

--- var/spack/repos/builtin/packages/mesa-demos/package.py
+++ var/spack/repos/builtin/packages/mesa-demos/package.py
@@ -11,18 +11,21 @@ class MesaDemos(AutotoolsPackage):
     """This package provides some demo applications for testing Mesa."""
 
     homepage = "https://www.mesa3d.org"
-    url      = "https://github.com/freedesktop/mesa-demos/archive/mesa-demos-8.3.0.tar.gz"
+    url      = "https://gitlab.freedesktop.org/mesa/demos/-/archive/mesa-demos-8.4.0/demos-mesa-demos-8.4.0.tar.gz"
 
+    version('8.4.0', sha256='e9d235e6dad69d6b00877bf07e6d1859e368c0873e5401ec68a6ddb43375e900')
     version('8.3.0', sha256='9bc1b37f4fc7bfc3f818f2d3851ffde28e8167ef11dca87f4781e9ef6206901f')
     version('8.2.0', sha256='5a9f71b815d968d0c3b77edfcc3782d0211f8520b00da9e554ccfed80c8889f6')
     version('8.1.0', sha256='cc5826105355830208c90047fc38c5b09fa3ab0045366e7e859104935b00b76d')
 
-    variant('osmesa', default=False, description="Enable the OSMesa frontend.")
-
-    is_linux = sys.platform.startswith('linux')
-    variant('glx', default=is_linux, description="Enable the GLX frontend.")
-
-    conflicts('+osmesa', when='+glx')
+    variant('gl',
+            default='glx' if sys.platform.startswith('linux') else 'osmesa',
+            values=('glx', 'osmesa', 'other'), multi=False,
+            description='The OpenGL provider to use')
+    conflicts('osmesa', when='gl=glx')
+    conflicts('osmesa', when='gl=other')
+    conflicts('glx', when='gl=osmesa')
+    conflicts('glx', when='gl=other')
 
     depends_on('autoconf',  type='build')
     depends_on('automake',  type='build')
@@ -31,17 +34,19 @@ class MesaDemos(AutotoolsPackage):
     depends_on('pkgconfig', type='build')
 
     depends_on('gl')
-    depends_on('glx', when='+glx')
-    depends_on('libx11',  when='+glx')
-    depends_on('libxext', when='+glx')
-    depends_on('osmesa', when='+osmesa')
+    depends_on('osmesa', when='gl=osmesa')
+    depends_on('glx', when='gl=glx')
+    depends_on('libx11',  when='gl=glx')
+    depends_on('libxext', when='gl=glx')
 
-    depends_on('glu', when='~osmesa')
-    depends_on('mesa-glu+osmesa', when='+osmesa')
-    depends_on('freeglut')
+    depends_on('glu')
     depends_on('glew@1.5.4:')
 
+    # OSMesa demos don't actually use glut
+    patch('osmesa-glut.patch')
+
     def configure_args(self):
+        spec = self.spec
         args = [
             '--disable-egl',
             '--disable-gles1',
@@ -52,8 +57,15 @@ class MesaDemos(AutotoolsPackage):
             '--disable-gbm',
             '--disable-freetype2',
             '--disable-rbug',
-            self.enable_or_disable('osmesa'),
-            self.enable_or_disable('x11', variant='glx'),
+            '--without-glut',
         ]
+        if 'gl=glx' in spec:
+            args.append('--enable-x11')
+        else:
+            args.append('--disable-x11')
+        if 'gl=osmesa' in spec:
+            args.append('--enable-osmesa')
+        else:
+            args.append('--disable-osmesa')
 
         return args
