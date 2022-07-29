ace4fae518e460ce535ceca5e804422f4e35b1a5
Adam J. Stewart <ajstewart426@gmail.com>
2022-07-29 14:59:48 -0700

    Revert "MACOSX_DEPLOYMENT_TARGET: MAJOR.MINOR"
    This reverts commit ee4f310f1da0c5693094fa5ded5ccb2707847ac7.

--- lib/spack/spack/operating_systems/mac_os.py
+++ lib/spack/spack/operating_systems/mac_os.py
@@ -150,8 +150,9 @@ class MacOs(OperatingSystem):
         # only used the minor component)
         part = 1 if version >= Version('11') else 2
 
-        name = mac_releases.get(str(version.up_to(part)), "macos")
-        super(MacOs, self).__init__(name, version.up_to(2))
+        mac_ver = str(version.up_to(part))
+        name = mac_releases.get(mac_ver, "macos")
+        super(MacOs, self).__init__(name, mac_ver)
 
     def __str__(self):
         return self.name
