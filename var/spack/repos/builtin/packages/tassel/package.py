fe68f94339963db5998b259a07c54974c893d7dc
James Prewett <download@carc.unm.edu>
2022-08-03 14:57:13 -0600

    changed single quotes to double quotes to appease the gods ;)

--- var/spack/repos/builtin/packages/tassel/package.py
+++ var/spack/repos/builtin/packages/tassel/package.py
@@ -13,14 +13,19 @@ class Tassel(Package):
     homepage = "https://www.maizegenetics.net/tassel"
     git      = "https://bitbucket.org/tasseladmin/tassel-5-standalone.git"
 
-    version('5.2.39', commit='ae96ae75c3c9a9e8026140b6c775fa4685bdf531')
-    version('3.0.174', commit='612a92c0f677d6ec040c17e146671974a3cdc2da', git='git://git.code.sf.net/p/tassel/tassel3-standalone.git')
+    version("5.2.39", commit="ae96ae75c3c9a9e8026140b6c775fa4685bdf531")
+    version(
+        "3.0.174", 
+        commit="612a92c0f677d6ec040c17e146671974a3cdc2da", 
+        git="git://git.code.sf.net/p/tassel/tassel3-standalone.git"
+     )
 
-    depends_on('java', type=('build', 'run'))
-    depends_on('perl', type=('build', 'run'))
+
+    depends_on("java", type=("build", "run"))
+    depends_on("perl", type=("build", "run"))
 
     def install(self, spec, prefix):
-        install_tree('.', prefix.bin)
+        install_tree(".", prefix.bin)
 
     def setup_run_environment(self, env):
-        env.prepend_path('CLASSPATH', self.prefix.bin.lib)
+        env.prepend_path("CLASSPATH", self.prefix.bin.lib)
