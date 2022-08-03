5fa7fb25ec407f011a6525d6132f9c1e1f59152b
iarspider <iarspider@gmail.com>
2022-08-03 11:34:18 +0200

    Add checksum for py-arror 1.2.1 and 1.2.2

--- var/spack/repos/builtin/packages/py-arrow/package.py
+++ var/spack/repos/builtin/packages/py-arrow/package.py
@@ -17,6 +17,8 @@ class PyArrow(PythonPackage):
     homepage = "https://arrow.readthedocs.io/en/latest/"
     pypi = "arrow/arrow-0.16.0.tar.gz"
 
+    version("1.2.2", sha256="05caf1fd3d9a11a1135b2b6f09887421153b94558e5ef4d090b567b47173ac2b")
+    version("1.2.1", sha256="c2dde3c382d9f7e6922ce636bf0b318a7a853df40ecb383b29192e6c5cc82840")
     version("0.16.0", sha256="92aac856ea5175c804f7ccb96aca4d714d936f1c867ba59d747a8096ec30e90a")
     version("0.14.7", sha256="67f8be7c0cf420424bc62d8d7dc40b44e4bb2f7b515f9cc2954fb36e35797656")
     version("0.14.1", sha256="2d30837085011ef0b90ff75aa0a28f5c7d063e96b7e76b6cbc7e690310256685")
@@ -27,3 +29,4 @@ class PyArrow(PythonPackage):
         "py-backports-functools-lru-cache@1.2.1:", type=("build", "run"), when="^python@2.7:2.8"
     )
     depends_on("py-python-dateutil", type=("build", "run"))
+    depends_on("py-typing-extensions", type=("build", "run"), when="^python@:3.8 @1.2.1:")
