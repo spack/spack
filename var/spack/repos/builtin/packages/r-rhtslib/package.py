a8c226bc4f8e75e92f3534f88daf1807debec892
glennpj <glennpj@users.noreply.github.com>
2022-08-03 02:21:12 +0000

    [@spackbot] updating style on behalf of glennpj

--- var/spack/repos/builtin/packages/r-rhtslib/package.py
+++ var/spack/repos/builtin/packages/r-rhtslib/package.py
@@ -27,7 +27,7 @@ class RRhtslib(RPackage):
     #             branches.
     #             Aborting
     # version("1.28.0", commit='214fde2218bdbca89f1e12a30d2e081e76915aef')
-    version("1.28.0", branch='RELEASE_3_15')
+    version("1.28.0", branch="RELEASE_3_15")
     version("1.26.0", commit="f5b20e97b283942877529f750b28398782552655")
     version("1.22.0", commit="899b79faa54d42c7c9b9a2bc49972109637d367f")
     version("1.18.1", commit="751a2ebaed43b7991204b27bd6c7870645001d82")
