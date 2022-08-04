9a5c55fe189be03c26c5201cf91b73efda405ff9
iarspider <iarspider@gmail.com>
2022-08-04 18:56:43 +0200

    Add checksums for py-onnx 1.10.1, 1.11.0, 1.12.0 (#31891)
    * Add checksums for py-onnx 1.10.1, 1.11.0, 1.12.0
    
    * Update package.py

--- var/spack/repos/builtin/packages/py-onnx/package.py
+++ var/spack/repos/builtin/packages/py-onnx/package.py
@@ -19,6 +19,9 @@ class PyOnnx(PythonPackage):
     homepage = "https://github.com/onnx/onnx"
     pypi = "Onnx/onnx-1.6.0.tar.gz"
 
+    version("1.12.0", sha256="13b3e77d27523b9dbf4f30dfc9c959455859d5e34e921c44f712d69b8369eff9")
+    version("1.11.0", sha256="eca224c7c2c8ee4072a0743e4898a84a9bdf8297b5e5910a2632e4c4182ffb2a")
+    version("1.10.1", sha256="9d941ba76cab55db8913ecad9dc50cefeb368460f6338a91783a5d7643f3a044")
     version("1.8.1", sha256="9d65c52009a90499f8c25fdfe5acda3ac88efe0788eb1d5f2575a989277145fb")
     version("1.6.0", sha256="3b88c3fe521151651a0403c4d131cb2e0311bd28b753ef692020a432a81ce345")
     version("1.5.0", sha256="1a584a4ef62a6db178c257fffb06a9d8e61b41c0a80bfd8bcd8a253d72c4b0b4")
@@ -33,12 +36,16 @@ class PyOnnx(PythonPackage):
     depends_on("py-protobuf@:3.17", when="@:1.8", type=("build", "run"))
     # https://github.com/protocolbuffers/protobuf/issues/10051
     # https://github.com/onnx/onnx/issues/4222
-    depends_on("protobuf@:3")
-    depends_on("py-protobuf@:3", type=("build", "run"))
+    depends_on("protobuf@:3", when="@1.10.1")
+    depends_on("py-protobuf@:3", type=("build", "run"), when="@1.10.1")
+    depends_on("protobuf@3.12.2:", when="@1.11.0")
+    depends_on("py-protobuf@3.12.2:", type=("build", "run"), when="@1.11.0")
+    depends_on("protobuf@3.12.2:3.20.1", when="@1.12.0:")
+    depends_on("py-protobuf@3.12.2:3.20.1", type=("build", "run"), when="@1.12.0:")
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-numpy@1.16.6:", type=("build", "run"), when="@1.8.1:")
-    depends_on("py-six", type=("build", "run"))
-    depends_on("py-typing@3.6.4:", when="^python@:3.4", type=("build", "run"))
+    depends_on("py-six", type=("build", "run"), when="@:1.8.1")
+    depends_on("py-typing@3.6.4:", when="@:1.8.1 ^python@:3.4", type=("build", "run"))
     depends_on("py-typing-extensions@3.6.2.1:", type=("build", "run"))
     depends_on("cmake@3.1:", type="build")
     depends_on("py-pytest-runner", type="build")
