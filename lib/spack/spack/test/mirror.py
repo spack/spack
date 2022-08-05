d83466e57233543c135b60154dc8b21decebe5f6
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-08-05 19:19:51 +0200

    spack mirror: skip non-concretizable specs (#31897)
    fixes #31736
    
    Catch errors when concretizing specs and report them as
    debug messages. The corresponding spec is skipped.
    
    Co-authored-by: Greg Becker <becker33@llnl.gov>

--- lib/spack/spack/test/mirror.py
+++ lib/spack/spack/test/mirror.py
@@ -311,3 +311,19 @@ def test_get_all_versions(specs, expected_specs):
     output_list = [str(x) for x in output_list]
     # Compare sets since order is not important
     assert set(output_list) == set(expected_specs)
+
+
+@pytest.mark.regression("31736")
+def test_non_concretizable_spec_does_not_raise():
+    s = Spec("doesnotexist")
+
+    class Stats(object):
+        called = False
+
+        def error(self):
+            self.called = True
+
+    mirror_stats = Stats()
+    result = spack.mirror._add_single_spec(s, mirror=None, mirror_stats=mirror_stats)
+    assert result is False
+    assert mirror_stats.called is True
