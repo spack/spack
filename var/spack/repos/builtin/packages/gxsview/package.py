1e283bbd61ba7be797fba52d19fb57cc4b64d669
Olivier Cessenat <cessenat@gmail.com>
2022-08-08 18:33:37 +0200

    gxsview: comment for gcc11

--- var/spack/repos/builtin/packages/gxsview/package.py
+++ var/spack/repos/builtin/packages/gxsview/package.py
@@ -28,7 +28,8 @@ class Gxsview(QMakePackage):
     conflicts("%gcc@:7.2.0", msg="Requires C++17 compiler support")  # need C++17 standard
 
     patch("vtk9.patch", when="^vtk@9:")
-    # gcc11 compilation rule for std::numeric_limits, avoid ‘numeric_limits’ is not a member of ‘std’
+    # gcc11 compilation rule for std::numeric_limits,
+    # avoid ‘numeric_limits’ is not a member of ‘std’
     patch("gcc11.patch", when="@2021.07.01 %gcc@11:")
 
     build_directory = "gui"
