e62b1e98212de425ebf76bf11b546723890b1fd6
sairaj <sairaj@sairajs-MacBook-Pro.local>
2022-08-10 01:02:59 +0530

    changed p.name to p, added line inside if statement

--- lib/spack/spack/cmd/list.py
+++ lib/spack/spack/cmd/list.py
@@ -119,8 +119,9 @@ def filter_by_name(pkgs, args):
 
     # If tags have been specified on the command line, filter by tags
     if args.tags:
+        pkgs = [p for p in pkgs if p in packages_with_tags] 
         packages_with_tags = spack.repo.path.packages_with_tags(*args.tags)
-    pkgs = [p for p in pkgs if p.name in packages_with_tags]      
+         
         
 
     return sorted(pkgs, key=lambda s: s.lower())
