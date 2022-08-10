55481955e5780b74c6c4ad10526b90a21df21a07
Mikael Simberg <mikael.simberg@iki.fi>
2022-08-10 15:41:02 +0200

    Only restrict CMake version in umpire when examples and rocm are enabled

--- var/spack/repos/builtin/packages/umpire/package.py
+++ var/spack/repos/builtin/packages/umpire/package.py
@@ -83,7 +83,7 @@ class Umpire(CachedCMakePackage, CudaPackage, ROCmPackage):
 
     depends_on("cmake@3.8:", type="build")
     depends_on("cmake@3.9:", when="+cuda", type="build")
-    depends_on("cmake@:3.20", when="+rocm", type="build")
+    depends_on("cmake@:3.20", when="+examples +rocm", type="build")
     depends_on("cmake@3.14:", when="@2022.03.0:")
 
     depends_on("blt@0.5.0:", type="build", when="@2022.03.0:")
