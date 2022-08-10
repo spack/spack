20d9508b3b013de5d572b5dc74ec7492a7c9d822
Olivier Cessenat <cessenat@gmail.com>
2022-08-10 18:06:03 +0200

    perl-fth: adding version 0.527

--- var/spack/repos/builtin/packages/perl-fth/package.py
+++ var/spack/repos/builtin/packages/perl-fth/package.py
@@ -27,6 +27,7 @@ class PerlFth(Package):
 
     maintainers = ["cessenat"]
 
+    version('0.527', sha256='0711af1639e0da5d2c51a98857fa0fbe187b1460bfbd1eeb6a0478458777cd81')
     version("0.526", sha256="ada1c7306111d59d64572fe8a9b038026fd0daebaff630924997ef2dc22d87a8")
     version("0.525", sha256="378116febeb20f4b0c1e298de90305e8494335949d853c7e390d1b6386c1326a")
     version("0.524", sha256="2f378e969d1dd267985342f7fb1b3a0b9fd73334627cbc7ab17d61717bcd3c29")
