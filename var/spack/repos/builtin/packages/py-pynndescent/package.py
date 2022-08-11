e210e4ef94278f0e38580163dd500099e45327a8
qwertos <qwertos@users.noreply.github.com>
2022-08-11 23:18:34 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-pynndescent/package.py
+++ var/spack/repos/builtin/packages/py-pynndescent/package.py
@@ -11,13 +11,13 @@ class PyPynndescent(PythonPackage):
     approximate nearest neighbors."""
 
     homepage = "https://github.com/lmcinnes/pynndescent"
-    pypi     = "pynndescent/pynndescent-0.5.7.tar.gz"
+    pypi = "pynndescent/pynndescent-0.5.7.tar.gz"
 
-    version('0.5.7', sha256='ecb395255fa36a748b5870b4ba0300ea0f7da8b1964864b8edd62577a84dfd7d')
+    version("0.5.7", sha256="ecb395255fa36a748b5870b4ba0300ea0f7da8b1964864b8edd62577a84dfd7d")
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-scikit-learn@0.18:', type=('build', 'run'))
-    depends_on('py-scipy@1.0:', type=('build', 'run'))
-    depends_on('py-numba@0.51.2:', type=('build', 'run'))
-    depends_on('py-llvmlite@0.30:', type=('build', 'run'))
-    depends_on('py-joblib@0.11:', type=('build', 'run'))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-scikit-learn@0.18:", type=("build", "run"))
+    depends_on("py-scipy@1.0:", type=("build", "run"))
+    depends_on("py-numba@0.51.2:", type=("build", "run"))
+    depends_on("py-llvmlite@0.30:", type=("build", "run"))
+    depends_on("py-joblib@0.11:", type=("build", "run"))
