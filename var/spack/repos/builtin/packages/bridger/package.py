50765aaa47c794b64f39a00c3363a279dd43a922
snehring <7978778+snehring@users.noreply.github.com>
2022-08-15 13:55:43 -0500

    bridger: adding ncurses dep (#32103)

--- var/spack/repos/builtin/packages/bridger/package.py
+++ var/spack/repos/builtin/packages/bridger/package.py
@@ -19,6 +19,7 @@ class Bridger(MakefilePackage, SourceforgePackage):
         "2014-12-01", sha256="8fbec8603ea8ad2162cbd0c658e4e0a4af6453bdb53310b4b7e0d112e40b5737"
     )
     depends_on("boost + exception + filesystem + system + serialization + graph")
+    depends_on("ncurses~termlib")
     depends_on("perl", type="run")
 
     def flag_handler(self, name, flags):
