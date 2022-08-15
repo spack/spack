a24790d086e7a3b6ee1fd7f8c336a73bac778011
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-08-15 16:57:35 -0600

    Ignore two long lines in var/spack/repos/builtin/packages/ecflow/package.py

--- var/spack/repos/builtin/packages/ecflow/package.py
+++ var/spack/repos/builtin/packages/ecflow/package.py
@@ -50,13 +50,11 @@ class Ecflow(CMakePackage):
 
     # Use newer boost with v5
     depends_on(
-        "boost@1.72:+chrono+date_time+exception+filesystem+program_options+python+regex+" +
-            "serialization+system+test+thread+timer",
+        "boost@1.72:+chrono+date_time+exception+filesystem+program_options+python+regex+serialization+system+test+thread+timer", # noqa: E131
         when="@5:"
     )
     depends_on(
-        "boost@1.72:+chrono+date_time+exception+filesystem+program_options+python+regex+" +
-            "serialization+system+test+thread+timer+pic",
+        "boost@1.72:+chrono+date_time+exception+filesystem+program_options+python+regex+serialization+system+test+thread+timer+pic", # noqa: E131
         when="@5: +static_boost"
     )
 
