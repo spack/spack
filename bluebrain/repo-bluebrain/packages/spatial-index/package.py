ff5c14cdb67d6486501d5ea1b6f89673d63e4c89
Luc Grosheintz <luc.grosheintz@gmail.com>
2022-08-16 12:47:40 +0200

    Release version `0.6.0` of SpatialIndex. (#1667)

--- bluebrain/repo-bluebrain/packages/spatial-index/package.py
+++ bluebrain/repo-bluebrain/packages/spatial-index/package.py
@@ -13,8 +13,7 @@ class SpatialIndex(PythonPackage):
     url      = "git@bbpgitlab.epfl.ch:hpc/SpatialIndex.git"
 
     version('develop', branch='main', submodules=True)
-    version('0.5.2-rc2', commit="1a5a78c41d449582a843bcced41aac11bb96a2d0", submodules=True)
-    version('0.5.2-rc1', commit="796da683101e1fd3916644064703c9524a0e1e99", submodules=True)
+    version('0.6.0', tag="0.6.0", submodules=True)
     version('0.5.1', tag='0.5.1', submodules=True)
     version('0.5.0', tag='0.5.0', submodules=True)
     version('0.4.9', tag='0.4.9', submodules=True)
@@ -34,6 +33,7 @@ class SpatialIndex(PythonPackage):
     depends_on("py-setuptools")
     depends_on("cmake@3.2:", type="build")
     depends_on("boost@:1.70.0", when="@:0.5.1")
+    depends_on("boost@1.79.0:", when="@0.5.2:")
     depends_on("py-docopt", type=("build", "run"))
     depends_on("py-libsonata", type=("build", "run"), when="@0.2.2:")
     depends_on("py-morphio", type=("build", "run"))
@@ -41,11 +41,10 @@ class SpatialIndex(PythonPackage):
     depends_on("py-mvdtool~mpi", type=("build", "run"))
     depends_on("py-numpy-quaternion", type=("build", "run"), when="@0.2.1:")
     depends_on("py-numpy", type=("build", "run"))
-    depends_on("py-tqdm", when="@0.5.3:")
+    depends_on("py-tqdm", type=("build", "run"), when="@0.5.3:")
 
-    depends_on("boost@1.79.0:", when="@0.5.2:")
     depends_on("mpi", when="@0.5.2:")
-    depends_on("py-mpi4py", when="@0.5.2:")
+    depends_on("py-mpi4py", type=("build", "run"), when="@0.5.2:")
 
     @run_after('install')
     def install_headers(self):
