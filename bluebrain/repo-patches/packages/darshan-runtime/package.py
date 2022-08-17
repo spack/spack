25b2c77e0de5401065dacaa3d3b0123f304d101f
Sergio <sergio.rivasgomez@epfl.ch>
2022-08-17 11:38:07 +0200

    darshan-runtime: Fix log path issue (#1668)

---
+++ bluebrain/repo-patches/packages/darshan-runtime/package.py
@@ -0,0 +1,11 @@
+from spack.pkg.builtin.darshan_runtime import DarshanRuntime as BuiltinDarshanRuntime
+
+
+class DarshanRuntime(BuiltinDarshanRuntime):
+    __doc__ = BuiltinDarshanRuntime.__doc__
+
+    def setup_run_environment(self, env):
+        # The upstream recipe uses the $HOME environment variable, which leads to
+        # fixing the BBP CI user home directory in the module specification. With
+        # this change, the home directory will be interpreted during module load.
+        env.set('DARSHAN_LOG_DIR_PATH', "~")
