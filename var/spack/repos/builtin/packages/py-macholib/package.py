bc3fd992f402f705ff62ec20ac1d4c05aca85b63
Seth R Johnson <johnsonsr@ornl.gov>
2022-08-17 13:23:02 -0400

    Update dependencies
    Even 1.11 requires `altgraph (>=0.15)`. The lastest requires no
    additional dependencies.

--- var/spack/repos/builtin/packages/py-macholib/package.py
+++ var/spack/repos/builtin/packages/py-macholib/package.py
@@ -15,4 +15,4 @@ class PyMacholib(PythonPackage):
     version("1.11", "c4180ffc6f909bf8db6cd81cff4b6f601d575568f4d5dee148c830e9851eb9db")
 
     depends_on("py-setuptools", type=("build", "run"))
-    depends_on("py-altgraph", type=("build", "run"))
+    depends_on("py-altgraph@0.15:", type=("build", "run"))
