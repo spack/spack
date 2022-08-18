272d7124a07591882c1fc0fd7d1245a9924c03eb
Jen Herting <jen@herting.cc>
2022-08-18 11:27:35 -0400

    [py-loguru]
    - python is a runtime dependency
    - setuptools is a build dependency

--- var/spack/repos/builtin/packages/py-loguru/package.py
+++ var/spack/repos/builtin/packages/py-loguru/package.py
@@ -14,7 +14,8 @@ class PyLoguru(PythonPackage):
 
     version('0.6.0', sha256='066bd06758d0a513e9836fd9c6b5a75bfb3fd36841f4b996bc60b547a309d41c')
 
-    depends_on('python@3.5:', type='build')
+    depends_on('python@3.5:', type=('build', 'run'))
+    depends_on('py-setuptools', type='build')
     depends_on('py-aiocontextvars@0.2.0:', when='^python@3.6:', type=('build', 'run'))
     depends_on('py-colorama@0.3.4:', when='platform=windows', type=('build', 'run'))
     # Missing dependency required for windows
