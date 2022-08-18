6038b3ff8143c7e9dae41faa3138a69635a23232
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2022-08-18 03:25:56 +0200

    mpich: add patch fixing MPI_MIN for unsigned integers (#32188)

--- var/spack/repos/builtin/packages/mpich/package.py
+++ var/spack/repos/builtin/packages/mpich/package.py
@@ -180,6 +180,14 @@ with '-Wl,-commons,use_dylibs' and without
         when="@3.3:3.3.0",
     )
 
+    # Fix reduce operations for unsigned integers
+    # See https://github.com/pmodels/mpich/issues/6083
+    patch(
+        "https://github.com/pmodels/mpich/commit/3a1f618e017547c9710ab4fb01ae258a01477190.patch?full_index=1",
+        sha256="d4c0e99a80f6cb0cb0ced91f6ad5da776c4a70f70f805f08096939ec9a92483e",
+        when="@4.0:4.0.2",
+    )
+
     depends_on("findutils", type="build")
     depends_on("pkgconfig", type="build")
 
