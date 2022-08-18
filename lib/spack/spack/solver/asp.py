b0b5c771263c27f20910f78091feda724c3c9650
psakiev <psakiev@sandia.gov>
2022-08-18 06:30:51 -0600

    Add error messages to naked asserts in asp.py
    Naked assertions in asp.py if/when hit create a blank error message for
    users.  This PR adds error messages to all naked asserts in asp.py even
    if it seems unlikely they will ever be needed.

--- lib/spack/spack/solver/asp.py
+++ lib/spack/spack/solver/asp.py
@@ -307,7 +307,10 @@ def check_same_flags(flag_dict_1, flag_dict_2):
     for t in types:
         values1 = set(flag_dict_1.get(t, []))
         values2 = set(flag_dict_2.get(t, []))
-        assert values1 == values2
+        error_msg = "A mismatch in flags has occured:"
+        error_msg += "\n\tvalues1: {v1}\n\tvalues2: {v2}".format(v1=values1, v2=values2)
+        error_msg += "\n Please report this as an issue to the spack maintainers"
+        assert values1 == values2, error_msg
 
 
 def check_packages_exist(specs):
@@ -363,7 +366,11 @@ class Result(object):
 
         Modeled after traceback.format_stack.
         """
-        assert self.control
+        error_msg = (
+            "Internal Error: ASP Result.control not populated. Pleasse report to spack"
+            " maintainers"
+        )
+        assert self.control, error_msg
 
         symbols = dict((a.literal, a.symbol) for a in self.control.symbolic_atoms)
 
@@ -382,7 +389,11 @@ class Result(object):
         ensure unsatisfiability. This algorithm reduces the core to only those
         essential facts.
         """
-        assert self.control
+        error_msg = (
+            "Internal Error: ASP Result.control not populated. Pleasse report to spack"
+            " maintainers"
+        )
+        assert self.control, error_msg
 
         min_core = core[:]
         for fact in core:
@@ -821,7 +832,11 @@ class SpackSolverSetup(object):
     def spec_versions(self, spec):
         """Return list of clauses expressing spec's version constraints."""
         spec = specify(spec)
-        assert spec.name
+        msg = (
+            "Internal Error: spec with no name occured. Please report this to the spack"
+            " maintainers."
+        )
+        assert spec.name, msg
 
         if spec.concrete:
             return [fn.version(spec.name, spec.version)]
@@ -1137,7 +1152,11 @@ class SpackSolverSetup(object):
 
     def provider_defaults(self):
         self.gen.h2("Default virtual providers")
-        assert self.possible_virtuals is not None
+        msg = (
+            "Internal Error: possible_virtuals is not populated. Please report to the spack"
+            " maintainers"
+        )
+        assert self.possible_virtuals is not None, msg
         self.virtual_preferences(
             "all",
             lambda v, p, i: self.gen.fact(fn.default_provider_preference(v, p, i)),
@@ -1656,7 +1675,11 @@ class SpackSolverSetup(object):
 
     def virtual_providers(self):
         self.gen.h2("Virtual providers")
-        assert self.possible_virtuals is not None
+        msg = (
+            "Internal Error: possible_virtuals is not populated. Please report to the spack"
+            " maintainers"
+        )
+        assert self.possible_virtuals is not None, msg
 
         # what provides what
         for vspec in sorted(self.possible_virtuals):
@@ -2156,6 +2179,10 @@ class SpecBuilder(object):
                 tty.debug(msg)
                 continue
 
+            msg = (
+                "Internal Error: Uncallable action found in asp.py.  Please report to the spack"
+                " maintainers."
+            )
             assert action and callable(action)
 
             # ignore predicates on virtual packages, as they're used for
@@ -2226,7 +2253,14 @@ def _develop_specs_from_env(spec, env):
     path = os.path.normpath(os.path.join(env.path, dev_info["path"]))
 
     if "dev_path" in spec.variants:
-        assert spec.variants["dev_path"].value == path
+        error_msg = (
+            "The dev_path for spec {name} is not connected to a valid environment path."
+            "Please note that develop specs can only be used inside an environment"
+            "Path difs are \n\tdev_path:{dev_path}\n\tenv_based_path:{env_path}"
+        )
+        error_msg.format(name=spec.name, dev_path=spec.variants("dev_path"), env_path=path)
+
+        assert spec.variants["dev_path"].value == path, error_msg
     else:
         spec.variants.setdefault("dev_path", spack.variant.SingleValuedVariant("dev_path", path))
     spec.constrain(dev_info["spec"])
