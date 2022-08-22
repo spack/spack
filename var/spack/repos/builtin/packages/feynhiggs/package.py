e0c77f4b69089c181f615568e5e0bf3f9b4fbfec
haralmha <47558670+haralmha@users.noreply.github.com>
2022-08-22 12:20:09 +0200

    feynhiggs: update url (#31760)

--- var/spack/repos/builtin/packages/feynhiggs/package.py
+++ var/spack/repos/builtin/packages/feynhiggs/package.py
@@ -13,12 +13,12 @@ class Feynhiggs(AutotoolsPackage):
     with real/complex parameters at the highest level of accuracy."""
 
     homepage = "https://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/cFeynHiggs.html"
-    url = "https://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/FeynHiggs-2.18.1.tar.gz"
+    url = "https://lcgpackages.web.cern.ch/tarFiles/sources/MCGeneratorsTarFiles/FeynHiggs-2.18.1.tar.gz"
 
     maintainers = ["vvolkl"]
     tags = ["hep"]
 
-    version("2.18.1", sha256="a9cdc4e2759f96fb9bd981b7be1ba8df070fb20c46d5b95e0c9700fccafe5ee6")
+    version("2.18.1", sha256="3aba89cac6397d7e1a8a9d9dcfeed9fb32eeeee98768b0c0c9f444c2cc125ab9")
 
     def configure_args(self):
         return ["FFLAGS=-fPIC", "CFLAGS=-fPIC"]
