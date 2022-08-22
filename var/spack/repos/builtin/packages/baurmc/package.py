2097ef74cf99b390b98ad7878074780f8fdf90aa
vvolkl <vvolkl@users.noreply.github.com>
2022-08-22 10:53:24 +0000

    [@spackbot] updating style on behalf of vvolkl

--- var/spack/repos/builtin/packages/baurmc/package.py
+++ var/spack/repos/builtin/packages/baurmc/package.py
@@ -31,9 +31,9 @@ class Baurmc(AutotoolsPackage):
 
     def configure_args(self):
         return [
-            '--userfflags=-fno-automatic',
-            '--userfflags=-fallow-argument-mismatch',
-            '--enable-shared'
+            "--userfflags=-fno-automatic",
+            "--userfflags=-fallow-argument-mismatch",
+            "--enable-shared",
         ]
 
     def install(self, spec, prefix):
