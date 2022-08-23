b8c4a57b857fa7188745c296d4e51d5c4a6e8f9e
Robert Pavel <rspavel@lanl.gov>
2022-08-23 11:24:26 -0600

    Style Check and Fixed gfortran Check
    Fixed style issues and changed gnu toolchain check to a gfortran check
    due to hybrid compilers (e.g. clang+gfortran)

--- var/spack/repos/builtin/packages/e3sm-kernels/package.py
+++ var/spack/repos/builtin/packages/e3sm-kernels/package.py
@@ -6,8 +6,12 @@
 from spack.package import *
 import os
 
+
 class E3smKernels(MakefilePackage):
-    """Climate kernels for Co-design that  originate from the Energy Exascale Earth System Model (E3SM)."""
+    """
+    Climate kernels for Co-design that originate from the Energy
+    Exascale Earth System Model (E3SM).
+    """
 
     homepage = "https://github.com/e3SM-Project/codesign-kernels"
     url = "https://github.com/E3SM-Project/codesign-kernels/archive/refs/tags/v1.0.tar.gz"
@@ -26,7 +30,7 @@ class E3smKernels(MakefilePackage):
             "mmf-mpdata-tracer",
         ),
         description="Specify E3SM Kernel to Build",
-        multi=False
+        multi=False,
     )
 
     @property
@@ -38,7 +42,8 @@ class E3smKernels(MakefilePackage):
         # Spack will provide optimization flags
         # But we still need to pass in fortran flags for gfortran
         args = []
-        if self.spec.satisfies("%gcc"):
+        # Test for gfortran specifically due to hybrid compilers like llvm
+        if "gfortran" in self.compiler.fc:
             args.append("FFLAGS=-ffree-line-length-none")
         return args
 
@@ -46,7 +51,6 @@ class E3smKernels(MakefilePackage):
         # Manually copy binaries over
         mkdir(prefix.bin)
         if self.spec.variants["kernel"].value == "atmosphere":
-           install(os.path.join("atmosphere", "atm"), prefix.bin.atm)
+            install(os.path.join("atmosphere", "atm"), prefix.bin.atm)
         elif self.spec.variants["kernel"].value == "mmf-mpdata-tracer":
-           install(os.path.join("mmf-mpdata-tracer", "advect"), prefix.bin.advect)
-
+            install(os.path.join("mmf-mpdata-tracer", "advect"), prefix.bin.advect)
