8e51372aa251f12af1086e46467891e55b030b8e
qwertos <qwertos@users.noreply.github.com>
2022-08-23 11:21:52 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-python-javabridge/package.py
+++ var/spack/repos/builtin/packages/py-python-javabridge/package.py
@@ -11,11 +11,11 @@ class PyPythonJavabridge(PythonPackage):
     Java virtual machine (JVM) from Python and interact with it."""
 
     homepage = "https://github.com/CellProfiler/python-javabridge/"
-    pypi     = "python-javabridge/python-javabridge-4.0.3.tar.gz"
+    pypi = "python-javabridge/python-javabridge-4.0.3.tar.gz"
 
-    version('4.0.3', sha256='3fee0c235efcfe866f95695fdc0b6289eab2371043b32ff4ca6feff098de59c5')
+    version("4.0.3", sha256="3fee0c235efcfe866f95695fdc0b6289eab2371043b32ff4ca6feff098de59c5")
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-cython@0.29.16:', type='build')
-    depends_on('py-numpy@1.20.1:', type=('build', 'run'))
-    depends_on('java', type=('build', 'run', 'link'))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-cython@0.29.16:", type="build")
+    depends_on("py-numpy@1.20.1:", type=("build", "run"))
+    depends_on("java", type=("build", "run", "link"))
