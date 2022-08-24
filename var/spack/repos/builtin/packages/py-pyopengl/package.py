4a8b1aab03a6acb1cec233414df244d51f6568fc
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2022-08-24 19:48:52 +0200

    py-pyopengl: new simple package

---
+++ var/spack/repos/builtin/packages/py-pyopengl/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyPyopengl(PythonPackage):
+    """yOpenGL is the most common cross platform Python binding to OpenGL and related APIs."""
+
+    homepage = "http://pyopengl.sourceforge.net"
+    url = "https://files.pythonhosted.org/packages/py3/p/pyopengl/PyOpenGL-3.1.6-py3-none-any.whl"
+    list_url = "https://pypi.org/simple/pyopengl/"
+
+    version(
+        "3.1.6",
+        sha256="a7139bc3e15d656feae1f7e3ef68c799941ed43fadc78177a23db7e946c20738",
+        expand=False,
+    )
+
+    depends_on("python@3.6:", type=("build", "run"))
