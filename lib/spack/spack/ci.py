df14eb57aa0a83a6f5b252dfb4f6fa59525a9130
Scott Wittenburg <scott.wittenburg@kitware.com>
2022-08-24 12:25:08 -0600

    gitlab: produce list of rebuilt specs to copy

--- lib/spack/spack/ci.py
+++ lib/spack/spack/ci.py
@@ -36,6 +36,7 @@ import spack.repo
 import spack.util.executable as exe
 import spack.util.gpg as gpg_util
 import spack.util.spack_yaml as syaml
+import spack.util.url as url_util
 import spack.util.web as web_util
 from spack.error import SpackError
 from spack.reporters.cdash import CDash
@@ -645,6 +646,12 @@ def generate_gitlab_ci_yaml(
     spack_pipeline_type = os.environ.get("SPACK_PIPELINE_TYPE", None)
 
     spack_buildcache_copy = os.environ.get("SPACK_COPY_BUILDCACHE", None)
+    if spack_buildcache_copy:
+        buildcache_copies = {
+            "destination_url_prefix": spack_buildcache_copy,
+            "hashes": {},
+        }
+        buildcache_copy_src_prefix = remote_mirror_override or remote_mirror_url
 
     if "mirrors" not in yaml_root or len(yaml_root["mirrors"].values()) < 1:
         tty.die("spack ci generate requires an env containing a mirror")
@@ -1020,6 +1027,18 @@ def generate_gitlab_ci_yaml(
                         "{0} ({1})".format(release_spec, release_spec_dag_hash)
                     )
 
+                # Only keep track of these if we are doing the copy thing
+                if spack_buildcache_copy:
+                    # TODO: This assumes signed version of the spec
+                    buildcache_copies["hashes"][release_spec_dag_hash] = [
+                        url_util.join(buildcache_copy_src_prefix,
+                                      bindist.build_cache_relative_path(),
+                                      bindist.tarball_name(release_spec, ".spec.json.sig")),
+                        url_util.join(buildcache_copy_src_prefix,
+                                      bindist.build_cache_relative_path(),
+                                      bindist.tarball_path_name(release_spec, ".spack"))
+                    ]
+
                 if artifacts_root:
                     job_dependencies.append(
                         {"job": generate_job_name, "pipeline": "{0}".format(parent_pipeline_id)}
@@ -1285,6 +1304,19 @@ def generate_gitlab_ci_yaml(
         spack_stack_name = os.environ.get("SPACK_CI_STACK_NAME", None)
         if spack_stack_name:
             output_object["variables"]["SPACK_CI_STACK_NAME"] = spack_stack_name
+
+        if spack_buildcache_copy:
+            # Write out the file describing specs that should be copied
+            copy_specs_dir = os.path.join(pipeline_artifacts_dir, "specs_to_copy")
+
+            if not os.path.exists(copy_specs_dir):
+                os.makedirs(copy_specs_dir)
+
+            copy_specs_file = os.path.join(copy_specs_dir, "copy_{}_specs.json".format(
+                spack_stack_name if spack_stack_name else "rebuilt"))
+
+            with open(copy_specs_file, 'w') as fd:
+                fd.write(json.dumps(buildcache_copies))
 
         sorted_output = {}
         for output_key, output_value in sorted(output_object.items()):
