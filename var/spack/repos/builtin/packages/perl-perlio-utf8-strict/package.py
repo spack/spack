c9f719966d03b30c4a471af087624998a8c0d89c
dlkuehn <dkuehn@iastate.edu>
2022-08-26 15:29:44 -0500

    New packages: roary plus 5 new perl-* (#32217)
    * New package: roary

--- var/spack/repos/builtin/packages/perl-perlio-utf8-strict/package.py
+++ var/spack/repos/builtin/packages/perl-perlio-utf8-strict/package.py
@@ -12,4 +12,7 @@ class PerlPerlioUtf8Strict(PerlPackage):
     homepage = "https://metacpan.org/pod/PerlIO::utf8_strict"
     url = "http://search.cpan.org/CPAN/authors/id/L/LE/LEONT/PerlIO-utf8_strict-0.002.tar.gz"
 
+    version("0.009", sha256="ba82cf144820655d6d4836d12dde65f8895a3d905aeb4aa0b421249f43284c14")
     version("0.002", sha256="6e3163f8a2f1d276c975f21789d7a07843586d69e3e6156ffb67ef6680ceb75f")
+
+    depends_on("perl-module-build", type="build")
