ba3722b224803d3c454766b8a7fa35b6b6121396
Chris White <white238@llnl.gov>
2022-08-25 18:05:52 -0700

    Update lib/spack/spack/config.py
    Co-authored-by: Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -1020,9 +1020,13 @@ def read_config_file(filename, schema=None):
         raise ConfigFileError("Config file is empty or is not a valid YAML dict: %s" % filename)
 
     except MarkedYAMLError as e:
-        msg = "Error parsing yaml: %s" % (filename)
-        if e.context_mark:
-            msg += ": %s" % str(e.context_mark)
+        msg = "Error parsing yaml"
+        mark = e.context_mark if e.context_mark else e.problem_mark
+        if mark:
+            line, column = mark.line, mark.column
+            msg += ": near %s, %s, %s" % (mark.name, str(line), str(column))
+        else:
+            msg += ": %s" % (filename)
         msg += ": %s" % (e.problem)
         raise ConfigFileError(msg)
 
