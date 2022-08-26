6569e22e64735a7ed26bc8c58b3da1ee52ff291a
marcost2 <marcostolcachir@gmail.com>
2022-08-26 12:35:03 -0300

    perl-bignum: Fix styling issues

--- var/spack/repos/builtin/packages/perl-bignum/package.py
+++ var/spack/repos/builtin/packages/perl-bignum/package.py
@@ -10,11 +10,11 @@ class PerlBignum(PerlPackage):
     """bigint/bigrat/bignum - transparent big number support"""
 
     homepage = "https://github.com/pjacklam/p5-bignum"
-    url      = "https://cpan.metacpan.org/authors/id/P/PJ/PJACKLAM/bignum-0.66.tar.gz"
+    url = "https://cpan.metacpan.org/authors/id/P/PJ/PJACKLAM/bignum-0.66.tar.gz"
 
-    version('0.66', sha256='26d48fb4b63a4b738ab84b577f9de7cdec164fe5f8a7089010a1ec17e127ed97')
+    version("0.66", sha256="26d48fb4b63a4b738ab84b577f9de7cdec164fe5f8a7089010a1ec17e127ed97")
 
-    depends_on('perl@5.6.0:', type=('build', 'run'))
-    depends_on('perl-math-bigrat', type=('build', 'run'))
-    depends_on('perl-math-bigint', type=('build', 'run'))
-    depends_on('perl-extutils-makemaker', type=('build'))
+    depends_on("perl@5.6.0:", type=("build", "run"))
+    depends_on("perl-math-bigrat", type=("build", "run"))
+    depends_on("perl-math-bigint", type=("build", "run"))
+    depends_on("perl-extutils-makemaker", type=("build"))
