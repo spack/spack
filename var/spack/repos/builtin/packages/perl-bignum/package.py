cbe488958619186df6149108e402903c311eedc2
marcost2 <marcostolcachir@gmail.com>
2022-08-26 11:51:36 -0300

    perl-bignum: Add missing dependencies

--- var/spack/repos/builtin/packages/perl-bignum/package.py
+++ var/spack/repos/builtin/packages/perl-bignum/package.py
@@ -15,4 +15,6 @@ class PerlBignum(PerlPackage):
     version('0.66', sha256='26d48fb4b63a4b738ab84b577f9de7cdec164fe5f8a7089010a1ec17e127ed97')
 
     depends_on('perl@5.6.0:', type=('build', 'run'))
+    depends_on('perl-math-bigrat', type=('build', 'run'))
+    depends_on('perl-math-bigint', type=('build', 'run'))
     depends_on('perl-extutils-makemaker', type=('build'))
