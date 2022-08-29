0a20fa3f46611989b82b5933f44fdb780bfdbe7d
MikeG <michael.gevaert@epfl.ch>
2022-08-29 08:41:47 +0200

    bump brainbuilder to 0.18.0 (#1674)

--- bluebrain/repo-bluebrain/packages/brainbuilder/package.py
+++ bluebrain/repo-bluebrain/packages/brainbuilder/package.py
@@ -13,6 +13,7 @@ class Brainbuilder(PythonPackage):
     git      = "git@bbpgitlab.epfl.ch:nse/brainbuilder.git"
 
     version('develop', branch='main')
+    version('0.18.0', tag='brainbuilder-v0.18.0')
     version('0.17.0', tag='brainbuilder-v0.17.0')
 
     depends_on('py-setuptools', type=('build', 'run'))
@@ -30,6 +31,7 @@ class Brainbuilder(PythonPackage):
     depends_on('py-libsonata@0.1.6:', type=('build', 'run'))
     depends_on('py-voxcell@3.1.0:', type=('build', 'run'))
     depends_on('py-morphio@3.0.0:3.999', type=('build', 'run'))
+    depends_on('py-bluepysnap@0.13.0:0.999,develop', type=('build', 'run'))
 
     depends_on('py-attrs@:19.999', type=('build', 'run'))
     depends_on('py-entity-management@0.1.11:0.999', type=('build', 'run'))
