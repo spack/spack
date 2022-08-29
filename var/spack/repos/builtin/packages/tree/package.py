b45de2aad0bfbc3194f417742691c2ed60961c63
Alex Hedges <aphedges1@gmail.com>
2022-08-29 14:15:37 -0400

    tree: add 2.0.3

--- var/spack/repos/builtin/packages/tree/package.py
+++ var/spack/repos/builtin/packages/tree/package.py
@@ -19,6 +19,7 @@ class Tree(Package):
     homepage = "http://mama.indstate.edu/users/ice/tree/"
     url = "http://mama.indstate.edu/users/ice/tree/src/tree-1.7.0.tgz"
 
+    version("2.0.3", sha256="ba14e77b5f9dc7f8250c3f702ec5b6be2f93cd0fa87311bab3239676866a3b1d")
     version("2.0.2", sha256="7d693a1d88d3c4e70a73e03b8dbbdc12c2945d482647494f2f5bd83a479eeeaf")
     version("1.8.0", sha256="715d5d4b434321ce74706d0dd067505bb60c5ea83b5f0b3655dae40aa6f9b7c2")
     version("1.7.0", sha256="6957c20e82561ac4231638996e74f4cfa4e6faabc5a2f511f0b4e3940e8f7b12")
