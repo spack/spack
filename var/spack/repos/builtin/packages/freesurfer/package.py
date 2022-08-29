b8bec76bbcc4b71f05a823b15c7429c67bed3942
Rob Groner <rob.groner@psu.edu>
2022-08-29 09:57:08 -0400

    Change URLs to https and move url function to proper place.

--- var/spack/repos/builtin/packages/freesurfer/package.py
+++ var/spack/repos/builtin/packages/freesurfer/package.py
@@ -10,7 +10,7 @@ class Freesurfer(Package):
     """Freesurfer is an open source software suite for processing and analyzing
     (human) brain MRI images."""
 
-    homepage = "http://freesurfer.net/"
+    homepage = "https://freesurfer.net/"
 
     # A license is required, but is free to obtain.
     license_required = True
@@ -18,17 +18,17 @@ class Freesurfer(Package):
 
     maintainers = ["robgics"]
 
-    def url_for_version(self, version):
-        return "http://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/{0}/freesurfer-linux-centos7_x86_64-{1}.tar.gz".format(
-            version, version
-        )
-
     version("7.2.0", sha256="4cca78602f898bf633428b9d82cbb9b07e3ab97a86c620122050803779c86d62")
     version("7.1.1", sha256="6098b166fee8644f44f9ec88f3ffe88d05f2bc033cca60443e99e3e56f2e166b")
     version("7.1.0", sha256="1b8f26fe5c712433ddb74c47fe1895ed1d9fbff46cfae8aaae2697cb65ae8840")
 
     depends_on("mesa-glu")
     depends_on("qt")
+
+    def url_for_version(self, version):
+        return "https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/{0}/freesurfer-linux-centos7_x86_64-{1}.tar.gz".format(
+            version, version
+        )
 
     def setup_run_environment(self, env):
         env.prepend_path("PATH", self.prefix.bin)
