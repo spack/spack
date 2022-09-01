43c97383fc8d889dca4d442eece77862e6d6907a
Jared Popelar <jpopelar@txcorp.com>
2022-09-01 08:41:32 -0600

    Copy johnwparent's fix for installer.py

--- lib/spack/spack/installer.py
+++ lib/spack/spack/installer.py
@@ -879,7 +879,7 @@ class PackageInstaller(object):
             if spack.store.db.prefix_failed(dep):
                 action = "'spack install' the dependency"
                 msg = "{0} is marked as an install failure: {1}".format(dep_id, action)
-                raise InstallError(err.format(request.pkg_id, msg), pkg=dep_pkg)
+                raise InstallError(err.format(request.pkg_id, msg))
 
             # Attempt to get a read lock to ensure another process does not
             # uninstall the dependency while the requested spec is being
@@ -887,7 +887,7 @@ class PackageInstaller(object):
             ltype, lock = self._ensure_locked("read", dep_pkg)
             if lock is None:
                 msg = "{0} is write locked by another process".format(dep_id)
-                raise InstallError(err.format(request.pkg_id, msg), pkg=request.pkg)
+                raise InstallError(err.format(request.pkg_id, msg))
 
             # Flag external and upstream packages as being installed
             if dep_pkg.spec.external or dep_pkg.spec.installed_upstream:
@@ -942,7 +942,6 @@ class PackageInstaller(object):
                     "Install prefix collision for {0}".format(task.pkg_id),
                     long_msg="Prefix directory {0} already used by another "
                     "installed spec.".format(task.pkg.spec.prefix),
-                    pkg=task.pkg,
                 )
 
             # Make sure the installation directory is in the desired state
@@ -1636,8 +1635,7 @@ class PackageInstaller(object):
                 raise InstallError(
                     "Cannot proceed with {0}: {1} uninstalled {2}: {3}".format(
                         pkg_id, task.priority, dep_str, ",".join(task.uninstalled_deps)
-                    ),
-                    pkg=pkg,
+                    )
                 )
 
             # Skip the installation if the spec is not being installed locally
@@ -1662,7 +1660,7 @@ class PackageInstaller(object):
                 spack.hooks.on_install_failure(task.request.pkg.spec)
 
                 if self.fail_fast:
-                    raise InstallError(fail_fast_err, pkg=pkg)
+                    raise InstallError(fail_fast_err)
 
                 continue
 
@@ -1792,7 +1790,7 @@ class PackageInstaller(object):
                     )
                 # Terminate if requested to do so on the first failure.
                 if self.fail_fast:
-                    raise InstallError("{0}: {1}".format(fail_fast_err, str(exc)), pkg=pkg)
+                    raise InstallError("{0}: {1}".format(fail_fast_err, str(exc)))
 
                 # Terminate at this point if the single explicit spec has
                 # failed to install.
@@ -1801,7 +1799,7 @@ class PackageInstaller(object):
 
                 # Track explicit spec id and error to summarize when done
                 if task.explicit:
-                    failed_explicits.append((pkg, pkg_id, str(exc)))
+                    failed_explicits.append((pkg_id, str(exc)))
 
             finally:
                 # Remove the install prefix if anything went wrong during
@@ -1824,38 +1822,19 @@ class PackageInstaller(object):
         # Ensure we properly report if one or more explicit specs failed
         # or were not installed when should have been.
         missing = [
-            (request.pkg, request.pkg_id)
+            request.pkg_id
             for request in self.build_requests
             if request.install_args.get("install_package") and request.pkg_id not in self.installed
         ]
-
         if failed_explicits or missing:
-            for _, pkg_id, err in failed_explicits:
+            for pkg_id, err in failed_explicits:
                 tty.error("{0}: {1}".format(pkg_id, err))
 
-            for _, pkg_id in missing:
+            for pkg_id in missing:
                 tty.error("{0}: Package was not installed".format(pkg_id))
 
-            pkg = None
-            if len(failed_explicits) > 0:
-                pkg = failed_explicits[0][0]
-                ids = [pkg_id for _, pkg_id, _ in failed_explicits]
-                tty.debug(
-                    "Associating installation failure with first failed "
-                    "explicit package ({0}) from {1}".format(ids[0], ", ".join(ids))
-                )
-
-            if not pkg and len(missing) > 0:
-                pkg = missing[0][0]
-                ids = [pkg_id for _, pkg_id in missing]
-                tty.debug(
-                    "Associating installation failure with first "
-                    "missing package ({0}) from {1}".format(ids[0], ", ".join(ids))
-                )
-
             raise InstallError(
-                "Installation request failed.  Refer to reported errors for failing package(s).",
-                pkg=pkg,
+                "Installation request failed.  Refer to " "reported errors for failing package(s)."
             )
 
 
@@ -2154,7 +2133,7 @@ class BuildTask(object):
         # queue.
         if status == STATUS_REMOVED:
             msg = "Cannot create a build task for {0} with status '{1}'"
-            raise InstallError(msg.format(self.pkg_id, status), pkg=pkg)
+            raise InstallError(msg.format(self.pkg_id, status))
 
         self.status = status
 
@@ -2485,15 +2464,10 @@ class BuildRequest(object):
 
 
 class InstallError(spack.error.SpackError):
-    """Raised when something goes wrong during install or uninstall.
+    """Raised when something goes wrong during install or uninstall."""
 
-    The error can be annotated with a ``pkg`` attribute to allow the
-    caller to get the package for which the exception was raised.
-    """
-
-    def __init__(self, message, long_msg=None, pkg=None):
+    def __init__(self, message, long_msg=None):
         super(InstallError, self).__init__(message, long_msg)
-        self.pkg = pkg
 
 
 class BadInstallPhase(InstallError):
