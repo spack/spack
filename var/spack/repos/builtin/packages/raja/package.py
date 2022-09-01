93a9e25186ba9ebaa5a1d66698888f2b8586446f
Tom Scogland <scogland1@llnl.gov>
2022-09-01 14:27:56 -0700

    propagate openmp option to camp in umpire and use main camp for main and develop raja and umpire

--- var/spack/repos/builtin/packages/raja/package.py
+++ var/spack/repos/builtin/packages/raja/package.py
@@ -63,6 +63,8 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("camp@0.2.2:0.2.3", when="@0.14.0")
     depends_on("camp@0.1.0", when="@0.10.0:0.13.0")
     depends_on("camp@2022.03.0:", when="@2022.03.0:")
+    depends_on("camp@main", when="@main")
+    depends_on("camp@main", when="@develop")
     depends_on("camp+openmp", when="+openmp")
 
     depends_on("cmake@:3.20", when="+rocm", type="build")
--- var/spack/repos/builtin/packages/umpire/package.py
+++ var/spack/repos/builtin/packages/umpire/package.py
@@ -95,6 +95,9 @@ class Umpire(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("camp@0.2.2:0.2.3", when="@6.0.0")
     depends_on("camp@0.1.0", when="@5.0.0:5.0.1")
     depends_on("camp@2022.03.0:", when="@2022.03.0:")
+    depends_on("camp@main", when="@main")
+    depends_on("camp@main", when="@develop")
+    depends_on("camp+openmp", when="+openmp")
 
     with when("@5.0.0:"):
         with when("+cuda"):
