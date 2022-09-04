bbf43e15e331a4a3181795b99a4ec88201ee572a
Johannes Blaschke <johannes@blaschke.science>
2022-09-04 13:32:18 -0700

    Add OpenGraph extension for Sphinx

---
+++ var/spack/repos/builtin/packages/py-sphinxext-opengraph/package.py
@@ -0,0 +1,44 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install py-sphinxext-opengraph
+#
+# You can edit this file again by typing:
+#
+#     spack edit py-sphinxext-opengraph
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack.package import *
+
+
+class PySphinxextOpengraph(PythonPackage):
+    """Sphinx Extension to enable OGP support"""
+
+    homepage = "https://github.com/wpilibsuite/sphinxext-opengraph"
+    pypi = "sphinxext-opengraph/sphinxext-opengraph-0.6.3.tar.gz"
+
+    maintainers = ["JBlaschke"]
+
+    version("0.6.3", sha256="cd89e13cc7a44739f81b64ee57c1c20ef0c05dda5d1d8201d31ec2f34e4c29db")
+
+    depends_on("py-setuptools", type="build")
+
+    def global_options(self, spec, prefix):
+        options = []
+        return options
+
+    def install_options(self, spec, prefix):
+        options = []
+        return options
