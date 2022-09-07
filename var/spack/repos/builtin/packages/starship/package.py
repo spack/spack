dbec80162dc69e4cd94f3fd3cad3851b096ff41d
delucca <gpg@delucca.dev>
2022-09-07 20:18:33 -0300

    fix: lint

--- var/spack/repos/builtin/packages/starship/package.py
+++ var/spack/repos/builtin/packages/starship/package.py
@@ -1,25 +1,3 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install starship
-#
-# You can edit this file again by typing:
-#
-#     spack edit starship
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
