c033f8e1f23288b5dc2d5d8339b239afecae0866
John Parent <john.parent@kitware.com>
2022-09-07 14:45:02 -0400

    port expat

--- var/spack/repos/builtin/packages/expat/package.py
+++ var/spack/repos/builtin/packages/expat/package.py
@@ -8,7 +8,7 @@ import sys
 from spack.package import *
 
 
-class Expat(AutotoolsPackage):
+class Expat(CMakePackage):
     """Expat is an XML parser library written in C."""
 
     homepage = "https://libexpat.github.io/"
@@ -102,7 +102,7 @@ class Expat(AutotoolsPackage):
     # `~libbsd`.
     variant(
         "libbsd",
-        default=sys.platform != "darwin",
+        default=sys.platform != "darwin" and sys.platform != "win32",
         description="Use libbsd (for high quality randomness)",
     )
 
@@ -112,9 +112,13 @@ class Expat(AutotoolsPackage):
         url = "https://github.com/libexpat/libexpat/releases/download/R_{0}/expat-{1}.tar.bz2"
         return url.format(version.underscored, version.dotted)
 
-    def configure_args(self):
-        spec = self.spec
-        args = ["--without-docbook", "--enable-static"]
-        if "+libbsd" in spec and "@2.2.1:" in spec:
-            args.append("--with-libbsd")
+    def cmake_args(self):
+        args = [
+            self.define("EXPAT_BUILD_DOCS", False),
+            self.define("BUILD_SHARED_LIBS", False)
+        ]
+
+        if "+libbsd" in self.spec and "@2.2.1:" in self.spec:
+            args.append(self.define_from_variant("EXPAT_WITH_LIBBSD", "libbsd"))
+
         return args
