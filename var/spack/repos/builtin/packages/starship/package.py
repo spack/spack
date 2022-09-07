61f07acb3af414555fca49b008b48023c50d7501
delucca <gpg@delucca.dev>
2022-09-07 20:12:21 -0300

    fix: lint

--- var/spack/repos/builtin/packages/starship/package.py
+++ var/spack/repos/builtin/packages/starship/package.py
@@ -28,7 +28,22 @@ class Starship(Package):
 
     homepage = "https://starship.rs"
     url = "https://github.com/starship/starship/releases/download/v1.10.2/starship-x86_64-unknown-linux-gnu.tar.gz"
-    maintainers = ["andrewda","andytom","ATiltedTree","cappyzawa","davidkna","heyrict","jimleroyer","jletey","kidonng","matchai","Snuggle","tiffafoo","vladimyr","wyze"]
+    maintainers = [
+        "andrewda",
+        "andytom",
+        "ATiltedTree",
+        "cappyzawa",
+        "davidkna",
+        "heyrict",
+        "jimleroyer",
+        "jletey",
+        "kidonng",
+        "matchai",
+        "Snuggle",
+        "tiffafoo",
+        "vladimyr",
+        "wyze",
+    ]
 
     version("1.10.2", sha256="253a62e48c1b15d5465c876560b71e1d3485697e22ab7adea85e37cbe1a70a54")
 
