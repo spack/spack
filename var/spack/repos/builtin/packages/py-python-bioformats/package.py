bc519a680fe4cfeff290aa8de23b0ba763945ca2
qwertos <qwertos@users.noreply.github.com>
2022-09-08 13:31:12 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-python-bioformats/package.py
+++ var/spack/repos/builtin/packages/py-python-bioformats/package.py
@@ -13,12 +13,12 @@ class PyPythonBioformats(PythonPackage):
     a large number of formats, as well as writing to several formats."""
 
     homepage = "https://github.com/CellProfiler/python-bioformats/"
-    pypi     = "python-bioformats/python-bioformats-4.0.5.tar.gz"
+    pypi = "python-bioformats/python-bioformats-4.0.5.tar.gz"
 
-    version('4.0.5', sha256='f9fa3a2b3c0f1eac6070dff6c513444e9fde9a1f794ec4c21fca85833dbb5192')
-    version('4.0.0', sha256='9a952de4d326d961af0a497753a4b71b2f7844605023d170c931d3624e036506')
+    version("4.0.5", sha256="f9fa3a2b3c0f1eac6070dff6c513444e9fde9a1f794ec4c21fca85833dbb5192")
+    version("4.0.0", sha256="9a952de4d326d961af0a497753a4b71b2f7844605023d170c931d3624e036506")
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-boto3@1.14.23:', type=('build', 'run'))
-    depends_on('py-future@0.18.2:', type=('build', 'run'))
-    depends_on('py-python-javabridge@4.0.3', type=('build', 'run'))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-boto3@1.14.23:", type=("build", "run"))
+    depends_on("py-future@0.18.2:", type=("build", "run"))
+    depends_on("py-python-javabridge@4.0.3", type=("build", "run"))
