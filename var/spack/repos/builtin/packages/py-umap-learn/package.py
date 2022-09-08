66e07af841095f24236ed854c84b7313998f9f82
qwertos <qwertos@users.noreply.github.com>
2022-09-08 13:32:56 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-umap-learn/package.py
+++ var/spack/repos/builtin/packages/py-umap-learn/package.py
@@ -12,14 +12,14 @@ class PyUmapLearn(PythonPackage):
     but also for general non-linear dimension reduction."""
 
     homepage = "https://github.com/lmcinnes/umap"
-    pypi     = "umap-learn/umap-learn-0.5.3.tar.gz"
+    pypi = "umap-learn/umap-learn-0.5.3.tar.gz"
 
-    version('0.5.3', sha256='dbd57cb181c2b66d238acb5635697526bf24c798082daed0cf9b87f6a3a6c0c7')
+    version("0.5.3", sha256="dbd57cb181c2b66d238acb5635697526bf24c798082daed0cf9b87f6a3a6c0c7")
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-numpy@1.17:', type=('build', 'run'))
-    depends_on('py-scikit-learn@0.22:', type=('build', 'run'))
-    depends_on('py-scipy@1.0:', type=('build', 'run'))
-    depends_on('py-numba@0.49:', type=('build', 'run'))
-    depends_on('py-pynndescent@0.5:', type=('build', 'run'))
-    depends_on('py-tqdm', type=('build', 'run'))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-numpy@1.17:", type=("build", "run"))
+    depends_on("py-scikit-learn@0.22:", type=("build", "run"))
+    depends_on("py-scipy@1.0:", type=("build", "run"))
+    depends_on("py-numba@0.49:", type=("build", "run"))
+    depends_on("py-pynndescent@0.5:", type=("build", "run"))
+    depends_on("py-tqdm", type=("build", "run"))
