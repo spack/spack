45633badd95e9efa8ac7a00d8972636d2259074d
John Parent <john.parent@kitware.com>
2022-09-09 09:29:06 -0400

    re-escape regex replacement

--- lib/spack/llnl/util/filesystem.py
+++ lib/spack/llnl/util/filesystem.py
@@ -641,7 +641,7 @@ def copy_tree(src, dest, symlinks=True, ignore=None, _permissions=False):
                         def escaped_path(path):
                             return path.replace("\\", r"\\")
 
-                        new_target = re.sub(escaped_path(abs_src), abs_dest, target)
+                        new_target = re.sub(escaped_path(abs_src), escaped_path(abs_dest), target)
                         if new_target != target:
                             tty.debug("Redirecting link {0} to {1}".format(target, new_target))
                             target = new_target
