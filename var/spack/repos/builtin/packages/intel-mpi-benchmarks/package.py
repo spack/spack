982fb2cef126dd91be9dc45eaa10687861ae5a7c
Carson Woods <cwoods289@gmail.com>
2022-09-09 15:23:16 -0400

    Minor style changes

--- var/spack/repos/builtin/packages/intel-mpi-benchmarks/package.py
+++ var/spack/repos/builtin/packages/intel-mpi-benchmarks/package.py
@@ -44,7 +44,7 @@ class IntelMpiBenchmarks(MakefilePackage):
         msg="intel-mpi-benchmarks <= v2019.0 cannot be built with OpenMPI, "
         "please specify a different MPI implementation",
     )
-    
+
     # https://github.com/intel/mpi-benchmarks/pull/19
     patch("add_const.patch", when="@2019")
     # https://github.com/intel/mpi-benchmarks/pull/20
