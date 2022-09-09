0cdab5d4a3d48ecb606f70a7c6777696ca2d459e
Ishaan Desai <ishaandesai@gmail.com>
2022-09-09 13:12:23 +0200

    Missing comma

--- var/spack/repos/builtin/packages/py-pyprecice/package.py
+++ var/spack/repos/builtin/packages/py-pyprecice/package.py
@@ -45,7 +45,7 @@ class PyPyprecice(PythonPackage):
         "2.1.0",
         "2.0.2",
         "2.0.1",
-        "2.0.0"
+        "2.0.0",
     ]:
         depends_on("precice@" + ver, when="@" + ver)
 
