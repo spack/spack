a154ff8103b4e5cc03bfb28991e7d8b1b50a3736
qwertos <qwertos@users.noreply.github.com>
2022-09-09 08:01:58 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-session-info/package.py
+++ var/spack/repos/builtin/packages/py-session-info/package.py
@@ -11,10 +11,10 @@ class PySessionInfo(PythonPackage):
     loaded in the current session, Python, and the OS."""
 
     homepage = "https://gitlab.com/joelostblom/session_info"
-    pypi     = "session_info/session_info-1.0.0.tar.gz"
+    pypi = "session_info/session_info-1.0.0.tar.gz"
 
-    version('1.0.0', sha256='3cda5e03cca703f32ae2eadbd6bd80b6c21442cfb60e412c21cb8ad6d5cbb6b7')
+    version("1.0.0", sha256="3cda5e03cca703f32ae2eadbd6bd80b6c21442cfb60e412c21cb8ad6d5cbb6b7")
 
-    depends_on('python@3.6:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
-    depends_on('py-stdlib-list', type=('build', 'run'))
+    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-stdlib-list", type=("build", "run"))
