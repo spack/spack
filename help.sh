97ab3a7695ad39f84228f8fa6646ee19e287f2f8
frerappa <frerappa@gmail.com>
2022-09-12 15:25:35 -0300

    [py-parsel] Removed unnecessary files

--- help.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-# spack help command
-
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-# spack test command
-"spack" "-d" "-v" "test" "run" "--cdash-upload-url" "file://fake/submit.php?project=ci-unit-testing" "--cdash-build" "ci-test-build" "--cdash-site" "fake-site (test-runner)" "--cdash-buildstamp" "ci-test-build-stamp" "printing-package"
