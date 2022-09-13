b1787995d19e428d53af63f2753c170821737238
Jordan Ogas <jogas@lanl.gov>
2022-09-13 09:25:55 -0600

    charliecloud: deprecate old versions (#32462)
    * tidy, deprecate old versions
    
    * bump python
    
    * begrudgingly apply less readable style
    
    * adjust comment spacing
    
    * apply ghastly multiline function arguments

--- var/spack/repos/builtin/packages/charliecloud/package.py
+++ var/spack/repos/builtin/packages/charliecloud/package.py
@@ -18,42 +18,84 @@ class Charliecloud(AutotoolsPackage):
 
     version("master", branch="master")
     version("0.29", sha256="c89562e9dce4c10027434ad52eaca2140e2ba8667aa1ec9eadf789b4d7c1a6db")
-    version("0.28", sha256="1ce43b012f475bddb514bb75993efeda9e58ffa93ddbdbd9b86d647f57254c3b")
-    version("0.27", sha256="1142938ce73ec8a5dfe3a19a241b1f1ffbb63b582ac63d459aebec842c3f4b72")
-    version("0.26", sha256="5e1e64e869c59905fac0cbbd6ceb82340ee54728415d28ef588fd5de5557038a")
-    version("0.25", sha256="62d6fd211e3a573f54578e1b01d5c298f9788b7eaf2db46ac94c2dcef604cc94")
-    version("0.24", sha256="63379bcbad7b90b33457251696d6720416e4acefcf2b49cd6cb495a567e511c2")
-    version("0.23", sha256="5e458b943ad0e27d1264bb089e48d4a676219179b0e96a7d761387a36c45b4d9")
-    version("0.22", sha256="f65e4111ce87e449c656032da69f3b1cfc70a5a416a5e410329c1b0b2e953907")
-    version("0.21", sha256="024884074d283c4a0387d899161610fa4ae739ac1efcc9e53d7d626ddc20359f")
-    version("0.19", sha256="99619fd86860cda18f7f7a7cf7391f702ec9ebd3193791320dea647769996447")
-    version("0.18", sha256="15ce63353afe1fc6bcc10979496a54fcd5628f997cb13c827c9fc7afb795bdc5")
+    version(
+        "0.28",
+        deprecated=True,
+        sha256="1ce43b012f475bddb514bb75993efeda9e58ffa93ddbdbd9b86d647f57254c3b",
+    )
+    version(
+        "0.27",
+        deprecated=True,
+        sha256="1142938ce73ec8a5dfe3a19a241b1f1ffbb63b582ac63d459aebec842c3f4b72",
+    )
+    version(
+        "0.26",
+        deprecated=True,
+        sha256="5e1e64e869c59905fac0cbbd6ceb82340ee54728415d28ef588fd5de5557038a",
+    )
+    version(
+        "0.25",
+        deprecated=True,
+        sha256="62d6fd211e3a573f54578e1b01d5c298f9788b7eaf2db46ac94c2dcef604cc94",
+    )
+    version(
+        "0.24",
+        deprecated=True,
+        sha256="63379bcbad7b90b33457251696d6720416e4acefcf2b49cd6cb495a567e511c2",
+    )
+    version(
+        "0.23",
+        deprecated=True,
+        sha256="5e458b943ad0e27d1264bb089e48d4a676219179b0e96a7d761387a36c45b4d9",
+    )
+    version(
+        "0.22",
+        deprecated=True,
+        sha256="f65e4111ce87e449c656032da69f3b1cfc70a5a416a5e410329c1b0b2e953907",
+    )
+    version(
+        "0.21",
+        deprecated=True,
+        sha256="024884074d283c4a0387d899161610fa4ae739ac1efcc9e53d7d626ddc20359f",
+    )
+    version(
+        "0.19",
+        deprecated=True,
+        sha256="99619fd86860cda18f7f7a7cf7391f702ec9ebd3193791320dea647769996447",
+    )
+    version(
+        "0.18",
+        deprecated=True,
+        sha256="15ce63353afe1fc6bcc10979496a54fcd5628f997cb13c827c9fc7afb795bdc5",
+    )
+    variant("docs", default=False, description="Build man pages and html docs")
 
+    # Autoconf.
     depends_on("m4", type="build")
     depends_on("autoconf", type="build")
     depends_on("automake", type="build")
     depends_on("libtool", type="build")
 
-    depends_on("python@3.5:", type="run")
-    # Version 0.25+ bundle the preferred lark version.
-    depends_on("py-lark", type="run", when="@:0.24")
+    # Image manipulation.
+    depends_on("python@3.6:", type="run")
     depends_on("py-requests", type="run")
-    # autogen.sh requires pip and wheel (only needed for git checkouts)
-    depends_on("py-pip@21.1.2:", type="build", when="@master")
-    depends_on("py-wheel", type="build", when="@master")
-    depends_on("git@2.28.1:", type="run", when="@0.29:")
+    depends_on("git@2.28.1:", type="run", when="@0.29:")  # build cache
+    depends_on("py-lark", type="run", when="@:0.24")  # 0.25+ bundles lark
 
-    # Man pages and html docs variant.
-    variant("docs", default=False, description="Build man pages and html docs")
+    # Man page and html docs.
     depends_on("rsync", type="build", when="+docs")
     depends_on("py-sphinx", type="build", when="+docs")
     depends_on("py-sphinx-rtd-theme", type="build", when="+docs")
 
-    # See https://github.com/spack/spack/pull/16049.
-    conflicts("platform=darwin", msg="This package does not build on macOS")
-
     # Bash automated testing harness (bats).
     depends_on("bats@0.4.0", type="test")
+
+    # Require pip and wheel for git checkout builds (master).
+    depends_on("py-pip@21.1.2:", type="build", when="@master")
+    depends_on("py-wheel", type="build", when="@master")
+
+    # See https://github.com/spack/spack/pull/16049.
+    conflicts("platform=darwin", msg="This package does not build on macOS")
 
     def autoreconf(self, spec, prefix):
         which("bash")("autogen.sh")
