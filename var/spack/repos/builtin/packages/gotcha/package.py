6b2d768c314514822f1f917cf1a9c5dadfe46c60
Cameron Stanavige <stanavige1@llnl.gov>
2022-09-13 09:53:11 -0700

    unifyfs & gotcha: new releases (#32625)
    UnifyFS:
    - Add 1.0 release
    - Deprecate older, unsupported versions
    - Set fortran variant to true by default
    - Update gotcha and mochi-margo dependency versions for unifyfs@1.0
      and unifyfs@develop
    - Add conflict of unifyfs with libfabric 1.13.* versions
    - Update configure_args to use helper functions
    
    GOTCHA: Hasn't been under active development for a couple years but
    the develop branch has some fixes UnifyFS uses. To avoid having
    UnifyFS v1.0 depend on a develop branch of a dependency, this creates
    a new release in the Gotcha Spack package based on the most recent
    commit of the develop branch.

--- var/spack/repos/builtin/packages/gotcha/package.py
+++ var/spack/repos/builtin/packages/gotcha/package.py
@@ -17,6 +17,7 @@ class Gotcha(CMakePackage):
 
     version("develop", branch="develop")
     version("master", branch="master")
+    version("1.0.4", commit="08a5279cef051ed39bc92c5f77f55ca5b41a405e")
     version("1.0.3", tag="1.0.3")
     version("1.0.2", tag="1.0.2")
     version("0.0.2", tag="0.0.2")
