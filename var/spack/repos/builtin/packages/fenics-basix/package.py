297b9f9f52d5536b68f3b16e0a8f6c90c42da620
Garth N. Wells <gnw20@cam.ac.uk>
2022-09-13 15:35:14 +0100

    Skip to Basix 0.5.1
    The 0.5.1 release removes the C++ build dependency on Python that sneaked into the 0.5.0 build system.

--- var/spack/repos/builtin/packages/fenics-basix/package.py
+++ var/spack/repos/builtin/packages/fenics-basix/package.py
@@ -15,7 +15,7 @@ class FenicsBasix(CMakePackage):
     maintainers = ["mscroggs", "chrisrichardson", "garth-wells"]
 
     version("main", branch="main")
-    version("0.5.0", sha256="7cd5a8b053219941e84068731b88338002f1f09dfc73dcd41f9cd584588dc65a")
+    version("0.5.1", sha256="69133476ac35f0bd0deccb480676030378c341d7dfb2adaca22cd16b7e1dc1cb")
     version("0.4.2", sha256="a54f5e442b7cbf3dbb6319c682f9161272557bd7f42e2b8b8ccef88bc1b7a22f")
     version("0.3.0", sha256="9b148fd2a5485c94011fc6ca977ebdef0e51782a62b3654fc044f35b60e2bd07")
     version("0.2.0", sha256="e1ec537737adb283717060221635092474e3f2b5b5ba79dfac74aa496bec2fcb")
