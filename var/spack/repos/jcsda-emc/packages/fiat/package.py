be1052fb086ae4d242e101ba5aa0af89f2af1a3f
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-09-14 14:11:30 -0600

    Add patch intel_warnings.patch for package fiat, replace single quotes with double quotes

--- var/spack/repos/jcsda-emc/packages/fiat/package.py
+++ var/spack/repos/jcsda-emc/packages/fiat/package.py
@@ -13,36 +13,38 @@ class Fiat(CMakePackage):
     homepage = "https://github.com/ecmwf-ifs/fiat"
     git = "https://github.com/ecmwf-ifs/fiat.git"
 
-    maintainers = ['climbfuji']
+    maintainers = ["climbfuji"]
 
-    version('main', branch='main', no_cache=True)
+    version("main", branch="main", no_cache=True)
     # Defined for spack use only
-    version('1.0.0', commit='1295120464c3905e5edcbb887e4921686653eab8', preferred=True)
+    version("1.0.0", commit="1295120464c3905e5edcbb887e4921686653eab8", preferred=True)
 
-    variant('build_type', default='RelWithDebInfo',
-            description='CMake build type',
-            values=('Debug', 'Release', 'RelWithDebInfo'))
+    variant("build_type", default="RelWithDebInfo",
+            description="CMake build type",
+            values=("Debug", "Release", "RelWithDebInfo"))
 
-    variant('mpi', default=True, description='Use MPI?')
-    variant('openmp', default=True, description='Use OpenMP?')
-    variant('fckit', default=True, description='Use fckit?')
-    # variant('dr_hook_multi_precision_handles', default=False,
-    #    description='Use deprecated single precision handles for DR_HOOK?')
-    # variant('warnings', default=True, description='Enable compiler warnings')
+    variant("mpi", default=True, description="Use MPI?")
+    variant("openmp", default=True, description="Use OpenMP?")
+    variant("fckit", default=True, description="Use fckit?")
+    # variant("dr_hook_multi_precision_handles", default=False,
+    #    description="Use deprecated single precision handles for DR_HOOK?")
+    # variant("warnings", default=True, description="Enable compiler warnings")
 
-    depends_on('ecbuild', type=('build'))
-    depends_on('mpi', when='+mpi')
-    depends_on('eckit', when='+fckit')
-    depends_on('fckit', when='+fckit')
+    depends_on("ecbuild", type=("build"))
+    depends_on("mpi", when="+mpi")
+    depends_on("eckit", when="+fckit")
+    depends_on("fckit", when="+fckit")
+
+    patch("intel_warnings.patch")
 
     def cmake_args(self):
         args = [
-            self.define_from_variant('ENABLE_OMP', 'openmp'),
-            self.define_from_variant('ENABLE_MPI', 'mpi'),
-            self.define_from_variant('ENABLE_FCKIT', 'fckit')
-            # self.define_from_variant('ENABLE_DR_HOOK_MULTI_PRECISION_HANDLES',
-            #   'dr_hook_multi_precision_handles')
-            # self.define_from_variant('ENABLE_WARNINGS, 'warnings')
+            self.define_from_variant("ENABLE_OMP", "openmp"),
+            self.define_from_variant("ENABLE_MPI", "mpi"),
+            self.define_from_variant("ENABLE_FCKIT", "fckit")
+            # self.define_from_variant("ENABLE_DR_HOOK_MULTI_PRECISION_HANDLES",
+            #   "dr_hook_multi_precision_handles")
+            # self.define_from_variant("ENABLE_WARNINGS, "warnings")
         ]
 
         return args
