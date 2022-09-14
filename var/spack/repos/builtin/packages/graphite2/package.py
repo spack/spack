cb323b1f55894d31f376feb34ddad8c96194d2aa
Adam J. Stewart <ajstewart426@gmail.com>
2022-09-13 22:16:59 -0500

    graphite2: fix build on macOS (#32636)

--- var/spack/repos/builtin/packages/graphite2/package.py
+++ var/spack/repos/builtin/packages/graphite2/package.py
@@ -17,8 +17,15 @@ class Graphite2(CMakePackage):
     )
     url = "https://github.com/silnrsi/graphite/releases/download/1.3.13/graphite2-1.3.13.tgz"
 
+    version("1.3.14", sha256="f99d1c13aa5fa296898a181dff9b82fb25f6cc0933dbaa7a475d8109bd54209d")
     version("1.3.13", sha256="dd63e169b0d3cf954b397c122551ab9343e0696fb2045e1b326db0202d875f06")
 
     depends_on("python@3.6:", type="test")
 
     patch("regparm.patch")
+
+    @run_after("install")
+    def darwin_fix(self):
+        # The shared library is not installed correctly on Darwin; fix this
+        if self.spec.satisfies("platform=darwin"):
+            fix_darwin_install_name(self.prefix.lib)
