4203cf68897e153c45278ab870538625e1eedf72
Jack S. Hale <mail@jackhale.co.uk>
2022-09-15 16:17:29 +0200

    Updates for DOLFINx 0.5.1 and associated packages

--- var/spack/repos/builtin/packages/fenics-ufcx/package.py
+++ var/spack/repos/builtin/packages/fenics-ufcx/package.py
@@ -15,9 +15,12 @@ class FenicsUfcx(CMakePackage):
     homepage = "https://github.com/FEniCS/ffcx"
     git = "https://github.com/FEniCS/ffcx.git"
     url = "https://github.com/FEniCS/ffcx/archive/v0.4.2.tar.gz"
-    maintainers = ["ma595"]
+    maintainers = ["ma595", "jhale"]
 
     version("main", branch="main")
+    version("0.5.0.post0", sha256="039908c9998b51ba53e5deb3a97016062c262f0a4285218644304f7d3cd35882")
     version("0.4.2", sha256="3be6eef064d6ef907245db5b6cc15d4e603762e68b76e53e099935ca91ef1ee4")
+
+    depends_on("cmake@3.19:", type="build")
 
     root_cmakelists_dir = "cmake"
