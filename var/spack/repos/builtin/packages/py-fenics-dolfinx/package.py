44c7d75a3285040c654cc98d18aa6e7399897460
Jack S. Hale <mail@jackhale.co.uk>
2022-09-19 15:50:21 +0200

    Fix basix dep

--- var/spack/repos/builtin/packages/py-fenics-dolfinx/package.py
+++ var/spack/repos/builtin/packages/py-fenics-dolfinx/package.py
@@ -50,7 +50,7 @@ class PyFenicsDolfinx(PythonPackage):
     depends_on("fenics-dolfinx@0.1.0", when="@0.1.0")
 
     depends_on("fenics-basix@main", type=("build", "link"), when="@main")
-    depends_on("fenics-basix@0.5.1:0.5", type=("build", "link"), when="@0.5.1:0.5")
+    depends_on("fenics-basix@0.5.1:0.5", type=("build", "link"), when="@0.5.0:0.5")
     depends_on("fenics-basix@0.4.2", type=("build", "link"), when="@0.4.1")
     depends_on("fenics-basix@0.3.0", type=("build", "link"), when="@0.3.0")
     depends_on("fenics-basix@0.2.0", type=("build", "link"), when="@0.2.0")
