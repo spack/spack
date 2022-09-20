2e4b26596b6bba5a6febc29cc609ab4c7bca4754
Paulo Baraldi Mausbach <pbmausbach@hotmail.com>
2022-09-19 23:58:42 -0300

    Fix style

--- var/spack/repos/builtin/packages/py-river/package.py
+++ var/spack/repos/builtin/packages/py-river/package.py
@@ -8,15 +8,15 @@ from spack.package import *
 
 class PyRiver(PythonPackage):
     """River is a Python library for online machine learning. It aims to be the most
-	user-friendly library for doing machine learning on streaming data. River is
-	the result of a merge between creme and scikit-multiflow."""
+        user-friendly library for doing machine learning on streaming data. River is
+        the result of a merge between creme and scikit-multiflow."""
 
     homepage = "https://riverml.xyz/0.13.0/"
-    pypi     = "river/river-0.13.0.tar.gz"
+    pypi = "river/river-0.13.0.tar.gz"
 
     version('0.13.0', sha256='9d068b7a9db32302fbd581af81315681dfe61774a5d777fb3d5982d3c3061340')
 
-    depends_on('python@3.8:',type='build')
+    depends_on('python@3.8:', type='build')
     depends_on('py-numpy@1.18.5:')
     depends_on('py-pandas@1.3:')
     depends_on('py-python-dateutil@2.8.1:')
