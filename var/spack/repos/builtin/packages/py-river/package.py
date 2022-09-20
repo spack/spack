eb060c6c241866db992bd8281c483f97bcd7f6af
Paulo Baraldi Mausbach <pbmausbach@hotmail.com>
2022-09-20 00:25:23 -0300

    Fix style

--- var/spack/repos/builtin/packages/py-river/package.py
+++ var/spack/repos/builtin/packages/py-river/package.py
@@ -14,12 +14,12 @@ class PyRiver(PythonPackage):
     homepage = "https://riverml.xyz/0.13.0/"
     pypi = "river/river-0.13.0.tar.gz"
 
-    version('0.13.0', sha256='9d068b7a9db32302fbd581af81315681dfe61774a5d777fb3d5982d3c3061340')
+    version("0.13.0", sha256="9d068b7a9db32302fbd581af81315681dfe61774a5d777fb3d5982d3c3061340")
 
-    depends_on('python@3.8:', type='build')
-    depends_on('py-numpy@1.18.5:')
-    depends_on('py-pandas@1.3:')
-    depends_on('py-python-dateutil@2.8.1:')
-    depends_on('py-six@1.5:')
-    depends_on('py-pytz@2020.1:')
-    depends_on('py-scipy@1.5:')
+    depends_on("python@3.8:", type="build")
+    depends_on("py-numpy@1.18.5:")
+    depends_on("py-pandas@1.3:")
+    depends_on("py-python-dateutil@2.8.1:")
+    depends_on("py-six@1.5:")
+    depends_on("py-pytz@2020.1:")
+    depends_on("py-scipy@1.5:")
