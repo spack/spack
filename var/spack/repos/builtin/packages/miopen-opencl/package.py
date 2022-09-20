a47373bba29248f3017f2f97025cc157d5204e44
sreenivasa murthy kolam <sreenivasamurthy.kolam@amd.com>
2022-09-20 23:34:04 +0000

    bump up the version for rocm-5.2.3 release and fix style error

--- var/spack/repos/builtin/packages/miopen-opencl/package.py
+++ var/spack/repos/builtin/packages/miopen-opencl/package.py
@@ -145,7 +145,7 @@ class MiopenOpencl(CMakePackage):
     ]:
         depends_on("miopengemm@" + ver, when="@" + ver)
 
-    for ver in ["5.1.0", "5.1.3", "5.2.0", "5.2.1","5.2.3"]:
+    for ver in ["5.1.0", "5.1.3", "5.2.0", "5.2.1", "5.2.3"]:
         depends_on("mlirmiopen@" + ver, when="@" + ver)
 
     @classmethod
