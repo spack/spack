a959b8005bae94d1c34b258a42ebeb25090bd0df
Matthieu Dorier <matthieu.dorier@gmail.com>
2022-09-22 21:42:47 +0200

    [py-tinydb] update dependency
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-tinydb/package.py
+++ var/spack/repos/builtin/packages/py-tinydb/package.py
@@ -15,6 +15,6 @@ class PyTinydb(PythonPackage):
 
     version("4.7.0", sha256="357eb7383dee6915f17b00596ec6dd2a890f3117bf52be28a4c516aeee581100")
 
-    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("python@3.6:3", type=("build", "run"))
     depends_on("py-poetry-core", type="build")
     depends_on("py-typing-extensions@3.10.0:4", type=("build", "run"), when="^python@:3.7")
