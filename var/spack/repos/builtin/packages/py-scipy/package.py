55051d09257eb24da480067806843b96485e376d
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-09-22 13:59:43 -0600

    isort var/spack/repos/builtin/packages/py-scipy/package.py

--- var/spack/repos/builtin/packages/py-scipy/package.py
+++ var/spack/repos/builtin/packages/py-scipy/package.py
@@ -2,10 +2,9 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-from spack.package import *
-
-
 import platform
+
+from spack.package import *
 
 
 class PyScipy(PythonPackage):
