3e786e418b23c7f892b02ae45164184d3490e55d
Valentin Volkl <valentin.volkl@cern.ch>
2022-09-23 17:30:21 +0200

    style

--- var/spack/repos/builtin/packages/py-colorcet/package.py
+++ var/spack/repos/builtin/packages/py-colorcet/package.py
@@ -12,13 +12,13 @@ class PyColorcet(PythonPackage):
     Python plotting programs like Bokeh, Matplotlib, HoloViews, and Datashader."""
 
     homepage = "https://colorcet.holoviz.org/index.html"
-    pypi     = "colorcet/colorcet-3.0.0.tar.gz"
+    pypi = "colorcet/colorcet-3.0.0.tar.gz"
 
-    maintainers = ['vvolkl']
+    maintainers = ["vvolkl"]
 
-    version('3.0.0', sha256='21c522346a7aa81a603729f2996c22ac3f7822f4c8c303c59761e27d2dfcf3db')
+    version("3.0.0", sha256="21c522346a7aa81a603729f2996c22ac3f7822f4c8c303c59761e27d2dfcf3db")
 
-    depends_on('python@2.7:', type=('build', 'run'))
-    depends_on('py-setuptools@30.3.0:', type='build')
-    depends_on('py-param@1.7.0:', type=('build', 'run'))
-    depends_on('py-pyct@0.4.4:', type=('build', 'run'))
+    depends_on("python@2.7:", type=("build", "run"))
+    depends_on("py-setuptools@30.3.0:", type="build")
+    depends_on("py-param@1.7.0:", type=("build", "run"))
+    depends_on("py-pyct@0.4.4:", type=("build", "run"))
--- var/spack/repos/builtin/packages/py-pyct/package.py
+++ var/spack/repos/builtin/packages/py-pyct/package.py
@@ -10,12 +10,12 @@ from spack.package import *
 class PyPyct(PythonPackage):
     """Python package common tasks for users (e.g. copy examples, fetch data, ...)"""
 
-    pypi     = "pyct/pyct-0.4.8.tar.gz"
+    pypi = "pyct/pyct-0.4.8.tar.gz"
 
-    maintainers = ['vvolkl']
+    maintainers = ["vvolkl"]
 
-    version('0.4.8', sha256='23d7525b5a1567535c093aea4b9c33809415aa5f018dd77f6eb738b1226df6f7')
+    version("0.4.8", sha256="23d7525b5a1567535c093aea4b9c33809415aa5f018dd77f6eb738b1226df6f7")
 
-    depends_on('python@2.7:', type=('build', 'run'))
-    depends_on('py-setuptools@30.3.0:', type='build')
-    depends_on('py-param@1.7.0:', type=('build', 'run'))
+    depends_on("python@2.7:", type=("build", "run"))
+    depends_on("py-setuptools@30.3.0:", type="build")
+    depends_on("py-param@1.7.0:", type=("build", "run"))
