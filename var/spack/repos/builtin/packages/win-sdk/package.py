d843033e46bc6b93a0003947f197aede8d3e20e5
John Parent <john.parent@kitware.com>
2022-09-23 14:06:55 -0400

    Add package copyright statements

--- var/spack/repos/builtin/packages/win-sdk/package.py
+++ var/spack/repos/builtin/packages/win-sdk/package.py
@@ -1,3 +1,9 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
 import os
 import re
 
