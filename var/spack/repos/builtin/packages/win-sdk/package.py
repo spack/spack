6b5f1660af495d56fe3a181afffb998573df6ea0
John Parent <john.parent@kitware.com>
2022-09-23 10:09:55 -0400

    add driver packages

---
+++ var/spack/repos/builtin/packages/win-sdk/package.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+from spack.package import *
+
+
+class WinSdk(Package):
+    """
+    Windows Desktop C++ development SDK
+    Spack packaged used to define search heuristics
+    to locate the SDK on a filesystem
+    """
+
+    homepage = "https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/"
+    has_code = False
+
+    version("10.0.19041.0")
+    version("10.0.18362.0")
+    version("10.0.17763.0")
+    version("10.0.17134.0")
+    version("10.0.16299.0")
+    version("10.0.15063.0")
+    version("10.0.14393.0")
+    version("10.0.10586.0")
+    version("10.0.26639.0")
+
+    @classmethod
+    def determine_version(cls, exe):
+        """
+        WinSDK that we would like to
+        be discoverable externally by Spack.
+        """
+        version_match_pat = re.compile(r"[0-9][0-9].[0-9]+.[0-9][0-9][0-9][0-9][0-9]")
+        ver_str = re.search(version_match_pat, exe)
+        return ver_str if not ver_str else Version(ver_str.group())
+
+    def install(self, spec, prefix):
+        raise RuntimeError(
+            "This package is not installable from Spack\
+            and should be installed on the system prior to Spack use.\
+                If not installed this package should be installed via\
+                    the Visual Studio installer in order to use the \
+                        MSVC compiler on Windows."
+        )
