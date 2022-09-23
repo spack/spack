26365980fcdf6feb3740d4a369d5b6488c9f3ea2
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-09-23 20:48:34 +0200

    Fix sha256 for version 0.18

--- var/spack/repos/builtin/packages/py-versioneer/package.py
+++ var/spack/repos/builtin/packages/py-versioneer/package.py
@@ -17,7 +17,7 @@ class PyVersioneer(PythonPackage):
     maintainers = ["scemama"]
 
     version("0.26", sha256="84fc729aa296d1d26645a8f62f178019885ff6f9a1073b29a4a228270ac5257b")
-    version("0.18", sha256="cf895b67f5bc62d61c4837458069ded8f66b4e5764c19f7253c51ee27e8b3a99")
+    version("0.18", sha256="ead1f78168150011189521b479d3a0dd2f55c94f5b07747b484fd693c3fbf335")
 
     depends_on("python@3.6:", when="@0.19:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
