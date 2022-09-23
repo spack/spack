0a050785e98b07d4b7b4c791a3420a50a23387ca
Peter Scheibel <scheibel1@llnl.gov>
2022-09-23 00:29:48 -0700

    Skip all tests using problematic fixture on python 2.7 (#32768)

--- lib/spack/spack/test/concretize.py
+++ lib/spack/spack/test/concretize.py
@@ -1188,6 +1188,9 @@ class TestConcretize(object):
             second_spec.concretize()
         assert first_spec.dag_hash() != second_spec.dag_hash()
 
+    @pytest.mark.skipif(
+        sys.version_info[:2] == (2, 7), reason="Fixture fails intermittently with Python 2.7"
+    )
     @pytest.mark.regression("20292")
     @pytest.mark.parametrize(
         "context",
@@ -1510,6 +1513,9 @@ class TestConcretize(object):
             s = Spec("python target=k10").concretized()
         assert s.satisfies("target=k10")
 
+    @pytest.mark.skipif(
+        sys.version_info[:2] == (2, 7), reason="Fixture fails intermittently with Python 2.7"
+    )
     @pytest.mark.regression("29201")
     def test_delete_version_and_reuse(self, mutable_database, repo_with_changing_recipe):
         """Test that we can reuse installed specs with versions not
