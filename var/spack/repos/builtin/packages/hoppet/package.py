ff586864b2acaf939de79436251abaf9485c9f05
Valentin Volkl <valentin.volkl@cern.ch>
2022-09-26 17:09:31 +0200

    hoppet: fix typo in hep tag

--- var/spack/repos/builtin/packages/hoppet/package.py
+++ var/spack/repos/builtin/packages/hoppet/package.py
@@ -13,7 +13,7 @@ class Hoppet(AutotoolsPackage):
     homepage = "https://hoppet.hepforge.org/"
     url = "https://github.com/gavinsalam/hoppet/archive/refs/tags/hoppet-1.2.0.tar.gz"
 
-    tags = ["heps"]
+    tags = ["hep"]
     maintainers = ["haralmha"]
 
     version("1.2.0", sha256="6e00eb56a4f922d03dfceba7b389a3aaf51f277afa46d7b634d661e0797e8898")
