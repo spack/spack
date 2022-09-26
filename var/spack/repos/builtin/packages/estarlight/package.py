b44c85c9527dc7b4adde912fedcba692f4a4d296
Wouter Deconinck <wdconinc@gmail.com>
2022-09-26 17:56:50 -0500

    estarlight: properly treat variants in cmake_args

--- var/spack/repos/builtin/packages/estarlight/package.py
+++ var/spack/repos/builtin/packages/estarlight/package.py
@@ -21,19 +21,21 @@ class Estarlight(CMakePackage):
     version('master', branch='master')
     version('1.0.0', sha256='a963bd4e08218767030ddadb7ad72d8d93f2468dfdce309abf566e87326c81f4')
 
-    variant('hepmc3', default=True, description='Support HepMC3 writing')
-    variant('doxygen', default=False, description='Build documentation')
-    variant('pythia8', default=False, description='Use Pythia8 for parton showers')
-    variant('pythia6', default=False, description='Use Pythia6 for parton showers')
     variant('dpmjet', default=False, description='Use dpmjet for jets')
+    variant('hepmc3', default=False, description='Support HepMC3 writing')
+    variant('pythia6', default=False, description='Use Pythia6 for parton showers')
+    variant('pythia8', default=False, description='Use Pythia8 for parton showers')
 
-    depends_on('hepmc3', when='+hepmc3')
-    depends_on('doxygen', when='+doxygen')
-    depends_on('pythia8', when='+pythia8')
-    depends_on('pythia6', when='+pythia6')
     depends_on('dpmjet', when='+dpmjet')
+    depends_on('hepmc3', when='+hepmc3')
+    depends_on('pythia6', when='+pythia6')
+    depends_on('pythia8', when='+pythia8')
 
     def cmake_args(self):
-        args = []
+        args = [
+            self.define_from_variant("ENABLE_DPMJET", "dpmjet"),
+            self.define_from_variant("ENABLE_HEPMC3", "hepmc3"),
+            self.define_from_variant("ENABLE_PYTHIA6", "pythia6"),
+            self.define_from_variant("ENABLE_PYTHIA", "pythia8"),
+        ]
         return args
-
