5fbbc5fa6862372c95de95055fa83b96be70e26d
akhursev <aleksei.khursevich@intel.com>
2022-09-26 09:01:59 -0700

    2022.3 oneAPI release promotion (#32786)
    In addition to the new release, made intel-oneapi-compilers-classic version number match the compiler version number, instead of the version of the package that contains it.
    
    Co-authored-by: Robert Cohn <robert.s.cohn@intel.com>

--- var/spack/repos/builtin/packages/intel-oneapi-itac/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-itac/package.py
@@ -31,6 +31,12 @@ class IntelOneapiItac(IntelOneApiPackage):
 
     if platform.system() == "Linux":
         version(
+            "2021.7.0",
+            url="https://registrationcenter-download.intel.com/akdlm/irc_nas/18886/l_itac_oneapi_p_2021.7.0.8707_offline.sh",
+            sha256="719faeccfb1478f28110b72b1558187590a6f44cce067158f407ab335a7395bd",
+            expand=False,
+        )
+        version(
             "2021.6.0",
             url="https://registrationcenter-download.intel.com/akdlm/irc_nas/18694/l_itac_oneapi_p_2021.6.0.434_offline.sh",
             sha256="1ecc2735da960041b051e377cadb9f6ab2f44e8aa44d0f642529a56a3cbba436",
