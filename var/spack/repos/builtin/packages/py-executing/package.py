0ca0856f898a5059d6305c852666d33ddad6c1ad
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2022-09-27 17:42:58 +0200

    py-executing: add 1.1.0 (#32840)

--- var/spack/repos/builtin/packages/py-executing/package.py
+++ var/spack/repos/builtin/packages/py-executing/package.py
@@ -12,6 +12,7 @@ class PyExecuting(PythonPackage):
     homepage = "https://github.com/alexmojaki/executing"
     pypi = "executing/executing-0.8.2.tar.gz"
 
+    version("1.1.0", sha256="2c2c07d1ec4b2d8f9676b25170f1d8445c0ee2eb78901afb075a4b8d83608c6a")
     version("0.8.2", sha256="c23bf42e9a7b9b212f185b1b2c3c91feb895963378887bb10e64a2e612ec0023")
 
     depends_on("py-setuptools", type="build")
