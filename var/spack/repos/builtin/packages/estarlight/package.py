7253d78b0f40595101807e14fe2d721e56b43fd8
Wouter Deconinck <wdconinc@gmail.com>
2022-09-26 19:39:16 -0500

    estarlight: fix style

--- var/spack/repos/builtin/packages/estarlight/package.py
+++ var/spack/repos/builtin/packages/estarlight/package.py
@@ -12,24 +12,24 @@ class Estarlight(CMakePackage):
     production in electron-ion collisions."""
 
     homepage = "https://github.com/eic/estarlight"
-    url      = "https://github.com/eic/estarlight/archive/refs/tags/v1.0.0.zip"
+    url = "https://github.com/eic/estarlight/archive/refs/tags/v1.0.0.zip"
     list_url = "https://github.com/eic/estarlight/releases"
-    git      = "https://github.com/eic/estarlight.git"
+    git = "https://github.com/eic/estarlight.git"
 
-    maintainers = ['wdconinc']
+    maintainers = ["wdconinc"]
 
-    version('master', branch='master')
-    version('1.0.0', sha256='a963bd4e08218767030ddadb7ad72d8d93f2468dfdce309abf566e87326c81f4')
+    version("master", branch="master")
+    version("1.0.0", sha256="a963bd4e08218767030ddadb7ad72d8d93f2468dfdce309abf566e87326c81f4")
 
-    variant('dpmjet', default=False, description='Use dpmjet for jets')
-    variant('hepmc3', default=False, description='Support HepMC3 writing')
-    variant('pythia6', default=False, description='Use Pythia6 for parton showers')
-    variant('pythia8', default=False, description='Use Pythia8 for parton showers')
+    variant("dpmjet", default=False, description="Use dpmjet for jets")
+    variant("hepmc3", default=False, description="Support HepMC3 writing")
+    variant("pythia6", default=False, description="Use Pythia6 for parton showers")
+    variant("pythia8", default=False, description="Use Pythia8 for parton showers")
 
-    depends_on('dpmjet', when='+dpmjet')
-    depends_on('hepmc3', when='+hepmc3')
-    depends_on('pythia6', when='+pythia6')
-    depends_on('pythia8', when='+pythia8')
+    depends_on("dpmjet", when="+dpmjet")
+    depends_on("hepmc3", when="+hepmc3")
+    depends_on("pythia6", when="+pythia6")
+    depends_on("pythia8", when="+pythia8")
 
     def cmake_args(self):
         args = [
