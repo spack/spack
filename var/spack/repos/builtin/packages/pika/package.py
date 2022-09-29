f1e719ff938803b55e3a115d8df858bdd2bb62bc
Auriane R <aurianer@cscs.ch>
2022-09-29 16:12:06 +0200

    Format with black

--- var/spack/repos/builtin/packages/pika/package.py
+++ var/spack/repos/builtin/packages/pika/package.py
@@ -147,7 +147,7 @@ class Pika(CMakePackage, CudaPackage, ROCmPackage):
             if self.spec.satisfies("^cmake@3.21.0:3.21.2"):
                 args += [self.define("__skip_rocmclang", True)]
         if "@0.8: +rocm" in self.spec:
-            rocm_archs = spec.variants['amdgpu_target'].value
+            rocm_archs = spec.variants["amdgpu_target"].value
             rocm_archs = ";".join(rocm_archs)
             args.append(self.define("CMAKE_HIP_ARCHITECTURES", rocm_archs))
 
