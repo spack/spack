15b63f98ef83ccf4e8dcf8151de3bdba829a901d
John Parent <john.parent@kitware.com>
2022-09-30 16:16:24 -0400

    MSMPI to WDK dependency

--- var/spack/repos/builtin/packages/msmpi/package.py
+++ var/spack/repos/builtin/packages/msmpi/package.py
@@ -22,6 +22,7 @@ class Msmpi(Package):
     version("10.1.1", sha256="63c7da941fc4ffb05a0f97bd54a67968c71f63389a0d162d3182eabba1beab3d")
     version("10.0.0", sha256="cfb53cf53c3cf0d4935ab58be13f013a0f7ccb1189109a5b8eea0fcfdcaef8c1")
 
+    depends_on('wdk')
 
     def is_64bit(self):
         return platform.machine().endswith('64')
