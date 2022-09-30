9849f7f84845959522f0eab8a3e94b3d07625fbd
John Parent <john.parent@kitware.com>
2022-09-30 16:19:55 -0400

    Flesh out Windows MSMPI

--- var/spack/repos/builtin/packages/msmpi/package.py
+++ var/spack/repos/builtin/packages/msmpi/package.py
@@ -12,8 +12,8 @@ from spack import *
 
 
 class Msmpi(Package):
-    """MPICH is a high performance and widely portable implementation of
-    the Message Passing Interface (MPI) standard."""
+    """MSMPI is a Windows port of MPICH provided by the Windows team
+    """
 
     homepage = "https://docs.microsoft.com/en-us/message-passing-interface/microsoft-mpi"
     url      = "https://github.com/microsoft/Microsoft-MPI/archive/refs/tags/v10.1.1.tar.gz"
@@ -21,6 +21,9 @@ class Msmpi(Package):
 
     version("10.1.1", sha256="63c7da941fc4ffb05a0f97bd54a67968c71f63389a0d162d3182eabba1beab3d")
     version("10.0.0", sha256="cfb53cf53c3cf0d4935ab58be13f013a0f7ccb1189109a5b8eea0fcfdcaef8c1")
+
+
+    provides('mpi')
 
     depends_on('wdk')
 
@@ -35,7 +38,7 @@ class Msmpi(Package):
         ver = '' if not ver else ver.group()
         args.append('/p:GFORTRAN_BIN=%sbin\%s' % (os.environ['IFORT_COMPILER21'], arch))
         args.append('/p:VCToolsVersion=%s' % ver)
-        args.append('/p:WindowsTargetPlatformVersion=%s' % '')
+        args.append('/p:WindowsTargetPlatformVersion=%s' % str(self.spec['wdk'].version))
 
     def install(self):
         with working_dir(self.stage.build_directory, create=True):
