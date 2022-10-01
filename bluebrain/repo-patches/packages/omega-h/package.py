f241a88e424d69a25191e5ac95f1cda5a5a1e58e
Tristan Carel <tristan.carel@gmail.com>
2022-10-01 16:18:00 +0200

    omega-h: add 9.34.13 (#1713)

--- bluebrain/repo-patches/packages/omega-h/package.py
+++ bluebrain/repo-patches/packages/omega-h/package.py
@@ -20,9 +20,7 @@ class OmegaH(CMakePackage):
     tags = ['e4s']
     version('develop', branch='master')
     version('main', branch='main')
-    version('9.34.8', sha256='3a6717366a6235f068a65158495396215f3a9175bbf69684f1726a2eaf2d1aa1')
-    version('9.34.6', sha256='0fcdfedab6afb855ca982c429698eaa2c25e78909152b8bee508c80a54234aac')
-    version('9.34.5', sha256='1fa67122d2b6d2b3d0d05fa0c5ed1fa24234d072292b29cb334879ffb5adcc92')
+    version('9.34.13', sha256='2eadfd6d634abc0b50396a82fd446f8f0b586ba6e64788c47827162c2aadec02')
     version('9.33.2', sha256='02ddea3aca36170edb1a63c6e2af419004727d2346d759ab224c70bbfa3455da')
     version('9.33.0.dev1', sha256='984831746222317b9701a58042cb3da1754845a50cb17d865e3e8210beb5dd6b')
     version('9.32.5.dev3', sha256='83381f728688ad9d786f02946fe101bf577c7c3268a40e46264935e7cd1a5d97')
@@ -32,7 +30,6 @@ class OmegaH(CMakePackage):
     version('9.29.2', sha256='8eea6da0ebde44176a6d19fb858f89f872611cbad08cad65700757e096058465')
     version('9.28.0', sha256='681588974116025c6c1c535972386c23fe354502319fcbbbdc5b9d373429c08e')
     version('9.27.3', sha256='7c0b0c9c00cda97763be1287d3bf8b931bcdd07bf4971fc7258a3892d628cf20')
-    version('9.26.6', sha256='5022bf3f9be1a27668fd2ec396d66f6b0124dc4a3adeedb20a580a701d37d893')
 
     variant('shared', default=True, description='Build shared libraries')
     variant('mpi', default=True, description='Activates MPI support')
