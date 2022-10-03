305ae39d104056702dbbc1885672dca89959e4d6
renjithravindrankannath <94420380+renjithravindrankannath@users.noreply.github.com>
2022-10-03 04:13:52 -0700

    rvs binary path updated for 5.2 rocm release (#32892)

--- var/spack/repos/builtin/packages/rocm-validation-suite/package.py
+++ var/spack/repos/builtin/packages/rocm-validation-suite/package.py
@@ -146,9 +146,13 @@ class RocmValidationSuite(CMakePackage):
         depends_on("hip-rocclr@" + ver, when="@" + ver)
 
     def patch(self):
-        if "@4.5.0:" in self.spec:
+        if "@4.5.0:5.1" in self.spec:
             filter_file(
                 "@ROCM_PATH@/rvs", self.spec.prefix.rvs, "rvs/conf/deviceid.sh.in", string=True
+            )
+        elif "@5.2.0:" in self.spec:
+            filter_file(
+                "@ROCM_PATH@/bin", self.spec.prefix.bin, "rvs/conf/deviceid.sh.in", string=True
             )
 
     def cmake_args(self):
