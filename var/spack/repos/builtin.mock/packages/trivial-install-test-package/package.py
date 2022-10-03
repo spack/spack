8be6cad148062c5909fd40f842a8b64cbe263f7a
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2022-10-03 13:59:45 -0400

    black fmt

--- var/spack/repos/builtin.mock/packages/trivial-install-test-package/package.py
+++ var/spack/repos/builtin.mock/packages/trivial-install-test-package/package.py
@@ -12,14 +12,14 @@ class TrivialInstallTestPackage(Package):
     homepage = "http://www.example.com/trivial_install"
     url = "http://www.unit-test-should-replace-this-url/trivial_install-1.0.tar.gz"
 
-    variant('mkdir-share-dir', default=False)
+    variant("mkdir-share-dir", default=False)
 
     version("1.0", "0123456789abcdef0123456789abcdef")
 
     def install(self, spec, prefix):
         touch(join_path(prefix, "an_installation_file"))
 
-        if '+mkdir-share-dir' in spec:
+        if "+mkdir-share-dir" in spec:
             share = join_path(prefix, "share")
             # mkdir() will fail if <prefix>/share/ already exists, which it will if the package
             # sources were installed (in <prefix>/share/pkg/src) before this install() is run.
