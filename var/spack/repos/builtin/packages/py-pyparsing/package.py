d785f9b13d0e3afe8c4c8d8a2c5c7338c6a2f2e1
iarspider <iarspider@gmail.com>
2022-10-03 22:57:56 +0200

    Add checksum for py-pyparsing 3.0.9 (#32952)

--- var/spack/repos/builtin/packages/py-pyparsing/package.py
+++ var/spack/repos/builtin/packages/py-pyparsing/package.py
@@ -12,6 +12,7 @@ class PyPyparsing(PythonPackage):
     homepage = "https://pyparsing-docs.readthedocs.io/en/latest/"
     pypi = "pyparsing/pyparsing-2.4.2.tar.gz"
 
+    version("3.0.9", sha256="2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb")
     version("3.0.6", sha256="d9bdec0013ef1eb5a84ab39a3b3868911598afa494f5faa038647101504e2b81")
     version("2.4.7", sha256="c203ec8783bf771a155b207279b9bccb8dea02d8f0c9e5f8ead507bc3246ecc1")
     version("2.4.2", sha256="6f98a7b9397e206d78cc01df10131398f1c8b8510a2f4d97d9abd82e1aacdd80")
@@ -21,8 +22,10 @@ class PyPyparsing(PythonPackage):
     version("2.1.10", sha256="811c3e7b0031021137fc83e051795025fcb98674d07eb8fe922ba4de53d39188")
     version("2.0.3", sha256="06e729e1cbf5274703b1f47b6135ed8335999d547f9d8cf048b210fb8ebf844f")
 
+    depends_on("python@3.6.8:", when="@3.0.9:", type=("build", "run"))
     depends_on("python@3.6:", when="@3:", type=("build", "run"))
     depends_on("python@2.6:2.8,3.3:", type=("build", "run"))
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools", when="@:3.0.8", type="build")
+    depends_on("py-flit-core@3.2:3", when="@3.0.9:", type="build")
 
     import_modules = ["pyparsing"]
