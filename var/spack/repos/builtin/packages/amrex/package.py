4ff17ce0eefdaa950c528e64f383f657dfec13aa
Weiqun Zhang <WeiqunZhang@lbl.gov>
2022-10-03 21:01:49 -0700

    amrex: add v22.10 (#32966)

--- var/spack/repos/builtin/packages/amrex/package.py
+++ var/spack/repos/builtin/packages/amrex/package.py
@@ -14,7 +14,7 @@ class Amrex(CMakePackage, CudaPackage, ROCmPackage):
     mesh refinement (AMR) applications."""
 
     homepage = "https://amrex-codes.github.io/amrex/"
-    url = "https://github.com/AMReX-Codes/amrex/releases/download/22.09/amrex-22.09.tar.gz"
+    url = "https://github.com/AMReX-Codes/amrex/releases/download/22.10/amrex-22.10.tar.gz"
     git = "https://github.com/AMReX-Codes/amrex.git"
 
     test_requires_compiler = True
@@ -24,6 +24,7 @@ class Amrex(CMakePackage, CudaPackage, ROCmPackage):
     maintainers = ["WeiqunZhang", "asalmgren", "etpalmer63"]
 
     version("develop", branch="development")
+    version("22.10", sha256="458da410d7f43e428726bfc905123e85d05786080f892ebaa26f94c5f8e79b07")
     version("22.09", sha256="24601fbb9d554f7b66d7db89b14ff95dadb18d51db893af7ee6c70d4b7dd4be6")
     version("22.08", sha256="d89167c4567fa246b06478a5b160010a0117dc58be9e879beb15be53cb08b6e9")
     version("22.07", sha256="7df433c780ab8429362df8d6d995c95d87a7c3f31ab81d5b0f416203dece086d")
