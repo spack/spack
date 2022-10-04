16fd34e913f9e16f539981ffbef61767a80c7c3c
Sam Grayson <sam@samgrayson.me>
2022-10-04 14:42:30 -0600

    Fix comments from code review

--- var/spack/repos/builtin/packages/py-cmyt/package.py
+++ var/spack/repos/builtin/packages/py-cmyt/package.py
@@ -23,9 +23,10 @@ class PyCmyt(PythonPackage):
     version("1.0.1", "c20ec811c0cd71ffb3482e0d269bd22e0f088a3187dd615c59edf348cbefded1")
     version("1.0.0", "64751ecd5916efe817beb92d0b5b34b0dc134c8ee0dab3f212cb089b6b78bbd4")
 
-    depends_on("py-colorspacious@1.1.2:")
-    depends_on("py-matplotlib@2.1.0:")
-    depends_on("py-more-itertools@8.4.0:")
-    depends_on("py-numpy@1.13.3:")
-    depends_on("py-typing-extensions@3.10.0.2:", when="^python@:3.8")
+    depends_on('py-setuptools', type='build')
+    depends_on("py-colorspacious@1.1.2:", type=('build', 'run'))
+    depends_on("py-matplotlib@2.1.0:", type=('build', 'run'))
+    depends_on("py-more-itertools@8.4.0:", type=('build', 'run'))
+    depends_on("py-numpy@1.13.3:", type=('build', 'run'))
+    depends_on("py-typing-extensions@3.10.0.2:", when="^python@:3.7", type=('build', 'run'))
     # python_version < "3.8"
--- var/spack/repos/builtin/packages/py-unyt/package.py
+++ var/spack/repos/builtin/packages/py-unyt/package.py
@@ -21,5 +21,7 @@ class PyUnyt(PythonPackage):
     version("2.8.0", sha256="4fc95704cba4527604f21724d0d9845aea07831a6a2a3d1be9b9d18395e0c345")
     version("2.7.2", sha256="9ad36f890549d18ecb9926019e96c4d27d380e909895a89ac7f1fb168a451edd")
 
+    depends_on('py-setuptools', type='build')
+    depends_on('python@3.5:', type=('build', 'run'))
     depends_on("py-numpy@1.13.0:", type=('build', 'run'))
     depends_on("py-sympy@1.2:", type=('build', 'run'))
