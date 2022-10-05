38c8ef800e5df24c820d55a54b3a29c097c6caf1
Ivan Razumov <iarspider@gmail.com>
2022-10-05 16:56:29 +0200

    Add checksum for py-hepdata-lib 0.10.1, py-hepdata-validator 0.3.3

--- var/spack/repos/builtin/packages/py-hepdata-lib/package.py
+++ var/spack/repos/builtin/packages/py-hepdata-lib/package.py
@@ -12,6 +12,7 @@ class PyHepdataLib(PythonPackage):
     homepage = "https://github.com/HEPData/hepdata_lib"
     pypi = "hepdata_lib/hepdata_lib-0.9.0.tar.gz"
 
+    version("0.10.1", sha256="f6c3b85975792d5fde14212a4f8212c507f9e961ba77271487a99518e89c8dd2")
     version("0.9.0", sha256="b7b194b8af0428f34094ac403f8794a672c82d85e33154161d6b3744cc2b9896")
 
     depends_on("py-setuptools", type="build")
@@ -22,3 +23,4 @@ class PyHepdataLib(PythonPackage):
     depends_on("py-six", type=("build", "run"))
     depends_on("py-pytest-runner", type="build")
     depends_on("py-pytest-cov", type="build")
+    depends_on("py-hepdata-validator@0.3.2:", when="@0.10.1:", type=("build", "run"))
