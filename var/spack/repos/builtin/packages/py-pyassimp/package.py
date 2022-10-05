b246e497f50ff9e3f5bf9bed97c5e9526ba3baf9
qwertos <qwertos@users.noreply.github.com>
2022-10-05 20:39:46 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-pyassimp/package.py
+++ var/spack/repos/builtin/packages/py-pyassimp/package.py
@@ -3,17 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PyPyassimp(PythonPackage):
     """Python bindings for the Open Asset Import Library (ASSIMP)"""
 
     homepage = "https://github.com/mikedh/pyassimp"
-    pypi     = "pyassimp/pyassimp-4.1.4.tar.gz"
+    pypi = "pyassimp/pyassimp-4.1.4.tar.gz"
 
-    version('4.1.4', sha256='266bd4be170d46065b8c2ad0f5396dad10938a6bbf9a566c4e4d56456e33aa6a')
+    version("4.1.4", sha256="266bd4be170d46065b8c2ad0f5396dad10938a6bbf9a566c4e4d56456e33aa6a")
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-numpy', type=('build', 'run'))
-    depends_on('assimp')
+    depends_on("py-setuptools", type="build")
+    depends_on("py-numpy", type=("build", "run"))
+    depends_on("assimp")
