46e0101bcec8c588dab83c86d2c65ff1e1a6cedf
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-10-05 16:29:57 +0200

    Remove "repository=" argument from instance method

--- lib/spack/spack/tag.py
+++ lib/spack/spack/tag.py
@@ -113,15 +113,13 @@ class TagIndex(Mapping):
             spkgs, opkgs = self.tags[tag], other.tags[tag]
             self.tags[tag] = sorted(list(set(spkgs + opkgs)))
 
-    def update_package(self, pkg_name, repository=None):
+    def update_package(self, pkg_name):
         """Updates a package in the tag index.
 
         Args:
             pkg_name (str): name of the package to be removed from the index
-
         """
-        repository = repository or spack.repo.path
-        pkg_cls = repository.get_pkg_class(pkg_name)
+        pkg_cls = self.repository.get_pkg_class(pkg_name)
 
         # Remove the package from the list of packages, if present
         for pkg_list in self._tag_dict.values():
