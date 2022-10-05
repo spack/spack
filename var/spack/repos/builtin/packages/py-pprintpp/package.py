7ccb1ab25657c194f7878b9b4a250e464120781e
Jen Herting <jen@herting.cc>
2022-10-05 15:57:20 -0400

    [py-pprintpp] added dependency on setuptools

--- var/spack/repos/builtin/packages/py-pprintpp/package.py
+++ var/spack/repos/builtin/packages/py-pprintpp/package.py
@@ -13,3 +13,5 @@ class PyPprintpp(PythonPackage):
     pypi     = "pprintpp/pprintpp-0.4.0.tar.gz"
 
     version('0.4.0', sha256='ea826108e2c7f49dc6d66c752973c3fc9749142a798d6b254e1e301cfdbc6403')
+
+    depends_on('py-setuptools', type='build')
