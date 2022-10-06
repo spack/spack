0731279ac95c9f91cc90c393962e92bb08dca4f9
Jen Herting <jen@herting.cc>
2022-10-06 10:49:07 -0400

    [py-bravado-core] few small fixes

--- var/spack/repos/builtin/packages/py-bravado-core/package.py
+++ var/spack/repos/builtin/packages/py-bravado-core/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PyBravadoCore(PythonPackage):
@@ -17,7 +17,7 @@ class PyBravadoCore(PythonPackage):
 
     version('5.17.1', sha256='0da9c6f3814734622a55db3f62d08db6e188b25f3ebd087de370c91afb66a7f4')
 
-    depends_on('python@:2.7,3.5.1:', type=('build', 'run'))
+    depends_on('python@:2,3.5.1:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
 
     depends_on('py-jsonref', type=('build', 'run'))
@@ -31,4 +31,4 @@ class PyBravadoCore(PythonPackage):
     depends_on('py-pytz', type=('build', 'run'))
     depends_on('py-msgpack@0.5.2:', type=('build', 'run'))
 
-    depends_on('py-pyrsistent@0.17:', when='^python@:3.4.10', type='build')
+    depends_on('py-pyrsistent@0.17:', when='^python@:3.4', type='build')
