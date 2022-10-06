d9f7a7189d2400f079770a794536792df49d16ff
Thomas Madlener <thomas.madlener@desy.de>
2022-10-06 14:11:12 +0200

    podio, edm4hep: Add latest versions

--- var/spack/repos/builtin/packages/podio/package.py
+++ var/spack/repos/builtin/packages/podio/package.py
@@ -20,6 +20,7 @@ class Podio(CMakePackage):
     tags = ["hep", "key4hep"]
 
     version("master", branch="master")
+    version("0.16", sha256="4e149c2c9be9f9ca3a6d863498bb0f642dda1a43a19ac1afe7f99854ded5c510")
     version("0.15", sha256="6c1520877ba1bce250e35a2a56c0a3da89fae0916c5ed7d5548d658237e067d9")
     version("0.14.3", sha256="2a7a405dedc7f6980a0aad7df87b427a1f43bcf6d923a9bcce1698fd296359f7")
     version(
