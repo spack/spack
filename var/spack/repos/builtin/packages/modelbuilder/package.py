3227e9873020fdefe0d9d71fd1b136ba14374cdb
Ryan Krattiger <ryan.krattiger@kitware.com>
2022-10-05 22:36:25 -0500

    Modelbuilder: New package for modelbuilder

---
+++ var/spack/repos/builtin/packages/modelbuilder/package.py
@@ -0,0 +1,72 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install modelbuilder
+#
+# You can edit this file again by typing:
+#
+#     spack edit modelbuilder
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+from spack.package import *
+
+
+class Modelbuilder(CMakePackage):
+    """Modelbuilder is an open-source ParaView Client
+    with Simulation pre-processing workflow tools.
+    """
+
+    homepage = "https://www.computationalmodelbuilder.org"
+    url = "https://gitlab.kitware.com/cmb/cmb"
+    git = "https://gitlab.kitware.com/cmb/cmb.git"
+
+    maintainers = ["kwryankrattiger"]
+
+    version("master", branch="master")
+
+    releases = [
+        "22.04.0",
+        "21.12.0",
+        "21.07.0",
+        "21.05.0",
+    ]
+    for ver in releases:
+        version(ver, branch="v{0}".format(ver), submodules=False)
+
+    variant("doc", default=False, description="Build documentation.")
+    variant("python", default=True, description="Enable built-in Python shell.")
+    variant("remote", default=False, description="Enable remote rendering support.")
+    variant("shared", default=True, description="Build with shared libs.")
+    variant("smtk", default=True, description="Enable Built-in SMTK plugins.")
+
+    depends_on("smtk +paraview +qt", when="+smtk")
+    depends_on("smtk +shared", when="+smtk +shared")
+    depends_on("paraview +qt")
+    depends_on("paraview +shared", when="+shared")
+    depends_on("paraview +python3", when="+python")
+    depends_on("python@3:", when="+python")
+    depends_on("qt@5: +gui")
+    depends_on("py-sphinx", when="+doc")
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
+            self.define_from_variant("cmb_enable_pythonshell", "python"),
+            self.define_from_variant("cmb_enable_multiservers", "remote"),
+            self.define_from_variant("cmb_enable_documentation", "doc"),
+            # self.define_from_variant("cmb_enable_objectpicking", "smtk"),
+        ]
+        return args
