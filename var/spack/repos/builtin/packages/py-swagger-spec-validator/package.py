835ea1a3d715037882a60f87d39d444863ee65eb
Jen Herting <jen@herting.cc>
2022-10-06 10:26:21 -0400

    [py-swagger-spec-validator] a few fixes

--- var/spack/repos/builtin/packages/py-swagger-spec-validator/package.py
+++ var/spack/repos/builtin/packages/py-swagger-spec-validator/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PySwaggerSpecValidator(PythonPackage):
@@ -17,10 +17,8 @@ class PySwaggerSpecValidator(PythonPackage):
 
     version('2.7.6', sha256='73f33e631a58f407265f2f813d194f2762a2b86f9aa905e7eee3df9b7f9428d3')
 
-    # depends_on('python@2.X:2.Y,3.Z:', type=('build', 'run'))
-
     depends_on('py-setuptools', type='build')
     depends_on('py-jsonschema', type=('build', 'run'))
     depends_on('py-pyyaml', type=('build', 'run'))
     depends_on('py-six', type=('build', 'run'))
-    depends_on('py-pyrsistent@:0.16.9', when='^python@:3.0', type='build')
+    depends_on('py-pyrsistent@:0.16', when='^python@:3.0', type='build')
