78664e97b235278e63e42adc02e4be446a4ee18d
qwertos <qwertos@users.noreply.github.com>
2022-10-06 02:46:02 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-pprintpp/package.py
+++ var/spack/repos/builtin/packages/py-pprintpp/package.py
@@ -3,15 +3,15 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PyPprintpp(PythonPackage):
     """A drop-in replacement for pprint that's actually pretty"""
 
     homepage = "https://github.com/wolever/pprintpp"
-    pypi     = "pprintpp/pprintpp-0.4.0.tar.gz"
+    pypi = "pprintpp/pprintpp-0.4.0.tar.gz"
 
-    version('0.4.0', sha256='ea826108e2c7f49dc6d66c752973c3fc9749142a798d6b254e1e301cfdbc6403')
+    version("0.4.0", sha256="ea826108e2c7f49dc6d66c752973c3fc9749142a798d6b254e1e301cfdbc6403")
 
-    depends_on('py-setuptools', type='build')
+    depends_on("py-setuptools", type="build")
