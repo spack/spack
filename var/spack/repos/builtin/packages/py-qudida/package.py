9295e6f8f24988407296e9f068a14abb462adec8
qwertos <qwertos@users.noreply.github.com>
2022-10-08 17:20:33 +0000

    [@spackbot] updating style on behalf of qwertos

--- var/spack/repos/builtin/packages/py-qudida/package.py
+++ var/spack/repos/builtin/packages/py-qudida/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PyQudida(PythonPackage):
@@ -12,13 +12,13 @@ class PyQudida(PythonPackage):
     transformers."""
 
     homepage = "https://github.com/arsenyinfo/qudida"
-    pypi     = "qudida/qudida-0.0.4.tar.gz"
+    pypi = "qudida/qudida-0.0.4.tar.gz"
 
-    version('0.0.4', sha256='db198e2887ab0c9aa0023e565afbff41dfb76b361f85fd5e13f780d75ba18cc8')
+    version("0.0.4", sha256="db198e2887ab0c9aa0023e565afbff41dfb76b361f85fd5e13f780d75ba18cc8")
 
-    depends_on('python@3.5:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
-    depends_on('py-numpy@0.18:', type=('build', 'run'))
-    depends_on('py-scikit-learn@0.19.1:', type=('build', 'run'))
-    depends_on('py-typing-extensions', type=('build', 'run'))
-    depends_on('opencv@4.0.1:+python3', type=('build', 'run'))
+    depends_on("python@3.5:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-numpy@0.18:", type=("build", "run"))
+    depends_on("py-scikit-learn@0.19.1:", type=("build", "run"))
+    depends_on("py-typing-extensions", type=("build", "run"))
+    depends_on("opencv@4.0.1:+python3", type=("build", "run"))
