0d9e64d73c70f5eeb141cf7c642f5e268a3c6ed7
Ivan Razumov <iarspider@gmail.com>
2022-10-10 12:06:58 +0200

    Only set SPACK_REORDER_PATHS if property is set

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -494,7 +494,8 @@ def set_wrapper_variables(pkg, env):
     env.set(SPACK_INCLUDE_DIRS, ":".join(include_dirs))
     env.set(SPACK_RPATH_DIRS, ":".join(rpath_dirs))
 
-    env.set("SPACK_REORDER_PATHS", pkg.reorder_paths)
+    if pkg.reorder_paths:
+        env.set("SPACK_REORDER_PATHS", "TRUE")
 
 
 def determine_number_of_jobs(
