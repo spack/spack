7914300110813bcf67ed25f43237097434c07642
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-10-11 13:47:26 -0500

    Added py-medaka and dependencies

---
+++ var/spack/repos/builtin/packages/py-pyspoa/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class PyPyspoa(PythonPackage):
+    """Python bindings to spoa"""
+
+    homepage = "https://github.com/nanoporetech/pyspoa"
+    pypi = "pyspoa/pyspoa-0.0.8.tar.gz"
+
+    version("0.0.8", sha256="8299d18066b498a6ef294c5a33a99266ded06eeb022f67488d2caecba974b0a4")
+
+    depends_on("py-wheel@0.34.1:", type="build")
+    depends_on("py-setuptools", type="build")
+    depends_on("py-pybind11@2.4:", type=("build", "run"))
+    depends_on("py-auditwheel@3.1.0:", type=("build", "run"))
+    depends_on("py-scikit-build@0.11.1", type=("build", "run"))
