411e159bd99eb4da8c438326853331af992421c1
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2022-10-11 17:00:44 +0200

    bump bluepysnap to 0.13.2 (#1725)

--- bluebrain/repo-bluebrain/packages/py-bluepysnap/package.py
+++ bluebrain/repo-bluebrain/packages/py-bluepysnap/package.py
@@ -10,18 +10,18 @@ class PyBluepysnap(PythonPackage):
     """Pythonic Sonata circuits access API"""
 
     homepage = "https://github.com/BlueBrain/snap"
-    git      = "https://github.com/BlueBrain/snap.git"
+    git = "https://github.com/BlueBrain/snap.git"
     pypi = "bluepysnap/bluepysnap-0.12.0.tar.gz"
 
     version('develop', branch='master')
-    version('0.13.1', sha256='85a7bedb304f862fdc9138e98c84617ec7821f01439fde2989e179d7aba37ff0')
+    version('0.13.2', sha256='67f8eccf5a5038aa4400381028754cff03b7124768c46c4482121f5fad0af0a4')
 
     depends_on('python@3.7:', type=('build', 'run'))
     depends_on('py-setuptools', type=('build', 'run'))
 
     depends_on('py-cached-property@1.0:', type=('build', 'run'))
     depends_on('py-h5py@3.0.1:3.999', type=('build', 'run'))
-    depends_on('py-libsonata@0.1.6:0.999', type=('build', 'run'))
+    depends_on('py-libsonata@0.1.6:0.1.14', type=('build', 'run'))
     depends_on('py-morphio@3.0.0:3.999', type=('build', 'run'))
     depends_on('py-morph-tool@2.4.3:2.999', type=('build', 'run'))
     depends_on('py-numpy@1.8:1.999', type=('build', 'run'))
