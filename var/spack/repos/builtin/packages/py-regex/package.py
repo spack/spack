5d5c0d955895343e8850e3488a41cc0a3604cf79
iarspider <iarspider@gmail.com>
2022-10-12 09:29:36 +0200

    Update var/spack/repos/builtin/packages/py-regex/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-regex/package.py
+++ var/spack/repos/builtin/packages/py-regex/package.py
@@ -27,5 +27,5 @@ class PyRegex(PythonPackage):
         "2017.07.11", sha256="dbda8bdc31a1c85445f1a1b29d04abda46e5c690f8f933a9cc3a85a358969616"
     )
 
-    depends_on("py-setuptools", when="@:2018,2020:", type="build")
+    depends_on("py-setuptools", type="build")
     depends_on("python@3.6:", when="@2022.8.17:", type=("build", "run"))
