d4fcbf64dd50a53ce27a57e1b20295e0deca10da
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-10-12 11:55:05 -0500

    new package

---
+++ var/spack/repos/builtin/packages/pilercr/package.py
@@ -0,0 +1,24 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class Pilercr(Package):
+    """Identification and analysis of CRISPR repeats."""
+
+    homepage = "http://www.drive5.com/pilercr/"
+    url = "http://www.drive5.com/pilercr/pilercr1.06.tar.gz"
+
+    version("1.06", sha256="50175f7aa171674cda5ba255631f340f9cc7f80e8cc25135a4cb857147d91068")
+
+    def edit(self, spec, prefix):
+        # use shared instead of static libs
+        env['LDLIBS'] = "-lm"
+
+    def install(self, spec, prefix):
+        mkdirp(prefix.bin)
+        install("pilercr", prefix.bin)
