80e98b26982abe1b9710fe9bbaea50d15153e7e0
snehring <snehring@iastate.edu>
2022-10-12 17:28:56 -0500

    libbigwig: adding new package libbigwig

---
+++ var/spack/repos/builtin/packages/libbigwig/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Libbigwig(CMakePackage):
+    """A C library for reading/parsing local and remote bigWig and bigBed files."""
+
+    homepage = "https://github.com/dpryan79/libBigWig"
+    url = "https://github.com/dpryan79/libBigWig/archive/refs/tags/0.4.7.tar.gz"
+    maintainers = ["snehring"]
+
+    version("0.4.7", sha256="8e057797011d93fa00e756600898af4fe6ca2d48959236efc9f296abe94916d9")
+
+    depends_on("curl")
+    variant("curl", default=True, description="Build with curl support")
+
+    def cmake_args(self):
+        args = []
+        if self.spec.satisfies("~curl"):
+            args.append("-DWITH_CURL=OFF")
+        return args
