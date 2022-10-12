48e1fb3d4e4cc0d40d4a27cd75020293b7c6879d
iarspider <iarspider@gmail.com>
2022-10-12 09:31:55 +0200

    Update package.py

--- var/spack/repos/builtin/packages/py-jupyter-server-mathjax/package.py
+++ var/spack/repos/builtin/packages/py-jupyter-server-mathjax/package.py
@@ -17,7 +17,6 @@ class PyJupyterServerMathjax(PythonPackage):
 
     depends_on("python@3.6:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-    depends_on("py-wheel", type="build")
     depends_on("py-jupyter-packaging", type="build")
     depends_on("py-jupyter-packaging11@:1", when="@0.2.6:", type="build")
     depends_on("py-jupyter-server@1.1:1", when="@0.2.3", type=("build", "run"))
