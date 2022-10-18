d4a7d7a449c4f4882bbd008f47aeeb101d638024
John Parent <john.parent@kitware.com>
2022-10-18 19:37:42 -0400

    Silence noisy default shell actions

--- share/spack/qa/windows_test_setup.ps1
+++ share/spack/qa/windows_test_setup.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "SilentlyContinue"
 Write-Output F|xcopy .\share\spack\qa\configuration\windows_config.yaml $env:USERPROFILE\.spack\windows\config.yaml
 (Get-Item '.\lib\spack\docs\_spack_root').Delete()
 ./share/spack/setup-env.ps1
