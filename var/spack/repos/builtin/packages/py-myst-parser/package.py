ee8952acc91ab068c05fa2ed8a79f083b4415239
Zach Jibben <zjibben@lanl.gov>
2022-10-20 11:59:19 -0600

    Fix py-mdit-py-plugins and py-myst-parser dependencies

--- var/spack/repos/builtin/packages/py-myst-parser/package.py
+++ var/spack/repos/builtin/packages/py-myst-parser/package.py
@@ -17,13 +17,11 @@ class PyMystParser(PythonPackage):
     version("0.18.1", sha256="79317f4bb2c13053dd6e64f9da1ba1da6cd9c40c8a430c447a7b146a594c246d")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-sphinx@4.0.0:", type=("build", "run"))
-
-    # grabbing depencencies from
-    # https://github.com/executablebooks/MyST-Parser/blob/master/pyproject.toml
-    depends_on("py-docutils@0.15:", type=("build", "run"))
+    depends_on("py-flit-core@3.4:3", type=("build", "run"))
+    depends_on("py-docutils@0.15:0.19", type=("build", "run"))
     #depends_on("py-jinja2", type=("build", "run")) # let sphinx decide version
-    depends_on("py-markdown-it-py@1.0.0:", type=("build", "run"))
-    depends_on("py-mdit-py-plugins@0.3.1:", type=("build", "run"))
-    depends_on("py-pyyaml@5.0:", type=("build", "run"))
-    depends_on("py-typing-extensions@4.0:", type=("build", "run"))
+    depends_on("py-markdown-it-py@1.0.0:2", type=("build", "run"))
+    depends_on("py-mdit-py-plugins@0.3.1", type=("build", "run"))
+    depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-sphinx@4.0.0:5", type=("build", "run"))
+    depends_on("py-typing-extensions", type=("build", "run"))
