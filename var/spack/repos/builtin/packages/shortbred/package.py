2ae4daa37edcf50eeaf4f3b82f00d16d637a4ffb
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-10-20 15:44:57 -0500

    replaced package shortbred with py-shortbred (#33404)
    * fixed version numbers to python 2 and old biopython
    
    * changed shortbred pacakge to pypi, removed python 2 version
    
    * added package description
    
    * re-added shortbred package with depreciated flag
    
    * fixed style and removed unnecessary python dep (it can't build with python 2 anyway)
    
    * removed whitespace and readded the python2.7.9+ dep
    
    * fixed style

--- var/spack/repos/builtin/packages/shortbred/package.py
+++ var/spack/repos/builtin/packages/shortbred/package.py
@@ -13,7 +13,11 @@ class Shortbred(Package):
     homepage = "https://huttenhower.sph.harvard.edu/shortbred"
     url = "https://bitbucket.org/biobakery/shortbred/get/0.9.4.tar.gz"
 
-    version("0.9.4", sha256="a85e5609db79696d3f2d478408fc6abfeea7628de9f533c4e1e0ea3622b397ba")
+    version(
+        "0.9.4",
+        sha256="a85e5609db79696d3f2d478408fc6abfeea7628de9f533c4e1e0ea3622b397ba",
+        deprecated=True,
+    )
 
     depends_on("blast-plus@2.2.28:")
     depends_on("cdhit@4.6:")
