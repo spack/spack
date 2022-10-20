038026f51ce3250f07e5eeda15db839174ce0f94
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-10-20 09:46:04 +0200

    FIX CI after git update (#33429)
    Add `protocol.file.allow always` to git configuration in CI

--- .github/workflows/setup_git.ps1
+++ .github/workflows/setup_git.ps1
@@ -6,6 +6,10 @@ git config --global user.email "spack@example.com"
 git config --global user.name "Test User"
 git config --global core.longpaths true
 
+# See https://github.com/git/git/security/advisories/GHSA-3wp6-j8xr-qw85 (CVE-2022-39253)
+# This is needed to let some fixture in our unit-test suite run
+git config --global protocol.file.allow always
+
 if ($(git branch --show-current) -ne "develop")
 {
     git branch develop origin/develop
