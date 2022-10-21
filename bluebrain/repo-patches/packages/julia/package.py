3992634c885f22e7cd1a9f4ea67410bd52f82cec
Matthias Wolf <matthias.wolf@epfl.ch>
2022-10-21 12:57:56 -0400

    julia: remove customization (#1735)
    Setting LD_LIBRARY_PATH to a path with heavily vendored libraries breaks
    all kinds of things. Don't hand users a loaded gun pointing at their
    foot.

--- bluebrain/repo-patches/packages/julia/package.py
+++
@@ -1,8 +0,0 @@
-from spack.pkg.builtin.julia import Julia as BuiltinJulia
-
-
-class Julia(BuiltinJulia):
-    __doc__ = BuiltinJulia.__doc__
-
-    def setup_run_environment(self, env):
-        env.prepend_path("LD_LIBRARY_PATH", self.prefix.lib.julia)
