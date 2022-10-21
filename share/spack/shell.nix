41c1e24d106f66ae7dd675ade4c70b28eb94beb5
Daniel Barter <danielbarter@gmail.com>
2022-10-21 13:27:01 -0700

    adding fortran components to nix env

--- share/spack/shell.nix
+++ share/spack/shell.nix
@@ -7,7 +7,10 @@ stdenv.mkDerivation {
   buildInputs = with pkgs; [
     python3
     gnupg
+
+    # fortran compiler not exposed by standard environment
     gfortran
+    gfortran.cc.lib
   ];
 
   name = "spack-shell";
