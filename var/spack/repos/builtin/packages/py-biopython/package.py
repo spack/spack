3c9ce5e2cb8011f2a565d4cdc75d539741dd8ec3
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-10-21 12:37:00 -0500

    updated sha256
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-biopython/package.py
+++ var/spack/repos/builtin/packages/py-biopython/package.py
@@ -19,7 +19,7 @@ class PyBiopython(PythonPackage):
     version("1.78", sha256="1ee0a0b6c2376680fea6642d5080baa419fd73df104a62d58a8baf7a8bbe4564")
     version("1.73", sha256="70c5cc27dc61c23d18bb33b6d38d70edc4b926033aea3b7434737c731c94a5e0")
     version("1.70", sha256="4a7c5298f03d1a45523f32bae1fffcff323ea9dce007fb1241af092f5ab2e45b")
-    version("1.65", sha256="463cc81db84e9bfcdfb15629511c81ed556a6c0287e670dbfe80f03c65d2a88e")
+    version("1.65", sha256="6d591523ba4d07a505978f6e1d7fac57e335d6d62fb5b0bcb8c40bdde5c8998e")
 
     depends_on("python@2.6:2.7,3.3:", type=("build", "run"), when="@1.63:1.68")
     depends_on("python@2.7,3.3:", type=("build", "run"), when="@1.69")
