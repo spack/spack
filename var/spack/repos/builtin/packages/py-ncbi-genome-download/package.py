d1c11658c122f01905f24044b695690d4fbfe61f
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-10-25 14:00:23 -0500

    fixed style

--- var/spack/repos/builtin/packages/py-ncbi-genome-download/package.py
+++ var/spack/repos/builtin/packages/py-ncbi-genome-download/package.py
@@ -16,6 +16,6 @@ class PyNcbiGenomeDownload(PythonPackage):
     version("0.3.1", sha256="74675e94f184b8d80429641b27ed6d46ed81028d95156337de6d09f8dd739c6e")
 
     depends_on("py-setuptools", type="build")
-    depends_on('py-appdirs', type=('build', 'run'))
-    depends_on('py-requests@2.4.3:', type=('build', 'run'))
-    depends_on('py-tqdm', type=('build', 'run'))
+    depends_on("py-appdirs", type=("build", "run"))
+    depends_on("py-requests@2.4.3:", type=("build", "run"))
+    depends_on("py-tqdm", type=("build", "run"))
