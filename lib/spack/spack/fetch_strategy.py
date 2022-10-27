141f25ee8e12b0eea7616e2e6bf90963657b7270
John Parent <john.parent@kitware.com>
2022-10-27 11:50:44 -0400

    style

--- lib/spack/spack/fetch_strategy.py
+++ lib/spack/spack/fetch_strategy.py
@@ -866,7 +866,11 @@ class GitFetchStrategy(VCSFetchStrategy):
                 if self.stage:
                     self.stage.srcdir = repo_name
                 shutil.copytree(repo_name, dest, symlinks=True)
-                shutil.rmtree(repo_name, ignore_errors=False, onerror=fs.readonly_file_handler(ignore_errors=True))
+                shutil.rmtree(
+                    repo_name,
+                    ignore_errors=False,
+                    onerror=fs.readonly_file_handler(ignore_errors=True),
+                )
 
             with working_dir(dest):
                 checkout_args = ["checkout", commit]
