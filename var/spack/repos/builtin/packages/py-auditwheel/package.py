4a3b830a40f6a0d54e404e12f1db9af1acc1f11a
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-10-28 09:06:38 -0500

    Update var/spack/repos/builtin/packages/py-auditwheel/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-auditwheel/package.py
+++ var/spack/repos/builtin/packages/py-auditwheel/package.py
@@ -19,5 +19,5 @@ class PyAuditwheel(PythonPackage):
     depends_on("python@3.6:", type=("build", "run"))
     depends_on("py-setuptools@45:", type="build")
     depends_on("py-setuptools-scm@6.2:", type="build")
-    depends_on("py-pyelftools", type=("build", "run"))
-    depends_on("py-importlib-metadata", type=("build", "run"))
+    depends_on("py-pyelftools@0.24:", type=("build", "run"))
+    depends_on("py-importlib-metadata", when="^python@:3.7", type=("build", "run"))
