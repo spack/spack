3d9ba17dc8fc2ad1b0235a4c42146f5fd01044b9
Markus Ferrell <markus.ferrell@kitware.com>
2022-10-28 12:26:01 -0400

    remove skip from find tests

--- lib/spack/spack/test/cmd/find.py
+++ lib/spack/spack/test/cmd/find.py
@@ -25,7 +25,6 @@ install = SpackCommand("install")
 
 base32_alphabet = "abcdefghijklmnopqrstuvwxyz234567"
 
-pytestmark = pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 
 
 @pytest.fixture(scope="module")
@@ -349,6 +348,7 @@ def test_find_command_basic_usage(database):
     assert "mpileaks" in output
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 @pytest.mark.regression("9875")
 def test_find_prefix_in_env(
     mutable_mock_env_path, install_mockery, mock_fetch, mock_packages, mock_archive, config
