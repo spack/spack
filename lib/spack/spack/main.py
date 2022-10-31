8b67d93a1a88300af21d5e63dd2e6d831e62c2b6
Harmen Stoppels <harmenstoppels@gmail.com>
2022-10-31 12:45:34 +0100

    shouty global

--- lib/spack/spack/main.py
+++ lib/spack/spack/main.py
@@ -108,7 +108,7 @@ spack_working_dir = None
 spack_ld_library_path = os.environ.get("LD_LIBRARY_PATH", "")
 
 #: Whether to print backtraces on error
-show_backtrace = False
+SHOW_BACKTRACE = False
 
 
 def set_working_dir():
@@ -572,8 +572,8 @@ def setup_main_options(args):
 
     if args.debug or args.backtrace:
         spack.error.debug = True
-        global show_backtrace
-        show_backtrace = True
+        global SHOW_BACKTRACE
+        SHOW_BACKTRACE = True
 
     if args.debug:
         spack.util.debug.register_interrupt_handler()
@@ -1007,7 +1007,7 @@ def main(argv=None):
         e.die()  # gracefully die on any SpackErrors
 
     except KeyboardInterrupt:
-        if spack.config.get("config:debug") or show_backtrace:
+        if spack.config.get("config:debug") or SHOW_BACKTRACE:
             raise
         sys.stderr.write("\n")
         tty.error("Keyboard interrupt.")
@@ -1017,12 +1017,12 @@ def main(argv=None):
             return signal.SIGINT
 
     except SystemExit as e:
-        if spack.config.get("config:debug") or show_backtrace:
+        if spack.config.get("config:debug") or SHOW_BACKTRACE:
             traceback.print_exc()
         return e.code
 
     except Exception as e:
-        if spack.config.get("config:debug") or show_backtrace:
+        if spack.config.get("config:debug") or SHOW_BACKTRACE:
             raise
         tty.error(e)
         return 3
