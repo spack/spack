2e02c5919155970f2b65a04180ebfb97357aa94f
Paul Kent <kentpr@ornl.gov>
2022-11-02 09:36:38 -0400

    v1504

--- var/spack/repos/builtin/packages/llvm/package.py
+++ var/spack/repos/builtin/packages/llvm/package.py
@@ -36,6 +36,7 @@ class Llvm(CMakePackage, CudaPackage):
 
     # fmt: off
     version("main", branch="main")
+    version("15.0.4", sha256="e24b4d3bf7821dcb1c901d1e09096c1f88fb00095c5a6ef893baab4836975e52")
     version("15.0.3", sha256="8ac8e4c0982bf236526d737d385db5e1e66543ab217a9355d54159659eae3774")
     version("15.0.2", sha256="dc11d35e60ab61792baa607dff080c993b39de23fb93b3d3369ba15b0601c307")
     version("15.0.1", sha256="20bccb964e39f604fdc16d1258f94d2053fbdcdab2b2f6d5e20e6095ec403c00")
