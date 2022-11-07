32a6d0224262a47793abb6e8d5b63a831144de5d
Bailey Brown <bobits@rit.edu>
2022-11-07 16:17:19 -0500

    Fixed flake8 errors

--- var/spack/repos/builtin/packages/py-rasterstats/package.py
+++ var/spack/repos/builtin/packages/py-rasterstats/package.py
@@ -5,19 +5,22 @@
 
 from spack.package import *
 
+
 class PyRasterstats(PythonPackage):
-    """rasterstats is a Python module for summarizing geospatial raster datasets based on vector geometries. It includes functions for zonal statistics and interpolated point queries. The command-line interface allows for easy interoperability with other GeoJSON tools."""
+    """rasterstats is a Python module for summarizing geospatial raster datasets based 
+    on vector geometries. It includes functions for zonal statistics and interpolated p
+    oint queries. The command-line interface allows for easy interoperability with othe
+    r GeoJSON tools."""
 
     homepage = "https://github.com/perrygeo/python-rasterstats"
     pypi     = "rasterstats/rasterstats-0.17.0.tar.gz"
 
     version('0.17.0', sha256='27975ebc0a402865dd9e92ad4ecf0dd62678e320735cc81e104c730e6d001298')
 
-    depends_on("py-affine@:2",type=("build","run"))
-    depends_on("py-shapely",type=("build","run"))
-    depends_on("py-numpy@1.9:",type=("build","run"))
-    depends_on("py-rasterio@1.0:",type=("build","run"))
-    depends_on("py-cligj@0.4:",type=("build","run"))
-    depends_on("py-fiona",type=("build","run"))
-    depends_on("py-simplejson",type=("build","run"))
-
+    depends_on("py-affine@:2", type=("build", "run"))
+    depends_on("py-shapely", type=("build", "run"))
+    depends_on("py-numpy@1.9:", type=("build", "run"))
+    depends_on("py-rasterio@1.0:", type=("build", "run"))
+    depends_on("py-cligj@0.4:", type=("build", "run"))
+    depends_on("py-fiona", type=("build", "run"))
+    depends_on("py-simplejson", type=("build", "run"))
