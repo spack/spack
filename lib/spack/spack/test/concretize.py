eba19b98ffb67164f460b90d9c9e0acbd29466f1
Greg Becker <becker33@llnl.gov>
2022-11-06 16:40:00 -0800

    solver: do not punish explicitly requested compiler mismatches (#30074)

--- lib/spack/spack/test/concretize.py
+++ lib/spack/spack/test/concretize.py
@@ -352,6 +352,17 @@ class TestConcretize(object):
         assert set(spec.compiler_flags["cflags"]) == set(["-g"])
         assert spec.satisfies("^openblas cflags='-O3'")
 
+    def test_mixing_compilers_only_affects_subdag(self):
+        spack.config.set("packages:all:compiler", ["clang", "gcc"])
+        spec = Spec("dt-diamond%gcc ^dt-diamond-bottom%clang").concretized()
+        for dep in spec.traverse():
+            assert ("%clang" in dep) == (dep.name == "dt-diamond-bottom")
+
+    def test_compiler_inherited_upwards(self):
+        spec = Spec("dt-diamond ^dt-diamond-bottom%clang").concretized()
+        for dep in spec.traverse():
+            assert "%clang" in dep
+
     def test_architecture_inheritance(self):
         """test_architecture_inheritance is likely to fail with an
         UnavailableCompilerVersionError if the architecture is concretized
