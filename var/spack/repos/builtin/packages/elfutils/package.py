ed12eeffcbaf3cbd1cdd9480d3bb06593efaf578
Tim Haines <thaines.astro@gmail.com>
2022-11-07 05:23:10 -0600

    elfutils: add version 0.188 (#33715)

--- var/spack/repos/builtin/packages/elfutils/package.py
+++ var/spack/repos/builtin/packages/elfutils/package.py
@@ -24,6 +24,7 @@ class Elfutils(AutotoolsPackage, SourcewarePackage):
 
     maintainers = ["mwkrentel"]
 
+    version("0.188", sha256="fb8b0e8d0802005b9a309c60c1d8de32dd2951b56f0c3a3cb56d21ce01595dff")
     version("0.187", sha256="e70b0dfbe610f90c4d1fe0d71af142a4e25c3c4ef9ebab8d2d72b65159d454c8")
     version("0.186", sha256="7f6fb9149b1673d38d9178a0d3e0fb8a1ec4f53a9f4c2ff89469609879641177")
     version("0.185", sha256="dc8d3e74ab209465e7f568e1b3bb9a5a142f8656e2b57d10049a73da2ae6b5a6")
