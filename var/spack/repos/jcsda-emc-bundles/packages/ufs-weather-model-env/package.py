488993f536eec456050819208400224dd574efaa
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2022-11-08 02:34:54 +0000

    Restructure ufs-weather-model-env package and add variant for shared vs. static

--- var/spack/repos/jcsda-emc-bundles/packages/ufs-weather-model-env/package.py
+++ var/spack/repos/jcsda-emc-bundles/packages/ufs-weather-model-env/package.py
@@ -12,9 +12,9 @@ class UfsWeatherModelEnv(BundlePackage):
     homepage = "https://github.com/ufs-community/ufs-weather-model"
     git = "https://github.com/ufs-community/ufs-weather-model.git"
 
-    maintainers = ["kgerheiser", "climbfuji"]
+    maintainers = ["AlexanderRichert-NOAA", "climbfuji"]
 
-    version("1.0.0")
+    version("develop")
 
     variant(
         "debug",
@@ -22,22 +22,41 @@ class UfsWeatherModelEnv(BundlePackage):
         description="Build a debug version of certain dependencies (ESMF, MAPL)",
     )
     variant("python", default=True, description="Build Python dependencies")
+    variant("shared", default=True, description="Build dynamic libraries when possible")
 
-    depends_on("base-env", type="run")
     depends_on("ufs-pyenv", type="run", when="+python")
 
-    depends_on("fms@2022.01", type="run")
-    depends_on("bacio", type="run")
-    depends_on("crtm@2.4.0", type="run")
-    depends_on("g2", type="run")
-    depends_on("g2tmpl", type="run")
-    depends_on("ip", type="run")
-    depends_on("sp", type="run")
-    depends_on("w3nco", type="run")
+    depends_on("bacio@2.4.1")
+    depends_on("fms@2022.01")
+    depends_on("crtm@2.4.0")
+    depends_on("g2@3.4.5")
+    depends_on("g2tmpl@1.10.0")
+    depends_on("ip@3.3.3")
+    depends_on("sp@2.3.3")
+    depends_on("w3emc@2.9.2")
 
-    depends_on("esmf~debug", type="run", when="~debug")
-    depends_on("esmf+debug", type="run", when="+debug")
-    depends_on("mapl~debug", type="run", when="~debug")
-    depends_on("mapl+debug", type="run", when="+debug")
+    depends_on("hdf5@1.10.6~shared+mpi~tools~threadsafe~cxx+hl",when="~shared")
+    depends_on("jasper@2.0.25~shared",when="~shared")
+    depends_on("libjpeg-turbo@2.1.0~shared",when="~shared")
+    depends_on("libpng@1.6.37~shared",when="~shared")
+    depends_on("netcdf-c@4.7.4~shared~parallel-netcdf~cdf5~dap~doxygen~parallel-tests~utilities+v2~largefile+mpi",when="~shared")
+    depends_on("netcdf-fortran@4.5.4~shared",when="~shared")
+    depends_on("zlib@1.2.11~shared",when="~shared")
+    depends_on("parallelio@2.5.7+fortran~ipo~pnetcdf~shared")
 
-    # There is no need for install() since there is no code.
+    depends_on("hdf5@1.10.6+shared+mpi~tools~threadsafe~cxx+hl",when="+shared")
+    depends_on("jasper@2.0.25+shared",when="+shared")
+    depends_on("libjpeg-turbo@2.1.0+shared",when="+shared")
+    depends_on("libpng@1.6.37+shared",when="+shared")
+    depends_on("netcdf-c@4.7.4+shared~parallel-netcdf~cdf5~dap~doxygen~parallel-tests~utilities+v2~largefile+mpi",when="+shared")
+    depends_on("netcdf-fortran@4.5.4+shared",when="+shared")
+    depends_on("zlib@1.2.11+shared",when="+shared")
+    depends_on("parallelio@2.5.7+fortran~ipo~pnetcdf+shared")
+
+    esmf_ver = "8.3.0b09"
+    depends_on(f"esmf@{esmf_ver}~debug+caplinks", when="~debug")
+    depends_on(f"esmf@{esmf_ver}+debug+caplinks", when="+debug")
+    mapl_ver = "2.22.0"
+    depends_on(f"mapl@{mapl_ver}~debug~shared~pnetcdf~buildexe", type="run", when="~debug~shared")
+    depends_on(f"mapl@{mapl_ver}~debug+shared~pnetcdf~buildexe", type="run", when="~debug+shared")
+    depends_on(f"mapl@{mapl_ver}+debug", when="+debug")
