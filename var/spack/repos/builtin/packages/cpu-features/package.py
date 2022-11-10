0174aeafafcbba1b7fe71cc15d38edea83dcf68f
iarspider <iarspider@users.noreply.github.com>
2022-11-10 08:00:39 +0000

    [@spackbot] updating style on behalf of iarspider

--- var/spack/repos/builtin/packages/cpu-features/package.py
+++ var/spack/repos/builtin/packages/cpu-features/package.py
@@ -16,7 +16,7 @@ class CpuFeatures(CMakePackage):
     version("develop", branch="main", deprecated=True)
 
     version("0.7.0", tag="v0.7.0")
-    
+
     variant("shared", description="Build shared libraries", default=False)
 
     depends_on("cmake@3.0.0:", type="build")
