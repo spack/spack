afba70c639280557016974cacb9c0232c4bda24a
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-11-10 14:39:46 -0600

    make version resource consistent for r-go-db

--- var/spack/repos/builtin/packages/r-go-db/package.py
+++ var/spack/repos/builtin/packages/r-go-db/package.py
@@ -17,28 +17,28 @@ class RGoDb(RPackage):
 
     version(
         "3.16.0",
-        sha256="4652812d8ba380aeeb9b136efbc9365156397eec99c5ca36cfb8294139493b8e",
         url="https://bioconductor.org/packages/3.16/data/annotation/src/contrib/GO.db_3.16.0.tar.gz",
+        sha256="4652812d8ba380aeeb9b136efbc9365156397eec99c5ca36cfb8294139493b8e",
     )
     version(
         "3.15.0",
-        sha256="bac91d73c57f206fa5bc4a501a2aaf61b365cf411181ce44353370cdbc132d99",
         url="https://bioconductor.org/packages/3.15/data/annotation/src/contrib/GO.db_3.15.0.tar.gz",
+        sha256="bac91d73c57f206fa5bc4a501a2aaf61b365cf411181ce44353370cdbc132d99",
     )
     version(
         "3.14.0",
-        sha256="45d0a681a662667d45b2472d160b72f7058ad0a28dd0ca24742e11ddfd87d8e7",
         url="https://bioconductor.org/packages/3.14/data/annotation/src/contrib/GO.db_3.14.0.tar.gz",
+        sha256="45d0a681a662667d45b2472d160b72f7058ad0a28dd0ca24742e11ddfd87d8e7",
     )
     version(
         "3.12.1",
-        sha256="e0316959d3d32096f9432c897413dff74fce53e15ead7917a7724467d971dab9",
         url="https://bioconductor.org/packages/3.12/data/annotation/src/contrib/GO.db_3.12.1.tar.gz",
+        sha256="e0316959d3d32096f9432c897413dff74fce53e15ead7917a7724467d971dab9",
     )
     version(
         "3.4.1",
-        sha256="2fc2048e9d26edb98e35e4adc4d18c6df54f44836b5cc4a482d36ed99e058cc1",
         url="https://bioconductor.org/packages/3.5/data/annotation/src/contrib/GO.db_3.4.1.tar.gz",
+        sha256="2fc2048e9d26edb98e35e4adc4d18c6df54f44836b5cc4a482d36ed99e058cc1",
     )
 
     depends_on("r@2.7.0:", type=("build", "run"))
