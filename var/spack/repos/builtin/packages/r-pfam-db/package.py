2496d97e4fb1c8e22248b3e38d71e8f66315c512
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-11-10 14:42:34 -0600

    make version resource consistent for r-pfam-db

--- var/spack/repos/builtin/packages/r-pfam-db/package.py
+++ var/spack/repos/builtin/packages/r-pfam-db/package.py
@@ -17,33 +17,33 @@ class RPfamDb(RPackage):
 
     version(
         "3.16.0",
-        sha256="8082aaa94eb962b2cf42cfaead8ff9e1bdbd12fdeae82a49d0f3b37bd50eb30c",
         url="https://bioconductor.org/packages/3.16/data/annotation/src/contrib/PFAM.db_3.16.0.tar.gz",
+        sha256="8082aaa94eb962b2cf42cfaead8ff9e1bdbd12fdeae82a49d0f3b37bd50eb30c",
     )
     version(
         "3.15.0",
-        sha256="e7036a5f76779ab6e8a32eb659c54f52426c4824dee02903cffd85bed372720f",
         url="https://bioconductor.org/packages/3.15/data/annotation/src/contrib/PFAM.db_3.15.0.tar.gz",
+        sha256="e7036a5f76779ab6e8a32eb659c54f52426c4824dee02903cffd85bed372720f",
     )
     version(
         "3.14.0",
-        sha256="25c1915079e8f93d04e2cc1ab791f7f47813aaab5ac394feaf57520bb292d616",
         url="https://bioconductor.org/packages/3.14/data/annotation/src/contrib/PFAM.db_3.14.0.tar.gz",
+        sha256="25c1915079e8f93d04e2cc1ab791f7f47813aaab5ac394feaf57520bb292d616",
     )
     version(
         "3.12.0",
-        sha256="ec42d067522baf2d7d3ca78d4f8cc0dac08a4b98f1d890f52424e5d5b16f2fe9",
         url="https://bioconductor.org/packages/3.12/data/annotation/src/contrib/PFAM.db_3.12.0.tar.gz",
+        sha256="ec42d067522baf2d7d3ca78d4f8cc0dac08a4b98f1d890f52424e5d5b16f2fe9",
     )
     version(
         "3.10.0",
-        sha256="038888f95ce69230ac0e0b08aa3bcb09965682415520d437a7fb0a031eefe158",
         url="https://bioconductor.org/packages/3.10/data/annotation/src/contrib/PFAM.db_3.10.0.tar.gz",
+        sha256="038888f95ce69230ac0e0b08aa3bcb09965682415520d437a7fb0a031eefe158",
     )
     version(
         "3.4.1",
-        sha256="fc45a0d53139daf85873f67bd3f1b68f2d883617f4447caddbd2d7dcc58a393f",
         url="https://bioconductor.org/packages/3.5/data/annotation/src/contrib/PFAM.db_3.4.1.tar.gz",
+        sha256="fc45a0d53139daf85873f67bd3f1b68f2d883617f4447caddbd2d7dcc58a393f",
     )
 
     depends_on("r@2.7.0:", when="@3.4.1:", type=("build", "run"))
