4aae391253e4a12e6eabd7b0d91f7251a3bbd9f2
Robert Underwood <runderwood@anl.gov>
2022-11-12 09:30:27 -0500

    add sz3 and mdz smoke test

--- var/spack/repos/builtin/packages/sz3/package.py
+++ var/spack/repos/builtin/packages/sz3/package.py
@@ -13,14 +13,17 @@ class Sz3(CMakePackage):
     git = "https://github.com/szcompressor/SZ3"
 
     maintainers = ["disheng222"]
+    tags = ["e4s"]
 
     version("master")
+    version("3.1.7", commit="c49fd17f2d908835c41000c1286c510046c0480e")
     version("3.1.5.4", commit="4c6ddf628f27d36b28d1bbda02174359cd05573d")
     version("3.1.5.1", commit="5736a63b917e439dd62248b4ff6234e96726af5d")
     version("3.1.3.1", commit="323cb17b412d657c4be681b52c34beaf933fe7af")
     version("3.1.3", commit="695dff8dc326f3b165f6676d810f46add088a585")
 
     variant("hdf5", default=False, description="enable hdf5 filter support")
+    variant("mdz", default=True, description="build mdz executable")
 
     depends_on("zstd")
     depends_on("gsl")
@@ -35,5 +38,15 @@ class Sz3(CMakePackage):
         return [
             "-DSZ3_USE_BUNDLED_ZSTD=OFF",
             "-DSZ3_DEBUG_TIMINGS=OFF",
+            self.define_from_variant("BUILD_MDZ", "mdz"),
             self.define_from_variant("BUILD_H5Z_FILTER", "hdf5"),
         ]
+
+    def test(self):
+        if self.version < Version("3.1.7"):
+            return
+
+        self.run_test(self.prefix.bin.sz3_smoke_test, purpose="sz3 works")
+
+        if "+mdz" in self.spec:
+            self.run_test(self.prefix.bin.mdz_smoke_test, purpose="mdz works")
