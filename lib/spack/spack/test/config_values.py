fd0f0d4c90e912b2928bb60bea240c0a5fd5bf94
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-11-14 13:26:14 +0100

    Fix a bug/typo in a config_values.py fixture (#33886)

--- lib/spack/spack/test/config_values.py
+++ lib/spack/spack/test/config_values.py
@@ -28,7 +28,7 @@ def test_set_install_hash_length(hash_length, mutable_config, tmpdir):
         assert len(hash_str) == hash_length
 
 
-@pytest.mark.use_fixtures("mock_packages")
+@pytest.mark.usefixtures("mock_packages")
 def test_set_install_hash_length_upper_case(mutable_config, tmpdir):
     mutable_config.set("config:install_hash_length", 5)
     mutable_config.set(
