d86ed40fbcc3e89984c2c5398314fe5e06fc76f4
Erik Schnetter <schnetter@gmail.com>
2022-11-13 20:41:34 -0500

    meson: New version 0.64.0

--- var/spack/repos/builtin/packages/meson/package.py
+++ var/spack/repos/builtin/packages/meson/package.py
@@ -18,6 +18,7 @@ class Meson(PythonPackage):
 
     maintainers = ["eli-schwartz", "michaelkuhn"]
 
+    version("0.64.0", sha256="6477993d781b6efea93091616a6d6a0766c0e026076dbeb11249bf1c9b49a347")
     version("0.63.3", sha256="7c516c2099b762203e8a0a22412aa465b7396e6f9b1ab728bad6e6db44dc2659")
     version("0.63.2", sha256="023a3f7c74e68991154c3205a6975705861eedbf8130e013d15faa1df1af216e")
     version("0.63.1", sha256="f355829f0e8c714423f03a06604c04c216d4cbe3586f3154cb2181076b19207a")
