805d4d1a4bafde319aa8021efbce205909dc3f7d
blue42u <anderson.jonathonm@gmail.com>
2022-11-14 18:47:32 -0600

    Revert "fix perl libxcrypt.so dep (#33846)"
    This reverts commit bf1b2a828c61fb465f7641b6230fef4f17867162.

--- var/spack/repos/builtin/packages/perl/package.py
+++ var/spack/repos/builtin/packages/perl/package.py
@@ -110,7 +110,6 @@ class Perl(Package):  # Perl doesn't use Autotools, it should subclass Package
         depends_on("zlib")
         # :5.24.1 needs zlib@:1.2.8: https://rt.cpan.org/Public/Bug/Display.html?id=120134
         depends_on("zlib@:1.2.8", when="@5.20.3:5.24.1")
-        depends_on("libxcrypt")  # libcrypt.so.1
 
     conflicts("@5.34.1:", when="%msvc@:19.29.30136")
     # there has been a long fixed issue with 5.22.0 with regard to the ccflags
