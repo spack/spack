e6574ea29fb39cabd36391ee66825b01d09f6365
Paul Romano <paul.k.romano@gmail.com>
2022-11-16 23:32:34 -0600

    Update Python version dependency

--- var/spack/repos/builtin/packages/py-openmc/package.py
+++ var/spack/repos/builtin/packages/py-openmc/package.py
@@ -53,7 +53,8 @@ class PyOpenmc(PythonPackage):
         )
 
     depends_on("git", type="build")
-    depends_on("python@3.6:", type=("build", "run"), when="@0.13:")
+    depends_on("python@3.7:", type=("build", "run"), when="@0.13.2:")
+    depends_on("python@3.6:", type=("build", "run"), when="@0.13.0:0.13.1")
     depends_on("python@3.5:", type=("build", "run"), when="@:0.12")
     depends_on("py-cython", type="build")
     depends_on("py-h5py~mpi", when="~mpi", type=("build", "run"))
