b0ddffe8125402dcf1a0a0391e606437f813b9f2
snehring <snehring@iastate.edu>
2022-11-17 10:54:14 -0600

    py-dnaio: remove old python versions

--- var/spack/repos/builtin/packages/py-dnaio/package.py
+++ var/spack/repos/builtin/packages/py-dnaio/package.py
@@ -17,13 +17,12 @@ class PyDnaio(PythonPackage):
     version("0.4.2", sha256="fa55a45bfd5d9272409b714158fb3a7de5dceac1034a0af84502c7f503ee84f8")
     version("0.3", sha256="47e4449affad0981978fe986684fc0d9c39736f05a157f6cf80e54dae0a92638")
 
-    depends_on("py-setuptools", type=("build", "run"))
-    depends_on("py-setuptools-scm", when="@0.4:", type="build")
+    depends_on("py-setuptools@52:", type=("build", "run"))
+    depends_on("py-setuptools-scm@6.2:", when="@0.4:", type="build")
     # 0.9.1 deps
     depends_on("python@3.7:", type=("build", "run"), when="@0.9.1:")
     depends_on("py-cython@0.29.20:", type=("build", "run"), when="@0.9.1:")
     depends_on("py-xopen@1.4.0:", type=("build", "run"), when="@0.9.1:")
     # older deps
-    depends_on("python@3.4:", type=("build", "run"))
     depends_on("py-xopen", type=("build", "run"))
     depends_on("py-xopen@0.8.2:", when="@0.4:", type=("build", "run"))
