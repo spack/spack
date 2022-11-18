3f6efb1b539e62ce69090e648cd45091e43bbded
Sid Pendelberry <sid@rit.edu>
2022-11-18 11:14:11 -0500

    updated to limit python to >4

--- var/spack/repos/builtin/packages/py-ahpy/package.py
+++ var/spack/repos/builtin/packages/py-ahpy/package.py
@@ -16,7 +16,7 @@ class PyAhpy(PythonPackage):
 
     version('2.0', sha256='f0af7b81b51466a055778d84f64c98f5cc3e1ba34aaeaedc48ba0b91008e40e3')
 
-    depends_on("python@3.7:", type=("build", "run"))
+    depends_on("python@3.7:3", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
