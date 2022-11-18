30645f2e8fc11768c83be751131463601c3fbd11
Erik Heeren <erik.heeren@epfl.ch>
2022-11-18 11:06:22 +0100

    py-cmake-format: add sha for version 0.6.11

--- bluebrain/repo-patches/packages/py-cmake-format/package.py
+++ bluebrain/repo-patches/packages/py-cmake-format/package.py
@@ -5,9 +5,11 @@ from spack.pkg.builtin.py_cmake_format import PyCmakeFormat as BuiltinPyCmakeFor
 class PyCmakeFormat(BuiltinPyCmakeFormat):
     __doc__ = BuiltinPyCmakeFormat.__doc__
 
-    pypi = "cmake-format/cmake-format-0.6.9.tar.gz"
+    # the tarball filename format changed starting with 0.6.12
+    # Unfortunately, those tarballs are also broken - 0.6.11 is the last working version at the time of writing
+    # pypi = "cmake-format/cmake-format-0.6.12.tar.gz"
+    pypi = "cmake-format/cmake_format-0.6.9.tar.gz"
 
-    version("0.6.13", sha256="1a48b779067ecca68c498691d07d5c9d3df9803a7e0c5b641128fa6efe5ae489")
-    version("0.6.12", sha256="fd6e95c6f4f44fe7eb24cda118ba7e3e1a61997c24127ab5e7ca414940e07e5a")
+    version("0.6.11", sha256="aa3d0e6156bcd4ae8fbd630ccb1d4995179b37b1b31923f2eb53764104da75f3")
 
     depends_on('py-pyyaml', type=('run'))
