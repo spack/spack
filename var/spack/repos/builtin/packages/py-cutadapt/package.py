307c46d0b63c37feb28fa771f4055b5d358a734c
snehring <snehring@iastate.edu>
2022-11-18 10:30:17 -0600

    py-cutadapt: add cython dep

--- var/spack/repos/builtin/packages/py-cutadapt/package.py
+++ var/spack/repos/builtin/packages/py-cutadapt/package.py
@@ -25,6 +25,7 @@ class PyCutadapt(PythonPackage):
     depends_on("py-setuptools@43:", type=("build", "run"))
     depends_on("py-setuptools-scm@6.2:", type="build", when="@2.0:")
     depends_on("python@3.7:", type=("build", "run"), when="@4.1:")
+    depends_on("py-cython@0.29.20:", type=("build", "run"))
     # 4.1 deps
     depends_on("py-dnaio@0.7.1:", type=("build", "run"), when="@4.1:")
     depends_on("py-xopen@1.1:", type=("build", "run"), when="@4.1:")
