945ca900c3600875694ec8540ad59cd502d01a0c
John Parent <john.parent@kitware.com>
2022-11-20 17:46:11 -0500

    Fixup broken test in external find

--- lib/spack/spack/test/cmd/external.py
+++ lib/spack/spack/test/cmd/external.py
@@ -107,9 +107,7 @@ def test_find_external_update_config(mutable_config):
 
 def test_get_executables(working_env, mock_executable):
     cmake_path1 = mock_executable("cmake", output="echo cmake version 1.foo")
-
-    os.environ["PATH"] = os.pathsep.join([os.path.dirname(cmake_path1)])
-    path_to_exe = spack.detection.executables_in_path([cmake_path1])
+    path_to_exe = spack.detection.executables_in_path([os.path.dirname(cmake_path1)])
     cmake_exe = define_plat_exe("cmake")
     assert path_to_exe[cmake_path1] == cmake_exe
 
