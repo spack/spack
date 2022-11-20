cf389505de79ea83afe9f0dfc1d40e750b6f384c
Sam Grayson <sam@samgrayson.me>
2022-11-20 11:43:32 -0700

    Update dask and related packages (#33925)
    * Update dask and related packages
    
    * Update package dependency specs
    
    * Run spack style
    
    * Add new version of locket
    
    * Respond to comments
    
    * Added constraints
    
    * Add version constraints for py-dask+distributed
    
    * Run spack style
    
    * Update var/spack/repos/builtin/packages/py-dask/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Deprecated dask versions
    
    * Deprecated more dask and distirbuted
    
    * spack style --fix
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-locket/package.py
+++ var/spack/repos/builtin/packages/py-locket/package.py
@@ -12,6 +12,7 @@ class PyLocket(PythonPackage):
     homepage = "https://github.com/mwilliamson/locket.py"
     pypi = "locket/locket-0.2.0.tar.gz"
 
+    version("1.0.0", sha256="5c0d4c052a8bbbf750e056a8e65ccd309086f4f0f18a2eac306a8dfa4112a632")
     version("0.2.0", sha256="1fee63c1153db602b50154684f5725564e63a0f6d09366a1cb13dffcec179fb4")
 
     # pip silently replaces distutils with setuptools
