c5883fffd7310f704a8adc2f3666d7539cdfe4e1
Adam J. Stewart <ajstewart426@gmail.com>
2022-11-22 17:02:30 -0600

    Python: drop EOL versions (#33898)
    This PR removes [end of life](https://endoflife.date/python) versions of Python from Spack. Specifically, this includes all versions of Python older than 3.7.
    
    See https://github.com/spack/spack/discussions/31824 for rationale. Deprecated in #32615. And #28003.
    
    For anyone using software that relies on Python 2, you have a few options:
    
    * Upgrade the software to support Python 3. The `3to2` tool may get you most of the way there, although more complex libraries may need manual tweaking.
    * Add Python 2 as an [external package](https://spack.readthedocs.io/en/latest/build_settings.html#external-packages). Many Python libraries do not support Python 2, but you may be able to add older versions that did once upon a time.
    * Use Spack 0.19. Spack 0.19 is the last release to officially support Python 3.6 and older
    * Create and maintain your own [custom repository](https://spack.readthedocs.io/en/latest/repositories.html). Basically, you would need a package for Python 2 and any other Python 2-specific libraries you need.

--- var/spack/repos/builtin/packages/py-slepc4py/package.py
+++ var/spack/repos/builtin/packages/py-slepc4py/package.py
@@ -31,10 +31,6 @@ class PySlepc4py(PythonPackage):
     version("3.13.0", sha256="780eff0eea1a5217642d23cd563786ef22df27e1d772a1b0bb4ccc5701df5ea5")
     version("3.12.0", sha256="d8c06953b7d00f529a9a7fd016dfa8efdf1d05995baeea7688d1d59611f424f7")
     version("3.11.0", sha256="1e591056beee209f585cd781e5fe88174cd2a61215716a71d9eaaf9411b6a775")
-    version("3.10.0", sha256="6494959f44280d3b80e73978d7a6bf656c9bb04bb3aa395c668c7a58948db1c6")
-    version("3.9.0", sha256="84cab4216268c2cb7d01e7cdbb1204a3c3e13cdfcd7a78ea057095f96f68c3c0")
-    version("3.8.0", sha256="988815b3650b69373be9abbf2355df512dfd200aa74b1785b50a484d6dfee971")
-    version("3.7.0", sha256="139f8bb325dad00a0e8dbe5b3e054050c82547936c1b6e7812fb1a3171c9ad0b")
 
     patch("ldshared.patch", when="@:99")
     patch("ldshared-dev.patch", when="@main")
@@ -50,10 +46,6 @@ class PySlepc4py(PythonPackage):
     depends_on("py-petsc4py@3.13.0:3.13", when="@3.13.0:3.13", type=("build", "run"))
     depends_on("py-petsc4py@3.12.0:3.12", when="@3.12.0:3.12", type=("build", "run"))
     depends_on("py-petsc4py@3.11.0:3.11", when="@3.11.0:3.11", type=("build", "run"))
-    depends_on("py-petsc4py@3.10.0:3.10", when="@3.10.0:3.10", type=("build", "run"))
-    depends_on("py-petsc4py@3.9.0:3.9", when="@3.9.0:3.9", type=("build", "run"))
-    depends_on("py-petsc4py@3.8.0:3.8", when="@3.8.0:3.8", type=("build", "run"))
-    depends_on("py-petsc4py@3.7.0:3.7", when="@3.7.0:3.7", type=("build", "run"))
 
     depends_on("slepc")
     depends_on("slepc@3.18.0:3.18", when="@3.18.0:3.18")
@@ -63,10 +55,6 @@ class PySlepc4py(PythonPackage):
     depends_on("slepc@3.13.0:3.13", when="@3.13.0:3.13")
     depends_on("slepc@3.12.0:3.12", when="@3.12.0:3.12")
     depends_on("slepc@3.11.0:3.11", when="@3.11.0:3.11")
-    depends_on("slepc@3.10.0:3.10", when="@3.10.0:3.10")
-    depends_on("slepc@3.9.0:3.9", when="@3.9.0:3.9")
-    depends_on("slepc@3.8.0:3.8", when="@3.8.0:3.8")
-    depends_on("slepc@3.7.0:3.7", when="@3.7.0:3.7")
 
     @property
     def build_directory(self):
