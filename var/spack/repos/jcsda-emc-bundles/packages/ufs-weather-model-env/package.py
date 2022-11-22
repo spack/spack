552a2edbdc73ec74200f26597d95b6ec47a97d26
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2022-11-22 02:25:17 +0000

    UFS env: Remove versions

--- var/spack/repos/jcsda-emc-bundles/packages/ufs-weather-model-env/package.py
+++ var/spack/repos/jcsda-emc-bundles/packages/ufs-weather-model-env/package.py
@@ -14,7 +14,7 @@ class UfsWeatherModelEnv(BundlePackage):
 
     maintainers = ["AlexanderRichert-NOAA", "climbfuji"]
 
-    version("develop")
+    version("1.0.0")
 
     variant(
         "debug",
@@ -26,37 +26,35 @@ class UfsWeatherModelEnv(BundlePackage):
 
     depends_on("ufs-pyenv", type="run", when="+python")
 
-    depends_on("bacio@2.4.1")
-    depends_on("fms@2022.01")
-    depends_on("crtm@2.4.0")
-    depends_on("g2@3.4.5")
-    depends_on("g2tmpl@1.10.0")
-    depends_on("ip@3.3.3")
-    depends_on("sp@2.3.3")
-    depends_on("w3emc@2.9.2")
+    depends_on("bacio")
+    depends_on("fms")
+    depends_on("crtm")
+    depends_on("g2")
+    depends_on("g2tmpl")
+    depends_on("ip")
+    depends_on("sp")
+    depends_on("w3emc")
+    depends_on("parallelio+fortran~pnetcdf~shared")
 
-    depends_on("hdf5@1.10.6~shared+mpi~tools~threadsafe~cxx+hl",when="~shared")
-    depends_on("jasper@2.0.25~shared",when="~shared")
-    depends_on("libjpeg-turbo@2.1.0~shared",when="~shared")
-    depends_on("libpng@1.6.37~shared",when="~shared")
-    depends_on("netcdf-c@4.7.4~shared~parallel-netcdf~cdf5~dap~doxygen~parallel-tests~utilities+v2~largefile+mpi",when="~shared")
-    depends_on("netcdf-fortran@4.5.4~shared",when="~shared")
-    depends_on("zlib@1.2.11~shared",when="~shared")
-    depends_on("parallelio@2.5.7+fortran~ipo~pnetcdf~shared")
+    depends_on("hdf5+hl+mpi~shared", when="~shared")
+    depends_on("jasper~shared", when="~shared")
+    depends_on("libjpeg-turbo~shared", when="~shared")
+    depends_on("libpng~shared", when="~shared")
+    depends_on("netcdf-c~parallel-netcdf+v2+mpi~shared", when="~shared")
+    depends_on("netcdf-fortran~shared", when="~shared")
+    depends_on("zlib~shared", when="~shared")
+    depends_on("parallel-netcdf~shared", when="~shared")
 
-    depends_on("hdf5@1.10.6+shared+mpi~tools~threadsafe~cxx+hl",when="+shared")
-    depends_on("jasper@2.0.25+shared",when="+shared")
-    depends_on("libjpeg-turbo@2.1.0+shared",when="+shared")
-    depends_on("libpng@1.6.37+shared",when="+shared")
-    depends_on("netcdf-c@4.7.4+shared~parallel-netcdf~cdf5~dap~doxygen~parallel-tests~utilities+v2~largefile+mpi",when="+shared")
-    depends_on("netcdf-fortran@4.5.4+shared",when="+shared")
-    depends_on("zlib@1.2.11+shared",when="+shared")
-    depends_on("parallelio@2.5.7+fortran~ipo~pnetcdf+shared")
+    depends_on("hdf5+hl+mpi+shared", when="+shared")
+    depends_on("jasper+shared", when="+shared")
+    depends_on("libpng+shared", when="+shared")
+    depends_on("netcdf-c~parallel-netcdf+v2+mpi+shared", when="+shared")
+    depends_on("netcdf-fortran+shared", when="+shared")
+    depends_on("zlib+shared", when="+shared")
 
-    esmf_ver = "8.3.0b09"
-    depends_on(f"esmf@{esmf_ver}~debug+caplinks", when="~debug")
-    depends_on(f"esmf@{esmf_ver}+debug+caplinks", when="+debug")
-    mapl_ver = "2.22.0"
-    depends_on(f"mapl@{mapl_ver}~debug~shared~pnetcdf~buildexe", type="run", when="~debug~shared")
-    depends_on(f"mapl@{mapl_ver}~debug+shared~pnetcdf~buildexe", type="run", when="~debug+shared")
-    depends_on(f"mapl@{mapl_ver}+debug", when="+debug")
+    depends_on(f"esmf~debug", when="~debug")
+    depends_on(f"esmf+debug", when="+debug")
+    depends_on(f"mapl~pnetcdf~debug~shared", type="run", when="~debug~shared")
+    depends_on(f"mapl~pnetcdf~debug+shared", type="run", when="~debug+shared")
+    depends_on(f"mapl~pnetcdf+debug~shared", type="run", when="+debug~shared")
+    depends_on(f"mapl~pnetcdf+debug+shared", type="run", when="+debug+shared")
