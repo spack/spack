a381ea28cdc76c0e3b0a90a69c015833f46f57b2
Bernhard Kaindl <43588962+bernhardkaindl@users.noreply.github.com>
2022-11-23 22:37:37 +0000

    bcache: support external gettext when `libintl` is in glibc
    Many glibc-based Linux systems don't have gettext's libintl because
    libintl is included in the standard system's glibc (libc) itself.
    
    When using `spack external find gettext` on those, packages like
    `bcache` which unconditionally to link using `-lintl` fail to link
    with -lintl.
    
    Description of the fix:
    
    The libs property of spack's gettext recipe returns the list of libs,
    so when gettext provides libintl, use it. When not, there is no
    separate liblint library and the libintl API is provided by glibc.
    
    Tested with `spack external find gettext` on glibc-based Linux and
    in musl-based Alpine Linux to make sure that when -lintl is really
    needed, it is really used and nothing breaks.

--- var/spack/repos/builtin/packages/bcache/package.py
+++ var/spack/repos/builtin/packages/bcache/package.py
@@ -25,7 +25,9 @@ class Bcache(MakefilePackage):
     depends_on("pkgconfig", type="build")
 
     def setup_build_environment(self, env):
-        env.append_flags("LDFLAGS", "-lintl")
+        # Add -lintl if provided by gettext, otherwise libintl is provided by the system's glibc:
+        if "libintl" in self.spec["gettext"].libs.joined():
+            env.append_flags("LDFLAGS", "-lintl")
 
     patch(
         "func_crc64.patch",
