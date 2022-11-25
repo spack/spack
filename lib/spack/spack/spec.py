spack/spack

commit de8c827983556f77899adad46a2c7dd46388883b

Refactor a few classes related to package repositories (#32273)
Caches used by repositories don't reference the global spack.repo.path instance
anymore, but get the repository they refer to during initialization.
 
Spec.virtual now use the index, and computation done to compute the index 
use Repository.is_virtual_safe. 

Code to construct mock packages and mock repository has been factored into 
a unique MockRepositoryBuilder that is used throughout the codebase.

Add debug print for pushing and popping config scopes.

Changed spack.repo.use_repositories so that it can override or not previous repos

spack.repo.use_repositories updates spack.config.config according to the modifications done

Removed a peculiar behavior from spack.config.Configuration where push would always 
bubble-up a scope named command_line if it existed

lib/spack/spack/spec.py
@@ -1548,16 +1548,7 @@ class Spec(object):
 
     @property
     def virtual(self):
-        """Right now, a spec is virtual if no package exists with its name.
-
-        TODO: revisit this -- might need to use a separate namespace and
-        be more explicit about this.
-        Possible idea: just use conventin and make virtual deps all
-        caps, e.g., MPI vs mpi.
-        """
-        # This method can be called while regenerating the provider index
-        # So we turn off using the index to detect virtuals
-        return spack.repo.path.is_virtual(self.name, use_index=False)
+        return spack.repo.path.is_virtual(self.name)
 
     @property
     def concrete(self):
@@ -2627,7 +2618,9 @@ class Spec(object):
            a problem.
         """
         # Make an index of stuff this spec already provides
-        self_index = spack.provider_index.ProviderIndex(self.traverse(), restrict=True)
+        self_index = spack.provider_index.ProviderIndex(
+            repository=spack.repo.path, specs=self.traverse(), restrict=True
+        )
         changed = False
         done = False
 
@@ -3151,7 +3144,7 @@ class Spec(object):
         Raise an exception if there is a conflicting virtual
         dependency already in this spec.
         """
-        assert vdep.virtual
+        assert spack.repo.path.is_virtual_safe(vdep.name), vdep
 
         # note that this defensively copies.
         providers = provider_index.providers_for(vdep)
@@ -3216,16 +3209,18 @@ class Spec(object):
 
         # If it's a virtual dependency, try to find an existing
         # provider in the spec, and merge that.
-        if dep.virtual:
+        if spack.repo.path.is_virtual_safe(dep.name):
             visited.add(dep.name)
             provider = self._find_provider(dep, provider_index)
             if provider:
                 dep = provider
         else:
-            index = spack.provider_index.ProviderIndex([dep], restrict=True)
+            index = spack.provider_index.ProviderIndex(
+                repository=spack.repo.path, specs=[dep], restrict=True
+            )
             items = list(spec_deps.items())
             for name, vspec in items:
-                if not vspec.virtual:
+                if not spack.repo.path.is_virtual_safe(vspec.name):
                     continue
 
                 if index.providers_for(vspec):
@@ -3375,7 +3370,7 @@ class Spec(object):
         # Initialize index of virtual dependency providers if
         # concretize didn't pass us one already
         provider_index = spack.provider_index.ProviderIndex(
-            [s for s in all_spec_deps.values()], restrict=True
+            repository=spack.repo.path, specs=[s for s in all_spec_deps.values()], restrict=True
         )
 
         # traverse the package DAG and fill out dependencies according
@@ -3753,8 +3748,12 @@ class Spec(object):
                 return False
 
         # For virtual dependencies, we need to dig a little deeper.
-        self_index = spack.provider_index.ProviderIndex(self.traverse(), restrict=True)
-        other_index = spack.provider_index.ProviderIndex(other.traverse(), restrict=True)
+        self_index = spack.provider_index.ProviderIndex(
+            repository=spack.repo.path, specs=self.traverse(), restrict=True
+        )
+        other_index = spack.provider_index.ProviderIndex(
+            repository=spack.repo.path, specs=other.traverse(), restrict=True
+        )
 
         # This handles cases where there are already providers for both vpkgs
         if not self_index.satisfies(other_index):

lib/spack/spack/cmd/license.py
@@ -54,7 +54,6 @@ licensed_files = [
     r"^share/spack/.*\.fish$",
     r"^share/spack/qa/run-[^/]*$",
     r"^share/spack/bash/spack-completion.in$",
-    r"^share/spack/templates/misc/coconcretization.pyt$",
     # action workflows
     r"^.github/actions/.*\.py$",
     # all packages

lib/spack/spack/concretize.py
@@ -17,7 +17,6 @@ TODO: make this customizable and allow users to configure
 from __future__ import print_function
 
 import functools
-import os.path
 import platform
 import tempfile
 from contextlib import contextmanager
@@ -25,7 +24,6 @@ from itertools import chain
 
 import archspec.cpu
 
-import llnl.util.filesystem as fs
 import llnl.util.lang
 import llnl.util.tty as tty
 
@@ -753,37 +751,20 @@ def _concretize_specs_together_new(*abstract_specs, **kwargs):
 
 
 def _concretize_specs_together_original(*abstract_specs, **kwargs):
-    def make_concretization_repository(abstract_specs):
-        """Returns the path to a temporary repository created to contain
-        a fake package that depends on all of the abstract specs.
-        """
-        tmpdir = tempfile.mkdtemp()
-        repo_path, _ = spack.repo.create_repo(tmpdir)
-
-        debug_msg = "[CONCRETIZATION]: Creating helper repository in {0}"
-        tty.debug(debug_msg.format(repo_path))
-
-        pkg_dir = os.path.join(repo_path, "packages", "concretizationroot")
-        fs.mkdirp(pkg_dir)
-        environment = spack.tengine.make_environment()
-        template = environment.get_template("misc/coconcretization.pyt")
-
-        # Split recursive specs, as it seems the concretizer has issue
-        # respecting conditions on dependents expressed like
-        # depends_on('foo ^bar@1.0'), see issue #11160
-        split_specs = [
-            dep.copy(deps=False) for spec in abstract_specs for dep in spec.traverse(root=True)
-        ]
-
-        with open(os.path.join(pkg_dir, "package.py"), "w") as f:
-            f.write(template.render(specs=[str(s) for s in split_specs]))
-
-        return spack.repo.Repo(repo_path)
-
     abstract_specs = [spack.spec.Spec(s) for s in abstract_specs]
-    concretization_repository = make_concretization_repository(abstract_specs)
+    tmpdir = tempfile.mkdtemp()
+    builder = spack.repo.MockRepositoryBuilder(tmpdir)
+    # Split recursive specs, as it seems the concretizer has issue
+    # respecting conditions on dependents expressed like
+    # depends_on('foo ^bar@1.0'), see issue #11160
+    split_specs = [
+        dep.copy(deps=False) for spec1 in abstract_specs for dep in spec1.traverse(root=True)
+    ]
+    builder.add_package(
+        "concretizationroot", dependencies=[(str(x), None, None) for x in split_specs]
+    )
 
-    with spack.repo.additional_repository(concretization_repository):
+    with spack.repo.use_repositories(builder.root, override=False):
         # Spec from a helper package that depends on all the abstract_specs
         concretization_root = spack.spec.Spec("concretizationroot")
         concretization_root.concretize(tests=kwargs.get("tests", False))

lib/spack/spack/config.py
@@ -409,28 +409,22 @@ class Configuration(object):
     @_config_mutator
     def push_scope(self, scope):
         """Add a higher precedence scope to the Configuration."""
-        cmd_line_scope = None
-        if self.scopes:
-            highest_precedence_scope = list(self.scopes.values())[-1]
-            if highest_precedence_scope.name == "command_line":
-                # If the command-line scope is present, it should always
-                # be the scope of highest precedence
-                cmd_line_scope = self.pop_scope()
-
+        tty.debug("[CONFIGURATION: PUSH SCOPE]: {}".format(str(scope)), level=2)
         self.scopes[scope.name] = scope
-        if cmd_line_scope:
-            self.scopes["command_line"] = cmd_line_scope
 
     @_config_mutator
     def pop_scope(self):
         """Remove the highest precedence scope and return it."""
         name, scope = self.scopes.popitem(last=True)
+        tty.debug("[CONFIGURATION: POP SCOPE]: {}".format(str(scope)), level=2)
         return scope
 
     @_config_mutator
     def remove_scope(self, scope_name):
         """Remove scope by name; has no effect when ``scope_name`` does not exist"""
-        return self.scopes.pop(scope_name, None)
+        scope = self.scopes.pop(scope_name, None)
+        tty.debug("[CONFIGURATION: POP SCOPE]: {}".format(str(scope)), level=2)
+        return scope
 
     @property
     def file_scopes(self):

lib/spack/spack/main.py
@@ -577,7 +577,14 @@ def setup_main_options(args):
         spack.config.set("config:locks", args.locks, scope="command_line")
 
     if args.mock:
-        spack.repo.path = spack.repo.RepoPath(spack.paths.mock_packages_path)
+        import spack.util.spack_yaml as syaml
+
+        key = syaml.syaml_str("repos")
+        key.override = True
+        spack.config.config.scopes["command_line"].sections["repos"] = syaml.syaml_dict(
+            [(key, [spack.paths.mock_packages_path])]
+        )
+        spack.repo.path = spack.repo.create(spack.config.config)
 
     # If the user asked for it, don't check ssl certs.
     if args.insecure:

lib/spack/spack/patch.py
@@ -271,12 +271,13 @@ class UrlPatch(Patch):
         return data
 
 
-def from_dict(dictionary):
+def from_dict(dictionary, repository=None):
     """Create a patch from json dictionary."""
+    repository = repository or spack.repo.path
     owner = dictionary.get("owner")
     if "owner" not in dictionary:
         raise ValueError("Invalid patch dictionary: %s" % dictionary)
-    pkg_cls = spack.repo.path.get_pkg_class(owner)
+    pkg_cls = repository.get_pkg_class(owner)
 
     if "url" in dictionary:
         return UrlPatch(
@@ -329,7 +330,7 @@ class PatchCache(object):
 
     """
 
-    def __init__(self, data=None):
+    def __init__(self, repository, data=None):
         if data is None:
             self.index = {}
         else:
@@ -337,9 +338,11 @@ class PatchCache(object):
                 raise IndexError("invalid patch index; try `spack clean -m`")
             self.index = data["patches"]
 
+        self.repository = repository
+
     @classmethod
-    def from_json(cls, stream):
-        return PatchCache(sjson.load(stream))
+    def from_json(cls, stream, repository):
+        return PatchCache(repository=repository, data=sjson.load(stream))
 
     def to_json(self, stream):
         sjson.dump({"patches": self.index}, stream)
@@ -375,7 +378,7 @@ class PatchCache(object):
         # because it's the index key)
         patch_dict = dict(patch_dict)
         patch_dict["sha256"] = sha256
-        return from_dict(patch_dict)
+        return from_dict(patch_dict, repository=self.repository)
 
     def update_package(self, pkg_fullname):
         # remove this package from any patch entries that reference it.
@@ -397,8 +400,8 @@ class PatchCache(object):
             del self.index[sha256]
 
         # update the index with per-package patch indexes
-        pkg_cls = spack.repo.path.get_pkg_class(pkg_fullname)
-        partial_index = self._index_patches(pkg_cls)
+        pkg_cls = self.repository.get_pkg_class(pkg_fullname)
+        partial_index = self._index_patches(pkg_cls, self.repository)
         for sha256, package_to_patch in partial_index.items():
             p2p = self.index.setdefault(sha256, {})
             p2p.update(package_to_patch)
@@ -410,7 +413,7 @@ class PatchCache(object):
             p2p.update(package_to_patch)
 
     @staticmethod
-    def _index_patches(pkg_class):
+    def _index_patches(pkg_class, repository):
         index = {}
 
         # Add patches from the class
@@ -425,7 +428,7 @@ class PatchCache(object):
             for cond, dependency in conditions.items():
                 for pcond, patch_list in dependency.patches.items():
                     for patch in patch_list:
-                        dspec_cls = spack.repo.path.get_pkg_class(dependency.spec.name)
+                        dspec_cls = repository.get_pkg_class(dependency.spec.name)
                         patch_dict = patch.to_dict()
                         patch_dict.pop("sha256")  # save some space
                         index[patch.sha256] = {dspec_cls.fullname: patch_dict}

lib/spack/spack/provider_index.py
@@ -129,7 +129,7 @@ class _IndexBase(object):
 
 
 class ProviderIndex(_IndexBase):
-    def __init__(self, specs=None, restrict=False):
+    def __init__(self, repository, specs=None, restrict=False):
         """Provider index based on a single mapping of providers.
 
         Args:
@@ -143,17 +143,16 @@ class ProviderIndex(_IndexBase):
         TODO: as possible without overly restricting results, so it is
         TODO: not the best name.
         """
-        if specs is None:
-            specs = []
-
+        self.repository = repository
         self.restrict = restrict
         self.providers = {}
 
+        specs = specs or []
         for spec in specs:
             if not isinstance(spec, spack.spec.Spec):
                 spec = spack.spec.Spec(spec)
 
-            if spec.virtual:
+            if self.repository.is_virtual_safe(spec.name):
                 continue
 
             self.update(spec)
@@ -171,9 +170,10 @@ class ProviderIndex(_IndexBase):
             # Empty specs do not have a package
             return
 
-        assert not spec.virtual, "cannot update an index using a virtual spec"
+        msg = "cannot update an index passing the virtual spec '{}'".format(spec.name)
+        assert not self.repository.is_virtual_safe(spec.name), msg
 
-        pkg_provided = spec.package_class.provided
+        pkg_provided = self.repository.get_pkg_class(spec.name).provided
         for provided_spec, provider_specs in six.iteritems(pkg_provided):
             for provider_spec in provider_specs:
                 # TODO: fix this comment.
@@ -262,12 +262,12 @@ class ProviderIndex(_IndexBase):
 
     def copy(self):
         """Return a deep copy of this index."""
-        clone = ProviderIndex()
+        clone = ProviderIndex(repository=self.repository)
         clone.providers = self._transform(lambda vpkg, pset: (vpkg, set((p.copy() for p in pset))))
         return clone
 
     @staticmethod
-    def from_json(stream):
+    def from_json(stream, repository):
         """Construct a provider index from its JSON representation.
 
         Args:
@@ -281,7 +281,7 @@ class ProviderIndex(_IndexBase):
         if "provider_index" not in data:
             raise ProviderIndexError("YAML ProviderIndex does not start with 'provider_index'")
 
-        index = ProviderIndex()
+        index = ProviderIndex(repository=repository)
         providers = data["provider_index"]["providers"]
         index.providers = _transform(
             providers,

commit 3cccb21720e18cc05c821dd8b74a5fd63b8efc62

concrete specs are not virtual
Checking this ahead of time avoids hitting the repo cache.

Should make bootstrapping of gnupg in CI fast again, since it regressed
in de8c827.

lib/spack/spack/spec.py
@@ -1647,7 +1647,7 @@ class Spec(object):
 
     @property
     def virtual(self):
-        return spack.repo.path.is_virtual(self.name)
+        return not self.concrete and spack.repo.path.is_virtual(self.name)
 
     @property
     def concrete(self):