e7067319de32ba3801c7cda0027fa48f601fe47f
Victoria Cherkas <87643948+victoria-cherkas@users.noreply.github.com>
2022-11-25 23:54:51 +0100

    metkit, fdb: Add latest versions (#33289)

--- var/spack/repos/builtin/packages/fdb/package.py
+++ var/spack/repos/builtin/packages/fdb/package.py
@@ -12,9 +12,13 @@ class Fdb(CMakePackage):
 
     homepage = "https://github.com/ecmwf/fdb"
     url = "https://github.com/ecmwf/fdb/archive/refs/tags/5.7.8.tar.gz"
+    git = "https://github.com/ecmwf/fdb.git"
 
     maintainers = ["skosukhin"]
 
+    # master version of fdb is subject to frequent changes and is to be used experimentally.
+    version("master", branch="master")
+    version("5.10.8", sha256="6a0db8f98e13c035098dd6ea2d7559f883664cbf9cba8143749539122ac46099")
     version("5.7.8", sha256="6adac23c0d1de54aafb3c663d077b85d0f804724596623b381ff15ea4a835f60")
 
     variant("tools", default=True, description="Build the command line tools")
@@ -40,6 +44,8 @@ class Fdb(CMakePackage):
     depends_on("eckit+admin", when="+tools")
 
     depends_on("eccodes@2.10:")
+    depends_on("eccodes+tools", when="+tools")
+
     depends_on("metkit@1.5:+grib")
 
     depends_on("lustre", when="backends=lustre")
