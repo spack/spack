f23b073bb8f59aa6abf599becfd755685c73a575
Rutherford, Cameron <robert.rutherford@pnnl.gov>
2022-11-25 16:21:23 -0800

    Fix dependency specification for CuSolver variant in HiOp.

--- var/spack/repos/builtin/packages/hiop/package.py
+++ var/spack/repos/builtin/packages/hiop/package.py
@@ -63,7 +63,7 @@ class Hiop(CMakePackage, CudaPackage, ROCmPackage):
         description="Ultra safety checks - " "used for increased robustness and self-diagnostics",
     )
     variant("ginkgo", default=False, description="Enable/disable ginkgo solver")
-    variant("cusolver", default=False, description="Enable/disable cuSovler")
+    variant("cusolver_lu", default=False, when="@0.7.1:", description="Enable/disable cuSovler")
 
     depends_on("lapack")
     depends_on("blas")
@@ -97,6 +97,7 @@ class Hiop(CMakePackage, CudaPackage, ROCmPackage):
         depends_on("magma@{0}:".format(magma_v), when="@{0}:+rocm".format(hiop_v))
 
     depends_on("cuda@11:", when="@develop:+cuda")
+    conflicts("+cusolver_lu", when="~cuda", msg="Cusolver requires CUDA")
 
     depends_on("raja", when="+raja")
     depends_on("umpire", when="+raja")
@@ -111,6 +112,7 @@ class Hiop(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("hipsparse", when="+rocm")
 
     depends_on("suite-sparse", when="+kron")
+    depends_on("suite-sparse", when="+cusolver_lu")
 
     depends_on("coinhsl+blas", when="+sparse")
     depends_on("metis", when="+sparse")
@@ -122,8 +124,6 @@ class Hiop(CMakePackage, CudaPackage, ROCmPackage):
         when="+cuda+raja",
         msg="umpire+cuda exports device code and requires static libs",
     )
-    conflicts("+cusolver", when="~cuda", msg="Cusolver requires CUDA")
-    conflicts("+cusolver", when="@:0.5", msg="Cusolver support was introduced in HiOp 0.6")
 
     flag_handler = build_system_flags
 
@@ -159,7 +159,7 @@ class Hiop(CMakePackage, CudaPackage, ROCmPackage):
                 self.define_from_variant("HIOP_USE_COINHSL", "sparse"),
                 self.define_from_variant("HIOP_TEST_WITH_BSUB", "jsrun"),
                 self.define_from_variant("HIOP_USE_GINKGO", "ginkgo"),
-                self.define_from_variant("HIOP_USE_CUSOLVER", "cusolver"),
+                self.define_from_variant("HIOP_USE_CUSOLVER_LU", "cusolver_lu"),
             ]
         )
 
