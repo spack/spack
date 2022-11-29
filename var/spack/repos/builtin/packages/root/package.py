eb5153b35711448e7a9b2d626d5933be6f4a92a8
Hadrien Grasland <grasland@lal.in2p3.fr>
2022-11-29 17:59:15 +0100

    More logical ordering

--- var/spack/repos/builtin/packages/root/package.py
+++ var/spack/repos/builtin/packages/root/package.py
@@ -233,8 +233,8 @@ class Root(CMakePackage):
 
     # Python
     depends_on("python@2.7:", when="+python", type=("build", "run"))
-    depends_on("python@2.7:3.6", when=":6.13 +python", type=("build", "run"))
     depends_on("python@2.7:3.10", when=":6.26.09 +python", type=("build", "run"))
+    depends_on("python@2.7:3.6", when=":6.13 +python", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"), when="+tmva")
     # This numpy dependency was not intended and will hopefully
     # be fixed in 6.20.06.
