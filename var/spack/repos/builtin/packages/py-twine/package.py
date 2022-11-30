99c2e86b1bd42d8e36ccd4836eba85da75e6d46a
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-11-30 01:19:37 +0100

    py-twine: add 4.0.1

--- var/spack/repos/builtin/packages/py-twine/package.py
+++ var/spack/repos/builtin/packages/py-twine/package.py
@@ -11,12 +11,26 @@ class PyTwine(PythonPackage):
 
     homepage = "https://twine.readthedocs.io/"
     pypi = "twine/twine-2.0.0.tar.gz"
+    git = "https://github.com/pypa/twine.git"
 
+    version("4.0.1", sha256="96b1cf12f7ae611a4a40b6ae8e9570215daff0611828f5fe1f37a16255ab24a0")
     version("2.0.0", sha256="9fe7091715c7576df166df8ef6654e61bada39571783f2fd415bdcba867c6993")
 
+    depends_on("python@3.7:", when="@4:", type=("build", "run"))
     depends_on("python@3.6:", type=("build", "run"))
-    depends_on("py-pkginfo@1.4.2:", type=("build", "run"))
-    depends_on("py-readme-renderer@21.0:", type=("build", "run"))
-    depends_on("py-requests-toolbelt@0.8.0:0.8,0.9.1:", type=("build", "run"))
+    depends_on("py-setuptools@45:", when="@3.4.1:", type="build")
     depends_on("py-setuptools@0.7.0:", type=("build", "run"))
-    depends_on("py-tqdm@4.14:", type=("build", "run"))
+    depends_on("py-setuptools-scm+toml@6:", when="@3.4.2:", type="build")
+
+    depends_on("py-pkginfo@1.8.1:", when="@3.7:", type=("build", "run"))
+    depends_on("py-pkginfo@1.4.2:", type=("build", "run"))
+    depends_on("py-readme-renderer@35:", when="@4.0.1:", type=("build", "run"))
+    depends_on("py-readme-renderer@21.0:", type=("build", "run"))
+    depends_on("py-requests@2.20:", type=("build", "run"))
+    depends_on("py-requests-toolbelt@0.8.0:0.8,0.9.1:", type=("build", "run"))
+    depends_on("py-urllib3@1.26:", when="@3.8:", type=("build", "run"))
+    depends_on("py-importlib-metadata@3.6:", when="@3.4:", type=("build", "run"))
+    depends_on("py-keyring@15.1:", when="@3:", type=("build", "run"))
+    depends_on("py-rfc3986@1.4:", when="@3.2:", type=("build", "run"))
+    depends_on("py-rich@12:", when="@4:", type=("build", "run"))
+    depends_on("py-tqdm@4.14:", when="@:3", type=("build", "run"))
