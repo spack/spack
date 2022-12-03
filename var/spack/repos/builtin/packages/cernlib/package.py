ac017e085483222db441d1ef2e4c340ea3183e7c
andriish <andriish@users.noreply.github.com>
2022-12-03 16:54:49 +0100

    Update package.py

--- var/spack/repos/builtin/packages/cernlib/package.py
+++ var/spack/repos/builtin/packages/cernlib/package.py
@@ -3,22 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install cernlib
-#
-# You can edit this file again by typing:
-#
-#     spack edit cernlib
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
@@ -33,10 +17,9 @@ class Cernlib(CMakePackage):
 
     version("2022.11.08.0-free", sha256="733d148415ef78012ff81f21922d3bf641be7514b0242348dd0200cf1b003e46")
 
-    # FIXME: Add dependencies if required.
     depends_on("motif")
     depends_on("libx11")
 
     def cmake_args(self):
-        args = []
+        args = ["-DCERNLIB_BUILD_SHARED:BOOL=ON"]
         return args
