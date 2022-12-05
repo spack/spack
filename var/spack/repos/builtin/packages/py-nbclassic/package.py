d232e38117b0b9ed837094efd953844ac925c642
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-12-05 12:52:01 +0100

    py-nbclassic: add 0.4.8 and new package py-notebook-shim

--- var/spack/repos/builtin/packages/py-nbclassic/package.py
+++ var/spack/repos/builtin/packages/py-nbclassic/package.py
@@ -12,13 +12,19 @@ class PyNbclassic(PythonPackage):
     homepage = "https://github.com/jupyterlab/nbclassic"
     pypi = "nbclassic/nbclassic-0.3.1.tar.gz"
 
+    version("0.4.8", sha256="c74d8a500f8e058d46b576a41e5bc640711e1032cf7541dde5f73ea49497e283")
     version("0.3.5", sha256="99444dd63103af23c788d9b5172992f12caf8c3098dd5a35c787f0df31490c29")
     version("0.3.1", sha256="f920f8d09849bea7950e1017ff3bd101763a8d68f565a51ce053572e65aa7947")
 
+    depends_on("python@3.7:", when="@0.3.6:", type=("build", "run"))
     depends_on("python@3.6:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     # TODO: replace this after concretizer learns how to concretize separate build deps
     depends_on("py-jupyter-packaging11", when="@0.3.3:", type="build")
     # depends_on('py-jupyter-packaging@0.9:1', when='@0.3.3:', type='build')
-    depends_on("py-jupyter-server@1.8:1", type=("build", "run"))
-    depends_on("py-notebook@:6", type=("build", "run"))
+    depends_on("py-babel", when="@0.4:", type="build")
+    depends_on("py-jupyter-server@1.17:", when="@0.4:", type=("build", "run"))
+    depends_on("py-jupyter-server@1.8:", when="@0.3.7", type=("build", "run"))
+    depends_on("py-jupyter-server@1.8:1", when="@:0.3.6", type=("build", "run"))
+    depends_on("py-notebook-shim@0.1:", when="@0.3.6:", type=("build", "run"))
+    depends_on("py-notebook@:6", when="@:0.3.7", type=("build", "run"))
