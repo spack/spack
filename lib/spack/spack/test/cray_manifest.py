aafa90b260cd279cb5ca72b8b4eac7d6febb1df3
Peter Josef Scheibel <scheibel1@llnl.gov>
2022-12-06 17:11:17 -0800

    add test to confirm that cray platform identifier is transformed to linux

--- lib/spack/spack/test/cray_manifest.py
+++ lib/spack/spack/test/cray_manifest.py
@@ -233,6 +233,30 @@ def test_generate_specs_from_manifest():
     assert openmpi_spec["hwloc"]
 
 
+def test_translate_cray_platform_to_linux(monkeypatch):
+    test_linux_platform = spack.platforms.test.Test("linux")
+
+    def the_host_is_linux():
+        return test_linux_platform
+
+    monkeypatch.setattr(spack.platforms, "host", the_host_is_linux)
+
+    cray_arch = JsonArchEntry(platform="cray", os="rhel8", target="x86_64").to_dict()
+    spec_json = JsonSpecEntry(
+        name="cray-mpich",
+        hash="craympichfakehashaaa",
+        prefix="/path/to/cray-mpich/",
+        version="1.0.0",
+        arch=cray_arch,
+        compiler=_common_compiler.spec_json(),
+        dependencies={},
+        parameters={},
+    ).to_dict()
+
+    (spec,) = entries_to_specs([spec_json]).values()
+    assert spec.architecture.platform == "linux"
+
+
 def test_translate_compiler_name():
     nvidia_compiler = JsonCompilerEntry(
         name="nvidia",
