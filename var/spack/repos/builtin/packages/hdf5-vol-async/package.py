b7f730b6cf0b9240f7b8b65a2be06096d32b327d
Larry Knox <lrknox@hdfgroup.org>
2022-12-08 06:37:34 -0600

    Update hdf5 vol async version (#34376)
    * Add version hdf5-vol-async@1.4

--- var/spack/repos/builtin/packages/hdf5-vol-async/package.py
+++ var/spack/repos/builtin/packages/hdf5-vol-async/package.py
@@ -17,6 +17,7 @@ class Hdf5VolAsync(CMakePackage):
     tags = ["e4s"]
 
     version("develop", branch="develop")
+    version("1.4", tag="v1.4")
     version("1.3", tag="v1.3")
     version("1.2", tag="v1.2")
     version("1.1", tag="v1.1")
