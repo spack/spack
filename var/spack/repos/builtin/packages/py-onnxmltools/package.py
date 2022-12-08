37feb466d23b0c9bee04bd949f592ecee998c84a
iarspider <iarspider@gmail.com>
2022-12-08 13:34:21 +0100

    Add checksum for py-onnxmltools 1.11.1

--- var/spack/repos/builtin/packages/py-onnxmltools/package.py
+++ var/spack/repos/builtin/packages/py-onnxmltools/package.py
@@ -12,6 +12,13 @@ class PyOnnxmltools(PythonPackage):
     homepage = "https://github.com/onnx/onnxmltools"
     pypi = "onnxmltools/onnxmltools-1.10.0.tar.gz"
 
+    # Source tarball not available on PyPI
+    version(
+        "1.11.1",
+        url="https://github.com/onnx/onnxmltools/archive/1.11.1.tar.gz",
+        sha256="a739dc2147a2609eff2b2aad4a423b9795a49557c6b4c55b15c9ee323b4a01b7"
+    )
+
     version("1.11.0", sha256="174b857edcc2e4c56adbc7aed5234fff6a0f51a45956eb4c05c9f842c98bfa1f")
     version("1.10.0", sha256="4eb4605f18ed66553fc17438ac8cf5406d66dcc624bedd76d8067e1b08e6c75d")
 
