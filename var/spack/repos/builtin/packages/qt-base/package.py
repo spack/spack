dbb1791a6903f36232cd7f2c3f8e85c8a0387644
Sinan <sbulutw@gmail.com>
2022-12-09 11:47:29 -0800

    package/qt-base: add conflict for older gcc (#34420)

--- var/spack/repos/builtin/packages/qt-base/package.py
+++ var/spack/repos/builtin/packages/qt-base/package.py
@@ -82,6 +82,9 @@ class QtBase(CMakePackage):
         depends_on("libproxy")
         depends_on("openssl")
 
+    # Qt6 requires newer compilers: see https://github.com/spack/spack/issues/34418
+    conflicts("%gcc@:7")
+
     @property
     def archive_files(self):
         """Save both the CMakeCache and the config summary."""
