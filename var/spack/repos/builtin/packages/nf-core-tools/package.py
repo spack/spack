ab382e8ccf42bfb3ad7c6032492dd9a0b51beee5
Marco De La Pierre <marco.delapierre@gmail.com>
2022-12-09 11:01:40 +0800

    nf-core-tools: new version with extra dependency

--- var/spack/repos/builtin/packages/nf-core-tools/package.py
+++ var/spack/repos/builtin/packages/nf-core-tools/package.py
@@ -12,8 +12,9 @@ class NfCoreTools(PythonPackage):
     """
 
     homepage = "https://nf-co.re/tools"
-    pypi = "nf-core/nf-core-2.5.1.tar.gz"
+    pypi = "nf-core/nf-core-2.7.1.tar.gz"
 
+    version("2.7.1", sha256="90de62390314ef3141cee700667f017aa65c0346e40704a0f70d0662abcfb0db")
     version("2.6", sha256="47d4df906a60006249284bbf0bb84cdec48303a699c7c0d0a26f404a50e0811a")
     version("2.5.1", sha256="0303f6e3810ba1bc6ac843566ee9dea6b5edbf3527437dec5854b8c437456a4f")
 
@@ -23,12 +24,14 @@ class NfCoreTools(PythonPackage):
     depends_on("py-setuptools@40.6:", type="build")
 
     depends_on("py-click", type=("build", "run"))
+    depends_on("py-filetype", type=("build", "run"))
     depends_on("py-galaxy-tool-util", type=("build", "run"))
     depends_on("py-gitpython", type=("build", "run"))
     depends_on("py-jinja2", type=("build", "run"))
     depends_on("py-jsonschema@3.0:", type=("build", "run"))
     depends_on("py-markdown@3.3:", type=("build", "run"))
     depends_on("py-packaging", type=("build", "run"))
+    depends_on("py-pre-commit", type=("build", "run"))
     depends_on("py-prompt-toolkit@3.0.3:", type=("build", "run"))
     depends_on("py-pytest@7.0.0:", type=("build", "run"))
     depends_on("py-pytest-workflow@1.6.0:", type=("build", "run"))
