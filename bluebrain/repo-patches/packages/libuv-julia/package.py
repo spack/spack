ce526753a9c1e9edcab70447e84ef9581ee0dd0f
Matthias Wolf <matthias.wolf@epfl.ch>
2022-12-09 13:50:31 +0100

    julia: revert customization, remove external automake instead

--- bluebrain/repo-patches/packages/libuv-julia/package.py
+++
@@ -1,8 +0,0 @@
-from spack.package import *
-from spack.pkg.builtin.libuv_julia import LibuvJulia as BuiltinLibuvJulia
-
-
-class LibuvJulia(BuiltinLibuvJulia):
-    __doc__ = BuiltinLibuvJulia.__doc__
-
-    depends_on("automake@1.16:", type="build")
