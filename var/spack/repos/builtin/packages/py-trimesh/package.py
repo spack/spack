7a8d8f01686ff63ba95ac91e08025c3bef5238a0
heerener <116017729+heerener@users.noreply.github.com>
2022-12-12 16:47:53 +0000

    [@spackbot] updating style on behalf of heerener

--- var/spack/repos/builtin/packages/py-trimesh/package.py
+++ var/spack/repos/builtin/packages/py-trimesh/package.py
@@ -12,14 +12,22 @@ class PyTrimesh(PythonPackage):
     homepage = "https://github.com/mikedh/trimesh"
     pypi = "trimesh/trimesh-2.38.10.tar.gz"
 
-    version('3.17.1', sha256='025bb2fa3a2e87bdd6873f11db45a7ca19216f2f8b6aed29140fca57e32c298e', preferred=True)
-    version('2.38.10', sha256='866e73ea35641ff2af73867c891d7f9b90c75ccb8a3c1e8e06e16ff9af1f8c64', preferred=True)
+    version(
+        "3.17.1",
+        sha256="025bb2fa3a2e87bdd6873f11db45a7ca19216f2f8b6aed29140fca57e32c298e",
+        preferred=True,
+    )
+    version(
+        "2.38.10",
+        sha256="866e73ea35641ff2af73867c891d7f9b90c75ccb8a3c1e8e06e16ff9af1f8c64",
+        preferred=True,
+    )
 
-    depends_on('py-setuptools', type='build')
+    depends_on("py-setuptools", type="build")
 
-    depends_on('py-networkx', type='run')
-    depends_on('py-numpy', type='run')
-    depends_on('py-shapely', type='run')
-    depends_on('py-scipy', type='run')
-    depends_on('py-svgpath', type='run')
-    depends_on('py-lxml', type='run')
+    depends_on("py-networkx", type="run")
+    depends_on("py-numpy", type="run")
+    depends_on("py-shapely", type="run")
+    depends_on("py-scipy", type="run")
+    depends_on("py-svgpath", type="run")
+    depends_on("py-lxml", type="run")
