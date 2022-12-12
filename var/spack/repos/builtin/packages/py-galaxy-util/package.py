abfadbf7983ab22fc491987f48b7402d63c7ed4d
Marco De La Pierre <marco.delapierre@gmail.com>
2022-12-12 22:38:38 +0800

    nf-core-tools: adding also py-galaxy-util, as wheel

--- var/spack/repos/builtin/packages/py-galaxy-util/package.py
+++ var/spack/repos/builtin/packages/py-galaxy-util/package.py
@@ -7,19 +7,17 @@
 from spack.package import *
 
 
-class PyGalaxyToolUtil(PythonPackage):
-    """The Galaxy tool utilities."""
+class PyGalaxyUtil(PythonPackage):
+    """Galaxy Generic Utilities"""
 
     homepage = "https://github.com/galaxyproject/galaxy"
 
     version(
-        "22.1.5",
-        url="https://files.pythonhosted.org/packages/76/b0/4745ccb75fdb5c48a27a73900f8430119afe6c7c95b1aafed004f093788c/galaxy_tool_util-22.1.5-py2.py3-none-any.whl",
-        sha256="062a758024cda103d3d40ba232379c0c1853e8e181b58a65c4e954456cdcc040",
+        "22.1.2",
+        url="https://files.pythonhosted.org/packages/c6/6b/ca93c7a73a1d9c2153592007fd8264a357cb277de8d980ab6aa91c9cc8d1/galaxy_util-22.1.2-py2.py3-none-any.whl",
+        sha256="23c9ea814244dfb020e30ea3284d6dacfd9ba4119fb76c1cc2b3ab379463f43c",
         expand=False,
     )
 
     depends_on("python@3.7:3.10", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-
-    depends_on("py-galaxy-util", type=("build", "run"))
