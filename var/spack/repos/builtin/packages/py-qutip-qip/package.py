d9b8f35edf389e4adafdb5522cf7617fb73b1a28
Marco De La Pierre <marco.delapierre@gmail.com>
2022-12-12 23:05:19 +0800

    added recipes for py-qutip and py-qutip-qip

---
+++ var/spack/repos/builtin/packages/py-qutip-qip/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class PyQutipQip(PythonPackage):
+    """The QuTiP quantum information processing package"""
+
+    homepage = "https://github.com/qutip/qutip-qip"
+    pypi = "qutip-qip/qutip-qip-0.2.2.tar.gz"
+
+    version("0.2.2", sha256="c3cd49022684e5b929690c11d15118ab79cb2cd1073a8b6c9cbd2fefcc3ae73e")
+
+    depends_on("python@3:", type=("build", "run"))
+    depends_on("py-setuptools@42:", type="build")
+    depends_on("py-packaging", type="build")
+
+    depends_on("py-numpy@1.12:", type=("build", "run"))
+    depends_on("py-scipy@1.0:", type=("build", "run"))
+    depends_on("py-qutip@4.5:", type=("build", "run"))
+
+    depends_on("", type="run")
---
+++ var/spack/repos/builtin/packages/py-qutip/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class PyQutip(PythonPackage):
+    """QuTiP: The Quantum Toolbox in Python"""
+
+    homepage = "https://qutip.org/"
+    pypi = "qutip/qutip-4.7.0.tar.gz"
+
+    version("4.7.0", sha256="a9dde64457991ef1c5a7d4186b5348a16a71480a610f1c0902e4d656ddc12e31")
+
+    depends_on("python@3:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-packaging", type="build")
+
+    depends_on("py-cython@0.29.20:", type=("build", "run"))
+    depends_on("py-numpy@1.16.6:", type=("build", "run"))
+    depends_on("py-scipy@1.0:", type=("build", "run"))
+
