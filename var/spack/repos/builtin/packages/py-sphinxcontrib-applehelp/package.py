add19c554c4d638307fc364858fc3bd38e652c27
Manuela Kuhn <manuela.kuhn@ovgu.de>
2022-12-12 12:40:27 +0100

    py-sphinxcontrib-applehelp: add 1.0.2

--- var/spack/repos/builtin/packages/py-sphinxcontrib-applehelp/package.py
+++ var/spack/repos/builtin/packages/py-sphinxcontrib-applehelp/package.py
@@ -13,12 +13,14 @@ class PySphinxcontribApplehelp(PythonPackage):
 
     homepage = "http://sphinx-doc.org/"
     pypi = "sphinxcontrib-applehelp/sphinxcontrib-applehelp-1.0.1.tar.gz"
+    git = "https://github.com/sphinx-doc/sphinxcontrib-applehelp.git"
 
     # 'sphinx' requires 'sphinxcontrib-applehelp' at build-time, but
     # 'sphinxcontrib-applehelp' requires 'sphinx' at run-time. Don't bother trying to
     # import any modules for this package.
     import_modules = []  # type: List[str]
 
+    version("1.0.2", sha256="a072735ec80e7675e3f432fcae8610ecf509c5f1869d17e2eecff44389cdbc58")
     version("1.0.1", sha256="edaa0ab2b2bc74403149cb0209d6775c96de797dfd5b5e2a71981309efab3897")
 
     depends_on("python@3.5:", type=("build", "run"))
