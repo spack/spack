6902c52765d684f75f82150aa6e200e11a5ec598
Sam Grayson <sam@samgrayson.me>
2022-12-12 13:08:28 -0700

    Respond to comments

--- var/spack/repos/builtin/packages/py-azure-identity/package.py
+++ var/spack/repos/builtin/packages/py-azure-identity/package.py
@@ -23,18 +23,18 @@ class PyAzureIdentity(PythonPackage):
     version("1.3.1", sha256="5a59c36b4b05bdaec455c390feda71b6495fc828246593404351b9a41c2e877a")
     version("1.2.0", sha256="b32acd1cdb6202bfe10d9a0858dc463d8960295da70ae18097eb3b85ab12cb91")
 
-    # https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/identity/azure-identity/setup.py
+    # https://github.com/Azure/azure-sdk-for-python/blob/azure-identity_1.12.0/sdk/identity/azure-identity/setup.py
     depends_on("py-setuptools", type="build")
     with when("@1.12:"):
         depends_on("py-azure-core@1.11:1", type=("build", "run"))
         depends_on("py-cryptography@2.5:", type=("build", "run"))
-        depends_on("py-msal@1.20:1", type=("build", "run"))
+        depends_on("py-msal@1.12:1", type=("build", "run"))
         depends_on("py-msal-extensions@0.3:1", type=("build", "run"))
         depends_on("py-six@1.12:", type=("build", "run"))
 
     with when("@:1.11"):
-        depends_on("py-azure-core@1.0.0:1", type=("build", "run"))
+        depends_on("py-azure-core@1", type=("build", "run"))
         depends_on("py-cryptography@2.1.4:", type=("build", "run"))
-        depends_on("py-msal@1.0.0:1", type=("build", "run"))
+        depends_on("py-msal@1", type=("build", "run"))
         depends_on("py-msal-extensions@0.1.3:0.1", type=("build", "run"))
         depends_on("py-six@1.6:", type=("build", "run"))
