5565cfab36afe31ed88be6d67732a0dd45597217
heerener <116017729+heerener@users.noreply.github.com>
2022-12-12 13:31:41 +0000

    [@spackbot] updating style on behalf of heerener

--- var/spack/repos/builtin/packages/py-bmtk/package.py
+++ var/spack/repos/builtin/packages/py-bmtk/package.py
@@ -12,17 +12,17 @@ class PyBmtk(PythonPackage):
     homepage = "https://github.com/AllenInstitute/bmtk"
     pypi = "bmtk/bmtk-1.0.5.tar.gz"
 
-    version('1.0.7', sha256='11e85098cf3c940a3d64718645f4a24ee13c8a47438ef5d28e054cb27ee01702')
-    version('1.0.5', sha256='e0cb47b334467a6d124cfb99bbc67cc88f39f0291f4c39929f50d153130642a4')
+    version("1.0.7", sha256="11e85098cf3c940a3d64718645f4a24ee13c8a47438ef5d28e054cb27ee01702")
+    version("1.0.5", sha256="e0cb47b334467a6d124cfb99bbc67cc88f39f0291f4c39929f50d153130642a4")
 
-    depends_on('py-setuptools', type='build')
+    depends_on("py-setuptools", type="build")
 
-    depends_on('py-jsonschema',   type=('run'))
-    depends_on('py-pandas',       type=('run'))
-    depends_on('py-numpy',        type=('run'))
-    depends_on('py-six',          type=('run'))
-    depends_on('py-h5py',         type=('run'))
-    depends_on('py-matplotlib',   type=('run'))
-    depends_on('py-scipy',        type=('run'))
-    depends_on('py-scikit-image', type=('run'))
-    depends_on('py-sympy',        type=('run'))
+    depends_on("py-jsonschema", type=("run"))
+    depends_on("py-pandas", type=("run"))
+    depends_on("py-numpy", type=("run"))
+    depends_on("py-six", type=("run"))
+    depends_on("py-h5py", type=("run"))
+    depends_on("py-matplotlib", type=("run"))
+    depends_on("py-scipy", type=("run"))
+    depends_on("py-scikit-image", type=("run"))
+    depends_on("py-sympy", type=("run"))
