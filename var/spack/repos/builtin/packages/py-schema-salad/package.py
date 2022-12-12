d811161a4a3f07d0e180c5724ffd4e2146cc0124
Marco De La Pierre <marco.delapierre@gmail.com>
2022-12-12 16:45:46 +0800

    audit/style fixes, including adding missing dep package

--- var/spack/repos/builtin/packages/py-schema-salad/package.py
+++ var/spack/repos/builtin/packages/py-schema-salad/package.py
@@ -7,18 +7,28 @@
 from spack.package import *
 
 
-class Py(PythonPackage):
-    """
-    """
+class PySchemaSalad(PythonPackage):
+    """Schema Annotations for Linked Avro Data (SALAD)"""
 
-    homepage = ""
-    pypi = ""
+    homepage = "https://github.com/common-workflow-language/schema_salad"
+    pypi = "schema-salad/schema-salad-8.3.20221209165047.tar.gz"
 
-    version("", sha256="")
+    version("8.3.20221209165047", sha256="d97cc9a4d7c4255eb8000bcebaa8ac0d1d31801c921fd4113ab3051c1e326c7c")
 
-    depends_on("python", type=("build", "run"))
+    depends_on("python@3.6:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
 
-    depends_on("", type=("build", "run"))
+    depends_on("py-ruamel.yaml@0.17.6:0.17.21", when="^python@3.7:", type=("build", "run"))
+    depends_on("py-ruamel.yaml@0.16.12:0.17.21", type=("build", "run"))
+    depends_on("py-rdflib@4.2.2:6.999999", type=("build", "run"))
+    depends_on("py-rdflib@4.2.2:5.999999", when="^python@:3.6", type=("build", "run"))
+    depends_on("py-rdflib-jsonld@0.4.0:0.6.0", when="^python@:3.6", type=("build", "run"))
+    depends_on("py-mistune@2.0.3:2.0.999999", type=("build", "run"))
+    depends_on("py-cachecontrol@0.12.11+filecache", type=("build", "run"))
 
-    depends_on("", type="run")
+    depends_on("py-black@19.10b0:22.12", type=("build", "run"))
+
+    depends_on("py-mypy@0.991", type=("build", "run"))
+    depends_on("py-types-pkg-resources", type=("build", "run"))
+    depends_on("py-types-requests", type=("build", "run"))
+    depends_on("py-types-dataclasses", type=("build", "run"))
