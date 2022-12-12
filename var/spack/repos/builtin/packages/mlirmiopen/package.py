02b0a7e2579c83c9bd06b4f2f8fddcca236a78c3
sreenivasa murthy kolam <sreenivasamurthy.kolam@amd.com>
2022-12-12 12:37:25 +0000

    initial commit for 5.3.3 release

--- var/spack/repos/builtin/packages/mlirmiopen/package.py
+++ var/spack/repos/builtin/packages/mlirmiopen/package.py
@@ -11,12 +11,13 @@ class Mlirmiopen(CMakePackage):
     """Multi-Level Intermediate Representation for rocm miopen project."""
 
     homepage = "https://github.com/ROCmSoftwarePlatform/llvm-project-mlir"
-    url = "https://github.com/ROCmSoftwarePlatform/llvm-project-mlir/archive/refs/tags/rocm-5.2.3.tar.gz"
+    url = "https://github.com/ROCmSoftwarePlatform/llvm-project-mlir/archive/refs/tags/rocm-5.3.3.tar.gz"
     git = "https://github.com/ROCmSoftwarePlatform/llvm-project-mlir.git"
     tags = ["rocm"]
 
     maintainers = ["srekolam"]
 
+    version("5.3.3", sha256="e9aa407df775d00fdb9404689f69ac755575188f8b25c6bd0fa9599928c5c57f")
     version("5.2.3", sha256="29e1c352d203622fa083432d5d368caccb53ba141119fbb7e8d5247d99854625")
     version("5.2.1", sha256="9e305e05474076d84c78b7a796bca20b64c70ee3e2caa066c625216c5ee21d95")
     version("5.2.0", sha256="546121f203e7787d3501fbaf6673bdbeefbb39e0446b02c480454338362a1f01")
@@ -39,7 +40,7 @@ class Mlirmiopen(CMakePackage):
     depends_on("half")
     depends_on("pkgconfig", type="build")
 
-    for ver in ["5.1.0", "5.1.3", "5.2.0", "5.2.1", "5.2.3"]:
+    for ver in ["5.1.0", "5.1.3", "5.2.0", "5.2.1", "5.2.3", "5.3.3"]:
         depends_on("hip@" + ver, when="@" + ver)
         depends_on("llvm-amdgpu@" + ver, when="@" + ver)
         depends_on("hsa-rocr-dev@" + ver, when="@" + ver)
