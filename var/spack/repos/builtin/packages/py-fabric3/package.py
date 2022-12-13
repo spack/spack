e06dc064ed39722bbc6718d38474c57ad0735bf8
Marco De La Pierre <marco.delapierre@gmail.com>
2022-12-13 16:27:47 +0800

    one style fix

--- var/spack/repos/builtin/packages/py-fabric3/package.py
+++ var/spack/repos/builtin/packages/py-fabric3/package.py
@@ -8,13 +8,16 @@ from spack.package import *
 
 
 class PyFabric3(PythonPackage):
-    """Fabric is a simple, Pythonic tool for remote execution and deployment (py2.7/py3.4+ compatible fork).
+    """Fabric is a simple, Pythonic tool for
+    remote execution and deployment (py2.7/py3.4+ compatible fork).
     """
 
     homepage = "https://github.com/mathiasertl/fabric/"
     pypi = "fabric3/Fabric3-1.14.post1.tar.gz"
 
-    version("1.14.post1", sha256="647e485ec83f30b587862f92374d6affc217f3d79819d1d7f512e42e7ae51e81")
+    version(
+        "1.14.post1", sha256="647e485ec83f30b587862f92374d6affc217f3d79819d1d7f512e42e7ae51e81"
+    )
 
     depends_on("python", type=("build", "run"))
     depends_on("py-setuptools", type="build")
