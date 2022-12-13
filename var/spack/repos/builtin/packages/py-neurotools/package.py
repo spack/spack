9111adb2216ccf68e127c1289a4a80ef0394912b
Erik Heeren <erik.heeren@epfl.ch>
2022-12-13 08:54:30 +0100

    Process review remarks

--- var/spack/repos/builtin/packages/py-neurotools/package.py
+++ var/spack/repos/builtin/packages/py-neurotools/package.py
@@ -15,14 +15,11 @@ class PyNeurotools(PythonPackage):
 
     version("0.3.1", sha256="a459420fc0e9ff6b59af28716ddb0c75d11a63b8db80a5f4844e0d7a90c2c653")
 
-    depends_on("py-scipy", type="run")
-    depends_on("py-numpy", type="run")
-    depends_on("py-matplotlib", type="run")
-    depends_on("py-urllib3", type="run")
-    depends_on("py-mpi4py", type="run")
-    depends_on("py-tables", type="run")
-    depends_on("py-pyaml", type="run")
-    # py-interval is py2 only and most probably not used
-    # depends_on('py-interval', type='run')
+    depends_on("py-setuptools", type=("build", "run"))
+    depends_on("py-scipy", type=("build", "run"))
+    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-matplotlib", type=("build", "run"))
+    depends_on("py-tables", type=("build", "run"))
+    depends_on("py-pyaml", type=("build", "run"))
 
     patch("neurotools-0.3.1.patch", when="@0.3.1")
