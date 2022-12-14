d0ad4a8b4e0b02b01754939d8c68f04519e11255
Marco De La Pierre <marco.delapierre@gmail.com>
2022-12-14 17:20:58 +0800

    added newest versions (today) for nf-tower-agent and nf-tower-cli

--- var/spack/repos/builtin/packages/nf-tower-agent/package.py
+++ var/spack/repos/builtin/packages/nf-tower-agent/package.py
@@ -20,6 +20,12 @@ class NfTowerAgent(Package):
     if platform.machine() == "x86_64":
         if platform.system() == "Linux":
             version(
+                "0.5.0",
+                sha256="887f85aa9bb4688839c04b40887ce6446822ada7bdd858ec105cf44641ec8d2d",
+                url="https://github.com/seqeralabs/tower-agent/releases/download/v0.5.0/tw-agent-linux-x86_64",
+                expand=False,
+            )
+            version(
                 "0.4.5",
                 sha256="d3f38931ff769299b9f9f7e78d9f6a55f93914878c09117b8eaf5decd0c734ec",
                 url="https://github.com/seqeralabs/tower-agent/releases/download/v0.4.5/tw-agent-linux-x86_64",
