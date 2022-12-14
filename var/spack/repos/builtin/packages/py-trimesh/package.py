86aad230f4a5bb81598a661f4dd171dfad06f497
Erik Heeren <erik.heeren@epfl.ch>
2022-12-14 10:04:06 +0100

    py-trimesh: cleanup and optional dependencies

--- var/spack/repos/builtin/packages/py-trimesh/package.py
+++ var/spack/repos/builtin/packages/py-trimesh/package.py
@@ -12,20 +12,19 @@ class PyTrimesh(PythonPackage):
     homepage = "https://github.com/mikedh/trimesh"
     pypi = "trimesh/trimesh-2.38.10.tar.gz"
 
-    version(
-        "3.17.1",
-        sha256="025bb2fa3a2e87bdd6873f11db45a7ca19216f2f8b6aed29140fca57e32c298e",
-    )
-    version(
-        "2.38.10",
-        sha256="866e73ea35641ff2af73867c891d7f9b90c75ccb8a3c1e8e06e16ff9af1f8c64",
-    )
+    version("3.17.1", sha256="025bb2fa3a2e87bdd6873f11db45a7ca19216f2f8b6aed29140fca57e32c298e",)
+    version("2.38.10", sha256="866e73ea35641ff2af73867c891d7f9b90c75ccb8a3c1e8e06e16ff9af1f8c64",)
+
+    variant(
+        "easy",
+        default=False,
+        description="Install soft dependencies and unlock extra functionality")
 
     depends_on("py-setuptools@40.8:", type="build")
 
-    depends_on("py-networkx", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
-    depends_on("py-shapely", type=("build", "run"))
-    depends_on("py-scipy", type=("build", "run"))
-    depends_on("py-svgpath", type=("build", "run"))
-    depends_on("py-lxml", type=("build", "run"))
+    depends_on("py-networkx", type=("build", "run"), when="+easy")
+    depends_on("py-shapely", type=("build", "run"), when="+easy")
+    depends_on("py-scipy", type=("build", "run"), when="+easy")
+    depends_on("py-svgpath", type=("build", "run"), when="+easy")
+    depends_on("py-lxml", type=("build", "run"), when="+easy")
