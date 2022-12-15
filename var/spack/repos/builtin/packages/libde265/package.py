01e1983116730b2b5c11d8168fbf579747643d26
Ben Kirk <benkirk@ucar.edu>
2022-12-15 11:52:56 -0700

    libde265: an open source implementation of the h.265 video codec

---
+++ var/spack/repos/builtin/packages/libde265/package.py
@@ -0,0 +1,28 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Libde265(CMakePackage):
+    """libde265 is an open source implementation of the h.265 video codec.
+
+    It is written from scratch and has a plain C API to enable a simple integration into other software."""
+
+    homepage = "https://www.libde265.org"
+    url = "https://github.com/strukturag/libde265/archive/refs/tags/v1.0.9.tar.gz"
+
+    maintainers = ["benkirk"]
+
+    version("1.0.9", sha256="153554f407718a75f1e0ae197d35b43147ce282118a54f894554dbe27c32163d")
+
+    depends_on("cmake@3.13:", type="build")
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = []
+        return args
