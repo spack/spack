19046470d4213ad394e8920267f2ce2438cf247e
Ben Kirk <benkirk@ucar.edu>
2022-12-15 08:19:34 -0700

    GIMP: new package definition for building GIMP-2.10 from source

---
+++ var/spack/repos/builtin/packages/gimp/package.py
@@ -0,0 +1,76 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and otherargs
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack.package import *
+
+
+class Gimp(AutotoolsPackage):
+    """GIMP is a cross-platform image editor available for GNU/Linux,
+    macOS, Windows and more operating systems. It is free software,
+    you can change its source code and distribute your changes.
+
+    Whether you are a graphic designer, photographer, illustrator, or
+    scientist, GIMP provides you with sophisticated tools to get your job
+    done. You can further enhance your productivity with GIMP thanks to
+    many customization options and 3rd party plugins."""
+
+    homepage = "https://www.gimp.org"
+    url = "https://download.gimp.org/gimp/v2.10/gimp-2.10.32.tar.bz2"
+
+    maintainers = ["benkirk"]
+
+    version("2.10.32", sha256="3f15c70554af5dcc1b46e6dc68f3d8f0a6cc9fe56b6d78ac08c0fd859ab89a25")
+    version("2.10.30", sha256="88815daa76ed7d4277eeb353358bafa116cd2fcd2c861d95b95135c1d52b67dc")
+    version("2.10.28", sha256="4f4dc22cff1ab5f026feaa2ab55e05775b3a11e198186b47bdab79cbfa078826")
+    version("2.10.26", sha256="5ddbccf1db462a41df9a26197fcb0d24c7152753a36b3c8b8a9506b4136395f7")
+    version("2.10.24", sha256="bd1bb762368c0dd3175cf05006812dd676949c3707e21f4e6857435cb435989e")
+
+    variant("doc",         default=True,  description="build documentation with gtk-doc")
+    variant("ghostscript", default=False, description="build with ghostscript support")
+
+
+    # ref. https://www.gimp.org/source/
+    depends_on("pkgconfig", type="build")
+    depends_on("babl")
+    depends_on("fontconfig@2.12.4:")
+    depends_on("gegl")
+    depends_on("gexiv2")
+    depends_on("glib")
+    depends_on("glib-networking")
+    depends_on("gtk-doc", when="+doc")
+    depends_on("gtkplus@2.24.32:2.24.100")
+    depends_on("ghostscript", when="+ghostscript")
+    depends_on("intltool")
+    depends_on("jpeg")
+    depends_on("libexif")
+    #depends_on("libjxl")
+    depends_on("libmng")
+    depends_on("libmypaint@1.4.0")
+    depends_on("libpng")
+    depends_on("librsvg")
+    depends_on("libtiff")
+    depends_on("libxcursor")
+    depends_on("libxpm")
+    depends_on("mypaint-brushes@1.3.1")
+    depends_on("openexr")
+    depends_on("openjpeg")
+    depends_on("pango@1.29.4:")
+    depends_on("poppler+glib")
+    depends_on("poppler-data@0.4.7:")
+    depends_on("zlib")
+
+    def configure_args(self):
+        args = [
+            '--prefix={0}'.format(self.prefix),
+            '--disable-python',
+            '--without-webkit',
+            '--with-libxpm={0}'.format(self.spec['libxpm'].prefix),
+            'GIO_USE_TLS=gnutls',
+            'GIO_EXTRA_MODULES={0}/lib/gio/modules'.format(self.spec['glib-networking'].prefix),
+        ]
+        return args
+
+    def check(self):
+        """All build time checks open windows in the X server, don't do that"""
+        pass
