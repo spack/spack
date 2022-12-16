f4b66c07492be1b3438541301a691697dc24c35e
Adam J. Stewart <ajstewart426@gmail.com>
2022-12-16 15:58:11 -0600

    Update var/spack/repos/builtin/packages/py-bdbag/package.py

--- var/spack/repos/builtin/packages/py-bdbag/package.py
+++ var/spack/repos/builtin/packages/py-bdbag/package.py
@@ -17,7 +17,7 @@ class PyBdbag(PythonPackage):
 
     version("1.6.3", sha256="1ad2e4956045cb3d43a6276391ad919e42a90a2443727dbc5b1ac6eeb6d6e3c9")
 
-    depends_on("python@3.5:", type=("build", "run"))
+    depends_on("python@2.7:2,3.5:3", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools-scm@:5.999999", type="build")
 
