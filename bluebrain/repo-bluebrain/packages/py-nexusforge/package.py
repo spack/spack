b16a6c2499183bd90c9e6516b5cee3b1a7f2185a
MikeG <michael.gevaert@epfl.ch>
2022-12-20 10:00:32 +0100

    py-nexusforge: bump to 0.8.0, remove 0.6.3 and dependencies (#1793)
    * remove py-rdflib-jsonld, unshackle pyshacl
    * unpin py-prettytable
    
    Co-authored-by: Matthias Wolf <matthias.wolf@epfl.ch>

--- bluebrain/repo-bluebrain/packages/py-nexusforge/package.py
+++ bluebrain/repo-bluebrain/packages/py-nexusforge/package.py
@@ -25,28 +25,26 @@ class PyNexusforge(PythonPackage):
     homepage = "https://github.com/BlueBrain/nexus-forge"
     pypi = "nexusforge/nexusforge-0.6.3.tar.gz"
 
+    version('0.8.0', sha256='4358505ead26e41c2a0c4e6113cf3a486c9661e2a3899394497a2b5a94b70424')
     version('0.7.0', sha256='a8d2951d9ad18df9f2f4db31a4c18fcdd27bfcec929b03a3c91f133ea439413c')
-    version('0.6.3', sha256='ac97247509cf0e12ad4200511e0bd16d4789c0fa39450951ab54dea8c1aa9aa7')
 
     depends_on('py-setuptools', type='build')
-    depends_on('python@3.5:', when='@0.6.3', type=('build', 'run'))
-    depends_on('python@3.7:', when='@0.7.0', type=('build', 'run'))
+    depends_on('python@3.7:', type=('build', 'run'))
+
     depends_on('py-hjson', type=('build', 'run'))
     depends_on('py-pyyaml', type=('build', 'run'))
     depends_on('py-pandas', type=('build', 'run'))
     depends_on('py-nexus-sdk', type=('build', 'run'))
     depends_on('py-aiohttp', type=('build', 'run'))
     depends_on('py-pyld', type=('build', 'run'))
-    depends_on('py-pyshacl@0.11.6.post1', when='@0.6.3', type=('build', 'run'))
-    depends_on('py-pyshacl@0.17.2', when='@0.7.0', type=('build', 'run'))
-    depends_on('py-rdflib@:5.999', when='@0.6.3', type=('build', 'run'))
-    depends_on('py-rdflib@6.0.0:', when='@0.7.0', type=('build', 'run'))
-    depends_on('py-rdflib-jsonld@:0.6.099', when='@0.6.3', type=('build', 'run'))
+    depends_on('py-pyshacl@0.17.2', type=('build', 'run'))
+    depends_on('py-rdflib@6.0.0:', type=('build', 'run'))
     depends_on('py-nest-asyncio@1.5.1:', type=('build', 'run'))
-    depends_on('py-owlrl@5.2.3:', when='@0.7.0', type=('build', 'run'))
-    depends_on('py-pyparsing@2.0.2:', when='@0.7.0', type=('build', 'run'))
-    depends_on('py-elasticsearch-dsl@7.4.0:', when='@0.7.0', type=('build', 'run'))
+    depends_on('py-owlrl@5.2.3:', type=('build', 'run'))
+    depends_on('py-pyparsing@2.0.2:', type=('build', 'run'))
+    depends_on('py-elasticsearch-dsl@7.4.0:', type=('build', 'run'))
     depends_on('py-scikit-learn', type='run')
+
     depends_on('py-pytest', type='test')
     depends_on('py-pytest-cov', type='test')
     depends_on('py-tox', type='test')
