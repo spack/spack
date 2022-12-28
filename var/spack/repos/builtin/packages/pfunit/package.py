951de7507127932fc2202b02022dccd9e885cf3a
Bryce Allen <bdallen@uchicago.edu>
2022-12-28 15:10:25 -0500

    pfunit: fix max_array_rank for 4+, line len

--- var/spack/repos/builtin/packages/pfunit/package.py
+++ var/spack/repos/builtin/packages/pfunit/package.py
@@ -113,20 +113,25 @@ class Pfunit(CMakePackage):
             self.define_from_variant("BUILD_SHARED", "shared"),
             "-DCMAKE_Fortran_MODULE_DIRECTORY=%s" % spec.prefix.include,
             self.define_from_variant("BUILD_DOCS", "docs"),
-            "-DMAX_ASSERT_RANK=%s" % spec.variants["max_array_rank"].value,
         ]
 
         if self.spec.satisfies("%gcc@10:"):
             args.append("-DCMAKE_Fortran_FLAGS_DEBUG=-g -O2 -fallow-argument-mismatch")
+
+        if self.spec.satisfies("%gcc@5:"):
+            # prevents breakage when max_array_rank is larger than default
+            args.append("-DCMAKE_Fortran_FLAGS=-ffree-line-length-none")
 
         if spec.satisfies("@4.0.0:"):
             args.append("-DSKIP_MPI=%s" % ("YES" if "~mpi" in spec else "NO"))
             args.append("-DSKIP_OPENMP=%s" % ("YES" if "~openmp" in spec else "NO"))
             args.append("-DSKIP_FHAMCREST=%s" % ("YES" if "~fhamcrest" in spec else "NO"))
             args.append("-DSKIP_ESMF=%s" % ("YES" if "~esmf" in spec else "NO"))
+            args.append("-DMAX_ASSERT_RANK=%s" % spec.variants["max_array_rank"].value)
         else:
             args.append(self.define_from_variant("MPI", "mpi"))
             args.append(self.define_from_variant("OPENMP", "openmp"))
+            args.append("-DMAX_RANK=%s" % spec.variants["max_array_rank"].value)
 
         if spec.satisfies("+mpi"):
             args.extend(
