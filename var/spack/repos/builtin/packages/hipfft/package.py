bfca1bd1cfbd5667ca6a780af904b791aebc6402
Renjith Ravindran <Renjith.RavindranKannath@amd.com>
2022-12-28 16:46:00 -0800

    Setting lib install directory for hipfft

--- var/spack/repos/builtin/packages/hipfft/package.py
+++ var/spack/repos/builtin/packages/hipfft/package.py
@@ -114,5 +114,7 @@ class Hipfft(CMakePackage):
         elif self.spec.satisfies("@5.2.0:"):
             args.append(self.define("CMAKE_MODULE_PATH", self.spec["hip"].prefix.lib.cmake.hip))
             args.append(self.define("BUILD_FILE_REORG_BACKWARD_COMPATIBILITY", True))
+        if self.spec.satisfies("@5.3.0:"):
+            args.append(self.define("CMAKE_INSTALL_LIBDIR", "lib"))
 
         return args
