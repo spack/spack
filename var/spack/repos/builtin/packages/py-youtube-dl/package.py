0a35824b482c539cac4885ee028b154cfb1d9265
Glenn Johnson <glenn-johnson@uiowa.edu>
2022-12-30 23:53:15 -0600

    py-youtube-dl: add version 2021.12.17

--- var/spack/repos/builtin/packages/py-youtube-dl/package.py
+++ var/spack/repos/builtin/packages/py-youtube-dl/package.py
@@ -13,6 +13,9 @@ class PyYoutubeDl(PythonPackage):
     homepage = "https://github.com/ytdl-org/youtube-dl"
     pypi = "youtube_dl/youtube_dl-2020.3.24.tar.gz"
 
+    version(
+        "2021.12.17", sha256="bc59e86c5d15d887ac590454511f08ce2c47698d5a82c27bfe27b5d814bbaed2"
+    )
     version("2020.3.24", sha256="4b03efe439f7cae26eba909821d1df00a9a4eb82741cb2e8b78fe29702bd4633")
 
     depends_on("py-setuptools", type=("build", "run"))
