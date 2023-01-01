spack/spack

commit 9d936a2a756298a540f1c25fab46ccdc716980a4

singularity, apptainer: --without-conmon into @property config_options (#34474)
Per https://github.com/spack/spack/issues/34192, apptainer does not support `--without-conmon`, so we introduce a base class `config_options` property that can be overridden in the `apptainer` package.

var/spack/repos/builtin/packages/singularityce/package.py
@@ -70,12 +70,17 @@ class SingularityBase(MakefilePackage):
     def build_directory(self):
         return self.singularity_gopath_dir
 
+    # Allow overriding config options
+    @property
+    def config_options(self):
+        # Using conmon from spack
+        return ["--without-conmon"]
+
     # Hijack the edit stage to run mconfig.
     def edit(self, spec, prefix):
         with working_dir(self.build_directory):
             confstring = "./mconfig --prefix=%s" % prefix
-            # Using conmon from spack
-            confstring += " --without-conmon"
+            confstring += " ".join(config_options)
             if "~suid" in spec:
                 confstring += " --without-suid"
             if "~network" in spec:

var/spack/repos/builtin/packages/apptainer/package.py
@@ -39,15 +39,7 @@ class Apptainer(SingularityBase):
         "https://apptainer.org/docs/admin/main/admin_quickstart.html#apptainer-security",
     )
 
-    # This overrides SingularityBase (found in ../singularityce/package.py)
-    # Because Apptainer's mconfig has no option `--without-conmon`
-    # https://github.com/apptainer/apptainer/blob/v1.0.2/mconfig
-    def edit(self, spec, prefix):
-        with working_dir(self.build_directory):
-            confstring = "./mconfig --prefix=%s" % prefix
-            if "~suid" in spec:
-                confstring += " --without-suid"
-            if "~network" in spec:
-                confstring += " --without-network"
-            configure = Executable(confstring)
-            configure()
+    # Override config options from SingularityBase
+    @property
+    def config_options(self):
+        return []

commit b700aba08f226d6b2ba69f651f97accd363b9adc

singularity: bugfix add space between prefix and other options
This fixes two issues introduced in #34474: prefix got the next
option appended, and property was not resolved without the self.

Good intention for 2023: I shall check my PRs more thoroughly...

This PR now successfully builds an environment with both apptainer
and singularityce.

var/spack/repos/builtin/packages/singularityce/package.py
@@ -80,7 +80,7 @@ class SingularityBase(MakefilePackage):
     def edit(self, spec, prefix):
         with working_dir(self.build_directory):
             confstring = "./mconfig --prefix=%s" % prefix
-            confstring += " ".join(config_options)
+            confstring += " " + " ".join(self.config_options)
             if "~suid" in spec:
                 confstring += " --without-suid"
             if "~network" in spec: