a046641094e3fba9ee598d6123334cdeb19968b7
Ryan Krattiger <ryan.krattiger@kitware.com>
2023-01-02 15:46:05 -0600

    CI configuration boilerplate reduction and refactor
    Configuration:
    - New notation for list concatenation (prepend/append)
    - New notation for string concatenation (prepend/append)
    - Break out configuration files for: ci.yaml, cdash.yaml, view.yaml
    - Spack CI section refactored to improve self-consistency and
    composability
      - Scripts are now lists of lists and/or lists of strings
      - Job attributes are now listed under precedence ordered list that are
      composed/merged using Spack config merge rules.
      - "service-jobs" are identified explicitly rather than as a batch
    
    CI:
    - Consolidate common, platform, and architecture configurations for all CI stacks into composable configuration files
    - Make padding consistent across all stacks (256)
    - Merge all package -> runner mappings to be consistent across all
    stacks
    
    Unit Test:
    - Refactor CI module unit-tests for refactor configuration
    
    Docs:
    - Add docs for new notations in configuration.rst
    - Rewrite docs on CI pipelines to be consistent with refactored CI
    workflow

--- lib/spack/spack/environment/environment.py
+++ lib/spack/spack/environment/environment.py
@@ -2192,6 +2192,7 @@ class Environment(object):
             view = dict((name, view.to_dict()) for name, view in self.views.items())
         else:
             view = False
+
         yaml_dict["view"] = view
 
         if self.dev_specs:
