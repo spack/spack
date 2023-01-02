a252391cbc9a0baa0c06044d12b0407c0ee3c4b8
Rocco Meli <r.meli@bluemail.ch>
2023-01-02 13:26:54 +0100

    libxc: add v6.0.0 (#34621)

--- var/spack/repos/builtin/packages/libxc/package.py
+++ var/spack/repos/builtin/packages/libxc/package.py
@@ -13,6 +13,8 @@ class Libxc(AutotoolsPackage, CudaPackage):
     homepage = "https://tddft.org/programs/libxc/"
     url = "https://www.tddft.org/programs/libxc/down.php?file=2.2.2/libxc-2.2.2.tar.gz"
 
+    # Get checksum from latest release package at https://tddft.org/programs/libxc/download/
+    version("6.0.0", sha256="c2ca205a762200dfba2e6c9e8ca2061aaddc6b7cf42048859fe717a7aa07de7c")
     version("5.2.3", sha256="7b7a96d8eeb472c7b8cca7ac38eae27e0a8113ef44dae5359b0eb12592b4bcf2")
     version("5.1.7", sha256="1a818fdfe5c5f74270bc8ef0c59064e8feebcd66b8f642c08aecc1e7d125be34")
     version("5.1.5", sha256="02e4615a22dc3ec87a23efbd3d9be5bfad2445337140bad1720699571c45c3f9")
