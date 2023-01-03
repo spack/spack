8db1021e42f6f57f95558fd38b27523d50ad2e52
Alberto Invernizzi <invernizzi@cscs.ch>
2023-01-03 14:56:28 +0100

    add version 0.8.2

--- var/spack/repos/builtin/packages/neovim/package.py
+++ var/spack/repos/builtin/packages/neovim/package.py
@@ -17,6 +17,7 @@ class Neovim(CMakePackage):
 
     version("master", branch="master")
     version("stable", tag="stable")
+    version("0.8.2", sha256="c516c8db73e1b12917a6b2e991b344d0914c057cef8266bce61a2100a28ffcc9")
     version("0.8.0", sha256="505e3dfb71e2f73495c737c034a416911c260c0ba9fd2092c6be296655be4d18")
     version("0.7.2", sha256="ccab8ca02a0c292de9ea14b39f84f90b635a69282de38a6b4ccc8565bc65d096")
     version("0.7.0", sha256="792a9c55d5d5f4a5148d475847267df309d65fb20f05523f21c1319ea8a6c7df")
