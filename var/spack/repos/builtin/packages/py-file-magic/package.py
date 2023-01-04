54abe5bdea38178da8746ac857dd8cf90cb483ba
Sinan81 <Sinan81@users.noreply.github.com>
2023-01-04 05:40:19 +0000

    [@spackbot] updating style on behalf of Sinan81

--- var/spack/repos/builtin/packages/py-file-magic/package.py
+++ var/spack/repos/builtin/packages/py-file-magic/package.py
@@ -10,9 +10,9 @@ class PyFileMagic(PythonPackage):
     """This library is a Python ctypes interface to libmagic"""
 
     homepage = "https://pypi.org/project/file-magic/"
-    pypi     = "file-magic/file-magic-0.4.1.tar.gz"
+    pypi = "file-magic/file-magic-0.4.1.tar.gz"
 
-    version('0.4.1', sha256='a91d1483117f7ed48cd0238ad9be36b04824d57e9c38ea7523113989e81b9c53')
+    version("0.4.1", sha256="a91d1483117f7ed48cd0238ad9be36b04824d57e9c38ea7523113989e81b9c53")
 
-    depends_on('py-setuptools@61:', type='build')
-    depends_on('file', type='run')
+    depends_on("py-setuptools@61:", type="build")
+    depends_on("file", type="run")
