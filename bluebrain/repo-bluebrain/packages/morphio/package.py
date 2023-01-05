b32c8c6c6607b299a76dc01c1a443207f80b548a
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-01-05 11:34:10 +0100

    Build MorphIO without warnings, to avoid `-Werror`. (#1718)
    Build MorphIO without warnings, to avoid `-Werror`; and modernize formatting.

--- bluebrain/repo-bluebrain/packages/morphio/package.py
+++ bluebrain/repo-bluebrain/packages/morphio/package.py
@@ -10,8 +10,8 @@ class Morphio(CMakePackage):
     """Library for reading / writing morphology files"""
 
     homepage = "https://github.com/BlueBrain/MorphIO"
-    git      = "https://github.com/BlueBrain/MorphIO.git"
-    url      = "https://pypi.io/packages/source/m/morphio/MorphIO-3.3.2.tar.gz"
+    git = "https://github.com/BlueBrain/MorphIO.git"
+    url = "https://pypi.io/packages/source/m/morphio/MorphIO-3.3.2.tar.gz"
 
     version('develop', submodules=True)
 
@@ -32,6 +32,7 @@ class Morphio(CMakePackage):
         args = [
             '-DBUILD_BINDINGS:BOOL=OFF',
             '-DEXTERNAL_HIGHFIVE:BOOL=ON',
+            '-DMorphIO_CXX_WARNINGS=OFF',
         ]
         if self.spec.satisfies('+mpi'):
             args += [
