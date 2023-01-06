1081b69dd595223f96b059355ed76807e548a360
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-06 12:46:27 +0800

    Update var/spack/repos/builtin/packages/py-fastapi-utils/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-fastapi-utils/package.py
+++ var/spack/repos/builtin/packages/py-fastapi-utils/package.py
@@ -15,7 +15,7 @@ class PyFastapiUtils(PythonPackage):
 
     version("0.2.1", sha256="0e6c7fc1870b80e681494957abf65d4f4f42f4c7f70005918e9181b22f1bd759")
 
-    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("python@3.6:3", type=("build", "run"))
     depends_on("py-poetry@0.12:", type="build")
 
     depends_on("py-fastapi", type=("build", "run"))
