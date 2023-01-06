3b6809040f661746bbec06f85e5726b46525a074
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-06 09:48:28 +0800

    Update var/spack/repos/builtin/packages/py-paste/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-paste/package.py
+++ var/spack/repos/builtin/packages/py-paste/package.py
@@ -16,6 +16,6 @@ class PyPaste(PythonPackage):
     version("3.5.2", sha256="d5a7340c30bcdf3023dd0106c8a5c430dd8fe84aeb8113bc7b93f8dd729f4af6")
 
     depends_on("python@3:", type=("build", "run"))
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools", type=("build", "run"))
 
     depends_on("py-six@1.4.0:", type=("build", "run"))
