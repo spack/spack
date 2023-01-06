532b848ea200abb4e6930d79612ae1ef63503bd1
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-06 10:57:52 +0800

    updated qutip 2x versions

--- var/spack/repos/builtin/packages/py-qutip-qip/package.py
+++ var/spack/repos/builtin/packages/py-qutip-qip/package.py
@@ -13,12 +13,13 @@ class PyQutipQip(PythonPackage):
     homepage = "https://github.com/qutip/qutip-qip"
     pypi = "qutip-qip/qutip-qip-0.2.2.tar.gz"
 
+    version("0.2.3", sha256="b883e91e723b7d192e704d3280ecac222b961e2fc565e966ad140113e535309b")
     version("0.2.2", sha256="c3cd49022684e5b929690c11d15118ab79cb2cd1073a8b6c9cbd2fefcc3ae73e")
 
-    depends_on("python@3:", type=("build", "run"))
     depends_on("py-setuptools@42:", type="build")
     depends_on("py-packaging", type="build")
 
     depends_on("py-numpy@1.12:", type=("build", "run"))
     depends_on("py-scipy@1.0:", type=("build", "run"))
+
     depends_on("py-qutip@4.5:", type=("build", "run"))
--- var/spack/repos/builtin/packages/py-qutip/package.py
+++ var/spack/repos/builtin/packages/py-qutip/package.py
@@ -13,13 +13,12 @@ class PyQutip(PythonPackage):
     homepage = "https://qutip.org/"
     pypi = "qutip/qutip-4.7.0.tar.gz"
 
+    version("4.7.1", sha256="9a87178e68b145c2145b526caa943ccc8400a111325ced45bd17f9b893663af2")
     version("4.7.0", sha256="a9dde64457991ef1c5a7d4186b5348a16a71480a610f1c0902e4d656ddc12e31")
 
-    depends_on("python@3:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-packaging", type="build")
 
     depends_on("py-cython@0.29.20:", type=("build", "run"))
     depends_on("py-numpy@1.16.6:", type=("build", "run"))
     depends_on("py-scipy@1.0:", type=("build", "run"))
-
