96a87ae1a41aca1bfbf48bb5e24808d90de70b43
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-06 09:46:46 +0800

    Update var/spack/repos/builtin/packages/py-gunicorn/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-gunicorn/package.py
+++ var/spack/repos/builtin/packages/py-gunicorn/package.py
@@ -16,4 +16,4 @@ class PyGunicorn(PythonPackage):
     version("20.1.0", sha256="e0a968b5ba15f8a328fdfd7ab1fcb5af4470c28aaf7e55df02a99bc13138e6e8")
 
     depends_on("python@3.5:", type=("build", "run"))
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools@3:", type=("build", "run"))
