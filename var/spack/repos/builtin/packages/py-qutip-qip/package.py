72cc7d91a9d97e13a975568afcb225326d30a7ff
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-06 11:19:47 +0800

    py-qutip-qip: tarball url from github

--- var/spack/repos/builtin/packages/py-qutip-qip/package.py
+++ var/spack/repos/builtin/packages/py-qutip-qip/package.py
@@ -11,10 +11,12 @@ class PyQutipQip(PythonPackage):
     """The QuTiP quantum information processing package"""
 
     homepage = "https://github.com/qutip/qutip-qip"
-    pypi = "qutip-qip/qutip-qip-0.2.2.tar.gz"
+    url = "https://github.com/qutip/qutip-qip/archive/refs/tags/v0.2.2.tar.gz"
+    # using github for now, because pypi tarball is missing the VERSION file
+    #pypi = "qutip-qip/qutip-qip-0.2.2.tar.gz"
 
-    version("0.2.3", sha256="b883e91e723b7d192e704d3280ecac222b961e2fc565e966ad140113e535309b")
-    version("0.2.2", sha256="c3cd49022684e5b929690c11d15118ab79cb2cd1073a8b6c9cbd2fefcc3ae73e")
+    version("0.2.3", sha256="a6a3a549cf6983e3ecef2cf07d00be83c146321fb588e250a49d020788a4e590")
+    version("0.2.2", sha256="4a9c79bb31c2fb2c72428764b2a5f6d8b1c667cebc8257cce1395c7e87d11217")
 
     depends_on("py-setuptools@42:", type="build")
     depends_on("py-packaging", type="build")
