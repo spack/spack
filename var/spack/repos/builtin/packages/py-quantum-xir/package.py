8b97062afd5790aa9713f6496ba934b2136ed4f4
Marco De La Pierre <marco.delapierre@gmail.com>
2023-01-07 08:03:11 +0800

    py-quantum-xir with tarball from github

--- var/spack/repos/builtin/packages/py-quantum-xir/package.py
+++ var/spack/repos/builtin/packages/py-quantum-xir/package.py
@@ -11,9 +11,11 @@ class PyQuantumXir(PythonPackage):
     """XIR is an intermediate representation language for quantum circuits."""
 
     homepage = "https://github.com/XanaduAI/xir"
-    pypi = "quantum-xir/quantum-xir-0.2.1.tar.gz"
+    url = "https://github.com/XanaduAI/xir/archive/refs/tags/v0.2.1.tar.gz"
+    # using github for now, because pypi tarball is missing the requirements.txt file
+    # pypi = "quantum-xir/quantum-xir-0.2.1.tar.gz"
 
-    version("0.2.1", sha256="9de3bbfdac5efd23e83fe5d62d5ea684a2e3e55af90269de03827bdbcd26bbfa")
+    version("0.2.1", sha256="bfc28ff386a4a742973455b61eccc574517a994bb7cfd0ea0b9e7194dae55f31")
 
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
