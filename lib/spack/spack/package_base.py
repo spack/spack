2ec5bce4773ade52db8df5eb82951be55b72b0e4
Gregory Becker <becker33@llnl.gov>
2023-01-06 16:34:55 -0800

    fix package_base function signature

--- lib/spack/spack/package_base.py
+++ lib/spack/spack/package_base.py
@@ -912,7 +912,7 @@ class PackageBase(WindowsRPath, PackageViewMixin, metaclass=PackageMeta):
         """
         return self._implement_all_urls_for_version(version)[0]
 
-    def update_external_dependencies(self):
+    def update_external_dependencies(self, extendee_spec=None):
         """
         Method to override in package classes to handle external dependencies
         """
