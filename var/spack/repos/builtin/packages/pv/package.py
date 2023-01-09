51dc1f20cf39e56ba73872250d72f0d39480e9ce
Rémi Lacroix <remi.lacroix@idris.fr>
2023-01-10 00:07:36 +0100

    pv: Add version 1.6.20 (#34854)

--- var/spack/repos/builtin/packages/pv/package.py
+++ var/spack/repos/builtin/packages/pv/package.py
@@ -14,4 +14,5 @@ class Pv(AutotoolsPackage):
     homepage = "https://www.ivarch.com/programs/pv.shtml"
     url = "https://www.ivarch.com/programs/sources/pv-1.6.6.tar.bz2"
 
+    version("1.6.20", sha256="e831951eff0718fba9b1ef286128773b9d0e723e1fbfae88d5a3188814fdc603")
     version("1.6.6", sha256="608ef935f7a377e1439c181c4fc188d247da10d51a19ef79bcdee5043b0973f1")
