082dc073094a2a04829d441ab9d097d2aa190c87
MikeG <michael.gevaert@epfl.ch>
2023-01-10 08:54:49 +0100

    atlas-direction-vectors: bump to v0.1.4, drop v0.1.2 (#1801)
    * bump atlas-direction-vectors to v0.1.4, drop v0.1.2
    
    Co-authored-by: Matthias Wolf <matthias.wolf@epfl.ch>

--- bluebrain/repo-bluebrain/packages/py-atlas-direction-vectors/package.py
+++ bluebrain/repo-bluebrain/packages/py-atlas-direction-vectors/package.py
@@ -6,18 +6,17 @@ from spack.package import *
 class PyAtlasDirectionVectors(PythonPackage):
     """Commands to compute direction vectors in volumetric brain regions."""
     homepage = "https://github.com/BlueBrain/atlas-direction-vectors"
-    git      = "https://github.com/BlueBrain/atlas-direction-vectors.git"
-    pypi     = "atlas-direction-vectors/atlas-direction-vectors-0.1.2.tar.gz"
+    git = "https://github.com/BlueBrain/atlas-direction-vectors.git"
+    pypi = "atlas-direction-vectors/atlas-direction-vectors-0.1.2.tar.gz"
 
     version('develop', branch='main')
-    version('0.1.2', tag='d87ff0caa7e03f62fa03fcd3230c398c3f74f5b7f550ac204098befbba6b8f45')
+    version('0.1.4', sha256='5c856319e6b24a31f796e94ff62433c7c605d3af2d20de3361ecb8ae12f4033f')
 
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-atlas-commons@0.1.4:', type=('build', 'run'))
     depends_on('py-setuptools-scm', type='build')
     depends_on('py-click@7.0:', type=('build', 'run'))
     depends_on('py-numpy@1.15.0:', type=('build', 'run'))
-    depends_on('py-numpy-quaternion', type=('build', 'run'))
     depends_on('py-scipy@1.4.1:', type=('build', 'run'))
     depends_on('py-voxcell@3.0.0:', type=('build', 'run'))
     # note: this is an extra not declared in setup.py
@@ -25,10 +24,6 @@ class PyAtlasDirectionVectors(PythonPackage):
     # FIXME regiodesics requires an ancient boost
     # depends_on('regiodesics@0.1.0:', type='run')
     depends_on('py-pytest', type='test')
-
-    def patch(self):
-        # Purge version constraints caused by old (outdated) numba incompatibilities
-        filter_file(r'\[numba\]', '', 'setup.py')
 
     @run_after('install')
     @on_package_attributes(run_tests=True)
