7137664228acfd8e90e735788da6278cbec6b12f
Cameron Smith <smithc11@rpi.edu>
2023-01-10 12:01:22 -0500

    mfem: formatting

--- var/spack/repos/builtin/packages/mfem/package.py
+++ var/spack/repos/builtin/packages/mfem/package.py
@@ -805,18 +805,22 @@ class Mfem(Package, CudaPackage, ROCmPackage):
                 "apf_zoltan",
                 "spr",
             ]
-            pumi_dep_zoltan=[""]
-            pumi_dep_parmetis=[""]
+            pumi_dep_zoltan = [""]
+            pumi_dep_parmetis = [""]
             if "+zoltan" in spec["pumi"]:
                 pumi_dep_zoltan = ld_flags_from_dirs([spec["zoltan"].prefix.lib], ["zoltan"])
                 if "+parmetis" in spec["zoltan"]:
-                    pumi_dep_parmetis = ld_flags_from_dirs([spec["parmetis"].prefix.lib], ["parmetis"])
+                    pumi_dep_parmetis = ld_flags_from_dirs(
+                        [spec["parmetis"].prefix.lib], ["parmetis"]
+                    )
             options += [
                 "PUMI_OPT=-I%s" % spec["pumi"].prefix.include,
-                "PUMI_LIB=%s %s %s" % (
-                  ld_flags_from_dirs([spec["pumi"].prefix.lib], pumi_libs),
-                  pumi_dep_parmetis,
-                  pumi_dep_zoltan),
+                "PUMI_LIB=%s %s %s"
+                % (
+                    ld_flags_from_dirs([spec["pumi"].prefix.lib], pumi_libs),
+                    pumi_dep_parmetis,
+                    pumi_dep_zoltan,
+                ),
             ]
 
         if "+gslib" in spec:
