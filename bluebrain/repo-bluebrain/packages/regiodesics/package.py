142c64b7f626bafa956a071b8e596374f1483554
MikeG <michael.gevaert@epfl.ch>
2023-01-10 14:11:46 +0100

    bump regiodesics from 0.1.2 to 0.1.3 (#1804)
    * allows more modern boost

--- bluebrain/repo-bluebrain/packages/regiodesics/package.py
+++ bluebrain/repo-bluebrain/packages/regiodesics/package.py
@@ -13,11 +13,8 @@ class Regiodesics(CMakePackage):
     homepage = "https://bbpteam.epfl.ch/project/spaces/display/BBPNSE/Computing+neurons+direction+vectors"
     git = "ssh://git@bbpgitlab.epfl.ch/nse/archive/regiodesics.git"
 
-    submodules = True
+    version('develop')
+    version('0.1.3', tag='0.1.3')
 
-    version('0.1.2', tag='0.1.2')
-    version('0.1.1', tag='0.1.1')
-    version('0.1.0', tag='0.1.0')
-
-    depends_on('boost@:1.70.0')
+    depends_on('boost@1.70.0:')
     depends_on('openscenegraph')
