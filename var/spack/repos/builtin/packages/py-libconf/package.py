c23ed506f58744a159b143d13765c15da753cebf
vucoda <vucoda@gmail.com>
2023-01-12 04:13:06 +1030

    Update py-libconf package to newer major version (#34836)

--- var/spack/repos/builtin/packages/py-libconf/package.py
+++ var/spack/repos/builtin/packages/py-libconf/package.py
@@ -9,8 +9,9 @@ from spack.package import *
 class PyLibconf(PythonPackage):
     """A pure-Python libconfig reader/writer with permissive license"""
 
-    pypi = "libconf/libconf-1.0.1.tar.gz"
+    pypi = "libconf/libconf-2.0.1.tar.gz"
 
+    version("2.0.1", sha256="2f907258953ba60a95a82d5633726b47c81f2d5cf8d8801b092579016d757f4a")
     version("1.0.1", sha256="6dd62847bb69ab5a09155cb8be2328cce01e7ef88a35e7c37bea2b1a70f8bd58")
 
     depends_on("py-setuptools", type="build")
