4470e2211754036a32fb25ac564e7eb69bce627d
Christopher Christofi <77968333+ChristopherChristofi@users.noreply.github.com>
2023-01-11 17:50:00 +0000

    py-fasteners: add 0.15 (#34865)

--- var/spack/repos/builtin/packages/py-fasteners/package.py
+++ var/spack/repos/builtin/packages/py-fasteners/package.py
@@ -15,6 +15,7 @@ class PyFasteners(PythonPackage):
     version("0.18", sha256="cb7c13ef91e0c7e4fe4af38ecaf6b904ec3f5ce0dda06d34924b6b74b869d953")
     version("0.17.3", sha256="a9a42a208573d4074c77d041447336cf4e3c1389a256fd3e113ef59cf29b7980")
     version("0.16.3", sha256="b1ab4e5adfbc28681ce44b3024421c4f567e705cc3963c732bf1cba3348307de")
+    version("0.15", sha256="3a176da6b70df9bb88498e1a18a9e4a8579ed5b9141207762368a1017bf8f5ef")
     version("0.14.1", sha256="427c76773fe036ddfa41e57d89086ea03111bbac57c55fc55f3006d027107e18")
 
     depends_on("python@3.6:", when="@0.17:", type=("build", "run"))
