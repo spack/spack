0246b6a57a7643ec1599cd2cffd702826068ff57
Wileam Phan <wileam@phan.codes>
2023-01-12 12:14:38 -0500

    Add provides line in cuda-toolkit recipe

--- var/spack/repos/builtin/packages/cuda-toolkit/package.py
+++ var/spack/repos/builtin/packages/cuda-toolkit/package.py
@@ -460,6 +460,7 @@ class CudaToolkit(Package):
                 version(ver, sha256=pkg[0], url=pkg[1], expand=False, preferred=True)
             else:
                 version(ver, sha256=pkg[0], url=pkg[1], expand=False)
+        provides(f"cuda@{ver}", when=f"@{ver}")
 
     # macOS Mojave drops NVIDIA graphics card support -- official NVIDIA
     # drivers do not exist for Mojave. See
@@ -487,7 +488,7 @@ class CudaToolkit(Package):
     # see https://docs.nvidia.com/cuda/archive/11.3.1/cuda-gdb/index.html#common-issues-oss
     # see https://docs.nvidia.com/cuda/archive/11.4.0/cuda-gdb/index.html#release-notes
     depends_on("ncurses abi=5", type="run", when="@:11.3.99+dev")
-
+    
     provides("opencl@:1.2", when="@7:")
     provides("opencl@:1.1", when="@:6")
 
