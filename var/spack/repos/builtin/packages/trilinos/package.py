7b38a7e25d400cad15881c41008eb43ea27bc97d
Dominik Thoennes <dominik.thoennes@fau.de>
2023-01-13 18:13:15 +0100

    enable mumps solver in amesos2 if variant is set for trilinos

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -650,6 +650,7 @@ class Trilinos(CMakePackage, CudaPackage, ROCmPackage):
                 ),
                 define_from_variant("Amesos2_ENABLE_Basker", "basker"),
                 define_from_variant("Amesos2_ENABLE_LAPACK", "amesos2"),
+                define_from_variant('Amesos2_ENABLE_MUMPS', 'mumps'),
             ]
         )
 
