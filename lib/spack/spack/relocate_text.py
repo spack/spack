aa680dff884dd691de520f3081bb496eff191f96
Harmen Stoppels <me@harmenstoppels.nl>
2023-01-13 13:04:46 +0100

    license

--- lib/spack/spack/relocate_text.py
+++ lib/spack/spack/relocate_text.py
@@ -1,6 +1,7 @@
 # Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 """This module contains pure-Python classes and functions for replacing
 paths inside text files and binaries."""
