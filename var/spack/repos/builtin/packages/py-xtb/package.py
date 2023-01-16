c03d144786193a6bbfb770ab72a464f5c8a5ed05
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2023-01-16 11:12:28 +0100

    Update package files
    - use extends("python") instead of tampering with PYTHONPATH
    - use PyPI for downloading sdist of py-xtb
    - add simple-dftd3 0.7.0
    - add dftd4 3.5.0
    - remove --wrap-mode=nodownload from toml-f

--- var/spack/repos/builtin/packages/py-xtb/package.py
+++ var/spack/repos/builtin/packages/py-xtb/package.py
@@ -10,18 +10,19 @@ class PyXtb(PythonPackage):
     """Python API for the extended tight binding program package"""
 
     homepage = "https://xtb-python.readthedocs.org"
-    url = "https://github.com/grimme-lab/xtb-python/releases/download/v22.1/xtb-22.1.tar.gz"
+    pypi = "xtb/xtb-22.1.tar.gz"
+    git = "https://github.com/grimme-lab/xtb-python.git"
 
     maintainers = [
         *PythonPackage.maintainers,
         "awvwgk",
     ]
 
-    version("22.1", sha256="7a59e7b783fc6e8b7328f55211de681e535a83991b07c4bab73494063f5e9018")
+    version("22.1", "7a59e7b783fc6e8b7328f55211de681e535a83991b07c4bab73494063f5e9018")
 
     depends_on("pkgconfig", type="build")
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-cffi", type=("build", "run"))
-    depends_on("py-meson-python@0.11.0:", type="build")
-    depends_on("py-numpy", type="run")
+    depends_on("py-meson-python", type="build")
+    depends_on("py-numpy", type=("build", "run"))
     depends_on("xtb", type=("build", "run"))
--- var/spack/repos/builtin/packages/simple-dftd3/package.py
+++ var/spack/repos/builtin/packages/simple-dftd3/package.py
@@ -12,14 +12,23 @@ class SimpleDftd3(MesonPackage):
     """
 
     homepage = "https://dftd3.readthedocs.io"
-    url = "https://github.com/dftd3/simple-dftd3/releases/download/v0.6.0/s-dftd3-0.6.0-source.tar.xz"
+    url = "https://github.com/dftd3/simple-dftd3/releases/download/v0.7.0/s-dftd3-0.7.0.tar.xz"
     git = "https://github.com/dftd3/simple-dftd3.git"
 
     maintainers = ["awvwgk"]
 
     version("main", branch="main")
+    version("0.7.0", "5390b20102474a3c37240f38226f3a54a809b34bcb910317bdd109b8c2865f4e")
     version("0.6.0", "c057361565f570cb128489c70131487f71b6891a40e5292dfe37041596810dfe")
     version("0.5.1", "0411fdaebe438f652a970cb232ae3199c4cc840366ed05fda4c38e634632040d")
+
+    def url_for_version(self, version):
+        return (
+            "https://github.com/dftd3/simple-dftd3/releases/download/v{version}/"
+            + "s-dftd3-{version}-source.tar.xz"
+            if version < Version("0.7")
+            else "s-dftd3-{version}.tar.xz"
+        ).format(version=str(version))
 
     variant("openmp", default=True, description="Use OpenMP parallelisation")
     variant("python", default=False, description="Build Python extension module")
@@ -31,18 +40,11 @@ class SimpleDftd3(MesonPackage):
     depends_on("py-cffi", when="+python")
     depends_on("python@3.6:", when="+python")
 
+    extends("python", when="+python")
+
     def meson_args(self):
 
         return [
             "-Dopenmp={0}".format(str("+openmp" in self.spec).lower()),
             "-Dpython={0}".format(str("+python" in self.spec).lower()),
         ]
-
-    def setup_run_environment(self, env):
-        if "+python" in self.spec:
-            pydir = join_path(
-                self.prefix.lib,
-                "python" + str(self.spec["python"].version.up_to(2)),
-                "site-packages",
-            )
-            env.prepend_path("PYTHONPATH", pydir)
