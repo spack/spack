911e9edf07dc3ad457a49815a39603356ecd392b
Alec Scott <scott112@llnl.gov>
2023-01-16 03:46:20 -0800

    Add Go v1.19.5, v1.18.10, and remove deprecated versions (#34594)

--- var/spack/repos/builtin/packages/etcd/package.py
+++ var/spack/repos/builtin/packages/etcd/package.py
@@ -3,8 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-import platform
-
 from spack.package import *
 
 
@@ -15,21 +13,15 @@ class Etcd(Package):
     homepage = "https://etcd.io/"
     url = "https://github.com/etcd-io/etcd/archive/v3.4.7.tar.gz"
 
-    version("3.4.7", sha256="858f5ad8c830a66f6bd0cd19386deea64d374185b32f40650ba979e0a70b8b97")
-    version("3.4.6", sha256="e9ebd003f5545a05017a8dbdde236d6c9d25f98ee35f8ba237e57b75330664f9")
-    version("3.4.5", sha256="2888f73dc52ba89da470d9bd40b1348ffe8b3da51cd8fe8bff5a1a8db2e50d46")
-    version("3.4.4", sha256="46bcd0d034fe9cc6ae86a9f2a72bdc78761ca99bfd5ae4b96b24e4ad93fc627e")
-    version("3.3.20", sha256="a9fcd2a3343f7f5b99acae956dd7c4fe12f16772b660f16fa9c24368df002477")
+    maintainers = ["alecbcs"]
 
-    depends_on("go@:1.13.9")
+    version("3.4.23", sha256="055c608c4898d25f23aefbc845ff074bf5e8a07e61ed41dbd5cc4d4f59c93093")
 
-    def setup_run_environment(self, env):
-        if platform.machine() == "aarch64":
-            env.set("ETCD_UNSUPPORTED_ARCH", "arm64")
+    depends_on("go@1.19:")
 
     def setup_build_environment(self, env):
-        if platform.machine() == "aarch64":
-            env.set("ETCD_UNSUPPORTED_ARCH", "arm64")
+        # Point GOPATH at the top of the staging dir for the build step.
+        env.prepend_path("GOPATH", self.stage.path)
 
     def install(self, spec, prefix):
         make()
