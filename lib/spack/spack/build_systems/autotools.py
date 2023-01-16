a7e6ac8829e78b3bb228f1cdd20573f34ba21917
Gilles Gouaillardet <ggouaillardet@users.noreply.github.com>
2023-01-16 23:56:13 +0900

    fix libtool filter for Fujitsu compilers (#34916)
    Since legit regular expressions are matched, do not pass string=True
    to the filter
    
    Refs #34897

--- lib/spack/spack/build_systems/autotools.py
+++ lib/spack/spack/build_systems/autotools.py
@@ -427,15 +427,15 @@ To resolve this problem, please try the following:
             x.filter(regex="-nostdlib", repl="", string=True)
             rehead = r"/\S*/"
             for o in [
-                "fjhpctag.o",
-                "fjcrt0.o",
-                "fjlang08.o",
-                "fjomp.o",
-                "crti.o",
-                "crtbeginS.o",
-                "crtendS.o",
+                r"fjhpctag\.o",
+                r"fjcrt0\.o",
+                r"fjlang08\.o",
+                r"fjomp\.o",
+                r"crti\.o",
+                r"crtbeginS\.o",
+                r"crtendS\.o",
             ]:
-                x.filter(regex=(rehead + o), repl="", string=True)
+                x.filter(regex=(rehead + o), repl="")
         elif self.pkg.compiler.name == "dpcpp":
             # Hack to filter out spurious predep_objects when building with Intel dpcpp
             # (see https://github.com/spack/spack/issues/32863):
