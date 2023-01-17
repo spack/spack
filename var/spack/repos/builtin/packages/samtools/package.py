51388e5110f2f110d7d8b8f44133f918789c8d97
James Prewett <download@carc.unm.edu>
2023-01-17 13:57:44 -0700

    added dependency on htslib@1.16 (there is no htslib@1.16.1 as of now)

--- var/spack/repos/builtin/packages/samtools/package.py
+++ var/spack/repos/builtin/packages/samtools/package.py
@@ -41,6 +41,7 @@ class Samtools(Package):
     depends_on("python", type="run")
 
     # htslib became standalone @1.3.1, must use corresponding version
+    depends_on("htslib@1.16", when="@1.16.1")
     depends_on("htslib@1.15.1", when="@1.15.1")
     depends_on("htslib@1.15", when="@1.15")
     depends_on("htslib@1.14", when="@1.14")
