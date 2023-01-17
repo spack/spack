8ceb4e9a09b499638d73cbbe31e7d85693aba010
Harmen Stoppels <harmenstoppels@gmail.com>
2023-01-17 17:26:38 +0100

    python: 3.11.1, use -flto=thin when clang (#34951)

--- var/spack/repos/builtin/packages/python/package.py
+++ var/spack/repos/builtin/packages/python/package.py
@@ -44,6 +44,7 @@ class Python(Package):
     install_targets = ["install"]
     build_targets: List[str] = []
 
+    version("3.11.1", sha256="baed518e26b337d4d8105679caf68c5c32630d702614fc174e98cb95c46bdfa4")
     version("3.11.0", sha256="64424e96e2457abbac899b90f9530985b51eef2905951febd935f0e73414caeb")
     version(
         "3.10.8",
@@ -469,7 +470,11 @@ class Python(Package):
 
         if "+optimizations" in spec:
             config_args.append("--enable-optimizations")
-            config_args.append("--with-lto")
+            # Prefer thin LTO for faster compilation times.
+            if "@3.11.0: %clang@3.9:" in spec or "@3.11.0: %apple-clang@8:" in spec:
+                config_args.append("--with-lto=thin")
+            else:
+                config_args.append("--with-lto")
             config_args.append("--with-computed-gotos")
 
         if spec.satisfies("@3.7 %intel", strict=True):
