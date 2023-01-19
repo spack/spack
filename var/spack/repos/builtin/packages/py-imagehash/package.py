cf5ac1271765374850d0551e229715fa1e492348
Thomas Bouvier <contact@thomas-bouvier.io>
2023-01-19 18:01:50 +0100

    Use virtual package for `py-pillow`

--- var/spack/repos/builtin/packages/py-imagehash/package.py
+++ var/spack/repos/builtin/packages/py-imagehash/package.py
@@ -17,7 +17,7 @@ class PyImagehash(PythonPackage):
     version("4.3.1", sha256="7038d1b7f9e0585beb3dd8c0a956f02b95a346c0b5f24a9e8cc03ebadaf0aa70")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-pillow", type=("build", "run"))
+    depends_on("pil", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
     depends_on("py-pywavelets", type=("build", "run"))
