533ca41418f747db9a9d09ed06da3d0c7fdbf0ef
eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
2023-01-20 07:43:05 -0600

    hypre: add option for sycl variant (#34987)
    * hypre: add option for sycl variant
    
    * hypre +sycl conflicts with @:2.23
    
    * hypre +sycl: raise InstallError for unsupported compiler

--- var/spack/repos/builtin/packages/hypre/package.py
+++ var/spack/repos/builtin/packages/hypre/package.py
@@ -75,6 +75,7 @@ class Hypre(AutotoolsPackage, CudaPackage, ROCmPackage):
     variant("fortran", default=True, description="Enables fortran bindings")
     variant("gptune", default=False, description="Add the GPTune hookup code")
     variant("umpire", default=False, description="Enable Umpire support")
+    variant("sycl", default=False, description="Enable SYCL support")
 
     # Patch to add gptune hookup codes
     patch("ij_gptune.patch", when="+gptune@2.19.0")
@@ -138,6 +139,9 @@ class Hypre(AutotoolsPackage, CudaPackage, ROCmPackage):
 
     # Option added in v2.21.0
     conflicts("+umpire", when="@:2.20")
+
+    # Option added in v2.24.0
+    conflicts("+sycl", when="@:2.23")
 
     configure_directory = "src"
 
@@ -269,6 +273,15 @@ class Hypre(AutotoolsPackage, CudaPackage, ROCmPackage):
                     "--disable-rocsparse",
                 ]
             )
+
+        if "+sycl" in spec:
+            configure_args.append("--with-scyl")
+            sycl_compatible_compilers = ["dpcpp", "icpx"]
+            if not (os.path.basename(self.compiler.cxx) in sycl_compatible_compilers):
+                raise InstallError(
+                    "Hypre's SYCL GPU Backend requires DPC++ (dpcpp)"
+                    + " or the oneAPI CXX (icpx) compiler."
+                )
 
         if "+unified-memory" in spec:
             configure_args.append("--enable-unified-memory")
