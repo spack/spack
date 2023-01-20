df6154bba2002677da1108b6eac8deb4ae8827ba
AmintorDusko <amintor_dusko@hotmail.com>
2023-01-20 13:33:03 -0500

    enable cpp tests

--- var/spack/repos/builtin/packages/py-pennylane-lightning/package.py
+++ var/spack/repos/builtin/packages/py-pennylane-lightning/package.py
@@ -15,7 +15,6 @@ class PyPennylaneLightning(CMakePackage, PythonExtension):
     url = "https://github.com/PennyLaneAI/pennylane-lightning/archive/refs/tags/v0.28.0.tar.gz"
 
     maintainers = ["mlxd, AmintorDusko"]
-    phases = ["build_ext", "install"]
 
     version("develop", branch="master")
     version("0.28.0", sha256="f5849c2affb5fb57aca20feb40ca829d171b07db2304fde0a37c2332c5b09e18")
@@ -29,7 +28,7 @@ class PyPennylaneLightning(CMakePackage, PythonExtension):
     variant("verbose", default=False, description="Build with full verbosity")
 
     variant("cpptests", default=False, description="Build CPP tests")
-    variant("cppbenchmark", default=False, description="Build CPP benchmark examples")
+    variant("cppbenchmarks", default=False, description="Build CPP benchmark examples")
 
     variant(
         "build_type",
@@ -55,6 +54,18 @@ class PyPennylaneLightning(CMakePackage, PythonExtension):
     depends_on("py-numpy", type=("build", "run"), when="+python")
     depends_on("py-pybind11", type=("build"), when="+python")
     depends_on("py-pip", type="build", when="+python")
+    depends_on("py-wheel", type="build", when="+python")
+
+class CMakeBuilder(spack.build_systems.cmake.CMakeBuilder):
+    build_targets = ['runner', 'test']
+    phases = ("cmake", "build", "build_ext", "install")
+    build_directory = "build"
+
+    def __init__(self, spec):
+        super().__init__(spec)
+        if (not self.spec.variants["cpptests"].value):
+            self.build_targets = []
+            self.phases = ("build_ext", "install")
 
     def cmake_args(self):
         """
@@ -67,7 +78,7 @@ class PyPennylaneLightning(CMakePackage, PythonExtension):
             self.define_from_variant("ENABLE_BLAS", "blas"),
             self.define_from_variant("CMAKE_VERBOSE_MAKEFILE:BOOL", "verbose"),
             self.define_from_variant("BUILD_TESTS", "cpptests"),
-            self.define_from_variant("BUILD_BENCHMARKS", "cppbenchmark"),
+            self.define_from_variant("BUILD_BENCHMARKS", "cppbenchmarks"),
             self.define_from_variant("ENABLE_PYTHON", "python"),
         ]
 
@@ -82,18 +93,18 @@ class PyPennylaneLightning(CMakePackage, PythonExtension):
 
         return args
 
-    def build_ext(self, spec, prefix):
+    def build_ext(self, pkg, spec, prefix):
         if self.spec.variants["python"].value:
             cm_args = ";".join([s[2:] for s in self.cmake_args()])
             args = ["-i", f"--define={cm_args}"]
             build_ext = Executable(f"{self.spec['python'].command.path}" + " setup.py build_ext")
             build_ext(*args)
         else:
-            print("Python disabled")
+            print("Nothing to build: Python disabled")
 
-    def install(self, spec, prefix):
+    def install(self, pkg, spec, prefix):
         if self.spec.variants["python"].value:
             pip_args = std_pip_args + ["--prefix=" + prefix, "."]
             pip(*pip_args)
         else:
-            print("Python disabled")
+            print("Nothing to install: Python disabled")
