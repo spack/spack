8d2fb1d2062604c7177cc8fc7f5fd60455469e4c
Lee J. O'Riordan <lee@xanadu.ai>
2023-01-23 07:21:05 -0500

    Restructure PennyLane deps and order

--- var/spack/repos/builtin/packages/py-pennylane/package.py
+++ var/spack/repos/builtin/packages/py-pennylane/package.py
@@ -9,26 +9,34 @@ from spack.package import *
 
 class PyPennylane(PythonPackage):
     """PennyLane is a Python quantum machine learning library by Xanadu Inc."""
-
-    homepage = "https://github.com/XanaduAI/pennylane"
+    homepage = "https://docs.pennylane.ai/"
+    git = "https://github.com/PennyLaneAI/pennylane.git"
+    url = "https://github.com/PennyLaneAI/pennylane/archive/refs/tags/v0.28.0.tar.gz"
     pypi = "PennyLane/PennyLane-0.28.0.tar.gz"
 
-    maintainers = PythonPackage.maintainers + ["marcodelapierre"]
+    maintainers = PythonPackage.maintainers + ["mlxd", "marcodelapierre"]
 
     version("0.28.0", sha256="2a6100c00277c1eb59eab6856cdad7b1237e9d1fbda98b1e15020bd5a64b10a8")
 
     depends_on("python@3.8:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-
-    depends_on("py-numpy@:1.23", type=("build", "run"))
-    depends_on("py-scipy", type=("build", "run"))
-    depends_on("py-networkx", type=("build", "run"))
-    depends_on("py-retworkx", type=("build", "run"))
-    depends_on("py-autograd", type=("build", "run"))
-    depends_on("py-toml", type=("build", "run"))
-    depends_on("py-appdirs", type=("build", "run"))
     depends_on("py-semantic-version@2.7:", type=("build", "run"))
-    depends_on("py-autoray@0.3.1:", type=("build", "run"))
-    depends_on("py-cachetools", type=("build", "run"))
-    depends_on("py-pennylane-lightning@0.28:", type=("build", "run"))
-    depends_on("py-requests", type=("build", "run"))
+    depends_on("py-pip", type="build" )
+    depends_on("py-wheel", type="build")
+
+    depends_on("py-appdirs", type="run")
+    depends_on("py-autograd", type="run")
+    depends_on("py-autoray", type="run")
+    depends_on("py-cachetools", type="run")
+    depends_on("py-networkx", type="run")
+    depends_on("py-numpy@:1.23", type="run")
+    depends_on("py-pennylane-lightning@0.28:", type="run")
+    depends_on("py-retworkx", type="run")
+    depends_on("py-requests", type="run")
+    depends_on("py-scipy", type="run")
+    depends_on("py-toml", type="run")
+
+    # Test deps
+    depends_on("py-pytest", type="test")
+    depends_on("py-pytest-mock", type="test")
+    depends_on("py-flaky", type="test")
