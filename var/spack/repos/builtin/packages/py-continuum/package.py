c49329e6545c17c02ff301f7a6c1a5be362cb130
Thomas Bouvier <contact@thomas-bouvier.io>
2023-01-23 15:12:36 +0100

    `py-continuum`: add missing python dependency

--- var/spack/repos/builtin/packages/py-continuum/package.py
+++ var/spack/repos/builtin/packages/py-continuum/package.py
@@ -16,6 +16,8 @@ class PyContinuum(PythonPackage):
 
     version("1.2.7", sha256="baadcdbe2f5b3c05254307c73434a012f2a3d663e1be9469a03d4b82559e98e1")
 
+    depends_on("python@3.6:")
+
     depends_on("py-setuptools", type="build")
     depends_on("py-torch@1.2.0:", type=("build", "run"))
     depends_on("py-torchvision@0.4.0:", type=("build", "run"))
