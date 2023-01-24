40af1918714b079d064c9f798ebf3bb6f46cd8b7
Richard Berger <rberger@lanl.gov>
2023-01-24 13:29:39 -0700

    ports-of-call: add v1.4.0

--- var/spack/repos/builtin/packages/ports-of-call/package.py
+++ var/spack/repos/builtin/packages/ports-of-call/package.py
@@ -16,6 +16,7 @@ class PortsOfCall(CMakePackage):
     maintainers = ["rbberger"]
 
     version("main", branch="main")
+    version("1.4.0", sha256="e08ae556b7c30d14d77147d248d118cf5343a2e8c0847943385c602394bda0fa")
     version("1.3.0", sha256="54b4a62539c23b1a345dd87c1eac65f4f69db4e50336cd81a15a627ce80ce7d9")
     version(
         "1.2.0",
