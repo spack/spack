21d9b654539b70d7fa50d863792cfe7a429def94
Erik Heeren <erik.heeren@epfl.ch>
2023-01-24 11:23:56 +0100

    py-lazy: add 1.4 and 1.5

--- var/spack/repos/builtin/packages/py-lazy/package.py
+++ var/spack/repos/builtin/packages/py-lazy/package.py
@@ -11,6 +11,8 @@ class PyLazy(PythonPackage):
 
     pypi = "lazy/lazy-1.2.zip"
 
+    version("1.5", sha256="cb3d8612aa895a48afe8f08860573ba8ef5ee4fdbe1b3cd606c5f50a16152186")
+    version("1.4", sha256="2c6d27a5ab130fb85435320651a47403adcb37ecbcc501b0c6606391f65f5b43")
     version("1.2", sha256="127ea610418057b953f0d102bed83f2c367be13b59f8d0ddf3b8a86c7d31b970")
 
     depends_on("py-setuptools", type="build")
