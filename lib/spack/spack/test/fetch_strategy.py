80e84b0a3a14b37ddab8d1e8779208119b27f810
Harmen Stoppels <me@harmenstoppels.nl>
2023-01-25 14:57:06 +0100

    Print file summary on checksum validation failure
    Currently we print "sha256 checksum failed for [file]. Expected X but
    got Y".
    
    This PR extends that message with file size and contents info:
    
    "... but got Y. File size = 123456 bytes. Contents = b'abc...def'"
    
    That way we can immediately see if the file was downloaded only
    partially, or if we downloaded a text page instead of a binary, etc.

--- lib/spack/spack/test/fetch_strategy.py
+++ lib/spack/spack/test/fetch_strategy.py
@@ -5,7 +5,7 @@
 
 import pytest
 
-from spack.fetch_strategy import from_url_scheme
+import spack.fetch_strategy as fetch_strategy
 
 
 def test_fetchstrategy_bad_url_scheme():
@@ -13,4 +13,14 @@ def test_fetchstrategy_bad_url_scheme():
     unsupported scheme fails as expected."""
 
     with pytest.raises(ValueError):
-        fetcher = from_url_scheme("bogus-scheme://example.com/a/b/c")  # noqa: F841
+        fetcher = fetch_strategy.from_url_scheme("bogus-scheme://example.com/a/b/c")  # noqa: F841
+
+
+def test_filesummary(tmpdir):
+    p = str(tmpdir.join("xyz"))
+    with open(p, "wb") as f:
+        f.write(b"abcdefghijklmnopqrstuvwxyz")
+
+    assert fetch_strategy._filesummary(p, print_bytes=8) == (26, b"abcdefgh...stuvwxyz")
+    assert fetch_strategy._filesummary(p, print_bytes=13) == (26, b"abcdefghijklmnopqrstuvwxyz")
+    assert fetch_strategy._filesummary(p, print_bytes=100) == (26, b"abcdefghijklmnopqrstuvwxyz")
