fcbcd41a01223ab20f86d396ddcc372fefed1b63
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-01-25 07:52:43 -0700

    Update crtm and crtm-fix from spack/develop

--- var/spack/repos/builtin/packages/crtm-fix/package.py
+++ var/spack/repos/builtin/packages/crtm-fix/package.py
@@ -5,31 +5,34 @@
 
 import os
 
-from spack import *
+from spack.package import *
 
 
 class CrtmFix(Package):
-    """CRTM coeffecient files"""
+    """CRTM coefficient files"""
 
     homepage = "https://github.com/NOAA-EMC/crtm"
-    url = "ftp://ftp.ucar.edu/pub/cpaess/bjohns/fix_REL-2.3.0_emc.tgz"
+    url = "ftp://ftp.ssec.wisc.edu/pub/s4/CRTM/fix_REL-2.3.0_emc.tgz"
 
-    maintainers = ["kgerheiser"]
+    maintainers = [
+        "BenjaminTJohnson",
+        "edwardhartnett",
+        "AlexanderRichert-NOAA",
+        "Hang-Lei-NOAA",
+        "climbfuji",
+    ]
 
-    version("2.4.0", sha256="7924285b8aa25b0b864643f03e7f281ca1816958e24c76aa9531b3ca902bf6e9")
     version("2.4.0_emc", sha256="88d659ae5bc4434f7fafa232ff65b4c48442d2d1a25f8fc96078094fa572ac1a")
-    version("2.3.0_emc", sha256="fde73bb41c3c00666ab0eb8c40895e02d36fa8d7b0896c276375214a1ddaab8f")
+    version("2.3.0_emc", sha256="1452af2d1d11d57ef3c57b6b861646541e7042a9b0f3c230f9a82854d7e90924")
 
     variant("big_endian", default=True, description="Install big_endian fix files")
-    variant("little_endian", default=False,
-            description="Install little endian fix files")
+    variant("little_endian", default=False, description="Install little endian fix files")
     variant("netcdf", default=True, description="Install netcdf fix files")
 
-    conflicts("+big_endian", when="+little_endian",
-              msg="big_endian and little_endian conflict")
+    conflicts("+big_endian", when="+little_endian", msg="big_endian and little_endian conflict")
 
     def url_for_version(self, version):
-        url = "ftp://ftp.ucar.edu/pub/cpaess/bjohns/fix_REL-{}.tgz"
+        url = "ftp://ftp.ssec.wisc.edu/pub/s4/CRTM/fix_REL-{}.tgz"
         return url.format(version)
 
     def install(self, spec, prefix):
@@ -54,19 +57,22 @@ class CrtmFix(Package):
         # Remove the incorrect file, and install it as noACC,, then install
         # correct file under new name.
         if "+big_endian" in spec and spec.version == Version("2.4.0_emc"):
-            remove_path = join_path(os.getcwd(), "fix", "SpcCoeff",
-                                    "Big_Endian", "amsua_metop-c.SpcCoeff.bin")
+            remove_path = join_path(
+                os.getcwd(), "fix", "SpcCoeff", "Big_Endian", "amsua_metop-c.SpcCoeff.bin"
+            )
             fix_files.remove(remove_path)
 
             # This file is incorrect, install it as a different name.
-            install(join_path("fix", "SpcCoeff", "Big_Endian",
-                              "amsua_metop-c.SpcCoeff.bin"),
-                    join_path(self.prefix.fix, "amsua_metop-c.SpcCoeff.noACC.bin"))
+            install(
+                join_path("fix", "SpcCoeff", "Big_Endian", "amsua_metop-c.SpcCoeff.bin"),
+                join_path(self.prefix.fix, "amsua_metop-c.SpcCoeff.noACC.bin"),
+            )
 
             # This "Little_Endian" file is actually the correct one.
-            install(join_path("fix", "SpcCoeff", "Little_Endian",
-                              "amsua_metop-c_v2.SpcCoeff.bin"),
-                    join_path(self.prefix.fix, "amsua_metop-c.SpcCoeff.bin"))
+            install(
+                join_path("fix", "SpcCoeff", "Little_Endian", "amsua_metop-c_v2.SpcCoeff.bin"),
+                join_path(self.prefix.fix, "amsua_metop-c.SpcCoeff.bin"),
+            )
 
         for f in fix_files:
             install(f, self.prefix.fix)
