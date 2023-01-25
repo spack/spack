d3eb88aaa61de4dcf5268f721dc41d3f2b0be232
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2023-01-25 18:33:37 +0100

    Add packages for scine modules (#34969)
    * Add packages for scine modules
    
    - lbfgspp (dependency for scine-utilities)
    - scine-core
    - scine-utilities and py-scine-utilities (virtual)
    - scine-readuct and py-scine-readuct (virtual)
    - scine-sparrow and py-scine-sparrow (virtual)
    - scine-database and py-scine-database (virtual)
    - scine-molassembler and py-scine-molassembler (virtual)
    - scine-xtb and py-scine-xtb (virtual)
    - py-scine-chemoton
    - py-scine-puffin
    
    * Fix line-length for flake8
    
    * Remove virtual dependencies
    
    * Update detection of boost, minor fixes in packages / dependencies
    
    * Correctly declare build-only dependencies, add git versions
    
    * Add sparrow dummy packages, fix dependencies
    
    * Rename latest version from develop to master
    
    * Restore original sparrow package
    
    * Also rename latest version for chemoton and puffin

---
+++ var/spack/repos/builtin/packages/py-scine-chemoton/package.py
@@ -0,0 +1,27 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyScineChemoton(PythonPackage):
+    """Software driving the automated exploration of chemical reaction networks"""
+
+    homepage = "https://scine.ethz.ch/download/chemoton"
+    pypi = "scine_chemoton/scine_chemoton-2.2.0.tar.gz"
+    git = "https://github.com/qcscine/puffin.git"
+
+    version("master", branch="master")
+    version("2.2.0", "cda4f9de1e1c00ecc5e0b2d9c17a5edb56b468d454022e3f4045ec116ba2ec45")
+
+    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("py-networkx", type=("build", "run"))
+    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-scipy", type=("build", "run"))
+    depends_on("py-setproctitle", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("scine-database+python", type=("build", "run"))
+    depends_on("scine-molassembler+python", type=("build", "run"))
+    depends_on("scine-utilities+python", type=("build", "run"))
---
+++ var/spack/repos/builtin/packages/scine-molassembler/package.py
@@ -0,0 +1,98 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+
+from spack.package import *
+
+
+class ScineMolassembler(CMakePackage):
+    """Chemoinformatics toolkit with support for inorganic molecules."""
+
+    homepage = "https://scine.ethz.ch/download/molassembler"
+    url = "https://github.com/qcscine/molassembler/archive/refs/tags/1.2.1.tar.gz"
+    git = "https://github.com/qcscine/molassembler.git"
+
+    version("master", branch="master")
+    version("1.2.1", "c9fea41d383b7f54cf8a3ed4dabebe9e942fe3ef5b47895e3533e8ce42dacd38")
+
+    resource(
+        name="dev",
+        url="https://github.com/qcscine/development-utils/archive/refs/tags/5.0.1.tar.gz",
+        sha256="089ca500fc191e04a968ea166d2fe80178b227bc2e6d3926523fa2eee5f6492d",
+        placement="_dev",
+    )
+
+    resource(
+        name="nauty-finder",
+        url="https://raw.githubusercontent.com/conda-forge/scine-molassembler-feedstock/0aa909d/recipe/cmake/Findnauty.cmake",
+        sha256="0746e8a1e35687687866e82797ceb848d941a0c232ed5daa63689476b557e18e",
+        destination="cmake",
+        expand=False,
+    )
+
+    # resource(
+    #     name="ringdecomposerlib-finder",
+    #     url="https://raw.githubusercontent.com/conda-forge/scine-molassembler-feedstock/0aa909d/recipe/cmake/FindRingDecomposerLib.cmake",
+    #     sha256="5ecfbccf48a66d8477011aa576181b56d725f1716cca5a268683e586d761a3ec",
+    #     destination="cmake",
+    #     expand=False,
+    # )
+
+    patch(
+        "https://raw.githubusercontent.com/conda-forge/scine-molassembler-feedstock/0aa909d/recipe/patches/nlohmann_json.patch",
+        level=1,
+        sha256="a9afcc42b264620ee24505c1857b8493c2fb632a1d155362ba4519814922a6af",
+    )
+
+    patch(
+        "https://raw.githubusercontent.com/conda-forge/scine-molassembler-feedstock/0aa909d/recipe/patches/no_cfenv.patch",
+        level=1,
+        sha256="4d746ebd3567d7209200614613861995b999e65e7e26fc0338184fffa0cb3fb7",
+        when="platform=darwin",
+    )
+
+    variant("python", default=False, description="Build Python extension module")
+
+    depends_on("boost+system+filesystem+program_options cxxstd=17 @1.65.0:")
+    depends_on("eigen@3:")
+    depends_on("googletest", type="build")
+    depends_on("nauty")
+    depends_on("nlohmann-json", type="build")
+    depends_on("python@3.6:", when="+python", type=("build", "run"))
+    depends_on("py-pip", when="+python", type="build")
+    depends_on("py-pybind11@2.6.2:", when="+python", type="build")
+    # depends_on("ringdecomposerlib")
+    depends_on("scine-core")
+    depends_on("scine-utilities")
+    depends_on("scine-utilities+python", when="+python", type=("build", "run"))
+
+    extends("python", when="+python")
+
+    def patch(self):
+        os.rmdir("dev")
+        os.rename("_dev", "dev")
+
+        filter_file(
+            "find_package(pybind11 2.6.2 EXACT QUIET)",
+            "find_package(pybind11)",
+            "dev/cmake/ImportPybind11.cmake",
+        )
+
+    def cmake_args(self):
+        args = [
+            self.define("BUILD_SHARED_LIBS", True),
+            self.define("SCINE_BUILD_TESTS", self.run_tests),
+            self.define("SCINE_BUILD_PYTHON_BINDINGS", "+python" in self.spec),
+            self.define("SCINE_MARCH", ""),
+            self.define("BOOST_ROOT", self.spec["boost"].prefix),
+            self.define("BOOST_LIBRARY_DIR", self.spec["boost"].libs.directories[0]),
+            self.define("BOOST_INCLUDE_DIR", self.spec["boost"].headers.directories[0]),
+            self.define("BOOST_NO_SYSTEM_PATHS", True),
+            self.define("Boost_NO_BOOST_CMAKE", True),
+        ]
+        if "+python" in self.spec:
+            args.append(self.define("PYTHON_EXECUTABLE", self.spec["python"].command.path))
+        return args
