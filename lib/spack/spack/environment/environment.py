84917cfa79e29265b387b483d5773b9d20db3ec9
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2023-01-26 10:40:46 +0100

    Extract functions to read spec files from different format (#35094)
    This commit makes explicit the format version of the spec file
    we are reading from.
    
    Before there were different functions capable of reading some
    part of the spec file at multiple format versions. The decision
    was implicit, since checks were based on the structure of the
    JSON without ever checking a format version number.
    
    The refactor makes also explicit which spec file format is used
    by which database and lockfile format, since the information is
    stored in global mappings.
    
    To ensure we don't change the hash of old specs, JSON representations
    of specs have been added as data. A unit tests checks that we read
    the correct hash in, and that the hash stays the same when we
    re-serialize the spec using the most recent format version.
    
    Co-authored-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/spack/environment/environment.py
+++ lib/spack/spack/environment/environment.py
@@ -104,6 +104,15 @@ valid_environment_name_re = r"^\w[\w-]*$"
 #: version of the lockfile format. Must increase monotonically.
 lockfile_format_version = 4
 
+
+READER_CLS = {
+    1: spack.spec.SpecfileV1,
+    2: spack.spec.SpecfileV1,
+    3: spack.spec.SpecfileV2,
+    4: spack.spec.SpecfileV3,
+}
+
+
 # Magic names
 # The name of the standalone spec list in the manifest yaml
 user_speclist_name = "specs"
@@ -1436,7 +1445,7 @@ class Environment(object):
                         if test_dependency in current_spec[node.name]:
                             continue
                         current_spec[node.name].add_dependency_edge(
-                            test_dependency.copy(), deptype="test"
+                            test_dependency.copy(), deptypes="test"
                         )
 
         results = [
@@ -1942,7 +1951,7 @@ class Environment(object):
             "_meta": {
                 "file-type": "spack-lockfile",
                 "lockfile-version": lockfile_format_version,
-                "specfile-version": spack.spec.specfile_format_version,
+                "specfile-version": spack.spec.SPECFILE_FORMAT_VERSION,
             },
             # users specs + hashes are the 'roots' of the environment
             "roots": [{"hash": h, "spec": str(s)} for h, s in hash_spec_list],
@@ -1975,10 +1984,19 @@ class Environment(object):
 
         # Track specs by their DAG hash, allows handling DAG hash collisions
         first_seen = {}
+        current_lockfile_format = d["_meta"]["lockfile-version"]
+        try:
+            reader = READER_CLS[current_lockfile_format]
+        except KeyError:
+            msg = (
+                f"Spack {spack.__version__} cannot read environment lockfiles using the "
+                f"v{current_lockfile_format} format"
+            )
+            raise RuntimeError(msg)
 
         # First pass: Put each spec in the map ignoring dependencies
         for lockfile_key, node_dict in json_specs_by_hash.items():
-            spec = Spec.from_node_dict(node_dict)
+            spec = reader.from_node_dict(node_dict)
             if not spec._hash:
                 # in v1 lockfiles, the hash only occurs as a key
                 spec._hash = lockfile_key
@@ -1987,8 +2005,11 @@ class Environment(object):
         # Second pass: For each spec, get its dependencies from the node dict
         # and add them to the spec
         for lockfile_key, node_dict in json_specs_by_hash.items():
-            for _, dep_hash, deptypes, _ in Spec.dependencies_from_node_dict(node_dict):
-                specs_by_hash[lockfile_key]._add_dependency(specs_by_hash[dep_hash], deptypes)
+            name, data = reader.name_and_data(node_dict)
+            for _, dep_hash, deptypes, _ in reader.dependencies_from_node_dict(data):
+                specs_by_hash[lockfile_key]._add_dependency(
+                    specs_by_hash[dep_hash], deptypes=deptypes
+                )
 
         # Traverse the root specs one at a time in the order they appear.
         # The first time we see each DAG hash, that's the one we want to
