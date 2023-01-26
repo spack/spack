224bb7c01ba6b99c7bba2dfbdf8431dbd4316101
Erik Heeren <erik.heeren@epfl.ch>
2023-01-26 11:20:36 +0100

    py-nptyping: generic url, avoid pypi for now

--- var/spack/repos/builtin/packages/py-nptyping/package.py
+++ var/spack/repos/builtin/packages/py-nptyping/package.py
@@ -10,24 +10,12 @@ class PyNptyping(PythonPackage):
     """Type hints for numpy"""
 
     homepage = "https://github.com/ramonhagenaars/nptyping"
-    pypi = "nptyping/nptyping-2.4.1.tar.gz"
-
+    url = "https://github.com/ramonhagenaars/nptyping/archive/v2.4.1.tar.gz"
     # avoid pypi for now: https://github.com/ramonhagenaars/nptyping/issues/98
-    version(
-        "2.4.1",
-        sha256="1c1b2b08220d271f3e52dbf2bd9190e4dd15b3c04abfcf7a04ec533d3cc9fdab",
-        url="https://github.com/ramonhagenaars/nptyping/archive/v2.4.1.tar.gz",
-    )
-    version(
-        "1.4.1",
-        sha256="bbcedb967f8be1302dffdd999eb531b99712c6914078294b4411758d5899b3b6",
-        url="https://github.com/ramonhagenaars/nptyping/archive/v1.4.1.tar.gz",
-    )
-    version(
-        "1.0.1",
-        sha256="a00e672bfdaddc99aa6b25dd1ae89d7d58d2b76e8ad099bd69577bac2598589f",
-        url="https://github.com/ramonhagenaars/nptyping/archive/v1.0.1.tar.gz",
-    )
+
+    version("2.4.1", sha256="1c1b2b08220d271f3e52dbf2bd9190e4dd15b3c04abfcf7a04ec533d3cc9fdab")
+    version("1.4.1", sha256="bbcedb967f8be1302dffdd999eb531b99712c6914078294b4411758d5899b3b6")
+    version("1.0.1", sha256="a00e672bfdaddc99aa6b25dd1ae89d7d58d2b76e8ad099bd69577bac2598589f")
 
     depends_on("py-setuptools", type="build")
     depends_on("py-typish@1.7.0:", type=("build", "run"))
