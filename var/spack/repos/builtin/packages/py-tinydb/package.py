194a8d49a34e35b43fd711270654bd8980d5552b
spoutn1k <spoutn1k@users.noreply.github.com>
2023-01-26 18:23:31 +0000

    [@spackbot] updating style on behalf of spoutn1k

--- var/spack/repos/builtin/packages/py-tinydb/package.py
+++ var/spack/repos/builtin/packages/py-tinydb/package.py
@@ -20,6 +20,4 @@ class PyTinydb(PythonPackage):
 
     depends_on("python@3.6:3", type=("build", "run"))
     depends_on("py-poetry-core", type="build")
-    depends_on("py-typing-extensions@3.10.0:4",
-               type=("build", "run"),
-               when="^python@:3.7")
+    depends_on("py-typing-extensions@3.10.0:4", type=("build", "run"), when="^python@:3.7")
