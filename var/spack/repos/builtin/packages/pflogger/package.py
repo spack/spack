82ce850ebc0d154a0f79e456f193442efac5cbdb
Matthew Thompson <matthew.thompson@nasa.gov>
2023-01-26 01:06:05 -0500

    Update gftl, gftl-shared, fargparse, pfunit, yafyaml, pflogger (#35119)

--- var/spack/repos/builtin/packages/pflogger/package.py
+++ var/spack/repos/builtin/packages/pflogger/package.py
@@ -19,6 +19,7 @@ class Pflogger(CMakePackage):
     version("develop", branch="develop")
     version("main", branch="main")
 
+    version("1.9.2", sha256="783879eb1326a911f6e22c016e8530644ed0d315660405f2b43df42ba8670acc")
     version("1.9.1", sha256="918965f5a748a3a62e54751578f5935a820407b988b8455f7f25c266b5b7fe3c")
     version("1.9.0", sha256="aacd9b7e188bee3a54a4e681adde32e3bd95bb556cbbbd2c725c81aca5008003")
     version("1.8.0", sha256="28ce9ac8af374253b6dfd8f53f8fd271c787d432645ec9bc6a5a01601dc56e19")
@@ -47,6 +48,8 @@ class Pflogger(CMakePackage):
     depends_on("yafyaml@1.0.4:", when="@1.9:")
 
     depends_on("mpi", when="+mpi")
+
+    depends_on("cmake@3.12:", type="build")
 
     def cmake_args(self):
         spec = self.spec
