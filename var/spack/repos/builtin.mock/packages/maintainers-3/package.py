da3c89dafa2ba2208b553d180374166cf1a5d9af
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2023-01-27 07:51:24 +0100

    Add a `maintainers` directive (#35083)
    fixes #34879
    
    This commit adds a new maintainer directive,
    which by default extend the list of maintainers
    for a given package.
    
    The directive is backward compatible with the current
    practice of having a "maintainers" list declared at
    the class level.

--- var/spack/repos/builtin.mock/packages/maintainers-3/package.py
+++ var/spack/repos/builtin.mock/packages/maintainers-3/package.py
@@ -2,16 +2,16 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 from spack.package import *
+from spack.pkg.builtin.mock.maintainers_1 import Maintainers1
 
 
-class Maintainers1(Package):
-    """Package with a maintainers field."""
+class Maintainers3(Maintainers1):
+    """A second package with a maintainers field."""
 
     homepage = "http://www.example.com"
-    url = "http://www.example.com/maintainers-1.0.tar.gz"
+    url = "http://www.example.com/maintainers2-1.0.tar.gz"
 
-    maintainers("user1", "user2")
+    maintainers("user0", "user3")
 
     version("1.0", "0123456789abcdef0123456789abcdef")
