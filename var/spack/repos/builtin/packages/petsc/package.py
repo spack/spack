d183be4c36fe214f8fa3614d21fe934405eb8ea2
Tamara Dahlgren <dahlgren1@llnl.gov>
2023-01-27 12:41:21 -0800

    Bugfix: Update Petsc python dependency for at least 3.18:

--- var/spack/repos/builtin/packages/petsc/package.py
+++ var/spack/repos/builtin/packages/petsc/package.py
@@ -206,6 +206,7 @@ class Petsc(Package, CudaPackage, ROCmPackage):
 
     # Build dependencies
     depends_on("python@2.6:2.8,3.4:", type="build")
+    depends_on("python@3.4:", when="@3.18:", type="build")
 
     # Other dependencies
     depends_on("metis@5:~int64+real64", when="@:3.7+metis~int64+double")
