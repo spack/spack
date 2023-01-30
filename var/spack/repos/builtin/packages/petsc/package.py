be8eb25b0b6a715454e7fb717e52ccb30e8b3cac
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2023-01-30 10:21:50 -0800

    Bugfix: Update Petsc Python dependency (#35216)
    * Bugfix: Update Petsc python dependency for at least 3.18:
    * Update var/spack/repos/builtin/packages/petsc/package.py
    
    Co-authored-by: Satish Balay <balay@mcs.anl.gov>

--- var/spack/repos/builtin/packages/petsc/package.py
+++ var/spack/repos/builtin/packages/petsc/package.py
@@ -205,7 +205,9 @@ class Petsc(Package, CudaPackage, ROCmPackage):
     depends_on("rocprim", when="+rocm")
 
     # Build dependencies
-    depends_on("python@2.6:2.8,3.4:", type="build")
+    depends_on("python@2.6:2.8,3.4:3.8", when="@:3.13", type="build")
+    depends_on("python@2.6:2.8,3.4:", when="@3.14:3.17", type="build")
+    depends_on("python@3.4:", when="@3.18:", type="build")
 
     # Other dependencies
     depends_on("metis@5:~int64+real64", when="@:3.7+metis~int64+double")
