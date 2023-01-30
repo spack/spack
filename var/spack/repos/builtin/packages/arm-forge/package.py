399b2846df1cbd1cdd82bbbd0fa7054846148d2f
kent-cheung-arm <40630626+kent-cheung-arm@users.noreply.github.com>
2023-01-30 20:36:39 +0000

    arm-forge: add 22.1.3 (#35200)

--- var/spack/repos/builtin/packages/arm-forge/package.py
+++ var/spack/repos/builtin/packages/arm-forge/package.py
@@ -24,6 +24,9 @@ class ArmForge(Package):
 
     if platform.machine() == "aarch64":
         version(
+            "22.1.3", sha256="131884f998b82673e885a7b42cc883210e3a0229b50af374092140cdfd42a408"
+        )
+        version(
             "22.1.2", sha256="1774ed647a18d2cb47b2e4dd4f6f268f67817dfe6a6bca8ba2f9e71d2f18c690"
         )
         version(
@@ -65,6 +68,9 @@ class ArmForge(Package):
         version("21.0", sha256="2bcc745d0049d6b25c77c97b2d7bad7b4f804180972a2306a8599ce41f6a4573")
     elif platform.machine() == "ppc64le":
         version(
+            "22.1.3", sha256="6479c3a4ae6ce6648c37594eb0266161f06d9f89010fca9299855848661dda49"
+        )
+        version(
             "22.1.2", sha256="1c59d1ccdd83bbd953d891a8e5f4926949daab7f6f1cd73328128794615d23ad"
         )
         version(
@@ -105,6 +111,9 @@ class ArmForge(Package):
         )
         version("21.0", sha256="60cfa7dd1cd131ec85e67cb660f2f84cf30bb700d8979cae1f5f88af658fd249")
     elif platform.machine() == "x86_64":
+        version(
+            "22.1.3", sha256="4f8a8b1df6ad712e89c82eedf4bd85b93b57b3c8d5b37d13480ff058fa8f4467"
+        )
         version(
             "22.1.2", sha256="baeac2b49c38547a8f3dd6a5237544eefe8c7695d969784e6f861ce77b610542"
         )
