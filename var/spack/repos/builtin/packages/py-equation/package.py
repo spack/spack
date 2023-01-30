4fa5d0a9174ca01336c0efd3a16925c09e348e82
Erik Heeren <erik.heeren@epfl.ch>
2023-01-30 13:35:03 +0100

    py-equation: add variants for extra dependencies

--- var/spack/repos/builtin/packages/py-equation/package.py
+++ var/spack/repos/builtin/packages/py-equation/package.py
@@ -14,4 +14,10 @@ class PyEquation(PythonPackage):
 
     version("1.2.01", sha256="c8a21dc47d6c748fd19b6485978cf8c42fe31c43db7f44789d95fb5e9752b81c")
 
+    variant("vectormaths", description="Install VectorMaths dependencies", default=False)
+    variant("sciconst", description="Install SciConst dependencies", default=False)
+
     depends_on("py-setuptools", type=("build", "run"))
+
+    depends_on("py-numpy", when="+vectormaths", type=("build", "run"))
+    depends_on("py-scipy@0.12.0:", when="+sciconst", type=("build", "run"))
