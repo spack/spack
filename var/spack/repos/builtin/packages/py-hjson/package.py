053c8a3144629b3ca154e06aa2b56d26c08ab84a
Erik Heeren <erik.heeren@epfl.ch>
2023-01-30 19:49:07 +0100

    py-hjson: fix typo in latest version (#35234)

--- var/spack/repos/builtin/packages/py-hjson/package.py
+++ var/spack/repos/builtin/packages/py-hjson/package.py
@@ -12,7 +12,7 @@ class PyHjson(PythonPackage):
     homepage = "https://github.com/hjson/hjson-py"
     pypi = "hjson/hjson-3.0.2.tar.gz"
 
-    version("3.1.2", sha256="55af475a27cf83a7969c808399d7bccdec8fb836a07ddbd574587593b9cdcf75")
+    version("3.1.0", sha256="55af475a27cf83a7969c808399d7bccdec8fb836a07ddbd574587593b9cdcf75")
     version("3.0.2", sha256="2838fd7200e5839ea4516ece953f3a19892c41089f0d933ba3f68e596aacfcd5")
 
     depends_on("py-setuptools", type="build")
