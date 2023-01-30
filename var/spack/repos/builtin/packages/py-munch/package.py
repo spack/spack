f3115b0edcc808e31f0738ac55676755e1d58d3a
Adam J. Stewart <ajstewart426@gmail.com>
2023-01-30 12:56:25 -0700

    py-fiona: add v1.9.0 (#35247)

--- var/spack/repos/builtin/packages/py-munch/package.py
+++ var/spack/repos/builtin/packages/py-munch/package.py
@@ -11,9 +11,12 @@ class PyMunch(PythonPackage):
     access (a la JavaScript objects)."""
 
     homepage = "https://github.com/Infinidat/munch"
-    url = "https://github.com/Infinidat/munch/archive/2.2.0.tar.gz"
+    pypi = "munch/munch-2.5.0.tar.gz"
 
-    version("2.2.0", sha256="f354ea638e5e582c52d3e47eb54199d3eade94ee3552d64453ddfcbe953973f0")
+    version("2.5.0", sha256="2d735f6f24d4dba3417fa448cae40c6e896ec1fdab6cdb5e6510999758a4dbd2")
+    version("2.2.0", sha256="62fb4fb318e965a464b088e6af52a63e0905a50500b770596a939d3855e7aa15")
 
+    depends_on("py-pbr@3:", when="@2.5:", type="build")
+    depends_on("py-setuptools@17.1:", when="@2.5:", type="build")
     depends_on("py-setuptools", type="build")
     depends_on("py-six", type=("build", "run"))
