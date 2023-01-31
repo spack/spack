35f139628cc4f388677e6d3272b0faa38d9f4131
Erik Heeren <erik.heeren@epfl.ch>
2023-01-31 11:38:33 +0100

    py-matplotlib-scalebar: review remarks

--- var/spack/repos/builtin/packages/py-matplotlib-scalebar/package.py
+++ var/spack/repos/builtin/packages/py-matplotlib-scalebar/package.py
@@ -6,19 +6,19 @@
 from spack.package import *
 
 
-class PyMatplotlibscalebar(PythonPackage):
+class PyMatplotlibScalebar(PythonPackage):
     """Provides a new artist for matplotlib to display a scale bar,
     aka micron bar."""
 
     homepage = "https://github.com/ppinard/matplotlib-scalebar"
     pypi = "matplotlib-scalebar/matplotlib-scalebar-0.6.1.tar.gz"
-    git = "https://github.com/ppinard/matplotlib-scalebar"
+    git = "https://github.com/ppinard/matplotlib-scalebar.git"
 
-    version("develop", git=git, branch="master")
+    version("master", branch="master")
     version("0.8.1", sha256="14887af1093579c5e6afae51a0a1ecc3f715cdbc5c4d7ef59cdeec76ee6bb15d")
     version("0.6.1", sha256="85cec2bacf85aaf00a70cafa5786f7e66e7c0f6e9dc5c894fd6d1afaa7264ecd")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-setuptools-scm", type="build")
 
+    depends_on("python@3.7:3", type=("build", "run"))
     depends_on("py-matplotlib", type=("build", "run"))
