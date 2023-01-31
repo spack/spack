e835bb4f6230536015891bef893f0c45465f0fb7
Erik Heeren <erik.heeren@epfl.ch>
2023-01-31 10:39:08 +0100

    Update var/spack/repos/builtin/packages/py-jinja2-cli/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-jinja2-cli/package.py
+++ var/spack/repos/builtin/packages/py-jinja2-cli/package.py
@@ -15,6 +15,6 @@ class PyJinja2Cli(PythonPackage):
     version("0.8.2", sha256="a16bb1454111128e206f568c95938cdef5b5a139929378f72bb8cf6179e18e50")
     version("0.6.0", sha256="4b1be17ce8a8f133df02205c3f0d3ebfc3a68e795d26987f846a2316636427b7")
 
-    depends_on("py-setuptools", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
 
-    depends_on("py-jinja2", type="run")
+    depends_on("py-jinja2", type=("build", "run"))
