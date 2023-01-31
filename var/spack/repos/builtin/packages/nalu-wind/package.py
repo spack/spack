42deb4a07aafeea5b290fa017c9ad361e874b2d3
Alan Williams <william@sandia.gov>
2023-01-31 12:56:26 -0700

    It really did want a trailing comma...

--- var/spack/repos/builtin/packages/nalu-wind/package.py
+++ var/spack/repos/builtin/packages/nalu-wind/package.py
@@ -66,7 +66,7 @@ class NaluWind(CMakePackage, CudaPackage):
     depends_on("trilinos+muelu+belos+amesos2+ifpack2", when="+trilinos-solvers")
     conflicts(
         "~hypre~trilinos-solvers",
-        msg="nalu-wind: Must enable at least one of the linear-solvers: hypre or trilinos-solvers"
+        msg="nalu-wind: Must enable at least one of the linear-solvers: hypre or trilinos-solvers",
     )
     depends_on("kokkos-nvcc-wrapper", type="build", when="+cuda")
     for _arch in CudaPackage.cuda_arch_values:
