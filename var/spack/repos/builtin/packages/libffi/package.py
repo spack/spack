bde49b5f9aa85d579919a843aad87aea8f395cab
Erik Schnetter <schnetter@gmail.com>
2023-01-31 19:46:41 -0500

    libffi: New version 3.4.4 (#35087)

--- var/spack/repos/builtin/packages/libffi/package.py
+++ var/spack/repos/builtin/packages/libffi/package.py
@@ -15,6 +15,7 @@ class Libffi(AutotoolsPackage):
     homepage = "https://sourceware.org/libffi/"
     url = "https://github.com/libffi/libffi/releases/download/v3.4.2/libffi-3.4.2.tar.gz"
 
+    version("3.4.4", sha256="d66c56ad259a82cf2a9dfc408b32bf5da52371500b84745f7fb8b645712df676")
     version("3.4.3", sha256="4416dd92b6ae8fcb5b10421e711c4d3cb31203d77521a77d85d0102311e6c3b8")
     version("3.4.2", sha256="540fb721619a6aba3bdeef7d940d8e9e0e6d2c193595bc243241b77ff9e93620")
     version(
