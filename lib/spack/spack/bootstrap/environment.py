5bcd11a1d8e53ad4fcca00384020daf2747bdf6d
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2023-02-01 15:45:12 +0100

    Pin black to 22.12.0 to avoid spurious style changes (#35282)
    We need to sync changes on major versions with
    spackbot, to avoid the bot saying everything is
    allright and have CI failing nonetheless.

--- lib/spack/spack/bootstrap/environment.py
+++ lib/spack/spack/bootstrap/environment.py
@@ -171,7 +171,7 @@ def mypy_root_spec():
 
 def black_root_spec():
     """Return the root spec used to bootstrap black"""
-    return _root_spec("py-black")
+    return _root_spec("py-black@:22.12.0")
 
 
 def flake8_root_spec():
