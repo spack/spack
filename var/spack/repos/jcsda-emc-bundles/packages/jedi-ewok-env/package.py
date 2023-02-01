d83a7e7922c0e94044da408d79127a36c29671ff
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-02-01 10:44:48 -0700

    Add mysql+py-mysql-connector-python to jedi-ewok-env

--- var/spack/repos/jcsda-emc-bundles/packages/jedi-ewok-env/package.py
+++ var/spack/repos/jcsda-emc-bundles/packages/jedi-ewok-env/package.py
@@ -36,7 +36,13 @@ class JediEwokEnv(BundlePackage):
     depends_on("py-jinja2", type="run")
     depends_on("py-ruamel-yaml", type="run")
     depends_on("py-ruamel-yaml-clib", type="run")
+
+    # Workflow engines
     depends_on("ecflow", type="run")
+
+    # R2D2 mysql backend
+    depends_on("mysql", type="run")
+    depends_on("py-mysql-connector-python", type="run")
 
     depends_on("solo", when="+solo", type="run")
     depends_on("r2d2", when="+r2d2", type="run")
