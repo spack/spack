cff45e605d61858264115b36ce3dd53d352b4d7f
Wouter Deconinck <wdconinc@gmail.com>
2023-02-03 19:26:21 -0600

    Support packages for using scitokens on OSG
    The Open Science Grid (OSG)  encourages scitokens to provide
    certain services (e.g. writing to xrootd). Spack already
    supports this through scitokens-cpp and xrootd +scitokens-cpp.
    
    This adds py-htgettoken, a python utility to get a scitoken
    from a vault through web authentication. To support htgettoken,
    this also adds py-gssapi.
    
    This also adds the OSG CA cert collection which is typically
    at /etc/grid-security but pointed to in user installations by
    the X509_CERTS_DIR variable.
    
    This allows userspace through spack for functionality that
    otherwise depends on installing the RPMs provided by OSG.

---
+++ var/spack/repos/builtin/packages/py-htgettoken/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyHtgettoken(PythonPackage):
+    """htgettoken gets OIDC authentication tokens for High Throughput Computing
+    via a Hashicorp vault server."""
+
+    homepage = "https://github.com/fermitools/htgettoken"
+
+    # htgettoken is not available on PyPi
+    url = "https://github.com/fermitools/htgettoken/archive/refs/tags/v1.16.tar.gz"
+    list_url = "https://github.com/fermitools/htgettoken/releases/"
+    git = "https://github.com/fermitools/htgettoken.git"
+
+    maintainers = ["wdconinc"]
+
+    version("1.16-33-g3788bb4", commit="3788bb4733e5e8f856cee51566df9a36cbfe097d")
+    version("1.16-20-g8b72f48", commit="8b72f4800ef99923dac99dbe0756a26266a27886")
+    # Older versions do not have a python build system
+    version("1.16", sha256="984fd4746082086356d968b4db536f7b35d30e65a6589fa357caa3266cb98268", deprecated=True)
+
+    depends_on("py-setuptools", type="build")
+
+    depends_on("py-gssapi", type="run")
+    depends_on("py-paramiko", type="run")
+    depends_on("py-urllib3", type="run")
+
+    def setup_run_environment(self, env):
+        if env.get("XDG_RUNTIME_DIR") and env.get("UID"):
+            env.set("BEARER_TOKEN", join_path(env.get("XDG_RUNTIME_DIR"), "bt_u" + env.get("UID")))
