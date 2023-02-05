1da9d7245cd6fa364ad81ca21fe203a7a79720c7
Wouter Deconinck <wdconinc@gmail.com>
2023-02-04 21:17:39 -0600

    dpmjet: new versions

--- var/spack/repos/builtin/packages/dpmjet/package.py
+++ var/spack/repos/builtin/packages/dpmjet/package.py
@@ -18,6 +18,8 @@ class Dpmjet(MakefilePackage):
 
     maintainers = ["wdconinc"]
 
+    version("19.3.5", sha256="5a546ca20f86abaecda1828eb5b577aee8a532dffb2c5e7244667d5f25777909")
+    version("19.3.4", sha256="646f520aa67ef6355c45cde155a5dd55f7c9d661314358a7668f6ff472f5d5f9")
     version("19.3.3", sha256="4f449a36b48ff551beb4303d66bac18bebc52dbcac907f84ab7716c914ad6d8a")
     version("19.2.0", sha256="0f5c1af4419e1a8fa4b46cc24ae1da98abe5c119064275e1848538fe033f02cc")
     version("19.1.3", sha256="f2f7f9eee0fcd1e2770382fa6e3491418607e33de2272e04b6d75ebc97640474")
