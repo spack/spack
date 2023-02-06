1156f63c4fa071d7df65b9a913ed7aab16a9f19d
William Moses <gh@wsmoses.com>
2023-02-06 08:56:46 -0500

    enzyme: add v0.0.48 (#35304)

--- var/spack/repos/builtin/packages/enzyme/package.py
+++ var/spack/repos/builtin/packages/enzyme/package.py
@@ -24,6 +24,7 @@ class Enzyme(CMakePackage):
     root_cmakelists_dir = "enzyme"
 
     version("main", branch="main")
+    version("0.0.48", sha256="f5af62448dd2a8a316e59342ff445003581bc154f06b9b4d7a5a2c7259cf5769")
     version("0.0.32", sha256="9d42e42f7d0faf9beed61b2b1d27c82d1b369aeb9629539d5b7eafbe95379292")
     version("0.0.15", sha256="1ec27db0d790c4507b2256d851b256bf7e074eec933040e9e375d6e352a3c159")
     version("0.0.14", sha256="740641eeeeadaf47942ac88cc52e62ddc0e8c25767a501bed36ec241cf258b8d")
@@ -38,6 +39,7 @@ class Enzyme(CMakePackage):
 
     depends_on("llvm@7:12", when="@0.0.13:0.0.15")
     depends_on("llvm@7:14", when="@0.0.32:")  # TODO actual lower bound
+    depends_on("llvm@7:14", when="@0.0.48:")
     depends_on("cmake@3.9:", type="build")
 
     def cmake_args(self):
