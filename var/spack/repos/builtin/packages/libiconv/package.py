79f2cb61ae7ea2588aacf889cc19dfc44c880cd1
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-02-08 05:55:58 -0700

    Fix conflict statement for libiconv

--- var/spack/repos/builtin/packages/libiconv/package.py
+++ var/spack/repos/builtin/packages/libiconv/package.py
@@ -32,10 +32,10 @@ class Libiconv(AutotoolsPackage, GNUMirrorPackage):
 
     conflicts("@1.14", when="%gcc@5:")
 
-    # Version 1.16 fails building with Intel 18 with
+    # All versions fail to build with Intel 18 with
     #   iconv_no_i18n.o: In function `main':
     #   /path/to/spack-src/src/./iconv.c:1008: undefined reference to `libiconvlist'
-    conflicts("@1.16", when="%intel@18")
+    conflicts("@1.14:", when="%intel@18")
 
     def configure_args(self):
         args = ["--enable-extra-encodings"]
