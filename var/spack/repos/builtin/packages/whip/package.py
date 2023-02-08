b3e2ba536dbc538463239e658fe55ce06766a4b9
Mikael Simberg <mikael.simberg@iki.fi>
2023-02-08 19:36:05 +0100

    Add whip 0.2.0 (#35390)

--- var/spack/repos/builtin/packages/whip/package.py
+++ var/spack/repos/builtin/packages/whip/package.py
@@ -16,6 +16,7 @@ class Whip(CMakePackage, CudaPackage, ROCmPackage):
     maintainers("msimberg", "rasolca")
 
     version("main", branch="main")
+    version("0.2.0", sha256="d8fec662526accbd1624922fdf01a077d6f312cf253382660e4a2f65e28e8686")
     version("0.1.0", sha256="5d557794f4afc8332fc660948a342f69e22bc9e5d575ffb3e3944cf526db5ec9")
 
     depends_on("cmake@3.22:", type="build")
