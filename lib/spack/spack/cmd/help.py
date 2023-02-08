5376600981da64d4bdc5a717339379aeb874692b
Adam J. Stewart <ajstewart426@gmail.com>
2023-02-08 09:46:56 -0700

    spack help --spec: fix indentation (#35383)

--- lib/spack/spack/cmd/help.py
+++ lib/spack/spack/cmd/help.py
@@ -49,9 +49,9 @@ spec expression syntax:
       @r{-variant} or @r{~variant}          disable <variant>
       @r{--variant} or @r{~~variant}        propagate disable <variant>
       @B{variant=value}                 set non-boolean <variant> to <value>
-      @B{variant==value}                 propagate non-boolean <variant> to <value>
+      @B{variant==value}                propagate non-boolean <variant> to <value>
       @B{variant=value1,value2,value3}  set multi-value <variant> values
-      @B{variant==value1,value2,value3}  propagate multi-value <variant> values
+      @B{variant==value1,value2,value3} propagate multi-value <variant> values
 
     architecture variants:
       @m{platform=platform}             linux, darwin, cray, etc.
