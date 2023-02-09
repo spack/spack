06bbd9724451b6502828e04e6c2ffccb4f1a8b3b
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-02-09 08:45:05 -0700

    libiconv@1.16 doesn't build with Intel@18

--- var/spack/repos/builtin/packages/libiconv/package.py
+++ var/spack/repos/builtin/packages/libiconv/package.py
@@ -32,6 +32,11 @@ class Libiconv(AutotoolsPackage, GNUMirrorPackage):
 
     conflicts("@1.14", when="%gcc@5:")
 
+    # Version 1.16 fails building with Intel 18 with
+    #   iconv_no_i18n.o: In function `main':
+    #   /path/to/spack-src/src/./iconv.c:1008: undefined reference to `libiconvlist'
+    conflicts("@1.16", when="%intel@18")
+
     def configure_args(self):
         args = ["--enable-extra-encodings"]
 
