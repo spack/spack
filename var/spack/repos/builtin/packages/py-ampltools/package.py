afabff0f1b545c7e0ebaba5881f89c9a54aff1bd
Sangu Mbekelu <60823467+sm2939@users.noreply.github.com>
2023-02-09 15:46:36 -0500

    Update and rename var/spack/repos/builtin/py-ampltools/package.py to var/spack/repos/builtin/packages/py-ampltools/package.py
    Edited file to change copyright year/dependencies and edited directory

--- var/spack/repos/builtin/packages/py-ampltools/package.py
+++ var/spack/repos/builtin/packages/py-ampltools/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -16,3 +16,4 @@ class PyAmpltools(PythonPackage):
     version("0.4.6", sha256="d54b399c1d78d02e3f4023aa2335b57832deb7d31cdefe4e219e4f2a2bb19a83")
 
     depends_on("py-requests", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
