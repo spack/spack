7f87dea7707ee2576277aae01a0b3e016941b6d8
AmintorDusko <amintor_dusko@hotmail.com>
2023-02-09 10:54:44 -0500

    update py-pennylane package

--- var/spack/repos/builtin/packages/py-pennylane/package.py
+++ var/spack/repos/builtin/packages/py-pennylane/package.py
@@ -14,34 +14,32 @@ class PyPennylane(PythonPackage):
     git = "https://github.com/PennyLaneAI/pennylane.git"
     pypi = "PennyLane/PennyLane-0.28.0.tar.gz"
 
-    maintainers = ["mlxd", "AmintorDusko"]
+    maintainers("mlxd", "AmintorDusko", "marcodelapierre")
 
-    version("develop", branch="master")
+    version("master", branch="master")
     version("0.28.0", sha256="2a6100c00277c1eb59eab6856cdad7b1237e9d1fbda98b1e15020bd5a64b10a8")
 
     depends_on("python@3.8:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-    depends_on("py-semantic-version@2.7:", type=("build", "run"))
-    depends_on("py-pip", type="build")
-    depends_on("py-wheel", type="build")
 
-    depends_on("py-appdirs", type="run")
-    depends_on("py-autograd", type="run")
-    depends_on("py-autoray", type="run")
-    depends_on("py-cachetools", type="run")
-    depends_on("py-networkx", type="run")
-    depends_on("py-numpy@:1.23", type="run")
-    depends_on("py-pennylane-lightning+python", type="run")
-    depends_on("py-retworkx", type="run")
-    depends_on("py-requests", type="run")
-    depends_on("py-scipy", type="run")
-    depends_on("py-toml", type="run")
+    depends_on("py-numpy@:1.23", type=("build", "run"))
+    depends_on("py-scipy", type=("build", "run"))
+    depends_on("py-networkx", type=("build", "run"))
+    depends_on("py-retworkx", type=("build", "run"))
+    depends_on("py-autograd", type=("build", "run"))
+    depends_on("py-toml", type=("build", "run"))
+    depends_on("py-appdirs", type=("build", "run"))
+    depends_on("py-semantic-version@2.7:", type=("build", "run"))
+    depends_on("py-autoray", type=("build", "run"))
+    depends_on("py-cachetools", type=("build", "run"))
+    depends_on("py-pennylane-lightning+python", type=("build", "run"))
+    depends_on("py-requests", type=("build", "run"))
 
     # Test deps
-    depends_on("py-pytest", type=("run", "test"))
-    depends_on("py-pytest-xdist", type=("run", "test"))
-    depends_on("py-pytest-mock", type=("run", "test"))
-    depends_on("py-flaky", type=("run", "test"))
+    depends_on("py-pytest", type="test")
+    depends_on("py-pytest-xdist", type="test")
+    depends_on("py-pytest-mock", type="test")
+    depends_on("py-flaky", type="test")
 
     @run_after("install")
     @on_package_attributes(run_tests=True)
