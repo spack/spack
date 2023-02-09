24f9c23122a91db23f8538cfad097a5383a5ceae
Adrien4193 <39053578+Adrien4193@users.noreply.github.com>
2023-02-09 15:42:51 +0100

    brayns: release 3.1.0, py-brayns: release 3.1.0. (#1826)

--- bluebrain/repo-bluebrain/packages/brayns/package.py
+++ bluebrain/repo-bluebrain/packages/brayns/package.py
@@ -7,8 +7,7 @@ from spack.package import *
 
 
 class Brayns(CMakePackage):
-    """Interactive visualizer for large-scale scientific data
-    """
+    """Interactive visualizer for large-scale scientific data"""
 
     homepage = "https://github.com/BlueBrain/Brayns"
     git = "https://github.com/BlueBrain/Brayns.git"
@@ -17,6 +16,7 @@ class Brayns(CMakePackage):
 
     version('develop', branch='develop')
     version('3.0.0', tag='3.0.0')
+    version('3.1.0', tag='3.1.0')
 
     variant('blueconfig', default=True, description='BlueConfig support')
 
