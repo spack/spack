f659eddf079ec079c9fa1a3d454c62d60495dc37
Teo <wraith1995@users.noreply.github.com>
2023-02-09 21:46:12 -0500

    lowercase?

--- var/spack/repos/builtin/packages/Halide/package.py
+++ var/spack/repos/builtin/packages/Halide/package.py
@@ -5,7 +5,7 @@
 from spack.package import *
 
 
-class Halide(CMakePackage, PythonExtension):
+class halide(CMakePackage, PythonExtension):
     """Halide is a programming language designed to make it easier to write
     high-performance image and array processing code on modern machines."""
 
