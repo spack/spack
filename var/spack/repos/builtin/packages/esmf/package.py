21479bef624346f48ed6750bdd36fbc4ac30bf1f
Alex Richert <82525672+AlexanderRichert-NOAA@users.noreply.github.com>
2023-02-10 08:23:06 -0800

    Add AlexanderRichert-NOAA as ESMF maintainer (#35430)

--- var/spack/repos/builtin/packages/esmf/package.py
+++ var/spack/repos/builtin/packages/esmf/package.py
@@ -19,7 +19,7 @@ class Esmf(MakefilePackage):
     url = "https://github.com/esmf-org/esmf/archive/ESMF_8_0_1.tar.gz"
     git = "https://github.com/esmf-org/esmf.git"
 
-    maintainers("climbfuji", "jedwards4b")
+    maintainers("climbfuji", "jedwards4b", "AlexanderRichert-NOAA")
 
     # Develop is a special name for spack and is always considered the newest version
     version("develop", branch="develop")
