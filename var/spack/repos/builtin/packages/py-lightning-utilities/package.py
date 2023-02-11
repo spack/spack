4e1f877bd98bd77a538f3de665473e8495090865
Adam J. Stewart <ajstewart426@gmail.com>
2023-02-10 19:37:42 -0700

    py-pytorch-lightning: add v1.9.1 (#35445)
    * py-pytorch-lightning: add v1.9.1
    * flake8

--- var/spack/repos/builtin/packages/py-lightning-utilities/package.py
+++ var/spack/repos/builtin/packages/py-lightning-utilities/package.py
@@ -14,6 +14,10 @@ class PyLightningUtilities(PythonPackage):
 
     maintainers("adamjstewart")
 
+    version(
+        "0.6.0.post0",
+        sha256="6f02cfe59e6576487e709a0e66e07671563bde9e21b40e1c567918e4d753278c",
+    )
     version("0.5.0", sha256="01ef5b7fd50a8b54b849d8621720a65c36c91b374933a8384fb2be3d86cfa8f1")
     version("0.4.2", sha256="dc6696ab180117f7e97b5488dac1d77765ab891022f7521a97a39e10d362bdb8")
     version("0.4.1", sha256="969697b0debffd808d4cf3b74af4952f82bf6726f4ce561119037871547690a5")
@@ -26,7 +30,8 @@ class PyLightningUtilities(PythonPackage):
 
     # requirements/base.txt
     depends_on("py-importlib-metadata@4:", when="@0.4.1: ^python@:3.7", type=("build", "run"))
-    depends_on("py-packaging@20:", when="@0.5:", type=("build", "run"))
+    depends_on("py-packaging@17.1:", when="@0.6.0.post0:", type=("build", "run"))
+    depends_on("py-packaging@20:", when="@0.5:0.6.0.a", type=("build", "run"))
     depends_on("py-typing-extensions", when="@0.5:", type=("build", "run"))
 
     # Historical dependencies
