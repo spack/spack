86e37a3738c05f3ca35cc292fab21f672b3c497e
Rocco Meli <r.meli@bluemail.ch>
2023-02-13 21:11:21 +0100

    fixes

--- var/spack/repos/builtin/packages/py-fortls/package.py
+++ var/spack/repos/builtin/packages/py-fortls/package.py
@@ -16,11 +16,11 @@ class PyFortls(PythonPackage):
 
     version("2.13.0", sha256="23c5013e8dd8e1d65bf07be610d0827bc48aa7331a7a7ce13612d4c646d0db31")
 
-    depends_on("py-setuptools@45:", type=("build", "run"))
-    depends_on("py-wheel", type=("build", "run"))
+    depends_on("py-setuptools@45:", type="build")
+    depends_on("p)y-wheel", type="build")
     depends_on("py-packaging", type=("build", "run"))
-    depends_on("py-setuptools-scm@6.2:+toml", type=("build", "run"))
-    depends_on("py-setuptools-scm-git-archive", type=("build", "run"))
+    depends_on("py-setuptools-scm@6.2:+toml", type="build")
+    depends_on("py-setuptools-scm-git-archive", type="build")
 
     depends_on("py-json5", type=("build", "run"))
     depends_on("py-importlib-metadata", type=("build", "run"), when="python@3.7")
