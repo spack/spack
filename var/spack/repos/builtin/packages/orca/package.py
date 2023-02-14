3b772bf390a0f7532ec0eefc7effc19508f8f0ba
Hector Martinez-Seara <hseara@gmail.com>
2023-02-14 18:32:41 +0100

    Orca: reverted openmpi version
    Reverted `openmpi` version to the previous value.

--- var/spack/repos/builtin/packages/orca/package.py
+++ var/spack/repos/builtin/packages/orca/package.py
@@ -57,7 +57,7 @@ class Orca(Package):
         "4.0.1.2": "2.0.2",
         "4.2.0": "3.1.4",
         "4.2.1": "3.1.4",
-        "5.0.3": "4.1.1",
+        "5.0.3": "4.1.2",
         "5.0.3-f.1": "4.1.2",
     }
     for orca_version, openmpi_version in openmpi_versions.items():
