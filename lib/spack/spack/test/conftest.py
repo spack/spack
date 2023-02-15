bc427e8435eea35f12206b0bbf3db9f3c3a69569
psakiev <psakiev@sandia.gov>
2023-02-15 14:46:54 -0700

    Adjust vars in mutable_mock_env_path

--- lib/spack/spack/test/conftest.py
+++ lib/spack/spack/test/conftest.py
@@ -1554,11 +1554,11 @@ def mock_svn_repository(tmpdir_factory):
 @pytest.fixture()
 def mutable_mock_env_path(tmpdir_factory):
     """Fixture for mocking the internal spack environments directory."""
-    saved_path = ev.environment.env_path
+    saved_path = ev.environment.default_env_path
     mock_path = tmpdir_factory.mktemp("mock-env-path")
-    ev.environment.env_path = str(mock_path)
+    ev.environment.default_env_path = str(mock_path)
     yield mock_path
-    ev.environment.env_path = saved_path
+    ev.environment.default_env_path = saved_path
 
 
 @pytest.fixture()
