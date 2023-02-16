b274a67c81d1df1b5a8c6c8ca60addc5c77877ac
Wouter Deconinck <wdconinc@gmail.com>
2023-02-16 16:46:38 -0600

    geant4: new version 11.1.1
    Bugfix release only. No changes in build system.

--- var/spack/repos/builtin/packages/geant4/package.py
+++ var/spack/repos/builtin/packages/geant4/package.py
@@ -21,6 +21,7 @@ class Geant4(CMakePackage):
 
     maintainers("drbenmorgan")
 
+    version("11.1.1", sha256="c5878634da9ba6765ce35a469b2893044f4a6598aa948733da8436cdbfeef7d2")
     version("11.1.0", sha256="c4a23f2f502efeab56de43a4412b21f65c7ca1b0877b9bc1d7e845ee12edf70a")
     version("11.0.3", sha256="1e6560b802aa84e17255b83987dfc98a1457154fb603d0f340fae978238de3e7")
     version("11.0.2", sha256="661e1ab6f42e58910472d771e76ffd16a2b411398eed70f39808762db707799e")
