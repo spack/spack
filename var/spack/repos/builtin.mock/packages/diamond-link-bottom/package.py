30346f857f8eb185e2a12e3068118c024f6317f3
Harmen Stoppels <harmenstoppels@gmail.com>
2023-02-16 14:26:30 +0100

    spack uninstall: follow run/link edges on --dependents (#34058)
    `spack gc` removes build deps of explicitly installed specs, but somehow
    if you take one of the specs that `spack gc` would remove, and feed it
    to `spack uninstall /<hash>` by hash, it complains about all the
    dependents that still rely on it.
    
    This resolves the inconsistency by only following run/link type deps in
    spack uninstall.
    
    That way you can finally do `spack uninstall cmake` without having to
    remove all packages built with cmake.

---
+++ var/spack/repos/builtin.mock/packages/diamond-link-bottom/package.py
@@ -0,0 +1,15 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class DiamondLinkBottom(Package):
+    """Part of diamond-link-{top,left,right,bottom} group"""
+
+    homepage = "http://www.example.com"
+    url = "http://www.example.com/diamond-link-bottom-1.0.tar.gz"
+
+    version("1.0", "0123456789abcdef0123456789abcdef")
---
+++ var/spack/repos/builtin.mock/packages/diamond-link-left/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class DiamondLinkLeft(Package):
+    """Part of diamond-link-{top,left,right,bottom} group"""
+
+    homepage = "http://www.example.com"
+    url = "http://www.example.com/diamond-link-left-1.0.tar.gz"
+
+    version("1.0", "0123456789abcdef0123456789abcdef")
+
+    depends_on("diamond-link-bottom", type="link")
