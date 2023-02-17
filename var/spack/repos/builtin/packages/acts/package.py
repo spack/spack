2e9e2a81e2c9638a991cd8f62c1553629ece74c5
Wouter Deconinck <wdconinc@gmail.com>
2023-02-17 09:46:29 -0600

    acts: new versions 23.1.0, 23.2.0, 23.2.1
    - https://github.com/acts-project/acts/compare/v23.0.0...v23.1.0: no changes needed to package.py
    - https://github.com/acts-project/acts/compare/v23.1.0...v23.2.0: no changes needed to package.py
    - https://github.com/acts-project/acts/compare/v23.2.0...v23.2.1: no changes needed to package.py

--- var/spack/repos/builtin/packages/acts/package.py
+++ var/spack/repos/builtin/packages/acts/package.py
@@ -39,6 +39,9 @@ class Acts(CMakePackage, CudaPackage):
     # Supported Acts versions
     version("main", branch="main")
     version("master", branch="main", deprecated=True)  # For compatibility
+    version("23.2.1", commit="a9fe5167d4d3b6b53b28d3b17060a5f3e380cf3a", submodules=True)
+    version("23.2.0", commit="bc3120d23a72cfdd0ea8f9a0997f59caf311672b", submodules=True)
+    version("23.1.0", commit="4479f182a37650a538344f749b967d6f757bdf60", submodules=True)
     version("23.0.0", commit="5af1b1b5feb8ca8f4c2c69106a1b9ef612c70d9c", submodules=True)
     version("22.0.1", commit="a4ac99dd72828c5eb3fac06e146f3391958fca8c", submodules=True)
     version("22.0.0", commit="0fb6f8d2ace65338915451201e9ceb6cee11fb5e", submodules=True)
