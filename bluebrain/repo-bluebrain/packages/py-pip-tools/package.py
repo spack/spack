1b7bc73dd9e5c7fa02d4f3aa730f13c3ce654bc7
Tristan Carel <tristan.carel@epfl.ch>
2023-02-17 08:26:13 +0100

    py-pip-tools: use older version because of py-pip requirements (#1836)

--- bluebrain/repo-bluebrain/packages/py-pip-tools/package.py
+++ bluebrain/repo-bluebrain/packages/py-pip-tools/package.py
@@ -10,13 +10,17 @@ class PyPipTools(PythonPackage):
     """pip-tools keeps your pinned dependencies fresh."""
 
     homepage = "https://pip-tools.rtfd.io"
-    pypi = "pip-tools/pip-tools-6.11.0.tar.gz"
+    pypi = "pip-tools/pip-tools-6.10.0.tar.gz"
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
     # maintainers = ['github_user1', 'github_user2']
 
-    version('6.11.0', sha256='90c5dc150e3856e4463b81ccc99307ccf9554e5db8393eb273705cb0b8f71c60')
+    version('6.3.0', sha256='1835a1848bdfb22b2b6e5d10d630844ff5ee15e24b6c3bf92319c76f205d347f')
 
+    depends_on('py-click@7:')
     depends_on('py-setuptools@45:', type='build')
     depends_on('py-setuptools-scm+toml@6.2:', type='build')
+    depends_on('py-pip@20.3:')
+    depends_on('py-build')
+    depends_on('py-wheel')
