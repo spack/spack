7de9ec8cd3373f5c09b4bc211205a37a094b6172
Harmen Stoppels <harmenstoppels@gmail.com>
2023-02-18 21:27:34 +0100

    openblas: fix prereqs / make race (#35395)
    Fix a race in the makefile where the shared lib was built before the
    object files were available.
    
    See https://github.com/xianyi/OpenBLAS/issues/3899

--- var/spack/repos/builtin/packages/openblas/package.py
+++ var/spack/repos/builtin/packages/openblas/package.py
@@ -182,6 +182,10 @@ class Openblas(MakefilePackage):
         when="@0.3.21 %gcc@:9",
     )
 
+    # Generic fix (https://github.com/xianyi/OpenBLAS/pull/3902) so we don't
+    # have to build tests
+    patch("fix-shared-tests-prereqs.patch", when="@0.2.20:0.3.21")
+
     # See https://github.com/spack/spack/issues/19932#issuecomment-733452619
     # Notice: fixed on Amazon Linux GCC 7.3.1 (which is an unofficial version
     # as GCC only has major.minor releases. But the bound :7.3.0 doesn't hurt)
