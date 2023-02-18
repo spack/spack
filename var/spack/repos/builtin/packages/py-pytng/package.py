4cef58b127e7ca595aa07da8baff76e13a095547
Rocco Meli <r.meli@bluemail.ch>
2023-02-18 22:33:12 +0100

    fix

--- var/spack/repos/builtin/packages/py-pytng/package.py
+++ var/spack/repos/builtin/packages/py-pytng/package.py
@@ -1,4 +1,3 @@
-var / spack / repos / builtin / packages / py - pytng / package.py
 # Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
