c8ea365923b597b9fa7966319ebffd2df74b581e
Alec Scott <hi@alecbcs.com>
2023-02-17 23:51:11 -0800

    Add restic v0.15.1

--- var/spack/repos/builtin/packages/restic/package.py
+++ var/spack/repos/builtin/packages/restic/package.py
@@ -14,10 +14,12 @@ class Restic(Package):
 
     maintainers("alecbcs")
 
+    version("0.15.1", sha256="fce382fdcdac0158a35daa640766d5e8a6e7b342ae2b0b84f2aacdff13990c52")
     version("0.15.0", sha256="85a6408cfb0798dab52335bcb00ac32066376c32daaa75461d43081499bc7de8")
     version("0.14.0", sha256="78cdd8994908ebe7923188395734bb3cdc9101477e4163c67e7cc3b8fd3b4bd6")
     version("0.12.1", sha256="a9c88d5288ce04a6cc78afcda7590d3124966dab3daa9908de9b3e492e2925fb")
 
+    depends_on("go@1.18:", type="build", when="@0.15.0:")
     depends_on("go@1.15:", type="build", when="@0.14.0:")
     depends_on("go", type="build")
 
