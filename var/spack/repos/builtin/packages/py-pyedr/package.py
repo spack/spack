fb388e8bbc2d5b8db1656dce97853f6958decaa5
Rocco Meli <r.meli@bluemail.ch>
2023-02-20 07:59:41 +0100

    Update var/spack/repos/builtin/packages/py-pyedr/package.py

--- var/spack/repos/builtin/packages/py-pyedr/package.py
+++ var/spack/repos/builtin/packages/py-pyedr/package.py
@@ -23,4 +23,4 @@ class PyPyedr(PythonPackage):
     depends_on("py-pbr", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
 
-    depends_on("py-setuptools@42:", type="build")
+    depends_on("py-setuptools", type="build")
