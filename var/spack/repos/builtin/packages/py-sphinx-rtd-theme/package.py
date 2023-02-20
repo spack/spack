28dc131b243f3c1d4cd3632de7df35fa98ce11ed
MicK7 <mickey.phy@gmail.com>
2023-02-20 06:52:16 +0100

    set docutils officially supported version

--- var/spack/repos/builtin/packages/py-sphinx-rtd-theme/package.py
+++ var/spack/repos/builtin/packages/py-sphinx-rtd-theme/package.py
@@ -25,7 +25,7 @@ class PySphinxRtdTheme(PythonPackage):
     depends_on("python@2.7:2.8,3.4:", when="@1:", type=("build", "run"))
     depends_on("py-docutils@:0.16", when="@0.5.2:0", type=("build", "run"))
     depends_on("py-docutils@:0.17", when="@1:1.1", type=("build", "run"))
-    depends_on("py-docutils@:0.20", when="@1.2:", type=("build", "run"))
+    depends_on("py-docutils@:0.18", when="@1.2:", type=("build", "run"))
 
     def setup_build_environment(self, env):
         # Hack to prevent usage of npm in 0.5+
