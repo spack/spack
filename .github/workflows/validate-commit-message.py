dad94f6577d196f7754604622753e6fa306b6c14
Erik Heeren <erik.heeren@epfl.ch>
2023-02-20 10:59:00 +0100

    deploy: upgrade complainer
    * nopackage: support for  multiple packages in prefix
    
    * nopackage: strip all the keywords
    
    * nopackage: show changed files in PR
    
    * arrow: temporarily remove patch to test
    
    * nopackage: changed files as json
    
    * nopackage: formatting validate-commit-message.py
    
    Placeholders for new functions, and first type hints
    
    * nopackage: pass changed files and fetch-depth 0
    
    * nopackage: specify ref
    
    * nopackage: only check last commit
    
    * nopackage: show which commit is being checked
    
    * nopackage: kick job
    
    * nopackage: style and further work on validate-commit-message
    
    * validate-commit-message: upgrade
    
    Check for all changed packages, docs and deploy changes
    Ignore PR title for now.
    
    * Set fail state by default to catch script failures
    
    * Introduce script-failure variable
    
    * More debug logging
    
    * No more deepening is required
    
    * Fix indentation error
    
    * In-line replace script-failure=true with script-failure=false
    
    * Let there be newlines
    
    * More newlines... MORE!
    
    * Introduce a bug in the validator script
    
    * Remove bug again
    
    * Fix up comment message
    
    * Apply suggestions from code review
    
    Make the comment clearer
    
    Co-authored-by: Matthias Wolf <matthias.wolf@epfl.ch>
    
    * Relax the checks: satisfy at least one is enough
    
    * Check PR title only
    
    * Hopefully show pr.event.commits
    
    * Validate commit message: refactor
    
    * Proper logging, with a logger
    
    * Convert str arg to int
    
    * More debugging and a small bugfix
    
    * More bugfixes
    
    * Better comment message
    
    * Some more debug info in the workflow
    
    * DYI changed files
    
    * Fun with quotes
    
    * Revert "Fun with quotes"
    
    This reverts commit 72dde752d24ee49ebf4524cd04d0a69498da1629.
    
    * Set -x
    
    * Correct output name
    
    * Revert "arrow: temporarily remove patch to test"
    
    This reverts commit 0b3f8ed759a8f8bf377b8e4aabc328a94eeb332e.
    
    * Less ambiguity in the comment
    
    * Stop hardcoding things
    
    * Checkout without fancy parameters
    
    * okay, one fancy parameter then
    
    * More debug info
    
    * We don't need all the debug info any more
    
    ---------
    
    Co-authored-by: Matthias Wolf <matthias.wolf@epfl.ch>

--- .github/workflows/validate-commit-message.py
+++ .github/workflows/validate-commit-message.py
@@ -2,82 +2,247 @@
 
 # requires: gitpython
 
+import fileinput
+import json
+import logging
 import os
 import textwrap
 from argparse import ArgumentParser
 
-from git import Repo
+from git import Commit, Repo
 
-KEYWORDS = ['nopackage', 'deploy', 'docs']
 EXISTING_PACKAGES = []
 
+logger = logging.getLogger(__name__)
+sh = logging.StreamHandler()
+fmt = logging.Formatter("%(asctime)s %(message)s")
+sh.setFormatter(fmt)
+sh.setLevel(logging.DEBUG)
+logger.addHandler(sh)
+logger.setLevel(logging.DEBUG)
 
-def prefix_invalid(prefix):
-    packages = prefix.split(':')[0].split(',')
-    for package in map(str.strip, packages):
-        if package not in EXISTING_PACKAGES and package not in KEYWORDS:
-            return True
 
-    return False
+def get_changed_packages(changed_files: list[str]) -> list[str]:
+    """
+    Return all packages changed by the commit
+    """
+
+    changed_packages = []
+    changed_package_paths = [path for path in changed_files if "/packages/" in path]
+    logger.debug("Changed package paths: %s", changed_package_paths)
+    for package_path in changed_package_paths:
+        path_components = package_path.split("/")
+        changed_packages.append(path_components[path_components.index("packages") + 1])
+
+    logger.debug("Changed packages: %s", changed_packages)
+    return changed_packages
 
 
-def main(title):
-    repo = Repo('.')
+def get_unmentioned_packages(
+    prefixes: list[str], changed_files: list[str]
+) -> list[str]:
+    unmentioned_packages = []
 
-    faulty_commits = []
+    changed_packages = get_changed_packages(changed_files)
 
-    if prefix_invalid(title):
-        msg = textwrap.dedent(f"""\
-            * Pull Request Title
-              > {title}
+    for package in changed_packages:
+        if package not in prefixes:
+            unmentioned_packages.append(package)
 
-              Pull request title needs to be compliant as well, '
-              as it will be used for the merge/squash commit'
-            """)
-        faulty_commits.append(msg)
+    logger.debug("Packages changed but not mentioned: %s", unmentioned_packages)
+    return unmentioned_packages
 
-    for commit in repo.iter_commits():
-        print(f'Commit: {commit.message} (parents: {commit.parents})')
-        if len(commit.parents) > 1:
-            print('Not going beyond a merge commit')
-            break
 
-        prefix = commit.message.splitlines()[0]
-        if prefix_invalid(prefix):
-            quoted_commit_message = textwrap.indent(commit.message, prefix="  > ")
-            msg = f'* {commit.hexsha}\n{quoted_commit_message}'
-            faulty_commits.append(msg)
+def one_package_mentioned(prefixes: list[str], changed_files: list[str]) -> list[str]:
+    """Check whether at least one changed package is mentioned"""
 
-    if faulty_commits:
-        warning = 'These commits are not formatted correctly. '
-        warning += 'Please amend them to start with one of:\n'
-        warning += '* \\<package>: \n'
-        warning += '* \\<package>, <package>, ...: \n'
-        warning += f'* {", ".join(keyword + ":" for keyword in KEYWORDS)}\n\n'
-        warning += "### Faulty commits:\n"
-        faulty_commits.insert(0, warning)
-        with open('faulty_commits.txt', 'w') as fp:
-            fp.write('\n'.join(faulty_commits))
-        with open(os.environ['GITHUB_OUTPUT'], 'a') as fp:
+    changed_packages = get_changed_packages(changed_files)
+    return len([prefix for prefix in prefixes if prefix in changed_packages]) > 0
+
+
+def collect_prefixes(message: str) -> list[str]:
+    """
+    Collect all prefixes in the commit message
+    """
+    prefixes = []
+
+    for line in message.splitlines():
+        if ":" in line:
+            prefix = message.split(":")[0]
+            prefix_items = [item.strip() for item in prefix.split(",")]
+            prefixes.extend(prefix_items)
+
+    logger.debug("Prefixes: %s", prefixes)
+    return prefixes
+
+
+def process_message(
+    message: str,
+    changed_files: list[str],
+    docs_changed: bool,
+    deploy_changed: bool,
+    commit: Commit = None,
+) -> str:
+    """
+    Process a message (PR title or commit message).
+    If issues are found, return a message describing them.
+    """
+    logger.debug("Processing message: %s", message)
+    prefixes = collect_prefixes(message)
+    message = ""
+
+    minimal_prefix_present = False
+    if one_package_mentioned(prefixes, changed_files):
+        logger.debug("At least one package was mentioned")
+        minimal_prefix_present = True
+    elif docs_changed and "docs" in prefixes:
+        logger.debug("Docs were changed and mentioned")
+        minimal_prefix_present = True
+    elif deploy_changed and "deploy" in prefixes:
+        logger.debug("Deploy was changed and mentioned")
+        minimal_prefix_present = True
+
+    if not minimal_prefix_present:
+        unmentioned_packages = get_unmentioned_packages(prefixes, changed_files)
+        if unmentioned_packages:
+            message += textwrap.dedent(
+                f"""\
+                * The following packages were changed but not mentioned:
+                  `{", ".join(unmentioned_packages)}:`
+                  You can simply use the above list, \
+                  then explain what you changed.
+                  Alternatively, you can use one line per package \
+                  to describe the change per package.
+                  Please mention at least one package.
+                """
+            )
+
+        if docs_changed and "docs" not in prefixes:
+            message += textwrap.dedent(
+                """\
+                * Docs were changed but not mentioned.
+                  Please use the `docs:` prefix to explain this change.
+                """
+            )
+
+        if deploy_changed and "deploy" not in prefixes:
+            message += textwrap.dedent(
+                """\
+                * Deploy files were changed but not mentioned.
+                  Please use the `deploy:` prefix to explain this change.
+                """
+            )
+
+    return message
+
+
+def main(title: str, changed_files: list[str], commits: int) -> None:
+    logger.info(
+        "Setting fail state to make sure we catch any script failures- we'll clean up at the end"
+    )
+    with open(os.environ["GITHUB_OUTPUT"], "a") as fp:
+        fp.write("script-failure=true\n")
+    repo = Repo(".")
+
+    logger.debug("Title: %s", title)
+    logger.debug("Changed files: %s", changed_files)
+
+    message_issues = []
+    commit_issue = None
+    title_issue = None
+    warning = ""
+
+    docs_changed = any(
+        "documentation" in changed_file for changed_file in changed_files
+    )
+    deploy_changed = any(
+        changed_file.endswith("yml") or changed_file.endswith("yaml")
+        for changed_file in changed_files
+    )
+
+    if commits > 1:
+        title_issue = process_message(
+            title, changed_files, docs_changed, deploy_changed
+        )
+    else:
+        title_issue = process_message(
+            title, changed_files, docs_changed, deploy_changed
+        )
+
+        commit = next(repo.iter_commits())
+        logger.info(f"Checking commit: {commit.message} (parents: {commit.parents})")
+
+        commit_issue = process_message(
+            commit.message, changed_files, docs_changed, deploy_changed, commit
+        )
+
+    if title_issue:
+        message_issues.append(title_issue)
+        warning += textwrap.dedent(
+            """\
+            There are one or more issues with the title of this PR.
+            """
+        )
+
+    if commit_issue:
+        message_issues.append(commit_issue)
+        quoted_commit_message = textwrap.indent(commit.message, prefix="  > ")
+
+        warning += textwrap.dedent(
+            f"""\
+            There are one or more issues with the commit message of commit {commit.hexsha}.
+            Commit message:
+            {quoted_commit_message}
+            """
+        )
+
+    if commit_issue or title_issue:
+        if commits == 1:
+            one_commit = "The commit message of your commit must be compliant as well."
+        else:
+            one_commit = ""
+        warning += textwrap.dedent(
+            f"""\
+            Please satisfy at least one of the checks (one package, docs, or deploy).
+            The PR title must be compliant. {one_commit}
+            Issues:
+            """
+        )
+        message_issues.insert(0, warning)
+        with open("message_issues.txt", "w") as fp:
+            fp.write("\n".join(message_issues))
+        with open(os.environ["GITHUB_OUTPUT"], "a") as fp:
             fp.write("faulty-commits=true")
 
+    with fileinput.FileInput(os.environ["GITHUB_OUTPUT"], inplace=True) as file:
+        for line in file:
+            print(line.replace("script-failure=true", "script-failure=false"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--title", required=True, help="PR title")
+    parser.add_argument(
+        "--changed-files",
+        required=True,
+        help="JSON formatted list of files changed in PR",
+    )
+    parser.add_argument("--commits", required=True, help="Number of commits in the PR")
 
     args = parser.parse_args()
 
-    for spack_repo in ['./var/spack/repos/builder.test',
-                       './var/spack/repos/builtin',
-                       './var/spack/repos/builtin.mock',
-                       './var/spack/repos/tutorial',
-                       './bluebrain/repo-bluebrain',
-                       './bluebrain/repo-patches']:
+    for spack_repo in [
+        "./var/spack/repos/builder.test",
+        "./var/spack/repos/builtin",
+        "./var/spack/repos/builtin.mock",
+        "./var/spack/repos/tutorial",
+        "./bluebrain/repo-bluebrain",
+        "./bluebrain/repo-patches",
+    ]:
         try:
-            EXISTING_PACKAGES.extend(next(os.walk(f'{spack_repo}/packages'))[1])
+            EXISTING_PACKAGES.extend(next(os.walk(f"{spack_repo}/packages"))[1])
         except StopIteration:
-            print(f'No packages under {spack_repo}')
+            logger.critical(f"No packages under {spack_repo}")
             pass
 
-    main(args.title)
+    main(args.title, json.loads(args.changed_files), int(args.commits))
