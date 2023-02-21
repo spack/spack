264c6c124bfcf2b14cd740d080b7777e7657d4f3
Harmen Stoppels <harmenstoppels@gmail.com>
2023-02-21 14:30:47 +0100

    Spec.satisfies should be commutative when strict=False (#35598)
    The call:
    ```
    x.satisfies(y[, strict=False])
    ```
    is commutative, and tests non-empty intersection, whereas:
    ```
    x.satsifies(y, strict=True)
    ```
    is not commutative, and tests set-inclusion.
    
    There are 2 fast paths. When strict=False both self and other need to 
    be concrete, when strict=True we can optimize when other is concrete.

--- lib/spack/spack/test/spec_semantics.py
+++ lib/spack/spack/test/spec_semantics.py
@@ -1293,3 +1293,16 @@ def test_package_hash_affects_dunder_and_dag_hash(mock_packages, default_mock_co
     assert hash(a1) != hash(a2)
     assert a1.dag_hash() != a2.dag_hash()
     assert a1.process_hash() != a2.process_hash()
+
+
+def test_satisfies_is_commutative_with_concrete_specs(mock_packages, default_mock_concretization):
+    a1 = default_mock_concretization("a@1.0")
+    a2 = Spec("a@1.0")
+
+    # strict=False means non-empty intersection, which is commutative.
+    assert a1.satisfies(a2)
+    assert a2.satisfies(a1)
+
+    # strict=True means set inclusion, which is not commutative.
+    assert a1.satisfies(a2, strict=True)
+    assert not a2.satisfies(a1, strict=True)
