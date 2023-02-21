f79007313b3f4dfbbcbaf6e1fee32b197004c4c8
Garth N. Wells <gnw20@cam.ac.uk>
2023-02-21 20:37:32 +0000

    Update var/spack/repos/builtin/packages/py-fenics-dolfinx/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-fenics-dolfinx/package.py
+++ var/spack/repos/builtin/packages/py-fenics-dolfinx/package.py
@@ -59,7 +59,7 @@ class PyFenicsDolfinx(PythonPackage):
     depends_on("fenics-basix@0.1.0", type=("build", "link"), when="@0.1.0")
 
     depends_on("py-fenics-ffcx@main", type="run", when="@main")
-    depends_on("py-fenics-ffcx@0.6.0:0.6", type="run", when="@0.6.0:0.6")
+    depends_on("py-fenics-ffcx@0.6", type="run", when="@0.6")
     depends_on("py-fenics-ffcx@0.5.0.post0", type="run", when="@0.5.0:0.5")
     depends_on("py-fenics-ffcx@0.4.2", type="run", when="@0.4.1")
     depends_on("py-fenics-ffcx@0.3.0", type="run", when="@0.3.0")
