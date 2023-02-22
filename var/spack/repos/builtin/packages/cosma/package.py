b86ef1d15aa69dfbd51940f4afcf5be6fd2be110
Dr. Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2023-02-22 15:25:30 +0100

    Update cosma recipe
    Signed-off-by: Dr. Mathieu Taillefumier <mathieu.taillefumier@free.fr>

--- var/spack/repos/builtin/packages/cosma/package.py
+++ var/spack/repos/builtin/packages/cosma/package.py
@@ -12,14 +12,19 @@ class Cosma(CMakePackage):
     Distributed Communication-Optimal Matrix-Matrix Multiplication Library
     """
 
-    maintainers("haampie", "kabicm", "teonnik")
+    maintainers = ["haampie", "kabicm", "teonnik", "simonpintarelli"]
     homepage = "https://github.com/eth-cscs/COSMA"
     url = "https://github.com/eth-cscs/COSMA/releases/download/v2.5.1/COSMA-v2.5.1.tar.gz"
     git = "https://github.com/eth-cscs/COSMA.git"
 
     # note: The default archives produced with github do not have the archives
     #       of the submodules.
-    version("master", branch="master", submodules=True)
+    version("master", branch="master", submodules=False)
+    version("2.6.3", sha256="8ca96ca41458f1e9d0da70d524c5a03c677dba7238d23a578f852163b6d45ac9",
+            url="https://github.com/eth-cscs/COSMA/releases/download/v2.6.3/cosma.tar.gz")
+    version("2.6.2", sha256="2debb5123cc35aeebc5fd2f8a46cfd6356d1e27618c9bb57129ecd09aa400940",
+            url="https://github.com/eth-cscs/COSMA/releases/download/v2.6.2/cosma.tar.gz")
+    version("2.6.1", sha256="69aa6634a030674f0d9be61e7b0bf0dc17acf0fc9e7a90b40e3179e2254c8d67")
     version("2.5.1", sha256="085b7787597374244bbb1eb89bc69bf58c35f6c85be805e881e1c0b25166c3ce")
     version("2.5.0", sha256="7f68bb0ee5c80f9b8df858afcbd017ad4ed87ac09439d13d7d890844dbdd3d54")
     version("2.4.0", sha256="5714315ce06d48037f86cfee2d7f19340643fee95e9d7f1e92dc1b623b67e395")
@@ -34,13 +39,32 @@ class Cosma(CMakePackage):
     variant("rocm", default=False, description="Build with rocBLAS support")
     variant("scalapack", default=False, description="Build with ScaLAPACK API")
     variant("shared", default=False, description="Build the shared library version")
+    variant("tests", default=False, description="Build tests")
+    variant("apps", default=False, description="Build miniapp")
+    variant("profiling", default=False, description="Enable profiling")
+    variant("gpu_direct", default=False, description="GPU aware MPI")
 
-    depends_on("cmake@3.12:", type="build")
+    with when("+cuda"):
+        variant("nccl", default=False, description="Use cuda nccl")
+
+    depends_on("cmake@3.22:", type="build")
     depends_on("mpi@3:")
     depends_on("blas", when="~cuda ~rocm")
     depends_on("scalapack", when="+scalapack")
     depends_on("cuda", when="+cuda")
     depends_on("rocblas", when="+rocm")
+
+    with when("@2.6.3"):
+        depends_on("tiled-mm@2.2")
+    with when("@2.6.1:"):
+        depends_on("tiled-mm+rocm", when="+rocm")
+        depends_on("tiled-mm+cuda", when="+cuda")
+        depends_on("costa")
+        depends_on("costa+scalapack", when="+scalapack")
+        depends_on("cxxopts", when="+apps")
+        depends_on("cxxopts", when="+tests")
+        depends_on("semiprof", when="+profiling")
+        depends_on("costa+profiling", when="+profiling")
 
     def url_for_version(self, version):
         if version <= Version("2.3.0"):
@@ -72,7 +96,7 @@ class Cosma(CMakePackage):
                 [("^blis", "BLIS"), ("^amdblis", "BLIS"), ("^atlas", "ATLAS")]
             )
 
-        for query, cmake_arg in query_to_cmake_arg:
+        for (query, cmake_arg) in query_to_cmake_arg:
             if query in self.spec:
                 return cmake_arg
 
@@ -92,9 +116,11 @@ class Cosma(CMakePackage):
 
     def cmake_args(self):
         return [
-            self.define("COSMA_WITH_TESTS", False),
-            self.define("COSMA_WITH_APPS", False),
-            self.define("COSMA_WITH_PROFILING", False),
+            self.define_from_variant("COSMA_WITH_TESTS", "tests"),
+            self.define_from_variant("COSMA_WITH_APPS", "apps"),
+            self.define_from_variant("COSMA_WITH_NCCL", "nccl"),
+            self.define_from_variant("COSMA_WITH_GPU_AWARE_MPI", "gpu_direct"),
+            self.define_from_variant("COSMA_WITH_PROFILING", "profiling"),
             self.define("COSMA_WITH_BENCHMARKS", False),
             self.define("COSMA_BLAS", self.cosma_blas_cmake_arg()),
             self.define("COSMA_SCALAPACK", self.cosma_scalapack_cmake_arg()),
--- var/spack/repos/builtin/packages/costa/package.py
+++ var/spack/repos/builtin/packages/costa/package.py
@@ -6,39 +6,41 @@
 
 from spack.package import *
 
-
 class Costa(CMakePackage):
     """
     Distributed Communication-Optimal Matrix Transpose and Reshuffle Library
     Based on the paper: https://arxiv.org/abs/2106.06601
     """
 
-    maintainers("haampie", "kabicm", "RMeli")
+    maintainers = ["haampie", "kabicm"]
     homepage = "https://github.com/eth-cscs/COSTA"
+    url = "https://github.com/eth-cscs/COSTA/releases/download/v2.0/COSTA-v2.0.tar.gz"
     git = "https://github.com/eth-cscs/COSTA.git"
 
     # note: The default archives produced with github do not have the archives
     #       of the submodules.
     version("master", branch="master", submodules=True)
+    version("2.2", sha256="3e7333f012af76ec3508276ea90800313f6136504667021fe229e710bf6acdc7")
     version("2.1", sha256="c1e86452415083f7470b292d93ec60708b7c8dbafc2bac383636bb4b28135866")
     version("2.0", sha256="de250197f31f7d23226c6956a687c3ff46fb0ff6c621a932428236c3f7925fe4")
 
     variant("scalapack", default=False, description="Build with ScaLAPACK API")
     variant("shared", default=False, description="Build shared libraries")
+    variant("profiling", default=False, description="Enable profiling")
+    variant("tests", default=False, description="Enable tests")
+    variant("apps", default=False, description="Enable miniapp")
+    variant("benchmarks", default=False, description="Enable benchmarks")
 
     depends_on("cmake@3.12:", type="build")
     depends_on("mpi@3:")
     depends_on("scalapack", when="+scalapack")
 
     def url_for_version(self, version):
-        if version == Version("2.0"):
+        if version <= Version('2.0'):
             return "https://github.com/eth-cscs/COSTA/releases/download/v{0}/COSTA-v{1}.tar.gz".format(
                 version, version
             )
-        else:
-            return "https://github.com/eth-cscs/COSTA/archive/refs/tags/v{0}.tar.gz".format(
-                version
-            )
+        return "https://github.com/eth-cscs/COSTA/archive/refs/tags/v{0}.tar.gz" .format(version)
 
     def setup_build_environment(self, env):
         return
@@ -57,10 +59,10 @@ class Costa(CMakePackage):
 
     def cmake_args(self):
         return [
-            self.define("COSTA_WITH_BENCHMARKS", "OFF"),
-            self.define("COSTA_WITH_APPS", "OFF"),
-            self.define("COSTA_WITH_TESTS", "OFF"),
-            self.define("COSTA_WITH_PROFILING", "OFF"),
-            self.define("COSTA_SCALAPACK", self.costa_scalapack_cmake_arg()),
-            self.define("BUILD_SHARED_LIBS", "+shared" in self.spec),
+            self.define_from_variant("COSTA_WITH_BENCHMARKS", "benchmarks"),
+            self.define_from_variant("COSTA_WITH_APPS", "apps"),
+            self.define_from_variant("COSTA_WITH_TESTS", "tests"),
+            self.define_from_variant("COSTA_WITH_PROFILING", "profiling"),
+            self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
+            self.define("COSTA_SCALAPACK_IMPLEMENTATION", self.costa_scalapack_cmake_arg()),
         ]
