3db203dd07ee319e7b15853d802fd57219a2d3e3
Luke Diorio-Toth <ldioriototh@gmail.com>
2023-02-23 16:36:33 -0600

    revert to using external htslib
    currently uses bundled samtools and bcftools, and there is no way to use external versions for those dependencies

--- var/spack/repos/builtin/packages/py-pysam/package.py
+++ var/spack/repos/builtin/packages/py-pysam/package.py
@@ -27,8 +27,17 @@ class PyPysam(PythonPackage):
     depends_on("py-cython@0.17:", type="build")
     depends_on("curl")
     depends_on("xz")
+    depends_on("htslib@:1.6", when="@:0.13")
+    depends_on("htslib")
 
     def setup_build_environment(self, env):
         env.set("LDFLAGS", self.spec["curl"].libs.search_flags)
+        # this flag is supposed to be removed by cy_build.py, but for some reason isn't
         if self.spec.platform == "darwin":
             env.remove_flags("LDSHARED", "-bundle")
+        # linking htslib, see:
+        # http://pysam.readthedocs.org/en/latest/installation.html#external
+        # https://github.com/pysam-developers/pysam/blob/v0.9.0/setup.py#L79
+        env.set("HTSLIB_MODE", "external")
+        env.set("HTSLIB_LIBRARY_DIR", self.spec["htslib"].prefix.lib)
+        env.set("HTSLIB_INCLUDE_DIR", self.spec["htslib"].prefix.include)
