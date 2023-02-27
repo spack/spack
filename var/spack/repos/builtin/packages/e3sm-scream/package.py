3a398353036b8a5c0c15beabe29c130f57105a88
Maya Taylor <maya_taylor@brown.edu>
2023-02-27 07:48:55 -0800

    adding version flexibility for gcc support

--- var/spack/repos/builtin/packages/e3sm-scream/package.py
+++ var/spack/repos/builtin/packages/e3sm-scream/package.py
@@ -6,7 +6,7 @@
 from spack.package import *
 
 
-class E3smScream(Package):
+class E3smScream(Package, CudaPackage):
     """Builds dependencies for E3SM SCREAM"""
 
     homepage = "https://e3sm.org/"
@@ -17,22 +17,26 @@ class E3smScream(Package):
 
     version("master", branch="master", submodules=True)
 
-    depends_on("cmake@3.18.0")
-    depends_on("netcdf-fortran@4.4.4")
-    depends_on("netcdf-c@4.4.1")
-    depends_on("cuda")
-    depends_on("parallel-netcdf@1.10.0")
+    depends_on("cmake@3.18.0:")
+    depends_on("cuda", when="+cuda")
+    
+    # netcdf-c@4.4.5:4.7.0 incompatible with netcdf-fortran@4.5: (https://www.unidata.ucar.edu/support/help/MailArchives/netcdf/msg14846.html) 
+    depends_on("netcdf-c@4.7.1:")
+    depends_on("netcdf-fortran@4.4.5:")
+    depends_on("parallel-netcdf@1.10:")
+    
     depends_on("py-poetry", type=("build", "link", "run"))
     depends_on("py-pip", type=("build", "link", "run"))
     depends_on("py-pyyaml", type=("build", "link", "run"))
     depends_on("py-pylint", type=("build", "link", "run"))
     depends_on("py-psutil", type=("build", "link", "run"))
     depends_on("perl-xml-libxml", type=("build", "link", "run"))
-    depends_on("util-linux-uuid@:2.36.2", when="%intel")
-    depends_on("intel-mkl@2020.0.166", when="%intel", type=("build", "link", "run"))
 
-    conflicts("diffutils@3.8:", when="%intel")
-    conflicts("netcdf-c@4.5:")
+    #depends_on("util-linux-uuid", when="%intel")
+    #depends_on("intel-mkl@2020.0.166", when="%intel", type=("build", "link", "run"))
+    #depends_on("intel-mkl", when="%intel", type=("build", "link", "run"))
+    
+    #conflicts("diffutils@3.8:", when="%intel")
 
     def install(self, spec, prefix):
         mkdirp(prefix.lib)
