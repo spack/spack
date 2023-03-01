1633d33d4ff91473ffa6d309cb0616e4c121c3c6
Mosè Giordano <giordano@users.noreply.github.com>
2023-03-01 09:52:28 +0000

    Add new versions of Julia dependencies (#35622)

--- var/spack/repos/builtin/packages/libuv-julia/package.py
+++ var/spack/repos/builtin/packages/libuv-julia/package.py
@@ -13,6 +13,7 @@ class LibuvJulia(AutotoolsPackage):
     git = "https://github.com/JuliaLang/libuv.git"
 
     # julia's libuv fork doesn't tag (all?) releases, so we fix commits.
+    version("1.44.3", commit="2723e256e952be0b015b3c0086f717c3d365d97e")
     version("1.44.2", commit="e6f0e4900e195c8352f821abe2b3cffc3089547b")
     version("1.44.1", commit="1b2d16477fe1142adea952168d828a066e03ee4c")
     version("1.42.0", commit="3a63bf71de62c64097989254e4f03212e3bf5fc8")
--- var/spack/repos/builtin/packages/nghttp2/package.py
+++ var/spack/repos/builtin/packages/nghttp2/package.py
@@ -13,6 +13,10 @@ class Nghttp2(AutotoolsPackage):
     homepage = "https://nghttp2.org/"
     url = "https://github.com/nghttp2/nghttp2/releases/download/v1.26.0/nghttp2-1.26.0.tar.gz"
 
+    version("1.52.0", sha256="9877caa62bd72dde1331da38ce039dadb049817a01c3bdee809da15b754771b8")
+    version("1.51.0", sha256="2a0bef286f65b35c24250432e7ec042441a8157a5b93519412d9055169d9ce54")
+    version("1.50.0", sha256="d162468980dba58e54e31aa2cbaf96fd2f0890e6dd141af100f6bd1b30aa73c6")
+    version("1.48.0", sha256="66d4036f9197bbe3caba9c2626c4565b92662b3375583be28ef136d62b092998")
     version("1.47.0", sha256="62f50f0e9fc479e48b34e1526df8dd2e94136de4c426b7680048181606832b7c")
     version("1.44.0", sha256="3e4824d02ae27eca931e0bb9788df00a26e5fd8eb672cf52cbb89c1463ba16e9")
     version("1.26.0", sha256="daf7c0ca363efa25b2cbb1e4bd925ac4287b664c3d1465f6a390359daa3f0cf1")
