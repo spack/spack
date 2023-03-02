aa8f471810c23e5d1c5547ec2d1e13d21a21e88d
Loïc Pottier <48072795+lpottier@users.noreply.github.com>
2023-03-02 13:37:02 -0800

    Update var/spack/repos/builtin/packages/py-ipdb/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-ipdb/package.py
+++ var/spack/repos/builtin/packages/py-ipdb/package.py
@@ -24,6 +24,6 @@ class PyIpdb(PythonPackage):
     depends_on("py-ipython@7.17:", type=("build", "run"))
     depends_on("py-toml@0.10.2:", when="@:0.13.9", type=("build", "run"))
 
-    depends_on("py-ipython@7.31.1:", type=("build", "run"))
+    depends_on("py-ipython@7.31.1:", when="@0.13.10:", type=("build", "run"))
     depends_on("py-tomli", when="@0.13.10: ^python@:3.10", type=("build", "run"))
     depends_on("py-decorator", type=("build", "run"))
