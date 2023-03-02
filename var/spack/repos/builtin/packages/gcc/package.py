e01062e6da182ff3ae60aeca36defccea76671c4
Maciej Wójcik <w8jcik@gmail.com>
2023-03-02 22:56:01 +0100

    Shorter version notation

--- var/spack/repos/builtin/packages/gcc/package.py
+++ var/spack/repos/builtin/packages/gcc/package.py
@@ -440,8 +440,8 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage):
 
     # Backport libsanitizer patch for glibc >= 2.36
     # https://reviews.llvm.org/D129471
-    patch("glibc-2.36-libsanitizer-gcc-5-9.patch", when="@5.1.0:5.5.0,6.1.0:6.5.0,7.1.0:7.5.0,8.1.0:8.5.0,9.1.0:9.5.0")
-    patch("glibc-2.36-libsanitizer-gcc-10-12.patch", when="@10.1.0:10.4.0,11.1.0:11.3.0,12.1.0")
+    patch("glibc-2.36-libsanitizer-gcc-5-9.patch", when="@5.1:5.5,6.1:6.5,7.1:7.5,8.1:8.5,9.1:9.5")
+    patch("glibc-2.36-libsanitizer-gcc-10-12.patch", when="@10.1:10.4,11.1:11.3,12.1.0")
 
     # Older versions do not compile with newer versions of glibc
     # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=81712
