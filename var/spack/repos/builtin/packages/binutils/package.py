d311f163c7a60be991a2f54ea919461a5ed4d770
Pariksheet Nanda <pnanda@umich.edu>
2023-03-03 18:52:59 -0500

    binutils: Default enable nls on darwin (#35817)

--- var/spack/repos/builtin/packages/binutils/package.py
+++ var/spack/repos/builtin/packages/binutils/package.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 import re
+import sys
 
 import spack.build_systems.autotools
 from spack.package import *
@@ -48,7 +49,11 @@ class Binutils(AutotoolsPackage, GNUMirrorPackage):
     # --disable-ld flag
     variant("gold", default=False, when="+ld", description="build the gold linker")
     variant("libiberty", default=False, description="Also install libiberty.")
-    variant("nls", default=False, description="Enable Native Language Support")
+    # libc does not provide libintl on macOS and therefore needs gettext to
+    # provide libintl.  Therefore change to a sensible default for macOS.
+    variant(
+        "nls", default=(sys.platform == "darwin"), description="Enable Native Language Support"
+    )
     variant("headers", default=False, description="Install extra headers (e.g. ELF)")
     variant("lto", default=False, description="Enable lto.")
     variant(
