f7e58585bff99c871e66df012e1a90fde11ee071
Rocco Meli <r.meli@bluemail.ch>
2023-03-05 21:37:38 +0100

    Update var/spack/repos/builtin/packages/py-griddataformats/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-griddataformats/package.py
+++ var/spack/repos/builtin/packages/py-griddataformats/package.py
@@ -48,6 +48,6 @@ class PyGriddataformats(PythonPackage):
     depends_on("py-mrcfile", when="@1.0.1", type=("build", "run"))
 
     # Deprecated
-    depends_on("python@2.7:", when="@:0.5.0")
+    depends_on("python@2.7:", when="@:0.5.0", type=("build", "run"))
     depends_on("py-numpy@1.0.3:", type=("build", "run"), when="@:0.5.0")
     depends_on("py-six", type=("build", "run"), when="@:0.5.0")
