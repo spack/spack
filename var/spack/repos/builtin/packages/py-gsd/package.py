fa0868137e212d101d36f2c643af1fc4ebdcfca6
Rocco Meli <r.meli@bluemail.ch>
2023-03-06 20:18:39 +0100

    Update var/spack/repos/builtin/packages/py-gsd/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-gsd/package.py
+++ var/spack/repos/builtin/packages/py-gsd/package.py
@@ -27,3 +27,4 @@ class PyGsd(PythonPackage):
     depends_on("py-setuptools@42:", type="build", when="@2.8.0:")
     depends_on("py-cython", type="build")
     depends_on("py-numpy@1.9.3:", type=("build", "run"))
+    depends_on("py-numpy@1.9.3:1", when="@:1", type=("build", "run"))
