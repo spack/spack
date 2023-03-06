ebea3fd41c7faa27ea8dbe96ad4610c37c0ec345
Sangu-Mbekelu <60823467+Sangu-Mbekelu@users.noreply.github.com>
2023-03-06 14:58:51 +0000

    [@spackbot] updating style on behalf of Sangu-Mbekelu

--- var/spack/repos/builtin/packages/py-thop/package.py
+++ var/spack/repos/builtin/packages/py-thop/package.py
@@ -10,13 +10,13 @@ from spack.package import *
 class PyThop(PythonPackage):
     """A tool to count the FLOPs of PyTorch model."""
 
-
     homepage = "https://github.com/Lyken17/pytorch-OpCounter/"
     url = "https://files.pythonhosted.org/packages/bb/0f/72beeab4ff5221dc47127c80f8834b4bcd0cb36f6ba91c0b1d04a1233403/thop-0.1.1.post2209072238-py3-none-any.whl"
 
+    version(
+        "0.1.1.post2209072238",
+        sha256="01473c225231927d2ad718351f78ebf7cffe6af3bed464c4f1ba1ef0f7cdda27",
+        expand=False,
+    )
 
-    version("0.1.1.post2209072238", sha256="01473c225231927d2ad718351f78ebf7cffe6af3bed464c4f1ba1ef0f7cdda27", expand=False)
-
-
-    depends_on("py-setuptools", type=("build","run"))
-
+    depends_on("py-setuptools", type=("build", "run"))
