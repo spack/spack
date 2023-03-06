a65610eeab57719d77067d852589b0899bf4d89f
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-03-05 19:08:03 -0800

    fix style

--- var/spack/repos/builtin/packages/py-exarl/package.py
+++ var/spack/repos/builtin/packages/py-exarl/package.py
@@ -4,35 +4,34 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
-from spack import *
-import os
+from spack.package import *
 
 
 class PyExarl(PythonPackage):
     """A scalable software framework for reinforcement learning environments
     and agents/policies used for the Design and Control applications"""
 
-    homepage  = 'https://github.com/exalearn/EXARL'
-    git = 'https://github.com/exalearn/EXARL.git'
+    homepage = "https://github.com/exalearn/EXARL"
+    git = "https://github.com/exalearn/EXARL.git"
 
-    version('master', branch='master')
-    version('develop', branch='develop')
-    version('update-spack', branch='update-spack')
-    version('0.1.0', tag='v0.1.0')
+    version("master", branch="master")
+    version("develop", branch="develop")
+    version("update-spack", branch="update-spack")
+    version("0.1.0", tag="v0.1.0")
 
-    depends_on('python@3.6:',   type=('build', 'run'))
-    depends_on('git-lfs',       type=('build'))
-    depends_on('py-setuptools', type=('build'))
+    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("git-lfs", type=("build"))
+    depends_on("py-setuptools", type=("build"))
 
-    depends_on('py-ase',          type=('build', 'run'))
-    depends_on('py-numba',        type=('build', 'run'))
-    depends_on('py-lmfit',        type=('build', 'run'))
-    depends_on('py-seaborn',      type=('build', 'run'))
-    depends_on('py-keras',        type=('build', 'run'))
-    depends_on('py-mpi4py',       type=('build', 'run'))
-    depends_on('py-scikit-learn', type=('build', 'run'))
-    depends_on('py-tensorflow',   type=('build', 'run'))
-    depends_on('py-torch',        type=('build', 'run'))
-    depends_on('py-torchvision',  type=('build', 'run'))
+    depends_on("py-ase", type=("build", "run"))
+    depends_on("py-numba", type=("build", "run"))
+    depends_on("py-lmfit", type=("build", "run"))
+    depends_on("py-seaborn", type=("build", "run"))
+    depends_on("py-keras", type=("build", "run"))
+    depends_on("py-mpi4py", type=("build", "run"))
+    depends_on("py-scikit-learn", type=("build", "run"))
+    depends_on("py-tensorflow", type=("build", "run"))
+    depends_on("py-torch", type=("build", "run"))
+    depends_on("py-torchvision", type=("build", "run"))
 
-    phases = ['install']
+    phases = ["install"]
