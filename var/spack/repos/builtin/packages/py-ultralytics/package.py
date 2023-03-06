13dd81fcb2cde65fbd67abed3188ead1d3925b8b
Sangu-Mbekelu <60823467+Sangu-Mbekelu@users.noreply.github.com>
2023-03-06 15:24:37 +0000

    [@spackbot] updating style on behalf of Sangu-Mbekelu

--- var/spack/repos/builtin/packages/py-ultralytics/package.py
+++ var/spack/repos/builtin/packages/py-ultralytics/package.py
@@ -8,18 +8,15 @@ from spack.package import *
 
 class PyUltralytics(PythonPackage):
     """Ultralytics YOLOv8, developed by Ultralytics, is a cutting-edge, state-of-the-art
- (SOTA) model that builds upon the success of previous YOLO versions and introduces new
- features and improvements to further boost performance and flexibility. YOLOv8 is
- designed to be fast, accurate, and easy to use, making it an excellent choice for a
- wide range of object detection, image segmentation and image classification tasks."""
-
+    (SOTA) model that builds upon the success of previous YOLO versions and introduces new
+    features and improvements to further boost performance and flexibility. YOLOv8 is
+    designed to be fast, accurate, and easy to use, making it an excellent choice for a
+    wide range of object detection, image segmentation and image classification tasks."""
 
     homepage = "https://github.com/ultralytics/ultralytics"
     pypi = "ultralytics/ultralytics-8.0.50.tar.gz"
 
-
     version("8.0.50", sha256="fdcb22300b63b72aa52da1713c33c01741aca031a61f15327eb6f02615bb4b97")
-
 
     depends_on("py-setuptools", type=("build", "run"))
     depends_on("py-matplotlib@3.2.2:", type=("build", "run"))
@@ -37,5 +34,3 @@ class PyUltralytics(PythonPackage):
     depends_on("py-seaborn@0.11.0:", type=("build", "run"))
     depends_on("py-psutil", type=("build", "run"))
     depends_on("py-thop@0.1.1:", type=("build", "run"))
-
-
