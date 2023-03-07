0c06ecc71141ad9ec01f33f6ecd28ffbd84064f4
Alec Scott <hi@alecbcs.com>
2023-03-07 15:02:45 -0800

    iceauth: add v1.0.9 (#35880)

--- var/spack/repos/builtin/packages/iceauth/package.py
+++ var/spack/repos/builtin/packages/iceauth/package.py
@@ -14,6 +14,7 @@ class Iceauth(AutotoolsPackage, XorgPackage):
     homepage = "https://cgit.freedesktop.org/xorg/app/iceauth"
     xorg_mirror_path = "app/iceauth-1.0.7.tar.gz"
 
+    version("1.0.9", sha256="5ca274cf210453e7d7cf5c827a2fbc92149df83824f99a27cde17e1f20324dc6")
     version("1.0.7", sha256="6c9706cce276609876e768759ed4ee3b447cd17af4a61f9b5a374c7dda9696d8")
 
     depends_on("libice")
