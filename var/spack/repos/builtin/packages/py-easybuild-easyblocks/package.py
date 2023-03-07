44a1864b17f50a81230079f8f149fb78d86fb0e2
Alec Scott <hi@alecbcs.com>
2023-03-07 09:05:39 -0800

    easybuild: add v4.7.0 (#35853)
    * easybuild: add v4.7.0
    
    * Add v0.4.7 to all easybuild dependencies
    
    * Reorder versions newest to oldest
    
    * Fix styling on easybuild dependency loop

--- var/spack/repos/builtin/packages/py-easybuild-easyblocks/package.py
+++ var/spack/repos/builtin/packages/py-easybuild-easyblocks/package.py
@@ -15,11 +15,12 @@ class PyEasybuildEasyblocks(PythonPackage):
     pypi = "easybuild-easyblocks/easybuild-easyblocks-4.0.0.tar.gz"
     maintainers("boegel")
 
+    version("4.7.0", sha256="c23e81cbaa3e4fa5ab1bb8b2db759332867d61110bf4ec34763ea170780f0655")
     version("4.0.0", sha256="a0fdef6c33c786e323bde1b28bab942fd8e535c26842877d705e692e85b31b07")
 
-    depends_on("python@2.6:2.8,3.5:", when="@4:", type=("build", "run"))
+    depends_on("python@3.5:", type=("build", "run"))
     # pip silently replaces distutils with setuptools
     depends_on("py-setuptools", type="build")
 
-    for v in ["@4.0.0"]:
+    for v in ["@4.0.0", "@4.7.0"]:
         depends_on("py-easybuild-framework" + v, when=v, type="run")
--- var/spack/repos/builtin/packages/py-easybuild-framework/package.py
+++ var/spack/repos/builtin/packages/py-easybuild-framework/package.py
@@ -15,7 +15,8 @@ class PyEasybuildFramework(PythonPackage):
     pypi = "easybuild-framework/easybuild-framework-4.0.0.tar.gz"
     maintainers("boegel")
 
+    version("4.7.0", sha256="ea51c3cb88fca27daadd2fb55ee31f5f51fc60c4e3519ee9d275954540312df8")
     version("4.0.0", sha256="f5c40345cc8b9b5750f53263ade6c9c3a8cd3dfab488d58f76ac61a8ca7c5a77")
 
-    depends_on("python@2.6:2.8,3.5:", type=("build", "run"))
+    depends_on("python@3.5:", type=("build", "run"))
     depends_on("py-setuptools", type=("build", "run"))
