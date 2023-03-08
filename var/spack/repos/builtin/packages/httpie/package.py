5124b9f07f120255174be78ec57a657247eac2fe
Alec Scott <hi@alecbcs.com>
2023-03-07 22:05:44 -0800

    httpie: add v3.2.1 (#35879)
    * httpie: add v3.2.1
    
    * Add additional 3.2.1 dependencies to httpie
    
    * Add version condition to dependency
    
    * Reorder dependencies for efficiency
    
    * Update var/spack/repos/builtin/packages/httpie/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/httpie/package.py
+++ var/spack/repos/builtin/packages/httpie/package.py
@@ -13,6 +13,7 @@ class Httpie(PythonPackage):
     pypi = "httpie/httpie-2.6.0.tar.gz"
     maintainers("BoboTiG")
 
+    version("3.2.1", sha256="c9c0032ca3a8d62492b7231b2dd83d94becf3b71baf8a4bbcd9ed1038537e3ec")
     version("2.6.0", sha256="ef929317b239bbf0a5bb7159b4c5d2edbfc55f8a0bcf9cd24ce597daec2afca5")
     version("2.5.0", sha256="fe6a8bc50fb0635a84ebe1296a732e39357c3e1354541bf51a7057b4877e47f9")
     version(
@@ -27,11 +28,15 @@ class Httpie(PythonPackage):
     )
 
     depends_on("python@3.6:", when="@2.5:", type=("build", "run"))
-    depends_on("py-setuptools", type=("build", "run"))
     depends_on("py-charset-normalizer@2:", when="@2.6:", type=("build", "run"))
     depends_on("py-defusedxml@0.6:", when="@2.5:", type=("build", "run"))
-    depends_on("py-pygments@2.1.3:", type=("build", "run"))
-    depends_on("py-pygments@2.5.2:", when="@2.5:", type=("build", "run"))
     depends_on("py-requests@2.11:", type=("build", "run"))
     depends_on("py-requests@2.22:+socks", when="@2.5:", type=("build", "run"))
+    depends_on("py-pygments@2.1.3:", type=("build", "run"))
+    depends_on("py-pygments@2.5.2:", when="@2.5:", type=("build", "run"))
     depends_on("py-requests-toolbelt@0.9.1:", when="@2.5:", type=("build", "run"))
+    depends_on("py-multidict@4.7.0:", when="@3.2.1:", type=("build", "run"))
+    depends_on("py-setuptools", type=("build", "run"))
+    depends_on("py-importlib-metadata@1.4.0:", when="@3: ^python@:3.7", type=("build", "run"))
+    depends_on("py-rich@9.10.0:", when="@3.2.1:", type=("build", "run"))
+    depends_on("py-colorama@0.2.4:", when="platform=windows", type=("build", "run"))
