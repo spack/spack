5455e03ccce3e63c2746a3823c8ea91b8e15c07e
Jen Herting <jen@herting.cc>
2023-03-08 10:11:32 -0500

    [py-inflect] Modernize package
    - update copyright
    - update import
    - url -> pypi

--- var/spack/repos/builtin/packages/py-inflect/package.py
+++ var/spack/repos/builtin/packages/py-inflect/package.py
@@ -1,9 +1,9 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PyInflect(PythonPackage):
@@ -11,7 +11,7 @@ class PyInflect(PythonPackage):
     indefinite articles; convert numbers to words."""
 
     homepage = "https://github.com/jaraco/inflect"
-    url      = "https://pypi.io/packages/source/i/inflect/inflect-5.0.2.tar.gz"
+    pypi = "inflect/inflect-5.0.2.tar.gz"
 
     version('5.0.2', sha256='d284c905414fe37c050734c8600fe170adfb98ba40f72fc66fed393f5b8d5ea0')
 
