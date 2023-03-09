ce9f01fecfbaa97fca3741bcd0555289d73607bc
Gerhard Theurich <theurich@sourcespring.net>
2023-03-09 15:25:14 -0800

    Now trying to satisfy "black reformatting".

--- var/spack/repos/builtin/packages/esmf/package.py
+++ var/spack/repos/builtin/packages/esmf/package.py
@@ -31,22 +31,42 @@ class Esmf(MakefilePackage):
     version("8.4.1", sha256="1b54cee91aacaa9df400bd284614cbb0257e175f6f3ec9977a2d991ed8aa1af6")
     version("8.4.0", sha256="28531810bf1ae78646cda6494a53d455d194400f19dccd13d6361871de42ed0f")
     version("8.3.1", sha256="6c39261e55dcdf9781cdfa344417b9606f7f961889d5ec626150f992f04f146d")
-    version("8.3.0", sha256="0ff43ede83d1ac6beabd3d5e2a646f7574174b28a48d1b9f2c318a054ba268fd", deprecated=True)
+    version(
+        "8.3.0",
+        sha256="0ff43ede83d1ac6beabd3d5e2a646f7574174b28a48d1b9f2c318a054ba268fd",
+        deprecated=True
+    )
     version("8.3.0b09", commit="5b7e546c4b", deprecated=True)
     version("8.2.0", sha256="3693987aba2c8ae8af67a0e222bea4099a48afe09b8d3d334106f9d7fc311485")
     version("8.1.1", sha256="58c2e739356f21a1b32673aa17a713d3c4af9d45d572f4ba9168c357d586dc75")
-    version("8.1.0", sha256="367dc89ab8e273f6fbe3d63a64a9a6735cbaa77d5f144ebae4af198fee5bb203", deprecated=True)
+    version(
+        "8.1.0",
+        sha256="367dc89ab8e273f6fbe3d63a64a9a6735cbaa77d5f144ebae4af198fee5bb203",
+        deprecated=True
+    )
     version("8.0.1", sha256="9172fb73f3fe95c8188d889ee72fdadb4f978b1d969e1d8e401e8d106def1d84")
-    version("8.0.0", sha256="051dca45f9803d7e415c0ea146df15ce487fb55f0fce18ca61d96d4dba0c8774", deprecated=True)
-    version("7.1.0r", sha256="ae9a5edb8d40ae97a35cbd4bd00b77061f995c77c43d36334dbb95c18b00a889", deprecated=True)
+    version(
+        "8.0.0",
+        sha256="051dca45f9803d7e415c0ea146df15ce487fb55f0fce18ca61d96d4dba0c8774",
+        deprecated=True
+    )
+    version(
+        "7.1.0r",
+        sha256="ae9a5edb8d40ae97a35cbd4bd00b77061f995c77c43d36334dbb95c18b00a889",
+        deprecated=True
+    )
 
     variant("mpi", default=True, description="Build with MPI support")
     variant("external-lapack", default=False, description="Build with external LAPACK library")
     variant("netcdf", default=True, description="Build with NetCDF support")
     variant("pnetcdf", default=True, description="Build with pNetCDF support", when="+mpi")
     variant("xerces", default=False, description="Build with Xerces support")
-    variant("external-parallelio", default=True,
-            description="Build with external parallelio library", when="@8.3.1:+mpi")
+    variant(
+        "external-parallelio",
+        default=True,
+        description="Build with external parallelio library",
+        when="@8.3.1:+mpi"
+    )
     variant("debug", default=False, description="Build with debugging symbols and options enabled")
     variant("shared", default=True, description="Build shared library")
     # 'esmf_os', 'esmf_comm', 'esmf_pio' variants allow override values for their corresponding
@@ -188,11 +208,11 @@ class Esmf(MakefilePackage):
 
         if "+mpi" in spec:
             env.set("ESMF_CXX", spec["mpi"].mpicxx)
-            env.set("ESMF_CC",  spec["mpi"].mpicc)
+            env.set("ESMF_CC", spec["mpi"].mpicc)
             env.set("ESMF_F90", spec["mpi"].mpifc)
         else:
             env.set("ESMF_CXX", spack_cxx)
-            env.set("ESMF_CC",  spack_cc)
+            env.set("ESMF_CC", spack_cc)
             env.set("ESMF_F90", spack_fc)
 
         # This environment variable controls the build option.
@@ -203,8 +223,11 @@ class Esmf(MakefilePackage):
             # Build an optimized version of the library.
             env.set("ESMF_BOPT", "O")
 
-        if self.compiler.name in ["gcc", "clang", "apple-clang"] and gfortran_major_version >= 10 \
-           and self.version < Version("8.3.0"):
+        if (
+            self.compiler.name in ["gcc", "clang", "apple-clang"]
+            and gfortran_major_version >= 10
+            and self.version < Version("8.3.0")
+        ):
             env.set("ESMF_F90COMPILEOPTS", "-fallow-argument-mismatch")
 
         #######
