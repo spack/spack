00ac94812f1f8fab8761001b2e534d80925da65a
becker33 <becker33@users.noreply.github.com>
2023-03-09 20:28:54 +0000

    [@spackbot] updating style on behalf of becker33

--- lib/spack/spack/test/compilers/detection.py
+++ lib/spack/spack/test/compilers/detection.py
@@ -491,7 +491,8 @@ def test_aocc_version_detection(version_str, expected_version):
 
 
 @pytest.mark.regression("33901")
-@pytest.mark.parametrize("version_str",
+@pytest.mark.parametrize(
+    "version_str",
     [
         (
             "Apple clang version 11.0.0 (clang-1100.0.33.8)\n"
