e16df087848032aac61f11e8a2496821be96126d
Erik Heeren <erik.heeren@epfl.ch>
2023-03-10 17:30:41 +0100

    py-opencv-python: render obsolete and remove (#1893)
    * py-atldld: remove py-opencv-python dependency
    
    Replace with opencv+python3+imgproc so we can get rid of
    py-opencv-python and follow upstream practices.
    
    * py-opencv-python: is now obsolete
    
    * py-atldld: version to string
    
    * py-atldld: formatter wants * import, formatter gets * import
    
    * py-atldld: we still need 0.2.2 for py-atlinter
    
    * py-atldld: setup_run_environment is too hackish
    
    * py-atldld: os import is unused

--- bluebrain/repo-bluebrain/packages/py-atldld/package.py
+++ bluebrain/repo-bluebrain/packages/py-atldld/package.py
@@ -3,8 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.build_systems.python import PythonPackage
-from spack.directives import depends_on, version
+from spack.package import *
 
 
 class PyAtldld(PythonPackage):
@@ -12,13 +11,13 @@ class PyAtldld(PythonPackage):
 
     homepage = "atlas-download-tools.rtfd.io"
     git = "https://github.com/BlueBrain/Atlas-Download-Tools.git"
+    pypi = "atldld/atldld-0.3.4.tar.gz"
 
     maintainers = ["EmilieDel", "jankrepl", "Stannislav"]
 
-    version("0.3.2", tag="v0.3.2")
-    version("0.3.1", tag="v0.3.1")
-    version("0.3.0", tag="v0.3.0")
-    version("0.2.2", tag="v0.2.2")
+    # py-atlinter insists on atldld@0.2.2, and the diff with 0.3.4 is too big to quickly upgrade it
+    version("0.3.4", sha256="4385d279e984864814cdb586d19663c525fe2c1eef8dd4be19e8a87b8520a913")
+    version("0.2.2", sha256="4bdbb9ccc8e164c970940fc729a10bf883a67035e8c636261913cecb351835d3")
 
     # Build dependencies
     depends_on("python@3.7:", type=("build", "run"))
@@ -30,9 +29,15 @@ class PyAtldld(PythonPackage):
     depends_on("py-dataclasses", when="@0.3.1: ^python@3.6", type=("build", "run"))
     depends_on("py-matplotlib", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
-    depends_on("py-opencv-python", type=("build", "run"))
+    depends_on("opencv+python3+python_bindings_generator+imgproc", type=("build", "run"))
     depends_on("py-pandas", type=("build", "run"))
     depends_on("py-pillow", when="@0.3.1:", type=("build", "run"))
     depends_on("py-requests", type=("build", "run"))
     depends_on("py-responses", type=("build", "run"))
     depends_on("py-scikit-image", type=("build", "run"))
+
+    def patch(self):
+        if self.version == Version("0.2.2"):
+            # Like 0.3.4, it doesn't really need opencv-python
+            # if opencv is installed with Python bindings
+            filter_file('"opencv-python",', "", "setup.py")
