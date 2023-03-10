8c3b6af3030599b8629d474e0e7682aac32439d2
ulmononian <cameron_book@alumni.brown.edu>
2023-03-10 13:04:39 -0600

    Update package.py to address spack style check.

--- var/spack/repos/builtin/packages/gsi-ncdiag/package.py
+++ var/spack/repos/builtin/packages/gsi-ncdiag/package.py
@@ -10,23 +10,21 @@ class GsiNcdiag(CMakePackage):
     """GSI NetCDF Diagnostics Library and Utility Tools"""
 
     homepage = "https://github.com/NOAA-EMC/GSI-ncdiag"
-    url      = "https://github.com/NOAA-EMC/GSI-ncdiag/archive/refs/tags/v1.0.0.tar.gz"
+    url = "https://github.com/NOAA-EMC/GSI-ncdiag/archive/refs/tags/v1.0.0.tar.gz"
+    
+    maintainers = ["ulmononian"]
 
-    maintainers = ['ulmononian']
+    version("1.0.0", sha256="7251d6139c2bc1580db5f7f019e10a4c73d188ddd52ccf21ecc9e39d50a6af51")
 
-    version('1.0.0', sha256='7251d6139c2bc1580db5f7f019e10a4c73d188ddd52ccf21ecc9e39d50a6af51')
+    variant("serial", default=True, description="Enable Serial NetCDF diagnostics")
 
-    variant('serial', default=True, description='Enable Serial NetCDF diagnostics')
-
-    depends_on('mpi')
-    depends_on('netcdf-fortran@4.5.2:')
+    depends_on("mpi")
+    depends_on("netcdf-fortran@4.5.2:")
 
     def cmake_args(self):
-        args = [
-            self.define_from_variant('ENABLE_NCDIAG_SERIAL', 'serial')
-        ]
+        args = [self.define_from_variant("ENABLE_NCDIAG_SERIAL", "serial")]
 
-        args.append(self.define('CMAKE_C_COMPILER', self.spec['mpi'].mpicc))
-        args.append(self.define('CMAKE_Fortran_COMPILER', self.spec['mpi'].mpifc))
+        args.append(self.define("CMAKE_C_COMPILER", self.spec["mpi"].mpicc))
+        args.append(self.define("CMAKE_Fortran_COMPILER", self.spec["mpi"].mpifc))
 
         return args
