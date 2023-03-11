cd451f8126ed4ebf27397c2a9ba9fe6c71d4d256
Houjun Tang <htang4@lbl.gov>
2023-03-11 06:36:56 -1000

    Update package.py

--- var/spack/repos/builtin/packages/hdf5-vol-async/package.py
+++ var/spack/repos/builtin/packages/hdf5-vol-async/package.py
@@ -24,7 +24,7 @@ class Hdf5VolAsync(CMakePackage):
     version("1.1", tag="v1.1")
     version("1.0", tag="v1.0")
     
-    variant("memcpy", default=False, description="Enable buffer copy for asynchronous dataset write")
+    variant("memcpy", default=False, description="Enable buffer copy for dataset write")
 
     depends_on("mpi")
     depends_on("argobots@main")
@@ -44,7 +44,7 @@ class Hdf5VolAsync(CMakePackage):
             self.define("CMAKE_C_COMPILER", self.spec["mpi"].mpicc),
             self.define("BUILD_SHARED_LIBS", True),
             self.define("BUILD_TESTING", self.run_tests),
-            self.define_from_variant('ENABLE_WRITE_MEMCPY', 'memcpy'),
+            self.define_from_variant("ENABLE_WRITE_MEMCPY", "memcpy"),
         ]
         return args
 
