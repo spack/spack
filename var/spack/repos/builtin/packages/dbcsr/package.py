91f420211e7055762caad481aca1147f17decb51
Rocco Meli <r.meli@bluemail.ch>
2023-03-11 23:39:01 +0100

    Update var/spack/repos/builtin/packages/dbcsr/package.py

--- var/spack/repos/builtin/packages/dbcsr/package.py
+++ var/spack/repos/builtin/packages/dbcsr/package.py
@@ -78,7 +78,7 @@ class Dbcsr(CMakePackage, CudaPackage, ROCmPackage):
 
     dbcsr_amdgpu_targets = "gfx906"
     with when("@2.1.0"):
-        dbcsr_amdgpu_targets = (*dbcsr_amdgpu_targets, "gfx908")  # Mi100
+        dbcsr_amdgpu_targets = (dbcsr_amdgpu_targets, "gfx908")  # Mi100
     with when("@2.3.0"):
         dbcsr_amdgpu_targets = (*dbcsr_amdgpu_targets, "gfx90a")  # Mi250
     amd_msg = "DBCSR only supports amdgpu_target {0}".format(dbcsr_amdgpu_targets)
