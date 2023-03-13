85c208d3ad9601b20fb7d1ddf408a789e4d4ca91
Bernhard Kaindl <43588962+bernhardkaindl@users.noreply.github.com>
2023-03-13 12:15:33 +0000

    Add the new dependencies requested by adamjsteward

--- var/spack/repos/builtin/packages/py-pymatgen/package.py
+++ var/spack/repos/builtin/packages/py-pymatgen/package.py
@@ -15,7 +15,7 @@ class PyPymatgen(PythonPackage):
     homepage = "http://www.pymatgen.org/"
     pypi = "pymatgen/pymatgen-2023.3.10.tar.gz"
 
-    version("2023.3.10", sha256="20a5d1681e2951d2710d5358197b5b58992e2b0ec4217a5ba966cf28513c1283")
+    version("2022.8.23", sha256="e61c3eed297dc4865c21b0da95aea0fc76e78bd0ead3d2533494aefc05acb303")
     version("2021.3.9", sha256="a6f22d69133a48b7801bfd5e6a2878b47b4b4b2ef1a377b87c6c573be14cbf16")
     version(
         "2020.12.31", sha256="5002490facd47c55d2dae42c35712e061c1f5d881180485c0543a899589856d6"
@@ -49,13 +49,20 @@ class PyPymatgen(PythonPackage):
     # dependencies of never versions
     depends_on("py-matplotlib@1.5:", when="@2021.1.1:", type=("build", "run"))
     depends_on("py-monty@3.0.2:", when="@2021.1.1:", type=("build", "run"))
+    # Enable once py-mp-api is packaged. Then, newer versions can be added:
+    # depends_on("py-mp-api@0.27.3:", when="@2022.9:", type=("build", "run"))
+    depends_on("py-networkx@2.2:", when="@2021.1.1:", type=("build", "run"))
     depends_on("py-numpy@1.20.1:", when="@2021.1.1:", type=("build", "run"))
     depends_on("py-palettable@3.1.1:", when="@2021.1.1:", type=("build", "run"))
     depends_on("py-pandas", when="@2021.1.1:", type=("build", "run"))
     depends_on("py-plotly@4.5.0:", when="@2021.1.1:", type=("build", "run"))
+    depends_on("py-pybtex", when="@2022:", type=("build", "run"))
     depends_on("py-ruamel-yaml@0.15.6:", when="@2021.1.1:", type=("build", "run"))
+    depends_on("py-ruamel-yaml@0.17.0:", when="@2022:", type=("build", "run"))
     depends_on("py-scipy@1.5.0:", when="@2021.1.1:", type=("build", "run"))
     depends_on("py-spglib@1.9.9.44:", when="@2021.1.1:", type=("build", "run"))
+    depends_on("py-spglib@2.0.2:", when="@2022.10.22:", type=("build", "run"))
     depends_on("py-sympy", when="@2021.1.1:", type=("build", "run"))
+    depends_on("py-tabulate", when="@2022:", type=("build", "run"))
+    depends_on("py-tqdm", when="@2022:", type=("build", "run"))
     depends_on("py-uncertainties@3.1.4:", when="@2021.1.1:", type=("build", "run"))
-    depends_on("py-networkx@2.2:", when="@2021.1.1:", type=("build", "run"))
