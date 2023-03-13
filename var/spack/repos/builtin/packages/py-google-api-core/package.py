ba366be262bbfca955bf4bb1def22b56ef2d7234
Adam J. Stewart <ajstewart426@gmail.com>
2023-03-13 02:27:15 -0600

    py-earthengine-api: add v0.1.344 (#36053)

--- var/spack/repos/builtin/packages/py-google-api-core/package.py
+++ var/spack/repos/builtin/packages/py-google-api-core/package.py
@@ -16,13 +16,21 @@ class PyGoogleApiCore(PythonPackage):
     # Leave them out of 'import_modules' to avoid optional dependency.
     import_modules = ["google.api_core", "google.api_core.future"]
 
+    version("2.11.0", sha256="4b9bb5d5a380a0befa0573b302651b8a9a89262c1730e37bf423cec511804c22")
     version("1.14.2", sha256="2c23fbc81c76b941ffb71301bb975ed66a610e9b03f918feacd1ed59cf43a6ec")
 
-    depends_on("python@2.7:2.8,3.4:", type=("build", "run"))
-    depends_on("py-googleapis-common-protos@1.6:1", type=("build", "run"))
-    depends_on("py-protobuf@3.4.0:", type=("build", "run"))
-    depends_on("py-google-auth@0.4:1", type=("build", "run"))
-    depends_on("py-requests@2.18:2", type=("build", "run"))
-    depends_on("py-setuptools@34.0.0:", type=("build", "run"))
-    depends_on("py-six@1.10.0:", type=("build", "run"))
-    depends_on("py-pytz", type=("build", "run"))
+    with when("@2:"):
+        depends_on("py-setuptools", type=("build", "run"))
+        depends_on("py-googleapis-common-protos@1.56.2:1", type=("build", "run"))
+        depends_on("py-protobuf@3.19.5:3.19,3.20.2:4.20,4.21.6:4", type=("build", "run"))
+        depends_on("py-google-auth@2.14.1:2", type=("build", "run"))
+        depends_on("py-requests@2.18:2", type=("build", "run"))
+
+    with when("@:1"):
+        depends_on("py-googleapis-common-protos@1.6:1", type=("build", "run"))
+        depends_on("py-protobuf@3.4.0:", type=("build", "run"))
+        depends_on("py-google-auth@0.4:1", type=("build", "run"))
+        depends_on("py-requests@2.18:2", type=("build", "run"))
+        depends_on("py-setuptools@34.0.0:", type=("build", "run"))
+        depends_on("py-six@1.10.0:", type=("build", "run"))
+        depends_on("py-pytz", type=("build", "run"))
