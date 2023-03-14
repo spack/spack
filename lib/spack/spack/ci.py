spack/spack

commit 16c67ff9b4b94c9ae48c60638c94ec0610e6a082

ci: Increase the amount of pruning possible for PR pipelines (#35944)
By setting the traversal depth to 1, only specs matching the changed
package and direct dependents of those (and of course all dependencies
of that set) are removed from pruning candidacy.

share/spack/gitlab/cloud_pipelines/.gitlab-ci.yml
@@ -27,6 +27,7 @@ default:
     SPACK_BUILDCACHE_DESTINATION: "s3://spack-binaries-prs/${CI_COMMIT_REF_NAME}/${SPACK_CI_STACK_NAME}"
     SPACK_PIPELINE_TYPE: "spack_pull_request"
     SPACK_PRUNE_UNTOUCHED: "True"
+    SPACK_UNTOUCHED_PRUNING_DEPENDENT_DEPTH: 1
 
 .protected-refs:
   only:

commit 1691b7caaca81d02fbd47ba082a81b1af9dcb246

Fix typo affecting Gitlab CI (#36103)
Introduced in #35944

lib/spack/spack/ci.py
@@ -770,9 +770,9 @@ def generate_gitlab_ci_yaml(
             dependent_depth = int(dependent_depth)
         except (TypeError, ValueError):
             tty.warn(
-                "Unrecognized value ({0}) ".format(dependent_depth),
-                "provide forSPACK_PRUNE_UNTOUCHED_DEPENDENT_DEPTH, ",
-                "ignoring it.",
+                f"Unrecognized value ({dependent_depth}) "
+                "provided for SPACK_PRUNE_UNTOUCHED_DEPENDENT_DEPTH, "
+                "ignoring it."
             )
             dependent_depth = None
 

share/spack/gitlab/cloud_pipelines/.gitlab-ci.yml
@@ -53,7 +53,7 @@ default:
     SPACK_BUILDCACHE_DESTINATION: "s3://spack-binaries-prs/${CI_COMMIT_REF_NAME}/${SPACK_CI_STACK_NAME}"
     SPACK_PIPELINE_TYPE: "spack_pull_request"
     SPACK_PRUNE_UNTOUCHED: "True"
-    SPACK_UNTOUCHED_PRUNING_DEPENDENT_DEPTH: 1
+    SPACK_PRUNE_UNTOUCHED_DEPENDENT_DEPTH: "1"
 
 .protected-refs:
   only: