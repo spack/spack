aefe4da3a8b496e5e7d1721f953a25af830d0bc2
Erik Heeren <erik.heeren@epfl.ch>
2023-03-14 09:21:25 +0100

    py-pdf2image: 1.16.3 source now available on pypi

--- var/spack/repos/builtin/packages/py-pdf2image/package.py
+++ var/spack/repos/builtin/packages/py-pdf2image/package.py
@@ -12,7 +12,7 @@ class PyPdf2image(PythonPackage):
     homepage = "https://pypi.org/project/pdf2image/"
     pypi = "pdf2image/pdf2image-1.12.1.tar.gz"
 
-    version("1.16.2", sha256="86761091eee35f4641ea98dfddb254254361d018be698a199aff7c1d37331803")
+    version("1.16.3", sha256="74208810c2cef4d9e347769b8e62a52303982ddb4f2dfd744c7ab4b940ae287e")
     version("1.12.1", sha256="a0d9906f5507192210a8d5d7ead63145e9dec4bccc4564b1fb644e923913c31c")
 
     depends_on("py-setuptools", type="build")
