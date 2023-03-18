d8e04232bc383086743d69d6f70dfca4404acd70
Benjamin Meyers <meyersbs@users.noreply.github.com>
2023-03-18 18:00:46 -0400

    New package py-pyisemail (#36112)

---
+++ var/spack/repos/builtin/packages/py-pyisemail/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyPyisemail(PythonPackage):
+    """pyIsEmail is a no-nonsense approach for checking whether that user-supplied
+    email address could be real."""
+
+    homepage = "https://github.com/michaelherold/pyIsEmail"
+    pypi = "pyisemail/pyisemail-2.0.1.tar.gz"
+
+    maintainers("meyersbs")
+
+    version("2.0.1", sha256="daf4b3fb2150a38f406b0aaba729e19fcd638a6d1c0549c25ff54c7b804618f8")
+
+    depends_on("py-hatchling", type="build")
+    depends_on("python@3.7:", type=("build", "run"))
+    depends_on("py-dnspython@2.0.0:", type=("build", "run"))
