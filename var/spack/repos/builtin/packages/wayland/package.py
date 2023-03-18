51912d6bfb10d7302d4d2a776d764fb6dcfd6de4
Wouter Deconinck <wdconinc@gmail.com>
2023-03-18 18:45:02 -0500

    wayland: no need to import build system, per flake8

--- var/spack/repos/builtin/packages/wayland/package.py
+++ var/spack/repos/builtin/packages/wayland/package.py
@@ -1,10 +1,8 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+so xxx# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-import spack.build_systems.autotools
-import spack.build_systems.meson
 from spack.package import *
 
 
