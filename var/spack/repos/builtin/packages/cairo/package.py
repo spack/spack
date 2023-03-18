90060687a056534ff57763f9178c0e5f6f9acadd
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2023-03-18 03:13:53 +0000

    Add static-only option for cairo

--- var/spack/repos/builtin/packages/cairo/package.py
+++ var/spack/repos/builtin/packages/cairo/package.py
@@ -39,6 +39,7 @@ class Cairo(AutotoolsPackage):
     variant("fc", default=False, description="Enable cairo's Fontconfig font backend feature")
     variant("png", default=False, description="Enable cairo's PNG functions feature")
     variant("svg", default=False, description="Enable cairo's SVN functions feature")
+    variant("shared", default=True, description="Build shared libraries")
 
     depends_on("libx11", when="+X")
     depends_on("libxext", when="+X")
@@ -83,6 +84,7 @@ class Cairo(AutotoolsPackage):
         args.extend(self.enable_or_disable("gobject"))
         args.extend(self.enable_or_disable("ft"))
         args.extend(self.enable_or_disable("fc"))
+        args.extend(self.enable_or_disable("shared"))
 
         return args
 
