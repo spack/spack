cd806cd0c02a8132a3d67070df1fa7e07523c354
Wouter Deconinck <wdconinc@gmail.com>
2023-03-18 14:47:17 -0500

    wayland-protocols: change url from github (defunct) to freedesktop.org

--- var/spack/repos/builtin/packages/wayland-protocols/package.py
+++ var/spack/repos/builtin/packages/wayland-protocols/package.py
@@ -10,11 +10,11 @@ class WaylandProtocols(AutotoolsPackage):
     """wayland-protocols contains Wayland protocols that add functionality not
     available in the Wayland core protocol. Such protocols either add
     completely new functionality, or extend the functionality of some other
-    protocol either in Wayland core, or some other protocol i
-    n wayland-protocols."""
+    protocol either in Wayland core, or some other protocol in
+    wayland-protocols."""
 
     homepage = "https://wayland.freedesktop.org/"
-    url = "https://github.com/wayland-project/wayland-protocols/archive/1.20.tar.gz"
+    url = "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/1.20/wayland-protocols-1.20.tar.gz"
 
     version("1.20", sha256="b59cf0949aeb1f71f7db46b63b1c5a6705ffde8cb5bd194f843fbd9b41308dda")
 
