ad95b7ea6dcb4ca96bb13261c0c4af6c33e748be
Alec Scott <hi@alecbcs.com>
2023-03-18 17:13:50 -0700

    perl-compress-raw-zlib: add v2.204

--- var/spack/repos/builtin/packages/perl-compress-raw-zlib/package.py
+++ var/spack/repos/builtin/packages/perl-compress-raw-zlib/package.py
@@ -12,6 +12,7 @@ class PerlCompressRawZlib(PerlPackage):
     homepage = "https://metacpan.org/pod/Compress::Raw::Zlib"
     url = "https://cpan.metacpan.org/authors/id/P/PM/PMQS/Compress-Raw-Zlib-2.081.tar.gz"
 
+    version("2.204", sha256="f161f4297efadbed79c8b096a75951784fc5ccd3170bd32866a19e5c6876d13f")
     version("2.081", sha256="e156de345bd224bbdabfcab0eeb3f678a3099a4e86c9d1b6771d880b55aa3a1b")
 
     depends_on("zlib")
