9b58a73995ddb9875715fbe29744db8c2fc98ecd
Alec Scott <hi@alecbcs.com>
2023-03-18 20:51:06 -0700

    bumpversion: add v0.6.0 and bump2version dependency (#36021)
    * bumpversion: add v0.6.0
    
    * Add bump2version dependency package

--- var/spack/repos/builtin/packages/bumpversion/package.py
+++ var/spack/repos/builtin/packages/bumpversion/package.py
@@ -11,7 +11,9 @@ class Bumpversion(PythonPackage):
 
     pypi = "bumpversion/bumpversion-0.5.0.tar.gz"
 
+    version("0.6.0", sha256="4ba55e4080d373f80177b4dabef146c07ce73c7d1377aabf9d3c3ae1f94584a6")
     version("0.5.3", sha256="6744c873dd7aafc24453d8b6a1a0d6d109faf63cd0cd19cb78fd46e74932c77e")
     version("0.5.0", sha256="030832b9b46848e1c1ac6678dba8242a021e35e908b65565800c9650291117dc")
 
     depends_on("py-setuptools", type="build")
+    depends_on("bump2version", type=("build", "run"), when="@0.6:")
