2cee861c4a9b82d9b2142b154dc0ccc2f09ab64a
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2023-03-21 16:04:05 +0100

    discotec package: use some review suggestions

--- var/spack/repos/builtin/packages/discotec/package.py
+++ var/spack/repos/builtin/packages/discotec/package.py
@@ -19,13 +19,7 @@ class Discotec(CMakePackage):
 
     version("main", branch="main")
 
-    variant(
-        "build_type",
-        default="Release",
-        values=("Release", "RelWithDebInfo", "Debug"),
-        description="Build type for DisCoTec",
-    )
-    variant("enableft", default=False, description="DisCoTec with algorithm-based fault tolerance")
+    variant("ft", default=False, description="DisCoTec with algorithm-based fault tolerance")
     variant("gene", default=False, description="Build for GENE (as task library)")
     variant("hdf5", default=True, description="Interpolation output with HDF5")
     variant("lto", default=True, description="Build with link-time optimization")
@@ -38,7 +32,7 @@ class Discotec(CMakePackage):
     depends_on(
         "boost +test +serialization +filesystem +system +program_options", type=("build", "run")
     )
-    depends_on("cmake@3.24.2:", type=("build"))
+    depends_on("cmake@3.24.2:", type="build")
     depends_on("glpk")
     depends_on("highfive+mpi+boost+ipo", when="+hdf5")
     depends_on("mpi", type=("build", "run"))
@@ -46,17 +40,17 @@ class Discotec(CMakePackage):
 
     def cmake_args(self):
         args = [
-            "-DDISCOTEC_BUILD_MISSING_DEPS=OFF",
-            "-DDISCOTEC_ENABLEFT={0}".format("ON" if "+enableft" in self.spec else "OFF"),
-            "-DDISCOTEC_GENE={0}".format("ON" if "+gene" in self.spec else "OFF"),
-            "-DDISCOTEC_OPENMP={0}".format("ON" if "+openmp" in self.spec else "OFF"),
-            "-DDISCOTEC_TIMING={0}".format("ON" if "+timing" in self.spec else "OFF"),
-            "-DDISCOTEC_TEST={0}".format("ON" if "+test" in self.spec else "OFF"),
-            "-DDISCOTEC_USE_HDF5={0}".format("ON" if "+hdf5" in self.spec else "OFF"),
-            "-DDISCOTEC_USE_HIGHFIVE={0}".format("ON" if "+hdf5" in self.spec else "OFF"),
-            "-DDISCOTEC_USE_LTO={0}".format("ON" if "+lto" in self.spec else "OFF"),
-            "-DDISCOTEC_USE_VTK={0}".format("ON" if "+vtk" in self.spec else "OFF"),
-            "-DDISCOTEC_WITH_SELALIB={0}".format("ON" if "+selalib" in self.spec else "OFF")
+            self.define('DISCOTEC_BUILD_MISSING_DEPS', False),
+            self.define_from_variant('DISCOTEC_ENABLEFT', 'ft'),
+            self.define_from_variant('DISCOTEC_GENE', 'gene'),
+            self.define_from_variant('DISCOTEC_OPENMP', 'openmp'),
+            self.define_from_variant('DISCOTEC_TIMING', 'timing'),
+            self.define_from_variant('DISCOTEC_TEST', 'test'),
+            self.define_from_variant('DISCOTEC_USE_HDF5', 'hdf5'),
+            self.define_from_variant('DISCOTEC_USE_HIGHFIVE', 'hdf5'),
+            self.define_from_variant('DISCOTEC_USE_LTO', 'lto'),
+            self.define_from_variant('DISCOTEC_USE_VTK', 'vtk'),
+            self.define_from_variant('DISCOTEC_WITH_SELALIB', 'selalib'),
         ]
 
         return args
