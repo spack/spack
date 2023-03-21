a6627be5c8f63ee020c844868879b372755644a5
Eric Martin <erimar77@users.noreply.github.com>
2023-03-20 22:17:01 -0500

    py-reportseff: add new package (#36113)
    * py-reportseff: add new package
    
    * Update var/spack/repos/builtin/packages/py-reportseff/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Update var/spack/repos/builtin/packages/py-reportseff/package.py
    
    Co-authored-by: Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
    
    * Update var/spack/repos/builtin/packages/py-reportseff/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Add py-importlib-metadata preqreq
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    Co-authored-by: Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>

---
+++ var/spack/repos/builtin/packages/py-reportseff/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyReportseff(PythonPackage):
+    """A python script for tabular display of slurm efficiency information."""
+
+    homepage = "https://github.com/troycomi/reportseff"
+    pypi = "reportseff/reportseff-2.7.2.tar.gz"
+
+    version("2.7.2", sha256="63cf99ceb0111de511636b214ade937c6c1b8444531e8026dbc34ddf92049c41")
+
+    depends_on("python@3.7:3", type=("build", "run"))
+    depends_on("py-poetry-core@1:", type="build")
+    depends_on("py-click@6.7:", type=("build", "run"))
+    depends_on("py-importlib-metadata@4.8.2:4", when="^python@:3.7", type=("build", "run"))
