665a09ace96234b130f3af4a6a12757cc0378f69
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2023-03-21 15:54:52 +0100

    discotec package: add selalib flag

--- var/spack/repos/builtin/packages/discotec/package.py
+++ var/spack/repos/builtin/packages/discotec/package.py
@@ -19,28 +19,29 @@ class Discotec(CMakePackage):
 
     version("main", branch="main")
 
-    variant("enableft", default=False, description="DisCoTec with algorithm-based fault tolerance")
-    variant("gene", default=False, description="Build for GENE (as task library)")
-    variant("timing", default=True, description="With high-res timers")
-    variant("test", default=True, description="Build Boost tests")
-    variant("openmp", default=False, description="Parallelize with OpenMP")
-    variant("hdf5", default=True, description="Interpolation output with HDF5")
-    variant("vtk", default=False, description="Build with VTK support")
-    variant("lto", default=True, description="Build with link-time optimization")
     variant(
         "build_type",
         default="Release",
         values=("Release", "RelWithDebInfo", "Debug"),
         description="Build type for DisCoTec",
     )
+    variant("enableft", default=False, description="DisCoTec with algorithm-based fault tolerance")
+    variant("gene", default=False, description="Build for GENE (as task library)")
+    variant("hdf5", default=True, description="Interpolation output with HDF5")
+    variant("lto", default=True, description="Build with link-time optimization")
+    variant("openmp", default=False, description="Parallelize with OpenMP")
+    variant("timing", default=True, description="With high-res timers")
+    variant("test", default=True, description="Build Boost tests")
+    variant("selalib", default=False, description="Build selalib example")
+    variant("vtk", default=False, description="Build with VTK support")
 
-    depends_on("cmake@3.24.2:", type=("build"))
-    depends_on("mpi", type=("build", "run"))
     depends_on(
         "boost +test +serialization +filesystem +system +program_options", type=("build", "run")
     )
+    depends_on("cmake@3.24.2:", type=("build"))
     depends_on("glpk")
     depends_on("highfive+mpi+boost+ipo", when="+hdf5")
+    depends_on("mpi", type=("build", "run"))
     depends_on("vtk", when="+vtk")
 
     def cmake_args(self):
@@ -48,13 +49,14 @@ class Discotec(CMakePackage):
             "-DDISCOTEC_BUILD_MISSING_DEPS=OFF",
             "-DDISCOTEC_ENABLEFT={0}".format("ON" if "+enableft" in self.spec else "OFF"),
             "-DDISCOTEC_GENE={0}".format("ON" if "+gene" in self.spec else "OFF"),
+            "-DDISCOTEC_OPENMP={0}".format("ON" if "+openmp" in self.spec else "OFF"),
             "-DDISCOTEC_TIMING={0}".format("ON" if "+timing" in self.spec else "OFF"),
             "-DDISCOTEC_TEST={0}".format("ON" if "+test" in self.spec else "OFF"),
-            "-DDISCOTEC_OPENMP={0}".format("ON" if "+openmp" in self.spec else "OFF"),
             "-DDISCOTEC_USE_HDF5={0}".format("ON" if "+hdf5" in self.spec else "OFF"),
             "-DDISCOTEC_USE_HIGHFIVE={0}".format("ON" if "+hdf5" in self.spec else "OFF"),
             "-DDISCOTEC_USE_LTO={0}".format("ON" if "+lto" in self.spec else "OFF"),
             "-DDISCOTEC_USE_VTK={0}".format("ON" if "+vtk" in self.spec else "OFF"),
+            "-DDISCOTEC_WITH_SELALIB={0}".format("ON" if "+selalib" in self.spec else "OFF")
         ]
 
         return args
