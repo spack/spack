10ed9f5cb8ac23019821deda5a5827b1a6ddc75d
Cleveland <cleveland@lanl.gov>
2023-03-22 08:49:28 -0600

    add opppy-0_1_6 and opppy-0_1_7 releases to the spack recipes

--- var/spack/repos/builtin/packages/py-opppy/package.py
+++ var/spack/repos/builtin/packages/py-opppy/package.py
@@ -17,6 +17,8 @@ class PyOpppy(PythonPackage):
     maintainers("clevelam")
 
     version("master", branch="master")
+    version("0_1_7", sha256="6ef17e289de2c469ac931288fc5debee77583b300e35fa72080ee06d0e27f7af")
+    version("0_1_6", sha256="1f385ffa39c90d3b338aceb563c5f4952d01041fff0ec94084c84e9e1107023e")
     version("0_1_5", sha256="d9df166d347c18d4f145059b4c2fb23dbfbecf0dd5a3398f29e52d3e261844b0")
     version("0_1_4", sha256="22d81a64856f4c12f8079440c837d7d1f45153e68c405b45bed8b6d35831e948")
     version("0_1_3", sha256="c3ca97f2ff8ab319b5c7257baa8cab852387dc00d426b4534c06f0894363c541")
