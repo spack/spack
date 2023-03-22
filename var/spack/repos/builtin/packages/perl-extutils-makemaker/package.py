a0723a4e2f53099b19ea6af155093695def897e7
Alec Scott <hi@alecbcs.com>
2023-03-22 00:29:52 -0700

    perl-extutils-makemaker: add v7.68 (#36311)

--- var/spack/repos/builtin/packages/perl-extutils-makemaker/package.py
+++ var/spack/repos/builtin/packages/perl-extutils-makemaker/package.py
@@ -15,6 +15,7 @@ class PerlExtutilsMakemaker(PerlPackage):
     homepage = "https://github.com/Perl-Toolchain-Gang/ExtUtils-MakeMaker"
     url = "http://search.cpan.org/CPAN/authors/id/B/BI/BINGOS/ExtUtils-MakeMaker-7.24.tar.gz"
 
+    version("7.68", sha256="270238d253343b833daa005fb57a3a5d8916b59da197698a632b141e7acad779")
     version("7.24", sha256="416abc97c3bb2cc72bef28852522f2859de53e37bf3d0ae8b292067d78755e69")
 
     depends_on("perl@5.6.0:", type=("build", "run"))
