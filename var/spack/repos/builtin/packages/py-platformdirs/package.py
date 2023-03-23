d20fee0c426ff46a854572ac9fe8ee5ee7729422
Adam J. Stewart <ajstewart426@gmail.com>
2023-03-23 12:55:06 -0500

    Update various Jupyter packages (#36332)
    * Update various Jupyter packages
    * Fix missing versions

--- var/spack/repos/builtin/packages/py-platformdirs/package.py
+++ var/spack/repos/builtin/packages/py-platformdirs/package.py
@@ -14,6 +14,7 @@ class PyPlatformdirs(PythonPackage):
     homepage = "https://github.com/platformdirs/platformdirs"
     pypi = "platformdirs/platformdirs-2.4.0.tar.gz"
 
+    version("3.1.1", sha256="024996549ee88ec1a9aa99ff7f8fc819bb59e2c3477b410d90a16d32d6e707aa")
     version("2.5.2", sha256="58c8abb07dcb441e6ee4b11d8df0ac856038f944ab98b7be6b27b2a3c7feef19")
     version("2.4.1", sha256="440633ddfebcc36264232365d7840a970e75e1018d15b4327d11f91909045fda")
     version("2.4.0", sha256="367a5e80b3d04d2428ffa76d33f124cf11e8fff2acdaa9b43d545f5c7d661ef2")
@@ -27,11 +28,15 @@ class PyPlatformdirs(PythonPackage):
     )
 
     depends_on("python@3.7:", when="@2.4.1:", type=("build", "run"))
-    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("py-hatch-vcs@0.3:", when="@3:", type="build")
+    depends_on("py-hatch-vcs", when="@2.5.2:", type="build")
+    depends_on("py-hatchling@1.12.2:", when="@3:", type="build")
+    depends_on("py-hatchling@0.22.0:", when="@2.5.2:", type="build")
+    depends_on("py-typing-extensions@4.4:", when="@3: ^python@:3.7", type=("build", "run"))
+
+    # Historical dependencies
     depends_on("py-setuptools@44:", when="@:2.5.1", type="build")
     depends_on("py-setuptools-scm@5:+toml", when="@:2.5.1", type="build")
-    depends_on("py-hatchling@0.22.0:", when="@2.5.2:", type="build")
-    depends_on("py-hatch-vcs", when="@2.5.2:", type="build")
 
 
 class PythonPipBuilder(spack.build_systems.python.PythonPipBuilder):
