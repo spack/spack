4d9ea62eac51b9fe4a29a4f30ea0a078e4e57210
Matthieu Dorier <mdorier@anl.gov>
2023-03-23 18:08:55 +0100

    [mochi-margo] margo version 0.13.1 added (#36344)

--- var/spack/repos/builtin/packages/mochi-margo/package.py
+++ var/spack/repos/builtin/packages/mochi-margo/package.py
@@ -17,6 +17,7 @@ class MochiMargo(AutotoolsPackage):
     maintainers("carns", "mdorier", "fbudin69500", "chuckatkins")
 
     version("main", branch="main")
+    version("0.13.1", sha256="cff1decb94089cd0f9c0930b02092838679827b09ce4a2f3a359d59caee28782")
     version("0.13", sha256="9a5a4aa81ceb10e010fbad6c7bb8d39d082fe6e61ed33b2b2d2b056917f401d8")
     version("0.12.1", sha256="ff9d0f8722aff17737cd63f27758314b2ed78e518cd45d1fb9f0e3b7ccbcef50")
     version("0.12", sha256="d9949423d87d74ae20b0e344fdc76cc7e0a62249f219b05297b8f44891f75a8e")
