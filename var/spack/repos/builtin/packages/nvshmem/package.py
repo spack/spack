821dbcbc2fb869e210647c167f778d82efa8a555
Brian C. Van Essen <vanessen1@llnl.gov>
2023-03-23 13:49:29 -0700

    Added new versions of NVSHMEM.  Enabled CMake support.  Added variant for IBRC.

--- var/spack/repos/builtin/packages/nvshmem/package.py
+++ var/spack/repos/builtin/packages/nvshmem/package.py
@@ -6,7 +6,7 @@
 from spack.package import *
 
 
-class Nvshmem(MakefilePackage, CudaPackage):
+class Nvshmem(CMakePackage, MakefilePackage, CudaPackage):
     """NVSHMEM is a parallel programming interface based on OpenSHMEM that
     provides efficient and scalable communication for NVIDIA GPU
     clusters. NVSHMEM creates a global address space for data that spans
@@ -18,6 +18,8 @@ class Nvshmem(MakefilePackage, CudaPackage):
 
     maintainers("bvanessen")
 
+    version("2.9.0-2", sha256="c0e0d1a5b13f3e2f12fd79b44645dde5370e2baf7392082aee7aa0a0063d75a6")
+    version("2.8.0-3", sha256="7d4ef226630a94b587d18e02c27decc8b41d6f4ee52a26e25644b23cd18da81f")
     version("2.7.0-6", sha256="23ed9b0187104dc87d5d2bc1394b6f5ff29e8c19138dc019d940b109ede699df")
     version("2.6.0-1", sha256="fc0e8de61b034f3a079dc231b1d0955e665a9f57b5013ee98b6743647bd60417")
     version("2.5.0-19", sha256="dd800b40f1d296e1d3ed2a9885adcfe745c3e57582bc809860e87bd32abcdc60")
@@ -38,7 +40,15 @@ class Nvshmem(MakefilePackage, CudaPackage):
         when="@2.6:",
         description="Build with support for GPU initiated communication",
     )
+    variant(
+        "ibrc",
+        default=False,
+        when="@2.6:",
+        description="Build with support for IBRC remote transport",
+    )
     conflicts("~cuda")
+
+    build_system("makefile", "cmake", default="makefile")
 
     def url_for_version(self, version):
         ver_str = "{0}".format(version)
@@ -73,12 +83,26 @@ class Nvshmem(MakefilePackage, CudaPackage):
 
             if self.spec.satisfies("^spectrum-mpi") or self.spec.satisfies("^openmpi"):
                 env.set("NVSHMEM_MPI_IS_OMPI", "1")
+                if self.spec.satisfies("^openmpi") and self.spec.satisfies("^openmpi +pmix"):
+                    env.set("NVSHMEM_PMIX_SUPPORT", "1")
+                else:
+                    env.set("NVSHMEM_PMIX_SUPPORT", "0")
             else:
                 env.set("NVSHMEM_MPI_IS_OMPI", "0")
+                env.set("NVSHMEM_PMIX_SUPPORT", "0")
 
         if "+shmem" in self.spec:
             env.set("NVSHMEM_SHMEM_SUPPORT", "1")
             env.set("SHMEM_HOME", self.spec["mpi"].prefix)
+            env.set("NVSHMEM_LSHMEM", "-loshmem")
 
         if "+gpu_initiated_support" in self.spec:
             env.set("NVSHMEM_GPUINITIATED_SUPPORT", "1")
+
+        if "+ibrc" in self.spec:
+            env.set("NVSHMEM_IBRC_SUPPORT", "1")
+
+    @when("@2.6:")
+    def setup_run_environment(self, env):
+        if "+nccl" in self.spec:
+            env.append_flags("NVSHMEM_BOOTSTRAP", "MPI")
