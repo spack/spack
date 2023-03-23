70fd442ee16c7595124dab1f1e5c492007f55f7c
Matthias Wolf <matthias.wolf@epfl.ch>
2023-03-23 16:54:17 +0100

    py-antspyx: upstreamed. (#1924)
    Fixes BSD-338

--- var/spack/repos/builtin/packages/py-webcolors/package.py
+++ var/spack/repos/builtin/packages/py-webcolors/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -10,7 +10,7 @@ class PyWebcolors(PythonPackage):
     """Working with color names and values formats defined by HTML and CSS."""
 
     homepage = "https://pypi.org/project/webcolors/"
-    url = "https://files.pythonhosted.org/packages/a7/df/b97bf02a97bbd5ed874fec7c5418bf0dd51e8d042ac46bbf2bf5983e89fd/webcolors-1.11.1.tar.gz"
+    pypi = "webcolors/webcolors-1.11.1.tar.gz"
 
     version("1.11.1", sha256="76f360636957d1c976db7466bc71dcb713bb95ac8911944dffc55c01cb516de6")
 
