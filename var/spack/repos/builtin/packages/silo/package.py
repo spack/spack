33b008ba98fd5bb9f46f500c1fb4b0e5e677c55c
Eric Brugger <brugger1@llnl.gov>
2023-03-23 06:34:01 -0700

    Replace conflict with modified 4.11 patch.

--- var/spack/repos/builtin/packages/silo/package.py
+++ var/spack/repos/builtin/packages/silo/package.py
@@ -67,7 +67,7 @@ class Silo(AutotoolsPackage):
     patch("H5FD_class_t-terminate.patch", when="@:4.10.2-bsd")
 
     # H5EPR_SEMI_COLON.patch was fixed in current dev
-    patch("H5EPR_SEMI_COLON.patch", when="@:4.11-bsd")
+    patch("H5EPR_SEMI_COLON.patch", when="@:4.11")
 
     # Fix missing F77 init, fixed in 4.9
     patch("48-configure-f77.patch", when="@:4.8")
@@ -84,9 +84,6 @@ class Silo(AutotoolsPackage):
     # hzip and fpzip are not available in the BSD releases
     conflicts("+hzip", when="@4.10.2-bsd,4.11-bsd")
     conflicts("+fpzip", when="@4.10.2-bsd,4.11-bsd")
-
-    # silo 4.11 doesn't compile with gcc 11.1
-    conflicts("%gcc@11.1:", when="@4.11")
 
     # zfp include missing
     patch("zfp_error.patch", when="@4.11 +hdf5")
