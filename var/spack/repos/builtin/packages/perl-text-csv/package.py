15020c3d3c649e10b7f0a7f6f0d21349befea11a
Alec Scott <hi@alecbcs.com>
2023-03-24 07:13:06 -0700

    perl-text-csv: add v2.02 (#36395)

--- var/spack/repos/builtin/packages/perl-text-csv/package.py
+++ var/spack/repos/builtin/packages/perl-text-csv/package.py
@@ -12,4 +12,5 @@ class PerlTextCsv(PerlPackage):
     homepage = "https://metacpan.org/pod/Text::CSV"
     url = "http://search.cpan.org/CPAN/authors/id/I/IS/ISHIGAKI/Text-CSV-1.95.tar.gz"
 
+    version("2.02", sha256="84120de3e10489ea8fbbb96411a340c32cafbe5cdff7dd9576b207081baa9d24")
     version("1.95", sha256="7e0a11d9c1129a55b68a26aa4b37c894279df255aa63ec8341d514ab848dbf61")
