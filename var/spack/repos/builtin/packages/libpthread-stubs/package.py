cc534013dfacd8159e57d874a689c2cdd6790e0f
Wouter Deconinck <wdconinc@gmail.com>
2023-03-27 02:48:55 -0500

    libpthread-stubs: adapt to XorgPackage (#36453)
    This resolves a loose end from #36241 (missed due to package name). `libpthread-stubs` is another package from the xcb project that is now tracked through https://gitlab.freedesktop.org/xorg/ instead. No new versions; no changed hashes.

--- var/spack/repos/builtin/packages/libpthread-stubs/package.py
+++ var/spack/repos/builtin/packages/libpthread-stubs/package.py
@@ -6,12 +6,22 @@
 from spack.package import *
 
 
-class LibpthreadStubs(AutotoolsPackage):
+class LibpthreadStubs(AutotoolsPackage, XorgPackage):
     """The libpthread-stubs package provides weak aliases for pthread
     functions not provided in libc or otherwise available by default."""
 
-    homepage = "https://xcb.freedesktop.org/"
-    url = "https://xcb.freedesktop.org/dist/libpthread-stubs-0.4.tar.gz"
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/pthread-stubs"
+    xorg_mirror_path = "xcb/libpthread-stubs-0.4.tar.gz"
 
-    version("0.4", sha256="50d5686b79019ccea08bcbd7b02fe5a40634abcfd4146b6e75c6420cc170e9d9")
-    version("0.3", sha256="3031f466cf0b06de6b3ccbf2019d15c4fcf75229b7d226a711bc1885b3a82cde")
+    maintainers("wdconinc")
+
+    version(
+        "0.4",
+        sha256="50d5686b79019ccea08bcbd7b02fe5a40634abcfd4146b6e75c6420cc170e9d9",
+        url="https://xcb.freedesktop.org/dist/libpthread-stubs-0.4.tar.gz",
+    )
+    version(
+        "0.3",
+        sha256="3031f466cf0b06de6b3ccbf2019d15c4fcf75229b7d226a711bc1885b3a82cde",
+        url="https://xcb.freedesktop.org/dist/libpthread-stubs-0.3.tar.gz",
+    )
