44a081a184fd189c6875deae6c9908bba2158eb6
Erik Heeren <erik.heeren@epfl.ch>
2023-03-28 15:55:33 +0200

    hadoop: 3.3.3

--- var/spack/repos/builtin/packages/hadoop/package.py
+++ var/spack/repos/builtin/packages/hadoop/package.py
@@ -17,6 +17,7 @@ class Hadoop(Package):
 
     license("Apache-2.0")
 
+    version("3.3.3", sha256="fa71c61bbaa427129aef09fec028b34dd542c65ad90fdccec5e7ef93d83b8764")
     version("3.3.2", sha256="b341587495b12eec0b244b517f21df88eb46ef634dc7dc3e5969455b80ce2ce5")
     version("3.3.0", sha256="ea1a0f0afcdfb9b6b9d261cdce5a99023d7e8f72d26409e87f69bda65c663688")
     version("3.2.2", sha256="97e73b46c3972cd3c40c2295bd9488843c24e8503c36e7c57f6e6ecc4e12b8c3")
