df00f6b7c10c7c451fc6b8d49ebb2ff7aa6bd2fb
jamessmillie <smillie@txcorp.com>
2023-03-28 15:47:34 -0600

    Fixing concretize unit test failure where uuid is the same on all OS, not windows specific. Making abridged version of environment unit test for windows

--- lib/spack/spack/test/environment.py
+++ lib/spack/spack/test/environment.py
@@ -5,11 +5,13 @@
 
 import os
 import pickle
+import sys
 
 import pytest
 
 from spack.environment import Environment
 from spack.environment.environment import SpackEnvironmentViewError, _error_on_nonempty_view_dir
+from llnl.util.symlink import _windows_can_symlink
 
 
 def test_environment_pickle(tmpdir):
@@ -19,6 +21,9 @@ def test_environment_pickle(tmpdir):
     assert isinstance(env2, Environment)
 
 
+@pytest.mark.skipif(
+    sys.platform == "win32" and not _windows_can_symlink(), reason="Requires elevated privileges."
+)
 def test_error_on_nonempty_view_dir(tmpdir):
     """Error when the target is not an empty dir"""
     with tmpdir.as_cwd():
@@ -45,3 +50,23 @@ def test_error_on_nonempty_view_dir(tmpdir):
 
         with pytest.raises(SpackEnvironmentViewError):
             _error_on_nonempty_view_dir("file")
+
+
+@pytest.mark.skipif(_windows_can_symlink(), reason="Requires base privileges.")
+@pytest.mark.skipif(sys.platform != "win32", reason="Windows only.")
+def test_error_on_nonempty_view_dir__win32_base(tmpdir):
+    """Error when the target is not an empty dir. Cant make broken links on purpose
+    without elevated privileges on windows so this version is abridged for windows
+    base user privileges."""
+    with tmpdir.as_cwd():
+        os.mkdir("empty_dir")
+        os.mkdir("nonempty_dir")
+        with open(os.path.join("nonempty_dir", "file"), "wb"):
+            pass
+
+        # This is OK.
+        _error_on_nonempty_view_dir("empty_dir")
+
+        # This is not OK.
+        with pytest.raises(SpackEnvironmentViewError):
+            _error_on_nonempty_view_dir("nonempty_dir")
