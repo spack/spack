a937e58ba6649d7c85d871d097c3cc6c75a3cb3e
Gregory Becker <becker33@llnl.gov>
2023-03-29 13:28:56 -0700

    update completion

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -977,7 +977,7 @@ _spack_env_loads() {
 _spack_env_view() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help"
+        SPACK_COMPREPLY="-h --help -f --force"
     else
         SPACK_COMPREPLY=""
     fi
