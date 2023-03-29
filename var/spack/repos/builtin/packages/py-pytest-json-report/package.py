e844e5b0bbdd6f50f0be9c5221b63893d0f67e72
Angus Gibson <angus@agibson.me>
2023-03-29 11:39:25 +1100

    py-pytest-json-report: new package

---
+++ var/spack/repos/builtin/packages/py-pytest-json-report/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyPytestJsonReport(PythonPackage):
+    """A pytest plugin to create test reports as JSON"""
+
+    homepage = "https://github.com/numirias/pytest-json-report"
+    pypi = "pytest-json-report/pytest-json-report-1.5.0.tar.gz"
+
+    maintainers("angus-g")
+
+    version("1.5.0", sha256="2dde3c647851a19b5f3700729e8310a6e66efb2077d674f27ddea3d34dc615de")
+
+    depends_on("py-pytest@3.8.0:")
+    depends_on("py-pytest-metadata")
