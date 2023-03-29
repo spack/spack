589414f4dd4d7f3eeb485644c2b17fd304ab1116
Luc Berger-Vergiat <lberge@sandia.gov>
2023-03-29 12:24:23 -0600

    Kokkos: add release 4.0.0

--- var/spack/repos/builtin/packages/kokkos/package.py
+++ var/spack/repos/builtin/packages/kokkos/package.py
@@ -25,6 +25,7 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
 
     version("master", branch="master")
     version("develop", branch="develop")
+    version("4.0.00", sha256="1829a423883d4b44223c7c3a53d3c51671145aad57d7d23e6a1a4bebf710dcf6")
     version("3.7.01", sha256="0481b24893d1bcc808ec68af1d56ef09b82a1138a1226d6be27c3b3c3da65ceb")
     version("3.7.00", sha256="62e3f9f51c798998f6493ed36463f66e49723966286ef70a9dcba329b8443040")
     version("3.6.01", sha256="1b80a70c5d641da9fefbbb652e857d7c7a76a0ebad1f477c253853e209deb8db")
