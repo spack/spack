d5273127bfecaeab3e5f1118a2f000e6b0613733
Ryan Krattiger <ryan.krattiger@kitware.com>
2023-03-29 10:01:28 -0500

    Ascent: 0.9.0 cannot build with vtk-m@2

--- var/spack/repos/builtin/packages/ascent/package.py
+++ var/spack/repos/builtin/packages/ascent/package.py
@@ -169,7 +169,7 @@ class Ascent(CMakePackage, CudaPackage):
     # VTK-m
     #######################
 
-    depends_on("vtk-m@1.9:", when="@0.9.0:")
+    depends_on("vtk-m@1.9:1.9", when="@0.9.0:")
 
     depends_on("vtk-m~tbb", when="@0.9.0: +vtkh")
     depends_on("vtk-m+openmp", when="@0.9.0: +vtkh+openmp")
