f13a8761cf97c41120f6b3219f85e0b713ee7d85
Angus Gibson <angus@agibson.me>
2023-03-29 12:48:51 +1100

    Fix py-pytest-json-report dependencies
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-pytest-json-report/package.py
+++ var/spack/repos/builtin/packages/py-pytest-json-report/package.py
@@ -16,5 +16,6 @@ class PyPytestJsonReport(PythonPackage):
 
     version("1.5.0", sha256="2dde3c647851a19b5f3700729e8310a6e66efb2077d674f27ddea3d34dc615de")
 
-    depends_on("py-pytest@3.8.0:")
-    depends_on("py-pytest-metadata")
+    depends_on("py-setuptools", type="build")
+    depends_on("py-pytest@3.8:", type=("build", "run"))
+    depends_on("py-pytest-metadata", type=("build", "run"))
