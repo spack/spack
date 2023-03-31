ad486ee8cb4322ebdf91db6813929c5aa171ec37
Benjamin Meyers <meyersbs@users.noreply.github.com>
2023-03-31 10:32:31 -0400

    New package py-json2html (#36111)
    * New package py-json2html
    
    * Remove deprecated python deps

---
+++ var/spack/repos/builtin/packages/py-json2html/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyJson2html(PythonPackage):
+    """Python wrapper to convert JSON into a human readable HTML Table
+    representation."""
+
+    homepage = "https://github.com/softvar/json2html"
+    pypi = "json2html/json2html-1.3.0.tar.gz"
+
+    maintainers("meyersbs")
+
+    version("1.3.0", sha256="8951a53662ae9cfd812685facdba693fc950ffc1c1fd1a8a2d3cf4c34600689c")
+
+    depends_on("py-setuptools", type="build")
