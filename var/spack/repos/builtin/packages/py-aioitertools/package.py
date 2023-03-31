1d06b0441bd69da171aab3ccc8d2819036e29c67
Alec Scott <scott112@llnl.gov>
2023-03-31 10:32:48 -0700

    Update package.py

--- var/spack/repos/builtin/packages/py-aioitertools/package.py
+++ var/spack/repos/builtin/packages/py-aioitertools/package.py
@@ -18,7 +18,6 @@ class PyAioitertools(PythonPackage):
     version("0.7.1", sha256="54a56c7cf3b5290d1cb5e8974353c9f52c677612b5d69a859369a020c53414a3")
 
     depends_on("python@3.6:", type=("build", "run"))
-    depends_on("python@3.6:3.9", type=("build", "run"), when="@0.11.0:")
     depends_on("py-flit-core@2:3", type="build")
     depends_on("py-typing-extensions@3.7:", when="@0.7.1", type=("build", "run"))
     depends_on("py-typing-extensions@4:", when="@0.11.0: ^python@:3.9", type=("build", "run"))
