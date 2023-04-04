3fbc9ada46942ab954fdf056223c9efe3e5f0d76
Adam J. Stewart <ajstewart426@gmail.com>
2023-04-03 20:12:48 -0500

    py-pandas: add v2.0.0 (#36617)

---
+++ var/spack/repos/builtin/packages/py-tzdata/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyTzdata(PythonPackage):
+    """Provider of IANA time zone data."""
+
+    homepage = "https://github.com/python/tzdata"
+    pypi = "tzdata/tzdata-2023.3.tar.gz"
+
+    version("2023.3", sha256="11ef1e08e54acb0d4f95bdb1be05da659673de4acbd21bf9c69e94cc5e907a3a")
+
+    depends_on("py-setuptools@40.8:", type="build")
