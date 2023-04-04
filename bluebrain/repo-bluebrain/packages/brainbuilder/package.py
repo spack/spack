ccbeeeb8d1b17eafa6ef163b94750f28d65530f5
MikeG <michael.gevaert@epfl.ch>
2023-04-04 11:03:59 +0200

    brainbuilder: bump to 0.18.4 (#1943)

--- bluebrain/repo-bluebrain/packages/brainbuilder/package.py
+++ bluebrain/repo-bluebrain/packages/brainbuilder/package.py
@@ -13,7 +13,7 @@ class Brainbuilder(PythonPackage):
     git = "ssh://git@bbpgitlab.epfl.ch/nse/brainbuilder.git"
 
     version("develop", branch="main")
-    version("0.18.3", tag="brainbuilder-v0.18.3")
+    version("0.18.4", tag="brainbuilder-v0.18.4")
 
     depends_on("py-setuptools", type=("build", "run"))
 
@@ -28,11 +28,10 @@ class Brainbuilder(PythonPackage):
     depends_on("py-scipy@0.13:", type=("build", "run"))
     depends_on("py-tqdm@4.0:", type=("build", "run"))
     depends_on("py-bluepy@2.1:", type=("build", "run"))
-    depends_on("py-libsonata@0.1.6:", type=("build", "run"))
+    depends_on("py-libsonata@0.1.8:", type=("build", "run"))
     depends_on("py-voxcell@3.1.1:", type=("build", "run"))
     depends_on("py-morphio@3", type=("build", "run"))
-    depends_on("py-bluepysnap@0.13.0:0", type=("build", "run"), when="@:0.18.3")
-    depends_on("py-bluepysnap@1.0.3:", type=("build", "run"), when="@0.18.4:")
+    depends_on("py-bluepysnap@1.0.3:", type=("build", "run"))
 
     depends_on("py-attrs@:19", type=("build", "run"))
     depends_on("py-entity-management@0.1.11:0", type=("build", "run"))
