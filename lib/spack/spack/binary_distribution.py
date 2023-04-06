99f61977f76a6fa66a5e9118463b0cab5e19a8db
Alex Richert <alexander.richert@noaa.gov>
2023-04-06 10:36:45 -0700

    Resolve name inconsistency for ensure_package_relocatable

--- lib/spack/spack/binary_distribution.py
+++ lib/spack/spack/binary_distribution.py
@@ -1323,7 +1323,7 @@ def _build_tarball_in_stage_dir(
                 tty.die(e)
     else:
         try:
-            check_package_relocatable(workdir, spec, allow_root)
+            ensure_package_relocatable(workdir, spec, allow_root)
         except Exception as e:
             shutil.rmtree(workdir)
             shutil.rmtree(tarfile_dir)
