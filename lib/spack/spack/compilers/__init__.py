4ee7ef7a755d2c7db18b9b2a1ba8bca658d4702c
Harmen Stoppels <harmenstoppels@gmail.com>
2023-04-06 17:58:16 +0200

    fix printing

--- lib/spack/spack/compilers/__init__.py
+++ lib/spack/spack/compilers/__init__.py
@@ -459,7 +459,10 @@ def get_compilers(config, cspec=None, arch_spec=None):
 
     for items in config:
         items = items["compiler"]
-        if cspec and items["spec"] != str(cspec):
+
+        # NOTE: in principle this should be equality not satisfies, but config can still
+        # be written in old format gcc@10.1.0 instead of gcc@=10.1.0.
+        if cspec and not cspec.satisfies(items["spec"]):
             continue
 
         # If an arch spec is given, confirm that this compiler
