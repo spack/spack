6407d50adb41be055c6f7b5213c24b7c239b1047
Jonathon Anderson <17242663+blue42u@users.noreply.github.com>
2023-04-11 13:40:09 -0500

    Update var/spack/repos/builtin/packages/py-torch/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-torch/package.py
+++ var/spack/repos/builtin/packages/py-torch/package.py
@@ -116,7 +116,7 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
 
     # https://github.com/pytorch/pytorch/issues/97397
     conflicts(
-        "~tensorpipe", when="+distributed", msg="TensorPipe must be enabled with +distributed"
+        "~tensorpipe", when="@1.8: +distributed", msg="TensorPipe must be enabled with +distributed"
     )
 
     conflicts(
