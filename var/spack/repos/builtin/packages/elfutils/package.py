8876ecde51e72b03cefa51b4c0ca16a8aa5fd04a
tmadlener <thomas.madlener@desy.de>
2023-04-11 10:12:42 +0200

    elfutils: Make libarchive iconv variant version dependent
    Older versions build fine with iconv

--- var/spack/repos/builtin/packages/elfutils/package.py
+++ var/spack/repos/builtin/packages/elfutils/package.py
@@ -78,14 +78,16 @@ class Elfutils(AutotoolsPackage, SourcewarePackage):
     # debuginfod has extra dependencies
     # NB: Waiting on an elfutils patch before we can use libmicrohttpd@0.9.51
     depends_on("libmicrohttpd@0.9.33:0.9.50", type="link", when="+debuginfod")
-    # libarchive with iconv doesn't configure: https://github.com/spack/spack/issues/36710
-    depends_on("libarchive@3.1.2: ~iconv", type="link", when="+debuginfod")
+    depends_on("libarchive@3.1.2:", type="link", when="+debuginfod")
     depends_on("sqlite@3.7.17:", type="link", when="+debuginfod")
     depends_on("curl@7.29.0:", type="link", when="+debuginfod")
 
     conflicts("%gcc@7.2.0:", when="@0.163")
 
     provides("elf@1")
+
+    # libarchive with iconv doesn't configure: https://github.com/spack/spack/issues/36710
+    conflicts("libarchive@3.6.2 +iconv", when="+debuginfod")
 
     # Elfutils uses nested functions in C code, which is implemented
     # in gcc, but not in clang. C code compiled with gcc is
