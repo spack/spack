041218ef9460cb83239cc37454914a3c513e68ff
Alec Scott <hi@alecbcs.com>
2023-04-11 21:49:58 -0700

    task: add v2.6.2 (#36750)

--- var/spack/repos/builtin/packages/task/package.py
+++ var/spack/repos/builtin/packages/task/package.py
@@ -12,6 +12,7 @@ class Task(CMakePackage):
     homepage = "https://www.taskwarrior.org"
     url = "https://taskwarrior.org/download/task-2.4.4.tar.gz"
 
+    version("2.6.2", sha256="b1d3a7f000cd0fd60640670064e0e001613c9e1cb2242b9b3a9066c78862cfec")
     version("2.5.1", sha256="d87bcee58106eb8a79b850e9abc153d98b79e00d50eade0d63917154984f2a15")
     version("2.4.4", sha256="7ff406414e0be480f91981831507ac255297aab33d8246f98dbfd2b1b2df8e3b")
 
