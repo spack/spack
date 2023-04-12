a241d7818e5c988ef41ff8ab458cd0a97204de46
wdconinc <wdconinc@users.noreply.github.com>
2023-04-12 19:15:26 +0000

    [@spackbot] updating style on behalf of wdconinc

--- var/spack/repos/builtin/packages/py-codecov/package.py
+++ var/spack/repos/builtin/packages/py-codecov/package.py
@@ -13,8 +13,12 @@ class PyCodecov(PythonPackage):
     pypi = "codecov/codecov-2.0.15.tar.gz"
 
     # Since codecov has been removed from PyPI, py-codecov is deprecated.
-    # The new codecov uploader can be installed with the package codecov. 
-    version("2.0.15", sha256="8ed8b7c6791010d359baed66f84f061bba5bd41174bf324c31311e8737602788", deprecated=True)
+    # The new codecov uploader can be installed with the package codecov.
+    version(
+        "2.0.15",
+        sha256="8ed8b7c6791010d359baed66f84f061bba5bd41174bf324c31311e8737602788",
+        deprecated=True,
+    )
 
     depends_on("py-setuptools", type=("build", "run"))
     depends_on("py-requests@2.7.9:", type=("build", "run"))
