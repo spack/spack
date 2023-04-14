98729170af6ca2282ff0e299636353e72e3c43a9
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-04-14 08:15:11 -0600

    Add fckit@0.10.1

--- var/spack/repos/builtin/packages/fckit/package.py
+++ var/spack/repos/builtin/packages/fckit/package.py
@@ -20,6 +20,7 @@ class Fckit(CMakePackage):
 
     version('master', branch='master')
     version('develop', branch='develop')
+    version("0.10.1", sha256="9cde04fefa50624bf89068ab793cc2e9437c0cd1c271a41af7d54dbd37c306be")
     version("0.10.0", sha256="f16829f63a01cdef5e158ed2a51f6d4200b3fe6dce8f251af158141a1afe482b")
     version("0.9.5", sha256="183cd78e66d3283d9e6e8e9888d3145f453690a4509fb701b28d1ac6757db5de")
 
