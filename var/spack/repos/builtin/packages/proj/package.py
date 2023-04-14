0cfdc218e3cbd3563ae5ef863fc39ea4ef597661
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2023-04-15 00:18:21 +0200

    proj: fix installation of datum grid with Autotools (#36906)

--- var/spack/repos/builtin/packages/proj/package.py
+++ var/spack/repos/builtin/packages/proj/package.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import spack.builder
 from spack.build_systems import autotools, cmake
 from spack.package import *
 
@@ -105,7 +106,7 @@ class Proj(CMakePackage, AutotoolsPackage):
         self.setup_run_environment(env)
 
 
-class BaseBuilder:
+class BaseBuilder(metaclass=spack.builder.PhaseCallbacksMeta):
     def setup_dependent_build_environment(self, env, dependent_spec):
         self.pkg.setup_run_environment(env)
 
@@ -117,7 +118,7 @@ class BaseBuilder:
         install_tree(join_path("share", "proj"), self.prefix.share.proj)
 
 
-class CMakeBuilder(cmake.CMakeBuilder, BaseBuilder):
+class CMakeBuilder(BaseBuilder, cmake.CMakeBuilder):
     def cmake_args(self):
         args = [
             self.define_from_variant("ENABLE_TIFF", "tiff"),
@@ -128,7 +129,7 @@ class CMakeBuilder(cmake.CMakeBuilder, BaseBuilder):
         return args
 
 
-class AutotoolsBuilder(autotools.AutotoolsBuilder, BaseBuilder):
+class AutotoolsBuilder(BaseBuilder, autotools.AutotoolsBuilder):
     def configure_args(self):
         args = []
 
