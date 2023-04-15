5d03a85e033b81fde05cdc6aba8e2deb14a60da5
Adam J. Stewart <ajstewart426@gmail.com>
2023-04-15 17:52:00 -0500

    Deprecate ancient GEE

--- var/spack/repos/builtin/packages/py-earthengine-api/package.py
+++ var/spack/repos/builtin/packages/py-earthengine-api/package.py
@@ -14,7 +14,11 @@ class PyEarthengineApi(PythonPackage):
     pypi = "earthengine-api/earthengine-api-0.1.186.tar.gz"
 
     version("0.1.344", sha256="bc5a270b8296aaae8574e68dfd93fe878bc5fbe77d1c41f90bcb5e5b830ca5c8")
-    version("0.1.186", sha256="ced86dc969f5db13eea91944e29c39146bacbb7026a46f4b4ac349b365979627")
+    version(
+        "0.1.186",
+        sha256="ced86dc969f5db13eea91944e29c39146bacbb7026a46f4b4ac349b365979627",
+        deprecated=True,
+    )
 
     depends_on("py-setuptools", type="build")
     depends_on("py-google-cloud-storage", when="@0.1.344:", type=("build", "run"))
