7ca3420b284f02141c2e6a4fef99b450ac29cc58
Wouter Deconinck <wdconinc@gmail.com>
2023-04-17 15:47:26 -0500

    codecov: versions -> _versions, fix style

--- var/spack/repos/builtin/packages/codecov/package.py
+++ var/spack/repos/builtin/packages/codecov/package.py
@@ -13,16 +13,16 @@ class Codecov(Package):
 
     homepage = "https://codecov.io"
 
-    versions = {
+    _versions = {
         "0.4.1": {
             "linux": {
-                "x86_64": "32cb14b5f3aaacd67f4c1ff55d82f037d3cd10c8e7b69c051f27391d2e66e15c",
+                "x86_64": "32cb14b5f3aaacd67f4c1ff55d82f037d3cd10c8e7b69c051f27391d2e66e15c"
             },
             "darwin": {
-                "x86_64": "4ab0f06f06e9c4d25464f155b0aff36bfc1e8dbcdb19bfffd586beed1269f3af",
+                "x86_64": "4ab0f06f06e9c4d25464f155b0aff36bfc1e8dbcdb19bfffd586beed1269f3af"
             },
             "windows": {
-                "x86_64": "e0cda212aeaebe695509ce8fa2d608760ff70bc932003f544f1ad368ac5450a8",
+                "x86_64": "e0cda212aeaebe695509ce8fa2d608760ff70bc932003f544f1ad368ac5450a8"
             },
         }
     }
@@ -30,9 +30,9 @@ class Codecov(Package):
     system = platform.system().lower()
     machine = platform.machine().lower()
 
-    for ver in versions:
-        if system in versions[ver] and machine in versions[ver][system]:
-            version(ver, sha256=versions[ver][system][machine], expand=False)
+    for ver in _versions:
+        if system in _versions[ver] and machine in _versions[ver][system]:
+            version(ver, sha256=_versions[ver][system][machine], expand=False)
 
     def url_for_version(self, version):
         _url_base = f"https://github.com/codecov/uploader/releases/download/v{version}/codecov"
