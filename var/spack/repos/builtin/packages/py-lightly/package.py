83b7b498b789f2a172faa476894a491f8d4d876c
Adam J. Stewart <ajstewart426@gmail.com>
2023-04-17 08:02:13 -0500

    py-lightly: py-torch+distributed required (#36955)

--- var/spack/repos/builtin/packages/py-lightly/package.py
+++ var/spack/repos/builtin/packages/py-lightly/package.py
@@ -17,15 +17,23 @@ class PyLightly(PythonPackage):
 
     version("1.4.1", sha256="4c64657639c66ee5c8b4b8d300fc9b5287dc7e14a260f3a2e04917dca7f57f5b")
 
+    # lightly/requirements/base.txt
     depends_on("py-certifi@14.05.14:", type=("build", "run"))
     depends_on("py-hydra-core@1:", type=("build", "run"))
     depends_on("py-lightly-utils@0.0", type=("build", "run"))
     depends_on("py-numpy@1.18.1:", type=("build", "run"))
     depends_on("py-python-dateutil@2.5.3:", type=("build", "run"))
     depends_on("py-requests@2.23:", type=("build", "run"))
+    # https://github.com/lightly-ai/lightly/pull/1149
     depends_on("py-setuptools@21:65.5.1", type=("build", "run"))
     depends_on("py-six@1.10:", type=("build", "run"))
-    depends_on("py-torchvision", type=("build", "run"))
     depends_on("py-tqdm@4.44:", type=("build", "run"))
     depends_on("py-urllib3@1.15.1:", type=("build", "run"))
+
+    # lightly/requirements/torch.txt
+    depends_on("py-torch", type=("build", "run"))
+    depends_on("py-torchvision", type=("build", "run"))
     depends_on("py-pytorch-lightning@1.0.4:", type=("build", "run"))
+
+    # https://github.com/lightly-ai/lightly/issues/1153
+    depends_on("py-torch+distributed", type=("build", "run"))
