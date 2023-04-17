3f4833922bdbe61436bce2eecb2725b214084f46
RemiLacroix-IDRIS <RemiLacroix-IDRIS@users.noreply.github.com>
2023-04-17 07:56:17 +0000

    [@spackbot] updating style on behalf of RemiLacroix-IDRIS

--- var/spack/repos/builtin/packages/netcdf95/package.py
+++ var/spack/repos/builtin/packages/netcdf95/package.py
@@ -7,14 +7,14 @@ from spack.package import *
 
 
 class Netcdf95(CMakePackage):
-    """NetCDF95 is an alternative Fortran interface to the NetCDF library                                                                   
-       which uses Fortran 2003 features."""
+    """NetCDF95 is an alternative Fortran interface to the NetCDF library
+    which uses Fortran 2003 features."""
 
     homepage = "https://lguez.github.io/NetCDF95/"
-    git      = "https://github.com/lguez/NetCDF95.git"
+    git = "https://github.com/lguez/NetCDF95.git"
 
     maintainers = ["RemiLacroix-IDRIS"]
 
     version("0.3", tag="v0.3", submodules=True)
 
-    depends_on('netcdf-fortran')
+    depends_on("netcdf-fortran")
