9cc8608db2a845e6b9046d3aeb14d566f0aa7ef8
Benjamin Meyers <meyersbs@users.noreply.github.com>
2023-04-18 08:45:12 -0400

    Update var/spack/repos/builtin/packages/py-outdated/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-outdated/package.py
+++ var/spack/repos/builtin/packages/py-outdated/package.py
@@ -17,7 +17,7 @@ class PyOutdated(PythonPackage):
 
     version("0.2.2", sha256="4b7fdec88e36711120d096d485fc4d5035e4e5ffbd907cf3a6ce2af43058b970")
 
-    depends_on("py-setuptools@44:", type="build")
+    depends_on("py-setuptools@44:", type=("build", "run"))
     depends_on("py-setuptools-scm@3.4.3:+toml", type="build")
     depends_on("py-littleutils", type=("build", "run"))
     depends_on("py-requests", type=("build", "run"))
