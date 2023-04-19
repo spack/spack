d51af675ef345a191aa995b82609e5f6776184a3
Harmen Stoppels <harmenstoppels@gmail.com>
2023-04-19 23:17:47 +0200

    make version(...) kwargs explicit (#36998)
    - [x] Replace `version(ver, checksum=None, **kwargs)` signature with
          `version(ver, checksum=None, *, sha256=..., ...)` explicitly listing all arguments.
    - [x] Fix various issues in packages:
      - `tags` instead of `tag`
      - `default` instead of `preferred`
      - `sha26` instead of `sha256`
      - etc
    
    Also, use `sha256=...` consistently.
    
    Note: setting `sha256` currently doesn't validate the checksum length, so you could do
    `sha256="a"*32` and it would get checked as `md5`... but that's something for another PR.

--- var/spack/repos/builder.test/packages/old-style-autotools/package.py
+++ var/spack/repos/builder.test/packages/old-style-autotools/package.py
@@ -15,8 +15,8 @@ class OldStyleAutotools(AutotoolsPackage):
     homepage = "http://www.example.com"
     url = "http://www.example.com/a-1.0.tar.gz"
 
-    version("2.0", "abcdef0123456789abcdef0123456789")
-    version("1.0", "0123456789abcdef0123456789abcdef")
+    version("2.0", md5="abcdef0123456789abcdef0123456789")
+    version("1.0", md5="0123456789abcdef0123456789abcdef")
 
     def configure(self, spec, prefix):
         pass
