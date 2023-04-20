4806fdad61dd247f130734ef1ec0ddaf0abfc244
Adam J. Stewart <ajstewart426@gmail.com>
2023-04-20 17:29:53 -0500

    py-lightly: add v1.4.2 (#37053)
    * py-lightly: add v1.4.2
    * pytorch and lightning 2.0 now supported

--- var/spack/repos/builtin/packages/py-lightly/package.py
+++ var/spack/repos/builtin/packages/py-lightly/package.py
@@ -15,6 +15,7 @@ class PyLightly(PythonPackage):
 
     maintainers("adamjstewart")
 
+    version("1.4.2", sha256="bae451fcd04fbd3cc14b044a2583ae24591533d4a8a6ff51e5f1477f9a077648")
     version("1.4.1", sha256="4c64657639c66ee5c8b4b8d300fc9b5287dc7e14a260f3a2e04917dca7f57f5b")
 
     # lightly/requirements/base.txt
@@ -24,16 +25,18 @@ class PyLightly(PythonPackage):
     depends_on("py-numpy@1.18.1:", type=("build", "run"))
     depends_on("py-python-dateutil@2.5.3:", type=("build", "run"))
     depends_on("py-requests@2.23:", type=("build", "run"))
-    # https://github.com/lightly-ai/lightly/pull/1149
-    depends_on("py-setuptools@21:65.5.1", type=("build", "run"))
+    depends_on("py-setuptools@21:", when="@1.4.2:", type="build")
+    depends_on("py-setuptools@21:65.5.1", when="@:1.4.1", type=("build", "run"))
     depends_on("py-six@1.10:", type=("build", "run"))
     depends_on("py-tqdm@4.44:", type=("build", "run"))
     depends_on("py-urllib3@1.15.1:", type=("build", "run"))
 
     # lightly/requirements/torch.txt
     depends_on("py-torch", type=("build", "run"))
+    depends_on("py-torch@:1", when="@:1.4.1", type=("build", "run"))
     depends_on("py-torchvision", type=("build", "run"))
     depends_on("py-pytorch-lightning@1.0.4:", type=("build", "run"))
+    depends_on("py-pytorch-lightning@1.0.4:1", when="@:1.4.1", type=("build", "run"))
 
     # https://github.com/lightly-ai/lightly/issues/1153
     depends_on("py-torch+distributed", type=("build", "run"))
