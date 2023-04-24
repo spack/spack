f68808bea640002f34ccc0a66a2006bd462ce33e
Sangu-Mbekelu <60823467+Sangu-Mbekelu@users.noreply.github.com>
2023-04-24 17:07:17 +0000

    [@spackbot] updating style on behalf of Sangu-Mbekelu

--- var/spack/repos/builtin/packages/py-sacrebleu/package.py
+++ var/spack/repos/builtin/packages/py-sacrebleu/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PySacrebleu(PythonPackage):
@@ -13,18 +13,18 @@ class PySacrebleu(PythonPackage):
     comparable BLEU scores."""
 
     homepage = "https://github.com/mjpost/sacrebleu"
-    pypi    = "sacrebleu/sacrebleu-2.0.0.tar.gz"
+    pypi = "sacrebleu/sacrebleu-2.0.0.tar.gz"
 
-    version('2.0.0', sha256='51fb69b6683f1b9999cd180143bb6b21d7841744537c9aab235cfe676550f0cf')
+    version("2.0.0", sha256="51fb69b6683f1b9999cd180143bb6b21d7841744537c9aab235cfe676550f0cf")
 
-    depends_on('python@3.6.0:',                 type=('build', 'run'))
-    depends_on('py-setuptools',                 type='build')
-    depends_on('py-portalocker',                type=('build', 'run'))
-    depends_on('py-regex',                      type=('build', 'run'))
-    depends_on('py-tabulate@0.8.9:',            type=('build', 'run'))
-    depends_on('py-numpy@1.17:',                type=('build', 'run'))
-    depends_on('py-colorama',                   type=('build', 'run'))
+    depends_on("python@3.6.0:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-portalocker", type=("build", "run"))
+    depends_on("py-regex", type=("build", "run"))
+    depends_on("py-tabulate@0.8.9:", type=("build", "run"))
+    depends_on("py-numpy@1.17:", type=("build", "run"))
+    depends_on("py-colorama", type=("build", "run"))
 
     def patch(self):
-        touch = which('touch')
-        touch('CHANGELOG.md')
+        touch = which("touch")
+        touch("CHANGELOG.md")
