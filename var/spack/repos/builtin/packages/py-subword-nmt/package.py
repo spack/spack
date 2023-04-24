9b34c1893e8ec9d458a578c6bc9bd42a2a17308c
Sangu-Mbekelu <60823467+Sangu-Mbekelu@users.noreply.github.com>
2023-04-24 17:06:21 +0000

    [@spackbot] updating style on behalf of Sangu-Mbekelu

--- var/spack/repos/builtin/packages/py-subword-nmt/package.py
+++ var/spack/repos/builtin/packages/py-subword-nmt/package.py
@@ -3,18 +3,18 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class PySubwordNmt(PythonPackage):
     """This repository contains preprocessing scripts to segment text into
     subword units."""
 
-# pypi only has the whl file.
+    # pypi only has the whl file.
     homepage = "https://github.com/joeynmt/joeynmt"
-    url      = "https://github.com/rsennrich/subword-nmt/archive/refs/tags/v0.3.7.zip"
+    url = "https://github.com/rsennrich/subword-nmt/archive/refs/tags/v0.3.7.zip"
 
-    version('0.3.7', sha256='5c3eafe8d85d872a3bbde722b130fb25db19cc7942561936bfae26b6daf51ba0')
+    version("0.3.7", sha256="5c3eafe8d85d872a3bbde722b130fb25db19cc7942561936bfae26b6daf51ba0")
 
-    depends_on('python')
-    depends_on('py-setuptools', type='build')
+    depends_on("python")
+    depends_on("py-setuptools", type="build")
