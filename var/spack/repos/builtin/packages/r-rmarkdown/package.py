bf83ce515b86c0fa0815a93d31731a54e9f5a353
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-25 17:49:41 -0500

    add version 2.21 to r-rmarkdown

--- var/spack/repos/builtin/packages/r-rmarkdown/package.py
+++ var/spack/repos/builtin/packages/r-rmarkdown/package.py
@@ -14,6 +14,7 @@ class RRmarkdown(RPackage):
 
     cran = "rmarkdown"
 
+    version("2.21", sha256="c25b20a422d11a115c93460f41c488874002154abb349b14e0d6518682fdac28")
     version("2.17", sha256="aa576c458ec4c2e8468aaa4e3f60202d8d9c7ef54fa01d6b2d243bffee08c4be")
     version("2.16", sha256="d3d34e0419c419d3ab20eb60952a0f0f4c391d202277af55dcd673d25561fa71")
     version("2.14", sha256="e9ec17afa4d9d6e8cf555b56e0c00acc189d8ec0b4406680b14d71d62f0c3220")
@@ -29,6 +30,7 @@ class RRmarkdown(RPackage):
     depends_on("r@3.0:", type=("build", "run"))
     depends_on("r-bslib@0.2.5.1:", type=("build", "run"), when="@2.14:")
     depends_on("r-evaluate@0.13:", type=("build", "run"))
+    depends_on("r-fontawesome@0.5.0:", type=("build", "run"), when="@2.21:")
     depends_on("r-htmltools@0.3.5:", type=("build", "run"))
     depends_on("r-htmltools@0.5.1:", type=("build", "run"), when="@2.16:")
     depends_on("r-jquerylib", type=("build", "run"), when="@2.11:")
@@ -41,6 +43,7 @@ class RRmarkdown(RPackage):
     depends_on("r-xfun@0.15:", type=("build", "run"), when="@2.6:")
     depends_on("r-xfun@0.21:", type=("build", "run"), when="@2.8:")
     depends_on("r-xfun@0.30:", type=("build", "run"), when="@2.14:")
+    depends_on("r-xfun@0.36:", type=("build", "run"), when="@2.21:")
     depends_on("r-yaml@2.1.19:", type=("build", "run"))
     depends_on("pandoc@1.12.3:")
     depends_on("pandoc@1.14:", when="@2.6:")
