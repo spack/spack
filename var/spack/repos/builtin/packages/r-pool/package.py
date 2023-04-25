76f25d48d2dcc8557d6b68df6a4f1bf2a67a5fcd
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-25 17:24:51 -0500

    add version 1.0.1 to r-pool

--- var/spack/repos/builtin/packages/r-pool/package.py
+++ var/spack/repos/builtin/packages/r-pool/package.py
@@ -15,9 +15,12 @@ class RPool(RPackage):
 
     cran = "pool"
 
+    version("1.0.1", sha256="73d5dffd55e80fdadb88401f12570fcf08e932c4c86761931241f9841fddadbf")
     version("0.1.6", sha256="cdbe5f6c7f757c01893dc9870df0fb8d300829da0e427f6c2559b01caa52d9e1")
 
     depends_on("r@3.0.0:", type=("build", "run"))
     depends_on("r-dbi", type=("build", "run"))
     depends_on("r-r6", type=("build", "run"))
+    depends_on("r-withr", type=("build", "run"), when="@1.0.1:")
+    depends_on("r-rlang@1.0.0:", type=("build", "run"), when="@1.0.1:")
     depends_on("r-later@1.0.0:", type=("build", "run"))
