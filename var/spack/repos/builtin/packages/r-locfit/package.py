ea6fb8b34ba932baa5099a2f09a6008066cbed9d
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-25 16:54:56 -0500

    add version 1.5-9.7 to r-locfit

--- var/spack/repos/builtin/packages/r-locfit/package.py
+++ var/spack/repos/builtin/packages/r-locfit/package.py
@@ -14,6 +14,7 @@ class RLocfit(RPackage):
 
     cran = "locfit"
 
+    version("1.5-9.7", sha256="48e5fcd089fbc609d8e4c62c390425fba1dd167ad95ae0bddc175cbbe1517aff")
     version("1.5-9.6", sha256="1ee89e4003cb769feae61ada7ac0a971df30644824f7ed84a21dd5719f713476")
     version("1.5-9.5", sha256="fd9f2bad9d8beec8be4843dc80e38ebe0f388835a7003490f67e57eeb9e6de23")
     version("1.5-9.4", sha256="d9d3665c5f3d49f698fb4675daf40a0550601e86db3dc00f296413ceb1099ced")
