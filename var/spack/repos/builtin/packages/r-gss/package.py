5d6a46bb4654f56ec0fb162d458be17e08758543
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-25 16:31:20 -0500

    add version 2.2-4 to r-gss

--- var/spack/repos/builtin/packages/r-gss/package.py
+++ var/spack/repos/builtin/packages/r-gss/package.py
@@ -14,6 +14,7 @@ class RGss(RPackage):
 
     cran = "gss"
 
+    version("2.2-4", sha256="953e89dfe3bee9cac51df3e5325bf4d1496ad76e4393706c4efdb1834c0c7441")
     version("2.2-3", sha256="24306401cf4e5869f8a690eca7e17c044ece83edd66969bd2daf5976272d244b")
     version("2.2-2", sha256="1da4da894378ee730cff9628e8b4d2a0d7dfa344b94e5bce6953e66723c21fe4")
     version("2.1-10", sha256="26c47ecae6a9b7854a1b531c09f869cf8b813462bd8093e3618e1091ace61ee2")
