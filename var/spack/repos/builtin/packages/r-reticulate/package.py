c2a91ff3eb5f1ca2240fbc5eaf798dedb5377314
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-25 17:45:36 -0500

    add version 1.28 to r-reticulate

--- var/spack/repos/builtin/packages/r-reticulate/package.py
+++ var/spack/repos/builtin/packages/r-reticulate/package.py
@@ -17,6 +17,7 @@ class RReticulate(RPackage):
 
     cran = "reticulate"
 
+    version("1.28", sha256="58a299ed18faaa3ff14936752fcc2b86e64ae18fc9f36befdfd492ccb251516f")
     version("1.26", sha256="3fd74823bde1b0e094db7c2bf6b40ee2501f8d724b4c35b53da95c3c588c74c5")
     version("1.25", sha256="2125af9e75939c3b7c0dc74f28f42606e816d63aa1143baf631c318ff5ff3a2c")
     version("1.24", sha256="b918c5204916601f757ad0fc629b2ae1eabab7cdf7f6aa2e219d26e506d916cc")
