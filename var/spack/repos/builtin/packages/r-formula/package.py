28619fe59c7db720cf63c57082115375c9da1b56
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-25 16:11:15 -0500

    add version 1.2-5 to r-formula

--- var/spack/repos/builtin/packages/r-formula/package.py
+++ var/spack/repos/builtin/packages/r-formula/package.py
@@ -16,6 +16,7 @@ class RFormula(RPackage):
 
     cran = "Formula"
 
+    version("1.2-5", sha256="86254674600d64e18b65d52f42d7ebfc217c8e1945cb63ac06da22cbf04d355c")
     version("1.2-4", sha256="cb70e373b5ed2fc8450937fb3321d37dfd22dcc6f07cb872a419d51205125caf")
     version("1.2-3", sha256="1411349b20bd09611a9fd0ee6d15f780c758ad2b0e490e908facb49433823872")
     version("1.2-2", sha256="8def4600fb7457d38db8083733477501b54528974aa216e4adf8871bff4aa429")
