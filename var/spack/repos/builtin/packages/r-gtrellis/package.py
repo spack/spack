d034bb96428c9744c6f3e36ad86684ed6bdbea86
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-27 16:50:11 -0500

    add version 1.32.0 to bioconductor package r-gtrellis

--- var/spack/repos/builtin/packages/r-gtrellis/package.py
+++ var/spack/repos/builtin/packages/r-gtrellis/package.py
@@ -18,6 +18,7 @@ class RGtrellis(RPackage):
 
     bioc = "gtrellis"
 
+    version("1.32.0", commit="4c2361bde90a556aa5a585d45f056b3ddc806f9b")
     version("1.30.0", commit="da93b30ef812e231c47aa83da5c521f1801b3d14")
     version("1.28.0", commit="d770a7b3441e4003869c88cfd8e21fd6508b86c4")
     version("1.26.0", commit="f2c3121b31ad1b422e2cf503435d0590e903ff3f")
