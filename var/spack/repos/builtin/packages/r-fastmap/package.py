bb464112e598bc026890027b5903c2584aa06f67
Alec Scott <hi@alecbcs.com>
2023-04-26 17:27:49 -0700

    r-fastmap: add v1.1.1 (#37196)

--- var/spack/repos/builtin/packages/r-fastmap/package.py
+++ var/spack/repos/builtin/packages/r-fastmap/package.py
@@ -18,5 +18,6 @@ class RFastmap(RPackage):
 
     cran = "fastmap"
 
+    version("1.1.1", sha256="3623809dd016ae8abd235200ba7834effc4b916915a059deb76044137c5c7173")
     version("1.1.0", sha256="9113e526b4c096302cfeae660a06de2c4c82ae4e2d3d6ef53af6de812d4c822b")
     version("1.0.1", sha256="4778b05dfebd356f8df980dfeff3b973a72bca14898f870e5c40c1d84db9faec")
