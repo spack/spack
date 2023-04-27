2bdcdb09a4c5a544c9583a152ca8a4f3a519a497
Chris Green <greenc@fnal.gov>
2023-04-27 11:37:23 -0500

    [py-sphinx-design] New versions 0.4.0, 0.4.1 (#37234)
    * [py-sphinx-design] New versions 0.4.0, 0.4.1
    
    * conflicts() -> depends_on()
    
    Per @adamjstewart
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-sphinx-design/package.py
+++ var/spack/repos/builtin/packages/py-sphinx-design/package.py
@@ -14,8 +14,11 @@ class PySphinxDesign(PythonPackage):
 
     maintainers("ax3l", "adamjstewart")
 
+    version("0.4.1", sha256="5b6418ba4a2dc3d83592ea0ff61a52a891fe72195a4c3a18b2fa1c7668ce4708")
+    version("0.4.0", sha256="b92948614900967499617d99aadd38ce5975ede924a18c7478cc6b8ec188f76b")
     version("0.3.0", sha256="7183fa1fae55b37ef01bda5125a21ee841f5bbcbf59a35382be598180c4cefba")
 
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-flit-core@3.4:3", type=("build"))
-    depends_on("py-sphinx@4:5", type=("build", "run"))
+    depends_on("py-sphinx@4:5", when="@0.3", type=("build", "run"))
+    depends_on("py-sphinx@4:6", when="@0.4:", type=("build", "run"))
