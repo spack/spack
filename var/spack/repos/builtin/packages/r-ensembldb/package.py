5ba9fc2ca9d13c7a13d792e6b21b947785a9b9e1
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-27 16:43:43 -0500

    add version 2.24.0 to bioconductor package r-ensembldb

--- var/spack/repos/builtin/packages/r-ensembldb/package.py
+++ var/spack/repos/builtin/packages/r-ensembldb/package.py
@@ -24,6 +24,7 @@ class REnsembldb(RPackage):
 
     bioc = "ensembldb"
 
+    version("2.24.0", commit="45a79a438fde11b0d244b071e6ae2b652100be03")
     version("2.22.0", commit="4dda178a14e080c643bbd8c4dd6378bfe4e6ee9f")
     version("2.20.2", commit="ac1fb8389efd88099600af298d6bb3384206f9ed")
     version("2.20.1", commit="e547d184730cfe5e65f59e4f3512395fb1cdba1a")
