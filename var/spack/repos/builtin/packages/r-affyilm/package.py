d0e1e27b34bc93d753f1342f6dc4e43c1e73779b
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-27 16:34:49 -0500

    add version 1.52.0 to bioconductor package r-affyilm

--- var/spack/repos/builtin/packages/r-affyilm/package.py
+++ var/spack/repos/builtin/packages/r-affyilm/package.py
@@ -16,6 +16,7 @@ class RAffyilm(RPackage):
 
     bioc = "affyILM"
 
+    version("1.52.0", commit="08ed8c60921ba1b9e04fa90e156eef1a3c899d15")
     version("1.50.0", commit="185cd8e4712a3378ce7a156d4940224bbb2c4122")
     version("1.48.0", commit="4603a4c4d6c2330a8a56a7bb657dc56c51a9393a")
     version("1.46.0", commit="67ffbfa6c881ed83d15604bf4463fe5dba81036b")
