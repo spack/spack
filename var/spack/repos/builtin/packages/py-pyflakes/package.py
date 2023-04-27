f7bb6f1bd5c3ef4f7da2de5a8c5f1f3ec784bd41
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-04-27 02:53:49 +0200

    py-flake8: add 6.0.0 and update dependencies (#37222)

--- var/spack/repos/builtin/packages/py-pyflakes/package.py
+++ var/spack/repos/builtin/packages/py-pyflakes/package.py
@@ -12,6 +12,7 @@ class PyPyflakes(PythonPackage):
     homepage = "https://github.com/PyCQA/pyflakes"
     pypi = "pyflakes/pyflakes-2.4.0.tar.gz"
 
+    version("3.0.1", sha256="ec8b276a6b60bd80defed25add7e439881c19e64850afd9b346283d4165fd0fd")
     version("2.5.0", sha256="491feb020dca48ccc562a8c0cbe8df07ee13078df59813b83959cbdada312ea3")
     version("2.4.0", sha256="05a85c2872edf37a4ed30b0cce2f6093e1d0581f8c19d7393122da7e25b2b24c")
     version("2.3.0", sha256="e59fd8e750e588358f1b8885e5a4751203a0516e0ee6d34811089ac294c8806f")
