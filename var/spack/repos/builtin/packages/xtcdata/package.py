086a06989363308e004be2d675b86a01a8c5b7c7
eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
2023-04-28 06:31:06 -0500

    new package: xtcdata (#37261)

---
+++ var/spack/repos/builtin/packages/xtcdata/package.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Xtcdata(CMakePackage):
+    """LCLS II Developement: XTCData."""
+
+    homepage = "https://github.com/slac-lcls/lcls2"
+    url = "https://github.com/slac-lcls/lcls2/archive/refs/tags/3.3.37.tar.gz"
+
+    maintainers = ["valmar"]
+
+    version("3.3.37", sha256="127a5ae44c9272039708bd877849a3af354ce881fde093a2fc6fe0550b698b72")
+
+    root_cmakelists_dir = "xtcdata"
