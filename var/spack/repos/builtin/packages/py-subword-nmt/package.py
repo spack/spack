f6210308eb2207ab381daeb515513ffe80b9a557
Sangu Mbekelu <60823467+Sangu-Mbekelu@users.noreply.github.com>
2023-04-27 23:43:29 -0400

    Update package.py
    updating package based on review

--- var/spack/repos/builtin/packages/py-subword-nmt/package.py
+++ var/spack/repos/builtin/packages/py-subword-nmt/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -15,6 +15,5 @@ class PySubwordNmt(PythonPackage):
     url = "https://github.com/rsennrich/subword-nmt/archive/refs/tags/v0.3.7.zip"
 
     version("0.3.7", sha256="5c3eafe8d85d872a3bbde722b130fb25db19cc7942561936bfae26b6daf51ba0")
-
-    depends_on("python")
+    
     depends_on("py-setuptools", type="build")
