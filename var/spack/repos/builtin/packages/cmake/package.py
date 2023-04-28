f50574cb9ec80c288fda64122d0b4caad5629f63
Gregory Becker <becker33@llnl.gov>
2023-04-28 14:31:14 -0700

    fixup: revert two incorrect deletions

--- var/spack/repos/builtin/packages/cmake/package.py
+++ var/spack/repos/builtin/packages/cmake/package.py
@@ -155,6 +155,13 @@ class Cmake(Package):
     version("3.0.2", sha256="6b4ea61eadbbd9bec0ccb383c29d1f4496eacc121ef7acf37c7a24777805693e")
     version("2.8.10.2", sha256="ce524fb39da06ee6d47534bbcec6e0b50422e18b62abc4781a4ba72ea2910eb1")
 
+    variant(
+        "build_type",
+        default="Release",
+        description="CMake build type",
+        values=("Debug", "Release", "RelWithDebInfo", "MinSizeRel"),
+    )
+
     # Revert the change that introduced a regression when parsing mpi link
     # flags, see: https://gitlab.kitware.com/cmake/cmake/issues/19516
     patch("cmake-revert-findmpi-link-flag-list.patch", when="@3.15.0")
