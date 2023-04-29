5dc247d034653d1c5f741385ffb71fa44d5c75b3
Glenn Johnson <glenn-johnson@uiowa.edu>
2023-04-28 20:50:22 -0500

    r-affyplm: add zlib dependency

--- var/spack/repos/builtin/packages/r-affyplm/package.py
+++ var/spack/repos/builtin/packages/r-affyplm/package.py
@@ -35,3 +35,4 @@ class RAffyplm(RPackage):
     depends_on("r-gcrma", type=("build", "run"))
     depends_on("r-preprocesscore@1.5.1:", type=("build", "run"))
     depends_on("r-zlibbioc", type=("build", "run"))
+    depends_on("zlib")
