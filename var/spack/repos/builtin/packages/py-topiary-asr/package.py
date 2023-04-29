937e36ec4a02a8563a11109af54e53e43e95dac4
snehring <7978778+snehring@users.noreply.github.com>
2023-04-29 16:52:32 -0500

    Feature/topiary (#37157)
    * generax: adding new package generax
    
    * muscle5: adding new package muscle5
    
    * py-custom-inherit: adding new package py-custom-inherit
    
    * py-ete3: adding new package py-ete3
    
    * py-itolapi: adding new package py-itolapi
    
    * py-opentree: adding new package py-opentree
    
    * py-pypng: adding new package py-pypng
    
    * py-toyplot: adding new package py-toyplot
    
    * py-toytree: adding new package py-toytree
    
    * py-pastml: adding new package py-pastml
    
    * raxml-ng: adding new version 1.1.0
    
    * py-topiary: adding new package py-topiary
    
    * generax: adding master branch version
    generax: adding version 2.0.1
    generax: add mpi variant
    
    * py-topiary: add main
    
    * generax: correcting commit for 2.0.1
    
    * Update var/spack/repos/builtin/packages/py-itolapi/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Update var/spack/repos/builtin/packages/py-opentree/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * py-topiary-asr: rename package, requested changes.
    
    * Update var/spack/repos/builtin/packages/py-topiary-asr/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

---
+++ var/spack/repos/builtin/packages/py-topiary-asr/package.py
@@ -0,0 +1,62 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyTopiaryAsr(PythonPackage):
+    """Python framework for doing ancestral sequence reconstruction."""
+
+    homepage = "https://github.com/harmslab/topiary"
+
+    url = "https://github.com/harmslab/topiary/archive/refs/tags/v0.9.9.tar.gz"
+    git = "https://github.com/harmslab/topiary.git"
+
+    maintainers("snehring")
+
+    version("main", branch="main")
+    version("0.9.9", sha256="5601fba92e7add33a3732482426b2c7ef46b0fccc4a4ea11357537e1b937903c")
+
+    depends_on("python@3.8:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+
+    depends_on("py-biopython", type=("build", "run"))
+    depends_on("py-numpy@:1.21", type=("build", "run"))
+    depends_on("py-pandas", type=("build", "run"))
+    depends_on("py-matplotlib", type=("build", "run"))
+    depends_on("py-ete3", type=("build", "run"))
+    depends_on("py-toytree", type=("build", "run"))
+    depends_on("py-dendropy", type=("build", "run"))
+    depends_on("py-pastml", type=("build", "run"))
+    depends_on("py-openpyxl", type=("build", "run"))
+    depends_on("py-opentree", type=("build", "run"))
+    depends_on("py-tqdm", type=("build", "run"))
+    depends_on("py-xlrd", type=("build", "run"))
+    # while undocumented, this requires mpi4py to run
+    depends_on("py-mpi4py", type=("build", "run"))
+
+    # runtime deps from https://topiary-asr.readthedocs.io/en/latest/installation.html#required-libraries
+    depends_on("blast-plus", type="run")
+    depends_on("muscle5@5.0:", type="run")
+    depends_on("generax@2.0:+mpi", type="run")
+    depends_on("raxml-ng@1.1:", type="run")
+
+    depends_on("mpi", type="run")
+    depends_on("openmpi+legacylaunchers", type="run", when="^openmpi schedulers=slurm")
+
+    conflicts("mpich")
+
+    def patch(self):
+        if self.spec.satisfies("^raxml-ng+mpi"):
+            filter_file(
+                r"RAXML_BINARY\s*=\s*\"raxml-ng\"$",
+                'RAXML_BINARY = "raxml-ng-mpi"',
+                join_path("topiary", "raxml", "_raxml.py"),
+            )
+            filter_file(
+                r"binary\s*=\s*\"raxml-ng\"",
+                'binary = "raxml-ng-mpi"',
+                join_path("topiary", "_private", "installed.py"),
+            )
