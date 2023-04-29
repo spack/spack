47cbd7b9e2df0369854d3e495f9397de561a2aee
snehring <7978778+snehring@users.noreply.github.com>
2023-04-28 19:55:30 -0500

    Update var/spack/repos/builtin/packages/py-topiary-asr/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-topiary-asr/package.py
+++ var/spack/repos/builtin/packages/py-topiary-asr/package.py
@@ -23,7 +23,7 @@ class PyTopiaryAsr(PythonPackage):
     depends_on("py-setuptools", type="build")
 
     depends_on("py-biopython", type=("build", "run"))
-    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-numpy@:1.21", type=("build", "run"))
     depends_on("py-pandas", type=("build", "run"))
     depends_on("py-matplotlib", type=("build", "run"))
     depends_on("py-ete3", type=("build", "run"))
