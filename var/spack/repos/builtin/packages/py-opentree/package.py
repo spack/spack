8183210e59b032b0f927dfebca75963d258617a1
snehring <7978778+snehring@users.noreply.github.com>
2023-04-29 16:52:32 -0500

    Feature/topiary (#37157)
    * generax: adding new package generax
    
    * muscle5: adding new package muscle5
    
    * py-custom-inherit: adding new package py-custom-inherit
    
    * py-ete3: adding new package py-ete3
    
    * py-itolapi: adding new package py-itolapi
    
    * py-opentree: adding new package py-opentree
    
    * py-pypng: adding new package py-pypng
    
    * py-toyplot: adding new package py-toyplot
    
    * py-toytree: adding new package py-toytree
    
    * py-pastml: adding new package py-pastml
    
    * raxml-ng: adding new version 1.1.0
    
    * py-topiary: adding new package py-topiary
    
    * generax: adding master branch version
    generax: adding version 2.0.1
    generax: add mpi variant
    
    * py-topiary: add main
    
    * generax: correcting commit for 2.0.1
    
    * Update var/spack/repos/builtin/packages/py-itolapi/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Update var/spack/repos/builtin/packages/py-opentree/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * py-topiary-asr: rename package, requested changes.
    
    * Update var/spack/repos/builtin/packages/py-topiary-asr/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

---
+++ var/spack/repos/builtin/packages/py-opentree/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyOpentree(PythonPackage):
+    """Library for interacting with Open Tree of Life resources."""
+
+    homepage = "https://github.com/OpenTreeOfLife/python-opentree"
+    pypi = "opentree/opentree-1.0.1.tar.gz"
+
+    maintainers("snehring")
+
+    version("1.0.1", sha256="a765ae37cd72c232c292506c170656aaaa4be81e6de6ccd4845eec01bfe45e4d")
+
+    depends_on("py-setuptools", type=("build", "run"))
+
+    depends_on("py-requests@2.18:", type=("build", "run"))
+    depends_on("py-dendropy@4.4.0:", type=("build", "run"))
