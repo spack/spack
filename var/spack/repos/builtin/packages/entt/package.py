6d05d920f9ddf08e2472c4270718f1736156eb9a
Alec Scott <hi@alecbcs.com>
2023-04-30 05:30:28 +0000

    entt: add v3.11.1

--- var/spack/repos/builtin/packages/entt/package.py
+++ var/spack/repos/builtin/packages/entt/package.py
@@ -14,6 +14,7 @@ class Entt(CMakePackage):
     homepage = "https://entt.docsforge.com"
     url = "https://github.com/skypjack/entt/archive/v3.5.2.tar.gz"
 
+    version("3.11.1", sha256="0ac010f232d3089200c5e545bcbd6480cf68b705de6930d8ff7cdb0a29f5b47b")
     version("3.5.2", sha256="f9271293c44518386c402c9a2188627819748f66302df48af4f6d08e30661036")
 
     depends_on("cmake@3.7.0:", type="build")
