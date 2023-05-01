18b606f2d37f8352fd4b8258f755b4f305924fa9
Adam J. Stewart <ajstewart426@gmail.com>
2023-05-01 14:17:50 -0500

    py-sphinx: add v7.0.0 (#37352)

--- var/spack/repos/builtin/packages/py-sphinx/package.py
+++ var/spack/repos/builtin/packages/py-sphinx/package.py
@@ -14,6 +14,8 @@ class PySphinx(PythonPackage):
 
     maintainers("adamjstewart")
 
+    version("7.0.0", sha256="283c44aa28922bb4223777b44ac0d59af50a279ac7690dfe945bb2b9575dc41b")
+
     version("6.2.1", sha256="6d56a34697bb749ffa0152feafc4b19836c755d90a7c59b72bc7dfd371b9cc6b")
     version("6.2.0", sha256="9ef22c2941bc3d0ff080d25a797f7521fc317e857395c712ddde97a19d5bb440")
     version("6.1.3", sha256="0dac3b698538ffef41716cf97ba26c1c7788dba73ce6f150c1ff5b4720786dd2")
