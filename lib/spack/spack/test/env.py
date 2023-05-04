8a248cc203bd8bb09f269d64917259ef3f27e4c4
Chris Green <greenc@fnal.gov>
2023-05-04 09:49:28 -0500

    Tests

--- lib/spack/spack/test/env.py
+++ lib/spack/spack/test/env.py
@@ -316,16 +316,9 @@ def test_cannot_initiliaze_if_dirname_exists_as_a_file(tmp_path):
         ev.create_in_dir(dir_name)
 
 
-def test_cannot_initiliaze_if_init_file_does_not_exist(tmp_path):
+def test_cannot_initialize_if_init_file_does_not_exist(tmp_path):
     """Tests that initializing an environment passing a non-existing init file raises an error."""
     init_file = tmp_path / ev.manifest_name
-    with pytest.raises(ev.SpackEnvironmentError, match="cannot initialize"):
-        ev.create_in_dir(tmp_path, init_file=init_file)
-
-
-def test_cannot_initialize_from_random_file(tmp_path):
-    init_file = tmp_path / "foo.txt"
-    init_file.touch()
     with pytest.raises(ev.SpackEnvironmentError, match="cannot initialize"):
         ev.create_in_dir(tmp_path, init_file=init_file)
 
@@ -419,3 +412,40 @@ def test_preserving_comments_when_adding_specs(
 
     content = spack_yaml.read_text()
     assert content == expected_yaml
+
+
+@pytest.mark.parametrize("filename", [ev.lockfile_name, "as9582g54.lock", "m3ia54s.json"])
+def test_initialize_from_lockfile(tmp_path, filename):
+    """Test that we can initialize an environment from a valid lockfile"""
+
+    init_file = tmp_path / filename
+    env_dir = tmp_path / "env_dir"
+    init_file.write_text('{ "roots": [] }\n')
+    ev.initialize_environment_dir(env_dir, init_file)
+
+    assert os.path.getsize(env_dir / ev.manifest_name) == len(ev.default_manifest_yaml())
+
+
+def test_cannot_initialize_from_bad_lockfile(tmp_path):
+    """Test that we fail on an incorrectly constructed lockfile"""
+
+    init_file = tmp_path / ev.lockfile_name
+    env_dir = tmp_path / "env_dir"
+
+    init_file.write_text("Not a legal JSON file\n")
+
+    with pytest.raises(ev.SpackEnvironmentError, match="from lockfile"):
+        ev.initialize_environment_dir(env_dir, init_file)
+
+
+@pytest.mark.parametrize("filename", ["random.txt", "random.yaml", ev.manifest_name])
+def test_initialize_from_random_file_as_manifest(tmp_path, filename):
+    """Test that any file not recognized as a lockfile is treated as a manifest"""
+
+    init_file = tmp_path / filename
+    env_dir = tmp_path / "env_dir"
+    init_file.touch()
+
+    ev.initialize_environment_dir(env_dir, init_file)
+
+    assert os.path.getsize(env_dir / ev.manifest_name) == 0
