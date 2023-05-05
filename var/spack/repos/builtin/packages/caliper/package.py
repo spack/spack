30aa89d3f2decf7d0bf2b71cf1c0852d289394e7
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-05-04 20:53:53 -0400

    patch missing libunwind include dir

--- var/spack/repos/builtin/packages/caliper/package.py
+++ var/spack/repos/builtin/packages/caliper/package.py
@@ -85,6 +85,7 @@ class Caliper(CMakePackage, CudaPackage, ROCmPackage):
     conflicts("+rocm+cuda")
 
     patch("for_aarch64.patch", when="target=aarch64:")
+    patch("sampler-service-missing-libunwind-include-dir.patch", when="@2.9.0 +rocm")
 
     def cmake_args(self):
         spec = self.spec
