cdf7f535c8a6326bbc3c3b4ff9c7d54a19e82b50
Robert Blake <blake14@llnl.gov>
2023-05-08 09:58:52 -0700

    Removing 3.0.

--- var/spack/repos/builtin/packages/py-kosh/package.py
+++ var/spack/repos/builtin/packages/py-kosh/package.py
@@ -21,7 +21,6 @@ class PyKosh(PythonPackage):
     maintainers("doutriaux1")
 
     version("3.0.1", sha256="e0d97c93476930ccfe8ed892517439d555760d66d57f9a8e52459fb5910ba398")
-    version("3.0", sha256="6c2f1071ed4a7816ff002b4eda965754d395c917e48e1f5e18042242a4b825d2")
     version("2.2", sha256="3c79c3b7e7b64018ec5987dd7148886a6c619a28cda6f84e61a57439c9f3d7a3")
     version("2.1", sha256="597ed5beb4c3c3675b4af15ee7bfb60a463d5bda2222cd927061737ed073d562")
     version("2.0", sha256="059e431e3d3219b53956cb464d9e10933ca141dc89662f55d9c633e35c8b3a1e")
@@ -39,4 +38,3 @@ class PyKosh(PythonPackage):
         depends_on("py-pandas")
         depends_on("py-hdbscan@0.8.29:")
         depends_on("py-tqdm")
-    patch("version-fix.patch", when="@3.0:3.0.0")  # fixed with v3.0.1
