47e209cf458090bc97d002920db96e1291010d86
Antonio Bellotta <antonio.bellotta@epfl.ch>
2023-05-08 13:39:55 +0200

    py-archive-advisor: bump version (#1998)
    py-archive-advisor: bump version to 0.0.3

--- bluebrain/repo-bluebrain/packages/py-archive-advisor/package.py
+++ bluebrain/repo-bluebrain/packages/py-archive-advisor/package.py
@@ -13,6 +13,7 @@ class PyArchiveAdvisor(PythonPackage):
     git = "ssh://git@bbpgitlab.epfl.ch/hpc/archive-advisor.git"
 
     version("develop", branch="main")
+    version("0.0.3", tag="0.0.3")
     version("0.0.2", tag="0.0.2")
     version("0.0.1", tag="0.0.1")
 
