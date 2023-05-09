0361f98fa649159552775ab014ec356379f3baa4
eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
2023-05-09 13:16:02 -0500

    py-loguru: add v0.2.5, v0.3.0 (#37574)
    * py-loguru: add v0.2.5
    
    * py-loguru: add v0.3.0

--- var/spack/repos/builtin/packages/py-loguru/package.py
+++ var/spack/repos/builtin/packages/py-loguru/package.py
@@ -13,6 +13,8 @@ class PyLoguru(PythonPackage):
     pypi = "loguru/loguru-0.6.0.tar.gz"
 
     version("0.6.0", sha256="066bd06758d0a513e9836fd9c6b5a75bfb3fd36841f4b996bc60b547a309d41c")
+    version("0.3.0", sha256="f2a0fa92f334d37a13351aa36ab18e8039649a3741836b4b6d8b8bce7e8457ac")
+    version("0.2.5", sha256="68297d9f23064c2f4764bb5d0c5c767f3ed7f9fc1218244841878f5fc7c94add")
 
     depends_on("python@3.5:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
