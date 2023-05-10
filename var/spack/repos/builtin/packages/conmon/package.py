deb7c9e605dd87200296e2427042ebddb80416d0
Alec Scott <hi@alecbcs.com>
2023-05-10 01:47:54 -0700

    conmon: add v2.1.7 (#37320)

--- var/spack/repos/builtin/packages/conmon/package.py
+++ var/spack/repos/builtin/packages/conmon/package.py
@@ -13,6 +13,7 @@ class Conmon(MakefilePackage):
     url = "https://github.com/containers/conmon/archive/v2.0.30.tar.gz"
     maintainers("bernhardkaindl")
 
+    version("2.1.7", sha256="7d0f9a2f7cb8a76c51990128ac837aaf0cc89950b6ef9972e94417aa9cf901fe")
     version("2.1.5", sha256="ee3179ee2b9a9107acec00eb546062cf7deb847f135a3b81503d22b0d226b3ed")
     version("2.0.30", sha256="4b0a98fbe8a63c42f60edac25c19aa6606caa7b1e4fe7846fc7f7de0b566ba25")
 
