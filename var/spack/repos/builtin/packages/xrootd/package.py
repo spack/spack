b891cf318c0706650d4fe70f027831614bc6c30f
Chris Green <greenc@fnal.gov>
2023-05-11 16:22:12 -0500

    New version 5.5.5

--- var/spack/repos/builtin/packages/xrootd/package.py
+++ var/spack/repos/builtin/packages/xrootd/package.py
@@ -17,6 +17,7 @@ class Xrootd(CMakePackage):
 
     maintainers("gartung", "greenc-FNAL", "marcmengel", "vitodb", "wdconinc")
 
+    version("5.5.5", sha256="0710caae527082e73d3bf8f9d1dffe95808afd3fcaaaa15ab0b937b8b226bc1f")
     version("5.5.4", sha256="41a8557ea2d118b1950282b17abea9230b252aa5ee1a5959173e2534b7d611d3")
     version("5.5.3", sha256="703829c2460204bd3c7ba8eaa23911c3c9a310f6d436211ba0af487ef7f6a980")
     version("5.5.2", sha256="ec4e0490b8ee6a3254a4ea4449342aa364bc95b78dc9a8669151be30353863c6")
