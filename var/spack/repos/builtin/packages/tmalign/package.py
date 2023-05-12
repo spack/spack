1aaa26037b9c0148bbfbaedf612d52e8aba8b0b5
snehring <snehring@iastate.edu>
2023-05-12 11:28:35 -0500

    tmalign: new version 20220412

--- var/spack/repos/builtin/packages/tmalign/package.py
+++ var/spack/repos/builtin/packages/tmalign/package.py
@@ -10,12 +10,35 @@ class Tmalign(Package):
     """TM-align is an algorithm for sequence-order independent protein
     structure comparisons."""
 
-    homepage = "https://zhanglab.ccmb.med.umich.edu/TM-align"
-    url = "http://zhanglab.ccmb.med.umich.edu/TM-align/TM-align-C/TMalignc.tar.gz"
+    homepage = "https://zhanggroup.org/TM-align/"
+    url = "https://zhanggroup.org/TM-align/TMalign.cpp"
+
+    maintainers("snehring")
 
     version(
-        "2016-05-25", sha256="ce7f68289f3766d525afb0a58e3acfc28ae05f538d152bd33d57f8708c60e2af"
+        "20220412",
+        sha256="09227c46705ca8cf7c922a6e1672c34d7ed4daba32e5c7c484306808db54117a",
+        expand=False,
     )
+    version(
+        "2016-05-25",
+        sha256="ce7f68289f3766d525afb0a58e3acfc28ae05f538d152bd33d57f8708c60e2af",
+        url="http://zhanglab.ccmb.med.umich.edu/TM-align/TM-align-C/TMalignc.tar.gz",
+        deprecated=True,
+    )
+
+    variant("fast-math", default=False, when="@20220412:")
+
+    with when("@20220412:"):
+        phases = ["build", "install"]
+
+    def build(self, spec, prefix):
+        cxx = Executable(self.compiler.cxx)
+        args = ["-O3"]
+        if spec.satisfies("+fast-math"):
+            args.append("-ffast-math")
+        args.extend(["-lm", "-o", "TMalign", "TMalign.cpp"])
+        cxx(*args)
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
