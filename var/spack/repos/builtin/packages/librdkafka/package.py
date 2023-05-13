70b468478d6c5adb70b85ca88923fd14b5a7e4d1
dale-mittleman <83711285+dale-mittleman@users.noreply.github.com>
2023-05-13 19:00:12 -0400

    Adding librdkafka versions 1.9.2, 2.0.2 (#37501)
    Co-authored-by: Alec Scott <hi@alecbcs.com>

--- var/spack/repos/builtin/packages/librdkafka/package.py
+++ var/spack/repos/builtin/packages/librdkafka/package.py
@@ -14,6 +14,8 @@ class Librdkafka(AutotoolsPackage):
     url = "https://github.com/edenhill/librdkafka/archive/v1.5.0.tar.gz"
 
     version("2.1.0", sha256="d8e76c4b1cde99e283a19868feaaff5778aa5c6f35790036c5ef44bc5b5187aa")
+    version("2.0.2", sha256="f321bcb1e015a34114c83cf1aa7b99ee260236aab096b85c003170c90a47ca9d")
+    version("1.9.2", sha256="3fba157a9f80a0889c982acdd44608be8a46142270a389008b22d921be1198ad")
     version("1.5.0", sha256="f7fee59fdbf1286ec23ef0b35b2dfb41031c8727c90ced6435b8cf576f23a656")
     version("1.4.4", sha256="0984ffbe17b9e04599fb9eceb16cfa189f525a042bef02474cd1bbfe1ea68416")
     version("1.4.2", sha256="3b99a36c082a67ef6295eabd4fb3e32ab0bff7c6b0d397d6352697335f4e57eb")
