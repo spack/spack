3eee09b83f1ea4b537f457070b8e5c1bec6fcb2a
Andrew W Elble <aweits@rit.edu>
2023-05-15 12:10:40 -0400

    routinator: update, deprecate old version (#37676)

--- var/spack/repos/builtin/packages/routinator/package.py
+++ var/spack/repos/builtin/packages/routinator/package.py
@@ -14,9 +14,15 @@ class Routinator(Package):
 
     maintainers("aweits")
 
-    version("0.11.2", sha256="00f825c53168592da0285e8dbd228018e77248d458214a2c0f86cd0ca45438f5")
+    version("0.12.1", sha256="8150fe544f89205bb2d65bca46388f055cf13971d3163fe17508bf231f9ab8bc")
+    version(
+        "0.11.2",
+        sha256="00f825c53168592da0285e8dbd228018e77248d458214a2c0f86cd0ca45438f5",
+        deprecated=True,
+    )
 
-    depends_on("rust@1.56:")
+    depends_on("rust@1.56:", when="@0.11.2")
+    depends_on("rust@1.63:", when="@0.12.1")
 
     def install(self, spec, prefix):
         cargo = which("cargo")
