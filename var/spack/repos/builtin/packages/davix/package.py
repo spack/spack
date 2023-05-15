b2be635a56ec7057d571d5dcbd6ca421b92ec28c
Chris Green <greenc@fnal.gov>
2023-05-15 14:46:52 -0500

    [davix] Enable third party copies with gSOAP (#37648)
    * [davix] Enable third party copies with gSOAP
    
    * Add FNAL Spack team to maintainers

--- var/spack/repos/builtin/packages/davix/package.py
+++ var/spack/repos/builtin/packages/davix/package.py
@@ -12,6 +12,8 @@ class Davix(CMakePackage):
     homepage = "https://davix.web.cern.ch/davix/docs/devel/index.html"
     url = "https://github.com/cern-fts/davix/releases/download/R_0_8_1/davix-0.8.1.tar.gz"
 
+    maintainers("gartung", "greenc-FNAL", "marcmengel", "vitodb")
+
     version("0.8.1", sha256="3f42f4eadaf560ab80984535ffa096d3e88287d631960b2193e84cf29a5fe3a6")
     version("0.8.0", sha256="2f108da0408a83fb5b9f0c68150d360ba733e4b3a0fe298d45b0d32d28ab7124")
     version("0.7.6", sha256="a2e7fdff29f7ba247a3bcdb08ab1db6d6ed745de2d3971b46526986caf360673")
@@ -33,8 +35,15 @@ class Davix(CMakePackage):
     depends_on("uuid")
     depends_on("openssl")
 
+    variant("thirdparty", default=False)
+    depends_on("gsoap", when="+thirdparty")
+
     def cmake_args(self):
-        cmake_args = ["-DCMAKE_CXX_STANDARD={0}".format(self.spec.variants["cxxstd"].value)]
+        cmake_args = [
+            self.define_from_variant("CMAKE_CXX_STANDARD", variant="cxxstd"),
+            self.define_from_variant("ENABLE_THIRD_PARTY_COPY", variant="thirdparty"),
+        ]
+
         if "darwin" in self.spec.architecture:
             cmake_args.append("-DCMAKE_MACOSX_RPATH=ON")
         return cmake_args
