c91e5c4668c49a085655f04477d2a80c0a6bbb1c
ulmononian <cameron_book@alumni.brown.edu>
2023-05-18 11:19:33 -0500

    Add checksums for new esmf, fms, & hdf5 versions; update fms version in ufs-weather model-env.

--- var/spack/repos/builtin/packages/esmf/package.py
+++ var/spack/repos/builtin/packages/esmf/package.py
@@ -29,6 +29,7 @@ class Esmf(MakefilePackage):
     # Develop is a special name for spack and is always considered the newest version
     version("develop", branch="develop")
     # generate chksum with spack checksum esmf@x.y.z
+    version("8.4.2", sha256="969304efa518c7859567fa6e65efd960df2b4f6d72dbf2c3f29e39e4ab5ae594")
     version("8.4.1", sha256="1b54cee91aacaa9df400bd284614cbb0257e175f6f3ec9977a2d991ed8aa1af6")
     version(
         "8.4.0",
