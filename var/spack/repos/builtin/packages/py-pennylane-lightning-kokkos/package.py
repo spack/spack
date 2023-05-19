b1c385f5692284934f5aba3b03313edf739be622
Lee J. O'Riordan <loriordan@gmail.com>
2023-05-19 15:49:17 -0400

    Remove PL hard pin and rely on PLQ to define version

--- var/spack/repos/builtin/packages/py-pennylane-lightning-kokkos/package.py
+++ var/spack/repos/builtin/packages/py-pennylane-lightning-kokkos/package.py
@@ -72,8 +72,7 @@ class PyPennylaneLightningKokkos(CMakePackage, PythonExtension, CudaPackage, ROC
     depends_on("py-pybind11", type="build")
     depends_on("py-pip", type="build")
     depends_on("py-wheel", type="build")
-    depends_on("py-pennylane@:0.29", type=("build", "run"), when="@0.29")
-    depends_on("py-pennylane@0.30:", type=("build", "run"), when="@0.30:")
+    depends_on("py-pennylane@:0.28", type=("build", "run"))
     depends_on("py-pennylane-lightning@0.30:~kokkos", type=("build", "run"), when="@0.30.0:")
     depends_on("py-pennylane-lightning@0.28:0.29~kokkos", type=("build", "run"), when="@0.29.1")
 
