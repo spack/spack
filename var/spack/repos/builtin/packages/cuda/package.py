58ea215549776f9f3d8b314e169c839920e3c2c2
Freja Nordsiek <fnordsie@posteo.net>
2023-05-22 11:18:12 +0200

    Fixed cuda package's libs property to return all non-stub non-compat libraries as well as non-compat stub libraries that don't have a non-stub version.

--- var/spack/repos/builtin/packages/cuda/package.py
+++ var/spack/repos/builtin/packages/cuda/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 import os
+import os.path
 import platform
 import re
 from glob import glob
@@ -633,14 +634,23 @@ class Cuda(Package):
 
     @property
     def libs(self):
-        libs = find_libraries("libcudart", root=self.prefix, shared=True, recursive=True)
-
         filtered_libs = []
         # CUDA 10.0 provides Compatability libraries for running newer versions
         # of CUDA with older drivers. These do not work with newer drivers.
-        for lib in libs:
+        #
+        # Add all non-stub libraries while building a set of the filenames for
+        # later exclusion.
+        non_stub_libs = set()
+        for lib in find_libraries("*", root=self.prefix, shared=True, recursive=True):
             parts = lib.split(os.sep)
             if "compat" not in parts and "stubs" not in parts:
+                filtered_libs.append(lib)
+                non_stub_libs.add(parts[-1])
+        # Add all stub libraries that don't have a non-stub version.
+        for lib in find_libraries(os.path.join("stubs", "*"), root=self.prefix,
+                                  shared=True, recursive=True):
+            parts = lib.split(os.sep)
+            if "compat" not in parts and parts[-1] not in non_stub_libs:
                 filtered_libs.append(lib)
         return LibraryList(filtered_libs)
 
