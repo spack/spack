830cfce01c74271659609a1cb4a74fc26ac91594
Jonathon Anderson <janderson@rice.edu>
2023-05-23 08:32:01 -0500

    containerize: Preserve view symlink

--- lib/spack/spack/container/writers/__init__.py
+++ lib/spack/spack/container/writers/__init__.py
@@ -176,12 +176,15 @@ class PathContext(tengine.Context):
     @tengine.context_property
     def paths(self):
         """Important paths in the image"""
-        Paths = collections.namedtuple("Paths", ["environment", "store", "hidden_view", "view"])
+        Paths = collections.namedtuple(
+            "Paths", ["environment", "store", "hidden_view", "view", "view_parent"]
+        )
         return Paths(
             environment="/opt/spack-environment",
             store="/opt/software",
             hidden_view="/opt/._view",
             view="/opt/view",
+            view_parent="/opt",
         )
 
     @tengine.context_property
