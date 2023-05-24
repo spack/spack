101b034f032909bfe10eaae31a3e7e6625835339
fpanichi <fpanichi@amd.com>
2023-05-24 18:14:32 +0530

    Cloverleaf: fixed version logic.

--- var/spack/repos/builtin/packages/cloverleaf/package.py
+++ var/spack/repos/builtin/packages/cloverleaf/package.py
@@ -3,7 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from os import *
 from spack.package import *
 
 class Cloverleaf(MakefilePackage):
@@ -14,10 +13,10 @@ class Cloverleaf(MakefilePackage):
     homepage = "https://uk-mac.github.io/CloverLeaf"
 
     # cuda: done
-    version("master", branch="master",git="https://github.com/UK-MAC/CloverLeaf_CUDA", when="build=cuda")
+    version("master_cuda", branch="master",git="https://github.com/UK-MAC/CloverLeaf_CUDA")
     # mpi_only: done
-    version("master", branch="master",git="https://github.com/UK-MAC/CloverLeaf_MPI", when="build=mpi_only")
-    version("1.3", commit="a675f0d63ea72ef386c7c497b08d1e6ca4f89479",git="https://github.com/UK-MAC/CloverLeaf_MPI", when="build=mpi_only")
+    version("master_mpi_only", branch="master",git="https://github.com/UK-MAC/CloverLeaf_MPI")
+    version("1.3_mpi_only", commit="a675f0d63ea72ef386c7c497b08d1e6ca4f89479",git="https://github.com/UK-MAC/CloverLeaf_MPI")
     # openacc_cray: done
     version("master", branch="master",git="https://github.com/UK-MAC/CloverLeaf_OpenACC", when="build=openacc_cray")
     version("1.3", commit="0ddf495cf21cc59f84e274617522a1383e2c328c",git="https://github.com/UK-MAC/CloverLeaf_OpenACC", when="build=openacc_cray")
@@ -30,34 +29,28 @@ class Cloverleaf(MakefilePackage):
     # openmp_offload: done
     version("master", branch="master",git="https://github.com/UK-MAC/CloverLeaf_Offload", when="build=openmp_offload")
     # ref: done
-    version("master", branch="master",git="https://github.com/UK-MAC/CloverLeaf_ref", when="build=ref")
-    version("1.3", commit="0ddf495cf21cc59f84e274617522a1383e2c328c",git="https://github.com/UK-MAC/CloverLeaf_ref", when="build=ref")
-    version("1.1", commit="5667c3aa565c79cb43b7d956c84c68b6d82d1e94", git="https://github.com/UK-MAC/CloverLeaf_ref", when="build=ref")
+    version("master_ref", branch="master",git="https://github.com/UK-MAC/CloverLeaf_ref")
+    version("1.3_ref", commit="0ddf495cf21cc59f84e274617522a1383e2c328c",git="https://github.com/UK-MAC/CloverLeaf_ref")
+    version("1.1_ref", commit="5667c3aa565c79cb43b7d956c84c68b6d82d1e94", git="https://github.com/UK-MAC/CloverLeaf_ref")
     # serial: done
-    version("master", branch="master",git="https://github.com/UK-MAC/CloverLeaf_Serial", when="build=serial")
-    version("1.3", commit="b9a2b9c496b5eb1e7e30912d58e32d9dce930a0c",git="https://github.com/UK-MAC/CloverLeaf_Serial", when="build=serial")
+    version("master_serial", branch="master",git="https://github.com/UK-MAC/CloverLeaf_Serial")
+    version("1.3_serial", commit="b9a2b9c496b5eb1e7e30912d58e32d9dce930a0c",git="https://github.com/UK-MAC/CloverLeaf_Serial")
 
-    variant(
-        "build",
-        default="ref",
-        description="Type of Parallelism Build",
-        values=("cuda", "mpi_only", "openacc_cray", "openmp_only", "ref", "serial", "openmp4_only", "openmp_offload"),
-    )
     variant("ieee", default=False, description="Build with IEEE standards")
     variant("debug", default=False, description="Build with DEBUG flags")
 
-    depends_on("mpi", when="build=cuda")
-    depends_on("mpi", when="build=mpi_only")
-    depends_on("mpi", when="build=openacc_cray")
-    depends_on("mpi", when="build=ref")
-    depends_on("cuda", when="build=cuda")
+    depends_on("mpi", when="@master_cuda")
+    depends_on("mpi", when="@master_mpi_only")
+    depends_on("mpi", when="@master_openacc_cray")
+    depends_on("mpi", when="@master_ref")
+    depends_on("cuda", when="@master_cuda")
 
-    conflicts("build=cuda", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
-    conflicts("build=openacc_cray", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
-    conflicts("build=serial", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
-    conflicts("build=openmp_only", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
-    conflicts("build=openmp4_only", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
-    conflicts("build=openmp_offload", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
+    conflicts("@master_cuda", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
+    conflicts("@master_openacc_cray", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
+    conflicts("@master_serial", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
+    conflicts("@master_openmp_only", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
+    conflicts("@master_oenmp4_only", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
+    conflicts("@master_openmp_offload", when="%aocc", msg="Currently AOCC supports only ref and MPI variants")
 
     @property
     def build_targets(self):
@@ -72,8 +65,8 @@ class Cloverleaf(MakefilePackage):
 
         if "%gcc" in self.spec:
             targets.append("COMPILER=GNU")
-            targets.append("FLAGS_GNU=")
-            targets.append("CFLAGS_GNU=")
+#            targets.append("FLAGS_GNU=")
+#            targets.append("CFLAGS_GNU=")
         elif "%cce" in self.spec:
             targets.append("COMPILER=CRAY")
             targets.append("FLAGS_CRAY=")
