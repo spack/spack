c70fc4d1cf1e5aeff9249d227339cc62f045371a
Ryan Honeyager <109982075+rhoneyager-tomorrow@users.noreply.github.com>
2023-05-26 10:55:29 -0400

    Update package.py

--- var/spack/repos/builtin/packages/fckit/package.py
+++ var/spack/repos/builtin/packages/fckit/package.py
@@ -39,7 +39,9 @@ class Fckit(CMakePackage):
     depends_on("llvm-openmp", when="+openmp %apple-clang", type=("build", "run"))
     variant('shared', default=True)
     variant("fismahigh", default=False, description="Apply patching for FISMA-high compliance")
-    variant('badfinal', default=False, description='Forcibly disable automatic finalization of derived types')
+    variant("finalize_ddts", default="auto",
+            description="Enable / disable automatic finalization of derived types",
+            values=("auto", "no", "yes"))
 
     def cmake_args(self):
         args = [
@@ -52,8 +54,8 @@ class Fckit(CMakePackage):
         if '~shared' in self.spec:
             args.append('-DBUILD_SHARED_LIBS=OFF')
         
-        if 'badfinal' in self.spec:
-            args.append('-DENABLE_FINAL:BOOL=OFF')
+        if 'finalize_ddts=auto' not in self.spec:
+            args.append(self.define_from_variant('ENABLE_FINAL', 'finalize_ddts'))
 
         if self.spec.satisfies('%intel') or self.spec.satisfies('%gcc'):
             cxxlib = 'stdc++'
