e91a30a0e1d2fc9f74e5dd8bcc4a35ec52548128
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-05-27 00:19:40 +0200

    py-contourpy: add 1.0.7 (#37914)

--- var/spack/repos/builtin/packages/py-contourpy/package.py
+++ var/spack/repos/builtin/packages/py-contourpy/package.py
@@ -12,10 +12,13 @@ class PyContourpy(PythonPackage):
     homepage = "https://github.com/contourpy/contourpy"
     pypi = "contourpy/contourpy-1.0.5.tar.gz"
 
+    version("1.0.7", sha256="d8165a088d31798b59e91117d1f5fc3df8168d8b48c4acc10fc0df0d0bdbcc5e")
     version("1.0.5", sha256="896631cd40222aef3697e4e51177d14c3709fda49d30983269d584f034acc8a4")
 
+    depends_on("python@3.8:", when="@1.0.7:", type=("build", "link", "run"))
     depends_on("python@3.7:", type=("build", "link", "run"))
-    depends_on("py-build", type="build")
     depends_on("py-pybind11@2.6:", type=("build", "link"))
     depends_on("py-setuptools@42:", type="build")
     depends_on("py-numpy@1.16:", type=("build", "run"))
+
+    depends_on("py-build", when="@:1.0.5", type="build")
