31fe76059acf2fc541ae117d3a4392228600be0f
Carlos Bederian <carlos.bederian@unc.edu.ar>
2023-05-26 14:42:37 -0300

    Filter intel -xHost

--- var/spack/repos/builtin/packages/nwchem/package.py
+++ var/spack/repos/builtin/packages/nwchem/package.py
@@ -76,7 +76,7 @@ class Nwchem(Package):
         else:
             args.extend(["NWCHEM_MODULES=all python"])
             # archspec flags are injected through the compiler wrapper
-            filter_file("(-mtune=native|-mcpu=native)", "", "src/config/makefile.h")
+            filter_file("(-mtune=native|-mcpu=native|-xHost)", "", "src/config/makefile.h")
 
         # TODO: query if blas/lapack/scalapack uses 64bit Ints
         # A flag to distinguish between 32bit and 64bit integers in linear
