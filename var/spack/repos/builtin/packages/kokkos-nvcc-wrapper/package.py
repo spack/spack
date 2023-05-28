2957acf3d6185cd8dd7e77ef7bc8bf08f06d0544
Bruno Turcksin <bruno.turcksin@gmail.com>
2023-05-28 04:19:30 -0400

    kokkos-nvcc-wrapper: Remove unnecessary dependencies (#37794)

--- var/spack/repos/builtin/packages/kokkos-nvcc-wrapper/package.py
+++ var/spack/repos/builtin/packages/kokkos-nvcc-wrapper/package.py
@@ -17,6 +17,8 @@ class KokkosNvccWrapper(Package):
     git = "https://github.com/kokkos/kokkos.git"
     url = "https://github.com/kokkos/kokkos/archive/3.1.01.tar.gz"
 
+    maintainers("Rombur")
+
     version("3.2.00", sha256="05e1b4dd1ef383ca56fe577913e1ff31614764e65de6d6f2a163b2bddb60b3e9")
     version("3.1.01", sha256="ff5024ebe8570887d00246e2793667e0d796b08c77a8227fe271127d36eec9dd")
     version("3.1.00", sha256="b935c9b780e7330bcb80809992caa2b66fd387e3a1c261c955d622dae857d878")
@@ -24,10 +26,7 @@ class KokkosNvccWrapper(Package):
     version("master", branch="master")
     version("develop", branch="develop")
 
-    variant("mpi", default=True, description="use with MPI as the underlying compiler")
     depends_on("cuda")
-    depends_on("mpi", when="+mpi")
-    depends_on("cmake@3.10:", type="build")
 
     def install(self, spec, prefix):
         src = os.path.join("bin", "nvcc_wrapper")
