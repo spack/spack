d717b3a33f2607bde5b77518d4da37aab1b95096
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2023-05-28 02:42:44 -0600

    Automated deployment to update package flux-core 2023-05-04 (#37421)
    Co-authored-by: github-actions <github-actions@users.noreply.github.com>

--- var/spack/repos/builtin/packages/flux-core/package.py
+++ var/spack/repos/builtin/packages/flux-core/package.py
@@ -20,6 +20,7 @@ class FluxCore(AutotoolsPackage):
     maintainers("grondo")
 
     version("master", branch="master")
+    version("0.50.0", sha256="77414299a7ca081199aa0f57bcaea3e05860e2095df73c0f6b7672b88fadf683")
     version("0.49.0", sha256="9b8d7af1d8aaa7ee110bcb9815b6b8647af686de949097c9bb2a0269d5551051")
     version("0.48.0", sha256="32c1bfdde44123e90606422807d381406874bb6dbec170ddb493f905208cc275")
     version("0.47.0", sha256="c13c8df3dd3db565ff7a3db727f087b7c1a3946b98c4b945ac43fe44a4c534c3")
