1006e696d47ec8d6e0b48aefda1d069d1330bff6
Brian Vanderwende <vanderwb@ucar.edu>
2023-05-29 03:21:48 -0600

    bbcp: fix cloning "master" version (#37802)

--- var/spack/repos/builtin/packages/bbcp/package.py
+++ var/spack/repos/builtin/packages/bbcp/package.py
@@ -12,7 +12,10 @@ class Bbcp(Package):
     homepage = "https://www.slac.stanford.edu/~abh/bbcp/"
     git = "https://www.slac.stanford.edu/~abh/bbcp/bbcp.git"
 
-    version("master", branch="master")
+    maintainers("vanderwb")
+
+    # Stanford's git server does not support "smart https" shallow clones
+    version("master", branch="master", get_full_repo=True)
 
     depends_on("zlib")
     depends_on("openssl")
