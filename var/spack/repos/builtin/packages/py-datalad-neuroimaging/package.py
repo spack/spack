79ce83c6b9c36300d0e120eaa7eef52d480d3462
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-06-01 21:31:54 +0200

    py-datalad-neuroimaging: add 0.3.3 (#38017)

--- var/spack/repos/builtin/packages/py-datalad-neuroimaging/package.py
+++ var/spack/repos/builtin/packages/py-datalad-neuroimaging/package.py
@@ -12,12 +12,19 @@ class PyDataladNeuroimaging(PythonPackage):
     homepage = "https://github.com/datalad/datalad-neuroimaging"
     pypi = "datalad_neuroimaging/datalad_neuroimaging-0.3.1.tar.gz"
 
+    version("0.3.3", sha256="49a6852e68892e6cd13f245bca2d400abded5ea4fd2a69c9be41c710d0c49bb1")
     version("0.3.1", sha256="aaf7a3d10e8e7df1d8dee09e485bbe26787f496fb2302ed7ddea55a470a0f96e")
 
+    depends_on("py-setuptools@43:", when="@0.3.2:", type="build")
     depends_on("py-setuptools", type="build")
+
+    depends_on("py-datalad@0.16.7:", when="@0.3.2:", type=("build", "run"))
     depends_on("py-datalad@0.12:", type=("build", "run"))
+    depends_on("py-datalad-deprecated@0.2.7:", when="@0.3.3:", type=("build", "run"))
     depends_on("py-pydicom", type=("build", "run"))
+    depends_on("py-pybids@0.15.1:", when="@0.3.2:", type=("build", "run"))
     depends_on("py-pybids@0.9.2:", type=("build", "run"))
     depends_on("py-nibabel", type=("build", "run"))
     depends_on("py-pandas", type=("build", "run"))
+    depends_on("py-datalad-metalad@0.4.5:", when="@0.3.2:", type=("build", "run"))
     depends_on("git-annex", type="run")
