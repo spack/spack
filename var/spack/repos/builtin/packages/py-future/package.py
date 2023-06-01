f29ece63d5fabf32e7cf5935dd07502363fbb138
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-06-01 23:48:13 +0200

    py-future: add 0.18.3 (#38041)
    * py-future: add 0.18.3
    
    * Update var/spack/repos/builtin/packages/py-future/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-future/package.py
+++ var/spack/repos/builtin/packages/py-future/package.py
@@ -11,7 +11,9 @@ class PyFuture(PythonPackage):
 
     homepage = "https://python-future.org/"
     pypi = "future/future-0.18.2.tar.gz"
+    git = "https://github.com/PythonCharmers/python-future.git"
 
+    version("0.18.3", sha256="34a17436ed1e96697a86f9de3d15a3b0be01d8bc8de9c1dffd59fb8234ed5307")
     version("0.18.2", sha256="b1bead90b70cf6ec3f0710ae53a525360fa360d306a86583adc6bf83a4db537d")
     version("0.17.1", sha256="67045236dcfd6816dc439556d009594abf643e5eb48992e36beac09c2ca659b8")
     version("0.17.0", sha256="eb6d4df04f1fb538c99f69c9a28b255d1ee4e825d479b9c62fc38c0cf38065a4")
@@ -21,55 +23,14 @@ class PyFuture(PythonPackage):
     depends_on("py-setuptools", type="build")
 
     @property
-    def import_modules(self):
-        modules = [
-            "copyreg",
-            "_thread",
-            "past",
-            "past.types",
-            "past.translation",
-            "past.utils",
-            "past.builtins",
-            "reprlib",
-            "html",
-            "builtins",
-            "http",
-            "_dummy_thread",
-            "queue",
-            "xmlrpc",
-            "libfuturize",
-            "libfuturize.fixes",
-            "future",
-            "future.moves",
-            "future.moves.test",
-            "future.moves.urllib",
-            "future.moves.html",
-            "future.moves.http",
-            "future.moves.dbm",
-            "future.moves.xmlrpc",
-            "future.types",
-            "future.tests",
-            "future.utils",
-            "future.builtins",
-            "future.backports",
-            "future.backports.test",
-            "future.backports.urllib",
-            "future.backports.html",
-            "future.backports.http",
-            "future.backports.xmlrpc",
-            "future.backports.email",
-            "future.backports.email.mime",
-            "future.standard_library",
-            "libpasteurize",
-            "libpasteurize.fixes",
-            "socketserver",
-            "_markupbase",
-        ]
+    def skip_modules(self):
+        modules = []
 
-        if "platform=windows" in self.spec:
-            modules.append("winreg")
-
-        if "+tkinter" in self.spec["python"]:
+        if "~tkinter" in self.spec["python"]:
             modules.extend(["tkinter", "future.moves.tkinter"])
+
+        if self.spec.satisfies("@:0.17.1 ^python@3.10:"):
+            # The Iterable abstract class was removed from collections in Python 3.10
+            modules.extend(["future.backports", "past.types", "past.builtins"])
 
         return modules
