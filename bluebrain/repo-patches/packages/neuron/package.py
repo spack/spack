c5c93f5bb48cc337ca4c42b5c65423810309a269
Ioannis Magkanaris <iomagkanaris@gmail.com>
2023-06-03 08:59:12 +0200

    neuron: enable some unsafe math optimisations with GCC (#2023)
    For RelWithDebInfo and Release builds with GCC:
    * Pass -fopenmp-simd when openmp is *not* enabled
    * Pass a subset of -ffast-math
    To enable vectorisation of mechanism code.
    
    Co-authored-by: Olli Lupton <oliver.lupton@epfl.ch>

--- bluebrain/repo-patches/packages/neuron/package.py
+++ bluebrain/repo-patches/packages/neuron/package.py
@@ -297,6 +297,24 @@ class Neuron(CMakePackage):
             compilation_flags.append(
                 self.spec.architecture.target.optimization_flags(self.spec.compiler)
             )
+            # In case we're using GCC compiler we enable certain optimization options
+            # to allow vectorization of mechanism kernels in case `libmvec` is available
+            # in the system.
+            # Due to the fact that the generated code by NMODL includes `#pragma omp simd`
+            # clauses we also need to enable `+openmp` or add `-fopenmp-simd` to make sure
+            # that the code gets vectorized
+            if "%gcc" in self.spec and self.spec.variants["build_type"].value in [
+                "Release",
+                "RelWithDebInfo",
+            ]:
+                compilation_flags += [
+                    "-ffinite-math-only",
+                    "-fno-math-errno",
+                    "-funsafe-math-optimizations",
+                    "-fno-associative-math",
+                ]
+                if "+openmp" not in self.spec:
+                    compilation_flags.append("-fopenmp-simd")
         if "%intel" in self.spec:
             # icpc: command line warning #10121: overriding '-march=skylake' with '-march=skylake'
             compilation_flags.append("-diag-disable=10121")
