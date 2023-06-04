62ce84e9a59f87f4388cea4401cda53b400560bf
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-06-05 00:30:33 +0200

    py-hatch-fancy-pypi-readme: add 23.1.0 (#38075)

--- var/spack/repos/builtin/packages/py-hatch-fancy-pypi-readme/package.py
+++ var/spack/repos/builtin/packages/py-hatch-fancy-pypi-readme/package.py
@@ -12,9 +12,9 @@ class PyHatchFancyPypiReadme(PythonPackage):
     homepage = "https://github.com/hynek/hatch-fancy-pypi-readme"
     pypi = "hatch_fancy_pypi_readme/hatch_fancy_pypi_readme-22.7.0.tar.gz"
 
+    version("23.1.0", sha256="b1df44063094af1e8248ceacd47a92c9cf313d6b9823bf66af8a927c3960287d")
     version("22.7.0", sha256="dedf2ba0b81a2975abb1deee9310b2eb85d22380fda0d52869e760b5435aa596")
 
-    depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-hatchling", type=("build", "run"))
     depends_on("py-tomli", when="^python@:3.10", type=("build", "run"))
     depends_on("py-typing-extensions", when="^python@:3.7", type=("build", "run"))
