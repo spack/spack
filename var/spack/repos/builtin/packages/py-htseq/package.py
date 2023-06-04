55236b4fd469ec14314907b172f678a1fd144dc7
George Young <A-N-Other@users.noreply.github.com>
2023-06-04 14:34:48 +0100

    Removing SWIG

--- var/spack/repos/builtin/packages/py-htseq/package.py
+++ var/spack/repos/builtin/packages/py-htseq/package.py
@@ -26,7 +26,6 @@ class PyHtseq(PythonPackage):
     # build-only dependencies
     depends_on("py-setuptools", type="build")
     depends_on("py-cython@0.29.5:", type="build")
-    depends_on("swig@3.0.8:", type="build")
     # run dependencies
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-pysam", type=("build", "run"))
