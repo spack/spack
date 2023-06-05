bc4aa7511afcea3a96b8440a8213837cbd55c7e4
Bill Williams <wwilliam47@gmail.com>
2023-06-05 10:42:17 +0200

    Fix variant spec

--- var/spack/repos/builtin/packages/scorep/package.py
+++ var/spack/repos/builtin/packages/scorep/package.py
@@ -90,7 +90,7 @@ class Scorep(AutotoolsPackage):
     # two components of cube -- cubew and cubelib.
 
     # SCOREP 8
-    depends_on("binutils+libs=shared~libs=static", type="link", when="@8:")
+    depends_on("binutils libs=shared", type="link", when="@8:")
     depends_on("otf2@3:", when="@8:")
     depends_on("cubew@4.8:", when="@8:")
     depends_on("cubelib@4.8:", when="@8:")
