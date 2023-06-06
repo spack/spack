2c6e79af7b5e872088eedea208f775b5de87eeca
Bill Williams <wwilliam47@gmail.com>
2023-06-06 11:21:51 +0200

    Style mixup

--- var/spack/repos/builtin/packages/scorep/package.py
+++ var/spack/repos/builtin/packages/scorep/package.py
@@ -91,7 +91,9 @@ class Scorep(AutotoolsPackage):
 
     # SCOREP 8
     depends_on("binutils libs=shared", type="link", when="@8:")
-    conflicts("binutils libs=static", when="@8:") # cap this if and when we find a reliable workaround
+    conflicts(
+        "binutils libs=static", when="@8:"
+    ) # cap this if and when we find a reliable workaround
     depends_on("otf2@3:", when="@8:")
     depends_on("cubew@4.8:", when="@8:")
     depends_on("cubelib@4.8:", when="@8:")
