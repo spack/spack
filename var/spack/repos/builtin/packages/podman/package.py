8b8bbb3a69351ac98f3869b7a9d19f5ddf1848f5
Carson Woods <cwoods289@gmail.com>
2023-06-07 12:54:27 -0600

    Add new podman versions and correct the versions which patch is applied to

--- var/spack/repos/builtin/packages/podman/package.py
+++ var/spack/repos/builtin/packages/podman/package.py
@@ -13,13 +13,20 @@ class Podman(Package):
     url = "https://github.com/containers/podman/archive/refs/tags/v4.3.1.tar.gz"
     maintainers("bernhardkaindl")
 
+    version("4.5.1", sha256="ee2c8b02b7fe301057f0382637b995a9c6c74e8d530692d6918e4c509ade6e39")
+    version("4.5.0", sha256="830a633630bf6e61f2b8d4ca00efdd9a173ef25cdd49d4a4364c293e088561df")
+    version("4.4.4", sha256="2dacfe7041b83e2cb05fda58bd1fbdae61348a427f5b9073b96b36154de894a1")
+    version("4.4.3", sha256="c4770787f008923ec3181ea49c31bdb4d2b77458dbc842f43053fc96a1914ee0")
+    version("4.4.2", sha256="59cec158438efa8a3e651b19e150d9afd90f7e3f07c30605a997e18b8c54b67c")
+    version("4.4.1", sha256="0b84dbc3ca1d3cc75708635e3a322c481bb679103040866024b1fa2be6826455")
+    version("4.4.0", sha256="4bb966fd960b0aae05181e30672a6b20be365225758110eb251b81214414cb25")
     version("4.3.1", sha256="455c29c4ee78cd6365e5d46e20dd31a5ce4e6e1752db6774253d76bd3ca78813")
     version("3.4.7", sha256="4af6606dd072fe946960680611ba65201be435b43edbfc5cc635b2a01a899e6e")
     version("3.4.2", sha256="b0c4f9a11eb500b1d440d5e51a6c0c632aa4ac458e2dc0362f50f999eb7fbf31")
 
     # See <https://github.com/containers/podman/issues/16996> for the
     # respective issue and the suggested patch
-    patch("markdown-utf8.diff", when="@4:")
+    patch("markdown-utf8.diff", when="@4:4.3.14.3.14.3.14.3.1")
 
     depends_on("go", type="build")
     depends_on("go-md2man", type="build")
