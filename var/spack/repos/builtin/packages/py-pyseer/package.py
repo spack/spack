a022a169c545250694b98573358f02136aec7e26
Luke Diorio-Toth <ldioriototh@gmail.com>
2023-06-07 08:29:57 -0500

    builds, added bwa
    dependencies are a mess, but still installs incorrect py-scipy version

--- var/spack/repos/builtin/packages/py-pyseer/package.py
+++ var/spack/repos/builtin/packages/py-pyseer/package.py
@@ -19,19 +19,22 @@ class PyPyseer(PythonPackage):
     # build options
     variant("matplotlib", default=True, description="Enables automatic scree plots")
     variant("dendropy", default=True, description="Enables calculating phylogeny distances")
-    variant("kmer", default=True, description="Enables k-mer annotation")
+    variant("kmer", default=True, description="Enables k-mer mapping and annotation")
 
     # build deps
     depends_on("py-setuptools", type="build")
 
     # additional deps
     # https://pyseer.readthedocs.io/en/master/installation.html
+    # also from
+    # github.com/bioconda/bioconda-recipes/blob/master/recipes/pyseer/meta.yaml
+    # the deps are messy and this project is not updated
     depends_on("py-numpy@1.15.2:", type=("build", "run"))
     depends_on("py-scipy@1.1.0:", type=("build", "run"))
     depends_on("py-pandas@0.23.4:", type=("build", "run"))
     depends_on("py-scikit-learn@0.20.0:", type=("build", "run"))
     depends_on("py-statsmodels@0.9.0:", type=("build", "run"))
-    depends_on("py-pysam@0.15.1:", type=("build", "run"))
+    depends_on("py-pysam@0.15.3:", type=("build", "run"))
     depends_on("py-glmnet-python@1.0", type=("build", "run"))
     depends_on("py-dendropy@4.4.0:", type=("build", "run"))
     depends_on("py-tqdm@4.20.0:", type=("build", "run"))
@@ -41,3 +44,4 @@ class PyPyseer(PythonPackage):
     depends_on("py-dendropy@4.3.0", when="+dendropy", type=("build", "run"))
     depends_on("py-pybedtools@2.27.0:", when="+kmer", type=("build", "run"))
     depends_on("bedops@2.4.9:", when="+kmer", type=("build", "run"))
+    depends_on("bwa", when="+kmer", type=("build", "run"))
