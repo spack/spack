82590eea2ffe41add506e48e4844d3783ff655f9
Jen Herting <jen@herting.cc>
2023-06-07 15:45:58 -0400

    [py-wasabi] added version 1.1.2

--- var/spack/repos/builtin/packages/py-wasabi/package.py
+++ var/spack/repos/builtin/packages/py-wasabi/package.py
@@ -13,6 +13,8 @@ class PyWasabi(PythonPackage):
     homepage = "https://ines.io/"
     pypi = "wasabi/wasabi-0.6.0.tar.gz"
 
+    version("1.1.2", sha256="1aaef3aceaa32edb9c91330d29d3936c0c39fdb965743549c173cb54b16c30b5")
     version("0.6.0", sha256="b8dd3e963cd693fde1eb6bfbecf51790171aa3534fa299faf35cf269f2fd6063")
 
     depends_on("py-setuptools", type="build")
+    depends_on("py-typing-extensions@3.7.4.1:4.4", type=("build", "run"), when="@1.1.2:^python@:3.7")
