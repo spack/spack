92999642213988da9dcbb535e570c16d595c9ee6
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-06-09 01:09:41 +0200

    py-lxml: add 4.9.2 (#38191)

--- var/spack/repos/builtin/packages/py-lxml/package.py
+++ var/spack/repos/builtin/packages/py-lxml/package.py
@@ -14,6 +14,7 @@ class PyLxml(PythonPackage):
     pypi = "lxml/lxml-4.6.1.tar.gz"
     git = "https://github.com/lxml/lxml"
 
+    version("4.9.2", sha256="2455cfaeb7ac70338b3257f41e21f0724f4b5b0c0e7702da67ee6c3640835b67")
     version("4.9.1", sha256="fe749b052bb7233fe5d072fcb549221a8cb1a16725c47c37e42b0b9cb3ff2c3f")
     version("4.9.0", sha256="520461c36727268a989790aef08884347cd41f2d8ae855489ccf40b50321d8d7")
     version("4.8.0", sha256="f63f62fc60e6228a4ca9abae28228f35e1bd3ce675013d1dfb828688d50c6e23")
@@ -31,8 +32,8 @@ class PyLxml(PythonPackage):
     variant("htmlsoup", default=False, description="Enable BeautifulSoup4 backend")
     variant("cssselect", default=False, description="Enable cssselect module")
 
-    depends_on("python@2.7:2.8,3.5:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
+
     depends_on("libxml2@2.9.2:", type=("build", "link", "run"))
     depends_on("libxslt@1.1.27:", type=("build", "link", "run"))
     depends_on("py-html5lib", when="+html5", type=("build", "run"))
