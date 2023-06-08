fec2a40efe155615739445be8b86b08e8c8679e0
Wouter Deconinck <wdconinc@gmail.com>
2023-06-08 11:53:02 -0500

    (r-rcpp)ensmallen: new package (#38249)
    * ensmallen: new package
      ensmallen is a high-quality C++ library for non-linear numerical optimization.
    * r-rcppensmallen: new package
    
    ---------
    
    Co-authored-by: wdconinc <wdconinc@users.noreply.github.com>

---
+++ var/spack/repos/builtin/packages/r-rcppensmallen/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class RRcppensmallen(RPackage):
+    """Rcpp integration for the Ensmallen templated C++ mathematical    optimization library."""
+
+    cran = "RcppEnsmallen"
+
+    version(
+        "0.2.19.0.1", sha256="b4a9bde4dde309a52a47b56790389ecab14fe64066098d2a38b1b588ba3d8631"
+    )
+
+    depends_on("r@3.3.0:", type=("build", "run"))
+    depends_on("r-rcpp", type=("build", "run"))
+    depends_on("r-rcpparmadillo@0.9.800.0.0:", type=("build", "run"))
