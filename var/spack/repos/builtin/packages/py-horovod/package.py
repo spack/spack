b2163cec3cb3e48234a2e12114b3ca095ab5da8e
Thomas Bouvier <contact@thomas-bouvier.io>
2023-06-08 05:51:19 +0200

    `py-horovod`: add versions v0.27.0 and v0.28.0 (#38016)
    * `py-horovod`: add versions v0.27.0 and v0.28.0
    
    * fix: typo
    
    * Add upper version bound for `py-pyarrow`

--- var/spack/repos/builtin/packages/py-horovod/package.py
+++ var/spack/repos/builtin/packages/py-horovod/package.py
@@ -14,9 +14,11 @@ class PyHorovod(PythonPackage, CudaPackage):
     homepage = "https://github.com/horovod"
     git = "https://github.com/horovod/horovod.git"
 
-    maintainers("adamjstewart", "aweits", "tgaddair")
+    maintainers("adamjstewart", "aweits", "tgaddair", "thomas-bouvier")
 
     version("master", branch="master", submodules=True)
+    version("0.28.0", tag="v0.28.0", submodules=True)
+    version("0.27.0", tag="v0.27.0", submodules=True)
     version("0.26.1", tag="v0.26.1", submodules=True)
     version("0.26.0", tag="v0.26.0", submodules=True)
     version("0.25.0", tag="v0.25.0", submodules=True)
@@ -118,7 +120,7 @@ class PyHorovod(PythonPackage, CudaPackage):
     depends_on("py-petastorm@0.9.8:", type=("build", "run"), when="frameworks=spark @0.21.1:")
     depends_on("py-petastorm@0.11:", type=("build", "run"), when="frameworks=spark @0.22:")
     depends_on("py-petastorm@0.12:", type=("build", "run"), when="frameworks=spark @0.26:")
-    depends_on("py-pyarrow@0.15.0:", type=("build", "run"), when="frameworks=spark")
+    depends_on("py-pyarrow@0.15.0:10", type=("build", "run"), when="frameworks=spark")
     depends_on("py-pyspark@2.3.2:", type=("build", "run"), when="frameworks=spark ^python@:3.7")
     depends_on("py-pyspark@3.0.0:", type=("build", "run"), when="frameworks=spark ^python@3.8:")
     depends_on("py-fsspec", type=("build", "run"), when="frameworks=spark @0.22.1:0.24.1")
