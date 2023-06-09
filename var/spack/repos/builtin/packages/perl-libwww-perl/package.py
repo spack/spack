252856459b0318fe3e2a0ac7710968fa8c5a187e
snehring <snehring@iastate.edu>
2023-06-09 12:22:08 -0500

    perl-libwww-perl: adding perl-clone dependency

--- var/spack/repos/builtin/packages/perl-libwww-perl/package.py
+++ var/spack/repos/builtin/packages/perl-libwww-perl/package.py
@@ -19,6 +19,7 @@ class PerlLibwwwPerl(PerlPackage):
     version("6.33", sha256="97417386f11f007ae129fe155b82fd8969473ce396a971a664c8ae6850c69b99")
     version("6.29", sha256="4c6f2697999d2d0e6436b584116b12b30dc39990ec0622751c1a6cec2c0e6662")
 
+    depends_on("perl-clone", type=("build", "run"))
     depends_on("perl-encode-locale", type=("build", "run"))
     depends_on("perl-file-listing", type=("build", "run"))
     depends_on("perl-html-parser", type=("build", "run"))
