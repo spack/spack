832e2e3221bc7cae94b9b6c6e2a10ecd6a170fa8
Marc Schouler <marc.schouler@inria.fr>
2023-06-09 10:17:41 +0200

    Enforce multiple requested changes

--- var/spack/repos/builtin/packages/py-iterative-stats/package.py
+++ var/spack/repos/builtin/packages/py-iterative-stats/package.py
@@ -9,25 +9,24 @@ from spack.package import *
 class PyIterativeStats(PythonPackage):
     """Bacis iterative statistics implementation."""
 
-    homepage = "https://github.com/IterativeStatistics/BasicIterativeStatistics"
-    url = "https://github.com/IterativeStatistics/BasicIterativeStatistics/archive/refs/heads/main.zip"
+    pypi = "iterative-stats/iterative-stats-4.0.0.zip"
     git = "https://github.com/IterativeStatistics/BasicIterativeStatistics.git"
     maintainers("frobinou")
 
-    version("0.0.4", sha256="6e5194ff5ca11e901157aa2a2a9a15ab9bb22ce55d95596b5b3b703b0ea80e69")
     version("main", branch="main")
+    version("0.0.4", sha256="6e5194ff5ca11e901157aa2a2a9a15ab9bb22ce55d95596b5b3b703b0ea80e69")
 
     # main dependencies
-    depends_on("python@3.8.0:", type=("build", "run"))
+    depends_on("python@3.8.0:3.10", type=("build", "run"))
     depends_on("py-poetry-core@1.0.0:", type=("build"))
-    depends_on("py-pyaml@6.0:", type=("build"))
-    depends_on("py-numpy@1.19.0:", type=("run"))
+    depends_on("py-pyyaml@6.0", type=("build", "run"))
+    depends_on("py-numpy@1.19:1", type=("build", "run"))
 
     # dev dependencies
-    depends_on("py-pytest@6.2.1:", type=("test"))
-    depends_on("py-autopep8@1.6.0:", type=("test"))
-    depends_on("openturns@1.19.0:", type=("test"))
-    depends_on("py-scipy@1.8.0:", type=("test"))
+    depends_on("py-pytest@6.2.1:6", type=("test"))
+    depends_on("py-autopep8@1.6.0", type=("test"))
+    depends_on("openturns@1.19", type=("test"))
+    depends_on("py-scipy@1.8", type=("test"))
 
     @run_after("install")
     @on_package_attributes(run_tests=True)
