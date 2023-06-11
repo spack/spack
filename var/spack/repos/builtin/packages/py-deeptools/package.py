8afc7cc7beeec8eee03504e5df884310589aa2f5
George Young <A-N-Other@users.noreply.github.com>
2023-06-11 19:25:32 +0100

    Update var/spack/repos/builtin/packages/py-deeptools/package.py
    Co-authored-by: Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>

--- var/spack/repos/builtin/packages/py-deeptools/package.py
+++ var/spack/repos/builtin/packages/py-deeptools/package.py
@@ -26,7 +26,7 @@ class PyDeeptools(PythonPackage):
     depends_on("py-py2bit@0.2.0:", type=("build", "run"))
     depends_on("py-pybigwig@0.2.1:", type=("build", "run"))
     depends_on("py-pysam@0.14.0:", type=("build", "run"))
-    depends_on("py-matplotlib@3.3.0:", type=("build", "run"), when="@3.5:")
+    depends_on("py-matplotlib@3.3.0:", type=("build", "run"), when="@3.5.1:")
     depends_on("py-matplotlib@2.1.2:", type=("build", "run"), when="@:3.3.0")
     depends_on("py-numpydoc@0.5:", type=("build", "run"))
     depends_on("py-plotly@4.9:", type=("build", "run"), when="@3.5.1")
