74b71352f8266d79837ec93648072f87e184fe07
Adam J. Stewart <ajstewart426@gmail.com>
2023-06-12 08:55:40 -0500

    Deprecate R packages for spatial analysis (#38158)
    * Deprecate R packages for spatial analysis
    
    * [@spackbot] updating style on behalf of adamjstewart
    
    ---------
    
    Co-authored-by: adamjstewart <adamjstewart@users.noreply.github.com>

--- var/spack/repos/builtin/packages/r-rgdal/package.py
+++ var/spack/repos/builtin/packages/r-rgdal/package.py
@@ -23,15 +23,51 @@ class RRgdal(RPackage):
 
     cran = "rgdal"
 
-    version("1.6-6", sha256="d742d7aadfc004771e61cac28a1faffeb4dbda981dea19115be11c541087399a")
-    version("1.6-2", sha256="7eab4b0adaa788b985cd33da214799d562a943f5788631ebd1301dfc69ca033d")
-    version("1.5-32", sha256="4583a4e187492eb936b59bc6bfeefea687b115bc3ae25172e0ed348b38f473ed")
-    version("1.5-28", sha256="7f54432cfa8c0db463f68e8856c3ca0a90671dc841ac5203af049eb318e261a2")
-    version("1.5-19", sha256="6fbfd71b19e7b8d9dbddc91cb1eef8890c74a1e2bf8f619da165ff51bf1231b2")
-    version("1.5-18", sha256="53467c19bc93d8ea311458eaa281c8c456168ab75e84d76ef5cc6c00f53122df")
-    version("1.4-4", sha256="2532e76e0af27d145f799d70006a5dbecb2d3be698e3d0bbf580f4c41a34c5d7")
-    version("1.3-9", sha256="3e44f88d09894be4c0abd8874d00b40a4a5f4542b75250d098ffbb3ba41e2654")
-    version("1.2-16", sha256="017fefea4f9a6d4540d128c707197b7025b55e4aff98fc763065366b025b03c9")
+    version(
+        "1.6-6",
+        sha256="d742d7aadfc004771e61cac28a1faffeb4dbda981dea19115be11c541087399a",
+        deprecated=True,
+    )
+    version(
+        "1.6-2",
+        sha256="7eab4b0adaa788b985cd33da214799d562a943f5788631ebd1301dfc69ca033d",
+        deprecated=True,
+    )
+    version(
+        "1.5-32",
+        sha256="4583a4e187492eb936b59bc6bfeefea687b115bc3ae25172e0ed348b38f473ed",
+        deprecated=True,
+    )
+    version(
+        "1.5-28",
+        sha256="7f54432cfa8c0db463f68e8856c3ca0a90671dc841ac5203af049eb318e261a2",
+        deprecated=True,
+    )
+    version(
+        "1.5-19",
+        sha256="6fbfd71b19e7b8d9dbddc91cb1eef8890c74a1e2bf8f619da165ff51bf1231b2",
+        deprecated=True,
+    )
+    version(
+        "1.5-18",
+        sha256="53467c19bc93d8ea311458eaa281c8c456168ab75e84d76ef5cc6c00f53122df",
+        deprecated=True,
+    )
+    version(
+        "1.4-4",
+        sha256="2532e76e0af27d145f799d70006a5dbecb2d3be698e3d0bbf580f4c41a34c5d7",
+        deprecated=True,
+    )
+    version(
+        "1.3-9",
+        sha256="3e44f88d09894be4c0abd8874d00b40a4a5f4542b75250d098ffbb3ba41e2654",
+        deprecated=True,
+    )
+    version(
+        "1.2-16",
+        sha256="017fefea4f9a6d4540d128c707197b7025b55e4aff98fc763065366b025b03c9",
+        deprecated=True,
+    )
 
     depends_on("r@3.3.0:", type=("build", "run"))
     depends_on("r@3.5.0:", type=("build", "run"), when="@1.5:")
