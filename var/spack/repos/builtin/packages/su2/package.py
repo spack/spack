9bcef0f9fa987701a0f106a8936d1e5b4adb7a8f
kjrstory <kjrstory@gmail.com>
2023-06-13 15:05:46 +0900

    SU2: add brief comment of meson_version.patch

--- var/spack/repos/builtin/packages/su2/package.py
+++ var/spack/repos/builtin/packages/su2/package.py
@@ -61,6 +61,8 @@ class Su2(MesonPackage):
     depends_on("openblas", when="+openblas ~mkl")
     depends_on("cmake", type="build", when="+mpp")
 
+    # Remove the part that fixes the meson version to 0.61.1. 
+    # This fix is considered meaningless and will be removed in the next version(@7.6:) of SU2.
     patch("meson_version.patch", when="@7.4.0:7.5.1")
 
     def meson_args(self):
