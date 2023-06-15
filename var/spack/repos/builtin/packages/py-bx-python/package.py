36cf6f2c7c1e2e0c0f41dab191d6f65afa45a3f0
George Young <A-N-Other@users.noreply.github.com>
2023-06-15 20:12:18 +0100

    Remove python upper bound

--- var/spack/repos/builtin/packages/py-bx-python/package.py
+++ var/spack/repos/builtin/packages/py-bx-python/package.py
@@ -16,7 +16,7 @@ class PyBxPython(PythonPackage):
     version("0.9.0", sha256="fe545c44d2ea74b239d41e9090618aaf6a859d1a1f64b4a21b133cb602dfdb49")
     version("0.8.8", sha256="ad0808ab19c007e8beebadc31827e0d7560ac0e935f1100fb8cc93607400bb47")
 
-    depends_on("python@3.7:3.10", when="@0.8.13:", type=("build", "run"))
+    depends_on("python@3.7:", when="@0.8.13:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-cython", type="build")
     depends_on("py-numpy", type=("build", "run"))
