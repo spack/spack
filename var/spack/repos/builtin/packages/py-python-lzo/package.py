0fd69e074ce597292e25d87479caffd76ed0e301
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2023-06-15 16:35:05 +0100

    py-python-lzo: add 1.15

--- var/spack/repos/builtin/packages/py-python-lzo/package.py
+++ var/spack/repos/builtin/packages/py-python-lzo/package.py
@@ -13,6 +13,7 @@ class PyPythonLzo(PythonPackage):
     homepage = "https://github.com/jd-boyd/python-lzo"
     pypi = "python-lzo/python-lzo-1.12.tar.gz"
 
+    version("1.15", sha256="a57aaa00c5c3a0515dd9f7426ba2cf601767dc19dc023d8b99d4a13b0a327b49")
     version("1.12", sha256="97a8e46825e8f1abd84c2a3372bc09adae9745a5be5d3af2692cd850dac35345")
 
     # pip silently replaces distutils with setuptools
