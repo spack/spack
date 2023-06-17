e945a7abe29b0d0fa903e4cf5caa83e3efc613b1
Morten Kristensen <me@mortens.dev>
2023-06-17 17:33:17 +0200

    Removed obsolete dep and setuptools is only for build-time
    - setuptools are not used as runtime
    - py27 isn't strictly necessary

--- var/spack/repos/builtin/packages/py-vermin/package.py
+++ var/spack/repos/builtin/packages/py-vermin/package.py
@@ -38,8 +38,7 @@ class PyVermin(PythonPackage):
     version("0.10.4", sha256="bd765b84679fb3756b26f462d2aab4af3183fb65862520afc1517f6b39dea8bf")
     version("0.10.0", sha256="3458a4d084bba5c95fd7208888aaf0e324a07ee092786ee4e5529f539ab4951f")
 
-    depends_on("python@2.7:", type=("build", "run"))
-    depends_on("py-setuptools", type=("build", "run"))
+    depends_on("py-setuptools", type=("build"))
 
     @run_after("install")
     @on_package_attributes(run_tests=True)
