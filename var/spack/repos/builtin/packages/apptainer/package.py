d85f25a901d250db9fbbde395e83be2cf22d6903
snehring <7978778+snehring@users.noreply.github.com>
2023-06-20 14:20:48 -0500

    apptainer: adding version 1.1.9 (#38492)

--- var/spack/repos/builtin/packages/apptainer/package.py
+++ var/spack/repos/builtin/packages/apptainer/package.py
@@ -27,6 +27,7 @@ class Apptainer(SingularityBase):
     git = "https://github.com/apptainer/apptainer.git"
 
     version("main", branch="main")
+    version("1.1.9", sha256="c615777539154288542cf393d3fd44c04ccb3260bc6330dc324d4e4ebe902bfa")
     version("1.1.7", sha256="e6d3956a26c3965703402e17f153ba07f59bf710068806462b314d2d04e825e7")
     version("1.1.6", sha256="5f32d305279a51ce8bdbe69e733c4ac12b1efdcb77758fab8ec9463e96a8fd82")
     version("1.1.5", sha256="3eadb26b6656a89a111abe29c7e50eab0023e9a8718f1e77e46ca871398bfa67")
