e49941a452b9873072904811f1cc4ce68592a58f
snehring <snehring@iastate.edu>
2023-06-21 15:56:57 -0500

    emblmygff3: adding py-setuptools dep

--- var/spack/repos/builtin/packages/emblmygff3/package.py
+++ var/spack/repos/builtin/packages/emblmygff3/package.py
@@ -21,6 +21,7 @@ class Emblmygff3(PythonPackage):
     version("2.2", sha256="225b2b50da9064f779e164b2859506d7540d11fa78f7d41b5c0d94f02f7845c5")
     version("2.1", sha256="64aef403bc64088eca504b69acffb3fb16ec4448cd3d6c9692b7baf276b92fd2")
 
+    depends_on("py-setuptools", type="build")
     depends_on("python@3.8.0:", type=("build", "run"))
     depends_on("py-biopython@1.78:", type=("build", "run"))
     depends_on("py-bcbio-gff@0.6.4:", type=("build", "run"))
