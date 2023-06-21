2503efb988e918bb7d430d678f6b717840f0baad
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-06-21 19:34:04 +0200

    py-prometheus-client: add 0.17.0 (#38471)

--- var/spack/repos/builtin/packages/py-prometheus-client/package.py
+++ var/spack/repos/builtin/packages/py-prometheus-client/package.py
@@ -9,8 +9,10 @@ from spack.package import *
 class PyPrometheusClient(PythonPackage):
     """Prometheus instrumentation library for Python applications."""
 
+    homepage = "https://github.com/prometheus/client_python"
     pypi = "prometheus_client/prometheus_client-0.7.1.tar.gz"
 
+    version("0.17.0", sha256="9c3b26f1535945e85b8934fb374678d263137b78ef85f305b1156c7c881cd11b")
     version("0.14.1", sha256="5459c427624961076277fdc6dc50540e2bacb98eebde99886e59ec55ed92093a")
     version("0.12.0", sha256="1b12ba48cee33b9b0b9de64a1047cbd3c5f2d0ab6ebcead7ddda613a750ec3c5")
     version("0.7.1", sha256="71cd24a2b3eb335cb800c7159f423df1bd4dcd5171b234be15e3f31ec9f622da")
@@ -22,20 +24,14 @@ class PyPrometheusClient(PythonPackage):
     depends_on("py-setuptools", type="build")
     # Notice: prometheus_client/twisted/_exposition.py imports 'twisted.web.wsgi'
     # which was not ported to Python 3 until twisted 16.0.0
-    depends_on("py-twisted", when="+twisted", type=("build", "run"))
     depends_on("py-twisted@16:", when="@0.12.0: +twisted ^python@3:", type=("build", "run"))
-    depends_on("python@2.7:2,3.4:", when="@0.12.0", type=("build", "run"))
-    depends_on("python@3.6:", when="@0.14.1:", type=("build", "run"))
+    depends_on("py-twisted", when="+twisted", type=("build", "run"))
 
     @property
-    def import_modules(self):
-        modules = [
-            "prometheus_client",
-            "prometheus_client.openmetrics",
-            "prometheus_client.bridge",
-        ]
+    def skip_modules(self):
+        modules = []
 
-        if "+twisted" in self.spec:
+        if self.spec.satisfies("~twisted"):
             modules.append("prometheus_client.twisted")
 
         return modules
