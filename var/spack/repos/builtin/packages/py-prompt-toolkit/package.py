082934f73f377295aa706c8924ad8531d1579b69
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-06-22 10:04:23 +0200

    py-prompt-toolkit: add 3.0.38 (#38472)

--- var/spack/repos/builtin/packages/py-prompt-toolkit/package.py
+++ var/spack/repos/builtin/packages/py-prompt-toolkit/package.py
@@ -9,34 +9,14 @@ from spack.package import *
 class PyPromptToolkit(PythonPackage):
     """Library for building powerful interactive command lines in Python"""
 
+    homepage = "https://github.com/prompt-toolkit/python-prompt-toolkit"
     pypi = "prompt_toolkit/prompt_toolkit-1.0.9.tar.gz"
 
     # 'prompt_toolkit.contrib.ssh' requires 'asyncssh', but 'asyncssh' isn't listed as a
     # dependency. Leave out of 'import_modules' to avoid unnecessary dependency.
-    import_modules = [
-        "prompt_toolkit",
-        "prompt_toolkit.filters",
-        "prompt_toolkit.lexers",
-        "prompt_toolkit.input",
-        "prompt_toolkit.layout",
-        "prompt_toolkit.output",
-        "prompt_toolkit.completion",
-        "prompt_toolkit.contrib",
-        "prompt_toolkit.contrib.completers",
-        "prompt_toolkit.contrib.regular_languages",
-        "prompt_toolkit.contrib.telnet",
-        "prompt_toolkit.key_binding",
-        "prompt_toolkit.key_binding.bindings",
-        "prompt_toolkit.styles",
-        "prompt_toolkit.shortcuts",
-        "prompt_toolkit.shortcuts.progress_bar",
-        "prompt_toolkit.formatted_text",
-        "prompt_toolkit.eventloop",
-        "prompt_toolkit.application",
-        "prompt_toolkit.widgets",
-        "prompt_toolkit.clipboard",
-    ]
+    skip_modules = ["prompt_toolkit.contrib.ssh"]
 
+    version("3.0.38", sha256="23ac5d50538a9a38c8bde05fecb47d0b403ecd0662857a86f886f798563d5b9b")
     version("3.0.31", sha256="9ada952c9d1787f52ff6d5f3484d0b4df8952787c087edf6a1f7c2cb1ea88148")
     version("3.0.29", sha256="bd640f60e8cecd74f0dc249713d433ace2ddc62b65ee07f96d358e0b152b6ea7")
     version("3.0.24", sha256="1bb05628c7d87b645974a1bad3f17612be0c29fa39af9f7688030163f680bad6")
@@ -48,9 +28,14 @@ class PyPromptToolkit(PythonPackage):
     version("1.0.16", sha256="c1cedd626e08b8ee830ee65897de754113ff3f3035880030c08b01674d85c5b4")
     version("1.0.9", sha256="cd6523b36adc174cc10d54b1193eb626b4268609ff6ea92c15bcf1996609599c")
 
+    depends_on("python@3.7:", when="@3.0.37:", type=("build", "run"))
     depends_on("python@3.6.2:", when="@3.0.24:", type=("build", "run"))
     depends_on("python@3.6.1:", when="@3:3.0.17", type=("build", "run"))
-    depends_on("python@2.6:2.8,3.3:", when="@:2", type=("build", "run"))
+    # collections.Mapping was removed in python@3.10
+    depends_on("python@:3.9", when="@1.0.9", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-    depends_on("py-six@1.9.0:", when="@:2", type=("build", "run"))
+
     depends_on("py-wcwidth", type=("build", "run"))
+
+    # Historical dependencies
+    depends_on("py-six@1.9.0:", when="@:2", type=("build", "run"))
