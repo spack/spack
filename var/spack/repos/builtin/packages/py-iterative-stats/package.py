7eedbc02d21db3dde225c41cf46db007cd2477b1
mschouler <schouler.marc@gmail.com>
2023-06-23 22:07:04 +0200

    Add recipe for iterative-stats (#38039)
    * Add recipe for iterative-stats
    
    * Fix branch name and remove comment
    
    * Add git link
    
    * Add package maintainer
    
    * Enforce multiple requested changes
    
    * Update var/spack/repos/builtin/packages/py-iterative-stats/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Update checksum
    
    * Fix openturns dependency specification
    
    * Add python variant spec to openturns
    
    ---------
    
    Co-authored-by: Marc Schouler <marc.schouler@inria.fr>
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

---
+++ var/spack/repos/builtin/packages/py-iterative-stats/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyIterativeStats(PythonPackage):
+    """Bacis iterative statistics implementation."""
+
+    pypi = "iterative-stats/iterative_stats-0.0.4.tar.gz"
+    git = "https://github.com/IterativeStatistics/BasicIterativeStatistics.git"
+    maintainers("frobinou")
+
+    version("main", branch="main")
+    version("0.0.4", sha256="7e838aa79de867b0e312be8cdf9319bb70824b624c684e968636cc8d4c9d5712")
+
+    # main dependencies
+    depends_on("python@3.8.0:3.10", type=("build", "run"))
+    depends_on("py-poetry-core@1.0.0:", type=("build"))
+    depends_on("py-pyyaml@6.0", type=("build", "run"))
+    depends_on("py-numpy@1.19:1", type=("build", "run"))
+
+    # dev dependencies
+    depends_on("py-pytest@6.2.1:6", type=("test"))
+    depends_on("py-autopep8@1.6.0", type=("test"))
+    depends_on("openturns@1.19+python+libxml2", type=("test"))
+    depends_on("py-scipy@1.8", type=("test"))
+
+    @run_after("install")
+    @on_package_attributes(run_tests=True)
+    def install_test(self):
+        pytest = which("pytest")
+        pytest()
