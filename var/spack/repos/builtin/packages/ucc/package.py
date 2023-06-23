4f8ad4654bae8279a965c9c5e0dc5e885234360f
Carlos Bederian <carlos.bederian@unc.edu.ar>
2023-06-23 20:17:38 -0300

    ucc: use conditional variant for cuda

--- var/spack/repos/builtin/packages/ucc/package.py
+++ var/spack/repos/builtin/packages/ucc/package.py
@@ -19,12 +19,11 @@ class Ucc(AutotoolsPackage, CudaPackage):
     version("1.1.0", sha256="74c8ba75037b5bd88cb703e8c8ae55639af3fecfd4428912a433c010c97b4df7")
     version("1.0.0", sha256="d3b4aa7004bf339d35952a1699a6e408064ba578bdc93861f5f07527ad0a5e8c")
 
-    variant("cuda", default=False, description="Enable CUDA TL")
+    variant("cuda", default=False, description="Enable CUDA TL", when="@1.1:")
     variant("nccl", default=False, description="Enable NCCL TL")
     variant("rccl", default=False, description="Enable RCCL TL", when="@1.1:")
     variant("ucp", default=True, description="Enable UCP TL")
 
-    conflicts("+cuda", when="@:1.0", msg="UCC CUDA TL added in version 1.1")
     conflicts("cuda@12:", when="@1.1", msg="UCC 1.1 supports CUDA <12")
     conflicts("~cuda", when="+nccl", msg="UCC NCCL TL requires CUDA")
 
