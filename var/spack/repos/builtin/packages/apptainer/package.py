6098f4df1641f1b71560e09c1ac6cb8f15abae3d
snehring <7978778+snehring@users.noreply.github.com>
2023-06-24 09:02:51 -0500

    apptainer: add squashfuse dep (#38499)

--- var/spack/repos/builtin/packages/apptainer/package.py
+++ var/spack/repos/builtin/packages/apptainer/package.py
@@ -36,6 +36,7 @@ class Apptainer(SingularityBase):
     version("1.0.2", sha256="2d7a9d0a76d5574459d249c3415e21423980d9154ce85e8c34b0600782a7dfd3")
 
     depends_on("go@1.17.5:", when="@1.1.0:")
+    depends_on("squashfuse", type="run")
 
     singularity_org = "apptainer"
     singularity_name = "apptainer"
