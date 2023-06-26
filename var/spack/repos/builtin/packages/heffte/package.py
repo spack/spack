7291063bb4e4b1044f4ef3135eb659b34af8c4d0
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2023-06-26 06:10:55 -0700

    tests/heffte/tasmanian: convert to new stand-alone test process (#35750)

--- var/spack/repos/builtin/packages/heffte/package.py
+++ var/spack/repos/builtin/packages/heffte/package.py
@@ -3,10 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-import os
-
-from llnl.util import tty
-
 from spack.package import *
 
 
@@ -130,68 +126,38 @@ class Heffte(CMakePackage, CudaPackage, ROCmPackage):
 
         return args
 
-    def cmake_bin(self, set_path=True):
-        """(Hack) Set/get cmake dependency path. Sync with Tasmanian."""
-        filepath = join_path(self.install_test_root, "cmake_bin_path.txt")
-        if set_path:
-            with open(filepath, "w") as out_file:
-                cmake_bin = join_path(self.spec["cmake"].prefix.bin, "cmake")
-                out_file.write("{0}\n".format(cmake_bin))
-        elif os.path.isfile(filepath):
-            with open(filepath, "r") as in_file:
-                return in_file.read().strip()
-
     @run_after("install")
     def setup_smoke_test(self):
         install_tree(
             self.prefix.share.heffte.testing, join_path(self.install_test_root, "testing")
         )
-        self.cmake_bin(set_path=True)
 
-    def test(self):
-        cmake_bin = self.cmake_bin(set_path=False)
-
-        if not cmake_bin:
-            tty.msg("Skipping heffte test: cmake_bin_path.txt not found")
-            return
-
+    def test_make_test(self):
+        """build and run make(test)"""
         # using the tests copied from <prefix>/share/heffte/testing
         cmake_dir = self.test_suite.current_test_cache_dir.testing
 
         options = [cmake_dir]
         if "+rocm" in self.spec:
-            options.append(
-                "-Dhip_DIR=" + join_path(self.spec["hip"].prefix, "lib", "cmake", "hip")
-            )
-            options.append(
-                "-DAMDDeviceLibs_DIR="
-                + join_path(self.spec["llvm-amdgpu"].prefix, "lib", "cmake", "AMDDeviceLibs")
-            )
-            options.append(
-                "-Damd_comgr_DIR="
-                + join_path(self.spec["comgr"].prefix, "lib", "cmake", "amd_comgr")
-            )
-            options.append(
-                "-Dhsa-runtime64_DIR="
-                + join_path(self.spec["hsa-rocr-dev"].prefix, "lib", "cmake", "hsa-runtime64")
-            )
-            options.append(
-                "-DHSA_HEADER=" + join_path(self.spec["hsa-rocr-dev"].prefix, "include")
-            )
-            options.append(
-                "-Drocfft_DIR=" + join_path(self.spec["rocfft"].prefix, "lib", "cmake", "rocfft")
+            options.extend(
+                [
+                    f"-Dhip_DIR={self.spec['hip'].prefix.lib.cmake.hip}",
+                    "-DAMDDeviceLibs_DIR="
+                    + f"{self.spec['llvm-amdgpu'].prefix.lib.cmake.AMDDeviceLibs}",
+                    f"-Damd_comgr_DIR={self.spec['comgr'].prefix.lib.cmake.amd_comgr}",
+                    "-Dhsa-runtime64_DIR="
+                    + f"{self.spec['hsa-rocr-dev'].prefix.lib.cmake.hsa-runtime64}",
+                    "-DHSA_HEADER={self.spec['hsa-rocr-dev'].prefix.include}",
+                    "-Drocfft_DIR={self.spec['rocfft'].prefix.lib.cmake.rocfft}",
+                ]
             )
 
-        # Provide the path to the MPI dependency so that the test can find an MPI launcher
-        options.append("-DMPI_HOME=" + self.spec["mpi"].prefix)
+        # Provide the root directory of the MPI installation.
+        options.append(f"-DMPI_HOME={self.spec['mpi'].prefix}")
 
-        if not self.run_test(cmake_bin, options=options, purpose="Generate the Makefile"):
-            tty.msg("Skipping heffte test: failed to generate Makefile")
-            return
+        cmake = which(self.spec["cmake"].prefix.bin.cmake)
+        cmake(*options)
 
-        if not self.run_test("make", purpose="Build test software"):
-            tty.msg("Skipping heffte test: failed to build test")
-            return
-
-        if not self.run_test("make", options=["test"], purpose="Run test"):
-            tty.msg("Failed heffte test: failed to run test")
+        make = which("make")
+        make()
+        make("test")
