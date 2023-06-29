1e1eaee109f7fcab8ad66f2c683da076111d7b0b
Thomas Bouvier <contact@thomas-bouvier.io>
2023-06-29 10:46:07 +0200

    Update dependencies

--- var/spack/repos/builtin/packages/py-dm-tree/package.py
+++ var/spack/repos/builtin/packages/py-dm-tree/package.py
@@ -20,12 +20,14 @@ class PyDmTree(PythonPackage):
     maintainers("aweits")
 
     version("0.1.8", sha256="0fcaabbb14e7980377439e7140bd05552739ca5e515ecb3119f234acee4b9430")
+    version("0.1.7", sha256="a54a5794b51ba585dc62f5d19c9d907f73107d5f0569417f8866ef3f75be5134")
     version("0.1.6", sha256="6776404b23b4522c01012ffb314632aba092c9541577004ab153321e87da439a")
     version("0.1.5", sha256="a951d2239111dfcc468071bc8ff792c7b1e3192cab5a3c94d33a8b2bda3127fa")
 
     depends_on("py-setuptools", type="build")
-    depends_on("bazel", type="build")
-    depends_on("py-six@1.12.0:", type=("build", "run"))
+    depends_on("cmake", when="@0.1.7:", type="build")
+    depends_on("bazel@:5", when="@:0.1.6", type="build")
+    depends_on("py-six@1.12.0:", when="@:0.1.6", type=("build", "run"))
 
     # This is set later
     tmp_path = None
