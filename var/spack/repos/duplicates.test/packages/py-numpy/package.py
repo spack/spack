8d2977f5e187eed129b80d5d98dd0c68588c75a1
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2023-06-29 17:37:48 +0200

    Add unit-tests for use cases requiring separate concretization of build deps

---
+++ var/spack/repos/duplicates.test/packages/py-numpy/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack.package import *
+
+
+class PyNumpy(Package):
+    """An extension that depends on pinned build dependencies"""
+
+    homepage = "http://www.example.com"
+    url = "http://www.example.com/tdep-1.0.tar.gz"
+
+    tags = ["build-tools"]
+
+    version("1.25.0", md5="0123456789abcdef0123456789abcdef")
+
+    extends("python")
+    depends_on("py-setuptools@=59", type=("build", "run"))
+    depends_on("gmake@4.1", type="build")
