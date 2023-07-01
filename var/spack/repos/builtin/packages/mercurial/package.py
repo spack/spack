977c59ac97b331331f8c2eba5f8ff5b3ab4b9b90
Harmen Stoppels <harmenstoppels@gmail.com>
2023-07-01 12:49:49 +0200

    mercurial: add 6.x, fix python bounds (#38668)
    * mercurial: add 6.x, fix python bounds
    
    * remove python@2 versions, cause package audits is complaining

--- var/spack/repos/builtin/packages/mercurial/package.py
+++ var/spack/repos/builtin/packages/mercurial/package.py
@@ -14,17 +14,24 @@ class Mercurial(PythonPackage):
     homepage = "https://www.mercurial-scm.org"
     url = "https://www.mercurial-scm.org/release/mercurial-5.3.tar.gz"
 
+    version("6.4.5", sha256="b0b4b00b8b2639c8be387394796f0425beb339314df7e72937f8ddd2a41b1b8a")
+    version("6.3.3", sha256="13c97ff589c7605e80a488f336852ce1d538c5d4143cfb33be69bdaddd9157bd")
+    version("6.2.3", sha256="98d1ae002f68adf53d65c5947fe8b7a379f98cf05d9b8ea1f4077d2ca5dce9db")
+    version("6.1.4", sha256="f361f9802b36e357ac019ceb712ca11de8332b07deadeed8dfa904f05bf7ca78")
+    version("6.0.3", sha256="67f13647a46517a2b567cdcb73c98721d75d36a0432debb15022b77f9c138333")
     version("5.8", sha256="fc5d6a8f6478d88ef83cdd0ab6d86ad68ee722bbdf4964e6a0b47c3c6ba5309f")
     version("5.7.1", sha256="cb5139144ccb2ef648f36963c8606d47dea1cb0e22aa2c055d6f860ce3fde7b0")
     version("5.7", sha256="609c3e7c9276dd75b03b713eccc10f5e0553001f35ae21600bcea1509699c601")
     version("5.6.1", sha256="e55c254f4904c45226a106780e57f4279aee03368f6ff6a981d5d2a38243ffad")
     version("5.3", sha256="e57ff61d6b67695149dd451922b40aa455ab02e01711806a131a1e95c544f9b9")
-    version("5.1.2", sha256="15af0b090b23649e0e53621a88dde97b55a734d7cb08b77d3df284db70d44e2e")
-    version("5.1.1", sha256="35fc8ba5e0379c1b3affa2757e83fb0509e8ac314cbd9f1fd133cf265d16e49f")
-    version("4.9.1", sha256="1bdd21bb87d1e05fb5cd395d488d0e0cc2f2f90ce0fd248e31a03595da5ccb47")
-    version("4.4.1", sha256="8f2a5512d6cc2ffb08988aef639330a2f0378e4ac3ee0e1fbbdb64d9fff56246")
 
-    depends_on("python+bz2+ssl+zlib@2.7:2.8,3.5.3:3.5,3.6.2:", type=("build", "run"))
+    depends_on("python+bz2+ssl+zlib", type=("build", "run"))
+    depends_on("python@3.5:", when="@5.2:", type=("build", "run"))
+    depends_on("python@3.6:", when="@6.2:", type=("build", "run"))
+    # Upperbounds because of forward compat issues.
+    depends_on("python@:3.9", when="@:6.0.0", type=("build", "run"))
+    depends_on("python@:3.10", when="@:6.3.0", type=("build", "run"))
+
     depends_on("py-setuptools", type="build")
     depends_on("py-docutils", type="build")
     depends_on("py-pygments", type=("build", "run"))
