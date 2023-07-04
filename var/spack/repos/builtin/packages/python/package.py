1b619a0f51f025ed1778f340b4c373b48b250a63
Harmen Stoppels <me@harmenstoppels.nl>
2023-07-04 18:42:52 +0200

    get the range right...

--- var/spack/repos/builtin/packages/python/package.py
+++ var/spack/repos/builtin/packages/python/package.py
@@ -312,7 +312,7 @@ class Python(Package):
 
     # See https://github.com/python/cpython/issues/106424
     # datetime.now(timezone.utc) segfaults
-    conflicts("@3.8:", when="%oneapi@2022.2.1:")
+    conflicts("@3.9:", when="%oneapi@2022.2.1:")
 
     # Used to cache various attributes that are expensive to compute
     _config_vars: Dict[str, Dict[str, str]] = {}
