9608586944684c6accba0664db053b15cd9d60cd
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2023-07-05 16:21:43 +0200

    circuit-build: update to v5.0.0 (#2067)

--- bluebrain/repo-bluebrain/packages/circuit-build/package.py
+++ bluebrain/repo-bluebrain/packages/circuit-build/package.py
@@ -13,15 +13,15 @@ class CircuitBuild(PythonPackage):
     git = "ssh://git@bbpgitlab.epfl.ch/nse/circuit-build.git"
 
     version("develop", branch="main")
-    version("4.1.0", tag="circuit-build-v4.1.0")
+    version("5.0.0", tag="circuit-build-v5.0.0")
 
+    depends_on("python@3.9:", type=("build", "run"))
     depends_on("py-setuptools", type=("build", "run"))
 
     depends_on("py-click@7.0:", type=("build", "run"))
     depends_on("py-pyyaml@5.0:", type=("build", "run"))
     depends_on("snakemake@6.0:", type=("build", "run"))
     depends_on("py-jsonschema@3.2.0:", type=("build", "run"))
-    depends_on("py-jinja2@2.10.0:", type=("build", "run"))
 
     def setup_run_environment(self, env):
         env.prepend_path("PATH", self.spec["snakemake"].prefix.bin)
