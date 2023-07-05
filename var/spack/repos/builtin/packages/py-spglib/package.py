df610c5d87e6c5cf4fa342a063d252b4269fb1f1
Manuela Kuhn <manuela.kuhn@ovgu.de>
2023-07-05 13:16:52 +0200

    Remove py-setuptools as run dependency

--- var/spack/repos/builtin/packages/py-spglib/package.py
+++ var/spack/repos/builtin/packages/py-spglib/package.py
@@ -18,7 +18,5 @@ class PySpglib(PythonPackage):
     version("1.16.1", sha256="9fd2fefbd83993b135877a69c498d8ddcf20a9980562b65b800cfb4cdadad003")
     version("1.9.9.18", sha256="cbbb8383320b500dc6100b83d5e914a26a97ef8fc97c82d8921b10220e4126cd")
 
-    # Most Python packages only require setuptools as a build dependency.
-    # However, spglib requires setuptools during runtime as well.
-    depends_on("py-setuptools@18.0:", type=("build", "run"))
+    depends_on("py-setuptools@18.0:", type="build")
     depends_on("py-numpy", type=("build", "run"))
