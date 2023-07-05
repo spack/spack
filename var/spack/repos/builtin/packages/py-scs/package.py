b7e0710e7caa40084ac080723ccf5b83af0bf4fa
Manuela Kuhn <manuela.kuhn@ovgu.de>
2023-07-05 05:13:30 +0200

    Restrict py-pip version for straightforward packages

--- var/spack/repos/builtin/packages/py-scs/package.py
+++ var/spack/repos/builtin/packages/py-scs/package.py
@@ -31,6 +31,12 @@ class PyScs(PythonPackage, CudaPackage):
     depends_on("py-numpy@1.7:", type=("build", "run"))
     depends_on("py-scipy@0.13.2:", type=("build", "run"))
 
+    # in newer pip versions --install-options does not exist
+    depends_on("py-pip@:23.0", when="+float32", type="build")
+    depends_on("py-pip@:23.0", when="+extra_verbose", type="build")
+    depends_on("py-pip@:23.0", when="+int32", type="build")
+    depends_on("py-pip@:23.0", when="+blas64", type="build")
+
     def install_options(self, spec, prefix):
         args = []
         if (
