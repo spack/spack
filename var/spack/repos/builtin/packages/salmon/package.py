2369d08a2bd9131b7a12118846f5d86958fa70ea
George Young <A-N-Other@users.noreply.github.com>
2023-07-05 10:18:11 +0100

    Update package.py

--- var/spack/repos/builtin/packages/salmon/package.py
+++ var/spack/repos/builtin/packages/salmon/package.py
@@ -85,6 +85,8 @@ class Salmon(CMakePackage):
             when="@{0}".format(ver),
         )
 
+    # `%gcc13:` requires `<cstdint>` to be manually included. Fixed upstream, so we patch to 
+    # allow building of previous salmon versions
     patch(
         "https://github.com/COMBINE-lab/salmon/commit/ffb2a11.patch?full_index=1",
         sha256="5ed3512bae665c1d72002911ab9ee6d213f10df63019ebd9e8e0ecde03823a73",
