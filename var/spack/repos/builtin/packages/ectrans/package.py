4e474f8fe7c81f373538cf68eb66973c96a0d5f9
Alex Richert <82525672+AlexanderRichert-NOAA@users.noreply.github.com>
2023-07-06 07:32:36 -0700

    Adapt valid-style.yml for JCSDA spack (#290)
    * Adapt valid-style.yml for JCSDA spack
    
    * Update copyright years for ectrans and fiat
    
    * update ectrans style
    
    * style fix ectrans/package.py
    
    ---------
    
    Co-authored-by: Dom Heinzeller <dom.heinzeller@icloud.com>

--- var/spack/repos/builtin/packages/ectrans/package.py
+++ var/spack/repos/builtin/packages/ectrans/package.py
@@ -1,9 +1,9 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class Ectrans(CMakePackage):
@@ -17,46 +17,49 @@ class Ectrans(CMakePackage):
     git = "https://github.com/ecmwf-ifs/ectrans.git"
     url = "https://github.com/ecmwf-ifs/ectrans/archive/1.1.0.tar.gz"
 
-    maintainers('climbfuji')
+    maintainers("climbfuji")
 
-    version('develop', branch='develop', no_cache=True)
-    version('main', branch='main', no_cache=True)
+    version("develop", branch="develop", no_cache=True)
+    version("main", branch="main", no_cache=True)
     version("1.2.0", sha256="2ee6dccc8bbfcc23faada1d957d141f24e41bb077c1821a7bc2b812148dd336c")
     version("1.1.0", sha256="3c9848bb65033fbe6d791084ee347b3adf71d5dfe6d3c11385000017b6469a3e")
 
-    variant('build_type', default='RelWithDebInfo',
-            description='CMake build type',
-            values=('Debug', 'Release', 'RelWithDebInfo'))
+    variant(
+        "build_type",
+        default="RelWithDebInfo",
+        description="CMake build type",
+        values=("Debug", "Release", "RelWithDebInfo"),
+    )
 
-    variant('mpi', default=True, description='Use MPI?')
-    variant('openmp', default=True, description='Use OpenMP?')
+    variant("mpi", default=True, description="Use MPI?")
+    variant("openmp", default=True, description="Use OpenMP?")
 
-    variant('double_precision', default=True, description='Support for double precision?')
-    variant('single_precision', default=True, description='Support for single precision?')
+    variant("double_precision", default=True, description="Support for double precision?")
+    variant("single_precision", default=True, description="Support for single precision?")
 
-    variant('mkl',  default=False, description='Use MKL?')
-    variant('fftw', default=True, description='Use FFTW?')
+    variant("mkl", default=False, description="Use MKL?")
+    variant("fftw", default=True, description="Use FFTW?")
 
-    variant('transi', default=True, description='Compile TransI C-interface to trans?')
+    variant("transi", default=True, description="Compile TransI C-interface to trans?")
 
-    depends_on('ecbuild', type='build')
-    depends_on('mpi', when='+mpi')
-    depends_on('blas')
-    depends_on('lapack')
-    depends_on('fftw-api', when='+fftw')
-    depends_on('mkl', when='+mkl')
+    depends_on("ecbuild", type="build")
+    depends_on("mpi", when="+mpi")
+    depends_on("blas")
+    depends_on("lapack")
+    depends_on("fftw-api", when="+fftw")
+    depends_on("mkl", when="+mkl")
 
-    depends_on('fiat~mpi',  when='~mpi')
-    depends_on('fiat+mpi',  when='+mpi')
+    depends_on("fiat~mpi", when="~mpi")
+    depends_on("fiat+mpi", when="+mpi")
 
     def cmake_args(self):
         args = [
-            self.define_from_variant('ENABLE_MPI', 'mpi'),
-            self.define_from_variant('ENABLE_OMP', 'openmp'),
-            self.define_from_variant('ENABLE_DOUBLE_PRECISION', 'double_precision'),
-            self.define_from_variant('ENABLE_SINGLE_PRECISION', 'single_precision'),
-            self.define_from_variant('ENABLE_FFTW', 'fftw'),
-            self.define_from_variant('ENABLE_MKL', 'mkl'),
-            self.define_from_variant('ENABLE_TRANSI', 'transi')
+            self.define_from_variant("ENABLE_MPI", "mpi"),
+            self.define_from_variant("ENABLE_OMP", "openmp"),
+            self.define_from_variant("ENABLE_DOUBLE_PRECISION", "double_precision"),
+            self.define_from_variant("ENABLE_SINGLE_PRECISION", "single_precision"),
+            self.define_from_variant("ENABLE_FFTW", "fftw"),
+            self.define_from_variant("ENABLE_MKL", "mkl"),
+            self.define_from_variant("ENABLE_TRANSI", "transi"),
         ]
         return args
