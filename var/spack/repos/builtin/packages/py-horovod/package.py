669f4cc49201df0af944d0732a54dd8c9ea24acb
Thomas Bouvier <contact@thomas-bouvier.io>
2023-07-06 06:56:47 +0200

    `py-horovod`: update to v0.28.1 (#38732)

--- var/spack/repos/builtin/packages/py-horovod/package.py
+++ var/spack/repos/builtin/packages/py-horovod/package.py
@@ -17,6 +17,7 @@ class PyHorovod(PythonPackage, CudaPackage):
     maintainers("adamjstewart", "aweits", "tgaddair", "thomas-bouvier")
 
     version("master", branch="master", submodules=True)
+    version("0.28.1", tag="v0.28.1", submodules=True)
     version("0.28.0", tag="v0.28.0", submodules=True)
     version("0.27.0", tag="v0.27.0", submodules=True)
     version("0.26.1", tag="v0.26.1", submodules=True)
