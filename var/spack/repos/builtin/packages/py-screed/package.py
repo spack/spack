bda7e228aa00f4bbdf3254645c2822ec94758bfb
George Young <A-N-Other@users.noreply.github.com>
2023-07-06 11:38:45 +0100

    Update var/spack/repos/builtin/packages/py-screed/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-screed/package.py
+++ var/spack/repos/builtin/packages/py-screed/package.py
@@ -15,6 +15,6 @@ class PyScreed(PythonPackage):
     version("1.1.2", sha256="734ffa7a8a645286496d895b736f91d6b2988956e2fd42358123d93ec8519b6a")
 
     depends_on("py-setuptools@48:", type="build")
-    depends_on("py-setuptools-scm@4:5.0.2", type="build")
+    depends_on("py-setuptools-scm@4:5", type="build")
     depends_on("py-setuptools-scm-git-archive", type="build")
     depends_on("py-wheel@0.29.0:", type="build")
