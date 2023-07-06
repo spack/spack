9e9a7ccedd0b917e46a3ce9cfb1a919814284b9f
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-07-06 16:46:35 -0700

    rename to py-pynvtx

--- var/spack/repos/builtin/packages/py-pynvtx/packages.py
+++ var/spack/repos/builtin/packages/py-pynvtx/packages.py
@@ -6,7 +6,7 @@
 from spack.package import *
 
 
-class PyNvtx(PythonPackage):
+class PyPyNvtx(PythonPackage):
     """A thin python wrapper for the nvToolsExt (NVTX) library, using
     pybind11. This wrapper is meant to be as thin as possible -- so
     only provides minimal support. Currently supported features are:
@@ -15,12 +15,12 @@ class PyNvtx(PythonPackage):
     Function decorator: PyNVTX.annotate
     Automatic decorator generation PyNVTX.annotate_all_methods()"""
 
-    homepage = "https://github.com/JBlaschke/PyNVTX/tree/master"
+    homepage = "https://github.com/JBlaschke/PyNVTX"
     pypi = "PyNVTX/PyNVTX-0.3.3.tar.gz"
 
     maintainers("DaxLynch")
 
     version("0.3.3", sha256="8877b2d90bbf9d279d517a80a8f35a0a0a8179ebabf0729e806798a84bee6c72")
 
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools@40.8:", type="build")
     depends_on("py-pybind11", type=("build", "run"))
