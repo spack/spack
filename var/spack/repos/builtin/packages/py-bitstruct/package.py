888cbef46794516af4c732cf6126dfe26f643f5f
Dax Lynch <81718016+DaxLynch@users.noreply.github.com>
2023-07-06 12:45:23 -0700

    Update package.py
    To pass the style prechecks

--- var/spack/repos/builtin/packages/py-bitstruct/package.py
+++ var/spack/repos/builtin/packages/py-bitstruct/package.py
@@ -3,30 +3,14 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-bitstruct
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-bitstruct
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
 class PyBitstruct(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """This module is intended to have a similar interface as the python
+    struct module, but working on bits instead of primitive data types
+    (char, int, …)"""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/eerimoq/bitstruct"
     pypi = "bitstruct/bitstruct-8.17.0.tar.gz"
 
@@ -35,4 +19,3 @@ class PyBitstruct(PythonPackage):
     version("8.17.0", sha256="eb94b40e4218a23aa8f90406b836a9e6ed83e48b8d112ce3f96408463bd1b874")
 
     depends_on("py-setuptools", type="build")
-
