f9f4dce0993fe38283b771e5d9db191bd26b5a3d
eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
2023-07-06 17:26:18 -0700

    Update var/spack/repos/builtin/packages/py-callmonitor/package.py
    Co-authored-by: Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>

--- var/spack/repos/builtin/packages/py-callmonitor/package.py
+++ var/spack/repos/builtin/packages/py-callmonitor/package.py
@@ -17,4 +17,4 @@ class PyCallmonitor(PythonPackage):
     version("0.3.7", sha256="11bacfe5940c3f6aff223e8e761b033d540542b4d738f7fef38cd923b3be0cbc")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-numpy")
+    depends_on("py-numpy", type=("build", "run"))
