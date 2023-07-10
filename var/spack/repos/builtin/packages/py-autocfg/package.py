75724e6d57201ffa3ad70b296d86699bc885132b
Manuela Kuhn <manuela.kuhn@ovgu.de>
2023-07-10 13:17:22 +0200

    py-gluoncv: switch to PyPI and add 0.10.5.post0

---
+++ var/spack/repos/builtin/packages/py-autocfg/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyAutocfg(PythonPackage):
+    """Deep learning configuration."""
+
+    homepage = "https://github.com/zhreshold/autocfg"
+    pypi = "autocfg/autocfg-0.0.8.tar.gz"
+
+    version("0.0.8", sha256="749986b4f3b3bd85b15298734bf8fa4a590e6c34a314ac515025e058ed76c319")
+
+    depends_on("py-setuptools", type="build")
+    depends_on("py-pyyaml", type=("build", "run"))
