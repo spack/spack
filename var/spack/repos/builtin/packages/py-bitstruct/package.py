66068e27780c531f1aa6712270801d1969761378
Dax Lynch <81718016+DaxLynch@users.noreply.github.com>
2023-07-09 18:19:07 -0700

    Added package py-bitstruct (#38761)
    * Added packages bitstruct, callmonitor, and PYnvtx
    
    * Revert "Added packages bitstruct, callmonitor, and PYnvtx"
    
    This reverts commit 76d25aa76bf4655a85c78725fe5c76c1792717c9.
    
    * py-bitstruct: This module is intended to have a similar interface as the python struct module, but working on bits instead of primitive data types (char, int, …)
    
    * Update package.py
    
    To pass the style prechecks
    
    * PyNVTX: new package
    
    * Delete package.py
    
    Accidentally added this package.
    
    * Update var/spack/repos/builtin/packages/py-bitstruct/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    ---------
    
    Co-authored-by: eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

---
+++ var/spack/repos/builtin/packages/py-bitstruct/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyBitstruct(PythonPackage):
+    """This module is intended to have a similar interface as the python
+    struct module, but working on bits instead of primitive data types
+    (char, int, ...)"""
+
+    homepage = "https://github.com/eerimoq/bitstruct"
+    pypi = "bitstruct/bitstruct-8.17.0.tar.gz"
+
+    maintainers("DaxLynch")
+
+    version("8.17.0", sha256="eb94b40e4218a23aa8f90406b836a9e6ed83e48b8d112ce3f96408463bd1b874")
+
+    depends_on("py-setuptools", type="build")
