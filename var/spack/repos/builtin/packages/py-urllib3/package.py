b8939143665457f0a860bb6b33f9396804bb2dff
Wouter Deconinck <wdconinc@gmail.com>
2023-07-10 12:41:23 -0500

    py-urllib3: new versions 2.*

--- var/spack/repos/builtin/packages/py-urllib3/package.py
+++ var/spack/repos/builtin/packages/py-urllib3/package.py
@@ -13,6 +13,8 @@ class PyUrllib3(PythonPackage):
     homepage = "https://urllib3.readthedocs.io/"
     pypi = "urllib3/urllib3-1.25.6.tar.gz"
 
+    version("2.0.3", sha256="bee28b5e56addb8226c96f7f13ac28cb4c301dd5ea8a6ca179c0b9835e032825")
+    version("1.26.16", sha256="8f135f6502756bde6b2a9b28989df5fbe87c9970cecaa69041edcce7f0589b14", preferred=True)
     version("1.26.12", sha256="3fa96cf423e6987997fc326ae8df396db2a8b7c667747d47ddd8ecba91f4a74e")
     version("1.26.6", sha256="f57b4c16c62fa2760b7e3d97c35b255512fb6b59a259730f36ba32ce9f8e342f")
     version("1.25.9", sha256="3018294ebefce6572a474f0604c2021e33b3fd8006ecd11d62107a5d2a963527")
@@ -26,15 +28,20 @@ class PyUrllib3(PythonPackage):
     variant("socks", default=False, description="SOCKS and HTTP proxy support")
     variant("secure", default=False, description="Add SSL/TLS support")
     variant("brotli", default=False, description="Add Brotli support")
+    variant("zstd", default=False, description="Add Zstandard support", when="@2:")
 
     depends_on("python@2.7:2.8,3.4:", when="@:1.25", type=("build", "run"))
     depends_on("python@2.7:2.8,3.5:", when="@1.26.6", type=("build", "run"))
     depends_on("python@2.7:2.8,3.6:3", when="@1.26.12:", type=("build", "run"))
+    depends_on("python@3.7:3", when="@2:", type=("build", "run"))
 
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools", when="@1", type="build")
+    depends_on("py-hatchling@1.6.0:1", when="@2", type="build")
 
     depends_on("py-pyopenssl@0.14:", when="+secure")
+    depends_on("py-pyopenssl@17.1.0:", when="+secure @2:")
     depends_on("py-cryptography@1.3.4:", when="+secure")
+    depends_on("py-cryptography@1.9:", when="+secure @2:")
     depends_on("py-idna@2:", when="+secure")
     depends_on("py-certifi", when="+secure")
     depends_on("py-urllib3-secure-extra", when="+secure @1.26.12:")
@@ -42,3 +49,6 @@ class PyUrllib3(PythonPackage):
     depends_on("py-pysocks@1.5.6,1.5.8:1", when="+socks")
 
     depends_on("py-brotlipy@0.6:", when="+brotli")
+    depends_on("py-brotlipy@1.0.9:", when"+brotli @2:")
+
+    depends_on("zstd@0.18.0:", when="+zstd @2:")
