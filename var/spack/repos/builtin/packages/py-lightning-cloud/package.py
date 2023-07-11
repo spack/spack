c5d133069518434ff1bf1eddd3c944893856939a
Adam J. Stewart <ajstewart426@gmail.com>
2023-07-10 21:42:09 -0500

    Update dependency packages

--- var/spack/repos/builtin/packages/py-lightning-cloud/package.py
+++ var/spack/repos/builtin/packages/py-lightning-cloud/package.py
@@ -12,6 +12,7 @@ class PyLightningCloud(PythonPackage):
     homepage = "https://lightning.ai/"
     pypi = "lightning_cloud/lightning_cloud-0.5.31.tar.gz"
 
+    version("0.5.37", sha256="1e0d577e1696e3aef974c589c4bd1ed22c70a332750863a393ec3949166137e0")
     version("0.5.36", sha256="990558d93a1b67d8bcbf8a87feb2ac455e13ec5223916ad2d5707d96df9558c9")
     version("0.5.31", sha256="a5a138f4abbeffe66ee476fb9a8d621befac0434ffeeeec1cc00ccd3d72ffc09")
 
