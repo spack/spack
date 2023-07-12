63832c413e20683c048ccc04043c8c35facea6ef
Harmen Stoppels <harmenstoppels@gmail.com>
2023-07-12 22:06:45 +0200

    Undo CI testing

--- var/spack/repos/builtin/packages/py-matplotlib/package.py
+++ var/spack/repos/builtin/packages/py-matplotlib/package.py
@@ -137,7 +137,6 @@ class PyMatplotlib(PythonPackage):
     )
     variant("latex", default=False, description="Enable LaTeX text rendering support")
     variant("fonts", default=False, description="Enable support for system font detection")
-    variant("rebuild", default="yes", description="Test in CI")
 
     # https://matplotlib.org/stable/devel/dependencies.html
     # Runtime dependencies
