3acbc636cfb0645881e26ffacc67555a9da6b74e
Joe <joe@fluidnumerics.com>
2023-07-19 09:39:15 -0400

    Add new feq-parse version

--- var/spack/repos/builtin/packages/feq-parse/package.py
+++ var/spack/repos/builtin/packages/feq-parse/package.py
@@ -12,10 +12,11 @@ class FeqParse(CMakePackage):
     provided as strings."""
 
     homepage = "https://github.com/FluidNumerics/feq-parse"
-    url = "https://github.com/FluidNumerics/feq-parse/archive/v1.0.0.tar.gz"
+    url = "https://github.com/FluidNumerics/feq-parse/archive/v2.0.0.tar.gz"
 
     maintainers("fluidnumerics-joe")
 
+    version("2.0.0", sha256="f2dacad7ec8735abf61ee3e3969f105428de6f4fe85b2ac3849c801b077abe60")
     version("1.1.0", sha256="d33a4fd6904939bb70780e8f25f37c1291c4f24fd207feb4ffc0f8d89637d1e3")
     version("1.0.2", sha256="1cd1db7562908ea16fc65dc5268b654405d0b3d9dcfe11f409949c431b48a3e8")
 
