cfd08f519c4c9edfbbe6d992d5035aea5834e787
Jen Herting <jen@herting.cc>
2023-07-19 08:07:31 -0400

    [py-pydub] new package (#38966)

---
+++ var/spack/repos/builtin/packages/py-pydub/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyPydub(PythonPackage):
+    """Manipulate audio with an simple and easy high level interface"""
+
+    homepage = "http://pydub.com/"
+    pypi = "pydub/pydub-0.25.1.tar.gz"
+
+    version("0.25.1", sha256="980a33ce9949cab2a569606b65674d748ecbca4f0796887fd6f46173a7b0d30f")
+
+    depends_on("py-setuptools", type="build")
+    depends_on("ffmpeg", type="run")
