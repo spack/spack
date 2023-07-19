655d02e7f5099eda0ec4185dc633fd7a6310ead1
Joe <joe@fluidnumerics.com>
2023-07-19 10:00:02 -0400

    Swap 2.0.0 for 2.0.1 - resolves feq-parse build failure

--- var/spack/repos/builtin/packages/feq-parse/package.py
+++ var/spack/repos/builtin/packages/feq-parse/package.py
@@ -12,11 +12,11 @@ class FeqParse(CMakePackage):
     provided as strings."""
 
     homepage = "https://github.com/FluidNumerics/feq-parse"
-    url = "https://github.com/FluidNumerics/feq-parse/archive/v2.0.0.tar.gz"
+    url = "https://github.com/FluidNumerics/feq-parse/archive/v2.0.1.tar.gz"
 
     maintainers("fluidnumerics-joe")
 
-    version("2.0.0", sha256="f2dacad7ec8735abf61ee3e3969f105428de6f4fe85b2ac3849c801b077abe60")
+    version("2.0.1", sha256="08dd08bd100a0a2eb672a5b2792ad56a337df575c634aac0d7a300d7e484b21c")
     version("1.1.0", sha256="d33a4fd6904939bb70780e8f25f37c1291c4f24fd207feb4ffc0f8d89637d1e3")
     version("1.0.2", sha256="1cd1db7562908ea16fc65dc5268b654405d0b3d9dcfe11f409949c431b48a3e8")
 
