b4d1c7ba6c6bffaba92f2a98422cf72069cc047b
Adam J. Stewart <ajstewart426@gmail.com>
2023-07-20 11:50:24 -0500

    py-lightly: add v1.4.13 (#39019)

--- var/spack/repos/builtin/packages/py-lightly/package.py
+++ var/spack/repos/builtin/packages/py-lightly/package.py
@@ -15,6 +15,7 @@ class PyLightly(PythonPackage):
 
     maintainers("adamjstewart")
 
+    version("1.4.13", sha256="d3819d75b85534f98603743486b263eb1dc62cd5b20de30d5bad07227f1e0d98")
     version("1.4.12", sha256="66aea90f096c474b26515bafbef728632516d6f8f13ab5caba38b09903d1b86e")
     version("1.4.11", sha256="3c767a15368c4294a14067a93a4b07e91bd45295313f1e132f28d00270bb69d0")
     version("1.4.10", sha256="77381c913d06f3c02ad7b36a0434bfd7e27a738648f555a51c9912d80dd11125")
