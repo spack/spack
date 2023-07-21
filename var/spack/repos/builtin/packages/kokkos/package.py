576998d5df84636a35017732b7950a5d5d737b22
Bruno Turcksin <bruno.turcksin@gmail.com>
2023-07-21 13:39:40 -0400

    Fix sycl flag
    Co-authored-by: Robert Cohn <rscohn2@gmail.com>

--- var/spack/repos/builtin/packages/kokkos/package.py
+++ var/spack/repos/builtin/packages/kokkos/package.py
@@ -330,7 +330,7 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
                 self.define("CMAKE_CXX_COMPILER", self.spec["kokkos-nvcc-wrapper"].kokkos_cxx)
             )
 
-        if "+sycl" in self.spec:
+        if self.spec.satisfies("%oneapi") or self.spec.satisfies("%intel"):
             options.append(self.define("CMAKE_CXX_FLAGS", "-fp-model=precise"))
 
         return options
