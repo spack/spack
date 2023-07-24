fb239014aaf1e526cc48d743cd1ca7c3dccce412
Harmen Stoppels <me@harmenstoppels.nl>
2023-07-24 12:52:04 +0200

    improve

--- var/spack/repos/builtin/packages/libdeflate/package.py
+++ var/spack/repos/builtin/packages/libdeflate/package.py
@@ -15,6 +15,7 @@ class Libdeflate(MakefilePackage, CMakePackage):
     maintainers("dorton21")
 
     version("1.18", sha256="225d982bcaf553221c76726358d2ea139bb34913180b20823c782cede060affd")
+    version("1.14", sha256="89e7df898c37c3427b0f39aadcf733731321a278771d20fc553f92da8d4808ac")
     version("1.10", sha256="5c1f75c285cd87202226f4de49985dcb75732f527eefba2b3ddd70a8865f2533")
     version("1.7", sha256="a5e6a0a9ab69f40f0f59332106532ca76918977a974e7004977a9498e3f11350")
 
@@ -26,13 +27,11 @@ class Libdeflate(MakefilePackage, CMakePackage):
     with when("build_system=cmake"):
         depends_on("cmake@3.7:", type="build")
 
-    @when("@:1.14")
-    def patch(self):
-        filter_file(r"\/usr\/local", self.prefix, "Makefile")
-
 
 class MakefileBuilder(spack.build_systems.makefile.MakefileBuilder):
-    pass
+    def install(self, pkg, spec, prefix):
+        with working_dir(self.build_directory):
+            make("install", f"PREFIX={prefix}")
 
 
 class CMakeBuilder(spack.build_systems.cmake.CMakeBuilder):
