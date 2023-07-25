4f5343414e088917e4a84e6452925e81ce87e9fa
Marc Schouler <marc.schouler@inria.fr>
2023-07-25 20:42:56 +0200

    Fix version and remove url

--- var/spack/repos/builtin/packages/py-hostlist/package.py
+++ var/spack/repos/builtin/packages/py-hostlist/package.py
@@ -14,11 +14,7 @@ class PyHostlist(PythonPackage):
     git = "https://github.com/LLNL/py-hostlist.git"
 
     version("master", branch="master")
-    version(
-        "1.23",
-        sha256="56e0156b501f792c078114f07324f34f37827041581ee5d1ffdce89cca533219",
-        url="https://www.nsc.liu.se/~kent/python-hostlist/python-hostlist-1.23.0.tar.gz",
-    )
+    version("1.23.0", sha256="56e0156b501f792c078114f07324f34f37827041581ee5d1ffdce89cca533219")
 
     # build dependencies
     depends_on("py-setuptools", type=("build"))
