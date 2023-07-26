c0e04d8e97fa3d738e5cb3cad514d40b742ac357
becker33 <becker33@users.noreply.github.com>
2023-07-26 23:40:48 +0000

    [@spackbot] updating style on behalf of becker33

--- var/spack/repos/builtin/packages/amg2023/package.py
+++ var/spack/repos/builtin/packages/amg2023/package.py
@@ -8,9 +8,9 @@ from spack.package import *
 
 class Amg2023(MakefilePackage):
     """AMG2023 is a parallel algebraic multigrid solver for linear systems
-       arising from problems on unstructured grids. The driver provided here
-       builds linear systems for various 3-dimensional problems. It requires
-       an installation of hypre-2.27.0 or higher.
+    arising from problems on unstructured grids. The driver provided here
+    builds linear systems for various 3-dimensional problems. It requires
+    an installation of hypre-2.27.0 or higher.
     """
 
     tags = ["benchmark"]
@@ -39,38 +39,43 @@ class Amg2023(MakefilePackage):
         return (flags, None, None)
 
     def edit(self, spec, prefix):
-        makefile = FileFilter('Makefile')
+        makefile = FileFilter("Makefile")
         if "+mpi" in spec:
             makefile.filter(r"CC\s*=.*", f"CC = {spec['mpi'].mpicc}")
             makefile.filter(r"CXX\s*=.*", f"CXX = {spec['mpi'].mpicxx}")
-            makefile.filter(r'#MPIPATH = .*', f"MPIPATH = {spec['mpi'].prefix}")
-            makefile.filter(r'#MPIINCLUDE', 'MPIINCLUDE')
+            makefile.filter(r"#MPIPATH = .*", f"MPIPATH = {spec['mpi'].prefix}")
+            makefile.filter(r"#MPIINCLUDE", "MPIINCLUDE")
             if spec["mpi"].extra_attributes and "ldflags" in spec["mpi"].extra_attributes:
-               makefile.filter('#MPILIBS    = .*', 'MPILIBS    = {0}'.format(spec["mpi"].extra_attributes["ldflags"]))
+                makefile.filter(
+                    "#MPILIBS    = .*",
+                    "MPILIBS    = {0}".format(spec["mpi"].extra_attributes["ldflags"]),
+                )
             else:
-               makefile.filter('#MPILIBDIRS', 'MPILIBDIRS')
-               makefile.filter('#MPILIBS', 'MPILIBS')
-            makefile.filter('#MPIFLAGS', 'MPIFLAGS')
+                makefile.filter("#MPILIBDIRS", "MPILIBDIRS")
+                makefile.filter("#MPILIBS", "MPILIBS")
+            makefile.filter("#MPIFLAGS", "MPIFLAGS")
         else:
-            makefile.filter(r"CC\s*=.*", 'CC = {0}'.format(spack_cc))
-            makefile.filter('CXX\s*=.*', 'CXX = {0}'.format(spack_cxx))
+            makefile.filter(r"CC\s*=.*", "CC = {0}".format(spack_cc))
+            makefile.filter("CXX\s*=.*", "CXX = {0}".format(spack_cxx))
 
-        makefile.filter(r'HYPRE_DIR = .*', f'HYPRE_DIR = {spec["hypre"].prefix}')
+        makefile.filter(r"HYPRE_DIR = .*", f'HYPRE_DIR = {spec["hypre"].prefix}')
 
         if spec["hypre"].satisfies("+cuda"):
-            makefile.filter('HYPRE_CUDA_PATH    = .*', 'HYPRE_CUDA_PATH    = %s' % (spec["cuda"].prefix))
-            makefile.filter('HYPRE_CUDA_INCLUDE = #', 'HYPRE_CUDA_INCLUDE = ')
-            makefile.filter('HYPRE_CUDA_LIBS    = #', 'HYPRE_CUDA_LIBS    = ')
-            makefile.filter('HYPRE_HIP_PATH    =', '#HYPRE_HIP_PATH    =')
-            makefile.filter('HYPRE_HIP_INCLUDE =', '#HYPRE_HIP_INCLUDE =')
-            makefile.filter('HYPRE_HIP_LIBS    =', '#HYPRE_HIP_LIBS    =')
+            makefile.filter(
+                "HYPRE_CUDA_PATH    = .*", "HYPRE_CUDA_PATH    = %s" % (spec["cuda"].prefix)
+            )
+            makefile.filter("HYPRE_CUDA_INCLUDE = #", "HYPRE_CUDA_INCLUDE = ")
+            makefile.filter("HYPRE_CUDA_LIBS    = #", "HYPRE_CUDA_LIBS    = ")
+            makefile.filter("HYPRE_HIP_PATH    =", "#HYPRE_HIP_PATH    =")
+            makefile.filter("HYPRE_HIP_INCLUDE =", "#HYPRE_HIP_INCLUDE =")
+            makefile.filter("HYPRE_HIP_LIBS    =", "#HYPRE_HIP_LIBS    =")
 
         if spec["hypre"].satisfies("+rocm"):
-            makefile.filter('HYPRE_HIP_PATH    = .*', 'HYPRE_HIP_PATH    = ${ROCM_PATH}')
+            makefile.filter("HYPRE_HIP_PATH    = .*", "HYPRE_HIP_PATH    = ${ROCM_PATH}")
         else:
-            makefile.filter('HYPRE_HIP_PATH    =', '#HYPRE_HIP_PATH    =')
-            makefile.filter('HYPRE_HIP_INCLUDE =', '#HYPRE_HIP_INCLUDE =')
-            makefile.filter('HYPRE_HIP_LIBS    =', '#HYPRE_HIP_LIBS    =')
+            makefile.filter("HYPRE_HIP_PATH    =", "#HYPRE_HIP_PATH    =")
+            makefile.filter("HYPRE_HIP_INCLUDE =", "#HYPRE_HIP_INCLUDE =")
+            makefile.filter("HYPRE_HIP_LIBS    =", "#HYPRE_HIP_LIBS    =")
 
     def install(self, spec, prefix):
         make()
