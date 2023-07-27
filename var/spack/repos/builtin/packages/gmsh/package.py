e9545d43003ce37d7c2f9ba937b6ed6a4e345cca
Hou Yuewen <77230686+isaachyw@users.noreply.github.com>
2023-07-27 11:35:05 -0400

    add gmsh version 4.11.1

--- var/spack/repos/builtin/packages/gmsh/package.py
+++ var/spack/repos/builtin/packages/gmsh/package.py
@@ -21,6 +21,7 @@ class Gmsh(CMakePackage):
     git = "https://gitlab.onelab.info/gmsh/gmsh.git"
 
     version("master", branch="master")
+    version("4.11.1", sha256="c329b096ecacfad336df7c770d7f26a3b96fa3a87dbe60efbea1e79761cbdcb1")
     version("4.10.3", sha256="a87d59ccea596d493d375b0d6bc380079a5e5a4baebf0d3383018b0cd6bd8e33")
     version("4.8.4", sha256="760dbdc072eaa3c82d066c5ba3b06eacdd3304eb2a97373fe4ada9509f0b6ace")
     version("4.7.1", sha256="c984c295116c757ed165d77149bd5fdd1068cbd7835e9bcd077358b503891c6a")
