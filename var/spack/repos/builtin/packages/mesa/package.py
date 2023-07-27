8908b7584ebd299a038f25cfdbd2478881e5419d
Sreenivasa Murthy Kolam <sreenivasamurthy.kolam@amd.com>
2023-07-27 22:12:17 +0530

    Updates  to rocm recipes for rocm-5.5.0 and rocm-5.5.1 releases (#37910)
    * initial commit for rocm-5.5.0 release
    * fix the hipsparse  build error for 5.5.0
    * fix build error for amrex .add hiprand as a dependency
    * modify the patch for rocprofiler-dev
    * add hiprand for +rocm build
    * initial commit for rocm-5.5.1 release
    * bump up the version for rocm-5.5.1 release.
    * bump up the version for rocmlir.miopen to use this backend only till 5.5
    * add new recipe py-barectf and add it as dependency for rocprofiler-dev
    * revert the changes for rocprofiler-dev for 5.5.0/1 for now as it depends
      on hsa-amdaqlprofile.so which is a closed source and no spack recipe is
      available for now.
    * add rocm-core as dependency for rocm packages from 5.5.0 onwards
    * avoid download of the gtest for building unit tests

--- var/spack/repos/builtin/packages/mesa/package.py
+++ var/spack/repos/builtin/packages/mesa/package.py
@@ -19,13 +19,16 @@ class Mesa(MesonPackage):
     url = "https://archive.mesa3d.org/mesa-20.2.1.tar.xz"
 
     version("main", tag="main")
-    version("22.3.2", sha256="c15df758a8795f53e57f2a228eb4593c22b16dffd9b38f83901f76cd9533140b")
-    version("22.2.5", sha256="850f063146f8ebb262aec04f666c2c1e5623f2a1987dda24e4361b17b912c73b")
+
     version(
-        "22.1.6",
-        sha256="22ced061eb9adab8ea35368246c1995c09723f3f71653cd5050c5cec376e671a",
+        "23.0.3",
+        sha256="386362a5d80df3b096636b67f340e1ce67b705b44767d5bdd11d2ed1037192d5",
         preferred=True,
     )
+    version("23.0.2", sha256="1b7d3399fc6f16f030361f925d33ebc7600cbf98094582f54775b6a1180529e7")
+    version("22.3.2", sha256="c15df758a8795f53e57f2a228eb4593c22b16dffd9b38f83901f76cd9533140b")
+    version("22.2.5", sha256="850f063146f8ebb262aec04f666c2c1e5623f2a1987dda24e4361b17b912c73b")
+    version("22.1.6", sha256="22ced061eb9adab8ea35368246c1995c09723f3f71653cd5050c5cec376e671a")
     version("22.1.2", sha256="0971226b4a6a3d10cfc255736b33e4017e18c14c9db1e53863ac1f8ae0deb9ea")
     version("22.0.5", sha256="5ee2dc06eff19e19b2867f12eb0db0905c9691c07974f6253f2f1443df4c7a35")
     version("22.0.2", sha256="df4fa560dcce6680133067cd15b0505fc424ca703244ce9ab247c74d2fab6885")
@@ -136,14 +139,14 @@ class Mesa(MesonPackage):
     # llvm::Module::setOverrideStackAlignment was added in LLVM 13.0.0, but forks based
     # on development versions of LLVM 13 may or may not have it. Use SFINAE to detect
     # the existence of the function and call it only if it is available.
-    patch("handle_missing_set_override_stack_alignment.patch", when="@21.2.3:")
+    patch("handle_missing_set_override_stack_alignment.patch", when="@21.2.3:22.3")
 
     # ROCm 5.3.0 is providing llvm15. Gallivm coroutine is disabled in mesa upstream version
     # for llvm-15. Until mesa release is available with this changes below patch is required
     # in order to move on with ROCm 5.3.0 and ROCm 5.4.0.
     # The revised patch was part of https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17518/diffs.
 
-    patch("0001-disable-gallivm-coroutine-for-libllvm15.patch", when="@22.1.2: ^libllvm@15:")
+    patch("0001-disable-gallivm-coroutine-for-libllvm15.patch", when="@22.1.2:22.3 ^libllvm@15")
 
     # Explicitly use the llvm-config tool
     def patch(self):
--- var/spack/repos/builtin/packages/rocm-smi-lib/package.py
+++ var/spack/repos/builtin/packages/rocm-smi-lib/package.py
@@ -18,14 +18,15 @@ class RocmSmiLib(CMakePackage):
 
     homepage = "https://github.com/RadeonOpenCompute/rocm_smi_lib"
     git = "https://github.com/RadeonOpenCompute/rocm_smi_lib.git"
-    url = "https://github.com/RadeonOpenCompute/rocm_smi_lib/archive/rocm-5.4.3.tar.gz"
+    url = "https://github.com/RadeonOpenCompute/rocm_smi_lib/archive/rocm-5.5.0.tar.gz"
     tags = ["rocm"]
 
     maintainers("srekolam", "renjithravindrankannath")
     libraries = ["librocm_smi64"]
 
     version("master", branch="master")
-
+    version("5.5.1", sha256="37f32350bfaf6c697312628696d1b1d5fd9165f183882759bc6cb9a5d65b9430")
+    version("5.5.0", sha256="0703f49b1c2924cc1d3f613258eabdff1925cb5bcf7cf22bb6b955dd065e4ce8")
     version("5.4.3", sha256="34d550272e420684230ceb7845aefcef79b155e51cf9ec55e31fdba2a4ed177b")
     version("5.4.0", sha256="4b110c9ec104ec39fc458b1b6f693662ab75395b75ed402b671d8e58c7ae63fe")
     version("5.3.3", sha256="c2c2a377c2e84f0c40297a97b6060dddc49183c2771b833ebe91ed98a98e4119")
@@ -111,6 +112,8 @@ class RocmSmiLib(CMakePackage):
     depends_on("cmake@3:", type="build")
     depends_on("python@3:", type=("build", "run"), when="@3.9.0:")
 
+    for ver in ["5.5.0", "5.5.1"]:
+        depends_on("rocm-core@" + ver, when="@" + ver)
     patch("disable_pdf_generation_with_doxygen_and_latex.patch", when="@4.5.2:")
 
     def cmake_args(self):
