5fc4ecd6a1c5996aeae3e0f61cca7398e754e387
mschouler <schouler.marc@gmail.com>
2023-07-31 19:39:56 +0200

    py-python-hostlist: package addition (#39035)
    * Add recipe for py-hostlist
    
    * Fix style
    
    * Fix style
    
    * Add homepage, fix version url and remove unnecessary dependency
    
    * Fix version and remove url
    
    * Rename package and fix git link
    
    ---------
    
    Co-authored-by: Marc Schouler <marc.schouler@inria.fr>

---
+++ var/spack/repos/builtin/packages/py-python-hostlist/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyPythonHostlist(PythonPackage):
+    """The hostlist.py module knows how to expand and collect hostlist expressions."""
+
+    homepage = "https://www.nsc.liu.se/~kent/python-hostlist/"
+    pypi = "python-hostlist/python-hostlist-1.23.0.tar.gz"
+    git = "git://www.nsc.liu.se/~kent/python-hostlist.git"
+
+    version("master", branch="master")
+    version("1.23.0", sha256="56e0156b501f792c078114f07324f34f37827041581ee5d1ffdce89cca533219")
+
+    # build dependencies
+    depends_on("py-setuptools", type=("build"))
