dd04c6b3af4d89685deb49aab8485bc51b798a75
ChristopherChristofi <christopherlchristofi@outlook.com>
2023-07-31 23:58:05 +0100

    py-igraph: new package with version 0.10.6

---
+++ var/spack/repos/builtin/packages/py-igraph/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyIgraph(PythonPackage):
+    """Python interface to the igraph high performance graph library,
+    primarily aimed at complex network research and analysis."""
+
+    homepage = "https://github.com/igraph/python-igraph"
+    pypi = "igraph/igraph-0.10.6.tar.gz"
+
+    version("0.10.6", sha256="76f7aad294514412f835366a7d9a9c1e7a34c3e6ef0a6c3a1a835234323228e8")
+
+    depends_on('cmake', type='build')
+    depends_on("py-setuptools", type=("build", "run"))
+    depends_on("py-texttable@1.6.2:", type=("build", "run"))
