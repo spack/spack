ede24eff4f17a336efbbe2f5087bf1b75520f239
Sarah El-Jurf <37907745+sbeljurf@users.noreply.github.com>
2023-08-01 16:19:07 -0700

    Update package.py

--- var/spack/repos/builtin/packages/py-trata/package.py
+++ var/spack/repos/builtin/packages/py-trata/package.py
@@ -35,4 +35,3 @@ class PyTrata(PythonPackage):
         depends_on("py-scipy", type=("build", "run"))
         depends_on("py-scikit-learn", type=("build", "run"))
         depends_on("py-matplotlib", type=("build", "run"))
-        
