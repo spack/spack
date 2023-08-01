d7bc1e1f5c31b93d0e75cffa9d130e6494d4dc52
Peter Josef Scheibel <scheibel1@llnl.gov>
2023-08-01 13:50:44 -0700

    forgot to add module

---
+++ lib/spack/spack/debug.py
@@ -0,0 +1,8 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+"""A module to help debug Spack (including package.py methods)"""
+
+_pdb = False
