766df3efaf18073c5f076c50096abc41dd7c3549
Wouter Deconinck <wdconinc@gmail.com>
2023-08-01 09:28:15 -0500

    acts: new version 28.0.0

--- var/spack/repos/builtin/packages/acts/package.py
+++ var/spack/repos/builtin/packages/acts/package.py
@@ -39,6 +39,7 @@ class Acts(CMakePackage, CudaPackage):
     # Supported Acts versions
     version("main", branch="main")
     version("master", branch="main", deprecated=True)  # For compatibility
+    version("28.0.0", commit="0d8aa418c00e8f79bab2cf88234f3433670b447c", submodules=True)
     version("27.1.0", commit="219480220738318fbedb943cac85415687d75b66", submodules=True)
     version("27.0.0", commit="4d7029bd4e9285fcda2770aef6d78a7f833cb14f", submodules=True)
     version("26.0.0", commit="d43af5c3bcf44f593721940e569ba267579e0334", submodules=True)
