5e6623760eabd6f62eb353eea9e1f50b541662b2
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2023-08-02 10:57:52 +0200

    style

--- var/spack/repos/builtin/packages/py-quantities/package.py
+++ var/spack/repos/builtin/packages/py-quantities/package.py
@@ -19,7 +19,6 @@ class PyQuantities(PythonPackage):
     version("0.12.4", sha256="a33d636d1870c9e1127631185d89b0105a49f827d6aacd44ad9d8f151f331d8b")
     version("0.12.3", sha256="582f3c7aeba897846761e966615e01202a5e5d06add304492931b05085d19883")
 
-
     depends_on("python@2.7.0:2.7,3.4:3.7", type=("build", "run"), when="@0.12.3")
     depends_on("python@2.7.0:2.7,3.4:3.8", type=("build", "run"), when="@0.12.4:0.12.5")
     depends_on("python@3.7:3.10", type=("build", "run"), when="@0.13.0:0.13.1")
