a365f104a07a989024baec92d1c0f63cfdab2201
pabloaledo <112545720+pabloaledo@users.noreply.github.com>
2023-08-02 19:22:27 +0200

    bioconductor-dupradar, bioconductor-rsubread: add packages (#39206)
    * bioconductor-dupradar, bioconductor-rsubread: add packages
    * [@spackbot] updating style on behalf of pabloaledo

---
+++ var/spack/repos/builtin/packages/bioconductor-dupradar/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class BioconductorDupradar(RPackage):
+    """Assessment of duplication rates in RNA-Seq datasets"""
+
+    homepage = "https://bioconductor.org/packages/3.16/bioc/html/dupRadar.html"
+    url = "https://bioconductor.org/packages/release/bioc/src/contrib/dupRadar_1.30.0.tar.gz"
+
+    bioc = "dupradar"
+
+    version("1.30.0", sha256="a299d7a4578047dfc19237e34255b0f50f70ce41d29762ef9f5a7741ba35aa3d")
+
+    depends_on("r-kernsmooth")
+    depends_on("subread")
+    depends_on("bioconductor-rsubread")
