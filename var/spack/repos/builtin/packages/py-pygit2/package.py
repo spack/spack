6caa4eb65c9a3fa720f7b8d7057f47b712b83608
Sid Pendelberry <sid@rit.edu>
2023-08-02 09:36:45 -0400

    updated version of py-pygit2

--- var/spack/repos/builtin/packages/py-pygit2/package.py
+++ var/spack/repos/builtin/packages/py-pygit2/package.py
@@ -12,8 +12,10 @@ class PyPygit2(PythonPackage):
     """
 
     homepage = "https://www.pygit2.org/"
-    pypi = "pygit2/pygit2-0.24.1.tar.gz"
+    pypi = "pygit2/pygit2-1.12.2.tar.gz"
 
+    version("1.12.2", sha256="56e85d0e66de957d599d1efb2409d39afeefd8f01009bfda0796b42a4b678358")
+    version("1.11.1", sha256="793f583fd33620f0ac38376db0f57768ef2922b89b459e75b1ac440377eb64ec")
     version("1.6.0", sha256="7aacea4e57011777f4774421228e5d0ddb9a6ddb87ac4b542346d17ab12a4d62")
     version("1.4.0", sha256="cbeb38ab1df9b5d8896548a11e63aae8a064763ab5f1eabe4475e6b8a78ee1c8")
     version("1.3.0", sha256="0be93f6a8d7cbf0cc79ae2f0afb1993fc055fc0018c27e2bd01ba143e51d4452")
@@ -23,17 +25,21 @@ class PyPygit2(PythonPackage):
     depends_on("py-setuptools", type="build")
     # Version must match with libgit2
     # See: https://www.pygit2.org/install.html
-    depends_on("libgit2@1.1.0:1.1", when="@1.4:")
+    depends_on("libgit2@1.6.0:1.6", when="@1.12.0:1.12")
+    depends_on("libgit2@1.5.0:1.5", when="@1.11.0:1.11")
+    depends_on("libgit2@1.1.0:1.1", when="@1.4.0:1.6")
     depends_on("libgit2@1.0.0:1.0", when="@1.2:1.3")
     depends_on("libgit2@0:1.0", when="@1.1.0:1.1")
     depends_on("libgit2@0.28.0:0.28", when="@0.28:1.0")
     depends_on("libgit2@0.24:0.27", when="@0.24:0.27")
-    depends_on("python@3.6:", when="@1.4.0:")
-    depends_on("python@3.6:3.8", when="@1.2:1.3")
-    depends_on("python@3.5:3.8", when="@1.0:1.1")
-    depends_on("python@2.7:3.7", when="@0.28:0")
+    depends_on("python@3.8:", when="@1.11.1")
+    depends_on("python@3.7:", when="@1.4.0:1.6.0")
+    depends_on("python@:3.8", when="@1.2:1.3")
+    depends_on("python@:3.8", when="@1.0:1.1")
+    depends_on("python@3.7", when="@0.28:0")
     depends_on("py-six", type=("build", "run"), when="@:0.28.2")
-    depends_on("py-cffi@1.4.0:", type=("build", "run"))
+    depends_on("py-cffi@1.9.1:", when="@1.11.1:", type=("build", "run"))
+    depends_on("py-cffi@1.4.0:", when="@:1.6.0", type=("build", "run"))
     depends_on("py-cached-property", when="@1.1.0:1.5", type=("build", "run"))
     depends_on("py-cached-property", when="@1.6.0: ^python@:3.7", type=("build", "run"))
 
