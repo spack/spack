fc2ac325117e770542361ddf9d5a80efe42a8510
afzpatel <Afzal.Patel@amd.com>
2023-08-02 12:58:33 +0000

    initial commit to add composable kernel package

--- var/spack/repos/builtin/packages/miopen-hip/package.py
+++ var/spack/repos/builtin/packages/miopen-hip/package.py
@@ -156,6 +156,7 @@ class MiopenHip(CMakePackage):
     for ver in ["5.4.0", "5.4.3", "5.5.0", "5.5.1"]:
         depends_on("rocmlir@" + ver, when="@" + ver)
         depends_on("nlohmann-json", type="link")
+        depends_on("composable-kernel@" + ver, when="@" + ver)
 
     def setup_build_environment(self, env):
         if "@3.9.0:" in self.spec:
@@ -200,10 +201,7 @@ class MiopenHip(CMakePackage):
         if self.spec.satisfies("@5.1.0:5.3"):
             mlir_inc = spec["mlirmiopen"].prefix.include
             args.append(self.define("CMAKE_CXX_FLAGS", "-I{0}".format(mlir_inc)))
-        # TODO: need to turn on composable-kernel to on at a later date
-        # requires a new recipe for composable-kernel
         if self.spec.satisfies("@5.4.0:"):
-            args.append(self.define("MIOPEN_USE_COMPOSABLEKERNEL", "OFF"))
             args.append(
                 "-DNLOHMANN_JSON_INCLUDE={0}".format(self.spec["nlohmann-json"].prefix.include)
             )
