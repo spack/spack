42738037010e266a968c2c6dc436b5d1bfa70ded
Alex Richert <alexander.richert@noaa.gov>
2023-08-02 20:24:06 -0700

    add metplus develop version

--- var/spack/repos/builtin/packages/metplus/package.py
+++ var/spack/repos/builtin/packages/metplus/package.py
@@ -14,9 +14,11 @@ class Metplus(Package):
 
     homepage = "https://dtcenter.org/community-code/metplus"
     url = "https://github.com/dtcenter/METplus/archive/refs/tags/v4.1.0.tar.gz"
+    git = "https://github.com/dtcenter/METplus"
 
     maintainers("AlexanderRichert-NOAA")
 
+    version("develop", branch="develop")
     version("5.0.1", sha256="0e22b4f6791496551d99f68247d382b2af02c90b34c172a64c6f060e774bdced")
     version("5.0.0", sha256="59d519bd062559b4cece9f8672e2e282b200057bc77e2e0937414003d8f2dd50")
     version("4.1.4", sha256="6b8ac395f885807fcbeea07d814bbf97cec343566cb37bee088a3c5b65880ac7")
