4113502c34d1dc8d426575929f957ad9f54337ec
Matthieu Dorier <mdorier@anl.gov>
2023-08-03 12:14:48 -0500

    kafka: new versions (#39249)
    * kafka: version 2.13-3.5.0
    * kafka: version 2.13-3.5.1 and 2.13-3.4.1

--- var/spack/repos/builtin/packages/kafka/package.py
+++ var/spack/repos/builtin/packages/kafka/package.py
@@ -19,6 +19,15 @@ class Kafka(Package):
     list_depth = 1
 
     version(
+        "2.13-3.5.1", sha256="f7b74d544023f2c0ec52a179de59975cb64e34ea03650d829328b407b560e4da"
+    )
+    version(
+        "2.13-3.5.0", sha256="2a3a468e069be570f16d612a8196c2f7a9207b7eac865be934d20ce1252eb2f8"
+    )
+    version(
+        "2.13-3.4.1", sha256="a76f17a52b8f2cd31de11571ff366a714820b6e4e02893b0159d09db0edaf998"
+    )
+    version(
         "2.13-2.4.0", sha256="c1c5246c7075459687b3160b713a001f5cd1cc563b9a3db189868d2f22aa9110"
     )
     version(
