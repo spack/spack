94641a1addce052d92f1f1bf5f7120a5d25d0170
Stephen Sachs <stephenmsachs@gmail.com>
2023-08-03 16:04:38 +0200

    intel compiler breaks when using patchelf@0.18.0
    See https://github.com/spack/spack/issues/39252

--- var/spack/repos/builtin/packages/intel-oneapi-compilers/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-compilers/package.py
@@ -166,6 +166,8 @@ class IntelOneapiCompilers(IntelOneApiPackage):
     homepage = "https://software.intel.com/content/www/us/en/develop/tools/oneapi.html"
 
     depends_on("patchelf", type="build")
+    See https://github.com/spack/spack/issues/39252
+    conflicts("patchelf@0.18.0")
 
     # TODO: effectively gcc is a direct dependency of intel-oneapi-compilers, but we
     # cannot express that properly. For now, add conflicts for non-gcc compilers
