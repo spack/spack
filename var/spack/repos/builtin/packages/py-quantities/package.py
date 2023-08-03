5c0e831d5afe5ee9193f77d54f9d246c6ebb17e5
Moritz Kern <92092328+Moritz-Alexander-Kern@users.noreply.github.com>
2023-08-03 11:41:46 +0200

    Update var/spack/repos/builtin/packages/py-quantities/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-quantities/package.py
+++ var/spack/repos/builtin/packages/py-quantities/package.py
@@ -32,5 +32,5 @@ class PyQuantities(PythonPackage):
 
     depends_on("py-numpy@1.8.2:1.16", type=("build", "run"), when="@0.12.3")
     depends_on("py-numpy@1.8.2:1.17", type=("build", "run"), when="@0.12.4:0.12")
-    depends_on("py-numpy@1.16:", type=("build", "run"), when="@0.13.0:0.13.1")
+    depends_on("py-numpy@1.16:", type=("build", "run"), when="@0.13")
     depends_on("py-numpy@1.19:", type=("build", "run"), when="@0.14:")
