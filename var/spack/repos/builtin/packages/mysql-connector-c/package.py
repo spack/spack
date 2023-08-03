a5b64ce411fbd4ecaea8c02a15117df376fa8a5d
pabloaledo <112545720+pabloaledo@users.noreply.github.com>
2023-08-03 14:04:37 +0200

    ucsc-bedclip: add package (#39209)
    Signed-off-by: Pablo <pablo.aledo@seqera.io>

---
+++ var/spack/repos/builtin/packages/mysql-connector-c/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class MysqlConnectorC(CMakePackage):
+    """MySQL Connector/C, the C interface for communicating with MySQL servers.
+
+    Connector/C is a client library that implements the C API for client/server
+    communication. It is a standalone replacement for the MySQL client library
+    shipped with MySQL Server distributions."""
+
+    homepage = "https://dev.mysql.com/downloads/connector/c/"
+    url = "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-src.tar.gz"
+
+    depends_on("cmake")
+
+    patch("fix-cmake.patch", when="@6.1.11")
+
+    version("6.1.11", sha256="c8664851487200162b38b6f3c8db69850bd4f0e4c5ff5a6d161dbfb5cb76b6c4")
---
+++ var/spack/repos/builtin/packages/ucsc-bedclip/package.py
@@ -0,0 +1,46 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class UcscBedclip(Package):
+    """Remove lines from bed file that refer to off-chromosome locations."""
+
+    homepage = "http://hgdownload.cse.ucsc.edu/admin/exe/"
+    url = "http://hgdownload.cse.ucsc.edu/admin/exe/userApps.archive/userApps.v449.src.tgz"
+
+    version("377", sha256="932f149c19641064a9cd3f2382cbb54b45a9292b8444792872d531346925d676")
+    version("449", sha256="b5a86863d6cfe2120f6c796a13b1572ad05b22622f6534b95c9d26ccbede09b7")
+
+    depends_on("libpng")
+    depends_on("libuuid")
+    depends_on("gmake")
+    depends_on("mysql-connector-c")
+    depends_on("openssl")
+    depends_on("zlib")
+
+    def setup_build_environment(self, env):
+        env.set("MYSQLLIBS", "-lmysqlclient")
+        env.set("L", "-lssl")
+        env.set("BINDIR", "bin")
+
+    def install(self, spec, prefix):
+        with working_dir("kent/src/lib"):
+            make()
+        with working_dir("kent/src/htslib"):
+            make()
+        with working_dir("kent/src/jkOwnLib"):
+            make()
+        with working_dir("kent/src/hg/lib"):
+            make()
+        with working_dir("kent/src/hg/lib"):
+            make()
+        with working_dir("kent/src/utils/bedClip"):
+            mkdirp("bin")
+            mkdirp(prefix.bin)
+            make()
+            make("install")
+            install("bin/bedClip", prefix.bin)
