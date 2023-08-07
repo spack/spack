7ba8ee15b33f74ee8208fbb08282f60d7103c507
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2023-08-07 19:49:31 +0200

    py-pyopengl: build from source and improve variants

--- var/spack/repos/builtin/packages/py-pyopengl/package.py
+++ var/spack/repos/builtin/packages/py-pyopengl/package.py
@@ -12,46 +12,30 @@ class PyPyopengl(PythonPackage):
     """PyOpenGL is the most common cross platform Python binding to OpenGL and related APIs."""
 
     homepage = "http://pyopengl.sourceforge.net"
-    url = "https://files.pythonhosted.org/packages/py3/p/pyopengl/PyOpenGL-3.1.6-py3-none-any.whl"
-    list_url = "https://pypi.org/simple/pyopengl/"
+    pypi = "pyopengl/PyOpenGL-3.1.6.tar.gz"
 
-    version(
-        "3.1.6",
-        sha256="a7139bc3e15d656feae1f7e3ef68c799941ed43fadc78177a23db7e946c20738",
-        expand=False,
-    )
+    version("3.1.6", sha256="8ea6c8773927eda7405bffc6f5bb93be81569a7b05c8cac50cd94e969dce5e27")
 
-    variant(
-        "gl",
-        default="glx" if sys.platform.startswith("linux") else "other",
-        values=("glx", "osmesa", "other"),
-        multi=False,
-        description="The OpenGL provider to use",
-    )
-    variant("glu", default=True)
-    variant("glut", default=True)
+    variant("glu", default=True, description="Enable OpenGL Utility (GLU) binding.")
+    variant("glut", default=True, description="Enable OpenGL Utility Toolkit (GLUT) binding.")
 
-    conflicts("osmesa", when="gl=glx")
-    conflicts("osmesa", when="gl=other")
-    conflicts("glx", when="gl=osmesa")
-    conflicts("glx", when="gl=other")
+    conflicts("osmesa", when="^glx")
+    conflicts("glx", when="^osmesa")
 
-    depends_on("python@3.6:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
     # actually installing PyOpenGL itself just requires python
     # but tests (and possibly dependent packages) need OpenGL libraries
-    depends_on("gl", type=("build", "run"))
-    depends_on("glx", when="gl=glx", type=("build", "run"))
-    depends_on("osmesa", when="gl=osmesa", type=("build", "run"))
-    depends_on("glu", when="+glu", type=("build", "run"))
-    depends_on("freeglut", when="+glut", type=("build", "run"))
+    depends_on("gl", type=("test", "run"))
+    depends_on("glu", when="+glu", type=("test", "run"))
+    depends_on("freeglut+shared", when="+glut", type=("test", "run"))
 
     def setup_build_environment(self, env):
         # PyOpenGL uses ctypes.cdll (or similar), which searches LD_LIBRARY_PATH
         lib_dirs = self.spec["gl"].libs.directories
-        if "gl=glx" in self.spec:
+        if "^glx" in self.spec:
             lib_dirs.extend(self.spec["glx"].libs.directories)
             env.set("PYOPENGL_PLATFORM", "glx")
-        if "gl=osmesa" in self.spec:
+        if "^osmesa" in self.spec:
             lib_dirs.extend(self.spec["osmesa"].libs.directories)
             env.set("PYOPENGL_PLATFORM", "osmesa")
         if "+glu" in self.spec:
