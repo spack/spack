305c4c83a49a2eb551959fdf3630c79abf9f73c5
Jen Herting <jen@herting.cc>
2023-08-07 19:56:53 -0400

    [py-gensim] added version 4.3.1 (#38272)
    * [py-gensim] added version 4.3.1
    
    * [py-gensim] flake8
    
    * [py-gensim] removed version limitation on py-cython

--- var/spack/repos/builtin/packages/py-gensim/package.py
+++ var/spack/repos/builtin/packages/py-gensim/package.py
@@ -16,18 +16,27 @@ class PyGensim(PythonPackage):
 
     maintainers("adamjstewart")
 
+    version("4.3.1", sha256="8b5f11c0e6a5308086b48e8f6841223a4fa1a37d513684612b7ee854b533015f")
     version("3.8.3", sha256="786adb0571f75114e9c5f7a31dd2e6eb39a9791f22c8757621545e2ded3ea367")
     version("3.8.1", sha256="33277fc0a8d7b0c7ce70fcc74bb82ad39f944c009b334856c6e86bf552b1dfdc")
     version("3.8.0", sha256="ec5de7ff2bfa8692fa96a846bb5aae52f267fc322fbbe303c1f042d258af5766")
 
     depends_on("python@2.7:2.8,3.5:", type=("build", "run"))
+    depends_on("python@3.8:", type=("build", "run"), when="@4.3.1:")
     depends_on("py-setuptools", type="build")
 
+    depends_on("py-cython", type=("build", "run"), when="@4.3.1:")
+
     depends_on("py-numpy@1.11.3:", type=("build", "run"))
+    depends_on("py-numpy@1.18.5:", type=("build", "run"), when="@4.3.1:")
 
     depends_on("py-scipy@0.18.1:", type=("build", "run"))
+    depends_on("py-scipy@1.7.0:", type=("build", "run"), when="@4.3.1:")
 
-    depends_on("py-six@1.5.0:", type=("build", "run"))
+    depends_on("py-six@1.5.0:", type=("build", "run"), when="@:3.8.3")
 
     depends_on("py-smart-open@1.7.0:", when="@3.8.0", type=("build", "run"))
     depends_on("py-smart-open@1.8.1:", when="@3.8.1:", type=("build", "run"))
+
+    def setup_build_environment(self, env):
+        env.set("GENSIM_CYTHON_REQUIRES", "Cython=={0}".format(self.spec["py-cython"].version))
