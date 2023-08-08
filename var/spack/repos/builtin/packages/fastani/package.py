5036e33b2a6ff531a08c8199ecabb87efb8b51f3
snehring <7978778+snehring@users.noreply.github.com>
2023-08-08 18:33:56 -0500

    Update py-gtdbtk to 2.3.2 (#39296)
    * fastani: adding version 1.34 and changing build_system
    
    * py-gtdbtk: adding version 2.3.2
    
    * fastani: adding requested changes
    
    * py-gtdbtk: adding requested changes
    
    * fastani: remove conditional

--- var/spack/repos/builtin/packages/fastani/package.py
+++ var/spack/repos/builtin/packages/fastani/package.py
@@ -6,18 +6,23 @@
 from spack.package import *
 
 
-class Fastani(AutotoolsPackage):
+class Fastani(CMakePackage, AutotoolsPackage):
     """Fast, alignment-free computation of whole-genome Average Nucleotide
     Identity (ANI)"""
 
     homepage = "https://github.com/ParBLiSS/FastANI"
     url = "https://github.com/ParBLiSS/FastANI/archive/v1.33.tar.gz"
+    maintainers("snehring")
 
+    version("1.34", sha256="dc185cf29b9fa40cdcc2c83bb48150db46835e49b9b64a3dbff8bc4d0f631cb1")
     version("1.33", sha256="0b18b3074094722fb1b2247c1a1c4eb96295fff369b837f422e05072740e0013")
 
-    depends_on("autoconf", type="build")
+    build_system(conditional("cmake", when="@1.34:"), "autotools", default="cmake")
+
+    depends_on("autoconf", type="build", when="build_system=autotools")
+    depends_on("automake", type="build", when="build_system=autotools")
+    depends_on("cmake@3.20:", type="build", when="build_system=cmake")
+    depends_on("libtool", type="build", when="build_system=autotools")
+    depends_on("m4", type="build", when="build_system=autotools")
     depends_on("gsl", type=("build", "link"))
     depends_on("zlib", type=("build", "link"))
-    depends_on("automake", type="build")
-    depends_on("libtool", type="build")
-    depends_on("m4", type="build")
