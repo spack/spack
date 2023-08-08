879d39fe1d89edaf4213ef8a0c24e14b00b59d83
Cristian Di Pietrantonio <cdipietrantonio@pawsey.org.au>
2023-08-08 14:35:56 +0800

    Changed URL to source code.

--- var/spack/repos/builtin/packages/ncview/package.py
+++ var/spack/repos/builtin/packages/ncview/package.py
@@ -10,7 +10,7 @@ class Ncview(AutotoolsPackage):
     """Simple viewer for NetCDF files."""
 
     homepage = "https://cirrus.ucsd.edu/ncview/"
-    url = "ftp://cirrus.ucsd.edu/pub/ncview/ncview-2.1.7.tar.gz"
+    url = "https://cirrus.ucsd.edu/~pierce/ncview/ncview-2.1.9.tar.gz"
 
     version("2.1.8", sha256="e8badc507b9b774801288d1c2d59eb79ab31b004df4858d0674ed0d87dfc91be")
     version("2.1.7", sha256="a14c2dddac0fc78dad9e4e7e35e2119562589738f4ded55ff6e0eca04d682c82")
