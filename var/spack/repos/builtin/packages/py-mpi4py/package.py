1552dc8fb7c3a3f607f92b346e049b237929d9e9
Cristian Di Pietrantonio <cdipietrantonio@pawsey.org.au>
2023-08-08 15:32:17 +0800

    Changes the way MPICC is set
    in newer pip versions --install-option does not exist, but the proposed
    method always works.

--- var/spack/repos/builtin/packages/py-mpi4py/package.py
+++ var/spack/repos/builtin/packages/py-mpi4py/package.py
@@ -31,8 +31,7 @@ class PyMpi4py(PythonPackage):
     depends_on("python@2.6:2.7,3.2:")
     depends_on("python@2.7:2.8,3.5:", when="@3.1:")
     depends_on("py-setuptools@40.9:", type="build")
-    # in newer pip versions --install-option does not exist
-    depends_on("py-pip@:23.0", type="build")
+    depends_on("py-pip", type="build")
     depends_on("mpi")
     depends_on("py-cython@0.27.0:", type="build")
 
@@ -40,8 +39,8 @@ class PyMpi4py(PythonPackage):
     conflicts("^py-cython@3:")
 
     @when("@3.1:")
-    def install_options(self, spec, prefix):
-        return ["--mpicc=%s -shared" % spec["mpi"].mpicc]
+    def setup_build_environment(self, env):
+        env.set("MPICC", f"{self.spec['mpi'].mpicc} -shared")
 
     @run_before("install")
     def cythonize(self):
