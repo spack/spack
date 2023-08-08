cce25e42d5f94f9f08e952c582a13dea078b7c48
snehring <snehring@iastate.edu>
2023-08-08 17:06:00 -0500

    fastani: adding requested changes

--- var/spack/repos/builtin/packages/fastani/package.py
+++ var/spack/repos/builtin/packages/fastani/package.py
@@ -17,16 +17,12 @@ class Fastani(CMakePackage, AutotoolsPackage):
     version("1.34", sha256="dc185cf29b9fa40cdcc2c83bb48150db46835e49b9b64a3dbff8bc4d0f631cb1")
     version("1.33", sha256="0b18b3074094722fb1b2247c1a1c4eb96295fff369b837f422e05072740e0013")
 
-    build_system(
-        conditional("cmake", when="@1.34:"),
-        conditional("autotools", when="@1.33"),
-        default="cmake",
-    )
+    build_system(conditional("cmake", when="@1.34:"), conditional("autotools"), default="cmake")
 
-    depends_on("autoconf", type="build", when="@1.33")
-    depends_on("automake", type="build", when="@1.33")
-    depends_on("cmake@3.20:", type="build", when="@1.34:")
-    depends_on("libtool", type="build", when="@1.33")
-    depends_on("m4", type="build", when="@1.33")
+    depends_on("autoconf", type="build", when="build_system=autotools")
+    depends_on("automake", type="build", when="build_system=autotools")
+    depends_on("cmake@3.20:", type="build", when="build_system=cmake")
+    depends_on("libtool", type="build", when="build_system=autotools")
+    depends_on("m4", type="build", when="build_system=autotools")
     depends_on("gsl", type=("build", "link"))
     depends_on("zlib", type=("build", "link"))
