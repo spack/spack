6bd66a452d77ad059b359d93be832870e9aaa19c
Sid Pendelberry <sid@rit.edu>
2023-08-09 11:29:31 -0400

    removed python dependency

--- var/spack/repos/builtin/packages/py-pgzip/package.py
+++ var/spack/repos/builtin/packages/py-pgzip/package.py
@@ -15,5 +15,4 @@ class PyPgzip(PythonPackage):
     version("0.3.4", sha256="ef56449039bc6e88558e46fe6bb11e3faaeef445d3985a9fb286795ff842c480")
     version("0.3.1", sha256="a9c2df369311473ec3c239f26bf01638bdc6b6094d89ff4c81c6ef5c84eb24b7")
 
-    depends_on("python", type=("build", "run"))
     depends_on("py-setuptools", type="build")
