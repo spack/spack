002c213b48f53231f76267ff234e798e7cef38df
Cristian Di Pietrantonio <cdipietrantonio@pawsey.org.au>
2023-08-09 09:56:39 +0800

    Fixes style, adds maintainer, sets url.

--- var/spack/repos/builtin/packages/idg/package.py
+++ var/spack/repos/builtin/packages/idg/package.py
@@ -7,21 +7,21 @@ from spack.package import *
 
 class Idg(CMakePackage):
     """
-    Image Domain Gridding (IDG) is a fast method for convolutional resampling (gridding/degridding) 
-    of radio astronomical data (visibilities). Direction-dependent effects (DDEs) 
+    Image Domain Gridding (IDG) is a fast method for convolutional resampling (gridding/degridding)
+    of radio astronomical data (visibilities). Direction-dependent effects (DDEs)
     or A-tems can be applied in the gridding process.
     """
 
     homepage = "https://www.astron.nl/citt/IDG/"
-    git      = "https://git.astron.nl/RD/idg.git"
+    git = "https://git.astron.nl/RD/idg.git"
+    url = "https://git.astron.nl/RD/idg/-/archive/1.2.0/idg-1.2.0.tar.gz"
 
+    maintainers("pelahi")
+
+    version("1.2.0", commit="ccf8951283c12547326800adae99440c70177449")
     version("1.0.0", commit="3322756fb8b6e3bb1fe5293f3e07e40623ff8486")
     version("0.8.1", commit="a09f3c85094c592f9304fff4c31e920c7592c3c3")
 
     depends_on("boost")
     depends_on("fftw-api@3")
     depends_on("blas")
-
-    def url_for_version(self, version):
-        return ("https://git.astron.nl/RD/idg/-/archive/{0}/idg-{0}.tar.gz".format(version))
-
