274ba284dc8b51545c7c83571bfa3318c77d1388
William R Tobin <4522899+wrtobin@users.noreply.github.com>
2023-08-09 11:04:05 -0700

    style conformance

--- var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
@@ -192,7 +192,13 @@ class IntelOneapiMkl(IntelOneApiLibraryPackage):
         if self.spec.satisfies("+cluster"):
             if any(
                 self.spec.satisfies(m)
-                for m in ["^intel-oneapi-mpi", "^intel-mpi", "^mpich", "^cray-mpich", "mpi_family=mpich"]
+                for m in [
+                    "^intel-oneapi-mpi",
+                    "^intel-mpi",
+                    "^mpich",
+                    "^cray-mpich",
+                    "mpi_family=mpich",
+                ]
             ):
                 libs.append(self._xlp64_lib("libmkl_blacs_intelmpi"))
             elif any(self.spec.satisfies(m) for m in ["^openmpi", "mpi_family=openmpi"]):
