98b5ed890c8be4786767dd05c11dbd2a147b01c2
Sven Goldberg <sven.goldberg@dlr.de>
2023-08-11 16:24:46 +0200

    t8code: Add maintainer

--- var/spack/repos/builtin/packages/py-heat/package.py
+++ var/spack/repos/builtin/packages/py-heat/package.py
@@ -14,9 +14,7 @@ class PyHeat(PythonPackage):
     homepage = "https://github.com/helmholtz-analytics/heat/"
     url = "https://github.com/helmholtz-analytics/heat/archive/refs/tags/v1.3.0.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
+    maintainers("mrfh92")
 
     version("1.3.0", sha256="2e66e82df87431075be633bc921a4953dd258dfa91ed31283c964bcb3dea1ffc")
 
