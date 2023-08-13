880b79f2c9267420bb5609e2d84e59da70bea058
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-08-13 10:20:58 -0700

    release 0.0.1 py-dlio-profiler-py (#39377)

---
+++ var/spack/repos/builtin/packages/py-dlio-profiler-py/package.py
@@ -0,0 +1,30 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyDlioProfilerPy(PythonPackage):
+    """A low-level profiler for capture I/O calls from deep learning applications."""
+
+    homepage = "https://github.com/hariharan-devarajan/dlio-profiler.git"
+    git = "https://github.com/hariharan-devarajan/dlio-profiler.git"
+    maintainers("hariharan-devarajan")
+
+    version("develop", branch="dev")
+    version("master", branch="master")
+    version("0.0.1", tag="v0.0.1")
+    depends_on("cpp-logger@0.0.1")
+    depends_on("brahma@0.0.1")
+    depends_on("gotcha@develop")
+    depends_on("py-setuptools@42:", type="build")
+    depends_on("py-pybind11", type=("build", "run"))
+    depends_on("py-ninja", type="build")
+    depends_on("py-cmake@3.12:", type="build")
+
+    def setup_build_environment(self, env):
+        env.set("DLIO_PROFILER_DIR", self.prefix)
+        env.set("DLIO_PYTHON_SITE", python_purelib)
+        env.set("DLIO_BUILD_DEPENDENCIES", "0")
