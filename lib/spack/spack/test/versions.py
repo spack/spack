71db7625f5e54009471ac2f92f9893aaa82ee586
Peter Josef Scheibel <scheibel1@llnl.gov>
2023-08-16 14:10:51 -0700

    test should run on windows, now that I think on it

--- lib/spack/spack/test/versions.py
+++ lib/spack/spack/test/versions.py
@@ -674,7 +674,6 @@ def test_git_ref_comparisons(mock_git_version_info, install_mockery, mock_packag
     assert str(spec_branch.version) == "git.1.x=1.2"
 
 
-@pytest.mark.not_on_windows("Not supported on Windows (yet)")
 def test_git_branch_with_slash():
     class MockLookup(object):
         def get(self, ref):
