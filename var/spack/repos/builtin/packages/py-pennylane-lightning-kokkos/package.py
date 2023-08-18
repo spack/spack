744798f49ef74f83d50b8b4fbade33cfa4f94737
Vincent Michaud-Rioux <vinci1359@gmail.com>
2023-08-18 13:41:22 -0400

    Update var/spack/repos/builtin/packages/py-pennylane-lightning-kokkos/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-pennylane-lightning-kokkos/package.py
+++ var/spack/repos/builtin/packages/py-pennylane-lightning-kokkos/package.py
@@ -73,7 +73,7 @@ class PyPennylaneLightningKokkos(CMakePackage, PythonExtension, CudaPackage, ROC
     depends_on("py-pybind11", type="link")
     depends_on("py-pip", type="build")
     depends_on("py-wheel", type="build")
-    depends_on("py-pennylane@0.30:", type=("build", "run"), when="@:0.31.0")
+    depends_on("py-pennylane@0.30:", type=("build", "run"), when="@0.31:")
     depends_on("py-pennylane@0.28:", type=("build", "run"), when="@:0.30.0")
     depends_on("py-pennylane-lightning@0.31:~kokkos", type=("build", "run"), when="@0.31.0:")
     depends_on("py-pennylane-lightning@0.30:~kokkos", type=("build", "run"), when="@0.30.0:")
