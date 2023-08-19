a38535fa8ee17c6fcafeb7a4ab7665a27d2b569e
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-08-19 04:53:04 -0700

    tau: updated hash

--- var/spack/repos/builtin/packages/tau/package.py
+++ var/spack/repos/builtin/packages/tau/package.py
@@ -26,7 +26,7 @@ class Tau(Package):
     tags = ["e4s"]
 
     version("master", branch="master")
-    version("2.32.1", sha256="5cc4c6765407373237a69c565dce2933f09be6c34cd3ec25608ba98818668a97")
+    version("2.32.1", sha256="0eec3de46b0873846dfc639270c5e30a226b463dd6cb41aa12e975b7563f0eeb")
     version("2.32", sha256="ee774a06e30ce0ef0f053635a52229152c39aba4f4933bed92da55e5e13466f3")
     version("2.31.1", sha256="bf445b9d4fe40a5672a7b175044d2133791c4dfb36a214c1a55a931aebc06b9d")
     version("2.31", sha256="27e73c395dd2a42b91591ce4a76b88b1f67663ef13aa19ef4297c68f45d946c2")
@@ -138,7 +138,6 @@ class Tau(Package):
     conflicts("+sqlite", when="@:2.29.1")
 
     patch("unwind.patch", when="@2.29.0")
-    patch("tau-2.32.1-rocm-smi-dir.patch", when="@2.32.1 +rocm")
 
     filter_compiler_wrappers("Makefile", relative_root="include")
     filter_compiler_wrappers("Makefile.tau*", relative_root="lib")
@@ -146,9 +145,6 @@ class Tau(Package):
 
     def set_compiler_options(self, spec):
         useropt = ["-O2 -g", self.rpath_args]
-
-        if self.spec.satisfies("@2.32.1 +rocm"):
-            useropt.append("-I{0}/include".format(spec["rocm-smi-lib"].prefix))
 
         if self.spec.satisfies("%oneapi"):
             useropt.append("-Wno-error=implicit-function-declaration")
