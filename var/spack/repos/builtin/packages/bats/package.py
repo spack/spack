8a03cc2b9c419b315051c49f50eabd4e63be55d8
Kamil Iskra <iskra@mcs.anl.gov>
2023-08-21 20:34:16 +0000

    [bats] Fix style

--- var/spack/repos/builtin/packages/bats/package.py
+++ var/spack/repos/builtin/packages/bats/package.py
@@ -13,7 +13,11 @@ class Bats(Package):
     url = "https://github.com/bats-core/bats-core/archive/refs/tags/v1.10.0.tar.gz"
 
     version("1.10.0", sha256="a1a9f7875aa4b6a9480ca384d5865f1ccf1b0b1faead6b47aa47d79709a5c5fd")
-    version("0.4.0", sha256="480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7", url="https://github.com/sstephenson/bats/archive/v0.4.0.tar.gz")
+    version(
+        "0.4.0",
+        sha256="480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7",
+        url="https://github.com/sstephenson/bats/archive/v0.4.0.tar.gz",
+    )
 
     def install(self, spec, prefix):
         bash = which("bash")
