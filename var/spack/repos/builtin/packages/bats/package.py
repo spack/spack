97fefedc0139e447e318022cb9c3f23a74a5c1ab
Kamil Iskra <iskra@mcs.anl.gov>
2023-08-21 20:25:35 -0500

    [bats] Update to v1.10.0 (#39561)
    * [bats] Update to v1.10.0
      Original bats has been abandoned for years; nowadays it's
      community-maintained.
    * [bats] Fix style

--- var/spack/repos/builtin/packages/bats/package.py
+++ var/spack/repos/builtin/packages/bats/package.py
@@ -9,11 +9,16 @@ from spack.package import *
 class Bats(Package):
     """Bats is a TAP-compliant testing framework for Bash."""
 
-    homepage = "https://github.com/sstephenson/bats"
-    url = "https://github.com/sstephenson/bats/archive/v0.4.0.tar.gz"
+    homepage = "https://github.com/bats-core/bats-core"
+    url = "https://github.com/bats-core/bats-core/archive/refs/tags/v1.10.0.tar.gz"
 
-    version("0.4.0", sha256="480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7")
+    version("1.10.0", sha256="a1a9f7875aa4b6a9480ca384d5865f1ccf1b0b1faead6b47aa47d79709a5c5fd")
+    version(
+        "0.4.0",
+        sha256="480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7",
+        url="https://github.com/sstephenson/bats/archive/v0.4.0.tar.gz",
+    )
 
     def install(self, spec, prefix):
         bash = which("bash")
-        bash("install.sh", prefix)
+        bash("./install.sh", prefix)
