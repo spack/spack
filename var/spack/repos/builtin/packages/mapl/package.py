469c326551f670d2977358d606d87c83d13f88b0
Matthew Thompson <matthew.thompson@nasa.gov>
2023-08-24 16:47:41 -0400

    style fixes

--- var/spack/repos/builtin/packages/mapl/package.py
+++ var/spack/repos/builtin/packages/mapl/package.py
@@ -78,13 +78,41 @@ class Mapl(CMakePackage):
     version("2.23.1", sha256="563f3e9f33adae298835e7de7a4a29452a2a584d191248c59494c49d3ee80d24")
     version("2.23.0", sha256="ae25ec63d0f288599c668f35fdbccc76abadbfc6d48f95b6eb4e7a2c0c69f241")
     version("2.22.0", sha256="3356b8d29813431d272c5464e265f3fe3ce1ac7f49ae6d41da34fe4b82aa691a")
-    version("2.12.3", sha256="e849eff291939509e74830f393cb2670c2cc96f6160d8060dbeb1742639c7d41", deprecated=True)
-    version("2.11.0", sha256="76351e026c17e2044b89085db639e05ba0e7439a174d14181e01874f0f93db44", deprecated=True)
-    version("2.8.1", sha256="a7657d4c52a66c3a6663e436d2c2dd4dbb81addd747e1ace68f59843665eb739", deprecated=True)
-    version("2.8.0", sha256="6da60a21ab77ecebc80575f25b756c398ef48f635ab0b9c96932a1d4ebd8b4a0", deprecated=True)
-    version("2.7.3", sha256="e8cdc0816471bb4c42673c2fa66d9d749f5a18944cd31580a2d6fd6d961ba163", deprecated=True)
-    version("2.7.2", sha256="8f123352c665c434a18ff87304a71a61fb3342919adcccfea2a40729992d9f93", deprecated=True)
-    version("2.7.1", sha256="8239fdbebd2caa47a232c24927f7a91196704e35c8b7909e1bbbefccf0647ea6", deprecated=True)
+    version(
+        "2.12.3",
+        sha256="e849eff291939509e74830f393cb2670c2cc96f6160d8060dbeb1742639c7d41",
+        deprecated=True,
+    )
+    version(
+        "2.11.0",
+        sha256="76351e026c17e2044b89085db639e05ba0e7439a174d14181e01874f0f93db44",
+        deprecated=True,
+    )
+    version(
+        "2.8.1",
+        sha256="a7657d4c52a66c3a6663e436d2c2dd4dbb81addd747e1ace68f59843665eb739",
+        deprecated=True,
+    )
+    version(
+        "2.8.0",
+        sha256="6da60a21ab77ecebc80575f25b756c398ef48f635ab0b9c96932a1d4ebd8b4a0",
+        deprecated=True,
+    )
+    version(
+        "2.7.3",
+        sha256="e8cdc0816471bb4c42673c2fa66d9d749f5a18944cd31580a2d6fd6d961ba163",
+        deprecated=True,
+    )
+    version(
+        "2.7.2",
+        sha256="8f123352c665c434a18ff87304a71a61fb3342919adcccfea2a40729992d9f93",
+        deprecated=True,
+    )
+    version(
+        "2.7.1",
+        sha256="8239fdbebd2caa47a232c24927f7a91196704e35c8b7909e1bbbefccf0647ea6",
+        deprecated=True,
+    )
 
     # Versions later than 3.14 remove FindESMF.cmake
     # from ESMA_CMake.
@@ -112,7 +140,7 @@ class Mapl(CMakePackage):
         tag="v3.21.0",
         when="@2.22.0:2.33",
     )
-    # NOTE: Remove this resource(), the patch() commands below 
+    # NOTE: Remove this resource(), the patch() commands below
     # and the actual patches when MAPL 2.12 and older are deleted
     resource(
         name="esma_cmake",
@@ -231,7 +259,7 @@ class Mapl(CMakePackage):
 
     def patch(self):
         if "~shared" in self.spec["netcdf-c"]:
-            nc_pc_cmd = ["nc-config","--static","--libs"]
+            nc_pc_cmd = ["nc-config", "--static", "--libs"]
             nc_flags = subprocess.check_output(nc_pc_cmd, encoding="utf8").strip()
             filter_file(
                 "(target_link_libraries[^)]+PUBLIC )", r"\1 %s " % nc_flags, "pfio/CMakeLists.txt"
