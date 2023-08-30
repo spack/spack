40ad7dab7cd73c336a35cd072eb33e630baf2b3e
Raffaele Solcà <rasolca@cscs.ch>
2023-08-30 17:50:25 +0200

    apply suggestion

--- var/spack/repos/builtin/packages/dla-future/package.py
+++ var/spack/repos/builtin/packages/dla-future/package.py
@@ -52,7 +52,7 @@ class DlaFuture(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("umpire+rocm~shared", when="+rocm")
     depends_on("umpire@4.1.0:")
 
-    depends_on("pika@0.15.1:", when="@0.1.0")
+    depends_on("pika@0.15.1:", when="@0.1")
     depends_on("pika@0.16:", when="@0.2.0:")
     depends_on("pika-algorithms@0.1:")
     depends_on("pika +mpi")
