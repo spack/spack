d85d344eacdced0f1e15ec498e8eae0ea07f3674
Guilherme Gibosky <guilherme.gibosky@fast2mine.com>
2023-08-30 20:44:55 -0300

    Added mor dependences

--- var/spack/repos/builtin/packages/dune-common/package.py
+++ var/spack/repos/builtin/packages/dune-common/package.py
@@ -1,27 +1,25 @@
-# copyright 2013-2023 lawrence livermore national security, llc and other
-# spack project developers. see the top-level copyright file for details.
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# spdx-license-identifier: (apache-2.0 or mit)
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
 from spack.package import *
 
 
-class dunecommon(pythonpackage):
-    """dune, the distributed and unified numerics environment
-        is a modular toolbox for solving partial differential
-        equations (pdes) with grid-based methods.
-        it supports the easy implementation of methods
-        like finite elements (fe), finite volumes (fv),
-        and also finite differences (fd).
-    """
+class DuneCommon(PythonPackage):
+    """Dune, the distributed and unified numerics environment."""
 
     homepage = "https://www.dune-project.org/doc/gettingstarted/"
     git = "https://gitlab.dune-project.org/core/dune-common"
     pypi = "dune-common/dune-common-2.9.0.tar.gz"
+    url = "https://files.pythonhosted.org/packages/c4/3e/48a506e9f42e1c3f384f55f578b887bba77916cb51f7ceff9d0bd9f315a8/dune-common-2.9.0.tar.gz"
 
-    version("2.9.0")
+    version("2.9.0", sha256="35e6ede3da10be5c665db4cabb2bf4bd76a2e483255b2fca63c0a9d5ecad064c")
 
+    # FIXME: Add dependencies if required.
+    depends_on("py-setuptools", type=("build", "run"))
     depends_on("cmake@3.13.0:", type="build")
     depends_on("mpi@2:")
     depends_on("pkgconf", type="build")
+    depends_on("gcc@7:", type="build")
