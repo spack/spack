a43bf08195e5204816695a18cdc4d747199ac69e
Adam Grofe <adamgrofe@microsoft.com>
2023-08-30 18:41:51 +0000

    Format scine-qcmaquis recipe using black

--- var/spack/repos/builtin/packages/scine-qcmaquis/package.py
+++ var/spack/repos/builtin/packages/scine-qcmaquis/package.py
@@ -14,22 +14,35 @@ class ScineQcmaquis(CMakePackage):
     homepage = "https://scine.ethz.ch/download/qcmaquis"
     git = "https://github.com/qcscine/qcmaquis.git"
 
-    version('master', branch='master')
-    version('3.1.3', branch='release-3.1.3')
-    version('3.1.2', branch='release-3.1.2')
-    variant('blas', values=('openblas','mkl'), default='openblas', description='Which blas library to use.')
-    variant('symmetries', default='TwoU1;SU2U1;SU2U1PG;TwoU1PG', description='Which wave functions symmetries to compile (e.g. "TwoU1;SU2U1;SU2U1PG;TwoU1PG")')
-    variant('build_tests', default=True, description='Whether to build unit tests using gtest and gmock')
+    version("master", branch="master")
+    version("3.1.3", branch="release-3.1.3")
+    version("3.1.2", branch="release-3.1.2")
+    variant(
+        "blas",
+        values=("openblas", "mkl"),
+        default="openblas",
+        description="Which blas library to use.",
+    )
+    variant(
+        "symmetries",
+        default="TwoU1;SU2U1;SU2U1PG;TwoU1PG",
+        description='Which wave functions symmetries to compile (e.g. "TwoU1;SU2U1;SU2U1PG;TwoU1PG")',
+    )
+    variant(
+        "build_tests",
+        default=True,
+        description="Whether to build unit tests using gtest and gmock",
+    )
 
-    root_cmakelists_dir = 'dmrg'
+    root_cmakelists_dir = "dmrg"
 
-    depends_on('hdf5~mpi')
-    depends_on('lapack')
-    depends_on('openblas+ilp64 threads=openmp', when='blas=openblas')
-    depends_on('intel-oneapi-mkl', when='blas=mkl')
-    depends_on('gsl')
-    depends_on('boost+program_options+filesystem+system+thread+serialization')
-    depends_on('googletest+gmock', when='build_tests=True')
+    depends_on("hdf5~mpi")
+    depends_on("lapack")
+    depends_on("openblas+ilp64 threads=openmp", when="blas=openblas")
+    depends_on("intel-oneapi-mkl", when="blas=mkl")
+    depends_on("gsl")
+    depends_on("boost+program_options+filesystem+system+thread+serialization")
+    depends_on("googletest+gmock", when="build_tests=True")
 
     def cmake_args(self):
         args = []
@@ -38,9 +51,13 @@ class ScineQcmaquis(CMakePackage):
             args.extend(["-DBUILD_SYMMETRIES={}".format(build_symm)])
         build_tests = str(self.spec.variants["build_tests"].value)
         if build_tests:
-           args.extend(["-DQCMAQUIS_TESTS=On"])
+            args.extend(["-DQCMAQUIS_TESTS=On"])
         return args
 
     def patch(self):
         if self.version <= Version("3.1.2"):
-            filter_file("#include <vector>", "#include <vector>\n#include <map>", "dmrg/framework/dmrg/utils/results_collector.h")
+            filter_file(
+                "#include <vector>",
+                "#include <vector>\n#include <map>",
+                "dmrg/framework/dmrg/utils/results_collector.h",
+            )
