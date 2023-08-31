72ceac7ff10ac899e4e44861ef574fd56e2922a9
Guilherme Gibosky <guilherme.gibosky@fast2mine.com>
2023-08-30 21:13:45 -0300

    Black Refactoring

--- var/spack/repos/builtin/packages/dune-common/package.py
+++ var/spack/repos/builtin/packages/dune-common/package.py
@@ -15,10 +15,7 @@ class DuneCommon(PythonPackage):
     pypi = "dune-common/dune-common-2.9.0.tar.gz"
     url = "https://files.pythonhosted.org/packages/c4/3e/48a506e9f42e1c3f384f55f578b887bba77916cb51f7ceff9d0bd9f315a8/dune-common-2.9.0.tar.gz"
 
-    version(
-        "2.9.0",
-        sha256="35e6ede3da10be5c665db4cabb2bf4bd76a2e483255b2fca63c0a9d5ecad064c",
-    )
+    version("2.9.0", sha256="35e6ede3da10be5c665db4cabb2bf4bd76a2e483255b2fca63c0a9d5ecad064c")
 
     depends_on("py-setuptools", type=("build", "run"))
     depends_on("cmake@3.13.0:", type="build")
