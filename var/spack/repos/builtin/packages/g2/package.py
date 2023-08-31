8487a72f88b9381131590bdd98e102118b35a911
Edward Hartnett <edward.hartnett@noaa.gov>
2023-08-31 01:28:33 -0600

    added version 3.4.6

--- var/spack/repos/builtin/packages/g2/package.py
+++ var/spack/repos/builtin/packages/g2/package.py
@@ -19,6 +19,7 @@ class G2(CMakePackage):
 
     maintainers("t-brown", "AlexanderRichert-NOAA", "Hang-Lei-NOAA", "edwardhartnett")
 
+    version("3.4.6", sha256="c4b03946365ce0bacf1e10e8412a5debd72d8671d1696aa4fb3f3adb119175fe")
     version("3.4.5", sha256="c18e991c56964953d778632e2d74da13c4e78da35e8d04cb742a2ca4f52737b6")
     version("3.4.3", sha256="679ea99b225f08b168cbf10f4b29f529b5b011232f298a5442ce037ea84de17c")
 
