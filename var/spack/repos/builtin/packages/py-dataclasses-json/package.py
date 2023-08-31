3597cda59945438bf23fa45ae5f546026c52e35a
Jen Herting <jen@herting.cc>
2023-08-31 13:59:47 -0400

    [py-dataclasses-json] limiting py-poetry-core versions

--- var/spack/repos/builtin/packages/py-dataclasses-json/package.py
+++ var/spack/repos/builtin/packages/py-dataclasses-json/package.py
@@ -15,6 +15,6 @@ class PyDataclassesJson(PythonPackage):
     version("0.5.12", sha256="70e28da52e36f4be6b724e1f1e77fbcd19e0e0a6bf9a4c4c6e5abf713d4dab5a")
 
     depends_on("python@3.7:3.11", type=("build", "run"))
-    depends_on("py-poetry-core", type="build")
+    depends_on("py-poetry-core@1.2:", type="build")
     depends_on("py-typing-inspect@0.4:0", type=("build", "run"))
     depends_on("py-marshmallow@3.18:3", type=("build", "run"))
