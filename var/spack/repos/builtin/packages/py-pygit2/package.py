e766397a17f101fff5dbe08aff278d9f0d78777f
Sid Pendelberry <sid@rit.edu>
2023-08-31 15:31:50 -0400

    removed version 1.12.2 because dependency could not be met

--- var/spack/repos/builtin/packages/py-pygit2/package.py
+++ var/spack/repos/builtin/packages/py-pygit2/package.py
@@ -14,7 +14,7 @@ class PyPygit2(PythonPackage):
     homepage = "https://www.pygit2.org/"
     pypi = "pygit2/pygit2-1.12.2.tar.gz"
 
-    version("1.12.2", sha256="56e85d0e66de957d599d1efb2409d39afeefd8f01009bfda0796b42a4b678358")
+    version("1.12.1", sha256="56e85d0e66de957d599d1efb2409d39afeefd8f01009bfda0796b42a4b678358")
     version("1.11.1", sha256="793f583fd33620f0ac38376db0f57768ef2922b89b459e75b1ac440377eb64ec")
     version("1.6.0", sha256="7aacea4e57011777f4774421228e5d0ddb9a6ddb87ac4b542346d17ab12a4d62")
     version("1.4.0", sha256="cbeb38ab1df9b5d8896548a11e63aae8a064763ab5f1eabe4475e6b8a78ee1c8")
@@ -32,7 +32,6 @@ class PyPygit2(PythonPackage):
     depends_on("libgit2@1.0", when="@1.2:1.3")
     depends_on("libgit2@0.99:1.0", when="@1.1")
     depends_on("libgit2@0.28", when="@0.28:1.0")
-    depends_on("python@3.12:", when="@1.12.2:")
     depends_on("python@3.8:3.11", when="@1.11:1.12.1")
     depends_on("python@:3.10", when="@1.7:1.10")
     depends_on("python@:3.9", when="@1.4:1.6")
