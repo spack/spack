56663148a233d0dde478914c5800df514d0df67c
Adam Grofe <adamgrofe@microsoft.com>
2023-09-01 00:07:07 +0000

    Update Scine-QCMaquis recipe following the review
    PR URL: https://github.com/spack/spack/pull/39709
    
    Changes:
      - Updated Symmetries variant to us multi feature
      - Added dependency to boost+chrono since it was undocumented
      - Use define_from_variant to setup CMake args
      - Make version 3.1.2 be preferred since 3.1.3 build is broken

--- var/spack/repos/builtin/packages/scine-qcmaquis/package.py
+++ var/spack/repos/builtin/packages/scine-qcmaquis/package.py
@@ -16,7 +16,7 @@ class ScineQcmaquis(CMakePackage):
 
     version("master", branch="master")
     version("3.1.3", branch="release-3.1.3")
-    version("3.1.2", branch="release-3.1.2")
+    version("3.1.2", branch="release-3.1.2", preferred=True)
     variant(
         "blas",
         values=("openblas", "mkl"),
@@ -25,8 +25,10 @@ class ScineQcmaquis(CMakePackage):
     )
     variant(
         "symmetries",
-        default="TwoU1;SU2U1;SU2U1PG;TwoU1PG",
-        description='Wave functions symmetries to compile (e.g. "TwoU1;SU2U1;SU2U1PG;TwoU1PG")',
+        default="SU2U1PG,TwoU1PG",
+        description='Wave functions symmetries to compile (e.g. "SU2U1PG,TwoU1PG")',
+        values=("U1", "TwoU1", "TwoU1PG", "NU1", "Z2", "SU2U1", "SU2U1PG", "U1DG", "NONE"),
+        multi=True,
     )
     variant(
         "build_tests",
@@ -41,21 +43,18 @@ class ScineQcmaquis(CMakePackage):
     depends_on("openblas+ilp64 threads=openmp", when="blas=openblas")
     depends_on("intel-oneapi-mkl", when="blas=mkl")
     depends_on("gsl")
-    depends_on("boost+program_options+filesystem+system+thread+serialization")
+    depends_on("boost+program_options+filesystem+system+thread+serialization+chrono")
     depends_on("googletest+gmock", when="build_tests=True")
 
     def cmake_args(self):
-        args = []
-        build_symm = str(self.spec.variants["symmetries"].value)
-        if build_symm:
-            args.extend(["-DBUILD_SYMMETRIES={}".format(build_symm)])
-        build_tests = str(self.spec.variants["build_tests"].value)
-        if build_tests:
-            args.extend(["-DQCMAQUIS_TESTS=On"])
+        args = [
+            self.define_from_variant("BUILD_SYMMETRIES", "symmetries"),
+            self.define_from_variant("QCMAQUIS_TESTS", "build_tests"),
+        ]
         return args
 
     def patch(self):
-        if self.version <= Version("3.1.2"):
+        if self.version <= Version("3.1.3"):
             filter_file(
                 "#include <vector>",
                 "#include <vector>\n#include <map>",
