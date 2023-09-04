4b0d75567491d96b7295956ef7d4ec1b753dd61c
LydDeb <lyderic.de@gmail.com>
2023-09-04 15:35:47 +0200

    py-aniso8601: Add version 3.0.2 required by py-graphene

--- var/spack/repos/builtin/packages/py-aniso8601/package.py
+++ var/spack/repos/builtin/packages/py-aniso8601/package.py
@@ -13,5 +13,6 @@ class PyAniso8601(PythonPackage):
     pypi = "aniso8601/aniso8601-9.0.1.tar.gz"
 
     version("9.0.1", sha256="72e3117667eedf66951bb2d93f4296a56b94b078a8a95905a052611fb3f1b973")
+    version("3.0.2", sha256="7849749cf00ae0680ad2bdfe4419c7a662bef19c03691a19e008c8b9a5267802")
 
     depends_on("py-setuptools", type="build")
