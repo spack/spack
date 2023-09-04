467e71b86eb83b6e9668a5856c3a693c0f0556ad
nanborges <nan_borges@yahoo.com.br>
2023-09-03 23:55:49 -0300

    Update package.py
    - correcting syntax of dependencie os pu-setuptools

--- var/spack/repos/builtin/packages/py-robotframework/package.py
+++ var/spack/repos/builtin/packages/py-robotframework/package.py
@@ -7,7 +7,8 @@ from spack.package import *
 
 
 class PyRobotframework(PythonPackage):
-    """Robot Framework is a generic open source automation framework for acceptance testing, acceptance test driven development (ATDD), and robotic process automation (RPA). It has simple plain text syntax and it can be extended easily with generic and custom libraries.
+    """Robot Framework is a generic open source automation framework for acceptance testing, acceptance test driven development (ATDD), and robotic process automation (RPA). 
+    It has simple plain text syntax and it can be extended easily with generic and custom libraries.
 
     You can learn more about EPICS here: https://robotframework.org/
     """
@@ -17,5 +18,5 @@ class PyRobotframework(PythonPackage):
 
     version("6.1.1", sha256="3fa18f2596a4df2418c4b59abf43248327c15ed38ad8665f6a9a9c75c95d7789")
 
-    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on("py-setuptools", type=("build", "run"))
     depends_on("python@3.6:", type=("build", "run"))
