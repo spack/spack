131606f2e69b333e17f2e73fbe0c072d248df654
Ana Carolina C Pelissari <43182685+anacchp@users.noreply.github.com>
2023-09-04 23:43:12 -0300

    Updated package.py file
    Updated package.py file

--- var/spack/repos/builtin/packages/py-knockknock/package.py
+++ var/spack/repos/builtin/packages/py-knockknock/package.py
@@ -7,17 +7,12 @@ from spack.package import *
 
 
 class PyKnockknock(PythonPackage):
-    
     homepage = "https://pypi.org/project/knockknock"
 
     url = "https://pypi.org/project/knockknock/#files/knockknock-0.1.8.1.tar.gz"
 
-
     version("0.1.8.1", sha256="c7fa2038ddd5a10379b5ef11b775865298c637cbd9a6a175c2c40d8e2b8df773")
 
-
-    depends_on("py-setuptools", type="build")
-    
     depends_on("py-hatchling", type="build")
     depends_on("py-flit-core", type="build")
     depends_on("py-poetry-core", type="build")
