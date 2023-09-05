efcd88bd6280181112f7d5330efb520105532c0d
pabloaledo <112545720+pabloaledo@users.noreply.github.com>
2023-09-05 13:29:55 +0200

    Add maintainers to some bioconductor packages (#39801)
    Signed-off-by: Pablo <pablo.aledo@seqera.io>

--- var/spack/repos/builtin/packages/ucsc-bedclip/package.py
+++ var/spack/repos/builtin/packages/ucsc-bedclip/package.py
@@ -11,6 +11,7 @@ class UcscBedclip(Package):
 
     homepage = "http://hgdownload.cse.ucsc.edu/admin/exe/"
     url = "http://hgdownload.cse.ucsc.edu/admin/exe/userApps.archive/userApps.v449.src.tgz"
+    maintainers("pabloaledo")
 
     version("377", sha256="932f149c19641064a9cd3f2382cbb54b45a9292b8444792872d531346925d676")
     version("449", sha256="b5a86863d6cfe2120f6c796a13b1572ad05b22622f6534b95c9d26ccbede09b7")
