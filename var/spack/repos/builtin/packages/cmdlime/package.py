b233ed233c5c7d5e0a68856b363364bcc98b2b20
Matheus Henrique <matheushmshenrique@gmail.com>
2023-09-06 01:38:03 -0300

    Add cmdlime package (#39835)
    * Add cmdlime package
    * remove the blank line at the end of file

---
+++ var/spack/repos/builtin/packages/cmdlime/package.py
@@ -0,0 +1,16 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Cmdlime(CMakePackage):
+    """cmdlime is a C++17 header-only library for command line parsing that
+    provides a concise, declarative interface without many details to remember."""
+
+    homepage = "https://github.com/kamchatka-volcano/cmdlime"
+    url = "https://github.com/kamchatka-volcano/cmdlime/archive/refs/tags/v2.5.0.tar.gz"
+
+    version("2.5.0", sha256="d5188d7f075142fcb546099a4ee2a967f8248109c0bee8c084e0e00f37603481")
