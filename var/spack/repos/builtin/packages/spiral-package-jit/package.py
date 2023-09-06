f1158327e50cfcd3b01974fb411bed736a3b4e6f
Patrick Broderick <50112491+broderickpt@users.noreply.github.com>
2023-09-06 16:35:33 -0400

    FFTX: replace extends with variants (#39701)
    The spiral-software package had a number of extensions, but does not
    work unless they actually exist in the spiral software prefix (creating
    a view is not sufficient). With the removal of "spack activate"
    (different from "spack env activate"), a new approach is needed to
    support optional components of `spiral-software` . This commit updates
    the spiral-software package to copy the dependency installations into
    its own prefix.
    
    This commit also adds versions for `fftx` and `spiral-software`, as
    well as an optional `spiral-software-jit` package.

---
+++ var/spack/repos/builtin/packages/spiral-package-jit/package.py
@@ -0,0 +1,31 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class SpiralPackageJit(Package):
+    """This is the SPIRAL package for Just-In-Time (JIT) or Real-Time
+    Compilation (RTC)."""
+
+    homepage = "https://spiralgen.com"
+    url = "https://github.com/spiral-software/spiral-package-jit/archive/refs/tags/1.0.2.tar.gz"
+    git = "https://github.com/spiral-software/spiral-package-jit.git"
+
+    maintainers("spiralgen")
+    # Although this package 'extends("spiral-software")' don't declare it as
+    # such.  If this package is required spiral-software should be installed
+    # with the +jit variant active
+
+    version("develop", branch="develop")
+    version("main", branch="main")
+    version("1.0.2", sha256="d7fac0493ac406a8b1874491223c3a9a1c6727ea1aa39de7ef4694c59aac9d26")
+    version("1.0.1", sha256="acf22db04e705276f06642d7f2ebf161f6c347f93bb1bdd6e3ddcfc4b7be5707")
+
+    # JIT package is an extension for Spiral (spec: spiral-software).
+
+    def install(self, spec, prefix):
+        spiral_pkgs = join_path(prefix, "namespaces", "packages", "jit")
+        install_tree(".", spiral_pkgs)
--- var/spack/repos/builtin/packages/spiral-package-mpi/package.py
+++ var/spack/repos/builtin/packages/spiral-package-mpi/package.py
@@ -9,20 +9,21 @@ from spack.package import *
 class SpiralPackageMpi(Package):
     """This is the SPIRAL package for MPI."""
 
-    homepage = "https://spiral.net"
-    url = "https://github.com/spiral-software/spiral-package-mpi/archive/refs/tags/1.0.0.tar.gz"
+    homepage = "https://spiralgen.com"
+    url = "https://github.com/spiral-software/spiral-package-mpi/archive/refs/tags/1.1.0.tar.gz"
     git = "https://github.com/spiral-software/spiral-package-mpi.git"
 
     maintainers("spiralgen")
-    extends("spiral-software")
+    # Although this package 'extends("spiral-software")' don't declare it as
+    # such.  If this package is required spiral-software should be installed
+    # with the +mpi variant active
 
     version("develop", branch="develop")
     version("main", branch="main")
+    version("1.1.0", sha256="baf3c9dac7fee330e4bb4adbd24cc7e55f27fc27417644c0b216124f9052f1f5")
     version("1.0.0", sha256="64896a82aacce9cc8abe88b921e09ba7a5fceb8262e490f60a7088583c2c2151")
 
-    # MPI package is an extension for Spiral (spec: spiral-software).  Spiral finds
-    # extensions in the "namespaces/packages" folder.  Install the tree in a similarly
-    # named folder so that when activated it'll get symlinked to the correct place.
+    # MPI package is an extension for Spiral (spec: spiral-software).
 
     def install(self, spec, prefix):
         spiral_pkgs = join_path(prefix, "namespaces", "packages", "mpi")
