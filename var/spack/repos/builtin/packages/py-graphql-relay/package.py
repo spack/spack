84116fbcaae09c8f94632e77223d8a2bc40d7d2a
Lydéric Debusschère <lyderic.de@gmail.com>
2023-09-07 09:40:13 +0200

    py-graphql-relay: Update package.py
    Remove leftovers from version 3.2.0:
    * archive name in pypi
    * dependencies

--- var/spack/repos/builtin/packages/py-graphql-relay/package.py
+++ var/spack/repos/builtin/packages/py-graphql-relay/package.py
@@ -10,16 +10,14 @@ class PyGraphqlRelay(PythonPackage):
     """Relay library for graphql-core."""
 
     homepage = "https://github.com/graphql-python/graphql-relay-py"
-    pypi = "graphql-relay/graphql-relay-3.2.0.tar.gz"
+    pypi = "graphql-relay/graphql-relay-2.0.1.tar.gz"
 
     maintainers("LydDeb")
 
     version("2.0.1", sha256="870b6b5304123a38a0b215a79eace021acce5a466bf40cd39fa18cb8528afabb")
 
-    depends_on("python@3.6:3.10", type=("build", "run"))
-    depends_on("py-setuptools@59:69", type="build")
-    depends_on("py-poetry-core@1", type="build")
+    depends_on("python@3.4:3.8", type=("build", "run"), when="@2")
+    depends_on("py-setuptools", type="build")
     depends_on("py-graphql-core@2.2:2", type=("build", "run"), when="@2")
     depends_on("py-six@1.12:", type=("build", "run"), when="@2")
     depends_on("py-promise@2.2:2", type=("build", "run"), when="@2")
-    depends_on("py-typing-extensions@4.1:4", type=("build", "run"), when="^python@:3.7")
