a4a5264de18c44a022c61c3104967c1201a0913d
Theresa Pollinger <theresa.pollinger@ipvs.uni-stuttgart.de>
2023-09-07 08:48:36 +0200

    selalib package: follow linter suggestions (?)

--- var/spack/repos/builtin/packages/selalib/package.py
+++ var/spack/repos/builtin/packages/selalib/package.py
@@ -3,7 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
+
 
 class Selalib(CMakePackage):
     """SeLaLib is a modular library for the kinetic and gyrokinetic simulation of tokamak plasmas by the semi-lagrangian or particle-in-cell methods"""
@@ -21,10 +22,13 @@ class Selalib(CMakePackage):
     variant("openmp", default=True)
     variant("compression", default=False)
 
-
-    requires("%gcc@10.0.0:", "%clang@16.0.0:", "%intel@18.0:", "%oneapi@18.0:",
+    requires(
+        "%gcc@10.0.0:", 
+        "%clang@16.0.0:",
+        "%intel@18.0:",
+        "%oneapi@18.0:",
         policy="one_of",
-        msg="SeLaLib requires new-enough Fortran compiler"
+        msg="SeLaLib requires new-enough Fortran compiler",
     )
 
     depends_on("cmake@3.6.0:", type=("build"))
@@ -35,7 +39,8 @@ class Selalib(CMakePackage):
     depends_on("hdf5+fortran+cxx")
     depends_on("mpi+fortran+cxx")
     depends_on("python@3.0.0:")
-    depends_on("zfp+fortran", when="+compression") # beware: compiling w/ zfp may throw type mismatch errors
+    # beware: compiling w/ zfp may throw type mismatch errors
+    depends_on("zfp+fortran", when="+compression")
 
     def cmake_args(self):
         args = [
@@ -43,10 +48,9 @@ class Selalib(CMakePackage):
             self.define_from_variant("HDF5_PARALLEL_ENABLED", "mpi"),
             self.define_from_variant("USE_FMEMPOOL", "fmempool"),
             self.define("FFTW_ENABLED", "ON"),
-            #self.define("CMAKE_Fortran_FLAGS","-ffree-line-length-512")
         ]
         return args
 
     def setup_build_environment(self, env):
-        env.set('FFTW_INCLUDE', self.spec['fftw'].prefix.include)
-        env.set('FFTW_ROOT', self.spec['fftw'].prefix)
+        env.set("FFTW_INCLUDE", self.spec["fftw"].prefix.include)
+        env.set("FFTW_ROOT", self.spec["fftw"].prefix)
