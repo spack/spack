faa25c80f9bb53f09a03bd5ce953ab4dd163493e
Mike Renfro <mike+github@renf.ro>
2023-09-07 08:43:12 -0500

    Remove whitespace for black check

--- var/spack/repos/builtin/packages/py-bx-python/package.py
+++ var/spack/repos/builtin/packages/py-bx-python/package.py
@@ -15,7 +15,7 @@ class PyBxPython(PythonPackage):
 
     version("0.9.0", sha256="fe545c44d2ea74b239d41e9090618aaf6a859d1a1f64b4a21b133cb602dfdb49")
     version("0.8.8", sha256="ad0808ab19c007e8beebadc31827e0d7560ac0e935f1100fb8cc93607400bb47")
-    
+
     # See https://pypi.org/project/bx-python/(version)/#files for which Python versions
     # work with which releases.
     depends_on("python@3.7:3.11", when="@=0.9.0", type=("build", "run"))
