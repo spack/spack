56ab8e3adaa47dee89d6378537867db6d7b430b6
genric <genrich.ivaska@epfl.ch>
2023-09-08 17:43:27 +0200

    py-bbp-workflow: bump (#2111)

--- bluebrain/repo-bluebrain/packages/py-bbp-workflow/package.py
+++ bluebrain/repo-bluebrain/packages/py-bbp-workflow/package.py
@@ -12,7 +12,7 @@ class PyBbpWorkflow(PythonPackage):
     homepage = "https://bbpgitlab.epfl.ch/nse/bbp-workflow"
     git = "ssh://git@bbpgitlab.epfl.ch/nse/bbp-workflow.git"
 
-    version("3.1.32.dev3", tag="bbp-workflow-v3.1.32.dev3")
+    version("3.1.33.dev1", tag="bbp-workflow-v3.1.33.dev1")
 
     depends_on("py-setuptools", type=("build"))
 
