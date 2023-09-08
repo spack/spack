bb7ac8a134cafd23fa9984b68b7c39542959b7b8
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2023-09-08 11:42:54 +0200

    Apply small patch to ascent

--- var/spack/repos/builtin/packages/ascent/package.py
+++ var/spack/repos/builtin/packages/ascent/package.py
@@ -140,6 +140,8 @@ class Ascent(CMakePackage, CudaPackage):
     # patch for finding RAJA more reliably
     # https://github.com/Alpine-DAV/ascent/pull/1123
     patch("ascent-find-raja-pr1123.patch", when="@0.9.0")
+    # patch for avoiding collisions between BLT Targets
+    patch("ascent-do-not-find-camp-if-umpire-found.patch", when="@0.9.2: ^umpire@2022.10.0:")
 
     ##########################################################################
     # package dependencies
