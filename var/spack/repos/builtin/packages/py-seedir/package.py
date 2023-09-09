3e38e67173e464772ec6352d535eaf21ecc3beaf
vboxuserm <vboxuserm@Ubuntu.myguest.virtualbox.org>
2023-09-09 18:08:03 -0300

    fix: removed blank spaces

--- var/spack/repos/builtin/packages/py-seedir/package.py
+++ var/spack/repos/builtin/packages/py-seedir/package.py
@@ -11,12 +11,12 @@ class PySeedir(PythonPackage):
 
     homepage = "https://earnestt1234.github.io/seedir/seedir/index.html"
     pypi = "seedir/seedir-0.4.2.tar.gz"
-    git = "https://github.com/earnestt1234/seedir.git" 
+    git = "https://github.com/earnestt1234/seedir.git"
 
     version("0.4.2", sha256="a549896f37fb31243cf9c8f44405821d9176b2b0c91804f87b7018ea0a738d2b")
     version("0.3.1", sha256="bbf6aef0fa51cd7e6ba8bdaff67a0913bc7199cd6302a92676fbae409112db54")
     version("0.2.0", sha256="49d89cb6074267dc76e7048cc5f2904c5eef8657c206d2086ecdd96ba89fc6d6")
 
-    depends_on("python@3.8:", type=("build", "run")) 
-    depends_on("py-natsort", type=("build", "run")) 
-    depends_on("py-emoji", type=("build", "run")) 
+    depends_on("python@3.8:", type=("build", "run"))
+    depends_on("py-natsort", type=("build", "run"))
+    depends_on("py-emoji", type=("build", "run"))
