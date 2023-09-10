c7c0218524f1f10ed548178371b6179ef37a58ec
borin98 <borinmacedo@gmail.com>
2023-09-09 22:20:48 -0300

    Removing template and returning depends_on("py-setuptools", type="build") on package.py

--- var/spack/repos/builtin/packages/py-pytorch-toolkit-cu-11-8/package.py
+++ var/spack/repos/builtin/packages/py-pytorch-toolkit-cu-11-8/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-pytorch-toolkit-cu-11-8
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-pytorch-toolkit-cu-11-8
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
@@ -32,3 +15,4 @@ class PyPytorchToolkitCu118(PythonPackage):
     version("0.1.6.post20", sha256="a266c8bbc3c883f42888bdd85b6fd21da2a6941fb270db554caad409cca3b89c", expand=False)
 
     depends_on("python@3.8:3.11", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
