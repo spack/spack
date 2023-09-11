0417ef0ec04a20034cb7aa2d8d6edc7b59c8a411
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-09-11 21:56:34 +0200

    py-dunamai: add 1.18.0 (#39849)

--- var/spack/repos/builtin/packages/py-dunamai/package.py
+++ var/spack/repos/builtin/packages/py-dunamai/package.py
@@ -12,6 +12,7 @@ class PyDunamai(PythonPackage):
     homepage = "https://github.com/mtkennerly/dunamai"
     pypi = "dunamai/dunamai-1.13.1.tar.gz"
 
+    version("1.18.0", sha256="5200598561ea5ba956a6174c36e402e92206c6a6aa4a93a6c5cb8003ee1e0997")
     version("1.17.0", sha256="459381b585a1e78e4070f0d38a6afb4d67de2ee95064bf6b0438ec620dde0820")
     version("1.13.1", sha256="49597bdf653bdacdeb51ec6e0f1d4d2327309376fc83e6f1d42af6e29600515f")
 
@@ -19,4 +20,4 @@ class PyDunamai(PythonPackage):
     depends_on("py-poetry-core@1:", type="build")
 
     depends_on("py-packaging@20.9:", type=("build", "run"))
-    depends_on("py-importlib-metadata@1.6:", when="^pyhton@:3.7", type=("build", "run"))
+    depends_on("py-importlib-metadata@1.6:", when="^python@:3.7", type=("build", "run"))
