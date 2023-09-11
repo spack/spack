d284797254902f65ecb94a6094b940a146f4000d
Rocco Meli <r.meli@bluemail.ch>
2023-09-11 23:06:20 +0200

    Update MDAnalysis (#39787)
    * ensure umpire~cuda~rocm when ~cuda~rocm
    
    * update mdanalysis
    
    * Update var/spack/repos/builtin/packages/py-mdanalysis/package.py
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * Update var/spack/repos/builtin/packages/py-mdanalysis/package.py
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-mdanalysistests/package.py
+++ var/spack/repos/builtin/packages/py-mdanalysistests/package.py
@@ -14,16 +14,20 @@ class PyMdanalysistests(PythonPackage):
 
     maintainers("RMeli")
 
+    version("2.6.1", sha256="043f7451f4d9c42ea9e6609a81a6002948e2c74fd268282e0831416789b22e5e")
+    version("2.6.0", sha256="16fdd10e5240b606e8f9210b7cbd9e4be110e6b8d79bb6e72ce6250c4731a817")
     version("2.5.0", sha256="a15b53b7f8bed67900a2bf542bbb3cab81dc71674fa6cddb3248dd11880e4c9d")
     version("2.4.3", sha256="6fbdeccdbfb249f76520ee3605d007cd70292187e3754d0184c71e5afe133abb")
     version("2.4.2", sha256="6e8fb210a4268691c77717ea5157e82d85874a4f7ee0f8f177718451a44ee793")
 
     # Version need to match MDAnalysis'
+    depends_on("py-mdanalysis@2.6.1", when="@2.6.1", type=("build", "run"))
+    depends_on("py-mdanalysis@2.6.0", when="@2.6.0", type=("build", "run"))
     depends_on("py-mdanalysis@2.5.0", when="@2.5.0", type=("build", "run"))
     depends_on("py-mdanalysis@2.4.3", when="@2.4.3", type=("build", "run"))
     depends_on("py-mdanalysis@2.4.2", when="@2.4.2", type=("build", "run"))
 
-    depends_on("python@3.9:", when="@2.5.0", type=("build", "run"))
+    depends_on("python@3.9:", when="@2.5.0:", type=("build", "run"))
     depends_on("python@3.8:", type=("build", "run"))
 
     depends_on("py-pytest@3.3.0:", type=("build", "run"))
