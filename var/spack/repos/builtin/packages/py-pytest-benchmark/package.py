f37b177d4d75abf27be29f587b90adb05039464f
stefano-corda <102236773+stefano-corda@users.noreply.github.com>
2023-09-12 12:44:21 +0000

    add cuda-python pyclibrary packages

--- var/spack/repos/builtin/packages/py-pytest-benchmark/package.py
+++ var/spack/repos/builtin/packages/py-pytest-benchmark/package.py
@@ -14,6 +14,7 @@ class PyPytestBenchmark(PythonPackage):
     pypi = "pytest-benchmark/pytest-benchmark-3.2.3.tar.gz"
 
     version("3.2.3", sha256="ad4314d093a3089701b24c80a05121994c7765ce373478c8f4ba8d23c9ba9528")
+    version("3.4.1", sha256="40e263f912de5a81d891619032983557d62a3d85843f9a9f30b98baea0cd7b47", preferred=True)
 
     depends_on("python@2.7:2.8,3.5:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
