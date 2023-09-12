9f64f984e7f6c1256f9686b4e7dc2cd9af19471f
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2023-09-12 14:43:26 +0100

    py-rseqec: add 5.0.1

--- var/spack/repos/builtin/packages/py-rseqc/package.py
+++ var/spack/repos/builtin/packages/py-rseqc/package.py
@@ -14,6 +14,8 @@ class PyRseqc(PythonPackage):
     homepage = "http://rseqc.sourceforge.net"
     pypi = "RSeQC/RSeQC-2.6.4.tar.gz"
 
+    version("5.0.1", sha256="3c7d458784861af352d8da3f4f1cc8941934b37643164e9b74f929a32bd9ca80")
+    version("4.0.1", sha256="6a16a3c56b73917082d3ac060a113c7d32cccf39f86efa87a6f1e6b52642210d")
     version("3.0.1", sha256="d5f4cb2c24a7348929f5c4947d84c5869e8cd2cba5ba5248d991ebb37c4c6b3d")
 
     depends_on("python@3.5:", type=("build", "run"))
