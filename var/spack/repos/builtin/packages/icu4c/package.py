d8786a60b3f2e792c9be84491964d03827ae9346
Patrick Gartung <gartung@fnal.gov>
2023-09-12 21:32:58 -0500

    Add cxxstd=20 variant to icu4c

--- var/spack/repos/builtin/packages/icu4c/package.py
+++ var/spack/repos/builtin/packages/icu4c/package.py
@@ -28,7 +28,7 @@ class Icu4c(AutotoolsPackage):
     variant(
         "cxxstd",
         default="11",
-        values=("11", "14", "17"),
+        values=("11", "14", "17", "20"),
         multi=False,
         description="Use the specified C++ standard when building",
     )
