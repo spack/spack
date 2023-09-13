aeb6ca59f95fc9b804147086098f508e14843114
Alex Richert <82525672+AlexanderRichert-NOAA@users.noreply.github.com>
2023-09-13 18:35:29 -0400

    Add two versions to ncio recipe (#39999)

--- var/spack/repos/builtin/packages/ncio/package.py
+++ var/spack/repos/builtin/packages/ncio/package.py
@@ -14,10 +14,13 @@ class Ncio(CMakePackage):
 
     homepage = "https://github.com/NOAA-EMC/NCEPLIBS-ncio"
     url = "https://github.com/NOAA-EMC/NCEPLIBS-ncio/archive/refs/tags/v1.0.0.tar.gz"
+    git = "https://github.com/NOAA-EMC/NCEPLIBS-ncio"
 
     maintainers("edwardhartnett", "AlexanderRichert-NOAA", "Hang-Lei-NOAA")
 
+    version("develop", branch="develop")
     version("1.1.2", sha256="2e4506fe3176344f28e837f2f69bcbd3dda51a64cacf33af0e435b13abe094fc")
+    version("1.1.1", sha256="c1cfeb3d731a2ae858bdfc02b2914fd6c8eac5cc76c7e67eb2580ae5e7500a2b")
     version("1.1.0", sha256="9de05cf3b8b1291010197737666cede3d621605806379b528d2146c4f02d08f6")
     version("1.0.0", sha256="2e2630b26513bf7b0665619c6c3475fe171a9d8b930e9242f5546ddf54749bd4")
 
