ff4a0c2d7bf507ef87d855f29c47e64448f554ac
David J. Gardner <gardner48@llnl.gov>
2023-09-17 14:13:29 -0700

    add new suite-sparse versions

--- var/spack/repos/builtin/packages/suite-sparse/package.py
+++ var/spack/repos/builtin/packages/suite-sparse/package.py
@@ -12,9 +12,17 @@ class SuiteSparse(Package):
     """
 
     homepage = "https://people.engr.tamu.edu/davis/suitesparse.html"
-    url = "https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v4.5.3.tar.gz"
+    url = "https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/refs/tags/v4.5.3.tar.gz"
     git = "https://github.com/DrTimothyAldenDavis/SuiteSparse.git"
 
+    version("7.2.0", sha256="ce63c34f566d0aeae3c85fdc5b72d293f7e834d10ec0a0417b4c0823ce6c0474")
+    version("7.1.0", sha256="4cd3d161f9aa4f98ec5fa725ee5dc27bca960a3714a707a7d12b3d0abb504679")
+    version("7.0.1", sha256="dc2f8d5c2657c120b30cce942f634ec08fc3a4b0b10e19d3eef7790b2bec8d1e")
+    version("7.0.0", sha256="66e0a21a4c58f4fa7a96823c6726226ebd5f605263a37e1afa61f143cf6b464d")
+    version("6.0.3", sha256="7111b505c1207f6f4bd0be9740d0b2897e1146b845d73787df07901b4f5c1fb7")
+    version("6.0.2", sha256="c5d960cd210279c3c83a27747aca2fdeb2e4a13af42870ca0635739accdc6847")
+    version("6.0.1", sha256="06f55a0449775d1f43d8a33ba8c8417ea8cf76c27833b465c49825baa8741d0c")
+    version("6.0.0", sha256="3b07fc5cec46fa66f18f0fbd6a81ad5d552533020bb3595f27c24a0274c89b7a")
     version("5.13.0", sha256="59c6ca2959623f0c69226cf9afb9a018d12a37fab3a8869db5f6d7f83b6b147d")
     version("5.12.0", sha256="5fb0064a3398111976f30c5908a8c0b40df44c6dd8f0cc4bfa7b9e45d8c647de")
     version("5.11.0", sha256="fdd957ed06019465f7de73ce931afaf5d40e96e14ae57d91f60868b8c123c4c8")
