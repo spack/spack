da30fe072d97e077e4f30bce310688a5e21b2a05
Lydéric Debusschère <lyderic.de@gmail.com>
2023-09-18 17:46:30 +0200

    [chore] py-aniso8601: Add version 7.0.0 (#39808)
    * [chore] py-aniso8601: Add version 3.0.2 required by py-graphene
    
    * py-aniso8601: Add version 7.0.0 and constraints on python version
    
    * py-aniso8601: fix style
    
    * [fix] py-aniso8601: remove version 3.0.2 which depends on removed version of python; set the minimal version of python to 3.7
    
    * py-aniso8601: remove version constraint on python

--- var/spack/repos/builtin/packages/py-aniso8601/package.py
+++ var/spack/repos/builtin/packages/py-aniso8601/package.py
@@ -13,5 +13,6 @@ class PyAniso8601(PythonPackage):
     pypi = "aniso8601/aniso8601-9.0.1.tar.gz"
 
     version("9.0.1", sha256="72e3117667eedf66951bb2d93f4296a56b94b078a8a95905a052611fb3f1b973")
+    version("7.0.0", sha256="513d2b6637b7853806ae79ffaca6f3e8754bdd547048f5ccc1420aec4b714f1e")
 
     depends_on("py-setuptools", type="build")
