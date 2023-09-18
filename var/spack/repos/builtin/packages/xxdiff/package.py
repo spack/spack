97be3ef498a476bc58fe20ee9ced1569d06db6a7
Brian Vanderwende <vanderwb@ucar.edu>
2023-09-18 13:40:22 -0600

    xxdiff: add new package  (#40021)
    * Add new package xxdiff
    
    * style fixes
    
    * Versioning updates to match best practices
    
    * Full hash and flex, bison as build deps

---
+++ var/spack/repos/builtin/packages/xxdiff/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Xxdiff(MakefilePackage):
+    """Graphical File And Directories Comparator And Merge Tool."""
+
+    homepage = "https://furius.ca/xxdiff/"
+    git = "https://github.com/blais/xxdiff.git"
+
+    maintainers("vanderwb")
+
+    version("master", branch="master")
+    version("2023-01-10", commit="604300ea9875611726ba885fb14f872b964df579")
+
+    depends_on("flex@2.5.31:", type="build")
+    depends_on("bison", type="build")
+    depends_on("qt@5:", type=("build", "link", "run"))
+
+    def edit(self, spec, prefix):
+        env["QMAKE"] = "qmake"
+
+    def build(self, spec, prefix):
+        with working_dir("src"):
+            # Create the makefile
+            make("-f", "Makefile.bootstrap")
+            make()
+
+    def install(self, spec, prefix):
+        mkdir(prefix.bin)
+        install("bin/xxdiff", prefix.bin)
