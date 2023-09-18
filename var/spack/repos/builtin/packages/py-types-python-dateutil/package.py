d9b448113187cb161f9ec1b3e799af74e7f0b8e3
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-09-18 18:34:53 +0200

    py-types-python-dateutil: add 2.8.19.14 (#40038)

--- var/spack/repos/builtin/packages/py-types-python-dateutil/package.py
+++ var/spack/repos/builtin/packages/py-types-python-dateutil/package.py
@@ -12,7 +12,7 @@ class PyTypesPythonDateutil(PythonPackage):
     homepage = "https://github.com/python/typeshed"
     pypi = "types-python-dateutil/types-python-dateutil-2.8.19.tar.gz"
 
+    version("2.8.19.14", sha256="1f4f10ac98bb8b16ade9dbee3518d9ace017821d94b057a425b069f834737f4b")
     version("2.8.19", sha256="bfd3eb39c7253aea4ba23b10f69b017d30b013662bb4be4ab48b20bbd763f309")
 
-    depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
