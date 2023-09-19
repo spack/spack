a35e20a30af34e3d19e8a777855b18089a3b9202
James Taliaferro <taliaferro1@llnl.gov>
2023-09-19 10:14:14 -0700

    reorder to group variants and dependencies together

--- var/spack/repos/builtin/packages/libsixel/package.py
+++ var/spack/repos/builtin/packages/libsixel/package.py
@@ -19,19 +19,15 @@ class Libsixel(AutotoolsPackage):
     version("1.8.6", sha256="37611d60c7dbcee701346967336dbf135fdd5041024d5f650d52fae14c731ab9")
 
     variant("curl", default=False, description="build with libcurl")
-    depends_on("curl", when="+curl")
-
     variant("gd", default=False, description="build with libgd")
-    depends_on("libgd", when="+gd")
-
-
     variant("gdk-pixbuf2", default=False, description="build with gdk-pixbuf2")
-    depends_on("gdk-pixbuf", when="+gdk-pixbuf2")
-
     variant("jpeg", default=True, description="build with libjpeg")
-    depends_on("libjpeg", when="+jpeg")
-
     variant("png", default=True, description="build with libpng")
+
+    depends_on("curl", when="+curl")
+    depends_on("libgd", when="+gd")
+    depends_on("gdk-pixbuf", when="+gdk-pixbuf2")
+    depends_on("libjpeg", when="+jpeg")
     depends_on("libpng", when="+png")
 
     # there's one more flag for adding the Python interface to libsixel
@@ -40,13 +36,13 @@ class Libsixel(AutotoolsPackage):
 
     def configure_args(self):
         args_map = {
-          "--with-libcurl": "+curl",
-          "--with-gd": "+gd",
-          "--with-gdk-pixbuf2": "+gdk-pixbuf2",
-          "--with-png": "+png",
-          "--with-jpeg": "+jpeg"
+            "--with-libcurl": "+curl",
+            "--with-gd": "+gd",
+            "--with-gdk-pixbuf2": "+gdk-pixbuf2",
+            "--with-png": "+png",
+            "--with-jpeg": "+jpeg",
         }
-        args=[]
+        args = []
         for flag, variant in args_map.items():
             flag_value = "yes" if variant in self.spec else "no"
             args.append("{}={}".format(flag, flag_value))
