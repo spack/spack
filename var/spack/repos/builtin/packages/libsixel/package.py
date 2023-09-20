884982dc57ca443f49538fe46f254fa41c6ecd17
James Taliaferro <taliaferro1@llnl.gov>
2023-09-20 14:27:30 -0700

    Switch to using source from fork
    The original developer of libsixel, Hayaki Saito (@saitoha), disappeared
    in early 2020 and has not updated the repository or been seen since.
    However, a fork of the project has been created at libsixel/libsixel,
    and that fork has been getting much more regular updates. In this commit
    I switch the package to using the better-maintained, now apparently
    canonical fork of the project.
    
    That project switched the build system from autotools to Meson, so much
    of the build arguments code needed to be rewritten. The newest official
    release also contained an error in meson.build which would break on
    newer versions of Meson. That error only applied to the libjpeg and
    libpng variants, though, so I switched the default to use libgd which
    has broader format support anyway.

--- var/spack/repos/builtin/packages/libsixel/package.py
+++ var/spack/repos/builtin/packages/libsixel/package.py
@@ -6,44 +6,45 @@
 from spack.package import *
 
 
-class Libsixel(AutotoolsPackage):
+class Libsixel(MesonPackage):
     """
     This package provides encoder/decoder implementation for DEC SIXEL graphics, and some converter programs.
     """
 
-    homepage = "https://github.com/saitoha/libsixel"
-    url = "https://github.com/saitoha/libsixel/archive/refs/tags/v1.8.6.tar.gz"
+    homepage = "https://github.com/libsixel/libsixel"
+    url = "https://github.com/libsixel/libsixel/archive/refs/tags/v1.10.3.tar.gz"
 
     maintainers("taliaferro")
 
-    version("1.8.6", sha256="37611d60c7dbcee701346967336dbf135fdd5041024d5f650d52fae14c731ab9")
+    version("1.10.3", sha256="028552eb8f2a37c6effda88ee5e8f6d87b5d9601182ddec784a9728865f821e0")
 
-    variant("curl", default=False, description="build with libcurl")
-    variant("gd", default=False, description="build with libgd")
+    variant("img2sixel", default=True, description="build binary img2sixel")
+    variant("sixel2png", default=True, description="build binary sixel2png")
+    variant("gd", default=True, description="build with libgd")
+    variant("jpeg", default=False, description="build with libjpeg")
+    variant("png", default=False, description="build with libpng")
+    variant("libcurl", default=False, description="build with libcurl")
     variant("gdk-pixbuf2", default=False, description="build with gdk-pixbuf2")
-    variant("jpeg", default=True, description="build with libjpeg")
-    variant("png", default=True, description="build with libpng")
 
-    depends_on("curl", when="+curl")
+
+    depends_on("curl", when="+libcurl")
     depends_on("libgd", when="+gd")
     depends_on("gdk-pixbuf", when="+gdk-pixbuf2")
     depends_on("libjpeg", when="+jpeg")
     depends_on("libpng", when="+png")
 
-    # there's one more flag for adding the Python interface to libsixel
-    # must run configure with --with-python
-    # but arguably should that be a separate py-libsixel?
-
-    def configure_args(self):
-        args_map = {
-            "--with-libcurl": "+curl",
-            "--with-gd": "+gd",
-            "--with-gdk-pixbuf2": "+gdk-pixbuf2",
-            "--with-png": "+png",
-            "--with-jpeg": "+jpeg",
-        }
-        args = []
-        for flag, variant in args_map.items():
-            flag_value = "yes" if variant in self.spec else "no"
-            args.append("{}={}".format(flag, flag_value))
+    def meson_args(self):
+        options = [
+            "img2sixel",
+            "sixel2png",
+            "gd",
+            "jpeg",
+            "png",
+            "libcurl",
+            "gdk-pixbuf2"
+        ]
+        args=[]
+        for option in options:
+            state="enabled" if "+{}".format(option) in self.spec else "disabled"
+            args.append("-D{option}={state}".format(option=option, state=state))
         return args
