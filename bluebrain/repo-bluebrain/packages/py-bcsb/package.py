f35cb173a79d612eb49e01f1f178ff0dc323ab5c
Adrien4193 <39053578+Adrien4193@users.noreply.github.com>
2023-09-21 10:45:42 +0200

    py-bcsb: create package and deploy. (#2110)

---
+++ bluebrain/repo-bluebrain/packages/py-bcsb/package.py
@@ -0,0 +1,24 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyBcsb(PythonPackage):
+    """Backend service for Brayns Circuit Studio."""
+
+    homepage = "https://bbpgitlab.epfl.ch/viz/brayns/braynscircuitstudiobackend"
+    git = "ssh://git@bbpgitlab.epfl.ch/viz/brayns/braynscircuitstudiobackend.git"
+
+    version("develop", branch="develop")
+    version("2.0.0", tag="v2.0.0")
+
+    depends_on("python@3.10:", type=("build", "run"))
+    depends_on("py-setuptools", type=("build", "run"))
+
+    depends_on("py-websockets@10.3:", type=("run"))
+    depends_on("py-libsonata@0.1.23:", type=("run"))
+    depends_on("py-psutil@5.9.5:", type=("run"))
+    depends_on("py-bluepy@2.5.1", type=("run"))
