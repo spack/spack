7bf574da8d67573b8717b1d8fe3c485bde896221
Thomas 'elfprince13' Dickerson <thomas@geopi.pe>
2023-09-21 16:52:17 -0400

    Lints from spack style

--- var/spack/repos/builtin/packages/rkt-racket-lib/package.py
+++ var/spack/repos/builtin/packages/rkt-racket-lib/package.py
@@ -9,12 +9,11 @@ from spack.package import *
 class RktRacketLib(RacketPackage):
     """Stub package for packages which are currently part of core racket installation (but which may change in the future)."""
 
-    git      = "ssh://git@github.com/racket/racket.git"
-    
+    git = "ssh://git@github.com/racket/racket.git"
 
-    maintainers = ['elfprince13']
+    maintainers = ["elfprince13"]
 
-    version('8.3', commit='cab83438422bfea0e4bd74bc3e8305e6517cf25f') #tag='v8.3'
-    depends_on('racket@8.3', type=('build', 'run'), when='@8.3')
+    version("8.3", commit="cab83438422bfea0e4bd74bc3e8305e6517cf25f") #tag="v8.3"
+    depends_on("racket@8.3", type=("build", "run"), when="@8.3")
 
-    racket_name = 'racket-lib'
+    racket_name = "racket-lib"
