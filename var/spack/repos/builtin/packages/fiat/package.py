4a8a5588a4e164ab28c13c39b1a8ad8b63ce65ac
eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
2023-09-22 06:37:18 -0700

    Update var/spack/repos/builtin/packages/fiat/package.py
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/fiat/package.py
+++ var/spack/repos/builtin/packages/fiat/package.py
@@ -31,9 +31,6 @@ class Fiat(CMakePackage):
     variant("mpi", default=True, description="Use MPI?")
     variant("openmp", default=True, description="Use OpenMP?")
     variant("fckit", default=True, description="Use fckit?")
-    # variant("dr_hook_multi_precision_handles", default=False,
-    #    description="Use deprecated single precision handles for DR_HOOK?")
-    # variant("warnings", default=True, description="Enable compiler warnings")
 
     depends_on("ecbuild", type=("build"))
     depends_on("mpi", when="+mpi")
