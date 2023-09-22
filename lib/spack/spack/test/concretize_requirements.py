d0ef6152b3b007b41d3d19cdc41bce7cf74d8eb1
Peter Scheibel <scheibel1@llnl.gov>
2023-09-21 17:28:33 -0700

    cleanup

--- lib/spack/spack/test/concretize_requirements.py
+++ lib/spack/spack/test/concretize_requirements.py
@@ -511,8 +511,8 @@ packages:
     spec_y = Spec("y").concretized()
     assert spec_y.satisfies("cflags=-g")
 
-    asp = solve("--show=asp", "w")
-    import pdb; pdb.set_trace()
+    # asp = solve("--show=asp", "w")
+    # import pdb; pdb.set_trace()
 
     spec_w = Spec("w").concretized()
     assert spec_w.satisfies("cflags=-O1")
