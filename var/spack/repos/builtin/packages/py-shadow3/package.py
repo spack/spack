19a6bf40470d77a688feef47c2bd1d93d548da3c
Lucca Bavia <luccabavia@gmail.com>
2023-09-23 11:23:11 -0300

    Change formatting

--- var/spack/repos/builtin/packages/py-shadow3/package.py
+++ var/spack/repos/builtin/packages/py-shadow3/package.py
@@ -7,28 +7,17 @@ from spack.package import *
 
 
 class PyShadow3(PythonPackage):
-    """
-    SHADOW is an open source ray tracing code for modeling optical systems. 
+    """SHADOW is an open source ray tracing code for modeling optical systems. 
     Targeted to synchrotron radiation beamlines, it has unique features for 
     designing X-ray optical systems.
     """
 
     homepage = "https://github.com/oasys-kit/shadow3"
-    url = (
-        "https://files.pythonhosted.org/packages/11/29/"
-        "c330f8ed5fa1d77927105bb6884a928d74ac9ec89e03181f3084a6cdb361/"
-        "shadow3-23.9.19-cp310-cp310-manylinux_2_17_x86_64."
-        "manylinux2014_x86_64.whl"
-    )
+    url = "https://files.pythonhosted.org/packages/11/29/c330f8ed5fa1d77927105bb6884a928d74ac9ec89e03181f3084a6cdb361/shadow3-23.9.19-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl"
 
     maintainers("luccabavia")
 
-    version(
-        "23.9.19", 
-        sha256=(
-            "d06920433552526398fd1373df03264a65dcdf020c0a67e196ac6f78f8673867"
-        ), 
-        expand=False
-    )
+    version("23.9.19", sha256=(
+        "d06920433552526398fd1373df03264a65dcdf020c0a67e196ac6f78f8673867"), expand=False)
 
     depends_on("py-setuptools", type="build")
