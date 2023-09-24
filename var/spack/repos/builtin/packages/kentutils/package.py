2f0dc2cb9d03dc4bf259fb45af7bbfb9b0d88ad1
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2023-09-24 18:33:01 +0100

    kentutils update

--- var/spack/repos/builtin/packages/kentutils/package.py
+++ var/spack/repos/builtin/packages/kentutils/package.py
@@ -9,20 +9,25 @@ from spack.package import *
 class Kentutils(MakefilePackage):
     """Jim Kent command line bioinformatic utilities"""
 
-    homepage = "https://github.com/ucscGenomeBrowser/kent-core"
-    url = "https://github.com/ucscGenomeBrowser/kent-core/archive/refs/tags/v449.tar.gz"
+    homepage = "https://genome.cse.ucsc.edu/"
+    url = "https://hgdownload.cse.ucsc.edu/admin/exe/userApps.archive/userApps.v453.src.tgz"
 
-    version("449", sha256="640e7997e9ce220d6ab465e944bf11e678a9e3858ec6b346b024dc6f44e84713")
+    version("453", sha256="f48fc1aa370bc3f0b874effd3087f97ecd0c65c8e3fd5ec9d9300d897006f72e")
     version(
         "302.1",
         commit="d8376c5d52a161f2267346ed3dc94b5dce74c2f9",
         git="https://github.com/ENCODE-DCC/kentUtils.git",
+        deprecated=True,
     )
 
     depends_on("libpng")
     depends_on("openssl")
     depends_on("libuuid")
     depends_on("mariadb")
+    depends_on("zlib-ng")
+    depends_on("freetype")
+    depends_on("iconv")
+    # depends_on("util-linux")
 
     conflicts("%cce", when="@302.1")
     conflicts("%apple-clang", when="@302.1")
@@ -32,6 +37,9 @@ class Kentutils(MakefilePackage):
     conflicts("%pgi", when="@302.1")
     conflicts("%xl", when="@302.1")
     conflicts("%xl_r", when="@302.1")
+
+    def setup_build_environment(self, env):
+        env.append_flags("CFLAGS", "-I{}".format(self.spec["iconv"].prefix.lib))
 
     def install(self, spec, prefix):
         install_tree("bin", prefix.bin)
