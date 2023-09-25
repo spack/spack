f11d7ac742b418d5fd58ced9ae909597501db238
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-09-25 17:01:46 +0200

    llvm: fix llvm@14 build with apple-clang-15

--- var/spack/repos/builtin/packages/llvm/package.py
+++ var/spack/repos/builtin/packages/llvm/package.py
@@ -560,6 +560,8 @@ class Llvm(CMakePackage, CudaPackage):
     patch("add-include-for-libelf-llvm-12-14.patch", when="@12:14")
     patch("add-include-for-libelf-llvm-15.patch", when="@15")
 
+    patch("sanitizer-platform-limits-posix-xdr-macos.patch", when="@10:14 platform=darwin")
+
     # The functions and attributes below implement external package
     # detection for LLVM. See:
     #
@@ -926,6 +928,29 @@ class Llvm(CMakePackage, CudaPackage):
 
         # Semicolon seperated list of runtimes to enable
         if runtimes:
+            if self.spec.satisfies("@:14"):
+                # The older versions are not
+                # careful enough with the order of
+                # the runtimes.
+                # Instead of
+                # applying
+                # https://github.com/llvm/llvm-project/commit/06400a0142af8297b5d39b8f34a7c59db6f9910c,
+                # which might be incompatible with the version that we install,
+                # we sort the runtimes here according to the same order as
+                # in the aforementioned commit:
+                runtimes_order = [
+                    "libc",
+                    "libunwind",
+                    "libcxxabi",
+                    "libcxx",
+                    "compiler-rt",
+                    "openmp",
+                ]
+                runtimes.sort(
+                    key=lambda x: runtimes_order.index(x)
+                    if x in runtimes_order
+                    else len(runtimes_order)
+                )
             cmake_args.extend(
                 [
                     define("LLVM_ENABLE_RUNTIMES", runtimes),
