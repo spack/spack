5045c7071b4b9039a6b0e04e57f217c9c2dc5063
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-09-25 09:44:41 -0700

    unconditionally depend on lapack; check provider name to determine if ENABLE_MKL shoudl be set

--- var/spack/repos/builtin/packages/gsibec/package.py
+++ var/spack/repos/builtin/packages/gsibec/package.py
@@ -27,19 +27,20 @@ class Gsibec(CMakePackage):
     version("1.0.3", sha256="f104daf55705c5093a3d984073f082017bc9166f51ded36c7f7bb8adf233c916")
     version("1.0.2", sha256="7dc02f1f499e0d9f2843440f517d6c8e5d10ea084cbb2567ec198ba06816bc8b")
 
-    variant("mkl", default=False, description="Use MKL for LAPACK implementation")
-
     depends_on("mpi", type=("build", "run"))
     depends_on("netcdf-c +mpi", type=("build", "run"))
     depends_on("netcdf-fortran", type=("build", "run"))
 
-    depends_on("mkl", when="+mkl", type=("build", "run"))
-    depends_on("lapack", when="~mkl", type=("build", "run"))
+    depends_on("lapack", type=("build", "run"))
 
     depends_on("ecbuild", type=("build"))
     depends_on("jedi-cmake", type=("build"))
     depends_on("sp", type=("build"))
 
     def cmake_args(self):
-        args = [self.define_from_variant("ENABLE_MKL", "mkl")]
+        args = []
+
+        mkl_providers = ["intel-mkl", "intel-oneapi-mkl", "intel-parallel-studio"]
+        args.append(self.define("ENABLE_MKL", spec["lapack"].name in mkl_providers))
+
         return args
