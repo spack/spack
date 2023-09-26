d57fe421719cc953bf55de0028864ec5314eea6a
stefano-corda <102236773+stefano-corda@users.noreply.github.com>
2023-09-26 11:54:31 +0000

    [@spackbot] updating style on behalf of stefano-corda

--- var/spack/repos/builtin/packages/py-cuda-python/package.py
+++ var/spack/repos/builtin/packages/py-cuda-python/package.py
@@ -17,7 +17,7 @@ class PyCudaPython(PythonPackage):
     version("11.8.0", sha256="afc4f0ac46c0e734a71f97d52390424aec1bc9fceb324e30095ca09bc678ff72")
 
     depends_on("py-setuptools", type="build")
-    
+
     depends_on("python@3.8:", type=("build", "run"))
 
     depends_on("py-future", type=("build", "run"))
@@ -29,7 +29,7 @@ class PyCudaPython(PythonPackage):
     depends_on("py-numpy@1.21.1:", type=("build", "run"))
     depends_on("py-pyclibrary@0.1.7:", type=("build", "run"))
 
-    depends_on("cuda@:11.8",)
+    depends_on("cuda@:11.8")
 
     def setup_build_environment(self, env):
-        env.prepend_path('LD_LIBRARY_PATH', self.spec['cuda'].prefix.lib)
+        env.prepend_path("LD_LIBRARY_PATH", self.spec["cuda"].prefix.lib)
--- var/spack/repos/builtin/packages/py-pyclibrary/package.py
+++ var/spack/repos/builtin/packages/py-pyclibrary/package.py
@@ -8,13 +8,12 @@ from spack.pkg.builtin.boost import Boost
 
 
 class PyPyclibrary(PythonPackage):
-    """Pyclibrary
-    """
+    """Pyclibrary"""
 
     homepage = "https://pyclibrary.readthedocs.io/en/latest/"
     pypi = "pyclibrary/pyclibrary-0.1.7.tar.gz"
 
     version("0.1.7", sha256="91ed4479754ef21744d8056f8d6c2d269ae7832a90c1e1d4b1128ab0fa76ed18")
 
-    depends_on("py-setuptools",  type=("build"))
+    depends_on("py-setuptools", type=("build"))
     depends_on("python@3.8:", type=("build", "run"))
