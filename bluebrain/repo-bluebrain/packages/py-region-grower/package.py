20774a284d2acea5ed295476d79f2caa5aadfd3a
Adrien Berchet <adrien.berchet@epfl.ch>
2023-09-26 15:09:30 +0200

    py-region-grower: bump to 1.1.4 (#2132)

--- bluebrain/repo-bluebrain/packages/py-region-grower/package.py
+++ bluebrain/repo-bluebrain/packages/py-region-grower/package.py
@@ -13,7 +13,7 @@ class PyRegionGrower(PythonPackage):
     git = "ssh://git@bbpgitlab.epfl.ch/neuromath/region-grower.git"
 
     version("develop", branch="main")
-    version("1.1.3", tag="region-grower-v1.1.3")
+    version("1.1.4", tag="region-grower-v1.1.4")
 
     depends_on("py-setuptools", type="build")
 
