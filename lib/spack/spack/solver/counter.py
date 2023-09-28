bbf3a0e3f7a980e9c76fbe61c8838719bedc0934
Peter Scheibel <scheibel1@llnl.gov>
2023-09-28 15:31:41 -0700

    dont access .provided attr for virtuals

--- lib/spack/spack/solver/counter.py
+++ lib/spack/spack/solver/counter.py
@@ -32,7 +32,8 @@ class Counter:
         self._possible_dependencies: PossibleDependencies = set()
         self._possible_virtuals: Set[str] = set(x.name for x in specs if x.virtual)
         for x in specs:
-            self._possible_virtuals.update(str(i) for i in x.package_class.provided.keys())
+            if not x.virtual:
+                self._possible_virtuals.update(str(i) for i in x.package_class.provided.keys())
 
     def possible_dependencies(self) -> PossibleDependencies:
         """Returns the list of possible dependencies"""
