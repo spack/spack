1dec290583064b5c1bc47e5684c7b2ba9f7240b5
eugeneswalker <38933153+eugeneswalker@users.noreply.github.com>
2023-10-02 12:26:55 -0700

    vtk-m@2.0: depend on kokkos@3.7:3.9 per issue #40268 (#40281)

--- var/spack/repos/builtin/packages/vtk-m/package.py
+++ var/spack/repos/builtin/packages/vtk-m/package.py
@@ -102,6 +102,7 @@ class VtkM(CMakePackage, CudaPackage, ROCmPackage):
 
     # VTK-m uses the default Kokkos backend
     depends_on("kokkos", when="+kokkos")
+    depends_on("kokkos@3.7:3.9", when="@2.0 +kokkos")
     # VTK-m native CUDA and Kokkos CUDA backends are not compatible
     depends_on("kokkos ~cuda", when="+kokkos +cuda +cuda_native")
     depends_on("kokkos +cuda", when="+kokkos +cuda ~cuda_native")
