bdf30b5f2df971e5f1a2e07ce9aab4aa1f38a338
Adam J. Stewart <ajstewart426@gmail.com>
2023-10-02 21:38:20 +0200

    Better documentation of dependency quirks

--- var/spack/repos/builtin/packages/py-torchgeo/package.py
+++ var/spack/repos/builtin/packages/py-torchgeo/package.py
@@ -31,8 +31,12 @@ class PyTorchgeo(PythonPackage):
     variant("style", default=False, description="Install style checking tools")
     variant("tests", default=False, description="Install testing tools")
 
+    # NOTE: historically, dependencies had upper bounds based on semantic version compatibility.
+    # However, these were removed to improve maintainability and flexibility of the recipe.
+
     # Required dependencies
     depends_on("python@3.9:", when="@0.5:", type=("build", "run"))
+    # COWC dataset requires unpacking .bz2 files.
     depends_on("python+bz2", type=("build", "run"))
     depends_on("py-setuptools@61:", when="@0.5:", type="build")
     depends_on("py-setuptools@42:", type="build")
@@ -40,10 +44,11 @@ class PyTorchgeo(PythonPackage):
     depends_on("py-fiona@1.8.19:", when="@0.5:", type=("build", "run"))
     depends_on("py-fiona@1.8:", when="@0.3:", type=("build", "run"))
     depends_on("py-fiona@1.5:", type=("build", "run"))
-    # Only part of lightning[pytorch-extra] we actually require
+    # Only part of lightning[pytorch-extra] we actually require.
     depends_on("py-jsonargparse@4.18:+signatures", when="@0.5:", type=("build", "run"))
     depends_on("py-kornia@0.6.9:", when="@0.5:", type=("build", "run"))
     depends_on("py-kornia@0.6.5:", when="@0.4.1:", type=("build", "run"))
+    # https://github.com/microsoft/torchgeo/pull/1123
     depends_on("py-kornia@0.6.5:0.6.9", when="@0.4.0", type=("build", "run"))
     depends_on("py-kornia@0.6.4:0.6.9", when="@0.3", type=("build", "run"))
     depends_on("py-kornia@0.5.11:0.6.9", when="@0.2", type=("build", "run"))
@@ -58,6 +63,7 @@ class PyTorchgeo(PythonPackage):
     depends_on("py-pandas@1.1.3:", when="@0.5:", type=("build", "run"))
     depends_on("pil@8:", when="@0.5:", type=("build", "run"))
     depends_on("pil@6.2:", type=("build", "run"))
+    # JPEG, TIFF, and compressed PNG support required for file I/O in several datasets.
     depends_on("pil+jpeg+tiff+zlib", type=("build", "run"))
     depends_on("py-pyproj@3:", when="@0.5:", type=("build", "run"))
     depends_on("py-pyproj@2.2:", type=("build", "run"))
@@ -82,13 +88,19 @@ class PyTorchgeo(PythonPackage):
 
     # Optional dependencies
     with when("+datasets"):
+        # GDAL and libtiff are both dependencies of rasterio.
+        # Sentinel 2 dataset requires OpenJPEG to read .jp2 files.
         depends_on("gdal+openjpeg", when="@0.3.1:", type="run")
+        # JPEG required for GDAL to read JPEG files
+        # LIBDEFLATE, ZLIB, and ZSTD required for compressed file I/O.
         depends_on("libtiff+jpeg+libdeflate+zlib+zstd", type="run")
         depends_on("py-h5py@3:", when="@0.5:", type="run")
         depends_on("py-h5py@2.6:", type="run")
         depends_on("py-laspy@2:", when="@0.2:", type="run")
         depends_on("opencv@4.4.0.46:", when="@0.5:", type="run")
         depends_on("opencv@3.4.2.17:", type="run")
+        # LandCover.ai dataset requires ability to read .tif and write .jpg and .png files.
+        # Doing this from Python requires both imgcodecs and Python bindings.
         depends_on("opencv+imgcodecs+jpeg+png+python3+tiff", type="run")
         depends_on("py-pycocotools@2.0.5:", when="@0.5:", type="run")
         depends_on("py-pycocotools@2:", type="run")
@@ -132,11 +144,13 @@ class PyTorchgeo(PythonPackage):
     depends_on("py-omegaconf@2.1:", when="@:0.4.0", type=("build", "run"))
     depends_on("py-packaging@17:", when="@0.3", type=("build", "run"))
     depends_on("py-pytorch-lightning@1.5.1:", when="@0.3.1:0.4.0", type=("build", "run"))
+    # https://github.com/microsoft/torchgeo/pull/697
     depends_on("py-pytorch-lightning@1.5.1:1.8", when="@0.3.0", type=("build", "run"))
     depends_on("py-pytorch-lightning@1.3:1.8", when="@:0.2", type=("build", "run"))
     depends_on("py-scikit-learn@0.21:", when="@0.3:0.4", type=("build", "run"))
     depends_on("py-scikit-learn@0.18:", when="@:0.2", type=("build", "run"))
     depends_on("open3d@0.11.2:+python", when="@0.2:0.3+datasets", type="run")
+    # https://github.com/microsoft/torchgeo/pull/1537
     depends_on("py-pandas@0.23.2:2.0", when="@0.3:0.4+datasets", type="run")
     depends_on("py-pandas@0.19.1:2.0", when="@0.2+datasets", type="run")
     depends_on("py-omegaconf@2.1:", when="@0.4.1+tests", type="run")
