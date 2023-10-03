dd5cf5fec38765641b120392ae83ce5cf4761a29
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-10-03 13:04:30 +0200

    py-wcwidth: add 0.2.7 (#40256)

--- var/spack/repos/builtin/packages/py-wcwidth/package.py
+++ var/spack/repos/builtin/packages/py-wcwidth/package.py
@@ -9,8 +9,10 @@ from spack.package import *
 class PyWcwidth(PythonPackage):
     """Measures number of Terminal column cells of wide-character codes"""
 
+    homepage = "https://github.com/jquast/wcwidth"
     pypi = "wcwidth/wcwidth-0.1.7.tar.gz"
 
+    version("0.2.7", sha256="1b6d30a98ddd5ce9bbdb33658191fd2423fc9da203fe3ef1855407dcb7ee4e26")
     version("0.2.5", sha256="c4d647b99872929fdb7bdcaa4fbe7f01413ed3d98077df798530e5b04f116c83")
     version("0.1.7", sha256="3df37372226d6e63e1b1e1eda15c594bca98a22d33a23832a90998faa96bc65e")
 
