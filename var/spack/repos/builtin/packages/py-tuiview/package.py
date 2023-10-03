7ee2033ec190b0d04987fafb857d47731377582d
Sam Gillingham <gillingham.sam@gmail.com>
2023-10-04 08:37:16 +1000

    comma

--- var/spack/repos/builtin/packages/py-tuiview/package.py
+++ var/spack/repos/builtin/packages/py-tuiview/package.py
@@ -27,7 +27,7 @@ class PyTuiview(PythonPackage):
     version(
         "1.1.7",
         sha256="fbf0bf29cc775357dad4f8a2f0c2ffa98bbf69d603a96353e75b321adef67573",
-        deprecated=True
+        deprecated=True,
     )
 
     # pip silently replaces distutils with setuptools
