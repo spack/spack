05fe77cff349ce3248ef1e4f3a52582e47bc4b90
Sam Gillingham <gillingham.sam@gmail.com>
2023-10-04 08:42:07 +1000

    try doing in one

--- var/spack/repos/builtin/packages/py-rios/package.py
+++ var/spack/repos/builtin/packages/py-rios/package.py
@@ -31,5 +31,4 @@ class PyRios(PythonPackage):
     depends_on("py-setuptools", type="build")
     depends_on("py-numpy", type=("build", "run"))
     depends_on("gdal+python", type=("build", "run"))
-    if "+parallel" in spec:
-        depends_on("py-cloudpickle", type=("build", "run"), when="@1.4.16:")
+    depends_on("py-cloudpickle", type=("build", "run"), when="@1.4.16:+parallel")
