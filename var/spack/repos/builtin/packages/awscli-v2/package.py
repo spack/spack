0fe8862fce54fea6adf7f247a5a64c80db579159
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-10-03 08:30:30 -0600

    Update var/spack/repos/builtin/packages/awscli-v2/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/awscli-v2/package.py
+++ var/spack/repos/builtin/packages/awscli-v2/package.py
@@ -17,7 +17,7 @@ class AwscliV2(AutotoolsPackage):
     version("2.13.22", sha256="dd731a2ba5973f3219f24c8b332a223a29d959493c8a8e93746d65877d02afc1")
 
     depends_on("python@3.8:", type=("build", "run"))
-    depends_on("py-flit-core@3.7:3.8", type=("build"))
+    depends_on("py-flit-core@3.7.1:3.8.0", type=("build"))
     depends_on("py-pip@22:23", type=("build"))
     depends_on("py-colorama@0.2.5:0.4.6")
     depends_on("py-docutils@0.10:0.19")
