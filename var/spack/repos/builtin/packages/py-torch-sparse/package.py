b11e7521c36b83392f350fe04dab3432ec115ba5
Jen Herting <jen@herting.cc>
2023-10-08 11:20:46 -0400

    py-torch-sparse: add v0.6.17 (#39495)
    * [py-torch-sparse] New version 0.6.17
    
    * [py-torch-sparse] added dependency on parallel-hashmap
    
    * [py-torch-sparse]
    
    - spack only supports python@3.7:
    - py-pytest-runner only needed with old versions

--- var/spack/repos/builtin/packages/py-torch-sparse/package.py
+++ var/spack/repos/builtin/packages/py-torch-sparse/package.py
@@ -13,18 +13,19 @@ class PyTorchSparse(PythonPackage):
     homepage = "https://github.com/rusty1s/pytorch_sparse/"
     url = "https://github.com/rusty1s/pytorch_sparse/archive/0.6.7.tar.gz"
 
+    version("0.6.17", sha256="c964a70ed978bff65009250eb12fae96317c60c9a04d7d1b07f0beee8b4b9c22")
     version("0.6.8", sha256="98f7ff1f0f9cd5031bc81c70c11970c3864545ae33677025a6efd2466a97e6f9")
     version("0.6.7", sha256="0d038a1502548692972a085cd0496460b5d2050bb7328427add990f081d6c44d")
 
     variant("cuda", default=False, description="Enable CUDA support")
 
-    depends_on("python@3.6:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
-    depends_on("py-pytest-runner", type="build")
+    depends_on("py-pytest-runner", when="@:0.6.8", type="build")
     depends_on("py-scipy", type=("build", "run"))
     depends_on("py-torch", type=("build", "run"))
     depends_on("py-torch-scatter+cuda", when="+cuda")
     depends_on("py-torch-scatter~cuda", when="~cuda")
+    depends_on("parallel-hashmap", when="@0.6.17:")
 
     def setup_build_environment(self, env):
         if "+cuda" in self.spec:
