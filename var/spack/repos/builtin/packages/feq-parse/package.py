4c244a327b7a58666dae93ea0755cf72892f6340
Joe Schoonover <11430768+fluidnumerics-joe@users.noreply.github.com>
2023-10-09 18:31:23 +0800

    feq-parse: add v2.0.3 (#40230)

--- var/spack/repos/builtin/packages/feq-parse/package.py
+++ var/spack/repos/builtin/packages/feq-parse/package.py
@@ -16,8 +16,11 @@ class FeqParse(CMakePackage):
 
     maintainers("fluidnumerics-joe")
 
+    version("2.0.3", sha256="a1c42507801adc55a63a9a904807058079d54e002e10f2b29a916b06fc815f80")
     version("2.0.1", sha256="08dd08bd100a0a2eb672a5b2792ad56a337df575c634aac0d7a300d7e484b21c")
     version("1.1.0", sha256="d33a4fd6904939bb70780e8f25f37c1291c4f24fd207feb4ffc0f8d89637d1e3")
     version("1.0.2", sha256="1cd1db7562908ea16fc65dc5268b654405d0b3d9dcfe11f409949c431b48a3e8")
 
     depends_on("cmake@3.0.2:", type="build")
+
+    parallel = False
