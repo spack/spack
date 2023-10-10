6f9fef78d18d74225d2feab85dc3267c8a0abf45
Peter Scheibel <scheibel1@llnl.gov>
2023-10-10 16:09:38 -0700

    Revert "intermediate work on imposing based on IDs vs specs"
    This reverts commit 664e9af0301259fa6cdf9fff6b7f81b8241565ca.

--- lib/spack/spack/test/concretize_requirements.py
+++ lib/spack/spack/test/concretize_requirements.py
@@ -507,9 +507,6 @@ packages:
 """
     update_packages_config(conf_str)
 
-    asp = solve("--show=asp", "y")
-    import pdb; pdb.set_trace()
-
     spec_y = Spec("y").concretized()
     assert spec_y.satisfies("cflags=-g")
 
@@ -789,7 +786,6 @@ def test_conditional_requirements_from_packages_yaml(
         assert spec.satisfies(match_str) is expected
 
 
-@pytest.mark.xfail
 @pytest.mark.parametrize(
     "packages_yaml,spec_str,expected_message",
     [
