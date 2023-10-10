dabd9167b035a0e17d5a12e2dd7b13e44defdffb
Fernando Pereira <fernando.pereira@epfl.ch>
2023-10-10 14:31:36 +0200

    py-neurodamus: bump to 2.16.4 (#2146)

--- bluebrain/repo-bluebrain/packages/py-neurodamus/package.py
+++ bluebrain/repo-bluebrain/packages/py-neurodamus/package.py
@@ -12,6 +12,7 @@ class PyNeurodamus(PythonPackage):
     git = "https://github.com/BlueBrain/neurodamus.git"
 
     version("develop", branch="main")
+    version("2.16.4", tag="2.16.4")
     version("2.16.3", tag="2.16.3")
     version("2.16.2", tag="2.16.2")
     version("2.16.1", tag="2.16.1")
