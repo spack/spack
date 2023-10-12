27ea8c18bfdf9048a4e23a01a5d5d238246e24ac
Tristan Carel <tristan.carel@epfl.ch>
2023-10-12 13:16:35 +0200

    gmsh: bump to 4.11.1 (#2150)

--- bluebrain/repo-patches/packages/gmsh/package.py
+++ bluebrain/repo-patches/packages/gmsh/package.py
@@ -8,6 +8,8 @@ from spack.pkg.builtin.gmsh import Gmsh as BuiltinGmsh
 class Gmsh(BuiltinGmsh):
     __doc__ = BuiltinGmsh.__doc__
 
+    version("4.11.1", sha256="c5fe1b7cbd403888a814929f2fd0f5d69e27600222a18c786db5b76e8005b365")
+
     def setup_run_environment(self, env):
         if os.path.isdir(self.prefix.lib):
             lib_dir = self.prefix.lib
