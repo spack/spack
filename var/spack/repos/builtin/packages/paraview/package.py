46e3451a5ccaedfb51a1179ebe728508ad83f509
Ben Boeckel <ben.boeckel@kitware.com>
2023-10-13 18:48:09 -0400

    paraview: require C++11-supporting protobuf for `master` too

--- var/spack/repos/builtin/packages/paraview/package.py
+++ var/spack/repos/builtin/packages/paraview/package.py
@@ -228,6 +228,7 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
     # but paraview uses C++11 by default. Use for 5.11+ until ParaView updates
     # its C++ standard level.
     depends_on("protobuf@3.4:3.21", when="@5.11:")
+    depends_on("protobuf@3.4:3.21", when="@master")
     depends_on("libxml2")
     depends_on("lz4")
     depends_on("xz")
