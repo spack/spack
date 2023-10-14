32e96ebdd9ebf3503c771cba8578bddb20bc5cd9
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-10-14 19:23:54 +0200

    py-urllib3: add 2.0.6 (#40207)
    * py-urllib3: add 2.0.5
    
    * Add py-brotli package
    
    * Group brotli dependencies and make limits more specific
    
    * Add minimum version limits to variants
    
    * Remove python upper limit for py-brotli
    
    * Fix restrictions for py-brotli dependency
    
    * Fix py-brotli dependency
    
    * py-urllib3: add 2.0.6

---
+++ var/spack/repos/builtin/packages/py-brotli/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyBrotli(PythonPackage):
+    """Python bindings for the Brotli compression library."""
+
+    homepage = "https://github.com/google/brotli"
+    pypi = "Brotli/Brotli-1.1.0.tar.gz"
+
+    version("1.1.0", sha256="81de08ac11bcb85841e440c13611c00b67d3bf82698314928d0b676362546724")
+
+    depends_on("py-setuptools", type="build")
--- var/spack/repos/builtin/packages/py-urllib3/package.py
+++ var/spack/repos/builtin/packages/py-urllib3/package.py
@@ -12,7 +12,10 @@ class PyUrllib3(PythonPackage):
 
     homepage = "https://urllib3.readthedocs.io/"
     pypi = "urllib3/urllib3-1.25.6.tar.gz"
+    git = "https://github.com/urllib3/urllib3.git"
 
+    version("2.0.6", sha256="b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564")
+    version("2.0.5", sha256="13abf37382ea2ce6fb744d4dad67838eec857c9f4f57009891805e0b5e123594")
     version("1.26.12", sha256="3fa96cf423e6987997fc326ae8df396db2a8b7c667747d47ddd8ecba91f4a74e")
     version("1.26.6", sha256="f57b4c16c62fa2760b7e3d97c35b255512fb6b59a259730f36ba32ce9f8e342f")
     version("1.25.9", sha256="3018294ebefce6572a474f0604c2021e33b3fd8006ecd11d62107a5d2a963527")
@@ -23,22 +26,29 @@ class PyUrllib3(PythonPackage):
     version("1.20", sha256="97ef2b6e2878d84c0126b9f4e608e37a951ca7848e4855a7f7f4437d5c34a72f")
     version("1.14", sha256="dd4fb13a4ce50b18338c7e4d665b21fd38632c5d4b1d9f1a1379276bd3c08d37")
 
-    variant("socks", default=False, description="SOCKS and HTTP proxy support")
+    variant("brotli", default=False, when="@1.25:", description="Add Brotli support")
     variant("secure", default=False, description="Add SSL/TLS support")
-    variant("brotli", default=False, description="Add Brotli support")
+    variant("socks", default=False, when="@1.15:", description="SOCKS and HTTP proxy support")
 
-    depends_on("python@2.7:2.8,3.4:", when="@:1.25", type=("build", "run"))
-    depends_on("python@2.7:2.8,3.5:", when="@1.26.6", type=("build", "run"))
-    depends_on("python@2.7:2.8,3.6:3", when="@1.26.12:", type=("build", "run"))
+    depends_on("py-hatchling@1.6:1", when="@2:", type="build")
 
-    depends_on("py-setuptools", type="build")
+    with when("+brotli"):
+        depends_on("py-brotli@1.0.9:", when="@1.26.9:", type=("build", "run"))
 
-    depends_on("py-pyopenssl@0.14:", when="+secure")
-    depends_on("py-cryptography@1.3.4:", when="+secure")
-    depends_on("py-idna@2:", when="+secure")
-    depends_on("py-certifi", when="+secure")
-    depends_on("py-urllib3-secure-extra", when="+secure @1.26.12:")
+        # Historical dependencies
+        depends_on("py-brotlipy@0.6:", when="@:1.26.8", type=("build", "run"))
 
-    depends_on("py-pysocks@1.5.6,1.5.8:1", when="+socks")
+    with when("+secure"):
+        depends_on("py-pyopenssl@17.1:", when="@2:", type=("build", "run"))
+        depends_on("py-pyopenssl@0.14:", when="@1", type=("build", "run"))
+        depends_on("py-cryptography@1.9:", when="@2:", type=("build", "run"))
+        depends_on("py-cryptography@1.3.4:", when="@1", type=("build", "run"))
+        depends_on("py-idna@2:", type=("build", "run"))
+        depends_on("py-certifi", type=("build", "run"))
+        depends_on("py-urllib3-secure-extra", when="@1.26.12:", type=("build", "run"))
 
-    depends_on("py-brotlipy@0.6:", when="+brotli")
+    depends_on("py-pysocks@1.5.6,1.5.8:1", when="+socks", type=("build", "run"))
+
+    # Historical dependencies
+    depends_on("py-setuptools", when="@1", type="build")
+    depends_on("python@3.6:3", when="@1.26.12:1", type=("build", "run"))
