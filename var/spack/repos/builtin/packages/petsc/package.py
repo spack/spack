826ca766c005ecf7b69b6bcbf9ad3f8a0bc11979
Satish Balay <balay@mcs.anl.gov>
2023-10-16 16:23:04 -0500

    petsc: add in gmake as dependency - so that consistent make gets used between petsc and slepc builds [that can have different env for each of the builds]

--- var/spack/repos/builtin/packages/petsc/package.py
+++ var/spack/repos/builtin/packages/petsc/package.py
@@ -208,6 +208,8 @@ class Petsc(Package, CudaPackage, ROCmPackage):
     patch("revert-3.18.0-ver-format-for-dealii.patch", when="@3.18.0")
 
     depends_on("diffutils", type="build")
+    # not listed as a "build" dependency - so that slepc build gets the same dependency
+    depends_on("gmake")
 
     # Virtual dependencies
     # Git repository needs sowing to build Fortran interface
