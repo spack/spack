3649826b47598f7fee8984cfc7d9479b3f0c9641
Tuomas Koskela <t.koskela@ucl.ac.uk>
2023-10-16 14:18:29 +0100

    Add fft and libxc paths

--- var/spack/repos/builtin/packages/conquest/package.py
+++ var/spack/repos/builtin/packages/conquest/package.py
@@ -63,12 +63,16 @@ class Conquest(MakefilePackage):
 
         lapack_ld = self.spec["lapack"].libs.ld_flags
         blas_ld = self.spec["blas"].libs.ld_flags
+        fftw_ld = self.spec["fftw"].libs.ld_flags
+        libxc_ld = self.spec["libxc"].libs.ld_flags
 
         defs_file = FileFilter("./src/system.make")
 
         defs_file.filter("COMPFLAGS=.*", f"COMPFLAGS= {fflags}")
         defs_file.filter("LINKFLAGS=.*", f"LINKFLAGS= {ldflags}")
-        defs_file.filter("# BLAS=.*", f"BLAS= {lapack_ld} -llapack {blas_ld} -lblas")
+        defs_file.filter("# BLAS=.*", f"BLAS= {lapack_ld} {blas_ld}")
+        defs_file.filter("FFT_LIB=.*", f"FFT_LIB={fftw_ld}")
+        defs_file.filter("XC_LIB=.*", f"XC_LIB={libxc_ld} -lxcf90 -lxc")
 
         if "+openmp" in self.spec:
             defs_file.filter("OMP_DUMMY = DUMMY", "OMP_DUMMY = ")
