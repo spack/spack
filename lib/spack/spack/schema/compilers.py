279058e183644623bf1b167ea6f63a06385fdac4
Harmen Stoppels <me@harmenstoppels.nl>
2023-10-20 09:51:49 +0200

    schema/compilers.py: fix validation of 2+ entries (#40627)
    Fix the following syntax which validates only the first array entry:
    
    ```python
    "compilers": {
        "type": "array",
        "items": [
            {
                "type": ...
            }
        ]
    }
    ```
    
    to
    
    ```python
    "compilers": {
        "type": "array",
        "items": {
            "type": ...
        }
    }
    ```
    
    which validates the entire array.
    
    Oops...

--- lib/spack/spack/schema/compilers.py
+++ lib/spack/spack/schema/compilers.py
@@ -14,63 +14,61 @@ import spack.schema.environment
 properties = {
     "compilers": {
         "type": "array",
-        "items": [
-            {
-                "type": "object",
-                "additionalProperties": False,
-                "properties": {
-                    "compiler": {
-                        "type": "object",
-                        "additionalProperties": False,
-                        "required": ["paths", "spec", "modules", "operating_system"],
-                        "properties": {
-                            "paths": {
-                                "type": "object",
-                                "required": ["cc", "cxx", "f77", "fc"],
-                                "additionalProperties": False,
-                                "properties": {
-                                    "cc": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "cxx": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "f77": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "fc": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                },
-                            },
-                            "flags": {
-                                "type": "object",
-                                "additionalProperties": False,
-                                "properties": {
-                                    "cflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "cxxflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "fflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "cppflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "ldflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                    "ldlibs": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                                },
-                            },
-                            "spec": {"type": "string"},
-                            "operating_system": {"type": "string"},
-                            "target": {"type": "string"},
-                            "alias": {"anyOf": [{"type": "string"}, {"type": "null"}]},
-                            "modules": {
-                                "anyOf": [{"type": "string"}, {"type": "null"}, {"type": "array"}]
-                            },
-                            "implicit_rpaths": {
-                                "anyOf": [
-                                    {"type": "array", "items": {"type": "string"}},
-                                    {"type": "boolean"},
-                                ]
-                            },
-                            "environment": spack.schema.environment.definition,
-                            "extra_rpaths": {
-                                "type": "array",
-                                "default": [],
-                                "items": {"type": "string"},
+        "items": {
+            "type": "object",
+            "additionalProperties": False,
+            "properties": {
+                "compiler": {
+                    "type": "object",
+                    "additionalProperties": False,
+                    "required": ["paths", "spec", "modules", "operating_system"],
+                    "properties": {
+                        "paths": {
+                            "type": "object",
+                            "required": ["cc", "cxx", "f77", "fc"],
+                            "additionalProperties": False,
+                            "properties": {
+                                "cc": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "cxx": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "f77": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "fc": {"anyOf": [{"type": "string"}, {"type": "null"}]},
                             },
                         },
-                    }
-                },
-            }
-        ],
+                        "flags": {
+                            "type": "object",
+                            "additionalProperties": False,
+                            "properties": {
+                                "cflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "cxxflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "fflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "cppflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "ldflags": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                                "ldlibs": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                            },
+                        },
+                        "spec": {"type": "string"},
+                        "operating_system": {"type": "string"},
+                        "target": {"type": "string"},
+                        "alias": {"anyOf": [{"type": "string"}, {"type": "null"}]},
+                        "modules": {
+                            "anyOf": [{"type": "string"}, {"type": "null"}, {"type": "array"}]
+                        },
+                        "implicit_rpaths": {
+                            "anyOf": [
+                                {"type": "array", "items": {"type": "string"}},
+                                {"type": "boolean"},
+                            ]
+                        },
+                        "environment": spack.schema.environment.definition,
+                        "extra_rpaths": {
+                            "type": "array",
+                            "default": [],
+                            "items": {"type": "string"},
+                        },
+                    },
+                }
+            },
+        },
     }
 }
 
