620190c0b85d914a273e62982802c9e2d0fe3e59
Adam J. Stewart <ajstewart426@gmail.com>
2023-10-20 07:23:54 -0500

    TensorFlow/Keras/TensorBoard: add v2.14.0 (#40297)
    Co-authored-by: adamjstewart <adamjstewart@users.noreply.github.com>

--- var/spack/repos/builtin/packages/py-keras/package.py
+++ var/spack/repos/builtin/packages/py-keras/package.py
@@ -21,6 +21,7 @@ class PyKeras(PythonPackage):
     git = "https://github.com/keras-team/keras.git"
     url = "https://github.com/keras-team/keras/archive/refs/tags/v2.7.0.tar.gz"
 
+    version("2.14.0", sha256="a845d446b6ae626f61dde5ab2fa952530b6c17b4f9ed03e9362bd20172d00cca")
     version("2.13.1", sha256="b3591493cce75a69adef7b192cec6be222e76e2386d132cd4e34aa190b0ecbd5")
     version("2.12.0", sha256="6336cebb6b2b0a91f7efd3ff3a9db3a94f2abccf07a40323138afb80826aec62")
     version("2.11.0", sha256="e7a7c4199ac76ea750d145c1d84ae1b932e68b9bca34e83596bd66b2fc2ad79e")
@@ -61,7 +62,7 @@ class PyKeras(PythonPackage):
     depends_on("py-pydot", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
     depends_on("py-six", type=("build", "run"))
-    for minor_ver in range(6, 14):
+    for minor_ver in range(6, 15):
         depends_on(
             "py-tensorflow@2.{}".format(minor_ver),
             type=("build", "run"),
---
+++ var/spack/repos/builtin/packages/py-ml-dtypes/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyMlDtypes(PythonPackage):
+    """A stand-alone implementation of several NumPy dtype extensions
+    used in machine learning libraries."""
+
+    homepage = "https://github.com/jax-ml/ml_dtypes"
+    pypi = "ml_dtypes/ml_dtypes-0.3.1.tar.gz"
+    git = "https://github.com/jax-ml/ml_dtypes.git"
+    submodules = True
+
+    version("0.3.1", tag="v0.3.1", commit="bbeedd470ecac727c42e97648c0f27bfc312af30")
+    version("0.2.0", tag="v0.2.0", commit="5b9fc9ad978757654843f4a8d899715dbea30e88")
+
+    depends_on("python@3.9:", when="@0.3:", type=("build", "link", "run"))
+    depends_on("py-numpy@1.21:", type=("build", "link", "run"))
+    # Build dependencies are overconstrained, older versions work just fine
+    depends_on("py-pybind11", type=("build", "link"))
+    depends_on("py-setuptools", type="build")
