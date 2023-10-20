330e0159bdfe1b991a7aaad626b3fed491a82ea3
LydDeb <lyderic.debusschere@eolen.com>
2023-10-20 12:06:21 +0200

    py-corner: remove py-wheel dependence with respect to reviewing commentary

--- var/spack/repos/builtin/packages/py-corner/package.py
+++ var/spack/repos/builtin/packages/py-corner/package.py
@@ -17,7 +17,6 @@ class PyCorner(PythonPackage):
     version("2.2.2", sha256="4bc79f3b6778c270103f0926e64ef2606c48c3b6f92daf5382fc4babf5d608d1")
 
     depends_on("python@3.9:", type=("build", "run"))
-    depends_on("py-wheel", type="build")
     depends_on("py-setuptools@62.0:", type="build")
     depends_on("py-setuptools-scm", type="build")
     depends_on("py-matplotlib@2.1:", type=("build", "run"))
