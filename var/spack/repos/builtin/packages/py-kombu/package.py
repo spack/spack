1a47e22e146a555684f0da7281f7dd5c30e79513
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2023-10-21 15:37:29 +0200

    py-kombu: fix typo due to rebase

--- var/spack/repos/builtin/packages/py-kombu/package.py
+++ var/spack/repos/builtin/packages/py-kombu/package.py
@@ -27,7 +27,7 @@ class PyKombu(PythonPackage):
 
     depends_on("py-setuptools", type="build")
     # "pytz>dev" in tests_require: setuptools parser changed in v60 and errors.
-    depends_on("py-setuptools@:59", when="@4.6:5.2", when="@:5.2.3", type="build")
+    depends_on("py-setuptools@:59", when="@4.6:5.2", type="build")
     depends_on("py-amqp@2.5.2:2.5", when="@:4.6.6", type=("build", "run"))
     depends_on("py-amqp@2.6.0:2.6", when="@4.6.7:4", type=("build", "run"))
     depends_on("py-amqp@5.0.0:5", when="@5.0.0:5", type=("build", "run"))
