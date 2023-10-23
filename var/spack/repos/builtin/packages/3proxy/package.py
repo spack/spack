a4cb99b5996d6b56d40adbadb79708c46ce5159a
Aiden Grossman <agrossman154@yahoo.com>
2023-10-23 02:43:54 -0700

    3proxy: respect compiler choice (#39240)

--- var/spack/repos/builtin/packages/3proxy/package.py
+++ var/spack/repos/builtin/packages/3proxy/package.py
@@ -24,7 +24,9 @@ class _3proxy(MakefilePackage):
     depends_on("m4", type="build")
 
     def build(self, spec, prefix):
-        make("-f", f"Makefile.{platform.system()}")
+        make("-f", f"Makefile.{platform.system()}", f"CC={spack_cc}")
 
     def install(self, spec, prefix):
-        make("-f", f"Makefile.{platform.system()}", f"prefix={prefix}", "install")
+        make(
+            "-f", f"Makefile.{platform.system()}", f"prefix={prefix}", f"CC={spack_cc}", "install"
+        )
