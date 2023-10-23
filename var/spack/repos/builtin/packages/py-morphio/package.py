3b6a54d99614214f2407ecf71d3156c79c7e4140
Erik Heeren <erik.heeren@epfl.ch>
2023-10-23 14:43:18 +0200

    py-morphio: dependency fixes (#2168)
    * py-morhpio: dependencies fix
    
    * py-morphio: version / branch cleanup

--- var/spack/repos/builtin/packages/py-morphio/package.py
+++ var/spack/repos/builtin/packages/py-morphio/package.py
@@ -15,18 +15,17 @@ class PyMorphio(PythonPackage):
     git = "https://github.com/BlueBrain/MorphIO.git"
     pypi = "morphio/MorphIO-3.3.2.tar.gz"
 
-    version("develop", branch="master", submodules=True)
-    version("unifurcation", branch="unifurcation", submodules=True)
+    version("master", branch="master", submodules=True)
 
     version("3.3.6", sha256="0f2e55470d92a3d89f2141ae905ee104fd16257b93dafb90682d90171de2f4e6")
 
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools-scm", type="build")
 
-    depends_on("ninja", type=("build", "run"))
-    depends_on("cmake@3.2:", type=("build", "run"))
-    depends_on("py-numpy@1.14.1:", type="run")
+    depends_on("ninja", type="build")
+    depends_on("cmake@3.2:", type="build")
+    depends_on("py-numpy@1.14.1:", type=("build", "run"))
     if sys.platform == "win32":
         depends_on("py-h5py@3", type=("build", "run"))
     else:
-        depends_on("hdf5", type=("build", "run"))
+        depends_on("hdf5")
