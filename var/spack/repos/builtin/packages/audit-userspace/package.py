3ae3bfd997f4113b897ad997d33849ccee7ebc5f
Jordan GALBY <jordan.galby@cea.fr>
2023-10-26 12:11:42 +0200

    audit-userspace: Add 3.1.2
    Fixes build errors on almalinux 8 gcc 8.5.0:
    
    ```txt
         1352    audit_wrap.c: In function '_wrap_audit_rule_data_buf_set':
      >> 1353    audit_wrap.c:5010:17: error: cast specifies array type
         1354         arg1->buf = (char [])(char *)memcpy(malloc((size)*sizeof(char)), (const char *)(arg2), sizeof(char)*(size));
         1355                     ^
      >> 1356    audit_wrap.c:5010:15: error: invalid use of flexible array member
         1357         arg1->buf = (char [])(char *)memcpy(malloc((size)*sizeof(char)), (const char *)(arg2), sizeof(char)*(size));
         1358                   ^
      >> 1359    audit_wrap.c:5012:15: error: invalid use of flexible array member
         1360         arg1->buf = 0;
         1361                   ^
    ```

--- var/spack/repos/builtin/packages/audit-userspace/package.py
+++ var/spack/repos/builtin/packages/audit-userspace/package.py
@@ -15,6 +15,7 @@ class AuditUserspace(AutotoolsPackage):
     license("LGPL-2.1-or-later")
 
     version("4.0.1", sha256="f964610dc0c1e68075d5ae4b14d6280d1164b6eca3a4a13721d1a711681403d9")
+    version("3.1.2", sha256="4516dbfd1bea0eea10a30f907e50f17087673a536ec6322a2a568dff4ebe50f4")
     version("3.1.1", sha256="6a97cc472920639d736e9927353be05e323f351067fcf6e5d34439cafa0e9006")
     version("2.8.5", sha256="835ffdd65056ba0c26509dbf48882713b00dbe70e1d8cf25d538501136c2e3e9")
     version("2.8.4", sha256="089dfdceb38edf056202a6de4892fd0c9aaa964c08bd7806c5d0c7c33f09e18d")
