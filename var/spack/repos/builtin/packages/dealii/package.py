5726501daa7539338cb8419258c6ef09cac8d2e9
Daniel Arndt <arndtd@ornl.gov>
2023-10-27 13:55:23 -0400

    Remove workaround

--- var/spack/repos/builtin/packages/dealii/package.py
+++ var/spack/repos/builtin/packages/dealii/package.py
@@ -230,13 +230,6 @@ class Dealii(CMakePackage, CudaPackage):
     # do not require +rol to make concretization of xsdk possible
     depends_on("trilinos+amesos+aztec+epetra+ifpack+ml+muelu+sacado", when="+trilinos")
     depends_on("trilinos~hypre", when="+trilinos+int64")
-    # If this passess CI, we can remove it.
-    # # TODO: temporary disable Tpetra when using CUDA due to
-    # # namespace "Kokkos::Impl" has no member "cuda_abort"
-    # depends_on(
-    #     "trilinos@master+rol~amesos2~ifpack2~intrepid2~kokkos~tpetra~zoltan2",
-    #     when="+trilinos+cuda",
-    # )
     for a in CudaPackage.cuda_arch_values:
         arch_str = "+cuda cuda_arch=" + a
         trilinos_spec = "trilinos " + arch_str
