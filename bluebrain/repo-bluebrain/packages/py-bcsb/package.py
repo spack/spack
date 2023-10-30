b216beaddae724bf16135cafe708d38a8f595ece
Adrien4193 <39053578+Adrien4193@users.noreply.github.com>
2023-10-30 10:46:00 +0100

    py-bcsb: version 2.1.1. (#2171)

--- bluebrain/repo-bluebrain/packages/py-bcsb/package.py
+++ bluebrain/repo-bluebrain/packages/py-bcsb/package.py
@@ -15,6 +15,7 @@ class PyBcsb(PythonPackage):
     version("develop", branch="develop")
     version("2.0.0", tag="v2.0.0")
     version("2.1.0", tag="v2.1.0")
+    version("2.1.1", tag="v2.1.1")
 
     depends_on("python@3.10:", type=("build", "run"))
     depends_on("py-setuptools", type=("build", "run"))
