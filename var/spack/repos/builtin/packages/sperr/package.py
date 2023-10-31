1872e4d5901588bc492b12cf5277d6dfc03e5eb6
Samuel Li <sam@cisl-m121a>
2023-10-30 23:21:24 -0600

    address review comments

--- var/spack/repos/builtin/packages/sperr/package.py
+++ var/spack/repos/builtin/packages/sperr/package.py
@@ -22,16 +22,17 @@ class Sperr(CMakePackage):
     version("0.6.2", sha256="d986997e2d79a1f27146ad02c623359976a1e72a1ab0d957e128d430cda3782d")
     version("0.5", sha256="20ad48c0e7599d3e5866e024d0c49648eb817f72ad5459f5468122cf14a97171")
 
+    # Variants
+    variant("shared", description="build shared libaries", default=True)
+    variant("openmp", description="use OpenMP in 3D inputs", default=True)
+    variant("utilities", description="build SPERR CLI utilities", default=True)
+    variant("zstd", description="use ZSTD for more compression", default=True, when="@:0.6.2")
+    variant("bundle_zstd", description="Use SPERR bundled ZSTD. Keep it off in SPACK builds.", default=False, when="@:0.6.2")
+
+    # Depend ons
     depends_on("git", type="build")
     depends_on("pkgconfig", type=("build"), when="+zstd")
-    # only need zstd if version is up to 0.6.2
     depends_on("zstd", type=("build", "link"), when="@:0.6.2+zstd")
-
-    variant("shared", description="build shared libaries", default=True)
-    variant("openmp", description="use openmp in 3D inputs", default=True)
-    variant("utilities", description="build SPERR CLI utilities", default=True)
-    # only take zstd variant if version is up to 0.6.2
-    variant("zstd", description="use zstd for more compression", default=True, when="@:0.6.2")
 
     def cmake_args(self):
         # ensure the compiler supports OpenMP if it is used
@@ -43,7 +44,7 @@ class Sperr(CMakePackage):
             self.define_from_variant("USE_OMP", "openmp"),
             self.define_from_variant("BUILD_CLI_UTILITIES", "utilities"),
             self.define_from_variant("USE_ZSTD", "zstd"),
-            "-DUSE_BUNDLED_ZSTD=OFF", # not in use since version 0.7.x
+            self.define_from_variant("USE_BUNDLED_ZSTD", "bundle_zstd"),
             "-DSPERR_PREFER_RPATH=OFF",
             "-DBUILD_UNIT_TESTS=OFF",
         ]
