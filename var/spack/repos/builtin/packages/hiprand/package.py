209e3a20292396e641ba9a6e01cae1d4faad39a9
sreenivasa murthy kolam <sreenivasamurthy.kolam@amd.com>
2023-10-31 04:22:02 +0000

    using the checksum for hiprand-5.7.1

--- var/spack/repos/builtin/packages/hiprand/package.py
+++ var/spack/repos/builtin/packages/hiprand/package.py
@@ -14,7 +14,7 @@ class Hiprand(CMakePackage, CudaPackage, ROCmPackage):
 
     homepage = "https://github.com/ROCmSoftwarePlatform/hipRAND"
     git = "https://github.com/ROCmSoftwarePlatform/hipRAND.git"
-    url = "https://github.com/ROCmSoftwarePlatform/hipRAND/archive/rocm-5.5.0.tar.gz"
+    url = "https://github.com/ROCmSoftwarePlatform/hipRAND/archive/rocm-5.7.1.tar.gz"
     tags = ["rocm"]
 
     maintainers("cgmb", "srekolam", "renjithravindrankannath")
@@ -22,7 +22,7 @@ class Hiprand(CMakePackage, CudaPackage, ROCmPackage):
 
     version("develop", branch="develop")
     version("master", branch="master")
-    version("5.7.1", commit="8babdbe0cf4dc330c9ec5a0231ac059187a7bc8a")
+    version("5.7.1", sha256="81a9f5f0960dce125ce1ab1c7eb58bb07c8756346f9e46a1cc65aa61d5a114f8")
     version("5.7.0", sha256="4dee76719839503b02ce7d38e1c61bbdb2da18da7f63a7ef7012c84c71aa0a9d")
     version("5.6.1", sha256="a73d5578bc7f8dff0b8960e4bff97bc4fc28f508a19ed6acd1cfd4d3e76b47ee")
     version("5.6.0", sha256="8c214e2f90337a5317a69950026bf337b1e567d43bb9ae64f2a802af2228c313")
