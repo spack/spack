cfd152b5db9c1c1456d8f3f95afe3820a14ef4c0
Harmen Stoppels <me@harmenstoppels.nl>
2023-10-31 14:17:25 +0100

    wording

--- lib/spack/spack/cmd/checksum.py
+++ lib/spack/spack/cmd/checksum.py
@@ -70,7 +70,7 @@ def setup_parser(subparser):
     subparser.add_argument(
         "versions",
         nargs="*",
-        help="checksum only specific version (if ommitted, Spack will search for remote versions)",
+        help="checksum these specific versions (if ommitted, Spack searches for remote versions)",
     )
     arguments.add_common_arguments(subparser, ["jobs"])
     subparser.epilog = (
