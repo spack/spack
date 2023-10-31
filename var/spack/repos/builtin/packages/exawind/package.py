aa6e417c4b07f580a675f170f8b01ed0b5602024
psakiev <psakiev@sandia.gov>
2023-10-31 10:52:16 -0600

    Exawind: fix license header

--- var/spack/repos/builtin/packages/exawind/package.py
+++ var/spack/repos/builtin/packages/exawind/package.py
@@ -1,9 +1,7 @@
-# Copyright (c) 2022, National Technology & Engineering Solutions of Sandia,
-# LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
-# Government retains certain rights in this software.
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# This software is released under the BSD 3-clause license. See LICENSE file
-# for more details.
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
 
