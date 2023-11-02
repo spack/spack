62473165141df093919c6bca377a72d79ecd5b3e
Martin Aumüller <aumuell@reserv.at>
2023-11-02 11:00:54 +0100

    libtheora: add git versions
    stable as version name for theora-1.1 branch was chosen so that it sorts between 1.1.x and master

--- var/spack/repos/builtin/packages/libtheora/package.py
+++ var/spack/repos/builtin/packages/libtheora/package.py
@@ -17,7 +17,10 @@ class Libtheora(AutotoolsPackage, MSBuildPackage):
 
     homepage = "https://www.theora.org"
     url = "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.xz"
+    git = "https://gitlab.xiph.org/xiph/theora.git"
 
+    version("master", branch="master")
+    version("stable", branch="theora-1.1")
     version("1.1.1", sha256="f36da409947aa2b3dcc6af0a8c2e3144bc19db2ed547d64e9171c59c66561c61")
     version("1.1.0", sha256="3d7b4fb1c115f1a530afd430eed2e8861fa57c8b179ec2d5a5d8f1cd0c7a4268")
 
@@ -47,7 +50,7 @@ class Libtheora(AutotoolsPackage, MSBuildPackage):
     patch(
         "https://gitlab.xiph.org/xiph/theora/-/commit/7288b539c52e99168488dc3a343845c9365617c8.diff",
         sha256="e01ef71a1c19783a0b323b90a625e5c360ddb7ee03d2b6c201f1519f1704ea11",
-        when="^libpng@1.6:",
+        when="@:1.1.1 ^libpng@1.6:",
     )
     # add -no-undefined
     patch(
