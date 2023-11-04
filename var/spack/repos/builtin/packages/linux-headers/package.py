88ee3a0fba3cc806d2cddbad9740dd2c67ac8a4e
zv-io <30916613+zv-io@users.noreply.github.com>
2023-11-04 06:21:12 -0500

    linux-headers: support multiple versions (#40877)
    The download URL for linux-headers was hardcoded to 4.x;
    we need to derive the correct URL from the version number.

--- var/spack/repos/builtin/packages/linux-headers/package.py
+++ var/spack/repos/builtin/packages/linux-headers/package.py
@@ -20,6 +20,10 @@ class LinuxHeaders(Package):
     version("6.2.8", sha256="fed0ad87d42f83a70ce019ff2800bc30a855e672e72bf6d54a014d98d344f665")
     version("4.9.10", sha256="bd6e05476fd8d9ea4945e11598d87bc97806bbc8d03556abbaaf809707661525")
 
+    def url_for_version(self, version):
+        url = "https://www.kernel.org/pub/linux/kernel/v{0}.x/linux-{1}.tar.xz"
+        return url.format(version.up_to(1), version)
+
     def setup_build_environment(self, env):
         # This variable is used in the Makefile. If it is defined on the
         # system, it can break the build if there is no build recipe for
