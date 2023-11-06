1fa5d4bdc3386c21ac1075f5f0d0a6a7649c07ef
m-shunji <m.shunji@fujitsu.com>
2023-11-06 15:13:25 +0900

    blitz: fix for fujitsu compiler

--- var/spack/repos/builtin/packages/blitz/package.py
+++ var/spack/repos/builtin/packages/blitz/package.py
@@ -17,6 +17,15 @@ class Blitz(AutotoolsPackage):
     version("1.0.2", sha256="500db9c3b2617e1f03d0e548977aec10d36811ba1c43bb5ef250c0e3853ae1c2")
 
     depends_on("python@3:", type="build")
+    depends_on("m4", type="build")
+    depends_on("autoconf", type="build")
+    depends_on("automake", type="build")
+    depends_on("libtool", type="build")
+
+    patch("fujitsu_compiler_specfic_header.patch", when="%fj")
+
+    def autoreconf(self, spec, prefix):
+        autoreconf("--install", "--verbose", "--force")
 
     build_targets = ["lib"]
 
