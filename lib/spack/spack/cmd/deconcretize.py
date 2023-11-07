51c40dd10fd7263f163c443924dcf21b390fba17
Gregory Becker <becker33@llnl.gov>
2023-11-06 17:02:32 -0800

    deconcretize: update interface and add tests

--- lib/spack/spack/cmd/deconcretize.py
+++ lib/spack/spack/cmd/deconcretize.py
@@ -17,13 +17,7 @@ import spack.traverse as traverse
 
 description = "remove specs from the concretized lockfile of an environment"
 section = "environments"
-level = "short"
-
-error_message = """You can either:
-    a) use a more specific spec, or
-    b) specify the spec by its hash (e.g. `spack deconcretize /hash`), or
-    c) use `spack deconcretize --all` to deconcretize ALL matching specs.
-"""
+level = "long"
 
 # Arguments for display_specs when we find ambiguity
 display_args = {"long": True, "show_flags": False, "variants": False, "indent": 4}
@@ -31,99 +25,48 @@ display_args = {"long": True, "show_flags": False, "variants": False, "indent": 
 
 def setup_parser(subparser):
     subparser.add_argument(
-        "-f",
-        "--force",
+        "--root",
         action="store_true",
-        dest="force",
-        help="remove regardless of whether other packages or environments " "depend on this one",
+        help="Deconcretize only environment roots. Additional roots that depend on matching specs"
+        " will be ignored",
     )
-    arguments.add_common_arguments(subparser, ["recurse_dependents", "yes_to_all", "specs"])
+    arguments.add_common_arguments(subparser, ["yes_to_all", "specs"])
     subparser.add_argument(
         "-a",
         "--all",
         action="store_true",
         dest="all",
-        help="deconcretize ALL specs that match each supplied spec",
+        help="Deconcretize ALL specs that match each supplied spec",
     )
 
 
-def find_matching_specs(
-    env: ev.Environment, specs: List[spack.spec.Spec], allow_multiple_matches: bool = False
-):
-    """Returns a list of concrete specs in the environment matching the abstract specs from CLI
+def get_deconcretize_list(args, specs, env):
+    env_specs = [s for _, s in env.concretized_specs()]
+    to_deconcretize = []
+    errors = []
 
-    Args:
-        env: active environment
-        specs: list of specs to be matched against concrete environment
-        allow_multiple_matches: if True mulitple matches are permitted
-
-    Return:
-        list: list of specs
-    """
-    matching_specs = []
-    has_errors = False
-    for spec in specs:
-        if spec is any:
-            matching = list(env.all_specs())
+    for s in specs:
+        if args.root:
+            to_deconc = [e for e in env_specs if e.satisfies(s)]
         else:
-            matching = [s for s in env.all_specs() if s.satisfies(spec)]
+            to_deconc = [e for e in env_specs if any(d.satisfies(s) for d in e.traverse())]
 
-        if not allow_multiple_matches and len(matching) > 1:
+        if len(to_deconc) < 1:
+            tty.warn(f"No matching specs to deconcretize for {s}")
+
+        elif len(to_deconc) > 1 and not args.all:
+            errors.append((s, to_deconc))
+
+        to_deconcretize.extend(to_deconc)
+
+    if errors:
+        for spec, matching in errors:
             tty.error(f"{spec} matches multiple concrete specs:")
             sys.stderr.write("\n")
             spack.cmd.display_specs(matching, output=sys.stderr, **display_args)
             sys.stderr.write("\n")
             sys.stderr.flush()
-            has_errors = True
-
-        if len(matching) == 0 and spec is not any:
-            tty.die(f"{spec} does not match any concrete spec.")
-
-        matching_specs.extend(matching)
-
-    if has_errors:
-        tty.die(error_message)
-
-    return matching_specs
-
-
-def concrete_dependents(
-    specs: List[spack.spec.Spec], env: ev.Environment
-) -> List[spack.spec.Spec]:
-    # Note this traversal will not return any spec in the original list
-    return [
-        spec
-        for spec in traverse.traverse_nodes(
-            specs,
-            root=False,
-            order="breadth",
-            cover="nodes",
-            direction="parents",
-            key=lambda s: s.dag_hash(),
-        )
-        if spec.dag_hash() in env.concretized_order
-    ]
-
-
-def get_deconcretize_list(args, specs, env):
-    matching_specs = find_matching_specs(env, specs, args.all)
-    dependent_specs = concrete_dependents(matching_specs, env)
-    to_deconcretize = matching_specs + dependent_specs if args.dependents else matching_specs
-
-    dangling_dependents = dependent_specs and not args.dependents
-    has_error = not args.force and dangling_dependents
-
-    if has_error:
-        tty.info("Refusing to deconcretize the following specs")
-        spack.cmd.display_specs(matching_specs, **display_args)
-        print()
-        tty.info("The following dependents are still concrete in the environment:")
-        spack.cmd.display_specs(dependent_specs, **display_args)
-        tty.die(
-            "There are still dependents.",
-            "use `spack deconcretize --dependents` to deconcretize dependents too",
-            "use `spack deconcretize --force` to override",
-        )
+        tty.die("Use '--all' to deconcretize all matching specs, or be more specific")
 
     return to_deconcretize
 
@@ -131,11 +74,10 @@ def get_deconcretize_list(args, specs, env):
 def deconcretize_specs(args, specs):
     env = spack.cmd.require_active_env(cmd_name="deconcretize")
 
-    deconcretize_list = get_deconcretize_list(args, specs, env)
-
-    if not deconcretize_list:
-        tty.warn("There are no concrete specs to deconcretize.")
-        return
+    if args.specs:
+        deconcretize_list = get_deconcretize_list(args, specs, env)
+    else:
+        deconcretize_list = [s for _, s in env.concretized_specs()]
 
     if not args.yes_to_all:
         confirmation.confirm_action(deconcretize_list, "deconcretized", "deconcretization")
--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -401,7 +401,7 @@ _spack() {
     then
         SPACK_COMPREPLY="-h --help -H --all-help --color -c --config -C --config-scope -d --debug --timestamp --pdb -e --env -D --env-dir -E --no-env --use-env-repo -k --insecure -l --enable-locks -L --disable-locks -m --mock -b --bootstrap -p --profile --sorted-profile --lines -v --verbose --stacktrace --backtrace -V --version --print-shell-vars"
     else
-        SPACK_COMPREPLY="add arch audit blame bootstrap build-env buildcache cd change checksum ci clean clone commands compiler compilers concretize concretise config containerize containerise create debug dependencies dependents deprecate dev-build develop diff docs edit env extensions external fetch find gc gpg graph help info install license list load location log-parse maintainers make-installer mark mirror module patch pkg providers pydoc python reindex remove rm repo resource restage solve spec stage style tags test test-env tutorial undevelop uninstall unit-test unload url verify versions view"
+        SPACK_COMPREPLY="add arch audit blame bootstrap build-env buildcache cd change checksum ci clean clone commands compiler compilers concretize concretise config containerize containerise create debug deconcretize dependencies dependents deprecate dev-build develop diff docs edit env extensions external fetch find gc gpg graph help info install license list load location log-parse maintainers make-installer mark mirror module patch pkg providers pydoc python reindex remove rm repo resource restage solve spec stage style tags test test-env tutorial undevelop uninstall unit-test unload url verify versions view"
     fi
 }
 
@@ -935,6 +935,15 @@ _spack_debug_create_db_tarball() {
 
 _spack_debug_report() {
     SPACK_COMPREPLY="-h --help"
+}
+
+_spack_deconcretize() {
+    if $list_options
+    then
+        SPACK_COMPREPLY="-h --help --root -y --yes-to-all -a --all"
+    else
+        _all_packages
+    fi
 }
 
 _spack_dependencies() {
