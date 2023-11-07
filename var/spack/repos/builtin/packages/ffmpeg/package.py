8afb66458be2057f8858338dc7145df13bf4338d
Martin Aumüller <aumuell@reserv.at>
2023-11-07 16:47:31 +0100

    ffmpeg: all versions older than 6.0 require patch for binutils 2.41 compatibility

--- var/spack/repos/builtin/packages/ffmpeg/package.py
+++ var/spack/repos/builtin/packages/ffmpeg/package.py
@@ -119,7 +119,7 @@ class Ffmpeg(AutotoolsPackage):
     patch(
         "https://git.ffmpeg.org/gitweb/ffmpeg.git/commitdiff_plain/effadce6c756247ea8bae32dc13bb3e6f464f0eb",
         sha256="d1ea47c29968507fee772234bc734d29958b62ab92400801ef28559b538a9168",
-        when="@6.0",
+        when="@:6.0",
     )
 
     @property
