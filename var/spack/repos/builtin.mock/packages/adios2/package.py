4774a26b26ea9000bc14c954ca306323769b6b3f
Richarda Butler <39577672+RikkiButler20@users.noreply.github.com>
2023-11-07 12:04:41 -0800

    Propagate variant across nodes that don't have that variant (#38512)
    Before this PR, variant were not propagated to leaf nodes that could accept 
    the propagated value, if some intermediate node couldn't accept it.
    
    This PR fixes that issue by marking nodes as "candidate" for propagation
    and by setting the variant only if it can be accepted by the node.
    
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

---
+++ var/spack/repos/builtin.mock/packages/adios2/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class Adios2(Package):
+    """This packagae has the variants shared and
+    bzip2, both defaulted to True"""
+
+    homepage = "https://example.com"
+    url = "https://example.com/adios2.tar.gz"
+
+    version("2.9.1", sha256="ddfa32c14494250ee8a48ef1c97a1bf6442c15484bbbd4669228a0f90242f4f9")
+
+    variant("shared", default=True, description="Build shared libraries")
+    variant("bzip2", default=True, description="Enable BZip2 compression")
+
+    depends_on("bzip2")
