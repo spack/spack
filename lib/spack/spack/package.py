7a4df732e1a6b6eaf6d6a9675c5857e7e53d5445
Brian Van Essen <vanessen1@llnl.gov>
2023-11-09 10:08:37 -0800

    DiHydrogen, Hydrogen, and Aluminum CachedCMakePackage (#39714)

--- lib/spack/spack/package.py
+++ lib/spack/spack/package.py
@@ -32,6 +32,7 @@ from spack.build_systems.autotools import AutotoolsPackage
 from spack.build_systems.bundle import BundlePackage
 from spack.build_systems.cached_cmake import (
     CachedCMakePackage,
+    cmake_cache_filepath,
     cmake_cache_option,
     cmake_cache_path,
     cmake_cache_string,
