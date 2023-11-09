a8539e3c1bea9674023f75f777ebd0724403c3e2
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-11-09 13:08:04 -0800

    new release cpp-logger v0.0.2 (#40972)

--- var/spack/repos/builtin/packages/cpp-logger/package.py
+++ var/spack/repos/builtin/packages/cpp-logger/package.py
@@ -16,3 +16,4 @@ class CppLogger(CMakePackage):
     version("develop", branch="develop")
     version("master", branch="master")
     version("0.0.1", tag="v0.0.1", commit="d48b38ab14477bb7c53f8189b8b4be2ea214c28a")
+    version("0.0.2", tag="v0.0.2", commit="329a48401033d2d2a1f1196141763cab029220ae")
