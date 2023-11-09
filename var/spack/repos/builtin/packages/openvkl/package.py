54447f6e5991a090b1197cbe49726a39a3df76eb
Martin Aumüller <aumuell@reserv.at>
2023-11-09 14:54:22 +0100

    openvkl: add v2.0.0

--- var/spack/repos/builtin/packages/openvkl/package.py
+++ var/spack/repos/builtin/packages/openvkl/package.py
@@ -16,6 +16,7 @@ class Openvkl(CMakePackage):
 
     # maintainers("github_user1", "github_user2")
 
+    version("2.0.0", sha256="469c3fba254c4fcdd84f8a9763d2e1aaa496dc123b5a9d467cc0a561e284c4e6")
     version("1.3.2", sha256="7704736566bf17497a3e51c067bd575316895fda96eccc682dae4aac7fb07b28")
     version("1.3.1", sha256="c9cefb6c313f2b4c0331e9629931759a6bc204ec00deed6ec0becad1670a1933")
     version("1.3.0", sha256="c6d4d40e6d232839c278b53dee1e7bd3bd239c3ccac33f49b465fc65a0692be9")
