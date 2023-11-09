1cf2ebfe5ccec2aad8406d6bdc80ae3487662297
afzpatel <afzal.patel@amd.com>
2023-11-09 13:48:39 -0800

    remove develop version

--- var/spack/repos/builtin/packages/hip-tensor/package.py
+++ var/spack/repos/builtin/packages/hip-tensor/package.py
@@ -16,12 +16,11 @@ class HipTensor(CMakePackage, ROCmPackage):
 
     maintainers("srekolam", "afzpatel")
 
-    version("develop", branch="develop")
     version("master", branch="master")
     version("5.7.1", sha256="96743d4e695fe865aef4097ae31d9b4e42a2d5a92135a005b0d187d9c0b17645")
     version("5.7.0", sha256="4b17f6d43b17fe2dc1d0c61e9663d4752006f7898cc94231206444a1663eb252")
 
-    for ver in ["5.7.0", "5.7.1", "master", "develop"]:
+    for ver in ["5.7.0", "5.7.1", "master"]:
         depends_on("composable-kernel@" + ver, when="@" + ver)
 
     def setup_build_environment(self, env):
