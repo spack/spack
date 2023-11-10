541c34e0b42e289bf95a6840d1e4b0679961a966
Pavel Saviankou <p.saviankou@fz-juelich.de>
2023-11-10 12:08:38 +0100

    fixes style

--- var/spack/repos/builtin/packages/cube-blade/package.py
+++ var/spack/repos/builtin/packages/cube-blade/package.py
@@ -16,7 +16,11 @@ class CubeBlade(AutotoolsPackage):
     version("0.5", sha256="0770cd4c2e1b8f31bdb6dadf39232b709aee869835e12f9e1ac670d0b276a689")
     version("0.4", sha256="fae8214a76d98991d300a33384f7ffe2fbe6e0f7760bb62ae592ae5b215d389f")
     version("0.3", sha256="0320cb86d492c85c7cf74677bd467217def0422ef38f46b7108ee592db6f6deb")
-    version("0.2", sha256="ab3c5bbca79e2ec599166e75b3c96a8f6a18b3064414fc39e56f78aaae9c165c", deprecated=True )
+    version(
+            "0.2",
+            sha256="ab3c5bbca79e2ec599166e75b3c96a8f6a18b3064414fc39e56f78aaae9c165c",
+            deprecated=True,
+        )
 
     depends_on("cube@4.8:+gui", when="@0.5:")
     depends_on("cube@4.7:+gui", when="@0.4:")
