2d26fcbe71b50cabe9162b68f48654765237aa61
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2023-11-10 12:22:28 +0100

    Revert "Revert changes to ci.py (.cmake in artifacts)"
    This reverts commit 2202d9475b9745759c977c6b0a16477aa5328334.

--- lib/spack/spack/ci.py
+++ lib/spack/spack/ci.py
@@ -1554,8 +1554,15 @@ def copy_stage_logs_to_artifacts(job_spec: spack.spec.Spec, job_log_dir: str) ->
         return
 
     stage_dir = job_pkg.stage.path
+    cmake_cache = os.path.join(stage_dir, "*.cmake")
     tty.debug("stage dir: {0}".format(stage_dir))
-    for file in [job_pkg.log_path, job_pkg.env_mods_path, *job_pkg.builder.archive_files]:
+    file_list = [
+        job_pkg.log_path,
+        job_pkg.env_mods_path,
+        *job_pkg.builder.archive_files,
+        cmake_cache,
+    ]
+    for file in file_list:
         copy_files_to_artifacts(file, job_log_dir)
 
 
