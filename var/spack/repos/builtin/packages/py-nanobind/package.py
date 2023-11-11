09b6f3957dceef2de3860e0603ed829916768160
Matthew Archer <36638242+ma595@users.noreply.github.com>
2023-11-11 22:16:45 +0000

    update to latest version (#40905)

--- var/spack/repos/builtin/packages/py-nanobind/package.py
+++ var/spack/repos/builtin/packages/py-nanobind/package.py
@@ -24,6 +24,9 @@ class PyNanobind(PythonPackage):
 
     version("master", branch="master", submodules=True)
     version(
+        "1.8.0", tag="v1.8.0", commit="1a309ba444a47e081dc6213d72345a2fbbd20795", submodules=True
+    )
+    version(
         "1.7.0", tag="v1.7.0", commit="555ec7595c89c60ce7cf53e803bc226dc4899abb", submodules=True
     )
     version(
