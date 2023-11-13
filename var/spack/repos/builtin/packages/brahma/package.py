9347b46716f5944909f0c7a09f0f4fc959b2ed19
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-11-13 23:25:12 +0100

    Release Brahma v0.0.2 (#40994)

--- var/spack/repos/builtin/packages/brahma/package.py
+++ var/spack/repos/builtin/packages/brahma/package.py
@@ -15,11 +15,14 @@ class Brahma(CMakePackage):
 
     version("develop", branch="dev")
     version("master", branch="master")
+    version("0.0.2", tag="v0.0.2", commit="bac58d5aa8962a5c902d401fbf8021aff9104d3c")
     version("0.0.1", tag="v0.0.1", commit="15156036f14e36511dfc3f3751dc953540526a2b")
 
     variant("mpi", default=False, description="Enable MPI support")
-    depends_on("cpp-logger@0.0.1")
-    depends_on("gotcha@develop")
+    depends_on("cpp-logger@0.0.1", when="@:0.0.1")
+    depends_on("cpp-logger@0.0.2", when="@0.0.2:")
+    depends_on("gotcha@1.0.4", when="@:0.0.1")
+    depends_on("gotcha@1.0.5", when="@0.0.2:")
     depends_on("catch2@3.0.1")
 
     depends_on("mpi", when="+mpi")
