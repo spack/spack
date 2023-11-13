a0a63987412b76461e8b54642cd56e28b3823248
Adam J. Stewart <ajstewart426@gmail.com>
2023-11-13 03:33:33 -0600

    py-mypy: add v1.4:v1.7 (#41015)

--- var/spack/repos/builtin/packages/py-mypy/package.py
+++ var/spack/repos/builtin/packages/py-mypy/package.py
@@ -15,6 +15,13 @@ class PyMypy(PythonPackage):
 
     maintainers("adamjstewart")
 
+    version("1.7.0", sha256="1e280b5697202efa698372d2f39e9a6713a0395a756b1c6bd48995f8d72690dc")
+    version("1.6.1", sha256="4d01c00d09a0be62a4ca3f933e315455bde83f37f892ba4b08ce92f3cf44bcc1")
+    version("1.6.0", sha256="4f3d27537abde1be6d5f2c96c29a454da333a2a271ae7d5bc7110e6d4b7beb3f")
+    version("1.5.1", sha256="b031b9601f1060bf1281feab89697324726ba0c0bae9d7cd7ab4b690940f0b92")
+    version("1.5.0", sha256="f3460f34b3839b9bc84ee3ed65076eb827cd99ed13ed08d723f9083cada4a212")
+    version("1.4.1", sha256="9bbcd9ab8ea1f2e1c8031c21445b511442cc45c89951e49bbf852cbb70755b1b")
+    version("1.4.0", sha256="de1e7e68148a213036276d1f5303b3836ad9a774188961eb2684eddff593b042")
     version("1.3.0", sha256="e1f4d16e296f5135624b34e8fb741eb0eadedca90862405b1f1fde2040b9bd11")
     version("1.2.0", sha256="f70a40410d774ae23fcb4afbbeca652905a04de7948eaf0b1789c8d1426b72d1")
     version("1.1.1", sha256="ae9ceae0f5b9059f33dbc62dea087e942c0ccab4b7a003719cb70f9b8abfa32f")
@@ -43,31 +50,34 @@ class PyMypy(PythonPackage):
     version("0.670", sha256="e80fd6af34614a0e898a57f14296d0dacb584648f0339c2e000ddbf0f4cc2f8d")
 
     # pyproject.toml
-    depends_on("py-setuptools@40.6.2:", when="@0.790:", type=("build", "run"))
-    depends_on("py-setuptools", type=("build", "run"))
+    depends_on("py-setuptools@40.6.2:", when="@0.790:", type="build")
+    depends_on("py-setuptools", type="build")
     depends_on("py-wheel@0.30:", when="@0.790:", type="build")
+    depends_on("py-types-psutil", when="@0.981:", type="build")
+    depends_on("py-types-setuptools", when="@0.981:", type="build")
+
+    # setup.py
+    depends_on("python@3.8:", when="@1.5:", type=("build", "run"))
+    depends_on("python@3.7:", when="@0.981:", type=("build", "run"))
+    depends_on("py-typing-extensions@4.1:", when="@1.5:", type=("build", "run"))
     depends_on("py-typing-extensions@3.10:", when="@0.930:", type=("build", "run"))
     depends_on("py-typing-extensions@3.7.4:", when="@0.700:", type=("build", "run"))
     depends_on("py-mypy-extensions@1:", when="@1.1:", type=("build", "run"))
     depends_on("py-mypy-extensions@0.4.3:", when="@0.930:1.0", type=("build", "run"))
     depends_on("py-mypy-extensions@0.4.3:0.4", when="@0.700:0.929", type=("build", "run"))
     depends_on("py-mypy-extensions@0.4.0:0.4", when="@:0.699", type=("build", "run"))
-    depends_on("py-typed-ast@1.4.0:1", when="@0.920: ^python@:3.7", type=("build", "run"))
-    depends_on("py-typed-ast@1.4.0:1.4", when="@0.900:0.910 ^python@:3.7", type=("build", "run"))
-    depends_on("py-typed-ast@1.4.0:1.4", when="@0.700:0.899", type=("build", "run"))
-    depends_on("py-typed-ast@1.3.1:1.3", when="@:0.699", type=("build", "run"))
     depends_on("py-tomli@1.1:", when="@0.950: ^python@:3.10", type=("build", "run"))
     depends_on("py-tomli@1.1:", when="@0.930:0.949", type=("build", "run"))
     depends_on("py-tomli@1.1:2", when="@0.920:0.929", type=("build", "run"))
-    depends_on("py-types-psutil", when="@0.981:", type="build")
-    depends_on("py-types-setuptools", when="@0.981:", type="build")
-    depends_on("py-types-typed-ast@1.5.8:1.5", when="@0.981:", type="build")
-
-    # setup.py
-    depends_on("python@3.7:", when="@0.981:", type=("build", "run"))
 
     # Historical dependencies
+    depends_on("py-types-typed-ast@1.5.8.5:1.5", when="@1.2:1.4", type="build")
+    depends_on("py-types-typed-ast@1.5.8:1.5", when="@0.981:1.1", type="build")
     depends_on("py-toml", when="@0.900:0.910", type=("build", "run"))
+    depends_on("py-typed-ast@1.4.0:1", when="@0.920:1.4 ^python@:3.7", type=("build", "run"))
+    depends_on("py-typed-ast@1.4.0:1.4", when="@0.900:0.910 ^python@:3.7", type=("build", "run"))
+    depends_on("py-typed-ast@1.4.0:1.4", when="@0.700:0.899", type=("build", "run"))
+    depends_on("py-typed-ast@1.3.1:1.3", when="@:0.699", type=("build", "run"))
 
     # https://github.com/python/mypy/issues/13627
     conflicts("^python@3.10.7:", when="@:0.971")
