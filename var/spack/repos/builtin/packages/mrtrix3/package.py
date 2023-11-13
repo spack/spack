5fa2f3239118efb910a1e6e040abbc656aed3d34
Brendan Moloney <moloney.brendan@gmail.com>
2023-11-12 22:04:07 -0800

    mrtrix3: Fix some issues w/ 3.0.3 and add 3.0.4

--- var/spack/repos/builtin/packages/mrtrix3/package.py
+++ var/spack/repos/builtin/packages/mrtrix3/package.py
@@ -17,6 +17,10 @@ class Mrtrix3(Package):
     git = "https://github.com/MRtrix3/mrtrix3.git"
 
     version(
+        "3.0.4", 
+        sha256="f1d1aa289cfc3e46e3a8eca93594b23d061c6d50a0cd03727433a7e2cd14f71a",
+    )
+    version(
         "3.0.3",
         sha256="6ec7d5a567d8d7338e85575a74565189a26ec8971cbe8fb24a49befbc446542e",
         preferred=True,
@@ -27,10 +31,13 @@ class Mrtrix3(Package):
     depends_on("py-numpy", type=("build", "run"))
     depends_on("glu")
     depends_on("qt+opengl@4.7:")
-    depends_on("eigen")
+    depends_on("eigen@3.3", when="@3.0.3")
+    depends_on("eigen@3.4", when="@3.0.4:")
     depends_on("zlib-api")
     depends_on("libtiff")
     depends_on("fftw")
+
+    patch("fix_includes.patch", when="@3.0.3:")
 
     conflicts("%gcc@7:", when="@2017-09-25")  # MRtrix3/mrtrix3#1041
 
