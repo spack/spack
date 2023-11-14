533192143c425aba52c56adca6db14b649901a1f
LydDeb <lyderic.debusschere@eolen.com>
2023-11-14 10:56:49 +0100

    py-rapidfuzz: fix dependences; add py-rapidfuzz-capi and py-jarowinkler

--- var/spack/repos/builtin/packages/py-rapidfuzz/package.py
+++ var/spack/repos/builtin/packages/py-rapidfuzz/package.py
@@ -17,10 +17,12 @@ class PyRapidfuzz(PythonPackage):
     version("1.8.2", sha256="d6efbb2b6b18b3a67d7bdfbcd9bb72732f55736852bbef823bdf210f9e0c6c90")
 
     depends_on("python", type=("build", "link", "run"))
-    depends_on("py-setuptools@42:", when="@3:", type="build")
+    depends_on("py-setuptools@42:", when="@2:", type="build")
     depends_on("py-setuptools", type="build")
     depends_on("py-scikit-build@0.17", when="@3:", type="build")
     depends_on("py-scikit-build@0.13:", when="@2.2:", type="build")
+    depends_on("py-rapidfuzz-capi@1.0.5", when="@2", type="build")
+    depends_on("py-jarowinkler@1.2.0:1", when="@2", type=("build", "run"))
 
     # CMakeLists.txt
     depends_on("cmake@3.12:", type="build")
