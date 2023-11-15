125345c8073737e15ee6a4781406aea2f2ce9463
Sinan81 <Sinan81@users.noreply.github.com>
2023-11-15 07:33:47 +0000

    [@spackbot] updating style on behalf of Sinan81

--- var/spack/repos/builtin/packages/lemon/package.py
+++ var/spack/repos/builtin/packages/lemon/package.py
@@ -18,30 +18,29 @@ class Lemon(CMakePackage):
     version("1.3.1", sha256="71b7c725f4c0b4a8ccb92eb87b208701586cf7a96156ebd821ca3ed855bad3c8")
 
     # coin variant currently fails due to a missing dependency library
-    #variant("coin", default=False, description="Enable Coin solver backend") #TODO currently not building
+    # variant("coin", default=False, description="Enable Coin solver backend") #TODO currently not building
     variant("ilog", default=False, description="Enable ILOG (CPLEX) solver backend")
     variant("glpk", default=True, description="Enable GLPK solver backend")
     # soplex not mentioned in docs but shown in cmakecache
-    #variant("soplex", default=False, description="Enable SOPLEX solver backend") #TODO
-
+    # variant("soplex", default=False, description="Enable SOPLEX solver backend") #TODO
 
     depends_on("glpk", when="+glpk")
     depends_on("cplex", when="+ilog")
-    depends_on("coinutils", when="+coin") # just a guess
-    #depends_on("cbc", when="+coin")
-    #depends_on("clp", when="+coin")
-    #depends_on("bzip2", when="+coin")
-    #depends_on("soplex", when="+soplex") # no such package in Spack yet. TODO
+    depends_on("coinutils", when="+coin")  # just a guess
+    # depends_on("cbc", when="+coin")
+    # depends_on("clp", when="+coin")
+    # depends_on("bzip2", when="+coin")
+    # depends_on("soplex", when="+soplex") # no such package in Spack yet. TODO
 
     def cmake_args(self):
         spec = self.spec
         args = []
         args.extend(
             [
-                #f"-DLEMON_ENABLE_COIN={spec.variants['coin'].value}", #TODO
+                # f"-DLEMON_ENABLE_COIN={spec.variants['coin'].value}", #TODO
                 f"-DLEMON_ENABLE_ILOG={spec.variants['ilog'].value}",
                 f"-DLEMON_ENABLE_GLPK={spec.variants['glpk'].value}",
-                #f"-DLEMON_ENABLE_SOPLEX={spec.variants['soplex'].value}", #TODO
+                # f"-DLEMON_ENABLE_SOPLEX={spec.variants['soplex'].value}", #TODO
             ]
         )
         return args
