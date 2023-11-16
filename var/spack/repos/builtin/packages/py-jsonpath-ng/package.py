688ec0d44d956eb010ffea131e807cb9de873585
Erik Heeren <erik.heeren@epfl.ch>
2023-11-16 17:46:16 +0100

    py-jsonpath-ng: fix typo in version number

--- var/spack/repos/builtin/packages/py-jsonpath-ng/package.py
+++ var/spack/repos/builtin/packages/py-jsonpath-ng/package.py
@@ -15,7 +15,7 @@ class PyJsonpathNg(PythonPackage):
     homepage = "https://github.com/h2non/jsonpath-ng"
     pypi = "jsonpath-ng/jsonpath-ng-1.5.2.tar.gz"
 
-    version("1.6.9", sha256="5483f8e9d74c39c9abfab554c070ae783c1c8cbadf5df60d561bc705ac68a07e")
+    version("1.6.0", sha256="5483f8e9d74c39c9abfab554c070ae783c1c8cbadf5df60d561bc705ac68a07e")
     version("1.5.3", sha256="a273b182a82c1256daab86a313b937059261b5c5f8c4fa3fc38b882b344dd567")
     version("1.5.2", sha256="144d91379be14d9019f51973bd647719c877bfc07dc6f3f5068895765950c69d")
 
