44645a6bc88831fbad8cf6a40aa70d0498b2c4dc
Sven Goldberg <sven.goldberg@dlr.de>
2023-11-17 15:33:00 +0100

    heat: Changes after review

--- var/spack/repos/builtin/packages/py-heat/package.py
+++ var/spack/repos/builtin/packages/py-heat/package.py
@@ -11,19 +11,11 @@ class PyHeat(PythonPackage):
     and machine learning. It provides highly optimized algorithms and data structures for tensor
     computations using CPUs, GPUs and distributed cluster systems on top of MPI."""
 
-    homepage = "https://github.com/helmholtz-analytics/heat/"
-    url = "https://github.com/helmholtz-analytics/heat/archive/refs/tags/v1.3.0.tar.gz"
+    pypi="heat/heat-1.3.0.tar.gz"
 
     maintainers("mrfh92", "ClaudiaComito", "JuanPedroGHM")
 
-    version("1.3.0", sha256="2e66e82df87431075be633bc921a4953dd258dfa91ed31283c964bcb3dea1ffc")
-
-    depends_on("py-mpi4py@3:", type=("build", "run"))
-    depends_on("py-numpy@1.2:", type=("build", "run"))
-    depends_on("py-torch@1.8:2.0.1", type=("build", "run"))
-    depends_on("py-scipy@0.14:", type=("build", "run"))
-    depends_on("py-pillow@6:", type=("build", "run"))
-    depends_on("py-torchvision@0.8:", type=("build", "run"))
+    version("1.3.0", sha256="fa247539a559881ffe574a70227d3c72551e7c4a9fb29b0945578d6a840d1c87")
 
     variant("docutils", default=False, description="Use the py-docutils package")
     variant("hdf5", default=False, description="Use the py-h5py package needed for HDF5 support")
@@ -37,12 +29,20 @@ class PyHeat(PythonPackage):
         description="Use py-scikit-learn and py-matplotlib for the example tests",
     )
 
-    depends_on("py-docutils@0.16:", when="+docutils", type=("link"))
-    depends_on("py-h5py@2.8.0:", when="+hdf5", type=("link"))
-    depends_on("py-netcdf4@1.5.6:", when="+netcdf", type=("link"))
-    depends_on("py-pre-commit@1.18.3:", when="+dev", type=("link"))
-    depends_on("py-scikit-learn@0.24.0:", when="+examples", type=("link"))
-    depends_on("py-matplotlib@3.1.0:", when="+examples", type=("link"))
+    depends_on("python@3.8:")
+    depends_on("py-numpy@1.20:", type=("build", "run"))
+    depends_on("py-torch@1.8:2.0.1", type=("build", "run"))
+    depends_on("py-scipy@0.14:", type=("build", "run"))
+    depends_on("pil@6:", type=("build", "run"))
+    depends_on("py-torchvision@0.8:", type=("build", "run"))
+    depends_on("py-mpi4py@3:", type=("build", "run"))
+
+    depends_on("py-docutils@0.16:", when="+docutils", type=("build", "link", "run"))
+    depends_on("py-h5py@2.8.0:", when="+hdf5", type=("build", "link", "run"))
+    depends_on("py-netcdf4@1.5.6:", when="+netcdf", type=("build", "link", "run"))
+    depends_on("py-pre-commit@1.18.3:", when="+dev", type=("build", "link", "run"))
+    depends_on("py-scikit-learn@0.24.0:", when="+examples", type=("build", "link", "run"))
+    depends_on("py-matplotlib@3.1.0:", when="+examples", type=("build", "link", "run"))
 
     def install_options(self, spec, prefix):
         options = []
