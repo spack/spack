cd8c9e11c0ac3744650042f17a347e71745421d1
Gavin John <gavinnjohn@gmail.com>
2023-11-17 11:17:25 -0600

    Fat finger

--- var/spack/repos/builtin/packages/py-quast/package.py
+++ var/spack/repos/builtin/packages/py-quast/package.py
@@ -27,7 +27,7 @@ class PyQuast(PythonPackage):
     # for instance depends_on('boost +filesystem')
     # See https://github.com/spack/spack/pull/22303 for reference
     depends_on(Boost.with_default_variants)
-=    depends_on("perl@5.6.0:")
+    depends_on("perl@5.6.0:")
     depends_on("python@2.5:,3.3:")
     depends_on("py-setuptools", type="build")
     depends_on("py-matplotlib", type=("build", "run"))
