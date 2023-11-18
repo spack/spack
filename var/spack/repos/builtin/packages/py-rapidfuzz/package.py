b1d27677a6b7153bb41750dda2c08bcffcd0c472
Lydéric Debusschère <lyderic.de@gmail.com>
2023-11-18 15:28:56 +0100

    py-cleo: add versions 2.0.0 2.0.1; add maintainers (#40611)
    * py-cleo: add versions 2.0.0 2.0.1; add maintainers
    
    * py-cleo: add forgotten dependence
    
    * py-cleo: update from review: remove preferred version, remove a dependence, fix py-rapidfuzz version
    
    * py-cleo: deprecated version 1.0.0a5; add version 1.0.0; update dependences
    
    * py-cleo: add version 2.1.0; update version range of dependences
    
    * py-crashtest: add version 0.4.1, dependence of py-cleo
    
    * py-cleo: update dependence
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * py-cleo: update dependence py-clikit
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * py-cleo: update dependence py-rapidfuzz
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    
    * py-rapidfuzz: add version 2.2.0 dependence of py-cleo@2
    
    * py-cleo: fix version range of py-crashtest
    
    * py-rapidfuzz: fix dependences; add py-rapidfuzz-capi and py-jarowinkler
    
    ---------
    
    Co-authored-by: LydDeb <lyderic.debusschere@eolen.com>
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-rapidfuzz/package.py
+++ var/spack/repos/builtin/packages/py-rapidfuzz/package.py
@@ -13,12 +13,16 @@ class PyRapidfuzz(PythonPackage):
     pypi = "rapidfuzz/rapidfuzz-1.8.2.tar.gz"
 
     version("3.3.1", sha256="6783b3852f15ed7567688e2e358757a7b4f38683a915ba5edc6c64f1a3f0b450")
+    version("2.2.0", sha256="acb8839aac452ec61a419fdc8799e8a6e6cd21bed53d04678cdda6fba1247e2f")
     version("1.8.2", sha256="d6efbb2b6b18b3a67d7bdfbcd9bb72732f55736852bbef823bdf210f9e0c6c90")
 
     depends_on("python", type=("build", "link", "run"))
-    depends_on("py-setuptools@42:", when="@3:", type="build")
+    depends_on("py-setuptools@42:", when="@2:", type="build")
     depends_on("py-setuptools", type="build")
     depends_on("py-scikit-build@0.17", when="@3:", type="build")
+    depends_on("py-scikit-build@0.13:", when="@2.2:", type="build")
+    depends_on("py-rapidfuzz-capi@1.0.5", when="@2", type="build")
+    depends_on("py-jarowinkler@1.2.0:1", when="@2", type=("build", "run"))
 
     # CMakeLists.txt
     depends_on("cmake@3.12:", type="build")
