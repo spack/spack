22e6db2deeed0880740a705927dfeae54a516a06
jmcarcell <jmcarcell@users.noreply.github.com>
2023-11-20 16:04:20 +0100

    gaudi: add a patch for catch2

--- var/spack/repos/builtin/packages/gaudi/package.py
+++ var/spack/repos/builtin/packages/gaudi/package.py
@@ -60,6 +60,12 @@ class Gaudi(CMakePackage):
     patch("link_target_fixes.patch", when="@33.0:34")
     patch("link_target_fixes32.patch", when="@:32.2")
     patch("fmt_fix.patch", when="@36.6:36.12 ^fmt@10:")
+    # fix issues with catch2 3.1 and above
+    patch(
+          "https://gitlab.cern.ch/gaudi/Gaudi/-/commit/110f2189f386c3a23150ccdfdc47c1858fc7098e.patch?full_index=1",
+          sha256="f63276e9c4b6a51ad52507ac54f85e81b0564076e6a08b4243300bd79ba8a7d0",
+          when="@37.1 ^catch2@3.1:",
+    )
 
     # These dependencies are needed for a minimal Gaudi build
     depends_on("aida")
