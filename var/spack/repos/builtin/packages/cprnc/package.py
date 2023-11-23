146a5e293f60310550070a30e55f8ab5b2e049a7
James Edwards <jedwards@ucar.edu>
2023-11-23 09:50:55 -0700

    address style errors

--- var/spack/repos/builtin/packages/cprnc/package.py
+++ var/spack/repos/builtin/packages/cprnc/package.py
@@ -5,13 +5,14 @@
 
 from spack.package import *
 
+
 class Cprnc(CMakePackage):
     """CPRNC is a netcdf file comparison tool used by CESM
     and other scientific programs."""
-    
+
     url = "https://github.com/ESMCI/cprnc/archive/refs/tags/v1.0.1.tar.gz"
     homepage =  "https://github.com/ESMCI/cprnc"
-
+    
     maintainers("jedwards4b", "billsacks")
 
     version("1.0.1", sha256="19517b52688f5ce40c385d7a718e06bf88a8731335943bc32e2b8410c489d6eb")
