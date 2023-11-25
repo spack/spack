1adf2cab360af8711125dc08fe7aee7e9c6fa317
Erik Heeren <erik.heeren@epfl.ch>
2023-11-25 15:51:13 +0100

    py-dictdiffer: version bump (#41080)
    * py-dictdiffer: version bump
    
    * py-dictdiffer: removed runtime py-setuptools dependency in 0.9.0
    
    * Update var/spack/repos/builtin/packages/py-dictdiffer/package.py
    
    Co-authored-by: Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>

--- var/spack/repos/builtin/packages/py-dictdiffer/package.py
+++ var/spack/repos/builtin/packages/py-dictdiffer/package.py
@@ -12,9 +12,11 @@ class PyDictdiffer(PythonPackage):
     homepage = "https://github.com/inveniosoftware/dictdiffer"
     pypi = "dictdiffer/dictdiffer-0.8.1.tar.gz"
 
+    version("0.9.0", sha256="17bacf5fbfe613ccf1b6d512bd766e6b21fb798822a133aa86098b8ac9997578")
     version("0.8.1", sha256="1adec0d67cdf6166bda96ae2934ddb5e54433998ceab63c984574d187cc563d2")
 
     depends_on("python@2.7:2.8,3.4:", type=("build", "run"))
-    depends_on("py-setuptools", type=("build", "run"))
+    depends_on("py-setuptools", type="build", when="@0.9:")
+    depends_on("py-setuptools", type=("build", "run"), when="@:0.8")
     depends_on("py-setuptools-scm@3.1.0:", type="build")
     depends_on("py-pytest-runner@2.7:", type="build")
