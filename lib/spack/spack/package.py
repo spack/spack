c482534c1d3168da8053c4c47a203da300054a71
Alec Scott <hi@alecbcs.com>
2023-11-27 13:15:16 -0700

    CargoPackage: add new build system for Cargo packages (#41192)
    Co-authored-by: Tom Scogland <scogland1@llnl.gov>

--- lib/spack/spack/package.py
+++ lib/spack/spack/package.py
@@ -37,6 +37,7 @@ from spack.build_systems.cached_cmake import (
     cmake_cache_path,
     cmake_cache_string,
 )
+from spack.build_systems.cargo import CargoPackage
 from spack.build_systems.cmake import CMakePackage, generator
 from spack.build_systems.cuda import CudaPackage
 from spack.build_systems.generic import Package
