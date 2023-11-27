a8a36a4dbe42036a5681bd3a91488cc17a7aa3fe
Adam J. Stewart <ajstewart426@gmail.com>
2023-11-27 20:39:47 +0100

    Deprecate older tokenizers

--- var/spack/repos/builtin/packages/py-tokenizers/package.py
+++ var/spack/repos/builtin/packages/py-tokenizers/package.py
@@ -17,8 +17,16 @@ class PyTokenizers(PythonPackage):
     version("0.13.3", sha256="2e546dbb68b623008a5442353137fbb0123d311a6d7ba52f2667c8862a75af2e")
     version("0.13.1", sha256="3333d1cee5c8f47c96362ea0abc1f81c77c9b92c6c3d11cbf1d01985f0d5cf1d")
     version("0.10.3", sha256="1a5d3b596c6d3a237e1ad7f46c472d467b0246be7fd1a364f12576eb8db8f7e6")
-    version("0.6.0", sha256="1da11fbfb4f73be695bed0d655576097d09a137a16dceab2f66399716afaffac")
-    version("0.5.2", sha256="b5a235f9c71d04d4925df6c4fa13b13f1d03f9b7ac302b89f8120790c4f742bc")
+    version(
+        "0.6.0",
+        sha256="1da11fbfb4f73be695bed0d655576097d09a137a16dceab2f66399716afaffac",
+        deprecated=True,
+    )
+    version(
+        "0.5.2",
+        sha256="b5a235f9c71d04d4925df6c4fa13b13f1d03f9b7ac302b89f8120790c4f742bc",
+        deprecated=True,
+    )
 
     # TODO: This package currently requires internet access to install.
     depends_on("py-maturin@1", when="@0.14:", type="build")
@@ -36,4 +44,4 @@ class PyTokenizers(PythonPackage):
     # A nightly or dev version of rust is required to build older versions.
     # https://github.com/huggingface/tokenizers/issues/176
     # https://github.com/PyO3/pyo3/issues/5
-    depends_on("rust@nightly", when="@:0.10", type="build")
+    depends_on("rust@nightly", when="@:0.8", type="build")
