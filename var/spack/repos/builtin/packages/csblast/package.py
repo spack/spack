2ea4dc85fc18c726e7b6678c8d31d108798ba382
Gabriel Cretin <gabrielcretin@gmail.com>
2023-11-27 09:47:28 +0100

    csblast: compile instead of using precompiled

--- var/spack/repos/builtin/packages/csblast/package.py
+++ var/spack/repos/builtin/packages/csblast/package.py
@@ -7,16 +7,32 @@
 from spack.package import *
 
 
-class Csblast(Package):
-    """FIXME: Put a proper description of your package here."""
+class Csblast(MakefilePackage):
+    """Context-Specific BLAST is a tool that searches a protein sequence
+    that extends BLAST (Basic Local Alignment Search Tool)
+    using context-specific mutation probabilities
+    """
 
-    homepage = "http://wwwuser.gwdg.de/~compbiol/data/csblast/releases"
-    url = "http://wwwuser.gwdg.de/~compbiol/data/csblast/releases/csblast-2.2.3_linux64.tar.gz"
+    homepage = "https://github.com/cangermueller/csblast"
+    url = "https://github.com/cangermueller/csblast/archive/refs/tags/v2.2.3.tar.gz"
 
-    version("2.2.3", sha256="36fae7a9d2e5673015d19d34d2d55551b4e37bc3db2fcb7f2380ac6eba556599")
+    version("2.2.4", sha256="76848da4d45a618ae903cafc00ff6387e7decb17b839aca83d9a9438537edf0d")
+    version("2.2.3", sha256="3cf8dc251e85af6942552eae3d33e45a5a1c6d73c5e7f1a00ce26d6974c0d434")
+
+    depends_on("sparsehash", type=("build", "run"))
+
+    def setup_build_environment(self, env):
+        env.prepend_path("LD_LIBRARY_PATH", self.spec["sparsehash"].prefix.include)
+
+    def build(self, spec, prefix):
+        with working_dir(join_path(self.stage.source_path, "src")):
+            filter_file(
+                "INC = -I\$\$HOME\/include",
+                f"INC = -I{self.spec['sparsehash'].prefix.include}",
+                "Makefile",
+            )
+            make("csblast")
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        mkdirp(prefix.data)
         install_tree("bin", prefix.bin)
-        install_tree("data", prefix.data)
