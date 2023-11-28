677240cb151101290695b8ae6488c6705dbbc57e
sreenivasa murthy kolam <sreenivasamurthy.kolam@amd.com>
2023-11-28 10:57:11 +0000

    add support for gfx941,gfx942 for rocm-5.7.0 release onwards

--- lib/spack/spack/build_systems/rocm.py
+++ lib/spack/spack/build_systems/rocm.py
@@ -108,6 +108,8 @@ class ROCmPackage(PackageBase):
         "gfx90a:xnack+",
         "gfx90c",
         "gfx940",
+        "gfx941",
+        "gfx942",
         "gfx1010",
         "gfx1011",
         "gfx1012",
@@ -167,7 +169,7 @@ class ROCmPackage(PackageBase):
     depends_on("llvm-amdgpu@4.3.0:", when="amdgpu_target=gfx90a")
     depends_on("llvm-amdgpu@4.3.0:", when="amdgpu_target=gfx90a:xnack-")
     depends_on("llvm-amdgpu@4.3.0:", when="amdgpu_target=gfx90a:xnack+")
-    depends_on("llvm-amdgpu@5.2.0:", when="amdgpu_target=gfx940")
+    depends_on("llvm-amdgpu@5.7.0:", when="amdgpu_target=gfx940")
     depends_on("llvm-amdgpu@4.5.0:", when="amdgpu_target=gfx1013")
     depends_on("llvm-amdgpu@3.8.0:", when="amdgpu_target=gfx1030")
     depends_on("llvm-amdgpu@3.9.0:", when="amdgpu_target=gfx1031")
@@ -180,6 +182,8 @@ class ROCmPackage(PackageBase):
     depends_on("llvm-amdgpu@5.3.0:", when="amdgpu_target=gfx1101")
     depends_on("llvm-amdgpu@5.3.0:", when="amdgpu_target=gfx1102")
     depends_on("llvm-amdgpu@5.3.0:", when="amdgpu_target=gfx1103")
+    depends_on("llvm-amdgpu@5.7.0:", when="amdgpu_target=gfx941")
+    depends_on("llvm-amdgpu@5.7.0:", when="amdgpu_target=gfx942")
 
     # Compiler conflicts
 
