7c722d5a1a20e268b8fd4b8bd8fefca9a557e351
LydDeb <lyderic.debusschere@eolen.com>
2023-11-28 11:01:45 +0100

    py-poetry-core: Add version 1.7.0

--- var/spack/repos/builtin/packages/py-poetry-core/package.py
+++ var/spack/repos/builtin/packages/py-poetry-core/package.py
@@ -12,6 +12,7 @@ class PyPoetryCore(PythonPackage):
     homepage = "https://github.com/python-poetry/poetry-core"
     pypi = "poetry-core/poetry_core-1.6.1.tar.gz"
 
+    version("1.7.0", sha256="8f679b83bd9c820082637beca1204124d5d2a786e4818da47ec8acefd0353b74")
     version("1.6.1", sha256="0f9b0de39665f36d6594657e7d57b6f463cc10f30c28e6d1c3b9ff54c26c9ac3")
     version("1.2.0", sha256="ceccec95487e46c63a41761fbac5211b809bca22658e25a049f4c7da96269f71")
     version("1.1.0", sha256="d145ae121cf79118a8901b60f2c951c4edcc16f55eb8aaefc156aa33aa921f07")
@@ -20,7 +21,7 @@ class PyPoetryCore(PythonPackage):
 
     depends_on("python@3.7:3", when="@1.1.0:", type=("build", "run"))
     depends_on("python@:3", type=("build", "run"))
-    depends_on("py-importlib-metadata@1.7:", when="@1.1: ^python@:3.7", type=("build", "run"))
+    depends_on("py-importlib-metadata@1.7:", when="@1.1:1.6 ^python@:3.7", type=("build", "run"))
     depends_on("py-importlib-metadata@1.7:1", when="@:1.0 ^python@:3.7", type=("build", "run"))
 
     def url_for_version(self, version):
