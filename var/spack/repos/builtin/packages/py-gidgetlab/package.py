16cf115b2e8795a63df79d3afae30b9646a03187
Alec Scott <scott112@llnl.gov>
2023-11-29 11:10:57 -0800

    gidgetlab: add new package

---
+++ var/spack/repos/builtin/packages/py-gidgetlab/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class PyGidgetlab(PythonPackage):
+    """An asynchronous GitLab API library."""
+
+    homepage = "https://gitlab.com/beenje/gidgetlab"
+    pypi = "gidgetlab/gidgetlab-1.1.0.tar.gz"
+
+    maintainers("alecbcs")
+
+    license("Apache-2.0")
+
+    version("1.1.0", sha256="314ec2cddc898317ec45d99068665dbf33c0fee1f52df6671f28ad35bb51f902")
+
+    depends_on("py-setuptools@45:", type=("build", "run"))
+    depends_on("py-setuptools-scm@6.2:", type="build")
+
+    depends_on("py-aiohttp", type=("build", "run"))
+    depends_on("py-cachetools", type=("build", "run"))
