28f5ff8dd9cd17337e89359b3606cf72733cb84e
Christopher Christofi <77968333+ChristopherChristofi@users.noreply.github.com>
2023-11-30 20:37:26 +0000

    perl-parse-yapp: add new package with version 1.21 (#41348)

---
+++ var/spack/repos/builtin/packages/perl-parse-yapp/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PerlParseYapp(PerlPackage):
+    """Parse::Yapp - Perl extension for generating and using LALR parsers."""
+
+    homepage = "https://metacpan.org/pod/Parse::Yapp"
+    url = "https://cpan.metacpan.org/authors/id/W/WB/WBRASWELL/Parse-Yapp-1.21.tar.gz"
+
+    version("1.21", sha256="3810e998308fba2e0f4f26043035032b027ce51ce5c8a52a8b8e340ca65f13e5")
+
+    depends_on("perl-extutils-makemaker", type="build")
