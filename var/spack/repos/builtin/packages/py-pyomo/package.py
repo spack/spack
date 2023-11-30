e254f541e0f23a45290cdd85c40b2bd2208f0779
Miranda Mundt <mmundt@sandia.gov>
2023-11-30 07:23:51 -0700

    Add Pyomo 6.7.0

--- var/spack/repos/builtin/packages/py-pyomo/package.py
+++ var/spack/repos/builtin/packages/py-pyomo/package.py
@@ -18,6 +18,7 @@ class PyPyomo(PythonPackage):
     # Maintainer accurate as of 11/21/2023
     maintainers("mrmundt")
 
+    version("6.7.0", sha256="a245ec609ef2fd907269f0b8e0923f74d5bf868b2ec0e62bf2a30b3f253bd17b")
     version("6.6.2", sha256="c8ad55213ff8b1a2c4e469110db8079722d5a6f364c6c46a42e2f750fc9e4d26")
     version("6.6.1", sha256="3fb0aba7b0f4120e6ce0f242502c0e61478d61e326bc90b7dc392bbefd114b34")
     version("6.6.0", sha256="8766c08041b8d91fbc5166d220c9e723fed6057d18be1178bae3b6583376c65e")
@@ -51,7 +52,7 @@ class PyPyomo(PythonPackage):
     variant("optional", default=False, description="Install optional dependencies. (Version 6.1+)")
 
     # python_requires
-    depends_on("python@3.8:3.11", when="@6.7:", type=("build", "run"))
+    depends_on("python@3.8:3.12", when="@6.7:", type=("build", "run"))
     depends_on("python@3.7:3.11", when="@6.4:6.6", type=("build", "run"))
     depends_on("python@3.6:3.10", when="@6.3", type=("build", "run"))
     depends_on("python@3.6:3.9", when="@6.0:6.2", type=("build", "run"))
