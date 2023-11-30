296731679e90210b1d2807655f1e1c7aec82ea86
LydDeb <lyderic.debusschere@eolen.com>
2023-11-30 08:16:21 +0100

    py-sphinxcontrib-moderncmakedomain: add version 3.26.4 and 3.27.0

--- var/spack/repos/builtin/packages/py-sphinxcontrib-moderncmakedomain/package.py
+++ var/spack/repos/builtin/packages/py-sphinxcontrib-moderncmakedomain/package.py
@@ -14,7 +14,10 @@ class PySphinxcontribModerncmakedomain(PythonPackage):
 
     maintainers("LydDeb")
 
+    version("3.27.0", sha256="51e259e91f58d17cc0fac9307fd40106aa59d5acaa741887903fc3660361d1a1")
+    version("3.26.4", sha256="c4a62d586ed1a9baf1790b816fcc04c249dd3ac239bc7c7b79663951a0a463b8")
     version("3.25.0", sha256="4138e4d3f60e5c4b3982caa10033693bfc1009cdd851766754d5990d9d1e992a")
 
     depends_on("py-hatchling", type="build")
     depends_on("py-sphinx", type=("build", "run"))
+    depends_on("py-sphinx@2:", when="@3.27:", type=("build", "run"))
