4b93c57d440b244532597d0c5b840a9f8b3412ec
Harmen Stoppels <me@harmenstoppels.nl>
2023-11-30 08:37:11 +0100

    argparse: make scope choices lazy s.t. validation in tests works (#41344)

--- lib/spack/spack/cmd/compiler.py
+++ lib/spack/spack/cmd/compiler.py
@@ -14,6 +14,7 @@ from llnl.util.tty.color import colorize
 import spack.compilers
 import spack.config
 import spack.spec
+from spack.cmd.common import arguments
 
 description = "manage compilers"
 section = "system"
@@ -22,8 +23,6 @@ level = "long"
 
 def setup_parser(subparser):
     sp = subparser.add_subparsers(metavar="SUBCOMMAND", dest="compiler_command")
-
-    scopes = spack.config.scopes()
 
     # Find
     find_parser = sp.add_parser(
@@ -47,7 +46,7 @@ def setup_parser(subparser):
     find_parser.add_argument("add_paths", nargs=argparse.REMAINDER)
     find_parser.add_argument(
         "--scope",
-        choices=scopes,
+        choices=arguments.ConfigScopeChoices(),
         metavar=spack.config.SCOPES_METAVAR,
         default=spack.config.default_modify_scope("compilers"),
         help="configuration scope to modify",
@@ -61,7 +60,7 @@ def setup_parser(subparser):
     remove_parser.add_argument("compiler_spec")
     remove_parser.add_argument(
         "--scope",
-        choices=scopes,
+        choices=arguments.ConfigScopeChoices(),
         metavar=spack.config.SCOPES_METAVAR,
         default=None,
         help="configuration scope to modify",
@@ -71,7 +70,7 @@ def setup_parser(subparser):
     list_parser = sp.add_parser("list", help="list available compilers")
     list_parser.add_argument(
         "--scope",
-        choices=scopes,
+        choices=arguments.ConfigScopeChoices(),
         metavar=spack.config.SCOPES_METAVAR,
         default=spack.config.default_list_scope(),
         help="configuration scope to read from",
@@ -82,7 +81,7 @@ def setup_parser(subparser):
     info_parser.add_argument("compiler_spec")
     info_parser.add_argument(
         "--scope",
-        choices=scopes,
+        choices=arguments.ConfigScopeChoices(),
         metavar=spack.config.SCOPES_METAVAR,
         default=spack.config.default_list_scope(),
         help="configuration scope to read from",
