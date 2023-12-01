f798071a119f20544e64f0964062bf943f502bf0
Manuela Kuhn <36827019+manuelakuhn@users.noreply.github.com>
2023-12-01 14:39:18 +0100

    py-urllib3: add 2.1.0 and 2.0.7 (#41358)

--- var/spack/repos/builtin/packages/py-urllib3/package.py
+++ var/spack/repos/builtin/packages/py-urllib3/package.py
@@ -14,6 +14,8 @@ class PyUrllib3(PythonPackage):
     pypi = "urllib3/urllib3-1.25.6.tar.gz"
     git = "https://github.com/urllib3/urllib3.git"
 
+    version("2.1.0", sha256="df7aa8afb0148fa78488e7899b2c59b5f4ffcfa82e6c54ccb9dd37c1d7b52d54")
+    version("2.0.7", sha256="c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84")
     version("2.0.6", sha256="b19e1a85d206b56d7df1d5e683df4a7725252a964e3993648dd0fb5a1c157564")
     version("2.0.5", sha256="13abf37382ea2ce6fb744d4dad67838eec857c9f4f57009891805e0b5e123594")
     version("1.26.12", sha256="3fa96cf423e6987997fc326ae8df396db2a8b7c667747d47ddd8ecba91f4a74e")
@@ -27,9 +29,11 @@ class PyUrllib3(PythonPackage):
     version("1.14", sha256="dd4fb13a4ce50b18338c7e4d665b21fd38632c5d4b1d9f1a1379276bd3c08d37")
 
     variant("brotli", default=False, when="@1.25:", description="Add Brotli support")
-    variant("secure", default=False, description="Add SSL/TLS support")
     variant("socks", default=False, when="@1.15:", description="SOCKS and HTTP proxy support")
+    # Historical variant
+    variant("secure", default=False, when="@:2.0", description="Add SSL/TLS support")
 
+    depends_on("python@3.8:", when="@2.1:", type=("build", "run"))
     depends_on("py-hatchling@1.6:1", when="@2:", type="build")
 
     with when("+brotli"):
@@ -38,6 +42,9 @@ class PyUrllib3(PythonPackage):
         # Historical dependencies
         depends_on("py-brotlipy@0.6:", when="@:1.26.8", type=("build", "run"))
 
+    depends_on("py-pysocks@1.5.6,1.5.8:1", when="+socks", type=("build", "run"))
+
+    # Historical dependencies
     with when("+secure"):
         depends_on("py-pyopenssl@17.1:", when="@2:", type=("build", "run"))
         depends_on("py-pyopenssl@0.14:", when="@1", type=("build", "run"))
@@ -47,8 +54,5 @@ class PyUrllib3(PythonPackage):
         depends_on("py-certifi", type=("build", "run"))
         depends_on("py-urllib3-secure-extra", when="@1.26.12:", type=("build", "run"))
 
-    depends_on("py-pysocks@1.5.6,1.5.8:1", when="+socks", type=("build", "run"))
-
-    # Historical dependencies
     depends_on("py-setuptools", when="@1", type="build")
     depends_on("python@3.6:3", when="@1.26.12:1", type=("build", "run"))
