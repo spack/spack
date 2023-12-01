f00ab84e90f5526c7e5cfb045ac874e61effad87
Felix Werner <Felix.Werner@mpi-hd.mpg.de>
2023-12-01 09:04:32 +0100

    healpix-cxx: add version 3.82.0.

--- var/spack/repos/builtin/packages/healpix-cxx/package.py
+++ var/spack/repos/builtin/packages/healpix-cxx/package.py
@@ -11,13 +11,24 @@ class HealpixCxx(AutotoolsPackage):
     Hierarchical Equal Area isoLatitude Pixelation of a sphere."""
 
     homepage = "https://healpix.sourceforge.io"
-    url = "https://ayera.dl.sourceforge.net/project/healpix/Healpix_3.50/healpix_cxx-3.50.0.tar.gz"
 
+    version("3.82.0", sha256="9c1b0bbbcf007359d1ef10ae3ae9a2f46c72a4eb0c2fdbb43683289002ba8552")
     version("3.50.0", sha256="6538ee160423e8a0c0f92cf2b2001e1a2afd9567d026a86ff6e2287c1580cb4c")
 
     depends_on("cfitsio")
-    depends_on("libsharp", type="build")
+    depends_on("libsharp", when="@3.50.0")
+    depends_on("libsharp2", when="@3.82.0:")
 
+    def url_for_version(self, version):
+        major, minor, patch = version
+        return f"https://sourceforge.net/projects/healpix/files/Healpix_{major}.{minor}/healpix_cxx-{major}.{minor}.{patch}.tar.gz/download"
+
+    @when("@3.82.0:")
+    def setup_build_environment(self, env):
+        env.set("SHARP_CFLAGS", "-I{0}".format(self.spec["libsharp2"].prefix.include))
+        env.set("SHARP_LIBS", "-L{0} -lsharp".format(self.spec["libsharp2"].prefix.lib))
+
+    @when("@3.50.0")
     def patch(self):
         spec = self.spec
         configure_fix = FileFilter("configure")
