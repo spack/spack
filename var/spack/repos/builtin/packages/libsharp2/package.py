6cfb7b09a8f17183ac204ba4a8fc18e3aafd516a
Felix Werner <Felix.Werner@mpi-hd.mpg.de>
2023-12-01 09:03:36 +0100

    Add libsharp2 from HEALPix.

---
+++ var/spack/repos/builtin/packages/libsharp2/package.py
@@ -0,0 +1,41 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Libsharp2(AutotoolsPackage):
+    """Libsharp2 is a code library for spherical harmonic transforms (SHTs) and
+    spin-weighted spherical harmonic transforms, which evolved from the libpsht
+    library. Because the upstream repository has no tags or releases, this
+    package tracks the versions found in HEALPix releases."""
+
+    variant("openmp", default=True, description="Build with openmp support")
+    variant("mpi", default=True, description="Build with MPI support")
+    variant("pic", default=True, description="Generate position-independent code (PIC)")
+
+    homepage = "https://gitlab.mpcdf.mpg.de/mtr/libsharp"
+    git = "https://gitlab.mpcdf.mpg.de/mtr/libsharp.git"
+
+    version("3.82.0", sha256="47629f057a2daf06fca3305db1c6950edb9e61bbe2d7ed4d98ff05809da2a127")
+
+    depends_on("autoconf", type="build")
+    depends_on("mpi", when="+mpi")
+
+    configure_directory = "src/common_libraries/libsharp"
+
+    def url_for_version(self, version):
+        major, minor, patch = version
+        return f"https://sourceforge.net/projects/healpix/files/Healpix_{major}.{minor}/healpix_cxx-{major}.{minor}.{patch}.tar.gz/download"
+
+    def configure_args(self):
+        args = []
+        if "+openmp" not in self.spec:
+            args.append("--disable-openmp")
+        if "+mpi" not in self.spec:
+            args.append("--disable-mpi")
+        if "+pic" in self.spec:
+            args.append("--enable-pic")
+        return args
