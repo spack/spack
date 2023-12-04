c0fd238bafe098fef6df8351ee9509c957c0c0e5
LydDeb <lyderic.debusschere@eolen.com>
2023-12-04 14:07:27 +0100

    py-sphinx-prompt: fix dependences with respect to reviewing

--- var/spack/repos/builtin/packages/py-sphinx-prompt/package.py
+++ var/spack/repos/builtin/packages/py-sphinx-prompt/package.py
@@ -16,9 +16,11 @@ class PySphinxPrompt(PythonPackage):
 
     version("1.8.0", sha256="47482f86fcec29662fdfd23e7c04ef03582714195d01f5d565403320084372ed")
 
+    depends_on("python@3.9:3.11", type=("build","run"))
+    depends_on("py-poetry@1.6", type=("build","run"))
     depends_on("py-poetry-core@1.0.0:", type="build")
     depends_on("py-poetry-plugin-tweak-dependencies-version", type="build")
     depends_on("py-poetry-dynamic-versioning", type="build")
-    depends_on("py-sphinx", type=("build", "run"))
-    depends_on("py-pygments", type=("build", "run"))
-    depends_on("py-docutils", type=("build", "run"))
+    depends_on("py-sphinx@7.2.5", type=("build", "run"))
+    depends_on("py-pygments@2.16.1", type=("build", "run"))
+    depends_on("py-docutils@0.20.1", type=("build", "run"))
