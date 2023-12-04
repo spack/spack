7cecd508b120bb51b22f3ab1b5c23d5ad4b55d1b
LydDeb <lyderic.debusschere@eolen.com>
2023-12-04 10:08:24 +0100

    py-python-pptx: use pil instead of pillow, remove version constraint on python

--- var/spack/repos/builtin/packages/py-python-pptx/package.py
+++ var/spack/repos/builtin/packages/py-python-pptx/package.py
@@ -17,8 +17,7 @@ class PyPythonPptx(PythonPackage):
     version("0.6.23", sha256="587497ff28e779ab18dbb074f6d4052893c85dedc95ed75df319364f331fedee")
     version("0.6.21", sha256="7798a2aaf89563565b3c7120c0acfe9aff775db0db3580544e3bf4840c2e378f")
 
-    depends_on("python@2:2.7,3.6:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-lxml@3.1.0:", type=("build", "run"))
-    depends_on("py-pillow@3.3.2:", type=("build", "run"))
+    depends_on("pil@3.3.2:", type=("build", "run"))
     depends_on("py-xlsxwriter@0.5.7:", type=("build", "run"))
