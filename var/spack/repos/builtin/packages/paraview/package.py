54afefc788383c5efcbe16904e7b7425d89997d4
John Biddiscombe <biddisco@cscs.ch>
2023-12-04 17:53:06 +0100

    Add EGL support to ParaView and Glew (#39800)
    * Add EGL support to ParaView and Glew
    
    add a package for egl that provides GL but also adds
    EGL libs and headers for projects that need them
    
    Fix a header problem with the opengl package
    
    Format files using black
    
    * better description for egl variant description
    
    Co-authored-by: Vicente Bolea <vicente.bolea@gmail.com>
    
    * better check/setup of non egl variant dependencies
    
    Co-authored-by: Vicente Bolea <vicente.bolea@gmail.com>
    
    * Add biddisco as maintainer
    
    * Fix unused var style warning
    
    * Add egl conflicts for other gl providers
    
    ---------
    
    Co-authored-by: Vicente Bolea <vicente.bolea@gmail.com>

--- var/spack/repos/builtin/packages/paraview/package.py
+++ var/spack/repos/builtin/packages/paraview/package.py
@@ -67,6 +67,7 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
     variant("fortran", default=False, description="Enable Fortran support")
     variant("mpi", default=True, description="Enable MPI support")
     variant("osmesa", default=False, description="Enable OSMesa support")
+    variant("egl", default=False, description="Enable EGL in the OpenGL library being used")
     variant("qt", default=False, description="Enable Qt (gui) support")
     variant("opengl2", default=True, description="Enable OpenGL2 backend")
     variant("examples", default=False, description="Build examples")
@@ -186,12 +187,16 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
 
     depends_on("gl@3.2:", when="+opengl2")
     depends_on("gl@1.2:", when="~opengl2")
-    depends_on("glew")
+    depends_on("glew", when="~egl")
+    depends_on("glew gl=egl", when="+egl")
+
     depends_on("osmesa", when="+osmesa")
     for p in ["linux", "cray"]:
-        depends_on("glx", when="~osmesa platform={}".format(p))
-        depends_on("libxt", when="~osmesa platform={}".format(p))
+        depends_on("glx", when="~egl ~osmesa platform={}".format(p))
+        depends_on("libxt", when="~egl ~osmesa platform={}".format(p))
     conflicts("+qt", when="+osmesa")
+    conflicts("+qt", when="+egl")
+    conflicts("+egl", when="+osmesa")
 
     depends_on("ospray@2.1:2", when="+raytracing")
     depends_on("openimagedenoise", when="+raytracing")
@@ -419,19 +424,29 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
             """Negated ternary for spec variant to OFF/ON string"""
             return variant_bool(feature, on="OFF", off="ON")
 
+        def use_x11():
+            """Return false if osmesa or egl are requested"""
+            if "+osmesa" in spec or "+egl" in spec:
+                return "OFF"
+            if spec.satisfies("platform=windows"):
+                return "OFF"
+            return "ON"
+
         rendering = variant_bool("+opengl2", "OpenGL2", "OpenGL")
         includes = variant_bool("+development_files")
-        use_x11 = nvariant_bool("+osmesa") if not spec.satisfies("platform=windows") else "OFF"
 
         cmake_args = [
             "-DVTK_OPENGL_HAS_OSMESA:BOOL=%s" % variant_bool("+osmesa"),
-            "-DVTK_USE_X:BOOL=%s" % use_x11,
+            "-DVTK_USE_X:BOOL=%s" % use_x11(),
             "-DPARAVIEW_INSTALL_DEVELOPMENT_FILES:BOOL=%s" % includes,
             "-DBUILD_TESTING:BOOL=OFF",
             "-DOpenGL_GL_PREFERENCE:STRING=LEGACY",
             self.define_from_variant("PARAVIEW_ENABLE_VISITBRIDGE", "visitbridge"),
             self.define_from_variant("VISIT_BUILD_READER_Silo", "visitbridge"),
         ]
+
+        if "+egl" in spec:
+            cmake_args.append("-DVTK_OPENGL_HAS_EGL:BOOL=ON")
 
         if spec.satisfies("@5.12:"):
             cmake_args.append("-DVTK_MODULE_USE_EXTERNAL_VTK_fast_float:BOOL=OFF")
