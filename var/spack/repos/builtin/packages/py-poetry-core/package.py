6d4ac6025451e42a70975a8dff95cd6356556c33
LydDeb <lyderic.debusschere@eolen.com>
2023-12-06 13:38:47 +0100

    py-poetry-core: add version constraint on python

--- var/spack/repos/builtin/packages/py-poetry-core/package.py
+++ var/spack/repos/builtin/packages/py-poetry-core/package.py
@@ -19,6 +19,7 @@ class PyPoetryCore(PythonPackage):
     version("1.0.8", sha256="951fc7c1f8d710a94cb49019ee3742125039fc659675912ea614ac2aa405b118")
     version("1.0.7", sha256="98c11c755a16ef6c5673c22ca94a3802a7df4746a0853a70b6fae8b9f5cac206")
 
+    depends_on("python@3.8:3", when="@1.7.0:", type=("build", "run"))
     depends_on("python@3.7:3", when="@1.1.0:", type=("build", "run"))
     depends_on("python@:3", type=("build", "run"))
     depends_on("py-importlib-metadata@1.7:", when="@1.1:1.6 ^python@:3.7", type=("build", "run"))
