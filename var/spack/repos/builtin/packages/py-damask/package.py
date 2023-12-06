dd8c31e2111af335279c08c1843df2622839bf35
Martin Diehl <mail@martin-diehl.net>
2023-12-06 16:17:55 +0100

    ensuring correct Python versions for py-damask

--- var/spack/repos/builtin/packages/py-damask/package.py
+++ var/spack/repos/builtin/packages/py-damask/package.py
@@ -31,6 +31,13 @@ class PyDamask(PythonPackage):
         "3.0.0-alpha4", sha256="0bb8bde43b27d852b1fb6e359a7157354544557ad83d87987b03f5d629ce5493"
     )
 
+    depends_on("py-pandas@0.24:", type=("build", "run"), when="@3.0.0-alpha8:")
+    depends_on("py-numpy@1.17:", type=("build", "run"), when="@3.0.0-alpha8:")
+    depends_on("py-scipy@1.2:", type=("build", "run"), when="@3.0.0-alpha8:")
+    depends_on("py-h5py@2.9:", type=("build", "run"), when="@3.0.0-alpha8:")
+    depends_on("vtk+python@8.1:", type=("build", "run"), when="@3.0.0-alpha8:")
+    depends_on("py-matplotlib@3.0:", type=("build", "run"), when="@3.0.0-alpha8:")
+    depends_on("py-pyyaml@3.12:", type=("build", "run"), when="@3.0.0-alpha8:")
     depends_on("python@3.8:", type=("build", "run"), when="@3.0.0-alpha6:")
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-setuptools@40.6:", type="build")
