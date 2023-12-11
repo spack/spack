ccc061a451526e968f830df4ef02c15232a05e46
Arne Becker <arne@ebi.ac.uk>
2023-12-11 11:13:52 +0000

    Add myself as maintainer

--- var/spack/repos/builtin/packages/perl-test-nowarnings/package.py
+++ var/spack/repos/builtin/packages/perl-test-nowarnings/package.py
@@ -13,6 +13,8 @@ class PerlTestNowarnings(PerlPackage):
     homepage = "https://metacpan.org/pod/Test::NoWarnings"
     url = "https://cpan.metacpan.org/authors/id/H/HA/HAARG/Test-NoWarnings-1.06.tar.gz"
 
+    maintainers("EbiArnie")
+
     version("1.06", sha256="c2dc51143b7eb63231210e27df20d2c8393772e0a333547ec8b7a205ed62f737")
 
     depends_on("perl@5.6.0:", type=("build", "link", "run", "test"))
