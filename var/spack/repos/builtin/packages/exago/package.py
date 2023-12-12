d9c6825fbf80d6d81e8ad0909d6f8e5e9367e481
Cameron Rutherford <robert.rutherford@pnnl.gov>
2023-12-12 11:17:26 -0500

    exago: Add v1.2.0 and patches for builds without python or tests. (#41350)
    Co-authored-by: Satish Balay <balay@mcs.anl.gov>

--- var/spack/repos/builtin/packages/exago/package.py
+++ var/spack/repos/builtin/packages/exago/package.py
@@ -36,6 +36,9 @@ class Exago(CMakePackage, CudaPackage, ROCmPackage):
         "1.3.0", tag="v1.3.0", commit="58b039d746a6eac8e84b0afc01354cd58caec485", submodules=True
     )
     version(
+        "1.2.0", tag="v1.2.0", commit="255a214ec747b7bdde7a6d8151c083067b4d0907", submodules=True
+    )
+    version(
         "1.1.2", tag="v1.1.2", commit="db3bb16e19c09e01402071623258dae4d13e5133", submodules=True
     )
     version(
@@ -180,7 +183,11 @@ class Exago(CMakePackage, CudaPackage, ROCmPackage):
         depends_on("umpire {0}".format(rocm_dep), when="+raja {0}".format(rocm_dep))
         depends_on("camp {0}".format(rocm_dep), when="+raja {0}".format(rocm_dep))
 
+    # CMake patches to support ~python and ~testing
     patch("exago-1.6.0.patch", when="@1.6.0")
+    patch("exago-1.5.0.patch", when="@1.5.0:1.5.1")
+    patch("exago-1.3.0.patch", when="@1.3.0:1.4.1")
+    patch("exago-1.1.0.patch", when="@1.1.0:1.2.0")
 
     flag_handler = build_system_flags
 
