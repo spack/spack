57d7cecef2f17931d692eda35ab9171cb21b07dc
Wouter Deconinck <wdconinc@gmail.com>
2023-12-13 13:25:30 -0600

    geant4: depends_on vecgeom@1.2.6:

--- var/spack/repos/builtin/packages/geant4/package.py
+++ var/spack/repos/builtin/packages/geant4/package.py
@@ -114,7 +114,8 @@ class Geant4(CMakePackage):
 
     # Vecgeom specific versions for each Geant4 version
     with when("+vecgeom"):
-        depends_on("vecgeom@1.2.0:", when="@11.1:")
+        depends_on("vecgeom@1.2.6:", when="@11.2:")
+        depends_on("vecgeom@1.2.0:", when="@11.1")
         depends_on("vecgeom@1.1.18:1.1", when="@11.0.0:11.0")
         depends_on("vecgeom@1.1.8:1.1", when="@10.7.0:10.7")
         depends_on("vecgeom@1.1.5", when="@10.6.0:10.6")
