24fd00671de518f87be730fa8f73b44dafaae28a
Dax <Declanxlynch@gmail.com>
2023-12-13 15:52:20 -0800

    added the oneapi patch back

--- var/spack/repos/builtin/packages/xyce/package.py
+++ var/spack/repos/builtin/packages/xyce/package.py
@@ -143,6 +143,13 @@ class Xyce(CMakePackage):
         when="@7.4:7.6 +pymi",
     )
 
+     # fix oneapi issue
+    patch(
+        "454-oneapi-xyce.patch",
+        sha256="8b81cabbb2eee3e98852f350463d0eff6a277bbb435095f4bd6d8f08cf1a4057",
+        when="%oneapi"
+    )
+
     # fix cmake not passing tests issue
     patch(
         "xyceCmake.patch",
