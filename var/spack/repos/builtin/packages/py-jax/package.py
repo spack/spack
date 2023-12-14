2f9e921366862c93886a3e2de0c01daf7aed7069
Gabriel Cretin <gabrielcretin@gmail.com>
2023-12-14 20:54:30 +0100

    py-jax: add 0.4.14

--- var/spack/repos/builtin/packages/py-jax/package.py
+++ var/spack/repos/builtin/packages/py-jax/package.py
@@ -21,20 +21,24 @@ class PyJax(PythonPackage):
     homepage = "https://github.com/google/jax"
     pypi = "jax/jax-0.2.25.tar.gz"
 
+    version("0.4.14", sha256="18fed3881f26e8b13c8cb46eeeea3dba9eb4d48e3714d8e8f2304dd6e237083d")
     version("0.4.3", sha256="d43f08f940aa30eb339965cfb3d6bee2296537b0dc2f0c65ccae3009279529ae")
     version("0.3.23", sha256="bff436e15552a82c0ebdef32737043b799e1e10124423c57a6ae6118c3a7b6cd")
     version("0.2.25", sha256="822e8d1e06257eaa0fdc4c0a0686c4556e9f33647fa2a766755f984786ae7446")
 
-    depends_on("python@3.8:", when="@0.4:", type=("build", "run"))
+    depends_on("python@3.9:", when="@0.4.14:", type=("build", "run"))
+    depends_on("python@3.8:", when="@:0.4.13", type=("build", "run"))
     depends_on("py-setuptools", type="build")
+    depends_on("py-numpy@1.22:", when="@0.4.14:", type=("build", "run"))
     depends_on("py-numpy@1.20:", when="@0.3:", type=("build", "run"))
     depends_on("py-numpy@1.18:", type=("build", "run"))
-    depends_on("py-opt-einsum", type=("build", "run"))
+    depends_on("py-scipy@1.7:", when="@0.4.7:", type=("build", "run"))
     depends_on("py-scipy@1.5:", when="@0.3:", type=("build", "run"))
     depends_on("py-scipy@1.2.1:", type=("build", "run"))
+    depends_on("py-opt-einsum", type=("build", "run"))
 
     # See _minimum_jaxlib_version in jax/version.py
-    jax_to_jaxlib = {"0.4.3": "0.4.2", "0.3.23": "0.3.15", "0.2.25": "0.1.69"}
+    jax_to_jaxlib = {"0.4.3": "0.4.2", "0.4.14":"0.4.11", "0.3.23": "0.3.15", "0.2.25": "0.1.69"}
 
     for jax, jaxlib in jax_to_jaxlib.items():
         depends_on(f"py-jaxlib@{jaxlib}:", when=f"@{jax}", type=("build", "run"))
