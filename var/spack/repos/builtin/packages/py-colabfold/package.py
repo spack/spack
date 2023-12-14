cde3db7d07f35c6fcc214b55c4e095bb4f8c2f01
Gabriel Cretin <gabrielcretin@gmail.com>
2023-12-14 20:45:51 +0100

    py-colabfold: fix dependencies

--- var/spack/repos/builtin/packages/py-colabfold/package.py
+++ var/spack/repos/builtin/packages/py-colabfold/package.py
@@ -15,30 +15,40 @@ class PyColabfold(PythonPackage, CudaPackage):
     url = "https://github.com/sokrypton/ColabFold/archive/refs/tags/v1.5.2.tar.gz"
     git = "https://github.com/sokrypton/ColabFold"
 
-    version("1.5.3", branch="main", commit="ca6b297021bb876157b75d84d591ed350cbece85")
     version(
-        "1.5.2",
-        sha256="b3b82e831e241a6ab40f2b0c6d560ac5328c6a0b505a0186c75e397ca1a16477"
+        "1.5.3",
+        sha256="1b2776f285981796559effbc3691ebbcfcde68514cc05559583ebab76c4c25e8"
     )
 
     variant("jax", default=True, description="Install with Jax")
     variant("alphafold_colabfold", default=True, description="Install with py-alphafold-colabfold")
 
-    depends_on("py-poetry-core@1", type="build")
 
+    # Inspired by the meta.yaml file of the corresponding Conda environment available here:
+    # https://anaconda.org/bioconda/colabfold/1.5.3/download/noarch/colabfold-1.5.3-pyh7cba7a3_1.tar.bz2
+
+    patch(
+        "fix-jax-04-nan.patch",
+        when="1.5.3",
+        sha256="6244c2143987dd4a6a87c1174c7a801c220034fbeb8d13f901b0f950b69d8543"
+    )
+
+    depends_on("py-poetry-core@1.8.1", type="build")
+    depends_on("py-poetry@1.7", type="build")
     depends_on("python@3.7.1:3.10", type=("build", "run"))
 
-    depends_on("py-absl-py@1", type=("build", "run"))
-    depends_on("py-jax@0.3.25:", type=("build", "run"), when="+jax")
-    depends_on("py-matplotlib@3.2.2:", type=("build", "run"))
-    depends_on("py-tensorflow@2.11.0:", type=("build", "run"))
-    depends_on("py-tensorflow@2.11.0: +cuda", type=("build", "run"), when="+cuda")
+    depends_on("py-absl-py@1.4:", type=("build", "run"))
+    depends_on("py-jax@0.4.14", type=("build", "run"), when="+jax")
+    depends_on("py-jaxlib@0.4.14 +cuda cuda_arch=61,70,75,80,86", type=("build", "run"), when="+jax +cuda")
+    depends_on("py-matplotlib@3.2.2:3", type=("build", "run"))
+    depends_on("py-tensorflow@2.12.1 ~cuda", type=("build", "run"))
     depends_on("py-numpy@1.21.6:", type=("build", "run"))
     depends_on("py-pandas@1.3.4:", type=("build", "run"))
     depends_on("py-alphafold-colabfold@2.3.5", type=("build", "run"), when="+alphafold_colabfold")
+    depends_on("py-alphafold-colabfold@2.3.5 +cuda cuda_arch=61,70,75,80,86", type=("build", "run"), when="+alphafold_colabfold +cuda")
     depends_on("py-requests@2.26.0:", type=("build", "run"))
-    depends_on("py-tqdm@4.62.2:", type=("build", "run"))
+    depends_on("py-tqdm", type=("build", "run"))
     depends_on("py-appdirs@1.4.4:", type=("build", "run"))
     depends_on("py-py3dmol@2.0.1:", type=("build", "run"))
-    depends_on("py-dm-haiku@0.0.9", type=("build", "run"))
+    depends_on("py-dm-haiku", type=("build", "run"))
     depends_on("py-importlib-metadata@4.8.2:4", type=("build", "run"))
