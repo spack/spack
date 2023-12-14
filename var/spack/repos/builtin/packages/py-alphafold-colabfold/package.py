3941fd15e38b98ac8165dc412763d357a4c5a339
Gabriel Cretin <gabrielcretin@gmail.com>
2023-12-14 20:46:32 +0100

    py-alphafold-colabfold: fix dependencies

--- var/spack/repos/builtin/packages/py-alphafold-colabfold/package.py
+++ var/spack/repos/builtin/packages/py-alphafold-colabfold/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-
+import glob
 from spack.package import *
 
 
@@ -23,43 +23,28 @@ class PyAlphafoldColabfold(PythonPackage, CudaPackage):
 
     # lots of hints on versions and patching taken from docker/Dockerfile
     # and requirements.txt
-    depends_on("python@3.7:", type=("build", "run"))
-    depends_on("py-setuptools", type="build")
-    depends_on("py-absl-py@0.13.0:", type=("build", "run"), when="@2.1.1")
-    depends_on("py-absl-py@1.0.0:", type=("build", "run"), when="@2.2.4:")
+    depends_on("python@3.7:3.10", type=("build", "run"))
+    depends_on("py-setuptools@68:", type="build")
+    depends_on("py-absl-py@1.0.0:", type=("build", "run"), when="@2.3.5:")
     depends_on("py-biopython@1.79:", type=("build", "run"))
-    depends_on("py-chex@0.0.7:", type=("build", "run"))
-    depends_on("py-dm-haiku@0.0.4:", type=("build", "run"), when="@2.1.1")
-    depends_on("py-dm-haiku@0.0.7:", type=("build", "run"), when="@2.2.4:")
+    depends_on("py-chex@0.1.8", type=("build", "run"))
+    depends_on("py-dm-haiku@0.0.7:", type=("build", "run"), when="@2.3.5:")
     depends_on("py-dm-tree@0.1.6:", type=("build", "run"))
     depends_on("py-docker", type=("build", "run"))
     depends_on("py-immutabledict@2.0.0:", type=("build", "run"))
-    depends_on("py-jax@0.2.14:", type=("build", "run"), when="@2.1.1")
-    depends_on("py-jax@0.3.17:", type=("build", "run"), when="@2.2.4:")
+    depends_on("py-jax@0.3.17:", type=("build", "run"), when="@2.3.5:")
+    depends_on("py-jaxlib@0.3.17: +cuda cuda_arch=61,70,75,80,86", type=("build", "run"), when="@2.3.5: +cuda")
     depends_on("py-ml-collections@0.1.0:", type=("build", "run"))
-    depends_on("py-numpy@1.19.5:", type=("build", "run"), when="@2.1.1")
-    depends_on("py-numpy@1.21.6:", type=("build", "run"), when="@2.2.4:")
+    depends_on("py-numpy@1.21.6:", type=("build", "run"), when="@2.3.5:")
     depends_on("py-pandas@1.3.4:", type=("build", "run"))
-    depends_on("py-protobuf@3.19:", type=("build", "run"), when="@2.2.4:")
+    depends_on("py-protobuf@3.19:", type=("build", "run"), when="@2.3.5:")
     depends_on("py-scipy@1.7.0:", type=("build", "run"))
     depends_on("py-pdbfixer@1.7", type=("build", "run"))
-    depends_on("py-tensorflow@2.5:", type=("build", "run"), when="@2.1.1")
-    depends_on("py-tensorflow@2.9:", type=("build", "run"), when="@2.2.4:")
-#    depends_on(
-#        "openmm@7.5.1+cuda",
-#        type="run",
-#        patches=[
-#            patch(
-#                "https://raw.githubusercontent.com/deepmind/alphafold/main/docker/openmm.patch",
-#                sha256="a5a0ced820f3ecc56ae634c3111f80614863559b0587954a2658c8d4b2a07ae3",
-#                working_dir="wrappers/python",
-#                level=0,
-#            )
-#        ],
-#    )
+    depends_on("py-tensorflow@2.9: ~cuda", type=("build", "run"), when="@2.3.5:")
+    depends_on("openmm@7.7.0 +cuda")
     depends_on("hmmer", type="run")
-    depends_on("kalign2", type="run")
-    depends_on("hh-suite", type="run")
+    depends_on("kalign2@2.04", type="run")
+    depends_on("hh-suite@3.3.0", type="run")
     depends_on("aria2", type="run")
 
     resource(
@@ -70,12 +55,6 @@ class PyAlphafoldColabfold(PythonPackage, CudaPackage):
         expand=False,
         placement="chemprops",
     )
-
-    #patch(
-    #    url_or_filename="https://github.com/sokrypton/alphafold/commit/1f04253c620703d6768b20987b3b5e075314095f.patch",
-    #    sha256="b29d56b236e65ae609b7e4edf7e72c2b9a3a7ff048c9939950cab12192ca0e43",
-    #    when="@2.3.5 ^jax@0.3.26:"
-    #)
 
     @run_after("install")
     def install_scripts(self):
