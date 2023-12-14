a164bf2f0f71730471b33252b5254966ac9362e9
Gabriel Cretin <gabrielcretin@gmail.com>
2023-12-14 20:27:36 +0100

    py-cachecontrol: add @0.13.1

--- var/spack/repos/builtin/packages/py-cachecontrol/package.py
+++ var/spack/repos/builtin/packages/py-cachecontrol/package.py
@@ -13,6 +13,7 @@ class PyCachecontrol(PythonPackage):
     homepage = "https://github.com/ionrock/cachecontrol"
     pypi = "CacheControl/CacheControl-0.12.10.tar.gz"
 
+    version("0.13.1", sha256="f012366b79d2243a6118309ce73151bf52a38d4a5dac8ea57f09bd29087e506b")
     version("0.12.11", sha256="a5b9fcc986b184db101aa280b42ecdcdfc524892596f606858e0b7a8b4d9e144")
     version("0.12.10", sha256="d8aca75b82eec92d84b5d6eb8c8f66ea16f09d2adb09dbca27fe2d5fc8d3732d")
 
@@ -23,3 +24,10 @@ class PyCachecontrol(PythonPackage):
     depends_on("py-requests", type=("build", "run"))
     depends_on("py-msgpack@0.5.2:", type=("build", "run"))
     depends_on("py-lockfile@0.9:", when="+filecache", type="run")
+
+    def url_for_version(self, version):
+        if version <= Version("0.13.0"):
+            url = "https://files.pythonhosted.org/packages/source/c/cachecontrol/CacheControl-{}.tar.gz"
+        elif version > Version("0.13.0"):
+            url = "https://files.pythonhosted.org/packages/source/c/cachecontrol/cachecontrol-{}.tar.gz"
+        return url.format(version)
