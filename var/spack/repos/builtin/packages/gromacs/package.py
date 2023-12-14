88be5adf5c445a6ff5ce2b1d071aa70005d645fd
Daniel Ahlin <ahlin@google.com>
2023-12-14 11:40:08 +0100

    gromacs: Add nvshmem dependencies for cufftmp variant
    Workaround for the issue mentioned on
    https://manual.gromacs.org/current/install-guide/index.html#using-cufftmp

--- var/spack/repos/builtin/packages/gromacs/package.py
+++ var/spack/repos/builtin/packages/gromacs/package.py
@@ -307,6 +307,8 @@ class Gromacs(CMakePackage, CudaPackage):
         when="@2021.1:2021.2",
     )
 
+    patch("cufftmp-cmake-add-nvshmem.patch", when="+cufftmp")
+
     filter_compiler_wrappers(
         "*.cmake", relative_root=os.path.join("share", "cmake", "gromacs_mpi")
     )
@@ -542,8 +544,20 @@ class CMakeBuilder(spack.build_systems.cmake.CMakeBuilder):
         if "+cufftmp" in self.spec:
             options.append("-DGMX_USE_CUFFTMP=ON")
             options.append(
-                f'-DcuFFTMp_ROOT={self.spec["nvhpc"].prefix}/Linux_{self.spec.target.family}'
-                + f'/{self.spec["nvhpc"].version}/math_libs'
+                f'-DcuFFTMp_ROOT={self.spec["nvhpc"].prefix}/Linux_{self.spec.target.family}' +
+                f'/{self.spec["nvhpc"].version}/math_libs'
+            )
+            options.append(
+                f'-DcuFFTMp_ROOT={self.spec["nvhpc"].prefix}/Linux_{self.spec.target.family}' +
+                f'/{self.spec["nvhpc"].version}/math_libs'
+            )
+            # Add option to include cufft-mp's nvhshmem dependency.
+            # This currently requires the NVIDIA device drivers to be installed on the build
+            # machine to fulfill nvshmem_host's dependency on nvidia-ml.
+            options.append(
+                f'-DcuFFTMp_NVSHMEM_ROOT={self.spec["nvhpc"].prefix}/Linux_{self.spec.target.family}' +
+                f'/{self.spec["nvhpc"].version}/comm_libs/{self.spec["cuda"].version.up_to(2)}' +
+                f'/nvshmem_cufftmp_compat'
             )
 
         if "+heffte" in self.spec:
