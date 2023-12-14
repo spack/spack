574182f57b6de9a43c6d300c8277be415226e8ed
Harry Sharma <harrysharma1066@gmail.com>
2023-12-14 14:58:46 +0000

    vbz-compression: fix style

--- var/spack/repos/builtin/packages/vbz-compression/package.py
+++ var/spack/repos/builtin/packages/vbz-compression/package.py
@@ -31,12 +31,10 @@ class VbzCompression(CMakePackage):
     version("1.0.0", commit="2db0e3f62fa7a612dc42dc802401c26781eed068", submodules=True)
     version("0.9.3", commit="9a748efcdd0754be835e1080cf7086f3451e17d1", submodules=True)
 
-
     depends_on("cmake@3.11:")
     depends_on("zstd@1.3.1:")
     depends_on("hdf5@1.8.16:")
     #  depends_on("htslib")
-
 
     def cmake_args(self):
         args = [
