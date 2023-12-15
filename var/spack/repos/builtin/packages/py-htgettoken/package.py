cfa2f19f48699a762af77ece8a9c5bc4db3a75d5
Wouter Deconinck <wdconinc@gmail.com>
2023-12-15 07:50:01 -0600

    py-htgettoken: use os.environ, avoid AttributeError
    This avoids the following error:
    ```
    Warning: could not load runtime environment due to AttributeError: 'EnvironmentModifications' object has no attribute 'get'
    ```

--- var/spack/repos/builtin/packages/py-htgettoken/package.py
+++ var/spack/repos/builtin/packages/py-htgettoken/package.py
@@ -33,8 +33,8 @@ class PyHtgettoken(PythonPackage):
     depends_on("py-urllib3", type=("build", "run"))
 
     def setup_run_environment(self, env):
-        dir = env.get("XDG_RUNTIME_DIR") or "/tmp"
-        uid = env.get("UID") or os.geteuid()
+        dir = os.environ["XDG_RUNTIME_DIR"] or "/tmp"
+        uid = os.environ["UID"] or os.geteuid()
         file = join_path(dir, "bt_u" + uid)
         env.set("BEARER_TOKEN", file)
         env.set("BEARER_TOKEN_FILE", file)
