135ee2d4543c179b8351082499775f7ab1778c51
Gabriel Cretin <gabrielcretin@gmail.com>
2023-12-18 13:46:32 +0100

    py-chex: add @0.1.8 + deps

--- var/spack/repos/builtin/packages/py-chex/package.py
+++ var/spack/repos/builtin/packages/py-chex/package.py
@@ -13,14 +13,17 @@ class PyChex(PythonPackage):
     homepage = "https://github.com/deepmind/chex"
     pypi = "chex/chex-0.1.0.tar.gz"
 
+    version("0.1.8", sha256="590394fdb48f392019321de568b3ec7f962516f0c660641b790246b9a4142a79")
     version("0.1.5", sha256="686858320f8f220c82a6c7eeb54dcdcaa4f3d7f66690dacd13a24baa1ee8299e")
     version("0.1.0", sha256="9e032058f5fed2fc1d5e9bf8e12ece5910cf6a478c12d402b6d30984695f2161")
 
     depends_on("python@3.7:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-absl-py@0.9.0:", type=("build", "run"))
+    depends_on("py-typing-extensions@4.2.0:", when="@0.1.6:", type=("build", "run"))
     depends_on("py-dm-tree@0.1.5:", type=("build", "run"))
-    depends_on("py-jax@0.1.55:", type=("build", "run"))
+    depends_on("py-jax@0.4.6:", when="@0.1.7:", type=("build", "run"))
+    depends_on("py-jax@0.1.55:", when="@:0.1.6", type=("build", "run"))
     depends_on("py-jaxlib@0.1.37:", type=("build", "run"))
     depends_on("py-numpy@1.18.0:", type=("build", "run"))
     depends_on("py-toolz@0.9.0:", type=("build", "run"))
