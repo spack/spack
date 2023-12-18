8961a09e3e31a470e3f79cf63fff15bd9a719127
Terry Cojean <terry.cojean@kit.edu>
2023-12-18 15:06:58 +0100

    Review updates.
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>
    Signed-off-by: Terry Cojean <terry.cojean@kit.edu>

--- var/spack/repos/builtin/packages/py-carputils/package.py
+++ var/spack/repos/builtin/packages/py-carputils/package.py
@@ -30,7 +30,7 @@ class PyCarputils(PythonPackage):
 
     # cusettings:11: DeprecationWarning:
     # The distutils package is deprecated and slated for removal in Python 3.12.
-    conflicts("python@3.10:", when="@:oc13.0")
+    conflicts("python@3.12:", when="@:oc13.0")
 
     depends_on("py-numpy@1.14.5:", type=("build", "run"))
     depends_on("py-setuptools@41.6.0:", type=("build", "run"))
