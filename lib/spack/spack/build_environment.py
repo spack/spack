8659655b33427c12a2a62158564f5e823d222700
John Parent <john.parent@kitware.com>
2023-12-21 11:57:15 -0500

    Refactor use of env

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -155,7 +155,8 @@ def translate_paths(name: str, env_val: str) -> str:
     """
 
     spack_paths = env_val.split(os.pathsep)
-    root = ShortLinkManager.get_next_view(name)
+    root = ShortLinkManager.get_next_view(env_val)
+    ShortLink(name, root, spack_paths).create_filesystem_view()
     return root
 
 
@@ -477,8 +478,7 @@ def set_wrapper_variables(pkg, env):
             env_paths.append(ci)
 
     tty.debug("Adding compiler bin/ paths: " + " ".join(env_paths))
-    for item in env_paths:
-        env.prepend_path("PATH", item)
+    env.prepend_path("PATH", translate_paths("PATH", os.pathsep.join(env_paths)))
     env.set_path(SPACK_ENV_PATH, env_paths)
 
     # Working directory for the spack command itself, for debug logs.
@@ -1076,7 +1076,7 @@ class SetupContext:
         for d in ("bin", "bin64"):
             bin_dir = os.path.join(dep.prefix, d)
             if os.path.isdir(bin_dir):
-                env.prepend_path("PATH", bin_dir)
+                env.prepend_path("PATH", translate_paths("PATH", bin_dir))
 
 
 def get_cmake_prefix_path(pkg):
