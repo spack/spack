6e36adf9515a3ba46af7f42f47a717b5c41c6e5b
WeinaJi <weina.ji@epfl.ch>
2023-12-22 17:24:38 +0100

    py-neurodamus: bump to 2.17.0 (#2238)

--- bluebrain/repo-bluebrain/packages/py-neurodamus/package.py
+++ bluebrain/repo-bluebrain/packages/py-neurodamus/package.py
@@ -12,6 +12,7 @@ class PyNeurodamus(PythonPackage):
     git = "https://github.com/BlueBrain/neurodamus.git"
 
     version("develop", branch="main")
+    version("2.17.0", tag="2.17.0")
     version("2.16.6", tag="2.16.6")
     version("2.16.5", tag="2.16.5")
     version("2.16.4", tag="2.16.4")
