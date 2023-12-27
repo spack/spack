a5e75e358fb18c829befffb90d2cf9ca1e015903
Adam J. Stewart <ajstewart426@gmail.com>
2023-12-27 01:03:51 +0100

    py-pbr: setuptools required at run-time (#41866)

--- var/spack/repos/builtin/packages/py-pbr/package.py
+++ var/spack/repos/builtin/packages/py-pbr/package.py
@@ -25,4 +25,4 @@ class PyPbr(PythonPackage):
     version("1.8.1", sha256="e2127626a91e6c885db89668976db31020f0af2da728924b56480fc7ccf09649")
 
     depends_on("python@2.6:", type=("build", "run"))
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools", type=("build", "run"))
