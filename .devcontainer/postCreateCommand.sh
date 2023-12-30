9657bb51a7b38349c942e67c74fe20ea6e5d0e14
Wouter Deconinck <wdconinc@gmail.com>
2023-12-29 16:33:15 -0800

    fix: use develop, not develop-root mirror

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -11,8 +11,10 @@ EOF
 # Ensure generic targets for maximum matching with buildcaches
 spack config --scope site add "packages:all:require:[target=x86_64_v3]"
 spack config --scope site add "concretizer:targets:granularity:generic"
+
+# Find compiler and install gcc-runtime
 spack compiler find --scope site
 
 # Setup buildcaches
-spack mirror add --scope site develop-root https://binaries.spack.io/develop/root
+spack mirror add --scope site develop https://binaries.spack.io/develop
 spack buildcache keys --install --trust
