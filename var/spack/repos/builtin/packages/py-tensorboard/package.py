c6d694b6890b3c3e5d94d2fdc7eebf123e037c35
Adam J. Stewart <ajstewart426@gmail.com>
2024-01-01 22:03:05 +0100

    py-tensorboard: fix import tests (#41874)

--- var/spack/repos/builtin/packages/py-tensorboard/package.py
+++ var/spack/repos/builtin/packages/py-tensorboard/package.py
@@ -14,6 +14,9 @@ class PyTensorboard(PythonPackage):
     url = "https://files.pythonhosted.org/packages/py3/t/tensorboard/tensorboard-2.9.1-py3-none-any.whl"
     list_url = "https://pypi.org/simple/tensorboard/"
 
+    # Requires tensorflow
+    skip_modules = ["tensorboard.summary._tf"]
+
     maintainers("aweits")
 
     license("Apache-2.0")
