43ca8ae53a48e35cfc1f973090371d6124d24859
LydDeb <lyderic.debusschere@eolen.com>
2024-01-02 08:52:35 +0100

    py-poetry-plugin-tweak-dependencies-version: add python version constraint

--- var/spack/repos/builtin/packages/py-poetry-plugin-tweak-dependencies-version/package.py
+++ var/spack/repos/builtin/packages/py-poetry-plugin-tweak-dependencies-version/package.py
@@ -17,6 +17,7 @@ class PyPoetryPluginTweakDependenciesVersion(PythonPackage):
 
     version("1.5.1", sha256="4e0be2b8e23a04e542c5090deb5b6e191750ec45bace98ea8b55150844c6026b")
 
+    depends_on("python@3.9:3.11", type=("build", "run"))
     depends_on("py-poetry-core@1.0.0:", type="build")
     depends_on("py-poetry-dynamic-versioning", type="build")
     depends_on("py-poetry@1.6", type=("build", "run"))
