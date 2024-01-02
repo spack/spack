86c95938fceafe2d3b7eb595ae0df91ffa4ffd52
Todd Gamblin <tgamblin@llnl.gov>
2024-01-02 00:21:30 -0800

    Update copyright year to 2024 (#41919)
    It was time to run `spack license update-copyright-year` again.

--- lib/spack/spack/cmd/create.py
+++ lib/spack/spack/cmd/create.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -27,7 +27,7 @@ level = "short"
 
 
 package_template = '''\
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
