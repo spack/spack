e05457a0df670d8a7603fab46a83e7213a724ffa
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2024-01-03 14:03:37 +0100

    improve regex / bug that matches with -DMPICH_SKIP_MPICXX=1

--- var/spack/repos/builtin/packages/neuron/package.py
+++ var/spack/repos/builtin/packages/neuron/package.py
@@ -178,13 +178,13 @@ class Neuron(CMakePackage):
         # replace compilers from makefile
         compilers = [("CC", "cc_compiler"), ("CXX", "cxx_compiler")]
         for compiler_var, compiler_env in compilers:
-            pattern = "{0}\\s*{1}.+".format(compiler_var, assign_operator)
+            pattern = "(?:^|\\s){0}\\s*{1}.+".format(compiler_var, assign_operator)
             replacement = "{0} = {1}".format(compiler_var, locals()[compiler_env])
             filter_file(pattern, replacement, nrnmech_makefile)
 
         if spec.satisfies("@8:+coreneuron"):
             nrnmakefile = join_path(self.prefix, "share/coreneuron/nrnivmodl_core_makefile")
-            filter_file("CXX\\s*=.+", "CXX = {0}".format(cxx_compiler), nrnmakefile)
+            filter_file("(?:^|\\s)CXX\\s*=.+", "CXX = {0}".format(cxx_compiler), nrnmakefile)
 
     def setup_run_environment(self, env):
         env.prepend_path("PATH", join_path(self.prefix, "bin"))
