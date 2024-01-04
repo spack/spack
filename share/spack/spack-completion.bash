204c246c5db8d93891b62ccf7a30c8f5586ecdc2
Todd Gamblin <tgamblin@llnl.gov>
2024-01-03 21:29:05 -0800

    `spack list`: add `--namesapce` / `--repo` option
    This adds options to `spack list` that allow you to list only packages from specific
    repositories/namespaces, e.g.:
    
    ```console
    spack list -r builtin
    ```
    
    only lists packages from the `builtin` repo, while:
    
    ```console
    spack list -r myrepo -r myrepo2
    ```
    
    would list packages from `myrepo` and `myrepo2`, but not from `builtin`. Note that you
    can use the same argument multiple times.
    
    You can use either `-r` / `--repo` or `-N` / `--namespace`. `-N` is there to match the
    corresponding option on `spack find`.
    
    - [x] add `-r` / `--repo` / `-N` / `--namespace` argument
    - [x] add test

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -1320,7 +1320,7 @@ _spack_license_update_copyright_year() {
 _spack_list() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help -d --search-description --format -v --virtuals -t --tag --count --update"
+        SPACK_COMPREPLY="-h --help -r --repo -N --namespace -d --search-description --format -v --virtuals -t --tag --count --update"
     else
         _all_packages
     fi
