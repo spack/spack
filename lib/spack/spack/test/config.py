826dd8545d3f5fe7c41e03fcabd075208fc703d5
Peter Scheibel <scheibel1@llnl.gov>
2024-01-04 10:18:35 -0800

    add (now failing) test

--- lib/spack/spack/test/config.py
+++ lib/spack/spack/test/config.py
@@ -1479,6 +1479,8 @@ def test_config_file_read_invalid_yaml(tmpdir, mutable_empty_config):
         ('x:y:"{z}"', True),
         ('x:"y"+:z', True),
         ('x:"y"trail:z', False),
+        ("x:y:[1.0]", True),
+        ("x:y:['1.0']", True)
     ],
 )
 def test_config_path_dsl(path, it_should_work):
