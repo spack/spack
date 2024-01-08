b7979c8df5f993026bb393a80f9eeb47fb228701
Dani <44979434+d-beltran@users.noreply.github.com>
2024-01-08 18:40:39 +0100

    replaced ' by " for the style test

--- var/spack/repos/builtin/packages/biobb-common/package.py
+++ var/spack/repos/builtin/packages/biobb-common/package.py
@@ -11,23 +11,23 @@ class BiobbCommon(PythonPackage):
 
     # Homepage and download url
     homepage = "https://github.com/bioexcel/biobb_common"
-    git = 'https://github.com/bioexcel/biobb_common.git'
+    git = "https://github.com/bioexcel/biobb_common.git"
 
     # Set the gitlab accounts of this package maintainers
-    maintainers = ['d-beltran']
+    maintainers = ["d-beltran"]
 
     # Versions
-    version('4.0.0', branch='master')
+    version("4.0.0", branch="master")
 
     # Dependencies
-    depends_on('py-setuptools')
-    depends_on('python@3.8:', type=('build', 'run'))
-    depends_on('py-pyyaml', type=('build', 'run'))
-    depends_on('py-requests', type=('build', 'run'))
-    depends_on('py-biopython@1.78:1.80', type=('build', 'run'))
+    depends_on("py-setuptools")
+    depends_on("python@3.8:", type=("build", "run"))
+    depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-requests", type=("build", "run"))
+    depends_on("py-biopython@1.78:1.80", type=("build", "run"))
 
     # Test
-    @run_after('install')
+    @run_after("install")
     @on_package_attributes(run_tests=True)
     def check_install(self):
-        python("-c", 'import biobb_common')
+        python("-c", "import biobb_common")
