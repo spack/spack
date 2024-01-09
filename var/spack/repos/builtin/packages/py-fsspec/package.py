d351906a4e8f21b6b4e824ef95aef65123e81c58
Adam J. Stewart <ajstewart426@gmail.com>
2024-01-09 23:39:21 +0100

    py-fsspec: fix import tests (#41860)

--- var/spack/repos/builtin/packages/py-fsspec/package.py
+++ var/spack/repos/builtin/packages/py-fsspec/package.py
@@ -14,6 +14,9 @@ class PyFsspec(PythonPackage):
 
     license("BSD-3-Clause")
 
+    # Requires pytest
+    skip_modules = ["fsspec.tests"]
+
     version("2023.10.0", sha256="330c66757591df346ad3091a53bd907e15348c2ba17d63fd54f5c39c4457d2a5")
     version("2023.1.0", sha256="fbae7f20ff801eb5f7d0bedf81f25c787c0dfac5e982d98fa3884a9cde2b5411")
     version("2022.11.0", sha256="259d5fd5c8e756ff2ea72f42e7613c32667dc2049a4ac3d84364a7ca034acb8b")
