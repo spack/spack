7e58edde23cbe6cc044072567abf0be98b7aefb9
Arne Becker <101113822+EbiArnie@users.noreply.github.com>
2024-01-10 23:44:43 +0000

    perl-set-object: New package (#41704)
    * perl-set-object: New package
    
    Adds Set::Object
    
    * Removed copyright line
    
    * New year

---
+++ var/spack/repos/builtin/packages/perl-set-object/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PerlSetObject(PerlPackage):
+    """Unordered collections (sets) of Perl Objects"""
+
+    homepage = "https://metacpan.org/pod/Set::Object"
+    url = "https://cpan.metacpan.org/authors/id/R/RU/RURBAN/Set-Object-1.42.tar.gz"
+
+    maintainers("EbiArnie")
+
+    version("1.42", sha256="d18c5a8a233eabbd0206cf3da5b00fcdd7b37febf12a93dcc3d1c026e6fdec45")
+
+    def test_use(self):
+        """Test 'use module'"""
+        options = ["-we", 'use strict; use Set::Object; print("OK\n")']
+
+        perl = self.spec["perl"].command
+        out = perl(*options, output=str.split, error=str.split)
+        assert "OK" in out
