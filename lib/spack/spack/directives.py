684d696a5c62efc12f5297ac53387e3f4dcda7e1
tjfulle <tjfulle@users.noreply.github.com>
2024-01-12 17:15:07 +0000

    [@spackbot] updating style on behalf of tjfulle

--- lib/spack/spack/directives.py
+++ lib/spack/spack/directives.py
@@ -323,10 +323,9 @@ class DirectiveMeta(type):
         return _decorator
 
 
-SubmoduleCallback = Callable[
-    "spack.package_base.PackageBase", Union[str, List[str], bool]
-]
+SubmoduleCallback = Callable["spack.package_base.PackageBase", Union[str, List[str], bool]]
 directive = DirectiveMeta.directive
+
 
 @directive("versions")
 def version(
