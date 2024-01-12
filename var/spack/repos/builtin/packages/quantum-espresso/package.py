9df047ddceb4de5881ae55c34171eaef929f8f16
Paul R. C. Kent <kentpr@ornl.gov>
2024-01-12 12:36:46 -0500

    Add QE v7.3 (#42056)

--- var/spack/repos/builtin/packages/quantum-espresso/package.py
+++ var/spack/repos/builtin/packages/quantum-espresso/package.py
@@ -25,6 +25,7 @@ class QuantumEspresso(CMakePackage, Package):
     license("GPL-2.0-only")
 
     version("develop", branch="develop")
+    version("7.3", sha256="edc2a0f3315c69966df4f82ec86ab9f682187bc9430ef6d2bacad5f27f08972c")
     version("7.2", sha256="b348a4a7348b66a73545d9ca317a2645755c98d343c1cfe8def475ad030808c0")
     version("7.1", sha256="d56dea096635808843bd5a9be2dee3d1f60407c01dbeeda03f8256a3bcfc4eb6")
     version("7.0", sha256="85beceb1aaa1678a49e774c085866d4612d9d64108e0ac49b23152c8622880ee")
