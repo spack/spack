424c3e0ba45ecd857db7d7d07a8f3062c93ad51b
Maciej Wójcik <w8jcik@gmail.com>
2024-01-14 14:25:58 +0100

    Reformatting

--- var/spack/repos/builtin/packages/gromacs-chain-coordinate/package.py
+++ var/spack/repos/builtin/packages/gromacs-chain-coordinate/package.py
@@ -4,7 +4,8 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
-from spack.pkg.builtin.gromacs import Gromacs, CMakeBuilder as GromacsCMakeBuilder
+from spack.pkg.builtin.gromacs import CMakeBuilder as GromacsCMakeBuilder
+from spack.pkg.builtin.gromacs import Gromacs
 
 
 class GromacsChainCoordinate(Gromacs):
