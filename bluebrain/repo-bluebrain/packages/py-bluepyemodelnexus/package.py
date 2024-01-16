5e6ad07bd9d18513fbe626e46e760eb0ffa89e25
ilkilic <10600022+ilkilic@users.noreply.github.com>
2024-01-16 17:10:30 +0100

    py-bluepyemodelnexus: bump (#2255)
    Co-authored-by: Kiliç Ilkan Fabrice <ikilic@bb-fvff870aq05n.epfl.ch>

--- bluebrain/repo-bluebrain/packages/py-bluepyemodelnexus/package.py
+++ bluebrain/repo-bluebrain/packages/py-bluepyemodelnexus/package.py
@@ -12,7 +12,7 @@ class PyBluepyemodelnexus(PythonPackage):
     homepage = "https://bbpgitlab.epfl.ch/cells/bluepyemodelnexus"
     git = "ssh://git@bbpgitlab.epfl.ch/cells/bluepyemodelnexus.git"
 
-    version("0.0.6", tag="bluepyemodelnexus-v0.0.6")
+    version("0.0.7", tag="bluepyemodelnexus-v0.0.7")
 
     depends_on("py-setuptools", type="build")
 
