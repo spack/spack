16b5271e8b907774c2209ead3d42999d18a9899f
Arne Becker <101113822+EbiArnie@users.noreply.github.com>
2024-01-17 16:35:46 +0000

    perl-rose-db-object and deps: New packages (#42029)
    - Deprecates 1.63 in DateTime
    - Adds Rose::DateTime
    - Adds Rose::DB
    - Adds Rose::DB::Object

--- var/spack/repos/builtin/packages/perl-datetime/package.py
+++ var/spack/repos/builtin/packages/perl-datetime/package.py
@@ -10,14 +10,18 @@ class PerlDatetime(PerlPackage):
     """DateTime - A date and time object for Perl"""
 
     homepage = "https://metacpan.org/pod/DateTime"
-    url = "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-1.63.tar.gz"
-
-    license("Artistic-2.0")
+    url = "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-1.65.tar.gz"
 
     maintainers("EbiArnie")
 
+    license("Artistic-2.0")
+
     version("1.65", sha256="0bfda7ff0253fb3d88cf4bdb5a14afb8cea24d147975d5bdf3c88b40e7ab140e")
-    version("1.63", sha256="1b11e49ec6e184ae2a10eccd05eda9534f32458fc644c12ab710c29a3a816f6f")
+    version(
+        "1.63",
+        sha256="1b11e49ec6e184ae2a10eccd05eda9534f32458fc644c12ab710c29a3a816f6f",
+        deprecated=True,
+    )
 
     depends_on("perl@5.8.4:", type=("build", "link", "run", "test"))
     depends_on("perl-cpan-meta-check@0.011:", type=("build", "test"))
