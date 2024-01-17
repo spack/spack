df96cda2f62c4c64ad2cbebd2a9527968438bd33
Maciej Wójcik <w8jcik@gmail.com>
2024-01-17 15:44:45 +0100

    py-biobb-analysis: Require dependency during build for tests
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-biobb-analysis/package.py
+++ var/spack/repos/builtin/packages/py-biobb-analysis/package.py
@@ -16,6 +16,6 @@ class PyBiobbAnalysis(PythonPackage):
 
     version("4.1.0", sha256="b4f0637f5f823c218ba4e066c5d7eb3288c2a9ee91fc72e9211f689e1d5c2a58")
 
-    depends_on("py-biobb-common@4.1.0", type="run")
+    depends_on("py-biobb-common@4.1.0", type=("build", "run"))
 
     depends_on("py-setuptools", type="build")
