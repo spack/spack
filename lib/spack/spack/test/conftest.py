dd58e922e7dc8d808f5f8bd1ad41d7c407ce2d90
Harmen Stoppels <me@harmenstoppels.nl>
2024-01-18 17:28:50 +0100

    oci: only push in parallel when forking (#42143)

--- lib/spack/spack/test/conftest.py
+++ lib/spack/spack/test/conftest.py
@@ -1950,17 +1950,5 @@ def pytest_runtest_setup(item):
 
 @pytest.fixture(scope="function")
 def disable_parallel_buildcache_push(monkeypatch):
-    class MockPool:
-        def map(self, func, args):
-            return [func(a) for a in args]
-
-        def starmap(self, func, args):
-            return [func(*a) for a in args]
-
-        def __enter__(self):
-            return self
-
-        def __exit__(self, *args):
-            pass
-
-    monkeypatch.setattr(spack.cmd.buildcache, "_make_pool", MockPool)
+    """Disable process pools in tests."""
+    monkeypatch.setattr(spack.cmd.buildcache, "_make_pool", spack.cmd.buildcache.NoPool)
