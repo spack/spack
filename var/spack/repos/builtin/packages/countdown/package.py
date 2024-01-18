804859377c2d36e4024baf84d3712073e079ee9b
Daniele Cesarini <d.cesarini@cineca.it>
2024-01-18 12:31:58 +0100

    Fixed conflicts syntax

--- var/spack/repos/builtin/packages/countdown/package.py
+++ var/spack/repos/builtin/packages/countdown/package.py
@@ -22,11 +22,9 @@ class Countdown(CMakePackage, CudaPackage):
     version("1.1.1", sha256="ee7f00ffc047f000a21a7a71f6ea6f4049afb1a8407608adc04993929ceba917")
 
     variant("acc_mpi", default=True, 
-        description="Enable the instrumentation of all accessory MPI functions",
-        conflict="+excl_all_mpi")
+        description="Enable the instrumentation of all accessory MPI functions")
     variant("coll_mpi", default=True, 
-        description="Enable the instrumentation of all collective MPI functions",
-        conflict="+excl_all_mpi")
+        description="Enable the instrumentation of all collective MPI functions")
     variant("debug_mpi", default=False, description="Enable the debug prints on MPI functions")
     variant("excl_all_mpi", default=False, 
         description="Disable the instrumentation of all MPI functions, "
@@ -34,14 +32,24 @@ class Countdown(CMakePackage, CudaPackage):
     variant("hwp_auto_discovery", default=True, description="Autodiscovery of hwp-states")
     variant("mosquitto", default=False, description="Enable MQTT message passing")
     variant("no_profiling", default=False, description="Disable MPI profiling")
-    variant("use_cpufreq", default=True, description="Manual set of cpufreq interface",
-            conflict="+hwp_auto_discovery")
-    variant("use_hwp", default=False, description="Manual set if hwp-states are available",
-            conflict="+hwp_auto_discovery")
+    variant("use_cpufreq", default=True, description="Manual set of cpufreq interface")
+    variant("use_hwp", default=False, description="Manual set if hwp-states are available")
     variant("p2p_mpi", default=True, 
-            description="Enable the instrumentation of all point-to-point MPI functions",
-            conflict="+excl_all_mpi")
+            description="Enable the instrumentation of all point-to-point MPI functions")
     variant("shared", default=True, description="Build shared libraries")
+
+    conflicts("+acc_mpi", when="+excl_all_mpi")
+    conflicts("+coll_mpi", when="+excl_all_mpi")
+    conflicts("+p2p_mpi", when="+excl_all_mpi")
+    conflicts("+excl_all_mpi", when="+acc_mpi")
+    conflicts("+excl_all_mpi", when="+coll_mpi")
+    conflicts("+excl_all_mpi", when="+p2p_mpi")
+    conflicts("+hwp_auto_discovery", when="+use_cpufreq")
+    conflicts("+hwp_auto_discovery", when="+use_hwp")
+    conflicts("+use_cpufreq", when="+hwp_auto_discovery")
+    conflicts("+use_cpufreq", when="+use_hwp")
+    conflicts("+use_hwp", when="+hwp_auto_discovery")
+    conflicts("+use_hwp", when="+use_cpufreq")
     
     depends_on("cmake@3.0.0:", type="build")
     depends_on("hwloc", type="link")
