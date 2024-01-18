c48399e42031004cfe279e8d9bdded3002f61380
Daniele Cesarini <d.cesarini@cineca.it>
2024-01-18 12:20:31 +0100

    Added conflicts and some reordering

--- var/spack/repos/builtin/packages/countdown/package.py
+++ var/spack/repos/builtin/packages/countdown/package.py
@@ -13,7 +13,7 @@ class Countdown(CMakePackage, CudaPackage):
     time to solution of the application."""
 
     homepage = "https://github.com/EEESlab/countdown"
-    url = "https://github.com/EEESlab/countdown/archive/refs/tags/v1.1.0.tar.gz"
+    url = "https://github.com/EEESlab/countdown/archive/refs/tags/v1.1.1.tar.gz"
 
     license("BSD-3-Clause")
 
@@ -22,41 +22,46 @@ class Countdown(CMakePackage, CudaPackage):
     version("1.1.1", sha256="ee7f00ffc047f000a21a7a71f6ea6f4049afb1a8407608adc04993929ceba917")
 
     variant("acc_mpi", default=True, 
-        description="Enable the instrumentation of all accessory MPI functions")
+        description="Enable the instrumentation of all accessory MPI functions",
+        conflict="+excl_all_mpi")
     variant("coll_mpi", default=True, 
-        description="Enable the instrumentation of all collective MPI functions")
-    variant("cpufreq_manual_set", default=True, description="Manual set of cpufreq interface")
+        description="Enable the instrumentation of all collective MPI functions",
+        conflict="+excl_all_mpi")
     variant("debug_mpi", default=False, description="Enable the debug prints on MPI functions")
     variant("excl_all_mpi", default=False, 
         description="Disable the instrumentation of all MPI functions, "
                     "except for initialization and finalization")
     variant("hwp_auto_discovery", default=True, description="Autodiscovery of hwp-states")
-    variant("hwp_manual_set", default=False, description="Manual set if hwp-states are available")
     variant("mosquitto", default=False, description="Enable MQTT message passing")
     variant("no_profiling", default=False, description="Disable MPI profiling")
+    variant("use_cpufreq", default=True, description="Manual set of cpufreq interface",
+            conflict="+hwp_auto_discovery")
+    variant("use_hwp", default=False, description="Manual set if hwp-states are available",
+            conflict="+hwp_auto_discovery")
     variant("p2p_mpi", default=True, 
-            description="Enable the instrumentation of all point-to-point MPI functions")
+            description="Enable the instrumentation of all point-to-point MPI functions",
+            conflict="+excl_all_mpi")
     variant("shared", default=True, description="Build shared libraries")
     
-    depends_on('cmake@3.0.0:', type='build')
-    depends_on('hwloc', type='link')
-    depends_on('mpi@3.0.0:', type='link')
-    depends_on("mosquitto", when="+mosquitto", type='link')
+    depends_on("cmake@3.0.0:", type="build")
+    depends_on("hwloc", type="link")
+    depends_on("mpi@3.0.0:", type="link")
+    depends_on("mosquitto", when="+mosquitto", type="link")
 
     def cmake_args(self):
         args = [
-            self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
             self.define_from_variant("CNTD_ENABLE_CUDA", "cuda"),
-            self.define_from_variant("CNTD_DISABLE_PROFILING_MPI", "no_profiling"),
             self.define_from_variant("CNTD_DISABLE_ACCESSORY_MPI", "acc_mpi"),
-            self.define_from_variant("CNTD_ENABLE_P2P_MPI", "p2p_mpi"),
             self.define_from_variant("CNTD_ENABLE_COLLECTIVE_MPI", "coll_mpi"),
-            self.define_from_variant("CNTD_DISABLE_ALL_MPI_EXCEPT_INI_FIN", "excl_all_mpi"),
             self.define_from_variant("CNTD_ENABLE_DEBUG_MPI", "debug_mpi"),
-            self.define_from_variant("CNTD_ENABLE_MOSQUITTO", "mosquitto"),
+            self.define_from_variant("CNTD_DISABLE_ALL_MPI_EXCEPT_INI_FIN", "excl_all_mpi"),
             self.define_from_variant("CNTD_HWP_AUTO_DISCOVER", "hwp_auto_discovery"),
-            self.define_from_variant("CNTD_HWP_DISCOVERED", "hwp_manual_set"),
-            self.define_from_variant("CNTD_USE_CPUFREQ", "cpufreq_manual_set"),
+            self.define_from_variant("CNTD_ENABLE_MOSQUITTO", "mosquitto"),
+            self.define_from_variant("CNTD_DISABLE_PROFILING_MPI", "no_profiling"),
+            self.define_from_variant("CNTD_USE_CPUFREQ", "use_cpufreq"),
+            self.define_from_variant("CNTD_HWP_DISCOVERED", "use_hwp"),
+            self.define_from_variant("CNTD_ENABLE_P2P_MPI", "p2p_mpi"),
+            self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
         ]
 
         return args
