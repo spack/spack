9d470a82f66d1b34276df73d0bf0eb5bf8220da1
Alec Scott <hi@alecbcs.com>
2024-01-19 12:30:08 -0800

    ripgrep: add v14.1.0 (#42181)

--- var/spack/repos/builtin/packages/ripgrep/package.py
+++ var/spack/repos/builtin/packages/ripgrep/package.py
@@ -19,6 +19,7 @@ class Ripgrep(CargoPackage):
 
     license("MIT OR Unlicense")
 
+    version("14.1.0", sha256="33c6169596a6bbfdc81415910008f26e0809422fda2d849562637996553b2ab6")
     version("14.0.3", sha256="f5794364ddfda1e0411ab6cad6dd63abe3a6b421d658d9fee017540ea4c31a0e")
     version("13.0.0", sha256="0fb17aaf285b3eee8ddab17b833af1e190d73de317ff9648751ab0660d763ed2")
     version("11.0.2", sha256="0983861279936ada8bc7a6d5d663d590ad34eb44a44c75c2d6ccd0ab33490055")
