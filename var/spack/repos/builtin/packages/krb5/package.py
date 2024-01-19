dbbe4d27b30d4413bee503005e49274537281a06
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-01-19 17:37:25 +0100

    Revert "krb5: add perl as a build dependency"

--- var/spack/repos/builtin/packages/krb5/package.py
+++ var/spack/repos/builtin/packages/krb5/package.py
@@ -36,7 +36,6 @@ class Krb5(AutotoolsPackage):
     depends_on("openssl@:1", when="@:1.19")
     depends_on("openssl")
     depends_on("gettext")
-    depends_on("perl", type="build")
     depends_on("findutils", type="build")
     depends_on("pkgconfig", type="build", when="^openssl~shared")
 
