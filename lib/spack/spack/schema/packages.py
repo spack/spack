e34ff533bf1248268e2069b7ce5d337761bd8c08
Peter Scheibel <scheibel1@llnl.gov>
2024-01-19 10:46:57 -0800

    auto style fix

--- lib/spack/spack/schema/packages.py
+++ lib/spack/spack/schema/packages.py
@@ -42,7 +42,10 @@ requirements = {
                             "spec": {"type": "string"},
                             "message": {"type": "string"},
                             "when": {"type": "string"},
-                            "turn_off_for": {"type": "array", "items": {"type": "string", "enum": ["externals", "build"]}}
+                            "turn_off_for": {
+                                "type": "array",
+                                "items": {"type": "string", "enum": ["externals", "build"]},
+                            },
                         },
                     },
                     {"type": "string"},
