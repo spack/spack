503dad5d582d678f1032853b4bcea3f817c832d7
Maciej Wójcik <w8jcik@gmail.com>
2024-01-21 19:32:26 +0100

    py-nglview: Correct dependency types

--- var/spack/repos/builtin/packages/py-nglview/package.py
+++ var/spack/repos/builtin/packages/py-nglview/package.py
@@ -16,10 +16,10 @@ class PyNglview(PythonPackage):
 
     version("3.0.8", sha256="f9e468cd813dac319cbeca6ae20ae099008ff3a06399f5d23d75582dde28623a")
 
-    depends_on("py-ipywidgets@7:", type="run")
-    depends_on("py-jupyterlab-widgets", type="run")
+    depends_on("py-ipywidgets@7:", type=("build", "run"))
+    depends_on("py-jupyterlab-widgets", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
 
     depends_on("py-setuptools@40.8.0:", type="build")
-    depends_on("py-jupyter-packaging@0.7.9:0.7", type=("build", "run"))
+    depends_on("py-jupyter-packaging@0.7.9:0.7", type="build")
     depends_on("py-versioneer-518", type="build")
