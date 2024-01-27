1865e228c4f6bd3cdafe175eb37eaa70147a065d
Robert Cohn <robert.s.cohn@intel.com>
2024-01-27 00:22:51 -0500

    [intel-oneapi-mkl] workaround linking issue for threads=openmp (#42327)

--- var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
@@ -179,7 +179,6 @@ class IntelOneapiMkl(IntelOneApiLibraryPackage):
 
     def _find_mkl_libs(self, shared):
         libs = []
-        threading_libs = []
 
         if self.spec.satisfies("+cluster"):
             libs.extend([self._xlp64_lib("libmkl_scalapack"), "libmkl_cdft_core"])
@@ -192,12 +191,6 @@ class IntelOneapiMkl(IntelOneApiLibraryPackage):
                 libs.append("libmkl_intel_thread")
             else:
                 libs.append("libmkl_gnu_thread")
-
-            # this is slightly different than what link-line advisor suggests.
-            # here it uses what the compiler suggests to use to enable openmp,
-            # instead of being explicit about in which path openmp libraries
-            # are located (e.g. intel libiomp5, gcc libgomp, clang libomp).
-            threading_libs += [self.compiler.openmp_flag]
         else:
             libs.append("libmkl_sequential")
 
@@ -247,8 +240,6 @@ class IntelOneapiMkl(IntelOneApiLibraryPackage):
                 resolved_libs = resolved_libs + self.spec["mpi"].libs
         except spack.error.NoLibrariesError:
             pass
-
-        resolved_libs += threading_libs
 
         return resolved_libs
 
