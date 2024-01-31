b5da12c240976f170ad5b235c81248a4b718e250
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-01-31 22:01:54 +0100

    ASP-based solver: improve reusing nodes with gcc-runtime
    This PR skips emitting dependency constraints on "gcc-runtime",
    for concrete specs that are considered for reuse.
    
    Instead, an appropriate version of gcc-runtime is recomputed
    considering also the concrete nodes from reused specs.
    
    This ensures that root nodes in a DAG have always a runtime
    that is at a version greater or equal than their dependencies.

--- lib/spack/spack/test/concretize_compiler_runtimes.py
+++ lib/spack/spack/test/concretize_compiler_runtimes.py
@@ -60,3 +60,36 @@ def test_external_nodes_do_not_have_runtimes(runtime_repo, mutable_config, tmp_p
     assert a.dependencies("gcc-runtime")
     assert a.dependencies("b")
     assert not b.dependencies("gcc-runtime")
+
+
+@pytest.mark.parametrize(
+    "root_str,reused_str,expected,nruntime",
+    [
+        # The reused runtime is older than we need, thus we'll add a more recent one for a
+        ("a%gcc@10.2.1", "b%gcc@4.5.0", {"a": "gcc-runtime@10.2.1", "b": "gcc-runtime@4.5.0"}, 2),
+        # The root is compiled with an older compiler, thus we'll reuse the runtime from b
+        ("a%gcc@4.5.0", "b%gcc@10.2.1", {"a": "gcc-runtime@10.2.1", "b": "gcc-runtime@10.2.1"}, 1),
+    ],
+)
+def test_reusing_specs_with_gcc_runtime(root_str, reused_str, expected, nruntime, runtime_repo):
+    """Tests that we can reuse specs with a "gcc-runtime" leaf node. In particular, checks
+    that the semantic for gcc-runtimes versions accounts for reused packages too.
+    """
+    reused_spec = spack.spec.Spec(reused_str).concretized()
+    assert f"{expected['b']}" in reused_spec
+
+    setup = spack.solver.asp.SpackSolverSetup(tests=False)
+    driver = spack.solver.asp.PyclingoDriver()
+    result, _, _ = driver.solve(
+        setup, [spack.spec.Spec(f"{root_str} ^{reused_str}")], reuse=[reused_spec]
+    )
+
+    root = result.specs[0]
+
+    runtime_a = root.dependencies("gcc-runtime")[0]
+    assert runtime_a.satisfies(expected["a"])
+    runtime_b = root["b"].dependencies("gcc-runtime")[0]
+    assert runtime_b.satisfies(expected["b"])
+
+    runtimes = [x for x in root.traverse() if x.name == "gcc-runtime"]
+    assert len(runtimes) == nruntime
