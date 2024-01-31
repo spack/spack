30b1a33faf02c27234ce37d868ce6d49631b1020
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2024-01-31 09:08:28 +0000

    seqkit: update to @2.6.1, drop deprecated version and build system

--- var/spack/repos/builtin/packages/seqkit/package.py
+++ var/spack/repos/builtin/packages/seqkit/package.py
@@ -12,28 +12,20 @@ class Seqkit(Package):
     homepage = "https://bioinf.shenwei.me/seqkit/"
     url = "https://github.com/shenwei356/seqkit/archive/refs/tags/v2.4.0.tar.gz"
 
-    version("2.4.0", sha256="c319f3d5feb7c99309e654042432959f01bbc5f7e4c71f55dc9854df46c73c7f")
-    version(
-        "0.10.1",
-        sha256="82f1c86dc4bd196403a56c2bf3ec063e5674a71777e68d940c4cc3d8411d2e9d",
-        url="https://github.com/shenwei356/seqkit/releases/download/v0.10.1/seqkit_linux_amd64.tar.gz",
-        deprecated=True,
-    )
+    license("MIT", checked_by="A-N-Other")
 
-    depends_on("go@1.17:", type="build", when="@2.0.0:")
-    depends_on("go@1.16:", type="build", when="@0.15.0:0.16.1")
+    version("2.6.1", sha256="d88249bd3b630c908ebd308abaa9cd7acb7a781c12bab877d3daaab56f43c443")
+    version("2.5.1", sha256="76d105921f918be20e616fbb607fe0fb2db603535a254ec0f853cb36bef817da")
+    version("2.4.0", sha256="c319f3d5feb7c99309e654042432959f01bbc5f7e4c71f55dc9854df46c73c7f")
+
+    depends_on("go@1.17:", type="build")
 
     phases = ["build", "install"]
 
     def build(self, spec, prefix):
-        """Build using `go build`"""
-
-        if self.spec.satisfies("@0.15.0:"):
-            go("build", "-C", "seqkit")
+        # Build using `go build`
+        go("build", "-C", "seqkit")
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        if self.spec.satisfies("@0.15.0:"):
-            install(join_path("seqkit", "seqkit"), prefix.bin)
-        else:
-            install("seqkit", prefix.bin)
+        install(join_path("seqkit", "seqkit"), prefix.bin)
