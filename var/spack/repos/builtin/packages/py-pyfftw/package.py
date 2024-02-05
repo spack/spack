214f354b83482f0afa537e307ea507b3b280ca04
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2024-02-05 10:11:55 +0000

    updates from github review

--- var/spack/repos/builtin/packages/py-pyfftw/package.py
+++ var/spack/repos/builtin/packages/py-pyfftw/package.py
@@ -21,18 +21,18 @@ class PyPyfftw(PythonPackage):
     version("0.11.1", sha256="05ea28dede4c3aaaf5c66f56eb0f71849d0d50f5bc0f53ca0ffa69534af14926")
     version("0.10.4", sha256="739b436b7c0aeddf99a48749380260364d2dc027cf1d5f63dafb5f50068ede1a")
 
-    depends_on("python@3.8:", when="@0.13.1:")
+    depends_on("python@3.8:", type=("build", "run"), when="@0.13.1:")
 
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools@:59.4.0", type="build")
     depends_on("py-cython@0.29.18:0", type="build", when="@0.13:")
     depends_on("py-cython@0.29:0", type="build", when="@:0.13")
 
     depends_on("py-numpy@1.20:1", type=("build", "run"), when="@0.13.1:")
-    depends_on("py-numpy@1.16:1", type=("build", "run"), when="@0.13")
-    depends_on("py-numpy@1.10:1", type=("build", "run"), when="@0.11:0.13")
+    depends_on("py-numpy@1.16:1", type=("build", "run"), when="@0.13.0")
+    depends_on("py-numpy@1.10:1", type=("build", "run"), when="@0.11:0.12")
     depends_on("py-numpy@1.6:1", type=("build", "run"), when="@:0.10.4")
 
-    depends_on("fftw")
+    depends_on("fftw@3.3:")
 
     def setup_build_environment(self, env):
         env.append_flags("LDFLAGS", self.spec["fftw"].libs.search_flags)
