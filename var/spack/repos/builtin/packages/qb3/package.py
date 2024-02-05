5d8a09c0b1e2aaa019d54ceff28de453dd0a1b06
Tom Payerle <payerle@umd.edu>
2024-02-05 12:32:23 -0500

    qb3: Update libs method to not use recursive find_libraries()
    Based on suggestion from @alalazo

--- var/spack/repos/builtin/packages/qb3/package.py
+++ var/spack/repos/builtin/packages/qb3/package.py
@@ -23,4 +23,15 @@ class Qb3(CMakePackage):
     def libs(self):
         # Override because libs have different case than Spack package name
         name = "libQB3*"
-        return find_libraries(name, root=self.prefix, shared=True, recursive=True)
+        # We expect libraries to be in either lib64 or lib directory
+        # Try lib64 first
+        root = self.prefix.lib64
+        liblist = find_libraries(name, root=root, shared=True, recursive=False)
+        if liblist:
+            # Found libs in lib64, so return those
+            return liblist
+        else:
+            # Did not find libs in lib64, so try lib w/out 64 suffix
+            root = self.prefix.lib
+            liblist = find_libraries(name, root=root, shared=True, recursive=False)
+            return liblist
