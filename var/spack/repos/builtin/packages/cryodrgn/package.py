2ea29f0f59dfca5efdeb78452a8c8f1137852a5e
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2024-02-05 13:27:02 +0000

    correcting dependency ranges

--- var/spack/repos/builtin/packages/cryodrgn/package.py
+++ var/spack/repos/builtin/packages/cryodrgn/package.py
@@ -18,20 +18,22 @@ class Cryodrgn(PythonPackage):
 
     version("2.3.0", sha256="9dd75967fddfa56d6b2fbfc56933c50c9fb994326112513f223e8296adbf0afc")
 
+    depends_on("python@3.7:", type=("build", "run"))
+
     depends_on("py-setuptools@61:", type="build")
     depends_on("py-setuptools-scm@6.2:", type="build")
 
     depends_on("py-torch@1:", type=("build", "run"))
-    depends_on("py-pandas@:2", type=("build", "run"))
-    depends_on("py-numpy@:1.23", type=("build", "run"))
-    depends_on("py-matplotlib@:3.7", type=("build", "run"))
+    depends_on("py-pandas@:1", type=("build", "run"))
+    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-matplotlib", type=("build", "run"))
     depends_on("py-pyyaml", type=("build", "run"))
     depends_on("py-scipy@1.3.1:", type=("build", "run"))
     depends_on("py-scikit-learn", type=("build", "run"))
-    depends_on("py-seaborn@:0.12", type=("build", "run"))
+    depends_on("py-seaborn@:0.11", type=("build", "run"))
     depends_on("py-cufflinks", type=("build", "run"))
     depends_on("py-jupyterlab", type=("build", "run"))
     depends_on("py-notebook@:7", type=("build", "run"))
     depends_on("py-umap-learn", type=("build", "run"))
-    depends_on("py-ipywidgets@:8", type=("build", "run"))
+    depends_on("py-ipywidgets@:7", type=("build", "run"))
     depends_on("py-healpy", type=("build", "run"))
