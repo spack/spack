0de1be780a011de2d08254a1d6d28e1a6f2e9d9f
inagaki <inagaki-k@fujitsu.com>
2024-02-06 17:25:31 +0900

    siesta-relmax3: fixed styles

--- var/spack/repos/builtin/packages/siesta-relmax3/package.py
+++ var/spack/repos/builtin/packages/siesta-relmax3/package.py
@@ -4,17 +4,16 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
-import os
 import shutil
 
 from spack.package import *
 
 
 class SiestaRelmax3(MakefilePackage):
-    """The MaX3 branch of the SIESTA program is a special branch 
-    that contains all the features currently implemented within 
-    the MaX Center of Excellence EU H2020 project. 
-    This branch is intended for HPC users who want to try 
+    """The MaX3 branch of the SIESTA program is a special branch
+    that contains all the features currently implemented within
+    the MaX Center of Excellence EU H2020 project.
+    This branch is intended for HPC users who want to try
     the new optimizations and enhancements, and provide feedback on them.
     """
 
@@ -61,7 +60,7 @@ class SiestaRelmax3(MakefilePackage):
         sh = which("sh")
         with working_dir("Obj", create=True):
             sh("../Src/obj_setup.sh")
-            
+
             # setup arch.make(compilers and flags)
             shutil.copy("../Config/mk-build/sample-arch-makes/gcc-modules.mk", "./arch.make")
             arch_make = FileFilter("./arch.make")
@@ -71,24 +70,45 @@ class SiestaRelmax3(MakefilePackage):
                 arch_make.filter("FFLAGS=.*", "FFLAGS= -Kfast --Nlst -Cpp -X08")
                 arch_make.filter("FFLAGS_DEBUG=.*", "FFLAGS_DEBUG= -g -O0 -fPIC -Nlst -Cpp -X08")
                 arch_make.filter("RANLIB=echo", "RANLIB=echo\nLDFLAGS= -Kparallel -Kopenmp")
-            
+
             # setup build.mk
             build_mk = FileFilter("./build.mk")
             build_mk.filter("#XMLF90_ROOT=.*", "XMLF90_ROOT={0}".format(spec["xmlf90"].prefix))
             build_mk.filter("#PSML_ROOT=.*", "PSML_ROOT={0}".format(spec["libpsml"].prefix))
             build_mk.filter("#GRIDXC_ROOT=.*", "GRIDXC_ROOT={0}".format(spec["libgridxc"].prefix))
             build_mk.filter("#ELPA_ROOT=.*", "ELPA_ROOT={0}".format(spec["elsi"].prefix))
-            build_mk.filter("#ELPA_INCLUDE_DIRECTORY=.*", "ELPA_INCLUDE_DIRECTORY={0}".format(spec["elsi"].prefix.include))
+            build_mk.filter(
+                "#ELPA_INCLUDE_DIRECTORY=.*",
+                "ELPA_INCLUDE_DIRECTORY={0}".format(spec["elsi"].prefix.include),
+            )
             build_mk.filter("#FLOOK_ROOT=.*", "FLOOK_ROOT={0}".format(spec["flook"].prefix))
             build_mk.filter("#DBCSR_ROOT=.*", "DBCSR_ROOT={0}".format(spec["dbcsr"].prefix))
-            build_mk.filter("#OMM_SPARSE_BUNDLE_ROOT=.*", "OMM_SPARSE_BUNDLE_ROOT={0}".format(spec["omm-bundle"].prefix))
+            build_mk.filter(
+                "#OMM_SPARSE_BUNDLE_ROOT=.*",
+                "OMM_SPARSE_BUNDLE_ROOT={0}".format(spec["omm-bundle"].prefix),
+            )
             build_mk.filter("#NETCDF_ROOT=.*", "NETCDF_ROOT={0}".format(spec["netcdf-c"].prefix))
-            build_mk.filter("#NETCDF_LIBS=.*","NETCDF_LIBS= -L{0} -L{1} -L{2} -lnetcdff -lnetcdf  -lhdf5_hl -lhdf5"
-             .format(spec["netcdf-c"].prefix.lib,spec["netcdf-fortran"].prefix.lib,spec["hdf5"].prefix.lib))
-            build_mk.filter("#NETCDF_INCFLAGS=.*", "NETCDF_INCFLAGS=-I{0} -I{1}"
-             .format(spec["netcdf-c"].prefix.include,spec["netcdf-fortran"].prefix.include))
-            build_mk.filter("#SCALAPACK_LIBS=.*", "SCALAPACK_LIBS={0}".format(spec["scalapack"].libs))
-            build_mk.filter("#LAPACK_LIBS=.*", "LAPACK_LIBS={0} {1}".format(spec["lapack"].libs,spec["blas"].libs))
+            build_mk.filter(
+                "#NETCDF_LIBS=.*",
+                "NETCDF_LIBS= -L{0} -L{1} -L{2} -lnetcdff -lnetcdf  -lhdf5_hl -lhdf5".format(
+                    spec["netcdf-c"].prefix.lib,
+                    spec["netcdf-fortran"].prefix.lib,
+                    spec["hdf5"].prefix.lib,
+                ),
+            )
+            build_mk.filter(
+                "#NETCDF_INCFLAGS=.*",
+                "NETCDF_INCFLAGS=-I{0} -I{1}".format(
+                    spec["netcdf-c"].prefix.include, spec["netcdf-fortran"].prefix.include
+                ),
+            )
+            build_mk.filter(
+                "#SCALAPACK_LIBS=.*", "SCALAPACK_LIBS={0}".format(spec["scalapack"].libs)
+            )
+            build_mk.filter(
+                "#LAPACK_LIBS=.*",
+                "LAPACK_LIBS={0} {1}".format(spec["lapack"].libs, spec["blas"].libs),
+            )
 
     def build(self, spec, prefix):
         with working_dir("Obj"):
@@ -98,4 +118,3 @@ class SiestaRelmax3(MakefilePackage):
         mkdir(prefix.bin)
         with working_dir("Obj"):
             install("siesta", prefix.bin)
-
