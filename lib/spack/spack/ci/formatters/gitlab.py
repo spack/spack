9272799ee8882928c43893b90e1b2a29dd093b50
Scott Wittenburg <scott.wittenburg@kitware.com>
2024-02-06 14:28:42 -0700

    explicit import from parent package

--- lib/spack/spack/ci/formatters/gitlab.py
+++ lib/spack/spack/ci/formatters/gitlab.py
@@ -14,7 +14,7 @@ import spack.binary_distribution as bindist
 import spack.config as cfg
 import spack.util.spack_yaml as syaml
 
-from ... import *
+from .. import PipelineDag
 from . import formatter
 
 
