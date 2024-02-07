1c5b10920490bc2e626ce94c6ce864dd8ddeb3bf
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2024-02-07 23:44:00 +0000

    probconsrna: correcting build, adding patch

--- var/spack/repos/builtin/packages/probconsrna/package.py
+++ var/spack/repos/builtin/packages/probconsrna/package.py
@@ -6,20 +6,27 @@
 from spack.package import *
 
 
-class Probconsrna(Package):
-    """Experimental version of PROBCONS with parameters estimated via
-    unsupervised training on BRAliBASE"""
+class Probconsrna(MakefilePackage):
+    """Experimental version of PROBCONS with parameters estimated via unsupervised training
+    on BRAliBASE"""
 
     homepage = "http://probcons.stanford.edu/"
     url = "http://probcons.stanford.edu/probconsRNA.tar.gz"
 
-    version("2005-6-7", sha256="7fe4494bd423db1d5f33f5ece2c70f9f66a0d9112e28d3eaa7dfdfe7fa66eba8")
+    version("1.10", sha256="7fe4494bd423db1d5f33f5ece2c70f9f66a0d9112e28d3eaa7dfdfe7fa66eba8")
 
-    def install(self, build, prefix):
+    # update includes to bring inline with modern C++
+    # (from patch file in the bioconda recipe)
+    patch("probconsrna.patch")
+
+    def edit(self, spec, prefix):
+        filter_file("CXX = .*", f"CXX = {spack_cxx}", "Makefile")
+
+    def build(self, spec, prefix):
+        make("probcons", "compare", "project")
+
+    def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        install("compare", prefix.bin)
-        install("makegnuplot", prefix.bin)
-        install("probcons", prefix.bin)
-        # needed for tcoffee
         install("probcons", prefix.bin.probconsRNA)
+        install("compare", prefix.bin)
         install("project", prefix.bin)
