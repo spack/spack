40cdd83d5f02d284299c4055575d73ff73e826f4
Alex C Leute <aclrc@rit.edu>
2024-02-07 13:39:29 -0500

    py-glmnet-python: Updated homepage, and added comments

--- var/spack/repos/builtin/packages/py-glmnet-python/package.py
+++ var/spack/repos/builtin/packages/py-glmnet-python/package.py
@@ -8,16 +8,21 @@ from spack.package import *
 
 
 class PyGlmnetPython(PythonPackage):
-    """Glmnet for python. Not to be confused with py-glmnet"""
+    """This is a python version of the popular glmnet library (beta release).
+    Glmnet fits the entire lasso or elastic-net regularization path for linear
+    regression, logistic and multinomial regression models, poisson regression
+    and the cox model."""
 
-    homepage = "https://www.example.com"
+    # Not to be confused with py-glmnet
+
+    homepage = "https://github.com/johnlees/glmnet_python/"
     # Not availible on PyPI. Note that this is a fork of
     # https://github.com/bbalasub1/glmnet_python, as required for py-pyseer
     url = "https://github.com/johnlees/glmnet_python/archive/v1.0.2.zip"
 
     version("1.0.2", sha256="cc80020dcebc5366dcc061aec59318efac69d23578066326d925bfc27a23cb27")
 
-    depends_on("gcc", type="build")
+    depends_on("gcc", type="build") # For gfortran
     depends_on("py-joblib@0.10.3:", type=("build", "run"))
     # Not in setup.py, but imported and used:
     depends_on("py-numpy", type=("build", "run")) 
