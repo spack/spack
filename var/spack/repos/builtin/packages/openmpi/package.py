09fdea959f69430ba84f8bcc77e62157fcb8547e
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2024-02-08 15:41:46 +0100

    openmpi: add patch fixing MPI_MIN for unsigned long (#32392)

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -411,6 +411,13 @@ class Openmpi(AutotoolsPackage, CudaPackage):
     # To fix performance regressions introduced while fixing a bug in older
     # gcc versions on x86_64, Refs. open-mpi/ompi#8603
     patch("opal_assembly_arch.patch", when="@4.0.0:4.0.5,4.1.0")
+    # Fix reduce operations for unsigned long integers
+    # See https://github.com/open-mpi/ompi/issues/10648
+    patch(
+        "https://github.com/open-mpi/ompi/commit/8e6d9ba8058a0c128438dbc0cd6476f1abb1d4f1.patch?full_index=1",
+        sha256="12f3aabbcdb02f28138e250273c2f62591db4b1f9f8aa3dcc3ef9ed551f4f587",
+        when="@4.0.7,4.1.2:4.1.4",
+    )
     # To fix an error in Open MPI configury related to findng dl lib.
     # This is specific to the 5.0.0 release.
     patch("fix-for-dlopen-missing-symbol-problem.patch", when="@5.0.0")
