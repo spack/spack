fc0a416e6e3761311740418fedd4bc0f4554ffe7
James Beal <jb23@sanger.ac.uk>
2024-02-09 09:01:23 +0000

    New version

--- var/spack/repos/builtin/packages/py-scipy/package.py
+++ var/spack/repos/builtin/packages/py-scipy/package.py
@@ -18,6 +18,7 @@ class PyScipy(PythonPackage):
     license("BSD-3-Clause")
 
     version("main", branch="main")
+    version("1.12.0", sha256="4bf5abab8a36d20193c698b0f1fc282c1d083c94723902c447e5d2f1780936a3")
     version("1.11.4", sha256="90a2b78e7f5733b9de748f589f09225013685f9b218275257f8a8168ededaeaa")
     version("1.11.3", sha256="bba4d955f54edd61899776bad459bf7326e14b9fa1c552181f0479cc60a568cd")
     version("1.11.2", sha256="b29318a5e39bd200ca4381d80b065cdf3076c7d7281c5e36569e99273867f61d")
