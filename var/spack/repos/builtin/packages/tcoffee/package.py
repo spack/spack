153274882c6ffdfeaed5f2237a6f7f1763324337
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2024-02-09 10:50:43 +0000

    tcoffee: final updates

--- var/spack/repos/builtin/packages/tcoffee/package.py
+++ var/spack/repos/builtin/packages/tcoffee/package.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from datetime import date
 import os
 
 from spack.package import *
@@ -12,22 +13,26 @@ class Tcoffee(MakefilePackage):
     multiple alignments of DNA, RNA, protein sequences and structures"""
 
     homepage = "https://tcoffee.org/Projects/tcoffee/index.html"
+
+    # tcoffee is officially distributed through
+    #   https://tcoffee.org/Projects/tcoffee/index.html
+    # however these source bundles do not build properly with spack - error-free completion
+    # but the binary segfaults. These bundles are generated from a CI pipeline running on
+    # the top-level GitHub source, which does build properly, so we use that...
     git = "https://github.com/cbcrg/tcoffee.git"
-    url = "https://s3.eu-central-1.amazonaws.com/tcoffee-packages/Archives/T-COFFEE_distribution_Version_13.46.0.919e8c6b.tar.gz"
 
     license("GPL-2.0-only", checked_by="A-N-Other")
 
-    version(
-        "13.46.0.919e8c6b",
-        sha256="31fd0ca0734974c93cb68bef6e394f463a4589c3315fd28cf2bd41b8a167db22")
+    version("13.46.0.919e8c6b", tag="Version_13.46.0.919e8c6b")
     version(
         "11.0",
         commit="f389b558e91d0f82e7db934d9a79ce285f853a71",
         deprecated=True,
     )
 
-    depends_on("perl", type="run")
+    depends_on("perl", type=("build", "link", "run"))
     depends_on("perl-xml-simple", type="run")
+    depends_on("perl-soap-lite", type="run")
 
     # t_coffee / common dependencies
     depends_on("strike", type="run")
@@ -36,30 +41,37 @@ class Tcoffee(MakefilePackage):
     depends_on("mafft", type="run")
     depends_on("clustal-omega", type="run")
 
+    # Variants and their required dependencies are formed by parsing the program:mode pairs
+    # within the ./lib/data_headers/tclinkdb.txt file.
     #
-    # Variants and their required dependencies are taken from the ./install script (L1642-)
-    #
-    # Notes:
+    # Notes on listed dependencies:
+    # * All dependencies are technically optional. They extend the core functionality.
     # - `x3dna`, `hmmtop`, and `fugue` are distributed manually / under manual license
-    #   agreement. These have been removed to allow simpler install of the package.
-    # - `saracoffee` variant is disabled for now due to dependency on `x3dna`.
-    # - `dialign-tx` doesn't compile properly and is remaining as an optional dependency
-    # - `lsqman` and `align_pdb` are extremely old. I can't find a extant source of these
-    #   programs and/or can't persuade them to compile.
-    #
+    #   agreement. These have been removed here to allow simpler install of the package.
+    #   - `saracoffee` variant is a stub due to dependency on `x3dna`.
+    #   - `psicoffee` variant is a stub due to dependency on `hmmtop`.
+    # - `lsqman` and `align_pdb` are extremely old. I can't find extant / reputable sources
+    #   for these programs.
+    # - `dialign-t` has disappeared in favour of its successor `dialign-tx`. I can't find an
+    #   extant download for this version.
+    # - `muscle4` was never released properly and is dropped here. This was an experimental
+    #   version between `3` and `muscle5`.
+    # - The version of `upp` expected by tcoffee is obsolete and the dependency is dropped
+    #   here. UPP2 is now distributed with `sepp` but has a different interface.
 
+    variant("all", default=True, description="Enable all functionality", sticky=True)
     variant("mcoffee", default=False, description="Meta-alignment with multiple MSA programs")
     variant("psicoffee", default=False, description="Homology extended MSA")
     variant("expresso", default=False, description="Very accurate structure-based MSA")
     variant("3dcoffee", default=False, description="Multiple structure alignments")
     variant("rcoffee", default=False, description="RNA MSA")
-    # variant("saracoffee", default=False, description="Structure-based multiple RNA alignment")
-    variant("trmsd", default=False, description="RMSD-based structural clustering of related proteins")
+    variant("saracoffee", default=False, description="Structure-based multiple RNA alignment")
+    variant("trmsd", default=False, description="RMSD-based structural clustering of proteins")
+    variant("seq_reformat", default=False, description="Clean, reformat, and compare MSAs and trees")
 
-    with when("+mcoffee"):
+    with when("+all") or when("+mcoffee"):
         depends_on("clustalw", type="run") # this is clustalw2
-        depends_on("dialign", type="run") # this is dialign-t
-        # depends_on("dialign-tx", type="run")
+        depends_on("dialign-tx", type="run")
         depends_on("poamsa", type="run")
         depends_on("probcons", type="run")
         depends_on("probconsrna", type="run")
@@ -75,7 +87,7 @@ class Tcoffee(MakefilePackage):
         depends_on("prank", type="run")
         depends_on("fsa", type="run")
 
-    with when("+expresso") or when("+3dcoffee"):
+    with when("+all") or when("+expresso") or when("+3dcoffee"):
         depends_on("sap", type="run")
         depends_on("tmalign", type="run")
         depends_on("mustang", type="run")
@@ -83,11 +95,11 @@ class Tcoffee(MakefilePackage):
         # depends_on("pdb_align", type="run")
         # depends_on("fugue", type="run")
 
-    with when("+psicoffee"):
+    with when("+all") or when("+psicoffee"):
         # depends_on("hmmtop", type="run")
         pass
 
-    with when("+rcoffee"):
+    with when("+all") or when("+rcoffee"):
         depends_on("clustalw", type="run") # this is clustalw2
         depends_on("famsa", type="run")
         depends_on("muscle", type="run")
@@ -95,18 +107,18 @@ class Tcoffee(MakefilePackage):
         depends_on("consan", type="run")
         depends_on("viennarna", type="run")
 
-    # with when("+saracoffee"):
-    #     depends_on("x3dna", type="run")
+    with when("+all") or when("+saracoffee"):
+        # depends_on("x3dna", type="run")
+        pass
 
-    with when("+trmsd"):
+    with when("+all") or when("+trmsd"):
         depends_on("phylip", type="run")
 
-    @property
-    def build_directory(self):
-        if self.spec.satisfies("@13:"):
-            return "t_coffee_source"
-        else:
-            return "compile"
+    with when("+all") or when("+seq_reformat"):
+        depends_on("phylip", type="run")
+        depends_on("viennarna", type="run")
+
+    build_directory = "t_coffee/src"
 
     @property
     def sys_max_pid(self):
@@ -119,14 +131,14 @@ class Tcoffee(MakefilePackage):
             return 4194304 # 64-bit default
 
     def edit(self, spec, prefix):
-        with working_dir(self.build_directory):
-            filter_file("CC=.*", f"CC={spack_cxx}", "makefile")
-            filter_file("#define MAX_N_PID.*", f"#define MAX_N_PID {self.sys_max_pid}", "coffee_defines.h")
-
-    def flag_handler(self, name, flags):
-        if name == "cxxflags":
-            flags.append("-std=c++11")
-        return flags, None, None
+        filter_file(
+            "CC =.*",
+            f"CC = {spack_cxx}",
+            join_path(self.build_directory, "makefile"))
+        filter_file(
+            "#define MAX_N_PID.*",
+            f"#define MAX_N_PID {self.sys_max_pid}",
+            join_path("lib", "coffee_defines.h"))
 
     def build(self, spec, prefix):
         with working_dir(self.build_directory):
@@ -134,9 +146,8 @@ class Tcoffee(MakefilePackage):
 
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
-        with working_dir(self.build_directory):
-            install("t_coffee", prefix.bin)
-        install_tree("mcoffee", prefix.mcoffee)
+        install(join_path(self.build_directory, "t_coffee"), prefix.bin)
+        install_tree(join_path("lib", "mcoffee"), prefix.mcoffee)
 
     def setup_run_environment(self, env):
         env.set("MCOFFEE_4_TCOFFEE", self.prefix.mcoffee)
