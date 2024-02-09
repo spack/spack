b89b5e30e2d16d7fb94dc29c25fa554b93c9f6e3
inagaki <inagaki-k@fujitsu.com>
2024-02-09 12:04:23 +0900

    xmlf90: pinned commit, added comment and fixed style

--- var/spack/repos/builtin/packages/xmlf90/package.py
+++ var/spack/repos/builtin/packages/xmlf90/package.py
@@ -15,22 +15,22 @@ class Xmlf90(AutotoolsPackage):
 
     license("BSD-2-Clause")
 
-    version("1.6.3", tag="1.6.3")
-    version("1.6.2", tag="1.6.2")
-    version("1.6.1", tag="1.6.1")
-    version("1.6.0", tag="1.6.0")
-    version("1.5.6", tag="1.5.6")
-    version("1.5.5", tag="1.5.5")
-    version("1.5.4", tag="xmlf90-1.5.4")
-    version("1.5.3", tag="xmlf90-1.5.3")
-    version("1.5.2", tag="xmlf90-1.5.2")
+    version("1.6.3", tag="1.6.3", commit="963fe5d11487233d43ac59dd0c1340a7b2cc3dc5")
+    version("1.6.2", tag="1.6.2", commit="2be1d8d2253233093857e8555e50e74857d0bb51")
+    version("1.6.1", tag="1.6.1", commit="773726675fc4290334444adcc76d9af96871f25f")
+    version("1.6.0", tag="1.6.0", commit="36c64350138af1edfbbd8e0ce23b36a0ac8db617")
+    version("1.5.6", tag="1.5.6", commit="c51a18e86b0d9e7d624509e799ee823c95f69fd1")
+    version("1.5.5", tag="1.5.5", commit="166c0458750f973e2d6f86dd59738f3e3088295e")
+    version("1.5.4", tag="xmlf90-1.5.4", commit="5b21b2c63c834adaa2327e22daa0c856644b2c75")
+    version("1.5.3", tag="xmlf90-1.5.3", commit="d55a5bcf8a0d4e592a03585209435d342d8e6d0f")
+    version("1.5.2", tag="xmlf90-1.5.2", commit="b238aec8719b7c40c7b7252c5f200818a0d5004a")
 
     depends_on("autoconf@2.69:", type="build")
     depends_on("automake@1.14:", type="build")
     depends_on("libtool@2.4.2:", type="build")
     depends_on("m4", type="build")
-    
-    build_directory = "spack-build"
+
+    # Additional include directory specification required for Fujitsu compiler
     patch("fj_modmakefile.patch", when="%fj")
 
     @when("@1.5.2")
