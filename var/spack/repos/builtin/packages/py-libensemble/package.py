a42e5e8309246e230b6cce76c4ad5cc20d33d35f
Stephen Hudson <shudson@anl.gov>
2024-02-11 14:57:04 -0600

    libEnsemble: add v1.2.0 (#42591)

--- var/spack/repos/builtin/packages/py-libensemble/package.py
+++ var/spack/repos/builtin/packages/py-libensemble/package.py
@@ -12,7 +12,7 @@ class PyLibensemble(PythonPackage):
     """Library for managing ensemble-like collections of computations."""
 
     homepage = "https://libensemble.readthedocs.io"
-    pypi = "libensemble/libensemble-1.1.0.tar.gz"
+    pypi = "libensemble/libensemble-1.2.0.tar.gz"
     git = "https://github.com/Libensemble/libensemble.git"
     maintainers("shuds13", "jlnav")
 
@@ -21,6 +21,7 @@ class PyLibensemble(PythonPackage):
     license("BSD-3-Clause")
 
     version("develop", branch="develop")
+    version("1.2.0", sha256="e1076e8eea7844d3799f92d136586eca4da34ec753bf41a8d1be04d7a45ec4c1")
     version("1.1.0", sha256="3e3ddc4233272d3651e9d62c7bf420018930a4b9b135ef9ede01d5356235c1c6")
     version("1.0.0", sha256="b164e044f16f15b68fd565684ad8ce876c93aaeb84e5078f4ea2a29684b110ca")
     version("0.10.2", sha256="ef8dfe5d233dcae2636a3d6aa38f3c2ad0f42c65bd38f664e99b3e63b9f86622")
@@ -60,6 +61,7 @@ class PyLibensemble(PythonPackage):
     depends_on("py-psutil", when="@0.7.1:", type=("build", "run"))
     depends_on("py-setuptools", when="@0.10.2:", type="build")
     depends_on("py-setuptools", when="@:0.10.1", type=("build", "run"))
+    depends_on("py-pydantic@1.10:", when="@1.2.0:", type=("build", "run"))
     depends_on("py-pydantic@:1", when="@0.10:", type=("build", "run"))
     depends_on("py-tomli@1.2.1:", when="@1:", type=("build", "run"))
     depends_on("py-tomli", when="@0.10:", type=("build", "run"))
