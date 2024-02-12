856f5955101589f188ca6042af1b64e426f2c833
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2024-02-12 10:27:06 +0100

    add latest tags of eccodes/eckit/metkit

--- var/spack/repos/builtin/packages/eckit/package.py
+++ var/spack/repos/builtin/packages/eckit/package.py
@@ -20,6 +20,7 @@ class Eckit(CMakePackage):
 
     license("Apache-2.0")
 
+    version("1.25.2", sha256="a611d26d50a9f2133b75100567a890eb0e0a48a96669b8c8475baf9d6f359397")
     version("1.24.5", sha256="2fd74e04c20a59f9e13635828d9da880e18f8a2cb7fd3bfd0201e07071d6ec41")
     version("1.24.4", sha256="b6129eb4f7b8532aa6905033e4cf7d09aadc8547c225780fea3db196e34e4671")
     version("1.23.1", sha256="cd3c4b7a3a2de0f4a59f00f7bab3178dd59c0e27900d48eaeb357975e8ce2f15")
