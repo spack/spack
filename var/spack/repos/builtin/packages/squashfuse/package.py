d0a1ea7fa0c1e0e05df5137591f18c2e4b2944e5
Jordan Ogas <jogas@lanl.gov>
2024-02-12 16:52:20 -0700

    add autofriends to 0.1.105 deps

--- var/spack/repos/builtin/packages/squashfuse/package.py
+++ var/spack/repos/builtin/packages/squashfuse/package.py
@@ -56,10 +56,10 @@ class Squashfuse(AutotoolsPackage):
     depends_on("xz", when="+xz")
     depends_on("zstd", when="+zstd")
 
-    depends_on("m4", type="build", when="@master")
-    depends_on("autoconf", type="build", when="@master")
-    depends_on("automake", type="build", when="@master")
-    depends_on("libtool", type="build", when="@master")
+    depends_on("m4", type="build", when="@0.1.105,master")
+    depends_on("autoconf", type="build", when="@0.1.105,master")
+    depends_on("automake", type="build", when="@0.1.105,master")
+    depends_on("libtool", type="build", when="@0.1.105,master")
 
     def flag_handler(self, name, flags):
         if name == "cflags" and "+min_size" in self.spec:
