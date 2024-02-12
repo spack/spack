ec5d1b14bc2bd5a6a9cc02e35f57f848af39f9ca
Mikael Simberg <mikael.simberg@iki.fi>
2024-02-12 10:38:50 +0100

    pika: add v0.22.2 (#42617)

--- var/spack/repos/builtin/packages/pika/package.py
+++ var/spack/repos/builtin/packages/pika/package.py
@@ -19,6 +19,7 @@ class Pika(CMakePackage, CudaPackage, ROCmPackage):
 
     license("BSL-1.0")
 
+    version("0.22.2", sha256="eeffa8584336b239aca167f0056e815b1b6d911e46cbb3cd6b8b811d101c1052")
     version("0.22.1", sha256="b0de0649bee336847622f97b59b34a80cb3cfd9a931bbdb38299bc4904f19b92")
     version("0.22.0", sha256="75f8932f3a233958c69802b483335eeeb39032ea66f12442f6f77048e259bdea")
     version("0.21.0", sha256="0ab24966e6ae026b355147f02354af4bd2117c342915fe844addf8e493735a33")
