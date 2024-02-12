62d03a9ebd5f38912b7eda0412a02f6939a50f17
Dani <44979434+d-beltran@users.noreply.github.com>
2024-02-12 10:19:47 +0100

    added setuptools dependency

--- var/spack/repos/builtin/packages/py-biobb-gromacs/package.py
+++ var/spack/repos/builtin/packages/py-biobb-gromacs/package.py
@@ -18,6 +18,7 @@ class PyBiobbGromacs(PythonPackage):
     version("4.1.1", sha256="270cce747fc214471527438c8319bda0613be5b76da9f4684e6f138d1927d2f7")
 
     # Dependencies
+    depends_on("py-setuptools", type="build")
     depends_on("python@3.8:", type=("build", "run"))
     depends_on("py-biobb-common@4.1.0", type=("build", "run"))
     depends_on("gromacs", type=("run"))
