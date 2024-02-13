4bcf686e86026722cb49b15ca6ba027660ae58f5
LMS Bioinformatics <bioinformatics@lms.mrc.ac.uk>
2024-02-13 10:41:16 +0000

    seqkit: add @2.7.0, convert to GoPackage

--- var/spack/repos/builtin/packages/seqkit/package.py
+++ var/spack/repos/builtin/packages/seqkit/package.py
@@ -3,10 +3,19 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+
+import os
+
 from spack.package import *
 
 
-class Seqkit(Package):
+class GoBuilder(spack.build_systems.go.GoBuilder):
+    @property
+    def build_directory(self):
+        return join_path(self.pkg.stage.source_path, "seqkit")
+
+
+class Seqkit(GoPackage):
     """seqkit: a cross-platform and ultrafast toolkit for FASTA/Q file manipulation"""
 
     homepage = "https://bioinf.shenwei.me/seqkit/"
@@ -14,18 +23,9 @@ class Seqkit(Package):
 
     license("MIT", checked_by="A-N-Other")
 
+    version("2.7.0", sha256="b5c723ffd4640659860fc70a71c218d8f53bea0eae571cecc98eff04c7291e02")
     version("2.6.1", sha256="d88249bd3b630c908ebd308abaa9cd7acb7a781c12bab877d3daaab56f43c443")
     version("2.5.1", sha256="76d105921f918be20e616fbb607fe0fb2db603535a254ec0f853cb36bef817da")
     version("2.4.0", sha256="c319f3d5feb7c99309e654042432959f01bbc5f7e4c71f55dc9854df46c73c7f")
 
     depends_on("go@1.17:", type="build")
-
-    phases = ["build", "install"]
-
-    def build(self, spec, prefix):
-        # Build using `go build`
-        go("build", "-C", "seqkit")
-
-    def install(self, spec, prefix):
-        mkdirp(prefix.bin)
-        install(join_path("seqkit", "seqkit"), prefix.bin)
