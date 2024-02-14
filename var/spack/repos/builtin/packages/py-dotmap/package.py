c52b7626127835cbeb7d92dfb9a97735ef4e35fe
jonas-eschle <jonas-eschle@users.noreply.github.com>
2024-02-14 03:26:06 +0000

    [@spackbot] updating style on behalf of jonas-eschle

--- var/spack/repos/builtin/packages/py-dotmap/package.py
+++ var/spack/repos/builtin/packages/py-dotmap/package.py
@@ -10,14 +10,13 @@ from spack.package import *
 class PyDotmap(PythonPackage):
     """`DotMap` is a dot-access `dict` subclass that
 
-        -   has dynamic hierarchy creation (autovivification)
-        -   can be initialized with keys
-        -   easily initializes from `dict`
-        -   easily converts to `dict`
-        -   is ordered by insertion
+    -   has dynamic hierarchy creation (autovivification)
+    -   can be initialized with keys
+    -   easily initializes from `dict`
+    -   easily converts to `dict`
+    -   is ordered by insertion
 
-        The key feature is exactly what you want: dot-access
-"""
+    The key feature is exactly what you want: dot-access"""
 
     homepage = "https://github.com/drgrib/dotmap"
     pypi = "dotmap/dotmap-1.3.30.tar.gz"
