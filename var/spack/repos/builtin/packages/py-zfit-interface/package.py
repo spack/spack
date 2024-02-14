3aefa0c1eff86085ff3345c7ea0a971ef456d2bd
jonas-eschle <jonas-eschle@users.noreply.github.com>
2024-02-14 01:49:23 +0000

    [@spackbot] updating style on behalf of jonas-eschle

--- var/spack/repos/builtin/packages/py-zfit-interface/package.py
+++ var/spack/repos/builtin/packages/py-zfit-interface/package.py
@@ -5,6 +5,7 @@
 
 from spack.package import *
 
+
 class PyZfitInterface(PythonPackage):
     """
     zfit model fitting interface for HEP
@@ -14,16 +15,16 @@ class PyZfitInterface(PythonPackage):
     pypi = "zfit_interface/zfit_interface-0.0.3.tar.gz"
 
     maintainers("jonas-eschle")
-    license('BSD-3-Clause', checked_by='jonas-eschle')
+    license("BSD-3-Clause", checked_by="jonas-eschle")
 
-    
-    version('0.0.3', sha256='c41cf79f1da4150b9a60bb1e8cab15df895b6ff4b753e2306494a7abda4150d0', expand=False)
+    version(
+        "0.0.3",
+        sha256="c41cf79f1da4150b9a60bb1e8cab15df895b6ff4b753e2306494a7abda4150d0",
+        expand=False,
+    )
 
-    depends_on('python@3.9:', type=('build', 'run'))
-    
-    depends_on('py-setuptools', type='build')
-    depends_on('py-numpy', type='run')
-    depends_on('py-uhi', type='run')
+    depends_on("python@3.9:", type=("build", "run"))
 
-
-
+    depends_on("py-setuptools", type="build")
+    depends_on("py-numpy", type="run")
+    depends_on("py-uhi", type="run")
