7073a9245abc1fa7793ef5201dc5aa63e5cc8f6f
Jonas Eschle <mayou36@jonas.eschle.com>
2024-02-13 20:38:21 -0500

    add package py dotmap

---
+++ var/spack/repos/builtin/packages/py-dotmap/package.py
@@ -0,0 +1,24 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class PyDotmap(PythonPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://github.com/drgrib/dotmap"
+    pypi = "dotmap/dotmap-1.3.30.tar.gz"
+
+
+
+    license("MIT", checked_by="jonas-eschle")
+
+    version("1.3.30", sha256="5821a7933f075fb47563417c0e92e0b7c031158b4c9a6a7e56163479b658b368")
+
+
+    depends_on("py-setuptools", type="build")
+
