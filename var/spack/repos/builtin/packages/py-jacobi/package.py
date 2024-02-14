bb24bf440db3d6003058cef890af81593561b8d0
Jonas Eschle <mayou36@jonas.eschle.com>
2024-02-13 20:54:49 -0500

    add package py-jacobi

---
+++ var/spack/repos/builtin/packages/py-jacobi/package.py
@@ -0,0 +1,25 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyJacobi(PythonPackage):
+    """Fast numerical derivatives for analytic functions with arbitrary round-off error and error propagation."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://github.com/hdembinski/jacobi"
+    pypi = "jacobi/jacobi-0.9.2.tar.gz"
+
+    maintainers("jonas-eschle")
+    license("MIT", checked_by="jonas-eschle")
+
+    version("0.9.2", sha256="c11f481663246ef1c2da915b9f9ab4ef229051fb14e0afc232d4668301320828")
+
+
+    depends_on("python@3.8:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-toml", type=("build"))
+    depends_on("py-setuptools", type="build")
