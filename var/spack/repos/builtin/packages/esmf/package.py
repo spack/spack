22e8c82e6059bc04a78a88f9682e426b7d05bf23
Alex Richert <alexrichert@users.noreply.github.com>
2024-02-15 13:50:42 -0800

    Add aocc support to ESMF

--- var/spack/repos/builtin/packages/esmf/package.py
+++ var/spack/repos/builtin/packages/esmf/package.py
@@ -233,6 +233,8 @@ class Esmf(MakefilePackage):
             env.set("ESMF_COMPILER", "nvhpc")
         elif self.compiler.name == "cce":
             env.set("ESMF_COMPILER", "cce")
+        elif self.compiler.name == "aocc":
+            env.set("ESMF_COMPILER", "aocc")
         else:
             msg = "The compiler you are building with, "
             msg += '"{0}", is not supported by ESMF.'
