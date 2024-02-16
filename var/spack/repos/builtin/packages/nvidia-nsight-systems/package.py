c50037d9a68d53ce3849571e37fa523a3ca1c1ed
Scot Halverson <shalverson@nvidia.com>
2024-02-16 07:49:08 -0800

    cleaning up licenses and boilerplate

--- var/spack/repos/builtin/packages/nvidia-nsight-systems/package.py
+++ var/spack/repos/builtin/packages/nvidia-nsight-systems/package.py
@@ -1,7 +1,7 @@
 # Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+# SPDX-License-Identifier: (https://docs.nvidia.com/nsight-systems/CopyrightAndLicenses/index.html)
 
 # ----------------------------------------------------------------------------
 # If you submit this package back to Spack as a pull request,
@@ -27,38 +27,20 @@ import os
 
 
 class NvidiaNsightSystems(Package):
-    """FIXME: Put a proper description of your package here."""
+    """NVIDIA Nsight™ Systems is a system-wide performance analysis tool designed to visualize an application’s algorithms, identify the largest opportunities to optimize, and tune to scale efficiently across any quantity or size of CPUs and GPUs, from large servers to our smallest system on a chip (SoC)"""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
-    url = "nvidia-nsight-systems"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
+    homepage = "https://developer.nvidia.com/nsight-systems"
+    url = "https://developer.download.nvidia.com/devtools/repos/"
     maintainers("scothalverson")
+    license("NVIDIA Software License Agreement")
 
-    # FIXME: Add the SPDX identifier of the project's license below.
-    # See https://spdx.org/licenses/ for a list. Upon manually verifying
-    # the license, set checked_by to your Github username.
-    license("UNKNOWN", checked_by="github_user1")
-
-    # FIXME: Add proper versions here.
-    # version("1.2.4")
-
-    # FIXME: Add dependencies if required.
     depends_on("libarchive programs='bsdtar'")
-
-    
     version('2024.1.1', sha256='b67168897c30b8f7a2c5c2ff1b2bc2bf1a544d4a220bf6eb0db6af9835aefe07', url='https://developer.download.nvidia.com/devtools/repos/rhel8/arm64/nsight-systems-cli-2024.1.1-2024.1.1.59_3380207-0.aarch64.rpm', expand=False)
 
     def install(self, spec, prefix):
-        # FIXME: Unknown build system
         bsdtar = which("bsdtar")
         rpm_file = glob(join_path(self.stage.source_path, "nsight-systems*.rpm"))[0]
-        
-
         params = ['-x', '-f', rpm_file]
-
         ver = prefix.split("/")[-1].split("-")[-2]
         bsdtar(*params)
         params = ['-x', '-f', 'data.tar.gz']
