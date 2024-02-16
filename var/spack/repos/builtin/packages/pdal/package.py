e737a536694ff93951d9fd971d12dd62eaad0e34
Sinan <sbulutw@gmail.com>
2024-02-16 11:28:03 -0800

    Update var/spack/repos/builtin/packages/pdal/package.py
    Co-authored-by: Alec Scott <alec@bcs.sh>

--- var/spack/repos/builtin/packages/pdal/package.py
+++ var/spack/repos/builtin/packages/pdal/package.py
@@ -11,10 +11,10 @@ class Pdal(CMakePackage):
     It is very much like the GDAL library which handles raster and vector data.
     """
 
-    license("BSD Open Source license.")
-
     homepage = "https://pdal.io"
     url = "https://github.com/PDAL/PDAL/archive/refs/tags/2.6.2.tar.gz"
+    
+    license("BSD Open Source license.")
 
     version("2.6.2", sha256="ec4175cfe19dc6b70a0434850f837317f7202f84b63cd8dcc65ca83e04678f57")
     version("2.6.1", sha256="da6e615f01b6110414ef3e2250f112e49df129091abc91ba6866bb01dc68454e")
