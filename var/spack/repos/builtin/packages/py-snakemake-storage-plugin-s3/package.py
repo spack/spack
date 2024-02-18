1ed85a12a8bca365905921ef21d71be8c1bd33d9
Maciej Wójcik <w8jcik@gmail.com>
2024-02-18 19:00:15 +0100

    py-snakemake-storage-plugin-s3: reorder

--- var/spack/repos/builtin/packages/py-snakemake-storage-plugin-s3/package.py
+++ var/spack/repos/builtin/packages/py-snakemake-storage-plugin-s3/package.py
@@ -10,11 +10,11 @@ from spack.package import *
 class PySnakemakeStoragePluginS3(PythonPackage):
     """A Snakemake storage plugin for S3 API storage (AWS S3, MinIO, etc.)."""
 
+    homepage = "https://github.com/snakemake/snakemake-storage-plugin-s3"
     pypi = "snakemake_storage_plugin_s3/snakemake_storage_plugin_s3-0.2.9.tar.gz"
 
     license("MIT")
 
-    homepage = "https://github.com/snakemake/snakemake-storage-plugin-s3"
     version("0.2.9", sha256="41cb23326f37dc576d4a1deac15b3f5a647e26ca957a69ae06b128c044ba2f32")
 
     depends_on("py-boto3@1.33:1", type=("build", "run"))
