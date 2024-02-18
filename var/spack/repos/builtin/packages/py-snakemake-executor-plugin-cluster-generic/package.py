f3bee7ea701357d5d6ac8de86b90f35b7daf2b87
Maciej Wójcik <w8jcik@gmail.com>
2024-02-18 04:22:26 +0100

    py-snakemake-executor-plugin-cluster-generic: add constraint for Python
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-snakemake-executor-plugin-cluster-generic/package.py
+++ var/spack/repos/builtin/packages/py-snakemake-executor-plugin-cluster-generic/package.py
@@ -23,5 +23,5 @@ class PySnakemakeExecutorPluginClusterGeneric(PythonPackage):
     depends_on("py-snakemake-interface-common@1.13:1", type=("build", "run"))
     depends_on("py-snakemake-interface-executor-plugins@8.1:8", type=("build", "run"))
 
-    depends_on("python@3.11:", type=("build", "run"))
+    depends_on("python@3.11:3", type=("build", "run"))
     depends_on("py-poetry-core", type="build")
