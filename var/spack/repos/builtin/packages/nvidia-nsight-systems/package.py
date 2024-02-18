b88dc05b0c17b25b3a6c8b7048de67e6661d2444
Scot Halverson <shalverson@nvidia.com>
2024-02-18 14:11:26 -0700

    fixed some arch issues.  should be functional for both arm and x86_64 now

--- var/spack/repos/builtin/packages/nvidia-nsight-systems/package.py
+++ var/spack/repos/builtin/packages/nvidia-nsight-systems/package.py
@@ -37,7 +37,7 @@ class NvidiaNsightSystems(Package):
     depends_on("libarchive programs='bsdtar'")
     arch = platform.uname()[-1]
     if arch == "x86_64":
-        version('2024.1.1', sha256='890a585807a355b3d986443e5d27727f527d5ac83e901961a1e7761012f565c3', url='https://developer.download.nvidia.com/devtools/repos/rhel8/x86_64/nsight-systems-2024.1.1-2024.1.1.59_3380207-0.x86_64.rpm', expand=False)
+        version('2024.1.1', sha256='96f57548e0bd69cb02cd1fe8c70ed4a650636ecb3a5ea5ec490c8049adc2beb5', url='https://developer.download.nvidia.com/devtools/repos/rhel8/x86_64/nsight-systems-2024.1.1-2024.1.1.59_3380207-0.x86_64.rpm', expand=False)
     if arch == "aarch64":
         version('2024.1.1', sha256='41dc15ae128ef1de8e582b66bb465ac6bd67b9d20ef77fc70528b735d80fb3ec', url='https://developer.download.nvidia.com/devtools/repos/rhel8/arm64/nsight-systems-2024.1.1-2024.1.1.59_3380207-0.aarch64.rpm', expand=False)
 
@@ -49,13 +49,19 @@ class NvidiaNsightSystems(Package):
         bsdtar(*params)
         #params = ['-x', '-f', 'data.tar.gz']
         #bsdtar(*params)
+
+        arch = platform.uname()[-1]
+        if arch == "aarch64":
+            folders = [ "/documentation", "/host-linux-armv8a", "/target-linux-sbsa-armv8"]
+        elif arch == "x86_64":
+            folders = [ "/documentation", "/host-linux-x64", "/target-linux-x64"]
         base_path = ''
         if os.path.exists('opt/nvidia/nsight-systems-cli/'):
             base_path = 'opt/nvidia/nsight-systems-cli/'
         elif os.path.exists('opt/nvidia/nsight-systems/'):
             base_path = 'opt/nvidia/nsight-systems/'
 
-        for sd in [ "/documentation", "/host-linux-armv8", "/target-linux-sbsa-armv8"]:
+        for sd in folders:
             shutil.copytree(base_path + ver + sd, prefix + sd)
         os.mkdir(prefix + "/bin")
         os.symlink(prefix + "/host-linux-armv8/nsys-ui", prefix + "/bin/nsys-ui")
