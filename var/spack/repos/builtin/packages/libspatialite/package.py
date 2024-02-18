6efadb38ca323f4752e0b44ef74f5671eba5f83d
Sinan81 <Sinan81@users.noreply.github.com>
2024-02-18 20:57:12 +0000

    [@spackbot] updating style on behalf of Sinan81

--- var/spack/repos/builtin/packages/libspatialite/package.py
+++ var/spack/repos/builtin/packages/libspatialite/package.py
@@ -19,15 +19,20 @@ class Libspatialite(AutotoolsPackage):
     version("5.1.0", sha256="43be2dd349daffe016dd1400c5d11285828c22fea35ca5109f21f3ed50605080")
     version("5.0.1", sha256="eecbc94311c78012d059ebc0fae86ea5ef6eecb13303e6e82b3753c1b3409e98")
     version(
-        "5.0.0-beta0", sha256="caacf5378a5cfab9b8e98bb361e2b592e714e21f5c152b795df80d0ab1da1c42",
-        deprecated=True
+        "5.0.0-beta0",
+        sha256="caacf5378a5cfab9b8e98bb361e2b592e714e21f5c152b795df80d0ab1da1c42",
+        deprecated=True,
     )
     version("5.0.0", sha256="7b7fd70243f5a0b175696d87c46dde0ace030eacc27f39241c24bac5dfac6dac")
-    version("4.3.0a", sha256="88900030a4762904a7880273f292e5e8ca6b15b7c6c3fb88ffa9e67ee8a5a499",
-        deprecated=True
+    version(
+        "4.3.0a",
+        sha256="88900030a4762904a7880273f292e5e8ca6b15b7c6c3fb88ffa9e67ee8a5a499",
+        deprecated=True,
     )
-    version("3.0.1", sha256="4983d6584069fd5ff0cfcccccee1015088dab2db177c0dc7050ce8306b68f8e6",
-        deprecated=True
+    version(
+        "3.0.1",
+        sha256="4983d6584069fd5ff0cfcccccee1015088dab2db177c0dc7050ce8306b68f8e6",
+        deprecated=True,
     )
 
     depends_on("pkgconfig", type="build")
