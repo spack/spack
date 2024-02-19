4060645a96f809e160c318167f241cd38ac36930
Geoffrey Lentner <glentner@purdue.edu>
2024-02-19 05:02:43 -0500

    duckdb: add v0.9.2 (#42374)

--- var/spack/repos/builtin/packages/duckdb/package.py
+++ var/spack/repos/builtin/packages/duckdb/package.py
@@ -11,11 +11,13 @@ class Duckdb(CMakePackage):
     """DuckDB is an in-process SQL OLAP Database Management System."""
 
     homepage = "https://duckdb.org"
-    url = "https://github.com/duckdb/duckdb/archive/refs/tags/v0.8.1.tar.gz"
+    url = "https://github.com/duckdb/duckdb/archive/refs/tags/v0.9.2.tar.gz"
     git = "https://github.com/duckdb/duckdb.git"
 
     license("MIT")
+    maintainers("glentner")
 
     version("master", branch="master")
+    version("0.9.2", sha256="afff7bd925a98dc2af4039b8ab2159b0705cbf5e0ee05d97f7bb8dce5f880dc2")
     version("0.8.1", sha256="a0674f7e320dc7ebcf51990d7fc1c0e7f7b2c335c08f5953702b5285e6c30694")
     version("0.7.1", sha256="67f840f861e5ffbe137d65a8543642d016f900b89dd035492d562ad11acf0e1e")
