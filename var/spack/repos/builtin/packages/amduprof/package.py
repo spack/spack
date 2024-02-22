da99b76f6fa3f32a7e07926bfe2f700e1eca30b8
zzzoom <zzzoom@users.noreply.github.com>
2024-02-22 06:44:01 +0000

    [@spackbot] updating style on behalf of zzzoom

--- var/spack/repos/builtin/packages/amduprof/package.py
+++ var/spack/repos/builtin/packages/amduprof/package.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 
 class Amduprof(Package):
@@ -11,17 +11,17 @@ class Amduprof(Package):
     and provides event information unique to the AMD "Zen"-based processors."""
 
     homepage = "https://developer.amd.com/amd-uprof/"
-    url      = "https://developer.amd.com/wordpress/media/files/AMDuProf_Linux_x64_3.5.671.tar.bz2"
+    url = "https://developer.amd.com/wordpress/media/files/AMDuProf_Linux_x64_3.5.671.tar.bz2"
 
-    version('3.5.671', sha256='a73066305228658a14af5ecd6cf45a1aa47ae94f7e9d14db31f43013d3ef1a43')
+    version("3.5.671", sha256="a73066305228658a14af5ecd6cf45a1aa47ae94f7e9d14db31f43013d3ef1a43")
 
-    depends_on('binutils', type='run')
+    depends_on("binutils", type="run")
 
     # TODO: build Power Profiling driver on Linux
     # TODO: ROCm for GPU tracing and profiling
     # TODO: BCC and eBPF for OS tracing
 
-    conflicts('platform=darwin')
+    conflicts("platform=darwin")
 
     def install(self, spec, prefix):
-        install_tree('.', prefix)
+        install_tree(".", prefix)
