40f59198369ea421d9429224f0de25c5b584c4e4
Tom Payerle <payerle@umd.edu>
2024-02-22 15:56:58 -0500

    qb3: Incorporating suggested cleanup from @alalazo

--- var/spack/repos/builtin/packages/qb3/package.py
+++ var/spack/repos/builtin/packages/qb3/package.py
@@ -24,14 +24,8 @@ class Qb3(CMakePackage):
         # Override because libs have different case than Spack package name
         name = "libQB3*"
         # We expect libraries to be in either lib64 or lib directory
-        # Try lib64 first
-        root = self.prefix.lib64
-        liblist = find_libraries(name, root=root, shared=True, recursive=False)
-        if liblist:
-            # Found libs in lib64, so return those
-            return liblist
-        else:
-            # Did not find libs in lib64, so try lib w/out 64 suffix
-            root = self.prefix.lib
+        for root in (self.prefix.lib64, self.prefix.lib):
             liblist = find_libraries(name, root=root, shared=True, recursive=False)
-            return liblist
+            if liblist:
+                break
+        return liblist
