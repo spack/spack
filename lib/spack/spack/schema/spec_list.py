cdec906f1d89d864572f776947ebebea6e057a6d
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-02-23 08:10:14 +0100

    Fix typo

--- lib/spack/spack/schema/spec_list.py
+++ lib/spack/spack/schema/spec_list.py
@@ -2,7 +2,7 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-matix_schema = {"type": "array", "items": {"type": "array", "items": {"type": "string"}}}
+matrix_schema = {"type": "array", "items": {"type": "array", "items": {"type": "string"}}}
 
 spec_list_schema = {
     "type": "array",
@@ -13,7 +13,7 @@ spec_list_schema = {
                 "type": "object",
                 "additionalProperties": False,
                 "properties": {
-                    "matrix": matix_schema,
+                    "matrix": matrix_schema,
                     "exclude": {"type": "array", "items": {"type": "string"}},
                 },
             },
