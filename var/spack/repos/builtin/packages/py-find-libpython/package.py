c17d2d0d06414062cb56a0cae1cee07d124f2748
Erik Heeren <erik.heeren@epfl.ch>
2024-02-23 13:08:01 +0100

    Update var/spack/repos/builtin/packages/py-find-libpython/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-find-libpython/package.py
+++ var/spack/repos/builtin/packages/py-find-libpython/package.py
@@ -16,4 +16,5 @@ class PyFindLibpython(PythonPackage):
 
     version("0.3.0", sha256="6e7fe5d9af7fad6dc066cb5515a0e9c90a71f1feb2bb2f8e4cdbb4f83276e9e5")
 
-    depends_on("py-setuptools@43:", type="build")
+    depends_on("py-setuptools@42:", type="build")
+    depends_on("py-setuptools-scm+toml", type="build")
