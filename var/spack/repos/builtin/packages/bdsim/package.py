13c0562fe0062bc55660eee87f308b32954d1f84
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2024-02-27 08:34:42 +0100

    bdsim: use the same C++ standard as in ROOT, add a patch (#42031)
    Co-authored-by: jmcarcell <jmcarcell@users.noreply.github.com>

--- var/spack/repos/builtin/packages/bdsim/package.py
+++ var/spack/repos/builtin/packages/bdsim/package.py
@@ -22,7 +22,9 @@ class Bdsim(CMakePackage):
 
     license("GPL-3.0-or-later")
 
-    version("develop", branch="develop")
+    version("master", branch="master")
+    version("1.7.6", sha256="92f53aa0a9fbd3cafd218f9e58ae4d1e7115733e641191c1658243fefb436600")
+    version("1.7.0", sha256="713ce3c9d94f340ca774ce1803e0c4f992b904dbc28ce4129713abe883e98683")
     version("1.6.0", sha256="e3241d2d097cb4e22249e315c1474da9b3657b9c6893232d9f9e543a5323f717")
 
     depends_on("cmake")
@@ -32,3 +34,10 @@ class Bdsim(CMakePackage):
     depends_on("clhep")
     depends_on("flex")
     depends_on("bison")
+
+    patch("c++-standard.patch", when="@:1.7.6")
+
+    def cmake_args(self):
+        args = []
+        args.append(f"-DCMAKE_CXX_STANDARD={self.spec['root'].variants['cxxstd'].value}")
+        return args
