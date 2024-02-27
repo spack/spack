c7555f0cd6bbbb100b8ad09702a30441409df70a
Martin Lang <martin.lang@mpsd.mpg.de>
2024-02-27 13:50:47 +0100

    Remove flags without effect

--- var/spack/repos/builtin/packages/berkeleygw/package.py
+++ var/spack/repos/builtin/packages/berkeleygw/package.py
@@ -232,7 +232,9 @@ class Berkeleygw(MakefilePackage):
                 f90_flags += " -fallow-argument-mismatch"
             buildopts.append("COMPFLAG=-DGNU")
             buildopts.append("MOD_OPT=-J ")
-            buildopts.append("FCPP=cpp -P -nostdinc -C -E -std=c11")
+            # std c11 prevents problems with linebreaks and fortran comments
+            # containing // (which is interpreted as C++ style comment)
+            buildopts.append("FCPP=cpp -C -nostdinc -std=c11")
             if "+mpi" in spec:
                 buildopts.append("F90free=%s %s" % (spec["mpi"].mpifc, f90_flags))
                 buildopts.append("C_COMP=%s %s" % (spec["mpi"].mpicc, c_flags))
