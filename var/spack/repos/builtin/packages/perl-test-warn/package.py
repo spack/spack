7fbd1d4f1145d73ed0b5d49989e40e1cec7bfdd7
Arne Becker <arne@ebi.ac.uk>
2024-02-28 11:28:04 +0000

    Revive older version

--- var/spack/repos/builtin/packages/perl-test-warn/package.py
+++ var/spack/repos/builtin/packages/perl-test-warn/package.py
@@ -17,6 +17,7 @@ class PerlTestWarn(PerlPackage):
     license("Artistic-1.0-Perl OR GPL-1.0-or-later")
 
     version("0.37", sha256="98ca32e7f2f5ea89b8bfb9a0609977f3d153e242e2e51705126cb954f1a06b57")
+    version("0.30", sha256="8197555b94189d919349a03f7058f83861f145af9bee59f505bfe47562144e41")
 
     depends_on("perl@5.6.0:", type=("build", "link", "run", "test"))
     depends_on("perl-sub-uplevel@0.12:", type=("build", "run", "test"))
