b2f86d11455f6bcd85628ff6c2af570797d16f81
Arne Becker <101113822+EbiArnie@users.noreply.github.com>
2024-02-28 20:26:09 +0000

    perl-yaml-syck: New package (#42892)
    Adds YAML::Syck

---
+++ var/spack/repos/builtin/packages/perl-yaml-syck/package.py
@@ -0,0 +1,29 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PerlYamlSyck(PerlPackage):
+    """Fast, lightweight YAML loader and dumper"""
+
+    homepage = "https://metacpan.org/pod/YAML::Syck"
+    url = "https://cpan.metacpan.org/authors/id/T/TO/TODDR/YAML-Syck-1.34.tar.gz"
+
+    maintainers("EbiArnie")
+
+    license("MIT")
+
+    version("1.34", sha256="cc9156ccaebda798ebfe2f31b619e806577f860ed1704262f17ffad3c6e34159")
+
+    depends_on("perl@5.6.0:", type=("build", "link", "run", "test"))
+
+    def test_use(self):
+        """Test 'use module'"""
+        options = ["-we", 'use strict; use YAML::Syck; print("OK\n")']
+
+        perl = self.spec["perl"].command
+        out = perl(*options, output=str.split, error=str.split)
+        assert "OK" in out
