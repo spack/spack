ba0085b5889bc6e6ab205cac3c972e0d6495705a
Tim Fuller <tjfulle@sandia.gov>
2024-02-29 08:07:48 -0700

    hopefully this will pass with windows pathseps...

--- lib/spack/spack/test/entry_points.py
+++ lib/spack/spack/test/entry_points.py
@@ -19,11 +19,12 @@ class MockConfigEntryPoint:
 
     def load(self):
         self.dir.ensure("spack/etc", dir=True)
-        with open(os.path.join(self.dir, "spack/etc/config.yaml"), "w") as fh:
+        f = os.path.join(self.dir, "spack", "etc", "config.yaml")
+        with open(f, "w") as fh:
             fh.write("config:\n  install_tree:\n    root: /spam/opt\n")
 
         def ep():
-            return os.path.join(self.dir, "spack/etc")
+            return os.path.join(self.dir, "spack", "etc")
 
         return ep
 
@@ -35,7 +36,8 @@ class MockExtensionsEntryPoint:
 
     def load(self):
         self.dir.ensure("spack/spack-ext/ext/cmd", dir=True)
-        with open(os.path.join(self.dir, "spack/spack-ext/ext/cmd/spam.py"), "w") as fh:
+        f = os.path.join(self.dir, "spack", "spack-ext", "ext", "cmd", "spam.py")
+        with open(f, "w") as fh:
             fh.write("description = 'hello world extension command'\n")
             fh.write("section = 'test command'\n")
             fh.write("level = 'long'\n")
@@ -43,7 +45,7 @@ class MockExtensionsEntryPoint:
             fh.write("def spam(parser, args):\n    print('spam for all!')\n")
 
         def ep():
-            return os.path.join(self.dir, "spack/spack-ext")
+            return os.path.join(self.dir, "spack", "spack-ext")
 
         return ep
 
@@ -59,9 +61,7 @@ def entry_points_factory(tmpdir):
     return entry_points
 
 
-@pytest.mark.skipif(sys.version_info[:2] < (3, 8), reason="Python>=3.8 required")
-def test_spack_entry_points(monkeypatch, tmpdir):
-    """Test config scope entry point"""
+def patch_entry_points(tmpdir, monkeypatch):
     entry_points = entry_points_factory(tmpdir)
     try:
         try:
@@ -75,16 +75,33 @@ def test_spack_entry_points(monkeypatch, tmpdir):
         except ImportError:
             return
         monkeypatch.setattr(pkg_resources, "iter_entry_points", entry_points)
-    config_paths = spack.config.config_paths_from_entry_points()
-    for name, path in config_paths:
-        if name == "plugin-mypackage_config":
-            assert path == os.path.join(tmpdir, "spack/etc")
-            break
+
+
+@pytest.mark.skipif(sys.version_info[:2] < (3, 8), reason="Python>=3.8 required")
+def test_spack_entry_point_config(monkeypatch, tmpdir):
+    """Test config scope entry point"""
+    patch_entry_points(tmpdir, monkeypatch)
+    config_paths = dict(spack.config.config_paths_from_entry_points())
+    config_path = config_paths.get("plugin-mypackage_config")
+    my_config_path = os.path.join(tmpdir, "spack", "etc")
+    if config_path is None:
+        raise ValueError("Did not find entry point config in %s" % str(config_paths))
     else:
-        assert 0, "mypackage_config entry_point not loaded"
+        assert os.path.samefile(config_path, my_config_path)
     config = spack.config.create()
-    assert config.get("config:install_tree:root") == "/spam/opt"
+    assert config.get("config:install_tree:root", scope="plugin-mypackage_config") == "/spam/opt"
+
+
+@pytest.mark.skipif(sys.version_info[:2] < (3, 8), reason="Python>=3.8 required")
+def test_spack_entry_point_extension(monkeypatch, tmpdir):
+    """Test config scope entry point"""
+    patch_entry_points(tmpdir, monkeypatch)
+    my_ext = os.path.join(tmpdir, "spack", "spack-ext")
     extensions = spack.extensions.get_extension_paths()
-    assert os.path.join(tmpdir, "spack/spack-ext") in extensions
+    found = bool([ext for ext in extensions if os.path.samefile(ext, my_ext)])
+    if not found:
+        raise ValueError("Did not find extension in %s" % ", ".join(extensions))
     extensions = spack.extensions.extension_paths_from_entry_points()
-    assert os.path.join(tmpdir, "spack/spack-ext") in extensions
+    found = bool([ext for ext in extensions if os.path.samefile(ext, my_ext)])
+    if not found:
+        raise ValueError("Did not find extension in %s" % ", ".join(extensions))
