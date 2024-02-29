f2e083e11c4ef30a18be19e892e9ed16c72afed7
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-02-29 06:06:57 +0100

    Fix a typo in a directive

--- var/spack/repos/builtin/packages/py-jaxlib/package.py
+++ var/spack/repos/builtin/packages/py-jaxlib/package.py
@@ -81,7 +81,7 @@ class PyJaxlib(PythonPackage, CudaPackage):
     )
 
     # https://github.com/google/jax/issues/19992
-    conflicts("@0.4.25", when="platform=ppc64le:")
+    conflicts("@0.4.25", when="target=ppc64le:")
 
     def patch(self):
         self.tmp_path = tempfile.mkdtemp(prefix="spack")
