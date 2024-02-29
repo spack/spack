1cd896c66ed7aa387faa99c974a0b27ceb4895a0
psakievich <psakiev@sandia.gov>
2024-02-29 11:30:43 -0700

    Use correct function

--- lib/spack/spack/projections.py
+++ lib/spack/spack/projections.py
@@ -13,7 +13,7 @@ def get_projection(projections, spec):
     all_projection = None
     for spec_like, projection in projections.items():
         if spec.satisfies(spec_like):
-            return spack.util.path.canonicalize_path(projection)
+            return spack.util.path.substitute_path_variables(projection)
         elif spec_like == "all":
-            all_projection = spack.util.path.canonicalize_path(projection)
+            all_projection = spack.util.path.substitute_path_variables(projection)
     return all_projection
