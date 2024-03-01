c8084bef3c41656e7272062c66661df6ead2b7c6
Justin M. LaPre <jmlapre@sandia.gov>
2024-03-01 18:56:41 +0000

    py_cheap_repr: add initial package.py

---
+++ var/spack/repos/builtin/packages/py-cheap-repr/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack.package import *
+
+
+class PyCheapRepr(PythonPackage):
+    """This library provides short, fast, configurable string representations,
+    and an easy API for registering your own. It's an improvement of the
+    standard library module reprlib (repr in Python 2)."""
+
+    homepage = "https://pypi.org/project/cheap-repr/"
+    url = "https://pypi.org/packages/source/c/cheap-repr/cheap_repr-0.5.1.tar.gz"
+    list_url = "https://pypi.org/simple/cheap-repr/"
+
+    license("MIT", checked_by="jmlapre")
+
+    version("0.5.1", sha256="31ec63b9d8394aa23d746c8376c8307f75f9fca0b983566b8bcf13cc661fe6dd")
+
+    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools-scm", type="build")
