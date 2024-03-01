1f7c4afd3636e0692666524012841ce7a6e0bbdc
Kayla Butler <butler59@llnl.gov>
2024-02-29 16:36:45 -0800

    Add test

--- lib/spack/spack/test/concretize.py
+++ lib/spack/spack/test/concretize.py
@@ -524,6 +524,62 @@ class TestConcretize:
         assert not spec.satisfies("^a foo=bar")
         assert not spec.satisfies("^b foo=bar")
 
+<<<<<<< HEAD
+=======
+    @pytest.mark.only_clingo(
+        "Optional compiler propagation isn't deprecated for original concretizer"
+    )
+    def test_concretize_propagate_specified_variant(self):
+        """Test that only the specified variant is propagated to the dependencies"""
+        spec = Spec("splice-b++bar")
+        spec.concretize()
+
+        assert spec.satisfies("+bar") and spec.satisfies("^splice-a+bar")
+        assert spec.satisfies("+bar") and spec.satisfies("^splice-z+bar")
+        assert spec.satisfies("+foo") and not spec.satisfies("^splice-a+foo")
+        assert spec.satisfies("+foo") and not spec.satisfies("^splice-z+foo")
+
+    @pytest.mark.only_clingo(
+        "Optional compiler propagation isn't deprecated for original concretizer"
+    )
+    def test_concretize_propagate_variant_not_in_source(self):
+        """Test that variant is still propagated even if the source pkg
+        doesn't have the variant"""
+
+        spec = Spec("callpath++debug")
+        spec.concretize()
+
+        assert spec.satisfies("^mpich+debug")
+        assert not spec.satisfies("callpath+debug")
+        assert not spec.satisfies("^dyninst+debug")
+
+
+    @pytest.mark.only_clingo(
+        "Optional compiler propagation isn't deprecated for original concretizer"
+    )
+    def test_concretize_propagate_variant_not_in_source_or_dependencies(self):
+        """Test propagating a variant that is not in the source package
+        or in any of the dependents fails"""
+
+        spec = Spec("callpath++shared")
+        spec.concretize()
+
+        # raises some kind of error
+
+
+    @pytest.mark.only_clingo(
+        "Optional compiler propagation isn't deprecated for original concretizer"
+    )
+    def test_concretize_propagate_variant_in_source_not_dependencies(self):
+        """Does a thing"""
+
+        spec = Spec("hdf5~~mpi")
+        spec.concretize()
+
+        assert spec.satisfies("hdf5~mpi")
+        assert not spec.satisfies("^mpich~mpi")
+
+>>>>>>> cf51b59c2e (Add test)
     def test_no_matching_compiler_specs(self, mock_low_high_config):
         # only relevant when not building compilers as needed
         with spack.concretize.enable_compiler_existence_check():
