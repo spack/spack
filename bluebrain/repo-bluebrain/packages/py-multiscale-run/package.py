d7061f1c65caa83314e401e7a25a1e514cf8facb
Tristan Carel <tristan.carel@epfl.ch>
2024-03-04 18:23:27 +0100

    steps: bump requirements / fix build on Apple M1 (#2338)
    * petsc: new arm64 patch to ignore linker warnings
    * gmsh: new version 4.12.2
    * steps: update sundials requirements. `develop` now supports SUNDIALS up to version 6 (included)
    * py-multiscale-run: force steps version

--- bluebrain/repo-bluebrain/packages/py-multiscale-run/package.py
+++ bluebrain/repo-bluebrain/packages/py-multiscale-run/package.py
@@ -40,7 +40,7 @@ class PyMultiscaleRun(PythonPackage):
     depends_on("py-pyyaml", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
-    depends_on("steps@5:", type=("build", "run"))
+    depends_on("steps@5.0.0b", type=("build", "run"))
 
     depends_on("py-pytest", type="test")
 
