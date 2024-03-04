b21c29bffd2214d1be25b907becb27888700bb90
Teague Sterling <teaguesterling@users.noreply.github.com>
2024-03-04 10:35:43 -0800

    Including suggestions and comments from review.

--- var/spack/repos/builtin/packages/duckdb/package.py
+++ var/spack/repos/builtin/packages/duckdb/package.py
@@ -19,14 +19,14 @@ class Duckdb(MakefilePackage):
 
     version("master", branch="master")
     version("0.10.0", sha256="5a925b8607d00a97c1a3ffe6df05c0a62a4df063abd022ada82ac1e917792013")
-    version("0.9.2", sha256="afff7bd925a98dc2af4039b8ab2159b0705cbf5e0ee05d97f7bb8dce5f880dc2")
-    version("0.9.1", sha256="37a43188d9354ce3ca101b2b118d867f5f76d04c3b83c09d86fd7508351a631b")
-    version("0.9.0", sha256="3dbf3326a831bf0797591572440e81a3d6d668f8e33a25ce04efae19afc3a23d")
-    version("0.8.1", sha256="a0674f7e320dc7ebcf51990d7fc1c0e7f7b2c335c08f5953702b5285e6c30694")
-    version("0.8.0", sha256="df3b8e0b72bce38914f0fb1cd02235d8b616df9209beb14beb06bfbcaaf2e97f")
-    version("0.7.1", sha256="67f840f861e5ffbe137d65a8543642d016f900b89dd035492d562ad11acf0e1e")
+    version("0.9.2", sha256="afff7bd925a98dc2af4039b8ab2159b0705cbf5e0ee05d97f7bb8dce5f880dc2", deprecated=True)
+    version("0.9.1", sha256="37a43188d9354ce3ca101b2b118d867f5f76d04c3b83c09d86fd7508351a631b", deprecated=True)
+    version("0.9.0", sha256="3dbf3326a831bf0797591572440e81a3d6d668f8e33a25ce04efae19afc3a23d", deprecated=True)
+    version("0.8.1", sha256="a0674f7e320dc7ebcf51990d7fc1c0e7f7b2c335c08f5953702b5285e6c30694", deprecated=True)
+    version("0.8.0", sha256="df3b8e0b72bce38914f0fb1cd02235d8b616df9209beb14beb06bfbcaaf2e97f", deprecated=True)
+    version("0.7.1", sha256="67f840f861e5ffbe137d65a8543642d016f900b89dd035492d562ad11acf0e1e", deprecated=True)
 
-    depends_on("python@3:")
+    depends_on("python@3.7:")
     depends_on("cmake", type="build")
     depends_on("gmake", type="build")
     depends_on("ninja", when="+ninjabuild", type="build")
@@ -38,7 +38,7 @@ class Duckdb(MakefilePackage):
     variant("cli", default=True, description="Compile with command line client")
     variant("icu", default=False, description="Compile with bundled ICU library")
     variant("ninjabuild", default=True, description="Use GEN=ninja to build")
-    variant("openssl", default=False, description="Compile with bundled OpenSSL library")
+    variant("openssl", default=False, description="Compile with bundled OpenSSL library", when="@:0.9.2")
 
     # Extensions
     variant("excel", default=True, description="Include Excel formatting extension in build")
@@ -53,7 +53,7 @@ class Duckdb(MakefilePackage):
     variant("odbc", default=False, description="Build with ODBC driver (may not work)")
     variant("python", default=False, description="Build with Python driver (may not work)")
 
-    def edit(self, spec, prefix):
+    def setup_build_environment(self, spec, prefix):
         if "+ninjabuild" in spec:
             env["GEN"] = "ninja"
         variant_flags = [
@@ -86,13 +86,13 @@ class Duckdb(MakefilePackage):
         # and thus defaults to something arbitrary and breaks extensions.
         # We use the Spack version to inject it in the right place during the build
 
-        v = self.spec.version
+        version = self.spec.version
         if not self.spec.satisfies("@0.10.0:"):
             # Prior to version 0.10.0, this was sufficient
             filter_file(
                 r'(message\(STATUS "git hash \$\{GIT_COMMIT_HASH\}, '
                 r'version \$\{DUCKDB_VERSION\}"\))',
-                'set(DUCKDB_VERSION "v{0}")\n\\1'.format(self.spec.version),
+                'set(DUCKDB_VERSION "v{0}")\n\\1'.format(version),
                 "CMakeLists.txt",
             )
         else:
@@ -100,7 +100,7 @@ class Duckdb(MakefilePackage):
             for i, n in enumerate(["MAJOR", "MINOR", "PATCH"]):
                 filter_file(
                     r"set\(DUCKDB_{0}_VERSION \d+\)".format(n),
-                    "set(DUCKDB_{0}_VERSION {1})".format(n, v[i]),
+                    "set(DUCKDB_{0}_VERSION {1})".format(n, version[i]),
                     "CMakeLists.txt",
                 )
             # Need to manually set DUCKDB_NORMALIZED_VERSION for helper scripts
