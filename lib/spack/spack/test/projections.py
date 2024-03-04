570c7fb5cafc99e8c1f6bf5ca3a5b6c6b99b89df
psakiev <psakiev@sandia.gov>
2024-03-04 09:00:28 -0700

    Use monkeypatch to set env

--- lib/spack/spack/test/projections.py
+++ lib/spack/spack/test/projections.py
@@ -10,9 +10,10 @@ import spack.projections
 import spack.spec
 
 
-def test_projection_expansion(mock_packages):
+def test_projection_expansion(mock_packages, monkeypatch):
     """Test that env variables and spack config variables are expanded in projections"""
-    os.environ["FOO_ENV_VAR"] = "test-string"
+
+    monkeypatch.setenv("FOO_ENV_VAR", "test-string")
     projections = {"all": "{name}-{version}/$FOO_ENV_VAR/$date"}
     spec = spack.spec.Spec("fake@1.0")
     projection = spack.projections.get_projection(projections, spec)
