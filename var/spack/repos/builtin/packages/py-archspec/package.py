a62187fd859d0888afce2f53d2c42cdc310bef06
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-03-05 09:33:45 +0100

    archspec: add v0.2.3

--- var/spack/repos/builtin/packages/py-archspec/package.py
+++ var/spack/repos/builtin/packages/py-archspec/package.py
@@ -17,6 +17,7 @@ class PyArchspec(PythonPackage):
 
     license("Apache-2.0")
 
+    version("0.2.3", sha256="d07deb5b6e2ab3b74861e217523d02e69be8522f6e6565f4cc5d2062eb1a5d2c")
     version("0.2.2", sha256="d922c9fd80a5234d8cef883fbe0e146b381c449062c0405f91714ebad1edc035")
     version("0.2.1", sha256="0974a8a95831d2d43cce906c5b79a35d5fd2bf9be478b0e3b7d83ccc51ac815e")
     version("0.2.0", sha256="6aaba5ebdb5c3633c400d8c221a6a18716da0c64b367a8509f4217b22e91a5f5")
