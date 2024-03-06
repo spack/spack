d5fd38eb9fc8c52fb67c450f3135c362d519e2b7
alex391 <36964815+alex391@users.noreply.github.com>
2024-03-06 20:45:14 +0000

    [@spackbot] updating style on behalf of alex391

--- var/spack/repos/builtin/packages/py-bravado/package.py
+++ var/spack/repos/builtin/packages/py-bravado/package.py
@@ -13,19 +13,19 @@ class PyBravado(PythonPackage):
     """
 
     homepage = "https://github.com/Yelp/bravado"
-    pypi     = "bravado/bravado-11.0.3.tar.gz"
+    pypi = "bravado/bravado-11.0.3.tar.gz"
 
-    version('11.0.3', sha256='1bb6ef75d84140c851fffe6420baaee5037d840070cfe11d60913be6ab8e0530')
+    version("11.0.3", sha256="1bb6ef75d84140c851fffe6420baaee5037d840070cfe11d60913be6ab8e0530")
 
-    depends_on('python@:2,3.5.1:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
+    depends_on("python@:2,3.5.1:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
 
-    depends_on('py-bravado-core@5.16.1:', type=('build', 'run'))
-    depends_on('py-msgpack', type=('build', 'run'))
-    depends_on('py-python-dateutil', type=('build', 'run'))
-    depends_on('py-pyyaml', type=('build', 'run'))
-    depends_on('py-requests@2.17:', type=('build', 'run'))
-    depends_on('py-six', type=('build', 'run'))
-    depends_on('py-simplejson', type=('build', 'run'))
-    depends_on('py-monotonic', type=('build', 'run'))
-    depends_on('py-typing-extensions', type=('build', 'run'))
+    depends_on("py-bravado-core@5.16.1:", type=("build", "run"))
+    depends_on("py-msgpack", type=("build", "run"))
+    depends_on("py-python-dateutil", type=("build", "run"))
+    depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-requests@2.17:", type=("build", "run"))
+    depends_on("py-six", type=("build", "run"))
+    depends_on("py-simplejson", type=("build", "run"))
+    depends_on("py-monotonic", type=("build", "run"))
+    depends_on("py-typing-extensions", type=("build", "run"))
--- var/spack/repos/builtin/packages/py-swagger-spec-validator/package.py
+++ var/spack/repos/builtin/packages/py-swagger-spec-validator/package.py
@@ -13,12 +13,12 @@ class PySwaggerSpecValidator(PythonPackage):
     """
 
     homepage = "https://github.com/Yelp/swagger_spec_validator"
-    pypi     = "swagger-spec-validator/swagger-spec-validator-2.7.6.tar.gz"
+    pypi = "swagger-spec-validator/swagger-spec-validator-2.7.6.tar.gz"
 
-    version('2.7.6', sha256='73f33e631a58f407265f2f813d194f2762a2b86f9aa905e7eee3df9b7f9428d3')
+    version("2.7.6", sha256="73f33e631a58f407265f2f813d194f2762a2b86f9aa905e7eee3df9b7f9428d3")
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-jsonschema', type=('build', 'run'))
-    depends_on('py-pyyaml', type=('build', 'run'))
-    depends_on('py-six', type=('build', 'run'))
-    depends_on('py-pyrsistent@:0.16', when='^python@:3.0', type='build')
+    depends_on("py-setuptools", type="build")
+    depends_on("py-jsonschema", type=("build", "run"))
+    depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-six", type=("build", "run"))
+    depends_on("py-pyrsistent@:0.16", when="^python@:3.0", type="build")
