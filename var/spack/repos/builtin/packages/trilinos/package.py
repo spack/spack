c76fa07c8a2fbbfee6d9d71fb2012dae8160c8e3
Samuel Browne <sebrown@sandia.gov>
2024-03-06 13:53:53 -0700

    Add new Kokkos version dependency
    Also uniformize the syntax between the CUDA and ROCm blocks.

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -384,19 +384,21 @@ class Trilinos(CMakePackage, CudaPackage, ROCmPackage):
     # ###################### Dependencies ##########################
 
     # External Kokkos
-    depends_on("kokkos@4.1.00", when="@14.4.0: +kokkos")
+    depends_on("kokkos@4.2.01", when="@15.1.0: +kokkos")
+    depends_on("kokkos@4.1.00", when="@14.4.0:15.0.0 +kokkos")
+
     depends_on("kokkos +wrapper", when="trilinos@14.4.0: +kokkos +wrapper")
     depends_on("kokkos ~wrapper", when="trilinos@14.4.0: +kokkos ~wrapper")
 
     for a in CudaPackage.cuda_arch_values:
-        arch_str = "+cuda cuda_arch=" + a
-        kokkos_spec = "kokkos@4.1.00 " + arch_str
-        depends_on(kokkos_spec, when="@14.4.0 +kokkos " + arch_str)
+        arch_str = "+cuda cuda_arch={0}".format(a)
+        kokkos_spec = "kokkos {0}".format(arch_str)
+        depends_on(kokkos_spec, when="@14.4.0: +kokkos {0}".format(arch_str))
 
     for a in ROCmPackage.amdgpu_targets:
         arch_str = "+rocm amdgpu_target={0}".format(a)
-        kokkos_spec = "kokkos@4.1.00 {0}".format(arch_str)
-        depends_on(kokkos_spec, when="@14.4.0 +kokkos {0}".format(arch_str))
+        kokkos_spec = "kokkos {0}".format(arch_str)
+        depends_on(kokkos_spec, when="@14.4.0: +kokkos {0}".format(arch_str))
 
     depends_on("adios2", when="+adios2")
     depends_on("binder@1.3:", when="@15: +python", type="build")
