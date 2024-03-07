3fa8639927ff4eb846a04db7344a262d6f424952
Harmen Stoppels <me@harmenstoppels.nl>
2024-03-07 17:48:34 +0100

    shorten

--- lib/spack/llnl/util/lang.py
+++ lib/spack/llnl/util/lang.py
@@ -847,26 +847,24 @@ def get_entry_points(*, group: str):
     """Wrapper for ``importlib.metadata.entry_points``
 
     Args:
-        group (str): the group of entry points to select
+        group: entry points to select
 
     Returns:
-        EntryPoints for ``group``
-
+        EntryPoints for ``group`` or empty list if unsupported
     """
 
     try:
         import importlib.metadata  # type: ignore  # novermin
-
-        try:
-            entry_points = importlib.metadata.entry_points(group=group)
-        except TypeError:
-            # Prior to Python 3.10, entry_points accepted no parameters and always
-            # returned a dictionary of entry points, keyed by group.  See
-            # https://docs.python.org/3/library/importlib.metadata.html#entry-points
-            entry_points = importlib.metadata.entry_points().get(group, [])
-        yield from entry_points
     except ImportError:
-        yield from ()
+        return []
+
+    try:
+        return importlib.metadata.entry_points(group=group)
+    except TypeError:
+        # Prior to Python 3.10, entry_points accepted no parameters and always
+        # returned a dictionary of entry points, keyed by group.  See
+        # https://docs.python.org/3/library/importlib.metadata.html#entry-points
+        return importlib.metadata.entry_points().get(group, [])
 
 
 def load_module_from_file(module_name, module_path):
