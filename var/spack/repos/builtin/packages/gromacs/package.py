6f5ee62840ce39bf82bd62cc132dbefb446367ae
lpottier <48072795+lpottier@users.noreply.github.com>
2024-03-08 23:25:39 +0000

    [@spackbot] updating style on behalf of lpottier

--- var/spack/repos/builtin/packages/gromacs/package.py
+++ var/spack/repos/builtin/packages/gromacs/package.py
@@ -282,7 +282,7 @@ class Gromacs(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("cmake@3.18.4:3", type="build", when="@main")
     depends_on("cmake@3.16.0:3", type="build", when="%fj")
     depends_on("pkgconfig", type="build")
-    
+
     depends_on("cuda", when="+cuda")
     with when("+rocm"):
         depends_on("sycl")
