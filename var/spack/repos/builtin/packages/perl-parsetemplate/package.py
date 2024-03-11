edb8b4e1c5724029cb4159fdc200a3f61c60a27b
ChristopherChristofi <christopherlchristofi@outlook.com>
2024-03-11 16:20:36 +0000

    perl-parsetemplate: update to latest copyright date and add license

--- var/spack/repos/builtin/packages/perl-parsetemplate/package.py
+++ var/spack/repos/builtin/packages/perl-parsetemplate/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -11,5 +11,7 @@ class PerlParsetemplate(PerlPackage):
 
     homepage = "https://metacpan.org/pod/Parse::Template"
     url = "https://cpan.metacpan.org/authors/id/P/PS/PSCUST/ParseTemplate-3.08.tar.gz"
+
+    license("Artistic-1.0-Perl OR GPL-1.0-or-later")
 
     version("3.08", sha256="3c7734f53999de8351a77cb09631d7a4a0482b6f54bca63d69d5a4eec8686d51")
