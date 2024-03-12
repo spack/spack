304641ba58b72cc1acb8bad4c2bbd6ad1204acd3
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-03-12 09:31:15 +0100

    Update archspec to v0.2.3 (#42854)

--- lib/spack/spack/target.py
+++ lib/spack/spack/target.py
@@ -102,7 +102,10 @@ class Target:
         if self.microarchitecture.vendor == "generic":
             return str(self)
 
-        return syaml.syaml_dict(self.microarchitecture.to_dict(return_list_of_items=True))
+        # Get rid of compiler flag information before turning the uarch into a dict
+        uarch_dict = self.microarchitecture.to_dict()
+        uarch_dict.pop("compilers", None)
+        return syaml.syaml_dict(uarch_dict.items())
 
     def __repr__(self):
         cls_name = self.__class__.__name__
