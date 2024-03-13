9ac204110f78c084e052ef64a34a1932073f10b3
sbulut <sbulut@3vgeomatics.com>
2024-03-13 00:18:49 -0700

    restore header and licence

--- var/spack/repos/builtin/packages/py-line-profiler/package.py
+++ var/spack/repos/builtin/packages/py-line-profiler/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -13,6 +13,8 @@ class PyLineProfiler(PythonPackage):
 
     homepage = "https://github.com/pyutils/line_profiler"
     pypi = "line_profiler/line_profiler-2.0.tar.gz"
+
+    license("PSF-2.0")
 
     version("4.1.2", sha256="aa56578b0ff5a756fe180b3fda7bd67c27bbd478b3d0124612d8cf00e4a21df2")
     version("3.5.1", sha256="77400208bfbd5d4341938a9a3a4fb5194f5af7fc23b2d496c913755f8310e8b8")
