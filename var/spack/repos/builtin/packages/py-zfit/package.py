be2160eb7accfd53bc23d68774d70e241be67122
Jonas Eschle <mayou36@jonas.eschle.com>
2024-03-14 16:42:58 -0400

    enh: add 0.18

--- var/spack/repos/builtin/packages/py-zfit/package.py
+++ var/spack/repos/builtin/packages/py-zfit/package.py
@@ -18,9 +18,9 @@ class PyZfit(PythonPackage):
     maintainers("jonas-eschle")
     license("BSD-3-Clause", checked_by="jonas-eschle")
 
-    # TODO: reactivate once TF 2.15 is ready https://github.com/spack/spack/pull/41069
-    # version("0.18.1", sha256="fbc6b3a636d8dc74fb2e69dfec5855f534c4583ec18efac9e9107ad45b18eb43")
-    # version("0.18.0", sha256="21d9479480f74945c67707b715780693bd4e94062c551bf41fe04a2eddb47fab")
+    version("0.18.2", sha256="099b111e135937966b4c6342c7738731f112aea33e1b9f4a9785d2eac9e530f1")
+    version("0.18.1", sha256="fbc6b3a636d8dc74fb2e69dfec5855f534c4583ec18efac9e9107ad45b18eb43")
+    version("0.18.0", sha256="21d9479480f74945c67707b715780693bd4e94062c551bf41fe04a2eddb47fab")
     version("0.17.0", sha256="cd60dfc360c82666af4e8dddd78edb0ab95a095b9dd0868457f0981dc03afa5a")
     version("0.16.0", sha256="b3b170af23b61d7e265d6fb1bab1d052003f3fb41b3c537527cc1e5a1066dc10")
     version("0.15.5", sha256="00a1138429e8a7f830c9e229b9c0bcd6071b95dadd8c87eb81191079fb679225")
@@ -54,7 +54,7 @@ class PyZfit(PythonPackage):
         with when("+hs3"):
             depends_on("py-asdf")
 
-        depends_on("py-attrs", when="@0.15:18.0")
+        depends_on("py-attrs", when="@0.15")
         depends_on("py-typing-extensions", when="^python@:3.8")
         depends_on("py-boost-histogram")
         depends_on("py-colorama")
