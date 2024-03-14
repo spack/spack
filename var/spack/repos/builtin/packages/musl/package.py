9a17670e61fc283335208385e11096b95d5e7e67
Harmen Stoppels <me@harmenstoppels.nl>
2024-03-14 15:36:16 +0100

    provide libc

--- var/spack/repos/builtin/packages/musl/package.py
+++ var/spack/repos/builtin/packages/musl/package.py
@@ -29,6 +29,8 @@ class Musl(MakefilePackage):
 
     license("MIT")
 
+    provides("libc")
+
     version("1.2.4", sha256="7a35eae33d5372a7c0da1188de798726f68825513b7ae3ebe97aaaa52114f039")
     version("1.2.3", sha256="7d5b0b6062521e4627e099e4c9dc8248d32a30285e959b7eecaa780cf8cfd4a4")
     version("1.2.2", sha256="9b969322012d796dc23dda27a35866034fa67d8fb67e0e2c45c913c3d43219dd")
