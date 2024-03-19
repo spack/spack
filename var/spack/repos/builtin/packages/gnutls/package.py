add2c2ab39cf3ee781df0ec12e7562ccda3ca117
Alec Scott <hi@alecbcs.com>
2024-03-19 14:23:19 -0700

    gnutls: add v3.8.3 (#43229)
    Co-authored-by: Harmen Stoppels <harmenstoppels@gmail.com>

--- var/spack/repos/builtin/packages/gnutls/package.py
+++ var/spack/repos/builtin/packages/gnutls/package.py
@@ -17,8 +17,11 @@ class Gnutls(AutotoolsPackage):
     homepage = "https://www.gnutls.org"
     url = "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.5/gnutls-3.5.19.tar.xz"
 
+    maintainers("alecbcs")
+
     license("LGPL-2.1-or-later")
 
+    version("3.8.3", sha256="f74fc5954b27d4ec6dfbb11dea987888b5b124289a3703afcada0ee520f4173e")
     version("3.7.8", sha256="c58ad39af0670efe6a8aee5e3a8b2331a1200418b64b7c51977fb396d4617114")
     version("3.6.15", sha256="0ea8c3283de8d8335d7ae338ef27c53a916f15f382753b174c18b45ffd481558")
     version("3.6.14", sha256="5630751adec7025b8ef955af4d141d00d252a985769f51b4059e5affa3d39d63")
@@ -35,6 +38,10 @@ class Gnutls(AutotoolsPackage):
 
     # gnutls+guile is currently broken on MacOS.  See Issue #11668
     conflicts("+guile", when="platform=darwin")
+
+    # -Wimplicit-int is an error in newer clang
+    conflicts("%clang@16:", when="@:3.7")
+    conflicts("%apple-clang@15:", when="@:3.7")
 
     # Note that version 3.3.9 of gnutls doesn't support nettle 3.0.
     depends_on("nettle@3.4.1:", when="@3.6.7.1:")
