4ed830d9f1348fc5b178fa9a9f050a4c22488822
jmcarcell <jmcarcell@users.noreply.github.com>
2024-03-20 13:16:55 +0100

    fastjet: add version 3.4.2

--- var/spack/repos/builtin/packages/fastjet/package.py
+++ var/spack/repos/builtin/packages/fastjet/package.py
@@ -25,6 +25,7 @@ class Fastjet(AutotoolsPackage):
 
     license("GPL-2.0-only")
 
+    version("3.4.2", sha256="b3d33155b55ce43f420cd6d99b525acf7bdc2593a7bb7ea898a9ddb3d8ca38e3")
     version("3.4.1", sha256="05608c6ff213f06dd9de723813d6b4dccd51e661ac13098f74bfc9eeaf1cb5aa")
     version("3.4.0", sha256="ee07c8747c8ead86d88de4a9e4e8d1e9e7d7614973f5631ba8297f7a02478b91")
     version("3.3.4", sha256="432b51401e1335697c9248519ce3737809808fc1f6d1644bfae948716dddfc03")
