a1eeae1d35544d21921eec2e24b367378345a194
Jordan Ogas <jogas@lanl.gov>
2024-03-26 13:22:14 -0600

    pkg-config: avoid warning as error

--- var/spack/repos/builtin/packages/pkg-config/package.py
+++ var/spack/repos/builtin/packages/pkg-config/package.py
@@ -61,4 +61,8 @@ class PkgConfig(AutotoolsPackage):
             # the cycle by using the internal glib.
             config_args.append("--with-internal-glib")
 
+        if "oneapi" in self.spec.compiler.name:
+            # Avoid treating int-conversion warning as error with oneapi.
+            config_args.append("CFLAGS=-Wno-error=int-conversion")
+
         return config_args
