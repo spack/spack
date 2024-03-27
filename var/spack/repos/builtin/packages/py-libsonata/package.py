b6449ce9480b4b4a263a6f16f3ddc2173333a907
Tristan Carel <tristan.carel@epfl.ch>
2024-03-27 11:34:00 +0100

    Fix min required versions / use spack packages instead of submodules

--- var/spack/repos/builtin/packages/py-libsonata/package.py
+++ var/spack/repos/builtin/packages/py-libsonata/package.py
@@ -13,20 +13,22 @@ class PyLibsonata(PythonPackage):
     git = "https://github.com/BlueBrain/libsonata.git"
     pypi = "libsonata/libsonata-0.1.14.tar.gz"
 
-    submodules = True
-
     maintainers("tristan0x")
 
     version("master", branch="master")
     version("0.1.25", sha256="b332efa718123ee265263e1583a5998eaa945a13b8a22903873764cf1d8173fa")
 
-    depends_on("cmake@3.3:", type="build")
+    depends_on("catch2@2.13:", type="test")
+    depends_on("cmake@3.16:", type="build")
+    depends_on("fmt@7.1:")
     depends_on("hdf5@1.14:")
-    depends_on("py-pybind11@2.11:")
+    depends_on("highfive@2.9:")
+    depends_on("nlohmann-json@3.9.1")
+    depends_on("py-pybind11@2.11.1:")
 
     depends_on("py-numpy@1.17.3:", type=("build", "run"))
     depends_on("py-setuptools@42:", type="build", when="@0.1:")
     depends_on("py-setuptools-scm@3.4:", type="build", when="@0.1:")
 
     def patch(self):
-        filter_file("EXTLIB_FROM_SUBMODULES", "FALSE", "python/CMakeLists.txt")
+        filter_file("-DEXTLIB_FROM_SUBMODULES=ON", "-DEXTLIB_FROM_SUBMODULES=OFF", "setup.py")
