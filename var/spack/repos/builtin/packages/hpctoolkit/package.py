18478856e8fe8b11f577ec0fc63b7bd7aaa2ad3a
afzpatel <122491982+afzpatel@users.noreply.github.com>
2024-03-27 13:36:10 -0400

    bump rocprofiler-dev to 6.0 and add aqlprofile (#42459)
    * Initial commit to bump rocprofiler-dev to 6.0 and add aqlprofile recipe
    * bump to 6.0.2 and extracting binaries from deb pkg
    * fixes for hpctoolkit build errors
    * add yum and zyp aqlprofile packages
    * fix style issues

--- var/spack/repos/builtin/packages/hpctoolkit/package.py
+++ var/spack/repos/builtin/packages/hpctoolkit/package.py
@@ -229,6 +229,9 @@ class Hpctoolkit(AutotoolsPackage, MesonPackage):
     depends_on("python@3.4:", type="build", when="@2020.03:2020.08")
     patch("python3.patch", when="@2020.03:2020.08")
 
+    # HIP header files require HIP_PLATFORM_AMD to be set for AMD GPUs
+    patch("define-hip-platform-as-amd.patch", when="^hip@6:")
+
     # Fix a bug where make would mistakenly overwrite hpcrun-fmt.h.
     # https://gitlab.com/hpctoolkit/hpctoolkit/-/merge_requests/751
     def patch(self):
