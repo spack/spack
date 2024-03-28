a60b4bf1d5ad48ade15975ffea8b300475573975
Greg Becker <becker33@llnl.gov>
2024-03-28 08:00:55 -0700

    spack find: add options for local/upstream only (#42999)
    Users requested an option to filter between local/upstream results in `spack find` output.
    
    ```
    # default behavior, same as without --install-tree argument
    $ spack find --install-tree all
    
    # show only local results
    $ spack find --install-tree local  
    
    # show results from all upstreams
    $ spack find --install-tree upstream 
    
    # show results from a particular upstream or the local install_tree
    $ spack find --install-tree /path/to/install/tree/root
    ```
    
    ---------
    
    Co-authored-by: becker33 <becker33@users.noreply.github.com>

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -1179,7 +1179,7 @@ _spack_fetch() {
 _spack_find() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help --format -H --hashes --json -d --deps -p --paths --groups --no-groups -l --long -L --very-long -t --tag -N --namespaces -c --show-concretized -f --show-flags --show-full-compiler -x --explicit -X --implicit -u --unknown -m --missing -v --variants --loaded -M --only-missing --deprecated --only-deprecated --start-date --end-date"
+        SPACK_COMPREPLY="-h --help --format -H --hashes --json -d --deps -p --paths --groups --no-groups -l --long -L --very-long -t --tag -N --namespaces -c --show-concretized -f --show-flags --show-full-compiler -x --explicit -X --implicit -u --unknown -m --missing -v --variants --loaded -M --only-missing --deprecated --only-deprecated --install-tree --start-date --end-date"
     else
         _installed_packages
     fi
