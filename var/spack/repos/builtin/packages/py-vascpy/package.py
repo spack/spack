2da54bab34cf746dec3b1581add994457a73623a
Tristan Carel <tristan.carel@epfl.ch>
2024-04-02 09:14:21 +0200

    py-pytest-mpi, py-vascpy: backport from upstream (#2367)
    * py-vascpy: backport from upstream
    * py-pytest-mpi:i backport from upstream

--- var/spack/repos/builtin/packages/py-vascpy/package.py
+++ var/spack/repos/builtin/packages/py-vascpy/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -14,17 +14,20 @@ class PyVascpy(PythonPackage):
     git = "https://github.com/BlueBrain/vascpy.git"
     pypi = "vascpy/vascpy-0.1.0.tar.gz"
 
-    version("develop", branch="main")
+    license("Apache-2.0")
+
+    maintainers("tristan0x")
+
+    version("main", branch="main")
     version("0.1.1", sha256="1b6bd1399a0388b36241364de74ef709cda2b659e45448fbbdd7efc93bbd8b27")
-    version("0.1.0", sha256="0d6aa4faebe75bce36f44bd6f884d015a8f24e1dc76977d8c7fd6be7ea8e725b")
 
+    depends_on("py-setuptools-scm@3.4:", type="build")
     depends_on("py-setuptools@42:", type="build")
-    depends_on("py-setuptools-scm", type="build")
 
-    depends_on("py-numpy@1.17.0:", type=("build", "run"))
-    depends_on("py-scipy@1.0.0:", type=("build", "run"))
-    depends_on("py-h5py@3.4.0:", type=("build", "run"))
-    depends_on("py-pandas@1.0.0:", type=("build", "run"))
-    depends_on("py-morphio@3.0.0:", type=("build", "run"))
-    depends_on("py-libsonata@0.1.8:", type=("build", "run"))
     depends_on("py-click@8.0.0:", type=("build", "run"))
+    depends_on("py-h5py@3.4.0:", type=("build", "run"))
+    depends_on("py-libsonata@0.1.8:", type=("build", "run"))
+    depends_on("py-morphio@3.0.0:", type=("build", "run"))
+    depends_on("py-numpy@1.17.0:", type=("build", "run"))
+    depends_on("py-pandas@1.0.0:", type=("build", "run"))
+    depends_on("py-scipy@1.0.0:", type=("build", "run"))
