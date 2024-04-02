43c27b8bd9082b2dffa51ed28c95905160098d7a
Renjith Ravindran Kannath <Renjith.RavindranKannath@amd.com>
2024-04-01 22:29:30 -0700

    Style check error fix

--- var/spack/repos/builtin/packages/rocm-opencl/package.py
+++ var/spack/repos/builtin/packages/rocm-opencl/package.py
@@ -87,13 +87,11 @@ class RocmOpencl(CMakePackage):
     patch(
         "https://github.com/ROCm/clr/commit/c4f773db0b4ccbbeed4e3d6c0f6bff299c2aa3f0.patch?full_index=1",
         sha256="5bb9b0e08888830ccf3a0a658529fe25f4ee62b5b8890f349bf2cc914236eb2f",
-        #working_dir="clr",
         when="@5.7:",
     )
     patch(
         "https://github.com/ROCm/clr/commit/7868876db742fb4d44483892856a66d2993add03.patch?full_index=1",
         sha256="7668b2a710baf4cb063e6b00280fb75c4c3e0511575e8298a9c7ae5143f60b33",
-        #working_dir="clr",
         when="@5.7:",
     )
 
