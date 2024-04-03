fee44c8d5f652ac84886103c832f5ca925bf44d3
Greg Sjaardema <gsjaardema@gmail.com>
2024-04-03 15:46:57 -0600

    Seacas exodusii 04 2024 (#43468)
    * SEACAS: Update package.py to handle new SEACAS project name
      The base project name for the SEACAS project has changed from
      "SEACASProj" to "SEACAS" as of @2022-10-14, so the package
      needed to be updated to use the new project name when needed.
    
      The refactor also changes several:
          "-DSome_CMAKE_Option:BOOL=ON"
      to
         define("Some_CMAKE_Option", True)
    * SEACAS, EXODUSII: New version; deprecate older versions; better variant descriptions
    * [@spackbot] updating style on behalf of gsjaardema
    * Fix long lines reported by flake8
    
    ---------
    
    Co-authored-by: gsjaardema <gsjaardema@users.noreply.github.com>

--- var/spack/repos/builtin/packages/exodusii/package.py
+++ var/spack/repos/builtin/packages/exodusii/package.py
@@ -32,6 +32,9 @@ class Exodusii(CMakePackage):
 
     version("master", branch="master")
     version(
+        "2024-04-03", sha256="72b095bae64b2b6c232630f79de763c6ade00c9b1199fc6980800891b2ab3751"
+    )
+    version(
         "2024-03-11", sha256="5d417aa652e4ec8d66e27714c63b8cb5a7f878fb7b2ec55f629636fcff7c0f00"
     )
     version(
@@ -59,39 +62,61 @@ class Exodusii(CMakePackage):
         "2022-01-27", sha256="d21c14b9b30f773cef8e2029773f3cc35da021eebe9060298231f95021eb814f"
     )
     version(
-        "2021-10-11", sha256="5c04d252e1c4a10b037aa352b89487e581ec6b52bdb46e9e85f101bbdcd9c388"
+        "2021-10-11",
+        sha256="5c04d252e1c4a10b037aa352b89487e581ec6b52bdb46e9e85f101bbdcd9c388",
+        deprecated=True,
     )
     version(
-        "2021-04-05", sha256="f40d318674753287b8b28d2b4e5cca872cd772d4c7383af4a8f3eeb48fcc7ec0"
+        "2021-04-05",
+        sha256="f40d318674753287b8b28d2b4e5cca872cd772d4c7383af4a8f3eeb48fcc7ec0",
+        deprecated=True,
     )
     version(
-        "2021-04-02", sha256="811037a68eaff0daf9f34bd31b2ab1c9b8f028dfcb998ab01fbcb80d9458257c"
+        "2021-04-02",
+        sha256="811037a68eaff0daf9f34bd31b2ab1c9b8f028dfcb998ab01fbcb80d9458257c",
+        deprecated=True,
     )
     version(
-        "2021-01-20", sha256="6ff7c3f0651138f2e2305b5270108ca45f96346a739b35a126a0a260c91cbe64"
+        "2021-01-20",
+        sha256="6ff7c3f0651138f2e2305b5270108ca45f96346a739b35a126a0a260c91cbe64",
+        deprecated=True,
     )
     version(
-        "2021-01-06", sha256="69cafef17d8e624c2d9871f3a281ff3690116a6f82162fe5c1507bb4ecd6a32a"
+        "2021-01-06",
+        sha256="69cafef17d8e624c2d9871f3a281ff3690116a6f82162fe5c1507bb4ecd6a32a",
+        deprecated=True,
     )
     version(
-        "2020-08-13", sha256="5b128a8ad9b0a69cff4fe937828d6d1702f1fe8aa80d4751e6522939afe62957"
+        "2020-08-13",
+        sha256="5b128a8ad9b0a69cff4fe937828d6d1702f1fe8aa80d4751e6522939afe62957",
+        deprecated=True,
     )
     version(
-        "2020-05-12", sha256="0402facf6cf23d903d878fb924b5d57e9f279dead5b92cf986953a6b91a6e81f"
+        "2020-05-12",
+        sha256="0402facf6cf23d903d878fb924b5d57e9f279dead5b92cf986953a6b91a6e81f",
+        deprecated=True,
     )
     version(
-        "2020-03-16", sha256="ed1d42c8c657931ecd45367a465cf9c00255772d9cd0811fc9baacdb67fc71fa"
+        "2020-03-16",
+        sha256="ed1d42c8c657931ecd45367a465cf9c00255772d9cd0811fc9baacdb67fc71fa",
+        deprecated=True,
     )
     version(
-        "2020-01-16", sha256="db69dca25595e88a40c00db0ccf2afed1ecd6008ba30bb478a4e1c5dd61998b8"
+        "2020-01-16",
+        sha256="db69dca25595e88a40c00db0ccf2afed1ecd6008ba30bb478a4e1c5dd61998b8",
+        deprecated=True,
     )
     version(
-        "2019-12-18", sha256="88a71de836aa26fd63756cf3ffbf3978612edc5b6c61fa8de32fe9d638007774"
+        "2019-12-18",
+        sha256="88a71de836aa26fd63756cf3ffbf3978612edc5b6c61fa8de32fe9d638007774",
+        deprecated=True,
     )
     version(
-        "2019-10-14", sha256="f143d90e8a7516d25979d1416e580dea638332db723f26ae94a712dfe4052e8f"
+        "2019-10-14",
+        sha256="f143d90e8a7516d25979d1416e580dea638332db723f26ae94a712dfe4052e8f",
+        deprecated=True,
     )
-    version("2016-08-09", commit="2ffeb1bd39454ad5aa230e12969ce976f3d1c92b")
+    version("2016-08-09", commit="2ffeb1bd39454ad5aa230e12969ce976f3d1c92b", deprecated=True)
 
     patch("Fix-ioss-tpl.patch", when="@2021-10-11:")
 
