58f280fba01598a6d342a630e71e24ec50f2f1d6
Luc Berger-Vergiat <lberge@sandia.gov>
2024-04-03 10:20:41 -0600

    Nalu: updating Trilinos recipe a bit
    Basic changes to build/install nalu properly using Spack.
    Some more changes would be nice for instance adding an
    option to build against Trilinos master or develop. Adding
    a dependency on googletest to avoid the annoying build
    failures in the unit-tests.

--- var/spack/repos/builtin/packages/nalu/package.py
+++ var/spack/repos/builtin/packages/nalu/package.py
@@ -31,21 +31,21 @@ class Nalu(CMakePackage):
 
     # Required dependencies
     depends_on("mpi")
-    depends_on("yaml-cpp@0.5.3:", when="+shared")
-    depends_on("yaml-cpp~shared@0.5.3:", when="~shared")
+    depends_on("yaml-cpp@0.5.3:0.6.2", when="+shared")
+    depends_on("yaml-cpp~shared@0.5.3:0.6.2", when="~shared")
     # Cannot build Trilinos as a shared library with STK on Darwin
     # which is why we have a 'shared' variant for Nalu
     # https://github.com/trilinos/Trilinos/issues/2994
     depends_on(
-        "trilinos"
+        "trilinos@develop"
         "+mpi+exodus+tpetra+muelu+belos+ifpack2+amesos2+zoltan+stk+boost"
-        "~superlu-dist+superlu+hdf5+shards~hypre"
-        "@master"
+        "~superlu-dist+superlu+hdf5+shards~hypre gotype=long"
     )
     depends_on("trilinos~shared", when="~shared")
     # Optional dependencies
     depends_on("tioga", when="+tioga+shared")
     depends_on("tioga~shared", when="+tioga~shared")
+    depends_on("fmt")
 
     def cmake_args(self):
         spec = self.spec
