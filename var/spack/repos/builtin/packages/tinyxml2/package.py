227a78835df217150fa30e646bd5c5e28ba20db9
one <wangan.cs@gmail.com>
2024-04-03 23:52:20 +0800

    Add new versions of tinyxml2
    Added 7.0.0 to 10.0.0

--- var/spack/repos/builtin/packages/tinyxml2/package.py
+++ var/spack/repos/builtin/packages/tinyxml2/package.py
@@ -14,6 +14,10 @@ class Tinyxml2(CMakePackage):
 
     license("Zlib")
 
+    version("10.0.0", sha256="3bdf15128ba16686e69bce256cc468e76c7b94ff2c7f391cc5ec09e40bff3839")
+    version("9.0.0", sha256="cc2f1417c308b1f6acc54f88eb70771a0bf65f76282ce5c40e54cfe52952702c")
+    version("8.0.0", sha256="6ce574fbb46751842d23089485ae73d3db12c1b6639cda7721bf3a7ee862012c")
+    version("7.0.0", sha256="fa0d1c745d65d4d833e62cb183e23c2034dc7a35ec1a4977e808bdebb9b4fe60")
     version("6.2.0", sha256="cdf0c2179ae7a7931dba52463741cf59024198bbf9673bf08415bcb46344110f")
     version("4.0.1", sha256="14b38ef25cc136d71339ceeafb4856bb638d486614103453eccd323849267f20")
     version("4.0.0", sha256="90add44f06de081047d431c08d7269c25b4030e5fe19c3bc8381c001ce8f258c")
