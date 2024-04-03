1d0cd4aa2831064f4b63cebd8820426b9352858e
Martin Aumüller <aumuell@reserv.at>
2024-04-03 12:22:19 +0200

    qt-declarative: add v6.7.0

--- var/spack/repos/builtin/packages/qt-declarative/package.py
+++ var/spack/repos/builtin/packages/qt-declarative/package.py
@@ -16,6 +16,7 @@ class QtDeclarative(QtPackage):
 
     license("BSD-3-Clause")
 
+    version("6.7.0", sha256="dc3fec16cbe0f706b2b6114e5dbb884269543f2d679a0a3a63b4f686156adf73")
     version("6.6.2", sha256="6079545e04e7704fcab8e50687e1ee9df8d3bb43288a1602ff0f142e640a5b51")
     version("6.6.1", sha256="b1f5a75c2ea967d21b2c45f56ba1de66e2bf14a581b2f0d8e776441f1bebd0e7")
     version("6.6.0", sha256="2e52ef00736a9954426adf454cfb365fabdffb5703c814c188bc866cbf9f4dad")
