3a0efeecf151d879cef7053903bb9485b61c9561
Alex Richert <alexander.richert@noaa.gov>
2024-04-04 09:56:19 -0700

    add g2c@1.9.0 (#43482)

--- var/spack/repos/builtin/packages/g2c/package.py
+++ var/spack/repos/builtin/packages/g2c/package.py
@@ -18,6 +18,7 @@ class G2c(CMakePackage):
     maintainers("AlexanderRichert-NOAA", "Hang-Lei-NOAA", "edwardhartnett")
 
     version("develop", branch="develop")
+    version("1.9.0", sha256="5554276e18bdcddf387a08c2dd23f9da310c6598905df6a2a244516c22ded9aa")
     version("1.8.0", sha256="4ce9f5a7cb0950699fe08ebc5a463ab4d09ef550c050391a319308a2494f971f")
     version("1.7.0", sha256="73afba9da382fed73ed8692d77fa037bb313280879cd4012a5e5697dccf55175")
     version("1.6.4", sha256="5129a772572a358296b05fbe846bd390c6a501254588c6a223623649aefacb9d")
