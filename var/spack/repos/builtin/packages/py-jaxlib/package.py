4d7c90eabcfe7e620efc9e980435ab483e70b729
Adam J. Stewart <ajstewart426@gmail.com>
2024-04-07 00:17:25 +0200

    v0.4.6 doesn't work either

--- var/spack/repos/builtin/packages/py-jaxlib/package.py
+++ var/spack/repos/builtin/packages/py-jaxlib/package.py
@@ -100,7 +100,7 @@ class PyJaxlib(PythonPackage, CudaPackage):
     )
 
     # https://github.com/google/jax/issues/19992
-    conflicts("@0.4.7:", when="target=ppc64le:")
+    conflicts("@0.4.6:", when="target=ppc64le:")
 
     def patch(self):
         self.tmp_path = tempfile.mkdtemp(prefix="spack")
