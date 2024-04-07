93e6f5fa4e47c6cc9183eae1b531c261a6664598
Jonas Eschle <mayou36@jonas.eschle.com>
2024-04-07 06:04:23 -0400

    Update jax & jaxlib versions (#42863)
    * upgrade new versions
    
    * style fix
    
    * update jaxlib deps (not cuda and bazel yet)
    
    * update jaxlib cuda versions
    
    * update jaxlib cuda versions
    
    * update jaxlib cuda versions
    
    * chore: style fix
    
    * Update package.py
    
    * Update package.py
    
    * fix:  typo
    
    * docs: add source for cuda version
    
    * py-jaxlib 0.4.14 also doesn't build on ppc64le
    
    * Add 0.4.26
    
    ---------
    
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/builtin/packages/py-jaxlib/package.py
+++ var/spack/repos/builtin/packages/py-jaxlib/package.py
@@ -20,9 +20,19 @@ class PyJaxlib(PythonPackage, CudaPackage):
     license("Apache-2.0")
     maintainers("adamjstewart")
 
+    version("0.4.26", sha256="ddc14da1eaa34f23430d40ad9b9585088575cac439a2fa1c6833a247e1b221fd")
     version("0.4.25", sha256="fc1197c401924942eb14185a61688d0c476e3e81ff71f9dc95e620b57c06eec8")
     version("0.4.24", sha256="c4e6963c2c36f634a9a1765e476a1ed4e6c4a7954465ebf72e29f344c28ddc28")
+    version("0.4.23", sha256="e4c06d62ba54becffd91abc862627b8b11b79c5a77366af8843b819665b6d568")
+    version("0.4.21", sha256="8d57f66d00b9c0b824b1eff84adda5b765a412b3f316ef7c773632d1edbf9477")
+    version("0.4.20", sha256="058410d2bc12f7562c7b01e0c8cd587cb68059c12f78bc945055e5ddc445f5fd")
+    version("0.4.19", sha256="51242b217a1f82474e42d24f09ed5dedff951eeb4579c6e49e706d1adfd6949d")
     version("0.4.16", sha256="85c8bc050abe0a2cf62e8cfc7edb4904dd3807924b5714ec6277f291c576b5ca")
+    version("0.4.14", sha256="9f309476a8f6337717b059b8d10b5859b4134c30cf8f1220bb70379b5e2744a4")
+    version("0.4.11", sha256="bdfc45f33970beba5caf28d061668a4863f05994deea26791db50ea605fc2e36")
+    version("0.4.7", sha256="0578d5dd5035b5225cadb6a62ca5f93dd76b70292268502fc01a0fd9ca7001d0")
+    version("0.4.6", sha256="2c9bf8962815bc54ef524e33dc8eda9d165d379fe87e0df210f316adead27787")
+    version("0.4.4", sha256="881f402c7983b56b185e182d5315dd64c9f5320be96213d0415996ece1826806")
     version("0.4.3", sha256="2104735dc22be2b105e5517bd5bc6ae97f40e8e9e54928cac1585c6112a3d910")
     version(
         "0.3.22",
@@ -40,9 +50,12 @@ class PyJaxlib(PythonPackage, CudaPackage):
     # build/build.py
     depends_on("py-build", when="@0.4.14:", type="build")
 
+    # Based on PyPI wheels
+    depends_on("python@3.9:3.12", when="@0.4.17:", type=("build", "run"))
+    depends_on("python@3.9:3.11", when="@0.4.14:0.4.16", type=("build", "run"))
+    depends_on("python@3.8:3.11", when="@0.4.6:0.4.13", type=("build", "run"))
+
     # jaxlib/setup.py
-    depends_on("python@3.9:", when="@0.4.14:", type=("build", "run"))
-    depends_on("python@3.8:", when="@0.4:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-scipy@1.9:", when="@0.4.19:", type=("build", "run"))
     depends_on("py-scipy@1.7:", when="@0.4.7:", type=("build", "run"))
@@ -63,12 +76,16 @@ class PyJaxlib(PythonPackage, CudaPackage):
     depends_on("bazel@4.2.1", when="@0.1.75:0.1.76", type="build")
     depends_on("bazel@4.1.0", when="@0.1.70:0.1.74", type="build")
 
-    # README.md
-    depends_on("cuda@11.4:", when="@0.4:+cuda")
+    # jaxlib/setup.py
+    depends_on("cuda@12.1.105:", when="@0.4.26:+cuda")
+    depends_on("cuda@11.8:", when="@0.4.11:+cuda")
+    depends_on("cuda@11.4:", when="@0.4.0:0.4.7+cuda")
     depends_on("cuda@11.1:", when="@0.3+cuda")
     # https://github.com/google/jax/issues/12614
     depends_on("cuda@11.1:11.7.0", when="@0.1+cuda")
-    depends_on("cudnn@8.2:", when="@0.4:+cuda")
+
+    depends_on("cudnn@8.8:", when="@0.4.11:+cuda")
+    depends_on("cudnn@8.2:", when="@0.4:0.4.7+cuda")
     depends_on("cudnn@8.0.5:", when="+cuda")
 
     # Historical dependencies
@@ -83,7 +100,7 @@ class PyJaxlib(PythonPackage, CudaPackage):
     )
 
     # https://github.com/google/jax/issues/19992
-    conflicts("@0.4.16:0.4.25", when="target=ppc64le:")
+    conflicts("@0.4.4:", when="target=ppc64le:")
 
     def patch(self):
         self.tmp_path = tempfile.mkdtemp(prefix="spack")
