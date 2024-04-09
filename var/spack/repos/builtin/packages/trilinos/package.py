9e867313edc42ba032a139a9c2e7ff7312d93c3b
psakievich <psakiev@sandia.gov>
2024-04-09 06:49:06 -0600

    Trilinos: More accurate stk boot dependency (#43550)
    Boost was not required as of `@13.4.0`

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -420,7 +420,7 @@ class Trilinos(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("binder@1.3:", when="@15: +python", type="build")
     depends_on("blas")
     depends_on("boost+graph+math+exception+stacktrace", when="+boost")
-    depends_on("boost+graph+math+exception+stacktrace", when="@:14.0.0 +stk")
+    depends_on("boost+graph+math+exception+stacktrace", when="@:13.4.0 +stk")
     depends_on("cgns", when="+exodus")
     depends_on("cmake@3.23:", type="build", when="@14.0.0:")
     depends_on("hdf5+hl", when="+hdf5")
