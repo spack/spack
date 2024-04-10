a2ba23755b1487ec471053ada7d031d6e35b1676
Martin Aumüller <aumuell@reserv.at>
2024-04-10 23:30:09 +0200

    qt-*: add v6.6.2, v6.6.3, and v6.7.0 (#43559)
    * qt-base: add v6.6.2
    
    * qt-declarative: add v6.6.2
    
    * qt-quick3d: add v6.6.2
    
    * qt-quicktimeline: add v6.6.2
    
    * qt-shadertools: add v6.6.2
    
    * qt-svg: add v6.6.2
    
    * qt-base: add v6.7.0
    
    * qt-svg: add v6.7.0
    
    * qt-declarative: add v6.7.0
    
    * qt-quick3d: add v6.7.0
    
    * qt-quicktimeline: add v6.7.0
    
    * qt-shadertools: add v6.7.0
    
    * qt-*: add v6.6.3
    
    ---------
    
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- var/spack/repos/builtin/packages/qt-base/package.py
+++ var/spack/repos/builtin/packages/qt-base/package.py
@@ -93,6 +93,9 @@ class QtBase(QtPackage):
 
     license("BSD-3-Clause")
 
+    version("6.7.0", sha256="e17f016ec987092423e86d732c0f9786124598877fa00970fd806da113c02ca5")
+    version("6.6.3", sha256="11abfcae323d295129f644f1828064e05af7d64d49edb0e00bfb8e8cb9691259")
+    version("6.6.2", sha256="2cbdc4791c5838fddb1ce7ee693b165bb4acf3f81acd6c1bf9e56413b25050df")
     version("6.6.1", sha256="eb091c56e8c572d35d3da36f94f9e228892d43aecb559fa4728a19f0e44914c4")
     version("6.6.0", sha256="882f39ea3a40a0894cd64e515ce51711a4fab79b8c47bc0fe0279e99493a62cf")
     version("6.5.3", sha256="174021c4a630df2e7e912c2e523844ad3cb5f90967614628fd8aa15ddbab8bc5")
