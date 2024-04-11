f62230323c1a082cd36445ee01ab3cdcb93be015
Tristan Carel <tristan.carel@epfl.ch>
2024-04-11 21:19:30 +0200

    py-simpleeval: add version 0.9.13 (#43568)

--- var/spack/repos/builtin/packages/py-simpleeval/package.py
+++ var/spack/repos/builtin/packages/py-simpleeval/package.py
@@ -16,7 +16,8 @@ class PySimpleeval(PythonPackage):
 
     license("MIT")
 
+    version("0.9.13", sha256="4a30f9cc01825fe4c719c785e3762623e350c4840d5e6855c2a8496baaa65fac")
     version("0.9.12", sha256="3e0be507486d4e21cf9d08847c7e57dd61a1603950399985f7c5a0be7fd33e36")
 
     depends_on("py-setuptools@30.3.0:", type="build")
-    depends_on("py-build", type="build")
+    depends_on("py-build", type="build", when="@:0.9.12")
