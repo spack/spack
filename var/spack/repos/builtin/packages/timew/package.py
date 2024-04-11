fc994b8b00c10542fa30bcc8da90debab0821338
James Taliaferro <james@taliafer.ro>
2024-04-10 18:16:47 -0700

    fix style checks

--- var/spack/repos/builtin/packages/timew/package.py
+++ var/spack/repos/builtin/packages/timew/package.py
@@ -3,23 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install timew
-#
-# You can edit this file again by typing:
-#
-#     spack edit timew
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
@@ -35,5 +18,3 @@ class Timew(CMakePackage):
     license("MIT", checked_by="taliaferro")
 
     version("1.7.1", sha256="5e0817fbf092beff12598537c894ec1f34b0a21019f5a3001fe4e6d15c11bd94")
-
-    # depends_on("foo")
