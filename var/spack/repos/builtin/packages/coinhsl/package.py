af723fc6df05aed7b426f1d9f0d87f78c5732c29
Andrew Lister <andrew.lister@stfc.ac.uk>
2024-04-11 18:33:18 +0100

    CoinHSL: Support the Meson build system

--- var/spack/repos/builtin/packages/coinhsl/package.py
+++ var/spack/repos/builtin/packages/coinhsl/package.py
@@ -3,12 +3,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-import os
-
 from spack.package import *
 
 
-class Coinhsl(AutotoolsPackage):
+class Coinhsl(MesonPackage, AutotoolsPackage):
     """CoinHSL is a collection of linear algebra libraries (KB22, MA27,
     MA28, MA54, MA57, MA64, MA77, MA86, MA97, MC19, MC34, MC64, MC68,
     MC69, MC78, MC80, OF01, ZB01, ZB11) bundled for use with IPOPT and
@@ -20,61 +18,81 @@ class Coinhsl(AutotoolsPackage):
     the download file. Alternatively, add this file to a mirror so
     that Spack can find it. For instructions on how to set up a
     mirror, see https://spack.readthedocs.io/en/latest/mirrors.html"""
+    build_system(
+        conditional("autotools", when="@:2019.05.21"),
+        conditional("meson", when="@2022.11.09:"),
+        default="meson")
 
-    # NOTE(oxberry1@llnl.gov): an HTTPS version of the URL below does not
-    # exist
     homepage = "https://www.hsl.rl.ac.uk/ipopt/"
-    url = "file://{0}/coinhsl-archive-2014.01.17.tar.gz".format(os.getcwd())
     manual_download = True
 
-    # CoinHSL has a few versions that vary with respect to stability/features
-    # and licensing terms.
+    # Meson builds
+    version("2023.11.17",
+            sha256="43438fb9317dd4648625a6f5dd46ffedf1d33bd47d05885805b651fe93729065")
+    version("2023.11.17-archive",
+            sha256="8bb19f6e4ed6057cea801b116f0b86868a100d378952c827092b9c9dbedfc4d2")
+    version("2023.05.26",
+            sha256="e39faf1edb5ce83ff80e443f6d7ebb322fa6abfc5286cb54d6a6030c4a826306")
+    version("2023.05.26-archive",
+            sha256="6fd92bbbb2eaa22de5bd9944a282e0089f6a90fb10dbc991182a67b07eaafce7")
+    version("2022.12.02-archive",
+            sha256="5ebdbf8a70a9e10863c5809cf53e4e236b07f65ab2e96cc367ba8131dc19e232")
+    version("2022.11.09",
+            sha256="d6d9089bb9cf3eb0e4af195f1a2f10cd61da42eddf8da73a12b8c62902bceee3")
 
-    # Version 2019.05.21 is a full-featured "release candidate"
-    # version available via an "academic license" that can be used for
-    # personal teaching and research purposes only. For a full list of
-    # conditions, see https://www.hsl.rl.ac.uk/academic.html.
-    version(
-        "2019.05.21", sha256="95ce1160f0b013151a3e25d40337775c760a8f3a79d801a1d190598bf4e4c0c3"
-    )
+    with when("build-system=meson"):  # TODO: check for archive flag in version
+        depends_on("blas")
+        depends_on("lapack")
+        variant("metis", default=True, description="Build with Metis support.")
+        depends_on("metis", when="+metis")
 
-    # Version 2015.06.23 is a full-featured "stable"
-    # version available via an "academic license" that can be used for
-    # personal teaching and research purposes only. For a full list of
-    # conditions, see https://www.hsl.rl.ac.uk/academic.html.
-    version(
-        "2015.06.23",
-        sha256="3e955a2072f669b8f357ae746531b37aea921552e415dc219a5dd13577575fb3",
-        preferred=True,
-    )
+    def meson_args(self):
+        spec = self.spec
+        args = []
+        if spec.satisfies("@???"):  # TODO: check for archive flag
+            return []
 
-    # Version 2014.01.17 is a full-featured "stable" version available
-    # via an "academic license" that can be used for personal teaching
-    # and research purposes only.
-    version(
-        "2014.01.17", sha256="ed49fea62692c5d2f928d4007988930da9ff9a2e944e4c559d028671d122437b"
-    )
+        blas = spec["blas"].names[0]
+        blas_paths = [sf[2:] for sf in spec["blas"].search_flags.split()]
+        lapack = spec["lapack"].names[0]
+        lapack_paths = [sf[2:] for sf in spec["lapack"].search_flags.split()]
+        args.append(f"-Dlibblas={blas}")
+        args.extend([f"-Dlibblas_path={p}"
+                     for p in blas_paths
+                     ])
+        args.append(f"-Dliblapack={lapack}")
+        args.extend([f"-Dlibblas_path={p}"
+                     for p in lapack_paths
+                     ])
+        if spec.satisfies("+metis"):
+            metis = spec["metis"]
+            if metis.satisfies("@5"):
+                args.append("-Dlibmetis_version=5")
+            else:
+                args.append("-Dlibmetis_version=4")
+            args.extend([
+                f"-Dlibmetis_include={metis.prefix.include}",
+                f"-Dlibmetis_path={metis.prefix.lib}",
+            ])
 
-    # Version 2014.01.10 only has MA27, MA28, and MC19, and is
-    # available as a "personal license" that is free to all, and
-    # permits commercial use, but *not redistribution* (emphasis from
-    # original source).
-    version(
-        "2014.01.10", sha256="7c2be60a3913b406904c66ee83acdbd0709f229b652c4e39ee5d0876f6b2e907"
-    )
+    # Autotools builds
+    version("2019.05.21",
+            sha256="95ce1160f0b013151a3e25d40337775c760a8f3a79d801a1d190598bf4e4c0c3")
+    version("2015.06.23",
+            sha256="3e955a2072f669b8f357ae746531b37aea921552e415dc219a5dd13577575fb3")
+    version("2014.01.17",
+            sha256="ed49fea62692c5d2f928d4007988930da9ff9a2e944e4c559d028671d122437b")
+    version("2014.01.10",
+            sha256="7c2be60a3913b406904c66ee83acdbd0709f229b652c4e39ee5d0876f6b2e907")
 
-    # CoinHSL fails to build in parallel
-    parallel = False
-
-    variant("blas", default=False, description="Link to external BLAS library")
-
-    depends_on("blas", when="+blas")
+    with when("build-system=autotools"):
+        parallel = False
+        variant("blas", default=False, description="Link to external BLAS library")
+        depends_on("blas", when="+blas")
 
     def configure_args(self):
         spec = self.spec
         args = []
-
         if spec.satisfies("+blas"):
-            args.append("--with-blas={0}".format(spec["blas"].libs.ld_flags))
-
+            args.append(f"--with-blas={spec['blas'].libs.ld_flags}")
         return args
