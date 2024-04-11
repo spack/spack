f6b7ee653e11850bba06ce340b83fdfc1339a10e
Luc Berger <lberge@sandia.gov>
2024-04-11 12:41:28 -0600

    Kokkos Ecosystem: 4.3.00 (#43607)
    * Kokkos Kernels: adding missing TPLs and pre-conditions
      Adding variants and dependencies for rocBLAS and rocSPARSE.
      Also adding a "when=" close to the TPL variants that prevents
      enabling the TPLs in versions of the library when it was not
      yet available.
    * Kokkos Kernels: remove comment for better format
    * Kokkos Kernels: adding cusolver and rocsolver for at version 4.3.00
    * Kokkos Ecosystem: updating packages for release 4.3.00
    * Kokkos: adding arch for SG2042
    * Removing sg2042 from spack_micro_arch_map
      Removing it here and will work to add it in the proper generic spack location, likely:   `spack/lib/spack/external/archspec/json/cpu/microarchitectures.json` ?

--- var/spack/repos/builtin/packages/kokkos-kernels/package.py
+++ var/spack/repos/builtin/packages/kokkos-kernels/package.py
@@ -25,6 +25,7 @@ class KokkosKernels(CMakePackage, CudaPackage):
     # openssl sha256 kokkos-kernels-x.y.z.tar.gz
     version("develop", branch="develop")
     version("master", branch="master")
+    version("4.3.00", sha256="03c3226ee97dbca4fa56fe69bc4eefa0673e23c37f2741943d9362424a63950e")
     version("4.2.01", sha256="058052b3a40f5d4e447b7ded5c480f1b0d4aa78373b0bc7e43804d0447c34ca8")
     version("4.2.00", sha256="c65df9a101dbbef2d8fd43c60c9ea85f2046bb3535fa1ad16e7c661ddd60401e")
     version("4.1.00", sha256="d6a4108444ea226e43bf6a9c0dfc557f223a72b1142bf81aa78dd60e16ac2d56")
@@ -47,6 +48,7 @@ class KokkosKernels(CMakePackage, CudaPackage):
     depends_on("kokkos")
     depends_on("kokkos@master", when="@master")
     depends_on("kokkos@develop", when="@develop")
+    depends_on("kokkos@4.3.00", when="@4.3.00")
     depends_on("kokkos@4.2.01", when="@4.2.01")
     depends_on("kokkos@4.2.00", when="@4.2.00")
     depends_on("kokkos@4.1.00", when="@4.1.00")
@@ -146,6 +148,8 @@ class KokkosKernels(CMakePackage, CudaPackage):
         "lapacke": (False, "clapack", "LAPACKE", "@3.1.00:", "Link to LAPACKE library"),
         "rocblas": (False, "rocblas", "ROCBLAS", "@3.6.00:", "Link to AMD BLAS library"),
         "rocsparse": (False, "rocsparse", "ROCSPARSE", "@3.6.00:", "Link to AMD sparse library"),
+        "cusolver": (False, "cuda", None, "@4.3.00:", "Link to CUDA solver library"),
+        "rocsolver": (False, "rocsolver", "ROCSOLVER", "@4.3.00:", "Link to AMD solver library"),
     }
 
     for tpl in tpls:
