13fc78294d46cca55a7a1bc5eafd3b59b3ff9601
YI Zeping <18586016708@163.com>
2024-04-11 11:04:58 +0800

    Packages: llvm cxx flag remove, new versions, and binutils build issue fix (#43567)
    * add c++11 header to gold for compiler not defaulting to c++11
    
    * glibc: add 2.39
    
    * llvm: add 18.1.3
    
    * fix #42314, remove cxx11 flag for llvm; should be controlled by cmake.
    
    * modify patch
    
    * llvm version
    
    * add gmake version request

--- var/spack/repos/builtin/packages/glibc/package.py
+++ var/spack/repos/builtin/packages/glibc/package.py
@@ -24,6 +24,7 @@ class Glibc(AutotoolsPackage, GNUMirrorPackage):
     license("LGPL-2.1-or-later")
 
     version("master", branch="master")
+    version("2.39", sha256="97f84f3b7588cd54093a6f6389b0c1a81e70d99708d74963a2e3eab7c7dc942d")
     version("2.38", sha256="16e51e0455e288f03380b436e41d5927c60945abd86d0c9852b84be57dd6ed5e")
     version("2.37", sha256="e3a790c2f84eed5c5d569ed6172c253c607dd3962135437da413aa39aa4fd352")
     version("2.36", sha256="02efa6ffbbaf3e10e88f16818a862608d04b0ef838c66f6025ae120530792c9c")
@@ -165,6 +166,8 @@ class Glibc(AutotoolsPackage, GNUMirrorPackage):
 
     # See 2d7ed98add14f75041499ac189696c9bd3d757fe
     depends_on("gmake@:4.3", type="build", when="@:2.36")
+    # Since f2873d2da0ac9802e0b570e8e0b9e7e04a82bf55
+    depends_on("gmake@4.0:", type="build", when="@2.28:")
 
     # From 2.29: generates locale/C-translit.h
     # before that it's a test dependency.
