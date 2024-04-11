0aa6aa1f6599b3fb8cd310ab8ce80cca5ac38eef
Wouter Deconinck <wdconinc@gmail.com>
2024-04-11 11:10:04 -0500

    cairo: add 1.17.6 for continuity; doc lines

--- var/spack/repos/builtin/packages/cairo/package.py
+++ var/spack/repos/builtin/packages/cairo/package.py
@@ -16,6 +16,7 @@ class Cairo(MesonPackage, AutotoolsPackage):
 
     license("LGPL-2.1-or-later OR MPL-1.1", checked_by="tgamblin")
 
+    # Cairo has meson.build @1.17.4:, but we only support @1.17.8: here
     build_system(
         conditional("autotools", when="@:1.17.6"),
         conditional("meson", when="@1.17.8:"),
@@ -23,16 +24,22 @@ class Cairo(MesonPackage, AutotoolsPackage):
     )
 
     version("1.18.0", sha256="243a0736b978a33dee29f9cca7521733b78a65b5418206fef7bd1c3d4cf10b64")
+    # 1.17.8: https://gitlab.freedesktop.org/cairo/cairo/-/issues/646 (we enable tee by default)
+    version(
+        "1.17.6",
+        sha256="4eebc4c2bad0402bc3f501db184417094657d111fb6c06f076a82ea191fe1faf",
+        url="https://cairographics.org/snapshots/cairo-1.17.6.tar.xz",
+    )
     version(
         "1.17.4",
         sha256="74b24c1ed436bbe87499179a3b27c43f4143b8676d8ad237a6fa787401959705",
         url="https://cairographics.org/snapshots/cairo-1.17.4.tar.xz",
-    )  # Snapshot
+    )
     version(
         "1.17.2",
         sha256="6b70d4655e2a47a22b101c666f4b29ba746eda4aa8a0f7255b32b2e9408801df",
         url="https://cairographics.org/snapshots/cairo-1.17.2.tar.xz",
-    )  # Snapshot
+    )
     version("1.16.0", sha256="5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331")
     version("1.14.12", sha256="8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16")
     version("1.14.8", sha256="d1f2d98ae9a4111564f6de4e013d639cf77155baf2556582295a0f00a9bc5e20")
@@ -79,7 +86,7 @@ class Cairo(MesonPackage, AutotoolsPackage):
 
     # patch from https://gitlab.freedesktop.org/cairo/cairo/issues/346
     patch("fontconfig.patch", when="@1.16.0:1.17.2")
-    # Don't regenerate docs to avoid a dependency on gtk-doc
+    # Patch autogen.sh to not regenerate docs to avoid a dependency on gtk-doc
     patch("disable-gtk-docs.patch", when="build_system=autotools ^autoconf@2.70:")
 
     def check(self):
