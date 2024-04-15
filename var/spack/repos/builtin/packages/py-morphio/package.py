500d3d6e1bcd6b3b59c1bcf24021607706e6012a
Tristan Carel <tristan.carel@epfl.ch>
2024-04-15 22:06:48 +0200

    py-morphio: add version 3.3.7, update license (#43420)

--- var/spack/repos/builtin/packages/py-morphio/package.py
+++ var/spack/repos/builtin/packages/py-morphio/package.py
@@ -15,10 +15,14 @@ class PyMorphio(PythonPackage):
     git = "https://github.com/BlueBrain/MorphIO.git"
     pypi = "morphio/MorphIO-3.3.2.tar.gz"
 
-    license("LGPL-3.0-or-later")
+    license("LGPL-3.0-or-later", when="@:3.3.6")
+    license("Apache-2.0", when="@3.3.7:")
+
+    maintainers("tristan0x")
 
     version("master", branch="master", submodules=True)
 
+    version("3.3.7", sha256="7cb6676fcbaa2ac154742ca8aed3fcb03bb4643852e36e368858580133b61732")
     version("3.3.6", sha256="0f2e55470d92a3d89f2141ae905ee104fd16257b93dafb90682d90171de2f4e6")
 
     depends_on("py-setuptools@24.2:", type="build")
