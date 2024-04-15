3c0b6880fb7f7562334f226f1113b58a443cec55
James Taliaferro <james@taliafer.ro>
2024-04-15 11:20:36 -0700

    remove FIXMEs

--- var/spack/repos/builtin/packages/editorconfig/package.py
+++ var/spack/repos/builtin/packages/editorconfig/package.py
@@ -13,18 +13,9 @@ class Editorconfig(CMakePackage):
     homepage = "https://editorconfig.org/"
     url = "https://github.com/editorconfig/editorconfig-core-c/archive/refs/tags/v0.12.7.tar.gz"
 
-    # FIXME: Add the SPDX identifier of the project's license below.
-    # See https://spdx.org/licenses/ for a list. Upon manually verifying
-    # the license, set checked_by to your Github username.
     license("BSD-2-Clause", checked_by="taliaferro")
 
     version("0.12.7", sha256="f89d2e144fd67bdf0d7acfb2ac7618c6f087e1b3f2c3a707656b4180df422195")
 
     depends_on("pcre2")
 
-    # def cmake_args(self):
-    #     # FIXME: Add arguments other than
-    #     # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-    #     # FIXME: If not needed delete this function
-    #     args = []
-    #     return args
