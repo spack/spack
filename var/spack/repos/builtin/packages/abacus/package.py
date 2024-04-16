ee81248eabb63b623ce920d13dd620c2ed1dc739
yizeyi18 <18586016708@163.com>
2024-04-16 15:15:07 +0800

    new version

--- var/spack/repos/builtin/packages/abacus/package.py
+++ var/spack/repos/builtin/packages/abacus/package.py
@@ -27,6 +27,7 @@ class Abacus(MakefilePackage, CMakePackage, CudaPackage, ROCmPackage):
     license("LGPL-3.0-or-later")
 
     version("develop", branch="develop")
+    version("3.6.1", sha256="ef460417a153216b53e2c45f0bb559cd80ba311ad1b9df3aea6ac7cc30d5f458")
     version("3.6.0", sha256="9efdfe91922e37526745df2ea3fa455ff062d3ad10092459faaeb038197bc710")
     version("3.5.4", sha256="ab7cbdd07a951da116cc4fe4dfa23e7ac41dda9f35c35d16c267920f267f4722")
     version("3.5.3", sha256="f56066473dbd562f170f40809738076c0862321499ae7fcbd71508581f9ba7bf")
