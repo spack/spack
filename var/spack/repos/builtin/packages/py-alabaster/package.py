3b9a18cf5d6a32739541f79d43040c5b9b811b5a
Adam J. Stewart <ajstewart426@gmail.com>
2024-04-17 11:46:12 +0200

    Fix syntax error

--- var/spack/repos/builtin/packages/py-alabaster/package.py
+++ var/spack/repos/builtin/packages/py-alabaster/package.py
@@ -20,6 +20,6 @@ class PyAlabaster(PythonPackage):
     version("0.7.10", sha256="37cdcb9e9954ed60912ebc1ca12a9d12178c26637abdf124e3cde2341c257fe0")
     version("0.7.9", sha256="47afd43b08a4ecaa45e3496e139a193ce364571e7e10c6a87ca1a4c57eb7ea08")
 
-    depends_on("python@3.9:", when="@0.7.16:", type=("build", "run")
+    depends_on("python@3.9:", when="@0.7.16:", type=("build", "run"))
     depends_on("py-flit-core@3.7:", when="@0.7.16:", type="build")
     depends_on("py-setuptools", when="@:0.7.13", type="build")
