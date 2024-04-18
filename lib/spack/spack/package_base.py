ed6a83f12ef86f64828f6e289ee6edf6556d6f8f
Gregory Becker <becker33@llnl.gov>
2024-04-18 12:03:21 -0700

    remove vestigial line change

--- lib/spack/spack/package_base.py
+++ lib/spack/spack/package_base.py
@@ -249,7 +249,6 @@ class DetectablePackageMeta(type):
                 specs = []
                 for version_str, objs in objs_by_version.items():
                     variants = cls.determine_variants(objs, version_str)
-
                     # Normalize output to list
                     if not isinstance(variants, list):
                         variants = [variants]
