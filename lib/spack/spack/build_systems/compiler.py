abcd1a411261b371c54245b88415d2088dd34309
Gregory Becker <becker33@llnl.gov>
2024-04-22 15:16:39 -0700

    CompilerPackage: type hints and better names

--- lib/spack/spack/build_systems/compiler.py
+++ lib/spack/spack/build_systems/compiler.py
@@ -4,9 +4,10 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 import itertools
 import os
+import pathlib
 import re
 import sys
-from typing import Sequence, Tuple, Union
+from typing import Dict, List, Sequence, Tuple, Union
 
 import llnl.util.tty as tty
 from llnl.util.lang import classproperty
@@ -14,42 +15,47 @@ from llnl.util.lang import classproperty
 import spack.compiler
 import spack.package_base
 
+# Local "type" for type hints
+Path = Union[str, pathlib.Path]
+
 
 class CompilerPackage(spack.package_base.PackageBase):
     """A Package mixin for all common logic for packages that implement compilers"""
 
     # TODO: how do these play nicely with other tags
-    tags = ["compiler"]
+    tags: Sequence[str] = ["compiler"]
 
     #: Optional suffix regexes for searching for this type of compiler.
     #: Suffixes are used by some frameworks, e.g. macports uses an '-mp-X.Y'
     #: version suffix for gcc.
-    suffixes: Sequence[str] = [r"-.*"]
+    compiler_suffixes: List[str] = [r"-.*"]
 
     #: Optional prefix regexes for searching for this compiler
-    prefixes: Sequence[str] = []
+    compiler_prefixes: List[str] = []
 
     #: Compiler argument(s) that produces version information
     #: If multiple arguments, the earlier arguments must produce errors when invalid
-    version_argument: Union[str, Tuple[str]] = "-dumpversion"
+    compiler_version_argument: Union[str, Tuple[str]] = "-dumpversion"
 
     #: Regex used to extract version from compiler's output
-    version_regex: str = "(.*)"
+    compiler_version_regex: str = "(.*)"
 
     #: Static definition of languages supported by this class
     compiler_languages: Sequence[str] = ["c", "cxx", "fortran"]
 
-    def __init__(self, spec):
+    def __init__(self, spec: "spack.spec.Spec"):
         super().__init__(spec)
-        assert set(self.supported_languages) <= set(self.compiler_languages)
+        msg = f"Supported languages for {spec} are not a subset of possible supported languages"
+        msg += f"    supports: {self.supported_languages}, valid values: {self.compiler_languages}"
+        assert set(self.supported_languages) <= set(self.compiler_languages), msg
 
     @property
-    def supported_languages(self):
+    def supported_languages(self) -> Sequence[str]:
         """Dynamic definition of languages supported by this package"""
         return self.compiler_languages
 
     @classproperty
-    def compiler_names(cls):
+    def compiler_names(cls) -> Sequence[str]:
         """Construct list of compiler names from per-language names"""
         names = []
         for language in cls.compiler_languages:
@@ -57,11 +63,11 @@ class CompilerPackage(spack.package_base.PackageBase):
         return names
 
     @classproperty
-    def executables(cls):
+    def executables(cls) -> Sequence[str]:
         """Construct executables for external detection from names, prefixes, and suffixes."""
         regexp_fmt = r"^({0}){1}({2})$"
-        prefixes = [""] + cls.prefixes
-        suffixes = [""] + cls.suffixes
+        prefixes = [""] + cls.compiler_prefixes
+        suffixes = [""] + cls.compiler_suffixes
         if sys.platform == "win32":
             ext = r"\.(?:exe|bat)"
             suffixes += [suf + ext for suf in suffixes]
@@ -71,15 +77,15 @@ class CompilerPackage(spack.package_base.PackageBase):
         ]
 
     @classmethod
-    def determine_version(cls, exe):
-        version_argument = cls.version_argument
+    def determine_version(cls, exe: Path):
+        version_argument = cls.compiler_version_argument
         if isinstance(version_argument, str):
-            version_argument = [version_argument]
+            version_argument = (version_argument,)
 
         for va in version_argument:
             try:
                 output = spack.compiler.get_compiler_version_output(exe, va)
-                match = re.search(cls.version_regex, output)
+                match = re.search(cls.compiler_version_regex, output)
                 if match:
                     return ".".join(match.groups())
             except spack.util.executable.ProcessError:
@@ -88,12 +94,12 @@ class CompilerPackage(spack.package_base.PackageBase):
                 tty.debug(e)
 
     @classmethod
-    def compiler_bindir(cls, prefix):
+    def compiler_bindir(cls, prefix: Path) -> Path:
         """Overridable method for the location of the compiler bindir within the preifx"""
         return os.path.join(prefix, "bin")
 
     @classmethod
-    def determine_compiler_exes(cls, prefix):
+    def determine_compiler_exes(cls, prefix: Path) -> Sequence[Path]:
         """Compute the executables in the compiler prefix that may be compiler executables"""
         exes = []
         bindir = cls.compiler_bindir(prefix)
@@ -103,7 +109,7 @@ class CompilerPackage(spack.package_base.PackageBase):
         return exes
 
     @classmethod
-    def determine_compiler_paths(cls, exes):
+    def determine_compiler_paths(cls, exes: Sequence[Path]) -> Dict[str, Path]:
         """Compute the paths to compiler executables associated with this package
 
         This is a helper method for ``determine_variants`` to compute the ``extra_attributes``
@@ -140,6 +146,6 @@ class CompilerPackage(spack.package_base.PackageBase):
         return paths
 
     @classmethod
-    def determine_variants(cls, exes, version_str):
+    def determine_variants(cls, exes: Sequence[Path], version_str: str) -> Tuple:
         # path determination is separated so it can be reused in subclasses
         return "", {"compilers": cls.determine_compiler_paths(exes=exes)}
