7d64c5389d96599151d6d05a32d749189a63b68a
John Parent <john.parent@kitware.com>
2024-04-23 16:02:32 -0400

    Mock externals for Windows runs

--- lib/spack/spack/bootstrap/core.py
+++ lib/spack/spack/bootstrap/core.py
@@ -562,9 +562,9 @@ def ensure_winsdk_external_or_raise() -> None:
             missing_packages_lst.append("win-sdk")
         missing_packages = " & ".join(missing_packages_lst)
         raise RuntimeError(
-            f"Unable to find the {missing_packages}, please install these packages\
-via the Visual Studio installer\
-before proceeding with Spack or provide the path to a non standard install via\
+            f"Unable to find the {missing_packages}, please install these packages \
+via the Visual Studio installer \
+before proceeding with Spack or provide the path to a non standard install with \
 'spack external find --path'"
         )
     # wgl/sdk are not required for bootstrapping Spack, but
