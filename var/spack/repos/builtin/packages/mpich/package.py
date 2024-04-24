4d0d6459feb87a00468a63860ab5e5be219dc67c
Ken Raffenetti <raffenet@users.noreply.github.com>
2024-04-24 14:33:59 -0500

    mpich: add 4.2.0 release (#42687)

--- var/spack/repos/builtin/packages/mpich/package.py
+++ var/spack/repos/builtin/packages/mpich/package.py
@@ -31,6 +31,7 @@ class Mpich(AutotoolsPackage, CudaPackage, ROCmPackage):
 
     version("develop", submodules=True)
     version("4.2.1", sha256="23331b2299f287c3419727edc2df8922d7e7abbb9fd0ac74e03b9966f9ad42d7")
+    version("4.2.0", sha256="a64a66781b9e5312ad052d32689e23252f745b27ee8818ac2ac0c8209bc0b90e")
     version("4.1.2", sha256="3492e98adab62b597ef0d292fb2459b6123bc80070a8aa0a30be6962075a12f0")
     version("4.1.1", sha256="ee30471b35ef87f4c88f871a5e2ad3811cd9c4df32fd4f138443072ff4284ca2")
     version("4.1", sha256="8b1ec63bc44c7caa2afbb457bc5b3cd4a70dbe46baba700123d67c48dc5ab6a0")
