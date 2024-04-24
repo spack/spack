ae1ef1fa99c53434f0d27ca2468a2ed7b47435b7
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-04-24 13:11:57 -0600

    Add in other versions

--- var/spack/repos/builtin/packages/rosco/package.py
+++ var/spack/repos/builtin/packages/rosco/package.py
@@ -14,19 +14,28 @@ class Rosco(CMakePackage):
     """
 
     homepage = "https://rosco.readthedocs.io/en/latest/"
-    url = "https://github.com/NREL/ROSCO/archive/refs/tags/v.2.9.1.tar.gz"
+    url = "https://github.com/NREL/ROSCO/archive/refs/tags/v2.9.0.tar.gz"
     git = "https://github.com/NREL/ROSCO.git"
 
     maintainers("dzalkind", "ndevelder")
 
     version("develop", branch="develop")
     version("main", branch="main")
-    version("2.9.1", sha256="c226f802fd641910e8f00d9e479cdb9ea5988a8ddb9c86c455c0ee952a842b36")
+    version("2.9.0", sha256="eb7f6220207b8a07c9570fb64bab591906b0c19d73ac4c24bb8dca252722ca79")
+    version("2.8.0", sha256="7a2e3a7bebdf6ee73884a9e3502f904cc3a3f1aa1bf672c223ecbaa041bfc48f")
+    version("2.7.0", sha256="b6a2cda92680cf6a460d194901a2f16c2635710a82788576a6383a3bb189fc83")
+    version("2.6.0", sha256="ca1c1a6ac53e8220b107accccfb8b5299772c38b7c77cd8d2ba383e9825daeaa")
+    version("2.5.1", sha256="55fe7bba612321baa6e089ee1156ef4db2e3bccf1b69534829b06f3367fff05d")
 
     variant("shared", default=False, description="Build shared libraries")
     variant("pic", default=False, description="Position independent code")
 
-    root_cmakelists_dir = "rosco/controller"
+    @property
+    def root_cmakelists_dir(self):
+        if self.spec.version >= Version("2.9.0"):
+            return "rosco/controller"
+        else:
+            return "ROSCO"
 
     def cmake_args(self):
         options = []
