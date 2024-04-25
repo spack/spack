dd12884a7700a0fd0c2dc11e90eaa97685747226
Matthias Wolf <matthias.wolf@epfl.ch>
2024-04-25 11:36:39 +0200

    py-fz-td-recipe: bump, switch to PyPI (#2414)

--- bluebrain/repo-bluebrain/packages/py-fz-td-recipe/package.py
+++ bluebrain/repo-bluebrain/packages/py-fz-td-recipe/package.py
@@ -11,21 +11,20 @@ class PyFzTdRecipe(PythonPackage):
     building."""
 
     homepage = "https://github.com/BlueBrain/fz-td-recipe"
-    git = "ssh://git@github.com/BlueBrain/fz-td-recipe.git"
+    git = "https://github.com/BlueBrain/fz-td-recipe.git"
+    pypi = "fz-td-recipe/fz_td_recipe-0.2.2.tar.gz"
 
     version("develop", branch="main")
-    version("0.2.0", tag="fz-td-recipe-v0.2.0")
+    version("0.2.2", sha256="34eded0440c065c5a3fabe43ed0c736a6b4174b8f6ac24a4275818954d6cabda")
     version("0.1.2", tag="fz-td-recipe-v0.1.2")
-    version("0.1.1", tag="fz-td-recipe-v0.1.1")
-    version("0.1.0", tag="fz-td-recipe-v0.1.0")
 
-    depends_on("py-setuptools", type="build", when="@:0.2")
-    depends_on("py-hatchling", type="build", when="@0.2.1:")
-    depends_on("py-hatch-vcs", type="build", when="@0.2.1:")
+    depends_on("py-setuptools", type="build", when="@0.1.2")
+    depends_on("py-hatchling", type="build", when="@0.2.2:")
+    depends_on("py-hatch-vcs", type="build", when="@0.2.2:")
 
     depends_on("py-click", type=("build", "run"), when="@0.2:")
     depends_on("py-jsonschema", type=("build", "run"), when="@0.2:")
     depends_on("py-lxml@:4", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
-    depends_on("py-pandas", type=("build", "run"), when="@0.1.2:")
-    depends_on("py-pyyaml", type=("build", "run"), when="@0.2:")
+    depends_on("py-pandas", type=("build", "run"))
+    depends_on("py-pyyaml", type=("build", "run"), when="@0.2.2:")
