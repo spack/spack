ec715b0d2510c4aeb61e4e59558a39c78862495c
Garth N. Wells <gnw20@cam.ac.uk>
2024-04-25 17:11:07 +0100

    Dependency fix

--- var/spack/repos/builtin/packages/py-fenics-ufl/package.py
+++ var/spack/repos/builtin/packages/py-fenics-ufl/package.py
@@ -21,7 +21,7 @@ class PyFenicsUfl(PythonPackage):
     license("LGPL-3.0-or-later")
 
     version("main", branch="main")
-    version("2024.1.0.post0", sha256="f078594a6406a9dd4e5eefbc73fe0c168b331a566a75b06c74a5fd6169121e5b")
+    version("2024.1.0.post1", sha256="6e38e93a2c8417271c9fb316e0d0ea5fe1101c6a37b2496fff8290e7ea7ead74")
     version("2023.2.0", sha256="d1d3209e8ebd4bd70513c26890f51823bac90edc956233c47bd8e686e064436e")
     version(
         "2023.1.1.post0", sha256="9e6e87f1447635029cec42604f62a76bba84899beb4b8822af10389d1f93a9b6"
@@ -42,7 +42,6 @@ class PyFenicsUfl(PythonPackage):
 
     depends_on("python@3.8:", when="@2023.2.0:", type=("build", "run"))
 
-    depends_on("py-pip@22.3:", when="@2024.1.0:", type="build")
     depends_on("py-setuptools@62:", when="@2023.2.0:", type="build")
     depends_on("py-setuptools@58:", when="@2022.1.0:2023.1.1.post0", type="build")
     depends_on("py-setuptools@40:", when="@2016.2.0:2021.1.0", type="build")
