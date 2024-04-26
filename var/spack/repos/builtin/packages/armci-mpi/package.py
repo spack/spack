f3b3e20995f2b271b32707dcfec4ba6888c922bc
Jeff Hammond <jeff.science@gmail.com>
2024-04-26 15:11:02 +0300

    style
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- var/spack/repos/builtin/packages/armci-mpi/package.py
+++ var/spack/repos/builtin/packages/armci-mpi/package.py
@@ -3,25 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install armci-mpi
-#
-# You can edit this file again by typing:
-#
-#     spack edit armci-mpi
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
-
 
 class ArmciMpi(AutotoolsPackage):
     """ARMCI-MPI is an implementation of the ARMCI library used by Global Arrays.
@@ -35,9 +17,7 @@ class ArmciMpi(AutotoolsPackage):
 
     license("BSD-3-Clause", checked_by="jeffhammond")
 
-    version(
-        "0.4", sha256="bcc3bb189b23bf653dcc69bc469eb86eae5ebc5ad94ab5f83e52ddbdbbebf1b1"
-    )
+    version("0.4", sha256="bcc3bb189b23bf653dcc69bc469eb86eae5ebc5ad94ab5f83e52ddbdbbebf1b1")
     version(
         "0.3.1-beta", sha256="f3eaa8f365fb55123ecd9ced401086b0732e37e4df592b27916d71a67ab34fe9"
     )
