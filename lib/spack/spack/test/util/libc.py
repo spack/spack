47d0c50c2a33bfe903d787e9551f9656311945f0
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-04-26 18:09:22 +0200

    Fix unit tests

--- lib/spack/spack/test/util/libc.py
+++ lib/spack/spack/test/util/libc.py
@@ -8,19 +8,19 @@ from spack.util import libc
 
 
 @pytest.mark.parametrize(
-    "startfile_prefix,expected",
+    "libc_prefix,startfile_prefix,expected",
     [
         # Ubuntu
-        ("/usr/lib/x86_64-linux-gnu", "/usr/include/x86_64-linux-gnu"),
-        ("/usr/lib/x86_64-linux-musl", "/usr/include/x86_64-linux-musl"),
-        ("/usr/lib/aarch64-linux-gnu", "/usr/include/aarch64-linux-gnu"),
-        ("/usr/lib/aarch64-linux-musl", "/usr/include/aarch64-linux-musl"),
+        ("/usr", "/usr/lib/x86_64-linux-gnu", "/usr/include/x86_64-linux-gnu"),
+        ("/usr", "/usr/lib/x86_64-linux-musl", "/usr/include/x86_64-linux-musl"),
+        ("/usr", "/usr/lib/aarch64-linux-gnu", "/usr/include/aarch64-linux-gnu"),
+        ("/usr", "/usr/lib/aarch64-linux-musl", "/usr/include/aarch64-linux-musl"),
         # rhel-like
-        ("/usr/lib64", "/usr/include"),
-        ("/usr/lib", "/usr/include"),
+        ("/usr", "/usr/lib64", "/usr/include"),
+        ("/usr", "/usr/lib", "/usr/include"),
     ],
 )
 @pytest.mark.not_on_windows("The unit test deals with unix-like paths")
-def test_header_dir_computation(startfile_prefix, expected):
+def test_header_dir_computation(libc_prefix, startfile_prefix, expected):
     """Tests that we compute the correct header directory from the prefix of the libc startfiles"""
-    assert libc.libc_include_dir_from_startfile_prefix(startfile_prefix) == expected
+    assert libc.libc_include_dir_from_startfile_prefix(libc_prefix, startfile_prefix) == expected
