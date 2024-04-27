26a2332ff625051df9e5aa38d104db31ed3630cb
Jeff Hammond <jeff.science@gmail.com>
2024-04-27 11:41:07 +0300

    check rejects option that dozens of packages use
    i do not have time to fight with this nonsense
    
    Run . share/spack/setup-env.sh
    ==> Error: armcimpi version 'master' has extra arguments: 'branch'
    Valid arguments for a url fetcher are:
        'url', 'sha256', 'md5', 'sha1', 'sha224', 'sha384', 'sha512', and 'checksum'
    Error: Process completed with exit code 1.
    
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- var/spack/repos/builtin/packages/armcimpi/package.py
+++ var/spack/repos/builtin/packages/armcimpi/package.py
@@ -18,8 +18,6 @@ class Armcimpi(AutotoolsPackage):
 
     license("BSD-3-Clause", checked_by="jeffhammond")
 
-    version("master", branch="master")
-
     version("0.4", sha256="bcc3bb189b23bf653dcc69bc469eb86eae5ebc5ad94ab5f83e52ddbdbbebf1b1")
     version(
         "0.3.1-beta", sha256="f3eaa8f365fb55123ecd9ced401086b0732e37e4df592b27916d71a67ab34fe9"
@@ -48,4 +46,3 @@ class Armcimpi(AutotoolsPackage):
 
     def setup_dependent_build_environment(self, env, dependent_spec):
         env.set("ARMCIMPI_DIR", self.prefix)
-
