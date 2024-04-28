ffe53321aa9dc30853cc5d638e1e1bd1521ecb34
Thomas Bouvier <contact@thomas-bouvier.io>
2024-04-28 18:54:13 +0200

    py-wandb: add version v0.16.6

--- var/spack/repos/builtin/packages/py-wandb/package.py
+++ var/spack/repos/builtin/packages/py-wandb/package.py
@@ -17,6 +17,7 @@ class PyWandb(PythonPackage):
 
     license("MIT")
 
+    version("0.16.6", sha256="86f491e3012d715e0d7d7421a4d6de41abef643b7403046261f962f3e512fe1c")
     version("0.13.9", sha256="0a17365ce1f18306ce7a7f16b943094fac7284bb85f4e52c0685705602f9e307")
 
     depends_on("py-setuptools", type=("build", "run"))
@@ -29,9 +30,11 @@ class PyWandb(PythonPackage):
     depends_on("py-typing-extensions", type=("build", "run"), when="^python@:3.9")
 
     depends_on("py-pyyaml", type=("build", "run"))
-    depends_on("py-click@7:", type=("build", "run"))
+    depends_on("py-click@7:", type=("build", "run"), when="@0.13")
+    depends_on("py-click@7.1:", type=("build", "rim"), when="@0.16:")
     conflicts("^py-click@8.0.0")
     depends_on("py-gitpython@1:", type=("build", "run"))
+    conflicts("^py-gitpython@3.1.29")
     depends_on("py-requests@2", type=("build", "run"))
     depends_on("py-psutil@5:", type=("build", "run"))
     depends_on("py-sentry-sdk@1.0.0:", type=("build", "run"))
