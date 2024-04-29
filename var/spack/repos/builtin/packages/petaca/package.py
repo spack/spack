e33ba24ce7148cfe28ca1e3313c024121d39a51f
Zach Jibben <zjibben@lanl.gov>
2024-04-29 15:42:11 -0600

    Update Petaca (#43849)
    * Add recent petaca releases
    * Add Petaca 24.04

--- var/spack/repos/builtin/packages/petaca/package.py
+++ var/spack/repos/builtin/packages/petaca/package.py
@@ -28,6 +28,10 @@ class Petaca(CMakePackage):
     license("MIT")
 
     version("develop", branch="master")
+    version("24.04", sha256="951a0ba3380502fa09009af8613471256bbcb4edc14fe810f7e3afe7adc21b7e")
+    version("23.12", sha256="8d7f1c7170500db52a624c891fb1563b8cfc322d138d935a5b4c6f12d9bdbd23")
+    version("23.11.1", sha256="5c0809965046be5391b476edf1bc0c80584c3bc4fc5ecbf915a2344c6c5b9914")
+    version("23.11", sha256="2230ea8e90ee00bd571c557be4f208077e1606310bef2e43a0257102cc875c45")
     version("22.03", sha256="e6559e928c7cca6017ef0582c204eee775f6bb3f927f1c224c515c2ad574cc32")
     version("21.03", commit="f17df95193ca1a3879687a59a91a123be25e3efa")
 
