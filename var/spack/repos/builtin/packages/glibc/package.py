95f4ca83504f4c1407bbce2252fe65e153f1e058
Harmen Stoppels <me@harmenstoppels.nl>
2024-04-29 13:43:17 +0200

    glibc: provides iconv
    `iconv` is a bit of weird virtual because the only shared API between
    `glibc` and `libiconv` is:
    
    ```
    iconv
    iconv_open
    iconv_close
    ```
    
    whereas `libiconv` has further symbols and an executable.
    
    Hopefully this commit does not break concretization when a package
    actually needs `libiconv` functionality

--- var/spack/repos/builtin/packages/glibc/package.py
+++ var/spack/repos/builtin/packages/glibc/package.py
@@ -29,6 +29,7 @@ class Glibc(AutotoolsPackage, GNUMirrorPackage):
     license("LGPL-2.1-or-later")
 
     provides("libc")
+    provides("iconv")
 
     version("master", branch="master")
     version("2.39", sha256="97f84f3b7588cd54093a6f6389b0c1a81e70d99708d74963a2e3eab7c7dc942d")
