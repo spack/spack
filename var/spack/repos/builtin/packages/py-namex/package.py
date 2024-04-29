6b7bb9c27ca26bcfb292ac723b1c0b4fe5db285b
Adam J. Stewart <ajstewart426@gmail.com>
2024-04-29 22:30:52 +0200

    py-keras: add v3.3.3 (#43885)

--- var/spack/repos/builtin/packages/py-namex/package.py
+++ var/spack/repos/builtin/packages/py-namex/package.py
@@ -14,6 +14,7 @@ class PyNamex(PythonPackage):
 
     license("Apache-2.0")
 
+    version("0.0.8", sha256="32a50f6c565c0bb10aa76298c959507abdc0e850efe085dc38f3440fcb3aa90b")
     version("0.0.7", sha256="84ba65bc4d22bd909e3d26bf2ffb4b9529b608cb3f9a4336f776b04204ced69b")
 
     depends_on("py-setuptools", type="build")
