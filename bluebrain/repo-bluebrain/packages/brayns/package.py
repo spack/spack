708ee1917444c4698a448cf733132ca3c1b405ab
Adrien4193 <39053578+Adrien4193@users.noreply.github.com>
2024-04-30 16:01:25 +0200

    brayns,py-brayns: Brayns 3.7.2. (#2422)

--- bluebrain/repo-bluebrain/packages/brayns/package.py
+++ bluebrain/repo-bluebrain/packages/brayns/package.py
@@ -15,8 +15,8 @@ class Brayns(CMakePackage):
     submodules = False
 
     version("develop", branch="develop")
+    version("3.7.2", tag="3.7.2")
     version("3.7.1", tag="3.7.1")
-    version("3.6.0", tag="3.6.0")
 
     depends_on("cmake@3.15:", type="build")
     depends_on("ispc@1.18.0:", type="build")
