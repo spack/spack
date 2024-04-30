dac306d48e1425b68f83ef65846f06d3f5918d74
Alex Richert <alexander.richert@noaa.gov>
2024-04-29 17:10:47 -0700

    grads: fix libpng and g2c deps

--- var/spack/repos/builtin/packages/grads/package.py
+++ var/spack/repos/builtin/packages/grads/package.py
@@ -63,10 +63,13 @@ class Grads(AutotoolsPackage):
             return url.format(version.up_to(2), version)
 
     # Name of grib2 C library has changed in recent versions
-    with when("+grib2"):
-
-        def patch(self):
+    def patch(self):
+        if self.spec.satisfies("@:2.2.2"):
+            filter_file("png15", "png", "configure")
+        
+        if self.spec.satisfies("+grib2"):
             filter_file("grib2c", "g2c", "configure")
+            filter_file("G2_VERSION", "G2C_VERSION", "src/gacfg.c")
 
     def setup_build_environment(self, env):
         env.set("SUPPLIBS", "/")
