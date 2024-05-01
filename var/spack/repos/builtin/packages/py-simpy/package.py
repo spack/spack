4aa897cff9d11695db20bb4aa01fe6fac6bb9ffe
Alex Leute <acl2809@rit.edu>
2024-05-01 11:52:56 -0400

    py-simpy: Fix hash

--- var/spack/repos/builtin/packages/py-simpy/package.py
+++ var/spack/repos/builtin/packages/py-simpy/package.py
@@ -12,7 +12,7 @@ class PySimpy(PythonPackage):
     homepage = "https://simpy.readthedocs.io/"
     pypi = "simpy/simpy-4.0.2.tar.gz"
 
-    version("4.1.1", sha256="6d8adc0229df6b02fb7e26dcd1338703b4f4f63f167a5ac2a7213cb80aba4484")
+    version("4.1.1", sha256="06d0750a7884b11e0e8e20ce0bc7c6d4ed5f1743d456695340d13fdff95001a6")
     version("4.0.2", sha256="6d8adc0229df6b02fb7e26dcd1338703b4f4f63f167a5ac2a7213cb80aba4484")
 
     depends_on("py-setuptools@42:", type="build", when="@4.0.2")
