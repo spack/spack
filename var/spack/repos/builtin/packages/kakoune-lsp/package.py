3aaf4a04b9a69a0e6b72b7e251b44823946e12ce
James Taliaferro <james@taliafer.ro>
2024-05-03 15:43:32 -0700

    add myself as a maintainer

--- var/spack/repos/builtin/packages/kakoune-lsp/package.py
+++ var/spack/repos/builtin/packages/kakoune-lsp/package.py
@@ -12,6 +12,8 @@ class KakouneLsp(CargoPackage):
     homepage = "https://github.com/kakoune-lsp/kakoune-lsp"
     url = "https://github.com/kakoune-lsp/kakoune-lsp/archive/refs/tags/v17.0.1.tar.gz"
 
+    maintainers("taliaferro")
+
     license("UNLICENSE", checked_by="taliaferro")
 
     version("17.0.1", sha256="c32172a7d13621d7f7fd8b32b819865fd58a38c0c431d3cedd6046fb6de42f44")
