e1a893dba4d3beda4a77371ed49668ba1dba3976
James Taliaferro <james@taliafer.ro>
2024-05-03 15:39:33 -0700

    new package: kakoune-lsp

---
+++ var/spack/repos/builtin/packages/kakoune-lsp/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class KakouneLsp(CargoPackage):
+    """Kakoune Language Server Protocol Client"""
+
+    homepage = "https://github.com/kakoune-lsp/kakoune-lsp"
+    url = "https://github.com/kakoune-lsp/kakoune-lsp/archive/refs/tags/v17.0.1.tar.gz"
+
+    license("UNLICENSE", checked_by="taliaferro")
+
+    version("17.0.1", sha256="c32172a7d13621d7f7fd8b32b819865fd58a38c0c431d3cedd6046fb6de42f44")
+
+    # FIXME: Add dependencies if required.
+    # depends_on("foo")
+
+
