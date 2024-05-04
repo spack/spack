9365420571193ef4b8ce2c6a33629a55e5d07170
jmcarcell <jmcarcell@users.noreply.github.com>
2024-05-04 22:36:51 +0200

    Remove urls

--- var/spack/repos/builtin/packages/madgraph5amc/package.py
+++ var/spack/repos/builtin/packages/madgraph5amc/package.py
@@ -36,13 +36,11 @@ class Madgraph5amc(MakefilePackage):
     version(
         "2.8.1",
         sha256="acda34414beba201e529b8c03f87f4893fb3f99ed2956a131d60a387e76c5b8c",
-        url="https://launchpad.net/mg5amcnlo/2.0/2.8.x/+download/MG5_aMC_v2.8.1.tar.gz",
         deprecated=True,
     )
     version(
         "2.7.3.py3",
         sha256="400c26f9b15b07baaad9bd62091ceea785c2d3a59618fdc27cad213816bc7225",
-        url="",
         deprecated=True,
     )
 
