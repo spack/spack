4abbbdec38bb2d8ed967b62e5c470aec3a343623
domke <673751-domke@users.noreply.gitlab.com>
2024-05-05 19:04:28 +0900

    rollback cmake change and move settings over to fujitsu-mpi

--- var/spack/repos/builtin/packages/fujitsu-mpi/package.py
+++ var/spack/repos/builtin/packages/fujitsu-mpi/package.py
@@ -55,13 +55,13 @@ class FujitsuMpi(Package):
 
     def setup_dependent_build_environment(self, env, dependent_spec):
         if self.spec.satisfies("%gcc"):
-            env.set('MPI_C_COMPILER', self.prefix.bin.mpicc)
-            env.set('MPI_CXX_COMPILER', self.prefix.bin.mpicxx)
-            env.set('MPI_Fortran_COMPILER', self.prefix.bin.mpifort)
+            env.set("MPI_C_COMPILER", self.prefix.bin.mpicc)
+            env.set("MPI_CXX_COMPILER", self.prefix.bin.mpicxx)
+            env.set("MPI_Fortran_COMPILER", self.prefix.bin.mpifort)
         else:
-            env.set('MPI_C_COMPILER', self.prefix.bin.mpifcc)
-            env.set('MPI_CXX_COMPILER', self.prefix.bin.mpiFCC)
-            env.set('MPI_Fortran_COMPILER', self.prefix.bin.mpifrt)
+            env.set("MPI_C_COMPILER", self.prefix.bin.mpifcc)
+            env.set("MPI_CXX_COMPILER", self.prefix.bin.mpiFCC)
+            env.set("MPI_Fortran_COMPILER", self.prefix.bin.mpifrt)
 
     def setup_run_environment(self, env):
         # Because MPI are both compilers and runtimes, we set up the compilers
