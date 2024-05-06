5ad4bf931be707a47238b9e6e8da1e6d9e1b4269
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-05-06 17:46:43 +0200

    Bootstrap GnuPG and clingo binaries together on macOS

--- .github/workflows/bootstrap-test.sh
+++ .github/workflows/bootstrap-test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -ex
+set -e
 source share/spack/setup-env.sh
+$PYTHON bin/spack bootstrap disable github-actions-v0.4
 $PYTHON bin/spack bootstrap disable spack-install
 $PYTHON bin/spack -d solve zlib
 tree $BOOTSTRAP/store
