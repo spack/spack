77d41b18cf5c0a48dba63ddc3194dfba07403815
alvaro-sch <alvarolegit@gmail.com>
2024-05-06 10:55:08 -0300

    Fixed openmpi versions

--- var/spack/repos/builtin/packages/orca/package.py
+++ var/spack/repos/builtin/packages/orca/package.py
@@ -59,8 +59,8 @@ class Orca(Package):
         "4.0.1.2": "2.0.2",
         "4.2.0": "3.1.4",
         "4.2.1": "3.1.4",
-        "5.0.3": "4.1.2",
-        "5.0.4": "4.1.2",
+        "5.0.3": "4.1.1",
+        "5.0.4": "4.1.1",
     }
     for orca_version, openmpi_version in openmpi_versions.items():
         depends_on(
