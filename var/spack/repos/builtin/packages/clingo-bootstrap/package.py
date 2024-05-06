252c0d696e915c23490fcb18750f56dfdd75ba05
Harmen Stoppels <me@harmenstoppels.nl>
2024-05-06 12:38:25 +0200

    clingo bootstrap: remove 3.12 patch and concretizer workarounds

--- var/spack/repos/builtin/packages/clingo-bootstrap/package.py
+++ var/spack/repos/builtin/packages/clingo-bootstrap/package.py
@@ -32,13 +32,6 @@ class ClingoBootstrap(Clingo):
         description="Enable a series of Spack-specific optimizations (PGO, LTO, mimalloc)",
     )
 
-    variant(
-        "force_setuptools",
-        default=False,
-        description="Force a dependency on setuptools to help the old concretizer",
-    )
-    depends_on("py-setuptools", type="build", when="+force_setuptools")
-
     # Enable LTO
     conflicts("~ipo", when="+optimized")
 
