bbd4b5be7b695ea9b3bebfcfd049a257f654fd65
Alec Scott <hi@alecbcs.com>
2024-05-06 00:30:34 -0700

    rust: add v1.78.0 (#44012)

--- var/spack/repos/builtin/packages/rust/package.py
+++ var/spack/repos/builtin/packages/rust/package.py
@@ -36,6 +36,7 @@ class Rust(Package):
     version("nightly")
 
     # Stable versions.
+    version("1.78.0", sha256="ff544823a5cb27f2738128577f1e7e00ee8f4c83f2a348781ae4fc355e91d5a9")
     version("1.76.0", sha256="9e5cff033a7f0d2266818982ad90e4d3e4ef8f8ee1715776c6e25073a136c021")
     version("1.75.0", sha256="5b739f45bc9d341e2d1c570d65d2375591e22c2d23ef5b8a37711a0386abc088")
     version("1.74.0", sha256="882b584bc321c5dcfe77cdaa69f277906b936255ef7808fcd5c7492925cf1049")
