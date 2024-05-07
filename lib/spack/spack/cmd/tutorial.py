6e75b7012d59f91fb51efbfbb40ae891015eb484
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-05-07 17:56:29 +0200

    Update the tutorial command to point to releases/v0.22 (#44056)

--- lib/spack/spack/cmd/tutorial.py
+++ lib/spack/spack/cmd/tutorial.py
@@ -23,7 +23,7 @@ level = "long"
 
 
 # tutorial configuration parameters
-tutorial_branch = "releases/v0.21"
+tutorial_branch = "releases/v0.22"
 tutorial_mirror = "file:///mirror"
 tutorial_key = os.path.join(spack.paths.share_path, "keys", "tutorial.pub")
 
