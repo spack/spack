076437b0ef3115fc58c0d3b76b2e11c4838cbb6b
Carlos Bederian <carlos.bederian@unc.edu.ar>
2024-05-08 03:40:13 -0300

    openmm: add 8.1.1, 8.1.0

--- var/spack/repos/builtin/packages/openmm/package.py
+++ var/spack/repos/builtin/packages/openmm/package.py
@@ -17,6 +17,8 @@ class Openmm(CMakePackage, CudaPackage):
     homepage = "https://openmm.org/"
     url = "https://github.com/openmm/openmm/archive/7.4.1.tar.gz"
 
+    version("8.1.1", sha256="347ad9f04dd88a673f7871127d9f23a75caf2c1a460a3f21f3328a24dc6547d0")
+    version("8.1.0", sha256="6f53bd2ae0a5e7c14aa9b429135ee2786a602afe2d71c871b808c51a19635645")
     version("8.0.0", sha256="dc63d7b47c8bb7b169c409cfd63d909ed0ce1ae114d37c627bf7a4231acf488e")
     version("7.7.0", sha256="51970779b8dc639ea192e9c61c67f70189aa294575acb915e14be1670a586c25")
     version("7.6.0", sha256="5a99c491ded9ba83ecc3fb1d8d22fca550f45da92e14f64f25378fda0048a89d")
