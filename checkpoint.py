61d0507db9052859b4e2905f943db7738d54a8c1
Peter Scheibel <scheibel1@llnl.gov>
2024-05-15 14:18:46 -0700

    more organization and layout

--- checkpoint.py
+++ checkpoint.py
@@ -1,4 +1,5 @@
 import inspect
+import json
 import os
 import pathlib
 import sys
@@ -8,12 +9,38 @@ import spack.environment as ev
 import spack.traverse as traverse
 
 
-def main():
-    env = ev.active_environment()
+class Checkpoint:
+    def __init__(self, env):
+        self.checksums = {}
 
-    if not env:
-        raise ValueError("An active env is required.")
+    def paths_of_interest(self):
+        return [
+            self.env.manifest_path,
+        ]
 
+    def calculate_checksums(self):
+        pass
+
+    @staticmethod
+    def read(path):
+        pass
+
+    def write(path):
+        pass
+
+def get_config_hashes(env):
+    # packages.yaml
+    # repos.yaml
+    # concretize.yaml (could swap reuse behavior)
+    # compilers.yaml (could change flags)
+    # (not) config.yaml
+
+    for section in ["packages", "repos", "concretize", "compilers"]:
+        data = spack.config.get_config(section)
+        # Possible issue: if I convert the retrieved section to yaml, will
+        # all the lines have a consistent order?
+
+def get_package_files(env):
     concrete_roots = list(y for x, y in env.concretized_specs())
 
     package_files = set()
@@ -28,7 +55,16 @@ def main():
 
         package_files.update(inspect.getfile(x) for x in inheritance_hierarchy[:cutoff])
 
-    print("\n".join(package_files))
+    return package_files
+
+def main():
+    env = ev.active_environment()
+
+    if not env:
+        raise ValueError("An active env is required.")
+
+    print("\n".join(get_package_files(env)))
+
 
 if __name__ == "__main__":
     main()
