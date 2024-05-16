6525478eeaf509141f945f62d724dc37a601954a
Peter Scheibel <scheibel1@llnl.gov>
2024-05-16 11:45:07 -0700

    hash configs

--- checkpoint.py
+++ checkpoint.py
@@ -7,6 +7,7 @@ import sys
 import spack
 import spack.environment as ev
 import spack.traverse as traverse
+import spack.util.crypto as crypto
 
 
 class Checkpoint:
@@ -28,6 +29,17 @@ class Checkpoint:
     def write(path):
         pass
 
+
+def _get_config_scopes():
+    return [x for x in spack.config.CONFIG.scopes.values()
+            if not isinstance(x, spack.config.InternalConfigScope)]
+
+
+def _checksum(path):
+    fn = crypto.hash_fun_for_algo("sha256")
+    return crypto.checksum(fn, path)
+
+
 def get_config_hashes(env):
     # packages.yaml
     # repos.yaml
@@ -35,12 +47,19 @@ def get_config_hashes(env):
     # compilers.yaml (could change flags)
     # (not) config.yaml
 
-    for section in ["packages", "repos", "concretize", "compilers"]:
-        data = spack.config.get(section)
-        # Possible issue: if I convert the retrieved section to yaml, will
-        # all the lines have a consistent order?
-        # Might be safer to just get all the files and hash them
-        import pdb; pdb.set_trace()
+    cfg_paths = set()
+
+    scopes = _get_config_scopes()
+    for scope in scopes:
+        for section in ["packages", "repos", "concretizer", "compilers"]:
+            cfg_path = scope.get_section_filename(section)
+            if cfg_path and os.path.exists(cfg_path):
+                cfg_paths.add(cfg_path)
+
+
+    path_to_hash = frozenset((x, _checksum(x)) for x in cfg_paths)
+    return path_to_hash
+
 
 def get_package_files(env):
     concrete_roots = list(y for x, y in env.concretized_specs())
@@ -67,7 +86,9 @@ def main():
 
     print("\n".join(get_package_files(env)))
 
-    get_config_hashes(env)
+    cfg_hashes = get_config_hashes(env)
+    import pdb; pdb.set_trace()
+    print(list(cfg_hashes)[:2])
 
 
 if __name__ == "__main__":
