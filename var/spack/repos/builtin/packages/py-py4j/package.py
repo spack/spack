4653a51cf4dddd624111c04651ac63ed25616814
Teague Sterling <teaguesterling@gmail.com>
2024-05-19 09:55:12 -0700

    Changing default java requirement. I'm not sure this is the right call

--- var/spack/repos/builtin/packages/py-py4j/package.py
+++ var/spack/repos/builtin/packages/py-py4j/package.py
@@ -26,7 +26,8 @@ class PyPy4j(PythonPackage):
     version("0.10.4", sha256="406fbfdbcbbb398739f61fafd25724670a405a668eb08c1721d832eadce06aae")
     version("0.10.3", sha256="f4570108ad014dd52a65c2288418e31cb8227b5ecc39ad7fc7fe98314f7a26f2")
 
-    variant("java", default=True, description="Require java via Spack instead of using system JRE")
+    # Should this be True by default? It's required for py4j to work but wasn't originally the case
+    variant("java", default=False, description="Require java via Spack instead of using system JRE")
 
     depends_on("py-setuptools", type="build")
     depends_on("java", when="+java", type="run")
