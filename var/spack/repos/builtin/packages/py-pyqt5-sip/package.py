fe4e8b2018c135ea4e2bc0cfe4a5c43b857a70b0
Michael Kuhn <michael.kuhn@ovgu.de>
2024-05-19 08:05:18 +0200

    py-pyqt5-sip: add 12.13.0 and fix build with gcc@14 (#44133)

--- var/spack/repos/builtin/packages/py-pyqt5-sip/package.py
+++ var/spack/repos/builtin/packages/py-pyqt5-sip/package.py
@@ -14,7 +14,14 @@ class PyPyqt5Sip(PythonPackage):
 
     license("GPL-2.0-only")
 
+    version("12.13.0", sha256="7f321daf84b9c9dbca61b80e1ef37bdaffc0e93312edae2cd7da25b953971d91")
     version("12.12.1", sha256="8fdc6e0148abd12d977a1d3828e7b79aae958e83c6cb5adae614916d888a6b10")
     version("12.9.0", sha256="d3e4489d7c2b0ece9d203ae66e573939f7f60d4d29e089c9f11daa17cfeaae32")
 
     depends_on("py-setuptools@30.3:", type="build")
+
+    patch(
+        "https://src.fedoraproject.org/rpms/python-pyqt5-sip/raw/841f58ce66df4dfcf11713e7adb6bd301403d5a8/f/afc99fa84d0d.patch",
+        sha256="82a326749b145b30eda3f0040cd7099c4c06a57a5e9626687b0a983de1ebfc3e",
+        when="@12.12: %gcc@14:",
+    )
