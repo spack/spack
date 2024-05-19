6834ce9055718f03cd04f55aa0c388edfeddb318
Teague Sterling <teaguesterling@gmail.com>
2024-05-19 09:24:02 -0700

    Version bump to meet py-spark expectations

--- var/spack/repos/builtin/packages/py-googleapis-common-protos/package.py
+++ var/spack/repos/builtin/packages/py-googleapis-common-protos/package.py
@@ -15,6 +15,7 @@ class PyGoogleapisCommonProtos(PythonPackage):
     license("Apache-2.0")
 
     version("1.58.0", sha256="c727251ec025947d545184ba17e3578840fc3a24a0516a020479edab660457df")
+    version("1.56.4", sha256="c25873c47279387cfdcbdafa36149887901d36202cb645a0e4f29686bf6e4417")
     version("1.55.0", sha256="53eb313064738f45d5ac634155ae208e121c963659627b90dfcb61ef514c03e1")
     version("1.6.0", sha256="e61b8ed5e36b976b487c6e7b15f31bb10c7a0ca7bd5c0e837f4afab64b53a0c6")
 
