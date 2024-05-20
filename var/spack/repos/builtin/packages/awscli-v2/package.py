3da982798172f953c93177b877eaa0c5c214573e
Teague Sterling <teaguesterling@users.noreply.github.com>
2024-05-20 09:21:09 -0700

    awscli-v2: add v2.15.53, and other updates (#44258)
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/awscli-v2/package.py
+++ var/spack/repos/builtin/packages/awscli-v2/package.py
@@ -11,29 +11,32 @@ class AwscliV2(PythonPackage):
 
     homepage = "https://docs.aws.amazon.com/cli"
     url = "https://github.com/aws/aws-cli/archive/refs/tags/2.13.22.tar.gz"
+    list_url = "https://github.com/aws/aws-cli/tags"
 
-    maintainers("climbfuji")
+    maintainers("climbfuji", "teaguesterling")
 
+    version("2.15.53", sha256="a4f5fd4e09b8f2fb3d2049d0610c7b0993f9aafaf427f299439f05643b25eb4b")
     version("2.13.22", sha256="dd731a2ba5973f3219f24c8b332a223a29d959493c8a8e93746d65877d02afc1")
 
-    depends_on("python@3.8:3.11", type=("build", "run"))
-    depends_on("py-flit-core@3.7.1:3.8.0", type=("build"))
-    depends_on("py-colorama@0.2.5:0.4.6", type=("build", "run"))
-    depends_on("py-docutils@0.10:0.19", type=("build", "run"))
-    depends_on("py-cryptography@3.3.2:40.0.1", type=("build", "run"))
-    depends_on("py-ruamel-yaml@0.15:0.17.21", type=("build", "run"))
-    depends_on("py-ruamel-yaml-clib@0.2:0.2.7", type=("build", "run"))
-    depends_on("py-prompt-toolkit@3.0.24:3.0.38", type=("build", "run"))
-    depends_on("py-distro@1.5:1.8", type=("build", "run"))
-    depends_on("py-awscrt@0.16.4:0.16.16", type=("build", "run"))
-    depends_on("py-python-dateutil@2.1:2", type=("build", "run"))
-    depends_on("py-jmespath@0.7.1:1.0", type=("build", "run"))
-    depends_on("py-urllib3@1.25.4:1.26", type=("build", "run"))
+    with default_args(type=("build", "run")):
+        depends_on("python@3.8:")
+        depends_on("py-flit-core@3.7.1:3.8.0")
+        depends_on("py-colorama@0.2.5:0.4.6")
+        depends_on("py-docutils@0.10:0.19")
+        depends_on("py-cryptography@3.3.2:40.0.1")
+        depends_on("py-ruamel-yaml@0.15:0.17.21")
+        depends_on("py-ruamel-yaml-clib@0.2:0.2.7", when="^python@:3.9")
+        depends_on("py-prompt-toolkit@3.0.24:3.0.38")
+        depends_on("py-distro@1.5:1.8")
+        depends_on("py-awscrt@0.16.4:0.16.16", when="@2.13")
+        depends_on("py-awscrt@0.19.18:0.19.19", when="@2.15")
+        depends_on("py-python-dateutil@2.1:2.8.2")
+        depends_on("py-jmespath@0.7.1:1.0")
+        depends_on("py-urllib3@1.25.4:1.26")
 
     variant("examples", default=True, description="Install code examples")
 
-    @run_after("install")
-    @when("~examples")
+    @run_after("install", when="~examples")
     def post_install(self):
         examples_dir = join_path(python_purelib, "awscli", "examples")
         remove_directory_contents(examples_dir)
