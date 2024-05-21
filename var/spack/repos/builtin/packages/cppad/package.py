103c480591785d39ac6f36163e9ea6983b41072c
whart222 <whart222@gmail.com>
2024-05-21 08:39:13 -0600

    Updating cppad package

--- var/spack/repos/builtin/packages/cppad/package.py
+++ var/spack/repos/builtin/packages/cppad/package.py
@@ -10,19 +10,24 @@ class Cppad(CMakePackage):
     """A Package for Differentiation of C++ Algorithms."""
 
     homepage = "https://www.coin-or.org/CppAD/"
-    url = "http://www.coin-or.org/download/source/CppAD/cppad-20170114.gpl.tgz"
+    url = "https://github.com/coin-or/CppAD/archive/refs/tags/20240000.4.tar.gz"
     git = "https://github.com/coin-or/CppAD.git"
 
     version("develop", branch="master")
-    version(
-        "20180000.0", sha256="1c355713e720fc5226ff3d6db2909922d46cd7ee0d36ee7985882f86905f655a"
-    )
+    version("20240000.4", sha256="0dfc1e30b32d5dd3086ee3adb6d2746a019e9d670b644c4d5ec1df3c35dd1fe5")
+    version("20220000.5", sha256="9fb4562f6169855eadcd86ac4671593d1c0edf97bb6ce7cbb28e19af2bfc165e")
+    version("20180000.0", sha256="1c355713e720fc5226ff3d6db2909922d46cd7ee0d36ee7985882f86905f655a")
     version("20170114", sha256="fa3980a882be2a668a7522146273a1b4f1d8dabe66ad4aafa8964c8c1fd6f957")
 
+
     def cmake_args(self):
-        # This package does not obey CMAKE_INSTALL_PREFIX
         args = [
             self.define("cppad_prefix", self.prefix),
             self.define("cmake_install_docdir", "share/cppad/doc"),
         ]
+        # This package does not obey CMAKE_INSTALL_PREFIX
+        #args.append("-DCMAKE_INSTALL_PREFIX=%s" % self.prefix)
+
+        args.append("-DCMAKE_BUILD_TYPE=Release")
+
         return args
