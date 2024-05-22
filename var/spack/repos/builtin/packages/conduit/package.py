fa7ace3672e9b72362ec747eac77b23cf435f2d9
Cyrus Harrison <cyrush@llnl.gov>
2024-05-22 01:05:40 -0700

    conduit: add v0.9.2 (#44308)

--- var/spack/repos/builtin/packages/conduit/package.py
+++ var/spack/repos/builtin/packages/conduit/package.py
@@ -45,6 +45,7 @@ class Conduit(CMakePackage):
     # is to bridge any spack dependencies that are still using the name master
     version("master", branch="develop", submodules=True)
     # note: 2021-05-05 latest tagged release is now preferred instead of develop
+    version("0.9.2", sha256="45d5a4eccd0fc978d153d29c440c53c483b8f29dfcf78ddcc9aa15c59b257177")
     version("0.9.1", sha256="a3f1168738dcf72f8ebf83299850301aaf56e803f40618fc1230a755d0d05363")
     version("0.9.0", sha256="844e012400ab820967eef6cec15e1aa9a68cb05119d0c1f292d3c01630111a58")
     version("0.8.8", sha256="99811e9c464b6f841f52fcd47e982ae47cbb01cba334cff43eabe13eea58c0df")
