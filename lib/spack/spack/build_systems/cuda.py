0ee1caabb44412d58bed7b1988dbcdbdfa70fbc8
pauleonix <paul.grosse-bley@ziti.uni-heidelberg.de>
2024-05-22 10:50:56 +0200

    cuda: add v12.4.1 (#43488)

--- lib/spack/spack/build_systems/cuda.py
+++ lib/spack/spack/build_systems/cuda.py
@@ -137,11 +137,14 @@ class CudaPackage(PackageBase):
         conflicts("%gcc@11.2:", when="+cuda ^cuda@:11.5")
         conflicts("%gcc@12:", when="+cuda ^cuda@:11.8")
         conflicts("%gcc@13:", when="+cuda ^cuda@:12.3")
+        conflicts("%gcc@14:", when="+cuda ^cuda@:12.4")
         conflicts("%clang@12:", when="+cuda ^cuda@:11.4.0")
         conflicts("%clang@13:", when="+cuda ^cuda@:11.5")
         conflicts("%clang@14:", when="+cuda ^cuda@:11.7")
         conflicts("%clang@15:", when="+cuda ^cuda@:12.0")
-        conflicts("%clang@16:", when="+cuda ^cuda@:12.3")
+        conflicts("%clang@16:", when="+cuda ^cuda@:12.1")
+        conflicts("%clang@17:", when="+cuda ^cuda@:12.3")
+        conflicts("%clang@18:", when="+cuda ^cuda@:12.4")
 
         # https://gist.github.com/ax3l/9489132#gistcomment-3860114
         conflicts("%gcc@10", when="+cuda ^cuda@:11.4.0")
