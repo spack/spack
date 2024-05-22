07df4cb4e44e829a84ab78338a5bebfb669b0e99
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2024-05-22 01:04:27 -0700

    kcov: convert to new stand-alone test process (tested with latest version) (#44309)

--- var/spack/repos/builtin/packages/kcov/package.py
+++ var/spack/repos/builtin/packages/kcov/package.py
@@ -12,11 +12,17 @@ class Kcov(CMakePackage):
     compilation options"""
 
     homepage = "https://simonkagstrom.github.io/kcov/index.html"
-    url = "https://github.com/SimonKagstrom/kcov/archive/38.tar.gz"
+    url = "https://github.com/SimonKagstrom/kcov/archive/refs/tags/v42.tar.gz"
 
     license("GPL-2.0-or-later")
 
-    version("38", sha256="b37af60d81a9b1e3b140f9473bdcb7975af12040feb24cc666f9bb2bb0be68b4")
+    version("42", sha256="2c47d75397af248bc387f60cdd79180763e1f88f3dd71c94bb52478f8e74a1f8")
+    version(
+        "38",
+        sha256="b37af60d81a9b1e3b140f9473bdcb7975af12040feb24cc666f9bb2bb0be68b4",
+        url="https://github.com/SimonKagstrom/kcov/archive/38.tar.gz",
+        deprecated=True,
+    )
 
     depends_on("cmake@2.8.4:", type="build")
     depends_on("zlib-api")
@@ -29,9 +35,13 @@ class Kcov(CMakePackage):
         # https://github.com/Homebrew/homebrew-core/blob/master/Formula/kcov.rb
         return ["-DSPECIFY_RPATH=ON"]
 
+    def test_kcov_help(self):
+        """run installed kcov help"""
+        kcov = Executable(self.prefix.bin.kcov)
+        # The help message exits with an exit code of 1
+        kcov("-h", ignore_errors=1)
+
     @run_after("install")
     @on_package_attributes(run_tests=True)
-    def test_install(self):
-        # The help message exits with an exit code of 1
-        kcov = Executable(self.prefix.bin.kcov)
-        kcov("-h", ignore_errors=1)
+    def check_install(self):
+        self.test_kcov_help()
