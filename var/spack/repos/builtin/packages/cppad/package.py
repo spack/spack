b6ec6c0589d8819d3ab41bbf328f71bac6b6e3c4
whart222 <whart222@gmail.com>
2024-05-21 20:02:12 -0600

    Pushing changes based on Tamara's feedback.

--- var/spack/repos/builtin/packages/cppad/package.py
+++ var/spack/repos/builtin/packages/cppad/package.py
@@ -11,11 +11,11 @@ class Cppad(CMakePackage):
 
     maintainers("whart222")
 
-    homepage = "https://www.coin-or.org/CppAD/"
+    homepage = "https://github.com/coin-or/CppAD"
     url = "https://github.com/coin-or/CppAD/archive/refs/tags/20240000.4.tar.gz"
     git = "https://github.com/coin-or/CppAD.git"
 
-    version("develop", branch="master")
+    version("master", branch="master")
     version(
         "20240000.4", sha256="0dfc1e30b32d5dd3086ee3adb6d2746a019e9d670b644c4d5ec1df3c35dd1fe5"
     )
@@ -23,9 +23,15 @@ class Cppad(CMakePackage):
         "20220000.5", sha256="9fb4562f6169855eadcd86ac4671593d1c0edf97bb6ce7cbb28e19af2bfc165e"
     )
     version(
-        "20180000.0", sha256="1c355713e720fc5226ff3d6db2909922d46cd7ee0d36ee7985882f86905f655a"
+        "20180000.0",
+        sha256="1c355713e720fc5226ff3d6db2909922d46cd7ee0d36ee7985882f86905f655a",
+        deprecated=True,
     )
-    version("20170114", sha256="fa3980a882be2a668a7522146273a1b4f1d8dabe66ad4aafa8964c8c1fd6f957")
+    version(
+        "20170114",
+        sha256="fa3980a882be2a668a7522146273a1b4f1d8dabe66ad4aafa8964c8c1fd6f957",
+        deprecated=True,
+    )
 
     def cmake_args(self):
         args = [
