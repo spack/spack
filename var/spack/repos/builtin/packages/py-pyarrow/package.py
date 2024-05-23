9661e03025c1de09cadd96530e3dc1cdb13f29d8
Teague Sterling <teaguesterling@users.noreply.github.com>
2024-05-23 15:37:53 -0700

    Update package.py

--- var/spack/repos/builtin/packages/py-pyarrow/package.py
+++ var/spack/repos/builtin/packages/py-pyarrow/package.py
@@ -63,9 +63,10 @@ class PyPyarrow(PythonPackage, CudaPackage):
     depends_on("py-pip@:23.0", type="build")
 
     depends_on("py-numpy@1.16.6:", type=("build", "run"), when="@3:")
-    depends_on("py-numpy@1.14:", type=("build", "run"), when="@0.15:")
-    # Oldest supported numpy prior to Python 3.9 is 1.25
-    depends_on("py-numpy@:1.25", type=("build", "run"), when="^python@3.8")
+    # Prior to python 3.9 numpy must be >=0.14,<1.25
+    depends_on("py-numpy@0.14:1.24", when="^python@:3.8")
+    depends_on("py-numpy@1.25:", when="^python@3.9:")
+    
 
     arrow_versions = (
         "@0.9.0",
