10e341a2a4d35866dc493e74085057352aa11295
Wouter Deconinck <wdconinc@gmail.com>
2024-05-25 08:44:35 -0500

    py-dask-histogram: new package

---
+++ var/spack/repos/spack_repo/builtin/packages/py_dask_histogram/package.py
@@ -0,0 +1,26 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyDaskHistogram(PythonPackage):
+    """Histograms with task scheduling."""
+
+    homepage = "https://github.com/dask-contrib/dask-histogram"
+    pypi = "dask_histogram/dask_histogram-2024.3.0.tar.gz"
+
+    maintainers("wdconinc")
+
+    license("BSD-3-Clause", checked_by="wdconinc")
+
+    version("2024.3.0", sha256="834d4d25f5e2c417f5e792fafaa55484c20c9f3812d175125de7ac34f994ef7b")
+
+    depends_on("py-hatchling@1.8.0:", type="build")
+    depends_on("py-hatch-vcs", type="build")
+
+    depends_on("py-boost-histogram@1.3.2:", type=("build", "run"))
+    depends_on("py-dask@2021.03.0:", type=("build", "run"))
