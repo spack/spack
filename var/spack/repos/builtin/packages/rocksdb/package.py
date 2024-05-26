bccde4f92cd9ef834e90dfb3a2acb3aeb782b535
Michael Kuhn <michael.kuhn@ovgu.de>
2024-05-26 21:15:03 +0200

    rocksdb: add 9.2.1 (#44384)

--- var/spack/repos/builtin/packages/rocksdb/package.py
+++ var/spack/repos/builtin/packages/rocksdb/package.py
@@ -16,6 +16,7 @@ class Rocksdb(MakefilePackage):
     license("Apache-2.0 OR GPL-2.0-only")
 
     version("master", git=git, branch="master", submodules=True)
+    version("9.2.1", sha256="bb20fd9a07624e0dc1849a8e65833e5421960184f9c469d508b58ed8f40a780f")
     version("8.6.7", sha256="cdb2fc3c6a556f20591f564cb8e023e56828469aa3f76e1d9535c443ba1f0c1a")
     version("8.1.1", sha256="9102704e169cfb53e7724a30750eeeb3e71307663852f01fa08d5a320e6155a8")
     version("7.7.3", sha256="b8ac9784a342b2e314c821f6d701148912215666ac5e9bdbccd93cf3767cb611")
