db50bb8326a1870569c340a24f9ff8485f6d3a76
Wouter Deconinck <wdconinc@gmail.com>
2024-05-28 12:19:31 -0500

    xorg-docs: add v1.7.3 (#44388)
    * xorg-sgml-doctools: add v1.12.1
    
    * xorg-docs: add v1.7.3
    
    * util-macros: add v1.20.1 (now distributed as xz)
    
    * util-macros: prefer spec.satisfies
    
    Co-authored-by: Alec Scott <hi@alecbcs.com>
    
    ---------
    
    Co-authored-by: Alec Scott <hi@alecbcs.com>

--- var/spack/repos/builtin/packages/xorg-sgml-doctools/package.py
+++ var/spack/repos/builtin/packages/xorg-sgml-doctools/package.py
@@ -18,8 +18,10 @@ class XorgSgmlDoctools(AutotoolsPackage, XorgPackage):
 
     license("MIT")
 
+    version("1.12.1", sha256="8de3406f96a02bc3ab51ff47ba1612d9a11fc25d2edcaa06caa2cb2420d7bae0")
     version("1.12", sha256="985a0329e6a6dadd6ad517f8d54f8766ab4b52bb8da7b07d02ec466bec444bdb")
     version("1.11", sha256="986326d7b4dd2ad298f61d8d41fe3929ac6191c6000d6d7e47a8ffc0c34e7426")
 
     depends_on("pkgconfig", type="build")
-    depends_on("util-macros", type="build")
+    depends_on("util-macros@1.8:", type="build")
+    depends_on("util-macros@1.20:", type="build", when="@1.12.1:")
