a0e4eec4acff4b580fbd7b4c9ae5a7b8c3a83269
Wouter Deconinck <wdconinc@gmail.com>
2024-05-28 14:00:21 -0500

    xorg libs: new versions

--- var/spack/repos/builtin/packages/libxaw/package.py
+++ var/spack/repos/builtin/packages/libxaw/package.py
@@ -10,13 +10,14 @@ class Libxaw(AutotoolsPackage, XorgPackage):
     """Xaw is the X Athena Widget Set.
     Xaw is a widget set based on the X Toolkit Intrinsics (Xt) Library."""
 
-    homepage = "https://cgit.freedesktop.org/xorg/lib/libXaw"
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libXaw"
     xorg_mirror_path = "lib/libXaw-1.0.13.tar.gz"
 
     license("MIT")
 
     maintainers("wdconinc")
 
+    version("1.0.16", sha256="012f90adf8739f2f023d63a5fee1528949cf2aba92ef7ac1abcfc2ae9cf28798")
     version("1.0.15", sha256="ca8a613884c922985202075b3cc8ee8821bfa83a5eb066189ae3cca131e63972")
     version("1.0.14", sha256="59cfed2712cc80bbfe62dd1aacf24f58d74a76dd08329a922077b134a8d8048f")
     version("1.0.13", sha256="7e74ac3e5f67def549722ff0333d6e6276b8becd9d89615cda011e71238ab694")
--- var/spack/repos/builtin/packages/libxaw3d/package.py
+++ var/spack/repos/builtin/packages/libxaw3d/package.py
@@ -10,13 +10,15 @@ class Libxaw3d(AutotoolsPackage, XorgPackage):
     """Xaw3d is the X 3D Athena Widget Set.
     Xaw3d is a widget set based on the X Toolkit Intrinsics (Xt) Library."""
 
-    homepage = "https://cgit.freedesktop.org/xorg/lib/libXaw3d"
+    homepage = "https://gitlab.freedesktop.org/xorg/lib/libXaw3d"
     xorg_mirror_path = "lib/libXaw3d-1.6.2.tar.gz"
 
     license("MIT")
 
     maintainers("wdconinc")
 
+    version("1.6.6", sha256="0cdb8f51c390b0f9f5bec74454e53b15b6b815bc280f6b7c969400c9ef595803")
+    version("1.6.5", sha256="1123d80c58f45616ef18502081eeec5e92f20c7e7dd82a24f9e2e4f3c0e86dc7")
     version("1.6.4", sha256="09fecfdab9d7d5953567883e2074eb231bc7a122a06e5055f9c119090f1f76a7")
     version("1.6.2", sha256="847dab01aeac1448916e3b4edb4425594b3ac2896562d9c7141aa4ac6c898ba9")
 
@@ -25,6 +27,7 @@ class Libxaw3d(AutotoolsPackage, XorgPackage):
     depends_on("libxmu")
     depends_on("libxext")
     depends_on("libxpm")
+    depends_on("xproto@7.0.22:")
 
     depends_on("pkgconfig", type="build")
     depends_on("util-macros", type="build")
