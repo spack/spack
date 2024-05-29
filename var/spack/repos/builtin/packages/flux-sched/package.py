f7c281f90ae49d522113e11deb0be82e716771ed
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2024-05-29 11:05:52 -0600

    Update var/spack/repos/builtin/packages/flux-sched/package.py
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/flux-sched/package.py
+++ var/spack/repos/builtin/packages/flux-sched/package.py
@@ -74,7 +74,7 @@ class FluxSched(CMakePackage, AutotoolsPackage):
     depends_on("yaml-cpp@0.6.3")
     depends_on("uuid")
     depends_on("pkgconfig")
-    conflicts("%gcc@:9.3")
+    conflicts("%gcc@:9.3", when="@0.34:")
     depends_on("py-sphinx@1.6.3:", when="+docs", type="build")
 
     depends_on("flux-core", type=("build", "link", "run"))
