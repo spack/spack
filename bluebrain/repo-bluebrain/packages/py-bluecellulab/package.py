df80a3dfd281542d736320ba54bd5c518396075a
Tristan Carel <tristan.carel@epfl.ch>
2024-05-29 08:02:16 +0200

    py-emodel-generalisation: allow latest libsonata 0.1.26 (#2445)
    * py-emodel-generalisation: accepts py-bluecellulab@2.6
    
    For now, py-emodel-generalization is stuck with py-bluecellulab@1.7.6
    which does not seem compatible with the latest libsonata (0.1.26).
    This change loosen the restriction on py-bluecellulab requirements to
    use the latest version 2.6.10
    
    * py-bluecellulab: add 2.6.15
    
    * py-emodel-generalisation: align deps to setup.py
    
    * py-bluecellulab: add 2.6.11
    
    * bump emodel-gen
    
    * fix
    
    ---------
    
    Co-authored-by: arnaudon <alexis.arnaudon@epfl.ch>

--- bluebrain/repo-bluebrain/packages/py-bluecellulab/package.py
+++ bluebrain/repo-bluebrain/packages/py-bluecellulab/package.py
@@ -12,6 +12,8 @@ class PyBluecellulab(PythonPackage):
     homepage = "https://github.com/BlueBrain/BlueCelluLab"
     pypi = "bluecellulab/bluecellulab-1.7.6.tar.gz"
 
+    version("2.6.15", sha256="8e366debbd0b531826d49264b8349a0f18d3741f3cd71cf0b8a14a3569a69a19")
+    version("2.6.11", sha256="00bedd9d57f0c484a44ddc424d5c49d4c57d16691b4d7a40da6699bc4983966d")
     version("2.6.10", sha256="9192137727753a6a58295a7fad6270887c4ce38957a6a82ed62b72dc5da3ab47")
     version("1.7.6", sha256="a60ec17c44e759b6726c399d764e4220f6641bf3d845fc3fbfe56d07dba9e6d5")
 
--- bluebrain/repo-bluebrain/packages/py-emodel-generalisation/package.py
+++ bluebrain/repo-bluebrain/packages/py-emodel-generalisation/package.py
@@ -11,9 +11,9 @@ class PyEmodelGeneralisation(PythonPackage):
 
     homepage = "https://github.com/BlueBrain/emodel-generalisation"
     git = "https://github.com/BlueBrain/emodel-generalisation.git"
-    pypi = "emodel-generalisation/emodel-generalisation-0.2.5.tar.gz"
+    pypi = "emodel-generalisation/emodel_generalisation-0.2.11.tar.gz"
 
-    version("0.2.10", sha256="31b0a83bc2c0976a5e26c14f00af8d52949c0320390a692eb90952f8d26a7bc3")
+    version("0.2.11", sha256="b84822a1ae0a4b53728b55f6dcfd291ca59d6244f2430650699333ba93e372a8")
 
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools-scm", type="build")
@@ -23,19 +23,19 @@ class PyEmodelGeneralisation(PythonPackage):
     depends_on("py-pandas@1.5.3:", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
     depends_on("py-datareuse@0.0.3:", type=("build", "run"))
-    depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-pyyaml@6:", type=("build", "run"))
     depends_on("py-bluepyopt@1.13.86:", type=("build", "run"))
     depends_on("py-neurom@3.2.2:", type=("build", "run"))
-    depends_on("py-efel@5.5.7:", type=("build", "run"))
-    depends_on("py-morph-tool@2.9:", type=("build", "run"))
+    depends_on("py-efel@5.5.5:", type=("build", "run"))
+    depends_on("py-morph-tool@2.9.1:", type=("build", "run"))
     depends_on("neuron+python@8.0:", type=("build", "run"))
     depends_on("py-matplotlib@3.6.2:", type=("build", "run"))
-    depends_on("py-bluecellulab@1.7.6:2.3.1", type=("build", "run"))
+    depends_on("py-bluecellulab@2.6.11", type=("build", "run"))
     depends_on("py-seaborn@0.12.2:", type=("build", "run"))
     depends_on("py-bluepyparallel@0.2.2:", type=("build", "run"))
     depends_on("py-xgboost@1.7.5:1", type=("build", "run"))
     depends_on("py-diameter-synthesis@0.5.4:", type=("build", "run"))
-    depends_on("py-voxcell@3.1.6:", type=("build", "run"))
+    depends_on("py-voxcell@3.1.5:", type=("build", "run"))
     depends_on("py-shap@0.41.0:", type=("build", "run"))
     depends_on("py-scikit-learn@1.1.3:", type=("build", "run"))
-    depends_on("py-luigi-tools@0.3.4:", type=("build", "run"))
+    depends_on("py-luigi-tools@0.3.3:", type=("build", "run"))
