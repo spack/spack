bd473365948bb1968b2615e962884e0abf7489f8
snehring <7978778+snehring@users.noreply.github.com>
2024-06-01 02:03:35 -0500

    apptainer: adding version 1.3.1 (#44104)
    e2fsprogs: adding version 1.47.0 and fuse2fs variant
    fuse-overlayfs: adding version 1.13
    squashfuse: adding version 0.5.1 and 0.5.2
    e2fsprogs: fixing audit errors
    apptainer: expanding deps to cover more versions, fix binary_path logic.
    apptainer: add go version deps, fix e2fsprogs patch sums, rework dep paths
    fuse-overlayfs: tightening up the libfuse dep
    
    Signed-off-by: Shane Nehring <snehring@iastate.edu>

--- var/spack/repos/builtin/packages/apptainer/package.py
+++ var/spack/repos/builtin/packages/apptainer/package.py
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 
+from glob import glob
+from os.path import basename
+
 from spack.package import *
 from spack.pkg.builtin.singularityce import SingularityBase
 
@@ -33,6 +36,7 @@ class Apptainer(SingularityBase):
     )
 
     version("main", branch="main")
+    version("1.3.1", sha256="6956c689c4a8f148789c5c34b33c15ad8f3460b4cee3f48022119fd872eacee9")
     version("1.2.5", sha256="606b67ef97683e1420401718687d258b1034fdf2edae72eeacd0828dffbfc2c2")
     version("1.1.9", sha256="c615777539154288542cf393d3fd44c04ccb3260bc6330dc324d4e4ebe902bfa")
     version("1.1.7", sha256="e6d3956a26c3965703402e17f153ba07f59bf710068806462b314d2d04e825e7")
@@ -42,8 +46,15 @@ class Apptainer(SingularityBase):
     version("1.1.3", sha256="c7bf7f4d5955e1868739627928238d02f94ca9fd0caf110b0243d65548427899")
     version("1.0.2", sha256="2d7a9d0a76d5574459d249c3415e21423980d9154ce85e8c34b0600782a7dfd3")
 
+    depends_on("e2fsprogs@1.47:+fuse2fs", type="run")
     depends_on("go@1.17.5:", when="@1.1.0:")
+    depends_on("go@1.19:", when="@1.2:")
+    depends_on("go@1.20:", when="@1.3:")
+    depends_on("gocryptfs@2.4:", type="run", when="@1.3:")
     depends_on("squashfuse", type="run")
+    depends_on("squashfuse@0.5.1:", type="run", when="@1.3:")
+    depends_on("fuse-overlayfs", type="run")
+    depends_on("fuse-overlayfs@1.13:", type="run", when="@1.3:")
 
     singularity_org = "apptainer"
     singularity_name = "apptainer"
@@ -65,3 +76,19 @@ class Apptainer(SingularityBase):
         # Certain go modules this build pulls in cannot be built with anything
         # other than -O0. Best to just discard any injected flags.
         return (None, flags, None)
+
+    # They started vendoring the fuse bits and assume they'll be in the
+    # libexec/apptainer prefix as a result. When singularity is run with
+    # suid it doesn't search the user's $PATH for security reasons.
+    # Since we don't use the vendored deps and instead install them in
+    # their own prefixes they are not found by default.
+    # This is likely only relevant for 1.3:, but it should be fine everywhere
+    @run_after("install")
+    def fix_binary_path(self):
+        for i in [
+            s for s in ["e2fsprogs", "gocryptfs", "squashfuse", "fuse-overlayfs"] if s in self.spec
+        ]:
+            for binary in glob(join_path(self.spec[i].prefix.bin, "*")):
+                symlink(
+                    binary, join_path(self.spec.prefix.libexec.apptainer.bin, basename(binary))
+                )
