ef8185804e2d32adc4d90fac60b7066d70054048
Teague Sterling <teaguesterling@gmail.com>
2024-05-31 18:45:02 -0700

    Added version referenced by mofrendorf

--- var/spack/repos/builtin/packages/kentutils/package.py
+++ var/spack/repos/builtin/packages/kentutils/package.py
@@ -13,6 +13,7 @@ class Kentutils(MakefilePackage):
     url = "https://hgdownload.cse.ucsc.edu/admin/exe/userApps.archive/userApps.v453.src.tgz"
 
     version("465", sha256="eef17b1f3182d1d9dc99b5c73a6b0468d5d3bd80470f25d3f7706cc1372e04b0")
+    version("464", sha256="24e20fe68e2a2894d802c87662f69a62f71b3c15fafb2e4d6c3c425c63638bb2")
     version("460", sha256="b955e56ee880074521ef1ab1371491f47e66dc6fdd93b05328386dd675a635fa")
     version("455", sha256="e458cadad7c4a5c1b8385edafffa1b29380ac725a0c20535bf5a3bab99fe80db")
     # This version isn't present in the archive any more
