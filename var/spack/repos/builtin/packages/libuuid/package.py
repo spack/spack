19067299e91044a8cfa6b804d598a5895fc28f9a
Teague Sterling <teaguesterling@users.noreply.github.com>
2024-06-02 19:30:12 -0700

    Update package.py

--- var/spack/repos/builtin/packages/libuuid/package.py
+++ var/spack/repos/builtin/packages/libuuid/package.py
@@ -16,4 +16,4 @@ class Libuuid(AutotoolsPackage, SourceforgePackage):
 
     provides("uuid")
 
-    patch("fix-flock-sys-file-issue.patch", when="%gcc@14:")  # This may be OS specific as well
+    patch("fix-flock-sys-file-issue.patch", when="%gcc@13:")  # This may be OS specific as well
