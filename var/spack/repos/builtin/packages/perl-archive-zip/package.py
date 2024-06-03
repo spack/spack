9f36a60c6d2e982863c760446b499942403d0226
Teague Sterling <teaguesterling@gmail.com>
2024-06-02 18:15:33 -0700

    Adding dependency for perl-archive-zip

--- var/spack/repos/builtin/packages/perl-archive-zip/package.py
+++ var/spack/repos/builtin/packages/perl-archive-zip/package.py
@@ -15,3 +15,5 @@ class PerlArchiveZip(PerlPackage):
     license("GPL-1.0-or-later OR Artistic-1.0-Perl")
 
     version("1.68", sha256="984e185d785baf6129c6e75f8eb44411745ac00bf6122fb1c8e822a3861ec650")
+
+    depends_on("perl-compress-raw-zlib")
