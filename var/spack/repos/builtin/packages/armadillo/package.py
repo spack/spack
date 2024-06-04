b55707eb671cfab3ba466d89c59a13618a0c7a64
Chris Marsh <chrismarsh.c2@gmail.com>
2024-06-04 13:57:12 -0600

    style fix

--- var/spack/repos/builtin/packages/armadillo/package.py
+++ var/spack/repos/builtin/packages/armadillo/package.py
@@ -42,7 +42,6 @@ class Armadillo(CMakePackage):
     # platform's compiler is adding `#define linux 1`.
     patch("undef_linux.patch", when="platform=linux")
 
-
     def flag_handler(self, name, flags):
         spec = self.spec
         if name == "ldflags":
@@ -50,7 +49,6 @@ class Armadillo(CMakePackage):
                 flags.append("-Wl,-ld_classic")
 
         return (flags, None, None)
-
 
     def patch(self):
         # Do not include Find{BLAS_type} because we are specifying the
