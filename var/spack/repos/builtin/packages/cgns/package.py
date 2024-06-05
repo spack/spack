0a5aa6761a7068b5256e1a85c45c1c70db389e95
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2024-06-05 22:03:40 +0200

    cgns: patch for gcc14

--- var/spack/repos/builtin/packages/cgns/package.py
+++ var/spack/repos/builtin/packages/cgns/package.py
@@ -73,6 +73,10 @@ class Cgns(CMakePackage):
     # https://bugs.gentoo.org/662210
     patch("no-matherr.patch", when="@:3.3.1 +tools")
 
+    # patch for gcc14 due to using internal tk type/function,
+    # see https://github.com/CGNS/CGNS/pull/757
+    patch("gcc14.patch", when="@:4.4.0 %gcc@14:")
+
     def cmake_args(self):
         spec = self.spec
         options = []
