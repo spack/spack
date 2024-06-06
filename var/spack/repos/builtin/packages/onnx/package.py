1f6838b7bbd7a1e170a49db9e5f67662890acbcb
Wouter Deconinck <wdconinc@gmail.com>
2024-06-06 07:11:23 -0500

    (py-)onnx: add v1.16.1 (#44485)

--- var/spack/repos/builtin/packages/onnx/package.py
+++ var/spack/repos/builtin/packages/onnx/package.py
@@ -20,6 +20,7 @@ class Onnx(CMakePackage):
     license("Apache-2.0")
 
     version("master", branch="master")
+    version("1.16.1", sha256="0e6aa2c0a59bb2d90858ad0040ea1807117cc2f05b97702170f18e6cd6b66fb3")
     version("1.16.0", sha256="0ce153e26ce2c00afca01c331a447d86fbf21b166b640551fe04258b4acfc6a4")
     version("1.15.0", sha256="c757132e018dd0dd171499ef74fca88b74c5430a20781ec53da19eb7f937ef68")
     version("1.14.1", sha256="e296f8867951fa6e71417a18f2e550a730550f8829bd35e947b4df5e3e777aa1")
