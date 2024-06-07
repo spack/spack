4f4d696ffecce059484c00acfe9b87e56fef053f
Alec Scott <scott112@llnl.gov>
2024-06-07 14:31:52 -0700

    fd: add v10.1.0

--- var/spack/repos/builtin/packages/fd/package.py
+++ var/spack/repos/builtin/packages/fd/package.py
@@ -16,6 +16,7 @@ class Fd(CargoPackage):
 
     license("Apache-2.0 OR MIT")
 
+    version("10.1.0", sha256="ee4b2403388344ff60125c79ff25b7895a170e7960f243ba2b5d51d2c3712d97")
     version("9.0.0", sha256="306d7662994e06e23d25587246fa3fb1f528579e42a84f5128e75feec635a370")
     version("8.7.0", sha256="13da15f3197d58a54768aaad0099c80ad2e9756dd1b0c7df68c413ad2d5238c9")
     version("8.4.0", sha256="d0c2fc7ddbe74e3fd88bf5bb02e0f69078ee6d2aeea3d8df42f508543c9db05d")
