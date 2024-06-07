52eabe6b085052a0cc84bc841cc462f967f14e4e
Teague Sterling <teaguesterling@gmail.com>
2024-06-06 20:11:00 -0700

    Adding variants for introspection and introsection setup

--- var/spack/repos/builtin/packages/libxfce4util/package.py
+++ var/spack/repos/builtin/packages/libxfce4util/package.py
@@ -19,6 +19,30 @@ class Libxfce4util(AutotoolsPackage):
 
     version("4.16.0", sha256="60598d745d1fc81ff5ad3cecc3a8d1b85990dd22023e7743f55abd87d8b55b83")
 
+    variant("glibtop", default=True, description="Build with glibtop support")
+    variant("introspection", default=True, description="Build with gobject-introspection support")
+    variant("vala", default=True, description="Build with vala support")
+
+    with default_args(type=("run", "link", "build")):
+        depends_on("libgtop", when="+glibtop")
+        depends_on("gobject-introspection", when="+introspection")
+        depends_on("vala", when="+vala")
+
     with when("@4.16"):
-        depends_on("glib@2.50:", type=("build", "link", "run"))
         depends_on("intltool@0.35.0:", type="build")
+        with default_args(type=("run", "link", "build")):
+            depends_on("glib@2.50:")
+            depends_on("gobject-introspection@1.60:", when="+introspection")
+
+    def configure_args(self):
+        args = []
+
+        args += self.enable_or_disable("glibtop")
+        args += self.enable_or_disable("introspection")
+        args += self.enable_or_disable("vala")
+
+        return args
+
+    def setup_dependent_build_environment(self, env, dep_spec):
+        if self.spec.satisfies("+introspection") and dep_spec.satisfies("+introspection"):
+            env.append_path("XDG_DATA_DIRS", self.prefix.share)
