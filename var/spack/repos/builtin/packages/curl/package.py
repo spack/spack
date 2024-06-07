ca5fc55b03606d28c7105e1c009c534f54094cbd
Alec Scott <scott112@llnl.gov>
2024-06-07 14:25:04 -0700

    curl: add v8.8.0

--- var/spack/repos/builtin/packages/curl/package.py
+++ var/spack/repos/builtin/packages/curl/package.py
@@ -32,6 +32,7 @@ class Curl(NMakePackage, AutotoolsPackage):
 
     license("curl")
 
+    version("8.8.0", sha256="40d3792d38cfa244d8f692974a567e9a5f3387c547579f1124e95ea2a1020d0d")
     version("8.7.1", sha256="05bbd2b698e9cfbab477c33aa5e99b4975501835a41b7ca6ca71de03d8849e76")
     version("8.6.0", sha256="b4785f2d8877fa92c0e45d7155cf8cc6750dbda961f4b1a45bcbec990cf2fa9b")
     version("8.4.0", sha256="e5250581a9c032b1b6ed3cf2f9c114c811fc41881069e9892d115cc73f9e88c6")
