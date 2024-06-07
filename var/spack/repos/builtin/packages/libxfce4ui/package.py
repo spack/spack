1c9c9f218113c59095c39efbbe72673d6e613809
Teague Sterling <teaguesterling@gmail.com>
2024-06-06 20:12:13 -0700

    Adding variants for introspection and introsection setup with new libxfce4ui package

--- var/spack/repos/builtin/packages/libxfce4ui/package.py
+++ var/spack/repos/builtin/packages/libxfce4ui/package.py
@@ -18,11 +18,39 @@ class Libxfce4ui(AutotoolsPackage):
 
     version("4.16.0", sha256="8b06c9e94f4be88a9d87c47592411b6cbc32073e7af9cbd64c7b2924ec90ceaa")
 
+    variant("glibtop", default=True, description="Build with glibtop support")
+    variant("introspection", default=True, description="Build with gobject-introspection support")
+    variant("vala", default=True, description="Build with vala support")
+
+    with default_args(type=("build", "link", "run")):
+        depends_on("libgtop", when="+glibtop")
+        depends_on("gobject-introspection", when="+introspection")
+        depends_on("vala", when="+vala")
+
+        depends_on("libxfce4util+glibtop", when="+glibtop")
+        depends_on("libxfce4util+introspection", when="+introspection")
+        depends_on("libxfce4util+vala", when="+vala")
+
     with when("@4.16"):
         depends_on("intltool@0.35.0:", type="build")
         with default_args(type=("build", "link", "run")):
             depends_on("libxfce4util@4.16")
+            depends_on("xfconf@4.16")
+
             depends_on("glib@2.50:")
             depends_on("gtkplus@3.22:")
-            depends_on("libgtop@2")
+            depends_on("libgtop@2", when="+glibtop")
+            depends_on("gobject-introspection@1.60:", when="+introspection")
 
+    def configure_args(self):
+        args = []
+
+        args += self.enable_or_disable("glibtop")
+        args += self.enable_or_disable("introspection")
+        args += self.enable_or_disable("vala")
+
+        return args
+
+    def setup_dependent_build_environment(self, env, dep_spec):
+        if self.spec.satisfies("+introspection") and dep_spec.satisfies("+introspection"):
+            env.append_path("XDG_DATA_DIRS", self.prefix.share)
