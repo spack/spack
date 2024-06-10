c0c2894c45386fb4b169effb66f2d66746a293bb
Dave Keeshan <dave.keeshan@daxzio.com>
2024-06-10 21:24:42 +0100

    Revert depends_on for flex back to the deafult case

--- var/spack/repos/builtin/packages/verilator/package.py
+++ var/spack/repos/builtin/packages/verilator/package.py
@@ -75,7 +75,7 @@ class Verilator(AutotoolsPackage):
     depends_on("libtool", type="build")
     depends_on("help2man", type="build")
     depends_on("bison", type="build")
-    depends_on("flex", type=("build", "link"))
+    depends_on("flex")
     depends_on("perl", type=("build", "run"))
     depends_on("ccache", type=("build", "run"), when="@5.018:")
 
