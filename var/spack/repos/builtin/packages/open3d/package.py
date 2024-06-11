ddcf1a4b2e2712e264532779413650fe9bb187b8
AcriusWinter <152348900+AcriusWinter@users.noreply.github.com>
2024-06-11 12:22:50 -0700

    Renamed build-time test method (#44572)
    * libdistributed: renamed test to check_test
    * libpressio-rmetric: renamed test method
    * libpressio-tools: renamed test method
    * Renamed build-time test methods for: libpressio-opt, libpressio-tthresh, opened
    
    ---------
    
    Co-authored-by: Jackson Peter Lawrence <lawrence31@borax5.llnl.gov>

--- var/spack/repos/builtin/packages/open3d/package.py
+++ var/spack/repos/builtin/packages/open3d/package.py
@@ -116,7 +116,7 @@ class Open3d(CMakePackage, CudaPackage):
 
     @run_after("install")
     @on_package_attributes(run_tests=True)
-    def test(self):
+    def check_import(self):
         if "+python" in self.spec:
             self.run_test(
                 python.path,
