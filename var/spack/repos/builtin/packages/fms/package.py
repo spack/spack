26d48cdf910d78a2cd7298ef218fb19d08e58a5a
Matthew Thompson <matthew.thompson@nasa.gov>
2024-06-11 10:08:04 -0400

    clean up comment for spack style

--- var/spack/repos/builtin/packages/fms/package.py
+++ var/spack/repos/builtin/packages/fms/package.py
@@ -104,8 +104,6 @@ class Fms(CMakePackage):
     depends_on("netcdf-fortran")
     depends_on("mpi")
     depends_on("libyaml", when="+yaml")
-
-    # when using apple-clang version 15.x or newer, need to use the llvm-openmp library when +openmp
     depends_on("llvm-openmp", when="+openmp %apple-clang@15:", type=("build", "run"))
 
     def cmake_args(self):
