0e17be786a0d85d3beb38aac9f875ad1f8c60330
Jackson Lawrence <jlawrence8736@gmail.com>
2024-06-13 11:38:40 -0700

    Renamed vtk-m test methods and changed how tests that dont apply to build are skipped from old to new method

--- var/spack/repos/builtin/packages/vtk-m/package.py
+++ var/spack/repos/builtin/packages/vtk-m/package.py
@@ -227,13 +227,11 @@ class VtkM(CMakePackage, CudaPackage, ROCmPackage):
         return options
 
     # Delegate in the vtk-m built smoke test
-    def smoke_test(self):
+    def test_smoke_test(self):
         spec = self.spec
 
         if "+examples" not in spec:
-            raise RuntimeError(
-                "Examples needed for smoke test missing", "reinstall with `+examples` variant"
-            )
+            raise SkipTest("Package must be installed with 'examples'")
 
         testdir = "smoke_test_build"
         with working_dir(testdir, create=True):
@@ -248,7 +246,7 @@ class VtkM(CMakePackage, CudaPackage, ROCmPackage):
     @run_after("install")
     @on_package_attributes(run_tests=True)
     def build_test(self):
-        self.smoke_test()
+        self.test_smoke_test()
 
-    def test(self):
-        self.smoke_test()
+    def test_build(self):
+        self.test_smoke_test()
