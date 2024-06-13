93e64f04daa0d284d280eea90c737b65d7548de7
lucasrpatten <68756367+lucasrpatten@users.noreply.github.com>
2024-06-13 10:43:42 -0600

    Updated LAMMPS Package File
    Update LAMMPS Package File so that lammps can build successfully with the +kokkos and +cuda flags enabled

--- var/spack/repos/builtin/packages/lammps/package.py
+++ var/spack/repos/builtin/packages/lammps/package.py
@@ -656,7 +656,10 @@ class Lammps(CMakePackage, CudaPackage, ROCmPackage, PythonExtension):
 
     # propagate CUDA and ROCm architecture when +kokkos
     for arch in CudaPackage.cuda_arch_values:
-        depends_on("kokkos+cuda cuda_arch=%s" % arch, when="+kokkos+cuda cuda_arch=%s" % arch)
+        depends_on(
+            "kokkos+cuda+wrapper cuda_arch=%s" % arch, 
+            when="+kokkos+cuda cuda_arch=%s" % arch
+        )
 
     for arch in ROCmPackage.amdgpu_targets:
         depends_on(
