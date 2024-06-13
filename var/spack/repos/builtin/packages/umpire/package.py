fa64389fd3f3c9b9eea84ff32b682f3533ddd2e3
Adrien Bernede <51493078+adrienbernede@users.noreply.github.com>
2024-06-13 18:31:38 +0200

    Add latest releases to camp, raja and umpire (#44699)
    * Add latest releases to camp, raja and umpire

--- var/spack/repos/builtin/packages/umpire/package.py
+++ var/spack/repos/builtin/packages/umpire/package.py
@@ -21,11 +21,17 @@ class Umpire(CachedCMakePackage, CudaPackage, ROCmPackage):
     git = "https://github.com/LLNL/Umpire.git"
     tags = ["radiuss", "e4s"]
 
-    maintainers("davidbeckingsale")
+    maintainers("davidbeckingsale", "adrienbernede")
 
     license("MIT")
 
     version("develop", branch="develop", submodules=False)
+    version(
+        "2024.02.1",
+        tag="v2024.02.1",
+        commit="3058d562fc707650e904f9321b1ee9bcebad3ae2",
+        submodules=False,
+    )
     version(
         "2024.02.0",
         tag="v2024.02.0",
