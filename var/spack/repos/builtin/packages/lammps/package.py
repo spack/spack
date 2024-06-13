b96b065693c7c7f5d62b10a27c0f3ae5bcaeb8c3
lucasrpatten <68756367+lucasrpatten@users.noreply.github.com>
2024-06-13 10:48:40 -0600

    Removed Trailing Space

--- var/spack/repos/builtin/packages/lammps/package.py
+++ var/spack/repos/builtin/packages/lammps/package.py
@@ -657,7 +657,7 @@ class Lammps(CMakePackage, CudaPackage, ROCmPackage, PythonExtension):
     # propagate CUDA and ROCm architecture when +kokkos
     for arch in CudaPackage.cuda_arch_values:
         depends_on(
-            "kokkos+cuda+wrapper cuda_arch=%s" % arch, 
+            "kokkos+cuda+wrapper cuda_arch=%s" % arch,
             when="+kokkos+cuda cuda_arch=%s" % arch
         )
 
