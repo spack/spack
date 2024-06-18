21c0b95803690c3474c91a8a159785ba86520b17
Alec Scott <scott112@llnl.gov>
2024-06-17 22:45:29 -0700

    rclone: shrink completion instructions

--- var/spack/repos/builtin/packages/rclone/package.py
+++ var/spack/repos/builtin/packages/rclone/package.py
@@ -64,10 +64,6 @@ class Rclone(Package):
         mkdirp(self.fish_completion_path)
         mkdirp(self.zsh_completion_path)
 
-        rclone("genautocomplete", "bash", "rclone.bash")
-        rclone("genautocomplete", "fish", "rclone.fish")
-        rclone("genautocomplete", "zsh", "_rclone")
-
-        install("rclone.bash", self.bash_completion_path)
-        install("rclone.fish", self.fish_completion_path)
-        install("_rclone", self.zsh_completion_path)
+        rclone("genautocomplete", "bash", join_path(self.bash_completion_path, "rclone.bash"))
+        rclone("genautocomplete", "fish", join_path(self.fish_completion_path, "rclone.fish"))
+        rclone("genautocomplete", "zsh", join_path(self.zsh_completion_path, "_rclone"))
