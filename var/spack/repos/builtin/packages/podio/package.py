dc81b74241025d1e22fc124540e81195e102f939
tmadlener <thomas.madlener@desy.de>
2024-06-20 09:48:08 +0200

    Update version range for python extension

--- var/spack/repos/builtin/packages/podio/package.py
+++ var/spack/repos/builtin/packages/podio/package.py
@@ -97,7 +97,7 @@ class Podio(CMakePackage):
     conflicts("+rntuple", when="@:0.16", msg="rntuple support requires at least podio@0.17")
 
     # See https://github.com/AIDASoft/podio/pull/599 that landed after 0.99
-    extends("python", when="@0.99.1:")
+    extends("python", when="@1.0:")
 
     def cmake_args(self):
         args = [
