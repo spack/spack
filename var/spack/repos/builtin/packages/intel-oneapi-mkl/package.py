e531d0574593095b9291210a0a9adb04a22ee79d
Robert Cohn <rscohn2@gmail.com>
2024-06-21 13:05:08 -0400

    remove preference for 2024.0 mkl

--- var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-mkl/package.py
@@ -42,7 +42,6 @@ class IntelOneapiMkl(IntelOneApiLibraryPackage):
         url="https://registrationcenter-download.intel.com/akdlm//IRC_NAS/86d6a4c1-c998-4c6b-9fff-ca004e9f7455/l_onemkl_p_2024.0.0.49673_offline.sh",
         sha256="2a3be7d01d75ba8cc3059f9a32ae72e5bfc93e68e72e94e79d7fa6ea2f7814de",
         expand=False,
-        preferred=True,
     )
     version(
         "2023.2.0",
