77d1d9635de566a56ef8a3e68e271f65569f277d
Teague Sterling <teaguesterling@users.noreply.github.com>
2024-06-22 16:34:39 -0700

    Update package.py

--- var/spack/repos/builtin/packages/novnc/package.py
+++ var/spack/repos/builtin/packages/novnc/package.py
@@ -2,26 +2,7 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 from spack.package import *
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install novnc
-#
-# You can edit this file again by typing:
-#
-#     spack edit novnc
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-# from spack.package import *
 
 
 class Novnc(Package):
