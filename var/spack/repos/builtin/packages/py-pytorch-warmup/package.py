dd9986cfea2d1c3f40598c3f52238146e7478a82
Alex C Leute <acl2809@rit.edu>
2024-06-26 11:10:58 -0400

    py-clip-anytorch: ran black
    py-langchain-core: ran black
    
    py-pydantic: ran black
    
    py-dalle2-pytorch: ran black

--- var/spack/repos/builtin/packages/py-pytorch-warmup/package.py
+++ var/spack/repos/builtin/packages/py-pytorch-warmup/package.py
@@ -15,7 +15,6 @@ class PyPytorchWarmup(PythonPackage):
     homepage = "https://github.com/Tony-Y/pytorch_warmup"
     pypi = "pytorch-warmup/pytorch-warmup-0.1.1.tar.gz"
 
-
     license("MIT", checked_by="alex391")
 
     version("0.1.1", sha256="c594760b29657a127aa6a8c3424dd0b5068140b3b7d4988118f4a9f3e99b1457")
