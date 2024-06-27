079efbabb2de71cea901b8fc341bd893c657010d
Renjith Ravindran Kannath <Renjith.RavindranKannath@amd.com>
2024-06-26 21:59:41 -0700

    Removing version check of deprecated versions

--- var/spack/repos/builtin/packages/rocm-cmake/package.py
+++ var/spack/repos/builtin/packages/rocm-cmake/package.py
@@ -61,14 +61,9 @@ class RocmCmake(CMakePackage):
     def cache_test_sources(self):
         """Copy the tests source files after the package is installed to an
         install test subdirectory for use during `spack test run`."""
-        if self.spec.satisfies("@:5.1.0"):
-            return
         self.cache_extra_test_sources([self.test_src_dir])
 
     def test(self):
-        if self.spec.satisfies("@:5.1.0"):
-            print("Skipping: stand-alone tests")
-            return
         test_dir = join_path(self.test_suite.current_test_cache_dir, self.test_src_dir)
         with working_dir(test_dir, create=True):
             cmake_bin = join_path(self.spec["cmake"].prefix.bin, "cmake")
