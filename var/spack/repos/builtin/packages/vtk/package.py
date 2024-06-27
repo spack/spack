2c251b9b5fe93b6e4cf6078b726924a0a56bc5ed
Chris Marsh <chrismarsh.c2@gmail.com>
2024-06-27 13:00:56 -0600

    vtk: Fix proj dependency ranges  (#44370)
    * Improve proj version selection to avoid too new of a proj with older api usage in vtk9
    
    * Proj constraint range around 8.2.1
    
    
    ---------
    
    Co-authored-by: John Parent <john.parent@kitware.com>

--- var/spack/repos/builtin/packages/vtk/package.py
+++ var/spack/repos/builtin/packages/vtk/package.py
@@ -171,8 +171,9 @@ class Vtk(CMakePackage):
     depends_on("utf8cpp", when="@9:")
     depends_on("gl2ps", when="@8.1:")
     depends_on("gl2ps@1.4.1:", when="@9:")
-    depends_on("proj@4", when="@8.2.0")
-    depends_on("proj@4:", when="@9:")
+    # "8.2.1a" uses an internal proj so this special cases 8.2.1a
+    depends_on("proj@4:7", when="@:8.2.0, 9:9.1")
+    depends_on("proj@8:", when="@9.2:")
     depends_on("cgns@4.1.1:+mpi", when="@9.1: +mpi")
     depends_on("cgns@4.1.1:~mpi", when="@9.1: ~mpi")
     with when("@9.1:"):
