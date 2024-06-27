762886284c242a3364c132052a770e9f2a9b3d63
Thomas Madlener <thomas.madlener@desy.de>
2024-06-27 12:55:40 +0200

    podio: add version 1.0.1 (#44854)

--- var/spack/repos/builtin/packages/podio/package.py
+++ var/spack/repos/builtin/packages/podio/package.py
@@ -20,6 +20,7 @@ class Podio(CMakePackage):
     tags = ["hep", "key4hep"]
 
     version("master", branch="master")
+    version("1.0.1", sha256="915531a2bcf638011bb6cc19715bbc46d846ec8b985555a1afdcd6abc017e21b")
     version("1.0", sha256="491f335e148708e387e90e955a6150e1fc2e01bf6b4980b65e257ab0619559a9")
     version("0.99", sha256="c823918a6ec1365d316e0a753feb9d492e28903141dd124a1be06efac7c1877a")
     version(
