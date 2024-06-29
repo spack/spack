4e6c6a75b37462744380ed63e682e4b0a4894fa3
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2024-06-29 15:54:51 -0400

    vtk: add 9.3.1 version

--- var/spack/repos/builtin/packages/vtk/package.py
+++ var/spack/repos/builtin/packages/vtk/package.py
@@ -24,6 +24,11 @@ class Vtk(CMakePackage):
 
     license("BSD-3-Clause")
 
+    version(
+        "9.3.1",
+        sha256="8354ec084ea0d2dc3d23dbe4243823c4bfc270382d0ce8d658939fd50061cab8",
+        preferred=True,
+    )
     version("9.2.6", sha256="06fc8d49c4e56f498c40fcb38a563ed8d4ec31358d0101e8988f0bb4d539dd12")
     version("9.2.2", sha256="1c5b0a2be71fac96ff4831af69e350f7a0ea3168981f790c000709dcf9121075")
     version("9.1.0", sha256="8fed42f4f8f1eb8083107b68eaa9ad71da07110161a3116ad807f43e5ca5ce96")
@@ -278,6 +283,7 @@ class Vtk(CMakePackage):
             cmake_args.extend(
                 [
                     "-DVTK_USE_EXTERNAL:BOOL=ON",
+                    "-DVTK_MODULE_USE_EXTERNAL_VTK_fast_float:BOOL=OFF",
                     "-DVTK_MODULE_USE_EXTERNAL_VTK_libharu:BOOL=OFF",
                     "-DVTK_MODULE_USE_EXTERNAL_VTK_pegtl:BOOL=OFF",
                     "-DHDF5_ROOT={0}".format(spec["hdf5"].prefix),
