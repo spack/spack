f3c62e8e7231fb22d42a7b7022ad405ce3bf5a5c
Jackson Lawrence <jlawrence8736@gmail.com>
2024-07-03 08:42:13 -0700

    remove kokkos

--- var/spack/repos/builtin/packages/pinentry/package.py
+++ var/spack/repos/builtin/packages/pinentry/package.py
@@ -6,7 +6,6 @@
 
 from spack.package import *
 
-
 class Pinentry(AutotoolsPackage):
     """pinentry is a small collection of dialog programs that allow GnuPG to
     read passphrases and PIN numbers in a secure manner.
