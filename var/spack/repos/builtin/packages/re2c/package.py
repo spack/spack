97a23ae50e741b6850517b76ab46de89100f3d2c
Carsten Uphoff <carsten.uphoff@intel.com>
2024-07-04 03:08:45 +0200

    tiny-tensor-compiler: fix missing dependencies (#44465)
    * tiny-tensor-compiler: fix missing dependencies
    * tiny-tensor-compiler: Remove dependency type for opencl c headers
    * Re2c@3.1 has a mandatory Python interpreter dependency
    * Remove re2c@3.1
    
    ---------
    
    Signed-off-by: Carsten Uphoff <carsten.uphoff@intel.com>

--- var/spack/repos/builtin/packages/re2c/package.py
+++ var/spack/repos/builtin/packages/re2c/package.py
@@ -17,6 +17,10 @@ class Re2c(Package):
 
     license("Public-Domain")
 
+    # Python 3.7 is required for re2c@3.1, but the clingo bootstrap on OpenSUSE Leap uses
+    # the system Python 3.6 such that the bootstrap fails with an UnsatisfiableVersionSpecError
+    # version("3.1", sha256="0ac299ad359e3f512b06a99397d025cfff81d3be34464ded0656f8a96676c029")
+    version("3.0", sha256="b3babbbb1461e13fe22c630a40c43885efcfbbbb585830c6f4c0d791cf82ba0b")
     version("2.2", sha256="0fc45e4130a8a555d68e230d1795de0216dfe99096b61b28e67c86dfd7d86bda")
     version("2.1.1", sha256="036ee264fafd5423141ebd628890775aa9447a4c4068a6307385d7366fe711f8")
     version("2.1", sha256="8cba0d95c246c670de8f97f57def83a9c0f2113eaa6f7e4867a941f48f633540")
@@ -33,6 +37,7 @@ class Re2c(Package):
     depends_on("gmake", when="platform=linux")
     depends_on("gmake", when="platform=darwin")
     depends_on("gmake", when="platform=freebsd")
+    # depends_on("python@3:", when="@3.1:", type="build")
 
     @property
     def make_tool(self):
