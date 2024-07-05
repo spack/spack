8301a971314880ec84a63ae1fe88de36a77d3270
aurianer <aurianer@cscs.ch>
2024-07-05 11:58:11 +0200

    Add latest releases of py-flash-attn

--- var/spack/repos/builtin/packages/py-flash-attn/package.py
+++ var/spack/repos/builtin/packages/py-flash-attn/package.py
@@ -11,12 +11,18 @@ class PyFlashAttn(PythonPackage):
     This package provides the official implementation of FlashAttention.
     """
 
-    pypi = "flash-attn/flash_attn-2.5.4.tar.gz"
+    pypi = "flash-attn/flash_attn-0.0.0.tar.gz"
 
     maintainers("aurianer")
 
     license("BSD")
 
+    version(
+        "2.5.9.post1", sha256="a92db1683a5b141a0f4371d251ae9f73e9aef629b3a58a50d0ef430266c68782"
+    )
+    version("2.5.8", sha256="2e5b2bcff6d5cff40d494af91ecd1eb3c5b4520a6ce7a0a8b1f9c1ed129fb402")
+    version("2.5.7", sha256="7c079aef4e77c4e9a71a3cd88662362e0fe82f658db0b2dbff6f279de2a387a8")
+    version("2.5.6", sha256="d25801aa060877cad997939bd7130faf620fdbeda947c3ffde5865906d430c36")
     version("2.5.5", sha256="751cee17711d006fe7341cdd78584af86a6239afcfe43b9ed11c84db93126267")
     version("2.5.4", sha256="d83bb427b517b07e9db655f6e5166eb2607dccf4d6ca3229e3a3528c206b0175")
     version("2.4.2", sha256="eb822a8c4219b610e9d734cbc8cd9ee4547f27433815a2b90dc1462766feefc1")
