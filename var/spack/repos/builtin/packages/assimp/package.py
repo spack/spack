38648e86957562506907587c984b563591e490dc
Wouter Deconinck <wdconinc@gmail.com>
2024-07-06 14:11:55 -0500

    assimp: add v5.4.2

--- var/spack/repos/builtin/packages/assimp/package.py
+++ var/spack/repos/builtin/packages/assimp/package.py
@@ -19,6 +19,7 @@ class Assimp(CMakePackage):
     license("BSD-3-Clause")
 
     version("master", branch="master")
+    version("5.4.2", sha256="7414861a7b038e407b510e8b8c9e58d5bf8ca76c9dfe07a01d20af388ec5086a") 
     version("5.4.0", sha256="a90f77b0269addb2f381b00c09ad47710f2aab6b1d904f5e9a29953c30104d3f")
     version("5.3.1", sha256="a07666be71afe1ad4bc008c2336b7c688aca391271188eb9108d0c6db1be53f1")
     version("5.2.5", sha256="b5219e63ae31d895d60d98001ee5bb809fb2c7b2de1e7f78ceeb600063641e1a")
