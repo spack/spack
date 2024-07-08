7790a933646575f0db9dfa7b2c6623c377a453db
Auriane R <48684432+aurianer@users.noreply.github.com>
2024-07-09 00:21:14 +0200

    [py-datasets] Add py-datasets version 2.20.0 (#44903)
    * Add py-datasets version 2.20.0
    
    * Add dependency requirements for version 2.20 + refactor
    
    * Add missing tqdm and requests versions and to install latest py-datasets
    
    * Add missing python requirements for 2.8.0 and 2.20.0

--- var/spack/repos/builtin/packages/py-requests/package.py
+++ var/spack/repos/builtin/packages/py-requests/package.py
@@ -15,6 +15,7 @@ class PyRequests(PythonPackage):
 
     license("Apache-2.0")
 
+    version("2.32.2", sha256="dd951ff5ecf3e3b3aa26b40703ba77495dab41da839ae72ef3c8e5d8e2433289")
     version("2.31.0", sha256="942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1")
     version("2.28.2", sha256="98b1b2782e3c6c4904938b84c0eb932721069dfdb9134313beff7c83c2df24bf")
     version("2.28.1", sha256="7c5599b102feddaa661c826c56ab4fee28bfd17f5abca1ebbe3e7f19d7c97983")
