699ed105f84717ea3f0dfaa5b6aace2d7e840dae
Wouter Deconinck <wdconinc@gmail.com>
2024-07-08 10:18:08 -0500

    xrootd: new variant +ec, depends_on isa-l

--- var/spack/repos/builtin/packages/xrootd/package.py
+++ var/spack/repos/builtin/packages/xrootd/package.py
@@ -65,6 +65,7 @@ class Xrootd(CMakePackage):
     version("4.3.0", sha256="d34865772d975b5d58ad80bb05312bf49aaf124d5431e54dc8618c05a0870e3c")
 
     variant("davix", default=True, description="Build with Davix")
+    variant("ec", default=True, description="Build with erasure coding component support", when="@5.7.0:")
     variant("http", default=True, description="Build with HTTP support")
     variant("krb5", default=False, description="Build with KRB5 support")
     variant("python", default=False, description="Build pyxroot Python extension")
@@ -131,6 +132,7 @@ class Xrootd(CMakePackage):
     conflicts("^cmake@:3.0", when="@5.0.0")
     conflicts("^cmake@:3.15.99", when="@5.5.4:5.5")
     depends_on("davix", when="+davix")
+    depends_on("isa-l", when="+ec")
     depends_on("pkgconfig", type="build", when="+davix")
     depends_on("libxml2", when="+http")
     depends_on("uuid", when="@4.11.0:")
@@ -213,6 +215,7 @@ class Xrootd(CMakePackage):
             define_from_variant("ENABLE_READLINE", "readline"),
             define_from_variant("ENABLE_KRB5", "krb5"),
             define_from_variant("ENABLE_SCITOKENS", "scitokens-cpp"),
+            define_from_variant("ENABLE_XRDEC", "ec"),
             define_from_variant("XRDCL_ONLY", "client_only"),
             define("ENABLE_CEPH", False),
             define("ENABLE_CRYPTO", True),
@@ -220,6 +223,7 @@ class Xrootd(CMakePackage):
             define("ENABLE_MACAROONS", False),
             define("ENABLE_VOMS", False),
             define("FORCE_ENABLED", True),
+            define("USE_SYSTEM_ISAL", True),
         ]
         # see https://github.com/spack/spack/pull/11581
         if "+python" in self.spec:
