6bb1b4e5af99cd908e3696b1cfbb737f842069b1
Ryan Krattiger <ryan.krattiger@kitware.com>
2024-07-09 10:49:37 -0500

    Add tests for include --concrete

--- lib/spack/spack/test/cmd/include.py
+++ lib/spack/spack/test/cmd/include.py
@@ -19,9 +19,10 @@ pytestmark = [
 env = SpackCommand("env")
 add = SpackCommand("add")
 include = SpackCommand("include")
+concretize = SpackCommand("concretize")
 
 
-def test_include_add_configs(tmp_path):
+def test_include_config_add(tmp_path):
     """
     Test adding valid and invalid includes to an environment
     and the resulting ordering of includes is correct.
@@ -33,21 +34,24 @@ def test_include_add_configs(tmp_path):
     package_config_f = """
 packages:
   {pkg}:
+    buildable: False
     externals:
     - spec: {spec}
-      prefix: /usr
+      prefix: {prefix}
 """
 
     for idx, spec in enumerate(["abseil-cpp@20240116.2", "libelf@0.8.10", "zlib@1.3.1"]):
         (config_dir / f"{idx}").mkdir(parents=True, exist_ok=False)
         with open(config_dir / f"{idx}" / "packages.yaml", "w") as fd:
-            fd.write(package_config_f.format(pkg=spec.split("@")[0], spec=spec))
+            fd.write(
+                package_config_f.format(pkg=spec.split("@")[0], spec=spec, prefix=str(tmp_path))
+            )
 
     # Create a test environment
     env("create", "-d", str(config_dir / "test"))
     test = ev.read(str(config_dir / "test"))
     with test:
-        add("libelf")
+        add("libelf@0.8.10")
         # Test inserting multiple includes
         out = include("add", str(config_dir / "0"), str(config_dir / "1"))
         assert "Adding includes" in out
@@ -74,16 +78,8 @@ packages:
     assert str(config_dir / "1") in test.manifest.pristine_configuration["include"][1]
     assert str(config_dir / "0") in test.manifest.pristine_configuration["include"][2]
 
-    test.concretize()
-    hashes = test.concretized_order
-    assert len(hashes) == 1
-    spec = test.specs_by_hash[hashes[0]]
-    # Make sure we are getting the included external
-    assert spec.external()
-    assert spec.satisfies("libelf@0.8.10")
 
-
-def test_include_remove_configs(tmp_path):
+def test_include_config_remove(tmp_path):
     """Test removing includes from an activate environment"""
     config_dir = tmp_path / "configs"
     config_dir.mkdir(parents=True, exist_ok=False)
@@ -117,7 +113,7 @@ spack:
     assert str(config_dir / "2") in test.manifest.pristine_configuration["include"][0]
 
 
-def test_include_list_configs(tmp_path):
+def test_include_config_list(tmp_path):
     """Test listing includes in an active environment"""
 
     config_dir = tmp_path / "configs"
@@ -141,6 +137,135 @@ spack:
     with test:
         # use --expand to get the full path
         out = include("list", "--expand")
-        assert "==> Included Configuration Scopes" in out
         assert str(config_dir / "0") in out
         assert str(config_dir / "2") in out
+
+
+def test_include_concrete_add(tmp_path):
+    """Test listing includes in an active environment"""
+
+    test_env_template = """
+spack:
+    specs:
+    - {0}
+"""
+
+    # Create a B environment
+    env_dir = tmp_path / "env"
+    env_dir.mkdir(parents=True, exist_ok=False)
+
+    def create_test_env(name, config):
+        (env_dir / name).mkdir(parents=True, exist_ok=False)
+        with open(str(env_dir / name / "spack.yaml"), "w") as fd:
+            fd.write(config)
+
+        e = ev.read(str(env_dir / name))
+        with e:
+            concretize()
+
+        e = ev.read(str(env_dir / name))
+        return e.specs_by_hash, e
+
+    bspecs, _ = create_test_env("B", test_env_template.format("b"))
+    especs, _ = create_test_env("E", test_env_template.format("e"))
+
+    # Create a A environment that includes B and E
+    _, testa = create_test_env("A", test_env_template.format("a"))
+    with testa:
+        include("--concrete", "add", str(env_dir / "B"))
+        include("--concrete", "add", str(env_dir / "E"))
+        concretize("-f")
+
+    testa = ev.read(str(env_dir / "A"))
+    assert str(env_dir / "B") in testa.manifest.pristine_configuration["include_concrete"]
+    assert str(env_dir / "E") in testa.manifest.pristine_configuration["include_concrete"]
+    # TODO: Add checks for reuse of included concrete specs once that works
+
+
+def test_include_concrete_remove(tmp_path):
+    """Test listing includes in an active environment"""
+
+    test_env_template = """
+spack:
+    specs:
+    - {0}
+"""
+
+    # Create a B environment
+    env_dir = tmp_path / "env"
+    env_dir.mkdir(parents=True, exist_ok=False)
+
+    def create_test_env(name, config):
+        (env_dir / name).mkdir(parents=True, exist_ok=False)
+        with open(str(env_dir / name / "spack.yaml"), "w") as fd:
+            fd.write(config)
+
+        e = ev.read(str(env_dir / name))
+        with e:
+            concretize()
+
+        e = ev.read(str(env_dir / name))
+        return e.specs_by_hash, e
+
+    bspecs, _ = create_test_env("B", test_env_template.format("b"))
+    especs, _ = create_test_env("E", test_env_template.format("e"))
+
+    # Create a A environment that includes B and E
+    _, testa = create_test_env("A", test_env_template.format("a"))
+    with testa:
+        include("--concrete", "add", str(env_dir / "B"))
+        include("--concrete", "add", str(env_dir / "E"))
+        include("--concrete", "remove", str(env_dir / "B"))
+        concretize("-f")
+
+    testa = ev.read(str(env_dir / "A"))
+    assert str(env_dir / "B") not in testa.manifest.pristine_configuration["include_concrete"]
+    assert str(env_dir / "E") in testa.manifest.pristine_configuration["include_concrete"]
+    # TODO: Add checks for reuse of included concrete specs once that works
+
+
+def test_include_concrete_list(tmp_path):
+    """Test listing includes in an active environment"""
+
+    test_env_template = """
+spack:
+    specs:
+    - {0}
+"""
+
+    # Create a B environment
+    env_dir = tmp_path / "env"
+    env_dir.mkdir(parents=True, exist_ok=False)
+
+    # Make a configs dir
+    (env_dir / "configs").mkdir(parents=True, exist_ok=False)
+
+    def create_test_env(name, config):
+        (env_dir / name).mkdir(parents=True, exist_ok=False)
+        with open(str(env_dir / name / "spack.yaml"), "w") as fd:
+            fd.write(config)
+
+        e = ev.read(str(env_dir / name))
+        with e:
+            concretize()
+
+        e = ev.read(str(env_dir / name))
+        return e.specs_by_hash, e
+
+    bspecs, _ = create_test_env("B", test_env_template.format("b"))
+    especs, _ = create_test_env("E", test_env_template.format("e"))
+
+    # Create a A environment that includes B and E
+    _, testa = create_test_env("A", test_env_template.format("a"))
+    with testa:
+        include("--concrete", "add", str(env_dir / "B"))
+        include("--concrete", "add", str(env_dir / "E"))
+        include("add", str(env_dir / "configs"))
+
+        out = include("--concrete", "list", "--expand")
+        # Ensure all of the include concrete dirs are listed
+        assert str(env_dir / "B") in out
+        assert str(env_dir / "E") in out
+
+        # Ensure the include configs dir is not listed
+        assert str(env_dir / "configs") not in out
