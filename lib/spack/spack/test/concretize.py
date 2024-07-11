403d0722bffa89ef7a348f3f15c43428d08eb973
psakiev <psakiev@sandia.gov>
2024-07-11 15:30:34 -0600

    Update fixtures

--- lib/spack/spack/test/concretize.py
+++ lib/spack/spack/test/concretize.py
@@ -3129,7 +3129,7 @@ def test_versions_with_custom_git_ref_based_versions_pin_to_commits(
 def test_git_ref_version_can_be_reused(
     input_version,
     mock_git_version_info,
-    install_mockery_mutable_config,
+    install_mockery,
     do_not_check_runtimes_on_reuse,
     monkeypatch,
 ):
@@ -3155,7 +3155,7 @@ def test_git_ref_version_can_be_reused(
 @pytest.mark.only_clingo("clingo only reuse feature being tested")
 @pytest.mark.parametrize("standard_version", ["2.0.0", "2.1.5", "2.1.6"])
 def test_reuse_prefers_standard_over_git_versions(
-    standard_version, install_mockery_mutable_config, do_not_check_runtimes_on_reuse, monkeypatch
+    standard_version, install_mockery, do_not_check_runtimes_on_reuse, monkeypatch
 ):
     """
     order matters in this test. typically reuse would pick the highest versioned installed match
