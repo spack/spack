be80745fe72b4539bb9bad4ac94f69384807dbf5
Nicole Cheetham <nicole.cheetham33@gmail.com>
2024-07-15 17:55:59 -0400

    Update list test to agree with new depb docstring

--- lib/spack/spack/test/cmd/list.py
+++ lib/spack/spack/test/cmd/list.py
@@ -58,7 +58,7 @@ def test_list_filter(mock_packages):
 
 
 def test_list_search_description(mock_packages):
-    output = list("--search-description", "one build dependency")
+    output = list("--search-description", "one dependency")
     assert "depb" in output
 
 
