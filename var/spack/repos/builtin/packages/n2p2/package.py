321aabf8bb02c168c11817c105ec709e96eac21c
Jackson Lawrence <jlawrence8736@gmail.com>
2024-07-15 17:05:31 -0700

    progress

--- var/spack/repos/builtin/packages/n2p2/package.py
+++ var/spack/repos/builtin/packages/n2p2/package.py
@@ -57,16 +57,15 @@ class N2p2(MakefilePackage):
 
     def edit(self, spec, prefix):
         makefile = FileFilter(join_path("src", "makefile"))
-        makefile.filter("MODE=.*", "MODE={0}".format("shared" if "+shared" in spec else "static"))
+        makefile.filter("MODE=.*", f"MODE={'shared' if '+shared' in spec else 'static'}")
 
         makefile = FileFilter(join_path("src", "makefile.gnu"))
         blas_libs = self.spec["blas"].libs
-        makefile.filter("PROJECT_CC=.*", "PROJECT_CC={0}".format(spack_cxx))
-        makefile.filter("PROJECT_MPICC=.*", "PROJECT_MPICC={0}".format(self.spec["mpi"].mpicxx))
-        makefile.filter("PROJECT_CFLAGS=.*", "PROJECT_CFLAGS={0}".format(self.compiler.cxx11_flag))
+        makefile.filter("PROJECT_CC=.*", f"PROJECT_CC={spack_cxx}")
+        makefile.filter("PROJECT_MPICC=.*", f"PROJECT_MPICC={self.spec['mpi'].mpicxx}")
+        makefile.filter("PROJECT_CFLAGS=.*", f"PROJECT_CFLAGS={self.compiler.cxx11_flag}")
         makefile.filter(
-            "PROJECT_LDFLAGS_BLAS.*",
-            "PROJECT_LDFLAGS_BLAS={0} -lgsl -lgslcblas".format(blas_libs.ld_flags),
+            "PROJECT_LDFLAGS_BLAS.*", f"PROJECT_LDFLAGS_BLAS={blas_libs.ld_flags} -lgsl -lgslcblas"
         )
 
     def build(self, spec, prefix):
@@ -92,7 +91,7 @@ class N2p2(MakefilePackage):
     def setup_build_tests(self):
         """Copy the build test files after the package is installed to an
         install test subdirectory for use during `spack test run`."""
-        self.cache_extra_test_sources(".")
+        cache_extra_test_sources(self)
 
     def test_n2p2(self):
         """Run benchmark tests"""
@@ -103,25 +102,22 @@ class N2p2(MakefilePackage):
             make("clean")
             make(
                 "MODE=test",
-                "PROJECT_GSL={0}".format(self.spec["gsl"].prefix.include),
-                "PROJECT_EIGEN={0}".format(self.spec["eigen"].prefix.include.eigen3),
+                f"PROJECT_GSL={self.spec['gsl'].prefix.include}",
+                f"PROJECT_EIGEN={self.spec['eigen'].prefix.include.eigen3}",
             )
             make(
                 "MODE=test",
                 "lammps-nnp",
-                "PROJECT_GSL={0}".format(self.spec["gsl"].prefix.include),
-                "PROJECT_EIGEN={0}".format(self.spec["eigen"].prefix.include.eigen3),
+                f"PROJECT_GSL={self.spec['gsl'].prefix.include}",
+                f"PROJECT_EIGEN={self.spec['eigen'].prefix.include.eigen3}",
             )
             make("pynnp", "MODE=test")
 
         with working_dir(join_path(self.install_test_root, "test"), create=False):
             if self.spec.satisfies("%fj"):
                 f = FileFilter(join_path("cpp", "nnp_test.h"))
-                f.filter(
-                    "(example.co",
-                    '("{0} -n 1 " + example.co'.format(self.spec["mpi"].prefix.bin.mpirun),
-                    string=True,
-                )
+                mpirun = self.spec["mpi"].prefix.bin.mpirun
+                f.filter("(example.co", f'("{mpirun} -n 1 " + example.co', string=True)
 
             f = FileFilter(join_path("cpp", "makefile"))
             f.filter("log_level=.*", "log_level=$(LOG_LEVEL) 2>&1 | tee -a ../output_cpp.txt")
@@ -133,6 +129,6 @@ class N2p2(MakefilePackage):
             make("python", parallel=False)
 
             test_dir = self.test_suite.current_test_data_dir
-            expected_file = join_path(test_dir, "expected-result-{0}.txt".format(self.version))
+            expected_file = join_path(test_dir, f"expected-result-{self.version}.txt")
             check_n2p2 = Executable(join_path(test_dir, "result-check.sh"))
             check_n2p2("./output_cpp.txt", "./output_python.txt", expected_file)
