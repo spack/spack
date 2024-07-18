c616036d81396441ec85df96fac9e8bc227ba2cf
Brendan Moloney <moloney.brendan@gmail.com>
2024-07-18 14:53:19 -0700

    Make method of disabling libomp CUDA plugin version specific

--- var/spack/repos/builtin/packages/llvm/package.py
+++ var/spack/repos/builtin/packages/llvm/package.py
@@ -854,15 +854,19 @@ class Llvm(CMakePackage, CudaPackage, CompilerPackage):
                 cmake_args.append(define("LIBOMPTARGET_NVPTX_ENABLE_BCLIB", True))
         else:
             # still build libomptarget but disable cuda
-            cmake_args.extend(
-                [
-                    define("CUDA_TOOLKIT_ROOT_DIR", "IGNORE"),
-                    define("CUDA_SDK_ROOT_DIR", "IGNORE"),
-                    define("CUDA_NVCC_EXECUTABLE", "IGNORE"),
-                    define("LIBOMPTARGET_DEP_CUDA_DRIVER_LIBRARIES", "IGNORE"),
-                    define("LIBOMPTARGET_BUILD_CUDA_PLUGIN", "FALSE"),
-                ]
-            )
+            if spec.satisfies("@:13"):
+                cmake_args.extend(
+                    [
+                        define("CUDA_TOOLKIT_ROOT_DIR", "IGNORE"),
+                        define("CUDA_SDK_ROOT_DIR", "IGNORE"),
+                        define("CUDA_NVCC_EXECUTABLE", "IGNORE"),
+                        define("LIBOMPTARGET_DEP_CUDA_DRIVER_LIBRARIES", "IGNORE"),
+                    ]
+                )
+            elif spec.satisfies("@14:18"):
+                cmake_args += define("LIBOMPTARGET_BUILD_CUDA_PLUGIN", False)
+            elif spec.satisfies("@19:"):
+                cmake_args += define("LIBOMPTARGET_PLUGINS_TO_BUILD", "host")
 
         cmake_args.append(from_variant("LIBOMPTARGET_ENABLE_DEBUG", "libomptarget_debug"))
 
