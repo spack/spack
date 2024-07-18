e3e75f4086dc4e93b518e05e3d88277e25725696
Richard Berger <rberger@lanl.gov>
2024-07-17 23:46:47 -0600

    libmesh: add missing v1.7.1 release

--- var/spack/repos/builtin/packages/libmesh/package.py
+++ var/spack/repos/builtin/packages/libmesh/package.py
@@ -20,6 +20,7 @@ class Libmesh(AutotoolsPackage):
 
     version("master", branch="master", submodules=True)
 
+    version("1.7.1", sha256="0387d62773cf92356eb128ba92f767e56c298d78f4b97446e68bf288da1eb6b4")
     version("1.4.1", sha256="67eb7d5a9c954d891ca1386b70f138333a87a141d9c44213449ca6be69a66414")
     version("1.4.0", sha256="62d7fce89096c950d1b38908484856ea63df57754b64cde6582e7ac407c8c81d")
     version("1.3.1", sha256="638cf30d05c249315760f16cbae4804964db8857a04d5e640f37617bef17ab0f")
