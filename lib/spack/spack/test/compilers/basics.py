0b7e8f97d7b5179133be92ae53a1b1c6205b9d10
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-07-23 14:24:36 +0200

    Fix formatting in lib/spack/spack/test/compilers/basics.py

--- lib/spack/spack/test/compilers/basics.py
+++ lib/spack/spack/test/compilers/basics.py
@@ -967,6 +967,7 @@ def test_compiler_environment(working_env):
     with compiler.compiler_environment():
         assert os.environ["TEST"] == "yes"
 
+
 def test_compiler_environment_always_flags(working_env):
     """Test whether flags that are labeled as "ALWAYS_*FLAGS" are set in the compiler environment"""
     compiler = Compiler(
