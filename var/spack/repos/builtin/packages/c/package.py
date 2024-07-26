43f5c3004bec64fe334607faec9580ee5ad17f9e
Jackson Lawrence <jlawrence8736@gmail.com>
2024-07-26 12:04:55 -0700

    remove run_test

--- var/spack/repos/builtin/packages/c/package.py
+++ var/spack/repos/builtin/packages/c/package.py
@@ -14,17 +14,20 @@ class C(Package):
     homepage = "http://open-std.org/JTC1/SC22/WG14/www/standards"
     virtual = True
 
-    def test_hello_world(self):
+    def test_c(self):
         """Compile and run 'Hello world'"""
         test_source = self.test_suite.current_test_data_dir
 
         for test in os.listdir(test_source):
-            with test_part(self, f"test_hello_world_{test}", f"Test {test}"):
+            with test_part(self, f"test_c_{test}", f"Test {test}"):
                 filepath = test_source.join(test)
                 exe_name = "%s.exe" % test
                 cc_exe = os.environ["CC"]
                 cc_opts = ["-o", exe_name, filepath]
-                compiled = self.run_test(cc_exe, options=cc_opts, installed=True)
+                comp_exe = which(join_path(self.prefix.bin, cc_exe))
+                if comp_exe is None:
+                    raise SkipTest(f"{str(cc_exe)} not found in {self.version}")
+                compiled = cc_exe(*cc_opts)
 
                 if compiled:
                     expected = ["Hello world", "YES!"]
@@ -34,4 +37,4 @@ class C(Package):
                     out = exe(output=str.split, error=str.split)
                     check_outputs(expected, out)
                 else:
-                    assert False, "Did not compile"
+                   assert False, "Did not compile"
