d54f31c7a9461356b7aa2c7de4744dd5c360b64f
Teague Sterling <teaguesterling@gmail.com>
2024-07-28 20:37:39 -0700

    adding rust-toml min rust version
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/rust-toml/package.py
+++ var/spack/repos/builtin/packages/rust-toml/package.py
@@ -14,3 +14,5 @@ class RustToml(CargoPackage):
     license("APACHE-2.0 OR MIT", checked_by="teaguesterling")
 
     version("0.22.16", sha256="8cdd73db2290c389f724605244f057c725e91bfd327521c1e566eaf11bd04a96")
+
+    depends_on("rust@1.65:")
