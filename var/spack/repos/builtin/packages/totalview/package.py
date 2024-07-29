fbc3661a6a7d27327ef0ddd9342fe6b2f2b7cf6b
suzannepaterno <suzanne.paterno@perforce.com>
2024-07-29 12:46:56 -0400

    Update package.py
    Update to pass style test

--- var/spack/repos/builtin/packages/totalview/package.py
+++ var/spack/repos/builtin/packages/totalview/package.py
@@ -20,32 +20,31 @@ class Totalview(Package):
     license_comment = "#"
     license_files = ["tv_license/license.lic"]
     license_vars = ["RLM_LICENSE"]
-    
  
     # As the install of Totalview is via multiple tarballs, the base install
     # will be the documentation.  The architecture-specific tarballs are added
     # as resources dependent on the specific architecture used.
-    
+
     version("2024.2-x86-64",
         md5="1740fe50dbe87e17b48752e27937838a",
         url="https://dslwuu69twiif.cloudfront.net/totalview/2024.2/totalview_2024.2.11_linux_x86-64.tar",
     )
-    
+
     version("2024.2-powerle",
         md5="29d0cf1f764ae1fe85caae092e55eb70",
         url="https://dslwuu69twiif.cloudfront.net/totalview/2024.2/totalview_2024.2.11_linux_powerle.tar",
     )
-    
+
     version("2024.2-linux-arm64",
         md5="91d711f25e3605999fae7ca1290ac949",
         url="https://dslwuu69twiif.cloudfront.net/totalview/2024.2/totalview_2024.2.11_linux_arm64.tar",
     )
-    
+
     version("2024.1-x86-64",
         md5="c406533efaf7decd23ce569a01bd0ee6",
         url="https://dslwuu69twiif.cloudfront.net/totalview/2024.1/totalview_2024.1.21_linux_x86-64.tar",
     )
-    
+
     version("2024.1-powerle",
         md5="443736bef3aa7b4ae83bc441ff6acaf6",
         url="https://dslwuu69twiif.cloudfront.net/totalview/2024.1/totalview_2024.1.21_linux_powerle.tar",
@@ -55,8 +54,6 @@ class Totalview(Package):
         md5="58dff4cb2a74950d56502016b0676ef7",
         url="https://dslwuu69twiif.cloudfront.net/totalview/2024.1/totalview_2024.1.21_linux_arm64.tar",
     )
-    
-
     
 
     def setup_run_environment(self, env):
@@ -70,7 +67,7 @@ class Totalview(Package):
         env.prepend_path("TVDSVRLAUNCHCMD", "ssh")
 
     def install(self, spec, prefix):
-        # Assemble install line
+    # Assemble install line
         install_cmd = which("./Install")
         arg_list = ["-agree", "-nosymlink", "-directory", "{0}".format(prefix)]
 
