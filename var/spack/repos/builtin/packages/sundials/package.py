868794094f1a3ea96362f74ddc57386ddd4d0dab
Jackson Lawrence <jlawrence8736@gmail.com>
2024-07-29 09:20:45 -0700

    get rid of self.install_test_root

--- var/spack/repos/builtin/packages/sundials/package.py
+++ var/spack/repos/builtin/packages/sundials/package.py
@@ -757,7 +757,7 @@ class Sundials(CMakePackage, CudaPackage, ROCmPackage):
     @run_after("install")
     def setup_smoke_tests(self):
         if "+examples-install" in self.spec:
-            install_tree(self._smoke_tests_path, join_path(self.install_test_root, "testing"))
+            install_tree(self._smoke_tests_path, join_path(install_test_root(self), "testing"))
 
     def build_smoke_tests(self):
 
