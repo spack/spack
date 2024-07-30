f21eea36653e463cf611ebca5402d42eb64fdf57
Chris Marsh <chrismarsh.c2@gmail.com>
2024-07-30 11:38:07 -0600

    Mac OS UUID virtual: platform-specific virtuals not correctly prioritized (#43002)
    `apple-libuuid` includes types that aren't available in other `uuid`
    providers; this cause issues in consuming packages (e.g., py-matplotlib)
    that use SDKs like CarbonCore.framework when they attempt to use
    `util-linux-uuid` as a `uuid` provider on Mac OS.
    
    Tweak `util-linux-uuid` to indicate that it does not provide `uuid`
    on Mac OS.

--- var/spack/repos/builtin/packages/libuuid/package.py
+++ var/spack/repos/builtin/packages/libuuid/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import sys
+
 from spack.package import *
 
 
@@ -20,6 +22,7 @@ class Libuuid(AutotoolsPackage, SourceforgePackage):
 
     depends_on("c", type="build")  # generated
 
-    provides("uuid")
+    if sys.platform not in ["darwin", "win32"]:
+        provides("uuid")
 
     conflicts("%gcc@14:")
