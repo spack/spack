b010e3dead85c11af91aa86f3663dd05d731650b
Adam J. Stewart <ajstewart426@gmail.com>
2024-07-30 11:45:31 +0200

    Static ONNX

--- var/spack/repos/builtin/packages/onnx/package.py
+++ var/spack/repos/builtin/packages/onnx/package.py
@@ -48,20 +48,32 @@ class Onnx(CMakePackage):
     version(
         "1.7.0_2020-05-31", commit="a82c6a7010e2e332d8f74ad5b0c726fd47c85376"
     )  # py-torch@1.6:1.7
-    version("1.6.0_2020-02-16", commit="9fdae4c68960a2d44cd1cc871c74a6a9d469fa1f")  # py-torch@1.5
-    version("1.6.0_2019-11-06", commit="fea8568cac61a482ed208748fdc0e1a8e47f62f5")  # py-torch@1.4
-    version("1.6.0_2019-09-26", commit="034921bd574cc84906b7996c07873454b7dd4135")  # py-torch@1.3
-    version("1.5.0_2019-07-25", commit="28ca699b69b5a31892619defca2391044a9a6052")  # py-torch@1.2
-    version("1.5.0_2019-04-25", commit="22662bfd4dcc6baebf29e3b823a051676f991001")  # py-torch@1.1
-    version("1.3.0_2018-12-04", commit="42804705bdbf179d1a98394008417e1392013547")  # py-torch@1.0
     version(
-        "1.2.2_2018-07-16", commit="b2817a682f25f960586f06caa539bbbd7a96b859"
+        "1.6.0_2020-02-16", commit="9fdae4c68960a2d44cd1cc871c74a6a9d469fa1f", deprecated=True
+    )  # py-torch@1.5
+    version(
+        "1.6.0_2019-11-06", commit="fea8568cac61a482ed208748fdc0e1a8e47f62f5", deprecated=True
+    )  # py-torch@1.4
+    version(
+        "1.6.0_2019-09-26", commit="034921bd574cc84906b7996c07873454b7dd4135", deprecated=True
+    )  # py-torch@1.3
+    version(
+        "1.5.0_2019-07-25", commit="28ca699b69b5a31892619defca2391044a9a6052", deprecated=True
+    )  # py-torch@1.2
+    version(
+        "1.5.0_2019-04-25", commit="22662bfd4dcc6baebf29e3b823a051676f991001", deprecated=True
+    )  # py-torch@1.1
+    version(
+        "1.3.0_2018-12-04", commit="42804705bdbf179d1a98394008417e1392013547", deprecated=True
+    )  # py-torch@1.0
+    version(
+        "1.2.2_2018-07-16", commit="b2817a682f25f960586f06caa539bbbd7a96b859", deprecated=True
     )  # py-torch@0.4.1
     version(
-        "1.1.0_2018-04-19", commit="7e1bed51cc508a25b22130de459830b5d5063c41"
+        "1.1.0_2018-04-19", commit="7e1bed51cc508a25b22130de459830b5d5063c41", deprecated=True
     )  # py-torch@0.4.0
 
-    depends_on("cxx", type="build")  # generated
+    depends_on("cxx", type="build")
 
     generator("ninja")
     depends_on("cmake@3.1:", type="build")
@@ -73,4 +85,4 @@ class Onnx(CMakePackage):
             filter_file("CMAKE_CXX_STANDARD 11", "CMAKE_CXX_STANDARD 14", "CMakeLists.txt")
 
     def cmake_args(self):
-        return [self.define("ONNX_ML", True)]
+        return [self.define("ONNX_ML", True), self.define("BUILD_SHARED_LIBS", False)]
