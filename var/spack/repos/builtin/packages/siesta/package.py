0f76a444c680ae5e4186b623dcb6c1af7c811b60
U-PALLAS\boudaoud <boudaoud@pc44.pallas.cines.fr>
2024-07-31 12:38:39 +0200

    Precheck style corrections

--- var/spack/repos/builtin/packages/siesta/package.py
+++ var/spack/repos/builtin/packages/siesta/package.py
@@ -6,8 +6,10 @@
 
 import os
 import shutil
+
+from spack.build_systems import cmake
 from spack.package import *
-from spack.build_systems import cmake, makefile
+
 
 class Siesta(MakefilePackage, CMakePackage):
     """SIESTA performs electronic structure calculations and ab initio molecular
@@ -17,17 +19,23 @@ class Siesta(MakefilePackage, CMakePackage):
     build_system(conditional("cmake", when="@5.0.0:"), "makefile", default="makefile")
 
     homepage = "https://departments.icmab.es/leem/siesta/"
-    git ="https://gitlab.com/siesta-project/siesta"
+    git = "https://gitlab.com/siesta-project/siesta"
 
-    version("5.0.1",url = "https://gitlab.com/siesta-project/siesta/-/archive/5.0.1/siesta-5.0.1.tar.gz",
-            sha256="800a22a831c1d36c6f5fe4aa9c49ee510cbd49a0b2f87b3c8bf3edb6ebd0193a"
+    version(
+        "5.0.1",
+        url="https://gitlab.com/siesta-project/siesta/-/archive/5.0.1/siesta-5.0.1.tar.gz",
+        sha256="800a22a831c1d36c6f5fe4aa9c49ee510cbd49a0b2f87b3c8bf3edb6ebd0193a",
     )
 
-    version("5.0.0",url = "https://gitlab.com/siesta-project/siesta/-/archive/rel-5.0/siesta-rel-5.0.tar.gz",
-            sha256="a407c31e8fd101efe4bfc68831870c524a8c397a39de7271d31ce0e6a4df92bb"
+    version(
+        "5.0.0",
+        url="https://gitlab.com/siesta-project/siesta/-/archive/rel-5.0/siesta-rel-5.0.tar.gz",
+        sha256="a407c31e8fd101efe4bfc68831870c524a8c397a39de7271d31ce0e6a4df92bb",
     )
-    version("4.1.5",url = "https://gitlab.com/siesta-project/siesta/-/archive/v4.1.5/siesta-v4.1.5.tar.gz", 
-            sha256="518df31aa6213af5e24cc73abb537b2c89a925b487171f5339d743d0c7140b3f"
+    version(
+        "4.1.5",
+        url="https://gitlab.com/siesta-project/siesta/-/archive/v4.1.5/siesta-v4.1.5.tar.gz",
+        sha256="518df31aa6213af5e24cc73abb537b2c89a925b487171f5339d743d0c7140b3f",
     )
 
     license("GPL-3.0-or-later")
@@ -47,13 +55,17 @@ class Siesta(MakefilePackage, CMakePackage):
     patch("configure.patch", when="@:4.0")
 
     variant("mpi", default=True, description="Builds with mpi support")
-    variant("openmp",default=True, description="Enables OpenMP support")
+    variant("openmp", default=True, description="Enables OpenMP support")
     variant("netcdf", default=False, description="Compile with Netcdf")
     variant("metis", default=False, description="Activate Metis as a possible ordering library")
     variant("elpa", default=False, description="Use ELPA")
     variant("mumps", default=False, description="Compile with support for MUMPS solvers")
     variant("pexsi", default=False, description="Compile with PEXSI")
-    variant("cray", default=False, description="Enable specific cray settings for using cray-hdf5 and cray-netcdf modulefiles")
+    variant(
+        "cray",
+        default=False,
+        description="Enable specific cray settings for using cray-hdf5 and cray-netcdf modulefiles",
+    )
     variant("debug", default=False, description="Build in debug  mode")
     variant(
         "build_type",
@@ -69,16 +81,14 @@ class Siesta(MakefilePackage, CMakePackage):
     depends_on("scalapack")
     depends_on("netcdf-c")
     depends_on("netcdf-fortran")
-    depends_on("cray-libsci+openmp", when="^cray-libsci")
+    depends_on("cray-libsci+openmp", when="^[virtuals=cray-libsci] cray-libsci")
     depends_on("metis@5:", when="+metis")
     depends_on("elpa", when="+elpa")
     depends_on("mumps", when="+mumps")
     depends_on("pexsi", when="+pexsi")
 
-    #TODO :
     with when("build_system=cmake"):
         depends_on("scalapack", when="+mpi")
-
 
     def flag_handler(self, name, flags):
         if "%gcc@10:" in self.spec and name == "fflags":
@@ -87,7 +97,7 @@ class Siesta(MakefilePackage, CMakePackage):
 
     def edit(self, spec, prefix):
         sh = which("sh")
-        if "+cray" in spec :
+        if "+cray" in spec:
             netcdff_prefix = os.environ.get("NETCDF_DIR", "")
             hdf5_prefix = os.environ.get("HDF5_DIR", "")
         if spec.satisfies("@:4.0.2"):
@@ -104,14 +114,18 @@ class Siesta(MakefilePackage, CMakePackage):
                 # Intel's mpiifort is not found
                 "MPIFC=%s" % spec["mpi"].mpifc,
             ]
-            if "+cray" in spec :
-               configure_args += ["--with-netcdf={0}/lib/libnetcdff.so".format(netcdff_prefix),]
+            if "+cray" in spec:
+	    	configure_args += ["--with-netcdf={0}/lib/libnetcdff.so".format(netcdff_prefix)]
             else:
-                configure_args += ["--with-netcdf=%s" % (spec["netcdf-fortran"].libs + spec["netcdf-c"].libs),]
+                configure_args += [
+                    "--with-netcdf=%s" % (spec["netcdf-fortran"].libs + spec["netcdf-c"].libs)
+                ]
             
             if self.spec.satisfies("%gcc"):
-                if "+cray" in spec :
-                    configure_args.append("FCFLAGS=-ffree-line-length-0 -I{0}/include".format(netcdff_prefix))
+                if "+cray" in spec:
+                    configure_args.append(
+                        "FCFLAGS=-ffree-line-length-0 -I{0}/include".format(netcdff_prefix)
+                    )
                 else:
                     configure_args.append("FCFLAGS=-ffree-line-length-0")
             for d in ["Obj", "Obj_trans"]:
@@ -126,64 +140,89 @@ class Siesta(MakefilePackage, CMakePackage):
 
         elif self.spec.satisfies("@:4.1.5"):
             with working_dir("Obj", create=True):
-                  sh("../Src/obj_setup.sh")
-                  if spec.satisfies("@:4.1.5%gcc"):
-                      shutil.copy("./gfortran.make", "./arch.make")
-                      libs_arg = []
-                      fppflags_arg = []
-                      arch_make = FileFilter("./arch.make")
-                      arch_make.filter("FFLAGS = .*", "FFLAGS = {0}".format('-O2 -fPIC -ftree-vectorize -fallow-argument-mismatch'))
-                      
-                      if "+debug" in spec:
-                            arch_make.filter("FFLAGS_DEBUG=.*", "FFLAGS_DEBUG= -g -O1")
-                      
-                      if "^cray-libsci" in spec:
-                            libs_arg.append("-L{0}/lib -lsci_gnu".format(spec["cray-libsci"].prefix))
-                      
-                      with open("arch.make", "a") as f:
-                            if "+mpi" in spec:
-                                arch_make.filter("CC = .*", "CC = {0}".format(spec["mpi"].mpicc))
-                                arch_make.filter("FC = .*", "FC = {0}".format(spec["mpi"].mpifc))
-                                if "^cray-libsci" in spec:
-                                    libs_arg.append("-lsci_gnu_mpi")
-                                f.write("MPI_INTERFACE = libmpi_f90.a\n")
-                                f.write("MPI_INCLUDE = .\n")
-                                fppflags_arg.append("-DMPI ")
-                            
-                            if "+openmp" in spec:
-                                f.write("FFLAGS += -fopenmp\n")
-                                f.write("LIBS += -fopenmp\n")
-                            
-                            if "+netcdf" in spec:
-                                if "+cray" in spec :
-                                    libs_arg.append("-L{0}/lib -lnetcdff -lnetcdf".format(netcdff_prefix))
-                                    libs_arg.append("-L{0}/lib -lhdf5_fortran -lhdf5".format(hdf5_prefix))
-                                else:
-                                    libs_arg.append("-L{0}/lib -lnetcdff -lnetcdf".format(spec["netcdf-fortran"].prefix))
-                                    libs_arg.append("-L{0}/lib -lhdf5_fortran -lhdf5".format(spec["hdf5"].prefix))
-                            
-                            if "+metis" in spec :
-                                libs_arg.append("-L{0} -lmetis".format(self.spec["metis"].prefix.lib))
-                                fppflags_arg.append("-DSIESTA__METIS ")
-                            
-                            if "elpa" in spec :
-                                elpa = spec["elpa"]
-                                elpa_suffix = "_openmp" if "+openmp" in elpa else ""
-                                elpa_incdir = elpa.headers.directories[0]
-                                libs_arg.append("-L{0} -lelpa{1}".format(self.spec["elpa"].prefix.lib, elpa_suffix))
-                                fppflags_arg.append("-DSIESTA__ELPA  -I{0}".format(join_path(elpa_incdir, "modules")))
-                            
-                            if "mumps" in spec :
-                                libs_arg.append("-L{0} -lmumps_common -lzmumps".format(self.spec["mumps"].prefix.lib))
-                                fppflags_arg.append("-DSIESTA__MUMPS ")
-                            
-                            if "+pexsi" in spec :
-                                libs_arg.append("-L{0} -lpexsi_linux".format(self.spec["pexsi"].prefix.lib))
-                                fppflags_arg.append("-DSIESTA__PEXSI ")
-                                f.write("INCFLAGS += -I{0}/include".format(self.spec["pexsi"].prefix))
+                sh("../Src/obj_setup.sh")
+                if spec.satisfies("@:4.1.5%gcc"):
+                    shutil.copy("./gfortran.make", "./arch.make")
+                    libs_arg = []
+                    fppflags_arg = []
+                    arch_make = FileFilter("./arch.make")
+                    arch_make.filter(
+                        "FFLAGS = .*",
+                        "FFLAGS = {0}".format(
+                            "-O2 -fPIC -ftree-vectorize -fallow-argument-mismatch"
+                        ),
+                    )
 
-                            arch_make.filter("^LIBS =.*","LIBS = {0}".format(" ".join(libs_arg)))
-                            f.write("FPPFLAGS = {0}".format(" ".join(fppflags_arg)))
+                    if "+debug" in spec:
+                        arch_make.filter("FFLAGS_DEBUG=.*", "FFLAGS_DEBUG= -g -O1")
+
+                    if "^cray-libsci" in spec:
+                        libs_arg.append("-L{0}/lib -lsci_gnu".format(spec["cray-libsci"].prefix))
+
+                    with open("arch.make", "a") as f:
+                        if "+mpi" in spec:
+                            arch_make.filter("CC = .*", "CC = {0}".format(spec["mpi"].mpicc))
+                            arch_make.filter("FC = .*", "FC = {0}".format(spec["mpi"].mpifc))
+                            if "^cray-libsci" in spec:
+                                libs_arg.append("-lsci_gnu_mpi")
+                            f.write("MPI_INTERFACE = libmpi_f90.a\n")
+                            f.write("MPI_INCLUDE = .\n")
+                            fppflags_arg.append("-DMPI ")
+
+                        if "+openmp" in spec:
+                            f.write("FFLAGS += -fopenmp\n")
+                            f.write("LIBS += -fopenmp\n")
+
+                        if "+netcdf" in spec:
+                            if "+cray" in spec:
+                                libs_arg.append(
+                                    "-L{0}/lib -lnetcdff -lnetcdf".format(netcdff_prefix)
+                                )
+                                libs_arg.append(
+                                    "-L{0}/lib -lhdf5_fortran -lhdf5".format(hdf5_prefix)
+                                )
+                            else:
+                                libs_arg.append(
+                                    "-L{0}/lib -lnetcdff -lnetcdf".format(
+                                        spec["netcdf-fortran"].prefix
+                                    )
+                                )
+                                libs_arg.append(
+                                    "-L{0}/lib -lhdf5_fortran -lhdf5".format(spec["hdf5"].prefix)
+                                )
+
+                        if "+metis" in spec:
+                            libs_arg.append("-L{0} -lmetis".format(self.spec["metis"].prefix.lib))
+                            fppflags_arg.append("-DSIESTA__METIS ")
+
+                        if "elpa" in spec:
+                            elpa = spec["elpa"]
+                            elpa_suffix = "_openmp" if "+openmp" in elpa else ""
+                            elpa_incdir = elpa.headers.directories[0]
+                            libs_arg.append(
+                                "-L{0} -lelpa{1}".format(self.spec["elpa"].prefix.lib, elpa_suffix)
+                            )
+                            fppflags_arg.append(
+                                "-DSIESTA__ELPA  -I{0}".format(join_path(elpa_incdir, "modules"))
+                            )
+
+                        if "mumps" in spec:
+                            libs_arg.append(
+                                "-L{0} -lmumps_common -lzmumps".format(
+                                    self.spec["mumps"].prefix.lib
+                                )
+                            )
+                            fppflags_arg.append("-DSIESTA__MUMPS ")
+
+                        if "+pexsi" in spec:
+                            libs_arg.append(
+                                "-L{0} -lpexsi_linux".format(self.spec["pexsi"].prefix.lib)
+                            )
+                            fppflags_arg.append("-DSIESTA__PEXSI ")
+                            f.write("INCFLAGS += -I{0}/include".format(self.spec["pexsi"].prefix))
+
+                        arch_make.filter("^LIBS =.*", "LIBS = {0}".format(" ".join(libs_arg)))
+                        f.write("FPPFLAGS = {0}".format(" ".join(fppflags_arg)))
 
 
     def build(self, spec, prefix):
@@ -220,30 +259,30 @@ class CMakeBuilder(cmake.CMakeBuilder):
         args += ["-DBLAS_LIBRARIES={0}".format(self.spec["blas"].libs.link_flags)]
         args += ["-DLAPACK_LIBRARIES={0}".format(self.spec["lapack"].libs.link_flags)]
         
-        
         if "+mpi" in spec:
             args += [
                 "-DCMAKE_C_COMPILER=%s" % spec["mpi"].mpicc,
                 "-DCMAKE_CXX_COMPILER=%s" % spec["mpi"].mpicxx,
-                "-DCMAKE_Fortran_COMPILER=%s" % spec["mpi"].mpifc
-                ]
+                "-DCMAKE_Fortran_COMPILER=%s" % spec["mpi"].mpifc,
+            ]
             args += ["-DSIESTA_WITH_MPI=ON"]
             args += ["-DSCALAPACK_LIBRARY={0}".format(spec["scalapack"].libs.joined(";"))]
         
-        if "+openmp" in spec:
+	if "+openmp" in spec:
             args += ["-DSIESTA_WITH_OPENMP=ON"]
-            if "+cray" in spec :
+            if "+cray" in spec:
                 args += ["-DFortran_FLAGS=-fopenmp"]
 
         if "build_type=Debug" in spec:
-            args += ["-DFortran_FLAGS=-Og -g -Wall -fcheck=all -fbacktrace -Warray-bounds -Wunused -Wuninitialized"]
+            args += [
+                "-DFortran_FLAGS=-Og -g -Wall -fcheck=all -fbacktrace -Warray-bounds -Wunused -Wuninitialized"
+            ]
         else:
             args += ["-DFortran_FLAGS=-O2 -fPIC -ftree-vectorize  -fallow-argument-mismatch"]
 
-
         if "+netcdf" in spec:
             args += ["-DSIESTA_WITH_NETCDF=ON"]
-            if "+cray" in spec :
+            if "+cray" in spec:
                 args += ["-DNetCDF_PATH={0}".format(os.environ.get("NETCDF_DIR", ""))]
             else:
                 args += ["-DNetCDF_PATH={0}".format(spec["netcdf-fortran"].prefix)]
@@ -253,12 +292,14 @@ class CMakeBuilder(cmake.CMakeBuilder):
 
         if "+mumps" in spec:
             args += ["-DSIESTA__MUMPS=ON"]
-            args += ["-DSIESTA_LINKER_FLAGS=-L{0} -lmumps_common -lzmumps".format(self.spec["mumps"].prefix.lib)]
+            args += [
+                "-DSIESTA_LINKER_FLAGS=-L{0} -lmumps_common -lzmumps".format(
+                    self.spec["mumps"].prefix.lib
+                )
+            ]
 
         if "+metis" in spec:
             args += ["-DSIESTA__METIS=ON"]
             args += ["-DSIESTA_LINKER_FLAGS=-L{0} -lmetis".format(self.spec["metis"].prefix.lib)]
 
         return args
-
-
