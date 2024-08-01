48f9d6bdf27ae99254d46a3e992b83ee04b43cf9
John Parent <john.parent@kitware.com>
2024-08-01 15:32:33 -0400

    style

--- var/spack/repos/builtin/packages/win-file/package.py
+++ var/spack/repos/builtin/packages/win-file/package.py
@@ -13,13 +13,12 @@ class WinFile(Package):
     systems via the Mingw-w64 cross compiler and a custom Spack repository
     """
 
-
     homepage = "https://www.darwinsys.com/file/"
-    url = "https://spack.github.io/windows-bootstrap-resources/resources/file/5.45/file_5.45.tar.gz"
-
+    url = (
+        "https://spack.github.io/windows-bootstrap-resources/resources/file/5.45/file_5.45.tar.gz"
+    )
 
     executables = ["^file$"]
-
 
     version("5.45", sha256="11b8f3abf647c711bc50ef8451c8d6e955f11c4afd8b0a98f2ac65e9b6e10d5e")
 
@@ -30,7 +29,6 @@ class WinFile(Package):
         output = Executable(exe)("--version", output=str, error=str)
         match = re.search(r"file-(\S+)", output)
         return match.group(1) if match else None
-
 
     def install(self, spec, prefix):
         shutil.move(self.stage.source_path, prefix)
