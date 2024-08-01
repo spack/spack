f0367f8a039b3fb2710c622357a47f23c17d5787
Chris Marsh <chrismarsh.c2@gmail.com>
2024-08-01 13:19:17 -0600

    Add dask 2024.3 and distributed 2024.7

--- var/spack/repos/builtin/packages/py-dask/package.py
+++ var/spack/repos/builtin/packages/py-dask/package.py
@@ -16,6 +16,7 @@ class PyDask(PythonPackage):
 
     license("BSD-3-Clause")
 
+    version("2024.7.1", sha256="dbaef2d50efee841a9d981a218cfeb50392fc9a95e0403b6d680450e4f50d531")
     version("2023.4.1", sha256="9dc72ebb509f58f3fe518c12dd5a488c67123fdd66ccb0b968b34fd11e512153")
     version("2022.10.2", sha256="42cb43f601709575fa46ce09e74bea83fdd464187024f56954e09d9b428ceaab")
     version("2021.6.2", sha256="8588fcd1a42224b7cfcd2ebc8ad616734abb6b1a4517efd52d89c7dd66eb91f8")
@@ -29,6 +30,7 @@ class PyDask(PythonPackage):
     variant("dataframe", default=True, description="Install requirements for dask.dataframe")
     variant("distributed", default=True, description="Install requirements for dask.distributed")
     variant("diagnostics", default=False, description="Install requirements for dask.diagnostics")
+    
     variant(
         "delayed",
         default=True,
@@ -54,6 +56,7 @@ class PyDask(PythonPackage):
     depends_on("py-toolz@0.10.0:", type=("build", "run"), when="@2023.4.1:")
     depends_on("py-partd@0.3.10:", type=("build", "run"), when="@2021.3.1:")
     depends_on("py-partd@1.2.0:", type=("build", "run"), when="@2023.4.0:")
+    depends_on("py-partd@1.4.0:", type=("build", "run"), when="@2024.7.1:")
     depends_on("py-click@7.0:", type=("build", "run"), when="@2022.10.2:")
     depends_on("py-click@8.0:", type=("build", "run"), when="@2023.4.1:")
     depends_on("py-importlib-metadata@4.13.0:", type=("build", "run"), when="@2023.4.0:")
@@ -89,6 +92,7 @@ class PyDask(PythonPackage):
     depends_on("py-pandas@0.25.0:", type=("build", "run"), when="@2020.12.0: +dataframe")
     depends_on("py-pandas@1.0:", type=("build", "run"), when="@2022.10.2: +dataframe")
     depends_on("py-pandas@1.3:", type=("build", "run"), when="@2023.4.0: +dataframe")
+    depends_on("py-pandas@2.0:", type=("build", "run"), when="@2024.7.1: +dataframe")
     # The dependency on py-toolz is non-optional starting version 2021.3.1
     depends_on("py-toolz@0.8.2:", type=("build", "run"), when="@:2021.3.0 +dataframe")
     # The dependency on py-partd is non-optional starting version 2021.3.1
@@ -103,6 +107,7 @@ class PyDask(PythonPackage):
     depends_on("py-distributed@2021.6.2", type=("build", "run"), when="@2021.6.2 +distributed")
     depends_on("py-distributed@2022.10.2", type=("build", "run"), when="@2022.10.2 +distributed")
     depends_on("py-distributed@2023.4.1", type=("build", "run"), when="@2023.4.1 +distributed")
+    depends_on("py-distributed@2024.7.1", type=("build", "run"), when="@2024.7.1 +distributed")
 
     # Requirements for dask.diagnostics
     depends_on("py-bokeh@1.0.0:1,2.0.1:", type=("build", "run"), when="+diagnostics")
