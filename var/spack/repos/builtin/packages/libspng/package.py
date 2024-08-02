0b66df92be6408b6ea54d4acb6372bf0f383e492
Alec Scott <hi@alecbcs.com>
2024-08-01 19:22:42 -0700

    Update var/spack/repos/builtin/packages/libspng/package.py

--- var/spack/repos/builtin/packages/libspng/package.py
+++ var/spack/repos/builtin/packages/libspng/package.py
@@ -15,6 +15,7 @@ class Libspng(CMakePackage):
     url = "https://github.com/randy408/libspng/archive/refs/tags/v0.7.2.tar.gz"
 
     maintainers("sethrj")
+
     license("BSD-2-Clause")
 
     version("0.7.4", sha256="47ec02be6c0a6323044600a9221b049f63e1953faf816903e7383d4dc4234487")
