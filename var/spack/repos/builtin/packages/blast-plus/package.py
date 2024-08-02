2031dd6eb8a83fd6b64a558604316b973517ba6d
Diego Alvarez S. <dialvarezs@gmail.com>
2024-08-01 23:39:03 -0300

    Add blast+ v2.15.0, v2.16.0 (#45425)

--- var/spack/repos/builtin/packages/blast-plus/package.py
+++ var/spack/repos/builtin/packages/blast-plus/package.py
@@ -14,6 +14,8 @@ class BlastPlus(AutotoolsPackage):
 
     maintainers("weijianwen")
 
+    version("2.16.0", sha256="17c93cf009721023e5aecf5753f9c6a255d157561638b91b3ad7276fd6950c2b")
+    version("2.15.0", sha256="6918c370524c8d44e028bf491e8f245a895e07c66c77b261ce3b38d6058216e0")
     version("2.14.1", sha256="712c2dbdf0fb13cc1c2d4f4ef5dd1ce4b06c3b57e96dfea8f23e6e99f5b1650e")
     version("2.13.0", sha256="89553714d133daf28c477f83d333794b3c62e4148408c072a1b4620e5ec4feb2")
     version("2.12.0", sha256="fda3c9c9d488cad6c1880a98a236d842bcf3610e3e702af61f7a48cf0a714b88")
