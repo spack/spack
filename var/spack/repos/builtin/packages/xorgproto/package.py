a99f1ec6295c89a6a854c05e729fc90368e92bed
Teague Sterling <teaguesterling@gmail.com>
2024-08-03 07:55:59 -0700

    xorgproto: new package
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

---
+++ var/spack/repos/builtin/packages/xorgproto/package.py
@@ -0,0 +1,39 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack.package import *
+
+
+class Xorgproto(MesonPackage):
+    """X Window System unified protocol definitions replacing standalone protos"""
+
+    homepage = "https://gitlab.freedesktop.org/xorg/proto/xorgproto"
+    url = "https://gitlab.freedesktop.org/xorg/proto/xorgproto/-/archive/xorgproto-2024.1/xorgproto-xorgproto-2024.1.tar.bz2"
+
+    maintainers("teaguesterling")
+
+    license("OTHER", checked_by="teaguesterling")
+
+    version("2024.1", sha256="3959b2d17d86dd9d165dc24d26f372ca64f27127cd381739366ba8383a6cd51a")
+
+    depends_on("util-macros", type="build")
+
+    provides("bigreqsproto")
+    provides("compositeproto")
+    provides("damageproto")
+    provides("dmxproto")
+    provides("dri2proto")
+    provides("dri3proto")
+    provides("fixesproto")
+    provides("fontcacheproto")
+    provides("presentproto")
+    provides("randrproto")
+    provides("recordproto")
+    provides("renderproto")
+    provides("resourceproto")
+    provides("scrnsaverproto")
+    provides("videoproto")
+    provides("xineramaproto")
+    provides("xf86driproto")
+    provides("xcmiscproto")
