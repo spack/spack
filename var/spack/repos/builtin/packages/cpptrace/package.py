8455785011490a0fa01e7e361a9333b2fffd6b52
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-08-06 12:26:42 -0700

    cpptrace: add symbols-backend

--- var/spack/repos/builtin/packages/cpptrace/package.py
+++ var/spack/repos/builtin/packages/cpptrace/package.py
@@ -29,6 +29,12 @@ class Cpptrace(CMakePackage):
             default="unwind",
             values=("unwind", "execinfo", "libunwind", "nothing"),
         )
+        variant(
+            "symbols-backend",
+            multi=False,
+            default="libdwarf",
+            values=("libdwarf", "libbacktrace", "addr2line", "libdl", "nothing"),
+        )
 
     with when("platform=darwin"):
         variant(
@@ -36,6 +42,12 @@ class Cpptrace(CMakePackage):
             multi=False,
             default="unwind",
             values=("unwind", "execinfo", "libunwind", "nothing"),
+        )
+        variant(
+            "symbols-backend",
+            multi=False,
+            default="libdwarf",
+            values=("libdwarf", "libbacktrace", "addr2line", "libdl", "nothing"),
         )
 
     with when("platform=windows"):
@@ -45,8 +57,10 @@ class Cpptrace(CMakePackage):
             default="dbghelp",
             values=("winapi", "dbghelp", "libunwind", "nothing"),
         )
+        variant("symbols-backend", multi=False, default="dbghelp", values=("dbghelp", "nothing"))
 
     depends_on("libunwind", when="unwinding-backend=libunwind")
+    depends_on("libdwarf", when="symbols-backend=libdwarf")
 
     depends_on("googletest", type="test")
 
@@ -61,9 +75,13 @@ class Cpptrace(CMakePackage):
             from_variant("BUILD_SHARED_LIBS", "shared"),
             from_variant("CPPTRACE_POSITION_INDEPENDENT_CODE", "pic"),
             define("CPPTRACE_BUILD_TESTING", self.run_tests),
+            define("CPPTRACE_USE_EXTERNAL_LIBDWARF", True),
             define("CPPTRACE_USE_EXTERNAL_GTEST", True),
             define(
                 f"CPPTRACE_UNWIND_WITH_{spec.variants['unwinding-backend'].value.upper()}", True
+            ),
+            define(
+                f"CPPTRACE_GET_SYMBOLS_WITH_{spec.variants['symbols-backend'].value.upper()}", True
             ),
         ]
 
