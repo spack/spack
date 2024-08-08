cfb054613d75d3725865c253b3f87ff1e3fbdcea
Teague Sterling <teaguesterling@gmail.com>
2024-08-07 22:50:22 -0700

    py-glean-sdk: add v60.0.6 and fix 60.0.1 checksum
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/py-glean-sdk/package.py
+++ var/spack/repos/builtin/packages/py-glean-sdk/package.py
@@ -13,6 +13,7 @@ class PyGleanSdk(PythonPackage):
 
     license("MPL-2.0", checked_by="teaguesterling")
 
+    version("60.5.0", sha256="0a23adad449c05d2cc522dc28ef98287b59a42ff112e53e3c8b4cfe9c938f6ae")
     version("60.4.0", sha256="24bc608e06580962ce029cc4c09a51af75e4a29b3d889232b298f87208acbf62")
     version("60.0.1", sha256="ba7fb8b1e4ecd50da4dc2e02ef887a71d93f848580e17a6f3e947ed3bcf68726")
 
