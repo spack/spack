59302b276c78cacbced9289e968a0ea0ee8cb0e9
Wouter Deconinck <wdconinc@gmail.com>
2024-08-12 07:30:03 -0500

    aspell: add v0.60.8.1

--- var/spack/repos/builtin/packages/aspell/package.py
+++ var/spack/repos/builtin/packages/aspell/package.py
@@ -18,6 +18,7 @@ class Aspell(AutotoolsPackage, GNUMirrorPackage):
 
     license("LGPL-2.1-or-later")
 
+    version("0.60.8.1", sha256="d6da12b34d42d457fa604e435ad484a74b2effcd120ff40acd6bb3fb2887d21b")
     version("0.60.8", sha256="f9b77e515334a751b2e60daab5db23499e26c9209f5e7b7443b05235ad0226f2")
     version("0.60.6.1", sha256="f52583a83a63633701c5f71db3dc40aab87b7f76b29723aeb27941eff42df6e1")
 
