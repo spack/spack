7a7e885c7b514ef253aee680d87c2e1e36c94bdb
Tamara Dahlgren <dahlgren1@llnl.gov>
2024-08-12 09:13:27 -0700

    c: bugfix and simplification of the stand-alone tests

--- var/spack/repos/builtin/packages/c/package.py
+++ var/spack/repos/builtin/packages/c/package.py
@@ -15,31 +15,16 @@ class C(Package):
     virtual = True
 
     def test_c(self):
-        """Compile and run 'Hello world'"""
+        """build and run C examples"""
+        cc = which(os.environ["CC"])
+        expected = ["Hello world", "YES!"]
 
         test_source = self.test_suite.current_test_data_dir
-
-        cc_exe = os.environ["CC"]
-        cc_exe = which(join_path(self.prefix.bin, cc_exe))
-        if cc_exe is None:
-            raise SkipTest(f"{os.environ['CC']} not found in {self.version}")
-
         for test in os.listdir(test_source):
-            with test_part(self, f"test_c_{test}", f"Test {test}"):
-                filepath = test_source.join(test)
-                exe_name = f"{test}.exe"
-                cc_opts = ["-o", exe_name, filepath]
-                comp_exe = which(join_path(self.prefix.bin, cc_exe))
-                if comp_exe is None:
-                    raise SkipTest(f"{str(cc_exe)} not found in {self.version}")
-                compiled = cc_exe(*cc_opts)
-
-                if compiled:
-                    expected = ["Hello world", "YES!"]
-                    exe = which(join_path(self.prefix.bin, exe_name))
-                    if exe is None:
-                        raise SkipTest(f"{exe} not found in {self.version}")
-                    out = exe(output=str.split, error=str.split)
-                    check_outputs(expected, out)
-                else:
-                    assert False, "Did not compile"
+            exe_name = f"{test}.exe"
+            with test_part(self, f"test_c_{test}", f"build and run {exe_name}"):
+                filepath = join_path(test_source, test)
+                cc("-o", exe_name, filepath)
+                exe = which(exe_name)
+                out = exe(output=str.split, error=str.split)
+                check_outputs(expected, out)
