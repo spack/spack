c8feb0996a85477eb69bad22eaa318b2a03672bf
becker33 <becker33@users.noreply.github.com>
2024-08-14 00:17:42 +0000

    [@spackbot] updating style on behalf of becker33

--- lib/spack/spack/build_systems/oneapi.py
+++ lib/spack/spack/build_systems/oneapi.py
@@ -12,12 +12,12 @@ from llnl.util import tty
 from llnl.util.filesystem import HeaderList, LibraryList, find_libraries, join_path, mkdirp
 from llnl.util.link_tree import LinkTree
 
+import spack.util.path
 from spack.build_environment import dso_suffix
 from spack.directives import conflicts, license, redistribute, variant
 from spack.package_base import InstallError
 from spack.util.environment import EnvironmentModifications
 from spack.util.executable import Executable
-import spack.util.path
 
 from .generic import Package
 
