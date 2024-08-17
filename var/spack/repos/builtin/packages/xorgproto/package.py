745b26ee34a8d9fb369237404a71b69a25572a2b
Wouter Deconinck <wdconinc@gmail.com>
2024-08-17 10:41:27 -0500

    xorgproto: depends_on meson type build

--- var/spack/repos/builtin/packages/xorgproto/package.py
+++ var/spack/repos/builtin/packages/xorgproto/package.py
@@ -17,5 +17,5 @@ class Xorgproto(MesonPackage, XorgPackage):
 
     version("2024.1", sha256="4f6b9b4faf91e5df8265b71843a91fc73dc895be6210c84117a996545df296ce")
 
-    depends_on("meson@0.56:")
+    depends_on("meson@0.56:", type="build")
     depends_on("util-macros", type="build")
