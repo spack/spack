e3e0b55d3cc26f69cecc3a9a67002522e50c9db2
Joseph C Wang <joequant@gmail.com>
2024-08-23 02:21:19 +0800

    py-awkward-cpp: add latest version to 37

--- var/spack/repos/builtin/packages/py-awkward-cpp/package.py
+++ var/spack/repos/builtin/packages/py-awkward-cpp/package.py
@@ -18,6 +18,7 @@ class PyAwkwardCpp(PythonPackage):
 
     license("BSD-3-Clause")
 
+    version("37", sha256="6dff5fce4afcadb492bbf7cb20908572d99bdc32aa2bea8681bacfb69b2aa86d")
     version("12", sha256="429f7fcc37a671afa67fe9680f2edc3a123d1c74d399e5889c654f9529f9f8f2")
     version("11", sha256="02d719a4da7487564b29b8e8b78925a32ac818b6f5572c2f55912b4e0e59c7a4")
     version("10", sha256="d1c856cb6ef5cf3d4f67506a7efc59239f595635865cc9f4ab18440b8bfb11c6")
