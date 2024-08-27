2442161d6a2c3b9976ef9e5fbc57690630ea01cb
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2024-08-27 15:00:34 +0200

    address review

--- var/spack/repos/builtin/packages/impalajit/package.py
+++ var/spack/repos/builtin/packages/impalajit/package.py
@@ -42,7 +42,6 @@ class Impalajit(CMakePackage):
         args.append(self.define_from_variant("SHARED_LIB", "shared"))
         args.append(self.define("TESTS", self.run_tests))
 
-        if self.compiler.name != "intel":
+        if not self.spec.satisfies("%intel"):
             args.append("-DINTEL_COMPILER=OFF")
-
         return args
