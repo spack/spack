d4ba8f9b8af1286a584843dbaabdd1ff247fb86d
Etienne Malaboeuf <eti.malaboeuf@gmail.com>
2024-08-28 17:15:46 +0200

    Using the Cray GTL.

--- var/spack/repos/builtin/packages/lammps/package.py
+++ var/spack/repos/builtin/packages/lammps/package.py
@@ -792,6 +792,22 @@ class Lammps(CMakePackage, CudaPackage, ROCmPackage, PythonExtension):
 
     root_cmakelists_dir = "cmake"
 
+    def flag_handler(self, name, flags):
+        wrapper_flags = []
+        build_system_flags = []
+
+        if self.spec.satisfies("+rocm"):
+            if self.spec.satisfies("^cray-mpich"):
+                gtl_lib = self.spec["cray-mpich"].package.gtl_lib
+                build_system_flags.extend(gtl_lib.get(name) or [])
+            # hipcc is not wrapped, we need to pass the flags via the build
+            # system.
+            build_system_flags.extend(flags)
+        else:
+            wrapper_flags.extend(flags)
+
+        return (wrapper_flags, [], build_system_flags)
+
     def cmake_args(self):
         spec = self.spec
 
