b7dde54bd9ce9a435543fb17affb806905a07af2
Gregory Becker <becker33@llnl.gov>
2024-08-30 15:48:05 -0700

    move explicit splice config under concretizer section

--- lib/spack/spack/schema/merged.py
+++ lib/spack/spack/schema/merged.py
@@ -25,7 +25,6 @@ import spack.schema.mirrors
 import spack.schema.modules
 import spack.schema.packages
 import spack.schema.repos
-import spack.schema.splice
 import spack.schema.upstreams
 import spack.schema.view
 
@@ -44,7 +43,6 @@ properties: Dict[str, Any] = union_dicts(
     spack.schema.modules.properties,
     spack.schema.packages.properties,
     spack.schema.repos.properties,
-    spack.schema.splice.properties,
     spack.schema.upstreams.properties,
     spack.schema.view.properties,
 )
--- lib/spack/spack/test/concretize.py
+++ lib/spack/spack/test/concretize.py
@@ -2286,7 +2286,7 @@ class TestConcretize:
             "replacement": f"/{mpich_spec.dag_hash()}",
             "transitive": transitive,
         }
-        spack.config.CONFIG.set("splice", [splice_info])
+        spack.config.CONFIG.set("concretizer", {"splice": {"explicit": [splice_info]}})
 
         spec = spack.spec.Spec("hdf5 ^zmpi").concretized()
 
