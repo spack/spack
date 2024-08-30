71b4ce173d610202b0331d40d1ba63fd70173e19
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-08-30 10:11:17 +0200

    Remove `DetectedPackage` class (#46071)
    This PR simplifies the code doing external spec detection by removing 
    the `DetectedPackage` class. Now, functions accepting or returning lists 
    of `DetectedPackage`, will accept or return list of specs.
    
    Performance doesn't seem to change if we use `Spec.__reduce__` instead 
    of `DetectionPackage.__reduce__`.

--- lib/spack/spack/detection/path.py
+++ lib/spack/spack/detection/path.py
@@ -24,7 +24,6 @@ import spack.util.environment as environment
 import spack.util.ld_so_conf
 
 from .common import (
-    DetectedPackage,
     WindowsCompilerExternalPaths,
     WindowsKitExternalPaths,
     _convert_to_iterable,
@@ -229,7 +228,7 @@ class Finder:
 
     def detect_specs(
         self, *, pkg: Type["spack.package_base.PackageBase"], paths: List[str]
-    ) -> List[DetectedPackage]:
+    ) -> List["spack.spec.Spec"]:
         """Given a list of files matching the search patterns, returns a list of detected specs.
 
         Args:
@@ -295,16 +294,16 @@ class Finder:
                     warnings.warn(msg)
                     continue
 
-                if spec.external_path:
-                    prefix = spec.external_path
+                if not spec.external_path:
+                    spec.external_path = prefix
 
-                result.append(DetectedPackage(spec=spec, prefix=prefix))
+                result.append(spec)
 
         return result
 
     def find(
         self, *, pkg_name: str, repository, initial_guess: Optional[List[str]] = None
-    ) -> List[DetectedPackage]:
+    ) -> List["spack.spec.Spec"]:
         """For a given package, returns a list of detected specs.
 
         Args:
@@ -388,7 +387,7 @@ def by_path(
     *,
     path_hints: Optional[List[str]] = None,
     max_workers: Optional[int] = None,
-) -> Dict[str, List[DetectedPackage]]:
+) -> Dict[str, List["spack.spec.Spec"]]:
     """Return the list of packages that have been detected on the system, keyed by
     unqualified package name.
 
