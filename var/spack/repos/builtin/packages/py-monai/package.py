9ecd8a88f037ea1d585d197a1ad9a9501ccbcee5
Jen Herting <jen@herting.cc>
2024-08-30 10:51:11 -0400

    [py-monai] added license

--- var/spack/repos/builtin/packages/py-monai/package.py
+++ var/spack/repos/builtin/packages/py-monai/package.py
@@ -12,6 +12,8 @@ class PyMonai(PythonPackage):
     homepage = "https://monai.io/"
     url = "https://github.com/Project-MONAI/MONAI/archive/refs/tags/0.8.1.tar.gz"
 
+    license("Apache-2.0", checked_by="qwertos")
+
     version("1.3.2", sha256="e370e1fcd78854fb22c2414fa7419c15ff5afce67b923ce666d0f12979015136")
     version("0.8.1", sha256="e1227e6406cc47c23f6846f617350879ceba353915b948d917bf4308b17ea861")
     version("0.8.0", sha256="a63df7d5a680d9641c223ea090ff843a7d6f20bdb62095bd44f3b0480a4706ed")
