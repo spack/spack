f3012944cea13dbbf2b9218cfb9fbc82af0da155
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2024-09-02 03:25:58 +0200

    add myself as maintainer

--- var/spack/repos/builtin/packages/cln/package.py
+++ var/spack/repos/builtin/packages/cln/package.py
@@ -18,7 +18,8 @@ class Cln(AutotoolsPackage):
     homepage = "https://www.ginac.de/CLN/"
     url = "https://www.ginac.de/CLN/cln-1.3.7.tar.bz2"
     git = "git://www.ginac.de/cln.git"
-
+    maintainers("prudhomm")
+    
     license("GPL-2.0-or-later")
 
     version("master", branch="master")
