c09a34e114b1bfb87a3d2d294a177438afceb084
Philipp Edelmann <pedelmann@lanl.gov>
2024-09-03 07:15:56 -0600

    reorder versions and dependencies

--- var/spack/repos/builtin/packages/makedepf90/package.py
+++ var/spack/repos/builtin/packages/makedepf90/package.py
@@ -17,7 +17,8 @@ class Makedepf90(AutotoolsPackage):
 
     license("GPL-2.0-only", checked_by="tukss")
 
+    version("3.0.1", sha256="a11601ea14ad793f23fca9c7e7df694b6337f962ccc930d995d72e172edf29ee")
+
     depends_on("c", type="build")
     depends_on("flex", type="build")
     depends_on("bison", type="build")
-    version("3.0.1", sha256="a11601ea14ad793f23fca9c7e7df694b6337f962ccc930d995d72e172edf29ee")
