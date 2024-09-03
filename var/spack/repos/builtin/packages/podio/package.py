336a2d365e4f8097e02589f9d60f550e3e0f2319
jmcarcell <jmcarcell@users.noreply.github.com>
2024-09-03 21:40:49 +0200

    podio: Add an rpath variant to set the rpath

--- var/spack/repos/builtin/packages/podio/package.py
+++ var/spack/repos/builtin/packages/podio/package.py
@@ -95,6 +95,7 @@ class Podio(CMakePackage):
         description="Build the RDataSource for reading podio collections",
         when="@1.0.2:",
     )
+    variant("rpath", default=False, description="Set RPATH for PODIO libraries")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
@@ -138,6 +139,7 @@ class Podio(CMakePackage):
             self.define_from_variant("ENABLE_SIO", "sio"),
             self.define_from_variant("ENABLE_RNTUPLE", "rntuple"),
             self.define_from_variant("ENABLE_DATASOURCE", "datasource"),
+            self.define_from_variant("PODIO_SET_RPATH", "rpath"),
             self.define("CMAKE_CXX_STANDARD", self.spec.variants["cxxstd"].value),
             self.define("BUILD_TESTING", self.run_tests),
         ]
