d8761ca11aa10ccaceba54ac78f72e4578e5b59a
Robert Underwood <runderwood@anl.gov>
2024-09-05 14:46:42 -0400

    Updated version of sz3
    Supercedes #46128

--- var/spack/repos/builtin/packages/sz3/package.py
+++ var/spack/repos/builtin/packages/sz3/package.py
@@ -16,6 +16,7 @@ class Sz3(CMakePackage):
     tags = ["e4s"]
 
     version("master")
+    version("3.2.0", commit="b3dab4018425803a55d8073dc55dade7fa46b7b4")
     version("3.1.8", commit="e308ebf8528c233286874b920c72c0a6c0218fb2")
     version("3.1.7", commit="c49fd17f2d908835c41000c1286c510046c0480e")
     version("3.1.5.4", commit="4c6ddf628f27d36b28d1bbda02174359cd05573d")
