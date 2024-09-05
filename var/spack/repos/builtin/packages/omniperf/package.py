ceb787cbeb692c3956354cde677184771f72295a
Afzal Patel <afzal.patel@amd.com>
2024-09-05 07:38:15 -0700

    add new ROCm packages omniperf, rocal and rocdecode

---
+++ var/spack/repos/builtin/packages/omniperf/package.py
@@ -0,0 +1,44 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Omniperf(CMakePackage):
+    """Advanced Profiling and Analytics for AMD Hardware"""
+
+    homepage = "https://github.com/ROCm/omniperf"
+    git = "https://github.com/ROCm/omniperf.git"
+    url = "https://github.com/ROCm/omniperf/archive/refs/tags/rocm-6.2.0.tar.gz"
+
+    tags = ["rocm"]
+
+    maintainers("afzpatel", "srekolam", "renjithravindrankannath")
+
+    license("MIT")
+
+    version("6.2.0", sha256="b2ad49324a07aef977833d62741509a5d799b92758db56a16b4ab5636b6e231e")
+
+    depends_on("python@3.8:")
+    depends_on("py-astunparse@1.6.2:")
+    depends_on("py-colorlover")
+    depends_on("py-pyyaml")
+    depends_on("py-matplotlib")
+    depends_on("py-pandas")
+    depends_on("py-pymongo")
+    depends_on("py-tabulate")
+    depends_on("py-tqdm")
+    depends_on("py-kaleido")
+    depends_on("py-plotille")
+    depends_on("py-dash-svg")
+    depends_on("py-dash")
+    depends_on("py-dash-bootstrap-components")
+
+    # VERSION.sha is not in the auto-generated ROCm release tarball
+    patch("0001-remove-VERSION.sha-install.patch")
+
+    def cmake_args(self):
+        args = [self.define("ENABLE_TESTS", self.run_tests)]
+        return args
