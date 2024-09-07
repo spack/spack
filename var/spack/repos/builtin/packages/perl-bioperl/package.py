d2c8523c245dce354ee1890710dfbadcee2f0673
Teague Sterling <teaguesterling@gmail.com>
2024-09-07 11:34:43 -0700

    perl-bioperl: update url
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/perl-bioperl/package.py
+++ var/spack/repos/builtin/packages/perl-bioperl/package.py
@@ -38,16 +38,8 @@ class PerlBioperl(PerlPackage):
 
     version("1.7.8", sha256="c490a3be7715ea6e4305efd9710e5edab82dabc55fd786b6505b550a30d71738")
     version("1.7.6", sha256="df2a3efc991b9b5d7cc9d038a1452c6dac910c9ad2a0e47e408dd692c111688d")
-    version(
-        "1.7.2",
-        sha256="17aa3aaab2f381bbcaffdc370002eaf28f2c341b538068d6586b2276a76464a1",
-        url="https://cpan.metacpan.org/authors/id/C/CJ/CJFIELDS/BioPerl-1.007002.tar.gz",
-    )
-    version(
-        "1.6.924",
-        sha256="616a7546bb3c58504de27304a0f6cb904e18b6bbcdb6a4ec8454f2bd37bb76d0",
-        url="https://cpan.metacpan.org/authors/id/C/CJ/CJFIELDS/BioPerl-1.6.924.tar.gz",
-    )
+    version("1.7.2", sha256="17aa3aaab2f381bbcaffdc370002eaf28f2c341b538068d6586b2276a76464a1")
+    version("1.6.924", sha256="616a7546bb3c58504de27304a0f6cb904e18b6bbcdb6a4ec8454f2bd37bb76d0")
 
     # This is technically the same as 1.7.2, but with a more conventional version number.
     version(
