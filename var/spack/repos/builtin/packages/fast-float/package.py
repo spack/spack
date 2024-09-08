3e355ef0f797f00f291ec288a30b2edfab7c0796
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-08 14:45:13 -0400

    fast-float: add v6.1.5 and v6.1.6

--- var/spack/repos/builtin/packages/fast-float/package.py
+++ var/spack/repos/builtin/packages/fast-float/package.py
@@ -15,6 +15,8 @@ class FastFloat(CMakePackage):
 
     license("Apache-2.0 OR BSL-1.0 OR MIT", checked_by="pranav-sivararamn")
 
+    version("6.1.6", sha256="4458aae4b0eb55717968edda42987cabf5f7fc737aee8fede87a70035dba9ab0")
+    version("6.1.5", sha256="597126ff5edc3ee59d502c210ded229401a30dafecb96a513135e9719fcad55f")
     version("6.1.4", sha256="12cb6d250824160ca16bcb9d51f0ca7693d0d10cb444f34f1093bc02acfce704")
 
     depends_on("cxx", type="build")
