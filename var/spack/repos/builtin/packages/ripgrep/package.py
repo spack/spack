be378bd2088791bb7c8927b4f6f4169a45533bcc
Alec Scott <alec@llnl.gov>
2024-09-12 08:26:50 -0700

    ripgrep: fix rust dependency type

--- var/spack/repos/builtin/packages/ripgrep/package.py
+++ var/spack/repos/builtin/packages/ripgrep/package.py
@@ -25,4 +25,4 @@ class Ripgrep(CargoPackage):
     version("13.0.0", sha256="0fb17aaf285b3eee8ddab17b833af1e190d73de317ff9648751ab0660d763ed2")
     version("11.0.2", sha256="0983861279936ada8bc7a6d5d663d590ad34eb44a44c75c2d6ccd0ab33490055")
 
-    depends_on("rust@1.72:", when="@14:")
+    depends_on("rust@1.72:", type="build", when="@14:")
