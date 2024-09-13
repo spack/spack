1285f75683267f97865cecb758b0f5561211aed8
Richard Berger <richard.berger@outlook.com>
2024-09-13 15:54:34 -0600

    aspell: add new versions of en and de dictionaries

--- var/spack/repos/builtin/packages/aspell6-de/package.py
+++ var/spack/repos/builtin/packages/aspell6-de/package.py
@@ -15,6 +15,10 @@ class Aspell6De(AspellDictPackage, GNUMirrorPackage):
     license("GPL-2.0-or-later")
 
     version(
+        "6-de-20161207-7-0",
+        sha256="c2125d1fafb1d4effbe6c88d4e9127db59da9ed92639c7cbaeae1b7337655571",
+    )
+    version(
         "6-de-20030222-1",
         sha256="ba6c94e11bc2e0e6e43ce0f7822c5bba5ca5ac77129ef90c190b33632416e906",
     )
