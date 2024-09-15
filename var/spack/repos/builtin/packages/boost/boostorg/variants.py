e063fe6b220c1c86a2433726aeba6373c6e63f42
Tim Haines <thaines.astro@gmail.com>
2024-09-15 06:29:58 -0500

    Extract random variant

--- var/spack/repos/builtin/packages/boost/boostorg/variants.py
+++ var/spack/repos/builtin/packages/boost/boostorg/variants.py
@@ -209,6 +209,16 @@ def load():
     # ----------------------------------------------------------------------
 
     variants.add(
+        "random",
+        when="@1.15.0:",
+        buildable="@1.43.0:",
+        conflicts=[
+            {"when": "cxxstd=98", "msg": "Boost.random requires cxxstd >= 11"},
+            {"when": "cxxstd=03", "msg": "Boost.random requires cxxstd >= 11"},
+        ],
+        description="A complete system for random number generation",
+    )
+    variants.add(
         "graph",
         when="@1.18.0:",
         buildable="@1.18.0:",
--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -91,7 +91,6 @@ class Boost(Package):
     # fmt: off
     with_default_variants = "boost" + "".join(
         [
-            "+random",
             "+regex",
             "+serialization",
             "+system",
@@ -107,7 +106,6 @@ class Boost(Package):
     # dependencies and/or because there is a great deal of customization
     # possible (and it would be difficult to choose sensible defaults)
     all_libs = [
-        "random",
         "regex",
         "serialization",
         "stacktrace",
