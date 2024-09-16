8de881e17e22a42d32e13960d47642f25b81eb5d
Ashim Mahara <am7539@g.rit.edu>
2024-09-16 16:18:28 -0400

    deleted the FIXME

--- var/spack/repos/builtin/packages/py-evodiff/package.py
+++ var/spack/repos/builtin/packages/py-evodiff/package.py
@@ -9,7 +9,6 @@ from spack.package import *
 class PyEvodiff(PythonPackage):
     """Python package for generation of protein sequences and evolutionary alignments via discrete diffusion models"""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/microsoft/evodiff"
     pypi = "evodiff/evodiff-1.1.0.tar.gz"
 
