aaf59d71dbec2315197abae84b5163588e5b9d34
Tamara Dahlgren <dahlgren1@llnl.gov>
2024-09-17 16:03:29 -0700

    py-test-callback: resolve builder issues

--- var/spack/repos/builtin.mock/packages/py-test-callback/package.py
+++ var/spack/repos/builtin.mock/packages/py-test-callback/package.py
@@ -3,8 +3,12 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import llnl.util.filesystem as fs
+
 import spack.pkg.builtin.mock.python as mp
 from spack.package import *
+
+from spack.build_systems._checks import BaseBuilder, execute_install_time_tests
 
 
 class PyTestCallback(mp.Python):
@@ -13,7 +17,11 @@ class PyTestCallback(mp.Python):
     homepage = "http://www.example.com"
     url = "http://www.example.com/test-callback-1.0.tar.gz"
 
-    install_time_test_callbacks = ["test_callback"]
+    #: This attribute is used in UI queries that need to know the build
+    #: system base class
+    build_system_class = "PyTestCallback"
+
+    build_system("testcallback")
 
     version("1.0", "00000000000000000000000000000110")
     version("2.0", "00000000000000000000000000000120")
@@ -23,3 +31,19 @@ class PyTestCallback(mp.Python):
 
     def test_callback(self):
         print("PyTestCallback test")
+
+
+@spack.builder.builder("testcallback")
+class MyBuilder(BaseBuilder):
+    phases = ("install",)
+
+    #: Callback names for install-time test
+    install_time_test_callbacks = ["test_callback"]
+
+    def install(self, pkg, spec, prefix):
+        pkg.install(spec, prefix)
+
+    spack.builder.run_after("install")(execute_install_time_tests)
+
+    def test_callback(self):
+        self.pkg.test_callback()
