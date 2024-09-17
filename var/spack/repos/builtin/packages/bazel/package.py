b33690421b92643368d35f90d253269edd3d852e
Adam J. Stewart <ajstewart426@gmail.com>
2024-09-17 14:17:13 +0200

    Bazel: add GCC 13 support for v6

--- var/spack/repos/builtin/packages/bazel/package.py
+++ var/spack/repos/builtin/packages/bazel/package.py
@@ -120,6 +120,14 @@ class Bazel(Package):
     # https://blog.bazel.build/2021/05/21/bazel-4-1.html
     conflicts("platform=darwin target=aarch64:", when="@:4.0")
 
+    # https://github.com/bazelbuild/bazel/issues/18642
+    patch(
+        "https://github.com/bazelbuild/bazel/pull/20785.patch?full_index=1",
+        sha256="85dde31d129bbd31e004c5c87f23cdda9295fbb22946dc6d362f23d83bae1fd8",
+        when="@6.0:6.4",
+    )
+    conflicts("%gcc@13:", when="@:5")
+
     # Patches for compiling various older bazels which had ICWYU violations revealed by
     # (but not unique to) GCC 11 header changes. These are derived from
     # https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/29084/
@@ -131,8 +139,6 @@ class Bazel(Package):
     # Bazel-4.0.0 does not compile with gcc-11
     # Newer versions of grpc and abseil dependencies are needed but are not in bazel-4.0.0
     conflicts("@4.0.0", when="%gcc@11:")
-    # https://github.com/bazelbuild/bazel/issues/18642
-    conflicts("@:6", when="%gcc@13:")
 
     executables = ["^bazel$"]
 
