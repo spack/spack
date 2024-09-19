973ade5304c17653b7b340bf6861652a9539c057
Brett Viren <bv@bnl.gov>
2024-09-19 15:07:00 -0400

    Bump arrayfire to version 3.9.0 which requires a small patch to top cmakefiles.txt.

--- var/spack/repos/builtin/packages/arrayfire/package.py
+++ var/spack/repos/builtin/packages/arrayfire/package.py
@@ -17,17 +17,13 @@ class Arrayfire(CMakePackage, CudaPackage):
 
     license("FreeImage")
 
-    version("master")
-    version("3.8.1", commit="823e8e399fe8c120c6ec7ec75f09e6106b3074ca", tag="v3.8.1")
-    version(
-        "3.7.3", commit="59ac7b980d1ae124aae914fb29cbf086c948954d", submodules=True, tag="v3.7.3"
-    )
-    version(
-        "3.7.2", commit="218dd2c99300e77496239ade76e94b0def65d032", submodules=True, tag="v3.7.2"
-    )
-    version(
-        "3.7.0", commit="fbea2aeb6f7f2d277dcb0ab425a77bb18ed22291", submodules=True, tag="v3.7.0"
-    )
+    with default_args(submodules=True, no_cache=True):
+        version("master")
+        version("3.9.0", commit="b59a1ae535da369db86451e5b28a7bc0eaf3e84a", tag="v3.9.0")
+        version("3.8.1", commit="823e8e399fe8c120c6ec7ec75f09e6106b3074ca", tag="v3.8.1")
+        version("3.7.3", commit="59ac7b980d1ae124aae914fb29cbf086c948954d", tag="v3.7.3")
+        version("3.7.2", commit="218dd2c99300e77496239ade76e94b0def65d032", tag="v3.7.2")
+        version("3.7.0", commit="fbea2aeb6f7f2d277dcb0ab425a77bb18ed22291", tag="v3.7.0")
 
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
@@ -50,6 +46,10 @@ class Arrayfire(CMakePackage, CudaPackage):
 
     depends_on("fontconfig", when="+forge")
     depends_on("glfw@3.1.4:", when="+forge")
+
+    # 3.9.0 introduced a cmake bug due to absolute paths being used.
+    # add_subdirectory not given a binary directory... (referencing internal build of span-lite).
+    patch("add-build-dir-to-cmake.patch", level=0, when="@3.9.0:")
 
     conflicts("cuda_arch=none", when="+cuda", msg="CUDA architecture is required")
 
