d4c1e666e440e7e0aeec02220d769aebbd7d053c
Stephen Herbener <stephen.herbener@gmail.com>
2024-09-20 13:01:29 -0600

    Added new version (0.0.3) and removed obsolete site-packages.patch file

--- var/spack/repos/builtin/packages/bufr-query/package.py
+++ var/spack/repos/builtin/packages/bufr-query/package.py
@@ -18,6 +18,7 @@ class BufrQuery(CMakePackage, PythonExtension):
 
     license("Apache-2.0", checked_by="srherbener")
 
+    version("0.0.3", sha256="f2952a190cc1d7714a3bfe481fb1545459639ba304fc31b941062b471dea1d41")
     version("0.0.2", sha256="b87a128246e79e3c76e3158d89823e2ae38e9ee1a5a81b6f7b423837bdb93a1f")
     version("0.0.1", sha256="001990d864533c101b93d1c351edf50cf8b5ccc575e442d174735f6c332d3d03")
 
