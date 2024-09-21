5a2411b003ec351510cb74778f3a72950b8c33d3
Wouter Deconinck <wdconinc@gmail.com>
2024-09-21 10:23:13 -0500

    bird: change url and checksums, add v2.15.1

--- var/spack/repos/builtin/packages/bird/package.py
+++ var/spack/repos/builtin/packages/bird/package.py
@@ -14,13 +14,15 @@ class Bird(AutotoolsPackage):
     systems and distributed under the GNU General Public License."""
 
     homepage = "https://bird.network.cz/"
-    url = "https://github.com/BIRD/bird/archive/v2.0.2.tar.gz"
+    url = "https://gitlab.nic.cz/labs/bird/-/archive/v2.0.2/bird-v2.0.2.tar.gz"
 
-    license("GPL-2.0-or-later")
+    license("GPL-2.0-or-later", checked_by="wdconinc")
 
-    version("2.0.2", sha256="bd42d48fbcc2c0046d544f1183cd98193ff15b792d332ff45f386b0180b09335")
-    version("2.0.1", sha256="cd6ea4a39ca97ad16d364bf80f919f0e75eba02dd7fe46be40f55d78d022244a")
+    version("2.15.1", sha256="5a4cf55c4767192aa57880ac5f6763e5b8c26f688ab5934df96e3615c4b0a1e1")
+    version("2.0.2", sha256="e1e9ac92faf5893890c478386fdbd3c391ec2e9b911b1dfccec7b7fa825e9820")
+    version("2.0.1", sha256="c222968bb017e6b77d14f4e778f437b84f4ccae686355a3ad8e88799285e7636")
 
+    depends_on("c", type="build")
     depends_on("autoconf", type="build")
     depends_on("automake", type="build")
     depends_on("libtool", type="build")
