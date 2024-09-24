783dcf1f86e9646e4667ee3b34a721cc951763ec
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-09-24 19:17:24 +0200

    omniperf: Fix runtime deps, fix contourpy build
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- var/spack/repos/builtin/packages/omniperf/package.py
+++ var/spack/repos/builtin/packages/omniperf/package.py
@@ -11,7 +11,7 @@ class Omniperf(CMakePackage):
 
     homepage = "https://github.com/ROCm/omniperf"
     git = "https://github.com/ROCm/omniperf.git"
-    url = "https://github.com/ROCm/omniperf/archive/refs/tags/rocm-6.2.0.tar.gz"
+    url = "https://github.com/ROCm/omniperf/archive/refs/tags/rocm-6.2.1.tar.gz"
 
     tags = ["rocm"]
 
@@ -19,11 +19,12 @@ class Omniperf(CMakePackage):
 
     license("MIT")
 
-    version("6.2.0", sha256="b2ad49324a07aef977833d62741509a5d799b92758db56a16b4ab5636b6e231e")
+    version("6.2.1", sha256="c14cb73b9fe17a3cca31489a73e8ea49cb278093f8d64c433b1bac387445074a")
 
     depends_on("python@3.8:")
-    depends_on("py-astunparse@1.6.2:")
-    depends_on("py-colorlover")
+    depends_on("py-pip", type="run")
+    depends_on("py-astunparse@1.6.2", type=("build", "run"))  # wants exact version
+    depends_on("py-colorlover", type=("build", "run"))
     depends_on("py-pyyaml")
     depends_on("py-matplotlib")
     depends_on("py-pandas")
@@ -32,9 +33,9 @@ class Omniperf(CMakePackage):
     depends_on("py-tqdm")
     depends_on("py-kaleido")
     depends_on("py-plotille")
-    depends_on("py-dash-svg")
-    depends_on("py-dash")
-    depends_on("py-dash-bootstrap-components")
+    depends_on("py-dash-svg", type=("build", "run"))
+    depends_on("py-dash", type=("build", "run"))
+    depends_on("py-dash-bootstrap-components", type=("build", "run"))
 
     # VERSION.sha is not in the auto-generated ROCm release tarball
     patch("0001-remove-VERSION.sha-install.patch")
@@ -42,3 +43,7 @@ class Omniperf(CMakePackage):
     def cmake_args(self):
         args = [self.define("ENABLE_TESTS", self.run_tests)]
         return args
+
+    @run_after("install")
+    def after_install(self):
+        touch(join_path(self.spec.prefix.libexec.omniperf, "VERSION.sha"))
