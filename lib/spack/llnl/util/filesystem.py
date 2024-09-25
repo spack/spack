964667bd1234f01ac75d45185b3563dc7b30aa6b
John Parent <john.parent@kitware.com>
2024-09-25 18:18:24 -0400

    Add additional search heuristics

--- lib/spack/llnl/util/filesystem.py
+++ lib/spack/llnl/util/filesystem.py
@@ -2398,6 +2398,9 @@ def find_libraries(
         static_ext = "a"
         shared_ext = "so"
 
+    if sys.platform == "win32":
+        libraries.extend([x.replace("lib", "") for x in libraries])
+
     # Construct the right suffix for the library
     if shared:
         # Used on both Linux and macOS
@@ -2426,13 +2429,12 @@ def find_libraries(
     #   Search for name with platform specific suffix for lib type (i.e. z.a)
     #   Search for name with platform specific prefix (i.e. libz)
     #   Search for both (i.e. libz.a)
-    #   Search for all of the above with a regex to handle any verseioning suffixes (libz.so.2.3)
     #   On Windows search for static libs with name suffix _static
 
     # List of libraries we are searching with suffixes
     libraries.extend(["{0}{1}".format(lib, suffix) for lib in libraries for suffix in suffixes])
     libraries.extend(["{0}.{1}".format(lib, ext) for lib in libraries for ext in extensions])
-    libraries.extend(["{0}{1}".format(prefix, lib) for lib in libraries for prefix in prefixes])
+    libraries.extend(["{0}{1}".format(prefix, lib) for lib in libraries for prefix in prefixes if not lib.startswith(prefix)])
 
     if not recursive:
         if max_depth:
