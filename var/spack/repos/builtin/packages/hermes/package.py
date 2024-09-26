28568b2b137b556a84ab467fa7fe9b8d4f6b1ff9
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-09-26 16:53:06 -0500

    feat(hermes): add more versions and new dependency

--- var/spack/repos/builtin/packages/hermes/package.py
+++ var/spack/repos/builtin/packages/hermes/package.py
@@ -11,38 +11,133 @@ class Hermes(CMakePackage):
     I/O buffering system that aims to significantly accelerate I/O performance.
     """
 
-    homepage = "http://www.cs.iit.edu/~scs/assets/projects/Hermes/Hermes.html"
+    homepage = "https://grc.iit.edu/research/projects/hermes"
     git = "https://github.com/HDFGroup/hermes.git"
 
-    maintainers("hyoklee")
+    maintainers("lukemartinlogan", "hyoklee")
 
-    license("GPL-2.0-only")
+    version("master", branch="master", submodules=True)
 
-    version("master", branch="master")
+    version(
+        "1.2.1",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v1.2.1.tar.gz",
+        sha256="d60ee5d6856dc1a1f389fb08f61252cc7736d1c38d3049043749640897fe3b6d",
+    )
+    version(
+        "1.2.0",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v1.2.0.tar.gz",
+        sha256="280379f393695462279c1af11371995d134f33998596812bd960d6f44a86c339",
+    )
+    version(
+        "1.1.0",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v1.1.0.tar.gz",
+        sha256="022df20d9e394754f6126dfcedd845afde879fcb2738d763f53657e59e058c1e",
+    )
+    version(
+        "1.0.5-beta",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v1.0.5-beta.tar.gz",
+        sha256="1f3ba51a8beda4bc1314d6541b800de1525f5e233a6f498fcde6dc43562ddcb7",
+    )
+    version(
+        "1.0.0-beta",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v1.0.0-beta.tar.gz",
+        sha256="301084cced32aa00532ab4bebd638c31b0512c881ffab20bf5da4b7739defac2",
+    )
+    version(
+        "0.9.9-beta",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v0.9.9-beta.tar.gz",
+        sha256="d2e0025a9bd7a3f05d3ab608c727ed15d86ed30cf582549fe996875daf6cb649",
+    )
+    version(
+        "0.9.8-beta",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v0.9.8-beta.tar.gz",
+        sha256="68e9a977c25c53dcab7d7f6ef0df96b2ba4a09a06aa7c4a490c67faa2a78f077",
+    )
+    version(
+        "0.9.5-beta",
+        url="https://github.com/HDFGroup/hermes/archive/refs/tags/v0.9.5-beta.tar.gz",
+        sha256="f48d15591a6596e8e54897362ec2591bc71e5de92933651f4768145e256336ca",
+    )
     version(
         "0.9.0-beta",
         url="https://github.com/HDFGroup/hermes/archive/refs/tags/v0.9.0-beta.tar.gz",
         sha256="abf258a52fa79729dfeb28559957abf8945f3ad37cadefb3bc685227c5f057a8",
     )
 
+    variant("adios", default=False, description="Build Adios tests")
+    variant("ares", default=False, description="Enable full libfabric install")
+    variant("compress", default=False, description="Build Adios tests")
+    variant("debug", default=False, description="Build shared libraries")
+    variant("encrypt", default=False, description="Build Adios tests")
+    variant("mpiio", default=True, description="Enable MPI I/O adapter")
+    variant("python", default=False, description="Build Python Wrapper")
+    variant("stdio", default=True, description="Enable STDIO adapter")
+    variant("vfd", default=False, description="Enable HDF5 VFD")
+    variant("zmq", default=False, description="Build ZeroMQ tests")
+
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
+    depends_on("libelf")
 
-    variant("vfd", default=False, description="Enable HDF5 VFD")
+    depends_on("hermes_shm@master+boost+cereal+mochi")
 
-    depends_on("mochi-thallium~cereal@0.8:")
-    depends_on("catch2")
-    depends_on("glpk")
-    depends_on("glog@0.4.0:")
-    depends_on("mpi")
-    depends_on("hdf5@1.14.0:", when="+vfd")
-    depends_on("yaml-cpp")
+    depends_on("hermes_shm+adios", when="+adios")
+    depends_on("hermes_shm+ares", when="+ares")
+    depends_on("hermes_shm+compress", when="+compress")
+    depends_on("hermes_shm+debug", when="+debug")
+    depends_on("hermes_shm+encrypt", when="+encrypt")
+    depends_on("hermes_shm+mpiio", when="+mpiio")
+    depends_on("hermes_shm+vfd", when="+vfd")
+    depends_on("hermes_shm+zmq", when="+zmq")
 
     def cmake_args(self):
-        args = [
-            self.define("HERMES_RPC_THALLIUM", True),
-            self.define("HERMES_INSTALL_TESTS", True),
-            self.define("BUILD_TESTING", True),
-            self.define_from_variant("HERMES_ENABLE_VFD", "vfd"),
-        ]
+        args = []
+        if "+debug" in self.spec:
+            args.append("-DCMAKE_BUILD_TYPE=Debug")
+        else:
+            args.append("-DCMAKE_BUILD_TYPE=Release")
+        if "+mpiio" in self.spec:
+            args.append("-DHERMES_ENABLE_MPIIO_ADAPTER=ON")
+            if "openmpi" in self.spec:
+                args.append("-DHERMES_OPENMPI=ON")
+            elif "mpich" in self.spec:
+                args.append("-DHERMES_MPICH=ON")
+        if "+stdio" in self.spec:
+            args.append("-DHERMES_ENABLE_STDIO_ADAPTER=ON")
+        if "+vfd" in self.spec:
+            args.append("-DHERMES_ENABLE_VFD=ON")
+        if "+compress" in self.spec:
+            args.append(self.define("HERMES_ENABLE_COMPRESSION", "ON"))
+        if "+encrypt" in self.spec:
+            args.append(self.define("HERMES_ENABLE_ENCRYPTION", "ON"))
+        if "+adios" in self.spec:
+            args.append(self.define("HERMES_ENABLE_ADIOS", "ON"))
+        if "+python" in self.spec:
+            args.append(self.define("HERMES_ENABLE_PYTHON", "ON"))
         return args
+
+    def set_include(self, env, path):
+        env.append_flags("CFLAGS", "-I{}".format(path))
+        env.append_flags("CXXFLAGS", "-I{}".format(path))
+        env.prepend_path("INCLUDE", "{}".format(path))
+        env.prepend_path("CPATH", "{}".format(path))
+
+    def set_lib(self, env, path):
+        env.prepend_path("LIBRARY_PATH", path)
+        env.prepend_path("LD_LIBRARY_PATH", path)
+        env.append_flags("LDFLAGS", "-L{}".format(path))
+        env.prepend_path("PYTHONPATH", "{}".format(path))
+
+    def set_flags(self, env):
+        self.set_include(env, "{}/include".format(self.prefix))
+        self.set_include(env, "{}/include".format(self.prefix))
+        self.set_lib(env, "{}/lib".format(self.prefix))
+        self.set_lib(env, "{}/lib64".format(self.prefix))
+        env.prepend_path("CMAKE_PREFIX_PATH", "{}/cmake".format(self.prefix))
+        env.prepend_path("CMAKE_MODULE_PATH", "{}/cmake".format(self.prefix))
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        self.set_flags(spack_env)
+
+    def setup_run_environment(self, env):
+        self.set_flags(env)
