481a9410c8422ed4fb9794a3a1317fcb5c90ce62
Alec Scott <scott112@llnl.gov>
2024-09-27 10:44:46 -0700

    Update path functions to take pathlike inputs

--- lib/spack/spack/package_completions.py
+++ lib/spack/spack/package_completions.py
@@ -4,44 +4,45 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from pathlib import Path
+from typing import Union
 
 """Functions relating to shell autocompletion scripts for packages."""
 
 
-def bash_completion_path(root: str) -> Path:
+def bash_completion_path(root: Union[str, Path]) -> Path:
     """
     Return standard path for bash completion scripts under root.
 
     Args:
-        root (str): The prefix root under which to generate the path.
+        root (Union[str, Path]): The prefix root under which to generate the path.
 
     Returns:
         Standard path for bash completion scripts under root.
     """
-    return Path(root + "/share/bash-completion/completions")
+    return Path(root) / "share" / "bash-completion" / "completions"
 
 
-def zsh_completion_path(root: str) -> Path:
+def zsh_completion_path(root: Union[str, Path]) -> Path:
     """
     Return standard path for zsh completion scripts under root.
 
     Args:
-        root (str): The prefix root under which to generate the path.
+        root (Union[str, Path]): The prefix root under which to generate the path.
 
     Returns:
         Standard path for zsh completion scripts under root.
     """
-    return Path(root + "/share/zsh/site-functions")
+    return Path(root) / "share" / "zsh" / "site-functions"
 
 
-def fish_completion_path(root: str) -> Path:
+def fish_completion_path(root: Union[str, Path]) -> Path:
     """
     Return standard path for fish completion scripts under root.
 
     Args:
-        root (str): The prefix root under which to generate the path.
+        root (Union[str, Path]): The prefix root under which to generate the path.
 
     Returns:
         Standard path for fish completion scripts under root.
     """
-    return Path(root + "/share/fish/vendor_completions.d")
+    return Path(root) / "share" / "fish" / "vendor_completions.d"
