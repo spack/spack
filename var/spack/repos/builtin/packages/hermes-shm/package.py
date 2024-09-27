9f26071c9bc5941457010a7b9edef87dc819c527
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-09-27 12:00:39 -0500

    fix(hermes-shm): address @alalazo reviews

--- var/spack/repos/builtin/packages/hermes-shm/package.py
+++ var/spack/repos/builtin/packages/hermes-shm/package.py
@@ -85,27 +85,3 @@ class HermesShm(CMakePackage):
             self.define_from_variant("HERMES_ENABLE_ZMQ_TESTS", "zmq"),
         ]
 
-    def set_include(self, env, path):
-        env.append_flags("CFLAGS", "-I{}".format(path))
-        env.append_flags("CXXFLAGS", "-I{}".format(path))
-        env.prepend_path("INCLUDE", "{}".format(path))
-        env.prepend_path("CPATH", "{}".format(path))
-
-    def set_lib(self, env, path):
-        env.prepend_path("LIBRARY_PATH", path)
-        env.prepend_path("LD_LIBRARY_PATH", path)
-        env.append_flags("LDFLAGS", "-L{}".format(path))
-
-    def set_flags(self, env):
-        self.set_include(env, "{}/include".format(self.prefix))
-        self.set_include(env, "{}/include".format(self.prefix))
-        self.set_lib(env, "{}/lib".format(self.prefix))
-        self.set_lib(env, "{}/lib64".format(self.prefix))
-        env.prepend_path("CMAKE_PREFIX_PATH", "{}/cmake".format(self.prefix))
-        env.prepend_path("CMAKE_MODULE_PATH", "{}/cmake".format(self.prefix))
-
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        self.set_flags(spack_env)
-
-    def setup_run_environment(self, env):
-        self.set_flags(env)
