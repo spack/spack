3bdcf15e9248e3813a50f4f519ce6ade56591517
Wouter Deconinck <wdconinc@gmail.com>
2024-09-28 11:42:01 -0500

    turnserver: add v4.6.2 (fix CVEs)

--- var/spack/repos/builtin/packages/turnserver/package.py
+++ var/spack/repos/builtin/packages/turnserver/package.py
@@ -9,13 +9,18 @@ from spack.package import *
 class Turnserver(AutotoolsPackage):
     """coturn TURN server project."""
 
-    homepage = "https://coturn.net/turnserver"
-    url = "https://coturn.net/turnserver/v4.5.1.3/turnserver-4.5.1.3.tar.gz"
+    homepage = "https://github.com/coturn/coturn"
+    url = "https://github.com/coturn/coturn/archive/4.6.2.tar.gz"
 
-    license("OpenSSL")
+    license("BSD-3-Clause", checked_by="wdconinc")
 
-    version("4.5.1.3", sha256="408bf7fde455d641bb2a23ba2df992ea0ae87b328de74e66e167ef58d8e9713a")
+    version("4.6.2", sha256="408bf7fde455d641bb2a23ba2df992ea0ae87b328de74e66e167ef58d8e9713a")
+    version(
+        "4.5.1.3",
+        sha256="408bf7fde455d641bb2a23ba2df992ea0ae87b328de74e66e167ef58d8e9713a",
+        url="https://coturn.net/turnserver/v4.5.1.3/turnserver-4.5.1.3.tar.gz",
+    )
 
-    depends_on("c", type="build")  # generated
+    depends_on("c", type="build")
 
     depends_on("libevent")
