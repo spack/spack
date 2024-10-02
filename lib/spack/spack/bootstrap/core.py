da0dc92b611abef4840f4afecd4a425bb71aa691
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-10-02 16:33:25 +0200

    Restore bootstrapping from sources
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/bootstrap/core.py
+++ lib/spack/spack/bootstrap/core.py
@@ -278,7 +278,12 @@ class SourceBootstrapper(Bootstrapper):
 
         # Install the spec that should make the module importable
         with spack.config.override(self.mirror_scope):
-            PackageInstaller([concrete_spec.package], fail_fast=True).install()
+            PackageInstaller(
+                [concrete_spec.package],
+                fail_fast=True,
+                package_use_cache=False,
+                dependencies_use_cache=False,
+            ).install()
 
         if _try_import_from_store(module, query_spec=concrete_spec, query_info=info):
             self.last_search = info
