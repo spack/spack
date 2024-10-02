7f6344010ff4a0699953b04c8922fa6ce78d9a4f
Adam J. Stewart <ajstewart426@gmail.com>
2024-10-02 23:03:01 +0200

    Try TF_NVCC_CLANG

--- var/spack/repos/builtin/packages/py-jaxlib/package.py
+++ var/spack/repos/builtin/packages/py-jaxlib/package.py
@@ -184,7 +184,7 @@ build --local_cpu_resources={make_jobs}
                     [
                         f"--bazel_options=--repo_env=LOCAL_CUDA_PATH={spec['cuda'].prefix}",
                         f"--bazel_options=--repo_env=LOCAL_CUDNN_PATH={spec['cudnn'].prefix}",
-                        "--bazel_options=--action_env=CUDA_NVCC=1",
+                        "--bazel_options=--action_env=TF_NVCC_CLANG=1",
                         "--bazel_options=--@local_config_cuda//:cuda_compiler=nvcc",
                     ]
                 )
