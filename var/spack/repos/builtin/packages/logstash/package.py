7c32d79527470f961c488834942b5c17d901529a
Wouter Deconinck <wdconinc@gmail.com>
2024-10-08 15:29:07 -0500

    logstash: depends_on java@11: and set LS_JAVA_HOME

--- var/spack/repos/builtin/packages/logstash/package.py
+++ var/spack/repos/builtin/packages/logstash/package.py
@@ -22,14 +22,17 @@ class Logstash(Package):
         # https://nvd.nist.gov/vuln/detail/CVE-2019-7612
         version("6.6.0", sha256="5a9a8b9942631e9d4c3dfb8d47075276e8c2cff343841145550cc0c1cfe7bba7")
 
-    depends_on("c", type="build")  # generated
-    depends_on("cxx", type="build")  # generated
+    depends_on("java@11:", type="build")
 
     def url_for_version(self, version):
         if self.spec.satisfies("@:6"):
-            return f"https://artifacts.elastic.co/downloads/logstash/logstash-{version}.tar.gz"
+            return automatically added to the packagef"https://artifacts.elastic.co/downloads/logstash/logstash-{version}.tar.gz"
         else:
             return f"https://artifacts.elastic.co/downloads/logstash/logstash-{version}-linux-x86_64.tar.gz"
 
     def install(self, spec, prefix):
         install_tree(".", prefix)
+
+    def setup_run_environment(self, env):
+        # do not use the bundled jdk
+        env.set("LS_JAVA_HOME", self.spec["java"].home)
