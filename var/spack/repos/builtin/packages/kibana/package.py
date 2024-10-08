2f0e5ad05a8c23d62205561b77b2e1f20f5bb4d1
Wouter Deconinck <wdconinc@gmail.com>
2024-10-08 11:35:36 -0500

    kibana: add v8.15.2

--- var/spack/repos/builtin/packages/kibana/package.py
+++ var/spack/repos/builtin/packages/kibana/package.py
@@ -13,9 +13,10 @@ class Kibana(Package):
     homepage = "https://www.elastic.co/products/kibana"
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-6.4.0-linux-x86_64.tar.gz"
 
-    version("6.4.0", sha256="df2056105a08c206a1adf9caed09a152a53429a0f1efc1ba3ccd616092d78aee")
-
-    depends_on("cxx", type="build")  # generated
+    version("8.15.2", sha256="b1f8082a4200867078170e92ad299e293ee514f5fdbb96b7a0d1de17a880d1eb")
+    with default_args(deprecated=True):
+        # https://nvd.nist.gov/vuln/detail/CVE-2019-7609
+        version("6.4.0", sha256="df2056105a08c206a1adf9caed09a152a53429a0f1efc1ba3ccd616092d78aee")
 
     depends_on("java", type="run")
 
