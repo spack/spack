480f14e9f2eb88ebcbbcdb5b62ac411992d129a8
Wouter Deconinck <wdconinc@gmail.com>
2024-10-10 08:33:31 -0500

    zookeeper: add v3.8.4

--- var/spack/repos/builtin/packages/zookeeper/package.py
+++ var/spack/repos/builtin/packages/zookeeper/package.py
@@ -13,14 +13,15 @@ class Zookeeper(Package):
     """
 
     homepage = "https://archive.apache.org"
-    url = "https://archive.apache.org/dist/zookeeper/zookeeper-3.4.11/zookeeper-3.4.11.tar.gz"
+    urls = [
+        "https://archive.apache.org/dist/zookeeper/zookeeper-3.8.4/apache-zookeeper-3.8.4.tar.gz",
+        "https://archive.apache.org/dist/zookeeper/zookeeper-3.4.11/zookeeper-3.4.11.tar.gz",
+    ]
 
     license("Apache-2.0")
 
+    version("3.8.4", sha256="b15bf5b4e4977d04447be0c53743c57df985b6f6786c067b12821a8d79922294")
     version("3.4.11", sha256="f6bd68a1c8f7c13ea4c2c99f13082d0d71ac464ffaf3bf7a365879ab6ad10e84")
-
-    depends_on("c", type="build")  # generated
-    depends_on("cxx", type="build")  # generated
 
     def install(self, spec, prefix):
         install_tree(".", prefix)
