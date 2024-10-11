11d4ec59f0b7f185b5557ab956b59ce05fceae8a
Wouter Deconinck <wdconinc@gmail.com>
2024-10-11 18:56:02 -0500

    ftgl: fix style

--- var/spack/repos/builtin/packages/ftgl/package.py
+++ var/spack/repos/builtin/packages/ftgl/package.py
@@ -37,7 +37,7 @@ class Ftgl(CMakePackage):
     patch(
         "https://patch-diff.githubusercontent.com/raw/frankheckenbach/ftgl/pull/20.patch?full_index=1",
         sha256="e2a0810fbf68403931bef4fbfda22e010e01421c92eeaa45f62e4e47f2381ebd",
-        when="@2.4.0"
+        when="@2.4.0",
     )
 
     def cmake_args(self):
