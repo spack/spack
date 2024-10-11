38f0cb7ec4892d7019c3339957a456ff56fe4704
Bernhard Kaindl <bernhardkaindl7@gmail.com>
2024-10-11 22:59:00 +0000

    Let's mark @:2.10.32 as deprecated and remove after 0.23 is released.

--- var/spack/repos/builtin/packages/gimp/package.py
+++ var/spack/repos/builtin/packages/gimp/package.py
@@ -26,11 +26,22 @@ class Gimp(AutotoolsPackage):
     license("GPL-3.0-or-later")
 
     version("2.10.38", sha256="50a845eec11c8831fe8661707950f5b8446e35f30edfb9acf98f85c1133f856e")
-    version("2.10.32", sha256="3f15c70554af5dcc1b46e6dc68f3d8f0a6cc9fe56b6d78ac08c0fd859ab89a25")
-    version("2.10.30", sha256="88815daa76ed7d4277eeb353358bafa116cd2fcd2c861d95b95135c1d52b67dc")
-    version("2.10.28", sha256="4f4dc22cff1ab5f026feaa2ab55e05775b3a11e198186b47bdab79cbfa078826")
-    version("2.10.26", sha256="5ddbccf1db462a41df9a26197fcb0d24c7152753a36b3c8b8a9506b4136395f7")
-    version("2.10.24", sha256="bd1bb762368c0dd3175cf05006812dd676949c3707e21f4e6857435cb435989e")
+    with default_args(deprecated=True):
+        version(
+            "2.10.32", sha256="3f15c70554af5dcc1b46e6dc68f3d8f0a6cc9fe56b6d78ac08c0fd859ab89a25"
+        )
+        version(
+            "2.10.30", sha256="88815daa76ed7d4277eeb353358bafa116cd2fcd2c861d95b95135c1d52b67dc"
+        )
+        version(
+            "2.10.28", sha256="4f4dc22cff1ab5f026feaa2ab55e05775b3a11e198186b47bdab79cbfa078826"
+        )
+        version(
+            "2.10.26", sha256="5ddbccf1db462a41df9a26197fcb0d24c7152753a36b3c8b8a9506b4136395f7"
+        )
+        version(
+            "2.10.24", sha256="bd1bb762368c0dd3175cf05006812dd676949c3707e21f4e6857435cb435989e"
+        )
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
