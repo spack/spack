faab306e5a0a1e12eb83cd699cf63dce952c594c
Wouter Deconinck <wdconinc@gmail.com>
2024-10-12 18:58:38 -0500

    dosfstools: add v4.2

--- var/spack/repos/builtin/packages/dosfstools/package.py
+++ var/spack/repos/builtin/packages/dosfstools/package.py
@@ -15,10 +15,11 @@ class Dosfstools(AutotoolsPackage):
 
     license("GPL-3.0-or-later")
 
+    version("4.2", sha256="355a6725524b50e64ae94060ed28579e0e004c519fc964f7085188cd87a99ba7")
     version("4.1", sha256="8ff9c2dcc01551fe9de8888cb41eb1051fd58bdf1ab3a93d3d88916f0a4ffd1b")
     version("4.0", sha256="77975e289e695cb8c984a3c0a15a3bbf3af90be83c26983d43abcde9ec48eea5")
 
-    depends_on("c", type="build")  # generated
+    depends_on("c", type="build")
 
     depends_on("automake", type="build")
     depends_on("autoconf", type="build")
