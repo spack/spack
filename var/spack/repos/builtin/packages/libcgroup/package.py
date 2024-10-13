578dc0d139017a0b368cce7a6d8e627d12c62da3
Wouter Deconinck <wdconinc@gmail.com>
2024-10-13 11:06:00 -0500

    libcgroup: deprecate versions up to 0.41

--- var/spack/repos/builtin/packages/libcgroup/package.py
+++ var/spack/repos/builtin/packages/libcgroup/package.py
@@ -15,9 +15,11 @@ class Libcgroup(AutotoolsPackage):
     license("LGPL-2.1-only")
 
     version("3.1.0", sha256="976ec4b1e03c0498308cfd28f1b256b40858f636abc8d1f9db24f0a7ea9e1258")
-    version("0.41", sha256="e4e38bdc7ef70645ce33740ddcca051248d56b53283c0dc6d404e17706f6fb51")
-    version("0.37", sha256="15c8f3febb546530d3495af4e4904b3189c273277ca2d8553dec882cde1cd0f6")
-    version("0.36", sha256="8dcd2ae220435b3de736d3efb0023fdf1192d7a7f4032b439f3cf5342cff7b4c")
+    with default_args(deprecated=True):
+        # https://nvd.nist.gov/vuln/detail/CVE-2018-14348
+        version("0.41", sha256="e4e38bdc7ef70645ce33740ddcca051248d56b53283c0dc6d404e17706f6fb51")
+        version("0.37", sha256="15c8f3febb546530d3495af4e4904b3189c273277ca2d8553dec882cde1cd0f6")
+        version("0.36", sha256="8dcd2ae220435b3de736d3efb0023fdf1192d7a7f4032b439f3cf5342cff7b4c")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
