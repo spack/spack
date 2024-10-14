a5c734cb8a1e9800aa58f21d445727d7ee81da65
caetano melone <caetanomelone@gmail.com>
2024-10-14 10:16:45 -0300

    add oom check script

---
+++ share/spack/gitlab/cloud_pipelines/scripts/common/oom-check.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$CI_JOB_STATUS" != "failed" ]; then
+    exit 0
+fi
+
+# TODO if it's a UO runner then we don't want to check for OOM
+echo $CI_RUNNER_DESCRIPTION
+echo $CI_RUNNER_ID
+echo $CI_RUNNER_SHORT_TOKEN
+echo $CI_RUNNER_TAGS
+
+proc1_cgroup=$(cat /proc/1/cgroup)
+
+(
+  dmesg | grep -q "oom-kill.*$(echo "$proc1_cgroup" | tr / '\n' | tail -1)" \
+    && echo "oom kill"
+) || (
+  dmesg | grep -q "oom-kill.*$(echo "$proc1_cgroup" | tr / '\n' | tail -2 | head -1)" \
+    && echo "oom kill pod"
+) || true
