b9873c5cea18b3c1573b57372ed7575ca3fef151
Harmen Stoppels <me@harmenstoppels.nl>
2024-10-15 11:30:07 +0200

    python: drop build-tools tag (#46980)
    Remove the `build-tools` tag of python, otherwise these types of
    concretizations are possible:
    
    ```
    py-root
      ^py-pip
        ^python@3.12
      ^python@3.13
    ```
    
    So, a package would be configured with py-pip using python 3.12, but
    installed for 3.13, which does not work.

--- var/spack/repos/builtin/packages/python/package.py
+++ var/spack/repos/builtin/packages/python/package.py
@@ -47,7 +47,7 @@ class Python(Package):
     url = "https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz"
     list_url = "https://www.python.org/ftp/python/"
     list_depth = 1
-    tags = ["windows", "build-tools"]
+    tags = ["windows"]
 
     maintainers("skosukhin", "scheibelp")
 
