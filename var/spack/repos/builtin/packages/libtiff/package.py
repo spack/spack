686c29b72deafc429f270cf4cb0f8f1db5cb3cda
wdconinc <wdconinc@users.noreply.github.com>
2024-10-15 19:50:24 +0000

    [@spackbot] updating style on behalf of wdconinc

--- var/spack/repos/builtin/packages/libtiff/package.py
+++ var/spack/repos/builtin/packages/libtiff/package.py
@@ -50,7 +50,9 @@ class Libtiff(CMakePackage, AutotoolsPackage):
         version("4.3.0", sha256="0e46e5acb087ce7d1ac53cf4f56a09b221537fc86dfc5daaad1c2e89e1b37ac8")
         version("4.2.0", sha256="eb0484e568ead8fa23b513e9b0041df7e327f4ee2d22db5a533929dfc19633cb")
         version("4.1.0", sha256="5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634")
-        version("4.0.10", sha256="2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4")
+        version(
+            "4.0.10", sha256="2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4"
+        )
         version("4.0.9", sha256="6e7bdeec2c310734e734d19aae3a71ebe37a4d842e0e23dbb1b8921c0026cfcd")
         version("4.0.8", sha256="59d7a5a8ccd92059913f246877db95a2918e6c04fb9d43fd74e5c3390dac2910")
         version("4.0.7", sha256="9f43a2cfb9589e5cecaa66e16bf87f814c945f22df7ba600d63aac4632c4f019")
