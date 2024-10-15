00ef9f6a72d42944769f90ed9a9e79798e918ded
John Parent <john.parent@kitware.com>
2024-10-15 13:46:47 -0400

    Libmng: Restore Autotools system
    CMake, when building its Qt gui, depends on Qt, which in turn, depends on libmng, a CMake based build. To avoid this obvious cyclic dependency, we re-introduce libmng's autotools build into Spack and require when building Qt as a CMake dependency, libmng is built with autotools

--- var/spack/repos/builtin/packages/cmake/package.py
+++ var/spack/repos/builtin/packages/cmake/package.py
@@ -161,7 +161,7 @@ class Cmake(Package):
     depends_on("gmake", when="platform=darwin")
     depends_on("gmake", when="platform=freebsd")
 
-    depends_on("qt", when="+qtgui")
+    depends_on("qt ^libmng build_system=autotools", when="+qtgui")
 
     # See https://gitlab.kitware.com/cmake/cmake/-/issues/21135
     conflicts(
