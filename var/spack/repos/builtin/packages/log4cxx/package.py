5008519a5600b41fd4d9c6487e8aee89ee8b7c52
Wouter Deconinck <wdconinc@gmail.com>
2024-10-14 22:46:10 -0500

    log4cxx: add v1.2.0 (#46974)

--- var/spack/repos/builtin/packages/log4cxx/package.py
+++ var/spack/repos/builtin/packages/log4cxx/package.py
@@ -14,18 +14,41 @@ class Log4cxx(CMakePackage):
 
     maintainers("nicmcd")
 
-    license("Apache-2.0")
+    license("Apache-2.0", checked_by="wdconinc")
 
-    version("0.12.1", sha256="7bea5cb477f0e31c838f0e1f4f498cc3b30c2eae74703ddda923e7e8c2268d22")
-    version("0.12.0", sha256="bd5b5009ca914c8fa7944b92ea6b4ca6fb7d146f65d526f21bf8b3c6a0520e44")
+    version("1.2.0", sha256="09f4748aa5675ef5c0770bedbf5e00488668933c5a935a43ac5b85be2436c48a")
+    with default_args(deprecated=True):
+        # https://nvd.nist.gov/vuln/detail/CVE-2023-31038
+        version(
+            "0.12.1", sha256="7bea5cb477f0e31c838f0e1f4f498cc3b30c2eae74703ddda923e7e8c2268d22"
+        )
+        version(
+            "0.12.0", sha256="bd5b5009ca914c8fa7944b92ea6b4ca6fb7d146f65d526f21bf8b3c6a0520e44"
+        )
 
-    variant("cxxstd", default="17", description="C++ standard", values=("11", "17"), multi=False)
+    variant(
+        "cxxstd",
+        default="17",
+        description="C++ standard",
+        values=("11", "17"),
+        multi=False,
+        when="@:1.1",
+    )
+    variant(
+        "cxxstd",
+        default="20",
+        description="C++ standard",
+        values=("11", "17", "20"),
+        multi=False,
+        when="@1.2:",
+    )
 
     depends_on("cmake@3.13:", type="build")
 
     depends_on("apr-util")
     depends_on("apr")
     depends_on("boost+thread+system", when="cxxstd=11")
+    depends_on("expat")
     depends_on("zlib-api")
     depends_on("zip")
 
