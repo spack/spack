76414a8216137dfd807fae20ed752a60ac73210a
Wouter Deconinck <wdconinc@gmail.com>
2024-10-15 14:31:17 -0500

    yara: add v4.5.2

--- var/spack/repos/builtin/packages/yara/package.py
+++ var/spack/repos/builtin/packages/yara/package.py
@@ -15,6 +15,7 @@ class Yara(AutotoolsPackage):
 
     license("BSD-3-Clause")
 
+    version("4.5.2", sha256="1f87056fcb10ee361936ee7b0548444f7974612ebb0e681734d8de7df055d1ec")
     version("3.9.0", sha256="ebe7fab0abadb90449a62afbd24e196e18b177efe71ffd8bf22df95c5386f64d")
 
     depends_on("c", type="build")  # generated
@@ -24,3 +25,4 @@ class Yara(AutotoolsPackage):
     depends_on("automake", type="build")
     depends_on("libtool", type="build")
     depends_on("m4", type="build")
+    depends_on("pkgconfig", type="build", when="@4:")
