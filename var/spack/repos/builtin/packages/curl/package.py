24cf5bda0cd963aba2d4d9739598b0bd8816d24a
Harmen Stoppels <me@harmenstoppels.nl>
2024-10-16 21:01:33 +0200

    curl: mbedtls 3.6.0 bound should be forward not backward compat

--- var/spack/repos/builtin/packages/curl/package.py
+++ var/spack/repos/builtin/packages/curl/package.py
@@ -114,9 +114,10 @@ class Curl(NMakePackage, AutotoolsPackage):
     depends_on("gnutls", when="tls=gnutls")
 
     with when("tls=mbedtls"):
-        depends_on("mbedtls@:2 +pic", when="@:7.78")
-        depends_on("mbedtls@2: +pic", when="@7.79:")
-        depends_on("mbedtls@3.6.0: +pic", when="@8.8.0:")
+        depends_on("mbedtls +pic")
+        depends_on("mbedtls@:2", when="@:7.78")
+        depends_on("mbedtls@:3.5", when="@:8.7")
+        depends_on("mbedtls@2:", when="@7.79:")
 
     depends_on("nss", when="tls=nss")
 
