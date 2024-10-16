ad37d7829e871a7cc4b40f6bd6e1d4cc198b9679
Andres Quan <aquan9@users.noreply.github.com>
2024-10-16 18:09:30 +0000

    Add myself as a maintainer

--- var/spack/repos/builtin/packages/py-hpc-beeflow/package.py
+++ var/spack/repos/builtin/packages/py-hpc-beeflow/package.py
@@ -18,6 +18,7 @@ class PyHpcBeeflow(PythonPackage):
     pypi = "hpc-beeflow/hpc_beeflow-0.1.9.tar.gz"
 
     license("MIT")
+    maintainers("aquan9")
 
     version("0.1.9", sha256="196eb9155a5ca6e35d0cc514e0609cf352fc757088707306653496b83a311ac1")
     version("0.1.8", sha256="7ffc7f2a6e6c4892b9432ba939ee7179d1189cedf82382752a3c268d70fceddd")
