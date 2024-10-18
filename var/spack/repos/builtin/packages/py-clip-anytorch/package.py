0d8d7d14527a696dda3be38f0a1dabe4b862e6b3
Bernhard Kaindl <bernhardkaindl7@gmail.com>
2024-10-18 18:05:20 +0200

    Apply suggestion from Wouter on fixing CI
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- var/spack/repos/builtin/packages/py-clip-anytorch/package.py
+++ var/spack/repos/builtin/packages/py-clip-anytorch/package.py
@@ -13,7 +13,7 @@ class PyClipAnytorch(PythonPackage):
     natural language to predict the most relevant text snippet, given an image,
     without directly optimizing for the task, similarly to the zero-shot
     capabilities of GPT-2 and 3. We found CLIP matches the performance of the
-    original ResNet50 on ImageNet “zero-shot” without using any of the original
+    original ResNet50 on ImageNet "zero-shot" without using any of the original
     1.28M labeled examples, overcoming several major challenges in computer
     vision."""
 
