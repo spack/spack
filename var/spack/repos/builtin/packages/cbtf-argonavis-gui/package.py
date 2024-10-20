09ed560947656ca02a637d7c5f6c4877000cf0b8
Tim Haines <thaines.astro@gmail.com>
2024-10-19 23:56:46 -0500

    cbtf-argonavis-gui: Update Boost dependency

--- var/spack/repos/builtin/packages/cbtf-argonavis-gui/package.py
+++ var/spack/repos/builtin/packages/cbtf-argonavis-gui/package.py
@@ -6,7 +6,6 @@
 import os
 
 from spack.package import *
-from spack.pkg.builtin.boost import Boost
 
 
 class CbtfArgonavisGui(QMakePackage):
@@ -28,12 +27,7 @@ class CbtfArgonavisGui(QMakePackage):
 
     depends_on("qt@5.10.0:")
 
-    depends_on("boost@1.66.0:1.69.0")
-
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
-    # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants)
+    depends_on("boost@1.66.0:+filesystem+program_options+system+test")
 
     # For MRNet
     depends_on("mrnet@5.0.1-3+lwthreads", when="@1.3.0.0:9999")
