d8015edc0395012cef6a72c72177cfade7fc0b3a
Tim Haines <thaines.astro@gmail.com>
2024-10-20 00:01:31 -0500

    dire: Update Boost dependency
    The only version currently available is 2.004, and it does not use Boost.

--- var/spack/repos/builtin/packages/dire/package.py
+++ var/spack/repos/builtin/packages/dire/package.py
@@ -27,10 +27,6 @@ class Dire(Package):
 
     depends_on("zlib-api")
 
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
-    # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants)
     depends_on("lhapdf")
     depends_on("hepmc")
     depends_on("pythia8@8.226:")
