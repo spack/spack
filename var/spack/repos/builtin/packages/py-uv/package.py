59a2a87937f1f733283fea01d0ee77a813ddad46
Ashim Mahara <48154590+ashim-mahara@users.noreply.github.com>
2024-10-22 16:54:31 -0400

    py-uv: relaxing rust dependency (#47148)

--- var/spack/repos/builtin/packages/py-uv/package.py
+++ var/spack/repos/builtin/packages/py-uv/package.py
@@ -18,8 +18,8 @@ class PyUv(PythonPackage):
     version("0.4.16", sha256="2144995a87b161d063bd4ef8294b1e948677bd90d01f8394d0e3fca037bb847f")
     version("0.4.15", sha256="8e36b8e07595fc6216d01e729c81a0b4ff029a93cc2ef987a73d3b650d6d559c")
 
-    depends_on("rust@1.81:", type=("build", "run"))
+    depends_on("rust", type=("build", "run"))
     depends_on("python@3.8:", type=("build", "run"))
-    depends_on("py-maturin@1:1", type=("build"))
+    depends_on("py-maturin@1:1", type="build")
 
     executables = ["^uv$"]
