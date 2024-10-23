e63e8b5efa46658d28e671de4106f96752fa3788
Laura Weber <LRWeber@users.noreply.github.com>
2024-10-23 00:17:14 -0700

    cqrlib: added version 1.1.3, added Makefile patch to fix libtool error (#47153)

--- var/spack/repos/builtin/packages/cqrlib/package.py
+++ var/spack/repos/builtin/packages/cqrlib/package.py
@@ -10,12 +10,10 @@ class Cqrlib(MakefilePackage):
     """CQRlib -- ANSI C API for Quaternion Rotations"""
 
     homepage = "https://cqrlib.sourceforge.net/"
-    url = (
-        "https://downloads.sourceforge.net/project/cqrlib/cqrlib/CQRlib-1.1.2/CQRlib-1.1.2.tar.gz"
-    )
 
     license("LGPL-2.1-or-later")
 
+    version("1.1.3", sha256="90ecd9aabfb72e55e56957c7b233910d18b8c2bb522a8e59eddbcc4618c72d0e")
     version("1.1.2", sha256="af3cf2402974579f3c6efc6a6174a5da52786db4bfee9d38d504d93bc42410fd")
 
     depends_on("c", type="build")  # generated
@@ -23,7 +21,12 @@ class Cqrlib(MakefilePackage):
 
     depends_on("libtool", type="build")
 
-    patch("cqr.patch")
+    patch("cqr.patch", when="@1.1.2")
+    patch("Makefile.patch", when="@1.1.2:")
+
+    def url_for_version(self, version):
+        full_vers = str(version)
+        return f"https://downloads.sourceforge.net/project/cqrlib/cqrlib/CQRlib-{full_vers}/CQRlib-{full_vers}.tar.gz"
 
     def edit(self, spec, prefix):
         mf = FileFilter("Makefile")
