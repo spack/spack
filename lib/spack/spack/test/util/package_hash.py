30533486ba0b1e9ba3ce2edf9bd779cca53e8125
Jordan Galby <jordan.galby@cea.fr>
2024-11-07 15:19:48 +0100

    package_hash: Fix roundtrip after docstring removal
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/spack/test/util/package_hash.py
+++ lib/spack/spack/test/util/package_hash.py
@@ -174,6 +174,9 @@ class ManyDocstrings:
     def method2():
         """ELEVEN"""
         return "TWELVE"
+
+def empty_func():
+    """THIRTEEN"""
 '''
 
 many_strings_no_docstrings = """\
@@ -189,6 +192,9 @@ class ManyDocstrings:
 
     def method2():
         return 'TWELVE'
+
+def empty_func():
+    pass
 """
 
 
