5064b099610eeb6c7c7d4edd631d6ee592a24e74
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-11-12 11:12:26 +0100

    Exempt "compilers" and "runtimes" from default requirements

--- var/spack/repos/builtin.mock/packages/gcc/package.py
+++ var/spack/repos/builtin.mock/packages/gcc/package.py
@@ -12,9 +12,10 @@ class Gcc(CompilerPackage, Package):
     homepage = "http://www.example.com"
     url = "http://www.example.com/gcc-1.0.tar.gz"
 
-    version("1.0", md5="0123456789abcdef0123456789abcdef")
-    version("2.0", md5="abcdef0123456789abcdef0123456789")
+    version("14.0", md5="abcdef0123456789abcdef0123456789")
     version("3.0", md5="def0123456789abcdef0123456789abc")
+    version("2.0", md5="abcdef0123456789abcdef0123456789")
+    version("1.0", md5="0123456789abcdef0123456789abcdef")
 
     variant(
         "languages",
@@ -27,6 +28,8 @@ class Gcc(CompilerPackage, Package):
     provides("c", when="languages=c")
     provides("cxx", when="languages=c++")
     provides("fortran", when="languages=fortran")
+
+    depends_on("c", type="build")
 
     c_names = ["gcc"]
     cxx_names = ["g++"]
