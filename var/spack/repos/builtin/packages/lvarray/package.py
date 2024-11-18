b90787801e909a829a6d352d91666acd8636df38
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-11-18 13:20:52 +0100

    builtin: minimal fix for _get_host_config_path

--- var/spack/repos/builtin/packages/lvarray/package.py
+++ var/spack/repos/builtin/packages/lvarray/package.py
@@ -121,12 +121,8 @@ class Lvarray(CMakePackage, CudaPackage):
             var = "-".join([var, "cuda"])
 
         hostname = socket.gethostname().rstrip("1234567890")
-        host_config_path = "%s-%s-%s%s.cmake" % (
-            hostname,
-            self._get_sys_type(spec),
-            spec.compiler,
-            var,
-        )
+        compiler_str = f"{self['c'].name}-{self['c'].version}"
+        host_config_path = f"{hostname}-{self._get_sys_type(spec)}-{compiler_str}{var}.cmake"
 
         dest_dir = self.stage.source_path
         host_config_path = os.path.abspath(pjoin(dest_dir, host_config_path))
