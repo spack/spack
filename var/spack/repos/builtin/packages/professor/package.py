72e5550ae9292bacb031611fddea43e6e6a597ab
Wouter Deconinck <wdconinc@gmail.com>
2025-01-01 17:11:45 -0600

    professor: add v2.5.0

--- var/spack/repos/builtin/packages/professor/package.py
+++ var/spack/repos/builtin/packages/professor/package.py
@@ -14,6 +14,10 @@ class Professor(Package):
 
     maintainers("mjk655")
 
+    version("2.5.0", sha256="19bfb4924d9f36a245ee1fa62cdd02a92b11f4ae3a13bdd068979155938c8079")
+    # Note: version 2.4.2 tar ball name and content is different from other versions
+    version("2.4.1", sha256="98efb19fa1590841dacd4f1e6c26e677cd419091bea69ef638c6111073732684")
+    version("2.4.0", sha256="e6b28aa41d5df41d6c948056e7eaa7b47d3e4576f15d6bed8cafba8794e0e22e")
     version("2.3.3", sha256="60c5ba00894c809e2c31018bccf22935a9e1f51c0184468efbdd5d27b211009f")
 
     variant(
@@ -37,6 +41,11 @@ class Professor(Package):
 
     def setup_build_environment(self, env: EnvironmentModifications) -> None:
         env.set("PROF_VERSION", self.spec.version)
+
+    @when("@2.5.0:")
+    def configure(self, spec, prefix):
+        configure = Executable("configure")
+        configure(f"--prefix={prefix}", f"--with-eigen={self.spec['eigen'].prefix}")
 
     def install(self, spec, prefix):
         make()
