c8f15489352a23c5425debe24b2aad3ab37e0508
Andres Quan <aquan@lanl.gov>
2025-01-06 16:18:04 -0700

    Fix more styling issues.
    Signed-off-by: Angelica Loshak <loshak@lanl.gov>

--- lib/spack/spack/cmd/mirror.py
+++ lib/spack/spack/cmd/mirror.py
@@ -50,8 +50,7 @@ def setup_parser(subparser: argparse.ArgumentParser) -> None:
         "--jobs",
         type=int,
         default=16,
-        help="Use a given number of threads to make the mirror"
-        " (used in combination with -a)"
+        help="Use a given number of threads to make the mirror (used in combination with -a)"
     )
     create_parser.add_argument("-f", "--file", help="file with specs of packages to put in mirror")
     create_parser.add_argument(
@@ -611,7 +610,7 @@ def mirror_create(args):
         mirror_specs,
         path=path,
         skip_unstable_versions=args.skip_unstable_versions,
-        threads=args.parallel
+        threads=args.parallel,
     )
 
 
@@ -670,12 +669,7 @@ def create_mirror_for_all_specs(mirror_specs, path, skip_unstable_versions, thre
     with ThreadPoolExecutor(max_workers=threads) as executor:
         # Submit tasks to the thread pool
         _ = [
-            executor.submit(
-                create_mirror_for_one_spec,
-                candidate,
-                mirror_cache,
-                mirror_stats
-            )
+            executor.submit(create_mirror_for_one_spec, candidate, mirror_cache, mirror_stats)
             for candidate in mirror_specs
         ]
     process_mirror_stats(*mirror_stats.stats())
@@ -683,9 +677,7 @@ def create_mirror_for_all_specs(mirror_specs, path, skip_unstable_versions, thre
 
 def create_mirror_for_individual_specs(mirror_specs, path, skip_unstable_versions, threads):
     present, mirrored, error = spack.mirrors.utils.create(
-        path,
-        mirror_specs,
-        skip_unstable_versions
+        path, mirror_specs, skip_unstable_versions
     )
     tty.msg("Summary for mirror in {}".format(path))
     process_mirror_stats(present, mirrored, error)
