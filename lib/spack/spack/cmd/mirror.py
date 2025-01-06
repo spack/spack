dfab12a6433a8cc3c79a51305a2a70f79023d18f
Andres Quan <aquan@lanl.gov>
2025-01-06 16:30:29 -0700

    Another style fix
    Signed-off-by: Angelica Loshak <loshak@lanl.gov>

--- lib/spack/spack/cmd/mirror.py
+++ lib/spack/spack/cmd/mirror.py
@@ -50,7 +50,7 @@ def setup_parser(subparser: argparse.ArgumentParser) -> None:
         "--jobs",
         type=int,
         default=16,
-        help="Use a given number of threads to make the mirror (used in combination with -a)"
+        help="Use a given number of threads to make the mirror (used in combination with -a)",
     )
     create_parser.add_argument("-f", "--file", help="file with specs of packages to put in mirror")
     create_parser.add_argument(
