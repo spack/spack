b34450dc442db3ef92c5d1595aa0ed9657191d40
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2025-01-09 14:45:56 -0800

    config.scopes_from_paths: add docstring spacing for readthedocs

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -789,7 +789,9 @@ def scopes_from_paths(
     required: List[str] = [],
 ) -> List[ConfigScope]:
     """Load included config scopes
+
     Scopes are added in reverse order so that highest-precedence scopes are last.
+
     Args:
         includes: list of paths to be included
         name_prefix: environment's name prefix
@@ -797,6 +799,7 @@ def scopes_from_paths(
         relative_root: path to root directory for relative paths or ``None``
             if relative paths are not supported
         required: list of paths that are required
+
     Raises:
         ValueError: included path has an unsupported URL scheme, is required
             but does not exist, or is not allowed to be relative
