ab6e0231e5dcd35845f4d053ebc1449822f14576
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2025-01-27 18:56:21 -0800

    Fix readthedocs FetchError complaint

--- lib/spack/spack/util/web.py
+++ lib/spack/spack/util/web.py
@@ -324,7 +324,8 @@ def check_curl_code(returncode: int) -> None:
     Arguments:
         returncode: curl return code
 
-    Raises FetchError if the curl returncode indicates failure
+    Raises:
+        spack.error.FetchError if the curl returncode indicates failure
     """
     if returncode == 0:
         return
@@ -376,7 +377,7 @@ def fetch_url_text(
     Returns: path to the fetched file
 
     Raises:
-        FetchError if the curl returncode indicates failure
+        spack.error.FetchError if the curl returncode indicates failure
     """
     if not url:
         raise spack.error.FetchError("A URL is required to fetch its text")
