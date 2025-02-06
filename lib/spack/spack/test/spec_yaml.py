d6d32cdf89dac01681b016cd6d5d25421dd49abd
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2025-02-06 14:21:10 -0800

    RepoSplit/test: flag use of builtin namespace for consideration

--- lib/spack/spack/test/spec_yaml.py
+++ lib/spack/spack/test/spec_yaml.py
@@ -367,7 +367,7 @@ ordered_spec = collections.OrderedDict(
         ),
         ("compiler", collections.OrderedDict([("name", "apple-clang"), ("version", "13.0.0")])),
         ("name", "zlib"),
-        ("namespace", "builtin"),
+        ("namespace", "builtin"),  # TODO/RepoSplit: Rename this if changes
         (
             "parameters",
             collections.OrderedDict(
