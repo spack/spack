a940d692878ca735942b8de55e218df26f1c707c
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2025-02-10 14:37:33 -0700

    limit some patches by chapel version

--- var/spack/repos/builtin/packages/chapel/package.py
+++ var/spack/repos/builtin/packages/chapel/package.py
@@ -72,8 +72,8 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
 
     patch("fix_spack_cc_wrapper_in_cray_prgenv.patch", when="@2.0.0:")
     patch("fix_chpl_shared_lib_path.patch", when="@2.1:2.2 +python-bindings")
-    patch("fix_chpl_shared_lib_path_2.3.patch", when="@2.2.1: +python-bindings")
-    patch("fix_chpl_line_length.patch")
+    patch("fix_chpl_shared_lib_path_2.3.patch", when="@2.2.1:2.3 +python-bindings")
+    patch("fix_chpl_line_length.patch", when="@:2.3.0")
     patch("fix_checkChplInstall.patch", when="@:2.3.0")  # PR 26317
     patch("fix_llvm_include_path_2.3.patch", when="@=2.3.0 llvm=bundled")  # PR 26402
 
