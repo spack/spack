07dc483dbad811e25a227ae385e3e480237f951f
Kyle Knoepfel <knoepfel@fnal.gov>
2025-02-13 12:25:09 -0600

    Add paths in the correct order and prune duplicates

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -1076,7 +1076,8 @@ class SetupContext:
                 env.prepend_path("PATH", bin_dir)
 
         for cp_dir in spack.build_systems.cmake.get_cmake_prefix_path(dep.package):
-            env.prepend_path("CMAKE_PREFIX_PATH", cp_dir)
+            env.append_path("CMAKE_PREFIX_PATH", cp_dir)
+        env.prune_duplicate_paths("CMAKE_PREFIX_PATH")
 
 
 def load_external_modules(context: SetupContext) -> None:
