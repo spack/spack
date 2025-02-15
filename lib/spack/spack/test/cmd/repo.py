ca5b3bc0c83db9609bf9d4dc85bc8b28516eb8f8
Finlay Brechin <finlaybrechin@gmail.com>
2025-02-15 23:20:19 +0000

    Adding ability for repo paths from a manifest file to be expanded when creating an environment.
    A unit test was added to check that an environment variable will be expanded.
    Also, a bug was fixed in the expansion of develop paths where if an environment variable
    was in the path that then produced an absolute path the path would not be extended.

--- lib/spack/spack/test/cmd/repo.py
+++ lib/spack/spack/test/cmd/repo.py
@@ -5,9 +5,13 @@ import os
 
 import pytest
 
+import spack.config
+import spack.environment as ev
 import spack.main
+from spack.main import SpackCommand
 
 repo = spack.main.SpackCommand("repo")
+env = SpackCommand("env")
 
 
 def test_help_option():
@@ -33,3 +37,33 @@ def test_create_add_list_remove(mutable_config, tmpdir):
     repo("remove", "--scope=site", str(tmpdir))
     output = repo("list", "--scope=site", output=str)
     assert "mockrepo" not in output
+
+
+def test_env_repo_path_vars_substitution(
+    tmpdir, install_mockery, mutable_mock_env_path, monkeypatch
+):
+    """Test Spack correctly substitues repo paths with environment variables when creating an
+    environment from a manifest file."""
+    # store the repo path in an environment variable that will be used in the environment
+    testrepo = "/somepath"
+    monkeypatch.setenv("CUSTOM_REPO_PATH", testrepo)
+
+    # setup environment from spack.yaml
+    envdir = tmpdir.mkdir("env")
+    with envdir.as_cwd():
+        with open("spack.yaml", "w", encoding="utf-8") as f:
+            f.write(
+                """\
+spack:
+  specs: []
+
+  repos:
+    - $CUSTOM_REPO_PATH
+"""
+            )
+        # creating env from manifest file
+        env("create", "test", "./spack.yaml")
+        # check that repo path was correctly substituted with the environment variable
+        with ev.read("test") as newenv:
+            repos_specs = spack.config.get("repos", default={}, scope=newenv.scope_name)
+            assert testrepo in repos_specs
