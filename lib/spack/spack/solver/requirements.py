40b02a5256863d1a1b76c2a3a456916cfa5bc502
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-02-20 11:00:51 +0100

    Disregard only default compiler requirements for runtimes

--- lib/spack/spack/solver/requirements.py
+++ lib/spack/spack/solver/requirements.py
@@ -222,11 +222,13 @@ class RequirementParser:
         if kind != RequirementKind.DEFAULT:
             return False
 
-        # Reject default requirements for runtimes and compilers
-        if pkg_name in self.runtime_pkgs:
-            return True
-
-        if pkg_name in self.compiler_pkgs:
+        # Reject requirements with dependencies for runtimes and compilers
+        # These are usually requests on compilers, in the form of %<compiler>
+        involves_dependencies = bool(constraint.dependencies())
+        if involves_dependencies and (
+            pkg_name in self.runtime_pkgs or pkg_name in self.compiler_pkgs
+        ):
+            tty.debug(f"[{__name__}] Rejecting '{constraint}' for compiler package {pkg_name}")
             return True
 
         # Requirements under all: are applied only if they are satisfiable considering only
