17506638f7da523e8ee39d5c84acc6d57cf88388
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-02-24 10:57:11 +0100

    Fix style issue
    Possible bug in mypy?

--- lib/spack/spack/solver/asp.py
+++ lib/spack/spack/solver/asp.py
@@ -3055,7 +3055,7 @@ class SpackSolverSetup:
 
         candidate_compilers.update(compilers_from_reuse)
         self.possible_compilers = list(candidate_compilers)
-        self.possible_compilers.sort()
+        self.possible_compilers.sort()  # type: ignore[call-overload]
 
         self.gen.h1("Reusable concrete specs")
         self.define_concrete_input_specs(specs, self.pkgs)
