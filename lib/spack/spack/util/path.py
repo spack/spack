aa49d2310c05e6b4403b66c97b64a497996d9a34
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2025-02-26 10:05:09 -0800

    canonicalize_path: tweak that hopefully resolves windows test_substitute_config_variables failure

--- lib/spack/spack/util/path.py
+++ lib/spack/spack/util/path.py
@@ -262,11 +262,14 @@ def canonicalize_path(path: str, default_wd: Optional[str] = None) -> str:
 
     url = urllib.parse.urlparse(path)
     url_path = urllib.request.url2pathname(url.path)
-    if url.scheme and url.scheme != "file":
-        # Have a remote URL so simply return it with substitutions
-        return os.path.normpath(path)
+    if url.scheme:
+        if url.scheme != "file":
+            # Have a remote URL so simply return it with substitutions
+            return os.path.normpath(path)
 
-    path = url_path
+        # Drop the URL scheme from the local path
+        path = url_path
+
     if not os.path.isabs(path):
         if filename:
             path = os.path.join(filename, path)
