8a06ce7540c9593dbb47d8cace46e07d3c8ef70d
Röhrig-Zöllner, Melven <melven.roehrig-zoellner@dlr.de>
2025-02-28 10:54:18 +0100

    py-numpy: fix +ilp64 for ^openblas

--- var/spack/repos/spack_repo/builtin/packages/py_numpy/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_numpy/package.py
@@ -345,12 +345,12 @@ class PyNumpy(PythonPackage):
             else:
                 lapack = "armpl-dynamic-lp64-seq"
 
-        if ("ilp64" in blas.split("-")) != ("ilp64" in lapack.split("-")):
+        if spec["blas"].satisfies("+ilp64") != spec["lapack"].satisfies("+ilp64"):
             raise InstallError(
                 "Either both blas and lapack must use ilp64 or none:"
                 " ({0} vs. {1})".format(blas, lapack)
             )
-        use_ilp64 = "ilp64" in blas.split("-")
+        use_ilp64 = spec["blas"].satisfies("+ilp64")
 
         return blas, lapack, use_ilp64
 
