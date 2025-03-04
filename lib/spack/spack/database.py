4739f0bd72d730ba8ec9712ad9ed1f5ab9bffc16
Marc Mengel <mengel@fnal.gov>
2025-03-04 16:25:38 -0600

    fixing [E501] line too long (130 > 99 characters)

--- lib/spack/spack/database.py
+++ lib/spack/spack/database.py
@@ -1803,8 +1803,7 @@ class Database:
 
         results = list(local_results) + list(x for x in upstream_results if x not in local_results)
         results.sort(
-            key=operator.attrgetter("architecture", "versions", "compiler"),
-            reverse=True
+            key=operator.attrgetter("architecture", "versions", "compiler"), reverse=True
         )  # type: ignore[call-overload]
         return results
 
