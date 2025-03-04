38209b8968b5f651387950b2252a4a499081d84e
Wouter Deconinck <wdconinc@gmail.com>
2025-03-04 12:19:17 -0600

    veccore: apply ScalarWrapper.h patch for all compilers

--- var/spack/repos/builtin/packages/veccore/package.py
+++ var/spack/repos/builtin/packages/veccore/package.py
@@ -39,11 +39,11 @@ class Veccore(CMakePackage):
 
     variant("vc", default=False, description="Enable Vc backend")
 
-    # This fixes a typo which is causes recent compilers to fail
+    # Fix a typo in ScalarWrapper.h
     patch(
         "https://github.com/root-project/veccore/commit/247b9c6bd48a26b298a73683bb6015b08829ca70.patch?full_index=1",
         sha256="7ea1b7ade302201fefe0b460ed32e86c39f241e4554e77e19c144985339e47f9",
-        when="@:0.8.1 %clang@19:"
+        when="@:0.8.1",
     )
 
     depends_on("cmake@3.16:", type="build", when="@0.8.1:")
