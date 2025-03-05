a3af4265613a6e5cf3faa2bc3e973b2a130bf66f
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-03-05 10:33:06 +0100

    arpack-ng: update to use oneapi packages
    Also, remove deprecated versions
    
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin/packages/arpack-ng/package.py
+++ var/spack/repos/builtin/packages/arpack-ng/package.py
@@ -41,6 +41,7 @@ class ArpackNg(CMakePackage, AutotoolsPackage):
     build_system("cmake", "autotools", default="cmake")
 
     version("develop", branch="master")
+    version("3.9.1", sha256="f6641deb07fa69165b7815de9008af3ea47eb39b2bb97521fbf74c97aba6e844")
     version("3.9.0", sha256="24f2a2b259992d3c797d80f626878aa8e2ed5009d549dad57854bbcfb95e1ed0")
     version("3.8.0", sha256="ada5aeb3878874383307239c9235b716a8a170c6d096a6625bfd529844df003d")
     version("3.7.0", sha256="972e3fc3cd0b9d6b5a737c9bf6fd07515c0d6549319d4ffb06970e64fa3cc2d6")
@@ -49,11 +50,6 @@ class ArpackNg(CMakePackage, AutotoolsPackage):
     version("3.6.0", sha256="3c88e74cc10bba81dc2c72c4f5fff38a800beebaa0b4c64d321c28c9203b37ea")
     version("3.5.0", sha256="50f7a3e3aec2e08e732a487919262238f8504c3ef927246ec3495617dde81239")
     version("3.4.0", sha256="69e9fa08bacb2475e636da05a6c222b17c67f1ebeab3793762062248dd9d842f")
-    version(
-        "3.3.0",
-        sha256="ad59811e7d79d50b8ba19fd908f92a3683d883597b2c7759fdcc38f6311fe5b3",
-        deprecated=True,
-    )
 
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
@@ -62,11 +58,6 @@ class ArpackNg(CMakePackage, AutotoolsPackage):
     variant("shared", default=True, description="Enables the build of shared libraries")
     variant("mpi", default=True, description="Activates MPI support")
     variant("icb", default=False, when="@3.6:", description="Activates iso_c_binding support")
-
-    # The function pdlamch10 does not set the return variable.
-    # This is fixed upstream
-    # see https://github.com/opencollab/arpack-ng/issues/34
-    patch("pdlamch10.patch", when="@3.3.0")
 
     patch("make_install.patch", when="@3.4.0")
     patch("parpack_cmake.patch", when="@3.4.0")
@@ -100,17 +91,13 @@ class ArpackNg(CMakePackage, AutotoolsPackage):
             if self.spec.satisfies("%cce"):
                 flags.append("-hnopattern")
 
-        return (flags, None, None)
+        return flags, None, None
 
     @property
     def libs(self):
-        # TODO: do we need spec['arpack-ng:parallel'].libs ?
-        # query_parameters = self.spec.last_query.extra_parameters
         libraries = ["libarpack"]
-
         if self.spec.satisfies("+mpi"):
             libraries = ["libparpack"] + libraries
-
         return find_libraries(libraries, root=self.prefix, shared=True, recursive=True)
 
 
@@ -136,10 +123,8 @@ class CMakeBuilder(spack.build_systems.cmake.CMakeBuilder):
         ]
 
         # If 64-bit BLAS is used:
-        if (
-            spec.satisfies("^openblas+ilp64")
-            or spec.satisfies("^intel-mkl+ilp64")
-            or spec.satisfies("^intel-parallel-studio+mkl+ilp64")
+        if spec.satisfies("^[virtuals=lapack] openblas+ilp64") or spec.satisfies(
+            "^[virtuals=lapack] intel-oneapi-mkl+ilp64"
         ):
             options.append("-DINTERFACE64=1")
 
