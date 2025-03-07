eca1f623ce6bbeea27adf52801c1d343bd8d98c7
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2025-03-07 12:43:30 -0800

    test_spec_list_expansions: use mock_packages fixture

--- lib/spack/spack/test/spec_list.py
+++ lib/spack/spack/test/spec_list.py
@@ -10,6 +10,8 @@ from spack.environment.list import SpecListParser
 from spack.installer import PackageInstaller
 from spack.spec import Spec
 
+pytestmark = [pytest.mark.usefixtures("mock_packages")]
+
 DEFAULT_EXPANSION = [
     "mpileaks",
     "zmpi@1.0",
@@ -83,8 +85,8 @@ class TestSpecList:
             ),
             # A constraint affects both the root and a dependency
             (
-                [{"matrix": [["gromacs"], ["%gcc"], ["+plumed ^plumed%gcc"]]}],
-                ["gromacs+plumed%gcc ^plumed%gcc"],
+                [{"matrix": [["version-test-root"], ["%gcc"], ["^version-test-pkg%gcc"]]}],
+                ["version-test-root%gcc ^version-test-pkg%gcc"],
             ),
         ],
     )
@@ -158,7 +160,7 @@ class TestSpecList:
         assert result.specs == DEFAULT_SPECS
 
     @pytest.mark.regression("16841")
-    def test_spec_list_matrix_exclude(self, mock_packages):
+    def test_spec_list_matrix_exclude(self):
         parser = SpecListParser()
         result = parser.parse_user_specs(
             name="specs",
@@ -171,7 +173,7 @@ class TestSpecList:
         )
         assert len(result.specs) == 1
 
-    def test_spec_list_exclude_with_abstract_hashes(self, mock_packages, install_mockery):
+    def test_spec_list_exclude_with_abstract_hashes(self, install_mockery):
         # Put mpich in the database so it can be referred to by hash.
         mpich_1 = spack.concretize.concretize_one("mpich+debug")
         mpich_2 = spack.concretize.concretize_one("mpich~debug")
