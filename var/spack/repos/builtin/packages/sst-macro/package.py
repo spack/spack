ed6055224c7f69dd56606a39ae0b02bb5d8c6b94
Eric Berquist <ejberqu@sandia.gov>
2025-03-10 09:20:02 -0600

    SST: update package maintainers

--- var/spack/repos/builtin/packages/sst-macro/package.py
+++ var/spack/repos/builtin/packages/sst-macro/package.py
@@ -18,7 +18,7 @@ class SstMacro(AutotoolsPackage):
     git = "https://github.com/sstsimulator/sst-macro.git"
     url = "https://github.com/sstsimulator/sst-macro/releases/download/v14.1.0_Final/sstmacro-14.1.0.tar.gz"
 
-    maintainers("berquist")
+    maintainers("berquist", "jmlapre")
 
     version("14.1.0", sha256="241f42f5c460b0e7462592a7f412bda9c9de19ad7a4b62c22f35be4093b57014")
     version("14.0.0", sha256="3962942268dd9fe6ebd4462e2d6d305ab757f3f510487e84687146a8d461be13")
