8b663186af4c4a431c6292ff09dbe512bf89ec05
Afzal Patel <Afzal.Patel@amd.com>
2025-03-10 13:41:35 -0700

    remove comment

--- var/spack/repos/spack_repo/builtin/packages/functionalplus/package.py
+++ var/spack/repos/spack_repo/builtin/packages/functionalplus/package.py
@@ -2,30 +2,12 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install functionalplus
-#
-# You can edit this file again by typing:
-#
-#     spack edit functionalplus
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
 class Functionalplus(CMakePackage):
     """Functional Programming Library for C++. Write concise and readable C++ code."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/Dobiasd/FunctionalPlus"
     url = "https://github.com/Dobiasd/FunctionalPlus/archive/refs/tags/v0.2.25.tar.gz"
 
