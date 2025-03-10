edd74ba9b28c040e1f106f1178b7987c6b5f482e
elfprince13 <elfprince13@users.noreply.github.com>
2025-03-10 18:12:09 +0000

    [@spackbot] updating style on behalf of elfprince13

--- var/spack/repos/builtin/packages/apple-veclib/package.py
+++ var/spack/repos/builtin/packages/apple-veclib/package.py
@@ -6,28 +6,34 @@ from spack.package import *
 
 class AppleVeclib(BundlePackage):
     """Shim package for the core Accelerate vecLib framework from Apple"""
+
     homepage = "https://developer.apple.com/documentation/accelerate"
     maintainers("elfprince13")
 
-    version("1068.60") # Sequoia 15.3.1
+    version("1068.60")  # Sequoia 15.3.1
 
     provides("blas")
     # according to https://developer.apple.com/documentation/accelerate/blas-library
     # there's even a way to get lapack 3.12.0 but start with the bare minimum to get
     # this working
-    provides("lapack@3.9.1",when="@1068.60:")
+    provides("lapack@3.9.1", when="@1068.60:")
 
-    requires(
-        "platform=darwin",
-        msg="Apple vecLib is only available on Darwin",
-    )
+    requires("platform=darwin", msg="Apple vecLib is only available on Darwin")
 
     @property
     def headers(self):
         return HeaderList(
-            join_path(self.prefix, "System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.Framework/Headers")
+            join_path(
+                self.prefix,
+                "System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.Framework/Headers",
+            )
         )
 
     @property
     def libs(self):
-        return LibraryList(join_path(self.prefix, "System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.Framework"))
+        return LibraryList(
+            join_path(
+                self.prefix,
+                "System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.Framework",
+            )
+        )
