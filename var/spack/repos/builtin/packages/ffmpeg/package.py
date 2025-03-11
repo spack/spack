e95b0a71bb4c006160d3feb670b62ef7c68b83bb
Michael Kuhn <michael.kuhn@ovgu.de>
2025-03-11 20:16:15 +0100

    ffmpeg: Fix aom dependency

--- var/spack/repos/builtin/packages/ffmpeg/package.py
+++ var/spack/repos/builtin/packages/ffmpeg/package.py
@@ -103,6 +103,7 @@ class Ffmpeg(AutotoolsPackage):
 
     depends_on("pkgconfig", type="build")
 
+    depends_on("aom@2.0.0:", when="@7.1:+libaom")
     depends_on("aom", when="+libaom")
     depends_on("bzip2", when="+bzlib")
     depends_on("fontconfig", when="+drawtext")
