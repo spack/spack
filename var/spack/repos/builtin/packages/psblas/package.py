abd55e317491df20f3342aa3f680b5974f4557cc
Fabio Durastante <fabio.durastante@unipi.it>
2025-03-12 09:46:13 +0100

    Used black to reformat the file

--- var/spack/repos/builtin/packages/psblas/package.py
+++ var/spack/repos/builtin/packages/psblas/package.py
@@ -23,38 +23,56 @@ class Psblas(AutotoolsPackage):
     license("BSD-3-Clause", checked_by="cirdans-home")
 
     version("development", branch="development")
-    version("3.9.0-rc1",
-            sha256="7a7091ce52582b6fc442e8793e36461be36c0947272ea803ad72736ec2d56da8",
-            preferred=True,
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.9.0-rc1.tar.gz")
-    version("3.8.1-2",
-            sha256="285ddb7c9a793ea7ecb428d68cf23f4cc04f1c567631aa84bc2bedb65a3d1b0c",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1-2.tar.gz")
-    version("3.8.1-rc1-3",
-            sha256="00b994158d12644853180d1115ed530b1aed3903c38a9d7bc3d3efc203b2890b",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1-rc1-3.tar.gz",
-            deprecated=True)
-    version("3.8.1-rc1-1",
-            sha256="4f7371000592d73603010adef654d4e9d27a9692d5204db20c0f7b39c462fd83",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1-rc1-1.tar.gz",
-            deprecated=True)
-    version("3.8.1",
-            sha256="02e1f00e644426eb15eb08c735cf9c8ae692392f35c2cfe4f7474e1ab91575dc",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1.tar.gz")
-    version("3.8.0-2",
-            sha256="86a76bb0987edddd4c10c810d7f18e13742aadc66ac14ad3679669809c1184fa",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.0-2.tar.gz")
-    version("3.8.0-1",
-            sha256="dca593010f258af669eb40d92c4934eaaa016ef17280d4ab1208afc3d5aa13f0",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.0-1.tar.gz",
-            deprecated=True)
-    version("3.8.0",
-            sha256="0eaef8f5fd41313ee1925a54bda33c5e1756a6d14b679c54e8423556881875e8",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.0.tar.gz",
-            deprecated=True)
-    version("3.7.0.2",
-            sha256="2c4f5ae23051b689ee9186db6baf8ca77eed7c8cb430fbc665329394036cc7f7",
-            url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.7.0-2.tar.gz")
+    version(
+        "3.9.0-rc1",
+        sha256="7a7091ce52582b6fc442e8793e36461be36c0947272ea803ad72736ec2d56da8",
+        preferred=True,
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.9.0-rc1.tar.gz",
+    )
+    version(
+        "3.8.1-2",
+        sha256="285ddb7c9a793ea7ecb428d68cf23f4cc04f1c567631aa84bc2bedb65a3d1b0c",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1-2.tar.gz",
+    )
+    version(
+        "3.8.1-rc1-3",
+        sha256="00b994158d12644853180d1115ed530b1aed3903c38a9d7bc3d3efc203b2890b",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1-rc1-3.tar.gz",
+        deprecated=True,
+    )
+    version(
+        "3.8.1-rc1-1",
+        sha256="4f7371000592d73603010adef654d4e9d27a9692d5204db20c0f7b39c462fd83",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1-rc1-1.tar.gz",
+        deprecated=True,
+    )
+    version(
+        "3.8.1",
+        sha256="02e1f00e644426eb15eb08c735cf9c8ae692392f35c2cfe4f7474e1ab91575dc",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.1.tar.gz",
+    )
+    version(
+        "3.8.0-2",
+        sha256="86a76bb0987edddd4c10c810d7f18e13742aadc66ac14ad3679669809c1184fa",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.0-2.tar.gz",
+    )
+    version(
+        "3.8.0-1",
+        sha256="dca593010f258af669eb40d92c4934eaaa016ef17280d4ab1208afc3d5aa13f0",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.0-1.tar.gz",
+        deprecated=True,
+    )
+    version(
+        "3.8.0",
+        sha256="0eaef8f5fd41313ee1925a54bda33c5e1756a6d14b679c54e8423556881875e8",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.8.0.tar.gz",
+        deprecated=True,
+    )
+    version(
+        "3.7.0.2",
+        sha256="2c4f5ae23051b689ee9186db6baf8ca77eed7c8cb430fbc665329394036cc7f7",
+        url="https://github.com/sfilippone/psblas3/archive/refs/tags/v3.7.0-2.tar.gz",
+    )
 
     # Explicit phases: autoreconf, configure, build, install, and post_install
     # which compiles the examples in the prefix/samples folder
@@ -67,10 +85,10 @@ class Psblas(AutotoolsPackage):
     depends_on("cxx", type="build")  # generated
     depends_on("fortran", type="build")  # generated
     # LPK/IPK: Integer precision variants
-    variant("LPK", default=8, values=int,
-            description="Length in bytes for long integers (8 or 4)")
-    variant("IPK", default=4, values=int,
-            description="Length in bytes for short integers (8 or 4)")
+    variant("LPK", default=8, values=int, description="Length in bytes for long integers (8 or 4)")
+    variant(
+        "IPK", default=4, values=int, description="Length in bytes for short integers (8 or 4)"
+    )
     # MPI
     variant("mpi", default=True, description="Activates MPI support")
     depends_on("mpi", when="+mpi")
@@ -78,11 +96,17 @@ class Psblas(AutotoolsPackage):
     depends_on("blas")
     depends_on("lapack")
     # CUDA
-    variant("cuda", default=False, description="Activate CUDA support",
-            when="@development,3.9.0-rc1")
+    variant(
+        "cuda", default=False, description="Activate CUDA support", when="@development,3.9.0-rc1"
+    )
     depends_on("cuda", when="+cuda")
-    variant("cudacc", default="70,75,80,86,89,90", multi=True,
-            description="Specify CUDA Compute Capabilities", when="+cuda")
+    variant(
+        "cudacc",
+        default="70,75,80,86,89,90",
+        multi=True,
+        description="Specify CUDA Compute Capabilities",
+        when="+cuda",
+    )
     # Metis
     variant("metis", default=False, description="Activate METIS suppot")
     depends_on("metis@5:+int64", when="+metis LPK=8")
@@ -91,11 +115,19 @@ class Psblas(AutotoolsPackage):
     variant("amd", default=False, description="Activate AMD support via SuiteSparse")
     depends_on("suite-sparse", when="+amd")
     # OpenMP
-    variant("openmp", default=False, description="Activates OpenMP support",
-            when="@development,3.9.0-rc1")
+    variant(
+        "openmp",
+        default=False,
+        description="Activates OpenMP support",
+        when="@development,3.9.0-rc1",
+    )
     # OpenACC support (requires GCC >= 14.2.0)
-    variant("openacc", default=False, description="Activate OpenACC support",
-            when="@development,3.9.0-rc1")
+    variant(
+        "openacc",
+        default=False,
+        description="Activate OpenACC support",
+        when="@development,3.9.0-rc1",
+    )
     depends_on("gcc@14.2.0:+nvptx", when="+openacc", type="build")
     # Additional configure options
     variant("ccopt", default="none", description="Additional CCOPT flags")
@@ -105,16 +137,18 @@ class Psblas(AutotoolsPackage):
     variant("libs", default="none", description="Additional link flags")
     variant("clibs", default="none", description="Additional CLIBS flags")
     variant("flibs", default="none", description="Additional FLIBS flags")
-    variant("extra_nvcc", default="none", description="Additional EXTRA_NVCC flags",
-            when="+cuda")
-    variant("extraopenacc", default="none", description="Additional EXTRAOPENACC flags",
-            when="+openacc")
-    variant("ccopenacc", default="none", description="Additional CCOPENACC flags",
-            when="+openacc")
-    variant("cxxopenacc", default="none", description="Additional CXXOPENACC flags",
-            when="+openacc")
-    variant("fcopenacc", default="none", description="Additional FCOPENACC flags",
-            when="+openacc")
+    variant("extra_nvcc", default="none", description="Additional EXTRA_NVCC flags", when="+cuda")
+    variant(
+        "extraopenacc",
+        default="none",
+        description="Additional EXTRAOPENACC flags",
+        when="+openacc",
+    )
+    variant("ccopenacc", default="none", description="Additional CCOPENACC flags", when="+openacc")
+    variant(
+        "cxxopenacc", default="none", description="Additional CXXOPENACC flags", when="+openacc"
+    )
+    variant("fcopenacc", default="none", description="Additional FCOPENACC flags", when="+openacc")
 
     def configure_args(self):
         args = [f"--prefix={self.prefix}"]
