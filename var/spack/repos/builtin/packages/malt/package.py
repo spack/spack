672f1f02ac223f4d1ef6ffe4ab3ce4b974d01027
Sebastien Valat <sebastien.valat@inria.fr>
2025-03-13 22:05:21 +0100

    malt: Add version 1.2.5

--- var/spack/repos/builtin/packages/malt/package.py
+++ var/spack/repos/builtin/packages/malt/package.py
@@ -20,6 +20,7 @@ class Malt(CMakePackage):
     license("CECILL-C")
 
     # Versions
+    version("1.2.5", sha256="9660e42f92230e6acf5c19df5195f59a4c2d7d919eeab4410fe943507eee2c67")
     version("1.2.4", sha256="47068fe981b4cbbfe30eeff37767d9057992f8515106d7809ce090d3390a712f")
     version("1.2.3", sha256="edba5d9e6a11308f82b9c8b61871e47a8ae18493bf8bff7b6ff4f4a4369428de")
     version("1.2.2", sha256="543cace664203fd9eb6b7d4945c573a3e507a43da105b5dc7ac03c78e9bb1a10")
