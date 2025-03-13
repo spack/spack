d3484ec8868e79d2ebcbb57192f6b8b27626fdac
psakievich <psakievich@users.noreply.github.com>
2025-03-13 21:33:52 +0000

    [@spackbot] updating style on behalf of psakievich

--- lib/spack/spack/cmd/develop.py
+++ lib/spack/spack/cmd/develop.py
@@ -42,7 +42,7 @@ def setup_parser(subparser):
         default=True,
         help=(
             "(default) clone the package unless the path already exists, "
-              "use --force to overwrite"
+            "use --force to overwrite"
         ),
     )
 
