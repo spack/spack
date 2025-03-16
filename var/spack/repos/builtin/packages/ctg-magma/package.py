34e4076aefcd9011fa7c2a7d1ec2e7b96e033dd2
Teague Sterling <teaguesterling@gmail.com>
2025-03-16 16:50:04 -0700

    ctg-magma: new package
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/ctg-magma/package.py
+++ var/spack/repos/builtin/packages/ctg-magma/package.py
@@ -13,12 +13,24 @@ class CtgMagma(MakefilePackage):
 
     license("GPL-3.0-only")
 
+    variant("bundled-deps", default=False, description="Use bundled versions of eigen and boost")
+
+    with when("~bundled-deps"):
+        depends_on("eigen")
+        depends_on("boost@1.74:1.81+math+random")  # Boost::math requires C++14 after v1.82
+
     version(
-        "1.10", 
+        "1.10",
         url="https://vu.data.surfsara.nl/index.php/s/1OOi7bxLWef0GwY/download",
         sha256="c744c662204e6888f007b1db2c54d608545f2e71a73e5b42bf43102597c178af",
         extension="zip",
     )
+
+    def patch(self):
+        if self.spec.satisfies("~bundled-deps"):
+            filter_file(
+                "CXX_FLAGS += -I src/eigen -I src/boost/boost_1_74_0", "", "makefile", string=True
+            )
 
     def install(self, spec, prefix):
         makedirs(prefix.bin)
