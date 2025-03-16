d7a5feb72e37835b87ad91029b34c0105289e216
Teague Sterling <teaguesterling@gmail.com>
2025-03-16 15:05:19 -0700

    new pacakge: regenie
    Signed-off-by: Teague Sterling <teaguesterling@gmail.com>

--- var/spack/repos/builtin/packages/regenie/package.py
+++ var/spack/repos/builtin/packages/regenie/package.py
@@ -18,47 +18,61 @@ class Regenie(CMakePackage):
 
     version("4.1", sha256="a7d8ad321ca66bd10fa5ed651c63069886f5cb5ef8e900ca9a0c5b7e3dfc7da5")
 
-    variant("boostio", default=True, description="Build with Boost IO support")
+    variant("with-boostio", default=True, description="Build with Boost IO support")
+    variant("with-htslib", default=True, description="Build with HTSLib support")
     variant("static", default=False, description="Build a statically linked version")
     variant("bundled-eigen", default=False, description="Build with vendored eigen library")
     variant("bundled-cxxopts", default=False, description="Build with vendored cxxopts library")
     variant("bundled-lbfgspp", default=False, description="Build with vendored LBFGSpp library")
     variant(
-        "bgen-bundled-deps", default=False, description="Build with sqlite, boost, and zstd from bgen"
+        "bgen-bundled-deps",
+        default=False,
+        description="Build with sqlite, boost, and zstd from bgen",
     )
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("fortran", type="build")
 
+    depends_on("cmake@3.13:", type="build")
     depends_on("zlib-api")
     depends_on("openssl")
     depends_on("bzip2")
     depends_on("lzma")
     depends_on("libdeflate")
+    depends_on("python")
     depends_on("netlib-lapack+lapacke+external-blas")
     depends_on("openblas threads=openmp")
-    depends_on("htslib")
-    depends_on("htslib+pic", when="+static")
-    depends_on("python@3")
     depends_on("bgen+headers+libs", when="~bgen-bundled-deps")
     depends_on("bgen+full-source", when="+bgen-bundled-deps")
-    depends_on("cmake@3.13:")
-    depends_on("boost+iostreams", when="+boostio")
+    depends_on("boost+iostreams", when="+with-boostio")
     depends_on("eigen@3.4:", when="~bundled-eigen")
     depends_on("cxxopts@3", when="~bundled-cxxopts")
     depends_on("cxxopts@3.0", when="~bundled-cxxopts+bgen-bundled-deps")
     depends_on("lbfgspp", when="~bundled-lbfgspp")
 
+    with when("+with-htslib"):
+        depends_on("htslib")
+        depends_on("htslib+pic", when="+static")
+
     with when("~bgen-bundled-deps"):
         depends_on("bgen~bundled-deps")
         depends_on("zstd")
         depends_on("sqlite@3")
-        depends_on("boost@1.55:+chrono+date_time+exception+filesystem+math+system+thread+timer")
+
+        # regenie forces use of C++11 standard, boost::math requires C++14 after v1.82
+        depends_on(
+            "boost@1.55:1.81+chrono+date_time+exception+filesystem+math+system+thread+timer"
+        )
         depends_on("zstd libs=static", when="+static")
         with when("~static"):
             depends_on("zstd libs=shared")
             depends_on("boost+shared")
+
+    # cxxopts changed the name of the base exception class after 3.0
+    # This patch also changed the exception variable from e to msg to avoid any
+    # misleading error messages pointing to "e" from boost::math
+    patch("fix-cxxopts-with-new-boost.patch", when="^cxxopts@3.1:")
 
     def patch(self):
         satisfies = self.spec.satisfies
@@ -91,21 +105,6 @@ class Regenie(CMakePackage):
 
         # Record any libraries that will be statically linked by default
         statics = ["hts"]
-        statics_lib_overrides = {
-            "boost": [
-                "boost_chrono",
-                "boost_exception",
-                "boost_date_time",
-                "boost_filesystem",
-                "boost_math",
-                "boost_system",
-                "boost_thread",
-                "boost_timer",
-            ]
-        }
-
-        if satisfies("+boostio"):
-            statics_lib_overrides["boost"].append("boost_iostreams")
 
         # Avoid using (some) vendored dependencies included with regenie
         for dep, old_path in [
@@ -115,19 +114,15 @@ class Regenie(CMakePackage):
         ]:
             if self.spec.satisfies(f"~bundled-{dep}"):
                 lib = self.spec[dep]
-                filter_file(
-                    old_path, 
-                    dep_dirs(lib.headers), 
-                    "CMakeLists.txt",
-                    string=True,
-                )
+                filter_file(old_path, dep_dirs(lib.headers), "CMakeLists.txt", string=True)
 
         # Avoid using vendored dependencies distribued with bgen
         if satisfies("~bgen-bundled-deps"):
+
+            # zstd and sqlite can be replaced with subsitutions
             for dep, lib_name, old_lib, old_inc in [
                 ("zstd", "zstd", "zstd-1.1.0", "zstd-1.1.0/lib"),
                 ("sqlite", "sqlite3", "sqlite3", "sqlite3"),
-                ("boost", "boost", "boost_1_55_0", "boost_1_55_0/"),
             ]:
                 lib = self.spec[dep]
                 lib_pat = f'"${{BGEN_PATH}}/build/3rd_party/{old_lib}"'
@@ -136,36 +131,65 @@ class Regenie(CMakePackage):
                 filter_file(inc_pat, dep_dirs(lib.headers), "CMakeLists.txt", string=True)
                 statics.append(lib_name)
 
+            # Boost needs more careful handling using find_package since bgen created a single
+            # static archive that doesn't find/replace as easily
+            statics.append("boost")
+            boost = self.spec["boost"]
+            boost_vers = boost.version.up_to(2)
+            boost_comps = [
+                "chrono",
+                "exception",
+                "date_time",
+                "filesystem",
+                "system",
+                "thread",
+                "timer",
+            ]
+            if satisfies("+with-boostio"):
+                boost_comps.append("iostreams")
+            boost_lib = (
+                "find_library(Boost_LIBRARY libboost.a"
+                ' HINTS "${BGEN_PATH}/build/3rd_party/boost_1_55_0" REQUIRED)'
+            )
+            boost_pkg = (
+                f"find_package(Boost {boost_vers}" f" COMPONENTS {' '.join(boost_comps)} REQUIRED)"
+            )
+            for find, replace in [
+                (boost_lib, boost_pkg),
+                ("${Boost_LIBRARY}", "${Boost_LIBRARIES}"),
+                ("${BGEN_PATH}/3rd_party/boost_1_55_0/", "${Boost_INCLUDE_DIR}"),
+            ]:
+                filter_file(find, replace, "CMakeLists.txt", string=True)
+
         # Convert static libraries to shared unless we are actually building static
         if satisfies("~static"):
-            for lib_name in statics:
-                if lib_name in statics_lib_overrides:
-                    lib_override = " ".join(statics_lib_overrides[lib_name])
-                else:
-                    lib_override = lib_name
-                filter_file(f"lib{lib_name}.a", lib_override, "CMakeLists.txt", string=True)
-        elif satisfies("~bgen-bundled-deps"):
-            for lib_name, overrides in statics_lib_overrides.items():
-                override = [f"lib{override}.a" for override in statics_lib_overrides[lib_name]]
-                filter_file(f"lib{lib_name}.a", " ".join(override), "CMakeLists.txt", string=True)
+            for lib in statics:
+                filter_file(f"lib{lib}.a", lib, "CMakeLists.txt", string=True)
 
+        # The bgen package installs libraries and headers in more traditional locations
+        # if we don't use the full source option. Patch the paths for compiling
         bgen = self.spec["bgen"]
         if satisfies("^bgen~full-source"):
             for old_path, new_path in [
                 ("${BGEN_PATH}/build", bgen.prefix.lib.bgen),
-                ("${BGEN_PATH} ${BGEN_PATH}/genfile/include/", f"{bgen.prefix.include} {bgen.prefix.include.genfile}"),
+                (
+                    "${BGEN_PATH} ${BGEN_PATH}/genfile/include/",
+                    f"{bgen.prefix.include} {bgen.prefix.include.bgen}",
+                ),
                 ("${BGEN_PATH}/db/include/", bgen.prefix.include.db),
             ]:
                 filter_file(old_path, new_path, "CMakeLists.txt", string=True)
 
     def setup_build_environment(self, env):
         bgen = self.spec["bgen"]
-        htslib = self.spec["htslib"]
         openblas = self.spec["openblas"]
-        env.set("HTSLIB_PATH", htslib.prefix.lib)
         env.set("OPENBLAS_ROOT", openblas.prefix)
-        env.set("STATIC", "1" if self.spec.satisfies("+static") else "0")
-        env.set("HAS_BOOST_IOSTREAM", "1" if self.spec.satisfies("+boostio") else "0")
+        if self.spec.satisfies("+static"):
+            env.set("STATIC", "1")
+        if self.spec.satisfies("+with-boostio"):
+            env.set("HAS_BOOST_IOSTREAM", "1")
+        if self.spec.satisfies("with-htslib"):
+            env.set("HTSLIB_PATH", self.spec["htslib"].prefix.lib)
         if self.spec.satisfies("^bgen~full-source"):
             env.set("BGEN_PATH", bgen.prefix)
         else:
