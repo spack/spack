884f8a00051c6c7c6f55284cf9aadad59275f2aa
Robert Maaskant <robert@maaskant.io>
2025-03-20 13:55:21 +0100

    py-deephyper: add v0.9.3

--- var/spack/repos/builtin/packages/py-deephyper/package.py
+++ var/spack/repos/builtin/packages/py-deephyper/package.py
@@ -18,31 +18,44 @@ class PyDeephyper(PythonPackage):
 
     license("BSD-3-Clause")
 
+    version("0.9.3", sha256="e28f9f6ca596edee7fa73f7377ca17ddab99ac4b3c7f15db27cb6e786f770f23")
+
     with default_args(deprecated=True):
         version("master", branch="master")
         version("0.6.0", sha256="cda2dd7c74bdca4203d9cd637c4f441595f77bae6d77ef8e4a056b005357de34")
         version("0.4.2", sha256="ee1811a22b08eff3c9098f63fbbb37f7c8703e2f878f2bdf2ec35a978512867f")
 
     with default_args(type="build"):
-        depends_on("py-setuptools@42:", when="@0.6.0")
-        depends_on("py-setuptools@40:49.1")
+        depends_on("py-hatchling", when="@0.9:")
 
-        depends_on("py-wheel@0.36.2")
+        depends_on("py-setuptools@42:", when="@0.6.0")
+        depends_on("py-setuptools@40:49.1", when="@:0.6")
+
+        depends_on("py-wheel@0.36.2", when="@:0.6")
 
         depends_on("py-cython@0.29.24:", when="@0.6.0")
         depends_on("py-cython@0.29.24:2", when="@0.4.2")
 
     with default_args(type=("build", "run")):
         depends_on("python@3.7:3.11", when="@0.6.0")
-        depends_on("python@3.7:3.9")
+        depends_on("python@3.7:3.9", when="@0.4.2")
 
+        depends_on("py-configspace@1.1.1:", when="@0.9.3:")
         depends_on("py-configspace@0.4.20:")
+
+        depends_on("py-cloudpickle", when="@0.9.3:")
 
         depends_on("py-dm-tree")
 
+        depends_on("py-jinja2@3.1.4:", when="@0.9.3:")
         depends_on("py-jinja2@:3.1", when="@0.6.0")
         depends_on("py-jinja2@:3.0", when="@0.4.2")
 
+        depends_on("py-loky@3.4:")
+
+        depends_on("py-matplotlib")
+
+        depends_on("py-numpy@1.26:", when="@0.9.3:")
         depends_on("py-numpy@1.20:", when="@0.6.0")
         depends_on("py-numpy")
 
@@ -51,12 +64,19 @@ class PyDeephyper(PythonPackage):
         depends_on("py-packaging@20.5:", when="@0.6.0 target=aarch64: platform=darwin")
         depends_on("py-packaging")
 
+        depends_on("py-parse", when="@0.9.3:")
+
         depends_on("py-scikit-learn@0.23.1:")
 
+        depends_on("py-scipy@1.10:", when="@0.9.3")
         depends_on("py-scipy@1.7:", when="@0.6.0")
         depends_on("py-scipy@0.19.1:")
 
         depends_on("py-tqdm@4.64.0:")
+
+        depends_on("py-psutil", when="@0.9.3")
+
+        depends_on("py-pymoo@0.6:", when="@0.9.3")
 
         depends_on("py-pyyaml")
 
