2dc0cf5f4db1bb49e038d93ff09bd168a6e10340
Ahmad Rezaii <ahmad.rezaii@hpe.com>
2025-03-20 16:38:04 -0600

    update 2.4 sha256

--- var/spack/repos/builtin/packages/chapel/package.py
+++ var/spack/repos/builtin/packages/chapel/package.py
@@ -58,6 +58,7 @@ class Chapel(AutotoolsPackage, CudaPackage, ROCmPackage):
 
     version("main", branch="main")
 
+    version("2.4.0", sha256="a51a472488290df12d1657db2e7118ab519743094f33650f910d92b54c56f315")
     version("2.3.0", sha256="0185970388aef1f1fae2a031edf060d5eac4eb6e6b1089e7e3b15a130edd8a31")
     version("2.2.0", sha256="bb16952a87127028031fd2b56781bea01ab4de7c3466f7b6a378c4d8895754b6")
     version("2.1.0", sha256="72593c037505dd76e8b5989358b7580a3fdb213051a406adb26a487d26c68c60")
