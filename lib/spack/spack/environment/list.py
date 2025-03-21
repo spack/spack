49b54824373c150d34d834abb18e340cd9a9d885
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-03-21 08:25:04 +0100

    Simplify "when" evaluation for "definitions"
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/environment/list.py
+++ lib/spack/spack/environment/list.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 import itertools
-from typing import Dict, List
+from typing import Any, Dict, List
 
 import spack.spec
 import spack.variant
@@ -252,33 +252,29 @@ def _sigilify(item, sigil):
 
 class SpecListParser:
     def __init__(self):
-        self.spec_lists: Dict[str, SpecList] = {}
+        self.definitions: Dict[str, SpecList] = {}
 
-    def parse_definitions(self, data) -> Dict[str, SpecList]:
-        self.spec_lists = {}
+    def parse_definitions(self, data: Dict[str, Any]) -> Dict[str, SpecList]:
+        self.definitions = {}
         for item in data:
             self._process_definition(item)
-        return self.spec_lists
+
+        return self.definitions
 
     def _process_definition(self, entry):
         """Process a single spec definition item."""
         when_string = entry.get("when")
-        if when_string is not None:
-            when = spack.spec.eval_conditional(when_string)
-            assert len([x for x in entry if x != "when"]) == 1
-        else:
-            when = True
-            assert len(entry) == 1
+        if when_string is not None and not spack.spec.eval_conditional(when_string):
+            return
 
-        if when:
-            for name, spec_list in entry.items():
-                if name == "when":
-                    continue
-                user_specs = SpecList(name, spec_list, self.spec_lists.copy())
-                if name in self.spec_lists:
-                    self.spec_lists[name].extend(user_specs)
-                else:
-                    self.spec_lists[name] = user_specs
+        for name, yaml_list in entry.items():
+            if name == "when":
+                continue
+            user_specs = SpecList(name, yaml_list, self.definitions.copy())
+            if name in self.definitions:
+                self.definitions[name].extend(user_specs)
+            else:
+                self.definitions[name] = user_specs
 
 
 class SpecListError(SpackError):
