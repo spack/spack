e6ce3ef5dec0da2fb0d0b04496ecb322d991e1c8
吴坎 <github@wu-kan.cn>
2025-03-25 18:01:21 +0800

    Update package.py

--- var/spack/repos/builtin/packages/py-triton/package.py
+++ var/spack/repos/builtin/packages/py-triton/package.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import sys
+
 from spack.package import *
 
 
@@ -17,6 +19,38 @@ class PyTriton(PythonPackage):
     version("main", branch="main")
     version("2.1.0", sha256="4338ca0e80a059aec2671f02bfc9320119b051f378449cf5f56a1273597a3d99")
 
+    if sys.platform.startswith("linux"):
+        version(
+            "3.1.0-py3.12",
+            sha256="c8182f42fd8080a7d39d666814fa36c5e30cc00ea7eeeb1a2983dbb4c99a0fdc",
+            url="https://files.pythonhosted.org/packages/78/eb/65f5ba83c2a123f6498a3097746607e5b2f16add29e36765305e4ac7fdd8/triton-3.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        )
+        version(
+            "3.1.0-py3.11",
+            sha256="0f34f6e7885d1bf0eaaf7ba875a5f0ce6f3c13ba98f9503651c1e6dc6757ed5c",
+            url="https://files.pythonhosted.org/packages/86/17/d9a5cf4fcf46291856d1e90762e36cbabd2a56c7265da0d1d9508c8e3943/triton-3.1.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        )
+        version(
+            "3.1.0-py3.10",
+            sha256="6b0dd10a925263abbe9fa37dcde67a5e9b2383fc269fdf59f5657cac38c5d1d8",
+            url="https://files.pythonhosted.org/packages/98/29/69aa56dc0b2eb2602b553881e34243475ea2afd9699be042316842788ff5/triton-3.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        )
+        version(
+            "3.1.0-py3.9",
+            sha256="aafa9a20cd0d9fee523cd4504aa7131807a864cd77dcf6efe7e981f18b8c6c11",
+            url="https://files.pythonhosted.org/packages/c4/69/57e0fed438d547524e08bfedc587078314176ad1c15c8be904d3f03149ec/triton-3.1.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        )
+        version(
+            "3.1.0-py3.8",
+            sha256="6dadaca7fc24de34e180271b5cf864c16755702e9f63a16f62df714a8099126a",
+            url="https://files.pythonhosted.org/packages/15/3c/e972ac0dd0f35ba5fb7058152dd52127a225f579eba2d7527eb1ffb3891a/triton-3.1.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        )
+
+    for py_ver in ["3.8", "3.9", "3.10", "3.11", "3.12"]:
+        depends_on(
+            "python@" + py_ver + ".0:" + py_ver, when="@3.1.0-py" + py_ver, type=("build", "run")
+        )
+
     depends_on("c", type="build")  # generated
     depends_on("cxx", type="build")  # generated
 
@@ -31,4 +65,12 @@ class PyTriton(PythonPackage):
         if self.spec.satisfies("%clang"):
             env.set("TRITON_BUILD_WITH_CLANG_LLD", "True")
 
-    build_directory = "python"
+    @property
+    def build_directory(self):
+        if str(self.version) in [
+            triton_ver + "-py" + py_ver
+            for py_ver in ["3.8", "3.9", "3.10", "3.11", "3.12"]
+            for triton_ver in ["3.1.0"]
+        ]:
+            return "."
+        return "python"
