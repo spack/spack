cb8ab6ccc14a319f91c1aa10732e763a6231b693
Afzal Patel <Afzal.Patel@amd.com>
2025-03-25 09:32:06 -0700

    add conflict for cuda and remove py-jaxlib from aarch64 pipeline

--- var/spack/repos/builtin/packages/py-jaxlib/package.py
+++ var/spack/repos/builtin/packages/py-jaxlib/package.py
@@ -182,6 +182,9 @@ class PyJaxlib(PythonPackage, CudaPackage, ROCmPackage):
     # Fails to build with freshly released CUDA (#48708).
     conflicts("^cuda@12.8:", when="@:0.4.31")
 
+    # external CUDA is not supported https://github.com/jax-ml/jax/issues/23689
+    conflicts("+cuda", when="@0.4.32:")
+
     resource(
         name="xla",
         url="https://github.com/ROCm/xla/archive/07543ab117699a57c1267b453a62f89b1d5953fd.tar.gz",
