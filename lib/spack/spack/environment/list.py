8c1c6d0d32d0fdb07fee0e4c862b13c7c42b1ccb
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-03-25 17:36:36 +0100

    Remove references from SpecList
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/environment/list.py
+++ lib/spack/spack/environment/list.py
@@ -12,32 +12,14 @@ from spack.spec import Spec
 
 
 class SpecList:
-    @staticmethod
-    def from_parser(*, name, yaml_list, expanded_list) -> "SpecList":
-        result = SpecList(name=name, yaml_list=yaml_list)
-        result._expanded_list = expanded_list
-        return result
-
-    def __init__(self, name="specs", yaml_list=None, reference=None):
-        yaml_list = yaml_list or []
-        reference = reference or {}
-
+    def __init__(self, *, name: str = "specs", yaml_list=None, expanded_list=None):
         self.name = name
-        self._reference = reference  # TODO: Do we need defensive copy here?
-
-        # Validate yaml_list before assigning
-        if not all(isinstance(s, str) or isinstance(s, (list, dict)) for s in yaml_list):
-            raise ValueError(
-                "yaml_list can contain only valid YAML types!  Found:\n  %s"
-                % [type(s) for s in yaml_list]
-            )
-        self.yaml_list = yaml_list[:]
-
+        self.yaml_list = yaml_list[:] if yaml_list is not None else []
         # Expansions can be expensive to compute and difficult to keep updated
         # We cache results and invalidate when self.yaml_list changes
-        self._expanded_list = None
+        self.specs_as_yaml_list = expanded_list or []
         self._constraints = None
-        self._specs = None
+        self._specs: Optional[List[Spec]] = None
 
     @property
     def is_matrix(self):
@@ -45,12 +27,6 @@ class SpecList:
             if isinstance(item, dict):
                 return True
         return False
-
-    @property
-    def specs_as_yaml_list(self):
-        if self._expanded_list is None:
-            self._expanded_list = self._expand_references(self.yaml_list)
-        return self._expanded_list
 
     @property
     def specs_as_constraints(self):
@@ -68,7 +44,7 @@ class SpecList:
     @property
     def specs(self) -> List[Spec]:
         if self._specs is None:
-            specs = []
+            specs: List[Spec] = []
             # This could be slightly faster done directly from yaml_list,
             # but this way is easier to maintain.
             for constraint_list in self.specs_as_constraints:
@@ -80,12 +56,13 @@ class SpecList:
 
         return self._specs
 
-    def add(self, spec):
-        self.yaml_list.append(str(spec))
+    def add(self, spec: Spec):
+        spec_str = str(spec)
+        self.yaml_list.append(spec_str)
 
         # expanded list can be updated without invalidation
-        if self._expanded_list is not None:
-            self._expanded_list.append(str(spec))
+        if self.specs_as_yaml_list is not None:
+            self.specs_as_yaml_list.append(spec_str)
 
         # Invalidate cache variables when we change the list
         self._constraints = None
@@ -107,9 +84,9 @@ class SpecList:
         # Remove may contain more than one string representation of the same spec
         for item in remove:
             self.yaml_list.remove(item)
+            self.specs_as_yaml_list.remove(item)
 
         # invalidate cache variables when we change the list
-        self._expanded_list = None
         self._constraints = None
         self._specs = None
 
@@ -122,67 +99,15 @@ class SpecList:
         """
         self.yaml_list[idx] = spec
 
-        # invalidate cache variables when we change the list
-        self._expanded_list = None
+        # FIXME: invalidate cache variables when we change the list
         self._constraints = None
         self._specs = None
 
-    def extend(self, other, copy_reference=True):
+    def extend(self, other: "SpecList", copy_reference=True) -> None:
         self.yaml_list.extend(other.yaml_list)
-        self._expanded_list = None
+        self.specs_as_yaml_list.extend(other.specs_as_yaml_list)
         self._constraints = None
         self._specs = None
-
-        if copy_reference:
-            self._reference = other._reference
-
-    def update_reference(self, reference):
-        self._reference = reference
-        self._expanded_list = None
-        self._constraints = None
-        self._specs = None
-
-    def _parse_reference(self, name):
-        sigil = ""
-        name = name[1:]
-
-        # Parse specs as constraints
-        if name.startswith("^") or name.startswith("%"):
-            sigil = name[0]
-            name = name[1:]
-
-        # Make sure the reference is valid
-        if name not in self._reference:
-            msg = f"SpecList '{self.name}' refers to named list '{name}'"
-            msg += " which does not appear in its reference dict."
-            raise UndefinedReferenceError(msg)
-
-        return name, sigil
-
-    def _expand_references(self, yaml):
-        if isinstance(yaml, list):
-            ret = []
-
-            for item in yaml:
-                # if it's a reference, expand it
-                if isinstance(item, str) and item.startswith("$"):
-                    # replace the reference and apply the sigil if needed
-                    name, sigil = self._parse_reference(item)
-
-                    referent = [
-                        _sigilify(item, sigil) for item in self._reference[name].specs_as_yaml_list
-                    ]
-                    ret.extend(referent)
-                else:
-                    # else just recurse
-                    ret.append(self._expand_references(item))
-            return ret
-        elif isinstance(yaml, dict):
-            # There can't be expansions in dicts
-            return dict((name, self._expand_references(val)) for (name, val) in yaml.items())
-        else:
-            # Strings are just returned
-            return yaml
 
     def __len__(self):
         return len(self.specs)
@@ -264,10 +189,12 @@ class Definition(NamedTuple):
 
 
 class SpecListParser:
+    """Parse definitions and user specs from data in environments"""
+
     def __init__(self):
         self.definitions: Dict[str, SpecList] = {}
 
-    def parse_definitions(self, *, data: Dict[str, Any]) -> Dict[str, SpecList]:
+    def parse_definitions(self, *, data: List[Dict[str, Any]]) -> Dict[str, SpecList]:
         definitions_from_yaml: Dict[str, List[Definition]] = {}
         for item in data:
             value = self._parse_yaml_definition(item)
@@ -311,9 +238,7 @@ class SpecListParser:
                 continue
             combined_yaml_list.extend(def_part.yaml_list)
         expanded_list = self._expand_yaml_list(combined_yaml_list)
-        return SpecList.from_parser(
-            name=name, yaml_list=combined_yaml_list, expanded_list=expanded_list
-        )
+        return SpecList(name=name, yaml_list=combined_yaml_list, expanded_list=expanded_list)
 
     def _expand_yaml_list(self, raw_yaml_list):
         result = []
