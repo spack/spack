49d2e01e515ab36aebc7b1feb65ffd8022bfbc15
Alec Scott <hi@alecbcs.com>
2025-03-26 13:18:48 -0700

    Add rust dependency version constraint

--- var/spack/repos/builtin/packages/difftastic/package.py
+++ var/spack/repos/builtin/packages/difftastic/package.py
@@ -16,3 +16,5 @@ class Difftastic(CargoPackage):
     license("MIT")
 
     version("0.63.0", sha256="f96bcf4fc961921d52cd9fe5aa94017924abde3d5a3b5a4727b103e9c2d4b416")
+
+    depends_on("rust@0.64.0:", type="build")
