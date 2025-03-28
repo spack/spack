a69d13867c9d6ec176477aceb25b972c134d163a
Paul Gessinger <hello@paulgessinger.com>
2025-03-28 13:51:16 +0100

    change `apple-glu` the same way

--- var/spack/repos/builtin/packages/apple-glu/package.py
+++ var/spack/repos/builtin/packages/apple-glu/package.py
@@ -12,7 +12,5 @@ class AppleGlu(AppleGlBase):
 
     provides("glu@1.3")
 
-    requires(
-        "platform=darwin %apple-clang",
-        msg="Apple-GLU is only available on Darwin, when using Apple Clang",
-    )
+    depends_on("apple-clang", type="build")
+    requires("platform=darwin", msg="Apple-GL is only available on Darwin, when using Apple Clang")
