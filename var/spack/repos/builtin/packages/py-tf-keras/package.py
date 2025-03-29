86dafa9703a4c77d15e049e5edf083648c6ee4ee
Jonas Eschle <mayou36@jonas.eschle.com>
2025-03-29 21:18:12 +0100

    fix typo

--- var/spack/repos/builtin/packages/py-tf-keras/package.py
+++ var/spack/repos/builtin/packages/py-tf-keras/package.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.build_systems.python import PythonPipBuilder
 from spack.package import *
 
 
@@ -21,7 +20,8 @@ class PyTfKeras(PythonPackage):
 
     license("Apache-2.0", checked_by="jonas-eschle")
 
-    max_minor = 18
+    max_minor = 19
+    version("2.19.0", sha256="b09a407d87a4571ce1e8ca985cfc68483e3d63b2518a5d79a97ad92cb64dbe9c")
     version("2.18.0", sha256="ebf744519b322afead33086a2aba872245473294affd40973694f3eb7c7ad77d")
 
     # build_system = PythonPipBuilder
@@ -40,7 +40,8 @@ class PyTfKeras(PythonPackage):
     depends_on("py-pydot", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
 
-    depends_on("protobuf@3.20.3", type="build", when="@2.18:")
+    depends_on("protobuf@3.20.3", type="build", when="@2.18")
+    depends_on("protobuf@4.23.0", type="build", when="@2.19:")
     # the tf-keras versions are following along with TF versions
     for minor_ver in range(18, max_minor + 1):
         depends_on(f"py-tensorflow@2.{minor_ver}", type=("build", "run"), when=f"@2.{minor_ver}")
@@ -49,11 +50,10 @@ class PyTfKeras(PythonPackage):
     depends_on("py-portpicker", type=("build", "run"))
     depends_on("py-pyyaml", type=("build", "run"))
     depends_on("pil", type=("build", "run"))
-    depends_on("py-numpy@1.26.0:2.0", type=("build", "run"))
+    depends_on("py-numpy@1.26.0:2.0", type=("build", "run"), when="@2.18:")
 
     depends_on("bazel", type="build")
 
     depends_on("py-six", type=("build", "run"))
     depends_on("py-absl-py", type=("build", "run"))
     depends_on("py-h5py", type=("build", "run"))
-
