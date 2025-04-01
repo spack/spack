2295a7f98ca469477b7ad01da1aec08177a435d5
Marie Houillon <houillonmarie@gmail.com>
2025-04-01 11:28:39 +0200

    Update meshtool version for openCARP v17.0

--- var/spack/repos/builtin/packages/meshtool/package.py
+++ var/spack/repos/builtin/packages/meshtool/package.py
@@ -17,7 +17,7 @@ class Meshtool(MakefilePackage):
     # Version to use with openCARP releases
     # It is possible that different openCARP releases rely on the same
     # meshtool version
-    version("oc17.0", commit="867431d6bde35ad41104f611aa57130ef58cfb79")
+    version("oc17.0", commit="d6ab63fa6915995b203f4f6b278ed0c853ad15fa")
     version("oc16.0", commit="867431d6bde35ad41104f611aa57130ef58cfb79")
     version("oc15.0", commit="867431d6bde35ad41104f611aa57130ef58cfb79")
     version("oc13.0", commit="867431d6bde35ad41104f611aa57130ef58cfb79")
