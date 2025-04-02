ab955a533b5103a3d4da7e6334dd2552d811789b
Chris Richardson <chris@bpi.cam.ac.uk>
2025-04-02 12:01:00 +0100

    Add C dependency

--- var/spack/repos/builtin/packages/fenics-dolfinx/package.py
+++ var/spack/repos/builtin/packages/fenics-dolfinx/package.py
@@ -32,6 +32,7 @@ class FenicsDolfinx(CMakePackage):
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")  # generated
+    depends_on("c", type="build")  # HDF5 dependency requires C in CMake
 
     # Graph partitioner dependencies
     depends_on("kahip@3.12:", when="partitioners=kahip")
