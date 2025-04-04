414f9ea521fcc5daa201a9b4599878bca31776f2
Peter Brady <ptb@lanl.gov>
2025-04-04 11:34:56 -0500

    update libffi to 3.4.7

--- var/spack/repos/builtin/packages/libffi/package.py
+++ var/spack/repos/builtin/packages/libffi/package.py
@@ -16,6 +16,7 @@ class Libffi(AutotoolsPackage):
 
     license("MIT")
 
+    version("3.4.7", sha256="138607dee268bdecf374adf9144c00e839e38541f75f24a1fcf18b78fda48b2d")
     version("3.4.6", sha256="b0dea9df23c863a7a50e825440f3ebffabd65df1497108e5d437747843895a4e")
     version("3.4.5", sha256="96fff4e589e3b239d888d9aa44b3ff30693c2ba1617f953925a70ddebcc102b2")
     version("3.4.4", sha256="d66c56ad259a82cf2a9dfc408b32bf5da52371500b84745f7fb8b645712df676")
