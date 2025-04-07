c2e6ef04b3e069d5cc4cb6f43ce2237349f48481
Kyle Brindley <kbrindley@lanl.gov>
2025-04-07 12:33:21 -0600

    MAINT: update dill versions

--- var/spack/repos/builtin/packages/py-dill/package.py
+++ var/spack/repos/builtin/packages/py-dill/package.py
@@ -13,6 +13,9 @@ class PyDill(PythonPackage):
 
     license("BSD-3-Clause")
 
+    version("0.3.9", sha256="81aa267dddf68cbfe8029c42ca9ec6a4ab3b22371d1c450abc54422577b4512c")
+    version("0.3.8", sha256="3ebe3c479ad625c4553aca177444d89b486b1d84982eeacded644afc0cf797ca")
+    version("0.3.7", sha256="cc1c8b182eb3013e24bd475ff2e9295af86c1a38eb1aff128dac8962a9ce3c03")
     version("0.3.6", sha256="e5db55f3687856d8fbdab002ed78544e1c4559a130302693d839dfe8f93f2373")
     version("0.3.5.1", sha256="d75e41f3eff1eee599d738e76ba8f4ad98ea229db8b085318aa2b3333a208c86")
     version("0.3.4", sha256="9f9734205146b2b353ab3fec9af0070237b6ddae78452af83d2fca84d739e675")
