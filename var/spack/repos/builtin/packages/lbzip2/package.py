349fe1a9674d43056e119564d37652a7b76ee8a2
Wouter Deconinck <wdconinc@gmail.com>
2025-04-07 20:54:14 +0000

    lbzip2: depends on c, autotools

--- var/spack/repos/builtin/packages/lbzip2/package.py
+++ var/spack/repos/builtin/packages/lbzip2/package.py
@@ -12,6 +12,12 @@ class Lbzip2(AutotoolsPackage):
     homepage = "https://github.com/kjn/lbzip2/"
     url = "https://github.com/kjn/lbzip2/archive/refs/tags/v2.5.tar.gz"
 
+    depends_on("c", type="build")
+
+    depends_on("autoconf", type="build")
+    depends_on("automake", type="build")
+    depends_on("libtool", type="build")
+
     version(
         "2.5",
         sha256="7be69ece83ecdc8f12b9201d838eee5cdb499f2fd68cffd2af58866076ccac43",
