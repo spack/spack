bf9e5a98507e36272d4ff4d1a2f8f12dbe250e3d
Robert Maaskant <robert@maaskant.io>
2025-04-08 12:08:16 +0200

    py-deephyper: add back dependencies for deprecated versions

--- var/spack/repos/builtin/packages/py-deephyper/package.py
+++ var/spack/repos/builtin/packages/py-deephyper/package.py
@@ -27,7 +27,7 @@ class PyDeephyper(PythonPackage):
     # exists upstream, disabled in Spack due to missing dependencies, contributions welcome
     # variant("jax-cpu", default=False, description="Build with JAX dependencies")
     variant("torch", default=False, description="Build with PyTorch dependencies")
-    
+
     # Variants for storage/parallel backends
     variant("mpi", default=False, description="Build with MPI dependencies")
     variant("ray", default=False, description="Build with Ray dependencies")
@@ -40,20 +40,29 @@ class PyDeephyper(PythonPackage):
     with default_args(deprecated=True):
         version("0.6.0", sha256="cda2dd7c74bdca4203d9cd637c4f441595f77bae6d77ef8e4a056b005357de34")
         version("0.4.2", sha256="ee1811a22b08eff3c9098f63fbbb37f7c8703e2f878f2bdf2ec35a978512867f")
-    
+
     # Build backend
     with default_args(type="build"):
         depends_on("py-hatchling@1.25:", when="@master")
         depends_on("py-hatchling@1.25:", when="@develop")
         depends_on("py-hatchling@1.25:", when="@0.9:")
-        depends_on("py-setuptools@42:", when="@0.8:")
-        depends_on("py-cython@0.29.24:", when="@0.8:")
 
+        depends_on("py-setuptools@42:", when="@0.8:")
+        depends_on("py-setuptools@42:", when="@0.6.0")
+        depends_on("py-setuptools@40:49.1", when="@:0.6")
+
+        depends_on("py-cython@0.29.24:", when="@0.8:")
+        depends_on("py-cython@0.29.24:", when="@0.6.0")
+        depends_on("py-cython@0.29.24:2", when="@0.4.2")
+
+        depends_on("py-wheel@0.36.2", when="@:0.6")
+        
     # Python versions
     with default_args(type=("build", "run")):
-
         depends_on("python@3.10:", when="@0.9:")
         depends_on("python@3.9:", when="@0.8:")
+        depends_on("python@3.7:3.11", when="@0.6.0")
+        depends_on("python@3.7:3.9", when="@0.4.2")
 
     # Dependencies from setup/toml files
     with default_args(type=("build", "run")):
@@ -61,23 +70,33 @@ class PyDeephyper(PythonPackage):
         depends_on("py-configspace@1.1.1:", when="@0.8:")
         depends_on("py-configspace@0.4.20:")
         depends_on("py-cloudpickle", when="@0.9.3:")
-        depends_on("py-dm-tree", when="@0.8:")
+        depends_on("py-dm-tree")
         depends_on("py-jinja2@3.1.4:", when="@0.8:")
+        depends_on("py-jinja2@:3.1", when="@0.6.0")
+        depends_on("py-jinja2@:3.0", when="@0.4.2")
         depends_on("py-loky@3.4:", when="@0.9.3:")
-        depends_on("py-matplotlib", when="@0.8:")
+        depends_on("py-matplotlib")
         depends_on("py-numpy@1.26.0:", when="@0.8:")
+        depends_on("py-numpy@1.20:", when="@0.6.0")
+        depends_on("py-numpy")
         depends_on("openssl@3.4.0:", when="@0.8:")
-        depends_on("py-pandas@0.24.2:", when="@0.8:")
-        depends_on("py-packaging", when="@0.8:")
+        depends_on("py-pandas@0.24.2:")
+        depends_on("py-packaging@20.5:", when="@0.6.0 target=aarch64: platform=darwin")
+        depends_on("py-packaging")
         depends_on("py-parse", when="@0.8:")
         depends_on("py-psutil", when="@0.8:")
         depends_on("py-pymoo@0.6:", when="@0.8:")
         depends_on("py-pyyaml", when="@0.8:")
-        depends_on("py-scikit-learn@0.23.1:", when="@0.8:")
+        depends_on("py-scikit-learn@0.23.1:")
         depends_on("py-scipy@1.10:", when="@0.8:")
+        depends_on("py-scipy@1.7:", when="@0.6.0")
+        depends_on("py-scipy@0.19.1:")
+        depends_on("py-tqdm@4.64.0:")
+        depends_on("py-psutil", when="@0.8:")
+        depends_on("py-pymoo@0.6:", when="@0.8:")
+        depends_on("py-pyyaml")
+        depends_on("py-tinydb", when="@0.4.2")
 
-        depends_on("py-tqdm@4.64.0:", when="@0.8:")
-        
     with when("+dev"), default_args(type=("build", "run")):
         depends_on("py-pytest")
 
