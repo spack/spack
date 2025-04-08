1173972257833500d5eb3bce4e31010a5b1f4df1
Benoit Martin <bmartin@cea.fr>
2025-04-08 14:00:07 +0200

    Add PDI versions 1.8.2, 1.8.3 and 1.9.0

--- var/spack/repos/builtin/packages/pdiplugin-mpi/package.py
+++ var/spack/repos/builtin/packages/pdiplugin-mpi/package.py
@@ -17,6 +17,9 @@ class PdipluginMpi(CMakePackage):
     maintainers("jbigot")
 
     version("develop", branch="main", no_cache=True)
+    version("1.9.0", commit="a4eef2172375cb415e27d1f0c5fc36649bd563f8")
+    version("1.8.3", commit="2e035d8b99db5679efe97fdc3a29b2a940770a58")
+    version("1.8.2", commit="d963f59b4293846934fd3f348eb37d7286450011")
     version("1.8.1", commit="105161d5c93431d674c73ef365dce3eb724b4fcb")
     version("1.8.0", commit="edce72fc198475bab1541cc0b77a30ad02da91c5")
 
@@ -25,6 +28,9 @@ class PdipluginMpi(CMakePackage):
     depends_on("cmake@3.16.3:", type=("build"), when="@1.8:")
     depends_on("mpi", type=("build", "link", "run"))
     depends_on("pdi@develop", type=("link", "run"), when="@develop")
+    depends_on("pdi@1.9.0", type=("link", "run"), when="@1.9.0")
+    depends_on("pdi@1.8.3", type=("link", "run"), when="@1.8.3")
+    depends_on("pdi@1.8.2", type=("link", "run"), when="@1.8.2")
     depends_on("pdi@1.8.1", type=("link", "run"), when="@1.8.1")
     depends_on("pdi@1.8.0", type=("link", "run"), when="@1.8.0")
     depends_on("pkgconfig", type=("build"))
