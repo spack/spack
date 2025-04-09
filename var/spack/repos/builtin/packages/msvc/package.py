36951d706a9ab3c13086b73e6901840f8d96181a
John Parent <john.parent@kitware.com>
2025-04-09 14:37:08 -0400

    Non make executables

--- var/spack/repos/builtin/packages/msvc/package.py
+++ var/spack/repos/builtin/packages/msvc/package.py
@@ -94,8 +94,8 @@ class Msvc(Package, CompilerPackage):
         """Populates dependent module with tooling available from VS"""
         # We want these to resolve to the paths set by MSVC's VCVARs
         # so no paths
-        module.nmake = MakeExecutable("nmake", jobs=1)
-        module.msbuild = MakeExecutable("msbuild", jobs=1)
+        module.nmake = Executable("nmake")
+        module.msbuild = Executable("msbuild")
 
     def setup_dependent_build_environment(self, env, dependent_spec):
         self.init_msvc()
