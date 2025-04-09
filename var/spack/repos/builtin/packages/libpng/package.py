3c8ec392b80137ee5a38efcb73473cb273bc76e1
Adam J. Stewart <ajstewart426@gmail.com>
2025-04-09 10:10:23 +0200

    libpng: add v1.6.47

--- var/spack/repos/builtin/packages/libpng/package.py
+++ var/spack/repos/builtin/packages/libpng/package.py
@@ -17,6 +17,7 @@ class Libpng(CMakePackage):
 
     license("Libpng")
 
+    version("1.6.47", sha256="b213cb381fbb1175327bd708a77aab708a05adde7b471bc267bd15ac99893631")
     version("1.6.39", sha256="1f4696ce70b4ee5f85f1e1623dc1229b210029fa4b7aee573df3e2ba7b036937")
     version("1.6.37", sha256="505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca")
     # From http://www.libpng.org/pub/png/libpng.html (2019-04-15)
