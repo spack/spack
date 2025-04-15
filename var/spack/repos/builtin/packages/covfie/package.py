e22a06c4842c622e2c9619d56334b54f18a4813e
Alec Scott <hi@alecbcs.com>
2025-04-15 11:28:17 -0400

    covfie: update for best practices

--- var/spack/repos/builtin/packages/covfie/package.py
+++ var/spack/repos/builtin/packages/covfie/package.py
@@ -6,26 +6,26 @@ from spack.package import *
 
 
 class Covfie(CMakePackage, CudaPackage):
-    """
-    Covfie is a library for compositional descriptions of storage methods for
-    vector fields and other structured multi-dimensional data.
-    """
+    """Covfie is a library for compositional descriptions of storage methods for
+    vector fields and other structured multi-dimensional data."""
 
     homepage = "https://github.com/acts-project/covfie"
-    url = "https://github.com/acts-project/covfie/archive/refs/tags/v0.1.0.tar.gz"
+    url = "https://github.com/acts-project/covfie/archive/refs/tags/v0.13.0.tar.gz"
+    git = "https://github.com/acts-project/covfie.git"
     list_url = "https://github.com/acts-project/covfie/tags"
-
-    license("MPL-2.0")
 
     maintainers("stephenswat")
 
+    license("MPL-2.0")
+
+    version("main", branch="main")
     version("0.13.0", sha256="e9cd0546c7bc9539f440273bbad303c97215ccd87403cedb4aa387a313938d57")
     version("0.12.1", sha256="c33d7707ee30ab5fa8df686a780600343760701023ac0b23355627e1f2f044de")
     version("0.12.0", sha256="e35e94075a40e89c4691ff373e3061577295d583a2546c682b2d652d9fce7828")
     version("0.11.0", sha256="39fcd0f218d3b4f3aacc6af497a8cda8767511efae7a72b47781f10fd4340f4f")
     version("0.10.0", sha256="d44142b302ffc193ad2229f1d2cc6d8d720dd9da8c37989ada4f23018f86c964")
 
-    depends_on("cxx", type="build")  # generated
+    depends_on("cxx", type="build")
 
     depends_on("cmake@3.21:", type="build", when="@0.11:")
     depends_on("cmake@3.18:", type="build")
