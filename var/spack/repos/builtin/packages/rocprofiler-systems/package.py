1595114c000d5f6e8f6a09c6ec4119812cdd9b03
Afzal Patel <122491982+afzpatel@users.noreply.github.com>
2025-04-15 20:57:53 -0400

    Update package.py

--- var/spack/repos/builtin/packages/rocprofiler-systems/package.py
+++ var/spack/repos/builtin/packages/rocprofiler-systems/package.py
@@ -114,7 +114,7 @@ class RocprofilerSystems(CMakePackage):
             depends_on(f"roctracer-dev@{ver}", when=f"@{ver}")
             depends_on(f"rocprofiler-dev@{ver}", when=f"@{ver}")
 
-    # adds additional included which are required gcc@13 build
+    # adds additional includes which are required for gcc-13+ build
     patch("gcc_13_fix.patch", when="%gcc@13:")
 
     def cmake_args(self):
