15b5a21c8701f24d33217927d974632d06fff012
Chris White <white238@llnl.gov>
2025-04-16 10:30:16 -0700

    update libcerf to use new URL and CMake for new versions but keep old URL and autoconf for 1.3

--- var/spack/repos/builtin/packages/libcerf/package.py
+++ var/spack/repos/builtin/packages/libcerf/package.py
@@ -4,23 +4,49 @@
 
 
 from spack.package import *
+from spack.build_systems.autotools import AutotoolsBuilder
+from spack.build_systems.cmake import CMakeBuilder
 
-
-class Libcerf(AutotoolsPackage, SourceforgePackage):
+class Libcerf(AutotoolsPackage, CMakePackage):
     """A self-contained C library providing complex error functions, based
     on Faddeeva's plasma dispersion function w(z). Also provides Dawson's
     integral and Voigt's convolution of a Gaussian and a Lorentzian
 
     """
 
-    homepage = "https://sourceforge.net/projects/libcerf/"
-    sourceforge_mirror_path = "libcerf/libcerf-1.3.tgz"
+    homepage = "https://jugit.fz-juelich.de/mlz/libcerf/"
+    url = "https://jugit.fz-juelich.de/mlz/libcerf/-/archive/v2.4/libcerf-v2.4.tar.gz"
 
-    version("1.3", sha256="d7059e923d3f370c89fb4d19ed4f827d381bc3f0e36da5595a04aeaaf3e6a859")
+    license("MIT")
 
-    depends_on("c", type="build")  # generated
-    depends_on("fortran", type="build")  # generated
+    version("2.4", sha256="080b30ae564c3dabe3b89264522adaf5647ec754021572bee54929697b276cdc")
+    version("2.3", sha256="cceefee46e84ce88d075103390b4f9d04c34e4bc3b96d733292c36836d4f7065")
+    version("1.3", sha256="d7059e923d3f370c89fb4d19ed4f827d381bc3f0e36da5595a04aeaaf3e6a859",
+            url="https://sourceforge.net/projects/libcerf/files/libcerf-1.3.tgz")
 
+    variant("cpp", default=False, when="@2:", description="Compile source as C++")
+
+    # Build system
+    build_system(
+        conditional("cmake", when="@2:"),
+        conditional("autotools", when="@=1.3"),
+        default="cmake",
+    )
+
+    depends_on("c", type="build")
+    depends_on("fortran", type="build")
+
+
+class CMakeBuilder(CMakeBuilder):
+    def cmake_args(self):
+        args = []
+
+        args.append(self.define_from_variant("CERF_CPP", "cpp"))
+
+        return args
+
+
+class AutotoolsBuilder(AutotoolsBuilder):
     def configure_args(self):
         spec = self.spec
         options = []
