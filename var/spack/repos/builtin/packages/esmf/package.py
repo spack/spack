e08b2b05293d37ea6cd35fb641ed8cad1eb01296
Matthew Thompson <matthew.thompson@nasa.gov>
2025-04-23 07:54:16 -0400

    esmf: add v8.8.1

--- var/spack/repos/builtin/packages/esmf/package.py
+++ var/spack/repos/builtin/packages/esmf/package.py
@@ -30,6 +30,7 @@ class Esmf(MakefilePackage, PythonExtension):
     # Develop is a special name for spack and is always considered the newest version
     version("develop", branch="develop")
     # generate chksum with 'spack checksum esmf@x.y.z'
+    version("8.8.1", sha256="b0acb59d4f000bfbdfddc121a24819bd2a50997c7b257b0db2ceb96f3111b173")
     version("8.8.0", sha256="f89327428aeef6ad34660b5b78f30d1c55ec67efb8f7df1991fdaa6b1eb3a27c")
     version("8.7.0", sha256="d7ab266e2af8c8b230721d4df59e61aa03c612a95cc39c07a2d5695746f21f56")
     version("8.6.1", sha256="dc270dcba1c0b317f5c9c6a32ab334cb79468dda283d1e395d98ed2a22866364")
