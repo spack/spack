42f34f8b2682e96a56825c90163343705a0939de
Alec Scott <scott112@llnl.gov>
2025-04-24 14:35:49 -0700

    py-repligit: add v0.1.1

--- var/spack/repos/builtin/packages/py-repligit/package.py
+++ var/spack/repos/builtin/packages/py-repligit/package.py
@@ -17,6 +17,7 @@ class PyRepligit(PythonPackage):
     license("Apache-2.0 WITH LLVM-exception")
 
     version("main", branch="main")
+    version("0.1.1", sha256="e1fec2b080dd657502b967148fbb7dd5d33eb02fc47a2e91ed7bbfebf082410e")
     version("0.1.0", sha256="9beac1a14542704f2e5af6a2f3d391d8adf2112ae3c70e98339db251a9e1079e")
 
     variant("aiohttp", default="False", description="Enable aiohttp support")
