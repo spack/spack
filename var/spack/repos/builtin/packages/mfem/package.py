6343708620dfe7bd7f8644c768ddc9fc719f403b
Veselin Dobrev <dobrev@llnl.gov>
2025-04-24 01:38:42 -0700

    [mfem] Set official mfem v4.8 source

--- var/spack/repos/builtin/packages/mfem/package.py
+++ var/spack/repos/builtin/packages/mfem/package.py
@@ -49,8 +49,12 @@ class Mfem(Package, CudaPackage, ROCmPackage):
     # other version.
     version("develop", branch="master")
 
-    # FIXME: update this when v4.8 is released
-    version("4.8.0-dev", branch="mfem-4.8-dev")
+    version(
+        "4.8.0",
+        sha256="49bd2a076b0d87863092cb55f8524b5292d9afb2e48c19f80222ada367819016",
+        url="https://bit.ly/mfem-4-8",
+        extension="tar.gz",
+    )
 
     version(
         "4.7.0",
