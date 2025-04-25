3499c75eada5baf5b6b6b955a27aff5dbc6174be
Peter Scheibel <scheibel1@llnl.gov>
2025-04-24 23:34:32 -0700

    add test

--- lib/spack/spack/test/package_class.py
+++ lib/spack/spack/test/package_class.py
@@ -226,6 +226,17 @@ def test_cache_extra_sources(install_mockery, spec, sources, extras, expect):
     shutil.rmtree(os.path.dirname(source_path))
 
 
+def test_mirror_check_human_readable_format(mock_packages, mutable_config, tmpdir):
+    """Package base is ultimately responsible for """
+    import spack.config
+    s = spack.concretize.concretize_one("pkg-a@2.0")
+    spack.config.add(f'mirrors:{{"test": {tmpdir}}}')
+    human_readable_path = os.path.join(tmpdir, "pkg-a", "pkg-a-2.0.tar.gz")
+    x = list(s.package.stage[0]._generate_fetchers())
+    assert any(human_readable_path in getattr(f, "url", "")
+               for f in s.package.stage[0]._generate_fetchers())
+
+
 def test_cache_extra_sources_fails(install_mockery):
     s = spack.concretize.concretize_one("pkg-a")
 
