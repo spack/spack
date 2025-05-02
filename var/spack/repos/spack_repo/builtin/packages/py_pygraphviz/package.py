49af98fa830b98d47fbc46f6fe0131d2ab52b6b0
Sinan81 <Sinan81@users.noreply.github.com>
2025-05-02 21:02:16 +0000

    [@spackbot] updating style on behalf of Sinan81

--- var/spack/repos/spack_repo/builtin/packages/py_pygraphviz/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_pygraphviz/package.py
@@ -36,11 +36,11 @@ class PyPygraphviz(PythonPackage):
             fext = "zip"
         return url.format(version, fext)
 
-    # some how graphviz isn't detected during install. 
+    # some how graphviz isn't detected during install.
     @run_before("install")
     def fix_setup(self):
         inc_dir = self["graphviz"].prefix.include
         lib_dir = self["graphviz"].prefix.lib
         fsetup = FileFilter(join_path(self.build_directory, "setup.py"))
-        fsetup.filter( "include_dirs=[],", f"include_dirs = [\"{inc_dir}\"],", string=True )
-        fsetup.filter( "library_dirs=[],", f"library_dirs = [\"{lib_dir}\"],", string=True )
+        fsetup.filter("include_dirs=[],", f'include_dirs = ["{inc_dir}"],', string=True)
+        fsetup.filter("library_dirs=[],", f'library_dirs = ["{lib_dir}"],', string=True)
