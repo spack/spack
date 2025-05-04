3fc1cf6d1126025fabbf0095372be1cf0e5058cd
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-05-04 13:06:17 +0200

    remove extra comments

--- var/spack/repos/builtin/packages/shamrock/package.py
+++ var/spack/repos/builtin/packages/shamrock/package.py
@@ -2,25 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-shamrock
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-shamrock
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
-
 
 class Shamrock(CMakePackage):
     """The Shamrock exascale framework for astrophysics"""
