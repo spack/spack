ac71c958a3c6c432f93394961344c250af8a3e4b
Paolo Tricerri <Paolo.Tricerri@arm.com>
2025-05-04 21:24:48 +0000

    Moving libstd++fs

--- var/spack/repos/builtin/packages/armpl-gcc/package.py
+++ var/spack/repos/builtin/packages/armpl-gcc/package.py
@@ -472,13 +472,10 @@ class ArmplGcc(Package):
             recursive=True,
         )
 
-        armpl_libs += find_system_libraries(["libm", "libstdc++"])
+        armpl_libs += find_system_libraries(["libm", "libstdc++", "libstdc++fs"])
 
         if self.spec.satisfies("+shared"):
             armpl_libs += find_system_libraries(["libgomp"])
-
-        if self.spec.satisfies("%gcc@:8"):
-            armpl_libs += find_system_libraries(["libstdc++fs"])
 
         return armpl_libs
 
