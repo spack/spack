16c20fac038b84b0919ef1eb0cfbb8393bb9cf33
sbulut <sbulut@3vgeomatics.com>
2025-05-06 10:18:59 -0700

    package/qwtpolar: specify qwt version to fix build issue

--- var/spack/repos/builtin/packages/qwtpolar/package.py
+++ var/spack/repos/builtin/packages/qwtpolar/package.py
@@ -19,6 +19,8 @@ class Qwtpolar(QMakePackage):
 
     depends_on("qt@4.4:")
     depends_on("qwt@6.1:")
+    # build fails with qwt@6.2
+    depends_on("qwt@:6.1", when="@1.1.1")
 
     def patch(self):
         # Modify hardcoded prefix
