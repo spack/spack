5d0fb3c3cb1f6b83a660a772f2ce67be07199f40
Chris Marsh <chrismarsh.c2@gmail.com>
2025-05-07 23:24:45 +0000

    style

--- var/spack/repos/spack_repo/builtin/packages/py_panel/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_panel/package.py
@@ -18,7 +18,7 @@ class PyPanel(PythonPackage):
     version("1.6.3a2", sha256="c4413f8f604c7457b4f0fe7b3d375cc8cdeeacdbf01480b89cd00a49453a734a")
     version("1.5.2", sha256="30a45f314716bdde2de5c002fbd3a0b4d6ff85459e2179284df559455ff1534b")
     version("0.14.4", sha256="b853d2f53d7738ec6372525360c5bf9427a71ed990685ccac703bc9b442e9951")
-        
+
     depends_on("python@3.7:", type=("build", "run"), when="@0.14.4")
     depends_on("python@3.10:", type=("build", "run"), when="@1.5.2:")
 
@@ -30,7 +30,7 @@ class PyPanel(PythonPackage):
 
     depends_on("py-bokeh@2.4.3:2.4", type=("build", "run"), when="@0.14.4")
     depends_on("py-bokeh@3.5:3.6", type=("build", "run"), when="@1.5.2:")
-    depends_on("py-bokeh@3.7", type=("build", "run"), when="@1.6.3:")   
+    depends_on("py-bokeh@3.7", type=("build", "run"), when="@1.6.3:")
 
     depends_on("py-param@1.12:", type=("build", "run"), when="@0.14.4")
     depends_on("py-param@2.1:2", type=("build", "run"), when="@1.5.2:")
@@ -57,11 +57,3 @@ class PyPanel(PythonPackage):
     # a guess that the more recent version of node-js
     # should go with a more recent version of npm
     depends_on("npm@9:", type=("build", "run"))
-
-
-
-
-    
-    
-    
-
