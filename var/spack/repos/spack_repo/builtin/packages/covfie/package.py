179838c5cd279598b8e3b6a48da78cbba511c91a
Seth R Johnson <johnsonsr@ornl.gov>
2025-05-11 06:46:16 -0400

    covfie: add version 0.14

--- var/spack/repos/spack_repo/builtin/packages/covfie/package.py
+++ var/spack/repos/spack_repo/builtin/packages/covfie/package.py
@@ -19,6 +19,7 @@ class Covfie(CMakePackage, CudaPackage):
     license("MPL-2.0")
 
     version("main", branch="main")
+    version("0.14.0", sha256="b4d8afa712c6fc0e2bc6474367d65fad652864b18d0255c5f2c18fd4c6943993")
     version("0.13.0", sha256="e9cd0546c7bc9539f440273bbad303c97215ccd87403cedb4aa387a313938d57")
     version("0.12.1", sha256="c33d7707ee30ab5fa8df686a780600343760701023ac0b23355627e1f2f044de")
     version("0.12.0", sha256="e35e94075a40e89c4691ff373e3061577295d583a2546c682b2d652d9fce7828")
