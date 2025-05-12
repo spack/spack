7b71e4b9b1b8533cb304e7ec0ad87642b3928a69
Alex Richert <alexander.richert@noaa.gov>
2025-05-12 11:47:00 -0700

    Update lib/spack/spack/test/cmd/mirror.py
    Co-authored-by: Greg Becker <becker33@llnl.gov>

--- lib/spack/spack/test/cmd/mirror.py
+++ lib/spack/spack/test/cmd/mirror.py
@@ -74,7 +74,7 @@ def test_mirror_spec_from_env(mutable_mock_env_path, tmp_path, mock_packages, mo
 
     e = ev.read(env_name)
     assert set(os.listdir(mirror_dir)) == set([s.name for s in e.user_specs])
-    spec = list(e.specs_by_hash.values())[0]
+    spec = e.concrete_roots()[0]
     mirror_res = os.listdir(os.path.join(mirror_dir, spec.name))
     expected = ["%s.tar.gz" % spec.format("{name}-{version}")]
     assert mirror_res == expected
