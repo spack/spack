3c85dcf4a87f94e59e7a8348c09abc3b9b4e4990
Bram Veenboer <bram.veenboer@gmail.com>
2025-05-14 09:56:17 +0200

    Update order of version from newest to oldest

--- var/spack/repos/spack_repo/builtin/packages/py_pynvml/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_pynvml/package.py
@@ -17,9 +17,9 @@ class PyPynvml(PythonPackage):
 
     license("Unlicense")
 
-    version("8.0.4", sha256="c8d4eadc648c7e12a3c9182a9750afd8481b76412f83747bcc01e2aa829cde5d")
-    version("11.5.3", sha256="183d223ae487e5f00402d8da06c68c978ef8a9295793ee75559839c6ade7b229")
     version("12.0.0", sha256="299ce2451a6a17e6822d6faee750103e25b415f06f59abb8db65d30f794166f5")
+    version("11.5.3", sha256="183d223ae487e5f00402d8da06c68c978ef8a9295793ee75559839c6ade7b229")
+    version("8.0.4", sha256="c8d4eadc648c7e12a3c9182a9750afd8481b76412f83747bcc01e2aa829cde5d")
 
     depends_on("py-setuptools", type="build")
     depends_on("python@3.6:3.11", when="@8.0.4", type=("build", "run"))
