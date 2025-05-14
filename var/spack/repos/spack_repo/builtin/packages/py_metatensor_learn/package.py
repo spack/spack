c95f0d2c64f566fa28ff90fd669efc81e88b9a5f
Rohit Goswami <rgoswami@ieee.org>
2025-05-14 20:04:00 +0000

    ENH: Setup py-metatensor-learn and py-metatensor-operations

---
+++ var/spack/repos/spack_repo/builtin/packages/py_metatensor_learn/package.py
@@ -0,0 +1,40 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyMetatensorLearn(PythonPackage):
+    """Building blocks for the atomistic machine learning models based on PyTorch and NumPy"""
+
+    homepage = "https://docs.metatensor.org"
+    pypi = "metatensor-learn/metatensor_learn-0.3.2.tar.gz"
+
+    import_modules = ["metatensor.learn"]
+
+    maintainers("HaoZeke", "luthaf")
+    license("BSD-3-Clause", checked_by="HaoZeke")
+
+    version("0.3.2", sha256="987f63228888882a6189137ddb89f913b2fde1072c3caa83a39b9f5d50388b51")
+    version("0.3.1", sha256="576bf02a83bc513a6a7fea29c864f202d2933386f23a722525bb981d6a55a65c")
+    version("0.3.0", sha256="01dd854c40bbc1149cb92aba9c69510f0fcedb7a9d6f4175bb43b1f00838a664")
+    version("0.2.3", sha256="c561bc97ab7c5699645140d7494c93d66f7029e02c3c9cd34744ca14f7ef150f")
+    version("0.2.2", sha256="ab1b76b2c2a1686a04e811f6d261eb6e7d30c6727ed795c3f55deb61ded98494")
+
+    extends("python@3.9:")
+
+    variant("torch", default=False, description="With PyTorch")
+
+    # pyproject.toml
+    depends_on("py-setuptools@68:", type="build")
+    depends_on("py-packaging@23:", type="build")
+    depends_on("py-pip@22.1:", type="build")
+    depends_on("py-torch@2.6:", type=("build", "run"), when="+torch")
+    depends_on("py-numpy", type=("build", "run"), when="+torch")
+    # >=0.3.0 and <0.4.0
+    depends_on("py-metatensor-operations@0.3:",
+               type=("build", "run"),
+               when="@0.3:")
+    conflicts("py-metatensor-operations@0.4.0:",
+               when="@0.3:")
---
+++ var/spack/repos/spack_repo/builtin/packages/py_metatensor_operations/package.py
@@ -0,0 +1,38 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyMetatensorOperations(PythonPackage):
+    """Operations to manipulate metatensor data types."""
+
+    homepage = "https://docs.metatensor.org"
+    pypi = "metatensor-operations/metatensor_operations-0.3.3.tar.gz"
+
+    import_modules = ["metatensor.operations"]
+
+    maintainers("HaoZeke", "luthaf")
+    license("BSD-3-Clause", checked_by="HaoZeke")
+
+    version("0.3.3", sha256="432d267ce1f3c5ee11994d5348e70bc517a3c19ef68982af7bb470463e3c1b6b")
+    version("0.3.2", sha256="90f815fc09632be9dd9176a89368fb7acc7c628f081201c2916220f61eb7a1af")
+    version("0.3.1", sha256="715b34eb199232d005481a7c763e2f5ff69f23535cccaa9d4b07ca9977516b76")
+    version("0.3.0", sha256="65aa60e9cbc98243bcb5bc3d7d89bae8400e3963be5fbcf1b5fbd7bec8b0b9b4")
+    version("0.2.4", sha256="a93999596f28ac0d6c6c7e2c4a39de7cf25a9dd48c50dd4ba6f0b95a1b9f1124")
+    version("0.2.3", sha256="409134773d0a59ff08e6b31ec24d1e5dcbb0f8fc8d1d2e94def10242de088122")
+    version("0.2.2", sha256="64898aaf11d1ba7e9bdc48c25030839736b762c10c6cf6b2a3efad9c6f73988b")
+
+    extends("python@3.9:")
+    variant("torch", default=False, description="With PyTorch")
+
+    # pyproject.toml
+    depends_on("py-setuptools@77:", type="build")
+    depends_on("py-packaging@23:", type="build")
+    depends_on("py-pip@22.1:", type="build")
+    depends_on("py-torch@2.6:", type=("build", "run"), when="+torch")
+    depends_on("py-libmetatensor@0.1.10:", type="run")
+    conflicts("py-libmetatensor@0.2.0:")
+    # XXX: Needs to be declared upstream
+    depends_on("py-numpy", type=("build", "run"))
