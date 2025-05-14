cc0fb5b834c3eb1435a1d84c9ec6bc6e073467be
Bram Veenboer <bram.veenboer@gmail.com>
2025-05-14 10:09:36 +0200

    Remove unneeded requirement on python@3.6
    Since Spack only has Python 3.6 or newer anyway.

--- var/spack/repos/spack_repo/builtin/packages/py_pynvml/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_pynvml/package.py
@@ -22,5 +22,5 @@ class PyPynvml(PythonPackage):
     version("8.0.4", sha256="c8d4eadc648c7e12a3c9182a9750afd8481b76412f83747bcc01e2aa829cde5d")
 
     depends_on("py-setuptools", type="build")
-    depends_on("python@3.6:3.11", when="@8.0.4", type=("build", "run"))
-    depends_on("python@3.6:", when="@11:", type=("build", "run"))
+    depends_on("python", when="@11:", type=("build", "run"))
+    depends_on("python@:3.11", when="@8.0.4", type=("build", "run"))
