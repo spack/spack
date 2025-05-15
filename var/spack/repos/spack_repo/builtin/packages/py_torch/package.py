3b6dff592e8973e98f5ef3fffe6ec0dfc747d339
Renjith Ravindran <Renjith.RavindranKannath@amd.com>
2025-05-14 23:55:19 -0700

    Fix style

--- var/spack/repos/spack_repo/builtin/packages/py_torch/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_torch/package.py
@@ -317,9 +317,9 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
         depends_on("aotriton@0.8.1b", when="@2.5:2.6")
         depends_on("aotriton@0.9.1b", when="@2.7")
         depends_on("aotriton@0.9.1b", when="@main")
-        depends_on("composable-kernel@:6.3.2",  when="@2.5.0:2.5.2")
-        depends_on("composable-kernel@6.3.2",  when="@2.6:")
-        depends_on("composable-kernel@6.3.2",  when="@main")
+        depends_on("composable-kernel@:6.3.2", when="@2.5.0:2.5.2")
+        depends_on("composable-kernel@6.3.2", when="@2.6:")
+        depends_on("composable-kernel@6.3.2", when="@main")
     depends_on("mpi", when="+mpi")
     depends_on("ucc", when="+ucc")
     depends_on("ucx", when="+ucc")
@@ -408,9 +408,10 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
     patch("PR152569-Update-spack-includes-2.5.patch", when="@2.5+rocm")
     patch("PR152569-Update-spack-includes-2.6.patch", when="@2.6+rocm")
     patch("PR152569-Update-spack-includes-2.7.patch", when="@2.7+rocm")
-    patch("https://github.com/pytorch/pytorch/commit/38e81a53324146d445a81eb8f80bccebe623eb35.patch?full_index=1",
-          sha256="ef05dfff1502963b87679295c07d5f2bd452879708f7124274cc549ed67cd587",
-          when="@2.6:+rocm"
+    patch(
+        "https://github.com/pytorch/pytorch/commit/38e81a53324146d445a81eb8f80bccebe623eb35.patch?full_index=1",
+        sha256="ef05dfff1502963b87679295c07d5f2bd452879708f7124274cc549ed67cd587",
+        when="@2.6:+rocm"
     )
 
     # Fixes compilation with Clang 9.0.0 and Apple Clang 11.0.3
