d5a35e188df411b5ed8b48acd99f527c12dd87b1
Victor A. P. Magri <paludettomag1@llnl.gov>
2025-05-14 22:02:46 -0400

    hypre: fix ~fortran variant

--- var/spack/repos/spack_repo/builtin/packages/hypre/package.py
+++ var/spack/repos/spack_repo/builtin/packages/hypre/package.py
@@ -398,8 +398,9 @@ class Hypre(AutotoolsPackage, CudaPackage, ROCmPackage):
         makefile = join_path(install_test_root(self), self.extra_install_tests, "Makefile")
         filter_file(r"^HYPRE_DIR\s* =.*", f"HYPRE_DIR = {self.prefix}", makefile)
         filter_file(r"^CC\s*=.*", f"CC = {os.environ['CC']}", makefile)
-        filter_file(r"^F77\s*=.*", f"F77 = {os.environ['F77']}", makefile)
         filter_file(r"^CXX\s*=.*", f"CXX = {os.environ['CXX']}", makefile)
+        if self.spec.satisfies("+fortan"):
+            filter_file(r"^F77\s*=.*", f"F77 = {os.environ['F77']}", makefile)
 
     @property
     def _cached_tests_work_dir(self):
