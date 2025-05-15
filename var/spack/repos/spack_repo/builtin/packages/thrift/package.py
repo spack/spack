ca761d4d5582dc5cf82b76da2a0ecdb37e0465c6
Chris Marsh <chrismarsh.c2@gmail.com>
2025-05-15 15:52:08 +0000

    add missing boost depends

--- var/spack/repos/spack_repo/builtin/packages/thrift/package.py
+++ var/spack/repos/spack_repo/builtin/packages/thrift/package.py
@@ -69,6 +69,8 @@ class Thrift(CMakePackage, AutotoolsPackage):
             depends_on("automake")
             depends_on("libtool")
 
+    # min ver in build/cmake/BoostMacros.cmake
+    depends_on("boost@1.56:", when="@0.13: +cpp")
     depends_on("glib@2:", when="+c_glib")
     depends_on("openssl", when="+openssl")
     depends_on("libevent@2:", when="+libevent")
