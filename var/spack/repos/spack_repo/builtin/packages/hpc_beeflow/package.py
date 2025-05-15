4c1fdac1f6715ea80b6cd758712d0f11a188495f
Krishna Chilleri <krishnachilleri@gmail.com>
2025-05-15 15:55:01 -0600

    remove 0.1.9 version and downgrade version of cwl-utils

--- var/spack/repos/spack_repo/builtin/packages/hpc_beeflow/package.py
+++ var/spack/repos/spack_repo/builtin/packages/hpc_beeflow/package.py
@@ -26,13 +26,13 @@ class HpcBeeflow(PythonPackage):
     license("MIT")
 
     version("0.1.10", sha256="b7863798e15591a16f6cd265f9b5b7385779630f1c37d8a2a5178b8bf89fc664")
-    version("0.1.9", sha256="196eb9155a5ca6e35d0cc514e0609cf352fc757088707306653496b83a311ac1")
+    #version("0.1.9", sha256="196eb9155a5ca6e35d0cc514e0609cf352fc757088707306653496b83a311ac1")
 
     depends_on("neo4j@5.17.0", type=("build", "run"))
     depends_on("redis@7.4.0", type=("build", "run"))
 
-    depends_on("python@3.8.3:3.13.0", when="@0.1.10:", type=("build", "run"))
-    depends_on("python@3.8.3:3.12.2", when="@:0.1.9", type=("build", "run"))
+    depends_on("python@3.8.3:3.13.0", type=("build", "run"))
+    #depends_on("python@3.8.3:3.12.2", when="@:0.1.9", type=("build", "run"))
     depends_on("py-poetry@0.12:", type="build")
 
     depends_on("py-flask@2.0:", type=("build", "run"))
@@ -41,13 +41,13 @@ class HpcBeeflow(PythonPackage):
     depends_on("py-neo4j@5:", type=("build", "run"))
     depends_on("py-pyyaml@6.0.1:", type=("build", "run"))
     depends_on("py-flask-restful@0.3.9", type=("build", "run"))
-    depends_on("py-cwl-utils@0.16:", type=("build", "run"))
+    depends_on("py-cwl-utils@0.16", type=("build", "run"))
     depends_on("py-apscheduler@3.6.3:", type=("build", "run"))
     depends_on("py-jsonpickle@2.2.0:", type=("build", "run"))
-    depends_on("py-requests@:2.28", when="@:0.1.9", type=("build", "run"))
-    depends_on("py-requests@2.32.3:", when="@0.1.10:", type=("build", "run"))
-    depends_on("py-requests-unixsocket@0.3.0:", when="@:0.1.9", type=("build", "run"))
-    depends_on("py-requests-unixsocket@0.4.1:", when="@0.1.10:", type=("build", "run"))
+    #depends_on("py-requests@:2.28", when="@:0.1.9", type=("build", "run"))
+    depends_on("py-requests@2.32.3:", type=("build", "run"))
+    #depends_on("py-requests-unixsocket@0.3.0:", when="@:0.1.9", type=("build", "run"))
+    depends_on("py-requests-unixsocket@0.4.1:", type=("build", "run"))
     depends_on("py-python-daemon@2.3.1:", type=("build", "run"))
     depends_on("py-gunicorn@20.1.0:22", type=("build", "run"))
     depends_on("py-typer@0.5.0", type=("build", "run"))
@@ -55,8 +55,8 @@ class HpcBeeflow(PythonPackage):
     depends_on("py-celery+redis+sqlalchemy@5.3.4:", type=("build", "run"))
     depends_on("py-docutils@0.18.1", type=("build", "run"))
     depends_on("py-graphviz@0.20.3:", type=("build", "run"))
-    depends_on("py-networkx@3.1", when="@0.1.10:", type=("build", "run"))
-    depends_on("py-pre-commit@3.5.0", when="@0.1.10:", type=("build", "run"))
+    depends_on("py-networkx@3.1", type=("build", "run"))
+    depends_on("py-pre-commit@3.5.0", type=("build", "run"))
     depends_on("py-mypy-extensions", type=("build", "run"))
 
     def setup_run_environment(self, env):
