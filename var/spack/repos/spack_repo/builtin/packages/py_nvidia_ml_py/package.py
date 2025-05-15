b57e2a1f9e6e980fdf6b86eaece4002f566e6395
Bram Veenboer <bram.veenboer@gmail.com>
2025-05-15 21:15:38 +0200

    Remove spaces on empty line

--- var/spack/repos/spack_repo/builtin/packages/py_nvidia_ml_py/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_nvidia_ml_py/package.py
@@ -36,7 +36,7 @@ class PyNvidiaMlPy(PythonPackage):
     version("11.450.51", sha256="5aa6dd23a140b1ef2314eee5ca154a45397b03e68fd9ebc4f72005979f511c73")
 
     # pip silently replaces distutils with setuptools
-    
+
     def url_for_version(self, version):
         url = "https://files.pythonhosted.org/packages/source/n/nvidia-ml-py/nvidia{0}ml{0}py-{1}.tar.gz"
         if version > Version("12.560.30"):
