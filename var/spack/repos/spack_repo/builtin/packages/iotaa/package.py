4e0450967c86f5e1edb240db81e0774f6587734e
WeirAE <weirbs@gmail.com>
2025-05-16 16:25:33 +0000

    fix boilerplate

--- var/spack/repos/spack_repo/builtin/packages/iotaa/package.py
+++ var/spack/repos/spack_repo/builtin/packages/iotaa/package.py
@@ -1,22 +1,6 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install iotaa
-#
-# You can edit this file again by typing:
-#
-#     spack edit iotaa
-#
-# See the Spack documentation for more information on packaging.
+# SPDX-License-Identifier: Apache-2.0
 # ----------------------------------------------------------------------------
 
 from spack.build_systems.python import PythonPipBuilder
--- var/spack/repos/spack_repo/builtin/packages/uwtools/package.py
+++ var/spack/repos/spack_repo/builtin/packages/uwtools/package.py
@@ -1,6 +1,6 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+# SPDX-License-Identifier: GPL-2.0
 # ----------------------------------------------------------------------------
 
 from spack.build_systems.python import PythonPipBuilder
