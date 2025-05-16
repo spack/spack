299757c3c7e0bd96eb566a88dcb1b4ae7a57456b
Harmen Stoppels <me@harmenstoppels.nl>
2025-05-16 18:18:51 +0200

    migrate builtin pkgs from api v1.0 -> 2.0

---
+++ var/spack/test_repos/spack_repo/builtin_mock/packages/attributes_foo_app/package.py
@@ -0,0 +1,12 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.bundle import BundlePackage
+from spack.package import *
+
+
+class AttributesFooApp(BundlePackage):
+    version("1.0")
+    depends_on("bar")
+    depends_on("baz")
---
+++ var/spack/test_repos/spack_repo/builtin_mock/packages/no_url_or_version/package.py
@@ -0,0 +1,12 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.generic import Package
+from spack.package import *
+
+
+class NoUrlOrVersion(Package):
+    """Mock package that has no url and no version."""
+
+    homepage = "https://example.com/"
