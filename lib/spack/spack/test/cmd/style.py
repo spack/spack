88bf6383e186bc28df3f96d61fcab7c55be074d1
Harmen Stoppels <me@harmenstoppels.nl>
2025-05-16 20:44:42 +0200

    adapt style test

--- lib/spack/spack/test/cmd/style.py
+++ lib/spack/spack/test/cmd/style.py
@@ -241,14 +241,14 @@ def test_external_root(external_style_root, capfd):
     assert "%s Imports are incorrectly sorted" % str(py_file) in output
 
     # mypy error
-    assert 'lib/spack/spack/dummy.py:9: error: Name "Package" is not defined' in output
+    assert 'lib/spack/spack/dummy.py:47: error: Name "version" is not defined' in output
 
     # black error
     assert "--- lib/spack/spack/dummy.py" in output
     assert "+++ lib/spack/spack/dummy.py" in output
 
     # flake8 error
-    assert "lib/spack/spack/dummy.py:6: [F401] 'os' imported but unused" in output
+    assert "lib/spack/spack/dummy.py:8: [F401] 'os' imported but unused" in output
 
 
 @pytest.mark.skipif(not FLAKE8, reason="flake8 is not installed.")
