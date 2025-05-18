80fc9965d3b1cea96be623c43025e2434d40c7b8
Gregory Becker <becker33@llnl.gov>
2025-05-18 11:45:50 -0700

    add missing test_repo package
    Signed-off-by: Gregory Becker <becker33@llnl.gov>

---
+++ var/spack/test_repos/builtin.mock/packages/conditional-languages/package.py
@@ -0,0 +1,20 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack.package import *
+
+
+class ConditionalLanguages(Package):
+    """Conditional depends on c/cxx/fortran with a variant for each"""
+
+    homepage = "https://dev.null"
+
+    version("1.0")
+
+    variant("c", default=False, description="depend on c")
+    variant("cxx", default=False, description="depend on cxx")
+    variant("fortran", default=False, description="depend on fortran")
+
+    depends_on("c", type="build", when="+c")
+    depends_on("cxx", type="build", when="+cxx")
+    depends_on("fortran", type="build", when="+fortran")
