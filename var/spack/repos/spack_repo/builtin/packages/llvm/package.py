ae8e452c94615628bce69179b0ef59c65fa70c62
Harmen Stoppels <me@harmenstoppels.nl>
2025-05-19 16:14:06 +0200

    remove double import

--- var/spack/repos/spack_repo/builtin/packages/llvm/package.py
+++ var/spack/repos/spack_repo/builtin/packages/llvm/package.py
@@ -13,7 +13,6 @@ from llnl.util.lang import classproperty
 
 from spack.operating_systems.mac_os import macos_sdk_path
 from spack.package import *
-from spack.package import PackageBase
 
 
 class LlvmDetection(PackageBase):
