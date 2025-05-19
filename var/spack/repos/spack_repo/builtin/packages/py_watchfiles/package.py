648708525efe74529031f21143fd808dda44ff8d
Fernando Ayats <ayatsfer@gmail.com>
2025-05-19 09:02:13 +0000

    py-watchfiles: add version 1.0.5

--- var/spack/repos/spack_repo/builtin/packages/py_watchfiles/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_watchfiles/package.py
@@ -15,7 +15,9 @@ class PyWatchfiles(PythonPackage):
 
     license("MIT")
 
+    version("1.0.5", sha256="b7529b5dcc114679d43827d8c35a07c493ad6f083633d573d81c660abc5979e9")
     version("0.18.1", sha256="4ec0134a5e31797eb3c6c624dbe9354f2a8ee9c720e0b46fc5b7bab472b7c6d4")
 
-    depends_on("py-maturin@0.13", type="build")
+    depends_on("py-maturin@0.13", type="build", when="@0.18.1")
+    depends_on("py-maturin@0.14:2", type="build", when="@1:")
     depends_on("py-anyio@3:", type=("build", "run"))
