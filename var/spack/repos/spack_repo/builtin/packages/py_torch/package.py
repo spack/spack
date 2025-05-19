af1af72fbbc3da60ebe88c06979ab4fd4210c745
renjithravindrankannath <94420380+renjithravindrankannath@users.noreply.github.com>
2025-05-19 08:59:19 -0700

    Update var/spack/repos/spack_repo/builtin/packages/py_torch/package.py
    Co-authored-by: Adam J. Stewart <ajstewart426@gmail.com>

--- var/spack/repos/spack_repo/builtin/packages/py_torch/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_torch/package.py
@@ -317,7 +317,7 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
         depends_on("aotriton@0.8.1b", when="@2.5:2.6")
         depends_on("aotriton@0.9.1b", when="@2.7")
         depends_on("aotriton@0.9.1b", when="@main")
-        depends_on("composable-kernel@:6.3.2", when="@2.5.0:2.5.2")
+        depends_on("composable-kernel@:6.3.2", when="@2.5")
         depends_on("composable-kernel@6.3.2:", when="@2.6:")
     depends_on("mpi", when="+mpi")
     depends_on("ucc", when="+ucc")
