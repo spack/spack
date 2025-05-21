6f19859ed4716b3a2fd131e2352f69511abc4db9
Chris Green <greenc@fnal.gov>
2025-05-21 14:31:36 -0500

    Update command completion

--- share/spack/spack-completion.fish
+++ share/spack/spack-completion.fish
@@ -1236,13 +1236,15 @@ complete -c spack -n '__fish_spack_using_command config list' -s h -l help -f -a
 complete -c spack -n '__fish_spack_using_command config list' -s h -l help -d 'show this help message and exit'
 
 # spack config list-scopes
-set -g __fish_spack_optspecs_spack_config_list_scopes h/help f/file non-platform
+set -g __fish_spack_optspecs_spack_config_list_scopes h/help f/file non-platform included
 complete -c spack -n '__fish_spack_using_command config list-scopes' -s h -l help -f -a help
 complete -c spack -n '__fish_spack_using_command config list-scopes' -s h -l help -d 'show this help message and exit'
 complete -c spack -n '__fish_spack_using_command config list-scopes' -s f -l file -f -a file
 complete -c spack -n '__fish_spack_using_command config list-scopes' -s f -l file -d 'list only writable scopes with an associated file'
 complete -c spack -n '__fish_spack_using_command config list-scopes' -l non-platform -f -a non_platform
 complete -c spack -n '__fish_spack_using_command config list-scopes' -l non-platform -d 'list only non-platform scopes'
+complete -c spack -n '__fish_spack_using_command config list-scopes' -l included -f -a included
+complete -c spack -n '__fish_spack_using_command config list-scopes' -l included -d 'list only included scopes (overrides --file and --non-platform)'
 
 # spack config add
 set -g __fish_spack_optspecs_spack_config_add h/help f/file=
