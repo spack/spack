cfe4533db502bd2bb992d78f65f31eb2099b2c14
Marc Mengel <mengel@fnal.gov>
2025-05-21 16:04:07 -0500

    style

--- lib/spack/spack/paths.py
+++ lib/spack/spack/paths.py
@@ -93,8 +93,8 @@ def _get_user_cache_path():
     subdir = h.hexdigest()[:8]
 
     return os.path.expanduser(
-           os.getenv("SPACK_USER_CACHE_PATH") or os.path.join("~%s.spack" % os.sep, subdir)
-        )
+       os.getenv("SPACK_USER_CACHE_PATH") or os.path.join("~%s.spack" % os.sep, subdir)
+    )
 
 
 user_cache_path = str(PurePath(_get_user_cache_path()))
