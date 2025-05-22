8dec31c0e66ba9750fda0fa36d883841fbdf3d84
Tara Drwenski <drwenski1@llnl.gov>
2025-05-22 09:05:19 -0500

    Add missing import to quandary package

--- var/spack/repos/spack_repo/builtin/packages/quandary/package.py
+++ var/spack/repos/spack_repo/builtin/packages/quandary/package.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems.cached_cmake import CachedCMakePackage
+from spack_repo.builtin.build_systems.cached_cmake import CachedCMakePackage, cmake_cache_path
 from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
 
