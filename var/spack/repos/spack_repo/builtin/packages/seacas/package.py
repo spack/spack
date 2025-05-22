7b9af95f6291c69c38729769ec95d33e1a5bb60a
Greg Sjaardema <gsjaardema@gmail.com>
2025-05-22 09:28:50 -0600

    seacas: new version, aprepro, ejoin, ioss decomp
    Improve handling of commented if/elseif/endif/include/loop blocks in aprepro to avoid unexpected output
    
    ejoin nodeset merge, combine same-name nodesets
    
    exodiff permuted connectivity option
    
    ioss auto-decomp with omitted blocks gives balanced results

--- var/spack/repos/spack_repo/builtin/packages/seacas/package.py
+++ var/spack/repos/spack_repo/builtin/packages/seacas/package.py
@@ -37,6 +37,9 @@ class Seacas(CMakePackage):
     # ###################### Versions ##########################
     version("master", branch="master")
     version(
+        "2025-05-22", sha256="31dd6e9885280aa1b884c6b3e8fed17ada3d1610760f870e87434526e616e100"
+    )
+    version(
         "2025-05-05", sha256="9e9872cee4223482d74918b0f50cc3ec77791f79330915611a0e1d5691c15184"
     )
     version(
