472ffeb0b29e13c8bb34aedad343a29cdf2937bc
Greg Sjaardema <gsjaardema@gmail.com>
2025-05-22 11:52:26 -0600

    Pull version forward for EJoin fix

--- var/spack/repos/spack_repo/builtin/packages/seacas/package.py
+++ var/spack/repos/spack_repo/builtin/packages/seacas/package.py
@@ -37,7 +37,7 @@ class Seacas(CMakePackage):
     # ###################### Versions ##########################
     version("master", branch="master")
     version(
-        "2025-05-22", sha256="93308a631597e7b35a02bc54b55372f6206798c5f5ea07d7c7b7c7c18abd692b"
+        "2025-05-22", sha256="beb1621b5a3ac326491e2bd149d3b0d1484aaeee0de6b3d1a6bf487bb87288de"
     )
     version(
         "2025-05-05", sha256="9e9872cee4223482d74918b0f50cc3ec77791f79330915611a0e1d5691c15184"
