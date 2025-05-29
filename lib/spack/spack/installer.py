5fd324012a7202c5ede2eb76817b842e7f41b49f
Kathleen Shea <shea9@llnl.gov>
2025-05-29 14:15:16 -0700

    first draft of jobserver implementation

--- lib/spack/spack/installer.py
+++ lib/spack/spack/installer.py
@@ -2407,7 +2407,7 @@ class PackageInstaller:
         return None
 
     def setup_jobserver(self) -> Tuple[str, int]:
-        """Setup FIFO implemnetation of make jobserver."""
+        """Setup FIFO implementation of make jobserver."""
         if sys.platform != "win32":
             # create a named FIFO pipe for make jobserver
             fifo_directory = tempfile.mkdtemp(prefix="jobserver_fifo")
