1b78b37c2ae22743fcfa5aaccc2749ad3006f831
Ralf Mueller <ralf.mueller@dkrz.de>
2025-05-30 12:19:46 +0200

    add v2.5.1 and v.2.5.2

--- var/spack/repos/spack_repo/builtin/packages/cdo/package.py
+++ var/spack/repos/spack_repo/builtin/packages/cdo/package.py
@@ -22,6 +22,16 @@ class Cdo(AutotoolsPackage):
     maintainers("skosukhin", "Try2Code")
 
     version(
+        "2.5.2",
+        sha256="3b28da72d75547663b1b9b08332bfe3f884d27742d0eeeb7f3c8b2c70f521fa9",
+        url="https://code.mpimet.mpg.de/attachments/download/29938/cdo-2.5.0.tar.gz",
+    )
+    version(
+        "2.5.1",
+        sha256="418bf91e864cbfe547c3c8e150d31419cfa715e7d345508c5591b1abda5457d1",
+        url="https://code.mpimet.mpg.de/attachments/download/29864/cdo-2.5.0.tar.gz",
+    )
+    version(
         "2.5.0",
         sha256="e865c05c1b52fd76b80e33421554db81b38b75210820bdc40e8690f4552f68e2",
         url="https://code.mpimet.mpg.de/attachments/download/29786/cdo-2.5.0.tar.gz",
