04021ee03887802ebd8c57ce66b02615e6f7e576
Thomas Bouvier <contact@thomas-bouvier.io>
2025-05-30 17:51:03 +0200

    orjson: update version

--- var/spack/repos/spack_repo/builtin/packages/py_orjson/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_orjson/package.py
@@ -15,6 +15,7 @@ class PyOrjson(PythonPackage):
 
     license("Apache-2.0")
 
+    version("3.10.18", sha256="e8da3947d92123eda795b68228cafe2724815621fe35e8e320a9e9593a4bcd53")
     version("3.10.3", sha256="2b166507acae7ba2f7c315dcf185a9111ad5e992ac81f2d507aac39193c2c818")
     version("3.9.15", sha256="95cae920959d772f30ab36d3b25f83bb0f3be671e986c72ce22f8fa700dae061")
     version("3.8.14", sha256="5ea93fd3ef7be7386f2516d728c877156de1559cda09453fc7dd7b696d0439b3")
@@ -23,11 +24,11 @@ class PyOrjson(PythonPackage):
     depends_on("c", type="build")  # generated
 
     with default_args(type="build"):
-        with when("@3.8"):
-            depends_on("rust@1.60:")
-            depends_on("python@3.7:")
-            depends_on("py-maturin@0.13:0.14")
         with when("@03.9:"):
             depends_on("rust@1.72:")
             depends_on("python@3.8:")
             depends_on("py-maturin@1")
+        with when("@3.8"):
+            depends_on("rust@1.60:")
+            depends_on("python@3.7:")
+            depends_on("py-maturin@0.13:0.14")
