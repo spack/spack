768f502004a9ab54e3d62f27083d15e464a2fc18
eap <eparker@ucar.edu>
2025-05-30 14:15:41 -0600

    Add package.py

---
+++ var/spack/repos/spack_repo/builtin/packages/py_paho_mqtt/package.py
@@ -0,0 +1,34 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+
+from spack.package import *
+
+
+class PyPahoMqtt(PythonPackage):
+    """paho-mqtt is the Eclipse Paho MQTT Python client library, which
+    implements versions 5.0, 3.1.1, and 3.1 of the MQTT protocol.
+
+    This library provides a client class which enables applications to connect
+    to an MQTT broker to publish messages, and to subscribe to topics and
+    receive published messages. It also provides some helper functions to make
+    publishing one off messages to an MQTT server very straightforward.
+    """
+
+    homepage = "https://eclipse.dev/paho/"
+    #pypi = "bcolz/bcolz-1.2.1.tar.gz"https://files.pythonhosted.org/packages/39/15/0a6214e76d4d32e7f663b109cf71fb22561c2be0f701d67f93950cd40542/paho_mqtt-2.1.0.tar.gz
+    pypi = "paho-mqtt/paho_mqtt-2.1.0.tar.gz"
+
+    version("2.1.0", sha256="12d6e7511d4137555a3f6ea167ae846af2c7357b10bc6fa4f7c3968fc1723834")
+    version("2.0.0", sha256="13b205f29251e4f2c66a6c923c31fc4fd780561e03b2d775cff8e4f2915cf947")
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("py-hatchling", type="build")
+
+    variant("proxy", default=False, description="Enable socket proxy support")
+
+    with when("+proxy"):
+        depends_on("py-pysocks", type=("build", "run"))
