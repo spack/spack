646abe45b64d948f443d0289c107ba97ed98e49b
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2025-05-31 19:41:12 -0500

    Tried to reformat based upon other Perl packages

--- var/spack/repos/spack_repo/builtin/packages/perl_try_tiny_retry/package.py
+++ var/spack/repos/spack_repo/builtin/packages/perl_try_tiny_retry/package.py
@@ -1,8 +1,8 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack_repo.builtin.build_systems.perl import PerlPackage
 
 from spack.package import *
 
@@ -13,8 +13,10 @@ class PerlTryTinyRetry(PerlPackage):
     homepage = "https://metacpan.org/pod/Try::Tiny::Retry"
     url = "https://cpan.metacpan.org/authors/id/D/DA/DAGOLDEN/Try-Tiny-Retry-0.004.tar.gz"
 
-    license("Apache-2.0", checked_by="caelanjmiller")
+    maintainers("caelanjmiller")
 
+    license("Apache-2.0", checked_by="caelanjmiller")
+    
     version("0.004", sha256="8af70c1bd46d749388738b7ea03c703cac898be939a5ff37117a01ed7eda8bbe")
     version("0.003", sha256="6367bf56aa129b7b240b330bd106941fc8c3c22b85fd77ca106aa2778b441e32")
     version("0.002", sha256="4f6dd3addcab7dd2726f450cecaaadb21b33da59bec5df58f07fd91a87d9c38a")
