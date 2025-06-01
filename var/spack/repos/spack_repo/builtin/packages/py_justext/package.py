ba25846d6291ae768835ce7e5bcc253618854b90
Thomas Bouvier <contact@thomas-bouvier.io>
2025-06-01 14:04:23 +0200

    fix: fix package url

--- var/spack/repos/spack_repo/builtin/packages/py_justext/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_justext/package.py
@@ -11,7 +11,7 @@ class PyJustext(PythonPackage):
     """Heuristic based boilerplate removal tool."""
 
     homepage = "https://github.com/miso-belica/jusText"
-    pypi = "jusText/jusText-3.0.2.tar.gz"
+    pypi = "justext/justext-3.0.2.tar.gz"
 
     version("3.0.2", sha256="13496a450c44c4cd5b5a75a5efcd9996066d2a189794ea99a49949685a0beb05")
 
