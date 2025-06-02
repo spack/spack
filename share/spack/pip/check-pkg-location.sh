afcae83c1aa2dc840294c3a7e898c9065061a739
Matthew L. Curry <mlcurry@sandia.gov>
2025-06-02 09:14:31 -0600

    Update path

--- share/spack/pip/check-pkg-location.sh
+++ share/spack/pip/check-pkg-location.sh
@@ -6,7 +6,7 @@
 
 # This script checks that Spack packages are installed in ~/.local.
 
-NPKGS="$(../../../bin/spack find -p | grep zlib | grep -Fo "$HOME/.local" | wc -l)"
+NPKGS="$(spack find -p | grep zlib | grep -Fo "$HOME/.local" | wc -l)"
 if ! [[ "$NPKGS" == 1 ]]; then
     echo "Expected package not found in $HOME/.local"
     exit 1
