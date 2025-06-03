c3b1f3cec726d53f8a8dde42b3cc19acda3396c9
Chris Marsh <chrismarsh.c2@gmail.com>
2025-06-03 16:24:42 +0000

    add a few spaces and reorder constraints. bound qt at paraview < 6

--- var/spack/repos/spack_repo/builtin/packages/paraview/package.py
+++ var/spack/repos/spack_repo/builtin/packages/paraview/package.py
@@ -236,15 +236,15 @@ class Paraview(CMakePackage, CudaPackage, ROCmPackage):
     depends_on("mpi", when="+mpi")
     conflicts("mpi", when="~mpi")
 
-    depends_on("qt@:4", when="@:5.2.0+qt")
+    depends_on("qt@:4", when="@:5.2.0 +qt")
     depends_on("qt+sql", when="+qt")
 
     # https://discourse.paraview.org/t/paraview-5-9-and-minimum-recommended-qt-version/5333
-    depends_on("qt@5.12:5 +sql", when="+qt @5.9:")
+    depends_on("qt@5.12:5 +sql", when="@5.9:5.13 +qt")
 
     with when("+qt"):
-        depends_on("qt+opengl", when="@5.3.0:+opengl2")
-        depends_on("qt~opengl", when="@5.3.0:~opengl2")
+        depends_on("qt+opengl", when="@5.3.0: +opengl2")
+        depends_on("qt~opengl", when="@5.3.0: ~opengl2")
 
     depends_on("gl@3.2:", when="+opengl2")
     depends_on("gl@1.2:", when="~opengl2")
