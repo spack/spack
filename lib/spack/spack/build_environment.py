bef502e44a5e96604b336e1f406d73bb16328d9d
Kathleen Shea <shea9@llnl.gov>
2025-06-04 11:06:19 -0700

    wip

--- lib/spack/spack/build_environment.py
+++ lib/spack/spack/build_environment.py
@@ -1311,7 +1311,6 @@ def start_build_process(
         if sys.platform != "win32" and sys.stdin.isatty() and hasattr(sys.stdin, "fileno"):
             input_fd = Connection(os.dup(sys.stdin.fileno()))
         mflags = os.environ.get("MAKEFLAGS")
-        print("mflags in build environment", mflags)
         if mflags is not None:
             m = re.search(r"--jobserver-[^=]*=(\d),(\d)", mflags)
             if m:
