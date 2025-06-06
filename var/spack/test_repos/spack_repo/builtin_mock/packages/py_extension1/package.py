5fa4878fea60be698e74d610b116ebb77a047cdf
Peter Scheibel <scheibel1@llnl.gov>
2025-06-06 11:33:20 -0700

    style (import) fix

--- var/spack/test_repos/spack_repo/builtin_mock/packages/py_extension1/package.py
+++ var/spack/test_repos/spack_repo/builtin_mock/packages/py_extension1/package.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 import os
 
-from spack_repo.builtin_mock.build_systems.python import PythonExtension
 from spack_repo.builtin_mock.build_systems.generic import Package
+from spack_repo.builtin_mock.build_systems.python import PythonExtension
 
 from spack.package import *
 
