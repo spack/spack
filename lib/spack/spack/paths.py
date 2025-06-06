4ec27c4ecb56e4fba262d7240296bd410f74a580
Peter Scheibel <scheibel1@llnl.gov>
2025-06-06 10:33:30 -0700

    update comment

--- lib/spack/spack/paths.py
+++ lib/spack/spack/paths.py
@@ -65,7 +65,9 @@ class Location_vars(Enum):
 
 
 # This is for tests that want to clean the environment of XDG_ variables that
-# affect spack behavior (and the corresponding SPACK_ overrides)
+# affect spack behavior (and the corresponding SPACK_ overrides). Note that
+# these vars will affect .default_test_path for the running instance, but
+# the unit tests will not see the env vars
 def _unset_xdg_vars(env):
     saved = {}
     for xdg_var in itertools.chain(XDG_vars, XDG_overrides):
