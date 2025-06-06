a0ed2f07d595d777f4e6f3d23fc296c1b5cca2c4
Bram Veenboer <bram.veenboer@gmail.com>
2025-06-06 07:59:23 +0200

    Remove unneeded suffix from homepage

--- var/spack/repos/spack_repo/builtin/packages/py_pyclibrary/package.py
+++ var/spack/repos/spack_repo/builtin/packages/py_pyclibrary/package.py
@@ -10,7 +10,7 @@ from spack.package import *
 class PyPyclibrary(PythonPackage):
     """C parser and bindings automation for Python."""
 
-    homepage = "https://pyclibrary.readthedocs.io/en/latest/"
+    homepage = "https://pyclibrary.readthedocs.io"
     pypi = "pyclibrary/pyclibrary-0.2.2.tar.gz"
 
     version("0.2.2", sha256="9902fffe361bb86f57ab62aa4195ec4dd382b63c5c6892be6d9784ec0a3575f7")
