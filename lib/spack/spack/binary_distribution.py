01e1b242538682b735a8f871d26244afdf232eff
psakiev <psakiev@sandia.gov>
2025-06-08 09:05:55 -0600

    Rebuild the index not the contents

--- lib/spack/spack/binary_distribution.py
+++ lib/spack/spack/binary_distribution.py
@@ -193,10 +193,16 @@ class BinaryCacheIndex:
         self._mirrors_for_spec: Dict[str, List[MirrorForSpec]] = {}
 
     def _init_local_index_cache(self):
-        cache_key = self._index_contents_key
-        exists = self._index_file_cache.init_entry(cache_key)
+        # seems logical but fails bootstrapping
+        # cache_key = self._index_contents_key
+        # exists = self._index_file_cache.init_entry(cache_key)
+        # cache_path = self._index_file_cache.cache_path(cache_key)
+        # if not exists and self._index_file_cache_initialized:
+        # raise FileNotFoundError(f"Missing {cache_path}")
 
-        if not self._index_file_cache_initialized or not exists:
+        if not self._index_file_cache_initialized:
+            cache_key = self._index_contents_key
+            self._index_file_cache.init_entry(cache_key)
             cache_path = self._index_file_cache.cache_path(cache_key)
 
             self._local_index_cache = {}
@@ -255,6 +261,11 @@ class BinaryCacheIndex:
             try:
                 cache_exists = self._index_file_cache.init_entry(cache_key)
                 cache_path = self._index_file_cache.cache_path(cache_key)
+                if not cache_exists:
+                    # recreate index if it is missing
+                    cache_entry = self._local_index_cache[cache_key]
+                    self._fetch_and_cache_index(url_and_version, cache_entry)
+                    cache_exists = self._index_file_cache.init_entry(cache_key)
                 if cache_exists:
                     with self._index_file_cache.read_transaction(cache_key):
                         db._read_from_file(cache_path)
