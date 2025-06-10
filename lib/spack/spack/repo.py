7874310fe69f9a2c1d51a0bd09c6f7f24e4b7235
Alec Scott <scott112@llnl.gov>
2025-06-10 13:28:11 +0200

    Use working_dir create instead of mkdirp

--- lib/spack/spack/repo.py
+++ lib/spack/spack/repo.py
@@ -1664,8 +1664,7 @@ class RemoteRepoDescriptor(RepoDescriptor):
     ) -> None:
         with self.write_transaction:
             try:
-                fs.mkdirp(self.destination)
-                with fs.working_dir(self.destination):
+                with fs.working_dir(self.destination, create=True):
                     # do not fetch if the package repository was fetched by another
                     # process while we were waiting for the lock
                     fetched = self._fetched()
@@ -1959,6 +1958,7 @@ def create_and_enable(config: spack.config.Configuration) -> RepoPath:
 PATH: RepoPath = llnl.util.lang.Singleton(
     lambda: create_and_enable(spack.config.CONFIG)
 )  # type: ignore[assignment]
+
 
 # Add the finder to sys.meta_path
 REPOS_FINDER = ReposFinder()
