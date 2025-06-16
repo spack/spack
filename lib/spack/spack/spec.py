3e83305ea09d91f3ebeb582cede169167110f57b
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-06-16 15:23:31 +0200

    Spec: avoid format function with growth O(paths)
    Spec._format_dependencies recurse for transitive deps,
    and follows every path. The time to run this function
    thus increases exponentially for concrete specs.
    
    We should avoid that.
    
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -2160,9 +2160,13 @@ class Spec:
         """
         include = include or (lambda dep: True)
         parts = []
-        direct, transitive = lang.stable_partition(
-            self.edges_to_dependencies(), predicate_fn=lambda x: x.direct
-        )
+        if self.concrete:
+            direct = self.edges_to_dependencies()
+            transitive: List[DependencySpec] = []
+        else:
+            direct, transitive = lang.stable_partition(
+                self.edges_to_dependencies(), predicate_fn=lambda x: x.direct
+            )
 
         # helper for direct and transitive loops below
         def format_edge(edge, sigil, dep_spec=None):
@@ -2194,6 +2198,11 @@ class Spec:
                 parts.append(format_edge(edge, "%", edge.spec))
             finally:
                 edge.spec.name = old_name
+
+        if self.concrete:
+            # Concrete specs should go no further, as the complexity
+            # below is O(paths)
+            return " ".join(parts).strip()
 
         # transitive dependencies (with any direct dependencies)
         for edge in sorted(transitive, key=lambda x: x.spec.name):
