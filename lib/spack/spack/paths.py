589ad184e36191aec6584fcae7e6ba7f53cac4c7
Harmen Stoppels <harmenstoppels@gmail.com>
2025-06-20 09:14:09 +0200

    misc cache: restore old defaults

--- lib/spack/spack/paths.py
+++ lib/spack/spack/paths.py
@@ -115,7 +115,7 @@ package_repos_path = os.path.join(user_cache_path, "package_repos")
 default_user_bootstrap_path = os.path.join(user_cache_path, "bootstrap")
 
 #: transient caches for Spack data (virtual cache, patch sha256 lookup, etc.)
-default_misc_cache_path = os.path.join(user_cache_path, spack_instance_id, "cache")
+default_misc_cache_path = os.path.join(user_cache_path, "cache")
 
 #: concretization cache for Spack concretizations
 default_conc_cache_path = os.path.join(default_misc_cache_path, "concretization")
