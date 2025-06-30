68d4595cef8081eca0d0aec6ad3571d647e8a34e
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-06-30 11:12:14 +0200

    Add back UnsupportedCompilerFlag, and deprecate it
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/compilers/error.py
+++ lib/spack/spack/compilers/error.py
@@ -1,6 +1,8 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
+import warnings
+
 from ..error import SpackError
 
 
@@ -9,4 +11,17 @@ class CompilerAccessError(SpackError):
         super().__init__(
             f"Compiler '{compiler.spec}' has executables that are missing"
             f" or are not executable: {paths}"
+        )
+
+
+class UnsupportedCompilerFlag(SpackError):
+    """Raised when a compiler does not support a flag type (e.g. a flag to enforce a
+    language standard).
+    """
+
+    def __init__(self, message, long_message=None):
+        warnings.warn(
+            "UnsupportedCompilerFlag is deprecated, use CompilerError instead",
+            DeprecationWarning,
+            stacklevel=2,
         )
