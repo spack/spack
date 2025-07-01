31ffa6d5ef011464759cfe59e17d1901e2debd98
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-07-01 15:48:17 +0200

    solver: fix issues with identical externals (modulo compiler)
    fixes #50955
    
    Externals are coded with a pair of "trigger conditions"
    and "imposed conditions".
    
    In this case the trigger conditions were the same for
    both mpich:
    ```
    condition_requirement(22,"node","mpich").
    condition_requirement(22,"node_version_satisfies","mpich","=4.3.0").
    ```
    but the imposed conditions were conflicting.
    
    To fix the issue, relax the set of rules and allow clingo
    to not impose an external condition.
    
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/test/concretization/requirements.py
+++ lib/spack/spack/test/concretization/requirements.py
@@ -1344,3 +1344,32 @@ def test_preferring_compilers_can_be_overridden(mutable_config, mock_packages):
 
     assert concrete.satisfies("%c=gcc")
     assert concrete["pkg-b"].satisfies("%c=llvm")
+
+
+@pytest.mark.regression("50955")
+def test_multiple_externals_and_requirement(concretize_scope, mock_packages):
+    """Tests that we can concretize a required virtual, when we have multiple externals specs for
+    it, differing only by the compiler.
+    """
+    packages_yaml = """
+packages:
+  c:
+    require: gcc
+  mpi:
+    require: mpich
+  mpich:
+    buildable: false
+    externals:
+    - spec: "mpich@4.3.0 %gcc"
+      prefix: /opt/somewhere
+    - spec: "mpich@4.3.0 %clang"
+      prefix: /opt/somewhere/else
+"""
+    update_packages_config(packages_yaml)
+
+    s = spack.spec.Spec("mpileaks")
+    concrete = spack.concretize.concretize_one(s)
+
+    assert concrete.satisfies("%gcc")
+    assert concrete["mpi"].satisfies("mpich@4.3.0")
+    assert concrete["mpi"].prefix == "/opt/somewhere"
