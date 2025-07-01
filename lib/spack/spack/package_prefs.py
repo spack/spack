05413f26c0d877108dfb9209033caf7f938e99f5
psakiev <psakiev@sandia.gov>
2025-07-01 12:15:15 -0600

    Revert package pref change

--- lib/spack/spack/package_prefs.py
+++ lib/spack/spack/package_prefs.py
@@ -149,9 +149,10 @@ class PackagePrefs:
         # Only return variants that are actually supported by the package
         pkg_cls = spack.repo.PATH.get_pkg_class(pkg_name)
         spec = spack.spec.Spec(f"{pkg_name} {variants}")
-        allowed_variants = [*pkg_cls.variant_names(), "commit"]
         return {
-            name: variant for name, variant in spec.variants.items() if name in allowed_variants
+            name: variant
+            for name, variant in spec.variants.items()
+            if name in pkg_cls.variant_names()
         }
 
 
