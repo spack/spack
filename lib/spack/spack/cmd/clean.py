7f81f0698447f5291b412d158f327db951982b29
Peter Scheibel <scheibel1@llnl.gov>
2025-07-01 20:56:49 -0700

    add clean option to keep develop stages

--- lib/spack/spack/cmd/clean.py
+++ lib/spack/spack/cmd/clean.py
@@ -35,6 +35,9 @@ def setup_parser(subparser: argparse.ArgumentParser) -> None:
         "-s", "--stage", action="store_true", help="remove all temporary build stages (default)"
     )
     subparser.add_argument(
+        "--non-dev-stages", action="store_true", help="remove most temporary build stages"
+    )
+    subparser.add_argument(
         "-d", "--downloads", action="store_true", help="remove cached downloads"
     )
     subparser.add_argument(
@@ -92,6 +95,7 @@ def clean(parser, args):
         [
             args.specs,
             args.stage,
+            args.non_dev_stages,
             args.downloads,
             args.failures,
             args.misc_cache,
@@ -114,6 +118,10 @@ def clean(parser, args):
     if args.stage:
         tty.msg("Removing all temporary build stages")
         spack.stage.purge()
+
+    if args.non_dev_stages:
+        tty.msg("Removing all non-develop build stages")
+        spack.stage.purge(keep_dev_stages_in_use=True)
 
     if args.downloads:
         tty.msg("Removing cached downloads")
