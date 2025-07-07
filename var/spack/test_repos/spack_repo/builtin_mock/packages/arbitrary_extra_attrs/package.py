3f5ee605c3c9679a9d47425ab90b4bf8531c364f
Peter Scheibel <scheibel1@llnl.gov>
2025-07-07 15:31:00 -0700

    simplify test package

--- var/spack/test_repos/spack_repo/builtin_mock/packages/arbitrary_extra_attrs/package.py
+++ var/spack/test_repos/spack_repo/builtin_mock/packages/arbitrary_extra_attrs/package.py
@@ -1,12 +1,12 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-from spack_repo.builtin_mock.build_systems import autotools
+from spack_repo.builtin_mock.build_systems.generic import Package
 
 from spack.package import *
 
 
-class ArbitraryExtraAttrs(autotools.AutotoolsPackage):
+class ArbitraryExtraAttrs(Package):
     """Simple package with one optional dependency"""
 
     url = "http://www.example.com/a-1.0.tar.gz"
@@ -20,17 +20,3 @@ class ArbitraryExtraAttrs(autotools.AutotoolsPackage):
     def check(self):
         exampledict = self.spec.extra_attributes["exampledict"]
         assert exampledict == {"a": 1, "b": "2"}
-
-class AutotoolsBuilder(autotools.AutotoolsBuilder):
-    def autoreconf(self, pkg, spec, prefix):
-        pass
-
-    def configure(self, pkg, spec, prefix):
-        pass
-
-    def build(self, pkg, spec, prefix):
-        exampledict = spec.extra_attributes["exampledict"]
-        assert exampledict == {"a": 1, "b": "2"}
-
-    def install(self, pkg, spec, prefix):
-        mkdirp(prefix.bin)
