00a99f54ccf736217992b6ede9581a24644491e7
Alec Scott <scott112@llnl.gov>
2025-07-28 11:40:48 -0700

    Update logo and header title
    Signed-off-by: Alec Scott <scott112@llnl.gov>

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -309,14 +309,19 @@ html_theme = "sphinxawesome_theme"
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = project
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_spack_root/share/spack/logo/spack-logo-text.svg"
+# html_logo = "_spack_root/share/spack/logo/spack-logo-text.svg"
+
+html_theme_options = {
+    "logo_light": "_spack_root/share/spack/logo/spack-logo.svg",
+    "logo_dark": "_spack_root/share/spack/logo/spack-logo-white.svg",
+}
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
