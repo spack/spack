98c0fc7f5645113e5fef6c4549658fb6c429cbc5
Harmen Stoppels <me@harmenstoppels.nl>
2025-07-28 15:15:16 +0200

    docs: try furo theme
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -300,12 +300,7 @@ pygments_style = "style.SpackStyle"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {"logo_only": True}
+html_theme = "furo"
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = ["_themes"]
@@ -319,7 +314,7 @@ html_theme_options = {"logo_only": True}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_spack_root/share/spack/logo/spack-logo-white-text.svg"
+html_logo = "_spack_root/share/spack/logo/spack-logo-text.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
