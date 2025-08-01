1bbb2ae0ab82c9382492d0f323749e41262fe92c
Richard Berger <rberger@lanl.gov>
2025-08-01 10:56:18 -0600

    allow vars in some package_attributes overrides
    this adds variable expansion to the git and url properties
    
    Signed-off-by: Richard Berger <rberger@lanl.gov>

--- lib/spack/spack/repo.py
+++ lib/spack/spack/repo.py
@@ -731,8 +731,15 @@ class RepoPath:
     @staticmethod
     def from_config(config: spack.config.Configuration) -> "RepoPath":
         """Create a RepoPath from a configuration object."""
+
+        def replace_vars_in_package_attrs(attrs):
+            for a in ("git", "url"):
+                if a in attrs:
+                    attrs[a] = spack.util.path.substitute_path_variables(attrs[a])
+            return attrs
+
         overrides = {
-            pkg_name: data["package_attributes"]
+            pkg_name: replace_vars_in_package_attrs(data["package_attributes"])
             for pkg_name, data in config.get_config("packages").items()
             if pkg_name != "all" and "package_attributes" in data
         }
