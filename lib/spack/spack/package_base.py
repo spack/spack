4639e151e15035a4e3a580b73afad3e946803d1c
Tim Fuller <tjfulle@sandia.gov>
2025-08-04 15:03:32 -0600

    Update lib/spack/spack/package_base.py
    Co-authored-by: Phil Sakievich <psakiev@sandia.gov>
    Signed-off-by: Tim Fuller <tjfulle@sandia.gov>

--- lib/spack/spack/package_base.py
+++ lib/spack/spack/package_base.py
@@ -1397,6 +1397,7 @@ class PackageBase(WindowsRPath, PackageViewMixin, metaclass=PackageMeta):
         if not self.spec.versions.concrete:
             raise ValueError("Cannot retrieve fetcher for package without concrete version.")
         if not self._fetcher:
+            # assign private member with the public setter api for error checking
             self.fetcher = fs.for_package_version(self)
         return self._fetcher
 
