bdcf9b602c631d43977af3cf802de5e9d0bad788
Gregory Becker <becker33@llnl.gov>
2025-08-19 06:32:27 -0700

    tests for error cases
    Signed-off-by: Gregory Becker <becker33@llnl.gov>

--- lib/spack/spack/test/cmd/develop.py
+++ lib/spack/spack/test/cmd/develop.py
@@ -139,6 +139,28 @@ class TestDevelop:
             spec = next(e.roots())
             assert spec.satisfies("dev_path=*")
 
+    def test_develop_applies_changes_spec_conflict(self, monkeypatch):
+        env("create", "test")
+        with ev.read("test") as e:
+            e.add("mpich@1.0")
+            e.concretize()
+            e.write()
+
+            monkeypatch.setattr(spack.stage.Stage, "steal_source", lambda x, y: None)
+            with pytest.raises(ev.SpackEnvironmentDevelopError, match="conflicts with concrete"):
+                develop("mpich@1.1")
+
+    def test_develop_applies_changes_path_conflict(self, monkeypatch):
+        env("create", "test")
+        with ev.read("test") as e:
+            e.add("mpich@1.0 dev_path=foobar")
+            e.concretize()
+            e.write()
+
+            monkeypatch.setattr(spack.stage.Stage, "steal_source", lambda x, y: None)
+            with pytest.raises(ev.SpackEnvironmentDevelopError, match="Conflict with provided"):
+                develop("mpich")
+
     def test_develop_no_apply_changes(self, monkeypatch):
         env("create", "test")
         with ev.read("test") as e:
