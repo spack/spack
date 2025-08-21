58e6b6eb2cc153976522a903cecf7353841d2242
psakiev <psakiev@sandia.gov>
2025-08-20 18:44:23 -0600

    Fix monkeypatch calling method
    Signed-off-by: psakiev <psakiev@sandia.gov>

--- lib/spack/spack/test/concretization/core.py
+++ lib/spack/spack/test/concretization/core.py
@@ -4079,7 +4079,9 @@ def test_commit_variant_enters_the_hash(mutable_config, mock_packages, monkeypat
 
         spec.variants["commit"] = spack.variant.SingleValuedVariant("commit", f"{'a' * 40}")
 
-    monkeypatch.setattr(spack.package_base.PackageBase, "resolve_binary_provenance", _mock_resolve)
+    monkeypatch.setattr(
+        spack.package_base.PackageBase, "_resolve_binary_provenance", _mock_resolve
+    )
 
     before = spack.concretize.concretize_one("git-ref-package@develop")
     first_call = False
