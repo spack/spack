7accd42e4a2d8a5349aa39030f6899b2b2dcebe0
Ryan Krattiger <ryan.krattiger@kitware.com>
2025-09-26 10:59:55 -0500

    Add a spack-rebase git command to handle rebasing on deferred pipelines
    Signed-off-by: Ryan Krattiger <ryan.krattiger@kitware.com>

--- share/spack/setup-dev.sh
+++ share/spack/setup-dev.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Setup spack
+source share/spack/setup-env.sh
+
 # Install git hooks for spack git workflow
 cp share/spack/git/hooks/* .git/hooks/
+
+# Add the spack git commands to the path
+_spack_pathadd PATH share/spack/git/
