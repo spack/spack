f3604cf22ff7a5a2985ceef58ecd6cfe110470ec
Harmen Stoppels <me@harmenstoppels.nl>
2025-10-01 08:02:30 +0200

    docs: remove authors from pdf
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/docs/conf.py
+++ lib/spack/docs/conf.py
@@ -511,7 +511,7 @@ latex_elements = {
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [("index", "Spack.tex", "Spack Documentation", "Todd Gamblin", "manual")]
+latex_documents = [("index", "Spack.tex", "Spack Documentation", "", "manual")]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
