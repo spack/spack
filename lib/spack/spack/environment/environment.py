7ad6e189c7e6e98b25dab684b29a21820ee481d7
Gregory Becker <becker33@llnl.gov>
2025-10-31 16:51:36 -0700

    refactor generalized mutator to use Spec.mutate method
    Signed-off-by: Gregory Becker <becker33@llnl.gov>

--- lib/spack/spack/environment/environment.py
+++ lib/spack/spack/environment/environment.py
@@ -12,7 +12,7 @@ import re
 import shutil
 import stat
 import warnings
-from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, Iterable, List, Optional, Sequence, Tuple, Union
 
 import spack
 import spack.concretize
@@ -1468,29 +1468,30 @@ class Environment:
         """Mutate concrete specs to include dev_path provenance pointing to path.
 
         This will fail if any existing concrete spec for the same package does not satisfy the
+
         given develop spec."""
-        def apply_dev_path(spec):
-            # Remove any existing dev_path variant in all cases
-            # If setting a path, add the new variant
-            spec.variants.pop("dev_path", None)
-            if not path:
-                return
-            spec.variants["dev_path"] = vt.VariantValue(vt.VariantType.SINGLE, "dev_path", (path,))
+        selector = spack.spec.Spec(spec.name)
+
+        mutator = spack.spec.Spec()
+        if path:
+            variant = vt.VariantValue(vt.VariantType.SINGLE, "dev_path", (path,))
+        else:
+            variant = None
+        mutator.variants["dev_path"] = variant
 
         msg = (
             f"Develop spec '{spec}' conflicts with concrete specs in environment."
             " Try again with 'spack develop --no-modify-concrete-specs'"
             " and run 'spack concretize --force' to apply your changes."
         )
-        selector = spack.spec.Spec(spec.name)
-        self.mutate(selector=selector, mutator=apply_dev_path, validator=spec, msg=msg)
+        self.mutate(selector, mutator, validator=spec, msg=msg)
 
     def mutate(
         self,
         selector: spack.spec.Spec,
-        mutator: Callable,
+        mutator: spack.spec.Spec,
         validator: Optional[spack.spec.Spec] = None,
-        msg: Optional[str] = None
+        msg: Optional[str] = None,
     ):
         """Mutate concrete specs of an environment
 
@@ -1500,6 +1501,7 @@ class Environment:
         """
         # Find all specs that this mutation applies to
         modify_specs = []
+        modified_specs = []
         for dep in self.all_specs_generator():
             if dep.satisfies(selector):
                 if not dep.satisfies(validator or selector):
@@ -1511,11 +1513,13 @@ class Environment:
 
         # Manipulate selected specs
         for s in modify_specs:
-            mutator(s)
+            modified = s.mutate(mutator, rehash=False)
+            if modified:
+                modified_specs.append(s)
 
         # Identify roots modified and invalidate all dependent hashes
         modified_roots = []
-        for parent in traverse.traverse_nodes(modify_specs, direction="parents"):
+        for parent in traverse.traverse_nodes(modified_specs, direction="parents"):
             # record whether this parent is a root before we modify the hash
             if parent.dag_hash() in self.specs_by_hash:
                 modified_roots.append((parent, parent.dag_hash()))
--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -4856,6 +4856,85 @@ class Spec:
 
         return spec
 
+    def mutate(self, mutator, rehash=True) -> bool:
+        """Mutate concrete spec to match constraints represented by mutator.
+
+        Mutation can modify the spec version, variants, compiler flags, and architecture.
+        Mutation cannot change the spec name, namespace, dependencies, or abstract_hash.
+        Any attribute which is unset will not be touched.
+        Variant values can be replaced with the literal ``None`` to remove the variant.
+        ``None`` as a variant value is represented by ``VariantValue(..., (None,))``.
+
+        If ``rehash``, concrete spec and its dependents have hashes updated.
+
+        Returns whether the spec was modified by the mutation"""
+        assert self.concrete
+
+        if mutator.name and mutator.name != self.name:
+            raise SpecMutationError(f"Cannot mutate spec name: spec {self} mutator {mutator}")
+
+        if mutator.namespace and mutator.namespace != self.namespace:
+            raise SpecMutationError(f"Cannot mutate spec namespace: spec {self} mutator {mutator}")
+
+        if len(mutator.dependencies()) > 0:
+            raise SpecMutationError(f"Cannot mutate dependencies: spec {self} mutator {mutator}")
+
+        if mutator.versions != vn.VersionList(":") and not mutator.versions.concrete:
+            raise SpecMutationError(
+                f"Cannot mutate abstract version: spec {self} mutator {mutator}"
+            )
+
+        if mutator.abstract_hash and mutator.abstract_hash != self.abstract_hash:
+            raise SpecMutationError(f"Cannot mutate abstract_hash: spec {self} mutator {mutator}")
+
+        changed = False
+
+        if mutator.versions != vn.VersionList(":") and self.versions != mutator.versions:
+            self.versions = mutator.versions
+            changed = True
+
+        for name, variant in mutator.variants.items():
+            if variant == self.variants.get(name, None):
+                continue
+
+            # None instead of VariantValue(..., (None,)) used as a sigil to remove variant
+            self.variants.pop(name, None)
+            if variant is not None:
+                self.variants[name] = variant
+            changed = True
+
+        for name, flags in mutator.compiler_flags:
+            if not flags or flags == self.compiler_flags[name]:
+                continue
+            self.compiler_flags[name] = flags
+            changed = True
+
+        if mutator.architecture:
+            if mutator.platform and mutator.platform != self.architecture.platform:
+                self.architecture.platform = mutator.platform
+                changed = True
+            if mutator.os and mutator.os != self.architecture.os:
+                self.architecture.os = mutator.os
+                changed = True
+            if mutator.target and mutator.target != self.architecture.target:
+                self.architecture.target = mutator.target
+                changed = True
+
+        if changed and rehash:
+            roots = []
+            for parent in spack.traverse.traverse_nodes([self], direction="parents"):
+                if not parent.dependents():
+                    roots.append(parent)
+                # invalidate hashes
+                parent._mark_root_concrete(False)
+                parent.clear_caches()
+
+            for root in roots:
+                # compute new hashes on full DAGs
+                root._finalize_concretization()
+
+        return changed
+
     def clear_caches(self, ignore: Tuple[str, ...] = ()) -> None:
         """
         Clears all cached hashes in a Spec, while preserving other properties.
@@ -5724,3 +5803,7 @@ class SpliceError(spack.error.SpecError):
 
 class InvalidEdgeError(spack.error.SpecError):
     """Raised when an edge doesn't pass validation checks."""
+
+
+class SpecMutationError(spack.error.SpecError):
+    """Raised when a mutation is attempted with invalid attributes."""
