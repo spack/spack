56cae6cc209476406639ce5f4d3a647031c3e89f
Guillaume Peillex <guillaume.peillex@gmail.com>
2025-11-01 12:04:49 +0100

    spack compiler list command now outputs the aliased compilers along with builtin ones
    Signed-off-by: Guillaume Peillex <guillaume.peillex@gmail.com>

--- lib/spack/spack/cmd/compiler.py
+++ lib/spack/spack/cmd/compiler.py
@@ -12,7 +12,7 @@ import spack.config
 import spack.llnl.util.tty as tty
 import spack.spec
 import spack.store
-from spack.aliases import LEGACY_COMPILER_TO_BUILTIN
+from spack.aliases import LEGACY_COMPILER_TO_BUILTIN, BUILTIN_TO_LEGACY_COMPILER
 from spack.cmd.common import arguments
 from spack.llnl.util.lang import index_by
 from spack.llnl.util.tty.colify import colify
@@ -137,7 +137,7 @@ def compiler_remove(args):
 def compiler_info(args):
     """Print info about all compilers matching a spec."""
     query = spack.spec.Spec(args.compiler_spec)
-    # Replace legacy compiler name by its builtin name if available
+    #  Replace legacy compiler name by its builtin name if available
     query.name = LEGACY_COMPILER_TO_BUILTIN.get(query.name, query.name)
     all_compilers = spack.compilers.config.all_compilers(scope=args.scope, init_config=False)
 
@@ -182,9 +182,19 @@ def compiler_list(args):
     def _is_compiler(x):
         return x.name in supported_compilers and x.package.supported_languages and not x.external
 
+    def _is_aliased(x):
+        return x.name in BUILTIN_TO_LEGACY_COMPILER
+
+    def _build_alias_spec(x):
+        alias_spec = x.copy(deps=False)
+        alias_spec.name = BUILTIN_TO_LEGACY_COMPILER[x.name]
+        return alias_spec
+
     compilers_from_store = [x for x in spack.store.STORE.db.query() if _is_compiler(x)]
     compilers_from_yaml = spack.compilers.config.all_compilers(scope=args.scope, init_config=False)
     compilers = compilers_from_yaml + compilers_from_store
+    aliases_compilers = [_build_alias_spec(x) for x in compilers if _is_aliased(x)]
+    compilers += aliases_compilers
 
     if args.remote:
         compilers.extend(
@@ -211,7 +221,7 @@ def compiler_list(args):
     # convert them to '' (in which case it still evaluates to False but is a
     # string type). Tuples produced by this are guaranteed to be comparable in
     # Python 3
-    convert_str = lambda tuple_container: tuple(str(x) if x else "" for x in tuple_container)
+    def convert_str(tuple_container): return tuple(str(x) if x else "" for x in tuple_container)
 
     index_str_keys = list((convert_str(x), y) for x, y in index.items())
     ordered_sections = sorted(index_str_keys, key=lambda item: item[0])
