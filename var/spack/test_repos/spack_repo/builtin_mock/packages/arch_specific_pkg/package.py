090826dad505b37bb35bd2b7f606a06dfd6bbf07
Angelica Loshak <loshak@lanl.gov>
2025-12-16 22:45:03 +0000

    Remove print statements
    Signed-off-by: Angelica Loshak <loshak@lanl.gov>

--- var/spack/test_repos/spack_repo/builtin_mock/packages/arch_specific_pkg/package.py
+++ var/spack/test_repos/spack_repo/builtin_mock/packages/arch_specific_pkg/package.py
@@ -11,8 +11,6 @@ from spack.package import *
 
 class ArchSpecificPkg(Package):
     if spack.mirrors.utils.evaluate_or_true_if_mirror_all(platform.machine() == "x86_64"):
-        print("Adding x86_64 version")
         version("1.0", sha256="a" * 64, url="https://example.com/pkg-1.0-x86_64.tar.gz")
     if spack.mirrors.utils.evaluate_or_true_if_mirror_all(platform.machine() == "aarch64"):
-        print("Adding aarch64 version")
         version("1.0", sha256="b" * 64, url="https://example.com/pkg-1.0-aarch64.tar.gz")
