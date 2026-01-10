0d61e5cdf34496f1e20f61943104860290518f2c
Peter Scheibel <scheibel1@llnl.gov>
2026-01-09 16:25:20 -0800

    update error message

--- lib/spack/spack/trace.py
+++ lib/spack/spack/trace.py
@@ -78,4 +78,6 @@ def warn_writes_into_spack():
     if sys.version_info[:2] >= (3, 8):
         sys.addaudithook(_guard_writes)  # novermin
     else:
-        raise ValueError(f"Cannot detect writes for Python {sys.version_info[:2]}")
+        raise ValueError(
+            f"Cannot detect writes for Python {sys.version_info[:2]} (supported in 3.8 and later)"
+        )
