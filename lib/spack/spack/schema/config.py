d79cef8f0043db8df50e2db08c384c846c60576a
Harmen Stoppels <me@harmenstoppels.nl>
2026-01-14 02:28:48 +0100

    config: relax concurrent_packages to minimum 0 (#51840)
    In the new installer, the number of concurrent packages is bound by
    `build_jobs`, and users don't have to bother setting
    `concurrent_packages` too. As a result, the new installer currently
    ignores `concurrent_packages`.
    
    In Spack v1.2 however, the new installer *will* respect
    `concurrent_packages` as an upper limit on the number of concurrent
    package installs. That makes the default value of `1` in config
    problematic.
    
    The suggestion is to use a default value of `0` from Spack v1.2 onwards,
    and make that a mean "default settings". For the old installer that
    means no package parallelism, for the new installer that means at most
    `build_jobs`.
    
    This commit allows the value `0` in config, and is primarily meant to be
    backported to ensure forward compatibilty with newer config files in
    older Spack version.
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>
    Signed-off-by: Gregory Becker <becker33@llnl.gov>

--- lib/spack/spack/schema/config.py
+++ lib/spack/spack/schema/config.py
@@ -70,7 +70,7 @@ properties: Dict[str, Any] = {
             "dirty": {"type": "boolean"},
             "build_language": {"type": "string"},
             "build_jobs": {"type": "integer", "minimum": 1},
-            "concurrent_packages": {"type": "integer", "minimum": 1},
+            "concurrent_packages": {"type": "integer", "minimum": 0},
             "ccache": {"type": "boolean"},
             "db_lock_timeout": {"type": "integer", "minimum": 1},
             "package_lock_timeout": {
