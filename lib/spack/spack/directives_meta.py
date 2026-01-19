8983e8116d4753222751307354f02b6c0769677f
Harmen Stoppels <me@harmenstoppels.nl>
2026-01-19 19:42:13 +0100

    comment
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/spack/directives_meta.py
+++ lib/spack/spack/directives_meta.py
@@ -112,7 +112,7 @@ class DirectiveMeta(type):
             if isinstance(arg, (list, tuple)):
                 # Descend into args that are lists or tuples
                 DirectiveMeta._remove_directives(arg)
-            elif callable(arg):
+            elif callable(arg):  # directives are always callable, and very rare
                 # Remove directives args from the exec queue
                 for directive in directives:
                     if arg is directive:
