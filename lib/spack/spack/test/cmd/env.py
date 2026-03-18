5251c566dbd08078789e6f938cee07aeab2a6bd3
Peter Josef Scheibel <scheibel1@llnl.gov>
2026-03-17 20:13:58 -0700

    another attempt (still passes)
    Signed-off-by: Peter Josef Scheibel <scheibel1@llnl.gov>

--- lib/spack/spack/test/cmd/env.py
+++ lib/spack/spack/test/cmd/env.py
@@ -657,7 +657,7 @@ def test_compiler_target_env(
 spack:
   specs:
   - pkg-a
-  - libdwarf %gcc@12.100.100
+  - libdwarf
   packages:
     all:
       require:
@@ -673,11 +673,19 @@ spack:
           flags:
             cflags: {cflags}
       require: "gcc"
+    libelf:
+      externals:
+      - spec: libelf@0.8.12 %gcc@12.100.100
+        prefix: /fake2
+      buildable: false
+      require: "libelf"
 """)
         env("create", "test", "spack.yaml")
 
     with ev.read("test") as e:
         e.concretize()
+        x = list(e.concretized_specs())
+        y = x[1][1]
         import pdb; pdb.set_trace()
         print('hi')
 
