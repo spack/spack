spack/spack

commit af1edd44f13ed3fb53c7f647c7e25280f8bc817e

new_installer.py: new process group instead of session (#52192)
In build subprocesses, create a new process group instead of a new session.
The benefit is that tools like pstree neatly show the spack install process
with all its concurrent builds as a tree, making it easier to verify whether
the jobserver is working correctly: -j N <--> N leaf nodes.

Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

lib/spack/spack/new_installer.py
@@ -474,8 +474,9 @@ def worker_function(
 
     global_state.restore()
 
-    # Start a new session, so our SIGTERM handler can kill all child processes.
-    os.setsid()
+    # Isolate the process group to shield against Ctrl+C and enable safe killpg() cleanup. In
+    # constrast to setsid(), this keeps a neat process group hierarchy for utils like pstree.
+    os.setpgid(0, 0)
 
     # Reset SIGTSTP to default in case the parent had a custom handler.
     signal.signal(signal.SIGTSTP, signal.SIG_DFL)

commit 51a5d88a93f7a279c8dfb99d0acaf1f5ea213926

new_installer.py: sub_process < /dev/null
In the Python build subprocess, redirect stdin to /dev/null. This
regressed after #52192 as that change attaches the user's stdin.

Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

lib/spack/spack/new_installer.py
@@ -512,6 +512,13 @@ def worker_function(
         sys.stderr.fileno(), "w", buffering=1, encoding=sys.stderr.encoding, closefd=False
     )
 
+    # Detach stdin from the terminal like `./build < /dev/null`. This would not be necessary if we
+    # used os.setsid() instead of os.setpgid(), but that would "break" pstree output.
+    devnull_fd = os.open(os.devnull, os.O_RDONLY)
+    os.dup2(devnull_fd, 0)
+    os.close(devnull_fd)
+    sys.stdin = open(os.devnull, "r", encoding=sys.stdin.encoding)
+
     # Open the log file created by the parent process.
     log_fd = os.open(log_path, os.O_WRONLY | os.O_TRUNC, 0o644)
     tee = Tee(echo_control, parent, log_fd)