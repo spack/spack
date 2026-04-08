f32d70cd8e9dd6ac07784b6d78c206211af47c3d
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2026-04-08 12:20:24 +0200

    solver: recover error message for flag propagation conflicts
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/solver/error_handler.py
+++ lib/spack/spack/solver/error_handler.py
@@ -216,11 +216,8 @@ class ErrorFormatter:
             f"{source} because package: {pkg} is set to exclude it"
         )
 
-    def flag_propagation_conflict(self, pkg: str, source2: str, flag_type: str) -> str:
-        return (
-            f"{pkg}: cannot propagate compiler flags '{flag_type}' "
-            f"from multiple sources including {source2}"
-        )
+    def flag_propagation_conflict(self, pkg: str, source1: str, source2: str, flag_type: str) -> str:
+        return f"{source1} and {source2} cannot both propagate compiler flags '{flag_type}' to {pkg}"
 
     def compiler_mixing_disabled(self, pkg: str, language: str) -> str:
         return f"Compiler mixing is disabled for the {language} language"
