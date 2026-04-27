b7a19af906d3d7420a3b03b638bf1c332ce255fb
Peter Scheibel <scheibel1@llnl.gov>
2026-04-27 11:08:07 -0700

    omit check on win32 for now
    Signed-off-by: Peter Scheibel <scheibel1@llnl.gov>

--- lib/spack/spack/stage.py
+++ lib/spack/spack/stage.py
@@ -116,16 +116,16 @@ def create_stage_root(path: str) -> None:
     if user_paths:
         mkdirp(user_paths[-1], mode=stat.S_IRWXU)
     for p in user_paths:
-        # Ensure access controls of subdirs from `$user` on down are
-        # restricted to the user.
-        owner_uid = get_owner_uid(p)
-        if user_uid != owner_uid:
-            raise OSError(
-                errno.EACCES,
-                f"Expected user {user_uid} to own {p}, but it is owned by {owner_uid}",
-            )
-
         if not sys.platform == "win32":
+            # Ensure access controls of subdirs from `$user` on down are
+            # restricted to the user.
+            owner_uid = get_owner_uid(p)
+            if user_uid != owner_uid:
+                raise OSError(
+                    errno.EACCES,
+                    f"Expected user {user_uid} to own {p}, but it is owned by {owner_uid}",
+                )
+
             # And only the user should be able to write or read it
             p_stat = os.lstat(p)
             err_prefix = f"Cannot create stage root {path}:"
