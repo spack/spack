95fb0c68692e77b26ecd1a15d5e53df6b6a7be7a
John Parent <john.parent@kitware.com>
2026-05-01 15:51:35 -0400

    try develop
    Signed-off-by: John Parent <john.parent@kitware.com>

--- var/spack/test_repos/spack_repo/integration_test/packages/smoke1/package.py
+++ var/spack/test_repos/spack_repo/integration_test/packages/smoke1/package.py
@@ -10,27 +10,6 @@ from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack.package import *
 
 
-def make_pkg_tarball(version_str):
-    import hashlib
-
-    archive_name = f"smoke1-{version_str}.tgz"
-    package_root = os.path.dirname(__file__)
-    archive = os.path.join(package_root, archive_name)
-    if not os.path.exists(archive):
-        import tarfile
-
-        with tarfile.open(archive, "w|gz") as tgz:
-            tgz.add(os.path.join(package_root, "smoke1-src"), arcname=".")
-    sha256_hash = hashlib.sha256()
-
-    with open(archive, "rb") as f:
-        chunked_data = f.read(4096)
-        while chunked_data:
-            sha256_hash.update(chunked_data)
-            chunked_data = f.read(4096)
-    return sha256_hash.hexdigest()
-
-
 class Smoke1(CMakePackage):
     """Smoke test - compile and run a simple CMake project
     Basic Smoke test - compiles a one file CMake based project into an executable
@@ -47,7 +26,7 @@ class Smoke1(CMakePackage):
 
     license("MIT")
 
-    version("0.1", sha256=make_pkg_tarball("0.1"))
+    version("0.1")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
