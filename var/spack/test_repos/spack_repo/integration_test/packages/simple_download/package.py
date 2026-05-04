55779742f0b99767b19a68bbe0effbd887d9398f
John Parent <john.parent@kitware.com>
2026-05-04 15:47:55 -0400

    style
    Signed-off-by: John Parent <john.parent@kitware.com>

--- var/spack/test_repos/spack_repo/integration_test/packages/simple_download/package.py
+++ var/spack/test_repos/spack_repo/integration_test/packages/simple_download/package.py
@@ -6,16 +6,17 @@ import os
 from urllib.request import pathname2url
 
 from spack_repo.builtin.build_systems.generic import Package
+
 from spack.package import *
 
 
 class SimpleDownload(Package):
-    """Test package to test a simple package install using a 
+    """Test package to test a simple package install using a
     basic URL fetcher
     """
+
     archive_name = "simple_download-0.1.tar.gz"
     url = "file:" + pathname2url(os.path.join(os.path.dirname(__file__), archive_name))
-
 
     version("0.1", sha256="825ff2bf662c7d662f1c4632324b615a7da56ed3c0c624d9a31d1d718fcae648")
 
