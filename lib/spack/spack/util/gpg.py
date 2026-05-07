12df3597efa21b141f7b48b9be3d8aae2f9c31bf
John Parent <john.parent@kitware.com>
2026-05-07 19:26:31 -0400

    Try trust model always
    Signed-off-by: John Parent <john.parent@kitware.com>

--- lib/spack/spack/util/gpg.py
+++ lib/spack/spack/util/gpg.py
@@ -306,7 +306,7 @@ def verify(signature, file=None, suppress_warnings=False):
     if file:
         args.append(file)
     kwargs = {"error": str} if suppress_warnings else {}
-    GPG("--verify", *args, **kwargs)
+    GPG("--verify", "--trust-model", "always", *args, **kwargs)
 
 
 @_autoinit
