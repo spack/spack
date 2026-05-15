fc41f0c3a21c38ee9242da4fb6603457622f23df
Peter Scheibel <scheibel1@llnl.gov>
2026-05-14 20:09:30 -0700

    forgot the part about printing the warning

--- lib/spack/spack/main.py
+++ lib/spack/spack/main.py
@@ -975,6 +975,7 @@ def _warn_about_old_dotspack():
         msg += " - it is not currently used by this spack instance."
     msg += ("\nIf all spack instances are >= 1.2, you can use"
             " `spack migrate --clear` to silence this warning")
+    tty.warn(msg)
 
 
 def _main(argv=None):
