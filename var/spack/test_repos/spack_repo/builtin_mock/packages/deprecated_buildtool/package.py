bee077d807c7b66f259095fb5824e48307e69120
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2026-07-04 08:20:56 +0200

    Address review
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

---
+++ var/spack/test_repos/spack_repo/builtin_mock/packages/deprecated_buildtool/package.py
@@ -0,0 +1,17 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack_repo.builtin_mock.build_systems.generic import Package
+
+from spack.package import *
+
+
+class DeprecatedBuildtool(Package):
+    """A build tool that carries a deprecated runtime dependency"""
+
+    homepage = "http://www.example.com"
+    url = "http://www.example.com/deprecated-buildtool-1.0.tar.gz"
+
+    version("1.0", sha256="abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890")
+
+    depends_on("deprecated-versions@1.1.0", type="run")
