ea9a92356b8faee960d819b0e750abbbb21adb34
Harmen Stoppels <me@harmenstoppels.nl>
2026-07-10 16:10:59 +0200

    ArchSpec: fix to_dict/from_dict for abstract specs
    Serializing an abstract spec with a partial architecture like
    `os=redhat6` or `platform=test` crashes with an error like
    
    ```
    AttributeError: 'NoneType' object has no attribute 'vendor'
    ```
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/spack/test/spec_yaml.py
+++ lib/spack/spack/test/spec_yaml.py
@@ -522,6 +522,18 @@ def test_pickle_roundtrip_for_abstract_specs(spec_str):
     assert str(s) == str(t)
 
 
+@pytest.mark.parametrize(
+    "spec_str", ["zlib os=redhat6", "zlib platform=test", "zlib os=debian6 target=x86_64"]
+)
+def test_dict_roundtrip_for_abstract_specs_with_partial_arch(spec_str):
+    """Abstract specs with a partial architecture survive to_dict/from_dict.
+    Regression test: ArchSpec.to_dict crashed with AttributeError when target was None."""
+    s = spack.spec.Spec(spec_str)
+    t = spack.spec.Spec.from_dict(s.to_dict())
+    assert s == t
+    assert str(s) == str(t)
+
+
 def test_specfile_alias_is_updated():
     """Tests that the SpecfileLatest alias gets updated on a Specfile version bump"""
     specfile_class_name = f"SpecfileV{spack.spec.SPECFILE_FORMAT_VERSION}"
