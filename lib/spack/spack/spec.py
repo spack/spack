9b3b2f3537cf349aa1aa8c96ab5dc62e140f2895
Harmen Stoppels <me@harmenstoppels.nl>
2026-07-23 11:41:14 +0200

    spec: remove finalize branch in _constrain
    Earlier there is
    
    ```
    if other.concrete and other._satisfies(self, resolve_virtuals=resolve_virtuals):
        self._dup(other)
        return True
    ```
    
    and later there is
    
    ```
    if other.concrete and not self.concrete and other.satisfies(self):
        self._finalize_concretization()
    ```
    
    Remove the latter.
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -3104,9 +3104,6 @@ class Spec:
         if deps:
             changed |= self._constrain_dependencies(other, resolve_virtuals=resolve_virtuals)
 
-        if other.concrete and not self.concrete and other.satisfies(self):
-            self._finalize_concretization()
-
         return changed
 
     def _constrain_dependencies(self, other: "Spec", resolve_virtuals: bool = True) -> bool:
