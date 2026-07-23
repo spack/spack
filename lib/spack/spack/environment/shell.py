706309b9d4d7608f217abab12112b18500143346
Kayla Butler <butler59@llnl.gov>
2026-07-23 13:48:13 -0700

    Fix pulled changes
    Signed-off-by: Kayla Butler <butler59@llnl.gov>

--- lib/spack/spack/environment/shell.py
+++ lib/spack/spack/environment/shell.py
@@ -187,10 +187,10 @@ def deactivate(active_env, view) -> EnvironmentModifications:
     """
     env_mods = EnvironmentModifications()
 
-    if active is None:
+    if active_env is None:
         return env_mods
 
-    with active.manifest.use_config():
+    with active_env.manifest.use_config():
         env_vars_yaml = spack.config.CONFIG.get("env_vars", None)
     if env_vars_yaml:
         env_mods.extend(spack.schema.environment.parse(env_vars_yaml).reversed())
