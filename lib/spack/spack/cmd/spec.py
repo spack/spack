e57f91671820c25f25cab2564663fd17391f0420
Kayla Butler <butler59@llnl.gov>
2026-08-12 10:44:25 -0700

    Check for specs in env
    Signed-off-by: Kayla Butler <butler59@llnl.gov>

--- lib/spack/spack/cmd/spec.py
+++ lib/spack/spack/cmd/spec.py
@@ -86,6 +86,9 @@ def spec(parser, args):
     if args.specs:
         concrete_specs = spack.cmd.parse_specs(args.specs, concretize=True)
     elif env:
+        if not list(env.all_user_specs):
+            args.subparser.error("active environment has no root specs, please provide at least one spec")
+
         env.concretize()
         concrete_specs = env.concrete_roots()
     else:
