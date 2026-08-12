spack/spack

commit 5fda204835666cc68592418713ea9992192f4f13

Refactor the GPG module to better support signing/verification workflows (#52430)
The GPG module is central to the verification pipeline used for
build cache binaries. This is important for both bootstrapping and for
install. However, the current architecture for GPG utilizes a shared
global state for all signing and verification which can lead to
incorrect contexts being used.

In order to improve Spack's ability to control the context of GPG, the
GPG util has been refactored to wrap all of the components required to
configuring GPG into a single class `Gpg`. This class owns its own gpg
home, socket, gpgconf, and gpg command wrapper. This model enables
bootstrap and install to create their own configurable instances of gpg
without needing to update global state.

In addition to cleaning up global state, this refactored also
implements a more complete parsing of GPG keys and signature information
allowing for more detailed interactions with GPG keys beyond just
fingerprint in Spack without having to re-consult Gpg to extract
additional information.

This refactor also improves the workflow around trusting keys. The
previous workflow had a TOCTOU where the keys were listed and then
trusted from the keyfile. The list was verified before promoting keys to
ultimate, but could leave unexpected keys in the keyring if the file
changed. The new version automatically untrusts keys
that are new but not found in the initial listing. It also prompts the
user to
verify that each new key should be trusted before finalizing the
ultimate trust step.

---------

Signed-off-by: Ryan Krattiger <ryan.krattiger@kitware.com>

lib/spack/spack/binary_distribution.py
@@ -648,7 +648,7 @@ def select_signing_key() -> str:
             "Use spack gpg init and spack gpg create"
             " to create a default key."
         )
-    return keys[0]
+    return str(keys[0])
 
 
 def _push_index(db: BuildCacheDatabase, temp_dir: str, cache_prefix: str, name: str = ""):
@@ -2325,7 +2325,7 @@ def _get_keys(
         tty.debug("Found key {0}".format(fingerprint))
         if install:
             if trust:
-                spack.util.gpg.trust(key_blob_path)
+                spack.util.gpg.trust(key_blob_path, True)
                 tty.debug(f"Added {fingerprint} to trusted keys.")
                 saved_fingerprints.append(fingerprint)
             else:
@@ -2375,7 +2375,7 @@ def _get_keys_v2(mirror_url, install=False, trust=False, force=False) -> Optiona
         tty.debug("Found key {0}".format(fingerprint))
         if install:
             if trust:
-                spack.util.gpg.trust(stage.save_filename)
+                spack.util.gpg.trust(stage.save_filename, True)
                 tty.debug("Added this key to trusted keys.")
                 saved_fingerprints.append(fingerprint)
             else:

lib/spack/spack/test/binary_distribution.py
@@ -151,7 +151,7 @@ def test_push_and_fetch_keys(mock_gnupghome, tmp_path: pathlib.Path):
 
         keys = spack.util.gpg.public_keys()
         assert len(keys) == 1
-        fpr = keys[0]
+        fpr = str(keys[0])
 
         spack.binary_distribution._url_push_keys(
             mirror, keys=[fpr], tmpdir=str(tmp_path), update_index=True
@@ -166,7 +166,7 @@ def test_push_and_fetch_keys(mock_gnupghome, tmp_path: pathlib.Path):
 
         new_keys = spack.util.gpg.public_keys()
         assert len(new_keys) == 1
-        assert new_keys[0] == fpr
+        assert str(new_keys[0]) == fpr
 
 
 @pytest.mark.maybeslow

lib/spack/spack/ci/__init__.py
@@ -581,7 +581,7 @@ def import_signing_key(base64_signing_key: str) -> None:
         with open(sign_key_path, "w", encoding="utf-8") as fd:
             fd.write(decoded_key)
 
-        key_import_output = spack_gpg("trust", sign_key_path)
+        key_import_output = spack_gpg("trust", "-y", sign_key_path)
         tty.debug(f"spack gpg trust {sign_key_path}")
         tty.debug(key_import_output)
 
@@ -1053,7 +1053,11 @@ def reproduce_ci_job(url, work_dir, autostart, gpg_url, runtime, use_local_head)
                 f"share/spack/setup-env.{platform_script_ext}",
             ),
         ],
-        ["spack", "gpg", "trust", mounted_gpg_path if job_image else gpg_path] if gpg_path else [],
+        (
+            ["spack", "gpg", "trust", "-y", mounted_gpg_path if job_image else gpg_path]
+            if gpg_path
+            else []
+        ),
         ["spack", "env", "activate", mounted_env_dir if job_image else repro_dir],
         [
             (

lib/spack/spack/cmd/ci.py
@@ -318,7 +318,7 @@ def ci_rebuild(args):
     # Fail early if signing is required but we don't have a signing key
     sign_binaries = require_signing is not None and require_signing.lower() == "true"
     if sign_binaries and not spack_ci.can_sign_binaries():
-        gpg_util.list(False, True)
+        gpg_util.glist(False, True)
         tty.die("SPACK_REQUIRE_SIGNING=True => spack must have exactly one signing key")
 
     # Construct absolute paths relative to current $CI_PROJECT_DIR

lib/spack/spack/cmd/gpg.py
@@ -24,7 +24,8 @@ def setup_parser(subparser: argparse.ArgumentParser) -> None:
     subparsers = subparser.add_subparsers(help="GPG sub-commands")
 
     trust = subparsers.add_parser("trust", help=gpg_trust.__doc__)
-    trust.add_argument("keyfile", type=str, help="add a key to the trust store")
+    trust.add_argument("keyfile", type=str, help="add a keyfile to the trust store")
+    arguments.add_common_arguments(trust, ["yes_to_all"])
     trust.set_defaults(func=gpg_trust, subparser=trust)
 
     untrust = subparsers.add_parser("untrust", help=gpg_untrust.__doc__)
@@ -57,15 +58,22 @@ def setup_parser(subparser: argparse.ArgumentParser) -> None:
     )
     create.set_defaults(func=gpg_create, subparser=create)
 
-    list = subparsers.add_parser("list", help=gpg_list.__doc__)
-    list.add_argument("--trusted", action="store_true", default=True, help="list trusted keys")
-    list.add_argument(
+    glist = subparsers.add_parser("list", help=gpg_list.__doc__)
+    glist.add_argument(
+        "--fmt",
+        "-f",
+        action="store",
+        help="Format to list keys with (default (gpg), colons, keys, <key format string>)",
+    )
+    glist.add_argument("--trusted", action="store_true", default=True, help="list trusted keys")
+    glist.add_argument(
         "--signing", action="store_true", help="list keys which may be used for signing"
     )
-    list.set_defaults(func=gpg_list, subparser=list)
+    glist.set_defaults(func=gpg_list, subparser=glist)
 
     init = subparsers.add_parser("init", help=gpg_init.__doc__)
     init.add_argument("--from", metavar="DIR", type=str, dest="import_dir", help=argparse.SUPPRESS)
+    arguments.add_common_arguments(init, ["yes_to_all"])
     init.set_defaults(func=gpg_init, subparser=init)
 
     export = subparsers.add_parser("export", help=gpg_export.__doc__)
@@ -141,6 +149,7 @@ def gpg_create(args):
     if args.export or args.secret:
         new_sec_keys = set(spack.util.gpg.signing_keys())
         new_keys = new_sec_keys.difference(old_sec_keys)
+        new_keys = [str(k) for k in new_keys]
 
     if args.export:
         spack.util.gpg.export_keys(args.export, new_keys)
@@ -152,18 +161,18 @@ def gpg_export(args):
     """export a gpg key, optionally including secret key"""
     keys = args.keys
     if not keys:
-        keys = spack.util.gpg.signing_keys()
+        keys = [str(k) for k in spack.util.gpg.signing_keys()]
     spack.util.gpg.export_keys(args.location, keys, args.secret)
 
 
 def gpg_list(args):
     """list keys available in the keyring"""
-    spack.util.gpg.list(args.trusted, args.signing)
+    spack.util.gpg.glist(args.trusted, args.signing, args.fmt)
 
 
 def gpg_trust(args):
     """add a key to the keyring"""
-    spack.util.gpg.trust(args.keyfile)
+    spack.util.gpg.trust(args.keyfile, args.yes_to_all)
 
 
 def gpg_init(args):
@@ -176,7 +185,7 @@ def gpg_init(args):
         for filename in filenames:
             if not filename.endswith(".key"):
                 continue
-            spack.util.gpg.trust(os.path.join(root, filename))
+            spack.util.gpg.trust(os.path.join(root, filename), args.yes_to_all)
 
 
 def gpg_untrust(args):

lib/spack/spack/test/cmd/gpg.py
@@ -48,7 +48,6 @@ def test_find_gpg(cmd_name, version, tmp_path: pathlib.Path, mock_gnupghome, mon
     else:
         spack.util.gpg.init(force=True)
         assert spack.util.gpg.GPG is not None
-        assert spack.util.gpg.GPGCONF is not None
 
 
 def test_no_gpg_in_path(tmp_path: pathlib.Path, mock_gnupghome, monkeypatch, mutable_config):
@@ -63,7 +62,7 @@ def test_gpg(tmp_path: pathlib.Path, mutable_config, mock_gnupghome):
     MOCK_KEY = "B27095DEEF1787C3C8C85917DCA0241840A5DAE2"
 
     # Import the default key.
-    gpg("init", "--from", mock_gpg_keys_path)
+    gpg("init", "-y", "--from", mock_gpg_keys_path)
 
     # List the keys.
     # TODO: Test the output here.
@@ -90,7 +89,7 @@ def test_gpg(tmp_path: pathlib.Path, mutable_config, mock_gnupghome):
         "Spack testing 1",
         "spack@googlegroups.com",
     )
-    keyfp = spack.util.gpg.signing_keys()[0]
+    keyfp = spack.util.gpg.signing_keys()[0].fpr
 
     # List the keys.
     # TODO: Test the output here.
@@ -147,7 +146,8 @@ def test_gpg(tmp_path: pathlib.Path, mutable_config, mock_gnupghome):
     assert out.count(keyfp) == 0
 
     # Trust the exported public key (keyfpr)
-    gpg("trust", str(export_path))
+    # Trust the exported key.
+    gpg("trust", "-y", str(export_path))
     out = gpg("list", "--signing")
     assert out.count("sec ") == 1
     assert out.count("pub ") == 2

lib/spack/spack/test/util/util_gpg.py
@@ -4,6 +4,7 @@
 
 import os
 import pathlib
+import time
 
 import pytest
 
@@ -17,60 +18,65 @@ def has_socket_dir():
 
 
 def test_parse_gpg_output_case_one():
+    now = int(time.time())
     # Two keys, fingerprint for primary keys, but not subkeys
-    output = """sec::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA:AAAAAAAAAA:::::::::
+    output = f"""sec:-:2048:1:AAAAAAAAAAAAAAAA:{now}:{now}::-:::CESces::::::23:{now}:0:
 fpr:::::::::XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:
-uid:::::::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>:
-ssb::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA::::::::::
-sec::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA:AAAAAAAAAA:::::::::
+uid:-::::{now}::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>::::::::::0:
+ssb:-:2048:1:AAAAAAAAAAAAAAAA:{now}:::-:::ces::::::23:
+sec:-:2048:1:AAAAAAAAAAAAAAAA:{now}:{now}::-:::CESces::::::23::0:
 fpr:::::::::YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY:
-uid:::::::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>:
-ssb::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA::::::::::
+uid:-::::{now}::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>::::::::::0:
+ssb:-:2048:1:AAAAAAAAAAAAAAAA:{now}:::-:::ces::::::23:
 """
-    keys = spack.util.gpg._parse_secret_keys_output(output)
+    keys = spack.util.gpg._parse_gpg_output(output)
 
     assert len(keys) == 2
-    assert keys[0] == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-    assert keys[1] == "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+    assert keys[0].fpr == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    assert keys[1].fpr == "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+
+    assert f"{keys[0]:colons}" in output
+    assert f"{keys[1]:colons}" in output
 
 
 def test_parse_gpg_output_case_two():
+    now = int(time.time())
     # One key, fingerprint for primary key as well as subkey
-    output = """sec:-:2048:1:AAAAAAAAAA:AAAAAAAA:::-:::escaESCA:::+:::23::0:
+    output = f"""sec:-:2048:1:AAAAAAAAAA:{now}:::-:::escaESCA:::+:::23::0:
 fpr:::::::::XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:
 grp:::::::::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA:
-uid:-::::AAAAAAAAA::AAAAAAAAA::Joe (Test) <j.s@s.com>::::::::::0:
-ssb:-:2048:1:AAAAAAAAA::::::esa:::+:::23:
+uid:-::::{now}::AAAAAAAAA::Joe (Test) <j.s@s.com>::::::::::0:
+ssb:-:2048:1:AAAAAAAAA:{now}:::::esa:::+:::23:
 fpr:::::::::YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY:
 grp:::::::::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA:
 """
-    keys = spack.util.gpg._parse_secret_keys_output(output)
+    keys = spack.util.gpg._parse_gpg_output(output)
 
     assert len(keys) == 1
-    assert keys[0] == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    assert keys[0].fpr == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 
 def test_parse_gpg_output_case_three():
+    now = int(time.time())
     # Two keys, fingerprint for primary keys as well as subkeys
-    output = """sec::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA:AAAAAAAAAA:::::::::
+    output = f"""sec::2048:1:AAAAAAAAAAAAAAAA:{now}:{now}:::::::::
 fpr:::::::::WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW:
-uid:::::::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>:
-ssb::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA::::::::::
+uid:::::{now}::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>:
+ssb::2048:1:AAAAAAAAAAAAAAAA:{now}::::::::::
 fpr:::::::::XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:
-sec::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA:AAAAAAAAAA:::::::::
+sec::2048:1:AAAAAAAAAAAAAAAA:{now}:{now}:::::::::
 fpr:::::::::YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY:
-uid:::::::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>:
-ssb::2048:1:AAAAAAAAAAAAAAAA:AAAAAAAAAA::::::::::
+uid:::::{now}::AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA::Joe (Test) <j.s@s.com>:
+ssb::2048:1:AAAAAAAAAAAAAAAA:{now}::::::::::
 fpr:::::::::ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ:"""
 
-    keys = spack.util.gpg._parse_secret_keys_output(output)
+    keys = spack.util.gpg._parse_gpg_output(output)
 
     assert len(keys) == 2
-    assert keys[0] == "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
-    assert keys[1] == "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+    assert keys[0].fpr == "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+    assert keys[1].fpr == "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
 
 
-@pytest.mark.requires_executables("gpg2")
 def test_really_long_gnupghome_dir(tmp_path: pathlib.Path, has_socket_dir):
     if not has_socket_dir:
         pytest.skip("This test requires /var/run/user/$(id -u)")
@@ -87,4 +93,83 @@ def test_really_long_gnupghome_dir(tmp_path: pathlib.Path, has_socket_dir):
         spack.util.gpg.create(
             name="Spack testing 1", email="test@spack.io", comment="Spack testing key", expires="0"
         )
-        spack.util.gpg.list(True, True)
+        spack.util.gpg.glist(True, True)
+
+
+def test_gpg_capabilities_case_insensitvie():
+    for e in spack.util.gpg.GpgKeyCapability:
+        assert e == spack.util.gpg.GpgKeyCapability(e.value.lower())
+        assert e == spack.util.gpg.GpgKeyCapability(e.value.upper())
+
+    assert spack.util.gpg.GpgKeyCapability("q") == spack.util.gpg.GpgKeyCapability.UNKNOWN
+
+
+def test_gpg_trust_case_insensitive():
+    for e in spack.util.gpg.GpgKeyTrust:
+        assert e == spack.util.gpg.GpgKeyTrust(e.value.lower())
+        assert e == spack.util.gpg.GpgKeyTrust(e.value.upper())
+
+    for alt_unknown in ("i", "o"):
+        assert (
+            spack.util.gpg.GpgKeyTrust(alt_unknown.lower()) == spack.util.gpg.GpgKeyTrust.UNKNOWN
+        )
+        assert (
+            spack.util.gpg.GpgKeyTrust(alt_unknown.upper()) == spack.util.gpg.GpgKeyTrust.UNKNOWN
+        )
+
+    assert spack.util.gpg.GpgKeyTrust("v") == spack.util.gpg.GpgKeyTrust.ERROR
+
+
+def test_gpg_trust_ownertrust():
+    assert 0 == spack.util.gpg.GpgKeyTrust.UNKNOWN.ownertrust
+    assert 1 == spack.util.gpg.GpgKeyTrust.EXPIRED.ownertrust
+    assert 2 == spack.util.gpg.GpgKeyTrust.UNDEFINED.ownertrust
+    assert 3 == spack.util.gpg.GpgKeyTrust.NEVER.ownertrust
+    assert 4 == spack.util.gpg.GpgKeyTrust.MARGINAL.ownertrust
+    assert 5 == spack.util.gpg.GpgKeyTrust.FULL.ownertrust
+    assert 6 == spack.util.gpg.GpgKeyTrust.ULTIMATE.ownertrust
+    assert 7 == spack.util.gpg.GpgKeyTrust.REVOKED.ownertrust
+    assert 8 == spack.util.gpg.GpgKeyTrust.ERROR.ownertrust
+    assert 8 == spack.util.gpg.GpgKeyTrust.KNOWN.ownertrust
+    assert 8 == spack.util.gpg.GpgKeyTrust.SPECIAL.ownertrust
+
+    assert spack.util.gpg.GpgKeyTrust(0) == spack.util.gpg.GpgKeyTrust.UNKNOWN
+    assert spack.util.gpg.GpgKeyTrust(1) == spack.util.gpg.GpgKeyTrust.EXPIRED
+    assert spack.util.gpg.GpgKeyTrust(2) == spack.util.gpg.GpgKeyTrust.UNDEFINED
+    assert spack.util.gpg.GpgKeyTrust(3) == spack.util.gpg.GpgKeyTrust.NEVER
+    assert spack.util.gpg.GpgKeyTrust(4) == spack.util.gpg.GpgKeyTrust.MARGINAL
+    assert spack.util.gpg.GpgKeyTrust(5) == spack.util.gpg.GpgKeyTrust.FULL
+    assert spack.util.gpg.GpgKeyTrust(6) == spack.util.gpg.GpgKeyTrust.ULTIMATE
+    assert spack.util.gpg.GpgKeyTrust(7) == spack.util.gpg.GpgKeyTrust.REVOKED
+    assert spack.util.gpg.GpgKeyTrust(8) == spack.util.gpg.GpgKeyTrust.ERROR
+
+    assert spack.util.gpg.GpgKeyTrust(9) == spack.util.gpg.GpgKeyTrust.ERROR
+
+
+def test_gpg_key_algorithm():
+    with pytest.raises(
+        ValueError, match="GpgKeyAlgorithm can only be constructed from another Enum or an `int`"
+    ):
+        spack.util.gpg.GpgKeyAlgorithm("need a number")
+
+    assert spack.util.gpg.GpgKeyAlgorithm(40) == spack.util.gpg.GpgKeyAlgorithm.UNKNOWN
+    assert spack.util.gpg.GpgKeyAlgorithm(300) == spack.util.gpg.GpgKeyAlgorithm.LIBGCRYPT
+
+    for e in spack.util.gpg.GpgKeyAlgorithm:
+        assert str(e) == f"{e}"
+        assert "2048" in f"{e:2048}"
+
+
+def test_gpg_key_type():
+    str_to_type = [
+        ("pub", spack.util.gpg.GpgKeyType.PUBLIC),
+        ("sub", spack.util.gpg.GpgKeyType.PUBLIC_SUBKEY),
+        ("sec", spack.util.gpg.GpgKeyType.SECRET),
+        ("ssb", spack.util.gpg.GpgKeyType.SECRET_SUBKEY),
+        ("sec#", spack.util.gpg.GpgKeyType.SECRET_SUBKEY_ONLY),
+        ("rvk", spack.util.gpg.GpgKeyType.REVOCATION),
+    ]
+
+    for s, t in str_to_type:
+        assert s == str(t)
+        assert spack.util.gpg.GpgKeyType(s) == t

commit b0a5a697c61c9ca8ea7d8787cd5ff663a487e52d

binary_distribution: show fingerprints in PickKeyException
select_signing_key raised PickKeyException(str(keys)) where keys is a
List[GpgKey]; stringifying the list uses each element's repr, and GpgKey
defines __str__ (the fingerprint) but no __repr__, so the error showed
'<spack.util.gpg.GpgKey object at 0x...>' instead of the fingerprints
needed for 'spack buildcache create -k <key hash>'.

Format one fingerprint per line in the exception instead. Regressed in
5fda204835 (#52430), which changed signing_keys() from returning
List[str] fingerprints to List[GpgKey].

Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

lib/spack/spack/binary_distribution.py
@@ -628,7 +628,7 @@ def select_signing_key() -> str:
     keys = spack.util.gpg.signing_keys()
     num = len(keys)
     if num > 1:
-        raise PickKeyException(str(keys))
+        raise PickKeyException(keys)
     elif num == 0:
         raise NoKeyException(
             "No default key available for signing.\n"
@@ -2913,8 +2913,10 @@ class PickKeyException(spack.error.SpackError):
     Raised when multiple keys can be used to sign.
     """
 
-    def __init__(self, keys):
-        err_msg = "Multiple keys available for signing\n%s\n" % keys
+    def __init__(self, keys: List[spack.util.gpg.GpgKey]):
+        err_msg = "Multiple keys available for signing\n%s\n" % "\n".join(
+            f"  {key}" for key in keys
+        )
         err_msg += "Use spack buildcache create -k <key hash> to pick a key."
         super().__init__(err_msg)
 

lib/spack/spack/test/binary_distribution.py
@@ -1650,3 +1650,17 @@ def test_url_buildcache_hash_from_manifest_name(spec_manifest, result):
             result = URLBuildcacheEntry.hash_from_manifest_name(spec_manifest)
     else:
         assert result == URLBuildcacheEntry.hash_from_manifest_name(spec_manifest)
+
+
+def test_select_signing_key_shows_fingerprints(monkeypatch):
+    class Key:
+        def __init__(self, fpr):
+            self.fpr = fpr
+
+        def __str__(self):
+            return self.fpr
+
+    keys = [Key("AAAA"), Key("BBBB")]
+    monkeypatch.setattr(spack.util.gpg, "signing_keys", lambda *a: keys)
+    with pytest.raises(spack.binary_distribution.PickKeyException, match="AAAA\n  BBBB"):
+        spack.binary_distribution.select_signing_key()