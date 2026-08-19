spack/spack

commit d567e1a5299ec8d0929e1b1bfbdd03825bb70fdb

solver/core.py: refactor AspFunction (#51829)
* ensure the argument type is a tuple
* inline `_id` in `__str__` for a minor perf improvement
* order branch on argument type by most likely in practice to avoid a
  chain of isinstance checks
* use `type(...) is int` to remove a branch on bool that was only needed
  with `isinstance(..., int)`.
* remove redundant inheritance of `AspObject`

Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

* diff.py: fix existing type issue list -> tuple

Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

---------

Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

lib/spack/spack/solver/core.py
@@ -5,7 +5,7 @@
 import importlib
 import pathlib
 from types import ModuleType
-from typing import Any, Callable, NamedTuple, Optional, Tuple, Union
+from typing import Any, Callable, NamedTuple, Optional, Tuple
 
 import spack.platforms
 from spack.llnl.util import lang
@@ -28,34 +28,13 @@ ast_type = _ast_getter("ast_type", "type")
 ast_sym = _ast_getter("symbol", "term")
 
 
-class AspObject:
-    """Object representing a piece of ASP code."""
-
-    __slots__ = ()
-
-
-def _id(thing: Any) -> Union[str, int, AspObject]:
-    """Quote string if needed for it to be a valid identifier."""
-    if thing is True or thing is False:
-        return f'"{thing}"'
-    elif isinstance(thing, (AspObject, int)):
-        return thing
-    else:
-        if isinstance(thing, str):
-            # escape characters that cannot be in clingo strings
-            thing = thing.replace("\\", r"\\")
-            thing = thing.replace("\n", r"\n")
-            thing = thing.replace('"', r"\"")
-        return f'"{thing}"'
-
-
-class AspVar(AspObject):
+class AspVar:
     """Represents a variable in an ASP rule, allows for conditionally generating
     rules"""
 
     __slots__ = ("name",)
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
 
     def __str__(self) -> str:
@@ -63,16 +42,16 @@ class AspVar(AspObject):
 
 
 @lang.key_ordering
-class AspFunction(AspObject):
+class AspFunction:
     """A term in the ASP logic program"""
 
     __slots__ = ("name", "args")
 
-    def __init__(self, name: str, args: Optional[Tuple[Any, ...]] = None) -> None:
+    def __init__(self, name: str, args: Tuple[Any, ...] = ()) -> None:
         self.name = name
-        self.args = () if args is None else tuple(args)
+        self.args = args
 
-    def _cmp_key(self) -> Tuple[str, Optional[Tuple[Any, ...]]]:
+    def _cmp_key(self) -> Tuple[str, Tuple[Any, ...]]:
         return self.name, self.args
 
     def __call__(self, *args: Any) -> "AspFunction":
@@ -99,15 +78,23 @@ class AspFunction(AspObject):
         return AspFunction(self.name, self.args + args)
 
     def __str__(self) -> str:
-        args = f"({','.join(str(_id(arg)) for arg in self.args)})"
-        return f"{self.name}{args}"
+        parts = []
+        for arg in self.args:
+            if type(arg) is str:
+                arg = arg.replace("\\", r"\\").replace("\n", r"\n").replace('"', r"\"")
+                parts.append(f'"{arg}"')
+            elif type(arg) is AspFunction or type(arg) is int or type(arg) is AspVar:
+                parts.append(str(arg))
+            else:
+                parts.append(f'"{arg}"')
+        return f"{self.name}({','.join(parts)})"
 
     def __repr__(self) -> str:
         return str(self)
 
 
 class _AspFunctionBuilder:
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> AspFunction:
         return AspFunction(name)
 
 

lib/spack/spack/cmd/diff.py
@@ -47,12 +47,12 @@ def setup_parser(subparser: argparse.ArgumentParser) -> None:
     )
 
 
-def shift(asp_function):
+def shift(asp_function: asp.AspFunction) -> asp.AspFunction:
     """Transforms ``attr("foo", "bar")`` into ``foo("bar")``."""
-    if not asp_function.args:
+    args = asp_function.args
+    if not args:
         raise ValueError(f"Can't shift ASP function with no arguments: {str(asp_function)}")
-    first, *rest = asp_function.args
-    return asp.AspFunction(first, rest)
+    return asp.AspFunction(args[0], args[1:])
 
 
 def compare_specs(a, b, to_string=False, color=None, ignore_packages=None):

commit 99940142424248ceaf45cc0e3e8126e88bc06165

solver: fix syaml_* asp escaping (#52898)
Fix the following regression introduced in
d567e1a5299ec8d0929e1b1bfbdd03825bb70fdb

```
$ spack -c concretizer:duplicates:max_dupes:cmake:2 solve cmake
concretize.lp:16:24-25: info: operation undefined: (X+-1)
```

which happens because `2` is a syaml_int instead of an int, so
solver/core.py should not assume builtin types and instead use
isinstance when escaping values for asp.

Similar for syaml_str:

```
$ spack -c 'concretizer:os_compatible:{"a\"b":["c"]}' solve --show asp zlib
os_compatible("a"b","c").
```

The idea is to preserve the original performance by manually ordering
the branches from most to least used when possible.

Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

lib/spack/spack/solver/core.py
@@ -62,11 +62,19 @@ class AspFunction:
     def __str__(self) -> str:
         parts = []
         for arg in self.args:
+            # exact type checks first, ordered by frequency
             if type(arg) is str:
                 arg = arg.replace("\\", r"\\").replace("\n", r"\n").replace('"', r"\"")
                 parts.append(f'"{arg}"')
             elif type(arg) is AspFunction or type(arg) is int or type(arg) is AspVar:
                 parts.append(str(arg))
+            # subclasses miss the checks above: config values are syaml_str / syaml_int. bool is
+            # an int subclass, but is not a number in ASP, so it is quoted below.
+            elif isinstance(arg, int) and not isinstance(arg, bool):
+                parts.append(str(arg))
+            elif isinstance(arg, str):
+                arg = arg.replace("\\", r"\\").replace("\n", r"\n").replace('"', r"\"")
+                parts.append(f'"{arg}"')
             else:
                 parts.append(f'"{arg}"')
         return f"{self.name}({','.join(parts)})"

lib/spack/spack/test/concretization/core.py
@@ -5684,3 +5684,14 @@ def test_parallel_edges_in_a_literal_reach_the_solver(mock_packages, config):
 
     with pytest.raises(spack.error.UnsatisfiableSpecError):
         spack.concretize.concretize_one("mpileaks ^mpich@3.0.3 ^mpich@3.0.4")
+
+
+@pytest.mark.regression("51829")
+def test_asp_facts_with_config_values():
+    """Config values are syaml_str / syaml_int subclasses of str / int, and have to be emitted
+    as ASP strings and numbers. Booleans are strings."""
+    fn = spack.solver.core.fn
+    assert str(fn.max_dupes("cmake", syaml.syaml_int(2))) == 'max_dupes("cmake",2)'
+    assert str(fn.max_dupes("cmake", 2)) == 'max_dupes("cmake",2)'
+    assert str(fn.os_compatible(syaml.syaml_str('a"b\\c'), "d")) == r'os_compatible("a\"b\\c","d")'
+    assert str(fn.variant_value("x", True)) == 'variant_value("x","True")'