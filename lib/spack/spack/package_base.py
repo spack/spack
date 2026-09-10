3c4c86f4b2ef930c54dd617dfd084023681e493d
Harmen Stoppels <me@harmenstoppels.nl>
2026-09-10 14:13:48 +0200

    spec.py: simplify VariantMap and FlagMap (#52939)
    Many problems in Python can be solved by removing a layer of indirection. The mutable mapping wrapper would take 2 or 3 Python frames what can be done in a single call to the C-side (even if I'm not the biggest fan of sub-classing builtins)
    
    On old cpython, using `typing.Dict[K, V]` as a base type is actually quite inefficient, so a little bit of `if TYPE_CHECKING` cheating with the base type ensures that we have types for keys and values without runtime overhead.
    
    | Python | `x in m` | `if not m` | `m[k]` | `sorted(m.items())` |
    | ------ | -------- | -------- | -------- | -------- |
    | 3.6 | 314 -> 65 | 170 -> 17 | 176 -> 106 | 3886 -> 750 |
    | 3.8 | 210 -> 59 | 129 -> 18 | 127 -> 67 | 2953 -> 637 |
    | 3.11 | 79 -> 33 | 53 -> 13 | 49 -> 35 | 1510 -> 471 |
    | 3.14 | 89 -> 38 | 58 -> 15 | 42 -> 26 | 1677 -> 600 | 
    
    The validation of `VariantMap.__setitem__` is avoided by making invalid state impossible through `VariantMap.set(...)` and replacing runtime type checks with static type checking.
    
    `spack solve` performance improves mildly, pickle size reduces a bit too.
    
    note: `VariantMap.dict` is kept to ensure old `boost/package.py` doesn't break, cause it used this private attribute explicitly.
    
    ---------
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- lib/spack/spack/package_base.py
+++ lib/spack/spack/package_base.py
@@ -1023,9 +1023,7 @@ class PackageBase(WindowsRPath, PackageViewMixin, metaclass=PackageMeta):
         else:
             v_attrs = cls.versions.get(spec.version, {})
             if "commit" in v_attrs:
-                spec.variants["commit"] = spack.variant.SingleValuedVariant(
-                    "commit", v_attrs["commit"]
-                )
+                spec.variants.set(spack.variant.SingleValuedVariant("commit", v_attrs["commit"]))
                 return
             ref = v_attrs.get("tag") or v_attrs.get("branch")
 
@@ -1059,7 +1057,7 @@ class PackageBase(WindowsRPath, PackageViewMixin, metaclass=PackageMeta):
             sha = spack.util.git.get_commit_sha(url, ref)
 
         if sha:
-            spec.variants["commit"] = spack.variant.SingleValuedVariant("commit", sha)
+            spec.variants.set(spack.variant.SingleValuedVariant("commit", sha))
 
     def resolve_binary_provenance(self):
         """
