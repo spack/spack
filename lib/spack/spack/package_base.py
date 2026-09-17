005e7f4bff1968db26ad43375b9c5d14a65b2bbf
Angelica Loshak <loshak@lanl.gov>
2026-09-17 20:56:21 +0000

    Fix sort_by_pkg_preference, more tests pass now
    Signed-off-by: Angelica Loshak <loshak@lanl.gov>

--- lib/spack/spack/package_base.py
+++ lib/spack/spack/package_base.py
@@ -2876,8 +2876,13 @@ def sort_by_pkg_preference(
     """Sorts the list of versions passed in input according to the preferences in the package. The
     return value does not contain duplicate versions. Most preferred versions first.
     """
-    s = [(v, pkg.versions.get(v, {})) for v in dedupe(versions)]
-    return [v for v, _ in sorted(s, reverse=True, key=concretization_version_order)]
+    vdefs = []
+    for version in dedupe(versions):
+        version_def = pkg.version_definitions(version)
+        if version_def:
+            vdefs.append(version_def[-1][1])
+
+    return [vdef.version for vdef in sorted(vdefs, reverse=True, key=concretization_version_order)]
 
 
 def concretization_version_order(
