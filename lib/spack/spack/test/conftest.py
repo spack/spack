8f412f2d25385ae8ec543acada3dfe8e5cd07227
Peter Scheibel <scheibel1@llnl.gov>
2026-09-17 17:40:59 -0700

    rewrite var reference so it doesn't trip an import check

--- lib/spack/spack/test/conftest.py
+++ lib/spack/spack/test/conftest.py
@@ -123,10 +123,10 @@ def mock_spack_instance(tmp_path, set_home, monkeypatch, clear_env_vars, modifie
     base_prefix.mkdir()
 
     real_etc_spack = Path(spack.paths.prefix) / "etc" / "spack"
-    sim_etc_spack = base_prefix / "etc" / "spack"
-    sim_etc_spack.parent.mkdir(parents=True, exist_ok=True)
+    simulated_etc = base_prefix / "etc" / "spack"
+    simulated_etc.parent.mkdir(parents=True, exist_ok=True)
     shutil.copytree(
-        real_etc_spack, sim_etc_spack, ignore=shutil.ignore_patterns("isolate", "layout")
+        real_etc_spack, simulated_etc, ignore=shutil.ignore_patterns("isolate", "layout")
     )
 
     set_home(str(home_dir))
