b5b1ddb1690df6b8da3bde0187e4c815bbc69e40
Seth R Johnson <johnsonsr@ornl.gov>
2026-09-23 11:03:51 -0400

    Update commands
    Signed-off-by: Seth R Johnson <johnsonsr@ornl.gov>

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -1212,7 +1212,7 @@ _spack_external() {
 _spack_external_find() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help --not-buildable --exclude -p --path --scope --all -t --tag -j --jobs"
+        SPACK_COMPREPLY="-h --help --not-buildable --exclude -p --path --scope --cray-manifest -j --jobs --all -t --tag"
     else
         _all_packages
     fi
